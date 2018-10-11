6fabb49549631c8cc495b882af71b2411830edfa
Kris Koskelin <kkoskelin@flexion.us>
2018-10-11 16:25:24 -0500

    functional test for USA banner

--- web-client/cypress/integration/starter_spec.js
+++ web-client/cypress/integration/starter_spec.js
@@ -14,4 +14,20 @@ describe('App Component first page', function() {
     cy.get('#hello-button').click();
     cy.get('#response').should('contain.text', 'Hello World!');
   });
+
+  describe('USA Banner', () => {
+    it('shows header and hides content', () => {
+      cy.contains('.usa-banner-header', 'how you know');
+      cy.get('.usa-banner-content').should('not.exist');
+    });
+    it('shows reveals content when clicked, hides when clicked again', () => {
+      cy.get('.usa-banner-button').click();
+      cy.contains(
+        '.usa-banner-content',
+        'you are connecting to the official website',
+      );
+      cy.get('.usa-banner-button').click();
+      cy.get('.usa-banner-content').should('not.exist');
+    });
+  });
 });
