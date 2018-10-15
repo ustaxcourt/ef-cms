604c4f8b3e193c4ae941d2cf0b33b5ef7e0db60f
Cody Seibert <cseibert113@gmail.com>
2018-10-15 10:51:21 -0400

    Various changes to try and get sonarqube working in Jenkins

--- web-client/run-sonar.sh
+++
@@ -1,5 +0,0 @@
-docker run -ti -v $(pwd):/root/src -f Dockerfile.sonar sonar-scanner \
-  -Dsonar.login=admin \
-  -Dsonar.password=$SONAR_PASSWORD \
-  -Dsonar.host.url=https://$(cd ../management/management/ && terraform output sonarqube_url) \
-  -Dsonar.projectBaseDir=./src
