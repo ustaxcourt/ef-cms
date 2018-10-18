0d69a6274486b9be17420cc4b0f2dc1d99fcf1d2
Cody Seibert <cseibert113@gmail.com>
2018-10-18 17:43:33 -0400

    adding pipeline verification to break the pipeline when no changes are detected in the sub directories

---
+++ web-client/exit-when-no-changes.sh
@@ -0,0 +1,5 @@
+COUNT=$(git diff --name-only develop | grep web-client | wc -l)
+if [ $COUNT == "0" ] ; then
+  echo "No changes detected inside the web-client folder; exiting ending the pipeline";
+  exit 1;
+fi
