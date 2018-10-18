d73d3d401ea8b6fe027d692a7f56447659d8d3d4
Cody Seibert <cseibert113@gmail.com>
2018-10-18 18:15:28 -0400

    trying with expressions

--- serverless-api/exit-when-no-changes.sh
+++ serverless-api/exit-when-no-changes.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 COUNT=$(git diff --name-only origin/develop | grep serverless-api | wc -l)
 if [ $COUNT == "0" ] ; then
-  echo "No changes detected inside the serverless-api folder; exiting ending the pipeline";
-  exit 1;
+  echo "SKIP"
+else
+  echo "CONTINUE"
 fi
