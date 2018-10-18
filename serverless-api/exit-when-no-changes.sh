9e4f1f1b026ce17f0433fe6284b4a772d02827fe
Cody Seibert <cseibert113@gmail.com>
2018-10-18 18:01:47 -0400

    adding bash

--- serverless-api/exit-when-no-changes.sh
+++ serverless-api/exit-when-no-changes.sh
@@ -1,8 +1,6 @@
-DIFFS=$(git diff --name-only origin/develop)
-GREP=$(echo $DIFFS | grep serverless-api)
-WC=$(echo $GREP | wc -l)
-# COUNT=$(git diff --name-only origin/develop | grep serverless-api | wc -l)
-if [ $WC == "0" ] ; then
+#!/bin/bash
+COUNT=$(git diff --name-only origin/develop | grep serverless-api | wc -l)
+if [ $COUNT == "0" ] ; then
   echo "No changes detected inside the serverless-api folder; exiting ending the pipeline";
   exit 1;
 fi
--- web-client/exit-when-no-changes.sh
+++ web-client/exit-when-no-changes.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 COUNT=$(git diff --name-only origin/develop | grep web-client | wc -l)
 if [ $COUNT == "0" ] ; then
   echo "No changes detected inside the web-client folder; exiting ending the pipeline";
