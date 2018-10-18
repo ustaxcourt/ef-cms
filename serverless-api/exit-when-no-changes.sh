13dc3150a77b5e2155b1061336fcef566606ac21
Cody Seibert <cseibert113@gmail.com>
2018-10-18 17:59:41 -0400

    checking wc

--- serverless-api/exit-when-no-changes.sh
+++ serverless-api/exit-when-no-changes.sh
@@ -1,5 +1,8 @@
-COUNT=$(git diff --name-only origin/develop | grep serverless-api | wc -l)
-if [ $COUNT == "0" ] ; then
+DIFFS=$(git diff --name-only origin/develop)
+GREP=$(echo $DIFFS | grep serverless-api)
+WC=$(echo $GREP | wc -l)
+# COUNT=$(git diff --name-only origin/develop | grep serverless-api | wc -l)
+if [ $WC == "0" ] ; then
   echo "No changes detected inside the serverless-api folder; exiting ending the pipeline";
   exit 1;
 fi
