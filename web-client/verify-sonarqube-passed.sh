15bcf3811c78cf2526c30d460bed3fbce0764928
Cody Seibert <cseibert113@gmail.com>
2018-10-22 16:47:30 -0400

    fixing sonar location

--- web-client/verify-sonarqube-passed.sh
+++ web-client/verify-sonarqube-passed.sh
@@ -1,6 +1,6 @@
 FULL_URL="https://sonarcloud.io"
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-sonar-scanner-3.2.0.1227-macosx/bin/sonar-scanner -Dsonar.branch.name=${BRANCH_NAME} -Dsonar.organization=flexion-github -Dsonar.projectBaseDir=. -Dsonar.login=${SONAR_TOKEN} -Dsonar.host.url=$FULL_URL
+sonar-scanner -Dsonar.branch.name=${BRANCH_NAME} -Dsonar.organization=flexion-github -Dsonar.projectBaseDir=. -Dsonar.login=${SONAR_TOKEN} -Dsonar.host.url=$FULL_URL
 sleep 5
 PROJECT_KEY=$(grep sonar.projectKey sonar-project.properties | sed 's/sonar.projectKey=\(.*\)/\1/')
 CURL_URL="$FULL_URL/api/qualitygates/project_status?projectKey=$PROJECT_KEY&branch=${BRANCH_NAME}"
