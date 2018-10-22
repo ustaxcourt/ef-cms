60b8604242271c77f83e402422be4f95af0d635e
Will Sadler <wsadler@flexion.us>
2018-10-22 17:21:05 -0500

    * add sonar for serverless api

---
+++ serverless-api/verify-sonarqube-passed.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FULL_URL="https://sonarcloud.io"
+sonar-scanner -Dsonar.branch.name=${branch_name} -Dsonar.organization=flexion-github -Dsonar.projectBaseDir=. -Dsonar.login=${EF_CMS_API_SONAR_TOKEN} -Dsonar.host.url=$FULL_URL
+sleep 5
+PROJECT_KEY=$(grep sonar.projectKey sonar-project.properties | sed 's/sonar.projectKey=\(.*\)/\1/')
+CURL_URL="$FULL_URL/api/qualitygates/project_status?projectKey=$PROJECT_KEY&branch=${branch_name}"
+JSON=$(curl -u admin:$SONAR_PASSWORD -X GET -H 'Accept: application/json' $CURL_URL)
+STATUS=$(echo $JSON | jq -r ".projectStatus.status")
+if [[ $STATUS == 'ERROR' ]] ; then
+  echo "SonarQube Failed"
+  exit 1;
+fi
