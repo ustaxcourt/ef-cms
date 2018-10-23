ad6fab30cf8b0ca5c65bf2be59dc448119391aec
Cody Seibert <cseibert113@gmail.com>
2018-10-23 11:53:04 -0400

    forcing failure

--- serverless-api/verify-sonarqube-passed.sh
+++ serverless-api/verify-sonarqube-passed.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 FULL_URL="https://sonarcloud.io"
-sonar-scanner -Dsonar.branch.name=${branch_name} -Dsonar.organization=flexion-github -Dsonar.projectBaseDir=. -Dsonar.login=${EF_CMS_API_SONAR_TOKEN} -Dsonar.host.url=$FULL_URL
+OUTPUT=$(sonar-scanner -Dsonar.branch.name=${branch_name} -Dsonar.organization=flexion-github -Dsonar.projectBaseDir=. -Dsonar.login=${SONAR_TOKEND} -Dsonar.host.url=$FULL_URL)
 sleep 10
 PROJECT_KEY=$(grep sonar.projectKey sonar-project.properties | sed 's/sonar.projectKey=\(.*\)/\1/')
 CURL_URL="$FULL_URL/api/qualitygates/project_status?projectKey=$PROJECT_KEY&branch=${branch_name}"
 JSON=$(curl -u admin:$SONAR_PASSWORD -X GET -H 'Accept: application/json' $CURL_URL)
 STATUS=$(echo $JSON | jq -r ".projectStatus.status")
-if [[ $STATUS == 'ERROR' ]] ; then
+CONTAINS_FAILURE=$(echo $OUTPUT | grep 'EXECUTION FAILURE')
+if [[ $STATUS == 'ERROR' ]] || [[ $CONTAINS_FAILURE != '' ]] ; then
   echo "SonarQube Failed"
   exit 1;
 fi
-
-exit 1;
