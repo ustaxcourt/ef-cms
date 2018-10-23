fa25886ec825392e9daa70d30e85965120dd8db4
Cody Seibert <cseibert113@gmail.com>
2018-10-23 11:36:39 -0400

    testing to see if a failed sonarqube actually fails jenkins

--- web-client/verify-sonarqube-passed.sh
+++ web-client/verify-sonarqube-passed.sh
@@ -1,6 +1,6 @@
 FULL_URL="https://sonarcloud.io"
 sonar-scanner -Dsonar.branch.name=${branch_name} -Dsonar.organization=flexion-github -Dsonar.projectBaseDir=. -Dsonar.login=${SONAR_TOKEN} -Dsonar.host.url=$FULL_URL
-sleep 5
+sleep 10
 PROJECT_KEY=$(grep sonar.projectKey sonar-project.properties | sed 's/sonar.projectKey=\(.*\)/\1/')
 CURL_URL="$FULL_URL/api/qualitygates/project_status?projectKey=$PROJECT_KEY&branch=${branch_name}"
 JSON=$(curl -u admin:$SONAR_PASSWORD -X GET -H 'Accept: application/json' $CURL_URL)
