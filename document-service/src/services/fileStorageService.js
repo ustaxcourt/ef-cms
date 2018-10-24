38606a48233a9388db8f763c37fc4cbbc4994dfd
Cody Seibert <cseibert113@gmail.com>
2018-10-24 16:14:19 -0400

    adding the create upload policy endpoint

---
+++ document-service/src/services/fileStorageService.js
@@ -0,0 +1,21 @@
+const { S3 } = require('aws-sdk');
+
+const s3 = new S3({
+  region: 'us-east-1',
+  s3ForcePathStyle: true,
+  endpoint: process.env.S3_ENDPOINT
+});
+
+exports.createUploadPolicy = () => {
+  return new Promise((resolve, reject) => {
+    s3.createPresignedPost({
+      Bucket: process.env.DOCUMENTS_BUCKET_NAME,
+      Conditions: [
+        ['starts-with', '$key', ''],
+      ],
+    }, (err, data) => {
+      if (err) return reject(err);
+      resolve(data);
+    });
+  });
+}
