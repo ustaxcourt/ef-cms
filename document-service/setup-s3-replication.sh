06ddb2e3ada572e461b6b0989fb50c24b89ed56b
Cody Seibert <cseibert113@gmail.com>
2018-10-25 17:42:17 -0400

    creating iam role; enable versioning on the buckets; creating s3 replication script

---
+++ document-service/setup-s3-replication.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+stage=$1
+BUCKET="efcms-documents-${stage}-us-east-1"
+backup_bucket="efcms-documents-${stage}-us-east-2"
+accountWithQuotes=$(aws sts get-caller-identity --query "Account")
+account=$(echo "${accountWithQuotes}" | sed s/\"/\ /g)
+CONFIGURATION=$(sed "s/ACCOUNT_NUMBER/${account}/g" replication-configuration.json | sed "s/BACKUP_BUCKET/${backup_bucket}/g")
+aws s3api put-bucket-replication --bucket "${BUCKET}" --replication-configuration "${CONFIGURATION}"
