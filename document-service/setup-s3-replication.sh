8e18bacd390697d879ab91241efdee6df7789f24
Cody Seibert <cseibert113@gmail.com>
2018-10-26 10:41:42 -0400

    fixing shellcheck

--- document-service/setup-s3-replication.sh
+++ document-service/setup-s3-replication.sh
@@ -3,6 +3,7 @@ stage=$1
 BUCKET="efcms-documents-${stage}-us-east-1"
 backup_bucket="efcms-documents-${stage}-us-east-2"
 accountWithQuotes=$(aws sts get-caller-identity --query "Account")
-account=$(echo "${accountWithQuotes}" | sed s/\"/\ /g)
+accountWithQuotePrefix="${accountWithQuotes%\"}"
+account="${accountWithQuotePrefix#\"}"
 CONFIGURATION=$(sed "s/ACCOUNT_NUMBER/${account}/g" replication-configuration.json | sed "s/BACKUP_BUCKET/${backup_bucket}/g")
 aws s3api put-bucket-replication --bucket "${BUCKET}" --replication-configuration "${CONFIGURATION}"
