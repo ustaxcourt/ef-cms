327eba82997a00848a2cff5497c93a50bb0c328d
Eric Sorenson <eric@sorenson.software>
2018-10-26 09:22:45 -0500

    upload the file, not the file name

--- web-client/src/main/actions.js
+++ web-client/src/main/actions.js
@@ -1,21 +1,35 @@
 import { state } from 'cerebral';
 import Petition from '../entities/petition';
 
-export const getDocumentPolicy = async ({ api, environment, path }) => {
+export const getDocumentPolicy = async ({ api, environment, store, path }) => {
   let response;
   try {
     response = await api.getDocumentPolicy(environment.getBaseUrl());
-    return path.success({ policy: response });
+    store.set(state.petition.policy, response);
+    return path.success();
   } catch (error) {
-    return path.error({ alertError: 'Document policy retrieval failed' });
+    store.set(state.alertError, 'Document policy retrieval failed');
   }
 };
 
-export const addDocumentToUser = async ({ api, environment }) => {
-  const response = await api.addDocumentToUser(environment.getBaseUrl());
-  return {
-    response,
-  };
+export const addDocumentToUser = async ({
+  api,
+  environment,
+  store,
+  path,
+  get,
+}) => {
+  try {
+    const response = await api.addDocumentToUser(
+      environment.getBaseUrl(),
+      get(state.user),
+      'type',
+    );
+    store.set(state.petition.id, response);
+    return path.success();
+  } catch (error) {
+    store.set(state.alertError, 'Adding document to user failed');
+  }
 };
 
 export const uploadDocumentToS3 = async ({ api, get }) => {
