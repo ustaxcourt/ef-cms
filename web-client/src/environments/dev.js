1075f93cf0ac158de4a3b0d63145f0535440a2a6
Eric Sorenson <eric@sorenson.software>
2018-10-29 15:03:15 -0500

    make route a provider, working on tests

--- web-client/src/environments/dev.js
+++ web-client/src/environments/dev.js
@@ -1,6 +1,5 @@
 // const API_URL = 'http://localhost:3000/v1';
-const API_URL =
-  'https://fvtnozpme2.execute-api.us-east-1.amazonaws.com/will/v1';
+const API_URL = 'https://7wjn89p2z4.execute-api.us-east-1.amazonaws.com/dev/v1';
 
 /**
  * Context for the dev environment
--- web-client/src/main/providers.js
+++ web-client/src/main/providers.js
@@ -2,46 +2,35 @@ import axios from 'axios';
 
 export const api = {
   getDocumentPolicy: async baseUrl => {
-    try {
-      const response = await axios.get(`${baseUrl}/documents/policy`);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
+    const response = await axios.get(`${baseUrl}/documents/policy`);
+    return response.data;
   },
 
   getDocumentId: async (baseUrl, user, type) => {
-    try {
-      const response = await axios.post(`${baseUrl}/documents`, {
-        userId: user,
-        documentType: type,
-      });
-      return response.data;
-    } catch (error) {
-      return error;
-    }
+    const response = await axios.post(`${baseUrl}/documents`, {
+      userId: user,
+      documentType: type,
+    });
+    return response.data;
   },
 
   uploadDocumentToS3: async (policy, documentId, file) => {
-    try {
-      let formData = new FormData();
-      formData.append('key', documentId);
-      formData.append('X-Amz-Algorithm', policy.fields['X-Amz-Algorithm']);
-      formData.append('X-Amz-Credential', policy.fields['X-Amz-Credential']);
-      formData.append('X-Amz-Date', policy.fields['X-Amz-Date']);
-      formData.append(
-        'X-Amz-Security-Token',
-        policy.fields['X-Amz-Security-Token'],
-      );
-      formData.append('Policy', policy.fields.Policy);
-      formData.append('X-Amz-Signature', policy.fields['X-Amz-Signature']);
-      formData.append('file', file, file.name);
-      await axios.post(policy.url, formData, {
-        headers: { 'Content-Type': 'multipart/form-data' },
-      });
-      return;
-    } catch (error) {
-      return error;
-    }
+    let formData = new FormData();
+    formData.append('key', documentId);
+    formData.append('X-Amz-Algorithm', policy.fields['X-Amz-Algorithm']);
+    formData.append('X-Amz-Credential', policy.fields['X-Amz-Credential']);
+    formData.append('X-Amz-Date', policy.fields['X-Amz-Date']);
+    formData.append(
+      'X-Amz-Security-Token',
+      policy.fields['X-Amz-Security-Token'],
+    );
+    formData.append('Policy', policy.fields.Policy);
+    formData.append('X-Amz-Signature', policy.fields['X-Amz-Signature']);
+    formData.append('file', file, 'file.name');
+    await axios.post(policy.url, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
   },
 };
