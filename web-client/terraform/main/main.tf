928bba2ae72eadb6bf6ed589d299cdbf42285761
Cody Seibert <cseibert113@gmail.com>
2018-10-30 16:45:56 -0400

    refactoring terraform to use same dynamodb lock table and state bucket

--- web-client/terraform/main/main.tf
+++ web-client/terraform/main/main.tf
@@ -11,7 +11,6 @@ module "environment" {
   source = "../common"
 
   environment = "${var.environment}"
-  deployment = "${var.deployment}"
   dns_domain = "${var.dns_domain}"
   cloudfront_default_ttl = "${var.cloudfront_default_ttl}"
   cloudfront_max_ttl = "${var.cloudfront_max_ttl}"
