37b1a6fd3878f7151c9e53f56a239640ff89f7f6
Cody Seibert <cseibert113@gmail.com>
2018-11-01 17:56:00 -0400

    fixes

--- document-service/run-serverless.sh
+++ document-service/run-serverless.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 slsStage=$1
 region=$2
-pushd src
-  find . -type f -exec chmod -R ugo+r {} ";"
-popd
-SLS_DEPLOYMENT_BUCKET=gov.ustaxcourt.ef-cms.apis.${slsStage}.${region}.deploys ./node_modules/.bin/sls create_domain --stage ${slsStage} --region ${region} --verbose
-ENVIRONMENT=${slsStage} SLS_DEPLOYMENT_BUCKET=gov.ustaxcourt.ef-cms.apis.${slsStage}.${region}.deploys ./node_modules/.bin/sls deploy --stage ${slsStage} --region ${region} --verbose
-./configure-custom-api-access-logging.sh ${slsStage} ./config-custom-access-logs.json ${region}
+cd src && find . -type f -exec chmod -R ugo+r {} ";"
+cd ..
+SLS_DEPLOYMENT_BUCKET="gov.ustaxcourt.ef-cms.apis.${slsStage}.${region}.deploys" ./node_modules/.bin/sls create_domain --stage "${slsStage}" --region "${region}" --verbose
+ENVIRONMENT="${slsStage}" SLS_DEPLOYMENT_BUCKET="gov.ustaxcourt.ef-cms.apis.${slsStage}.${region}.deploys" ./node_modules/.bin/sls deploy --stage "${slsStage}" --region "${region}" --verbose
+./configure-custom-api-access-logging.sh "${slsStage}" ./config-custom-access-logs.json "${region}"
