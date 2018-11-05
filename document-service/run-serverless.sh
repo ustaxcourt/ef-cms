d5de3c4773098cce507a174930b9456388508b57
Cody Seibert <cseibert113@gmail.com>
2018-11-05 14:01:52 -0500

    changing buckets to be scoped to dns

--- document-service/run-serverless.sh
+++ document-service/run-serverless.sh
@@ -9,6 +9,7 @@ slsStage=$1
 region=$2
 cd src && find . -type f -exec chmod -R ugo+r {} ";"
 cd ..
-SLS_DEPLOYMENT_BUCKET="${DNS_DOMAIN}.apis.${slsStage}.${region}.deploys" ./node_modules/.bin/sls create_domain --stage "${slsStage}" --region "${region}" --verbose
-ENVIRONMENT="${slsStage}" SLS_DEPLOYMENT_BUCKET="${DNS_DOMAIN}.apis.${slsStage}.${region}.deploys" ./node_modules/.bin/sls deploy --stage "${slsStage}" --region "${region}" --verbose
+SLS_DEPLOYMENT_BUCKET="${DNS_DOMAIN}.documents.${slsStage}.${region}.deploys"
+SLS_DEPLOYMENT_BUCKET="${SLS_DEPLOYMENT_BUCKET}" ./node_modules/.bin/sls create_domain --stage "${slsStage}" --region "${region}" --domain "${DNS_DOMAIN}" --verbose
+ENVIRONMENT="${slsStage}" SLS_DEPLOYMENT_BUCKET="${SLS_DEPLOYMENT_BUCKET}" ./node_modules/.bin/sls deploy --stage "${slsStage}" --region "${region}" --domain "${DNS_DOMAIN}" --verbose
 ./configure-custom-api-access-logging.sh "${slsStage}" ./config-custom-access-logs.json "${region}"
