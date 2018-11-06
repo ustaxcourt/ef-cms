ccb65d30f87e4809f700de70bd7dd5d199d1e3e4
Kris Koskelin <kkoskelin@flexion.us>
2018-11-06 12:03:12 -0600

    #126 starting layout of case detail

--- web-client/src/components/CaseDetail.jsx
+++ web-client/src/components/CaseDetail.jsx
@@ -7,11 +7,47 @@ import { state } from 'cerebral';
  */
 export default connect(
   {
-    docketNumber: state.petition.docketNumber,
+    caseDetail: state.caseDetail,
+    docketNumber: state.docketNumber,
   },
-  function CaseDetail({ docketNumber }) {
+  function CaseDetail({ docketNumber, caseDetail }) {
     return (
-      <React.Fragment>{docketNumber} is your docket number.</React.Fragment>
+      <section className="usa-section usa-grid">
+        <h1>Docket number: {docketNumber}</h1>
+        <p>
+          {caseDetail.petitionerName}, Petitioner v. Commissioner of Internal
+          Revenue Service, Respondent
+        </p>
+        <h2>Required actions</h2>
+        <p>
+          Pay petition filing fee, Deadline: November 7, 2018,{' '}
+          <button>Pay Fee</button>
+        </p>
+        <h2>Case activities</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Date submitted</th>
+              <th>Filings and proceedings</th>
+              <th>Date served</th>
+            </tr>
+          </thead>
+          <tbody>
+            {!caseDetail.activities.length && (
+              <tr>
+                <td colSpan="3">(none)</td>
+              </tr>
+            )}
+            {caseDetail.activities.map((item, idx) => (
+              <tr key={idx}>
+                <td>{item.dateSubmitted}</td>
+                <td>{item.filingsAndProceedings}</td>
+                <td>{item.dateServed}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
     );
   },
 );
