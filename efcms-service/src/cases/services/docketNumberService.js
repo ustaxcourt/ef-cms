4f1cbbd5d444d20c34ef2fefed81cc35e75dadd9
Cody Seibert <cseibert113@gmail.com>
2018-11-05 19:38:47 -0500

    adding some logic to generate a auto incrementing id starting at 1

--- efcms-service/src/cases/services/docketNumberService.js
+++ efcms-service/src/cases/services/docketNumberService.js
@@ -1,4 +1,26 @@
-exports.createDocketNumber = () => {
-  //TODO
-  return '12345-18'
+const client = require('../../services/dynamodbClientService');
+
+const TABLE_NAME =
+  process.env.CASES_DYNAMODB_TABLE || 'efcms-cases-dev';
+
+exports.createDocketNumber = async () => {
+  const params = {
+    TableName: TABLE_NAME,
+    Key: {
+      caseId: 'docketNumberCounter'
+    },
+    UpdateExpression: 'ADD #a :x',
+    ExpressionAttributeNames: {
+      '#a' : "id"
+    },
+    ExpressionAttributeValues: {
+      ':x' : 1
+    },
+    ReturnValues: "UPDATED_NEW"
+  };
+  const id = await client.updateConsistent(params);
+  const plus100 = id + 100;
+  const last2YearDigits = new Date().getFullYear().toString().substr(-2);
+  const pad = `00000${plus100}`.substr(-5);
+  return `${pad}-${last2YearDigits}`;
 }
