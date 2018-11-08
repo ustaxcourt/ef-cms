aea10a90b508f931d6d54c691984b9a185ff7da3
Will Sadler <wsadler@flexion.us>
2018-11-08 17:15:21 -0600

    * initial commit of get work

--- efcms-service/src/cases/middleware/caseMiddleware.js
+++ efcms-service/src/cases/middleware/caseMiddleware.js
@@ -2,7 +2,8 @@ const uuidv4 = require('uuid/v4');
 const client = require('../../middleware/dynamodbClientService');
 const docketNumberService = require('./docketNumberGenerator');
 
-const TABLE_NAME = process.env.STAGE ? `efcms-cases-${process.env.STAGE}` : 'efcms-cases-dev';
+const TABLE_NAME = process.env.STAGE ? `efcms-cases-${process.env.STAGE}` : 'efcms-cases-local';
+
 const NUM_REQUIRED_DOCUMENTS = 3; //because 3 is a magic number
 
 /**
@@ -49,7 +50,36 @@ exports.create = async (userId, documents) => {
     },
   };
   return client.put(params);
+
 };
+
+exports.getCase = async (userId, caseId) => {
+  //TODO add expression to limit to user
+  const params = {
+    TableName: TABLE_NAME,
+    KeyConditionExpression: 'userId = :userId and caseId = :caseId',
+    ExpressionAttributeValues: {
+      ':userId': userId,
+      ':caseId': caseId
+    }
+  };
+  return await client.query(params);
+};
+
+exports.getCases = (userId) => {
+  //TODO add expression to limit to user
+  const params = {
+    TableName: TABLE_NAME,
+    IndexName: "UserIdIndex",
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': userId
+    }
+  };
+  return client.query(params);
+};
+
+
 
 
 
--- efcms-service/src/middleware/S3Service.js
+++ efcms-service/src/middleware/S3Service.js
@@ -20,3 +20,24 @@ exports.createUploadPolicy = () => {
     );
   });
 };
+
+exports.createDownloadPolicy = () => {
+  const s3 = new S3({
+    region: process.env.AWS_REGION,
+    s3ForcePathStyle: true,
+    endpoint: process.env.S3_ENDPOINT,
+  });
+
+  return new Promise((resolve, reject) => {
+    s3.getSignedUrl('getObject',
+      {
+        Bucket: process.env.DOCUMENTS_BUCKET_NAME,
+        Conditions: [['starts-with', '$key', '']],
+      },
+      (err, data) => {
+        if (err) return reject(err);
+        resolve(data);
+      },
+    );
+  });
+};
