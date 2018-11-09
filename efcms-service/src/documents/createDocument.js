6e545e507b57e52a92b1f8c7a01d28df3329713e
Cody Seibert <cseibert113@gmail.com>
2018-11-09 18:57:54 -0500

    refactoring createDocument

--- efcms-service/src/documents/createDocument.js
+++ efcms-service/src/documents/createDocument.js
@@ -1,5 +1,5 @@
-const { createDone } = require('../middleware/apiGatewayHelper');
-const documentService = require('./services/documentService');
+const { createDocument } = require('./services/documentBlobDAO');
+const { handle } = require('../middleware/apiGatewayHelper');
 
 /**
  * Create Document API Lambda
@@ -9,26 +9,31 @@ const documentService = require('./services/documentService');
  * @param callback
  */
 
-exports.create = (event, context, callback) => {
-  const done = createDone(callback);
+// exports.create = (event, context, callback) => {
+//   const done = createDone(callback);
 
-  let body;
-  try {
-    body = JSON.parse(event.body);
-  } catch (error) {
-    done(new Error('problem parsing event body: ' + error));
-    return;
-  }
+//   let body;
+//   try {
+//     body = JSON.parse(event.body);
+//   } catch (error) {
+//     done(new Error('problem parsing event body: ' + error));
+//     return;
+//   }
 
-  if (!body || !body.documentType || !body.userId) {
-    done(new Error('documentType and userId are required'));
-    return;
-  }
+//   if (!body || !body.documentType || !body.userId) {
+//     done(new Error('documentType and userId are required'));
+//     return;
+//   }
 
-  documentService
-    .create(body.userId, body.documentType)
-    .then(document => {
-      done(null, document);
-    })
-    .catch(done);
-};
+//   documentService
+//     .create(body.userId, body.documentType)
+//     .then(document => {
+//       done(null, document);
+//     })
+//     .catch(done);
+// };
+
+exports.create = async event =>
+  handle(() =>
+    createDocument(JSON.parse(event.body))
+  )
--- efcms-service/src/persistence/aws/awsS3Persistence.js
+++ efcms-service/src/persistence/aws/awsS3Persistence.js
@@ -1,5 +1,6 @@
 const { S3 } = require('aws-sdk');
 
+// TODO: use environment to get regions and endpoints, etc
 exports.getDocumentDownloadUrl = ({ documentId }) => {
   const s3 = new S3({
     region: process.env.AWS_REGION,
@@ -19,3 +20,24 @@ exports.getDocumentDownloadUrl = ({ documentId }) => {
     });
   });
 }
+
+exports.createUploadPolicy = () => {
+  const s3 = new S3({
+    region: process.env.AWS_REGION,
+    s3ForcePathStyle: true,
+    endpoint: process.env.S3_ENDPOINT,
+  });
+
+  return new Promise((resolve, reject) => {
+    s3.createPresignedPost(
+      {
+        Bucket: process.env.DOCUMENTS_BUCKET_NAME,
+        Conditions: [['starts-with', '$key', '']],
+      },
+      (err, data) => {
+        if (err) return reject(err);
+        resolve(data);
+      },
+    );
+  });
+};
