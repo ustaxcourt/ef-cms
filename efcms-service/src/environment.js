75923ed7dee22d89003db6e2da6c9e43faeca5b7
Cody Seibert <cseibert113@gmail.com>
2018-11-09 10:32:06 -0500

    refactoring the download policy url to use the persistence factory;

--- efcms-service/src/environment.js
+++ efcms-service/src/environment.js
@@ -3,3 +3,14 @@ exports.get = key => {
     DOCUMENTS_TABLE: process.env.STAGE ? `efcms-documents-${process.env.STAGE}` : 'efcms-documents-local',
   }[key];
 }
+
+exports.entityPersistenceLookup = key => {
+  switch (key) {
+    case 'files':
+      return 'awsPersistence';
+    case 'documents':
+      return 'awsPersistence';
+    default:
+      throw new Error('unsupported key');
+  }
+};
