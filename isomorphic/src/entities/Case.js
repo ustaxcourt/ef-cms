d039530319ddd6821d028520f7059a520f4d474f
Cody Seibert <cseibert113@gmail.com>
2018-11-13 14:42:25 -0500

    adding validation for case

--- isomorphic/src/entities/Case.js
+++ isomorphic/src/entities/Case.js
@@ -1,7 +1,35 @@
-function Case(rawCase) {}
+const Joi = require('joi');
+
+const uuidVersions = {
+  version: ['uuidv4'],
+};
+
+const caseSchema = Joi.object().keys({
+  caseId: Joi.string()
+    .uuid(uuidVersions)
+    .optional(),
+  userId: Joi.string()
+    .uuid(uuidVersions)
+    .optional(),
+  documents: Joi.array()
+    .length(3)
+    .items(
+      Joi.object({
+        documentId: Joi.string()
+          .uuid(uuidVersions)
+          .required(),
+        documentType: Joi.string().required(),
+      }),
+    )
+    .required(),
+});
+
+function Case(rawCase) {
+  this.documents = rawCase.documents;
+}
 
 Case.prototype.isValid = function isValid() {
-  return true;
+  return Joi.validate(this, caseSchema).error === null;
 };
 
 module.exports = Case;
