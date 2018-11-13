4615f21ac7c41b7cb4971e0ba81a39fc4075c0c2
Eric Sorenson <eric@sorenson.software>
2018-11-13 13:11:38 -0600

    splitting case initiator and case

--- isomorphic/src/entities/Case.js
+++ isomorphic/src/entities/Case.js
@@ -1,16 +1,7 @@
-function Case(petition) {
-  this.petitionFile = petition.petitionFile;
-  this.requestForPlaceOfTrial = petition.requestForPlaceOfTrial;
-  this.statementOfTaxpayerIdentificationNumber =
-    petition.statementOfTaxpayerIdentificationNumber;
-}
+function Case(rawCase) {}
 
 Case.prototype.isValid = function isValid() {
-  return (
-    !!this.petitionFile &&
-    !!this.requestForPlaceOfTrial &&
-    !!this.statementOfTaxpayerIdentificationNumber
-  );
+  return true;
 };
 
 module.exports = Case;
