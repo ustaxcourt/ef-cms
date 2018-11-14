4e12259d1f3d50d1a08dde314b4a8be330d65096
Cody Seibert <cseibert113@gmail.com>
2018-11-14 15:55:58 -0500

    fixing issue with policy

--- efcms-service/run-serverless.sh
+++ efcms-service/run-serverless.sh
@@ -2,10 +2,12 @@
 
 slsStage=$1
 region=$2
-accountId=$(aws sts get-caller-identity --query "Account")
+ACCOUNT_ID=$(aws sts get-caller-identity --query "Account")
+ACCOUNT_ID="${ACCOUNT_ID%\"}"
+ACCOUNT_ID="${ACCOUNT_ID#\"}"
 cd src && find . -type f -exec chmod -R ugo+r {} ";"
 cd ..
 SLS_DEPLOYMENT_BUCKET="${EFCMS_DOMAIN}.efcms.${slsStage}.${region}.deploys"
 SLS_DEPLOYMENT_BUCKET="${SLS_DEPLOYMENT_BUCKET}" ./node_modules/.bin/sls create_domain --stage "${slsStage}" --region "${region}" --domain "${EFCMS_DOMAIN}" --verbose
-ENVIRONMENT="${slsStage}" SLS_DEPLOYMENT_BUCKET="${SLS_DEPLOYMENT_BUCKET}" ./node_modules/.bin/sls deploy --stage "${slsStage}" --region "${region}" --domain "${EFCMS_DOMAIN}" --verbose --documentsTableName efcms-documents-"${slsStage}" --casesTableName efcms-cases-"${slsStage}" --accountId "${accountId}"
+ENVIRONMENT="${slsStage}" SLS_DEPLOYMENT_BUCKET="${SLS_DEPLOYMENT_BUCKET}" ./node_modules/.bin/sls deploy --stage "${slsStage}" --region "${region}" --domain "${EFCMS_DOMAIN}" --verbose --documentsTableName "efcms-documents-${slsStage}" --casesTableName "efcms-cases-${slsStage}" --accountId "${ACCOUNT_ID}"
 ./configure-custom-api-access-logging.sh "${slsStage}" ./config-custom-access-logs.json "${region}"
