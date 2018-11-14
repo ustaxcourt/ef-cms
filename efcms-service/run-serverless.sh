2d3eceb6085638786df82986e2dae036510c8077
Cody Seibert <cseibert113@gmail.com>
2018-11-14 13:56:34 -0500

    refactoring to use terraform

--- efcms-service/run-serverless.sh
+++ efcms-service/run-serverless.sh
@@ -6,5 +6,5 @@ cd src && find . -type f -exec chmod -R ugo+r {} ";"
 cd ..
 SLS_DEPLOYMENT_BUCKET="${EFCMS_DOMAIN}.efcms.${slsStage}.${region}.deploys"
 SLS_DEPLOYMENT_BUCKET="${SLS_DEPLOYMENT_BUCKET}" ./node_modules/.bin/sls create_domain --stage "${slsStage}" --region "${region}" --domain "${EFCMS_DOMAIN}" --verbose
-ENVIRONMENT="${slsStage}" SLS_DEPLOYMENT_BUCKET="${SLS_DEPLOYMENT_BUCKET}" ./node_modules/.bin/sls deploy --stage "${slsStage}" --region "${region}" --domain "${EFCMS_DOMAIN}" --verbose
+ENVIRONMENT="${slsStage}" SLS_DEPLOYMENT_BUCKET="${SLS_DEPLOYMENT_BUCKET}" ./node_modules/.bin/sls deploy --stage "${slsStage}" --region "${region}" --domain "${EFCMS_DOMAIN}" --verbose --documentsTableName efcms-documents-$ENVIRONMENT --casesTableName efcms-cases-$ENVIRONMENT
 ./configure-custom-api-access-logging.sh "${slsStage}" ./config-custom-access-logs.json "${region}"
