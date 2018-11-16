9bb80d031832f43524ccc2c005d4c4d1f043cf6b
Cody Seibert <cseibert113@gmail.com>
2018-11-16 14:15:28 -0500

    fixing issue with domains not being created

--- efcms-service/run-serverless.sh
+++ efcms-service/run-serverless.sh
@@ -8,6 +8,6 @@ ACCOUNT_ID="${ACCOUNT_ID#\"}"
 cd src && find . -type f -exec chmod -R ugo+r {} ";"
 cd ..
 SLS_DEPLOYMENT_BUCKET="${EFCMS_DOMAIN}.efcms.${slsStage}.${region}.deploys"
-SLS_DEPLOYMENT_BUCKET="${SLS_DEPLOYMENT_BUCKET}" ./node_modules/.bin/sls create_domain --stage "${slsStage}" --region "${region}" --domain "${EFCMS_DOMAIN}" --verbose
+SLS_DEPLOYMENT_BUCKET="${SLS_DEPLOYMENT_BUCKET}" ./node_modules/.bin/sls create_domain --stage "${slsStage}" --region "${region}" --domain "${EFCMS_DOMAIN}"  --documentsTableName "efcms-documents-${slsStage}" --casesTableName "efcms-cases-${slsStage}" --accountId "${ACCOUNT_ID}" --verbose
 ENVIRONMENT="${slsStage}" SLS_DEPLOYMENT_BUCKET="${SLS_DEPLOYMENT_BUCKET}" ./node_modules/.bin/sls deploy --stage "${slsStage}" --region "${region}" --domain "${EFCMS_DOMAIN}" --verbose --documentsTableName "efcms-documents-${slsStage}" --casesTableName "efcms-cases-${slsStage}" --accountId "${ACCOUNT_ID}"
 ./configure-custom-api-access-logging.sh "${slsStage}" ./config-custom-access-logs.json "${region}"
