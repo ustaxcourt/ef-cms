eb50fb83a422146fd7df69dde147b02baf9de80a
Waldo Jaquith <waldo.jaquith@gsa.gov>
2018-11-19 13:50:29 -0500

    Delete Node modules I'm not sure how these wound up in there.

--- document-service/node_modules/serverless-offline/src/debugLog.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = typeof process.env.SLS_DEBUG !== 'undefined' ?
-  console.log.bind(null, '[offline]') :
-  (() => null);
--- document-service/node_modules/vandium-utils/lib/is_string.js
+++
@@ -1,8 +0,0 @@
-'use string';
-
-function isString( value ) {
-
-    return !!value && ((typeof value === 'string') || (value instanceof String));
-}
-
-module.exports = isString;
