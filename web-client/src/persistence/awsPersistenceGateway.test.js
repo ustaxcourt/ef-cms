e320f278d0ec0369d2fdd3b9a448f3f623842dbc
Eric Sorenson <eric@sorenson.software>
2018-11-19 09:04:24 -0600

    refactored getUser use case

--- web-client/src/persistence/awsPersistenceGateway.test.js
+++ web-client/src/persistence/awsPersistenceGateway.test.js
@@ -68,26 +68,6 @@ const fakeDocumentId = {
 };
 
 describe('AWS petition gateway', () => {
-  describe('Get user', () => {
-    it('Success taxpayer', async () => {
-      const user = awsPersistenceGateway.getUser('taxpayer');
-      assert.equal(user.userId, 'taxpayer');
-      assert.equal(user.role, 'taxpayer');
-    });
-    it('Success petitionsclerk', async () => {
-      const user = awsPersistenceGateway.getUser('petitionsclerk');
-      assert.equal(user.userId, 'petitionsclerk');
-      assert.equal(user.role, 'petitionsclerk');
-    });
-    it('Failure', async () => {
-      try {
-        awsPersistenceGateway.getUser('Bad actor');
-      } catch (e) {
-        assert.equal(e.message, 'Username is incorrect');
-      }
-    });
-  });
-
   describe('Get cases for taxpayer', () => {
     let mock;
 
