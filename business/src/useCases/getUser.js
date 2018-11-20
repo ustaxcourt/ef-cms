34af937e472187fcc8f7afa0020e44e32ded51ea
Eric Sorenson <ericsorenson@Erics-MacBook-Pro.local>
2018-11-20 12:05:17 -0600

    added back missing token

--- business/src/useCases/getUser.js
+++ business/src/useCases/getUser.js
@@ -3,17 +3,19 @@ const User = require('../entities/User');
 module.exports = userId => {
   if (userId === 'taxpayer') {
     return new User({
-      userId: userId,
-      role: 'taxpayer',
       firstName: 'Test',
       lastName: 'Taxpayer',
+      role: 'taxpayer',
+      token: 'taxpayer',
+      userId: userId,
     });
   } else if (userId === 'petitionsclerk') {
     return new User({
-      userId: userId,
-      role: 'petitionsclerk',
       firstName: 'Petitions',
       lastName: 'Clerk',
+      role: 'petitionsclerk',
+      token: 'petitionsclerk',
+      userId: userId,
     });
   }
   return;
