c8b0723d5c7589666d12e7f78941afc85fa0986b
Eric Sorenson <ericsorenson@Erics-MacBook-Pro.local>
2018-11-21 11:43:48 -0600

    fixed file upload spec

--- web-client/src/tests/taxpayer.test.js
+++ web-client/src/tests/taxpayer.test.js
@@ -1,14 +1,11 @@
 import { CerebralTest } from 'cerebral/test';
-import { JSDOM } from 'jsdom';
+import FormData from 'form-data';
 import assert from 'assert';
 
 import mainModule from '../main';
 import applicationContext from '../applicationContexts/dev';
 
-const jsdom = new JSDOM('');
-global.window = jsdom.window;
-global.FormData = jsdom.window.FormData;
-global.File = jsdom.window.File;
+global.FormData = FormData;
 
 mainModule.providers.applicationContext = applicationContext;
 mainModule.providers.router = { route: () => {} };
@@ -23,26 +20,26 @@ test.setState('user', {
   userId: 'taxpayer',
 });
 
+const fakeFile = new Buffer(['TEST'], {
+  type: 'application/pdf',
+});
+fakeFile.name = 'requestForPlaceOfTrial.pdf';
+
 describe('Initiate case', () => {
   it('Submits successfully', async () => {
     await test.runSequence('gotoFilePetition');
     await test.runSequence('updatePetitionValue', {
       key: 'petitionFile',
-      value: new File(['TEST'], 'petitionFile.pdf', {
-        type: 'application/pdf',
-      }),
+      value: fakeFile,
     });
+
     await test.runSequence('updatePetitionValue', {
       key: 'requestForPlaceOfTrial',
-      value: new File(['TEST'], 'requestForPlaceOfTrial.pdf', {
-        type: 'application/pdf',
-      }),
+      value: fakeFile,
     });
     await test.runSequence('updatePetitionValue', {
       key: 'statementOfTaxpayerIdentificationNumber',
-      value: new File(['TEST'], 'statementOfTaxpayerIdentificationNumber.pdf', {
-        type: 'application/pdf',
-      }),
+      value: fakeFile,
     });
     await test.runSequence('submitFilePetition');
     assert.deepEqual(test.getState('alertSuccess'), {
