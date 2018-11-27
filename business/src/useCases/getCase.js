76559602e5e74bd5ed65cd437ff368975309647d
Will Sadler <wsadler@flexion.us>
2018-11-27 09:28:39 -0600

    127 Docket Number Search * refactor persistence methods to not pass in entity and be more explicit * add useCase specs * add docketNumber regex to caseId as a valid identifier * add 403 and 404 return codes to error handling * refactor custom applicationContext out of lambdas

--- business/src/useCases/getCase.js
+++ business/src/useCases/getCase.js
@@ -5,17 +5,31 @@ const {
 const { NotFoundError, UnauthorizedError } = require('../errors/errors');
 
 exports.getCase = async ({ userId, caseId, applicationContext }) => {
-  const caseRecord = await applicationContext.persistence.get({
-    entity: { caseId, entityType: 'case' },
-    applicationContext,
-  });
+  let caseRecord;
+  const isUUID =
+    caseId &&
+    /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i.test(
+      caseId,
+    );
+  if (isUUID) {
+    caseRecord = await applicationContext.persistence.getCaseByCaseId({
+      caseId,
+      applicationContext,
+    });
+  } else if (caseId && /\d{5}-\d{2}/.test(caseId)) {
+    //docketNumber
+    caseRecord = await applicationContext.persistence.getCaseByDocketNumber({
+      docketNumber: caseId,
+      applicationContext,
+    });
+  }
 
-  if (!caseRecord) {
+  if (!caseRecord || (Array.isArray(caseRecord) && !caseRecord.length)) {
     throw new NotFoundError(`Case ${caseId} was not found.`);
   }
 
   if (!isAuthorized(userId, GET_CASE, caseRecord.userId)) {
-    throw new UnauthorizedError('Unauthorized for getCase');
+    throw new UnauthorizedError('Unauthorized');
   }
 
   return caseRecord;
--- efcms-service/src/documents/createPolicyUrl.js
+++ efcms-service/src/documents/createPolicyUrl.js
@@ -1,4 +1,5 @@
 const { handle } = require('../middleware/apiGatewayHelper');
+const applicationContext = require('../applicationContext');
 const {
   createUploadPolicy: uploadPolicyUseCase,
 } = require('ef-cms-shared/src/useCases/createPolicyUrl');
@@ -9,23 +10,6 @@ const {
  * @param context
  * @param callback
  */
-
-const {
-  persistence: { createUploadPolicy },
-  environment: { s3Endpoint, region, documentsBucketName },
-} = require('../applicationContext');
-
-const applicationContext = {
-  persistence: {
-    createUploadPolicy,
-  },
-  environment: {
-    s3Endpoint,
-    region,
-    documentsBucketName,
-  },
-};
-
 /**
  * Create Upload Policy API Lambda
  */
