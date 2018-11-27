b357a06215f73ca595709b2f930e2fbfbc1217f9
Cody Seibert <cseibert113@gmail.com>
2018-11-27 09:23:08 -0500

    testing without cache dir now

--- efcms-service/docker-init.sh
+++ efcms-service/docker-init.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-npm_config_cache="${npm_config_cache}" # prevent shellcheck err
 docker build -t efcms-build -f ../Dockerfile.build ..
-docker run -e "npm_config_cache=${npm_config_cache}" -v "${npm_config_cache}:${npm_config_cache}" --rm efcms-build /bin/sh -c 'echo "init completed"'
+docker run --rm efcms-build /bin/sh -c 'echo "init completed"'
