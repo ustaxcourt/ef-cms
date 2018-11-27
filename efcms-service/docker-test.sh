9c36f07d3006d52c6ff967e69abf7b738a6c4fd6
Cody Seibert <cseibert113@gmail.com>
2018-11-27 08:29:05 -0500

    checking if build fails

--- efcms-service/docker-test.sh
+++ efcms-service/docker-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build -t efcms-build -f ../Dockerfile.build ..
-docker run --name "${CONTAINER_NAME}" efcms-build /bin/sh -c 'cd efcms-service && npm run test'
+docker run --name "${CONTAINER_NAME}" efcms-build /bin/sh -exc 'cd efcms-service && npm run test'
 docker cp "${CONTAINER_NAME}:/home/app/efcms-service/coverage" coverage
 docker rm "${CONTAINER_NAME}"
