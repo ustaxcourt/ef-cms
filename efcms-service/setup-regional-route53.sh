53a0dc2b2eaa37cd16d972428d19c552f845a428
Cody Seibert <cseibert113@gmail.com>
2018-11-27 13:18:04 -0500

    making everything passed in as arguments to docker scripts; fixing issue with undefined domain not being passed in

--- efcms-service/setup-regional-route53.sh
+++ efcms-service/setup-regional-route53.sh
@@ -7,7 +7,6 @@ STACKNAME="ef-cms-${ENVIRONMENT}"
 HOSTEDZONE=$(aws route53 list-hosted-zones --query "HostedZones[?Name=='${DOMAIN}.'].Id" --output text)
 REGION1=$(aws cloudformation describe-stacks --stack-name "${STACKNAME}" --region us-east-1 --query "Stacks[0].Outputs[?OutputKey==\`DomainName\`].OutputValue" --output text)
 REGION2=$(aws cloudformation describe-stacks --stack-name "${STACKNAME}" --region us-west-1 --query "Stacks[0].Outputs[?OutputKey==\`DomainName\`].OutputValue" --output text)
-
 aws route53 change-resource-record-sets \
   --hosted-zone-id "${HOSTEDZONE}" \
   --change-batch  '{
