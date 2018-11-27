df8d60a27625c3689e97b2688514c7540c8af469
Cody Seibert <cseibert113@gmail.com>
2018-11-27 13:31:39 -0500

    removing debug code

--- efcms-service/verify-sonarqube-passed.sh
+++ efcms-service/verify-sonarqube-passed.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 FULL_URL="https://sonarcloud.io"
 branch_name=$branch_name
-OUTPUT="EXECUTION FAILURE" #$(sonar-scanner -Dsonar.projectKey="${SONAR_KEY}" -Dsonar.branch.name="${branch_name}" -Dsonar.organization="${SONAR_ORG}" -Dsonar.projectBaseDir=. -Dsonar.login="${SONAR_TOKEN}" -Dsonar.host.url="${FULL_URL}")
+OUTPUT=$(sonar-scanner -Dsonar.projectKey="${SONAR_KEY}" -Dsonar.branch.name="${branch_name}" -Dsonar.organization="${SONAR_ORG}" -Dsonar.projectBaseDir=. -Dsonar.login="${SONAR_TOKEN}" -Dsonar.host.url="${FULL_URL}")
 sleep 10
 PROJECT_KEY=$(grep sonar.projectKey sonar-project.properties | sed 's/sonar.projectKey=\(.*\)/\1/')
 CURL_URL="${FULL_URL}/api/qualitygates/project_status?projectKey=${PROJECT_KEY}&branch=${branch_name}"
