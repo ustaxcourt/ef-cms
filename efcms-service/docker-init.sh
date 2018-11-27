ead687528f305c72d6309f1fe94f798bfdc8a9c0
Cody Seibert <cseibert113@gmail.com>
2018-11-27 09:02:27 -0500

    using ci

--- efcms-service/docker-init.sh
+++ efcms-service/docker-init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+npm_config_cache="${npm_config_cache}"
 docker build -t efcms-build -f ../Dockerfile.build ..
 docker run -e "npm_config_cache=${npm_config_cache}" -v "${npm_config_cache}:${npm_config_cache}" --rm efcms-build /bin/sh -c 'echo "init completed"'
