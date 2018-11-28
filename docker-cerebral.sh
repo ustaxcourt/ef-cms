f7cdfcac3f2b24a5198787c4f6fc26ca69577cc2
Cody Seibert <cseibert@flexion.us>
2018-11-28 14:38:37 -0500

    more refactoring

--- docker-cerebral.sh
+++ docker-cerebral.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 docker build -t main-build -f Dockerfile.main .
-docker run main-build /bin/sh -c "cd efcms-service && npm run install:dynamodb && (npm start &) && sleep 5 && cd ../web-client && npm run test"
+docker run main-build /bin/sh -c "cd efcms-service && npm run install:dynamodb && (npm start &) && ../wait-until.sh http://localhost:3000/v1/swagger && cd ../web-client && npm run test"
