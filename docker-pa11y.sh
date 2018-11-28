a35d78d2d06287464d5d050c98f9362441de8c16
Cody Seibert <cseibert@flexion.us>
2018-11-28 12:20:43 -0500

    fixing pa11y

---
+++ docker-pa11y.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+docker build -t web-client-build -f Dockerfile.web-client .
+docker run -e AWS_PROFILE=FlexionUSTC -v ~/.aws:/root/.aws --rm web-client-build /bin/sh -c 'cd efcms-service && npm run install:dynamodb && (npm start &) && ../wait-until.sh http://localhost:3000/v1/swagger && cd ../web-client && (npm run dev &) && ../wait-until.sh http://localhost:1234 && npm run test:pa11y'
