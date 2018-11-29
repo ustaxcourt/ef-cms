8b6b827e41595d4f0c60bb71bcb35ae818334a44
Cody Seibert <cseibert@flexion.us>
2018-11-28 21:59:48 -0500

    test

--- docker-cerebral.sh
+++ docker-cerebral.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
-docker build -t main-build -f Dockerfile.main .
-docker run -e AWS_ACCESS_KEY_ID=noop -e AWS_SECRET_ACCESS_KEY=noop main-build /bin/sh -c "cd efcms-service && npm run install:dynamodb && (npm start &) && ../wait-until.sh http://localhost:3000/v1/swagger && cd ../web-client && npm run test"
+docker build -t web-client-build -f Dockerfile.web-client .
+docker run -e AWS_ACCESS_KEY_ID=noop -e AWS_SECRET_ACCESS_KEY=noop web-client-build /bin/sh -c "cd efcms-service && npm run install:dynamodb && (npm start &) && ../wait-until.sh http://localhost:3000/v1/swagger && cd ../web-client && npm run test"
