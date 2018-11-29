a9aa3d7611a0bc219e754916f52d20b868d1e67c
Cody Seibert <cseibert@flexion.us>
2018-11-29 02:18:20 -0500

    fixing docker image

--- web-client/docker-terraform.sh
+++ web-client/docker-terraform.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 ENV=$1
-docker build -t efcms-build -f ../Dockerfile.build ..
-docker run -e "EFCMS_DOMAIN=${EFCMS_DOMAIN}" --rm efcms-build /bin/sh -c "cd web-client/terraform/main && ../bin/deploy-app.sh ${ENV}"
+docker build -t web-client-build -f ../Dockerfile.build ..
+docker run -e "EFCMS_DOMAIN=${EFCMS_DOMAIN}" --rm web-client-build /bin/sh -c "cd web-client/terraform/main && ../bin/deploy-app.sh ${ENV}"
