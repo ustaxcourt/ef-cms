c1b57f2669d68d5ea3c83bc3fa508e1ead0660ff
Will Sadler <wsadler@flexion.us>
2018-11-30 10:57:36 -0600

    127 fix document dates * add userId and createdAt to the Cases documents schema * lint issue in dynamo persistence * return documentMetadata objects not just ids in the uploadcasepdfs use case * pass the documents returned from persistence/use case back to the createCase use case instead of making them anew

--- business/src/persistence/dynamodbClientService.js
+++ business/src/persistence/dynamodbClientService.js
@@ -7,7 +7,7 @@ const region = process.env.AWS_REGION || 'us-east-1';
  * @param params
  */
 
-const removeAWSGlobalFields = (item) => {
+const removeAWSGlobalFields = item => {
   // dynamodb always adds these fields for purposes of global tables
   delete item['aws:rep:deleting'];
   delete item['aws:rep:updateregion'];
