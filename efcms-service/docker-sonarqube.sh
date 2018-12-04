405ec6db693a95979f854cc357aeff1e11c3b2a5
Cody Seibert <cseibert@flexion.us>
2018-12-04 12:45:10 -0500

    fixing dir location

--- efcms-service/docker-sonarqube.sh
+++ efcms-service/docker-sonarqube.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 branch_name="${branch_name}"
 docker build -t api-sonarqube -f ../Dockerfile.web-client ..
-docker run -e "SONAR_KEY=${SONAR_KEY}" -e "branch_name=${branch_name}" -e "SONAR_ORG=${SONAR_ORG}" -e "SONAR_TOKEN=${SONAR_TOKEN}" -v "$(pwd)/coverage:/home/app/coverage" --rm api-sonarqube /bin/sh -c 'cd web-client && ./verify-sonarqube-passed.sh'
+docker run -e "SONAR_KEY=${SONAR_KEY}" -e "branch_name=${branch_name}" -e "SONAR_ORG=${SONAR_ORG}" -e "SONAR_TOKEN=${SONAR_TOKEN}" -v "$(pwd)/coverage:/home/app/coverage" --rm api-sonarqube /bin/sh -c 'cd efcms-service && ./verify-sonarqube-passed.sh'
