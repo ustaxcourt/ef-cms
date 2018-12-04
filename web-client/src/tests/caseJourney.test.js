d04a74616241796f059f13b59e27628226486b10
Eric Sorenson <ericsorenson@Erics-MacBook-Pro.local>
2018-12-04 16:08:50 -0600

    refactoring more computeds

--- web-client/src/tests/caseJourney.test.js
+++ web-client/src/tests/caseJourney.test.js
@@ -1,12 +1,7 @@
-import { CerebralTest, runCompute } from 'cerebral/test';
+import { CerebralTest } from 'cerebral/test';
 import FormData from 'form-data';
 import assert from 'assert';
-import {
-  formattedCaseDetail,
-  formattedCases,
-} from '../main/computeds/formattedCaseDetail';
 
-import { formattedSearchParams } from '../main/computeds/formattedSearchParams';
 import mainModule from '../main';
 import applicationContext from '../applicationContexts/dev';
 
@@ -78,33 +73,6 @@ describe('Tax payer', async () => {
       assert.equal(test.getState('currentPage'), 'CaseDetail');
       assert.ok(test.getState('caseDetail'));
     });
-  });
-});
-
-describe('formatted case details computed', () => {
-  it('formats the docket number', () => {
-    const result = runCompute(formattedCaseDetail, {
-      state: { caseDetail: { docketNumber: '00101-18' } },
-    });
-
-    assert.equal(result.docketNumber, '101-18');
-  });
-
-  it('formats the docket number in a list of cases', () => {
-    const result = runCompute(formattedCases, {
-      state: { cases: [{ docketNumber: '00101-18' }] },
-    });
-    assert.equal(result[0].docketNumber, '101-18');
-  });
-});
-
-describe('formatted search parameters computed', () => {
-  it('formats a docket number search param', () => {
-    const result = runCompute(formattedSearchParams, {
-      state: { searchTerm: '101-18' },
-    });
-
-    assert.equal(result, '00101-18');
   });
 });
 
