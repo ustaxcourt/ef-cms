0886686fc3d8b69258a0f0730136187e364f3fcf
Kris Koskelin <kkoskelin@flexion.us>
2018-12-05 13:08:07 -0600

    Updating with comments

--- web-client/src/main/sequences/loginWithToken.js
+++ web-client/src/main/sequences/loginWithToken.js
@@ -2,4 +2,10 @@ import updateLoginValue from '../sequences/updateLoginValue';
 import submitLogin from '../sequences/submitLogIn';
 import gotoDashboard from '../sequences/gotoDashboard';
 
+/**
+ * Combine several sequences; set login value, and
+ * continue with other sequences used when submitting login form
+ * and navigating to dashboard
+ *
+ */
 export default [...updateLoginValue, ...submitLogin, ...gotoDashboard];
