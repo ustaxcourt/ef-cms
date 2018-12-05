96d909c590895b0a6fffe0a8f17b14b78e171db8
Kris Koskelin <kkoskelin@flexion.us>
2018-12-05 11:33:47 -0600

    Scrolling notifications into view
    Modest refactor, possibly opportunity to DRY-up yet? (Discuss)

--- web-client/src/components/ErrorNotification.jsx
+++ web-client/src/components/ErrorNotification.jsx
@@ -1,13 +1,22 @@
 import { state } from 'cerebral';
+import PropTypes from 'prop-types';
 import React from 'react';
 
 import { connect } from '@cerebral/react';
+class ErrorNotification extends React.Component {
+  componentDidUpdate() {
+    this.focusNotification();
+  }
+  focusNotification() {
+    const notification = this.notificationRef.current;
+    if (notification) {
+      notification.scrollIntoView();
+    }
+  }
+  render() {
+    const alertError = this.props.alertError;
+    this.notificationRef = React.createRef();
 
-export default connect(
-  {
-    alertError: state.alertError,
-  },
-  function ErrorNotification({ alertError }) {
     return (
       <React.Fragment>
         {alertError && (
@@ -15,6 +24,7 @@ export default connect(
             className="usa-alert usa-alert-error"
             aria-live="assertive"
             role="alert"
+            ref={this.notificationRef}
           >
             <div className="usa-alert-body">
               <h3 className="usa-alert-heading">{alertError.title}</h3>
@@ -24,5 +34,16 @@ export default connect(
         )}
       </React.Fragment>
     );
+  }
+}
+
+ErrorNotification.propTypes = {
+  alertError: PropTypes.object,
+};
+
+export default connect(
+  {
+    alertError: state.alertError,
   },
+  ErrorNotification,
 );
