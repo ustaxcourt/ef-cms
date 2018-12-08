3405bd4fb24081dfd9b7680232af532892163760
Cody Seibert <cseibert@flexion.us>
2018-12-08 12:47:25 -0500

    refactoring case initiator response

--- shared/src/business/entities/CaseInitiatorResponse.test.js
+++ shared/src/business/entities/CaseInitiatorResponse.test.js
@@ -2,8 +2,8 @@ const assert = require('assert');
 
 const CaseInitiatorResponse = require('./CaseInitiatorResponse');
 
-describe('Petition entity', () => {
-  it('Creates a valid petition', () => {
+describe('CaseInitiatorResponse', () => {
+  it('Creates a valid CaseInitiatorResponse', () => {
     const caseInitiatorResponse = new CaseInitiatorResponse({
       petitionDocumentId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
       requestForPlaceOfTrialDocumentId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
@@ -12,12 +12,40 @@ describe('Petition entity', () => {
     });
     assert.ok(caseInitiatorResponse.isValid());
   });
-  it('Creates an invalid petition', () => {
-    const caseInitiatorResponse = new CaseInitiatorResponse({
-      requestForPlaceOfTrialDocumentId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
-      statementOfTaxpayerIdentificationNumberDocumentId:
-        'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+
+  describe('invalid', () => {
+    it('missing petitionDocumentId', () => {
+      const caseInitiatorResponse = new CaseInitiatorResponse({
+        requestForPlaceOfTrialDocumentId:
+          'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+        statementOfTaxpayerIdentificationNumberDocumentId:
+          'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+      });
+      assert.ok(!caseInitiatorResponse.isValid());
     });
-    assert.ok(!caseInitiatorResponse.isValid());
+    it('missing requestForPlaceOfTrialDocumentId', () => {
+      const caseInitiatorResponse = new CaseInitiatorResponse({
+        petitionDocumentId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+        statementOfTaxpayerIdentificationNumberDocumentId:
+          'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+      });
+      assert.ok(!caseInitiatorResponse.isValid());
+    });
+    it('missing statementOfTaxpayerIdentificationNumberDocumentId', () => {
+      const caseInitiatorResponse = new CaseInitiatorResponse({
+        petitionDocumentId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+        requestForPlaceOfTrialDocumentId:
+          'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+      });
+      assert.ok(!caseInitiatorResponse.isValid());
+    });
+    it('non guid properties', () => {
+      const caseInitiatorResponse = new CaseInitiatorResponse({
+        petitionDocumentId: 'not a uuid',
+        statementOfTaxpayerIdentificationNumberDocumentId: 'not a uuid',
+        requestForPlaceOfTrialDocumentId: 'not a uuid',
+      });
+      assert.ok(!caseInitiatorResponse.isValid());
+    });
   });
 });
