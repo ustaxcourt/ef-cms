a26169e647aab8c4171643237c9bf4de235838a6
Eric Sorenson <ericsorenson@Erics-MacBook-Pro.local>
2018-12-10 20:59:39 -0600

    added new case detail views

---
+++ web-client/src/views/CaseDetailPublic.jsx
@@ -0,0 +1,224 @@
+import { connect } from '@cerebral/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { sequences, state } from 'cerebral';
+import moment from 'moment';
+import React from 'react';
+
+import SuccessNotification from './SuccessNotification';
+import ErrorNotification from './ErrorNotification';
+import openDocumentBlob from './openDocumentBlob';
+
+export default connect(
+  {
+    caseDetail: state.formattedCaseDetail,
+    currentTab: state.currentTab,
+    showDetails: state.paymentInfo.showDetails,
+    togglePaymentDetails: sequences.togglePaymentDetails,
+    updateCurrentTab: sequences.updateCurrentTab,
+    user: state.user,
+    viewDocument: sequences.viewDocument,
+  },
+  function CaseDetail({
+    caseDetail,
+    currentTab,
+    showDetails,
+    togglePaymentDetails,
+    updateCurrentTab,
+    user,
+    viewDocument,
+  }) {
+    return (
+      <React.Fragment>
+        <div className="usa-grid breadcrumb">
+          <FontAwesomeIcon icon="caret-left" />
+          <a href="/" id="queue-nav">
+            Back to dashboard
+          </a>
+        </div>
+        <section className="usa-section usa-grid">
+          <h1 tabIndex="-1">Docket number: {caseDetail.docketNumber}</h1>
+          <p>
+            {user.firstName} {user.lastName}, Petitioner v. Commissioner of
+            Internal Revenue, Respondent
+          </p>
+          <hr />
+          <SuccessNotification />
+          <ErrorNotification />
+          <nav className="horizontal-tabs">
+            <ul role="tabslist">
+              <li
+                role="presentation"
+                className={currentTab == 'Docket Record' ? 'active' : ''}
+              >
+                <button
+                  role="tab"
+                  className="tab-link"
+                  onClick={() => updateCurrentTab({ value: 'Docket Record' })}
+                >
+                  Docket Record
+                </button>
+              </li>
+              <li className={currentTab == 'Case Information' ? 'active' : ''}>
+                <button
+                  role="tab"
+                  className="tab-link"
+                  onClick={() =>
+                    updateCurrentTab({ value: 'Case Information' })
+                  }
+                >
+                  Case Information
+                </button>
+              </li>
+            </ul>
+          </nav>
+          {currentTab == 'Case Information' && (
+            <div className="tab-content" role="tabpanel">
+              {!caseDetail.payGovId && (
+                <ul className="usa-accordion">
+                  <li>
+                    <button
+                      className="usa-accordion-button"
+                      aria-expanded={showDetails}
+                      aria-controls="paymentInfo"
+                      id="actions-button"
+                      onClick={() => togglePaymentDetails()}
+                    >
+                      <span>
+                        <FontAwesomeIcon
+                          icon="flag"
+                          color="#CD2026"
+                          size="sm"
+                        />{' '}
+                        Pay $60.00 filing fee.
+                      </span>
+                    </button>
+                    {showDetails && (
+                      <div
+                        id="paymentInfo"
+                        className="usa-accordion-content usa-grid-full"
+                        aria-hidden="false"
+                      >
+                        <div className="usa-width-one-half">
+                          <h3>Pay by debit card/credit card.</h3>
+                          <p>Copy your docket number(s) and pay online.</p>
+                          <div id="paygov-link-container">
+                            <a
+                              className="usa-button"
+                              href="https://pay.gov/public/form/start/60485840"
+                              aria-label="pay.gov u.s. tax court filing fees"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              Pay now
+                            </a>
+                          </div>
+                          <p>
+                            <i>
+                              Note: it may take up to X days for your payment to
+                              appear online.
+                            </i>
+                          </p>
+                        </div>
+                        <div className="usa-width-one-half">
+                          <h4>Canʼt afford to pay the fee?</h4>
+                          <p>
+                            You may be eligible for a filing fee waiver. File an
+                            application to request a waiver.
+                          </p>
+                          <p>
+                            <a
+                              href="https://www.ustaxcourt.gov/forms/Application_for_Waiver_of_Filing_Fee.pdf"
+                              aria-label="View download application pdf"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              Download application
+                            </a>
+                          </p>
+                          <h4>Mail in payment</h4>
+                          <p>Make checks/money order payable to:</p>
+                          <address>
+                            Clerk, United States Tax Court
+                            <br />
+                            400 2nd St NW
+                            <br />
+                            Washington, DC 20217
+                            <br />
+                          </address>
+                        </div>
+                      </div>
+                    )}
+                  </li>
+                </ul>
+              )}
+            </div>
+          )}
+          {currentTab == 'Docket Record' && (
+            <div className="tab-content" role="tabpanel">
+              <table className="responsive-table" id="docket-record">
+                <thead>
+                  <tr>
+                    <th>Date filed</th>
+                    <th>Title</th>
+                    <th>Filed by</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {caseDetail.documents.map((item, idx) => (
+                    <tr key={idx}>
+                      <td className="responsive-title">
+                        <span className="responsive-label">Activity date</span>
+                        {moment(item.createdAt).format('l')}
+                      </td>
+                      <td>
+                        <span className="responsive-label">Title</span>
+                        <button
+                          className="pdf-link"
+                          aria-label="View PDF"
+                          onClick={() => {
+                            viewDocument({
+                              documentId: item.documentId,
+                              callback: openDocumentBlob,
+                            });
+                          }}
+                        >
+                          <FontAwesomeIcon icon="file-pdf" />
+                          {item.documentType}
+                        </button>
+                      </td>
+                      <td>
+                        <span className="responsive-label">Filed by</span>
+                        Petitioner
+                      </td>
+                      <td>
+                        <span className="responsive-label">Status</span>
+                        {caseDetail.irsSendDate && (
+                          <span>
+                            R served on {moment(caseDetail.irsDate).format('L')}
+                          </span>
+                        )}
+                        {!caseDetail.irsSendDate && <span>{item.status}</span>}
+                      </td>
+                      <td />
+                    </tr>
+                  ))}
+                  {caseDetail.payGovId && (
+                    <tr>
+                      <td>{moment(caseDetail.payGovDate).format('l')}</td>
+                      <td>Filing fee paid</td>
+                      <td />
+                      <td />
+                      <td />
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </section>
+      </React.Fragment>
+    );
+  },
+);
---
+++ web-client/src/views/CaseDetailRespondent.jsx
@@ -0,0 +1,205 @@
+import { connect } from '@cerebral/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { sequences, state } from 'cerebral';
+import React from 'react';
+
+import SuccessNotification from './SuccessNotification';
+import ErrorNotification from './ErrorNotification';
+import openDocumentBlob from './openDocumentBlob';
+
+export default connect(
+  {
+    caseDetail: state.formattedCaseDetail,
+    currentTab: state.currentTab,
+    submitSendToIRS: sequences.submitToIRS,
+    submitUpdateCase: sequences.submitUpdateCase,
+    updateCaseValue: sequences.updateCaseValue,
+    updateCurrentTab: sequences.updateCurrentTab,
+    updateFormValue: sequences.updateFormValue,
+    viewDocument: sequences.viewDocument,
+  },
+  function CaseDetail({
+    caseDetail,
+    currentTab,
+    submitUpdateCase,
+    submitSendToIRS,
+    updateCaseValue,
+    updateCurrentTab,
+    updateFormValue,
+    viewDocument,
+  }) {
+    return (
+      <React.Fragment>
+        <div className="usa-grid breadcrumb">
+          <FontAwesomeIcon icon="caret-left" />
+          <a href="/" id="queue-nav">
+            Back to dashboard
+          </a>
+        </div>
+        <section className="usa-section usa-grid">
+          <h1 tabIndex="-1">Docket number: {caseDetail.docketNumber}</h1>
+          <p>
+            {caseDetail.userId} v. Commissioner of Internal Revenue, Respondent
+          </p>
+          <p>
+            <span className="usa-label">{caseDetail.status}</span>
+          </p>
+          <hr />
+          <SuccessNotification />
+          <ErrorNotification />
+          <nav className="horizontal-tabs">
+            <ul role="tabslist">
+              <li
+                role="presentation"
+                className={currentTab == 'Docket Record' ? 'active' : ''}
+              >
+                <button
+                  role="tab"
+                  className="tab-link"
+                  onClick={() => updateCurrentTab({ value: 'Docket Record' })}
+                  id="docket-record-tab"
+                >
+                  Docket Record
+                </button>
+              </li>
+              <li className={currentTab == 'Case Information' ? 'active' : ''}>
+                <button
+                  role="tab"
+                  className="tab-link"
+                  id="case-info-tab"
+                  onClick={() =>
+                    updateCurrentTab({ value: 'Case Information' })
+                  }
+                >
+                  Case Information
+                </button>
+              </li>
+            </ul>
+          </nav>
+          {currentTab == 'Docket Record' && (
+            <div className="tab-content" role="tabpanel">
+              <button>
+                <FontAwesomeIcon icon="cloud-upload-alt" /> File Document
+              </button>
+              <button id="send-to-irs" onClick={() => submitSendToIRS()}>
+                Send to IRS
+              </button>
+              <table className="responsive-table">
+                <thead>
+                  <tr>
+                    <th>Date filed</th>
+                    <th>Title</th>
+                    <th>Filed by</th>
+                    <th>Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {caseDetail.documents.map((document, idx) => (
+                    <tr key={idx}>
+                      <td className="responsive-title">
+                        <span className="responsive-label">Activity date</span>
+                        {document.createdAtFormatted}
+                      </td>
+                      <td>
+                        <span className="responsive-label">Title</span>
+                        <button
+                          className="pdf-link"
+                          aria-label="View PDF"
+                          onClick={() =>
+                            viewDocument({
+                              documentId: document.documentId,
+                              callback: openDocumentBlob,
+                            })
+                          }
+                        >
+                          <FontAwesomeIcon icon="file-pdf" />
+                          {document.documentType}
+                        </button>
+                      </td>
+                      <td>
+                        <span className="responsive-label">Filed by</span>
+                        Petitioner
+                      </td>
+                      <td>
+                        <span className="responsive-label">Status</span>
+                        {caseDetail.showIrsServedDate && (
+                          <span>{caseDetail.datePetitionSentToIrsMessage}</span>
+                        )}
+                        {caseDetail.showDocumentStatus && (
+                          <span>{document.status}</span>
+                        )}
+                      </td>
+                    </tr>
+                  ))}
+                  {caseDetail.showPaymentRecord && (
+                    <tr>
+                      <td>{caseDetail.payGovDateFormatted}</td>
+                      <td>Filing fee paid</td>
+                      <td />
+                      <td />
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
+          )}
+          {currentTab == 'Case Information' && (
+            <div className="tab-content" role="tabpanel">
+              <fieldset className="usa-fieldset-inputs usa-sans">
+                <legend>Petition fee</legend>
+                {caseDetail.showPaymentRecord && (
+                  <React.Fragment>
+                    <p className="label">Paid by pay.gov</p>
+                    <p>{caseDetail.payGovId}</p>
+                  </React.Fragment>
+                )}
+                {caseDetail.showPaymentOptions && (
+                  <ul className="usa-unstyled-list">
+                    <li>
+                      <input
+                        id="paygov"
+                        type="radio"
+                        name="paymentType"
+                        value="payGov"
+                        onChange={e => {
+                          updateFormValue({
+                            key: e.target.name,
+                            value: e.target.value,
+                          });
+                        }}
+                      />
+                      <label htmlFor="paygov">Paid by pay.gov</label>
+                      {caseDetail.showPayGovIdInput && (
+                        <React.Fragment>
+                          <label htmlFor="paygovid">Payment ID</label>
+                          <input
+                            id="paygovid"
+                            type="text"
+                            name="payGovId"
+                            value={caseDetail.payGovId || ''}
+                            onChange={e => {
+                              updateCaseValue({
+                                key: e.target.name,
+                                value: e.target.value,
+                              });
+                            }}
+                          />
+                          <button
+                            id="update-case-page-end"
+                            onClick={() => submitUpdateCase()}
+                          >
+                            Save updates
+                          </button>
+                        </React.Fragment>
+                      )}
+                    </li>
+                  </ul>
+                )}
+              </fieldset>
+            </div>
+          )}
+        </section>
+      </React.Fragment>
+    );
+  },
+);
