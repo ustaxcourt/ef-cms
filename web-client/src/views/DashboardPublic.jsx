c551bb1f15aa12863cea668cfd49fe298b4379a9
Kris Koskelin <kkoskelin@flexion.us>
2018-12-14 14:32:01 -0600

    User and dashboard initial setup
    docketclerk and senior attorney

--- web-client/src/views/DashboardPublic.jsx
+++ web-client/src/views/DashboardPublic.jsx
@@ -6,7 +6,7 @@ import ErrorNotification from './ErrorNotification';
 
 export default connect(
   {},
-  function Dashboard() {
+  function DashboardPublic() {
     return (
       <section className="usa-section usa-grid">
         <h1 tabIndex="-1">Public Dashboard</h1>
