a3e54c06f07550e8cd144b4cedce08138ede9d83
Cody Seibert <cseibert@flexion.us>
2018-12-18 08:42:57 -0500

    adding sequences that can be used to update and get cases; adding more to case journey test

--- shared/src/proxies/workitems/getWorkItemProxy.js
+++ shared/src/proxies/workitems/getWorkItemProxy.js
@@ -8,7 +8,7 @@ const axios = require('axios');
  * @param userToken
  * @returns {Promise<*>}
  */
-exports.getCase = async ({ applicationContext, workItemId, userId }) => {
+exports.getWorkItem = async ({ applicationContext, workItemId, userId }) => {
   const userToken = userId; //TODO refactor for jwt
   const response = await axios.get(
     `${applicationContext.getBaseUrl()}/workitems/${workItemId}`,
