b9e897d1acf9850acd069e77c1f413a8af1a6451
Eric Sorenson <eric@sorenson.software>
2018-12-20 10:36:40 -0600

    test refactoring

---
+++ web-client/src/tests/journey/petitionsClerkRecordsPayGovId.js
@@ -0,0 +1,21 @@
+import caseDetailHelper from '../../presenter/computeds/caseDetailHelper';
+
+export default (test, runCompute) => {
+  return it('Petitions clerk records pay.gov ID', async () => {
+    await test.runSequence('updateCaseValueSequence', {
+      key: 'payGovId',
+      value: '123',
+    });
+    await test.runSequence('submitUpdateCaseSequence');
+    test.setState('caseDetail', {});
+    await test.runSequence('gotoCaseDetailSequence', {
+      docketNumber: test.docketNumber,
+    });
+    expect(test.getState('caseDetail.payGovId')).toEqual('123');
+
+    const helper = runCompute(caseDetailHelper, {
+      state: test.getState(),
+    });
+    expect(helper.showPaymentRecord).toEqual(true);
+  });
+};
