feec4eb4937ef777115d79a1463d0e8d8798a78b
Cody Seibert <cseibert@flexion.us>
2018-12-27 13:26:13 -0500

    fixing a bug related to self assigning; adding a method to export the current state of dynamodb local to the efcms.json file; starting with clean seed data

--- web-client/src/presenter/computeds/formattedWorkQueue.js
+++ web-client/src/presenter/computeds/formattedWorkQueue.js
@@ -10,8 +10,10 @@ export const formatWorkItem = workItem => {
     message =>
       (message.createdAtFormatted = moment(message.createdAt).format('L')),
   );
+  result.assigneeName = result.assigneeName || 'Unassigned';
   result.caseStatus =
     result.caseStatus === 'general' ? 'general docket' : result.caseStatus;
+
   return result;
 };
 
