53f2418384da0e58f9bedf11a05a758f6eb61076
Cody Seibert <cseibert@flexion.us>
2018-12-26 22:38:59 -0500

    fixing minor bug when assigning a work item to self

--- web-client/src/presenter/actions/assignSelectedWorkItemsAction.js
+++ web-client/src/presenter/actions/assignSelectedWorkItemsAction.js
@@ -3,6 +3,7 @@ import { state } from 'cerebral';
 export default async ({ applicationContext, get, store }) => {
   const selectedWorkItems = get(state.selectedWorkItems);
   const sectionWorkQueue = get(state.sectionWorkQueue);
+  const workQueue = get(state.workQueue);
   const assigneeId = get(state.assigneeId);
   const assigneeName = get(state.assigneeName);
   await applicationContext.getUseCases().assignWorkItems({
@@ -14,12 +15,28 @@ export default async ({ applicationContext, get, store }) => {
     })),
     userId: get(state.user.token),
   });
+
   store.set(
     state.sectionWorkQueue,
-    sectionWorkQueue.map(workItem => ({
-      ...workItem,
-      assigneeId,
-      assigneeName,
-    })),
+    sectionWorkQueue.map(workItem => {
+      if (
+        selectedWorkItems.find(item => item.workItemId === workItem.workItemId)
+      ) {
+        return {
+          ...workItem,
+          assigneeId,
+          assigneeName,
+        };
+      } else {
+        return workItem;
+      }
+    }),
   );
+
+  selectedWorkItems.forEach(item => {
+    if (!workQueue.find(existing => existing.workItemId === item.workItemId)) {
+      workQueue.push(item);
+    }
+  });
+  store.set(state.workQueue, workQueue);
 };
