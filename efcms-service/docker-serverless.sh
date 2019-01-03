12a6396283d3d28ba864ecce94bd6aff079824e2
Cody Seibert <cseibert@flexion.us>
2019-01-03 13:20:19 -0500

    reduce deploy size

--- efcms-service/docker-serverless.sh
+++ efcms-service/docker-serverless.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 STAGE=$1
 REGION=$2
-docker build -t efcms-build -f ../Dockerfile.build ..
+docker build -t efcms-build -f ../Dockerfile.deploy ..
 docker run -e "EFCMS_DOMAIN=${EFCMS_DOMAIN}" --rm efcms-build /bin/sh -c "cd efcms-service && ./run-serverless.sh ${STAGE} ${REGION}"
