df343cc3343074758710fe6ad351d9c0f26df4c1
Kris Koskelin <kkoskelin@flexion.us>
2019-01-04 13:51:12 -0600

    Refactoring Work Queues to separate components

---
+++ web-client/src/views/IndividualWorkQueue.jsx
@@ -0,0 +1,88 @@
+import { connect } from '@cerebral/react';
+import React from 'react';
+import { state, sequences } from 'cerebral';
+
+export default connect(
+  {
+    setFocusedWorkItem: sequences.setFocusedWorkItem,
+    workQueue: state.formattedWorkQueue,
+  },
+  function IndividualWorkQueue({ setFocusedWorkItem, workQueue }) {
+    return (
+      <table
+        className="work-queue"
+        id="my-work-queue"
+        aria-describedby="tab-my-queue"
+      >
+        <thead>
+          <tr>
+            <th colSpan="2" aria-label="Docket Number">
+              Docket
+            </th>
+            <th>Received</th>
+            <th>Document</th>
+            <th>Status</th>
+            <th>From</th>
+            <th>To</th>
+          </tr>
+        </thead>
+        {workQueue.map(item => (
+          <tbody key={item.workItemId}>
+            <tr
+              onClick={() =>
+                setFocusedWorkItem({ workItemId: item.workItemId })
+              }
+            >
+              <td className="focus-toggle">
+                <button
+                  className="focus-button"
+                  aria-label="Expand message detail"
+                  aria-expanded={item.isFocused}
+                />
+              </td>
+              <td>{item.docketNumber}</td>
+              <td>{item.messages[0].createdAtFormatted}</td>
+              <td>
+                <a
+                  href={`/case-detail/${item.docketNumber}/documents/${
+                    item.document.documentId
+                  }`}
+                  className="case-link"
+                >
+                  {item.document.documentType}
+                </a>
+              </td>
+              <td>{item.caseStatus}</td>
+              <td>{item.messages[0].sentBy}</td>
+              <td>{item.assigneeName}</td>
+            </tr>
+            {item.isFocused && (
+              <tr
+                className="queue-focus queue-message"
+                onClick={() =>
+                  setFocusedWorkItem({ workItemId: item.workItemId })
+                }
+              >
+                <td className="focus-toggle">
+                  <button
+                    className="focus-button"
+                    tabIndex="-1"
+                    aria-disabled="true"
+                  />
+                </td>
+                <td colSpan="2" aria-hidden="true" />
+                <td
+                  colSpan="4"
+                  className="message-detail"
+                  aria-label="Message detail"
+                >
+                  {item.messages[0].message}
+                </td>
+              </tr>
+            )}
+          </tbody>
+        ))}
+      </table>
+    );
+  },
+);
