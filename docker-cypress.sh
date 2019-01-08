9f08e8d5c8120f63e83ac7583e0615d0727b2b85
Cody Seibert <cseibert@flexion.us>
2019-01-08 16:42:24 -0500

    trying to archive the cypress videos

--- docker-cypress.sh
+++ docker-cypress.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -e
 docker build -t cypress -f Dockerfile.web-client .
-docker run -e SLS_DEBUG=* -e AWS_ACCESS_KEY_ID=noop -e AWS_SECRET_ACCESS_KEY=noop --rm cypress /bin/sh -c 'cd efcms-service && npm run install:dynamodb && (npm start &) && ../wait-until.sh http://localhost:3000/v1/swagger && cd ../web-client && (npm run dev &) && ../wait-until.sh http://localhost:1234 && npm run cypress'
+set +e
+docker run --name "${CONTAINER_NAME}" -e SLS_DEBUG=* -e AWS_ACCESS_KEY_ID=noop -e AWS_SECRET_ACCESS_KEY=noop cypress /bin/sh -c 'cd efcms-service && npm run install:dynamodb && (npm start &) && ../wait-until.sh http://localhost:3000/v1/swagger && cd ../web-client && (npm run dev &) && ../wait-until.sh http://localhost:1234 && npm run cypress'
+CODE="$?"
+set -e
+mkdir -p cypress
+docker cp "${CONTAINER_NAME}:/home/app/web-client/cypress/videos" cypress
+docker cp "${CONTAINER_NAME}:/home/app/web-client/cypress/screenshots" cypress
+docker rm "${CONTAINER_NAME}"
+exit "${CODE}"
