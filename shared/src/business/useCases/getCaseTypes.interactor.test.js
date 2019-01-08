e492dede37215302001e7f9174cfd876ad0845ee
Will Sadler <wsadler@flexion.us>
2019-01-08 16:59:35 -0600

    fix specs

--- shared/src/business/useCases/getCaseTypes.interactor.test.js
+++ shared/src/business/useCases/getCaseTypes.interactor.test.js
@@ -8,6 +8,17 @@ describe('Get case types', () => {
       userId: 'taxpayer',
     });
     expect(caseTypes.length).toEqual(12);
+    expect(caseTypes[0].type).not.toBeUndefined();
+    expect(caseTypes[0].description).not.toBeUndefined();
+    expect(typeof caseTypes[0].type).toEqual('string');
+    expect(typeof caseTypes[0].description).toEqual('string');
+    let error;
+    try {
+      JSON.parse(JSON.stringify(caseTypes[0]));
+    } catch (e) {
+      error = e;
+    }
+    expect(error).toBeUndefined();
   });
 
   it('throws a UnauthorizedError if user is unauthorized', async () => {
