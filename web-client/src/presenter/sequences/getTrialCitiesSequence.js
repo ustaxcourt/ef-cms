df75f09677bdb64ee0cc3a6d5486d931ad1b5c1d
Will Sadler <wsadler@flexion.us>
2019-01-09 17:06:42 -0600

    402 Trial Cities, Procedures Tests * change the interactor to get user from appctx * when changing the procedureType set the value on the petition and clear the preferredTrialCity * modify the view to use updatePetitionValueSequence

--- web-client/src/presenter/sequences/getTrialCitiesSequence.js
+++ web-client/src/presenter/sequences/getTrialCitiesSequence.js
@@ -1,8 +1,12 @@
+import { props, state } from 'cerebral';
+import { set } from 'cerebral/factories';
 import getTrialCities from '../actions/getTrialCitiesAction';
 import setAlertError from '../actions/setAlertErrorAction';
 import setTrialCities from '../actions/setTrialCitiesAction';
 
 export default [
+  set(state.petition.procedureType, props.value),
+  set(state.petition.preferredTrialCity, ''),
   getTrialCities,
   {
     error: [setAlertError],
