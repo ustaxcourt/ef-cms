d4b8d7bf2bf181b6e3b193bc837eb5d2706ad484
Eric Sorenson <eric@sorenson.software>
2019-01-11 13:48:53 -0600

    hint text compute logic

--- web-client/src/presenter/computeds/startCaseHelper.js
+++ web-client/src/presenter/computeds/startCaseHelper.js
@@ -9,6 +9,8 @@ export default get => {
     uploadsFinished: 0, // TODO: derive from state
     uploadPercentage: 0, // TODO: derive from state
     trialCities: form.trialCities || [],
+    showRegularTrialCitiesHint: form.procedureType === 'large',
+    showSmallTrialCitiesHint: form.procedureType === 'small',
     irsNoticeDate:
       form.year && form.month && form.day
         ? `${form.year}-${form.month}-${form.day}`
