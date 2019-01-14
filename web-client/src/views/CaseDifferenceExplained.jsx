ee0c0b9b3893adbc8ef64db6d5ae81716058e71d
Kris Koskelin <kkoskelin@flexion.us>
2019-01-14 14:22:57 -0600

    402: case difference explained
    Markup, sequence for toggling visibility, lots of styling

--- web-client/src/views/CaseDifferenceExplained.jsx
+++ web-client/src/views/CaseDifferenceExplained.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { connect } from '@cerebral/react';
 
@@ -7,13 +8,67 @@ export default connect(
   function CaseDifferenceExplained() {
     return (
       <div className="subsection case-difference">
-        <h3>Do I qualify for a small tax case procedure?</h3>
+        <h3 id="small-case-qualifications">
+          Do I qualify for a small tax case procedure?
+        </h3>
         <p>
           Small tax cases must adhere to the following dollar limits depending
           on the notice you received:
         </p>
+        <table
+          aria-labelledby="small-case-qualifications"
+          className="small-case-qualifications-table"
+        >
+          <thead>
+            <tr>
+              <th>Deficiency</th>
+              <th>Collection</th>
+              <th>Spousal Relief</th>
+              <th>Worker Classification</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>&lt; $50,000 for each year</td>
+              <td>&lt; $50,000 for all years</td>
+              <td>&lt; $50,000 for all years</td>
+              <td>&lt; $50,000 for any calendar quarter</td>
+            </tr>
+          </tbody>
+        </table>
+
         <h3>If you file as a small tax case, you'll have:</h3>
-        <p>boxes go here</p>
+        <div role="list" className="small-case-features usa-grid-full">
+          <div role="listitem" className="usa-width-one-fourth feature">
+            <h4>More Location Options</h4>
+            <p>
+              Small case trials are held in 15 more locations than regular
+              cases.
+            </p>
+          </div>
+          <div role="listitem" className="usa-width-one-fourth feature">
+            <h4>Less Formal Procedures</h4>
+            <p>
+              Small case pre-trial and trial procedures are less formal than
+              regular cases
+            </p>
+          </div>
+          <div role="listitem" className="usa-width-one-fourth feature">
+            <h4>Relaxed Evidence Rules</h4>
+            <p>Judges can consider any evidence that's relevant.</p>
+          </div>
+          <div role="listitem" className="usa-width-one-fourth feature warning">
+            <h4>
+              {' '}
+              <FontAwesomeIcon icon="exclamation-triangle" />
+              No Appeals Process
+            </h4>
+            <p>
+              If you lose your case, or lose some issues in your case, you can't
+              appeal the decision.
+            </p>
+          </div>
+        </div>
       </div>
     );
   },
