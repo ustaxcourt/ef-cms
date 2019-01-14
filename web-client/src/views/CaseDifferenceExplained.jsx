8b136774fb7cd7d0460222e4bc8bd77044cdffe5
Kris Koskelin <kkoskelin@flexion.us>
2019-01-14 08:56:27 -0600

    402: case difference explained (WIP)

---
+++ web-client/src/views/CaseDifferenceExplained.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { connect } from '@cerebral/react';
+
+export default connect(
+  {},
+  function CaseDifferenceExplained() {
+    return (
+      <div className="subsection case-difference">
+        <h3>Do I qualify for a small tax case procedure?</h3>
+        <p>
+          Small tax cases must adhere to the following dollar limits depending
+          on the notice you received:
+        </p>
+        <h3>If you file as a small tax case, you'll have:</h3>
+        <p>boxes go here</p>
+      </div>
+    );
+  },
+);
