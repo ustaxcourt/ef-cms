ad6bd05974df72f34f7ce9d77c4b688cd9070bca
Will Sadler <wsadler@flexion.us>
2019-01-15 17:22:31 -0600

    regex

--- web-client/src/presenter/actions/setCaseIdFromSearchAction.js
+++ web-client/src/presenter/actions/setCaseIdFromSearchAction.js
@@ -1,10 +1,10 @@
 import { state } from 'cerebral';
-const docketNumberMatcher = /^(\d{3,5}-\d{2}).*/;
+const docketNumberMatcher = /^(\d{3,5}-\d{2})[XWSL]L?(.*)$/;
 
 export default ({ store, get }) => {
   const searchTerm = get(state.searchTerm);
-  const match = docketNumberMatcher.exec(searchTerm);
-  const docketNumber = match && match.length > 1 ? match[1] : searchTerm;
+  const match = docketNumberMatcher.exec(searchTerm.trim());
+  const docketNumber = match && match.length > 1 && match[2] === '' ? match[1] : searchTerm;
   store.set(state.caseId, docketNumber);
   return {
     caseId: docketNumber,
