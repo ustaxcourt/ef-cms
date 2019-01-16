0844166365b9b05d995b02aefa1b5770b356ba56
Will Sadler <wsadler@flexion.us>
2019-01-16 07:49:52 -0600

    lint

--- web-client/src/presenter/actions/setCaseIdFromSearchAction.js
+++ web-client/src/presenter/actions/setCaseIdFromSearchAction.js
@@ -4,7 +4,8 @@ const docketNumberMatcher = /^(\d{3,5}-\d{2})[XWPRSL]L?(.*)$/;
 export default ({ store, get }) => {
   const searchTerm = get(state.searchTerm);
   const match = docketNumberMatcher.exec(searchTerm.trim());
-  const docketNumber = match && match.length > 1 && match[2] === '' ? match[1] : searchTerm;
+  const docketNumber =
+    match && match.length > 1 && match[2] === '' ? match[1] : searchTerm;
   store.set(state.caseId, docketNumber);
   return {
     caseId: docketNumber,
