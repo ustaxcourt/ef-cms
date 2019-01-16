2984d0a33649a88d3c21c299647089a4629daba3
Cody Seibert <cseibert@flexion.us>
2019-01-16 13:52:16 -0500

    adding some case journey tests

--- web-client/src/tests/journey/taxpayerViewsCaseDetail.js
+++ web-client/src/tests/journey/taxpayerViewsCaseDetail.js
@@ -1,14 +1,26 @@
+import { runCompute } from 'cerebral/test';
+
+import { formattedCaseDetail } from '../../presenter/computeds/formattedCaseDetail';
+
 export default test => {
   return it('Taxpayer views case detail', async () => {
     await test.runSequence('gotoCaseDetailSequence', {
       docketNumber: test.docketNumber,
     });
+
     const caseDetail = test.getState('caseDetail');
+    const caseDetailFormatted = runCompute(formattedCaseDetail, {
+      state: test.getState(),
+    });
     expect(test.getState('currentPage')).toEqual('CaseDetailPetitioner');
     expect(caseDetail.docketNumber).toEqual(test.docketNumber);
+    expect(caseDetail.docketNumberSuffix).toEqual('W');
+    expect(caseDetailFormatted.docketNumberWithSuffix).toEqual(
+      `${test.docketNumber}W`,
+    );
     expect(caseDetail.documents.length).toEqual(1);
     expect(caseDetail.preferredTrialCity).toEqual('Chattanooga, TN');
-    expect(caseDetail.caseType).toEqual('noticeOfDeficiency');
+    expect(caseDetail.caseType).toEqual('Whistleblower');
     expect(caseDetail.procedureType).toEqual('Regular');
     await test.runSequence('viewDocumentSequence', {
       documentId: test.getState('caseDetail.documents.0.documentId'),
