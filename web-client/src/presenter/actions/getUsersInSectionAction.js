e68d7369d546030a7bc44a62a1839158bf81fc0a
Cody Seibert <cseibert@flexion.us>
2019-01-17 00:34:05 -0500

    just making some small changes to allow the petitions section items to show up

--- web-client/src/presenter/actions/getUsersInSectionAction.js
+++ web-client/src/presenter/actions/getUsersInSectionAction.js
@@ -2,7 +2,7 @@ export default section => async ({ applicationContext, path }) => {
   const users = await applicationContext
     .getUseCases()
     .getUsersInSection({ section, applicationContext });
-  return path.success({
+  return {
     users,
-  });
+  };
 };
