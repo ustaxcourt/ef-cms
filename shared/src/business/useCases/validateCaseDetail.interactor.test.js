f3008e961e84887b8edb48122a7e22be5ea1fb69
Cody Seibert <cseibert@flexion.us>
2019-01-21 09:15:04 -0500

    more tests

--- shared/src/business/useCases/validateCaseDetail.interactor.test.js
+++ shared/src/business/useCases/validateCaseDetail.interactor.test.js
@@ -1,70 +1,31 @@
 const { validateCaseDetail } = require('./validateCaseDetail.interactor');
-const Case = require('../entities/Case');
-const { omit } = require('lodash');
-const moment = require('moment');
 
 describe('validate case detail', () => {
-  it('returns the expected errors object on an empty case', () => {
+  it('returns an object containing all errors', () => {
     const errors = validateCaseDetail({
-      caseDetail: {},
+      caseDetail: {
+        caseTitle: 'abc',
+        docketNumber: '101-18',
+        documents: [{}],
+        petitioners: [
+          {
+            name: 'john doe',
+          },
+        ],
+      },
     });
-    expect(errors).toBeTruthy();
+    expect(errors).toBeFalsy();
   });
 
-  // it('returns the expected errors object when caseType is defined', () => {
-  //   const errors = validatePetition({
-  //     petition: {
-  //       caseType: 'defined',
-  //     },
-  //     applicationContext: {
-  //       getEntityConstructors: () => ({
-  //         Petition,
-  //       }),
-  //     },
-  //   });
-  //   expect(errors).toEqual(omit(Petition.errorToMessageMap, ['caseType']));
-  // });
-  //
-  // it('returns the expected errors object', () => {
-  //   const errors = validatePetition({
-  //     petition: {
-  //       caseType: 'defined',
-  //       procedureType: 'defined',
-  //       petitionFile: new File([], 'test.png'),
-  //       preferredTrialCity: 'defined',
-  //       irsNoticeDate: new Date().toISOString(),
-  //       signature: true,
-  //     },
-  //     applicationContext: {
-  //       getEntityConstructors: () => ({
-  //         Petition,
-  //       }),
-  //     },
-  //   });
-  //   expect(errors).toEqual(null);
-  // });
-  //
-  // it('returns an error for a irs notice date in the future', () => {
-  //   const futureDate = moment().add(1, 'days');
-  //
-  //   const errors = validatePetition({
-  //     petition: {
-  //       caseType: 'defined',
-  //       procedureType: 'defined',
-  //       petitionFile: new File([], 'test.png'),
-  //       preferredTrialCity: 'defined',
-  //       irsNoticeDate: futureDate.toDate().toISOString(),
-  //       signature: true,
-  //     },
-  //     applicationContext: {
-  //       getEntityConstructors: () => ({
-  //         Petition,
-  //       }),
-  //     },
-  //   });
-  //
-  //   expect(errors).toEqual({
-  //     irsNoticeDate: 'IRS Notice Date is a required field.',
-  //   });
-  // });
+  it('returns the expected errors when passed bad date objects', () => {
+    const errors = validateCaseDetail({
+      caseDetail: {
+        irsNoticeDate: 'aa',
+        payGovDate: '12',
+      },
+    });
+    expect(errors).toBeTruthy();
+    expect(errors.irsNoticeDate).toBeTruthy();
+    expect(errors.payGovDate).toBeTruthy();
+  });
 });
