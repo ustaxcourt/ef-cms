e55119c1d36d211c0f06751e07fda123961eb2ac
Cody Seibert <cseibert@flexion.us>
2019-01-21 09:15:04 -0500

    more tests

--- shared/src/business/entities/Petition.js
+++ shared/src/business/entities/Petition.js
@@ -34,15 +34,4 @@ joiValidationDecorator(
   Petition.errorToMessageMap,
 );
 
-// const original = Petition.prototype.getValidationErrors;
-//
-// Petition.prototype.getValidationErrors = function() {
-//   const errors = original.call(this);
-//   if (!errors) return null;
-//   for (let key of Object.keys(errors)) {
-//     errors[key] = Petition.errorToMessageMap[key];
-//   }
-//   return errors;
-// };
-
 module.exports = Petition;
