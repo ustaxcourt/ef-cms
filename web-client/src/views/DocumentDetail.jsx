297bafb849bc8532eba312dc529fa32611033731
Eric Sorenson <eric@sorenson.software>
2019-01-22 14:54:34 -0600

    plugging in state and sequences

--- web-client/src/views/DocumentDetail.jsx
+++ web-client/src/views/DocumentDetail.jsx
@@ -65,7 +65,6 @@ class DocumentDetail extends React.Component {
             <ul role="tablist">
               <li className={helper.showDocumentInfo ? 'active' : ''}>
                 <button
-                  disabled
                   role="tab"
                   className="tab-link"
                   id="tab-document-info"
