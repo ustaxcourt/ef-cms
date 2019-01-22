a2782659d47a3163145303e642fc190360b9cf45
Will Sadler <wsadler@flexion.us>
2019-01-22 15:38:42 -0600

    514 Error Alert * set AlertError in state on submit update case * add new action for validation errors * integrate it into sequence

--- web-client/src/presenter/sequences/submitUpdateCaseSequence.js
+++ web-client/src/presenter/sequences/submitUpdateCaseSequence.js
@@ -1,10 +1,9 @@
-import { props, state } from 'cerebral';
-import { set } from 'cerebral/factories';
 import clearAlerts from '../actions/clearAlertsAction';
 import clearForm from '../actions/clearFormAction';
 import setAlertError from '../actions/setAlertErrorAction';
 import setAlertSuccess from '../actions/setAlertSuccessAction';
 import setCase from '../actions/setCaseAction';
+import setValidationAlertErrorsAction from '../actions/setValidationAlertErrorsAction';
 import updateCase from '../actions/updateCaseAction';
 import validateCaseDetail from '../actions/validateCaseDetailAction';
 import getFormCombinedWithCaseDetail from '../actions/getFormCombinedWithCaseDetailAction';
@@ -21,6 +20,6 @@ export default [
         success: [setCase, setAlertSuccess, clearForm],
       },
     ],
-    error: [set(state.caseDetailErrors, props.errors)],
+    error: [setValidationAlertErrorsAction],
   },
 ];
