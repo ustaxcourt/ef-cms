81adf1b5ae973a1c07fab902fea996d78bddf56e
Cody Seibert <cseibert@flexion.us>
2019-01-25 17:04:18 -0500

    fixing sonarqube to fail build on failures

--- web-client/verify-sonarqube-passed.sh
+++ web-client/verify-sonarqube-passed.sh
@@ -3,9 +3,8 @@ FULL_URL="https://sonarcloud.io"
 branch_name=$branch_name
 OUTPUT=$(sonar-scanner -Dsonar.projectKey="${SONAR_KEY}" -Dsonar.branch.name="${branch_name}" -Dsonar.organization="${SONAR_ORG}" -Dsonar.projectBaseDir=. -Dsonar.login="${SONAR_TOKEN}" -Dsonar.host.url="${FULL_URL}")
 sleep 10
-PROJECT_KEY=$(grep sonar.projectKey sonar-project.properties | sed 's/sonar.projectKey=\(.*\)/\1/')
-CURL_URL="${FULL_URL}/api/qualitygates/project_status?projectKey=${PROJECT_KEY}&branch=${branch_name}"
-JSON=$(curl -u "admin:${SONAR_PASSWORD}" -X GET -H 'Accept: application/json' "${CURL_URL}")
+CURL_URL="${FULL_URL}/api/qualitygates/project_status?projectKey=${SONAR_KEY}&branch=${branch_name}"
+JSON=$(curl -X GET -H 'Accept: application/json' "${CURL_URL}")
 STATUS=$(echo "${JSON}" | jq -r ".projectStatus.status")
 set +e
 CONTAINS_FAILURE=$(echo "${OUTPUT}" | grep 'EXECUTION FAILURE')
