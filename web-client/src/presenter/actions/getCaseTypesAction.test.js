3b3acc2b7c284caac996a6d6950e5b3832483c94
Cody Seibert <cseibert@flexion.us>
2019-01-25 17:35:16 -0500

    adding a test

--- web-client/src/presenter/actions/getCaseTypesAction.test.js
+++ web-client/src/presenter/actions/getCaseTypesAction.test.js
@@ -2,23 +2,27 @@ import { runAction } from 'cerebral/test';
 
 import presenter from '..';
 
+import sinon from 'sinon';
 import getCaseTypesAction from './getCaseTypesAction';
+const getCaseTypesStub = sinon.stub().resolves(null);
 
 presenter.providers.applicationContext = {
   getUseCases: () => ({
-    getCaseTypes: async () => {
-      return null;
-    },
+    getCaseTypes: getCaseTypesStub,
   }),
 };
 
+const successStub = sinon.stub();
+const errorStub = sinon.stub();
+
 presenter.providers.path = {
-  success() {},
-  error() {},
+  success: successStub,
+  error: errorStub,
 };
 
 describe('getCaseTypesAction', async () => {
-  it('should take the error path if the getCaseTypes fails', async () => {
+  it('calls the success path on success', async () => {
+    getCaseTypesStub.resolves([]);
     await runAction(getCaseTypesAction, {
       state: {
         user: {
@@ -30,5 +34,22 @@ describe('getCaseTypesAction', async () => {
         presenter,
       },
     });
+    expect(successStub.called).toBeTruthy();
+  });
+
+  it('should take the error path if the getCaseTypes fails', async () => {
+    getCaseTypesStub.resolves(null);
+    await runAction(getCaseTypesAction, {
+      state: {
+        user: {
+          userId: 'docketclerk',
+          token: 'docketclerk',
+        },
+      },
+      modules: {
+        presenter,
+      },
+    });
+    expect(errorStub.called).toBeTruthy();
   });
 });
