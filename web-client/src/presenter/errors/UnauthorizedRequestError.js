4fd03e3e612e1ffd230f568185f46bbbe346e694
Kris Koskelin <kkoskelin@flexion.us>
2019-01-28 14:37:58 -0600

    lint

--- web-client/src/presenter/errors/UnauthorizedRequestError.js
+++ web-client/src/presenter/errors/UnauthorizedRequestError.js
@@ -3,6 +3,6 @@ export class UnauthorizedRequestError extends CerebralError {
   // HTTP 403, 404
   constructor() {
     super(arguments);
-    this.title = "We can't find the page you requested";
+    this.title = 'We cannot find the page you requested';
   }
 }
