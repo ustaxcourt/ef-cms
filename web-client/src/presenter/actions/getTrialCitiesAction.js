73c2390b3f4daf8ee034525ef40f4db922b23c92
Eric Sorenson <eric@sorenson.software>
2019-01-29 13:38:25 -0600

    removing error paths from actions

--- web-client/src/presenter/actions/getTrialCitiesAction.js
+++ web-client/src/presenter/actions/getTrialCitiesAction.js
@@ -1,17 +1,8 @@
-export default async ({ applicationContext, path, props }) => {
+export default async ({ applicationContext, props }) => {
   const useCases = applicationContext.getUseCases();
   const trialCities = await useCases.getTrialCities({
     procedureType: props.value,
     applicationContext: applicationContext,
   });
-  if (trialCities) {
-    return path.success({ trialCities });
-  } else {
-    return path.error({
-      alertError: {
-        title: 'Problem retrieving trial cities',
-        message: 'There was an error retrieving the trial cities.',
-      },
-    });
-  }
+  return { trialCities };
 };
