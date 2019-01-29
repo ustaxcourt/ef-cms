4984ad3c971af1b66979651e62845682889f27bc
Eric Sorenson <eric@sorenson.software>
2019-01-29 13:38:25 -0600

    removing error paths from actions

--- web-client/src/presenter/actions/createCaseAction.js
+++ web-client/src/presenter/actions/createCaseAction.js
@@ -1,42 +1,31 @@
 import { state } from 'cerebral';
 import { omit } from 'lodash';
 
-export default async ({ applicationContext, get, store, path }) => {
-  try {
-    const { petitionFile } = get(state.petition);
-    const useCases = applicationContext.getUseCases();
+export default async ({ applicationContext, get, store }) => {
+  const { petitionFile } = get(state.petition);
+  const useCases = applicationContext.getUseCases();
 
-    const fileHasUploaded = () => {
-      store.set(
-        state.petition.uploadsFinished,
-        get(state.petition.uploadsFinished) + 1,
-      );
-    };
-
-    const form = omit(
-      {
-        ...get(state.form),
-        irsNoticeDate: `${get(state.form.year)}-${get(state.form.month)}-${get(
-          state.form.day,
-        )}`,
-      },
-      ['year', 'month', 'day', 'trialCities', 'signature'],
+  const fileHasUploaded = () => {
+    store.set(
+      state.petition.uploadsFinished,
+      get(state.petition.uploadsFinished) + 1,
     );
+  };
 
-    await useCases.filePetition({
-      applicationContext,
-      petitionMetadata: form,
-      petitionFile,
-      fileHasUploaded,
-    });
+  const form = omit(
+    {
+      ...get(state.form),
+      irsNoticeDate: `${get(state.form.year)}-${get(state.form.month)}-${get(
+        state.form.day,
+      )}`,
+    },
+    ['year', 'month', 'day', 'trialCities', 'signature'],
+  );
 
-    return path.success();
-  } catch (err) {
-    return path.error({
-      alertError: {
-        title: 'An server error has occured trying to create the case',
-        message: err.message,
-      },
-    });
-  }
+  await useCases.filePetition({
+    applicationContext,
+    petitionMetadata: form,
+    petitionFile,
+    fileHasUploaded,
+  });
 };
--- web-client/src/presenter/actions/getInternalUsersAction.js
+++ web-client/src/presenter/actions/getInternalUsersAction.js
@@ -1,17 +1,6 @@
-export default async ({ applicationContext, path }) => {
+export default async ({ applicationContext }) => {
   const users = await applicationContext
     .getUseCases()
     .getInternalUsers({ applicationContext });
-  if (!users || !users.length) {
-    return path.error({
-      alertError: {
-        title: 'User not found',
-        message: 'Username or password are incorrect',
-      },
-    });
-  } else {
-    return path.success({
-      users,
-    });
-  }
+  return { users };
 };
