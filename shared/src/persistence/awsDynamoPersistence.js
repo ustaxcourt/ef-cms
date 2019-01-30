90c847526fa31462f954dbade3c17df8f95a19c5
Cody Seibert <cseibert@flexion.us>
2019-01-30 17:08:58 -0500

    adding an interactor for getting the completed workitems in the last 7 days

--- shared/src/persistence/awsDynamoPersistence.js
+++ shared/src/persistence/awsDynamoPersistence.js
@@ -16,6 +16,46 @@ const stripInternalKeys = items => {
 };
 exports.stripInternalKeys = stripInternalKeys;
 
+const getSortRecordsViaMapping = async ({
+  applicationContext,
+  key,
+  type,
+  foreignKey,
+  afterDate,
+  isVersioned = false,
+}) => {
+  const TABLE = `efcms-${applicationContext.environment.stage}`;
+
+  const mapping = await client.query({
+    applicationContext,
+    TableName: TABLE,
+    ExpressionAttributeNames: {
+      '#pk': 'pk',
+      '#sk': 'sk',
+    },
+    ExpressionAttributeValues: {
+      ':pk': `${key}|${type}`,
+      ':afterDate': afterDate,
+    },
+    KeyConditionExpression: '#pk = :pk AND #sk >= :afterDate',
+  });
+
+  const ids = mapping.map(metadata => metadata[foreignKey]);
+
+  const results = await client.batchGet({
+    applicationContext,
+    tableName: TABLE,
+    keys: ids.map(id => ({
+      pk: id,
+      sk: isVersioned ? '0' : id,
+    })),
+  });
+
+  return stripInternalKeys(results);
+};
+
+exports.getSortRecordsViaMapping = getSortRecordsViaMapping;
+
 const getRecordsViaMapping = async ({
   applicationContext,
   key,
