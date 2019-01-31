076820ff92d7545dbcf96cb59e22831c87e2e389
Eric Sorenson <eric@sorenson.software>
2019-01-31 10:25:49 -0600

    add tests

--- web-client/src/presenter/sequences/clickServeToIrsSequence.js
+++ web-client/src/presenter/sequences/clickServeToIrsSequence.js
@@ -2,11 +2,13 @@ import { toggle } from 'cerebral/factories';
 import { state } from 'cerebral';
 
 import clearAlerts from '../actions/clearAlertsAction';
+import getFormCombinedWithCaseDetail from '../actions/getFormCombinedWithCaseDetailAction';
 import setValidationAlertErrorsAction from '../actions/setValidationAlertErrorsAction';
 import validateCaseDetail from '../actions/validateCaseDetailAction';
 
 export default [
   clearAlerts,
+  getFormCombinedWithCaseDetail,
   validateCaseDetail,
   {
     success: [toggle(state.showModal)],
