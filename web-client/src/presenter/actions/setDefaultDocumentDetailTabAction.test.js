901a7fbf227d3c4d0dd6c3296deb89996fef82dd
Eric Sorenson <eric@sorenson.software>
2019-02-04 08:43:43 -0600

    implement PR comments

--- web-client/src/presenter/actions/setDefaultDocumentDetailTabAction.test.js
+++ web-client/src/presenter/actions/setDefaultDocumentDetailTabAction.test.js
@@ -3,22 +3,22 @@ import { runAction } from 'cerebral/test';
 import setDefaultDocumentDetailTab from './setDefaultDocumentDetailTabAction';
 
 describe('setDefaultDocumentDetailTab', async () => {
-  it('returns "Document Info" when isEditablePetition is true', async () => {
+  it('returns "Document Info" when showCaseDetailsEdit is true', async () => {
     const { state } = await runAction(setDefaultDocumentDetailTab, {
       state: {
         documentDetailHelper: {
-          isEditablePetition: true,
+          showCaseDetailsEdit: true,
         },
       },
     });
     expect(state.currentTab).toEqual('Document Info');
   });
 
-  it('returns "Pending Messages" when isEditablePetition is false', async () => {
+  it('returns "Pending Messages" when showCaseDetailsEdit is false', async () => {
     const { state } = await runAction(setDefaultDocumentDetailTab, {
       state: {
         documentDetailHelper: {
-          isEditablePetition: false,
+          showCaseDetailsEdit: false,
         },
       },
     });
--- web-client/src/presenter/computeds/documentDetailHelper.js
+++ web-client/src/presenter/computeds/documentDetailHelper.js
@@ -7,11 +7,8 @@ export default get => {
       const actions = get(state.workItemActions);
       return actions[workItemId] === action;
     },
-    isEditablePetition: ['New', 'Recalled'].includes(caseDetail.status),
     showCaseDetailsView: caseDetail.status === 'Batched for IRS',
     showCaseDetailsEdit: ['New', 'Recalled'].includes(caseDetail.status),
-    showServeToIrsButton: ['New', 'Recalled'].includes(caseDetail.status),
-    showRecallButton: caseDetail.status === 'Batched for IRS',
     showDocumentInfo: get(state.currentTab) === 'Document Info',
     showPendingMessages: get(state.currentTab) === 'Pending Messages',
   };
