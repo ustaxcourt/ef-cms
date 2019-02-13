6e062077bf38b18879813341fe7d088051b10cd5
Cody Seibert <cseibert113@gmail.com>
2019-02-13 16:04:01 -0500

    removing volume mounts

--- efcms-service/docker-cognito-users.sh
+++ efcms-service/docker-cognito-users.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 ENV=$1
 docker build -t efcms-build -f ../Dockerfile.build ..
-docker run -v ~/.aws:/root/.aws -e "AWS_PROFILE=FlexionUSTC" --rm efcms-build /bin/sh -c "cd efcms-service && ./setup-cognito-users.sh $ENV"
+docker run --rm efcms-build /bin/sh -c "cd efcms-service && ./setup-cognito-users.sh $ENV"
