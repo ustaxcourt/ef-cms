3ceaffb1dd0fd2756e25718fb6abdb1326f051f0
Cody Seibert <cseibert113@gmail.com>
2019-02-13 16:04:01 -0500

    removing volume mounts

--- web-client/docker-build.sh
+++ web-client/docker-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 ENV=$1
 docker build -t web-client-build -f ../Dockerfile.web-client ..
-docker run -v ~/.aws:/root/.aws -e "AWS_PROFILE=FlexionUSTC" -e "API_URL=${API_URL}" --name "${CONTAINER_NAME}" web-client-build /bin/sh -c "cd web-client && ./build-dist.sh $ENV"
+docker run -e "API_URL=${API_URL}" --name "${CONTAINER_NAME}" web-client-build /bin/sh -c "cd web-client && ./build-dist.sh $ENV"
 CODE="$?"
 docker cp "${CONTAINER_NAME}:/home/app/web-client/dist" dist
 docker rm "${CONTAINER_NAME}"
