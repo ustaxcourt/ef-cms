c348c904baa1aab1a3b3081ac4ed6697948eed79
Kris Koskelin <kkoskelin@flexion.us>
2019-02-14 08:50:34 -0600

    Entity validation routines
    Bump coverage for User entity

--- shared/src/business/entities/PetitionForEstateWithoutExecutor.js
+++ shared/src/business/entities/PetitionForEstateWithoutExecutor.js
@@ -3,21 +3,23 @@ const {
 } = require('../../utilities/JoiValidationDecorator');
 const joi = require('joi-browser');
 
-function PetitionForPetitioner(rawPetition) {
+function PetitionForEstateWithoutExecutor(rawPetition) {
   Object.assign(this, rawPetition);
 }
 
-PetitionForPetitioner.errorToMessageMap = {
-  name: 'Name is a required field',
+PetitionForEstateWithoutExecutor.errorToMessageMap = {
+  nameOfDecedent: 'Name of Decedent is a required field',
+  address1: 'Address is a required field',
+  inCareOf: 'In Care Of is a required field',
 };
 
 joiValidationDecorator(
-  PetitionForPetitioner,
+  PetitionForEstateWithoutExecutor,
   joi.object().keys({
     contactPrimary: joi
       .object()
       .keys({
-        name: joi.string().required(),
+        nameOfDecedent: joi.string().required(),
         address1: joi.string().required(),
         city: joi.string().required(),
         state: joi.string().required(),
@@ -25,11 +27,12 @@ joiValidationDecorator(
         country: joi.string().required(),
         phone: joi.string().required(),
         email: joi.string().required(),
+        inCareOf: joi.string().required(),
       })
       .required(),
   }),
   undefined,
-  PetitionForPetitioner.errorToMessageMap,
+  PetitionForEstateWithoutExecutor.errorToMessageMap,
 );
 
-module.exports = PetitionForPetitioner;
+module.exports = PetitionForEstateWithoutExecutor;
