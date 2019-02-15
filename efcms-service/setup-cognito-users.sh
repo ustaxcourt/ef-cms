7897c8c5424fb2f35dc5b0c97845fb243f21731e
Cody Seibert <cseibert113@gmail.com>
2019-02-15 11:57:35 -0500

    appending $i to the names

--- efcms-service/setup-cognito-users.sh
+++ efcms-service/setup-cognito-users.sh
@@ -14,7 +14,7 @@ CLIENT_ID="${CLIENT_ID#\"}"
 generate_post_data() {
   email=$1
   role=$2
-  name=$role
+  name="Test ${role}$i"
   cat <<EOF
 {
   "email": "$email",
@@ -28,13 +28,14 @@ EOF
 createAccount() {
   email=$1
   role=$2
+  i=$3
   # TODO: this is some generic old expired token... but needed for now
   token="eyJraWQiOiJ2U2pTa3FZVkJjVkJOWk5qZ1gzWFNzcERZSjU4QmQ3OGYrSzlDSXhtck44PSIsImFsZyI6IlJTMjU2In0.eyJhdF9oYXNoIjoiSmk3ekxYSnRkTklJbEEyU1dRTkhfdyIsInN1YiI6IjVmOGUwMGViLTgyMGEtNDNjMi05M2IzLTQyY2FlZjgxZjAxYiIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfN3VSa0YwQXhuIiwiY29nbml0bzp1c2VybmFtZSI6IjVmOGUwMGViLTgyMGEtNDNjMi05M2IzLTQyY2FlZjgxZjAxYiIsImF1ZCI6IjZ0dTZqMXN0djV1Z2N1dDdkcXNxZHVybjhxIiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE1NTAwOTkyOTgsIm5hbWUiOiJUZXN0IERvY2tldGNsZXJrIiwiZXhwIjoxNTUwMTAyODk4LCJjdXN0b206cm9sZSI6ImRvY2tldGNsZXJrIiwiaWF0IjoxNTUwMDk5Mjk4LCJlbWFpbCI6ImRvY2tldGNsZXJrMUBleGFtcGxlLmNvbSJ9.lGDd5xVHj-NJFSdifJyx4gOdt4J2BOxtrmYxebRouQf_XI0j2gqbKne4Jr3nzlpL9pWibj8PBX8zLYj4PKSdPvVFI_zTXoApR4duUOQtInwCbffwojPnzNPS4dGJhnfNnH9Wn_EdMMRcWbbdXoKyUMQmdYXD5sUJh2wX3Eur9r9bJ2MdLuw6Iuy6aQ86mD1_qkk24Oq-LVLA9MboD97vY42aXlbUFInmld_TgjvNuXcw4QFztUPUrtz7O-tRdOtfzBRaDYUg6SOZl4fN-RoI3l1SULhBYWE1yBXE173uOOGse_rqpjfnXs_tcCmoicQEcjzpD9WLWXLyEX0FObj9gQ"
 
   curl --header "Content-Type: application/json" \
     --header "Authorization: Bearer ${token}" \
     --request POST \
-    --data "$(generate_post_data "${email}" "${role}")" \
+    --data "$(generate_post_data "${email}" "${role}" "${i}")" \
       "https://${restApiId}.execute-api.us-east-1.amazonaws.com/${ENV}/v1/users"
 
   response=$(aws cognito-idp admin-initiate-auth \
@@ -62,7 +63,7 @@ createManyAccounts() {
   role=$1
   for i in $(seq 1 5);
   do
-    createAccount "${emailPrefix}${i}@example.com" "${role}"
+    createAccount "${emailPrefix}${i}@example.com" "${role}" "${i}"
   done
 }
 
