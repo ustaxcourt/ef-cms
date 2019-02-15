64cba2570496ca8d23e9e481db7d5b970d98d043
Will Sadler <wsadler@flexion.us>
2019-02-15 09:09:04 -0600

    776 section users * add auth to getInteractors

--- shared/src/business/useCases/utilities/getInteractorForGetUsers.js
+++ shared/src/business/useCases/utilities/getInteractorForGetUsers.js
@@ -1,6 +1,16 @@
 const { getUsersInSection } = require('../users/getUsersInSection.interactor');
+const {
+  isAuthorized,
+  CASE_METADATA,
+} = require('../../../authorization/authorizationClientService');
+const { UnauthorizedError } = require('../../../errors/errors');
 
-exports.getInteractorForGetUsers = ({ section }) => {
+exports.getInteractorForGetUsers = ({ section, applicationContext }) => {
+  const user = applicationContext.getCurrentUser();
+  if (!isAuthorized(user, CASE_METADATA)) {
+    throw new UnauthorizedError('Unauthorized');
+  }
+
   if (section) {
     return getUsersInSection;
   } else {
