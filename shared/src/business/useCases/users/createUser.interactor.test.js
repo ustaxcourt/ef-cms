cad3c66ce82505e106e61f960d24ad7fd7045977
Will Sadler <wsadler@flexion.us>
2019-02-18 11:46:08 -0600

    ship it!

--- shared/src/business/useCases/users/createUser.interactor.test.js
+++ shared/src/business/useCases/users/createUser.interactor.test.js
@@ -1,16 +1,16 @@
 const { createUser } = require('./createUser.interactor');
 
-const MOCK_USER = {
-  userId: 'petitioner1@example.com',
-  role: 'petitions',
-  name: 'Test Petitioner',
-};
 describe('create user', () => {
   it('creates the user', async () => {
+    const mockUser = {
+      userId: 'petitionsclerk1@example.com',
+      role: 'petitions',
+      name: 'Test PetitionsClerk',
+    };
     const applicationContext = {
       getPersistenceGateway: () => {
         return {
-          createUser: () => Promise.resolve(MOCK_USER),
+          createUser: () => Promise.resolve(mockUser),
         };
       },
       getCurrentUser: () => {
@@ -21,11 +21,45 @@ describe('create user', () => {
       },
       environment: { stage: 'local' },
     };
-    const userToCreate = { userId: 'petitioner1@example.com' };
+    const userToCreate = { userId: 'petitionsclerk1@example.com' };
     const user = await createUser({
       userToCreate,
       applicationContext,
     });
     expect(user).not.toBeUndefined();
+  });
+
+  it('throws unauthorized', async () => {
+    const mockUser = {
+      userId: 'petitioner1@example.com',
+      role: 'petitioner',
+      name: 'Test Petitioner',
+    };
+    const applicationContext = {
+      getPersistenceGateway: () => {
+        return {
+          createUser: () => Promise.resolve(mockUser),
+        };
+      },
+      getCurrentUser: () => {
+        return {
+          userId: 'admin',
+          role: 'petitioner',
+        };
+      },
+      environment: { stage: 'local' },
+    };
+    const userToCreate = { userId: 'petitioner1@example.com' };
+    let error;
+    try {
+      await createUser({
+        userToCreate,
+        applicationContext,
+      });
+    } catch (err) {
+      error = err;
+    }
+
+    expect(error).not.toBeUndefined();
   });
 });
