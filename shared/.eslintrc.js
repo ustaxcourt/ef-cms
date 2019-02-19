784d7231493416e919c3eec75f61f4ff54979274
Will Sadler <wsadler@flexion.us>
2019-02-19 08:29:34 -0600

    add jsdoc

--- shared/.eslintrc.js
+++ shared/.eslintrc.js
@@ -30,6 +30,18 @@ module.exports = {
         allowChildren: false,
       },
     ],
+    "jsdoc/require-param": 1,
+    "jsdoc/require-param-description": 1,
+    "jsdoc/require-param-name": 1,
+    "jsdoc/require-param-type": 1,
+    "jsdoc/valid-types": 1,
+    "jsdoc/require-returns": 1,
+    "jsdoc/require-returns-check": 1,
+    "jsdoc/require-returns-description": 1,
+    "jsdoc/require-returns-type": 1,
+    "jsdoc/newline-after-description": 1,
+    "jsdoc/check-param-names": 1,
+    "jsdoc/check-types": 1,
   },
   settings: {
     react: {
