e2c5e8f7051d2800f297238f2ecc73687d86d848
Kris Koskelin <kkoskelin@flexion.us>
2019-02-20 17:00:38 -0600

    Docket record component
    Reuse of layout, etc; required additional helpers.

---
+++ web-client/src/views/DocketRecord.jsx
@@ -0,0 +1,125 @@
+import { connect } from '@cerebral/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { sequences, state } from 'cerebral';
+import React from 'react';
+
+export default connect(
+  {
+    baseUrl: state.baseUrl,
+    caseDetail: state.formattedCaseDetail,
+    clearDocumentSequence: sequences.clearDocumentSequence,
+    helper: state.caseDetailHelper,
+    token: state.token,
+    updateCurrentTabSequence: sequences.updateCurrentTabSequence,
+  },
+  function DocketRecord({
+    baseUrl,
+    caseDetail,
+    helper,
+    clearDocumentSequence,
+    token,
+    updateCurrentTabSequence,
+  }) {
+    return (
+      <React.Fragment>
+        {helper.showFileDocumentButton && (
+          <button
+            id="button-file-document"
+            className="usa-button"
+            onClick={() => {
+              clearDocumentSequence();
+              updateCurrentTabSequence({ value: 'File Document' });
+            }}
+          >
+            <FontAwesomeIcon icon="cloud-upload-alt" />
+            File Document
+          </button>
+        )}
+        <table
+          className="responsive-table"
+          id="docket-record"
+          aria-label="docket record"
+        >
+          <thead>
+            <tr>
+              <th>Date filed</th>
+              <th>Title</th>
+              <th>Filed by</th>
+              <th>Status</th>
+              {helper.showDocketRecordActionColumn && <th>Action</th>}
+            </tr>
+          </thead>
+          <tbody>
+            {caseDetail.documents.map((document, idx) => (
+              <tr key={idx}>
+                <td className="responsive-title">
+                  <span className="responsive-label">Activity date</span>
+                  {document.createdAtFormatted}
+                </td>
+                <td>
+                  <span className="responsive-label">Title</span>
+                  {helper.showDirectDownloadLink && (
+                    <a
+                      href={`${baseUrl}/documents/${
+                        document.documentId
+                      }/documentDownloadUrl?token=${token}`}
+                      target="_blank"
+                      rel="noreferrer noopener"
+                      aria-label={`View PDF: ${document.documentType}`}
+                    >
+                      <FontAwesomeIcon icon="file-pdf" />
+                      {document.documentType}
+                    </a>
+                  )}
+                  {helper.showDocumentDetailLink && (
+                    <a
+                      href={`/case-detail/${
+                        caseDetail.docketNumber
+                      }/documents/${document.documentId}`}
+                      aria-label="View PDF"
+                    >
+                      <FontAwesomeIcon icon="file-pdf" />
+                      {document.documentType}
+                    </a>
+                  )}
+                </td>
+                <td>
+                  <span className="responsive-label">Filed by</span>
+                  {document.filedBy}
+                </td>
+                <td>
+                  <span className="responsive-label">Status</span>
+                  {document.isStatusServed && (
+                    <span>{caseDetail.datePetitionSentToIrsMessage}</span>
+                  )}
+                  {helper.showDocumentStatus && <span>{document.status}</span>}
+                </td>
+                {helper.showDocketRecordActionColumn && <td />}
+              </tr>
+            ))}
+            {helper.showPaymentRecord && (
+              <tr>
+                <td>{caseDetail.payGovDateFormatted}</td>
+                <td>Filing fee paid</td>
+                <td />
+                <td />
+                {helper.showDocketRecordActionColumn && <td />}
+              </tr>
+            )}
+            {helper.showPreferredTrialCity && (
+              <tr>
+                <td>{caseDetail.createdAtFormatted}</td>
+                <td>
+                  Request for Place of Trial at {caseDetail.preferredTrialCity}
+                </td>
+                <td />
+                <td />
+                {helper.showDocketRecordActionColumn && <td />}
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  },
+);
