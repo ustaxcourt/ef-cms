0e07d2de12c62f86b4f9d113aa4de354bc146a8f
Eric Sorenson <eric@sorenson.software>
2019-02-20 16:04:26 -0600

    extracted tabs component

--- web-client/src/views/WorkQueue.jsx
+++ web-client/src/views/WorkQueue.jsx
@@ -1,63 +1,20 @@
 import { connect } from '@cerebral/react';
+import { state } from 'cerebral';
 import React from 'react';
-import { state, sequences } from 'cerebral';
-import SectionWorkQueue from './SectionWorkQueue';
+
+import { WorkQueueTabs } from './WorkQueueTabs';
 import IndividualWorkQueue from './IndividualWorkQueue';
+import SectionWorkQueue from './SectionWorkQueue';
 
 export default connect(
   {
-    chooseWorkQueueSequence: sequences.chooseWorkQueueSequence,
     workQueueHelper: state.workQueueHelper,
   },
-  function WorkQueue({ chooseWorkQueueSequence, workQueueHelper }) {
+  function WorkQueue({ workQueueHelper }) {
     return (
       <React.Fragment>
         <h1 tabIndex="-1">Work Queue</h1>
-        <nav className="horizontal-tabs subsection">
-          <ul role="tablist">
-            <li
-              className={
-                workQueueHelper.showIndividualWorkQueue ? 'active' : ''
-              }
-            >
-              <button
-                role="tab"
-                className="tab-link"
-                id="tab-my-queue"
-                aria-controls="tab-individual-panel"
-                aria-selected={workQueueHelper.showIndividualWorkQueue}
-                onClick={() =>
-                  chooseWorkQueueSequence({
-                    queue: 'my',
-                    box: 'inbox',
-                  })
-                }
-              >
-                My Queue
-              </button>
-            </li>
-            <li
-              className={workQueueHelper.showSectionWorkQueue ? 'active' : ''}
-            >
-              <button
-                role="tab"
-                className="tab-link"
-                id="tab-work-queue"
-                aria-controls="tab-section-panel"
-                aria-selected={workQueueHelper.showSectionWorkQueue}
-                onClick={() =>
-                  chooseWorkQueueSequence({
-                    queue: 'section',
-                    box: 'inbox',
-                  })
-                }
-              >
-                Section Queue
-              </button>
-            </li>
-          </ul>
-        </nav>
-
+        <WorkQueueTabs />
         {workQueueHelper.showIndividualWorkQueue && (
           <div role="tabpanel" id="tab-individual-panel">
             <IndividualWorkQueue />
