2c40c4d32434a9ea0017387e387ce250481f3acf
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-02-21 10:11:59 -0800

    a little more refactoring of contact view options

--- web-client/src/views/StartCase/Contacts.jsx
+++ web-client/src/views/StartCase/Contacts.jsx
@@ -9,29 +9,8 @@ export default connect(
   function Contacts({ startCaseHelper }) {
     return (
       <React.Fragment>
-        {(startCaseHelper.showPetitionerContact ||
-          startCaseHelper.showDonorContact ||
-          startCaseHelper.showCorporationContact ||
-          startCaseHelper.showEstateWithoutExecutorContact ||
-          startCaseHelper.showTransfereeContact) && <ContactPrimary />}
-        {(startCaseHelper.showPetitionerAndSpouseContact ||
-          startCaseHelper.showPetitionerAndDeceasedSpouseContact ||
-          startCaseHelper.showEstateWithExecutorContact ||
-          startCaseHelper.showConservatorContact ||
-          startCaseHelper.showCustodianContact ||
-          startCaseHelper.showGuardianContact ||
-          startCaseHelper.showIncompetentPersonContact ||
-          startCaseHelper.showMinorContact ||
-          startCaseHelper.showPartnershipBBAContact ||
-          startCaseHelper.showPartnershipOtherContact ||
-          startCaseHelper.showPartnershipTaxMattersContact ||
-          startCaseHelper.showSurvivingSpouseContact ||
-          startCaseHelper.showTrustAndTrusteeContact) && (
-          <React.Fragment>
-            <ContactPrimary />
-            <ContactSecondary />
-          </React.Fragment>
-        )}
+        {startCaseHelper.showPrimaryContact && <ContactPrimary />}
+        {startCaseHelper.showSecondaryContact && <ContactSecondary />}
       </React.Fragment>
     );
   },
