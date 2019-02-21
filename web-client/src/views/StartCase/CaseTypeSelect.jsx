6969dc15bd168d8f0c4a69310355f87b63a8ceba
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-02-21 09:09:20 -0800

    588: add notice options to start a case view

---
+++ web-client/src/views/StartCase/CaseTypeSelect.jsx
@@ -0,0 +1,55 @@
+import { connect } from '@cerebral/react';
+import { sequences, state, props } from 'cerebral';
+import React from 'react';
+
+export default connect(
+  {
+    caseTypes: state.caseTypes,
+    legend: props.legend,
+    updateFormValueSequence: sequences.updateFormValueSequence,
+    validationErrors: state.validationErrors,
+    validateStartCaseSequence: sequences.validateStartCaseSequence,
+  },
+  function CorporationContact({
+    caseTypes,
+    legend,
+    updateFormValueSequence,
+    validationErrors,
+    validateStartCaseSequence,
+  }) {
+    return (
+      <div
+        className={
+          'usa-form-group ' +
+          (validationErrors.caseType ? 'usa-input-error' : '')
+        }
+      >
+        <fieldset>
+          <legend>{legend}</legend>
+          <select
+            name="caseType"
+            id="case-type"
+            aria-labelledby="case-type"
+            onChange={e => {
+              updateFormValueSequence({
+                key: e.target.name,
+                value: e.target.value,
+              });
+              validateStartCaseSequence();
+            }}
+          >
+            <option value="">-- Select --</option>
+            {caseTypes.map(caseType => (
+              <option key={caseType.type} value={caseType.type}>
+                {caseType.description}
+              </option>
+            ))}
+          </select>
+        </fieldset>
+        <div className="usa-input-error-message beneath">
+          {validationErrors.caseType}
+        </div>
+      </div>
+    );
+  },
+);
