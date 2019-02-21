9adba0f4a4f5c75cce1996874331b5a5659d3a4f
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-02-21 15:44:01 -0800

    change jsdoc back to object

--- web-client/src/presenter/actions/getCaseTypesAction.js
+++ web-client/src/presenter/actions/getCaseTypesAction.js
@@ -4,7 +4,7 @@
  * @param {Object} providers the providers object
  * @param {Object} providers.applicationContext needed for getting the getCaseTypes use case
  * @param {Function} providers.get the cerebral get function used for getting state.user.userId
- * @returns {Array} contains the caseTypes array returned from the getCaseTypes use case
+ * @returns {Object} contains the caseTypes array returned from the getCaseTypes use case
  */
 export const getCaseTypes = async ({ applicationContext }) => {
   const caseTypes = await applicationContext.getUseCases().getCaseTypes({
