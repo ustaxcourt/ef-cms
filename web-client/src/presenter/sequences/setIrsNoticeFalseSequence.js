bed2948689be0b8ff90627f1f852385bc6f035fb
Cody Seibert <cseibert113@gmail.com>
2019-02-21 14:05:16 -0500

    adding the has irs notice radio buttons to the case detail screen

---
+++ web-client/src/presenter/sequences/setIrsNoticeFalseSequence.js
@@ -0,0 +1,9 @@
+import { set } from 'cerebral/factories';
+import { state } from 'cerebral';
+
+export default [
+  set(state.form.irsMonth, ''),
+  set(state.form.irsDay, ''),
+  set(state.form.irsYear, ''),
+  set(state.caseDetail.hasIrsNotice, false),
+];
