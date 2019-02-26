bbbb4faf24fa85ae20500dad2f11f34ad544d7be
Eric Sorenson <eric@sorenson.software>
2019-02-26 14:08:19 -0600

    Capitalization fixes

--- web-client/src/views/CaseListPetitioner.jsx
+++ web-client/src/views/CaseListPetitioner.jsx
@@ -29,26 +29,26 @@ export const CaseListPetitioner = connect(
         <table className="responsive-table dashboard" id="case-list">
           <thead>
             <tr>
-              <th>Docket number</th>
-              <th>Date filed</th>
-              <th>Petitioner name</th>
+              <th>Docket Number</th>
+              <th>Date Filed</th>
+              <th>Petitioner Name</th>
             </tr>
           </thead>
           <tbody>
             {caseList.map(item => (
               <tr key={item.docketNumber}>
                 <td className="responsive-title">
-                  <span className="responsive-label">Docket number</span>
+                  <span className="responsive-label">Docket Number</span>
                   <a href={'/case-detail/' + item.docketNumber}>
                     {item.docketNumberWithSuffix}
                   </a>
                 </td>
                 <td>
-                  <span className="responsive-label">Date filed</span>
+                  <span className="responsive-label">Date Filed</span>
                   {item.createdAtFormatted}
                 </td>
                 <td>
-                  <span className="responsive-label">Petitioner name</span>
+                  <span className="responsive-label">Petitioner Name</span>
                   {item.petitionerName}
                 </td>
               </tr>
