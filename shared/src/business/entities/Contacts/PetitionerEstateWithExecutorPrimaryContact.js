4782cbd108b4d61006408d108c09ff61b94e0b03
Cody Seibert <cseibert113@gmail.com>
2019-02-27 12:47:03 -0500

    refactoring the entity types to dry things up

--- shared/src/business/entities/Contacts/PetitionerEstateWithExecutorPrimaryContact.js
+++ shared/src/business/entities/Contacts/PetitionerEstateWithExecutorPrimaryContact.js
@@ -1,40 +1,6 @@
-const {
-  joiValidationDecorator,
-} = require('../../../utilities/JoiValidationDecorator');
-const joi = require('joi-browser');
+const { createContactFactory } = require('./PetitionContact');
 
-function PetitionerEstateWithExecutorPrimaryContact(raw) {
-  Object.assign(this, raw);
-}
-
-PetitionerEstateWithExecutorPrimaryContact.errorToMessageMap = {
-  name: 'Name is a required field.',
-  address1: 'Address is a required field.',
-  city: 'City is a required field.',
-  state: 'State is a required field.',
-  zip: [
-    {
-      contains: 'match',
-      message: 'Please enter a valid zip code.',
-    },
-    'Zip Code is a required field.',
-  ],
-};
-
-joiValidationDecorator(
-  PetitionerEstateWithExecutorPrimaryContact,
-  joi.object().keys({
-    name: joi.string().required(),
-    address1: joi.string().required(),
-    city: joi.string().required(),
-    state: joi.string().required(),
-    zip: joi
-      .string()
-      .regex(/^\d{5}(-\d{4})?$/)
-      .required(),
-  }),
-  undefined,
-  PetitionerEstateWithExecutorPrimaryContact.errorToMessageMap,
-);
-
-module.exports = PetitionerEstateWithExecutorPrimaryContact;
+exports.getPetitionerEstateWithExecutorPrimaryContact = createContactFactory({
+  additionalErrorMappings: {},
+  additionalValidation: {},
+});
--- web-client/e2e/journey/taxpayerCreatesNewCase.js
+++ web-client/e2e/journey/taxpayerCreatesNewCase.js
@@ -701,6 +701,11 @@ export default (test, fakeFile) => {
     });
 
     await test.runSequence('submitFilePetitionSequence');
+
+    console.log(test.getState('form'));
+    console.log(test.getState('form'));
+    console.log(test.getState('form'));
+
     expect(test.getState('alertError')).toEqual(null);
 
     expect(test.getState('alertSuccess')).toEqual({
