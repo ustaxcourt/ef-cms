4925780a102d756139e37df818df2f8aeb0ef23b
Cody Seibert <cseibert113@gmail.com>
2019-02-28 14:17:17 -0500

    adding jsdocs

--- shared/src/business/entities/Contacts/PetitionerPartnershipRepContact.js
+++ shared/src/business/entities/Contacts/PetitionerPartnershipRepContact.js
@@ -1,5 +1,8 @@
 const { createContactFactory } = require('./PetitionContact');
 
+/**
+ * returns the constructor used for creating the PetitionerPartnershipRepContact entity
+ */
 exports.getPetitionerPartnershipRepContact = createContactFactory({
   additionalErrorMappings: {},
   additionalValidation: {},
