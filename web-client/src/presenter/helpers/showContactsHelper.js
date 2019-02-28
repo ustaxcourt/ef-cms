83675a885a7b4753e4f731b2d63fedf3affff63f
William Bagayoko <wbagayoko@gmail.com>
2019-02-28 11:14:32 -0600

    655: update to use includes

--- web-client/src/presenter/helpers/showContactsHelper.js
+++ web-client/src/presenter/helpers/showContactsHelper.js
@@ -1,40 +1,42 @@
 import { PARTY_TYPES } from '../../../../shared/src/business/entities/Contacts/PetitionContact';
 
 export const showContactsHelper = partyType => {
-  const contactPrimary =
-    partyType === PARTY_TYPES.petitioner ||
-    partyType === PARTY_TYPES.petitionerSpouse ||
-    partyType === PARTY_TYPES.petitionerDeceasedSpouse ||
-    partyType === PARTY_TYPES.estate ||
-    partyType === PARTY_TYPES.estateWithoutExecutor ||
-    partyType === PARTY_TYPES.trust ||
-    partyType === PARTY_TYPES.corporation ||
-    partyType === PARTY_TYPES.partnershipAsTaxMattersPartner ||
-    partyType === PARTY_TYPES.partnershipOtherThanTaxMatters ||
-    partyType === PARTY_TYPES.partnershipBBA ||
-    partyType === PARTY_TYPES.conservator ||
-    partyType === PARTY_TYPES.guardian ||
-    partyType === PARTY_TYPES.custodian ||
-    partyType === PARTY_TYPES.nextFriendForMinor ||
-    partyType === PARTY_TYPES.nextFriendForIncompetentPerson ||
-    partyType === PARTY_TYPES.donor ||
-    partyType === PARTY_TYPES.transferee ||
-    partyType === PARTY_TYPES.survivingSpouse;
+  const contactPrimary = [
+    PARTY_TYPES.conservator,
+    PARTY_TYPES.corporation,
+    PARTY_TYPES.custodian,
+    PARTY_TYPES.donor,
+    PARTY_TYPES.estate,
+    PARTY_TYPES.estateWithoutExecutor,
+    PARTY_TYPES.guardian,
+    PARTY_TYPES.nextFriendForIncompetentPerson,
+    PARTY_TYPES.nextFriendForMinor,
+    PARTY_TYPES.partnershipAsTaxMattersPartner,
+    PARTY_TYPES.partnershipBBA,
+    PARTY_TYPES.partnershipOtherThanTaxMatters,
+    PARTY_TYPES.petitioner,
+    PARTY_TYPES.petitionerDeceasedSpouse,
+    PARTY_TYPES.petitionerSpouse,
+    PARTY_TYPES.survivingSpouse,
+    PARTY_TYPES.transferee,
+    PARTY_TYPES.trust,
+  ].includes(partyType);
 
-  const contactSecondary =
-    partyType === PARTY_TYPES.petitionerSpouse ||
-    partyType === PARTY_TYPES.petitionerDeceasedSpouse ||
-    partyType === PARTY_TYPES.estate ||
-    partyType === PARTY_TYPES.trust ||
-    partyType === PARTY_TYPES.partnershipAsTaxMattersPartner ||
-    partyType === PARTY_TYPES.partnershipOtherThanTaxMatters ||
-    partyType === PARTY_TYPES.partnershipBBA ||
-    partyType === PARTY_TYPES.conservator ||
-    partyType === PARTY_TYPES.guardian ||
-    partyType === PARTY_TYPES.custodian ||
-    partyType === PARTY_TYPES.nextFriendForMinor ||
-    partyType === PARTY_TYPES.nextFriendForIncompetentPerson ||
-    partyType === PARTY_TYPES.survivingSpouse;
+  const contactSecondary = [
+    PARTY_TYPES.conservator,
+    PARTY_TYPES.custodian,
+    PARTY_TYPES.estate,
+    PARTY_TYPES.guardian,
+    PARTY_TYPES.nextFriendForIncompetentPerson,
+    PARTY_TYPES.nextFriendForMinor,
+    PARTY_TYPES.partnershipAsTaxMattersPartner,
+    PARTY_TYPES.partnershipBBA,
+    PARTY_TYPES.partnershipOtherThanTaxMatters,
+    PARTY_TYPES.petitionerDeceasedSpouse,
+    PARTY_TYPES.petitionerSpouse,
+    PARTY_TYPES.survivingSpouse,
+    PARTY_TYPES.trust,
+  ].includes(partyType);
 
   return { contactPrimary, contactSecondary };
 };
