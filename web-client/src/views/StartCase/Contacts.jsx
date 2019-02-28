5dd605d2cca5ce4d048fc679916e99129cbcfdfc
Eric Sorenson <eric@sorenson.software>
2019-02-28 12:51:20 -0600

    missed named imports

--- web-client/src/views/StartCase/Contacts.jsx
+++ web-client/src/views/StartCase/Contacts.jsx
@@ -1,8 +1,9 @@
 import { connect } from '@cerebral/react';
 import { state } from 'cerebral';
 import React from 'react';
-import ContactPrimary from './ContactPrimary';
-import ContactSecondary from './ContactSecondary';
+
+import { ContactPrimary } from './ContactPrimary';
+import { ContactSecondary } from './ContactSecondary';
 
 export const Contacts = connect(
   { startCaseHelper: state.startCaseHelper },
