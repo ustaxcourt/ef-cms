b6e7ded9a27b1c39aceddbe85fb8dfc2c4f01b47
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-03-01 11:05:08 -0800

    699: add ownership disclosure statement link on parties tab

--- web-client/src/presenter/computeds/caseDetailEditHelper.js
+++ web-client/src/presenter/computeds/caseDetailEditHelper.js
@@ -4,13 +4,33 @@ import { showContactsHelper } from '../computeds/showContactsHelper';
 export const caseDetailEditHelper = get => {
   const { PARTY_TYPES } = get(state.constants);
   const partyType = get(state.caseDetail.partyType);
+  const documents = get(state.caseDetail.documents);
 
   const showContacts = showContactsHelper(partyType, PARTY_TYPES);
+
+  const showOwnershipDisclosureStatement =
+    partyType === PARTY_TYPES.partnershipAsTaxMattersPartner ||
+    partyType === PARTY_TYPES.partnershipOtherThanTaxMatters ||
+    partyType === PARTY_TYPES.partnershipBBA ||
+    partyType === PARTY_TYPES.corporation;
+
+  let ownershipDisclosureStatementDocumentId;
+  if (showOwnershipDisclosureStatement && documents) {
+    const odsDocs = documents.filter(document => {
+      return document.documentType === 'Ownership Disclosure Statement';
+    });
+    if (odsDocs[0]) {
+      ownershipDisclosureStatementDocumentId = odsDocs[0].documentId;
+    }
+  }
 
   return {
     partyTypes: PARTY_TYPES,
 
     showPrimaryContact: showContacts.contactPrimary,
     showSecondaryContact: showContacts.contactSecondary,
+
+    showOwnershipDisclosureStatement,
+    ownershipDisclosureStatementDocumentId,
   };
 };
