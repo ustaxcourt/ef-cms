896a0031db3846f1978b5983c713bda5be51de8f
William Bagayoko <wbagayoko@gmail.com>
2019-03-01 12:19:16 -0600

    699: Add Container Tabs

--- web-client/src/views/CaseDetailEdit/PartyInformation.jsx
+++ web-client/src/views/CaseDetailEdit/PartyInformation.jsx
@@ -3,7 +3,7 @@ import { state, sequences } from 'cerebral';
 import React from 'react';
 import { Contacts } from '../StartCase/Contacts';
 
-export const CaseDetailEditPartyInformation = connect(
+export const PartyInformation = connect(
   {
     caseDetail: state.caseDetail,
     updateCasePartyTypeSequence: sequences.updateCasePartyTypeSequence,
