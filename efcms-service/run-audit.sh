7c36e7910e4a037bf809e357e33d58f6a5b4eec9
Cody Seibert <cseibert113@gmail.com>
2019-03-01 13:04:17 -0500

    adding tr

--- efcms-service/run-audit.sh
+++ efcms-service/run-audit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-ISSUE_COUNT=$(npm audit --json | jq -r '.actions' | jq -r '.[] | .module' | grep -cv -e 'static-eval' -e 'serverless-dynamodb-local')
+ISSUE_COUNT=$(npm audit --json | jq -r '.actions' | jq -r '.[] | .module' | grep -cv -e 'static-eval' -e 'serverless-dynamodb-local' | tr -d ' ')
 if [ "${ISSUE_COUNT}" -ne "0" ] ; then
   echo "Audit issues found"
   exit 1;
