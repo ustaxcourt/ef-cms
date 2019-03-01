aee03aa7e3191f8294c3d1a5d8fef8eb81490465
Cody Seibert <cseibert113@gmail.com>
2019-03-01 13:03:50 -0500

    fixing shellcheck

--- web-client/run-audit.sh
+++ web-client/run-audit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-ISSUE_COUNT=$(npm audit --json | jq -r '.actions' | jq -r '.[] | .module' | grep -v "static-eval" | wc -l | tr -d ' ')
+ISSUE_COUNT=$(npm audit --json | jq -r '.actions' | jq -r '.[] | .module' | grep -cv -e 'static-eval' | tr -d ' ')
 if [ "${ISSUE_COUNT}" -ne "0" ] ; then
   echo "Audit issues found"
   exit 1;
