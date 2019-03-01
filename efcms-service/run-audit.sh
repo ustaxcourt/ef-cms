1f9443d47a353149dfae635ae8c7e179dfb348d4
Cody Seibert <cseibert113@gmail.com>
2019-03-01 13:03:50 -0500

    fixing shellcheck

--- efcms-service/run-audit.sh
+++ efcms-service/run-audit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-ISSUE_COUNT=$(npm audit --json | jq -r '.actions' | jq -r '.[] | .module' | grep -v "static-eval" | grep -v "serverless-dynamodb-local" | wc -l | tr -d ' ')
+ISSUE_COUNT=$(npm audit --json | jq -r '.actions' | jq -r '.[] | .module' | grep -cv -e 'static-eval' -e 'serverless-dynamodb-local')
 if [ "${ISSUE_COUNT}" -ne "0" ] ; then
   echo "Audit issues found"
   exit 1;
