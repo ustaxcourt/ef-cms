c5a30c5ef45f3570f60901182857929a8fe43046
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-03-01 09:05:34 -0800

    use country_types constant

--- web-client/src/presenter/actions/resetContactsAction.js
+++ web-client/src/presenter/actions/resetContactsAction.js
@@ -3,8 +3,8 @@ import { state } from 'cerebral';
 import { showContactsHelper } from '../computeds/showContactsHelper';
 
 /**
- * Forwards the workItem associated with props.workItemId to a form.forwardRecipientId and also adds the message set on form.forwardMessage.
- * Displays a success alert on success.
+ * clears primary and secondary contact in caseDetail depending on
+ * party type (except email); also defaults countryType to domestic
  *
  * @param {Object} providers the providers object
  * @param {Function} providers.get the cerebral get function
@@ -12,14 +12,16 @@ import { showContactsHelper } from '../computeds/showContactsHelper';
  */
 export const resetContactsAction = async ({ get, store }) => {
   const partyType = get(state.caseDetail.partyType);
-  const { PARTY_TYPES } = get(state.constants);
+  const { PARTY_TYPES, COUNTRY_TYPES } = get(state.constants);
   const showContacts = showContactsHelper(partyType, PARTY_TYPES);
 
   store.set(state.caseDetail.contactPrimary, {
-    countryType: 'domestic',
+    countryType: COUNTRY_TYPES.DOMESTIC,
     email: get(state.caseDetail.contactPrimary.email),
   });
   if (showContacts.contactSecondary) {
-    store.set(state.caseDetail.contactSecondary, { countryType: 'domestic' });
+    store.set(state.caseDetail.contactSecondary, {
+      countryType: COUNTRY_TYPES.DOMESTIC,
+    });
   }
 };
