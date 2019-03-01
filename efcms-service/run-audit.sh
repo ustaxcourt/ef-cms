cf39c239cdde336278d39e8dd1a7268499ef002e
Cody Seibert <cseibert113@gmail.com>
2019-03-01 12:51:50 -0500

    audit used to be commented out, adding it back in

---
+++ efcms-service/run-audit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+ISSUE_COUNT=$(npm audit --json | jq -r '.actions' | jq -r '.[] | .module' | grep -v "static-eval" | grep -v "serverless-dynamodb-local" | wc -l | tr -d ' ')
+if [ "${ISSUE_COUNT}" -ne "0" ] ; then
+  echo "Audit issues found"
+  exit 1;
+fi
