318ee652a09cd4428bc2034b74fc995b8c0f4839
Cody Seibert <cseibert113@gmail.com>
2019-03-07 16:59:29 -0500

    working on making better load testing scripts

---
+++ efcms-service/aws-login.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+ENV=$1
+
+REGION="us-east-1"
+restApiId=$(aws apigateway get-rest-apis --region="${REGION}" --query "items[?name=='${ENV}-ef-cms'].id" --output text)
+
+USER_POOL_ID=$(aws cognito-idp list-user-pools --query "UserPools[?Name == 'efcms-${ENV}'].Id | [0]" --max-results 30 --region "${REGION}")
+USER_POOL_ID="${USER_POOL_ID%\"}"
+USER_POOL_ID="${USER_POOL_ID#\"}"
+
+CLIENT_ID=$(aws cognito-idp list-user-pool-clients --user-pool-id "${USER_POOL_ID}" --query "UserPoolClients[?ClientName == 'client'].ClientId | [0]" --max-results 30 --region "${REGION}")
+CLIENT_ID="${CLIENT_ID%\"}"
+CLIENT_ID="${CLIENT_ID#\"}"
+
+response=$(aws cognito-idp admin-initiate-auth \
+  --user-pool-id "${USER_POOL_ID}" \
+  --client-id "${CLIENT_ID}" \
+  --region "${REGION}" \
+  --auth-flow ADMIN_NO_SRP_AUTH \
+  --auth-parameters USERNAME="petitioner1@example.com"',PASSWORD'="Testing1234$") # Temp until we grant ADMIN role access to all endpoints 
+  # --auth-parameters USERNAME="ustcadmin@example.com"',PASSWORD'="${USTC_ADMIN_PASS}") 
+
+adminToken=$(echo "${response}" | jq -r ".AuthenticationResult.IdToken")
+
+echo $adminToken
