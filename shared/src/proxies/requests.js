5d71bf5166f5e5710bc18a85a7ef4a4a75a64b8a
Cody Seibert <cseibert113@gmail.com>
2019-03-11 16:29:02 -0400

    drying up the proxies

---
+++ shared/src/proxies/requests.js
@@ -0,0 +1,77 @@
+/**
+ *
+ * get
+ *
+ * @param applicationContext
+ * @param userId
+ * @param status
+ * @returns {Promise<*>}
+ */
+exports.get = ({ applicationContext, endpoint, params }) =>
+  applicationContext
+    .getHttpClient()
+    .get(`${applicationContext.getBaseUrl()}${endpoint}`, {
+      headers: {
+        Authorization: `Bearer ${applicationContext.getCurrentUserToken()}`,
+      },
+      params,
+    })
+    .then(response => response.data);
+
+/**
+ *
+ * post
+ *
+ * @param applicationContext
+ * @param userId
+ * @param status
+ * @returns {Promise<*>}
+ */
+exports.post = ({ applicationContext, endpoint, body }) =>
+  applicationContext
+    .getHttpClient()
+    .post(`${applicationContext.getBaseUrl()}${endpoint}`, body, {
+      headers: {
+        Authorization: `Bearer ${applicationContext.getCurrentUserToken()}`,
+      },
+    })
+    .then(response => response.data);
+
+/**
+ *
+ * put
+ *
+ * @param applicationContext
+ * @param userId
+ * @param status
+ * @returns {Promise<*>}
+ */
+exports.put = ({ applicationContext, endpoint, body }) =>
+  applicationContext
+    .getHttpClient()
+    .put(`${applicationContext.getBaseUrl()}${endpoint}`, body, {
+      headers: {
+        Authorization: `Bearer ${applicationContext.getCurrentUserToken()}`,
+      },
+    })
+    .then(response => response.data);
+
+/**
+ *
+ * remove
+ *
+ * @param applicationContext
+ * @param userId
+ * @param status
+ * @returns {Promise<*>}
+ */
+exports.remove = ({ applicationContext, endpoint, params }) =>
+  applicationContext
+    .getHttpClient()
+    .delete(`${applicationContext.getBaseUrl()}${endpoint}`, {
+      headers: {
+        Authorization: `Bearer ${applicationContext.getCurrentUserToken()}`,
+      },
+      params,
+    })
+    .then(response => response.data);
