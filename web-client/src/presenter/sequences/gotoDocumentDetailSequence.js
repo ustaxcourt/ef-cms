1753bca53a68d7ff70263600edaf6cdcde225411
Kris Koskelin <kkoskelin@flexion.us>
2019-03-12 10:52:46 -0500

    310: case caption edit (WIP)

--- web-client/src/presenter/sequences/gotoDocumentDetailSequence.js
+++ web-client/src/presenter/sequences/gotoDocumentDetailSequence.js
@@ -1,22 +1,21 @@
-import { state } from 'cerebral';
-import { set } from 'cerebral/factories';
-
 import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { clearFormsAction } from '../actions/clearFormsAction';
 import { clearWorkItemActionMapAction } from '../actions/clearWorkItemActionMapAction';
 import { getCaseAction } from '../actions/getCaseAction';
+import { getCaseTypesAction } from '../actions/getCaseTypesAction';
 import { getInternalUsersAction } from '../actions/getInternalUsersAction';
 import { getProcedureTypesAction } from '../actions/getProcedureTypesAction';
+import { set } from 'cerebral/factories';
 import { setBaseUrlAction } from '../actions/setBaseUrlAction';
 import { setCaseAction } from '../actions/setCaseAction';
+import { setCaseTypesAction } from '../actions/setCaseTypesAction';
 import { setCurrentPageAction } from '../actions/setCurrentPageAction';
 import { setDefaultDocumentDetailTabAction } from '../actions/setDefaultDocumentDetailTabAction';
 import { setDocumentIdAction } from '../actions/setDocumentIdAction';
 import { setFormForCaseAction } from '../actions/setFormForCaseAction';
 import { setInternalUsersAction } from '../actions/setInternalUsersAction';
 import { setProcedureTypesAction } from '../actions/setProcedureTypesAction';
-import { getCaseTypesAction } from '../actions/getCaseTypesAction';
-import { setCaseTypesAction } from '../actions/setCaseTypesAction';
+import { state } from 'cerebral';
 
 export const gotoDocumentDetailSequence = [
   setCurrentPageAction('Loading'),
