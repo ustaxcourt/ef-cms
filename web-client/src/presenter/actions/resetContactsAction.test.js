2e72cac80202098e34099b7aec2a14300126bb13
Cody Seibert <cseibert113@gmail.com>
2019-03-12 14:34:42 -0400

    fixing some test descriptions

--- web-client/src/presenter/actions/resetContactsAction.test.js
+++ web-client/src/presenter/actions/resetContactsAction.test.js
@@ -38,7 +38,7 @@ describe('resetContactsAction', async () => {
     });
   });
 
-  it('clears the contactPrimary except for countryType and email for an international address', async () => {
+  it('clears the contactPrimary except for countryType (which should be set back to the domestic default) and email for an international address', async () => {
     const { state } = await runAction(resetContactsAction, {
       state: {
         caseDetail: {
