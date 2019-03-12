b8c4abd297a3093577125654c280084b9b6e24cd
Eric Sorenson <eric@sorenson.software>
2019-03-12 15:02:05 -0500

    color tweak

--- web-client/src/views/Interstitial.jsx
+++ web-client/src/views/Interstitial.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 export const Interstitial = () => (
-  <div className="progress-indicator">
+  <div className="progress-indicator" aria-live="assertive">
     <FontAwesomeIcon icon="sync" className="fa-spin spinner" size="6x" />
   </div>
 );
