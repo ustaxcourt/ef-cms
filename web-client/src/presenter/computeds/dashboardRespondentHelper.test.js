da7035905bea8d91c6553087e15e67ec6dead35c
Kris Koskelin <kkoskelin@flexion.us>
2019-03-13 10:24:13 -0500

    Fix test descriptions

--- web-client/src/presenter/computeds/dashboardRespondentHelper.test.js
+++ web-client/src/presenter/computeds/dashboardRespondentHelper.test.js
@@ -2,7 +2,7 @@ import { dashboardRespondentHelper } from './dashboardRespondentHelper';
 import { runCompute } from 'cerebral/test';
 
 describe('respondent dashboard helper', () => {
-  it('shows the right things when there are no cases', () => {
+  it('shows "no cases" but not case list when there are no cases', () => {
     const result = runCompute(dashboardRespondentHelper, {
       state: {
         cases: [],
@@ -11,7 +11,7 @@ describe('respondent dashboard helper', () => {
     expect(result.showCaseList).toEqual(false);
     expect(result.showNoCasesMessage).toEqual(true);
   });
-  it('shows the right things when there are no cases', () => {
+  it('shows case list but not "no cases" when there is a case', () => {
     const result = runCompute(dashboardRespondentHelper, {
       state: {
         cases: [{ something: true }],
