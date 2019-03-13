42bd08954d83a0a62e24112ae808cf3488819cb0
Cody Seibert <cseibert113@gmail.com>
2019-03-13 15:02:07 -0400

    removing todo

--- web-client/e2e/caseJourney.test.js
+++ web-client/e2e/caseJourney.test.js
@@ -10,7 +10,7 @@ import docketClerkLogIn from './journey/docketClerkLogIn';
 import docketClerkSelectsAssignee from './journey/docketClerkSelectsAssignee';
 import docketClerkSelectsWorkItems from './journey/docketClerkSelectsWorkItems';
 import docketClerkViewsCaseDetail from './journey/docketClerkViewsCaseDetail';
-import docketClerkViewsDashboard from './journey/docketClerkViewsDashboard'; // TODO: this will need to change since uploaded stipulated decisions do NOT create a work item to the docketclerk user any more
+import docketClerkViewsDashboard from './journey/docketClerkViewsDashboard';
 import docketClerkViewsDashboardAfterForward from './journey/docketClerkViewsDashboardAfterForward';
 import docketClerkViewsDashboardWithoutWorkItem from './journey/docketClerkViewsDashboardWithoutWorkItem';
 import docketClerkViewsDecisionDocument from './journey/docketClerkViewsDecisionDocument';
@@ -52,12 +52,6 @@ const {
   PARTY_TYPES,
   COUNTRY_TYPES,
 } = require('../../shared/src/business/entities/contacts/PetitionContact');
-
-
-
-
-
-
 
 let test;
 global.FormData = FormData;
