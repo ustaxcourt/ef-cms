c3fc54d590ee80e1d63b3a734afd59a55145d7f2
Eric Sorenson <eric@sorenson.software>
2019-03-13 08:24:29 -0500

    added aria label

--- web-client/src/views/Interstitial.jsx
+++ web-client/src/views/Interstitial.jsx
@@ -2,7 +2,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 export const Interstitial = () => (
-  <div className="progress-indicator" aria-live="assertive">
+  <div
+    aria-label="please wait"
+    aria-live="assertive"
+    className="progress-indicator"
+  >
     <FontAwesomeIcon icon="sync" className="fa-spin spinner" size="6x" />
   </div>
 );
