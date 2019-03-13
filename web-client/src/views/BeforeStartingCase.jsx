7b042fa7ebb9a3941df9c088475b3cc481e67550
Eric Sorenson <eric@sorenson.software>
2019-03-13 11:13:16 -0500

    Update the "Before You Get Started Page" with instructional text

--- web-client/src/views/BeforeStartingCase.jsx
+++ web-client/src/views/BeforeStartingCase.jsx
@@ -23,9 +23,9 @@ export const BeforeStartingCase = () => (
             Notice of Determination, you’ll need to include a copy with your
             Petition. The U.S. Tax Court must receive all Petitions in a timely
             manner. The IRS notice shows the last date to file or the number of
-            days you have to file a petition.{' '}
+            days you have to file a Petition.{' '}
             <strong>
-              The Court must receive your electronically filed petition no later
+              The Court must receive your electronically filed Petition no later
               than 11:59 pm Eastern Time on the last date to file.
             </strong>
           </p>
@@ -36,9 +36,10 @@ export const BeforeStartingCase = () => (
           <FontAwesomeIcon icon={['far', 'edit']} />
         </div>
         <div className="before-explanation">
-          <h3>Fill Out Your Petition Form</h3>
+          <h3>Fill Out The Required Forms</h3>
+          <p className="label">Petition Form</p>
           <p>
-            Complete the petition form,{' '}
+            Complete the Petition form,{' '}
             <a
               href="https://www.ustaxcourt.gov/forms/Petition_Simplified_Form_2.pdf"
               target="_blank"
@@ -46,7 +47,7 @@ export const BeforeStartingCase = () => (
             >
               USTC Form 2
             </a>
-            , or you can upload your own petition that complies with the
+            , or you can upload your own Petition that complies with the
             requirements of the{' '}
             <a
               href="https://www.ustaxcourt.gov/rules.htm"
@@ -56,9 +57,38 @@ export const BeforeStartingCase = () => (
               Tax Court Rules of Practice and Procedure
             </a>
             . <strong>Do not</strong> include personal information (such as
-            Social Security Numbers or employer identification numbers,
+            Social Security Numbers or Taxpayer Identification Numbers,
             birthdates, names of minor children, or financial account
             information) in your Petition.
+          </p>
+          <p className="label">Statement of Taxpayer Identification</p>
+          <p>
+            Complete the Statement of Taxpayer Identification form,{' '}
+            <a
+              href="https://www.ustaxcourt.gov/forms/Form_4_Statement_of_Taxpayer_Identification_Number.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              USTC Form 4
+            </a>
+            . This is the only document that should contain your Social Security
+            Number (SSN), Taxpayer Identification Number (TIN), or Employer
+            Identification Number (EIN). This document is sent to the IRS to
+            help identify you, but it’s never viewed by the Court or stored as
+            part of the public record.
+          </p>
+          <p className="label">Ownership Disclosure Statement</p>
+          <p>
+            If you’re filing for a business, you’ll need to complete and submit
+            the Ownership Disclosure Statement,{' '}
+            <a
+              href="https://www.ustaxcourt.gov/forms/Ownership_Disclosure_Statement_Form_6.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              USTC Form 6
+            </a>
+            .
           </p>
         </div>
       </div>
@@ -73,7 +103,7 @@ export const BeforeStartingCase = () => (
           </h3>
           <p>
             If the IRS notice includes personal information (such as Social
-            Security Numbers or employer identification numbers), remove or
+            Security Numbers or Taxpayer Identification Numbers), remove or
             redact that information before including it with your Petition. You
             can remove this information by deleting it, marking through it so
             itʼs illegible, or any other method that will prevent it from being
@@ -89,7 +119,7 @@ export const BeforeStartingCase = () => (
         <div className="before-explanation">
           <h3>Combine Your Petition and IRS Notice(s) Into a Single PDF</h3>
           <p>
-            Scan your petition and IRS notice into one Petition PDF or combine
+            Scan your Petition and IRS notice into one Petition PDF or combine
             them digitally. This is what youʼll upload to the Court to start
             your case.{' '}
             <a href={howToMergePDFs} target="_blank" rel="noopener noreferrer">
@@ -108,7 +138,7 @@ export const BeforeStartingCase = () => (
           <h3>Donʼt Submit Extra Documents With Your Petition</h3>
           <p>
             <strong>Do not</strong> include any additional documents with your
-            Petition, except for the IRS Notice. Documents that might be
+            Petition, except for the IRS notice. Documents that might be
             evidence can be submitted at a later time.
           </p>
         </div>
