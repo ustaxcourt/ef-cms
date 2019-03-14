016ac480297fc43c88f6d27695ccbb889f696ae6
Cody Seibert <cseibert113@gmail.com>
2019-03-14 16:36:51 -0400

    testing the createWorkItemInteractor

--- shared/src/test/mockDocuments.js
+++ shared/src/test/mockDocuments.js
@@ -10,7 +10,7 @@ exports.MOCK_DOCUMENTS = [
   },
   {
     createdAt: '2018-11-21T20:49:28.192Z',
-    documentId: 'c6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
+    documentId: 'e6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
     documentType: 'Answer',
     reviewDate: '2018-11-21T20:49:28.192Z',
     reviewUser: 'petitionsclerk',
