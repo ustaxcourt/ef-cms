584ce6f9bc7ecba1ae0ba4de173e9263952f2db4
Cody Seibert <cseibert113@gmail.com>
2019-03-14 00:26:13 -0400

    working on interactor

--- web-client/src/presenter/actions/createWorkItemAction.js
+++ web-client/src/presenter/actions/createWorkItemAction.js
@@ -1,9 +1,26 @@
 import { state } from 'cerebral';
 
-export const createWorkItemAction = async ({ get }) => {
-  // TODO: invoke the createWorkItem interactor / proxy
-  const form = get(state.form);
-  return {
-    form,
-  };
+export const createWorkItemAction = async ({
+  applicationContext,
+  get,
+  path,
+}) => {
+  const { assigneeId, message } = get(state.form);
+  const { caseId } = get(state.caseDetail);
+  const documentId = get(state.documentId);
+
+  await applicationContext.getUseCases().createWorkItem({
+    applicationContext,
+    assigneeId,
+    caseId,
+    documentId,
+    message,
+  });
+
+  return path.success({
+    alertSuccess: {
+      message: 'The message thread was created.',
+      title: 'Your message thread was created successfully.',
+    },
+  });
 };
