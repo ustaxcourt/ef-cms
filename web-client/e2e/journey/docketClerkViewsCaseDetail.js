ddbfd54156fdb241eb386872fe8db6055207fe79
Kris Koskelin <kkoskelin@flexion.us>
2019-03-15 11:58:46 -0500

    962: sort messages on case detail and document detail views

--- web-client/e2e/journey/docketClerkViewsCaseDetail.js
+++ web-client/e2e/journey/docketClerkViewsCaseDetail.js
@@ -6,21 +6,22 @@ export default test => {
     await test.runSequence('gotoCaseDetailSequence', {
       docketNumber: test.docketNumber,
     });
-    expect(
-      runCompute(extractedPendingMessagesFromCaseDetail, {
-        state: test.getState(),
-      }).map(message => ({
-        assigneeId: message.assigneeId,
-        from: message.currentMessage.from,
-        fromUserId: message.currentMessage.fromUserId,
-        message: message.currentMessage.message,
-      })),
-    ).toMatchObject([
+    let result = runCompute(extractedPendingMessagesFromCaseDetail, {
+      state: test.getState(),
+    });
+    result = result.map(message => ({
+      assigneeId: message.assigneeId,
+      createdAt: message.currentMessage.createdAt,
+      from: message.currentMessage.from,
+      fromUserId: message.currentMessage.fromUserId,
+      message: message.currentMessage.message,
+    }));
+    expect(result).toMatchObject([
       {
-        assigneeId: '63784910-c1af-4476-8988-a02f92da8e09',
-        from: 'Test Petitionsclerk1',
-        fromUserId: '4805d1ab-18d0-43ec-bafb-654e83405416',
-        message: 'Petition batched for IRS',
+        assigneeId: null,
+        from: 'Test Respondent',
+        fromUserId: '5805d1ab-18d0-43ec-bafb-654e83405416',
+        message: 'Stipulated Decision filed by Respondent is ready for review.',
       },
       {
         assigneeId: null,
@@ -29,10 +30,10 @@ export default test => {
         message: 'Answer filed by Respondent is ready for review.',
       },
       {
-        assigneeId: null,
-        from: 'Test Respondent',
-        fromUserId: '5805d1ab-18d0-43ec-bafb-654e83405416',
-        message: 'Stipulated Decision filed by Respondent is ready for review.',
+        assigneeId: '63784910-c1af-4476-8988-a02f92da8e09',
+        from: 'Test Petitionsclerk1',
+        fromUserId: '4805d1ab-18d0-43ec-bafb-654e83405416',
+        message: 'Petition batched for IRS',
       },
     ]);
   });
