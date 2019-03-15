26b3f3036850acc111d706ebabaf5b1c96fdead2
William Bagayoko <wbagayoko@gmail.com>
2019-03-15 09:37:04 -0500

    #962 Clear Users on Cancel and Save

--- web-client/src/presenter/sequences/dismissCreateMessageModalSequence.js
+++ web-client/src/presenter/sequences/dismissCreateMessageModalSequence.js
@@ -2,10 +2,12 @@ import { clearModalAction } from '../actions/clearModalAction';
 import { stopShowValidationAction } from '../actions/stopShowValidationAction';
 import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { clearFormAction } from '../actions/clearFormAction';
+import { clearUsersAction } from '../actions/clearUsersAction';
 
 export const dismissCreateMessageModalSequence = [
   stopShowValidationAction,
   clearFormAction,
+  clearUsersAction,
   clearAlertsAction,
   clearModalAction,
 ];
