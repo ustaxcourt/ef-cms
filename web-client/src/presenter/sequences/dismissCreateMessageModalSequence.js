00a26f0bf8196209985beb493b6c5a7a407036fd
William Bagayoko <wbagayoko@gmail.com>
2019-03-15 09:22:28 -0500

    962 Clear Form and Validation on Cancel

--- web-client/src/presenter/sequences/dismissCreateMessageModalSequence.js
+++ web-client/src/presenter/sequences/dismissCreateMessageModalSequence.js
@@ -1,7 +1,11 @@
 import { clearModalAction } from '../actions/clearModalAction';
 import { stopShowValidationAction } from '../actions/stopShowValidationAction';
+import { clearAlertsAction } from '../actions/clearAlertsAction';
+import { clearFormAction } from '../actions/clearFormAction';
 
 export const dismissCreateMessageModalSequence = [
   stopShowValidationAction,
+  clearFormAction,
+  clearAlertsAction,
   clearModalAction,
 ];
