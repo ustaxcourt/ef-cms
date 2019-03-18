e8cdc010cfa98ee1ec6af7e61980b8dc77611e9d
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-03-18 08:41:44 -0700

    jsdoc update

--- web-client/src/presenter/actions/runBatchProcessAction.js
+++ web-client/src/presenter/actions/runBatchProcessAction.js
@@ -1,11 +1,8 @@
 /**
- * takes the state.caseDetail and updates it via the updateCase use case.
+ * runs the Send to IRS batch process
  *
  * @param {Object} providers the providers object
  * @param {Object} providers.applicationContext the application context needed for getting the updateCase use case
- * @param {Object} providers.get the cerebral store used for getting state.caseDetail
- * @param {Object} providers.props the cerebral store used for getting props.combinedCaseDetailWithForm
- * @returns {Object} the alertSuccess and the caseDetail
  */
 export const runBatchProcessAction = async ({ applicationContext }) => {
   await applicationContext.getUseCases().runBatchProcess({
