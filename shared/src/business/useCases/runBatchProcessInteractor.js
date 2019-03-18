c0475c51bdb49ecd5040ef2393d588880a594d65
Cody Seibert <cseibert113@gmail.com>
2019-03-18 09:45:04 -0400

    putting the s3 logic into a persistence method

--- shared/src/business/useCases/runBatchProcessInteractor.js
+++ shared/src/business/useCases/runBatchProcessInteractor.js
@@ -19,38 +19,24 @@ exports.runBatchProcess = async ({ caseId, applicationContext }) => {
       caseId,
     });
 
-  //get document ids
-  const ids = caseToBatch.documents.map(document => document.documentId);
-  const names = caseToBatch.documents.map(
+  const s3Ids = caseToBatch.documents.map(document => document.documentId);
+  const fileNames = caseToBatch.documents.map(
     document => `${document.documentType}.pdf`,
   );
+  const zipName = `${
+    caseToBatch.docketNumber
+  }_${caseToBatch.contactPrimary.name.replace(/\s/g, '_')}.zip`;
 
-  const region = applicationContext.environment.region;
-  const bucket = applicationContext.environment.documentsBucketName;
-
-  const s3Client = new aws.S3({
-    endpoint: applicationContext.environment.s3Endpoint,
-    s3ForcePathStyle: 'true',
-    signatureVersion: 'v4',
+  await applicationContext.getPersistenceGateway().zipS3Documents({
+    applicationContext,
+    fileNames,
+    s3Ids,
+    zipName,
   });
 
-  function uploadFromStream(s3Client) {
-    const pass = new stream.PassThrough();
-
-    const params = {
-      Body: pass,
-      Bucket: bucket,
-      Key: `${
-        caseToBatch.docketNumber
-      }_${caseToBatch.contactPrimary.name.replace(/\s/g, '_')}.zip`,
-    };
-    s3Client.upload(params, function() {});
-
-    return pass;
-  }
-
-  s3Zip
-    .setArchiverOptions({ gzip: false })
-    .archive({ bucket: bucket, region: region, s3: s3Client }, '', ids, names)
-    .pipe(uploadFromStream(s3Client));
+  return {
+    fileNames,
+    s3Ids,
+    zipName,
+  };
 };
