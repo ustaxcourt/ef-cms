5f18b4e7510ea93c76e8c8883e424a6b5ccc121b
Cody Seibert <cseibert113@gmail.com>
2019-03-19 15:16:35 -0400

    updating the zap report

--- docker-zap.sh
+++ docker-zap.sh
@@ -4,6 +4,6 @@ set +e
 docker run --name "${CONTAINER_NAME}" -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t https://efcms-dev.ustc-case-mgmt.flexion.us/v1/swagger.json -f openapi -d -g gen.conf -r zap-report.html -z "-configfile /zap/wrk/options.prop"
 CODE="$?"
 set -e
-docker cp "${CONTAINER_NAME}:/home/app/zap-report.html" docs/zap-report.html
+docker cp "${CONTAINER_NAME}:/zap/wrk/zap-report.html" docs/zap-report.html
 docker rm "${CONTAINER_NAME}"
 exit "${CODE}"
