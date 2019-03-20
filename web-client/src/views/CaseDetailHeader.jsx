492b28ca47ac6fec46f9b8c0090ac835b3b17443
William Bagayoko <wbagayoko@gmail.com>
2019-03-20 11:46:18 -0500

    367 Case Detail Header
    * dry up case detail header
    * clean up to save space
    * Decrease Docket Number to H2
    * Move "New" label to the right of the docket number

---
+++ web-client/src/views/CaseDetailHeader.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { sequences, state } from 'cerebral';
+import { connect } from '@cerebral/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { UpdateCaseCaptionModalDialog } from './CaseDetailEdit/UpdateCaseCaptionModalDialog';
+
+export const CaseDetailHeader = connect(
+  {
+    caseDetail: state.formattedCaseDetail,
+    caseHelper: state.caseDetailHelper,
+    openCaseCaptionModalSequence: sequences.openCaseCaptionModalSequence,
+    showModal: state.showModal,
+  },
+  ({ caseDetail, caseHelper, showModal, openCaseCaptionModalSequence }) => {
+    return (
+      <React.Fragment>
+        <h2 className="captioned" tabIndex="-1">
+          <a href={'/case-detail/' + caseDetail.docketNumber}>
+            Docket Number: {caseDetail.docketNumberWithSuffix}
+          </a>
+          <span
+            className="usa-label case-status-label"
+            aria-label={`status: ${caseDetail.status}`}
+          >
+            <span aria-hidden="true">{caseDetail.status}</span>
+          </span>
+        </h2>
+        <p className="float-left">{caseDetail.caseTitle} </p>
+        {caseHelper.showCaptionEditButton && (
+          <p className="float-left">
+            <button
+              className="link"
+              id="caption-edit-button"
+              onClick={() => {
+                openCaseCaptionModalSequence();
+              }}
+            >
+              <FontAwesomeIcon icon="edit" size="sm" /> Edit
+            </button>
+          </p>
+        )}
+        {showModal == 'UpdateCaseCaptionModalDialog' && (
+          <UpdateCaseCaptionModalDialog />
+        )}
+        <div className="clear-both" />
+      </React.Fragment>
+    );
+  },
+);
