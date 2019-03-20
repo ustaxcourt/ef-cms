557c4230c3dbe43a4b5776738069dff917b65bc5
Eric Sorenson <eric@sorenson.software>
2019-03-20 15:59:14 -0500

    add a spinner after the recall

--- web-client/src/presenter/sequences/submitRecallPetitionFromIRSHoldingQueueSequence.js
+++ web-client/src/presenter/sequences/submitRecallPetitionFromIRSHoldingQueueSequence.js
@@ -1,17 +1,15 @@
 import { clearAlertsAction } from '../actions/clearAlertsAction';
+import { clearModalAction } from '../actions/clearModalAction';
 import { navigateToDashboardAction } from '../actions/navigateToDashboardAction';
 import { recallPetitionFromIRSHoldingQueueAction } from '../actions/recallPetitionFromIRSHoldingQueueAction';
 import { setAlertSuccessAction } from '../actions/setAlertSuccessAction';
-import { clearModalAction } from '../actions/clearModalAction';
-import { getCaseAction } from '../actions/getCaseAction';
-import { setCaseAction } from '../actions/setCaseAction';
+import { setCurrentPageAction } from '../actions/setCurrentPageAction';
 
 export const submitRecallPetitionFromIRSHoldingQueueSequence = [
   clearAlertsAction,
   clearModalAction,
+  setCurrentPageAction('Interstitial'),
   recallPetitionFromIRSHoldingQueueAction,
-  getCaseAction,
-  setCaseAction,
   setAlertSuccessAction,
   navigateToDashboardAction,
 ];
