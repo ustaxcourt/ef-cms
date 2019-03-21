baac969b538c4236e47233318e687a171f8c2ad6
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-03-20 19:16:28 -0700

    367: validation for Send To message functionality

--- web-client/src/presenter/sequences/getUsersInSectionSendToSequence.js
+++ web-client/src/presenter/sequences/getUsersInSectionSendToSequence.js
@@ -1,10 +1,10 @@
 import { getUsersInSelectedSectionAction } from '../actions/getUsersInSelectedSectionAction';
 import { setUsersAction } from '../actions/setUsersAction';
 import { set } from 'cerebral/factories';
-import { props, state } from 'cerebral';
+import { state } from 'cerebral';
 
 export const getUsersInSectionSequence = [
-  set(state[props.form].assigneeId, ''),
+  set(state.form.assigneeId, ''),
   getUsersInSelectedSectionAction,
   setUsersAction,
 ];
