650bbd7b4db8ac7acf924afd74d756f292025d0b
William Bagayoko <wbagayoko@gmail.com>
2019-03-26 11:17:37 -0500

    311: more map value paths

--- web-client/src/presenter/computeds/mapValueHelper.js
+++ web-client/src/presenter/computeds/mapValueHelper.js
@@ -8,5 +8,22 @@ export const mapValueHelper = value => {
     object[key] = true;
   }
 
+  if (_.isBoolean(value) || _.isNumber(value)) {
+    const key = value.toString();
+    object[key] = true;
+  }
+
+  if (_.isArray(value)) {
+    for (let i = 0; i < value.length; i++) {
+      object[i.toString()] = mapValueHelper(value[i]);
+    }
+  }
+
+  if (_.isPlainObject(value)) {
+    _.map(value, (value, key) => {
+      object[key] = mapValueHelper(value);
+    });
+  }
+
   return object;
 };
