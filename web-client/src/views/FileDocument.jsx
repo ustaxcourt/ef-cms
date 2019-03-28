05807c140003b920b1417675931e82faa681844f
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-03-28 15:41:44 -0700

    269: make File Document its own page

--- web-client/src/views/FileDocument.jsx
+++ web-client/src/views/FileDocument.jsx
@@ -5,16 +5,11 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import { DocumentCategoryAccordion } from './DocumentCategoryAccordion';
+import { CaseDetailHeader } from './CaseDetailHeader';
+import { SuccessNotification } from './SuccessNotification';
+import { ErrorNotification } from './ErrorNotification';
 
 class FilePetitionComponent extends React.Component {
-  componentDidMount() {
-    this.focusMain();
-  }
-  focusMain(e) {
-    e && e.preventDefault();
-    document.querySelector('#file-a-document-header').focus();
-    return false;
-  }
   render() {
     const submitDocumentSequence = this.props.submitDocumentSequence;
     const toggleDocumentCategoryAccordionSequence = this.props
@@ -25,115 +20,130 @@ class FilePetitionComponent extends React.Component {
 
     return (
       <React.Fragment>
-        <h2 tabIndex="-1" id="file-a-document-header">
-          File a Document
-        </h2>
-        <h3>What Type of Document Are You Filing?</h3>
-        <p>
-          Choose the document category, then you’ll be able to select a document
-          type.
-        </p>
-        <div className="usa-accordion document-category">
-          <button
-            type="button"
-            className="usa-accordion-button document-category-accordion"
-            aria-expanded={!!form.showDocumentCategoryAccordion}
-            aria-controls="document-category-accordion-container"
-            onClick={() => toggleDocumentCategoryAccordionSequence()}
-          >
-            <span className="usa-banner-button-text">
-              <FontAwesomeIcon icon="question-circle" size="sm" />
-              Need help determining what document category to select?
-              {form.showDocumentCategoryAccordion ? (
-                <FontAwesomeIcon icon="caret-up" />
-              ) : (
-                <FontAwesomeIcon icon="caret-down" />
-              )}
-            </span>
-          </button>
-          <div
-            id="document-category-accordion-container"
-            className="usa-accordion-content"
-            aria-hidden={!form.showDocumentCategoryAccordion}
-          >
-            <DocumentCategoryAccordion />
-          </div>
+        <div className="usa-grid breadcrumb">
+          <FontAwesomeIcon icon="caret-left" />
+          <a href="/" id="queue-nav">
+            Back
+          </a>
         </div>
+        <section className="usa-section usa-grid FileDocument">
+          <CaseDetailHeader />
+          <hr aria-hidden="true" />
 
-        <div className="usa-grid-full">
-          <div className="usa-width-one-half">
-            <form
-              id="file-a-document"
-              aria-labelledby="file-a-document-header"
-              role="form"
-              noValidate
-              onSubmit={e => {
-                e.preventDefault();
-                submitDocumentSequence();
-              }}
+          <SuccessNotification />
+          <ErrorNotification />
+          <h2 tabIndex="-1" id="file-a-document-header">
+            File a Document
+          </h2>
+          <h3>What Type of Document Are You Filing?</h3>
+          <p>
+            Choose the document category, then you’ll be able to select a
+            document type.
+          </p>
+          <div className="usa-accordion document-category">
+            <button
+              type="button"
+              className="usa-accordion-button document-category-accordion"
+              aria-expanded={!!form.showDocumentCategoryAccordion}
+              aria-controls="document-category-accordion-container"
+              onClick={() => toggleDocumentCategoryAccordionSequence()}
             >
-              <div className="blue-container">
-                <div className="ustc-form-group">
-                  <label htmlFor="document-type">Document Category</label>
-                  <select
-                    name="documentType"
-                    id="document-type"
-                    onChange={e => {
-                      updateDocumentValueSequence({
-                        key: e.target.name,
-                        value: e.target.value,
-                      });
-                    }}
-                  >
-                    <option value="">- Select -</option>
-                    {constants.CATEGORIES.map(category => {
-                      return (
-                        <option key={category} value={category}>
-                          {category}
-                        </option>
-                      );
-                    })}
-                  </select>
-                </div>
-                <div className="ustc-form-group">
-                  <button
-                    type="button"
-                    className="usa-button"
-                    onClick={() =>
-                      toggleDocumentCategoryAccordionSequence({ value: false })
-                    }
-                  >
-                    Next, Choose Document Type
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
-          <div className="usa-width-one-third push-right">
-            <div className="blue-container gray-background">
-              <h3>Frequently Used Documents</h3>
-              <ul className="ustc-unstyled-list">
-                <li>
-                  <a href="#a">Motion for Judgment on The Pleadings</a>
-                </li>
-                <li>
-                  <a href="#a">Application for Waiver of Filing Fee</a>
-                </li>
-                <li>
-                  <a href="#a">Motion for a New Trial</a>
-                </li>
-                <li>
-                  <a href="#a">
-                    Motion for Protective Order Persuant to Rule 103
-                  </a>
-                </li>
-                <li>
-                  <a href="#a">Motion for Continuance</a>
-                </li>
-              </ul>
+              <span className="usa-banner-button-text">
+                <FontAwesomeIcon icon="question-circle" size="sm" />
+                Need help determining what document category to select?
+                {form.showDocumentCategoryAccordion ? (
+                  <FontAwesomeIcon icon="caret-up" />
+                ) : (
+                  <FontAwesomeIcon icon="caret-down" />
+                )}
+              </span>
+            </button>
+            <div
+              id="document-category-accordion-container"
+              className="usa-accordion-content"
+              aria-hidden={!form.showDocumentCategoryAccordion}
+            >
+              <DocumentCategoryAccordion />
             </div>
           </div>
-        </div>
+
+          <div className="usa-grid-full">
+            <div className="usa-width-one-half">
+              <form
+                id="file-a-document"
+                aria-labelledby="file-a-document-header"
+                role="form"
+                noValidate
+                onSubmit={e => {
+                  e.preventDefault();
+                  submitDocumentSequence();
+                }}
+              >
+                <div className="blue-container">
+                  <div className="ustc-form-group">
+                    <label htmlFor="document-type">Document Category</label>
+                    <select
+                      name="documentType"
+                      id="document-type"
+                      onChange={e => {
+                        updateDocumentValueSequence({
+                          key: e.target.name,
+                          value: e.target.value,
+                        });
+                      }}
+                    >
+                      <option value="">- Select -</option>
+                      {constants.CATEGORIES.map(category => {
+                        return (
+                          <option key={category} value={category}>
+                            {category}
+                          </option>
+                        );
+                      })}
+                    </select>
+                  </div>
+                  <div className="ustc-form-group">
+                    <button
+                      type="button"
+                      className="usa-button"
+                      onClick={() =>
+                        toggleDocumentCategoryAccordionSequence({
+                          value: false,
+                        })
+                      }
+                    >
+                      Next, Choose Document Type
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div className="usa-width-one-third push-right">
+              <div className="blue-container gray-background">
+                <h3>Frequently Used Documents</h3>
+                <ul className="ustc-unstyled-list">
+                  <li>
+                    <a href="#a">Motion for Judgment on The Pleadings</a>
+                  </li>
+                  <li>
+                    <a href="#a">Application for Waiver of Filing Fee</a>
+                  </li>
+                  <li>
+                    <a href="#a">Motion for a New Trial</a>
+                  </li>
+                  <li>
+                    <a href="#a">
+                      Motion for Protective Order Persuant to Rule 103
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#a">Motion for Continuance</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
       </React.Fragment>
     );
   }
