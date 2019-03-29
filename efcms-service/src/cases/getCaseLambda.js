d2be1d6e77b9a2c3b66ed35de0dcc0700a9ed0ce
Eric Sorenson <eric@sorenson.software>
2019-03-29 14:58:40 -0500

    added async await

--- efcms-service/src/cases/getCaseLambda.js
+++ efcms-service/src/cases/getCaseLambda.js
@@ -9,19 +9,19 @@ const createApplicationContext = require('../applicationContext');
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
 exports.handler = event =>
-  handle(event, () => {
+  handle(event, async () => {
     try {
       const user = getUserFromAuthHeader(event);
       const applicationContext = createApplicationContext(user);
-      const caseDetail = applicationContext.getUseCases().getCase({
+      const caseDetail = await applicationContext.getUseCases().getCase({
         applicationContext,
         caseId: event.pathParameters.caseId,
       });
-      console.log('user =', JSON.stringify(user));
-      console.info('case =', JSON.stringify(caseDetail));
+      console.log('user', JSON.stringify(user));
+      console.log('case', JSON.stringify(caseDetail));
       return caseDetail;
     } catch (e) {
-      console.error('error =', JSON.stringify(e));
+      console.error('error', JSON.stringify(e));
       throw e;
     }
   });
