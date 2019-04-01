e531f7dbdb42d5f1d1a82a3d8b4028b7e4323d0f
Cody Seibert <cseibert113@gmail.com>
2019-04-01 10:25:51 -0400

    sorting the docket record to make petition first, trial location second, and ods third

--- web-client/src/presenter/computeds/formattedCaseDetail.test.js
+++ web-client/src/presenter/computeds/formattedCaseDetail.test.js
@@ -235,6 +235,90 @@ describe('formatYearAmounts', () => {
     );
   });
 
+  it('sorts the docet record in the expected order', async () => {
+    const caseDetail = {
+      caseCaption: 'Brett Osborne, Petitioner',
+      docketRecord: [
+        {
+          description: 'Petition',
+          documentId: 'Petition',
+          filedBy: 'Jessica Frase Marine',
+          filingDate: '2019-10-28T21:14:39.488Z',
+        },
+        {
+          description: 'Request for Place of Trial',
+          documentId: null,
+          filedBy: 'Jessica Frase Marine',
+          filingDate: '2019-01-28T21:14:39.488Z',
+        },
+        {
+          description: 'Ownership Disclosure Statement',
+          documentId: 'Ownership Disclosure Statement',
+          filedBy: 'Jessica Frase Marine',
+          filingDate: '2019-03-28T21:14:39.488Z',
+        },
+        {
+          description: 'Other',
+          documentId: 'Other',
+          filedBy: 'Jessica Frase Marine',
+          filingDate: '2019-01-01T21:14:39.488Z',
+        },
+      ],
+      documents: [
+        {
+          createdAt: '2019-02-28T21:14:39.488Z',
+          documentId: 'Petition',
+          documentType: 'Petition',
+          showValidationInput: '2019-02-28T21:14:39.488Z',
+          status: 'served',
+        },
+        {
+          createdAt: '2019-03-28T21:14:39.488Z',
+          documentId: 'Ownership Disclosure Statement',
+          documentType: 'Ownership Disclosure Statement',
+          showValidationInput: '2019-03-28T21:14:39.488Z',
+          status: 'served',
+        },
+        {
+          createdAt: '2019-01-01T21:14:39.488Z',
+          documentId: 'Other',
+          documentType: 'Other',
+          showValidationInput: '2019-01-01T21:14:39.488Z',
+          status: 'served',
+        },
+      ],
+      hasIrsNotice: false,
+      hasVerifiedIrsNotice: false,
+      petitioners: [{ name: 'bob' }],
+    };
+    const result = await runCompute(formattedCaseDetail, {
+      state: {
+        caseDetail,
+        caseDetailErrors: {},
+      },
+    });
+    expect(result.docketRecordWithDocument[0]).toMatchObject({
+      document: {
+        documentType: 'Petition',
+      },
+    });
+    expect(result.docketRecordWithDocument[1]).toMatchObject({
+      record: {
+        description: 'Request for Place of Trial',
+      },
+    });
+    expect(result.docketRecordWithDocument[2]).toMatchObject({
+      document: {
+        documentType: 'Ownership Disclosure Statement',
+      },
+    });
+    expect(result.docketRecordWithDocument[3]).toMatchObject({
+      document: {
+        documentType: 'Other',
+      },
+    });
+  });
+
   describe('case name mapping', () => {
     it("should remove ', Petitioner' from caseCaption", async () => {
       const caseDetail = {
