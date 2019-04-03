595d3c6ca919886f4b5504589188724d655d4303
Cody Seibert <cseibert113@gmail.com>
2019-04-03 09:53:41 -0400

    consolidating our build to use a single dockerfile to hopefully speed up the build and reduce complexity

--- efcms-service/docker-init.sh
+++ efcms-service/docker-init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
-docker build -t efcms-build -f ../Dockerfile.build ..
+docker build -t efcms-build -f ../Dockerfile ..
 docker run --rm efcms-build /bin/sh -c 'echo "init completed"'
--- web-client/docker-build.sh
+++ web-client/docker-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 ENV=$1
-docker build -t web-client-build -f ../Dockerfile.web-client ..
+docker build -t web-client-build -f ../Dockerfile ..
 docker run -e "EFCMS_DOMAIN=${EFCMS_DOMAIN}" -e "COGNITO_SUFFIX=${COGNITO_SUFFIX}" -e "API_URL=${API_URL}" --name "${CONTAINER_NAME}" web-client-build /bin/sh -c "cd web-client && ./build-dist.sh $ENV"
 CODE="$?"
 docker cp "${CONTAINER_NAME}:/home/app/web-client/dist" dist
