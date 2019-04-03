a9f7fa45a7856ab3c9d4c28201a700c81623d58a
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-04-03 09:18:46 -0700

    269: validation

--- web-client/src/presenter/sequences/selectDocumentSequence.js
+++ web-client/src/presenter/sequences/selectDocumentSequence.js
@@ -4,9 +4,13 @@ import { set } from 'cerebral/factories';
 import { state } from 'cerebral';
 
 export const selectDocumentSequence = [
+  set(state.showValidation, true),
   validateSelectDocumentTypeAction,
   {
     error: [setValidationErrorsAction],
-    success: [set(state.form.isDocumentTypeSelected, true)],
+    success: [
+      set(state.showValidation, false),
+      set(state.form.isDocumentTypeSelected, true),
+    ],
   },
 ];
