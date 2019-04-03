7d89cd165cb5968704680ba269e0ee1f74786e73
Cody Seibert <cseibert113@gmail.com>
2019-04-03 09:53:41 -0400

    consolidating our build to use a single dockerfile to hopefully speed up the build and reduce complexity

--- efcms-service/docker-cognito-customize.sh
+++ efcms-service/docker-cognito-customize.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 ENV=$1
-docker build -t efcms-build -f ../Dockerfile.build ..
+docker build -t efcms-build -f ../Dockerfile ..
 docker run --rm efcms-build /bin/sh -c "cd efcms-service && ./setup-cognito-ui.sh $ENV"
