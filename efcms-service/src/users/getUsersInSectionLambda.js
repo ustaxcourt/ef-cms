7d5f56d921d39e3e45c7067b503c46bf6d5f16f7
Eric Sorenson <eric@sorenson.software>
2019-04-04 14:35:28 -0500

    auto sort service requires

--- efcms-service/src/users/getUsersInSectionLambda.js
+++ efcms-service/src/users/getUsersInSectionLambda.js
@@ -1,8 +1,8 @@
+const createApplicationContext = require('../applicationContext');
 const {
   handle,
   getUserFromAuthHeader,
 } = require('../middleware/apiGatewayHelper');
-const createApplicationContext = require('../applicationContext');
 
 /**
  * creates a new document and attaches it to a case.  It also creates a work item on the docket section.
