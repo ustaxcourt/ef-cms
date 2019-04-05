591010c3c87dcec172eafcff1ba9a16fa2b4eb6e
William Bagayoko <wbagayoko@gmail.com>
2019-04-05 09:17:47 -0500

    980 Use case spec / catch for no scenario

--- shared/src/business/entities/externalDocument/ExternalDocumentFactory.js
+++ shared/src/business/entities/externalDocument/ExternalDocumentFactory.js
@@ -35,25 +35,28 @@ function ExternalDocumentFactory() {}
  * @param documentMetadata
  */
 ExternalDocumentFactory.get = documentMetadata => {
-  const scenario = documentMetadata.scenario.toLowerCase().trim();
-  switch (scenario) {
-    case 'nonstandard a':
-      return new ExternalDocumentNonStandardA(documentMetadata);
-    case 'nonstandard b':
-      return new ExternalDocumentNonStandardB(documentMetadata);
-    case 'nonstandard c':
-      return new ExternalDocumentNonStandardC(documentMetadata);
-    case 'nonstandard d':
-      return new ExternalDocumentNonStandardD(documentMetadata);
-    case 'nonstandard e':
-      return new ExternalDocumentNonStandardE(documentMetadata);
-    case 'nonstandard f':
-      return new ExternalDocumentNonStandardF(documentMetadata);
-    case 'nonstandard g':
-      return new ExternalDocumentNonStandardG(documentMetadata);
-    case 'nonstandard h':
-      return new ExternalDocumentNonStandardH(documentMetadata);
+  if (documentMetadata && documentMetadata.scenario) {
+    const scenario = documentMetadata.scenario.toLowerCase().trim();
+    switch (scenario) {
+      case 'nonstandard a':
+        return new ExternalDocumentNonStandardA(documentMetadata);
+      case 'nonstandard b':
+        return new ExternalDocumentNonStandardB(documentMetadata);
+      case 'nonstandard c':
+        return new ExternalDocumentNonStandardC(documentMetadata);
+      case 'nonstandard d':
+        return new ExternalDocumentNonStandardD(documentMetadata);
+      case 'nonstandard e':
+        return new ExternalDocumentNonStandardE(documentMetadata);
+      case 'nonstandard f':
+        return new ExternalDocumentNonStandardF(documentMetadata);
+      case 'nonstandard g':
+        return new ExternalDocumentNonStandardG(documentMetadata);
+      case 'nonstandard h':
+        return new ExternalDocumentNonStandardH(documentMetadata);
+    }
   }
+
   // standard - default
   return new ExternalDocumentStandard(documentMetadata);
 };
