d0dbec5b571065289ff63aec6b49edf66fc71e75
William Bagayoko <wbagayoko@gmail.com>
2019-04-05 11:14:20 -0500

    980 Non Standard H Secondary Document

--- shared/src/business/entities/externalDocument/ExternalDocumentNonStandardH.js
+++ shared/src/business/entities/externalDocument/ExternalDocumentNonStandardH.js
@@ -2,34 +2,36 @@ const joi = require('joi-browser');
 const {
   joiValidationDecorator,
 } = require('../../../utilities/JoiValidationDecorator');
-
 /**
  *
  * @param rawProps
+ * @param ExternalDocumentFactory
  * @constructor
  */
-function ExternalDocumentNonStandardH(rawProps) {
+function ExternalDocumentNonStandardH(rawProps, ExternalDocumentFactory) {
   Object.assign(this, rawProps);
+  const { secondaryDocument } = rawProps;
+  this.secondaryDocument = ExternalDocumentFactory.get(secondaryDocument || {});
 }
 
 ExternalDocumentNonStandardH.errorToMessageMap = {
   category: 'You must select a category.',
   documentType: 'You must select a document type.',
-  secondaryCategory: 'You must select a category.',
-  secondaryDocumentType: 'You must select a document type.',
+  secondaryDocument: 'You must select a document.',
 };
 
 ExternalDocumentNonStandardH.schema = joi.object().keys({
   category: joi.string().required(),
   documentType: joi.string().required(),
-  secondaryCategory: joi.string().required(),
-  secondaryDocumentType: joi.string().required(),
+  secondaryDocument: joi.object().required(),
 });
 
 joiValidationDecorator(
   ExternalDocumentNonStandardH,
   ExternalDocumentNonStandardH.schema,
-  undefined,
+  function() {
+    return !this.getFormattedValidationErrors();
+  },
   ExternalDocumentNonStandardH.errorToMessageMap,
 );
 
