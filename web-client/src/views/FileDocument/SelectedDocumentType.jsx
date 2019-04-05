fb2c3d93d318ebb9757627f1a1f1eed75545212e
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-04-05 13:50:44 -0700

    a little view refactor

--- web-client/src/views/FileDocument/SelectedDocumentType.jsx
+++ web-client/src/views/FileDocument/SelectedDocumentType.jsx
@@ -43,7 +43,7 @@ export const SelectedDocumentType = connect(
             </div>
           </div>
           {fileDocumentHelper.primary.showNonstandardForm && (
-            <NonstandardForm />
+            <NonstandardForm level="primary" />
           )}
         </div>
 
