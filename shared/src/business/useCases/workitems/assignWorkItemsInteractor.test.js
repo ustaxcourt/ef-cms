e2208dc0f08ec8190712a55bd109c27b6cd098be
Cody Seibert <cseibert113@gmail.com>
2019-04-08 17:37:52 -0400

    more tests and refactoring

--- shared/src/business/useCases/workitems/assignWorkItemsInteractor.test.js
+++ shared/src/business/useCases/workitems/assignWorkItemsInteractor.test.js
@@ -79,30 +79,4 @@ describe('assignWorkItems', () => {
     }
     expect(error).toBeDefined();
   });
-
-  it('be successful when all validation passed', async () => {
-    const applicationContext = {
-      environment: { stage: 'local' },
-      getCurrentUser: () => {
-        return new User({
-          name: 'Test Docketclerk',
-          role: 'docketclerk',
-          userId: '6805d1ab-18d0-43ec-bafb-654e83405416',
-        });
-      },
-      getPersistenceGateway: () => {
-        return {
-          getWorkItemById: async () => MOCK_WORK_ITEM,
-          saveWorkItem: async () => ({
-            abc: 123,
-          }),
-        };
-      },
-    };
-    await assignWorkItems({
-      applicationContext,
-      userId: '6805d1ab-18d0-43ec-bafb-654e83405416',
-      workItems: [MOCK_WORK_ITEM],
-    });
-  });
 });
