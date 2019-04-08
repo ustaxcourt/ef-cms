86c215aed6a1111566d0049571cb02a71b53637e
Eric Sorenson <eric@sorenson.software>
2019-04-08 13:59:40 -0500

    more logging

--- efcms-service/src/workitems/getWorkItemsBySectionLambda.js
+++ efcms-service/src/workitems/getWorkItemsBySectionLambda.js
@@ -13,8 +13,16 @@ exports.handler = event =>
     const section = event.pathParameters.section;
     const user = getUserFromAuthHeader(event);
     const applicationContext = createApplicationContext(user);
-    return applicationContext.getUseCases().getWorkItemsBySection({
-      applicationContext,
-      section,
-    });
+    try {
+      const results = applicationContext.getUseCases().getWorkItemsBySection({
+        applicationContext,
+        section,
+      });
+      applicationContext.logger.info('User', user);
+      applicationContext.logger.info('Results', results);
+      return results;
+    } catch (e) {
+      applicationContext.logger.error(e);
+      throw e;
+    }
   });
