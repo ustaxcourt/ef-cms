4dcfa3c6915ed69606f958d4f676ed615e6c1822
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-04-09 14:07:18 -0700

    test for future date

--- shared/src/business/entities/externalDocument/ExternalDocumentNonStandardD.test.js
+++ shared/src/business/entities/externalDocument/ExternalDocumentNonStandardD.test.js
@@ -14,6 +14,22 @@ describe('ExternalDocumentNonStandardD', () => {
       });
     });
 
+    it('should have error message for future date', () => {
+      const serviceDate = '2050-12-12';
+      const extDoc = ExternalDocumentFactory.get({
+        category: 'Supporting Document',
+        documentTitle: 'Certificate of Service [Document Name] [Date]',
+        documentType: 'Certificate of Service',
+        previousDocument: 'Petition',
+        scenario: 'Nonstandard D',
+        serviceDate,
+      });
+      expect(extDoc.getFormattedValidationErrors()).toEqual({
+        serviceDate:
+          'Service date is in the future. Please enter a valid date.',
+      });
+    });
+
     it('should be valid when all fields are present', () => {
       const serviceDate = new Date().toISOString();
       const extDoc = ExternalDocumentFactory.get({
