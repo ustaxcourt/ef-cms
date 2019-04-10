4a8e80ca4aab010837cfad446ed7073803808788
William Bagayoko <wbagayoko@gmail.com>
2019-04-10 15:33:39 -0500

    980 validateExternalDocumentInformation use case

---
+++ shared/src/business/useCases/externalDocument/validateExternalDocumentInformationInteractor.test.js
@@ -0,0 +1,31 @@
+const sinon = require('sinon');
+const {
+  validateExternalDocumentInformation,
+} = require('./validateExternalDocumentInformationInteractor');
+
+describe('validateExternalDocument', () => {
+  let ExternalDocumentInformationFactory;
+  let spy;
+
+  beforeEach(() => {
+    spy = sinon.stub().returns(null);
+    ExternalDocumentInformationFactory = {
+      get: () => ({
+        getFormattedValidationErrors: spy,
+      }),
+    };
+  });
+
+  it('calls external document information factory to get validation errors', () => {
+    const errors = validateExternalDocumentInformation({
+      applicationContext: {
+        getEntityConstructors: () => ({
+          ExternalDocumentInformationFactory,
+        }),
+      },
+      documentMetadata: {},
+    });
+
+    expect(errors).toEqual(null);
+  });
+});
