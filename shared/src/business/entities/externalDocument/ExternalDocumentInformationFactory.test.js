37651052a4d4cac0109706a5e7265e87343f70bb
William Bagayoko <wbagayoko@gmail.com>
2019-04-11 10:46:59 -0500

    936 Empty Test Cases

--- shared/src/business/entities/externalDocument/ExternalDocumentInformationFactory.test.js
+++ shared/src/business/entities/externalDocument/ExternalDocumentInformationFactory.test.js
@@ -3,6 +3,37 @@ const {
 } = require('./ExternalDocumentInformationFactory');
 
 describe('ExternalDocumentInformationFactory', () => {
+  let baseDoc;
+
+  describe('Standard Document', () => {
+    beforeEach(() => {
+      baseDoc = {
+        category: 'Application',
+        documentTitle: 'Application for Waiver of Filing Fee',
+        documentType: 'Application for Waiver of Filing Fee',
+        scenario: 'Standard',
+      };
+    });
+
+    it('should require primary document file', () => {});
+
+    it('should require certificate of service radio be selected', () => {});
+
+    describe('Has Certificate of Service', () => {
+      beforeEach(() => {
+        baseDoc.certificateOfService = true;
+      });
+
+      it('should require certificate of service date be entered', () => {});
+
+      it('should require certificate of service date be in the past', () => {});
+    });
+
+    it('should require exhibits radio be selected', () => {});
+
+    it('should require attachments radio be selected', () => {});
+  });
+
   describe('validation', () => {
     it('should have error messages for basic object', () => {
       const extDocInfo = ExternalDocumentInformationFactory.get({});
