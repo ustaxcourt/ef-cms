728354b17b0f97a72e36522154304803370b2ec1
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-04-12 10:16:32 -0700

    set page level validation in submit sequence

--- web-client/src/presenter/sequences/submitExternalDocumentInformationSequence.js
+++ web-client/src/presenter/sequences/submitExternalDocumentInformationSequence.js
@@ -1,14 +1,21 @@
 import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { set } from 'cerebral/factories';
+import { setAlertErrorAction } from '../actions/setAlertErrorAction';
+import { setValidationAlertErrorsAction } from '../actions/setValidationAlertErrorsAction';
 import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
 import { state } from 'cerebral';
 import { validateExternalDocumentInformationAction } from '../actions/FileDocument/validateExternalDocumentInformationAction';
 
 export const submitExternalDocumentInformationSequence = [
+  clearAlertsAction,
   set(state.showValidation, true),
   validateExternalDocumentInformationAction,
   {
-    error: [setValidationErrorsAction],
+    error: [
+      setAlertErrorAction,
+      setValidationErrorsAction,
+      setValidationAlertErrorsAction,
+    ],
     success: [set(state.showValidation, false), clearAlertsAction],
   },
 ];
