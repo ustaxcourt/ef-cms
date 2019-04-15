330ff8f4d0363a1f0aacea8b480d7f249ad35b1b
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-04-15 08:32:37 -0400

    302: file document review view

--- web-client/src/presenter/presenter.js
+++ web-client/src/presenter/presenter.js
@@ -37,6 +37,7 @@ import { notFoundErrorSequence } from './sequences/notFoundErrorSequence';
 import { openCaseCaptionModalSequence } from './sequences/openCaseCaptionModalSequence';
 import { openCreateMessageModalSequence } from './sequences/openCreateMessageModalSequence';
 import { removeYearAmountSequence } from './sequences/removeYearAmountSequence';
+import { reviewExternalDocumentInformationSequence } from './sequences/reviewExternalDocumentInformationSequence';
 import { runBatchProcessSequence } from './sequences/runBatchProcessSequence';
 import { selectAssigneeSequence } from './sequences/selectAssigneeSequence';
 import { selectDocumentSequence } from './sequences/selectDocumentSequence';
@@ -142,6 +143,7 @@ export const presenter = {
     openCaseCaptionModalSequence,
     openCreateMessageModalSequence,
     removeYearAmountSequence,
+    reviewExternalDocumentInformationSequence,
     runBatchProcessSequence,
     selectAssigneeSequence,
     selectDocumentSequence,
