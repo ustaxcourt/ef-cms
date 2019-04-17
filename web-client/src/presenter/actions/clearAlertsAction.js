2b31f06ca12302805b7e48f9453563e9653e1bc0
Kris Koskelin <kkoskelin@flexion.us>
2019-04-17 11:38:44 -0500

    302: preserve alerts across navigation
    Provided new state variable to make this determination

--- web-client/src/presenter/actions/clearAlertsAction.js
+++ web-client/src/presenter/actions/clearAlertsAction.js
@@ -4,11 +4,17 @@ import { state } from 'cerebral';
  * Clears any and all alerts that might be enabled.
  *
  * @param {Object} providers the providers object
+ * @param {Object} providers.get the cerebral get function to retrieve state values
  * @param {Object} providers.store the cerebral store object used for clearing alertError, alertSuccess, caseDetailErrors
  */
-export const clearAlertsAction = ({ store }) => {
-  store.set(state.alertError, null);
-  store.set(state.alertSuccess, null);
+export const clearAlertsAction = ({ get, store }) => {
+  const saveAlerts = get(state.saveAlertsForNavigation);
+  if (!saveAlerts) {
+    store.set(state.alertError, null);
+    store.set(state.alertSuccess, null);
+  }
+
   store.set(state.caseDetailErrors, {});
+  store.set(state.saveAlertsForNavigation, false);
   store.set(state.validationErrors, {});
 };
