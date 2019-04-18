e12df593fc1e9326352b62d21d90c70138e1b4b8
Matt Hopson <matthopson@gmail.com>
2019-04-18 10:05:03 -0600

    Add shared/test-output and write pdf file  on test

--- shared/src/business/useCases/addCoverToPDFDocumentInteractor.test.js
+++ shared/src/business/useCases/addCoverToPDFDocumentInteractor.test.js
@@ -6,6 +6,7 @@ const {
 
 const { PDFDocumentFactory } = require('pdf-lib');
 const testAssetsPath = path.join(__dirname, '../../../test-assets/');
+const testOutputPath = path.join(__dirname, '../../../test-output/');
 
 function testPdfDocBytes() {
   return fs.readFileSync(testAssetsPath + 'sample.pdf');
@@ -42,5 +43,7 @@ describe('addCoverToPDFDocument', () => {
     const newPdfDoc = PDFDocumentFactory.load(newPdfData);
     const newPdfDocPages = newPdfDoc.getPages();
     expect(newPdfDocPages.length).toEqual(2);
+
+    fs.writeFile(testOutputPath + 'addCoverToPDFDocument.pdf', newPdfData);
   });
 });
