42f467186de1de1a6492c333f9e0dc5169acafa9
Cody Seibert <cseibert113@gmail.com>
2019-04-19 17:09:18 -0400

    fixing merge conflict

--- web-client/src/views/DocketRecord.jsx
+++ web-client/src/views/DocketRecord.jsx
@@ -60,129 +60,134 @@ export const DocketRecord = connect(
             <FontAwesomeIcon icon="cloud-upload-alt" /> File Document
           </a>
         )}
-        <table
-          className="responsive-table row-border-only"
-          id="docket-record"
-          aria-label="docket record"
-        >
-          <thead>
-            <tr>
-              <th className="center-column">No.</th>
-              <th>Date</th>
-              <th className="center-column">Event</th>
-              <th className="icon-column" />
-              <th>Filings and Proceedings</th>
-              <th>Filed By</th>
-              <th>Action</th>
-              <th>Served</th>
-              <th className="center-column">Parties</th>
-            </tr>
-          </thead>
-          <tbody>
-            {caseDetail.docketRecordWithDocument.map(
-              ({ record, document }, index) => (
-                <tr key={index}>
-                  <td className="responsive-title center-column">
-                    {index + 1}
-                    <span className="responsive-label push-right">
+        <div className="scrollable-table-container">
+          <table
+            className="responsive-table row-border-only"
+            id="docket-record"
+            aria-label="docket record"
+          >
+            <thead>
+              <tr>
+                <th className="center-column">No.</th>
+                <th>Date</th>
+                <th className="center-column">Event</th>
+                <th className="icon-column" />
+                <th>Filings and Proceedings</th>
+                <th>Filed By</th>
+                <th>Action</th>
+                <th>Served</th>
+                <th className="center-column">Parties</th>
+              </tr>
+            </thead>
+            <tbody>
+              {caseDetail.docketRecordWithDocument.map(
+                ({ record, document }, index) => (
+                  <tr key={index}>
+                    <td className="responsive-title center-column">
+                      {index + 1}
+                      <span className="responsive-label push-right">
+                        {record.createdAtFormatted}
+                      </span>
+                    </td>
+                    <td className="hide-on-mobile">
+                      <span className="responsive-label">Date</span>
                       {record.createdAtFormatted}
-                    </span>
-                  </td>
-                  <td className="hide-on-mobile">
-                    <span className="responsive-label">Date</span>
-                    {record.createdAtFormatted}
-                  </td>
-                  <td className="center-column">
-                    <span className="responsive-label">Event</span>
-                    {document && document.eventCode}
-                  </td>
-                  <td className="filing-type-icon hide-on-mobile">
-                    {document && document.isPaper && (
-                      <FontAwesomeIcon icon={['fas', 'file-alt']} />
-                    )}
-                    {document &&
-                      helper.showDirectDownloadLink &&
-                      document.processingStatus !== 'complete' && (
-                        <FontAwesomeIcon
-                          icon="spinner"
-                          className="fa-spin spinner"
-                        />
+                    </td>
+                    <td className="center-column">
+                      <span className="responsive-label">Event</span>
+                      {document && document.eventCode}
+                    </td>
+                    <td className="filing-type-icon hide-on-mobile">
+                      {document && document.isPaper && (
+                        <FontAwesomeIcon icon={['fas', 'file-alt']} />
                       )}
-                  </td>
-                  <td>
-                    <span className="responsive-label">
-                      Filings and Proceedings
-                    </span>
-                    {document &&
-                      helper.showDirectDownloadLink &&
-                      document.processingStatus === 'complete' &&
-                      renderDocumentLink(
-                        document.documentId,
-                        record.description,
-                        document.isPaper,
-                      )}
-                    {document &&
-                      helper.showDirectDownloadLink &&
-                      document.processingStatus !== 'complete' && (
-                        <React.Fragment>
-                          <span
-                            className="usa-label-uploading"
-                            aria-label="document uploading marker"
-                          >
-                            <span aria-hidden="true">Uploading</span>
-                          </span>
-                          {record.description}
-                        </React.Fragment>
-                      )}
-                    {document && helper.showDocumentDetailLink && (
-                      <a
-                        href={documentHelper({
-                          docketNumber: caseDetail.docketNumber,
-                          documentId: document.documentId,
-                        })}
-                        aria-label="View PDF"
-                      >
-                        {document && document.isPaper && (
-                          <span className="filing-type-icon-mobile">
-                            <FontAwesomeIcon icon={['fas', 'file-alt']} />
-                          </span>
+                      {document &&
+                        helper.showDirectDownloadLink &&
+                        document.processingStatus !== 'complete' && (
+                          <FontAwesomeIcon
+                            icon="spinner"
+                            className="fa-spin spinner"
+                          />
                         )}
-                        {record.description}
-                      </a>
-                    )}
-                    {!document &&
-                      record.documentId &&
-                      renderDocumentLink(record.documentId, record.description)}
-                    {!document && !record.documentId && record.description}
-                    {record.filingsAndProceedings &&
-                      ` ${record.filingsAndProceedings}`}
-                  </td>
-                  <td>
-                    <span className="responsive-label">Filed by</span>
-                    {document && document.filedBy}
-                  </td>
-                  <td>
-                    <span className="responsive-label">Action</span>
-                    {record.action}
-                  </td>
-                  <td>
-                    <span className="responsive-label">Served</span>
-                    {document && document.isStatusServed && (
-                      <span>{caseDetail.datePetitionSentToIrsMessage}</span>
-                    )}
-                    {document && helper.showDocumentStatus && (
-                      <span>{document.status}</span>
-                    )}
-                  </td>
-                  <td className="center-column">
-                    <span className="responsive-label">Parties</span>
-                    {record.servedParties}
-                  </td>
-                </tr>
-              ),
-            )}
-          </tbody>
-        </table>
+                    </td>
+                    <td>
+                      <span className="responsive-label">
+                        Filings and Proceedings
+                      </span>
+                      {document &&
+                        helper.showDirectDownloadLink &&
+                        document.processingStatus === 'complete' &&
+                        renderDocumentLink(
+                          document.documentId,
+                          record.description,
+                          document.isPaper,
+                        )}
+                      {document &&
+                        helper.showDirectDownloadLink &&
+                        document.processingStatus !== 'complete' && (
+                          <React.Fragment>
+                            <span
+                              className="usa-label-uploading"
+                              aria-label="document uploading marker"
+                            >
+                              <span aria-hidden="true">Uploading</span>
+                            </span>
+                            {record.description}
+                          </React.Fragment>
+                        )}
+                      {document && helper.showDocumentDetailLink && (
+                        <a
+                          href={documentHelper({
+                            docketNumber: caseDetail.docketNumber,
+                            documentId: document.documentId,
+                          })}
+                          aria-label="View PDF"
+                        >
+                          {document && document.isPaper && (
+                            <span className="filing-type-icon-mobile">
+                              <FontAwesomeIcon icon={['fas', 'file-alt']} />
+                            </span>
+                          )}
+                          {record.description}
+                        </a>
+                      )}
+                      {!document &&
+                        record.documentId &&
+                        renderDocumentLink(
+                          record.documentId,
+                          record.description,
+                        )}
+                      {!document && !record.documentId && record.description}
+                      {record.filingsAndProceedings &&
+                        ` ${record.filingsAndProceedings}`}
+                    </td>
+                    <td>
+                      <span className="responsive-label">Filed by</span>
+                      {document && document.filedBy}
+                    </td>
+                    <td>
+                      <span className="responsive-label">Action</span>
+                      {record.action}
+                    </td>
+                    <td>
+                      <span className="responsive-label">Served</span>
+                      {document && document.isStatusServed && (
+                        <span>{caseDetail.datePetitionSentToIrsMessage}</span>
+                      )}
+                      {document && helper.showDocumentStatus && (
+                        <span>{document.status}</span>
+                      )}
+                    </td>
+                    <td className="center-column">
+                      <span className="responsive-label">Parties</span>
+                      {record.servedParties}
+                    </td>
+                  </tr>
+                ),
+              )}
+            </tbody>
+          </table>
+        </div>
       </React.Fragment>
     );
   },
