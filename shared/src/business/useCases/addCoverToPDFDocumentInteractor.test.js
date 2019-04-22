0b556673d18862a64eca263dde71bde47c9d5f5d
Matt Hopson <matthopson@gmail.com>
2019-04-22 09:03:13 -0600

    Tiny bit of test cleanup

--- shared/src/business/useCases/addCoverToPDFDocumentInteractor.test.js
+++ shared/src/business/useCases/addCoverToPDFDocumentInteractor.test.js
@@ -15,31 +15,62 @@ function testPdfDocBytes() {
 describe('addCoverToPDFDocument', () => {
   let testPdfDoc;
 
+  const testingCaseData = {
+    caseId: 'c6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
+    contactPrimary: {
+      name: 'Johnny Taxpayer',
+    },
+    createdAt: '2019-04-19T14:45:15.595Z',
+    docketNumber: '101-19',
+    documents: [
+      {
+        certificateOfService: false,
+        createdAt: '2019-04-19T14:45:15.595Z',
+        documentId: 'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
+        documentType: 'Answer',
+        isPaper: false,
+        processingStatus: 'pending',
+        userId: 'petitionsclerk',
+      },
+    ],
+    partyType: 'Petitioner',
+  };
+
+  const optionalTestingCaseData = {
+    ...testingCaseData,
+    contactPrimary: {
+      name: 'Janie Taxpayer',
+    },
+    contactSecondary: {
+      name: 'Janie Taxpayer',
+    },
+    docketNumber: '102-19',
+    documents: [
+      {
+        ...testingCaseData.documents[0],
+        certificateOfService: true,
+        documentId: 'b6b81f4d-1e47-423a-8caf-6d2fdc3d3858',
+        documentType:
+          'Motion for Entry of Order that Undenied Allegations be Deemed Admitted Pursuant to Rule 37(c)',
+      },
+    ],
+    partyType: 'Petitioner & Spouse',
+  };
+
+  const updateDocumentProcessingStatusStub = sinon.stub().resolves(null);
+  const getObjectStub = sinon.stub().returns({
+    promise: async () => ({
+      Body: testPdfDoc,
+    }),
+  });
+
   beforeEach(() => {
     testPdfDoc = testPdfDocBytes();
   });
 
   it('adds a cover page to a pdf document', async () => {
-    const getCaseByCaseIdStub = sinon.stub().resolves({
-      caseId: 'c6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
-      contactPrimary: {
-        name: 'Johnny Taxpayer',
-      },
-      createdAt: '2019-04-19T14:45:15.595Z',
-      docketNumber: '101-19',
-      documents: [
-        {
-          certificateOfService: false,
-          createdAt: '2019-04-19T14:45:15.595Z',
-          documentId: 'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
-          documentType: 'Answer',
-          isPaper: false,
-          processingStatus: 'pending',
-          userId: 'petitionsclerk',
-        },
-      ],
-      partyType: 'Petitioner',
-    });
+    const getCaseByCaseIdStub = sinon.stub().resolves(testingCaseData);
+
     const saveDocumentStub = sinon
       .stub()
       .callsFake(({ document: newPdfData }) => {
@@ -48,12 +79,6 @@ describe('addCoverToPDFDocument', () => {
           newPdfData,
         );
       });
-    const updateDocumentProcessingStatusStub = sinon.stub().resolves(null);
-    const getObjectStub = sinon.stub().returns({
-      promise: async () => ({
-        Body: testPdfDoc,
-      }),
-    });
 
     const params = {
       applicationContext: {
@@ -80,30 +105,8 @@ describe('addCoverToPDFDocument', () => {
   });
 
   it('adds a cover page to a pdf document with optional data', async () => {
-    const getCaseByCaseIdStub = sinon.stub().resolves({
-      caseId: 'c6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
-      contactPrimary: {
-        name: 'Johnny Taxpayer',
-      },
-      contactSecondary: {
-        name: 'Janie Taxpayer',
-      },
-      createdAt: '2019-04-19T14:45:15.595Z',
-      docketNumber: '101-19',
-      documents: [
-        {
-          certificateOfService: true,
-          createdAt: '2019-04-19T14:45:15.595Z',
-          documentId: 'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
-          documentType:
-            'Motion for Entry of Order that Undenied Allegations be Deemed Admitted Pursuant to Rule 37(c)',
-          isPaper: false,
-          processingStatus: 'pending',
-          userId: 'petitionsclerk',
-        },
-      ],
-      partyType: 'Petitioner & Spouse',
-    });
+    const getCaseByCaseIdStub = sinon.stub().resolves(optionalTestingCaseData);
+
     const saveDocumentStub = sinon
       .stub()
       .callsFake(({ document: newPdfData }) => {
@@ -112,12 +115,6 @@ describe('addCoverToPDFDocument', () => {
           newPdfData,
         );
       });
-    const updateDocumentProcessingStatusStub = sinon.stub().resolves(null);
-    const getObjectStub = sinon.stub().returns({
-      promise: async () => ({
-        Body: testPdfDoc,
-      }),
-    });
 
     const params = {
       applicationContext: {
@@ -132,7 +129,7 @@ describe('addCoverToPDFDocument', () => {
         }),
       },
       caseId: 'c6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
-      documentId: 'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
+      documentId: 'b6b81f4d-1e47-423a-8caf-6d2fdc3d3858',
     };
 
     const newPdfData = await addCoverToPDFDocument(params);
