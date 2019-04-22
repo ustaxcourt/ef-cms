547db64e3ab95317cd02d837c2ba2d487ad9ccb1
Cody Seibert <cseibert113@gmail.com>
2019-04-22 14:27:11 -0400

    more coverage

--- shared/src/business/useCases/externalDocument/generateDocumentTitleInteractor.js
+++ shared/src/business/useCases/externalDocument/generateDocumentTitleInteractor.js
@@ -9,6 +9,7 @@ exports.generateDocumentTitle = ({ applicationContext, documentMetadata }) => {
   const externalDocument = applicationContext
     .getEntityConstructors()
     .ExternalDocumentFactory.get(documentMetadata);
+  console.log('externalDocument', externalDocument);
 
   return externalDocument.getDocumentTitle();
 };
