d6971c5fe156280b258be33ecf8beae749e18c7f
William Bagayoko <wbagayoko@gmail.com>
2019-04-22 12:29:47 -0500

    937 Docket Record Index
    #937 display from assigned index

--- web-client/src/views/DocketRecord.jsx
+++ web-client/src/views/DocketRecord.jsx
@@ -83,7 +83,7 @@ export const DocketRecord = connect(
                 ({ record, document, index }) => (
                   <tr key={index}>
                     <td className="responsive-title center-column">
-                      {index + 1}
+                      {index}
                       <span className="responsive-label push-right">
                         {record.createdAtFormatted}
                       </span>
@@ -210,7 +210,7 @@ export const DocketRecord = connect(
               {caseDetail.docketRecordWithDocument.map(
                 ({ record, document, index }) => (
                   <tr key={index}>
-                    <td className="center-column">{index + 1}</td>
+                    <td className="center-column">{index}</td>
                     <td>{record.createdAtFormatted}</td>
                     <td className="center-column">
                       {document && document.eventCode}
