060ff400f4869e9f69619acf126960bd97c8778c
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-04-29 13:56:11 -0700

    531: allow multiple practitioners on a case; store practitioner on document

--- web-client/e2e/journey/practitionerViewsCaseDetailOfOwnedCase.js
+++ web-client/e2e/journey/practitionerViewsCaseDetailOfOwnedCase.js
@@ -6,7 +6,7 @@ export default test => {
     });
     expect(test.getState('currentPage')).toEqual('CaseDetail');
     expect(test.getState('screenMetadata.caseOwnedByUser')).toEqual(true);
-    expect(test.getState('caseDetail.practitioner.name')).toEqual(
+    expect(test.getState('caseDetail.practitioners.0.name')).toEqual(
       'Test Practitioner',
     );
   });
