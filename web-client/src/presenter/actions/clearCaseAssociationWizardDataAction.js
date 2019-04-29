79156a291cd89ffef9d42831ed620b42afc83852
William Bagayoko <wbagayoko@gmail.com>
2019-04-29 16:54:34 -0500

    531 Clear objections

--- web-client/src/presenter/actions/clearCaseAssociationWizardDataAction.js
+++ web-client/src/presenter/actions/clearCaseAssociationWizardDataAction.js
@@ -10,6 +10,12 @@ export const clearCaseAssociationWizardDataAction = ({ store, props }) => {
   switch (props.key) {
     case 'certificateOfService':
       store.set(state.form.certificateOfServiceDate, null);
+      store.set(state.form.certificateOfServiceMonth, null);
+      store.set(state.form.certificateOfServiceDay, null);
+      store.set(state.form.certificateOfServiceYear, null);
+      break;
+    case 'documentType':
+      store.set(state.form.objections, null);
       break;
   }
 };
