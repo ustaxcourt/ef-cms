127699825eeec2e6e339f6255cb8999e23901609
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-04-29 14:20:55 -0700

    use caseDetail instead of verifyCaseForUser endpoint to determine if practitioner is associated with case

--- web-client/e2e/journey/practitionerViewsCaseDetailOfOwnedCase.js
+++ web-client/e2e/journey/practitionerViewsCaseDetailOfOwnedCase.js
@@ -5,7 +5,6 @@ export default test => {
       docketNumber: test.docketNumber,
     });
     expect(test.getState('currentPage')).toEqual('CaseDetail');
-    expect(test.getState('screenMetadata.caseOwnedByUser')).toEqual(true);
     expect(test.getState('caseDetail.practitioners.0.name')).toEqual(
       'Test Practitioner',
     );
