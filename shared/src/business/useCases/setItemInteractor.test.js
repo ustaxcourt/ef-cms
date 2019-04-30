95f0d0aaf705075b0f6a42dadac55e8160b07f3a
Cody Seibert <cseibert113@gmail.com>
2019-04-30 13:52:19 -0400

    coverage

---
+++ shared/src/business/useCases/setItemInteractor.test.js
@@ -0,0 +1,24 @@
+const { setItem } = require('./setItemInteractor');
+
+describe('setItemInteractor', () => {
+  let applicationContext;
+
+  it('should be able to set an item', async () => {
+    applicationContext = {
+      getPersistenceGateway: () => ({
+        getItem: require('../../persistence/localStorage/getItem').getItem,
+        setItem: require('../../persistence/localStorage/setItem').setItem,
+      }),
+    };
+    await setItem({
+      applicationContext,
+      key: 'abc',
+      value: '123',
+    });
+    const result = await applicationContext.getPersistenceGateway().getItem({
+      applicationContext,
+      key: 'abc',
+    });
+    expect(result).toEqual('123');
+  });
+});
