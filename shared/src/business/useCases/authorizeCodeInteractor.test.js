f3a86f86dc8bc5d745af28c8a708066619bdb841
Cody Seibert <cseibert113@gmail.com>
2019-04-30 09:10:47 -0400

    shared coverage

---
+++ shared/src/business/useCases/authorizeCodeInteractor.test.js
@@ -0,0 +1,37 @@
+const sinon = require('sinon');
+const { authorizeCode } = require('./authorizeCodeInteractor');
+
+describe('authorizeCodeInteractor', () => {
+  let applicationContext;
+
+  it('returns the expected token and refresh token', async () => {
+    const postStub = sinon.stub().resolves({
+      data: {
+        id_token: '123',
+        refresh_token: 'abc',
+      },
+    });
+    applicationContext = {
+      getCognitoClientId: () => 'abc',
+      getCognitoRedirectUrl: () => 'http://example.com',
+      getCognitoTokenUrl: () => 'http://example.com/oauth2/token',
+      getCurrentUser: () => {
+        return {};
+      },
+      getHttpClient: () => ({
+        post: postStub,
+      }),
+    };
+    const response = await authorizeCode({ applicationContext });
+    expect(response).toEqual({
+      refreshToken: 'abc',
+      token: '123',
+    });
+    expect(postStub.getCall(0).args[0]).toEqual(
+      'http://example.com/oauth2/token',
+    );
+    expect(postStub.getCall(0).args[1]).toEqual(
+      'client_id=abc&grant_type=authorization_code&redirect_uri=http%3A%2F%2Fexample.com',
+    );
+  });
+});
