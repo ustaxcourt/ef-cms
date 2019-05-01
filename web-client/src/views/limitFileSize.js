65a5700c038b4e5c403959cd5ed3fac9f7aaf452
Eric Sorenson <eric@sorenson.software>
2019-05-01 15:20:47 -0500

    update error message

--- web-client/src/views/limitFileSize.js
+++ web-client/src/views/limitFileSize.js
@@ -1,7 +1,7 @@
 export const limitFileSize = (e, callback) => {
   // 500MB limit
   if (e.target.files[0].size >= 500 * 1024 * 1024) {
-    alert('Your file must be less than 500 MB.');
+    alert('Your file size is too big. The maximum file size is 500MB.');
     e.target.value = null;
   } else {
     callback();
