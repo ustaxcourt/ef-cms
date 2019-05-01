c12434c603ff65fa93545d1bd7f6316b51d6810f
Eric Sorenson <eric@sorenson.software>
2019-05-01 15:26:12 -0500

    variable name updates

--- web-client/src/views/limitFileSize.js
+++ web-client/src/views/limitFileSize.js
@@ -1,6 +1,9 @@
+const MB = 1024 * 1024;
+export const MAX_SIZE = 500 * MB;
+
 export const limitFileSize = (e, callback) => {
   // 500MB limit
-  if (e.target.files[0].size >= 500 * 1024 * 1024) {
+  if (e.target.files[0].size >= MAX_SIZE) {
     alert('Your file size is too big. The maximum file size is 500MB.');
     e.target.value = null;
   } else {
