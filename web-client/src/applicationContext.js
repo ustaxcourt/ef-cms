776b74d0c34140d4cc50d1f718f8251f6675b003
Eric Sorenson <eric@sorenson.software>
2019-05-02 15:25:35 -0500

    reduce timeout for troubleshooting

--- web-client/src/applicationContext.js
+++ web-client/src/applicationContext.js
@@ -218,7 +218,8 @@ const applicationContext = {
     SECTIONS,
     SESSION_DEBOUNCE: 250,
     SESSION_MODAL_TIMEOUT: 5000 * 60, // 5 minutes
-    SESSION_TIMEOUT: 1000 * 60 * 55, // 55 minutes
+    // SESSION_TIMEOUT: 1000 * 60 * 55, // 55 minutes
+    SESSION_TIMEOUT: 1000 * 60 * 5, // 5 minutes
     TRIAL_CITIES,
   }),
   getCurrentUser,
