c52c39c95f60ac70649e499fce1c96dc5614e362
William Bagayoko <wbagayoko@gmail.com>
2019-05-02 15:45:50 -0500

    rename actions

--- web-client/src/presenter/actions/DocketEntry/restoreWizardDataAction.js
+++ web-client/src/presenter/actions/DocketEntry/restoreWizardDataAction.js
@@ -1,13 +1,13 @@
 import { state } from 'cerebral';
 
 /**
- * restore the documents filed in the wizard on screen
+ * restore wizard data
  *
  * @param {Object} providers the providers object
  * @param {Object} providers.store the cerebral store object
  * @param {Object} providers.props the cerebral props object
  */
-export const restoreFiledWizardDocumentIdsAction = async ({ store, props }) => {
+export const restoreWizardDataAction = async ({ store, props }) => {
   const { filedDocumentIds } = props;
   store.set(state.screenMetadata.filedDocumentIds, filedDocumentIds);
 };
--- web-client/src/presenter/actions/DocketEntry/stashWizardDataAction.js
+++ web-client/src/presenter/actions/DocketEntry/stashWizardDataAction.js
@@ -1,14 +1,14 @@
 import { state } from 'cerebral';
 
 /**
- * stash the documents filed in the wizard in a prop
+ * stash wizard data in props
  *
  * @param {Object} providers the providers object
  * @param {Object} providers.store the cerebral store object
  * @param {Object} providers.applicationContext the application context
  * @param {Object} providers.props the cerebral props object
  */
-export const stashFiledWizardDocumentIdsAction = async ({ get, props }) => {
+export const stashWizardDataAction = async ({ get, props }) => {
   const { primaryDocumentFileId } = props;
 
   const filedDocumentIds = get(state.screenMetadata.filedDocumentIds) || [];
