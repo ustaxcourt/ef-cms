0beebb46b933ee9ef87bc65bf496f7c3b46fb8c1
Cody Seibert <cseibert113@gmail.com>
2019-05-03 17:02:05 -0400

    displaying all done

---
+++ web-client/src/presenter/actions/closeFileUploadStatusModalAction.js
@@ -0,0 +1,15 @@
+import { state } from 'cerebral';
+
+/**
+ * @param {Object} providers the providers object
+ * @param {Object} providers.store the cerebral store object used for setting showModal
+ */
+export const closeFileUploadStatusModalAction = async ({ store }) => {
+  await new Promise(resolve => {
+    store.set(state.percentComplete, 100);
+    store.set(state.timeRemaining, 0);
+    setTimeout(resolve, 3000);
+  }).then(() => {
+    store.set(state.showModal, '');
+  });
+};
