c17fba3627bd08260ec49a14639a635b72a33836
Matt Hopson <matthopson@gmail.com>
2019-05-06 16:41:57 -0500

    WIP header layout and nav logic

--- web-client/src/presenter/computeds/headerHelper.js
+++ web-client/src/presenter/computeds/headerHelper.js
@@ -3,7 +3,27 @@ import { state } from 'cerebral';
 export const headerHelper = get => {
   const user = get(state.user);
 
+  const isUserInternal = user => {
+    const internalRoles = ['petitionsclerk', 'docketclerk'];
+    if (user && user.role && internalRoles.indexOf(user.role) > -1) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  const isUserExternal = user => {
+    const externalRoles = ['petitioner', 'practitioner'];
+    if (user && user.role && externalRoles.indexOf(user.role) > -1) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
   return {
+    showDocumentQC: isUserInternal(user),
+    showMessages: isUserInternal(user),
+    showMyCases: isUserExternal(user),
     showSearchInHeader: user && user.role && user.role !== 'practitioner',
   };
 };
