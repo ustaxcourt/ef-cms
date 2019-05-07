ceae640953ea84ceb3bdb947518b4f27647506bf
Joseph Sutton <jsutton@flexion.us>
2019-05-07 09:22:15 -0500

    adding more test coverage

--- web-client/src/presenter/actions/DocketEntry/updateDocketEntryWizardDataAction.test.js
+++ web-client/src/presenter/actions/DocketEntry/updateDocketEntryWizardDataAction.test.js
@@ -24,4 +24,70 @@ describe('updateDocketEntryWizardDataAction', () => {
     expect(result.state.form.certificateOfServiceMonth).toEqual(null);
     expect(result.state.form.certificateOfServiceYear).toEqual(null);
   });
+
+  it('clears form values when props.key=eventCode', async () => {
+    const result = await runAction(updateDocketEntryWizardDataAction, {
+      props: {
+        key: 'eventCode',
+      },
+      state: {
+        constants: {
+          INTERNAL_CATEGORY_MAP: ['documentTitle'],
+        },
+        form: {
+          documentTitle: 'document title',
+          secondaryDocument: {
+            freeText: 'Guy Fieri is my spirit animal.',
+            ordinalValue: 'asdf',
+            previousDocument: {},
+            serviceDate: new Date(),
+            trialLocation: 'Flavortown',
+          },
+        },
+      },
+    });
+
+    expect(result.state.form).toEqual({
+      freeText: null,
+      ordinalValue: null,
+      previousDocument: null,
+      secondaryDocument: null,
+      serviceDate: null,
+      trialLocation: null,
+    });
+  });
+
+  it('clears secondaryDocument form items', async () => {
+    const result = await runAction(updateDocketEntryWizardDataAction, {
+      props: {
+        key: 'secondaryDocument.eventCode',
+      },
+      state: {
+        constants: {
+          INTERNAL_CATEGORY_MAP: ['documentTitle'],
+        },
+        form: {
+          documentTitle: 'document title',
+          secondaryDocument: {
+            freeText: 'Guy Fieri is my spirit animal.',
+            ordinalValue: 'asdf',
+            previousDocument: {},
+            serviceDate: new Date(),
+            trialLocation: 'Flavortown',
+          },
+        },
+      },
+    });
+
+    expect(result.state.form).toEqual({
+      documentTitle: 'document title',
+      secondaryDocument: {
+        freeText: null,
+        ordinalValue: null,
+        previousDocument: null,
+        serviceDate: null,
+        trialLocation: null,
+      },
+    });
+  });
 });
