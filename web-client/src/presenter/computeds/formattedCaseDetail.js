b20eca005061b337a1a38c3430a5a7f32c456fb8
Kris Koskelin <kkoskelin@flexion.us>
2019-05-08 11:25:24 -0500

    1451: docket record sorting functions
    WIP, must be wired up to retrieve sort strings from correct place in state

--- web-client/src/presenter/computeds/formattedCaseDetail.js
+++ web-client/src/presenter/computeds/formattedCaseDetail.js
@@ -135,25 +135,6 @@ const formatDocketRecordWithDocument = (
   });
 };
 
-const getDocketRecordSortFunc = function(sortBy) {
-  const byIndex = (a, b) => a.index - b.index;
-  const byIndexReverse = (a, b) => byIndex(b, a);
-  const byDate = (a, b) => a.index - b.index; // TODO
-  const byDateReverse = (a, b) => byDate(b, a);
-
-  switch (sortBy) {
-    case 'byIndexDesc':
-      return byIndexReverse;
-    case 'byIndex':
-      return byIndex;
-    case 'byDateDesc':
-      return byDateReverse;
-    case 'byDate': // fall through
-    default:
-      return byDate;
-  }
-};
-
 const formatCase = (caseDetail, caseDetailErrors) => {
   if (_.isEmpty(caseDetail)) {
     return {};
@@ -170,6 +151,11 @@ const formatCase = (caseDetail, caseDetailErrors) => {
       result.documents,
     );
   }
+
+  // establish an initial sort by ascending index
+  result.docketRecordWithDocument.sort((a, b) => {
+    return a.index - b.index;
+  });
 
   if (result.respondent)
     result.respondent.formattedName = `${result.respondent.name} ${
@@ -222,17 +208,62 @@ const formatCase = (caseDetail, caseDetailErrors) => {
   return result;
 };
 
+const getDocketRecordSortFunc = function(sortBy) {
+  const byIndex = (a, b) => a.index - b.index;
+  const byDate = (a, b) => {
+    const secondsDifference = 30 * 1000;
+    const aDate = new Date(a.record.filingDate);
+    const bDate = new Date(b.record.filingDate);
+    if (Math.abs(aDate - bDate) < secondsDifference) {
+      // treat as equal timestamps
+      return 0;
+    }
+    return aDate - bDate;
+  };
+
+  switch (sortBy) {
+    case 'byIndex': // fall-through
+    case 'byIndexDesc':
+      return byIndex;
+    case 'byDate': // fall through, is the default sort method
+    case 'byDateDesc':
+    default:
+      return byDate;
+  }
+};
+
+const sortDocketRecords = (docketRecords, sortBy = '') => {
+  const sortFunc = getDocketRecordSortFunc(sortBy);
+  const isReversed = sortBy.indexOf('Desc') > -1;
+  const result = docketRecords.sort(sortFunc);
+  if (isReversed) {
+    // reversing AFTER the sort keeps sorting stable
+    return result.reverse();
+  }
+  return result;
+};
+
 export const formattedCases = get => {
   const cases = get(state.cases);
   const docketRecordSort = get(state.prefs.docketRecordSort);
   return cases.map(caseObj => {
     const result = formatCase(caseObj);
-    return result.sort(getDocketRecordSortFunc(docketRecordSort));
+    result.docketRecordWithDocument = sortDocketRecords(
+      result.docketRecordWithDocument,
+      docketRecordSort,
+    );
+    return result;
   });
 };
 
 export const formattedCaseDetail = get => {
   const caseDetail = get(state.caseDetail);
+  const docketRecordSort = get(state.prefs.docketRecordSort);
   const caseDetailErrors = get(state.caseDetailErrors);
-  return formatCase(caseDetail, caseDetailErrors);
+  const result = formatCase(caseDetail, caseDetailErrors);
+  result.docketRecordWithDocument = sortDocketRecords(
+    result.docketRecordWithDocument,
+    docketRecordSort,
+  );
+  return result;
 };
