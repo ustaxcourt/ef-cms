57569e6016ea8fbb449ea1d7706e90c8056e649e
Joseph Sutton <jsutton@flexion.us>
2019-05-08 16:14:29 -0500

    email component class name updates

--- web-client/src/views/StartCase/Email.jsx
+++ web-client/src/views/StartCase/Email.jsx
@@ -11,23 +11,25 @@ export const Email = connect(
     return (
       <React.Fragment>
         <div className="usa-form-group">
-          <div className="usa-width-five-twelfths email-input">
-            <label htmlFor="email">Email Address</label>
-            {email}
-          </div>
-          <div className="usa-width-seven-twelfths">
-            <div
-              id="change-email-hint"
-              className="alert-gold add-bottom-margin"
-            >
-              <span className="usa-form-hint ustc-form-hint-with-svg">
-                <FontAwesomeIcon
-                  icon={['far', 'arrow-alt-circle-left']}
-                  className="fa-icon-gold"
-                  size="sm"
-                />
-                To change your email, go to your Account Settings.
-              </span>
+          <div className="grid-row grid-gap">
+            <div className="grid-col-5 email-input">
+              <label htmlFor="email">Email Address</label>
+              {email}
+            </div>
+            <div className="grid-col-7">
+              <div
+                id="change-email-hint"
+                className="alert-gold add-bottom-margin"
+              >
+                <span className="usa-hint ustc-form-hint-with-svg">
+                  <FontAwesomeIcon
+                    icon={['far', 'arrow-alt-circle-left']}
+                    className="fa-icon-gold"
+                    size="sm"
+                  />
+                  To change your email, go to your Account Settings.
+                </span>
+              </div>
             </div>
           </div>
         </div>
