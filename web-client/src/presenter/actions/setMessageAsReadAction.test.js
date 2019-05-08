d8092d6b5df70886deac6cb4fd8b0b03e0a3f6bc
Joseph Sutton <jsutton@flexion.us>
2019-05-08 11:50:32 -0500

    removed unnecessary variable in test

--- web-client/src/presenter/actions/setMessageAsReadAction.test.js
+++ web-client/src/presenter/actions/setMessageAsReadAction.test.js
@@ -2,15 +2,12 @@ import { setMessageAsReadAction } from './setMessageAsReadAction';
 import sinon from 'sinon';
 
 describe('setMessageAsReadAction', () => {
-  let messageId;
   let get;
   let applicationContext;
 
   it('should set message as read', async () => {
     let setMessageAsReadStub = sinon.stub();
-
-    messageId = 123;
-    get = sinon.stub().returns(messageId);
+    get = sinon.stub();
     applicationContext = {
       getUseCases: () => ({
         setMessageAsRead: setMessageAsReadStub,
