5ff0f6bf8a57b9b1814fdeb8693070404b13a0a4
Matt Hopson <matthopson@gmail.com>
2019-05-08 13:16:18 -0500

    Lint fixes

--- web-client/src/presenter/actions/closeAccountMenuAction.js
+++ web-client/src/presenter/actions/closeAccountMenuAction.js
@@ -3,6 +3,6 @@ import { state } from 'cerebral';
 /**
  * Closes the account menu
  */
-export const closeAccountMenuAction = ({ store, get }) => {
+export const closeAccountMenuAction = ({ store }) => {
   store.set(state.isAccountMenuOpen, false);
 };
