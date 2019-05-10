9899f00f3cc6aa1c15f0aae60d7ff01229806850
Joseph Sutton <jsutton@flexion.us>
2019-05-10 15:43:06 -0500

    addressed jsdoc warnings

--- web-client/src/presenter/actions/closeAccountMenuAction.js
+++ web-client/src/presenter/actions/closeAccountMenuAction.js
@@ -3,6 +3,7 @@ import { state } from 'cerebral';
 /**
  * Closes the account menu
  *
+ * @param {Object} providers the providers object
  * @param {Object} providers.store the cerebral store object
  */
 export const closeAccountMenuAction = ({ store }) => {
