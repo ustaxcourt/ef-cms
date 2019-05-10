d2420e6c7ec0ed946cda79705deacac8193c9b2b
Matt Hopson <matthopson@gmail.com>
2019-05-10 14:32:42 -0500

    Add seniorattorney and respondent roles to header logic

--- web-client/src/presenter/computeds/headerHelper.test.js
+++ web-client/src/presenter/computeds/headerHelper.test.js
@@ -27,8 +27,16 @@ describe('headerHelper', () => {
         },
       },
     });
+    const result3 = runCompute(headerHelper, {
+      state: {
+        user: {
+          role: 'seniorattorney',
+        },
+      },
+    });
     expect(result1.showDocumentQC).toBeTruthy();
     expect(result2.showDocumentQC).toBeTruthy();
+    expect(result3.showDocumentQC).toBeTruthy();
   });
   it('should show messages for internal users', () => {
     const result1 = runCompute(headerHelper, {
@@ -45,8 +53,16 @@ describe('headerHelper', () => {
         },
       },
     });
+    const result3 = runCompute(headerHelper, {
+      state: {
+        user: {
+          role: 'seniorattorney',
+        },
+      },
+    });
     expect(result1.showMessages).toBeTruthy();
     expect(result2.showMessages).toBeTruthy();
+    expect(result3.showMessages).toBeTruthy();
   });
   it('should show cases for external users', () => {
     const result1 = runCompute(headerHelper, {
@@ -63,8 +79,16 @@ describe('headerHelper', () => {
         },
       },
     });
+    const result3 = runCompute(headerHelper, {
+      state: {
+        user: {
+          role: 'respondent',
+        },
+      },
+    });
     expect(result1.showMyCases).toBeTruthy();
     expect(result2.showMyCases).toBeTruthy();
+    expect(result3.showMyCases).toBeTruthy();
   });
   it('should NOT show search in header for practitioners', () => {
     const result = runCompute(headerHelper, {
@@ -91,8 +115,16 @@ describe('headerHelper', () => {
         },
       },
     });
+    const result3 = runCompute(headerHelper, {
+      state: {
+        user: {
+          role: 'respondent',
+        },
+      },
+    });
     expect(result1.showDocumentQC).toBeFalsy();
     expect(result2.showDocumentQC).toBeFalsy();
+    expect(result3.showDocumentQC).toBeFalsy();
   });
   it('should NOT show messages for external users', () => {
     const result1 = runCompute(headerHelper, {
@@ -109,8 +141,16 @@ describe('headerHelper', () => {
         },
       },
     });
+    const result3 = runCompute(headerHelper, {
+      state: {
+        user: {
+          role: 'respondent',
+        },
+      },
+    });
     expect(result1.showMessages).toBeFalsy();
     expect(result2.showMessages).toBeFalsy();
+    expect(result3.showMessages).toBeFalsy();
   });
   it('should NOT show cases for internal users', () => {
     const result1 = runCompute(headerHelper, {
@@ -127,7 +167,15 @@ describe('headerHelper', () => {
         },
       },
     });
+    const result3 = runCompute(headerHelper, {
+      state: {
+        user: {
+          role: 'seniorattorney',
+        },
+      },
+    });
     expect(result1.showMyCases).toBeFalsy();
     expect(result2.showMyCases).toBeFalsy();
+    expect(result3.showMyCases).toBeFalsy();
   });
 });
