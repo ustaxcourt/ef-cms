6f688b4e0508992d9241ab9fa42d21bc87a7bc82
Matt Hopson <matthopson@gmail.com>
2019-05-14 14:19:46 -0600

    Updates to cypress tests

--- web-client/cypress/support/pages/dashboard.js
+++ web-client/cypress/support/pages/dashboard.js
@@ -3,22 +3,20 @@ exports.navigateTo = username => {
 };
 
 exports.viewMyOutbox = () => {
-  cy.get('button#tab-my-queue').click();
   cy.get('button#individual-sent-tab').click();
 };
 
 exports.viewMyInbox = () => {
-  cy.get('button#tab-my-queue').click();
   cy.get('button#individual-inbox-tab').click();
 };
 
 exports.viewSectionInbox = () => {
-  cy.get('button#tab-work-queue').click();
+  cy.get('button.button-switch-box').click();
   cy.get('button#section-inbox-tab').click();
 };
 
 exports.viewSectionOutbox = () => {
-  cy.get('button#tab-work-queue').click();
+  cy.get('button.button-switch-box').click();
   cy.get('button#section-sent-tab').click();
 };
 
