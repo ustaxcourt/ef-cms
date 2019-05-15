9f4df5966f4e512d78a53c1076e9a1e2a17cf730
Joseph Sutton <jsutton@flexion.us>
2019-05-15 11:27:18 -0500

    removed unnecessary div

--- web-client/src/views/IndividualWorkQueueInbox.jsx
+++ web-client/src/views/IndividualWorkQueueInbox.jsx
@@ -84,17 +84,7 @@ export const IndividualWorkQueueInbox = connect(
                     </a>
                   </div>
                   <div className="message-document-detail">
-                    <div className="message-content">
-                      {/*item.currentMessage.message*/}
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Aliquam consectetur venenatis blandit. Praesent vehicula,
-                      libero non pretium vulputate, lacus arcu facilisis lectus,
-                      sed feugiat tellus nulla eu dolor. Nulla porta bibendum
-                      lectus quis euismod. Aliquam volutpat ultricies porttitor.
-                      Cras risus nisi, accumsan vel cursus ut, sollicitudin
-                      vitae dolor. Fusce scelerisque eleifend lectus in
-                      bibendum. Suspendisse lacinia egestas felis a volutpat.
-                    </div>
+                    {item.currentMessage.message}
                   </div>
                 </td>
                 <td className="message-queue-row">{item.caseStatus}</td>
