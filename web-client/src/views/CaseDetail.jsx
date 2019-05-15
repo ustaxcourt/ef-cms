b1e2fed7e6f6a4ba4378ca380df841c2bd7f735c
Eric Sorenson <eric@sorenson.software>
2019-05-15 15:56:00 -0500

    case detail headers

--- web-client/src/views/CaseDetail.jsx
+++ web-client/src/views/CaseDetail.jsx
@@ -26,8 +26,8 @@ export const CaseDetail = connect(
   }) {
     return (
       <React.Fragment>
-        <section className="usa-section grid-container">
-          <div className="grid-container padding-x-0">
+        <div className="big-blue-header">
+          <div className="grid-container">
             <div className="grid-row">
               <div className="tablet:grid-col-6">
                 <h1 className="captioned" tabIndex="-1">
@@ -50,12 +50,10 @@ export const CaseDetail = connect(
               </div>
             </div>
           </div>
-
-          <hr aria-hidden="true" />
-
+        </div>
+        <section className="usa-section grid-container">
           <SuccessNotification />
           <ErrorNotification />
-
           {caseHelper.showActionRequired && (
             <div className="subsection">
               <div className="title">
