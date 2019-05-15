9f488b71033fdabca8ed7ae20ff38237f745249d
Matt Hopson <matthopson@gmail.com>
2019-05-15 13:22:54 -0600

    Clear header when changing views

--- web-client/src/presenter/sequences/gotoBeforeStartCaseSequence.js
+++ web-client/src/presenter/sequences/gotoBeforeStartCaseSequence.js
@@ -1,5 +1,7 @@
+import { clearCurrentPageHeaderAction } from '../actions/clearCurrentPageHeaderAction';
 import { setCurrentPageAction } from '../actions/setCurrentPageAction';
 
 export const gotoBeforeStartCaseSequence = [
   setCurrentPageAction('BeforeStartingCase'),
+  clearCurrentPageHeaderAction,
 ];
