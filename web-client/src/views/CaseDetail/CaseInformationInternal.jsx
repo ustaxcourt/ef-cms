68db6bfbd07f34ac4977156f0c6d4b73df776433
William Bagayoko <wbagayoko@gmail.com>
2019-05-16 11:27:08 -0500

    1659 componentize chunks
    [skip ci]

--- web-client/src/views/CaseDetail/CaseInformationInternal.jsx
+++ web-client/src/views/CaseDetail/CaseInformationInternal.jsx
@@ -1,6 +1,66 @@
 import { connect } from '@cerebral/react';
 import { state } from 'cerebral';
+import PropTypes from 'prop-types';
 import React from 'react';
+
+const PetitionDetails = ({ caseDetail, showPaymentRecord }) => (
+  <React.Fragment>
+    <div className="grid-row">
+      <div className="grid-col-6">
+        <p className="label">Notice/Case Type</p>
+        <p>{caseDetail.caseType}</p>
+      </div>
+      <div className="grid-col-6">
+        <p className="label">Case Procedure</p>
+        <p>{caseDetail.procedureType}</p>
+      </div>
+    </div>
+    <div className="grid-row">
+      <div className="grid-col-6">
+        <p className="label">IRS Notice Date</p>
+        <p id="irs-notice-date">{caseDetail.irsNoticeDateFormatted}</p>
+      </div>
+      <div className="grid-col-6">
+        {showPaymentRecord && (
+          <React.Fragment>
+            <p className="label">Petition Fee Paid</p>
+            <p id="pay-gov-id-display">{caseDetail.payGovId}</p>
+          </React.Fragment>
+        )}
+      </div>
+    </div>
+  </React.Fragment>
+);
+
+PetitionDetails.propTypes = {
+  caseDetail: PropTypes.object,
+  showPaymentRecord: PropTypes.bool,
+};
+
+const TrialInformation = ({ caseDetail }) => (
+  <React.Fragment>
+    <div className="grid-row">
+      <div className="grid-col-6">
+        <p className="label">Place of Trial</p>
+        <p>{caseDetail.preferredTrialCity}</p>
+      </div>
+      <div className="grid-col-6">
+        <p className="label">Trial Date</p>
+        <p>Not scheduled</p>
+      </div>
+    </div>
+    <div className="grid-row">
+      <div className="grid-col-6">
+        <p className="label">Assigned Judge</p>
+        <p>Not assigned</p>
+      </div>
+    </div>
+  </React.Fragment>
+);
+
+TrialInformation.propTypes = {
+  caseDetail: PropTypes.object,
+};
 
 export const CaseInformationInternal = connect(
   {
@@ -16,32 +76,10 @@ export const CaseInformationInternal = connect(
               <div className="card height-full">
                 <div className="content-wrapper">
                   <h3 className="underlined">Petition Details</h3>
-                  <div className="grid-row">
-                    <div className="grid-col-6">
-                      <p className="label">Notice/Case Type</p>
-                      <p>{caseDetail.caseType}</p>
-                    </div>
-                    <div className="grid-col-6">
-                      <p className="label">Case Procedure</p>
-                      <p>{caseDetail.procedureType}</p>
-                    </div>
-                  </div>
-                  <div className="grid-row">
-                    <div className="grid-col-6">
-                      <p className="label">IRS Notice Date</p>
-                      <p id="irs-notice-date">
-                        {caseDetail.irsNoticeDateFormatted}
-                      </p>
-                    </div>
-                    <div className="grid-col-6">
-                      {helper.showPaymentRecord && (
-                        <React.Fragment>
-                          <p className="label">Petition Fee Paid</p>
-                          <p id="pay-gov-id-display">{caseDetail.payGovId}</p>
-                        </React.Fragment>
-                      )}
-                    </div>
-                  </div>
+                  <PetitionDetails
+                    caseDetail={caseDetail}
+                    showPaymentRecord={helper.showPaymentRecord}
+                  />
                 </div>
               </div>
             </div>
@@ -49,23 +87,7 @@ export const CaseInformationInternal = connect(
               <div className="card height-full">
                 <div className="content-wrapper">
                   <h3 className="underlined">Trial Information</h3>
-
-                  <div className="grid-row">
-                    <div className="grid-col-6">
-                      <p className="label">Place of Trial</p>
-                      <p>{caseDetail.preferredTrialCity}</p>
-                    </div>
-                    <div className="grid-col-6">
-                      <p className="label">Trial Date</p>
-                      <p>Not scheduled</p>
-                    </div>
-                  </div>
-                  <div className="grid-row">
-                    <div className="grid-col-6">
-                      <p className="label">Assigned Judge</p>
-                      <p>Not assigned</p>
-                    </div>
-                  </div>
+                  <TrialInformation caseDetail={caseDetail} />
                 </div>
               </div>
             </div>
