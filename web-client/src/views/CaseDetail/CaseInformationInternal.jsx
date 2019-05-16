c2a119e2f014e2f0c152ff6dd789d6e853f116e9
William Bagayoko <wbagayoko@gmail.com>
2019-05-15 23:45:15 -0500

    1659 Card View

--- web-client/src/views/CaseDetail/CaseInformationInternal.jsx
+++ web-client/src/views/CaseDetail/CaseInformationInternal.jsx
@@ -13,43 +13,51 @@ export const CaseInformationInternal = connect(
         <div className="grid-container padding-x-0">
           <div className="grid-row grid-gap">
             <div className="grid-col-6">
-              <h3 className="underlined">Petition Details</h3>
-              <div className="grid-container padding-x-0">
-                <div className="grid-row">
-                  <div className="grid-col-4">
-                    <p className="label">Notice/Case Type</p>
-                    <p>{caseDetail.caseType}</p>
-                    <p className="label">IRS Notice Date</p>
-                    <p id="irs-notice-date">
-                      {caseDetail.irsNoticeDateFormatted}
-                    </p>
-                  </div>
-                  <div className="grid-col-4">
-                    <p className="label">Case Procedure</p>
-                    <p>{caseDetail.procedureType}</p>
-                  </div>
-                  <div className="grid-col-4">
-                    {helper.showPaymentRecord && (
-                      <React.Fragment>
-                        <p className="label">Petition Fee Paid</p>
-                        <p id="pay-gov-id-display">{caseDetail.payGovId}</p>
-                      </React.Fragment>
-                    )}
+              <div className="card height-full">
+                <div className="content-wrapper">
+                  <h3 className="underlined">Petition Details</h3>
+                  <div className="grid-container padding-x-0">
+                    <div className="grid-row">
+                      <div className="grid-col-4">
+                        <p className="label">Notice/Case Type</p>
+                        <p>{caseDetail.caseType}</p>
+                        <p className="label">IRS Notice Date</p>
+                        <p id="irs-notice-date">
+                          {caseDetail.irsNoticeDateFormatted}
+                        </p>
+                      </div>
+                      <div className="grid-col-4">
+                        <p className="label">Case Procedure</p>
+                        <p>{caseDetail.procedureType}</p>
+                      </div>
+                      <div className="grid-col-4">
+                        {helper.showPaymentRecord && (
+                          <React.Fragment>
+                            <p className="label">Petition Fee Paid</p>
+                            <p id="pay-gov-id-display">{caseDetail.payGovId}</p>
+                          </React.Fragment>
+                        )}
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
             <div className="grid-col-6">
-              <h3 className="underlined">Trial Information</h3>
-              <div className="grid-col-4">
-                <p className="label">Place of Trial</p>
-                <p>{caseDetail.preferredTrialCity}</p>
-                <p className="label">Assigned Judge</p>
-                <p>Not assigned</p>
-              </div>
-              <div className="grid-col-4">
-                <p className="label">Trial Date</p>
-                <p>Not scheduled</p>
+              <div className="card height-full">
+                <div className="content-wrapper">
+                  <h3 className="underlined">Trial Information</h3>
+                  <div className="grid-col-4">
+                    <p className="label">Place of Trial</p>
+                    <p>{caseDetail.preferredTrialCity}</p>
+                    <p className="label">Assigned Judge</p>
+                    <p>Not assigned</p>
+                  </div>
+                  <div className="grid-col-4">
+                    <p className="label">Trial Date</p>
+                    <p>Not scheduled</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
