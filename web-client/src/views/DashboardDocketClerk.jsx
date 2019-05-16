10a72d8dd872a5bf0f7dfdfe503d09987d95e933
Matt Hopson <matthopson@gmail.com>
2019-05-16 09:52:34 -0600

    Use internal dashboard page header directly

--- web-client/src/views/DashboardDocketClerk.jsx
+++ web-client/src/views/DashboardDocketClerk.jsx
@@ -1,12 +1,16 @@
+import { HeaderDashboardInternal } from './HeaderDashboardInternal';
 import { ErrorNotification } from './ErrorNotification';
 import { SuccessNotification } from './SuccessNotification';
 import { WorkQueue } from './WorkQueue';
 import React from 'react';
 
 export const DashboardDocketClerk = () => (
-  <section className="usa-section grid-container">
-    <SuccessNotification />
-    <ErrorNotification />
-    <WorkQueue />
-  </section>
+  <>
+    <HeaderDashboardInternal />
+    <section className="usa-section grid-container">
+      <SuccessNotification />
+      <ErrorNotification />
+      <WorkQueue />
+    </section>
+  </>
 );
