ff2334f604ae120fb5d433e4fdd52180883ea9b1
William Bagayoko <wbagayoko@gmail.com>
2019-05-15 23:45:15 -0500

    1659 Card View

--- web-client/src/views/CaseDetail/CaseInformationPublic.jsx
+++ web-client/src/views/CaseDetail/CaseInformationPublic.jsx
@@ -12,38 +12,42 @@ export const CaseInformationPublic = connect(
       <div className="petitions-details">
         <div className="grid-container padding-x-0">
           <div className="grid-row grid-gap">
-            <div className="card tablet:grid-col-6">
-              <div className="content-wrapper">
-                <h3 className="underlined">Petition Details</h3>
-                <div className="grid-container padding-x-0">
-                  {helper.showPaymentRecord && (
-                    <React.Fragment>
-                      <p className="label">Petition Fee Paid</p>
-                      <p>
-                        Paid by pay.gov
-                        <br />
-                        {caseDetail.payGovDateFormatted}
-                      </p>
-                    </React.Fragment>
-                  )}
+            <div className="tablet:grid-col-6">
+              <div className="card height-full">
+                <div className="content-wrapper">
+                  <h3 className="underlined">Petition Details</h3>
+                  <div className="grid-container padding-x-0">
+                    {helper.showPaymentRecord && (
+                      <React.Fragment>
+                        <p className="label">Petition Fee Paid</p>
+                        <p>
+                          Paid by pay.gov
+                          <br />
+                          {caseDetail.payGovDateFormatted}
+                        </p>
+                      </React.Fragment>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="card tablet:grid-col-6">
-              <div className="content-wrapper">
-                <h3 className="underlined">Trial Information</h3>
-                <div className="grid-row">
-                  <div className="tablet:grid-col-4">
-                    <p className="label">Place of Trial</p>
-                    <p>{caseDetail.preferredTrialCity}</p>
-                  </div>
-                  <div className="tablet:grid-col-4">
-                    <p className="label">Assigned Judge</p>
-                    <p>Not assigned</p>
-                  </div>
-                  <div className="tablet:grid-col-4">
-                    <p className="label">Trial Date</p>
-                    <p>Not scheduled</p>
+            <div className="tablet:grid-col-6">
+              <div className="card height-full">
+                <div className="content-wrapper">
+                  <h3 className="underlined">Trial Information</h3>
+                  <div className="grid-row">
+                    <div className="tablet:grid-col-4">
+                      <p className="label">Place of Trial</p>
+                      <p>{caseDetail.preferredTrialCity}</p>
+                    </div>
+                    <div className="tablet:grid-col-4">
+                      <p className="label">Assigned Judge</p>
+                      <p>Not assigned</p>
+                    </div>
+                    <div className="tablet:grid-col-4">
+                      <p className="label">Trial Date</p>
+                      <p>Not scheduled</p>
+                    </div>
                   </div>
                 </div>
               </div>
