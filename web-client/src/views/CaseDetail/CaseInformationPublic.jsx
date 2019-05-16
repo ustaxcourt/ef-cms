5ece3b3f2622b5702bb2383417476d6011d2d713
William Bagayoko <wbagayoko@gmail.com>
2019-05-16 11:27:08 -0500

    1659 componentize chunks
    [skip ci]

--- web-client/src/views/CaseDetail/CaseInformationPublic.jsx
+++ web-client/src/views/CaseDetail/CaseInformationPublic.jsx
@@ -1,6 +1,54 @@
 import { connect } from '@cerebral/react';
 import { state } from 'cerebral';
+import PropTypes from 'prop-types';
 import React from 'react';
+
+const PetitionDetails = ({ caseDetail, showPaymentRecord }) => (
+  <React.Fragment>
+    <div className="grid-container padding-x-0">
+      {showPaymentRecord && (
+        <React.Fragment>
+          <p className="label">Petition Fee Paid</p>
+          <p>
+            Paid by pay.gov
+            <br />
+            {caseDetail.payGovDateFormatted}
+          </p>
+        </React.Fragment>
+      )}
+    </div>
+  </React.Fragment>
+);
+
+PetitionDetails.propTypes = {
+  caseDetail: PropTypes.object,
+  showPaymentRecord: PropTypes.bool,
+};
+
+const TrialInformation = ({ caseDetail }) => (
+  <React.Fragment>
+    <div className="grid-row">
+      <div className="tablet:grid-col-6">
+        <p className="label">Place of Trial</p>
+        <p>{caseDetail.preferredTrialCity}</p>
+      </div>
+      <div className="tablet:grid-col-6">
+        <p className="label">Trial Date</p>
+        <p>Not scheduled</p>
+      </div>
+    </div>
+    <div className="grid-row">
+      <div className="tablet:grid-col-6">
+        <p className="label">Assigned Judge</p>
+        <p>Not assigned</p>
+      </div>
+    </div>
+  </React.Fragment>
+);
+
+TrialInformation.propTypes = {
+  caseDetail: PropTypes.object,
+};
 
 export const CaseInformationPublic = connect(
   {
@@ -16,18 +64,10 @@ export const CaseInformationPublic = connect(
               <div className="card height-full">
                 <div className="content-wrapper">
                   <h3 className="underlined">Petition Details</h3>
-                  <div className="grid-container padding-x-0">
-                    {helper.showPaymentRecord && (
-                      <React.Fragment>
-                        <p className="label">Petition Fee Paid</p>
-                        <p>
-                          Paid by pay.gov
-                          <br />
-                          {caseDetail.payGovDateFormatted}
-                        </p>
-                      </React.Fragment>
-                    )}
-                  </div>
+                  <PetitionDetails
+                    caseDetail={caseDetail}
+                    showPaymentRecord={helper.showPaymentRecord}
+                  />
                 </div>
               </div>
             </div>
@@ -35,22 +75,7 @@ export const CaseInformationPublic = connect(
               <div className="card height-full">
                 <div className="content-wrapper">
                   <h3 className="underlined">Trial Information</h3>
-                  <div className="grid-row">
-                    <div className="tablet:grid-col-6">
-                      <p className="label">Place of Trial</p>
-                      <p>{caseDetail.preferredTrialCity}</p>
-                    </div>
-                    <div className="tablet:grid-col-6">
-                      <p className="label">Trial Date</p>
-                      <p>Not scheduled</p>
-                    </div>
-                  </div>
-                  <div className="grid-row">
-                    <div className="tablet:grid-col-6">
-                      <p className="label">Assigned Judge</p>
-                      <p>Not assigned</p>
-                    </div>
-                  </div>
+                  <TrialInformation caseDetail={caseDetail} />
                 </div>
               </div>
             </div>
