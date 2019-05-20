aca8756930c3a58ef82b8f9ce43fd44b67b0ddd9
Kris Koskelin <kkoskelin@flexion.us>
2019-05-20 14:29:27 -0500

    undo change to fetch notifications sequence
    I still prefer my refactor to the action itself :D

--- web-client/src/presenter/sequences/fetchUserNotificationsSequence.js
+++ web-client/src/presenter/sequences/fetchUserNotificationsSequence.js
@@ -1,11 +1,7 @@
 import { getNotificationsAction } from '../actions/getNotificationsAction';
-import { isLoggedInAction } from '../actions/isLoggedInAction';
 import { setNotificationsAction } from '../actions/setNotificationsAction';
 
 export const fetchUserNotificationsSequence = [
-  isLoggedInAction,
-  {
-    isLoggedIn: [getNotificationsAction, setNotificationsAction],
-    unauthorized: [], // hakuna matata
-  },
+  getNotificationsAction,
+  setNotificationsAction,
 ];
