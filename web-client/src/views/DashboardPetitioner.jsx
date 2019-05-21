25ed13d3af9d7e8d6bc10577968cf6ffa62420b5
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-05-21 12:33:22 -0700

    1663: full width blue header

--- web-client/src/views/DashboardPetitioner.jsx
+++ web-client/src/views/DashboardPetitioner.jsx
@@ -12,83 +12,89 @@ export const DashboardPetitioner = connect(
   { helper: state.dashboardPetitionerHelper, user: state.user },
   ({ user, helper }) => {
     return (
-      <section className="usa-section grid-container">
-        <h1 tabIndex="-1">Welcome, {user.name}</h1>
-        <SuccessNotification />
-        <ErrorNotification />
-        <div className="grid-row grid-gap taxpayer-tools">
-          <div className="tablet:grid-col-8">
-            {helper.showWhatToExpect && <WhatToExpect />}
-            {helper.showCaseList && <CaseListPetitioner />}
-          </div>
-          <div className="tablet:grid-col-4">
-            <div className="card">
-              <div className="content-wrapper gray">
-                <h3>Taxpayer Tools</h3>
-                <hr />
-                <p>
-                  <FontAwesomeIcon
-                    icon="file-pdf"
-                    size="1x"
-                    className="fa-icon-blue"
-                  />
-                  <a
-                    href={howToPrepareYourDocuments}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Petition Filing Guide
-                  </a>
-                </p>
-                <p>
-                  <a
-                    href="https://www.ustaxcourt.gov/dpt_cities.htm"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Find a court location
-                  </a>
-                  <FontAwesomeIcon
-                    icon="external-link-alt"
-                    size="sm"
-                    className="fa-icon-blue margin-left-05"
-                  />
-                </p>
-                <p>
-                  <a
-                    href="https://www.ustaxcourt.gov/forms.htm"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    View forms
-                  </a>
-                  <FontAwesomeIcon
-                    icon="external-link-alt"
-                    size="sm"
-                    className="fa-icon-blue margin-left-05"
-                  />
-                </p>
-              </div>
-            </div>
-            <div className="card">
-              <div className="content-wrapper gray">
-                <h3>Other Filing Options</h3>
-                <hr />
-                <p>
-                  <a href="/" target="_blank" rel="noopener noreferrer">
-                    How to file a case by mail or in person
-                  </a>
-                  <FontAwesomeIcon
-                    icon="external-link-alt"
-                    size="sm"
-                    className="fa-icon-blue margin-left-05"
-                  />
-                </p>
-              </div>
-            </div>
+      <React.Fragment>
+        <div className="big-blue-header">
+          <div className="grid-container">
+            <h1 tabIndex="-1">Welcome, {user.name}</h1>
           </div>
         </div>
-      </section>
+        <section className="usa-section grid-container">
+          <SuccessNotification />
+          <ErrorNotification />
+          <div className="grid-row grid-gap taxpayer-tools">
+            <div className="tablet:grid-col-8">
+              {helper.showWhatToExpect && <WhatToExpect />}
+              {helper.showCaseList && <CaseListPetitioner />}
+            </div>
+            <div className="tablet:grid-col-4">
+              <div className="card">
+                <div className="content-wrapper gray">
+                  <h3>Taxpayer Tools</h3>
+                  <hr />
+                  <p>
+                    <FontAwesomeIcon
+                      icon="file-pdf"
+                      size="1x"
+                      className="fa-icon-blue"
+                    />
+                    <a
+                      href={howToPrepareYourDocuments}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Petition Filing Guide
+                    </a>
+                  </p>
+                  <p>
+                    <a
+                      href="https://www.ustaxcourt.gov/dpt_cities.htm"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Find a court location
+                    </a>
+                    <FontAwesomeIcon
+                      icon="external-link-alt"
+                      size="sm"
+                      className="fa-icon-blue margin-left-05"
+                    />
+                  </p>
+                  <p>
+                    <a
+                      href="https://www.ustaxcourt.gov/forms.htm"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      View forms
+                    </a>
+                    <FontAwesomeIcon
+                      icon="external-link-alt"
+                      size="sm"
+                      className="fa-icon-blue margin-left-05"
+                    />
+                  </p>
+                </div>
+              </div>
+              <div className="card">
+                <div className="content-wrapper gray">
+                  <h3>Other Filing Options</h3>
+                  <hr />
+                  <p>
+                    <a href="/" target="_blank" rel="noopener noreferrer">
+                      How to file a case by mail or in person
+                    </a>
+                    <FontAwesomeIcon
+                      icon="external-link-alt"
+                      size="sm"
+                      className="fa-icon-blue margin-left-05"
+                    />
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </React.Fragment>
     );
   },
 );
