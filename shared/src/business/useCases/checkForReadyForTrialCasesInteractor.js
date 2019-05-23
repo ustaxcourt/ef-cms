117951a201492ef6da566061cee7768bf4855a3b
William Bagayoko <wbagayoko@gmail.com>
2019-05-23 16:52:53 -0500

    1187 Case Catalog

--- shared/src/business/useCases/checkForReadyForTrialCasesInteractor.js
+++ shared/src/business/useCases/checkForReadyForTrialCasesInteractor.js
@@ -1,7 +1,40 @@
+const { Case, STATUS_TYPES } = require('../entities/Case');
+
 /**
  * @param applicationContext
  * @returns {Promise<*>}
  */
 exports.checkForReadyForTrialCases = async ({ applicationContext }) => {
+  applicationContext.logger.info('Time', new Date().toISOString());
+
+  const caseCatalog = await applicationContext
+    .getPersistenceGateway()
+    .getAllCatalogCases({
+      applicationContext,
+    });
+
+  for (let caseRecord of caseCatalog) {
+    const caseId = caseRecord.caseId;
+    const caseToCheck = await applicationContext
+      .getPersistenceGateway()
+      .getCaseByCaseId({
+        applicationContext,
+        caseId,
+      });
+
+    const caseEntity = new Case(caseToCheck);
+
+    if (caseEntity.status === STATUS_TYPES.generalDocket) {
+      caseEntity.checkForReadyForTrialCases();
+
+      if (caseEntity.status === STATUS_TYPES.generalDocketReadyForTrial) {
+        await applicationContext.getPersistenceGateway().updateCase({
+          applicationContext,
+          caseToUpdate: caseEntity.validate().toRawObject(),
+        });
+      }
+    }
+  }
+
   applicationContext.logger.info('Time', new Date().toISOString());
 };
