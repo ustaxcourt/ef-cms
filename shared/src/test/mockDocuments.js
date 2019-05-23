34ae4892e2f330a34f7c050118ec0bf4506440a5
William Bagayoko <wbagayoko@gmail.com>
2019-05-23 17:16:32 -0500

    1187 Update Tests
    * http endpoint

--- shared/src/test/mockDocuments.js
+++ shared/src/test/mockDocuments.js
@@ -23,6 +23,7 @@ exports.MOCK_DOCUMENTS = [
     createdAt: '2018-11-21T20:49:28.192Z',
     documentId: 'e6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
     documentType: 'Answer',
+    eventCode: 'A',
     processingStatus: 'pending',
     reviewDate: '2018-11-21T20:49:28.192Z',
     reviewUser: 'petitionsclerk',
