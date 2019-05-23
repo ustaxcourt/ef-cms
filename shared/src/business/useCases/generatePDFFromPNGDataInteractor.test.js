851c1d944b2b4da90ed67f61d3a24373e9a3bd80
Matt Hopson <matthopson@gmail.com>
2019-05-23 12:42:40 -0600

    Add some tests for generatePDFFromPNGData

---
+++ shared/src/business/useCases/generatePDFFromPNGDataInteractor.test.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const sinon = require('sinon');
+const {
+  generatePDFFromPNGData,
+} = require('./generatePDFFromPNGDataInteractor.js');
+const { PDFDocumentFactory } = require('pdf-lib');
+
+const testAssetsPath = path.join(__dirname, '../../../test-assets/');
+const testOutputPath = path.join(__dirname, '../../../test-output/');
+
+function testImgDocBytes() {
+  return fs.readFileSync(testAssetsPath + 'sample.png');
+}
+
+describe('generatePDFFromPNGData', () => {
+  let testImg;
+  beforeEach(() => {
+    testImg = testImgDocBytes();
+  });
+
+  it('creates a pdf document from the specified img data', async () => {
+    const newPdfData = await generatePDFFromPNGData([testImg, testImg]);
+
+    fs.writeFile(testOutputPath + 'generatePDFFromPNGData.pdf', newPdfData);
+
+    const newPdfDoc = PDFDocumentFactory.load(newPdfData);
+    const newPdfDocPages = newPdfDoc.getPages();
+    expect(newPdfDocPages.length).toEqual(2);
+  });
+});
