cfb19ecb793e1ee05ac97882f112169a57302aee
Kris Koskelin <kkoskelin@flexion.us>
2019-05-24 11:41:58 -0500

    More pdf progress
    [skip ci]

--- efcms-service/src/tools/pdf-to-ps.js
+++ efcms-service/src/tools/pdf-to-ps.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const gs = require('ghostscript4js');
 
 /*
@@ -10,23 +11,57 @@ Add the following to your ~/.bash_profile or ~/.bashrc
 `export GS4JS_HOME="/usr/local/lib"` 
 const gs = require('ghostscript4js');
 
+consult docs at https://www.ghostscript.com/doc/current/VectorDevices.htm#Extensions
+
+Convert PDF to PS:
+gs -dBATCH -dSAFER -dNOPAUSE -q -sDEVICE=ps2write -sOutputFile=intermediate.ps -f FILENAME
+
+Convert PS to PDF:
+gs -dBATCH -dSAFER -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=pdf-from-ps.pdf -dPDFSETTINGS=/prepress -dHaveTrueTypes=true -dEmbedAllFonts=true -dSubsetFonts=false -f example.ps
 */
 
 const args = process.argv.slice(2);
 const [inputFile, outputFile='output.pdf'] = args;
 
-const pdf2ps = `-dBATCH -dSAFER -dNOPAUSE -q -sDEVICE=ps2write -sOutputFile=intermediate.ps -f ${inputFile}`;
-const ps2pdf = `-dBATCH -dSAFER -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=${outputFile} -dPDFSETTINGS=/prepress -dHaveTrueTypes=true -dEmbedAllFonts=true -dSubsetFonts=false -f intermediate.ps`;
+const pdf2psArgs = ['-dBATCH',
+  '-dSAFER',
+  '-dNOPAUSE',
+  '-q',
+  '-sDEVICE=ps2write',
+  '-sOutputFile=intermediate.ps',
+  `-f ${inputFile}`,
+];
+
+const ps2pdfArgs = [
+  '-dQUIET',
+  '-dBATCH',
+  '-dSAFER',
+  '-dNOPAUSE',
+  '-dNOCACHE',
+  '-sDEVICE=pdfwrite',
+  '-dPDFSETTINGS=/prepress',
+  '-sColorConversionStrategy=/LeaveColorUnchanged',
+  '-dAutoFilterColorImages=true',
+  '-dAutoFilterGrayImages=true',
+  '-dDownsampleMonoImages=true',
+  '-dDownsampleGrayImages=true',
+  '-dDownsampleColorImages=true',
+  `-sOutputFile=${outputFile}`,
+  '-f intermediate.ps',
+];
+
+const ps2pdf_cmd = ps2pdfArgs.join(' ');
+const pdf2ps_cmd = pdf2psArgs.join(' ');
 
 try {
   // Take decision based on Ghostscript version
   const version = gs.version();
   console.log('GS Version', version);
 
-  gs.executeSync(pdf2ps);
+  gs.executeSync(pdf2ps_cmd);
   console.log('Created PS intermediate.ps');
 
-  gs.executeSync(ps2pdf);
+  gs.executeSync(ps2pdf_cmd);
   console.log(`Created PDF ${outputFile}`);
 } catch (err) {
   console.log(err, err.message);
