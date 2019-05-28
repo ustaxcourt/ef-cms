f7de88647da97894b743aa4136ed31d491df8ef2
Eric Sorenson <eric@sorenson.software>
2019-05-28 14:19:12 -0500

    added virus scan interactor

--- efcms-service/src/documents/virusScanLambda.js
+++ efcms-service/src/documents/virusScanLambda.js
@@ -1,43 +1,29 @@
 const createApplicationContext = require('../applicationContext');
 const {
   handle,
-  // getUserFromAuthHeader,
+  getUserFromAuthHeader,
 } = require('../middleware/apiGatewayHelper');
-const { promises: fs } = require('fs');
 
+/**
+ * creates a new document and attaches it to a case.  It also creates a work item on the docket section.
+ *
+ * @param {Object} event the AWS event object
+ * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
+ */
 exports.handler = event =>
   handle(event, async () => {
-    // const user = getUserFromAuthHeader(event);
-    const applicationContext = createApplicationContext({
-      at_hash: 'sgapA2ZMWplnvqZDxFYEsQ',
-      aud: '6tu6j1stv5ugcut7dqsqdurn8q',
-      auth_time: 1550154248,
-      'cognito:username': 'a46afe60-ad3a-47ae-bd49-43d6d62aa469',
-      'custom:role': 'petitioner',
-      email: 'petitioner1@example.com',
-      email_verified: false,
-      event_id: '30b0b2b2-3064-11e9-994b-75200a6e47a3',
-      exp: 1550157848,
-      iat: 1550154248,
-      iss: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_7uRkF0Axn',
-      name: 'Test Petitioner',
-      sub: 'a46afe60-ad3a-47ae-bd49-43d6d62aa469',
-      token_use: 'id',
-    });
     const documentId = event.pathParameters.documentId;
-
+    const user = getUserFromAuthHeader(event);
+    const applicationContext = createApplicationContext(user);
     try {
-      // download S3 doc to /tmp dir
-      const documentObject = await applicationContext
-        .getPersistenceGateway()
-        .getDocument({
-          applicationContext,
-          documentId,
-        });
-
-      await fs.writeFile('/tmp/document.pdf', documentObject);
-
-      return 'Welcome to Flavortown.';
+      const results = await applicationContext.getUseCases().virusScanDocument({
+        applicationContext,
+        documentId,
+        user: JSON.parse(event.body),
+      });
+      applicationContext.logger.info('User', user);
+      applicationContext.logger.info('Results', results);
+      return results;
     } catch (e) {
       applicationContext.logger.error(e);
       throw e;
