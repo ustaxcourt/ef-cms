c28826f9cdedfcbae1d6d69a0145f02a0e41cda9
Joseph Sutton <jsutton@flexion.us>
2019-05-28 15:02:04 -0500

    fixed typo in persistence method

--- efcms-service/src/documents/virusScanLambda.js
+++ efcms-service/src/documents/virusScanLambda.js
@@ -13,8 +13,23 @@ const {
 exports.handler = event =>
   handle(event, async () => {
     const documentId = event.pathParameters.documentId;
-    const user = getUserFromAuthHeader(event);
-    const applicationContext = createApplicationContext(user);
+    // const user = getUserFromAuthHeader(event);
+    const applicationContext = createApplicationContext({
+      at_hash: 'sgapA2ZMWplnvqZDxFYEsQ',
+      aud: '6tu6j1stv5ugcut7dqsqdurn8q',
+      auth_time: 1550154248,
+      'cognito:username': 'a46afe60-ad3a-47ae-bd49-43d6d62aa469',
+      'custom:role': 'petitioner',
+      email: 'petitioner1@example.com',
+      email_verified: false,
+      event_id: '30b0b2b2-3064-11e9-994b-75200a6e47a3',
+      exp: 1550157848,
+      iat: 1550154248,
+      iss: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_7uRkF0Axn',
+      name: 'Test Petitioner',
+      sub: 'a46afe60-ad3a-47ae-bd49-43d6d62aa469',
+      token_use: 'id',
+    });
     try {
       const results = await applicationContext.getUseCases().virusScanDocument({
         applicationContext,
