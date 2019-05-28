73a24848c9f8b943a4b0db6db349bddd09fbf5fa
Matt Hopson <matthopson@gmail.com>
2019-05-28 11:30:10 -0600

    Dynamically inject scanner scripts

--- web-client/src/presenter/actions/scannerShutdownAction.js
+++ web-client/src/presenter/actions/scannerShutdownAction.js
@@ -1,3 +1,13 @@
-export const scannerShutdownAction = () => {
-  console.log('scanner shutdown');
+import { state } from 'cerebral';
+
+export const scannerShutdownAction = ({ store, get }) => {
+  const dynanScriptClass = get(state.scanner.dynanScriptClass);
+  if (dynanScriptClass) {
+    const injectedScripts = Array.from(
+      document.getElementsByClassName(dynanScriptClass),
+    );
+    injectedScripts.map(scriptEl => scriptEl.remove());
+  }
+  store.set(state.scanner.initiateScriptLoaded, false);
+  store.set(state.scanner.configScriptLoaded, false);
 };
