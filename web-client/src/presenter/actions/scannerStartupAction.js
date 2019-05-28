575beabb958e2c6ee0fbeaf965f61ef87dfa30ce
Matt Hopson <matthopson@gmail.com>
2019-05-28 10:06:31 -0600

    Refactor Scan component and stub code injection seq

---
+++ web-client/src/presenter/actions/scannerStartupAction.js
@@ -0,0 +1,3 @@
+export const scannerStartupAction = () => {
+  console.log('scanner startup');
+};
--- web-client/src/ustc-ui/Scan/Scan.jsx
+++ web-client/src/ustc-ui/Scan/Scan.jsx
@@ -1,10 +1,19 @@
+import { PropTypes } from 'prop-types';
 import { connect } from '@cerebral/react';
+import { sequences } from 'cerebral';
 import { state } from 'cerebral';
 import React from 'react';
 
-export const Scan = connect(
-  { isScanning: state.isScanning },
-  ({ isScanning, onDoneClicked, onScanClicked }) => {
+class ScanComponent extends React.Component {
+  componentWillMount() {
+    this.props.scannerStartup();
+  }
+
+  componentWillUnmount() {
+    this.props.scannerShutdown();
+  }
+  render() {
+    const { isScanning, onDoneClicked, onScanClicked } = this.props;
     return (
       <>
         <button
@@ -31,5 +40,22 @@ export const Scan = connect(
         )}
       </>
     );
+  }
+}
+
+ScanComponent.propTypes = {
+  isScanning: PropTypes.bool,
+  onDoneClicked: PropTypes.func,
+  onScanClicked: PropTypes.func,
+  scannerShutdown: PropTypes.func,
+  scannerStartup: PropTypes.func,
+};
+
+export const Scan = connect(
+  {
+    isScanning: state.isScanning,
+    scannerShutdown: sequences.scannerShutdownSequence,
+    scannerStartup: sequences.scannerStartupSequence,
   },
+  ScanComponent,
 );
