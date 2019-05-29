0bb263a41b2abed41fceb1f402c25904a2c7ac87
Joseph Sutton <jsutton@flexion.us>
2019-05-29 15:05:56 -0500

    addressed shellcheck

--- efcms-service/docker-clamav.sh
+++ efcms-service/docker-clamav.sh
@@ -2,4 +2,4 @@
 
 set -e
 docker pull amazonlinux:2
-docker run --rm -ti -v `pwd`/runtimes/clamav:/opt/app amazonlinux:2 /bin/bash -c "cd /opt/app && chmod +x ./build.sh && ./build.sh"
+docker run --rm -ti -v $(pwd)/runtimes/clamav:/opt/app amazonlinux:2 /bin/bash -c "cd /opt/app && chmod +x ./build.sh && ./build.sh"
