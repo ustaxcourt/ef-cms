4a496a640ccf0f77de3c61d18eb4981cde7ad81a
Joseph Sutton <jsutton@flexion.us>
2019-05-29 14:49:41 -0500

    added clamav lambda layer

---
+++ efcms-service/docker-clamav.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -e
+docker pull amazonlinux:2
+docker run --rm -ti -v `pwd`/runtimes/clamav:/opt/app amazonlinux:2 /bin/bash -c "cd /opt/app && chmod +x ./build.sh && ./build.sh"
