52e2bbec8525843077b1bd71fe0d4f56aa3b70ed
William Bagayoko <wbagayoko@gmail.com>
2019-05-30 16:21:58 -0500

    1071 form updates [skip ci]
    * Update form fields to display Exhibits/Attachments/Objections based off of document selection (William)
    * Add supporting document form for Motion document

--- web-client/src/presenter/computeds/requestAccessHelper.js
+++ web-client/src/presenter/computeds/requestAccessHelper.js
@@ -4,6 +4,7 @@ export const requestAccessHelper = (get, applicationContext) => {
   const { PARTY_TYPES } = get(state.constants);
   const caseDetail = get(state.caseDetail);
   const form = get(state.form);
+  const documentType = get(state.form.documentType);
   const validationErrors = get(state.validationErrors);
   const showSecondaryParty =
     caseDetail.partyType === PARTY_TYPES.petitionerSpouse ||
@@ -17,12 +18,43 @@ export const requestAccessHelper = (get, applicationContext) => {
       .formatDateString(certificateOfServiceDate, 'MMDDYY');
   }
 
+  const documentWithExhibits =
+    [
+      'Motion to Substitute Parties and Change Caption',
+      'Notice of Intervention',
+      'Notice of Election to Participate',
+      'Notice of Election to Intervene',
+    ].indexOf(documentType) !== -1;
+
+  const documentWithAttachments =
+    [
+      'Motion to Substitute Parties and Change Caption',
+      'Notice of Intervention',
+      'Notice of Election to Participate',
+      'Notice of Election to Intervene',
+    ].indexOf(documentType) !== -1;
+
+  const documentWithObjections =
+    [
+      'Substitution of Counsel',
+      'Motion to Substitute Parties and Change Caption',
+    ].indexOf(documentType) !== -1;
+
+  const documentWithSupportingDocuments =
+    ['Motion to Substitute Parties and Change Caption'].indexOf(
+      documentType,
+    ) !== -1;
+
   const partyValidationError =
     validationErrors.representingPrimary ||
     validationErrors.representingSecondary;
 
   let exported = {
     certificateOfServiceDateFormatted,
+    documentWithAttachments,
+    documentWithExhibits,
+    documentWithObjections,
+    documentWithSupportingDocuments,
     partyValidationError,
     showPrimaryDocumentValid: !!form.primaryDocumentFile,
     showSecondaryParty,
