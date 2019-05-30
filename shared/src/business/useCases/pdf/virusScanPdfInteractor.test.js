20781dc2e69507009318e86a372407f1d163b15a
Eric Sorenson <eric@sorenson.software>
2019-05-30 14:52:44 -0500

    clamscan interactor working and tested

---
+++ shared/src/business/useCases/pdf/virusScanPdfInteractor.test.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const path = require('path');
+const sinon = require('sinon');
+const { virusScanPdf } = require('./virusScanPdfInteractor');
+
+const testAssetsPath = path.join(__dirname, '../../../../test-assets/');
+
+function testAsset(name) {
+  return fs.readFileSync(testAssetsPath + name);
+}
+
+describe('virusScanPdf', () => {
+  jest.setTimeout(60000);
+
+  it('detects a clean PDF', async () => {
+    const cleanParams = {
+      applicationContext: {
+        environment: { documentsBucketName: 'documents' },
+        getStorageClient: () => ({
+          getObject: sinon.stub().returns({
+            promise: async () => ({
+              Body: testAsset('sample.pdf'),
+            }),
+          }),
+        }),
+        logger: {
+          time: () => null,
+          timeEnd: () => null,
+        },
+      },
+      documentId: 'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
+    };
+    const result = await virusScanPdf(cleanParams);
+    expect(result.viruses).toHaveLength(0);
+  });
+
+  it('detects an infected PDF', async () => {
+    const infectedParams = {
+      applicationContext: {
+        environment: { documentsBucketName: 'documents' },
+        getStorageClient: () => ({
+          getObject: sinon.stub().returns({
+            promise: async () => ({
+              Body: testAsset('fake-virus.pdf'),
+            }),
+          }),
+        }),
+        logger: {
+          time: () => null,
+          timeEnd: () => null,
+        },
+      },
+      documentId: 'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
+    };
+    const result = await virusScanPdf(infectedParams);
+    expect(result.viruses).toHaveLength(1);
+  });
+});
