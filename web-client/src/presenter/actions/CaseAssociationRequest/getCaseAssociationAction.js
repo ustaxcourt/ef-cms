11898c7bf0b3326cef194239594515797e96310c
William Bagayoko <wbagayoko@gmail.com>
2019-05-31 12:40:45 -0500

    1071 practitioner pending access UI
    * client-side pending association request

---
+++ web-client/src/presenter/actions/CaseAssociationRequest/getCaseAssociationAction.js
@@ -0,0 +1,33 @@
+import { some } from 'lodash';
+import { state } from 'cerebral';
+
+/**
+ * Determines if there is a pending association of the practitioner to the case
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.get the cerebral get function to retrieve state values
+ * @param {object} providers.applicationContext needed for getting the getCase use case
+ * @param {object} providers.props the cerebral props object containing props.docketNumber
+ * @returns {object} contains the association returned from the use case
+ */
+export const getCaseAssociationAction = async ({ applicationContext, get }) => {
+  const caseDetailPractitioners = get(state.caseDetail.practitioners);
+  const userId = get(state.user.userId);
+  const caseId = get(state.caseDetail.caseId);
+
+  if (some(caseDetailPractitioners, { userId })) {
+    return { isAssociated: true };
+  }
+
+  if (
+    await applicationContext.getUseCases().verifyPendingCaseForUser({
+      applicationContext,
+      caseId,
+      userId,
+    })
+  ) {
+    return { pendingAssociation: true };
+  }
+
+  return { notAssociated: true };
+};
