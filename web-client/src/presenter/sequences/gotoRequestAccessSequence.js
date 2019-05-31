4afc4ae16d6ee6561f53f75aade927f2efdaf053
William Bagayoko <wbagayoko@gmail.com>
2019-05-31 13:26:22 -0500

    1071 practitioner pending access UI
    * Request Access button is hidden
    * Label is displayed “Request for Access Pending”

--- web-client/src/presenter/sequences/gotoRequestAccessSequence.js
+++ web-client/src/presenter/sequences/gotoRequestAccessSequence.js
@@ -2,10 +2,12 @@ import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { clearFormAction } from '../actions/clearFormAction';
 import { clearScreenMetadataAction } from '../actions/clearScreenMetadataAction';
 import { getCaseAction } from '../actions/getCaseAction';
+import { getCaseAssociationAction } from '../actions/CaseAssociationRequest/getCaseAssociationAction';
 import { isLoggedInAction } from '../actions/isLoggedInAction';
 import { redirectToCognitoAction } from '../actions/redirectToCognitoAction';
 import { set } from 'cerebral/factories';
 import { setCaseAction } from '../actions/setCaseAction';
+import { setCaseAssociationAction } from '../actions/CaseAssociationRequest/setCaseAssociationAction';
 import { setCurrentPageAction } from '../actions/setCurrentPageAction';
 import { state } from 'cerebral';
 
@@ -17,6 +19,8 @@ const gotoRequestAccess = [
   clearScreenMetadataAction,
   getCaseAction,
   setCaseAction,
+  getCaseAssociationAction,
+  setCaseAssociationAction,
   set(state.wizardStep, 'RequestAccess'),
   setCurrentPageAction('RequestAccessWizard'),
 ];
