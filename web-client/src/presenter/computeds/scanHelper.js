eefde3193feb02a2dd8f87afcd8034b3b0f69774
Matt Hopson <matthopson@gmail.com>
2019-06-03 10:36:39 -0600

    Disable scan feature for now

--- web-client/src/presenter/computeds/scanHelper.js
+++ web-client/src/presenter/computeds/scanHelper.js
@@ -1,6 +1,9 @@
 import { state } from 'cerebral';
 
 export const scanHelper = get => {
+  // Master switch for the time being
+  const scanFeatureEnabled = false;
+
   const internalRoles = ['petitionsclerk', 'docketclerk', 'seniorattorney'];
   const user = get(state.user);
   const initiateScriptLoaded = get(state.scanner.initiateScriptLoaded);
@@ -8,7 +11,12 @@ export const scanHelper = get => {
 
   return {
     hasLoadedScanDependencies: initiateScriptLoaded && configScriptLoaded,
-    hasScanFeature: !!(user && user.role && internalRoles.includes(user.role)),
+    hasScanFeature: !!(
+      user &&
+      user.role &&
+      internalRoles.includes(user.role) &&
+      scanFeatureEnabled
+    ),
     showScannerSourceModal: get(state.showModal) === 'SelectScannerSourceModal',
     sources: get(state.scanner.sources),
   };
