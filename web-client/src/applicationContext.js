b1f1d2f90a14ca342fd5ec0e70de6e0e8830ea9b
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-06-03 18:25:33 -0700

    1191: endpoint, interactor, and persistence method to get trial sessions

--- web-client/src/applicationContext.js
+++ web-client/src/applicationContext.js
@@ -81,6 +81,7 @@ import { getProcedureTypes } from '../../shared/src/business/useCases/getProcedu
 import { getScannerInterface } from '../../shared/src/business/useCases/getScannerInterfaceInteractor';
 import { getSentWorkItemsForSection } from '../../shared/src/proxies/workitems/getSentWorkItemsForSectionProxy';
 import { getSentWorkItemsForUser } from '../../shared/src/proxies/workitems/getSentWorkItemsForUserProxy';
+import { getTrialSessions } from '../../shared/src/proxies/trialSessions/getTrialSessionsProxy';
 import { getUser } from '../../shared/src/business/useCases/getUserInteractor';
 import { getUsersInSection } from '../../shared/src/proxies/users/getUsersInSectionProxy';
 import { getWorkItem } from '../../shared/src/proxies/workitems/getWorkItemProxy';
@@ -162,6 +163,7 @@ const allUseCases = {
   getProcedureTypes,
   getSentWorkItemsForSection,
   getSentWorkItemsForUser,
+  getTrialSessions,
   getUser,
   getUsersInSection,
   getWorkItem,
