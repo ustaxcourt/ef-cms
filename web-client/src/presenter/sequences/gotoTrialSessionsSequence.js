05d3445095f5b54e4ca6dd13d1778ed59d593eb9
Kris Koskelin <kkoskelin@flexion.us>
2019-06-04 16:34:11 -0500

    1191: view progress

--- web-client/src/presenter/sequences/gotoTrialSessionsSequence.js
+++ web-client/src/presenter/sequences/gotoTrialSessionsSequence.js
@@ -1,10 +1,10 @@
 import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { clearErrorAlertsAction } from '../actions/clearErrorAlertsAction';
-import { getTrialSessionsAction } from '../actions/getTrialSessionsAction';
+import { getTrialSessionsAction } from '../actions/TrialSessions/getTrialSessionsAction';
 import { isLoggedInAction } from '../actions/isLoggedInAction';
 import { redirectToCognitoAction } from '../actions/redirectToCognitoAction';
 import { setCurrentPageAction } from '../actions/setCurrentPageAction';
-import { setTrialSessionsAction } from '../actions/setTrialSessionsAction';
+import { setTrialSessionsAction } from '../actions/TrialSessions/setTrialSessionsAction';
 
 const gotoTrialSessions = [
   setCurrentPageAction('Interstitial'),
