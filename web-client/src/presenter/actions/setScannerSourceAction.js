253018a81d0b3dbbc68a81cd030d5b5d5e2744b1
Eric Sorenson <eric@sorenson.software>
2019-06-05 16:00:50 -0500

    fix linter

--- web-client/src/presenter/actions/setScannerSourceAction.js
+++ web-client/src/presenter/actions/setScannerSourceAction.js
@@ -3,8 +3,8 @@ import { state } from 'cerebral';
 /**
  * sets a scanner source
  *
- * @param {Object} providers the providers object
- * @param {Object} providers.applicationContext the application context used for getting the scanner API
+ * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context used for getting the scanner API
  * @param {Function} providers.props the cerebral props object used for getting the props.scannerSourceName
  * @param {Function} providers.store the cerebral store used for setting state.scanner.scannerSourceName
  */
