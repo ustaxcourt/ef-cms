32a0f07c160e14773b5828649cd3e79ae9c2586e
Joseph Sutton <jsutton@flexion.us>
2019-06-07 09:08:45 -0500

    fixed unit test

--- shared/src/business/useCases/pdf/validatePdfInteractor.test.js
+++ shared/src/business/useCases/pdf/validatePdfInteractor.test.js
@@ -53,7 +53,6 @@ describe('validatePdf', () => {
       documentId: 'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
     };
 
-    const result = await validatePdf(infectedParams);
-    expect(result).toBe(false);
+    await expect(validatePdf(infectedParams)).rejects.toThrow();
   });
 });
