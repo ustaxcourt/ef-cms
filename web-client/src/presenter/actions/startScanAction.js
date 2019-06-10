1eb5b85f48b1e90f0f1e40cf76bbaabd8105c8ae
Cody Seibert <cseibert113@gmail.com>
2019-06-10 15:30:58 -0400

    scanner error case

--- web-client/src/presenter/actions/startScanAction.js
+++ web-client/src/presenter/actions/startScanAction.js
@@ -7,9 +7,29 @@ import { state } from 'cerebral';
  * @param {object} providers.applicationContext the application context used for getting the scanner API
  * @param {Function} providers.store the cerebral store used for setting state.path
  */
-export const startScanAction = async ({ applicationContext, store }) => {
+export const startScanAction = async ({ applicationContext, store, props }) => {
   store.set(state.isScanning, true);
+
   const scanner = applicationContext.getScanner();
 
-  scanner.startScanSession();
+  if (
+    props.scannerSourceIndex &&
+    scanner.getSourceNameByIndex(props.scannerSourceIndex) ===
+      props.scannerSourceName
+  ) {
+    scanner.setSourceByIndex(props.scannerSourceIndex);
+    scanner.startScanSession();
+    store.set(state.submitting, false);
+  } else {
+    await applicationContext.getUseCases().removeItem({
+      applicationContext,
+      key: 'scannerSourceIndex',
+    });
+    await applicationContext.getUseCases().removeItem({
+      applicationContext,
+      key: 'scannerSourceName',
+    });
+    store.set(state.isScanning, false);
+    alert('there was an issue with the cached source; please scan again');
+  }
 };
