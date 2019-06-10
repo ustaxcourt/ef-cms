c4f7730cbbe406ae3b12ddfb0d3fa65e26cb25ed
Eric Sorenson <eric@sorenson.software>
2019-06-10 13:23:59 -0500

    disable validator

--- web-client/src/presenter/actions/createCaseAction.js
+++ web-client/src/presenter/actions/createCaseAction.js
@@ -60,22 +60,22 @@ export const createCaseAction = async ({
       documentId: document.documentId,
     });
 
-    const isValid = await applicationContext.getUseCases().validatePdf({
+    // disable for demo
+    // await applicationContext.getUseCases().validatePdf({
+    //   applicationContext,
+    //   documentId: document.documentId,
+    // });
+
+    await applicationContext.getUseCases().sanitizePdf({
       applicationContext,
       documentId: document.documentId,
     });
 
-    if (isValid) {
-      await applicationContext.getUseCases().sanitizePdf({
-        applicationContext,
-        documentId: document.documentId,
-      });
-      await applicationContext.getUseCases().createCoverSheet({
-        applicationContext,
-        caseId: caseDetail.caseId,
-        documentId: document.documentId,
-      });
-    }
+    await applicationContext.getUseCases().createCoverSheet({
+      applicationContext,
+      caseId: caseDetail.caseId,
+      documentId: document.documentId,
+    });
   }
 
   return path.success({
--- web-client/src/presenter/actions/createCaseFromPaperAction.js
+++ web-client/src/presenter/actions/createCaseFromPaperAction.js
@@ -118,22 +118,22 @@ export const createCaseFromPaperAction = async ({
       applicationContext,
       documentId: document.documentId,
     });
-    const isValid = await applicationContext.getUseCases().validatePdf({
+
+    // disable for demo
+    // await applicationContext.getUseCases().validatePdf({
+    //   applicationContext,
+    //   documentId: document.documentId,
+    // });
+
+    await applicationContext.getUseCases().sanitizePdf({
       applicationContext,
       documentId: document.documentId,
     });
-
-    if (isValid) {
-      await applicationContext.getUseCases().sanitizePdf({
-        applicationContext,
-        documentId: document.documentId,
-      });
-      await applicationContext.getUseCases().createCoverSheet({
-        applicationContext,
-        caseId: caseDetail.caseId,
-        documentId: document.documentId,
-      });
-    }
+    await applicationContext.getUseCases().createCoverSheet({
+      applicationContext,
+      caseId: caseDetail.caseId,
+      documentId: document.documentId,
+    });
   }
 
   return path.success({
