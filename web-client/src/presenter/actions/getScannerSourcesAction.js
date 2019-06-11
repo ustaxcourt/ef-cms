58206b21a0b2b5ad2cadd272b60dcc678f4a7a09
Eric Sorenson <eric@sorenson.software>
2019-06-11 13:36:41 -0500

    JSDoc fixes

--- web-client/src/presenter/actions/getScannerSourcesAction.js
+++ web-client/src/presenter/actions/getScannerSourcesAction.js
@@ -7,10 +7,7 @@ import { state } from 'cerebral';
  * @param {object} providers.applicationContext the application context used for getting the scanner API
  * @param {Function} providers.store the cerebral store used for setting state.scanner.sources
  */
-export const getScannerSourcesAction = async ({
-  applicationContext,
-  store,
-}) => {
+export const getScannerSourcesAction = ({ applicationContext, store }) => {
   const scanner = applicationContext.getScanner();
   const sources = scanner.getSources();
   store.set(state.scanner.sources, sources);
