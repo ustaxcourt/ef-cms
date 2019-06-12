dec08c587af00e659f4fdd6840ba71103bb733e7
Cody Seibert <cseibert113@gmail.com>
2019-06-12 10:50:54 -0400

    syntax issue

--- web-client/src/presenter/actions/startScanAction.js
+++ web-client/src/presenter/actions/startScanAction.js
@@ -7,6 +7,7 @@ import { state } from 'cerebral';
  * @param {object} providers.applicationContext the application context used for getting the scanner API
  * @param {Function} providers.store the cerebral store used for setting state.path
  *
+ */
 export const startScanAction = async ({ applicationContext, store, props }) => {
   store.set(state.isScanning, true);
 
