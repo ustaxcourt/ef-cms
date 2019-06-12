d7c199eec49b751996f2ac3ae1af810ba853d79a
William Bagayoko <wbagayoko@gmail.com>
2019-06-12 14:05:54 -0500

    1192 1192 Set Trial Session Calendar Use Case

---
+++ shared/src/business/useCases/trialSessions/setTrialSessionCalendarInteractor.test.js
@@ -0,0 +1,83 @@
+const {
+  setTrialSessionCalendar,
+} = require('./setTrialSessionCalendarInteractor');
+const { User } = require('../../entities/User');
+
+const { MOCK_CASE } = require('../../../test/mockCase');
+
+const MOCK_TRIAL = {
+  maxCases: 100,
+  sessionType: 'Regular',
+  startDate: '2025-12-01T00:00:00.000Z',
+  term: 'Fall',
+  termYear: '2025',
+  trialLocation: 'Birmingham, AL',
+};
+
+describe('setTrialSessionCalendarInteractor', () => {
+  let applicationContext;
+
+  it('throws an exception when it fails to find the cases for a trial session', async () => {
+    applicationContext = {
+      getCurrentUser: () => {
+        return new User({
+          name: 'Petitioner',
+          role: 'petitioner',
+          userId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+        });
+      },
+      getPersistenceGateway: () => ({
+        deleteCaseTrialSessionSortMappingRecords: () => {},
+        getEligibleCasesForTrialSession: () => [MOCK_CASE],
+        getTrialSessionById: () => MOCK_TRIAL,
+        updateCase: () => {},
+        updateTrialSession: () => {},
+      }),
+    };
+
+    let error;
+
+    try {
+      await setTrialSessionCalendar({
+        applicationContext,
+        trialSessionId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+      });
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).toBeDefined();
+  });
+
+  it('should find the cases for a trial session successfully', async () => {
+    applicationContext = {
+      getCurrentUser: () => {
+        return new User({
+          name: 'Docket Clerk',
+          role: 'docketclerk',
+          userId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+        });
+      },
+      getPersistenceGateway: () => ({
+        deleteCaseTrialSessionSortMappingRecords: () => {},
+        getEligibleCasesForTrialSession: () => [MOCK_CASE],
+        getTrialSessionById: () => MOCK_TRIAL,
+        updateCase: () => {},
+        updateTrialSession: () => {},
+      }),
+    };
+
+    let error;
+
+    try {
+      await setTrialSessionCalendar({
+        applicationContext,
+        trialSessionId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+      });
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).toBeUndefined();
+  });
+});
