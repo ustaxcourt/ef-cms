e604f81f94e00ae14e3df8999cc9195f7d66c85e
Matt Hopson <matthopson@gmail.com>
2019-06-12 13:27:28 -0600

    Set and get inbox counts in qc journey

--- web-client/e2e/journey/petitionsClerkViewsMyDocumentQC.js
+++ web-client/e2e/journey/petitionsClerkViewsMyDocumentQC.js
@@ -1,4 +1,7 @@
-export default test => {
+import { runCompute } from 'cerebral/test';
+import { workQueueHelper } from '../../src/presenter/computeds/workQueueHelper';
+
+export default (test, storeCount) => {
   return it('Petitions clerk views My Document QC', async () => {
     await test.runSequence('navigateToPathSequence', {
       path: '/document-qc/my/inbox',
@@ -9,5 +12,12 @@ export default test => {
     expect(workQueueIsInternal).toBeFalsy();
     expect(workQueueToDisplay.queue).toEqual('my');
     expect(workQueueToDisplay.box).toEqual('inbox');
+
+    if (storeCount) {
+      const helper = await runCompute(workQueueHelper, {
+        state: test.getState(),
+      });
+      test.petitionsClerkMyDocumentQCInboxCount = helper.inboxCount;
+    }
   });
 };
--- web-client/e2e/journey/petitionsClerkViewsSectionDocumentQC.js
+++ web-client/e2e/journey/petitionsClerkViewsSectionDocumentQC.js
@@ -1,4 +1,7 @@
-export default test => {
+import { runCompute } from 'cerebral/test';
+import { workQueueHelper } from '../../src/presenter/computeds/workQueueHelper';
+
+export default (test, storeCount) => {
   return it('Petitions clerk views Section Document QC', async () => {
     await test.runSequence('navigateToPathSequence', {
       path: '/document-qc/section/inbox',
@@ -9,5 +12,12 @@ export default test => {
     expect(workQueueIsInternal).toBeFalsy();
     expect(workQueueToDisplay.queue).toEqual('section');
     expect(workQueueToDisplay.box).toEqual('inbox');
+
+    if (storeCount) {
+      const helper = await runCompute(workQueueHelper, {
+        state: test.getState(),
+      });
+      test.petitionsClerkSectionDocumentQCInboxCount = helper.inboxCount;
+    }
   });
 };
