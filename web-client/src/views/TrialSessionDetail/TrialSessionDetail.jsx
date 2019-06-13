ecd3b7c3a4e8e6c3094214b95a2e5b35ca9f0dd5
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-06-13 11:31:49 -0700

    1192: eligible cases view

--- web-client/src/views/TrialSessionDetail/TrialSessionDetail.jsx
+++ web-client/src/views/TrialSessionDetail/TrialSessionDetail.jsx
@@ -1,6 +1,8 @@
 import { BigHeader } from '../BigHeader';
+import { EligibleCases } from './EligibleCases';
 import { ErrorNotification } from '../ErrorNotification';
 import { SuccessNotification } from '../SuccessNotification';
+import { Tab, Tabs } from '../../ustc-ui/Tabs/Tabs';
 import React from 'react';
 
 export const TrialSessionDetail = () => (
@@ -10,7 +12,20 @@ export const TrialSessionDetail = () => (
       <SuccessNotification />
       <ErrorNotification />
 
-      <div>Trial Session Info Goes Here</div>
+      <Tabs
+        defaultActiveTab="EligibleCases"
+        bind="trialsessiondetails.caseList"
+      >
+        <Tab
+          tabName="EligibleCases"
+          title="Eligible Cases"
+          id="eligible-cases-tab"
+        >
+          <div id="eligible-cases-tab-content">
+            <EligibleCases />
+          </div>
+        </Tab>
+      </Tabs>
     </section>
   </>
 );
