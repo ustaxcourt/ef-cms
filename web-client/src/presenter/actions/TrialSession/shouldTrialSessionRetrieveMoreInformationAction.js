7d689cb1d3a44275d7ed7208b81ae1e1197d09c5
William Bagayoko <wbagayoko@gmail.com>
2019-06-14 17:41:06 -0500

    jsdocs fix

--- web-client/src/presenter/actions/TrialSession/shouldTrialSessionRetrieveMoreInformationAction.js
+++ web-client/src/presenter/actions/TrialSession/shouldTrialSessionRetrieveMoreInformationAction.js
@@ -1,7 +1,7 @@
 import { state } from 'cerebral';
 
 /**
- * used to determine is the trial session should retrieve eligible cases
+ * used to determine if the trial session should retrieve eligible cases
  *
  * @param {object} providers the providers object
  * @param {object} providers.path the next object in the path (this is defined in the sequence right after this action is invoked)
