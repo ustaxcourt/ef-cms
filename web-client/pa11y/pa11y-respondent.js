f1c70961ee2bfb9f8dc00ceccc0f3d838d571656
William Bagayoko <wbagayoko@gmail.com>
2019-06-19 17:22:12 -0500

    1889 update pa11y test

--- web-client/pa11y/pa11y-respondent.js
+++ web-client/pa11y/pa11y-respondent.js
@@ -3,12 +3,12 @@ module.exports = [
   'http://localhost:1234/mock-login?token=respondent&path=/case-detail/101-19',
   {
     actions: [
-      'wait for #button-request-access to be visible',
-      'click element #button-request-access',
+      'wait for #button-first-irs-document to be visible',
+      'click element #button-first-irs-document',
       'wait for #file-a-document-header to be visible',
     ],
     notes: 'checks a11y of revealed form for filing document',
     url:
-      'http://localhost:1234/mock-login?token=respondent&path=/case-detail/103-19&info=can-file-document',
+      'http://localhost:1234/mock-login?token=respondent&path=/case-detail/102-19&info=can-file-document',
   },
 ];
