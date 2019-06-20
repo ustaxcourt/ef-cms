3342f2adb8f0dd8ddb9ff305744bf87f20c3ad82
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-06-20 10:27:53 -0700

    1889: journey test for associating respondents to cases

---
+++ web-client/e2e/journey/respondentFilesDocumentForAssociatedCase.js
@@ -0,0 +1,90 @@
+export default (test, fakeFile) => {
+  return it('Respondent files document for associated case', async () => {
+    await test.runSequence('gotoCaseDetailSequence', {
+      docketNumber: test.docketNumber,
+    });
+
+    await test.runSequence('gotoFileDocumentSequence', {
+      docketNumber: test.docketNumber,
+    });
+
+    await test.runSequence('updateFileDocumentWizardFormValueSequence', {
+      key: 'category',
+      value: 'Miscellaneous',
+    });
+
+    await test.runSequence('updateFileDocumentWizardFormValueSequence', {
+      key: 'documentType',
+      value: 'Civil Penalty Approval Form',
+    });
+
+    await test.runSequence('validateSelectDocumentTypeSequence');
+
+    expect(test.getState('validationErrors')).toEqual({});
+
+    await test.runSequence('selectDocumentSequence');
+
+    expect(test.getState('form.documentType')).toEqual(
+      'Civil Penalty Approval Form',
+    );
+
+    expect(test.getState('form.partyPrimary')).toEqual(undefined);
+
+    await test.runSequence('updateFileDocumentWizardFormValueSequence', {
+      key: 'certificateOfService',
+      value: true,
+    });
+
+    await test.runSequence('updateFileDocumentWizardFormValueSequence', {
+      key: 'hasSupportingDocuments',
+      value: false,
+    });
+
+    await test.runSequence('updateFileDocumentWizardFormValueSequence', {
+      key: 'attachments',
+      value: false,
+    });
+    await test.runSequence('updateFileDocumentWizardFormValueSequence', {
+      key: 'exhibits',
+      value: false,
+    });
+    await test.runSequence('updateFileDocumentWizardFormValueSequence', {
+      key: 'objections',
+      value: 'no',
+    });
+
+    await test.runSequence('updateFileDocumentWizardFormValueSequence', {
+      key: 'certificateOfServiceMonth',
+      value: '12',
+    });
+    await test.runSequence('updateFileDocumentWizardFormValueSequence', {
+      key: 'certificateOfServiceDay',
+      value: '12',
+    });
+    await test.runSequence('updateFileDocumentWizardFormValueSequence', {
+      key: 'certificateOfServiceYear',
+      value: '2000',
+    });
+
+    await test.runSequence('updateFileDocumentWizardFormValueSequence', {
+      key: 'primaryDocumentFile',
+      value: fakeFile,
+    });
+
+    await test.runSequence('updateFileDocumentWizardFormValueSequence', {
+      key: 'partyPrimary',
+      value: true,
+    });
+
+    await test.runSequence('updateFileDocumentWizardFormValueSequence', {
+      key: 'partyRespondent',
+      value: true,
+    });
+
+    await test.runSequence('reviewExternalDocumentInformationSequence');
+
+    expect(test.getState('validationErrors')).toEqual({});
+
+    await test.runSequence('submitExternalDocumentSequence');
+  });
+};
---
+++ web-client/e2e/journey/respondentViewsCaseDetailOfBatchedCase.js
@@ -0,0 +1,21 @@
+import { runCompute } from 'cerebral/test';
+
+import { caseDetailHelper } from '../../src/presenter/computeds/caseDetailHelper';
+
+export default test => {
+  return it('Respondent views case detail', async () => {
+    test.setState('caseDetail', {});
+    await test.runSequence('gotoCaseDetailSequence', {
+      docketNumber: test.docketNumber,
+    });
+    expect(test.getState('currentPage')).toEqual('CaseDetail');
+    expect(test.getState('caseDetail.docketNumber')).toEqual(test.docketNumber);
+    expect(test.getState('caseDetail.status')).toEqual('Batched for IRS');
+    expect(test.getState('caseDetail.documents').length).toEqual(2);
+
+    const helper = runCompute(caseDetailHelper, {
+      state: test.getState(),
+    });
+    expect(helper.showActionRequired).toEqual(false);
+  });
+};
