da511da02753f447a0189a525b1c654ab9e40573
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-06-20 07:00:23 -0700

    testing text components wip

---
+++ web-client/src/views/CreateOrder/CreateOrder.jsx
@@ -0,0 +1,33 @@
+import { BigHeader } from '../BigHeader';
+import { ErrorNotification } from '../ErrorNotification';
+import { SuccessNotification } from '../SuccessNotification';
+import { TextEditor } from './TextEditor';
+import { connect } from '@cerebral/react';
+import React from 'react';
+
+export const CreateOrder = connect(
+  {},
+  () => {
+    return (
+      <>
+        <BigHeader text="Create Order" />
+        <section className="usa-section grid-container DocumentDetail">
+          <h2 className="heading-1">A Header</h2>
+          <SuccessNotification />
+          <ErrorNotification />
+
+          <div className="grid-container padding-x-0">
+            <div className="grid-row grid-gap">
+              <div className="grid-col-5">
+                <div className="blue-container">
+                  <TextEditor />
+                </div>
+              </div>
+              <div className="grid-col-7"></div>
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  },
+);
