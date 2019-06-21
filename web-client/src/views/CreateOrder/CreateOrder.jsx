a0af6f7d14c99b8960c5a3a3805cf688c1019ecd
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-06-21 08:11:53 -0700

    437: example for POC with quill react component

--- web-client/src/views/CreateOrder/CreateOrder.jsx
+++ web-client/src/views/CreateOrder/CreateOrder.jsx
@@ -16,16 +16,24 @@ export const CreateOrder = connect(
       <>
         <BigHeader text="Create Order" />
         <section className="usa-section grid-container DocumentDetail">
-          <h2 className="heading-1">A Header</h2>
+          <h2 className="heading-1">Header</h2>
           <SuccessNotification />
           <ErrorNotification />
 
-          <div className="blue-container">
-            <TextEditor
-              form={form}
-              updateFormValueSequence={updateFormValueSequence}
-            />
-            {form.richText}
+          <div className="grid-container padding-x-0">
+            <div className="grid-row grid-gap">
+              <div className="grid-col-8">
+                <div className="blue-container">
+                  <TextEditor
+                    form={form}
+                    updateFormValueSequence={updateFormValueSequence}
+                  />
+                  <br></br>
+                  {form.richText}
+                </div>
+              </div>
+              <div className="grid-col-4"></div>
+            </div>
           </div>
         </section>
       </>
