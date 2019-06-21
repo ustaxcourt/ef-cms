0fb6214706d93e1d27ee7338a05a8b6c3f8ed24f
Cody Seibert <cseibert113@gmail.com>
2019-06-20 22:44:34 -0400

    fixing tests

--- shared/src/business/entities/WorkItem.js
+++ shared/src/business/entities/WorkItem.js
@@ -244,9 +244,14 @@ WorkItem.prototype.setAsCompleted = function({ message, user }) {
 /**
  * complete the work item as the IRS user with the message 'Served on IRS'
  */
-WorkItem.prototype.setAsSentToIRS = function() {
+WorkItem.prototype.setAsSentToIRS = function({
+  batchedByUserId,
+  batchedByName,
+}) {
   this.completedAt = new Date().toISOString();
   this.completedMessage = 'Served on IRS';
+  this.completedBy = batchedByName;
+  this.completedByUserId = batchedByUserId;
 
   this.addMessage(
     new Message({
