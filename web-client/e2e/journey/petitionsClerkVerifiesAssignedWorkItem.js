a35c0e821fa1497a187d30e129f47ee4a954aa08
Eric Sorenson <eric@sorenson.software>
2019-06-24 14:51:04 -0500

    apply lint fixes to e2e dir

--- web-client/e2e/journey/petitionsClerkVerifiesAssignedWorkItem.js
+++ web-client/e2e/journey/petitionsClerkVerifiesAssignedWorkItem.js
@@ -1,7 +1,6 @@
 export default test => {
   return it('Petitions clerk verifies assignment of work item', async () => {
-    const workItemId =
-      test.taxpayerNewCases[0].documents[0].workItems[0].workItemId;
+    const { workItemId } = test.taxpayerNewCases[0].documents[0].workItems[0];
 
     await test.runSequence('gotoDashboardSequence');
     expect(test.getState('currentPage')).toEqual('DashboardPetitionsClerk');
