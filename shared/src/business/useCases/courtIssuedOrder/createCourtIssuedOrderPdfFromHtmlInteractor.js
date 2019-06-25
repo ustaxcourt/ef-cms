0bb5aeb2981e7aebe6b2f7a349c4d745f505d963
William Bagayoko <wbagayoko@gmail.com>
2019-06-25 17:51:57 -0500

    hello world with puppeteer

--- shared/src/business/useCases/courtIssuedOrder/createCourtIssuedOrderPdfFromHtmlInteractor.js
+++ shared/src/business/useCases/courtIssuedOrder/createCourtIssuedOrderPdfFromHtmlInteractor.js
@@ -1,12 +1,38 @@
+const chromium = require('chrome-aws-lambda');
+
 /**
+ *
  * createCourtIssuedOrderPdfFromHtml
  * @param applicationContext
  * @param entryMetadata
  * @returns {object} errors (null if no errors)
  */
-exports.createCourtIssuedOrderPdfFromHtml = ({
+exports.createCourtIssuedOrderPdfFromHtml = async ({
   applicationContext,
   htmlString,
 }) => {
-  return "hello world";
+  let browser;
+  let result;
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    });
+
+    let page = await browser.newPage();
+
+    await page.goto('https://example.com');
+
+    result = await page.title();
+  } catch (error) {
+    throw error;
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+
+  return result;
 };
