567c0622a3a2602f9f4e08bcca695f34116912f5
William Bagayoko <wbagayoko@gmail.com>
2019-06-25 17:37:36 -0500

    hello world / install puppeteer

--- efcms-service/src/courtIssuedOrder/createCourtIssuedOrderPdfFromHtmlLambda.js
+++ efcms-service/src/courtIssuedOrder/createCourtIssuedOrderPdfFromHtmlLambda.js
@@ -1,5 +1,4 @@
 const createApplicationContext = require('../applicationContext');
-const { getUserFromAuthHeader } = require('../middleware/apiGatewayHelper');
 const { handle } = require('../middleware/apiGatewayHelper');
 
 /**
@@ -10,7 +9,7 @@ const { handle } = require('../middleware/apiGatewayHelper');
  */
 exports.handler = event =>
   handle(event, async () => {
-    const user = getUserFromAuthHeader(event);
+    const user = {}; //getUserFromAuthHeader(event);
     const applicationContext = createApplicationContext(user);
     try {
       const results = await applicationContext
