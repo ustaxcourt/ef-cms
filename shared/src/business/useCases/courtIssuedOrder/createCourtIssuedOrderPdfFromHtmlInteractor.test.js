910db9d0253c222374f319b3668f78879fa47b3a
William Bagayoko <wbagayoko@gmail.com>
2019-06-26 13:33:25 -0500

    437 update test

--- shared/src/business/useCases/courtIssuedOrder/createCourtIssuedOrderPdfFromHtmlInteractor.test.js
+++ shared/src/business/useCases/courtIssuedOrder/createCourtIssuedOrderPdfFromHtmlInteractor.test.js
@@ -2,11 +2,30 @@ const {
   createCourtIssuedOrderPdfFromHtml,
 } = require('./createCourtIssuedOrderPdfFromHtmlInteractor');
 
+const pageMock = {
+  pdf: () => {
+    return 'Hello World';
+  },
+  setContent: () => {},
+};
+
+const browserMock = {
+  close: () => {},
+  newPage: () => pageMock,
+};
+
+const chromiumMock = {
+  puppeteer: {
+    launch: () => browserMock,
+  },
+};
 describe('createCourtIssuedOrderPdfFromHtml', () => {
-  it('returns the htmlString back', () => {
-    const result = createCourtIssuedOrderPdfFromHtml({
-      applicationContext: {},
-      htmlString: 'Hello World',
+  it('returns the pdf result', async () => {
+    const result = await createCourtIssuedOrderPdfFromHtml({
+      applicationContext: {
+        getChromium: () => chromiumMock,
+      },
+      htmlString: 'Hello World from the use case',
     });
 
     expect(result).toEqual('Hello World');
