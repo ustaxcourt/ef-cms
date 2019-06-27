fb14c85ea9245ccabeda5f08a40c1a4ea5e8dff7
Eric Sorenson <eric@sorenson.software>
2019-06-27 14:51:25 -0500

    fix env variable issues

--- web-client/e2e/sentWorkItemsExpireAfter7Days.test.js
+++ web-client/e2e/sentWorkItemsExpireAfter7Days.test.js
@@ -11,7 +11,7 @@ import applicationContextFactory from '../../web-api/src/applicationContext';
 
 const test = setupTest();
 
-xdescribe('verify old sent work items do not show up in the outbox', () => {
+describe('verify old sent work items do not show up in the outbox', () => {
   let workItem6Days;
   let workItem7Days;
   let workItem8Days;
