a3a3ee6aa89481de0aed7e325e974fcb1e4c76ed
Joseph Sutton <jsutton@flexion.us>
2019-06-27 08:40:03 -0500

    removed import

--- shared/src/business/useCases/generateSignedDocumentInteractor.test.js
+++ shared/src/business/useCases/generateSignedDocumentInteractor.test.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const {
   generateSignedDocument,
-  getPageDimensions,
 } = require('./generateSignedDocumentInteractor.js');
 const { PDFDocumentFactory } = require('pdf-lib');
 
