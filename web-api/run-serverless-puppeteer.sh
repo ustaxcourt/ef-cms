1d2860edca97f7677d0c80558f54123ac52b098d
William Bagayoko <wbagayoko@gmail.com>
2019-06-27 22:03:54 -0500

    layer build

--- web-api/run-serverless-puppeteer.sh
+++ web-api/run-serverless-puppeteer.sh
@@ -4,5 +4,7 @@ slsStage=$1
 region=$2
 layer="serverless-puppeteer.yml"
 
+tar -xvzf runtimes/puppeteer/puppeteer_lambda_layer.tar.gz -C runtimes/puppeteer
+
 SLS_DEPLOYMENT_BUCKET="${EFCMS_DOMAIN}.efcms.${slsStage}.${region}.deploys"
 SLS_DEPLOYMENT_BUCKET="${SLS_DEPLOYMENT_BUCKET}" ./node_modules/.bin/sls deploy --config "${layer}" --stage "${slsStage}" --region "${region}" --verbose
