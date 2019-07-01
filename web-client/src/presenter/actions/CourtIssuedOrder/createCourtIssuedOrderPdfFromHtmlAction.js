b5264d1d885d1d2679037cb42614c33e49b60bd9
William Bagayoko <wbagayoko@gmail.com>
2019-07-01 15:45:43 -0500

    437 Make file from blob [skip ci]

--- web-client/src/presenter/actions/CourtIssuedOrder/createCourtIssuedOrderPdfFromHtmlAction.js
+++ web-client/src/presenter/actions/CourtIssuedOrder/createCourtIssuedOrderPdfFromHtmlAction.js
@@ -1,6 +1,21 @@
 import { state } from 'cerebral';
 
 /**
+ * create File from Blob
+ * A Blob() is almost a File() - it's just missing the two properties below which we will add
+ * https://stackoverflow.com/questions/27159179/how-to-convert-blob-to-file-in-javascript/29390393
+ *
+ * @param {Blob} theBlob
+ * @param {string} fileName
+ * @returns {File} the new file
+ */
+function blobToFile(theBlob, fileName) {
+  theBlob.lastModifiedDate = new Date();
+  theBlob.name = fileName;
+  return theBlob;
+}
+
+/**
  * generate pdf url from html string
  *
  * @param {object} providers the providers object
@@ -26,8 +41,8 @@ export const createCourtIssuedOrderPdfFromHtmlAction = ({
       htmlString,
     });
 
-  return {
-    pdfFile: new File([pdfBlob], documentTitle),
-    pdfUrl: window.URL.createObjectURL(pdfBlob),
-  };
+  const pdfUrl = window.URL.createObjectURL(pdfBlob);
+  const pdfFile = blobToFile(pdfBlob, documentTitle);
+
+  return { pdfFile, pdfUrl };
 };
