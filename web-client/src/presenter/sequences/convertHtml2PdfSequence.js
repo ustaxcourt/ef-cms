7d87863d04a4811126dc38364df625da90e0b642
William Bagayoko <wbagayoko@gmail.com>
2019-07-01 12:57:56 -0500

    437 Submit Order wip [skip ci]

--- web-client/src/presenter/sequences/convertHtml2PdfSequence.js
+++ web-client/src/presenter/sequences/convertHtml2PdfSequence.js
@@ -1,9 +1,11 @@
 import { createOrderAction } from '../actions/createOrderAction';
 import { getPdfPreviewUrlAction } from '../actions/getPdfPreviewUrlAction';
+import { setPdfFileAction } from '../actions/CourtIssuedOrder/setPdfFileAction';
 import { setPdfPreviewUrlAction } from '../actions/setPdfPreviewUrlAction';
 
 export const convertHtml2PdfSequence = [
   createOrderAction,
   getPdfPreviewUrlAction,
   setPdfPreviewUrlAction,
+  setPdfFileAction,
 ];
