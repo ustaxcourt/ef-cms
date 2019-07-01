968f40272d00caf6f5df91c4b66250cd9c2d64ed
Cody Seibert <cseibert113@gmail.com>
2019-07-01 14:01:00 -0400

    trying to figure out which endpoints or actions are no longer used

--- web-client/src/presenter/sequences/assignSelectedWorkItemsSequence.js
+++ web-client/src/presenter/sequences/assignSelectedWorkItemsSequence.js
@@ -1,13 +1,11 @@
 import { assignSelectedWorkItemsAction } from '../actions/assignSelectedWorkItemsAction';
-import { getWorkItemsForSectionAction } from '../actions/getWorkItemsForSectionAction';
+import { chooseWorkQueueSequence } from '../sequences/chooseWorkQueueSequence';
 import { setFormSubmittingAction } from '../actions/setFormSubmittingAction';
-import { setWorkItemsAction } from '../actions/setWorkItemsAction';
 import { unsetFormSubmittingAction } from '../actions/unsetFormSubmittingAction';
 
 export const assignSelectedWorkItemsSequence = [
   setFormSubmittingAction,
   assignSelectedWorkItemsAction,
-  getWorkItemsForSectionAction,
-  setWorkItemsAction,
+  ...chooseWorkQueueSequence,
   unsetFormSubmittingAction,
 ];
