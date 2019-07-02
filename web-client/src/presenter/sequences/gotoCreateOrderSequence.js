3d49e895a028f732b4c5e07b94eaa8929733faa3
William Bagayoko <wbagayoko@gmail.com>
2019-07-02 10:27:41 -0500

    437 On Refresh direct to case detail with modal

--- web-client/src/presenter/sequences/gotoCreateOrderSequence.js
+++ web-client/src/presenter/sequences/gotoCreateOrderSequence.js
@@ -2,7 +2,10 @@ import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { clearFormAction } from '../actions/clearFormAction';
 import { clearModalAction } from '../actions/clearModalAction';
 import { convertHtml2PdfSequence } from './convertHtml2PdfSequence';
+import { hasOrderTypeSelectedAction } from '../actions/CourtIssuedOrder/hasOrderTypeSelectedAction';
 import { isLoggedInAction } from '../actions/isLoggedInAction';
+import { navigateToCaseDetailAction } from '../actions/navigateToCaseDetailAction';
+import { openCreateOrderChooseTypeModalSequence } from './openCreateOrderChooseTypeModalSequence';
 import { redirectToCognitoAction } from '../actions/redirectToCognitoAction';
 import { set } from 'cerebral/factories';
 import { setCasePropFromStateAction } from '../actions/setCasePropFromStateAction';
@@ -22,10 +25,21 @@ const gotoCreateOrder = [
   setCurrentPageAction('CreateOrder'),
 ];
 
+const gotoCaseDetailWithModal = [
+  ...openCreateOrderChooseTypeModalSequence,
+  navigateToCaseDetailAction,
+];
+
 export const gotoCreateOrderSequence = [
   isLoggedInAction,
   {
-    isLoggedIn: gotoCreateOrder,
+    isLoggedIn: [
+      hasOrderTypeSelectedAction,
+      {
+        no: gotoCaseDetailWithModal,
+        proceed: gotoCreateOrder,
+      },
+    ],
     unauthorized: [redirectToCognitoAction],
   },
 ];
