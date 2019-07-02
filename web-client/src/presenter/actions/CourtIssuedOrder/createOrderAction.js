04f8c57d1d16daec3a87d8c07578b47f64ae0bb0
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-07-02 08:23:19 -0700

    437: clean up pdf styles

--- web-client/src/presenter/actions/CourtIssuedOrder/createOrderAction.js
+++ web-client/src/presenter/actions/CourtIssuedOrder/createOrderAction.js
@@ -12,18 +12,22 @@ const replaceWithID = (replacements, domString) => {
   return doc;
 };
 
-export const createOrderAction = ({ get }) => {
+export const createOrderAction = ({ applicationContext, get }) => {
   let richText = get(state.form.richText) || '';
-  let documentTitle = get(state.form.documentTitle);
+  let documentTitle = (get(state.form.documentTitle) || '').toUpperCase();
   richText = richText.replace(/\t/g, '&nbsp;&nbsp;&nbsp;&nbsp;');
-  const caseCaption = get(state.caseDetail.caseCaption);
+  const caseCaption = get(state.caseDetail.caseCaption) || '';
+  const caseCaptionNames =
+    applicationContext.getCaseCaptionNames(caseCaption) + ', ';
+  const caseCaptionPostfix = caseCaption.replace(caseCaptionNames, '');
   const docketNumberWithSuffix = get(
     state.formattedCaseDetail.docketNumberWithSuffix,
   );
 
   const doc = replaceWithID(
     {
-      '#caseCaption': caseCaption,
+      '#caseCaptionNames': caseCaptionNames,
+      '#caseCaptionPostfix': caseCaptionPostfix,
       '#docketNumber': docketNumberWithSuffix,
       '#orderBody': richText,
       '#orderTitleHeader': documentTitle,
