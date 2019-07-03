2df9a44d025584780d2964f15caaccadb05cfc94
Matt Hopson <matthopson@gmail.com>
2019-07-03 15:51:25 -0600

    Updates to sign stip decision journey

--- web-client/e2e/srAttorneySignsProposedStipDecision.test.js
+++ web-client/e2e/srAttorneySignsProposedStipDecision.test.js
@@ -1,22 +1,17 @@
 require('isomorphic-fetch');
-import {
-  CATEGORIES,
-  CATEGORY_MAP,
-  INTERNAL_CATEGORY_MAP,
-} from '../../shared/src/business/entities/Document';
+
+const {
+  ContactFactory,
+} = require('../../shared/src/business/entities/contacts/ContactFactory');
 import { Case } from '../../shared/src/business/entities/cases/Case';
 import { CerebralTest } from 'cerebral/test';
-import { TRIAL_CITIES } from '../../shared/src/business/entities/TrialCities';
-import { isFunction, mapValues } from 'lodash';
-import FormData from 'form-data';
-const {
-  COUNTRY_TYPES,
-  PARTY_TYPES,
-} = require('../../shared/src/business/entities/contacts/PetitionContact');
 import { Document } from '../../shared/src/business/entities/Document';
+import { TrialSession } from '../../shared/src/business/entities/TrialSession';
 import { applicationContext } from '../src/applicationContext';
+import { isFunction, mapValues } from 'lodash';
 import { presenter } from '../src/presenter/presenter';
 import { withAppContextDecorator } from '../src/withAppContext';
+import FormData from 'form-data';
 
 // docketclerk
 import docketClerkLogIn from './journey/docketClerkLogIn';
@@ -59,6 +54,19 @@ fakeFile.name = 'fakeFile.pdf';
 global.FormData = FormData;
 global.Blob = () => {};
 presenter.providers.applicationContext = applicationContext;
+presenter.providers.applicationContext = {
+  ...applicationContext,
+  getUseCases: () => {
+    return {
+      ...applicationContext.getUseCases(),
+      loadPDFForSigning: () => {
+        return new Promise(resolve => {
+          resolve(null);
+        });
+      },
+    };
+  },
+};
 
 presenter.providers.router = {
   externalRoute: () => null,
@@ -116,18 +124,25 @@ describe('Sr. Attroney Signs Proposed Stipulated Decision', () => {
         removeItem: () => null,
         setItem: () => null,
       },
+      pdfjsObj: {
+        getData: () => {
+          return new Promise(resolve => {
+            resolve(new Uint8Array(fakeFile));
+          });
+        },
+      },
     };
 
     test.setState('constants', {
       CASE_CAPTION_POSTFIX: Case.CASE_CAPTION_POSTFIX,
-      CATEGORIES,
-      CATEGORY_MAP,
-      COUNTRY_TYPES,
+      CATEGORIES: Document.CATEGORIES,
+      CATEGORY_MAP: Document.CATEGORY_MAP,
+      COUNTRY_TYPES: ContactFactory.COUNTRY_TYPES,
       DOCUMENT_TYPES_MAP: Document.initialDocumentTypes,
-      INTERNAL_CATEGORY_MAP,
-      PARTY_TYPES,
+      INTERNAL_CATEGORY_MAP: Document.INTERNAL_CATEGORY_MAP,
+      PARTY_TYPES: ContactFactory.PARTY_TYPES,
       STATUS_TYPES: Case.STATUS_TYPES,
-      TRIAL_CITIES,
+      TRIAL_CITIES: TrialSession.TRIAL_CITIES,
     });
   });
 
