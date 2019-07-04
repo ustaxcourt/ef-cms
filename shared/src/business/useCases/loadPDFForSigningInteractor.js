e2d5a6038f408fbeacec219947d48a896e136676
Matt Hopson <matthopson@gmail.com>
2019-07-03 20:51:41 -0600

    Revert some changes around file handling

--- shared/src/business/useCases/loadPDFForSigningInteractor.js
+++ shared/src/business/useCases/loadPDFForSigningInteractor.js
@@ -15,15 +15,7 @@ exports.loadPDFForSigning = async ({ applicationContext, documentId }) => {
       documentId,
     });
 
-    // There is a discrepency in how Node handles this response
-    // (in the case of testing) vs how it's returned when run from
-    // a browser. Additional changes in getDocument.js were added
-    // to ensure something actually gets returned
-    let arrayBuffer = pdfData;
-    if (pdfData instanceof Blob) {
-      arrayBuffer = await new Response(pdfData).arrayBuffer();
-    }
-
+    const arrayBuffer = await new Response(pdfData).arrayBuffer();
     return await pdfjsLib.getDocument(arrayBuffer).promise;
   } catch (err) {
     throw new Error('error loading PDF');
