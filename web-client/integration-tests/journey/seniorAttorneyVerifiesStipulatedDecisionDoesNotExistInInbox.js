5423ad23e2efb4bdaf9761a224bd215cf1cfede4
Matt Hopson <matthopson@gmail.com>
2019-07-03 20:52:39 -0600

    Harden test against some old data

--- web-client/integration-tests/journey/seniorAttorneyVerifiesStipulatedDecisionDoesNotExistInInbox.js
+++ web-client/integration-tests/journey/seniorAttorneyVerifiesStipulatedDecisionDoesNotExistInInbox.js
@@ -1,3 +1,5 @@
+import { applicationContext } from '../../src/applicationContext';
+
 import { getFormattedMyInbox } from '../helpers';
 export default test => {
   describe('Senior Attorney verifies Stipulated Decision does not exist in inbox', () => {
@@ -8,9 +10,12 @@ export default test => {
     });
 
     it('verifies the stipulated decision does not exist', async () => {
+      const currentUser = applicationContext.getCurrentUser();
       const myInbox = await getFormattedMyInbox(test);
       const stipDecision = myInbox.find(
-        item => item.document.documentType === 'Proposed Stipulated Decision',
+        item =>
+          item.document.documentType === 'Proposed Stipulated Decision' &&
+          item.assigneeId !== currentUser.userId,
       );
 
       expect(stipDecision).toBeUndefined();
