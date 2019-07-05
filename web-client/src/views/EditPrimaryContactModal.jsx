eaedb0404c59194c382522f8714c8fb1bb71bc8e
Matt Hopson <matthopson@gmail.com>
2019-07-05 11:15:33 -0600

    WIP - add country and phone to edit

--- web-client/src/views/EditPrimaryContactModal.jsx
+++ web-client/src/views/EditPrimaryContactModal.jsx
@@ -1,10 +1,11 @@
-import { sequences } from 'cerebral';
+import { sequences, state } from 'cerebral';
 
 import { ModalDialog } from './ModalDialog';
 import { connect } from '@cerebral/react';
 import React from 'react';
 
 import { Address } from './StartCase/Address';
+import { Country } from './StartCase/Country';
 
 class EditPrimaryContactModalComponent extends ModalDialog {
   constructor(props) {
@@ -20,11 +21,37 @@ class EditPrimaryContactModalComponent extends ModalDialog {
     return (
       <div>
         <h3 className="margin-bottom-3">Edit Your Contact Information</h3>
+        <Country
+          bind="caseDetail"
+          type="contactPrimary"
+          onBlur="updateCaseValueSequence"
+          onChange="updateCaseValueSequence"
+        />
         <Address
           bind="caseDetail"
           type="contactPrimary"
           onBlur="updateCaseValueSequence"
           onChange="updateCaseValueSequence"
+        />
+        <label className="usa-label" htmlFor="phone">
+          Phone Number
+        </label>
+        <input
+          autoCapitalize="none"
+          className="usa-input"
+          id="phone"
+          name="contactPrimary.phone"
+          type="tel"
+          value={this.props.caseDetail.contactPrimary.phone || ''}
+          onBlur={() => {
+            this.props.updateCaseValueSequence();
+          }}
+          onChange={e => {
+            this.props.updateCaseValueSequence({
+              key: e.target.name,
+              value: e.target.value,
+            });
+          }}
         />
       </div>
     );
@@ -34,7 +61,9 @@ class EditPrimaryContactModalComponent extends ModalDialog {
 export const EditPrimaryContactModal = connect(
   {
     cancelSequence: sequences.cancelEditPrimaryContactSequence,
+    caseDetail: state.caseDetail,
     confirmSequence: sequences.submitEditPrimaryContactSequence,
+    updateCaseValueSequence: sequences.updateCaseValueSequence,
   },
   EditPrimaryContactModalComponent,
 );
