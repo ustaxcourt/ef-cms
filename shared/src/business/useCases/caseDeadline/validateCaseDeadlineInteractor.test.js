d6bfc3725022cb8f2e31f0069a50de65785fadbb
William Bagayoko <wbagayoko@gmail.com>
2019-07-05 14:01:07 -0500

    increase shared coverage [skip ci]

--- shared/src/business/useCases/caseDeadline/validateCaseDeadlineInteractor.test.js
+++ shared/src/business/useCases/caseDeadline/validateCaseDeadlineInteractor.test.js
@@ -18,4 +18,24 @@ describe('validateCaseDeadlineInteractor', () => {
       Object.keys(CaseDeadline.errorToMessageMap),
     );
   });
+
+  it('returns the null when there are no errors', () => {
+    const mockCaseDeadline = {
+      caseDeadlineId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+      caseId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+      deadlineDate: '2019-03-01T21:42:29.073Z',
+      description: 'hello world',
+    };
+
+    const errors = validateCaseDeadlineInteractor({
+      applicationContext: {
+        getEntityConstructors: () => ({
+          CaseDeadline,
+        }),
+      },
+      caseDeadline: mockCaseDeadline,
+    });
+
+    expect(errors).toEqual(null);
+  });
 });
