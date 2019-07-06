d1d9ebaa57c767fd28292912012ef848b2c557c3
Matt Hopson <matthopson@gmail.com>
2019-07-05 19:19:36 -0600

    Add test

--- web-client/src/presenter/actions/setContactPrimaryToEditAction.js
+++ web-client/src/presenter/actions/setContactPrimaryToEditAction.js
@@ -1,5 +1,13 @@
 import { state } from 'cerebral';
 
+/**
+ * sets the state.contactToEdit based on the props.caseTypes passed in
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.get the cerebral get function
+ * @param {object} providers.store the cerebral store used for setting the state.contactToEdit
+ */
+
 export const setContactPrimaryToEditAction = ({ get, store }) => {
   const contactPrimary = get(state.caseDetail.contactPrimary);
   store.set(state.contactToEdit, {
