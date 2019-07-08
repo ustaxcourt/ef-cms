e3ac222c92d9750f45f91e86882b3479c0e5e965
Kris Koskelin <kkoskelin@flexion.us>
2019-07-08 11:45:30 -0500

    Addressing test issues for clarity

--- shared/src/business/useCases/workitems/getDocumentQCInboxForSectionInteractor.js
+++ shared/src/business/useCases/workitems/getDocumentQCInboxForSectionInteractor.js
@@ -19,7 +19,7 @@ exports.getDocumentQCInboxForSection = async ({
 }) => {
   const user = applicationContext.getCurrentUser();
 
-  if (!isAuthorized(user, WORKITEM, user.userId)) {
+  if (!isAuthorized(user, WORKITEM)) {
     throw new UnauthorizedError(
       'Unauthorized for getting completed work items',
     );
