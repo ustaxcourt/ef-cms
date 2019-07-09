8b51267f082993a9f98b180fe3de6efbf4d36d8c
Joseph Sutton <jsutton@flexion.us>
2019-07-09 09:43:16 -0500

    added integration test to edit primary contact of a case

---
+++ web-client/integration-tests/journey/taxpayerEditsCaseContactInformation.js
@@ -0,0 +1,24 @@
+export default test => {
+  return it('Petitioner updates primary contact information', async () => {
+    const contactPrimary = {
+      ...test.getState('caseDetail.contactPrimary'),
+      address1: '100 Main St.',
+      address2: 'Grand View Apartments',
+      address3: 'Apt. #104',
+      city: 'Jordan',
+      countryType: 'domestic',
+      phone: '1111111111',
+      postalCode: '55352',
+      state: 'MN',
+    };
+
+    await test.runSequence('updateContactPrimaryValueSequence', {
+      key: 'contactPrimary',
+      value: contactPrimary,
+    });
+
+    await test.runSequence('submitEditPrimaryContactSequence');
+
+    expect(test.getState('caseDetail.contactPrimary')).toEqual(contactPrimary);
+  });
+};
