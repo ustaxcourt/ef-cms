1212f8b8a7ff0a7192eba86027ee1800dfa00df4
Cody Seibert <cseibert113@gmail.com>
2019-07-09 15:04:08 -0400

    refactoring how the regional domain name is obtained

--- web-api/setup-regional-route53.sh
+++ web-api/setup-regional-route53.sh
@@ -5,8 +5,8 @@ DOMAIN="${EFCMS_DOMAIN}"
 SUBDOMAIN="efcms-${ENVIRONMENT}.${DOMAIN}"
 STACKNAME="ef-cms-${ENVIRONMENT}"
 HOSTEDZONE=$(aws route53 list-hosted-zones --query "HostedZones[?Name=='${DOMAIN}.'].Id" --output text)
-REGION1=$(aws cloudformation describe-stacks --stack-name "${STACKNAME}" --region us-east-1 --query "Stacks[0].Outputs[?OutputKey==\`DomainName\`].OutputValue" --output text)
-REGION2=$(aws cloudformation describe-stacks --stack-name "${STACKNAME}" --region us-west-1 --query "Stacks[0].Outputs[?OutputKey==\`DomainName\`].OutputValue" --output text)
+REGION1=$(aws apigateway get-domain-name --domain-name "${SUBDOMAIN}" --region us-east-1 --query "regionalDomainName" --output text)
+REGION2=$(aws apigateway get-domain-name --domain-name "${SUBDOMAIN}" --region us-west-1 --query "regionalDomainName" --output text)
 aws route53 change-resource-record-sets \
   --hosted-zone-id "${HOSTEDZONE}" \
   --change-batch  '{
