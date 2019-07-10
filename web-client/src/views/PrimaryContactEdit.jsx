8b50b22c9f0028be55c17903d2cba64076d4a895
Eric Sorenson <eric@sorenson.software>
2019-07-10 14:39:33 -0500

    working validation, saving on new page

--- web-client/src/views/PrimaryContactEdit.jsx
+++ web-client/src/views/PrimaryContactEdit.jsx
@@ -8,69 +8,93 @@ import React from 'react';
 export const PrimaryContactEdit = connect(
   {
     caseDetail: state.caseDetail,
+    submitEditPrimaryContactSequence:
+      sequences.submitEditPrimaryContactSequence,
     updateCaseValueSequence: sequences.updateCaseValueSequence,
-    validateCaseDetailSequence: sequences.validateCaseDetailSequence,
+    validateContactPrimarySequence: sequences.validateContactPrimarySequence,
     validationErrors: state.validationErrors,
   },
   ({
     caseDetail,
+    submitEditPrimaryContactSequence,
     updateCaseValueSequence,
-    validateCaseDetailSequence,
+    validateContactPrimarySequence,
     validationErrors,
   }) => {
     return (
-      <section className="usa-section grid-container">
-        <h1>Edit Your Contact Information</h1>
-        <div className="blue-container">
-          <Country
-            bind={'caseDetail'}
-            type={'contactPrimary'}
-            onBlur={validateCaseDetailSequence}
-            onChange={updateCaseValueSequence}
-          />
-          <Address
-            bind={'caseDetail'}
-            type={'contactPrimary'}
-            onBlur={validateCaseDetailSequence}
-            onChange={updateCaseValueSequence}
-          />
-          <div
-            className={
-              'usa-form-group ' +
-              (validationErrors &&
-              validationErrors.contactPrimary &&
-              validationErrors.contactPrimary.phone
-                ? 'usa-form-group--error'
-                : '')
-            }
-          >
-            <label className="usa-label" htmlFor="phone">
-              Phone Number
-            </label>
-            <input
-              autoCapitalize="none"
-              className="usa-input"
-              id="phone"
-              name="contactPrimary.phone"
-              type="tel"
-              value={caseDetail.contactPrimary.phone || ''}
-              onBlur={() => {
-                validateCaseDetailSequence();
-              }}
-              onChange={e => {
-                updateCaseValueSequence({
-                  key: e.target.name,
-                  value: e.target.value,
-                });
-              }}
-            />
-            <Text
-              bind={'validationErrors.contactPrimary.phone'}
-              className="usa-error-message"
-            />
+      <>
+        <div className="big-blue-header">
+          <div className="grid-container">
+            <h1>My Contact Information</h1>
           </div>
         </div>
-      </section>
+        <section className="usa-section grid-container">
+          <h2>Edit Your Contact Information for This Case</h2>
+          <div className="blue-container">
+            <Country
+              bind={'caseDetail'}
+              type={'contactPrimary'}
+              onBlur={'validateContactPrimarySequence'}
+              onChange={'updateCaseValueSequence'}
+            />
+            <Address
+              bind={'caseDetail'}
+              type={'contactPrimary'}
+              onBlur={'validateContactPrimarySequence'}
+              onChange={'updateCaseValueSequence'}
+            />
+            <div
+              className={
+                'usa-form-group ' +
+                (validationErrors &&
+                validationErrors.contactPrimary &&
+                validationErrors.contactPrimary.phone
+                  ? 'usa-form-group--error'
+                  : '')
+              }
+            >
+              <label className="usa-label" htmlFor="phone">
+                Phone Number
+              </label>
+              <input
+                autoCapitalize="none"
+                className="usa-input"
+                id="phone"
+                name="contactPrimary.phone"
+                type="tel"
+                value={caseDetail.contactPrimary.phone || ''}
+                onBlur={() => {
+                  validateContactPrimarySequence();
+                }}
+                onChange={e => {
+                  updateCaseValueSequence({
+                    key: e.target.name,
+                    value: e.target.value,
+                  });
+                }}
+              />
+              <Text
+                bind={'validationErrors.contactPrimary.phone'}
+                className="usa-error-message"
+              />
+            </div>
+          </div>
+          <button
+            className="usa-button margin-top-3 margin-right-3"
+            onBlur={() => {
+              submitEditPrimaryContactSequence();
+            }}
+          >
+            Save
+          </button>
+          <a
+            className="usa-button usa-button--outline"
+            href={`/case-detail/${caseDetail.docketNumber}`}
+          >
+            Cancel
+          </a>
+        </section>
+      </>
     );
   },
 );
