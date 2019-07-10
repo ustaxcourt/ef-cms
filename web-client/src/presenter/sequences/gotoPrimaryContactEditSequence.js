96311f05a3a68b57733b17bc2a6c3108f52c0689
Eric Sorenson <eric@sorenson.software>
2019-07-10 11:47:40 -0500

    begin new page for primary contact modal

---
+++ web-client/src/presenter/sequences/gotoPrimaryContactEditSequence.js
@@ -0,0 +1,29 @@
+import { clearAlertsAction } from '../actions/clearAlertsAction';
+import { clearFormAction } from '../actions/clearFormAction';
+import { clearScreenMetadataAction } from '../actions/clearScreenMetadataAction';
+import { getCaseAction } from '../actions/getCaseAction';
+import { isLoggedInAction } from '../actions/isLoggedInAction';
+import { redirectToCognitoAction } from '../actions/redirectToCognitoAction';
+import { set } from 'cerebral/factories';
+import { setCaseAction } from '../actions/setCaseAction';
+import { setCurrentPageAction } from '../actions/setCurrentPageAction';
+import { state } from 'cerebral';
+
+const gotoPrimaryContactEdit = [
+  setCurrentPageAction('Interstitial'),
+  set(state.showValidation, false),
+  clearAlertsAction,
+  clearFormAction,
+  clearScreenMetadataAction,
+  getCaseAction,
+  setCaseAction,
+  setCurrentPageAction('PrimaryContactEdit'),
+];
+
+export const gotoPrimaryContactEditSequence = [
+  isLoggedInAction,
+  {
+    isLoggedIn: gotoPrimaryContactEdit,
+    unauthorized: [redirectToCognitoAction],
+  },
+];
