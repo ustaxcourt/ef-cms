8fcec53773c8dee1860b35e0e746c68e50afefe3
William Bagayoko <wbagayoko@gmail.com>
2019-07-11 10:19:52 -0500

    1508 View All Docs Buttons / Refresh [skip ci]

--- web-client/src/presenter/sequences/gotoViewAllDocumentsSequence.js
+++ web-client/src/presenter/sequences/gotoViewAllDocumentsSequence.js
@@ -1,5 +1,13 @@
+import { getCaseAction } from '../actions/getCaseAction';
+import { set } from 'cerebral/factories';
+import { setCaseAction } from '../actions/setCaseAction';
 import { setCurrentPageAction } from '../actions/setCurrentPageAction';
+import { state } from 'cerebral';
 
 export const gotoViewAllDocumentsSequence = [
+  setCurrentPageAction('Interstitial'),
+  getCaseAction,
+  setCaseAction,
+  set(state.allDocumentsAccordion, ''),
   setCurrentPageAction('ViewAllDocuments'),
 ];
