a00a42504ec883171d191c705a08cb16ae42a388
William Bagayoko <wbagayoko@gmail.com>
2019-07-11 10:19:52 -0500

    1508 View All Docs Buttons / Refresh [skip ci]

--- web-client/src/views/FileDocument/ViewAllDocuments.jsx
+++ web-client/src/views/FileDocument/ViewAllDocuments.jsx
@@ -1,13 +1,22 @@
 import { Accordion, AccordionItem } from '../../ustc-ui/Accordion/Accordion';
+import { FormCancelModalDialog } from '../FormCancelModalDialog';
 import { connect } from '@cerebral/react';
 import { sequences, state } from 'cerebral';
 import React from 'react';
 
 export const ViewAllDocuments = connect(
   {
+    caseDetail: state.caseDetail,
+    formCancelToggleCancelSequence: sequences.formCancelToggleCancelSequence,
+    showModal: state.showModal,
     viewAllDocumentsHelper: state.viewAllDocumentsHelper,
   },
-  ({ viewAllDocumentsHelper }) => {
+  ({
+    caseDetail,
+    formCancelToggleCancelSequence,
+    showModal,
+    viewAllDocumentsHelper,
+  }) => {
     return (
       <React.Fragment>
         <div className="grid-container">
@@ -15,7 +24,7 @@ export const ViewAllDocuments = connect(
             <div className="tablet:grid-col-12">
               <h1
                 className="margin-bottom-05"
-                id="file-a-document-header"
+                id="view-all-document-header"
                 tabIndex="-1"
               >
                 All Document Categories
@@ -23,7 +32,7 @@ export const ViewAllDocuments = connect(
               <p className="margin-bottom-5 margin-top-05 ">
                 Select the document type under the category you wish to file....
               </p>
-              <Accordion headingLevel="3">
+              <Accordion bind="allDocumentsAccordion" headingLevel="3">
                 {viewAllDocumentsHelper.sections.map((title, index) => {
                   return (
                     <AccordionItem key={`item-${index}`} title={title}>
@@ -44,6 +53,26 @@ export const ViewAllDocuments = connect(
                 })}
               </Accordion>
             </div>
+          </div>
+          <div className="button-box-container margin-bottom-4">
+            <a
+              className="usa-button margin-right-205"
+              href={`/case-detail/${caseDetail.docketNumber}/file-a-document`}
+            >
+              Back to File a Document
+            </a>
+            <button
+              className="usa-button usa-button--unstyled"
+              id="cancel-button"
+              onClick={() => {
+                formCancelToggleCancelSequence();
+              }}
+            >
+              Cancel
+            </button>
+            {showModal === 'FormCancelModalDialogComponent' && (
+              <FormCancelModalDialog onCancelSequence="closeModalAndReturnToCaseDetailSequence" />
+            )}
           </div>
         </div>
       </React.Fragment>
