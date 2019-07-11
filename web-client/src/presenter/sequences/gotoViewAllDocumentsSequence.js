b0b58c457f91a0f665b06f3a06b708df476ecff7
William Bagayoko <wbagayoko@gmail.com>
2019-07-11 18:45:22 -0500

    1508 View All Documents Modal updates [skip ci]

--- web-client/src/presenter/sequences/gotoViewAllDocumentsSequence.js
+++ web-client/src/presenter/sequences/gotoViewAllDocumentsSequence.js
@@ -1,8 +1,16 @@
+import { clearAlertsAction } from '../actions/clearAlertsAction';
+import { clearFormAction } from '../actions/clearFormAction';
+import { clearModalAction } from '../actions/clearModalAction';
+import { clearModalStateAction } from '../actions/clearModalStateAction';
 import { navigateToViewAllDocumentsAction } from '../actions/FileDocument/navigateToViewAllDocumentsAction';
 import { set } from 'cerebral/factories';
 import { state } from 'cerebral';
 
 export const gotoViewAllDocumentsSequence = [
+  clearAlertsAction,
+  clearFormAction,
+  clearModalAction,
+  clearModalStateAction,
   set(state.allDocumentsAccordion, ''),
   navigateToViewAllDocumentsAction,
 ];
