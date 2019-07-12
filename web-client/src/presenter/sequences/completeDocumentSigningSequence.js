f76c85914767ed07693c1b34b2f7cd04377d09a6
Matt Hopson <matthopson@gmail.com>
2019-07-12 11:40:42 -0500

    Add validation to signing sequence

--- web-client/src/presenter/sequences/completeDocumentSigningSequence.js
+++ web-client/src/presenter/sequences/completeDocumentSigningSequence.js
@@ -1,13 +1,25 @@
+import { clearAlertsAction } from '../actions/clearAlertsAction';
+import { clearFormAction } from '../actions/clearFormAction';
 import { clearPDFSignatureDataAction } from '../actions/clearPDFSignatureDataAction';
 import { completeDocumentSigningAction } from '../actions/completeDocumentSigningAction';
 import { createWorkItemSequence } from './createWorkItemSequence';
 import { gotoDashboardSequence } from './gotoDashboardSequence';
 import { setDocumentIdAction } from '../actions/setDocumentIdAction';
+import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
+import { validateInitialWorkItemMessageAction } from '../actions/validateInitialWorkItemMessageAction';
 
 export const completeDocumentSigningSequence = [
-  completeDocumentSigningAction,
-  setDocumentIdAction,
-  ...createWorkItemSequence,
-  clearPDFSignatureDataAction,
-  ...gotoDashboardSequence,
+  clearAlertsAction,
+  validateInitialWorkItemMessageAction,
+  {
+    error: [setValidationErrorsAction],
+    success: [
+      completeDocumentSigningAction,
+      setDocumentIdAction,
+      ...createWorkItemSequence,
+      clearPDFSignatureDataAction,
+      ...gotoDashboardSequence,
+      clearFormAction,
+    ],
+  },
 ];
