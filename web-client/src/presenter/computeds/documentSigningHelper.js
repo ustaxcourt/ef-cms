3a1ac6c819d02271fdc086f5a1178060ebe09183
Joseph Sutton <jsutton@flexion.us>
2019-07-15 11:19:20 -0500

    added signature helper for document signing

---
+++ web-client/src/presenter/computeds/documentSigningHelper.js
@@ -0,0 +1,26 @@
+import { state } from 'cerebral';
+
+export const documentSigningHelper = get => {
+  const signatureApplied = get(state.pdfForSigning.signatureApplied);
+  const signatureData = get(state.pdfForSigning.signatureData);
+  const currentPageNumber = get(state.pdfForSigning.pageNumber);
+  const pdfjsObj = get(state.pdfForSigning.pdfjsObj);
+  const totalPages = pdfjsObj.numPages;
+
+  const disablePrevious =
+    currentPageNumber === 1 || (!!signatureData || signatureApplied);
+  const disableNext =
+    currentPageNumber === totalPages || (!!signatureData || signatureApplied);
+  const previousPageNumber =
+    currentPageNumber === 1 ? 1 : currentPageNumber - 1;
+  const nextPageNumber =
+    currentPageNumber === totalPages ? totalPages : currentPageNumber + 1;
+
+  return {
+    disableNext,
+    disablePrevious,
+    nextPageNumber,
+    previousPageNumber,
+    totalPages,
+  };
+};
