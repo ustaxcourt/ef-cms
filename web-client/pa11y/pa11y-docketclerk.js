96c7774e77f7770e08bd6e795d8f7fcb90d09414
Eric Sorenson <eric@sorenson.software>
2019-07-15 11:28:27 -0500

    added pa11y test for signatures

--- web-client/pa11y/pa11y-docketclerk.js
+++ web-client/pa11y/pa11y-docketclerk.js
@@ -26,6 +26,7 @@ module.exports = [
     url:
       'http://localhost:1234/mock-login?token=docketclerk&path=/case-detail/103-19/documents/f1aa4aa2-c214-424c-8870-d0049c5744d7&info=forwarding-form',
   },
+  'http://localhost:1234/mock-login?token=docketclerk&path=/case-detail/103-19/documents/f1aa4aa2-c214-424c-8870-d0049c5744d7/sign',
   'http://localhost:1234/mock-login?token=docketclerk&path=/case-detail/103-19/add-docket-entry',
   {
     actions: [
