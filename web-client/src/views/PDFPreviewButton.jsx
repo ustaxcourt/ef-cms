33cb38bef253e7190139433fbf9e20e121678bf5
Matt Hopson <matthopson@gmail.com>
2019-07-16 12:37:03 -0600

    Move pdf preview link to component with modal

---
+++ web-client/src/views/PDFPreviewButton.jsx
@@ -0,0 +1,26 @@
+import { connect } from '@cerebral/react';
+import { sequences, state } from 'cerebral';
+import React from 'react';
+
+import { PDFPreviewModal } from './PDFPreviewModal';
+
+export const PDFPreviewButton = connect(
+  {
+    openPdfPreviewModalSequence: sequences.openPdfPreviewModalSequence,
+    showModal: state.showModal,
+  },
+  ({ file, openPdfPreviewModalSequence, showModal }) => {
+    return (
+      <>
+        <button
+          className="usa-button usa-button--unstyled"
+          type="button"
+          onClick={() => openPdfPreviewModalSequence({ file })}
+        >
+          {file.name}
+        </button>
+        {showModal === 'PDFPreviewModal' && <PDFPreviewModal pdfFile={file} />}
+      </>
+    );
+  },
+);
