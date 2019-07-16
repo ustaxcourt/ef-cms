b8fe165cff673f876e4d79e2393889adafce47c7
Joseph Sutton <jsutton@flexion.us>
2019-07-16 15:27:40 -0500

    removed spinner from preview sequence

--- web-client/src/presenter/sequences/openPdfPreviewModalSequence.js
+++ web-client/src/presenter/sequences/openPdfPreviewModalSequence.js
@@ -1,9 +1,3 @@
 import { openPdfPreviewModalAction } from '../actions/openPdfPreviewModalAction';
-import { setFormSubmittingAction } from '../actions/setFormSubmittingAction';
-import { unsetFormSubmittingAction } from '../actions/unsetFormSubmittingAction';
 
-export const openPdfPreviewModalSequence = [
-  setFormSubmittingAction,
-  openPdfPreviewModalAction,
-  unsetFormSubmittingAction,
-];
+export const openPdfPreviewModalSequence = [openPdfPreviewModalAction];
