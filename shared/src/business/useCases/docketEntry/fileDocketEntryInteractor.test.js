24d599a1951379d48bb493fea08d4b2594515cc4
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-07-16 12:34:34 -0500

    add test for docket entry interactor

---
+++ shared/src/business/useCases/docketEntry/fileDocketEntryInteractor.test.js
@@ -0,0 +1,144 @@
+const sinon = require('sinon');
+const {
+  fileDocketEntryInteractor,
+} = require('./fileDocketEntryInteractor');
+const { User } = require('../../entities/User');
+
+describe('fileDocketEntryInteractor', () => {
+  let applicationContext;
+
+  let caseRecord = {
+    caseId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+    createdAt: '',
+    docketNumber: '45678-18',
+    documents: [
+      {
+        documentId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+        documentType: 'Answer',
+        userId: 'respondent',
+      },
+      {
+        documentId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+        documentType: 'Answer',
+        userId: 'respondent',
+      },
+      {
+        documentId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+        documentType: 'Answer',
+        userId: 'respondent',
+      },
+    ],
+    role: 'petitioner',
+    userId: 'taxpayer',
+  };
+  it('should throw an error if not authorized', async () => {
+    let error;
+    try {
+      applicationContext = {
+        environment: { stage: 'local' },
+        getCurrentUser: () => {
+          return {
+            name: 'Olivia Jade',
+            role: 'seniorattorney',
+            userId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+          };
+        },
+        getPersistenceGateway: () => ({
+          getCaseByCaseId: async () => caseRecord,
+          saveWorkItemForNonPaper: async () => caseRecord,
+          updateCase: async () => caseRecord,
+        }),
+      };
+      await fileDocketEntryInteractor({
+        applicationContext,
+        documentMetadata: {
+          caseId: caseRecord.caseId,
+          documentType: 'Memorandum in Support',
+        },
+        primaryDocumentFileId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+      });
+    } catch (err) {
+      error = err;
+    }
+    expect(error.message).toContain('Unauthorized');
+  });
+
+  it('add documents and workitems', async () => {
+    let error;
+    let getCaseByCaseIdSpy = sinon.stub().returns(caseRecord);
+    let saveWorkItemForNonPaperSpy = sinon.spy();
+    let updateCaseSpy = sinon.spy();
+    try {
+      applicationContext = {
+        environment: { stage: 'local' },
+        getCurrentUser: () => {
+          return new User({
+            name: 'Olivia Jade',
+            role: 'respondent',
+            userId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+          });
+        },
+        getPersistenceGateway: () => ({
+          getCaseByCaseId: getCaseByCaseIdSpy,
+          saveWorkItemForNonPaper: saveWorkItemForNonPaperSpy,
+          updateCase: updateCaseSpy,
+        }),
+      };
+      await fileDocketEntryInteractor({
+        applicationContext,
+        documentMetadata: {
+          caseId: caseRecord.caseId,
+          documentType: 'Memorandum in Support',
+        },
+        primaryDocumentFileId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+      });
+    } catch (err) {
+      error = err;
+    }
+    expect(error).toBeUndefined();
+    expect(getCaseByCaseIdSpy.called).toEqual(true);
+    expect(saveWorkItemForNonPaperSpy.called).toEqual(true);
+    expect(updateCaseSpy.called).toEqual(true);
+  });
+
+  it('add documents but not workitems for paper filed documents', async () => {
+    let error;
+    let getCaseByCaseIdSpy = sinon.stub().returns(caseRecord);
+    let saveWorkItemForNonPaperSpy = sinon.spy();
+    let saveWorkItemForDocketClerkFilingExternalDocumentSpy = sinon.spy();
+    let updateCaseSpy = sinon.spy();
+    try {
+      applicationContext = {
+        environment: { stage: 'local' },
+        getCurrentUser: () => {
+          return {
+            name: 'Olivia Jade',
+            role: 'docketclerk',
+            userId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+          };
+        },
+        getPersistenceGateway: () => ({
+          getCaseByCaseId: getCaseByCaseIdSpy,
+          saveWorkItemForDocketClerkFilingExternalDocument: saveWorkItemForDocketClerkFilingExternalDocumentSpy,
+          saveWorkItemForNonPaper: saveWorkItemForNonPaperSpy,
+          updateCase: updateCaseSpy,
+        }),
+      };
+      await fileDocketEntryInteractor({
+        applicationContext,
+        documentMetadata: {
+          caseId: caseRecord.caseId,
+          documentType: 'Memorandum in Support',
+          isPaper: true,
+        },
+        primaryDocumentFileId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+      });
+    } catch (err) {
+      error = err;
+    }
+    expect(error).toBeUndefined();
+    expect(getCaseByCaseIdSpy.called).toEqual(true);
+    expect(saveWorkItemForNonPaperSpy.called).toEqual(false);
+    expect(updateCaseSpy.called).toEqual(true);
+  });
+});
