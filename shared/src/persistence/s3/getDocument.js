4ca20274c996a024df202a8ab781b2fb317564d8
Eric Sorenson <eric@sorenson.software>
2019-07-17 15:08:26 -0500

    migrate documents to their own serverless file

--- shared/src/persistence/s3/getDocument.js
+++ shared/src/persistence/s3/getDocument.js
@@ -4,7 +4,7 @@ const getDownloadPolicy = async ({ applicationContext, documentId }) => {
   } = await applicationContext
     .getHttpClient()
     .get(
-      `${applicationContext.getBaseUrl()}/api/documents/${documentId}/download-policy-url`,
+      `${applicationContext.getBaseUrl()}/documents/${documentId}/download-policy-url`,
       {
         headers: {
           Authorization: `Bearer ${applicationContext.getCurrentUserToken()}`,
