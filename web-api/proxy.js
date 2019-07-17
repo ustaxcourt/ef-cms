8ca9bea874a075c3bfd3e78f956049ad2247ae0c
Eric Sorenson <eric@sorenson.software>
2019-07-17 09:08:53 -0500

    moving users into a new serverless file

--- web-api/proxy.js
+++ web-api/proxy.js
@@ -3,18 +3,34 @@ var proxy = require('http-proxy-middleware');
 
 var app = express();
 
-app.use('/api', proxy({
-  pathRewrite: {
-    '^/api': '/',
-  }, 
-  target: 'http://localhost:3001',
-}));
+app.use(
+  '/api',
+  proxy({
+    pathRewrite: {
+      '^/api': '/',
+    },
+    target: 'http://localhost:3001',
+  }),
+);
 
-app.use('/cases', proxy({
-  pathRewrite: {
-    '^/cases': '/',
-  }, 
-  target: 'http://localhost:3002',
-}));
+app.use(
+  '/cases',
+  proxy({
+    pathRewrite: {
+      '^/cases': '/',
+    },
+    target: 'http://localhost:3002',
+  }),
+);
+
+app.use(
+  '/users',
+  proxy({
+    pathRewrite: {
+      '^/cases': '/',
+    },
+    target: 'http://localhost:3003',
+  }),
+);
 
 app.listen(3000);
---
+++ web-api/run-serverless-users.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+slsStage=$1
+region=$2
+
+USER_POOL_ID=$(aws cognito-idp list-user-pools --query "UserPools[?Name == 'efcms-${slsStage}'].Id | [0]" --max-results 30 --region "us-east-1")
+USER_POOL_ID="${USER_POOL_ID%\"}"
+USER_POOL_ID="${USER_POOL_ID#\"}"
+
+ACCOUNT_ID=$(aws sts get-caller-identity --query "Account")
+ACCOUNT_ID="${ACCOUNT_ID%\"}"
+ACCOUNT_ID="${ACCOUNT_ID#\"}"
+export NODE_PRESERVE_SYMLINKS=1
+find ./src -type f -exec chmod -R ugo+r {} ";"
+
+npm run build
+cp src/handlers.js /tmp
+cp dist/* src
+
+SLS_DEPLOYMENT_BUCKET="${EFCMS_DOMAIN}.efcms.${slsStage}.${region}.deploys"
+SLS_DEPLOYMENT_BUCKET="${SLS_DEPLOYMENT_BUCKET}" ./node_modules/.bin/sls create_domain --config "serverless-users.yml" --stage "${slsStage}" --region "${region}" --domain "${EFCMS_DOMAIN}" --userPoolId "${USER_POOL_ID}" --efcmsTableName="efcms-${slsStage}" --accountId "${ACCOUNT_ID}" --verbose
+
+ENVIRONMENT="${slsStage}" SLS_DEPLOYMENT_BUCKET="${SLS_DEPLOYMENT_BUCKET}" ./node_modules/.bin/sls deploy --config "serverless-users.yml" --stage "${slsStage}" --region "${region}" --domain "${EFCMS_DOMAIN}"  --userPoolId "${USER_POOL_ID}" --verbose --efcmsTableName="efcms-${slsStage}" --accountId "${ACCOUNT_ID}"
+./configure-custom-api-access-logging.sh "${slsStage}" ./config-custom-access-logs.json "${region}"
+
+cp /tmp/handlers.js src
