e77940162664f0a54f5fdf1db5e901b9b6b7d1d9
Joseph Sutton <jsutton@flexion.us>
2019-07-17 13:54:12 -0500

    fixed shell scripts

--- web-api/run-serverless-users.sh
+++ web-api/run-serverless-users.sh
@@ -14,7 +14,7 @@ export NODE_PRESERVE_SYMLINKS=1
 find ./src -type f -exec chmod -R ugo+r {} ";"
 
 npm run build
-cp src/handlers.js /tmp
+cp src/userHandlers.js /tmp
 cp dist/* src
 
 SLS_DEPLOYMENT_BUCKET="${EFCMS_DOMAIN}.efcms.${slsStage}.${region}.deploys"
@@ -23,4 +23,4 @@ SLS_DEPLOYMENT_BUCKET="${SLS_DEPLOYMENT_BUCKET}" ./node_modules/.bin/sls create_
 ENVIRONMENT="${slsStage}" SLS_DEPLOYMENT_BUCKET="${SLS_DEPLOYMENT_BUCKET}" ./node_modules/.bin/sls deploy --config "serverless-users.yml" --stage "${slsStage}" --region "${region}" --domain "${EFCMS_DOMAIN}"  --userPoolId "${USER_POOL_ID}" --verbose --efcmsTableName="efcms-${slsStage}" --accountId "${ACCOUNT_ID}"
 ./configure-custom-api-access-logging.sh "${slsStage}" ./config-custom-access-logs.json "${region}"
 
-cp /tmp/handlers.js src
+cp /tmp/userHandlers.js src
