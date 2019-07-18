6808e02708c782ae3e07a34c98f14beb874288ca
Eric Sorenson <eric@sorenson.software>
2019-07-18 15:59:42 -0500

    move user lambdas into the right place

--- web-api/src/apiHandlers.js
+++ web-api/src/apiHandlers.js
@@ -3,26 +3,8 @@ module.exports = {
     .handler,
   createCourtIssuedOrderPdfFromHtmlLambda: require('./courtIssuedOrder/createCourtIssuedOrderPdfFromHtmlLambda')
     .handler,
-  getCasesByUserLambda: require('./cases/getCasesByUserLambda').handler,
-  getDocumentQCBatchedForUserLambda: require('./workitems/getDocumentQCBatchedForUserLambda')
-    .handler,
-  getDocumentQCInboxForUserLambda: require('./workitems/getDocumentQCInboxForUserLambda')
-    .handler,
-  getDocumentQCServedForUserLambda: require('./workitems/getDocumentQCServedForUserLambda')
-    .handler,
-  getInboxMessagesForUserLambda: require('./workitems/getInboxMessagesForUserLambda')
-    .handler,
   getNotificationsLambda: require('./users/getNotificationsLambda').handler,
-  getSentMessagesForUserLambda: require('./workitems/getSentMessagesForUserLambda')
-    .handler,
-  practitionerCaseAssociationLambda: require('./cases/practitionerCaseAssociationLambda')
-    .handler,
-  practitionerPendingCaseAssociationLambda: require('./cases/practitionerPendingCaseAssociationLambda')
-    .handler,
   runBatchProcessLambda: require('./cases/runBatchProcessLambda').handler,
   swaggerJsonLambda: require('./swagger/swaggerJsonLambda').handler,
   swaggerLambda: require('./swagger/swaggerLambda').handler,
-  verifyCaseForUserLambda: require('./cases/verifyCaseForUserLambda').handler,
-  verifyPendingCaseForUserLambda: require('./cases/verifyPendingCaseForUserLambda')
-    .handler,
 };
--- web-api/src/usersHandlers.js
+++ web-api/src/usersHandlers.js
@@ -1,4 +1,22 @@
 module.exports = {
   createUserLambda: require('./users/createUserLambda').handler,
+  getCasesByUserLambda: require('./cases/getCasesByUserLambda').handler,
+  getDocumentQCBatchedForUserLambda: require('./workitems/getDocumentQCBatchedForUserLambda')
+    .handler,
+  getDocumentQCInboxForUserLambda: require('./workitems/getDocumentQCInboxForUserLambda')
+    .handler,
+  getDocumentQCServedForUserLambda: require('./workitems/getDocumentQCServedForUserLambda')
+    .handler,
+  getInboxMessagesForUserLambda: require('./workitems/getInboxMessagesForUserLambda')
+    .handler,
   getInternalUsersLambda: require('./users/getInternalUsersLambda').handler,
+  getSentMessagesForUserLambda: require('./workitems/getSentMessagesForUserLambda')
+    .handler,
+  practitionerCaseAssociationLambda: require('./cases/practitionerCaseAssociationLambda')
+    .handler,
+  practitionerPendingCaseAssociationLambda: require('./cases/practitionerPendingCaseAssociationLambda')
+    .handler,
+  verifyCaseForUserLambda: require('./cases/verifyCaseForUserLambda').handler,
+  verifyPendingCaseForUserLambda: require('./cases/verifyPendingCaseForUserLambda')
+    .handler,
 };
