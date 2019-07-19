6efdfd2558f72b79f733c728dbe7aa9a855e9e82
Joseph Sutton <jsutton@flexion.us>
2019-07-19 10:59:41 -0500

    created ses terraform files

---
+++ web-api/terraform/template/ses.tf
@@ -0,0 +1,57 @@
+resource "aws_ses_domain_identity" "email_domain_east" {
+  provider = "aws.us-east-1"
+  domain = "${var.dns_domain}"
+}
+
+resource "aws_ses_domain_mail_from" "main" {
+  domain           = "${aws_ses_domain_identity.email_domain_east.domain}"
+  mail_from_domain = "noreply.${aws_ses_domain_identity.email_domain_east.domain}"
+}
+
+# SPF Validation Record
+resource "aws_route53_record" "spf_mail_from" {
+  zone_id = "${data.aws_route53_zone.zone.id}"
+  name    = "${aws_ses_domain_mail_from.main.mail_from_domain}"
+  type    = "TXT"
+  ttl     = "600"
+  records = ["v=spf1 include:amazonses.com -all"]
+}
+
+resource "aws_route53_record" "spf_domain" {
+  zone_id = "${data.aws_route53_zone.zone.id}"
+  name    = "${var.dns_domain}"
+  type    = "TXT"
+  ttl     = "600"
+  records = ["v=spf1 include:amazonses.com -all"]
+}
+
+# Sending MX Record
+data "aws_region" "current" {}
+
+resource "aws_route53_record" "mx_send_mail_from" {
+  zone_id = "${data.aws_route53_zone.zone.id}"
+  name    = "${aws_ses_domain_mail_from.main.mail_from_domain}"
+  type    = "MX"
+  ttl     = "600"
+  records = ["10 feedback-smtp.${data.aws_region.current.name}.amazonses.com"]
+}
+
+# Receiving MX Record
+resource "aws_route53_record" "mx_receive" {
+  zone_id = "${data.aws_route53_zone.zone.id}"
+  name    = "${var.dns_domain}"
+  type    = "MX"
+  ttl     = "600"
+  records = ["10 inbound-smtp.${data.aws_region.current.name}.amazonaws.com"]
+}
+
+#
+# DMARC TXT Record
+#
+resource "aws_route53_record" "txt_dmarc" {
+  zone_id = "${data.aws_route53_zone.zone.id}"
+  name    = "_dmarc.${var.dns_domain}"
+  type    = "TXT"
+  ttl     = "600"
+  records = ["v=DMARC1; p=none; rua=mailto:${var.ses_dmarc_rua};"]
+}
