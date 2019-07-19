cb123144c0d8668a19ddf9b5ff1fb9a711f2f8cc
Cody Seibert <cseibert113@gmail.com>
2019-07-19 14:13:08 -0400

    removing path

--- web-client/src/presenter/sequences/serveDocumentSequence.js
+++ web-client/src/presenter/sequences/serveDocumentSequence.js
@@ -1,12 +1,4 @@
 import { clearModalAction } from '../actions/clearModalAction';
 import { serveDocumentAction } from '../actions/serveDocumentAction';
-import { setAlertErrorAction } from '../actions/setAlertErrorAction';
 
-export const serveDocumentSequence = [
-  serveDocumentAction,
-  {
-    error: [setAlertErrorAction],
-    success: [],
-  },
-  clearModalAction,
-];
+export const serveDocumentSequence = [serveDocumentAction, clearModalAction];
