d1f9c11b3d7bd62e572579b7d81998927d841362
Cody Seibert <cseibert113@gmail.com>
2019-07-19 14:05:47 -0400

    creating the sequence for serving the document

---
+++ web-client/src/presenter/actions/serveDocumentAction.js
@@ -0,0 +1,21 @@
+import { state } from 'cerebral';
+
+/**
+ * serves the current document
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the applicationContext
+ * @param {Function} providers.get the cerebral get function
+ * @param {object} providers.store the cerebral store used for setting the state.alertError
+ * @returns the path to take
+ */
+export const serveDocumentAction = async ({ applicationContext, get }) => {
+  const documentId = get(state.documentId);
+  const caseId = get(state.caseDetail.caseId);
+
+  await applicationContext.getUseCases().serveSignedStipDecisionInteractor({
+    applicationContext,
+    caseId,
+    documentId,
+  });
+};
