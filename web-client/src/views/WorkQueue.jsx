0c22686e5aebf2a65c39cbe7b77209310644e8f2
Kris Koskelin <kkoskelin@flexion.us>
2019-07-22 11:30:57 -0500

    1299: componentize work queue actions
    add icon to button, fix button sizing for those with icons

--- web-client/src/views/WorkQueue.jsx
+++ web-client/src/views/WorkQueue.jsx
@@ -1,41 +1,20 @@
-import { If } from '../ustc-ui/If/If';
 import { IndividualWorkQueue } from './IndividualWorkQueue';
 import { SectionWorkQueue } from './SectionWorkQueue';
+import { WorkQueueActionButtons } from './WorkQueue/WorkQueueActionButtons';
 import { connect } from '@cerebral/react';
-import { sequences, state } from 'cerebral';
+import { state } from 'cerebral';
 import React from 'react';
 
 export const WorkQueue = connect(
   {
-    runBatchProcessSequence: sequences.runBatchProcessSequence,
     workQueueHelper: state.workQueueHelper,
   },
-  ({ runBatchProcessSequence, workQueueHelper }) => {
+  ({ workQueueHelper }) => {
     return (
       <React.Fragment>
         {workQueueHelper.showIndividualWorkQueue && <IndividualWorkQueue />}
 
         {workQueueHelper.showSectionWorkQueue && <SectionWorkQueue />}
-
-        {workQueueHelper.showStartCaseButton && (
-          <div className="fix-top-right">
-            <a
-              className="usa-button align-right"
-              href="/start-a-case"
-              id="init-file-petition"
-            >
-              Create a Case
-            </a>
-            <If bind="workQueueHelper.showRunBatchIRSProcessButton">
-              <button
-                className="usa-button usa-button--outline margin-left-1"
-                onClick={() => runBatchProcessSequence()}
-              >
-                Run IRS Batch Process
-              </button>
-            </If>
-          </div>
-        )}
       </React.Fragment>
     );
   },
