b0877db1c04ea987dec461fbffff227017c39271
Joseph Sutton <jsutton@flexion.us>
2019-07-23 10:40:32 -0500

    updated SES template

--- web-api/terraform/template/ses.tf
+++ web-api/terraform/template/ses.tf
@@ -95,18 +95,17 @@ resource "aws_ses_template" "case_served" {
   <p>Dear {{name}},</p>
   <p>A document has been served on your Tax Court case:</p>
   <p>
-    {{docketNumber}}<br />
-    {{caseCaption}}
+    Docket Number: {{docketNumber}}<br />
+    Case Name: {{caseCaption}}
   </p>
   <p>
-    {{documentName}}<br />
-    Served {{serviceDate}} {{serviceTime}} EST
+    Document: {{documentName}}<br />
+    Served: {{serviceDate}} at {{serviceTime}} ET
   </p>
   <p>To view this document, please <a href="{{loginUrl}}">log in to the US Tax Court online</a>.</p>
   <p>Certain documents may require your action.</p>
   <p>
-    Please do not reply to this message. This e-mail is an automated
-    notification from an account which is unable to receive replies.
+    Please do not reply to this message. This e-mail is an automated notification from an account which is unable to receive replies.
   </p>
 EOF
 }
