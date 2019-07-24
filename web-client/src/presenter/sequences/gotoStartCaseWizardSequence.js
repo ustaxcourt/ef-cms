728f482f566b7ef02a8cf81503b0dcc0e7dce735
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-07-24 12:06:48 -0500

    1448: create start case wizard

---
+++ web-client/src/presenter/sequences/gotoStartCaseWizardSequence.js
@@ -0,0 +1,45 @@
+import { clearAlertsAction } from '../actions/clearAlertsAction';
+import { clearFormAction } from '../actions/clearFormAction';
+import { clearPetitionAction } from '../actions/clearPetitionAction';
+import { clearScreenMetadataAction } from '../actions/clearScreenMetadataAction';
+import { getCaseTypesAction } from '../actions/getCaseTypesAction';
+import { getFilingTypesAction } from '../actions/getFilingTypesAction';
+import { getProcedureTypesAction } from '../actions/getProcedureTypesAction';
+import { getUserRoleAction } from '../actions/getUserRoleAction';
+import { prepareFormAction } from '../actions/prepareFormAction';
+import { set } from 'cerebral/factories';
+import { setCaseTypesAction } from '../actions/setCaseTypesAction';
+import { setCurrentPageAction } from '../actions/setCurrentPageAction';
+import { setFilingTypesAction } from '../actions/setFilingTypesAction';
+import { setProcedureTypesAction } from '../actions/setProcedureTypesAction';
+import { state } from 'cerebral';
+
+export const gotoStartCaseWizardSequence = [
+  clearAlertsAction,
+  clearPetitionAction,
+  clearFormAction,
+  clearScreenMetadataAction,
+  prepareFormAction,
+  set(state.showValidation, false),
+  getCaseTypesAction,
+  setCaseTypesAction,
+  getProcedureTypesAction,
+  setProcedureTypesAction,
+  getUserRoleAction,
+  {
+    docketclerk: [setCurrentPageAction('StartCaseInternal')],
+    petitioner: [
+      getFilingTypesAction,
+      setFilingTypesAction,
+      set(state.wizardStep, 'StartCaseStep1'),
+      setCurrentPageAction('StartCaseWizard'),
+    ],
+    petitionsclerk: [setCurrentPageAction('StartCaseInternal')],
+    practitioner: [
+      getFilingTypesAction,
+      setFilingTypesAction,
+      set(state.wizardStep, 'StartCaseStep1'),
+      setCurrentPageAction('StartCaseWizard'),
+    ],
+  },
+];
