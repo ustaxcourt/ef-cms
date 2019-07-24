4406daabea9026aa1a7c6032a432cce8c83222ba
Eric Sorenson <eric@sorenson.software>
2019-07-24 15:44:31 -0500

    updating paths to root

--- web-api/start-s3rver.js
+++ web-api/start-s3rver.js
@@ -4,7 +4,7 @@ const S3rver = require('s3rver');
 console.log('starting s3rver');
 
 new S3rver({
-  cors: fs.readFileSync('./cors-policy.xml', 'utf-8'),
+  cors: fs.readFileSync('./web-api/cors-policy.xml', 'utf-8'),
   directory: './storage/s3',
   hostname: '0.0.0.0',
   port: 9000,
