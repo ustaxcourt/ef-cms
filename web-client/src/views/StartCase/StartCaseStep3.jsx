ca352b6c3f1292ee469cc013a681ad8cd04e18da
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-07-24 13:09:06 -0500

    update button styling on all start case wizard steps

--- web-client/src/views/StartCase/StartCaseStep3.jsx
+++ web-client/src/views/StartCase/StartCaseStep3.jsx
@@ -402,7 +402,7 @@ export const StartCaseStep3 = connect(
 
         <div className="button-box-container">
           <button
-            className="usa-button margin-right-205"
+            className="usa-button margin-right-205 margin-bottom-4"
             id="submit-case"
             type="button"
             onClick={() => {
@@ -411,7 +411,11 @@ export const StartCaseStep3 = connect(
           >
             Continue to Step 4 of 5
           </button>
-          <button className="usa-button usa-button--outline" type="button">
+          <button
+            className="usa-button usa-button--outline margin-bottom-1"
+            type="button"
+            onClick={() => history.back()}
+          >
             Back
           </button>
           <button
