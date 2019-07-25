a535f0bd5bbfd09b95540339185f3aff54e07121
Cody Seibert <cseibert113@gmail.com>
2019-07-25 13:27:26 -0400

    loading images from base64 encoded strings when running without a scanner

--- shared/src/business/useCases/getScannerInterfaceInteractor.js
+++ shared/src/business/useCases/getScannerInterfaceInteractor.js
@@ -64,6 +64,43 @@ exports.getScannerInterface = () => {
       };
     };
 
+    const loadDynamsoft = ({ applicationContext, cb }) => {
+      const dynanScriptClass = 'dynam-scanner-injection';
+
+      // Create a script element to inject into the header
+      const script1 = document.createElement('script');
+      script1.type = 'text/javascript';
+      script1.async = true;
+      script1.setAttribute('class', dynanScriptClass);
+
+      // Set a reference so we can remove later
+      // store.set(state.scanner.dynanScriptClass, dynanScriptClass);
+
+      // Reduce duplicating the above code
+      const script2 = script1.cloneNode();
+
+      // Set some state when the scripts are loaded
+      script1.onload = function() {
+        // store.set(state.scanner.initiateScriptLoaded, true);
+
+        script2.onload = function() {
+          // store.set(state.scanner.configScriptLoaded, true);
+          cb(dynanScriptClass);
+        };
+      };
+
+      // Handle script load errors?
+
+      // Get the scanner resources URI based on applicationContext
+      const scannerResourceUri = applicationContext.getScannerResourceUri();
+      script1.src = `${scannerResourceUri}/dynamsoft.webtwain.initiate.js`;
+      script2.src = `${scannerResourceUri}/dynamsoft.webtwain.config.js`;
+
+      // Inject scripts into <head />
+      document.getElementsByTagName('head')[0].appendChild(script1);
+      document.getElementsByTagName('head')[0].appendChild(script2);
+    };
+
     const getSourceStatus = () => {
       // 0	The Data Source is closed
       // 1	The Data Source is opened
@@ -106,6 +143,7 @@ exports.getScannerInterface = () => {
       getSourceNameByIndex,
       getSourceStatus,
       getSources,
+      loadDynamsoft,
       setSourceByIndex,
       setSourceByName,
       startScanSession,
