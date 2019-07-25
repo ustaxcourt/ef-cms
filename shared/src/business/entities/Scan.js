a15dab152c04101d808f712a0cab3c9b90ecfd7f
Joseph Sutton <jsutton@flexion.us>
2019-07-25 15:42:29 -0500

    added validation to Scan entity

--- shared/src/business/entities/Scan.js
+++ shared/src/business/entities/Scan.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const joi = require('joi-browser');
 const {
   joiValidationDecorator,
@@ -18,6 +19,53 @@ function Scan({ applicationContext, rawScan }) {
 
 Scan.name = 'Scan';
 
-Scan.prototype.addBatch = function() {};
+/**
+ * adds a batch to the current scan
+ *
+ * @param {Batch} batch Batch entity
+ * @returns {Scan} Scan entity
+ */
+Scan.prototype.addBatch = function(batch) {
+  this.batches.push(batch);
+  return this;
+};
+
+/**
+ * removes a batch from the current scan
+ *
+ * @param {Batch} batchEntity Batch entity to remove
+ * @returns {Scan} Scan entity
+ */
+Scan.prototype.removeBatch = function(batchEntity) {
+  const { batchId } = batchEntity;
+
+  this.batches = _.remove(this.batches, batch => batchId === batch.batchId);
+
+  return this;
+};
+
+Scan.errorToMessageMap = {
+  batches: 'Invalid batch index',
+  pages: 'At least one page is required',
+};
+
+Scan.schema = joi.object().keys({
+  createdAt: joi
+    .date()
+    .iso()
+    .required(),
+  pages: joi
+    .array()
+    .min(1)
+    .required(),
+  scanId: joi
+    .string()
+    .uuid({
+      version: ['uuidv4'],
+    })
+    .required(),
+});
+
+joiValidationDecorator(Scan, Scan.schema, undefined, Scan.errorToMessageMap);
 
 module.exports = { Scan };
