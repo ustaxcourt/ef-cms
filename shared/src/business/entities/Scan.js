310729c6a7c3eba19f9f873fc6029b6838bfedec
Joseph Sutton <jsutton@flexion.us>
2019-07-26 09:31:27 -0500

    removed nonexistent import

--- shared/src/business/entities/Scan.js
+++ shared/src/business/entities/Scan.js
@@ -3,7 +3,6 @@ const {
   joiValidationDecorator,
 } = require('../../utilities/JoiValidationDecorator');
 const { createISODateString } = require('../utilities/DateHandler');
-const { generatePdfFromPngs } = require('../../utilities/generatePdfFromPngs');
 const { remove } = require('lodash');
 
 /**
