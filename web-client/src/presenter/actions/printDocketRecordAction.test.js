f413b0449272439924093ec89c9acb991dfb0d39
Matt Hopson <matthopson@gmail.com>
2019-07-29 14:53:19 -0600

    Add tests

---
+++ web-client/src/presenter/actions/printDocketRecordAction.test.js
@@ -0,0 +1,58 @@
+import { presenter } from '../presenter';
+import { printDocketRecordAction } from './printDocketRecordAction';
+import { runAction } from 'cerebral/test';
+
+presenter.providers.applicationContext = {
+  getEntityConstructors: () => {
+    return {
+      ContactFactory: {
+        COUNTRY_TYPES: {
+          INTERNATIONAL: 'International',
+        },
+      },
+    };
+  },
+};
+
+const state = {
+  caseDetailHelper: {
+    caseCaptionPostfix: 'Caption Postfix',
+    showCaseNameForPrimary: false,
+  },
+  formattedCaseDetail: {
+    caseCaption: 'Test Caption',
+    caseDetailHelper: {},
+    contactPrimary: {
+      name: 'Test Petitioner',
+    },
+    contactSecondary: {},
+    docketNumberWithSuffix: '123-45S',
+    docketRecordWithDocument: [],
+    practitioners: [],
+    respondents: [],
+  },
+};
+
+describe('printDocketRecordAction', () => {
+  it('generate html for a printable docket record', async () => {
+    const result = await runAction(printDocketRecordAction, {
+      modules: {
+        presenter,
+      },
+      state,
+    });
+
+    expect(result.output.docketNumber).toEqual(
+      state.formattedCaseDetail.docketNumberWithSuffix,
+    );
+    expect(result.output.docketRecordHtml).toContain(
+      state.formattedCaseDetail.caseCaption,
+    );
+    expect(result.output.docketRecordHtml).toContain(
+      state.formattedCaseDetail.contactPrimary.name,
+    );
+    expect(result.output.docketRecordHtml).toContain(
+      state.caseDetailHelper.caseCaptionPostfix,
+    );
+  });
+});
