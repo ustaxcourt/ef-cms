0b1759e7fbcc4f7205aeeea4fc3d2383db721643
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-07-31 13:43:20 -0500

    1324: user updates for practitioners and respondents

--- shared/src/business/entities/User.js
+++ shared/src/business/entities/User.js
@@ -11,6 +11,10 @@ const { getSectionForRole } = require('./WorkQueue');
  */
 function User(rawUser) {
   this.email = rawUser.email;
+  this.addressLine1 = rawUser.addressLine1;
+  this.addressLine2 = rawUser.addressLine2;
+  this.barnumber = rawUser.barnumber;
+  this.phone = rawUser.phone;
   this.name = rawUser.name;
   this.role = rawUser.role || 'petitioner';
   this.section = getSectionForRole(this.role);
@@ -21,6 +25,9 @@ function User(rawUser) {
 joiValidationDecorator(
   User,
   joi.object().keys({
+    addressLine1: joi.string().optional(),
+    addressLine2: joi.string().optional(),
+    barnumber: joi.string().optional(),
     email: joi.string().optional(),
     name: joi.string().optional(),
     token: joi.string().optional(),
