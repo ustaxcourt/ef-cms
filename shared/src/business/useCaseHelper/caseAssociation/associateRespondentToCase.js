f7d9f3fd77f34bd663e2659e7924567ac48893b3
William Bagayoko <wbagayoko@gmail.com>
2019-07-31 16:30:01 -0500

    1324 Manual Association

---
+++ shared/src/business/useCaseHelper/caseAssociation/associateRespondentToCase.js
@@ -0,0 +1,50 @@
+const { Case } = require('../../entities/cases/Case');
+
+/**
+ * associateRespondentToCase
+ *
+ * @param caseId
+ * @param applicationContext
+ * @returns {Promise<*>}
+ */
+exports.associateRespondentToCase = async ({
+  applicationContext,
+  caseId,
+  user,
+}) => {
+  const isAssociated = await applicationContext
+    .getPersistenceGateway()
+    .verifyCaseForUser({
+      applicationContext,
+      caseId,
+      userId: user.userId,
+    });
+
+  if (!isAssociated) {
+    await applicationContext.getPersistenceGateway().associateUserWithCase({
+      applicationContext,
+      caseId: caseId,
+      userId: user.userId,
+    });
+
+    const caseToUpdate = await applicationContext
+      .getPersistenceGateway()
+      .getCaseByCaseId({
+        applicationContext,
+        caseId,
+      });
+
+    const caseEntity = new Case(caseToUpdate);
+
+    caseEntity.attachRespondent({
+      user,
+    });
+
+    await applicationContext.getPersistenceGateway().updateCase({
+      applicationContext,
+      caseToUpdate: caseEntity.validate().toRawObject(),
+    });
+
+    return caseEntity.toRawObject();
+  }
+};
