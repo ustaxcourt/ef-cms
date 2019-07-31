822bb01b9bc426501790ba3528a5b49f4ee19022
Joseph Sutton <jsutton@flexion.us>
2019-07-31 07:53:18 -0500

    added integration test for deleting a batch

---
+++ web-client/integration-tests/journey/petitionsClerkDeletesScannedBatch.js
@@ -0,0 +1,14 @@
+export default test => {
+  return it('Petitions clerk deletes a batch', async () => {
+    const selectedDocumentType = test.getState('documentSelectedForScan');
+    const batches = test.getState(`batches.${selectedDocumentType}`);
+
+    await test.runSequence('openConfirmDeleteBatchModalSequence', {
+      batchIndexToDelete: batches[0],
+    });
+
+    await test.runSequence('removeBatchSequence');
+
+    expect(test.getState(`batches.${selectedDocumentType}`)).toHaveLength(0);
+  });
+};
