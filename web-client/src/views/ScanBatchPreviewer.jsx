56748412639d171e0e1920790840a2d46339f57b
Matt Hopson <matthopson@gmail.com>
2019-08-01 11:31:51 -0600

    Add voiceover support

--- web-client/src/views/ScanBatchPreviewer.jsx
+++ web-client/src/views/ScanBatchPreviewer.jsx
@@ -202,6 +202,9 @@ export const ScanBatchPreviewer = connect(
                   <tr key={batch.index}>
                     <td>
                       <button
+                        aria-label={`batch ${batch.index + 1} -- ${
+                          batch.pages.length
+                        } pages total`}
                         className="usa-button usa-button--unstyled"
                         onClick={e => {
                           e.preventDefault();
@@ -218,6 +221,7 @@ export const ScanBatchPreviewer = connect(
                     </td>
                     <td>
                       <button
+                        aria-label={`rescan batch ${batch.index + 1}`}
                         className="usa-button usa-button--unstyled no-underline"
                         onClick={e => {
                           e.preventDefault();
@@ -232,6 +236,9 @@ export const ScanBatchPreviewer = connect(
                     </td>
                     <td>
                       <button
+                        aria-label={`remove batch ${batch.index + 1} - with ${
+                          batch.pages.length
+                        } total pages`}
                         className="usa-button usa-button--unstyled no-underline red-warning float-right"
                         onClick={e => {
                           e.preventDefault();
@@ -439,6 +446,7 @@ export const ScanBatchPreviewer = connect(
                     Scanner: {scanBatchPreviewerHelper.scannerSource || 'None'}
                   </span>
                   <button
+                    aria-label="change scanner source"
                     className="usa-button usa-button--unstyled change-scanner-button"
                     style={{ color: 'white' }}
                     onClick={e => {
