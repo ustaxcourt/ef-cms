3708d0c10c649bb50d19311a57e82c6852e4e944
William Bagayoko <wbagayoko@gmail.com>
2019-08-01 11:04:48 -0500

    1324 associatePractitionerWithCase

---
+++ shared/src/business/useCases/manualAssociation/associatePractitionerWithCaseInteractor.js
@@ -0,0 +1,39 @@
+const {
+  ASSOCIATE_USER_WITH_CASE,
+  isAuthorized,
+} = require('../../../authorization/authorizationClientService');
+const {
+  associatePractitionerToCase,
+} = require('../../useCaseHelper/caseAssociation/associatePractitionerToCase');
+const { UnauthorizedError } = require('../../../errors/errors');
+
+/**
+ * associatePractitionerWithCaseInteractor
+ *
+ * @param {object} params the params object
+ * @param {object} params.applicationContext the application context
+ * @param {string} params.caseId the case id
+ * @param {string} params.userId the user id
+ * @returns {*} the result
+ */
+exports.associatePractitionerWithCaseInteractor = async ({
+  applicationContext,
+  caseId,
+  userId,
+}) => {
+  const authenticatedUser = applicationContext.getCurrentUser();
+
+  if (!isAuthorized(authenticatedUser, ASSOCIATE_USER_WITH_CASE)) {
+    throw new UnauthorizedError('Unauthorized');
+  }
+
+  const user = await applicationContext
+    .getPersistenceGateway()
+    .getUserById({ applicationContext, userId });
+
+  return await associatePractitionerToCase({
+    applicationContext,
+    caseId,
+    user,
+  });
+};
