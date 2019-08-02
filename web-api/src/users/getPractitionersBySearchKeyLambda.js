beee576ac9e33892d5185494828b2598587e2dfc
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-08-02 09:30:02 -0500

    switch to GET requests

--- web-api/src/users/getPractitionersBySearchKeyLambda.js
+++ web-api/src/users/getPractitionersBySearchKeyLambda.js
@@ -15,7 +15,7 @@ exports.handler = event =>
     const user = getUserFromAuthHeader(event);
     const applicationContext = createApplicationContext(user);
     try {
-      const { searchKey } = JSON.parse(event.body);
+      const { searchKey } = event.queryStringParameters;
       const results = await applicationContext
         .getUseCases()
         .getPractitionersBySearchKeyInteractor({
