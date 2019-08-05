d2f3122ab6880ea66f61f00ec12242948fbad34a
William Bagayoko <wbagayoko@gmail.com>
2019-08-05 13:34:51 -0500

    1324 path update

--- web-client/src/presenter/actions/caseAssociation/validateAddPractitionerAction.test.js
+++ web-client/src/presenter/actions/caseAssociation/validateAddPractitionerAction.test.js
@@ -1,4 +1,4 @@
-import { presenter } from '../presenter';
+import { presenter } from '../../presenter';
 import { runAction } from 'cerebral/test';
 import { validateAddPractitionerAction } from './validateAddPractitionerAction';
 import sinon from 'sinon';
