a33c0312c28da5ca239b2d70cbc87f812a34c12f
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-08-05 09:34:53 -0500

    1324: integration tests

---
+++ web-client/integration-tests/petitionsClerkCounselAssociationJourney.test.js
@@ -0,0 +1,132 @@
+import { Case } from '../../shared/src/business/entities/cases/Case';
+import { CerebralTest } from 'cerebral/test';
+import { Document } from '../../shared/src/business/entities/Document';
+import { TrialSession } from '../../shared/src/business/entities/trialSessions/TrialSession';
+import { applicationContext } from '../src/applicationContext';
+import { isFunction, mapValues } from 'lodash';
+import { presenter } from '../src/presenter/presenter';
+import { uploadPetition } from './helpers';
+import { withAppContextDecorator } from '../src/withAppContext';
+import FormData from 'form-data';
+import petitionsClerkAddsPractitionersToCase from './journey/petitionsClerkAddsPractitionersToCase';
+import petitionsClerkAddsRespondentsToCase from './journey/petitionsClerkAddsRespondentsToCase';
+import petitionsClerkLogIn from './journey/petitionsClerkLogIn';
+import petitionsClerkSignsOut from './journey/petitionsClerkSignsOut';
+import petitionsClerkViewsCaseDetail from './journey/petitionsClerkViewsCaseDetail';
+import taxPayerSignsOut from './journey/taxpayerSignsOut';
+import taxpayerLogin from './journey/taxpayerLogIn';
+import taxpayerViewsDashboard from './journey/taxpayerViewsDashboard';
+const {
+  ContactFactory,
+} = require('../../shared/src/business/entities/contacts/ContactFactory');
+const { Order } = require('../../shared/src/business/entities/orders/Order');
+
+let test;
+
+const fakeData =
+  'JVBERi0xLjEKJcKlwrHDqwoKMSAwIG9iagogIDw8IC9UeXBlIC9DYXRhbG9nCiAgICAgL1BhZ2VzIDIgMCBSCiAgPj4KZW5kb2JqCgoyIDAgb2JqCiAgPDwgL1R5cGUgL1BhZ2VzCiAgICAgL0tpZHMgWzMgMCBSXQogICAgIC9Db3VudCAxCiAgICAgL01lZGlhQm94IFswIDAgMzAwIDE0NF0KICA+PgplbmRvYmoKCjMgMCBvYmoKICA8PCAgL1R5cGUgL1BhZ2UKICAgICAgL1BhcmVudCAyIDAgUgogICAgICAvUmVzb3VyY2VzCiAgICAgICA8PCAvRm9udAogICAgICAgICAgIDw8IC9GMQogICAgICAgICAgICAgICA8PCAvVHlwZSAvRm9udAogICAgICAgICAgICAgICAgICAvU3VidHlwZSAvVHlwZTEKICAgICAgICAgICAgICAgICAgL0Jhc2VGb250IC9UaW1lcy1Sb21hbgogICAgICAgICAgICAgICA+PgogICAgICAgICAgID4+CiAgICAgICA+PgogICAgICAvQ29udGVudHMgNCAwIFIKICA+PgplbmRvYmoKCjQgMCBvYmoKICA8PCAvTGVuZ3RoIDg0ID4+CnN0cmVhbQogIEJUCiAgICAvRjEgMTggVGYKICAgIDUgODAgVGQKICAgIChDb25ncmF0aW9ucywgeW91IGZvdW5kIHRoZSBFYXN0ZXIgRWdnLikgVGoKICBFVAplbmRzdHJlYW0KZW5kb2JqCgp4cmVmCjAgNQowMDAwMDAwMDAwIDY1NTM1IGYgCjAwMDAwMDAwMTggMDAwMDAgbiAKMDAwMDAwMDA3NyAwMDAwMCBuIAowMDAwMDAwMTc4IDAwMDAwIG4gCjAwMDAwMDA0NTcgMDAwMDAgbiAKdHJhaWxlcgogIDw8ICAvUm9vdCAxIDAgUgogICAgICAvU2l6ZSA1CiAgPj4Kc3RhcnR4cmVmCjU2NQolJUVPRgo=';
+
+const fakeFile = Buffer.from(fakeData, 'base64');
+fakeFile.name = 'fakeFile.pdf';
+
+global.FormData = FormData;
+global.Blob = () => {};
+global.File = () => {
+  return fakeFile;
+};
+global.URL = {
+  createObjectURL: () => {
+    return fakeData;
+  },
+};
+presenter.providers.applicationContext = applicationContext;
+presenter.providers.router = {
+  externalRoute: () => null,
+  route: async url => {
+    if (url === `/case-detail/${test.docketNumber}`) {
+      await test.runSequence('gotoCaseDetailSequence', {
+        docketNumber: test.docketNumber,
+      });
+    }
+
+    if (url === '/') {
+      await test.runSequence('gotoDashboardSequence');
+    }
+  },
+};
+
+presenter.state = mapValues(presenter.state, value => {
+  if (isFunction(value)) {
+    return withAppContextDecorator(value, applicationContext);
+  }
+  return value;
+});
+
+test = CerebralTest(presenter);
+
+describe('Petitions Clerk Counsel Association Journey', () => {
+  beforeEach(() => {
+    jest.setTimeout(30000);
+    global.window = {
+      DOMParser: () => {
+        return {
+          parseFromString: () => {
+            return {
+              children: [
+                {
+                  innerHTML: 'something',
+                },
+              ],
+              querySelector: () => {},
+            };
+          },
+        };
+      },
+      URL: {
+        createObjectURL: () => {
+          return fakeData;
+        },
+        revokeObjectURL: () => {},
+      },
+      localStorage: {
+        removeItem: () => null,
+        setItem: () => null,
+      },
+    };
+
+    test.setState('constants', {
+      CASE_CAPTION_POSTFIX: Case.CASE_CAPTION_POSTFIX,
+      CATEGORIES: Document.CATEGORIES,
+      CATEGORY_MAP: Document.CATEGORY_MAP,
+      COUNTRY_TYPES: ContactFactory.COUNTRY_TYPES,
+      INTERNAL_CATEGORY_MAP: Document.INTERNAL_CATEGORY_MAP,
+      ORDER_TYPES_MAP: Order.ORDER_TYPES,
+      PARTY_TYPES: ContactFactory.PARTY_TYPES,
+      TRIAL_CITIES: TrialSession.TRIAL_CITIES,
+    });
+  });
+
+  taxpayerLogin(test);
+  it('Create test case', async () => {
+    await uploadPetition(test, {
+      contactSecondary: {
+        address1: '734 Cowley Parkway',
+        city: 'Amazing',
+        countryType: 'domestic',
+        name: 'Jimothy Schultz',
+        phone: '+1 (884) 358-9729',
+        postalCode: '77546',
+        state: 'AZ',
+      },
+      partyType: 'Petitioner & Spouse',
+    });
+  });
+  taxpayerViewsDashboard(test);
+  taxPayerSignsOut(test);
+
+  petitionsClerkLogIn(test);
+  petitionsClerkViewsCaseDetail(test);
+  petitionsClerkAddsPractitionersToCase(test);
+  petitionsClerkAddsRespondentsToCase(test);
+  petitionsClerkSignsOut(test);
+});
