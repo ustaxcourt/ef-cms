c447311943bbcf52e663979dccd50d11147035a5
Joseph Sutton <jsutton@flexion.us>
2019-08-05 13:58:37 -0500

    fixed acronym for Ownership Disclosure Statement

--- web-client/src/views/ScanBatchPreviewer.jsx
+++ web-client/src/views/ScanBatchPreviewer.jsx
@@ -516,9 +516,9 @@ export const ScanBatchPreviewer = connect(
             <Tab
               icon={['fas', 'check-circle']}
               iconColor="green"
-              showIcon={scanHelper.osdFileCompleted}
+              showIcon={scanHelper.odsFileCompleted}
               tabName="ownershipDisclosureFile"
-              title="OSD"
+              title="ODS"
             />
           </Tabs>
 
