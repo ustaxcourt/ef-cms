a3b969ceed67f2348417a9e059f6585549279d22
Matt Hopson <matthopson@gmail.com>
2019-08-06 08:06:51 -0600

    Update docs

--- web-client/src/presenter/actions/startScanAction.js
+++ web-client/src/presenter/actions/startScanAction.js
@@ -6,6 +6,7 @@ import { state } from 'cerebral';
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context used for getting the scanner API
  * @param {Function} providers.store the cerebral store used for setting state.path
+ * @returns {object} the path to execute
  *
  */
 export const startScanAction = async ({
