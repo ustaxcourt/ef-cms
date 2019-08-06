a404961cbac729305531d2f530e99b5f21aaab67
Kris Koskelin <kkoskelin@flexion.us>
2019-08-06 15:57:42 -0500

    1272: judge dashboard start (wip)
    [skip ci]

---
+++ web-client/src/views/TrialSessions/TrialSessionsSummary.jsx
@@ -0,0 +1,58 @@
+import { connect } from '@cerebral/react';
+import { state } from 'cerebral';
+import React from 'react';
+
+export const TrialSessionsSummary = connect(
+  {
+    recentTrialSessions: state.formattedTrialSessions.formattedSessions,
+    upcomingTrialSessions: state.formattedTrialSessions.formattedSessions,
+  },
+  ({ recentTrialSessions, upcomingTrialSessions }) => {
+    return (
+      <React.Fragment>
+        <table
+          aria-label="trial sessions"
+          className="usa-table ustc-table trial-sessions subsection"
+          id="sessions-summary"
+        >
+          <thead>
+            <tr>
+              <th colSpan="3">Upcoming Trial Sessions</th>
+              <th>
+                <button className="usa-button--unstyled">View All</button>
+              </th>
+            </tr>
+          </thead>
+          {upcomingTrialSessions.map((trialDate, idxDate) => (
+            <React.Fragment key={idxDate}>
+              <tbody>
+                <tr className="trial-date">
+                  <td colSpan="3">{trialDate.dateFormatted}</td>
+                  <td colSpan="3">{trialDate.locationFormatted}</td>
+                </tr>
+              </tbody>
+            </React.Fragment>
+          ))}
+          <thead>
+            <tr>
+              <th colSpan="2">Recent Trial Sessions</th>
+              <th>
+                <button className="usa-button--unstyled">View All</button>
+              </th>
+            </tr>
+          </thead>
+          {recentTrialSessions.map((trialDate, idxDate) => (
+            <React.Fragment key={idxDate}>
+              <tbody>
+                <tr className="trial-date">
+                  <td colSpan="3">{trialDate.dateFormatted}</td>
+                  <td colSpan="3">{trialDate.locationFormatted}</td>
+                </tr>
+              </tbody>
+            </React.Fragment>
+          ))}
+        </table>
+      </React.Fragment>
+    );
+  },
+);
