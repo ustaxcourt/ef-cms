97ba44ded5b5ad11abe3818086cdf71d17a1922e
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-08-06 15:00:20 -0500

    1324: clear search bar when counsel has been added

--- web-client/src/presenter/sequences/caseAssociation/associatePractitionerWithCaseSequence.js
+++ web-client/src/presenter/sequences/caseAssociation/associatePractitionerWithCaseSequence.js
@@ -1,5 +1,6 @@
 import { associatePractitionerWithCaseAction } from '../../actions/ManualAssociation/associatePractitionerWithCaseAction';
 import { clearAlertsAction } from '../../actions/clearAlertsAction';
+import { clearFormAction } from '../../actions/clearFormAction';
 import { clearModalAction } from '../../actions/clearModalAction';
 import { clearModalStateAction } from '../../actions/clearModalStateAction';
 import { getCaseAction } from '../../actions/getCaseAction';
@@ -25,6 +26,7 @@ export const associatePractitionerWithCaseSequence = [
           setAlertSuccessAction,
           clearModalAction,
           clearModalStateAction,
+          clearFormAction,
           setCasePropFromStateAction,
           getCaseAction,
           setCaseAction,
