ab74a76f9116c02881ef9313d095e36aec83d208
Kris Koskelin <kkoskelin@flexion.us>
2019-08-07 16:19:01 -0500

    1272: upcoming sessions style adjustments
    using grid system, better layout and more accessible; adding initial pa11y tests

--- web-client/src/views/TrialSessions/TrialSessionsSummary.jsx
+++ web-client/src/views/TrialSessions/TrialSessionsSummary.jsx
@@ -12,55 +12,68 @@ export const TrialSessionsSummary = connect(
   },
   ({ recentTrialSessions, upcomingTrialSessions, user }) => {
     return (
-      <React.Fragment>
-        <table
-          aria-label="trial sessions"
-          className="usa-table ustc-table trial-sessions subsection"
-          id="sessions-summary"
-        >
-          <thead>
-            <tr>
-              <th colSpan="3">Upcoming Trial Sessions</th>
-              <th>
-                <a
-                  className="usa-button usa-button--unstyled"
-                  href={`/trial-sessions?judge=${user.userId}`}
-                >
-                  View All
-                </a>
-              </th>
-            </tr>
-          </thead>
-          {upcomingTrialSessions.map((trialDate, idxDate) => (
-            <React.Fragment key={idxDate}>
-              <tbody>
-                <tr className="trial-date">
-                  <td colSpan="3">{trialDate.formattedStartDate}</td>
-                  <td colSpan="3">{trialDate.trialLocation}</td>
-                </tr>
-              </tbody>
-            </React.Fragment>
-          ))}
-          <thead>
-            <tr>
-              <th colSpan="3">Recent Trial Sessions</th>
-              <th>
-                <button className="usa-button--unstyled">View All</button>
-              </th>
-            </tr>
-          </thead>
-          {recentTrialSessions.map((trialDate, idxDate) => (
-            <React.Fragment key={idxDate}>
-              <tbody>
-                <tr className="trial-date">
-                  <td colSpan="3">{trialDate.formattedStartDate}</td>
-                  <td colSpan="3">{trialDate.trialLocation}</td>
-                </tr>
-              </tbody>
-            </React.Fragment>
-          ))}
-        </table>
-      </React.Fragment>
+      <div className="card">
+        <div className="grid-container content-wrapper gray">
+          <div className="grid-row underlined">
+            <div className="grid-col-8">
+              <h3>Upcoming Trial Sessions</h3>
+            </div>
+            <div className="grid-col-4">
+              <a
+                className="usa-link float-right"
+                href={`/trial-sessions?judge=${user.userId}`}
+              >
+                View All
+              </a>
+            </div>
+          </div>
+          <div role="list">
+            {upcomingTrialSessions.map((trialDate, idxDate) => (
+              <div
+                className="grid-row margin-top-4 margin-bottom-4"
+                key={idxDate}
+                role="listitem"
+              >
+                <div className="grid-col-6">{trialDate.formattedStartDate}</div>
+                <div className="grid-col-6">
+                  <a href="/trial-session-details/">
+                    {trialDate.trialLocation}
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="grid-row underlined margin-top-4">
+            <div className="grid-col-8">
+              <h3>Recent Trial Sessions</h3>
+            </div>
+            <div className="grid-col-4">
+              <a
+                className="usa-link float-right"
+                href={`/trial-sessions?type=recent&judge=${user.userId}`}
+              >
+                View All
+              </a>
+            </div>
+          </div>
+          <div className="margin-bottom-0" role="list">
+            {recentTrialSessions.map((trialDate, idxDate) => (
+              <div
+                className="grid-row margin-top-4 margin-bottom-4"
+                key={idxDate}
+                role="listitem"
+              >
+                <div className="grid-col-6">{trialDate.formattedStartDate}</div>
+                <div className="grid-col-6">
+                  <a href="/trial-session-details/">
+                    {trialDate.trialLocation}
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     );
   },
 );
