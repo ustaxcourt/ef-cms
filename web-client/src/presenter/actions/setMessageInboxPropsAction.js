0e23dfa51b5c6c468acc794d57cf7345da78fb2f
William Bagayoko <wbagayoko@gmail.com>
2019-08-09 10:07:28 -0500

    1272 set props to ensure judge inbox is brought up on dashboard

---
+++ web-client/src/presenter/actions/setMessageInboxPropsAction.js
@@ -0,0 +1,12 @@
+/**
+ * sets the props so that message inbox is shown
+ *
+ * @returns {object} the props for message inbox to show
+ */
+export const setMessageInboxPropsAction = () => {
+  return {
+    box: 'inbox',
+    queue: 'my',
+    workQueueIsInternal: true,
+  };
+};
