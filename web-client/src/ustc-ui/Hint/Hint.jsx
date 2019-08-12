0109a1e6504604c175c493c6097298a4a6892a39
Cody Seibert <cseibert113@gmail.com>
2019-08-12 11:47:11 -0400

    adding a gold alert

--- web-client/src/ustc-ui/Hint/Hint.jsx
+++ web-client/src/ustc-ui/Hint/Hint.jsx
@@ -3,12 +3,25 @@ import { connect } from '@cerebral/react';
 import React from 'react';
 
 export const Hint = connect(props => {
-  const { children } = props;
+  const { children, exclamation, fullWidth } = props;
 
   return (
-    <div className="alert-gold add-bottom-margin">
+    <div
+      className={`alert-gold add-bottom-margin ${
+        fullWidth ? 'full-width' : ''
+      }`}
+    >
       <span className="usa-hint ustc-form-hint-with-svg">
-        <FontAwesomeIcon className="fa-icon-gold" icon="flag" size="lg" />
+        {!exclamation && (
+          <FontAwesomeIcon className="fa-icon-gold" icon="flag" size="lg" />
+        )}
+        {exclamation && (
+          <FontAwesomeIcon
+            className="fa-icon-gold"
+            icon={['fas', 'exclamation-circle']}
+            size="lg"
+          />
+        )}
         {children}
       </span>
     </div>
