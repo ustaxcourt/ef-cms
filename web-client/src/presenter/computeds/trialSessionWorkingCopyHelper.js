4958efdf443a69641d56f5a020b9f55771662f11
William Bagayoko <wbagayoko@gmail.com>
2019-08-12 12:42:52 -0500

    2565: Case Name / margin-top

--- web-client/src/presenter/computeds/trialSessionWorkingCopyHelper.js
+++ web-client/src/presenter/computeds/trialSessionWorkingCopyHelper.js
@@ -4,11 +4,19 @@ import {
 } from './formattedTrialSessionDetails';
 import { state } from 'cerebral';
 
-export const trialSessionWorkingCopyHelper = get => {
+export const trialSessionWorkingCopyHelper = (get, applicationContext) => {
   const trialSession = get(state.trialSession) || {};
+
+  const formatCaseName = myCase => {
+    myCase.caseName = applicationContext.getCaseCaptionNames(
+      myCase.caseCaption || '',
+    );
+    return myCase;
+  };
 
   let formattedSessions = (trialSession.calendaredCases || [])
     .map(formatCase)
+    .map(formatCaseName)
     .sort(compareCasesByDocketNumber);
 
   return {
