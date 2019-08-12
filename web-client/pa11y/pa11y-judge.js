2c3b15b0f2daf158199bfd98c5d671d3d8210be7
William Bagayoko <wbagayoko@gmail.com>
2019-08-12 10:28:53 -0500

    2565 add pa11y links

--- web-client/pa11y/pa11y-judge.js
+++ web-client/pa11y/pa11y-judge.js
@@ -1,4 +1,7 @@
 module.exports = [
   'http://localhost:1234/mock-login?token=judgeArmen&path=/',
   'http://localhost:1234/mock-login?token=judgeArmen&path=/trial-sessions',
+  'http://localhost:1234/mock-login?token=judgeArmen&path=/trial-sessions',
+  'http://localhost:1234/mock-login?token=judgeArmen&path=/trial-session-detail/208a959f-9526-4db5-b262-e58c476a4655',
+  'http://localhost:1234/mock-login?token=judgeArmen&path=/trial-session-working-copy/208a959f-9526-4db5-b262-e58c476a4655',
 ];
