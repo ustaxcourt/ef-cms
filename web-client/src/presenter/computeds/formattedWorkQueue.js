20670a7d204b9a0a6b945c79e1c6c37c9928f6d5
Cody Seibert <cseibert113@gmail.com>
2019-08-13 16:50:44 -0400

    sorting the in progress tab by filed column

--- web-client/src/presenter/computeds/formattedWorkQueue.js
+++ web-client/src/presenter/computeds/formattedWorkQueue.js
@@ -295,11 +295,13 @@ export const formattedWorkQueue = (get, applicationContext) => {
     documentQc: {
       my: {
         batched: 'batchedAt',
+        inProgress: 'receivedAt',
         inbox: 'receivedAt',
         outbox: user.role === 'petitionsclerk' ? 'completedAt' : 'receivedAt',
       },
       section: {
         batched: 'batchedAt',
+        inProgress: 'receivedAt',
         inbox: 'receivedAt',
         outbox: user.role === 'petitionsclerk' ? 'completedAt' : 'receivedAt',
       },
