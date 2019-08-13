3be1e9b7e60fadcd14ff14d301ad89d528e22e5b
Kris Koskelin <kkoskelin@flexion.us>
2019-08-13 11:48:39 -0500

    simplifying proxy setup
    tweak path rewrite so destinations no longer get leading double-slash (.e.g //101-19)

--- web-api/proxy.js
+++ web-api/proxy.js
@@ -1,76 +1,30 @@
-var express = require('express');
-var proxy = require('http-proxy-middleware');
+const express = require('express');
+const proxy = require('http-proxy-middleware');
 
-var app = express();
+const app = express();
 
-app.use(
-  '/api',
-  proxy({
-    pathRewrite: {
-      '^/api': '/',
-    },
-    target: 'http://localhost:3001',
-  }),
-);
+// do not include trailing slashes
+const proxyDestinations = {
+  '/api': 'http://localhost:3001',
+  '/cases': 'http://localhost:3002',
+  '/documents': 'http://localhost:3004',
+  '/sections': 'http://localhost:3006',
+  '/trial-sessions': 'http://localhost:3007',
+  '/users': 'http://localhost:3003',
+  '/work-items': 'http://localhost:3005',
+};
 
-app.use(
-  '/cases',
-  proxy({
-    pathRewrite: {
-      '^/cases': '/',
-    },
-    target: 'http://localhost:3002',
-  }),
-);
-
-app.use(
-  '/users',
-  proxy({
-    pathRewrite: {
-      '^/users': '/',
-    },
-    target: 'http://localhost:3003',
-  }),
-);
-
-app.use(
-  '/documents',
-  proxy({
-    pathRewrite: {
-      '^/documents': '/',
-    },
-    target: 'http://localhost:3004',
-  }),
-);
-
-app.use(
-  '/work-items',
-  proxy({
-    pathRewrite: {
-      '^/work-items': '/',
-    },
-    target: 'http://localhost:3005',
-  }),
-);
-
-app.use(
-  '/sections',
-  proxy({
-    pathRewrite: {
-      '^/sections': '/',
-    },
-    target: 'http://localhost:3006',
-  }),
-);
-
-app.use(
-  '/trial-sessions',
-  proxy({
-    pathRewrite: {
-      '^/trial-sessions': '/',
-    },
-    target: 'http://localhost:3007',
-  }),
-);
+Object.entries(proxyDestinations).forEach(([uri, target]) => {
+  const pathRewrite = {};
+  // e.g. ^/api/foo becomes /foo
+  pathRewrite[`^${uri}`] = '';
+  app.use(
+    uri,
+    proxy({
+      pathRewrite,
+      target,
+    }),
+  );
+});
 
 app.listen(3000);
