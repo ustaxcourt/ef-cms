fc18e6fb5b3f4b5dfcf921677cee672ce09835d0
Kris Koskelin <kkoskelin@flexion.us>
2019-08-13 13:50:59 -0500

    ensure proxies are listening on all IP addresses

--- web-api/proxy.js
+++ web-api/proxy.js
@@ -1,30 +1,46 @@
 const express = require('express');
+const isReachable = require('is-reachable');
 const proxy = require('http-proxy-middleware');
 
-const app = express();
+const PROXY_REACHABLE_TIMEOUT = 30 * 1000; // 30 seconds
 
 // do not include trailing slashes
-const proxyDestinations = {
-  '/api': 'http://localhost:3001',
-  '/cases': 'http://localhost:3002',
-  '/documents': 'http://localhost:3004',
-  '/sections': 'http://localhost:3006',
-  '/trial-sessions': 'http://localhost:3007',
-  '/users': 'http://localhost:3003',
-  '/work-items': 'http://localhost:3005',
+const PROXY_DESTINATIONS = {
+  '/api': 'http://0.0.0.0:3001',
+  '/cases': 'http://0.0.0.0:3002',
+  '/documents': 'http://0.0.0.0:3004',
+  '/sections': 'http://0.0.0.0:3006',
+  '/trial-sessions': 'http://0.0.0.0:3007',
+  '/users': 'http://0.0.0.0:3003',
+  '/work-items': 'http://0.0.0.0:3005',
 };
 
-Object.entries(proxyDestinations).forEach(([uri, target]) => {
-  const pathRewrite = {};
-  // e.g. ^/api/foo becomes /foo
-  pathRewrite[`^${uri}`] = '';
-  app.use(
-    uri,
-    proxy({
-      pathRewrite,
-      target,
-    }),
-  );
-});
+const proxyMain = async () => {
+  const app = express();
 
-app.listen(3000);
+  Object.entries(PROXY_DESTINATIONS).forEach(([uri, target]) => {
+    const pathRewrite = {};
+    // e.g. ^/api/foo becomes /foo
+    pathRewrite[`^${uri}`] = '';
+    app.use(
+      uri,
+      proxy({
+        pathRewrite,
+        target,
+      }),
+    );
+  });
+
+  app.listen(3000, '0.0.0.0');
+
+  const isProxyReachable = uri => {
+    // hitting '/TEST/PROXY' to deliberately elicit a 404 rather than 403 which clutters authorization logs
+    return isReachable(`${uri}/TEST/PROXY`, {
+      timeout: PROXY_REACHABLE_TIMEOUT,
+    });
+  };
+
+  await Promise.all(Object.values(PROXY_DESTINATIONS).map(isProxyReachable));
+};
+
+proxyMain();
