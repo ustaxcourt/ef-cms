791837dbba9bfc20d7a7dd039412c485cb25bc4e
William Bagayoko <wbagayoko@gmail.com>
2019-08-19 13:34:18 -0500

    update tests

--- web-client/src/presenter/actions/TrialSessionWorkingCopy/setAddEditSessionNoteModalStateAction.test.js
+++ web-client/src/presenter/actions/TrialSessionWorkingCopy/setAddEditSessionNoteModalStateAction.test.js
@@ -11,22 +11,16 @@ describe('setAddEditSessionNoteModalStateAction', () => {
       modules: {
         presenter,
       },
-      props: { docketNumber: '123-12' },
       state: {
         trialSession: {
-          calendaredCases: [
-            { caseCaption: 'Sisqo, Petitioner', docketNumber: '123-12' },
-          ],
+          startDate: '12-12-2001',
+          trialLocation: "Bob's Burgers",
         },
         trialSessionWorkingCopy: {
-          caseMetadata: {
-            '123-12': { notes: 'i got some notes' },
-          },
+          sessionNotes: 'i got some notes',
         },
       },
     });
-    expect(result.state.modal.caseCaptionNames).toEqual('Sisqo');
-    expect(result.state.modal.docketNumber).toEqual('123-12');
     expect(result.state.modal.notes).toEqual('i got some notes');
   });
 });
