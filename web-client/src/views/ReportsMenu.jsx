632127e4aef64c00ac5e92d0c39fbb94741a2ae3
Kris Koskelin <kkoskelin@flexion.us>
2019-08-21 17:02:30 -0500

    439: rework nav for consistency with USWDS 2 (WIP)
    [skip ci]

--- web-client/src/views/ReportsMenu.jsx
+++ web-client/src/views/ReportsMenu.jsx
@@ -1,95 +1,55 @@
 import { connect } from '@cerebral/react';
-import { sequences, state } from 'cerebral';
-import PropTypes from 'prop-types';
+import { sequences } from 'cerebral';
 import React from 'react';
-
-export const ReportsMenuItems = () => {
-  return (
-    <ul className="usa-unstyled-list">
-      <li>
-        <a
-          className="reports-menu-item usa-button usa-button--unstyled"
-          href="/reports/case-deadlines"
-          id="all-deadlines"
-        >
-          Deadlines
-        </a>
-      </li>
-    </ul>
-  );
-};
+import classNames from 'classnames';
 
 export const ReportsMenu = connect(
   {
-    isReportsMenuOpen: state.menuHelper.isReportsMenuOpen,
-    toggleReportsMenu: sequences.toggleReportsMenuSequence,
-    user: state.user,
+    toggleReportsMenuSequence: sequences.toggleReportsMenuSequence,
   },
-  ({ isReportsMenuOpen, toggleReportsMenu }) => {
+  ({ isExpanded, toggleReportsMenuSequence }) => {
     return (
-      <div
-        className={
-          isReportsMenuOpen ? 'reports-menu open' : 'reports-menu closed'
-        }
-      >
-        <div className="reports-button-container margin-top-neg-1">
-          <button
-            aria-label="reports menu"
-            className="usa-button usa-button--unstyled header-tab-button"
-            type="button"
-            onClick={() => toggleReportsMenu()}
-          >
-            Reports
-          </button>
-        </div>
-        {isReportsMenuOpen && <ReportsMenuContent />}
-      </div>
+      <>
+        <button
+          aria-expanded={isExpanded}
+          className={classNames(
+            'usa-accordion__button usa-nav__link',
+            isExpanded && 'usa-current',
+          )}
+          onClick={() => toggleReportsMenuSequence()}
+        >
+          <span>Reports</span>
+        </button>
+        {isExpanded && (
+          <ul className="usa-nav__submenu">
+            <li className="usa-nav__submenu-item">
+              <a href="/reports/case-deadlines" id="all-deadlines">
+                Deadlines
+              </a>
+            </li>
+            <li className="usa-nav__submenu-item">
+              <a href="/reports/case-deadlines" id="all-deadlines">
+                Deadlines
+              </a>
+            </li>
+            <li className="usa-nav__submenu-item">
+              <a href="/reports/case-deadlines" id="all-deadlines">
+                Deadlines
+              </a>
+            </li>
+            <li className="usa-nav__submenu-item">
+              <a href="/reports/case-deadlines" id="all-deadlines">
+                Deadlines
+              </a>
+            </li>
+            <li className="usa-nav__submenu-item">
+              <a href="/reports/case-deadlines" id="all-deadlines">
+                Deadlines
+              </a>
+            </li>
+          </ul>
+        )}
+      </>
     );
   },
-);
-
-class ReportsMenuContentComponent extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(e) {
-    const targetClasses = Array.from(e.target.classList);
-    if (targetClasses.includes('reports-menu-item')) {
-      return true;
-    } else {
-      // set a small delay to account for state updates in parent
-      setTimeout(this.props.closeReportsMenu, 200);
-    }
-  }
-  componentDidMount() {
-    document.addEventListener('mousedown', this.handleClick);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('mousedown', this.handleClick);
-  }
-
-  render() {
-    return <div className="reports-menu-content">{ReportsMenuItems()}</div>;
-  }
-}
-
-ReportsMenuContentComponent.propTypes = {
-  closeReportsMenu: PropTypes.func,
-  signOutSequence: PropTypes.func.isRequired,
-};
-
-ReportsMenuItems.propTypes = {
-  signOut: PropTypes.func,
-};
-
-const ReportsMenuContent = connect(
-  {
-    closeReportsMenu: sequences.closeReportsMenuSequence,
-    signOutSequence: sequences.signOutSequence,
-  },
-  ReportsMenuContentComponent,
 );
