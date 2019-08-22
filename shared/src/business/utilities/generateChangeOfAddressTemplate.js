d0f9607781c9db912bed752a4375ea88d03a8a13
Matt Hopson <matthopson@gmail.com>
2019-08-22 13:28:45 -0600

    Refactor for address and telephone changes

--- shared/src/business/utilities/generateChangeOfAddressTemplate.js
+++ shared/src/business/utilities/generateChangeOfAddressTemplate.js
@@ -6,28 +6,10 @@ const printChangeOfAddressTemplate = fs.readFileSync(
   'utf8',
 );
 
-/**
- * hydrates printChangeOfAddressTemplate with contact and case info
- *
- * @param {object} caseDetail the case being updated
- * @param {object} newData updated contact information
- * @param {object} oldData the old contact information
- * @returns {string} pdfContentHtml in string form
- */
-exports.generateChangeOfAddressTemplate = ({
-  caseDetail,
-  newData,
-  oldData,
-}) => {
-  let oldAddress = '';
-  let newAddress = '';
-  let oldCityStateZip = '';
-  let newCityStateZip = '';
+const getAddressPhoneDiff = ({ newData, oldData }) => {
   const diff = {};
-  const newContactFields = Object.keys(newData);
-
-  // Get changes diff
-  newContactFields.map(key => {
+  const fields = Object.keys(newData);
+  fields.map(key => {
     const oldValue = oldData[key];
     const newValue = newData[key];
     if (oldValue !== newValue) {
@@ -37,52 +19,117 @@ exports.generateChangeOfAddressTemplate = ({
       };
     }
   });
+  return diff;
+};
 
-  if (diff.address1) {
-    oldAddress += `<div>${diff.address1.oldData}</div>`;
-    newAddress += `<div>${diff.address1.newData}</div>`;
+exports.getAddressPhoneDiff = getAddressPhoneDiff;
+
+/**
+ * returns the appropiate documentType given the old and new contact data
+ *
+ * @param {object} newData updated contact information
+ * @param {object} oldData the old contact information
+ * @returns {string} documentType for the address / phone change scenario
+ */
+const getDocumentTypeForAddressChange = ({ diff, newData, oldData }) => {
+  let documentType;
+  if (!diff) {
+    diff = getAddressPhoneDiff({ newData, oldData });
   }
 
-  if (diff.address2) {
-    oldAddress += `<div>${diff.address2.oldData}</div>`;
-    newAddress += `<div>${diff.address2.newData}</div>`;
+  const addressFields = [
+    'address1',
+    'address2',
+    'address3',
+    'city',
+    'state',
+    'postalCode',
+  ];
+
+  const isAddressChange = Object.keys(diff).some(field =>
+    addressFields.includes(field),
+  );
+  const isPhoneChange = !!diff.phone;
+
+  if (isAddressChange && !isPhoneChange) {
+    documentType = 'Notice of Change of Address';
   }
 
-  if (diff.address3) {
-    oldAddress += `<div>${diff.address3.oldData}</div>`;
-    newAddress += `<div>${diff.address3.newData}</div>`;
+  if (isPhoneChange && !isAddressChange) {
+    documentType = 'Notice of Change of Telephone Number';
   }
 
-  if (diff.city) {
-    oldCityStateZip += diff.city.oldData;
-    newCityStateZip += diff.city.newData;
+  if (isAddressChange && isPhoneChange) {
+    documentType = 'Notice of Change of Address and Telephone Number';
   }
 
-  if (diff.state) {
-    oldCityStateZip += (diff.city ? ', ' : '') + diff.state.oldData;
-    newCityStateZip += (diff.city ? ', ' : '') + diff.state.newData;
+  return documentType;
+};
+
+exports.getDocumentTypeForAddressChange = getDocumentTypeForAddressChange;
+
+/**
+ * hydrates printChangeOfAddressTemplate with contact and case info
+ *
+ * @param {object} caseDetail the case being updated
+ * @param {string} documentTitle the document title to use on the pdf
+ * @param {object} newData updated contact information
+ * @param {object} oldData the old contact information
+ * @returns {string} pdfContentHtml in string form
+ */
+exports.generateChangeOfAddressTemplate = ({
+  caseDetail,
+  documentTitle,
+  newData,
+  oldData,
+}) => {
+  let oldAddress = '';
+  let newAddress = '';
+
+  if (!documentTitle) {
+    documentTitle = getDocumentTypeForAddressChange({ newData, oldData });
   }
 
-  if (diff.postalCode) {
-    oldCityStateZip +=
-      (diff.city || diff.state ? ' ' : '') + diff.postalCode.oldData;
-    newCityStateZip +=
-      (diff.city || diff.state ? ' ' : '') + diff.postalCode.newData;
-  }
+  oldAddress += `<div>${oldData.city}, ${oldData.state} ${oldData.postalCode}</div>`;
+  newAddress += `<div>${newData.city}, ${newData.state} ${newData.postalCode}</div>`;
 
-  if (newCityStateZip !== '') {
-    oldAddress += `<div>${oldCityStateZip}</div>`;
-    newAddress += `<div>${newCityStateZip}</div>`;
-  }
+  if (documentTitle === 'Notice of Change of Telephone Number') {
+    oldAddress = `<div>${oldData.phone}</dvi>`;
+    newAddress = `<div>${newData.phone}</dvi>`;
+  } else {
+    oldAddress = `<div>${oldData.address1}</div>`;
+    newAddress = `<div>${newData.address1}</div>`;
 
-  if (diff.phone) {
-    oldAddress += `<div>${diff.phone.oldData}</div>`;
-    newAddress += `<div>${diff.phone.newData}</div>`;
+    if (oldData.address2) {
+      oldAddress += `<div>${oldData.address2}</div>`;
+    }
+
+    if (newData.address2) {
+      newAddress += `<div>${newData.address2}</div>`;
+    }
+
+    if (oldData.address3) {
+      oldAddress += `<div>${oldData.address3}</div>`;
+    }
+
+    if (newData.address3) {
+      newAddress += `<div>${newData.address3}</div>`;
+    }
+
+    if (documentTitle === 'Notice of Change of Address') {
+      // address only1
+    }
+
+    if (documentTitle === 'Notice of Change of Address and Telephone Number') {
+      oldAddress += `<div>${oldData.phone}</dvi>`;
+      newAddress += `<div>${newData.phone}</dvi>`;
+    }
   }
 
   const pdfContentHtml = printChangeOfAddressTemplate
     .replace(/{{ oldAddress }}/g, oldAddress)
     .replace(/{{ newAddress }}/g, newAddress)
+    .replace(/{{ documentTitle }}/g, documentTitle)
     .replace(/{{ caption }}/g, caseDetail.caseCaption)
     .replace(/{{ captionPostfix }}/g, caseDetail.caseCaptionPostfix)
     .replace(
