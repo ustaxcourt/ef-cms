f1e8d7c053c51ccc16837a44bfd14dea9c9d7687
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-08-22 13:38:37 -0500

    desktop header all working/looking well

--- web-client/src/views/AccountMenu.jsx
+++ web-client/src/views/AccountMenu.jsx
@@ -2,47 +2,52 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { connect } from '@cerebral/react';
 import { sequences, state } from 'cerebral';
 import React from 'react';
+import classNames from 'classnames';
 
 export const AccountMenu = connect(
   {
     closeAccountMenu: sequences.closeAccountMenuSequence,
-    isAccountMenuOpen: state.menuHelper.isAccountMenuOpen,
     signOutSequence: sequences.signOutSequence,
     toggleAccountMenu: sequences.toggleAccountMenuSequence,
     user: state.user,
   },
-  ({ isAccountMenuOpen, signOutSequence, toggleAccountMenu, user }) => {
+  ({ isExpanded, signOutSequence, toggleAccountMenu, user }) => {
     return (
-      <ul>
-        <li className="usa-nav__primary-item">
+      <ul className="usa-nav__primary usa-accordion">
+        <li
+          className={classNames(
+            'usa-nav__primary-item',
+            isExpanded && 'usa-nav__submenu--open',
+          )}
+        >
           <button
-            aria-expanded={isAccountMenuOpen}
-            className="usa-accordion__button usa-nav__link"
+            aria-expanded={isExpanded}
+            className={classNames(
+              'usa-accordion__button usa-nav__link hidden-underline',
+            )}
             title={`Hello, ${user.name}`}
             onClick={() => toggleAccountMenu()}
           >
             <span>
               <FontAwesomeIcon
-                className="account-menu-icon user-icon"
+                className="account-menu-icon"
                 icon={['far', 'user']}
-              />
-              <FontAwesomeIcon
-                className="account-menu-icon caret"
-                icon={['fa', 'caret-down']}
               />
             </span>
           </button>
-          <ul className="usa-nav__submenu">
-            <li className="usa-nav__submenu-item">
-              <button
-                className="account-menu-item usa-button--unstyled"
-                id="log-out"
-                onClick={() => signOutSequence()}
-              >
-                Log Out
-              </button>
-            </li>
-          </ul>
+          {isExpanded && (
+            <ul className="usa-nav__submenu position-right-0">
+              <li className="usa-nav__submenu-item">
+                <button
+                  className="account-menu-item usa-button usa-button--unstyled"
+                  id="log-out"
+                  onClick={() => signOutSequence()}
+                >
+                  Log Out
+                </button>
+              </li>
+            </ul>
+          )}
         </li>
       </ul>
     );
