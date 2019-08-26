052867f2e26c4c49e902e882f03ce6a1521dae02
William Bagayoko <wbagayoko@gmail.com>
2019-08-25 19:47:04 -0500

    1282 Batch wip

--- web-api/src/trialSessions/batchDownloadTrialSessionLambda.js
+++ web-api/src/trialSessions/batchDownloadTrialSessionLambda.js
@@ -1,44 +1,8 @@
 const createApplicationContext = require('../applicationContext');
 const {
-  headers,
-  sendError,
-  sendOk,
+  getUserFromAuthHeader,
+  handle,
 } = require('../middleware/apiGatewayHelper');
-const {
-  NotFoundError,
-  UnauthorizedError,
-} = require('../../../shared/src/errors/errors');
-const { getUserFromAuthHeader } = require('../middleware/apiGatewayHelper');
-
-const customHandle = async (event, fun) => {
-  if (event.source === 'serverless-plugin-warmup') {
-    return sendOk('Lambda is warm!');
-  }
-  try {
-    const pdfBuffer = await fun();
-    return {
-      body: pdfBuffer.toString('base64'),
-      headers: {
-        ...headers,
-        'Content-Type': 'application/zip',
-        'accept-ranges': 'bytes',
-      },
-      isBase64Encoded: true,
-      statusCode: 200,
-    };
-  } catch (err) {
-    console.error('err', err);
-    if (err instanceof NotFoundError) {
-      err.statusCode = 404;
-      return sendError(err);
-    } else if (err instanceof UnauthorizedError) {
-      err.statusCode = 403;
-      return sendError(err);
-    } else {
-      return sendError(err);
-    }
-  }
-};
 
 /**
  * batch download trial session
@@ -47,15 +11,15 @@ const customHandle = async (event, fun) => {
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
 exports.handler = event =>
-  customHandle(event, async () => {
+  handle(event, async () => {
     const user = getUserFromAuthHeader(event);
     const applicationContext = createApplicationContext(user);
     try {
       const results = await applicationContext
         .getUseCases()
         .batchDownloadTrialSessionInteractor({
-          ...JSON.parse(event.body),
           applicationContext,
+          ...JSON.parse(event.body),
         });
       applicationContext.logger.info('User', user);
       applicationContext.logger.info('Results', results);
