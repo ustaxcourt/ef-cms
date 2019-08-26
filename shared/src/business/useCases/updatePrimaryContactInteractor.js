63daa0525c2e4062b3488bd14d7df575fad51053
Matt Hopson <matthopson@gmail.com>
2019-08-26 10:19:39 -0600

    Don't display deceased spouse in contact update

--- shared/src/business/useCases/updatePrimaryContactInteractor.js
+++ shared/src/business/useCases/updatePrimaryContactInteractor.js
@@ -41,8 +41,18 @@ exports.updatePrimaryContactInteractor = async ({
     throw new UnauthorizedError('Unauthorized for update case contact');
   }
 
+  let caseNameToUse;
   const caseEntity = new Case(caseToUpdate);
-  const caseName = Case.getCaseCaptionNames(caseEntity.caseCaption);
+  const spousePartyTypes = [
+    ContactFactory.PARTY_TYPES.petitionerSpouse,
+    ContactFactory.PARTY_TYPES.petitionerDeceasedSpouse,
+  ];
+
+  if (spousePartyTypes.includes(caseEntity.partyType)) {
+    caseNameToUse = caseEntity.contactPrimary.name;
+  } else {
+    caseNameToUse = Case.getCaseCaptionNames(caseEntity.caseCaption);
+  }
 
   const documentType = applicationContext
     .getUtilities()
@@ -59,7 +69,7 @@ exports.updatePrimaryContactInteractor = async ({
         caseCaptionPostfix: Case.CASE_CAPTION_POSTFIX,
       },
       documentTitle: documentType.title,
-      name: caseName,
+      name: caseNameToUse,
       newData: contactInfo,
       oldData: caseEntity.contactPrimary,
     });
@@ -82,7 +92,7 @@ exports.updatePrimaryContactInteractor = async ({
   const newDocumentId = applicationContext.getUniqueId();
 
   const changeOfAddressDocument = new Document({
-    additionalInfo2: `for ${caseName}`,
+    additionalInfo2: `for ${caseNameToUse}`,
     caseId,
     documentId: newDocumentId,
     documentType: documentType.title,
