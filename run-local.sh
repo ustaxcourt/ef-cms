b4a2cd11ac2171816da39ed6b832a2ddee230008
Joseph Sutton <jsutton@flexion.us>
2019-08-27 15:30:59 -0500

    ci flag running dynamodb

--- run-local.sh
+++ run-local.sh
@@ -2,9 +2,11 @@
 echo "killing dynamo if already running"
 pkill -f DynamoDBLocal
 
+if [ -e $CI ] then
 echo "starting dynamo"
-./web-api/start-dynamo.sh &
-DYNAMO_PID=$!
+  ./web-api/start-dynamo.sh &
+  DYNAMO_PID=$!
+fi
 
 node ./web-api/start-s3rver &
 S3RVER_PID=$!
@@ -50,5 +52,8 @@ npx sls offline start "$@" --config web-api/serverless-trial-sessions.yml &
 echo "starting proxy"
 node ./web-api/proxy.js
 
-pkill -P $DYNAMO_PID
+if [ -e $DYNAMO_PID ] then
+  pkill -P $DYNAMO_PID
+fi
+
 kill $S3RVER_PID
--- web-api/start-dynamo.sh
+++ web-api/start-dynamo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # download the dynamo jar file if needed
-if [ ! -e ".dynamodb/dynamo.tar.gz" ]; then
-  mkdir -p .dynamodb
-  curl --create-dirs -o .dynamodb/dynamo.tar.gz http://s3-us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.tar.gz
-  cd .dynamodb && tar -xvf dynamo.tar.gz && cd ..
-fi
+# if [ ! -e ".dynamodb/dynamo.tar.gz" ]; then
+#   mkdir -p .dynamodb
+#   curl --create-dirs -o .dynamodb/dynamo.tar.gz http://s3-us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.tar.gz
+#   cd .dynamodb && tar -xvf dynamo.tar.gz && cd ..
+# fi
 
 # start dynamo
 # cd .dynamodb && java -Djava.library.path=.dynamodb/DynamoDBLocal_lib -Xmx1024m -jar DynamoDBLocal.jar -inMemory -sharedDb
