b9cddc785dccd79faae6632baef3601261ed3054
Cody Seibert <cseibert113@gmail.com>
2019-08-27 13:23:27 -0400

    fixing coverage

--- shared/src/business/useCases/users/updateUserContactInformationInteractor.test.js
+++ shared/src/business/useCases/users/updateUserContactInformationInteractor.test.js
@@ -6,7 +6,61 @@ const { MOCK_USERS } = require('../../../test/mockUsers');
 const { UnauthorizedError } = require('../../../errors/errors');
 
 describe('updateUserContactInformationInteractor', () => {
-  it('retrieves the users in the petitions section', async () => {
+  it('updates the user and respondents in the case', async () => {
+    const updateCaseSpy = jest.fn();
+    const updateUserSpy = jest.fn();
+    const applicationContext = {
+      environment: { stage: 'local' },
+      getCurrentUser: () => {
+        return MOCK_USERS['f7d90c05-f6cd-442c-a168-202db587f16f'];
+      },
+      getPersistenceGateway: () => {
+        return {
+          getCasesByUser: () =>
+            Promise.resolve([
+              {
+                ...MOCK_CASE,
+                respondents: [
+                  {
+                    userId: 'f7d90c05-f6cd-442c-a168-202db587f16f',
+                  },
+                ],
+              },
+            ]),
+          getUserById: () =>
+            Promise.resolve(MOCK_USERS['f7d90c05-f6cd-442c-a168-202db587f16f']),
+          updateCase: updateCaseSpy,
+          updateUser: updateUserSpy,
+        };
+      },
+    };
+    await updateUserContactInformationInteractor({
+      applicationContext,
+      contactInfo: {
+        addressLine1: '999',
+        addressLine2: 'circle st.',
+      },
+      userId: 'f7d90c05-f6cd-442c-a168-202db587f16f',
+    });
+    expect(updateUserSpy).toHaveBeenCalled();
+
+    expect(updateUserSpy.mock.calls[0][0].user).toMatchObject({
+      addressLine1: '999',
+      addressLine2: 'circle st.',
+    });
+    expect(updateCaseSpy).toHaveBeenCalled();
+    expect(updateCaseSpy.mock.calls[0][0].caseToUpdate).toMatchObject({
+      respondents: [
+        {
+          addressLine1: '999',
+          addressLine2: 'circle st.',
+          userId: 'f7d90c05-f6cd-442c-a168-202db587f16f',
+        },
+      ],
+    });
+  });
+
+  it('updates the user and practitioners in the case', async () => {
     const updateCaseSpy = jest.fn();
     const updateUserSpy = jest.fn();
     const applicationContext = {
