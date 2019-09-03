9bea2117d0e13d371e9b0875a3243b25be24a23f
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-09-03 09:29:42 -0500

    fix tests; change import

--- shared/src/business/test/createTestApplicationContext.js
+++ shared/src/business/test/createTestApplicationContext.js
@@ -1,5 +1,5 @@
 const docketNumberGenerator = require('../../persistence/dynamo/cases/docketNumberGenerator');
-import uuidv4 from 'uuid/v4';
+const uuidv4 = require('uuid/v4');
 const {
   addWorkItemToSectionInbox,
 } = require('../../persistence/dynamo/workitems/addWorkItemToSectionInbox');
