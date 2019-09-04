3307de7e9432ed0605f3d4048ec31724d5904f25
Cody Seibert <cseibert113@gmail.com>
2019-09-04 11:42:43 -0400

    setting async to true

--- web-api/src/users/updateUserContactInformationLambda.js
+++ web-api/src/users/updateUserContactInformationLambda.js
@@ -3,13 +3,13 @@ const { getUserFromAuthHeader } = require('../middleware/apiGatewayHelper');
 const { handle } = require('../middleware/apiGatewayHelper');
 
 /**
- * used for fetching full user data
+ * updates the user contact info (used for a practitioner or respondent)
  *
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-exports.handler = event =>
-  handle(event, async () => {
+exports.handler = event => {
+  return handle(event, async () => {
     const user = getUserFromAuthHeader(event);
     const applicationContext = createApplicationContext(user);
     try {
@@ -17,8 +17,11 @@ exports.handler = event =>
         .getUseCases()
         .updateUserContactInformationInteractor({
           applicationContext,
-          contactInfo: JSON.parse(event.body),
-          userId: event.pathParameters.userId,
+          contactInfo:
+            typeof event.body === 'string'
+              ? JSON.parse(event.body)
+              : event.body,
+          userId: (event.pathParameters || event.path).userId,
         });
       applicationContext.logger.info('User', user);
       applicationContext.logger.info('Results', results);
@@ -28,3 +31,4 @@ exports.handler = event =>
       throw e;
     }
   });
+};
