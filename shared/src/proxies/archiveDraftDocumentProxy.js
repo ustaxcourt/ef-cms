f638b902e5938a5dde36861f7bca8aab7ffa494c
Cody Seibert <cseibert113@gmail.com>
2019-09-05 11:03:39 -0400

    adding proxy

---
+++ shared/src/proxies/archiveDraftDocumentProxy.js
@@ -0,0 +1,23 @@
+const { remove } = require('./requests');
+
+/**
+ * getCaseInteractor
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context
+ * @param {string} providers.documentId the document id to archive
+ * @returns {Promise<*>} the promise of the api call
+ */
+exports.archiveDraftDocumentInteractor = ({
+  applicationContext,
+  caseId,
+  documentId,
+}) => {
+  return remove({
+    applicationContext,
+    body: {
+      caseId,
+    },
+    endpoint: `/documents/${documentId}`,
+  });
+};
