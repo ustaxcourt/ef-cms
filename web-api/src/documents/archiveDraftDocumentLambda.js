28849da180f8b9af44a8a5eabb8aceadaef551f2
Cody Seibert <cseibert113@gmail.com>
2019-09-05 10:58:58 -0400

    adding the archive draft document endpoint;

---
+++ web-api/src/documents/archiveDraftDocumentLambda.js
@@ -0,0 +1,30 @@
+const createApplicationContext = require('../applicationContext');
+const {
+  getUserFromAuthHeader,
+  handle,
+} = require('../middleware/apiGatewayHelper');
+
+/**
+ * archives the draft document information from a case
+ *
+ * @param {object} event the AWS event object
+ * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
+ */
+exports.handler = event =>
+  handle(event, async () => {
+    const user = getUserFromAuthHeader(event);
+    const applicationContext = createApplicationContext(user);
+    const { documentId } = event.pathParameters || {};
+
+    applicationContext.logger.info('Event', event);
+    try {
+      await applicationContext.getUseCases().archiveDraftDocumentInteractor({
+        applicationContext,
+        documentId,
+      });
+      applicationContext.logger.info('User', user);
+    } catch (e) {
+      applicationContext.logger.error(e);
+      throw e;
+    }
+  });
