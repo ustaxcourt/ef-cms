d3ae07f96405f5ae2246f010e11863f879585799
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-09-05 14:52:16 -0500

    nav header styling updates

---
+++ web-client/src/views/AdvancedSearch/SearchResults.jsx
@@ -0,0 +1,137 @@
+import { BigHeader } from '../BigHeader';
+import { StateSelect } from './StateSelect';
+import { connect } from '@cerebral/react';
+import { state } from 'cerebral';
+import React from 'react';
+
+export const SearchResults = connect(
+  {
+    searchResults: state.searchResults,
+  },
+  ({ searchResults }) => {
+    return (
+      <>
+        <BigHeader text="Advanced Search" />
+
+        <section className="usa-section grid-container advanced-search">
+          <div className="header-with-blue-background">
+            <h3>Enter Search Criteria</h3>
+          </div>
+          <div className="blue-container">
+            <div className="grid-row grid-gap">
+              <div className="grid-col-4 right-gray-border">
+                <label className="usa-label" htmlFor="petitionerName">
+                  Petitioner name <span className="usa-hint">(required)</span>
+                </label>
+                <input
+                  className="usa-input"
+                  id="petitioner-name"
+                  name="petitionerName"
+                  type="text"
+                  value={form.petitionerName || ''}
+                  onChange={e => {
+                    updateFormValueSequence({
+                      key: e.target.name,
+                      value: e.target.value,
+                    });
+                  }}
+                />
+              </div>
+
+              <div className="grid-col-4 right-gray-border">
+                <div className="grid-row grid-gap">
+                  <div className="grid-col-7">
+                    <label className="usa-label" htmlFor="country-type">
+                      Country
+                    </label>
+                    <select
+                      className="usa-select"
+                      id="country-type"
+                      name="countryType"
+                      value={form.countryType}
+                      onChange={e => {
+                        updateFormValueSequence({
+                          key: e.target.name,
+                          value: e.target.value,
+                        });
+                      }}
+                    >
+                      <option value={constants.COUNTRY_TYPES.DOMESTIC}>
+                        - United States -
+                      </option>
+                      <option value={constants.COUNTRY_TYPES.INTERNATIONAL}>
+                        - International -
+                      </option>
+                    </select>
+                  </div>
+
+                  <div className="grid-col-5">
+                    <label className="usa-label" htmlFor="petitionerName">
+                      State
+                    </label>
+                    <StateSelect
+                      bind={form.select}
+                      updateFormValueSequence={updateFormValueSequence}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="grid-col-4">
+                <div className="grid-row grid-gap">
+                  <div className="grid-col-7">
+                    <label className="display-block" htmlFor="year-filed">
+                      Year filed
+                    </label>
+                    <div className="usa-form-group--year display-inline-block">
+                      <input
+                        className="usa-input"
+                        id="year-filed-min"
+                        name="yearFiledMin"
+                        type="text"
+                        value={form.yearFiledMin || ''}
+                        onChange={e => {
+                          updateFormValueSequence({
+                            key: e.target.name,
+                            value: e.target.value,
+                          });
+                        }}
+                      />
+                    </div>
+                    <span className="margin-right-2">to</span>
+                    <div className="usa-form-group--year display-inline-block">
+                      <input
+                        className="usa-input"
+                        id="year-filed-max"
+                        name="yearFiledMax"
+                        type="text"
+                        value={form.yearFiledMax || ''}
+                        onChange={e => {
+                          updateFormValueSequence({
+                            key: e.target.name,
+                            value: e.target.value,
+                          });
+                        }}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="grid-col-5">
+                    <button
+                      className="usa-button advanced-search__button"
+                      onClick={() => submitAdvancedSearchSequence()}
+                    >
+                      Search
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <SearchResults />
+        </section>
+      </>
+    );
+  },
+);
