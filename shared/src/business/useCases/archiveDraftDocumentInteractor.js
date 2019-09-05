bae672b8d36dac4d27623ecf575fecea37e20f4f
Cody Seibert <cseibert113@gmail.com>
2019-09-05 10:58:58 -0400

    adding the archive draft document endpoint;

---
+++ shared/src/business/useCases/archiveDraftDocumentInteractor.js
@@ -0,0 +1,45 @@
+const {
+  ARCHIVE_DOCUMENT,
+  isAuthorized,
+} = require('../../authorization/authorizationClientService');
+const { Case } = require('../entities/cases/Case');
+const { UnauthorizedError } = require('../../errors/errors');
+
+/**
+ * archiveDraftDocumentInteractor
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context
+ * @param {string} providers.caseId the id of the case to update notes
+ * @param {string} providers.notes the notes to update
+ * @returns {object} the updated case note returned from persistence
+ */
+exports.archiveDraftDocumentInteractor = async ({
+  applicationContext,
+  caseId,
+  documentId,
+}) => {
+  const user = applicationContext.getCurrentUser();
+
+  if (!isAuthorized(user, ARCHIVE_DOCUMENT)) {
+    throw new UnauthorizedError('Unauthorized');
+  }
+
+  const caseToUpdate = await applicationContext
+    .getPersistenceGateway()
+    .getCaseByCaseId({
+      applicationContext,
+      caseId,
+    });
+
+  const caseEntity = new Case(caseToUpdate, { applicationContext });
+
+  const documentToArchive = caseEntity.getDocumentById({ documentId });
+
+  documentToArchive.archive();
+
+  await applicationContext.getPersistenceGateway().updateCase({
+    applicationContext,
+    caseToUpdate: caseEntity.validate().toRawObject(),
+  });
+};
