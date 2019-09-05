c164c606ffdbbb5e352ce14a7e78c5b2009e6edd
Cody Seibert <cseibert113@gmail.com>
2019-09-05 11:03:39 -0400

    adding proxy

--- web-api/src/documents/archiveDraftDocumentLambda.js
+++ web-api/src/documents/archiveDraftDocumentLambda.js
@@ -14,13 +14,14 @@ exports.handler = event =>
   handle(event, async () => {
     const user = getUserFromAuthHeader(event);
     const applicationContext = createApplicationContext(user);
-    const { documentId } = event.pathParameters || {};
+    const { documentId } = event.pathParameters;
 
     applicationContext.logger.info('Event', event);
     try {
       await applicationContext.getUseCases().archiveDraftDocumentInteractor({
         applicationContext,
         documentId,
+        ...JSON.parse(event.body),
       });
       applicationContext.logger.info('User', user);
     } catch (e) {
