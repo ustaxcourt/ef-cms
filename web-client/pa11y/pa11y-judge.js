04d8c55e94a8f2c6896a0d52e14b50f75861ecd0
Kris Koskelin <kkoskelin@flexion.us>
2019-09-06 12:06:49 -0500

    2828: fixing pa11y url

--- web-client/pa11y/pa11y-judge.js
+++ web-client/pa11y/pa11y-judge.js
@@ -1,7 +1,8 @@
 module.exports = [
   'http://localhost:1234/mock-login?token=judgeArmen&path=/',
   'http://localhost:1234/mock-login?token=judgeArmen&path=/search',
-  'http://localhost:1234/mock-login?token=judgeArmen&path=/search/no-cases',
+  'http://localhost:1234/mock-login?token=judgeArmen&path=/search/no-matches',
+  'http://localhost:1234/mock-login?token=judgeArmen&path=/search?petitionerName=de',
   'http://localhost:1234/mock-login?token=judgeArmen&path=/trial-sessions',
   'http://localhost:1234/mock-login?token=judgeArmen&path=/trial-sessions',
   'http://localhost:1234/mock-login?token=judgeArmen&path=/trial-session-detail/959c4338-0fac-42eb-b0eb-d53b8d0195cc&info=calendared-case',
