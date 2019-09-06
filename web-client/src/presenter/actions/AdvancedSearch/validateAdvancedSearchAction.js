04a20ce657b3f1fa06aa484665128876bad31e08
Kris Koskelin <kkoskelin@flexion.us>
2019-09-06 11:48:01 -0500

    2828: advanced search validation

---
+++ web-client/src/presenter/actions/AdvancedSearch/validateAdvancedSearchAction.js
@@ -0,0 +1,32 @@
+import { isEmpty } from 'lodash';
+import { state } from 'cerebral';
+
+/**
+ * validate advanced search form
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context
+ * @param {Function} providers.get the cerebral get function
+ * @returns {Promise} async action
+ */
+export const validateAdvancedSearchAction = async ({ get, path }) => {
+  const errors = {};
+  const form = get(state.form);
+  if (!form.petitionerName) {
+    errors.petitionerName = 'Enter a name';
+  }
+
+  const isValid = isEmpty(errors);
+
+  if (isValid) {
+    return path.success();
+  } else {
+    return path.error({
+      alertError: {
+        messages: Object.values(errors),
+        title: 'Please correct the following errors:',
+      },
+      errors,
+    });
+  }
+};
--- web-client/src/presenter/sequences/submitAdvancedSearchSequence.js
+++ web-client/src/presenter/sequences/submitAdvancedSearchSequence.js
@@ -1,9 +1,19 @@
+import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { props, state } from 'cerebral';
 import { set } from 'cerebral/factories';
+import { setAlertErrorAction } from '../actions/setAlertErrorAction';
+import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
 import { submitAdvancedSearchAction } from '../actions/AdvancedSearch/submitAdvancedSearchAction';
+import { validateAdvancedSearchAction } from '../actions/AdvancedSearch/validateAdvancedSearchAction';
 
 export const submitAdvancedSearchSequence = [
-  //validate search action
-  submitAdvancedSearchAction,
-  set(state.searchResults, props.searchResults),
+  validateAdvancedSearchAction,
+  {
+    error: [setAlertErrorAction, setValidationErrorsAction],
+    success: [
+      clearAlertsAction,
+      submitAdvancedSearchAction,
+      set(state.searchResults, props.searchResults),
+    ],
+  },
 ];
