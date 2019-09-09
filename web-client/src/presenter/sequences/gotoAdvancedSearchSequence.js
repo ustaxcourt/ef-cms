7cd497a7771ab16547446ebef12ae3c936408203
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-09-09 07:59:14 -0500

    2828: default countryType; hide state field

--- web-client/src/presenter/sequences/gotoAdvancedSearchSequence.js
+++ web-client/src/presenter/sequences/gotoAdvancedSearchSequence.js
@@ -3,11 +3,13 @@ import { clearFormAction } from '../actions/clearFormAction';
 import { clearScreenMetadataAction } from '../actions/clearScreenMetadataAction';
 import { setAdvancedSearchPropsOnFormAction } from '../actions/AdvancedSearch/setAdvancedSearchPropsOnFormAction';
 import { setCurrentPageAction } from '../actions/setCurrentPageAction';
+import { setDefaultCountryTypeOnAdvancedSearchFormAction } from '../actions/AdvancedSearch/setDefaultCountryTypeOnAdvancedSearchFormAction';
 
 export const gotoAdvancedSearchSequence = [
   clearAlertsAction,
   clearScreenMetadataAction,
   clearFormAction,
+  setDefaultCountryTypeOnAdvancedSearchFormAction,
   setAdvancedSearchPropsOnFormAction,
   setCurrentPageAction('AdvancedSearch'),
 ];
