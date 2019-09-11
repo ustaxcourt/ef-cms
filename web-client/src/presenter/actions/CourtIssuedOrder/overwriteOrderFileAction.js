fcd480030078bd477b0cc6a4f5d1287d40613fd2
Cody Seibert <cseibert113@gmail.com>
2019-09-11 12:23:35 -0400

    pr comments

--- web-client/src/presenter/actions/CourtIssuedOrder/overwriteOrderFileAction.js
+++ web-client/src/presenter/actions/CourtIssuedOrder/overwriteOrderFileAction.js
@@ -5,7 +5,9 @@ import { state } from 'cerebral';
  *
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
- * @returns {object} the next path based on if validation was successful or error
+ * @param {object} providers.path the cerebral path to take depending on if the file was uploaded successfully or not
+ * @param {object} providers.get the cerebral get method used for getting state
+ * @returns {object} the next path based on if the file was successfully uploaded or not
  */
 export const overwriteOrderFileAction = async ({
   applicationContext,
