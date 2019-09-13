3a34d061e262a7dd946db1bccf9dc36a5b744be5
Cody Seibert <cseibert113@gmail.com>
2019-09-13 10:38:14 -0400

    adding extra security for generating the upload policy to prevent external users from overwriting existing s3 files

--- shared/src/business/useCases/getUploadPolicyInteractor.js
+++ shared/src/business/useCases/getUploadPolicyInteractor.js
@@ -10,14 +10,32 @@ const { UnauthorizedError } = require('../../errors/errors');
  * @param {object} providers.applicationContext the application context
  * @returns {Array<string>} the filing type options based on user role
  */
-exports.getUploadPolicyInteractor = async ({ applicationContext }) => {
+exports.getUploadPolicyInteractor = async ({
+  applicationContext,
+  documentId,
+}) => {
   const user = applicationContext.getCurrentUser();
 
   if (!isAuthorized(user, UPLOAD_DOCUMENT)) {
     throw new UnauthorizedError('Unauthorized');
   }
 
+  // we don't want external users to be able to overwrite existing s3 files
+  if (user.isExternalUser()) {
+    const isFileExists = await applicationContext
+      .getPersistenceGateway()
+      .isFileExists({
+        applicationContext,
+        documentId,
+      });
+
+    if (isFileExists) {
+      throw new Error('Unauthorized');
+    }
+  }
+
   return applicationContext.getPersistenceGateway().getUploadPolicy({
     applicationContext,
+    documentId,
   });
 };
