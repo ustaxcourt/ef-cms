f5746a291c8f60bfd198501e7fc9f8cfc0baf643
Kris Koskelin <kkoskelin@flexion.us>
2019-09-17 14:33:33 -0400

    handle when formattedCase itself is not provided

--- web-client/src/ustc-ui/CaseLink/CaseLink.jsx
+++ web-client/src/ustc-ui/CaseLink/CaseLink.jsx
@@ -3,14 +3,19 @@ import React from 'react';
 import classNames from 'classnames';
 
 export const CaseLink = connect(props => {
-  const { children, className, formattedCase } = props;
+  const { children, className, docketNumber, formattedCase } = props;
+
+  const docketNumberString =
+    docketNumber || (formattedCase && formattedCase.docketNumber);
+  const docketNumberWithSuffixString =
+    formattedCase && formattedCase.docketNumberWithSuffix;
 
   return (
     <a
       className={classNames('no-wrap', className)}
-      href={`/case-detail/${formattedCase.docketNumber}`}
+      href={`/case-detail/${docketNumberString}`}
     >
-      {children || formattedCase.docketNumberWithSuffix}
+      {children || docketNumberWithSuffixString || docketNumberString}
     </a>
   );
 });
