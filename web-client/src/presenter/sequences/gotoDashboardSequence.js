bf2910af246bf65c9fe7d1cf48255a57ff1aee8e
Kris Koskelin <kkoskelin@flexion.us>
2019-09-17 17:19:50 -0400

    pulling apart messages and dashboard routing, WIP
    [skip ci]

--- web-client/src/presenter/sequences/gotoDashboardSequence.js
+++ web-client/src/presenter/sequences/gotoDashboardSequence.js
@@ -1,21 +1,14 @@
-import { chooseWorkQueueSequence } from './chooseWorkQueueSequence';
 import { clearErrorAlertsAction } from '../actions/clearErrorAlertsAction';
-import { getBaseRouteAction } from '../actions/getBaseRouteAction';
 import { getCasesByUserAction } from '../actions/getCasesByUserAction';
-import { getTrialSessionsAction } from '../actions/TrialSession/getTrialSessionsAction';
 import { getUserAction } from '../actions/getUserAction';
 import { getUserRoleAction } from '../actions/getUserRoleAction';
-import { getUsersInSectionAction } from '../actions/getUsersInSectionAction';
 import { isLoggedInAction } from '../actions/isLoggedInAction';
-import { parallel } from 'cerebral/factories';
+import { navigateToPathAction } from '../actions/navigateToPathAction';
 import { redirectToCognitoAction } from '../actions/redirectToCognitoAction';
 import { set } from 'cerebral/factories';
 import { setCasesAction } from '../actions/setCasesAction';
 import { setCurrentPageAction } from '../actions/setCurrentPageAction';
-import { setMessageInboxPropsAction } from '../actions/setMessageInboxPropsAction';
-import { setTrialSessionsAction } from '../actions/TrialSession/setTrialSessionsAction';
 import { setUserAction } from '../actions/setUserAction';
-import { setUsersAction } from '../actions/setUsersAction';
 import { state } from 'cerebral';
 
 const goToDashboard = [
@@ -26,46 +19,14 @@ const goToDashboard = [
   clearErrorAlertsAction,
   getUserRoleAction,
   {
-    docketclerk: [
-      parallel([
-        [getUsersInSectionAction({ section: 'docket' }), setUsersAction],
-        [
-          setCurrentPageAction('DashboardDocketClerk'),
-          ...chooseWorkQueueSequence,
-        ],
-      ]),
-    ],
-    judge: [
-      getBaseRouteAction,
-      {
-        dashboard: [setMessageInboxPropsAction],
-        'document-qc': [],
-        messages: [],
-      },
-      ...chooseWorkQueueSequence,
-      getTrialSessionsAction,
-      setTrialSessionsAction,
-      getBaseRouteAction,
-      {
-        dashboard: [setCurrentPageAction('DashboardJudge')],
-        'document-qc': [setCurrentPageAction('MessagesJudge')],
-        messages: [setCurrentPageAction('MessagesJudge')],
-      },
-    ],
+    docketclerk: [set(state.path, '/messages'), navigateToPathAction],
+    judge: [set(state.path, '/messages'), navigateToPathAction],
     petitioner: [
       getCasesByUserAction,
       setCasesAction,
       setCurrentPageAction('DashboardPetitioner'),
     ],
-    petitionsclerk: [
-      parallel([
-        [getUsersInSectionAction({ section: 'petitions' }), setUsersAction],
-        [
-          setCurrentPageAction('DashboardPetitionsClerk'),
-          ...chooseWorkQueueSequence,
-        ],
-      ]),
-    ],
+    petitionsclerk: [set(state.path, '/messages'), navigateToPathAction],
     practitioner: [
       getCasesByUserAction,
       setCasesAction,
@@ -76,10 +37,7 @@ const goToDashboard = [
       setCasesAction,
       setCurrentPageAction('DashboardRespondent'),
     ],
-    seniorattorney: [
-      setCurrentPageAction('DashboardSeniorAttorney'),
-      ...chooseWorkQueueSequence,
-    ],
+    seniorattorney: [set(state.path, '/messages'), navigateToPathAction],
   },
 ];
 
