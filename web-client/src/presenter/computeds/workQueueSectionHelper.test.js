ddc478e719b2103127eea397ab4bc993ee0fddf5
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-09-18 13:52:21 -0400

    Work queue/messages display fix - not showing judge section

--- web-client/src/presenter/computeds/workQueueSectionHelper.test.js
+++ web-client/src/presenter/computeds/workQueueSectionHelper.test.js
@@ -10,4 +10,17 @@ describe('workQueueSectionHelper', () => {
     expect(sectionDisplay('clerkofcourt')).toBe('Clerk of the Court');
     expect(chambersDisplay('urdasChambers')).toBe("Urda's Chambers");
   });
+
+  it('returns the chambers display for section display if the section is a chambers', () => {
+    const { chambersDisplay, sectionDisplay } = runCompute(
+      workQueueSectionHelper,
+    );
+    expect(sectionDisplay('urdasChambers')).toBe("Urda's Chambers");
+    expect(chambersDisplay('urdasChambers')).toBe("Urda's Chambers");
+  });
+
+  it('returns undefined for sectionDisplay if the section is not a regular section or a chambers', () => {
+    const { sectionDisplay } = runCompute(workQueueSectionHelper);
+    expect(sectionDisplay('something')).toBeUndefined();
+  });
 });
