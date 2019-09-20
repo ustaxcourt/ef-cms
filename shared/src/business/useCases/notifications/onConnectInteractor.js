98560fe28e0185e4f3921481c2aa3720ab572481
William Bagayoko <wbagayoko@gmail.com>
2019-09-20 10:49:48 -0500

    Update tests / lint

--- shared/src/business/useCases/notifications/onConnectInteractor.js
+++ shared/src/business/useCases/notifications/onConnectInteractor.js
@@ -1,18 +1,9 @@
-const {
-  isAuthorized,
-  WORKITEM,
-} = require('../../../authorization/authorizationClientService');
-const { UnauthorizedError } = require('../../../errors/errors');
-
 /**
  * onConnectInteractor
  *
  */
 exports.onConnectInteractor = async ({ applicationContext, connectionId }) => {
   const authorizedUser = applicationContext.getCurrentUser();
-  // if (!isAuthorized(authorizedUser, WORKITEM)) {
-  //   throw new UnauthorizedError('Unauthorized to assign work item');
-  // }
 
   console.log('user', authorizedUser);
 
