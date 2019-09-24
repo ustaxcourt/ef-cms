d4e98047dd257f35d93b609eff249e91271a5f86
Cody Seibert <cseibert113@gmail.com>
2019-09-24 08:44:36 -0400

    fixing issue with params

--- web-api/src/trialSessions/batchDownloadTrialSessionLambda.js
+++ web-api/src/trialSessions/batchDownloadTrialSessionLambda.js
@@ -15,7 +15,7 @@ exports.handler = event =>
     const user = getUserFromAuthHeader(event);
     const applicationContext = createApplicationContext(user);
     try {
-      const { trialSessionId } = event.pathParameters || {};
+      const { trialSessionId } = event.pathParameters || event.path;
       const results = await applicationContext
         .getUseCases()
         .batchDownloadTrialSessionInteractor({
