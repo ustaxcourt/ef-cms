d7b11b9eb9a38540cdad57974c734c27bd79569e
Cody Seibert <cseibert113@gmail.com>
2019-09-24 21:00:47 -0400

    fixining spinner;

--- web-client/src/presenter/actions/setupNotificationListenerAction.js
+++ web-client/src/presenter/actions/setupNotificationListenerAction.js
@@ -1,5 +1,8 @@
+import { state } from 'cerebral';
+
 export const setupNotificationListenerAction = async ({
   applicationContext,
+  store,
 }) => {
   const token = applicationContext.getCurrentUserToken();
 
@@ -9,8 +12,10 @@ export const setupNotificationListenerAction = async ({
   socket.onopen = () => {};
 
   socket.onmessage = event => {
+    // TODO: we should check for an event name
     const message = JSON.parse(event.data);
     const { url } = message;
+    store.set(state.waitingForResponse, false);
     window.open(url, '_blank');
     socket.close();
   };
--- web-client/src/presenter/sequences/batchDownloadTrialSessionSequence.js
+++ web-client/src/presenter/sequences/batchDownloadTrialSessionSequence.js
@@ -1,11 +1,9 @@
 import { batchDownloadTrialSessionAction } from '../actions/batchDownloadTrialSessionAction';
 import { setWaitingForResponseAction } from '../actions/setWaitingForResponseAction';
 import { setupNotificationListenerAction } from '../actions/setupNotificationListenerAction';
-import { unsetWaitingForResponseAction } from '../actions/unsetWaitingForResponseAction';
 
 export const batchDownloadTrialSessionSequence = [
   setWaitingForResponseAction,
   setupNotificationListenerAction,
   batchDownloadTrialSessionAction,
-  unsetWaitingForResponseAction,
 ];
