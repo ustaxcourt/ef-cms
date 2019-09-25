0e500d1ef52dcd7ffd0475c87f859227dcd94c45
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-09-25 10:15:12 -0600

    test coverage - remove unnecessary logic

--- shared/src/persistence/dynamo/cases/createCase.js
+++ shared/src/persistence/dynamo/cases/createCase.js
@@ -22,7 +22,7 @@ exports.createCase = async ({ applicationContext, caseToCreate }) => {
     saveVersionedCase({
       applicationContext,
       caseToSave: caseToCreate,
-      existingVersion: (caseToCreate || {}).currentVersion,
+      existingVersion: caseToCreate.currentVersion,
     }),
     createMappingRecord({
       applicationContext,
