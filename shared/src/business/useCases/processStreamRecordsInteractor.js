4fc0ecafc93a81442686e00b6e1c80dab07417f3
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-09-25 10:06:27 -0600

    remove unneeded try/catch

--- shared/src/business/useCases/processStreamRecordsInteractor.js
+++ shared/src/business/useCases/processStreamRecordsInteractor.js
@@ -9,22 +9,18 @@ exports.processStreamRecordsInteractor = async ({
   recordsToProcess,
 }) => {
   applicationContext.logger.info('Time', createISODateString());
-  try {
-    recordsToProcess.forEach(async record => {
-      if (['INSERT', 'MODIFY'].includes(record.eventName)) {
-        try {
-          await applicationContext.getSearchClient().index({
-            body: { ...record.dynamodb.NewImage },
-            id: record.dynamodb.Keys.pk.S,
-            index: 'efcms',
-          });
-        } catch (e) {
-          applicationContext.logger.info('Error', JSON.stringify(e));
-        }
+  recordsToProcess.forEach(async record => {
+    if (['INSERT', 'MODIFY'].includes(record.eventName)) {
+      try {
+        await applicationContext.getSearchClient().index({
+          body: { ...record.dynamodb.NewImage },
+          id: record.dynamodb.Keys.pk.S,
+          index: 'efcms',
+        });
+      } catch (e) {
+        applicationContext.logger.info('Error', e);
       }
-    });
-  } catch (e) {
-    console.log('error!', e);
-  }
+    }
+  });
   applicationContext.logger.info('Time', createISODateString());
 };
