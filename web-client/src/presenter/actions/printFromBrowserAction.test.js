3f487a6d57667604ac92b1839436e743c56caebb
Joseph Sutton <jsutton@flexion.us>
2019-09-26 12:14:35 -0500

    added print screen and action/sequence for printing orders needed page

---
+++ web-client/src/presenter/actions/printFromBrowserAction.test.js
@@ -0,0 +1,18 @@
+import { printFromBrowserAction } from './printFromBrowserAction';
+import { runAction } from 'cerebral/test';
+
+describe('printFromBrowserAction', () => {
+  let printFromWindowStub;
+
+  beforeEach(() => {
+    printFromWindowStub = jest.fn();
+
+    global.window.print = printFromWindowStub;
+  });
+
+  it('prints from the browser', async () => {
+    await runAction(printFromBrowserAction);
+
+    expect(printFromWindowStub).toHaveBeenCalled();
+  });
+});
