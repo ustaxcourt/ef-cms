b7afd0aceec7f468f444cb1bc7602e5fbee2f1fb
William Bagayoko <wbagayoko@gmail.com>
2019-09-26 09:27:35 -0500

    1282 Update JSDocs

--- shared/src/persistence/dynamo/notifications/deleteUserConnection.js
+++ shared/src/persistence/dynamo/notifications/deleteUserConnection.js
@@ -1,11 +1,11 @@
 const client = require('../../dynamodbClientService');
 
 /**
- * createTrialSession
+ * deleteUserConnection
  *
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
- * @param {object} providers.trialSession the trial session data
+ * @param {object} providers.connectionId the websocket connection id
  * @returns {Promise} the promise of the call to persistence
  */
 exports.deleteUserConnection = async ({ applicationContext, connectionId }) => {
