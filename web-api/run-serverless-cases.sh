6e9823800a220ccfccec2d465a4012e5d8efe298
Cody Seibert <cseibert113@gmail.com>
2019-09-26 16:09:23 -0400

    getting this to work on aws

--- web-api/run-serverless-cases.sh
+++ web-api/run-serverless-cases.sh
@@ -1,2 +1,9 @@
 #!/bin/bash -e
+
+pushd ./web-api/terraform/main
+  ../bin/deploy-init.sh "${1}"
+  export DYNAMO_STREAM_ARN=$(terraform output dynamo_stream_arn)
+  export ELASTICSEARCH_ENDPOINT=$(terraform output elasticsearch_endpoint)
+popd
+
 ./web-api/run-serverless.sh "${1}" "${2}" "casesHandlers.js" "serverless-cases.yml" "build:api:cases"
