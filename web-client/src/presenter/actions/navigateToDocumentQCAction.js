91009ee3ac8a3b11548f61232d1ec9aae5c843c2
Matt Hopson <matthopson@gmail.com>
2019-09-27 07:23:39 -0600

    Navigate to document qc when serving with no orders

---
+++ web-client/src/presenter/actions/navigateToDocumentQCAction.js
@@ -0,0 +1,10 @@
+/**
+ * changes the route to messages, which is "/document-qc"
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.router the riot.router object that is used for changing the route
+ * @returns {Promise} async action
+ */
+export const navigateToDocumentQCAction = async ({ router }) => {
+  await router.route('/document-qc');
+};
