ea187602ec3eb3f8c2677180c1d590533eae44ae
Joseph Sutton <jsutton@flexion.us>
2019-09-30 15:01:55 -0500

    added document detail logic

--- web-client/integration-tests/journey/petitionsClerkUpdatesCaseWithOrders.js
+++ web-client/integration-tests/journey/petitionsClerkUpdatesCaseWithOrders.js
@@ -1,11 +1,15 @@
 import { caseDetailHelper as caseDetailHelperComputed } from '../../src/presenter/computeds/caseDetailHelper';
+import { documentDetailHelper as documentDetailHelperComputed } from '../../src/presenter/computeds/documentDetailHelper';
 import { runCompute } from 'cerebral/test';
 import { withAppContextDecorator } from '../../src/withAppContext';
 
 const caseDetailHelper = withAppContextDecorator(caseDetailHelperComputed);
+const documentDetailHelper = withAppContextDecorator(
+  documentDetailHelperComputed,
+);
 
 export default test => {
-  return it('Petitions clerk adds an order', async () => {
+  return it('Petitions clerk adds orders to a case', async () => {
     // order for amended petition
     await test.runSequence('updateFormValueSequence', {
       key: 'orderForAmendedPetition',
@@ -14,9 +18,14 @@ export default test => {
     await test.runSequence('autoSaveCaseSequence');
     expect(test.getState('caseDetailErrors')).toEqual({});
 
-    const helper = runCompute(caseDetailHelper, {
+    const caseHelper = runCompute(caseDetailHelper, {
       state: test.getState(),
     });
-    expect(helper.hasOrders).toEqual(true);
+    expect(caseHelper.hasOrders).toEqual(true);
+
+    const documentHelper = runCompute(documentDetailHelper, {
+      state: test.getState(),
+    });
+    expect(documentHelper.showViewOrdersNeededButton).toEqual(true);
   });
 };
