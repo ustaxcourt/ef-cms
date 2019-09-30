ace40e0401ed7fd1fb066d0822a6f1bb039a84e4
Matt Hopson <matthopson@gmail.com>
2019-09-30 11:51:25 -0600

    Update Case caption label

--- web-client/src/views/CaseDetailEdit/PartyInformation.jsx
+++ web-client/src/views/CaseDetailEdit/PartyInformation.jsx
@@ -31,7 +31,7 @@ export const PartyInformation = connect(
         <div className="subsection">
           <div className="usa-form-group">
             <label className="usa-label" htmlFor="case-caption">
-              Case Caption
+              Case caption
             </label>
             <textarea
               className="usa-textarea"
