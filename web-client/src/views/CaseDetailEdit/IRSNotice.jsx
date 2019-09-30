225325b533f16243ddd1b9091c6778bfc3642baa
Matt Hopson <matthopson@gmail.com>
2019-09-30 12:12:12 -0600

    Update Notice attached label

--- web-client/src/views/CaseDetailEdit/IRSNotice.jsx
+++ web-client/src/views/CaseDetailEdit/IRSNotice.jsx
@@ -31,7 +31,7 @@ export const IRSNotice = connect(
       return (
         <fieldset className="usa-fieldset" id="irs-verified-notice-radios">
           <legend htmlFor="irs-verified-notice-radios">
-            Notice Attached to Petition?
+            Notice attached to petition?
           </legend>
           <div className="usa-radio usa-radio__inline">
             <input
