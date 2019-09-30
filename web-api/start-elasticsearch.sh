6351dfc0630efcdf6dcf31315970379cf70da14d
Kris Koskelin <kkoskelin@flexion.us>
2019-09-30 14:17:22 -0500

    updated script to use better vars

--- web-api/start-elasticsearch.sh
+++ web-api/start-elasticsearch.sh
@@ -2,13 +2,14 @@
 
 # download the elasticsearch archive if needed
 ES_DOWNLOAD="https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.3.2-darwin-x86_64.tar.gz"
-ES_DESTINATION=".elasticsearch"
+ES_ARCH_RENAMED="elasticsearch.tar.gz"
+ES_DESTINATION=".elasticsearch/"
 
-if [ ! -f "./.elasticsearch/bin/elasticsearch" ]; then
-  mkdir -p .elasticsearch/
-  curl $ES_DOWNLOAD --output "./.elasticsearch/elasticsearch.tar.gz" --create-dirs
-  cd .elasticsearch && tar -xvf "elasticsearch.tar.gz" --strip-components=1 && cd ..
+if [ ! -d "$ES_DESTINATION" ]; then
+  mkdir -p "$ES_DESTINATION"
+  curl "$ES_DOWNLOAD" --output "${ES_DESTINATION}/${ES_ARCH_RENAMED}" --create-dirs
+  cd "$ES_DESTINATION" && tar -xvf "$ES_ARCH_RENAMED" --strip-components=1 && cd ..
 fi
 
 # start elasticsearch
-cd .elasticsearch && ./bin/elasticsearch
+cd "$ES_DESTINATION" && ./bin/elasticsearch
