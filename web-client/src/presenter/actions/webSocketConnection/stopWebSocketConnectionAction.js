a94a4d35b71ea800beeca264d01b1ab08482f5bd
William Bagayoko <wbagayoko@gmail.com>
2019-09-30 09:36:04 -0500

    update jsdocs

--- web-client/src/presenter/actions/webSocketConnection/stopWebSocketConnectionAction.js
+++ web-client/src/presenter/actions/webSocketConnection/stopWebSocketConnectionAction.js
@@ -1,5 +1,8 @@
 /**
- * Stop websocket connection
+ * stopWebSocketConnectionAction
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.socket the socket object
  */
 export const stopWebSocketConnectionAction = ({ socket }) => {
   socket.stop();
