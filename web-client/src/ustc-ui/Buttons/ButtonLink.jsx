a4d03d7b91a5e0e372bd611002892752ed51f592
Eric Sorenson <eric@sorenson.software>
2019-09-30 15:20:24 -0500

    button component covers all types of button styles

--- web-client/src/ustc-ui/Buttons/ButtonLink.jsx
+++ web-client/src/ustc-ui/Buttons/ButtonLink.jsx
@@ -1,19 +1,38 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
 
 export const ButtonLink = props => {
-  const { className, ...remainingProps } = props;
+  const {
+    children,
+    className,
+    icon,
+    link,
+    secondary,
+    ...remainingProps
+  } = props;
   const classes = classNames(
-    'usa-button usa-button--unstyled ustc-button--unstyled bg-orange',
+    'usa-button margin-right-205 margin-bottom-205',
+    secondary && 'usa-button--outline',
+    link && 'usa-button--unstyled ustc-button--unstyled',
     className,
   );
 
   return (
-    <button className={classes} type="button" {...remainingProps}></button>
+    <button className={classes} type="button" {...remainingProps}>
+      {icon && (
+        <FontAwesomeIcon className="margin-right-05" icon={icon} size="1x" />
+      )}
+      {children}
+    </button>
   );
 };
 
 ButtonLink.propTypes = {
+  children: PropTypes.node,
   className: PropTypes.string,
+  icon: PropTypes.string,
+  link: PropTypes.bool,
+  secondary: PropTypes.bool,
 };
