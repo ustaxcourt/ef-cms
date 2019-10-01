9ae8642a0dfb24d1cfa3be75411b880b5202110c
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-10-01 11:15:16 -0700

    fix aws region for elasticsearch index settings

--- web-api/elasticsearch-index-settings.js
+++ web-api/elasticsearch-index-settings.js
@@ -1,53 +1,36 @@
 (async () => {
-  let indexExists;
+  const AWS = require('aws-sdk');
+  AWS.config.region = 'us-east-1';
+
+  const elasticsearch = require('elasticsearch');
+  const connectionClass = require('http-aws-es');
+
+  AWS.config.httpOptions.timeout = 300000;
+
+  const { EnvironmentCredentials } = AWS;
+
+  const environment = {
+    elasticsearchEndpoint: process.env.ELASTICSEARCH_ENDPOINT,
+    region: 'us-east-1',
+  };
+
+  const searchClientCache = new elasticsearch.Client({
+    amazonES: {
+      credentials: new EnvironmentCredentials('AWS'),
+      region: environment.region,
+    },
+    apiVersion: '7.1',
+    connectionClass: connectionClass,
+    host: {
+      host: `${environment.elasticsearchEndpoint}`,
+      port: 443,
+      protocol: 'https',
+    },
+    log: 'warning',
+  });
+
   try {
-    const AWS = require('aws-sdk');
-
-    const elasticsearch = require('elasticsearch');
-    const connectionClass = require('http-aws-es');
-
-    AWS.config.httpOptions.timeout = 300000;
-
-    const { EnvironmentCredentials } = AWS;
-
-    const environment = {
-      elasticsearchEndpoint: process.env.ELASTICSEARCH_ENDPOINT,
-      region: 'us-east-1',
-    };
-
-    console.log('elasticsearch endpoint is', environment.elasticsearchEndpoint);
-
-    const searchClientCache = new elasticsearch.Client({
-      amazonES: {
-        credentials: new EnvironmentCredentials('AWS'),
-        region: environment.region,
-      },
-      apiVersion: '7.1',
-      connectionClass: connectionClass,
-      host: {
-        host: `${environment.elasticsearchEndpoint}`,
-        port: 443,
-        protocol: 'https',
-      },
-      log: 'warning',
-    });
-
-    await searchClientCache.index({
-      body: { something: 'something' },
-      id: '1234567890',
-      index: 'efcms',
-    });
-
-    /*searchClientCache.indices.create({
-      body: {
-        settings: {
-          'index.mapping.total_fields.limit': '2000',
-        },
-      },
-      index: 'efcms',
-    });*/
-
-    /*indexExists = await searchClientCache.indices.exists({
+    const indexExists = await searchClientCache.indices.exists({
       body: {},
       index: 'efcms',
     });
@@ -67,9 +50,8 @@
         },
         index: 'efcms',
       });
-    }*/
+    }
   } catch (e) {
-    console.log('inside the catch');
     console.log(e);
   }
 })();
