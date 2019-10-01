7baea2aa5dbaf1c9fb1f74e44f2bee311e53fec3
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-10-01 10:56:27 -0700

    index settings wip

--- web-api/elasticsearch-index-settings.js
+++ web-api/elasticsearch-index-settings.js
@@ -1,49 +1,75 @@
 (async () => {
-  const AWS = require('aws-sdk');
+  let indexExists;
+  try {
+    const AWS = require('aws-sdk');
 
-  const elasticsearch = require('elasticsearch');
-  const connectionClass = require('http-aws-es');
+    const elasticsearch = require('elasticsearch');
+    const connectionClass = require('http-aws-es');
 
-  AWS.config.httpOptions.timeout = 300000;
+    AWS.config.httpOptions.timeout = 300000;
 
-  const { EnvironmentCredentials } = AWS;
+    const { EnvironmentCredentials } = AWS;
 
-  const environment = {
-    elasticsearchEndpoint: process.env.ELASTICSEARCH_ENDPOINT,
-    region: process.env.AWS_REGION,
-  };
+    const environment = {
+      elasticsearchEndpoint: process.env.ELASTICSEARCH_ENDPOINT,
+      region: 'us-east-1',
+    };
 
-  const searchClientCache = new elasticsearch.Client({
-    amazonES: {
-      credentials: new EnvironmentCredentials('AWS'),
-      region: environment.region,
-    },
-    apiVersion: '7.1',
-    connectionClass: connectionClass,
-    host: environment.elasticsearchEndpoint,
-    log: 'warning',
-    port: 443,
-    protocol: 'https',
-  });
+    console.log('elasticsearch endpoint is', environment.elasticsearchEndpoint);
 
-  const indexExists = await searchClientCache.indices.exists({
-    index: 'efcms',
-  });
-  if (!indexExists) {
-    searchClientCache.indices.create({
+    const searchClientCache = new elasticsearch.Client({
+      amazonES: {
+        credentials: new EnvironmentCredentials('AWS'),
+        region: environment.region,
+      },
+      apiVersion: '7.1',
+      connectionClass: connectionClass,
+      host: {
+        host: `${environment.elasticsearchEndpoint}`,
+        port: 443,
+        protocol: 'https',
+      },
+      log: 'warning',
+    });
+
+    await searchClientCache.index({
+      body: { something: 'something' },
+      id: '1234567890',
+      index: 'efcms',
+    });
+
+    /*searchClientCache.indices.create({
       body: {
         settings: {
           'index.mapping.total_fields.limit': '2000',
         },
       },
       index: 'efcms',
-    });
-  } else {
-    searchClientCache.indices.putSettings({
-      body: {
-        'index.mapping.total_fields.limit': '2000',
-      },
+    });*/
+
+    /*indexExists = await searchClientCache.indices.exists({
+      body: {},
       index: 'efcms',
     });
+    if (!indexExists) {
+      searchClientCache.indices.create({
+        body: {
+          settings: {
+            'index.mapping.total_fields.limit': '2000',
+          },
+        },
+        index: 'efcms',
+      });
+    } else {
+      searchClientCache.indices.putSettings({
+        body: {
+          'index.mapping.total_fields.limit': '2000',
+        },
+        index: 'efcms',
+      });
+    }*/
+  } catch (e) {
+    console.log('inside the catch');
+    console.log(e);
   }
 })();
