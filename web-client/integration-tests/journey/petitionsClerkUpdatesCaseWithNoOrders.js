b9c111ae34e5a972546dbd27e404a85e8b8dd4c0
Joseph Sutton <jsutton@flexion.us>
2019-10-02 09:28:53 -0500

    refactored integration test, added another test case

--- web-client/integration-tests/journey/petitionsClerkUpdatesCaseWithNoOrders.js
+++ web-client/integration-tests/journey/petitionsClerkUpdatesCaseWithNoOrders.js
@@ -9,19 +9,21 @@ const documentDetailHelper = withAppContextDecorator(
 );
 
 export default test => {
-  return it('Petitions clerk adds orders to a case', async () => {
-    // order for amended petition = true
+  return it('Petitions clerk removes orders from a case', async () => {
+    // order for designating place of trial = false
     await test.runSequence('updateFormValueSequence', {
-      key: 'orderForAmendedPetition',
-      value: true,
+      key: 'orderDesignatingPlaceOfTrial',
+      value: false,
     });
     await test.runSequence('autoSaveCaseSequence');
     expect(test.getState('caseDetailErrors')).toEqual({});
 
+    console.log('caseDetail', test.getState('caseDetail'));
+
     const caseHelper = runCompute(caseDetailHelper, {
       state: test.getState(),
     });
-    expect(caseHelper.hasOrders).toEqual(true);
+    expect(caseHelper.hasOrders).toEqual(false);
 
     const documentHelper = runCompute(documentDetailHelper, {
       state: test.getState(),
