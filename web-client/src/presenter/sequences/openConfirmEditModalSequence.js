bdf0678e549aaa9ea46c65bddc449f2f22ce4e1e
Cody Seibert <cseibert113@gmail.com>
2019-10-04 10:48:25 -0400

    pr comments

--- web-client/src/presenter/sequences/openConfirmEditModalSequence.js
+++ web-client/src/presenter/sequences/openConfirmEditModalSequence.js
@@ -1,12 +1,11 @@
 import { clearModalStateAction } from '../actions/clearModalStateAction';
 import { props, state } from 'cerebral';
 import { set } from 'cerebral/factories';
+import { setConfirmEditModalStateAction } from '../actions/setConfirmEditModalStateAction';
 
 export const openConfirmEditModalSequence = [
   clearModalStateAction,
   set(state.path, props.path),
-  set(state.docketNumber, props.docketNumber),
-  set(state.documentIdToEdit, props.documentIdToEdit),
-  set(state.caseId, props.caseId),
+  setConfirmEditModalStateAction,
   set(state.showModal, 'ConfirmEditModal'),
 ];
