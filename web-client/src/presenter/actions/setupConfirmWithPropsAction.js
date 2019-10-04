56d02107343b0884a080d4890152964cce207ce8
Joseph Sutton <jsutton@flexion.us>
2019-10-03 21:40:14 -0500

    added unit test to action and entity

--- web-client/src/presenter/actions/setupConfirmWithPropsAction.js
+++ web-client/src/presenter/actions/setupConfirmWithPropsAction.js
@@ -7,6 +7,7 @@ export const setupConfirmWithPropsAction = async ({ get }) => {
   const caseId = get(state.caseDetail.caseId);
   const documentIdToEdit = get(state.documentIdToEdit);
   const docketNumber = get(state.caseDetail.docketNumber);
+
   return {
     caseId,
     docketNumber,
