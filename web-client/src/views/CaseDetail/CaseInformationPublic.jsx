272082ff29da7989637f68d77575ca9afd947106
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-10-07 09:02:53 -0700

    2102: form updates; success message; remove petition details card

--- web-client/src/views/CaseDetail/CaseInformationPublic.jsx
+++ web-client/src/views/CaseDetail/CaseInformationPublic.jsx
@@ -4,28 +4,6 @@ import { state } from 'cerebral';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const PetitionDetails = ({ caseDetail, showPaymentRecord }) => (
-  <React.Fragment>
-    <div className="grid-container padding-x-0">
-      {showPaymentRecord && (
-        <React.Fragment>
-          <p className="label">Petition Fee Paid</p>
-          <p>
-            Paid by pay.gov
-            <br />
-            {caseDetail.payGovDateFormatted}
-          </p>
-        </React.Fragment>
-      )}
-    </div>
-  </React.Fragment>
-);
-
-PetitionDetails.propTypes = {
-  caseDetail: PropTypes.object,
-  showPaymentRecord: PropTypes.bool,
-};
-
 const TrialInformation = ({ caseDetail }) => (
   <React.Fragment>
     <div className="grid-row">
@@ -53,57 +31,28 @@ TrialInformation.propTypes = {
 
 export const CaseInformationPublic = connect(
   {
-    caseDetailHelper: state.caseDetailHelper,
     formattedCaseDetail: state.formattedCaseDetail,
   },
-  ({ caseDetailHelper, formattedCaseDetail }) => {
+  ({ formattedCaseDetail }) => {
     return (
       <div className="petitions-details">
         <div className="grid-container padding-x-0">
-          <div className="grid-row grid-gap">
-            <NonMobile>
-              <div className="tablet:grid-col-6">
-                <div className="card height-full">
-                  <div className="content-wrapper">
-                    <h3 className="underlined">Petition Details</h3>
-                    <PetitionDetails
-                      caseDetail={formattedCaseDetail}
-                      showPaymentRecord={caseDetailHelper.showPaymentRecord}
-                    />
-                  </div>
-                </div>
+          <NonMobile>
+            <div className="card height-full">
+              <div className="content-wrapper">
+                <h3 className="underlined">Trial Information</h3>
+                <TrialInformation caseDetail={formattedCaseDetail} />
               </div>
-            </NonMobile>
-            <Mobile>
-              <div className="tablet:grid-col-6">
-                <div className="case-info-card">
-                  <h3>Petition Details</h3>
-                  <PetitionDetails
-                    caseDetail={formattedCaseDetail}
-                    showPaymentRecord={caseDetailHelper.showPaymentRecord}
-                  />
-                </div>
+            </div>
+          </NonMobile>
+          <Mobile>
+            <div className="margin-top-2">
+              <div className="case-info-card">
+                <h3>Trial Information</h3>
+                <TrialInformation caseDetail={formattedCaseDetail} />
               </div>
-            </Mobile>
-            <NonMobile>
-              <div className="tablet:grid-col-6">
-                <div className="card height-full">
-                  <div className="content-wrapper">
-                    <h3 className="underlined">Trial Information</h3>
-                    <TrialInformation caseDetail={formattedCaseDetail} />
-                  </div>
-                </div>
-              </div>
-            </NonMobile>
-            <Mobile>
-              <div className="tablet:grid-col-6 margin-top-2">
-                <div className="case-info-card">
-                  <h3>Trial Information</h3>
-                  <TrialInformation caseDetail={formattedCaseDetail} />
-                </div>
-              </div>
-            </Mobile>
-          </div>
+            </div>
+          </Mobile>
         </div>
       </div>
     );
