4423cfaabc6c34820f86de44b01bf0765ed3825d
Joseph Sutton <jsutton@flexion.us>
2019-10-08 08:45:42 -0500

    updated pdfjsObj.getData in integration test

--- web-client/integration-tests/journey/petitionsClerkClearsSignatureFromDraftDocument.js
+++ web-client/integration-tests/journey/petitionsClerkClearsSignatureFromDraftDocument.js
@@ -2,7 +2,6 @@ export default test => {
   return it('Petitions clerk applies signature to a draft document', async () => {
     await test.runSequence('setPDFSignatureDataSequence', {
       isPdfAlreadySigned: false,
-      pdfjsObj: { getData: () => {} },
       signatureApplied: false,
       signatureData: null,
     });
