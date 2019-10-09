be0f148b9361c2f7802ac6970c1e51f3525499da
Matt Hopson <matthopson@gmail.com>
2019-10-09 09:59:08 -0600

    Alert Success with printReceiptLink

--- web-client/src/presenter/actions/FileDocument/getFileExternalDocumentAlertSuccessAction.js
+++ web-client/src/presenter/actions/FileDocument/getFileExternalDocumentAlertSuccessAction.js
@@ -28,10 +28,16 @@ export const getFileExternalDocumentAlertSuccessAction = ({ get, props }) => {
     };
   }
 
+  let message =
+    'You can access your documents at any time from the docket record below.';
+
+  if (props.printReceiptLink) {
+    message = `You can <a href="${props.printReceiptLink}">print a receipt</a> for your filing or access your documents at any time from the docket record below.`;
+  }
+
   return {
     alertSuccess: {
-      message:
-        'You can access your documents at any time from the docket record below.',
+      message,
       title: 'Your filing has been successfully submitted.',
     },
   };
