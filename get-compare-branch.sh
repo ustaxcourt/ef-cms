2720ecf774aed42143a750229d287eaaa2f40d29
Joseph Sutton <jsutton@flexion.us>
2019-10-09 11:00:06 -0500

    added default compare branch (stored in CI)

--- get-compare-branch.sh
+++ get-compare-branch.sh
@@ -3,5 +3,5 @@
 if [ ! -z $CIRCLE_PR_NUMBER ]; then
     echo $(curl -fsSL https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls/$CIRCLE_PR_NUMBER | jq -r '.base.ref')
 else
-    echo "develop"
+    echo $DEFAULT_COMPARE_BRANCH
 fi
