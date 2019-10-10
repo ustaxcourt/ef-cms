bbf42a96d550c42b75da5d3a7587002b4d439546
Joseph Sutton <jsutton@flexion.us>
2019-10-10 09:53:20 -0500

    debugging

--- stack-deploy-check.sh
+++ stack-deploy-check.sh
@@ -6,8 +6,14 @@ COMPARE_BRANCH=$3   # ex: develop, staging
 ENV=$4              # ex: exp, stg, dev
 REGION=$5           # ex: us-east-1, us-west-1
 
-REDEPLOY_VIA_DEPS="$(git --no-pager diff --name-only origin/${COMPARE_BRANCH} | node ./web-api/dependency-tree-check.js ${STACK_NAME})"
-REDEPLOY_VIA_SLS="$((git --no-pager diff --name-only origin/${COMPARE_BRANCH} | grep -c "${STACK_PATTERN}") || test $? = 1)"
+echo "stack name ${1} ${STACK_NAME}"
+echo "stack pattern ${2} ${STACK_PATTERN}"
+echo "compare branch ${3} ${COMPARE_BRANCH}"
+echo "env ${4} ${ENV}"
+echo "region ${5} ${REGION}"
+
+REDEPLOY_VIA_DEPS=$(git --no-pager diff --name-only origin/${COMPARE_BRANCH} | node ./web-api/dependency-tree-check.js ${STACK_NAME})
+REDEPLOY_VIA_SLS=$(git --no-pager diff --name-only origin/${COMPARE_BRANCH} | grep -c "${STACK_PATTERN}" || test $? = 1)
 
 if [[ -n "$REDEPLOY_VIA_DEPS" ]] || [[ "$REDEPLOY_VIA_SLS" != "0" ]]; then
   ./web-api/run-${STACK_PATTERN}.sh "${ENV}" "${REGION}"
