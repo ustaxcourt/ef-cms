01ce3d55717d755cbecadf040c8f22face53a465
William Bagayoko <wbagayoko@gmail.com>
2019-10-10 11:23:41 -0500

    991 edit docket entry wip

--- web-client/src/presenter/sequences/gotoEditDocketEntrySequence.js
+++ web-client/src/presenter/sequences/gotoEditDocketEntrySequence.js
@@ -1,13 +1,15 @@
 import { clearFormAction } from '../actions/clearFormAction';
 import { clearScansAction } from '../actions/clearScansAction';
 import { clearScreenMetadataAction } from '../actions/clearScreenMetadataAction';
+import { deconstructReceivedAtDateToFormAction } from '../actions/EditDocketRecord/deconstructReceivedAtDateToFormAction';
 import { getCaseAction } from '../actions/getCaseAction';
 import { isLoggedInAction } from '../actions/isLoggedInAction';
 import { redirectToCognitoAction } from '../actions/redirectToCognitoAction';
 import { set } from 'cerebral/factories';
+import { setBaseUrlAction } from '../actions/setBaseUrlAction';
 import { setCaseAction } from '../actions/setCaseAction';
 import { setCurrentPageAction } from '../actions/setCurrentPageAction';
-import { setDocketEntryFormForEditAction } from '../actions/EditDocketRecord/setDocketEntryFormForEditAction';
+import { setDocketEntryFormForDocketEditAction } from '../actions/EditDocketRecord/setDocketEntryFormForDocketEditAction';
 import { setDocumentIdAction } from '../actions/setDocumentIdAction';
 import { state } from 'cerebral';
 
@@ -19,12 +21,11 @@ export const gotoEditDocketEntry = [
   clearScreenMetadataAction,
   getCaseAction,
   setCaseAction,
-  setDocketEntryFormForEditAction,
+  setBaseUrlAction,
+  setDocketEntryFormForDocketEditAction,
+  deconstructReceivedAtDateToFormAction,
   setDocumentIdAction,
-  set(state.isEditingDocketEntry, true),
   set(state.wizardStep, 'PrimaryDocumentForm'),
-  set(state.documentUploadMode, 'scan'),
-  set(state.documentSelectedForScan, 'primaryDocumentFile'),
   setCurrentPageAction('EditDocketEntry'),
 ];
 
