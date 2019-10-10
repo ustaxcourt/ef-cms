c154ade40e21a3618ca7907a440dbabc602abc02
Matt Hopson <matthopson@gmail.com>
2019-10-10 13:53:27 -0600

    DRY up customHandle

--- web-api/src/courtIssuedOrder/createCourtIssuedOrderPdfFromHtmlLambda.js
+++ web-api/src/courtIssuedOrder/createCourtIssuedOrderPdfFromHtmlLambda.js
@@ -1,44 +1,6 @@
+const { customHandle } = require('../customHandle');
 const createApplicationContext = require('../applicationContext');
-const {
-  headers,
-  sendError,
-  sendOk,
-} = require('../middleware/apiGatewayHelper');
-const {
-  NotFoundError,
-  UnauthorizedError,
-} = require('../../../shared/src/errors/errors');
 const { getUserFromAuthHeader } = require('../middleware/apiGatewayHelper');
-
-const customHandle = async (event, fun) => {
-  if (event.source === 'serverless-plugin-warmup') {
-    return sendOk('Lambda is warm!');
-  }
-  try {
-    const pdfBuffer = await fun();
-    return {
-      body: pdfBuffer.toString('base64'),
-      headers: {
-        ...headers,
-        'Content-Type': 'application/pdf',
-        'accept-ranges': 'bytes',
-      },
-      isBase64Encoded: true,
-      statusCode: 200,
-    };
-  } catch (err) {
-    console.error('err', err);
-    if (err instanceof NotFoundError) {
-      err.statusCode = 404;
-      return sendError(err);
-    } else if (err instanceof UnauthorizedError) {
-      err.statusCode = 403;
-      return sendError(err);
-    } else {
-      return sendError(err);
-    }
-  }
-};
 
 /**
  * create court issued order pdf from html
