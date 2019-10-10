9b4ee397f2efa958ed18701f58ffd7eae65fef71
Joseph Sutton <jsutton@flexion.us>
2019-10-10 10:18:02 -0500

    added layer check for deployment script

--- check-deploy-stack.sh
+++ check-deploy-stack.sh
@@ -6,12 +6,6 @@ COMPARE_BRANCH=$3   # ex: develop, staging
 ENV=$4              # ex: exp, stg, dev
 REGION=$5           # ex: us-east-1, us-west-1
 
-echo "stack name ${1} ${STACK_NAME}"
-echo "stack pattern ${2} ${STACK_PATTERN}"
-echo "compare branch ${3} ${COMPARE_BRANCH}"
-echo "env ${4} ${ENV}"
-echo "region ${5} ${REGION}"
-
 REDEPLOY_VIA_DEPS=$(git --no-pager diff --name-only origin/${COMPARE_BRANCH} | node ./web-api/dependency-tree-check.js ${STACK_NAME})
 REDEPLOY_VIA_SLS=$(git --no-pager diff --name-only origin/${COMPARE_BRANCH} | grep -c "${STACK_PATTERN}" || test $? = 1)
 
