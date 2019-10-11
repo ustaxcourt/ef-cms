4398cea6ed4855149393e9fbcde06b4b908f70dc
Matt Hopson <matthopson@gmail.com>
2019-10-11 14:29:06 -0600

    Bug fix

--- shared/src/business/useCases/caseAssociation/deleteCounselFromCaseInteractor.js
+++ shared/src/business/useCases/caseAssociation/deleteCounselFromCaseInteractor.js
@@ -10,7 +10,7 @@ const { UnauthorizedError } = require('../../../errors/errors');
  *
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
- * @param {string} providers.caseId the id of the case the case note is attached to
+ * @param {string} providers.caseId the id of the case the user is attached to
  * @param {string} providers.userIdToDelete the id of the user to be removed from the case
  * @returns {Promise} the promise of the delete call
  */
