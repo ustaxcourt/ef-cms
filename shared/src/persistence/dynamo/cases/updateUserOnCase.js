9f773b5c658f09426eaf4f6a664783f3c3754281
Matt Hopson <matthopson@gmail.com>
2019-10-11 12:11:02 -0600

    Add jsdoc

--- shared/src/persistence/dynamo/cases/updateUserOnCase.js
+++ shared/src/persistence/dynamo/cases/updateUserOnCase.js
@@ -1,5 +1,14 @@
 const client = require('../../dynamodbClientService');
 
+/**
+ * updateUserOnCase
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context
+ * @param {string} providers.caseId the id of the case to update the mapping records for
+ * @param {string} providers.userToUpdate the user record to update
+ * @returns {Promise} the return from the persistence put call
+ */
 exports.updateUserOnCase = async ({
   applicationContext,
   caseId,
