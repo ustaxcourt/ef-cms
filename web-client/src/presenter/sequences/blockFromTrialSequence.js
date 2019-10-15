3a683e6ab884c3c2cefb0218adccdb23bb255cbf
Cody Seibert <cseibert113@gmail.com>
2019-10-15 17:46:07 -0400

    moving actions to casedetail dir

--- web-client/src/presenter/sequences/blockFromTrialSequence.js
+++ web-client/src/presenter/sequences/blockFromTrialSequence.js
@@ -1,11 +1,11 @@
-import { blockFromTrialAction } from '../actions/blockFromTrialAction';
+import { blockFromTrialAction } from '../actions/CaseDetail/blockFromTrialAction';
 import { clearModalAction } from '../actions/clearModalAction';
 import { clearModalStateAction } from '../actions/clearModalStateAction';
 import { setAlertSuccessAction } from '../actions/setAlertSuccessAction';
 import { setCaseAction } from '../actions/setCaseAction';
 import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
 import { startShowValidationAction } from '../actions/startShowValidationAction';
-import { validateBlockFromTrialAction } from '../actions/validateBlockFromTrialAction';
+import { validateBlockFromTrialAction } from '../actions/CaseDetail/validateBlockFromTrialAction';
 
 export const blockFromTrialSequence = [
   startShowValidationAction,
--- web-client/src/presenter/sequences/validateBlockFromTrialSequence.js
+++ web-client/src/presenter/sequences/validateBlockFromTrialSequence.js
@@ -1,7 +1,7 @@
 import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
 import { shouldValidateAction } from '../actions/shouldValidateAction';
-import { validateBlockFromTrialAction } from '../actions/validateBlockFromTrialAction';
+import { validateBlockFromTrialAction } from '../actions/CaseDetail/validateBlockFromTrialAction';
 
 export const validateBlockFromTrialSequence = [
   shouldValidateAction,
