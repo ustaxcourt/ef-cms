b4cbce4f517cfb5c0affa8f40f1650cc31dd702b
William Bagayoko <wbagayoko@gmail.com>
2019-10-15 02:46:09 -0500

    991 update navigation of complete

--- web-client/src/presenter/sequences/completeDocketEntryQCSequence.js
+++ web-client/src/presenter/sequences/completeDocketEntryQCSequence.js
@@ -1,11 +1,15 @@
 import { completeDocketEntryQCAction } from '../actions/EditDocketRecord/completeDocketEntryQCAction';
 import { computeCertificateOfServiceFormDateAction } from '../actions/FileDocument/computeCertificateOfServiceFormDateAction';
 import { computeDateReceivedAction } from '../actions/DocketEntry/computeDateReceivedAction';
+import { navigateToCaseDetailAction } from '../actions/navigateToCaseDetailAction';
 import { setCaseAction } from '../actions/setCaseAction';
+import { setCurrentPageAction } from '../actions/setCurrentPageAction';
 
 export const completeDocketEntryQCSequence = [
+  setCurrentPageAction('Interstitial'),
   computeDateReceivedAction,
   computeCertificateOfServiceFormDateAction,
   completeDocketEntryQCAction,
   setCaseAction,
+  navigateToCaseDetailAction,
 ];
