ee96761f3b25573c6b99c4357af1b99b1f73b67f
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-10-16 11:32:11 -0700

    1188: sort cases by docket number in blocked cases report table

--- web-client/src/presenter/computeds/blockedCasesReportHelper.test.js
+++ web-client/src/presenter/computeds/blockedCasesReportHelper.test.js
@@ -26,13 +26,17 @@ describe('blockedCasesReportHelper', () => {
   it('returns blockedCasesCount as the length of the blockedCases array', () => {
     const result = runCompute(blockedCasesReportHelper, {
       state: {
-        blockedCases: [{ caseId: '1' }, { caseId: '2' }, { caseId: '3' }],
+        blockedCases: [
+          { caseId: '1', docketNumber: '101-19' },
+          { caseId: '2', docketNumber: '102-19' },
+          { caseId: '3', docketNumber: '103-19' },
+        ],
       },
     });
     expect(result).toMatchObject({ blockedCasesCount: 3 });
   });
 
-  it('formats blocked cases with caseName, docketNumberWithSuffix, and blockedDateFormatted', () => {
+  it('formats blocked cases with caseName, docketNumberWithSuffix, and blockedDateFormatted and sorts by docket number', () => {
     const result = runCompute(blockedCasesReportHelper, {
       state: {
         blockedCases: [
@@ -40,7 +44,7 @@ describe('blockedCasesReportHelper', () => {
             blockedDate: '2019-03-01T21:42:29.073Z',
             caseCaption: 'Brett Osborne, Petitioner',
             caseId: '1',
-            docketNumber: '101-19',
+            docketNumber: '105-19',
           },
           {
             blockedDate: '2019-07-01T21:42:29.073Z',
@@ -53,7 +57,7 @@ describe('blockedCasesReportHelper', () => {
             caseCaption:
               'Tatum Craig, Wayne Obrien, Partnership Representative, Petitioner(s)',
             caseId: '3',
-            docketNumber: '103-19',
+            docketNumber: '103-18',
             docketNumberSuffix: 'S',
           },
         ],
@@ -63,13 +67,15 @@ describe('blockedCasesReportHelper', () => {
       blockedCasesCount: 3,
       blockedCasesFormatted: [
         {
-          blockedDate: '2019-03-01T21:42:29.073Z',
-          blockedDateFormatted: '03/01/19',
-          caseCaption: 'Brett Osborne, Petitioner',
-          caseId: '1',
-          caseName: 'Brett Osborne',
-          docketNumber: '101-19',
-          docketNumberWithSuffix: '101-19',
+          blockedDate: '2018-03-05T21:42:29.073Z',
+          blockedDateFormatted: '03/05/18',
+          caseCaption:
+            'Tatum Craig, Wayne Obrien, Partnership Representative, Petitioner(s)',
+          caseId: '3',
+          caseName: 'Tatum Craig, Wayne Obrien, Partnership Representative',
+          docketNumber: '103-18',
+          docketNumberSuffix: 'S',
+          docketNumberWithSuffix: '103-18S',
         },
         {
           blockedDate: '2019-07-01T21:42:29.073Z',
@@ -81,15 +87,13 @@ describe('blockedCasesReportHelper', () => {
           docketNumberWithSuffix: '102-19',
         },
         {
-          blockedDate: '2018-03-05T21:42:29.073Z',
-          blockedDateFormatted: '03/05/18',
-          caseCaption:
-            'Tatum Craig, Wayne Obrien, Partnership Representative, Petitioner(s)',
-          caseId: '3',
-          caseName: 'Tatum Craig, Wayne Obrien, Partnership Representative',
-          docketNumber: '103-19',
-          docketNumberSuffix: 'S',
-          docketNumberWithSuffix: '103-19S',
+          blockedDate: '2019-03-01T21:42:29.073Z',
+          blockedDateFormatted: '03/01/19',
+          caseCaption: 'Brett Osborne, Petitioner',
+          caseId: '1',
+          caseName: 'Brett Osborne',
+          docketNumber: '105-19',
+          docketNumberWithSuffix: '105-19',
         },
       ],
     });
