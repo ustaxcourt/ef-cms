09787ad6f42f202e681d2c61fd814a0d0814b6b5
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-10-17 11:33:12 -0700

    re-create eligible for trial mapping record when case is removed from trial

--- shared/src/business/useCases/trialSessions/removeCaseFromTrialInteractor.js
+++ shared/src/business/useCases/trialSessions/removeCaseFromTrialInteractor.js
@@ -7,11 +7,13 @@ const { Case } = require('../../entities/cases/Case');
 const { TrialSession } = require('../../entities/trialSessions/TrialSession');
 
 /**
- * getCalendaredCasesForTrialSessionInteractor
+ * removeCaseFromTrialInteractor
  *
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
- * @param {string} providers.trialSessionId the id of the trial session to get the calendared cases
+ * @param {string} providers.caseId the id of the case to remove from trial
+ * @param {string} providers.disposition the reason the case is being removed from trial
+ * @param {string} providers.trialSessionId the id of the trial session containing the case to set to removedFromTrial
  * @returns {Promise} the promise of the getCalendaredCasesForTrialSession call
  */
 exports.removeCaseFromTrialInteractor = async ({
@@ -51,6 +53,14 @@ exports.removeCaseFromTrialInteractor = async ({
   const caseEntity = new Case(myCase, { applicationContext });
 
   caseEntity.removeFromTrial();
+
+  await applicationContext
+    .getPersistenceGateway()
+    .createCaseTrialSortMappingRecords({
+      applicationContext,
+      caseId: caseEntity.caseId,
+      caseSortTags: caseEntity.generateTrialSortTags(),
+    });
 
   return await applicationContext.getPersistenceGateway().updateCase({
     applicationContext,
