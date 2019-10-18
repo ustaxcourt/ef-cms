33fa5695be64823372536fc06494714753405ea6
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-10-18 11:37:37 -0700

    test coverage

---
+++ web-client/src/presenter/actions/CaseDetail/validateAddToTrialSessionAction.test.js
@@ -0,0 +1,51 @@
+import { presenter } from '../../presenter';
+import { runAction } from 'cerebral/test';
+import { validateAddToTrialSessionAction } from './validateAddToTrialSessionAction';
+
+describe('validateAddToTrialSessionAction', () => {
+  let successStub;
+  let errorStub;
+
+  beforeEach(() => {
+    successStub = jest.fn();
+    errorStub = jest.fn();
+
+    presenter.providers.path = {
+      error: errorStub,
+      success: successStub,
+    };
+  });
+
+  it('should call path.success and not path.error if trialSessionId is on state.modal', async () => {
+    await runAction(validateAddToTrialSessionAction, {
+      modules: {
+        presenter,
+      },
+      state: {
+        modal: { trialSessionId: '123' },
+      },
+    });
+
+    expect(successStub).toHaveBeenCalled();
+    expect(errorStub).not.toHaveBeenCalled();
+  });
+
+  it('should call path.error with and error message and not call path.success if trialSessionId is not on state.modal', async () => {
+    await runAction(validateAddToTrialSessionAction, {
+      modules: {
+        presenter,
+      },
+      state: {
+        modal: {},
+      },
+    });
+
+    expect(errorStub).toHaveBeenCalled();
+    expect(errorStub.mock.calls[0][0]).toEqual({
+      errors: {
+        trialSessionId: 'Select a Trial Session',
+      },
+    });
+    expect(successStub).not.toHaveBeenCalled();
+  });
+});
