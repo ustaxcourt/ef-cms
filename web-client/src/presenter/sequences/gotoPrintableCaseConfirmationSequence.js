f11308356e7ae7cc196a254f64c71a55a1241635
William Bagayoko <wbagayoko@gmail.com>
2019-10-22 08:11:22 -0500

    653 goto route sequence

---
+++ web-client/src/presenter/sequences/gotoPrintableCaseConfirmationSequence.js
@@ -0,0 +1,16 @@
+import { generateCaseConfirmationPdfUrlAction } from '../actions/CaseConfirmation/generateCaseConfirmationPdfUrlAction';
+import { getCaseAction } from '../actions/getCaseAction';
+import { set } from 'cerebral/factories';
+import { setCaseAction } from '../actions/setCaseAction';
+import { setWaitingForResponseAction } from '../actions/setWaitingForResponseAction';
+import { state } from 'cerebral';
+import { unsetWaitingForResponseAction } from '../actions/unsetWaitingForResponseAction';
+
+export const gotoPrintableCaseConfirmationSequence = [
+  setWaitingForResponseAction,
+  getCaseAction,
+  setCaseAction,
+  generateCaseConfirmationPdfUrlAction,
+  set(state.currentPage, 'PrintableDocketRecord'),
+  unsetWaitingForResponseAction,
+];
