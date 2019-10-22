e0a897f740ce9c7bc5938041fd9381da41473828
Cody Seibert <cseibert113@gmail.com>
2019-10-22 13:46:51 -0400

    fixing seed data docket number

--- web-client/pa11y/pa11y-petitionsclerk.js
+++ web-client/pa11y/pa11y-petitionsclerk.js
@@ -248,7 +248,7 @@ module.exports = [
       'http://localhost:1234/mock-login?token=petitionsclerk&path=/case-detail/104-19&info=add-case-to-session-modal',
   },
 
-  //this url probably needs to be moved to calendaring when those users are created
+  // this url probably needs to be moved to calendaring when those users are created
   'http://localhost:1234/mock-login?token=petitionsclerk&path=/trial-session-detail/5b18af9e-4fbd-459b-8db7-7b15108c7fa5&info=non-calendared-case',
   'http://localhost:1234/mock-login?token=petitionsclerk&path=/reports/case-deadlines',
 ];
