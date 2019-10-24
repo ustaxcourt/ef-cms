ace87464dc4f04ec377a5dbf0d3388494a40852c
William Bagayoko <wbagayoko@gmail.com>
2019-10-24 15:11:13 -0500

    653 Update tests

--- web-client/src/presenter/actions/getCreateCaseAlertSuccessAction.js
+++ web-client/src/presenter/actions/getCreateCaseAlertSuccessAction.js
@@ -6,10 +6,11 @@
  * @returns {object} the alertSuccess object with default strings
  */
 export const getCreateCaseAlertSuccessAction = ({ props }) => {
+  const { docketNumber } = props.caseDetail;
   return {
     alertSuccess: {
       linkText: 'Print receipt.',
-      linkUrl: `/case-detail/${props.docketNumber}/confirmation`,
+      linkUrl: `/case-detail/${docketNumber}/confirmation`,
       message: 'You can access your case at any time from the case list below.',
       title: 'Your petition has been successfully submitted.',
     },
