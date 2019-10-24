d565d3ef023e107387159a4bbddfeef3ea104695
Matt Hopson <matthopson@gmail.com>
2019-10-24 14:43:07 -0600

    Add test and jsdocs for getUserPermissions

--- shared/src/authorization/getUserPermissions.js
+++ shared/src/authorization/getUserPermissions.js
@@ -1,5 +1,11 @@
 import { ROLE_PERMISSIONS, isAuthorized } from './authorizationClientService';
 
+/**
+ * Returns permissions based on the given user
+ *
+ * @param {object} user the user to check for authorization
+ * @returns {object} ROLE_PERMISSIONS keys with boolean value based on user role
+ */
 export const getUserPermissions = user => {
   if (user) {
     const permissions = {};
