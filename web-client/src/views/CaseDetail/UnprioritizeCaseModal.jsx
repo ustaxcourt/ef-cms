716665b5a4c7593be6568d87153bcf144d31ebc4
Cody Seibert <cseibert113@gmail.com>
2019-10-24 14:29:56 -0400

    refactoring all modals

--- web-client/src/views/CaseDetail/UnprioritizeCaseModal.jsx
+++ web-client/src/views/CaseDetail/UnprioritizeCaseModal.jsx
@@ -3,32 +3,27 @@ import { connect } from '@cerebral/react';
 import { sequences } from 'cerebral';
 import React from 'react';
 
-class UnprioritizeCaseModalComponent extends ModalDialog {
-  constructor(props) {
-    super(props);
-    this.modal = {
-      cancelLabel: 'No, cancel',
-      classNames: '',
-      confirmLabel: 'Yes, remove high priority',
-      title: 'Are You Sure You Want to Remove the High Priority on This Case?',
-    };
-  }
-
-  renderBody() {
-    return (
-      <div className="margin-bottom-4" id="unprioritize-modal">
-        <div className="margin-bottom-2">
-          This case will be set for trial according to standard priority.{' '}
-        </div>
-      </div>
-    );
-  }
-}
-
 export const UnprioritizeCaseModal = connect(
   {
     cancelSequence: sequences.clearModalSequence,
     confirmSequence: sequences.unprioritizeCaseSequence,
   },
-  UnprioritizeCaseModalComponent,
+  ({ cancelSequence, confirmSequence }) => {
+    return (
+      <ModalDialog
+        cancelLabel="No, cancel"
+        cancelSequence={cancelSequence}
+        className=""
+        confirmLabel="Yes, remove high priority"
+        confirmSequence={confirmSequence}
+        title="Are You Sure You Want to Remove the High Priority on This Case?"
+      >
+        <div className="margin-bottom-4" id="unprioritize-modal">
+          <div className="margin-bottom-2">
+            This case will be set for trial according to standard priority.{' '}
+          </div>
+        </div>
+      </ModalDialog>
+    );
+  },
 );
