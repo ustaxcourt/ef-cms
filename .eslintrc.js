c8e559ad58684a82b3871f609a8cab429f737de4
Eric Sorenson <eric@sorenson.software>
2019-10-24 11:33:27 -0500

    fixed manual merge error

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,14 +18,15 @@ module.exports = {
     'security',
     'sort-destructure-keys',
     'sort-keys-fix',
+    'spellcheck',
   ],
   rules: {
     'no-prototype-builtins': 0,
+    'react/prop-types': 0,
     'require-atomic-updates': 0,
     'security/detect-child-process': 0,
     'security/detect-non-literal-fs-filename': 0,
     'security/detect-object-injection': 0,
-    'react/prop-types': 0,
     'react/jsx-sort-props': [
       'error',
       {
