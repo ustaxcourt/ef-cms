26b962ac8041d13c5d13d6ffb9cd0797f92f72e5
Cody Seibert <cseibert113@gmail.com>
2019-10-25 12:10:20 -0400

    refactoring various permissions

--- web-client/src/views/WorkQueue/WorkQueueActionButtons.jsx
+++ web-client/src/views/WorkQueue/WorkQueueActionButtons.jsx
@@ -1,5 +1,4 @@
 import { Button } from '../../ustc-ui/Button/Button';
-import { If } from '../../ustc-ui/If/If';
 import { connect } from '@cerebral/react';
 import { sequences, state } from 'cerebral';
 import React from 'react';
@@ -21,11 +20,11 @@ export const WorkQueueActionButtons = connect(
             >
               Create a Case
             </Button>
-            <If bind="workQueueHelper.showRunBatchIRSProcessButton">
+            {workQueueHelper.showRunBatchIRSProcessButton && (
               <Button secondary onClick={() => runBatchProcessSequence()}>
                 Run IRS Batch Process
               </Button>
-            </If>
+            )}
           </div>
         )}
       </React.Fragment>
