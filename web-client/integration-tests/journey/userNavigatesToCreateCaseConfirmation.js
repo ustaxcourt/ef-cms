7533ba179e280b06c972fd7f82f4bc62b9ca12c8
William Bagayoko <wbagayoko@gmail.com>
2019-10-30 14:23:47 -0500

    653 E2E

--- web-client/integration-tests/journey/userNavigatesToCreateCaseConfirmation.js
+++ web-client/integration-tests/journey/userNavigatesToCreateCaseConfirmation.js
@@ -1,7 +1,7 @@
 export default test => {
   it('user sees the case confirmation pdf', async () => {
     await test.runSequence('gotoPrintableCaseConfirmationSequence', {
-      docketNumber: props.docketNumber,
+      docketNumber: test.docketNumber,
     });
     const pdfPreviewUrl = test.getState('pdfPreviewUrl');
     expect(pdfPreviewUrl).not.toBeNull;
