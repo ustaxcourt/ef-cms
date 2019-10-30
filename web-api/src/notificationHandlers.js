924a54d08b08673781843d61c383a33258f13578
Eric Sorenson <eric@sorenson.software>
2019-10-30 10:56:55 -0500

    temporary fix for fs readFile

--- web-api/src/notificationHandlers.js
+++ web-api/src/notificationHandlers.js
@@ -1,6 +1,3 @@
-require('core-js/stable');
-require('regenerator-runtime/runtime');
-
 module.exports = {
   connectLambda: require('./notifications/connectLambda').handler,
   disconnectLambda: require('./notifications/disconnectLambda').handler,
