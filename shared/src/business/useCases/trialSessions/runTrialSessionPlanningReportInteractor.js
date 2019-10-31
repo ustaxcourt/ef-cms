20e35c5bc845a66d94e2cfc7c272fd92a547e18a
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-10-31 15:17:46 -0700

    1190: endpoint for generating the planning report (wip) [skip ci]

---
+++ shared/src/business/useCases/trialSessions/runTrialSessionPlanningReportInteractor.js
@@ -0,0 +1,130 @@
+const {
+  isAuthorized,
+  ROLE_PERMISSIONS,
+} = require('../../../authorization/authorizationClientService');
+const { ContactFactory } = require('../../entities/contacts/ContactFactory');
+const { invert } = require('lodash');
+const { TrialSession } = require('../../entities/trialSessions/TrialSession');
+const { UnauthorizedError } = require('../../../errors/errors');
+
+export const getPreviousTerm = (currentTerm, currentYear) => {
+  const terms = [
+    `winter ${+currentYear - 1}`,
+    `spring ${currentYear}`,
+    `fall ${currentYear}`,
+    `winter ${currentYear}`,
+  ];
+  const termsReversed = terms.reverse();
+  const termI = terms.findIndex(t => `${currentTerm} ${currentYear}` === t);
+  const [term, year] = termsReversed[termI + 1].split(' ');
+  return {
+    term,
+    year,
+  };
+};
+
+/**
+ * runTrialSessionPlanningReportInteractor
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context
+ * @returns {Promise} the promise of the runTrialSessionPlanningReportInteractor call
+ */
+exports.runTrialSessionPlanningReportInteractor = async ({
+  applicationContext,
+  term,
+  year,
+}) => {
+  const user = applicationContext.getCurrentUser();
+
+  if (!isAuthorized(user, ROLE_PERMISSIONS.TRIAL_SESSIONS)) {
+    throw new UnauthorizedError('Unauthorized');
+  }
+
+  const previousTerms = [];
+  let currentTerm = term;
+  let currentYear = year;
+  for (let i = 0; i < 3; i++) {
+    const previous = getPreviousTerm(currentTerm, currentYear);
+    previousTerms.push(previous);
+    currentTerm = previous.term;
+    currentYear = previous.year;
+  }
+
+  const trialCities = TrialSession.TRIAL_CITIES.ALL;
+  trialCities.sort((a, b) => {
+    if (a.state === b.state) {
+      return a.city.localeCompare(b.city);
+    } else {
+      return a.state.localeCompare(b.state);
+    }
+  });
+
+  const allTrialSessions = await applicationContext
+    .getPersistenceGateway()
+    .getTrialSessions({ applicationContext });
+
+  const trialLocationData = [];
+  for (const trialLocation of trialCities) {
+    const trialCityState = `${trialLocation.city}, ${trialLocation.state}`;
+    const trialCityStateStripped = trialCityState.replace(/[\s.,]/g, '');
+    const stateAbbreviation = invert(ContactFactory.US_STATES)[
+      trialLocation.state
+    ];
+
+    const eligibleCasesSmall = await applicationContext
+      .getPersistenceGateway()
+      .getEligibleCasesForTrialCity({
+        applicationContext,
+        procedureType: 'Small',
+        trialCity: trialCityStateStripped,
+      });
+    const eligibleCasesRegular = await applicationContext
+      .getPersistenceGateway()
+      .getEligibleCasesForTrialCity({
+        applicationContext,
+        procedureType: 'Regular',
+        trialCity: trialCityStateStripped,
+      });
+
+    const smallCaseCount = eligibleCasesSmall.length;
+    const regularCaseCount = eligibleCasesRegular.length;
+    const allCaseCount = smallCaseCount + regularCaseCount;
+
+    const previousTermsData = [];
+    previousTerms.forEach(previousTerm => {
+      const previousTermSession = allTrialSessions.find(
+        trialSession =>
+          trialSession.term === previousTerm.term &&
+          trialSession.termYear === previousTerm.year &&
+          trialSession.trialLocation === trialCityState,
+      );
+
+      if (
+        previousTermSession &&
+        previousTermSession.sessionType &&
+        previousTermSession.judge
+      ) {
+        const sessionTypeChar = previousTermSession.sessionType.charAt(0);
+        const strippedJudgeName = previousTermSession.judge.name.replace(
+          'Judge ',
+          '',
+        );
+        previousTermsData.push(`(${sessionTypeChar}) ${strippedJudgeName}`);
+      } else {
+        previousTermsData.push('');
+      }
+    });
+
+    trialLocationData.push({
+      allCaseCount: allCaseCount,
+      previousTermsData,
+      regularCaseCount: regularCaseCount,
+      smallCaseCount: smallCaseCount,
+      stateAbbreviation,
+      trialCityState,
+    });
+  }
+
+  return { previousTerms, trialLocationData };
+};
---
+++ shared/src/business/useCases/trialSessions/runTrialSessionPlanningReportInteractor.test.js
@@ -0,0 +1,103 @@
+const sinon = require('sinon');
+const {
+  getPreviousTerm,
+  runTrialSessionPlanningReportInteractor,
+} = require('./runTrialSessionPlanningReportInteractor');
+const { TrialSession } = require('../../entities/trialSessions/TrialSession');
+const { User } = require('../../entities/User');
+
+describe('run trial session planning report', () => {
+  let applicationContext;
+
+  it('throws error if user is unauthorized', async () => {
+    applicationContext = {
+      environment: { stage: 'local' },
+      getCurrentUser: () => {
+        return {
+          role: User.ROLES.petitioner,
+          userId: 'petitioner',
+        };
+      },
+      getPersistenceGateway: () => {
+        return {
+          getEligibleCasesForTrialCity: sinon.stub().returns([]),
+        };
+      },
+    };
+    await expect(
+      runTrialSessionPlanningReportInteractor({
+        applicationContext,
+        term: 'winter',
+        year: '2020',
+      }),
+    ).rejects.toThrow();
+  });
+
+  it('returns previous terms and the trial locations and case counts', async () => {
+    const getEligibleCasesForTrialCityStub = sinon
+      .stub()
+      .returns([{ caseId: '123' }, { caseId: '123' }]);
+    const getTrialSessionsStub = sinon.stub().returns([
+      {
+        judge: { name: 'Judge Armen' },
+        sessionType: 'Regular',
+        term: 'spring',
+        termYear: '2020',
+        trialLocation: 'Birmingham, Alabama',
+        trialSessionId: '123',
+      },
+    ]);
+    applicationContext = {
+      environment: { stage: 'local' },
+      getCurrentUser: () => {
+        return {
+          role: User.ROLES.petitionsClerk,
+          userId: 'petitionsClerk',
+        };
+      },
+      getPersistenceGateway: () => {
+        return {
+          getEligibleCasesForTrialCity: getEligibleCasesForTrialCityStub,
+          getTrialSessions: getTrialSessionsStub,
+        };
+      },
+    };
+    const results = await runTrialSessionPlanningReportInteractor({
+      applicationContext,
+      term: 'winter',
+      year: '2020',
+    });
+
+    expect(results.previousTerms).toMatchObject([
+      { term: 'fall', year: '2020' },
+      { term: 'spring', year: '2020' },
+      { term: 'winter', year: '2019' },
+    ]);
+    expect(results.trialLocationData.length).toEqual(
+      TrialSession.TRIAL_CITIES.ALL.length,
+    );
+    expect(results.trialLocationData[0]).toMatchObject({
+      allCaseCount: 4,
+      previousTermsData: ['', '(R) Armen', ''],
+      regularCaseCount: 2,
+      smallCaseCount: 2,
+      stateAbbreviation: 'AL',
+      trialCityState: 'Birmingham, Alabama',
+    });
+  });
+
+  describe('get previous term', () => {
+    it('returns previous term and same year if the previous term is winter', () => {
+      const result = getPreviousTerm('winter', '2020');
+      expect(result).toEqual({ term: 'fall', year: '2020' });
+    });
+    it('returns previous term and same year if the previous term is fall', () => {
+      const result = getPreviousTerm('fall', '2020');
+      expect(result).toEqual({ term: 'spring', year: '2020' });
+    });
+    it('returns previous term and previous year if the previous term is spring', () => {
+      const result = getPreviousTerm('spring', '2020');
+      expect(result).toEqual({ term: 'winter', year: '2019' });
+    });
+  });
+});
