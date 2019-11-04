23a5ef4f73ffd7cc4e87002c122522cb6e7f9a4e
Joseph Sutton <jsutton@flexion.us>
2019-11-04 09:19:44 -0600

    fixed syntax error

--- web-client/src/presenter/sequences/submitCompleteSequence.js
+++ web-client/src/presenter/sequences/submitCompleteSequence.js
@@ -13,7 +13,7 @@ export const submitCompleteSequence = [
   completeWorkItemAction,
   clearCompleteFormAction,
   unsetShowForwardInputs,
-  navigateToMessagesAction
+  navigateToMessagesAction,
   setAlertSuccessAction,
   unsetWaitingForResponseAction,
 ];
