c646d3f2820c626040035af70d564b4a6dafa186
Joseph Sutton <jsutton@flexion.us>
2019-11-06 08:50:22 -0600

    added runtime for the clamav lambda layer and serverless configs for clamav

---
+++ web-api/run-serverless-clamav.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e	
+
+slsStage=$1	
+region=$2	
+layer="serverless-clamav.yml"	
+
+tar -xvzf web-api/runtimes/clamav/clamav_lambda_layer.tar.gz -C web-api/runtimes/clamav	
+
+SLS_DEPLOYMENT_BUCKET="${EFCMS_DOMAIN}.efcms.${slsStage}.${region}.deploys"	
+SLS_DEPLOYMENT_BUCKET="${SLS_DEPLOYMENT_BUCKET}" ./node_modules/.bin/sls deploy --config "web-api/${layer}" --stage "${slsStage}" --region "${region}" --verbose
