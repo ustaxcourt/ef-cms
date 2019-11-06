9ac7e44f9d05e2793a1d250b712eaec493296ccf
Matt Hopson <matthopson@gmail.com>
2019-11-06 10:54:23 -0700

    Update edit case caption modal

--- web-client/src/views/CaseDetailEdit/UpdateCaseCaptionModalDialog.jsx
+++ web-client/src/views/CaseDetailEdit/UpdateCaseCaptionModalDialog.jsx
@@ -8,13 +8,15 @@ export const UpdateCaseCaptionModalDialog = connect(
     cancelSequence: sequences.dismissCaseCaptionModalSequence,
     caseCaption: state.caseCaption,
     confirmSequence: sequences.updateCaseDetailSequence,
-    setCaseCaptionSequence: sequences.setCaseCaptionSequence,
+    constants: state.constants,
+    updateModalValueSequence: sequences.updateModalValueSequence,
   },
   ({
     cancelSequence,
     caseCaption,
     confirmSequence,
-    setCaseCaptionSequence,
+    constants,
+    updateModalValueSequence,
   }) => {
     return (
       <ModalDialog
@@ -23,7 +25,7 @@ export const UpdateCaseCaptionModalDialog = connect(
         className=""
         confirmLabel="Save"
         confirmSequence={confirmSequence}
-        title="Edit Case Caption"
+        title="Edit Case"
       >
         <div className="margin-bottom-2">
           <label className="usa-label" htmlFor="caption">
@@ -34,11 +36,37 @@ export const UpdateCaseCaptionModalDialog = connect(
             className="caption usa-textarea"
             defaultValue={caseCaption}
             id="caption"
+            name="caseCaption"
             onChange={e =>
-              setCaseCaptionSequence({ caseCaption: e.target.value })
+              updateModalValueSequence({
+                key: e.target.name,
+                value: e.target.value,
+              })
             }
           />
-          v. Commissioner of Internal Revenue, Respondent
+        </div>
+        <div className="margin-bottom-4">
+          <label className="usa-label" htmlFor="caseStatusType">
+            Case Status
+          </label>
+          <select
+            className="case-status usa-select"
+            id="caseStatusType"
+            name="caseStatusType"
+            onChange={e => {
+              updateModalValueSequence({
+                key: e.target.name,
+                value: e.target.value,
+              });
+            }}
+          >
+            <option value="">- Select -</option>
+            {Object.keys(constants.STATUS_TYPES).map(key => (
+              <option key={key} value={constants.STATUS_TYPES[key]}>
+                {constants.STATUS_TYPES[key]}
+              </option>
+            ))}
+          </select>
         </div>
       </ModalDialog>
     );
