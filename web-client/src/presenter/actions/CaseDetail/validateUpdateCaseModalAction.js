025f6b2dcf54c718df54f14f53d022c2dbfed966
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-11-07 10:41:38 -0800

    update jsdoc

--- web-client/src/presenter/actions/CaseDetail/validateUpdateCaseModalAction.js
+++ web-client/src/presenter/actions/CaseDetail/validateUpdateCaseModalAction.js
@@ -2,9 +2,10 @@ import { isEmpty } from 'lodash';
 import { state } from 'cerebral';
 
 /**
- * validate the block from trial reason
+ * validate the update case modal
  *
  * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context
  * @param {Function} providers.get the cerebral get function used for getting state.modal
  * @param {object} providers.path the cerebral path which contains the next path in the sequence (path of success or error)
  * @returns {object} the next path based on if validation was successful or error
