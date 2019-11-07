841d5eb271ebd0c46e2edaef17bcdb3ae28097d0
William Bagayoko <wbagayoko@gmail.com>
2019-11-07 08:11:37 -0600

    2156 fetchPendingItemsInteractor
    use elasticsearch

---
+++ shared/src/business/useCases/pendingItems/fetchPendingItemsInteractor.js
@@ -0,0 +1,66 @@
+const AWS = require('aws-sdk');
+const {
+  isAuthorized,
+  ROLE_PERMISSIONS,
+} = require('../../../authorization/authorizationClientService');
+const { get } = require('lodash');
+const { UnauthorizedError } = require('../../../errors/errors');
+
+/**
+ * fetchPendingItemsInteractor
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context
+ * @param {string} providers.judge the optional judge filter
+ * @returns {object} the case data
+ */
+exports.fetchPendingItemsInteractor = async ({ applicationContext, judge }) => {
+  const authorizedUser = applicationContext.getCurrentUser();
+
+  if (!isAuthorized(authorizedUser, ROLE_PERMISSIONS.PENDING_ITEMS)) {
+    throw new UnauthorizedError('Unauthorized');
+  }
+
+  const searchParameters = {
+    body: {
+      _source: [
+        'blocked',
+        'blockedDate',
+        'blockedReason',
+        'caseCaption',
+        'docketNumber',
+        'docketNumberSuffix',
+        'status',
+      ],
+      query: {
+        bool: {
+          must: [{ match: { 'blocked.BOOL': true } }],
+        },
+      },
+      size: 5000,
+    },
+    index: 'efcms',
+  };
+
+  if (judge) {
+    searchParameters.body['_source'].push(['associatedJudge']);
+    searchParameters.body.query.bool.must.push({
+      match: { 'associatedJudge.S': judge },
+    });
+  }
+
+  const body = await applicationContext
+    .getSearchClient()
+    .search(searchParameters);
+
+  const foundCases = [];
+  const hits = get(body, 'hits.hits');
+
+  if (hits && hits.length > 0) {
+    hits.forEach(hit => {
+      foundCases.push(AWS.DynamoDB.Converter.unmarshall(hit['_source']));
+    });
+  }
+
+  return foundCases;
+};
---
+++ shared/src/business/useCases/pendingItems/fetchPendingItemsInteractor.test.js
@@ -0,0 +1,79 @@
+const {
+  fetchPendingItemsInteractor,
+} = require('./fetchPendingItemsInteractor');
+const { User } = require('../../entities/User');
+
+describe('fetchPendingItemsInteractor', () => {
+  let searchSpy;
+
+  const applicationContext = {
+    environment: { stage: 'local' },
+    getCurrentUser: () => {
+      return {
+        role: User.ROLES.petitionsClerk,
+        userId: 'petitionsclerk',
+      };
+    },
+    getSearchClient: () => ({
+      search: searchSpy,
+    }),
+  };
+
+  it('calls search function with correct params and returns records', async () => {
+    searchSpy = jest.fn(async () => {
+      return {
+        hits: {
+          hits: [
+            {
+              _source: {
+                caseId: { S: '1' },
+              },
+            },
+            {
+              _source: {
+                caseId: { S: '2' },
+              },
+            },
+          ],
+        },
+      };
+    });
+
+    const results = await fetchPendingItemsInteractor({
+      applicationContext,
+      judge: 'Judge Armen',
+    });
+
+    expect(searchSpy).toHaveBeenCalled();
+    expect(searchSpy.mock.calls[0][0].body.query.bool.must).toEqual([
+      {
+        match: { 'blocked.BOOL': true },
+      },
+      {
+        match: { 'associatedJudge.S': 'Judge Armen' },
+      },
+    ]);
+    expect(results).toEqual([{ caseId: '1' }, { caseId: '2' }]);
+  });
+
+  it('should throw an unauthorized error if the user does not have access to blocked cases', async () => {
+    applicationContext.getCurrentUser = () => {
+      return {
+        role: User.ROLES.petitioner,
+        userId: 'petitioner',
+      };
+    };
+
+    let error;
+    try {
+      await fetchPendingItemsInteractor({
+        applicationContext,
+        judge: 'Judge Armen',
+      });
+    } catch (err) {
+      error = err;
+    }
+    expect(error).not.toBeNull();
+    expect(error.message).toContain('Unauthorized');
+  });
+});
