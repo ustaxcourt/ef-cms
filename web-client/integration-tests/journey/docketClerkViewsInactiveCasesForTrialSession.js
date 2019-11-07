38a17ea3ed70e56ebfe623510db24f3f03c68c38
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-11-07 15:01:59 -0800

    2377: working on journey test

---
+++ web-client/integration-tests/journey/docketClerkViewsInactiveCasesForTrialSession.js
@@ -0,0 +1,31 @@
+import { Case } from '../../../shared/src/business/entities/cases/Case';
+import { formattedTrialSessionDetails as formattedTrialSessionDetailsComputed } from '../../src/presenter/computeds/formattedTrialSessionDetails';
+import { runCompute } from 'cerebral/test';
+import { withAppContextDecorator } from '../../src/withAppContext';
+
+const formattedTrialSessionDetails = withAppContextDecorator(
+  formattedTrialSessionDetailsComputed,
+);
+
+export default test => {
+  return it('Docket clerk views inactive cases for a trial session', async () => {
+    await test.runSequence('gotoTrialSessionDetailSequence', {
+      trialSessionId: test.trialSessionId,
+    });
+
+    const trialSessionDetailsFormatted = runCompute(
+      withAppContextDecorator(formattedTrialSessionDetails),
+      {
+        state: test.getState(),
+      },
+    );
+
+    expect(trialSessionDetailsFormatted.inactiveCases.length).toEqual(1);
+    expect(trialSessionDetailsFormatted.inactiveCases[0].caseId).toEqual(
+      test.caseId,
+    );
+    expect(trialSessionDetailsFormatted.inactiveCases[0].disposition).toEqual(
+      `Status was changed to ${Case.STATUS_TYPES.submitted}`,
+    );
+  });
+};
