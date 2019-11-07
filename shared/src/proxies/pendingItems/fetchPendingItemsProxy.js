4bae4f1889720e4973b43301c5e364abc1355526
William Bagayoko <wbagayoko@gmail.com>
2019-11-07 07:43:50 -0600

    2156 fetchPendingItemsLambda

--- shared/src/proxies/pendingItems/fetchPendingItemsProxy.js
+++ shared/src/proxies/pendingItems/fetchPendingItemsProxy.js
@@ -1,7 +1,7 @@
 const { get } = require('../requests');
 
 /**
- * fetchPendingItemsInteractorProxy
+ * fetchPendingItemsProxy
  *
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
