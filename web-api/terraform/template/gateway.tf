9f44c6a1d873f921c9f35cb25fa4964d8bb87c0b
Cody Seibert <cseibert113@gmail.com>
2019-11-07 14:49:57 -0500

    refactoring all role and policy creation out of the application and putting into a separate terraform

--- web-api/terraform/template/gateway.tf
+++ web-api/terraform/template/gateway.tf
@@ -1,55 +1,9 @@
 resource "aws_api_gateway_account" "account_us_east_1" {
-  cloudwatch_role_arn = "${aws_iam_role.cloudwatch.arn}"
+  cloudwatch_role_arn = "${var.cloudwatch_role_arn}"
   provider = "aws.us-east-1"
 }
 
 resource "aws_api_gateway_account" "account_us_west_2" {
-  cloudwatch_role_arn = "${aws_iam_role.cloudwatch.arn}"
+  cloudwatch_role_arn = "${var.cloudwatch_role_arn}"
   provider = "aws.us-west-1"
-}
-
-resource "aws_iam_role" "cloudwatch" {
-  name = "api_gateway_cloudwatch_global_${var.environment}"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "apigateway.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "cloudwatch" {
-  name = "cloudwatch_policy_${var.environment}"
-  role = "${aws_iam_role.cloudwatch.id}"
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:DescribeLogGroups",
-                "logs:DescribeLogStreams",
-                "logs:PutLogEvents",
-                "logs:GetLogEvents",
-                "logs:FilterLogEvents"
-            ],
-            "Resource": "*"
-        }
-    ]
-}
-EOF
 }
