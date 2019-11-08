ca5e714b066546d607a6e7fe6022ac477f4db950
Kris Koskelin <kkoskelin@flexion.us>
2019-11-08 15:42:02 -0600

    make a module with handlebars, pug, sass, or png

--- shared/createModule.js
+++ shared/createModule.js
@@ -19,21 +19,22 @@ const createModule = filePath => {
     );
     // maybe should escape them before writing module?
   }
-  const theCode = `// This is a generated file, do not edit\nmodule.exports =\n  '${contents}';\n`;
+  const theCode = `// This is a generated file, do not edit\nmodule.exports =\n  \`${contents}\`;\n`;
   const outputPath = `${filePath}_.js`;
   console.info(outputPath);
   fs.writeFileSync(outputPath, theCode);
 };
 
 const readFile = filePath => {
+  // TODO: deal with mime-types other than image/png
   const isBase64 = BINARY_BASE64.some(extension =>
     filePath.endsWith(extension),
   );
-  return fs.readFileSync(filePath, {
+  const contents = fs.readFileSync(filePath, {
     encoding: isBase64 ? 'base64' : 'utf8',
   });
+  return isBase64 ? `data:image/png;base64,${contents}` : contents;
 };
 
 const files = [...targets, ...process.argv.slice(2)];
 files.forEach(createModule);
-// files.forEach(f => console.log(f));
