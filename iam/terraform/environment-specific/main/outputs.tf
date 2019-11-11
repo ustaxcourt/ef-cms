b441b0fddad65b40b50f7984bc8b0fe72a302532
Cody Seibert <cseibert113@gmail.com>
2019-11-11 11:11:11 -0500

    separating the permissions into global account permissions / roles and environment specific;

--- iam/terraform/environment-specific/main/outputs.tf
+++ iam/terraform/environment-specific/main/outputs.tf
@@ -7,10 +7,6 @@ output "s3_replication_role_arn" {
   value = "${aws_iam_role.s3_replication_role.arn}"
 }
 
-output "cloudwatch_role_arn" {
-  value = "${aws_iam_role.cloudwatch.arn}"
-}
-
 output "lambda_role_arn" {
   value = "${aws_iam_role.lambda_role.arn}"
 }
