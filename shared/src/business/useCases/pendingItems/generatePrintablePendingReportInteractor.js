04eb6b7c92822aa08cd760d5beb299d79a9aa82a
William Bagayoko <wbagayoko@gmail.com>
2019-11-12 01:28:37 -0600

    2156 generatePrintablePendingReportInteractor

---
+++ shared/src/business/useCases/pendingItems/generatePrintablePendingReportInteractor.js
@@ -0,0 +1,30 @@
+const {
+  isAuthorized,
+  ROLE_PERMISSIONS,
+} = require('../../../authorization/authorizationClientService');
+const { UnauthorizedError } = require('../../../errors/errors');
+
+/**
+ * generatePrintablePendingReportInteractor
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context
+ * @param {string} providers.judge the optional judge filter
+ * @param {string} providers.caseId the optional caseId filter
+ * @returns {Array} the url of the document
+ */
+exports.generatePrintablePendingReportInteractor = async ({
+  applicationContext,
+  caseId,
+  judge,
+}) => {
+  const authorizedUser = applicationContext.getCurrentUser();
+
+  if (!isAuthorized(authorizedUser, ROLE_PERMISSIONS.PENDING_ITEMS)) {
+    throw new UnauthorizedError('Unauthorized');
+  }
+
+  return await applicationContext
+    .getUseCaseHelpers()
+    .generatePendingReportPdf({ applicationContext, caseId, judge });
+};
