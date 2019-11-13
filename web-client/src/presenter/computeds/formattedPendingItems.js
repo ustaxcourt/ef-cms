917b04d8bf4146e14c381b56a549c0dd84cc278a
Kris Koskelin <kkoskelin@flexion.us>
2019-11-13 08:52:54 -0600

    removing pending report sorting options
    layout, capitalization changes

--- web-client/src/presenter/computeds/formattedPendingItems.js
+++ web-client/src/presenter/computeds/formattedPendingItems.js
@@ -16,31 +16,18 @@ export const formatPendingItem = (item, { applicationContext }) => {
 };
 
 export const formattedPendingItems = (get, applicationContext) => {
-  const sort = get(state.screenMetadata.sort);
-  const sortOrder = get(state.screenMetadata.sortOrder);
-
-  const sortFuncs = {
-    date: (a, b) =>
-      applicationContext
-        .getUtilities()
-        .compareISODateStrings(a.receivedAt, b.receivedAt),
-    judge: (a, b) =>
-      applicationContext.getUtilities().compareStrings(a.judge, b.judge),
-  };
-
   let items = get(state.pendingItems).map(item =>
     formatPendingItem(item, { applicationContext }),
   );
   const judgeFilter = get(state.screenMetadata.pendingItemsFilters.judge);
   const judges = sortedUniq(items.map(i => i.associatedJudgeFormatted).sort());
 
-  items = items.sort(sortFuncs[sort]);
+  items = items.sort((a, b) =>
+    applicationContext.getUtilities().compareStrings(a.judge, b.judge),
+  );
 
   if (judgeFilter) {
     items = items.filter(i => i.associatedJudgeFormatted === judgeFilter);
-  }
-  if (sortOrder == 'desc') {
-    items = items.reverse();
   }
 
   const result = {
