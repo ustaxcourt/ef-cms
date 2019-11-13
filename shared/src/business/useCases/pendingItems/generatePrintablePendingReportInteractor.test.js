04c2109eae31f6755a2604c5538166463e61c3d1
William Bagayoko <wbagayoko@gmail.com>
2019-11-12 22:08:31 -0600

    2156 coverage

--- shared/src/business/useCases/pendingItems/generatePrintablePendingReportInteractor.test.js
+++ shared/src/business/useCases/pendingItems/generatePrintablePendingReportInteractor.test.js
@@ -1,11 +1,13 @@
 const {
   generatePrintablePendingReportInteractor,
 } = require('./generatePrintablePendingReportInteractor');
+const { MOCK_CASE } = require('../../../test/mockCase');
 const { User } = require('../../entities/User');
 
 describe('generatePrintablePendingReportInteractor', () => {
   let generatePendingReportPdfSpy;
   let fetchPendingItemsSpy;
+  let getCaseByCaseIdSpy = jest.fn(() => MOCK_CASE);
 
   const applicationContext = {
     environment: { stage: 'local' },
@@ -15,6 +17,7 @@ describe('generatePrintablePendingReportInteractor', () => {
         userId: 'petitionsclerk',
       };
     },
+    getPersistenceGateway: () => ({ getCaseByCaseId: getCaseByCaseIdSpy }),
     getUseCaseHelpers: () => ({
       fetchPendingItems: fetchPendingItemsSpy,
       generatePendingReportPdf: generatePendingReportPdfSpy,
@@ -32,6 +35,20 @@ describe('generatePrintablePendingReportInteractor', () => {
 
     expect(generatePendingReportPdfSpy).toHaveBeenCalled();
     expect(results).toEqual('https://some.url');
+  });
+
+  it('should generate the title for the report', async () => {
+    generatePendingReportPdfSpy = jest.fn(() => 'https://some.url');
+    fetchPendingItemsSpy = jest.fn(() => []);
+
+    await generatePrintablePendingReportInteractor({
+      applicationContext,
+      caseId: '123',
+    });
+
+    expect(generatePendingReportPdfSpy.mock.calls[0][0]).toMatchObject({
+      reportTitle: 'Pending Report for Docket 101-18',
+    });
   });
 
   it('should throw an unauthorized error if the user does not have access', async () => {
