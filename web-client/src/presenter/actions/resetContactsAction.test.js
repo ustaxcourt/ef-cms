9ebdc1580bacf904b49a67eab0ccf83329427376
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-11-18 13:22:12 -0600

    another test fix

--- web-client/src/presenter/actions/resetContactsAction.test.js
+++ web-client/src/presenter/actions/resetContactsAction.test.js
@@ -1,6 +1,6 @@
 import { ContactFactory } from '../../../../shared/src/business/entities/contacts/ContactFactory';
 import { applicationContext } from '../../applicationContext';
-import { presenter } from '../../presenter';
+import { presenter } from '../presenter';
 import { resetContactsAction } from './resetContactsAction';
 import { runAction } from 'cerebral/test';
 
