56e79f0cebf669ffda1c6c33bf54ad3bd4fe1f1a
Cody Seibert <cseibert113@gmail.com>
2019-11-20 10:11:01 -0500

    save the entry when serving

--- web-client/src/presenter/sequences/serveCourtIssuedDocumentSequence.js
+++ web-client/src/presenter/sequences/serveCourtIssuedDocumentSequence.js
@@ -1,19 +1,47 @@
 import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { clearModalAction } from '../actions/clearModalAction';
+import { isEditingDocketEntryAction } from '../actions/CourtIssuedDocketEntry/isEditingDocketEntryAction';
 import { navigateToCaseDetailAction } from '../actions/navigateToCaseDetailAction';
 import { serveCourtIssuedDocumentAction } from '../actions/serveCourtIssuedDocumentAction';
+import { setAlertErrorAction } from '../actions/setAlertErrorAction';
 import { setAlertSuccessAction } from '../actions/setAlertSuccessAction';
 import { setSaveAlertsForNavigationAction } from '../actions/setSaveAlertsForNavigationAction';
+import { setValidationAlertErrorsAction } from '../actions/setValidationAlertErrorsAction';
+import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
 import { setWaitingForResponseAction } from '../actions/setWaitingForResponseAction';
+import { startShowValidationAction } from '../actions/startShowValidationAction';
+import { stopShowValidationAction } from '../actions/stopShowValidationAction';
+import { submitCourtIssuedDocketEntryAction } from '../actions/CourtIssuedDocketEntry/submitCourtIssuedDocketEntryAction';
 import { unsetWaitingForResponseAction } from '../actions/unsetWaitingForResponseAction';
+import { updateCourtIssuedDocketEntryAction } from '../actions/CourtIssuedDocketEntry/updateCourtIssuedDocketEntryAction';
+import { validateCourtIssuedDocketEntryAction } from '../actions/CourtIssuedDocketEntry/validateCourtIssuedDocketEntryAction';
 
 export const serveCourtIssuedDocumentSequence = [
   clearAlertsAction,
-  setWaitingForResponseAction,
-  serveCourtIssuedDocumentAction,
-  setSaveAlertsForNavigationAction,
-  setAlertSuccessAction,
-  clearModalAction,
-  unsetWaitingForResponseAction,
-  navigateToCaseDetailAction,
+  startShowValidationAction,
+  validateCourtIssuedDocketEntryAction,
+  {
+    error: [
+      setAlertErrorAction,
+      setValidationErrorsAction,
+      setValidationAlertErrorsAction,
+      clearModalAction,
+    ],
+    success: [
+      stopShowValidationAction,
+      clearAlertsAction,
+      setWaitingForResponseAction,
+      isEditingDocketEntryAction,
+      {
+        no: [submitCourtIssuedDocketEntryAction],
+        yes: [updateCourtIssuedDocketEntryAction],
+      },
+      serveCourtIssuedDocumentAction,
+      setAlertSuccessAction,
+      clearModalAction,
+      setSaveAlertsForNavigationAction,
+      navigateToCaseDetailAction,
+      unsetWaitingForResponseAction,
+    ],
+  },
 ];
