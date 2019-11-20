b8b514c86032c212982094f8cf671f342058eba6
Matt Hopson <matthopson@gmail.com>
2019-11-20 09:54:04 -0700

    Helper for getting parties to electronically serve

---
+++ shared/src/business/utilities/aggregateElectronicallyServedParties.js
@@ -0,0 +1,39 @@
+const {
+  constants,
+  setServiceIndicatorsForCase,
+} = require('./setServiceIndicatorsForCase');
+
+/**
+ * aggregateElectronicallyServedParties
+ *
+ * @param {object} caseEntity the case entity with parties to be served
+ * @returns {Array} the aggregated contact information for all parties
+ */
+
+const aggregateElectronicallyServedParties = caseEntity => {
+  const formattedCase = setServiceIndicatorsForCase(caseEntity);
+
+  const parties = [
+    formattedCase.contactPrimary,
+    formattedCase.contactSecondary,
+    ...formattedCase.practitioners,
+    ...formattedCase.respondents,
+  ];
+
+  const aggregated = [];
+  parties.map(party => {
+    if (
+      party &&
+      party.email &&
+      party.serviceIndicator === constants.SI_ELECTRONIC
+    ) {
+      aggregated.push({
+        email: party.email,
+        name: party.name,
+      });
+    }
+  });
+  return aggregated;
+};
+
+module.exports = { aggregateElectronicallyServedParties };
