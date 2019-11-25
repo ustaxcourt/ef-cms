b98126a2c9d20342a02b83a49228d59ff273934a
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-11-25 14:50:55 -0800

    3516: DoD - npm update, pa11y for public site

---
+++ web-client/pa11y-ci-public.config.js
@@ -0,0 +1,20 @@
+const urls = ['http://localhost:5678/'];
+
+// see https://github.com/pa11y/pa11y#command-line-interface
+
+module.exports = {
+  defaults: {
+    chromeLaunchConfig: {
+      args: ['--no-sandbox'],
+    },
+    concurrency: 3,
+    debug: true,
+    'include-notices': true,
+    'include-warnings': true,
+    standard: 'WCAG2AA',
+    timeout: 30000,
+    useIncognitoBrowserContext: true,
+    wait: 5000,
+  },
+  urls,
+};
