b4a3f4ad2a862b7458ebf05759f08f326d2db6d8
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-11-25 10:12:04 -0800

    clean up component a bit

--- web-client/src/views/PublicSearch.jsx
+++ web-client/src/views/PublicSearch.jsx
@@ -2,6 +2,15 @@ import { BigHeader } from './BigHeader';
 import { Mobile, NonMobile } from '../ustc-ui/Responsive/Responsive';
 import React from 'react';
 
+const SearchInformation = () => (
+  <p>
+    Anyone can search for a case in our system for cases filed{' '}
+    <span className="text-semibold">on or after May 1, 1986</span>. If you
+    aren’t affiliated with that case, you will only see limited information
+    about that case.
+  </p>
+);
+
 export const PublicSearch = () => {
   return (
     <>
@@ -10,12 +19,7 @@ export const PublicSearch = () => {
         <BigHeader text="Search for a Case" />
 
         <section className="usa-section grid-container advanced-search">
-          <p>
-            Anyone can search for a case in our system for cases filed{' '}
-            <span className="text-semibold">on or after May 1, 1986</span>. If
-            you aren’t affiliated with that case, you will only see limited
-            information about that case.
-          </p>
+          <SearchInformation />
         </section>
       </NonMobile>
       <Mobile>
@@ -23,12 +27,7 @@ export const PublicSearch = () => {
 
         <section className="usa-section grid-container advanced-search">
           <h2>Search for a Case</h2>
-          <p>
-            Anyone can search for a case in our system for cases filed{' '}
-            <span className="text-semibold">on or after May 1, 1986</span>. If
-            you aren’t affiliated with that case, you will only see limited
-            information about that case.
-          </p>
+          <SearchInformation />
         </section>
       </Mobile>
     </>
