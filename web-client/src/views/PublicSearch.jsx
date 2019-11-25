f83211ebc67dfb9cc8613767727facef09d9f2c6
Matt Hopson <matthopson@gmail.com>
2019-11-25 10:05:01 -0700

    Remove unused for linter

--- web-client/src/views/PublicSearch.jsx
+++ web-client/src/views/PublicSearch.jsx
@@ -1,15 +1,11 @@
-import { connect } from '@cerebral/react';
 import React from 'react';
 
-export const PublicSearch = connect(
-  {},
-  () => {
-    return (
-      <>
-        <section className="usa-section grid-container advanced-search">
-          <h1>Public Search</h1>
-        </section>
-      </>
-    );
-  },
-);
+export const PublicSearch = () => {
+  return (
+    <>
+      <section className="usa-section grid-container advanced-search">
+        <h1>Public Search</h1>
+      </section>
+    </>
+  );
+};
