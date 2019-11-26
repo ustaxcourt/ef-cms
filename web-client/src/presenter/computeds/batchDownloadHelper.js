f8e3e2181720bf602c6ae0491a35c56df6a3e7d1
William Bagayoko <wbagayoko@gmail.com>
2019-11-26 16:04:54 -0600

    3121: default params

--- web-client/src/presenter/computeds/batchDownloadHelper.js
+++ web-client/src/presenter/computeds/batchDownloadHelper.js
@@ -1,17 +1,13 @@
 import { state } from 'cerebral';
 
 export const batchDownloadHelper = get => {
-  const batchDownloads = get(state.batchDownloads) || {
-    fileCount: 3,
-    totalFiles: 7,
-  };
+  const fileCount = get(state.batchDownloads.fileCount) || 0;
+  const totalFiles = get(state.batchDownloads.totalFiles) || 0;
 
   const result = {
-    addedFiles: batchDownloads.fileCount,
-    percentComplete: Math.floor(
-      (batchDownloads.fileCount * 100) / batchDownloads.totalFiles,
-    ),
-    totalFiles: batchDownloads.totalFiles,
+    addedFiles: fileCount,
+    percentComplete: Math.floor((fileCount * 100) / totalFiles),
+    totalFiles,
   };
   return result;
 };
