fb591449d468346bef05f6b734e600c7b18fb947
Matt Hopson <matthopson@gmail.com>
2019-11-26 13:27:17 -0700

    Helper for aggregating search params

---
+++ shared/src/business/utilities/aggregateCommonQueryParams.js
@@ -0,0 +1,130 @@
+const { CaseSearch } = require('../entities/cases/CaseSearch');
+const aggregateCommonQueryParams = ({
+  applicationContext,
+  countryType,
+  petitionerName,
+  petitionerState,
+  yearFiledMax,
+  yearFiledMin,
+}) => {
+  const commonQuery = [];
+  const exactMatchesQuery = [];
+  const nonExactMatchesQuery = [];
+
+  if (petitionerName) {
+    const petitionerNameArray = petitionerName.toLowerCase().split(' ');
+    exactMatchesQuery.push({
+      bool: {
+        should: [
+          {
+            bool: {
+              minimum_should_match: petitionerNameArray.length,
+              should: petitionerNameArray.map(word => {
+                return {
+                  term: {
+                    'contactPrimary.M.name.S': word,
+                  },
+                };
+              }),
+            },
+          },
+          {
+            bool: {
+              minimum_should_match: petitionerNameArray.length,
+              should: petitionerNameArray.map(word => {
+                return {
+                  term: {
+                    'contactPrimary.M.secondaryName.S': word,
+                  },
+                };
+              }),
+            },
+          },
+          {
+            bool: {
+              minimum_should_match: petitionerNameArray.length,
+              should: petitionerNameArray.map(word => {
+                return {
+                  term: {
+                    'contactSecondary.M.name.S': word,
+                  },
+                };
+              }),
+            },
+          },
+        ],
+      },
+    });
+
+    nonExactMatchesQuery.push({
+      bool: {
+        should: [
+          { match: { 'contactPrimary.M.name.S': petitionerName } },
+          { match: { 'contactPrimary.M.secondaryName.S': petitionerName } },
+          { match: { 'contactSecondary.M.name.S': petitionerName } },
+        ],
+      },
+    });
+  }
+  if (countryType) {
+    commonQuery.push({
+      bool: {
+        should: [
+          {
+            match: {
+              'contactPrimary.M.countryType.S': countryType,
+            },
+          },
+          {
+            match: {
+              'contactSecondary.M.countryType.S': countryType,
+            },
+          },
+        ],
+      },
+    });
+  }
+  if (petitionerState) {
+    commonQuery.push({
+      bool: {
+        should: [
+          {
+            match: {
+              'contactPrimary.M.state.S': petitionerState,
+            },
+          },
+          {
+            match: {
+              'contactSecondary.M.state.S': petitionerState,
+            },
+          },
+        ],
+      },
+    });
+  }
+  if (yearFiledMin || yearFiledMax) {
+    const yearMin = yearFiledMin || CaseSearch.CASE_SEARCH_MIN_YEAR;
+    const yearMax =
+      yearFiledMax || applicationContext.getUtilities().formatNow('YYYY');
+
+    commonQuery.push({
+      range: {
+        'receivedAt.S': {
+          format: 'yyyy',
+          gte: `${yearMin}||/y`,
+          lte: `${yearMax}||/y`,
+        },
+      },
+    });
+  }
+
+  return {
+    commonQuery,
+    exactMatchesQuery,
+    nonExactMatchesQuery,
+  };
+};
+
+module.exports = {
+  aggregateCommonQueryParams,
+};
