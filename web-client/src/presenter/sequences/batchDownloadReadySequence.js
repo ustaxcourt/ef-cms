125b759eacc697189cdb7a52f53de6da4456a5cd
William Bagayoko <wbagayoko@gmail.com>
2019-11-26 14:06:23 -0600

    3121: unset progress once download is ready

--- web-client/src/presenter/sequences/batchDownloadReadySequence.js
+++ web-client/src/presenter/sequences/batchDownloadReadySequence.js
@@ -1,8 +1,12 @@
 import { displayDownloadLinkAction } from '../actions/batchDownload/displayDownloadLinkAction';
+import { state } from 'cerebral';
 import { stopWebSocketConnectionAction } from '../actions/webSocketConnection/stopWebSocketConnectionAction';
+import { unset } from 'cerebral/factories';
 import { unsetWaitingForResponseAction } from '../actions/unsetWaitingForResponseAction';
 
 export const batchDownloadReadySequence = [
+  unset(state.batchDownloads),
+  unset(state.zipInProgress),
   unsetWaitingForResponseAction,
   displayDownloadLinkAction,
   stopWebSocketConnectionAction,
