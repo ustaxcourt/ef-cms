078b889d7080ac0956d5825b59df88664503789e
Cody Seibert <cseibert113@gmail.com>
2019-11-27 14:39:57 -0500

    fixing tests

--- web-client/src/presenter/actions/AdvancedSearch/isOneResultFoundAction.js
+++ web-client/src/presenter/actions/AdvancedSearch/isOneResultFoundAction.js
@@ -2,7 +2,6 @@
  * takes a different path depending on the number of results
  *
  * @param {object} providers the providers object
- * @param {object} providers.applicationContext the application context
  * @param {Function} providers.path the cerebral path function
  * @param {object} providers.props the cerebral props object
  * @returns {Promise} async action
