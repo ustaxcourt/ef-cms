e66f8573c15e2a813bd9aee3fb3521889129599e
Matt Hopson <matthopson@gmail.com>
2019-12-02 12:24:52 -0600

    Programatically set next year

--- shared/src/business/useCases/trialSessions/validateTrialSessionInteractor.test.js
+++ shared/src/business/useCases/trialSessions/validateTrialSessionInteractor.test.js
@@ -6,6 +6,8 @@ const {
 } = require('./validateTrialSessionInteractor');
 const { omit } = require('lodash');
 
+const { formatNow } = require('../../utilities/DateHandler');
+
 describe('validateTrialSessionInteractor', () => {
   it('returns the expected errors object on an empty trial session', () => {
     const errors = validateTrialSessionInteractor({
@@ -30,12 +32,13 @@ describe('validateTrialSessionInteractor', () => {
   });
 
   it('returns null for a valid trial session', () => {
+    const nextYear = (parseInt(formatNow('YYYY')) + 1).toString();
     const MOCK_TRIAL = {
       maxCases: 100,
       sessionType: 'Regular',
-      startDate: '2020-12-01T00:00:00.000Z',
+      startDate: `${nextYear}-12-01T00:00:00.000Z`,
       term: 'Fall',
-      termYear: '2020',
+      termYear: nextYear,
       trialLocation: 'Birmingham, AL',
     };
 
