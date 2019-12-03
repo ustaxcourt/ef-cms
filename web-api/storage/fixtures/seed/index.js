79be8de2bd5d48eabacf4a017331a4f0600bf81c
Kristen Lohman <klohman@flexion.us>
2019-12-03 12:37:48 -0500

    Update index.js

--- web-api/storage/fixtures/seed/index.js
+++ web-api/storage/fixtures/seed/index.js
@@ -1,5 +1,4 @@
 module.exports = [
-  /*
   ...require('./101-19.json'),
   ...require('./102-19.json'),
   ...require('./103-19.json'),
@@ -9,7 +8,7 @@ module.exports = [
   ...require('./107-19.json'),
   ...require('./108-19.json'),
   ...require('./109-19.json'),
-  ...require('./110-19.json'), */
+  ...require('./110-19.json'), 
   ...require('./trial-sessions.json'),
   ...require('./trial-sessions-past.json'),
   ...require('./misc.json'),
