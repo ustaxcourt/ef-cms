a4a1c1e2a8f65faf444826d90cdaeea45d6a0d64
Cody Seibert <cseibert113@gmail.com>
2019-12-03 10:54:51 -0500

    40 - creating the public case detail header

---
+++ web-client/src/views/PublicCaseDetailHeader.jsx
@@ -0,0 +1,38 @@
+import { Button } from '../ustc-ui/Button/Button';
+import { CaseLink } from '../ustc-ui/CaseLink/CaseLink';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { UpdateCaseModalDialog } from './CaseDetailEdit/UpdateCaseModalDialog';
+import { connect } from '@cerebral/react';
+import { state } from 'cerebral';
+import React from 'react';
+
+export const PublicCaseDetailHeader = connect(
+  {
+    publicCaseDetailHeaderHelper: state.publicCaseDetailHeaderHelper,
+  },
+  ({ publicCaseDetailHeaderHelper }) => {
+    return (
+      <div className="big-blue-header">
+        <div className="grid-container">
+          <div className="grid-row">
+            <div className="tablet:grid-col-8">
+              <div className="margin-bottom-1">
+                <h1 className="heading-2 captioned" tabIndex="-1">
+                  <CaseLink
+                    docketNumber={publicCaseDetailHeaderHelper.docketNumber}
+                  >
+                    Docket Number:{' '}
+                    {publicCaseDetailHeaderHelper.docketNumberWithSuffix}
+                  </CaseLink>
+                </h1>
+                <p className="margin-y-0" id="case-title">
+                  <span>{publicCaseDetailHeaderHelper.caseTitle}</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  },
+);
