dfe7f5f28585b2443a3b74b42ed5a7966f69558e
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-12-04 12:22:29 -0800

    40: refactor to remove logic from view

--- web-client/src/views/Public/PublicFilingsAndProceedings.jsx
+++ web-client/src/views/Public/PublicFilingsAndProceedings.jsx
@@ -5,68 +5,50 @@ import React from 'react';
 
 export const PublicFilingsAndProceedings = connect(
   {
-    arrayIndex: props.arrayIndex,
     baseUrl: state.baseUrl,
     caseDetail: state.caseDetail,
-    document: props.document,
-    record: props.record,
+    entry: props.entry,
   },
-  ({ baseUrl, caseDetail, document, record }) => {
+  ({ baseUrl, caseDetail, entry }) => {
     const renderDocumentLink = (documentId, description, isPaper) => {
       return (
         <React.Fragment>
-          {!document.isInProgress && !document.isNotServedCourtIssuedDocument && (
-            <React.Fragment>
-              <a
-                aria-label={`View PDF: ${description}`}
-                href={`${baseUrl}/public-api/${caseDetail.caseId}/${documentId}/public-document-download-url`}
-                rel="noreferrer noopener"
-                target="_blank"
-              >
-                {isPaper && (
-                  <span className="filing-type-icon-mobile">
-                    <FontAwesomeIcon icon={['fas', 'file-alt']} />
-                  </span>
-                )}
-                {description}
-              </a>
-            </React.Fragment>
-          )}
-          {document.isInProgress && description}
+          <React.Fragment>
+            <a
+              aria-label={`View PDF: ${description}`}
+              href={`${baseUrl}/public-api/${caseDetail.caseId}/${documentId}/public-document-download-url`}
+              rel="noreferrer noopener"
+              target="_blank"
+            >
+              {isPaper && (
+                <span className="filing-type-icon-mobile">
+                  <FontAwesomeIcon icon={['fas', 'file-alt']} />
+                </span>
+              )}
+              {description}
+            </a>
+          </React.Fragment>
         </React.Fragment>
       );
     };
 
     return (
       <React.Fragment>
-        {document &&
-          document.processingStatus === 'complete' &&
-          document.isCourtIssuedDocument &&
-          !document.isNotServedCourtIssuedDocument &&
+        {entry.showLinkToDocument &&
           renderDocumentLink(
-            document.documentId,
-            record.description,
-            document.isPaper,
+            entry.documentId,
+            entry.description,
+            entry.isPaper,
           )}
 
-        {document &&
-          (!document.isCourtIssuedDocument ||
-            document.isNotServedCourtIssuedDocument) &&
-          (document.documentTitle || record.description)}
+        {entry.showDocumentDescriptionWithoutLink && entry.descriptionDisplay}
 
-        <span> {record.signatory}</span>
+        <span> {entry.signatory}</span>
 
-        {!document && record.description}
+        {!entry.hasDocument && entry.description}
 
         <span className="filings-and-proceedings">
-          {document &&
-            document.documentTitle &&
-            document.additionalInfo &&
-            ` ${document.additionalInfo}`}
-          {record.filingsAndProceedings && ` ${record.filingsAndProceedings}`}
-          {document &&
-            document.additionalInfo2 &&
-            ` ${document.additionalInfo2}`}
+          {entry.filingsAndProceedingsWithAdditionalInfo}
         </span>
       </React.Fragment>
     );
