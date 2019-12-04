bff37850be4fdc31e71d0103ad95322e34f2b7a9
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-12-04 12:26:00 -0800

    a little more cleanup

--- web-client/src/views/Public/PublicFilingsAndProceedings.jsx
+++ web-client/src/views/Public/PublicFilingsAndProceedings.jsx
@@ -10,36 +10,23 @@ export const PublicFilingsAndProceedings = connect(
     entry: props.entry,
   },
   ({ baseUrl, caseDetail, entry }) => {
-    const renderDocumentLink = (documentId, description, isPaper) => {
-      return (
-        <React.Fragment>
-          <React.Fragment>
-            <a
-              aria-label={`View PDF: ${description}`}
-              href={`${baseUrl}/public-api/${caseDetail.caseId}/${documentId}/public-document-download-url`}
-              rel="noreferrer noopener"
-              target="_blank"
-            >
-              {isPaper && (
-                <span className="filing-type-icon-mobile">
-                  <FontAwesomeIcon icon={['fas', 'file-alt']} />
-                </span>
-              )}
-              {description}
-            </a>
-          </React.Fragment>
-        </React.Fragment>
-      );
-    };
-
     return (
       <React.Fragment>
-        {entry.showLinkToDocument &&
-          renderDocumentLink(
-            entry.documentId,
-            entry.description,
-            entry.isPaper,
-          )}
+        {entry.showLinkToDocument && (
+          <a
+            aria-label={`View PDF: ${entry.description}`}
+            href={`${baseUrl}/public-api/${caseDetail.caseId}/${entry.documentId}/public-document-download-url`}
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            {entry.isPaper && (
+              <span className="filing-type-icon-mobile">
+                <FontAwesomeIcon icon={['fas', 'file-alt']} />
+              </span>
+            )}
+            {entry.description}
+          </a>
+        )}
 
         {entry.showDocumentDescriptionWithoutLink && entry.descriptionDisplay}
 
