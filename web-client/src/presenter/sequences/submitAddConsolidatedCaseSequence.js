6fe4a527591d907bc74d19d6205529083c283ae8
William Bagayoko <wbagayoko@gmail.com>
2019-12-05 11:13:26 -0600

    482 sequence updates (wip draft)

--- web-client/src/presenter/sequences/submitAddConsolidatedCaseSequence.js
+++ web-client/src/presenter/sequences/submitAddConsolidatedCaseSequence.js
@@ -1,7 +1,27 @@
+import { addConsolidatedCaseAction } from '../actions/caseConsolidation/addConsolidatedCaseAction';
+import { canConsolidateAction } from '../actions/caseConsolidation/canConsolidateAction';
 import { clearModalAction } from '../actions/clearModalAction';
 import { clearModalStateAction } from '../actions/clearModalStateAction';
+import { primePropsForCanConsolidateAction } from '../actions/caseConsolidation/primePropsForCanConsolidateAction';
+import { refreshCaseAction } from '../actions/refreshCaseAction';
+import { setAddConsolidatedCaseSuccessMessageAction } from '../actions/caseConsolidation/setAddConsolidatedCaseSuccessMessageAction';
+import { setCanConsolidateErrorAction } from '../actions/caseConsolidation/setCanConsolidateErrorAction';
+import { setWaitingForResponseAction } from '../actions/setWaitingForResponseAction';
+import { unsetWaitingForResponseAction } from '../actions/unsetWaitingForResponseAction';
 
 export const submitAddConsolidatedCaseSequence = [
-  clearModalAction,
-  clearModalStateAction,
+  primePropsForCanConsolidateAction,
+  canConsolidateAction,
+  {
+    error: [setCanConsolidateErrorAction],
+    success: [
+      setWaitingForResponseAction,
+      addConsolidatedCaseAction,
+      refreshCaseAction,
+      clearModalAction,
+      clearModalStateAction,
+      setAddConsolidatedCaseSuccessMessageAction,
+      unsetWaitingForResponseAction,
+    ],
+  },
 ];
