dd580da0e2c68d18f424e4162a731d0bbb69ef70
Matt Hopson <matthopson@gmail.com>
2019-12-05 12:51:07 -0600

    Set consolidation route, handler, interactors

---
+++ shared/src/proxies/setConsolidatedCaseProxy.js
@@ -0,0 +1,22 @@
+const { put } = require('./requests');
+
+/**
+ * setConsolidatedCaseInteractor
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context
+ * @param {string} providers.caseId the id of the case to consolidate
+ * @param {object} providers.leadCaseId the id of the lead case for consolidation
+ * @returns {Promise<*>} the promise of the api call
+ */
+exports.setConsolidatedCaseInteractor = ({
+  applicationContext,
+  caseId,
+  leadCaseId,
+}) => {
+  return put({
+    applicationContext,
+    body: leadCaseId,
+    endpoint: `/cases/${caseId}/consolidate-case`,
+  });
+};
