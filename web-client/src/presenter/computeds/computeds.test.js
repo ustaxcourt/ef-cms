8ce234ea53e8fc56def3d0cabe5b9da1ac854e19
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-12-04 19:59:09 -0800

    add code that was really being used; test fixes

---
+++ web-client/src/presenter/computeds/computeds.test.js
@@ -0,0 +1,53 @@
+import { runCompute } from 'cerebral/test';
+
+import { Case } from '../../../../shared/src/business/entities/cases/Case';
+import { formattedCases as formattedCasesComputed } from './formattedCaseDetail';
+import { withAppContextDecorator } from '../../withAppContext';
+
+const formattedCases = withAppContextDecorator(formattedCasesComputed);
+
+describe('formatted case details computed', () => {
+  it('formats the date in a list of cases', () => {
+    const result = runCompute(formattedCases, {
+      state: {
+        cases: [
+          {
+            caseCaption: 'Brett Osborne, Petitioner',
+            docketRecord: [],
+            documents: [
+              {
+                createdAt: '2018-11-21T20:49:28.192Z',
+                documentType: 'fakeType',
+                status: 'served',
+              },
+            ],
+            irsSendDate: '2018-11-21T20:49:28.192Z',
+            petitioners: [{ name: 'bob' }],
+          },
+        ],
+        constants: {
+          CASE_CAPTION_POSTFIX: Case.CASE_CAPTION_POSTFIX,
+        },
+      },
+    });
+    expect(result[0].irsDateFormatted).toContain('11/21/18');
+  });
+
+  it('formats the respondent name to include barNumber', () => {
+    const result = runCompute(formattedCases, {
+      state: {
+        cases: [
+          {
+            caseCaption: 'Brett Osborne, Petitioner',
+            petitioners: [{ name: 'bob' }],
+            respondents: [{ barNumber: '123', name: 'test' }],
+          },
+        ],
+        constants: {
+          CASE_CAPTION_POSTFIX: Case.CASE_CAPTION_POSTFIX,
+        },
+      },
+    });
+    expect(result[0].respondents[0].formattedName).toContain('test (123)');
+  });
+});
