098bf46b712a03f89bd82d3a0e6f5f3b246c889b
Cody Seibert <cseibert113@gmail.com>
2019-12-06 18:14:34 -0500

    displaying the paper contacts in a hint on the modal

--- web-client/src/views/ConfirmInitiateServiceModal.jsx
+++ web-client/src/views/ConfirmInitiateServiceModal.jsx
@@ -1,14 +1,21 @@
+import { Hint } from '../ustc-ui/Hint/Hint';
 import { ModalDialog } from './ModalDialog';
 import { connect } from '@cerebral/react';
-import { sequences } from 'cerebral';
+import { sequences, state } from 'cerebral';
 import React from 'react';
 
 export const ConfirmInitiateServiceModal = connect(
   {
     cancelSequence: sequences.clearModalSequence,
+    confirmInitiateServiceModalHelper: state.confirmInitiateServiceModalHelper,
     confirmSequence: sequences.serveCourtIssuedDocumentSequence,
   },
-  ({ cancelSequence, confirmSequence, documentTitle }) => {
+  ({
+    cancelSequence,
+    confirmInitiateServiceModalHelper,
+    confirmSequence,
+    documentTitle,
+  }) => {
     return (
       <ModalDialog
         cancelLabel="No, take me back"
@@ -25,6 +32,20 @@ export const ConfirmInitiateServiceModal = connect(
         <p className="margin-top-0 margin-bottom-5">
           <strong>{documentTitle}</strong>
         </p>
+        {confirmInitiateServiceModalHelper.showPaperAlert && (
+          <Hint exclamation fullWidth className="block">
+            <div className="margin-bottom-1">
+              This case has parties receiving paper service:
+            </div>
+            {confirmInitiateServiceModalHelper.contactsNeedingPaperService.map(
+              contact => (
+                <div className="margin-bottom-1" key={contact.name}>
+                  {contact.name}
+                </div>
+              ),
+            )}
+          </Hint>
+        )}
       </ModalDialog>
     );
   },
