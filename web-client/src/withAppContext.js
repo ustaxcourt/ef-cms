857491e79e1f0f462ed50ca73838a35f3a432268
Cody Seibert <cseibert113@gmail.com>
2019-12-06 14:10:36 -0500

    move views in the app seem to work after these small changes

--- web-client/src/withAppContext.js
+++ web-client/src/withAppContext.js
@@ -1,5 +1,15 @@
 import { applicationContext } from './applicationContext';
 
 export const withAppContextDecorator = (f, context) => {
-  return get => f(get, context || applicationContext);
+  return get => {
+    try {
+      return f(get, context || applicationContext);
+    } catch (err) {
+      if (process.env.USTC_DEBUG) {
+        return null;
+      } else {
+        throw err;
+      }
+    }
+  };
 };
