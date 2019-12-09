d62d9bf478597e4f9ed1bb75a21807d903e11902
Joseph Sutton <jsutton@flexion.us>
2019-12-09 14:24:54 -0600

    added unit test

---
+++ web-client/src/presenter/actions/caseConsolidation/getConsolidatedCasesByCaseAction.test.js
@@ -0,0 +1,62 @@
+import { getConsolidatedCasesByCaseAction } from './getConsolidatedCasesByCaseAction';
+import { presenter } from '../../presenter';
+import { runAction } from 'cerebral/test';
+
+describe('getConsolidatedCasesByCaseAction', () => {
+  let getConsolidatedCasesByCaseInteractorStub;
+
+  beforeEach(() => {
+    getConsolidatedCasesByCaseInteractorStub = jest.fn().mockReturnValue([
+      {
+        caseId: 'case-id-234',
+        createdAt: '2019-07-20T20:20:15.680Z',
+        leadCaseId: 'case-id-123',
+      },
+      {
+        caseId: 'case-id-123',
+        createdAt: '2019-07-19T20:20:15.680Z',
+        leadCaseId: 'case-id-123',
+      },
+      {
+        caseId: 'case-id-345',
+        createdAt: '2019-07-21T20:20:15.680Z',
+        leadCaseId: 'case-id-123',
+      },
+    ]);
+
+    presenter.providers.applicationContext = {
+      getUseCases: () => ({
+        getConsolidatedCasesByCaseInteractor: getConsolidatedCasesByCaseInteractorStub,
+      }),
+    };
+  });
+
+  it("gets the consolidated cases by the case's lead case", async () => {
+    const { state } = await runAction(getConsolidatedCasesByCaseAction, {
+      modules: { presenter },
+      state: {
+        caseDetail: {
+          leadCaseId: 'case-id-123',
+        },
+      },
+    });
+
+    expect(state.caseDetail.consolidatedCases).toEqual([
+      {
+        caseId: 'case-id-123',
+        createdAt: '2019-07-19T20:20:15.680Z',
+        leadCaseId: 'case-id-123',
+      },
+      {
+        caseId: 'case-id-234',
+        createdAt: '2019-07-20T20:20:15.680Z',
+        leadCaseId: 'case-id-123',
+      },
+      {
+        caseId: 'case-id-345',
+        createdAt: '2019-07-21T20:20:15.680Z',
+        leadCaseId: 'case-id-123',
+      },
+    ]);
+  });
+});
