6794127603fbe36d787570c8c82d25370b599783
Joseph Sutton <jsutton@flexion.us>
2019-12-09 12:51:52 -0600

    get consolidated cases by case via proxy interactor

---
+++ shared/src/proxies/getConsolidatedCasesByCaseProxy.js
@@ -0,0 +1,17 @@
+const { get } = require('./requests');
+
+/**
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context
+ * @returns {Promise<*>} the promise of the api call
+ */
+exports.getConsolidatedCasesByUserInteractor = ({
+  applicationContext,
+  caseId,
+}) => {
+  return get({
+    applicationContext,
+    endpoint: `/case/${caseId}/consolidated`,
+  });
+};
