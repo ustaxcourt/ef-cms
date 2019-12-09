c5db881caf69d7b18b92a040ab7ad371bd2cb58a
Joseph Sutton <jsutton@flexion.us>
2019-12-09 15:56:27 -0600

    jsdoc for proxy interactor

--- shared/src/proxies/getConsolidatedCasesByCaseProxy.js
+++ shared/src/proxies/getConsolidatedCasesByCaseProxy.js
@@ -1,9 +1,11 @@
 const { get } = require('./requests');
 
 /**
+ * calls a proxy to retrieve cases with the passed lead case id
  *
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
+ * @param {object} providers.caseId the case's id to pass to the endpoint
  * @returns {Promise<*>} the promise of the api call
  */
 exports.getConsolidatedCasesByCaseInteractor = ({
