cb5de9df45f52de7c2ed908a2f58707a00d78c6b
Joseph Sutton <jsutton@flexion.us>
2019-12-09 10:09:56 -0600

    moved file and added unit test

--- web-client/src/presenter/sequences/gotoDashboardSequence.js
+++ web-client/src/presenter/sequences/gotoDashboardSequence.js
@@ -2,7 +2,7 @@ import { chooseWorkQueueSequence } from './chooseWorkQueueSequence';
 import { clearErrorAlertsAction } from '../actions/clearErrorAlertsAction';
 import { closeMobileMenuAction } from '../actions/closeMobileMenuAction';
 import { getCasesByUserAction } from '../actions/getCasesByUserAction';
-import { getConsolidatedCasesByUserAction } from '../actions/getConsolidatedCasesByUserAction';
+import { getConsolidatedCasesByUserAction } from '../actions/caseConsolidation/getConsolidatedCasesByUserAction';
 import { getTrialSessionsAction } from '../actions/TrialSession/getTrialSessionsAction';
 import { getUserAction } from '../actions/getUserAction';
 import { isLoggedInAction } from '../actions/isLoggedInAction';
