46f2af03dcc6062fd9705c9ab413d52dc0b03973
Cody Seibert <cseibert113@gmail.com>
2019-12-09 14:39:42 -0500

    fixing the header and address on the pdfs

--- web-client/src/presenter/computeds/addCourtIssuedDocketEntryHelper.js
+++ web-client/src/presenter/computeds/addCourtIssuedDocketEntryHelper.js
@@ -3,7 +3,14 @@ import { state } from 'cerebral';
 
 export const addCourtIssuedDocketEntryHelper = (get, applicationContext) => {
   const { COURT_ISSUED_EVENT_CODES } = applicationContext.getConstants();
-  const caseDetail = get(state.caseDetail);
+  const caseDetail = applicationContext
+    .getUtilities()
+    .setServiceIndicatorsForCase({
+      ...get(state.caseDetail),
+    });
+
+  console.log('caseDetail', caseDetail);
+
   const form = get(state.form);
 
   const documentTypes = COURT_ISSUED_EVENT_CODES.map(type => ({
