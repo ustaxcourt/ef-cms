4201b896a5f94f67a510a0d96b1a215fe13d0487
Joseph Sutton <jsutton@flexion.us>
2019-12-11 14:52:13 -0600

    added jsdoc

--- web-client/src/presenter/actions/caseConsolidation/validateFileDocumentAction.js
+++ web-client/src/presenter/actions/caseConsolidation/validateFileDocumentAction.js
@@ -1,6 +1,14 @@
 import { isEmpty } from 'lodash';
 import { state } from 'cerebral';
 
+/**
+ * check to see if we can consolidate cases
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.get the cerebral get object
+ * @param {object} providers.path the next object in the path
+ * @returns {object} the path to take next
+ */
 export const validateFileDocumentAction = ({ get, path }) => {
   const casesToFileDocument = get(state.modal.casesToFileDocument);
 
--- web-client/src/presenter/actions/setModalErrorAction.js
+++ web-client/src/presenter/actions/setModalErrorAction.js
@@ -1,5 +1,13 @@
 import { state } from 'cerebral';
 
+/**
+ * sets modal error state
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.props the cerebral props
+ * @param {object} providers.store the cerebral store
+ * @returns {void}
+ */
 export const setModalErrorAction = ({ props, store }) => {
   const { error } = props;
 
