a38ff2d469b6f0aa1ccae9f3833c4a951a0577f0
Joseph Sutton <jsutton@flexion.us>
2019-12-11 12:29:43 -0600

    added modal dialog

---
+++ web-client/src/views/FileDocument/CheckConsolidatedCasesModal.jsx
@@ -0,0 +1,61 @@
+import { ConfirmModal } from '../../ustc-ui/Modal/ConfirmModal';
+import { FormGroup } from '../../ustc-ui/FormGroup/FormGroup';
+import { connect } from '@cerebral/react';
+import { sequences, state } from 'cerebral';
+import React from 'react';
+
+export const CheckConsolidatedCasesModal = connect(
+  {
+    caseDetail: state.caseDetail,
+    error: state.modal.error,
+    modal: state.modal,
+    updateModalValueSequence: sequences.updateModalValueSequence,
+  },
+  ({ caseDetail, error, modal, updateModalValueSequence }) => {
+    return (
+      <ConfirmModal
+        cancelLabel="Cancel"
+        className="add-consolidated-case-search-modal"
+        confirmLabel="Add to Case(s)"
+        title="Would You Like To File Your Document In Multiple Cases?"
+        onCancelSequence="clearModalSequence"
+        onConfirmSequence="gotoFileDocumentSequence"
+      >
+        <FormGroup errorText={error}>
+          <legend className="usa-legend" id="case-consolidated-info">
+            This case is part of a consolidated group. Please select which cases
+            to file this document in.
+          </legend>
+          {caseDetail.consolidatedCases.map((consolidatedCase, index) => (
+            <div className="padding-bottom-2" key={index}>
+              <input
+                className="usa-checkbox__input"
+                id={`case-${consolidatedCase.docketNumber}`}
+                name={`casesToFileDocument[${consolidatedCase.docketNumber}]`}
+                type="checkbox"
+                onChange={e => {
+                  const casesToFileDocument = {
+                    ...modal.casesToFileDocument,
+                    [consolidatedCase.docketNumber]: e.target.checked,
+                  };
+
+                  updateModalValueSequence({
+                    key: 'casesToFileDocument',
+                    value: casesToFileDocument,
+                  });
+                }}
+              />
+              <label
+                className="usa-checkbox__label"
+                htmlFor={`case-${consolidatedCase.docketNumber}`}
+              >
+                {consolidatedCase.docketNumber} {consolidatedCase.caseCaption}
+              </label>
+            </div>
+          ))}
+          <div className="usa-checkbox" id="confirm-legend"></div>
+        </FormGroup>
+      </ConfirmModal>
+    );
+  },
+);
