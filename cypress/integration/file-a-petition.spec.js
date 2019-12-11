b85789240c08b51440030b574e22a362f4c24457
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-12-11 14:30:30 -0800

    cypress test fix

--- cypress/integration/file-a-petition.spec.js
+++ cypress/integration/file-a-petition.spec.js
@@ -191,10 +191,4 @@ describe('can view case detail', () => {
   it('shows docket record table and data', () => {
     cy.get('table.case-detail.docket-record tbody tr').should('exist');
   });
-
-  it('accordion header expands/collapses', () => {
-    cy.get('#actions-button').should('exist');
-    cy.get('#actions-button').click();
-    cy.get('#paymentInfo').should('be.visible');
-  });
 });
