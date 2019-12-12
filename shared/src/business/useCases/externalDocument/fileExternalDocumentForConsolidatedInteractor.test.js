0bf7c054d5354c225cebb714ae3d92546702e183
Matt Hopson <matthopson@gmail.com>
2019-12-12 10:07:28 -0600

    Flesh out interactor for saving docs on consolidated set

--- shared/src/business/useCases/externalDocument/fileExternalDocumentForConsolidatedInteractor.test.js
+++ shared/src/business/useCases/externalDocument/fileExternalDocumentForConsolidatedInteractor.test.js
@@ -2,18 +2,19 @@ const {
   fileExternalDocumentForConsolidatedInteractor,
 } = require('./fileExternalDocumentForConsolidatedInteractor');
 const { ContactFactory } = require('../../entities/contacts/ContactFactory');
-const { MOCK_USERS } = require('../../../test/mockUsers');
 const { User } = require('../../entities/User');
 
 describe('fileExternalDocumentForConsolidatedInteractor', () => {
   let applicationContext;
   let caseRecords;
-  const caseId0 = '00000000-0000-0000-0000-000000000000';
-  const caseId1 = '11111111-1111-1111-1111-111111111111';
-  const documentId0 = 'dddddddd-0000-dddd-0000-dddddddddddd';
-  // const documentId1 = 'dddddddd-1111-dddd-1111-dddddddddddd';
+  let sendBulkTemplatedEmailMock;
+  const caseId0 = '00000000-b37b-479d-9201-067ec6e335bb';
+  const caseId1 = '11111111-b37b-479d-9201-067ec6e335bb';
+  const documentId0 = 'd0d0d0d0-b37b-479d-9201-067ec6e335bb';
 
   beforeEach(() => {
+    sendBulkTemplatedEmailMock = jest.fn();
+
     caseRecords = [
       {
         caseId: caseId0,
@@ -21,8 +22,8 @@ describe('fileExternalDocumentForConsolidatedInteractor', () => {
           email: 'fieri@example.com',
           name: 'Guy Fieri',
         },
-        createdAt: '',
-        docketNumber: '000-00',
+        createdAt: '2019-04-19T17:29:13.120Z',
+        docketNumber: '123-19',
         docketRecord: [],
         documents: [],
         leadCaseId: caseId0,
@@ -36,8 +37,8 @@ describe('fileExternalDocumentForConsolidatedInteractor', () => {
           email: 'ferrari@example.com',
           name: 'Enzo Ferrari',
         },
-        createdAt: '',
-        docketNumber: '111-00',
+        createdAt: '2019-04-19T17:29:13.120Z',
+        docketNumber: '234-19',
         docketRecord: [],
         documents: [],
         leadCaseId: caseId0,
@@ -46,28 +47,41 @@ describe('fileExternalDocumentForConsolidatedInteractor', () => {
         userId: 'petitioner',
       },
     ];
+
+    applicationContext = {
+      environment: { stage: 'local' },
+      getCurrentUser: () => {
+        return {
+          name: 'Guy Fieri',
+          role: User.ROLES.petitioner,
+          userId: 'a7d90c05-f6cd-442c-a168-202db587f16f',
+        };
+      },
+      getDispatchers: () => ({
+        sendBulkTemplatedEmail: sendBulkTemplatedEmailMock,
+      }),
+      getPersistenceGateway: () => ({
+        getCasesByLeadCaseId: async () => caseRecords,
+        getUserById: () => ({
+          name: 'Guy Fieri',
+          role: User.ROLES.petitioner,
+          userId: 'a7d90c05-f6cd-442c-a168-202db587f16f',
+        }),
+        saveWorkItemForNonPaper: async () => {},
+        updateCase: async ({ caseToUpdate }) => caseToUpdate,
+      }),
+      getUniqueId: () => 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+    };
   });
 
   it('Should throw an error if not authorized', async () => {
     let error;
+
+    applicationContext.getCurrentUser = () => {
+      return {};
+    };
+
     try {
-      applicationContext = {
-        environment: { stage: 'local' },
-        getCurrentUser: () => {
-          return {
-            name: 'adc',
-            role: User.ROLES.adc,
-            userId: 'g7d90c05-f6cd-442c-a168-202db587f16f',
-          };
-        },
-        getPersistenceGateway: () => ({
-          getCasesByLeadCaseId: async () => caseRecords,
-          getUserById: ({ userId }) => MOCK_USERS[userId],
-          //saveWorkItemForNonPaper: async () => caseRecord,
-          //updateCase: async () => caseRecord,
-        }),
-        getUniqueId: () => 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
-      };
       await fileExternalDocumentForConsolidatedInteractor({
         applicationContext,
         documentIds: ['dddddddd-1111-dddd-1111-dddddddddddd'],
@@ -86,34 +100,34 @@ describe('fileExternalDocumentForConsolidatedInteractor', () => {
     expect(caseRecords[0].documents.length).toEqual(0);
     expect(caseRecords[1].documents.length).toEqual(0);
 
-    await fileExternalDocumentForConsolidatedInteractor({
+    const result = await fileExternalDocumentForConsolidatedInteractor({
       applicationContext,
       documentIds: [documentId0],
-      documentMetaData: {
+      documentMetadata: {
         documentType: 'Memorandum in Support',
-        leadCaseId: caseId0,
       },
+      leadCaseId: caseId0,
     });
 
-    expect(caseRecords[0].documents[0].documentId).toEqual(documentId0);
-    expect(caseRecords[1].documents[0].documentId).toEqual(documentId0);
+    expect(result[0].documents[0].documentId).toEqual(documentId0);
+    expect(result[1].documents[0].documentId).toEqual(documentId0);
   });
 
   it('Should generate a docket record entry on each case in the consolidated set', async () => {
     expect(caseRecords[0].docketRecord.length).toEqual(0);
     expect(caseRecords[1].docketRecord.length).toEqual(0);
 
-    await fileExternalDocumentForConsolidatedInteractor({
+    const result = await fileExternalDocumentForConsolidatedInteractor({
       applicationContext,
       documentIds: [documentId0],
-      documentMetaData: {
+      documentMetadata: {
         documentType: 'Memorandum in Support',
-        leadCaseId: caseId0,
       },
+      leadCaseId: caseId0,
     });
 
-    expect(caseRecords[0].docketRecord[0].documentId).toEqual(documentId0);
-    expect(caseRecords[1].docketRecord[0].documentId).toEqual(documentId0);
+    expect(result[0].docketRecord[0].documentId).toEqual(documentId0);
+    expect(result[1].docketRecord[0].documentId).toEqual(documentId0);
   });
 
   it.skip('Should aggregate the filing parties for the docket record entry', async () => {
@@ -121,28 +135,30 @@ describe('fileExternalDocumentForConsolidatedInteractor', () => {
     await fileExternalDocumentForConsolidatedInteractor({
       applicationContext,
       documentIds: [documentId0],
-      documentMetaData: {
+      documentMetadata: {
         documentType: 'Memorandum in Support',
-        filingPartyNames: ['Guy Fieri', 'Enzo Ferrari'],
-        leadCaseId: caseId0,
       },
+      filingPartyNames: ['Guy Fieri', 'Enzo Ferrari'],
+      leadCaseId: caseId0,
     });
   });
 
   it('Should generate only ONE QC work item for the filing, to be found on the lead case document', async () => {
-    const leadCase = caseRecords.find(record => record.caseId === caseId0);
-    const nonLeadCase = caseRecords.find(record => record.caseId === caseId1);
-
-    await fileExternalDocumentForConsolidatedInteractor({
+    const result = await fileExternalDocumentForConsolidatedInteractor({
       applicationContext,
       documentIds: [documentId0],
-      documentMetaData: {
+      documentMetadata: {
         documentType: 'Memorandum in Support',
-        leadCaseId: caseId0,
       },
+      leadCaseId: caseId0,
     });
+
+    const leadCase = result.find(record => record.caseId === caseId0);
+    const nonLeadCase = result.find(record => record.caseId === caseId1);
 
     expect(leadCase.documents[0].workItems.length).toEqual(1);
     expect(nonLeadCase.documents[0].workItems.length).toEqual(0);
   });
+
+  // service on parties?
 });
