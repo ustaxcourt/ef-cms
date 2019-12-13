f0cc7182609ce678b4d64a89b92c9c154286b604
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-12-13 09:33:31 -0800

    test coverage

--- web-client/src/presenter/actions/setCaseDetailPageTabAction.test.js
+++ web-client/src/presenter/actions/setCaseDetailPageTabAction.test.js
@@ -10,4 +10,15 @@ describe('setDefaultDocumentDetailTab', () => {
     });
     expect(state.caseDetailPage.primaryTab).toEqual('DocketRecord');
   });
+
+  it('sets state.caseDetailPage.primaryTab to caseInformation and state.caseDetailPage.caseInformationTab to the passed in props.tab if isSecondary is true', async () => {
+    const { state } = await runAction(setCaseDetailPageTabAction, {
+      props: {
+        isSecondary: true,
+        tab: 'overview',
+      },
+    });
+    expect(state.caseDetailPage.primaryTab).toEqual('caseInformation');
+    expect(state.caseDetailPage.caseInformationTab).toEqual('overview');
+  });
 });
