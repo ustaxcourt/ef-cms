8558e0e0048cea2112d1022d4fc43c958b6f69ba
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-12-18 12:02:28 -0800

    jsdoc updates

--- web-client/src/presenter/actions/TrialSession/getJudgesCaseNoteForCaseAction.js
+++ web-client/src/presenter/actions/TrialSession/getJudgesCaseNoteForCaseAction.js
@@ -4,7 +4,7 @@ import { state } from 'cerebral';
  * Fetches the judge's case note for a case
  *
  * @param {object} providers the providers object
- * @param {object} providers.applicationContext needed for getting the getTrialSessionDetails use case
+ * @param {object} providers.applicationContext needed for getting the getJudgesCaseNoteInteractor use case
  * @param {object} providers.get the cerebral get function
  * @returns {object} contains the details of a judgesNote
  */
