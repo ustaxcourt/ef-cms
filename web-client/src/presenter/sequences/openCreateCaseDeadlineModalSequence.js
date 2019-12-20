60cc91810badb854818a4aa92287864f1a347249
Eric Sorenson <eric@sorenson.software>
2019-12-20 11:39:25 -0600

    fixed case deadline error message holdover

--- web-client/src/presenter/sequences/openCreateCaseDeadlineModalSequence.js
+++ web-client/src/presenter/sequences/openCreateCaseDeadlineModalSequence.js
@@ -1,9 +1,13 @@
+import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { clearFormAction } from '../actions/clearFormAction';
 import { clearModalStateAction } from '../actions/clearModalStateAction';
 import { setShowModalFactoryAction } from '../actions/setShowModalFactoryAction';
+import { stopShowValidationAction } from '../actions/stopShowValidationAction';
 
 export const openCreateCaseDeadlineModalSequence = [
+  stopShowValidationAction,
   clearModalStateAction,
   clearFormAction,
+  clearAlertsAction,
   setShowModalFactoryAction('CreateCaseDeadlineModalDialog'),
 ];
