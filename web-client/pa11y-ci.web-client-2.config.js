848cb13bed5285a2c3ec797ba6239e151e94443a
Joseph Sutton <jsutton@flexion.us>
2019-12-20 11:49:58 -0600

    added files...

---
+++ web-client/pa11y-ci.web-client-2.config.js
@@ -0,0 +1,24 @@
+const petitionsclerk = require('./pa11y/pa11y-petitionsclerk');
+const practitioner = require('./pa11y/pa11y-practitioner');
+const respondent = require('./pa11y/pa11y-respondent');
+
+const urls = [...petitionsclerk, ...practitioner, ...respondent];
+
+// see https://github.com/pa11y/pa11y#command-line-interface
+
+module.exports = {
+  defaults: {
+    chromeLaunchConfig: {
+      args: ['--no-sandbox'],
+    },
+    concurrency: 3,
+    debug: true,
+    'include-notices': true,
+    'include-warnings': true,
+    standard: 'WCAG2AA',
+    timeout: 30000,
+    useIncognitoBrowserContext: true,
+    wait: 5000,
+  },
+  urls,
+};
