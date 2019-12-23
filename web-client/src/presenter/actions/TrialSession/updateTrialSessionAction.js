ca962d9ac38b496644d37f067df4b1908b8a52ee
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-12-23 08:41:58 -0800

    pr feedback

--- web-client/src/presenter/actions/TrialSession/updateTrialSessionAction.js
+++ web-client/src/presenter/actions/TrialSession/updateTrialSessionAction.js
@@ -8,6 +8,7 @@ import { state } from 'cerebral';
  * @param {object} providers.applicationContext the application context
  * @param {Function} providers.get the cerebral get helper function
  * @param {object} providers.path the next object in the path
+ * @param {object} providers.props the cerebral props object
  * @returns {Promise<object>} the next path based on if creation was successful or error
  */
 export const updateTrialSessionAction = async ({
