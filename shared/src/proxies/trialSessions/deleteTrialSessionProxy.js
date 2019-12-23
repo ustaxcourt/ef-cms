bdef75468c1a38436530b0ddacc4e989cc4d8660
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-12-23 14:29:17 -0800

    1900: delete modal submit sequence, small fixes

---
+++ shared/src/proxies/trialSessions/deleteTrialSessionProxy.js
@@ -0,0 +1,19 @@
+const { remove } = require('../requests');
+
+/**
+ * deleteTrialSessionInteractor
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context
+ * @param {object} providers.trialSession the trial session data
+ * @returns {Promise<*>} the promise of the api call
+ */
+exports.deleteTrialSessionInteractor = ({
+  applicationContext,
+  trialSessionId,
+}) => {
+  return remove({
+    applicationContext,
+    endpoint: `/trial-sessions/${trialSessionId}`,
+  });
+};
