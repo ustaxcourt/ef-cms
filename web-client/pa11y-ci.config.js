e2714f4d748f6fec13fe3cc58fc068a72d566384
Matt Hopson <matthopson@gmail.com>
2019-12-24 11:24:18 -0600

    Test: Use /tmp for chromium

--- web-client/pa11y-ci.config.js
+++ web-client/pa11y-ci.config.js
@@ -37,7 +37,7 @@ if (process.env.CI) {
 module.exports = {
   defaults: {
     chromeLaunchConfig: {
-      args: ['--no-sandbox'],
+      args: ['--no-sandbox', '--disable-dev-shm-usage'],
     },
     concurrency: 3,
     debug: true,
