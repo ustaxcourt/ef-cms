ba63a98b8685f360fb9fd1595c0e9434bab8603f
Eric Sorenson <eric@sorenson.software>
2019-12-24 14:22:47 -0600

    reverted

--- web-client/pa11y-ci.web-client-1.config.js
+++ web-client/pa11y-ci.web-client-1.config.js
@@ -1,7 +1,9 @@
+const adc = require('./pa11y/pa11y-adc');
 const docketclerk = require('./pa11y/pa11y-docketclerk');
 const judge = require('./pa11y/pa11y-judge');
+const petitioner = require('./pa11y/pa11y-petitioner');
 
-const userUrls = [...docketclerk, ...judge];
+const userUrls = [...docketclerk, ...judge, ...petitioner, ...adc];
 
 const initialUrls = [
   'http://localhost:1234/',
