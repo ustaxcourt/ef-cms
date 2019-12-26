93759d0110da57893e0631a5915bbadc662721eb
Cody Seibert <cseibert113@gmail.com>
2019-12-26 11:09:50 -0500

    Update deploy-app.sh

--- iam/terraform/account-specific/bin/deploy-app.sh
+++ iam/terraform/account-specific/bin/deploy-app.sh
@@ -22,4 +22,4 @@ fi
 set -eo pipefail
 
 terraform init -backend=true -backend-config=bucket="${BUCKET}" -backend-config=key="${KEY}" -backend-config=dynamodb_table="${LOCK_TABLE}" -backend-config=region="${REGION}"
-TF_VAR_my_s3_state_bucket="${BUCKET}" TF_VAR_my_s3_state_key="${KEY}" terraform apply
+TF_VAR_my_s3_state_bucket="${BUCKET}" TF_VAR_my_s3_state_key="${KEY}" terraform apply --auto-approve
