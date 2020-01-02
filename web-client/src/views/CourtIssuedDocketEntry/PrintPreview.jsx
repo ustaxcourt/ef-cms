d53965301d0bffa83e03113122a31b9676a2e5be
Joseph Sutton <jsutton@flexion.us>
2020-01-02 14:14:09 -0600

    npm upgrade and applied prettier to file

--- web-client/src/views/CourtIssuedDocketEntry/PrintPreview.jsx
+++ web-client/src/views/CourtIssuedDocketEntry/PrintPreview.jsx
@@ -4,20 +4,17 @@ import { WarningNotification } from '../WarningNotification';
 import { connect } from '@cerebral/react';
 import React from 'react';
 
-export const PrintPreview = connect(
-  {},
-  () => {
-    return (
-      <>
-        <CaseDetailHeader hideActionButtons />
-        <section className="usa-section grid-container">
-          <h2>Print Paper Service Documents</h2>
-          <WarningNotification />
-          <div className="print-docket-record">
-            <PdfPreview />
-          </div>
-        </section>
-      </>
-    );
-  },
-);
+export const PrintPreview = connect({}, () => {
+  return (
+    <>
+      <CaseDetailHeader hideActionButtons />
+      <section className="usa-section grid-container">
+        <h2>Print Paper Service Documents</h2>
+        <WarningNotification />
+        <div className="print-docket-record">
+          <PdfPreview />
+        </div>
+      </section>
+    </>
+  );
+});
