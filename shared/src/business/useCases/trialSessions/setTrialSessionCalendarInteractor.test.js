60f4d4af3a23c5ed1e363eb61528c367ef7f0fb2
Matt Hopson <matthopson@gmail.com>
2020-01-03 14:19:54 -0700

    Tests

--- shared/src/business/useCases/trialSessions/setTrialSessionCalendarInteractor.test.js
+++ shared/src/business/useCases/trialSessions/setTrialSessionCalendarInteractor.test.js
@@ -107,45 +107,4 @@ describe('setTrialSessionCalendarInteractor', () => {
     expect(updateTrialSession.called).toEqual(true);
     expect(error).toBeUndefined();
   });
-
-  it('should generate a Notice of Trial and add it to the case', async () => {
-    let calendaredCase = {
-      ...MOCK_CASE,
-      caseId: '1f1aa3f7-e2e3-43e6-885d-4ce341588c76',
-      docketNumber: '102-19',
-    };
-
-    let eligibleCase = { ...MOCK_CASE };
-
-    applicationContext = {
-      getCurrentUser: () => {
-        return new User({
-          name: 'Docket Clerk',
-          role: User.ROLES.docketClerk,
-          userId: '6805d1ab-18d0-43ec-bafb-654e83405416',
-        });
-      },
-      getPersistenceGateway: () => ({
-        deleteCaseTrialSortMappingRecords: () => {},
-        getCalendaredCasesForTrialSession: () => [calendaredCase],
-        getEligibleCasesForTrialSession: () => [eligibleCase],
-        getTrialSessionById: () => MOCK_TRIAL,
-        saveDocument: saveDocumentMock,
-        updateCase: () => {},
-        updateTrialSession: jest.fn(),
-      }),
-      getUniqueId: () => 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
-      getUseCases: () => ({
-        generateNoticeOfTrialIssuedInteractor: generateNoticeOfTrialIssuedInteractorMock,
-      }),
-    };
-
-    await setTrialSessionCalendarInteractor({
-      applicationContext,
-      trialSessionId: '6805d1ab-18d0-43ec-bafb-654e83405416',
-    });
-
-    expect(generateNoticeOfTrialIssuedInteractorMock).toHaveBeenCalled();
-    expect(saveDocumentMock).toHaveBeenCalled();
-  });
 });
