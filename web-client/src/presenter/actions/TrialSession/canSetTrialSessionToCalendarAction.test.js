2ed9921def5138a37ba046c914d03aaa41821a08
Joseph Sutton <jsutton@flexion.us>
2020-01-03 11:26:43 -0600

    added unit test for action

---
+++ web-client/src/presenter/actions/TrialSession/canSetTrialSessionToCalendarAction.test.js
@@ -0,0 +1,58 @@
+import { canSetTrialSessionToCalendarAction } from './canSetTrialSessionToCalendarAction';
+import { presenter } from '../../presenter';
+import { runAction } from 'cerebral/test';
+
+describe('canSetTrialSessionToCalendarAction', () => {
+  let canSetTrialSessionAsCalendaredInteractorStub;
+  let pathNoStub;
+  let pathYesStub;
+
+  beforeEach(() => {
+    canSetTrialSessionAsCalendaredInteractorStub = jest.fn();
+    pathNoStub = jest.fn();
+    pathYesStub = jest.fn();
+
+    presenter.providers.applicationContext = {
+      getUseCases: () => ({
+        canSetTrialSessionAsCalendaredInteractor: canSetTrialSessionAsCalendaredInteractorStub,
+      }),
+    };
+
+    presenter.providers.path = {
+      no: pathNoStub,
+      yes: pathYesStub,
+    };
+  });
+
+  it('has invalid criteria to set trial session as calendared', async () => {
+    canSetTrialSessionAsCalendaredInteractorStub.mockReturnValue(false);
+
+    const result = await runAction(canSetTrialSessionToCalendarAction, {
+      modules: {
+        presenter,
+      },
+      state: {
+        trialSession: {},
+      },
+    });
+
+    expect(canSetTrialSessionAsCalendaredInteractorStub).toHaveBeenCalled();
+    expect(pathNoStub).toHaveBeenCalled();
+  });
+
+  it('has valid criteria to set trial session as calendared', async () => {
+    canSetTrialSessionAsCalendaredInteractorStub.mockReturnValue(true);
+
+    await runAction(canSetTrialSessionToCalendarAction, {
+      modules: {
+        presenter,
+      },
+      state: {
+        trialSession: {},
+      },
+    });
+
+    expect(canSetTrialSessionAsCalendaredInteractorStub).toHaveBeenCalled();
+    expect(pathYesStub).toHaveBeenCalled();
+  });
+});
