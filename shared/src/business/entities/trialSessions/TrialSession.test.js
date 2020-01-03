eec9fc537300f2f3ac556d75e7f05fa3ed7c9531
Joseph Sutton <jsutton@flexion.us>
2020-01-03 09:33:06 -0600

    removed address2 from calendar validation

--- shared/src/business/entities/trialSessions/TrialSession.test.js
+++ shared/src/business/entities/trialSessions/TrialSession.test.js
@@ -327,7 +327,6 @@ describe('TrialSession entity', () => {
         {
           ...VALID_TRIAL_SESSION,
           address1: '123 Flavor Ave',
-          address2: 'Ayy',
           city: 'Flavortown',
           judge: { name: 'Judge Armen' },
           postalCode: '12345',
@@ -341,12 +340,11 @@ describe('TrialSession entity', () => {
       expect(trialSession.canSetAsCalendared()).toBeTruthy();
     });
 
-    it('should not  able to set a trial session as calendared if one or more properties are not null', () => {
+    it('should NOT be able to set a trial session as calendared if one or more properties are not null', () => {
       const trialSession = new TrialSession(
         {
           ...VALID_TRIAL_SESSION,
           address1: '123 Flavor Ave',
-          address2: 'Ayy',
           city: 'Flavortown',
           judge: {},
           postalCode: '12345',
