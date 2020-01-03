c057ecd76c0604727cea36f4a4ef9d35cfee0ff3
Matt Hopson <matthopson@gmail.com>
2020-01-03 10:37:37 -0700

    Generate NoT on cases when setting calendar

--- shared/src/business/useCases/trialSessions/setTrialSessionCalendarInteractor.js
+++ shared/src/business/useCases/trialSessions/setTrialSessionCalendarInteractor.js
@@ -3,6 +3,7 @@ const {
   ROLE_PERMISSIONS,
 } = require('../../../authorization/authorizationClientService');
 const { Case } = require('../../entities/cases/Case');
+const { Document } = require('../../entities/Document');
 const { TrialSession } = require('../../entities/trialSessions/TrialSession');
 const { UnauthorizedError } = require('../../../errors/errors');
 
@@ -62,15 +63,63 @@ exports.setTrialSessionCalendarInteractor = async ({
     });
 
   /**
+   * generates a notice of trial session and adds to the case
+   *
+   * @param {object} caseEntity the case entity
+   * @returns {void}
+   */
+  const setNoticeOfTrialIssuedForCase = async caseEntity => {
+    const noticeOfTrialIssued = await applicationContext
+      .getUseCases()
+      .generateNoticeOfTrialIssuedInteractor({
+        applicationContext,
+        docketNumber: caseEntity.docketNumber,
+        trialSessionId: trialSessionEntity.trialSessionId,
+      });
+
+    // TODO: Add cover sheet
+
+    const newDocumentId = applicationContext.getUniqueId();
+
+    await applicationContext.getPersistenceGateway().saveDocument({
+      applicationContext,
+      document: noticeOfTrialIssued,
+      documentId: newDocumentId,
+    });
+
+    const documentTitle = `Notice of Trial on ${trialSession.startDate} at ${trialSession.startTime}`;
+
+    const noticeDocument = new Document(
+      {
+        caseId: caseEntity.caseId,
+        documentId: newDocumentId,
+        documentTitle,
+        documentType: Document.NOTICE_OF_TRIAL.documentType,
+        eventCode: Document.NOTICE_OF_TRIAL.eventCode,
+        filedBy: user.name,
+        processingStatus: 'complete',
+        userId: user.userId,
+      },
+      { applicationContext },
+    );
+
+    caseEntity.addDocument(noticeDocument);
+    caseEntity.setNoticeOfTrialDate();
+    // TODO: Set for service
+  };
+
+  /**
    * sets a manually added case as calendared with the trial session details
    *
    * @param {object} caseRecord the providers object
    * @returns {Promise} the promise of the updateCase call
    */
-  const setManuallyAddedCaseAsCalendared = caseRecord => {
+  const setManuallyAddedCaseAsCalendared = async caseRecord => {
     const caseEntity = new Case(caseRecord, { applicationContext });
 
     caseEntity.setAsCalendared(trialSessionEntity);
+
+    await setNoticeOfTrialIssuedForCase(caseEntity);
 
     return applicationContext.getPersistenceGateway().updateCase({
       applicationContext,
@@ -84,12 +133,14 @@ exports.setTrialSessionCalendarInteractor = async ({
    * @param {object} caseRecord the providers object
    * @returns {Promise} the promises of the updateCase and deleteCaseTrialSortMappingRecords calls
    */
-  const setTrialSessionCalendarForEligibleCase = caseRecord => {
+  const setTrialSessionCalendarForEligibleCase = async caseRecord => {
     const { caseId } = caseRecord;
     const caseEntity = new Case(caseRecord, { applicationContext });
 
     caseEntity.setAsCalendared(trialSessionEntity);
     trialSessionEntity.addCaseToCalendar(caseEntity);
+
+    await setNoticeOfTrialIssuedForCase(caseEntity);
 
     return Promise.all([
       applicationContext.getPersistenceGateway().updateCase({
