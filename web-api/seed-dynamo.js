e19b7bcaa09d32b5a8d80385c5adfa8adc5fa2f6
William Bagayoko <wbagayoko@gmail.com>
2020-01-06 11:08:30 -0600

    add a user

--- web-api/seed-dynamo.js
+++ web-api/seed-dynamo.js
@@ -1,5 +1,10 @@
 const AWS = require('aws-sdk');
+const createApplicationContext = require('./src/applicationContext');
 const seedEntries = require('./storage/fixtures/seed');
+//const { getUserFromAuthHeader } = require('./src/middleware/apiGatewayHelper');
+const {
+  createUserRecords,
+} = require('../shared/src/persistence/dynamo/users/createUser.js');
 
 const args = process.argv.slice(2);
 
@@ -13,6 +18,22 @@ const client = new AWS.DynamoDB.DocumentClient({
 });
 
 const main = async () => {
+  const user = {
+    role: 'admin',
+  };
+  const applicationContext = createApplicationContext(user);
+
+  const result = await createUserRecords({
+    applicationContext,
+    user: {
+      email: 'docketclerk',
+      name: 'Test Docketclerk',
+      role: 'docketclerk',
+      section: 'docket',
+    },
+    userId: '2805d1ab-18d0-43ec-bafb-654e83405416',
+  });
+
   let entries;
   if (args[0]) {
     // eslint-disable-next-line security/detect-non-literal-require
