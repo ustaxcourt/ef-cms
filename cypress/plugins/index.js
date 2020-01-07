656f3a6d3e8d3cbec96d29c1202629ca6be36067
William Bagayoko <wbagayoko@gmail.com>
2020-01-07 16:10:35 -0600

    update task

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,9 +11,9 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-import { reseedDatabase } from './database';
+const { reseedDatabase } = require('./database');
 
-module.exports = (on, config) => {
+module.exports = on => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
   on('task', {
