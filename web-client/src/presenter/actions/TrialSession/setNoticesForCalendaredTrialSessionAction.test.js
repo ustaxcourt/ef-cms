3169a00239173464427f94a81da13b2c89456fe5
Joseph Sutton <jsutton@flexion.us>
2020-01-07 15:33:22 -0600

    added test coverage

--- web-client/src/presenter/actions/TrialSession/setNoticesForCalendaredTrialSessionAction.test.js
+++ web-client/src/presenter/actions/TrialSession/setNoticesForCalendaredTrialSessionAction.test.js
@@ -4,18 +4,38 @@ import { setNoticesForCalendaredTrialSessionAction } from './setNoticesForCalend
 
 describe('setNoticesForCalendaredTrialSessionAction', () => {
   let setNoticesForCalendaredTrialSessionInteractorStub;
+  let pathPaperStub;
+  let pathElectronicStub;
+  let createObjectURLStub;
 
   beforeEach(() => {
-    setNoticesForCalendaredTrialSessionInteractorStub = jest.fn();
+    setNoticesForCalendaredTrialSessionInteractorStub = jest
+      .fn()
+      .mockReturnValue(null);
+    pathPaperStub = jest.fn();
+    pathElectronicStub = jest.fn();
+    createObjectURLStub = jest.fn();
+
+    presenter.providers.router = {
+      createObjectURL: () => {
+        createObjectURLStub();
+        return '123456-abcdef';
+      },
+    };
 
     presenter.providers.applicationContext = {
       getUseCases: () => ({
         setNoticesForCalendaredTrialSessionInteractor: setNoticesForCalendaredTrialSessionInteractorStub,
       }),
     };
+
+    presenter.providers.path = {
+      electronic: pathElectronicStub,
+      paper: pathPaperStub,
+    };
   });
 
-  it('sets notices for the calendared trial session', async () => {
+  it('sets notices for the calendared trial session with all electronic cases', async () => {
     await runAction(setNoticesForCalendaredTrialSessionAction, {
       modules: {
         presenter,
@@ -30,5 +50,28 @@ describe('setNoticesForCalendaredTrialSessionAction', () => {
     expect(
       setNoticesForCalendaredTrialSessionInteractorStub,
     ).toHaveBeenCalled();
+    expect(pathElectronicStub).toHaveBeenCalled();
+  });
+
+  it('sets notices for the calendared trial session with at least one paper case', async () => {
+    setNoticesForCalendaredTrialSessionInteractorStub.mockReturnValue([
+      'pdf-bytes',
+    ]);
+
+    await runAction(setNoticesForCalendaredTrialSessionAction, {
+      modules: {
+        presenter,
+      },
+      state: {
+        trialSession: {
+          trialSessionId: 'abc-123',
+        },
+      },
+    });
+
+    expect(
+      setNoticesForCalendaredTrialSessionInteractorStub,
+    ).toHaveBeenCalled();
+    expect(pathPaperStub).toHaveBeenCalled();
   });
 });
