7adc53c04cdbfbf93120b906a1ea998b42dd1111
Cody Seibert <cseibert113@gmail.com>
2020-01-08 15:34:19 -0500

    removing ods logic

--- web-client/src/presenter/computeds/editPetitionerInformationHelper.js
+++ web-client/src/presenter/computeds/editPetitionerInformationHelper.js
@@ -13,22 +13,8 @@ export const editPetitionerInformationHelper = (get, applicationContext) => {
   const partyType = get(state.form.partyType);
   const showContacts = showContactsHelper(partyType, PARTY_TYPES);
 
-  let showOwnershipDisclosureStatement = false;
-
-  if (
-    [
-      PARTY_TYPES.partnershipAsTaxMattersPartner,
-      PARTY_TYPES.partnershipOtherThanTaxMatters,
-      PARTY_TYPES.partnershipBBA,
-      PARTY_TYPES.corporation,
-    ].includes(partyType)
-  ) {
-    showOwnershipDisclosureStatement = true;
-  }
-
   return {
     partyTypes: PARTY_TYPES,
-    showOwnershipDisclosureStatement,
     showPrimaryContact: showContacts.contactPrimary,
     showSecondaryContact: showContacts.contactSecondary,
   };
