e0e6ca8289b3983f0576085b4e36e63b62a780a6
William Bagayoko <wbagayoko@gmail.com>
2020-01-08 14:00:00 -0600

    lint

--- shared/src/business/useCases/trialSessions/setNoticesForCalendaredTrialSessionInteractor.test.js
+++ shared/src/business/useCases/trialSessions/setNoticesForCalendaredTrialSessionInteractor.test.js
@@ -88,7 +88,7 @@ describe('setNoticesForCalendaredTrialSessionInteractor', () => {
         sendBulkTemplatedEmail: sendBulkTemplatedEmailMock,
       }),
       getPersistenceGateway: () => ({
-        deleteCaseTrialSortMappingRecords: () => { },
+        deleteCaseTrialSortMappingRecords: () => {},
         getCalendaredCasesForTrialSession: () => calendaredCases,
         getTrialSessionById: () => MOCK_TRIAL,
         saveDocument: saveDocumentMock,
@@ -100,7 +100,7 @@ describe('setNoticesForCalendaredTrialSessionInteractor', () => {
             }
           });
         },
-        updateTrialSession: () => { },
+        updateTrialSession: () => {},
       }),
       getUniqueId: () => 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
       getUseCaseHelpers: () => ({
