ca234f5f7d8dddd1b3497567f8fccc138ee28143
Kris Koskelin <kkoskelin@flexion.us>
2020-01-09 15:16:36 -0600

    3917: updating template condition

--- web-client/src/presenter/actions/CourtIssuedOrder/createOrderAction.js
+++ web-client/src/presenter/actions/CourtIssuedOrder/createOrderAction.js
@@ -22,11 +22,16 @@ export const createOrderAction = async ({ applicationContext, get }) => {
     '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;',
   );
   const caseCaption = get(state.caseDetail.caseCaption) || '';
+  const isOrderEvent = get(state.form.eventCode) == 'NOT'; // 'NOT' === 'notice'
   let caseCaptionNames = applicationContext.getCaseCaptionNames(caseCaption);
   let caseCaptionPostfix = '';
   if (caseCaptionNames !== caseCaption) {
     caseCaptionNames += ', ';
     caseCaptionPostfix = caseCaption.replace(caseCaptionNames, '');
+  }
+  let signatureForNotice = '';
+  if (isOrderEvent) {
+    signatureForNotice = `<p>${applicationContext.getClerkOfCourtNameForSigning()}<br />Clerk of the Court</p>`;
   }
   const docketNumberWithSuffix = get(
     state.formattedCaseDetail.docketNumberWithSuffix,
@@ -39,6 +44,7 @@ export const createOrderAction = async ({ applicationContext, get }) => {
       '#docketNumber': docketNumberWithSuffix,
       '#orderBody': richText,
       '#orderTitleHeader': documentTitle,
+      '#signature': signatureForNotice,
     },
     orderTemplate,
   );
