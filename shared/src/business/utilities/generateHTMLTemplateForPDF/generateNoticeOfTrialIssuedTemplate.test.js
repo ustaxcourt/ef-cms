c649c4d1355805cf9553cb352e52f77ef9ef9d8a
William Bagayoko <wbagayoko@gmail.com>
2020-01-10 17:24:16 -0600

    1193 update test

--- shared/src/business/utilities/generateHTMLTemplateForPDF/generateNoticeOfTrialIssuedTemplate.test.js
+++ shared/src/business/utilities/generateHTMLTemplateForPDF/generateNoticeOfTrialIssuedTemplate.test.js
@@ -46,8 +46,8 @@ describe('generateNoticeOfTrialIssuedTemplate', () => {
     expect(result.indexOf('12345')).toBeGreaterThan(-1);
     expect(result.indexOf('Test Judge')).toBeGreaterThan(-1);
     expect(result.indexOf('NOTICE SETTING CASE FOR TRIAL')).toBeGreaterThan(-1);
-    expect(result.indexOf('beginning at 10:00 AM on 2/2/2020')).toBeGreaterThan(
-      -1,
-    );
+    expect(
+      result.indexOf('beginning at 10:00 AM on 02/02/2020'),
+    ).toBeGreaterThan(-1);
   });
 });
