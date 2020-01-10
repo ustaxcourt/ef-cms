d8c502304950c811b016b921b4d47000be37f3ec
Eric Sorenson <eric@sorenson.software>
2020-01-10 09:41:29 -0600

    no longer needed

--- shared/src/business/utilities/generateHTMLTemplateForPDF/generateNoticeOfTrialIssuedTemplate.js
+++ shared/src/business/utilities/generateHTMLTemplateForPDF/generateNoticeOfTrialIssuedTemplate.js
@@ -1,3 +1,4 @@
+const template = require('./noticeOfTrialIssued.pug_');
 const { generateHTMLTemplateForPDF } = require('./generateHTMLTemplateForPDF');
 
 /**
@@ -19,85 +20,15 @@ const generateNoticeOfTrialIssuedTemplate = async ({
     trialInfo,
   } = content;
 
-  const main = `
-    <div class="trial-at">
-      <table>
-        <thead>
-          <tr>
-            <th>Trial At:</th>
-          </tr>
-        </thead>
+  const pug = applicationContext.getPug();
 
-        <tbody>
-          <tr>
-            <td>
-              <p>${trialInfo.address2}</p>
-              <p>${trialInfo.courthouseName}</p>
-              <p>${trialInfo.address1}</p>
-              <p>
-                ${trialInfo.city}, ${trialInfo.state} ${trialInfo.postalCode}
-              </p>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <br /><br />
-    </div>
-
-    <div class="judge">
-      <table>
-        <thead>
-          <tr>
-            <th>Judge:</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td>
-              ${trialInfo.judge}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <br /><br />
-    </div>
-
-    <h2>NOTICE SETTING CASE FOR TRIAL</h2>
-    <p>This case is set for trial at the Trial Session beginning at ${trialInfo.startTime} on ${trialInfo.startDate}. The calendar for that Session will be called at that date and time, and the parties are expected to be present and to be prepared to try the case.  Your failure to appear may result in dismissal of the case and entry of decision against you.</p>
-    <p>The Court will set the time for each trial at the end of the calendar call.  In setting trial times the Court attempts to accommodate the parties, but the final determination of trial times rests in the Court’s discretion. </p>
-    <p>Information about presenting a case in the Tax Court can be found at <a target="_blank" href="www.ustaxcourt.gov">www.ustaxcourt.gov</a>.</p>
-    <p>The parties should contact each other promptly and cooperate fully so that the necessary steps can be taken to comply with these requirements.  Your failure to cooperate may also result in dismissal of the case and entry of decision against you.</p>
-
-    <div class="signature">
-      <p>Stephanie A. Servoss<p>
-      <p>Clerk of the Court</p>
-    </div>
-  `;
-
-  const styles = `
-    .trial-at {
-      width: 60%;
-      float: left;
-    }
-
-    .judge {
-      width: 35%;
-      float: right;
-    }
-
-    .signature {
-      margin-top:40px;
-      text-align: right;
-    }
-
-    th, td {
-      font-size: 10px;
-    }
-    th {
-      font-weight: 600;
-    }
-  `;
+  const compiledFunction = pug.compile(template);
+  const main = compiledFunction({
+    caption,
+    captionPostfix,
+    docketNumberWithSuffix,
+    trialInfo,
+  });
 
   const templateContent = {
     caption,
@@ -106,14 +37,9 @@ const generateNoticeOfTrialIssuedTemplate = async ({
     main,
   };
 
-  const options = {
-    styles,
-  };
-
   return await generateHTMLTemplateForPDF({
     applicationContext,
     content: templateContent,
-    options,
   });
 };
 
