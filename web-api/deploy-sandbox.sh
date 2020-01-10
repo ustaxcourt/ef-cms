7087b232c1f32ca1b02f491ec9e28153b2f0d604
Eric Sorenson <eric@sorenson.software>
2020-01-10 16:22:47 -0600

    add debug

--- web-api/deploy-sandbox.sh
+++ web-api/deploy-sandbox.sh
@@ -46,7 +46,7 @@ function prepare_serverless() {
 function run_development() {
 
     if [[ "${ENVIRONMENT}" == "dev" ]] || [[ "${ENVIRONMENT}" == "staging" ]] || [[ "${ENVIRONMENT}" == "test" ]] || [[ "${ENVIRONMENT}" == "prod" ]]; then
-        echo "Cannot use this script to deploy to dev, staging or prod."
+        echo "Cannot use this script to deploy to dev, staging, test, or prod."
         exit 1
     fi
 
