40f96bd2839816638dafdaeadd36d9164fae7ce2
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-01-10 11:07:26 -0800

    Devex: store failed ES indexing records and retry

---
+++ shared/src/business/useCases/reprocessFailedRecordsInteractor.js
@@ -0,0 +1,55 @@
+const AWS = require('aws-sdk');
+const { createISODateString } = require('../utilities/DateHandler');
+
+/**
+ * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context
+ * @param {Array<object>} providers.recordsToProcess the records to process
+ * @returns {object} the results of all the index calls for logging
+ */
+exports.reprocessFailedRecordsInteractor = async ({ applicationContext }) => {
+  applicationContext.logger.info('Time', createISODateString());
+
+  const recordsToProcess = await applicationContext
+    .getPersistenceGateway()
+    .getElasticsearchReindexRecords({ applicationContext });
+
+  if (recordsToProcess.length) {
+    const searchClient = applicationContext.getSearchClient();
+
+    const results = [];
+
+    for (const record of recordsToProcess) {
+      try {
+        const fullRecord = await applicationContext
+          .getPersistenceGateway()
+          .getRecord({
+            applicationContext,
+            recordPk: record.recordPk,
+            recordSk: record.recordSk,
+          });
+
+        results.push(
+          await searchClient.index({
+            body: { ...AWS.DynamoDB.Converter.marshall(fullRecord) },
+            id: record.recordPk,
+            index: 'efcms',
+          }),
+        );
+
+        await applicationContext
+          .getPersistenceGateway()
+          .deleteElasticsearchReindexRecord({
+            applicationContext,
+            recordPk: record.recordPk,
+            recordSk: record.recordSk,
+          });
+      } catch (e) {
+        applicationContext.logger.info('Error', e);
+      }
+    }
+
+    applicationContext.logger.info('Time', createISODateString());
+    return results;
+  }
+};
