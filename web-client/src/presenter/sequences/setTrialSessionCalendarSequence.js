29a36332aa36fb6d3b79745c136eace14df4228b
Joseph Sutton <jsutton@flexion.us>
2020-01-14 09:22:04 -0600

    added confirmation modal back

--- web-client/src/presenter/sequences/setTrialSessionCalendarSequence.js
+++ web-client/src/presenter/sequences/setTrialSessionCalendarSequence.js
@@ -1,5 +1,6 @@
 import { canSetTrialSessionToCalendarAction } from '../actions/TrialSession/canSetTrialSessionToCalendarAction';
 import { clearAlertsAction } from '../actions/clearAlertsAction';
+import { clearModalAction } from '../actions/clearModalAction';
 import { getCalendaredCasesForTrialSessionAction } from '../actions/TrialSession/getCalendaredCasesForTrialSessionAction';
 import { getSetTrialSessionCalendarAlertSuccessAction } from '../actions/TrialSession/getSetTrialSessionCalendarAlertSuccessAction';
 import { getTrialSessionDetailsAction } from '../actions/TrialSession/getTrialSessionDetailsAction';
@@ -21,7 +22,7 @@ export const setTrialSessionCalendarSequence = [
   setWaitingForResponseAction,
   canSetTrialSessionToCalendarAction,
   {
-    no: [setAlertWarningAction],
+    no: [setAlertWarningAction, clearModalAction],
     yes: [
       setTrialSessionCalendarAction,
       getTrialSessionDetailsAction,
