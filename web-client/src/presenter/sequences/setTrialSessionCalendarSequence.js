1b7d22df20898459c0fb378182fce9ee685a9315
William Bagayoko <wbagayoko@gmail.com>
2020-01-14 13:37:26 -0600

    1193 Show Warning Before Confirm Modal

--- web-client/src/presenter/sequences/setTrialSessionCalendarSequence.js
+++ web-client/src/presenter/sequences/setTrialSessionCalendarSequence.js
@@ -1,10 +1,9 @@
-import { canSetTrialSessionToCalendarAction } from '../actions/TrialSession/canSetTrialSessionToCalendarAction';
 import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { clearModalAction } from '../actions/clearModalAction';
 import { getCalendaredCasesForTrialSessionAction } from '../actions/TrialSession/getCalendaredCasesForTrialSessionAction';
 import { getSetTrialSessionCalendarAlertSuccessAction } from '../actions/TrialSession/getSetTrialSessionCalendarAlertSuccessAction';
 import { getTrialSessionDetailsAction } from '../actions/TrialSession/getTrialSessionDetailsAction';
-import { parallel, set } from 'cerebral/factories';
+import { set } from 'cerebral/factories';
 import { setAlertSuccessAction } from '../actions/setAlertSuccessAction';
 import { setAlertWarningAction } from '../actions/setAlertWarningAction';
 import { setCalendaredCasesOnTrialSessionAction } from '../actions/TrialSession/setCalendaredCasesOnTrialSessionAction';
@@ -20,32 +19,24 @@ import { unsetWaitingForResponseAction } from '../actions/unsetWaitingForRespons
 export const setTrialSessionCalendarSequence = [
   clearAlertsAction,
   setWaitingForResponseAction,
-  parallel([
-    canSetTrialSessionToCalendarAction,
-    {
-      no: [setAlertWarningAction],
-      yes: [
-        setTrialSessionCalendarAction,
-        getTrialSessionDetailsAction,
-        setTrialSessionDetailsAction,
-        getCalendaredCasesForTrialSessionAction,
-        setCalendaredCasesOnTrialSessionAction,
-        setNoticesForCalendaredTrialSessionAction,
-        {
-          electronic: [
-            getSetTrialSessionCalendarAlertSuccessAction,
-            setAlertSuccessAction,
-          ],
-          paper: [
-            ...setPdfPreviewUrlSequence,
-            setTrialSessionCalendarAlertWarningAction,
-            setAlertWarningAction,
-            set(state.currentPage, 'SimplePdfPreviewPage'),
-          ],
-        },
-      ],
-    },
-    clearModalAction,
-  ]),
+  setTrialSessionCalendarAction,
+  getTrialSessionDetailsAction,
+  setTrialSessionDetailsAction,
+  getCalendaredCasesForTrialSessionAction,
+  setCalendaredCasesOnTrialSessionAction,
+  setNoticesForCalendaredTrialSessionAction,
+  {
+    electronic: [
+      getSetTrialSessionCalendarAlertSuccessAction,
+      setAlertSuccessAction,
+    ],
+    paper: [
+      ...setPdfPreviewUrlSequence,
+      setTrialSessionCalendarAlertWarningAction,
+      setAlertWarningAction,
+      set(state.currentPage, 'SimplePdfPreviewPage'),
+    ],
+  },
+  clearModalAction,
   unsetWaitingForResponseAction,
 ];
