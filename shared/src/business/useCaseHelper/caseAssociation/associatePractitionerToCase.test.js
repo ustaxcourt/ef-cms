893008254eed56dc8ce4ac5197f9fdd80bfa8e48
Eric Sorenson <eric@sorenson.software>
2020-01-15 17:05:04 -0600

    updating tests to handle newly required fields

--- shared/src/business/useCaseHelper/caseAssociation/associatePractitionerToCase.test.js
+++ shared/src/business/useCaseHelper/caseAssociation/associatePractitionerToCase.test.js
@@ -11,6 +11,15 @@ describe('associatePractitionerToCase', () => {
     caseCaption: 'Case Caption',
     caseId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
     docketNumber: '123-19',
+    docketRecord: [
+      {
+        description: 'first record',
+        documentId: '8675309b-18d0-43ec-bafb-654e83405411',
+        eventCode: 'P',
+        filingDate: '2018-03-01T00:01:00.000Z',
+        index: 1,
+      },
+    ],
   };
 
   it('should not add mapping if already there', async () => {
