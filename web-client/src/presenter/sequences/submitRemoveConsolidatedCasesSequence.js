5b836cc07ac5673e91b0bbc03c1f37b9ce524d65
Kris Koskelin <kkoskelin@flexion.us>
2020-01-17 11:47:46 -0600

    3573: sequence, action, proxy to remove cases from consolidated list

--- web-client/src/presenter/sequences/submitRemoveConsolidatedCasesSequence.js
+++ web-client/src/presenter/sequences/submitRemoveConsolidatedCasesSequence.js
@@ -1,31 +1,27 @@
-import { addConsolidatedCaseAction } from '../actions/caseConsolidation/addConsolidatedCaseAction';
-import { canConsolidateAction } from '../actions/caseConsolidation/canConsolidateAction';
+import { canUnconsolidateAction } from '../actions/caseConsolidation/canUnconsolidateAction';
 import { clearModalAction } from '../actions/clearModalAction';
 import { clearModalStateAction } from '../actions/clearModalStateAction';
 import { getConsolidatedCasesByCaseAction } from '../actions/caseConsolidation/getConsolidatedCasesByCaseAction';
-import { primePropsForCanConsolidateAction } from '../actions/caseConsolidation/primePropsForCanConsolidateAction';
 import { refreshCaseAction } from '../actions/refreshCaseAction';
-import { setAddConsolidatedCaseSuccessMessageAction } from '../actions/caseConsolidation/setAddConsolidatedCaseSuccessMessageAction';
+import { removeConsolidatedCasesAction } from '../actions/caseConsolidation/removeConsolidatedCasesAction';
 import { setAlertSuccessAction } from '../actions/setAlertSuccessAction';
 import { setConsolidatedCasesForCaseAction } from '../actions/caseConsolidation/setConsolidatedCasesForCaseAction';
 import { setModalErrorAction } from '../actions/setModalErrorAction';
 import { setWaitingForResponseAction } from '../actions/setWaitingForResponseAction';
 import { unsetWaitingForResponseAction } from '../actions/unsetWaitingForResponseAction';
 
-export const submitAddConsolidatedCaseSequence = [
-  primePropsForCanConsolidateAction,
-  canConsolidateAction,
+export const submitRemoveConsolidatedCasesSequence = [
+  canUnconsolidateAction,
   {
     error: [setModalErrorAction],
     success: [
       setWaitingForResponseAction,
-      addConsolidatedCaseAction,
+      removeConsolidatedCasesAction,
       refreshCaseAction,
       getConsolidatedCasesByCaseAction,
       setConsolidatedCasesForCaseAction,
       clearModalAction,
       clearModalStateAction,
-      setAddConsolidatedCaseSuccessMessageAction,
       setAlertSuccessAction,
       unsetWaitingForResponseAction,
     ],
