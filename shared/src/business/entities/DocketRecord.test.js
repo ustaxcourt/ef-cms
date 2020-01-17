256e7f77335cba2f1511b072f1009310b9ba674d
Matt Hopson <matthopson@gmail.com>
2020-01-17 10:33:00 -0700

    Update docket record validation tests

--- shared/src/business/entities/DocketRecord.test.js
+++ shared/src/business/entities/DocketRecord.test.js
@@ -2,23 +2,69 @@ const { DocketRecord } = require('./DocketRecord');
 
 describe('DocketRecord', () => {
   describe('validation', () => {
-    it('returns false if a filingDate cannot be in the future.', () => {
+    it('fails validation if a filingDate is in the future.', () => {
       expect(
         new DocketRecord({
+          description: 'Test Docket Record',
+          eventCode: 'O',
           filingDate: new Date('9000-01-01').toISOString(),
+          index: 0,
         }).isValid(),
       ).toBeFalsy();
     });
 
-    it('returns true if a filingDate is not in the future', () => {
+    it('passes validation if a filingDate is not in the future', () => {
       expect(
         new DocketRecord({
+          description: 'Test Docket Record',
+          eventCode: 'O',
           filingDate: new Date('2000-01-01').toISOString(),
+          index: 0,
         }).isValid(),
       ).toBeTruthy();
     });
 
-    it('returns false if nothing is passed in', () => {
+    it('fails validation if a filingDate is omitted', () => {
+      expect(
+        new DocketRecord({
+          description: 'Test Docket Record',
+          eventCode: 'O',
+          index: 0,
+        }).isValid(),
+      ).toBeFalsy();
+    });
+
+    it('fails validation if a description is omitted', () => {
+      expect(
+        new DocketRecord({
+          eventCode: 'O',
+          filingDate: new Date('2000-01-01').toISOString(),
+          index: 0,
+        }).isValid(),
+      ).toBeFalsy();
+    });
+
+    it('fails validation if an eventCode is omitted', () => {
+      expect(
+        new DocketRecord({
+          description: 'Test Docket Record',
+          filingDate: new Date('2000-01-01').toISOString(),
+          index: 0,
+        }).isValid(),
+      ).toBeFalsy();
+    });
+
+    it('fails validation if an index is omitted', () => {
+      expect(
+        new DocketRecord({
+          description: 'Test Docket Record',
+          eventCode: 'O',
+          filingDate: new Date('2000-01-01').toISOString(),
+        }).isValid(),
+      ).toBeFalsy();
+    });
+
+    it('fails validation if nothing is passed in', () => {
       expect(new DocketRecord({}).isValid()).toBeFalsy();
     });
   });
