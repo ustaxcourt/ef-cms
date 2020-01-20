c064616ec76e9e8d19b45fa9b9e813286eec00c5
Matt Hopson <matthopson@gmail.com>
2020-01-20 10:21:06 -0700

    Add update docket entry meta tests

---
+++ shared/src/business/useCases/docketEntry/updateDocketEntryMetaInteractor.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context
+ * @param {object} providers.docketNumber the docket number of the case to be updated
+ * @param {object} providers.docketRecordIndex the index of the docket record entry to be updated
+ * @param {object} providers.docketEntryMeta the docket entry metadata
+ * @returns {object} the updated case after the documents are added
+ */
+exports.updateDocketEntryMetaInteractor = async ({
+  applicationContext,
+  docketEntryMeta,
+  docketNumber,
+  docketRecordIndex,
+}) => {};
