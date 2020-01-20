cd209f213b66d22da6e7be793633bb1154d60541
Matt Hopson <matthopson@gmail.com>
2020-01-20 12:58:34 -0700

    Flesh out interactor

--- shared/src/business/useCases/docketEntry/updateDocketEntryMetaInteractor.js
+++ shared/src/business/useCases/docketEntry/updateDocketEntryMetaInteractor.js
@@ -1,3 +1,13 @@
+const {
+  isAuthorized,
+  ROLE_PERMISSIONS,
+} = require('../../../authorization/authorizationClientService');
+const { Case } = require('../../entities/cases/Case');
+const { DocketRecord } = require('../../entities/DocketRecord');
+const { Document } = require('../../entities/Document');
+const { NotFoundError } = require('../../../errors/errors');
+const { UnauthorizedError } = require('../../../errors/errors');
+
 /**
  *
  * @param {object} providers the providers object
@@ -12,4 +22,59 @@ exports.updateDocketEntryMetaInteractor = async ({
   docketEntryMeta,
   docketNumber,
   docketRecordIndex,
-}) => {};
+}) => {
+  const user = applicationContext.getCurrentUser();
+
+  if (!isAuthorized(user, ROLE_PERMISSIONS.EDIT_DOCKET_ENTRY)) {
+    throw new UnauthorizedError('Unauthorized to update docket entry');
+  }
+
+  const caseToUpdate = applicationContext
+    .getPersistenceGateway()
+    .getCaseByDocketNumber({
+      applicationContext,
+      docketNumber,
+    });
+
+  if (!caseToUpdate) {
+    throw new NotFoundError(`Case ${docketNumber} was not found.`);
+  }
+
+  const caseEntity = new Case(caseToUpdate, { applicationContext });
+
+  const docketRecordEntry = caseEntity.docketRecord[docketRecordIndex];
+
+  const { description, filedBy, servedAt, servedParties } = docketEntryMeta;
+
+  const docketRecordEntity = new DocketRecord({
+    ...docketRecordEntry,
+    description: description || docketRecordEntry.description,
+    filedBy: filedBy || docketRecordEntry.filedBy,
+  });
+
+  if (servedAt || servedParties) {
+    const documentDetail = caseEntity.getDocumentById({
+      documentId: docketRecordEntity.documentId,
+    });
+
+    const documentEntity = new Document(
+      {
+        ...documentDetail,
+        servedAt: servedAt || documentDetail.servedAt,
+        servedParties: servedParties || documentDetail.servedParties,
+      },
+      { applicationContext },
+    );
+
+    caseEntity.updateDocument(documentEntity);
+  }
+
+  caseEntity.updateDocketRecordEntry(docketRecordEntity);
+
+  await applicationContext.getPersistenceGateway().updateCase({
+    applicationContext,
+    caseToUpdate: caseEntity.validate().toRawObject(),
+  });
+
+  return caseEntity.toRawObject();
+};
