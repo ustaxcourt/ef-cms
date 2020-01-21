9e27b5e21a4303c2d7b33efb6257c1ec78d6e812
Matt Hopson <matthopson@gmail.com>
2020-01-21 11:43:31 -0700

    Edit docket entry meta integration

---
+++ web-client/integration-tests/journey/docketClerkVerifiesDocketEntryMetaUpdates.js
@@ -0,0 +1,13 @@
+export default (test, docketRecordIndex = 1) => {
+  return it('docket clerk verifies docket entry meta update', async () => {
+    const caseDetail = test.getState('caseDetail');
+
+    const docketRecordEntry = caseDetail.docketRecord.find(
+      entry => entry.index === docketRecordIndex,
+    );
+
+    expect(docketRecordEntry.description).toEqual(
+      'New Docket Entry Description',
+    );
+  });
+};
