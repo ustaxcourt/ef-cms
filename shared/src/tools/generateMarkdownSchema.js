70f6260049274ec2f3f3a623f63549cd4cb4e65e
Joseph Sutton <jsutton@flexion.us>
2020-01-21 12:06:12 -0600

    updated jsdoc

--- shared/src/tools/generateMarkdownSchema.js
+++ shared/src/tools/generateMarkdownSchema.js
@@ -1,9 +1,12 @@
+const fs = require('fs');
 const { Case } = require('../business/entities/cases/Case');
 const { DocketRecord } = require('../business/entities/DocketRecord');
 const { Document } = require('../business/entities/Document');
 
 const entitiesDir = `${__dirname}/../../../docs/entities`;
 
-Case.generateSchemaMarkdown(entitiesDir);
-Document.generateSchemaMarkdown(entitiesDir);
-DocketRecord.generateSchemaMarkdown(entitiesDir);
+const schema = JSON.stringify(Case.getSchema().describe(), null, 2);
+
+console.log('schema', schema);
+
+fs.writeFileSync('schema.json', schema);
