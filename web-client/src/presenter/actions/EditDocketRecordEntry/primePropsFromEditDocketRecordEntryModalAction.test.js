4b02fb8824552565165864dd7032eddf19d6890e
Matt Hopson <matthopson@gmail.com>
2020-01-21 07:26:55 -0700

    Add test for param filter

--- web-client/src/presenter/actions/EditDocketRecordEntry/primePropsFromEditDocketRecordEntryModalAction.test.js
+++ web-client/src/presenter/actions/EditDocketRecordEntry/primePropsFromEditDocketRecordEntryModalAction.test.js
@@ -25,4 +25,34 @@ describe('primePropsFromEditDocketRecordEntryModalAction', () => {
       fromModal: true,
     });
   });
+
+  it('should filter empty strings from the given params in state.modal.form', async () => {
+    const result = await runAction(
+      primePropsFromEditDocketRecordEntryModalAction,
+      {
+        modules: { presenter },
+        state: {
+          modal: {
+            caseId: '456',
+            docketRecordIndex: 1,
+            form: {
+              someEmptyString: '', // should be removed
+              someObj: {
+                someNestedEmptyString: '', // should be removed
+              },
+              something: '123',
+            },
+            fromModal: true,
+          },
+        },
+      },
+    );
+
+    expect(result.output).toEqual({
+      caseId: '456',
+      docketRecordEntry: { someObj: {}, something: '123' },
+      docketRecordIndex: 1,
+      fromModal: true,
+    });
+  });
 });
