4424c8e2c3d548a2aa2d084568a4e71832f057e5
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-01-21 09:36:59 -0800

    clean up environment switching function; add tests

--- web-api/switch-environment-color.sh
+++ web-api/switch-environment-color.sh
@@ -6,14 +6,14 @@ CURRENT_COLOR=$(aws dynamodb get-item --region us-east-1 --table-name "efcms-${s
 
 if [[ $CURRENT_COLOR == 'green' ]] ; then
   NEW_COLOR='blue'
-elif [[ $CURRENT_COLOR == 'blue' ]] ; then
+else
   NEW_COLOR='green'
 fi
 
 aws dynamodb put-item --region us-east-1 --table-name "efcms-${slsStage}" --item '{"pk":{"S":"deployed-stack"},"sk":{"S":"deployed-stack"},"current":{"S":"'$NEW_COLOR'"}}'
 
 # switch base path mappings to new color
-node switch-environment-color.js ${slsStage} 'us-east-1'
-node switch-environment-color.js ${slsStage} 'us-west-1'
+node switch-environment-color.js ${slsStage} 'us-east-1' ${CURRENT_COLOR} ${NEW_COLOR}
+node switch-environment-color.js ${slsStage} 'us-west-1' ${CURRENT_COLOR} ${NEW_COLOR}
 
 echo "changed environment to ${NEW_COLOR}"
