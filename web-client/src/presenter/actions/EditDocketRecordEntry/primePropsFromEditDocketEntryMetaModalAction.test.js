9975f107663b7b6b079688505af42974acb653cc
Joseph Sutton <jsutton@flexion.us>
2020-01-21 10:48:50 -0600

    fixed docketRecordIndex and added action to call updateDocketEntryMeta interactor

--- web-client/src/presenter/actions/EditDocketRecordEntry/primePropsFromEditDocketEntryMetaModalAction.test.js
+++ web-client/src/presenter/actions/EditDocketRecordEntry/primePropsFromEditDocketEntryMetaModalAction.test.js
@@ -26,7 +26,7 @@ describe('primePropsFromEditDocketEntryMetaModalAction', () => {
     expect(result.output).toEqual({
       caseId: '456',
       docketRecordEntry: { something: '123' },
-      docketRecordIndex: 1,
+      docketRecordIndex: 0,
       fromModal: true,
     });
   });
@@ -56,7 +56,7 @@ describe('primePropsFromEditDocketEntryMetaModalAction', () => {
     expect(result.output).toEqual({
       caseId: '456',
       docketRecordEntry: { someObj: {}, something: '123' },
-      docketRecordIndex: 1,
+      docketRecordIndex: 0,
       fromModal: true,
     });
   });
