84cc10920e150318d697b9710e22c6c0d8759cc4
Matt Hopson <matthopson@gmail.com>
2020-01-22 07:31:46 -0700

    Write some additional tests

--- web-client/src/presenter/actions/EditDocketRecordEntry/updateDocketEntryMetaAction.test.js
+++ web-client/src/presenter/actions/EditDocketRecordEntry/updateDocketEntryMetaAction.test.js
@@ -4,9 +4,12 @@ import { updateDocketEntryMetaAction } from './updateDocketEntryMetaAction';
 
 describe('updateDocketEntryMetaAction', () => {
   let updateDocketEntryMetaInteractorStub;
+  let docketEntryMetaParam;
 
   beforeEach(() => {
-    updateDocketEntryMetaInteractorStub = jest.fn();
+    updateDocketEntryMetaInteractorStub = jest.fn(
+      ({ docketEntryMeta }) => (docketEntryMetaParam = docketEntryMeta),
+    );
 
     presenter.providers.applicationContext = {
       getUseCases: () => ({
@@ -28,5 +31,50 @@ describe('updateDocketEntryMetaAction', () => {
     });
 
     expect(updateDocketEntryMetaInteractorStub).toHaveBeenCalled();
+  });
+
+  it('converts the servedParties string into an array', async () => {
+    await runAction(updateDocketEntryMetaAction, {
+      modules: { presenter },
+      props: {
+        caseId: '123-45',
+        docketRecordEntry: {
+          servedParties: 'One,Two,Three',
+        },
+        docketRecordIndex: 1,
+      },
+    });
+
+    expect(docketEntryMetaParam.servedParties).toEqual(['One', 'Two', 'Three']);
+  });
+
+  it('converts the servedParties string into an array, stripping white space from array items', async () => {
+    await runAction(updateDocketEntryMetaAction, {
+      modules: { presenter },
+      props: {
+        caseId: '123-45',
+        docketRecordEntry: {
+          servedParties: 'One , Two , Three',
+        },
+        docketRecordIndex: 1,
+      },
+    });
+
+    expect(docketEntryMetaParam.servedParties).toEqual(['One', 'Two', 'Three']);
+  });
+
+  it('does no operation the servedParties value if it is not a string', async () => {
+    await runAction(updateDocketEntryMetaAction, {
+      modules: { presenter },
+      props: {
+        caseId: '123-45',
+        docketRecordEntry: {
+          servedParties: undefined,
+        },
+        docketRecordIndex: 1,
+      },
+    });
+
+    expect(docketEntryMetaParam.servedParties).toEqual(undefined);
   });
 });
