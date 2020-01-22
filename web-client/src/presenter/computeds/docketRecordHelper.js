675edfc63390bfc2dbe81a305f8d2c364b7acd34
William Bagayoko <wbagayoko@gmail.com>
2020-01-22 11:33:49 -0600

    3914 only show when item has been QCed

--- web-client/src/presenter/computeds/docketRecordHelper.js
+++
@@ -1,9 +0,0 @@
-import { state } from 'cerebral';
-
-export const docketRecordHelper = get => {
-  const permissions = get(state.permissions);
-
-  return {
-    showEditDocketRecordEntry: permissions.EDIT_DOCKET_ENTRY,
-  };
-};
