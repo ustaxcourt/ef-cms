133d11708806ce03b0ff691dd26b9b5bf5288022
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-01-22 13:45:50 -0800

    3922: integration test for sealing a case

---
+++ web-client/integration-tests/journey/associatedUserViewsCaseDetailForSealedCase.js
@@ -0,0 +1,12 @@
+export default test => {
+  return it('associated user views case detail for a sealed case', async () => {
+    await test.runSequence('gotoCaseDetailSequence', {
+      docketNumber: test.docketNumber,
+    });
+
+    expect(test.getState('caseDetail.sealedDate')).toBeDefined();
+    expect(test.getState('caseDetail.isSealed')).toBeTruthy();
+    //this user should see all case details because they are associated with the case
+    expect(test.getState('caseDetail.caseCaption')).toBeDefined();
+  });
+};
