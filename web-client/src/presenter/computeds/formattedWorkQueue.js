cf9911c0939aac06cb2fedd41b7211558c414936
Cody Seibert <cseibert113@gmail.com>
2020-01-23 12:31:08 -0500

    adding more tests

--- web-client/src/presenter/computeds/formattedWorkQueue.js
+++ web-client/src/presenter/computeds/formattedWorkQueue.js
@@ -63,6 +63,7 @@ export const formatWorkItem = ({
     .getUtilities()
     .formatDateString(result.createdAt, 'MMDDYY');
 
+  result.highPriority = !!result.highPriority;
   result.messages = orderBy(result.messages, 'createdAt', 'desc');
   result.messages.forEach(message => {
     message.createdAtFormatted = formatDateIfToday(
