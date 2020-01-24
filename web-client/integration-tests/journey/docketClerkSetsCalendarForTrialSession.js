004abf854c4916fa3f7708a61e0e84500d679e44
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-01-24 09:26:17 -0800

    another permissions fix

--- web-client/integration-tests/journey/docketClerkSetsCalendarForTrialSession.js
+++
@@ -1,9 +0,0 @@
-export default test => {
-  return it('Docket clerk sets the calendar for a trial session', async () => {
-    await test.runSequence('gotoTrialSessionDetailSequence', {
-      trialSessionId: test.trialSessionId,
-    });
-
-    await test.runSequence('setTrialSessionCalendarSequence');
-  });
-};
