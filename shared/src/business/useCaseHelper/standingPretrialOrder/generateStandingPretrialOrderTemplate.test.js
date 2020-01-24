88928ef01a51959422d8e231f76cfaa29b974854
William Bagayoko <wbagayoko@gmail.com>
2020-01-24 09:34:55 -0600

    review updates
    remove room number
    add dynamic trial judge

--- shared/src/business/useCaseHelper/standingPretrialOrder/generateStandingPretrialOrderTemplate.test.js
+++ shared/src/business/useCaseHelper/standingPretrialOrder/generateStandingPretrialOrderTemplate.test.js
@@ -23,6 +23,7 @@ describe('generateStandingPretrialOrderTemplate', () => {
           caseDetail.docketNumber + (caseDetail.docketNumberSuffix || ''),
         trialInfo: {
           city: 'City',
+          judge: { name: 'Test Judge' },
           startDate: '2020-02-02T05:00:00.000Z',
           startTime: '10:00',
           state: 'ST',
@@ -34,6 +35,7 @@ describe('generateStandingPretrialOrderTemplate', () => {
     expect(result.indexOf('Test Case Caption')).toBeGreaterThan(-1);
     expect(result.indexOf('123-45S')).toBeGreaterThan(-1);
     expect(result.indexOf('City')).toBeGreaterThan(-1);
+    expect(result.indexOf('Test Judge')).toBeGreaterThan(-1);
     expect(result.indexOf('ST')).toBeGreaterThan(-1);
     expect(result.indexOf('STANDING PRE TRIAL ORDER')).toBeGreaterThan(-1);
     expect(
