c41d37f5d3e47033904e9db1289e79eafbc71f15
Cody Seibert <cseibert113@gmail.com>
2020-01-24 15:39:09 -0500

    removing conosle log

--- web-client/src/presenter/sequences/updateModalValueSequence.js
+++ web-client/src/presenter/sequences/updateModalValueSequence.js
@@ -2,8 +2,5 @@ import { props, state } from 'cerebral';
 import { set } from 'cerebral/factories';
 
 export const updateModalValueSequence = [
-  props => {
-    console.log(props);
-  },
   set(state.modal[props.key], props.value),
 ];
