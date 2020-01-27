41ff699ddc2a0165e7a575d4d6788c8637e2f766
Joseph Sutton <jsutton@flexion.us>
2020-01-27 14:06:15 -0600

    20 cases (for now)

--- web-api/storage/scripts/trialSession/loadTestSmall.js
+++ web-api/storage/scripts/trialSession/loadTestSmall.js
@@ -14,23 +14,25 @@ AWS.config.secretAccessKey = 'noop';
 AWS.config.region = 'us-east-1';
 
 (async function() {
-  const NUM_CASES = 100;
+  const NUM_CASES = 20;
 
   const trialSessionEntity = await createTrialSession();
   const { trialSessionId } = trialSessionEntity;
 
-  // for (let i = 0; i < NUM_CASES; i++) {
-  try {
-    console.log('Creating Case');
-    const caseEntity = await createCase();
-    const { caseId } = caseEntity;
-    console.log(`Adding Case ${caseId} to Trial Session ${trialSessionId}`);
-    await addCaseToTrialSession({
-      caseId,
-      trialSessionId,
-    });
-  } catch (e) {
-    console.log('err', e);
+  for (let i = 0; i < NUM_CASES; i++) {
+    try {
+      console.log('Creating Case');
+      const caseEntity = await createCase();
+      const { caseId, docketNumber } = caseEntity;
+      console.log(
+        `Adding Case ${caseId} [${docketNumber}] to Trial Session ${trialSessionId}`,
+      );
+      await addCaseToTrialSession({
+        caseId,
+        trialSessionId,
+      });
+    } catch (e) {
+      console.log('err', e);
+    }
   }
-  // }
 })();
