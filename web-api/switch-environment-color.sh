db2d756bdf8d641b24f364b3426e0384af371029
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-01-28 07:45:41 -0800

    OpEx: separate blue-green deploy info into its own dynamo table

--- web-api/switch-environment-color.sh
+++ web-api/switch-environment-color.sh
@@ -2,7 +2,7 @@
 
 slsStage="${1}"
 
-CURRENT_COLOR=$(aws dynamodb get-item --region us-east-1 --table-name "efcms-${slsStage}" --key '{"pk":{"S":"deployed-stack"},"sk":{"S":"deployed-stack"}}' | jq -r ".Item.current.S")
+CURRENT_COLOR=$(aws dynamodb get-item --region us-east-1 --table-name "efcms-blue-green" --key '{"pk":{"S":"deployed-stack"},"sk":{"S":"${slsStage}"}}' | jq -r ".Item.current.S")
 
 if [[ $CURRENT_COLOR == 'green' ]] ; then
   NEW_COLOR='blue'
@@ -10,7 +10,7 @@ else
   NEW_COLOR='green'
 fi
 
-aws dynamodb put-item --region us-east-1 --table-name "efcms-${slsStage}" --item '{"pk":{"S":"deployed-stack"},"sk":{"S":"deployed-stack"},"current":{"S":"'$NEW_COLOR'"}}'
+aws dynamodb put-item --region us-east-1 --table-name "efcms-blue-green" --item '{"pk":{"S":"deployed-stack"},"sk":{"S":"${slsStage}"},"current":{"S":"'$NEW_COLOR'"}}'
 
 # switch base path mappings to new color
 node switch-environment-color.js ${slsStage} 'us-east-1' ${CURRENT_COLOR} ${NEW_COLOR}
