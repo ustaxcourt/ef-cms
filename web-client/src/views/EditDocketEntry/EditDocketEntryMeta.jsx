611392d2f4ab3c07d8e46786314227ca4019fe2a
Matt Hopson <matthopson@gmail.com>
2020-01-28 13:06:32 -0700

    Add tabs

--- web-client/src/views/EditDocketEntry/EditDocketEntryMeta.jsx
+++ web-client/src/views/EditDocketEntry/EditDocketEntryMeta.jsx
@@ -1,5 +1,8 @@
 import { CaseDetailHeader } from '../CaseDetail/CaseDetailHeader';
+import { EditDocketEntryMetaTabAction } from './EditDocketEntryMetaTabAction';
+import { EditDocketEntryMetaTabService } from './EditDocketEntryMetaTabService';
 import { ErrorNotification } from '../ErrorNotification';
+import { Tab, Tabs } from '../../ustc-ui/Tabs/Tabs';
 import { connect } from '@cerebral/react';
 import { state } from 'cerebral';
 import React from 'react';
@@ -16,8 +19,27 @@ export const EditDocketEntryMeta = connect(
           <ErrorNotification />
 
           <div className="grid-row grid-gap">
-            <div className="grid-col-5">
-              <editDocketEntryMetaHelper.docketEntryMetaFormComponent />
+            <div className="grid-col-5 DocumentDetail">
+              <Tabs
+                bind="caseDetailPage.caseInformationTab"
+                className="no-full-border-bottom tab-button-h3 container-tabs"
+              >
+                <Tab
+                  id="tab-document-info"
+                  tabName="documentInfo"
+                  title="Document Info"
+                >
+                  {editDocketEntryMetaHelper.docketEntryMetaFormComponent && (
+                    <editDocketEntryMetaHelper.docketEntryMetaFormComponent />
+                  )}
+                </Tab>
+                <Tab id="tab-service" tabName="service" title="Service">
+                  <EditDocketEntryMetaTabService />
+                </Tab>
+                <Tab id="tab-action" tabName="action" title="Action(s)">
+                  <EditDocketEntryMetaTabAction />
+                </Tab>
+              </Tabs>
             </div>
             <div className="grid-col-7">{/* TODO: File preview */}</div>
           </div>
