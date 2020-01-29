cd63adebc95de160f54158c664820dab07380d3b
Joseph Sutton <jsutton@flexion.us>
2020-01-29 12:11:50 -0600

    added fields to docket entry meta form with document

--- web-client/src/views/EditDocketEntry/EditDocketEntryMetaFormDocument.jsx
+++ web-client/src/views/EditDocketEntry/EditDocketEntryMetaFormDocument.jsx
@@ -1,25 +1,69 @@
 import { FormGroup } from '../../ustc-ui/FormGroup/FormGroup';
-import { classNames } from 'classnames';
+import { Inclusions } from '../AddDocketEntry/Inclusions';
 import { connect } from '@cerebral/react';
+import {
+  docketEntryOnChange,
+  onInputChange,
+  reactSelectValue,
+} from '../../ustc-ui/utils/documentTypeSelectHelper';
 import { limitLength } from '../../ustc-ui/utils/limitLength';
 import { sequences, state } from 'cerebral';
 import React from 'react';
+import Select from 'react-select';
+import classNames from 'classnames';
 
 export const EditDocketEntryMetaFormDocument = connect(
   {
+    caseDetail: state.caseDetail,
+    editDocketEntryMetaHelper: state.editDocketEntryMetaHelper,
     form: state.form,
-    updateFormValueSequence: sequences.updateFormValueSequence,
+    internalTypesHelper: state.internalTypesHelper,
+    updateDocketEntryFormValueSequence:
+      sequences.updateDocketEntryFormValueSequence,
     validateDocketRecordSequence: sequences.validateDocketRecordSequence,
     validationErrors: state.validationErrors,
   },
   ({
+    caseDetail,
+    editDocketEntryMetaHelper,
     form,
-    updateFormValueSequence,
+    internalTypesHelper,
+    updateDocketEntryFormValueSequence,
     validateDocketRecordSequence,
     validationErrors,
   }) => {
     return (
       <div className="blue-container">
+        <FormGroup errorText={validationErrors.lodged}>
+          <fieldset className="usa-fieldset">
+            <legend className="usa-legend">Filing status</legend>
+            {['File', 'Lodge'].map(option => (
+              <div className="usa-radio usa-radio__inline" key={option}>
+                <input
+                  checked={form.lodged === (option === 'Lodge')}
+                  className="usa-radio__input"
+                  id={`filing-status-${option}`}
+                  name="lodged"
+                  type="radio"
+                  value={option}
+                  onChange={e => {
+                    updateDocketEntryFormValueSequence({
+                      key: e.target.name,
+                      value: e.target.value === 'Lodge',
+                    });
+                    validateDocketRecordSequence();
+                  }}
+                />
+                <label
+                  className="usa-radio__label"
+                  htmlFor={`filing-status-${option}`}
+                >
+                  {option}
+                </label>
+              </div>
+            ))}
+          </fieldset>
+        </FormGroup>
         <FormGroup errorText={validationErrors.filingDate}>
           <fieldset className="usa-fieldset margin-bottom-0">
             <legend className="usa-legend" id="filing-date-legend">
@@ -43,7 +87,7 @@ export const EditDocketEntryMetaFormDocument = connect(
                   value={form.filingDateMonth || ''}
                   onBlur={() => validateDocketRecordSequence()}
                   onChange={e => {
-                    updateFormValueSequence({
+                    updateDocketEntryFormValueSequence({
                       key: e.target.name,
                       value: limitLength(e.target.value, 2),
                     });
@@ -67,7 +111,7 @@ export const EditDocketEntryMetaFormDocument = connect(
                   value={form.filingDateDay || ''}
                   onBlur={() => validateDocketRecordSequence()}
                   onChange={e => {
-                    updateFormValueSequence({
+                    updateDocketEntryFormValueSequence({
                       key: e.target.name,
                       value: limitLength(e.target.value, 2),
                     });
@@ -91,7 +135,7 @@ export const EditDocketEntryMetaFormDocument = connect(
                   value={form.filingDateYear || ''}
                   onBlur={() => validateDocketRecordSequence()}
                   onChange={e => {
-                    updateFormValueSequence({
+                    updateDocketEntryFormValueSequence({
                       key: e.target.name,
                       value: limitLength(e.target.value, 4),
                     });
@@ -101,31 +145,242 @@ export const EditDocketEntryMetaFormDocument = connect(
             </div>
           </fieldset>
         </FormGroup>
-
-        <FormGroup errorText={validationErrors.description}>
+        <FormGroup errorText={validationErrors.eventCode}>
           <label
             className="usa-label"
-            htmlFor="description"
-            id="description-label"
+            htmlFor="react-select-2-input"
+            id="document-type-label"
           >
-            Filings and proceedings
+            Document type
           </label>
-          <input
-            aria-describedby="description-label"
-            className="usa-input"
-            id="description"
-            name="description"
-            type="text"
-            value={form.description || ''}
-            onChange={e => {
-              updateFormValueSequence({
-                key: e.target.name,
-                value: e.target.value,
+
+          <Select
+            aria-describedby="document-type-label"
+            className="select-react-element"
+            classNamePrefix="select-react-element"
+            id="document-type"
+            isClearable={true}
+            name="eventCode"
+            options={internalTypesHelper.internalDocumentTypesForSelectSorted}
+            placeholder="- Select -"
+            value={reactSelectValue({
+              documentTypes:
+                internalTypesHelper.internalDocumentTypesForSelectSorted,
+              selectedEventCode: form.eventCode,
+            })}
+            onChange={(inputValue, { action, name }) => {
+              docketEntryOnChange({
+                action,
+                inputValue,
+                name,
+                updateSequence: updateDocketEntryFormValueSequence,
+                validateSequence: validateDocketRecordSequence,
               });
-              validateDocketRecordSequence();
+              return true;
+            }}
+            onInputChange={(inputText, { action }) => {
+              onInputChange({
+                action,
+                inputText,
+                updateSequence: validateDocketRecordSequence,
+              });
             }}
           />
         </FormGroup>
+        <FormGroup errorText={validationErrors.additionalInfo}>
+          <label
+            className="usa-label"
+            htmlFor="additional-info"
+            id="additional-info-label"
+          >
+            Additional info 1 <span className="usa-hint">(optional)</span>
+          </label>
+          <textarea
+            aria-describedby="additional-info-label"
+            autoCapitalize="none"
+            className="usa-textarea height-8"
+            id="additional-info"
+            name="additionalInfo"
+            type="text"
+            value={form.additionalInfo || ''}
+            onBlur={() => {
+              validateDocketRecordSequence();
+            }}
+            onChange={e => {
+              updateDocketEntryFormValueSequence({
+                key: e.target.name,
+                value: e.target.value,
+              });
+            }}
+          />
+        </FormGroup>
+        <FormGroup>
+          <div className="usa-checkbox">
+            <input
+              checked={form.addToCoversheet || false}
+              className="usa-checkbox__input"
+              id="add-to-coversheet"
+              name="addToCoversheet"
+              type="checkbox"
+              onChange={e => {
+                updateDocketEntryFormValueSequence({
+                  key: e.target.name,
+                  value: e.target.checked,
+                });
+                validateDocketRecordSequence();
+              }}
+            />
+            <label
+              className="usa-checkbox__label inline-block"
+              htmlFor="add-to-coversheet"
+            >
+              Add to cover sheet
+            </label>
+          </div>
+        </FormGroup>
+        <FormGroup>
+          <label
+            className="usa-label"
+            htmlFor="additional-info2"
+            id="additional-info2-label"
+          >
+            Additional info 2 <span className="usa-hint">(optional)</span>
+          </label>
+          <textarea
+            aria-describedby="additional-info2-label"
+            autoCapitalize="none"
+            className="usa-textarea height-8"
+            id="additional-info2"
+            name="additionalInfo2"
+            type="text"
+            value={form.additionalInfo2 || ''}
+            onBlur={() => {
+              validateDocketRecordSequence();
+            }}
+            onChange={e => {
+              updateDocketEntryFormValueSequence({
+                key: e.target.name,
+                value: e.target.value,
+              });
+            }}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Inclusions />
+        </FormGroup>
+        <FormGroup errorText={editDocketEntryMetaHelper.partyValidationError}>
+          <fieldset
+            className={classNames(
+              'usa-fieldset',
+              !editDocketEntryMetaHelper.showObjection && 'margin-bottom-0',
+            )}
+          >
+            <legend className="usa-legend">Who is filing this document?</legend>
+            <div className="usa-checkbox">
+              <input
+                checked={form.partyPrimary || false}
+                className="usa-checkbox__input"
+                id="party-primary"
+                name="partyPrimary"
+                type="checkbox"
+                onChange={e => {
+                  updateDocketEntryFormValueSequence({
+                    key: e.target.name,
+                    value: e.target.checked,
+                  });
+                  validateDocketRecordSequence();
+                }}
+              />
+              <label
+                className="usa-checkbox__label inline-block"
+                htmlFor="party-primary"
+              >
+                {caseDetail.contactPrimary.name}
+              </label>
+            </div>
+            {editDocketEntryMetaHelper.showSecondaryParty && (
+              <div className="usa-checkbox">
+                <input
+                  checked={form.partySecondary || false}
+                  className="usa-checkbox__input"
+                  id="party-secondary"
+                  name="partySecondary"
+                  type="checkbox"
+                  onChange={e => {
+                    updateDocketEntryFormValueSequence({
+                      key: e.target.name,
+                      value: e.target.checked,
+                    });
+                    validateDocketRecordSequence();
+                  }}
+                />
+                <label
+                  className="usa-checkbox__label inline-block"
+                  htmlFor="party-secondary"
+                >
+                  {caseDetail.contactSecondary.name}
+                </label>
+              </div>
+            )}
+            <div className="usa-checkbox">
+              <input
+                checked={form.partyRespondent || false}
+                className="usa-checkbox__input"
+                id="party-respondent"
+                name="partyRespondent"
+                type="checkbox"
+                onChange={e => {
+                  updateDocketEntryFormValueSequence({
+                    key: e.target.name,
+                    value: e.target.checked,
+                  });
+                  validateDocketRecordSequence();
+                }}
+              />
+              <label
+                className="usa-checkbox__label inline-block"
+                htmlFor="party-respondent"
+              >
+                Respondent
+              </label>
+            </div>
+          </fieldset>
+        </FormGroup>
+        {!editDocketEntryMetaHelper.showObjection && (
+          <FormGroup errorText={validationErrors.objections}>
+            <fieldset className="usa-fieldset margin-bottom-0">
+              <legend className="usa-legend" id="objections-legend">
+                Are there any objections to this document?
+              </legend>
+              {['Yes', 'No', 'Unknown'].map(option => (
+                <div className="usa-radio" key={option}>
+                  <input
+                    aria-describedby="objections-legend"
+                    checked={form.objections === option}
+                    className="usa-radio__input"
+                    id={`objections-${option}`}
+                    name="objections"
+                    type="radio"
+                    value={option}
+                    onChange={e => {
+                      updateDocketEntryFormValueSequence({
+                        key: e.target.name,
+                        value: e.target.value,
+                      });
+                      validateDocketRecordSequence();
+                    }}
+                  />
+                  <label
+                    className="usa-radio__label"
+                    htmlFor={`objections-${option}`}
+                  >
+                    {option}
+                  </label>
+                </div>
+              ))}
+            </fieldset>
+          </FormGroup>
+        )}
       </div>
     );
   },
