925f33f0cbc713fb239d1b391117a5c704250590
Joseph Sutton <jsutton@flexion.us>
2020-01-29 10:55:14 -0600

    compute which component to show

--- web-client/src/presenter/computeds/editDocketEntryMetaHelper.js
+++ web-client/src/presenter/computeds/editDocketEntryMetaHelper.js
@@ -1,5 +1,20 @@
-export const editDocketEntryMetaHelper = () => {
+import { state } from 'cerebral';
+
+export const editDocketEntryMetaHelper = get => {
   let docketEntryMetaFormComponent;
+
+  const caseDetail = get(state.caseDetail);
+  const docketRecordIndex = get(state.docketRecordIndex);
+
+  const docketRecord = caseDetail.docketRecord.find(
+    docketEntry => docketEntry.index == docketRecordIndex,
+  );
+
+  if (docketRecord.documentId) {
+    docketEntryMetaFormComponent = 'Document';
+  } else {
+    docketEntryMetaFormComponent = 'No Document';
+  }
 
   // TODO: Some logic to determine docketEntryMetaFormComponent value (one of: CourtIssued, Document, or NoDocument)
 
