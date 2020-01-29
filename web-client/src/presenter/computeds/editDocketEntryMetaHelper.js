8d61af78d84ae050a9320b3d2a7516fe203192a7
Joseph Sutton <jsutton@flexion.us>
2020-01-29 12:11:50 -0600

    added fields to docket entry meta form with document

--- web-client/src/presenter/computeds/editDocketEntryMetaHelper.js
+++ web-client/src/presenter/computeds/editDocketEntryMetaHelper.js
@@ -1,14 +1,40 @@
 import { state } from 'cerebral';
 
-export const editDocketEntryMetaHelper = get => {
+export const editDocketEntryMetaHelper = (get, applicationContext) => {
   let docketEntryMetaFormComponent;
 
   const caseDetail = get(state.caseDetail);
   const docketRecordIndex = get(state.docketRecordIndex);
+  const validationErrors = get(state.validationErrors);
+  const form = get(state.form);
 
   const docketRecord = caseDetail.docketRecord.find(
     docketEntry => docketEntry.index == docketRecordIndex,
   );
+
+  const {
+    INTERNAL_CATEGORY_MAP,
+    PARTY_TYPES,
+  } = applicationContext.getConstants();
+
+  const partyValidationError =
+    validationErrors &&
+    (validationErrors.partyPrimary ||
+      validationErrors.partySecondary ||
+      validationErrors.partyRespondent);
+
+  const objectionDocumentTypes = [
+    ...INTERNAL_CATEGORY_MAP['Motion'].map(entry => {
+      return entry.documentType;
+    }),
+    'Motion to Withdraw Counsel (filed by petitioner)',
+    'Motion to Withdraw as Counsel',
+    'Application to Take Deposition',
+  ];
+
+  const showSecondaryParty =
+    caseDetail.partyType === PARTY_TYPES.petitionerSpouse ||
+    caseDetail.partyType === PARTY_TYPES.petitionerDeceasedSpouse;
 
   if (docketRecord.documentId) {
     docketEntryMetaFormComponent = 'Document';
@@ -20,5 +46,8 @@ export const editDocketEntryMetaHelper = get => {
 
   return {
     docketEntryMetaFormComponent,
+    partyValidationError,
+    showObjection: objectionDocumentTypes.includes(form.documentType),
+    showSecondaryParty,
   };
 };
