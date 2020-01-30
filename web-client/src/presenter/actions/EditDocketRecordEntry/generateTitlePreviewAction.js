a9c3aa849e0c15abf1dc5009741c4a81bd8ef52b
William Bagayoko <wbagayoko@gmail.com>
2020-01-30 11:39:54 -0600

    3914 generateTitlePreviewAction

---
+++ web-client/src/presenter/actions/EditDocketRecordEntry/generateTitlePreviewAction.js
@@ -0,0 +1,25 @@
+import { isEmpty } from 'lodash';
+import { state } from 'cerebral';
+
+/**
+ * generate document titles for filing document
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context
+ * @param {object} providers.props the cerebral props object
+ */
+export const generateTitlePreviewAction = ({
+  applicationContext,
+  get,
+  store,
+}) => {
+  const documentMetadata = get(state.form);
+
+  let documentTitle = applicationContext
+    .getUseCases()
+    .generateDocumentTitleInteractor({
+      applicationContext,
+      documentMetadata,
+    });
+  store.set(state.screenMetadata.documentTitlePreview, documentTitle);
+};
