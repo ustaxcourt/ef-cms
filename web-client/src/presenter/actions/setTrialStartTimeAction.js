f97496d0deacd37768b2df9af73881b6ce6ed6a1
rachelschneiderman <rschneiderman379@gmail.com>
2020-02-04 08:55:19 -0800

    Added action that populates a default time for a new trial session.

--- web-client/src/presenter/actions/setTrialStartTimeAction.js
+++ web-client/src/presenter/actions/setTrialStartTimeAction.js
@@ -1,11 +1,10 @@
 import { state } from 'cerebral';
 
 /**
- * sets the state.form.startTime values to a default 10:00am value
+ * sets the state.form.startTime values to previously entered and saved value
  *
- * @param {object} providers the providers object
- * @param {object} providers.store the cerebral store
  * @param {object} providers.props the cerebral props object
+ * @param {object} providers.store the cerebral store
  */
 export const setTrialStartTimeAction = ({ props, store }) => {
   store.set(state.form.startTimeExtension, props.startTimeExtension);
