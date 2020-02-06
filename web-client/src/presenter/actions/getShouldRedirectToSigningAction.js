c1aa18b80239661e25d1aeb65ba62fbce1c186f7
Matt Hopson <matthopson@gmail.com>
2020-02-06 11:03:49 -0700

    Add jsdoc

--- web-client/src/presenter/actions/getShouldRedirectToSigningAction.js
+++ web-client/src/presenter/actions/getShouldRedirectToSigningAction.js
@@ -1,5 +1,16 @@
 import { state } from 'cerebral';
 
+/**
+ * Invokes the path in the sequence based on whether the court issued document is a notice or not.
+ * In the case of a notice, it sets some state for redirecting after save
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context
+ * @param {object} providers.path the cerebral path which is contains the next paths that can be invoked
+ * @param {object} providers.props the cerebral props object
+ * @param {object} providers.store the cerebral store
+ * @returns {object} continue path for the sequence
+ */
 export const getShouldRedirectToSigningAction = ({
   applicationContext,
   path,
