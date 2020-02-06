1848c36a9ad7adeaa0173078ecc104d31f6ff048
William Bagayoko <wbagayoko@gmail.com>
2020-02-05 22:57:19 -0600

    update view component

--- web-client/src/presenter/sequences/gotoEditUploadCourtIssuedDocumentSequence.js
+++ web-client/src/presenter/sequences/gotoEditUploadCourtIssuedDocumentSequence.js
@@ -3,7 +3,9 @@ import { clearScreenMetadataAction } from '../actions/clearScreenMetadataAction'
 import { getCaseAction } from '../actions/getCaseAction';
 import { isLoggedInAction } from '../actions/isLoggedInAction';
 import { redirectToCognitoAction } from '../actions/redirectToCognitoAction';
+import { setCaseAction } from '../actions/setCaseAction';
 import { setCurrentPageAction } from '../actions/setCurrentPageAction';
+import { setDocumentIdAction } from '../actions/setDocumentIdAction';
 import { setDocumentToFormAction } from '../actions/editUploadCourtIssuedDocument/setDocumentToFormAction';
 import { stopShowValidationAction } from '../actions/stopShowValidationAction';
 
@@ -13,6 +15,8 @@ const gotoEditUploadCourtIssuedDocument = [
   clearFormAction,
   clearScreenMetadataAction,
   getCaseAction,
+  setCaseAction,
+  setDocumentIdAction,
   setDocumentToFormAction,
   setCurrentPageAction('EditUploadCourtIssuedDocument'),
 ];
