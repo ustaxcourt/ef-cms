1d78ea58a8d1e54d60a644b3b1ca88943f34f7fb
Matt Hopson <matthopson@gmail.com>
2020-02-07 13:19:34 -0700

    Update secondary contact proxy

--- web-client/src/applicationContext.js
+++ web-client/src/applicationContext.js
@@ -203,6 +203,7 @@ import { updatePetitionDetailsInteractor } from '../../shared/src/proxies/update
 import { updatePetitionerInformationInteractor } from '../../shared/src/proxies/updatePetitionerInformationProxy';
 import { updatePrimaryContactInteractor } from '../../shared/src/proxies/updatePrimaryContactProxy';
 import { updateQcCompleteForTrialInteractor } from '../../shared/src/proxies/updateQcCompleteForTrialProxy';
+import { updateSecondaryContactInteractor } from '../../shared/src/proxies/updateSecondaryContactInteractor';
 import { updateTrialSessionInteractor } from '../../shared/src/proxies/trialSessions/updateTrialSessionProxy';
 import { updateTrialSessionWorkingCopyInteractor } from '../../shared/src/proxies/trialSessions/updateTrialSessionWorkingCopyProxy';
 import { updateUserContactInformationInteractor } from '../../shared/src/proxies/users/updateUserContactInformationProxy';
@@ -376,6 +377,7 @@ const allUseCases = {
   updatePetitionerInformationInteractor,
   updatePrimaryContactInteractor,
   updateQcCompleteForTrialInteractor,
+  updateSecondaryContactInteractor,
   updateTrialSessionInteractor,
   updateTrialSessionWorkingCopyInteractor,
   updateUserContactInformationInteractor,
