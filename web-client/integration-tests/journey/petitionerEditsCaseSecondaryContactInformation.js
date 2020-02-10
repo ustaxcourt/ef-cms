a24dc4aabf9fc45ec9ea8dca4fded51b49e32704
Matt Hopson <matthopson@gmail.com>
2020-02-10 09:39:47 -0700

    Update integration tests

--- web-client/integration-tests/journey/petitionerEditsCaseSecondaryContactInformation.js
+++
@@ -1,9 +0,0 @@
-export default () => {
-  return it('petitioner is shown a modal for Change of Address', async () => {
-    //TODO - update this to actually edit secondary contact
-    // await test.runSequence('openEditSecondaryContactModalSequence');
-    // expect(test.getState('showModal')).toEqual('EditSecondaryContact');
-    // await test.runSequence('dismissModalSequence');
-    // expect(test.getState('showModal')).toEqual('');
-  });
-};
