a0cc702f9bfff85cd6f13a3eb75115cc77debf1d
rachelschneiderman <rschneiderman379@gmail.com>
2020-02-10 13:09:44 -0800

    Added back migration that was accidentally removed during merge.

--- web-api/migrations/00005-service-indicator-respondents.js
+++ web-api/migrations/00005-service-indicator-respondents.js
@@ -1,9 +1,19 @@
 const { isCaseRecord } = require('./utilities');
 
+const {
+  SERVICE_INDICATOR_TYPES,
+} = require('../../shared/src/business/entities/cases/CaseConstants');
+
 const up = async (documentClient, tableName, forAllRecords) => {
   await forAllRecords(documentClient, tableName, async item => {
     if (!isCaseRecord(item)) return;
     if (!item.respondents) return;
+
+    item.respondents.forEach(respondent => {
+      if (!respondent.serviceIndicator) {
+        respondent.serviceIndicator = SERVICE_INDICATOR_TYPES.SI_ELECTRONIC;
+      }
+    });
 
     await documentClient
       .put({
