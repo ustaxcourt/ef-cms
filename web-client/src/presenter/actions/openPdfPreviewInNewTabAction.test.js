1ad6c5f7965b32f286f43b9d675497e96321f003
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-02-10 08:12:29 -0800

    Bug: refresh order pdf before opening in new tab (View Full PDF)

---
+++ web-client/src/presenter/actions/openPdfPreviewInNewTabAction.test.js
@@ -0,0 +1,19 @@
+import { openPdfPreviewInNewTabAction } from './openPdfPreviewInNewTabAction';
+import { runAction } from 'cerebral/test';
+
+describe('openPdfPreviewInNewTabAction', () => {
+  const openMock = jest.fn();
+
+  beforeEach(() => {
+    global.open = openMock;
+  });
+
+  it('should open the pdfUrl from props in a new tab', () => {
+    runAction(openPdfPreviewInNewTabAction, {
+      props: { pdfUrl: 'http://www.example.com' },
+    });
+
+    expect(openMock).toBeCalled();
+    expect(openMock).toBeCalledWith('http://www.example.com', '_blank');
+  });
+});
