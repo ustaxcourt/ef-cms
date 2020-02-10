be46ff7440459b605d824337afa22a0d12953399
rachelschneiderman <rschneiderman379@gmail.com>
2020-02-10 11:14:16 -0800

    Refactored shared migration functions into utilities file. Added check for case optional fields so migration won't throw an error.

--- web-api/migrations/00004-service-indicator.js
+++ web-api/migrations/00004-service-indicator.js
@@ -1,51 +1,29 @@
-const isCaseRecord = item => !!item.caseType; // only case records have a caseType defined
+const { isCaseRecord } = require('./utilities');
 
 const {
   SERVICE_INDICATOR_TYPES,
 } = require('../../shared/src/business/entities/cases/CaseConstants');
 
-const forAllRecords = async (documentClient, tableName, cb) => {
-  let hasMoreResults = true;
-  let lastKey = null;
-  while (hasMoreResults) {
-    hasMoreResults = false;
-
-    const results = await documentClient
-      .scan({
-        ExclusiveStartKey: lastKey,
-        TableName: tableName,
-      })
-      .promise();
-
-    for (let item of results.Items) {
-      await cb(item);
-    }
-
-    hasMoreResults = !!results.LastEvaluatedKey;
-    lastKey = results.LastEvaluatedKey;
-  }
-};
-
-const up = async (documentClient, tableName) => {
+const up = async (documentClient, tableName, forAllRecords) => {
   await forAllRecords(documentClient, tableName, async item => {
     if (!isCaseRecord(item)) return;
+    if (item.practitioners) {
+      item.practitioners.forEach(practitioner => {
+        if (!practitioner.serviceIndicator) {
+          practitioner.serviceIndicator = SERVICE_INDICATOR_TYPES.SI_ELECTRONIC;
+        }
+      });
 
-    item.practitioners.forEach(practitioner => {
-      if (!practitioner.serviceIndicator) {
-        practitioner.serviceIndicator = SERVICE_INDICATOR_TYPES.SI_ELECTRONIC;
-      }
-    });
-
-    await documentClient
-      .put({
-        Item: item,
-        TableName: tableName,
-      })
-      .promise();
+      await documentClient
+        .put({
+          Item: item,
+          TableName: tableName,
+        })
+        .promise();
+    }
   });
 };
 
 module.exports = {
-  forAllRecords,
   up,
 };
