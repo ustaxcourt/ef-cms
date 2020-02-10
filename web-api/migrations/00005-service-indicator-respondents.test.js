8afc15e242b3c4c0653bbc3f22ff69ce6925606d
rachelschneiderman <rschneiderman379@gmail.com>
2020-02-10 13:57:02 -0800

    Added additional test coverage.

--- web-api/migrations/00005-service-indicator-respondents.test.js
+++ web-api/migrations/00005-service-indicator-respondents.test.js
@@ -1,6 +1,9 @@
-const { fail } = require('jest');
+const {
+  SERVICE_INDICATOR_TYPES,
+} = require('../../shared/src/business/entities/cases/CaseConstants');
 const { forAllRecords } = require('./utilities');
 const { MOCK_CASE } = require('../../shared/src/test/mockCase');
+const { MOCK_DOCUMENTS } = require('../../shared/src/test/mockDocuments');
 const { up } = require('./00005-service-indicator-respondents');
 
 describe('service indicator respondents migration', () => {
@@ -11,7 +14,7 @@ describe('service indicator respondents migration', () => {
   beforeEach(() => {
     scanStub = jest.fn().mockReturnValue({
       promise: async () => ({
-        Items: [MOCK_CASE],
+        Items: [MOCK_DOCUMENTS[0]],
       }),
     });
 
@@ -30,11 +33,48 @@ describe('service indicator respondents migration', () => {
     };
   });
 
+  it('should not update the item when it is not a case', async () => {
+    scanStub = jest.fn().mockReturnValue({
+      promise: async () => ({
+        Items: [MOCK_DOCUMENTS[0]],
+      }),
+    });
+
+    await up(applicationContext.getDocumentClient(), '', forAllRecords);
+
+    expect(putStub.mock.calls.length).toBe(0);
+  });
+
   it('should not throw an error when a case does not have any respondents', async () => {
-    await up(applicationContext.getDocumentClient(), '', forAllRecords).catch(
-      () => {
-        fail();
-      },
+    await up(applicationContext.getDocumentClient(), '', forAllRecords);
+
+    expect(putStub.mock.calls.length).toBe(0);
+  });
+
+  it("should update each respondent's service indicator to electronic if one is not already specified", async () => {
+    const mockRespondents = [
+      {},
+      { serviceIndicator: SERVICE_INDICATOR_TYPES.SI_PAPER },
+    ];
+    const expectedRespondents = [
+      { serviceIndicator: SERVICE_INDICATOR_TYPES.SI_ELECTRONIC },
+      { serviceIndicator: SERVICE_INDICATOR_TYPES.SI_PAPER },
+    ];
+    scanStub = jest.fn().mockReturnValue({
+      promise: async () => ({
+        Items: [
+          {
+            ...MOCK_CASE,
+            respondents: mockRespondents,
+          },
+        ],
+      }),
+    });
+
+    await up(applicationContext.getDocumentClient(), '', forAllRecords);
+
+    expect(putStub.mock.calls[0][0].Item.respondents).toEqual(
+      expectedRespondents,
     );
   });
 });
