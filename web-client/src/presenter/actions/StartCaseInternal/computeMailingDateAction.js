2eff530df769f671f993942195d3e960c8712900
Joseph Sutton <jsutton@flexion.us>
2020-02-11 09:37:34 -0600

    added new action to compute mailing date for DateInput component

---
+++ web-client/src/presenter/actions/StartCaseInternal/computeMailingDateAction.js
@@ -0,0 +1,26 @@
+import { state } from 'cerebral';
+
+/**
+ * computes the mailing date from a month, day and year value
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.store the cerebral store object
+ * @param {object} providers.get the cerebral get function
+ */
+export const computeMailingDateAction = ({ get, store }) => {
+  let formDate = null;
+  const formMonth = get(state.form.mailingDateMonth);
+  const formDay = get(state.form.mailingDateDay);
+  const formYear = get(state.form.mailingDateYear);
+
+  if (formMonth || formDay || formYear) {
+    formDate = `${formYear}-${formMonth}-${formDay}`;
+
+    formDate = formDate
+      .split('-')
+      .map(segment => segment.padStart(2, '0'))
+      .join('-');
+  }
+
+  store.set(state.form.mailingDate, formDate);
+};
