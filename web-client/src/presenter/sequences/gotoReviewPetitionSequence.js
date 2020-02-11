1b57f80900a16ad098938492a6c550d869d01289
William Bagayoko <wbagayoko@gmail.com>
2020-02-11 15:38:47 -0600

    update sequences to validate before navigation

--- web-client/src/presenter/sequences/gotoReviewPetitionSequence.js
+++ web-client/src/presenter/sequences/gotoReviewPetitionSequence.js
@@ -1,35 +1,5 @@
-import { checkForActiveBatchesAction } from '../actions/checkForActiveBatchesAction';
-import { clearAlertsAction } from '../actions/clearAlertsAction';
-import { computeFormDateAction } from '../actions/computeFormDateAction';
-import { setAlertErrorAction } from '../actions/setAlertErrorAction';
 import { setCurrentPageAction } from '../actions/setCurrentPageAction';
-import { setShowModalFactoryAction } from '../actions/setShowModalFactoryAction';
-import { setValidationAlertErrorsAction } from '../actions/setValidationAlertErrorsAction';
-import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
-import { startShowValidationAction } from '../actions/startShowValidationAction';
-import { stopShowValidationAction } from '../actions/stopShowValidationAction';
-import { validatePetitionFromPaperAction } from '../actions/validatePetitionFromPaperAction';
 
 export const gotoReviewPetitionSequence = [
-  checkForActiveBatchesAction,
-  {
-    hasActiveBatches: [setShowModalFactoryAction('UnfinishedScansModal')],
-    noActiveBatches: [
-      clearAlertsAction,
-      startShowValidationAction,
-      computeFormDateAction,
-      validatePetitionFromPaperAction,
-      {
-        error: [
-          setAlertErrorAction,
-          setValidationErrorsAction,
-          setValidationAlertErrorsAction,
-        ],
-        success: [
-          stopShowValidationAction,
-          setCurrentPageAction('ReviewPetition'),
-        ],
-      },
-    ],
-  },
+  setCurrentPageAction('ReviewPetition'),
 ];
