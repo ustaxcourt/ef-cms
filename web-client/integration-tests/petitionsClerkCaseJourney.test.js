f50b9b33cc73affba753d1d6cf4df02a5e0fe2fc
William Bagayoko <wbagayoko@gmail.com>
2020-02-11 14:59:20 -0600

    update tests

--- web-client/integration-tests/petitionsClerkCaseJourney.test.js
+++ web-client/integration-tests/petitionsClerkCaseJourney.test.js
@@ -1,8 +1,6 @@
 import { fakeFile, setupTest } from './helpers';
 import petitionsClerkCreatesNewCase from './journey/petitionsClerkCreatesNewCase';
 import petitionsClerkLogIn from './journey/petitionsClerkLogIn';
-import petitionsClerkRunsBatchProcess from './journey/petitionsClerkRunsBatchProcess';
-import petitionsClerkSendsCaseToIRSHoldingQueue from './journey/petitionsClerkSendsCaseToIRSHoldingQueue';
 import petitionsClerkUpdatesCaseDetail from './journey/petitionsClerkUpdatesCaseDetail';
 import petitionsClerkViewsDocketRecordAfterServing from './journey/petitionsClerkViewsDocketRecordAfterServing';
 
@@ -16,7 +14,5 @@ describe('Case journey', () => {
   petitionsClerkLogIn(test);
   petitionsClerkCreatesNewCase(test, fakeFile);
   petitionsClerkUpdatesCaseDetail(test);
-  petitionsClerkSendsCaseToIRSHoldingQueue(test);
-  petitionsClerkRunsBatchProcess(test);
   petitionsClerkViewsDocketRecordAfterServing(test);
 });
