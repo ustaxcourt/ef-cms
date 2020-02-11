31c7f73bb6de747e3491fb039d96396476ffde30
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-02-11 10:02:56 -0800

    remove instances of stripInternalKeys so we receive an error when a use case returns data straight from dynamo without going through an entity

--- shared/src/persistence/dynamo/workitems/getWorkItemById.js
+++ shared/src/persistence/dynamo/workitems/getWorkItemById.js
@@ -1,14 +1,11 @@
 const client = require('../../dynamodbClientService');
-const { stripInternalKeys } = require('../../dynamo/helpers/stripInternalKeys');
 
 exports.getWorkItemById = ({ applicationContext, workItemId }) => {
-  return client
-    .get({
-      Key: {
-        pk: `workitem-${workItemId}`,
-        sk: `workitem-${workItemId}`,
-      },
-      applicationContext,
-    })
-    .then(stripInternalKeys);
+  return client.get({
+    Key: {
+      pk: `workitem-${workItemId}`,
+      sk: `workitem-${workItemId}`,
+    },
+    applicationContext,
+  });
 };
