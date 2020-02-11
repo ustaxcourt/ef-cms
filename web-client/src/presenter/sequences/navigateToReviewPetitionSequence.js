31f0e9a3d8d36ec615699e3b66f8d044dd6cad8b
William Bagayoko <wbagayoko@gmail.com>
2020-02-11 16:03:00 -0600

    lint

--- web-client/src/presenter/sequences/navigateToReviewPetitionSequence.js
+++ web-client/src/presenter/sequences/navigateToReviewPetitionSequence.js
@@ -1,11 +1,9 @@
 import { checkForActiveBatchesAction } from '../actions/checkForActiveBatchesAction';
 import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { computeDateReceivedAction } from '../actions/DocketEntry/computeDateReceivedAction';
-import { computeFormDateAction } from '../actions/computeFormDateAction';
 import { computeMailingDateAction } from '../actions/StartCaseInternal/computeMailingDateAction';
 import { navigateToReviewPetitionAction } from '../actions/StartCaseInternal/navigateToReviewPetitionAction';
 import { setAlertErrorAction } from '../actions/setAlertErrorAction';
-import { setCurrentPageAction } from '../actions/setCurrentPageAction';
 import { setShowModalFactoryAction } from '../actions/setShowModalFactoryAction';
 import { setValidationAlertErrorsAction } from '../actions/setValidationAlertErrorsAction';
 import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
