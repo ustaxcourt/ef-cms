2b43602e5ebc25c87c63ba4b5820c2ae8865b88e
William Bagayoko <wbagayoko@gmail.com>
2020-02-12 10:28:48 -0600

    update tests

--- web-client/src/presenter/computeds/reviewPetitionHelper.test.js
+++ web-client/src/presenter/computeds/reviewPetitionHelper.test.js
@@ -1,223 +1,59 @@
-import { ContactFactory } from '../../../../shared/src/business/entities/contacts/ContactFactory';
-import { advancedSearchHelper as advancedSearchHelperComputed } from './advancedSearchHelper';
 import { applicationContext } from '../../applicationContext';
+import { reviewPetitionHelper as reviewPetitionHelperComputed } from './reviewPetitionHelper';
 import { runCompute } from 'cerebral/test';
 import { withAppContextDecorator } from '../../withAppContext';
 
-let pageSizeOverride = 5;
+const { PAYMENT_STATUS } = applicationContext.getConstants();
 
-const advancedSearchHelper = withAppContextDecorator(
-  advancedSearchHelperComputed,
+const reviewPetitionHelper = withAppContextDecorator(
+  reviewPetitionHelperComputed,
   {
     ...applicationContext,
     getConstants: () => {
       return {
         ...applicationContext.getConstants(),
-        CASE_SEARCH_PAGE_SIZE: pageSizeOverride,
       };
     },
   },
 );
 
-describe('advancedSearchHelper', () => {
-  it('returns only showStateSelect when searchResults is undefined', () => {
-    const result = runCompute(advancedSearchHelper, {
+describe('reviewPetitionHelper', () => {
+  it('returns defaults when there is no form', () => {
+    const result = runCompute(reviewPetitionHelper, {
       state: {
-        advancedSearchForm: {},
+        form: {},
       },
     });
-    expect(result).toEqual({ showStateSelect: false });
+    expect(result).toEqual({
+      hasIrsNoticeFormatted: 'No',
+      irsNoticeDateFormatted: undefined,
+      mailingDateFormatted: undefined,
+      petitionPaymentStatusFormatted: 'Not Paid',
+      receivedAtFormatted: undefined,
+      shouldShowIrsNoticeDate: false,
+    });
   });
 
-  it('returns showStateSelect true when state.advancedSearchForm.countryType is "domestic"', () => {
-    const result = runCompute(advancedSearchHelper, {
+  it('returns defaults when there is no form', () => {
+    const result = runCompute(reviewPetitionHelper, {
       state: {
-        advancedSearchForm: {
-          countryType: ContactFactory.COUNTRY_TYPES.DOMESTIC,
+        form: {
+          hasVerifiedIrsNotice: true,
+          irsNoticeDate: '2020-01-05T03:30:45.007Z',
+          mailingDate: '2020-01-05T03:30:45.007Z',
+          petitionPaymentStatus: PAYMENT_STATUS.PAID,
+          receivedAt: '2020-01-05T03:30:45.007Z',
         },
       },
     });
-    expect(result).toEqual({ showStateSelect: true });
-  });
 
-  it('returns showStateSelect false when state.advancedSearchForm.countryType is "international"', () => {
-    const result = runCompute(advancedSearchHelper, {
-      state: {
-        advancedSearchForm: {
-          countryType: ContactFactory.COUNTRY_TYPES.INTERNATIONAL,
-        },
-      },
+    expect(result).toEqual({
+      hasIrsNoticeFormatted: 'Yes',
+      irsNoticeDateFormatted: '01/04/2020',
+      mailingDateFormatted: '01/04/2020',
+      petitionPaymentStatusFormatted: 'Paid',
+      receivedAtFormatted: '01/04/2020',
+      shouldShowIrsNoticeDate: true,
     });
-    expect(result).toEqual({ showStateSelect: false });
-  });
-
-  it('returns showNoMatches true and showSearchResults false if searchResults is an empty array', () => {
-    const result = runCompute(advancedSearchHelper, {
-      state: {
-        advancedSearchForm: { currentPage: 1 },
-        searchResults: [],
-      },
-    });
-    expect(result).toMatchObject({
-      showLoadMore: false,
-      showNoMatches: true,
-      showSearchResults: false,
-    });
-  });
-
-  it('returns showNoMatches false, showSearchResults true, and the results count if searchResults is an not empty array', () => {
-    const result = runCompute(advancedSearchHelper, {
-      state: {
-        advancedSearchForm: { currentPage: 1 },
-        searchResults: [
-          {
-            contactPrimary: { name: 'Test Person', state: 'TN' },
-            docketNumber: '101-19',
-          },
-        ],
-      },
-    });
-    expect(result).toMatchObject({
-      searchResultsCount: 1,
-      showLoadMore: false,
-      showNoMatches: false,
-      showSearchResults: true,
-    });
-  });
-
-  it('formats search results', () => {
-    const result = runCompute(advancedSearchHelper, {
-      state: {
-        advancedSearchForm: { currentPage: 1 },
-        searchResults: [
-          {
-            caseCaption: 'Test Petitioner, Petitioner',
-            contactPrimary: { name: 'Test Person', state: 'TN' },
-            docketNumber: '101-19',
-            receivedAt: '2019-03-01T05:00:00.000Z',
-          },
-          {
-            caseCaption: 'Test Petitioner & Another Petitioner, Petitioner(s)',
-            contactPrimary: { name: 'Test Person', state: 'TX' },
-            contactSecondary: { name: 'Another Person', state: 'TX' },
-            docketNumber: '102-18',
-            docketNumberSuffix: 'W',
-            receivedAt: '2019-05-01T05:00:00.000Z',
-          },
-        ],
-      },
-    });
-    expect(result.formattedSearchResults).toMatchObject([
-      {
-        caseCaptionNames: 'Test Petitioner',
-        contactPrimaryName: 'Test Person',
-        contactSecondaryName: undefined,
-        docketNumberWithSuffix: '101-19',
-        formattedFiledDate: '03/01/19',
-        fullStateNamePrimary: 'Tennessee',
-      },
-      {
-        caseCaptionNames: 'Test Petitioner & Another Petitioner',
-        contactPrimaryName: 'Test Person',
-        contactSecondaryName: 'Another Person',
-        docketNumberWithSuffix: '102-18W',
-        formattedFiledDate: '05/01/19',
-        fullStateNamePrimary: 'Texas',
-      },
-    ]);
-  });
-
-  it('only returns formatted results that should be currently shown based on form.currentPage', () => {
-    pageSizeOverride = 1;
-    let result = runCompute(advancedSearchHelper, {
-      state: {
-        advancedSearchForm: { currentPage: 1 },
-        searchResults: [
-          {
-            caseCaption: 'Test Petitioner, Petitioner',
-            contactPrimary: { name: 'Test Person', state: 'TN' },
-            docketNumber: '101-19',
-            receivedAt: '2019-03-01T05:00:00.000Z',
-          },
-          {
-            caseCaption: 'Test Petitioner & Another Petitioner, Petitioner(s)',
-            contactPrimary: { name: 'Test Person', state: 'TX' },
-            contactSecondary: { name: 'Another Person', state: 'TX' },
-            docketNumber: '102-18',
-            docketNumberSuffix: 'W',
-            receivedAt: '2018-05-01T05:00:00.000Z',
-          },
-        ],
-      },
-    });
-    expect(result.showLoadMore).toEqual(true);
-    expect(result.formattedSearchResults.length).toEqual(1);
-    expect(result.formattedSearchResults).toMatchObject([
-      {
-        caseCaptionNames: 'Test Petitioner',
-        contactPrimaryName: 'Test Person',
-        docketNumberWithSuffix: '101-19',
-        receivedAt: '2019-03-01T05:00:00.000Z',
-      },
-    ]);
-
-    result = runCompute(advancedSearchHelper, {
-      state: {
-        advancedSearchForm: { currentPage: 3 },
-        searchResults: [
-          {
-            caseCaption: 'Test Petitioner, Petitioner',
-            contactPrimary: { name: 'Test Person', state: 'TN' },
-            docketNumber: '101-19',
-            receivedAt: '2019-03-01T05:00:00.000Z',
-          },
-          {
-            caseCaption: 'Test Petitioner & Another Petitioner, Petitioner(s)',
-            contactPrimary: { name: 'Test Person', state: 'TX' },
-            contactSecondary: { name: 'Another Person', state: 'TX' },
-            docketNumber: '102-18',
-            docketNumberSuffix: 'W',
-            receivedAt: '2018-05-01T05:00:00.000Z',
-          },
-          {
-            caseCaption: 'Test Petitioner & Another Petitioner, Petitioner(s)',
-            contactPrimary: { name: 'Test Petitioner', state: 'CA' },
-            contactSecondary: { name: 'Another Petitioner', state: 'TN' },
-            docketNumber: '101-18',
-            docketNumberSuffix: 'W',
-            receivedAt: '2018-04-01T05:00:00.000Z',
-          },
-        ],
-      },
-    });
-    expect(result.showLoadMore).toEqual(false);
-    expect(result.formattedSearchResults.length).toEqual(3);
-    expect(result.formattedSearchResults).toMatchObject([
-      {
-        caseCaptionNames: 'Test Petitioner',
-        contactPrimaryName: 'Test Person',
-        contactSecondaryName: undefined,
-        docketNumberWithSuffix: '101-19',
-        formattedFiledDate: '03/01/19',
-        fullStateNamePrimary: 'Tennessee',
-      },
-      {
-        caseCaptionNames: 'Test Petitioner & Another Petitioner',
-        contactPrimaryName: 'Test Person',
-        contactSecondaryName: 'Another Person',
-        docketNumberWithSuffix: '102-18W',
-        formattedFiledDate: '05/01/18',
-        fullStateNamePrimary: 'Texas',
-      },
-      {
-        caseCaptionNames: 'Test Petitioner & Another Petitioner',
-        contactPrimaryName: 'Test Petitioner',
-        contactSecondaryName: 'Another Petitioner',
-        docketNumberWithSuffix: '101-18W',
-        formattedFiledDate: '04/01/18',
-        fullStateNamePrimary: 'California',
-        fullStateNameSecondary: 'Tennessee',
-      },
-    ]);
   });
 });
