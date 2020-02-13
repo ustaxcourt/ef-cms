ddc752028da72e5877963dd3a2d21845531a6364
Joseph Sutton <jsutton@flexion.us>
2020-02-13 08:24:01 -0600

    organized files a bit

--- web-client/src/presenter/actions/StartCaseInternal/getInternalCaseCaptionAction.js
+++ web-client/src/presenter/actions/StartCaseInternal/getInternalCaseCaptionAction.js
@@ -1,14 +1,18 @@
-import { Case } from '../../../../shared/src/business/entities/cases/Case';
 import { state } from 'cerebral';
 
 /**
  * Fetches the case caption using the getCaseCaption helper method
  *
  * @param {object} providers the providers object
+ * @param {Function} providers.applicationContext application context to get Case entity
  * @param {Function} providers.get the cerebral get function
  * @returns {object} contains the caseCaption
  */
-export const getInternalCaseCaptionAction = async ({ get }) => {
+export const getInternalCaseCaptionAction = async ({
+  applicationContext,
+  get,
+}) => {
+  const { Case } = applicationContext.getEntityConstructors();
   let caseCaption = Case.getCaseCaption(get(state.form)) || '';
 
   caseCaption += ` ${Case.CASE_CAPTION_POSTFIX}`;
--- web-client/src/presenter/actions/StartCaseInternal/getInternalCaseCaptionAction.test.js
+++ web-client/src/presenter/actions/StartCaseInternal/getInternalCaseCaptionAction.test.js
@@ -1,11 +1,18 @@
-import { Case } from '../../../../shared/src/business/entities/cases/Case';
-import { ContactFactory } from '../../../../shared/src/business/entities/contacts/ContactFactory';
+import { applicationContext } from '../../../applicationContext';
 import { getInternalCaseCaptionAction } from './getInternalCaseCaptionAction';
+import { presenter } from '../../presenter';
 import { runAction } from 'cerebral/test';
 
+presenter.providers.applicationContext = applicationContext;
+
 describe('getInternalCaseCaptionAction', () => {
+  const { Case, ContactFactory } = applicationContext.getEntityConstructors();
+
   it('should return just the respondent when party type has not been selected', async () => {
     const result = await runAction(getInternalCaseCaptionAction, {
+      modules: {
+        presenter,
+      },
       state: {
         form: {
           partyType: '',
@@ -18,6 +25,9 @@ describe('getInternalCaseCaptionAction', () => {
 
   it('should return a generated case caption when party type has been selected', async () => {
     const result = await runAction(getInternalCaseCaptionAction, {
+      modules: {
+        presenter,
+      },
       state: {
         form: {
           contactPrimary: {
