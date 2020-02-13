67876c59227116ff7c7646dd2db082da4c2ec3d1
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-02-13 13:32:32 -0800

    fixes for judges -> user notes

--- web-client/src/presenter/sequences/gotoTrialSessionWorkingCopySequence.js
+++ web-client/src/presenter/sequences/gotoTrialSessionWorkingCopySequence.js
@@ -72,7 +72,7 @@ const gotoTrialSessionDetails = [
       ...checkUserAssociationAndProceed,
     ],
     judge: checkUserAssociationAndProceed,
-    trialClerk: checkUserAssociationAndProceed,
+    trialclerk: checkUserAssociationAndProceed,
   },
 ];
 
