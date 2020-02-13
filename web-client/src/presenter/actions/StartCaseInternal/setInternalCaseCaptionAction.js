7d73e577ae872f18cb41db4d177ac61259f6ff7e
Joseph Sutton <jsutton@flexion.us>
2020-02-13 08:33:53 -0600

    removed async

--- web-client/src/presenter/actions/StartCaseInternal/setInternalCaseCaptionAction.js
+++ web-client/src/presenter/actions/StartCaseInternal/setInternalCaseCaptionAction.js
@@ -7,6 +7,6 @@ import { state } from 'cerebral';
  * @param {Function} providers.props the cerebral props object used for passing in props.caseCaption
  * @param {Function} providers.store the cerebral store used for setting the state.form.caseCaption
  */
-export const setInternalCaseCaptionAction = async ({ props, store }) => {
+export const setInternalCaseCaptionAction = ({ props, store }) => {
   store.set(state.form.caseCaption, props.caseCaption);
 };
