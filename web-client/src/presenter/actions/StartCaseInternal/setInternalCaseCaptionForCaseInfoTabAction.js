db5a0cee852fed2b4f13be6dd09515109dfb43b7
Joseph Sutton <jsutton@flexion.us>
2020-02-14 10:03:43 -0600

    renamed actions for better context

--- web-client/src/presenter/actions/StartCaseInternal/setInternalCaseCaptionForCaseInfoTabAction.js
+++ web-client/src/presenter/actions/StartCaseInternal/setInternalCaseCaptionForCaseInfoTabAction.js
@@ -7,7 +7,10 @@ import { state } from 'cerebral';
  * @param {Function} providers.props the cerebral props object used for passing in props.caseCaption
  * @param {Function} providers.store the cerebral store used for setting the state.form.caseCaption
  */
-export const setInternalCaseCaptionAction = ({ props, store }) => {
+export const setInternalCaseCaptionForCaseInfoTabAction = ({
+  props,
+  store,
+}) => {
   const { tab } = props;
 
   if (tab === 'caseInfo') {
