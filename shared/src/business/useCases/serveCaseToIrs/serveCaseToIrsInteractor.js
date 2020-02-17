646733490f2b141b56532ad41041f02b0659ecbc
rachelschneiderman <rschneiderman379@gmail.com>
2020-02-17 09:04:46 -0800

    Serve single electronic case to IRS.

--- shared/src/business/useCases/serveCaseToIrs/serveCaseToIrsInteractor.js
+++ shared/src/business/useCases/serveCaseToIrs/serveCaseToIrsInteractor.js
@@ -1,7 +1,15 @@
 const {
+  addDocketEntryForPaymentStatus,
+  deleteStinIfAvailable,
+  uploadZipOfDocuments,
+} = require('../runBatchProcessInteractor');
+const {
   isAuthorized,
   ROLE_PERMISSIONS,
 } = require('../../../authorization/authorizationClientService');
+const { Case } = require('../entities/cases/Case');
+const { createISODateString } = require('../utilities/DateHandler');
+const { PETITIONS_SECTION } = require('../entities/WorkQueue');
 const { UnauthorizedError } = require('../../../errors/errors');
 
 /**
@@ -18,4 +26,72 @@ exports.serveCaseToIrsInteractor = async ({ applicationContext, caseId }) => {
   if (!isAuthorized(user, ROLE_PERMISSIONS.UPDATE_CASE)) {
     throw new UnauthorizedError('Unauthorized');
   }
+
+  const caseToBatch = await applicationContext
+    .getPersistenceGateway()
+    .getCaseByCaseId({
+      applicationContext,
+      caseId: caseId,
+    });
+
+  const caseEntity = new Case(caseToBatch, { applicationContext });
+
+  addDocketEntryForPaymentStatus({ caseEntity });
+
+  await uploadZipOfDocuments({
+    applicationContext,
+    caseEntity,
+  });
+
+  await deleteStinIfAvailable({ applicationContext, caseEntity });
+
+  caseEntity.markAsSentToIRS(createISODateString());
+
+  const petitionDocument = caseEntity.documents.find(
+    document =>
+      document.documentType ===
+      Document.INITIAL_DOCUMENT_TYPES.petition.documentType,
+  );
+
+  const petitionDocumentEntity = new Document(petitionDocument, {
+    applicationContext,
+  });
+  petitionDocumentEntity.setAsServed();
+  caseEntity.updateDocument(petitionDocumentEntity);
+
+  const initializeCaseWorkItem = petitionDocument.workItems.find(
+    workItem => workItem.isInitializeCase,
+  );
+
+  initializeCaseWorkItem.setAsCompleted({
+    message: 'Served on IRS',
+    user,
+  });
+
+  const casePromises = [
+    applicationContext.getPersistenceGateway().putWorkItemInUsersOutbox({
+      applicationContext,
+      section: PETITIONS_SECTION,
+      userId: user.userId,
+      workItem: initializeCaseWorkItem,
+    }),
+    applicationContext.getPersistenceGateway().updateWorkItem({
+      applicationContext,
+      workItemToUpdate: initializeCaseWorkItem,
+    }),
+    applicationContext.getPersistenceGateway().updateCase({
+      applicationContext,
+      caseToUpdate: caseEntity.validate().toRawObject(),
+    }),
+    applicationContext.getUseCaseHelpers().generateCaseConfirmationPdf({
+      applicationContext,
+      caseEntity,
+    }),
+  ];
+
+  await Promise.all(casePromises);
+
+  return {
+    caseDetail: caseEntity.validate().toRawObject(),
+  };
 };
