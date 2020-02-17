9162ec79482fcaf9997b9c9b945a85dc299b084e
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-02-17 10:53:43 -0800

    Devex: migration test coverage

--- web-api/migrations/00007-previous-document.js
+++ web-api/migrations/00007-previous-document.js
@@ -1,28 +1,30 @@
-const { isCaseRecord } = require('./utilities');
+const createApplicationContext = require('../src/applicationContext');
+const { Case } = require('../../shared/src/business/entities/cases/Case');
+const { isCaseRecord, upGenerator } = require('./utilities');
+const applicationContext = createApplicationContext({});
 
-const up = async (documentClient, tableName, forAllRecords) => {
-  await forAllRecords(documentClient, tableName, async item => {
-    if (!isCaseRecord(item)) return;
+const mutateRecord = item => {
+  if (isCaseRecord(item)) {
+    item.documents.forEach(document => {
+      if (document.previousDocument) {
+        const previousDocumentTitle = document.previousDocument;
+        document.previousDocument = {
+          documentTitle: previousDocumentTitle,
+          documentType: previousDocumentTitle,
+        };
+      }
+    });
 
-    if (item.documents) {
-      item.documents.forEach(document => {
-        if (document.previousDocument) {
-          const previousDocumentTitle = document.previousDocument;
-          document.previousDocument = {
-            documentTitle: previousDocumentTitle,
-            documentType: previousDocumentTitle,
-          };
-        }
-      });
+    const caseEntity = new Case(item, { applicationContext })
+      .validate()
+      .toRawObject();
 
-      await documentClient
-        .put({
-          Item: item,
-          TableName: tableName,
-        })
-        .promise();
-    }
-  });
+    const itemToPut = {
+      ...item,
+      ...caseEntity,
+    };
+    return itemToPut;
+  }
 };
 
-module.exports = { up };
+module.exports = { mutateRecord, up: upGenerator(mutateRecord) };
