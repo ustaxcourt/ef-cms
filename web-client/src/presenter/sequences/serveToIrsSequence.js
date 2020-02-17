c06cafc6b7595f608190f0b3f06f4d6f8e975d90
rachelschneiderman <rschneiderman379@gmail.com>
2020-02-17 14:39:46 -0800

    Removed invalid test. Added actions in to navigate to the case confirmation pdf preview for serving a paper case to the IRS.

--- web-client/src/presenter/sequences/serveToIrsSequence.js
+++ web-client/src/presenter/sequences/serveToIrsSequence.js
@@ -1,16 +1,20 @@
+import { clearModalAction } from '../actions/clearModalAction';
 import { closeFileUploadStatusModalAction } from '../actions/closeFileUploadStatusModalAction';
 import { computeDateReceivedAction } from '../actions/DocketEntry/computeDateReceivedAction';
 import { computeIrsNoticeDateAction } from '../actions/StartCaseInternal/computeIrsNoticeDateAction';
 import { createCaseFromPaperAction } from '../actions/createCaseFromPaperAction';
 import { getServeToIrsAlertSuccessAction } from '../actions/StartCaseInternal/getServeToIrsAlertSuccessAction';
 import { navigateToCaseDetailAction } from '../actions/navigateToCaseDetailAction';
+import { navigateToPrintPreviewAction } from '../actions/navigateToPrintPreviewAction';
 import { openFileUploadErrorModal } from '../actions/openFileUploadErrorModal';
 import { openFileUploadStatusModalAction } from '../actions/openFileUploadStatusModalAction';
 import { serveCaseToIrsAction } from '../actions/StartCaseInternal/serveCaseToIrsAction';
 import { setAlertSuccessAction } from '../actions/setAlertSuccessAction';
 import { setCaseAction } from '../actions/setCaseAction';
 import { setDocumentIdAction } from '../actions/setDocumentIdAction';
+import { setPdfPreviewUrlAction } from '../actions/CourtIssuedOrder/setPdfPreviewUrlAction';
 import { setPetitionIdAction } from '../actions/setPetitionIdAction';
+import { setSaveAlertsForNavigationAction } from '../actions/setSaveAlertsForNavigationAction';
 import { showProgressSequenceDecorator } from '../utilities/sequenceHelpers';
 
 export const serveToIrsSequence = [
@@ -35,8 +39,11 @@ export const serveToIrsSequence = [
           ],
           paper: [
             getServeToIrsAlertSuccessAction,
+            setPdfPreviewUrlAction,
             setAlertSuccessAction,
-            navigateToCaseDetailAction,
+            clearModalAction,
+            setSaveAlertsForNavigationAction,
+            navigateToPrintPreviewAction,
           ],
         },
       ],
