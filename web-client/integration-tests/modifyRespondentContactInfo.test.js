ffb5b73544df705db58a2d41b5e330b877822c19
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-02-17 14:48:13 -0800

    4374: DOD - integration test, npm update, swagger

---
+++ web-client/integration-tests/modifyRespondentContactInfo.test.js
@@ -0,0 +1,42 @@
+import { loginAs, setupTest, uploadPetition } from './helpers';
+import respondentLogIn from './journey/respondentLogIn';
+import respondentSignsOut from './journey/respondentSignsOut';
+import respondentUpdatesAddress from './journey/respondentUpdatesAddress';
+import respondentViewsCaseDetailNoticeOfChangeOfAddress from './journey/respondentViewsCaseDetailNoticeOfChangeOfAddress';
+
+const test = setupTest();
+
+describe('Modify Respondent Contact Information', () => {
+  beforeAll(() => {
+    jest.setTimeout(30000);
+  });
+
+  let caseDetail;
+  test.createdDocketNumbers = [];
+
+  for (let i = 0; i < 3; i++) {
+    it(`create case #${i} and associate a respondent`, async () => {
+      await loginAs(test, 'petitioner');
+      caseDetail = await uploadPetition(test);
+      test.createdDocketNumbers.push(caseDetail.docketNumber);
+      await loginAs(test, 'petitionsclerk');
+      await test.runSequence('gotoCaseDetailSequence', {
+        docketNumber: test.createdDocketNumbers[i],
+      });
+      await test.runSequence('updateFormValueSequence', {
+        key: 'respondentSearch',
+        value: 'RT6789',
+      });
+      await test.runSequence('openAddRespondentModalSequence');
+      await test.runSequence('associateRespondentWithCaseSequence');
+      expect(test.getState('caseDetail.respondents.length')).toEqual(1);
+    });
+  }
+
+  respondentLogIn(test);
+  respondentUpdatesAddress(test);
+  for (let i = 0; i < 3; i++) {
+    respondentViewsCaseDetailNoticeOfChangeOfAddress(test, i);
+  }
+  respondentSignsOut(test);
+});
