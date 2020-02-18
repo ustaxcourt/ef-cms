e427667cc16eaeb9fef5b37675c233d666d8ecd2
Matt Hopson <matthopson@gmail.com>
2020-02-18 11:31:49 -0700

    Bug fix

--- web-api/src/public-api/generatePublicDocketRecordPdfLambda.js
+++ web-api/src/public-api/generatePublicDocketRecordPdfLambda.js
@@ -7,15 +7,21 @@ const { genericHandler } = require('../genericHandler');
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
 exports.handler = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    const { caseId, docketRecordSort } = JSON.parse(event.body);
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      const { caseId, docketRecordSort } = JSON.parse(event.body);
 
-    return await applicationContext
-      .getUseCases()
-      .generateDocketRecordPdfInteractor({
-        applicationContext,
-        caseId,
-        docketRecordSort,
-        includePartyDetail: false,
-      });
-  });
+      return await applicationContext
+        .getUseCases()
+        .generateDocketRecordPdfInteractor({
+          applicationContext,
+          caseId,
+          docketRecordSort,
+          includePartyDetail: false,
+        });
+    },
+    {
+      user: {},
+    },
+  );
