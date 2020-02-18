73072bafc4354daade2160d04c6758f44e9cedde
Joseph Sutton <jsutton@flexion.us>
2020-02-18 11:23:55 -0600

    remove unused line

--- web-api/deploy-diff.sh
+++ web-api/deploy-diff.sh
@@ -2,7 +2,6 @@
 
 STAGE=$1
 CHECK_PATH=$2
-ORG="${CIRCLE_PROJECT_USERNAME:-"flexion"}"
 CURRENT_SHA="${CIRCLE_SHA1:-`git rev-parse HEAD`}"
 
 DEPLOYED_SHA=`curl https://ui-$STAGE.$EFCMS_DOMAIN | awk '/meta name/{ gsub(/.*meta revision=\042|\042.*/,"");print }'`;
