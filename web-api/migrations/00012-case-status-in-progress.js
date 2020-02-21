b40e9b1bcf9a96c1cc4e830c55de997170e4ec1a
Joseph Sutton <jsutton@flexion.us>
2020-02-21 13:30:42 -0600

    return the raw object of the validated entity in new migration

--- web-api/migrations/00012-case-status-in-progress.js
+++ web-api/migrations/00012-case-status-in-progress.js
@@ -1,14 +1,24 @@
+const createApplicationContext = require('../src/applicationContext');
 const { Case } = require('../../shared/src/business/entities/cases/Case');
 const { isCaseRecord, upGenerator } = require('./utilities');
+const applicationContext = createApplicationContext({});
 
 const mutateRecord = item => {
   if (
     isCaseRecord(item) &&
     ['Batched for IRS', 'Recalled'].includes(item.status)
   ) {
-    item.status = Case.STATUS_TYPES.inProgress;
+    const caseEntity = new Case(
+      {
+        ...item,
+        status: Case.STATUS_TYPES.inProgress,
+      },
+      { applicationContext },
+    )
+      .validate()
+      .toRawObject();
 
-    return item;
+    return caseEntity;
   }
 };
 
