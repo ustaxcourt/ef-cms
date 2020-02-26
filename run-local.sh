6a953be34592e8659e4612bdf7c7b33cb67eec5d
Joseph Sutton <jsutton@flexion.us>
2020-02-26 11:05:13 -0600

    try with keeping start-s3rver.js script, but just remove buckets prior to initialization

--- run-local.sh
+++ run-local.sh
@@ -32,7 +32,7 @@ pkill -f s3rver
 
 echo "starting s3rver"
 rm -rf ./web-api/storage/s3/*
-npx s3rver -d ./web-api/storage/s3 -p 9000 --service-endpoint localhost --configure-bucket $DOCUMENTS_BUCKET_NAME ./web-api/cors-policy.xml --configure-bucket $TEMP_DOCUMENTS_BUCKET_NAME ./web-api/cors-policy.xml &
+node ./web-api/start-s3rver &
 S3RVER_PID=$!
 ./wait-until.sh http://localhost:9000/ 200
 
