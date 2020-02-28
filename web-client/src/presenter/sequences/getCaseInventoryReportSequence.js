bd68373824c76131bde5e45fc1e1d2437c33a514
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-02-28 14:50:52 -0800

    4382: UI validation if user tries to remove all filters on case inventory report

--- web-client/src/presenter/sequences/getCaseInventoryReportSequence.js
+++ web-client/src/presenter/sequences/getCaseInventoryReportSequence.js
@@ -1,8 +1,15 @@
+import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { clearCaseInventoryReportDataAction } from '../actions/CaseInventoryReport/clearCaseInventoryReportDataAction';
 import { getCaseInventoryReportAction } from '../actions/CaseInventoryReport/getCaseInventoryReportAction';
+import { setAlertErrorAction } from '../actions/setAlertErrorAction';
 import { showProgressSequenceDecorator } from '../utilities/sequenceHelpers';
+import { updateCaseInventoryFilterAction } from '../actions/CaseInventoryReport/updateCaseInventoryFilterAction';
 
 export const getCaseInventoryReportSequence = showProgressSequenceDecorator([
-  clearCaseInventoryReportDataAction,
-  getCaseInventoryReportAction,
+  clearAlertsAction,
+  updateCaseInventoryFilterAction,
+  {
+    no: [setAlertErrorAction],
+    proceed: [clearCaseInventoryReportDataAction, getCaseInventoryReportAction],
+  },
 ]);
