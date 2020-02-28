81d195d64584943eec4832efa9b5d7a4390d9e8f
Kris Koskelin <kkoskelin@flexion.us>
2020-02-28 13:40:26 -0600

    4296: abbreviate overly-long strings on PDFPreviewButtons

--- web-client/src/views/PDFPreviewButton.jsx
+++ web-client/src/views/PDFPreviewButton.jsx
@@ -2,6 +2,7 @@ import { Button } from '../ustc-ui/Button/Button';
 import { PDFPreviewErrorModal } from './PDFPreviewErrorModal';
 import { PDFPreviewModal } from './PDFPreviewModal';
 import { connect } from '@cerebral/react';
+import { getStringAbbreviation } from '../utilities/getStringAbbreviation';
 import { sequences, state } from 'cerebral';
 import React from 'react';
 
@@ -19,6 +20,8 @@ export const PDFPreviewButton = connect(
     title,
   }) => {
     const modalId = `PDFPreviewModal-${title}`;
+    const fullTitle = file.name || file.documentType || title;
+    const abbrevTitle = getStringAbbreviation(fullTitle, 50);
     return (
       <>
         <Button
@@ -26,11 +29,12 @@ export const PDFPreviewButton = connect(
           className="pdf-preview-btn padding-0"
           icon={['fas', 'file-pdf']}
           iconColor="blue"
+          title={fullTitle}
           onClick={() => {
             return openPdfPreviewModalSequence({ file, modalId });
           }}
         >
-          {file.name || file.documentType || title}
+          {abbrevTitle}
         </Button>
         {showModal == modalId &&
           (pdfPreviewModalHelper.displayErrorText ? (
