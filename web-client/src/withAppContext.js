a7ad4904f291d633dec0f91bb34c467f463bfb09
Kris Koskelin <kkoskelin@flexion.us>
2020-03-02 11:41:34 -0600

    test coverage for withAppContext

--- web-client/src/withAppContext.js
+++ web-client/src/withAppContext.js
@@ -1,9 +1,9 @@
 import { applicationContext } from './applicationContext';
 
 export const withAppContextDecorator = (f, context) => {
-  return get => {
+  return async get => {
     try {
-      return f(get, context || applicationContext);
+      return await f(get, context || applicationContext);
     } catch (err) {
       if (process.env.USTC_DEBUG) {
         return null;
