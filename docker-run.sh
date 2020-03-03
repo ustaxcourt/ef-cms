25da091cfa8b3ea488d441927e8520960ff9d792
Joseph Sutton <jsutton@flexion.us>
2020-03-02 19:10:28 -0600

    get s3rver running in docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -e
 docker build -t efcms -f Dockerfile .
-docker run -t -i \
-  -e AWS_ACCESS_KEY_ID=noop \
-  -e AWS_SECRET_ACCESS_KEY=noop \
+docker run -it \
+  -e AWS_ACCESS_KEY_ID=S3RVER \
+  -e AWS_SECRET_ACCESS_KEY=S3RVER \
   -p 1234:1234 \
   -p 5678:5678 \
   -p 3000:3000 \
   -p 8000:8000 \
   -p 8001:8001 \
   -p 9000:9000 \
+  -p 9200:9200 \
   --rm efcms /bin/sh \
   -c "(npm run start:api &) && (npm run dynamo:admin &) && (./wait-until-services.sh && sleep 10 && npm run print:success &) && (npm run start:client &) && npm run start:public"
