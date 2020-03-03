d43e05f36293b8236d4cf28d1964aa74df6b9891
Matt Hopson <matthopson@gmail.com>
2020-03-03 10:45:05 -0700

    Address sprint 34 feedback

--- web-client/src/presenter/actions/caseDetailEdit/setCaseCaptionForCaseInfoTabAction.test.js
+++ web-client/src/presenter/actions/caseDetailEdit/setCaseCaptionForCaseInfoTabAction.test.js
@@ -2,7 +2,7 @@ import { runAction } from 'cerebral/test';
 import { setCaseCaptionForCaseInfoTabAction } from './setCaseCaptionForCaseInfoTabAction';
 
 describe('setCaseCaptionForCaseInfoTabAction', () => {
-  it('should set state.form.caseCaption to the passed in props.caseCaption if the tab is caseInfo', async () => {
+  it('should set state.form.caseCaption to the passed in props.caseCaption if the tab is partyInfo', async () => {
     const result = await runAction(setCaseCaptionForCaseInfoTabAction, {
       props: {
         caseCaption: 'something something',
@@ -13,7 +13,7 @@ describe('setCaseCaptionForCaseInfoTabAction', () => {
     expect(result.state.caseDetail.caseCaption).toEqual('something something');
   });
 
-  it('should not set state.caseDetail.caseCaption to the passed in props.caseCaption if the tab is not caseInfo', async () => {
+  it('should not set state.caseDetail.caseCaption to the passed in props.caseCaption if the tab is not partyInfo', async () => {
     const result = await runAction(setCaseCaptionForCaseInfoTabAction, {
       props: {
         caseCaption: 'something something',
