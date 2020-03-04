6ff430f9ae566744d340227add4ee4ea9c308a0e
Joseph Sutton <jsutton@flexion.us>
2020-03-04 15:44:10 -0600

    cleaned up getCase methods

--- shared/src/persistence/dynamo/cases/getCaseByDocketNumber.js
+++ shared/src/persistence/dynamo/cases/getCaseByDocketNumber.js
@@ -24,30 +24,25 @@ exports.getCaseByDocketNumber = async ({
     stripWorkItems(aCase, applicationContext.isAuthorizedForWorkItems()),
   );
 
-  let docketRecord = [];
-
-  if (theCase) {
-    docketRecord = await client.query({
-      ExpressionAttributeNames: {
-        '#pk': 'pk',
-        '#sk': 'sk',
-      },
-      ExpressionAttributeValues: {
-        ':pk': `case|${theCase.caseId}`,
-        ':prefix': 'docket-record',
-      },
-      KeyConditionExpression: '#pk = :pk and begins_with(#sk, :prefix)',
-      applicationContext,
-    });
-
-    docketRecord =
-      docketRecord.length > 0 ? docketRecord : theCase.docketRecord;
-
-    return {
-      ...theCase,
-      docketRecord, // this is temp until sesed data fixed
-    };
-  } else {
+  if (!theCase) {
     return null;
   }
+
+  const docketRecord = await client.query({
+    ExpressionAttributeNames: {
+      '#pk': 'pk',
+      '#sk': 'sk',
+    },
+    ExpressionAttributeValues: {
+      ':pk': `case|${theCase.caseId}`,
+      ':prefix': 'docket-record',
+    },
+    KeyConditionExpression: '#pk = :pk and begins_with(#sk, :prefix)',
+    applicationContext,
+  });
+
+  return {
+    ...theCase,
+    docketRecord: docketRecord.length > 0 ? docketRecord : theCase.docketRecord, // this is temp until sesed data fixed
+  };
 };
