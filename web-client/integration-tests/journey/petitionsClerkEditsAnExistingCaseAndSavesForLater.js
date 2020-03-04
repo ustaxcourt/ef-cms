04f23b16470a839f51c78c6d9b3cf197a262d423
Rachel Schneiderman <rschneiderman@flexion.us>
2020-03-04 14:41:53 -0800

    Refactored where test fixture data is retrieved.

--- web-client/integration-tests/journey/petitionsClerkEditsAnExistingCaseAndSavesForLater.js
+++ web-client/integration-tests/journey/petitionsClerkEditsAnExistingCaseAndSavesForLater.js
@@ -1,8 +1,5 @@
 export default test => {
   return it('Petitions clerk edits an case and saves for later', async () => {
-    test.docketNumber = test.getState('cases.0.docketNumber');
-    test.documentId = test.getState('cases.0.documents.0.documentId');
-
     await test.runSequence('gotoDocumentDetailSequence', {
       docketNumber: test.docketNumber,
       documentId: test.documentId,
