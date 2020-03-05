9222dc9a422e347eb7ac03eac7e37a8212c5e717
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-03-05 11:23:36 -0800

    480: display inactive users in attorney list

--- web-client/src/presenter/computeds/attorneyListHelper.js
+++ web-client/src/presenter/computeds/attorneyListHelper.js
@@ -3,8 +3,13 @@ import { state } from 'cerebral';
 export const attorneyListHelper = get => {
   const practitionerUsers = get(state.practitionerUsers);
   const respondentUsers = get(state.respondentUsers);
+  const inactivePractitionerUsers = get(state.inactivePractitionerUsers);
+  const inactiveRespondentUsers = get(state.inactiveRespondentUsers);
 
-  const attorneyUsers = practitionerUsers.concat(respondentUsers);
+  const attorneyUsers = practitionerUsers
+    .concat(respondentUsers)
+    .concat(inactivePractitionerUsers)
+    .concat(inactiveRespondentUsers);
 
   return {
     attorneyUsers,
