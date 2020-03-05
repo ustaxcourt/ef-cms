3180cd81a098a02741699c2034de596b36f596f4
Eric Sorenson <eric@sorenson.software>
2020-03-05 11:28:07 -0600

    4296 scenario 1

--- web-client/integration-tests/journey/petitionsClerkCreatesNewCaseAndSavesForLater.js
+++ web-client/integration-tests/journey/petitionsClerkCreatesNewCaseAndSavesForLater.js
@@ -1,119 +1,121 @@
 export default (test, fakeFile, trialLocation = 'Birmingham, Alabama') => {
+  const formValues = [
+    {
+      key: 'dateReceivedMonth',
+      value: '01',
+    },
+    {
+      key: 'dateReceivedDay',
+      value: '01',
+    },
+    {
+      key: 'dateReceivedYear',
+      value: '2001',
+    },
+    {
+      key: 'mailingDate',
+      value: 'Some Day',
+    },
+    {
+      key: 'caseCaption',
+      value: 'Test Person, Deceased, Test Person, Surviving Spouse, Petitioner',
+    },
+    {
+      key: 'petitionFile',
+      value: fakeFile,
+    },
+    {
+      key: 'petitionFileSize',
+      value: 1,
+    },
+    {
+      key: 'stinFile',
+      value: fakeFile,
+    },
+    {
+      key: 'stinFileSize',
+      value: 1,
+    },
+    {
+      key: 'ownershipDisclosureFile',
+      value: fakeFile,
+    },
+    {
+      key: 'ownershipDisclosureFileSize',
+      value: 1,
+    },
+    {
+      key: 'requestForPlaceOfTrialFile',
+      value: fakeFile,
+    },
+    {
+      key: 'requestForPlaceOfTrialFileSize',
+      value: 1,
+    },
+    {
+      key: 'applicationForWaiverOfFilingFeeFile',
+      value: fakeFile,
+    },
+    {
+      key: 'applicationForWaiverOfFilingFeeFileSize',
+      value: 1,
+    },
+    {
+      key: 'preferredTrialCity',
+      value: trialLocation,
+    },
+    {
+      key: 'procedureType',
+      value: 'Small',
+    },
+    {
+      key: 'caseType',
+      value: 'Deficiency',
+    },
+    {
+      key: 'partyType',
+      value: 'Petitioner',
+    },
+    {
+      key: 'contactPrimary.countryType',
+      value: 'international',
+    },
+    {
+      key: 'contactPrimary.country',
+      value: 'Switzerland',
+    },
+    {
+      key: 'contactPrimary.name',
+      value: 'Test Person',
+    },
+    {
+      key: 'contactPrimary.address1',
+      value: '123 Abc Ln',
+    },
+    {
+      key: 'contactPrimary.city',
+      value: 'Cityville',
+    },
+    {
+      key: 'contactPrimary.postalCode',
+      value: '23-skidoo',
+    },
+    {
+      key: 'contactPrimary.email',
+      value: 'test@example.com',
+    },
+    {
+      key: 'contactPrimary.phone',
+      value: '1234567890',
+    },
+  ];
+
   return it('Petitions clerk creates a new case and saves for later', async () => {
     await test.runSequence('gotoStartCaseWizardSequence');
     await test.runSequence('navigateToReviewPetitionFromPaperSequence');
 
-    const formValues = [
-      {
-        key: 'dateReceivedMonth',
-        value: '01',
-      },
-      {
-        key: 'dateReceivedDay',
-        value: '01',
-      },
-      {
-        key: 'dateReceivedYear',
-        value: '2001',
-      },
-      {
-        key: 'mailingDate',
-        value: 'Some Day',
-      },
-      {
-        key: 'caseCaption',
-        value:
-          'Test Person, Deceased, Test Person, Surviving Spouse, Petitioner',
-      },
-      {
-        key: 'petitionFile',
-        value: fakeFile,
-      },
-      {
-        key: 'petitionFileSize',
-        value: 1,
-      },
-      {
-        key: 'stinFile',
-        value: fakeFile,
-      },
-      {
-        key: 'stinFileSize',
-        value: 1,
-      },
-      {
-        key: 'ownershipDisclosureFile',
-        value: fakeFile,
-      },
-      {
-        key: 'ownershipDisclosureFileSize',
-        value: 1,
-      },
-      {
-        key: 'requestForPlaceOfTrialFile',
-        value: fakeFile,
-      },
-      {
-        key: 'requestForPlaceOfTrialFileSize',
-        value: 1,
-      },
-      {
-        key: 'applicationForWaiverOfFilingFeeFile',
-        value: fakeFile,
-      },
-      {
-        key: 'applicationForWaiverOfFilingFeeFileSize',
-        value: 1,
-      },
-      {
-        key: 'preferredTrialCity',
-        value: trialLocation,
-      },
-      {
-        key: 'procedureType',
-        value: 'Small',
-      },
-      {
-        key: 'caseType',
-        value: 'Deficiency',
-      },
-      {
-        key: 'partyType',
-        value: 'Petitioner',
-      },
-      {
-        key: 'contactPrimary.countryType',
-        value: 'international',
-      },
-      {
-        key: 'contactPrimary.country',
-        value: 'Switzerland',
-      },
-      {
-        key: 'contactPrimary.name',
-        value: 'Test Person',
-      },
-      {
-        key: 'contactPrimary.address1',
-        value: '123 Abc Ln',
-      },
-      {
-        key: 'contactPrimary.city',
-        value: 'Cityville',
-      },
-      {
-        key: 'contactPrimary.postalCode',
-        value: '23-skidoo',
-      },
-      {
-        key: 'contactPrimary.email',
-        value: 'test@example.com',
-      },
-      {
-        key: 'contactPrimary.phone',
-        value: '1234567890',
-      },
-    ];
+    expect(test.getState('currentPage')).toEqual('StartCaseInternal');
+    expect(test.getState('startCaseInternal.tab')).toBe('partyInfo');
 
     formValues.forEach(async item => {
       await test.runSequence('updateFormValueSequence', item);
