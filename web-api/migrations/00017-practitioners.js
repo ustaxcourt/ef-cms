1db933b4518987b1c05a35cfebf99cc42933014b
Joseph Sutton <jsutton@flexion.us>
2020-03-05 21:01:46 -0600

    added practitioners migrations, and updated seed data for practitioners and respondents

---
+++ web-api/migrations/00017-practitioners.js
@@ -0,0 +1,32 @@
+const createApplicationContext = require('../src/applicationContext');
+const { Case } = require('../../shared/src/business/entities/cases/Case');
+const { isCaseRecord, upGenerator } = require('./utilities');
+const applicationContext = createApplicationContext({});
+
+const mutateRecord = async (item, documentClient, tableName) => {
+  if (isCaseRecord(item)) {
+    const caseEntity = new Case(item, { applicationContext });
+
+    for (const practitioner of caseEntity.practitioners) {
+      const practitionerId = applicationContext.getUniqueId();
+      await documentClient
+        .put({
+          Item: {
+            ...practitioner,
+            pk: `case|${item.caseId}`,
+            sk: `practitioner|${practitionerId}`,
+          },
+          TableName: tableName,
+        })
+        .promise();
+    }
+
+    delete caseEntity.practitioners;
+
+    console.log(`removing practitioners[] from case ${item.caseId}`);
+
+    return { ...item, ...caseEntity.toRawObject() };
+  }
+};
+
+module.exports = { mutateRecord, up: upGenerator(mutateRecord) };
