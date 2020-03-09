f7d690dbd89122f5f6c324a8245ee5baf09be0d1
Mrinalini <mrinalinisinha05@gmail.com>
2020-03-09 09:11:31 -0700

    Working on test scenario 15

--- web-client/integration-tests/journey/petitionsClerkEditsAnExistingCaseAndSavesForLater.js
+++ web-client/integration-tests/journey/petitionsClerkEditsAnExistingCaseAndSavesForLater.js
@@ -1,4 +1,5 @@
 import { Case } from '../../../shared/src/business/entities/cases/Case';
+import { getFormattedDocumentQCSectionOutbox, wait } from '../helpers';
 
 export default test => {
   return it('Petitions clerk edits an existing case and saves for later', async () => {
@@ -20,35 +21,77 @@ export default test => {
     await test.runSequence('navigateToReviewPetitionFromPaperSequence');
     await test.runSequence('gotoReviewPetitionFromPaperSequence');
 
-    // expect(test.getState('currentPage')).toEqual('ReviewPetitionFromPaper');
+    expect(test.getState('currentPage')).toEqual('ReviewPetitionFromPaper');
 
     await test.runSequence('saveSavedCaseForLaterSequence');
+    await wait(5000);
 
-    // expect(test.getState('currentPage')).toEqual('Messages');
+    expect(test.getState('currentPage')).toEqual('Messages');
 
-    /////
     await test.runSequence('gotoDocumentDetailSequence', {
       docketNumber: test.docketNumber,
       documentId: test.documentId,
     });
-
     await test.runSequence('openConfirmServeToIrsModalSequence');
 
     expect(test.getState('showModal')).toBe('ConfirmServeToIrsModal');
 
     await test.runSequence('saveCaseAndServeToIrsSequence');
+    await wait(5000);
 
-    expect(test.getState('currentPage')).toEqual('CaseDetail');
+    expect(test.getState('currentPage')).toEqual('CaseDetailInternal');
 
-    // await test.runSequence('chooseWorkQueueSequence', {
-    //   box: 'served',
-    //   queue: 'my',
-    // });
-    // const workQueue = test.getState('workQueue');
-    // const servedCase = workQueue.find(
-    //   i => i.docketNumber === test.docketNumber,
-    // );
+    await test.runSequence('navigateToPathSequence', {
+      path: '/document-qc/my/outbox',
+    });
+    await test.runSequence('chooseWorkQueueSequence', {
+      box: 'outbox',
+      queue: 'my',
+    });
+    await wait(5000);
 
-    // expect(servedCase.status).toEqual(Case.STATUS_TYPES.generalDocket);
+    const workQueueToDisplay = test.getState('workQueueToDisplay');
+
+    expect(workQueueToDisplay.workQueueIsInternal).toBeFalsy();
+    expect(workQueueToDisplay.queue).toEqual('my');
+    expect(workQueueToDisplay.box).toEqual('outbox');
+
+    const servedCase = test
+      .getState('workQueue')
+      .find(x => x.docketNumber === test.docketNumber);
+
+    expect(servedCase).toMatchObject({
+      caseTitle: 'Mona Schultz',
+    });
+    expect(servedCase.caseStatus).toEqual(Case.STATUS_TYPES.generalDocket);
+
+    await test.runSequence('navigateToPathSequence', {
+      path: 'document-qc/section/outbox',
+    });
+    await test.runSequence('chooseWorkQueueSequence', {
+      box: 'outbox',
+      queue: 'section',
+    });
+    await wait(5000);
+
+    const sectionWorkQueueToDisplay = test.getState('workQueueToDisplay');
+
+    expect(sectionWorkQueueToDisplay.workQueueIsInternal).toBeFalsy();
+    expect(sectionWorkQueueToDisplay.queue).toEqual('section');
+    expect(sectionWorkQueueToDisplay.box).toEqual('outbox');
+
+    const sectionServedCase = test
+      .getState('workQueue')
+      .find(x => x.docketNumber === test.docketNumber);
+
+    const results = await getFormattedDocumentQCSectionOutbox(test);
+    console.log(results);
+
+    expect(sectionServedCase).toMatchObject({
+      caseTitle: 'Mona Schultz',
+    });
+    expect(sectionServedCase.caseStatus).toEqual(
+      Case.STATUS_TYPES.generalDocket,
+    );
   });
 };
