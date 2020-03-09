8d733ebd23d57ed60de8fdfa89d06a58f0f870dc
amy smith <asmith@flexion.us>
2020-03-09 09:22:48 -0400

    updates to erro message text
    updates to erro message text

--- web-client/src/views/DocumentDetail/PendingMessages.jsx
+++ web-client/src/views/DocumentDetail/PendingMessages.jsx
@@ -335,7 +335,7 @@ export const PendingMessages = connect(
                             });
                           }}
                         />
-
+                        
                         {workItemMetadata.showChambersSelect && (
                           <Select
                             error={
