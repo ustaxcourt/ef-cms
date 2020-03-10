824338a33db623ef2fed8a0957b0bcf73dc3aa15
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-03-09 18:21:27 -0700

    pa11y test login updates

--- web-client/pa11y/pa11y-respondent.js
+++ web-client/pa11y/pa11y-respondent.js
@@ -1,6 +1,6 @@
 module.exports = [
-  'http://localhost:1234/mock-login?token=respondent&path=/',
-  'http://localhost:1234/mock-login?token=respondent&path=/case-detail/101-19',
+  'http://localhost:1234/mock-login?token=irsPractitioner&path=/',
+  'http://localhost:1234/mock-login?token=irsPractitioner&path=/case-detail/101-19',
   {
     actions: [
       'wait for #button-first-irs-document to be visible',
@@ -9,7 +9,7 @@ module.exports = [
     ],
     notes: 'checks a11y of revealed form for filing document',
     url:
-      'http://localhost:1234/mock-login?token=respondent&path=/case-detail/102-19&info=can-file-document',
+      'http://localhost:1234/mock-login?token=irsPractitioner&path=/case-detail/102-19&info=can-file-document',
   },
   {
     actions: [
@@ -18,7 +18,7 @@ module.exports = [
     ],
     notes: 'an associated respondent can see details of sealed case',
     url:
-      'http://localhost:1234/mock-login?token=respondent&path=/case-detail/102-20&info=associated-respondent',
+      'http://localhost:1234/mock-login?token=irsPractitioner&path=/case-detail/102-20&info=associated-respondent',
   },
-  'http://localhost:1234/mock-login?token=respondent&path=/search/no-matches',
+  'http://localhost:1234/mock-login?token=irsPractitioner&path=/search/no-matches',
 ];
