b07a6cae89049eef22998f3bdf712b9f46a81223
Joseph Sutton <jsutton@flexion.us>
2020-03-11 11:32:15 -0500

    refactored getCaseByCaseId persistence method

--- shared/src/persistence/dynamo/cases/getCaseByCaseId.js
+++ shared/src/persistence/dynamo/cases/getCaseByCaseId.js
@@ -11,73 +11,35 @@ const { stripWorkItems } = require('../../dynamo/helpers/stripWorkItems');
  * @returns {object} the case details
  */
 exports.getCaseByCaseId = async ({ applicationContext, caseId }) => {
-  const theCase = await client
-    .get({
-      Key: {
-        pk: `case|${caseId}`,
-        sk: `case|${caseId}`,
+  const caseItems = await client
+    .query({
+      ExpressionAttributeNames: {
+        '#pk': 'pk',
       },
+      ExpressionAttributeValues: {
+        ':pk': `case|${caseId}`,
+      },
+      KeyConditionExpression: '#pk = :pk',
       applicationContext,
     })
     .then(results =>
       stripWorkItems(results, applicationContext.isAuthorizedForWorkItems()),
     );
 
-  if (!theCase) {
+  if (!caseItems) {
     return null;
   }
 
-  const docketRecord = await client.query({
-    ExpressionAttributeNames: {
-      '#pk': 'pk',
-      '#sk': 'sk',
-    },
-    ExpressionAttributeValues: {
-      ':pk': `case|${theCase.caseId}`,
-      ':prefix': 'docket-record',
-    },
-    KeyConditionExpression: '#pk = :pk and begins_with(#sk, :prefix)',
-    applicationContext,
-  });
+  const theCase = caseItems.filter(item => item.sk.includes('case|')).pop();
 
-  const documents = await client.query({
-    ExpressionAttributeNames: {
-      '#pk': 'pk',
-      '#sk': 'sk',
-    },
-    ExpressionAttributeValues: {
-      ':pk': `case|${theCase.caseId}`,
-      ':prefix': 'document',
-    },
-    KeyConditionExpression: '#pk = :pk and begins_with(#sk, :prefix)',
-    applicationContext,
-  });
-
-  const practitioners = await client.query({
-    ExpressionAttributeNames: {
-      '#pk': 'pk',
-      '#sk': 'sk',
-    },
-    ExpressionAttributeValues: {
-      ':pk': `case|${theCase.caseId}`,
-      ':prefix': 'practitioner',
-    },
-    KeyConditionExpression: '#pk = :pk and begins_with(#sk, :prefix)',
-    applicationContext,
-  });
-
-  const respondents = await client.query({
-    ExpressionAttributeNames: {
-      '#pk': 'pk',
-      '#sk': 'sk',
-    },
-    ExpressionAttributeValues: {
-      ':pk': `case|${theCase.caseId}`,
-      ':prefix': 'respondent',
-    },
-    KeyConditionExpression: '#pk = :pk and begins_with(#sk, :prefix)',
-    applicationContext,
-  });
+  const docketRecord = caseItems.filter(item =>
+    item.sk.includes('docket-record|'),
+  );
+  const documents = caseItems.filter(item => item.sk.includes('document|'));
+  const practitioners = caseItems.filter(item =>
+    item.sk.includes('practitioner|'),
+  );
+  const respondents = caseItems.filter(item => item.sk.includes('respondent|'));
 
   const actualDocketRecord =
     docketRecord.length > 0 ? docketRecord : theCase.docketRecord;
@@ -90,8 +52,7 @@ exports.getCaseByCaseId = async ({ applicationContext, caseId }) => {
     ...theCase,
     docketRecord: sortedDocketRecord,
     documents: sortedDocuments,
-    practitioners:
-      practitioners.length > 0 ? practitioners : theCase.practitioners, // this is temp until sesed data fixed
-    respondents: respondents.length > 0 ? respondents : theCase.respondents, // this is temp until sesed data fixed
+    practitioners,
+    respondents,
   };
 };
