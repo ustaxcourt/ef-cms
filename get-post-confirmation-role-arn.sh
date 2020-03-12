3ef11e5a9604904f415a97a719ddfd233509254f
Waldo Jaquith <waldo.jaquith@gsa.gov>
2020-03-12 10:27:52 -0400

    Add a new IRS environment

--- get-post-confirmation-role-arn.sh
+++ get-post-confirmation-role-arn.sh
@@ -6,6 +6,8 @@ if [[ $BRANCH == 'develop' ]] ; then
   echo "${POST_CONFIRMATION_ROLE_ARN_DEV}"
 elif [[ $BRANCH == 'experimental' ]]  ; then
   echo "${POST_CONFIRMATION_ROLE_ARN_EXP}"
+elif [[ $BRANCH == 'irs' ]]  ; then
+  echo "${POST_CONFIRMATION_ROLE_ARN_IRS}"
 elif [[ $BRANCH == 'master' ]] ; then
   echo "${POST_CONFIRMATION_ROLE_ARN_PROD}"
 elif [[ $BRANCH == 'staging' ]]  ; then
