783427e96fb85c9835203145391d3a7c37a2c4cf
Mrinalini <mrinalinisinha05@gmail.com>
2020-03-12 12:30:58 -0700

    4296 work in progress- adding new sequence to onChange of Address.jsx input fields.

--- web-client/src/presenter/sequences/copyPrimaryContactSequence.js
+++ web-client/src/presenter/sequences/copyPrimaryContactSequence.js
@@ -1,7 +1,11 @@
 import { getFormContactPrimaryAction } from '../actions/StartCaseInternal/getFormContactPrimaryAction';
+import { getShouldCopyPrimaryToSecondaryAddressAction } from '../actions/ForwardForm/getShouldCopyPrimaryToSecondaryAddressAction';
 import { setFormContactSecondaryAddressAction } from '../actions/StartCaseInternal/setFormContactSecondaryAddressAction';
 
 export const copyPrimaryContactSequence = [
-  getFormContactPrimaryAction,
-  setFormContactSecondaryAddressAction,
+  getShouldCopyPrimaryToSecondaryAddressAction,
+  {
+    no: [],
+    yes: [getFormContactPrimaryAction, setFormContactSecondaryAddressAction],
+  },
 ];
