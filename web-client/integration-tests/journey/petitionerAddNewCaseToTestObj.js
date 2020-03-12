73d339bdabcfd2bd3751fbd9858772b9bbf0e4fc
Cody Seibert <cseibert113@gmail.com>
2020-03-12 11:17:18 -0400

    removing consoles

--- web-client/integration-tests/journey/petitionerAddNewCaseToTestObj.js
+++ web-client/integration-tests/journey/petitionerAddNewCaseToTestObj.js
@@ -2,8 +2,6 @@ export default (test, createdCases) => {
   return it('[TEST SETUP DATA] Adds the most recent case to the test object', async () => {
     await test.runSequence('gotoDashboardSequence');
     const petitionerNewCase = test.getState('cases.0');
-    console.log('petitionerNewCase', petitionerNewCase);
-    console.log(test.getState('cases'));
     expect(petitionerNewCase).toBeDefined();
     expect(petitionerNewCase.documents[0].workItems[0]).toBeDefined();
     createdCases.push(petitionerNewCase);
