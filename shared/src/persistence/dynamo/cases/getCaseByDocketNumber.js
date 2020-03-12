f74de8ffe94069ddbcbc4d871d77224878e1591c
Cody Seibert <cseibert113@gmail.com>
2020-03-12 14:49:55 -0400

    fixing issue with stripping work items

--- shared/src/persistence/dynamo/cases/getCaseByDocketNumber.js
+++ shared/src/persistence/dynamo/cases/getCaseByDocketNumber.js
@@ -1,9 +1,5 @@
 const client = require('../../dynamodbClientService');
-const {
-  getRecordViaMapping,
-} = require('../../dynamo/helpers/getRecordViaMapping');
-const { sortBy } = require('lodash');
-const { stripWorkItems } = require('../../dynamo/helpers/stripWorkItems');
+const { getCaseByCaseId } = require('../../dynamo/cases/getCaseByCaseId');
 
 /**
  * getCaseByDocketNumber
@@ -17,51 +13,23 @@ exports.getCaseByDocketNumber = async ({
   applicationContext,
   docketNumber,
 }) => {
-  const theCase = await getRecordViaMapping({
+  const [firstEntry] = await client.query({
+    ExpressionAttributeNames: {
+      '#pk': 'pk',
+    },
+    ExpressionAttributeValues: {
+      ':pk': `case-by-docket-number|${docketNumber}`,
+    },
+    KeyConditionExpression: '#pk = :pk',
     applicationContext,
-    pk: `case-by-docket-number|${docketNumber}`,
-    prefix: 'case',
-  }).then(aCase =>
-    stripWorkItems(aCase, applicationContext.isAuthorizedForWorkItems()),
-  );
+  });
 
-  if (!theCase) {
-    return null;
-  }
+  const [, caseId] = firstEntry.sk.split('|');
 
-  const caseItems =
-    (await client
-      .query({
-        ExpressionAttributeNames: {
-          '#pk': 'pk',
-        },
-        ExpressionAttributeValues: {
-          ':pk': `case|${theCase.caseId}`,
-        },
-        KeyConditionExpression: '#pk = :pk',
-        applicationContext,
-      })
-      .then(results =>
-        stripWorkItems(results, applicationContext.isAuthorizedForWorkItems()),
-      )) || [];
+  const fullCase = await getCaseByCaseId({
+    applicationContext,
+    caseId,
+  });
 
-  const docketRecord = caseItems.filter(item =>
-    item.sk.includes('docket-record|'),
-  );
-  const documents = caseItems.filter(item => item.sk.includes('document|'));
-  const practitioners = caseItems.filter(item =>
-    item.sk.includes('practitioner|'),
-  );
-  const respondents = caseItems.filter(item => item.sk.includes('respondent|'));
-
-  const sortedDocketRecord = sortBy(docketRecord, 'index');
-  const sortedDocuments = sortBy(documents, 'createdAt');
-
-  return {
-    ...theCase,
-    docketRecord: sortedDocketRecord,
-    documents: sortedDocuments,
-    practitioners,
-    respondents,
-  };
+  return fullCase;
 };
