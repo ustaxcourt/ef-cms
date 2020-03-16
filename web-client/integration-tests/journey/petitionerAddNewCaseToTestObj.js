c051d4ff13370332f0da9626d0ef28edf8739d16
Joseph Sutton <jsutton@flexion.us>
2020-03-16 13:52:26 -0500

    add small wait

--- web-client/integration-tests/journey/petitionerAddNewCaseToTestObj.js
+++ web-client/integration-tests/journey/petitionerAddNewCaseToTestObj.js
@@ -1,10 +1,9 @@
+import { wait } from '../helpers';
+
 export default (test, createdCases) => {
   return it('[TEST SETUP DATA] Adds the most recent case to the test object', async () => {
     const petitionerNewCase = test.getState('caseDetail');
-    console.log(
-      'petitionerNewCase.documents[0]',
-      petitionerNewCase.documents[0],
-    );
+    await wait(500);
     expect(petitionerNewCase).toBeDefined();
     expect(petitionerNewCase.documents[0].workItems[0]).toBeDefined();
     createdCases.push(petitionerNewCase);
