5c722e077c4a15416281b904689ea4c3e4909f3d
Joseph Sutton <jsutton@flexion.us>
2020-03-16 13:18:48 -0500

    console log out documents[0]

--- web-client/integration-tests/journey/petitionerAddNewCaseToTestObj.js
+++ web-client/integration-tests/journey/petitionerAddNewCaseToTestObj.js
@@ -1,6 +1,10 @@
 export default (test, createdCases) => {
   return it('[TEST SETUP DATA] Adds the most recent case to the test object', async () => {
     const petitionerNewCase = test.getState('caseDetail');
+    console.log(
+      'petitionerNewCase.documents[0]',
+      petitionerNewCase.documents[0],
+    );
     expect(petitionerNewCase).toBeDefined();
     expect(petitionerNewCase.documents[0].workItems[0]).toBeDefined();
     createdCases.push(petitionerNewCase);
