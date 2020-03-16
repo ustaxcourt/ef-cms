4ea53976463c9ce6a87ce8d6cf6191a6b8172a68
Cody Seibert <cseibert113@gmail.com>
2020-03-16 11:47:54 -0400

    refactor the way we get the latest case created

--- web-client/integration-tests/journey/petitionerAddNewCaseToTestObj.js
+++ web-client/integration-tests/journey/petitionerAddNewCaseToTestObj.js
@@ -1,7 +1,6 @@
 export default (test, createdCases) => {
   return it('[TEST SETUP DATA] Adds the most recent case to the test object', async () => {
-    await test.runSequence('gotoDashboardSequence');
-    const petitionerNewCase = test.getState('cases.0');
+    const petitionerNewCase = test.getState('caseDetail');
     expect(petitionerNewCase).toBeDefined();
     expect(petitionerNewCase.documents[0].workItems[0]).toBeDefined();
     createdCases.push(petitionerNewCase);
