9821fa537d0ed07f633eb0959db62bbbc154d67e
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-03-19 12:56:29 -0700

    DevEx: move procedureTypes into constants

--- web-client/integration-tests/journey/practitionerNavigatesToCreateCase.js
+++ web-client/integration-tests/journey/practitionerNavigatesToCreateCase.js
@@ -1,8 +1,5 @@
 export default test => {
   it('practitioner sees the procedure types and case types', async () => {
     await test.runSequence('gotoStartCaseWizardSequence');
-    const procedureTypes = test.getState('procedureTypes');
-    expect(procedureTypes).not.toBeNull;
-    expect(procedureTypes.length).toBeGreaterThan(0);
   });
 };
--- web-client/src/presenter/sequences/gotoEditPetitionDetailsSequence.js
+++ web-client/src/presenter/sequences/gotoEditPetitionDetailsSequence.js
@@ -1,9 +1,7 @@
 import { clearFormAction } from '../actions/clearFormAction';
 import { getCaseAction } from '../actions/getCaseAction';
-import { getProcedureTypesAction } from '../actions/getProcedureTypesAction';
 import { setCaseAction } from '../actions/setCaseAction';
 import { setCurrentPageAction } from '../actions/setCurrentPageAction';
-import { setProcedureTypesAction } from '../actions/setProcedureTypesAction';
 import { setupEditPetitionDetailFormAction } from '../actions/setupEditPetitionDetailFormAction';
 import { stopShowValidationAction } from '../actions/stopShowValidationAction';
 
@@ -12,8 +10,6 @@ export const gotoEditPetitionDetailsSequence = [
   clearFormAction,
   getCaseAction,
   setCaseAction,
-  getProcedureTypesAction,
-  setProcedureTypesAction,
   stopShowValidationAction,
   setupEditPetitionDetailFormAction,
   setCurrentPageAction('EditPetitionDetails'),
