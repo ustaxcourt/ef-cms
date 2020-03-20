0ee1d8774613ff99997fdb3efd839175ae62cada
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-03-20 11:16:06 -0700

    fixes for public search

--- web-client/src/presenter/state-public.js
+++ web-client/src/presenter/state-public.js
@@ -2,19 +2,20 @@ import { menuHelper } from './computeds/menuHelper';
 
 import { advancedSearchHelper } from './computeds/advancedSearchHelper';
 import { loadingHelper } from './computeds/loadingHelper';
+import { publicAlertHelper } from './computeds/public/publicAlertHelper';
 import { publicCaseDetailHeaderHelper } from './computeds/public/publicCaseDetailHeaderHelper';
 import { publicCaseDetailHelper } from './computeds/public/publicCaseDetailHelper';
 
 export const state = {
   advancedSearchForm: {},
   advancedSearchHelper,
+  alertHelper: publicAlertHelper,
   commonUI: {
     showBetaBar: true,
     showMobileMenu: false,
     showUsaBannerDetails: false,
   },
-  currentPage: 'PublicSearch',
-  docketNumberSearchForm: {},
+  currentPage: 'Interstitial',
   loadingHelper,
   menuHelper,
   publicCaseDetailHeaderHelper,
