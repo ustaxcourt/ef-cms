3ee727270c000432884caa118793726ef9f96c87
Matt Hopson <matthopson@gmail.com>
2020-03-20 10:48:19 -0600

    Fix lint

--- web-client/src/presenter/sequences/selectDocumentForPreviewSequence.js
+++ web-client/src/presenter/sequences/selectDocumentForPreviewSequence.js
@@ -1,6 +1,4 @@
-import { props, state } from 'cerebral';
 import { selectDocumentForPreviewAction } from '../actions/selectDocumentForPreviewAction';
-import { set } from 'cerebral/factories';
 import { setPdfPreviewUrlSequence } from './setPdfPreviewUrlSequence';
 
 export const selectDocumentForPreviewSequence = [
