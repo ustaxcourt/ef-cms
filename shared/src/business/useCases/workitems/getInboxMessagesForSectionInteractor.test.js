687cf6f4cd98f0c0661dc023b7dd24ef1c504407
Mrinalini <mrinalinisinha05@gmail.com>
2020-03-20 13:44:40 -0700

    uncommenting file

--- shared/src/business/useCases/workitems/getInboxMessagesForSectionInteractor.test.js
+++ shared/src/business/useCases/workitems/getInboxMessagesForSectionInteractor.test.js
@@ -1,66 +1,65 @@
-// const {
-//   applicationContext,
-// } = require('../../test/createTestApplicationContext');
-// const {
-//   getInboxMessagesForSectionInteractor,
-// } = require('./getInboxMessagesForSectionInteractor');
-// const { User } = require('../../entities/User');
+const {
+  getInboxMessagesForSectionInteractor,
+} = require('./getInboxMessagesForSectionInteractor');
+const { User } = require('../../entities/User');
 
-// describe('getInboxMessagesForSectionInteractor', () => {
-//   // let applicationContext;
-//   const mockPetitionsClerkUser = {
-//     role: User.ROLES.petitionsClerk,
-//     userId: 'petitionsClerk',
-//   };
+describe('getInboxMessagesForSectionInteractor', () => {
+  let applicationContext;
+  let getInboxMessagesForSectionStub;
+  let validateRawCollectionStub;
 
-//   const mockPetitionerUser = {
-//     role: User.ROLES.petitioner,
-//     userId: 'petitioner',
-//   };
+  beforeEach(() => {
+    getInboxMessagesForSectionStub = jest.fn();
+    validateRawCollectionStub = jest.fn();
 
-//   let getInboxMessagesForSectionStub;
-//   let validateRawCollectionStub;
+    applicationContext = {
+      getCurrentUser: () => {
+        return {
+          role: User.ROLES.petitionsClerk,
+          userId: 'petitionsClerk',
+        };
+      },
+      getEntityConstructors: () => ({
+        WorkItem: {
+          validateRawCollection: validateRawCollectionStub,
+        },
+      }),
+      getPersistenceGateway: () => ({
+        getInboxMessagesForSection: getInboxMessagesForSectionStub,
+      }),
+    };
+  });
 
-//   beforeEach(() => {
-//     getInboxMessagesForSectionStub = jest.fn();
-//     validateRawCollectionStub = jest.fn();
+  it('gets inbox messages for a section', async () => {
+    await getInboxMessagesForSectionInteractor({
+      applicationContext,
+      section: 'docket',
+    });
 
-//     // applicationContext = {
-//     //
-//     //   getEntityConstructors: () => ({
-//     //     WorkItem: {
-//     //       validateRawCollection: validateRawCollectionStub,
-//     //     },
-//     //   }),
-//     //   getPersistenceGateway: () => ({
-//     //     getInboxMessagesForSection: getInboxMessagesForSectionStub,
-//     //   }),
-//     // };
-//   });
+    expect(getInboxMessagesForSectionStub).toHaveBeenCalled();
+    expect(validateRawCollectionStub).toHaveBeenCalled();
+  });
 
-//   it('gets inbox messages for a section', async () => {
-//     applicationContext.getCurrentUser.mockReturnValue(mockPetitionsClerkUser);
-//     await getInboxMessagesForSectionInteractor({
-//       applicationContext,
-//       section: 'docket',
-//     });
+  it('throws an error if the user does not have access to the work item', async () => {
+    applicationContext = {
+      ...applicationContext,
+      getCurrentUser: () => {
+        return {
+          role: User.ROLES.petitioner,
+          userId: 'petitioner',
+        };
+      },
+    };
 
-//     expect(getInboxMessagesForSectionStub).toHaveBeenCalled();
-//     expect(validateRawCollectionStub).toHaveBeenCalled();
-//   });
-
-//   it('throws an error if the user does not have access to the work item', async () => {
-//     applicationContext.getCurrentUser.mockReturnValue(mockPetitionerUser);
-
-//     let error;
-//     try {
-//       await getInboxMessagesForSectionInteractor({
-//         applicationContext,
-//         section: 'docket',
-//       });
-//     } catch (e) {
-//       error = e;
-//     }
-//     expect(error).toBeDefined();
-//   });
-// });
+    let error;
+    try {
+      await getInboxMessagesForSectionInteractor({
+        applicationContext,
+        section: 'docket',
+      });
+    } catch (e) {
+      error = e;
+    }
+    expect(error).toBeDefined();
+  });
+});
