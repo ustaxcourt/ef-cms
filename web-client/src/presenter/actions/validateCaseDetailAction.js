7526e0d75283bbd581ad8039e0c86bc733559dc6
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-03-24 11:36:53 -0700

    Remove caseDetailErrors and use validationErrors instead

--- web-client/src/presenter/actions/validateCaseDetailAction.js
+++ web-client/src/presenter/actions/validateCaseDetailAction.js
@@ -1,10 +1,10 @@
 import { state } from 'cerebral';
 
 /**
- * validates the case detail form and sets state.caseDetailErrors when errors occur.
+ * validates the case detail form and sets state.validationErrors when errors occur.
  *
  * @param {object} providers the providers object
- * @param {object} providers.store the cerebral store used for setting the state.caseDetailErrors when validation errors occur
+ * @param {object} providers.store the cerebral store used for setting the state.validationErrors when validation errors occur
  * @param {object} providers.applicationContext the application context needed for getting the getUseCaseForDocumentUpload use case
  * @param {object} providers.path the cerebral path which contains the next path in the sequence (path of success or failure)
  * @param {object} providers.props the cerebral store used for getting the props.combinedCaseDetailWithForm
@@ -23,7 +23,7 @@ export const validateCaseDetailAction = ({
     caseDetail: combinedCaseDetailWithForm,
   });
 
-  store.set(state.caseDetailErrors, errors || {});
+  store.set(state.validationErrors, errors || {});
 
   if (!errors) {
     return path.success({
