99d40f6ca92294dcbeed7b03910fec3f2cd82eaf
Eric Sorenson <eric@sorenson.software>
2020-03-25 15:54:15 -0500

    lint fixes

--- web-client/src/presenter/actions/FileDocument/submitCaseAssociationRequestAction.test.js
+++ web-client/src/presenter/actions/FileDocument/submitCaseAssociationRequestAction.test.js
@@ -1,21 +1,23 @@
+import { User } from '../../../../../shared/src/business/entities/User';
 import { presenter } from '../../presenter';
 import { runAction } from 'cerebral/test';
-import { User } from '../../../../../shared/src/business/entities/User';
 import { submitCaseAssociationRequestAction } from './submitCaseAssociationRequestAction';
 
 import { applicationContextForClient } from '../../../../../shared/src/business/test/createTestApplicationContext';
 const applicationContext = applicationContextForClient;
 presenter.providers.applicationContext = applicationContext;
-const submitCaseAssociationRequestInteractor = applicationContext.getUseCases()
-  .submitCaseAssociationRequestInteractor;
-const submitPendingCaseAssociationRequestInteractor = applicationContext.getUseCases()
-  .submitPendingCaseAssociationRequestInteractor;
+const {
+  submitCaseAssociationRequestInteractor,
+} = applicationContext.getUseCases();
+const {
+  submitPendingCaseAssociationRequestInteractor,
+} = applicationContext.getUseCases();
 
 applicationContext.getCurrentUser.mockReturnValue(
   new User({
+    email: 'practitioner1@example.com',
     name: 'richard',
     role: User.ROLES.privatePractitioner,
-    email: 'practitioner1@example.com',
     userId: 'a805d1ab-18d0-43ec-bafb-654e83405416',
   }),
 );
