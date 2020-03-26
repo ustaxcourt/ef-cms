93a39fbc86039ed656a32ea9fe847fe9d9285d92
Kris Koskelin <kkoskelin@flexion.us>
2020-03-26 15:44:11 -0500

    test refactor
    [skip ci]

--- web-client/src/presenter/actions/archiveDraftDocumentAction.test.js
+++ web-client/src/presenter/actions/archiveDraftDocumentAction.test.js
@@ -1,9 +1,8 @@
-import { applicationContextForClient } from '../../../../shared/src/business/test/createTestApplicationContext';
+import { applicationContextForClient as applicationContext } from '../../../../shared/src/business/test/createTestApplicationContext';
 import { archiveDraftDocumentAction } from './archiveDraftDocumentAction';
 import { presenter } from '../presenter';
 import { runAction } from 'cerebral/test';
 
-const applicationContext = applicationContextForClient;
 presenter.providers.applicationContext = applicationContext;
 
 describe('archiveDraftDocumentAction', () => {
