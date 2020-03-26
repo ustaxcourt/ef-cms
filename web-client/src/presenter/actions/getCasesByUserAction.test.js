a21153663321f8e9f9a5321e357468889997102c
Cody Seibert <cseibert113@gmail.com>
2020-03-26 17:25:47 -0400

    rest of a-g

--- web-client/src/presenter/actions/getCasesByUserAction.test.js
+++ web-client/src/presenter/actions/getCasesByUserAction.test.js
@@ -3,22 +3,20 @@ import { User } from '../../../../shared/src/business/entities/User';
 import { getCasesByUserAction } from './getCasesByUserAction';
 import { presenter } from '../presenter';
 import { runAction } from 'cerebral/test';
-
-presenter.providers.applicationContext = {
-  getCurrentUser: () => {
-    return {
-      role: User.ROLES.privatePractitioner,
-      userId: '123',
-    };
-  },
-  getUseCases: () => ({
-    getCasesByUserInteractor: () => {
-      return [MOCK_CASE];
-    },
-  }),
-};
+import { applicationContextForClient } from '../../../../shared/src/business/test/createTestApplicationContext';
 
 describe('getCasesByUserAction', () => {
+  beforeEach(() => {
+    presenter.providers.applicationContext = applicationContextForClient;
+    applicationContextForClient.getCurrentUser.mockReturnValue({
+      role: User.ROLES.privatePractitioner,
+      userId: '123',
+    });
+    applicationContextForClient
+      .getUseCases()
+      .getCasesByUserInteractor.mockReturnValue([MOCK_CASE]);
+  });
+
   it('should return a list of cases associated with the specified user', async () => {
     const results = await runAction(getCasesByUserAction, {
       modules: {
