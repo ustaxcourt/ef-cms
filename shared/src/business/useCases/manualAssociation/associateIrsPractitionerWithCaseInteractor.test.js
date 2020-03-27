9d3742cb86276d39e0804afb33955483b372f04d
Rachel Schneiderman <rschneiderman@flexion.us>
2020-03-27 10:04:06 -0700

    Refactoring caseAssociation useCases

--- shared/src/business/useCases/manualAssociation/associateIrsPractitionerWithCaseInteractor.test.js
+++ shared/src/business/useCases/manualAssociation/associateIrsPractitionerWithCaseInteractor.test.js
@@ -1,15 +1,15 @@
 const {
+  applicationContext,
+} = require('../../test/createTestApplicationContext');
+const {
   associateIrsPractitionerWithCaseInteractor,
 } = require('./associateIrsPractitionerWithCaseInteractor');
 const {
   SERVICE_INDICATOR_TYPES,
 } = require('../../entities/cases/CaseConstants');
 const { MOCK_CASE } = require('../../../test/mockCase.js');
-const { User } = require('../../entities/User');
 
 describe('associateIrsPractitionerWithCaseInteractor', () => {
-  let applicationContext;
-
   let caseRecord = {
     caseCaption: 'Caption',
     caseId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
@@ -24,67 +24,15 @@ describe('associateIrsPractitionerWithCaseInteractor', () => {
   };
 
   it('should throw an error when not authorized', async () => {
-    let error;
-    try {
-      applicationContext = {
-        environment: { stage: 'local' },
-        getCurrentUser: () => {
-          return {
-            name: 'Olivia Jade',
-            role: User.ROLES.petitioner,
-            userId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
-          };
-        },
-        getPersistenceGateway: () => ({
-          getCaseByCaseId: async () => caseRecord,
-          getUserById: async () => ({
-            name: 'Olivia Jade',
-            role: User.ROLES.adc,
-            userId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
-          }),
-          updateCase: async () => caseRecord,
-          verifyCaseForUser: async () => true,
-        }),
-        getUniqueId: () => 'unique-id-1',
-      };
-      await associateIrsPractitionerWithCaseInteractor({
-        applicationContext,
-        caseId: caseRecord.caseId,
-        serviceIndicator: SERVICE_INDICATOR_TYPES.SI_ELECTRONIC,
-        userId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
-      });
-    } catch (err) {
-      error = err;
-    }
-    expect(error.message).toContain('Unauthorized');
+    await associateIrsPractitionerWithCaseInteractor({
+      applicationContext,
+      caseId: caseRecord.caseId,
+      serviceIndicator: SERVICE_INDICATOR_TYPES.SI_ELECTRONIC,
+      userId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+    }).rejects.toThrow('Unauthorized');
   });
 
   it('should add mapping for an irsPractitioner', async () => {
-    let updateCaseSpy = jest.fn();
-
-    applicationContext = {
-      environment: { stage: 'local' },
-      getCurrentUser: () => {
-        return {
-          name: 'Olivia Jade',
-          role: User.ROLES.adc,
-          userId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
-        };
-      },
-      getPersistenceGateway: () => ({
-        associateUserWithCase: () => {},
-        getCaseByCaseId: async () => caseRecord,
-        getUserById: async () => ({
-          name: 'Olivia Jade',
-          role: User.ROLES.irsPractitioner,
-          userId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
-        }),
-        updateCase: updateCaseSpy,
-        verifyCaseForUser: async () => false,
-      }),
-      getUniqueId: () => 'unique-id-1',
-    };
-
     await associateIrsPractitionerWithCaseInteractor({
       applicationContext,
       caseId: caseRecord.caseId,
@@ -92,6 +40,6 @@ describe('associateIrsPractitionerWithCaseInteractor', () => {
       userId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
     });
 
-    expect(updateCaseSpy).toBeCalled();
+    expect(applicationContext.getPersistenceGateway().updateCase).toBeCalled();
   });
 });
