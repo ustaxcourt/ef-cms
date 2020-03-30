367c573b4adb6e1dd14e838fbcce749284012292
Joseph Sutton <jsutton@flexion.us>
2020-03-30 09:02:35 -0500

    return correct value in order to trigger deploys

--- web-api/deploy-diff.sh
+++ web-api/deploy-diff.sh
@@ -7,7 +7,7 @@ CURRENT_SHA="${CIRCLE_SHA1:-`git rev-parse HEAD`}"
 DEPLOYED_SHA=`curl https://ui-$STAGE.$EFCMS_DOMAIN | awk '/meta name/{ gsub(/.*meta revision=\042|\042.*/,"");print }'`;
 
 if [[ -z $DEPLOYED_SHA ]]; then
-  echo "false"
+  echo "true"
 elif git diff --name-only $CURRENT_SHA $DEPLOYED_SHA | grep -q $CHECK_PATH; then
   echo "true"
 else
