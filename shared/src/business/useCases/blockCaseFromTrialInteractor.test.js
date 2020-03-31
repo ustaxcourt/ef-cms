1d3ef39aaf50e0817de2b3312a573ca6b185019d
Mrinalini <mrinalinisinha05@gmail.com>
2020-03-31 10:02:17 -0700

    refactoring

--- shared/src/business/useCases/blockCaseFromTrialInteractor.test.js
+++ shared/src/business/useCases/blockCaseFromTrialInteractor.test.js
@@ -1,67 +1,61 @@
 const {
   blockCaseFromTrialInteractor,
 } = require('./blockCaseFromTrialInteractor');
+const { applicationContext } = require('../test/createTestApplicationContext');
 const { MOCK_CASE } = require('../../test/mockCase');
 const { User } = require('../entities/User');
 
 describe('blockCaseFromTrialInteractor', () => {
-  let applicationContext;
-  let deleteCaseTrialSortMappingRecordsMock = jest.fn();
+  let user;
+
+  beforeEach(() => {
+    applicationContext.environment.stage = 'local';
+    applicationContext.getCurrentUser.mockImplementation(() => user);
+  });
 
   it('should update the case with the blocked flag set as true and attach a reason', async () => {
-    applicationContext = {
-      environment: { stage: 'local' },
-      getCurrentUser: () => {
-        return {
-          role: User.ROLES.petitionsClerk,
-          userId: 'petitionsclerk',
-        };
-      },
-      getPersistenceGateway: () => {
-        return {
-          deleteCaseTrialSortMappingRecords: deleteCaseTrialSortMappingRecordsMock,
-          getCaseByCaseId: () => Promise.resolve(MOCK_CASE),
-          updateCase: ({ caseToUpdate }) => caseToUpdate,
-        };
-      },
-      getUniqueId: () => 'unique-id-1',
+    user = {
+      role: User.ROLES.petitionsClerk,
+      userId: 'petitionsclerk',
     };
+    applicationContext
+      .getPersistenceGateway()
+      .getCaseByCaseId.mockResolvedValue(MOCK_CASE);
+    applicationContext
+      .getPersistenceGateway()
+      .updateCase.mockImplementation(({ caseToUpdate }) => caseToUpdate);
+
     const result = await blockCaseFromTrialInteractor({
       applicationContext,
       caseId: MOCK_CASE.caseId,
       reason: 'just because',
     });
+
     expect(result).toMatchObject({
       blocked: true,
       blockedReason: 'just because',
     });
-    expect(deleteCaseTrialSortMappingRecordsMock).toHaveBeenCalled();
     expect(
-      deleteCaseTrialSortMappingRecordsMock.mock.calls[0][0].caseId,
+      applicationContext.getPersistenceGateway()
+        .deleteCaseTrialSortMappingRecords,
+    ).toHaveBeenCalled();
+    expect(
+      applicationContext.getPersistenceGateway()
+        .deleteCaseTrialSortMappingRecords.mock.calls[0][0].caseId,
     ).toEqual(MOCK_CASE.caseId);
   });
 
   it('should throw an unauthorized error if the user has no access to block cases', async () => {
-    applicationContext = {
-      environment: { stage: 'local' },
-      getCurrentUser: () => {
-        return {
-          role: 'nope',
-          userId: 'nope',
-        };
-      },
-      getUniqueId: () => 'unique-id-1',
+    user = {
+      role: 'nope',
+      userId: 'nope',
     };
-    let error;
-    try {
-      await blockCaseFromTrialInteractor({
+
+    await expect(
+      blockCaseFromTrialInteractor({
         applicationContext,
         caseId: '123',
-      });
-    } catch (err) {
-      error = err;
-    }
-    expect(error).not.toBeNull();
-    expect(error.message).toContain('Unauthorized');
+      }),
+    ).rejects.toThrow('Unauthorized');
   });
 });
