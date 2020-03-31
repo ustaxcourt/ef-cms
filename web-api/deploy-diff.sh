da1c8c81be668fbcf3cb0ffcf07135c5b4a8489f
Waldo Jaquith <waldo.jaquith@gsa.gov>
2020-03-31 09:49:18 -0400

    return correct value in order to trigger deploys
    Apply patch from https://github.com/flexion/ef-cms/pull/4828/files

--- web-api/deploy-diff.sh
+++ web-api/deploy-diff.sh
@@ -6,7 +6,9 @@ CURRENT_SHA="${CIRCLE_SHA1:-`git rev-parse HEAD`}"
 
 DEPLOYED_SHA=`curl https://ui-$STAGE.$EFCMS_DOMAIN | awk '/meta name/{ gsub(/.*meta revision=\042|\042.*/,"");print }'`;
 
-if git diff --name-only $CURRENT_SHA $DEPLOYED_SHA | grep -q $CHECK_PATH; then
+if [[ -z $DEPLOYED_SHA ]]; then
+  echo "true"
+elif git diff --name-only $CURRENT_SHA $DEPLOYED_SHA | grep -q $CHECK_PATH; then
   echo "true"
 else
   echo "false"
