17249def39998502546886a087e91ed54225539f
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-04-02 14:11:19 -0700

    jsdocs

--- web-client/src/presenter/actions/AdvancedSearch/setPractitionerResultsAction.js
+++ web-client/src/presenter/actions/AdvancedSearch/setPractitionerResultsAction.js
@@ -1,11 +1,10 @@
 import { state } from 'cerebral';
 
-/* sets props.practitioners on state.advancedSearchForm.results
+/* sets props.practitioners on state.searchResults
  *
  * @param {object} providers the providers object
- * @param {object} providers.applicationContext the application context
- * @param {Function} providers.get cerebral get function
- * @returns {object} contains the practitioners returned from the getPractitionersByNameInteractor use case
+ * @param {object} providers.props props passed through via cerebral
+ * @param {object} providers.store the cerebral store
  */
 export const setPractitionerResultsAction = async ({ props, store }) => {
   store.set(state.searchResults, props.practitioners);
