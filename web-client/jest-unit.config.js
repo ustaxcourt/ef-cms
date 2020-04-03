63ae081d3a8436d8a64ee596cf0c338342ee4d3b
Joseph Sutton <jsutton@flexion.us>
2020-04-02 20:51:40 -0500

    fix test coverage in jest configs for unit and integration tests

--- web-client/jest-unit.config.js
+++ web-client/jest-unit.config.js
@@ -3,7 +3,19 @@ const baseConfig = require('../jest.config');
 module.exports = {
   ...baseConfig,
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.js', '!e2e/**/*.js', '!src/**/*.test.js'],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!integration-tests/**/*.js',
+    '!integration-tests-public/**/*.js',
+    '!src/applicationContext.js',
+    '!src/applicationContextPublic.js',
+    '!src/router.js',
+    '!src/routerPublic.js',
+    '!src/index.dev.js',
+    '!src/index.prod.js',
+    '!src/index-public.dev.js',
+    '!src/index-public.prod.js',
+  ],
   coverageDirectory: './coverage-unit',
   coverageThreshold: {
     global: {
