33cd9a25ea2df2a038e23d1913b19b6029401d44
Rachel Schneiderman <rschneiderman@flexion.us>
2020-04-03 09:32:17 -0700

    Advanced search screen now includes search by order tab [skip ci]

---
+++ web-client/src/views/AdvancedSearch/OrderSearch.jsx
@@ -0,0 +1,80 @@
+import { Button } from '../../ustc-ui/Button/Button';
+import { FormGroup } from '../../ustc-ui/FormGroup/FormGroup';
+import { connect } from '@cerebral/react';
+import React from 'react';
+
+export const OrderSearch = connect({}, function OrderSearch(
+  advancedSearchForm,
+  advancedSearchHelper,
+  clearCaseSearchByNameFormSequence,
+  constants,
+  submitAdvancedSearchSequence,
+  updateAdvancedSearchFormValueSequence,
+) {
+  return (
+    <>
+      <div className="header-with-blue-background grid-row">
+        <h3>Search Orders</h3>
+      </div>
+      <div className="blue-container advanced-search__form-container grid-row">
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            submitAdvancedSearchSequence();
+          }}
+        >
+          <div className="grid-row grid-gap">
+            <div className="tablet:grid-col-12">
+              <FormGroup>
+                <h4 className="margin-top-105">Enter Keyword or Phrase</h4>
+                <label
+                  className="usa-label margin-bottom-05"
+                  htmlFor="order-search"
+                >
+                  Search for...
+                </label>
+                <input
+                  className="usa-input"
+                  id="order-search"
+                  name="petitionerName"
+                  type="text"
+                  // value={
+                  //   advancedSearchForm.caseSearchByName.petitionerName || ''
+                  // }
+                  onChange={e => {
+                    updateAdvancedSearchFormValueSequence({
+                      formType: 'caseSearchByName',
+                      key: e.target.name,
+                      value: e.target.value,
+                    });
+                  }}
+                />
+              </FormGroup>
+            </div>
+          </div>
+
+          <div className="grid-row">
+            <div className="tablet:grid-col-12">
+              <Button
+                className="advanced-search__button"
+                id="advanced-search-button"
+                type="submit"
+              >
+                Search
+              </Button>
+              <Button
+                link
+                className="margin-left-1 tablet:margin-left-205 margin-right-0 padding-0 ustc-button--mobile-inline"
+                onClick={() => {
+                  clearCaseSearchByNameFormSequence();
+                }}
+              >
+                Clear Search
+              </Button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+});
