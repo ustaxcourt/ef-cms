ce2135b6a0153f0f7ba010499dd3e89eaebffa3d
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-04-03 09:34:26 -0700

    4365: rename Attorney -> Practitioner

--- shared/src/proxies/updateAttorneyUserProxy.js
+++ shared/src/proxies/updateAttorneyUserProxy.js
@@ -1,17 +1,17 @@
 const { put } = require('./requests');
 
 /**
- * updateAttorneyUserInteractor
+ * updatePractitionerUserInteractor
  *
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
  * @param {object} providers.user the user data
  * @returns {Promise<object>} the updated user data
  */
-exports.updateAttorneyUserInteractor = ({ applicationContext, user }) => {
+exports.updatePractitionerUserInteractor = ({ applicationContext, user }) => {
   return put({
     applicationContext,
     body: { user },
-    endpoint: '/users/attorney',
+    endpoint: '/users/practitioner',
   });
 };
