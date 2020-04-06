57574247b66876a98efc1ab89006562d017a154b
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-04-06 11:45:51 -0700

    4365: validation for practitioner search form

--- web-client/src/presenter/sequences/submitPractitionerNameSearchSequence.js
+++ web-client/src/presenter/sequences/submitPractitionerNameSearchSequence.js
@@ -1,12 +1,24 @@
 import { clearAlertsAction } from '../actions/clearAlertsAction';
+import { clearSearchResultsAction } from '../actions/AdvancedSearch/clearSearchResultsAction';
 import { getPractitionersByNameAction } from '../actions/AdvancedSearch/getPractitionersByNameAction';
+import { setAlertErrorAction } from '../actions/setAlertErrorAction';
 import { setPractitionerResultsAction } from '../actions/AdvancedSearch/setPractitionerResultsAction';
+import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
 import { showProgressSequenceDecorator } from '../utilities/sequenceHelpers';
+import { validatePractitionerSearchByNameAction } from '../actions/AdvancedSearch/validatePractitionerSearchByNameAction';
 
-export const submitPractitionerNameSearchSequence = showProgressSequenceDecorator(
-  [
-    clearAlertsAction,
-    getPractitionersByNameAction,
-    setPractitionerResultsAction,
-  ],
-);
+export const submitPractitionerNameSearchSequence = [
+  validatePractitionerSearchByNameAction,
+  {
+    error: [
+      setAlertErrorAction,
+      setValidationErrorsAction,
+      clearSearchResultsAction,
+    ],
+    success: showProgressSequenceDecorator([
+      clearAlertsAction,
+      getPractitionersByNameAction,
+      setPractitionerResultsAction,
+    ]),
+  },
+];
