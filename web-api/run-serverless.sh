e20a8ca390246421a74edb1c314de3f6769f2914
Matt Hopson <matthopson@gmail.com>
2020-04-06 13:52:37 -0600

    Revert "Fix hb key export"
    This reverts commit cb32e4df9754441242d228c3eaada98dbcdd882c.

--- web-api/run-serverless.sh
+++ web-api/run-serverless.sh
@@ -49,8 +49,8 @@ set -- \
   --userPoolId "${USER_POOL_ID}" \
   --dynamo_stream_arn="${DYNAMO_STREAM_ARN}" \
   --elasticsearch_endpoint="${ELASTICSEARCH_ENDPOINT}" \
-  --circleHoneybadgerApiKey="${CIRCLE_HONEYBADGER_API_KEY}" \
-  --verbose
+  --verbose \
+  --circleHoneybadgerApiKey="${CIRCLE_HONEYBADGER_API_KEY}"
 
 ./node_modules/.bin/sls create_domain "$@"
 echo "done running create_domain"
