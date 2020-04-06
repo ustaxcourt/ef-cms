c6eafd328316131162d74e1048ba5f914ad38038
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-04-06 12:17:00 -0700

    set state.searchResults to empty array

--- web-client/src/presenter/sequences/submitCaseDocketNumberSearchSequence.js
+++ web-client/src/presenter/sequences/submitCaseDocketNumberSearchSequence.js
@@ -2,10 +2,12 @@ import { caseExistsAction } from '../actions/caseExistsAction';
 import { clearSearchResultsAction } from '../actions/AdvancedSearch/clearSearchResultsAction';
 import { clearSearchTermAction } from '../actions/clearSearchTermAction';
 import { navigateToCaseDetailAction } from '../actions/navigateToCaseDetailAction';
+import { set } from 'cerebral/factories';
 import { setAlertErrorAction } from '../actions/setAlertErrorAction';
 import { setDocketNumberFromAdvancedSearchAction } from '../actions/AdvancedSearch/setDocketNumberFromAdvancedSearchAction';
 import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
 import { showProgressSequenceDecorator } from '../utilities/sequenceHelpers';
+import { state } from 'cerebral';
 import { validateCaseDocketNumberSearchAction } from '../actions/AdvancedSearch/validateCaseDocketNumberSearchAction';
 
 export const submitCaseDocketNumberSearchSequence = [
@@ -21,7 +23,7 @@ export const submitCaseDocketNumberSearchSequence = [
       setDocketNumberFromAdvancedSearchAction,
       caseExistsAction,
       {
-        error: [clearSearchResultsAction],
+        error: [set(state.searchResults, [])],
         success: [navigateToCaseDetailAction],
       },
     ]),
