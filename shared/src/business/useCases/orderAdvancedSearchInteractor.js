47c81b1a7e7b5fa1ac5581bcca16a802aad9cc06
Rachel Schneiderman <rschneiderman@flexion.us>
2020-04-07 14:48:35 -0700

    Fixed test and jsdocs.

--- shared/src/business/useCases/orderAdvancedSearchInteractor.js
+++ shared/src/business/useCases/orderAdvancedSearchInteractor.js
@@ -11,6 +11,8 @@ const { UnauthorizedError } = require('../../errors/errors');
  * orderAdvancedSearchInteractor
  *
  * @param {object} providers the providers object containing applicationContext, orderKeyword
+ * @param {object} providers.applicationContext the application context
+ * @param {string} orderKeyword the search term to look for in documents
  * @returns {object} the orders data
  */
 exports.orderAdvancedSearchInteractor = async ({
