d6ad2c80d1e5d443f1bb944ff09fd35d515e2739
Rachel Schneiderman <rschneiderman@flexion.us>
2020-04-08 14:41:21 -0700

    Retrieve case information for each order.

--- shared/src/business/useCases/orderAdvancedSearchInteractor.test.js
+++ shared/src/business/useCases/orderAdvancedSearchInteractor.test.js
@@ -38,6 +38,136 @@ describe('orderAdvancedSearchInteractor', () => {
         hits: [
           {
             _source: {
+              caseId: {
+                S: '1',
+              },
+              docketNumber: {
+                S: '103-19',
+              },
+              documentContents: {
+                S:
+                  'Everyone knows that Reeses Outrageous bars are the best candy',
+              },
+              documentTitle: {
+                S: 'Order for More Candy',
+              },
+              eventCode: {
+                S: 'ODD',
+              },
+              pk: {
+                S: 'case|491b05b4-483f-4b85-8dd7-2dd4c069eb50',
+              },
+              sk: {
+                S: 'document|8da3946f-f0e0-426e-a5bb-cb4c482fb737',
+              },
+            },
+          },
+          {
+            _source: {
+              caseId: {
+                S: '2',
+              },
+              docketNumber: {
+                S: '103-19',
+              },
+              documentContents: {
+                S: 'KitKats are inferior candies',
+              },
+              documentTitle: {
+                S: 'Order for KitKats',
+              },
+              eventCode: {
+                S: 'ODD',
+              },
+              pk: {
+                S: 'case|491b05b4-483f-4b85-8dd7-2dd4c069eb50',
+              },
+              sk: {
+                S: 'document|8da3946f-f0e0-426e-a5bb-cb4c482fb737',
+              },
+            },
+          },
+        ],
+      },
+    });
+    applicationContext.getUseCases().getCaseInteractor.mockResolvedValue({
+      caseTitle:
+        'Samson Workman, Petitioner v. Commissioner of Internal Revenue',
+      docketNumberSuffix: 'AAA',
+    });
+
+    const results = await orderAdvancedSearchInteractor({
+      applicationContext,
+      orderKeyword: mockKeywordForSearch,
+    });
+
+    expect(applicationContext.getSearchClient().search).toHaveBeenCalled();
+    expect(
+      applicationContext.getSearchClient().search.mock.calls[0][0].body.query
+        .bool.must,
+    ).toEqual([
+      { match: { 'pk.S': 'case|' } },
+      { match: { 'sk.S': 'document|' } },
+      {
+        bool: {
+          should: orderEventCodes.map(eventCode => ({
+            match: {
+              'eventCode.S': eventCode,
+            },
+          })),
+        },
+      },
+      {
+        exists: {
+          field: 'servedAt',
+        },
+      },
+      {
+        simple_query_string: {
+          default_operator: 'or',
+          fields: ['documentContents.S', 'documentTitle.S'],
+          query: mockKeywordForSearch,
+        },
+      },
+    ]);
+    expect(results).toEqual([
+      {
+        caseId: '1',
+        caseTitle:
+          'Samson Workman, Petitioner v. Commissioner of Internal Revenue',
+        docketNumber: '103-19',
+        docketNumberSuffix: 'AAA',
+        documentContents:
+          'Everyone knows that Reeses Outrageous bars are the best candy',
+        documentTitle: 'Order for More Candy',
+        eventCode: 'ODD',
+        pk: 'case|491b05b4-483f-4b85-8dd7-2dd4c069eb50',
+        sk: 'document|8da3946f-f0e0-426e-a5bb-cb4c482fb737',
+      },
+      {
+        caseId: '2',
+        caseTitle:
+          'Samson Workman, Petitioner v. Commissioner of Internal Revenue',
+        docketNumber: '103-19',
+        docketNumberSuffix: 'AAA',
+        documentContents: 'KitKats are inferior candies',
+        documentTitle: 'Order for KitKats',
+        eventCode: 'ODD',
+        pk: 'case|491b05b4-483f-4b85-8dd7-2dd4c069eb50',
+        sk: 'document|8da3946f-f0e0-426e-a5bb-cb4c482fb737',
+      },
+    ]);
+  });
+
+  xit('populates each found order with docketNumberSuffix and caseTitle', async () => {
+    const mockKeywordForSearch = 'outrageous';
+    const orderEventCodes = map(Order.ORDER_TYPES, 'eventCode');
+
+    applicationContext.getSearchClient().search.mockResolvedValue({
+      hits: {
+        hits: [
+          {
+            _source: {
               docketNumber: {
                 S: '103-19',
               },
