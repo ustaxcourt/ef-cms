1bce411fd11093e317c8e8ce6f6d22b288bc1099
Joseph Sutton <jsutton@flexion.us>
2020-04-09 14:23:21 -0500

    fixed lint error

--- web-client/src/views/CaseDetailEdit/PartyInformation.jsx
+++ web-client/src/views/CaseDetailEdit/PartyInformation.jsx
@@ -55,6 +55,7 @@ export const PartyInformation = connect(
         {caseDetailEditHelper.showOwnershipDisclosureStatement && (
           <div className="subsection">
             <div className="usa-form-group">
+              {/*eslint-disable-next-line jsx-a11y/label-has-for*/}
               <label className="usa-label">
                 Ownership Disclosure Statement
               </label>
