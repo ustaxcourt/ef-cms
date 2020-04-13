158ea3625933018e29bf461ce11830da08736fd3
Cody Seibert <cseibert@flexion.us>
2020-04-13 17:48:39 -0400

    defining roles using the constants

--- web-client/src/presenter/sequences/gotoCaseDetailSequence.js
+++ web-client/src/presenter/sequences/gotoCaseDetailSequence.js
@@ -6,6 +6,7 @@ import { getCaseAction } from '../actions/getCaseAction';
 import { getCaseAssociationAction } from '../actions/getCaseAssociationAction';
 import { getCaseDeadlinesForCaseAction } from '../actions/CaseDeadline/getCaseDeadlinesForCaseAction';
 import { getConsolidatedCasesByCaseAction } from '../actions/caseConsolidation/getConsolidatedCasesByCaseAction';
+import { getConstants } from '../../getConstants';
 import { getJudgesCaseNoteForCaseAction } from '../actions/TrialSession/getJudgesCaseNoteForCaseAction';
 import { parallel } from 'cerebral/factories';
 import { runPathForUserRoleAction } from '../actions/runPathForUserRoleAction';
@@ -22,6 +23,8 @@ import { setJudgesCaseNoteOnCaseDetailAction } from '../actions/TrialSession/set
 import { showModalFromQueryAction } from '../actions/showModalFromQueryAction';
 import { state } from 'cerebral';
 import { takePathForRoles } from './takePathForRoles';
+
+const { USER_ROLES } = getConstants();
 
 const gotoCaseDetailInternal = [
   showModalFromQueryAction,
@@ -58,12 +61,12 @@ export const gotoCaseDetailSequence = [
   {
     ...takePathForRoles(
       [
-        'adc',
-        'admissionsclerk',
-        'clerkofcourt',
-        'docketclerk',
-        'petitionsclerk',
-        'trialclerk',
+        USER_ROLES.adc,
+        USER_ROLES.admissionsClerk,
+        USER_ROLES.clerkOfCourt,
+        USER_ROLES.docketClerk,
+        USER_ROLES.petitionsClerk,
+        USER_ROLES.trialClerk,
       ],
       [parallel([gotoCaseDetailInternal, fetchUserNotificationsSequence])],
     ),
