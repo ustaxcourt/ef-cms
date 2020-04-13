9912b965f1d5042fa30edb2ed86e0cf806c1e45a
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-04-13 08:20:23 -0700

    4366: add Edit link to practitioner detail view for admissionsclerk

--- web-client/src/presenter/computeds/practitionerSearchFormHelper.js
+++ web-client/src/presenter/computeds/practitionerSearchFormHelper.js
@@ -3,5 +3,5 @@ import { state } from 'cerebral';
 export const practitionerSearchFormHelper = get => {
   const permissions = get(state.permissions);
 
-  return { showAddPractitioner: permissions.ADD_PRACTITIONER_USER };
+  return { showAddPractitioner: permissions.ADD_EDIT_PRACTITIONER_USER };
 };
