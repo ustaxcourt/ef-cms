af85bb9a1ec3d250c65570f23c187f266bc374d3
Matt Hopson <matthopson@gmail.com>
2020-04-13 12:55:37 -0600

    Update practitioner integration test

---
+++ web-client/integration-tests/journey/admissionsClerkEditsPractitionerInfo.js
@@ -0,0 +1,41 @@
+export const admissionsClerkEditsPractitionerInfo = test => {
+  return it('admissions clerk edits practitioner information', async () => {
+    await test.runSequence('gotoEditPractitionerUserSequence', {
+      barNumber: 'PT1234',
+    });
+
+    expect(test.getState('currentPage')).toEqual('EditPractitionerUser');
+    expect(test.getState('form.barNumber')).toEqual('PT1234');
+
+    await test.runSequence('updateFormValueSequence', {
+      key: 'firstName',
+      value: 'Ben',
+    });
+
+    await test.runSequence('updateFormValueSequence', {
+      key: 'middleName',
+      value: 'Leighton',
+    });
+
+    await test.runSequence('updateFormValueSequence', {
+      key: 'lastName',
+      value: 'Matlock',
+    });
+
+    await test.runSequence('updateFormValueSequence', {
+      key: 'contact.address1',
+      value: '123 Legal Way',
+    });
+
+    await test.runSequence('submitUpdatePractitionerUserSequence');
+
+    expect(test.getState('currentPage')).toEqual('PractitionerDetail');
+    expect(test.getState('practitionerDetail.barNumber')).toEqual('PT1234');
+    expect(test.getState('practitionerDetail.name')).toEqual(
+      'Ben Leighton Matlock',
+    );
+    expect(test.getState('practitionerDetail.contact.address1')).toEqual(
+      '123 Legal Way',
+    );
+  });
+};
