919c3fa71aa06344255dd3b32cb80e87948758a7
Matt Hopson <matthopson@gmail.com>
2020-04-13 07:39:49 -0600

    Accidental removal

--- web-client/src/presenter/actions/getUserByIdAction.test.js
+++ web-client/src/presenter/actions/getUserByIdAction.test.js
@@ -4,11 +4,19 @@ import { presenter } from '../presenter-mock';
 import { runAction } from 'cerebral/test';
 
 describe('getUserByIdAction', () => {
-  beforeAll(() => {});
+  beforeAll(() => {
+    presenter.providers.applicationContext = applicationContext;
+    applicationContext.getUseCases().getUserByIdInteractor.mockReturnValue({
+      role: 'privatePractitioner',
+      userId: '123',
+    });
+  });
 
   it('should call the user and return the user from the use case', async () => {
     const results = await runAction(getUserByIdAction, {
-      modules: {},
+      modules: {
+        presenter,
+      },
       props: {
         userId: '123',
       },
