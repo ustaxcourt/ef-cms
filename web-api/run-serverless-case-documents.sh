556f0bed254194a2fcafe31d13fa4e5307280281
Joseph Sutton <jsutton@flexion.us>
2020-04-14 10:30:55 -0500

    added ELASTICSEARCH_ENDPOINT env var to stack deployment

--- web-api/run-serverless-case-documents.sh
+++ web-api/run-serverless-case-documents.sh
@@ -1,2 +1,9 @@
 #!/bin/bash -e
+
+pushd ./web-api/terraform/main
+  ../bin/deploy-init.sh "${1}"
+  ELASTICSEARCH_ENDPOINT="$(terraform output elasticsearch_endpoint)"
+  export ELASTICSEARCH_ENDPOINT
+popd
+
 ./web-api/run-serverless.sh "${1}" "${2}" "caseDocumentsHandlers.js" "serverless-case-documents.yml" "build:api:case:documents"
