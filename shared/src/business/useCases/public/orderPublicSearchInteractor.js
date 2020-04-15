f80c913640c0b17d9d740aa9c0d89320af129911
Joseph Sutton <jsutton@flexion.us>
2020-04-14 19:40:43 -0500

    updated jsdoc

--- shared/src/business/useCases/public/orderPublicSearchInteractor.js
+++ shared/src/business/useCases/public/orderPublicSearchInteractor.js
@@ -1,3 +1,11 @@
+/**
+ * orderPublicSearchInteractor
+ *
+ * @param {object} providers object containing applicationContext and other necessary parameters needed for the interactor
+ * @param {object} providers.applicationContext application context object
+ * @param {object} providers.orderKeyword the keyword to be used in the order search
+ * @returns {object} the case data
+ */
 exports.orderPublicSearchInteractor = async ({
   applicationContext,
   orderKeyword,
