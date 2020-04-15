5388ea86ffe3f7c4bef369f781818c025f111cf9
Rachel Schneiderman <rschneiderman@flexion.us>
2020-04-15 08:59:44 -0700

    Public order search now working.

--- web-client/src/applicationContextPublic.js
+++ web-client/src/applicationContextPublic.js
@@ -14,6 +14,7 @@ import {
   getCognitoLoginUrl,
   getPublicSiteUrl,
 } from '../../shared/src/sharedAppContext.js';
+import { getJudgeLastName } from '../../shared/src/business/utilities/getFormattedJudgeName';
 import { getPublicCaseInteractor } from '../../shared/src/proxies/getPublicCaseProxy';
 import { orderPublicSearchInteractor } from '../../shared/src/proxies/orderPublicSearchProxy';
 import { validateCaseAdvancedSearchInteractor } from '../../shared/src/business/useCases/validateCaseAdvancedSearchInteractor';
@@ -50,6 +51,7 @@ const applicationContextPublic = {
       formatDateString,
       formatDocketRecord,
       formatDocketRecordWithDocument,
+      getJudgeLastName,
       sortDocketRecords,
     };
   },
