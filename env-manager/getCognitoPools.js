897a5f879b51f6ae81ca147c33c0ce19f5ebf134
Eric Sorenson <eric@sorenson.software>
2020-04-16 07:48:44 -0500

    MaxResults is required

--- env-manager/getCognitoPools.js
+++ env-manager/getCognitoPools.js
@@ -1,7 +1,9 @@
 const { filter } = require('lodash');
 
 exports.getCognitoPools = async ({ cognito, environment }) => {
-  const { IdentityPools } = await cognito.listIdentityPools({}).promise();
+  const { IdentityPools } = await cognito
+    .listIdentityPools({ MaxResults: 10 })
+    .promise();
   return filter(IdentityPools, pool => {
     return pool.IdentityPoolName.includes(`-${environment.name}`);
   });
