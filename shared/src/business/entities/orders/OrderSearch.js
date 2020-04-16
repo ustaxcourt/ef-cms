7d425ad1866a2e54e69078db4b6d3c14c030238e
Rachel Schneiderman <rschneiderman@flexion.us>
2020-04-16 10:36:13 -0700

    Advanced order search validation complete.

--- shared/src/business/entities/orders/OrderSearch.js
+++ shared/src/business/entities/orders/OrderSearch.js
@@ -15,15 +15,24 @@ OrderSearch.validationName = 'OrderSearch';
  */
 function OrderSearch(rawProps = {}) {
   this.orderKeyword = rawProps.orderKeyword;
+  this.docketNumber = rawProps.docketNumber;
+  this.caseTitleOrPetitioner = rawProps.caseTitleOrPetitioner;
 }
 
 OrderSearch.VALIDATION_ERROR_MESSAGES = {
+  chooseOneValue:
+    'Enter either a Docket number or a Case name/Petitioner name, not both',
   orderKeyword: 'Enter a keyword or phrase',
 };
 
-OrderSearch.schema = joi.object().keys({
-  orderKeyword: joi.string().required(),
-});
+OrderSearch.schema = joi
+  .object()
+  .keys({
+    caseTitleOrPetitioner: joi.string().empty(''),
+    docketNumber: joi.string().empty(''),
+    orderKeyword: joi.string().required(),
+  })
+  .oxor('caseTitleOrPetitioner', 'docketNumber');
 
 joiValidationDecorator(
   OrderSearch,
@@ -31,5 +40,19 @@ joiValidationDecorator(
   undefined,
   OrderSearch.VALIDATION_ERROR_MESSAGES,
 );
+
+const originalGetValidationErrors = OrderSearch.prototype.getValidationErrors;
+
+OrderSearch.prototype.getValidationErrors = function () {
+  const validationErrors = originalGetValidationErrors.call(this);
+
+
+  if (validationErrors && validationErrors['object.oxor']) {
+    validationErrors['chooseOneValue'] = validationErrors['object.oxor'];
+    delete validationErrors['object.oxor'];
+  }
+
+  return validationErrors;
+};
 
 module.exports = { OrderSearch };
