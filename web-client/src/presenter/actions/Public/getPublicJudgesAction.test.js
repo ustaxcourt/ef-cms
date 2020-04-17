57eeed6a236fbb064e25bf1f95fd5f97315698db
Rachel Schneiderman <rschneiderman@flexion.us>
2020-04-17 10:56:23 -0700

    Public order search judges dropdown is populated.

---
+++ web-client/src/presenter/actions/Public/getPublicJudgesAction.test.js
@@ -0,0 +1,35 @@
+import { User } from '../../../../../shared/src/business/entities/User';
+import { applicationContextForClient as applicationContext } from '../../../../../shared/src/business/test/createTestApplicationContext';
+import { getPublicJudgesAction } from './getPublicJudgesAction';
+import { presenter } from '../../presenter-public';
+import { runAction } from 'cerebral/test';
+
+describe('getPublicJudgesAction', () => {
+  beforeAll(() => {
+    presenter.providers.applicationContext = applicationContext;
+  });
+
+  it('gets the list of jugdges', async () => {
+    const mockJudges = [
+      {
+        name: 'Test Judge',
+        role: User.ROLES.judge,
+      },
+      {
+        name: 'Test Judge2',
+        role: User.ROLES.judge,
+      },
+    ];
+    applicationContext
+      .getUseCases()
+      .getPublicJudgesInteractor.mockReturnValue(mockJudges);
+
+    const result = await runAction(getPublicJudgesAction, {
+      modules: {
+        presenter,
+      },
+    });
+
+    expect(result.output.users).toMatchObject(mockJudges);
+  });
+});
