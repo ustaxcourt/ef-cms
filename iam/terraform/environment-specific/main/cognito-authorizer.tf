666ee0752575b83ebce0442b2e1945bb24f9515e
Cody Seibert <cseibert@flexion.us>
2020-04-21 10:56:05 -0400

    hooking authorizer into lambda

---
+++ iam/terraform/environment-specific/main/cognito-authorizer.tf
@@ -0,0 +1,38 @@
+
+resource "aws_iam_role_policy" "authorizer_invocation_policy" {
+  name = "cognito_authorizer_policy_${var.environment}"
+  role = "${aws_iam_role.authorizer_lambda.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "lambda:InvokeFunction",
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "authorizer_lambda" {
+  name = "authorizer_lambda_role_${var.environment}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
--- web-api/terraform/template/cognito-authorizer.tf
+++ web-api/terraform/template/cognito-authorizer.tf
@@ -4,18 +4,10 @@ data "archive_file" "zip_authorizer" {
   source_file = "${path.module}/cognito-authorizer/dist/index.js"
 }
 
-resource "aws_lambda_permission" "allow_trigger_authorizer" {
-  statement_id  = "AllowPostConfirmationExecutionFromCognito"
-  action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.cognito_authorizer_lambda.function_name}"
-  principal     = "cognito-idp.amazonaws.com"
-  source_arn    = "${aws_cognito_user_pool.pool.arn}"
-}
-
 resource "aws_lambda_function" "cognito_authorizer_lambda" {
   filename      = "${data.archive_file.zip_authorizer.output_path}"
   function_name = "cognito_authorizer_lambda_${var.environment}"
-  role          = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/iam_cognito_post_confirmation_lambda_role_${var.environment}"
+  role          = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/authorizer_lambda_role_${var.environment}"
   handler       = "index.handler"
   source_code_hash = "${data.archive_file.zip_authorizer.output_base64sha256}"
   
