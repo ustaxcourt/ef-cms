7cb69442dc39a64d00b56bbd306602b8af8441bd
Matt Hopson <matthopson@gmail.com>
2020-04-21 11:59:08 -0600

    Update tests

--- shared/src/business/test/createTestApplicationContext.js
+++ shared/src/business/test/createTestApplicationContext.js
@@ -334,6 +334,7 @@ const createTestApplicationContext = ({ user } = {}) => {
     barNumberGenerator: {
       createBarNumber: jest.fn().mockReturnValue('CS20001'),
     },
+    checkSearchClientMappings: jest.fn(),
     convertBlobToUInt8Array: jest
       .fn()
       .mockImplementation(() => new Uint8Array([])),
