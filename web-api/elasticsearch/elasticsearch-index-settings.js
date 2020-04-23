5c5b73077d2cc51b67a987d1b7af0f8b0d98b497
Matt Hopson <matthopson@gmail.com>
2020-04-23 12:45:47 -0600

    Refactor checkSearchClientMappings to use dynamic index

--- web-api/elasticsearch/elasticsearch-index-settings.js
+++ web-api/elasticsearch/elasticsearch-index-settings.js
@@ -1,5 +1,6 @@
 (async () => {
   const AWS = require('aws-sdk');
+  const elasticsearchIndexes = require('./elasticsearch-indexes');
   AWS.config.region = 'us-east-1';
   const connectionClass = require('http-aws-es');
   const elasticsearch = require('elasticsearch');
@@ -30,7 +31,7 @@
   });
 
   await Promise.all(
-    ['efcms', 'efcms-case', 'efcms-document', 'efcms-user'].map(async index => {
+    elasticsearchIndexes.map(async index => {
       try {
         const indexExists = await searchClientCache.indices.exists({
           body: {},
