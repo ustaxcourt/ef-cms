8fffffb1388a5d7903ef90d1f2e7741cdec8e91f
Mrinalini <mrinalinisinha05@gmail.com>
2020-04-27 12:07:15 -0700

    Not allowing contact secondary name and email address to be updated, refactoring tests to use testApplicationContext

--- shared/src/business/useCases/updateSecondaryContactInteractor.js
+++ shared/src/business/useCases/updateSecondaryContactInteractor.js
@@ -37,6 +37,21 @@ exports.updateSecondaryContactInteractor = async ({
     throw new NotFoundError(`Case ${caseId} was not found.`);
   }
 
+  if (caseToUpdate.contactSecondary) {
+    contactInfo = {
+      address1: contactInfo.address1,
+      address2: contactInfo.address2,
+      address3: contactInfo.address3,
+      city: contactInfo.city,
+      country: contactInfo.country,
+      countryType: contactInfo.countryType,
+      email: caseToUpdate.contactSecondary.email,
+      name: caseToUpdate.contactSecondary.name,
+      phone: contactInfo.phone,
+      postalCode: contactInfo.postalCode,
+    };
+  }
+
   const caseEntity = new Case(
     { ...caseToUpdate, contactSecondary: contactInfo },
     { applicationContext },
--- shared/src/business/useCases/updateSecondaryContactInteractor.test.js
+++ shared/src/business/useCases/updateSecondaryContactInteractor.test.js
@@ -1,134 +1,67 @@
 const {
-  createISODateString,
-  formatDateString,
-} = require('../../../../shared/src/business/utilities/DateHandler');
-const {
   updateSecondaryContactInteractor,
 } = require('./updateSecondaryContactInteractor');
-const { Case } = require('../entities/cases/Case');
+const { applicationContext } = require('../test/createTestApplicationContext');
 const { MOCK_CASE } = require('../../test/mockCase');
 const { User } = require('../entities/User');
 
-const fakeData =
-  'JVBERi0xLjEKJcKlwrHDqwoKMSAwIG9iagogIDw8IC9UeXBlIC9DYXRhbG9nCiAgICAgL1BhZ2VzIDIgMCBSCiAgPj4KZW5kb2JqCgoyIDAgb2JqCiAgPDwgL1R5cGUgL1BhZ2VzCiAgICAgL0tpZHMgWzMgMCBSXQogICAgIC9Db3VudCAxCiAgICAgL01lZGlhQm94IFswIDAgMzAwIDE0NF0KICA+PgplbmRvYmoKCjMgMCBvYmoKICA8PCAgL1R5cGUgL1BhZ2UKICAgICAgL1BhcmVudCAyIDAgUgogICAgICAvUmVzb3VyY2VzCiAgICAgICA8PCAvRm9udAogICAgICAgICAgIDw8IC9GMQogICAgICAgICAgICAgICA8PCAvVHlwZSAvRm9udAogICAgICAgICAgICAgICAgICAvU3VidHlwZSAvVHlwZTEKICAgICAgICAgICAgICAgICAgL0Jhc2VGb250IC9UaW1lcy1Sb21hbgogICAgICAgICAgICAgICA+PgogICAgICAgICAgID4+CiAgICAgICA+PgogICAgICAvQ29udGVudHMgNCAwIFIKICA+PgplbmRvYmoKCjQgMCBvYmoKICA8PCAvTGVuZ3RoIDg0ID4+CnN0cmVhbQogIEJUCiAgICAvRjEgMTggVGYKICAgIDUgODAgVGQKICAgIChDb25ncmF0aW9ucywgeW91IGZvdW5kIHRoZSBFYXN0ZXIgRWdnLikgVGoKICBFVAplbmRzdHJlYW0KZW5kb2JqCgp4cmVmCjAgNQowMDAwMDAwMDAwIDY1NTM1IGYgCjAwMDAwMDAwMTggMDAwMDAgbiAKMDAwMDAwMDA3NyAwMDAwMCBuIAowMDAwMDAwMTc4IDAwMDAwIG4gCjAwMDAwMDA0NTcgMDAwMDAgbiAKdHJhaWxlcgogIDw8ICAvUm9vdCAxIDAgUgogICAgICAvU2l6ZSA1CiAgPj4Kc3RhcnR4cmVmCjU2NQolJUVPRgo=';
-
-const fakeFile = Buffer.from(fakeData, 'base64');
-fakeFile.name = 'fakeFile.pdf';
-
-let updateCaseStub;
-let generateChangeOfAddressTemplateStub;
-let generatePdfFromHtmlInteractorStub;
-let getAddressPhoneDiffStub;
-let getDocumentTypeForAddressChangeStub;
-let saveDocumentFromLambdaStub;
-let sendServedPartiesEmailsStub;
-
-let persistenceGateway;
-let useCases;
-let applicationContext;
-
 describe('update secondary contact on a case', () => {
+  let mockCase = {
+    ...MOCK_CASE,
+    contactSecondary: {
+      address1: 'nothing',
+      city: 'Somewhere',
+      countryType: 'domestic',
+      email: 'secondary@example.com',
+      name: 'Secondary Party',
+      phone: '9876543210',
+      postalCode: '12345',
+      state: 'TN',
+    },
+    partyType: 'Petitioner & spouse',
+  };
+  const fakeData =
+    'JVBERi0xLjEKJcKlwrHDqwoKMSAwIG9iagogIDw8IC9UeXBlIC9DYXRhbG9nCiAgICAgL1BhZ2VzIDIgMCBSCiAgPj4KZW5kb2JqCgoyIDAgb2JqCiAgPDwgL1R5cGUgL1BhZ2VzCiAgICAgL0tpZHMgWzMgMCBSXQogICAgIC9Db3VudCAxCiAgICAgL01lZGlhQm94IFswIDAgMzAwIDE0NF0KICA+PgplbmRvYmoKCjMgMCBvYmoKICA8PCAgL1R5cGUgL1BhZ2UKICAgICAgL1BhcmVudCAyIDAgUgogICAgICAvUmVzb3VyY2VzCiAgICAgICA8PCAvRm9udAogICAgICAgICAgIDw8IC9GMQogICAgICAgICAgICAgICA8PCAvVHlwZSAvRm9udAogICAgICAgICAgICAgICAgICAvU3VidHlwZSAvVHlwZTEKICAgICAgICAgICAgICAgICAgL0Jhc2VGb250IC9UaW1lcy1Sb21hbgogICAgICAgICAgICAgICA+PgogICAgICAgICAgID4+CiAgICAgICA+PgogICAgICAvQ29udGVudHMgNCAwIFIKICA+PgplbmRvYmoKCjQgMCBvYmoKICA8PCAvTGVuZ3RoIDg0ID4+CnN0cmVhbQogIEJUCiAgICAvRjEgMTggVGYKICAgIDUgODAgVGQKICAgIChDb25ncmF0aW9ucywgeW91IGZvdW5kIHRoZSBFYXN0ZXIgRWdnLikgVGoKICBFVAplbmRzdHJlYW0KZW5kb2JqCgp4cmVmCjAgNQowMDAwMDAwMDAwIDY1NTM1IGYgCjAwMDAwMDAwMTggMDAwMDAgbiAKMDAwMDAwMDA3NyAwMDAwMCBuIAowMDAwMDAwMTc4IDAwMDAwIG4gCjAwMDAwMDA0NTcgMDAwMDAgbiAKdHJhaWxlcgogIDw8ICAvUm9vdCAxIDAgUgogICAgICAvU2l6ZSA1CiAgPj4Kc3RhcnR4cmVmCjU2NQolJUVPRgo=';
+
+  const fakeFile = Buffer.from(fakeData, 'base64');
+  fakeFile.name = 'fakeFile.pdf';
+
+  let mockUser = new User({
+    name: 'bob',
+    role: User.ROLES.petitioner,
+    userId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+  });
+
   beforeEach(() => {
-    updateCaseStub = jest.fn();
-    generateChangeOfAddressTemplateStub = jest.fn();
-    generatePdfFromHtmlInteractorStub = jest.fn();
-    getAddressPhoneDiffStub = jest.fn();
-    getDocumentTypeForAddressChangeStub = jest.fn();
-    saveDocumentFromLambdaStub = jest.fn();
-    sendServedPartiesEmailsStub = jest.fn();
+    applicationContext
+      .getPersistenceGateway()
+      .getCaseByCaseId.mockImplementation(() => mockCase);
 
-    persistenceGateway = {
-      getCaseByCaseId: () => ({
-        ...MOCK_CASE,
-        contactSecondary: {
-          address1: 'nothing',
-          city: 'Somewhere',
-          countryType: 'domestic',
-          email: 'secondary@example.com',
-          name: 'Secondary Party',
-          phone: '9876543210',
-          postalCode: '12345',
-          state: 'TN',
-        },
-        partyType: 'Petitioner & spouse',
-      }),
-      saveDocumentFromLambda: saveDocumentFromLambdaStub,
-      saveWorkItemForNonPaper: () => null,
-      updateCase: updateCaseStub,
-    };
+    applicationContext
+      .getChromiumBrowser()
+      .newPage()
+      .pdf.mockReturnValue(fakeData);
 
-    useCases = {
-      generatePdfFromHtmlInteractor: () => {
-        generatePdfFromHtmlInteractorStub();
-        return fakeFile;
-      },
-      userIsAssociated: () => true,
-    };
+    applicationContext
+      .getUseCases()
+      .generatePdfFromHtmlInteractor.mockReturnValue(fakeFile);
+    applicationContext.getUseCases().userIsAssociated.mockReturnValue(true);
 
-    applicationContext = {
-      environment: { stage: 'local' },
-      getCaseTitle: Case.getCaseTitle,
-      getChromiumBrowser: () => ({
-        close: () => null,
-        newPage: () => ({
-          pdf: () => fakeData,
-          setContent: () => null,
-        }),
-      }),
-      getCurrentUser: () => {
-        return new User({
-          name: 'bob',
-          role: User.ROLES.petitioner,
-          userId: '6805d1ab-18d0-43ec-bafb-654e83405416',
-        });
+    applicationContext.getCurrentUser.mockReturnValue(mockUser);
+
+    applicationContext.getUtilities().getAddressPhoneDiff.mockReturnValue({
+      address1: {
+        newData: 'new test',
+        oldData: 'test',
       },
-      getDispatchers: () => ({
-        sendBulkTemplatedEmail: () => null,
-      }),
-      getPersistenceGateway: () => {
-        return persistenceGateway;
-      },
-      getTemplateGenerators: () => {
-        return {
-          generateChangeOfAddressTemplate: async () => {
-            generateChangeOfAddressTemplateStub();
-            return '<html></html>';
-          },
-        };
-      },
-      getUniqueId: () => 'c6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
-      getUseCaseHelpers: () => ({
-        sendServedPartiesEmails: sendServedPartiesEmailsStub,
-      }),
-      getUseCases: () => useCases,
-      getUtilities: () => {
-        return {
-          createISODateString,
-          formatDateString,
-          getAddressPhoneDiff: () => {
-            getAddressPhoneDiffStub();
-            return {
-              address1: {
-                newData: 'new test',
-                oldData: 'test',
-              },
-            };
-          },
-          getDocumentTypeForAddressChange: () => {
-            getDocumentTypeForAddressChangeStub();
-            return {
-              eventCode: 'NCA',
-              title: 'Notice of Change of Address',
-            };
-          },
-        };
-      },
-      logger: {
-        error: e => console.log(e),
-        time: () => null,
-        timeEnd: () => null,
-      },
-    };
+    });
+
+    applicationContext
+      .getUtilities()
+      .getDocumentTypeForAddressChange.mockReturnValue({
+        eventCode: 'NCA',
+        title: 'Notice of Change of Address',
+      });
   });
 
   it('updates contactSecondary', async () => {
@@ -143,63 +76,58 @@ describe('update secondary contact on a case', () => {
         name: 'New Secondary',
         phone: '1234567890',
         postalCode: '99999',
-        serviceIndicator: 'Electronic',
         state: 'PA',
       },
     });
 
-    expect(updateCaseStub).toHaveBeenCalled();
-    expect(generateChangeOfAddressTemplateStub).toHaveBeenCalled();
-    expect(generatePdfFromHtmlInteractorStub).toHaveBeenCalled();
+    expect(
+      applicationContext.getPersistenceGateway().updateCase,
+    ).toHaveBeenCalled();
+    expect(
+      applicationContext.getTemplateGenerators()
+        .generateChangeOfAddressTemplate,
+    ).toHaveBeenCalled();
+    expect(
+      applicationContext.getUseCases().generatePdfFromHtmlInteractor,
+    ).toHaveBeenCalled();
     expect(caseDetail.documents[4].servedAt).toBeDefined();
-    expect(caseDetail.documents[4].servedParties).toEqual([
-      { email: 'petitioner@example.com', name: 'Test Petitioner' },
-      { email: 'secondary@example.com', name: 'New Secondary' },
-    ]);
   });
 
   it('throws an error if the case was not found', async () => {
-    persistenceGateway.getCaseByCaseId = async () => null;
-    let error = null;
-    try {
-      await updateSecondaryContactInteractor({
+    mockCase = null;
+
+    await expect(
+      updateSecondaryContactInteractor({
         applicationContext,
         caseId: 'a805d1ab-18d0-43ec-bafb-654e83405416',
         contactInfo: {},
-      });
-    } catch (err) {
-      error = err;
-    }
-    expect(error.message).toEqual(
+      }),
+    ).rejects.toThrow(
       'Case a805d1ab-18d0-43ec-bafb-654e83405416 was not found.',
     );
   });
 
   it('throws an error if the user making the request is not associated with the case', async () => {
-    persistenceGateway.getCaseByCaseId = async () => ({
+    mockCase = {
       ...MOCK_CASE,
       userId: '123',
-    });
-    useCases.userIsAssociated = () => false;
-    let error = null;
-    try {
-      await updateSecondaryContactInteractor({
+    };
+    applicationContext.getUseCases().userIsAssociated.mockReturnValue(false);
+
+    await expect(
+      updateSecondaryContactInteractor({
         applicationContext,
         caseId: 'a805d1ab-18d0-43ec-bafb-654e83405416',
         contactInfo: {},
-      });
-    } catch (err) {
-      error = err;
-    }
-    expect(error.message).toEqual('Unauthorized for update case contact');
+      }),
+    ).rejects.toThrow('Unauthorized for update case contact');
   });
 
   it('does not update the case if the contact information does not change', async () => {
-    const getUtilities = applicationContext.getUtilities();
-    applicationContext.getUtilities = () => ({
-      ...getUtilities,
-      getDocumentTypeForAddressChange: () => undefined, // returns undefined when there is no diff
-    });
+    applicationContext
+      .getUtilities()
+      .getDocumentTypeForAddressChange.mockReturnValue(undefined);
+
     await updateSecondaryContactInteractor({
       applicationContext,
       caseId: 'a805d1ab-18d0-43ec-bafb-654e83405416',
@@ -216,8 +144,58 @@ describe('update secondary contact on a case', () => {
       },
     });
 
-    expect(updateCaseStub).not.toHaveBeenCalled();
-    expect(generateChangeOfAddressTemplateStub).not.toHaveBeenCalled();
-    expect(generatePdfFromHtmlInteractorStub).not.toHaveBeenCalled();
+    expect(
+      applicationContext.getPersistenceGateway().updateCase,
+    ).not.toHaveBeenCalled();
+    expect(
+      applicationContext.getTemplateGenerators()
+        .generateChangeOfAddressTemplate,
+    ).not.toHaveBeenCalled();
+    expect(
+      applicationContext.getUseCases().generatePdfFromHtmlInteractor,
+    ).not.toHaveBeenCalled();
+  });
+
+  it('does not update the contact secondary email or name', async () => {
+    mockCase = {
+      ...MOCK_CASE,
+      contactSecondary: {
+        address1: 'nothing',
+        city: 'Somewhere',
+        countryType: 'domestic',
+        email: 'secondary@example.com',
+        name: 'Secondary Party',
+        phone: '9876543210',
+        postalCode: '12345',
+        state: 'TN',
+      },
+      partyType: 'Petitioner & spouse',
+    };
+    applicationContext
+      .getUtilities()
+      .getDocumentTypeForAddressChange.mockReturnValue(undefined);
+
+    const caseDetail = await updateSecondaryContactInteractor({
+      applicationContext,
+      caseId: 'a805d1ab-18d0-43ec-bafb-654e83405416',
+      contactInfo: {
+        // Matches current contact info
+        address1: 'nothing',
+        city: 'Somewhere',
+        countryType: 'domestic',
+        email: 'hello123@example.com',
+        name: 'Secondary Party Name Changed',
+        phone: '9876543210',
+        postalCode: '12345',
+        state: 'TN',
+      },
+    });
+
+    expect(caseDetail.contactSecondary.name).not.toBe(
+      'Secondary Party Name Changed',
+    );
+    expect(caseDetail.contactSecondary.name).toBe('Secondary Party');
+    expect(caseDetail.contactSecondary.email).not.toBe('hello123@example.com');
+    expect(caseDetail.contactSecondary.email).toBe('secondary@example.com');
   });
 });
