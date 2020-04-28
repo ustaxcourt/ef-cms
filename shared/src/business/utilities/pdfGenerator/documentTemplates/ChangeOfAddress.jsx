36935cedb15e66bf07d0b7fe43cf49a8a50cf83b
Matt Hopson <matthopson@gmail.com>
2020-04-28 10:15:38 -0600

    Use pug for base html template

--- shared/src/business/utilities/pdfGenerator/documentTemplates/ChangeOfAddress.jsx
+++ shared/src/business/utilities/pdfGenerator/documentTemplates/ChangeOfAddress.jsx
@@ -2,8 +2,6 @@ const React = require('react');
 
 const PDFDocumentHeader = require('../components/PDFDocumentHeader.jsx')
   .default;
-const PDFDocumentTemplate = require('../components/PDFDocumentTemplate.jsx')
-  .default;
 
 const renderTable = ({ data, label, options }) => {
   return (
@@ -40,31 +38,7 @@ const renderTable = ({ data, label, options }) => {
 
 const ChangeOfAddress = ({ name, newData, oldData, options }) => {
   return (
-    <PDFDocumentTemplate
-      pageStyles=".please-change {
-        margin-bottom: 20px;
-        font-size: 12px;
-        font-weight: 600;
-      }
-
-      th,
-      td {
-        font-size: 10px;
-      }
-
-      th {
-        font-weight: 600;
-      }
-
-      .case-information #caption {
-        line-height: 18px;
-      }
-
-      .extra-margin-top {
-        margin-top: 8px;
-      }"
-      pageTitle="Change of Contact Information"
-    >
+    <>
       <PDFDocumentHeader
         caseCaptionWithPostfix={options.caseCaptionWithPostfix}
         docketNumberWithSuffix={options.docketNumberWithSuffix}
@@ -80,7 +54,7 @@ const ChangeOfAddress = ({ name, newData, oldData, options }) => {
         <br />
         {renderTable({ data: newData, label: 'New', options })}
       </div>
-    </PDFDocumentTemplate>
+    </>
   );
 };
 
