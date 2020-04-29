ee5b43ff1a5ae26706f9d58bea90b1228f94867a
Kristen Lohman <klohman@flexion.us>
2020-04-29 15:24:04 -0400

    836 updates

--- web-client/src/views/CaseDifferenceExplained.jsx
+++ web-client/src/views/CaseDifferenceExplained.jsx
@@ -15,6 +15,11 @@ export const CaseDifferenceExplained = () => {
       <h3 className="margin-bottom-4">
         Do I Qualify for a Small Tax Case Procedure?
       </h3>
+      <p>
+        For specific case types, the amount of the tax owed will affect whether
+        or not you qualify to select a small tax case procedure. And if you do
+        qualify, you don’t have to select a small case procedure.
+      </p>
       <table
         aria-labelledby="small-case-qualifications"
         className="usa-table responsive-table ustc-table small-case-qualifications-table"
@@ -66,7 +71,7 @@ export const CaseDifferenceExplained = () => {
           ].map(caseType => (
             <tr key={caseType}>
               <td>{caseType}</td>
-              <td>Not eligible for “S” status</td>
+              <td>Not eligible for Small tax case procedure</td>
             </tr>
           ))}
         </tbody>
