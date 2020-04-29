e0126f5d5e454752a2e5f1b8b6784dbb86defe34
Mrinalini <mrinalinisinha05@gmail.com>
2020-04-29 13:09:57 -0700

    Moved icon into its own column, included uswds theme color to icon

--- web-client/src/views/AdvancedSearch/OrderSearchResults.jsx
+++ web-client/src/views/AdvancedSearch/OrderSearchResults.jsx
@@ -31,7 +31,8 @@ export const OrderSearchResults = connect(
             <table className="usa-table search-results responsive-table row-border-only">
               <thead>
                 <tr>
-                  <th aria-hidden="true"></th>
+                  <th aria-hidden="true" className="small-column"></th>
+                  <th aria-hidden="true" className="small-column"></th>
                   <th>Docket number</th>
                   <th>Case title</th>
                   <th>Order</th>
@@ -43,8 +44,10 @@ export const OrderSearchResults = connect(
                 {advancedOrderSearchHelper.formattedSearchResults.map(
                   (result, idx) => (
                     <tr className="search-result" key={idx}>
-                      <td aria-hidden="true">
+                      <td aria-hidden="true" className="small-column">
                         {idx + 1}
+                      </td>
+                      <td aria-hidden="true" className="small-column">
                         {result.isSealed && (
                           <Icon
                             aria-label="sealed"
