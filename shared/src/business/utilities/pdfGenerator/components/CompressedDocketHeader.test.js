6dc4473b6955e6b6846c5c1535b07bcb5dfd1157
Matt Hopson <matthopson@gmail.com>
2020-04-30 15:31:28 -0600

    Add compressed docket header component

---
+++ shared/src/business/utilities/pdfGenerator/components/CompressedDocketHeader.test.js
@@ -0,0 +1,31 @@
+const CompressedDocketHeader = require('./CompressedDocketHeader.jsx').default;
+const React = require('react');
+const { shallow } = require('enzyme');
+
+describe('CompressedDocketHeader', () => {
+  it('renders the case caption from props', () => {
+    let wrapper = shallow(<CompressedDocketHeader caption="Test Petitioner" />);
+    expect(wrapper.find('#caption').text()).toEqual(
+      'Test Petitioner, Petitioner(s) v. Commissioner of Internal Revenue, Respondent',
+    );
+  });
+
+  it('renders the docket number from props', () => {
+    let wrapper = shallow(
+      <CompressedDocketHeader docketNumberWithSuffix="123-45S" />,
+    );
+    expect(wrapper.find('#docket-number').text()).toEqual(
+      'Docket Number 123-45S',
+    );
+  });
+
+  it('only renders an h3 element if h3 prop is provided', () => {
+    let wrapper = shallow(<CompressedDocketHeader />);
+    let h3 = wrapper.find('.case-information h3');
+    expect(h3).toEqual({});
+
+    wrapper = shallow(<CompressedDocketHeader h3="Test H3" />);
+    h3 = wrapper.find('.case-information h3');
+    expect(h3.text()).toEqual('Test H3');
+  });
+});
