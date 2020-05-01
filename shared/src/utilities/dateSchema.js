841de9ca624aa77ea7f93d32a65dcfbcbc74d49d
Kris Koskelin <kkoskelin@flexion.us>
2020-05-01 11:11:24 -0500

    update possible formats for date

--- shared/src/utilities/dateSchema.js
+++ shared/src/utilities/dateSchema.js
@@ -1,4 +1,8 @@
 const joi = require('@hapi/joi').extend(require('@hapi/joi-date'));
 const { FORMATS } = require('../business/utilities/DateHandler');
 
-exports.getTimestampSchema = () => joi.date().iso().format(FORMATS.ISO);
+exports.getTimestampSchema = () => {
+  // eslint-disable-next-line spellcheck/spell-checker
+  // TODO: remove FORMATS.YYYYMMDD from valid timestamp formats after devex task
+  return joi.date().iso().format(FORMATS.ISO, FORMATS.YYYYMMDD);
+};
