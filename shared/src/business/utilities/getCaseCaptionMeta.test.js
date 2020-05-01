dea3b954e827583da5d48598b8759bc19963ce05
Matt Hopson <matthopson@gmail.com>
2020-05-01 08:37:50 -0600

    Get case caption meta with util and refactor

---
+++ shared/src/business/utilities/getCaseCaptionMeta.test.js
@@ -0,0 +1,42 @@
+const { Case } = require('../entities/cases/Case');
+const { getCaseCaptionMeta } = require('./getCaseCaptionMeta');
+
+describe('getCaseCaptionMeta', () => {
+  it('returns an object with the case caption, case caption postfix, and the combined case caption with postfix', () => {
+    const caseDetail = {
+      caseCaption: 'Eve Brewer, Petitioner',
+    };
+    expect(getCaseCaptionMeta(caseDetail)).toMatchObject({
+      CASE_CAPTION_POSTFIX: Case.CASE_CAPTION_POSTFIX,
+      caseCaption: 'Eve Brewer, Petitioner',
+      caseCaptionWithPostfix: `Eve Brewer, Petitioner ${Case.CASE_CAPTION_POSTFIX}`,
+    });
+  });
+
+  it('returns an object with the case title', () => {
+    const caseDetail = {
+      caseCaption: 'Selma Horn & Cairo Harris, Petitioners',
+    };
+    expect(getCaseCaptionMeta(caseDetail).caseTitle).toEqual(
+      'Selma Horn & Cairo Harris',
+    );
+  });
+
+  it('returns an object with the computed case caption extension for single petitioners', () => {
+    const caseDetail = {
+      caseCaption: 'Brett Osborne, Petitioner',
+    };
+    expect(getCaseCaptionMeta(caseDetail).caseCaptionExtension).toEqual(
+      'Petitioner',
+    );
+  });
+
+  it('returns an object with the computed case caption extension for multiple petitioners', () => {
+    const caseDetail = {
+      caseCaption: 'Garrett Carpenter, Leslie Bullock, Trustee, Petitioner(s)',
+    };
+    expect(getCaseCaptionMeta(caseDetail).caseCaptionExtension).toEqual(
+      'Petitioner(s)',
+    );
+  });
+});
