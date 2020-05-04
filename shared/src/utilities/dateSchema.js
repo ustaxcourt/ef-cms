22e3f6fad9d847ce465bbda5e84e484b484cbed2
Kris Koskelin <kkoskelin@flexion.us>
2020-05-04 11:47:31 -0500

    fix usage of `formats`

--- shared/src/utilities/dateSchema.js
+++ shared/src/utilities/dateSchema.js
@@ -4,5 +4,5 @@ const { FORMATS } = require('../business/utilities/DateHandler');
 exports.getTimestampSchema = () => {
   // eslint-disable-next-line spellcheck/spell-checker
   // TODO: remove FORMATS.YYYYMMDD from valid timestamp formats after devex task
-  return joi.date().iso().format(FORMATS.ISO, FORMATS.YYYYMMDD);
+  return joi.date().iso().format([FORMATS.ISO, FORMATS.YYYYMMDD]);
 };
