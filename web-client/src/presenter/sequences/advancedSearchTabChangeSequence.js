296d2171f15f28137c0b79270aa9ddca4b955e12
Mrinalini <mrinalinisinha05@gmail.com>
2020-05-04 13:39:22 -0700

    error messages cleared when switching tabs on advanced search screen

--- web-client/src/presenter/sequences/advancedSearchTabChangeSequence.js
+++ web-client/src/presenter/sequences/advancedSearchTabChangeSequence.js
@@ -1,7 +1,9 @@
+import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { clearSearchResultsAction } from '../actions/AdvancedSearch/clearSearchResultsAction';
 import { defaultAdvancedSearchFormAction } from '../actions/AdvancedSearch/defaultAdvancedSearchFormAction';
 
 export const advancedSearchTabChangeSequence = [
+  clearAlertsAction,
   defaultAdvancedSearchFormAction,
   clearSearchResultsAction,
 ];
