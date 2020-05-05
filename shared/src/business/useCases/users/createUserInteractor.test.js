3e4b39efd5e2e69b579666eac5813c32e30aae40
Rachel Schneiderman <rschneiderman@flexion.us>
2020-05-05 08:56:16 -0700

    Fixing valid role list for practitioner entity.

--- shared/src/business/useCases/users/createUserInteractor.test.js
+++ shared/src/business/useCases/users/createUserInteractor.test.js
@@ -9,19 +9,22 @@ const { createUserInteractor } = require('./createUserInteractor');
 const { User } = require('../../entities/User');
 
 describe('create user', () => {
+  let mockCurrentUser;
+
+  beforeEach(() => {
+    applicationContext.getCurrentUser.mockImplementation(() => mockCurrentUser);
+  });
+
   it('creates the user', async () => {
-    const mockUser = {
+    mockCurrentUser = {
+      role: 'admin',
+      userId: 'admin',
+    };
+    applicationContext.getPersistenceGateway().createUser.mockReturnValue({
       name: 'Test PetitionsClerk',
       role: User.ROLES.petitionsClerk,
       userId: 'petitionsclerk1@example.com',
-    };
-    applicationContext.getCurrentUser.mockReturnValue({
-      role: 'admin',
-      userId: 'admin',
     });
-    applicationContext
-      .getPersistenceGateway()
-      .createUser.mockReturnValue(mockUser);
     const userToCreate = { userId: 'petitionsclerk1@example.com' };
 
     const user = await createUserInteractor({
@@ -33,29 +36,22 @@ describe('create user', () => {
   });
 
   it('throws unauthorized for any user without an "admin" role', async () => {
-    const mockUser = {
+    mockCurrentUser = {
+      role: User.ROLES.petitioner,
+      userId: 'admin',
+    };
+    applicationContext.getPersistenceGateway().createUser.mockReturnValue({
       name: 'Test Petitioner',
       role: User.ROLES.petitioner,
       userId: 'petitioner1@example.com',
-    };
-    applicationContext.getCurrentUser.mockReturnValue({
-      role: User.ROLES.petitioner,
-      userId: 'admin',
     });
-    applicationContext
-      .getPersistenceGateway()
-      .createUser.mockReturnValue(mockUser);
     const userToCreate = { userId: 'petitioner1@example.com' };
-    let error;
-    try {
-      await createUserInteractor({
+
+    await expect(
+      createUserInteractor({
         applicationContext,
         user: userToCreate,
-      });
-    } catch (err) {
-      error = err;
-    }
-
-    expect(error instanceof UnauthorizedError).toBeTruthy();
+      }),
+    ).rejects.toThrow(UnauthorizedError);
   });
 });
