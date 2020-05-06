6f5d21dc427f9451dfc77d13fc4eb6b6412875fa
Matt Hopson <matthopson@gmail.com>
2020-05-06 13:07:55 -0500

    Add tests

---
+++ shared/src/business/utilities/generateHTMLTemplateForPDF/reactTemplateGenerator.test.js
@@ -0,0 +1,23 @@
+jest.mock('react');
+jest.mock('react-dom/server');
+const React = require('react');
+const ReactDOM = require('react-dom/server');
+
+const { reactTemplateGenerator } = require('./reactTemplateGenerator');
+
+describe('reactTemplateGenerator', () => {
+  beforeEach(() => {
+    React.createElement = jest.fn();
+    ReactDOM.renderToString = jest.fn();
+  });
+
+  it('calls the react render method and renders the result to string', () => {
+    reactTemplateGenerator({
+      componentName: 'SomeComponent',
+      data: { foo: 'bar' },
+    });
+
+    expect(React.createElement).toHaveBeenCalledWith();
+    expect(ReactDOM.renderToString).toHaveBeenCalled();
+  });
+});
