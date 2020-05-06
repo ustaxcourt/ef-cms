237d17e619297e9e1e5fcdfe70c1694aeb6e8f36
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-05-06 11:00:34 -0700

    4758: update petition from paper form to create case on submit

--- web-client/integration-tests/journey/petitionsClerkCreatesNewCase.js
+++ web-client/integration-tests/journey/petitionsClerkCreatesNewCase.js
@@ -8,7 +8,7 @@ export default (test, fakeFile, trialLocation = 'Birmingham, Alabama') => {
     await test.runSequence('gotoStartCaseWizardSequence');
     expect(test.getState('form.hasVerifiedIrsNotice')).toEqual(false);
 
-    await test.runSequence('reviewPetitionFromPaperSequence');
+    await test.runSequence('submitPetitionFromPaperSequence');
 
     expect(test.getState('alertError.title')).toEqual(
       'Please correct the following errors on the page:',
@@ -59,7 +59,7 @@ export default (test, fakeFile, trialLocation = 'Birmingham, Alabama') => {
       value: trialLocation,
     });
 
-    await test.runSequence('reviewPetitionFromPaperSequence');
+    await test.runSequence('submitPetitionFromPaperSequence');
 
     expect(
       test.getState('validationErrors.requestForPlaceOfTrialFile'),
@@ -105,7 +105,7 @@ export default (test, fakeFile, trialLocation = 'Birmingham, Alabama') => {
       value: 1,
     });
 
-    await test.runSequence('reviewPetitionFromPaperSequence');
+    await test.runSequence('submitPetitionFromPaperSequence');
 
     expect(
       test.getState('validationErrors.requestForPlaceOfTrialFile'),
@@ -186,7 +186,7 @@ export default (test, fakeFile, trialLocation = 'Birmingham, Alabama') => {
     expect(test.getState('alertError')).toBeUndefined();
     expect(test.getState('validationErrors')).toEqual({});
 
-    await test.runSequence('reviewPetitionFromPaperSequence');
+    await test.runSequence('submitPetitionFromPaperSequence');
 
     expect(test.getState('currentPage')).toEqual('ReviewPetitionFromPaper');
 
