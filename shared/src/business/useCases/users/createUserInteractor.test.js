126183e084d40f3151c02b4a9d9165b61e18bd7d
Mrinalini <mrinalinisinha05@gmail.com>
2020-05-06 10:10:24 -0700

    validating practitioner user before persisting user in db, added test

--- shared/src/business/useCases/users/createUserInteractor.test.js
+++ shared/src/business/useCases/users/createUserInteractor.test.js
@@ -9,42 +9,44 @@ const { createUserInteractor } = require('./createUserInteractor');
 const { User } = require('../../entities/User');
 
 describe('create user', () => {
-  let mockCurrentUser;
-
-  beforeEach(() => {
-    applicationContext.getCurrentUser.mockImplementation(() => mockCurrentUser);
-  });
-
   it('creates the user', async () => {
-    mockCurrentUser = {
-      role: 'admin',
-      userId: 'admin',
-    };
-    applicationContext.getPersistenceGateway().createUser.mockReturnValue({
+    const mockUser = {
       name: 'Test PetitionsClerk',
       role: User.ROLES.petitionsClerk,
       userId: 'petitionsclerk1@example.com',
+    };
+    applicationContext.getCurrentUser.mockReturnValue({
+      role: 'admin',
+      userId: 'admin',
     });
-    const userToCreate = { userId: 'petitionsclerk1@example.com' };
+    applicationContext
+      .getPersistenceGateway()
+      .createUser.mockReturnValue(mockUser);
 
+    const userToCreate = {
+      role: User.ROLES.petitionsClerk,
+      userId: 'petitionsclerk1@example.com',
+    };
     const user = await createUserInteractor({
       applicationContext,
       user: userToCreate,
     });
-
     expect(user).not.toBeUndefined();
   });
 
   it('throws unauthorized for any user without an "admin" role', async () => {
-    mockCurrentUser = {
-      role: User.ROLES.petitioner,
-      userId: 'admin',
-    };
-    applicationContext.getPersistenceGateway().createUser.mockReturnValue({
+    const mockUser = {
       name: 'Test Petitioner',
       role: User.ROLES.petitioner,
       userId: 'petitioner1@example.com',
+    };
+    applicationContext.getCurrentUser.mockReturnValue({
+      role: User.ROLES.petitioner,
+      userId: 'admin',
     });
+    applicationContext
+      .getPersistenceGateway()
+      .createUser.mockReturnValue(mockUser);
     const userToCreate = { userId: 'petitioner1@example.com' };
 
     await expect(
@@ -53,5 +55,41 @@ describe('create user', () => {
         user: userToCreate,
       }),
     ).rejects.toThrow(UnauthorizedError);
+  });
+
+  it('adds a barNumber and userId if the user is a practitioner', async () => {
+    const mockUser = {
+      barNumber: 'CS20001',
+      name: 'Test PrivatePractitioner',
+      role: User.ROLES.privatePractitioner,
+      userId: '745b7d39-8fae-4c2f-893c-3c829598bc71',
+    };
+    applicationContext.getCurrentUser.mockReturnValue({
+      role: 'admin',
+      userId: 'admin',
+    });
+    applicationContext
+      .getPersistenceGateway()
+      .createUser.mockReturnValue(mockUser);
+
+    const userToCreate = {
+      admissionsDate: new Date(),
+      admissionsStatus: 'Active',
+      birthYear: '1993',
+      employer: 'DOJ',
+      firstName: 'Test',
+      lastName: 'PrivatePractitioner',
+      originalBarState: 'CA',
+      practitionerType: 'Attorney',
+      role: User.ROLES.privatePractitioner,
+    };
+
+    const user = await createUserInteractor({
+      applicationContext,
+      user: userToCreate,
+    });
+    expect(user).not.toBeUndefined();
+    expect(user.barNumber).toBe('CS20001');
+    expect(user.userId).toBe('745b7d39-8fae-4c2f-893c-3c829598bc71');
   });
 });
