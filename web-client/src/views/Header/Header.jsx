a8d80276d6a19909671fbc236a866f4d5defd730
Kris Koskelin <kkoskelin@flexion.us>
2020-05-08 16:53:54 -0500

    testing husky, again

--- web-client/src/views/Header/Header.jsx
+++ web-client/src/views/Header/Header.jsx
@@ -1,5 +1,5 @@
 import { AccountMenu } from './AccountMenu';
-import { FontAwesomeIconz } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ReportsMenu } from './ReportsMenu';
 import { SearchBox } from './SearchBox';
 import { connect } from '@cerebral/react';
