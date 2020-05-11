d2553ce3ff5cb4c877c1c46bf2446117f8af8c95
Kris Koskelin <kkoskelin@flexion.us>
2020-05-11 13:19:13 -0500

    addressing PR comments, fixing test

--- web-client/src/presenter/sequences/updateAdvancedOpinionSearchFormValueSequence.js
+++
@@ -1,6 +0,0 @@
-import { props, state } from 'cerebral';
-import { set } from 'cerebral/factories';
-
-export const updateAdvancedOpinionSearchFormValueSequence = [
-  set(state.advancedSearchForm['opinionSearch'][props.key], props.value),
-];
