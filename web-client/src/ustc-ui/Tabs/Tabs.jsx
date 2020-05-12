788568a7a71ea984ca9ca799c982b12bc509d31c
Kris Koskelin <kkoskelin@flexion.us>
2020-05-12 12:39:51 -0500

    some lint fixes
    disabling some linting rules for future devex tasks

--- web-client/src/ustc-ui/Tabs/Tabs.jsx
+++ web-client/src/ustc-ui/Tabs/Tabs.jsx
@@ -1,9 +1,12 @@
 import { camelCase } from 'lodash';
 import { connect } from '@cerebral/react';
-import { decorateWithPostCallback } from '../utils/useCerebralState';
+import {
+  decorateWithPostCallback,
+  useCerebralStateFactory,
+} from '../utils/useCerebralState';
 import { getDefaultAttribute, map } from '../utils/ElementChildren';
 import { props, sequences, state } from 'cerebral';
-import { useCerebralStateFactory } from '../utils/useCerebralState';
+
 import React, { useState } from 'react';
 import classNames from 'classnames';
 
