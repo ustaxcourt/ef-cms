d3e9e1a0635b9e811bf996355eff4f521ee2811d
Rachel Schneiderman <rschneiderman@flexion.us>
2020-05-12 14:49:28 -0700

    Remove unnecessary paragraph element. Wrap download stin form link text on mobile. Added margin between continue and cancel buttons on mobile.

--- web-client/src/views/StartCase/StartCaseStep1.jsx
+++ web-client/src/views/StartCase/StartCaseStep1.jsx
@@ -69,13 +69,12 @@ export const StartCaseStep1 = connect(
             <p className="margin-top-0">
               <Button
                 link
-                className="usa-link--external text-left"
+                className="usa-link--external text-left mobile-text-wrap"
                 href="https://www.ustaxcourt.gov/forms/Form_4_Statement_of_Taxpayer_Identification_Number.pdf"
                 icon="file-pdf"
                 iconColor="blue"
                 overrideMargin="margin-right-1"
                 rel="noopener noreferrer"
-                shouldWrapText={true}
                 target="_blank"
               >
                 Download Statement of Taxpayer Identification Number (T.C. Form
