dd63003da93948acfc1dc1ad40be7f12eb84d899
Joseph Sutton <jsutton@flexion.us>
2020-05-12 10:04:03 -0500

    add unit tests

--- web-client/src/presenter/computeds/advancedSearchHelper.test.js
+++ web-client/src/presenter/computeds/advancedSearchHelper.test.js
@@ -1,6 +1,7 @@
 import { ContactFactory } from '../../../../shared/src/business/entities/contacts/ContactFactory';
 import { User } from '../../../../shared/src/business/entities/User';
 import {
+  advancedOpinionSearchHelper as advancedOpinionSearchHelperComputed,
   advancedOrderSearchHelper as advancedOrderSearchHelperComputed,
   advancedSearchHelper as advancedSearchHelperComputed,
 } from './advancedSearchHelper';
@@ -471,6 +472,149 @@ describe('advancedOrderSearchHelper', () => {
 
   it('formats search results for an order search', () => {
     const result = runCompute(advancedOrderSearchHelper, {
+      state: {
+        advancedSearchForm: { currentPage: 1 },
+        searchResults: [
+          {
+            caseCaption: 'Test Petitioner, Petitioner',
+            docketNumber: '101-19',
+            docketNumberSuffix: 'Z',
+            documentContents: 'Test Petitioner, Petitioner',
+            documentTitle: 'Order',
+            filingDate: '2019-03-01T05:00:00.000Z',
+            judge: 'Judge Buch',
+          },
+          {
+            caseCaption: 'Test Petitioner, Petitioner',
+            docketNumber: '102-19',
+            docketNumberSuffix: 'P',
+            documentContents: 'Test Petitioner, Petitioner',
+            documentTitle: 'Order for Stuff',
+            filingDate: '2019-03-01T05:00:00.000Z',
+            judge: 'Judge Cohen',
+          },
+        ],
+      },
+    });
+
+    expect(result.formattedSearchResults).toMatchObject([
+      {
+        caseTitle: 'Test Petitioner',
+        docketNumber: '101-19',
+        docketNumberSuffix: 'Z',
+        docketNumberWithSuffix: '101-19Z',
+        documentContents: 'Test Petitioner, Petitioner',
+        documentTitle: 'Order',
+        filingDate: '2019-03-01T05:00:00.000Z',
+        formattedFiledDate: '03/01/19',
+        judge: 'Judge Buch',
+      },
+      {
+        caseTitle: 'Test Petitioner',
+        docketNumber: '102-19',
+        docketNumberSuffix: 'P',
+        docketNumberWithSuffix: '102-19P',
+        documentContents: 'Test Petitioner, Petitioner',
+        documentTitle: 'Order for Stuff',
+        filingDate: '2019-03-01T05:00:00.000Z',
+        formattedFiledDate: '03/01/19',
+        judge: 'Judge Cohen',
+      },
+    ]);
+  });
+});
+
+describe('advancedOpinionSearchHelper', () => {
+  const pageSizeOverride = 5;
+
+  const advancedOpinionSearchHelper = withAppContextDecorator(
+    advancedOpinionSearchHelperComputed,
+    {
+      ...applicationContext,
+      getConstants: () => {
+        return {
+          ...applicationContext.getConstants(),
+          CASE_SEARCH_PAGE_SIZE: pageSizeOverride,
+        };
+      },
+    },
+  );
+
+  it('returns an empty object when searchResults is undefined', () => {
+    const result = runCompute(advancedOpinionSearchHelper, {
+      state: {
+        advancedSearchForm: {},
+      },
+    });
+
+    expect(result).toEqual({});
+  });
+
+  it('returns showNoMatches true and showSearchResults false when searchResults are empty', () => {
+    const result = runCompute(advancedOpinionSearchHelper, {
+      state: {
+        advancedSearchForm: { currentPage: 1 },
+        searchResults: [],
+      },
+    });
+
+    expect(result).toMatchObject({
+      showLoadMore: false,
+      showNoMatches: true,
+      showSearchResults: false,
+    });
+  });
+
+  it('returns isPublic false if state.isPublic is not defined', () => {
+    const result = runCompute(advancedOpinionSearchHelper, {
+      state: {
+        advancedSearchForm: { currentPage: 1 },
+        searchResults: [],
+      },
+    });
+
+    expect(result.isPublic).toBeFalsy();
+  });
+
+  it('returns isPublic true if state.isPublic is true', () => {
+    const result = runCompute(advancedOpinionSearchHelper, {
+      state: {
+        advancedSearchForm: { currentPage: 1 },
+        isPublic: true,
+        searchResults: [],
+      },
+    });
+
+    expect(result).toBeTruthy();
+  });
+
+  it('returns showNoMatches false, showSearchResults true, and the resultsCount when searchResults are not empty', () => {
+    const result = runCompute(advancedOpinionSearchHelper, {
+      state: {
+        advancedSearchForm: { currentPage: 1 },
+        searchResults: [
+          {
+            docketNumber: '101-19',
+            docketNumberSuffix: 'Z',
+            documentContents: 'Test Petitioner, Petitioner',
+            documentTitle: 'Order',
+            filingDate: '2019-03-01T05:00:00.000Z',
+            judge: 'Judge Buch',
+          },
+        ],
+      },
+    });
+
+    expect(result).toMatchObject({
+      searchResultsCount: 1,
+      showLoadMore: false,
+      showNoMatches: false,
+      showSearchResults: true,
+    });
+  });
+
+  it('formats search results for an opinion search', () => {
+    const result = runCompute(advancedOpinionSearchHelper, {
       state: {
         advancedSearchForm: { currentPage: 1 },
         searchResults: [
