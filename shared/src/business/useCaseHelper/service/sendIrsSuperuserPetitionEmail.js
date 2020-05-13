850e1d34197efe7d6c121ea2b366de010888ff56
Matt Hopson <matthopson@gmail.com>
2020-05-13 16:54:24 -0500

    Use react PetitionService template on service to irs

--- shared/src/business/useCaseHelper/service/sendIrsSuperuserPetitionEmail.js
+++ shared/src/business/useCaseHelper/service/sendIrsSuperuserPetitionEmail.js
@@ -1,33 +1,69 @@
-const { formatDateString } = require('../../utilities/DateHandler');
+const {
+  reactTemplateGenerator,
+} = require('../../utilities/generateHTMLTemplateForPDF/reactTemplateGenerator');
+const { Case } = require('../../entities/cases/Case');
 
 exports.sendIrsSuperuserPetitionEmail = async ({
   applicationContext,
   caseEntity,
   documentEntity,
 }) => {
+  const {
+    caseCaption,
+    contactPrimary,
+    contactSecondary,
+    docketNumber,
+    privatePractitioners,
+    trialLocation,
+  } = caseEntity;
+
+  const {
+    documentId,
+    documentTitle,
+    eventCode,
+    mailingDate,
+    servedAt,
+  } = documentEntity;
+
+  const docketEntry = caseEntity.docketRecord.find(
+    entry => entry.documentId === documentId,
+  );
+
+  const templateHtml = reactTemplateGenerator({
+    componentName: 'PetitionService',
+    data: {
+      caseDetail: {
+        caseTitle: Case.getCaseTitle(caseCaption),
+        docketNumber,
+        trialLocation,
+      },
+      contactPrimary,
+      contactSecondary,
+      docketEntryNumber: docketEntry.index,
+      documentDetail: {
+        documentTitle,
+        eventCode,
+        mailingDate,
+        servedAtFormatted: applicationContext
+          .getUtilities()
+          .formatDateString(servedAt, 'DATE_TIME_TZ'),
+      },
+      practitioners: privatePractitioners,
+      taxCourtLoginUrl: `https://ui-${process.env.STAGE}.${process.env.EFCMS_DOMAIN}`,
+    },
+  });
+
   const destination = {
     email: applicationContext.getIrsSuperuserEmail(),
     templateData: {
-      caseCaption: caseEntity.caseCaption,
-      docketNumber: caseEntity.docketNumber,
-      documentName: documentEntity.documentTitle,
-      loginUrl: `https://ui-${process.env.STAGE}.${process.env.EFCMS_DOMAIN}`,
-      name: 'IRS',
-      serviceDate: formatDateString(documentEntity.servedAt, 'MMDDYYYY'),
-      serviceTime: formatDateString(documentEntity.servedAt, 'TIME'),
+      emailContent: templateHtml,
     },
   };
 
   await applicationContext.getDispatchers().sendBulkTemplatedEmail({
     applicationContext,
     defaultTemplateData: {
-      caseCaption: 'undefined',
-      docketNumber: 'undefined',
-      documentName: 'undefined',
-      loginUrl: 'undefined',
-      name: 'undefined',
-      serviceDate: 'undefined',
-      serviceTime: 'undefined',
+      emailContent: '',
     },
     destinations: [destination],
     templateName: process.env.EMAIL_SERVED_TEMPLATE,
