54e723a17c6099caf6ed5749630418433287bbbb
Rachel Schneiderman <rschneiderman@flexion.us>
2020-05-13 10:04:21 -0700

    commented out pa11y test until functionality is implemented

--- web-client/pa11y/pa11y-public-user.js
+++ web-client/pa11y/pa11y-public-user.js
@@ -23,16 +23,19 @@ module.exports = [
     notes: 'checks a11y of advanced order search with no results',
     url: 'http://localhost:5678?info=no-results',
   },
-  {
-    actions: [
-      'wait for #tab-opinion to be visible',
-      'click element #tab-opinion',
-      'wait for #opinion-search to be visible',
-      'set field #opinion-search to sunglasses',
-      'click element button#advanced-search-button',
-      'wait for #no-search-results to be visible',
-    ],
-    notes: 'checks a11y of advanced opinion search with no results',
-    url: 'http://localhost:5678?info=no-results',
-  },
+  // TODO - This work will be part of #4837.
+  //  Add this test back in when opinion search results filter out opinions that
+  //  belong to a sealed case.
+  // {
+  //   actions: [
+  //     'wait for #tab-opinion to be visible',
+  //     'click element #tab-opinion',
+  //     'wait for #opinion-search to be visible',
+  //     'set field #opinion-search to sunglasses',
+  //     'click element button#advanced-search-button',
+  //     'wait for #no-search-results to be visible',
+  //   ],
+  //   notes: 'checks a11y of advanced opinion search with no results',
+  //   url: 'http://localhost:5678?info=no-results',
+  // },
 ];
