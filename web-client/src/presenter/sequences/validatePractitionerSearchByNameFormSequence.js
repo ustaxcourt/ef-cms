f61b30a33fa63b738ddd9b234bc1a442e0912754
Mrinalini <mrinalinisinha05@gmail.com>
2020-05-14 09:50:43 -0700

    updated practitioner search to validate on blur

--- web-client/src/presenter/sequences/validatePractitionerSearchByNameFormSequence.js
+++ web-client/src/presenter/sequences/validatePractitionerSearchByNameFormSequence.js
@@ -1,18 +1,20 @@
 import { clearAlertsAction } from '../actions/clearAlertsAction';
+import { clearSearchResultsAction } from '../actions/AdvancedSearch/clearSearchResultsAction';
+import { setAlertErrorAction } from '../actions/setAlertErrorAction';
 import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
-import { shouldValidateAction } from '../actions/shouldValidateAction';
+import { startShowValidationAction } from '../actions/startShowValidationAction';
+import { stopShowValidationAction } from '../actions/stopShowValidationAction';
 import { validatePractitionerSearchByNameAction } from '../actions/AdvancedSearch/validatePractitionerSearchByNameAction';
 
 export const validatePractitionerSearchByNameFormSequence = [
-  shouldValidateAction,
+  validatePractitionerSearchByNameAction,
   {
-    ignore: [],
-    validate: [
-      validatePractitionerSearchByNameAction,
-      {
-        error: [setValidationErrorsAction],
-        success: [clearAlertsAction],
-      },
+    error: [
+      setAlertErrorAction,
+      setValidationErrorsAction,
+      clearSearchResultsAction,
+      startShowValidationAction,
     ],
+    success: [clearAlertsAction, stopShowValidationAction],
   },
 ];
