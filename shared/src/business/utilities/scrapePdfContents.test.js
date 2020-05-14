3c2d4e1764f150129b3e6ff2b663140db4227c94
Joseph Sutton <jsutton@flexion.us>
2020-05-14 14:19:44 -0500

    added unit test for pdf utility method to scrape pdf contents

--- shared/src/business/utilities/scrapePdfContents.test.js
+++ shared/src/business/utilities/scrapePdfContents.test.js
@@ -1,9 +1,49 @@
-// import { applicationContext } from '../test/createTestApplicationContext';
-// import { scrapePdfContents } from './scrapePdfContents';
+import { applicationContext } from '../test/createTestApplicationContext';
+import { scrapePdfContents } from './scrapePdfContents';
 
 describe('scrapePdfContents', () => {
-  // let pdfBuffer = atob('');
-  // it('scrapes the pdf contents', async () => {
-  //   // todo
-  // });
+  beforeEach(() => {
+    applicationContext.getPdfJs().getDocument.mockReturnValue({
+      promise: {
+        getPage: jest.fn().mockResolvedValue({
+          getTextContent: jest.fn().mockResolvedValue({
+            items: [
+              {
+                str: 'this is the content',
+                transform: [0, 0, 0, 0, 'transform'],
+              },
+              {
+                str: 'this is some more content',
+                transform: [0, 0, 0, 0, 'transform'],
+              },
+            ],
+          }),
+        }),
+        numPages: 1,
+      },
+    });
+  });
+
+  it('scrapes the pdf contents', async () => {
+    const contents = await scrapePdfContents({
+      applicationContext,
+      pdfBuffer: [],
+    });
+
+    expect(contents.trim()).toEqual(
+      'this is the contentthis is some more content',
+    );
+  });
+
+  it('fails to scrape the pdf contents', async () => {
+    applicationContext.getPdfJs().getDocument.mockImplementation(() => {
+      throw new Error('what');
+    });
+    await expect(
+      scrapePdfContents({
+        applicationContext,
+        pdfBuffer: [],
+      }),
+    ).rejects.toThrow('error scraping PDF');
+  });
 });
