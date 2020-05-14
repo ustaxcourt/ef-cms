035fd34e895001e621da4d43702c67d781a53ee4
Mrinalini <mrinalinisinha05@gmail.com>
2020-05-14 10:09:59 -0700

    case search now validating on blur

--- web-client/src/presenter/sequences/validateCaseDocketNumberSearchFormSequence.js
+++ web-client/src/presenter/sequences/validateCaseDocketNumberSearchFormSequence.js
@@ -1,18 +1,20 @@
 import { clearAlertsAction } from '../actions/clearAlertsAction';
+import { clearSearchResultsAction } from '../actions/AdvancedSearch/clearSearchResultsAction';
+import { setAlertErrorAction } from '../actions/setAlertErrorAction';
 import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
-import { shouldValidateAction } from '../actions/shouldValidateAction';
+import { startShowValidationAction } from '../actions/startShowValidationAction';
+import { stopShowValidationAction } from '../actions/stopShowValidationAction';
 import { validateCaseDocketNumberSearchAction } from '../actions/AdvancedSearch/validateCaseDocketNumberSearchAction';
 
 export const validateCaseDocketNumberSearchFormSequence = [
-  shouldValidateAction,
+  validateCaseDocketNumberSearchAction,
   {
-    ignore: [],
-    validate: [
-      validateCaseDocketNumberSearchAction,
-      {
-        error: [setValidationErrorsAction],
-        success: [clearAlertsAction],
-      },
+    error: [
+      setAlertErrorAction,
+      setValidationErrorsAction,
+      clearSearchResultsAction,
+      startShowValidationAction,
     ],
+    success: [clearAlertsAction, stopShowValidationAction],
   },
 ];
