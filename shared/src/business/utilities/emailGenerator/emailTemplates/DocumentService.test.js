3d62ad65970592478b706ee26d9d1eb424d0c39e
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-05-14 13:44:47 -0700

    2383: update document service email content

--- shared/src/business/utilities/emailGenerator/emailTemplates/DocumentService.test.js
+++ shared/src/business/utilities/emailGenerator/emailTemplates/DocumentService.test.js
@@ -3,27 +3,82 @@ const { DocumentService } = require('./DocumentService.jsx');
 const { shallow } = require('enzyme');
 
 describe('DocumentService', () => {
-  it('renders the expected fields', () => {
+  const caseDetail = {
+    caseTitle: 'Test Case Title',
+    docketNumber: '123-45',
+  };
+  const docketEntryNumber = 1;
+  const documentDetail = {
+    documentTitle: 'Answer',
+    eventCode: 'A',
+    filedBy: 'Petr. Guy Fieri',
+    servedAtFormatted: '02/03/2020 12:00am EST',
+  };
+  const taxCourtLoginUrl = 'http://example.com/login';
+
+  it('renders case information', () => {
     const wrapper = shallow(
       <DocumentService
-        caseCaption="A Case Caption"
-        docketNumber="101-19P"
-        documentName="Petition"
-        loginUrl="http://example.com"
-        name="Bob James"
-        serviceDate="01/01/2020"
-        serviceTime="10:10pm"
+        caseDetail={caseDetail}
+        docketEntryNumber={docketEntryNumber}
+        documentDetail={documentDetail}
+        taxCourtLoginUrl={taxCourtLoginUrl}
       />,
     );
 
-    const documentService = wrapper.find('.document-service');
+    const caseInfo = wrapper.find('#case-information');
 
-    expect(documentService.text()).toContain('Bob James');
-    expect(documentService.text()).toContain('101-19P');
-    expect(documentService.text()).toContain('A Case Caption');
-    expect(documentService.text()).toContain('Petition');
-    expect(documentService.text()).toContain('01/01/2020');
-    expect(documentService.text()).toContain('10:10pm');
-    expect(wrapper.find(`a[href="${'http://example.com'}"]`).length).toEqual(1);
+    expect(caseInfo.text()).toContain(caseDetail.docketNumber);
+    expect(caseInfo.text()).toContain(caseDetail.caseTitle);
+  });
+
+  it('renders document information', () => {
+    const wrapper = shallow(
+      <DocumentService
+        caseDetail={caseDetail}
+        docketEntryNumber={docketEntryNumber}
+        documentDetail={documentDetail}
+        taxCourtLoginUrl={taxCourtLoginUrl}
+      />,
+    );
+    const documentInfo = wrapper.find('#document-information');
+
+    expect(documentInfo.text()).toContain(documentDetail.eventCode);
+    expect(documentInfo.text()).toContain(documentDetail.documentTitle);
+    expect(documentInfo.text()).toContain(
+      `Docket Entry No.: ${docketEntryNumber}`,
+    );
+    expect(documentInfo.text()).toContain(documentDetail.servedAtFormatted);
+    expect(documentInfo.text()).toContain(documentDetail.filedBy);
+  });
+
+  it('renders IRS information if user name is IRS', () => {
+    const wrapper = shallow(
+      <DocumentService
+        caseDetail={caseDetail}
+        docketEntryNumber={docketEntryNumber}
+        documentDetail={documentDetail}
+        name="IRS"
+        taxCourtLoginUrl={taxCourtLoginUrl}
+      />,
+    );
+    const documentInfo = wrapper.find('#irs-information');
+
+    expect(documentInfo.text()).toContain('For IRS only');
+  });
+
+  it('does not render IRS information if user name is not IRS', () => {
+    const wrapper = shallow(
+      <DocumentService
+        caseDetail={caseDetail}
+        docketEntryNumber={docketEntryNumber}
+        documentDetail={documentDetail}
+        name="Guy Fieri"
+        taxCourtLoginUrl={taxCourtLoginUrl}
+      />,
+    );
+    const documentInfo = wrapper.find('#irs-information');
+
+    expect(documentInfo).toEqual({});
   });
 });
--- shared/src/business/utilities/emailGenerator/emailTemplates/PetitionService.jsx
+++ shared/src/business/utilities/emailGenerator/emailTemplates/PetitionService.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-
 import { EmailHeader } from '../components/EmailHeader';
+import React from 'react';
 
 const RenderContactAddress = ({ contact }) => {
   return (
