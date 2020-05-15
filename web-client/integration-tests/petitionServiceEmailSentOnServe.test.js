933047610e9d7eb5732ddc4aea2f9db9dbc3cccf
Cody Seibert <cseibert@flexion.us>
2020-05-14 21:32:16 -0400

    lint and removing console logs

--- web-client/integration-tests/petitionServiceEmailSentOnServe.test.js
+++ web-client/integration-tests/petitionServiceEmailSentOnServe.test.js
@@ -1,6 +1,12 @@
-import { loginAs, setupTest } from './helpers';
+import {
+  deleteEmails,
+  getEmailsForAddress,
+  loginAs,
+  setupTest,
+  uploadPetition,
+} from './helpers';
+
 import { petitionsClerkSubmitsCaseToIrs } from './journey/petitionsClerkSubmitsCaseToIrs';
-import { deleteEmails, getEmailsForAddress, uploadPetition } from './helpers';
 
 const test = setupTest();
 
@@ -21,7 +27,6 @@ describe('Petition Service Email Sent on Serve', () => {
   it('should send the expected emails to irs super user', async () => {
     const emails = await getEmailsForAddress('irsSuperuserEmail@example.com');
     await deleteEmails(emails);
-    console.log(emails.length);
     expect(emails.length).toEqual(1);
     expect(emails[0].template.replace(/<!-- -->/g, '')).toContain(
       `docketNumber: ${test.docketNumber}`,
