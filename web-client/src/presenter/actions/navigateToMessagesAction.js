f0284d902f97f129fc0254d788535fc09d574e39
Rachel Schneiderman <rschneiderman@flexion.us>
2020-05-15 11:59:10 -0700

    Updating docs

--- web-client/src/presenter/actions/navigateToMessagesAction.js
+++ web-client/src/presenter/actions/navigateToMessagesAction.js
@@ -2,7 +2,7 @@
  * changes the route to messages, which is "/messages"
  *
  * @param {object} providers the providers object
- * @param {object} providers.router the providers.router object that is used for changing the route
+ * @param {object} providers.router the riot.router object that is used for changing the route
  * @returns {Promise} async action
  */
 export const navigateToMessagesAction = async ({ router }) => {
