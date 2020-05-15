e05f190ae740e947fbb84ebe254a9b320d580312
Cody Seibert <cseibert@flexion.us>
2020-05-15 15:10:05 -0400

    fixing tests

--- web-client/integration-tests/petitionServiceEmailSentOnServe.test.js
+++ web-client/integration-tests/petitionServiceEmailSentOnServe.test.js
@@ -1,5 +1,4 @@
 import {
-  deleteEmails,
   getEmailsForAddress,
   loginAs,
   setupTest,
@@ -19,6 +18,9 @@ describe('Petition Service Email Sent on Serve', () => {
   it('Create case', async () => {
     const caseDetail = await uploadPetition(test);
     test.docketNumber = caseDetail.docketNumber;
+    test.documentId = caseDetail.documents.find(
+      d => d.documentType === 'Petition',
+    ).documentId;
   });
 
   loginAs(test, 'petitionsclerk');
@@ -26,14 +28,15 @@ describe('Petition Service Email Sent on Serve', () => {
 
   it('should send the expected emails to irs super user', async () => {
     const emails = await getEmailsForAddress('irsSuperuserEmail@example.com');
-    await deleteEmails(emails);
     emails.forEach(email => {
       email.template = email.template.replace(/<!-- -->/g, '');
     });
     const orderEmail = emails.find(
       email =>
-        email.template.indexOf(`docketNumber: ${test.docketNumber}`) !== -1,
+        email.template.indexOf(`docketNumber: ${test.docketNumber}`) !== -1 &&
+        email.template.indexOf('A new Petition has been served') !== -1 &&
+        email.template.indexOf(`documentId: ${test.documentId}`) !== -1,
     );
-    expect(orderEmail.template).toContain('A new Petition has been served');
+    expect(orderEmail).toBeDefined();
   });
 });
