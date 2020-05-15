3fa3622c6538c72d61c40aefcf326ebb072d4edb
Rachel Schneiderman <rschneiderman@flexion.us>
2020-05-15 11:59:10 -0700

    Updating docs

--- web-client/src/presenter/actions/navigateToFilePetitionSuccessAction.js
+++ web-client/src/presenter/actions/navigateToFilePetitionSuccessAction.js
@@ -2,7 +2,7 @@
  * changes the route to file-a-petition/success
  *
  * @param {object} providers the providers object
- * @param {object} providers.router the providers.router object that is used for changing the route
+ * @param {object} providers.router the riot.router object that is used for changing the route
  * @returns {Promise} async action
  */
 export const navigateToFilePetitionSuccessAction = async ({ router }) => {
