3cb6dca38a0e85f82b1d0dd0297362f37cf9242c
Joseph Sutton <jsutton@flexion.us>
2020-05-18 13:52:30 -0500

    added test coverage to pdf content scraper

--- shared/src/business/utilities/scrapePdfContents.js
+++ shared/src/business/utilities/scrapePdfContents.js
@@ -20,11 +20,14 @@ const scrapePdfContents = async ({ applicationContext, pdfBuffer }) => {
         normalizeWhitespace: false,
       });
 
-      let lastY,
+      let lastY = null,
         pageText = '';
 
       for (let item of pageTextContent.items) {
-        if (lastY == item.transform[5] || !lastY) {
+        console.log('lastY', lastY);
+        console.log('item.transform[5]', item.transform[5]);
+        console.log('-----------------------');
+        if (lastY === item.transform[5] || !lastY) {
           pageText += item.str;
         } else {
           pageText += '\n' + item.str;
