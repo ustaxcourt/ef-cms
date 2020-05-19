22bc7312c6d3a5f70204c2f76c0f193c3152b3ef
Mrinalini <mrinalinisinha05@gmail.com>
2020-05-19 07:57:21 -0700

    updated jsdocs

--- web-client/src/presenter/actions/setOpinionTypesAction.js
+++ web-client/src/presenter/actions/setOpinionTypesAction.js
@@ -1,11 +1,11 @@
 import { state } from 'cerebral';
 
 /**
- * sets the state.opinionTypes which is used for displaying the opinion document types on the advanced search form.
+ * sets the state.opinionDocumentTypes which is used for displaying the opinion document types on the advanced search form.
  *
  * @param {object} providers the providers object
- * @param {object} providers.props the cerebral props object containing the props.caseDetail
- * @param {object} providers.store the cerebral store used for setting the state.caseDetail
+ * @param {object} providers.props the cerebral props object containing the props.opinionDocumentTypes
+ * @param {object} providers.store the cerebral store used for setting the state.opinionDocumentTypes
  */
 export const setOpinionTypesAction = ({ props, store }) => {
   store.set(state.opinionDocumentTypes, props.opinionDocumentTypes);
