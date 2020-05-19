5a53b106cae90f3b6db180c80feff67e50aad374
Mrinalini <mrinalinisinha05@gmail.com>
2020-05-19 11:07:25 -0700

    refined correspondence table to more closely match figma

--- web-client/src/views/Correspondence/DeleteCorrespondenceModal.jsx
+++ web-client/src/views/Correspondence/DeleteCorrespondenceModal.jsx
@@ -1,17 +1,18 @@
 import { ConfirmModal } from '../../ustc-ui/Modal/ConfirmModal';
 import { connect } from '@cerebral/react';
+import { sequences } from 'cerebral';
 import React from 'react';
 
 export const DeleteCorrespondenceModal = connect(
-  {},
+  { clearModalSequence: sequences.clearModalSequence },
   function DeleteCorrespondenceModal() {
     return (
       <ConfirmModal
         cancelLabel="No, Take me back"
         confirmLabel="Yes, Delete"
         title={'Are You Sure You Want to Delete This Document?'}
-        // onCancelSequence="clearModalSequence"
-        // onConfirmSequence="removeBatchSequence"
+        onCancelSequence="clearModalSequence"
+        onConfirmSequence="clearModalSequence"
       >
         <div className="padding-bottom-1">
           Once deleted, it can’t be restored.
