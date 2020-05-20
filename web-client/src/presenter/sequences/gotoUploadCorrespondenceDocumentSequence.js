fb2273e3c9d307868c44ba6d88ef29f972550bf8
Joseph Sutton <jsutton@flexion.us>
2020-05-20 15:21:49 -0500

    removed unnecessary add correspondence form values being set

--- web-client/src/presenter/sequences/gotoUploadCorrespondenceDocumentSequence.js
+++ web-client/src/presenter/sequences/gotoUploadCorrespondenceDocumentSequence.js
@@ -1,7 +1,6 @@
 import { clearFormAction } from '../actions/clearFormAction';
 import { clearScreenMetadataAction } from '../actions/clearScreenMetadataAction';
 import { getCaseAction } from '../actions/getCaseAction';
-import { initializeUploadFormAction } from '../actions/uploadCourtIssuedDocument/initializeUploadFormAction';
 import { isLoggedInAction } from '../actions/isLoggedInAction';
 import { redirectToCognitoAction } from '../actions/redirectToCognitoAction';
 import { setCaseAction } from '../actions/setCaseAction';
@@ -12,7 +11,6 @@ const gotoUploadCorrespondenceDocument = [
   setCurrentPageAction('Interstitial'),
   stopShowValidationAction,
   clearFormAction,
-  initializeUploadFormAction,
   clearScreenMetadataAction,
   getCaseAction,
   setCaseAction,
