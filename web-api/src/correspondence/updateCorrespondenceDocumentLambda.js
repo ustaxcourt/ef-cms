626f015ac6fbd5507f493d2d2d57b490baccf394
Rachel Schneiderman <rschneiderman@flexion.us>
2020-05-20 13:39:25 -0700

    Fixing test and linting error.

--- web-api/src/correspondence/updateCorrespondenceDocumentLambda.js
+++ web-api/src/correspondence/updateCorrespondenceDocumentLambda.js
@@ -1,15 +1,16 @@
-const { genericHandler } = require('../genericHandler');
+// const { genericHandler } = require('../genericHandler');
 
 /**
- * update a correspondence document
+ * upload a correspondence document
  *
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-exports.updateCorrespondenceDocumentLambda = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    // return await applicationContext.getUseCases().uploadCorrespondenceDocument({
-    //   ...JSON.parse(event.body),
-    //   applicationContext,
-    // });
-  });
+// TODO - implement in next step
+// exports.updateCorrespondenceDocumentLambda = event =>
+//   genericHandler(event, async ({ applicationContext }) => {
+//     return await applicationContext.getUseCases().uploadCorrespondenceDocument({
+//       ...JSON.parse(event.body),
+//       applicationContext,
+//     });
+//   });
