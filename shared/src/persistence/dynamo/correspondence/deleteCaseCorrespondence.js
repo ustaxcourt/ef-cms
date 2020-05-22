025bf724e5bb7232d52ce160ba8cb42037cc05ad
Rachel Schneiderman <rschneiderman@flexion.us>
2020-05-22 13:30:22 -0700

    Adding tests and updating documentation

--- shared/src/persistence/dynamo/correspondence/deleteCaseCorrespondence.js
+++ shared/src/persistence/dynamo/correspondence/deleteCaseCorrespondence.js
@@ -1,12 +1,12 @@
 const client = require('../../dynamodbClientService');
 
 /**
- * deleteCaseDeadline
+ * deleteCaseCorrespondence
  *
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
- * @param {string} providers.caseDeadlineId the id of the case deadline to delete
- * @param {string} providers.caseId the id of the case the deadline is attached to
+ * @param {string} providers.caseId the id of the case the correspondence is attached to
+ * @param {string} providers.documentIdToDelete the id of the correspondence document to delete
  * @returns {Array<Promise>} the promises for the persistence delete calls
  */
 exports.deleteCaseCorrespondence = async ({
