bf82addb918bc31c2c9ba24ab68a82fc0ac7388e
Joseph Sutton <jsutton@flexion.us>
2020-05-22 10:23:27 -0500

    jsdoc

--- shared/src/business/entities/Document.js
+++ shared/src/business/entities/Document.js
@@ -652,6 +652,14 @@ Document.prototype.isAutoServed = function () {
   );
 };
 
+/**
+ * retrieves formatted document type (stripped eventCode, without the dash).
+ * if it's TCOP - TC Opinion, it retrieves TC Opinion.
+ * if it's Summary Opinion, then it returns Summary Opinion
+ *
+ * @param {string} documentType document type to strip the event code
+ * @returns {string} formatted document type
+ */
 Document.getFormattedType = function (documentType) {
   return documentType.split('-').slice(-1).join('').trim();
 };
