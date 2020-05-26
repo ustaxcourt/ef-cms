e13bae22edb63f696235fa7915be4036315518aa
Rachel Schneiderman <rschneiderman@flexion.us>
2020-05-26 09:56:53 -0700

    Updated based on PR feedback.

--- web-client/src/presenter/sequences/closeModalAndReturnToCaseDetailDraftDocumentsSequence.js
+++ web-client/src/presenter/sequences/closeModalAndReturnToCaseDetailDraftDocumentsSequence.js
@@ -4,13 +4,11 @@ import { setCaseDetailPageTabActionGenerator } from '../actions/setCaseDetailPag
 import { setCaseDetailPageTabFrozenAction } from '../actions/CaseDetail/setCaseDetailPageTabFrozenAction';
 import { setCasePropFromStateAction } from '../actions/setCasePropFromStateAction';
 import { setIsPrimaryTabAction } from '../actions/setIsPrimaryTabAction';
-import { setTabToInProgressAction } from '../actions/editUploadCourtIssuedDocument/setTabToInProgressAction';
 
 export const closeModalAndReturnToCaseDetailDraftDocumentsSequence = [
   clearModalAction,
   setCasePropFromStateAction,
-  setTabToInProgressAction,
-  setCaseDetailPageTabActionGenerator(),
+  setCaseDetailPageTabActionGenerator('inProgress'),
   setIsPrimaryTabAction,
   setCaseDetailPageTabFrozenAction,
   navigateToCaseDetailAction,
