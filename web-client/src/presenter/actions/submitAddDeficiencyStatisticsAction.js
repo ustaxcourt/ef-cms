da829bb726f476cd0f68282d7e8a16c56d2c978c
Matt Hopson <matthopson@gmail.com>
2020-05-27 11:04:06 -0500

    Submit add deficiency statistics sequence

---
+++ web-client/src/presenter/actions/submitAddDeficiencyStatisticsAction.js
@@ -0,0 +1,53 @@
+import { state } from 'cerebral';
+/**
+ * submits the add deficiency statistics form
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.get the cerebral get function
+ * @param {object} providers.applicationContext the applicationContext
+ * @param {object} providers.path the next object in the path
+ * @returns {Promise<*>} the success or error path
+ */
+export const submitAddDeficiencyStatisticsAction = async ({
+  applicationContext,
+  get,
+  path,
+}) => {
+  const {
+    determinationDeficiencyAmount,
+    determinationTotalPenalties,
+    irsDeficiencyAmount,
+    irsTotalPenalties,
+    lastDateOfPeriod,
+    year,
+    yearOrPeriod,
+  } = get(state.form);
+
+  const { caseId } = get(state.caseDetail);
+
+  try {
+    applicationContext.getUseCases().addDeficiencyStatisticInteractor({
+      applicationContext,
+      caseId,
+      determinationDeficiencyAmount,
+      determinationTotalPenalties,
+      irsDeficiencyAmount,
+      irsTotalPenalties,
+      lastDateOfPeriod,
+      year,
+      yearOrPeriod,
+    });
+
+    return path.success({
+      alertSuccess: {
+        message: 'Statistics updated.',
+      },
+    });
+  } catch (e) {
+    return path.error({
+      alertError: {
+        title: 'Errors were found. Please correct your form and resubmit.',
+      },
+    });
+  }
+};
