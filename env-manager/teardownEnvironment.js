0ab51952a3635e38093c4c81874312e9e979c41d
Eric Sorenson <eric@sorenson.software>
2020-05-27 15:50:11 -0500

    deploy exp1

--- env-manager/teardownEnvironment.js
+++ env-manager/teardownEnvironment.js
@@ -1,14 +1,12 @@
-const { deleteCognitoPools } = require('./deleteCognitoPools');
 const { deleteCustomDomains } = require('./deleteCustomDomains');
-const { deleteDynamoDBTables } = require('./deleteDynamoDBTables');
-const { deleteS3Buckets } = require('./deleteS3Buckets');
 const { deleteStacks } = require('./deleteStacks');
 
-// TODO: get all values from ENV variables and validate that they exist
+const environmentName = 'eric';
+
 const environmentEast = {
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   apiVersion: 'latest',
-  name: 'exp',
+  name: environmentName,
   region: 'us-east-1',
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 };
@@ -16,7 +14,7 @@ const environmentEast = {
 const environmentWest = {
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   apiVersion: 'latest',
-  name: 'exp',
+  name: environmentName,
   region: 'us-west-1',
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 };
@@ -27,14 +25,6 @@ const teardownEnvironment = async () => {
 
   await deleteStacks({ environment: environmentEast });
   await deleteStacks({ environment: environmentWest });
-
-  await deleteDynamoDBTables({ environment: environmentEast });
-  await deleteDynamoDBTables({ environment: environmentWest });
-
-  await deleteCognitoPools({ environment: environmentEast });
-
-  await deleteS3Buckets({ environment: environmentEast });
-  await deleteS3Buckets({ environment: environmentWest });
 };
 
 teardownEnvironment();
