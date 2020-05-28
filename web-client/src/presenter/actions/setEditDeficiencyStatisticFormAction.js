9caaeb74a25a908f48e81012979c49962c635bb6
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-05-28 10:14:49 -0700

    fixes

--- web-client/src/presenter/actions/setEditDeficiencyStatisticFormAction.js
+++ web-client/src/presenter/actions/setEditDeficiencyStatisticFormAction.js
@@ -4,8 +4,9 @@ import { state } from 'cerebral';
  * sets the state.form using the statistic from the caseDetail
  *
  * @param {object} providers the providers object
- * @param {object} providers.get the cerebral get function
- * @param {object} providers.store the cerebral store used for setting the state.caseDetail
+ * @param {Function} providers.get the cerebral get function
+ * @param {object} providers.props the cerebral props object
+ * @param {object} providers.store the cerebral store
  */
 export const setEditDeficiencyStatisticFormAction = ({ get, props, store }) => {
   const { statistics } = get(state.caseDetail);
