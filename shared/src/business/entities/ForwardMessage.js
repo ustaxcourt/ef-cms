37689fc0cb9b1525ff9cd89b6a73646625b9a4e8
Eric Sorenson <eric@sorenson.software>
2020-06-01 11:52:17 -0500

    ForwardMessage.forwardMessage validations

--- shared/src/business/entities/ForwardMessage.js
+++ shared/src/business/entities/ForwardMessage.js
@@ -2,6 +2,7 @@ const joi = require('@hapi/joi');
 const {
   joiValidationDecorator,
 } = require('../../utilities/JoiValidationDecorator');
+import { SECTIONS } from './WorkQueue';
 
 ForwardMessage.VALIDATION_ERROR_MESSAGES = {
   assigneeId: 'Select a recipient',
@@ -29,8 +30,11 @@ joiValidationDecorator(
         version: ['uuidv4'],
       })
       .required(),
-    forwardMessage: joi.string().required(),
-    section: joi.string().required(),
+    forwardMessage: joi.string().max(500).required(),
+    section: joi
+      .string()
+      .valid(...SECTIONS)
+      .required(),
   }),
   ForwardMessage.VALIDATION_ERROR_MESSAGES,
 );
