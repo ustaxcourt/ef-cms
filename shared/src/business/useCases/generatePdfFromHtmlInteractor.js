d3c5ef94a1e8f1a2516fb3f7405c528a187bd7dc
Eric Sorenson <eric@sorenson.software>
2020-06-03 07:34:26 -0500

    enitity validation: WorkItem.assigneeId must be a uuidv4

--- shared/src/business/useCases/generatePdfFromHtmlInteractor.js
+++ shared/src/business/useCases/generatePdfFromHtmlInteractor.js
@@ -1,5 +1,6 @@
 /**
  * generatePdfFromHtmlInteractor
+ *
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
  * @param {string} providers.docketNumber the docket number of the case
