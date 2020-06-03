5a3b4cd1f42b70137854c3bebde19fea11d9df02
Mrinalini <mrinalinisinha05@gmail.com>
2020-06-03 12:41:23 -0700

    not consolidating cases when retrieving closed cases

--- shared/src/business/useCases/getClosedConsolidatedCasesInteractor.js
+++ shared/src/business/useCases/getClosedConsolidatedCasesInteractor.js
@@ -12,7 +12,6 @@ exports.getClosedConsolidatedCasesInteractor = async ({
 }) => {
   let openCases;
   let foundCases = [];
-  let userCaseIdsMap = {};
 
   const { userId } = await applicationContext.getCurrentUser();
 
@@ -20,69 +19,9 @@ exports.getClosedConsolidatedCasesInteractor = async ({
     .getPersistenceGateway()
     .getClosedCasesByUser({ applicationContext, userId });
 
-  const userCasesValidated = Case.validateRawCollection(openCases, {
+  foundCases = Case.validateRawCollection(openCases, {
     applicationContext,
   });
-
-  if (userCasesValidated.length) {
-    const caseMapping = {};
-    const leadCaseIdsToGet = [];
-
-    userCasesValidated.forEach(caseRecord => {
-      const { caseId, leadCaseId } = caseRecord;
-
-      caseRecord.isRequestingUserAssociated = true;
-      userCaseIdsMap[caseId] = true;
-
-      if (!leadCaseId || leadCaseId === caseId) {
-        caseMapping[caseId] = caseRecord;
-      }
-
-      if (leadCaseId) {
-        if (leadCaseIdsToGet.indexOf(leadCaseId) === -1) {
-          leadCaseIdsToGet.push(leadCaseId);
-        }
-      }
-    });
-
-    for (const leadCaseId of leadCaseIdsToGet) {
-      const consolidatedCases = await applicationContext
-        .getPersistenceGateway()
-        .getCasesByLeadCaseId({
-          applicationContext,
-          leadCaseId,
-        });
-
-      const consolidatedCasesValidated = Case.validateRawCollection(
-        consolidatedCases,
-        { applicationContext, filtered: true },
-      );
-
-      if (!caseMapping[leadCaseId]) {
-        const leadCase = consolidatedCasesValidated.find(
-          consolidatedCase => consolidatedCase.caseId === leadCaseId,
-        );
-        leadCase.isRequestingUserAssociated = false;
-        caseMapping[leadCaseId] = leadCase;
-      }
-
-      const caseConsolidatedCases = [];
-      consolidatedCasesValidated.forEach(consolidatedCase => {
-        consolidatedCase.isRequestingUserAssociated = !!userCaseIdsMap[
-          consolidatedCase.caseId
-        ];
-        if (consolidatedCase.caseId !== leadCaseId) {
-          caseConsolidatedCases.push(consolidatedCase);
-        }
-      });
-
-      caseMapping[leadCaseId].consolidatedCases = Case.sortByDocketNumber(
-        caseConsolidatedCases,
-      );
-    }
-
-    foundCases = Object.keys(caseMapping).map(caseId => caseMapping[caseId]);
-  }
 
   return foundCases;
 };
