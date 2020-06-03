da6de0f537950f3f3643adbdbd4d524c691bb6c7
Matt Hopson <matthopson@gmail.com>
2020-06-03 11:49:14 -0600

    Add receipt of petition to document generators

--- shared/src/business/utilities/pdfGenerator/documentTemplates/NoticeOfReceiptOfPetition.jsx
+++ shared/src/business/utilities/pdfGenerator/documentTemplates/NoticeOfReceiptOfPetition.jsx
@@ -22,40 +22,42 @@ export const NoticeOfReceiptOfPetition = ({
         docketNumberWithSuffix={docketNumberWithSuffix}
         h3="NOTIFICATION OF RECEIPT OF PETITION"
       />
-      <div className="margin-top-80">
+      <div>
         The Court received and filed your petition on {receivedAtFormatted} and
         served it on respondent on {servedDate}.
       </div>
 
-      <div className="margin-bottom-20">
+      <div className="margin-top-5 margin-bottom-20">
         (X) Request for Place of Trial at {preferredTrialCity}.
       </div>
 
-      <div className="info-box">
-        <h3>Your Docket Number: {docketNumberWithSuffix}</h3>
-        <p>
+      <div className="info-box margin-bottom-0">
+        <div className="info-box-header">
+          Your Docket Number: {docketNumberWithSuffix}
+        </div>
+        <div className="info-box-content">
           Please use this docket number on all papers and correspondence that
           you send to the Tax Court. Do not include your Social Security or
           Taxpayer Identification numbers on any documents you file with the
           Court.
-        </p>
+        </div>
       </div>
 
-      <div className="info-box">
-        <h3>Internet Access:</h3>
-        <p>
+      <div className="info-box margin-bottom-0">
+        <div className="info-box-header">Internet Access:</div>
+        <div className="info-box-content">
           To obtain further information about proceeding in the Tax Court,
           please visit{' '}
           <strong>
             <a href="http://www.ustaxcourt.gov">www.ustaxcourt.gov</a>
           </strong>{' '}
           and select &quot;Taxpayer Identification&quot;.
-        </p>
+        </div>
       </div>
 
       <div className="info-box">
-        <h3>Change of Address:</h3>
-        <p>
+        <div className="info-box-header">Change of Address:</div>
+        <div className="info-box-content">
           You must notify the Clerk of the Court if you change your address. If
           you filed your petition in paper, see Tax Court Form 10, Notice of
           Change of Address, under “Forms” on the Tax Court’s Website at{' '}
@@ -67,7 +69,7 @@ export const NoticeOfReceiptOfPetition = ({
           to notify the Clerk of the Court of a change of your address can mean
           you do not receive notices and documents essential to your case and
           can lead to dismissal of your case.
-        </p>
+        </div>
       </div>
 
       <p className="float-right width-third">
