4a5a31185362c7e3afc1517ba2171258509604e9
Joseph Sutton <jsutton@flexion.us>
2020-06-04 16:08:47 -0500

    consolidated set cases actions

--- web-client/src/presenter/actions/setCasesAction.js
+++ web-client/src/presenter/actions/setCasesAction.js
@@ -1,12 +1,13 @@
 import { state } from 'cerebral';
 
 /**
- * sets the state.closedCases based on the props.closedCaseList passed in
+ * sets the state.openCases and state.closedCases based on the props.openCaseList passed in
  *
  * @param {object} providers the providers object
  * @param {object} providers.store the cerebral store used for setting the state.cases
  * @param {object} providers.props the cerebral props object used for passing the props.caseList
  */
-export const setClosedCasesAction = ({ props, store }) => {
+export const setCasesAction = ({ props, store }) => {
+  store.set(state.openCases, props.openCaseList);
   store.set(state.closedCases, props.closedCaseList);
 };
