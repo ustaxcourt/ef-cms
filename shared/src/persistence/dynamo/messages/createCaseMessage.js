30dcc6f839c0ea1dddf587b543db4b2abf433388
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-06-04 08:57:57 -0700

    update persistence calls with new record setup

--- shared/src/persistence/dynamo/messages/createCaseMessage.js
+++ shared/src/persistence/dynamo/messages/createCaseMessage.js
@@ -11,61 +11,9 @@ const { put } = require('../../dynamodbClientService');
 exports.createCaseMessage = async ({ applicationContext, caseMessage }) => {
   await put({
     Item: {
+      gsi1pk: `message|${caseMessage.messageId}`,
       pk: `case|${caseMessage.caseId}`,
       sk: `message|${caseMessage.messageId}`,
-      ...caseMessage,
-    },
-    applicationContext,
-  });
-
-  await put({
-    Item: {
-      pk: `message|${caseMessage.messageId}`,
-      sk: `message|${caseMessage.messageId}`,
-      ...caseMessage,
-    },
-    applicationContext,
-  });
-
-  // create user inbox record
-  await put({
-    Item: {
-      gsi1pk: `message|${caseMessage.messageId}`,
-      pk: `user-inbox|${caseMessage.toUserId}`,
-      sk: `message|${caseMessage.messageId}`,
-      ...caseMessage,
-    },
-    applicationContext,
-  });
-
-  // create user outbox record
-  await put({
-    Item: {
-      gsi1pk: `message|${caseMessage.messageId}`,
-      pk: `user-outbox|${caseMessage.fromUserId}`,
-      sk: caseMessage.createdAt,
-      ...caseMessage,
-    },
-    applicationContext,
-  });
-
-  // create section inbox record
-  await put({
-    Item: {
-      gsi1pk: `message|${caseMessage.messageId}`,
-      pk: `section-inbox|${caseMessage.toSection}`,
-      sk: `message|${caseMessage.messageId}`,
-      ...caseMessage,
-    },
-    applicationContext,
-  });
-
-  // create section outbox record
-  await put({
-    Item: {
-      gsi1pk: `message|${caseMessage.messageId}`,
-      pk: `section-outbox|${caseMessage.fromSection}`,
-      sk: caseMessage.createdAt,
       ...caseMessage,
     },
     applicationContext,
