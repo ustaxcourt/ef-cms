34a28365f79c07b504ed7b28581cab462defea3d
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-06-04 11:10:47 -0700

    jsdoc, test fix

--- web-client/src/presenter/actions/chooseMessageBoxAction.js
+++ web-client/src/presenter/actions/chooseMessageBoxAction.js
@@ -4,7 +4,7 @@ import { state } from 'cerebral';
  * Used for changing the message queue (myself, section) and box (inbox, outbox, completed) from props
  *
  * @param {object} providers the providers object
- * @param {object} providers.store the cerebral store object used for setting workQueueToDisplay
+ * @param {object} providers.store the cerebral store object
  * @param {object} providers.props the cerebral props object
  */
 export const chooseMessageBoxAction = ({ props, store }) => {
--- web-client/src/presenter/actions/chooseMessageBoxAction.test.js
+++ web-client/src/presenter/actions/chooseMessageBoxAction.test.js
@@ -2,7 +2,7 @@ import { chooseMessageBoxAction } from './chooseMessageBoxAction';
 import { runAction } from 'cerebral/test';
 
 describe('chooseMessageBoxAction', () => {
-  it('sets state.caseDetail from props', async () => {
+  it('sets state.messageBoxToDisplay from props', async () => {
     const { state } = await runAction(chooseMessageBoxAction, {
       props: {
         box: 'inbox',
