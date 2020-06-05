20bc556fbb0e38cf2dd97370d6de52bc2d16856a
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-06-05 10:31:20 -0700

    4293: endpoints for other new messaging boxes

---
+++ shared/src/persistence/elasticsearch/messages/getUserOutboxMessages.js
@@ -0,0 +1,26 @@
+const { search } = require('../searchClient');
+
+exports.getUserOutboxMessages = async ({ applicationContext, userId }) => {
+  const query = {
+    body: {
+      query: {
+        bool: {
+          must: {
+            match: {
+              'fromUserId.S': userId,
+            },
+          },
+        },
+      },
+      size: 5000,
+    },
+    index: 'efcms-message',
+  };
+
+  const { results } = await search({
+    applicationContext,
+    searchParameters: query,
+  });
+
+  return results;
+};
