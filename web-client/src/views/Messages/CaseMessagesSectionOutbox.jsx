0df4a54ed1697230d972e0eb43799b13a80bd4db
Matt Hopson <matthopson@gmail.com>
2020-06-05 07:59:46 -0600

    Add case message inbox and sent tables

---
+++ web-client/src/views/Messages/CaseMessagesSectionOutbox.jsx
@@ -0,0 +1,42 @@
+import { connect } from '@cerebral/react';
+import { state } from 'cerebral';
+import React from 'react';
+
+export const CaseMessagesSectionOutbox = connect(
+  { formattedMessages: state.formattedMessages },
+  function CaseMessagesIndividualInbox({ formattedMessages }) {
+    return (
+      <>
+        <table className="usa-table work-queue subsection">
+          <thead>
+            <tr>
+              <th>Docket</th>
+              <th>Sent</th>
+              <th>Message</th>
+              <th>Case Status</th>
+              <th>To</th>
+              <th>From</th>
+              <th>Section</th>
+            </tr>
+          </thead>
+          <tbody>
+            {formattedMessages.map((message, idx) => {
+              return (
+                <tr key={idx}>
+                  <td>{message.docketNumberWithSuffix}</td>
+                  <td>{message.createdAtFormatted}</td>
+                  <td>{message.message}</td>
+                  <td>{message.caseStatus}</td>
+                  <td>{message.to}</td>
+                  <td>{message.from}</td>
+                  <td>{message.fromSection}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        {formattedMessages.length === 0 && <div>There are no messages.</div>}
+      </>
+    );
+  },
+);
