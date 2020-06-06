4b38f18b902fab5936cde18c9f47feea3e77c15e
Eric Sorenson <eric@sorenson.software>
2020-06-05 21:50:25 -0500

    addressed more comments, added updated docs

--- shared/src/business/entities/orders/Order.js
+++ shared/src/business/entities/orders/Order.js
@@ -61,14 +61,16 @@ Order.VALIDATION_ERROR_MESSAGES = {
   orderBody: 'Order body is required.',
 };
 
+Order.VALIDATION_RULES = {
+  documentTitle: joi.string().max(100).required(),
+  documentType: joi.string().required(), // TODO: add enum
+  eventCode: joi.string().optional(), // TODO: add enum
+  orderBody: joi.string().max(500).required(),
+};
+
 joiValidationDecorator(
   Order,
-  joi.object().keys({
-    documentTitle: joi.string().required(),
-    documentType: joi.string().required(),
-    eventCode: joi.string().optional(),
-    orderBody: joi.string().required(),
-  }),
+  joi.object().keys(Order.VALIDATION_RULES),
   Order.VALIDATION_ERROR_MESSAGES,
 );
 
