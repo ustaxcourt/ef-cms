b52baba71741849625bcc91fe53e732721f93f79
Cody Seibert <cseibert113@gmail.com>
2020-06-06 18:14:09 -0400

    deploy stage

--- web-api/terraform/template/api/index.js
+++ web-api/terraform/template/api/index.js
@@ -1,7 +1,18 @@
-const app = require('../../../src/app');
 const awsServerlessExpress = require('aws-serverless-express');
+const { app } = require('../../../src/app');
 const server = awsServerlessExpress.createServer(app);
 
 exports.handler = (event, context) => {
+  console.log(event);
   awsServerlessExpress.proxy(server, event, context);
 };
+
+// exports.handler = async (event, context) => {
+//   return {
+//     body: JSON.stringify({
+//       message: 'hello',
+//     }),
+//     headers: { 'Content-Type': 'application/json' },
+//     statusCode: 200,
+//   };
+// };
