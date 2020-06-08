27f40e1753a307b56cc628d93b9460f59ad0e2d5
Mrinalini <mrinalinisinha05@gmail.com>
2020-06-08 10:27:42 -0700

    added unit tests around processUserAssociatedCases

--- shared/src/business/useCases/getOpenConsolidatedCasesInteractor.test.js
+++ shared/src/business/useCases/getOpenConsolidatedCasesInteractor.test.js
@@ -1,5 +1,6 @@
 const {
   getOpenConsolidatedCasesInteractor,
+  processUserAssociatedCases,
 } = require('./getOpenConsolidatedCasesInteractor');
 const { applicationContext } = require('../test/createTestApplicationContext');
 const { MOCK_CASE } = require('../../test/mockCase');
@@ -79,5 +80,70 @@ describe('getOpenConsolidatedCasesInteractor', () => {
         docketNumberWithSuffix: MOCK_CASE.docketNumberWithSuffix,
       },
     ]);
+  });
+
+  describe('processUserAssociatedCases', () => {
+    it('should set isRequestingUserAssociated to true for each case', async () => {
+      processUserAssociatedCases(mockFoundCasesList);
+
+      expect(MOCK_CASE.isRequestingUserAssociated).toBe(true);
+    });
+
+    it('should add a case to casesAssociatedWithUserOrLeadCaseMap when it is a lead case', async () => {
+      mockFoundCasesList = [{ ...MOCK_CASE, isLeadCase: true }];
+
+      const result = processUserAssociatedCases(mockFoundCasesList);
+
+      expect(
+        result.casesAssociatedWithUserOrLeadCaseMap[MOCK_CASE.caseId],
+      ).toEqual({ ...MOCK_CASE, isLeadCase: true });
+    });
+
+    it('should add a case to casesAssociatedWithUserOrLeadCaseMap when it does not have a leadCaseId', async () => {
+      mockFoundCasesList = [MOCK_CASE];
+      const result = processUserAssociatedCases(mockFoundCasesList);
+
+      expect(
+        result.casesAssociatedWithUserOrLeadCaseMap[MOCK_CASE.caseId],
+      ).toEqual(MOCK_CASE);
+    });
+
+    it("should add a case's caseId to userAssociatedCaseIdsMap", async () => {
+      mockFoundCasesList = [MOCK_CASE];
+      const result = processUserAssociatedCases(mockFoundCasesList);
+
+      expect(result.userAssociatedCaseIdsMap[MOCK_CASE.caseId]).toEqual(true);
+    });
+
+    it('should add a case to leadCaseIdsAssociatedWithUser if it has a leadCaseId and is not associated with the user', async () => {
+      let mockCaseWithLeadCaseId = {
+        ...MOCK_CASE,
+        leadCaseId: applicationContext.getUniqueId(),
+      };
+      mockFoundCasesList = [mockCaseWithLeadCaseId];
+
+      const result = processUserAssociatedCases(mockFoundCasesList);
+
+      expect(
+        result.leadCaseIdsAssociatedWithUser.includes(
+          mockCaseWithLeadCaseId.leadCaseId.toString(),
+        ),
+      ).toBe(true);
+    });
+
+    it('should populate casesAssociatedWithUserOrLeadCaseMap, leadCaseIdsAssociatedWithUser and userAssociatedCaseIdsMap', async () => {
+      let mockCaseWithLeadCaseId = {
+        ...MOCK_CASE,
+        isLeadCase: true,
+        leadCaseId: applicationContext.getUniqueId(),
+      };
+      mockFoundCasesList = [mockCaseWithLeadCaseId, MOCK_CASE];
+
+      const result = processUserAssociatedCases(mockFoundCasesList);
+
+      expect(result.casesAssociatedWithUserOrLeadCaseMap).not.toBe({});
+      expect(result.leadCaseIdsAssociatedWithUser.length).toBe(1);
+      expect(result.userAssociatedCaseIdsMap).not.toBe({});
+    });
   });
 });
