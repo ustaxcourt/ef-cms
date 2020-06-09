fbb49a713038d7a4e4c311e8c53a340294ba5581
Mrinalini <mrinalinisinha05@gmail.com>
2020-06-09 14:58:26 -0700

    added pa11y tests for open/closed tabs

--- web-client/pa11y/pa11y-private-practitioner.js
+++ web-client/pa11y/pa11y-private-practitioner.js
@@ -5,4 +5,13 @@ module.exports = [
   'http://localhost:1234/mock-login?token=privatePractitioner&path=/file-a-petition/step-1',
   'http://localhost:1234/mock-login?token=privatePractitioner&path=/case-detail/102-19/request-access',
   'http://localhost:1234/mock-login?token=privatePractitioner&path=/search/no-matches',
+  {
+    actions: [
+      'wait for #tab-open to be visible',
+      'wait for #tab-closed to be visible',
+    ],
+    notes:
+      'a private practitioner can see two tabs displaying open and closed cases',
+    url: 'http://localhost:1234/mock-login?token=privatePractitioner&path=/',
+  },
 ];
