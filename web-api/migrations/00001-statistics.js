8aebc5209b264b26cecaa33e0265e62e91329d40
Joseph Sutton <jsutton@flexion.us>
2020-06-09 11:56:59 -0500

    split new migration logic into its own, separate migration

--- web-api/migrations/00001-statistics.js
+++ web-api/migrations/00001-statistics.js
@@ -5,64 +5,45 @@ const { Statistic } = require('../../shared/src/business/entities/Statistic');
 const applicationContext = createApplicationContext({});
 
 const mutateRecord = item => {
-  if (isCaseRecord(item)) {
+  if (
+    isCaseRecord(item) &&
+    item.caseType === Case.CASE_TYPES_MAP.deficiency &&
+    item.hasVerifiedIrsNotice === true
+  ) {
     let { statistics } = item;
-
-    if (
-      item.caseType === Case.CASE_TYPES_MAP.deficiency &&
-      item.hasVerifiedIrsNotice === true
-    ) {
-      if (!statistics || statistics.length === 0) {
-        const defaultStatistic = new Statistic(
-          {
-            irsDeficiencyAmount: 1,
-            irsTotalPenalties: 1,
-            year: '2012',
-            yearOrPeriod: 'Year',
-          },
-          { applicationContext },
-        );
-        statistics = [defaultStatistic];
-      } else {
-        statistics.forEach(statistic => {
-          if (statistic.deficiencyAmount) {
-            statistic.irsDeficiencyAmount = statistic.deficiencyAmount;
-          }
-          if (statistic.totalPenalties) {
-            statistic.irsTotalPenalties = statistic.totalPenalties;
-          }
-        });
-      }
-
-      const caseEntity = new Case(
+    if (!statistics || statistics.length === 0) {
+      const defaultStatistic = new Statistic(
         {
-          ...item,
-          statistics,
+          irsDeficiencyAmount: 1,
+          irsTotalPenalties: 1,
+          year: '2012',
+          yearOrPeriod: 'Year',
         },
         { applicationContext },
-      )
-        .validate()
-        .toRawObject();
-
-      return { ...item, ...caseEntity };
-    } else if (
-      (item.caseType !== Case.CASE_TYPES_MAP.deficiency ||
-        item.hasVerifiedIrsNotice !== true) &&
-      statistics &&
-      statistics.length
-    ) {
-      const caseEntity = new Case(
-        {
-          ...item,
-          statistics: [],
-        },
-        { applicationContext },
-      )
-        .validate()
-        .toRawObject();
-
-      return { ...item, ...caseEntity };
+      );
+      statistics = [defaultStatistic];
+    } else {
+      statistics.map(statistic => {
+        if (statistic.deficiencyAmount) {
+          statistic.irsDeficiencyAmount = statistic.deficiencyAmount;
+        }
+        if (statistic.totalPenalties) {
+          statistic.irsTotalPenalties = statistic.totalPenalties;
+        }
+      });
     }
+
+    const caseEntity = new Case(
+      {
+        ...item,
+        statistics,
+      },
+      { applicationContext },
+    )
+      .validate()
+      .toRawObject();
+
+    return { ...item, ...caseEntity };
   }
 };
 
