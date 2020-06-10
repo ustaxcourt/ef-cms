cdc257981050794bf17a8868f8a532c0046ef8c7
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-06-10 09:36:35 -0700

    hook up authorizer to lambda

--- web-api/terraform/template/api.tf
+++ web-api/terraform/template/api.tf
@@ -79,7 +79,8 @@ resource "aws_api_gateway_method" "api_method" {
   rest_api_id = "${aws_api_gateway_rest_api.gateway_for_api.id}"
   resource_id = "${aws_api_gateway_resource.api_resource.id}"
   http_method = "ANY"
-  authorization = "NONE"
+  authorization = "CUSTOM"
+  authorizer_id = "${aws_api_gateway_authorizer.custom_authorizer.id}"
 }
 
 resource "aws_api_gateway_authorizer" "custom_authorizer" {
