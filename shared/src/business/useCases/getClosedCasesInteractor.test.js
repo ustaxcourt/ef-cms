77edfb3552fd2a1c7e00fcfd1928a20ac02009ae
Mrinalini <mrinalinisinha05@gmail.com>
2020-06-10 13:53:33 -0700

    fixed code broken by automatic to merge

--- shared/src/business/useCases/getClosedCasesInteractor.test.js
+++ shared/src/business/useCases/getClosedCasesInteractor.test.js
@@ -1,5 +1,5 @@
 const { applicationContext } = require('../test/createTestApplicationContext');
-const { CASE_STATUS_TYPES } = require('../entities/cases/CaseConstants');
+const { CASE_STATUS_TYPES } = require('../entities/EntityConstants');
 const { getClosedCasesInteractor } = require('./getClosedCasesInteractor');
 const { MOCK_CASE } = require('../../test/mockCase');
 const { MOCK_USERS } = require('../../test/mockUsers');
