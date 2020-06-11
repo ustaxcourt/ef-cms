712ba9925b3e69469cc3b9b05bb9d97f6056e564
mrinsin <mrinalinisinha05@gmail.com>
2020-06-11 14:49:29 -0700

    displaying message for when there are no opinions

--- web-client/src/views/Public/TodaysOpinions.jsx
+++ web-client/src/views/Public/TodaysOpinions.jsx
@@ -15,37 +15,42 @@ export const TodaysOpinions = connect(
         <section className="usa-section grid-container todays-opinions">
           <h1>{todaysOpinionsHelper.formattedCurrentDate}</h1>
 
-          <table
-            aria-label="todays opinions"
-            className="usa-table todays-opinions responsive-table row-border-only"
-          >
-            <thead>
-              <tr>
-                <th aria-hidden="true" />
-                <th aria-hidden="true" />
-                <th>Docket Number</th>
-                <th>Case Title</th>
-                <th>Opinion Type</th>
-                <th>Pages</th>
-                <th>Date</th>
-                <th>Judge</th>
-              </tr>
-            </thead>
-            <tbody>
-              {todaysOpinionsHelper.formattedOpinions.map((opinion, idx) => (
-                <tr key={idx}>
-                  <td className="center-column">{idx + 1}</td>
-                  <td aria-hidden="true"></td>
-                  <td>{opinion.docketNumberWithSuffix}</td>
-                  <td>{opinion.caseCaption}</td>
-                  <td>{opinion.formattedDocumentType}</td>
-                  <td>{opinion.numberOfPages}</td>
-                  <td>{opinion.formattedFilingDate}</td>
-                  <td>{opinion.formattedJudgeName}</td>
+          {(todaysOpinionsHelper.formattedOpinions.length === 0) &
+          <p>There are no opinions today.</p>}
+
+          {todaysOpinionsHelper.formattedOpinions.length > 0 && (
+            <table
+              aria-label="todays opinions"
+              className="usa-table todays-opinions responsive-table row-border-only"
+            >
+              <thead>
+                <tr>
+                  <th aria-hidden="true" />
+                  <th aria-hidden="true" />
+                  <th>Docket Number</th>
+                  <th>Case Title</th>
+                  <th>Opinion Type</th>
+                  <th>Pages</th>
+                  <th>Date</th>
+                  <th>Judge</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {todaysOpinionsHelper.formattedOpinions.map((opinion, idx) => (
+                  <tr key={idx}>
+                    <td className="center-column">{idx + 1}</td>
+                    <td aria-hidden="true"></td>
+                    <td>{opinion.docketNumberWithSuffix}</td>
+                    <td>{opinion.caseCaption}</td>
+                    <td>{opinion.formattedDocumentType}</td>
+                    <td>{opinion.numberOfPages}</td>
+                    <td>{opinion.formattedFilingDate}</td>
+                    <td>{opinion.formattedJudgeName}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
         </section>
       </>
     );
