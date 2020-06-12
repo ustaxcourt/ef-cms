1b20bf80d04f19254dfc5cbcc3ff047de817e21d
Matt Hopson <matthopson@gmail.com>
2020-06-12 12:33:50 -0600

    Get case documents for case message dropdown

--- web-client/src/presenter/computeds/createCaseMessageModalHelper.test.js
+++ web-client/src/presenter/computeds/createCaseMessageModalHelper.test.js
@@ -2,9 +2,84 @@ import { createCaseMessageModalHelper } from './createCaseMessageModalHelper';
 import { runCompute } from 'cerebral/test';
 
 describe('createCaseMessageModalHelper', () => {
+  const formattedCaseDetail = {
+    docketRecordWithDocument: [],
+    draftDocuments: [],
+  };
+
+  it('returns documents on the docket record', () => {
+    const result = runCompute(createCaseMessageModalHelper, {
+      state: {
+        formattedCaseDetail: {
+          docketRecordWithDocument: [
+            { document: { documentId: '123' } },
+            { document: { documentId: '234' } },
+          ],
+          draftDocuments: [],
+        },
+        modal: {
+          form: {},
+        },
+        screenMetadata: {
+          showAddDocumentForm: true,
+        },
+      },
+    });
+
+    expect(result.documents).toEqual([
+      { documentId: '123' },
+      { documentId: '234' },
+    ]);
+  });
+
+  it('returns only documents from the docket record that have a document object', () => {
+    const result = runCompute(createCaseMessageModalHelper, {
+      state: {
+        formattedCaseDetail: {
+          docketRecordWithDocument: [
+            { document: { documentId: '123' } },
+            { document: undefined },
+          ],
+          draftDocuments: [],
+        },
+        modal: {
+          form: {},
+        },
+        screenMetadata: {
+          showAddDocumentForm: true,
+        },
+      },
+    });
+
+    expect(result.documents).toEqual([{ documentId: '123' }]);
+  });
+
+  it('returns draftDocuments from formattedCaseDetail', () => {
+    const result = runCompute(createCaseMessageModalHelper, {
+      state: {
+        formattedCaseDetail: {
+          docketRecordWithDocument: [],
+          draftDocuments: [{ documentId: '123' }, { documentId: '234' }],
+        },
+        modal: {
+          form: {},
+        },
+        screenMetadata: {
+          showAddDocumentForm: true,
+        },
+      },
+    });
+
+    expect(result.draftDocuments).toEqual([
+      { documentId: '123' },
+      { documentId: '234' },
+    ]);
+  });
+
   it('returns showAddDocumentForm true when the current attachment count is zero', () => {
     const result = runCompute(createCaseMessageModalHelper, {
       state: {
+        formattedCaseDetail,
         modal: {
           form: {},
         },
@@ -20,6 +95,7 @@ describe('createCaseMessageModalHelper', () => {
   it('returns showAddDocumentForm true when screenMetadata.showAddDocumentForm is true and the maximum number of attachments has not been met', () => {
     const result = runCompute(createCaseMessageModalHelper, {
       state: {
+        formattedCaseDetail,
         modal: {
           form: {
             attachments: [{}, {}, {}, {}], // 4/5 documents attached
@@ -37,6 +113,7 @@ describe('createCaseMessageModalHelper', () => {
   it('returns showAddDocumentForm false when screenMetadata.showAddDocumentForm is false and the maximum number of attachments has not been met', () => {
     const result = runCompute(createCaseMessageModalHelper, {
       state: {
+        formattedCaseDetail,
         modal: {
           form: {
             attachments: [{}, {}, {}, {}], // 4/5 documents attached
@@ -54,6 +131,7 @@ describe('createCaseMessageModalHelper', () => {
   it('returns showAddDocumentForm false when maximum number of attachments have been reached', () => {
     const result = runCompute(createCaseMessageModalHelper, {
       state: {
+        formattedCaseDetail,
         modal: {
           form: {
             attachments: [{}, {}, {}, {}, {}], // 5/5 documents attached
@@ -71,6 +149,7 @@ describe('createCaseMessageModalHelper', () => {
   it('returns showAddMoreDocumentsButton true when showAddDocumentForm is false and the current attachment count is greater than zero but less than the maximum', () => {
     const result = runCompute(createCaseMessageModalHelper, {
       state: {
+        formattedCaseDetail,
         modal: {
           form: {
             attachments: [{}, {}, {}, {}], // 4/5 documents attached
@@ -88,6 +167,7 @@ describe('createCaseMessageModalHelper', () => {
   it('returns showAddMoreDocumentsButton false when maximum number of attachments have been reached', () => {
     const result = runCompute(createCaseMessageModalHelper, {
       state: {
+        formattedCaseDetail,
         modal: {
           form: {
             attachments: [{}, {}, {}, {}, {}], // 5/5 documents attached
