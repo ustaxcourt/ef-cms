43ddacbcf907e68248569e1bbd1f605f11cfbc90
Rachel Schneiderman <rachelschneiderman@Rachels-MacBook-Pro.local>
2020-06-12 10:17:56 -0700

    Renamed action. Added tests.

--- web-client/src/presenter/actions/setRequestAccessWizardStepActionGenerator.js
+++ web-client/src/presenter/actions/setRequestAccessWizardStepActionGenerator.js
@@ -6,7 +6,7 @@ import { state } from 'cerebral';
  * @param {string} wizardStep the wizardStep
  * @returns {Function} a function that sets the wizardStep
  */
-export const setRequestAccessWizardStepAction = wizardStep => {
+export const setRequestAccessWizardStepActionGenerator = wizardStep => {
   return ({ store }) => {
     store.set(state.wizardStep, wizardStep);
   };
