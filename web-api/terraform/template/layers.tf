733f4dd510e15f22199a6982bc5dd67eb0e29541
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-06-12 13:38:05 -0700

    use s3 bucket to deploy puppeteer layer

--- web-api/terraform/template/layers.tf
+++ web-api/terraform/template/layers.tf
@@ -1,10 +1,3 @@
-resource "aws_lambda_layer_version" "puppeteer_layer" {
-  filename   = "../../runtimes/puppeteer/puppeteer_lambda_layer.zip"
-  layer_name = "puppeteer-${var.environment}"
-
-  compatible_runtimes = ["nodejs12.x"]
-}
-
 resource "aws_s3_bucket" "layer_bucket" {
   bucket = "${var.dns_domain}.efcms.${var.environment}.us-east-1.layers"
   acl = "private"
@@ -26,6 +19,19 @@ resource "aws_lambda_layer_version" "clamav_layer" {
   s3_bucket = "${aws_s3_bucket.layer_bucket.id}"
   s3_key = "${aws_s3_bucket_object.clamav_layer_object.key}"
   layer_name = "clamav-${var.environment}"
+
+  compatible_runtimes = ["nodejs12.x"]
+}
+
+resource "aws_s3_bucket_object" "puppeteer_layer_object" {
+  bucket = "${aws_s3_bucket.layer_bucket.id}"
+  key    = "${var.environment}_puppeteer_lambda_layer.zip"
+  source = "../../runtimes/puppeteer/puppeteer_lambda_layer.zip"
+}
+
+resource "aws_lambda_layer_version" "puppeteer_layer" {
+  filename   = "../../runtimes/puppeteer/puppeteer_lambda_layer.zip"
+  layer_name = "puppeteer-${var.environment}"
 
   compatible_runtimes = ["nodejs12.x"]
 }
