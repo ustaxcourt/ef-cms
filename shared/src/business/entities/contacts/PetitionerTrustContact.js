fd30e95cd61a0d48e52f4b3fbad5923c455cc25e
Eric Sorenson <eric@sorenson.software>
2020-06-15 17:41:12 -0500

    fix tests

--- shared/src/business/entities/contacts/PetitionerTrustContact.js
+++ shared/src/business/entities/contacts/PetitionerTrustContact.js
@@ -9,6 +9,6 @@ exports.getPetitionerTrustContact = ContactFactory.createContactFactory({
     secondaryName: 'Enter name of trustee',
   },
   additionalValidation: {
-    secondaryName: joi.string().max(100).required(),
+    secondaryName: joi.string().max(500).required(),
   },
 });
