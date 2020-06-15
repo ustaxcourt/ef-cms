e32b2d12f39698c39b8984fbbc3d15ff1a4ac40a
Matt Hopson <matthopson@gmail.com>
2020-06-15 13:41:07 -0600

    Set default attachment for viewer

--- web-client/src/presenter/sequences/gotoMessageDetailSequence.js
+++ web-client/src/presenter/sequences/gotoMessageDetailSequence.js
@@ -1,11 +1,14 @@
 import { clearErrorAlertsAction } from '../actions/clearErrorAlertsAction';
 import { closeMobileMenuAction } from '../actions/closeMobileMenuAction';
 import { getCaseAction } from '../actions/getCaseAction';
+import { getDefaultAttachmentToDisplayAction } from '../actions/getDefaultAttachmentToDisplayAction';
 import { getMessageAction } from '../actions/getMessageAction';
 import { isLoggedInAction } from '../actions/isLoggedInAction';
 import { redirectToCognitoAction } from '../actions/redirectToCognitoAction';
+import { setAttachmentDocumentToDisplayAction } from '../actions/setAttachmentDocumentToDisplayAction';
 import { setCaseAction } from '../actions/setCaseAction';
 import { setCurrentPageAction } from '../actions/setCurrentPageAction';
+
 import { setMessageAction } from '../actions/setMessageAction';
 import { showProgressSequenceDecorator } from '../utilities/sequenceHelpers';
 
@@ -17,6 +20,8 @@ const gotoMessageDetail = showProgressSequenceDecorator([
   setCaseAction,
   getMessageAction,
   setMessageAction,
+  getDefaultAttachmentToDisplayAction,
+  setAttachmentDocumentToDisplayAction,
   setCurrentPageAction('MessageDetail'),
 ]);
 
