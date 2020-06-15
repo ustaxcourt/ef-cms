0e364b0d7292341a0f8681bf0e9a9f23b9555c62
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-06-15 08:46:13 -0700

    fix public api url

--- web-api/src/app-public.js
+++ web-api/src/app-public.js
@@ -55,7 +55,7 @@ app.get(
   lambdaWrapper(getCaseForPublicDocketSearchLambda),
 );
 app.post(
-  '/public-api/:caseId/generate-docket-record',
+  '/public-api/cases/:caseId/generate-docket-record',
   lambdaWrapper(generatePublicDocketRecordPdfLambda),
 );
 app.get(
