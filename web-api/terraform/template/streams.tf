8e9270ecfcc8b87bb002f0b06dac895e489b9643
Cody Seibert <cseibert@flexion.us>
2020-06-15 20:40:01 -0400

    renaming lambda

--- web-api/terraform/template/streams.tf
+++ web-api/terraform/template/streams.tf
@@ -4,9 +4,9 @@ data "archive_file" "zip_streams" {
   source_file = "${path.module}/streams/dist/index.js"
 }
 
-resource "aws_lambda_function" "zip_lambda" {
+resource "aws_lambda_function" "zip_streams" {
   filename      = "${data.archive_file.zip_streams.output_path}"
-  function_name = "api_${var.environment}"
+  function_name = "streams_${var.environment}"
   role          = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/lambda_role_${var.environment}"
   handler       = "index.handler"
   source_code_hash = "${data.archive_file.zip_streams.output_base64sha256}"
@@ -41,6 +41,6 @@ resource "aws_lambda_function" "zip_lambda" {
 
 resource "aws_lambda_event_source_mapping" "streams_mapping" {
   event_source_arn  = "${aws_dynamodb_table.efcms-east.stream_arn}"
-  function_name     = "${aws_lambda_function.zip_lambda.arn}"
+  function_name     = "${aws_lambda_function.zip_streams.arn}"
   starting_position = "TRIM_HORIZON"
 }
