e27d67fc3731e0fa2b099efa15751fdfe6e1e23a
Rachel Schneiderman <rachelschneiderman@Rachels-MacBook-Pro.local>
2020-06-16 11:29:17 -0700

    Adding smoketest for qc-ing petition as petitions clerk.

--- cypress-smoketests/integration/petitions-clerk.spec.js
+++ cypress-smoketests/integration/petitions-clerk.spec.js
@@ -17,16 +17,30 @@ describe('Petitions clerk', () => {
     token = results.AuthenticationResult.IdToken;
   });
 
-  it('views the section inbox', () => {
+  it('should be able to view the section inbox', () => {
     cy.visit(`/log-in?token=${token}`);
     cy.get('.button-switch-box').should('exist');
   });
 
-  it('should verify advanced search works', () => {
-    cy.visit(`/log-in?token=${token}&path=/search`);
-    cy.get('#advanced-search-button').should('exist');
-    cy.get('#petitioner-name').type('THISNAMESHOULDNEVEREXIST');
-    cy.get('#advanced-search-button').click();
-    cy.get('#no-search-results').should('exist');
+  // Not necessary right now, this test exists as part of the deploy process already
+  // it('should be able to create a case', () => {});
+  // it('should be able to save newly created case for later', () => {});
+  it('should be able to QC a petition', () => {
+    cy.get('a[href*="document-qc/my/inbox"]').click();
+    cy.get('button:contains("Switch to")').click();
+    cy.get('a[href*="petition-qc"]').first().click();
+    cy.get('button#submit-case').click();
+    cy.get('button:contains("Save for Later")').click();
+    cy.get('div.usa-alert--success').should('exist');
   });
+
+  it('should be able to add statistics ', () => {});
+
+  // it('should verify advanced search works', () => {
+  //   cy.visit(`/log-in?token=${token}&path=/search`);
+  //   cy.get('#advanced-search-button').should('exist');
+  //   cy.get('#petitioner-name').type('THISNAMESHOULDNEVEREXIST');
+  //   cy.get('#advanced-search-button').click();
+  //   cy.get('#no-search-results').should('exist');
+  // });
 });
