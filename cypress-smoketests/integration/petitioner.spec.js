6ff4eec915c1c2ab612bb76d241735a74e7e850e
Rachel Schneiderman <rachelschneiderman@Rachels-MacBook-Pro.local>
2020-06-17 10:49:22 -0700

    [skip ci] Petitioner create case smoke test done. Refactored a bit.

--- cypress-smoketests/integration/petitioner.spec.js
+++ cypress-smoketests/integration/petitioner.spec.js
@@ -1,10 +1,18 @@
-const AWS = require('aws-sdk');
-const { getUserToken } = require('../support/pages/login');
-
-AWS.config = new AWS.Config();
-AWS.config.accessKeyId = Cypress.env('AWS_ACCESS_KEY_ID');
-AWS.config.secretAccessKey = Cypress.env('AWS_SECRET_ACCESS_KEY');
-AWS.config.region = 'us-east-1';
+const {
+  completeWizardStep1,
+  completeWizardStep2,
+  completeWizardStep3,
+  completeWizardStep4,
+  goToDashboard,
+  goToStartCreatePetition,
+  goToWizardStep1,
+  goToWizardStep2,
+  goToWizardStep3,
+  goToWizardStep4,
+  goToWizardStep5,
+  submitPetition,
+} = require('../support/pages/create-electronic-petition');
+const { getUserToken, login } = require('../support/pages/login');
 
 let token = null;
 
@@ -18,50 +26,29 @@ describe('Petitioner', () => {
   });
 
   it('should be able to login', () => {
-    cy.visit(`/log-in?token=${token}`);
+    login(token);
   });
 
   it('should be able to create a case', () => {
-    cy.get('a#file-a-petition').click();
+    goToStartCreatePetition();
 
-    cy.get('a[href*="file-a-petition/step-1"]').click();
+    goToWizardStep1();
+    completeWizardStep1();
+    goToWizardStep2();
+    completeWizardStep2();
+    goToWizardStep3();
+    completeWizardStep3();
+    goToWizardStep4();
+    completeWizardStep4();
+    goToWizardStep5();
 
-    cy.upload_file('w3-dummy.pdf', 'input#stin-file');
-    cy.get('button#submit-case').click();
+    submitPetition();
 
-    cy.upload_file('w3-dummy.pdf', 'input#petition-file');
-    cy.get('#irs-notice-radios').scrollIntoView();
-    cy.get('#irs-notice-radios label').first().click();
-    cy.get('#case-type').scrollIntoView().select('Notice of Deficiency');
-    cy.get('button#submit-case').click();
-
-    cy.get('label#filing-type-0').scrollIntoView().click();
-    cy.get('input#name').scrollIntoView().type('John');
-    cy.get('input[name="contactPrimary.address1"]')
-      .scrollIntoView()
-      .type('111 South West St.');
-    cy.get('input[name="contactPrimary.city"]')
-      .scrollIntoView()
-      .type('Orlando');
-    cy.get('select[name="contactPrimary.state"]').scrollIntoView().select('AL');
-    cy.get('input[name="contactPrimary.postalCode"]')
-      .scrollIntoView()
-      .type('12345');
-    cy.get('input#phone').scrollIntoView().type('1111111111');
-    cy.get('button#submit-case').click();
-
-    cy.get('label#procedure-type-0').scrollIntoView().click();
-    cy.get('#preferred-trial-city').scrollIntoView().select('Mobile, Alabama');
-    cy.get('button#submit-case').click();
-
-    cy.get('button#submit-case').scrollIntoView().click();
-
-    cy.get('a#button-back-to-dashboard').click();
+    goToDashboard();
   });
 
   it('should be able to file a document', () => {
     cy.get('a[href*="case-detail/"]').first().click();
+    // TODO - petition must be QC-d before document can be filed
   });
-
-  it('should be able to file a document', () => {});
 });
