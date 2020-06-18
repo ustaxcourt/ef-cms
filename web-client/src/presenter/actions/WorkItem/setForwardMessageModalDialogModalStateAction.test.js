1387acc064a8f510d9548c005134e04361df20c5
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-06-18 15:15:55 -0700

    4609: message detail thread view

--- web-client/src/presenter/actions/WorkItem/setForwardMessageModalDialogModalStateAction.test.js
+++ web-client/src/presenter/actions/WorkItem/setForwardMessageModalDialogModalStateAction.test.js
@@ -10,24 +10,22 @@ describe('setForwardMessageModalDialogModalStateAction', () => {
         modules: {
           presenter,
         },
-        props: {},
-        state: {
-          messageDetail: [
-            {
-              attachments: [
-                {
-                  documentId: 'a5273185-f694-4d9c-bc90-71eddc5e5937',
-                  documentTitle: 'Petition',
-                },
-              ],
-              from: 'test user 1',
-              fromSection: 'petitions',
-              fromUserId: '589002b0-dacd-4e84-874a-52d9898623c3',
-              parentMessageId: '530f9b43-4934-4b2f-9aa4-50dcbe8064fa',
-              subject: 'the subject',
-            },
-          ],
+        props: {
+          mostRecentMessage: {
+            attachments: [
+              {
+                documentId: 'a5273185-f694-4d9c-bc90-71eddc5e5937',
+                documentTitle: 'Petition',
+              },
+            ],
+            from: 'test user 1',
+            fromSection: 'petitions',
+            fromUserId: '589002b0-dacd-4e84-874a-52d9898623c3',
+            parentMessageId: '530f9b43-4934-4b2f-9aa4-50dcbe8064fa',
+            subject: 'the subject',
+          },
         },
+        state: {},
       },
     );
 
