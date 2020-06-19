614102897f9676565625e655a9543a5fb18f8bef
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-06-19 13:55:25 -0700

    4609: completed message detail page updates

--- web-client/src/presenter/computeds/formattedMessageDetail.js
+++ web-client/src/presenter/computeds/formattedMessageDetail.js
@@ -5,6 +5,10 @@ import { state } from 'cerebral';
 const formatMessage = (message, applicationContext) => {
   return {
     ...message,
+    completedAtFormatted: formatDateIfToday(
+      message.completedAt,
+      applicationContext,
+    ),
     createdAtFormatted: formatDateIfToday(
       message.createdAt,
       applicationContext,
@@ -22,13 +26,23 @@ export const formattedMessageDetail = (get, applicationContext) => {
     'desc',
   );
 
+  const { isCompleted } = formattedMessages[0];
+
+  const currentMessage = formattedMessages[0];
+
+  if (isCompleted) {
+    formattedMessages.unshift(currentMessage);
+  }
+
   const hasOlderMessages = formattedMessages.length > 1;
 
   return {
     attachments: formattedMessages[0].attachments,
     currentMessage: formattedMessages[0],
     hasOlderMessages,
+    isCompleted,
     olderMessages: formattedMessages.slice(1),
+    showActionButtons: !isCompleted,
     showOlderMessages: hasOlderMessages && isExpanded,
   };
 };
--- web-client/src/views/Messages/CaseMessagesIndividualCompleted.jsx
+++ web-client/src/views/Messages/CaseMessagesIndividualCompleted.jsx
@@ -18,6 +18,7 @@ export const CaseMessagesIndividualCompleted = connect(
               <th className="small">Completed</th>
               <th>Last Message</th>
               <th>Comment</th>
+              <th>Case Title</th>
             </tr>
           </thead>
           {formattedMessages.map((message, idx) => {
@@ -50,6 +51,7 @@ export const CaseMessagesIndividualCompleted = connect(
                   <td className="message-queue-row">
                     {message.completedMessage}
                   </td>
+                  <td className="message-queue-row">{message.caseTitle}</td>
                 </tr>
               </tbody>
             );
