c8019b79750d48532c70efc0d07ad3c5ae817f08
Joseph Sutton <jsutton@flexion.us>
2020-06-22 12:38:07 -0500

    5430: pull constants into component via state instead of passing as prop

--- web-client/src/views/CaseDetail/OtherPetitionerDisplay.jsx
+++ web-client/src/views/CaseDetail/OtherPetitionerDisplay.jsx
@@ -1,36 +1,47 @@
+import { connect } from '@cerebral/react';
+import { state } from 'cerebral';
 import React from 'react';
 
-export const OtherPetitionerDisplay = (petitioner, constants) => {
-  return (
-    <>
-      <p className="margin-top-0 address-name">
-        {petitioner.additionalName || petitioner.name}
-        {petitioner.inCareOf && (
-          <span>
-            <br />
-            c/o {petitioner.inCareOf}
+const OtherPetitionerDisplay = connect(
+  {
+    constants: state.constants,
+  },
+  function OtherPetitionerDisplay(petitioner, constants) {
+    return (
+      <>
+        <p className="margin-top-0 address-name">
+          {petitioner.additionalName || petitioner.name}
+          {petitioner.inCareOf && (
+            <span>
+              <br />
+              c/o {petitioner.inCareOf}
+            </span>
+          )}
+        </p>
+        <p>
+          <span className="address-line">{petitioner.address1}</span>
+          {petitioner.address2 && (
+            <span className="address-line">{petitioner.address2}</span>
+          )}
+          {petitioner.address3 && (
+            <span className="address-line">{petitioner.address3}</span>
+          )}
+          <span className="address-line">
+            {petitioner.city && `${petitioner.city}, `}
+            {petitioner.state} {petitioner.postalCode}
           </span>
-        )}
-      </p>
-      <p>
-        <span className="address-line">{petitioner.address1}</span>
-        {petitioner.address2 && (
-          <span className="address-line">{petitioner.address2}</span>
-        )}
-        {petitioner.address3 && (
-          <span className="address-line">{petitioner.address3}</span>
-        )}
-        <span className="address-line">
-          {petitioner.city && `${petitioner.city}, `}
-          {petitioner.state} {petitioner.postalCode}
-        </span>
-        {petitioner.countryType === constants.COUNTRY_TYPES.INTERNATIONAL && (
-          <span className="address-line">{petitioner.country}</span>
-        )}
-        {petitioner.phone && (
-          <span className="address-line margin-top-1">{petitioner.phone}</span>
-        )}
-      </p>
-    </>
-  );
-};
+          {petitioner.countryType === constants.COUNTRY_TYPES.INTERNATIONAL && (
+            <span className="address-line">{petitioner.country}</span>
+          )}
+          {petitioner.phone && (
+            <span className="address-line margin-top-1">
+              {petitioner.phone}
+            </span>
+          )}
+        </p>
+      </>
+    );
+  },
+);
+
+export { OtherPetitionerDisplay };
