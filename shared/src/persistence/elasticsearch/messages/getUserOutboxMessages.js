6fe628d66370b06610169052fe477a04c13db484
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-06-22 10:00:26 -0700

    4609: filter out messages older than 7 days from Sent and Completed boxes

--- shared/src/persistence/elasticsearch/messages/getUserOutboxMessages.js
+++ shared/src/persistence/elasticsearch/messages/getUserOutboxMessages.js
@@ -1,15 +1,34 @@
+const {
+  prepareDateFromString,
+} = require('../../../business/utilities/DateHandler');
 const { search } = require('../searchClient');
 
 exports.getUserOutboxMessages = async ({ applicationContext, userId }) => {
+  const filterDate = prepareDateFromString()
+    .startOf('day')
+    .subtract(7, 'd')
+    .utc()
+    .format();
+
   const query = {
     body: {
       query: {
         bool: {
-          must: {
-            match: {
-              'fromUserId.S': { operator: 'and', query: userId },
+          must: [
+            {
+              match: {
+                'fromUserId.S': { operator: 'and', query: userId },
+              },
             },
-          },
+            {
+              range: {
+                'createdAt.S': {
+                  format: 'strict_date_time', // ISO-8601 time stamp
+                  gte: filterDate,
+                },
+              },
+            },
+          ],
         },
       },
       size: 5000,
