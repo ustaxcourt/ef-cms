12d07e46c70568de026fb1c49f0388937d78e18c
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-06-22 14:36:00 -0700

    4609: integration tests for replying, forwarding, and completing messages

--- web-client/integration-tests/messagesJourney.test.js
+++ web-client/integration-tests/messagesJourney.test.js
@@ -1,7 +1,13 @@
+import { docketClerkCompletesMessageThread } from './journey/docketClerkCompletesMessageThread';
+import { docketClerkViewsForwardedMessageInInbox } from './journey/docketClerkViewsForwardedMessageInInbox';
 import { loginAs, setupTest, uploadPetition } from './helpers';
+import { petitionsClerk1RepliesToMessage } from './journey/petitionsClerk1RepliesToMessage';
 import { petitionsClerk1ViewsMessageDetail } from './journey/petitionsClerk1ViewsMessageDetail';
 import { petitionsClerk1ViewsMessageInbox } from './journey/petitionsClerk1ViewsMessageInbox';
 import { petitionsClerkCreatesNewMessageOnCase } from './journey/petitionsClerkCreatesNewMessageOnCase';
+import { petitionsClerkForwardsMessageToDocketClerk } from './journey/petitionsClerkForwardsMessageToDocketClerk';
+import { petitionsClerkViewsInProgressMessagesOnCaseDetail } from './journey/petitionsClerkViewsInProgressMessagesOnCaseDetail';
+import { petitionsClerkViewsReplyInInbox } from './journey/petitionsClerkViewsReplyInInbox';
 import { petitionsClerkViewsSentMessagesBox } from './journey/petitionsClerkViewsSentMessagesBox';
 
 const test = setupTest();
@@ -27,4 +33,14 @@ describe('messages journey', () => {
   loginAs(test, 'petitionsclerk1');
   petitionsClerk1ViewsMessageInbox(test);
   petitionsClerk1ViewsMessageDetail(test);
+  petitionsClerk1RepliesToMessage(test);
+
+  loginAs(test, 'petitionsclerk');
+  petitionsClerkViewsReplyInInbox(test);
+  petitionsClerkForwardsMessageToDocketClerk(test);
+  petitionsClerkViewsInProgressMessagesOnCaseDetail(test);
+
+  loginAs(test, 'docketclerk');
+  docketClerkViewsForwardedMessageInInbox(test);
+  docketClerkCompletesMessageThread(test);
 });
