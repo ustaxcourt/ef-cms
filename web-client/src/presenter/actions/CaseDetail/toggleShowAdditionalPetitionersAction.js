44a23ae186a58e5441575c0839b7237aa7fade96
Joseph Sutton <jsutton@flexion.us>
2020-06-22 11:35:40 -0500

    5430: added unit test and fixed jsdoc

--- web-client/src/presenter/actions/CaseDetail/toggleShowAdditionalPetitionersAction.js
+++ web-client/src/presenter/actions/CaseDetail/toggleShowAdditionalPetitionersAction.js
@@ -1,13 +1,13 @@
 import { state } from 'cerebral';
 
 /**
- * sets props.blockedCases on state.blockedCases
+ * sets state.showingAdditionalPetitioners its negated stored value (default false)
  *
  * @param {object} providers the providers object
- * @param {object} providers.props the props object
+ * @param {object} providers.get the cerebral get function
  * @param {Function} providers.store the cerebral store function
  */
-export const toggleShowAdditionalPetitionersAction = async ({ get, store }) => {
+export const toggleShowAdditionalPetitionersAction = ({ get, store }) => {
   const showingAdditionalPetitioners =
     get(state.showingAdditionalPetitioners) || false;
 
