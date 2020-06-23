7c6fbdf44da5140e13f08446c42ffd43ed5968b8
Mrinalini <mrinalinisinha05@gmail.com>
2020-06-23 13:29:08 -0700

    updated migration script to populate filedBy when needed

--- web-api/migrations/00009-document-required-fields.js
+++ web-api/migrations/00009-document-required-fields.js
@@ -5,41 +5,67 @@ const {
 const { isDocumentRecord, upGenerator } = require('./utilities');
 const { isEmpty } = require('lodash');
 const applicationContext = createApplicationContext({});
+const {
+  EXTERNAL_DOCUMENT_TYPES,
+} = require('../../shared/src/business/entities/EntityConstants');
 const { Document } = require('../../shared/src/business/entities/Document');
 
-const DEFAULT_SERVED_AT_DATE = createISODateString(new Date());
+const DEFAULT_SERVED_AT_DATE = createISODateString();
 const DEFAULT_SERVED_PARTIES = [
   {
     name: 'Served via migration.',
   },
 ];
+const DEFAULT_FILED_BY = 'Filed via migration.';
 
 const mutateRecord = async item => {
+  const bothOrNeitherServedAreDefined =
+    (!item.servedAt && !item.servedParties) ||
+    (item.servedAt && !isEmpty(item.servedParties));
+
+  let result;
+
   if (isDocumentRecord(item)) {
-    if (
-      (!item.servedAt && !item.servedParties) ||
-      (item.servedAt && !isEmpty(item.servedParties))
-    ) {
-      return;
+    if (!bothOrNeitherServedAreDefined) {
+      result = addMissingServedFieldsToDocument(item);
     }
-    return addMissingFieldsToDocument(item);
+    if (EXTERNAL_DOCUMENT_TYPES.includes((result || item).documentType)) {
+      result = addMissingFiledByFieldToDocument(result || item);
+    }
   }
+  return result;
 };
 
 module.exports = { mutateRecord, up: upGenerator(mutateRecord) };
 
 /**
- * checks for missing required fields and updates them if necessary
+ * checks for missing required servedAt and servedParties fields and updates them if necessary
  *
  * @param {object} item the document to be updated
  * @returns {Object} the updated document object
  */
-function addMissingFieldsToDocument(item) {
+function addMissingServedFieldsToDocument(item) {
   if (!item.servedAt) {
     item.servedAt = DEFAULT_SERVED_AT_DATE;
   }
   if (isEmpty(item.servedParties)) {
     item.servedParties = DEFAULT_SERVED_PARTIES;
+  }
+  const documentToUpdate = new Document({ ...item }, { applicationContext })
+    .validate()
+    .toRawObject();
+  return { ...item, ...documentToUpdate };
+}
+
+/**
+ * checks for missing required filedBy field and updates it if necessary
+ *
+ * @param {object} item the document to be updated
+ * @returns {Object} the updated document object
+ */
+function addMissingFiledByFieldToDocument(item) {
+  if (!item.filedBy) {
+    item.filedBy = DEFAULT_FILED_BY;
   }
   const documentToUpdate = new Document({ ...item }, { applicationContext })
     .validate()
