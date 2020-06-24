5ea2c5fa4af32a9adb43225f4cc920b1d6c4a5ad
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-06-24 08:42:09 -0500

    4611: add Create Order button to message details; open modal

--- web-client/src/presenter/actions/CourtIssuedOrder/navigateToCreateOrderAction.js
+++ web-client/src/presenter/actions/CourtIssuedOrder/navigateToCreateOrderAction.js
@@ -1,11 +1,21 @@
+import { state } from 'cerebral';
+
 /**
- * changes the route to view the create order page for the props.docketNumber
+ * changes the route to view the create order page for the props.docketNumber and messageId
  *
  * @param {object} providers the providers object
+ * @param {Function} providers.get the cerebral get function
  * @param {object} providers.router the riot.router object that is used for changing the route
  * @param {object} providers.props the cerebral props that contain the props.caseId
  * @returns {Promise} async action
  */
-export const navigateToCreateOrderAction = async ({ props, router }) => {
-  await router.route(`/case-detail/${props.docketNumber}/create-order`);
+export const navigateToCreateOrderAction = async ({ get, props, router }) => {
+  const messageId = get(state.messageId);
+  if (messageId) {
+    await router.route(
+      `/case-detail/${props.docketNumber}/create-order/${messageId}`,
+    );
+  } else {
+    await router.route(`/case-detail/${props.docketNumber}/create-order`);
+  }
 };
