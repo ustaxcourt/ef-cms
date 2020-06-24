e327cea22a1bebb6af4e7b206796093acc8ef630
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-06-24 10:03:26 -0500

    4611: submit order from message and add as attachment

--- shared/src/business/useCases/courtIssuedOrder/fileCourtIssuedOrderInteractor.test.js
+++ shared/src/business/useCases/courtIssuedOrder/fileCourtIssuedOrderInteractor.test.js
@@ -60,6 +60,7 @@ describe('fileCourtIssuedOrderInteractor', () => {
     preferredTrialCity: 'Fresno, California',
     procedureType: 'Regular',
     role: ROLES.petitioner,
+    status: 'New',
     userId: 'ddd6c900-388b-4151-8014-b3378076bfb0',
   };
 
@@ -248,6 +249,55 @@ describe('fileCourtIssuedOrderInteractor', () => {
         .toString()
         .indexOf('%PDF'),
     ).not.toEqual(-1);
+  });
+
+  it('should add order document to case message if a messageId is passed in', async () => {
+    applicationContext.getPersistenceGateway().getCaseMessage.mockReturnValue({
+      caseId: caseRecord.caseId,
+      caseStatus: caseRecord.status,
+      caseTitle: 'Petitioner',
+      createdAt: '2019-03-01T21:40:46.415Z',
+      docketNumber: caseRecord.docketNumber,
+      from: 'Test Petitionsclerk',
+      fromSection: 'petitions',
+      fromUserId: '4791e892-14ee-4ab1-8468-0c942ec379d2',
+      message: 'hey there',
+      messageId: 'a10d6855-f3ee-4c11-861c-c7f11cba4dff',
+      parentMessageId: '31687a1e-3640-42cd-8e7e-a8e6df39ce9a',
+      subject: 'hello',
+      to: 'Test Petitionsclerk2',
+      toSection: 'petitions',
+      toUserId: '449b916e-3362-4a5d-bf56-b2b94ba29c12',
+    });
+
+    await fileCourtIssuedOrderInteractor({
+      applicationContext,
+      documentMetadata: {
+        caseId: caseRecord.caseId,
+        docketNumber: '45678-18',
+        documentTitle: 'Order to do anything',
+        documentType: 'Order',
+        eventCode: 'O',
+        messageId: '6c1fd626-c1e1-4367-bca6-e00f9ef98cf5',
+        signedAt: '2019-03-01T21:40:46.415Z',
+        signedByUserId: mockUserId,
+        signedJudgeName: 'Dredd',
+      },
+      primaryDocumentFileId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+    });
+
+    expect(
+      applicationContext.getPersistenceGateway().updateCaseMessage,
+    ).toHaveBeenCalled();
+    expect(
+      applicationContext.getPersistenceGateway().updateCaseMessage.mock
+        .calls[0][0].caseMessage.attachments,
+    ).toEqual([
+      {
+        documentId: 'c54ba5a9-b37b-479d-9201-067ec6e335bb',
+        documentTitle: 'Order to do anything',
+      },
+    ]);
   });
 
   it('should throw an error if fails to parse pdf', async () => {
