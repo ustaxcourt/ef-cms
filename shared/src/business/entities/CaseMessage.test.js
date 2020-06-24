61e7440678ac37ef9db8c3061a6691165af87ff5
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-06-24 10:36:25 -0500

    4611: more strict case message validation

--- shared/src/business/entities/CaseMessage.test.js
+++ shared/src/business/entities/CaseMessage.test.js
@@ -73,6 +73,72 @@ describe('CaseMessage', () => {
         'completedByUserId',
       ]);
     });
+
+    it('creates a valid CaseMessage with attachments', () => {
+      const message = new CaseMessage(
+        {
+          attachments: [
+            {
+              documentId: 'b5533197-01c7-40e6-abf2-1a705fd6ed27',
+              documentTitle: 'Petition',
+              documentType: 'Petition',
+              eventCode: 'P',
+            },
+          ],
+          caseId: '3079c990-cc6c-4b99-8fca-8e31f2d9e7a8',
+          caseStatus: 'General Docket - Not at Issue',
+          caseTitle: 'Test Petitioner',
+          createdAt: '2019-01-01T17:29:13.122Z',
+          docketNumber: '123-45',
+          docketNumberWithSuffix: '123-45S',
+          from: 'gg',
+          fromSection: 'petitions',
+          fromUserId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+          message: 'hello world',
+          subject: 'hey!',
+          to: 'bob',
+          toSection: 'petitions',
+          toUserId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+        },
+        { applicationContext },
+      );
+      expect(message.isValid()).toBeTruthy();
+      expect(message.attachments).toEqual([
+        {
+          documentId: 'b5533197-01c7-40e6-abf2-1a705fd6ed27',
+          documentTitle: 'Petition',
+        },
+      ]);
+    });
+
+    it('creates an invalid CaseMessage with invalid attachments', () => {
+      const message = new CaseMessage(
+        {
+          attachments: [
+            {
+              documentType: 'Petition',
+              eventCode: 'P',
+            },
+          ],
+          caseId: '3079c990-cc6c-4b99-8fca-8e31f2d9e7a8',
+          caseStatus: 'General Docket - Not at Issue',
+          caseTitle: 'Test Petitioner',
+          createdAt: '2019-01-01T17:29:13.122Z',
+          docketNumber: '123-45',
+          docketNumberWithSuffix: '123-45S',
+          from: 'gg',
+          fromSection: 'petitions',
+          fromUserId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+          message: 'hello world',
+          subject: 'hey!',
+          to: 'bob',
+          toSection: 'petitions',
+          toUserId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+        },
+        { applicationContext },
+      );
+      expect(message.isValid()).toBeFalsy();
+    });
   });
 
   describe('markAsCompleted', () => {
--- shared/src/persistence/dynamo/messages/getCaseMessage.js
+++ shared/src/persistence/dynamo/messages/getCaseMessage.js
@@ -7,7 +7,7 @@ const { get } = require('../../dynamodbClientService');
  * @param {object} providers.applicationContext the application context
  * @param {string} providers.caseId the id of the case
  * @param {string} providers.messageId the id of the message
- * @returns {object} the created case message
+ * @returns {object} the retrieved case message
  */
 exports.getCaseMessage = async ({ applicationContext, caseId, messageId }) => {
   return await get({
