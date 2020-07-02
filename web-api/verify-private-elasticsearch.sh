51cd818658b9024b7f7fc2775d4e8c7d6a59a152
Cody Seibert <cseibert@flexion.us>
2020-07-02 11:41:38 -0400

    pr comments

--- web-api/verify-private-elasticsearch.sh
+++ web-api/verify-private-elasticsearch.sh
@@ -6,13 +6,13 @@
 # Arguments
 #   - $1 - the environment [dev, stg, prod, exp1, exp1, etc]
 
-[ -z "$1" ] && echo "The env to run smoketest to \$1 argument.  An example value of this includes [dev, stg, prod... ]" && exit 1
+[ -z "$1" ] && echo "Error: the env to run smoketest to \$1 argument.  An example value of this includes [dev, stg, prod... ]" && exit 1
+[ -z "$AWS_PROFILE" ] && [ -z "$AWS_ACCESS_KEY_ID"] && [ -z "$AWS_SECRET_ACCESS_KEY" ] && echo "Error: you must have AWS credentials setup to run this script" && exit 1
 
 ENV=$1
 
 endpoint=$(aws es describe-elasticsearch-domain --domain "efcms-search-$ENV" | jq -r ".DomainStatus.Endpoint" )
 
-echo $endpoint
 response=$(curl -I "https://$endpoint" | head -n 1 | cut -d$' ' -f2)
 
 if [[ "$response" != "403" ]]; then
