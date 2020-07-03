c3a8e92e959704d126a3dbf21dd5dc08d760d767
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-07-03 08:38:48 -0700

    make test less flaky

--- web-client/integration-tests/externalUserViewsOpenAndClosedCases.test.js
+++ web-client/integration-tests/externalUserViewsOpenAndClosedCases.test.js
@@ -2,6 +2,8 @@ import { docketClerkUpdatesCaseStatusToClosed } from './journey/docketClerkUpdat
 import { irsPractitionerViewsOpenAndClosedCases } from './journey/irsPractitionerViewsOpenAndClosedCases';
 import { loginAs, setupTest, uploadPetition } from './helpers';
 import { petitionerViewsOpenAndClosedCases } from './journey/petitionerViewsOpenAndClosedCases';
+import { petitionsClerkAddsPractitionersToCase } from './journey/petitionsClerkAddsPractitionersToCase';
+import { petitionsClerkAddsRespondentsToCase } from './journey/petitionsClerkAddsRespondentsToCase';
 import { privatePractitionerViewsOpenAndClosedCases } from './journey/privatePractitionerViewsOpenAndClosedCases';
 
 const test = setupTest();
@@ -18,6 +20,10 @@ describe('external user views open and closed cases', () => {
     expect(caseDetail.docketNumber).toBeDefined();
     test.docketNumber = caseDetail.docketNumber;
   });
+
+  loginAs(test, 'petitionsclerk');
+  petitionsClerkAddsPractitionersToCase(test, true);
+  petitionsClerkAddsRespondentsToCase(test);
 
   loginAs(test, 'docketclerk');
   docketClerkUpdatesCaseStatusToClosed(test);
