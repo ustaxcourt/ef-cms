7295f3db3cdfcdf05c0ce0708667c1319ae5862a
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-07-03 08:58:40 -0700

    3580: DOD - pa11y, accessibility updates

--- web-client/src/views/DocketRecord/DocumentViewer.jsx
+++ web-client/src/views/DocketRecord/DocumentViewer.jsx
@@ -1,3 +1,4 @@
+import { Button } from '../../ustc-ui/Button/Button';
 import { DocumentViewerDocument } from './DocumentViewerDocument';
 import { connect } from '@cerebral/react';
 import { sequences, state } from 'cerebral';
@@ -19,38 +20,39 @@ export const DocumentViewer = connect(
       <>
         <div className="grid-row grid-gap-5">
           <div className="grid-col-4">
-            <div className="document-viewer--documents">
-              <table className="document-viewer usa-table case-detail docket-record responsive-table row-border-only">
-                <tbody>
-                  {formattedCaseDetail.docketRecordWithDocument.map(
-                    ({ document, index, record }, idx) => {
-                      if (document) {
-                        const active =
-                          viewerDocumentToDisplay.documentId ===
-                          document.documentId
-                            ? 'active'
-                            : '';
+            <div className="border border-base-lighter document-viewer--documents">
+              {formattedCaseDetail.docketRecordWithDocument.map(
+                ({ document, index, record }, idx) => {
+                  if (document) {
+                    const active =
+                      viewerDocumentToDisplay.documentId === document.documentId
+                        ? 'active'
+                        : '';
 
-                        return (
-                          <tr
-                            className={active}
-                            key={idx}
-                            onClick={() => {
-                              setViewerDocumentToDisplaySequence({
-                                viewerDocumentToDisplay: document,
-                              });
-                            }}
-                          >
-                            <td className="center-column small">{index}</td>
-                            <td>{record.createdAtFormatted}</td>
-                            <td>{record.description}</td>
-                          </tr>
-                        );
-                      }
-                    },
-                  )}
-                </tbody>
-              </table>
+                    return (
+                      <Button
+                        className={`usa-button--unstyled attachment-viewer-button ${active}`}
+                        key={idx}
+                        onClick={() => {
+                          setViewerDocumentToDisplaySequence({
+                            viewerDocumentToDisplay: document,
+                          });
+                        }}
+                      >
+                        <div className="grid-row">
+                          <div className="grid-col-1">{index}</div>
+                          <div className="grid-col-3">
+                            {record.createdAtFormatted}
+                          </div>
+                          <div className="grid-col-8 no-indent">
+                            {record.description}
+                          </div>
+                        </div>
+                      </Button>
+                    );
+                  }
+                },
+              )}
             </div>
           </div>
 
