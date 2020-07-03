f6b8b963cc37b8729e07b7d55d7c0e8338cfe7fc
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-07-03 09:55:34 -0700

    DevEx: update troubleshooting documentation and new endpoint checklist

--- shared/src/test/mockUsers.js
+++ shared/src/test/mockUsers.js
@@ -1,4 +1,4 @@
-import { ROLES } from '../business/entities/EntityConstants';
+const { ROLES } = require('../business/entities/EntityConstants');
 
 exports.MOCK_USERS = {
   '330d4b65-620a-489d-8414-6623653ebc4f': {
