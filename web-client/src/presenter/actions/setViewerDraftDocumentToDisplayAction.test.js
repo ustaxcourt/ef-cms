840c2d4b40107564ec8b716db82a1f0716fcc9d4
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-07-03 12:00:43 -0700

    4613: draft document viewer right side panel

---
+++ web-client/src/presenter/actions/setViewerDraftDocumentToDisplayAction.test.js
@@ -0,0 +1,54 @@
+import { applicationContextForClient as applicationContext } from '../../../../shared/src/business/test/createTestApplicationContext';
+import { presenter } from '../presenter-mock';
+import { runAction } from 'cerebral/test';
+import { setViewerDraftDocumentToDisplayAction } from './setViewerDraftDocumentToDisplayAction';
+
+describe('setViewerDraftDocumentToDisplayAction', () => {
+  beforeAll(() => {
+    applicationContext
+      .getUseCases()
+      .getDocumentDownloadUrlInteractor.mockReturnValue({
+        url: 'www.example.com',
+      });
+    presenter.providers.applicationContext = applicationContext;
+  });
+
+  it('sets the viewerDraftDocumentToDisplay from props on state and sets the iframeSrc url from the return from the use case', async () => {
+    const result = await runAction(setViewerDraftDocumentToDisplayAction, {
+      modules: {
+        presenter,
+      },
+      props: {
+        viewerDraftDocumentToDisplay: { documentId: '1234' },
+      },
+      state: {
+        caseDetail: {
+          caseId: '48849291-d329-465d-a421-eecf06a671de',
+        },
+        viewerDraftDocumentToDisplay: null,
+      },
+    });
+    expect(result.state.viewerDraftDocumentToDisplay).toEqual({
+      documentId: '1234',
+    });
+    expect(result.state.iframeSrc).toEqual('www.example.com');
+  });
+
+  it('does not set iframeSrc if props.viewerDraftDocumentToDisplay is null', async () => {
+    const result = await runAction(setViewerDraftDocumentToDisplayAction, {
+      modules: {
+        presenter,
+      },
+      props: {
+        viewerDraftDocumentToDisplay: null,
+      },
+      state: {
+        caseDetail: {
+          caseId: '48849291-d329-465d-a421-eecf06a671de',
+        },
+        viewerDraftDocumentToDisplay: null,
+      },
+    });
+    expect(result.state.iframeSrc).toBeUndefined();
+  });
+});
