8589eef51a16c83ef7a4c6a8b8f438cd22abf711
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-07-03 08:38:48 -0700

    make test less flaky

--- web-client/integration-tests/journey/irsPractitionerViewsOpenAndClosedCases.js
+++ web-client/integration-tests/journey/irsPractitionerViewsOpenAndClosedCases.js
@@ -1,13 +1,9 @@
-import { refreshElasticsearchIndex } from '../helpers';
-
 export const irsPractitionerViewsOpenAndClosedCases = test => {
   return it('irs practitoner views open and closed cases', async () => {
-    await refreshElasticsearchIndex();
-
     await test.runSequence('gotoDashboardSequence');
 
     expect(test.getState('currentPage')).toEqual('DashboardRespondent');
     expect(test.getState('openCases').length).toBeGreaterThan(0);
-    expect(test.getState('closedCases').length).toEqual(0);
+    expect(test.getState('closedCases').length).toBeGreaterThan(0);
   });
 };
