46349321b74af281e57e53ac0b720b64c5ec2453
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-07-06 08:31:00 -0700

    4613: don't allow adding docket entry for document that requires signature

--- web-client/src/presenter/computeds/AdvancedSearch/advancedDocumentSearchHelper.js
+++ web-client/src/presenter/computeds/AdvancedSearch/advancedDocumentSearchHelper.js
@@ -1,7 +1,7 @@
 import { Document } from '../../../../../shared/src/business/entities/Document';
 import {
   OPINION_EVENT_CODES,
-  ORDER_DOCUMENT_TYPES,
+  ORDER_EVENT_CODES,
 } from '../../../../../shared/src/business/entities/EntityConstants';
 import { capitalize } from 'lodash';
 import { paginationHelper } from './advancedSearchHelper';
@@ -67,7 +67,7 @@ export const formatDocumentSearchResultRecord = (
     result.formattedJudgeName = result.judge
       ? applicationContext.getUtilities().getJudgeLastName(result.judge)
       : '';
-  } else if (ORDER_DOCUMENT_TYPES.includes(result.eventCode)) {
+  } else if (ORDER_EVENT_CODES.includes(result.eventCode)) {
     result.formattedSignedJudgeName = result.signedJudgeName
       ? applicationContext
           .getUtilities()
