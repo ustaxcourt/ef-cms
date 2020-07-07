f6ae90d3f3553acb02129b78c2bbd6fd716a91ca
Joseph Sutton <jsutton@flexion.us>
2020-07-07 17:15:48 -0500

    5525: added max date range for unservable document eventCode, 'CTRA', per UX

--- shared/src/business/entities/courtIssuedDocument/CourtIssuedDocumentTypeE.js
+++ shared/src/business/entities/courtIssuedDocument/CourtIssuedDocumentTypeE.js
@@ -6,17 +6,24 @@ const {
   FORMATS,
 } = require('../../utilities/DateHandler');
 const {
+  CORRECTED_TRANSCRIPT_DOCUMENT_TYPE,
+  VALIDATION_ERROR_MESSAGES,
+} = require('./CourtIssuedDocumentConstants');
+const {
   JoiValidationConstants,
 } = require('../../../utilities/JoiValidationConstants');
 const {
   joiValidationDecorator,
 } = require('../../../utilities/JoiValidationDecorator');
 const { replaceBracketed } = require('../../utilities/replaceBracketed');
-const { VALIDATION_ERROR_MESSAGES } = require('./CourtIssuedDocumentConstants');
 
 const yesterdayMoment = calculateISODate({ howMuch: -1, unit: 'days' });
 const yesterdayFormatted = formatDateString(
   createISODateString(yesterdayMoment),
+  FORMATS.MMDDYYYY,
+);
+const todayFormatted = formatDateString(
+  createISODateString(),
   FORMATS.MMDDYYYY,
 );
 
@@ -41,7 +48,13 @@ CourtIssuedDocumentTypeE.prototype.getDocumentTitle = function () {
 
 CourtIssuedDocumentTypeE.schema = {
   attachments: joi.boolean().required(),
-  date: JoiValidationConstants.ISO_DATE.min(yesterdayFormatted).required(),
+  date: joi.when('documentType', {
+    is: CORRECTED_TRANSCRIPT_DOCUMENT_TYPE,
+    otherwise: JoiValidationConstants.ISO_DATE.min(
+      yesterdayFormatted,
+    ).required(),
+    then: JoiValidationConstants.ISO_DATE.max(todayFormatted).required(),
+  }),
   documentTitle: joi.string().optional(),
   documentType: joi.string().required(),
 };
