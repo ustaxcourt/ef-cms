d524ae53d6665b3f6bda3e1d7941a12bf5a83832
Matt Hopson <matthopson@gmail.com>
2020-07-08 13:29:24 -0600

    Draft document signature and docket entry flow updates

--- web-client/src/presenter/computeds/draftDocumentViewerHelper.test.js
+++ web-client/src/presenter/computeds/draftDocumentViewerHelper.test.js
@@ -440,4 +440,46 @@ describe('draftDocumentViewerHelper', () => {
 
     expect(result.showEditButtonSigned).toEqual(false);
   });
+
+  it('should return showDocumentNotSignedAlert false if document is not signed and the event code does not require a signature', () => {
+    const result = runCompute(draftDocumentViewerHelper, {
+      state: {
+        caseDetail: {
+          ...getBaseState(docketClerkUser),
+          documents: [
+            {
+              documentId: '123',
+            },
+          ],
+        },
+        documentId: '123',
+        form: {
+          eventCode: 'MISC', // Does not require a signature
+        },
+      },
+    });
+
+    expect(result.showDocumentNotSignedAlert).toEqual(false);
+  });
+
+  it('should return showDocumentNotSignedAlert true if document is not signed but the event code requires a signature', () => {
+    const result = runCompute(draftDocumentViewerHelper, {
+      state: {
+        caseDetail: {
+          ...getBaseState(docketClerkUser),
+          documents: [
+            {
+              documentId: '123',
+            },
+          ],
+        },
+        documentId: '123',
+        form: {
+          eventCode: 'O', // Requires a signature
+        },
+      },
+    });
+
+    expect(result.showDocumentNotSignedAlert).toEqual(true);
+  });
 });
--- web-client/src/views/CourtIssuedDocketEntry/CourtIssuedDocketEntry.jsx
+++ web-client/src/views/CourtIssuedDocketEntry/CourtIssuedDocketEntry.jsx
@@ -76,22 +76,16 @@ export const CourtIssuedDocketEntry = connect(
                   <span className="text-bold">Docket entry preview: </span>
                   {addCourtIssuedDocketEntryHelper.formattedDocumentTitle}
                 </div>
-                {addCourtIssuedDocketEntryHelper.showSaveAndServeButton && (
-                  <Button
-                    className="margin-right-0"
-                    id="serve-to-parties-btn"
-                    onClick={() => {
-                      openConfirmInitiateServiceModalSequence();
-                    }}
-                  >
-                    Save and Serve
-                  </Button>
-                )}
-                {addCourtIssuedDocketEntryHelper.showDocumentNotSignedAlert && (
-                  <p className="text-secondary-dark text-semibold">
-                    Signature required to serve
-                  </p>
-                )}
+
+                <Button
+                  className="margin-right-0"
+                  id="serve-to-parties-btn"
+                  onClick={() => {
+                    openConfirmInitiateServiceModalSequence();
+                  }}
+                >
+                  Save and Serve
+                </Button>
               </div>
             </div>
           </div>
