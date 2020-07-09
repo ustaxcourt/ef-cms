cd7361ef0ca8bb3da73f5f1517a1faacf3a06807
Mrinalini <mrinalinisinha05@gmail.com>
2020-07-09 08:19:03 -0700

    undid deletion of required code

--- web-client/src/presenter/sequences/submitCourtIssuedOrderSequence.js
+++ web-client/src/presenter/sequences/submitCourtIssuedOrderSequence.js
@@ -1,9 +1,12 @@
 import { convertHtml2PdfSequence } from './convertHtml2PdfSequence';
+import { followRedirectAction } from '../actions/followRedirectAction';
+import { getEditDocumentEntryPointAction } from '../actions/getEditDocumentEntryPointAction';
 import { getEditedDocumentDetailParamsAction } from '../actions/getEditedDocumentDetailParamsAction';
 import { getFileExternalDocumentAlertSuccessAction } from '../actions/FileDocument/getFileExternalDocumentAlertSuccessAction';
 import { getShouldRedirectToSigningAction } from '../actions/getShouldRedirectToSigningAction';
 import { isEditingOrderAction } from '../actions/CourtIssuedOrder/isEditingOrderAction';
 import { isFormPristineAction } from '../actions/CourtIssuedOrder/isFormPristineAction';
+import { navigateToCaseDetailAction } from '../actions/navigateToCaseDetailAction';
 import { navigateToDraftDocumentsAction } from '../actions/navigateToDraftDocumentsAction';
 import { navigateToSignOrderAction } from '../actions/navigateToSignOrderAction';
 import { openFileUploadErrorModal } from '../actions/openFileUploadErrorModal';
@@ -24,7 +27,13 @@ const onFileUploadedSuccess = [
   getEditedDocumentDetailParamsAction,
   getShouldRedirectToSigningAction,
   {
-    no: navigateToDraftDocumentsAction,
+    no: [
+      followRedirectAction,
+      {
+        default: [navigateToDraftDocumentsAction],
+        success: [],
+      },
+    ],
     yes: navigateToSignOrderAction,
   },
 ];
