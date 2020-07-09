2721682afa05997a678e6a202bc3f81f5eedb915
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-07-09 11:31:42 -0500

    DevEx: content security policy refinement

--- web-client/terraform/common/cloudfront-edge/index.js
+++ web-client/terraform/common/cloudfront-edge/index.js
@@ -39,11 +39,14 @@ exports.handler = (event, context, callback) => {
   const contentSecurityPolicy = [
     'base-uri resource://pdf.js',
     `connect-src ${applicationUrl} ${cognitoUrl} ${s3Url} ${dynamsoftUrl} ${localUrl} ${websocketUrl} ${localWebsocketUrl} ${honeybadgerApiUrl}`,
-    `default-src ${applicationUrl} ${s3Url} data: blob:`,
+    "default-src 'none'",
     `form-action ${applicationUrl}`,
     "object-src 'none'",
     `script-src 'self' 'unsafe-inline' ${dynamsoftUrl} resource://pdf.js`,
     `style-src 'self' 'unsafe-inline' ${dynamsoftUrl}`,
+    `img-src ${applicationUrl} data:`,
+    `font-src ${applicationUrl}`,
+    `frame-src ${s3Url}`,
   ];
   headers['content-security-policy'] = [
     {
