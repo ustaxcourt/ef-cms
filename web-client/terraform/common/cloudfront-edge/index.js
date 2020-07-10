4d59efea254df523233173aa417d4e5972745647
Kris Koskelin <kkoskelin@flexion.us>
2020-07-10 11:42:56 -0500

    attempt to address manifest load refusal
    Seen in browser console "Refused to load manifest from 'https://ui-dev.ustc-case-mgmt.flexion.us/site.webmanifest' because it violates the following Content Security Policy directive: "default-src 'none'". Note that 'manifest-src' was not explicitly set, so 'default-src' is used as a fallback."

--- web-client/terraform/common/cloudfront-edge/index.js
+++ web-client/terraform/common/cloudfront-edge/index.js
@@ -40,6 +40,7 @@ exports.handler = (event, context, callback) => {
     'base-uri resource://pdf.js',
     `connect-src ${applicationUrl} ${cognitoUrl} ${s3Url} ${dynamsoftUrl} ${localUrl} ${websocketUrl} ${localWebsocketUrl} ${honeybadgerApiUrl}`,
     "default-src 'none'",
+    "manifest-src 'self'",
     `form-action ${applicationUrl}`,
     "object-src 'none'",
     `script-src 'self' 'unsafe-inline' 'unsafe-eval' ${dynamsoftUrl} resource://pdf.js`,
