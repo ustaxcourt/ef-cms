07df1715f1b798997624207c1acdbc176ed39994
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-07-13 14:05:13 -0700

    DevEx: eliminate todos - DRY up fakeFile

--- web-client/src/presenter/sequences/PDFPreviewModal/setPageSequence.test.js
+++ web-client/src/presenter/sequences/PDFPreviewModal/setPageSequence.test.js
@@ -1,5 +1,8 @@
 import { CerebralTest } from 'cerebral/test';
-import { applicationContextForClient as applicationContext } from '../../../../../shared/src/business/test/createTestApplicationContext';
+import {
+  applicationContextForClient as applicationContext,
+  getFakeFile,
+} from '../../../../../shared/src/business/test/createTestApplicationContext';
 import { loadPdfSequence } from '../../sequences/PDFPreviewModal/loadPdfSequence';
 import { presenter } from '../../presenter-mock';
 import { setPageSequence } from '../../sequences/PDFPreviewModal/setPageSequence';
@@ -26,11 +29,6 @@ function MockFileReader() {
 
 let test;
 
-const fakeData =
-  'JVBERi0xLjEKJcKlwrHDqwoKMSAwIG9iagogIDw8IC9UeXBlIC9DYXRhbG9nCiAgICAgL1BhZ2VzIDIgMCBSCiAgPj4KZW5kb2JqCgoyIDAgb2JqCiAgPDwgL1R5cGUgL1BhZ2VzCiAgICAgL0tpZHMgWzMgMCBSXQogICAgIC9Db3VudCAxCiAgICAgL01lZGlhQm94IFswIDAgMzAwIDE0NF0KICA+PgplbmRvYmoKCjMgMCBvYmoKICA8PCAgL1R5cGUgL1BhZ2UKICAgICAgL1BhcmVudCAyIDAgUgogICAgICAvUmVzb3VyY2VzCiAgICAgICA8PCAvRm9udAogICAgICAgICAgIDw8IC9GMQogICAgICAgICAgICAgICA8PCAvVHlwZSAvRm9udAogICAgICAgICAgICAgICAgICAvU3VidHlwZSAvVHlwZTEKICAgICAgICAgICAgICAgICAgL0Jhc2VGb250IC9UaW1lcy1Sb21hbgogICAgICAgICAgICAgICA+PgogICAgICAgICAgID4+CiAgICAgICA+PgogICAgICAvQ29udGVudHMgNCAwIFIKICA+PgplbmRvYmoKCjQgMCBvYmoKICA8PCAvTGVuZ3RoIDg0ID4+CnN0cmVhbQogIEJUCiAgICAvRjEgMTggVGYKICAgIDUgODAgVGQKICAgIChDb25ncmF0aW9ucywgeW91IGZvdW5kIHRoZSBFYXN0ZXIgRWdnLikgVGoKICBFVAplbmRzdHJlYW0KZW5kb2JqCgp4cmVmCjAgNQowMDAwMDAwMDAwIDY1NTM1IGYgCjAwMDAwMDAwMTggMDAwMDAgbiAKMDAwMDAwMDA3NyAwMDAwMCBuIAowMDAwMDAwMTc4IDAwMDAwIG4gCjAwMDAwMDA0NTcgMDAwMDAgbiAKdHJhaWxlcgogIDw8ICAvUm9vdCAxIDAgUgogICAgICAvU2l6ZSA1CiAgPj4Kc3RhcnR4cmVmCjU2NQolJUVPRgo=';
-const fakeFile = Buffer.from(fakeData, 'base64');
-fakeFile.name = 'fakeFile.pdf';
-
 describe('setPageSequence', () => {
   beforeAll(() => {
     applicationContext.getFileReaderInstance.mockReturnValue(
@@ -47,7 +45,7 @@ describe('setPageSequence', () => {
     test.setState('modal.pdfPreviewModal', {});
     await test.runSequence('loadPdfSequence', {
       ctx: 'abc',
-      file: fakeFile,
+      file: getFakeFile(),
     });
   });
 
