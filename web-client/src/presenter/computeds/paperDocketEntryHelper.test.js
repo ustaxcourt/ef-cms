809a70dd955145654511ed1b5e7b0af7e5e886c7
Joseph Sutton <jsutton@flexion.us>
2020-07-14 13:36:58 -0500

    #4295: properly check if the docket entry being edited has a document on the case

--- web-client/src/presenter/computeds/paperDocketEntryHelper.test.js
+++ web-client/src/presenter/computeds/paperDocketEntryHelper.test.js
@@ -2,33 +2,33 @@ import { paperDocketEntryHelper } from './paperDocketEntryHelper';
 import { runCompute } from 'cerebral/test';
 
 describe('paperDocketEntryHelper', () => {
-  it('should set showAddDocumentAlert to TRUE if documentId is NOT set and user is editing docket entry', () => {
+  it('should set showAddDocumentWarning to TRUE if documentId is NOT set and user is editing docket entry', () => {
     const result = runCompute(paperDocketEntryHelper, {
       state: {
         isEditingDocketEntry: true,
       },
     });
 
-    expect(result.showAddDocumentAlert).toBeTruthy();
+    expect(result.showAddDocumentWarning).toBeTruthy();
   });
 
-  it('should set showAddDocumentAlert to FALSE if documentId is NOT set and user is NOT editing docket entry', () => {
+  it('should set showAddDocumentWarning to FALSE if documentId is NOT set and user is NOT editing docket entry', () => {
     const result = runCompute(paperDocketEntryHelper, {
       state: {
         isEditingDocketEntry: false,
       },
     });
 
-    expect(result.showAddDocumentAlert).toBeFalsy();
+    expect(result.showAddDocumentWarning).toBeFalsy();
   });
 
-  it('should set showAddDocumentAlert to FALSE if documentId is set in the state and user is editing docket entry', () => {
+  it('should set showAddDocumentWarning to FALSE if documentId is set in the state and user is editing docket entry', () => {
     const result = runCompute(paperDocketEntryHelper, {
       state: {
         documentId: 'document-id-123',
         isEditingDocketEntry: true,
       },
     });
-    expect(result.showAddDocumentAlert).toBeFalsy();
+    expect(result.showAddDocumentWarning).toBeFalsy();
   });
 });
