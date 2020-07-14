e693dbab3abefce8ace85191b42bcb66614a7672
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-07-14 11:30:49 -0700

    scanner dialog will not always be shown

--- cypress-smoketests/support/pages/create-paper-case.js
+++ cypress-smoketests/support/pages/create-paper-case.js
@@ -26,11 +26,3 @@ exports.serveCaseToIrs = () => {
   cy.get('#ustc-start-a-case-form button#submit-case').scrollIntoView().click();
   cy.get('button#confirm').scrollIntoView().click();
 };
-
-exports.closeScannerSetupDialog = () => {
-  cy.get('div.dynamsoft-backdrop').should('exist');
-
-  cy.get('div.dynamsoft-dialog-close').click();
-
-  cy.get('div.dynamsoft-backdrop').should('not.exist');
-};
