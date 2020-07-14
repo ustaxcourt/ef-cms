a969a60565fc27813c389e18a14bbd1cc8999fba
Joseph Sutton <jsutton@flexion.us>
2020-07-14 13:36:58 -0500

    #4295: properly check if the docket entry being edited has a document on the case

--- web-client/src/presenter/computeds/paperDocketEntryHelper.js
+++ web-client/src/presenter/computeds/paperDocketEntryHelper.js
@@ -1,12 +1,23 @@
 import { state } from 'cerebral';
 
 export const paperDocketEntryHelper = get => {
-  const docketEntryHasDocument = get(state.documentId);
+  const documentId = get(state.documentId);
   const isEditingDocketEntry = get(state.isEditingDocketEntry);
+  const caseDetail = get(state.caseDetail);
+
+  const allCaseDocuments = [
+    ...(caseDetail.documents || []),
+    ...(caseDetail.correspondence || []),
+  ];
+  const document = allCaseDocuments.find(
+    item => item.documentId === documentId,
+  );
+
+  const docketEntryHasDocument = !!document.isFileAttached;
 
   return {
     docketEntryHasDocument,
     isEditingDocketEntry,
-    showAddDocumentAlert: isEditingDocketEntry && !docketEntryHasDocument,
+    showAddDocumentWarning: isEditingDocketEntry && !docketEntryHasDocument,
   };
 };
