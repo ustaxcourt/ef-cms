15457799fe59a21b7be303e04c09be6d940badf6
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-07-14 14:10:41 -0700

    put pa11y tests back

--- web-client/pa11y-ci.web-client-3.config.js
+++ web-client/pa11y-ci.web-client-3.config.js
@@ -4,10 +4,9 @@ const irsSuperuser = require('./pa11y/pa11y-irs-superuser');
 const petitioner = require('./pa11y/pa11y-petitioner');
 const { defaults, jsCheckDecorator } = require('./pa11y-ci.base-config.js');
 
-const urls = [
-  ...petitioner,
-  // ...adc, ...irsSuperuser, ...admissionsClerk
-].map(jsCheckDecorator);
+const urls = [...petitioner, ...adc, ...irsSuperuser, ...admissionsClerk].map(
+  jsCheckDecorator,
+);
 
 module.exports = {
   defaults,
