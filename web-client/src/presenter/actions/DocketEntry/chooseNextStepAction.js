d327b4bf95942ac50b51ea7951980252f8256a16
Cody Seibert <cseibert@flexion.us>
2020-07-14 15:02:30 -0400

    adding to js docs

--- web-client/src/presenter/actions/DocketEntry/chooseNextStepAction.js
+++ web-client/src/presenter/actions/DocketEntry/chooseNextStepAction.js
@@ -6,6 +6,7 @@ import { state } from 'cerebral';
  * @param {object} providers the providers object
  * @param {Function} providers.get cerebral get function
  * @param {object} providers.path the next object in the path (this is defined in the sequence right after this action is invoked)
+ * @param {object} providers.applicationContext the applicationContext
  * @returns {*} returns the next action in the sequence's path
  */
 export const chooseNextStepAction = ({ applicationContext, get, path }) => {
