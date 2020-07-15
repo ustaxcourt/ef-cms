74a90870b2aecfb9173e3afdebbebb3c123c1c3c
Matt Hopson <matthopson@gmail.com>
2020-07-15 14:38:46 -0600

    Update correspondence doc when deleting correspondence

--- web-client/src/presenter/sequences/deleteCorrespondenceDocumentSequence.js
+++ web-client/src/presenter/sequences/deleteCorrespondenceDocumentSequence.js
@@ -2,11 +2,13 @@ import { clearModalAction } from '../actions/clearModalAction';
 import { clearModalStateAction } from '../actions/clearModalStateAction';
 import { deleteCorrespondenceDocumentAction } from '../actions/CorrespondenceDocument/deleteCorrespondenceDocumentAction';
 import { getCaseAction } from '../actions/getCaseAction';
+import { getDefaultViewerCorrespondenceToDisplayAction } from '../actions/getDefaultViewerCorrespondenceToDisplayAction';
 import { getDeleteCorrespondenceDocumentAlertErrorAction } from '../actions/CorrespondenceDocument/getDeleteCorrespondenceDocumentAlertErrorAction';
 import { getDeleteCorrespondenceDocumentAlertSuccessAction } from '../actions/CorrespondenceDocument/getDeleteCorrespondenceDocumentAlertSuccessAction';
 import { setAlertErrorAction } from '../actions/setAlertErrorAction';
 import { setAlertSuccessAction } from '../actions/setAlertSuccessAction';
 import { setCaseAction } from '../actions/setCaseAction';
+import { setViewerCorrespondenceToDisplayAction } from '../actions/setViewerCorrespondenceToDisplayAction';
 
 export const deleteCorrespondenceDocumentSequence = [
   deleteCorrespondenceDocumentAction,
@@ -20,6 +22,8 @@ export const deleteCorrespondenceDocumentSequence = [
       setAlertSuccessAction,
       getCaseAction,
       setCaseAction,
+      getDefaultViewerCorrespondenceToDisplayAction,
+      setViewerCorrespondenceToDisplayAction,
     ],
   },
   clearModalAction,
