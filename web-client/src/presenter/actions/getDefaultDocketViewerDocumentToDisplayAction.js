08685f22becc5561d66019ce0e17aaf7b5cd1dd6
Cody Seibert <cseibert@flexion.us>
2020-07-16 13:22:07 -0400

    fixing bug with the default link click

--- web-client/src/presenter/actions/getDefaultDocketViewerDocumentToDisplayAction.js
+++ web-client/src/presenter/actions/getDefaultDocketViewerDocumentToDisplayAction.js
@@ -15,7 +15,10 @@ export const getDefaultDocketViewerDocumentToDisplayAction = ({
   props,
 }) => {
   const { documentId } = props;
-  let viewerDocumentToDisplay = null;
+
+  let viewerDocumentToDisplay = get(state.viewerDocumentToDisplay);
+
+  if (viewerDocumentToDisplay) return { viewerDocumentToDisplay };
 
   const { docketRecord, documents } = get(state.caseDetail);
 
