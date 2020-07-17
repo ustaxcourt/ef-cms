feef6e26a686a178cedcae51213b6e6eed465a5b
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-07-17 11:47:33 -0700

    jsdoc updates [skip ci]

--- shared/src/proxies/documents/servePaperFiledDocumentProxy.js
+++ shared/src/proxies/documents/servePaperFiledDocumentProxy.js
@@ -5,8 +5,8 @@ const { post } = require('../requests');
  *
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
- * @param {string} providers.documentId the id of the document to validate
- * @param {string} providers.caseId the id of the document to validate
+ * @param {string} providers.caseId the id of the case containing the document to serve
+ * @param {string} providers.documentId the id of the document to serve
  * @returns {Promise<*>} the promise of the api call
  */
 exports.servePaperFiledDocumentInteractor = ({
--- web-client/src/presenter/actions/DocketEntry/servePaperFiledDocumentAction.js
+++ web-client/src/presenter/actions/DocketEntry/servePaperFiledDocumentAction.js
@@ -6,8 +6,7 @@ import { state } from 'cerebral';
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
  * @param {Function} providers.get the cerebral get function
- * @param {object} providers.props the cerebral props object
- * @returns {Promise} async action
+ * @returns {object} props needed for later actions
  */
 export const servePaperFiledDocumentAction = async ({
   applicationContext,
