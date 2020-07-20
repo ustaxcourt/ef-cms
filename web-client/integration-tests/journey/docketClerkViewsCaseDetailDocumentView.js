beed6da42f7b6f8f4cde584e2674ea0e2c42e831
Mrinalini <mrinalinisinha05@gmail.com>
2020-07-20 10:09:29 -0700

    WIP testing service of court issued docs through document detail view

---
+++ web-client/integration-tests/journey/docketClerkViewsCaseDetailDocumentView.js
@@ -0,0 +1,43 @@
+import { formattedCaseDetail as formattedCaseDetailComputed } from '../../src/presenter/computeds/formattedCaseDetail';
+import { refreshElasticsearchIndex, viewCaseDetail } from '../helpers';
+import { runCompute } from 'cerebral/test';
+
+export const docketClerkViewsCaseDetailDocumentView = (
+  test,
+  docketNumber = null,
+) => {
+  return it('Docketclerk views case detail document view', async () => {
+    await test.runSequence('gotoCaseDetailSequence', {
+      docketNumber: docketNumber || test.docketNumber,
+    });
+
+    const caseDetail = test.getState('caseDetail');
+
+    await viewCaseDetail({
+      docketNumber: caseDetail.docketNumber,
+      test,
+    });
+    const formatted = runCompute(formattedCaseDetailComputed, {
+      state: test.getState(),
+    });
+
+    expect(formatted.pendingItemsDocketEntries.length).toEqual(1);
+
+    await refreshElasticsearchIndex();
+
+    expect(caseDetail.associatedJudge).toBeDefined();
+    expect(caseDetail.status).toBeDefined();
+    expect(caseDetail.userId).toBeDefined();
+
+    await test.runSequence('changeTabAndSetViewerDocumentToDisplaySequence', {
+      docketRecordTab: 'documentView',
+      viewerDocumentToDisplay: {
+        documentId: formatted.pendingItemsDocketEntries[0].documentId,
+      },
+    });
+
+    expect(
+      test.getState('currentViewMetadata.caseDetail.docketRecordTab'),
+    ).toEqual('documentView');
+  });
+};
