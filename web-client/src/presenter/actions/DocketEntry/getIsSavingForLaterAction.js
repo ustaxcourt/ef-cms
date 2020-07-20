b77aa6227a1321c97fdf64fe3c39f195cf855a6c
Joseph Sutton <jsutton@flexion.us>
2020-07-20 13:44:14 -0500

    updated jsdoc

--- web-client/src/presenter/actions/DocketEntry/getIsSavingForLaterAction.js
+++ web-client/src/presenter/actions/DocketEntry/getIsSavingForLaterAction.js
@@ -1,10 +1,9 @@
 /**
- * used to direct the user to the correct next page - either the isElectronic or isPaper
+ * used to save and serve, or just save a docket entry, depending if props.isSavingForLater is true
  *
  * @param {object} providers the providers object
- * @param {Function} providers.get cerebral get function
  * @param {object} providers.path the next object in the path (this is defined in the sequence right after this action is invoked)
- * @param {object} providers.applicationContext the applicationContext
+ * @param {object} providers.props cerebral props
  * @returns {*} returns the next action in the sequence's path
  */
 export const getIsSavingForLaterAction = ({ path, props }) => {
