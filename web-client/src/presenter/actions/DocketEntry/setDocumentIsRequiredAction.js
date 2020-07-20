be7b7fbc089a0eba730fe9d27646d400e58bb62e
Joseph Sutton <jsutton@flexion.us>
2020-07-20 13:45:11 -0500

    updated jsdoc

--- web-client/src/presenter/actions/DocketEntry/setDocumentIsRequiredAction.js
+++ web-client/src/presenter/actions/DocketEntry/setDocumentIsRequiredAction.js
@@ -1,10 +1,11 @@
 import { state } from 'cerebral';
 
 /**
- * sets the document as required when saving and serving a docket entry
+ * sets the document as required when saving AND serving a docket entry
  *
  * @param {object} providers the providers object
  * @param {object} providers.get the cerebral get function
+ * @param {object} providers.props cerebral props
  * @param {object} providers.store the cerebral store used for setting the state
  */
 export const setDocumentIsRequiredAction = ({ get, props, store }) => {
