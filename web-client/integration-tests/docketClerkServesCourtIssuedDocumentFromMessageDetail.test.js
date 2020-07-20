273f6b801093fbe7fb8b5ac7d88300c37d503efe
Mrinalini <mrinalinisinha05@gmail.com>
2020-07-20 08:46:06 -0700

    added test around serving court issued order for e filed case from message detail view

---
+++ web-client/integration-tests/docketClerkServesCourtIssuedDocumentFromMessageDetail.test.js
@@ -0,0 +1,61 @@
+import { createNewCaseMessageOnCase } from './journey/createNewCaseMessageOnCase';
+import { docketClerkAddsDocketEntryFromOrder } from './journey/docketClerkAddsDocketEntryFromOrder';
+import { docketClerkAddsDocketEntryFromOrderOfDismissal } from './journey/docketClerkAddsDocketEntryFromOrderOfDismissal';
+import { docketClerkCancelsAddDocketEntryFromOrder } from './journey/docketClerkCancelsAddDocketEntryFromOrder';
+import { docketClerkCreatesAnOrder } from './journey/docketClerkCreatesAnOrder';
+import { docketClerkCreatesCaseMessageFromOrder } from './journey/docketClerkCreatesCaseMessageFromOrder';
+import { docketClerkSignsOrder } from './journey/docketClerkSignsOrder';
+import { docketClerkViewsDraftOrder } from './journey/docketClerkViewsDraftOrder';
+import { fakeFile, loginAs, setupTest } from './helpers';
+import { petitionerChoosesCaseType } from './journey/petitionerChoosesCaseType';
+import { petitionerChoosesProcedureType } from './journey/petitionerChoosesProcedureType';
+import { petitionerCreatesNewCase } from './journey/petitionerCreatesNewCase';
+import { petitionsClerk1ServesOrderFromCaseMessageDetail } from './journey/petitionsClerk1ServesOrderFromCaseMessageDetail';
+import { petitionsClerk1ViewsMessageDetail } from './journey/petitionsClerk1ViewsMessageDetail';
+import { petitionsClerk1ViewsMessageInbox } from './journey/petitionsClerk1ViewsMessageInbox';
+
+const test = setupTest({
+  useCases: {
+    loadPDFForSigningInteractor: () => Promise.resolve(null),
+  },
+});
+test.draftOrders = [];
+
+describe('Docket Clerk Serves Court Issued Document From Message Detail', () => {
+  beforeAll(() => {
+    jest.setTimeout(40000);
+    global.window.pdfjsObj = {
+      getData: () => Promise.resolve(new Uint8Array(fakeFile)),
+    };
+  });
+
+  loginAs(test, 'petitioner@example.com');
+  petitionerChoosesProcedureType(test, { procedureType: 'Regular' });
+  petitionerChoosesCaseType(test);
+  petitionerCreatesNewCase(test, fakeFile);
+
+  loginAs(test, 'docketclerk@example.com');
+  docketClerkCreatesAnOrder(test, {
+    documentTitle: 'Order to do something',
+    eventCode: 'O',
+    expectedDocumentType: 'Order',
+  });
+  docketClerkCreatesAnOrder(test, {
+    documentTitle: 'Order of Dismissal',
+    eventCode: 'OD',
+    expectedDocumentType: 'Order of Dismissal',
+  });
+
+  docketClerkViewsDraftOrder(test, 0);
+  docketClerkSignsOrder(test, 0);
+  docketClerkAddsDocketEntryFromOrder(test, 0);
+  createNewCaseMessageOnCase(test, 0);
+
+  //login as petitionsclerk1
+  loginAs(test, 'petitionsclerk1@example.com');
+  petitionsClerk1ViewsMessageInbox(test);
+  petitionsClerk1ViewsMessageDetail(test);
+  petitionsClerk1ServesOrderFromCaseMessageDetail(test);
+  //view message
+  //serve order
+});
