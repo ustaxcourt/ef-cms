175f5ace56930509609013991df5495954a348dd
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-07-21 12:20:53 -0700

    5811: bug fixes - address feedback

--- web-client/src/views/Messages/MessageDetail.jsx
+++ web-client/src/views/Messages/MessageDetail.jsx
@@ -234,7 +234,7 @@ export const MessageDetail = connect(
                           <div className="grid-col-8">
                             {attachment.documentTitle}
                           </div>
-                          <div className="grid-col-4">
+                          <div className="grid-col-4 padding-left-105">
                             {attachment.showNotServed && (
                               <span className="text-semibold not-served">
                                 Not served
