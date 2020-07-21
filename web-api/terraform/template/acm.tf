56e2bdc7bdc9164e7514be2d44ae6b472c9b0211
Joseph Sutton <jsutton@flexion.us>
2020-07-21 18:19:51 -0500

    new efcms_domain / zone_name variables in frontend /dist/ and api TF modules [skip ci]

--- web-api/terraform/template/acm.tf
+++ web-api/terraform/template/acm.tf
@@ -1,18 +1,18 @@
 
 data "aws_route53_zone" "zone" {
-  name         = "${var.dns_domain}."
+  name         = "${var.zone_name}."
   private_zone = "false"
 }
 
 resource "aws_acm_certificate" "us-east-1" {
-  domain_name       = "efcms-${var.environment}.${var.dns_domain}"
+  domain_name       = var.dns_domain
   validation_method = "DNS"
 
   tags = {
-    Name          = "efcms-${var.environment}.${var.dns_domain}"
+    Name          = var.dns_domain
     ProductDomain = "EFCMS"
     Environment   = var.environment
-    Description   = "Certificate for efcms-${var.environment}.${var.dns_domain}"
+    Description   = "Certificate for ${var.dns_domain}"
     ManagedBy     = "terraform"
   }
 }
@@ -23,18 +23,18 @@ resource "aws_acm_certificate_validation" "dns_validation-us-east-1" {
 }
 
 resource "aws_acm_certificate" "us-west-1" {
-  domain_name       = "efcms-${var.environment}.${var.dns_domain}"
+  domain_name       = var.dns_domain
   validation_method = "DNS"
 
   tags = {
-    Name          = "efcms-${var.environment}.${var.dns_domain}"
+    Name          = var.dns_domain
     ProductDomain = "EFCMS"
     Environment   = var.environment
-    Description   = "Certificate for efcms-${var.environment}.${var.dns_domain}"
+    Description   = "Certificate for ${var.dns_domain}"
     ManagedBy     = "terraform"
   }
 
-  provider       = aws.us-west-1
+  provider = aws.us-west-1
 }
 
 resource "aws_route53_record" "record" {
@@ -44,25 +44,24 @@ resource "aws_route53_record" "record" {
   records = [
     aws_acm_certificate.us-east-1.domain_validation_options.0.resource_record_value,
   ]
-  ttl     = 60
+  ttl = 60
 }
 
 resource "aws_acm_certificate_validation" "dns_validation-us-west-1" {
   certificate_arn         = aws_acm_certificate.us-west-1.arn
   validation_record_fqdns = [aws_route53_record.record.fqdn]
-  provider       = aws.us-west-1
+  provider                = aws.us-west-1
 }
 
-
 resource "aws_acm_certificate" "ws-us-east-1" {
-  domain_name       = "efcms-${var.environment}-ws.${var.dns_domain}"
+  domain_name       = "ws.${var.dns_domain}"
   validation_method = "DNS"
 
   tags = {
-    Name          = "efcms-${var.environment}-ws.${var.dns_domain}"
+    Name          = "ws.${var.dns_domain}"
     ProductDomain = "EFCMS"
     Environment   = var.environment
-    Description   = "Certificate for efcms-${var.environment}-ws.${var.dns_domain}"
+    Description   = "Certificate for ws.${var.dns_domain}"
     ManagedBy     = "terraform"
   }
 }
@@ -79,26 +78,26 @@ resource "aws_route53_record" "ws-record" {
   records = [
     aws_acm_certificate.ws-us-east-1.domain_validation_options.0.resource_record_value,
   ]
-  ttl     = 60
+  ttl = 60
 }
 
 resource "aws_acm_certificate" "ws-us-west-1" {
-  domain_name       = "efcms-${var.environment}-ws.${var.dns_domain}"
+  domain_name       = "ws.${var.dns_domain}"
   validation_method = "DNS"
 
   tags = {
-    Name          = "efcms-${var.environment}-ws.${var.dns_domain}"
+    Name          = "ws.${var.dns_domain}"
     ProductDomain = "EFCMS"
     Environment   = var.environment
-    Description   = "Certificate for efcms-${var.environment}-ws.${var.dns_domain}"
+    Description   = "Certificate for ws.${var.dns_domain}"
     ManagedBy     = "terraform"
   }
 
-  provider       = aws.us-west-1
+  provider = aws.us-west-1
 }
 
 resource "aws_acm_certificate_validation" "ws-dns_validation-us-west-1" {
   certificate_arn         = aws_acm_certificate.ws-us-west-1.arn
   validation_record_fqdns = [aws_route53_record.ws-record.fqdn]
-  provider       = aws.us-west-1
+  provider                = aws.us-west-1
 }
