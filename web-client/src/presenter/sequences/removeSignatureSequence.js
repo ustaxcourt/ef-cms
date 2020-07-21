628181b9bc15b234b3b330e11823a5fb54486c2b
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-07-21 10:29:14 -0700

    some cleanup and tests wip [skip ci]

--- web-client/src/presenter/sequences/removeSignatureSequence.js
+++ web-client/src/presenter/sequences/removeSignatureSequence.js
@@ -1,32 +1,18 @@
 import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { clearModalAction } from '../actions/clearModalAction';
 import { clearModalStateAction } from '../actions/clearModalStateAction';
-// import { convertHtml2PdfSequence } from './convertHtml2PdfSequence';
-// import { navigateToSignOrderAction } from '../actions/navigateToSignOrderAction';
-// import { overwriteOrderFileAction } from '../actions/CourtIssuedOrder/overwriteOrderFileAction';
-// import { setDocumentToEditAction } from '../actions/setDocumentToEditAction';
-// import { setFormFromDraftStateAction } from '../actions/setFormFromDraftStateAction';
-import { setPropsForRemoveSignatureAction } from '../actions/setPropsForRemoveSignatureAction';
-import { showProgressSequenceDecorator } from '../utilities/sequenceHelpers';
-// import { submitCourtIssuedOrderAction } from '../actions/CourtIssuedOrder/submitCourtIssuedOrderAction';
 import { removeSignatureAction } from '../actions/removeSignatureAction';
 import { setCaseAction } from '../actions/setCaseAction';
+import { setPropsForRemoveSignatureAction } from '../actions/setPropsForRemoveSignatureAction';
 import { setViewerDraftDocumentToDisplayAction } from '../actions/setViewerDraftDocumentToDisplayAction';
+import { showProgressSequenceDecorator } from '../utilities/sequenceHelpers';
 
 export const removeSignatureSequence = showProgressSequenceDecorator([
   clearAlertsAction,
   setPropsForRemoveSignatureAction,
-  // setFormFromDraftStateAction,
-  // setDocumentToEditAction,
-  // convertHtml2PdfSequence,
   removeSignatureAction,
   clearModalAction,
   clearModalStateAction,
   setCaseAction,
   setViewerDraftDocumentToDisplayAction,
-  // overwriteOrderFileAction,
-  // {
-  //   error: [],
-  //   success: [submitCourtIssuedOrderAction, navigateToSignOrderAction],
-  // },
 ]);
