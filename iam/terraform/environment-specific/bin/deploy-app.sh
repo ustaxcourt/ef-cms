5d10742cfa65246cce0aac5982b601f2681d5bbb
Cody Seibert <cseibert@flexion.us>
2020-07-22 11:20:07 -0400

    forgot a quote

--- iam/terraform/environment-specific/bin/deploy-app.sh
+++ iam/terraform/environment-specific/bin/deploy-app.sh
@@ -36,4 +36,4 @@ fi
 set -eo pipefail
 
 terraform init -backend=true -backend-config=bucket="${BUCKET}" -backend-config=key="${KEY}" -backend-config=dynamodb_table="${LOCK_TABLE}" -backend-config=region="${REGION}"
-terraform apply -auto-approve -var "environment=${ENVIRONMENT}" -var "dns_domain=${EFCMS_DOMAIN}"
+terraform apply -auto-approve -var "environment=${ENVIRONMENT}"
