09cf607274bc1bdf2a77fcd8ceb1fa6756584d6b
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-07-24 13:07:38 -0700

    DevEx: use docketNumber instead of caseId for document download urls

--- shared/src/proxies/public/getPublicDownloadPolicyUrlProxy.js
+++ shared/src/proxies/public/getPublicDownloadPolicyUrlProxy.js
@@ -5,17 +5,17 @@ const { get } = require('./requests');
  *
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
- * @param {string} providers.caseId the id of the case containing the document
+ * @param {string} providers.docketNumber the docket number of the case containing the document
  * @param {string} providers.documentId the document id
  * @returns {Promise<*>} the promise of the api call
  */
 exports.getPublicDownloadPolicyUrl = ({
   applicationContext,
-  caseId,
+  docketNumber,
   documentId,
 }) => {
   return get({
     applicationContext,
-    endpoint: `/public-api/${caseId}/${documentId}/public-download-policy-url`,
+    endpoint: `/public-api/${docketNumber}/${documentId}/public-download-policy-url`,
   });
 };
