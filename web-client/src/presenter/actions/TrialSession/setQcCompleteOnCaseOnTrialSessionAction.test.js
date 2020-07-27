f7cc05b90a4288fd0693ccc8d5037e173d0e5b26
Rachel Schneiderman <rschneiderman@flexion.us>
2020-07-27 11:55:51 -0700

    Shared and client unit tests passing

--- web-client/src/presenter/actions/TrialSession/setQcCompleteOnCaseOnTrialSessionAction.test.js
+++ web-client/src/presenter/actions/TrialSession/setQcCompleteOnCaseOnTrialSessionAction.test.js
@@ -10,7 +10,7 @@ describe('setQcCompleteOnCaseOnTrialSessionAction', () => {
       },
       props: {
         updatedCase: {
-          caseId: '98dc2266-0f81-4b4b-9ff5-fd02dfb012f7',
+          docketNumber: '123-45',
           qcCompleteForTrial: { 'c2f09db1-24a1-4cff-a0cd-8c7b331f60d8': true },
         },
       },
@@ -18,7 +18,7 @@ describe('setQcCompleteOnCaseOnTrialSessionAction', () => {
         trialSession: {
           eligibleCases: [
             {
-              caseId: '98dc2266-0f81-4b4b-9ff5-fd02dfb012f7',
+              docketNumber: '123-45',
               qcCompleteForTrial: {
                 'c2f09db1-24a1-4cff-a0cd-8c7b331f60d8': false,
               },
@@ -29,7 +29,7 @@ describe('setQcCompleteOnCaseOnTrialSessionAction', () => {
     });
 
     expect(result.state.trialSession.eligibleCases[0]).toEqual({
-      caseId: '98dc2266-0f81-4b4b-9ff5-fd02dfb012f7',
+      docketNumber: '123-45',
       qcCompleteForTrial: {
         'c2f09db1-24a1-4cff-a0cd-8c7b331f60d8': true,
       },
