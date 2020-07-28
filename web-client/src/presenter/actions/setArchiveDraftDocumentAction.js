8affd95fa0cac74b860739d51a06a780f4af079e
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-07-28 10:09:30 -0700

    address pr comments; fix tests

--- web-client/src/presenter/actions/setArchiveDraftDocumentAction.js
+++ web-client/src/presenter/actions/setArchiveDraftDocumentAction.js
@@ -1,7 +1,7 @@
 import { state } from 'cerebral';
 
 /**
- * sets archive draft document state properties (documentId, caseId, and documentTitle)
+ * sets archive draft document state properties (documentId, docketNumber, and documentTitle)
  *
  * @param {object} providers the providers object
  * @param {object} providers.store the cerebral store
--- web-client/src/presenter/actions/setConfirmEditModalStateAction.test.js
+++ web-client/src/presenter/actions/setConfirmEditModalStateAction.test.js
@@ -5,7 +5,6 @@ describe('setConfirmEditModalStateAction', () => {
   it('sets the case and document to display in the modal', async () => {
     const result = await runAction(setConfirmEditModalStateAction, {
       props: {
-        caseId: 'abc-123',
         docketNumber: 'abc-123',
         documentIdToEdit: 'abc-123',
         parentMessageId: '987',
@@ -14,7 +13,6 @@ describe('setConfirmEditModalStateAction', () => {
 
     expect(result.state).toMatchObject({
       modal: {
-        caseId: 'abc-123',
         docketNumber: 'abc-123',
         documentIdToEdit: 'abc-123',
         parentMessageId: '987',
