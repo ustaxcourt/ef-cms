00d3bb223186ac372a1a7334a1723c27187a5339
Mrinalini <mrinalinisinha05@gmail.com>
2020-07-29 11:12:34 -0700

    hiding skip signature button when the pdf to sign is a proposed stip decision

--- web-client/src/presenter/computeds/pdfSignerHelper.js
+++ web-client/src/presenter/computeds/pdfSignerHelper.js
@@ -1,11 +1,23 @@
 import { state } from 'cerebral';
 
-export const pdfSignerHelper = get => {
+export const pdfSignerHelper = (get, applicationContext) => {
+  const {
+    PROPOSED_STIPULATED_DECISION_EVENT_CODE,
+  } = applicationContext.getConstants();
   const form = get(state.form);
-  const signatureData = get(state.pdfForSigning.signatureData);
-  const isPdfAlreadySigned = get(state.pdfForSigning.isPdfAlreadySigned);
-  const signatureApplied = get(state.pdfForSigning.signatureApplied);
+  const caseDetail = get(state.caseDetail);
+  const documentId = get(state.documentId);
+  const pdfForSigning = get(state.pdfForSigning);
+  const { isPdfAlreadySigned, signatureApplied, signatureData } = pdfForSigning;
+  const caseDocument = caseDetail.documents.find(
+    doc => doc.documentId === documentId,
+  );
+
   const isPlaced = signatureData || isPdfAlreadySigned;
+
+  const showSkipSignatureButton =
+    !isPlaced &&
+    caseDocument.eventCode !== PROPOSED_STIPULATED_DECISION_EVENT_CODE;
 
   const cursorClass =
     !signatureData && signatureApplied ? 'cursor-grabbing ' : 'cursor-grab ';
@@ -20,6 +32,7 @@ export const pdfSignerHelper = get => {
       form.assigneeId ||
       form.message,
     isPlaced,
+    showSkipSignatureButton,
     signatureClass: `${cursorClass} ${hideClass}`,
   };
 };
