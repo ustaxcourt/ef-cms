3ac14798544d0a1db98c66863b76356f6e3a617c
Joseph Sutton <jsutton@flexion.us>
2020-07-29 12:32:46 -0500

    remove unnecessary outputs

--- web-api/terraform/main/outputs.tf
+++ web-api/terraform/main/outputs.tf
@@ -1,12 +1,3 @@
-
-output "user_pool_id" {
-  value = module.ef-cms_apis.user_pool_id
-}
-
-output "dynamo_stream_arn" {
-  value = module.ef-cms_apis.dynamo_stream_arn
-}
-
 output "elasticsearch_endpoint" {
   value = module.ef-cms_apis.elasticsearch_endpoint
 }
