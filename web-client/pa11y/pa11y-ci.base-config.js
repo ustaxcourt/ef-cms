9b6f2ef82aab4247c9ee41de40a3c3fdee0c0afd
Kris Koskelin <kkoskelin@flexion.us>
2020-07-29 13:54:35 -0500

    *moves levers*

--- web-client/pa11y/pa11y-ci.base-config.js
+++ web-client/pa11y/pa11y-ci.base-config.js
@@ -16,7 +16,7 @@ const defaults = {
   chromeLaunchConfig: {
     args: ['--no-sandbox'],
   },
-  concurrency: 4,
+  concurrency: 8,
   debug: true,
   'include-notices': true,
   'include-warnings': true,
