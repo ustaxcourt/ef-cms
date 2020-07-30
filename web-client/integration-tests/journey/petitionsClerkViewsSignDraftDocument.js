08b8a67dfad5956bba6557183649cdc9009781ae
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-07-30 12:48:54 -0700

    5511: remove SignStipDecision.jsx

--- web-client/integration-tests/journey/petitionsClerkViewsSignDraftDocument.js
+++ web-client/integration-tests/journey/petitionsClerkViewsSignDraftDocument.js
@@ -1,11 +1,10 @@
 export const petitionsClerkViewsSignDraftDocument = test => {
   return it('Petitions clerk views sign draft document', async () => {
-    await test.runSequence('gotoSignPDFDocumentSequence', {
+    await test.runSequence('gotoSignOrderSequence', {
       docketNumber: test.docketNumber,
       documentId: test.documentId,
-      pageNumber: 1,
     });
 
-    expect(test.getState('currentPage')).toEqual('SignStipDecision');
+    expect(test.getState('currentPage')).toEqual('SignOrder');
   });
 };
