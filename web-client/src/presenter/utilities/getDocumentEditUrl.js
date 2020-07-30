86262770c86dd5141c82c9dd09f1f5656be93193
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-07-30 12:56:12 -0700

    remove references to deleted route

--- web-client/src/presenter/utilities/getDocumentEditUrl.js
+++ web-client/src/presenter/utilities/getDocumentEditUrl.js
@@ -1,15 +1,9 @@
-const getDocumentEditUrl = ({ applicationContext, caseDetail, document }) => {
+const getDocumentEditUrl = ({ caseDetail, document }) => {
   const MISCELLANEOUS_DOCUMENT_TYPE = 'Miscellaneous';
-  const STIPULATED_DECISION_DOCUMENT_TYPE = applicationContext.getConstants()
-    .SIGNED_DOCUMENT_TYPES.signedStipulatedDecision.documentType;
 
-  const isStipDecision =
-    document.documentType === STIPULATED_DECISION_DOCUMENT_TYPE;
   const isMiscellaneous = document.documentType === MISCELLANEOUS_DOCUMENT_TYPE;
 
-  const editUrl = isStipDecision
-    ? `/case-detail/${caseDetail.docketNumber}/documents/${document.documentId}/sign`
-    : isMiscellaneous
+  const editUrl = isMiscellaneous
     ? `/case-detail/${caseDetail.docketNumber}/edit-upload-court-issued/${document.documentId}`
     : `/case-detail/${caseDetail.docketNumber}/edit-order/${document.documentId}`;
 
