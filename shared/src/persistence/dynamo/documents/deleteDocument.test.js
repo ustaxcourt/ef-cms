fe20f384dfdc8a7aa230debef099caf07c61ccba
Mrinalini <mrinalinisinha05@gmail.com>
2020-07-31 07:15:45 -0700

    added unit test for new action

--- shared/src/persistence/dynamo/documents/deleteDocument.test.js
+++ shared/src/persistence/dynamo/documents/deleteDocument.test.js
@@ -1,5 +1,33 @@
+const client = require('../../dynamodbClientService');
+const {
+  applicationContext,
+} = require('../../../business/test/createTestApplicationContext');
+const { deleteDocument } = require('./deleteDocument');
+
+const mockDocumentId = '9b52c605-edba-41d7-b045-d5f992a499d3';
+const mockDocketNumber = '999-99';
+
 describe('deleteDocument', () => {
-  it('should fail because I havent added any tests', async () => {
-    expect(true).toBe(false);
+  beforeAll(() => {
+    applicationContext.filterCaseMetadata.mockImplementation(
+      ({ cases }) => cases,
+    );
+
+    client.delete = jest.fn();
+  });
+
+  it('makes a delete request with the given document data for the matching document id', async () => {
+    await deleteDocument({
+      applicationContext,
+      docketNumber: mockDocketNumber,
+      documentId: mockDocumentId,
+    });
+
+    expect(client.delete.mock.calls[0][0]).toMatchObject({
+      key: {
+        pk: `case|${mockDocketNumber}`,
+        sk: `document|${mockDocumentId}`,
+      },
+    });
   });
 });
