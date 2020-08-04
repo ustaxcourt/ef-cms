e35bbf68d0bbaf3b6e6e7d8983331e0703754da6
Mrinalini <mrinalinisinha05@gmail.com>
2020-08-04 11:57:10 -0700

    addressing bug with attachment not being selected when replying to or forwarding a message with no prior attachments

--- web-client/src/presenter/actions/CaseDetail/replyToMessageAction.js
+++ web-client/src/presenter/actions/CaseDetail/replyToMessageAction.js
@@ -1,5 +1,13 @@
 import { state } from 'cerebral';
 
+/**
+ * replies to the message
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context
+ * @param {Function} providers.get the cerebral get function
+ * @returns {object} contains the alert success message and parent message ID
+ */
 export const replyToMessageAction = async ({ applicationContext, get }) => {
   const form = get(state.modal.form);
 
@@ -18,5 +26,8 @@ export const replyToMessageAction = async ({ applicationContext, get }) => {
       message: 'Your message has been sent.',
     },
     parentMessageId,
+    viewerDocumentToDisplay: {
+      documentId: form.attachments[0].documentId,
+    },
   };
 };
