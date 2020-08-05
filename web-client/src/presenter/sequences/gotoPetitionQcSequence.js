1c0b0b3b6983fb6b11451cdcc49d4b8adb8e06ff
Rachel Schneiderman <rschneiderman@flexion.us>
2020-08-05 12:06:48 -0700

    4654 Clear documentSelectedForPreview after leaving document qc. Fixing tests.

--- web-client/src/presenter/sequences/gotoPetitionQcSequence.js
+++ web-client/src/presenter/sequences/gotoPetitionQcSequence.js
@@ -3,12 +3,13 @@ import { getCaseAction } from '../actions/getCaseAction';
 import { setCaseAction } from '../actions/setCaseAction';
 import { setCaseOnFormAction } from '../actions/setCaseOnFormAction';
 import { setCurrentPageAction } from '../actions/setCurrentPageAction';
-import { setDefaultFormDocumentForPreviewAction } from '../actions/setDefaultFormDocumentForPreviewAction';
+import { setDefaultDocumentSelectedForPreviewAction } from '../actions/setDefaultDocumentSelectedForPreviewAction';
 import { setDocumentDetailTabAction } from '../actions/setDocumentDetailTabAction';
 import { setDocumentForPreviewSequence } from '../sequences/setDocumentForPreviewSequence';
 import { setFormForCaseAction } from '../actions/setFormForCaseAction';
 import { setRedirectUrlAction } from '../actions/setRedirectUrlAction';
 import { stopShowValidationAction } from '../actions/stopShowValidationAction';
+import { unsetDocumentSelectedForPreviewAction } from '../actions/unsetDocumentSelectedForPreviewAction';
 
 export const gotoPetitionQcSequence = [
   setCurrentPageAction('Interstitial'),
@@ -20,7 +21,8 @@ export const gotoPetitionQcSequence = [
   setCaseAction,
   setCaseOnFormAction,
   setFormForCaseAction,
-  setDefaultFormDocumentForPreviewAction,
+  unsetDocumentSelectedForPreviewAction,
+  setDefaultDocumentSelectedForPreviewAction,
   setDocumentForPreviewSequence,
   setCurrentPageAction('PetitionQc'),
 ];
