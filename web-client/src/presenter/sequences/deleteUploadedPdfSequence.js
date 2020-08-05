5f66f5df5be0d176e5267efc719a545a2cf317aa
Rachel Schneiderman <rschneiderman@flexion.us>
2020-08-04 21:34:38 -0700

    Clear completed check mark on document tab when documents other than petition are deleted

--- web-client/src/presenter/sequences/deleteUploadedPdfSequence.js
+++ web-client/src/presenter/sequences/deleteUploadedPdfSequence.js
@@ -4,11 +4,13 @@ import { deleteUploadedPdfAction } from '../actions/deleteUploadedPdfAction';
 import { getFormValueDocumentAction } from '../actions/getFormValueDocumentAction';
 import { setCaseAction } from '../actions/setCaseAction';
 import { setDocumentUploadModeSequence } from './setDocumentUploadModeSequence';
+import { setFormValueAction } from '../actions/setFormValueAction';
 import { updateOrderForDesignatingPlaceOfTrialAction } from '../actions/updateOrderForDesignatingPlaceOfTrialAction';
 import { updateOrderForOdsAction } from '../actions/StartCaseInternal/updateOrderForOdsAction';
 
 export const deleteUploadedPdfSequence = [
   deleteUploadedPdfAction,
+  setFormValueAction,
   clearPdfPreviewUrlAction,
   setDocumentUploadModeSequence,
   clearModalSequence,
