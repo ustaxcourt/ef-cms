de71507f119f51a77e2b6be207dc008081f3c93c
Mrinalini <mrinalinisinha05@gmail.com>
2020-08-05 09:58:38 -0700

    adding, removing and readding pdfs during create a case working (not being saved in memory yet)

--- web-client/src/presenter/actions/petitionQcShouldShowPreviewAction.js
+++ web-client/src/presenter/actions/petitionQcShouldShowPreviewAction.js
@@ -1,6 +1,7 @@
 import { state } from 'cerebral';
 
 /**
+ * fixme
  * returns the yes path with file prop for the current document selected for scan, or the no path, otherwise
  *
  * @param {object} providers the providers object
@@ -8,11 +9,69 @@ import { state } from 'cerebral';
  * @param {object} providers.path the next object in the path
  * @returns {undefined}
  */
-export const petitionQcShouldShowPreviewAction = ({ get, path }) => {
+export const petitionQcShouldShowPreviewAction = async ({
+  applicationContext,
+  get,
+  path,
+}) => {
   const documentSelectedForPreview = get(
     state.currentViewMetadata.documentSelectedForPreview,
   );
+
+  console.log('documentSelectedForPreview', documentSelectedForPreview);
+
+  const { INITIAL_DOCUMENT_TYPES } = applicationContext.getConstants();
+
+  const documentTypeMap = {
+    applicationForWaiverOfFilingFeeFile:
+      INITIAL_DOCUMENT_TYPES.applicationForWaiverOfFilingFee.documentType,
+    ownershipDisclosureFile:
+      INITIAL_DOCUMENT_TYPES.ownershipDisclosure.documentType,
+    petitionFile: INITIAL_DOCUMENT_TYPES.petition.documentType,
+    requestForPlaceOfTrialFile:
+      INITIAL_DOCUMENT_TYPES.requestForPlaceOfTrial.documentType,
+    stinFile: INITIAL_DOCUMENT_TYPES.stin.documentType,
+  };
+
+  const documentTypeSelectedForPreview =
+    documentTypeMap[documentSelectedForPreview];
+
+  const { docketNumber, documents } = get(state.form);
+
+  const selectedDocument = get(documents).find(
+    document => document.documentType === documentTypeSelectedForPreview,
+  );
+
+  let pdfUrl;
+  let selectedDocumentId;
+
+  if (selectedDocument) {
+    const {
+      url,
+    } = await applicationContext
+      .getUseCases()
+      .getDocumentDownloadUrlInteractor({
+        applicationContext,
+        docketNumber,
+        documentId: selectedDocument.documentId,
+      });
+
+    pdfUrl = url;
+    selectedDocumentId = selectedDocument.documentId;
+  }
+
+  //should be getting from form.documents????
+  //check if pdf is on form aka in memory
+  // if not,
   const file = get(state.form[documentSelectedForPreview]);
 
-  return file ? path.yes({ file }) : path.no();
+  if (file) {
+    return path.pdfInMemory({ file });
+  }
+
+  if (selectedDocument) {
+    return path.pdfInS3({ documentId: selectedDocumentId, pdfUrl });
+  }
+
+  return path.no();
 };
--- web-client/src/presenter/sequences/petitionQcSelectDocumentForScanSequence.js
+++ web-client/src/presenter/sequences/petitionQcSelectDocumentForScanSequence.js
@@ -1,17 +1,23 @@
 import { petitionQcShouldShowPreviewAction } from '../actions/petitionQcShouldShowPreviewAction';
 import { selectDocumentForPreviewAction } from '../actions/selectDocumentForPreviewAction';
 import { set } from 'cerebral/factories';
+import { setDocumentIdAction } from '../actions/setDocumentIdAction';
+import { setPdfPreviewUrlAction } from '../actions/CourtIssuedOrder/setPdfPreviewUrlAction';
 import { setPdfPreviewUrlSequence } from './setPdfPreviewUrlSequence';
 import { state } from 'cerebral';
 
 export const petitionQcSelectDocumentForScanSequence = [
-  set(state.currentViewMetadata.documentUploadMode, 'scan'),
   petitionQcShouldShowPreviewAction,
   {
-    no: [],
-    yes: [
+    no: [set(state.currentViewMetadata.documentUploadMode, 'scan')],
+    pdfInMemory: [
       selectDocumentForPreviewAction,
       setPdfPreviewUrlSequence,
+      set(state.currentViewMetadata.documentUploadMode, 'preview'),
+    ],
+    pdfInS3: [
+      setPdfPreviewUrlAction,
+      setDocumentIdAction,
       set(state.currentViewMetadata.documentUploadMode, 'preview'),
     ],
   },
