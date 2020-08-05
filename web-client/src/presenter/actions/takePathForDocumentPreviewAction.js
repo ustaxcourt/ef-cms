663cfcc20edab46ab1943f565422a0c8f3d52e9c
Mrinalini <mrinalinisinha05@gmail.com>
2020-08-05 10:59:48 -0700

    [skip ci] code cleanup

--- web-client/src/presenter/actions/takePathForDocumentPreviewAction.js
+++ web-client/src/presenter/actions/takePathForDocumentPreviewAction.js
@@ -1,15 +1,12 @@
-import { state } from 'cerebral';
-
 /**
- * fixme
- * returns the yes path with file prop for the current document selected for scan, or the no path, otherwise
+ * returns the path to take based on teh available props for where a document is located
  *
  * @param {object} providers the providers object
  * @param {object} providers.props the cerebral get method
  * @param {object} providers.path the next object in the path
- * @returns {undefined}
+ * @returns {object} path execution results
  */
-export const petitionQcShouldShowPreviewAction = async ({ path, props }) => {
+export const takePathForDocumentPreviewAction = async ({ path, props }) => {
   if (props.fileFromBrowserMemory) {
     return path.pdfInMemory({ file: props.fileFromBrowserMemory });
   }
