fb8b1f4edcaa1151e80e02c9629aa4d694e21f60
Michael Marcotte <mike.marcotte@gmail.com>
2020-08-05 14:28:03 -0400

    fix statement; add policy and log group resource

--- web-api/terraform/template/elasticsearch.tf
+++ web-api/terraform/template/elasticsearch.tf
@@ -1,3 +1,31 @@
+resource "aws_cloudwatch_log_group" "elasticsearch_application_logs" {
+  name = "/aws/aes/debug_${var.environment}"
+}
+
+resource "aws_cloudwatch_log_resource_policy" "allow_elasticsearch_to_write_logs" {
+  policy_name = "allow_elasticsearch_to_write_logs"
+
+  policy_document = <<CONFIG
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "es.amazonaws.com"
+      },
+      "Action": [
+        "logs:PutLogEvents",
+        "logs:PutLogEventsBatch",
+        "logs:CreateLogStream"
+      ],
+      "Resource": "arn:aws:logs:*"
+    }
+  ]
+}
+CONFIG
+}
+
 resource "aws_elasticsearch_domain" "efcms-search" {
   domain_name           = "efcms-search-${var.environment}"
   elasticsearch_version = "7.4"
@@ -16,9 +44,8 @@ resource "aws_elasticsearch_domain" "efcms-search" {
     automated_snapshot_start_hour = 23
   }
 
-  log_publishing_options = {
-    enabled                  = "true"
-    cloudwatch_log_group_arn = "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/aes/debug_${var.environment}"
+  log_publishing_options {
+    cloudwatch_log_group_arn = aws_cloudwatch_log_group.elasticsearch_application_logs.arn
     log_type                 = "ES_APPLICATION_LOGS"
   }
 }
