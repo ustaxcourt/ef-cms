92269b0be4f3669f5fe3c7baaf288bb014c24560
Mrinalini <mrinalinisinha05@gmail.com>
2020-08-06 13:23:20 -0700

    fixed failing test

--- shared/src/business/useCases/serveCaseToIrs/serveCaseToIrsInteractor.test.js
+++ shared/src/business/useCases/serveCaseToIrs/serveCaseToIrsInteractor.test.js
@@ -395,10 +395,26 @@ describe('addDocketEntryForPaymentStatus', () => {
     expect(addedDocketRecord.filingDate).toEqual('Today');
   });
 
-  it('should add a docket entry for all intiially filed documents except for the petition', async () => {
+  it('should add a docket entry for all intiially filed documents except for the petition and stin file', async () => {
     const mockCase = {
       ...MOCK_CASE,
       docketRecord: {},
+      documents: [
+        ...MOCK_CASE.documents,
+        {
+          createdAt: '2018-11-21T20:49:28.192Z',
+          docketNumber: '101-18',
+          documentId: 'abc81f4d-1e47-423a-8caf-6d2fdc3d3859',
+          documentTitle:
+            INITIAL_DOCUMENT_TYPES.requestForPlaceOfTrial.documentTitle,
+          documentType:
+            INITIAL_DOCUMENT_TYPES.requestForPlaceOfTrial.documentType,
+          eventCode: INITIAL_DOCUMENT_TYPES.requestForPlaceOfTrial.eventCode,
+          isFileAttached: true,
+          processingStatus: 'pending',
+          userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+        },
+      ],
       isPaper: true,
       mailingDate: 'some day',
     };
@@ -429,11 +445,11 @@ describe('addDocketEntryForPaymentStatus', () => {
         .caseToUpdate.docketRecord[0],
     ).toMatchObject({
       action: undefined,
-      description: 'Statement of Taxpayer Identification',
+      description: INITIAL_DOCUMENT_TYPES.requestForPlaceOfTrial.documentTitle,
       documentId: 'abc81f4d-1e47-423a-8caf-6d2fdc3d3859',
       editState: undefined,
       entityName: 'DocketRecord',
-      eventCode: 'STIN',
+      eventCode: INITIAL_DOCUMENT_TYPES.requestForPlaceOfTrial.eventCode,
       index: 1,
     });
   });
