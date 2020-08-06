24d3b8c784be1c5272e10f2eb040ca94a49b5ef8
Joseph Sutton <jsutton@flexion.us>
2020-08-06 14:00:24 -0500

    fixed range styling

--- web-client/src/views/CaseDeadlines/DateRangePickerComponent.jsx
+++ web-client/src/views/CaseDeadlines/DateRangePickerComponent.jsx
@@ -37,8 +37,8 @@ export const DateRangePickerComponent = ({
   }, [startDateInputRef, endDateInputRef]);
 
   return (
-    <>
-      <div className="usa-date-range-picker" ref={dateRangePickerRef}>
+    <div ref={dateRangePickerRef}>
+      <div className="usa-date-range-picker">
         <div className="usa-form-group" ref={startDatePickerRef}>
           <label
             className="usa-label"
@@ -85,6 +85,6 @@ export const DateRangePickerComponent = ({
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
