8cbeb1880d1c2c88f5c1e2bb9bce7accd4c4dd5d
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-08-07 13:24:32 -0700

    jsdoc update

--- web-client/src/presenter/actions/EditDocketRecord/setPdfPreviewUrlForCompleteDocketEntryAction.js
+++ web-client/src/presenter/actions/EditDocketRecord/setPdfPreviewUrlForCompleteDocketEntryAction.js
@@ -1,10 +1,11 @@
 import { state } from 'cerebral';
 
 /**
- * sets the current docket entry data for edit
+ * sets the pdfPreviewUrl for the attached document on the complete docket entry view
  *
  * @param {object} providers the providers object
- * @param {object} providers.get the cerebral get method
+ * @param {object} providers.applicationContext the application context
+ * @param {Function} providers.get the cerebral get function
  * @param {object} providers.props the cerebral props object
  * @param {object} providers.store the cerebral store
  * @returns {void} sets state for docket entry edit
