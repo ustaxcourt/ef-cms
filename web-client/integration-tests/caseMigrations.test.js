a8d5f222ec602ef4090034adbf5da15462612a07
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-08-07 07:47:40 -0700

    5507: refresh elasticsearch index instead of waiting

--- web-client/integration-tests/caseMigrations.test.js
+++ web-client/integration-tests/caseMigrations.test.js
@@ -1,6 +1,6 @@
 import { MOCK_CASE } from '../../shared/src/test/mockCase.js';
 import { applicationContextForClient as applicationContext } from '../../shared/src/business/test/createTestApplicationContext';
-import { loginAs, refreshElasticsearchIndex, setupTest, wait } from './helpers';
+import { loginAs, refreshElasticsearchIndex, setupTest } from './helpers';
 import axios from 'axios';
 
 const test = setupTest();
@@ -324,7 +324,7 @@ describe('Case journey', () => {
   });
 
   it('Docketclerk searches for correspondence case by updated petitioner name and finds it', async () => {
-    await wait(15000);
+    await refreshElasticsearchIndex();
 
     await test.runSequence('gotoAdvancedSearchSequence');
 
