9e79f45935cd3ea26d73f32201b742ec634ff4b4
Cody Seibert <cseibert@flexion.us>
2020-08-10 10:35:30 -0400

    5993 - pr comments

--- web-client/src/presenter/actions/CaseDeadline/updateDateFromPickerAction.js
+++ web-client/src/presenter/actions/CaseDeadline/updateDateFromPickerAction.js
@@ -16,32 +16,24 @@ export const updateDateFromPickerAction = ({
   const filterStartDate = props.startDate;
   const filterEndDate = props.endDate;
 
-  if (filterStartDate !== undefined) {
-    if (filterStartDate) {
-      const formattedFilterStartDate = applicationContext
-        .getUtilities()
-        .createISODateString(filterStartDate, 'YYYY-MM-DD');
-      store.set(
-        state.screenMetadata.filterStartDateState,
-        formattedFilterStartDate,
-      );
-    } else {
-      store.unset(state.screenMetadata.filterStartDateState);
-    }
+  if (filterStartDate === '') {
+    store.unset(state.screenMetadata.filterStartDateState);
+  } else if (filterStartDate !== undefined) {
+    const formattedFilterStartDate = applicationContext
+      .getUtilities()
+      .createISODateString(filterStartDate, 'YYYY-MM-DD');
+    store.set(
+      state.screenMetadata.filterStartDateState,
+      formattedFilterStartDate,
+    );
   }
+  if (filterEndDate === '') {
+    store.unset(state.screenMetadata.filterEndDateState);
+  } else if (filterEndDate !== undefined) {
+    const formattedFilterEndDate = applicationContext
+      .getUtilities()
+      .createISODateString(filterEndDate, 'YYYY-MM-DD');
 
-  if (filterEndDate !== undefined) {
-    if (filterEndDate) {
-      const formattedFilterEndDate = applicationContext
-        .getUtilities()
-        .createISODateString(filterEndDate, 'YYYY-MM-DD');
-
-      store.set(
-        state.screenMetadata.filterEndDateState,
-        formattedFilterEndDate,
-      );
-    } else {
-      store.unset(state.screenMetadata.filterEndDateState);
-    }
+    store.set(state.screenMetadata.filterEndDateState, formattedFilterEndDate);
   }
 };
