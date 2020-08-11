441ab70fe131e9a6e637ed90e89e396931d1ebb1
Mrinalini <mrinalinisinha05@gmail.com>
2020-08-11 14:37:33 -0700

    more refactoring

--- web-client/src/presenter/computeds/workQueueSectionHelper.test.js
+++ web-client/src/presenter/computeds/workQueueSectionHelper.test.js
@@ -8,7 +8,10 @@ const workQueueSectionHelper = withAppContextDecorator(
   applicationContext,
 );
 
-const { CLERK_OF_COURT_SECTION } = applicationContext.getConstants();
+const {
+  CLERK_OF_COURT_SECTION,
+  URDAS_CHAMBERS_SECTION,
+} = applicationContext.getConstants();
 
 describe('workQueueSectionHelper', () => {
   it('returns the expected state when set', () => {
@@ -16,15 +19,21 @@ describe('workQueueSectionHelper', () => {
       workQueueSectionHelper,
     );
     expect(sectionDisplay(CLERK_OF_COURT_SECTION)).toBe('Clerk of the Court');
-    expect(chambersDisplay('urdasChambers')).toBe('Urda’s Chambers');
+    expect(
+      chambersDisplay(URDAS_CHAMBERS_SECTION.ARMENS_CHAMBERS_SECTION.section),
+    ).toBe(URDAS_CHAMBERS_SECTION.ARMENS_CHAMBERS_SECTION.label);
   });
 
   it('returns the chambers display for section display if the section is a chambers', () => {
     const { chambersDisplay, sectionDisplay } = runCompute(
       workQueueSectionHelper,
     );
-    expect(sectionDisplay('urdasChambers')).toBe('Urda’s Chambers');
-    expect(chambersDisplay('urdasChambers')).toBe('Urda’s Chambers');
+    expect(
+      sectionDisplay(URDAS_CHAMBERS_SECTION.ARMENS_CHAMBERS_SECTION.section),
+    ).toBe(URDAS_CHAMBERS_SECTION.ARMENS_CHAMBERS_SECTION.label);
+    expect(
+      chambersDisplay(URDAS_CHAMBERS_SECTION.ARMENS_CHAMBERS_SECTION.section),
+    ).toBe(URDAS_CHAMBERS_SECTION.ARMENS_CHAMBERS_SECTION.label);
   });
 
   it('returns undefined for sectionDisplay if the section is not a regular section or a chambers', () => {
