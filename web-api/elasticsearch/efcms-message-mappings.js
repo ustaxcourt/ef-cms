3685dfe40a49b7d2744b869d1bdc575e8917e036
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-08-11 08:34:06 -0700

    fix mappings for efcms-message

--- web-api/elasticsearch/efcms-message-mappings.js
+++ web-api/elasticsearch/efcms-message-mappings.js
@@ -1,88 +1,86 @@
 module.exports = {
-  mappings: {
-    dynamic: false,
-    properties: {
-      'attachments.L.M.documentId': {
-        type: 'text',
-      },
-      'attachments.L.M.documentTitle': {
-        type: 'text',
-      },
-      'caseId.S': {
-        type: 'text',
-      },
-      'caseStatus.S': {
-        type: 'text',
-      },
-      'caseTitle.S': {
-        type: 'text',
-      },
-      'completedAt.S': {
-        type: 'text',
-      },
-      'completedBySection.S': {
-        type: 'text',
-      },
-      'completedByUserId.S': {
-        type: 'text',
-      },
-      'completedMessage.S': {
-        type: 'text',
-      },
-      'createdAt.S': {
-        type: 'text',
-      },
-      'docketNumber.S': {
-        type: 'text',
-      },
-      'docketNumberWithSuffix.S': {
-        type: 'text',
-      },
-      'entityName.S': {
-        type: 'text',
-      },
-      'from.S': {
-        type: 'text',
-      },
-      'gsi1pk.S': {
-        type: 'text',
-      },
-      'indexedTimestamp.N': {
-        type: 'text',
-      },
-      'isCompleted.BOOL': {
-        type: 'boolean',
-      },
-      'isRepliedTo.BOOL': {
-        type: 'boolean',
-      },
-      'message.S': {
-        type: 'text',
-      },
-      'messageId.S': {
-        type: 'text',
-      },
-      'parentMessageId.S': {
-        type: 'text',
-      },
-      'pk.S': {
-        type: 'text',
-      },
-      'sk.S': {
-        type: 'text',
-      },
-      'subject.S': {
-        type: 'text',
-      },
-      'to.S': {
-        type: 'text',
-      },
-      'toSection.S': {
-        type: 'text',
-      },
-      'toUserId.S': {
-        type: 'text',
-      },
+  dynamic: false,
+  properties: {
+    'attachments.L.M.documentId.S': {
+      type: 'text',
+    },
+    'attachments.L.M.documentTitle.S': {
+      type: 'text',
+    },
+    'caseId.S': {
+      type: 'text',
+    },
+    'caseStatus.S': {
+      type: 'text',
+    },
+    'caseTitle.S': {
+      type: 'text',
+    },
+    'completedAt.S': {
+      type: 'text',
+    },
+    'completedBySection.S': {
+      type: 'text',
+    },
+    'completedByUserId.S': {
+      type: 'text',
+    },
+    'completedMessage.S': {
+      type: 'text',
+    },
+    'createdAt.S': {
+      type: 'text',
+    },
+    'docketNumber.S': {
+      type: 'text',
+    },
+    'docketNumberWithSuffix.S': {
+      type: 'text',
+    },
+    'entityName.S': {
+      type: 'text',
+    },
+    'from.S': {
+      type: 'text',
+    },
+    'gsi1pk.S': {
+      type: 'text',
+    },
+    'indexedTimestamp.N': {
+      type: 'text',
+    },
+    'isCompleted.BOOL': {
+      type: 'boolean',
+    },
+    'isRepliedTo.BOOL': {
+      type: 'boolean',
+    },
+    'message.S': {
+      type: 'text',
+    },
+    'messageId.S': {
+      type: 'text',
+    },
+    'parentMessageId.S': {
+      type: 'text',
+    },
+    'pk.S': {
+      type: 'text',
+    },
+    'sk.S': {
+      type: 'text',
+    },
+    'subject.S': {
+      type: 'text',
+    },
+    'to.S': {
+      type: 'text',
+    },
+    'toSection.S': {
+      type: 'text',
+    },
+    'toUserId.S': {
+      type: 'text',
     },
   },
 };
