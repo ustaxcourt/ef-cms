ddb192ff4d1b73a529d9da9980b14040ccfe12ae
Rachel Schneiderman <rschneiderman@flexion.us>
2020-08-12 13:53:12 -0700

    4654 - Added integration test for removing/readding petition file

--- web-client/integration-tests/petitionsClerkQCPaperFiledPetition.test.js
+++ web-client/integration-tests/petitionsClerkQCPaperFiledPetition.test.js
@@ -2,6 +2,7 @@ import { fakeFile, loginAs, setupTest } from './helpers';
 import { petitionsClerkCreatesNewCaseFromPaper } from './journey/petitionsClerkCreatesNewCaseFromPaper';
 import { petitionsClerkEditsSavedPetition } from './journey/petitionsClerkEditsSavedPetition';
 import { petitionsClerkRemovesAndReaddsPdfFromPetition } from './journey/petitionsClerkRemovesAndReaddsPdfFromPetition';
+import { petitionsClerkRemovesAndReaddsPetitionFile } from './journey/petitionsClerkRemovesAndReaddsPetitionFile';
 import { petitionsClerkReviewsPetitionAndSavesForLater } from './journey/petitionsClerkReviewsPetitionAndSavesForLater';
 import { petitionsClerkViewsSectionInProgress } from './journey/petitionsClerkViewsSectionInProgress';
 
@@ -16,6 +17,8 @@ describe('Petitions Clerk QCs Paper Filed Petition', () => {
   petitionsClerkCreatesNewCaseFromPaper(test, fakeFile);
   petitionsClerkReviewsPetitionAndSavesForLater(test);
   petitionsClerkViewsSectionInProgress(test);
+  petitionsClerkEditsSavedPetition(test);
+  petitionsClerkRemovesAndReaddsPetitionFile(test, fakeFile);
   petitionsClerkEditsSavedPetition(test);
   petitionsClerkRemovesAndReaddsPdfFromPetition(test, fakeFile);
 });
