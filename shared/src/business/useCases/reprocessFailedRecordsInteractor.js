7768c222f0a9ab6f9263a50bd26ea94fcd010cc9
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-08-12 14:08:06 -0700

    DevEx: clean up code that is no longer needed after disabling dynamic mappings

--- shared/src/business/useCases/reprocessFailedRecordsInteractor.js
+++ shared/src/business/useCases/reprocessFailedRecordsInteractor.js
@@ -1,75 +1,5 @@
 const { createISODateString } = require('../utilities/DateHandler');
 
-const checkSearchClientMappings = async ({ applicationContext, index }) => {
-  /**
-   * recursively searches the provided object for the provided key
-   * and returns the count of instances of that key
-   *
-   * @param {object} object the object to search
-   * @param {string} key the key to search for
-   * @returns {number} the total number of key matches
-   */
-  function countValues(object, key) {
-    let count = 0;
-    Object.keys(object).forEach(k => {
-      if (k === key) {
-        count++;
-      }
-      if (object[k] && typeof object[k] === 'object') {
-        const countToAdd = countValues(object[k], key);
-        count = count + countToAdd;
-      }
-    });
-    return count;
-  }
-
-  const fields = await applicationContext
-    .getPersistenceGateway()
-    .getIndexMappingFields({
-      applicationContext,
-      index,
-    });
-
-  const mappingLimit = await applicationContext
-    .getPersistenceGateway()
-    .getIndexMappingLimit({
-      applicationContext,
-      index,
-    });
-
-  let totalTypes = 0;
-  let fieldText = '';
-
-  for (let field of Object.keys(fields)) {
-    const typeMatches = countValues(fields[field], 'type');
-
-    if (typeMatches > 50) {
-      fieldText += `${field}: ${typeMatches}, `;
-    }
-    totalTypes += typeMatches;
-  }
-
-  if (fieldText !== '') {
-    await applicationContext.notifyHoneybadger(
-      `Warning: Search Client creating greater than 50 indexes for ${index} on the following fields: ${fieldText.substring(
-        0,
-        fieldText.length - 2,
-      )}`,
-    );
-  }
-
-  const currentPercent = (totalTypes / mappingLimit) * 100;
-  if (currentPercent >= 75) {
-    await applicationContext.notifyHoneybadger(
-      `Warning: Search Client Mappings have reached the 75% threshold for ${index} - currently ${currentPercent}%`,
-    );
-  } else if (currentPercent >= 50) {
-    await applicationContext.notifyHoneybadger(
-      `Warning: Search Client Mappings have reached the 50% threshold for ${index} - currently ${currentPercent}%`,
-    );
-  }
-};
-
 /**
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
@@ -77,14 +7,6 @@ const checkSearchClientMappings = async ({ applicationContext, index }) => {
  */
 exports.reprocessFailedRecordsInteractor = async ({ applicationContext }) => {
   applicationContext.logger.info('Time', createISODateString());
-
-  // Check mapping counts
-  const elasticsearchIndexes = applicationContext.getElasticsearchIndexes();
-  await Promise.all(
-    elasticsearchIndexes.map(index =>
-      checkSearchClientMappings({ applicationContext, index }),
-    ),
-  );
 
   const recordsToProcess = await applicationContext
     .getPersistenceGateway()
