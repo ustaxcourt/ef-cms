b05f5cca899647098cb9f17f287f1b003eefc084
Rachel Schneiderman <rschneiderman@flexion.us>
2020-08-12 11:51:18 -0700

    Fixing tests

--- web-client/src/presenter/computeds/workQueueSectionHelper.test.js
+++ web-client/src/presenter/computeds/workQueueSectionHelper.test.js
@@ -3,41 +3,43 @@ import { runCompute } from 'cerebral/test';
 import { withAppContextDecorator } from '../../withAppContext';
 import { workQueueSectionHelper as workQueueSectionHelperComputed } from './workQueueSectionHelper';
 
-const workQueueSectionHelper = withAppContextDecorator(
-  workQueueSectionHelperComputed,
-  applicationContext,
-);
-
-const {
-  CLERK_OF_COURT_SECTION,
-  URDAS_CHAMBERS_SECTION,
-} = applicationContext.getConstants();
-
 describe('workQueueSectionHelper', () => {
+  const workQueueSectionHelper = withAppContextDecorator(
+    workQueueSectionHelperComputed,
+    applicationContext,
+  );
+
+  const { JUDGES_CHAMBERS } = applicationContext.getConstants();
+
   it('returns the expected state when set', () => {
     const { chambersDisplay, sectionDisplay } = runCompute(
       workQueueSectionHelper,
     );
-    expect(sectionDisplay(CLERK_OF_COURT_SECTION)).toBe('Clerk of the Court');
+
+    expect(sectionDisplay(JUDGES_CHAMBERS.URDAS_CHAMBERS_SECTION.section)).toBe(
+      JUDGES_CHAMBERS.URDAS_CHAMBERS_SECTION.label,
+    );
     expect(
-      chambersDisplay(URDAS_CHAMBERS_SECTION.ARMENS_CHAMBERS_SECTION.section),
-    ).toBe(URDAS_CHAMBERS_SECTION.ARMENS_CHAMBERS_SECTION.label);
+      chambersDisplay(JUDGES_CHAMBERS.URDAS_CHAMBERS_SECTION.section),
+    ).toBe(JUDGES_CHAMBERS.URDAS_CHAMBERS_SECTION.label);
   });
 
   it('returns the chambers display for section display if the section is a chambers', () => {
     const { chambersDisplay, sectionDisplay } = runCompute(
       workQueueSectionHelper,
     );
+
+    expect(sectionDisplay(JUDGES_CHAMBERS.URDAS_CHAMBERS_SECTION.section)).toBe(
+      JUDGES_CHAMBERS.URDAS_CHAMBERS_SECTION.label,
+    );
     expect(
-      sectionDisplay(URDAS_CHAMBERS_SECTION.ARMENS_CHAMBERS_SECTION.section),
-    ).toBe(URDAS_CHAMBERS_SECTION.ARMENS_CHAMBERS_SECTION.label);
-    expect(
-      chambersDisplay(URDAS_CHAMBERS_SECTION.ARMENS_CHAMBERS_SECTION.section),
-    ).toBe(URDAS_CHAMBERS_SECTION.ARMENS_CHAMBERS_SECTION.label);
+      chambersDisplay(JUDGES_CHAMBERS.URDAS_CHAMBERS_SECTION.section),
+    ).toBe(JUDGES_CHAMBERS.URDAS_CHAMBERS_SECTION.label);
   });
 
   it('returns undefined for sectionDisplay if the section is not a regular section or a chambers', () => {
     const { sectionDisplay } = runCompute(workQueueSectionHelper);
+
     expect(sectionDisplay('something')).toBeUndefined();
   });
 });
