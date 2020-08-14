b6c8d6bfdfaddd44a75a64e5dbac188a73c2d7c6
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-08-14 13:33:10 -0700

    correctly set up mapping template for integration response for async lambda

--- web-api/terraform/template/lambdas/api.js
+++ web-api/terraform/template/lambdas/api.js
@@ -3,5 +3,7 @@ const { app } = require('../../../src/app');
 const server = awsServerlessExpress.createServer(app);
 
 exports.handler = (event, context) => {
+  event.body =
+    typeof event.body === 'string' ? event.body : JSON.stringify(event.body);
   awsServerlessExpress.proxy(server, event, context);
 };
