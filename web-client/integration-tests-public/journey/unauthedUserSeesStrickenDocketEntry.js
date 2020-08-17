fc13ff2f426bbc220594a6204cab5f36130efa70
Joseph Sutton <jsutton@flexion.us>
2020-08-17 15:02:46 -0500

    #5676: public automated test script

---
+++ web-client/integration-tests-public/journey/unauthedUserSeesStrickenDocketEntry.js
@@ -0,0 +1,35 @@
+import { applicationContextPublic } from '../../src/applicationContextPublic';
+import { publicCaseDetailHelper as publicCaseDetailHelperComputed } from '../../src/presenter/computeds/public/publicCaseDetailHelper';
+import { runCompute } from 'cerebral/test';
+import { withAppContextDecorator } from '../../src/withAppContext';
+
+const publicCaseDetailHelper = withAppContextDecorator(
+  publicCaseDetailHelperComputed,
+  applicationContextPublic,
+);
+
+export const unauthedUserSeesStrickenDocketEntry = (
+  test,
+  docketRecordIndex,
+) => {
+  return it('View case detail', async () => {
+    await test.runSequence('gotoPublicCaseDetailSequence', {
+      docketNumber: test.docketNumber,
+    });
+    expect(test.currentRouteUrl.includes('/case-detail')).toBeTruthy();
+    expect(test.getState('caseDetail.contactPrimary.name')).toBeDefined();
+
+    const { formattedDocketEntries } = runCompute(publicCaseDetailHelper, {
+      state: test.getState(),
+    });
+
+    const formattedDocketEntry = formattedDocketEntries.find(
+      docketEntry => docketEntry.index === docketRecordIndex,
+    );
+
+    expect(formattedDocketEntry.isStricken).toEqual(true);
+    expect(formattedDocketEntry.showDocumentDescriptionWithoutLink).toEqual(
+      true,
+    );
+  });
+};
