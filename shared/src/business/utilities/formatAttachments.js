d1d7f48fe14ffdb51df14c502331ef3291ef1e56
Matt Hopson <matthopson@gmail.com>
2020-08-17 13:39:10 -0600

    Add jsdocs for util

--- shared/src/business/utilities/formatAttachments.js
+++ shared/src/business/utilities/formatAttachments.js
@@ -1,3 +1,11 @@
+/**
+ * formats message attachments with meta data from aggregate case document arrays
+ *
+ * @params {object} params the params object
+ * @param {array} attachments attachments to format
+ * @param {object} caseDetail the case detail with documents, archivedDocuments (optional), and correspondence (optional)
+ * @returns {array} the formatted array of attachment objects
+ */
 export const formatAttachments = ({ attachments, caseDetail }) => {
   const { archivedDocuments = [], correspondence = [], documents } = caseDetail;
 
