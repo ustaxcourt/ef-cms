66cfe76417df9928e32a7df7d0d468623b9cb089
Mrinalini <mrinalinisinha05@gmail.com>
2020-08-21 08:06:36 -0700

    6070 code cleanup

--- web-client/src/presenter/actions/sealAddressAction.js
+++ web-client/src/presenter/actions/sealAddressAction.js
@@ -6,6 +6,7 @@ import { state } from 'cerebral';
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the applicationContext
  * @param {object} providers.get the cerebral get method
+ *  @returns {Promise} async action
  */
 export const sealAddressAction = async ({ applicationContext, get }) => {
   const { contactId } = get(state.contactToSeal);
