fba58536c36aceb23d18530a5bd601e1fc138a45
Mrinalini <mrinalinisinha05@gmail.com>
2020-08-24 12:20:46 -0700

    integration tests around sealing an address

---
+++ web-client/integration-tests/journey/docketClerkSealsContactInformation.js
@@ -0,0 +1,46 @@
+export const docketClerkSealsContactInformation = (
+  test,
+  contactType,
+  docketNumber,
+) => {
+  return it(`Docket clerk seals ${contactType} information`, async () => {
+    await test.runSequence('gotoCaseDetailSequence', {
+      docketNumber: docketNumber || test.docketNumber,
+    });
+
+    let contactToSeal;
+    if (
+      contactType === 'contactPrimary' ||
+      contactType === 'contactSecondary'
+    ) {
+      contactToSeal = test.getState(`caseDetail.${contactType}`);
+    } else {
+      contactToSeal = test.getState(`caseDetail.${contactType}`)[0];
+    }
+
+    expect(contactToSeal.isAddressSealed).toBe(false);
+
+    await test.runSequence('openSealAddressModalSequence', { contactToSeal });
+
+    expect(test.getState('contactToSeal')).toEqual(contactToSeal);
+    expect(test.getState('modal.showModal')).toEqual('SealAddressModal');
+
+    await test.runSequence('sealAddressSequence');
+
+    expect(test.getState('modal.showModal')).toBeUndefined();
+    expect(test.getState('alertSuccess.message')).toEqual(
+      `Address sealed for ${contactToSeal.name}.`,
+    );
+
+    if (
+      contactType === 'contactPrimary' ||
+      contactType === 'contactSecondary'
+    ) {
+      contactToSeal = test.getState(`caseDetail.${contactType}`);
+    } else {
+      contactToSeal = test.getState(`caseDetail.${contactType}`)[0];
+    }
+
+    expect(contactToSeal.isAddressSealed).toBe(true);
+  });
+};
