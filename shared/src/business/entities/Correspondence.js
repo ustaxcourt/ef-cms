42dc87857bfa3616f769078747ea9afc3f08f3d6
Kris Koskelin <kkoskelin@flexion.us>
2020-08-28 12:51:46 -0500

    WIP valid entities
    [skip ci]

--- shared/src/business/entities/Correspondence.js
+++ shared/src/business/entities/Correspondence.js
@@ -4,6 +4,7 @@ const {
 } = require('../../utilities/JoiValidationConstants');
 const {
   joiValidationDecorator,
+  validEntityDecorator,
 } = require('../../utilities/JoiValidationDecorator');
 const { createISODateString } = require('../utilities/DateHandler');
 
@@ -11,14 +12,15 @@ const { createISODateString } = require('../utilities/DateHandler');
  * @param {object} rawProps the raw document data
  * @constructor
  */
-function Correspondence(rawProps) {
+function Correspondence() {}
+Correspondence.prototype.init = function init(rawProps) {
   this.archived = rawProps.archived;
   this.documentTitle = rawProps.documentTitle;
   this.documentId = rawProps.documentId;
   this.filedBy = rawProps.filedBy;
   this.userId = rawProps.userId;
   this.filingDate = rawProps.filingDate || createISODateString();
-}
+};
 
 Correspondence.VALIDATION_RULES = {
   archived: joi
@@ -36,4 +38,4 @@ Correspondence.VALIDATION_RULES = {
 
 joiValidationDecorator(Correspondence, Correspondence.VALIDATION_RULES, {});
 
-module.exports = { Correspondence };
+module.exports = { Correspondence: validEntityDecorator(Correspondence) };
