3f02babc9e58aaabd6a9a688dc06f1731a3867d6
Kris Koskelin <kkoskelin@flexion.us>
2020-08-28 17:28:00 -0500

    more valid entities
    reducing a "console.trace" to a "console.warn" as I don't want it to break tests.

--- shared/src/business/entities/externalDocument/ExternalDocumentNonStandardJ.js
+++ shared/src/business/entities/externalDocument/ExternalDocumentNonStandardJ.js
@@ -3,6 +3,7 @@ const {
 } = require('../../../utilities/JoiValidationConstants');
 const {
   joiValidationDecorator,
+  validEntityDecorator,
 } = require('../../../utilities/JoiValidationDecorator');
 const {
   VALIDATION_ERROR_MESSAGES,
@@ -14,13 +15,15 @@ const { replaceBracketed } = require('../../utilities/replaceBracketed');
  * @param {object} rawProps the raw document data
  * @constructor
  */
-function ExternalDocumentNonStandardJ(rawProps) {
+function ExternalDocumentNonStandardJ() {}
+
+ExternalDocumentNonStandardJ.prototype.init = function init(rawProps) {
   this.category = rawProps.category;
   this.documentTitle = rawProps.documentTitle;
   this.documentType = rawProps.documentType;
   this.freeText = rawProps.freeText;
   this.freeText2 = rawProps.freeText2;
-}
+};
 
 ExternalDocumentNonStandardJ.prototype.getDocumentTitle = function () {
   return replaceBracketed(this.documentTitle, this.freeText, this.freeText2);
@@ -44,4 +47,8 @@ joiValidationDecorator(
   ExternalDocumentNonStandardJ.VALIDATION_ERROR_MESSAGES,
 );
 
-module.exports = { ExternalDocumentNonStandardJ };
+module.exports = {
+  ExternalDocumentNonStandardJ: validEntityDecorator(
+    ExternalDocumentNonStandardJ,
+  ),
+};
