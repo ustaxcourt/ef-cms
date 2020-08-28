b021901c55c6288c0961ba97533a6b003c390d58
Joseph Sutton <jsutton@flexion.us>
2020-08-28 14:11:28 -0500

    fix integration tests [skip ci]

--- web-client/integration-tests/journey/petitionsClerkViewsCaseDetailAfterAddingNotice.js
+++ web-client/integration-tests/journey/petitionsClerkViewsCaseDetailAfterAddingNotice.js
@@ -9,7 +9,8 @@ export const petitionsClerkViewsCaseDetailAfterAddingNotice = test => {
     expect(test.getState('currentPage')).toEqual('CaseDetailInternal');
     expect(test.getState('caseDetail.docketNumber')).toEqual(test.docketNumber);
     expect(test.getState('caseDetail.status')).toEqual(CASE_STATUS_TYPES.new);
-    expect(test.getState('caseDetail.documents').length).toEqual(3);
+    // expect(test.getState('caseDetail.documents').length).toEqual(3);
+    expect(test.getState('caseDetail.documents').length).toEqual(4);
     expect(
       test
         .getState('caseDetail.documents')
