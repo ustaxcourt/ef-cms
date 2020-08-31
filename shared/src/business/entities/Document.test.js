d9fdea51b9c6ef4c780a775050ad42646036c723
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-08-31 14:43:14 -0700

    Trello 636: check for isOnDocketRecord when calling strikeEntry

--- shared/src/business/entities/Document.test.js
+++ shared/src/business/entities/Document.test.js
@@ -1713,4 +1713,82 @@ describe('Document entity', () => {
       expect(document.isMinuteEntry).toBe(true);
     });
   });
+
+  describe('setNumberOfPages', () => {
+    it('sets the number of pages', () => {
+      const document = new Document(
+        {
+          description: 'Answer',
+          documentType: 'Answer',
+          eventCode: 'A',
+          filedBy: 'Test Petitioner',
+          filingDate: new Date('9000-01-01').toISOString(),
+          index: 1,
+        },
+        { applicationContext },
+      );
+      document.setNumberOfPages(13);
+      expect(document.numberOfPages).toEqual(13);
+    });
+  });
+
+  describe('strikeEntry', () => {
+    it('strikes a document if isOnDocketRecord is true', () => {
+      const document = new Document(
+        {
+          description: 'Answer',
+          documentType: 'Answer',
+          eventCode: 'A',
+          filedBy: 'Test Petitioner',
+          filingDate: new Date('9000-01-01').toISOString(),
+          index: 1,
+          isOnDocketRecord: true,
+        },
+        { applicationContext },
+      );
+      document.strikeEntry({
+        name: 'Test User',
+        userId: 'b07d648b-f5f3-4e81-bdb9-6e744f1d4125',
+      });
+      expect(document).toMatchObject({
+        isStricken: true,
+        strickenAt: expect.anything(),
+        strickenBy: 'Test User',
+        strickenByUserId: 'b07d648b-f5f3-4e81-bdb9-6e744f1d4125',
+      });
+    });
+
+    it('throws an error if isOnDocketRecord is false', () => {
+      const document = new Document(
+        {
+          description: 'Answer',
+          documentType: 'Answer',
+          eventCode: 'A',
+          filedBy: 'Test Petitioner',
+          filingDate: new Date('9000-01-01').toISOString(),
+          index: 1,
+          isOnDocketRecord: false,
+        },
+        { applicationContext },
+      );
+      let error;
+      try {
+        document.strikeEntry({
+          name: 'Test User',
+          userId: 'b07d648b-f5f3-4e81-bdb9-6e744f1d4125',
+        });
+      } catch (e) {
+        error = e;
+      }
+      expect(error).toEqual(
+        new Error('Cannot strike a document that is not on the docket record.'),
+      );
+      expect(document).toMatchObject({
+        isStricken: false,
+        strickenAt: undefined,
+        strickenBy: undefined,
+        strickenByUserId: undefined,
+      });
+    });
+  });
 });
