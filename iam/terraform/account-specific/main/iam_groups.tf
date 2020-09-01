b2016aa10a6e0789d1cb4e9f55819e792c5e4819
Julia Allen <julia.l.allen@gsa.gov>
2020-09-01 17:01:33 -0500

    move iam user group into account-specific

---
+++ iam/terraform/account-specific/main/iam_groups.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_group" "developers" {
+  name = "developers"
+}
+
+resource "aws_iam_policy" "es_admin_access" {
+  name = "es_admin_access_policy"
+  description = "A policy that grants an IAM user admin access to Elasticsearch service"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": "es:*",
+        "Resource": "*"
+      }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_group_policy_attachment" "es_admin_policy_attachment" {
+  group      = "developers"
+  policy_arn = aws_iam_policy.es_admin_access.arn
+}
--- web-api/terraform/template/elasticsearch.tf
+++ web-api/terraform/template/elasticsearch.tf
@@ -2,33 +2,6 @@ resource "aws_cloudwatch_log_group" "elasticsearch_application_logs" {
   name = "/aws/aes/debug_${var.environment}"
 }
 
-resource "aws_iam_group" "developers" {
-  name = "developers"
-}
-
-resource "aws_iam_policy" "es_admin_access" {
-  name = "es_admin_access_policy",
-  description = "A policy that grants an IAM user admin access to Elasticsearch service"
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Action": "es:*",
-        "Resource": "*"
-      }
-    ]
-}
-EOF
-}
-
-resource "aws_iam_group_policy_attachment" "es_admin_policy_attachment" {
-  group      = aws_iam_group.developers.developers
-  policy_arn = aws_iam_policy.es_admin_access.arn
-}
-
 resource "aws_cloudwatch_log_resource_policy" "allow_elasticsearch_to_write_logs" {
   policy_name = "allow_elasticsearch_to_write_logs"
 
@@ -77,7 +50,7 @@ resource "aws_elasticsearch_domain" "efcms-search" {
   }
 }
 
-resource "aws_elasticsearch_domain" "efcms-app-logs {
+resource "aws_elasticsearch_domain" "efcms-app-logs" {
   domain_name           = "efcms-app-logs-${var.environment}"
   elasticsearch_version = "7.4"
 
