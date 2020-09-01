53cb0cf9cf67d8812b2bf1632409c5c4703f7c80
Julia Allen <julia.l.allen@gsa.gov>
2020-09-01 11:26:41 -0500

    WIP - add config for app logs es instance

--- web-api/terraform/template/elasticsearch.tf
+++ web-api/terraform/template/elasticsearch.tf
@@ -2,6 +2,10 @@ resource "aws_cloudwatch_log_group" "elasticsearch_application_logs" {
   name = "/aws/aes/debug_${var.environment}"
 }
 
+resource "aws_iam_group" "developers" {
+  name = "developers"
+}
+
 resource "aws_cloudwatch_log_resource_policy" "allow_elasticsearch_to_write_logs" {
   policy_name = "allow_elasticsearch_to_write_logs"
 
@@ -48,4 +52,46 @@ resource "aws_elasticsearch_domain" "efcms-search" {
     cloudwatch_log_group_arn = aws_cloudwatch_log_group.elasticsearch_application_logs.arn
     log_type                 = "ES_APPLICATION_LOGS"
   }
+}
+
+resource "aws_elasticsearch_domain" "efcms-app-logs {
+  domain_name           = "efcms-app-logs-${var.environment}"
+  elasticsearch_version = "7.4"
+
+  cluster_config {
+    instance_type = "t2.small.elasticsearch"
+    instance_count = var.es_instance_count == "" ? "1" : var.es_instance_count
+  }
+
+  ebs_options{
+    ebs_enabled = true
+    volume_size = 10
+  }
+
+  snapshot_options {
+    automated_snapshot_start_hour = 23
+  }
+
+  log_publishing_options {
+    cloudwatch_log_group_arn = aws_cloudwatch_log_group.elasticsearch_application_logs.arn
+    log_type                 = "ES_APPLICATION_LOGS"
+  }
+}
+
+resource "aws_iam_policy" "es_admin_access" {
+  name = "es_admin_access_policy",
+  description = "A policy that grants an IAM user admin access to Elasticsearch service"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {   
+            "Effect": "Allow",
+            "Action": "es:*",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
 }
