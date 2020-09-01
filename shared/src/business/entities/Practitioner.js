399ede83d9fd7c3a1a2eed7186f0c3b2b89ffbf7
Rachel Schneiderman <rschneiderman@flexion.us>
2020-09-01 09:53:37 -0700

    Trello 764 - isUpdatingInformation flag has to be on User entity

--- shared/src/business/entities/Practitioner.js
+++ shared/src/business/entities/Practitioner.js
@@ -33,7 +33,6 @@ Practitioner.prototype.init = function init(rawUser) {
   this.additionalPhone = rawUser.additionalPhone;
   this.admissionsDate = rawUser.admissionsDate;
   this.admissionsStatus = rawUser.admissionsStatus;
-  this.isUpdatingInformation = rawUser.isUpdatingInformation;
   this.alternateEmail = rawUser.alternateEmail;
   this.barNumber = rawUser.barNumber;
   this.birthYear = rawUser.birthYear;
@@ -125,12 +124,6 @@ const practitionerValidation = {
   firstName: JoiValidationConstants.STRING.max(100)
     .required()
     .description('The first name of the practitioner.'),
-  isUpdatingInformation: joi
-    .boolean()
-    .optional()
-    .description(
-      'Whether the contact information for the user is being updated.',
-    ),
   lastName: JoiValidationConstants.STRING.max(100)
     .required()
     .description('The last name of the practitioner.'),
