eb1d738a3f792b12c8d235b239b6dfc6d3cda76c
Rachel Schneiderman <rschneiderman@flexion.us>
2020-09-02 12:04:10 -0700

    6281 - [skip ci] dynamo health check working

--- shared/src/business/useCases/health/getHealthCheckInteractor.js
+++ shared/src/business/useCases/health/getHealthCheckInteractor.js
@@ -1,3 +1,7 @@
+const {
+  describeDeployTable,
+  describeTable,
+} = require('../../../persistence/dynamodbClientService');
 const { search } = require('../../../persistence/elasticsearch/searchClient');
 
 const getElasticSearchStatus = async ({ applicationContext }) => {
@@ -24,24 +28,22 @@ const getElasticSearchStatus = async ({ applicationContext }) => {
 
 const getDynamoStatus = async ({ applicationContext }) => {
   try {
-    await search({
-      applicationContext,
-      searchParameters: {
-        body: {
-          _source: ['docketNumber'],
-          query: {
-            match_all: {},
-          },
-          size: 1,
-        },
-        index: 'efcms-case',
-      },
-    });
+    const { Table } = await describeTable({ applicationContext });
+
+    return Table.TableStatus === 'ACTIVE';
   } catch (e) {
     return false;
   }
+};
 
-  return true;
+const getDeployDynamoStatus = async ({ applicationContext }) => {
+  try {
+    const { Table } = await describeDeployTable({ applicationContext });
+
+    return Table.TableStatus === 'ACTIVE';
+  } catch (e) {
+    return false;
+  }
 };
 
 /**
@@ -55,6 +57,15 @@ exports.getHealthCheckInteractor = async ({ applicationContext }) => {
   });
 
   const dynamoStatus = await getDynamoStatus({ applicationContext });
+  const deployDynamoStatus = await getDeployDynamoStatus({
+    applicationContext,
+  });
 
-  return { elasticsearch: elasticSearchStatus };
+  return {
+    dynamo: {
+      efcms: dynamoStatus,
+      efcmsDeploy: deployDynamoStatus,
+    },
+    elasticsearch: elasticSearchStatus,
+  };
 };
