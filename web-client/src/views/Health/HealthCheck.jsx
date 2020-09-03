afcc0ed5d176b281359b9df6bc027e69bfd78b76
Mrinalini <mrinalinisinha05@gmail.com>
2020-09-03 14:36:17 -0700

    implementing new UI

--- web-client/src/views/Health/HealthCheck.jsx
+++ web-client/src/views/Health/HealthCheck.jsx
@@ -4,6 +4,22 @@ import { connect } from '@cerebral/react';
 import { state } from 'cerebral';
 import React from 'react';
 
+const RenderHealthStatus = ({ item }) => {
+  return (
+    <span className="float-right margin-right-205">
+      <h4 className="text-light">
+        {item ? 'Pass' : 'Fail'}{' '}
+        <FontAwesomeIcon
+          className="margin-left-1"
+          color={item ? 'green' : 'red'}
+          icon={item ? 'check-circle' : 'times-circle'}
+          size="lg"
+        />
+      </h4>
+    </span>
+  );
+};
+
 export const HealthCheck = connect(
   {
     health: state.health,
@@ -15,16 +31,60 @@ export const HealthCheck = connect(
         <BigHeader text="Health Check" />
         <section className="usa-section grid-container">
           <div className="grid-row grid-gap">
-            <div className="grid-col-6">
-              {' '}
-              <div className="margin-bottom-1">
-                Dynamsoft
-                <FontAwesomeIcon
-                  className="margin-left-1"
-                  color={health.dynamsoft ? 'green' : 'red'}
-                  icon="check-circle"
-                />
+            <div className="grid-col-3">
+              <div className="card height-8">
+                <h2 className="margin-top-205 margin-left-205">
+                  ClamAV
+                  <RenderHealthStatus item={health.clamAV} />
+                </h2>
               </div>
+              <div className="card height-8">
+                <h2 className="margin-top-205 margin-left-205">
+                  Cognito
+                  <RenderHealthStatus item={health.cognito} />
+                </h2>
+              </div>
+              <div className="card height-8">
+                <h2 className="margin-top-205 margin-left-205">
+                  Dynamsoft
+                  <RenderHealthStatus item={health.dynamsoft} />
+                </h2>
+              </div>
+            </div>
+            <div className="grid-col-3">
+              <div>
+                Dynamo
+                <div className="margin-bottom-1 margin-top-1 margin-left-1">
+                  EFCMS
+                  <FontAwesomeIcon
+                    className="margin-left-1"
+                    color={health.dynamo.efcms ? 'green' : 'red'}
+                    icon="check-circle"
+                  />
+                </div>
+                <div className="margin-bottom-1 margin-left-1">
+                  EFCMS Deploy
+                  <FontAwesomeIcon
+                    className="margin-left-1"
+                    color={health.dynamo.efcmsDeploy ? 'green' : 'red'}
+                    icon="check-circle"
+                  />
+                </div>
+              </div>
+              <div className="card height-8">
+                <h2 className="margin-top-205 margin-left-2">
+                  ElasticSearch
+                  <RenderHealthStatus item={health.elasticsearch} />
+                </h2>
+              </div>
+              <div className="card height-8">
+                <h2 className="margin-top-205 margin-left-2">
+                  Email Service
+                  <RenderHealthStatus item={health.emailService} />
+                </h2>
+              </div>
+            </div>
+            <div className="grid-col-3">
               <div>
                 S3
                 <div className="margin-bottom-1 margin-top-1 margin-left-1">
@@ -93,59 +153,7 @@ export const HealthCheck = connect(
                 </div>
               </div>
             </div>
-            <div className="grid-col-6">
-              <div>
-                Dynamo
-                <div className="margin-bottom-1 margin-top-1 margin-left-1">
-                  EFCMS
-                  <FontAwesomeIcon
-                    className="margin-left-1"
-                    color={health.dynamo.efcms ? 'green' : 'red'}
-                    icon="check-circle"
-                  />
-                </div>
-                <div className="margin-bottom-1 margin-left-1">
-                  EFCMS Deploy
-                  <FontAwesomeIcon
-                    className="margin-left-1"
-                    color={health.dynamo.efcmsDeploy ? 'green' : 'red'}
-                    icon="check-circle"
-                  />
-                </div>
-              </div>
-              <div className="margin-bottom-1">
-                Cognito
-                <FontAwesomeIcon
-                  className="margin-left-1"
-                  color={health.cognito ? 'green' : 'red'}
-                  icon="check-circle"
-                />
-              </div>
-              <div className="margin-bottom-1">
-                Elasticsearch
-                <FontAwesomeIcon
-                  className="margin-left-1"
-                  color={health.elasticsearch ? 'green' : 'red'}
-                  icon="check-circle"
-                />
-              </div>
-              <div>
-                Email Service
-                <FontAwesomeIcon
-                  className="margin-left-1"
-                  color={health.emailService ? 'green' : 'red'}
-                  icon="check-circle"
-                />
-              </div>
-              <div>
-                ClamAV
-                <FontAwesomeIcon
-                  className="margin-left-1"
-                  color={health.clamAV ? 'green' : 'red'}
-                  icon="check-circle"
-                />
-              </div>
-            </div>
+            <div className="grid-col-3"></div>
           </div>
         </section>
       </>
