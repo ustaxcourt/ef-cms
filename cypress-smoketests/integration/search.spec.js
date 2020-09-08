a29789de36e71964cc3fd2087de312d9b6b60361
Mrinalini Sinha <mrinalinisinha05@gmail.cok>
2020-09-08 12:22:06 -0700

    adding practitioner, opinion and order advanced search to smoketests

--- cypress-smoketests/integration/search.spec.js
+++ cypress-smoketests/integration/search.spec.js
@@ -1,14 +1,24 @@
 const {
+  gotoAdvancedPractitionerSearch,
   gotoAdvancedSearch,
+  goToOpinionSearch,
+  goToOrderSearch,
   searchByDocketNumber,
   searchByPetitionerName,
+  searchByPractitionerbarNumber,
+  searchByPractitionerName,
+  searchOpinionByKeyword,
+  searchOrderByKeyword,
 } = require('../support/pages/advanced-search');
+
 const { getUserToken, login } = require('../support/pages/login');
 
 const docketNumberToSearchBy = '103-20';
+const barNumberToSearchBy = 'PT1234';
 let token;
 
-describe('Case Search', () => {
+describe('Case Advanced Search', () => {
+  //create a case?
   before(async () => {
     const results = await getUserToken(
       'docketclerk1@example.com',
@@ -29,5 +39,70 @@ describe('Case Search', () => {
   it('should be able to search for case by docket number', () => {
     gotoAdvancedSearch();
     searchByDocketNumber(docketNumberToSearchBy);
+  });
+});
+
+describe('Practitoner Search', () => {
+  //create a case?
+  before(async () => {
+    const results = await getUserToken(
+      'docketclerk1@example.com',
+      'Testing1234$',
+    );
+    token = results.AuthenticationResult.IdToken;
+  });
+
+  it('should be able to login', () => {
+    login(token);
+  });
+
+  it('should be able to search for a practitioner by name', () => {
+    gotoAdvancedPractitionerSearch();
+    searchByPractitionerName();
+  });
+
+  it('should be able to search for for a practitioner by bar number', () => {
+    gotoAdvancedPractitionerSearch();
+    searchByPractitionerbarNumber(barNumberToSearchBy);
+  });
+});
+
+describe('Order Search', () => {
+  //create a case?
+  before(async () => {
+    const results = await getUserToken(
+      'docketclerk1@example.com',
+      'Testing1234$',
+    );
+    token = results.AuthenticationResult.IdToken;
+  });
+
+  it('should be able to login', () => {
+    login(token);
+  });
+
+  it('should  search for an order by keyword', () => {
+    goToOrderSearch();
+    searchOrderByKeyword('order');
+  });
+});
+
+describe('Opinion Search', () => {
+  //create a case?
+  before(async () => {
+    const results = await getUserToken(
+      'docketclerk1@example.com',
+      'Testing1234$',
+    );
+    token = results.AuthenticationResult.IdToken;
+  });
+
+  it('should be able to login', () => {
+    login(token);
+  });
+
+  it('should be able to search for an opinion by keyword', () => {
+    goToOpinionSearch();
+    searchOpinionByKeyword('opinion');
   });
 });
--- cypress-smoketests/support/pages/advanced-search.js
+++ cypress-smoketests/support/pages/advanced-search.js
@@ -2,6 +2,21 @@ exports.gotoAdvancedSearch = () => {
   cy.get('a.advanced').click();
 };
 
+exports.gotoAdvancedPractitionerSearch = () => {
+  cy.get('a.advanced').click();
+  cy.get('button#tab-practitioner').click();
+};
+
+exports.goToOrderSearch = () => {
+  cy.get('a.advanced').click();
+  cy.get('button#tab-order').click();
+};
+
+exports.goToOpinionSearch = () => {
+  cy.get('a.advanced').click();
+  cy.get('button#tab-opinion').click();
+};
+
 exports.searchByPetitionerName = () => {
   cy.get('input#petitioner-name').type('test');
   cy.get('button#advanced-search-button').click();
@@ -12,4 +27,28 @@ exports.searchByDocketNumber = docketNumber => {
   cy.get('input#docket-number').type(docketNumber);
   cy.get('button#docket-search-button').click();
   cy.url().should('contain', docketNumber);
+};
+
+exports.searchByPractitionerName = () => {
+  cy.get('input#practitioner-name').type('test');
+  cy.get('button#practitioner-search-by-name-button').click();
+  cy.get('table.search-results').should('exist');
+};
+
+exports.searchByPractitionerbarNumber = barNumber => {
+  cy.get('input#bar-number').type(barNumber);
+  cy.get('button.advanced-search__button').eq(1).click();
+  cy.url().should('contain', barNumber);
+};
+
+exports.searchOrderByKeyword = keyword => {
+  cy.get('input#order-search').type(keyword);
+  cy.get('button#advanced-search-button').click();
+  cy.get('table.search-results').should('exist');
+};
+
+exports.searchOpinionByKeyword = keyword => {
+  cy.get('input#opinion-search').type(keyword);
+  cy.get('button#advanced-search-button').click();
+  cy.get('table.search-results').should('exist');
 };
