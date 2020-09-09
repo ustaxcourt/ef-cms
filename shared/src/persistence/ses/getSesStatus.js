c8374e807128ecb71e0b2b4e2523453fd47bbcc9
Cody Seibert <cseibert@flexion.us>
2020-09-09 11:12:30 -0400

    6281 - switching to use get statistics

--- shared/src/persistence/ses/getSesStatus.js
+++ shared/src/persistence/ses/getSesStatus.js
@@ -2,26 +2,11 @@ exports.getSesStatus = async ({ applicationContext }) => {
   const SES = applicationContext.getEmailClient();
 
   try {
-    await SES.sendEmail({
-      Destination: {
-        ToAddresses: ['test@example.com'],
-      },
-      Message: {
-        Body: {
-          Text: {
-            Charset: 'UTF-8',
-            Data: 'This is a test status email.',
-          },
-        },
-        Subject: {
-          Charset: 'UTF-8',
-          Data: 'Test email',
-        },
-      },
-      Source: process.env.EMAIL_SOURCE,
-    }).promise();
-
-    return true;
+    const { SendDataPoints } = await SES.getSendStatistics({}).promise();
+    return SendDataPoints.every(
+      ({ Bounces, Complaints, Rejects }) =>
+        Bounces === 0 && Complaints === 0 && Rejects === 0,
+    );
   } catch (err) {
     console.log('error sending the ses email', err);
     return false;
