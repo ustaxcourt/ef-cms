2c5c98f8783ba9c68fcc82fb80965b72a155acc5
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-09-10 08:45:55 -0700

    636: rename document persistence methods to docketEntry

--- shared/src/persistence/dynamo/documents/deleteDocketEntry.js
+++ shared/src/persistence/dynamo/documents/deleteDocketEntry.js
@@ -1,23 +1,23 @@
 const client = require('../../dynamodbClientService');
 
 /**
- * deleteDocument
+ * deleteDocketEntry
  *
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
  * @param {object} providers.docketNumber the docket number of the record to delete from
- * @param {object} providers.documentId the documentId of the record to delete
+ * @param {object} providers.docketEntryId the docketEntryId of the record to delete
  */
-exports.deleteDocument = async ({
+exports.deleteDocketEntry = async ({
   applicationContext,
+  docketEntryId,
   docketNumber,
-  documentId,
 }) => {
   await client.delete({
     applicationContext,
     key: {
       pk: `case|${docketNumber}`,
-      sk: `docket-entry|${documentId}`,
+      sk: `docket-entry|${docketEntryId}`,
     },
   });
 };
