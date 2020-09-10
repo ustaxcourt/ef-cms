9da370a0ea68f5c033b75a02631359d4b956aaa3
Kris Koskelin <kkoskelin@flexion.us>
2020-09-10 10:45:28 -0500

    570: smoke tests for trial sessions (WIP)

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -81,6 +81,14 @@ Cypress.Commands.add('login', (username, route = '/') => {
   cy.url().should('not.include', '/mock-login');
 });
 
+Cypress.Commands.add('goToRoute', (...args) => {
+  cy.get('.progress-indicator').should('not.exist');
+  return cy.window().then(w => {
+    // eslint-disable-next-line no-underscore-dangle
+    w.__cy_route(...args);
+  });
+});
+
 /**
  * @param b64Data
  * @param contentType
