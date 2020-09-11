3ffe71ec4ab8e8c36fbff4ae48d71ef02f95ad26
Matt Hopson <matthopson@gmail.com>
2020-09-11 09:30:33 -0600

    Update jsdocs

--- shared/src/business/useCases/messages/setMessageAsReadInteractor.js
+++ shared/src/business/useCases/messages/setMessageAsReadInteractor.js
@@ -9,6 +9,7 @@ const { UnauthorizedError } = require('../../../errors/errors');
  *
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
+ * @param {string} providers.docketNumber the docket number for the case the message is associated with
  * @param {string} providers.messageId the id of the message to set as read
  * @returns {Promise} the promise of the setMessageAsRead call
  */
--- shared/src/proxies/messages/setMessageAsReadProxy.js
+++ shared/src/proxies/messages/setMessageAsReadProxy.js
@@ -5,6 +5,7 @@ const { post } = require('../requests');
  *
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
+ * @param {string} providers.docketNumber the docket number for the case the message is associated with
  * @param {string} providers.messageId the id of the message to set as read
  * @returns {Promise<*>} the promise of the api call
  */
