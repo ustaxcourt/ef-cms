6e68da2a7da8d1882b25e6649f8b773e36fbba63
Kristen Lohman <klohman@flexion.us>
2020-09-14 13:21:40 -0400

    trello #800

--- web-client/src/views/FileDocument/SelectDocumentType.jsx
+++ web-client/src/views/FileDocument/SelectDocumentType.jsx
@@ -61,52 +61,6 @@ export const SelectDocumentType = connect(
                 Cancel
               </Button>
             </div>
-
-            <NonMobile>
-              <div className="tablet:grid-col-4 tablet:grid-offset-2 bg-white need-help-selecting">
-                <div className="card">
-                  <div className="content-header bg-accent-cool-dark text-white heading-2">
-                    Need help selecting a document type?
-                  </div>
-                  <div className="content-wrapper">
-                    <p>
-                      Here are common reasons you might be filing a document.
-                      Underneath each is a list of document types that are
-                      usually associated with that reason.
-                    </p>
-                  </div>
-                  <Accordion headingLevel="3">
-                    {reasons.map(({ categories, reason }, reasonIndex) => {
-                      return (
-                        <AccordionItem
-                          key={`reason-${reasonIndex}`}
-                          title={reason}
-                        >
-                          {categories.map(({ category }, categoryIndex) => {
-                            return (
-                              <p key={`${reasonIndex}-${categoryIndex}`}>
-                                {category}
-                              </p>
-                            );
-                          })}
-                        </AccordionItem>
-                      );
-                    })}
-                  </Accordion>
-                  <div className="content-wrapper margin-top-3">
-                    <div>
-                      <Button
-                        secondary
-                        id="view-all-documents"
-                        onClick={() => gotoViewAllDocumentsSequence()}
-                      >
-                        View All Document Categories
-                      </Button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </NonMobile>
           </div>
         </div>
         {showModal === 'CheckConsolidatedCasesModal' && (
