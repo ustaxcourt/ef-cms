bb22c05a8247c5f97af693b7028e419744356f68
Julia Allen <julia.l.allen@gsa.gov>
2020-09-15 13:36:15 -0500

    add config due to terraform bug

--- web-api/terraform/template/streams.tf
+++ web-api/terraform/template/streams.tf
@@ -1,7 +1,7 @@
 data "archive_file" "zip_streams" {
   type        = "zip"
   output_path = "${path.module}/lambdas/streams.js.zip"
-  source_file = "${path.module}/lambdas/streams.js"
+  source_file = "${path.module}/lambdas/dist/streams.js"
 }
 
 resource "aws_lambda_function" "zip_streams" {
@@ -25,4 +25,10 @@ resource "aws_lambda_event_source_mapping" "streams_mapping" {
   event_source_arn  = aws_dynamodb_table.efcms-east.stream_arn
   function_name     = aws_lambda_function.zip_streams.arn
   starting_position = "TRIM_HORIZON"
+
+  # The below config was added due to a potential Terraform bug
+  # see https://github.com/terraform-providers/terraform-provider-aws/issues/14522
+  maximum_batching_window_in_seconds = 0
+  maximum_record_age_in_seconds      = 604800
+  maximum_retry_attempts             = 10000
 }
