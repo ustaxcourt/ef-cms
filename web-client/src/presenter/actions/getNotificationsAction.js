abd751962517c8c7d052f07d2a1de54612e0b3b9
Matt Hopson <matthopson@gmail.com>
2020-09-16 15:01:45 -0600

    Updating jsdocs

--- web-client/src/presenter/actions/getNotificationsAction.js
+++ web-client/src/presenter/actions/getNotificationsAction.js
@@ -6,6 +6,7 @@ import { state } from 'cerebral';
  * @param {object} providers the providers object
  * @param {Function} providers.get the cerebral get function used for getting state.user.token
  * @param {object} providers.applicationContext needed for getting the getCase use case
+ * @param {function} providers.get the cerebral get method
  * @returns {object} contains the caseDetail returned from the use case
  */
 export const getNotificationsAction = async ({ applicationContext, get }) => {
