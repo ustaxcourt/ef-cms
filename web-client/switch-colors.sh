fc0fe3072b6f45cce93b6079d387c887c4b45c77
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-09-18 11:43:23 -0700

    6271: setup deploying color in deploys table

--- web-client/switch-colors.sh
+++ web-client/switch-colors.sh
@@ -2,3 +2,5 @@
 
 node switch-public-ui-colors.js
 node switch-ui-colors.js
+
+aws dynamodb put-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --item '{"pk":{"S":"current-color"},"sk":{"S":"current-color"},"current":{"S":"'$DEPLOYING_COLOR'"}}'
