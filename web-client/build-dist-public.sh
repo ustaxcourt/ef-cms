67b56a7b627b7a9e3209ac12c0420923fe6c466a
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-09-21 12:59:59 -0700

    6271: use new url structure for cognito redirect uris

--- web-client/build-dist-public.sh
+++ web-client/build-dist-public.sh
@@ -9,7 +9,7 @@ DEPLOYING_COLOR=$2
 
 REGION="us-east-1"
 API_URL="https://public-api-${DEPLOYING_COLOR}.${EFCMS_DOMAIN}"
-COGNITO_REDIRECT_URL="https%3A//app.${EFCMS_DOMAIN}/log-in"
+COGNITO_REDIRECT_URL="https%3A//app-${EFCMS_DOMAIN}/log-in"
 
 USER_POOL_ID=$(aws cognito-idp list-user-pools --query "UserPools[?Name == 'efcms-${ENV}'].Id | [0]" --max-results 30 --region "${REGION}")
 USER_POOL_ID="${USER_POOL_ID%\"}"
