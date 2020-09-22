f1dff1f0360d3bfd383f6cdc73e8857490603628
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-09-22 10:42:15 -0700

    add some console logs

--- web-client/switch-colors.sh
+++ web-client/switch-colors.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 node ./web-client/switch-public-ui-colors.js
+echo "in between the switches"
 node ./web-client/switch-ui-colors.js
+
+echo "made it past the switch scripts" 
 
 aws dynamodb put-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --item '{"pk":{"S":"current-color"},"sk":{"S":"current-color"},"current":{"S":"'$DEPLOYING_COLOR'"}}'
 
