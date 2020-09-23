25171fa86a2028ca4b4cb972dce2e51729a84886
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-09-23 10:30:22 -0700

    6271: setup elasticsearch index settings for all elasticsearch indices

--- web-api/terraform/template/outputs.tf
+++ web-api/terraform/template/outputs.tf
@@ -1,3 +1,7 @@
 output "elasticsearch_endpoint" {
   value = aws_elasticsearch_domain.efcms-search.endpoint
 }
+
+output "elasticsearch_endpoint_1" {
+  value = module.elasticsearch_1.aws_elasticsearch_domain.efcms-search.endpoint
+}
