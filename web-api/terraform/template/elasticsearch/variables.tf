0a16f12a03ca76ea0123b96ac6d87615e2b3c9c7
Rachel Schneiderman <rschneiderman@flexion.us>
2020-09-23 07:45:36 -0700

    Added elasticsearch module

---
+++ web-api/terraform/template/elasticsearch/variables.tf
@@ -0,0 +1,15 @@
+variable "environment" {
+  type = string
+}
+
+variable "domain_name" {
+  type = string
+}
+
+variable "es_instance_count" {
+  type = string
+}
+
+variable "es_instance_type" {
+  type = string
+}
--- web-api/terraform/template/main.tf
+++ web-api/terraform/template/main.tf
@@ -1,4 +1,18 @@
-module "environment" {
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "us-east-1"
+}
+
+provider "aws" {
+  region = "us-west-1"
+  alias  = "us-west-1"
+}
+
+module "dynamo_table_1" {
   source = "./dynamo-table"
 
   environment = var.environment
@@ -7,5 +21,18 @@ module "environment" {
   providers = {
     aws.us-east-1 = aws.us-east-1
     aws.us-west-1 = aws.us-west-1
+  }
+}
+
+module "elasticsearch_1" {
+  source = "./elasticsearch"
+
+  environment       = var.environment
+  domain_name       = "efcms-search-${var.environment}-1"
+  es_instance_count = var.es_instance_count
+  es_instance_type  = "t2.small.elasticsearch"
+
+  providers = {
+    aws.us-east-1 = aws.us-east-1
   }
 }
