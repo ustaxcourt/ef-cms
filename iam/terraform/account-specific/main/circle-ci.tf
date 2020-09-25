bfc384ef2568f8c232e5cfea27841c0b24ab7dfa
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-09-25 09:44:13 -0700

    6271: add another circle permission

--- iam/terraform/account-specific/main/circle-ci.tf
+++ iam/terraform/account-specific/main/circle-ci.tf
@@ -208,7 +208,8 @@ resource "aws_iam_policy" "circle_ci_policy" {
         "iam:AddRoleToInstanceProfile",
         "iam:CreateServiceLinkedRole",
         "iam:ListAttachedRolePolicies",
-        "iam:DeleteRolePolicy"
+        "iam:DeleteRolePolicy",
+        "iam:DeleteRole"
       ],
       "Resource": [
         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/api_gateway_cloudwatch_global*",
