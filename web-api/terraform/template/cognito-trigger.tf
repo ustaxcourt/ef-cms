41f7ae1e3f800a764cab41a3ca1e93f3ed106650
Eric Sorenson <eric@sorenson.software>
2020-09-29 14:55:04 -0500

    6271 use blue / green table for user creation

--- web-api/terraform/template/cognito-trigger.tf
+++ web-api/terraform/template/cognito-trigger.tf
@@ -39,6 +39,7 @@ resource "aws_lambda_function" "cognito_post_confirmation_lambda" {
       CLAMAV_DEF_DIR                 = "/opt/var/lib/clamav"
       CIRCLE_HONEYBADGER_API_KEY     = var.honeybadger_key
       IRS_SUPERUSER_EMAIL            = var.irs_superuser_email
+      DYNAMODB_TABLE_NAME            = var.destination_table
     }
   }
 }
