71941c9a5ecd7321a0e9c63132af2c95583b0cb9
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-09-29 08:50:39 -0700

    route53 record fix

--- iam/terraform/shared/certificates/outputs.tf
+++ iam/terraform/shared/certificates/outputs.tf
@@ -5,7 +5,7 @@ output "acm_certificate_arn" {
 
 output "acm_certificate_dns_validation_record" {
   description = "record which is used to validate acm certificate"
-  value       = aws_route53_record.this.name
+  value       = aws_route53_record.this[0].name
 }
 
 output "dns_validation" {
