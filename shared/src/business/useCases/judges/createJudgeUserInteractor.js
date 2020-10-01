f375f4dd0f6eaf568e28533a8090fbe2c59d89c8
Mrinalini Sinha <mrinalinisinha05@gmail.cok>
2020-10-01 10:10:35 -0700

    4830 adding role permission to create judge user, adding endpoint, proxy, lambda and interactor for creating a judge user

---
+++ shared/src/business/useCases/judges/createJudgeUserInteractor.js
@@ -0,0 +1,35 @@
+const {
+  isAuthorized,
+  ROLE_PERMISSIONS,
+} = require('../../../authorization/authorizationClientService');
+const { UnauthorizedError } = require('../../../errors/errors');
+const { User } = require('../../entities/User');
+
+/**
+ * createJudgeUserInteractor
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context
+ * @param {object} providers.user the user data
+ * @returns {Promise} the promise of the createUser call
+ */
+exports.createJudgeUserInteractor = async ({ applicationContext }) => {
+  const requestUser = applicationContext.getCurrentUser();
+
+  if (!isAuthorized(requestUser, ROLE_PERMISSIONS.ADD_EDIT_JUDGE_USER)) {
+    throw new UnauthorizedError('Unauthorized for creating judge user');
+  }
+
+  const judge = new User(createdUser, { applicationContext })
+    .validate()
+    .toRawObject();
+
+  const createdUser = await applicationContext
+    .getPersistenceGateway()
+    .createJudgeUser({
+      applicationContext,
+      user: judge,
+    });
+
+  return createdUser;
+};
