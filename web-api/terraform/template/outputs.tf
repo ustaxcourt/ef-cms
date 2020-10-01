6f2271034e7b7ec047b9194faec553ba11b3eb94
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-10-01 09:16:47 -0700

    6271: add dynamo and elasticsearch version 3 to tf

--- web-api/terraform/template/outputs.tf
+++ web-api/terraform/template/outputs.tf
@@ -9,3 +9,7 @@ output "elasticsearch_endpoint_1" {
 output "elasticsearch_endpoint_2" {
   value = module.elasticsearch_2.endpoint
 }
+
+output "elasticsearch_endpoint_3" {
+  value = module.elasticsearch_3.endpoint
+}
