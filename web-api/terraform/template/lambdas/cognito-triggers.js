7e8441788eb6600045f692428d4dcf937fd24eb7
Cody Seibert <cseibert@flexion.us>
2020-10-03 01:26:40 -0400

    6600 - refactor trigger to only need subset of application context

--- web-api/terraform/template/lambdas/cognito-triggers.js
+++ web-api/terraform/template/lambdas/cognito-triggers.js
@@ -1,8 +1,33 @@
-const createApplicationContext = require('../../../src/applicationContext');
+const AWS = require('aws-sdk');
+const {
+  createPetitionerAccountInteractor,
+} = require('../../../../shared/src/business/useCases/users/createPetitionerAccountInteractor');
+const {
+  persistUser,
+} = require('../../../../shared/src/persistence/dynamo/users/persistUser');
+
+const { DynamoDB } = AWS;
+
+const applicationContext = {
+  getDocumentClient: () => {
+    return new DynamoDB.DocumentClient({
+      endpoint: process.env.DYNAMODB_ENDPOINT,
+      region: process.env.AWS_REGION,
+    });
+  },
+  getEnvironment: () => ({
+    dynamoDbTableName: process.env.DYNAMODB_TABLE_NAME,
+    stage: process.env.STAGE,
+  }),
+  getPersistenceGateway: () => ({
+    persistUser,
+  }),
+  getUseCases: () => ({
+    createPetitionerAccountInteractor,
+  }),
+};
 
 exports.handler = async event => {
-  const applicationContext = createApplicationContext();
-
   const { email, name, sub: userId } = event.request.userAttributes;
 
   await applicationContext.getUseCases().createPetitionerAccountInteractor({
