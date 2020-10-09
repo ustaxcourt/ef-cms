5aa8fefa95c290b80435f8d381416171b816051a
Kris Koskelin <kkoskelin@flexion.us>
2020-10-09 11:00:44 -0500

    6654: process migration in chunks with retries

--- web-api/performance-testing/migrationPerfTest.js
+++ web-api/performance-testing/migrationPerfTest.js
@@ -1,6 +1,20 @@
 const axios = require('axios');
+const axiosRetry = require('axios-retry');
 const migratedCase = require('./migratedCase');
+const { chunk, range } = require('lodash');
 const { CognitoIdentityServiceProvider } = require('aws-sdk');
+
+axiosRetry(axios, { retries: 20, retryDelay: axiosRetry.exponentialDelay });
+
+/*
+What are the OTHER bottlenecks?
+ * tax court network
+
+Estimates:
+ - Migrating 1000 cases: 79875.731ms ~~ 20K in 26min (chunk: 50)
+ - Migrating 1000 cases: 89851.463ms ~~ 20K in 30min (chunk 25)
+ - Migrating 1000 cases: 101678.208ms (2 failed!) ~~ 20K in 33min (chunk 100)
+*/
 
 const cognito = new CognitoIdentityServiceProvider({
   region: 'us-east-1',
@@ -8,7 +22,9 @@ const cognito = new CognitoIdentityServiceProvider({
 
 const ClientId = '5imm0ecu6ndbp8bl20b1nojanm';
 const UserPoolId = 'us-east-1_1QQo1Dzuy';
-const email = 'migrator@example.com';
+const USERNAME = 'migrator@example.com';
+const PASSWORD = process.env.DEFAULT_ACCOUNT_PASS;
+const CHUNK_SIZE = 50;
 
 const color = process.env.DEPLOYING_COLOR || 'blue';
 const domain = process.env.EFCMS_DOMAIN || 'exp2.ustc-case-mgmt.flexion.us';
@@ -29,7 +45,17 @@ const getMigratedCase = () => {
   };
 };
 
-const MIGRATE_CASE_COUNT = 50;
+let MIGRATE_CASE_COUNT;
+try {
+  MIGRATE_CASE_COUNT = parseInt(process.argv[2]);
+  if (isNaN(MIGRATE_CASE_COUNT)) {
+    throw new Error('must provide a number');
+  }
+} catch (e) {
+  console.log('Please provide a number of cases to be migrated.');
+  process.exit(0);
+}
+
 const MIGRATE_LABEL = `Migrating ${MIGRATE_CASE_COUNT} cases`;
 
 (async () => {
@@ -39,8 +65,8 @@ const MIGRATE_LABEL = `Migrating ${MIGRATE_CASE_COUNT} cases`;
     .adminInitiateAuth({
       AuthFlow: 'ADMIN_NO_SRP_AUTH',
       AuthParameters: {
-        PASSWORD: '', // use env var...
-        USERNAME: email,
+        PASSWORD,
+        USERNAME,
       },
       ClientId,
       UserPoolId,
@@ -56,14 +82,23 @@ const MIGRATE_LABEL = `Migrating ${MIGRATE_CASE_COUNT} cases`;
     timeout: 30000,
   });
 
-  const posts = [];
-  for (let i = 0; i < MIGRATE_CASE_COUNT; i++) {
-    try {
-      posts.push(axiosInstance.post(migrationEndpoint, getMigratedCase()));
-    } catch (e) {
-      console.log(e);
-    }
+  let migratedCasesCompleted = 0;
+  const migrationsToProcess = range(0, MIGRATE_CASE_COUNT); // [0,1,2,3,4,5]
+  const chunkedJobs = chunk(migrationsToProcess, CHUNK_SIZE); // e.g. [[0,1,2], [3,4,5]]
+  for (let chunkJob of chunkedJobs) {
+    const chunkPromises = chunkJob.map(() =>
+      axiosInstance.post(migrationEndpoint, getMigratedCase()),
+    );
+    const results = await Promise.allSettled(chunkPromises);
+    results.forEach((p, i) => {
+      const isRejected = p.status == 'rejected';
+      if (isRejected) {
+        console.log('>>> Index failed at', i);
+      }
+    });
+    migratedCasesCompleted += chunkJob.length;
+    console.log('Migrated', migratedCasesCompleted);
   }
-  await Promise.all(posts);
+
   console.timeEnd(MIGRATE_LABEL);
 })();
