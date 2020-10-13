5ccf4aa7f8fa74cf9392481031840c57c2520c3f
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-10-13 13:32:44 -0700

    test fixes, update efcms-local

--- web-client/src/presenter/actions/CaseDeadline/validateSearchDeadlinesAction.js
+++ web-client/src/presenter/actions/CaseDeadline/validateSearchDeadlinesAction.js
@@ -14,8 +14,8 @@ export const validateSearchDeadlinesAction = ({
   get,
   path,
 }) => {
-  const startDate = get(state.screenMetadata.filterStartDateState);
-  const endDate = get(state.screenMetadata.filterEndDateState);
+  const startDate = get(state.screenMetadata.filterStartDate);
+  const endDate = get(state.screenMetadata.filterEndDate);
 
   const errors = applicationContext
     .getUseCases()
