17d46f105fcbc84cb5fc0f3e9bce67a905f30688
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-10-13 12:42:57 -0700

    6683: show spinner when retrieving new deadlines

--- web-client/src/presenter/sequences/updateDateRangeForDeadlinesSequence.js
+++ web-client/src/presenter/sequences/updateDateRangeForDeadlinesSequence.js
@@ -4,26 +4,29 @@ import { setAlertErrorAction } from '../actions/setAlertErrorAction';
 import { setCaseDeadlinesAction } from '../actions/CaseDeadline/setCaseDeadlinesAction';
 import { setValidationAlertErrorsAction } from '../actions/setValidationAlertErrorsAction';
 import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
+import { showProgressSequenceDecorator } from '../utilities/sequenceHelpers';
 import { startShowValidationAction } from '../actions/startShowValidationAction';
 import { stopShowValidationAction } from '../actions/stopShowValidationAction';
 import { updateDateRangeForDeadlinesAction } from '../actions/CaseDeadline/updateDateRangeForDeadlinesAction';
 import { validateSearchDeadlinesAction } from '../actions/CaseDeadline/validateSearchDeadlinesAction';
 
-export const updateDateRangeForDeadlinesSequence = [
-  validateSearchDeadlinesAction,
-  {
-    error: [
-      startShowValidationAction,
-      setAlertErrorAction,
-      setValidationErrorsAction,
-      setValidationAlertErrorsAction,
-    ],
-    success: [
-      clearAlertsAction,
-      updateDateRangeForDeadlinesAction,
-      stopShowValidationAction,
-      getCaseDeadlinesAction,
-      setCaseDeadlinesAction,
-    ],
-  },
-];
+export const updateDateRangeForDeadlinesSequence = showProgressSequenceDecorator(
+  [
+    validateSearchDeadlinesAction,
+    {
+      error: [
+        startShowValidationAction,
+        setAlertErrorAction,
+        setValidationErrorsAction,
+        setValidationAlertErrorsAction,
+      ],
+      success: [
+        clearAlertsAction,
+        updateDateRangeForDeadlinesAction,
+        stopShowValidationAction,
+        getCaseDeadlinesAction,
+        setCaseDeadlinesAction,
+      ],
+    },
+  ],
+);
