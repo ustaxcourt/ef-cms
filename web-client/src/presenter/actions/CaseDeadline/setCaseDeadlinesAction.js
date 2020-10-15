688711abeedf5084db5d227c2e606db39be803bf
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-10-15 09:19:13 -0700

    6683: load more button for case deadlines report

--- web-client/src/presenter/actions/CaseDeadline/setCaseDeadlinesAction.js
+++ web-client/src/presenter/actions/CaseDeadline/setCaseDeadlinesAction.js
@@ -7,6 +7,16 @@ import { state } from 'cerebral';
  * @param {object} providers.props the cerebral props object containing the props.caseDeadlines
  * @param {object} providers.store the cerebral store used for setting the state.allCaseDeadlines
  */
-export const setCaseDeadlinesAction = ({ props, store }) => {
-  store.set(state.allCaseDeadlines, props.caseDeadlines);
+export const setCaseDeadlinesAction = ({ get, props, store }) => {
+  let caseDeadlines = get(state.caseDeadlineReport.caseDeadlines);
+  if (caseDeadlines) {
+    caseDeadlines.push(...props.caseDeadlines);
+  } else {
+    ({ caseDeadlines } = props);
+  }
+  store.set(state.caseDeadlineReport.caseDeadlines, caseDeadlines);
+  store.set(state.caseDeadlineReport.totalCount, props.totalCount);
+
+  const page = get(state.caseDeadlineReport.page) || 1;
+  store.set(state.caseDeadlineReport.page, page + 1);
 };
