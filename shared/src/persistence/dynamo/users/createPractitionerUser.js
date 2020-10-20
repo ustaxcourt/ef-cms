a9c1190eac8caa254b0150f7b75740b0e501d84f
Michael Marcotte <mike.marcotte@gmail.com>
2020-10-19 21:54:04 -0400

    allow the system to create inactive practitioners

--- shared/src/persistence/dynamo/users/createPractitionerUser.js
+++ shared/src/persistence/dynamo/users/createPractitionerUser.js
@@ -53,7 +53,13 @@ exports.createUserRecords = async ({ applicationContext, user, userId }) => {
 exports.createPractitionerUser = async ({ applicationContext, user }) => {
   let userId = applicationContext.getUniqueId();
 
-  if (![ROLES.privatePractitioner, ROLES.irsPractitioner].includes(user.role)) {
+  if (
+    ![
+      ROLES.privatePractitioner,
+      ROLES.irsPractitioner,
+      ROLES.inactivePractitioner,
+    ].includes(user.role)
+  ) {
     throw new Error(
       'Practitioner users must have either private or IRS practitioner role',
     );
