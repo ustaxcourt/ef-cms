ffdedb0bd2e8b0ec9ef9f4d29b31adbcaaefdd2e
Eric Sorenson <eric@sorenson.software>
2020-10-23 13:43:05 -0500

    DevEx: move trial session working copy cypress tests out of smoketests and re-enable them
    These tests were no longer usable in smoke tests because of #4830

--- cypress/integration/trial-session-working-copy.spec.js
+++ cypress/integration/trial-session-working-copy.spec.js
@@ -8,7 +8,7 @@ const {
   goToTrialSession,
   markCaseAsQcCompleteForTrial,
   setTrialSessionAsCalendared,
-} = require('../../support/pages/trial-sessions');
+} = require('../../cypress-smoketests/support/pages/trial-sessions');
 const {
   completeWizardStep1,
   completeWizardStep2,
@@ -24,48 +24,26 @@ const {
   goToWizardStep5,
   hasIrsNotice,
   submitPetition,
-} = require('../../support/pages/create-electronic-petition');
-const {
-  confirmUser,
-  getRestApi,
-  getUserToken,
-  login,
-} = require('../../support/pages/login');
+} = require('../../cypress-smoketests/support/pages/create-electronic-petition');
 const {
   goToCaseOverview,
   manuallyAddCaseToNewTrialSession,
-} = require('../../support/pages/case-detail');
+} = require('../../cypress-smoketests/support/pages/case-detail');
 
 faker.seed(faker.random.number());
-
-let token = null;
-let adminToken = null;
 
 const testData = {
   docketNumbers: [],
   judgeName: 'Cohen',
   preferredTrialCity: 'Boise, Idaho',
-  trialClerk: 'Test trialclerk3',
+  trialClerk: 'Test Trial Clerk',
   trialSessionIds: [],
 };
 
-const DEFAULT_ACCOUNT_PASS = Cypress.env('DEFAULT_ACCOUNT_PASS');
-
 describe('Petitioner', () => {
-  before(async () => {
-    const results = await getUserToken(
-      'petitioner1@example.com',
-      DEFAULT_ACCOUNT_PASS,
-    );
-    token = results.AuthenticationResult.IdToken;
-  });
-
-  it('should be able to login', () => {
-    login(token);
-  });
-
   describe('should be able to create the first case', () => {
     it('should complete wizard step 1', () => {
+      cy.login('petitioner');
       goToStartCreatePetition();
       goToWizardStep1();
       completeWizardStep1();
@@ -122,18 +100,6 @@ describe('Petitioner', () => {
 // eslint-disable-next-line no-unused-vars
 let judgeUserId;
 describe('Petitions Clerk', () => {
-  before(async () => {
-    const results = await getUserToken(
-      'petitionsclerk1@example.com',
-      DEFAULT_ACCOUNT_PASS,
-    );
-    token = results.AuthenticationResult.IdToken;
-  });
-
-  it('should be able to login', () => {
-    login(token);
-  });
-
   describe('should create and set a trial session', () => {
     beforeEach(() => {
       cy.server();
@@ -143,6 +109,7 @@ describe('Petitions Clerk', () => {
     });
 
     it('creates a trial session', () => {
+      cy.login('petitionsclerk');
       createTrialSession(testData);
     });
 
@@ -165,21 +132,9 @@ describe('Petitions Clerk', () => {
   });
 });
 
-describe.skip('Judge', () => {
-  before(async () => {
-    const results = await getUserToken(
-      'judge.smoke@example.com',
-      DEFAULT_ACCOUNT_PASS,
-    );
-    token = results.AuthenticationResult.IdToken;
-  });
-
-  after(async () => {
-    // await disableUser({ userId: judgeUserId });
-  });
-
+describe('Judge', () => {
   it('should be able to login', () => {
-    login(token);
+    cy.login('judgeCohen');
   });
 
   it('views trial session working copy', () => {
@@ -203,43 +158,9 @@ describe.skip('Judge', () => {
   });
 });
 
-// Skipping this test until #4830 is done
-// This test is currently failing as the story involves importing current and legacy judges.
-// Currently, multiple entries with the same judge name are avilable in the judge dropdown and therefore
-// the 'correct' judge user is not being associated with the newly created trial session.
-describe.skip('Judge Chambers', () => {
-  before(async () => {
-    const chambersToCreate = {
-      email: 'smokeysChambers1@example.com',
-      name: 'Smokey Chambers',
-      password: DEFAULT_ACCOUNT_PASS,
-      role: 'chambers',
-      section: 'smokeysChambers',
-    };
-
-    const restApi = await getRestApi();
-
-    cy.request({
-      body: chambersToCreate,
-      headers: {
-        Authorization: `Bearer ${adminToken}`,
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-      url: `${restApi}/users`,
-    });
-
-    await confirmUser({ email: 'smokeysChambers1@example.com' });
-
-    const result = await getUserToken(
-      'smokeysChambers1@example.com',
-      DEFAULT_ACCOUNT_PASS,
-    );
-    token = result.AuthenticationResult.IdToken;
-  });
-
+describe('Judge Chambers', () => {
   it('should be able to login', () => {
-    login(token);
+    cy.login('cohensChambers');
   });
 
   it('views trial session working copy', () => {
