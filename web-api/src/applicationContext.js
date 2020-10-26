c3e7a470ab90a788116a7acb1c98845909c7de77
Cody Seibert <cseibert@flexion.us>
2020-10-26 15:45:00 -0400

    6833 - return accurate counts of case inventory report

--- web-api/src/applicationContext.js
+++ web-api/src/applicationContext.js
@@ -1294,7 +1294,7 @@ module.exports = appContextUser => {
       }
     },
     getConstants: () => ({
-      CASE_INVENTORY_MAX_PAGE_SIZE: 5000,
+      CASE_INVENTORY_MAX_PAGE_SIZE: 20000, // the Chief Judge will have ~15k records, so setting to 20k to be safe
       OPEN_CASE_STATUSES: Object.values(CASE_STATUS_TYPES).filter(
         status => status !== CASE_STATUS_TYPES.closed,
       ),
