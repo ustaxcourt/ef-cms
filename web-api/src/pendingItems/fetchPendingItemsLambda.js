2b63509e709eb23ed9c33115876b4cff0bd981c3
Joseph Sutton <jsutton@flexion.us>
2020-10-26 09:36:07 -0500

    trigger timeout

--- web-api/src/pendingItems/fetchPendingItemsLambda.js
+++ web-api/src/pendingItems/fetchPendingItemsLambda.js
@@ -8,6 +8,10 @@ const { genericHandler } = require('../genericHandler');
  */
 exports.fetchPendingItemsLambda = event =>
   genericHandler(event, async ({ applicationContext }) => {
+    await new Promise(resolve => {
+      setTimeout(resolve, 31000);
+    });
+
     return await applicationContext.getUseCases().fetchPendingItemsInteractor({
       applicationContext,
       ...event.queryStringParameters,
