3d674fd42a66300c1651be2220c8b26b59e53344
Joseph Sutton <jsutton@flexion.us>
2020-10-27 11:38:03 -0500

    pass responseCode

--- web-client/src/presenter/errors/ErrorFactory.js
+++ web-client/src/presenter/errors/ErrorFactory.js
@@ -9,20 +9,20 @@ export const ErrorFactory = {
   getError: e => {
     let responseCode = (e.response && e.response.status) || e.statusCode;
 
-    let newError = new ActionError({ ...e, responseCode });
+    let newError = new ActionError(e);
     if (403 == responseCode) {
-      newError = new UnauthorizedRequestError({ ...e, responseCode });
+      newError = new UnauthorizedRequestError(e);
     } else if (404 == responseCode) {
-      newError = new NotFoundError({ ...e, responseCode });
+      newError = new NotFoundError(e);
     } else if (401 == responseCode) {
-      newError = new UnidentifiedUserError({ ...e, responseCode });
+      newError = new UnidentifiedUserError();
     } else if (/^4/.test(responseCode)) {
-      newError = new InvalidRequestError({ ...e, responseCode });
+      newError = new InvalidRequestError(e);
     } else if (/^5/.test(responseCode)) {
-      newError = new ServerInvalidResponseError({ ...e, responseCode });
+      newError = new ServerInvalidResponseError(e);
     } else if (!e.response) {
       // this should only happen if cognito throws a cors exception due to expired tokens or invalid tokens
-      newError = new UnidentifiedUserError({ ...e, responseCode });
+      newError = new UnidentifiedUserError(e);
     }
     newError.originalError = e;
     return newError;
