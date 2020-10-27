c1a95e87036b3162742d6607070ba658ff76d189
Joseph Sutton <jsutton@flexion.us>
2020-10-27 12:40:13 -0500

    #6685: notify HB and modify UI

--- web-client/src/views/Interstitial.jsx
+++ web-client/src/views/Interstitial.jsx
@@ -30,11 +30,12 @@ export const Interstitial = connect(
               <h1 tabIndex="-1">We’re experiencing technical problems</h1>
               <h2>Where do you go from here?</h2>
               <p>
-                You can return to the previous screen and try again, or visit
-                the United States Tax Court website for information on court
-                services and contact information.
+                Try again, or contact dawson.support@ustaxcourt.gov for
+                assistance.
               </p>
-              <Button onClick={() => history.back()}>Back</Button>
+              <Button onClick={() => history.back()}>
+                Go Back to Try Again
+              </Button>
             </section>
           </>
         )}
