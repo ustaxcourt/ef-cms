3cc8f2b767d75f5b5cee2e2980d6bcc46b9124f3
Matt Hopson <matthopson@gmail.com>
2020-10-28 16:28:56 -0600

    Add support for work item elasticsearch index

---
+++ web-api/elasticsearch/efcms-work-item-mappings.js
@@ -0,0 +1,34 @@
+module.exports = {
+  properties: {
+    'associatedJudge.S': {
+      type: 'text',
+    },
+    'completedAt.S': {
+      type: 'text',
+    },
+    'completedBy.S': {
+      type: 'text',
+    },
+    'docketNumber.S': {
+      type: 'text',
+    },
+    'highPriority.BOOL': {
+      type: 'boolean',
+    },
+    'inProgress.BOOL': {
+      type: 'boolean',
+    },
+    'isRead.BOOL': {
+      type: 'boolean',
+    },
+    'pk.S': {
+      type: 'text',
+    },
+    'section.S': {
+      type: 'text',
+    },
+    'sk.S': {
+      type: 'text',
+    },
+  },
+};
