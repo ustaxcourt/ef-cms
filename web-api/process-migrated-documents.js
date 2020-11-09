396c9d6b1d1568fd5f54129f2ae3a2daf5912293
Mrinalini Sinha <mrinalinisinha05@gmail.cok>
2020-11-09 12:43:33 -0800

    WIP

--- web-api/process-migrated-documents.js
+++ web-api/process-migrated-documents.js
@@ -4,8 +4,8 @@
 //   scrapePdfContents
 //   save the text content to S3 with a documentContentsId as the file name
 //   add documentContentsId to the docket entry (this will trigger update in ES)
-
 const AWS = require('aws-sdk');
+const { isEmpty } = require('lodash');
 
 const check = (value, message) => {
   if (!value) {
@@ -30,9 +30,68 @@ const run = async () => {
   const { Contents, IsTruncated } = await storageClient
     .listObjectsV2({
       Bucket: `${EFCMS_DOMAIN}-documents-${ENV}-${REGION}`,
-      MaxKeys: 1000,
+      MaxKeys: 10,
     })
     .promise();
+
+  const getPdfJs = async () => {
+    const pdfjsLib = require('pdfjs-dist');
+    pdfjsLib.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.js';
+
+    return pdfjsLib;
+  };
+
+  let pdfjsLib;
+
+  pdfjsLib = await getPdfJs();
+
+  for (const pdfBuffer of Contents) {
+    try {
+      const document = await pdfjsLib.getDocument(pdfBuffer).promise;
+
+      let scrapedText = '';
+
+      for (let i = 1; i <= document.numPages; i++) {
+        const page = await document.getPage(i);
+        const pageTextContent = await page.getTextContent({
+          disableCombineTextItems: false,
+          normalizeWhitespace: false,
+        });
+
+        let lastY = null,
+          pageText = '';
+
+        for (let item of pageTextContent.items) {
+          if (lastY === item.transform[5] || !lastY) {
+            pageText += ' ' + item.str;
+          } else {
+            pageText += '\n' + item.str;
+          }
+          lastY = item.transform[5];
+        }
+
+        if (!isEmpty(pageText)) {
+          scrapedText = `${scrapedText}\n\n${pageText}`;
+        }
+      }
+
+      console.log(JSON.stringify(scrapedText, null, 2));
+      return scrapedText;
+    } catch (e) {
+      const pdfjsVersion = pdfjsLib && pdfjsLib.version;
+      // throw new Error(`Error scraping PDF with PDF.JS v${pdfjsVersion}`);
+      throw new Error(e);
+    }
+  }
+
+  // while (IsTruncated) {
+  //   await storageClient
+  //     .listObjectsV2({
+  //       Bucket: `${EFCMS_DOMAIN}-documents-${ENV}-${REGION}`,
+  //       MaxKeys: 1000,
+  //     })
+  //     .promise();
+  // }
 };
 
 run();
