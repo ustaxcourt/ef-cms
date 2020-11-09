e4bdb8b4094a59820f5b98bb671285fa92d2bced
Mrinalini Sinha <mrinalinisinha05@gmail.cok>
2020-11-09 12:48:57 -0800

    #7030 adding integration tests

--- web-client/integration-tests/docketClerkSealsCase.test.js
+++ web-client/integration-tests/docketClerkSealsCase.test.js
@@ -1,7 +1,12 @@
 import { applicationContextForClient as applicationContext } from '../../shared/src/business/test/createTestApplicationContext';
 import { associatedUserAdvancedSearchForSealedCase } from './journey/associatedUserAdvancedSearchForSealedCase';
 import { associatedUserViewsCaseDetailForSealedCase } from './journey/associatedUserViewsCaseDetailForSealedCase';
+import { docketClerkAddsDocketEntryFromOrder } from './journey/docketClerkAddsDocketEntryFromOrder';
+import { docketClerkCreatesAnOrder } from './journey/docketClerkCreatesAnOrder';
 import { docketClerkSealsCase } from './journey/docketClerkSealsCase';
+import { docketClerkServesDocument } from './journey/docketClerkServesDocument';
+import { docketClerkSignsOrder } from './journey/docketClerkSignsOrder';
+import { externalUserSearchesForAnOrderOnSealedCase } from './journey/externalUserSearchesForAnOrderOnSealedCase';
 import { loginAs, setupTest, uploadPetition } from './helpers';
 import { petitionsClerkAddsPractitionersToCase } from './journey/petitionsClerkAddsPractitionersToCase';
 import { petitionsClerkAddsRespondentsToCase } from './journey/petitionsClerkAddsRespondentsToCase';
@@ -43,6 +48,15 @@ describe('Docket Clerk seals a case', () => {
 
   loginAs(test, 'docketclerk@example.com');
   docketClerkSealsCase(test);
+  docketClerkCreatesAnOrder(test, {
+    documentTitle: 'Order for a sealed case',
+    eventCode: 'O',
+    expectedDocumentType: 'Order',
+    signedAtFormatted: '01/02/2020',
+  });
+  docketClerkSignsOrder(test, 0);
+  docketClerkAddsDocketEntryFromOrder(test, 0);
+  docketClerkServesDocument(test, 0);
 
   //verify that an internal user can still find this case via advanced search by name
   loginAs(test, 'petitionsclerk@example.com');
@@ -64,6 +78,7 @@ describe('Docket Clerk seals a case', () => {
   loginAs(test, 'privatePractitioner3@example.com');
   unassociatedUserViewsCaseDetailForSealedCase(test);
   unassociatedUserAdvancedSearchForSealedCase(test);
+  externalUserSearchesForAnOrderOnSealedCase(test);
 
   loginAs(test, 'irsPractitioner3@example.com');
   unassociatedUserViewsCaseDetailForSealedCase(test);
