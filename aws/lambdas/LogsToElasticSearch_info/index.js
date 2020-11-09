af712877637d55ebad6666ba6df69a06c5300e34
Andrew Dunkman <andrew.dunkman@gsa.gov>
2020-11-09 12:54:54 -0500

    Refactor parsing and transforming messages.
    Functionally equivalent changes

--- aws/lambdas/LogsToElasticSearch_info/index.js
+++ aws/lambdas/LogsToElasticSearch_info/index.js
@@ -10,34 +10,28 @@ const gunzip = promisify(zlib.gunzip);
 const logFailedResponses = true;
 
 exports.handler = async (input, context) => {
-  // decode input from base64
-  let zippedInput = new Buffer.from(input.awslogs.data, 'base64');
+  let payload;
 
-  // decompress the input
-  let buffer;
   try {
-    buffer = await gunzip(zippedInput);
+    payload = await decompressAndParse(input.awslogs.data);
   } catch (error) {
     context.fail(error);
     return;
   }
 
-  // parse the input from JSON
-  const awslogsData = JSON.parse(buffer.toString('utf8'));
-
-  // transform the input to Elasticsearch documents
-  const elasticsearchBulkData = transform(awslogsData);
-
-  // skip control messages
-  if (!elasticsearchBulkData) {
+  if (payload.messageType === 'CONTROL_MESSAGE') {
     console.log('Received a control message');
     context.succeed('Control message handled successfully');
     return;
   }
 
+  const inserts = payload.logEvents
+    .map(convertLogEventToElasticSearchInsert.bind(null, payload))
+    .join('\n');
+
   // post documents to the Amazon Elasticsearch Service
-  const { error, failedItems, statusCode, success } = post(
-    elasticsearchBulkData,
+  const { error, failedItems, statusCode, success } = await post(
+    inserts + '\n',
   );
 
   console.log(`Response: ${JSON.stringify({ statusCode })}`);
@@ -51,50 +45,41 @@ exports.handler = async (input, context) => {
   }
 };
 
-/**
- * Transform the log input to Elasticsearch documents
- *
- * @returns {string} JSON-encoded string of messages
- */
-function transform(payload) {
-  if (payload.messageType === 'CONTROL_MESSAGE') {
-    return null;
-  }
+const decompressAndParse = async data => {
+  const zippedInput = new Buffer.from(data, 'base64');
+  const buffer = await gunzip(zippedInput);
+  return JSON.parse(buffer.toString('utf8'));
+};
 
-  let bulkRequestBody = '';
+const convertLogEventToElasticSearchInsert = (payload, logEvent) => {
+  const timestamp = new Date(1 * logEvent.timestamp);
 
-  payload.logEvents.forEach(function (logEvent) {
-    let timestamp = new Date(1 * logEvent.timestamp);
+  // index name format: cwl-YYYY.MM.DD
+  const indexName = [
+    `cwl-${timestamp.getUTCFullYear()}`, // year
+    ('0' + (timestamp.getUTCMonth() + 1)).slice(-2), // month
+    ('0' + timestamp.getUTCDate()).slice(-2), // day
+  ].join('.');
 
-    // index name format: cwl-YYYY.MM.DD
-    let indexName = [
-      'cwl-' + timestamp.getUTCFullYear(), // year
-      ('0' + (timestamp.getUTCMonth() + 1)).slice(-2), // month
-      ('0' + timestamp.getUTCDate()).slice(-2), // day
-    ].join('.');
+  let source = {};
+  source['logdata'] = extractData(logEvent.message, logEvent.extractedFields);
+  source['@id'] = logEvent.id;
+  source['@timestamp'] = new Date(1 * logEvent.timestamp).toISOString();
+  source['@message'] = logEvent.message;
+  source['@owner'] = payload.owner;
+  source['@log_group'] = payload.logGroup;
+  source['@log_stream'] = payload.logStream;
 
-    let source = {};
-    source['logdata'] = extractData(logEvent.message, logEvent.extractedFields);
-    source['@id'] = logEvent.id;
-    source['@timestamp'] = new Date(1 * logEvent.timestamp).toISOString();
-    source['@message'] = logEvent.message;
-    source['@owner'] = payload.owner;
-    source['@log_group'] = payload.logGroup;
-    source['@log_stream'] = payload.logStream;
+  // Dangling underscores required by Elasticsearch’s bulk insert format
+  /* eslint-disable no-underscore-dangle */
+  let action = { index: {} };
+  action.index._index = indexName;
+  action.index._type = 'ef-cms-info';
+  action.index._id = logEvent.id;
+  /* eslint-enable no-underscore-dangle */
 
-    // Dangling underscores required by Elasticsearch’s bulk insert format
-    /* eslint-disable no-underscore-dangle */
-    let action = { index: {} };
-    action.index._index = indexName;
-    action.index._type = 'ef-cms-info';
-    action.index._id = logEvent.id;
-    /* eslint-enable no-underscore-dangle */
-
-    bulkRequestBody +=
-      [JSON.stringify(action), JSON.stringify(source)].join('\n') + '\n';
-  });
-  return bulkRequestBody;
-}
+  return [JSON.stringify(action), JSON.stringify(source)].join('\n');
+};
 
 /**
  * Extract log data from an individual log message
@@ -170,47 +155,56 @@ function isNumeric(n) {
 
 /**
  * Sends a POST request to Elasticsearch.
+ *
+ * @returns {Promise<Object>} information about the response
  */
-function post(body) {
-  const requestParams = buildRequest(process.env.es_endpoint, body);
+async function post(body) {
+  return new Promise(resolve => {
+    const requestParams = buildRequest(process.env.es_endpoint, body);
 
-  const request = https.request(requestParams, response => {
-    let responseBody = '';
-    response.on('data', chunk => (responseBody += chunk));
+    const request = https.request(requestParams, response => {
+      let responseBody = '';
+      response.on('data', chunk => (responseBody += chunk));
 
-    response.on('end', () => {
-      let info = JSON.parse(responseBody);
-      let failedItems;
-      let success;
-      let error;
+      response.on('end', () => {
+        let info = JSON.parse(responseBody);
+        let failedItems;
+        let success;
+        let error;
 
-      if (response.statusCode >= 200 && response.statusCode < 299) {
-        failedItems = info.items.filter(function (x) {
-          return x.index.status >= 300;
-        });
+        if (response.statusCode >= 200 && response.statusCode < 299) {
+          failedItems = info.items.filter(function (x) {
+            return x.index.status >= 300;
+          });
 
-        success = {
-          attemptedItems: info.items.length,
-          failedItems: failedItems.length,
-          successfulItems: info.items.length - failedItems.length,
-        };
-      }
+          success = {
+            attemptedItems: info.items.length,
+            failedItems: failedItems.length,
+            successfulItems: info.items.length - failedItems.length,
+          };
+        }
 
-      if (response.statusCode !== 200 || info.errors === true) {
-        // prevents logging of failed entries, but allows logging
-        // of other errors such as access restrictions
-        delete info.items;
-        error = {
-          responseBody: info,
+        if (response.statusCode !== 200 || info.errors === true) {
+          // prevents logging of failed entries, but allows logging
+          // of other errors such as access restrictions
+          delete info.items;
+          error = {
+            responseBody: info,
+            statusCode: response.statusCode,
+          };
+        }
+
+        resolve({
+          error,
+          failedItems,
           statusCode: response.statusCode,
-        };
-      }
-
-      return { error, failedItems, statusCode: response.statusCode, success };
+          success,
+        });
+      });
     });
-  });
 
-  request.end(requestParams.body);
+    request.end(requestParams.body);
+  });
 }
 
 /**
