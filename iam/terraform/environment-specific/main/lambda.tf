0c892a7f6337e34d5d22c1597e3240a436b63e97
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-11-10 08:47:22 -0800

    4662: add permission for lambda to access SQS queue

--- iam/terraform/environment-specific/main/lambda.tf
+++ iam/terraform/environment-specific/main/lambda.tf
@@ -115,7 +115,14 @@ resource "aws_iam_role_policy" "lambda_policy" {
                 "arn:aws:execute-api:us-west-1:${data.aws_caller_identity.current.account_id}:*"
             ],
             "Effect": "Allow"
-        }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "sqs:SendMessage"
+            ],
+            "Resource": "arn:aws:sqs:us-east-1:${data.aws_caller_identity.current.account_id}:migrate_legacy_documents_*"
+        } 
     ]
 }
 EOF
