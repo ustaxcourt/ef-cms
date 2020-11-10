4ad04d527fed942a0a5c6eb18ce321bb6c7286ac
Rachel Schneiderman <rschneiderman@flexion.us>
2020-11-10 15:01:31 -0800

    4662 - WIP implement handler for parsing legacy document contents [skip ci]

---
+++ shared/src/business/useCases/migration/parseLegacyDocumentInteractor.test.js
@@ -0,0 +1,116 @@
+const {
+  applicationContext,
+} = require('../../test/createTestApplicationContext');
+const {
+  parseLegacyDocumentsInteractor,
+} = require('./parseLegacyDocumentsInteractor');
+const { MOCK_CASE } = require('../../../test/mockCase');
+
+describe('parseLegacyDocumentsInteractor', () => {
+  const mockDocketEntryId = MOCK_CASE.docketEntries[0].docketEntryId;
+  const mockDocketNumber = '101-20';
+  const mockDocumentsBucketName = 'mock-documents';
+  const mockPdfTextContents = 'Gotta have my bowl, gotta have cereal';
+  const mockUniqueID = '5e8efb4f-898e-4b7a-993f-691a6ef56fc9';
+
+  beforeAll(() => {
+    applicationContext.environment.documentsBucketName = mockDocumentsBucketName;
+
+    applicationContext
+      .getPersistenceGateway()
+      .getCaseByDocketNumber.mockReturnValue(MOCK_CASE);
+
+    applicationContext.getUniqueId.mockReturnValue(mockUniqueID);
+
+    applicationContext
+      .getUtilities()
+      .scrapePdfContents.mockReturnValue(mockPdfTextContents);
+  });
+
+  it('should retrieve the docketEntry document from s3 using the provided docketEntryId', async () => {
+    await parseLegacyDocumentsInteractor({
+      applicationContext,
+      docketEntryId: mockDocketEntryId,
+      docketNumber: mockDocketNumber,
+    });
+
+    expect(
+      applicationContext.getStorageClient().getObject.mock.calls[0][0],
+    ).toEqual({
+      Bucket: mockDocumentsBucketName,
+      Key: mockDocketEntryId,
+    });
+  });
+
+  it('should retrieve the case from persistence using the provided docketNumber', async () => {
+    await parseLegacyDocumentsInteractor({
+      applicationContext,
+      docketEntryId: mockDocketEntryId,
+      docketNumber: mockDocketNumber,
+    });
+
+    expect(
+      applicationContext.getPersistenceGateway().getCaseByDocketNumber.mock
+        .calls[0][0].docketNumber,
+    ).toBe(mockDocketNumber);
+  });
+
+  it('should throw an error when the case does not have a docketEntry matching the provided docketEntryId', async () => {
+    await expect(
+      parseLegacyDocumentsInteractor({
+        applicationContext,
+        docketEntryId: 'abc-123',
+        docketNumber: mockDocketNumber,
+      }),
+    ).rejects.toThrow('Docket entry not found.');
+  });
+
+  it('should get the text content from the document retrieved from s3', async () => {
+    await parseLegacyDocumentsInteractor({
+      applicationContext,
+      docketEntryId: mockDocketEntryId,
+      docketNumber: mockDocketNumber,
+    });
+
+    expect(
+      applicationContext.getUtilities().scrapePdfContents,
+    ).toHaveBeenCalled();
+  });
+
+  it('should save the text content from the document to s3', async () => {
+    await parseLegacyDocumentsInteractor({
+      applicationContext,
+      docketEntryId: mockDocketEntryId,
+      docketNumber: mockDocketNumber,
+    });
+
+    expect(
+      applicationContext.getPersistenceGateway().saveDocumentFromLambda.mock
+        .calls[0][0].key,
+    ).toEqual(mockUniqueID);
+    expect(
+      applicationContext.getPersistenceGateway().saveDocumentFromLambda.mock
+        .calls[0][0].document,
+    ).toEqual(Buffer.from(JSON.stringify(mockPdfTextContents)));
+  });
+
+  it("should set the docketEntry's documentContentsId to the generated unique ID", async () => {
+    await parseLegacyDocumentsInteractor({
+      applicationContext,
+      docketEntryId: mockDocketEntryId,
+      docketNumber: mockDocketNumber,
+    });
+
+    expect(
+      applicationContext.getPersistenceGateway().updateCase.mock.calls[0][0]
+        .caseToUpdate.docketEntries,
+    ).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          docketEntryId: mockDocketEntryId,
+          documentContentsId: mockUniqueID,
+        }),
+      ]),
+    );
+  });
+});
