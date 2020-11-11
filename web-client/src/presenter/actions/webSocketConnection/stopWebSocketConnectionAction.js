da443430a979afc1b715f5b6bdd53ba4f19414d6
Joseph Sutton <jsutton@flexion.us>
2020-11-10 23:10:54 -0600

    #6830: implement an interval-based ping from the ui to the websocket

--- web-client/src/presenter/actions/webSocketConnection/stopWebSocketConnectionAction.js
+++ web-client/src/presenter/actions/webSocketConnection/stopWebSocketConnectionAction.js
@@ -1,9 +1,14 @@
+import { state } from 'cerebral';
+
 /**
  * stopWebSocketConnectionAction
  *
  * @param {object} providers the providers object
  * @param {object} providers.socket the socket object
  */
-export const stopWebSocketConnectionAction = ({ socket }) => {
+export const stopWebSocketConnectionAction = ({ get, socket, store }) => {
+  let pingInterval = get(state.wsPingInterval);
+  clearInterval(pingInterval);
+  store.set(state.wsPingInterval, undefined);
   socket.stop();
 };
