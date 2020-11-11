e7c3dbf37b0ba298a8a6ed15ecc0143dd5909b8d
Mrinalini Sinha <mrinalinisinha05@gmail.cok>
2020-11-11 08:45:58 -0800

    #4662 adding all required components to in-file application context

--- web-api/terraform/template/lambdas/legacy-documents-migration.js
+++ web-api/terraform/template/lambdas/legacy-documents-migration.js
@@ -1,14 +1,57 @@
 const AWS = require('aws-sdk');
-const sqs = new AWS.SQS({ region: 'us-east-1' });
 const {
   parseLegacyDocumentsInteractor,
 } = require('../../../../shared/src/business/useCases/migration/parseLegacyDocumentsInteractor');
+const {
+  getCaseByDocketNumber,
+} = require('../../../../shared/src/persistence/dynamo/cases/getCaseByDocketNumber');
+const {
+  updateCase,
+} = require('../../../../shared/src/persistence/dynamo/cases/updateCase');
+const {
+  saveDocumentFromLambda,
+} = require('../../../../shared/src/persistence/s3/saveDocumentFromLambda');
+import {
+  getUniqueId,
+} from '../../../../shared/src/sharedAppContext';
 
-// TODO - add everything we need for parseLegacyDocumentsInteractor to this applicationContext
+const {
+  S3,
+  SQS
+} = AWS;
+const sqs = () => {
+  return new SQS({  region: 'us-east-1' });
+}
+const environment = {
+  s3Endpoint: process.env.S3_ENDPOINT || 'localhost',
+  documentsBucketName: process.env.DOCUMENTS_BUCKET_NAME || '',
+};
+let s3Cache;
+
 const applicationContext = {
+  environment,
+  getPersistenceGateway: () => {
+    getCaseByDocketNumber,
+    updateCase,
+    saveDocumentFromLambda
+  },
   getUseCases: () => ({
     parseLegacyDocumentsInteractor,
   }),
+  getUtilities: () => ({
+    scrapePdfContents,
+  }),
+  getUniqueId,
+  getStorageClient: () => {
+    if (!s3Cache) {
+      s3Cache = new S3({
+        endpoint: environment.s3Endpoint,
+        region: 'us-east-1',
+        s3ForcePathStyle: true,
+      });
+    }
+    return s3Cache;
+  }
 };
 
 exports.applicationContext = applicationContext;
