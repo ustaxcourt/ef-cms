19ba667376cdf4fa866d08ebd5c91b0288d387bf
Rachel Schneiderman <rschneiderman@flexion.us>
2020-11-11 18:16:42 -0800

    4662 - hardcode dynamo table name to blue for now

--- web-api/terraform/template/legacy-document-migration.tf
+++ web-api/terraform/template/legacy-document-migration.tf
@@ -25,6 +25,7 @@ resource "aws_lambda_function" "legacy_documents_migration_lambda" {
       MASTER_REGION                      = "us-east-1"
       MASTER_DYNAMODB_ENDPOINT           = "dynamodb.us-east-1.amazonaws.com"
       DYNAMODB_ENDPOINT                  = "dynamodb.us-east-1.amazonaws.com"
+      DYNAMODB_TABLE_NAME                = var.blue_table_name
     }
   }
 }
