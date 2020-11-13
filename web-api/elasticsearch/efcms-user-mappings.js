29fae88f396d47cdf498f7169480d392a7094599
Kris Koskelin <kkoskelin@flexion.us>
2020-11-13 15:50:29 -0600

    efcms-user: query efficiency

--- web-api/elasticsearch/efcms-user-mappings.js
+++ web-api/elasticsearch/efcms-user-mappings.js
@@ -4,15 +4,18 @@ module.exports = {
       type: 'keyword',
     },
     'barNumber.S': {
+      index: false,
       type: 'keyword',
     },
     'contact.M.state.S': {
+      index: false,
       type: 'text',
     },
     'entityName.S': {
       type: 'text',
     },
     'indexedTimestamp.N': {
+      index: false,
       type: 'text',
     },
     'name.S': {
