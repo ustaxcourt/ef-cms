de1bf0d2f34e9d4f987b233723b699e24ca9c76b
Kris Koskelin <kkoskelin@flexion.us>
2020-11-17 11:08:42 -0600

    adding missed test rollback

--- shared/src/persistence/elasticsearch/getPractitionersByName.test.js
+++ shared/src/persistence/elasticsearch/getPractitionersByName.test.js
@@ -6,23 +6,34 @@ jest.mock('./searchClient');
 const { search } = require('./searchClient');
 
 describe('getPractitionersByName', () => {
-  it('returns results from a single query', async () => {
-    search.mockImplementation(async () => {
-      // default behavior
-      return {
-        results: [
-          { barNumber: '009', name: 'other' },
-          { barNumber: '005', name: 'matches' },
-        ],
-        total: 2,
-      };
-    });
+  it('returns results multiple queries, returning uniq values', async () => {
+    search
+      .mockImplementation(async () => {
+        // default behavior
+        return {
+          results: [
+            { barNumber: '009', name: 'other' },
+            { barNumber: '005', name: 'matches' },
+          ],
+          total: 2,
+        };
+      })
+      .mockImplementationOnce(async () => {
+        // first call
+        return {
+          results: [
+            { barNumber: '007', name: 'some' },
+            { barNumber: '005', name: 'matches' },
+          ],
+          total: 0,
+        };
+      });
 
     const results = await getPractitionersByName({
       applicationContext,
       name: 'some practitioner name',
     });
-    expect(search).toHaveBeenCalledTimes(1);
-    expect(results.length).toBe(2);
+    expect(search).toHaveBeenCalledTimes(2);
+    expect(results.length).toBe(3);
   });
 });
