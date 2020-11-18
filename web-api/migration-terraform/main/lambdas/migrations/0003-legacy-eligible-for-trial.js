056739e85c21fa0fca43df14ba699a33b1c6d4ab
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-11-18 11:53:01 -0800

    6906: migration to add eligible for trial records if they do not already exist

---
+++ web-api/migration-terraform/main/lambdas/migrations/0003-legacy-eligible-for-trial.js
@@ -0,0 +1,81 @@
+const createApplicationContext = require('../../../../src/applicationContext');
+const {
+  aggregateCaseItems,
+} = require('../../../../../shared/src/persistence/dynamo/helpers/aggregateCaseItems');
+const {
+  Case,
+} = require('../../../../../shared/src/business/entities/cases/Case');
+const {
+  CASE_STATUS_TYPES,
+} = require('../../../../../shared/src/business/entities/EntityConstants');
+
+const applicationContext = createApplicationContext({});
+
+const migrateItems = async (items, documentClient) => {
+  const itemsAfter = items;
+  for (const item of items) {
+    if (
+      item.pk.includes('case|') &&
+      item.sk.includes('case|') &&
+      item.status === CASE_STATUS_TYPES.generalDocketReadyForTrial
+    ) {
+      const eligibleForTrialRecords = await documentClient
+        .query({
+          ExpressionAttributeNames: {
+            '#gsi1pk': 'gsi1pk',
+          },
+          ExpressionAttributeValues: {
+            ':gsi1pk': `eligible-for-trial-case-catalog|${item.docketNumber}`,
+          },
+          IndexName: 'gsi1',
+          KeyConditionExpression: '#gsi1pk = :gsi1pk',
+          TableName: process.env.SOURCE_TABLE,
+        })
+        .promise()
+        .then(res => {
+          return res.Items;
+        });
+
+      if (!eligibleForTrialRecords || !eligibleForTrialRecords.length) {
+        const fullCase = await documentClient
+          .query({
+            ExpressionAttributeNames: {
+              '#pk': 'pk',
+            },
+            ExpressionAttributeValues: {
+              ':pk': `case|${item.docketNumber}`,
+            },
+            KeyConditionExpression: '#pk = :pk',
+            TableName: process.env.SOURCE_TABLE,
+          })
+          .promise()
+          .then(res => {
+            return res.Items;
+          });
+
+        const caseRecord = aggregateCaseItems(fullCase);
+        const caseEntity = new Case(caseRecord, {
+          applicationContext,
+        }).validate();
+
+        const { hybrid, nonHybrid } = caseEntity.generateTrialSortTags();
+
+        itemsAfter.push({
+          docketNumber: item.docketNumber,
+          gsi1pk: `eligible-for-trial-case-catalog|${item.docketNumber}`,
+          pk: 'eligible-for-trial-case-catalog',
+          sk: nonHybrid,
+        });
+        itemsAfter.push({
+          docketNumber: item.docketNumber,
+          gsi1pk: `eligible-for-trial-case-catalog|${item.docketNumber}`,
+          pk: 'eligible-for-trial-case-catalog',
+          sk: hybrid,
+        });
+      }
+    }
+  }
+  return itemsAfter;
+};
+
+exports.migrateItems = migrateItems;
