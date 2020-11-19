e0835def8a9c9438394365fdea2ddd564f58329e
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-11-19 08:08:34 -0800

    Fix error when reprocessing migration records

--- web-api/migration-terraform/main/lambdas/migration-segments.js
+++ web-api/migration-terraform/main/lambdas/migration-segments.js
@@ -1,4 +1,5 @@
 const AWS = require('aws-sdk');
+const createApplicationContext = require('../../../src/applicationContext');
 const {
   migrateItems: migration0001,
 } = require('./migrations/0001-user-case-add-closed-date');
@@ -11,6 +12,7 @@ const {
 const { chunk, isEmpty } = require('lodash');
 const MAX_DYNAMO_WRITE_SIZE = 25;
 
+const applicationContext = createApplicationContext({});
 const dynamodb = new AWS.DynamoDB({
   maxRetries: 10,
   region: 'us-east-1',
@@ -33,24 +35,19 @@ const migrateRecords = async ({ documentClient, items }) => {
 };
 
 const reprocessItems = async ({ documentClient, items }) => {
-  const moreUnprocessedItems = [];
-
-  items = await migrateRecords({ documentClient, items });
-
-  for (let item of items) {
-    const results = await documentClient
-      .batchWrite({
-        RequestItems: item,
-      })
-      .promise();
-
-    if (!isEmpty(results.UnprocessedItems)) {
-      moreUnprocessedItems.push(results.UnprocessedItems);
-    }
-  }
-
-  if (moreUnprocessedItems.length) {
-    await reprocessItems(moreUnprocessedItems);
+  // items already been migrated. they simply could not be processed in the batchWrite. Try again recursively
+  const numUnprocessed = items[process.env.DESTINATION_TABLE].length;
+  applicationContext.logger.info(`reprocessing ${numUnprocessed} items`);
+  const results = await documentClient
+    .batchWrite({
+      RequestItems: items,
+    })
+    .promise();
+  if (!isEmpty(results.UnprocessedItems)) {
+    await reprocessItems({
+      documentClient,
+      items: results.UnprocessedItems,
+    });
   }
 };
 
@@ -77,7 +74,7 @@ const processItems = async ({ documentClient, items }) => {
     if (!isEmpty(results.UnprocessedItems)) {
       await reprocessItems({
         documentClient,
-        items: [results.UnprocessedItems],
+        items: results.UnprocessedItems,
       });
     }
   }
