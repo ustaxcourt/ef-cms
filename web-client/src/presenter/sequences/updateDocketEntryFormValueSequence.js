1d6f3f74232c6032097f065acdb7c18d77c19b14
Rachel Schneiderman <rschneiderman@flexion.us>
2020-11-20 14:24:10 -0800

    6767 - Undo unrelated changes

--- web-client/src/presenter/sequences/updateDocketEntryFormValueSequence.js
+++ web-client/src/presenter/sequences/updateDocketEntryFormValueSequence.js
@@ -1,9 +1,7 @@
-import { generateTitleAction } from '../actions/FileDocument/generateTitleAction';
 import { setFileDocumentFormValueAction } from '../actions/setFileDocumentFormValueAction';
 import { updateDocketEntryWizardDataAction } from '../actions/DocketEntry/updateDocketEntryWizardDataAction';
 
 export const updateDocketEntryFormValueSequence = [
   setFileDocumentFormValueAction,
   updateDocketEntryWizardDataAction,
-  generateTitleAction,
 ];
