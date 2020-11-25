1d8ac53b99b935cc70d39b1d42f2480d10d3e3da
Cody Seibert <cseibert@flexion.us>
2020-11-25 14:03:23 -0500

    6903 - records for public api

--- web-client/switch-public-api-colors.js
+++ web-client/switch-public-api-colors.js
@@ -24,19 +24,19 @@ const run = async () => {
     const { items } = await apigateway.getRestApis({ limit: 500 }).promise();
 
     const apiGatewayRecord = items.find(
-      record => record.name === `gateway_api_public_${ENV}_${DEPLOYING_COLOR}`,
+      record => record.name === `gateway_api_${ENV}_${DEPLOYING_COLOR}`,
     );
 
     apigateway.deleteBasePathMapping(
       {
         basePath: '(none)',
-        domainName: `public-api.${EFCMS_DOMAIN}`,
+        domainName: `api.${EFCMS_DOMAIN}`,
       },
       () => {
         console.log('Successfully deleted base path mapping'); // successful response
         apigateway.createBasePathMapping(
           {
-            domainName: `public-api.${EFCMS_DOMAIN}`,
+            domainName: `api.${EFCMS_DOMAIN}`,
             restApiId: apiGatewayRecord.id,
             stage: ENV,
           },
