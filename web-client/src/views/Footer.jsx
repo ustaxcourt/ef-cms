33faef73120dcd2b4fe2fccb3757e3aa5583a5f5
Matt Hopson <matthopson@gmail.com>
2020-11-25 16:53:57 -0700

    7142 - WIP - Work on footer

--- web-client/src/views/Footer.jsx
+++ web-client/src/views/Footer.jsx
@@ -1,9 +1,84 @@
+import { DeployedDate } from './DeployedDate';
 import React from 'react';
+import seal from '../images/ustc_seal.svg';
 
 export const Footer = () => (
-  <footer className="usa-footer usa-footer-medium site" role="contentinfo">
-    <div className="footer-section-bottom">
-      <div className="grid-container" />
+  <footer className="usa-footer usa-footer--slim" id="app-footer">
+    <div className="usa-footer__primary-section">
+      <div className="usa-footer__primary-container grid-row">
+        <div className="grid-col-9" id="footer-links">
+          <nav aria-label="Footer navigation" className="usa-footer__nav">
+            <ul className="grid-row grid-gap" id="footer-links">
+              <li className="usa-footer__primary-content show-on-mobile">
+                <a
+                  className="usa-footer__primary-link"
+                  href="#top"
+                  // eslint-disable-next-line react/jsx-no-target-blank
+                  target="_blank"
+                >
+                  [ICN] Return to top
+                </a>
+              </li>
+              <li className="usa-footer__primary-content">
+                <a
+                  className="usa-footer__primary-link"
+                  href="https://ustaxcourt.gov/dawson.html"
+                  // eslint-disable-next-line react/jsx-no-target-blank
+                  target="_blank"
+                >
+                  Frequently Asked Questions [ICN]
+                </a>
+              </li>
+              <li className="usa-footer__primary-content">
+                <a className="usa-footer__primary-link" href="/privacy">
+                  Privacy
+                </a>
+              </li>
+              <li className="usa-footer__primary-content">
+                <a className="usa-footer__primary-link" href="/contact">
+                  Contact Us
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="grid-col-3 hide-on-mobile text-right">
+          <a className="usa-footer__primary-link" href="#top">
+            [ICN] Return to top
+          </a>
+        </div>
+      </div>
+    </div>
+    <div className="usa-footer__secondary-section">
+      <div className="grid-container">
+        <div className="grid-row">
+          <div className="grid-col-6 footer-left">
+            <div className="grid-container">
+              <div className="grid-row grid-gap-2 usa-footer__logo">
+                <div className="">
+                  <div className="usa-logo">
+                    <a href="/">
+                      <img alt="USTC Seal" src={seal} />
+                    </a>
+                  </div>
+                </div>
+
+                <div className="grid-col-9">
+                  <h3 className="usa-footer__logo-heading">
+                    United States Tax Court
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="grid-col-6 footer-right">
+            This is a U.S. government system. Your use indicates your consent to
+            monitoring and recording. Therefore, no expectation of privacy is to
+            be assumed. Misuse is subject to criminal and civil penalties.
+            <DeployedDate />
+          </div>
+        </div>
+      </div>
     </div>
   </footer>
 );
