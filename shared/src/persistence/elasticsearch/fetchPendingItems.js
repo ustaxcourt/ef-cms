b80479987b25a286bdfd2f575c3cfdc7916ec6b5
Mrinalini Sinha <msinha@flexion.us>
2020-12-01 14:50:05 -0800

    #7198 changing the way 7134 is toggled to make space for multiple feature toggles [skip ci]

--- shared/src/persistence/elasticsearch/fetchPendingItems.js
+++ shared/src/persistence/elasticsearch/fetchPendingItems.js
@@ -1,19 +1,24 @@
+const { isCodeEnabled } = require('../../../../codeToggles');
 const { search } = require('./searchClient');
 
-exports.fetchPendingItems = async ({ applicationContext, judge, page }) => {
-  const caseSource = [
-    'associatedJudge',
-    'caseCaption',
-    'docketNumber',
-    'docketNumberSuffix',
-    'status',
-  ];
-  const docketEntrySource = [
-    'docketEntryId',
-    'documentType',
-    'documentTitle',
-    'receivedAt',
-  ];
+exports.fetchPendingItems = async ({
+  applicationContext,
+  judge,
+  page,
+  source,
+}) => {
+  const caseSource = isCodeEnabled(7134)
+    ? [
+        'associatedJudge',
+        'caseCaption',
+        'docketNumber',
+        'docketNumberSuffix',
+        'status',
+      ]
+    : source;
+  const docketEntrySource = isCodeEnabled(7134)
+    ? ['docketEntryId', 'documentType', 'documentTitle', 'receivedAt']
+    : source;
 
   const { PENDING_ITEMS_PAGE_SIZE } = applicationContext.getConstants();
 
@@ -43,19 +48,7 @@ exports.fetchPendingItems = async ({ applicationContext, judge, page }) => {
           must: [
             { match: { 'pk.S': 'case|' } },
             { match: { 'sk.S': 'docket-entry|' } },
-            {
-              bool: {
-                minimum_should_match: 1,
-                should: [
-                  {
-                    exists: {
-                      field: 'servedAt',
-                    },
-                  },
-                  { term: { 'isLegacyServed.BOOL': true } },
-                ],
-              },
-            },
+
             { term: { 'pending.BOOL': true } },
             hasParentParam,
           ],
@@ -65,6 +58,34 @@ exports.fetchPendingItems = async ({ applicationContext, judge, page }) => {
     },
     index: 'efcms-docket-entry',
   };
+
+  if (isCodeEnabled(7198)) {
+    const matchingOnServedAtOrLegacyServed = {
+      bool: {
+        minimum_should_match: 1,
+        should: [
+          {
+            exists: {
+              field: 'servedAt',
+            },
+          },
+          { term: { 'isLegacyServed.BOOL': true } },
+        ],
+      },
+    };
+
+    searchParameters.body.query.bool.must.push(
+      matchingOnServedAtOrLegacyServed,
+    );
+  } else {
+    const matchingOnServedAt = {
+      exists: {
+        field: 'servedAt',
+      },
+    };
+
+    searchParameters.body.query.bool.must.push(matchingOnServedAt);
+  }
 
   if (judge) {
     hasParentParam.has_parent.query = {
@@ -83,5 +104,11 @@ exports.fetchPendingItems = async ({ applicationContext, judge, page }) => {
     searchParameters,
   });
 
-  return { foundDocuments: results, total };
+  let result = { results, total };
+
+  if (isCodeEnabled(7134)) {
+    result = { foundDocuments: results, total };
+  }
+
+  return result;
 };
