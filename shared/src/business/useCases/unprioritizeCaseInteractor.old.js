458b5364ade913da6f0399d8529af2f8a6949750
Rachel Schneiderman <rschneiderman@flexion.us>
2020-12-02 13:15:08 -0800

    7178 - Add files for code toggle and updated tests

---
+++ shared/src/business/useCases/unprioritizeCaseInteractor.old.js
@@ -0,0 +1,62 @@
+const {
+  isAuthorized,
+  ROLE_PERMISSIONS,
+} = require('../../authorization/authorizationClientService');
+const { Case } = require('../entities/cases/Case');
+const { UnauthorizedError } = require('../../errors/errors');
+
+/**
+ * used for removing the high priority from a case
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context
+ * @param {string} providers.docketNumber the docket number of the case to unprioritize
+ * @returns {object} the case data
+ */
+exports.unprioritizeCaseInteractor = async ({
+  applicationContext,
+  docketNumber,
+}) => {
+  const authorizedUser = applicationContext.getCurrentUser();
+
+  if (!isAuthorized(authorizedUser, ROLE_PERMISSIONS.PRIORITIZE_CASE)) {
+    throw new UnauthorizedError('Unauthorized');
+  }
+
+  const caseToUpdate = await applicationContext
+    .getPersistenceGateway()
+    .getCaseByDocketNumber({
+      applicationContext,
+      docketNumber,
+    });
+
+  const caseEntity = new Case(caseToUpdate, { applicationContext });
+
+  caseEntity.unsetAsHighPriority();
+
+  if (caseEntity.isReadyForTrial()) {
+    await applicationContext
+      .getPersistenceGateway()
+      .updateCaseTrialSortMappingRecords({
+        applicationContext,
+        caseSortTags: caseEntity.generateTrialSortTags(),
+        docketNumber: caseEntity.docketNumber,
+      });
+  } else {
+    await applicationContext
+      .getPersistenceGateway()
+      .deleteCaseTrialSortMappingRecords({
+        applicationContext,
+        docketNumber: caseEntity.docketNumber,
+      });
+  }
+
+  const updatedCase = await applicationContext
+    .getPersistenceGateway()
+    .updateCase({
+      applicationContext,
+      caseToUpdate: caseEntity.validate().toRawObject(),
+    });
+
+  return new Case(updatedCase, { applicationContext }).validate().toRawObject();
+};
