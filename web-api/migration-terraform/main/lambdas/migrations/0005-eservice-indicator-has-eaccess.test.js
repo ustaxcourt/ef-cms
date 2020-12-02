512e5cf8535e78425c996549bd0106544310d3a0
Joseph Sutton <jsutton@flexion.us>
2020-12-02 13:13:47 -0600

    #7197: added migration for serviceIndicators for petitioners if they have eAccess

---
+++ web-api/migration-terraform/main/lambdas/migrations/0005-eservice-indicator-has-eaccess.test.js
@@ -0,0 +1,114 @@
+const {
+  SERVICE_INDICATOR_TYPES,
+} = require('../../../../../shared/src/business/entities/EntityConstants');
+const { migrateItems } = require('./0005-eservice-indicator-has-eaccess');
+
+describe('migrateItems', () => {
+  let documentClient;
+
+  const PRIMARY_CONTACT_ID = '31db3d83-23c2-4726-bff6-aaf61b7ce1b6';
+  const SECONDARY_CONTACT_ID = 'bf99085c-ece1-4e34-a3b1-3b10669c4793';
+
+  let mockCase;
+  let mockCaseData = {
+    contactPrimary: {
+      contactId: PRIMARY_CONTACT_ID,
+      hasEAccess: true,
+    },
+    contactSecondary: { contactId: SECONDARY_CONTACT_ID },
+    docketNumber: '123-20',
+    pk: 'case|123-20',
+    sk: 'case|123-20',
+  };
+
+  beforeEach(() => {
+    // bypassing obj reference shenanigans
+    mockCase = JSON.parse(JSON.stringify(mockCaseData));
+  });
+
+  it('should set the serviceIndicator for contactPrimary', async () => {
+    const results = await migrateItems([{ ...mockCase }], documentClient);
+
+    expect(results).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          contactPrimary: expect.objectContaining({
+            serviceIndicator: SERVICE_INDICATOR_TYPES.SI_ELECTRONIC,
+          }),
+          pk: 'case|123-20',
+          sk: 'case|123-20',
+        }),
+      ]),
+    );
+  });
+
+  it('should set the serviceIndicator for contactSecondary', async () => {
+    const results = await migrateItems(
+      [
+        {
+          ...mockCase,
+          contactPrimary: {
+            ...mockCase.contactPrimary,
+            hasEAccess: false,
+          },
+          contactSecondary: {
+            ...mockCase.contactSecondary,
+            hasEAccess: true,
+          },
+          docketNumber: '125-20',
+          pk: 'case|125-20',
+          sk: 'case|125-20',
+        },
+      ],
+      documentClient,
+    );
+
+    expect(results).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          contactPrimary: expect.not.objectContaining({
+            serviceIndicator: SERVICE_INDICATOR_TYPES.SI_ELECTRONIC,
+          }),
+          contactSecondary: expect.objectContaining({
+            serviceIndicator: SERVICE_INDICATOR_TYPES.SI_ELECTRONIC,
+          }),
+          pk: 'case|125-20',
+          sk: 'case|125-20',
+        }),
+      ]),
+    );
+  });
+
+  it('should set the serviceIndicator for contactPrimary and contactSecondary', async () => {
+    const results = await migrateItems(
+      [
+        {
+          ...mockCase,
+          contactSecondary: {
+            ...mockCase.contactSecondary,
+            hasEAccess: true,
+          },
+          docketNumber: '124-20',
+          pk: 'case|124-20',
+          sk: 'case|124-20',
+        },
+      ],
+      documentClient,
+    );
+
+    expect(results).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          contactPrimary: expect.objectContaining({
+            serviceIndicator: SERVICE_INDICATOR_TYPES.SI_ELECTRONIC,
+          }),
+          contactSecondary: expect.objectContaining({
+            serviceIndicator: SERVICE_INDICATOR_TYPES.SI_ELECTRONIC,
+          }),
+          pk: 'case|124-20',
+          sk: 'case|124-20',
+        }),
+      ]),
+    );
+  });
+});
