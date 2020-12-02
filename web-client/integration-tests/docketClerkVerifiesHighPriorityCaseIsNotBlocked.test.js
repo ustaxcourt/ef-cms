ef8248af2b6989b3b0397eb1debdbb4c983496db
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-12-02 13:41:39 -0800

    7178: add integration test for high priority case with a pending item

--- web-client/integration-tests/docketClerkVerifiesHighPriorityCaseIsNotBlocked.test.js
+++ web-client/integration-tests/docketClerkVerifiesHighPriorityCaseIsNotBlocked.test.js
@@ -1,3 +1,4 @@
+import { AUTOMATIC_BLOCKED_REASONS } from '../../shared/src/business/entities/EntityConstants';
 import { docketClerkAddsPaperFiledPendingDocketEntryAndServes } from './journey/docketClerkAddsPaperFiledPendingDocketEntryAndServes';
 import { fakeFile, loginAs, setupTest, uploadPetition } from './helpers';
 import { petitionsClerkPrioritizesCase } from './journey/petitionsClerkPrioritizesCase';
@@ -13,43 +14,41 @@ describe('Docket clerk verifies high priority case is not blocked', () => {
     jest.setTimeout(30000);
   });
 
-  afterAll(() => {
-    test.closeSocket();
-  });
-
-  // if a case is set to high priority, and then one of its docket entries is set to pending, that case should no longer be blocked
-  // if the high priority label is removed from the case, then the case should be updated to be blocked with blocked reason being pending
-
-  //create a case
   loginAs(test, 'petitioner@example.com');
   it('login as a petitioner and create a case', async () => {
-    caseDetail = await uploadPetition(test);
+    caseDetail = await uploadPetition(test, {
+      preferredTrialCity: 'Lubbock, Texas',
+    });
+    test.docketNumber = caseDetail.docketNumber;
     expect(caseDetail.docketNumber).toBeDefined();
   });
 
-  //set it to high priority
   loginAs(test, 'petitionsclerk@example.com');
   petitionsClerkPrioritizesCase(test);
-  //add a pending item
+
   loginAs(test, 'docketclerk@example.com');
-  const mockDayReceived = 30;
-  const mockMonthReceived = '04';
-  const mockYearReceived = 2001;
-  docketClerkAddsPaperFiledPendingDocketEntryAndServes({
-    dayReceived: mockDayReceived,
-    fakeFile,
-    monthReceived: mockMonthReceived,
-    test,
-    yearReceived: mockYearReceived,
+  docketClerkAddsPaperFiledPendingDocketEntryAndServes(test, fakeFile);
+
+  it('verify that the high-priority case is not automaticBlocked', async () => {
+    await test.runSequence('gotoCaseDetailSequence', {
+      docketNumber: test.docketNumber,
+    });
+    expect(test.getState('caseDetail.automaticBlocked')).toBeFalsy();
   });
 
-  //check blocked
-  // verify that case is eligible for trial
   loginAs(test, 'petitionsclerk@example.com');
   petitionsClerkVerifyEligibleCase(test);
-  //unprioritize case
   petitionsClerkUnprioritizesCase(test);
-  //check blocked and reason for block
-  // verify that case is NOT eligible for trial
+
+  it('verify that the non-high-priority case is set to automaticBlocked', async () => {
+    await test.runSequence('gotoCaseDetailSequence', {
+      docketNumber: test.docketNumber,
+    });
+    expect(test.getState('caseDetail.automaticBlocked')).toBeTruthy();
+    expect(test.getState('caseDetail.automaticBlockedReason')).toBe(
+      AUTOMATIC_BLOCKED_REASONS.pending,
+    );
+  });
+
   petitionsClerkVerifyNotEligibleCase(test);
 });
