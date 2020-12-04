0e8d678b631d99e77d12061f60915bd3b8f688a2
Michael Marcotte <mike.marcotte@gmail.com>
2020-12-03 19:39:38 -0500

    591: add flag to bypass docket entry generation the api handler for updating a practitioner via a PUT request will look for an optional flag `bypassDocketEntry` that will allow us to update a practitioner's contact information and have that propagate to all cases to which they are associated. helps refactor the function thath handles this into more manageable pieces

--- web-api/src/practitioners/updatePractitionerUserLambda.js
+++ web-api/src/practitioners/updatePractitionerUserLambda.js
@@ -8,11 +8,14 @@ const { genericHandler } = require('../genericHandler');
  */
 exports.updatePractitionerUserLambda = event =>
   genericHandler(event, async ({ applicationContext }) => {
+    const { bypassDocketEntry = false, user } = JSON.parse(event.body);
+
     return await applicationContext
       .getUseCases()
       .updatePractitionerUserInteractor({
         applicationContext,
         barNumber: event.pathParameters.barNumber,
-        user: JSON.parse(event.body).user,
+        bypassDocketEntry: bypassDocketEntry || false,
+        user,
       });
   });
