4c088fdcffba8e809d0e11c19950543bf1843ff7
Michael Marcotte <mike.marcotte@gmail.com>
2020-12-07 16:01:14 -0500

    ustc-591: make getCasesByUserId include all cases

--- shared/src/persistence/elasticsearch/getCasesByUserId.test.js
+++ shared/src/persistence/elasticsearch/getCasesByUserId.test.js
@@ -1,13 +1,10 @@
 const {
   applicationContext,
 } = require('../../business/test/createTestApplicationContext');
-const {
-  CASE_STATUS_TYPES,
-} = require('../../business/entities/EntityConstants');
 const { getCasesByUserId } = require('./getCasesByUserId');
 
 describe('getCasesByUserId', () => {
-  it('obtains all open cases and closed cases (within the last six [6] months) associated with the given user', async () => {
+  it('obtains all cases associated with the given user', async () => {
     const userId = 'user-id-123';
 
     await getCasesByUserId({
@@ -17,7 +14,7 @@ describe('getCasesByUserId', () => {
 
     expect(
       applicationContext.getSearchClient().search.mock.calls[0][0].body.size,
-    ).toEqual(5000);
+    ).toEqual(10000);
     expect(
       applicationContext.getSearchClient().search.mock.calls[0][0].body.query,
     ).toMatchObject({
@@ -39,37 +36,7 @@ describe('getCasesByUserId', () => {
                     query: `"${userId}"`,
                   },
                 },
-                { match: { 'status.S': CASE_STATUS_TYPES.closed } },
-                {
-                  range: {
-                    'closedDate.S': {
-                      format: 'strict_date_time',
-                      gte: expect.anything(),
-                    },
-                  },
-                },
               ],
-            },
-          },
-          {
-            bool: {
-              must: [
-                { match: { 'pk.S': 'case|' } },
-                { match: { 'sk.S': 'case|' } },
-                {
-                  query_string: {
-                    fields: [
-                      'privatePractitioners.L.M.userId.S',
-                      'irsPractitioners.L.M.userId.S',
-                      'userId.S',
-                    ],
-                    query: `"${userId}"`,
-                  },
-                },
-              ],
-              must_not: {
-                match: { 'status.S': CASE_STATUS_TYPES.closed },
-              },
             },
           },
         ],
