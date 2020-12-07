5ab740eee90c17f0ac15c8748f81a20859b7f7fe
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-12-07 14:57:42 -0800

    7262: update associated judge on migrated deadlines to match case wip [skip ci]

---
+++ web-api/migration-terraform/main/lambdas/migrations/0008-associated-judge-on-deadlines.js
@@ -0,0 +1,54 @@
+const createApplicationContext = require('../../../../src/applicationContext');
+const {
+  CASE_STATUS_TYPES,
+} = require('../../../../../shared/src/business/entities/EntityConstants');
+const {
+  CaseDeadline,
+} = require('../../../../../shared/src/business/entities/CaseDeadline');
+const applicationContext = createApplicationContext({});
+
+const migrateItems = async (items, documentClient) => {
+  const itemsAfter = [];
+  for (const item of items) {
+    if (
+      item.pk.includes('case-deadline|') &&
+      item.sk.includes('case-deadline|')
+    ) {
+      const caseRecord = await documentClient
+        .get({
+          Key: {
+            pk: `case|${item.docketNumber}`,
+            sk: `case|${item.docketNumber}`,
+          },
+          TableName: process.env.SOURCE_TABLE,
+        })
+        .promise()
+        .then(res => {
+          return res.Item;
+        });
+
+      if (
+        caseRecord.status === CASE_STATUS_TYPES.calendared &&
+        caseRecord.associatedJudge !== item.associatedJudge
+      ) {
+        item.associatedJudge = caseRecord.associatedJudge;
+      }
+
+      const updatedDeadlineRaw = new CaseDeadline(item, {
+        applicationContext,
+      })
+        .validate()
+        .toRawObject();
+
+      itemsAfter.push({
+        ...item,
+        ...updatedDeadlineRaw,
+      });
+    } else {
+      itemsAfter.push(item);
+    }
+  }
+  return itemsAfter;
+};
+
+exports.migrateItems = migrateItems;
