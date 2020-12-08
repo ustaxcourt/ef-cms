5e7cb394b7fdfc361f9833c90e69b0b88403d36c
Rachel Schneiderman <rschneiderman@flexion.us>
2020-12-08 09:54:25 -0800

    7120 - persist calendarNotes on trialsession.caseOrder, refactor naming

--- shared/src/proxies/trialSessions/addCaseToTrialSessionProxy.js
+++ shared/src/proxies/trialSessions/addCaseToTrialSessionProxy.js
@@ -5,19 +5,20 @@ const { post } = require('../requests');
  *
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
+ * @param {string} providers.calendarNotes notes on why the case was added to the trial session
  * @param {object} providers.docketNumber the case docket number
  * @param {object} providers.trialSessionId the trial session id
  * @returns {Promise<*>} the promise of the api call
  */
 exports.addCaseToTrialSessionInteractor = ({
   applicationContext,
+  calendarNotes,
   docketNumber,
   trialSessionId,
-  trialSessionNote,
 }) => {
   return post({
     applicationContext,
-    body: { trialSessionNote },
+    body: { calendarNotes },
     endpoint: `/trial-sessions/${trialSessionId}/cases/${docketNumber}`,
   });
 };
