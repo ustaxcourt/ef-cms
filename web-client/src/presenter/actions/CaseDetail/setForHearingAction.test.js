8136e9d190e3c1fd84b2071a8ec6806c11aaca7d
Joseph Sutton <jsutton@flexion.us>
2020-12-08 12:44:37 -0600

    #7120: added modal validation, sequences, and actions (except actual interactor) for set hearing

---
+++ web-client/src/presenter/actions/CaseDetail/setForHearingAction.test.js
@@ -0,0 +1,9 @@
+// import { runAction } from 'cerebral/test';
+// import { setForHearingAction } from './setForHearingAction';
+
+// TODO: placeholder for now until the interactor is built
+describe('setForHearingAction', () => {
+  it('should have a condition to pass', async () => {
+    expect(true).toEqual(true);
+  });
+});
