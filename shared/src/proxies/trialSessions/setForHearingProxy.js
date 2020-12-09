5fc9de5f9d57e907a40e8fd95398373588e1e4da
Joseph Sutton <jsutton@flexion.us>
2020-12-09 13:55:03 -0600

    #7120: fixed 'notes' to be 'calendarNotes' to be consistent

--- shared/src/proxies/trialSessions/setForHearingProxy.js
+++ shared/src/proxies/trialSessions/setForHearingProxy.js
@@ -6,20 +6,20 @@ const { post } = require('../requests');
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
  * @param {string} providers.docketNumber the docketNumber of the case to add the trial session set for hearing
- * @param {string} providers.note the note to go onto the trial session set for hearing
+ * @param {string} providers.calendarNotes the calendarNotes to go onto the trial session set for hearing
  * @param {string} providers.trialSessionId the id of the trial session to set the calendar
  * @returns {Promise<*>} the promise of the api call
  */
 exports.setForHearingInteractor = ({
   applicationContext,
+  calendarNotes,
   docketNumber,
-  note,
   trialSessionId,
 }) => {
   return post({
     applicationContext,
     body: {
-      note,
+      calendarNotes,
     },
     endpoint: `/trial-sessions/${trialSessionId}/set-hearing/${docketNumber}`,
   });
