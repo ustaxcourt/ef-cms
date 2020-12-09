5d5c914070fcf74eb962e5b7b2604f6b17e8a020
Michael Marcotte <mike.marcotte@gmail.com>
2020-12-09 11:39:56 -0500

    ustc-591: adjust sequences so get a success alert

--- web-client/src/presenter/sequences/adminContactUpdateCompleteSequence.js
+++ web-client/src/presenter/sequences/adminContactUpdateCompleteSequence.js
@@ -1,11 +1,7 @@
-import { getUserContactEditCompleteAlertSuccessAction } from '../actions/getUserContactEditCompleteAlertSuccessAction';
-import { setAlertSuccessAction } from '../actions/setAlertSuccessAction';
 import { stopWebSocketConnectionAction } from '../actions/webSocketConnection/stopWebSocketConnectionAction';
 import { unsetUserContactEditProgressAction } from '../actions/unsetUserContactEditProgressAction';
 
 export const adminContactUpdateCompleteSequence = [
   stopWebSocketConnectionAction,
   unsetUserContactEditProgressAction,
-  getUserContactEditCompleteAlertSuccessAction,
-  setAlertSuccessAction,
 ];
