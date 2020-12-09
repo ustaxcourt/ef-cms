ece82eb90d54b364137e5d23b558a3fe864ee864
Michael Marcotte <mike.marcotte@gmail.com>
2020-12-08 22:02:27 -0500

    ustc-591: remove todo

--- web-client/src/presenter/sequences/adminContactUpdateErrorSequence.js
+++ web-client/src/presenter/sequences/adminContactUpdateErrorSequence.js
@@ -1,6 +1,3 @@
 import { stopWebSocketConnectionAction } from '../actions/webSocketConnection/stopWebSocketConnectionAction';
 
-export const adminContactUpdateErrorSequence = [
-  stopWebSocketConnectionAction,
-  // TODO: might need to update some state
-];
+export const adminContactUpdateErrorSequence = [stopWebSocketConnectionAction];
