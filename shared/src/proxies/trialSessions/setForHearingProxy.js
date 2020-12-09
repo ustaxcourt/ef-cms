d807a76b27e4fc14f6885cc3a6de36f97829cd23
Joseph Sutton <jsutton@flexion.us>
2020-12-09 09:32:09 -0600

    #7120: added jsdoc

--- shared/src/proxies/trialSessions/setForHearingProxy.js
+++ shared/src/proxies/trialSessions/setForHearingProxy.js
@@ -1,11 +1,12 @@
 const { post } = require('../requests');
 
 /**
- * set hearing for trial session and case
+ * trial session set for hearing on a case
  *
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
  * @param {string} providers.docketNumber the docketNumber of the case to add the trial session set for hearing
+ * @param {string} providers.note the note to go onto the trial session set for hearing
  * @param {string} providers.trialSessionId the id of the trial session to set the calendar
  * @returns {Promise<*>} the promise of the api call
  */
