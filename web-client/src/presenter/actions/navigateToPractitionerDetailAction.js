c80166674e833f3cc71bb1756de9eafa9bdb7910
Michael Marcotte <mike.marcotte@gmail.com>
2020-12-10 18:28:37 -0500

    ustc-591: add get to jsdoc

--- web-client/src/presenter/actions/navigateToPractitionerDetailAction.js
+++ web-client/src/presenter/actions/navigateToPractitionerDetailAction.js
@@ -4,6 +4,7 @@ import { state } from 'cerebral';
  * changes the route to the practitioner detail page for the given props.barNumber
  *
  * @param {object} providers the providers object
+ * @param {object} providers.get the cerebral function to retrieve information from state
  * @param {object} providers.router the riot.router object that is used for changing the route
  * @param {object} providers.props the cerebral props object
  * @returns {Promise} async action
