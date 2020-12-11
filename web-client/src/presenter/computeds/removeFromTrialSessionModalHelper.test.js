7cebc0015f533da758b0daab2b1851148eec8307
Joseph Sutton <jsutton@flexion.us>
2020-12-11 08:50:53 -0600

    #7120: added test coverage

--- web-client/src/presenter/computeds/removeFromTrialSessionModalHelper.test.js
+++ web-client/src/presenter/computeds/removeFromTrialSessionModalHelper.test.js
@@ -12,6 +12,10 @@ describe('removeFromTrialSessionModalHelper', () => {
 
   beforeEach(() => {
     state = {
+      caseDetail: {
+        status: 'Calendared',
+        trialSessionId: 'abc-123',
+      },
       modal: {
         caseStatus: 'Submitted',
       },
@@ -35,5 +39,29 @@ describe('removeFromTrialSessionModalHelper', () => {
     });
 
     expect(result.associatedJudgeRequired).toEqual(false);
+  });
+
+  it('uses current case status for defaultCaseStatus', () => {
+    const result = runCompute(removeFromTrialSessionModalHelper, {
+      state: {
+        ...state,
+      },
+    });
+
+    expect(result.defaultCaseStatus).toEqual('Calendared');
+  });
+
+  it('uses General Docket, Ready for Trial for defaultCaseStatus when the trialSessionId being removed is attached to the case', () => {
+    state.modal.trialSessionId = 'abc-123';
+
+    const result = runCompute(removeFromTrialSessionModalHelper, {
+      state: {
+        ...state,
+      },
+    });
+
+    expect(result.defaultCaseStatus).toEqual(
+      'General Docket - At Issue (Ready for Trial)',
+    );
   });
 });
