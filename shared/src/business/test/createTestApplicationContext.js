a649af0bae2e5a35a0110107c2ad7d4cf27862b2
Matt Hopson <matthopson@gmail.com>
2020-12-14 10:13:27 -0700

    7120 - Ensure trial session is updated when adding case to hearing

--- shared/src/business/test/createTestApplicationContext.js
+++ shared/src/business/test/createTestApplicationContext.js
@@ -337,7 +337,7 @@ const createTestApplicationContext = ({ user } = {}) => {
   });
 
   const mockGetPersistenceGateway = appContextProxy({
-    addHearingToCase: jest.fn(),
+    addCaseToHearing: jest.fn(),
     addWorkItemToSectionInbox,
     bulkDeleteRecords: jest.fn().mockImplementation(bulkDeleteRecords),
     bulkIndexRecords: jest.fn().mockImplementation(bulkIndexRecords),
