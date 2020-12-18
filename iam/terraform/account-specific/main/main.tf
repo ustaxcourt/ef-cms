93e72d3b2974f3ed1de9cfe230c74e3906e137ff
Andrew Dunkman <andrew.dunkman@gsa.gov>
2020-12-18 13:47:28 -0500

    Subscribe to log groups in us-west-1, api-public.

--- iam/terraform/account-specific/main/main.tf
+++ iam/terraform/account-specific/main/main.tf
@@ -1,5 +1,15 @@
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "us-east-1"
+}
+
+provider "aws" {
+  region = "us-west-1"
+  alias  = "us-west-1"
 }
 
 terraform {
---
+++ iam/terraform/account-specific/main/regional-log-subscription-filters/variables.tf
@@ -0,0 +1,7 @@
+variable "logs_to_elasticsearch_lambda_arn" {
+  type = string
+}
+
+variable "log_group_environments" {
+  type = list(string)
+}
