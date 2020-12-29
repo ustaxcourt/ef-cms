9e2c7ca377e434e6f582d71f700bffd348695a0d
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-12-29 14:30:14 -0800

    7446: fix issue with integration test docket number clash; remove logs

--- web-client/integration-tests/modifyPractitionerContactInfo.test.js
+++ web-client/integration-tests/modifyPractitionerContactInfo.test.js
@@ -21,9 +21,9 @@ describe('Modify Practitioner Contact Information', () => {
   let caseDetail;
   test.createdDocketNumbers = [];
 
-  loginAs(test, 'privatePractitioner2@example.com');
-  it('login as a practitioner and create 3 cases', async () => {
-    for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 3; i++) {
+    loginAs(test, 'privatePractitioner2@example.com');
+    it('login as a practitioner and create 3 cases', async () => {
       caseDetail = await uploadPetition(
         test,
         {},
@@ -31,11 +31,8 @@ describe('Modify Practitioner Contact Information', () => {
       );
       expect(caseDetail.docketNumber).toBeDefined();
       test.createdDocketNumbers.push(caseDetail.docketNumber);
-    }
-  });
-  it('got pasted creating cases', () => {
-    console.log('we got pass creating cases');
-  });
+    });
+  }
 
   it('waits for elasticsearch', async () => {
     await refreshElasticsearchIndex();
@@ -43,17 +40,7 @@ describe('Modify Practitioner Contact Information', () => {
 
   practitionerUpdatesAddress(test);
 
-  it('got here', () => {
-    console.log('we got pass the address updates');
-  });
-
   for (let i = 0; i < 3; i++) {
     practitionerViewsCaseDetailNoticeOfChangeOfAddress(test, i);
   }
-
-  it('got there', () => {
-    console.log(
-      'we got pass the practitionerViewsCaseDetailNoticeOfChangeOfAddress calls',
-    );
-  });
 });
