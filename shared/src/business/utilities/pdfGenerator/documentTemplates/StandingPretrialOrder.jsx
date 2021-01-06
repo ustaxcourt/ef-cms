445cc4e95d3f21a95603d6e62876129d84338672
Rachel Schneiderman <rschneiderman@flexion.us>
2021-01-06 13:34:53 -0800

    7443: WIP updating standing pretrial order pdf [skip ci]

--- shared/src/business/utilities/pdfGenerator/documentTemplates/StandingPretrialOrder.jsx
+++ shared/src/business/utilities/pdfGenerator/documentTemplates/StandingPretrialOrder.jsx
@@ -14,28 +14,324 @@ export const StandingPretrialOrder = ({ options, trialInfo }) => {
       />
 
       <p>
-        <strong>
+        <span className="text-bold">
           The attached Notice Setting Case for Trial notifies the parties that
-          this case is calendared for trial at the trial session beginning on
-          {trialInfo.fullStartDate}.
-        </strong>
+          this case is calendared for trial at the remote trial session
+          beginning on
+          {trialInfo.fullStartDate}. This Order sets out the Court’s standing
+          procedures for the remote trial session.
+        </span>
       </p>
 
       <p>
-        <strong className="text-underline">
-          Communication Between the Parties.
-        </strong>{' '}
-        The parties shall begin discussing settlement and/or preparation of a
-        stipulation of facts as soon as practicable. Valuation cases and
-        reasonable compensation cases are generally susceptible of settlement,
-        and the Court expects the parties to negotiate in good faith with this
-        goal in mind. All minor issues should be settled so that the Court can
-        focus on the issue(s) needing a Court decision. If a party has trouble
-        communicating with another party or complying with this Order, the
-        affected party should promptly advise the Court in writing, with a copy
-        to each other party, or request a conference call for the parties and
-        the trial Judge.
+        <span className="text-underline text-bold">About the Court.</span> The
+        U.S. Tax Court hears disputes between taxpayers (petitioners) and the
+        IRS (respondent). The Court is independent of, and not affiliated with,
+        the IRS. Documents previously given to the IRS are not part of the
+        record in this case and may not be considered unless made a part of this
+        case.
       </p>
+
+      <p>
+        <span className="text-underline text-bold">Contact Information.</span>{' '}
+        The parties must provide the Court with current contact information. If
+        your phone number, email, or mailing address changes, inform the Court
+        right away by filing a Notice of Change of Address form, available on
+        the Court s website,{' '}
+        <a href="https://www.ustaxcourt.gov">www.ustaxcourt.gov</a>
+      </p>
+
+      <p>
+        <span className="text-underline text-bold">
+          Electronic Filing (eFiling)
+        </span>{' '}
+        The Court encourages registration for DAWSON so that you can
+        electronically file and view documents in your case. To register for
+        DAWSON, call (202) 521-4629 or email{' '}
+        <a href="mailto:dawson.support@ustaxcourt.gov">
+          dawson.support@ustaxcourt.gov
+        </a>
+        . If you are not registered for eFiling, you must send the opposing
+        party a copy of any document you file with the Court. eFiling will
+        remain available to parties during the trial session. For more
+        information, see{' '}
+        <a href="https://www.ustaxcourt.gov">www.ustaxcourt.gov</a>.
+      </p>
+
+      <p>
+        <span className="text-underline text-bold">
+          Communication Between the Parties.
+        </span>{' '}
+        The parties must begin discussing settlement and/or preparation of a
+        stipulation of facts (facts on which the parties agree) as soon as
+        possible. All minor issues should be settled so that the Judge can focus
+        on the issue(s) needing a decision. Some cases may be susceptible to
+        partial or full settlement, and the Court expects the parties to
+        negotiate in good faith with this goal in mind. If a party has trouble
+        communicating with another party or complying with this Order, that
+        party should inform the Judge right away by filing a Status Report or
+        requesting a conference call by calling the Judge&apos;s chambers at the
+        phone number listed below.
+      </p>
+
+      <p>
+        <span className="text-underline text-bold">Language Barriers.</span> All
+        Court proceedings are conducted in English. All documents must be filed
+        in English or include a certified English translation. You should let
+        the Judge know as early as possible that you require help with English.
+        It is generally the responsibility of each petitioner to bring an
+        interpreter. If you give advance notice, the Court may have one
+        available.
+      </p>
+
+      <p>
+        <span className="text-underline text-bold">Technology.</span> For remote
+        proceedings, you must appear before the Judge as instructed in the
+        Notice Setting Case for Trial. That may be by telephone or by video.
+        Information on how to use Zoomgov, including tips, can be found on the
+        Court&apos;s website,{' '}
+        <a href="https://www.ustaxcourt.gov">www.ustaxcourt.gov</a>. A personal
+        Zoom account is not required, and there is no cost to you. If you have
+        any concerns about your ability to fully participate in a remote Court
+        proceeding, you should immediately let the Judge know.
+      </p>
+
+      <p>
+        <span className="text-underline text-bold">Sanctions.</span> The Court
+        may impose appropriate sanctions, including dismissal, for any unexcused
+        failure to comply with this Order. See Rule 131(b). Any failures may
+        also be considered in relation to sanctions against and disciplinary
+        proceedings involving counsel. See Rule 202(a).
+      </p>
+
+      <span>
+        To allow the efficient disposition of all cases on the trial calendar:
+      </span>
+
+      <span>
+        It is ORDERED that the parties comply with the following deadlines and
+        requirements, unless the Court, upon request, grants an extension:
+      </span>
+
+      <ol>
+        <li>
+          <p>
+            <span className="text-bold">
+              No later than{' '}
+              <span className="text-underline">
+                60 days before the first day of the trial session
+              </span>
+              :{' '}
+            </span>
+            If a party wants to ask the Judge to decide all or part of the case
+            without trial, the party may file a Motion for Summary Judgment.
+          </p>
+        </li>
+        <li>
+          <p>
+            <span className="text-bold">
+              No later than{' '}
+              <span className="text-underline">
+                45 days before the first day of the trial session
+              </span>
+              :{' '}
+            </span>
+            The parties should file any motions related to discovery or
+            stipulations.
+          </p>
+        </li>
+        <li>
+          <p>
+            <span className="text-bold">
+              No later than{' '}
+              <span className="text-underline">
+                31 days before the first day of the trial session
+              </span>
+              :{' '}
+            </span>
+            The parties may file any Motions for Continuance (postponement of
+            trial), which the Judge will grant only in exceptional
+            circumstances. See Rule 133, Tax Court Rules of Practice and
+            Procedure (available at{' '}
+            <a href="https://www.ustaxcourt.gov">www.ustaxcourt.gov</a>). Even
+            joint Motions for Continuance are not automatically granted.
+          </p>
+        </li>
+        <li>
+          <p>
+            <span className="text-bold">
+              No later than{' '}
+              <span className="text-underline">
+                30 days before the first day of the trial session
+              </span>
+              :{' '}
+            </span>
+            If a party plans to call an expert witness at trial, a Motion for
+            Leave to File an Expert Report, with the expert report attached
+            (lodged), should be filed. An expert witness’s testimony may be
+            excluded if the party fails to comply with this Order and Rule
+            143(g).
+          </p>
+        </li>
+        <li>
+          <p>
+            <span className="text-bold">
+              No later than{' '}
+              <span className="text-underline">
+                21 days before the first day of the trial session
+              </span>
+              :{' '}
+            </span>
+            The parties{' '}
+            <span className="text-underline text-bold">shall file</span> one of
+            the following: a Proposed Stipulated Decision, a Pretrial
+            Memorandum, a Motion to Dismiss for Lack of Prosecution, or a Status
+            Report.
+          </p>
+          <ol>
+            <li>
+              <p>
+                <span className="text-underline text-bold">Settlement.</span> If
+                a basis for settlement has been reached, the Proposed Stipulated
+                Decision shall be electronically filed no later than 21 days
+                before the first day of the trial session. If the parties have
+                reached a basis for settlement and need additional time to file
+                the Proposed Stipulated Decision, a joint Status Report
+                including a summary of the basis of settlement shall be filed no
+                later than 21 days before the first day of the trial session. A
+                Stipulation of Settled Issues should be attached, if available.
+                The Status Report shall state the reasons for delay in filing
+                the Proposed Stipulated Decision. The Court will issue an Order
+                specifying the date by which the Proposed Stipulated Decision
+                will be due. If a basis for settlement is reached after the
+                trial session begins, the Court will handle any required
+                scheduling on the record.
+              </p>
+            </li>
+            <li>
+              <p>
+                <span className="text-underline text-bold">
+                  Pretrial Memoranda.{' '}
+                </span>
+                If a basis for settlement has not been reached and it appears
+                that a trial is necessary, each party shall file a Pretrial
+                Memorandum no later than 21 days before the first day of the
+                trial session. A Pretrial Memorandum form is attached to this
+                Order.
+              </p>
+            </li>
+            <li>
+              <p>
+                <span className="text-underline text-bold"> Witnesses.</span>
+                Witnesses shall be identified in the pretrial memorandum with a
+                brief summary of their anticipated testimony. Witnesses who are
+                not identified will not be permitted to testify at the trial
+                without a showing of good cause.
+              </p>
+            </li>
+            <li>
+              <p>
+                <span className="text-underline text-bold">
+                  {' '}
+                  Motion to Dismiss for Lack of Prosecution.
+                </span>
+                If a party has been unresponsive and has failed to cooperate in
+                preparing the case for trial or resolution or to participate in
+                preparing a Stipulation of Facts, the opposing party shall file
+                a Motion to Dismiss for Lack of Prosecution no later than 21
+                days before the first day of the trial session.
+              </p>
+            </li>
+          </ol>
+        </li>
+        <li>
+          <p>
+            <span className="text-bold">
+              No later than{' '}
+              <span className="text-underline">
+                14 days before the first day of the trial session
+              </span>
+              :{' '}
+            </span>
+            The parties{' '}
+            <span className="text-underline text-bold">shall file</span> a
+            Stipulation of Facts together with all stipulated documents.
+            Documents and pages should be numbered for parties to identify
+            documents and pages within documents easily.
+          </p>
+          <ol>
+            <li>
+              <p>
+                <span className="text-underline text-bold">
+                  Stipulation of Facts.
+                </span>
+                All facts and documents shall be stipulated (agreed upon in
+                writing) to the maximum extent possible. If a complete
+                stipulation of facts is not ready for submission no later than
+                14 days before the first day of the trial session, or when
+                otherwise ordered by the Court, and if the Court determines that
+                this is due to lack of cooperation by either party, the Court
+                may order sanctions against the uncooperative party.
+              </p>
+            </li>
+            <li>
+              <p>
+                <span className="text-underline text-bold">
+                  Unagreed Trial Exhibits.
+                </span>
+                All documents or materials (except impeachment documents or
+                materials) that a party expects to use at trial that are not in
+                the Stipulation of Facts shall be marked and filed as Proposed
+                Trial Exhibits. The Court may refuse to receive in evidence any
+                document or material that is not filed as a Proposed Trial
+                Exhibit no later than 14 days before the first day of the trial
+                session.
+              </p>
+            </li>
+          </ol>
+        </li>
+        <li>
+          <p>
+            <span className="text-underline text-bold">
+              Change in Case Status.
+            </span>
+            Status Report shall be filed to inform the Court if the status of
+            the case changes at any time before the trial date and after a
+            Pretrial Memorandum, Motion to Dismiss for Lack of Prosecution, or
+            Status Report is filed. Alternatively, if the case has settled, a
+            Proposed Stipulated Decision may be filed.
+          </p>
+        </li>
+        <li>
+          <p>
+            <span className="text-underline text-bold">
+              Remote Proceeding Access.
+            </span>
+            Parties shall be responsible for ensuring, to the best of their
+            abilities, that they and their witnesses have adequate technology
+            and internet resources to participate in a remote proceeding. The
+            parties should log on and test their connections at least 30 minutes
+            before the proceedings scheduled time.
+          </p>
+        </li>
+        <li>
+          <p>
+            <span className="text-underline text-bold">Time of Trial.</span>
+            All parties shall be prepared for trial at any time during the trial
+            session unless a specific date has been previously set by the Court.
+            After Pretrial Memoranda are filed, the Court may schedule a time
+            and date certain for the trial. The parties may also jointly contact
+            the Judge’s chambers to request a time and date certain for the
+            trial. If practicable, the Court will attempt to accommodate the
+            request, keeping in mind other scheduling requirements and the
+            anticipated length of the session. Parties should jointly inform the
+            Judge as early as possible if they expect trial to require 3 days or
+            more.
+          </p>
+        </li>
+      </ol>
+
+      {/* the stuff below is oooooolllllddddd */}
 
       <p>
         <strong className="text-underline">Continuances.</strong> Continuances
@@ -54,19 +350,6 @@ export const StandingPretrialOrder = ({ options, trialInfo }) => {
         failure to comply with this Order. See Rule 131(b). Such failure may
         also be considered in relation to sanctions against and disciplinary
         proceedings involving counsel. See Rule 202(a).
-      </p>
-
-      <p>
-        <strong className="text-underline">Electronic Filing (eFiling)</strong>{' '}
-        eFiling is required for most documents (except the petition) filed by
-        parties represented by counsel in cases in which the petition is filed
-        on or after July 1, 2010. Petitioners not represented by counsel may,
-        but are not required to, eFile. For more information about eFiling and
-        the Court&apos;s other electronic services, see{' '}
-        <a href="www.ustaxcourt.gov" target="_blank">
-          www.ustaxcourt.gov
-        </a>
-        .
       </p>
 
       <div style={{ marginBottom: '90px', marginLeft: '20px' }}>
