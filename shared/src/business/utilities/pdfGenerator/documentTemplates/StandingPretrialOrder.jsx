6ff6515ffd196b39305aa43fa4214d9d2e421de2
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-01-06 15:46:03 -0800

    7443: more work on the remote trial session standing pretrial order updates [skip ci]

--- shared/src/business/utilities/pdfGenerator/documentTemplates/StandingPretrialOrder.jsx
+++ shared/src/business/utilities/pdfGenerator/documentTemplates/StandingPretrialOrder.jsx
@@ -13,17 +13,28 @@ export const StandingPretrialOrder = ({ options, trialInfo }) => {
         h3="Standing Pretrial Order"
       />
 
-      {/* add box with location and time */}
+      <div className="text-center card">
+        <p>
+          This case is set for trial at the {trialInfo.trialLocation} trial
+          session beginning at {trialInfo.startTime} on{' '}
+          {trialInfo.fullStartDate}.
+        </p>
+        <p>
+          <span className="text-bold">
+            This proceeding will be conducted remotely using Zoomgov.
+          </span>
+          <br />
+          (Please refer to the Notice Setting Case for Trial for log-in
+          instructions.)
+        </p>
+      </div>
 
-      <p>
-        <span className="text-bold">
-          The attached Notice Setting Case for Trial notifies the parties that
-          this case is calendared for trial at the remote trial session
-          beginning on {trialInfo.fullStartDate}. This Order sets out the
-          Court’s standing procedures for the remote trial session.
-        </span>
+      <p className="text-bold">
+        The attached Notice Setting Case for Trial notifies the parties that
+        this case is calendared for trial at the remote trial session beginning
+        on {trialInfo.fullStartDate}. This Order sets out the Court’s standing
+        procedures for the remote trial session.
       </p>
-
       <p>
         <span className="text-underline text-bold">About the Court.</span> The
         U.S. Tax Court hears disputes between taxpayers (petitioners) and the
@@ -128,8 +139,11 @@ export const StandingPretrialOrder = ({ options, trialInfo }) => {
               </span>
               :{' '}
             </span>
-            If a party wants to ask the Judge to decide all or part of the case
-            without trial, the party may file a Motion for Summary Judgment.
+            <span className="text-normal">
+              If a party wants to ask the Judge to decide all or part of the
+              case without trial, the party may file a Motion for Summary
+              Judgment.
+            </span>
           </p>
         </li>
         <li>
@@ -141,8 +155,10 @@ export const StandingPretrialOrder = ({ options, trialInfo }) => {
               </span>
               :{' '}
             </span>
-            The parties should file any motions related to discovery or
-            stipulations.
+            <span className="text-normal">
+              The parties should file any motions related to discovery or
+              stipulations.
+            </span>
           </p>
         </li>
         <li>
@@ -154,12 +170,14 @@ export const StandingPretrialOrder = ({ options, trialInfo }) => {
               </span>
               :{' '}
             </span>
-            The parties may file any Motions for Continuance (postponement of
-            trial), which the Judge will grant only in exceptional
-            circumstances. See Rule 133, Tax Court Rules of Practice and
-            Procedure (available at{' '}
-            <a href="https://www.ustaxcourt.gov">www.ustaxcourt.gov</a>). Even
-            joint Motions for Continuance are not automatically granted.
+            <span className="text-normal">
+              The parties may file any Motions for Continuance (postponement of
+              trial), which the Judge will grant only in exceptional
+              circumstances. See Rule 133, Tax Court Rules of Practice and
+              Procedure (available at{' '}
+              <a href="https://www.ustaxcourt.gov">www.ustaxcourt.gov</a>). Even
+              joint Motions for Continuance are not automatically granted.
+            </span>
           </p>
         </li>
         <li>
@@ -171,11 +189,13 @@ export const StandingPretrialOrder = ({ options, trialInfo }) => {
               </span>
               :{' '}
             </span>
-            If a party plans to call an expert witness at trial, a Motion for
-            Leave to File an Expert Report, with the expert report attached
-            (lodged), should be filed. An expert witness’s testimony may be
-            excluded if the party fails to comply with this Order and Rule
-            143(g).
+            <span className="text-normal">
+              If a party plans to call an expert witness at trial, a Motion for
+              Leave to File an Expert Report, with the expert report attached
+              (lodged), should be filed. An expert witness’s testimony may be
+              excluded if the party fails to comply with this Order and Rule
+              143(g).
+            </span>
           </p>
         </li>
         <li>
@@ -187,30 +207,35 @@ export const StandingPretrialOrder = ({ options, trialInfo }) => {
               </span>
               :{' '}
             </span>
-            The parties{' '}
-            <span className="text-underline text-bold">shall file</span> one of
-            the following: a Proposed Stipulated Decision, a Pretrial
-            Memorandum, a Motion to Dismiss for Lack of Prosecution, or a Status
-            Report.
+            <span className="text-normal">
+              The parties{' '}
+              <span className="text-underline text-bold">shall file</span> one
+              of the following: a Proposed Stipulated Decision, a Pretrial
+              Memorandum, a Motion to Dismiss for Lack of Prosecution, or a
+              Status Report.
+            </span>
           </p>
           <ol className="text-bold" type="A">
             <li>
               <p>
-                <span className="text-underline text-bold">Settlement</span>. If
-                a basis for settlement has been reached, the Proposed Stipulated
-                Decision shall be electronically filed no later than 21 days
-                before the first day of the trial session. If the parties have
-                reached a basis for settlement and need additional time to file
-                the Proposed Stipulated Decision, a joint Status Report
-                including a summary of the basis of settlement shall be filed no
-                later than 21 days before the first day of the trial session. A
-                Stipulation of Settled Issues should be attached, if available.
-                The Status Report shall state the reasons for delay in filing
-                the Proposed Stipulated Decision. The Court will issue an Order
-                specifying the date by which the Proposed Stipulated Decision
-                will be due. If a basis for settlement is reached after the
-                trial session begins, the Court will handle any required
-                scheduling on the record.
+                <span className="text-underline text-bold">Settlement</span>.{' '}
+                <span className="text-normal">
+                  If a basis for settlement has been reached, the Proposed
+                  Stipulated Decision shall be electronically filed no later
+                  than 21 days before the first day of the trial session. If the
+                  parties have reached a basis for settlement and need
+                  additional time to file the Proposed Stipulated Decision, a
+                  joint Status Report including a summary of the basis of
+                  settlement shall be filed no later than 21 days before the
+                  first day of the trial session. A Stipulation of Settled
+                  Issues should be attached, if available. The Status Report
+                  shall state the reasons for delay in filing the Proposed
+                  Stipulated Decision. The Court will issue an Order specifying
+                  the date by which the Proposed Stipulated Decision will be
+                  due. If a basis for settlement is reached after the trial
+                  session begins, the Court will handle any required scheduling
+                  on the record.
+                </span>
               </p>
             </li>
             <li>
@@ -218,22 +243,24 @@ export const StandingPretrialOrder = ({ options, trialInfo }) => {
                 <span className="text-underline text-bold">
                   Pretrial Memoranda
                 </span>
-                . If a basis for settlement has not been reached and it appears
-                that a trial is necessary, each party shall file a Pretrial
-                Memorandum no later than 21 days before the first day of the
-                trial session. A Pretrial Memorandum form is attached to this
-                Order.
+                <span className="text-normal">
+                  . If a basis for settlement has not been reached and it
+                  appears that a trial is necessary, each party shall file a
+                  Pretrial Memorandum no later than 21 days before the first day
+                  of the trial session. A Pretrial Memorandum form is attached
+                  to this Order.
+                </span>
               </p>
-            </li>
-            <li>
               <ol className="text-bold" type="I">
                 <li>
                   <p>
                     <span className="text-underline text-bold"> Witnesses</span>
-                    . Witnesses shall be identified in the pretrial memorandum
-                    with a brief summary of their anticipated testimony.
-                    Witnesses who are not identified will not be permitted to
-                    testify at the trial without a showing of good cause.
+                    <span className="text-normal">
+                      . Witnesses shall be identified in the pretrial memorandum
+                      with a brief summary of their anticipated testimony.
+                      Witnesses who are not identified will not be permitted to
+                      testify at the trial without a showing of good cause.
+                    </span>
                   </p>
                 </li>
               </ol>
@@ -244,11 +271,14 @@ export const StandingPretrialOrder = ({ options, trialInfo }) => {
                   {' '}
                   Motion to Dismiss for Lack of Prosecution
                 </span>
-                . If a party has been unresponsive and has failed to cooperate
-                in preparing the case for trial or resolution or to participate
-                in preparing a Stipulation of Facts, the opposing party shall
-                file a Motion to Dismiss for Lack of Prosecution no later than
-                21 days before the first day of the trial session.
+                <span className="text-normal">
+                  . If a party has been unresponsive and has failed to cooperate
+                  in preparing the case for trial or resolution or to
+                  participate in preparing a Stipulation of Facts, the opposing
+                  party shall file a Motion to Dismiss for Lack of Prosecution
+                  no later than 21 days before the first day of the trial
+                  session.
+                </span>
               </p>
             </li>
           </ol>
@@ -262,11 +292,13 @@ export const StandingPretrialOrder = ({ options, trialInfo }) => {
               </span>
               :{' '}
             </span>
-            The parties{' '}
-            <span className="text-underline text-bold">shall file</span> a
-            Stipulation of Facts together with all stipulated documents.
-            Documents and pages should be numbered for parties to identify
-            documents and pages within documents easily.
+            <span className="text-normal">
+              The parties{' '}
+              <span className="text-underline text-bold">shall file</span> a
+              Stipulation of Facts together with all stipulated documents.
+              Documents and pages should be numbered for parties to identify
+              documents and pages within documents easily.
+            </span>
           </p>
           <ol className="text-bold" type="A">
             <li>
@@ -274,13 +306,15 @@ export const StandingPretrialOrder = ({ options, trialInfo }) => {
                 <span className="text-underline text-bold">
                   Stipulation of Facts
                 </span>
-                . All facts and documents shall be stipulated (agreed upon in
-                writing) to the maximum extent possible. If a complete
-                stipulation of facts is not ready for submission no later than
-                14 days before the first day of the trial session, or when
-                otherwise ordered by the Court, and if the Court determines that
-                this is due to lack of cooperation by either party, the Court
-                may order sanctions against the uncooperative party.
+                <span className="text-normal">
+                  . All facts and documents shall be stipulated (agreed upon in
+                  writing) to the maximum extent possible. If a complete
+                  stipulation of facts is not ready for submission no later than
+                  14 days before the first day of the trial session, or when
+                  otherwise ordered by the Court, and if the Court determines
+                  that this is due to lack of cooperation by either party, the
+                  Court may order sanctions against the uncooperative party.
+                </span>
               </p>
             </li>
             <li>
@@ -288,13 +322,15 @@ export const StandingPretrialOrder = ({ options, trialInfo }) => {
                 <span className="text-underline text-bold">
                   Unagreed Trial Exhibits
                 </span>
-                . All documents or materials (except impeachment documents or
-                materials) that a party expects to use at trial that are not in
-                the Stipulation of Facts shall be marked and filed as Proposed
-                Trial Exhibits. The Court may refuse to receive in evidence any
-                document or material that is not filed as a Proposed Trial
-                Exhibit no later than 14 days before the first day of the trial
-                session.
+                <span className="text-normal">
+                  . All documents or materials (except impeachment documents or
+                  materials) that a party expects to use at trial that are not
+                  in the Stipulation of Facts shall be marked and filed as
+                  Proposed Trial Exhibits. The Court may refuse to receive in
+                  evidence any document or material that is not filed as a
+                  Proposed Trial Exhibit no later than 14 days before the first
+                  day of the trial session.
+                </span>
               </p>
             </li>
           </ol>
@@ -304,11 +340,13 @@ export const StandingPretrialOrder = ({ options, trialInfo }) => {
             <span className="text-underline text-bold">
               Change in Case Status
             </span>
-            . Status Report shall be filed to inform the Court if the status of
-            the case changes at any time before the trial date and after a
-            Pretrial Memorandum, Motion to Dismiss for Lack of Prosecution, or
-            Status Report is filed. Alternatively, if the case has settled, a
-            Proposed Stipulated Decision may be filed.
+            <span className="text-normal">
+              . Status Report shall be filed to inform the Court if the status
+              of the case changes at any time before the trial date and after a
+              Pretrial Memorandum, Motion to Dismiss for Lack of Prosecution, or
+              Status Report is filed. Alternatively, if the case has settled, a
+              Proposed Stipulated Decision may be filed.
+            </span>
           </p>
         </li>
         <li>
@@ -316,26 +354,30 @@ export const StandingPretrialOrder = ({ options, trialInfo }) => {
             <span className="text-underline text-bold">
               Remote Proceeding Access
             </span>
-            . Parties shall be responsible for ensuring, to the best of their
-            abilities, that they and their witnesses have adequate technology
-            and internet resources to participate in a remote proceeding. The
-            parties should log on and test their connections at least 30 minutes
-            before the proceedings scheduled time.
+            <span className="text-normal">
+              . Parties shall be responsible for ensuring, to the best of their
+              abilities, that they and their witnesses have adequate technology
+              and internet resources to participate in a remote proceeding. The
+              parties should log on and test their connections at least 30
+              minutes before the proceedings scheduled time.
+            </span>
           </p>
         </li>
         <li>
           <p>
-            <span className="text-underline text-bold">Time of Trial</span>. All
-            parties shall be prepared for trial at any time during the trial
-            session unless a specific date has been previously set by the Court.
-            After Pretrial Memoranda are filed, the Court may schedule a time
-            and date certain for the trial. The parties may also jointly contact
-            the Judge’s chambers to request a time and date certain for the
-            trial. If practicable, the Court will attempt to accommodate the
-            request, keeping in mind other scheduling requirements and the
-            anticipated length of the session. Parties should jointly inform the
-            Judge as early as possible if they expect trial to require 3 days or
-            more.
+            <span className="text-underline text-bold">Time of Trial</span>
+            <span className="text-normal">
+              . All parties shall be prepared for trial at any time during the
+              trial session unless a specific date has been previously set by
+              the Court. After Pretrial Memoranda are filed, the Court may
+              schedule a time and date certain for the trial. The parties may
+              also jointly contact the Judge’s chambers to request a time and
+              date certain for the trial. If practicable, the Court will attempt
+              to accommodate the request, keeping in mind other scheduling
+              requirements and the anticipated length of the session. Parties
+              should jointly inform the Judge as early as possible if they
+              expect trial to require 3 days or more.
+            </span>
           </p>
         </li>
       </ol>
@@ -358,7 +400,7 @@ export const StandingPretrialOrder = ({ options, trialInfo }) => {
       <div style={{ pageBreakAfter: 'always' }}></div>
 
       <p style={{ marginBottom: '74px', marginLeft: '393px' }}>
-        Trial Calendar: {trialInfo.city}, {trialInfo.state}
+        Trial Calendar: {trialInfo.trialLocation} - Remote Proceedings
         <br />
         <span style={{ marginLeft: '55px' }}>
           Date: {trialInfo.fullStartDate}
