f593970791455c4816c5bc999253c8c277f9040c
Mrinalini Sinha <msinha@flexion.us>
2021-01-08 11:55:53 -0800

    adding test around bug fix

--- shared/src/business/useCases/courtIssuedDocument/addServedStampToDocument.test.js
+++ shared/src/business/useCases/courtIssuedDocument/addServedStampToDocument.test.js
@@ -12,15 +12,18 @@ describe('addServedStampToDocument', () => {
   let drawTextMock;
   let saveMock;
   let getTrimBoxMock;
+  let trimBoxReturnValue;
 
   beforeAll(() => {
     getPageDimensions.mockReturnValue([0, 0]);
   });
 
   beforeEach(() => {
+    trimBoxReturnValue = { width: 0, y: 0 };
+
     drawTextMock = jest.fn();
     saveMock = jest.fn();
-    getTrimBoxMock = jest.fn();
+    getTrimBoxMock = jest.fn().mockImplementation(() => trimBoxReturnValue);
 
     PDFDocument.load.mockReturnValue(
       Promise.resolve({
@@ -32,9 +35,9 @@ describe('addServedStampToDocument', () => {
           {
             drawRectangle: jest.fn(),
             drawText: drawTextMock,
+            getTrimBox: getTrimBoxMock,
           },
         ]),
-        getTrimBox: getTrimBoxMock,
         save: saveMock,
       }),
     );
@@ -60,5 +63,19 @@ describe('addServedStampToDocument', () => {
     });
 
     expect(drawTextMock.mock.calls[0][0]).toEqual('SERVED 01/01/20');
+    expect(drawTextMock.mock.calls[0][1]).toMatchObject({ y: 12 });
+  });
+
+  it('increases the y value of the rectangle and stamp when the image in the pdf exceeds the pages size', async () => {
+    trimBoxReturnValue = { width: 0, y: 20 };
+
+    applicationContext.getUtilities().formatNow.mockReturnValue('01/01/20');
+
+    await addServedStampToDocument({
+      applicationContext,
+      pdfData: testPdfDoc,
+    });
+
+    expect(drawTextMock.mock.calls[0][1]).toMatchObject({ y: 32 });
   });
 });
