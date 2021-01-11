3aad3cae2ec0d4a54bb4c6c96b0cccbd06d1e974
Joseph Sutton <jsutton@flexion.us>
2021-01-11 09:17:52 -0600

    7487: include pdf encryption check for pdfs; delete from S3 if invalid

--- shared/src/business/useCases/pdf/validatePdfInteractor.js
+++ shared/src/business/useCases/pdf/validatePdfInteractor.js
@@ -24,7 +24,22 @@ exports.validatePdfInteractor = async ({ applicationContext, key }) => {
   applicationContext.logger.debug('pdfHeaderBytes', pdfHeaderBytes);
   applicationContext.logger.debug('pdfHeaderString', pdfHeaderString);
 
-  if (pdfHeaderString !== '%PDF-') {
+  let pdfIsEncrypted = false;
+
+  const { PDFDocument } = await applicationContext.getPdfLib();
+
+  const pdfDoc = await PDFDocument.load(pdfData, { ignoreEncryption: true });
+  pdfIsEncrypted = pdfDoc.isEncrypted;
+
+  applicationContext.logger.debug('pdfIsEncrypted', pdfIsEncrypted);
+
+  if (pdfIsEncrypted || pdfHeaderString !== '%PDF-') {
+    applicationContext.logger.debug('pdf invalid, deleting from S3', key);
+    await applicationContext.getPersistenceGateway().deleteDocumentFromS3({
+      applicationContext,
+      key,
+    });
+
     throw new Error('invalid pdf');
   }
 
