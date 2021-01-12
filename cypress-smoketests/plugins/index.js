53a591f7b5f66fcda514f2740db5125d43e841f1
Mrinalini Sinha <msinha@flexion.us>
2021-01-12 14:20:23 -0800

    fixing broken test

--- cypress-smoketests/plugins/index.js
+++ cypress-smoketests/plugins/index.js
@@ -15,6 +15,8 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+
+  //to console.log items in cypress tests, call  cy.task('log', 'message to log');
   on('task', {
     log(message) {
       console.log(message);
