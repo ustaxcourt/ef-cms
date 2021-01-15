5da6a95d8f93f28aef5ac62296432921a4a715b7
Joseph Sutton <jsutton@flexion.us>
2021-01-15 00:42:53 -0600

    fix integration tests

--- web-client/src/presenter/sequences/updateDocketEntryFormValueSequence.js
+++ web-client/src/presenter/sequences/updateDocketEntryFormValueSequence.js
@@ -1,9 +1,11 @@
 import { computeFormDateFactoryAction } from '../actions/computeFormDateFactoryAction';
+import { setComputeFormDateFactoryAction } from '../actions/setComputeFormDateFactoryAction';
 import { setFileDocumentFormValueAction } from '../actions/setFileDocumentFormValueAction';
 import { updateDocketEntryWizardDataAction } from '../actions/DocketEntry/updateDocketEntryWizardDataAction';
 
 export const updateDocketEntryFormValueSequence = [
   setFileDocumentFormValueAction,
   computeFormDateFactoryAction(null),
+  setComputeFormDateFactoryAction('serviceDate'),
   updateDocketEntryWizardDataAction,
 ];
