8b6f492a66ea62a61b3e7a38b6b0828ad4a376a7
Joseph Sutton <jsutton@flexion.us>
2021-01-15 07:31:04 -0600

    rename factory actions

---
+++ web-client/src/presenter/actions/getComputedFormDateFactoryAction.js
@@ -0,0 +1,45 @@
+import { state } from 'cerebral';
+
+export const getComputedFormDateFactoryAction = (prefix, toIsoString) => {
+  const computeFormDateAction = ({ applicationContext, get }) => {
+    let formYear;
+    let formMonth;
+    let formDay;
+    let computedDate = null;
+
+    if (prefix) {
+      formYear = get(state.form[`${prefix}Year`]);
+      formMonth = get(state.form[`${prefix}Month`]);
+      formDay = get(state.form[`${prefix}Day`]);
+    } else {
+      formYear = get(state.form.year);
+      formMonth = get(state.form.month);
+      formDay = get(state.form.day);
+    }
+
+    computedDate = applicationContext.getUtilities().computeDate({
+      day: formDay,
+      month: formMonth,
+      year: formYear,
+    });
+
+    if (
+      toIsoString &&
+      applicationContext
+        .getUtilities()
+        .isValidDateString(`${formMonth}-${formDay}-${formYear}`)
+    ) {
+      computedDate = applicationContext
+        .getUtilities()
+        .createISODateStringFromObject({
+          day: formDay,
+          month: formMonth,
+          year: formYear,
+        });
+    }
+
+    return { computedDate };
+  };
+
+  return computeFormDateAction;
+};
