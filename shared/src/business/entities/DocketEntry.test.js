f318e404bd84625169d5e235951d460b4b4018c5
Rachel Schneiderman <rschneiderman@flexion.us>
2021-01-18 13:36:32 -0800

    Devex 872: define isServed method on docketentry entity that checks isLegacyServed along with servedAt

--- shared/src/business/entities/DocketEntry.test.js
+++ shared/src/business/entities/DocketEntry.test.js
@@ -121,6 +121,35 @@ describe('DocketEntry entity', () => {
     });
   });
 
+  describe('isServed', () => {
+    it('should return false when servedAt is undefined and isLegacyServed is false', () => {
+      const doc1 = new DocketEntry(
+        { isLegacyServed: undefined, servedAt: undefined },
+        { applicationContext },
+      );
+
+      expect(doc1.isServed()).toBeFalsy();
+    });
+
+    it('should return true when servedAt is defined', () => {
+      const doc1 = new DocketEntry(
+        { isLegacyServed: undefined, servedAt: '2020-07-17T19:28:29.675Z' },
+        { applicationContext },
+      );
+
+      expect(doc1.isServed()).toBeTruthy();
+    });
+
+    it('should return true when servedAt is undefined and isLegacyServed is true', () => {
+      const doc1 = new DocketEntry(
+        { isLegacyServed: true, servedAt: undefined },
+        { applicationContext },
+      );
+
+      expect(doc1.isServed()).toBeTruthy();
+    });
+  });
+
   describe('signedAt', () => {
     it('should implicitly set a signedAt for Notice event codes', () => {
       const myDoc = new DocketEntry(
