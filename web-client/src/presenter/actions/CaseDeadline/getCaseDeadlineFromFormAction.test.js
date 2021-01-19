29884fcd03e6d49da298a14f747e6368e764c117
Rachel Schneiderman <rschneiderman@flexion.us>
2021-01-19 12:55:41 -0800

    devex 204: adding more action tests

--- web-client/src/presenter/actions/CaseDeadline/getCaseDeadlineFromFormAction.test.js
+++ web-client/src/presenter/actions/CaseDeadline/getCaseDeadlineFromFormAction.test.js
@@ -3,14 +3,39 @@ import { getCaseDeadlineFromFormAction } from './getCaseDeadlineFromFormAction';
 import { presenter } from '../../presenter-mock';
 import { runAction } from 'cerebral/test';
 
-presenter.providers.applicationContext = applicationContext;
-
 describe('getCaseDeadlineFromFormAction', () => {
   const computedDateFromProps = '2020-01-01T05:00:00.000Z';
   const mockJudge = 'Buch';
   const mockDocketNumber = '105-20';
 
-  it.only('returns a caseDeadline with props.comptedDate and form values', async () => {
+  presenter.providers.applicationContext = applicationContext;
+
+  it('should not include deadlineDate in the caseDeadline when props.computedDate is undefined', async () => {
+    const result = await runAction(getCaseDeadlineFromFormAction, {
+      modules: {
+        presenter,
+      },
+      props: {
+        computedDate: undefined,
+      },
+      state: {
+        caseDetail: {
+          associatedJudge: mockJudge,
+          docketNumber: mockDocketNumber,
+        },
+        form: {
+          day: 5,
+          month: 2,
+          searchError: {},
+          year: 1993,
+        },
+      },
+    });
+
+    expect(result.output.deadlineDate).toBeUndefined();
+  });
+
+  it('returns a caseDeadline with props.computedDate and form values when props.computedDeadline is defined', async () => {
     const result = await runAction(getCaseDeadlineFromFormAction, {
       modules: {
         presenter,
@@ -36,29 +61,6 @@ describe('getCaseDeadlineFromFormAction', () => {
       associatedJudge: mockJudge,
       deadlineDate: computedDateFromProps,
       docketNumber: mockDocketNumber,
-    });
-  });
-
-  it('gets all case deadlines with a page from state.caseDeadlineReport.page', async () => {
-    await runAction(getCaseDeadlineFromFormAction, {
-      modules: {
-        presenter,
-      },
-      state: {
-        caseDeadlineReport: {
-          page: 3,
-        },
-        screenMetadata: {
-          filterEndDate: END_DATE,
-          filterStartDate: computedDateFromProps,
-        },
-      },
-    });
-    expect(
-      applicationContext.getUseCases().getCaseDeadlinesInteractor.mock
-        .calls[0][0],
-    ).toMatchObject({
-      page: 3,
     });
   });
 });
