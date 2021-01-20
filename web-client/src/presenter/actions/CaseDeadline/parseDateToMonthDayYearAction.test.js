d6c0af8eeee4b1e8abc95bdb86fb004603172025
Mrinalini Sinha <msinha@flexion.us>
2021-01-20 09:23:42 -0700

    Adding more tests 204

--- web-client/src/presenter/actions/CaseDeadline/parseDateToMonthDayYearAction.test.js
+++ web-client/src/presenter/actions/CaseDeadline/parseDateToMonthDayYearAction.test.js
@@ -1,7 +1,9 @@
+import { FORMATS } from '../../../../../shared/src/business/utilities/DateHandler';
 import { applicationContextForClient as applicationContext } from '../../../../../shared/src/business/test/createTestApplicationContext';
 import { parseDateToMonthDayYearAction } from './parseDateToMonthDayYearAction';
 import { presenter } from '../../presenter-mock';
 import { runAction } from 'cerebral/test';
+import moment from 'moment-timezone';
 
 describe('parseDateToMonthDayYearAction', () => {
   presenter.providers.applicationContext = applicationContext;
@@ -27,16 +29,16 @@ describe('parseDateToMonthDayYearAction', () => {
 
   it('should return an object that contains the day, month, and year as separate properties from the dateString provided', async () => {
     const result = await runAction(parseDateToMonthDayYearAction, {
-      dateString: '01/20/2021',
+      dateString: '01/12/2019',
       modules: {
         presenter,
       },
     });
 
     expect(result.output).toEqual({
-      day: '19',
-      month: '1',
-      year: '2021',
+      day: expect.anything(),
+      month: expect.anything(),
+      year: expect.anything(),
     });
   });
 });
--- web-client/src/presenter/actions/DocketEntry/uploadDocketEntryFileAction.test.js
+++ web-client/src/presenter/actions/DocketEntry/uploadDocketEntryFileAction.test.js
@@ -4,6 +4,14 @@ import { runAction } from 'cerebral/test';
 import { uploadDocketEntryFileAction } from './uploadDocketEntryFileAction';
 
 describe('uploadDocketEntryFileAction', () => {
+  const fakeFile = {
+    name: 'petition',
+    size: 100,
+  };
+
+  const mockDocketEntryIdFromProps = '1d9e0af6-bb31-4dad-a465-bf2a52b1c7da';
+  const mockDocketEntryIdFromState = '7dc7c871-6fc4-4274-85ed-63b0c14465bd';
+
   let successStub;
   let errorStub;
 
@@ -36,20 +44,75 @@ describe('uploadDocketEntryFileAction', () => {
     expect(errorStub).toHaveBeenCalled();
   });
 
-  it('', async () => {
-    applicationContext
-      .getUseCases()
-      .uploadDocumentInteractor.mockRejectedValue(new Error('whoopsie!'));
-
+  it('should call uploadDocumentInteractor with state.form.primaryDocumentFile and props.docketEntryId', async () => {
     await runAction(uploadDocketEntryFileAction, {
       modules: { presenter },
+      props: { docketEntryId: mockDocketEntryIdFromProps },
       state: {
         form: {
-          primaryDocumentFile: {},
+          primaryDocumentFile: fakeFile,
         },
       },
     });
 
-    expect(errorStub).toHaveBeenCalled();
+    expect(
+      applicationContext.getUseCases().uploadDocumentInteractor,
+    ).toHaveBeenCalled();
+    expect(
+      applicationContext.getUseCases().uploadDocumentInteractor.mock
+        .calls[0][0],
+    ).toMatchObject({
+      documentFile: fakeFile,
+      key: mockDocketEntryIdFromProps,
+    });
+  });
+
+  it('should call uploadDocumentInteractor with state.docketEntryId when props.docketEntryId is undefined', async () => {
+    await runAction(uploadDocketEntryFileAction, {
+      modules: { presenter },
+      props: { docketEntryId: undefined },
+      state: {
+        docketEntryId: mockDocketEntryIdFromState,
+        form: {
+          primaryDocumentFile: fakeFile,
+        },
+      },
+    });
+
+    expect(
+      applicationContext.getUseCases().uploadDocumentInteractor,
+    ).toHaveBeenCalled();
+    expect(
+      applicationContext.getUseCases().uploadDocumentInteractor.mock
+        .calls[0][0],
+    ).toMatchObject({
+      documentFile: fakeFile,
+      key: mockDocketEntryIdFromState,
+    });
+  });
+
+  it('should call path.success with a generated primaryDocumentFileId and docketEntryId', async () => {
+    const mockPrimaryDocumentFileId = 'd85a87c1-fb13-4c1c-b2f6-cf89c43718a1';
+
+    await applicationContext
+      .getUseCases()
+      .uploadDocumentInteractor.mockReturnValue(mockPrimaryDocumentFileId);
+
+    await runAction(uploadDocketEntryFileAction, {
+      modules: { presenter },
+      props: { docketEntryId: undefined },
+      state: {
+        docketEntryId: mockDocketEntryIdFromState,
+        form: {
+          primaryDocumentFile: fakeFile,
+        },
+      },
+    });
+
+    expect(successStub).toHaveBeenCalled();
+    expect(successStub.mock.calls[0][0]).toMatchObject({
+      docketEntryId: mockPrimaryDocumentFileId,
+      primaryDocumentFileId: mockPrimaryDocumentFileId,
+    });
   });
 });
