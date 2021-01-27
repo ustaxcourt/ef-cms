c64d43c62be5f284445c637d621efc16e09cded7
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-01-27 08:53:00 -0800

    883: filter out duplicate docket numbers when calling batchGet for getCasesByDocketNumbers

--- shared/src/persistence/dynamo/cases/getCasesByDocketNumbers.test.js
+++ shared/src/persistence/dynamo/cases/getCasesByDocketNumbers.test.js
@@ -29,4 +29,30 @@ describe('getCasesByDocketNumbers', () => {
       },
     });
   });
+
+  it('should filter out duplicate docket numbers before calling batchGet', async () => {
+    await getCasesByDocketNumbers({
+      applicationContext,
+      docketNumbers: ['123-20', '124-20', '123-20'],
+    });
+
+    expect(
+      applicationContext.getDocumentClient().batchGet.mock.calls[0][0],
+    ).toMatchObject({
+      RequestItems: {
+        'efcms-local': {
+          Keys: [
+            {
+              pk: 'case|123-20',
+              sk: 'case|123-20',
+            },
+            {
+              pk: 'case|124-20',
+              sk: 'case|124-20',
+            },
+          ],
+        },
+      },
+    });
+  });
 });
