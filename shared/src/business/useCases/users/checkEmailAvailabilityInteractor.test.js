48076cd61e519355d77f151a14db416666623148
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2021-01-27 16:00:26 -0700

    74-6 adding more tests

---
+++ shared/src/business/useCases/users/checkEmailAvailabilityInteractor.test.js
@@ -0,0 +1,50 @@
+const {
+  applicationContext,
+} = require('../../test/createTestApplicationContext');
+const {
+  checkEmailAvailabilityInteractor,
+} = require('./checkEmailAvailabilityInteractor');
+
+describe('checkEmailAvailabilityInteractor', () => {
+  const mockEmail = 'test@example.com';
+
+  it('should attempt to retrieve the user by email', async () => {
+    await checkEmailAvailabilityInteractor({
+      applicationContext,
+      email: mockEmail,
+    });
+
+    expect(
+      applicationContext.getPersistenceGateway().getCognitoUserByEmail.mock
+        .calls[0][0],
+    ).toMatchObject({
+      email: mockEmail,
+    });
+  });
+
+  it('should return true if the specified email is not already in use', async () => {
+    applicationContext
+      .getPersistenceGateway()
+      .getCognitoUserByEmail.mockReturnValue(false);
+
+    const result = await checkEmailAvailabilityInteractor({
+      applicationContext,
+      email: mockEmail,
+    });
+
+    expect(result).toBeTruthy();
+  });
+
+  it('should return false if the specified email is already in use', async () => {
+    applicationContext
+      .getPersistenceGateway()
+      .getCognitoUserByEmail.mockReturnValue(true);
+
+    const result = await checkEmailAvailabilityInteractor({
+      applicationContext,
+      email: mockEmail,
+    });
+
+    expect(result).toBeFalsy();
+  });
+});
