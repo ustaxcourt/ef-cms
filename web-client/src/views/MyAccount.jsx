f44260e5a1360be98929fc6902e3707862ba64ab
Cody Seibert <cseibert@flexion.us>
2021-01-27 12:25:06 -0500

    7406 - change header button to Button.jsx, unit tests, style page with columns

--- web-client/src/views/MyAccount.jsx
+++ web-client/src/views/MyAccount.jsx
@@ -3,21 +3,27 @@ import { ErrorNotification } from './ErrorNotification';
 import { MyContactInformation } from './MyContactInformation';
 import { SuccessNotification } from './SuccessNotification';
 import { connect } from '@cerebral/react';
-import { state } from 'cerebral';
 import React from 'react';
 
-export const MyAccount = connect(
-  { user: state.user },
-  function MyAccount({ user }) {
-    return (
-      <React.Fragment>
-        <BigHeader text={'My Account'} />
-        <section className="usa-section grid-container">
-          <SuccessNotification />
-          <ErrorNotification />
-          <MyContactInformation />
-        </section>
-      </React.Fragment>
-    );
-  },
-);
+export const MyAccount = connect({}, function MyAccount() {
+  return (
+    <React.Fragment>
+      <BigHeader text={'My Account'} />
+      <section className="usa-section grid-container">
+        <SuccessNotification />
+        <ErrorNotification />
+
+        <div className="grid-container padding-x-0">
+          <div className="grid-row grid-gap">
+            <div className="grid-col-4">
+              <MyContactInformation />
+            </div>
+            <div className="grid-col-4">
+              {/* TODO: put the change email here */}
+            </div>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
+  );
+});
