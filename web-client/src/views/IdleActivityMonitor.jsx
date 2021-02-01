b11ba4a1c4c84ef18022852bea71ec968ec25657
Joseph Sutton <jsutton@flexion.us>
2021-02-01 12:10:12 -0600

    7501: experiment with broadcast channel lib

--- web-client/src/views/IdleActivityMonitor.jsx
+++ web-client/src/views/IdleActivityMonitor.jsx
@@ -2,7 +2,7 @@ import { AppTimeoutModal } from './AppTimeoutModal';
 import { BroadcastChannel } from 'broadcast-channel';
 import { connect } from '@cerebral/react';
 import { sequences, state } from 'cerebral';
-import IdleTimer from 'react-idle-timer';
+import { useIdleTimer } from 'react-idle-timer';
 import React, { useEffect, useRef } from 'react';
 
 export const IdleActivityMonitor = connect(
@@ -13,39 +13,70 @@ export const IdleActivityMonitor = connect(
     showAppTimeoutModalHelper: state.showAppTimeoutModalHelper,
   },
   function IdleActivityMonitor({
-    constants,
-    setIdleStatusActiveSequence,
-    setIdleStatusIdleSequence,
+    // constants,
+    // setIdleStatusActiveSequence,
+    // setIdleStatusIdleSequence,
     showAppTimeoutModalHelper,
   }) {
     const idleTimer = useRef(null);
+    const id = Math.random();
     let channelHandle;
     useEffect(() => {
       channelHandle = new BroadcastChannel('ustc'); // TODO getConstants().CHANNEL_NAME
-      channelHandle.onmessage = msg => console.log(msg);
-      console.log('I have a channel handle.');
-      return () => channelHandle.close();
+      channelHandle.onmessage = msg => {
+        console.log('incoming message', msg, id);
+      };
+      console.log('I have a channel handle.', id);
+      // return () => channelHandle.close();
     }, []);
-    const onActive = () => {
-      channelHandle.postMessage('what up dawg');
-      setIdleStatusActiveSequence();
+
+    const onActive = e => {
+      console.log('onActive event', e, id);
+      if (channelHandle) {
+        console.log('firing off active', id);
+        channelHandle.postMessage('what up dawg' + id);
+        // setIdleStatusActiveSequence();
+      } else {
+        console.log('no channel handle for onActive', id);
+      }
     };
     const onIdle = () => {
-      channelHandle.postMessage('zzzz');
-      setIdleStatusIdleSequence();
+      if (channelHandle) {
+        console.log('firing off idle', id);
+        channelHandle.postMessage('zzzz ' + id);
+        // setIdleStatusIdleSequence();
+      } else {
+        console.log('no channel handle for onIdle', id);
+      }
     };
+
+    if (showAppTimeoutModalHelper.beginIdleMonitor) {
+      console.log('using idle timer');
+      useIdleTimer({
+        debounce: 250,
+        onAction: onActive,
+        onActive,
+        onIdle,
+        ref: idleTimer,
+        timeout: 10000,
+      });
+    }
 
     return (
       <>
-        {showAppTimeoutModalHelper.beginIdleMonitor && (
-          <IdleTimer
-            debounce={constants.SESSION_DEBOUNCE}
-            ref={idleTimer}
-            timeout={constants.SESSION_TIMEOUT}
-            onActive={onActive}
-            onIdle={onIdle}
-          />
-        )}
+        {/* {showAppTimeoutModalHelper.beginIdleMonitor &&
+          console.log('rendering idle timer') && (
+            <IdleTimer
+              debounce={constants.SESSION_DEBOUNCE}
+              ref={idleTimer}
+              timeout={constants.SESSION_TIMEOUT}
+              onAction={e => {
+                console.log("hey i'm active");
+                onActive(e);
+              }}
+              onIdle={onIdle}
+            />
+          )} */}
         {showAppTimeoutModalHelper.showModal && <AppTimeoutModal />}
       </>
     );
