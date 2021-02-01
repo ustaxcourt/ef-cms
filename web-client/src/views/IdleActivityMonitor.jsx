f7a7fe73e67d015057cfe00662eb900e4f9c68f6
Kris Koskelin <kkoskelin@flexion.us>
2021-02-01 11:30:50 -0600

    7501: experiment with broadcast channel WIP
    [skip ci]

--- web-client/src/views/IdleActivityMonitor.jsx
+++ web-client/src/views/IdleActivityMonitor.jsx
@@ -1,8 +1,9 @@
 import { AppTimeoutModal } from './AppTimeoutModal';
+import { BroadcastChannel } from 'broadcast-channel';
 import { connect } from '@cerebral/react';
 import { sequences, state } from 'cerebral';
 import IdleTimer from 'react-idle-timer';
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 export const IdleActivityMonitor = connect(
   {
@@ -18,6 +19,21 @@ export const IdleActivityMonitor = connect(
     showAppTimeoutModalHelper,
   }) {
     const idleTimer = useRef(null);
+    let channelHandle;
+    useEffect(() => {
+      channelHandle = new BroadcastChannel('ustc'); // TODO getConstants().CHANNEL_NAME
+      channelHandle.onmessage = msg => console.log(msg);
+      console.log('I have a channel handle.');
+      return () => channelHandle.close();
+    }, []);
+    const onActive = () => {
+      channelHandle.postMessage('what up dawg');
+      setIdleStatusActiveSequence();
+    };
+    const onIdle = () => {
+      channelHandle.postMessage('zzzz');
+      setIdleStatusIdleSequence();
+    };
 
     return (
       <>
@@ -26,8 +42,8 @@ export const IdleActivityMonitor = connect(
             debounce={constants.SESSION_DEBOUNCE}
             ref={idleTimer}
             timeout={constants.SESSION_TIMEOUT}
-            onActive={setIdleStatusActiveSequence}
-            onIdle={setIdleStatusIdleSequence}
+            onActive={onActive}
+            onIdle={onIdle}
           />
         )}
         {showAppTimeoutModalHelper.showModal && <AppTimeoutModal />}
