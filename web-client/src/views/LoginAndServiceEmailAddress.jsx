1afeec9dbd967fd90e50e4ab17c9aa01a58aee36
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-02-01 09:49:23 -0800

    7406: DOD - keyboard accessibility updates

--- web-client/src/views/LoginAndServiceEmailAddress.jsx
+++ web-client/src/views/LoginAndServiceEmailAddress.jsx
@@ -1,13 +1,14 @@
 import { Button } from '../ustc-ui/Button/Button';
 import { connect } from '@cerebral/react';
-import { state } from 'cerebral';
+import { sequences, state } from 'cerebral';
 import React from 'react';
 
 export const LoginAndServiceEmailAddress = connect(
   {
+    navigateToPathSequence: sequences.navigateToPathSequence,
     user: state.user,
   },
-  function LoginAndServiceEmailAddress({ user }) {
+  function LoginAndServiceEmailAddress({ navigateToPathSequence, user }) {
     return (
       <div className="card">
         <div className="content-wrapper gray">
@@ -18,8 +19,12 @@ export const LoginAndServiceEmailAddress = connect(
             <Button
               link
               className="text-left"
-              href="/change-login-and-service-email"
               icon="edit"
+              onClick={() =>
+                navigateToPathSequence({
+                  path: '/change-login-and-service-email',
+                })
+              }
             >
               Change Email
             </Button>
