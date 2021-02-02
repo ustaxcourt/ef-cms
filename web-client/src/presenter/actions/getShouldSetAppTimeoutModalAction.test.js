2d2f862046cfe800e8cfd47c40a2a258cf57c3d5
Matt Hopson <matthopson@gmail.com>
2021-02-02 10:59:37 -0700

    7501 - use broadcast channel for getShouldSetAppTimeoutModalAction [skip ci]

--- web-client/src/presenter/actions/getShouldSetAppTimeoutModalAction.test.js
+++ web-client/src/presenter/actions/getShouldSetAppTimeoutModalAction.test.js
@@ -1,8 +1,11 @@
+import { applicationContextForClient as applicationContext } from '../../../../shared/src/business/test/createTestApplicationContext';
 import { getShouldSetAppTimeoutModalAction } from './getShouldSetAppTimeoutModalAction';
 import { presenter } from '../presenter-mock';
 import { runAction } from 'cerebral/test';
 
 describe('getShouldSetAppTimeoutModalAction', () => {
+  const { ACTIVE, IDLE } = applicationContext.getConstants().IDLE_STATUS;
+
   let noMock;
   let yesMock;
 
@@ -14,52 +17,71 @@ describe('getShouldSetAppTimeoutModalAction', () => {
       no: noMock,
       yes: yesMock,
     };
+
+    presenter.providers.applicationContext = applicationContext;
   });
 
-  it('should return the yes path if all app instances have idleStatus of idle', async () => {
-    // TODO use applicationContext.getConstants().IDLE_STATUS constants here
+  it('should communicate with the broadcast gateway to get messages on the `idleStatus` topic', async () => {
+    applicationContext.getBroadcastGateway().getMessages.mockResolvedValue([
+      {
+        idleStatus: IDLE,
+      },
+      {
+        idleStatus: ACTIVE,
+      },
+    ]);
+
     await runAction(getShouldSetAppTimeoutModalAction, {
       modules: {
         presenter,
       },
-      state: {
-        appInstanceId: 'foo',
-        appInstances: [
-          {
-            appInstanceId: 'bar',
-            idleStatus: 'idle',
-          },
-          {
-            appInstanceId: 'foo',
-            idleStatus: 'idle',
-          },
-        ],
+    });
+
+    expect(
+      applicationContext.getBroadcastGateway().sendMessage,
+    ).toHaveBeenCalledWith({ topic: 'idleStatus' });
+    expect(
+      applicationContext.getBroadcastGateway().getMessages,
+    ).toHaveBeenCalled();
+  });
+
+  it('should return the yes path if all app instances have idleStatus of idle', async () => {
+    applicationContext.getBroadcastGateway().getMessages.mockResolvedValue([
+      {
+        idleStatus: IDLE,
+      },
+      {
+        idleStatus: IDLE,
+      },
+    ]);
+
+    await runAction(getShouldSetAppTimeoutModalAction, {
+      modules: {
+        presenter,
       },
     });
 
+    expect(noMock).not.toHaveBeenCalled();
     expect(yesMock).toHaveBeenCalled();
   });
 
   it('should return the no path any app instance does not have idleStatus of idle', async () => {
+    applicationContext.getBroadcastGateway().getMessages.mockResolvedValue([
+      {
+        idleStatus: IDLE,
+      },
+      {
+        idleStatus: ACTIVE,
+      },
+    ]);
+
     await runAction(getShouldSetAppTimeoutModalAction, {
       modules: {
         presenter,
       },
-      state: {
-        appInstanceId: 'foo',
-        appInstances: [
-          {
-            appInstanceId: 'bar',
-            idleStatus: 'active',
-          },
-          {
-            appInstanceId: 'foo',
-            idleStatus: 'idle',
-          },
-        ],
-      },
     });
 
     expect(noMock).toHaveBeenCalled();
+    expect(yesMock).not.toHaveBeenCalled();
   });
 });
