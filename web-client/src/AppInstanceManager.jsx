efbecbe6d3b415d55c16fe37fd894352654379cd
Joseph Sutton <jsutton@flexion.us>
2021-02-02 12:34:12 -0600

    7501: tweaking AppInstanceManager, jsdoc cleanup [skip ci]

--- web-client/src/AppInstanceManager.jsx
+++ web-client/src/AppInstanceManager.jsx
@@ -1,51 +1,75 @@
 import { BroadcastChannel } from 'broadcast-channel';
+import { connect } from '@cerebral/react';
 import { sequences, state } from 'cerebral';
 import React, { useEffect, useRef } from 'react';
 
-export const AppInstanceManager = ({ appInstanceId }) => {
-  const channelHandle = new BroadcastChannel('ustc'); // TODO getConstants().CHANNEL_NAME
+export const AppInstanceManager = connect(
+  {
+    // updateIdleStatusSequence: sequences.updateIdleStatusSequence,
+    // broadcastIdleStatusSequence: sequences.broadcastIdleStatusSequence,
+  },
+  function AppInstanceManager(
+    {
+      // broadcastIdleStatusSequence,
+      // updateIdleStatusSequence
+    },
+  ) {
+    // TODO: use BroadcastGateway
+    const channelHandle = new BroadcastChannel('ustc'); // TODO getConstants().CHANNEL_NAME
 
-  /**
-   *
-   */
-  function ChannelMessage({ message, recipient, subject, threadId }) {
-    this.threadId = threadId;
-    this.recipient = recipient;
-    this.sender = appInstanceId;
-    this.subject = subject;
-    this.message = message;
-  }
-
-  channelHandle.onmessage = msg => {
-    if (msg.to && msg.to !== appInstanceId) {
-      return;
+    /**
+     * channel message object structure
+     *
+     * @param {object} providers
+     * @param {string} providers.message
+     * @param {string} providers.recipient
+     * @param {string} providers.subject
+     * @param {string} providers.threadId
+     */
+    function ChannelMessage({ message, recipient, subject, threadId }) {
+      this.threadId = threadId;
+      this.recipient = recipient;
+      this.sender = appInstanceId;
+      this.subject = subject;
+      this.message = message;
     }
-    switch (msg.topic) {
-      case 'idleLogout':
-        updateIdleStatusSequence(msg);
-        break;
-      case 'idle':
-        // setIdleStatusIdleSequence();
-        break;
-    }
-  };
 
-  const buttonClick = () => {
-    console.log('ping?');
-    channelHandle.postMessage(new ChannelMessage({ topic: 'ping' }));
-  };
+    channelHandle.onmessage = msg => {
+      if (msg.recipient && msg.recipient !== appInstanceId) {
+        return;
+      }
 
-  const onSetup = () => {
-    console.log("I'm setting up");
-  };
+      switch (msg.subject) {
+        case 'idleLogout':
+          // updateIdleStatusSequence(msg);
+          break;
+        case 'idleStatus':
+          // todo: get current idle status through sequence return value
+          // broadcastIdleStatusSequence();
+          break;
+        case 'idle':
+          // setIdleStatusIdleSequence();
+          break;
+      }
+    };
 
-  const onTeardown = () => {
-    console.log("I'm tearing down");
-  };
+    const buttonClick = () => {
+      console.log('ping?');
+      channelHandle.postMessage(new ChannelMessage({ subject: 'ping' }));
+    };
 
-  useEffect(() => {
-    onSetup();
-    return onTeardown;
-  }, []);
-  return <button onClick={buttonClick}>This is my AIM button</button>;
-};
+    const onSetup = () => {
+      console.log("I'm setting up");
+    };
+
+    const onTeardown = () => {
+      console.log("I'm tearing down");
+    };
+
+    useEffect(() => {
+      onSetup();
+      return onTeardown;
+    }, []);
+    return <button onClick={buttonClick}>This is my AIM button</button>;
+  },
+);
