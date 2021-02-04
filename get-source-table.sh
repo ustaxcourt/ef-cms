a0385d2690c891fae5e2167cbd41842f049c64a3
Michael Marcotte <mike.marcotte@gmail.com>
2021-02-04 14:44:49 -0500

    ustc-277: make sure deploy table exists before attempting to read from it

--- get-source-table.sh
+++ get-source-table.sh
@@ -12,7 +12,13 @@
 
 ENV=$1
 
-SOURCE_TABLE_VERSION=$(aws dynamodb get-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --key '{"pk":{"S":"source-table-version"},"sk":{"S":"source-table-version"}}' | jq -r ".Item.current.S")
-[ -z "$SOURCE_TABLE_VERSION" ] && echo "efcms-${ENV}" && exit 
+EXISTS=$(aws dynamodb list-tables --region us-east-1 | grep efcms-deploy-${ENV} | wc -l)
 
-echo "efcms-${ENV}-${SOURCE_TABLE_VERSION}"
+if [ "${EXISTS}" -eq "1" ]; then
+  SOURCE_TABLE_VERSION=$(aws dynamodb get-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --key '{"pk":{"S":"source-table-version"},"sk":{"S":"source-table-version"}}' | jq -r ".Item.current.S")
+  [ -z "$SOURCE_TABLE_VERSION" ] && echo "efcms-${ENV}-alpha" && exit 
+
+  echo "efcms-${ENV}-${SOURCE_TABLE_VERSION}"
+else
+  echo "efcms-${ENV}-alpha"
+fi
