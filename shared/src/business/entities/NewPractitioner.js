073a906d54457291b08e84b9ef90111d377ee280
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2021-02-05 13:05:16 -0700

    WIP 7574 adding validation for new email [skip ci]

--- shared/src/business/entities/NewPractitioner.js
+++ shared/src/business/entities/NewPractitioner.js
@@ -22,15 +22,26 @@ NewPractitioner.prototype.init = function init(rawUser) {
   Practitioner.prototype.init.call(this, rawUser);
   this.firstName = rawUser.firstName;
   this.lastName = rawUser.lastName;
+  this.updatedEmail = rawUser.updatedEmail;
+  this.confirmEmail = rawUser.confirmEmail;
 };
 
 NewPractitioner.validationName = 'Practitioner';
 
 const VALIDATION_ERROR_MESSAGES = {
   ...Practitioner.VALIDATION_ERROR_MESSAGES,
+  confirmEmail: [
+    {
+      contains: 'must be [ref:updatedEmail]',
+      message: 'Email addresses do not match',
+    },
+    { contains: 'is required', message: 'Enter a valid email address' },
+    { contains: 'must be a valid', message: 'Enter a valid email address' },
+  ],
   email: 'Enter email address',
   firstName: 'Enter first name',
   lastName: 'Enter last name',
+  updatedEmail: 'Enter a valid email address',
 };
 
 joiValidationDecorator(
@@ -39,10 +50,16 @@ joiValidationDecorator(
     ...Practitioner.validationRules,
     admissionsStatus: JoiValidationConstants.STRING.required(),
     barNumber: JoiValidationConstants.STRING.optional().allow(null),
+    confirmEmail: JoiValidationConstants.EMAIL.when('updatedEmail', {
+      is: joi.exist().not(null),
+      otherwise: joi.optional().allow(null),
+      then: joi.valid(joi.ref('updatedEmail')).required(),
+    }),
     email: JoiValidationConstants.STRING.required(),
     firstName: JoiValidationConstants.STRING.required(),
     lastName: JoiValidationConstants.STRING.required(),
     role: JoiValidationConstants.STRING.optional().allow(null),
+    updatedEmail: JoiValidationConstants.EMAIL.optional().allow(null),
     userId: JoiValidationConstants.STRING.optional().allow(null),
   }),
   VALIDATION_ERROR_MESSAGES,
