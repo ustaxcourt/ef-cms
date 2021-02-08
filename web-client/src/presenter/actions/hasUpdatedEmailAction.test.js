2ea638b340d43a0f56dd94c381bad6d2a8d4b393
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2021-02-08 08:53:20 -0700

    7574 renaming action to be more accurate

--- web-client/src/presenter/actions/hasUpdatedEmailAction.test.js
+++ web-client/src/presenter/actions/hasUpdatedEmailAction.test.js
@@ -1,8 +1,8 @@
+import { hasUpdatedEmailAction } from './hasUpdatedEmailAction';
 import { presenter } from '../presenter-mock';
 import { runAction } from 'cerebral/test';
-import { shouldCheckEmailAvailabilityAction } from './shouldCheckEmailAvailabilityAction';
 
-describe('shouldCheckEmailAvailabilityAction', () => {
+describe('hasUpdatedEmailAction', () => {
   let pathNoStub = jest.fn();
   let pathYesStub = jest.fn();
 
@@ -14,7 +14,7 @@ describe('shouldCheckEmailAvailabilityAction', () => {
   });
 
   it('returns the yes path when updatedEmail is defined on state.form', async () => {
-    runAction(shouldCheckEmailAvailabilityAction, {
+    runAction(hasUpdatedEmailAction, {
       modules: { presenter },
       state: { form: { updatedEmail: 'blah@example.com' } },
     });
@@ -23,7 +23,7 @@ describe('shouldCheckEmailAvailabilityAction', () => {
   });
 
   it('returns the no path when updatedEmail is not defined on state.form', async () => {
-    runAction(shouldCheckEmailAvailabilityAction, {
+    runAction(hasUpdatedEmailAction, {
       modules: { presenter },
       state: { form: {} },
     });
--- web-client/src/presenter/sequences/adminContactUpdateInitialUpdateCompleteSequence.js
+++ web-client/src/presenter/sequences/adminContactUpdateInitialUpdateCompleteSequence.js
@@ -1,11 +1,11 @@
+import { hasUpdatedEmailAction } from '../actions/hasUpdatedEmailAction';
 import { navigateToPractitionerDetailAction } from '../actions/navigateToPractitionerDetailAction';
 import { setShowModalFactoryAction } from '../actions/setShowModalFactoryAction';
-import { shouldCheckEmailAvailabilityAction } from '../actions/shouldCheckEmailAvailabilityAction';
 import { unsetWaitingForResponseAction } from '../actions/unsetWaitingForResponseAction';
 
 export const adminContactUpdateInitialUpdateCompleteSequence = [
   unsetWaitingForResponseAction,
-  shouldCheckEmailAvailabilityAction,
+  hasUpdatedEmailAction,
   {
     no: [navigateToPractitionerDetailAction],
     yes: [setShowModalFactoryAction('EmailVerificationModal')],
