3fcf47be4643d08f9eb5b4310a3bc3dd20c2d06f
Joseph Sutton <jsutton@flexion.us>
2021-02-10 13:51:59 -0600

    fixing lint error

--- web-client/src/router.js
+++ web-client/src/router.js
@@ -599,7 +599,6 @@ const router = {
     registerRoute(
       '/case-detail/*/edit-order/*',
       ifHasAccess((docketNumber, docketEntryIdToEdit) => {
-        console.log('i should be firing this sequence?')
         setPageTitle(`${getPageTitleDocketPrefix(docketNumber)} Edit an order`);
         const sequence = app.getSequence('gotoEditOrderSequence');
         return sequence({
