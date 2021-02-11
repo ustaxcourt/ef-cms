e21c96e2e7dc637151e89320be1bc52f09c46784
Joseph Sutton <jsutton@flexion.us>
2021-02-11 10:50:07 -0600

    7751 - added migration and unit test to remove draftOrderState from non-drafted docket entries

--- web-api/migration-terraform/main/lambdas/migrations/0017-remove-draft-order-state.js
+++ web-api/migration-terraform/main/lambdas/migrations/0017-remove-draft-order-state.js
@@ -4,7 +4,7 @@ const {
 } = require('../../../../../shared/src/business/entities/DocketEntry');
 const applicationContext = createApplicationContext({});
 
-const migrateItems = async (items, documentClient) => {
+const migrateItems = async items => {
   const itemsAfter = [];
   for (const item of items) {
     if (
@@ -13,21 +13,19 @@ const migrateItems = async (items, documentClient) => {
       item.draftOrderState &&
       !item.isDraft
     ) {
-      const docketEntryEntity = new DocketEntry(
-        { ...item, proceedingType: trialSessionRecord.proceedingType },
-        {
-          applicationContext,
-        },
-      );
+      const docketEntryEntity = new DocketEntry(item, {
+        applicationContext,
+      });
 
       itemsAfter.push({
         ...item,
         ...docketEntryEntity.validate().toRawObject(),
+        draftOrderState: undefined,
       });
 
       const docketNumber = item.pk.split('|')[1];
       applicationContext.logger.info(
-        `migrating hearing for case ${docketNumber} and trial session id ${item.trialSessionId}`,
+        `removing draftOrderState for ${docketNumber} and docket entry id ${item.docketEntryId}`,
         {
           ...item,
           ...docketEntryEntity.validate().toRawObject(),
