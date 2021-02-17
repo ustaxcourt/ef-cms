6651ae5200ff4300f95e43f9af0f0c24e8a589ba
Matt Hopson <matthopson@gmail.com>
2021-02-17 13:52:59 -0700

    7685: Fix pa11y

--- web-client/pa11y/pa11y-admissionsclerk.js
+++ web-client/pa11y/pa11y-admissionsclerk.js
@@ -4,14 +4,14 @@ module.exports = [
   'http://localhost:1234/mock-login?token=admissionsclerk&path=/case-detail/124-20/edit-petitioner-information',
   {
     actions: [
-      'wait for #contactPrimary.email to be visible',
-      'set field #contactPrimary.email to petitioner1@example.com',
-      'set field #contactPrimary.confirmEmail to petitioner1@example.com',
+      'wait for #contactPrimary-email to be visible',
+      'set field #contactPrimary-email to petitioner1@example.com',
+      'set field #contactPrimary-confirmEmail to petitioner1@example.com',
       'click element #submit-edit-petitioner-information',
       'wait for #matching-email-modal to be visible',
     ],
     notes: 'checks a11y of matching email confirm modal',
     url:
-      'http://localhost:1234/mock-login?token=admissionsclerk&path=/case-detail/124-20/edit-petitioner-information?info=matching-email-modal',
+      'http://localhost:1234/mock-login?token=admissionsclerk&path=/case-detail/124-20/edit-petitioner-information&info=matching-email-modal',
   },
 ];
