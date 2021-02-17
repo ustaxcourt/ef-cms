5b6f577a8c70648cc16bd203789d9b5274ea9d15
Matt Hopson <matthopson@gmail.com>
2021-02-17 12:14:35 -0700

    7658: Show existing email modal, and submit email for contactPrimary

--- web-client/src/presenter/sequences/submitMatchingEmailFoundModalSequence.js
+++ web-client/src/presenter/sequences/submitMatchingEmailFoundModalSequence.js
@@ -2,11 +2,13 @@ import { addExistingUserToCaseAction } from '../actions/CaseDetail/addExistingUs
 import { clearModalAction } from '../actions/clearModalAction';
 import { navigateBackAction } from '../actions/navigateBackAction';
 import { showProgressSequenceDecorator } from '../utilities/sequenceHelpers';
+import { submitUpdatePetitionerInformationSequence } from './submitUpdatePetitionerInformationSequence';
 
 export const submitMatchingEmailFoundModalSequence = [
   showProgressSequenceDecorator([
     clearModalAction,
     addExistingUserToCaseAction,
+    submitUpdatePetitionerInformationSequence,
     navigateBackAction,
   ]),
 ];
