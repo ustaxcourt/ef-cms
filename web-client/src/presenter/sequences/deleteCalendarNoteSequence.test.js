66417586a0021dc0c96dcbcfb37b9e890f8eb194
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2021-02-22 14:36:14 -0700

    7522 another sequence test

---
+++ web-client/src/presenter/sequences/deleteCalendarNoteSequence.test.js
@@ -0,0 +1,51 @@
+import { CerebralTest } from 'cerebral/test';
+import { applicationContextForClient as applicationContext } from '../../../../shared/src/business/test/createTestApplicationContext';
+import { deleteCalendarNoteSequence } from './deleteCalendarNoteSequence';
+import { presenter } from '../presenter-mock';
+
+describe('deleteCalendarNoteSequence', () => {
+  const mockDocketNumber = '999-99';
+  const mockTrialSessionId = '4e544995-92a9-45e4-af0a-149dd9c24458';
+
+  const caseOrderItemWithoutNote = {
+    calendarNote: null,
+    docketNumber: mockDocketNumber,
+  };
+
+  let test;
+
+  beforeAll(() => {
+    presenter.providers.applicationContext = applicationContext;
+
+    presenter.sequences = {
+      deleteCalendarNoteSequence,
+    };
+    test = CerebralTest(presenter);
+
+    applicationContext
+      .getUseCases()
+      .saveCalendarNoteInteractor.mockReturnValue({
+        trialSessionId: mockTrialSessionId,
+      });
+
+    test.setState('caseDetail', {
+      docketNumber: mockDocketNumber,
+      trialsSessionId: mockTrialSessionId,
+    });
+
+    test.setState('trialSessions', [
+      {
+        caseOrder: [{ ...caseOrderItemWithoutNote, calendarNote: 'delete me' }],
+        trialSessionId: mockTrialSessionId,
+      },
+    ]);
+  });
+
+  it('should update state.trialSessions after deleting the note on the specified trialSession', async () => {
+    await test.runSequence('deleteCalendarNoteSequence');
+
+    expect(test.getState('trialSessions')).toEqual([
+      { trialSessionId: mockTrialSessionId },
+    ]);
+  });
+});
