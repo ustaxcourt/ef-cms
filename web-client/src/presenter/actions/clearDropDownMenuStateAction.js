97e030a32ebe141804bc18470e50a47047257032
Matt Hopson <matthopson@gmail.com>
2021-02-25 12:19:43 -0700

    7831: Add unit test

--- web-client/src/presenter/actions/clearDropDownMenuStateAction.js
+++ web-client/src/presenter/actions/clearDropDownMenuStateAction.js
@@ -3,7 +3,8 @@
  *
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext needed for getting the getCase use case
- * @param {Function} providers.get the cerebral get function
+ * @param {object} providers.props the cerebral the cerebral props object
+ * @param {object} providers.store the cerebral store object
  * @returns {object} contains the caseDetail returned from the use case
  */
 export const clearDropDownMenuStateAction = async ({ props, store }) => {
