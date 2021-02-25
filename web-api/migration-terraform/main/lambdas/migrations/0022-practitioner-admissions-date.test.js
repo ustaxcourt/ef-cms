3f8fcad2467a1f33e94f77b9019797412045790a
Joseph Sutton <jsutton@flexion.us>
2021-02-25 11:13:14 -0600

    6335: migration unit testing [skip ci]

--- web-api/migration-terraform/main/lambdas/migrations/0022-practitioner-admissions-date.test.js
+++ web-api/migration-terraform/main/lambdas/migrations/0022-practitioner-admissions-date.test.js
@@ -1,33 +1,43 @@
-const { migrateItems } = require('./0021-practitioner-search-upper-case');
+const { migrateItems } = require('./0022-practitioner-admissions-date');
+const {
+  ROLES,
+} = require('../../../../../shared/src/business/entities/EntityConstants');
+const { MOCK_PRACTITIONER } = require('../../../../../shared/src/test/mockUsers');
 
 describe('migrateItems', () => {
   let documentClient;
   const USER_ID = '97c627d1-6e13-4d45-a8e7-421055ac6e20';
 
-  const MOCK_PRIVATE_PRACTITIONER_BAR_NUMBER_RECORD = {
-    pk: 'privatePractitioner|pt1234',
+  const MOCK_IRS_PRACTITIONER = {
+    ...MOCK_PRACTITIONER,
+    pk: `user|${USER_ID}`,
     sk: `user|${USER_ID}`,
+    admissionsDate: '2020-02-29T15:50:41.686Z',
+    role: ROLES.irsPractitioner,
   };
-  const MOCK_PRIVATE_PRACTITIONER_NAME_RECORD = {
-    pk: 'privatePractitioner|bob barker',
+  const MOCK_PRIVATE_PRACTITIONER = {
+    ...MOCK_PRACTITIONER,
+    pk: `user|${USER_ID}`,
     sk: `user|${USER_ID}`,
+    admissionsDate: '2020-04-29T15:50:41.686Z',
+    role: ROLES.privatePractitioner,
   };
-  const MOCK_IRS_PRACTITIONER_BAR_NUMBER_RECORD = {
-    pk: 'irsPractitioner|rt9876',
+  const MOCK_INACTIVE_PRACTITIONER = {
+    ...MOCK_PRACTITIONER,
+    pk: `user|${USER_ID}`,
     sk: `user|${USER_ID}`,
-  };
-  const MOCK_IRS_PRACTITIONER_NAME_RECORD = {
-    pk: 'irsPractitioner|bob ross',
-    sk: `user|${USER_ID}`,
+    admissionsDate: '2020-03-29T15:50:41.686Z',
+    admissionsStatus: 'Inactive',
+    role: ROLES.inactivePractitioner,
   };
 
   beforeEach(() => {});
 
-  it('should return and not modify records that are NOT practitioner search records', async () => {
+  it('should return and not modify records that are NOT practitioner user records', async () => {
     const items = [
       {
-        pk: 'user|6d74eadc-0181-4ff5-826c-305200e8733d',
-        sk: 'user|6d74eadc-0181-4ff5-826c-305200e8733d',
+        pk: 'case|6d74eadc-0181-4ff5-826c-305200e8733d',
+        sk: 'case|6d74eadc-0181-4ff5-826c-305200e8733d',
       },
     ];
 
@@ -36,40 +46,77 @@ describe('migrateItems', () => {
     expect(results).toEqual(
       expect.arrayContaining([
         {
-          pk: 'user|6d74eadc-0181-4ff5-826c-305200e8733d',
-          sk: 'user|6d74eadc-0181-4ff5-826c-305200e8733d',
+          pk: 'case|6d74eadc-0181-4ff5-826c-305200e8733d',
+          sk: 'case|6d74eadc-0181-4ff5-826c-305200e8733d',
         },
       ]),
     );
   });
 
-  it('should return practitioner search records with the second part of the pk converted into upper case', async () => {
+  it('should return and not modify records that are NOT privatePractitioner, irsPractitioner, or inactivePractitioner user records', async () => {
     const items = [
-      MOCK_PRIVATE_PRACTITIONER_BAR_NUMBER_RECORD,
-      MOCK_PRIVATE_PRACTITIONER_NAME_RECORD,
-      MOCK_IRS_PRACTITIONER_BAR_NUMBER_RECORD,
-      MOCK_IRS_PRACTITIONER_NAME_RECORD,
+      {
+        pk: 'user|6d74eadc-0181-4ff5-826c-305200e8733d',
+        sk: 'user|6d74eadc-0181-4ff5-826c-305200e8733d',
+        role: 'petitioner',
+      },
     ];
 
     const results = await migrateItems(items, documentClient);
 
-    expect(results).toEqual([
+    expect(results[0]).toMatchObject(
+        {
+          pk: 'user|6d74eadc-0181-4ff5-826c-305200e8733d',
+          sk: 'user|6d74eadc-0181-4ff5-826c-305200e8733d',
+          role: 'petitioner',
+        }
+    );
+  });
+
+  it.only('should return and modify irsPractitioner records', async () => {
+    const items = [
+      MOCK_IRS_PRACTITIONER
+    ];
+
+    console.log('going in', items);
+
+    const results = await migrateItems(items, documentClient);
+
+    expect(results[0]).toMatchObject(
       {
-        pk: 'privatePractitioner|PT1234',
-        sk: 'user|97c627d1-6e13-4d45-a8e7-421055ac6e20',
+       ...MOCK_IRS_PRACTITIONER,
+       admissionsDate: '2020-02-29'
       },
+    );
+  });
+
+  it('should return and modify inactivePractitioner records', async () => {
+    const items = [
+      MOCK_INACTIVE_PRACTITIONER
+    ];
+
+    const results = await migrateItems(items, documentClient);
+
+    expect(results[0]).toMatchObject(
       {
-        pk: 'privatePractitioner|BOB BARKER',
-        sk: 'user|97c627d1-6e13-4d45-a8e7-421055ac6e20',
+       ...MOCK_INACTIVE_PRACTITIONER,
+       admissionsDate: '2020-03-29'
       },
+    );
+  });
+
+  it('should return and modify privatePractitioner records', async () => {
+    const items = [
+      MOCK_PRIVATE_PRACTITIONER
+    ];
+
+    const results = await migrateItems(items, documentClient);
+
+    expect(results[0]).toMatchObject(
       {
-        pk: 'irsPractitioner|RT9876',
-        sk: 'user|97c627d1-6e13-4d45-a8e7-421055ac6e20',
+       ...MOCK_PRIVATE_PRACTITIONER,
+       admissionsDate: '2020-04-29'
       },
-      {
-        pk: 'irsPractitioner|BOB ROSS',
-        sk: 'user|97c627d1-6e13-4d45-a8e7-421055ac6e20',
-      },
-    ]);
+    );
   });
 });
