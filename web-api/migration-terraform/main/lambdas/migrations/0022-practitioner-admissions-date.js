04a38c6bc53552835b032b6f7e9f9b7563bb0f67
rosie highsmith <ahighsmith@flexion.us>
2021-02-25 08:45:47 -0800

    [skip ci] Adding migration

--- web-api/migration-terraform/main/lambdas/migrations/0022-practitioner-admissions-date.js
+++ web-api/migration-terraform/main/lambdas/migrations/0022-practitioner-admissions-date.js
@@ -1,17 +1,45 @@
-// if item is a practitioner
-// convert admissionsDate to date only, no timestamp
-
+const createApplicationContext = require('../../../../src/applicationContext');
+const {
+  formatDateString,
+  FORMATS,
+} = require('../../../../../shared/src/business/utilities/DateHandler');
+const {
+  Practitioner,
+} = require('../../../../../shared/src/business/entities/Practitioner');
+const {
+  ROLES,
+} = require('../../../../../shared/src/business/entities/EntityConstants');
+const applicationContext = createApplicationContext({});
 
 const migrateItems = async items => {
   const itemsAfter = [];
-  for (const item of items) {
+  for (let item of items) {
     if (
-      (item.pk.startsWith('privatePractitioner|') ||
-        item.pk.startsWith('irsPractitioner|')) &&
-      item.sk.startsWith('user|')
+      item.sk.startsWith('user|') &&
+      item.pk.startsWith('user|') &&
+      [
+        ROLES.privatePractitioner,
+        ROLES.irsPractitioner,
+        ROLES.inactivePractitioner,
+      ].includes(item.role)
     ) {
+      console.log('*** before', item.admissionsDate);
 
-      
+      item.admissionsDate = formatDateString(
+        item.admissionsDate,
+        FORMATS.YYYYMMDD,
+      );
+
+      const practitioner = new Practitioner(item, {
+        applicationContext,
+      })
+        .validate()
+        .toRawObject();
+
+      console.log('*** after', item.admissionsDate);
+
+      item = { ...item, ...practitioner };
+      itemsAfter.push(item);
     }
     itemsAfter.push(item);
   }
