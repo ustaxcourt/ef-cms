7dc50427f2cc874ad505584647317e69c1130478
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-02-25 15:25:44 -0800

    7807: finish setting up cognito post authentication trigger; adding tests to interactor [skip ci]

--- web-api/terraform/template/lambdas/cognito-triggers.js
+++ web-api/terraform/template/lambdas/cognito-triggers.js
@@ -1,5 +1,77 @@
-const createApplicationContext = require('./applicationContext');
-const applicationContext = createApplicationContext({});
+const AWS = require('aws-sdk');
+const {
+  createLogger,
+} = require('../../../../shared/src/utilities/createLogger');
+const {
+  createPetitionerAccountInteractor,
+} = require('../../../../shared/src/business/useCases/users/createPetitionerAccountInteractor');
+const {
+  getCaseByDocketNumber,
+} = require('../../../../shared/src/persistence/dynamo/cases/getCaseByDocketNumber');
+const {
+  getDocketNumbersByUser,
+} = require('../../../../shared/src/persistence/dynamo/cases/getDocketNumbersByUser');
+const {
+  getUserById,
+} = require('../../../../shared/src/persistence/dynamo/users/getUserById');
+const {
+  persistUser,
+} = require('../../../../shared/src/persistence/dynamo/users/persistUser');
+const {
+  setUserEmailFromPendingEmailInteractor,
+} = require('../../../../shared/src/business/useCases/users/setUserEmailFromPendingEmailInteractor');
+const {
+  updateCase,
+} = require('../../../../shared/src/persistence/dynamo/cases/updateCase');
+const {
+  updateCaseAndAssociations,
+} = require('../../../../shared/src/business/useCaseHelper/caseAssociation/updateCaseAndAssociations');
+const {
+  updateUser,
+} = require('../../../../shared/src/persistence/dynamo/users/updateUser');
+
+const { DynamoDB } = AWS;
+const logger = createLogger({
+  defaultMeta: {
+    environment: {
+      stage: process.env.STAGE || 'local',
+    },
+  },
+});
+
+const applicationContext = {
+  getCurrentUser: () => ({}),
+  getDocumentClient: () => {
+    return new DynamoDB.DocumentClient({
+      endpoint: process.env.DYNAMODB_ENDPOINT,
+      region: process.env.AWS_REGION,
+    });
+  },
+  getEnvironment: () => ({
+    dynamoDbTableName: process.env.DYNAMODB_TABLE_NAME,
+    stage: process.env.STAGE,
+  }),
+  getPersistenceGateway: () => ({
+    getCaseByDocketNumber,
+    getDocketNumbersByUser,
+    getUserById,
+    persistUser,
+    updateCase,
+    updateUser,
+  }),
+  getUseCaseHelpers: () => ({ updateCaseAndAssociations }),
+  getUseCases: () => ({
+    createPetitionerAccountInteractor,
+    setUserEmailFromPendingEmailInteractor,
+  }),
+  logger: {
+    debug: logger.debug.bind(logger),
+    error: logger.error.bind(logger),
+    info: logger.info.bind(logger),
+  },
+};
+
+exports.applicationContext = applicationContext;
 
 exports.handler = async event => {
   if (event.triggerSource === 'PostConfirmation_ConfirmSignUp') {
