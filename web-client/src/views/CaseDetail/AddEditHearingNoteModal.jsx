01aa09bfbcbb673717c82417119dc8d5abeec3f5
Cody Seibert <cseibert@flexion.us>
2021-03-01 14:07:55 -0500

    7831 - addressing pr comments

--- web-client/src/views/CaseDetail/AddEditHearingNoteModal.jsx
+++ web-client/src/views/CaseDetail/AddEditHearingNoteModal.jsx
@@ -11,7 +11,7 @@ export const AddEditHearingNoteModal = connect(
       sequences.validateTrialSessionHearingNoteSequence,
     validationErrors: state.validationErrors,
   },
-  function AddEditCalendarNoteModal({
+  function AddEditHearingNoteModal({
     validateTrialSessionHearingNoteSequence,
     validationErrors,
   }) {
