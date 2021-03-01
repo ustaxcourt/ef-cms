4209b0f3d928034d67b5a3b069a72e043a09f617
rosie highsmith <ahighsmith@flexion.us>
2021-03-01 14:39:15 -0800

    7807: added migration test

--- web-api/migration-terraform/main/lambdas/migrations/0023-set-served-docket-entries-as-completed.test.js
+++ web-api/migration-terraform/main/lambdas/migrations/0023-set-served-docket-entries-as-completed.test.js
@@ -71,11 +71,36 @@ describe('migrateItems', () => {
   });
 
   it('should set docketEntry.processingStatus to complete when servedAt is defined and processingStatus is pending', async () => {
+    const mockDocketEntry = {
+      createdAt: '2018-11-21T20:49:28.192Z',
+      docketEntryId: 'c6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
+      docketNumber: '101-18',
+      documentTitle: 'Order that case is assigned to [Judge name] [Anything]',
+      documentType: 'Order that case is assigned',
+      eventCode: 'OAJ',
+      filedBy: 'Test Petitioner',
+      filingDate: '2018-03-01T00:01:00.000Z',
+      freeText: 'Cheese fries',
+      index: 1,
+      isFileAttached: true,
+      isOnDocketRecord: true,
+      judge: 'Fieri',
+      pk: 'case|999-99',
+      processingStatus: 'complete',
+      scenario: 'Type B',
+      signedAt: '2018-03-01T00:01:00.000Z',
+      signedByUserId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+      signedJudgeName: 'Judge Guy Fieri',
+      sk: 'docket-entry|c6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
+      userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    };
+
     const items = [
       {
-        pk: 'case|6d74eadc-0181-4ff5-826c-305200e8733d',
+        ...mockDocketEntry,
         processingStatus: DOCUMENT_PROCESSING_STATUS_OPTIONS.pending,
         servedAt: '2018-03-01T00:01:00.000Z',
+        servedParties: [],
         sk: 'docket-entry|6d74eadc-0181-4ff5-826c-305200e8733d',
       },
     ];
@@ -84,9 +109,10 @@ describe('migrateItems', () => {
 
     expect(results).toEqual([
       {
-        pk: 'case|6d74eadc-0181-4ff5-826c-305200e8733d',
+        ...mockDocketEntry,
         processingStatus: DOCUMENT_PROCESSING_STATUS_OPTIONS.COMPLETE,
         servedAt: '2018-03-01T00:01:00.000Z',
+        servedParties: [],
         sk: 'docket-entry|6d74eadc-0181-4ff5-826c-305200e8733d',
       },
     ]);
