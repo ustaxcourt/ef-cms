f1078dd5dd1c986816c33630e6e9362f06a9e3f7
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2021-03-04 14:43:17 -0700

    206 [skip ci] wip refactoring

--- web-client/src/presenter/actions/setDocumentUploadModeAction.js
+++ web-client/src/presenter/actions/setDocumentUploadModeAction.js
@@ -1,8 +1,21 @@
 import { state } from 'cerebral';
-
-export const setDocumentUploadModeAction = ({ props, store }) => {
-  store.set(
-    state.currentViewMetadata.documentUploadMode,
-    props.documentUploadMode,
-  );
-};
+/**
+ * fixme
+ *  returns a callback function that sets isEditingDocketEntry on state
+ *
+ * @param {string} isEditingDocketEntry the value of isEditingDocketEntry to be set
+ * @returns {Function} returns a callback function that sets isEditingDocketEntry on state
+ */
+export const setDocumentUploadModeAction = documentUploadMode =>
+  /**
+   * sets the value of state.isEditingDocket entry to the value passed in
+   *
+   * @param {object} providers the providers object
+   * @param {object} providers.store the cerebral store object
+   */
+  async ({ props, store }) => {
+    store.set(
+      state.currentViewMetadata.documentUploadMode,
+      props.documentUploadMode || documentUploadMode,
+    );
+  };
