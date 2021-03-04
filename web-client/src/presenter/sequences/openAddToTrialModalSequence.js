6795e613987f693c10e7f0cb1dc2328aaad5738e
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2021-03-04 15:52:13 -0700

    206 more refactoring

--- web-client/src/presenter/sequences/openAddToTrialModalSequence.js
+++ web-client/src/presenter/sequences/openAddToTrialModalSequence.js
@@ -1,10 +1,9 @@
 import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { getTrialSessionsAction } from '../actions/TrialSession/getTrialSessionsAction';
-import { set } from 'cerebral/factories';
+import { setShowAllLocationsFalseAction } from '../actions/setShowAllLocationsFalseAction';
 import { setShowModalFactoryAction } from '../actions/setShowModalFactoryAction';
 import { setTrialSessionsOnModalAction } from '../actions/TrialSession/setTrialSessionsOnModalAction';
 import { showProgressSequenceDecorator } from '../utilities/sequenceHelpers';
-import { state } from 'cerebral';
 import { stopShowValidationAction } from '../actions/stopShowValidationAction';
 
 export const openAddToTrialModalSequence = showProgressSequenceDecorator([
@@ -12,6 +11,6 @@ export const openAddToTrialModalSequence = showProgressSequenceDecorator([
   clearAlertsAction,
   getTrialSessionsAction,
   setTrialSessionsOnModalAction,
-  set(state.modal.showAllLocations, false),
+  setShowAllLocationsFalseAction,
   setShowModalFactoryAction('AddToTrialModal'),
 ]);
