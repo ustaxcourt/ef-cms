7a24c06fe7076c478cb54b114828c32d82e56a57
Kris Koskelin <kkoskelin@flexion.us>
2021-03-05 15:34:20 -0600

    Initial tests for updateCaseAndAssociations

--- shared/src/business/useCaseHelper/caseAssociation/updateCaseAndAssociations.test.js
+++ shared/src/business/useCaseHelper/caseAssociation/updateCaseAndAssociations.test.js
@@ -9,12 +9,25 @@ describe('updateCaseAndAssociations', () => {
   beforeAll(() => {
     applicationContext
       .getPersistenceGateway()
+      .getCaseByDocketNumber.mockReturnValue(MOCK_CASE);
+
+    applicationContext
+      .getPersistenceGateway()
       .updateCase.mockImplementation(updateCaseMock);
   });
+
   it('always sends valid entities to the updateCase persistence method', async () => {
     await updateCaseAndAssociations({
       applicationContext,
       caseToUpdate: MOCK_CASE,
     });
+    expect(
+      applicationContext.getPersistenceGateway().getCaseByDocketNumber,
+    ).toHaveBeenCalled();
+    expect(updateCaseMock).toHaveBeenCalled();
+    const updateArgs = updateCaseMock.mock.calls[0][0];
+
+    expect(updateArgs.caseToUpdate.isValidated).toBe(true);
+    expect(updateArgs.oldCase.isValidated).toBe(true);
   });
 });
