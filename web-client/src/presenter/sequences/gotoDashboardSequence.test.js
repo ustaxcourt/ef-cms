3feca9b84014a20d432719f2de96b0ef25ac9ab7
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-03-05 11:41:28 -0800

    Devex 882: more unit testing sequences

--- web-client/src/presenter/sequences/gotoDashboardSequence.test.js
+++ web-client/src/presenter/sequences/gotoDashboardSequence.test.js
@@ -1,25 +1,46 @@
-import { setupTest } from '../../../integration-tests/helpers';
+import { CerebralTest } from 'cerebral/test';
+import { ROLES } from '../../../../shared/src/business/entities/EntityConstants';
+import { applicationContextForClient as applicationContext } from '../../../../shared/src/business/test/createTestApplicationContext';
+import { gotoDashboardSequence } from '../sequences/gotoDashboardSequence';
+import { presenter } from '../presenter-mock';
 
 describe('gotoDashboardSequence', () => {
+  const user = {
+    role: ROLES.petitioner,
+    userId: '9bd4b89c-d46e-434d-9268-5d98904b40d6',
+  };
+  const openCases = [{ docketNumber: '111-22' }];
+  const closedCases = [{ docketNumber: '333-44' }];
+
   let test;
 
   beforeAll(() => {
-    test = setupTest({
-      useCases: {
-        getUserInteractor: jest.fn().mockResolvedValue({ name: 'test user' }),
-        setItemInteractor: jest.fn(),
-      },
-    });
+    presenter.providers.applicationContext = applicationContext;
+    presenter.sequences = {
+      gotoDashboardSequence,
+    };
+    test = CerebralTest(presenter);
+
+    applicationContext.getUseCases().getUserInteractor.mockReturnValue(user);
+
+    applicationContext
+      .getUseCases()
+      .getOpenConsolidatedCasesInteractor.mockReturnValue(openCases);
+
+    applicationContext
+      .getUseCases()
+      .getClosedCasesInteractor.mockReturnValue(closedCases);
   });
 
-  it('should set the current user on state', async () => {
-    test.applicationContext.getCurrentUser = jest.fn().mockReturnValue({});
+  it('should set up state for petitioner going to dashboard', async () => {
+    applicationContext.getCurrentUser.mockReturnValue(user);
 
     await test.runSequence('gotoDashboardSequence');
 
-    expect(test.getState('user')).toEqual({ name: 'test user' });
-    expect(
-      test.applicationContext.getUseCases().setItemInteractor.mock.calls[0][0],
-    ).toMatchObject({ key: 'user', value: { name: 'test user' } });
+    expect(test.getState()).toMatchObject({
+      closedCases,
+      openCases,
+      user,
+    });
   });
 });
