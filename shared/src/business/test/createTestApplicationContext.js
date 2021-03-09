ebebd3b21de6ef9dfb2921dcff12a83e85af0d86
Kris Koskelin <kkoskelin@flexion.us>
2021-03-09 12:37:39 -0600

    devex 851: update mock and test

--- shared/src/business/test/createTestApplicationContext.js
+++ shared/src/business/test/createTestApplicationContext.js
@@ -135,6 +135,9 @@ const {
   updateCaseAutomaticBlock,
 } = require('../useCaseHelper/automaticBlock/updateCaseAutomaticBlock');
 const {
+  updateCaseCorrespondence,
+} = require('../../persistence/dynamo/correspondence/updateCaseCorrespondence');
+const {
   updateDocketEntry,
 } = require('../../persistence/dynamo/documents/updateDocketEntry');
 const {
@@ -423,6 +426,9 @@ const createTestApplicationContext = ({ user } = {}) => {
     setPriorityOnAllWorkItems: jest.fn(),
     setWorkItemAsRead: jest.fn().mockImplementation(setWorkItemAsRead),
     updateCase: jest.fn().mockImplementation(updateCase),
+    updateCaseCorrespondence: jest
+      .fn()
+      .mockImplementation(updateCaseCorrespondence),
     updateCaseHearing: jest.fn(),
     updateCaseTrialSortMappingRecords: jest.fn(),
     updateDocketEntry: jest.fn().mockImplementation(updateDocketEntry),
