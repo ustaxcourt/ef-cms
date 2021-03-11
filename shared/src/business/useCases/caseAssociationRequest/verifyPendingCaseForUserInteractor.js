cabe676c1e65860616d6d5816a702bfbde500aee
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-03-11 13:41:21 -0800

    Devex 922: separate app context param in lambdas - cases pt 3

--- shared/src/business/useCases/caseAssociationRequest/verifyPendingCaseForUserInteractor.js
+++ shared/src/business/useCases/caseAssociationRequest/verifyPendingCaseForUserInteractor.js
@@ -1,16 +1,15 @@
 /**
  *
+ * @param {object} applicationContext the application context
  * @param {object} providers the providers object
- * @param {object} providers.applicationContext the application context
  * @param {string} providers.docketNumber the docket number of the case to verify
  * @param {string} providers.userId the user id to verify
  * @returns {Promise<*>} the promise of the pending case verification
  */
-exports.verifyPendingCaseForUserInteractor = async ({
+exports.verifyPendingCaseForUserInteractor = async (
   applicationContext,
-  docketNumber,
-  userId,
-}) => {
+  { docketNumber, userId },
+) => {
   return await applicationContext
     .getPersistenceGateway()
     .verifyPendingCaseForUser({
