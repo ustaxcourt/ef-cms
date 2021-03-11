af5bf75d5ae50bb2a41ed020fd94eecd550bd111
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-03-11 08:20:22 -0800

    7772: fix for document download for unserved court-issued documents

--- shared/src/business/useCases/getDownloadPolicyUrlInteractor.js
+++ shared/src/business/useCases/getDownloadPolicyUrlInteractor.js
@@ -5,6 +5,7 @@ const {
   INITIAL_DOCUMENT_TYPES,
   ROLES,
   STIPULATED_DECISION_EVENT_CODE,
+  UNSERVABLE_EVENT_CODES,
 } = require('../entities/EntityConstants');
 const {
   isAuthorized,
@@ -61,11 +62,11 @@ const handleIrsSuperUser = ({
 };
 
 const handleCourtIssued = ({ docketEntryEntity, userAssociatedWithCase }) => {
-  // look for other event_codes that are unservable
-  // formattedEntry.isUnservable =
-  // UNSERVABLE_EVENT_CODES.includes(formattedEntry.eventCode) ||
-  // formattedEntry.isLegacyServed;
-  if (!isServed(docketEntryEntity)) {
+  const isUnservable = UNSERVABLE_EVENT_CODES.includes(
+    docketEntryEntity.eventCode,
+  );
+
+  if (!isServed(docketEntryEntity) && !isUnservable) {
     throw new UnauthorizedError('Unauthorized to view document at this time.');
   } else if (
     docketEntryEntity.eventCode === STIPULATED_DECISION_EVENT_CODE &&
