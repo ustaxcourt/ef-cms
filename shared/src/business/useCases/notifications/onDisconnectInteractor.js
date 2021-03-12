cca64af717a1b0f3f44090050d614658d1863f14
Matt Hopson <matthopson@gmail.com>
2021-03-12 12:06:10 -0700

    devex 992: refactor applicationContext param on Notifications interactors and lambdas

--- shared/src/business/useCases/notifications/onDisconnectInteractor.js
+++ shared/src/business/useCases/notifications/onDisconnectInteractor.js
@@ -1,15 +1,15 @@
 /**
  * onDisconnectInteractor
  *
+ * @param {object} applicationContext the application context
  * @param {object} providers the providers object
- * @param {object} providers.applicationContext the application context
  * @param {string} providers.connectionId the websocket connection id
  * @returns {Promise} which resolves when connections have been deleted
  */
-exports.onDisconnectInteractor = async ({
+exports.onDisconnectInteractor = async (
   applicationContext,
-  connectionId,
-}) => {
+  { connectionId },
+) => {
   await applicationContext.getPersistenceGateway().deleteUserConnection({
     applicationContext,
     connectionId,
