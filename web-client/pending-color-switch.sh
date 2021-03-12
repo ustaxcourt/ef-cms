ec8810c223cb95eda517e176e4f57800db993f61
Rachel Schneiderman <rschneiderman@flexion.us>
2021-03-12 17:14:28 -0500

    Devex 892: fixing env variable access

--- web-client/pending-color-switch.sh
+++ web-client/pending-color-switch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ( ! command -v jq > /dev/null ) && echo "jq must be installed on your machine." && exit 1
-[ -z "${ENV}" ] && echo "You must have ENV set in your environment" && exit 1
+[ -z "$1" ] && echo "ERROR: The deploying env to \$1 argument.  An example value of this includes [dev, stg, prod... ]" && exit 1
 
 
 # disabling aws pager https://github.com/aws/aws-cli/pull/4702#issue-344978525
