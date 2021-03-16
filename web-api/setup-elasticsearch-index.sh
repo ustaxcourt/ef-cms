a8b4ab97fcdbe3b741e7299721fee3a837ce39a2
Joseph Sutton <jsutton@flexion.us>
2021-03-16 09:45:16 -0500

    strip double quotes

--- web-api/setup-elasticsearch-index.sh
+++ web-api/setup-elasticsearch-index.sh
@@ -23,5 +23,11 @@ pushd ./web-api/terraform/main
   ELASTICSEARCH_ENDPOINT_BETA="$(terraform output elasticsearch_endpoint_beta)"
 popd
 
+ELASTICSEARCH_ENDPOINT_ALPHA="${ELASTICSEARCH_ENDPOINT_ALPHA%\"}"
+ELASTICSEARCH_ENDPOINT_ALPHA="${ELASTICSEARCH_ENDPOINT_ALPHA#\"}"
+
+ELASTICSEARCH_ENDPOINT_BETA="${ELASTICSEARCH_ENDPOINT_BETA%\"}"
+ELASTICSEARCH_ENDPOINT_BETA="${ELASTICSEARCH_ENDPOINT_BETA#\"}"
+
 node ./web-api/elasticsearch/elasticsearch-index-settings.js ${ELASTICSEARCH_ENDPOINT_ALPHA}
 node ./web-api/elasticsearch/elasticsearch-index-settings.js ${ELASTICSEARCH_ENDPOINT_BETA}
