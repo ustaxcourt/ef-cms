1f494c1ea9dc4644240737065e7c0369c50304f8
Kris Koskelin <kkoskelin@flexion.us>
2021-03-17 17:15:03 -0500

    Devex 851: use helper method for individual persistence calls of different kinds of objects

--- shared/src/persistence/dynamo/cases/createCase.js
+++ shared/src/persistence/dynamo/cases/createCase.js
@@ -12,7 +12,7 @@ const fieldsToOmitBeforePersisting = [
 ];
 
 /**
- * createCase
+ * createCase -- should usually be called via createCaseAndAssociations use-case helper.
  *
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
@@ -20,48 +20,14 @@ const fieldsToOmitBeforePersisting = [
  * @returns {object} the case data
  */
 exports.createCase = async ({ applicationContext, caseToCreate }) => {
-  const [results] = await Promise.all([
-    client.put({
-      Item: {
-        pk: `case|${caseToCreate.docketNumber}`,
-        sk: `case|${caseToCreate.docketNumber}`,
-        ...omit(caseToCreate, fieldsToOmitBeforePersisting),
-      },
-      applicationContext,
-    }),
-    ...caseToCreate.docketEntries.map(docketEntry =>
-      client.put({
-        Item: {
-          pk: `case|${caseToCreate.docketNumber}`,
-          sk: `docket-entry|${docketEntry.docketEntryId}`,
-          ...docketEntry,
-        },
-        applicationContext,
-      }),
-    ),
-    ...caseToCreate.irsPractitioners.map(practitioner =>
-      client.put({
-        Item: {
-          pk: `case|${caseToCreate.docketNumber}`,
-          sk: `irsPractitioner|${practitioner.userId}`,
-          ...practitioner,
-        },
-        applicationContext,
-      }),
-    ),
-    ...caseToCreate.privatePractitioners.map(practitioner =>
-      client.put({
-        Item: {
-          pk: `case|${caseToCreate.docketNumber}`,
-          sk: `privatePractitioner|${practitioner.userId}`,
-          ...practitioner,
-        },
-        applicationContext,
-      }),
-    ),
-  ]);
-
-  return results;
+  return client.put({
+    Item: {
+      pk: `case|${caseToCreate.docketNumber}`,
+      sk: `case|${caseToCreate.docketNumber}`,
+      ...omit(caseToCreate, fieldsToOmitBeforePersisting),
+    },
+    applicationContext,
+  });
 };
 
 exports.fieldsToOmitBeforePersisting = fieldsToOmitBeforePersisting;
