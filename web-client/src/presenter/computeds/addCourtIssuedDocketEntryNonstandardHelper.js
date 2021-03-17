77cb063b8c3aa0ea13b56aa8ea90496a65324be4
rosie highsmith <ahighsmith@flexion.us>
2021-03-17 09:49:39 -0700

    7863: added description for type f

--- web-client/src/presenter/computeds/addCourtIssuedDocketEntryNonstandardHelper.js
+++ web-client/src/presenter/computeds/addCourtIssuedDocketEntryNonstandardHelper.js
@@ -19,6 +19,7 @@ export const addCourtIssuedDocketEntryNonstandardHelper = (
   let showDateLast = false;
   let showDocketNumbers = false;
   let showFreeText = false;
+  let showOptionalFreeText = false;
   let showJudge = false;
   let showTrialLocation = false;
 
@@ -42,6 +43,7 @@ export const addCourtIssuedDocketEntryNonstandardHelper = (
         showDateFirst = true;
         break;
       case 'Type F':
+        showOptionalFreeText = true;
         showJudge = true;
         showTrialLocation = true;
         break;
@@ -78,6 +80,7 @@ export const addCourtIssuedDocketEntryNonstandardHelper = (
     showDocketNumbers,
     showFreeText,
     showJudge,
+    showOptionalFreeText,
     showTrialLocation,
   };
 };
