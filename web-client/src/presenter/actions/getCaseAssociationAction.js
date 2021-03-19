4fc541e96031276372cc56620f4f8489e35ed910
Matt Hopson <matthopson@gmail.com>
2021-03-19 13:15:08 -0600

    7839 - More integration tests

--- web-client/src/presenter/actions/getCaseAssociationAction.js
+++ web-client/src/presenter/actions/getCaseAssociationAction.js
@@ -35,15 +35,18 @@ export const getCaseAssociationAction = async ({ applicationContext, get }) => {
 
     isAssociated = some(caseDetailRespondents, { userId: user.userId });
   } else if (user.role === USER_ROLES.petitioner) {
-    const caseContactPrimaryId = applicationContext
+    const contactPrimary = applicationContext
       .getUtilities()
-      .getContactPrimary(get(state.caseDetail)).contactId;
+      .getContactPrimary(get(state.caseDetail));
+
+    const caseContactPrimaryId = contactPrimary && contactPrimary.contactId;
+
     const caseContactSecondaryId = get(
       state.caseDetail.contactSecondary.contactId,
     );
 
     isAssociated =
-      caseContactPrimaryId === user.userId ||
+      (caseContactPrimaryId && caseContactPrimaryId === user.userId) ||
       (!!caseContactSecondaryId && caseContactSecondaryId === user.userId);
   } else if (user.role === USER_ROLES.irsSuperuser) {
     const docketEntries = get(state.caseDetail.docketEntries);
