d657711f4be2d05d349759ef47f81b137ac99100
Cody Seibert <cseibert@flexion.us>
2021-03-19 12:48:01 -0400

    7839 - fixing integration tests

--- shared/src/business/useCases/getCaseInteractor.js
+++ shared/src/business/useCases/getCaseInteractor.js
@@ -106,6 +106,7 @@ exports.getCaseInteractor = async (applicationContext, { docketNumber }) => {
   }
 
   const currentUser = applicationContext.getCurrentUser();
+  console.log('------caseRecord', caseRecord);
   let isAuthorizedToGetCase = isAuthorized(
     currentUser,
     ROLE_PERMISSIONS.GET_CASE,
