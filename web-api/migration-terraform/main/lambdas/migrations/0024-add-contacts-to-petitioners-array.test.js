40ec011754dc62f19efb685d634e7af6085c642e
Rachel Schneiderman <rschneiderman@flexion.us>
2021-03-23 12:12:53 -0400

    7839: Updating migration script to also add otherFilers to petitioners array [skip ci]

--- web-api/migration-terraform/main/lambdas/migrations/0024-add-contacts-to-petitioners-array.test.js
+++ web-api/migration-terraform/main/lambdas/migrations/0024-add-contacts-to-petitioners-array.test.js
@@ -1,12 +1,11 @@
 const {
   CONTACT_TYPES,
+  COUNTRY_TYPES,
 } = require('../../../../../shared/src/business/entities/EntityConstants');
 const {
   getContactPrimary,
 } = require('../../../../../shared/src/business/entities/cases/Case');
-const {
-  migrateItems,
-} = require('./0024-add-contact-primary-to-petitioners-array');
+const { migrateItems } = require('./0024-add-contacts-to-petitioners-array');
 const { MOCK_CASE } = require('../../../../../shared/src/test/mockCase');
 
 describe('migrateItems', () => {
@@ -70,5 +69,36 @@ describe('migrateItems', () => {
         }),
       ]),
     );
+  });
+
+  it('should properly populate the petitioners array on a case when otherFilers is undefined', async () => {
+    const items = [
+      {
+        pk: 'case|6d74eadc-0181-4ff5-826c-305200e8733d',
+        ...MOCK_CASE,
+        otherFilers: undefined,
+        petitioners: [
+          getContactPrimary(MOCK_CASE),
+          {
+            address1: '123 Main St',
+            city: 'Somewhere',
+            contactId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+            contactType: CONTACT_TYPES.otherFiler,
+            countryType: COUNTRY_TYPES.DOMESTIC,
+            email: 'petitioner@example.com',
+            name: 'Test Petitioner',
+            phone: '1234567',
+            postalCode: '12345',
+            state: 'TN',
+            title: 'Executor',
+          },
+        ],
+        sk: 'case|6d74eadc-0181-4ff5-826c-305200e8733d',
+      },
+    ];
+
+    const results = await migrateItems(items);
+
+    expect(results[0].petitioners.length).toBe(1);
   });
 });
