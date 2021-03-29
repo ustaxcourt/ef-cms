74dab18f6e914b91d5f9e22975c92d5d4901dafb
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2021-03-29 12:22:56 -0600

    7839 adding and updating tests

--- web-client/src/presenter/actions/setCaseOnFormAction.test.js
+++ web-client/src/presenter/actions/setCaseOnFormAction.test.js
@@ -1,22 +1,9 @@
-import { CONTACT_TYPES } from '../../../../shared/src/business/entities/EntityConstants';
-import { applicationContextForClient as applicationContext } from '../../../../shared/src/business/test/createTestApplicationContext';
-import { presenter } from '../presenter-mock';
 import { runAction } from 'cerebral/test';
 import { setCaseOnFormAction } from './setCaseOnFormAction';
 
 describe('setCaseOnFormAction', () => {
-  const mockContactPrimary = {
-    contactType: CONTACT_TYPES.primary,
-    name: 'Pattie the Primary',
-  };
-
-  beforeAll(() => {
-    presenter.providers.applicationContext = applicationContext;
-  });
-
   it('sets caseDetail on state.form from props', async () => {
     const { state } = await runAction(setCaseOnFormAction, {
-      modules: { presenter },
       props: {
         caseDetail: {
           docketNumber: '123-45',
@@ -27,52 +14,5 @@ describe('setCaseOnFormAction', () => {
     expect(state.form).toEqual({
       docketNumber: '123-45',
     });
-  });
-
-  it('should set state.form.contactPrimary from the props.caseDetail.petitioners array', async () => {
-    const { state } = await runAction(setCaseOnFormAction, {
-      modules: { presenter },
-      props: {
-        caseDetail: {
-          docketNumber: '123-45',
-          petitioners: [mockContactPrimary],
-        },
-      },
-    });
-
-    expect(state.form.contactPrimary).toEqual(mockContactPrimary);
-  });
-
-  it('should set state.form.contactSecondary from the props.caseDetail.petitioners array', async () => {
-    const mockContactSecondary = {
-      contactType: CONTACT_TYPES.secondary,
-      name: 'Sally the Secondary',
-    };
-
-    const { state } = await runAction(setCaseOnFormAction, {
-      modules: { presenter },
-      props: {
-        caseDetail: {
-          docketNumber: '123-45',
-          petitioners: [mockContactPrimary, mockContactSecondary],
-        },
-      },
-    });
-
-    expect(state.form.contactSecondary).toEqual(mockContactSecondary);
-  });
-
-  it('should set state.form.contactSecondary to undefined if the props.caseDetail.petitioners array does not have a secondary contact', async () => {
-    const { state } = await runAction(setCaseOnFormAction, {
-      modules: { presenter },
-      props: {
-        caseDetail: {
-          docketNumber: '123-45',
-          petitioners: [mockContactPrimary],
-        },
-      },
-    });
-
-    expect(state.form.contactSecondary).toBeUndefined();
   });
 });
