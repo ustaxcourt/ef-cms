f0c890f1a6a32af1e25e10bdac200fb9a3cb5bb5
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-03-29 11:12:32 -0700

    7839: fix contactSecondary on petition QC screen

--- web-client/src/presenter/actions/setCaseOnFormAction.test.js
+++ web-client/src/presenter/actions/setCaseOnFormAction.test.js
@@ -5,6 +5,11 @@ import { runAction } from 'cerebral/test';
 import { setCaseOnFormAction } from './setCaseOnFormAction';
 
 describe('setCaseOnFormAction', () => {
+  const mockContactPrimary = {
+    contactType: CONTACT_TYPES.primary,
+    name: 'Pattie the Primary',
+  };
+
   beforeAll(() => {
     presenter.providers.applicationContext = applicationContext;
   });
@@ -25,11 +30,6 @@ describe('setCaseOnFormAction', () => {
   });
 
   it('should set state.form.contactPrimary from the props.caseDetail.petitioners array', async () => {
-    const mockContactPrimary = {
-      contactType: CONTACT_TYPES.primary,
-      name: 'Pattie the Primary',
-    };
-
     const { state } = await runAction(setCaseOnFormAction, {
       modules: { presenter },
       props: {
@@ -41,5 +41,38 @@ describe('setCaseOnFormAction', () => {
     });
 
     expect(state.form.contactPrimary).toEqual(mockContactPrimary);
+  });
+
+  it('should set state.form.contactSecondary from the props.caseDetail.petitioners array', async () => {
+    const mockContactSecondary = {
+      contactType: CONTACT_TYPES.secondary,
+      name: 'Sally the Secondary',
+    };
+
+    const { state } = await runAction(setCaseOnFormAction, {
+      modules: { presenter },
+      props: {
+        caseDetail: {
+          docketNumber: '123-45',
+          petitioners: [mockContactPrimary, mockContactSecondary],
+        },
+      },
+    });
+
+    expect(state.form.contactSecondary).toEqual(mockContactSecondary);
+  });
+
+  it('should set state.form.contactSecondary to undefined if the props.caseDetail.petitioners array does not have a secondary contact', async () => {
+    const { state } = await runAction(setCaseOnFormAction, {
+      modules: { presenter },
+      props: {
+        caseDetail: {
+          docketNumber: '123-45',
+          petitioners: [mockContactPrimary],
+        },
+      },
+    });
+
+    expect(state.form.contactSecondary).toBeUndefined();
   });
 });
