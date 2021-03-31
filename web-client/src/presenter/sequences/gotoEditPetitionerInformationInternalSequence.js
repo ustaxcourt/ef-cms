82027e0d744ac0733724bd1195fd5dc97e7a54d0
rosie highsmith <ahighsmith@flexion.us>
2021-03-31 11:52:17 -0700

    7839: adding missed files

---
+++ web-client/src/presenter/sequences/gotoEditPetitionerInformationInternalSequence.js
@@ -0,0 +1,18 @@
+import { getCaseAction } from '../actions/getCaseAction';
+import { getUserPendingEmailAction } from '../actions/getUserPendingEmailAction';
+import { setCaseAction } from '../actions/setCaseAction';
+import { setCurrentPageAction } from '../actions/setCurrentPageAction';
+import { setUserPendingEmailAction } from '../actions/setUserPendingEmailAction';
+import { setupPetitionerInformationFormAction } from '../actions/setupPetitionerInformationFormAction';
+import { stopShowValidationAction } from '../actions/stopShowValidationAction';
+
+export const gotoEditPetitionerInformationInternalSequence = [
+  setCurrentPageAction('Interstitial'),
+  stopShowValidationAction,
+  getCaseAction,
+  setCaseAction,
+  setupPetitionerInformationFormAction,
+  getUserPendingEmailAction,
+  setUserPendingEmailAction,
+  setCurrentPageAction('EditPetitionerInformationInternal'),
+];
