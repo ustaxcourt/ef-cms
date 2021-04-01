c06545179f4b5577a3cae84254d50ab38031cedd
Rachel Schneiderman <rschneiderman@flexion.us>
2021-04-01 14:34:00 -0400

    7839: Addressing PR comments

--- shared/src/business/useCases/validatePetitionerInteractor.js
+++ shared/src/business/useCases/validatePetitionerInteractor.js
@@ -4,7 +4,10 @@ const { ContactFactory } = require('../entities/contacts/ContactFactory');
  * validatePetitionerInteractor
  *
  * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the applicationContext
  * @param {object} providers.contactInfo the contact data
+ * @param {object} providers.partyType the partyType of the case
+ * @param {object} providers.petitioners the petitioners on the case
  * @returns {object} errors (null if no errors)
  */
 exports.validatePetitionerInteractor = ({
