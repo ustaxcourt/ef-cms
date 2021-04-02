936780e4ce26b3715a3f1925d2f8dfb28bca1a8a
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-04-02 14:01:16 -0700

    7839: more tests for addressDisplayHelper

--- web-client/src/presenter/computeds/addressDisplayHelper.js
+++ web-client/src/presenter/computeds/addressDisplayHelper.js
@@ -27,7 +27,9 @@ export const addressDisplayHelper = (get, applicationContext) => {
 
   let showEditContacts = false;
 
-  if (user.role === USER_ROLES.privatePractitioner) {
+  if (user.role === USER_ROLES.petitioner) {
+    showEditContacts = true;
+  } else if (user.role === USER_ROLES.privatePractitioner) {
     showEditContacts = userAssociatedWithCase;
     showEditPrimaryContact = userAssociatedWithCase;
   } else if (
