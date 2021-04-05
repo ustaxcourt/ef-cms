c087057837f7c1704af6fb2bbeefcf72424b19f1
rosie highsmith <ahighsmith@flexion.us>
2021-04-05 10:32:10 -0700

    905: WIP writing last test [skip ci]

--- web-api/migration-terraform/main/lambdas/migrations/0026-update-to-stricter-date-format.test.js
+++ web-api/migration-terraform/main/lambdas/migrations/0026-update-to-stricter-date-format.test.js
@@ -5,8 +5,8 @@ describe('migrateItems', () => {
     it('should update createdAt and deadlineDate to be ISO formatted dates', async () => {
       const mockCaseDeadline = {
         associatedJudge: 'Judge Buch',
-        createdAt: '2019-04-01',
-        deadlineDate: '2019-03-01',
+        createdAt: '2019-07-01',
+        deadlineDate: '2019-08-01',
         description: 'One small step',
         docketNumber: '170-34',
         pk: 'case-deadline|000-00',
@@ -20,8 +20,47 @@ describe('migrateItems', () => {
       expect(results).toEqual([
         {
           ...mockCaseDeadline,
-          createdAt: '2019-04-01T04:00:00.000Z',
-          deadlineDate: '2019-03-01T05:00:00.000Z',
+          createdAt: '2019-07-01T04:00:00.000Z',
+          deadlineDate: '2019-08-01T04:00:00.000Z',
+        },
+      ]);
+    });
+
+    it('should throw an error when the item does not have a deadline date', async () => {
+      const mockCaseDeadline = {
+        associatedJudge: 'Judge Buch',
+        createdAt: '2019-07-01',
+        description: 'One small step',
+        docketNumber: '170-34',
+        pk: 'case-deadline|000-00',
+        sk: 'case-deadline|6d74eadc-0181-4ff5-826c-305200e8733d',
+      };
+
+      const items = [mockCaseDeadline];
+
+      await expect(migrateItems(items)).rejects.toThrow();
+    });
+
+    it('should not createISODateAtStartOfDayEST if the date is valid', async () => {
+      const mockCaseDeadline = {
+        associatedJudge: 'Judge Buch',
+        createdAt: '2019-07-01',
+        deadlineDate: '2019-08-01',
+        description: 'One small step',
+        docketNumber: '170-34',
+        pk: 'case-deadline|000-00',
+        sk: 'case-deadline|6d74eadc-0181-4ff5-826c-305200e8733d',
+      };
+
+      const items = [mockCaseDeadline];
+
+      const results = await migrateItems(items);
+
+      expect(results).toEqual([
+        {
+          ...mockCaseDeadline,
+          createdAt: '2019-07-01T04:00:00.000Z',
+          deadlineDate: '2019-08-01T04:00:00.000Z',
         },
       ]);
     });
