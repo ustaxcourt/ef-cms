3302fdef09d86a8bef3f9c260ba77697c3531176
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-04-06 08:03:17 -0700

    Devex 905: working on migration

--- web-api/migration-terraform/main/lambdas/migration-segments.js
+++ web-api/migration-terraform/main/lambdas/migration-segments.js
@@ -1,9 +1,6 @@
 const AWS = require('aws-sdk');
 const createApplicationContext = require('../../../src/applicationContext');
 const {
-  migrateItems: migration0025,
-} = require('./migrations/0025-docket-entry-received-at-strict-timestamp');
-const {
   migrateItems: migration0026,
 } = require('./migrations/0026-update-to-stricter-date-format');
 const {
@@ -30,8 +27,6 @@ const sqs = new AWS.SQS({ region: 'us-east-1' });
 
 // eslint-disable-next-line no-unused-vars
 const migrateRecords = async ({ documentClient, items }) => {
-  applicationContext.logger.info('about to run migration 0025');
-  items = await migration0025(items, documentClient);
   applicationContext.logger.info('about to run migration 0026');
   items = await migration0026(items, documentClient);
 
