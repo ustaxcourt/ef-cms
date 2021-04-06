61d5506b84384b97c80eedbcafb1d85f0b8acdc0
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2021-04-06 15:46:35 -0600

    905 new action for admissions date

--- web-client/src/presenter/actions/getComputedAdmissionsDateAction.js
+++ web-client/src/presenter/actions/getComputedAdmissionsDateAction.js
@@ -1,5 +1,28 @@
 import { state } from 'cerebral';
 
+const computeAdmissionsDate = ({ applicationContext, day, month, year }) => {
+  const { DATE_FORMATS } = applicationContext.getConstants();
+
+  const inputProvided = day || month || year;
+  if (!inputProvided) {
+    return null;
+  }
+  const yyyyPadded = `${year}`.padStart(4, '0');
+  const mmPadded = `${month}`.padStart(2, '0');
+  const ddPadded = `${day}`.padStart(2, '0');
+  const dateToParse = `${yyyyPadded}-${mmPadded}-${ddPadded}`;
+
+  const preparedDateISO = applicationContext
+    .getUtilities()
+    .prepareDateFromString(dateToParse, DATE_FORMATS.YYYYMMDD);
+
+  const yyyymmdd = applicationContext
+    .getUtilities()
+    .formatDateString(preparedDateISO, DATE_FORMATS.YYYYMMDD);
+
+  return yyyymmdd;
+};
+
 /**
  * computes admissions date from form date values
  *
@@ -11,30 +34,17 @@ export const getComputedAdmissionsDateAction = ({
   get,
 }) => {
   let formYear = get(state.form.year);
-  let formMonth = get(state.form.moth);
+  let formMonth = get(state.form.month);
   let formDay = get(state.form.day);
 
   let computedDate = null;
 
-  computedDate = applicationContext.getUtilities().computeDate({
+  computedDate = computeAdmissionsDate({
+    applicationContext,
     day: formDay,
     month: formMonth,
     year: formYear,
   });
-
-  if (
-    applicationContext
-      .getUtilities()
-      .isValidDateString(`${formMonth}-${formDay}-${formYear}`)
-  ) {
-    computedDate = applicationContext
-      .getUtilities()
-      .createISODateStringFromObject({
-        day: formDay,
-        month: formMonth,
-        year: formYear,
-      });
-  }
 
   return { computedDate };
 };
