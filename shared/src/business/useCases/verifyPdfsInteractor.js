d865019beab5e75df2708a8c824190e2e431d80d
Rachel Schneiderman <rschneiderman@flexion.us>
2021-04-06 14:19:54 -0400

    8089: Show upload error modal when PDF cant be verified during paper case creation

--- shared/src/business/useCases/verifyPdfsInteractor.js
+++
@@ -1,8 +0,0 @@
-/**
- * verifyPdfsInteractor
- *
- * @param {object} providers the providers object
- * @param {object} providers.applicationContext the application context
- * @returns {object} errors (null if no errors)
- */
-exports.verifyPdfsInteractor = ({ applicationContext, pdfDocs }) => {};
