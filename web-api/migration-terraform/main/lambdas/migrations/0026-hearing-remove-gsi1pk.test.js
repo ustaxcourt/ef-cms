26bfa09b3249175a5228842babe325677a534dbf
Matt Hopson <matthopson@gmail.com>
2021-04-06 09:02:15 -0600

    8019 - Rename var in test

--- web-api/migration-terraform/main/lambdas/migrations/0026-hearing-remove-gsi1pk.test.js
+++ web-api/migration-terraform/main/lambdas/migrations/0026-hearing-remove-gsi1pk.test.js
@@ -1,9 +1,9 @@
 const { migrateItems } = require('./0026-hearing-remove-gsi1pk.js');
 
 describe('migrateItems', () => {
-  let mockDocketEntry;
+  let mockHearing;
   beforeEach(() => {
-    mockDocketEntry = {
+    mockHearing = {
       chambersPhoneNumber: '1111111',
       createdAt: '2018-11-21T20:49:28.192Z',
       joinPhoneNumber: '0987654321',
@@ -28,7 +28,7 @@ describe('migrateItems', () => {
   it('should return and not modify records that are NOT case hearing mapping records', async () => {
     const items = [
       {
-        ...mockDocketEntry,
+        ...mockHearing,
         gsi1pk: 'trial-session-catalog',
         pk: 'case|123-45',
         sk: 'trial-session|6d74eadc-0181-4ff5-826c-305200e8733d',
@@ -39,7 +39,7 @@ describe('migrateItems', () => {
 
     expect(results).toEqual([
       {
-        ...mockDocketEntry,
+        ...mockHearing,
         gsi1pk: 'trial-session-catalog',
         pk: 'case|123-45',
         sk: 'trial-session|6d74eadc-0181-4ff5-826c-305200e8733d',
@@ -50,7 +50,7 @@ describe('migrateItems', () => {
   it('should return and not modify case hearing mapping records that do not contain a gsi1pk', async () => {
     const items = [
       {
-        ...mockDocketEntry,
+        ...mockHearing,
       },
     ];
 
@@ -62,7 +62,7 @@ describe('migrateItems', () => {
   it('should return and not modify case hearing mapping records that contain a gsi1pk other than `trial-session-catalog`', async () => {
     const items = [
       {
-        ...mockDocketEntry,
+        ...mockHearing,
         gsi1pk: 'something-else',
       },
     ];
@@ -75,7 +75,7 @@ describe('migrateItems', () => {
   it('should delete the gsi1pk property on case hearing records that have a gsi1pk of `trial-session-catalog`', async () => {
     const items = [
       {
-        ...mockDocketEntry,
+        ...mockHearing,
         gsi1pk: 'trial-session-catalog',
       },
     ];
@@ -84,7 +84,7 @@ describe('migrateItems', () => {
 
     expect(results).toEqual([
       {
-        ...mockDocketEntry,
+        ...mockHearing,
       },
     ]);
   });
