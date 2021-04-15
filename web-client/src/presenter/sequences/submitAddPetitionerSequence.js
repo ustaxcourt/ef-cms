ca2f2c6dbf9786493a483947392328d858bdac5e
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2021-04-15 13:31:24 -0600

    5386 wip adding action to set contact type on form

--- web-client/src/presenter/sequences/submitAddPetitionerSequence.js
+++ web-client/src/presenter/sequences/submitAddPetitionerSequence.js
@@ -1,26 +1,24 @@
 import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { hasUpdatedPetitionerEmailAction } from '../actions/hasUpdatedPetitionerEmailAction';
 import { openGainElectronicAccessToCaseModalSequence } from './openGainElectronicAccessToCaseModalSequence';
-import { setContactTypeOnNewPetitionerAction } from '../actions/setContactTypeOnNewPetitionerAction';
 import { setValidationAlertErrorsAction } from '../actions/setValidationAlertErrorsAction';
 import { showProgressSequenceDecorator } from '../utilities/sequenceHelpers';
 import { startShowValidationAction } from '../actions/startShowValidationAction';
 import { submitUpdatePetitionerInformationSequence } from './submitUpdatePetitionerInformationSequence';
 import { validatePetitionerAction } from '../actions/validatePetitionerAction';
 
-export const submitEditPetitionerSequence = [
+export const submitAddPetitionerSequence = [
   clearAlertsAction,
   startShowValidationAction,
-  setContactTypeOnNewPetitionerAction,
   validatePetitionerAction,
   {
     error: [setValidationAlertErrorsAction],
     success: showProgressSequenceDecorator([
-      // hasUpdatedPetitionerEmailAction,
-      // {
-      //   no: [submitUpdatePetitionerInformationSequence],
-      //   yes: [openGainElectronicAccessToCaseModalSequence],
-      // },
+      hasUpdatedPetitionerEmailAction,
+      {
+        no: [submitUpdatePetitionerInformationSequence],
+        yes: [openGainElectronicAccessToCaseModalSequence],
+      },
     ]),
   },
 ];
