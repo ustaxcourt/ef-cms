c6cf3e77696a4325f88a9895567e8ae50f208478
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-04-15 12:51:26 -0700

    5386: working on vlaidation [skip ci]

--- web-client/src/presenter/actions/setDefaultAddPetitionerToCaseFormAction.js
+++ web-client/src/presenter/actions/setDefaultAddPetitionerToCaseFormAction.js
@@ -1,3 +1,4 @@
+import { getContactSecondary } from '../../../../shared/src/business/entities/cases/Case';
 import { state } from 'cerebral';
 
 /**
@@ -6,6 +7,21 @@ import { state } from 'cerebral';
  * @param {object} providers the providers object
  * @param {object} providers.store the cerebral store
  */
-export const setDefaultAddPetitionerToCaseFormAction = ({ store }) => {
+export const setDefaultAddPetitionerToCaseFormAction = ({
+  applicationContext,
+  get,
+  store,
+}) => {
+  const { CONTACT_TYPES } = applicationContext.getConstants();
+
   store.set(state.form, { contact: {} });
+
+  const caseDetail = get(state.caseDetail);
+  const contactSecondary = getContactSecondary(caseDetail);
+
+  if (!contactSecondary) {
+    store.set(state.form.contactType, CONTACT_TYPES.secondary);
+  } else {
+    store.set(state.form.contactType, CONTACT_TYPES.otherPetitioner);
+  }
 };
--- web-client/src/presenter/sequences/submitEditPetitionerSequence.js
+++ web-client/src/presenter/sequences/submitEditPetitionerSequence.js
@@ -1,7 +1,6 @@
 import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { hasUpdatedPetitionerEmailAction } from '../actions/hasUpdatedPetitionerEmailAction';
 import { openGainElectronicAccessToCaseModalSequence } from './openGainElectronicAccessToCaseModalSequence';
-import { setContactTypeOnNewPetitionerAction } from '../actions/setContactTypeOnNewPetitionerAction';
 import { setValidationAlertErrorsAction } from '../actions/setValidationAlertErrorsAction';
 import { showProgressSequenceDecorator } from '../utilities/sequenceHelpers';
 import { startShowValidationAction } from '../actions/startShowValidationAction';
@@ -11,16 +10,15 @@ import { validatePetitionerAction } from '../actions/validatePetitionerAction';
 export const submitEditPetitionerSequence = [
   clearAlertsAction,
   startShowValidationAction,
-  setContactTypeOnNewPetitionerAction,
   validatePetitionerAction,
   {
     error: [setValidationAlertErrorsAction],
     success: showProgressSequenceDecorator([
-      // hasUpdatedPetitionerEmailAction,
-      // {
-      //   no: [submitUpdatePetitionerInformationSequence],
-      //   yes: [openGainElectronicAccessToCaseModalSequence],
-      // },
+      hasUpdatedPetitionerEmailAction,
+      {
+        no: [submitUpdatePetitionerInformationSequence],
+        yes: [openGainElectronicAccessToCaseModalSequence],
+      },
     ]),
   },
 ];
