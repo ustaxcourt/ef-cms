ca67e53c9647a26611a98fb7d23b2111a15c7b17
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-04-16 11:06:52 -0700

    5386: lint fix

--- web-client/src/presenter/sequences/setSelectedAddressOnFormSequence.js
+++ web-client/src/presenter/sequences/setSelectedAddressOnFormSequence.js
@@ -1,7 +1,5 @@
 import { setSelectedPetitionerAddressAction } from '../actions/setSelectedPetitionerAddressAction';
-import { setShowModalFactoryAction } from '../actions/setShowModalFactoryAction';
 
 export const setSelectedAddressOnFormSequence = [
   setSelectedPetitionerAddressAction,
-  // setShowModalFactoryAction('FormCancelModalDialog'),
 ];
