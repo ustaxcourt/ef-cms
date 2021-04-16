990ce37590075ccbc69076dd0154dc490c736e56
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-04-16 09:04:30 -0700

    5386: action to get and set petitioner addresses

--- web-client/src/presenter/actions/CaseDetail/getAndSetPetitionersAddressAction.js
+++ web-client/src/presenter/actions/CaseDetail/getAndSetPetitionersAddressAction.js
@@ -1,22 +1,20 @@
 import { state } from 'cerebral';
 
 /**
- * gets state.getPetitionersAddressAction
+ * gets and sets state.screenMetadata.petitionerAddresses
  *
  * @param {object} providers the providers object
  * @param {object} providers.get the cerebral get function
  * @param {Function} providers.store the cerebral store function
  */
 export const getAndSetPetitionersAddressAction = ({ get, store }) => {
-  // GO BACK AND DO A TEST
   const caseDetail = get(state.caseDetail);
 
-  const petitioners = {};
+  const petitionerAddresses = {};
 
-  caseDetail.petitioners?.map(petitioner => {
-    petitioner.address1;
+  caseDetail.petitioners.forEach(petitioner => {
+    petitionerAddresses[petitioner.contactId] = petitioner.address1;
   });
 
-  // result object will look like: contactId: address1
-  // { '2344-2asdvfxfd': 1234 main st' }
+  store.set(state.screenMetadata.petitionerAddresses, petitionerAddresses);
 };
