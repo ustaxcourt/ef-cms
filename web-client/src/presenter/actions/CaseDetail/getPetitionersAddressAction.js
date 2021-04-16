ab95183e74f7d57d557474c887ecd8df2fc339bc
rosie highsmith <ahighsmith@flexion.us>
2021-04-16 08:36:53 -0700

    5386: starting action to get all petitioners' address1es [skip ci]

---
+++ web-client/src/presenter/actions/CaseDetail/getPetitionersAddressAction.js
@@ -0,0 +1,22 @@
+import { state } from 'cerebral';
+
+/**
+ * gets state.getPetitionersAddressAction
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.get the cerebral get function
+ * @param {Function} providers.store the cerebral store function
+ */
+export const getPetitionersAddressAction = ({ get, store }) => {
+  // GO BACK AND DO A TEST
+  const caseDetail = get(state.caseDetail);
+
+  const petitioners = {};
+
+  caseDetail.petitioners?.map(petitioner => {
+    petitioner.address1;
+  });
+
+  // result object will look like: contactId: address1
+  // { '2344-2asdvfxfd': 1234 main st' }
+};
