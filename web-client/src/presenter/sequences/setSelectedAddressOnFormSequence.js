5a2d7b080584f0a79ab12200457dd79cc0782c5c
rosie highsmith <ahighsmith@flexion.us>
2021-04-16 09:50:58 -0700

    5386: working on setSelectedPetitionerAddressAction [skip ci]

--- web-client/src/presenter/sequences/setSelectedAddressOnFormSequence.js
+++ web-client/src/presenter/sequences/setSelectedAddressOnFormSequence.js
@@ -1,5 +1,7 @@
+import { setSelectedPetitionerAddressAction } from '../actions/setSelectedPetitionerAddressAction';
 import { setShowModalFactoryAction } from '../actions/setShowModalFactoryAction';
 
 export const setSelectedAddressOnFormSequence = [
+  setSelectedPetitionerAddressAction,
   setShowModalFactoryAction('FormCancelModalDialog'),
 ];
