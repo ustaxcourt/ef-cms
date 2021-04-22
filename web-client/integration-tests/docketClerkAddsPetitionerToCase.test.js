7ec749437715ab4df542c195fac7a045f7395946
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-04-22 08:55:43 -0700

    5386: integration tests wip [skip ci]

---
+++ web-client/integration-tests/docketClerkAddsPetitionerToCase.test.js
@@ -0,0 +1,33 @@
+import { docketClerkAddsPetitionerToCase } from './journey/docketClerkAddsPetitionerToCase';
+import { loginAs, setupTest, uploadPetition } from './helpers';
+import { petitionsClerkServesElectronicCaseToIrs } from './journey/petitionsClerkServesElectronicCaseToIrs';
+
+const test = setupTest();
+
+describe('docket clerk adds petitioner to case', () => {
+  beforeAll(() => {
+    jest.setTimeout(30000);
+  });
+
+  afterAll(() => {
+    test.closeSocket();
+  });
+
+  loginAs(test, 'petitioner@example.com');
+  it('Create test case', async () => {
+    const caseDetail = await uploadPetition(test);
+    expect(caseDetail.docketNumber).toBeDefined();
+    test.docketNumber = caseDetail.docketNumber;
+  });
+
+  loginAs(test, 'petitionsclerk@example.com');
+  petitionsClerkServesElectronicCaseToIrs(test);
+
+  loginAs(test, 'docketclerk@example.com');
+  docketClerkAddsPetitionerToCase(test);
+
+  // go to the add petitioner form
+  // fill in the form fields
+  // submit the form
+  // verify that the petitioner was added to the case
+});
