4ff5c2537cf86f5b8fc65aa78ef8eab3d7b18a3e
Cody Seibert <cseibert@flexion.us>
2021-04-23 16:52:22 -0400

    5386 - adding additional unit tests over the case

--- shared/src/business/entities/cases/Case.test.js
+++ shared/src/business/entities/cases/Case.test.js
@@ -5290,4 +5290,39 @@ describe('Case entity', () => {
       );
     });
   });
+
+  describe('getPractitionersRepresenting', () => {
+    it('should return the practitioner associated with the contactId provided', () => {
+      const caseEntity = new Case(
+        {
+          ...MOCK_CASE,
+          privatePractitioners: [{ representing: ['567'], userId: '567' }],
+        },
+        { applicationContext },
+      );
+
+      const practitioner = caseEntity.getPractitionersRepresenting('567');
+
+      expect(practitioner).toMatchObject([
+        {
+          representing: ['567'],
+          userId: '567',
+        },
+      ]);
+    });
+
+    it('should return an empty array if no practitioners are associated with the contactId provided', () => {
+      const caseEntity = new Case(
+        {
+          ...MOCK_CASE,
+          privatePractitioners: [{ representing: ['123'], userId: '567' }],
+        },
+        { applicationContext },
+      );
+
+      const practitioner = caseEntity.getPractitionersRepresenting('567');
+
+      expect(practitioner).toMatchObject([]);
+    });
+  });
 });
