9a1d38d2a1f6e5e5417f359638d257ef2a77cd3a
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-04-23 09:37:46 -0700

    Devex 837: rename AddDocketEntry to AddPaperFiling

--- web-client/integration-tests/journey/docketClerkEditsDocketEntryNonstandardC.js
+++ web-client/integration-tests/journey/docketClerkEditsDocketEntryNonstandardC.js
@@ -35,7 +35,7 @@ export const docketClerkEditsDocketEntryNonstandardC = test => {
       docketNumber: test.docketNumber,
     });
 
-    expect(test.getState('currentPage')).toEqual('AddDocketEntry');
+    expect(test.getState('currentPage')).toEqual('AddPaperFiling');
     expect(test.getState('docketEntryId')).toEqual(docketEntryId);
 
     await test.runSequence('updateDocketEntryFormValueSequence', {
