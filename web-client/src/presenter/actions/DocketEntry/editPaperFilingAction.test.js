6736f8cc66592c7779a21ebce7684cdf43a7adac
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-04-23 13:13:41 -0700

    Devex 837: unit test fixes

--- web-client/src/presenter/actions/DocketEntry/editPaperFilingAction.test.js
+++ web-client/src/presenter/actions/DocketEntry/editPaperFilingAction.test.js
@@ -15,128 +15,6 @@ describe('editPaperFilingAction', () => {
     };
   });
 
-  it('file a new docket entry with an uploaded file', async () => {
-    applicationContext
-      .getUseCases()
-      .fileDocketEntryInteractor.mockReturnValue(caseDetail);
-
-    const result = await runAction(editPaperFilingAction, {
-      modules: {
-        presenter,
-      },
-      props: {
-        primaryDocumentFileId: 'document-id-123',
-      },
-      state: {
-        caseDetail,
-        document: '123-456-789-abc',
-        form: {
-          primaryDocumentFile: {},
-        },
-      },
-    });
-
-    expect(
-      applicationContext.getUseCases().addCoversheetInteractor,
-    ).toHaveBeenCalled();
-    expect(
-      applicationContext.getUseCases().fileDocketEntryInteractor,
-    ).toHaveBeenCalled();
-    expect(
-      applicationContext.getUseCases().validatePdfInteractor,
-    ).toHaveBeenCalled();
-    expect(
-      applicationContext.getUseCases().virusScanPdfInteractor,
-    ).toHaveBeenCalled();
-    expect(result.output).toEqual({
-      caseDetail,
-      docketEntryId: 'document-id-123',
-      docketNumber: caseDetail.docketNumber,
-      overridePaperServiceAddress: true,
-    });
-  });
-
-  it('file a new docket entry with an uploaded file, but does not generate a coversheet when saved for later', async () => {
-    applicationContext
-      .getUseCases()
-      .fileDocketEntryInteractor.mockReturnValue(caseDetail);
-
-    const result = await runAction(editPaperFilingAction, {
-      modules: {
-        presenter,
-      },
-      props: {
-        isSavingForLater: true,
-        primaryDocumentFileId: 'document-id-123',
-      },
-      state: {
-        caseDetail,
-        document: '123-456-789-abc',
-        form: {
-          primaryDocumentFile: {},
-        },
-      },
-    });
-
-    expect(
-      applicationContext.getUseCases().addCoversheetInteractor,
-    ).not.toHaveBeenCalled();
-    expect(
-      applicationContext.getUseCases().fileDocketEntryInteractor,
-    ).toHaveBeenCalled();
-    expect(
-      applicationContext.getUseCases().validatePdfInteractor,
-    ).toHaveBeenCalled();
-    expect(
-      applicationContext.getUseCases().virusScanPdfInteractor,
-    ).toHaveBeenCalled();
-    expect(result.output).toEqual({
-      caseDetail,
-      docketEntryId: 'document-id-123',
-      docketNumber: caseDetail.docketNumber,
-      overridePaperServiceAddress: true,
-    });
-  });
-
-  it('file a new docket entry without an uploaded file', async () => {
-    applicationContext
-      .getUseCases()
-      .fileDocketEntryInteractor.mockReturnValue(caseDetail);
-
-    const result = await runAction(editPaperFilingAction, {
-      modules: {
-        presenter,
-      },
-      props: {
-        isSavingForLater: true,
-      },
-      state: {
-        caseDetail,
-        document: '123-456-789-abc',
-        form: {},
-      },
-    });
-
-    expect(
-      applicationContext.getUseCases().addCoversheetInteractor,
-    ).not.toHaveBeenCalled();
-    expect(
-      applicationContext.getUseCases().fileDocketEntryInteractor,
-    ).toHaveBeenCalled();
-    expect(
-      applicationContext.getUseCases().validatePdfInteractor,
-    ).not.toHaveBeenCalled();
-    expect(
-      applicationContext.getUseCases().virusScanPdfInteractor,
-    ).not.toHaveBeenCalled();
-    expect(result.output).toEqual({
-      caseDetail,
-      docketEntryId: expect.anything(),
-      docketNumber: caseDetail.docketNumber, // uuidv4
-      overridePaperServiceAddress: true,
-    });
-  });
-
   it('saves an existing docket entry with an uploaded file', async () => {
     applicationContext
       .getUseCases()
@@ -156,7 +34,6 @@ describe('editPaperFilingAction', () => {
           isFileAttached: true,
           primaryDocumentFile: {},
         },
-        isEditingDocketEntry: true,
       },
     });
 
@@ -197,7 +74,6 @@ describe('editPaperFilingAction', () => {
         docketEntryId: 'document-id-123',
         document: '123-456-789-abc',
         form: {},
-        isEditingDocketEntry: true,
       },
     });
 
@@ -245,7 +121,6 @@ describe('editPaperFilingAction', () => {
         form: {
           isFileAttached: true,
         },
-        isEditingDocketEntry: true,
       },
     });
 
