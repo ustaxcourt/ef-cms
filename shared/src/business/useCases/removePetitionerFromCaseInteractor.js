f812acfa2ab8e71cde5ce573f96ddbc9111f1d33
TElliott <telliott@flexion.us>
2021-04-23 11:34:24 -0500

    5386: WIP tests [skip ci]

--- shared/src/business/useCases/removePetitionerFromCaseInteractor.js
+++ shared/src/business/useCases/removePetitionerFromCaseInteractor.js
@@ -1,3 +1,14 @@
+const {
+  CASE_STATUS_TYPES,
+  CONTACT_TYPES,
+  DOCUMENT_PROCESSING_STATUS_OPTIONS,
+} = require('../entities/EntityConstants');
+const {
+  isAuthorized,
+  ROLE_PERMISSIONS,
+} = require('../../authorization/authorizationClientService');
+const { UnauthorizedError } = require('../../errors/errors');
+
 /**
  * used to remove a petitioner from a case
  *
@@ -7,7 +18,26 @@
  * @param {string} providers.docketNumber the docket number of the case
  * @returns {object} the case data
  */
+
 exports.removePetitionerFromCaseInteractor = async (
   applicationContext,
   { contactId, docketNumber },
-) => {};
+) => {
+  const user = applicationContext.getCurrentUser();
+
+  if (!isAuthorized(user, ROLE_PERMISSIONS.EDIT_PETITIONER_INFO)) {
+    throw new UnauthorizedError(
+      'Unauthorized for removing petitioner from case',
+    );
+  }
+
+  const oldCase = await applicationContext
+    .getPersistenceGateway()
+    .getCaseByDocketNumber({ applicationContext, docketNumber });
+
+  if (oldCase.status === CASE_STATUS_TYPES.new) {
+    throw new Error(
+      `Case with docketNumber ${oldCase.docketNumber} has not been served`,
+    );
+  }
+};
