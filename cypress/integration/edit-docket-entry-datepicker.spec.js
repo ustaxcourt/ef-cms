3c043f5d13df5d1585025da8f90426833de323a8
Kris Koskelin <kkoskelin@flexion.us>
2021-04-23 13:11:04 -0500

    Cypress 7 Upgrade: fix syntax for some click commands
    remove unnecessary 'spec' parameter for cypress public, as test files are specified in the config

--- cypress/integration/edit-docket-entry-datepicker.spec.js
+++ cypress/integration/edit-docket-entry-datepicker.spec.js
@@ -8,12 +8,12 @@ describe('Edit Docket Entry- Change Doc Type', function () {
       .clear()
       .type('Certificate of Service');
 
-    cy.get('#react-select-2-option-0').click();
+    cy.get('#react-select-2-option-0').click({ force: true });
     cy.get('input#date-of-service-date').clear().type('01/01/2020');
     cy.get('#document-type .select-react-element__input input')
       .clear()
       .type('Amended Certificate of Service');
-    cy.get('#react-select-2-option-0').click();
+    cy.get('#react-select-2-option-0').click({ force: true });
     cy.get('input#date-of-service-date').should('be.empty');
   });
 });
