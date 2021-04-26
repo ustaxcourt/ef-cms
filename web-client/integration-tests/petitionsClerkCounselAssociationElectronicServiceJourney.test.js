bf6fcdcfac54ce9ac5c6e1f99f524c19dfdc3913
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2021-04-26 16:17:04 -0600

    resetting contyact secondary service indicator

--- web-client/integration-tests/petitionsClerkCounselAssociationElectronicServiceJourney.test.js
+++ web-client/integration-tests/petitionsClerkCounselAssociationElectronicServiceJourney.test.js
@@ -1,18 +1,27 @@
 import { SERVICE_INDICATOR_TYPES } from '../../shared/src/business/entities/EntityConstants';
 import { applicationContextForClient as applicationContext } from '../../shared/src/business/test/createTestApplicationContext';
 import {
+  contactPrimaryFromState,
   contactSecondaryFromState,
   loginAs,
   refreshElasticsearchIndex,
   setupTest,
   uploadPetition,
 } from './helpers';
-import { petitionsClerkAddsPractitionersToCase } from './journey/petitionsClerkAddsPractitionersToCase';
-import { petitionsClerkRemovesPractitionerFromCase } from './journey/petitionsClerkRemovesPractitionerFromCase';
+// import { docketClerkAddsPetitionerToCase } from './journey/docketClerkAddsPetitionerToCase';
 import { petitionsClerkServesElectronicCaseToIrs } from './journey/petitionsClerkServesElectronicCaseToIrs';
 
 const test = setupTest();
 const { COUNTRY_TYPES, PARTY_TYPES } = applicationContext.getConstants();
+
+import { formattedCaseDetail as formattedCaseDetailComputed } from '../src/presenter/computeds/formattedCaseDetail';
+import { getContactSecondary } from '../../shared/src/business/entities/cases/Case';
+import { runCompute } from 'cerebral/test';
+import { withAppContextDecorator } from '../src/withAppContext';
+
+const formattedCaseDetail = withAppContextDecorator(
+  formattedCaseDetailComputed,
+);
 
 describe('Petitions Clerk Counsel Association Journey', () => {
   beforeAll(() => {
@@ -30,8 +39,7 @@ describe('Petitions Clerk Counsel Association Journey', () => {
         address1: '734 Cowley Parkway',
         city: 'Amazing',
         countryType: COUNTRY_TYPES.DOMESTIC,
-        email: 'petitioner2@example.com',
-        name: 'Jimothy Schultz',
+        name: 'Test Petitioner 2',
         phone: '+1 (884) 358-9729',
         postalCode: '77546',
         state: 'AZ',
@@ -45,6 +53,9 @@ describe('Petitions Clerk Counsel Association Journey', () => {
   loginAs(test, 'petitionsclerk@example.com');
   petitionsClerkServesElectronicCaseToIrs(test);
 
+  // loginAs(test, 'docketclerk@example.com');
+  // docketClerkAddsPetitionerToCase(test, { name: 'Test Petitioner 2' });
+
   loginAs(test, 'admissionsclerk@example.com');
   it('admissions clerk adds secondary petitioner email with existing cognito account to case', async () => {
     await refreshElasticsearchIndex();
@@ -55,6 +66,10 @@ describe('Petitions Clerk Counsel Association Journey', () => {
       contactId: contactSecondary.contactId,
       docketNumber: test.docketNumber,
     });
+
+    expect(contactSecondary.serviceIndicator).toEqual(
+      SERVICE_INDICATOR_TYPES.SI_PAPER,
+    );
 
     expect(test.getState('currentPage')).toEqual(
       'EditPetitionerInformationInternal',
@@ -100,6 +115,110 @@ describe('Petitions Clerk Counsel Association Journey', () => {
 
   loginAs(test, 'petitionsclerk@example.com');
 
-  petitionsClerkAddsPractitionersToCase(test);
-  petitionsClerkRemovesPractitionerFromCase(test);
+  it('Petitions clerk manually adds a privatePractitioner to case', async () => {
+    await test.runSequence('gotoCaseDetailSequence', {
+      docketNumber: test.docketNumber,
+    });
+
+    const practitionerBarNumber = 'PT1234';
+
+    expect(test.getState('caseDetail.privatePractitioners')).toEqual([]);
+
+    await test.runSequence('openAddPrivatePractitionerModalSequence');
+
+    expect(
+      test.getState('validationErrors.practitionerSearchError'),
+    ).toBeDefined();
+
+    await test.runSequence('updateFormValueSequence', {
+      key: 'practitionerSearch',
+      value: practitionerBarNumber,
+    });
+
+    await test.runSequence('openAddPrivatePractitionerModalSequence');
+
+    expect(
+      test.getState('validationErrors.practitionerSearchError'),
+    ).toBeUndefined();
+    expect(test.getState('modal.practitionerMatches.length')).toEqual(1);
+
+    //default selected because there was only 1 match
+    let practitionerMatch = test.getState('modal.practitionerMatches.0');
+    expect(test.getState('modal.user.userId')).toEqual(
+      practitionerMatch.userId,
+    );
+
+    await test.runSequence('updateModalValueSequence', {
+      key: 'representingPrimary',
+      value: true,
+    });
+    await test.runSequence('updateModalValueSequence', {
+      key: 'representingSecondary',
+      value: true,
+    });
+
+    expect(
+      test.getState('validationErrors.practitionerSearchError'),
+    ).toBeUndefined();
+
+    await test.runSequence('associatePrivatePractitionerWithCaseSequence');
+
+    expect(test.getState('caseDetail.privatePractitioners.length')).toEqual(1);
+    const contactPrimary = contactPrimaryFromState(test);
+    const contactSecondary = contactSecondaryFromState(test);
+
+    expect(
+      test.getState('caseDetail.privatePractitioners.0.representing'),
+    ).toEqual([contactPrimary.contactId, contactSecondary.contactId]);
+
+    expect(test.getState('caseDetail.privatePractitioners.0.name')).toEqual(
+      practitionerMatch.name,
+    );
+
+    let formatted = runCompute(formattedCaseDetail, {
+      state: test.getState(),
+    });
+
+    expect(formatted.privatePractitioners.length).toEqual(1);
+    expect(formatted.privatePractitioners[0].formattedName).toEqual(
+      `${practitionerMatch.name} (${practitionerMatch.barNumber})`,
+    );
+
+    await refreshElasticsearchIndex();
+  });
+
+  it('Petitions clerk removes a practitioner from a case', async () => {
+    expect(test.getState('caseDetail.privatePractitioners').length).toEqual(1);
+
+    await test.runSequence('openEditPrivatePractitionersModalSequence');
+
+    await test.runSequence('updateModalValueSequence', {
+      key: 'privatePractitioners.0.removeFromCase',
+      value: true,
+    });
+
+    await test.runSequence('submitEditPrivatePractitionersModalSequence');
+
+    await refreshElasticsearchIndex();
+
+    expect(test.getState('validationErrors')).toEqual({});
+
+    expect(test.getState('caseDetail.privatePractitioners').length).toEqual(0);
+  });
+
+  it('verifies the  service indicator for the second petitioner reverts to electronic', async () => {
+    await test.runSequence('gotoCaseDetailSequence', {
+      docketNumber: test.docketNumber,
+    });
+
+    const formattedCase = runCompute(formattedCaseDetail, {
+      state: test.getState(),
+    });
+
+    const contactSecondary = getContactSecondary(formattedCase);
+
+    expect(contactSecondary.serviceIndicator).toEqual(
+      SERVICE_INDICATOR_TYPES.SI_ELECTRONIC,
+    );
+  });
 });
