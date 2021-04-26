dac4dc8fbc29174622c6f60d32bcd732475c6ff1
rosie highsmith <ahighsmith@flexion.us>
2021-04-26 11:50:49 -0700

    7808: starting integration test to cover bug [skip ci]

---
+++ web-client/integration-tests/petitionsClerkCounselAssociationElectronicServiceJourney.test.js
@@ -0,0 +1,48 @@
+import { applicationContextForClient as applicationContext } from '../../shared/src/business/test/createTestApplicationContext';
+import { loginAs, setupTest, uploadPetition } from './helpers';
+import { petitionsClerkAddsPractitionersToCase } from './journey/petitionsClerkAddsPractitionersToCase';
+import { petitionsClerkAddsRespondentsToCase } from './journey/petitionsClerkAddsRespondentsToCase';
+import { petitionsClerkEditsPractitionerOnCase } from './journey/petitionsClerkEditsPractitionerOnCase';
+import { petitionsClerkRemovesPractitionerFromCase } from './journey/petitionsClerkRemovesPractitionerFromCase';
+import { petitionsClerkRemovesRespondentFromCase } from './journey/petitionsClerkRemovesRespondentFromCase';
+import { petitionsClerkViewsCaseDetail } from './journey/petitionsClerkViewsCaseDetail';
+
+const test = setupTest();
+const { COUNTRY_TYPES, PARTY_TYPES } = applicationContext.getConstants();
+
+describe('Petitions Clerk Counsel Association Journey', () => {
+  beforeAll(() => {
+    jest.setTimeout(30000);
+  });
+
+  afterAll(() => {
+    test.closeSocket();
+  });
+
+  loginAs(test, 'petitioner@example.com');
+  it('Create test case', async () => {
+    // todo: make this a service type of electronic and do the same thing!!!!d as;ldkfjasl;kdjf
+    const caseDetail = await uploadPetition(test, {
+      contactSecondary: {
+        address1: '734 Cowley Parkway',
+        city: 'Amazing',
+        countryType: COUNTRY_TYPES.DOMESTIC,
+        name: 'Jimothy Schultz',
+        phone: '+1 (884) 358-9729',
+        postalCode: '77546',
+        state: 'AZ',
+      },
+      partyType: PARTY_TYPES.petitionerSpouse,
+    });
+    expect(caseDetail.docketNumber).toBeDefined();
+    test.docketNumber = caseDetail.docketNumber;
+  });
+
+  loginAs(test, 'petitionsclerk@example.com');
+  petitionsClerkViewsCaseDetail(test);
+  petitionsClerkAddsPractitionersToCase(test);
+  petitionsClerkAddsRespondentsToCase(test);
+  petitionsClerkEditsPractitionerOnCase(test);
+  petitionsClerkRemovesPractitionerFromCase(test);
+  petitionsClerkRemovesRespondentFromCase(test);
+});
