6775e9202de030ec46dc6473b11c855146376abb
Eric Sorenson <eric@sorenson.software>
2021-04-27 18:21:48 -0500

    polyfill util

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -70,6 +70,7 @@ module.exports = {
     fallback: {
       crypto: require.resolve('crypto-browserify'),
       stream: require.resolve('stream-browserify'),
+      util: require.resolve('util/'),
     },
   },
   target: 'web',
