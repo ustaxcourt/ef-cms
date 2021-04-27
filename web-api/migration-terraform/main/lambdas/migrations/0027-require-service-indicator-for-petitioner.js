b6c6f2eddfcbf762307fadc3119abf5aab1e718f
Rachel Schneiderman <rschneiderman@flexion.us>
2021-04-27 13:17:18 -0400

    5386: Fixing seed data and integration tests

--- web-api/migration-terraform/main/lambdas/migrations/0027-require-service-indicator-for-petitioner.js
+++ web-api/migration-terraform/main/lambdas/migrations/0027-require-service-indicator-for-petitioner.js
@@ -14,11 +14,27 @@ const migrateItems = async items => {
     if (item.pk.startsWith('case|') && item.sk.startsWith('case|')) {
       for (const petitioner of item.petitioners) {
         if (!petitioner.serviceIndicator) {
+          // TODO - should we call setSerivceIndicatorsForCase?
+
+          // Refactor setServiceIndicators for case: if contactId is in a practitioner's array, they are represented
+
+          // if you are represented, SI is NONE
+          // if you have an email AND you are represented, SI is NONE
+          // if you have an email AND you are NOT represented, SI is ELECTRONIC
+          // if the case is paper, you are NOT represented, and you DONT have an email, SI is PAPER
           petitioner.serviceIndicator = SERVICE_INDICATOR_TYPES.SI_NONE;
         }
       }
 
       new Case(item, { applicationContext }).validate();
+
+      applicationContext.logger.info(
+        `Updating case ${item.docketNumber} to add serviceIndicator for any petitioners that don't already have one.`,
+        {
+          pk: item.pk,
+          sk: item.sk,
+        },
+      );
 
       itemsAfter.push(item);
     } else {
