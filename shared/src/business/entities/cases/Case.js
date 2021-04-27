5e14b303f632f58a0df69c48e1e2a81db04ea47b
Kris Koskelin <kkoskelin@flexion.us>
2021-04-27 15:12:25 -0500

    8188: trim whitespace at beginning and end of strings used for caption generation
    unrelated: add docket number to error message for logging purposes
    unrelated: do not attempt to retrieve pending user email for undefined contacts
    related: reduced complexity for caption generation code

--- shared/src/business/entities/cases/Case.js
+++ shared/src/business/entities/cases/Case.js
@@ -47,7 +47,7 @@ const {
 const {
   shouldGenerateDocketRecordIndex,
 } = require('../../utilities/shouldGenerateDocketRecordIndex');
-const { compact, includes, isEmpty } = require('lodash');
+const { clone, compact, includes, isEmpty } = require('lodash');
 const { compareStrings } = require('../../utilities/sortFunctions');
 const { ContactFactory } = require('../contacts/ContactFactory');
 const { Correspondence } = require('../Correspondence');
@@ -768,12 +768,38 @@ joiValidationDecorator(
  * @returns {string} the generated case caption
  */
 Case.getCaseCaption = function (rawCase) {
-  let caseCaption;
-  const primaryContact = getContactPrimary(rawCase) || rawCase.contactPrimary;
-  const secondaryContact =
-    getContactSecondary(rawCase) || rawCase.contactSecondary;
+  const primaryContact = clone(
+    getContactPrimary(rawCase) || rawCase.contactPrimary,
+  );
+  const secondaryContact = clone(
+    getContactSecondary(rawCase) || rawCase.contactSecondary,
+  );
 
-  switch (rawCase.partyType) {
+  // trim ALL white space from these non-validated strings
+  primaryContact.name = primaryContact.name.trim();
+  if (primaryContact.secondaryName) {
+    primaryContact.secondaryName = primaryContact.secondaryName.trim();
+  }
+  if (primaryContact.title) {
+    primaryContact.title = primaryContact.title.trim();
+  }
+  if (secondaryContact?.name) {
+    secondaryContact.name = secondaryContact.name.trim();
+  }
+  return generateCaptionFromContacts({
+    partyType: rawCase.partyType,
+    primaryContact,
+    secondaryContact,
+  });
+};
+
+const generateCaptionFromContacts = ({
+  partyType,
+  primaryContact,
+  secondaryContact,
+}) => {
+  let caseCaption;
+  switch (partyType) {
     case PARTY_TYPES.corporation:
     case PARTY_TYPES.petitioner:
       caseCaption = `${primaryContact.name}, Petitioner`;
