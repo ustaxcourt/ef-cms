58b707a6da7c44529e6330970cb87f6edf150f26
Kris Koskelin <kkoskelin@flexion.us>
2021-04-29 12:53:27 -0500

    8188: increase coverage per PR comment

--- shared/src/business/entities/cases/Case.test.js
+++ shared/src/business/entities/cases/Case.test.js
@@ -1652,6 +1652,40 @@ describe('Case entity', () => {
       });
       expect(caseCaption).toEqual('Bob Smith, Petitioner');
     });
+
+    it('where a party is an estate, gets the primary contact from the petitioners array and trims spaces from names and titles', () => {
+      const caseCaption = Case.getCaseCaption({
+        partyType: PARTY_TYPES.estate,
+        petitioners: [
+          {
+            contactType: CONTACT_TYPES.primary,
+            name: '    Frank Frink                 ',
+            secondaryName: '        Robert Childan            ',
+            title: '    Proprietor                 ',
+          },
+        ],
+      });
+      expect(caseCaption).toEqual(
+        'Estate of Frank Frink, Deceased, Robert Childan, Proprietor, Petitioner(s)',
+      );
+    });
+
+    it('where party is petitioner and spouse, gets the primary contact from the petitioners array and trims spaces from all names', () => {
+      const caseCaption = Case.getCaseCaption({
+        partyType: PARTY_TYPES.petitionerSpouse,
+        petitioners: [
+          {
+            contactType: CONTACT_TYPES.primary,
+            name: '      John Smith               ',
+          },
+          {
+            contactType: CONTACT_TYPES.secondary,
+            name: '         Helen Smith            ',
+          },
+        ],
+      });
+      expect(caseCaption).toEqual('John Smith & Helen Smith, Petitioners');
+    });
   });
 
   describe('generateNextDocketRecordIndex', () => {
