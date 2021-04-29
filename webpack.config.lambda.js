99b61db6c3fadd89e3ad677ffe2d4ae61a30ee8d
Eric Sorenson <eric@sorenson.software>
2021-04-29 15:35:11 -0500

    add EnvironmentPlugin to lambdas

--- webpack.config.lambda.js
+++ webpack.config.lambda.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
   mode: 'production',
   module: {
@@ -13,7 +15,29 @@ module.exports = {
       },
     ],
   },
-  plugins: [],
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      API_URL: null,
+      CI: null,
+      CIRCLE_SHA1: null,
+      COGNITO: null,
+      COGNITO_CLIENT_ID: null,
+      COGNITO_REDIRECT_URI: null,
+      COGNITO_TOKEN_URL: null,
+      ENV: null,
+      FILE_UPLOAD_MODAL_TIMEOUT: null,
+      NO_SCANNER: null,
+      NODE_ENV: null,
+      SCANNER_RESOURCE_URI: null,
+      SESSION_MODAL_TIMEOUT: null,
+      SESSION_TIMEOUT: null,
+      SKIP_VIRUS_SCAN: null,
+      STAGE: null,
+      USTC_DEBUG: null,
+      USTC_ENV: null,
+      WS_URL: null,
+    }),
+  ],
   resolve: {
     extensions: ['.js', '.jsx'],
   },
