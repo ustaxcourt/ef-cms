f2f6c07aed59b2ec7c4ae290500209487dd9f498
Rachel Schneiderman <rschneiderman@flexion.us>
2021-04-30 17:42:15 -0400

    8112: WIP petitioner card info mostly hard coded but looking OK [skip ci]

--- web-client/src/views/CaseDetail/PartiesInformation.jsx
+++ web-client/src/views/CaseDetail/PartiesInformation.jsx
@@ -1,117 +1,159 @@
+import { AddressDisplay } from './AddressDisplay';
 import { Button } from '../../ustc-ui/Button/Button';
 import { FormGroup } from '../../ustc-ui/FormGroup/FormGroup';
 import { connect } from '@cerebral/react';
+import { state } from 'cerebral';
 import React from 'react';
 import classNames from 'classnames';
 
-const PartiesInformation = connect({}, function PartiesInformation() {
-  return (
-    <>
-      <div className="grid-row grid-gap-5">
-        <div className="grid-col-3">
-          <div className="border border-base-lighter">
-            <div className="grid-row padding-left-205 grid-header">
-              Parties & Counsel
-            </div>
-            <div className="">
-              <Button
-                className={classNames(
-                  'usa-button--unstyled attachment-viewer-button',
-                )}
-              >
-                <div className="grid-row margin-left-205">
-                  Petitioner(s) & Counsel
-                </div>
-              </Button>
-              <Button
-                className={classNames(
-                  'usa-button--unstyled attachment-viewer-button',
-                )}
-              >
-                <div className="grid-row margin-left-205">
-                  Intervenor/Participants & Counsel
-                </div>
-              </Button>
-              <Button
-                className={classNames(
-                  'usa-button--unstyled attachment-viewer-button',
-                )}
-              >
-                <div className="grid-row margin-left-205">
-                  Respondent Counsel
-                </div>
-              </Button>
-            </div>
-          </div>
-        </div>
-        <div className="grid-col-9">
-          <div className="grid-row">
-            <div className="grid-col-4">
-              <h3>Petitioner(s)</h3>
-            </div>
-            <div className="grid-col-2">
-              <div className="text-right">
-                <span
-                  className="label margin-right-4 margin-top-05"
-                  id="practitioner-counsel-search-description"
+const PartiesInformation = connect(
+  { caseDetail: state.caseDetail },
+  function PartiesInformation({ caseDetail }) {
+    return (
+      <>
+        <div className="grid-row grid-gap-5">
+          <div className="grid-col-3">
+            <div className="border border-base-lighter">
+              <div className="grid-row padding-left-205 grid-header">
+                Parties & Counsel
+              </div>
+              <div className="">
+                <Button
+                  className={classNames(
+                    'usa-button--unstyled attachment-viewer-button',
+                  )}
                 >
-                  Add counsel
-                </span>
+                  <div className="grid-row margin-left-205">
+                    Petitioner(s) & Counsel
+                  </div>
+                </Button>
+                <Button
+                  className={classNames(
+                    'usa-button--unstyled attachment-viewer-button',
+                  )}
+                >
+                  <div className="grid-row margin-left-205">
+                    Intervenor/Participants & Counsel
+                  </div>
+                </Button>
+                <Button
+                  className={classNames(
+                    'usa-button--unstyled attachment-viewer-button',
+                  )}
+                >
+                  <div className="grid-row margin-left-205">
+                    Respondent Counsel
+                  </div>
+                </Button>
               </div>
             </div>
-            <div className="grid-col-4">
-              <FormGroup
-                className="margin-bottom-0 margin-top-0"
-                // errorText={validationErrors.practitionerSearchError}
-              >
-                <form
-                  className="usa-search"
-                  onSubmit={e => {
-                    e.preventDefault();
-                    // openAddPrivatePractitionerModalSequence();
-                  }}
+          </div>
+          <div className="grid-col-9">
+            <div className="grid-row">
+              <div className="grid-col-4">
+                <h3>Petitioner(s)</h3>
+              </div>
+              <div className="grid-col-2">
+                <div className="text-right">
+                  <span
+                    className="label margin-right-4 margin-top-05"
+                    id="practitioner-counsel-search-description"
+                  >
+                    Add counsel
+                  </span>
+                </div>
+              </div>
+              <div className="grid-col-4">
+                <FormGroup
+                  className="margin-bottom-0 margin-top-0"
+                  // errorText={validationErrors.practitionerSearchError}
                 >
-                  <div role="search">
-                    <label
-                      className="usa-sr-only"
-                      htmlFor="practitioner-search-field"
-                    >
-                      Search
-                    </label>
-                    <input
-                      aria-describedby="practitioner-counsel-search-description"
-                      className={classNames(
-                        'usa-input margin-bottom-0',
-                        // validationErrors.practitionerSearchError &&
-                        //   'usa-input--error',
-                      )}
-                      id="practitioner-search-field"
-                      name="practitionerSearch"
-                      placeholder="Enter bar no. or name"
-                      type="search"
-                      // value={form.practitionerSearch || ''}
-                    />
-                    <button
-                      className="small-search-button usa-button"
-                      id="search-for-practitioner"
-                      type="submit"
-                    >
-                      <span className="usa-search__submit-text">Search</span>
-                    </button>
-                  </div>
-                </form>
-              </FormGroup>
+                  <form
+                    className="usa-search"
+                    onSubmit={e => {
+                      e.preventDefault();
+                      // openAddPrivatePractitionerModalSequence();
+                    }}
+                  >
+                    <div role="search">
+                      <label
+                        className="usa-sr-only"
+                        htmlFor="practitioner-search-field"
+                      >
+                        Search
+                      </label>
+                      <input
+                        aria-describedby="practitioner-counsel-search-description"
+                        className={classNames(
+                          'usa-input margin-bottom-0',
+                          // validationErrors.practitionerSearchError &&
+                          //   'usa-input--error',
+                        )}
+                        id="practitioner-search-field"
+                        name="practitionerSearch"
+                        placeholder="Enter bar no. or name"
+                        type="search"
+                        // value={form.practitionerSearch || ''}
+                      />
+                      <button
+                        className="small-search-button usa-button"
+                        id="search-for-practitioner"
+                        type="submit"
+                      >
+                        <span className="usa-search__submit-text">Search</span>
+                      </button>
+                    </div>
+                  </form>
+                </FormGroup>
+              </div>
+              <div className="grid-col-2">
+                <Button link className="float-right" icon="plus-circle">
+                  Add Party
+                </Button>
+              </div>
             </div>
-            <div className="grid-col-2">
-              <Button link className="float-right" icon="plus-circle">
-                Add Party
-              </Button>
+
+            <div className="card height-full">
+              <div className="content-wrapper">
+                <h3>
+                  A Name Of A Person{' '}
+                  <Button
+                    link
+                    className="margin-top-1 padding-0 float-right"
+                    href={'/case-detail/'}
+                    icon="edit"
+                  >
+                    Edit
+                  </Button>
+                </h3>
+
+                <div className="bg-primary text-white padding-2">
+                  Petitioner
+                </div>
+
+                <AddressDisplay
+                  contact={caseDetail.petitioners[0]}
+                  showEmail={true}
+                  // showSealAddressLink={caseInformationHelper.showSealAddressLink}
+                />
+
+                {caseDetail.petitioners[0].serviceIndicator && (
+                  <div className="margin-top-4">
+                    <p className="semi-bold margin-bottom-0">
+                      Service preference
+                    </p>
+                    {caseDetail.petitioners[0].serviceIndicator}
+                  </div>
+                )}
+
+                <h3>Counsel</h3>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
-});
+      </>
+    );
+  },
+);
 
 export { PartiesInformation };
