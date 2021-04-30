4187df48c112dbe869a95626766aeb6e2a5b5072
Rachel Schneiderman <rschneiderman@flexion.us>
2021-04-30 16:53:06 -0400

    8112: WIP petitioners screen in parties tab [skip ci]

--- web-client/src/views/CaseDetail/PartiesInformation.jsx
+++ web-client/src/views/CaseDetail/PartiesInformation.jsx
@@ -8,7 +8,7 @@ const PartiesInformation = connect({}, function PartiesInformation() {
   return (
     <>
       <div className="grid-row grid-gap-5">
-        <div className="grid-col-4">
+        <div className="grid-col-3">
           <div className="border border-base-lighter">
             <div className="grid-row padding-left-205 grid-header">
               Parties & Counsel
@@ -44,64 +44,71 @@ const PartiesInformation = connect({}, function PartiesInformation() {
             </div>
           </div>
         </div>
-        <div className="grid-col-8">
-          <h3>Petitioner(s)</h3>
-          <div className="grid-col-3 text-right">
-            <span
-              className="label margin-right-4 margin-top-05"
-              id="practitioner-counsel-search-description"
-            >
-              Add counsel
-            </span>
-          </div>
-          <div className="grid-col-3 margin-top-neg-05">
-            <FormGroup
-              className="margin-bottom-0"
-              // errorText={validationErrors.practitionerSearchError}
-            >
-              <form
-                className="usa-search"
-                onSubmit={e => {
-                  e.preventDefault();
-                  // openAddPrivatePractitionerModalSequence();
-                }}
+        <div className="grid-col-9">
+          <div className="grid-row">
+            <div className="grid-col-4">
+              <h3>Petitioner(s)</h3>
+            </div>
+            <div className="grid-col-2">
+              <div className="text-right">
+                <span
+                  className="label margin-right-4 margin-top-05"
+                  id="practitioner-counsel-search-description"
+                >
+                  Add counsel
+                </span>
+              </div>
+            </div>
+            <div className="grid-col-5">
+              <FormGroup
+                className="margin-bottom-0 margin-top-0"
+                // errorText={validationErrors.practitionerSearchError}
               >
-                <div role="search">
-                  <label
-                    className="usa-sr-only"
-                    htmlFor="practitioner-search-field"
-                  >
-                    Search
-                  </label>
-                  <input
-                    aria-describedby="practitioner-counsel-search-description"
-                    className={classNames(
-                      'usa-input margin-bottom-0',
-                      // validationErrors.practitionerSearchError &&
-                      //   'usa-input--error',
-                    )}
-                    id="practitioner-search-field"
-                    name="practitionerSearch"
-                    placeholder="Enter bar no. or name"
-                    type="search"
-                    // value={form.practitionerSearch || ''}
-                    onChange={e => {
-                      // updateFormValueSequence({
-                      //   key: e.target.name,
-                      //   value: e.target.value,
-                      // });
-                    }}
-                  />
-                  <button
-                    className="usa-button"
-                    id="search-for-practitioner"
-                    type="submit"
-                  >
-                    <span className="usa-search__submit-text">Search</span>
-                  </button>
-                </div>
-              </form>
-            </FormGroup>
+                <form
+                  className="usa-search"
+                  onSubmit={e => {
+                    e.preventDefault();
+                    // openAddPrivatePractitionerModalSequence();
+                  }}
+                >
+                  <div role="search">
+                    <label
+                      className="usa-sr-only"
+                      htmlFor="practitioner-search-field"
+                    >
+                      Search
+                    </label>
+                    <input
+                      aria-describedby="practitioner-counsel-search-description"
+                      className={classNames(
+                        'usa-input margin-bottom-0',
+                        // validationErrors.practitionerSearchError &&
+                        //   'usa-input--error',
+                      )}
+                      id="practitioner-search-field"
+                      name="practitionerSearch"
+                      placeholder="Enter bar no. or name"
+                      type="search"
+                      // value={form.practitionerSearch || ''}
+                      onChange={e => {
+                        // updateFormValueSequence({
+                        //   key: e.target.name,
+                        //   value: e.target.value,
+                        // });
+                      }}
+                    />
+                    <button
+                      className="ustc-search-button usa-button"
+                      id="search-for-practitioner"
+                      type="submit"
+                    >
+                      <span className="usa-search__submit-text">Search</span>
+                    </button>
+                  </div>
+                </form>
+              </FormGroup>
+            </div>
+            <div className="grid-col-1">Add Party</div>
           </div>
         </div>
       </div>
