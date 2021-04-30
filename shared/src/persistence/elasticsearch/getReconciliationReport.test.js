a267318855225963296639ddc79624f65824c4a9
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-04-30 12:25:42 -0700

    8112: test coverage

--- shared/src/persistence/elasticsearch/getReconciliationReport.test.js
+++ shared/src/persistence/elasticsearch/getReconciliationReport.test.js
@@ -2,14 +2,17 @@ const {
   applicationContext,
 } = require('../../business/test/createTestApplicationContext');
 const {
-  CASE_STATUS_TYPES,
+  SERVED_PARTIES_CODES,
 } = require('../../business/entities/EntityConstants');
-const { getReadyForTrialCases } = require('./getReadyForTrialCases');
+const { getReconciliationReport } = require('./getReconciliationReport');
 jest.mock('./searchClient');
 const { search } = require('./searchClient');
 
 describe('getReconciliationReport', () => {
   it('should search for docket entries that were served between the provided date range and were served to either the respondent or both parties', async () => {
+    const mockEndDate = '2018-03-01T00:01:00.000Z';
+    const mockStartDate = '2018-05-01T00:01:00.000Z';
+
     search.mockImplementation(async () => {
       return {
         results: [{ docketNumber: '102-20' }, { docketNumber: '134-30' }],
@@ -19,13 +22,31 @@ describe('getReconciliationReport', () => {
 
     await getReconciliationReport({
       applicationContext,
-      reconciliationDateEnd: ,
-      reconciliationDateStart: ,
+      reconciliationDateEnd: mockEndDate,
+      reconciliationDateStart: mockStartDate,
     });
 
     expect(search.mock.calls[0][0].searchParameters.body.query).toMatchObject({
-      term: {
-        'status.S': CASE_STATUS_TYPES.generalDocket,
+      bool: {
+        must: [
+          {
+            terms: {
+              'servedPartiesCode.S': [
+                SERVED_PARTIES_CODES.RESPONDENT,
+                SERVED_PARTIES_CODES.BOTH,
+              ],
+            },
+          },
+          {
+            range: {
+              'servedAt.S': {
+                format: 'strict_date_time', // ISO-8601 time stamp
+                gte: mockStartDate,
+                lte: mockEndDate,
+              },
+            },
+          },
+        ],
       },
     });
   });
