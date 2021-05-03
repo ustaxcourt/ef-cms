1784c3c9ff2a93c1a641a58c8c7c0adf711e9dbf
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-05-03 13:51:32 -0700

    8112: cleanup

--- web-client/src/presenter/actions/setDefaultCaseDetailTabAction.js
+++ web-client/src/presenter/actions/setDefaultCaseDetailTabAction.js
@@ -4,6 +4,7 @@ import { state } from 'cerebral';
  * sets the currentViewMetadata.caseDetail.informationTab to a default value if it is not already set.
  *
  * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context
  * @param {object} providers.store the cerebral store used for setting the state.cases
  * @param {object} providers.props the cerebral props object
  * @param {object} providers.get the cerebral get function used for getting state from store
