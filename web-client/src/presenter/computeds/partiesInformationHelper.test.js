20b8c1494d27f4815395647ee211c3d7d94211d9
rosie highsmith <ahighsmith@flexion.us>
2021-05-03 11:58:08 -0700

    8112: adding helper test to get representingPractitioners [skip ci]

--- web-client/src/presenter/computeds/partiesInformationHelper.test.js
+++ web-client/src/presenter/computeds/partiesInformationHelper.test.js
@@ -1,17 +1,34 @@
-import { partiesInformationHelper } from './partiesInformationHelper';
+import { applicationContext } from '../../applicationContext';
+import { partiesInformationHelper as partiesInformationHelperComputed } from './partiesInformationHelper';
 import { runCompute } from 'cerebral/test';
+import { withAppContextDecorator } from '../../withAppContext';
 
 describe('partiesInformationHelper', () => {
+  const partiesInformationHelper = withAppContextDecorator(
+    partiesInformationHelperComputed,
+    applicationContext,
+  );
+
   it('should return formatted petitioners with representing practitioners', () => {
+    const mockId = '8ee0833f-6b82-4a8a-9803-8dab8bb49b63';
+    const mockPetitioner = {
+      contactId: mockId,
+    };
+    const mockPractitioner = {
+      name: 'Test Name',
+      representing: [mockId],
+    };
     const result = runCompute(partiesInformationHelper, {
       state: {
         caseDetail: {
-          petitioners: [{}],
+          petitioners: [mockPetitioner],
+          privatePractitioners: [mockPractitioner],
         },
       },
     });
 
-    expect(result.showIndividualMessages).toBeTruthy();
-    expect(result.showSectionMessages).toBeFalsy();
+    expect(result.formattedPetitioners).toMatchObject([
+      { ...mockPetitioner, representingPractitioners: mockPractitioner },
+    ]);
   });
 });
