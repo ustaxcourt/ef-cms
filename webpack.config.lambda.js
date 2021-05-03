a6ae0da3bfdfb33ec203dcd13f9c2bf5f76ba90c
Eric Sorenson <eric@sorenson.software>
2021-05-03 14:39:03 -0500

    remove EnvironmentPlugin on lambda

--- webpack.config.lambda.js
+++ webpack.config.lambda.js
@@ -1,5 +1,3 @@
-const webpack = require('webpack');
-
 module.exports = {
   mode: 'production',
   module: {
@@ -15,29 +13,10 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new webpack.EnvironmentPlugin({
-      API_URL: null,
-      CI: null,
-      CIRCLE_SHA1: null,
-      COGNITO: null,
-      COGNITO_CLIENT_ID: null,
-      COGNITO_REDIRECT_URI: null,
-      COGNITO_TOKEN_URL: null,
-      ENV: null,
-      FILE_UPLOAD_MODAL_TIMEOUT: null,
-      NO_SCANNER: null,
-      NODE_ENV: null,
-      SCANNER_RESOURCE_URI: null,
-      SESSION_MODAL_TIMEOUT: null,
-      SESSION_TIMEOUT: null,
-      SKIP_VIRUS_SCAN: null,
-      STAGE: null,
-      USTC_DEBUG: null,
-      USTC_ENV: null,
-      WS_URL: null,
-    }),
-  ],
+  optimization: {
+    minimize: true,
+  },
+  plugins: [],
   resolve: {
     extensions: ['.js', '.jsx'],
   },
