590dd4746485e57019371065a4c5e624b3831480
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-05-03 08:32:22 -0700

    Add auto-approve to migration terraform deploy

--- web-api/migration-terraform/bin/deploy-app.sh
+++ web-api/migration-terraform/bin/deploy-app.sh
@@ -50,4 +50,4 @@ export TF_VAR_destination_table=$DESTINATION_TABLE
 
 terraform init -backend=true -backend-config=bucket="${BUCKET}" -backend-config=key="${KEY}" -backend-config=dynamodb_table="${LOCK_TABLE}" -backend-config=region="${REGION}"
 terraform plan
-terraform apply
+terraform apply --auto-approve
