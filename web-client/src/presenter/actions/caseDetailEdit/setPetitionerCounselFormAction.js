f475b8c261c92ba4529d3ac620b88766db3f0f61
TElliott <telliott@flexion.us>
2021-05-03 11:42:00 -0500

    8214: [skip-ci] create edit petitioner

--- web-client/src/presenter/actions/caseDetailEdit/setPetitionerCounselFormAction.js
+++ web-client/src/presenter/actions/caseDetailEdit/setPetitionerCounselFormAction.js
@@ -1,22 +1,23 @@
+import { cloneDeep } from 'lodash';
 import { state } from 'cerebral';
 
 /**
- * computes the date from a month, day and year value
+ * sets petitioner counsel information
  *
  * @param {object} providers the providers object
- * @param {object} providers.applicationContext the application context
  * @param {Function} providers.get the cerebral get function
  * @param {object} providers.store the cerebral store object
  * @returns {object} props object
  */
-export const setPetitionerCounselFormAction = ({
-  applicationContext,
-  get,
-  props,
-  store,
-}) => {
+
+export const setPetitionerCounselFormAction = ({ get, props, store }) => {
   const caseDetail = get(state.caseDetail);
   const { barNumber } = props;
 
-  // get petitioner connsel info from privatePractitioners via barNumber (filter / find?)
+  const privatePractitioners = cloneDeep(caseDetail.privatePractitioners);
+  const privatePractitioner = privatePractitioners.find(
+    practitioner => practitioner.barNumber === barNumber,
+  );
+
+  store.set(state.form, privatePractitioner);
 };
---
+++ web-client/src/views/EditPetitionerCounsel.jsx
@@ -0,0 +1,157 @@
+/* eslint-disable complexity
+ */
+import { Button } from '../ustc-ui/Button/Button';
+import { CaseDetailHeader } from './CaseDetail/CaseDetailHeader';
+import { ErrorNotification } from './ErrorNotification';
+import { FormCancelModalDialog } from './FormCancelModalDialog';
+import { FormGroup } from '../ustc-ui/FormGroup/FormGroup';
+import { MatchingEmailFoundModal } from './CaseDetail/MatchingEmailFoundModal';
+import { NoMatchingEmailFoundModal } from './CaseDetail/NoMatchingEmailFoundModal';
+import { RemovePetitionerModal } from './CaseDetailEdit/RemovePetitionerModal';
+import { ServiceIndicatorRadios } from './ServiceIndicatorRadios';
+import { caseDetailContactHelper } from '../presenter/computeds/caseDetailContactHelper';
+import { connect } from '@cerebral/react';
+import { sequences, state } from 'cerebral';
+import React from 'react';
+
+export const EditPetitionerCounsel = connect(
+  {
+    COUNTRY_TYPES: state.constants.COUNTRY_TYPES,
+    editPetitionerInformationHelper: state.editPetitionerInformationHelper,
+    form: state.form,
+    formCancelToggleCancelSequence: sequences.formCancelToggleCancelSequence,
+    openRemovePetitionerModalSequence:
+      sequences.openRemovePetitionerModalSequence,
+    screenMetadata: state.screenMetadata,
+    showModal: state.modal.showModal,
+    submitEditPetitionerSequence: sequences.submitEditPetitionerSequence,
+    updateFormValueSequence: sequences.updateFormValueSequence,
+    validatePetitionerSequence: sequences.validatePetitionerSequence,
+    validationErrors: state.validationErrors,
+  },
+  function EditPetitionerInformationInternal({
+    editPetitionerInformationHelper,
+    form,
+    openRemovePetitionerModalSequence,
+    showModal,
+    // submitEditPetitionerSequence,
+    // updateFormValueSequence,
+    // validatePetitionerSequence,
+    validationErrors,
+  }) {
+    return (
+      <>
+        <CaseDetailHeader />
+
+        <section className="usa-section grid-container">
+          <ErrorNotification />
+
+          <h2>Edit Petitioner Counsel</h2>
+
+          <div className="blue-container margin-bottom-5">
+            <div className="usa-form-group">
+              <FormGroup
+                className="margin-bottom-0"
+                errorText={
+                  validationErrors && validationErrors.representingPrimary
+                }
+                id={'practitioner-representing'}
+              >
+                <fieldset className="usa-fieldset margin-bottom-0">
+                  <legend
+                    className="usa-legend usa-legend--text-normal"
+                    id={'practitioner-representing-legend'}
+                  >
+                    Representing
+                  </legend>
+                  <div className="usa-checkbox">
+                    <input
+                      aria-describedby={'practitioner-representing-legend'}
+                      checked={form.representingPrimary || false}
+                      className="usa-checkbox__input"
+                      id={'representing-primary'}
+                      name={'representingPrimary'}
+                      type="checkbox"
+                      // onChange={e => {
+                      //   updateModalValueSequence({
+                      //     key: e.target.name,
+                      //     value: e.target.checked,
+                      //   });
+                      //   validateEditPrivatePractitionersSequence();
+                      // }}
+                    />
+                    <label
+                      className="usa-checkbox__label inline-block"
+                      htmlFor={'representing-primary'}
+                    >
+                      {caseDetailContactHelper.contactPrimary.name}
+                    </label>
+                  </div>
+
+                  {caseDetailContactHelper.contactSecondary &&
+                    caseDetailContactHelper.contactSecondary.name && (
+                      <div className="usa-checkbox">
+                        <input
+                          aria-describedby={'practitioner-representing-legend'}
+                          checked={form.representingSecondary || false}
+                          className="usa-checkbox__input"
+                          id={'representing-secondary'}
+                          name={'representingSecondary'}
+                          type="checkbox"
+                          // onChange={e => {
+                          //   updateModalValueSequence({
+                          //     key: e.target.name,
+                          //     value: e.target.checked,
+                          //   });
+                          //   validateEditPrivatePractitionersSequence();
+                          // }}
+                        />
+                        <label
+                          className="usa-checkbox__label inline-block"
+                          htmlFor={'representing-secondary'}
+                        >
+                          {form.representingSecondary}
+                        </label>
+                      </div>
+                    )}
+                </fieldset>
+                <div className="margin-top-2">
+                  <ServiceIndicatorRadios
+                    bind={'form'}
+                    getValidationError={() =>
+                      validationErrors && validationErrors.serviceIndicator
+                    }
+                    // validateSequence={validateEditPrivatePractitionersSequence}
+                  />
+                </div>
+              </FormGroup>
+            </div>
+
+            {editPetitionerInformationHelper.showRemovePetitionerButton && (
+              <Button
+                link
+                className="red-warning no-wrap float-right"
+                icon="trash"
+                id="remove-petitioner-btn"
+                onClick={() => {
+                  openRemovePetitionerModalSequence();
+                }}
+              >
+                Remove this petitioner
+              </Button>
+            )}
+          </div>
+        </section>
+
+        {showModal === 'FormCancelModalDialog' && (
+          <FormCancelModalDialog onCancelSequence="closeModalAndReturnToCaseDetailSequence" />
+        )}
+        {showModal === 'MatchingEmailFoundModal' && <MatchingEmailFoundModal />}
+        {showModal === 'NoMatchingEmailFoundModal' && (
+          <NoMatchingEmailFoundModal />
+        )}
+        {showModal === 'RemovePetitionerModal' && <RemovePetitionerModal />}
+      </>
+    );
+  },
+);
