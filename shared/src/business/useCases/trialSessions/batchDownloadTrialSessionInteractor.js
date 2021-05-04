64778783e84f52bcdefdddfb1f740224f4701e78
Michael Marcotte <mike.marcotte@gmail.com>
2021-05-04 12:00:57 -0400

    8156 - encapsulate error into an object

--- shared/src/business/useCases/trialSessions/batchDownloadTrialSessionInteractor.js
+++ shared/src/business/useCases/trialSessions/batchDownloadTrialSessionInteractor.js
@@ -181,6 +181,7 @@ const batchDownloadTrialSessionInteractor = async (
   };
 
   const onError = error => {
+    applicationContext.logger.error('Archive Error', { error });
     applicationContext.getNotificationGateway().sendNotificationToUser({
       applicationContext,
       message: {
