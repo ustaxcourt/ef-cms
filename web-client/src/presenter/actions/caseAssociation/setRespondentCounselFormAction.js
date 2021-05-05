c3dcaf3e502d6bfa633b79ec5fdfef4190edf153
Joseph Sutton <jsutton@flexion.us>
2021-05-05 11:38:55 -0500

    8214: adding edit respondent counsel view and route, wip for form actions and sequences [skip ci]

---
+++ web-client/src/presenter/actions/caseAssociation/setRespondentCounselFormAction.js
@@ -0,0 +1,25 @@
+import { cloneDeep } from 'lodash';
+import { state } from 'cerebral';
+
+/**
+ * takes the respondent counsel using the barNumber in props and puts onto the form
+ *
+ * @param {object} providers the providers object
+ * @param {Function} providers.get the cerebral get function
+ * @param {object} providers.store the cerebral store object
+ * @param {object} providers.props the cerebral props
+ * @param {object} providers.applicationContext the applicationContext
+ * @returns {object} props object
+ */
+
+export const setRespondentCounselFormAction = ({ get, props, store }) => {
+  const caseDetail = get(state.caseDetail);
+  const { barNumber } = props;
+
+  const irsPractitioners = cloneDeep(caseDetail.irsPractitioners);
+  const irsPractitioner = irsPractitioners.find(
+    practitioner => practitioner.barNumber === barNumber,
+  );
+
+  store.set(state.form, irsPractitioner);
+};
