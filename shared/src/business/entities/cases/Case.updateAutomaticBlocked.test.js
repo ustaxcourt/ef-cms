d8634d9902cafca06b5ab27a1a2e9c97648dac1a
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-05-07 09:44:45 -0700

    Devex 864: address PR feedback

--- shared/src/business/entities/cases/Case.updateAutomaticBlocked.test.js
+++ shared/src/business/entities/cases/Case.updateAutomaticBlocked.test.js
@@ -1,9 +1,12 @@
 const {
   applicationContext,
 } = require('../../test/createTestApplicationContext');
+const {
+  MOCK_CASE,
+  MOCK_CASE_WITHOUT_PENDING,
+} = require('../../../test/mockCase');
 const { AUTOMATIC_BLOCKED_REASONS } = require('../EntityConstants');
 const { Case } = require('./Case');
-const { MOCK_CASE } = require('../../../test/mockCase');
 
 describe('updateAutomaticBlocked', () => {
   it('sets the case as automaticBlocked with a valid blocked reason', () => {
@@ -26,5 +29,26 @@ describe('updateAutomaticBlocked', () => {
     );
     expect(caseToUpdate.automaticBlockedDate).toBeDefined();
     expect(caseToUpdate.isValid()).toBeTruthy();
+  });
+
+  it('unsets the case as automatic blocked', () => {
+    const caseToUpdate = new Case(
+      {
+        ...MOCK_CASE_WITHOUT_PENDING,
+        automaticBlocked: true,
+        automaticBlockedReason: 'because reasons',
+      },
+      {
+        applicationContext,
+      },
+    );
+
+    expect(caseToUpdate.automaticBlocked).toBeTruthy();
+
+    caseToUpdate.updateAutomaticBlocked({});
+
+    expect(caseToUpdate.automaticBlocked).toBeFalsy();
+    expect(caseToUpdate.automaticBlockedReason).toBeUndefined();
+    expect(caseToUpdate.automaticBlockedDate).toBeUndefined();
   });
 });
