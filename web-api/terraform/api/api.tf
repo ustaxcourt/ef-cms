2e821294c55dcfb6f246ba1d873a11797917bb0f
Cody Seibert <cseibert@flexion.us>
2021-05-07 13:37:46 -0400

    removing min compress

--- web-api/terraform/api/api.tf
+++ web-api/terraform/api/api.tf
@@ -23,7 +23,6 @@ resource "aws_lambda_function" "api_lambda" {
 resource "aws_api_gateway_rest_api" "gateway_for_api" {
   name = "gateway_api_${var.environment}_${var.current_color}"
 
-  minimum_compression_size = "1"
   binary_media_types = ["*/*"]
 
   endpoint_configuration {
