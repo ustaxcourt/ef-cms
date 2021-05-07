346e0b1f554814d092c2594b7576751c49c821d5
Rachel Schneiderman <rschneiderman@flexion.us>
2021-05-07 10:51:32 -0400

    5385: Update docs

--- shared/src/proxies/removePetitionerAndUpdateCaptionProxy.js
+++ shared/src/proxies/removePetitionerAndUpdateCaptionProxy.js
@@ -5,6 +5,7 @@ const { put } = require('./requests');
  *
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
+ * @param {string} providers.caseCaption the updated caseCaption
  * @param {string} providers.contactId the contactId of the petitioner to remove
  * @param {string} providers.docketNumber the docket number of the case to update
  * @returns {Promise<*>} the promise of the api call
