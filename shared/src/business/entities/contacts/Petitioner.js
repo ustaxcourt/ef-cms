32a34a6ae32b44c8f676bd4af8f83cc80cf8a130
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-05-10 09:07:41 -0700

    8112: add limit to additional name

--- shared/src/business/entities/contacts/Petitioner.js
+++ shared/src/business/entities/contacts/Petitioner.js
@@ -31,31 +31,31 @@ Petitioner.prototype.init = function init(rawContact, { applicationContext }) {
     throw new TypeError('applicationContext must be defined');
   }
 
-  this.contactId = rawContact.contactId || applicationContext.getUniqueId();
+  this.additionalName = rawContact.additionalName;
   this.address1 = rawContact.address1;
   this.address2 = rawContact.address2 || undefined;
   this.address3 = rawContact.address3 || undefined;
   this.city = rawContact.city;
+  this.contactId = rawContact.contactId || applicationContext.getUniqueId();
   this.contactType = rawContact.contactType;
   this.country = rawContact.country;
   this.countryType = rawContact.countryType;
   this.email = rawContact.email;
+  this.hasEAccess = rawContact.hasEAccess || undefined;
   this.inCareOf = rawContact.inCareOf;
   this.isAddressSealed = rawContact.isAddressSealed || false;
-  this.sealedAndUnavailable = rawContact.sealedAndUnavailable || false;
   this.name = rawContact.name;
+  this.otherFilerType = rawContact.otherFilerType;
   this.phone = rawContact.phone;
   this.postalCode = rawContact.postalCode;
-  this.secondaryName = rawContact.secondaryName;
+  this.sealedAndUnavailable = rawContact.sealedAndUnavailable || false;
   this.serviceIndicator = rawContact.serviceIndicator;
   this.state = rawContact.state;
   this.title = rawContact.title;
-  this.additionalName = rawContact.additionalName;
-  this.otherFilerType = rawContact.otherFilerType;
-  this.hasEAccess = rawContact.hasEAccess || undefined;
 };
 
 Petitioner.VALIDATION_RULES = {
+  additionalName: JoiValidationConstants.STRING.max(100).optional(),
   address1: JoiValidationConstants.STRING.max(100).required(),
   address2: JoiValidationConstants.STRING.max(100).optional().allow(null),
   address3: JoiValidationConstants.STRING.max(100).optional().allow(null),
@@ -98,7 +98,6 @@ Petitioner.VALIDATION_RULES = {
     then: JoiValidationConstants.STRING.max(100).required(),
   }),
   sealedAndUnavailable: joi.boolean().optional(),
-  secondaryName: JoiValidationConstants.STRING.max(100).optional(),
   serviceIndicator: JoiValidationConstants.STRING.valid(
     ...Object.values(SERVICE_INDICATOR_TYPES),
   ).required(),
@@ -117,6 +116,12 @@ Petitioner.VALIDATION_RULES = {
 };
 
 Petitioner.VALIDATION_ERROR_MESSAGES = {
+  additionalName: [
+    {
+      contains: 'must be less than or equal to',
+      message: 'Limit is 100 characters. Enter 100 or fewer characters.',
+    },
+  ],
   address1: 'Enter mailing address',
   city: 'Enter city',
   country: 'Enter a country',
