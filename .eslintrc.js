ea6bf80cc1d4e6fbbc972944d3db759dc5c826ee
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-05-11 12:00:27 -0700

    Devex 864: split up DocketEntry.test.js into separate test files

--- .eslintrc.js
+++ .eslintrc.js
@@ -115,7 +115,7 @@ module.exports = {
     ],
     'max-lines': [
       'error',
-      { max: 2000, skipBlankLines: true, skipComments: true }, // TODO - devex 864 - max 500 lines
+      { max: 1500, skipBlankLines: true, skipComments: true }, // TODO - devex 864 - max 500 lines
     ],
     'no-irregular-whitespace': ['error', { skipStrings: false }],
     'no-prototype-builtins': 'off',
