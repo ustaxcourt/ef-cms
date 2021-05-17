839ca06cdba365b43971b3fc484f9b65a380e5aa
rosie highsmith <ahighsmith@flexion.us>
2021-05-17 12:16:03 -0700

    8131: finished resolved PR feedback

--- web-client/src/presenter/actions/setFilersFromFilersMapForModalAction.test.js
+++ web-client/src/presenter/actions/setFilersFromFilersMapForModalAction.test.js
@@ -2,20 +2,21 @@ import { runAction } from 'cerebral/test';
 import { setFilersFromFilersMapForModalAction } from './setFilersFromFilersMapForModalAction';
 
 describe('setFilersFromFilersMapForModalAction', () => {
+  const mockContactIdChecked = '28447d01-1722-4a68-a7e1-12b2987fe579';
+  const mockContactIdUnchecked = 'a662c530-65da-437c-98c7-14939c9cfd00';
+
   it('sets state.modal.filers to contactIds from filersMap', async () => {
     const result = await runAction(setFilersFromFilersMapForModalAction, {
       state: {
         modal: {
           filersMap: {
-            '28447d01-1722-4a68-a7e1-12b2987fe579': true,
-            'a662c530-65da-437c-98c7-14939c9cfd00': false,
+            [mockContactIdChecked]: true,
+            [mockContactIdUnchecked]: false,
           },
         },
       },
     });
 
-    expect(result.state.modal.filers).toEqual([
-      '28447d01-1722-4a68-a7e1-12b2987fe579',
-    ]);
+    expect(result.state.modal.filers).toEqual([mockContactIdChecked]);
   });
 });
