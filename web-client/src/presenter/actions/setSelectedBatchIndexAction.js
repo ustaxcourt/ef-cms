3f8b5b7598731af22bb247448bee7475c962a63b
Eric Sorenson <eric@sorenson.software>
2021-05-17 10:27:56 -0500

    library upgrades

--- web-client/src/presenter/actions/setSelectedBatchIndexAction.js
+++ web-client/src/presenter/actions/setSelectedBatchIndexAction.js
@@ -6,7 +6,6 @@ import { state } from 'cerebral';
  * @param {object} providers the providers object
  * @param {Function} providers.store the cerebral store used for setting state.path
  * @param {Function} providers.props the cerebral props object
- *
  */
 export const setSelectedBatchIndexAction = async ({ props, store }) => {
   store.set(state.scanner.selectedBatchIndex, props.selectedBatchIndex);
