2eef85b311a9f94c69266a6001b3effb03dc8c94
rosie highsmith <ahighsmith@flexion.us>
2021-05-18 08:08:17 -0700

    Revert "Merge branch 'staging' into latest-to-migration"
    This reverts commit e2f23acf0515e6693d6f3a4ddfde94ac6fea4b30, reversing
    changes made to c5b2ebf7a750034e9b1e540ea926984201c132fd.

--- wait-for-migration-to-finish.sh
+++ wait-for-migration-to-finish.sh
@@ -8,31 +8,14 @@
 # Requirements
 #   - aws must be installed on your machine
 
-get_total () {
-  response=$(aws sqs get-queue-attributes --attribute-names ApproximateNumberOfMessages ApproximateNumberOfMessagesNotVisible ApproximateNumberOfMessagesDelayed --queue-url="https://sqs.us-east-1.amazonaws.com/${AWS_ACCOUNT_ID}/migration_segments_queue_${ENV}" --region us-east-1 --query "Attributes" --output=text)
-    
-  local total=0
-  for count in $response
-    do
-      total=$(( $total + $count ))
-    done
-  echo "$total"
-}
-
 sleep 5
 while true
 do
-  
-  total=$(get_total)
+  response=$(aws sqs get-queue-attributes --attribute-names=ApproximateNumberOfMessages --queue-url="https://sqs.us-east-1.amazonaws.com/${AWS_ACCOUNT_ID}/migration_segments_queue_${ENV}" --region us-east-1 --query "Attributes.ApproximateNumberOfMessages" --output=text)
 
-  if [ "${total}" == 0 ]
+  if [ "${response}" == "0" ]
   then
-    sleep 120 # wait two minutes and re-check for consistency
-    recheck=$(get_total)
-    if [ "${recheck}" == 0 ]
-    then
-      break
-    fi
+    break
   fi
 
   sleep 5
