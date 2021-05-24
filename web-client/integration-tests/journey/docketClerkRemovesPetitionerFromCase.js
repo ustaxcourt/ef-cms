4639328cd832ade1a7467785f356927dfe245eb8
rosie highsmith <ahighsmith@flexion.us>
2021-05-24 10:23:18 -0700

    8135: working integration test for removing intervenor AND petitioner [skip ci]

--- web-client/integration-tests/journey/docketClerkRemovesPetitionerFromCase.js
+++ web-client/integration-tests/journey/docketClerkRemovesPetitionerFromCase.js
@@ -1,4 +1,8 @@
-import { CASE_STATUS_TYPES } from '../../../shared/src/business/entities/EntityConstants';
+import {
+  CASE_STATUS_TYPES,
+  CONTACT_TYPES,
+} from '../../../shared/src/business/entities/EntityConstants';
+import { contactPrimaryFromState, contactSecondaryFromState } from '../helpers';
 import { getPetitionerById } from '../../../shared/src/business/entities/cases/Case';
 
 export const docketClerkRemovesPetitionerFromCase = test => {
@@ -11,8 +15,12 @@ export const docketClerkRemovesPetitionerFromCase = test => {
       CASE_STATUS_TYPES.new,
     );
 
+    const contactPrimaryContactId = contactPrimaryFromState(test).contactId;
+    const originalSecondaryContactId = contactSecondaryFromState(test)
+      .contactId;
+
     await test.runSequence('gotoEditPetitionerInformationInternalSequence', {
-      contactId: test.intervenorContactId,
+      contactId: contactPrimaryContactId,
       docketNumber: test.docketNumber,
     });
 
@@ -23,8 +31,16 @@ export const docketClerkRemovesPetitionerFromCase = test => {
     await test.runSequence('openRemovePetitionerModalSequence');
     await test.runSequence('removePetitionerAndUpdateCaptionSequence');
 
+    const petitionerOnCase = getPetitionerById(
+      test.getState('caseDetail'),
+      originalSecondaryContactId,
+    ).contactType;
+
     expect(
-      getPetitionerById(test.getState('caseDetail'), test.intervenorContactId),
+      getPetitionerById(test.getState('caseDetail'), contactPrimaryContactId),
     ).toBeUndefined();
+
+    // primary contact should be replaced with the secondary contact
+    expect(petitionerOnCase).toBe(CONTACT_TYPES.primary);
   });
 };
