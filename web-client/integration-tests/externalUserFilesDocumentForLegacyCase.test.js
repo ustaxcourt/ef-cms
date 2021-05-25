d2bf8f828aa0d575655b07d22687c5761a78789d
TElliott <telliott@flexion.us>
2021-05-25 11:08:07 -0500

    8133: Add back integration test

--- web-client/integration-tests/externalUserFilesDocumentForLegacyCase.test.js
+++ web-client/integration-tests/externalUserFilesDocumentForLegacyCase.test.js
@@ -1,6 +1,7 @@
 import { associatedExternalUserViewsCaseDetailForOwnedCase } from './journey/associatedExternalUserViewsCaseDetailForOwnedCase.js';
 import { externalUserFilesDocumentForOwnedCase } from './journey/externalUserFilesDocumentForOwnedCase.js';
 import { fakeFile, loginAs, setupTest } from './helpers';
+import { getOtherFilers } from '../../shared/src/business/entities/cases/Case';
 
 const test = setupTest();
 
@@ -27,4 +28,21 @@ describe('an external user files a document for their legacy case', () => {
   loginAs(test, 'irsPractitioner@example.com');
   associatedExternalUserViewsCaseDetailForOwnedCase(test);
   externalUserFilesDocumentForOwnedCase(test, fakeFile);
+
+  loginAs(test, 'docketclerk@example.com');
+  it('verifies otherFiler parties receive paper service when serviceIndicator is set to paper', async () => {
+    await test.runSequence('gotoCaseDetailSequence', {
+      docketNumber: test.docketNumber,
+    });
+
+    const otherFilers = getOtherFilers(test.getState('caseDetail'));
+    const docketEntries = test.getState('caseDetail.docketEntries');
+    const lastServedDocument = docketEntries.pop();
+
+    const isOtherFilerServed = lastServedDocument.servedParties.find(
+      p => p.name === otherFilers[0].name && p.email === otherFilers[0].email,
+    );
+
+    expect(isOtherFilerServed).toBeTruthy();
+  });
 });
