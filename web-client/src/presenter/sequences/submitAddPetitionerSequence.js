416fb7f0cf9f64ba4edc2e34c3c8922c00472e74
Rachel Schneiderman <rschneiderman@flexion.us>
2021-05-25 12:10:35 -0400

    8134: Route to correct tab after removing petitioner

--- web-client/src/presenter/sequences/submitAddPetitionerSequence.js
+++ web-client/src/presenter/sequences/submitAddPetitionerSequence.js
@@ -3,6 +3,7 @@ import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { navigateToCaseDetailCaseInformationActionFactory } from '../actions/navigateToCaseDetailCaseInformationActionFactory';
 import { setAlertSuccessAction } from '../actions/setAlertSuccessAction';
 import { setCurrentPageAction } from '../actions/setCurrentPageAction';
+import { setPartyViewTabAfterRemoveAction } from '../actions/setPartyViewTabAfterRemoveAction';
 import { setSaveAlertsForNavigationAction } from '../actions/setSaveAlertsForNavigationAction';
 import { setValidationAlertErrorsAction } from '../actions/setValidationAlertErrorsAction';
 import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
@@ -18,13 +19,11 @@ export const submitAddPetitionerSequence = [
     error: [setValidationAlertErrorsAction, setValidationErrorsAction],
     success: showProgressSequenceDecorator([
       addPetitionerToCaseAction,
+      setPartyViewTabAfterRemoveAction,
       setSaveAlertsForNavigationAction,
       setAlertSuccessAction,
       setCurrentPageAction('Interstitial'),
-      navigateToCaseDetailCaseInformationActionFactory(
-        'parties',
-        'participantsAndCounsel',
-      ),
+      navigateToCaseDetailCaseInformationActionFactory('parties'),
     ]),
   },
 ];
