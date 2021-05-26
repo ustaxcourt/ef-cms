77ac7de2d109926d7c53fe4275da4f694cae36c6
Kris Koskelin <kkoskelin@flexion.us>
2021-05-26 10:48:00 -0500

    8325: revert removal of legacy migration documents

--- webpack.config.lambda.api.js
+++ webpack.config.lambda.api.js
@@ -10,6 +10,8 @@ module.exports = {
     'cognito-triggers':
       './web-api/terraform/template/lambdas/cognito-triggers.js',
     cron: './web-api/terraform/template/lambdas/cron.js',
+    'legacy-documents-migration':
+      './web-api/terraform/template/lambdas/legacy-documents-migration.js',
     streams: './web-api/terraform/template/lambdas/streams.js',
     websockets: './web-api/terraform/template/lambdas/websockets.js',
   },
