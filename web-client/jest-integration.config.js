037e9a24f05330b71b50b6a7e8ff13d2ca3bb416
Eric Sorenson <eric@sorenson.software>
2021-05-27 11:14:54 -0500

    run unit tests in GHA

--- web-client/jest-integration.config.js
+++ web-client/jest-integration.config.js
@@ -2,31 +2,9 @@ const baseConfig = require('../jest.config');
 
 module.exports = {
   ...baseConfig,
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.js',
-    '!integration-tests/**/*.js',
-    '!integration-tests-public/**/*.js',
-    '!src/applicationContext.js',
-    '!src/applicationContextPublic.js',
-    '!src/router.js',
-    '!src/routerPublic.js',
-    '!src/index.dev.js',
-    '!src/index.prod.js',
-    '!src/index-public.dev.js',
-    '!src/index-public.prod.js',
-  ],
-  coverageDirectory: './coverage-integration',
-  coverageThreshold: {
-    global: {
-      branches: 20,
-      functions: 20,
-      lines: 20,
-      statements: 20,
-    },
-  },
+  collectCoverage: false,
   globals: {
-    File: function () {},
+    File() {},
     FileReader: () => {},
     atob: x => x,
   },
