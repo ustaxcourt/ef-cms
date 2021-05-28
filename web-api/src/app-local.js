4ac365524931883e4a9fc27b50d99ac99a9b8ef5
Eric Sorenson <eric@sorenson.software>
2021-05-28 15:00:26 -0500

    babel is needed for local development

--- web-api/src/app-local.js
+++ web-api/src/app-local.js
@@ -1,5 +1,12 @@
+// babel is needed for local development, in prod this is done with webpack
+require('regenerator-runtime');
+require('@babel/register')({
+  extensions: ['.jsx'],
+  presets: ['@babel/preset-react', '@babel/preset-env'],
+});
+
 const { app } = require('./app');
 const port = 4000;
 
 app.listen(port);
-console.log(`listening on http://localhost:${port}`);
+console.log(`Listening on http://localhost:${port}`);
