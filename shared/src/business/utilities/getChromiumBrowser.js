c99459ea6b82d192d4419db810ecfaf1494ddc06
Eric Sorenson <eric@sorenson.software>
2021-05-28 16:20:11 -0500

    fix test

--- shared/src/business/utilities/getChromiumBrowser.js
+++ shared/src/business/utilities/getChromiumBrowser.js
@@ -1,6 +1,5 @@
-const chromium = require('chrome-aws-lambda');
-
 exports.getChromiumBrowser = async () => {
+  const chromium = require('chrome-aws-lambda');
   return await chromium.puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
