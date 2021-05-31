c853b8dcb4f838ebc2efaa313677255776016a6f
Rachel Schneiderman <rschneiderman@flexion.us>
2021-05-31 11:53:24 -0400

    7944: Set order title and event code when editing title

--- web-client/src/presenter/sequences/openEditOrderTitleModalSequence.js
+++ web-client/src/presenter/sequences/openEditOrderTitleModalSequence.js
@@ -1,5 +1,7 @@
+import { setEditOrderTitleDataOnModalAction } from '../actions/CourtIssuedOrder/setEditOrderTitleDataOnModalAction';
 import { setShowModalFactoryAction } from '../actions/setShowModalFactoryAction';
 
 export const openEditOrderTitleModalSequence = [
+  setEditOrderTitleDataOnModalAction,
   setShowModalFactoryAction('EditOrderTitleModal'),
 ];
