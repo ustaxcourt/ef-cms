ca8af1f764692f10290f08e6f2b5e448fc0e0d88
Kris Koskelin <kkoskelin@flexion.us>
2021-06-09 14:23:17 -0500

    8036: fix linting, test coverage

--- shared/src/business/utilities/pdfGenerator/documentTemplates/PractitionerCaseList.jsx
+++ shared/src/business/utilities/pdfGenerator/documentTemplates/PractitionerCaseList.jsx
@@ -1,5 +1,9 @@
 const React = require('react');
 
 export const PractitionerCaseList = ({ closedCases, openCases }) => {
-  return <div id="address-label-cover-sheet">TODO: FIX ME HELLO WORLD</div>;
+  return (
+    <div id="address-label-cover-sheet">
+      TODO: FIX ME HELLO WORLD {closedCases.length} {openCases.length}
+    </div>
+  );
 };
