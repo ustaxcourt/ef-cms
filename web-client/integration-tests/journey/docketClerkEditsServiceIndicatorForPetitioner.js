36d4f77705b080b4753222399d38592fbe764a00
TElliott <telliott@flexion.us>
2021-06-15 10:47:57 -0500

    8137: Removed paper/none-to-electronic validation for petitioners and updated integration

--- web-client/integration-tests/journey/docketClerkEditsServiceIndicatorForPetitioner.js
+++ web-client/integration-tests/journey/docketClerkEditsServiceIndicatorForPetitioner.js
@@ -1,10 +1,7 @@
 import { SERVICE_INDICATOR_TYPES } from '../../../shared/src/business/entities/EntityConstants';
 import { contactPrimaryFromState } from '../helpers';
 
-export const docketClerkEditsServiceIndicatorForPetitioner = (
-  test,
-  expectedServiceIndicator = null,
-) => {
+export const docketClerkEditsServiceIndicatorForPetitioner = test => {
   return it('docket clerk edits service indicator for a petitioner', async () => {
     let contactPrimary = contactPrimaryFromState(test);
 
@@ -14,7 +11,13 @@ export const docketClerkEditsServiceIndicatorForPetitioner = (
     });
 
     expect(test.getState('form.contact.serviceIndicator')).toEqual(
-      expectedServiceIndicator || SERVICE_INDICATOR_TYPES.SI_NONE,
+      SERVICE_INDICATOR_TYPES.SI_NONE,
+    );
+
+    const contactPrimaryResult = contactPrimaryFromState(test);
+
+    expect(contactPrimaryResult.serviceIndicator).toEqual(
+      SERVICE_INDICATOR_TYPES.SI_NONE,
     );
 
     await test.runSequence('updateFormValueSequence', {
@@ -22,32 +25,12 @@ export const docketClerkEditsServiceIndicatorForPetitioner = (
       value: SERVICE_INDICATOR_TYPES.SI_ELECTRONIC,
     });
 
-    if (!expectedServiceIndicator) {
-      await test.runSequence('submitEditPetitionerSequence');
-      expect(test.getState('validationErrors')).toMatchObject({
-        contact: {
-          serviceIndicator: expect.anything(),
-        },
-      });
-
-      const contactPrimaryResult = contactPrimaryFromState(test);
-
-      expect(contactPrimaryResult.serviceIndicator).toEqual(
-        SERVICE_INDICATOR_TYPES.SI_NONE,
-      );
-    }
-
-    await test.runSequence('updateFormValueSequence', {
-      key: 'contact.serviceIndicator',
-      value: SERVICE_INDICATOR_TYPES.SI_PAPER,
-    });
-
     await test.runSequence('submitEditPetitionerSequence');
 
     contactPrimary = contactPrimaryFromState(test);
 
     expect(contactPrimary.serviceIndicator).toEqual(
-      SERVICE_INDICATOR_TYPES.SI_PAPER,
+      SERVICE_INDICATOR_TYPES.SI_ELECTRONIC,
     );
   });
 };
