f61bf62dc7a759c16b52992d727328f45f6ea07e
rosie highsmith <ahighsmith@flexion.us>
2021-06-18 10:22:37 -0700

    devex-913: split out a utility method for uploadToS3

--- shared/src/business/useCases/serveCaseToIrs/serveCaseToIrsInteractor.js
+++ shared/src/business/useCases/serveCaseToIrs/serveCaseToIrsInteractor.js
@@ -278,28 +278,10 @@ exports.serveCaseToIrsInteractor = async (
   const caseConfirmationPdfName =
     caseEntityToUpdate.getCaseConfirmationGeneratedPdfFileName();
 
-  await new Promise((resolve, reject) => {
-    const documentsBucket = applicationContext.getDocumentsBucketName();
-    const s3Client = applicationContext.getStorageClient();
-
-    const params = {
-      Body: pdfData,
-      Bucket: documentsBucket,
-      ContentType: 'application/pdf',
-      Key: caseConfirmationPdfName,
-    };
-
-    s3Client.upload(params, function (err) {
-      if (err) {
-        applicationContext.logger.error(
-          'An error occurred while attempting to upload to S3',
-          err,
-        );
-        reject(err);
-      }
-
-      resolve();
-    });
+  await applicationContext.getUtilities().uploadToS3({
+    applicationContext,
+    caseConfirmationPdfName,
+    pdfData,
   });
 
   let urlToReturn;
