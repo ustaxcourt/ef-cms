ae77bfb815d50aeeea1d2cc1cdd149d243121b11
Joseph Sutton <jsutton@flexion.us>
2021-06-24 12:19:10 -0500

    8074: added conditional verbiage

--- web-client/src/presenter/actions/submitOtherStatisticsAction.js
+++ web-client/src/presenter/actions/submitOtherStatisticsAction.js
@@ -38,7 +38,9 @@ export const submitOtherStatisticsAction = async ({
   } catch (e) {
     return path.error({
       alertError: {
-        message: 'Statistic could not be added. Please try again.',
+        message: `Statistic could not be ${
+          isEditing ? 'edited' : 'added'
+        }. Please try again.`,
         title: 'Error',
       },
     });
