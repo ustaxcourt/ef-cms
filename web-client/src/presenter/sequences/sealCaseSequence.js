f65469db84d447ce9c5153c3c733511801e924f5
TElliott <telliott@flexion.us>
2021-06-24 14:29:33 -0500

    8074: Error handling for sealing case

--- web-client/src/presenter/sequences/sealCaseSequence.js
+++ web-client/src/presenter/sequences/sealCaseSequence.js
@@ -1,11 +1,15 @@
 import { clearModalAction } from '../actions/clearModalAction';
 import { sealCaseAction } from '../actions/CaseDetail/sealCaseAction';
+import { setAlertErrorAction } from '../actions/setAlertErrorAction';
 import { setAlertSuccessAction } from '../actions/setAlertSuccessAction';
 import { setCaseAction } from '../actions/setCaseAction';
 import { showProgressSequenceDecorator } from '../utilities/sequenceHelpers';
 
 export const sealCaseSequence = showProgressSequenceDecorator([
   sealCaseAction,
-  { error: [], success: [setAlertSuccessAction, setCaseAction] },
+  {
+    error: [setAlertErrorAction],
+    success: [setAlertSuccessAction, setCaseAction],
+  },
   clearModalAction,
 ]);
