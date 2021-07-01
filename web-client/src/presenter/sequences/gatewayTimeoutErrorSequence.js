4d122f78db98aa7bbdee37d3c221b43ccc6d4bbd
TElliott <telliott@flexion.us>
2021-07-01 13:06:39 -0500

    7368: Updated gatewayTimeoutErrorSequence with new action for search specific 504 errors

--- web-client/src/presenter/sequences/gatewayTimeoutErrorSequence.js
+++ web-client/src/presenter/sequences/gatewayTimeoutErrorSequence.js
@@ -1,9 +1,15 @@
 import { clearModalAction } from '../actions/clearModalAction';
+import { isSearchTimeoutErrorAction } from '../actions/isSearchTimeoutErrorAction';
 import { setAlertFromExceptionAction } from '../actions/setAlertFromExceptionAction';
+import { setSearchTimeoutAlertAction } from '../actions/setSearchTimeoutAlertAction';
 import { unsetWaitingForResponseOnErrorAction } from '../actions/unsetWaitingForResponseOnErrorAction';
 
 export const gatewayTimeoutErrorSequence = [
   unsetWaitingForResponseOnErrorAction,
-  setAlertFromExceptionAction,
+  isSearchTimeoutErrorAction,
+  {
+    no: [setAlertFromExceptionAction],
+    yes: [setSearchTimeoutAlertAction],
+  },
   clearModalAction,
 ];
