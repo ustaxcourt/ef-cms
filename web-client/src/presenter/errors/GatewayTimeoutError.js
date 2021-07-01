0371b90b1af5a350560d9ce5f51ed3bb19148990
TElliott <telliott@flexion.us>
2021-07-01 13:22:37 -0500

    7368: Updated generic 504 error message

--- web-client/src/presenter/errors/GatewayTimeoutError.js
+++ web-client/src/presenter/errors/GatewayTimeoutError.js
@@ -3,9 +3,9 @@ import { ActionError } from './ActionError';
 export class GatewayTimeoutError extends ActionError {
   // HTTP 504
   constructor() {
-    const message = 'The server took too long to process a response.';
+    const message = 'Try again.';
     super(message);
-    this.title = 'Gateway Timeout'; // TODO messaging here
+    this.title = 'The system is taking too long to respond';
     this.message = message;
   }
 }
