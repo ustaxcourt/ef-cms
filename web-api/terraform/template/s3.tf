d554e502a76f08267932445804cdd12e7d72b321
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-07-07 11:39:47 -0700

    6282: create sqs queue and s3 upload trigger

--- web-api/terraform/template/s3.tf
+++ web-api/terraform/template/s3.tf
@@ -179,6 +179,15 @@ resource "aws_s3_bucket" "quarantine_us_east_1" {
   }
 }
 
+resource "aws_s3_bucket_notification" "quarantine_bucket_notification" {
+  bucket = aws_s3_bucket.quarantine_us_east_1.id
+
+  queue {
+    queue_arn     = aws_sqs_queue.virus_scan_queue.arn
+    events        = ["s3:ObjectCreated:*"]
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "block_quarantine_east" {
   bucket = aws_s3_bucket.quarantine_us_east_1.id
 
@@ -225,3 +234,4 @@ resource "aws_s3_bucket_public_access_block" "block_quarantine_west" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
