9de17d7b0b31ea0abf2670e322b749440aaf38ee
rosie highsmith <ahighsmith@flexion.us>
2021-07-12 10:30:18 -0700

    8580-bug: get servedPartiesCode in getFormattedCaseDetail so it displays for internal and external users as well as public

--- shared/src/business/utilities/getFormattedCaseDetail.js
+++ shared/src/business/utilities/getFormattedCaseDetail.js
@@ -16,7 +16,7 @@ const {
 } = require('../entities/EntityConstants');
 const { Case } = require('../entities/cases/Case');
 const { cloneDeep, isEmpty, sortBy } = require('lodash');
-const { isServed } = require('../entities/DocketEntry');
+const { getServedPartiesCode, isServed } = require('../entities/DocketEntry');
 
 const TRANSCRIPT_AGE_DAYS_MIN = 90;
 const documentMeetsAgeRequirements = doc => {
@@ -124,6 +124,10 @@ const formatDocketEntry = (applicationContext, docketEntry) => {
 
   formattedEntry.qcNeeded =
     formattedEntry.qcWorkItemsUntouched && !formattedEntry.isInProgress;
+
+  formattedEntry.servedPartiesCode =
+    formattedEntry.servedPartiesCode ||
+    getServedPartiesCode(formattedEntry.servedParties);
 
   if (
     formattedEntry.isCourtIssuedDocument &&
