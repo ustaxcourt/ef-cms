db25d6ec84859770b99255752102f92b9c7be30e
Eric Sorenson <eric@sorenson.software>
2021-07-12 15:12:10 -0500

    maintenance: consolidate pug mocks

--- shared/src/business/utilities/documentGenerators/standingPretrialOrderForSmallCase.test.js
+++ shared/src/business/utilities/documentGenerators/standingPretrialOrderForSmallCase.test.js
@@ -40,10 +40,6 @@ describe('documentGenerators', () => {
         return sass;
       });
 
-      applicationContext.getPug.mockImplementation(() => {
-        return require('pug');
-      });
-
       applicationContext
         .getUseCases()
         .generatePdfFromHtmlInteractor.mockImplementation(
