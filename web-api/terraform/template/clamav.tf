ea92f3bc42b5aef02ed6bfb7266ad2c496424702
Joseph Sutton <jsutton@flexion.us>
2021-07-12 11:27:48 -0500

    6282: consolidate clamav fargate config for reference purposes

--- web-api/terraform/template/clamav.tf
+++ web-api/terraform/template/clamav.tf
@@ -89,3 +89,115 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
 #     monitor_script_s3_path = "${var.dns_domain}-monitor-script"
 #   }
 # }
+
+# Networking for Fargate
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "clamav_fargate_execution_${var.environment}"
+
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "clamav_fargate_task_${var.environment}"
+
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy_attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "s3_task" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_ecs_cluster" "cluster" {
+  name = "clamav_fargate_cluster_${var.environment}"
+
+  capacity_providers = ["FARGATE"]
+}
+
+resource "aws_ecs_task_definition" "definition" {
+  family                   = "clamav_fargate_task_${var.environment}"
+  task_role_arn            = var.ecs_task_role
+  execution_role_arn       = var.ecs_task_execution_role
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  requires_compatibilities = ["FARGATE"]
+
+  container_definitions = jsonencode(
+    [
+      {
+        "image" : "${data.aws_caller_identity.current.account_id}.dkr.ecr.us-east-1.amazonaws.com/clamav_spike:latest",
+        "command" : "bash ./run.sh",
+        "logConfiguration" : {
+          "logDriver" : "awslogs",
+          "options" : {
+            "awslogs-region" : "us-east-1",
+            "awslogs-group" : "/aws/ecs/clamav_fargate_${var.environment}",
+            "awslogs-stream-prefix" : "project"
+          }
+        },
+        "environment" : [
+          {
+            "name" : "SQS_QUEUE_URL",
+            "value" : "${aws_event_queue.clamav_event_queue.url}"
+          },
+          {
+            "name" : "QUARANTINE_BUCKET",
+            "value" : "${aws_s3_bucket.quarantine_bucket.bucket_domain_name}"
+          },
+          {
+            "name" : "CLEAN_BUCKET",
+            "value" : "${var.dns_domain}-documents-${var.environment}-us-east-1"
+          }
+        ]
+      }
+  ])
+
+  depends_on = [
+    aws_iam_role.ecs_task_role,
+    aws_iam_role.ecs_task_execution_role,
+    aws_s3_bucket.quarantine_bucket,
+    aws_event_queue.clamav_event_queue
+  ]
+}
