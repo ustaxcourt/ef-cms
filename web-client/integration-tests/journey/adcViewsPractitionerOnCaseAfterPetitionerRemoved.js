0656c1ea8a172160a30c8c0d46fa8daa1e3bacb6
Kris Koskelin <kkoskelin@flexion.us>
2021-07-12 11:53:15 -0500

    removing unneeded coverage config lines
    fixing 'test' identifier

--- web-client/integration-tests/journey/adcViewsPractitionerOnCaseAfterPetitionerRemoved.js
+++ web-client/integration-tests/journey/adcViewsPractitionerOnCaseAfterPetitionerRemoved.js
@@ -3,33 +3,37 @@ import { runCompute } from 'cerebral/test';
 import { viewCounselHelper } from '../../src/presenter/computeds/viewCounselHelper';
 import { withAppContextDecorator } from '../../src/withAppContext';
 
-export const adcViewsPractitionerOnCaseAfterPetitionerRemoved = test => {
-  return it('adc views practitioner modal on case after petitioner has been removed during petition QC', async () => {
-    await test.runSequence('gotoCaseDetailSequence', {
-      docketNumber: test.docketNumber,
+export const adcViewsPractitionerOnCaseAfterPetitionerRemoved =
+  cerebralTest => {
+    return it('adc views practitioner modal on case after petitioner has been removed during petition QC', async () => {
+      await cerebralTest.runSequence('gotoCaseDetailSequence', {
+        docketNumber: cerebralTest.docketNumber,
+      });
+
+      expect(cerebralTest.getState('currentPage')).toEqual(
+        'CaseDetailInternal',
+      );
+
+      const partiesInformationHelperComputed = runCompute(
+        withAppContextDecorator(partiesInformationHelper),
+        {
+          state: cerebralTest.getState(),
+        },
+      );
+
+      const petitioner =
+        partiesInformationHelperComputed.formattedPetitioners[0];
+
+      await cerebralTest.runSequence('showViewPetitionerCounselModalSequence', {
+        privatePractitioner: petitioner.representingPractitioners[0],
+      });
+
+      const viewCounselHelperComputed = runCompute(viewCounselHelper, {
+        state: cerebralTest.getState(),
+      });
+
+      expect(viewCounselHelperComputed.representingNames).toEqual([
+        'William Wonka',
+      ]);
     });
-
-    expect(test.getState('currentPage')).toEqual('CaseDetailInternal');
-
-    const partiesInformationHelperComputed = runCompute(
-      withAppContextDecorator(partiesInformationHelper),
-      {
-        state: test.getState(),
-      },
-    );
-
-    const petitioner = partiesInformationHelperComputed.formattedPetitioners[0];
-
-    await test.runSequence('showViewPetitionerCounselModalSequence', {
-      privatePractitioner: petitioner.representingPractitioners[0],
-    });
-
-    const viewCounselHelperComputed = runCompute(viewCounselHelper, {
-      state: test.getState(),
-    });
-
-    expect(viewCounselHelperComputed.representingNames).toEqual([
-      'William Wonka',
-    ]);
-  });
-};
+  };
