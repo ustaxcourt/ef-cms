e922020cc4ce2e78da2003c4795142815938d313
Joseph Sutton <jsutton@flexion.us>
2021-07-12 15:49:09 -0500

    6282: removed unnecessary config, removed container_definition.command

--- web-api/terraform/bin/deploy-app.sh
+++ web-api/terraform/bin/deploy-app.sh
@@ -114,4 +114,4 @@ export TF_VAR_cognito_table_name=$COGNITO_TRIGGER_TABLE_NAME
 
 terraform init -backend=true -backend-config=bucket="${BUCKET}" -backend-config=key="${KEY}" -backend-config=dynamodb_table="${LOCK_TABLE}" -backend-config=region="${REGION}"
 terraform plan -out execution-plan
-terraform apply -auto-approve execution-plan
+# terraform apply -auto-approve execution-plan
