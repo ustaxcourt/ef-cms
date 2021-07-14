10c67ba9030f903aaae30f772443178eb75f2cab
Joseph Sutton <jsutton@flexion.us>
2021-07-14 09:32:38 -0500

    6282: see if we can get away with things we possibly don't need in the network config

--- web-api/terraform/main/clamav.tf
+++ web-api/terraform/main/clamav.tf
@@ -1,27 +1,5 @@
 
-# Flow: S3 -> Event Notification -> SQS -> Fargate & Scan -> tag
-
-
-# resource "aws_s3_bucket" "clamav_s3_download" {
-#   bucket = "${var.dns_domain}-monitor-script"
-#   acl    = "private"
-# }
-
-# resource "aws_s3_bucket_object" "clamav_worker_object" {
-#   bucket = aws_s3_bucket.clamav_s3_download.id
-#   key    = "worker.js"
-#   source = "worker.js"
-#   etag   = filemd5("worker.js")
-# }
-
-# resource "aws_s3_bucket_public_access_block" "clamav" {
-#   bucket                  = aws_s3_bucket.clamav_s3_download.id
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
-
+# Flow: S3 -> Event Notification -> SQS -> Fargate Task: Consume SQS & Scan -> tag
 
 
 data "aws_caller_identity" "current" {}
@@ -61,41 +39,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   }
 }
 
-# ECS Fargate: see fargate.tf
-
-# EC2
-# resource "aws_instance" "clamav_worker" {
-#   ami           = "ami-0a313d6098716f372"
-#   instance_type = "t2.large"
-
-#   availability_zone = "us-east-1a"
-
-#   tags = {
-#     Name        = "clamav-${var.environment}"
-#     environment = var.environment
-#   }
-
-#   user_data = data.template_file.setup_clamav.rendered
-
-#   iam_instance_profile = "clamav_s3_download_instance_profile_${var.environment}"
-
-#   key_name = "cody for clamav"
-# }
-
-# data "template_file" "setup_clamav" {
-#   template = file("setup_clamav_worker.sh")
-
-#   vars = {
-#     documents_bucket_name  = aws_s3_bucket.documents_us_east_1.id
-#     sqs_queue_url          = aws_sqs_queue.clamav_event_queue.id
-#     quarantine_bucket      = aws_s3_bucket.quarantine_bucket.id
-#     environment            = var.environment
-#     monitor_script_s3_path = "${var.dns_domain}-monitor-script"
-#   }
-# }
-
 # Networking for Fargate
-# network.tf
 resource "aws_vpc" "clamav_vpc" {
   cidr_block = "10.0.0.0/16"
 }
@@ -113,21 +57,6 @@ resource "aws_route_table" "private" {
 resource "aws_route_table_association" "private_subnet" {
   subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.private.id
-}
-
-resource "aws_eip" "nat" {
-  vpc = true
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.clamav_vpc.id
-}
-
-
-resource "aws_route" "private_ngw" {
-  route_table_id         = aws_route_table.private.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.ngw.id
 }
 
 resource "aws_security_group" "egress-all" {
