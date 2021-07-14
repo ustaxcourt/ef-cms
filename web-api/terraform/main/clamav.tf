7ecef1b5485acae37e599317bc991cc1b1c836a4
Joseph Sutton <jsutton@flexion.us>
2021-07-13 19:09:48 -0500

    6282: adjusted memory on fargate task definition, added cloudwatch log group

--- web-api/terraform/main/clamav.tf
+++ web-api/terraform/main/clamav.tf
@@ -176,7 +176,7 @@ resource "aws_ecs_task_definition" "definition" {
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   network_mode             = "awsvpc"
   cpu                      = "256"
-  memory                   = "512"
+  memory                   = "1024"
   requires_compatibilities = ["FARGATE"]
 
 
