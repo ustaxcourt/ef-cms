1be8157b8f7b67f4ac7cfdb995183cc3e5af571f
Joseph Sutton <jsutton@flexion.us>
2021-07-14 10:00:29 -0500

    6282: put the Fargate task behind a NAT gateway

--- web-api/terraform/main/clamav.tf
+++ web-api/terraform/main/clamav.tf
@@ -49,6 +49,11 @@ resource "aws_subnet" "private" {
   cidr_block = "10.0.2.0/24"
 }
 
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.clamav_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.clamav_vpc.id
 }
@@ -57,6 +62,27 @@ resource "aws_route_table" "private" {
 resource "aws_route_table_association" "private_subnet" {
   subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route" "private_ngw" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.ngw.id
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.clamav_vpc.id
+}
+
+resource "aws_nat_gateway" "ngw" {
+  subnet_id     = aws_subnet.public.id
+  allocation_id = aws_eip.nat.id
+
+  depends_on = [aws_internet_gateway.igw]
 }
 
 resource "aws_security_group" "egress-all" {
