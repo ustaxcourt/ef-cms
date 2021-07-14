4fffce9f633b43111df149ae72174053181f7835
Joseph Sutton <jsutton@flexion.us>
2021-07-14 11:55:51 -0500

    6282: update run.sh script for Dockerfile

--- web-api/terraform/main/run.sh
+++ web-api/terraform/main/run.sh
@@ -6,4 +6,6 @@ service clamav-daemon start
 
 echo "Services started. Running worker."
 
-AWS_REGION='us-east-1' CLEAN_DOCUMENTS_BUCKET=${documents_bucket_name} ENV=${environment} SQS_QUEUE_URL=${sqs_queue_url} QUARANTINE_BUCKET=${quarantine_bucket} node worker.js
+printenv
+
+node worker.js
