48c0e4b86a53aa9cfafc425bdca5548d54dbecf3
Joseph Sutton <jsutton@flexion.us>
2021-07-15 12:17:13 -0500

    6282: get clamav docker script working

--- clamav-docker-to-ecr.sh
+++ clamav-docker-to-ecr.sh
@@ -14,9 +14,8 @@ AWS_ACCOUNT_ID=${AWS_ACCOUNT_ID:-$(aws sts get-caller-identity --query "Account"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
 
 # create the repository if it doesn't exist
-set -e
-aws ecr create-repository --repository-name clamav --region us-east-1 || :
+aws ecr create-repository --repository-name clamav --region us-east-1 2>/dev/null || :
 
-docker build --no-cache -t "clamav:$ENVIRONMENT" -f ./web-api/terraform/main/Dockerfile ./web-api/terraform/main/
-docker tag "clamav:$ENVIRONMENT" "$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/clamav:$ENVIRONMENT"
-docker push "$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/clamav:$ENVIRONMENT"
+docker build --no-cache -t "clamav:${ENVIRONMENT}" -f ./web-api/terraform/main/Dockerfile ./web-api/terraform/main/
+docker tag "clamav:${ENVIRONMENT}" "${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/clamav:${ENVIRONMENT}"
+docker push "${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/clamav:${ENVIRONMENT}"
