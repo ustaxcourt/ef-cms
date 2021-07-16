92c615596448eb260fc2457f52cde70bfd455da6
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-07-16 08:13:16 -0700

    6282: working on interactor [skip ci]

--- web-api/terraform/main/worker.js
+++ web-api/terraform/main/worker.js
@@ -1,8 +1,48 @@
-const main = () => {
+const createApplicationContext = require('../../../src/applicationContext');
+const applicationContext = createApplicationContext({});
+
+const getMessages = async ({ applicationContextWhat }) => {
+  return await applicationContext
+    .getMessagingClient()
+    .getMessages({ applicationContext: applicationContextWhat });
+};
+
+const getScanCompleteCallback = async ({ applicationContextWhat, message }) => {
+  return await applicationContextWhat
+    .getMessagingClient()
+    .deleteMessage({
+      QueueUrl: applicationContextWhat.environment.virusScanQueueUrl,
+      ReceiptHandle: message.ReceiptHandle,
+    })
+    .promise();
+};
+
+const scanMessages = async ({ applicationContextWhat, messages }) => {
+  const scanCalls = [];
+  messages.map(message => {
+    scanCalls.push(
+      applicationContextWhat
+        .getUseCases()
+        .virusScanPdfInteractor(applicationContextWhat, {
+          key: message.s3.object.key,
+          scanCompleteCallback: getScanCompleteCallback({
+            applicationContextWhat,
+            message,
+          }),
+        }),
+    );
+  });
+};
+
+const main = async () => {
   // TODO: use the applicationContext to call a use case to scan the PDF
   setTimeout(function () {
     console.log('hello world');
   }, 10 * 1000);
+
+  const messages = await getMessages({
+    applicationContextWhat: applicationContext,
+  });
 };
 
 main();
