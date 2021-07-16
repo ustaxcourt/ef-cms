2264e8d61f5acae38c5e4d8a7aa55988ffc9e1ac
Eric Sorenson <eric@sorenson.software>
2021-07-16 11:54:52 -0500

    6282: lint fix, use CJS

--- shared/src/persistence/sqs/deleteMessage.js
+++ shared/src/persistence/sqs/deleteMessage.js
@@ -1,4 +1,4 @@
-export const deleteMessage = ({ applicationContext, message, queueUrl }) =>
+exports.deleteMessage = ({ applicationContext, message, queueUrl }) =>
   applicationContext
     .getMessagingClient()
     .deleteMessage({
--- shared/src/persistence/sqs/getDocumentIdFromSQSMessage.js
+++ shared/src/persistence/sqs/getDocumentIdFromSQSMessage.js
@@ -1,4 +1,4 @@
-export const getDocumentIdFromSQSMessage = message => {
+exports.getDocumentIdFromSQSMessage = message => {
   const { Body: body } = message;
   const parsedBody = JSON.parse(body);
   const documentId = parsedBody.Records[0].s3.object.key;
