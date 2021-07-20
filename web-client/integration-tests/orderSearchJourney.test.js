224d5f85ea32de9ac004ee22759acf4551374b30
Matt Hopson <matthopson@gmail.com>
2021-07-20 11:55:06 -0600

    8546 - More integration tests [skip-ci]

--- web-client/integration-tests/orderSearchJourney.test.js
+++ web-client/integration-tests/orderSearchJourney.test.js
@@ -20,7 +20,7 @@ describe('order search journey', () => {
   });
 
   loginAs(cerebralTest, 'petitionsclerk@example.com');
-  it('searches for an order by keyword', async () => {
+  it('searches for an order by keyword `welcome to flavortown`', async () => {
     await cerebralTest.runSequence('gotoAdvancedSearchSequence');
     cerebralTest.setState('advancedSearchTab', ADVANCED_SEARCH_TABS.ORDER);
 
@@ -36,26 +36,138 @@ describe('order search journey', () => {
       `searchResults.${ADVANCED_SEARCH_TABS.ORDER}`,
     );
 
-    expect(searchResults).toMatchObject([
-      {
+    expect(searchResults).toEqual([
+      expect.objectContaining({
         docketNumber: '313-21',
         documentTitle: 'welcome to flavortown',
-      },
-      {
+      }),
+      expect.objectContaining({
         docketNumber: '313-21',
         documentContents: 'welcome to flavortown',
-      },
-      {
+      }),
+      expect.objectContaining({
         docketNumber: '313-21',
         documentContents: 'welcome to flavortown',
         documentTitle: 'welcome to flavortown',
-      },
-      {
+      }),
+      expect.objectContaining({
         docketNumber: '313-21',
         documentContents: 'welcome to flavortown.',
-      },
+      }),
     ]);
 
     expect(searchResults.length).toEqual(4);
+  });
+
+  it('searches for an order by keyword `welcome from flavortown`', async () => {
+    await cerebralTest.runSequence('gotoAdvancedSearchSequence');
+    cerebralTest.setState('advancedSearchTab', ADVANCED_SEARCH_TABS.ORDER);
+
+    cerebralTest.setState('advancedSearchForm', {
+      orderSearch: {
+        keyword: 'welcome from flavortown',
+      },
+    });
+
+    await cerebralTest.runSequence('submitOrderAdvancedSearchSequence');
+
+    const searchResults = cerebralTest.getState(
+      `searchResults.${ADVANCED_SEARCH_TABS.ORDER}`,
+    );
+
+    expect(searchResults).toEqual([
+      expect.objectContaining({
+        docketNumber: '313-21',
+        documentTitle: 'welcome from flavortown',
+      }),
+    ]);
+
+    expect(searchResults.length).toEqual(1);
+  });
+
+  it('searches for an order by keyword `welcome to flavor-town`', async () => {
+    await cerebralTest.runSequence('gotoAdvancedSearchSequence');
+    cerebralTest.setState('advancedSearchTab', ADVANCED_SEARCH_TABS.ORDER);
+
+    cerebralTest.setState('advancedSearchForm', {
+      orderSearch: {
+        keyword: 'welcome to flavor-town',
+      },
+    });
+
+    await cerebralTest.runSequence('submitOrderAdvancedSearchSequence');
+
+    const searchResults = cerebralTest.getState(
+      `searchResults.${ADVANCED_SEARCH_TABS.ORDER}`,
+    );
+
+    expect(searchResults).toEqual([
+      expect.objectContaining({
+        docketNumber: '313-21',
+        documentTitle: 'welcome to flavor-town',
+      }),
+    ]);
+
+    expect(searchResults.length).toEqual(1);
+  });
+
+  it('searches for an order by keyword `Welcome to Flavortown`', async () => {
+    await cerebralTest.runSequence('gotoAdvancedSearchSequence');
+    cerebralTest.setState('advancedSearchTab', ADVANCED_SEARCH_TABS.ORDER);
+
+    cerebralTest.setState('advancedSearchForm', {
+      orderSearch: {
+        keyword: 'Welcome to Flavortown',
+      },
+    });
+
+    await cerebralTest.runSequence('submitOrderAdvancedSearchSequence');
+
+    const searchResults = cerebralTest.getState(
+      `searchResults.${ADVANCED_SEARCH_TABS.ORDER}`,
+    );
+
+    expect(searchResults).toEqual([
+      expect.objectContaining({
+        docketNumber: '313-21',
+        documentTitle: 'welcome to flavortown',
+      }),
+      expect.objectContaining({
+        docketNumber: '313-21',
+        documentContents: 'welcome to flavortown',
+      }),
+      expect.objectContaining({
+        docketNumber: '313-21',
+        documentContents: 'welcome to flavortown',
+        documentTitle: 'welcome to flavortown',
+      }),
+      expect.objectContaining({
+        docketNumber: '313-21',
+        documentContents: 'welcome to flavortown.',
+      }),
+    ]);
+
+    expect(searchResults.length).toEqual(4);
+  });
+
+  it('searches for an order by keyword `welcomes to flavortown`', async () => {
+    await cerebralTest.runSequence('gotoAdvancedSearchSequence');
+    cerebralTest.setState('advancedSearchTab', ADVANCED_SEARCH_TABS.ORDER);
+
+    cerebralTest.setState('advancedSearchForm', {
+      orderSearch: {
+        keyword: 'welcomes to flavortown',
+      },
+    });
+
+    await cerebralTest.runSequence('submitOrderAdvancedSearchSequence');
+
+    const searchResults = cerebralTest.getState(
+      `searchResults.${ADVANCED_SEARCH_TABS.ORDER}`,
+    );
+
+    expect(searchResults).toEqual([]);
+
+    expect(searchResults.length).toEqual(0);
   });
 });
