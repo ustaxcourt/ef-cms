134b02a4c0b4e24ed1975a2a46c2c473bf18e8a6
Cody Seibert <cseibert@flexion.us>
2021-07-26 11:12:31 -0400

    8506 - verify tables and clusters removed

--- setup-for-blue-green-migration.sh
+++ setup-for-blue-green-migration.sh
@@ -13,6 +13,22 @@ if [[ "${SKIP_MIGRATION}" == "1" ]]; then
   exit 0
 fi
 
+echo "setting migrate flag to true"
+aws dynamodb put-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --item '{"pk":{"S":"migrate"},"sk":{"S":"migrate"},"current":{"S":"true"}}'
+
+SOURCE_TABLE_VERSION=$(aws dynamodb get-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --key '{"pk":{"S":"source-table-version"},"sk":{"S":"source-table-version"}}' | jq -r ".Item.current.S")
+echo "source table is currently ${SOURCE_TABLE_VERSION}"
+
+if [[ "${SOURCE_TABLE_VERSION}" == "beta" ]]; then
+  echo "setting destination table to alpha"
+  NEXT_VERSION="alpha"
+  aws dynamodb put-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --item '{"pk":{"S":"destination-table-version"},"sk":{"S":"destination-table-version"},"current":{"S":"alpha"}}'
+else
+  echo "setting destination table to beta"
+  NEXT_VERSION="beta"
+  aws dynamodb put-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --item '{"pk":{"S":"destination-table-version"},"sk":{"S":"destination-table-version"},"current":{"S":"beta"}}'
+fi
+
 aws dynamodb describe-table --table-name "efcms-${ENV}-${NEXT_VERSION}" --region us-east-1
 CODE=$?
 if [[ "${CODE}" != "0" ]]; then
@@ -32,20 +48,4 @@ CODE=$?
 if [[ "${CODE}" != "0" ]]; then
   echo "error: expected the efcms-search-${ENV}-${NEXT_VERSION} elasticsearch cluster to have been deleted from us-east-1 before running migration"
   exit 1
-fi
-
-echo "setting migrate flag to true"
-aws dynamodb put-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --item '{"pk":{"S":"migrate"},"sk":{"S":"migrate"},"current":{"S":"true"}}'
-
-SOURCE_TABLE_VERSION=$(aws dynamodb get-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --key '{"pk":{"S":"source-table-version"},"sk":{"S":"source-table-version"}}' | jq -r ".Item.current.S")
-echo "source table is currently ${SOURCE_TABLE_VERSION}"
-
-if [[ "${SOURCE_TABLE_VERSION}" == "beta" ]]; then
-  echo "setting destination table to alpha"
-  NEXT_VERSION="alpha"
-  aws dynamodb put-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --item '{"pk":{"S":"destination-table-version"},"sk":{"S":"destination-table-version"},"current":{"S":"alpha"}}'
-else
-  echo "setting destination table to beta"
-  NEXT_VERSION="beta"
-  aws dynamodb put-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --item '{"pk":{"S":"destination-table-version"},"sk":{"S":"destination-table-version"},"current":{"S":"beta"}}'
 fi
