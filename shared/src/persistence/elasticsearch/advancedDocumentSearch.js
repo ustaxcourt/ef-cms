6285292f3d2f68dd2e93b8be34763f94484481a9
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-08-03 12:15:12 -0700

    8650: cleanup

--- shared/src/persistence/elasticsearch/advancedDocumentSearch.js
+++ shared/src/persistence/elasticsearch/advancedDocumentSearch.js
@@ -83,7 +83,6 @@ exports.advancedDocumentSearch = async ({
   };
 
   if (docketNumber) {
-    console.log('docketNumber', docketNumber);
     caseQueryParams.has_parent.query.bool.must = {
       term: { 'docketNumber.S': docketNumber },
     };
