74859ac1f1be7fe485233c4884c69255c8e1dca7
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-08-05 11:10:35 -0700

    put warning back

--- .eslintrc.js
+++ .eslintrc.js
@@ -182,7 +182,7 @@ module.exports = {
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'warn',
     'require-atomic-updates': 'off',
-    'require-await': 'off',
+    'require-await': 'warn',
     'security/detect-child-process': 'off',
     'security/detect-non-literal-fs-filename': 'off',
     'security/detect-object-injection': 'off',
