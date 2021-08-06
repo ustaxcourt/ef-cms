a92fd4c57ba95cce3c0a76e96a367d0ce74b4839
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2021-08-06 16:37:42 +0530

    devex 971 updating implementation of refactored method

--- shared/src/business/useCases/createCaseFromPaperInteractor.test.js
+++ shared/src/business/useCases/createCaseFromPaperInteractor.test.js
@@ -178,6 +178,62 @@ describe('createCaseFromPaperInteractor', () => {
     expect(stinDocketEntry.index).toEqual(0);
   });
 
+  it('adds an RQT docket entry to the case when an RQT file is attached', async () => {
+    const caseFromPaper = await createCaseFromPaperInteractor(
+      applicationContext,
+      {
+        archivedDocketEntries: [],
+        ownershipDisclosureFileId: '413f62ce-7c8d-446e-aeda-14a2a625a611',
+        petitionFileId: '413f62ce-d7c8-446e-aeda-14a2a625a626',
+        petitionMetadata: {
+          caseCaption: 'caseCaption',
+          caseType: CASE_TYPES_MAP.other,
+          contactSecondary: {},
+          filingType: 'Myself',
+          hasIrsNotice: true,
+          irsNoticeDate: DATE,
+          mailingDate: 'testing',
+          partyType: PARTY_TYPES.petitioner,
+          petitionFile: new File([], 'petitionFile.pdf'),
+          petitionFileSize: 1,
+          petitionPaymentStatus: PAYMENT_STATUS.UNPAID,
+          petitioners: [
+            {
+              address1: '99 South Oak Lane',
+              address2: 'Culpa numquam saepe ',
+              address3: 'Eaque voluptates com',
+              city: 'Dignissimos voluptat',
+              contactType: CONTACT_TYPES.primary,
+              countryType: COUNTRY_TYPES.DOMESTIC,
+              email: 'petitioner1@example.com',
+              name: 'Diana Prince',
+              phone: '+1 (215) 128-6587',
+              postalCode: '69580',
+              state: 'AR',
+            },
+          ],
+          preferredTrialCity: 'Fresno, California',
+          procedureType: 'Small',
+          receivedAt: applicationContext.getUtilities().createISODateString(),
+          requestForPlaceOfTrialFile: new File(
+            [],
+            'requestForPlaceOfTrialFile.pdf',
+          ),
+          requestForPlaceOfTrialFileSize: 1,
+          stinFile: new File([], 'stinFile.pdf'),
+          stinFileSize: 1,
+        },
+        requestForPlaceOfTrialFileId: '413f62ce-7c8d-446e-aeda-14a2a625a611',
+        stinFileId: '413f62ce-7c8d-446e-aeda-14a2a625a611',
+      },
+    );
+
+    const stinDocketEntry = caseFromPaper.docketEntries.find(
+      d => d.eventCode === INITIAL_DOCUMENT_TYPES.stin.eventCode,
+    );
+    expect(stinDocketEntry.index).toEqual(0);
+  });
+
   it('creates a case from paper with a secondary contact', async () => {
     const caseFromPaper = await createCaseFromPaperInteractor(
       applicationContext,
