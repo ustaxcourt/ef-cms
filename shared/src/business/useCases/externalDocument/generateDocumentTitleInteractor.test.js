1d4418e4e07b8b6d42a62f48aedd7431510f8b20
Cody Seibert <cseibert@flexion.us>
2021-08-12 16:42:15 -0400

    8685 - clone deep the metadata to prevent objects title from changing due to reference

--- shared/src/business/useCases/externalDocument/generateDocumentTitleInteractor.test.js
+++ shared/src/business/useCases/externalDocument/generateDocumentTitleInteractor.test.js
@@ -38,4 +38,31 @@ describe('generateDocumentTitleInteractor', () => {
     ).toHaveBeenCalled();
     expect(title).toBe('abc Title Cool');
   });
+
+  it('should not overwrite the origrinal metadata previousDocument title passed as an argument', async () => {
+    const metadata = {
+      documentMetadata: {
+        documentTitle: 'abc [pizza]',
+        previousDocument: {
+          addToCoversheet: true,
+          additionalInfo: 'Cool',
+          documentTitle: 'Title',
+        },
+        scenario: 'nonstandard a',
+      },
+    };
+
+    const title = await generateDocumentTitleInteractor(
+      applicationContext,
+      metadata,
+    );
+
+    expect(
+      applicationContext.getUtilities().getDocumentTitleWithAdditionalInfo,
+    ).toHaveBeenCalled();
+    expect(title).toBe('abc Title Cool');
+    expect(metadata.documentMetadata.previousDocument.documentTitle).toBe(
+      'Title',
+    );
+  });
 });
