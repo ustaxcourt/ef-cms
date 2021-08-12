0193b8cbb619c3f575d088ffef1bca49d157254b
Eric Sorenson <eric@sorenson.software>
2021-08-12 14:03:38 -0500

    boost pa11y concurrency

--- web-client/pa11y/pa11y-ci.base-config.js
+++ web-client/pa11y/pa11y-ci.base-config.js
@@ -16,7 +16,7 @@ const defaults = {
   chromeLaunchConfig: {
     args: ['--no-sandbox'],
   },
-  concurrency: 8,
+  concurrency: 10,
   debug: true,
   'include-notices': true,
   'include-warnings': true,
