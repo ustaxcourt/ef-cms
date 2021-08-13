7d8a25e4166297cf30cfe06cbc6b1161ee5f0a0c
Cody Seibert <cseibert@flexion.us>
2021-08-13 11:22:34 -0400

    8508 - check the dlq for any messages and fail deploy

--- wait-for-migration-to-finish.sh
+++ wait-for-migration-to-finish.sh
@@ -19,15 +19,31 @@ get_total () {
   echo "$total"
 }
 
+fail_on_dlq () {
+  response=$(aws sqs get-queue-attributes --attribute-names ApproximateNumberOfMessages ApproximateNumberOfMessagesNotVisible ApproximateNumberOfMessagesDelayed --queue-url="https://sqs.us-east-1.amazonaws.com/${AWS_ACCOUNT_ID}/migration_segments_dl_queue_${ENV}" --region us-east-1 --query "Attributes" --output=text)
+  local total=0
+  for count in $response
+    do
+      total=$(( $total + $count ))
+    done
+  echo "$total"
+
+  if [[ "$total" != "0" ]]; then
+    echo "There are messages in the migration_segments_dl_queue_${ENV} queue"
+    exit 1
+  fi
+}
+
 sleep 5
 while true
 do
-  
+  fail_on_dlq
   total=$(get_total)
 
   if [ "${total}" == 0 ]
   then
     sleep 120 # wait two minutes and re-check for consistency
+    fail_on_dlq
     recheck=$(get_total)
     if [ "${recheck}" == 0 ]
     then
