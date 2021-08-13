08723d429081c92f9bd12e30333207e35af2ca47
rosie highsmith <ahighsmith@flexion.us>
2021-08-13 09:09:11 -0700

    8682: finished resetting start & endDate on changing dateRange [skip ci]

--- web-client/src/presenter/actions/AdvancedSearch/resetAdvancedDocumentSearchDatesAction.test.js
+++ web-client/src/presenter/actions/AdvancedSearch/resetAdvancedDocumentSearchDatesAction.test.js
@@ -2,4 +2,24 @@ import { presenter } from '../../presenter-mock';
 import { resetAdvancedDocumentSearchDatesAction } from './resetAdvancedDocumentSearchDatesAction';
 import { runAction } from 'cerebral/test';
 
-describe('resetAdvancedDocumentSearchDatesAction', () => {});
+describe('resetAdvancedDocumentSearchDatesAction', () => {
+  it('should unset startDate and endDate when the form dateRange is allDates', async () => {
+    const result = await runAction(resetAdvancedDocumentSearchDatesAction, {
+      modules: { presenter },
+      state: {
+        advancedSearchForm: {
+          orderSearch: {
+            dateRange: 'allDates',
+            endDate: '01/11/2001',
+            startDate: '01/01/2001',
+          },
+        },
+      },
+    });
+
+    expect(
+      result.state.advancedSearchForm.orderSearch.startDate,
+    ).toBeUndefined();
+    expect(result.state.advancedSearchForm.orderSearch.endDate).toBeUndefined();
+  });
+});
