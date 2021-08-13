2a3e35733e6b220307cb6f29e695ee664500167e
Cody Seibert <cseibert@flexion.us>
2021-08-13 12:53:41 -0400

    8508 - fail if error rate is greater than 50 percent

--- wait-for-migration-to-finish.sh
+++ wait-for-migration-to-finish.sh
@@ -26,7 +26,6 @@ fail_on_dlq () {
     do
       total=$(( $total + $count ))
     done
-  echo "$total"
 
   if [[ "$total" != "0" ]]; then
     echo "There are messages in the migration_segments_dl_queue_${ENV} queue"
@@ -34,16 +33,52 @@ fail_on_dlq () {
   fi
 }
 
+fail_on_segments_error_count () {
+  # check the past 5 minutes of cloudwatch metrics for invocation and error count and calculate a percentage
+  # anything greater than 50% should probably be considered a failed migration
+  start=$(node -e 'var d = new Date(); d.setTime(d.getTime() - (60 * 5000)); console.log(d.toISOString())')
+  end=$(node -e 'var d = new Date(); d.setTime(d.getTime()); console.log(d.toISOString())')
+  errorResponse=$(aws cloudwatch get-metric-statistics --metric-name Errors --namespace AWS/Lambda --statistics Sum --start-time $start --end-time $end --period 60 --dimensions Name=FunctionName,Value=migration_segments_lambda_${ENV} --query "Datapoints[].Sum" --output=text)
+  invocationsResponse=$(aws cloudwatch get-metric-statistics --metric-name Invocations --namespace AWS/Lambda --statistics Sum --start-time $start --end-time $end --period 60 --dimensions Name=FunctionName,Value=migration_segments_lambda_${ENV} --query "Datapoints[].Sum" --output=text)
+
+  local errorTotal=0
+  for count in $errorResponse
+    do
+      intCount=$(printf "%.0f\n" $count)
+      errorTotal=$(( $errorTotal + $intCount ))
+    done
+  
+  local invocationTotal=0
+  for count in $invocationsResponse
+    do
+      intCount=$(printf "%.0f\n" $count)
+      invocationTotal=$(( $invocationTotal + $intCount ))
+    done
+
+  if [[ "$errorTotal" -gt "0" ]] && [[ "$invocationTotal" -gt "0" ]]; then
+    echo "There were $errorTotal errors in the last 5 minutes"
+    echo "There were $invocationTotal invocations in the last 5 minutes"
+    local failurePercentage=$(( $errorTotal * 100 / $invocationTotal ))
+    echo "There were $failurePercentage% errors in the last 5 minutes"
+
+    if [ $failurePercentage -gt 50 ]; then
+      exit 1
+    fi
+  fi
+}
+
 sleep 5
 while true
 do
   fail_on_dlq
+  fail_on_segments_error_count
   total=$(get_total)
 
   if [ "${total}" == 0 ]
   then
     sleep 120 # wait two minutes and re-check for consistency
     fail_on_dlq
+  fail_on_segments_error_count
     recheck=$(get_total)
     if [ "${recheck}" == 0 ]
     then
