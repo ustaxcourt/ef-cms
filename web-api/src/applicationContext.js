3dc52d335e28682539b527ad711238f08d2de8c0
Joseph Sutton <jsutton@flexion.us>
2021-08-18 14:30:22 -0500

    8544: put local order search enabled flag back to true

--- web-api/src/applicationContext.js
+++ web-api/src/applicationContext.js
@@ -1838,7 +1838,7 @@ module.exports = (appContextUser, logger = createLogger()) => {
         getOpenConsolidatedCasesInteractor,
         getOrderSearchEnabledInteractor: applicationContext =>
           environment.stage === 'local'
-            ? false
+            ? true
             : getOrderSearchEnabledInteractor(applicationContext),
         getOutboxMessagesForSectionInteractor,
         getOutboxMessagesForUserInteractor,
