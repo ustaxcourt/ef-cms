c6b428d37052cf55d3703ecd7dc0acbf615371d5
Kris Koskelin <kkoskelin@flexion.us>
2021-08-19 16:17:48 -0500

    Devex 874: clean logging implementation
    use 'warn' instead of changing implementation to refer specifically to winston's "warning" function

--- web-api/terraform/template/lambdas/cognito-triggers.js
+++ web-api/terraform/template/lambdas/cognito-triggers.js
@@ -97,6 +97,7 @@ const applicationContext = {
     debug: logger.debug.bind(logger),
     error: logger.error.bind(logger),
     info: logger.info.bind(logger),
+    warn: logger.warn.bind(logger),
   },
 };
 
