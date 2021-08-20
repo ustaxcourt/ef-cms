fcc8f5b84712f917081b3a55dfd97608229d0ab4
TElliott <telliott@flexion.us>
2021-08-20 12:50:28 -0500

    8804: Add link and new header [skip-ci]

---
+++ web-client/src/views/AdvancedSearch/AdvancedSearchHeader.jsx
@@ -0,0 +1,29 @@
+import { Button } from '../../ustc-ui/Button/Button';
+import { connect } from '@cerebral/react';
+import { props } from 'cerebral';
+import React from 'react';
+
+export const AdvancedSearchHeader = connect(
+  {
+    feedBackUrl: props.feedBackUrl,
+  },
+  function AdvancedSearchHeader({ feedBackUrl }) {
+    return (
+      <div className="big-blue-header">
+        <div className="grid-container display-flex space-between flex-align-center">
+          <h1 tabIndex="-1">Search</h1>
+          <div>
+            <Button
+              className="usa-button--outline margin-right-0"
+              href={feedBackUrl}
+              icon="paper-plane"
+              iconColor="dark-blue"
+            >
+              Send Feedback
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  },
+);
