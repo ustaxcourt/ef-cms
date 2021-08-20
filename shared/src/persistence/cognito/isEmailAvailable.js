c8451a6e67aa6eb42a3385e4de806db5ef7e6f81
Cody Seibert <cseibert@flexion.us>
2021-08-20 12:12:11 -0400

    refactor isEmailAvailable to use new helper method

--- shared/src/persistence/cognito/isEmailAvailable.js
+++ shared/src/persistence/cognito/isEmailAvailable.js
@@ -1,14 +1,10 @@
+const { isUserAlreadyCreated } = require('../dynamo/users/createOrUpdateUser');
+
 exports.isEmailAvailable = async ({ applicationContext, email }) => {
-  try {
-    await applicationContext
-      .getCognito()
-      .adminGetUser({
-        UserPoolId: process.env.USER_POOL_ID,
-        Username: email,
-      })
-      .promise();
-    return false;
-  } catch (e) {
-    return true;
-  }
+  const inUse = await isUserAlreadyCreated({
+    UserPoolId: process.env.USER_POOL_ID,
+    applicationContext,
+    email,
+  });
+  return !inUse;
 };
