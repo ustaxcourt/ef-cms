876dba50f505368991a830ebb514959ccdace83d
Rachel Schneiderman <rschneiderman@flexion.us>
2021-08-23 12:24:24 -0400

    Devex 968: Try removing all external exclusions from lambda webpack config

--- webpack.config.lambda.api.js
+++ webpack.config.lambda.api.js
@@ -13,7 +13,7 @@ module.exports = {
     streams: './web-api/terraform/template/lambdas/streams.js',
     websockets: './web-api/terraform/template/lambdas/websockets.js',
   },
-  externals: ['aws-sdk', 'chrome-aws-lambda'],
+  externals: [],
   output: {
     clean: true,
     libraryTarget: 'umd',
