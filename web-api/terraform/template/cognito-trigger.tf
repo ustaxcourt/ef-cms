0dc9fe434be68c5c08dd59f1b1f6676b2666e097
Rachel Schneiderman <rschneiderman@flexion.us>
2021-08-23 13:07:25 -0400

    Devex 968: Remove blank lines

--- web-api/terraform/template/cognito-trigger.tf
+++ web-api/terraform/template/cognito-trigger.tf
@@ -33,7 +33,6 @@ resource "aws_lambda_function" "cognito_post_confirmation_lambda" {
   role             = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/iam_cognito_post_confirmation_lambda_role_${var.environment}"
   handler          = "cognito-triggers.handler"
   source_code_hash = data.archive_file.zip_triggers.output_base64sha256
-  
   timeout          = "29"
   runtime          = "nodejs14.x"
 
@@ -68,7 +67,6 @@ resource "aws_lambda_function" "cognito_post_authentication_lambda" {
   role             = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/iam_cognito_post_authentication_lambda_role_${var.environment}"
   handler          = "cognito-triggers.handler"
   source_code_hash = data.archive_file.zip_triggers.output_base64sha256
-  
   timeout          = "29"
   runtime          = "nodejs14.x"
 
