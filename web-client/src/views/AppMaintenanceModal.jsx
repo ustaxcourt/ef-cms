c5a95ea10153e457d8665b8b8b364b55453c2d7e
rosie highsmith <ahighsmith@flexion.us>
2021-08-23 12:26:03 -0700

    7095: working on the icon [skip ci]

--- web-client/src/views/AppMaintenanceModal.jsx
+++ web-client/src/views/AppMaintenanceModal.jsx
@@ -1,4 +1,5 @@
 import { Button } from '../ustc-ui/Button/Button';
+import { Icon } from '../ustc-ui/Icon/Icon';
 import { ModalDialog } from './ModalDialog';
 import { connect } from '@cerebral/react';
 import { sequences, state } from 'cerebral';
@@ -28,14 +29,19 @@ export const AppMaintenanceModal = connect(
         closeLink={false}
         showButtons={false}
       >
+        <Icon
+          aria-label="wrench"
+          className="huge-icon"
+          icon={['fas', 'wrench']}
+        />
         <h2 className="margin-top-5">DAWSON is undergoing maintenance.</h2>
-        <p className="margin-top-5">
+        <p className="margin-top-2">
           Your work may not be saved. Check back later for updates.
         </p>
-        <p className="margin-top-5 ">
+        <p className="margin-top-5">
           <Button onClick={confirmSequence}>Log Out</Button>
         </p>
-        <p className="text-center">
+        <p className="text-center margin-top-0">
           <Button
             link
             // link={cancelLink}
