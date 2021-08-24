48b57a47e9e126a1423b5fb1a071a50b8753524a
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-08-24 14:34:30 -0700

    7095: working on maintenance notification lambda

---
+++ shared/src/business/useCases/maintenance/sendMaintenanceNotificationsInteractor.js
@@ -0,0 +1,7 @@
+/**
+ * @param {object} applicationContext the application context
+ */
+exports.sendMaintenanceNotificationsInteractor = async applicationContext => {
+  applicationContext.logger.debug('this is working!');
+  //send notification to all users?
+};
--- web-api/terraform/template/main-east.tf
+++ web-api/terraform/template/main-east.tf
@@ -13,6 +13,7 @@ data "archive_file" "zip_api" {
   source_dir  = "${path.module}/../template/lambdas/dist/"
   excludes = ["${path.module}/../template/lambdas/dist/api-public.js",
     "${path.module}/../template/lambdas/dist/websockets.js",
+    "${path.module}/../template/lambdas/dist/maintenance-notify.js",
     "${path.module}/../template/lambdas/dist/cron.js",
     "${path.module}/../template/lambdas/dist/streams.js",
     "${path.module}/../template/lambdas/dist/cognito-triggers.js",
@@ -41,6 +42,23 @@ resource "null_resource" "websockets_east_object" {
   depends_on = [aws_s3_bucket.api_lambdas_bucket_east]
   provisioner "local-exec" {
     command = "aws s3 cp ${data.archive_file.zip_websockets.output_path} s3://${aws_s3_bucket.api_lambdas_bucket_east.id}/websockets_${var.deploying_color}.js.zip"
+  }
+
+  triggers = {
+    always_run = timestamp()
+  }
+}
+
+data "archive_file" "zip_maintenance_notify" {
+  type        = "zip"
+  output_path = "${path.module}/../template/lambdas/maintenance-notify.js.zip"
+  source_file = "${path.module}/../template/lambdas/dist/maintenance-notify.js"
+}
+
+resource "null_resource" "maintenance_notify_east_object" {
+  depends_on = [aws_s3_bucket.api_lambdas_bucket_east]
+  provisioner "local-exec" {
+    command = "aws s3 cp ${data.archive_file.zip_maintenance_notify.output_path} s3://${aws_s3_bucket.api_lambdas_bucket_east.id}/maintenance_notify_${var.deploying_color}.js.zip"
   }
 
   triggers = {
@@ -158,6 +176,18 @@ data "aws_s3_bucket_object" "websockets_green_east_object" {
   depends_on = [null_resource.websockets_east_object]
   bucket     = aws_s3_bucket.api_lambdas_bucket_east.id
   key        = "websockets_green.js.zip"
+}
+
+data "aws_s3_bucket_object" "maintenance_notify_blue_east_object" {
+  depends_on = [null_resource.maintenance_notify_east_object]
+  bucket     = aws_s3_bucket.api_lambdas_bucket_east.id
+  key        = "maintenance_notify_blue.js.zip"
+}
+
+data "aws_s3_bucket_object" "maintenance_notify_green_east_object" {
+  depends_on = [null_resource.maintenance_notify_east_object]
+  bucket     = aws_s3_bucket.api_lambdas_bucket_east.id
+  key        = "maintenance_notify_green.js.zip"
 }
 
 data "aws_s3_bucket_object" "puppeteer_blue_east_object" {
@@ -291,18 +321,19 @@ module "api-east-waf" {
 }
 
 module "api-east-green" {
-  api_object             = null_resource.api_east_object
-  api_public_object      = null_resource.api_public_east_object
-  websockets_object      = null_resource.websockets_east_object
-  puppeteer_layer_object = null_resource.puppeteer_layer_east_object
-  cron_object            = null_resource.cron_east_object
-  streams_object         = null_resource.streams_east_object
-  source                 = "../api/"
-  environment            = var.environment
-  dns_domain             = var.dns_domain
-  authorizer_uri         = aws_lambda_function.cognito_authorizer_lambda.invoke_arn
-  account_id             = data.aws_caller_identity.current.account_id
-  zone_id                = data.aws_route53_zone.zone.id
+  api_object                = null_resource.api_east_object
+  api_public_object         = null_resource.api_public_east_object
+  websockets_object         = null_resource.websockets_east_object
+  maintenance_notify_object = null_resource.maintenance_notify_east_object
+  puppeteer_layer_object    = null_resource.puppeteer_layer_east_object
+  cron_object               = null_resource.cron_east_object
+  streams_object            = null_resource.streams_east_object
+  source                    = "../api/"
+  environment               = var.environment
+  dns_domain                = var.dns_domain
+  authorizer_uri            = aws_lambda_function.cognito_authorizer_lambda.invoke_arn
+  account_id                = data.aws_caller_identity.current.account_id
+  zone_id                   = data.aws_route53_zone.zone.id
   lambda_environment = merge(data.null_data_source.locals.outputs, {
     DYNAMODB_ENDPOINT      = "dynamodb.us-east-1.amazonaws.com"
     CURRENT_COLOR          = "green"
@@ -314,34 +345,36 @@ module "api-east-green" {
   providers = {
     aws = aws.us-east-1
   }
-  current_color          = "green"
-  deploying_color        = var.deploying_color
-  lambda_bucket_id       = aws_s3_bucket.api_lambdas_bucket_east.id
-  public_object_hash     = data.aws_s3_bucket_object.api_public_green_east_object.etag
-  api_object_hash        = data.aws_s3_bucket_object.api_green_east_object.etag
-  websockets_object_hash = data.aws_s3_bucket_object.websockets_green_east_object.etag
-  puppeteer_object_hash  = data.aws_s3_bucket_object.puppeteer_green_east_object.etag
-  cron_object_hash       = data.aws_s3_bucket_object.cron_green_east_object.etag
-  streams_object_hash    = data.aws_s3_bucket_object.streams_green_east_object.etag
-  create_cron            = 1
-  create_streams         = 1
-  stream_arn             = data.aws_dynamodb_table.green_dynamo_table.stream_arn
-  web_acl_arn            = module.api-east-waf.web_acl_arn
+  current_color                  = "green"
+  deploying_color                = var.deploying_color
+  lambda_bucket_id               = aws_s3_bucket.api_lambdas_bucket_east.id
+  public_object_hash             = data.aws_s3_bucket_object.api_public_green_east_object.etag
+  api_object_hash                = data.aws_s3_bucket_object.api_green_east_object.etag
+  websockets_object_hash         = data.aws_s3_bucket_object.websockets_green_east_object.etag
+  maintenance_notify_object_hash = data.aws_s3_bucket_object.maintenance_notify_green_east_object.etag
+  puppeteer_object_hash          = data.aws_s3_bucket_object.puppeteer_green_east_object.etag
+  cron_object_hash               = data.aws_s3_bucket_object.cron_green_east_object.etag
+  streams_object_hash            = data.aws_s3_bucket_object.streams_green_east_object.etag
+  create_cron                    = 1
+  create_streams                 = 1
+  stream_arn                     = data.aws_dynamodb_table.green_dynamo_table.stream_arn
+  web_acl_arn                    = module.api-east-waf.web_acl_arn
 }
 
 module "api-east-blue" {
-  api_object             = null_resource.api_east_object
-  api_public_object      = null_resource.api_public_east_object
-  websockets_object      = null_resource.websockets_east_object
-  puppeteer_layer_object = null_resource.puppeteer_layer_east_object
-  cron_object            = null_resource.cron_east_object
-  streams_object         = null_resource.streams_east_object
-  source                 = "../api/"
-  environment            = var.environment
-  dns_domain             = var.dns_domain
-  authorizer_uri         = aws_lambda_function.cognito_authorizer_lambda.invoke_arn
-  account_id             = data.aws_caller_identity.current.account_id
-  zone_id                = data.aws_route53_zone.zone.id
+  api_object                = null_resource.api_east_object
+  api_public_object         = null_resource.api_public_east_object
+  websockets_object         = null_resource.websockets_east_object
+  maintenance_notify_object = null_resource.maintenance_notify_east_object
+  puppeteer_layer_object    = null_resource.puppeteer_layer_east_object
+  cron_object               = null_resource.cron_east_object
+  streams_object            = null_resource.streams_east_object
+  source                    = "../api/"
+  environment               = var.environment
+  dns_domain                = var.dns_domain
+  authorizer_uri            = aws_lambda_function.cognito_authorizer_lambda.invoke_arn
+  account_id                = data.aws_caller_identity.current.account_id
+  zone_id                   = data.aws_route53_zone.zone.id
   lambda_environment = merge(data.null_data_source.locals.outputs, {
     DYNAMODB_ENDPOINT      = "dynamodb.us-east-1.amazonaws.com"
     CURRENT_COLOR          = "blue"
@@ -353,17 +386,18 @@ module "api-east-blue" {
   providers = {
     aws = aws.us-east-1
   }
-  current_color          = "blue"
-  deploying_color        = var.deploying_color
-  lambda_bucket_id       = aws_s3_bucket.api_lambdas_bucket_east.id
-  public_object_hash     = data.aws_s3_bucket_object.api_public_blue_east_object.etag
-  api_object_hash        = data.aws_s3_bucket_object.api_blue_east_object.etag
-  websockets_object_hash = data.aws_s3_bucket_object.websockets_blue_east_object.etag
-  puppeteer_object_hash  = data.aws_s3_bucket_object.puppeteer_blue_east_object.etag
-  cron_object_hash       = data.aws_s3_bucket_object.cron_blue_east_object.etag
-  streams_object_hash    = data.aws_s3_bucket_object.streams_blue_east_object.etag
-  create_cron            = 1
-  create_streams         = 1
-  stream_arn             = data.aws_dynamodb_table.blue_dynamo_table.stream_arn
-  web_acl_arn            = module.api-east-waf.web_acl_arn
+  current_color                  = "blue"
+  deploying_color                = var.deploying_color
+  lambda_bucket_id               = aws_s3_bucket.api_lambdas_bucket_east.id
+  public_object_hash             = data.aws_s3_bucket_object.api_public_blue_east_object.etag
+  api_object_hash                = data.aws_s3_bucket_object.api_blue_east_object.etag
+  websockets_object_hash         = data.aws_s3_bucket_object.websockets_blue_east_object.etag
+  maintenance_notify_object_hash = data.aws_s3_bucket_object.maintenance_notify_blue_east_object.etag
+  puppeteer_object_hash          = data.aws_s3_bucket_object.puppeteer_blue_east_object.etag
+  cron_object_hash               = data.aws_s3_bucket_object.cron_blue_east_object.etag
+  streams_object_hash            = data.aws_s3_bucket_object.streams_blue_east_object.etag
+  create_cron                    = 1
+  create_streams                 = 1
+  stream_arn                     = data.aws_dynamodb_table.blue_dynamo_table.stream_arn
+  web_acl_arn                    = module.api-east-waf.web_acl_arn
 }
