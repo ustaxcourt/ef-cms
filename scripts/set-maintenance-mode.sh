f3bec0c3b5f2ccc228505658617ba271203e26e6
Rachel Schneiderman <rschneiderman@flexion.us>
2021-08-25 16:54:47 -0400

    Fix maintenance mode script, add comments for next steps [skip ci]

--- scripts/set-maintenance-mode.sh
+++ scripts/set-maintenance-mode.sh
@@ -19,5 +19,5 @@ aws dynamodb put-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --it
 
 if [ "$VALUE" == "true" ]; then
   CURRENT_COLOR=$(aws dynamodb get-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --key '{"pk":{"S":"current-color"},"sk":{"S":"current-color"}}' | jq -r ".Item.current.S")
-  aws lambda invoke --function-name "send_maintenance_notifications_"${ENV}"_"${CURRENT_COLOR}
+  aws lambda invoke --region us-east-1 --function-name "send_maintenance_notifications_"${ENV}"_"${CURRENT_COLOR} /dev/null
 fi
--- shared/src/business/useCases/maintenance/sendMaintenanceNotificationsInteractor.js
+++ shared/src/business/useCases/maintenance/sendMaintenanceNotificationsInteractor.js
@@ -4,4 +4,10 @@
 exports.sendMaintenanceNotificationsInteractor = async applicationContext => {
   applicationContext.logger.debug('this is working!');
   //send notification to all users?
+
+  // getWebSocketConnectionsByUserId to add gsi1pk to all dynamo records
+  //  add migration script
+
+  // get all connections by gsi1pk
+  // postToConnection - call for each connection to alert that maintenance mode is on
 };
