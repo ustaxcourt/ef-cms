1824bd5a8b8b9db2bebf58d140ea6dd9e515f161
Rachel Schneiderman <rschneiderman@flexion.us>
2021-08-26 12:24:11 -0400

    7095: Refactoring

--- shared/src/notifications/sendNotificationToConnection.js
+++ shared/src/notifications/sendNotificationToConnection.js
@@ -1,3 +1,11 @@
+/**
+ * sendNotificationToConnection
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.applicationContext the application context
+ * @param {object} providers.connection the connection to send the message to
+ * @param {string} providers.messageStringified the message
+ */
 exports.sendNotificationToConnection = async ({
   applicationContext,
   connection,
--- web-api/src/cases/sendMaintenanceNotificationsLambda.js
+++ web-api/src/cases/sendMaintenanceNotificationsLambda.js
@@ -1,7 +1,7 @@
 const { genericHandler } = require('../genericHandler');
 
 /**
- * lambda which is used to send notifications to all users when maintenance mode is engaged
+ * lambda which is used to send notifications to all users when maintenance mode is toggled
  *
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
