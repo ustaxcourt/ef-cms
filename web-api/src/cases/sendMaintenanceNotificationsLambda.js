17d424eea9493501140014c2d211f1fa9f4f1f03
Rachel Schneiderman <rschneiderman@flexion.us>
2021-08-27 12:09:38 -0400

    7095: Provide maintenance mode value to lambda

--- web-api/src/cases/sendMaintenanceNotificationsLambda.js
+++ web-api/src/cases/sendMaintenanceNotificationsLambda.js
@@ -6,15 +6,20 @@ const { genericHandler } = require('../genericHandler');
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-exports.sendMaintenanceNotificationsLambda = event =>
-  genericHandler(
+exports.sendMaintenanceNotificationsLambda = event => {
+  console.log(JSON.stringify(event, null, 2));
+
+  return genericHandler(
     event,
     async ({ applicationContext }) => {
       return await applicationContext
         .getUseCases()
-        .sendMaintenanceNotificationsInteractor(applicationContext);
+        .sendMaintenanceNotificationsInteractor(applicationContext, {
+          ...JSON.parse(event.body),
+        });
     },
     {
       user: {},
     },
   );
+};
