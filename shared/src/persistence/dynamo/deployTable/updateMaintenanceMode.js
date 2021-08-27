7a1a8eea2277cc8eaf22a280b19ef2999742ab33
Rachel Schneiderman <rschneiderman@flexion.us>
2021-08-27 12:09:38 -0400

    [skip ci] 7095: Provide maintenance mode value to lambda

--- shared/src/persistence/dynamo/deployTable/updateMaintenanceMode.js
+++ shared/src/persistence/dynamo/deployTable/updateMaintenanceMode.js
@@ -10,7 +10,7 @@ exports.updateMaintenanceMode = async ({
   applicationContext,
   maintenanceMode,
 }) => {
-  await client.update({
+  await client.updateToDeployTable({
     ExpressionAttributeNames: {
       '#current': 'current',
     },
