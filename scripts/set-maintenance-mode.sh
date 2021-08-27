15b2819825d034653d99e036d1a463082f7edf45
rosie highsmith <ahighsmith@flexion.us>
2021-08-27 08:42:10 -0700

    7095: adding persistence method to updateMaintenanceMode [skip ci]

--- scripts/set-maintenance-mode.sh
+++ scripts/set-maintenance-mode.sh
@@ -3,7 +3,7 @@
 # Sets the maintenance mode flag to the passed in value in the dynamo deploy table
 
 # Usage
-#   ./engage-maintenance-mode.sh true dev
+#   ./set-maintenance-mode.sh true dev
 
 # Arguments
 #   - $1 - true to engage maintenance mode, false to disengage maintenance mode
@@ -19,5 +19,8 @@ aws dynamodb put-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --it
 
 if [ "$VALUE" == "true" ]; then
   CURRENT_COLOR=$(aws dynamodb get-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --key '{"pk":{"S":"current-color"},"sk":{"S":"current-color"}}' | jq -r ".Item.current.S")
-  aws lambda invoke --region us-east-1 --function-name "send_maintenance_notifications_"${ENV}"_"${CURRENT_COLOR} /dev/null
+  aws lambda invoke --region us-east-1 \
+    --function-name "send_maintenance_notifications_"${ENV}"_"${CURRENT_COLOR} \
+    --payload '{ "maintenanceMode": '${1}' }' \
+    /dev/null
 fi
