e969ffdc61f97cd0aa59bea3118577a5c3ebb3a8
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2021-08-31 02:50:57 +0530

    7095 stopping web socket connection when logging out

--- web-client/src/presenter/sequences/signOutSequence.js
+++ web-client/src/presenter/sequences/signOutSequence.js
@@ -3,9 +3,11 @@ import { clearLoginFormAction } from '../actions/clearLoginFormAction';
 import { clearUserAction } from '../actions/clearUserAction';
 import { navigateToCognitoAction } from '../actions/navigateToCognitoAction';
 import { setCurrentPageAction } from '../actions/setCurrentPageAction';
+import { stopWebSocketConnectionAction } from '../actions/WebSocketConnection/stopWebSocketConnectionAction';
 
 export const signOutSequence = [
   setCurrentPageAction('Interstitial'),
+  stopWebSocketConnectionAction,
   clearAlertsAction,
   clearUserAction,
   clearLoginFormAction,
