072e22c3528877991bef6907a56d25fb84db2193
Cody Seibert <cseibert@flexion.us>
2021-08-31 10:58:16 -0400

    8511 - wait for operations to zero out

--- shared/admin-tools/elasticsearch/check-reindex-complete.js
+++ shared/admin-tools/elasticsearch/check-reindex-complete.js
@@ -9,25 +9,35 @@ const { getClient } = require('../../../web-api/elasticsearch/client');
 
 const environmentName = process.argv[2] || 'exp1';
 
-const getCounts = async ({ indexName, version }) => {
+const destinationVersion = process.env.DESTINATION_TABLE.split('-').pop();
+
+const getClusterStats = async ({ version }) => {
   const esClient = await getClient({ environmentName, version });
-  const info = await esClient.indices.stats({ index: indexName });
-  return info['_all'].total.docs.count;
+  const info = await esClient.indices.stats({
+    index: '_all',
+    level: 'indices',
+  });
+  return info;
 };
+
+const currentVersion = destinationVersion === 'alpha' ? 'beta' : 'alpha';
+console.log(destinationVersion, currentVersion);
 
 (async () => {
   let diffTotal = 0;
+  const currentInfo = await getClusterStats({ version: currentVersion });
+  let destinationInfo = await getClusterStats({ version: destinationVersion });
+
   for (const indexName of [
     'efcms-case',
     'efcms-docket-entry',
     'efcms-user',
     'efcms-user-case',
   ]) {
-    const [countAlpha, countBeta] = await Promise.all(
-      ['alpha', 'beta'].map(version => getCounts({ indexName, version })),
-    );
-
-    const diff = Math.abs(countAlpha - countBeta);
+    const countCurrent = currentInfo.indices[indexName].total.docs.count;
+    const countDestination =
+      destinationInfo.indices[indexName].total.docs.count;
+    const diff = Math.abs(countCurrent - countDestination);
     diffTotal += diff;
     console.log(`${indexName} has a diff of ${diff}`);
   }
@@ -35,5 +45,30 @@ const getCounts = async ({ indexName, version }) => {
   if (diffTotal > 0) {
     console.log('Indexes are not in sync, exiting with 1');
     process.exit(1);
+  }
+
+  let shouldKeepChecking = true;
+  while (shouldKeepChecking) {
+    shouldKeepChecking = false;
+
+    for (const indexName of [
+      'efcms-case-deadline',
+      'efcms-message',
+      'efcms-work-item',
+    ]) {
+      const operationsDestination =
+        destinationInfo.indices[indexName].total.translog.operations;
+      if (operationsDestination > 0) {
+        shouldKeepChecking = true;
+        console.log(
+          `${operationsDestination} operations on ${indexName} still processing, waiting 60 seconds to check operations again.`,
+        );
+        await new Promise(resolve => setTimeout(resolve, 60000));
+        destinationInfo = await getClusterStats({
+          version: destinationVersion,
+        });
+        break;
+      }
+    }
   }
 })();
