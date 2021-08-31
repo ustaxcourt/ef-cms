c4d29b754898ac1abe9c8e6f5f68032fb74242dd
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2021-08-31 10:31:20 -0700

    7095: remove the stop connection actions

--- web-client/src/presenter/sequences/adminContactUpdateCompleteSequence.js
+++ web-client/src/presenter/sequences/adminContactUpdateCompleteSequence.js
@@ -1,7 +1,5 @@
-import { stopWebSocketConnectionAction } from '../actions/WebSocketConnection/stopWebSocketConnectionAction';
 import { unsetUserContactEditProgressAction } from '../actions/unsetUserContactEditProgressAction';
 
 export const adminContactUpdateCompleteSequence = [
-  stopWebSocketConnectionAction,
   unsetUserContactEditProgressAction,
 ];
