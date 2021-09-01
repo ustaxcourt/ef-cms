7dd3c70b7a12d1a3bf9d187cd3201b938ec24f42
Rachel Schneiderman <rschneiderman@flexion.us>
2021-09-01 13:00:58 -0400

    7095: Implement interactor, add test [skip ci]

--- shared/src/business/useCases/getMaintenanceModeInteractor.js
+++ shared/src/business/useCases/getMaintenanceModeInteractor.js
@@ -4,4 +4,8 @@
  * @param {object} applicationContext the application context
  * @returns {boolean} the value of maintenance mode
  */
-exports.getMaintenanceModeInteractor = async applicationContext => {};
+exports.getMaintenanceModeInteractor = async applicationContext => {
+  return await applicationContext
+    .getPersistenceGateway()
+    .getMaintenanceMode({ applicationContext });
+};
