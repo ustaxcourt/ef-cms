6eb3beb596395fc68c7ea6918be7d494b4b2bfb7
Rachel Schneiderman <rschneiderman@flexion.us>
2021-09-03 16:42:08 -0400

    7095: Things are workingg

--- web-client/src/presenter/sequences/startWebSocketConnectionSequence.js
+++ web-client/src/presenter/sequences/startWebSocketConnectionSequence.js
@@ -1,5 +1,10 @@
+import { setShowModalFactoryAction } from '../actions/setShowModalFactoryAction';
 import { startWebSocketConnectionAction } from '../actions/WebSocketConnection/startWebSocketConnectionAction';
 
 export const startWebSocketConnectionSequence = [
   startWebSocketConnectionAction,
+  {
+    error: [setShowModalFactoryAction('WebSocketErrorModal')],
+    success: [],
+  },
 ];
