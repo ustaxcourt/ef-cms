edd5c7cc1396c1286ee51f7c8ef4528ff2dc8908
Michael Marcotte <mike.marcotte@gmail.com>
2021-09-03 14:37:35 -0400

    add script to get the number of petitions by filingtype and by month

---
+++ shared/admin-tools/elasticsearch/get-petition-counts.js
@@ -0,0 +1,133 @@
+const {
+  computeDate,
+  dateStringsCompared,
+} = require('../../src/business/utilities/DateHandler');
+const { getClient } = require('../../../web-api/elasticsearch/client');
+
+const environmentName = process.argv[2] || 'exp1';
+
+let allPetitions = false;
+
+const getAllItems = async () => {
+  if (allPetitions) return allPetitions;
+  const esClient = await getClient({ environmentName });
+  const allItems = [];
+  const responseQueue = [];
+
+  const res = await esClient.search({
+    _source: ['createdAt.S', 'isPaper.BOOL'],
+    body: {
+      query: {
+        bool: {
+          must: [
+            {
+              match: {
+                'eventCode.S': 'P',
+              },
+            },
+            {
+              range: {
+                'receivedAt.S': {
+                  gte: '2021-01-01T05:00:00.000Z',
+                  lte: '2021-12-01T05:00:00.000Z',
+                },
+              },
+            },
+          ],
+        },
+      },
+    },
+    index: 'efcms-docket-entry',
+    scroll: '60s',
+    size: 5000,
+  });
+
+  responseQueue.push(res);
+  while (responseQueue.length) {
+    const body = responseQueue.shift();
+
+    // collect the titles from this response
+    body.hits.hits.forEach(function (hit) {
+      allItems.push(hit['_source']);
+    });
+
+    // check to see if we have collected all of the quotes
+    if (body.hits.total.value === allItems.length) {
+      return allItems;
+    }
+
+    // get the next response if there are more quotes to fetch
+    responseQueue.push(
+      await esClient.scroll({
+        scroll: '60s',
+        scrollId: body['_scroll_id'],
+      }),
+    );
+  }
+};
+
+const getPetitions = async ({ gte, lte }) => {
+  console.log({ gte, lte });
+
+  const esClient = await getClient({ environmentName });
+  const query = {
+    body: {
+      query: {
+        bool: {
+          must: [
+            {
+              match: {
+                'eventCode.S': 'P',
+              },
+            },
+            {
+              range: {
+                'receivedAt.S': {
+                  gte,
+                  lte,
+                },
+              },
+            },
+          ],
+        },
+      },
+    },
+    index: 'efcms-docket-entry',
+    size: 10000,
+  };
+  const results = await esClient.search(query);
+  console.log(`Total: ${results.hits.total.value}`);
+  return results.hits.hits;
+};
+
+const getCounts = async ({ isPaper, month }) => {
+  const gte = computeDate({ day: 1, month, year: 2021 });
+  const lte = computeDate({ day: 1, month: month + 1, year: 2021 });
+
+  if (isPaper) {
+    const petitions = await getPetitions({ gte, lte });
+    return petitions.filter(p => p['_source']?.isPaper?.BOOL === isPaper)
+      .length;
+  } else {
+    allPetitions = await getAllItems();
+    console.log(`allPetitions.length: ${allPetitions.length};`);
+    allPetitions = allPetitions.filter(p => !p.isPaper?.BOOL);
+    return allPetitions.filter(
+      p =>
+        dateStringsCompared(p.createdAt.S, gte) >= 0 &&
+        dateStringsCompared(p.createdAt.S, lte) < 0,
+    ).length;
+  }
+};
+
+(async () => {
+  const results = {};
+
+  for (let month = 1; month <= 12; month++) {
+    results[month] = {
+      isElectronic: await getCounts({ isPaper: false, month }),
+      isPaper: await getCounts({ isPaper: true, month }),
+    };
+  }
+  console.log(results);
+})();
