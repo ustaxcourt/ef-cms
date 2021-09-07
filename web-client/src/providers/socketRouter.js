7f2ea7ecd403e04bbd2928fc55843cb063a6da9b
Rachel Schneiderman <rschneiderman@flexion.us>
2021-09-07 12:10:58 -0400

    7095: route to maintenance page when maintenance mode is on and the user reloads [skip ci]

--- web-client/src/providers/socketRouter.js
+++ web-client/src/providers/socketRouter.js
@@ -67,6 +67,7 @@ export const socketRouter = (app, onMessageCallbackFn) => {
       case 'maintenance_mode_engaged':
         await app.getSequence('openAppMaintenanceModalSequence')({
           ...message,
+          maintenanceMode: true,
           path: '/maintenance',
         });
         break;
