9adacf8b4744b7456fb567f3fd01d262ecd4e7bf
TElliott <telliott@flexion.us>
2021-09-10 11:28:40 -0500

    7643: updated more tests, added NOCE document type

--- shared/src/business/utilities/generateChangeOfAddressTemplate.js
+++ shared/src/business/utilities/generateChangeOfAddressTemplate.js
@@ -1,3 +1,6 @@
+const {
+  NOTICE_OF_CHANGE_CONTACT_INFORMATION_MAP,
+} = require('../../business/entities/EntityConstants');
 const { union } = require('lodash');
 
 /**
@@ -56,25 +59,21 @@ const getDocumentTypeForAddressChange = ({ diff, newData, oldData }) => {
   const isEmailChange = newData.email && newData.email !== oldData.email;
 
   if (isEmailChange) {
-    documentType = {
-      eventCode: 'NOCE',
-      title: 'Notice of Change of Email Address',
-    };
+    documentType = NOTICE_OF_CHANGE_CONTACT_INFORMATION_MAP.find(
+      e => e.eventCode === 'NOCE',
+    );
   } else if (isAddressChange && !isPhoneChange) {
-    documentType = {
-      eventCode: 'NCA',
-      title: 'Notice of Change of Address',
-    };
+    documentType = NOTICE_OF_CHANGE_CONTACT_INFORMATION_MAP.find(
+      e => e.eventCode === 'NCA',
+    );
   } else if (isPhoneChange && !isAddressChange) {
-    documentType = {
-      eventCode: 'NCP',
-      title: 'Notice of Change of Telephone Number',
-    };
+    documentType = NOTICE_OF_CHANGE_CONTACT_INFORMATION_MAP.find(
+      e => e.eventCode === 'NCP',
+    );
   } else if (isAddressChange && isPhoneChange) {
-    documentType = {
-      eventCode: 'NCAP',
-      title: 'Notice of Change of Address and Telephone Number',
-    };
+    documentType = NOTICE_OF_CHANGE_CONTACT_INFORMATION_MAP.find(
+      e => e.eventCode === 'NCAP',
+    );
   }
 
   return documentType;
