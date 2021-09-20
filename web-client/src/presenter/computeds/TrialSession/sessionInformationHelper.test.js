35f3eda89dbf1a3d214906a45108db1a5dd11fd2
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2021-09-20 10:07:10 -0600

    more helper stuff

--- web-client/src/presenter/computeds/TrialSession/sessionInformationHelper.test.js
+++ web-client/src/presenter/computeds/TrialSession/sessionInformationHelper.test.js
@@ -1,7 +1,8 @@
-import { sessionInformationHelper as sessionInformationHelperComputed } from './addCourtIssuedDocketEntryHelper';
-import { applicationContextForClient as applicationContext } from '../../../../shared/src/business/test/createTestApplicationContext';
+import { TRIAL_SESSION_SCOPE_TYPES } from '../../../../../shared/src/business/entities/EntityConstants';
+import { applicationContextForClient as applicationContext } from '../../../../../shared/src/business/test/createTestApplicationContext';
 import { runCompute } from 'cerebral/test';
-import { withAppContextDecorator } from '../../withAppContext';
+import { sessionInformationHelper as sessionInformationHelperComputed } from './sessionInformationHelper';
+import { withAppContextDecorator } from '../../../withAppContext';
 
 describe('sessionInformationHelper', () => {
   const sessionInformationHelper = withAppContextDecorator(
@@ -11,10 +12,23 @@ describe('sessionInformationHelper', () => {
     },
   );
 
-  it('should return isStandaloneSession true when form TRIAL_SESSION_SCOPE_TYPES is `Standalone Remote`', () => {
-    const result = runCompute(sessionInformationHelper, { state });
+  it(`should return isStandaloneSession true when form TRIAL_SESSION_SCOPE_TYPES is ${TRIAL_SESSION_SCOPE_TYPES.standaloneRemote}`, () => {
+    const result = runCompute(sessionInformationHelper, {
+      state: {
+        form: { sessionScope: TRIAL_SESSION_SCOPE_TYPES.standaloneRemote },
+      },
+    });
     expect(result.isStandaloneSession).toEqual(true);
   });
 
   //todo: should return false when TRIAL_SESSION_SCOPE_TYPES.locationBased
+
+  it(`should not include 'Special' or 'Motion/Hearing' as session types when form TRIAL_SESSION_SCOPE_TYPES is ${TRIAL_SESSION_SCOPE_TYPES.standaloneRemote}`, () => {
+    const result = runCompute(sessionInformationHelper, {
+      state: {
+        form: { sessionScope: TRIAL_SESSION_SCOPE_TYPES.standaloneRemote },
+      },
+    });
+    expect(result.sessionTypes).toEqual(['Regular', 'Small', 'Hybrid']);
+  });
 });
