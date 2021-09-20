9427db195f5895a44306a5c7eb9c9376928413c2
Rachel Schneiderman <rschneiderman@flexion.us>
2021-09-20 15:18:42 -0400

    8896: Adding more tests to cover helper [skip ci]

--- web-client/src/presenter/computeds/TrialSession/trialSessionInformationHelper.test.js
+++ web-client/src/presenter/computeds/TrialSession/trialSessionInformationHelper.test.js
@@ -1,4 +1,7 @@
-import { TRIAL_SESSION_PROCEEDING_TYPES } from '../../../../../shared/src/business/entities/EntityConstants';
+import {
+  TRIAL_SESSION_PROCEEDING_TYPES,
+  TRIAL_SESSION_SCOPE_TYPES,
+} from '../../../../../shared/src/business/entities/EntityConstants';
 import { applicationContextForClient as applicationContext } from '../../../../../shared/src/business/test/createTestApplicationContext';
 import { runCompute } from 'cerebral/test';
 import { trialSessionInformationHelper as trialSessionInformationHelperComputed } from './trialSessionInformationHelper';
@@ -13,7 +16,7 @@ describe('trialSessionInformationHelper', () => {
   );
 
   describe('displayRemoteProceedingForm', () => {
-    it(`should return true when form.proceedingType is ${TRIAL_SESSION_PROCEEDING_TYPES.remote}`, () => {
+    it(`should be true when form.proceedingType is ${TRIAL_SESSION_PROCEEDING_TYPES.remote}`, () => {
       const result = runCompute(trialSessionInformationHelper, {
         state: {
           form: { proceedingType: TRIAL_SESSION_PROCEEDING_TYPES.remote },
@@ -21,6 +24,70 @@ describe('trialSessionInformationHelper', () => {
       });
 
       expect(result.displayRemoteProceedingForm).toEqual(true);
+    });
+
+    it(`should be true when the session scope is ${TRIAL_SESSION_SCOPE_TYPES.standaloneRemote}`, () => {
+      const result = runCompute(trialSessionInformationHelper, {
+        state: {
+          form: { sessionScope: TRIAL_SESSION_SCOPE_TYPES.standaloneRemote },
+        },
+      });
+
+      expect(result.displayRemoteProceedingForm).toEqual(true);
+    });
+
+    it(`should be false when the session scope is NOT ${TRIAL_SESSION_SCOPE_TYPES.standaloneRemote} and form.proceedingType is NOT ${TRIAL_SESSION_PROCEEDING_TYPES.remote}`, () => {
+      const result = runCompute(trialSessionInformationHelper, {
+        state: {
+          form: { proceedingType: 'def', sessionScope: 'abc' },
+        },
+      });
+
+      expect(result.displayRemoteProceedingForm).toEqual(false);
+    });
+  });
+
+  describe('isStandaloneSession', () => {
+    it(`should be true when form.sessionScope is ${TRIAL_SESSION_SCOPE_TYPES.standaloneRemote}`, () => {
+      const result = runCompute(trialSessionInformationHelper, {
+        state: {
+          form: { sessionScope: TRIAL_SESSION_SCOPE_TYPES.standaloneRemote },
+        },
+      });
+
+      expect(result.displayRemoteProceedingForm).toEqual(true);
+    });
+
+    it(`should be false when the session scope is ${TRIAL_SESSION_SCOPE_TYPES.locationBased}`, () => {
+      const result = runCompute(trialSessionInformationHelper, {
+        state: {
+          form: { sessionScope: TRIAL_SESSION_SCOPE_TYPES.locationBased },
+        },
+      });
+
+      expect(result.displayRemoteProceedingForm).toEqual(false);
+    });
+  });
+
+  describe('title', () => {
+    it(`should be 'Remote Proceeding Information' when form.sessionScope is ${TRIAL_SESSION_SCOPE_TYPES.standaloneRemote}`, () => {
+      const result = runCompute(trialSessionInformationHelper, {
+        state: {
+          form: { sessionScope: TRIAL_SESSION_SCOPE_TYPES.standaloneRemote },
+        },
+      });
+
+      expect(result.title).toEqual('Remote Proceeding Information');
+    });
+
+    it(`should be 'Location Information' when form.sessionScope is ${TRIAL_SESSION_SCOPE_TYPES.locationBased}`, () => {
+      const result = runCompute(trialSessionInformationHelper, {
+        state: {
+          form: { sessionScope: TRIAL_SESSION_SCOPE_TYPES.locationBased },
+        },
+      });
+
+      expect(result.title).toEqual('Location Information');
     });
   });
 });
