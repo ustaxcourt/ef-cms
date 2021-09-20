f217aa3c211c80931fc70c564cfb7753fe01f183
Rachel Schneiderman <rschneiderman@flexion.us>
2021-09-20 15:20:26 -0400

    8896: Organize tests in sessionInformationHelper [skip ci]

--- web-client/src/presenter/computeds/TrialSession/sessionInformationHelper.test.js
+++ web-client/src/presenter/computeds/TrialSession/sessionInformationHelper.test.js
@@ -12,49 +12,53 @@ describe('sessionInformationHelper', () => {
     },
   );
 
-  it(`should return isStandaloneSession true when form.sessionScope is ${TRIAL_SESSION_SCOPE_TYPES.standaloneRemote}`, () => {
-    const result = runCompute(sessionInformationHelper, {
-      state: {
-        form: { sessionScope: TRIAL_SESSION_SCOPE_TYPES.standaloneRemote },
-      },
+  describe('isStandaloneSession', () => {
+    it(`should be true when form.sessionScope is ${TRIAL_SESSION_SCOPE_TYPES.standaloneRemote}`, () => {
+      const result = runCompute(sessionInformationHelper, {
+        state: {
+          form: { sessionScope: TRIAL_SESSION_SCOPE_TYPES.standaloneRemote },
+        },
+      });
+
+      expect(result.isStandaloneSession).toEqual(true);
     });
 
-    expect(result.isStandaloneSession).toEqual(true);
+    it(`should be false when form.sessionScope is ${TRIAL_SESSION_SCOPE_TYPES.locationBased}`, () => {
+      const result = runCompute(sessionInformationHelper, {
+        state: {
+          form: { sessionScope: TRIAL_SESSION_SCOPE_TYPES.locationBased },
+        },
+      });
+
+      expect(result.isStandaloneSession).toEqual(false);
+    });
   });
 
-  it(`should return isStandaloneSession false when form.sessionScope is ${TRIAL_SESSION_SCOPE_TYPES.locationBased}`, () => {
-    const result = runCompute(sessionInformationHelper, {
-      state: {
-        form: { sessionScope: TRIAL_SESSION_SCOPE_TYPES.locationBased },
-      },
+  describe('sessionTypes', () => {
+    it(`should NOT include 'Special' or 'Motion/Hearing' when form.sessionScope is ${TRIAL_SESSION_SCOPE_TYPES.standaloneRemote}`, () => {
+      const result = runCompute(sessionInformationHelper, {
+        state: {
+          form: { sessionScope: TRIAL_SESSION_SCOPE_TYPES.standaloneRemote },
+        },
+      });
+
+      expect(result.sessionTypes).toEqual(['Regular', 'Small', 'Hybrid']);
     });
 
-    expect(result.isStandaloneSession).toEqual(false);
-  });
+    it(`should include 'Special' and 'Motion/Hearing' when form.sessionScope is ${TRIAL_SESSION_SCOPE_TYPES.locationBased}`, () => {
+      const result = runCompute(sessionInformationHelper, {
+        state: {
+          form: { sessionScope: TRIAL_SESSION_SCOPE_TYPES.locationBased },
+        },
+      });
 
-  it(`should not include 'Special' or 'Motion/Hearing' as session types when form.sessionScope is ${TRIAL_SESSION_SCOPE_TYPES.standaloneRemote}`, () => {
-    const result = runCompute(sessionInformationHelper, {
-      state: {
-        form: { sessionScope: TRIAL_SESSION_SCOPE_TYPES.standaloneRemote },
-      },
+      expect(result.sessionTypes).toEqual([
+        'Regular',
+        'Small',
+        'Hybrid',
+        'Special',
+        'Motion/Hearing',
+      ]);
     });
-
-    expect(result.sessionTypes).toEqual(['Regular', 'Small', 'Hybrid']);
-  });
-
-  it(`should include 'Special' and 'Motion/Hearing' as session types when form.sessionScope is ${TRIAL_SESSION_SCOPE_TYPES.locationBased}`, () => {
-    const result = runCompute(sessionInformationHelper, {
-      state: {
-        form: { sessionScope: TRIAL_SESSION_SCOPE_TYPES.locationBased },
-      },
-    });
-
-    expect(result.sessionTypes).toEqual([
-      'Regular',
-      'Small',
-      'Hybrid',
-      'Special',
-      'Motion/Hearing',
-    ]);
   });
 });
