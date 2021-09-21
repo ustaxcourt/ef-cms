39e3d79e6175138b99ad0b034e8cd4decb58294f
Absolutestunna <joshujah007@gmail.com>
2021-09-21 15:13:53 -0400

    7643 setting up sqs to consume messages

--- web-api/src/applicationContextTriggers.js
+++ web-api/src/applicationContextTriggers.js
@@ -279,6 +279,7 @@ const applicationContext = {
     createPetitionerAccountInteractor,
     generatePdfFromHtmlInteractor,
     setUserEmailFromPendingEmailInteractor,
+    updatePetitionerCasesInteractor,
   }),
   getUtilities: () => ({
     calculateDifferenceInDays,
