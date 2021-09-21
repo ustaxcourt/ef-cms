7d0c99727f9f1d57a3f9ade01509630e9bc3322a
rosie highsmith <ahighsmith@flexion.us>
2021-09-21 13:16:13 -0700

    8896: working on migration tests [skip ci]

--- web-api/migration-terraform/main/lambdas/migrations/0002-session-scope.test.js
+++ web-api/migration-terraform/main/lambdas/migrations/0002-session-scope.test.js
@@ -2,9 +2,10 @@ const {
   MOCK_TRIAL_INPERSON,
 } = require('../../../../../shared/src/test/mockTrial');
 const {
-  SYSTEM_GENERATED_DOCUMENT_TYPES,
+  TRIAL_SESSION_SCOPE_TYPES,
 } = require('../../../../../shared/src/business/entities/EntityConstants');
-const { migrateItems } = require('./bug-0039-notice-of-trial-date');
+const { migrateItems } = require('./0002-session-scope');
+const { MOCK_CASE } = require('../../../../../shared/src/test/mockCase');
 
 describe('migrateItems', () => {
   let mockTrialSessionItem;
@@ -15,94 +16,64 @@ describe('migrateItems', () => {
       pk: `trial-session|${MOCK_TRIAL_INPERSON.trialSessionId}`,
       sk: `trial-session|${MOCK_TRIAL_INPERSON.trialSessionId}`,
     };
+  });
 
-    //   mockDocketEntryItems = [
-    //     {
-    //       archived: false,
-    //       docketEntryId: '83b77e98-4cf6-4fb4-b8c0-f5f90fd68f3c',
-    //       documentType: 'Answer',
-    //       eventCode: 'A',
-    //       filedBy: 'Test Petitioner',
-    //       pk: `case|${MOCK_CASE.docketNumber}`,
-    //       sk: 'docket-entry|83b77e98-4cf6-4fb4-b8c0-f5f90fd68f3c',
-    //       userId: '8bbfcd5a-b02b-4983-8e9c-6cc50d3d566c',
-    //     },
-    //   ];
+  it('should return and not modify records that are NOT trial session records', async () => {
+    const items = [
+      {
+        noticeOfTrialDate: '2025-04-10T04:00:00.000Z',
+        pk: `case|${MOCK_CASE.docketNumber}`,
+        sk: 'user|6d74eadc-0181-4ff5-826c-305200e8733d',
+      },
+      {
+        noticeOfTrialDate: '2025-04-10T04:00:00.000Z',
+        pk: `case|${MOCK_CASE.docketNumber}`,
+        sk: 'docket-entry|83b77e98-4cf6-4fb4-b8c0-f5f90fd68f3c',
+      },
+    ];
+    const results = await migrateItems(items);
 
-    //   documentClient = {
-    //     query: jest.fn().mockImplementation(() => ({
-    //       promise: async () => ({
-    //         Items: [mockTrialSessionItem, ...mockDocketEntryItems],
-    //       }),
-    //     })),
-    //   };
-    // });
+    expect(results).toEqual([
+      {
+        noticeOfTrialDate: '2025-04-10T04:00:00.000Z', // field doesn't belong here, but proving it is unmodified
+        pk: `case|${MOCK_CASE.docketNumber}`,
+        sk: 'user|6d74eadc-0181-4ff5-826c-305200e8733d',
+      },
+      {
+        noticeOfTrialDate: '2025-04-10T04:00:00.000Z', // field doesn't belong here, but proving it is unmodified
+        pk: `case|${MOCK_CASE.docketNumber}`,
+        sk: 'docket-entry|83b77e98-4cf6-4fb4-b8c0-f5f90fd68f3c',
+      },
+    ]);
+  });
 
-    // it('should return and not modify records that are NOT case records', async () => {
-    //   const items = [
-    //     {
-    //       noticeOfTrialDate: '2025-04-10T04:00:00.000Z',
-    //       pk: `case|${MOCK_CASE.docketNumber}`,
-    //       sk: 'user|6d74eadc-0181-4ff5-826c-305200e8733d',
-    //     },
-    //     {
-    //       noticeOfTrialDate: '2025-04-10T04:00:00.000Z',
-    //       pk: `case|${MOCK_CASE.docketNumber}`,
-    //       sk: 'docket-entry|83b77e98-4cf6-4fb4-b8c0-f5f90fd68f3c',
-    //     },
-    //   ];
-    //   const results = await migrateItems(items, documentClient);
+  it('should set sessionScope to locationBased if the item does not have one', async () => {
+    const items = [
+      {
+        ...mockTrialSessionItem,
+        sessionScope: undefined,
+      },
+    ];
 
-    //   expect(results).toEqual([
-    //     {
-    //       noticeOfTrialDate: '2025-04-10T04:00:00.000Z', // field doesn't belong here, but proving it is unmodified
-    //       pk: `case|${MOCK_CASE.docketNumber}`,
-    //       sk: 'user|6d74eadc-0181-4ff5-826c-305200e8733d',
-    //     },
-    //     {
-    //       noticeOfTrialDate: '2025-04-10T04:00:00.000Z', // field doesn't belong here, but proving it is unmodified
-    //       pk: `case|${MOCK_CASE.docketNumber}`,
-    //       sk: 'docket-entry|83b77e98-4cf6-4fb4-b8c0-f5f90fd68f3c',
-    //     },
-    //   ]);
+    const results = await migrateItems(items);
 
-    //   expect(documentClient.query).not.toHaveBeenCalled();
-    // });
+    expect(results[0].sessionScope).toBe(
+      TRIAL_SESSION_SCOPE_TYPES.locationBased,
+    );
+  });
 
-    // it('should unset the noticeOfTrialDate field for the given case if a notice of trial IS NOT found on the docket record', async () => {
-    //   const items = [
-    //     {
-    //       ...mockTrialSessionItem,
-    //       noticeOfTrialDate: '2025-04-10T04:00:00.000Z',
-    //     },
-    //   ];
+  it('should not set sessionScope if the item is a trial session and has a sessionScope', async () => {
+    const items = [
+      {
+        ...mockTrialSessionItem,
+        sessionScope: TRIAL_SESSION_SCOPE_TYPES.standaloneRemote,
+      },
+    ];
 
-    //   const results = await migrateItems(items, documentClient);
+    const results = await migrateItems(items);
 
-    //   expect(results[0].noticeOfTrialDate).toBeUndefined();
-    // });
-
-    // it('should NOT unset the noticeOfTrialDate field for the given case if a notice of trial IS found on the docket record', async () => {
-    //   mockDocketEntryItems.push({
-    //     archived: false,
-    //     docketEntryId: '99977e98-4cf6-4fb4-b8c0-f5f90fd68f3c',
-    //     documentType: SYSTEM_GENERATED_DOCUMENT_TYPES.noticeOfTrial.documentType,
-    //     eventCode: SYSTEM_GENERATED_DOCUMENT_TYPES.noticeOfTrial.eventCode,
-    //     filedBy: 'Test Petitioner',
-    //     pk: `case|${MOCK_CASE.docketNumber}`,
-    //     sk: 'docket-entry|99977e98-4cf6-4fb4-b8c0-f5f90fd68f3c',
-    //     userId: '8bbfcd5a-b02b-4983-8e9c-6cc50d3d566c',
-    //   });
-
-    //   const items = [
-    //     {
-    //       ...mockTrialSessionItem,
-    //       noticeOfTrialDate: '2025-04-10T04:00:00.000Z',
-    //     },
-    //   ];
-
-    //   const results = await migrateItems(items, documentClient);
-
-    //   expect(results[0].noticeOfTrialDate).toEqual('2025-04-10T04:00:00.000Z');
+    expect(results[0].sessionScope).toBe(
+      TRIAL_SESSION_SCOPE_TYPES.standaloneRemote,
+    );
   });
 });
