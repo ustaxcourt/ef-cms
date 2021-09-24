98c2de192c709ae5ac430e6f80458d0324df6170
Rachel Schneiderman <rschneiderman@flexion.us>
2021-09-24 13:12:56 -0400

    8511: [skip ci] Create reindex-status lambda, doesnt do anything yet

--- webpack.config.lambda.migration.js
+++ webpack.config.lambda.migration.js
@@ -6,6 +6,8 @@ module.exports = {
     migration: './web-api/migration-terraform/main/lambdas/migration.js',
     'migration-segments':
       './web-api/migration-terraform/main/lambdas/migration-segments.js',
+    'reindex-status':
+      './web-api/migration-terraform/main/lambdas/reindex-status.js',
   },
   externals: ['aws-sdk'],
   output: {
