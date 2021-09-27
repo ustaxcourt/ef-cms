77ae4fbab312f6d9c6b8e0a97da71e4c9d441a54
rosie highsmith <ahighsmith@flexion.us>
2021-09-27 09:40:25 -0700

    8511: adding enable-reindex-cron.sh

---
+++ enable-reindex-cron.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+[ -z "${ENV}" ] && echo "You must have ENV set in your environment" && exit 1
+[ -z "${AWS_ACCOUNT_ID}" ] && echo "You must have AWS_ACCOUNT_ID set in your environment" && exit 1
+[ -z "${DEPLOYING_COLOR}" ] && echo "You must have DEPLOYING_COLOR set in your environment" && exit 1
+
+aws events enable-rule --name "check_reindex_status_cron_${ENV}_${DEPLOYING_COLOR}" --region us-east-1
