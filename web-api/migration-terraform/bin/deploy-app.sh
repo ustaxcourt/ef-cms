7faa69c39fdd7a3ed6172011f5d9a5f20fc71b5b
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2021-09-27 09:57:19 -0600

    8511 adding tf code for setting up cron job

--- web-api/migration-terraform/bin/deploy-app.sh
+++ web-api/migration-terraform/bin/deploy-app.sh
@@ -47,7 +47,8 @@ export TF_VAR_source_table=$SOURCE_TABLE
 export TF_VAR_destination_table=$DESTINATION_TABLE
 export TF_VAR_dns_domain=$EFCMS_DOMAIN
 export TF_VAR_documents_bucket_name=$DOCUMENTS_BUCKET_NAME
+export TF_VAR_deploying_color=$DEPLOYING_COLOR
 
-# terraform init -backend=true -backend-config=bucket="${BUCKET}" -backend-config=key="${KEY}" -backend-config=dynamodb_table="${LOCK_TABLE}" -backend-config=region="${REGION}"
-# terraform plan
-# terraform apply -auto-approve
+terraform init -backend=true -backend-config=bucket="${BUCKET}" -backend-config=key="${KEY}" -backend-config=dynamodb_table="${LOCK_TABLE}" -backend-config=region="${REGION}"
+terraform plan
+terraform apply -auto-approve
