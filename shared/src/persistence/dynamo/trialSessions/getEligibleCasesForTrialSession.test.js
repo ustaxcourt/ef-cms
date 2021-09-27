5df19bcad33372951ef877de8e944bc366638b0a
Michael Marcotte <mike.marcotte@gmail.com>
2021-09-27 15:20:01 -0400

    De-dupe the eligible case docket numbers... that we receive from the query of mapping records for the given Trial Location prefix.
    We are working to address the underlying data issue that causes this.

--- shared/src/persistence/dynamo/trialSessions/getEligibleCasesForTrialSession.test.js
+++ shared/src/persistence/dynamo/trialSessions/getEligibleCasesForTrialSession.test.js
@@ -47,4 +47,32 @@ describe('getEligibleCasesForTrialSession', () => {
       },
     ]);
   });
+
+  it('should remove any duplicate cases it receives the list of cases it receives from the mapping records query', async () => {
+    client.query = jest.fn().mockReturnValue([
+      {
+        docketNumber: MOCK_CASE.docketNumber,
+        pk: 'eligible-for-trial-case-catalog',
+        sk: 'WashingtonDistrictofColumbia-R-D-20181212654321-101-18',
+      },
+      {
+        docketNumber: MOCK_CASE.docketNumber,
+        pk: 'eligible-for-trial-case-catalog',
+        sk: 'WashingtonDistrictofColumbia-R-D-20181212000000-101-18',
+      },
+    ]);
+
+    await getEligibleCasesForTrialSession({
+      applicationContext,
+    });
+    expect(client.batchGet).toHaveBeenCalledWith({
+      applicationContext,
+      keys: [
+        {
+          pk: `case|${MOCK_CASE.docketNumber}`,
+          sk: `case|${MOCK_CASE.docketNumber}`,
+        },
+      ],
+    });
+  });
 });
