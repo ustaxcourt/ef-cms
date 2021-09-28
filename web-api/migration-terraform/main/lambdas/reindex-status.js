29b2f6fe3cfb255b58902b29366ef1b25aff8730
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2021-09-28 11:43:25 -0600

    8511 [skip ci] updating check-reindex-complete to return a bool

--- web-api/migration-terraform/main/lambdas/reindex-status.js
+++ web-api/migration-terraform/main/lambdas/reindex-status.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
+const {
+  isReindexComplete,
+} = require('../../../../shared/admin-tools/elasticsearch/check-reindex-complete');
 const { find } = require('lodash');
-
-// Using the circle_workflow_id, get all jobs in the workflow, then with that grab the approval_request_id (which is the pending), then send command to approve job
 
 exports.handler = async () => {
   // TODO: Do something when this script execution exits with code 0 (approve circle CI workflow)
@@ -9,15 +10,18 @@ exports.handler = async () => {
   // exec('sh ./wait-for-reindex-to-finish.sh', (error, stdout, stderr) => {
   //   console.log(stdout);
   //   console.log(stderr);
+
   //   if (error !== null) {
   //     console.log(`exec error: ${error}`);
   //   }
   // });
   //TODO dont hardcode api token
-  const personalApiToken = '1234';
-  const workflowId = '1234';
 
-  //get all jobs in the workflow
+  await isReindexComplete();
+
+  const personalApiToken = '1234';
+  const workflowId = process.env.CIRCLE_WORKFLOW_ID;
+
   const get_all_jobs = {
     headers: { 'Circle-Token': personalApiToken },
     method: 'GET',
@@ -33,13 +37,12 @@ exports.handler = async () => {
     },
   );
 
-  //then make the req below
   const approve_job = {
     headers: { 'Circle-Token': personalApiToken },
     method: 'POST',
     url: `https://circleci.com/api/v2/workflow/${workflowId}/approve/${jobWithApprovalNeeded.approval_request_id}`,
   };
-  const response = await axios.post(approve_job.url, {}, approve_job);
+  return await axios.post(approve_job.url, {}, approve_job);
 };
 
 exports.handler();
