3026cdba23ef7c0924245a66537a4032d674ee26
Rachel Schneiderman <rschneiderman@flexion.us>
2021-09-29 08:56:26 -0700

    8511: Create disable step in circleci for reindex cron job [skip ci]

--- disable-reindex-cron.sh
+++ disable-reindex-cron.sh
@@ -3,4 +3,4 @@
 [ -z "${ENV}" ] && echo "You must have ENV set in your environment" && exit 1
 [ -z "${DEPLOYING_COLOR}" ] && echo "You must have DEPLOYING_COLOR set in your environment" && exit 1
 
-aws events enable-rule --name "check_reindex_status_cron_${ENV}_${DEPLOYING_COLOR}" --region us-east-1
+aws events disable-rule --name "check_reindex_status_cron_${ENV}_${DEPLOYING_COLOR}" --region us-east-1
