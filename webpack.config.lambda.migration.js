0f57c850975d573d20f44400fcdfdd120c0da917
rosie highsmith <ahighsmith@flexion.us>
2021-09-29 09:18:07 -0700

    Revert "8511: Bring isReindexComplete into the lambda [skip ci]"
    This reverts commit b25efb59d9fe1b21f37fd9ca75dcbb65f00397b2.

--- webpack.config.lambda.migration.js
+++ webpack.config.lambda.migration.js
@@ -1,4 +1,5 @@
 const config = require('./webpack.config.lambda');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   ...config,
@@ -15,4 +16,14 @@ module.exports = {
     libraryTarget: 'umd',
     path: __dirname + '/web-api/migration-terraform/main/lambdas/dist',
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './shared/admin-tools/elasticsearch/check-reindex-complete.js',
+          to: '.',
+        },
+      ],
+    }),
+  ],
 };
