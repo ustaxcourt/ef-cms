a05f4634a141bbd9f259816619ad90458e0693ae
Eric Sorenson <eric@sorenson.software>
2021-09-30 11:49:00 -0500

    8511 test coverage

--- web-api/migration-cron-terraform/main/lambdas/reindex-status.test.js
+++ web-api/migration-cron-terraform/main/lambdas/reindex-status.test.js
@@ -10,38 +10,43 @@ jest.mock(
 );
 jest.mock('axios');
 
-const applicationContext = require('../../../src/applicationContext');
 const axios = require('axios');
 
 describe('reindex-status', () => {
-  const mockJobs = [];
-  let mockEnvironment = {
-    CIRCLE_MACHINE_USER_TOKEN: '7806d03d-075d-46a3-a5fc-5d0e439f5d8e',
-    CIRCLE_WORKFLOW_ID: '7806d03d-075d-46a3-a5fc-5d0e439f5d8e',
-    MIGRATE_FLAG: 'false',
-  };
+  const mockJobs = [{}, { approval_request_id: 'MOCK_APPROVAL_REQUEST_ID' }];
 
-  beforeEach(() => {
-    applicationContext.logger = jest.fn();
-    //mock env vars
-    process.env = mockEnvironment;
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should make get and post requests if needsMigration is false and isReindexFinished is true', async () => {
+    process.env.MIGRATE_FLAG = false;
     isReindexComplete.mockReturnValueOnce(true);
     axios.post.mockImplementation(() => Promise.resolve({ status: 200 }));
     axios.get.mockImplementation(() =>
-      Promise.resolve({ data: { items: [mockJobs] } }),
+      Promise.resolve({ data: { items: mockJobs } }),
     );
-  });
 
-  it('should return 200 if needsMigration is false', async () => {
-    const result = await handler();
-
+    await handler();
     expect(axios.get).toHaveBeenCalledTimes(1);
     expect(axios.post).toHaveBeenCalledTimes(1);
-    expect(result.status).toBe(200);
   });
 
-  it('should return 200 if needsMigration is true and isReindexFinished is complete', async () => {});
-  it('should not make get and post requests when needsMigration is true and isReindexFinished is not complete', async () => {});
+  it('should make get and post requests if needsMigration is true and isReindexFinished is true', async () => {
+    process.env.MIGRATE_FLAG = true;
+    isReindexComplete.mockReturnValueOnce(true);
 
-  // it('should return 200 if isReindexFinished is complete', async () => {});
+    await handler();
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(axios.post).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not make get and post requests when needsMigration is true and isReindexFinished is false', async () => {
+    process.env.MIGRATE_FLAG = true;
+    isReindexComplete.mockReturnValueOnce(false);
+
+    await handler();
+    expect(axios.get).toHaveBeenCalledTimes(0);
+    expect(axios.post).toHaveBeenCalledTimes(0);
+  });
 });
