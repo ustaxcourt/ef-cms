fe9f69dbaaed75564367e5d85b1c0c3303b1939d
rosie highsmith <ahighsmith@flexion.us>
2021-10-06 09:51:59 -0700

    8919: cleanup, calling correct interactor [skip ci]

--- web-client/src/presenter/sequences/CaseAssociation/validateEditRespondentCounselSequence.js
+++ web-client/src/presenter/sequences/CaseAssociation/validateEditRespondentCounselSequence.js
@@ -1,7 +1,7 @@
 import { clearAlertsAction } from '../../actions/clearAlertsAction';
 import { setValidationErrorsAction } from '../../actions/setValidationErrorsAction';
 import { shouldValidateAction } from '../../actions/shouldValidateAction';
-import { validateEditRespondentCounselAction } from '../../actions/CaseAssociation/validateEditRespondentCounselAction';
+import { validateEditRespondentCounselAction } from '../../actions/caseAssociation/validateEditRespondentCounselAction';
 
 export const validateEditRespondentCounselSequence = [
   shouldValidateAction,
