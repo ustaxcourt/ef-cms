8bfda032d377a36ea15d54d39c89f0bf4d2d048e
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2021-10-07 10:43:32 -0600

    docs

--- shared/src/proxies/trialSessions/closeTrialSessionProxy.js
+++ shared/src/proxies/trialSessions/closeTrialSessionProxy.js
@@ -1,12 +1,10 @@
 const { post } = require('../requests');
 
 /**
- * addCaseToTrialSessionInteractor
+ * closeTrialSessionInteractor
  *
  * @param {object} applicationContext the application context
  * @param {object} providers the providers object
- * @param {string} providers.calendarNotes notes on why the case was added to the trial session
- * @param {object} providers.docketNumber the case docket number
  * @param {object} providers.trialSessionId the trial session id
  * @returns {Promise<*>} the promise of the api call
  */
