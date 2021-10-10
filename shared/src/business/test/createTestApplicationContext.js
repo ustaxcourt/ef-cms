952f1e7c8eea0965eebc4fdafc650ab2b233dc74
rosie highsmith <ahighsmith@flexion.us>
2021-10-10 14:43:07 -0700

    8919: revert mockImplementation of isFeatureEnabled and isolate it to the integration test.

--- shared/src/business/test/createTestApplicationContext.js
+++ shared/src/business/test/createTestApplicationContext.js
@@ -179,7 +179,6 @@ const { filterEmptyStrings } = require('../utilities/filterEmptyStrings');
 const { formatDollars } = require('../utilities/formatDollars');
 const { getConstants } = require('../../../../web-client/src/getConstants');
 const { getCropBox } = require('../../../src/business/utilities/getCropBox');
-const { getIsFeatureEnabled } = require('../utilities/getIsFeatureEnabled');
 const { getItem } = require('../../persistence/localStorage/getItem');
 const { getServedPartiesCode, isServed } = require('../entities/DocketEntry');
 const { removeItem } = require('../../persistence/localStorage/removeItem');
@@ -619,7 +618,7 @@ const createTestApplicationContext = ({ user } = {}) => {
     getUseCaseHelpers: mockGetUseCaseHelpers,
     getUseCases: emptyAppContextProxy,
     getUtilities: mockGetUtilities,
-    isFeatureEnabled: jest.fn().mockImplementation(getIsFeatureEnabled),
+    isFeatureEnabled: jest.fn(),
     logger: {
       debug: jest.fn(),
       error: jest.fn(),
