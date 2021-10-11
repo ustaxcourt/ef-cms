2f408a0ae515e3bb272171553fdae2caace7c051
Rachel Schneiderman <rschneiderman@flexion.us>
2021-10-11 18:28:19 -0400

    Devex 622: Delete ALL items in s3 buckets, not just first 1000

--- env-destroy-utils/deleteS3Buckets.js
+++ env-destroy-utils/deleteS3Buckets.js
@@ -10,47 +10,75 @@ exports.deleteS3Buckets = async ({ environment }) => {
   });
 
   for (const bucket of buckets) {
-    console.log(
-      'Deleting items from S3 Bucket:',
-      environment.region,
-      bucket.Name,
-    );
-
+    let numItems;
     const objects = await s3
-      .listObjects({ Bucket: bucket.Name, MaxKeys: 1000 })
+      .listObjects({
+        Bucket: bucket.Name,
+        MaxKeys: 1000,
+      })
       .promise();
 
     if (objects.Contents.length > 0) {
-      await s3
-        .deleteObjects({
-          Bucket: bucket.Name,
-          Delete: {
-            Objects: objects.Contents.map(object => {
-              return { Key: object.Key };
-            }),
-          },
-        })
-        .promise();
+      do {
+        console.log(
+          'Deleting items from S3 Bucket:',
+          environment.region,
+          bucket.Name,
+        );
+
+        await s3
+          .deleteObjects({
+            Bucket: bucket.Name,
+            Delete: {
+              Objects: objects.Contents.map(object => {
+                return { Key: object.Key };
+              }),
+            },
+          })
+          .promise();
+
+        const moreObjects = await s3
+          .listObjects({
+            Bucket: bucket.Name,
+            MaxKeys: 1000,
+          })
+          .promise();
+        numItems = moreObjects.Contents.length;
+      } while (numItems > 0);
     }
 
+    let numVersionItems;
     const objectVersions = await s3
       .listObjectVersions({ Bucket: bucket.Name, MaxKeys: 1000 })
       .promise();
 
     if (objectVersions.Versions.length > 0) {
-      await s3
-        .deleteObjects({
-          Bucket: bucket.Name,
-          Delete: {
-            Objects: objectVersions.Versions.map(objectVersion => {
-              return {
-                Key: objectVersion.Key,
-                VersionId: objectVersion.VersionId,
-              };
-            }),
-          },
-        })
-        .promise();
+      do {
+        console.log(
+          'Deleting version items from S3 Bucket:',
+          environment.region,
+          bucket.Name,
+        );
+
+        await s3
+          .deleteObjects({
+            Bucket: bucket.Name,
+            Delete: {
+              Objects: objectVersions.Versions.map(objectVersion => {
+                return {
+                  Key: objectVersion.Key,
+                  VersionId: objectVersion.VersionId,
+                };
+              }),
+            },
+          })
+          .promise();
+
+        const moreObjectVersions = await s3
+          .listObjectVersions({ Bucket: bucket.Name, MaxKeys: 1000 })
+          .promise();
+        numVersionItems = moreObjectVersions.Versions.length;
+      } while (numVersionItems > 0);
     }
   }
 };
