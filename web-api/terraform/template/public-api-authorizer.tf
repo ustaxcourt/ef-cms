725c9394620c3354c8ffae5dbc792d177c4ba60a
Cody Seibert <cseibert@flexion.us>
2021-10-13 15:18:34 -0400

    8955 - fixing issue with role

--- web-api/terraform/template/public-api-authorizer.tf
+++ web-api/terraform/template/public-api-authorizer.tf
@@ -7,7 +7,7 @@ data "archive_file" "zip_public_authorizer" {
 resource "aws_lambda_function" "public_api_authorizer_lambda" {
   filename         = data.archive_file.zip_public_authorizer.output_path
   function_name    = "public_api_authorizer_lambda_${var.environment}"
-  role             = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/public_api_authorizer_lambda_role_${var.environment}"
+  role             = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/public_api_authorizer_role_${var.environment}"
   handler          = "public-api-authorizer.handler"
   source_code_hash = data.archive_file.zip_public_authorizer.output_base64sha256
 
@@ -25,7 +25,7 @@ resource "aws_lambda_function" "public_api_authorizer_lambda" {
 resource "aws_lambda_function" "public_api_authorizer_lambda_west" {
   filename         = data.archive_file.zip_public_authorizer.output_path
   function_name    = "public_api_authorizer_lambda_${var.environment}"
-  role             = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/public_api_authorizer_lambda_role_${var.environment}"
+  role             = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/public_api_authorizer_role_${var.environment}"
   handler          = "public-api-authorizer.handler"
   source_code_hash = data.archive_file.zip_public_authorizer.output_base64sha256
 
