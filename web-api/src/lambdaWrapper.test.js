5ca56c8a97e4db8827310d5eaa221368c32f4138
Cody Seibert <cseibert@flexion.us>
2021-10-15 11:03:06 -0400

    8955 - fixing tests

--- web-api/src/lambdaWrapper.test.js
+++ web-api/src/lambdaWrapper.test.js
@@ -43,7 +43,7 @@ describe('lambdaWrapper', () => {
       Pragma: 'no-cache',
       Vary: 'Authorization',
       'X-Content-Type-Options': 'nosniff',
-      'X-Terminal-User': false,
+      'X-Terminal-User': undefined,
     });
   });
 
