e16257a77e9cac231532e7d576d11422ba3de822
Michael Marcotte <mike.marcotte@gmail.com>
2021-10-15 12:50:46 -0400

    ustc-1739: fix tests for put calls

--- shared/src/persistence/dynamo/workitems/saveWorkItemForDocketClerkFilingExternalDocument.js
+++ shared/src/persistence/dynamo/workitems/saveWorkItemForDocketClerkFilingExternalDocument.js
@@ -7,7 +7,7 @@ const { put } = require('../../dynamodbClientService');
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
  * @param {object} providers.workItem the work item data
- * @returns {Promise} resolves upon completion of persistence request
+ * @returns {Promise} resolves upon completion of persistence requests
  */
 exports.saveWorkItemForDocketClerkFilingExternalDocument = ({
   applicationContext,
