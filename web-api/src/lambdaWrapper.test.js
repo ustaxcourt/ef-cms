932855f35ec30802d23629ccdcd1d36dff55fdf8
TElliott <telliott@flexion.us>
2021-10-19 18:13:09 -0500

    8955: Revert test change

--- web-api/src/lambdaWrapper.test.js
+++ web-api/src/lambdaWrapper.test.js
@@ -43,7 +43,7 @@ describe('lambdaWrapper', () => {
       Pragma: 'no-cache',
       Vary: 'Authorization',
       'X-Content-Type-Options': 'nosniff',
-      'X-Terminal-User': undefined,
+      'X-Terminal-User': false,
     });
   });
 
