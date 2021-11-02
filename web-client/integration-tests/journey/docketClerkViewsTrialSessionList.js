e2a47cc6561e6117111b300b4ff3b36c792103db
Absolutestunna <joshujah007@gmail.com>
2021-11-02 17:21:29 -0400

    9068 - removed unnecessary test based on new prop format

--- web-client/integration-tests/journey/docketClerkViewsTrialSessionList.js
+++ web-client/integration-tests/journey/docketClerkViewsTrialSessionList.js
@@ -1,7 +1,6 @@
 import { find } from 'lodash';
 import { formattedTrialSessions as formattedTrialSessionsComputed } from '../../src/presenter/computeds/formattedTrialSessions';
 import { runCompute } from 'cerebral/test';
-import { trialCitiesHelper as trialCitiesHelperComputed } from '../../src/presenter/computeds/trialCitiesHelper';
 import { withAppContextDecorator } from '../../src/withAppContext';
 
 const formattedTrialSessions = withAppContextDecorator(
@@ -12,15 +11,6 @@ export const docketClerkViewsTrialSessionList = cerebralTest => {
   return it('Docket clerk views trial session list', async () => {
     await cerebralTest.runSequence('gotoTrialSessionsSequence');
     expect(cerebralTest.getState('currentPage')).toEqual('TrialSessions');
-
-    const result = await runCompute(
-      withAppContextDecorator(trialCitiesHelperComputed),
-    )('AllPlusStandalone');
-
-    expect(result.trialCitiesByState[0]).toEqual('Standalone Remote');
-    expect(
-      result.trialCitiesByState[result.trialCitiesByState.length - 1],
-    ).toEqual({ cities: ['Cheyenne, Wyoming'], state: 'Wyoming' });
 
     const formatted = runCompute(formattedTrialSessions, {
       state: cerebralTest.getState(),
