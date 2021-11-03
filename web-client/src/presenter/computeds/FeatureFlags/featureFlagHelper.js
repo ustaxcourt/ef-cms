09f074c6aacaee7fab79fcb04f91f17276caa913
Rachel Schneiderman <rschneiderman@flexion.us>
2021-11-03 11:25:04 -0400

    9066: WIP refactor featureFlagHelper [skip ci]

--- web-client/src/presenter/computeds/FeatureFlags/featureFlagHelper.js
+++ web-client/src/presenter/computeds/FeatureFlags/featureFlagHelper.js
@@ -5,17 +5,21 @@ export const featureFlagHelper = (get, applicationContext) => {
 
   const isUserInternal = applicationContext.getUtilities().isInternalUser(role);
   const isOrderSearchEnabled = get(state.isOrderSearchEnabled);
-  const isInternalOrderSearchEnabled = isOrderSearchEnabled && isUserInternal;
 
-  const isExternalOrderSearchEnabled = get(state.isExternalOrderSearchEnabled);
+  let isOrderSearchEnabledForRole = false;
+  if (isUserInternal) {
+    isOrderSearchEnabledForRole = isOrderSearchEnabled;
+  } else {
+    isOrderSearchEnabledForRole = get(state.isExternalOrderSearchEnabled);
+  }
 
   const isOpinionSearchEnabled = applicationContext.isFeatureEnabled(
     'advanced_opinion_search',
   );
 
   return {
-    isExternalOrderSearchEnabled,
-    isInternalOrderSearchEnabled,
     isOpinionSearchEnabled,
+    // isExternalOrderSearchEnabled,
+    isOrderSearchEnabledForRole,
   };
 };
