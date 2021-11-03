7568a7a83fe1c303e0f4065f94a6ceafccdd2a1c
Rachel Schneiderman <rschneiderman@flexion.us>
2021-11-03 13:59:42 -0400

    9066: WIP script to update deploy table values for order feature flags [skip ci]

--- scripts/rename-order-search-flag-to-internal.sh
+++ scripts/rename-order-search-flag-to-internal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Sets the order search enabled flag to "true" in the dynamo deploy table
+# Rename order-search-enabled flag to internal-order-search-enabled
 
 # Usage
 #   ./setup-internal-order-search-flag.sh dev
@@ -12,5 +12,8 @@
 
 ENV=$1
 
-aws dynamodb put-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --item '{"pk":{"S":"internal-order-search-enabled "},"sk":{"S":"internal-order-search-enabled "},"current":{"BOOL":true}}'
+CURRENT_INTERNAL_ORDER_SEARCH_VALUE=$(aws dynamodb get-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --key '{"pk":{"S":"order-search-enabled"},"sk":{"S":"order-search-enabled"}}' | jq -r ".Item.current.BOOL")
 
+aws dynamodb put-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --item '{"pk":{"S":"internal-order-search-enabled "},"sk":{"S":"internal-order-search-enabled "},"current":{"BOOL": '${CURRENT_INTERNAL_ORDER_SEARCH_VALUE}'}}'
+
+aws dynamodb delete-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --key '{"pk":{"S":"order-search-enabled "},"sk":{"S":"order-search-enabled "}}'
