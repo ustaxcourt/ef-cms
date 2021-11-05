2fa44045118f23bb6ddfd8ca28668f0a348a0c19
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2021-11-05 13:34:06 -0600

    [skip ci] more pa11y

--- web-client/pa11y/pa11y-ci-public.config.js
+++ web-client/pa11y/pa11y-ci-public.config.js
@@ -1,4 +1,5 @@
 const publicUser = require('./pa11y-public-user');
+const publicUserSearch = require('./pa11y-public-user-search');
 const { defaults, jsCheckDecorator } = require('./pa11y-ci.base-config.js');
 
 const initialUrls = [
@@ -6,7 +7,9 @@ const initialUrls = [
   'http://localhost:5678/case-detail/101-19/printable-docket-record',
 ];
 
-const urls = [...initialUrls, ...publicUser].map(jsCheckDecorator);
+const urls = [...initialUrls, ...publicUser, ...publicUserSearch].map(
+  jsCheckDecorator,
+);
 
 module.exports = {
   defaults,
