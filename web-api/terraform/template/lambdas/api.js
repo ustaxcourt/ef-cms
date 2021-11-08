dd3f09eb14bd55bf5b780a78b8e70f428c5469ba
Rachel Schneiderman <rschneiderman@flexion.us>
2021-11-08 12:00:31 -0500

    Fix package upgrade issue

--- web-api/terraform/template/lambdas/api.js
+++ web-api/terraform/template/lambdas/api.js
@@ -1,9 +1,4 @@
 const awsServerlessExpress = require('@vendia/serverless-express');
 const { app } = require('../../../src/app');
-const server = awsServerlessExpress({ app });
 
-exports.handler = (event, context) => {
-  event.body =
-    typeof event.body === 'string' ? event.body : JSON.stringify(event.body);
-  awsServerlessExpress.proxy(server, event, context);
-};
+exports.handler = awsServerlessExpress({ app }).handler;
