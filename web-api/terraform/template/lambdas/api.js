a1c4dede6349c0b10306218729e5c70fd27a32b9
Cody Seibert <cseibert113@gmail.com>
2021-11-08 17:04:11 -0500

    trying to hard code event source name

--- web-api/terraform/template/lambdas/api.js
+++ web-api/terraform/template/lambdas/api.js
@@ -1,4 +1,7 @@
 const awsServerlessExpress = require('@vendia/serverless-express');
 const { app } = require('../../../src/app');
 
-exports.handler = awsServerlessExpress({ app });
+exports.handler = awsServerlessExpress({
+  app,
+  eventSourceName: 'AWS_API_GATEWAY_V1', // the serverless-express library has a bug, so we need to hard code this for now
+});
