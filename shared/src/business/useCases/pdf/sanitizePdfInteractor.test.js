84a0531e0abdd4fbc5943d6b51749f56db5fba4d
rosie highsmith <ahighsmith@flexion.us>
2021-11-09 09:54:08 -0800

    9123: added test for sanitizePdfInteractor [skip ci]

--- shared/src/business/useCases/pdf/sanitizePdfInteractor.test.js
+++ shared/src/business/useCases/pdf/sanitizePdfInteractor.test.js
@@ -1,19 +1,19 @@
 const {
   applicationContext,
-  testInvalidPdfDoc,
   testPdfDoc,
 } = require('../../test/createTestApplicationContext');
 const { sanitizePdfInteractor } = require('./sanitizePdfInteractor');
 
 describe('sanitizePdfInteractor', () => {
-  const getPagesMock = jest.fn();
+  const saveMock = jest.fn();
 
   beforeEach(() => {
     applicationContext.getPdfLib = jest.fn().mockResolvedValue({
       PDFDocument: {
         load: jest.fn().mockResolvedValue({
-          getPages: getPagesMock,
+          getForm: () => ({ flatten: jest.fn() }),
           isEncrypted: false,
+          save: saveMock,
         }),
       },
     });
@@ -27,97 +27,16 @@ describe('sanitizePdfInteractor', () => {
     applicationContext.getPersistenceGateway().deleteDocumentFromS3 = jest.fn();
   });
 
-  it('sanitizes a clean PDF', async () => {
+  it('sanitizes a clean PDF and saves it to S3', async () => {
+    const mockKey = 'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859';
     const result = await sanitizePdfInteractor(applicationContext, {
-      key: 'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
+      key: mockKey,
     });
     expect(result).toBeTruthy();
-  });
-
-  it('throws an error and deletes the document from S3 when the PDF is encrypted', async () => {
-    applicationContext.getPdfLib = jest.fn().mockResolvedValue({
-      PDFDocument: {
-        load: jest.fn().mockResolvedValue({
-          isEncrypted: true,
-        }),
-      },
-    });
-
-    await expect(
-      sanitizePdfInteractor(applicationContext, {
-        key: 'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
-      }),
-    ).rejects.toThrow('invalid pdf');
-
+    expect(saveMock).toHaveBeenCalled();
     expect(
-      applicationContext.getPersistenceGateway().deleteDocumentFromS3,
-    ).toHaveBeenCalledWith({
-      applicationContext,
-      key: 'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
-    });
-
-    expect(applicationContext.logger.debug.mock.calls[3][0]).toEqual(
-      'PDF Invalid: Deleting from S3',
-    );
-
-    expect(applicationContext.logger.debug.mock.calls[3][1]).toEqual(
-      'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
-    );
-  });
-
-  it('throws an error and deletes the document from S3 when the PDF is actually a png', async () => {
-    applicationContext.getStorageClient().getObject.mockReturnValue({
-      promise: () => ({
-        Body: testInvalidPdfDoc,
-      }),
-    });
-
-    await expect(
-      sanitizePdfInteractor(applicationContext, {
-        key: 'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
-      }),
-    ).rejects.toThrow('invalid pdf');
-
-    expect(
-      applicationContext.getPersistenceGateway().deleteDocumentFromS3,
-    ).toHaveBeenCalledWith({
-      applicationContext,
-      key: 'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
-    });
-
-    expect(applicationContext.logger.debug.mock.calls[3][0]).toEqual(
-      'PDF Invalid: Deleting from S3',
-    );
-
-    expect(applicationContext.logger.debug.mock.calls[3][1]).toEqual(
-      'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
-    );
-  });
-
-  it('throws an error and deletes the document from s3 when pdf pages cannot be read', async () => {
-    getPagesMock.mockImplementation(() => {
-      throw new Error('cannot read pages');
-    });
-
-    await expect(
-      sanitizePdfInteractor(applicationContext, {
-        key: 'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
-      }),
-    ).rejects.toThrow('pdf pages cannot be read');
-
-    expect(
-      applicationContext.getPersistenceGateway().deleteDocumentFromS3,
-    ).toHaveBeenCalledWith({
-      applicationContext,
-      key: 'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
-    });
-
-    expect(applicationContext.logger.debug.mock.calls[3][0]).toEqual(
-      'PDF Pages Unreadable: Deleting from S3',
-    );
-
-    expect(applicationContext.logger.debug.mock.calls[3][1]).toEqual(
-      'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
-    );
+      applicationContext.getPersistenceGateway().saveDocumentFromLambda.mock
+        .calls[0][0].key,
+    ).toBe(mockKey);
   });
 });
