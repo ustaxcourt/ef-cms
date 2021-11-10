21500015bcce58b86464f2e7a3b4f0a678281532
Cody Seibert <cseibert113@gmail.com>
2021-11-10 10:13:36 -0500

    adding back event.body hack

--- web-api/terraform/template/lambdas/api.js
+++ web-api/terraform/template/lambdas/api.js
@@ -1,6 +1,10 @@
 const awsServerlessExpress = require('@vendia/serverless-express');
 const { app } = require('../../../src/app');
 
-exports.handler = awsServerlessExpress({
-  app,
-});
+exports.handler = (event, context) => {
+  event.body =
+    typeof event.body === 'string' ? event.body : JSON.stringify(event.body);
+  return awsServerlessExpress({
+    app,
+  })(event, context);
+};
