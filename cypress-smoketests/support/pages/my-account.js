019159ea9c3252673a31aad82ecfadf69923c408
Rachel Schneiderman <rschneiderman@flexion.us>
2021-11-10 10:01:54 -0500

    Add smoketest to cover change of address

---
+++ cypress-smoketests/support/pages/my-account.js
@@ -0,0 +1,20 @@
+const faker = require('faker');
+
+exports.goToMyAccount = () => {
+  cy.get('svg.account-menu-icon').parent().parent().click();
+  cy.get('button#my-account').click();
+};
+
+exports.goToEditContactInformation = () => {
+  cy.get('button.ustc-button--unstyled').contains('Edit').click();
+};
+
+exports.updateAddress1 = () => {
+  cy.get('input[id=contact\\.address1]')
+    .clear()
+    .type(faker.address.streetAddress());
+};
+
+exports.saveContactInformation = () => {
+  cy.get('button.usa-button').contains('Save').click();
+};
