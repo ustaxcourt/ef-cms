83e15e96fae9763de237df624f79e6cc6d6a4002
TElliott <telliott@flexion.us>
2021-11-10 16:08:47 -0600

    9119: WIP it rendered! [skip ci]

--- web-client/src/ustc-ui/PdfPreview/PdfViewer.jsx
+++ web-client/src/ustc-ui/PdfPreview/PdfViewer.jsx
@@ -1,5 +1,5 @@
 import 'pdfjs-dist/web/pdf_viewer.css';
-import { PDFViewer } from 'pdfjs-dist/web/pdf_viewer';
+import { EventBus, PDFViewer } from 'pdfjs-dist/web/pdf_viewer';
 import { connect } from '@cerebral/react';
 import { props, state } from 'cerebral';
 import React, { useEffect, useRef } from 'react';
@@ -9,6 +9,20 @@ const getPdfJs = async () => {
   const pdfjsWorker = await import('pdfjs-dist/build/pdf.worker.entry');
   pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
   return pdfjsLib;
+};
+const renderPdf = async (pdfPreviewUrl, ref) => {
+  // eslint-disable-next-line no-restricted-globals
+  const eventBus = new EventBus();
+
+  let pdfViewer = new PDFViewer({
+    container: ref,
+    eventBus,
+  });
+
+  const pdfJsLib = await getPdfJs();
+
+  const doc = await pdfJsLib.getDocument(pdfPreviewUrl).promise;
+  pdfViewer.setDocument(doc);
 };
 
 export const PdfViewer = connect(
@@ -20,24 +34,19 @@ export const PdfViewer = connect(
     const ref = useRef(null);
 
     useEffect(() => {
-      // eslint-disable-next-line no-restricted-globals
-      const element = document.getElementById('pdf-js');
-
-      let pdfViewer = new PDFViewer({
-        container: element,
-      });
-
-      getPdfJs()
-        .then(pdfJsLib => pdfJsLib.getDocument(pdfPreviewUrl))
-        .then(pdf => {
-          pdfViewer.setDocument(pdf);
-        });
+      renderPdf(pdfPreviewUrl, ref.current);
     });
     // conditional rendering, no life-cycle hooks.
     if (!pdfPreviewUrl || process.env.CI) {
       return noDocumentText || '';
     }
 
-    return <div id="pdf-js" ref={ref}></div>;
+    return (
+      <div>
+        <div id="pageContainer" ref={ref} style={{ position: 'absolute' }}>
+          <div className="pdfViewer" id="viewer"></div>
+        </div>
+      </div>
+    );
   },
 );
