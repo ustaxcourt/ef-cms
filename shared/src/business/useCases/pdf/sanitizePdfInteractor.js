91249a91aa04682fdc5b6389b11ba4b4113c2108
Kris Koskelin <kkoskelin@flexion.us>
2021-11-10 10:34:18 -0600

    9123: jsdoc updates for accuracy

--- shared/src/business/useCases/pdf/sanitizePdfInteractor.js
+++ shared/src/business/useCases/pdf/sanitizePdfInteractor.js
@@ -3,8 +3,8 @@
  *
  * @param {object} applicationContext the application context
  * @param {object} providers the providers object
- * @param {string} providers.key the key of the document to validate
- * @returns {object} errors (null if no errors)
+ * @param {string} providers.key the key of the document to sanitize
+ * @returns {boolean} true if successfully sanitized
  */
 exports.sanitizePdfInteractor = async (applicationContext, { key }) => {
   let { Body: pdfData } = await applicationContext
