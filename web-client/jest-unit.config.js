589f3a546e78a7b7e65a9c080a9df250b5ee99fa
Cody Seibert <cseibert113@gmail.com>
2021-11-11 10:58:20 -0500

    fixing tests

--- web-client/jest-unit.config.js
+++ web-client/jest-unit.config.js
@@ -31,7 +31,7 @@ module.exports = {
     atob: x => x,
     presenter: { providers: { applicationContext: {} } },
   },
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   //this is to ignore imported html files
   transform: {
     '^.+\\.html?$': './htmlLoader.js',
