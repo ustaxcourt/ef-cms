37f0778a6df3aa61a451d7cfdd88ec839012293b
Cody Seibert <cseibert113@gmail.com>
2021-11-11 11:55:51 -0500

    fixing tests

--- web-client/src/presenter/actions/scannerStartupAction.test.js
+++ web-client/src/presenter/actions/scannerStartupAction.test.js
@@ -1,14 +1,7 @@
-import { JSDOM } from 'jsdom';
 import { applicationContextForClient as applicationContext } from '../../../../shared/src/business/test/createTestApplicationContext';
 import { presenter } from '../presenter-mock';
 import { runAction } from 'cerebral/test';
 import { scannerStartupAction } from './scannerStartupAction';
-
-const jsdom = new JSDOM(
-  '<!DOCTYPE html><html><head></head><body></body></html>',
-);
-global.document = jsdom.window.document;
-global.window = jsdom.window;
 
 describe('scannerStartupAction', () => {
   beforeAll(() => {
