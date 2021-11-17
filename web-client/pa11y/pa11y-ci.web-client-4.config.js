604e4637d3a33968948aa3988e17b33c186e9857
Absolutestunna <joshujah007@gmail.com>
2021-11-17 14:01:20 -0500

    package upgrades: moved some user url tests from config 1 to config 4 to reduce testing time in circleci

--- web-client/pa11y/pa11y-ci.web-client-4.config.js
+++ web-client/pa11y/pa11y-ci.web-client-4.config.js
@@ -2,9 +2,12 @@ const { getOnly } = require('./helpers');
 
 const docketclerk = require('./pa11y-docketclerk');
 const judge = require('./pa11y-judge');
+const reportersOffice = require('./pa11y-reportersOffice');
 const { defaults, jsCheckDecorator } = require('./pa11y-ci.base-config.js');
 
-const urls = [...docketclerk, ...judge].map(jsCheckDecorator);
+const urls = [...docketclerk, ...judge, ...reportersOffice].map(
+  jsCheckDecorator,
+);
 
 module.exports = {
   defaults,
