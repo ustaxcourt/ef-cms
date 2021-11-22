310aef010da8ee58f5fae56169ee68a9bb646ffd
Rachel Schneiderman <rschneiderman379@gmail.com>
2021-11-22 16:30:37 -0500

    9077: Add integration test to catch bug [skip ci]

---
+++ web-client/integration-tests/secondPrivatePractitionerSearchesForSealedDocumentInAssociatedCase.test.js
@@ -0,0 +1,49 @@
+import { applicationContextForClient as applicationContext } from '../../shared/src/business/test/createTestApplicationContext';
+import { loginAs, setupTest, uploadPetition } from './helpers';
+import { petitionsClerkAddsPractitionersToCase } from './journey/petitionsClerkAddsPractitionersToCase';
+import { petitionsClerkCreateOrder } from './journey/petitionsClerkCreateOrder';
+import { petitionsClerkServesOrder } from './journey/petitionsClerkServesOrder';
+import { petitionsClerkSignsOrder } from './journey/petitionsClerkSignsOrder';
+import { petitionsClerkViewsCaseDetail } from './journey/petitionsClerkViewsCaseDetail';
+
+const cerebralTest = setupTest();
+const { COUNTRY_TYPES, PARTY_TYPES } = applicationContext.getConstants();
+
+describe('Petitions Clerk Counsel Association Journey', () => {
+  beforeAll(() => {
+    jest.setTimeout(30000);
+  });
+
+  afterAll(() => {
+    cerebralTest.closeSocket();
+  });
+
+  loginAs(cerebralTest, 'petitioner@example.com');
+  it('Create test case', async () => {
+    const caseDetail = await uploadPetition(cerebralTest, {
+      contactSecondary: {
+        address1: '734 Cowley Parkway',
+        city: 'Amazing',
+        countryType: COUNTRY_TYPES.DOMESTIC,
+        name: 'Jimothy Schultz',
+        phone: '+1 (884) 358-9729',
+        postalCode: '77546',
+        state: 'AZ',
+      },
+      partyType: PARTY_TYPES.petitionerSpouse,
+    });
+    expect(caseDetail.docketNumber).toBeDefined();
+    cerebralTest.docketNumber = caseDetail.docketNumber;
+  });
+
+  loginAs(cerebralTest, 'petitionsclerk@example.com');
+  petitionsClerkViewsCaseDetail(cerebralTest);
+  // create and serve an order
+  petitionsClerkCreateOrder(cerebralTest);
+  petitionsClerkSignsOrder(cerebralTest);
+  petitionsClerkServesOrder(cerebralTest);
+
+  // seal the case
+
+  petitionsClerkAddsPractitionersToCase(cerebralTest, false);
+});
