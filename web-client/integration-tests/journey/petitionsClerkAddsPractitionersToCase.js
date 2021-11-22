6e1b6e8adf8308d3ff0f9a8282c8c6350f7f78e6
halprin <halprin@users.noreply.github.com>
2021-11-22 16:11:02 -0700

    9077: Added ability to add a secondary practitioner to the primary petitioner in the integration tests

--- web-client/integration-tests/journey/petitionsClerkAddsPractitionersToCase.js
+++ web-client/integration-tests/journey/petitionsClerkAddsPractitionersToCase.js
@@ -9,7 +9,8 @@ const formattedCaseDetail = withAppContextDecorator(
 
 export const petitionsClerkAddsPractitionersToCase = (
   cerebralTest,
-  skipSecondary,
+  skipAddingPractitionerToSecondaryPetitioner,
+  addSecondaryPractitionerToPetitioner,
 ) => {
   return it('Petitions clerk manually adds multiple privatePractitioners to case', async () => {
     await cerebralTest.runSequence('gotoCaseDetailSequence', {
@@ -22,89 +23,20 @@ export const petitionsClerkAddsPractitionersToCase = (
       [],
     );
 
-    await cerebralTest.runSequence('openAddPrivatePractitionerModalSequence');
-
-    expect(
-      cerebralTest.getState('validationErrors.practitionerSearchError'),
-    ).toBeDefined();
-
-    await cerebralTest.runSequence('updateFormValueSequence', {
-      key: 'practitionerSearch',
-      value: practitionerBarNumber,
-    });
-
-    await cerebralTest.runSequence('openAddPrivatePractitionerModalSequence');
-
-    expect(
-      cerebralTest.getState('validationErrors.practitionerSearchError'),
-    ).toBeUndefined();
-    expect(cerebralTest.getState('modal.practitionerMatches.length')).toEqual(
-      1,
+    await associatePrivatePractitionerToPrimaryPetitioner(
+      cerebralTest,
+      practitionerBarNumber,
     );
 
-    //default selected because there was only 1 match
-    let practitionerMatch = cerebralTest.getState(
-      'modal.practitionerMatches.0',
-    );
-    expect(cerebralTest.getState('modal.user.userId')).toEqual(
-      practitionerMatch.userId,
-    );
-
-    const formattedCase = runCompute(formattedCaseDetail, {
-      state: cerebralTest.getState(),
-    });
-    const contactPrimary = formattedCase.petitioners[0];
-
-    await cerebralTest.runSequence('updateModalValueSequence', {
-      key: `representingMap.${contactPrimary.contactId}`,
-      value: true,
-    });
-
-    if (cerebralTest.intervenorContactId) {
-      await cerebralTest.runSequence('updateModalValueSequence', {
-        key: `representingMap.${cerebralTest.intervenorContactId}`,
-        value: true,
-      });
+    if (addSecondaryPractitionerToPetitioner) {
+      await associatePrivatePractitionerToPrimaryPetitioner(
+        cerebralTest,
+        'PT5432',
+      );
     }
-
-    expect(
-      cerebralTest.getState('validationErrors.practitionerSearchError'),
-    ).toBeUndefined();
-
-    await cerebralTest.runSequence(
-      'associatePrivatePractitionerWithCaseSequence',
-    );
-
-    expect(
-      cerebralTest.getState('caseDetail.privatePractitioners.length'),
-    ).toEqual(1);
-    expect(
-      cerebralTest.getState('caseDetail.privatePractitioners.0.representing'),
-    ).toContain(contactPrimary.contactId);
-
-    if (cerebralTest.intervenorContactId) {
-      expect(
-        cerebralTest.getState('caseDetail.privatePractitioners.0.representing'),
-      ).toContain(cerebralTest.intervenorContactId);
-    }
-
-    expect(
-      cerebralTest.getState('caseDetail.privatePractitioners.0.name'),
-    ).toEqual(practitionerMatch.name);
-
-    let formatted = runCompute(formattedCaseDetail, {
-      state: cerebralTest.getState(),
-    });
-
-    expect(formatted.privatePractitioners.length).toEqual(1);
-    expect(formatted.privatePractitioners[0].formattedName).toEqual(
-      `${practitionerMatch.name} (${practitionerMatch.barNumber})`,
-    );
-
-    cerebralTest.privatePractitioners = formatted.privatePractitioners[0];
 
     //add a second practitioner
-    if (!skipSecondary) {
+    if (!skipAddingPractitionerToSecondaryPetitioner) {
       await cerebralTest.runSequence('updateFormValueSequence', {
         key: 'practitionerSearch',
         value: 'PT5432',
@@ -114,10 +46,16 @@ export const petitionsClerkAddsPractitionersToCase = (
       expect(cerebralTest.getState('modal.practitionerMatches.length')).toEqual(
         1,
       );
-      practitionerMatch = cerebralTest.getState('modal.practitionerMatches.0');
+      const practitionerMatch = cerebralTest.getState(
+        'modal.practitionerMatches.0',
+      );
       expect(cerebralTest.getState('modal.user.userId')).toEqual(
         practitionerMatch.userId,
       );
+
+      const formattedCase = runCompute(formattedCaseDetail, {
+        state: cerebralTest.getState(),
+      });
 
       const contactSecondary = formattedCase.petitioners[1];
       await cerebralTest.runSequence('updateModalValueSequence', {
@@ -138,7 +76,7 @@ export const petitionsClerkAddsPractitionersToCase = (
         cerebralTest.getState('caseDetail.privatePractitioners.1.name'),
       ).toEqual(practitionerMatch.name);
 
-      formatted = runCompute(formattedCaseDetail, {
+      const formatted = runCompute(formattedCaseDetail, {
         state: cerebralTest.getState(),
       });
 
@@ -150,4 +88,86 @@ export const petitionsClerkAddsPractitionersToCase = (
       await refreshElasticsearchIndex();
     }
   });
+};
+
+const associatePrivatePractitionerToPrimaryPetitioner = async (
+  cerebralTest,
+  barNumber,
+) => {
+  await cerebralTest.runSequence('openAddPrivatePractitionerModalSequence');
+
+  expect(
+    cerebralTest.getState('validationErrors.practitionerSearchError'),
+  ).toBeDefined();
+
+  await cerebralTest.runSequence('updateFormValueSequence', {
+    key: 'practitionerSearch',
+    value: barNumber,
+  });
+
+  await cerebralTest.runSequence('openAddPrivatePractitionerModalSequence');
+
+  expect(
+    cerebralTest.getState('validationErrors.practitionerSearchError'),
+  ).toBeUndefined();
+  expect(cerebralTest.getState('modal.practitionerMatches.length')).toEqual(1);
+
+  //default selected because there was only 1 match
+  let practitionerMatch = cerebralTest.getState('modal.practitionerMatches.0');
+  expect(cerebralTest.getState('modal.user.userId')).toEqual(
+    practitionerMatch.userId,
+  );
+
+  const formattedCase = runCompute(formattedCaseDetail, {
+    state: cerebralTest.getState(),
+  });
+  const contactPrimary = formattedCase.petitioners[0];
+
+  await cerebralTest.runSequence('updateModalValueSequence', {
+    key: `representingMap.${contactPrimary.contactId}`,
+    value: true,
+  });
+
+  if (cerebralTest.intervenorContactId) {
+    await cerebralTest.runSequence('updateModalValueSequence', {
+      key: `representingMap.${cerebralTest.intervenorContactId}`,
+      value: true,
+    });
+  }
+
+  expect(
+    cerebralTest.getState('validationErrors.practitionerSearchError'),
+  ).toBeUndefined();
+
+  await cerebralTest.runSequence(
+    'associatePrivatePractitionerWithCaseSequence',
+  );
+
+  expect(
+    cerebralTest.getState('caseDetail.privatePractitioners.length'),
+  ).toEqual(1);
+  expect(
+    cerebralTest.getState('caseDetail.privatePractitioners.0.representing'),
+  ).toContain(contactPrimary.contactId);
+
+  if (cerebralTest.intervenorContactId) {
+    expect(
+      cerebralTest.getState('caseDetail.privatePractitioners.0.representing'),
+    ).toContain(cerebralTest.intervenorContactId);
+  }
+
+  expect(
+    cerebralTest.getState('caseDetail.privatePractitioners.0.name'),
+  ).toEqual(practitionerMatch.name);
+
+  let formatted = runCompute(formattedCaseDetail, {
+    state: cerebralTest.getState(),
+  });
+
+  expect(formatted.privatePractitioners.length).toEqual(1);
+  expect(formatted.privatePractitioners[0].formattedName).toEqual(
+    `${practitionerMatch.name} (${practitionerMatch.barNumber})`,
+  );
+
+  cerebralTest.privatePractitioners = formatted.privatePractitioners[0];
 };
--- web-client/integration-tests/secondPrivatePractitionerSearchesForSealedDocumentInAssociatedCase.test.js
+++ web-client/integration-tests/secondPrivatePractitionerSearchesForSealedDocumentInAssociatedCase.test.js
@@ -2,10 +2,11 @@ import { applicationContextForClient as applicationContext } from '../../shared/
 import { docketClerkSealsCase } from './journey/docketClerkSealsCase';
 import { loginAs, setupTest, uploadPetition } from './helpers';
 import { petitionsClerkAddsDocketEntryFromOrder } from './journey/petitionsClerkAddsDocketEntryFromOrder';
+import { petitionsClerkAddsPractitionersToCase } from './journey/petitionsClerkAddsPractitionersToCase';
 import { petitionsClerkCreateOrder } from './journey/petitionsClerkCreateOrder';
+import { petitionsClerkServesElectronicCaseToIrs } from './journey/petitionsClerkServesElectronicCaseToIrs';
 import { petitionsClerkServesOrder } from './journey/petitionsClerkServesOrder';
 import { petitionsClerkSignsOrder } from './journey/petitionsClerkSignsOrder';
-import { petitionsClerkServesElectronicCaseToIrs } from './journey/petitionsClerkServesElectronicCaseToIrs';
 
 const cerebralTest = setupTest();
 const { COUNTRY_TYPES, PARTY_TYPES } = applicationContext.getConstants();
@@ -52,68 +53,5 @@ describe('Petitions Clerk Counsel Association Journey', () => {
 
   loginAs(cerebralTest, 'petitionsclerk@example.com');
 
-  it('Associate two private practitioners to a petitioner', async () => {
-    await cerebralTest.runSequence('gotoCaseDetailSequence', {
-      docketNumber: cerebralTest.docketNumber,
-    });
-
-    const practitionerBarNumber = 'PT1234';
-
-    expect(cerebralTest.getState('caseDetail.privatePractitioners')).toEqual(
-      [],
-    );
-
-    await cerebralTest.runSequence('openAddPrivatePractitionerModalSequence');
-
-    expect(
-      cerebralTest.getState('validationErrors.practitionerSearchError'),
-    ).toBeDefined();
-
-    await cerebralTest.runSequence('updateFormValueSequence', {
-      key: 'practitionerSearch',
-      value: practitionerBarNumber,
-    });
-
-    await cerebralTest.runSequence('openAddPrivatePractitionerModalSequence');
-
-    expect(
-      cerebralTest.getState('validationErrors.practitionerSearchError'),
-    ).toBeUndefined();
-    expect(cerebralTest.getState('modal.practitionerMatches.length')).toEqual(
-      1,
-    );
-
-    //default selected because there was only 1 match
-    let practitionerMatch = cerebralTest.getState(
-      'modal.practitionerMatches.0',
-    );
-    expect(cerebralTest.getState('modal.user.userId')).toEqual(
-      practitionerMatch.userId,
-    );
-
-    const formattedCase = runCompute(formattedCaseDetail, {
-      state: cerebralTest.getState(),
-    });
-    const contactPrimary = formattedCase.petitioners[0];
-
-    await cerebralTest.runSequence('updateModalValueSequence', {
-      key: `representingMap.${contactPrimary.contactId}`,
-      value: true,
-    });
-
-    expect(
-      cerebralTest.getState('validationErrors.practitionerSearchError'),
-    ).toBeUndefined();
-
-    await cerebralTest.runSequence(
-      'associatePrivatePractitionerWithCaseSequence',
-    );
-
-    expect(
-      cerebralTest.getState('caseDetail.privatePractitioners.length'),
-    ).toEqual(1);
-    expect(
-      cerebralTest.getState('caseDetail.privatePractitioners.0.representing'),
-    ).toContain(contactPrimary.contactId);
-  });
+  petitionsClerkAddsPractitionersToCase(cerebralTest, true, true);
 });
