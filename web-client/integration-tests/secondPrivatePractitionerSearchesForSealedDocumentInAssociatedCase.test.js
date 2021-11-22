f6ff21b989e2e8bac5e2e3166c9a481c5fcbfd79
Rachel Schneiderman <rschneiderman379@gmail.com>
2021-11-22 18:48:09 -0500

    9077: Cleanup

--- web-client/integration-tests/secondPrivatePractitionerSearchesForSealedDocumentInAssociatedCase.test.js
+++ web-client/integration-tests/secondPrivatePractitionerSearchesForSealedDocumentInAssociatedCase.test.js
@@ -3,16 +3,15 @@ import { docketClerkSealsCase } from './journey/docketClerkSealsCase';
 import { loginAs, setupTest, uploadPetition } from './helpers';
 import { petitionsClerkAddsDocketEntryFromOrder } from './journey/petitionsClerkAddsDocketEntryFromOrder';
 import { petitionsClerkAddsPractitionerToPrimaryContact } from './journey/petitionsClerkAddsPractitionerToPrimaryContact';
-import { petitionsClerkAddsPractitionersToCase } from './journey/petitionsClerkAddsPractitionersToCase';
 import { petitionsClerkCreateOrder } from './journey/petitionsClerkCreateOrder';
 import { petitionsClerkServesElectronicCaseToIrs } from './journey/petitionsClerkServesElectronicCaseToIrs';
 import { petitionsClerkServesOrder } from './journey/petitionsClerkServesOrder';
 import { petitionsClerkSignsOrder } from './journey/petitionsClerkSignsOrder';
 
-const cerebralTest = setupTest();
-const { COUNTRY_TYPES, PARTY_TYPES } = applicationContext.getConstants();
-
 describe('Petitions Clerk Counsel Association Journey', () => {
+  const cerebralTest = setupTest();
+  const { COUNTRY_TYPES, PARTY_TYPES } = applicationContext.getConstants();
+
   beforeAll(() => {
     jest.setTimeout(30000);
   });
@@ -41,20 +40,16 @@ describe('Petitions Clerk Counsel Association Journey', () => {
 
   loginAs(cerebralTest, 'petitionsclerk@example.com');
   petitionsClerkServesElectronicCaseToIrs(cerebralTest);
-  // petitionsClerkViewsCaseDetail(cerebralTest);
-  // create and serve an order
+
   petitionsClerkCreateOrder(cerebralTest);
   petitionsClerkSignsOrder(cerebralTest);
   petitionsClerkAddsDocketEntryFromOrder(cerebralTest);
   petitionsClerkServesOrder(cerebralTest);
 
-  // seal the case
   loginAs(cerebralTest, 'docketclerk@example.com');
   docketClerkSealsCase(cerebralTest);
 
   loginAs(cerebralTest, 'petitionsclerk@example.com');
-
-  // petitionsClerkAddsPractitionersToCase(cerebralTest, true, true);
   petitionsClerkAddsPractitionerToPrimaryContact(cerebralTest, 'PT1234');
   petitionsClerkAddsPractitionerToPrimaryContact(cerebralTest, 'PT5432');
 });
