7cd8258199a9592c93ee01d8e25c855c23e28bc4
Kris Koskelin <kkoskelin@flexion.us>
2021-11-24 14:35:37 -0600

    8949: broadcast requests, receive requests for refresh tokens

---
+++ web-client/src/presenter/actions/receiveRefreshTokenAction.test.js
@@ -0,0 +1,25 @@
+import { applicationContextForClient as applicationContext } from '../../../../shared/src/business/test/createTestApplicationContext';
+import { presenter } from '../presenter-mock';
+import { receiveRefreshTokenAction } from './receiveRefreshTokenAction';
+import { runAction } from 'cerebral/test';
+
+describe('receiveRefreshTokenAction', () => {
+  beforeAll(() => {
+    presenter.providers.applicationContext = applicationContext;
+  });
+
+  it('should set the refresh token action in state', async () => {
+    const result = await runAction(receiveRefreshTokenAction, {
+      modules: {
+        presenter,
+      },
+      props: {
+        refreshToken: 'someThing',
+      },
+      state: {
+        refreshToken: undefined,
+      },
+    });
+    expect(result.state.refreshToken).toBe('someThing');
+  });
+});
