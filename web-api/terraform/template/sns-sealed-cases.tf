2c50a17be07e583e13289893ecf3803b5c2689b0
Michael Marcotte <mike.marcotte@gmail.com>
2021-11-30 13:42:04 -0500

    ustc-ato-65: create sns topic for sealed cases

---
+++ web-api/terraform/template/sns-sealed-cases.tf
@@ -0,0 +1,21 @@
+// Creates an SNS Topic that a Lambda can subscribe to in order seal cases in lower environments
+resource "aws_sns_topic" "sealed_case_notifier" {
+  name = "sealed_case_notifier"
+  count = var.is_production ? 1 : 0
+  policy = <<EOF
+{
+  "Sid": "",
+  "Effect": "Allow",
+  "Principal": {
+    "AWS": 
+      "arn:aws:iam::${var.lower_env_account_id}:root"
+    
+  },
+  "Action": [
+    "SNS:Subscribe",
+    "SNS:Receive"
+  ],
+  "Resource": "arn:aws:sns:us-east-1:${var.prod_env_account_id}:sealed_case_notifier"
+}
+EOF
+}
