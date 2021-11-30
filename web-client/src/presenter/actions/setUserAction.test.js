0088eb87eebe889b9ed7a6d37b31e239fbc2db76
Cody Seibert <cseibert113@gmail.com>
2021-11-30 10:26:05 -0500

    8949 - addressing pr comments

--- web-client/src/presenter/actions/setUserAction.test.js
+++ web-client/src/presenter/actions/setUserAction.test.js
@@ -6,16 +6,16 @@ import { setUserAction } from './setUserAction';
 describe('setUserAction', () => {
   beforeAll(() => {
     process.env.USTC_ENV = 'dev';
-
     presenter.providers.applicationContext = applicationContext;
   });
 
   afterEach(() => {
     delete process.env.USTC_ENV;
+    delete process.env.IS_LOCAL;
     delete global.window;
   });
 
-  it('stores the user onto local storage when running local', async () => {
+  it('stores the user onto the application context', async () => {
     const user = {
       userId: 'petitioner',
     };
@@ -32,5 +32,30 @@ describe('setUserAction', () => {
     expect(applicationContext.setCurrentUser.mock.calls[0][0]).toMatchObject(
       user,
     );
+  });
+
+  it('stores the user onto local storage when running local', async () => {
+    process.env.IS_LOCAL = 'true';
+    const user = {
+      userId: 'petitioner',
+    };
+    await runAction(setUserAction, {
+      modules: {
+        presenter,
+      },
+      props: {
+        user,
+      },
+      state: {},
+    });
+    expect(
+      applicationContext.getUseCases().setItemInteractor.mock.calls.length,
+    ).toEqual(1);
+    expect(
+      applicationContext.getUseCases().setItemInteractor.mock.calls[0][1],
+    ).toMatchObject({
+      key: 'user',
+      value: user,
+    });
   });
 });
--- web-client/src/presenter/actions/startRefreshIntervalAction.test.js
+++ web-client/src/presenter/actions/startRefreshIntervalAction.test.js
@@ -14,6 +14,9 @@ describe('startRefreshIntervalAction', () => {
 
     presenter.providers.applicationContext = applicationContext;
   });
+  afterEach(() => {
+    delete process.env.IS_LOCAL;
+  });
 
   it('starts the refresh interval for auth tokens', async () => {
     applicationContext.getUseCases().refreshTokenInteractor.mockResolvedValue({
@@ -35,5 +38,20 @@ describe('startRefreshIntervalAction', () => {
       applicationContext.getUseCases().setItemInteractor,
     ).not.toHaveBeenCalled();
     expect(result.state.refreshTokenInterval).toEqual('new-interval');
+  });
+
+  it('should save the token to localStorage if running locally', async () => {
+    process.env.IS_LOCAL = 'true';
+    applicationContext.getUseCases().refreshTokenInteractor.mockResolvedValue({
+      token: 'token-123',
+    });
+    await runAction(startRefreshIntervalAction, {
+      modules: {
+        presenter,
+      },
+    });
+    expect(
+      applicationContext.getUseCases().setItemInteractor,
+    ).toHaveBeenCalled();
   });
 });
