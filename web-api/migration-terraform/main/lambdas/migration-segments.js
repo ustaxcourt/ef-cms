e04f3428b27f4243edf5df06ca596f7174c10a3e
Michael Marcotte <mike.marcotte@gmail.com>
2021-12-01 17:53:28 -0500

    delete other references to migrations

--- web-api/migration-terraform/main/lambdas/migration-segments.js
+++ web-api/migration-terraform/main/lambdas/migration-segments.js
@@ -2,18 +2,6 @@ const AWS = require('aws-sdk');
 const createApplicationContext = require('../../../src/applicationContext');
 const promiseRetry = require('promise-retry');
 const {
-  migrateItems: bugMigration0039,
-} = require('./migrations/bug-0039-notice-of-trial-date');
-const {
-  migrateItems: migration0001,
-} = require('./migrations/0001-update-websockets-gsi1pk');
-const {
-  migrateItems: migration0002,
-} = require('./migrations/0002-session-scope');
-const {
-  migrateItems: migration0040,
-} = require('./migrations/bug-0040-case-received-at');
-const {
   migrateItems: validationMigration,
 } = require('./migrations/0000-validate-all-items');
 const { chunk } = require('lodash');
@@ -41,26 +29,6 @@ const migrateRecords = async ({
   // eslint-disable-next-line no-unused-vars
   ranMigrations = {},
 }) => {
-  if (!ranMigrations['0001-update-websockets-gsi1pk.js']) {
-    applicationContext.logger.debug('about to run migration 0001');
-    items = migration0001(items);
-  }
-
-  if (!ranMigrations['0002-session-scope.js']) {
-    applicationContext.logger.debug('about to run migration 0002');
-    items = migration0002(items);
-  }
-
-  if (!ranMigrations['bug-0040-case-received-at.js']) {
-    applicationContext.logger.debug('about to run migration 0040');
-    items = await migration0040(items, documentClient);
-  }
-
-  if (!ranMigrations['bug-0039-notice-of-trial-date.js']) {
-    applicationContext.logger.debug('about to run bug migration 0039');
-    items = await bugMigration0039(items, documentClient);
-  }
-
   applicationContext.logger.debug('about to run validation migration');
   items = await validationMigration(items);
 
