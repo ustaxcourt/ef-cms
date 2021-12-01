9bdbeb316f85c8b6038491a09b53fd3fa6f7fe05
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2021-12-01 16:13:52 -0600

    9077: Updating migration script. [skip ci]

--- web-api/migration-terraform/main/lambdas/migrations/0003-case-has-sealed-documents.js
+++ web-api/migration-terraform/main/lambdas/migrations/0003-case-has-sealed-documents.js
@@ -5,16 +5,11 @@ const {
 const {
   Case,
 } = require('../../../../../shared/src/business/entities/cases/Case');
-const {
-  INITIAL_DOCUMENT_TYPES,
-} = require('../../../../../shared/src/business/entities/EntityConstants');
 const { queryFullCase } = require('../utilities');
 const applicationContext = createApplicationContext({});
 
-const isElectronicCase = item => {
-  const isCaseItem = item.pk.startsWith('case|') && item.sk.startsWith('case|');
-
-  return isCaseItem && !item.isPaper === true;
+const isCaseRecord = item => {
+  return item.pk.startsWith('case|') && item.sk.startsWith('case|');
 };
 
 const migrateItems = async (items, documentClient) => {
@@ -25,36 +20,16 @@ const migrateItems = async (items, documentClient) => {
   const itemsAfter = [];
 
   for (const item of items) {
-    if (isElectronicCase(item)) {
+    if (isCaseRecord(item)) {
       const fullCase = await queryFullCase(documentClient, item.docketNumber);
 
       const caseRecord = aggregateCaseItems(fullCase);
 
-      const petitionItem = caseRecord.docketEntries.find(
-        entry => entry.eventCode === INITIAL_DOCUMENT_TYPES.petition.eventCode,
-      );
-
-      const daysDiff = applicationContext
-        .getUtilities()
-        .calculateDifferenceInDays(item.receivedAt, petitionItem.receivedAt);
-
-      if (daysDiff !== 0) {
-        // an electronic filing where the received at was changed from that of the petition
-
-        applicationContext.logger.info(
-          `Updating case.receivedAt from ${item.receivedAt} to ${petitionItem.receivedAt} to match petition. Difference in days: ${daysDiff}`,
-          {
-            pk: item.pk,
-            sk: item.sk,
-          },
-        );
-
-        item.receivedAt = petitionItem.receivedAt;
-      }
-
-      new Case(item, { applicationContext }).validateWithLogging(
+      const theCase = new Case(caseRecord, {
         applicationContext,
-      );
+      }).validateWithLogging(applicationContext);
+
+      item.hasSealedDocuments = theCase.hasSealedDocuments;
     }
 
     itemsAfter.push(item);
