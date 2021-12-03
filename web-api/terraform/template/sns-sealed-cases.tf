6a2c19e5de6a4bd352f3599491954e0f939bf3f3
Michael Marcotte <mike.marcotte@gmail.com>
2021-12-03 11:50:49 -0500

    ustc-ato-65: setup policy via tf

--- web-api/terraform/template/sns-sealed-cases.tf
+++ web-api/terraform/template/sns-sealed-cases.tf
@@ -2,18 +2,31 @@
 resource "aws_sns_topic" "seal_notifier" {
   name = "seal_notifier"
   count = var.prod_env_account_id == data.aws_caller_identity.current.account_id ? 1 : 0
-  policy = <<EOF
-{
-  "Sid": "",
-  "Effect": "Allow",
-  "Principal": {
-    "AWS": "arn:aws:iam::${var.lower_env_account_id}:root"
-  },
-  "Action": [
-    "SNS:Subscribe",
-    "SNS:Receive"
-  ],
-  "Resource": "arn:aws:sns:us-east-1:${var.prod_env_account_id}:seal_notifier"
 }
-EOF
+
+resource "aws_sns_topic_policy" "default" {
+  count = var.prod_env_account_id == data.aws_caller_identity.current.account_id ? 1 : 0
+  arn = aws_sns_topic.seal_notifier[0].arn
+  policy = data.aws_iam_policy_document.sns_topic_policy[0].json
+}
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  count = var.prod_env_account_id == data.aws_caller_identity.current.account_id ? 1 : 0
+  statement {
+    actions = [
+      "SNS:Subscribe",
+      "SNS:Receive",
+    ]
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.lower_env_account_id}:root"]
+    }
+
+    resources = [
+      aws_sns_topic.seal_notifier[0].arn,
+    ]
+  }
 }
