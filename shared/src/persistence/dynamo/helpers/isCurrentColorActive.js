c06a8d8a63a95292b98ed1c47a4f3f2849d49ab4
Michael Marcotte <mike.marcotte@gmail.com>
2021-12-06 17:00:09 -0500

    ustc-ato-65: address linting complaint

--- shared/src/persistence/dynamo/helpers/isCurrentColorActive.js
+++ shared/src/persistence/dynamo/helpers/isCurrentColorActive.js
@@ -4,7 +4,7 @@ const client = require('../../dynamodbClientService');
  * Checks to see if the environment currentColor matches that of the deploy table
  *
  * @param {object} applicationContext The applicationContext
- * @returns {<Promise>bool} whether or not the current color matches what's in the deploy table
+ * @returns {<Promise>} which resolves to whether or not the current color matches what's in the deploy table
  */
 exports.isCurrentColorActive = async () => {
   const [{ value: currentColor }] = await client.query({
