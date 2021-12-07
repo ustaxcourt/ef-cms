eecef1455916cf1b4690858ea0c3564dc1dfc326
Cody Seibert <cseibert113@gmail.com>
2021-12-07 12:23:00 -0500

    8949 - adding an integration tests to verify these endpoints

---
+++ web-client/integration-tests/authenticationCookies.test.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+describe('checks the /auth endpoints to verify the cookies are set and cleared correctly', () => {
+  it('should get back a token in the header when hitting the login endpoint', async () => {
+    const response = await axios.post(
+      'http://localhost:4000/auth/login',
+      {
+        code: 'abc',
+      },
+      {
+        withCredentials: true,
+      },
+    );
+    expect(response.data).toEqual({
+      token: 'DogCow',
+    });
+  });
+
+  it('should be able to hit the refresh endpoint get a new token', async () => {
+    const response = await axios.post(
+      'http://localhost:4000/auth/login',
+      {
+        code: 'abc',
+      },
+      {
+        withCredentials: true,
+      },
+    );
+
+    expect(response.data).toEqual({
+      token: 'DogCow',
+    });
+  });
+
+  it('after hitting DELETE@/auth/login, the refresh endpoint should no longer return a new token', async () => {
+    await axios.delete('http://localhost:4000/auth/login', {
+      withCredentials: true,
+    });
+
+    await expect(
+      axios.post('http://localhost:4000/auth/refresh', null, {
+        withCredentials: true,
+      }),
+    ).rejects.toThrow('Request failed with status code 400');
+  });
+});
