cba5a3fba8dd4a985e1141c79a6b612a69b9ea16
Rachel Schneiderman <rschneiderman379@gmail.com>
2021-12-10 17:47:27 -0500

    8949: AUpdate auth related endpoints to bypass maintenance check

--- web-api/src/auth/authenticateUserLambda.js
+++ web-api/src/auth/authenticateUserLambda.js
@@ -8,27 +8,31 @@ const { genericHandler } = require('../genericHandler');
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
 exports.authenticateUserLambda = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    const { refreshToken, token } = await applicationContext
-      .getUseCases()
-      .authenticateUserInteractor(applicationContext, JSON.parse(event.body));
-    const expiresAt = applicationContext.getUtilities().calculateISODate({
-      dateString: applicationContext.getUtilities().createISODateString(),
-      howMuch: 29,
-      units: 'days',
-    });
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      const { refreshToken, token } = await applicationContext
+        .getUseCases()
+        .authenticateUserInteractor(applicationContext, JSON.parse(event.body));
+      const expiresAt = applicationContext.getUtilities().calculateISODate({
+        dateString: applicationContext.getUtilities().createISODateString(),
+        howMuch: 29,
+        units: 'days',
+      });
 
-    return {
-      body: JSON.stringify({ token }),
-      headers: {
-        'Set-Cookie': createCookieString(
-          'refreshToken',
-          refreshToken,
-          expiresAt,
-          process.env.EFCMS_DOMAIN,
-          !process.env.IS_LOCAL,
-        ),
-      },
-      statusCode: 200,
-    };
-  });
+      return {
+        body: JSON.stringify({ token }),
+        headers: {
+          'Set-Cookie': createCookieString(
+            'refreshToken',
+            refreshToken,
+            expiresAt,
+            process.env.EFCMS_DOMAIN,
+            !process.env.IS_LOCAL,
+          ),
+        },
+        statusCode: 200,
+      };
+    },
+    { bypassMaintenanceCheck: true },
+  );
