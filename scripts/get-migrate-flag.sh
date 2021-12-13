1ae5fdeef96167f090edb6cbc539ba0201d8c263
Absolutestunna <joshujah007@gmail.com>
2021-12-13 14:09:16 -0500

    1015: small change to retrigger exp4 redeployment

--- scripts/get-migrate-flag.sh
+++ scripts/get-migrate-flag.sh
@@ -8,6 +8,7 @@
 # Arguments
 #   - $1 - the environment to check
 
+
 ( ! command -v jq > /dev/null ) && echo "jq must be installed on your machine." && exit 1
 [ -z "$1" ] && echo "The environment to check must be provided as the \$1 argument." && exit 1
 [ -z "${USTC_ADMIN_PASS}" ] && echo "You must have USTC_ADMIN_PASS set in your environment" && exit 1
