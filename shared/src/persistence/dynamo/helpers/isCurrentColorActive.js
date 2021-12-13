70a758752f9bb8b1af22a4651a97ed9b80ad2c15
Michael Marcotte <mike.marcotte@gmail.com>
2021-12-13 11:10:41 -0500

    ustc-ato-65: address pr feedback

--- shared/src/persistence/dynamo/helpers/isCurrentColorActive.js
+++ shared/src/persistence/dynamo/helpers/isCurrentColorActive.js
@@ -7,12 +7,11 @@ const client = require('../../dynamodbClientService');
  * @returns {Promise} which resolves to whether or not the current color matches what's in the deploy table
  */
 exports.isCurrentColorActive = async applicationContext => {
-  const { current: currentColor } = await client.get({
+  const { current: currentColor } = await client.getFromDeployTable({
     Key: {
       pk: 'current-color',
       sk: 'current-color',
     },
-    TableName: client.getDeployTableName({ applicationContext }),
     applicationContext,
   });
 
