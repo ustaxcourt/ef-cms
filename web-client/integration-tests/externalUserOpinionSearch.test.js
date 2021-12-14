0756cfc8ea1b2035f60f5ae33fd8e73096844952
TElliott <telliott@flexion.us>
2021-12-14 16:18:00 -0600

    9211: WIP adding integration tests [skip ci]

--- web-client/integration-tests/externalUserOpinionSearch.test.js
+++ web-client/integration-tests/externalUserOpinionSearch.test.js
@@ -1,7 +1,13 @@
 import {
+  ADVANCED_SEARCH_OPINION_TYPES,
+  ADVANCED_SEARCH_TABS,
+  DATE_RANGE_SEARCH_OPTIONS,
+} from '../../shared/src/business/entities/EntityConstants';
+import {
   loginAs,
   setOpinionSearchEnabled,
   setupTest,
+  updateOpinionForm,
 } from '../integration-tests/helpers';
 const cerebralTest = setupTest();
 
@@ -16,28 +22,41 @@ describe('verify opinion search works for external users', () => {
   });
 
   describe('private practitioner performs opinion search', () => {
+    loginAs(cerebralTest, 'privatePractitioner@example.com');
     it('should return an opinion from a sealed case', async () => {
-      //Private/IRS practitioner accesses Opinion Search, searches for keyword/phrase, results list is returned. User clicks on result, which opens in a new tab
-      loginAs(cerebralTest, 'privatePractitioner@example.com');
-
       // Private/IRS practitioner accesses Opinion Search, searches for Opinion in sealed case, Opinion is returned in results list.
       await cerebralTest.runSequence('gotoAdvancedSearchSequence');
       // cerebralTest.setState('advancedSearchTab', ADVANCED_SEARCH_TABS.OPINION);
 
       // get access for an opinion data on a sealed case
-      await cerebralTest.runSequence(
-        'updateAdvancedOpinionSearchFormValueSequence',
-        {
-          key: 'keyword',
-          value: 'sunglasses',
+      // await cerebralTest.runSequence(
+      //   'updateAdvancedOpinionSearchFormValueSequence',
+      //   {
+      //     key: 'keyword',
+      //     value: 'sunglasses',
+      //   },
+      // );
+
+      await updateOpinionForm(cerebralTest, {
+        dateRange: DATE_RANGE_SEARCH_OPTIONS.CUSTOM_DATES,
+        keyword: 'opinion',
+        opinionTypes: {
+          [ADVANCED_SEARCH_OPINION_TYPES.Memorandum]: true,
+          [ADVANCED_SEARCH_OPINION_TYPES.Bench]: true,
+          [ADVANCED_SEARCH_OPINION_TYPES.Summary]: true,
+          [ADVANCED_SEARCH_OPINION_TYPES['T.C.']]: true,
         },
-      );
+        startDate: '08/03/1995',
+      });
 
       await cerebralTest.runSequence('submitOpinionAdvancedSearchSequence');
+      expect(cerebralTest.getState('validationErrors')).toEqual({});
 
-      // cerebralTest.getState(`searchResults.${ADVANCED_SEARCH_TABS.OPINION}`),
-
-      // const stateOfAdvancedSearch = cerebralTest.getState('advancedSearchTab');
+      console.log('state', cerebralTest.getState('searchResults'));
+      const stateOfAdvancedSearch = cerebralTest.getState(
+        `searchResults.${ADVANCED_SEARCH_TABS.OPINION}`,
+      );
+      expect(stateOfAdvancedSearch).toEqual([]);
     });
   });
 });
