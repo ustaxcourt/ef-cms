1bb4f17f0701876d96b17496f0d1f76e798a4a6a
Rachel Schneiderman <rschneiderman379@gmail.com>
2021-12-14 10:50:17 -0500

    9261: Add script for setting up external opinion search flag

---
+++ scripts/setup-external-opinion-search-flag.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Sets the external opinion search enabled flag to "true" in the dynamo deploy table
+
+# Usage
+#   ENV=dev ./setup-external-opinion-search-flag.sh
+
+./check-env-variables.sh \
+  "ENV" \
+  "AWS_SECRET_ACCESS_KEY" \
+  "AWS_ACCESS_KEY_ID"
+
+ITEM=$(cat <<-END
+{
+    "pk": {
+        "S": "external-opinion-search-enabled"
+    },
+    "sk":{
+        "S": "external-opinion-search-enabled"
+    },
+    "current": {
+        "BOOL":true
+    }
+}
+END
+)
+
+aws dynamodb put-item \
+    --region us-east-1 \
+    --table-name "efcms-deploy-${ENV}" \
+    --item "${ITEM}"
+
