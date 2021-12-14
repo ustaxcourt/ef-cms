45e34db239549c8c476211b209734e2ff6a743bf
Cody Seibert <cseibert113@gmail.com>
2021-12-14 12:57:54 -0500

    refactor the smoketests to work for local and deployed

--- cypress-smoketests/support/pages/my-account.js
+++ cypress-smoketests/support/pages/my-account.js
@@ -11,7 +11,7 @@ exports.goToEditContactInformation = () => {
 
 exports.updateAddress1 = () => {
   cy.get('.progress-indicator').should('not.exist');
-  cy.get('h1').contains('Edit Contact Information').should('exist');
+  cy.contains('Edit Contact Information').should('exist');
 
   cy.get('input[id=contact\\.address1]')
     .clear()
@@ -20,13 +20,24 @@ exports.updateAddress1 = () => {
 
 exports.saveContactInformation = () => {
   cy.get('button.usa-button').contains('Save').click();
-  // the progress bar will take a while to finish when practitioner has too many open cases, so
-  // we check these intervals to prevent the cy.get('.progress-indicator').should('not.exist'); from timing out.
-  [25, 50, 75].forEach(expectedValue => {
-    cy.get('.progress-text').should(div => {
-      expect(parseInt(div.get(0).innerText.split('%')[0])).to.gt(expectedValue);
-    });
+
+  // when the practitioner has very little open cases, the progress bar is too quick and
+  // is gone before cypress has time to check it
+  cy.get('body').then($body => {
+    // synchronously query for element
+    if (!$body.find('.usa-alert--success').length) {
+      // the progress bar will take a while to finish when practitioner has too many open cases, so
+      // we check these intervals to prevent the cy.get('.progress-indicator').should('not.exist'); from timing out.
+      [25, 50, 75].forEach(expectedValue => {
+        cy.get('.progress-text').should(div => {
+          expect(parseInt(div.get(0).innerText.split('%')[0])).to.gt(
+            expectedValue,
+          );
+        });
+      });
+    }
   });
+
   cy.get('.progress-indicator').should('not.exist');
   cy.get('.progress-user-contact-edit').should('not.exist');
   cy.get('.usa-alert--success').should('exist');
