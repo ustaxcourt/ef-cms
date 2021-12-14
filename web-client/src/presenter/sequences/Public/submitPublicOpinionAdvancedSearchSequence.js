84654294fa38b58e1efd7c2be2beb790ac05911f
Rachel Schneiderman <rschneiderman379@gmail.com>
2021-12-14 11:31:11 -0500

    9261: Check if search is enabled when submitting on public

--- web-client/src/presenter/sequences/Public/submitPublicOpinionAdvancedSearchSequence.js
+++ web-client/src/presenter/sequences/Public/submitPublicOpinionAdvancedSearchSequence.js
@@ -1,8 +1,12 @@
 import { clearAlertsAction } from '../../actions/clearAlertsAction';
 import { clearSearchResultsAction } from '../../actions/AdvancedSearch/clearSearchResultsAction';
 import { clearSearchTermAction } from '../../actions/clearSearchTermAction';
+import { getConstants } from '../../../getConstants';
+import { getFeatureFlagValueFactoryAction } from '../../actions/getFeatureFlagValueFactoryAction';
 import { setAdvancedSearchResultsAction } from '../../actions/AdvancedSearch/setAdvancedSearchResultsAction';
 import { setAlertErrorAction } from '../../actions/setAlertErrorAction';
+import { setAlertWarningAction } from '../../actions/setAlertWarningAction';
+import { setDefaultAdvancedSearchTabAction } from '../../actions/setDefaultAdvancedSearchTabAction';
 import { setValidationErrorsAction } from '../../actions/setValidationErrorsAction';
 import { showProgressSequenceDecorator } from '../../utilities/showProgressSequenceDecorator';
 import { startShowValidationAction } from '../../actions/startShowValidationAction';
@@ -10,20 +14,27 @@ import { submitPublicOpinionAdvancedSearchAction } from '../../actions/Public/su
 import { validateOpinionAdvancedSearchAction } from '../../actions/AdvancedSearch/validateOpinionAdvancedSearchAction';
 
 export const submitPublicOpinionAdvancedSearchSequence = [
-  // TODO - check if search enabled?
-  clearSearchTermAction,
-  validateOpinionAdvancedSearchAction,
+  getFeatureFlagValueFactoryAction(
+    getConstants().ALLOWLIST_FEATURE_FLAGS.EXTERNAL_OPINION_SEARCH,
+  ),
   {
-    error: [
-      setAlertErrorAction,
-      setValidationErrorsAction,
-      clearSearchResultsAction,
-      startShowValidationAction,
+    no: [setAlertWarningAction, setDefaultAdvancedSearchTabAction],
+    yes: [
+      clearSearchTermAction,
+      validateOpinionAdvancedSearchAction,
+      {
+        error: [
+          setAlertErrorAction,
+          setValidationErrorsAction,
+          clearSearchResultsAction,
+          startShowValidationAction,
+        ],
+        success: showProgressSequenceDecorator([
+          clearAlertsAction,
+          submitPublicOpinionAdvancedSearchAction,
+          setAdvancedSearchResultsAction,
+        ]),
+      },
     ],
-    success: showProgressSequenceDecorator([
-      clearAlertsAction,
-      submitPublicOpinionAdvancedSearchAction,
-      setAdvancedSearchResultsAction,
-    ]),
   },
 ];
--- web-client/src/presenter/sequences/Public/submitPublicOrderAdvancedSearchSequence.js
+++ web-client/src/presenter/sequences/Public/submitPublicOrderAdvancedSearchSequence.js
@@ -1,8 +1,12 @@
 import { clearAlertsAction } from '../../actions/clearAlertsAction';
 import { clearSearchResultsAction } from '../../actions/AdvancedSearch/clearSearchResultsAction';
 import { clearSearchTermAction } from '../../actions/clearSearchTermAction';
+import { getConstants } from '../../../getConstants';
+import { getFeatureFlagValueFactoryAction } from '../../actions/getFeatureFlagValueFactoryAction';
 import { setAdvancedSearchResultsAction } from '../../actions/AdvancedSearch/setAdvancedSearchResultsAction';
 import { setAlertErrorAction } from '../../actions/setAlertErrorAction';
+import { setAlertWarningAction } from '../../actions/setAlertWarningAction';
+import { setDefaultAdvancedSearchTabAction } from '../../actions/setDefaultAdvancedSearchTabAction';
 import { setValidationErrorsAction } from '../../actions/setValidationErrorsAction';
 import { showProgressSequenceDecorator } from '../../utilities/showProgressSequenceDecorator';
 import { startShowValidationAction } from '../../actions/startShowValidationAction';
@@ -10,20 +14,27 @@ import { submitPublicOrderAdvancedSearchAction } from '../../actions/Public/subm
 import { validateOrderAdvancedSearchAction } from '../../actions/AdvancedSearch/validateOrderAdvancedSearchAction';
 
 export const submitPublicOrderAdvancedSearchSequence = [
-  // TODO - check if search enabled?
-  clearSearchTermAction,
-  validateOrderAdvancedSearchAction,
+  getFeatureFlagValueFactoryAction(
+    getConstants().ALLOWLIST_FEATURE_FLAGS.EXTERNAL_OPINION_SEARCH,
+  ),
   {
-    error: [
-      setAlertErrorAction,
-      setValidationErrorsAction,
-      clearSearchResultsAction,
-      startShowValidationAction,
+    no: [setAlertWarningAction, setDefaultAdvancedSearchTabAction],
+    yes: [
+      clearSearchTermAction,
+      validateOrderAdvancedSearchAction,
+      {
+        error: [
+          setAlertErrorAction,
+          setValidationErrorsAction,
+          clearSearchResultsAction,
+          startShowValidationAction,
+        ],
+        success: showProgressSequenceDecorator([
+          clearAlertsAction,
+          submitPublicOrderAdvancedSearchAction,
+          setAdvancedSearchResultsAction,
+        ]),
+      },
     ],
-    success: showProgressSequenceDecorator([
-      clearAlertsAction,
-      submitPublicOrderAdvancedSearchAction,
-      setAdvancedSearchResultsAction,
-    ]),
   },
 ];
