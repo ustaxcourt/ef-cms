2090adfd25498338c3655b67eabd791090b8cdae
rosie highsmith <ahighsmith@flexion.us>
2021-12-14 09:55:19 -0800

    9210: update integration test for public opinion search

--- web-client/integration-tests-public/journey/unauthedUserSearchesForOpinionByKeyword.js
+++ web-client/integration-tests-public/journey/unauthedUserSearchesForOpinionByKeyword.js
@@ -1,4 +1,7 @@
-import { ADVANCED_SEARCH_TABS } from '../../../shared/src/business/entities/EntityConstants';
+import {
+  ADVANCED_SEARCH_TABS,
+  DATE_RANGE_SEARCH_OPTIONS,
+} from '../../../shared/src/business/entities/EntityConstants';
 import { refreshElasticsearchIndex } from '../../integration-tests/helpers';
 
 export const unauthedUserSearchesForOpinionByKeyword = cerebralTest => {
@@ -9,22 +12,9 @@ export const unauthedUserSearchesForOpinionByKeyword = cerebralTest => {
 
     cerebralTest.setState('advancedSearchForm', {
       opinionSearch: {
+        dateRange: DATE_RANGE_SEARCH_OPTIONS.CUSTOM_DATES,
         keyword: 'osteodontolignikeratic',
-        startDate: '01/01/2001',
-      },
-    });
-
-    await cerebralTest.runSequence('submitPublicOpinionAdvancedSearchSequence');
-
-    expect(cerebralTest.getState('validationErrors')).toEqual({});
-    expect(
-      cerebralTest.getState(`searchResults.${ADVANCED_SEARCH_TABS.OPINION}`),
-    ).toEqual([]);
-
-    cerebralTest.setState('advancedSearchForm', {
-      opinionSearch: {
-        keyword: 'opinion',
-        opinionTypes: ['Memorandum Opinion'],
+        opinionTypes: { SOP: true },
         startDate: '01/01/2001',
       },
     });
@@ -39,8 +29,9 @@ export const unauthedUserSearchesForOpinionByKeyword = cerebralTest => {
     // search for an opinion on a sealed case
     cerebralTest.setState('advancedSearchForm', {
       opinionSearch: {
+        dateRange: DATE_RANGE_SEARCH_OPTIONS.CUSTOM_DATES,
         keyword: 'opinion',
-        opinionTypes: ['T.C. Opinion'],
+        opinionTypes: { TCOP: true },
         startDate: '01/01/2001',
       },
     });
@@ -49,7 +40,7 @@ export const unauthedUserSearchesForOpinionByKeyword = cerebralTest => {
 
     expect(
       cerebralTest.getState(`searchResults.${ADVANCED_SEARCH_TABS.OPINION}`),
-    ).toEqual(
+    ).toMatchObject(
       expect.arrayContaining([
         expect.objectContaining({
           docketEntryId: '130a3790-7e82-4f5c-8158-17f5d9d560e7',
