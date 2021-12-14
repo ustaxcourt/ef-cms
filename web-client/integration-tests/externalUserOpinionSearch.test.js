595d568f472ca66b03d5c5fbedf3929adb90aabd
Absolutestunna <joshujah007@gmail.com>
2021-12-14 15:30:55 -0500

    9211: modifying test for public opinion search [skip ci]

--- web-client/integration-tests/externalUserOpinionSearch.test.js
+++ web-client/integration-tests/externalUserOpinionSearch.test.js
@@ -16,7 +16,28 @@ describe('verify opinion search works for external users', () => {
   });
 
   describe('private practitioner performs opinion search', () => {
-    //Private/IRS practitioner accesses Opinion Search, searches for keyword/phrase, results list is returned. User clicks on result, which opens in a new tab
-    loginAs(cerebralTest, 'privatePractitioner@example.com');
+    it('should return an opinion from a sealed case', async () => {
+      //Private/IRS practitioner accesses Opinion Search, searches for keyword/phrase, results list is returned. User clicks on result, which opens in a new tab
+      loginAs(cerebralTest, 'privatePractitioner@example.com');
+
+      // Private/IRS practitioner accesses Opinion Search, searches for Opinion in sealed case, Opinion is returned in results list.
+      await cerebralTest.runSequence('gotoAdvancedSearchSequence');
+      // cerebralTest.setState('advancedSearchTab', ADVANCED_SEARCH_TABS.OPINION);
+
+      // get access for an opinion data on a sealed case
+      await cerebralTest.runSequence(
+        'updateAdvancedOpinionSearchFormValueSequence',
+        {
+          key: 'keyword',
+          value: 'sunglasses',
+        },
+      );
+
+      await cerebralTest.runSequence('submitOpinionAdvancedSearchSequence');
+
+      // cerebralTest.getState(`searchResults.${ADVANCED_SEARCH_TABS.OPINION}`),
+
+      // const stateOfAdvancedSearch = cerebralTest.getState('advancedSearchTab');
+    });
   });
 });
