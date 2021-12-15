4a780227b0726f796305df7a1e39d42414284a93
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2021-12-15 09:35:26 -0600

    9211: Adding test for not matching keyword and one for no keyword entered. [skip ci]

--- web-client/integration-tests/externalUserOpinionSearch.test.js
+++ web-client/integration-tests/externalUserOpinionSearch.test.js
@@ -53,5 +53,86 @@ describe('verify opinion search works for external users', () => {
         ]),
       );
     });
+
+    it('should not return an opinion that does not match', async () => {
+      // Private/IRS practitioner accesses Opinion Search, searches for Opinion in sealed case, Opinion is returned in results list.
+      await cerebralTest.runSequence('gotoAdvancedSearchSequence');
+      cerebralTest.setState('advancedSearchTab', ADVANCED_SEARCH_TABS.OPINION);
+
+      await updateOpinionForm(cerebralTest, {
+        dateRange: DATE_RANGE_SEARCH_OPTIONS.ALL_DATES,
+        keyword: 'sunglasses',
+        opinionTypes: {
+          [ADVANCED_SEARCH_OPINION_TYPES.Memorandum]: true,
+          [ADVANCED_SEARCH_OPINION_TYPES.Bench]: true,
+          [ADVANCED_SEARCH_OPINION_TYPES.Summary]: true,
+          [ADVANCED_SEARCH_OPINION_TYPES['T.C.']]: true,
+        },
+      });
+
+      await cerebralTest.runSequence('submitOpinionAdvancedSearchSequence');
+      expect(cerebralTest.getState('validationErrors')).toEqual({});
+
+      const stateOfAdvancedSearch = cerebralTest.getState(
+        `searchResults.${ADVANCED_SEARCH_TABS.OPINION}`,
+      );
+
+      expect(stateOfAdvancedSearch).not.toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            docketEntryId: '130a3790-7e82-4f5c-8158-17f5d9d560e7',
+            documentTitle:
+              'T.C. Opinion Judge Colvin Some very strong opinions about sunglasses',
+          }),
+          expect.objectContaining({
+            docketEntryId: 'd085a9da-b4a6-41d2-aa40-f933fe2d4188',
+            documentTitle:
+              'Summary Opinion Judge Ashford An opinion for testing',
+          }),
+        ]),
+      );
+    });
+
+    //Private/IRS practitioner accesses Opinion Search, searches using no keyword/phrase. Results list is returned.
+    it('should return results with no keyword/phrase', async () => {
+      await cerebralTest.runSequence('gotoAdvancedSearchSequence');
+      cerebralTest.setState('advancedSearchTab', ADVANCED_SEARCH_TABS.OPINION);
+
+      await updateOpinionForm(cerebralTest, {
+        dateRange: DATE_RANGE_SEARCH_OPTIONS.ALL_DATES,
+        opinionTypes: {
+          [ADVANCED_SEARCH_OPINION_TYPES.Memorandum]: true,
+          [ADVANCED_SEARCH_OPINION_TYPES.Bench]: true,
+          [ADVANCED_SEARCH_OPINION_TYPES.Summary]: true,
+          [ADVANCED_SEARCH_OPINION_TYPES['T.C.']]: true,
+        },
+      });
+
+      await cerebralTest.runSequence('submitOpinionAdvancedSearchSequence');
+      expect(cerebralTest.getState('validationErrors')).toEqual({});
+
+      const stateOfAdvancedSearch = cerebralTest.getState(
+        `searchResults.${ADVANCED_SEARCH_TABS.OPINION}`,
+      );
+
+      console.log(
+        'results',
+        cerebralTest.getState(`searchResults.${ADVANCED_SEARCH_TABS.OPINION}`),
+      );
+      expect(stateOfAdvancedSearch).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            docketEntryId: '130a3790-7e82-4f5c-8158-17f5d9d560e7',
+            documentTitle:
+              'T.C. Opinion Judge Colvin Some very strong opinions about sunglasses',
+          }),
+          expect.objectContaining({
+            docketEntryId: 'd085a9da-b4a6-41d2-aa40-f933fe2d4188',
+            documentTitle:
+              'Summary Opinion Judge Ashford An opinion for testing',
+          }),
+        ]),
+      );
+    });
   });
 });
