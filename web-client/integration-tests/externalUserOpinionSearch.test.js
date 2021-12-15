fe772c2e74ae238659f0e80ca5368db90f0d168a
Absolutestunna <joshujah007@gmail.com>
2021-12-15 12:49:58 -0500

    9211: added tests for keyword search and docket number (combo) [skip ci]

--- web-client/integration-tests/externalUserOpinionSearch.test.js
+++ web-client/integration-tests/externalUserOpinionSearch.test.js
@@ -60,7 +60,6 @@ describe('verify opinion search works for external users', () => {
       cerebralTest.setState('advancedSearchTab', ADVANCED_SEARCH_TABS.OPINION);
 
       await updateOpinionForm(cerebralTest, {
-        dateRange: DATE_RANGE_SEARCH_OPTIONS.ALL_DATES,
         keyword: 'sunglasses',
         opinionTypes: {
           [ADVANCED_SEARCH_OPINION_TYPES.Memorandum]: true,
@@ -101,8 +100,6 @@ describe('verify opinion search works for external users', () => {
       await updateOpinionForm(cerebralTest, {
         dateRange: DATE_RANGE_SEARCH_OPTIONS.ALL_DATES,
         opinionTypes: {
-          [ADVANCED_SEARCH_OPINION_TYPES.Memorandum]: true,
-          [ADVANCED_SEARCH_OPINION_TYPES.Bench]: true,
           [ADVANCED_SEARCH_OPINION_TYPES.Summary]: true,
           [ADVANCED_SEARCH_OPINION_TYPES['T.C.']]: true,
         },
@@ -115,10 +112,6 @@ describe('verify opinion search works for external users', () => {
         `searchResults.${ADVANCED_SEARCH_TABS.OPINION}`,
       );
 
-      console.log(
-        'results',
-        cerebralTest.getState(`searchResults.${ADVANCED_SEARCH_TABS.OPINION}`),
-      );
       expect(stateOfAdvancedSearch).toMatchObject(
         expect.arrayContaining([
           expect.objectContaining({
@@ -135,7 +128,61 @@ describe('verify opinion search works for external users', () => {
       );
     });
 
-    // Private/IRS practitioner accesses Opinion Search, searches using combination of keyword/phrase and filters. Results list is returned.
-    it('should return results with keyword/phrase and filters', async () => {});
+    describe('keywords/phrases AND filters for docket record and Petitioner/case name', () => {
+      // Private/IRS practitioner accesses Opinion Search, searches using combination of keyword/phrase and filters. Results list is returned.
+      it('should return results with keyword/phrase and filters for docket record', async () => {
+        await cerebralTest.runSequence('gotoAdvancedSearchSequence');
+        cerebralTest.setState(
+          'advancedSearchTab',
+          ADVANCED_SEARCH_TABS.OPINION,
+        );
+
+        await updateOpinionForm(cerebralTest, {
+          dateRange: DATE_RANGE_SEARCH_OPTIONS.ALL_DATES,
+          docketNumber: '105-20',
+          keyword: 'sunglasses',
+        });
+
+        await cerebralTest.runSequence('submitOpinionAdvancedSearchSequence');
+        expect(cerebralTest.getState('validationErrors')).toEqual({});
+
+        const stateOfAdvancedSearch = cerebralTest.getState(
+          `searchResults.${ADVANCED_SEARCH_TABS.OPINION}`,
+        );
+
+        expect(stateOfAdvancedSearch).toMatchObject(
+          expect.arrayContaining([
+            expect.objectContaining({
+              docketEntryId: '130a3790-7e82-4f5c-8158-17f5d9d560e7',
+              documentTitle:
+                'T.C. Opinion Judge Colvin Some very strong opinions about sunglasses',
+            }),
+          ]),
+        );
+      });
+
+      it('should NOT return results with keyword/phrase and filters for docket record', async () => {
+        await cerebralTest.runSequence('gotoAdvancedSearchSequence');
+        cerebralTest.setState(
+          'advancedSearchTab',
+          ADVANCED_SEARCH_TABS.OPINION,
+        );
+
+        await updateOpinionForm(cerebralTest, {
+          dateRange: DATE_RANGE_SEARCH_OPTIONS.ALL_DATES,
+          docketNumber: '200-20',
+          keyword: 'sunglasses',
+        });
+
+        await cerebralTest.runSequence('submitOpinionAdvancedSearchSequence');
+        expect(cerebralTest.getState('validationErrors')).toEqual({});
+
+        const stateOfAdvancedSearch = cerebralTest.getState(
+          `searchResults.${ADVANCED_SEARCH_TABS.OPINION}`,
+        );
+
+        expect(stateOfAdvancedSearch).toEqual([]);
+      });
+    });
   });
 });
