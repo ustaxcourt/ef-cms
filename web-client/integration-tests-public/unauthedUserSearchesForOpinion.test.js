ae908c8d20544b6edbeb64f35e8ee46feb472346
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2021-12-16 11:22:32 -0700

    9210 [skip ci] adding test for unauthed user searches for sealed opinion

--- web-client/integration-tests-public/unauthedUserSearchesForOpinion.test.js
+++ web-client/integration-tests-public/unauthedUserSearchesForOpinion.test.js
@@ -1,3 +1,8 @@
+import { ADVANCED_SEARCH_TABS } from '../../shared/src/business/entities/EntityConstants';
+import {
+  refreshElasticsearchIndex,
+  updateOpinionForm,
+} from '../integration-tests/helpers';
 import { setupTest } from './helpers';
 import { unauthedUserInvalidSearchForOpinion } from './journey/unauthedUserInvalidSearchForOpinion';
 import { unauthedUserNavigatesToPublicSite } from './journey/unauthedUserNavigatesToPublicSite';
@@ -9,4 +14,27 @@ describe('Unauthed user searches for an opinion by keyword', () => {
   unauthedUserNavigatesToPublicSite(cerebralTest);
   unauthedUserInvalidSearchForOpinion(cerebralTest);
   unauthedUserSearchesForOpinionByKeyword(cerebralTest);
+});
+
+describe('Unauthed user searches for a legacy sealed opinion by keyword', () => {
+  cerebralTest.docketNumber = '129-20';
+
+  unauthedUserNavigatesToPublicSite(cerebralTest);
+
+  it('public user searches for opinion by docket number', async () => {
+    await refreshElasticsearchIndex();
+    cerebralTest.setState('advancedSearchTab', ADVANCED_SEARCH_TABS.OPINION);
+
+    updateOpinionForm(cerebralTest, {
+      docketNumber: cerebralTest.docketNumber,
+    });
+
+    await cerebralTest.runSequence('submitPublicOpinionAdvancedSearchSequence');
+
+    expect(cerebralTest.getState('validationErrors')).toEqual({});
+
+    expect(
+      cerebralTest.getState(`searchResults.${ADVANCED_SEARCH_TABS.OPINION}`),
+    ).toEqual([]);
+  });
 });
