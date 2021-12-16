d5ff21aee62cab6bdafa612f7f64f58c5cb95551
halprin <halprin@users.noreply.github.com>
2021-12-16 12:48:41 -0700

    9278: Improve Cypress integration test for legacy judges to check every search result row

--- cypress-integration/integration/public/advanced-search.spec.js
+++ cypress-integration/integration/public/advanced-search.spec.js
@@ -53,11 +53,24 @@ describe('Advanced search', () => {
 
   describe('order', () => {
     it('should be able to search for an order by legacy judge', () => {
+      const judgeNameColumnIndex = 5;
+      const wantedLegacyJudge = 'Fieri';
+
       navigateToDashboard();
       clickOnSearchTab('order');
-      searchForOrderByJudge('Fieri');
+      searchForOrderByJudge(wantedLegacyJudge);
       searchForDocuments();
+
       expect(searchResultsTable()).to.exist;
+
+      //assert that every judge in the search result list is the wanted legacy judge
+      cy.get('tr.search-result').each(element => {
+        cy.wrap(element).within(() => {
+          cy.get('td')
+            .eq(judgeNameColumnIndex)
+            .should('have.text', wantedLegacyJudge);
+        });
+      });
     });
   });
 });
