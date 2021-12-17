e346fcfb7d33ff76fc02ace06a400dcc19e85171
Cody Seibert <cseibert113@gmail.com>
2021-12-17 10:17:14 -0500

    removing test migration

--- web-api/migration-terraform/main/lambdas/migrationsToRun.js
+++ web-api/migration-terraform/main/lambdas/migrationsToRun.js
@@ -1,12 +1,10 @@
 const {
   migrateItems: migration0003,
 } = require('./migrations/0003-case-has-sealed-documents');
-const { migrateItems: migration0004 } = require('./migrations/0004-testing');
 
 // MODIFY THIS ARRAY TO ADD NEW MIGRATIONS OR REMOVE OLD ONES
 const migrationsToRun = [
   { key: '0003-case-has-sealed-documents.js', script: migration0003 },
-  { key: '0004-testing.js', script: migration0004 },
 ];
 
 exports.migrationsToRun = migrationsToRun;
