d2631a427c1e5aab0674b42d212defd2e2a1ceba
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2021-12-17 16:40:31 -0600

    none: parameterized search and expected values for userSearchesForOpinionByDocketNumber. [skip ci]

--- web-client/integration-tests/journey/userSearchesForOpinionByDocketNumber.js
+++ web-client/integration-tests/journey/userSearchesForOpinionByDocketNumber.js
@@ -1,14 +1,18 @@
 import { ADVANCED_SEARCH_TABS } from '../../../shared/src/business/entities/EntityConstants';
 import { updateOpinionForm } from '../helpers';
 
-export const userSearchesForOpinionByDocketNumber = cerebralTest => {
+export const userSearchesForOpinionByDocketNumber = (
+  cerebralTest,
+  searchParams,
+  expectedObjectContents,
+) => {
   return it('should return a list of opinions', async () => {
+    console.log('*****searchParams*****', searchParams);
+    console.log('*****expectedObjectContents*****', expectedObjectContents);
     await cerebralTest.runSequence('gotoAdvancedSearchSequence');
     cerebralTest.setState('advancedSearchTab', ADVANCED_SEARCH_TABS.OPINION);
 
-    await updateOpinionForm(cerebralTest, {
-      docketNumber: cerebralTest.docketNumber,
-    });
+    await updateOpinionForm(cerebralTest, searchParams);
 
     await cerebralTest.runSequence('submitOpinionAdvancedSearchSequence');
     expect(cerebralTest.getState('validationErrors')).toEqual({});
@@ -18,11 +22,7 @@ export const userSearchesForOpinionByDocketNumber = cerebralTest => {
     );
 
     expect(stateOfAdvancedSearch).toMatchObject(
-      expect.arrayContaining([
-        expect.objectContaining({
-          docketEntryId: cerebralTest.docketEntryId,
-        }),
-      ]),
+      expect.arrayContaining([expect.objectContaining(expectedObjectContents)]),
     );
   });
 };
