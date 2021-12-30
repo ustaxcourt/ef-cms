fa5d16080d6ccd9a1f53c95881bb33af6fbd5922
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2021-12-30 09:29:24 -0600

    8516: adding a new test that just checks that the page reloads which 'warms up' cypress local storage behavior.

--- web-client/src/presenter/actions/reloadPageAction.js
+++ web-client/src/presenter/actions/reloadPageAction.js
@@ -2,12 +2,5 @@
  * Reloads the page
  */
 export const reloadPageAction = () => {
-  // we wrap this in a timeout because sometimes the page can refresh faster
-  // than cerebral can finish writing to localStorage
-  return new Promise(resolve => {
-    setTimeout(() => {
-      window.location.reload();
-      resolve();
-    }, 100);
-  });
+  window.location.reload();
 };
