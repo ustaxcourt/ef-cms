53aaee4cc08f9881f8c67044ef5a67c039e5f81d
Cody Seibert <cseibert113@gmail.com>
2021-12-30 09:50:02 -0500

    8516 - sometimes page can reload too fast and data is missing from localStorage

--- web-client/src/presenter/actions/reloadPageAction.js
+++ web-client/src/presenter/actions/reloadPageAction.js
@@ -2,5 +2,12 @@
  * Reloads the page
  */
 export const reloadPageAction = () => {
-  location.reload();
+  // we wrap this in a timeout because sometimes the page can refresh faster
+  // than cerebral can finish writing to localStorage
+  return new Promise(resolve => {
+    setTimeout(() => {
+      window.location.reload();
+      resolve();
+    }, 100);
+  });
 };
