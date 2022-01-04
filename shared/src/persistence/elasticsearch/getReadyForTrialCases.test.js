b2c4e41cad8d823b7fb5ee27a9547f59a3487716
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2022-01-04 16:53:15 -0600

    9310: Testing for the new docket entry search instead of case search.

--- shared/src/persistence/elasticsearch/getReadyForTrialCases.test.js
+++ shared/src/persistence/elasticsearch/getReadyForTrialCases.test.js
@@ -9,7 +9,7 @@ jest.mock('./searchClient');
 const { search } = require('./searchClient');
 
 describe('getReadyForTrialCases', () => {
-  it('should search for all cases whose status is `General Docket - Not at Issue`', async () => {
+  it('should search for docket entries of type `Answer` which were served greater than 45 days ago and whose case status is `General Docket - Not at Issue`', async () => {
     search.mockResolvedValue({
       results: [{ docketNumber: '102-20' }, { docketNumber: '134-30' }],
       total: 2,
@@ -20,8 +20,48 @@ describe('getReadyForTrialCases', () => {
     });
 
     expect(search.mock.calls[0][0].searchParameters.body.query).toMatchObject({
-      term: {
-        'status.S': CASE_STATUS_TYPES.generalDocket,
+      bool: {
+        filter: [
+          {
+            term: {
+              'entityName.S': 'DocketEntry',
+            },
+          },
+          {
+            exists: {
+              field: 'servedAt',
+            },
+          },
+        ],
+        must: [
+          {
+            term: {
+              'documentType.S': 'Answer',
+            },
+          },
+          {
+            range: {
+              'servedAt.S': {
+                lte: 'now-44d/d',
+              },
+            },
+          },
+          {
+            simple_query_string: {
+              default_operator: 'and',
+              fields: ['status.S'],
+              flags: 'OR|AND|ESCAPE|PHRASE',
+              query: CASE_STATUS_TYPES.generalDocket,
+            },
+          },
+        ],
+        must_not: [
+          {
+            term: {
+              'isStricken.BOOL': true,
+            },
+          },
+        ],
       },
     });
   });
