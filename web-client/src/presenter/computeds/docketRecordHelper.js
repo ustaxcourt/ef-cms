91c7983de4d27f5ee515bbcb91d0c6203f586a0a
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-01-26 13:00:13 -0800

    4252: Adding tests for docketRecordHelper

--- web-client/src/presenter/computeds/docketRecordHelper.js
+++ web-client/src/presenter/computeds/docketRecordHelper.js
@@ -2,8 +2,9 @@ import { state } from 'cerebral';
 
 export const docketRecordHelper = get => {
   const permissions = get(state.permissions);
-  const showPrintableDocketRecord =
-    get(state.caseDetail.canAllowPrintableDocketRecord) === true;
+  const showPrintableDocketRecord = get(
+    state.caseDetail.canAllowPrintableDocketRecord,
+  );
 
   return {
     showEditOrSealDocketRecordEntry:
