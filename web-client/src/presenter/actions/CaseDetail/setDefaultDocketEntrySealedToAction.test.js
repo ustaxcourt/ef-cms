42aac01591947c79b9720faeff525b7e6ebb747f
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-01-27 08:23:23 -0800

    4252: Fixing action test

--- web-client/src/presenter/actions/CaseDetail/setDefaultDocketEntrySealedToAction.test.js
+++ web-client/src/presenter/actions/CaseDetail/setDefaultDocketEntrySealedToAction.test.js
@@ -1,15 +1,22 @@
 import { applicationContextForClient as applicationContext } from '../../../../../shared/src/business/test/createTestApplicationContext';
+import { presenter } from '../../presenter-mock';
 import { runAction } from 'cerebral/test';
 import { setDefaultDocketEntrySealedToAction } from './setDefaultDocketEntrySealedToAction';
 
 describe('setDefaultDocketEntrySealedToAction', () => {
-  // todo: this test is failing after trying to use constants. i'm missing something but it can't find appContext
+  beforeAll(() => {
+    presenter.providers.applicationContext = applicationContext;
+  });
+
   it('should set the state.modal.docketEntrySealedTo to the value of "Public"', async () => {
     const { DOCKET_ENTRY_SEALED_TO_TYPES } = applicationContext.getConstants();
 
     const { state } = await runAction(setDefaultDocketEntrySealedToAction, {
       modal: {
         docketEntrySealedTo: 'This is private!!!!!',
+      },
+      modules: {
+        presenter,
       },
       state: {},
     });
