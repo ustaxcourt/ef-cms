7ba1ed61ea402cc026b66695955464ec7c1c7679
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-01-27 09:59:28 -0800

    4252: Removing comments and logs

--- shared/src/proxies/editDocketEntry/sealDocketEntryProxy.js
+++ shared/src/proxies/editDocketEntry/sealDocketEntryProxy.js
@@ -5,8 +5,9 @@ const { put } = require('../requests');
  *
  * @param {object} applicationContext the application context
  * @param {object} providers the providers object
- * @param {object} providers.docketNumber the docket number for a case
  * @param {object} providers.docketEntryId the docketEntryId to be stricken
+ * @param {object} providers.docketEntrySealedTo who to seal the docket entry from
+ * @param {object} providers.docketNumber the docket number for a case
  * @returns {Promise<*>} the promise of the api call
  */
 exports.sealDocketEntryInteractor = (
