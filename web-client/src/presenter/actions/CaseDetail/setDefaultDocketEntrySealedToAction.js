62acdfc0faa22fb5e4cfe445d126326a5aa33266
rosie highsmith <ahighsmith@flexion.us>
2022-01-26 16:15:07 -0800

    4252: fixing the last hardcoded 'Public' however def still WIP since test is failing for setDefaultDocketEntrySealedToAction [skip ci]

--- web-client/src/presenter/actions/CaseDetail/setDefaultDocketEntrySealedToAction.js
+++ web-client/src/presenter/actions/CaseDetail/setDefaultDocketEntrySealedToAction.js
@@ -1,11 +1,19 @@
 import { state } from 'cerebral';
 
 /**
- * Defaults state.modal.docketEntrySealedTo to 'Public'
+ * Defaults state.modal.docketEntrySealedTo to DOCKET_ENTRY_SEALED_TO_TYPES.PUBLIC
  *
  * @param {object} providers the providers object
  * @param {object} providers.store the cerebral store
  */
-export const setDefaultDocketEntrySealedToAction = ({ store }) => {
-  store.set(state.modal.docketEntrySealedTo, 'Public');
+export const setDefaultDocketEntrySealedToAction = ({
+  applicationContext,
+  store,
+}) => {
+  const { DOCKET_ENTRY_SEALED_TO_TYPES } = applicationContext.getConstants();
+
+  store.set(
+    state.modal.docketEntrySealedTo,
+    DOCKET_ENTRY_SEALED_TO_TYPES.PUBLIC,
+  );
 };
--- web-client/src/presenter/actions/CaseDetail/setDefaultDocketEntrySealedToAction.test.js
+++ web-client/src/presenter/actions/CaseDetail/setDefaultDocketEntrySealedToAction.test.js
@@ -1,8 +1,12 @@
+import { applicationContextForClient as applicationContext } from '../../../../../shared/src/business/test/createTestApplicationContext';
 import { runAction } from 'cerebral/test';
 import { setDefaultDocketEntrySealedToAction } from './setDefaultDocketEntrySealedToAction';
 
 describe('setDefaultDocketEntrySealedToAction', () => {
+  // todo: this test is failing after trying to use constants. i'm missing something but it can't find appContext
   it('should set the state.modal.docketEntrySealedTo to the value of "Public"', async () => {
+    const { DOCKET_ENTRY_SEALED_TO_TYPES } = applicationContext.getConstants();
+
     const { state } = await runAction(setDefaultDocketEntrySealedToAction, {
       modal: {
         docketEntrySealedTo: 'This is private!!!!!',
@@ -10,6 +14,8 @@ describe('setDefaultDocketEntrySealedToAction', () => {
       state: {},
     });
 
-    expect(state.modal.docketEntrySealedTo).toEqual('Public');
+    expect(state.modal.docketEntrySealedTo).toEqual(
+      DOCKET_ENTRY_SEALED_TO_TYPES.PUBLIC,
+    );
   });
 });
