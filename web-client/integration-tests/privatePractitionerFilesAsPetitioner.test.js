17cf3adce900b8ac09fed00caa2d82055905929b
halprin <halprin@users.noreply.github.com>
2022-02-01 15:36:59 -0700

    9323: Added comments for different petitioner removal scenarios [skip ci]

--- web-client/integration-tests/privatePractitionerFilesAsPetitioner.test.js
+++ web-client/integration-tests/privatePractitionerFilesAsPetitioner.test.js
@@ -1,4 +1,5 @@
 import { admissionsClerkEditsPetitionerEmail } from './journey/admissionsClerkEditsPetitionerEmail';
+import { docketClerkRemovesPetitionerFromCase } from './journey/docketClerkRemovesPetitionerFromCase';
 import { fakeFile } from '../integration-tests-public/helpers';
 import { loginAs, setupTest } from './helpers';
 import { petitionsClerkAddsPractitionersToCase } from './journey/petitionsClerkAddsPractitionersToCase';
@@ -32,7 +33,7 @@ describe('Bug 9323', () => {
     petitionsClerkAddsPractitionersToCase(cerebralTest, true);
   });
 
-  describe('BUG-9323 privatePractitioner remains on case as petitioner after removal', () => {
+  describe('BUG-9323 privatePractitioner remains on case as petitioner after practitioner removal', () => {
     const privatePractitionerEmail = 'privatePractitioner@example.com';
     const petitionsClerkEmail = 'petitionsclerk@example.com';
 
@@ -52,5 +53,29 @@ describe('Bug 9323', () => {
     practitionerViewsDashboard(cerebralTest);
   });
 
-  // TODO - Write inverse test of the one directly above
+  describe('BUG-9323 privatePractitioner remains on case as practitioner after petitioner removal', () => {
+    const privatePractitionerEmail = 'privatePractitioner@example.com';
+    const petitionsClerkEmail = 'petitionsclerk@example.com';
+
+    loginAs(cerebralTest, privatePractitionerEmail);
+    practitionerCreatesNewCase(cerebralTest, fakeFile); //test needs petition to have two petitioners which happens here
+
+    //1. represents both petitioners and they are one of those petitioners, doesn't matter which gets deleted = stays associated, stays privatepractioner associated
+    //2a. represents only themself and not the other petitioner, other petitioner deleted = stays associated, stays privatepractioner associated
+    //2b. represents only themself and not the other petitioner, themself petitioner deleted = not associated, not privatepractioner associated
+    //3a. they represent only the other petitioner that isn't themself, other petitioner deleted = stays associated, not privatepractioner associated
+    //3b. they represent only the other petitioner that isn't themself, themself petitioner deleted = stays associated, stays privatepractioner associated
+
+    loginAs(cerebralTest, petitionsClerkEmail);
+    petitionsClerkServesElectronicCaseToIrs(cerebralTest);
+
+    loginAs(cerebralTest, 'admissionsclerk@example.com');
+    admissionsClerkEditsPetitionerEmail(cerebralTest, privatePractitionerEmail);
+
+    loginAs(cerebralTest, 'docketclerk@example.com');
+    docketClerkRemovesPetitionerFromCase(cerebralTest);
+
+    loginAs(cerebralTest, privatePractitionerEmail);
+    practitionerViewsDashboard(cerebralTest);
+  });
 });
