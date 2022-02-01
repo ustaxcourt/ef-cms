6b232260166a9ee3b806258bb2fd5e4b7e25ba96
halprin <halprin@users.noreply.github.com>
2022-02-01 11:19:25 -0700

    9323: Fix Web Socket issue while running integration test

--- web-client/integration-tests/privatePractitionerFilesAsPetitioner.test.js
+++ web-client/integration-tests/privatePractitionerFilesAsPetitioner.test.js
@@ -10,9 +10,7 @@ import { practitionerViewsDashboard } from './journey/practitionerViewsDashboard
 
 const cerebralTest = setupTest();
 
-describe('privatePractitioner files as a petitioner', () => {
-  // const { COUNTRY_TYPES, PARTY_TYPES } = applicationContext.getConstants();
-
+describe('Bug 9323', () => {
   beforeAll(() => {
     jest.setTimeout(30000);
   });
@@ -21,54 +19,48 @@ describe('privatePractitioner files as a petitioner', () => {
     cerebralTest.closeSocket();
   });
 
-  //Order of operations
-  //1. Petitions clerk creates a petition with no e-mail for the petitioner
-  //2. Admissions clerk associates the private practitioner with the petitioner via private practitioner's e-mail
-  //3. Admissions clerk assigns the private practitioner to the petitioner on the case.
-  //4. Assert that the private practitioner to the petitioner on the case.
+  describe('privatePractitioner files as a petitioner', () => {
+    //Order of operations
+    //1. Petitions clerk creates a petition with no e-mail for the petitioner
+    //2. Admissions clerk associates the private practitioner with the petitioner via private practitioner's e-mail
+    //3. Admissions clerk assigns the private practitioner to the petitioner on the case.
+    //4. Assert that the private practitioner to the petitioner on the case.
 
-  loginAs(cerebralTest, 'petitionsclerk@example.com');
-  petitionsClerkCreatesNewCase(cerebralTest, fakeFile, undefined, true);
+    loginAs(cerebralTest, 'petitionsclerk@example.com');
+    petitionsClerkCreatesNewCase(cerebralTest, fakeFile, undefined, true);
 
-  loginAs(cerebralTest, 'admissionsclerk@example.com');
-  admissionsClerkEditsPetitionerEmail(
-    cerebralTest,
-    'privatePractitioner@example.com',
-  );
-  loginAs(cerebralTest, 'petitionsclerk@example.com');
-  petitionsClerkAddsPractitionersToCase(cerebralTest, true);
-});
-
-describe('BUG-9323 privatePractitioner remains on case as petitioner after removal', () => {
-  beforeAll(() => {
-    jest.setTimeout(30000);
+    loginAs(cerebralTest, 'admissionsclerk@example.com');
+    admissionsClerkEditsPetitionerEmail(
+      cerebralTest,
+      'privatePractitioner@example.com',
+    );
+    loginAs(cerebralTest, 'petitionsclerk@example.com');
+    petitionsClerkAddsPractitionersToCase(cerebralTest, true);
   });
 
-  afterAll(() => {
-    cerebralTest.closeSocket();
+  describe('BUG-9323 privatePractitioner remains on case as petitioner after removal', () => {
+    const privatePractitionerEmail = 'privatePractitioner@example.com';
+    const petitionsClerkEmail = 'petitionsclerk@example.com';
+
+    //Order of operations
+    //1. Practitioner files petition
+    //2. Admissions clerk associates the private practitioner with the petitioner via private practitioner's e-mail
+    //3. Any clerk removes privatePractitioner from the case
+    //4. Assert that the private practitioner remains the petitioner on the case
+
+    loginAs(privatePractitionerEmail);
+    practitionerCreatesNewCase(cerebralTest, fakeFile);
+
+    loginAs(cerebralTest, petitionsClerkEmail);
+    petitionsClerkServesElectronicCaseToIrs(cerebralTest);
+
+    loginAs(cerebralTest, 'admissionsclerk@example.com');
+    admissionsClerkEditsPetitionerEmail(cerebralTest, privatePractitionerEmail);
+
+    loginAs(cerebralTest, petitionsClerkEmail);
+    petitionsClerkRemovesPractitionerFromCase(cerebralTest);
+
+    loginAs(privatePractitionerEmail);
+    practitionerViewsDashboard(cerebralTest);
   });
-
-  const privatePractitionerEmail = 'privatePractitioner@example.com';
-  const petitionsClerkEmail = 'petitionsclerk@example.com';
-
-  //Order of operations
-  //1. Practitioner files petition
-  //2. Admissions clerk associates the private practitioner with the petitioner via private practitioner's e-mail
-  //3. Any clerk removes privatePractitioner from the case
-  //4. Assert that the private practitioner remains the petitioner on the case
-
-  loginAs(privatePractitionerEmail);
-  practitionerCreatesNewCase(cerebralTest, fakeFile);
-
-  loginAs(cerebralTest, petitionsClerkEmail);
-  petitionsClerkServesElectronicCaseToIrs(cerebralTest);
-
-  loginAs(cerebralTest, 'admissionsclerk@example.com');
-  admissionsClerkEditsPetitionerEmail(cerebralTest, privatePractitionerEmail);
-
-  loginAs(cerebralTest, petitionsClerkEmail);
-  petitionsClerkRemovesPractitionerFromCase(cerebralTest);
-
-  loginAs(privatePractitionerEmail);
-  practitionerViewsDashboard(cerebralTest);
 });
