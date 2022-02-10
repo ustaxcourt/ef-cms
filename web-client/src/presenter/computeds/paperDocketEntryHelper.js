f133398761c523786b75cee9848ea7fcf6e4d6ab
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-02-10 10:39:25 -0800

    Devex 859: Docket entries and correspondence on cases are defaulted to empty lists, should not need to default to [] in helpers

--- web-client/src/presenter/computeds/paperDocketEntryHelper.js
+++ web-client/src/presenter/computeds/paperDocketEntryHelper.js
@@ -1,34 +1,32 @@
 import { state } from 'cerebral';
 
 export const paperDocketEntryHelper = (get, applicationContext) => {
-  const docketEntryId = get(state.docketEntryId);
-  const isEditingDocketEntry = get(state.isEditingDocketEntry);
   const caseDetail = get(state.caseDetail);
+  const docketEntryId = get(state.docketEntryId);
   const documentUploadMode = get(state.currentViewMetadata.documentUploadMode);
+  const isEditingDocketEntry = get(state.isEditingDocketEntry);
 
   const canAllowDocumentServiceForCase = applicationContext
     .getUtilities()
     .canAllowDocumentServiceForCase(caseDetail);
 
   const allCaseDocuments = [
-    ...(caseDetail.docketEntries || []),
-    ...(caseDetail.correspondence || []),
+    ...caseDetail.docketEntries,
+    ...caseDetail.correspondence,
   ];
+
   const doc = allCaseDocuments.find(
     item => item.docketEntryId === docketEntryId,
   );
-
-  const docketEntryHasDocument =
-    doc && doc.isFileAttached && documentUploadMode === 'preview';
-
-  const showServiceWarning = !canAllowDocumentServiceForCase;
-  const showSaveAndServeButton = canAllowDocumentServiceForCase;
+  const docketEntryHasDocument = !!(doc && doc.isFileAttached);
+  const showAddDocumentWarning =
+    isEditingDocketEntry &&
+    !docketEntryHasDocument &&
+    documentUploadMode === 'preview';
 
   return {
-    docketEntryHasDocument,
-    isEditingDocketEntry,
-    showAddDocumentWarning: isEditingDocketEntry && !docketEntryHasDocument,
-    showSaveAndServeButton,
-    showServiceWarning,
+    showAddDocumentWarning,
+    showSaveAndServeButton: canAllowDocumentServiceForCase,
+    showServiceWarning: !canAllowDocumentServiceForCase,
   };
 };
--- web-client/src/presenter/computeds/paperDocketEntryHelper.test.js
+++ web-client/src/presenter/computeds/paperDocketEntryHelper.test.js
@@ -1,132 +1,173 @@
-import {
-  CASE_STATUS_TYPES,
-  INITIAL_DOCUMENT_TYPES,
-} from '../../../../shared/src/business/entities/EntityConstants';
-import { applicationContext } from '../../applicationContext';
+import { applicationContextForClient as applicationContext } from '../../../../shared/src/business/test/createTestApplicationContext';
 import { paperDocketEntryHelper as paperDocketEntryHelperComputed } from './paperDocketEntryHelper';
 import { runCompute } from 'cerebral/test';
 import { withAppContextDecorator } from '../../../src/withAppContext';
 
-const paperDocketEntryHelper = withAppContextDecorator(
-  paperDocketEntryHelperComputed,
-  applicationContext,
-);
 describe('paperDocketEntryHelper', () => {
-  it('should set showAddDocumentWarning to TRUE if docketEntryId is NOT set, documentUploadMode is not preview, and user is editing docket entry', () => {
-    const result = runCompute(paperDocketEntryHelper, {
-      state: {
-        caseDetail: {},
-        currentViewMetadata: {
-          documentUploadMode: 'scan',
+  const mockDocketEntryId = '3db0329d-e0ae-4cf7-8416-509818e5d92f';
+
+  const paperDocketEntryHelper = withAppContextDecorator(
+    paperDocketEntryHelperComputed,
+    applicationContext,
+  );
+
+  describe('showAddDocumentWarning', () => {
+    it('should be false when the user is not editing a docket entry', () => {
+      const result = runCompute(paperDocketEntryHelper, {
+        state: {
+          caseDetail: {
+            correspondence: [],
+            docketEntries: [
+              {
+                docketEntryId: mockDocketEntryId,
+                isFileAttached: false,
+              },
+            ],
+          },
+          currentViewMetadata: {
+            documentUploadMode: 'preview',
+          },
+          docketEntryId: mockDocketEntryId,
+          isEditingDocketEntry: false,
         },
-        isEditingDocketEntry: true,
-      },
+      });
+
+      expect(result.showAddDocumentWarning).toBe(false);
     });
 
-    expect(result.showAddDocumentWarning).toBeTruthy();
+    it('should be false when the user is editing a docket entry that was not found in the list of case documents', () => {
+      const result = runCompute(paperDocketEntryHelper, {
+        state: {
+          caseDetail: {
+            correspondence: [],
+            docketEntries: [],
+          },
+          currentViewMetadata: {
+            documentUploadMode: 'scan',
+          },
+          isEditingDocketEntry: false,
+        },
+      });
+
+      expect(result.showAddDocumentWarning).toBeFalsy();
+    });
+
+    it('should be false when the user is editing a docket entry with an attached file but the document upload mode is not preview', () => {
+      const result = runCompute(paperDocketEntryHelper, {
+        state: {
+          caseDetail: {
+            correspondence: [],
+            docketEntries: [
+              {
+                docketEntryId: mockDocketEntryId,
+                isFileAttached: true,
+              },
+            ],
+          },
+          currentViewMetadata: {
+            documentUploadMode: 'scan',
+          },
+          docketEntryId: mockDocketEntryId,
+          isEditingDocketEntry: true,
+        },
+      });
+
+      expect(result.showAddDocumentWarning).toBe(false);
+    });
+
+    it('should be true when the user is editing a docket entry that does not have a file attached and document upload mode is preview', () => {
+      const result = runCompute(paperDocketEntryHelper, {
+        state: {
+          caseDetail: {
+            correspondence: [],
+            docketEntries: [
+              {
+                docketEntryId: mockDocketEntryId,
+                isFileAttached: false,
+              },
+            ],
+          },
+          currentViewMetadata: {
+            documentUploadMode: 'preview',
+          },
+          docketEntryId: mockDocketEntryId,
+          isEditingDocketEntry: true,
+        },
+      });
+
+      expect(result.showAddDocumentWarning).toBe(true);
+    });
   });
 
-  it('should set showAddDocumentWarning to FALSE if docketEntryId is NOT set, documentUploadMode is not preview, and user is NOT editing docket entry', () => {
-    const result = runCompute(paperDocketEntryHelper, {
-      state: {
-        caseDetail: {},
-        currentViewMetadata: {
-          documentUploadMode: 'scan',
+  describe('showSaveAndServeButton', () => {
+    it('should be true when the case can allow service', () => {
+      applicationContext
+        .getUtilities()
+        .canAllowDocumentServiceForCase.mockReturnValue(true);
+
+      const result = runCompute(paperDocketEntryHelper, {
+        state: {
+          caseDetail: {
+            correspondence: [],
+            docketEntries: [],
+          },
         },
-        isEditingDocketEntry: false,
-      },
+      });
+
+      expect(result.showSaveAndServeButton).toBeTruthy();
     });
 
-    expect(result.showAddDocumentWarning).toBeFalsy();
+    it('should be false when the case can NOT allow service', () => {
+      applicationContext
+        .getUtilities()
+        .canAllowDocumentServiceForCase.mockReturnValue(false);
+
+      const result = runCompute(paperDocketEntryHelper, {
+        state: {
+          caseDetail: {
+            correspondence: [],
+            docketEntries: [],
+          },
+        },
+      });
+
+      expect(result.showSaveAndServeButton).toBeFalsy();
+    });
   });
 
-  it('should set showAddDocumentWarning to FALSE if docketEntryId is set in the state, documentUploadMode is preview, and user is editing docket entry with a document attached', () => {
-    const result = runCompute(paperDocketEntryHelper, {
-      state: {
-        caseDetail: {
-          docketEntries: [
-            { docketEntryId: 'document-id-123', isFileAttached: true },
-          ],
+  describe('showServiceWarning', () => {
+    it('should be false when the case can allow service', () => {
+      applicationContext
+        .getUtilities()
+        .canAllowDocumentServiceForCase.mockReturnValue(true);
+
+      const result = runCompute(paperDocketEntryHelper, {
+        state: {
+          caseDetail: {
+            correspondence: [],
+            docketEntries: [],
+          },
         },
-        currentViewMetadata: {
-          documentUploadMode: 'preview',
-        },
-        docketEntryId: 'document-id-123',
-        isEditingDocketEntry: true,
-      },
+      });
+
+      expect(result.showServiceWarning).toBeFalsy();
     });
 
-    expect(result.showAddDocumentWarning).toBeFalsy();
-  });
+    it('should be true when the case can NOT allow service', () => {
+      applicationContext
+        .getUtilities()
+        .canAllowDocumentServiceForCase.mockReturnValue(false);
 
-  it('should set showAddDocumentWarning to TRUE if docketEntryId is set in the state, documentUploadMode is not preview, and user is editing docket entry with NO document attached', () => {
-    const result = runCompute(paperDocketEntryHelper, {
-      state: {
-        caseDetail: {
-          docketEntries: [
-            { docketEntryId: 'document-id-123', isFileAttached: false },
-          ],
+      const result = runCompute(paperDocketEntryHelper, {
+        state: {
+          caseDetail: {
+            correspondence: [],
+            docketEntries: [],
+          },
         },
-        currentViewMetadata: {
-          documentUploadMode: 'scan',
-        },
-        docketEntryId: 'document-id-123',
-        isEditingDocketEntry: true,
-      },
+      });
+
+      expect(result.showServiceWarning).toBeTruthy();
     });
-
-    expect(result.showAddDocumentWarning).toBeTruthy();
-  });
-
-  it('should set showSaveAndServeButton to true and showServiceWarning to false if the case can allow service', () => {
-    const result = runCompute(paperDocketEntryHelper, {
-      state: {
-        caseDetail: {
-          docketEntries: [
-            {
-              docketEntryId: '123',
-              documentType: INITIAL_DOCUMENT_TYPES.petition.documentType,
-              servedAt: '2019-03-01T21:40:46.415Z',
-            },
-            { docketEntryId: 'document-id-123', isFileAttached: false },
-          ],
-          stattus: CASE_STATUS_TYPES.generalDocket,
-        },
-        currentViewMetadata: {
-          documentUploadMode: 'scan',
-        },
-        docketEntryId: 'document-id-123',
-        isEditingDocketEntry: true,
-      },
-    });
-
-    expect(result.showSaveAndServeButton).toBeTruthy();
-    expect(result.showServiceWarning).toBeFalsy();
-  });
-
-  it('should set showSaveAndServeButton to false and showServiceWarning to true if the case can NOT allow service', () => {
-    const result = runCompute(paperDocketEntryHelper, {
-      state: {
-        caseDetail: {
-          docketEntries: [
-            {
-              docketEntryId: '123',
-              documentType: INITIAL_DOCUMENT_TYPES.petition.documentType,
-              servedAt: undefined,
-            },
-            { docketEntryId: 'document-id-123', isFileAttached: false },
-          ],
-          status: CASE_STATUS_TYPES.new,
-        },
-        currentViewMetadata: {
-          documentUploadMode: 'scan',
-        },
-        docketEntryId: 'document-id-123',
-        isEditingDocketEntry: true,
-      },
-    });
-
-    expect(result.showSaveAndServeButton).toBeFalsy();
-    expect(result.showServiceWarning).toBeTruthy();
   });
 });
