7e42b2bc3a72a2dd89ac660e7c947d88bba2af02
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2022-02-10 16:51:00 -0600

    979: Remove conditional around default password

--- shared/admin-tools/user/setup-test-users.sh
+++ shared/admin-tools/user/setup-test-users.sh
@@ -2,9 +2,7 @@
 
 ENV=$1
 
-if [ -z ${DEFAULT_ACCOUNT_PASS} ]; then
-  # Getting the account-wide deployment settings and injecting them into the shell environment
-  . ./scripts/load-environment-from-secrets.sh
-fi
+# Getting the account-wide deployment settings and injecting them into the shell environment
+. ./scripts/load-environment-from-secrets.sh
 
 node shared/admin-tools/user/setup-test-users.js
