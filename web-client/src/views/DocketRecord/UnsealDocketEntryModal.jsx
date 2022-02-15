d82cb478f517a0dd55e6274985426c1cfbbde4b3
TElliott <telliott@flexion.us>
2022-02-15 11:33:34 -0600

    9206: Added unsealdocketentry modal [skip ci]

--- web-client/src/views/DocketRecord/UnsealDocketEntryModal.jsx
+++ web-client/src/views/DocketRecord/UnsealDocketEntryModal.jsx
@@ -1,24 +1,20 @@
 import { ConfirmModal } from '../../ustc-ui/Modal/ConfirmModal';
-import { connect } from '@cerebral/react';
-import { sequences } from 'cerebral';
 import React from 'react';
-
-export const UnsealDocketEntryModal = connect(
-  {
-    cancelSequence: sequences.dismissModalSequence,
-    confirmSequence: sequences.unsealDocketEntrySequence,
-  },
-  function UnsealDocketEntryModal({ cancelSequence, confirmSequence }) {
-    return (
-      <ConfirmModal
-        cancelLabel="Cancel"
-        confirmLabel="Unseal"
-        title="Unseal Document"
-        onCancel={cancelSequence}
-        onConfirm={confirmSequence}
-      >
-        Are you sure you want to unseal this document?
-      </ConfirmModal>
-    );
-  },
-);
+/**
+ * UnsealDocketEntryModal
+ *
+ * @returns {JSX.Element} Returns a modal dialog for unsealing a docket entry.
+ */
+export function UnsealDocketEntryModal() {
+  return (
+    <ConfirmModal
+      cancelLabel="Cancel"
+      confirmLabel="Unseal"
+      title="Unseal Document"
+      onCancelSequence="dismissModalSequence"
+      onConfirmSequence="unsealDocketEntrySequence"
+    >
+      Are you sure you want to unseal this document?
+    </ConfirmModal>
+  );
+}
