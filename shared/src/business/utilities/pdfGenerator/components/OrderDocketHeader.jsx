8e957ec77d0a114483b76a7be951e42b388af17a
Cody Seibert <cseibert113@gmail.com>
2022-02-18 17:06:29 -0500

    9093 - refactoring the order to use split scss files

--- shared/src/business/utilities/pdfGenerator/components/OrderDocketHeader.jsx
+++ shared/src/business/utilities/pdfGenerator/components/OrderDocketHeader.jsx
@@ -4,22 +4,20 @@ export const OrderDocketHeader = ({
   caseCaptionExtension,
   caseTitle,
   docketNumberWithSuffix,
-  h3,
+  orderTitle,
 }) => {
   return (
-    <>
-      <div className="case-information">
-        <div id="caption">
-          <div id="caption-title">{caseTitle}</div>
-          <div id="caption-extension">{caseCaptionExtension}</div>
-          <div id="caption-v">v.</div>
-          <div id="caption-commissioner">COMMISSIONER OF INTERNAL REVENUE</div>
-          <div id="caption-respondent">Respondent</div>
-        </div>
-        <div id="docket-number">Docket No. {docketNumberWithSuffix}</div>
-        <div className="clear"></div>
-        {h3 && <h3>{h3}</h3>}
+    <div className="order-docket-header">
+      <div id="caption">
+        <div id="caption-title">{caseTitle}</div>
+        <div id="caption-extension">{caseCaptionExtension}</div>
+        <div id="caption-v">v.</div>
+        <div id="caption-commissioner">COMMISSIONER OF INTERNAL REVENUE</div>
+        <div id="caption-respondent">Respondent</div>
       </div>
-    </>
+      <div id="docket-number">Docket No. {docketNumberWithSuffix}</div>
+      <div className="clear"></div>
+      {orderTitle && <h3 className="document-title">{orderTitle}</h3>}
+    </div>
   );
 };
