e5b1d02be3c864fd1855dc2f8369501cfb389d04
Cody Seibert <cseibert113@gmail.com>
2022-02-18 17:33:22 -0500

    9093 - working on inventory report

--- shared/src/business/utilities/pdfGenerator/components/PrimaryHeader.test.js
+++ shared/src/business/utilities/pdfGenerator/components/PrimaryHeader.test.js
@@ -6,7 +6,7 @@ describe('PrimaryHeader', () => {
   it('renders an h1 with United States Tax Court', () => {
     const wrapper = shallow(<PrimaryHeader />);
 
-    const h1 = wrapper.find('.court-header h1');
+    const h1 = wrapper.find('#primary-header h1');
     expect(h1.text()).toEqual('United States Tax Court');
   });
 
@@ -19,11 +19,11 @@ describe('PrimaryHeader', () => {
 
   it('only renders an h2 element if h2 prop is provided', () => {
     let wrapper = shallow(<PrimaryHeader />);
-    let h2 = wrapper.find('.court-header h2');
+    let h2 = wrapper.find('#primary-header h2');
     expect(h2).toEqual({});
 
     wrapper = shallow(<PrimaryHeader h2="Test H2" />);
-    h2 = wrapper.find('.court-header h2');
+    h2 = wrapper.find('#primary-header h2');
     expect(h2.text()).toEqual('Test H2');
   });
 });
