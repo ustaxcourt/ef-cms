b8fcd52f2ac7503d1c9f8ece51a6dcd428a54754
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-02-28 11:17:12 -0800

    8507: Delete source dynamo table

---
+++ scripts/delete-dynamo-table.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Deletes the specified dynamo table from the environment
+
+# Usage
+#   ./delete-dynamo-table.sh $SOURCE_TABLE
+
+# Arguments
+#   - $1 - the table to delete
+
+( ! command -v jq > /dev/null ) && echo "jq must be installed on your machine." && exit 1
+[ -z "$1" ] && echo "The table to delete must be provided as the \$1 argument." && exit 1
+
+SOURCE_TABLE=$1
+
+TABLE_STATUS=$(aws es delete-table --table-name "${SOURCE_TABLE}" --region us-east-1 | jq -r ".TableDescription.TableStatus")
+
+if [[ $TABLE_STATUS != "DELETING" ]] ; then
+  echo "The table ${SOURCE_TABLE} failed to delete."
+  exit 1
+fi
+
+echo "The table ${SOURCE_TABLE} was either successfully deleted or didn't exist prior to deletion attempt."
