3b9c4d68986722963a97ea5498e4b2cd06165dbc
TElliott <telliott@flexion.us>
2022-03-03 12:50:10 -0600

    9429: Grammar

--- shared/src/business/utilities/getClinicLetterKey.js
+++ shared/src/business/utilities/getClinicLetterKey.js
@@ -1,5 +1,5 @@
 /**
- * Formats the clinic letter key for used to store the clinic letters for each location and procedureType combination in s3
+ * Formats the clinic letter key used to store the clinic letters for each location and procedureType combination in s3
  *
  * @param {string} procedureType type of procedure for case
  * @param {string} trialLocation location of trial for case
