8f8834ab40e7b861e9f668449c2fb47cafe5a9ef
Cody Seibert <cseibert113@gmail.com>
2022-03-02 22:43:51 -0500

    9429 - found a way to wait for the data

--- web-client/integration-tests/petitionsClerkFilesPetitionWithClinicLetterAttached.test.js
+++ web-client/integration-tests/petitionsClerkFilesPetitionWithClinicLetterAttached.test.js
@@ -1,9 +1,6 @@
+import { docketClerkVerifiesPetitionReceiptLength } from './journey/docketClerkVerifiesPetitionReceiptLength';
 import { fakeFile, loginAs, setupTest } from './helpers';
 import { petitionsClerkCreatesNewCaseFromPaper } from './journey/petitionsClerkCreatesNewCaseFromPaper';
-
-// import { PDFDocument } from 'pdf-lib';
-
-const http = require('http');
 
 const cerebralTest = setupTest();
 
@@ -23,39 +20,13 @@ describe('Petitions Clerk creates a paper case which should have a clinic letter
     'Los Angeles, California',
     'Regular',
   );
-  // petitionsClerkSubmitsPaperCaseToIrs(cerebralTest);
+  docketClerkVerifiesPetitionReceiptLength(cerebralTest, 2);
 
-  it('should be able to serve the case', async () => {
-    console.log(cerebralTest.docketNumber);
-    expect(cerebralTest.getState('currentPage')).toEqual('ReviewSavedPetition');
-
-    await cerebralTest.runSequence('openConfirmServeToIrsModalSequence');
-
-    await cerebralTest.runSequence('serveCaseToIrsSequence');
-
-    expect(cerebralTest.getState('currentPage')).toEqual(
-      'PrintPaperPetitionReceipt',
-    );
-
-    // check if two pages somehow?
-
-    const pdfPreviewUrl = cerebralTest.getState('pdfPreviewUrl');
-
-    const fs = require('fs');
-    const file = fs.createWriteStream('file.pdf');
-    http.get(pdfPreviewUrl, function (response) {
-      response.pipe(file);
-    });
-    const opened = fs.readFile(file);
-    // const pdfDoc = await PDFDocument.;
-    console.log('pdf: ', opened);
-
-    // pdfDoc.getPages().length;
-
-    // console.log(numberOfPages);
-
-    // expect(numberOfPages).toEqual(2);
-
-    // await cerebralTest.runSequence('completePrintPaperPetitionReceiptSequence');
-  });
+  petitionsClerkCreatesNewCaseFromPaper(
+    cerebralTest,
+    fakeFile,
+    'Los Angeles, California',
+    'Small',
+  );
+  docketClerkVerifiesPetitionReceiptLength(cerebralTest, 1);
 });
