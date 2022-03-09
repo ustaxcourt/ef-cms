7b863c680df55f0d5885e6809295b3a91f4b50f8
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2022-03-09 11:18:32 -0600

    Fix reindex job

--- setup-for-reindex.sh
+++ setup-for-reindex.sh
@@ -2,6 +2,7 @@
 
 aws es describe-elasticsearch-domain --domain-name "efcms-search-${ENV}-${NEXT_VERSION}" --region us-east-1
 CODE=$?
+echo "ES describe exit code was ${CODE}."
 if [[ "${CODE}" == "1" ]]; then
   export REINDEX_FLAG=true
 fi
