20b7bd6ee0146b17daf789257ae5dc2b0f8c3588
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2022-03-09 12:40:06 -0600

    1046: Correct conditional to determine need for reindex

--- setup-for-reindex.sh
+++ setup-for-reindex.sh
@@ -2,7 +2,7 @@
 
 aws es describe-elasticsearch-domain --domain-name "efcms-search-${ENV}-${NEXT_VERSION}" --region us-east-1
 CODE=$?
-echo "ES describe exit code was ${CODE}."
-if [[ "${CODE}" == "1" ]]; then
+if [[ "${CODE}" != "0" ]]; then
+  echo "ES describe exit code was ${CODE}."
   export REINDEX_FLAG=true
 fi
