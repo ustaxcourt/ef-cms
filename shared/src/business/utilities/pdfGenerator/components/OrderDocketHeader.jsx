f03e3ef60603e1d02c8bf0502f9b5a60fcaf0494
Cody Seibert <cseibert113@gmail.com>
2022-03-09 15:13:05 -0500

    9093 - fixing issues related to default font family and some broken tests

--- shared/src/business/utilities/pdfGenerator/components/OrderDocketHeader.jsx
+++ shared/src/business/utilities/pdfGenerator/components/OrderDocketHeader.jsx
@@ -9,10 +9,14 @@ export const OrderDocketHeader = ({
   return (
     <div className="order-docket-header">
       <div id="caption">
-        <div id="caption-title">{caseTitle}</div>
+        <div id="caption-title">{caseTitle},</div>
         <div id="caption-extension">{caseCaptionExtension}</div>
         <div id="caption-v">v.</div>
-        <div id="caption-commissioner">COMMISSIONER OF INTERNAL REVENUE</div>
+        <div id="caption-commissioner">
+          COMMISSIONER OF INTERNAL
+          <br />
+          REVENUE,
+        </div>
         <div id="caption-respondent">Respondent</div>
       </div>
       <div id="docket-number">Docket No. {docketNumberWithSuffix}</div>
