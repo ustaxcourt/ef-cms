1515cced152215aaa0c6b297aefea97d50430693
Cody Seibert <cseibert113@gmail.com>
2022-03-10 19:43:51 -0500

    fixing hashes to match version in docker

--- image-compare-pdfs.js
+++ image-compare-pdfs.js
@@ -5,59 +5,60 @@ const { fromPath } = require('pdf2pic');
 const outputPath = './shared/test-output/document-generation';
 
 const pdfs = [
-  'Order.pdf',
   'Address_Label_Cover_Sheet.pdf',
-  'Notice_Trial_Issued.pdf',
   'Case_Inventory_Report.pdf',
-  'Notice_Trial_Issued_In_Person.pdf',
   'Change_Of_Address.pdf',
-  'Order.pdf',
   'CourtIssuedDocumentCoverSheet.pdf',
-  'Pending_Report.pdf',
   'CoverSheet.pdf',
-  'Practitioner_Case_List.pdf',
   'Docket_Record.pdf',
-  'Receipt_of_Filing.pdf',
-  'Notice.pdf',
-  'Standing_Pretrial_Order.pdf',
   'Notice_Of_Change_To_Remote_Proceeding.pdf',
-  'Standing_Pretrial_Order_For_Small_Case.pdf',
   'Notice_Of_Docket_Change.pdf',
-  'Trial_Calendar.pdf',
   'Notice_Receipt_Petition.pdf',
+  'Notice_Trial_Issued_In_Person.pdf',
+  'Notice_Trial_Issued.pdf',
+  'Notice.pdf',
+  'Order.pdf',
+  'Order.pdf',
+  'Pending_Report.pdf',
+  'Practitioner_Case_List.pdf',
+  'Receipt_of_Filing.pdf',
+  'Standing_Pretrial_Order_For_Small_Case.pdf',
+  'Standing_Pretrial_Order.pdf',
+  'Trial_Calendar.pdf',
   'Trial_Session_Planning_Report.pdf',
 ];
 
 const expectedHashes = {
-  'Address_Label_Cover_Sheet.pdf': '35fc53bfc2a9c90052eccd33797d925cc71e50ed',
-  'Case_Inventory_Report.pdf': '789c87f5a5428c150da17eb01653deaf889fa5c0',
-  'Change_Of_Address.pdf': '435f1e3b11f060d20722d2e9365c5f179c4325a6',
+  'Address_Label_Cover_Sheet.pdf': '59f4ae28dec68bc75136d19315b687ab7cfae228',
+  'Case_Inventory_Report.pdf': 'b4b155229c5751c266d60d94cf04e740e00da986',
+  'Change_Of_Address.pdf': '117a898932eae3af53ad9bd932823130807feaae',
   'CourtIssuedDocumentCoverSheet.pdf':
-    'b2be9c9011352e2bbd7d390ebc2ff88767d47277',
-  'CoverSheet.pdf': 'e0adc7143327c73b608cb35ae25af5112e327e15',
-  'Docket_Record.pdf': '8505dc02879fce375b6657e3cef3070d476b97ae',
-  'Notice.pdf': 'de71c72ef8cca9981f29cb969e612f22eb924184',
+    '6a53ede98183214111ba86bf9b595f321720ffc7',
+  'CoverSheet.pdf': '91801bab7ed525ca640a9c5e7600f5993d668c47',
+  'Docket_Record.pdf': '6365d2d2810ff76f7ff25ee6ff919a2c85a85435',
+  'Notice.pdf': 'b1761cb9c86bab61bbf457749f54dd9e481dae47',
   'Notice_Of_Change_To_Remote_Proceeding.pdf':
-    'b6b4738e0e19351d003a1c0e92f45a90997fedff',
-  'Notice_Of_Docket_Change.pdf': '9a54434eb605bdf6e6935b8320fcc3a9b3862bdc',
-  'Notice_Receipt_Petition.pdf': 'cd81cd6af027447e580f7d81ee569db011a6b7c2',
-  'Notice_Trial_Issued.pdf': '1a477683dea2b2d5461aff44ad8a07e07e446822',
+    '02a0d229b17afdf4e1e98c2b576e6f6e6bef796d',
+  'Notice_Of_Docket_Change.pdf': '8af2da16f6dcd2b9e39d0710ec064c54ae4b5359',
+  'Notice_Receipt_Petition.pdf': '7b5d0e1ba2a5f513af376a2999faf9871640f91f',
+  'Notice_Trial_Issued.pdf': 'f4deddb08732942c684b5424098240f27cbae434',
   'Notice_Trial_Issued_In_Person.pdf':
-    'db37afed156aa655f8b8647908b1ba6fdd42ed6e',
-  'Order.pdf': '565ec3283b3c38c714f3bd71f0b99633898c95be',
-  'Pending_Report.pdf': '6edf843b421e221509e8d1e2555a0f9516f49bdd',
-  'Practitioner_Case_List.pdf': 'dc0467afc7152f3867f6b9dffef980631bbe6e8e',
-  'Receipt_of_Filing.pdf': '64f2bb74df5db3768c8c1c7e304c84bd8f902a48',
-  'Standing_Pretrial_Order.pdf': '4e81f3db5b63dada86385fcdf09588f79f581c28',
+    '243579b1c9dee240a9480db720490e15dc9db883',
+  'Order.pdf': 'd500d2bb9b355f2f5cb768a9b9cbd7e5f7e4c8ed',
+  'Pending_Report.pdf': '4a0d3a13c158bb2d0f6e38ed45116e75b2826f63',
+  'Practitioner_Case_List.pdf': 'ec0b5d4c2211b772c7fc7900b25835ad9cd4c167',
+  'Receipt_of_Filing.pdf': 'a1d7fc4b9b000100572b9135a330fbff18320f0c',
+  'Standing_Pretrial_Order.pdf': 'd0dbbf8d8e7e3f81136af4a9305aa6ef6821efd8',
   'Standing_Pretrial_Order_For_Small_Case.pdf':
-    '4e81f3db5b63dada86385fcdf09588f79f581c28',
-  'Trial_Calendar.pdf': 'cd494a12ba7e0e261cd102739bc6a99501fc10e6',
+    'd0dbbf8d8e7e3f81136af4a9305aa6ef6821efd8',
+  'Trial_Calendar.pdf': '0722baf9f371d50772e8e74b3ff83af9b10958fd',
   'Trial_Session_Planning_Report.pdf':
-    '1586856d84a4bac4a851678e617ef0e7794f002e',
+    '7aee73c753cb80c3fdd4d9307c871d89be6fbe5d',
 };
 
 (async () => {
   try {
+    let fail = false;
     for (let pdf of pdfs) {
       const storeOutputImage = fromPath(path.join(outputPath, pdf), {
         density: 100,
@@ -75,11 +76,16 @@ const expectedHashes = {
         );
       });
 
-      console.log(`output hash for ${pdf}: `, hash);
-
       if (hash !== expectedHashes[pdf]) {
-        throw new Error('Hash mismatch');
+        fail = true;
+        console.log(
+          `unexpected hash for ${pdf}: received: ${hash} expected ${expectedHashes[pdf]}`,
+        );
       }
+    }
+
+    if (fail) {
+      throw new Error('Hash mismatch');
     }
   } catch (err) {
     console.error(err);
