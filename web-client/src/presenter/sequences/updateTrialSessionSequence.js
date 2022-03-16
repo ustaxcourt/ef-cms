946c983a8f266642d9bb734a7d00b95e305dff1f
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2022-03-16 11:01:55 -0500

    9326: Set waiting for response when updating trial session

--- web-client/src/presenter/sequences/updateTrialSessionSequence.js
+++ web-client/src/presenter/sequences/updateTrialSessionSequence.js
@@ -10,12 +10,14 @@ import { setPdfPreviewUrlAction } from '../actions/CourtIssuedOrder/setPdfPrevie
 import { setSaveAlertsForNavigationAction } from '../actions/setSaveAlertsForNavigationAction';
 import { setValidationAlertErrorsAction } from '../actions/setValidationAlertErrorsAction';
 import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
+import { setWaitingForResponseAction } from '../actions/setWaitingForResponseAction';
 import { showProgressSequenceDecorator } from '../utilities/showProgressSequenceDecorator';
 import { startShowValidationAction } from '../actions/startShowValidationAction';
 import { updateTrialSessionAction } from '../actions/TrialSession/updateTrialSessionAction';
 import { validateTrialSessionAction } from '../actions/TrialSession/validateTrialSessionAction';
 
 export const updateTrialSessionSequence = [
+  setWaitingForResponseAction,
   clearAlertsAction,
   startShowValidationAction,
   getComputedFormDateFactoryAction(null),
