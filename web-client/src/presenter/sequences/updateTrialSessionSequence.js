3a9599a6c2646996ed53120b8c3bf9347882c25c
Cody Seibert <cseibert113@gmail.com>
2022-03-18 16:03:15 -0400

    9326 - adding waiting text to the spinner

--- web-client/src/presenter/sequences/updateTrialSessionSequence.js
+++ web-client/src/presenter/sequences/updateTrialSessionSequence.js
@@ -6,13 +6,13 @@ import { setAlertErrorAction } from '../actions/setAlertErrorAction';
 import { setValidationAlertErrorsAction } from '../actions/setValidationAlertErrorsAction';
 import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
 import { setWaitingForResponseAction } from '../actions/setWaitingForResponseAction';
+import { setWaitingTextAction } from '../actions/setWaitingTextAction';
 import { startShowValidationAction } from '../actions/startShowValidationAction';
 import { updateTrialSessionAction } from '../actions/TrialSession/updateTrialSessionAction';
 import { validateTrialSessionAction } from '../actions/TrialSession/validateTrialSessionAction';
 
 export const updateTrialSessionSequence = [
   clearPdfPreviewUrlAction,
-  setWaitingForResponseAction,
   clearAlertsAction,
   startShowValidationAction,
   getComputedFormDateFactoryAction(null),
@@ -25,6 +25,10 @@ export const updateTrialSessionSequence = [
       setValidationAlertErrorsAction,
     ],
     success: [
+      setWaitingForResponseAction,
+      setWaitingTextAction(
+        'Please stay on this page while we process your request.',
+      ),
       updateTrialSessionAction,
       {
         error: [setAlertErrorAction],
