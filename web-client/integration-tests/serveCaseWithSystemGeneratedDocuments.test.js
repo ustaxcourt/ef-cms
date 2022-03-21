aa176b0d0b76c3ca348bdc0b99ea189f2f764f50
Rosie Highsmith <ahighsmith@flexion.us>
2022-03-21 14:19:56 -0700

    9001: change payment status on creating paper case integration test to UNPAID

--- web-client/integration-tests/serveCaseWithSystemGeneratedDocuments.test.js
+++ web-client/integration-tests/serveCaseWithSystemGeneratedDocuments.test.js
@@ -5,3 +5,30 @@
 // serve the case
 // check that OFF is on the docket record
 // verify that filing fee due date is set to Today+60 (no holidays or weekends)
+
+import { fakeFile, loginAs, setupTest } from './helpers';
+import { petitionsClerk1ServesPetitionFromMessageDetail } from './journey/petitionsClerk1ServesPetitionFromMessageDetail';
+import { petitionsClerk1ViewsMessageDetail } from './journey/petitionsClerk1ViewsMessageDetail';
+import { petitionsClerk1ViewsMessageInbox } from './journey/petitionsClerk1ViewsMessageInbox';
+import { petitionsClerkCreatesNewCaseFromPaper } from './journey/petitionsClerkCreatesNewCaseFromPaper';
+
+const cerebralTest = setupTest();
+cerebralTest.draftOrders = [];
+
+describe('Petitions Clerk Serves Paper Petition With System Generated Documents', () => {
+  beforeAll(() => {
+    jest.setTimeout(40000);
+  });
+
+  afterAll(() => {
+    cerebralTest.closeSocket();
+  });
+
+  loginAs(cerebralTest, 'petitionsclerk@example.com');
+  petitionsClerkCreatesNewCaseFromPaper(cerebralTest, fakeFile);
+  // expect
+  //   loginAs(cerebralTest, 'petitionsclerk1@example.com');
+  //   petitionsClerk1ViewsMessageInbox(cerebralTest);
+  //   petitionsClerk1ViewsMessageDetail(cerebralTest);
+  //   petitionsClerk1ServesPetitionFromMessageDetail(cerebralTest);
+});
