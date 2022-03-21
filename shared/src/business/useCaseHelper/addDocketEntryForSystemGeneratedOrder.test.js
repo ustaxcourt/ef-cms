ef12f6b5971ffd7f2cc70663fd3028593cd1fb11
rosie highsmith <ahighsmith@flexion.us>
2022-03-21 12:00:04 -0700

    9001: WIP Moved tests from interactor into useCaseHelper [skip ci]

---
+++ shared/src/business/useCaseHelper/addDocketEntryForSystemGeneratedOrder.test.js
@@ -0,0 +1,70 @@
+const {
+  addDocketEntryForSystemGeneratedOrder,
+} = require('./addDocketEntryForSystemGeneratedOrder');
+const {
+  SYSTEM_GENERATED_DOCUMENT_TYPES,
+} = require('../entities/EntityConstants');
+const { applicationContext } = require('../test/createTestApplicationContext');
+const { Case } = require('../entities/cases/Case');
+const { MOCK_CASE } = require('../../test/mockCase');
+
+describe('addDocketEntryForSystemGeneratedOrder', () => {
+  let user;
+  const caseEntity = new Case(MOCK_CASE, { applicationContext });
+
+  const { noticeOfAttachmentsInNatureOfEvidence, orderForFilingFee } =
+    SYSTEM_GENERATED_DOCUMENT_TYPES;
+
+  beforeEach(() => {
+    user = applicationContext.getCurrentUser();
+  });
+
+  it('should increase the docket entries and upload a generated pdf for noticeOfAttachments', async () => {
+    const newDocketEntriesFromNewCaseCount =
+      caseEntity.docketEntries.length + 1;
+
+    await addDocketEntryForSystemGeneratedOrder({
+      applicationContext,
+      caseEntity,
+      systemGeneratedDocument: noticeOfAttachmentsInNatureOfEvidence,
+      user,
+    });
+
+    expect(caseEntity.docketEntries.length).toEqual(
+      newDocketEntriesFromNewCaseCount,
+    );
+
+    expect(applicationContext.getDocumentGenerators().order).toHaveBeenCalled();
+    const passedInNoticeTitle =
+      applicationContext.getDocumentGenerators().order.mock.calls[0][0].data
+        .orderTitle;
+    expect(passedInNoticeTitle).toEqual(passedInNoticeTitle.toUpperCase()); //asserts that the passed in title was uppercase
+
+    expect(applicationContext.getUtilities().uploadToS3).toHaveBeenCalled();
+    expect(
+      applicationContext.getPersistenceGateway().saveDocumentFromLambda,
+    ).toHaveBeenCalled();
+  });
+
+  it('should increase the docket entries and upload a generated pdf for orderForFilingFee', async () => {
+    const newDocketEntriesFromNewCaseCount =
+      caseEntity.docketEntries.length + 1;
+
+    await addDocketEntryForSystemGeneratedOrder({
+      applicationContext,
+      caseEntity,
+      systemGeneratedDocument: orderForFilingFee,
+      user,
+    });
+
+    expect(caseEntity.docketEntries.length).toEqual(
+      newDocketEntriesFromNewCaseCount,
+    );
+
+    expect(applicationContext.getDocumentGenerators().order).toHaveBeenCalled();
+
+    expect(
+      applicationContext.getPersistenceGateway().saveDocumentFromLambda,
+    ).toHaveBeenCalled();
+  });
+});
