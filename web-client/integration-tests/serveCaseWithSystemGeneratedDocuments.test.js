1fece87cdb4d5ee1be8f4b0b670b846ac23417c7
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-03-21 14:59:48 -0700

    9001: Create fake test data [skip ci]

--- web-client/integration-tests/serveCaseWithSystemGeneratedDocuments.test.js
+++ web-client/integration-tests/serveCaseWithSystemGeneratedDocuments.test.js
@@ -6,11 +6,17 @@
 // check that OFF is on the docket record
 // verify that filing fee due date is set to Today+60 (no holidays or weekends)
 
+import {
+  CASE_TYPES_MAP,
+  COUNTRY_TYPES,
+  PARTY_TYPES,
+  PAYMENT_STATUS,
+} from '../../shared/src/business/entities/EntityConstants';
 import { fakeFile, loginAs, setupTest } from './helpers';
 // import { petitionsClerk1ServesPetitionFromMessageDetail } from './journey/petitionsClerk1ServesPetitionFromMessageDetail';
 // import { petitionsClerk1ViewsMessageDetail } from './journey/petitionsClerk1ViewsMessageDetail';
 // import { petitionsClerk1ViewsMessageInbox } from './journey/petitionsClerk1ViewsMessageInbox';
-import { petitionsClerkCreatesNewCaseFromPaper } from './journey/petitionsClerkCreatesNewCaseFromPaper';
+const { faker } = require('@faker-js/faker');
 
 const cerebralTest = setupTest();
 cerebralTest.draftOrders = [];
@@ -25,7 +31,133 @@ describe('Petitions Clerk Serves Paper Petition With System Generated Documents'
   });
 
   loginAs(cerebralTest, 'petitionsclerk@example.com');
-  petitionsClerkCreatesNewCaseFromPaper(cerebralTest, fakeFile);
+  it('should create a case from paper', async () => {
+    let formValues = [
+      {
+        key: 'receivedAtMonth',
+        value: faker.datatype.number({ max: 11, min: 1 }),
+      },
+      {
+        key: 'receivedAtDay',
+        value: faker.datatype.number({ max: 28, min: 1 }),
+      },
+      {
+        key: 'receivedAtYear',
+        value: faker.datatype.number({ max: 2021, min: 2001 }),
+      },
+      {
+        key: 'mailingDate',
+        value: faker.company.catchPhrase(),
+      },
+      {
+        key: 'petitionFile',
+        value: fakeFile,
+      },
+      {
+        key: 'petitionFileSize',
+        value: 1,
+      },
+      {
+        key: 'odsFile',
+        value: fakeFile,
+      },
+      {
+        key: 'odsFileSize',
+        value: 1,
+      },
+      {
+        key: 'ownershipDisclosureFile',
+        value: fakeFile,
+      },
+      {
+        key: 'ownershipDisclosureFileSize',
+        value: 1,
+      },
+      {
+        key: 'requestForPlaceOfTrialFile',
+        value: fakeFile,
+      },
+      {
+        key: 'requestForPlaceOfTrialFileSize',
+        value: 1,
+      },
+      {
+        key: 'applicationForWaiverOfFilingFeeFile',
+        value: fakeFile,
+      },
+      {
+        key: 'applicationForWaiverOfFilingFeeFileSize',
+        value: 1,
+      },
+      {
+        key: 'preferredTrialCity',
+        value: 'Birmingham, Alabama',
+      },
+      {
+        key: 'procedureType',
+        value: 'Small',
+      },
+      {
+        key: 'caseType',
+        value: CASE_TYPES_MAP.deficiency,
+      },
+      {
+        key: 'partyType',
+        value: PARTY_TYPES.petitionerDeceasedSpouse,
+      },
+      {
+        key: 'contactPrimary.countryType',
+        value: COUNTRY_TYPES.INTERNATIONAL,
+      },
+      {
+        key: 'contactPrimary.country',
+        value: 'Switzerland',
+      },
+      {
+        key: 'contactPrimary.name',
+        value: `${faker.name.firstName()} ${faker.name.lastName()}`,
+      },
+      {
+        key: 'contactPrimary.address1',
+        value: faker.address.streetAddress(),
+      },
+      {
+        key: 'contactPrimary.city',
+        value: faker.address.city(),
+      },
+      {
+        key: 'contactPrimary.postalCode',
+        value: faker.address.zipCode(),
+      },
+      {
+        key: 'contactPrimary.phone',
+        value: faker.phone.phoneNumber(),
+      },
+      {
+        key: 'petitionPaymentStatus',
+        value: PAYMENT_STATUS.UNPAID,
+      },
+    ];
+
+    for (const item of formValues) {
+      if (item.key === 'partyType') {
+        await cerebralTest.runSequence(
+          'updateStartCaseInternalPartyTypeSequence',
+          item,
+        );
+      } else if (item.key === 'petitionPaymentStatus') {
+        await cerebralTest.runSequence(
+          'updatePetitionPaymentFormValueSequence',
+          item,
+        );
+      } else {
+        await cerebralTest.runSequence('updateFormValueSequence', item);
+      }
+    }
+
+    await cerebralTest.runSequence('submitPetitionFromPaperSequence');
+  });
+
   // expect
   //   loginAs(cerebralTest, 'petitionsclerk1@example.com');
   //   petitionsClerk1ViewsMessageInbox(cerebralTest);
