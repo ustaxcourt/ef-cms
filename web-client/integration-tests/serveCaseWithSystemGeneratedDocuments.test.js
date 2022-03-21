eabbdaf8855ec64c8b6d7607c3fbcaf346dbbb59
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-03-21 14:06:51 -0700

    9001: Scaffold integration test [skip ci]

---
+++ web-client/integration-tests/serveCaseWithSystemGeneratedDocuments.test.js
@@ -0,0 +1,7 @@
+// create a paper case as a petitionsClerk
+// set payment status to "unpaid"
+// go to review and serve page
+// verify alert warning banner displays "OFF" stuff
+// serve the case
+// check that OFF is on the docket record
+// verify that filing fee due date is set to Today+60 (no holidays or weekends)
