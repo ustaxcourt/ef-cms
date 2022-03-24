955fe6fa0db9505b725be33008e051ea11b9cb04
halprin <halprin@users.noreply.github.com>
2022-03-24 15:51:15 -0600

    DevEx 1025: Move over order and notice generation over to the new meta header

--- shared/src/business/utilities/documentGenerators/order.js
+++ shared/src/business/utilities/documentGenerators/order.js
@@ -36,21 +36,12 @@ const order = async ({ applicationContext, data }) => {
     },
   });
 
-  const headerHtml = reactTemplateGenerator({
-    componentName: 'PageMetaHeaderDocket',
-    data: {
-      docketNumber: docketNumberWithSuffix,
-    },
-  });
-
   const pdf = await applicationContext
     .getUseCases()
     .generatePdfFromHtmlInteractor(applicationContext, {
       contentHtml: pdfContentHtml,
       displayHeaderFooter: true,
       docketNumber: docketNumberWithSuffix,
-      headerHtml,
-      overwriteHeader: true,
     });
 
   return pdf;
