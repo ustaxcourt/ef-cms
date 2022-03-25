29e06bcafb7ad9ec47b20ac7c714c1702e3d523e
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-03-25 07:16:32 -0700

    Devex 859: Don't need to default trialSession to an empty object for batch downloading feature, refactoring tests

--- web-client/src/presenter/actions/batchDownloadTrialSessionAction.test.js
+++ web-client/src/presenter/actions/batchDownloadTrialSessionAction.test.js
@@ -3,41 +3,57 @@ import { batchDownloadTrialSessionAction } from './batchDownloadTrialSessionActi
 import { presenter } from '../presenter-mock';
 import { runAction } from 'cerebral/test';
 
-presenter.providers.applicationContext = applicationContext;
-
-const { batchDownloadTrialSessionInteractor } =
-  applicationContext.getUseCases();
-const pathSuccessStub = jest.fn();
-const pathErrorStub = jest.fn();
-
-presenter.providers.path = {
-  error: pathErrorStub,
-  success: pathSuccessStub,
-};
-
 describe('batchDownloadTrialSessionAction', () => {
-  it('initializes the batch download for a trial session', async () => {
+  const pathSuccessStub = jest.fn();
+  const pathErrorStub = jest.fn();
+
+  const mockTrialSessionId = '6d3a06cc-17d7-4699-be4d-6bf93c0317e9';
+
+  presenter.providers.applicationContext = applicationContext;
+
+  presenter.providers.path = {
+    error: pathErrorStub,
+    success: pathSuccessStub,
+  };
+
+  it('should make a call to persistence to batch download cases for the specified trial session', async () => {
     await runAction(batchDownloadTrialSessionAction, {
       modules: {
         presenter,
       },
+      state: {
+        trialSession: { trialSessionId: mockTrialSessionId },
+      },
     });
 
-    expect(batchDownloadTrialSessionInteractor).toHaveBeenCalled();
+    expect(
+      applicationContext.getUseCases().batchDownloadTrialSessionInteractor.mock
+        .calls[0][1].trialSessionId,
+    ).toBe(mockTrialSessionId);
     expect(pathSuccessStub).toHaveBeenCalled();
   });
 
-  it('calls the error path if an exception is thrown', async () => {
-    batchDownloadTrialSessionInteractor.mockImplementation(() => {
-      throw new Error('Guy Fieri has connected to the server.');
-    }),
-      await runAction(batchDownloadTrialSessionAction, {
-        modules: {
-          presenter,
-        },
+  it('should call the error path to open a modal when an error occurs when downloading trial session data', async () => {
+    applicationContext
+      .getUseCases()
+      .batchDownloadTrialSessionInteractor.mockImplementation(() => {
+        throw new Error('Guy Fieri has connected to the server.');
       });
 
-    expect(batchDownloadTrialSessionInteractor).toThrow();
-    expect(pathErrorStub).toHaveBeenCalled();
+    await runAction(batchDownloadTrialSessionAction, {
+      modules: {
+        presenter,
+      },
+      state: {
+        trialSession: { trialSessionId: mockTrialSessionId },
+      },
+    });
+
+    expect(
+      applicationContext.getUseCases().batchDownloadTrialSessionInteractor,
+    ).toThrow();
+    expect(pathErrorStub).toHaveBeenCalledWith({
+      showModal: 'FileCompressionErrorModal',
+    });
   });
 });
