99381924649e76dac98a360253f3873b7459c37f
Timothy Shumway <tshumway@flexion.us>
2022-03-30 16:16:24 -0600

    9001 - removed Order For Filing Fee from image compare test

--- image-compare-pdfs.js
+++ image-compare-pdfs.js
@@ -20,7 +20,6 @@ const pdfs = [
   'Notice_Trial_Issued.pdf',
   'Notice.pdf',
   'Order.pdf',
-  'Order_For_Filing_Fee.pdf',
   'Pending_Report.pdf',
   'Practitioner_Case_List.pdf',
   'Receipt_of_Filing.pdf',
