d6c31fe9b5317de85726f8f15be242b3624750c9
Cody Seibert <cseibert113@gmail.com>
2022-04-06 12:15:44 -0400

    devex - removing the sleep

--- web-client/src/presenter/sequences/openAddToTrialModalSequence.js
+++ web-client/src/presenter/sequences/openAddToTrialModalSequence.js
@@ -11,6 +11,7 @@ export const openAddToTrialModalSequence = showProgressSequenceDecorator([
   clearAlertsAction,
   getTrialSessionsAction,
   setTrialSessionsOnModalAction,
+  setTrialSessionsOnModalAction,
   setShowAllLocationsFalseAction,
   setShowModalFactoryAction('AddToTrialModal'),
 ]);
