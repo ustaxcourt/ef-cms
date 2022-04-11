0bc7ab726c0e6d92dd7628ef873911139f084dfe
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2022-04-11 14:34:03 -0600

    exp2: adding bucket name

--- web-api/terraform/template/main-east.tf
+++ web-api/terraform/template/main-east.tf
@@ -12,7 +12,7 @@ data "local_file" "amended_petition_form_pdf" {
 }
 
 resource "aws_s3_bucket_object" "amended-petition-form-bucket-object-east" {
-  bucket     = aws_s3_bucket.documents_us_east_1
+  bucket     = aws_s3_bucket.documents_us_east_1.id
   key        = "amended-petition-form"
   content     = data.local_file.amended_petition_form_pdf.content
 }
