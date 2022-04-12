d2ecbd0c9a1646f8aae281295db6183b285eb50d
Cody Seibert <cseibert113@gmail.com>
2022-04-12 15:42:29 -0400

    9470 - changing logic to support P when generating the served parties code

--- shared/src/business/entities/DocketEntry.js
+++ shared/src/business/entities/DocketEntry.js
@@ -425,12 +425,17 @@ const getServedPartiesCode = servedParties => {
   let servedPartiesCode = '';
   if (servedParties && servedParties.length > 0) {
     if (
+      servedParties.length >= 2 &&
+      servedParties.find(party => party.role === ROLES.irsSuperuser)
+    ) {
+      servedPartiesCode = SERVED_PARTIES_CODES.BOTH;
+    } else if (
       servedParties.length === 1 &&
       servedParties[0].role === ROLES.irsSuperuser
     ) {
       servedPartiesCode = SERVED_PARTIES_CODES.RESPONDENT;
     } else {
-      servedPartiesCode = SERVED_PARTIES_CODES.BOTH;
+      servedPartiesCode = SERVED_PARTIES_CODES.PETITIONER;
     }
   }
   return servedPartiesCode;
