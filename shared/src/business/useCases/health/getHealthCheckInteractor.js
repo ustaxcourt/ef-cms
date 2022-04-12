bbe259b23a6ccf2978a1090e037b20c5de214950
Cody Seibert <cseibert113@gmail.com>
2022-04-12 16:07:20 -0400

    devex - fixing smoketests for dynamsoft changes

--- shared/src/business/useCases/health/getHealthCheckInteractor.js
+++ shared/src/business/useCases/health/getHealthCheckInteractor.js
@@ -58,7 +58,7 @@ const getDynamsoftStatus = async ({ applicationContext }) => {
         `${scannerResourceUri}/dynamsoft.webtwain.initiate.js`,
         `${scannerResourceUri}/dynamsoft.webtwain.config.js`,
         `${scannerResourceUri}/dynamsoft.webtwain.install.js`,
-        `${scannerResourceUri}/dynamsoft.webtwain.css`,
+        `${scannerResourceUri}/src/dynamsoft.webtwain.css`,
       ].map(url => {
         return axios.get(url, { cancelToken: source.token, timeout: 1000 });
       }),
