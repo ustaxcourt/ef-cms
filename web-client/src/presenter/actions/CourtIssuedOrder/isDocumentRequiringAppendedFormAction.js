90b2d1459944345858db22dfdf9104d75c65b9b4
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2022-04-13 11:02:40 -0600

    9341: removing extra doc [skip ci]

--- web-client/src/presenter/actions/CourtIssuedOrder/isDocumentRequiringAppendedFormAction.js
+++ web-client/src/presenter/actions/CourtIssuedOrder/isDocumentRequiringAppendedFormAction.js
@@ -5,12 +5,11 @@ import { state } from 'cerebral';
  * Determines if the document being uploaded requires an appended form
  *
  * @param {object} providers the providers object
- * @param {object} providers.applicationContext the application context
- * @param {object} providers.path the cerebral path to take depending on if the file was uploaded successfully or not
  * @param {object} providers.get the cerebral get method used for getting state
+ * @param {object} providers.path the cerebral path to take depending on if the file was uploaded successfully or not
  * @returns {object} the next path based on if the file was successfully uploaded or not
  */
-export const isDocumentRequiringAppendedFormAction = async ({ get, path }) => {
+export const isDocumentRequiringAppendedFormAction = ({ get, path }) => {
   const { eventCode } = get(state.documentToEdit);
 
   if (
