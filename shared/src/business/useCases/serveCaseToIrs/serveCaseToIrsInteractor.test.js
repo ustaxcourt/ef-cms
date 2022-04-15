b2ece456caedf39a8de48cfb6fe0fb913c377d68
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2022-04-15 12:53:58 -0500

    9342: Add test for OAPF

--- shared/src/business/useCases/serveCaseToIrs/serveCaseToIrsInteractor.test.js
+++ shared/src/business/useCases/serveCaseToIrs/serveCaseToIrsInteractor.test.js
@@ -923,6 +923,55 @@ describe('serveCaseToIrsInteractor', () => {
     });
   });
 
+  it('should replace brackets in orderForAmendedPetitionAndFilingFee content with the served date of the petition, and today plus sixty twice', async () => {
+    mockCase = {
+      ...MOCK_CASE,
+      orderForAmendedPetitionAndFilingFee: true,
+    };
+
+    await serveCaseToIrsInteractor(applicationContext, {
+      docketNumber: MOCK_CASE.docketNumber,
+    });
+
+    expect(
+      await applicationContext.getUseCaseHelpers()
+        .addDocketEntryForSystemGeneratedOrder.mock.calls[0][0]
+        .systemGeneratedDocument,
+    ).toMatchObject({
+      content: expect.not.stringContaining('['),
+    });
+
+    const today = formatNow(FORMATS.MONTH_DAY_YEAR);
+
+    const mockTodayPlus60 = getBusinessDateInFuture({
+      numberOfDays: 60,
+      startDate: formatNow(FORMATS.ISO),
+    });
+
+    expect(
+      await applicationContext.getUseCaseHelpers()
+        .addDocketEntryForSystemGeneratedOrder.mock.calls[0][0]
+        .systemGeneratedDocument,
+    ).toMatchObject({
+      content: `&nbsp;&nbsp;&nbsp;&nbsp;The Court filed on ${today}, a document as the petition of the above-named
+      petitioner(s) at the docket number indicated. That docket number MUST appear on all documents
+      and papers subsequently sent to the Court for filing or otherwise. The document did not comply with
+      the Rules of the Court as to the form and content of a proper petition. The filing fee was not paid.<br/>
+      <br/>
+      &nbsp;&nbsp;&nbsp;&nbsp;Accordingly, it is<br/>
+      <br/>
+      &nbsp;&nbsp;&nbsp;&nbsp;ORDERED that on or before ${mockTodayPlus60}, petitioner(s) shall file a proper
+      amended petition and pay the $60.00 filing fee. Waiver of the filing fee requires an affidavit
+      containing specific financial information regarding the inability to make such payment. An
+      Application for Waiver of Filing Fee and Affidavit form is available under "Case Related Forms" on
+      the Court's website at www.ustaxcourt.gov/case_related_forms.html.<br/>
+      <br/>
+      If, by ${mockTodayPlus60}, petitioner(s) do not file an Amended Petition and either pay the Court's
+      $60.00 filing fee or submit an Application for Waiver of the Filing Fee, the case will be dismissed or
+      other action taken as the Court deems appropriate.`,
+    });
+  });
+
   it('should underline "See" in orderToShowCause content', async () => {
     mockCase = {
       ...MOCK_CASE,
