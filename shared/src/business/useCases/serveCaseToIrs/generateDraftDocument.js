3d5102f9badf2b4268f549f09d31b214264a484b
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2022-04-18 10:11:23 -0500

    9342: Add JSDOC to generateDraftDocument function

--- shared/src/business/useCases/serveCaseToIrs/generateDraftDocument.js
+++ shared/src/business/useCases/serveCaseToIrs/generateDraftDocument.js
@@ -2,6 +2,14 @@ const {
   replaceBracketed,
 } = require('../../../business/utilities/replaceBracketed');
 
+/**
+ * @param {object} providers the providers object containing applicationContext, caseEntity, document, replacements
+ * @param providers.applicationContext api applicationContext
+ * @param providers.caseEntity the case to create a new document for
+ * @param providers.document a document similar to those defined in SYSTEM_GENERATED_DOCUMENT_TYPES in EntityConstants.js (i.e. must have the same properties)
+ * @param providers.replacements an ordered array of strings that replace bracketed placeholders in providers.document.content
+ * @returns {Promise<void>} does not return anything, rather creates a document and associated docket entry
+ */
 const generateDraftDocument = async ({
   applicationContext,
   caseEntity,
