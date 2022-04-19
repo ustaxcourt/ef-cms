d9c58e9ac1e507fc7fa131012ecd982c9e92263f
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2022-04-19 10:00:42 -0500

    9342: Remove console log

--- shared/src/business/useCases/courtIssuedOrder/updateCourtIssuedOrderInteractor.js
+++ shared/src/business/useCases/courtIssuedOrder/updateCourtIssuedOrderInteractor.js
@@ -51,12 +51,9 @@ exports.updateCourtIssuedOrderInteractor = async (
     throw new NotFoundError('Document not found');
   }
 
-  console.log('JOB::right above filter');
   const orderTypeEventCodes = Object.values(ORDER_TYPES)
     .filter(orderType => orderType.overrideFreeText)
     .map(d => d.eventCode);
-
-  console.log('JOB', orderTypeEventCodes);
 
   if (orderTypeEventCodes.includes(documentMetadata.eventCode)) {
     documentMetadata.freeText = documentMetadata.documentTitle;
