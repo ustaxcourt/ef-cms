369a617bdce42cc368e9e77ef6e31dca0ff6af93
Rosie Highsmith <ahighsmith@flexion.us>
2022-04-20 10:08:45 -0700

    9480: getting the judge by name [skip ci]

--- scripts/dynamo/offboard-judge-user.sh
+++ scripts/dynamo/offboard-judge-user.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
-# Sets the value for the request limit of the IP limiter
+# Updates the offboarded judge user in the dynamo table
 
 # Usage
-#   ./set-maintenance-mode.sh Guy
+#   ./offboard-judge-user.sh Guy
 
 # Arguments
 #   - $1 - The name of the judge to offboard
 
+# todo: 
+# find the judge (by name? userId is env specific)
+# update that judge\'s role to 'legacyJudge' and section to 'legacyJudgesChambers'
+
 [ -z "$1" ] && echo "The value to set for the judge name must be provided as the \$1 argument." && exit 1
 JUDGE_NAME=$1
 
-../../check-env-variables.sh \
+./check-env-variables.sh \
   "ENV" \
   "AWS_SECRET_ACCESS_KEY" \
   "AWS_ACCESS_KEY_ID"
 
-aws dynamodb scan \
-    --table-name MusicCollection \
-    --filter-expression "Artist = :a" \
-    --projection-expression "#ST, #AT" \
-    --expression-attribute-names file://expression-attribute-names.json \
-    --expression-attribute-values file://expression-attribute-values.json
-
-JUDGE_DATA=$(aws dynamodb scan --table-name "efcms-${ENV}-alpha"  --filter-expression "role = :a" "section"= :b  --expression-attribute-values {":a": {"S": "judge"}, ":b": {"S": "guysChambers"}}\)
+JUDGE_DATA=$(aws dynamodb scan \
+--table-name "efcms-${ENV}-alpha"  \
+--filter-expression "#judge_name = :a" \
+--expression-attribute-names '{"#judge_name":"name"}' \
+--expression-attribute-values '{":a":{"S":"'${JUDGE_NAME}'"}}' \
+--region us-east-1 \
+)
 echo "${JUDGE_DATA}"
-
 
 # ITEM=$(cat <<-END
 # {
