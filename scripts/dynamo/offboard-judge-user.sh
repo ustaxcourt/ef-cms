8aca2ed6f231fac614d43f29647eec40956cd892
Rosie Highsmith <ahighsmith@flexion.us>
2022-04-20 11:13:22 -0700

    9480: updating script [skip ci]
    9480: trying things [skip ci]
    
    9480: working script to update offboarded judge's role and section based on passed in userId
    
    9480: clean up script
    
    9480: update trial sessions smoketest to verify offboard judge does not exist on creation.

--- scripts/dynamo/offboard-judge-user.sh
+++ scripts/dynamo/offboard-judge-user.sh
@@ -1,53 +1,46 @@
 #!/bin/bash
 
-# Updates the offboarded judge user in the dynamo table
+# Updates the offboarded judge user in the dynamo table based on their userId passed in
 
 # Usage
-#   ./offboard-judge-user.sh Guy
+#   ./offboard-judge-user.sh 68686578-bc34-4aea-bc1d-25e505422843 alpha
 
 # Arguments
-#   - $1 - The name of the judge to offboard
+#   - $1 - The userId of the judge to offboard
+#   - $2 - The sourceTable to update e.g. alpha
 
-# todo: 
-# find the judge (by name? userId is env specific)
-# update that judge\'s role to 'legacyJudge' and section to 'legacyJudgesChambers'
+( ! command -v jq > /dev/null ) && echo "jq must be installed on your machine." && exit 1
+[ -z "$1" ] && echo "The value to set for the judge userId must be provided as the \$1 argument." && exit 1
+[ -z "$2" ] && echo "The value to set for the source table must be provided as the \$2 argument." && exit 1
 
-[ -z "$1" ] && echo "The value to set for the judge name must be provided as the \$1 argument." && exit 1
-JUDGE_NAME=$1
+USER_ID=$1
+SOURCE_TABLE=$2
+REGION=us-east-1
 
 ./check-env-variables.sh \
   "ENV" \
   "AWS_SECRET_ACCESS_KEY" \
   "AWS_ACCESS_KEY_ID"
 
-JUDGE_DATA=$(aws dynamodb scan \
---table-name "efcms-${ENV}-alpha"  \
---filter-expression "#judge_name = :a" \
---expression-attribute-names '{"#judge_name":"name"}' \
---expression-attribute-values '{":a":{"S":"'${JUDGE_NAME}'"}}' \
---region us-east-1 \
+JUDGE_USER=$(aws dynamodb get-item --region ${REGION} --table-name "efcms-${ENV}-${SOURCE_TABLE}" \
+--key '{"pk":{"S":"user|'"${USER_ID}"'"},"sk":{"S":"user|'"${USER_ID}"'"}}' \
+| jq -r ".Item" \
 )
-echo "${JUDGE_DATA}"
+echo "---Retrieved Judge User: ${JUDGE_USER}"
+if [ -z "${JUDGE_USER}" ]; then
+  echo "Could not find judge user with userId: ${USER_ID} on ${SOURCE_TABLE} table."
+  exit 1
+fi
 
-# ITEM=$(cat <<-END
-# {
-#   "maxInvocations": {
-#     "N": "5"
-#   },
-#   "name": {
-#     "S": "Guy"
-#   },
-#   "role": {
-#     "S": "judge"
-#   },
-#   "windowTime": {
-#     "N": "1000"
-#   }
-# }
-# END
-# )
+UPDATE_OUTPUT=$(aws dynamodb update-item \
+    --table-name "efcms-${ENV}-${SOURCE_TABLE}" \
+    --key '{"pk":{"S":"user|'"${USER_ID}"'"},"sk":{"S":"user|'"${USER_ID}"'"}}' \
+    --update-expression 'SET #role = :role, #section = :section' \
+    --expression-attribute-names '{"#role": "role", "#section": "section"}' \
+    --expression-attribute-values '{":role": {"S": "legacyJudge"}, ":section": {"S": "legacyJudgesChambers"}}' \
+    --return-values UPDATED_NEW \
+    --region ${REGION} \
+)
 
-# aws dynamodb put-item \
-#     --region us-east-1 \
-#     --table-name "efcms-deploy-${ENV}" \
-#     --item "${ITEM}"
+echo "---Updated attributes of user ${USER_ID}: ${UPDATE_OUTPUT} on ${SOURCE_TABLE} table."
+
