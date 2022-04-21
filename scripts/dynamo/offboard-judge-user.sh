1019486287e46751f93d460c803e016f1a7671c8
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2022-04-21 09:42:09 -0600

    9480: trying things [skip ci]

--- scripts/dynamo/offboard-judge-user.sh
+++ scripts/dynamo/offboard-judge-user.sh
@@ -15,34 +15,49 @@
 # do we need to specify source/destination table?
 
 ( ! command -v jq > /dev/null ) && echo "jq must be installed on your machine." && exit 1
-[ -z "$1" ] && echo "The value to set for the judge name must be provided as the \$1 argument." && exit 1
-JUDGE_NAME=$1
+# [ -z "$1" ] && echo "The value to set for the judge name must be provided as the \$1 argument." && exit 1
+# USER_ID=$1
 REGION=us-east-1
 
-./check-env-variables.sh \
+../../check-env-variables.sh \
   "ENV" \
   "AWS_SECRET_ACCESS_KEY" \
   "AWS_ACCESS_KEY_ID"
 
-JUDGE_USER_ID=$(aws dynamodb scan \
-  --table-name "efcms-${ENV}-alpha"  \
-  --filter-expression "#judge_name = :a AND #user_role = :b" \
-  --expression-attribute-names '{"#judge_name":"name","#user_role":"role"}' \
-  --expression-attribute-values '{":a":{"S":"'${JUDGE_NAME}'"},":b":{"S":"judge"}}' \
-  --region ${REGION} \
-  | jq -r ".Items[0].userId.S" \
-)
-echo "Judge user ID found for ${JUDGE_NAME} is: ${JUDGE_USER_ID}"
+# JUDGE_USER_ID=$(aws dynamodb scan \
+#   --table-name "efcms-${ENV}-alpha"  \
+#   --filter-expression "#judge_name = :a AND #user_role = :b" \
+#   --expression-attribute-names '{"#judge_name":"name","#user_role":"role"}' \
+#   --expression-attribute-values '{":a":{"S":"'${JUDGE_NAME}'"},":b":{"S":"judge"}}' \
+#   --region ${REGION} \
+#   | jq -r ".Items[0].userId.S" \
+# )
+# echo "Judge user ID found for ${JUDGE_NAME} is: ${JUDGE_USER_ID}"
 
 # for checking that we have the right object
-# JUDGE_USER=$(aws dynamodb get-item --region ${REGION} --table-name "efcms-${ENV}-alpha" \
-# --key '{"pk":{"S":"user|'${JUDGE_USER_ID}'"},"sk":{"S":"user|'${JUDGE_USER_ID}'"}' \
-# | jq -r ".Item.current.S"
-# )
+JUDGE_USER=$(aws dynamodb get-item --region ${REGION} --table-name "efcms-${ENV}-alpha" \
+--key '{"pk":{"S":"user|9bab3bd3-08a9-4833-9b7c-276f07d6098e"},"sk":{"S":"user|9bab3bd3-08a9-4833-9b7c-276f07d6098e"}}' \
+| jq -r ".Item"
+)
+# 9bab3bd3-08a9-4833-9b7c-276f07d6098e
+echo "${JUDGE_USER}"
 
-# echo "${JUDGE_USER}"
+OUTPUTTT=$(aws dynamodb update-item \
+    --table-name "efcms-${ENV}-alpha" \
+    --key '{"pk": {"S": "9bab3bd3-08a9-4833-9b7c-276f07d6098e"},"sk": {"S": "9bab3bd3-08a9-4833-9b7c-276f07d6098e"}}' \
+   --attribute-updates  '{"role": {"Value": {"S": "judge"},"Action": "PUT"}, "section": {"Value": {"S": "guysChambers"},"Action": "PUT"}}' \
+    --region ${REGION} \
+)
 
+echo "${OUTPUTTT}"
 # put-item fully overwrites the object, not just the attribute, should use update-item
 # OUTPUT=$(aws dynamodb put-item --region ${REGION} --table-name "efcms-${ENV}-alpha" \
 # --item '{"pk":{"S":"user|'${JUDGE_USER_ID}'"},"sk":{"S":"user|'${JUDGE_USER_ID}'"},"role":{"S":"legacyJudge"}}')
 # echo "${OUTPUT}"
+JUDGE_USER=$(aws dynamodb get-item --region ${REGION} --table-name "efcms-${ENV}-alpha" \
+--key '{"pk":{"S":"user|9bab3bd3-08a9-4833-9b7c-276f07d6098e"},"sk":{"S":"user|9bab3bd3-08a9-4833-9b7c-276f07d6098e"}}' \
+| jq -r ".Item"
+)
+# c3b4d14e-f764-4355-b1f9-0af887ef0210 -- guy test
+# 9bab3bd3-08a9-4833-9b7c-276f07d6098e -- guy on exp2
+echo "${JUDGE_USER}"
