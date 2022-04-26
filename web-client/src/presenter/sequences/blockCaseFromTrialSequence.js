4f37463c7b0cbf506fdb65fe4be508f1bf90cd46
Absolutestunna <joshujah007@gmail.com>
2022-04-26 11:14:46 -0400

    BUG: 9486 - added actions to retrieve and set the list of consolidated cases after a manual block operation

--- web-client/src/presenter/sequences/blockCaseFromTrialSequence.js
+++ web-client/src/presenter/sequences/blockCaseFromTrialSequence.js
@@ -1,8 +1,10 @@
 import { blockCaseFromTrialAction } from '../actions/CaseDetail/blockCaseFromTrialAction';
 import { clearModalAction } from '../actions/clearModalAction';
 import { clearModalStateAction } from '../actions/clearModalStateAction';
+import { getConsolidatedCasesByCaseAction } from '../actions/CaseConsolidation/getConsolidatedCasesByCaseAction';
 import { setAlertSuccessAction } from '../actions/setAlertSuccessAction';
 import { setCaseAction } from '../actions/setCaseAction';
+import { setConsolidatedCasesForCaseAction } from '../actions/CaseConsolidation/setConsolidatedCasesForCaseAction';
 import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
 import { showProgressSequenceDecorator } from '../utilities/showProgressSequenceDecorator';
 import { startShowValidationAction } from '../actions/startShowValidationAction';
@@ -19,6 +21,8 @@ export const blockCaseFromTrialSequence = [
       clearModalAction,
       clearModalStateAction,
       setCaseAction,
+      getConsolidatedCasesByCaseAction,
+      setConsolidatedCasesForCaseAction,
     ]),
   },
 ];
