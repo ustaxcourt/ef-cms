f9ae2f4114e5d0842469dd41bf02dccb20ef41f1
halprin <halprin@users.noreply.github.com>
2022-04-27 13:22:38 -0600

    9342: Update JSDoc on generateDraftDocument and another

--- shared/src/business/useCases/serveCaseToIrs/generateDraftDocument.js
+++ shared/src/business/useCases/serveCaseToIrs/generateDraftDocument.js
@@ -4,10 +4,10 @@ const {
 
 /**
  * @param {object} providers the providers object containing applicationContext, caseEntity, document, replacements
- * @param providers.applicationContext api applicationContext
- * @param providers.caseEntity the case to create a new document for
- * @param providers.document a document similar to those defined in SYSTEM_GENERATED_DOCUMENT_TYPES in EntityConstants.js (i.e. must have the same properties)
- * @param providers.replacements an ordered array of strings that replace bracketed placeholders in providers.document.content
+ * @param {object} providers.applicationContext api applicationContext
+ * @param {Case} providers.caseEntity the case to create a new document for
+ * @param {object} providers.document a document similar to those defined in SYSTEM_GENERATED_DOCUMENT_TYPES in EntityConstants.js (i.e. must have the same properties)
+ * @param {string[]} providers.replacements an ordered array of strings that replace bracketed placeholders in providers.document.content
  * @returns {Promise<void>} does not return anything, rather creates a document and associated docket entry
  */
 const generateDraftDocument = async ({
