969e3e6a5afdc5c1d2c0d8bcf2bb068242f91272
Rosie Highsmith <ahighsmith@flexion.us>
2022-05-02 10:42:10 -0700

    9485: make integration test more generic to check as petitioner.

--- web-client/src/views/CaseDetail/CaseDetailHeader.jsx
+++ web-client/src/views/CaseDetail/CaseDetailHeader.jsx
@@ -153,20 +153,23 @@ export const CaseDetailHeader = connect(
                       {formattedCaseDetail.docketNumberWithSuffix}
                     </CaseLink>
                   </h1>
+                  {formattedCaseDetail.isLeadCase && (
+                    <span
+                      aria-label={`isLeadCase: ${formattedCaseDetail.isLeadCase}`}
+                      className="usa-tag"
+                      id="lead-case-tag"
+                    >
+                      <span aria-hidden="true">Lead case</span>
+                    </span>
+                  )}
                   {caseDetailHeaderHelper.hidePublicCaseInformation && (
                     <>
-                      {formattedCaseDetail.isLeadCase && (
-                        <span
-                          aria-label={`isLeadCase: ${formattedCaseDetail.isLeadCase}`}
-                          className="usa-tag"
-                          id="lead-case-tag"
-                        >
-                          <span aria-hidden="true">Lead case</span>
-                        </span>
-                      )}
                       <span
                         aria-label={`status: ${formattedCaseDetail.status}`}
-                        className="usa-tag"
+                        className={classNames(
+                          'usa-tag',
+                          formattedCaseDetail.isLeadCase ? 'margin-left-1' : '',
+                        )}
                       >
                         <span aria-hidden="true">
                           {formattedCaseDetail.status}
