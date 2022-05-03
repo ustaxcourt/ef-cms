0122a164d4baede13cd7de6230604a69f3652035
Rosie Highsmith <ahighsmith@flexion.us>
2022-05-03 11:36:45 -0700

    9492: cypress test for complete & send message

--- cypress-smoketests/integration/messages.spec.js
+++ cypress-smoketests/integration/messages.spec.js
@@ -1,6 +1,59 @@
-// login as docketclerk
-// go to section document qc
-// click document for 103-20
+const {
+  fillOutMessageField,
+  goToDocumentNeedingQC,
+  goToMyDocumentQC,
+  goToSectionDocumentQC,
+  openCompleteAndSendMessageDialog,
+  progressIndicatorDoesNotExist,
+  selectRecipient,
+  selectSection,
+  sendMessage,
+} = require('../support/pages/document-qc');
+const {
+  getEnvironmentSpecificFunctions,
+} = require('../support/pages/environment-specific-factory');
+const { getUserToken, login } = getEnvironmentSpecificFunctions();
+
+const DEFAULT_ACCOUNT_PASS = Cypress.env('DEFAULT_ACCOUNT_PASS');
+let token = null;
+
+describe('Docket Clerk', () => {
+  before(async () => {
+    const results = await getUserToken(
+      'docketclerk1@example.com',
+      DEFAULT_ACCOUNT_PASS,
+    );
+    token = results.AuthenticationResult.IdToken;
+  });
+
+  it('should be able to login', () => {
+    login(token);
+  });
+
+  it('should go to section document QC inbox', () => {
+    goToMyDocumentQC();
+    goToSectionDocumentQC();
+  });
+
+  it('should go to first document needing QC', () => {
+    goToDocumentNeedingQC();
+  });
+
+  it('should open dialog to complete & send message', () => {
+    openCompleteAndSendMessageDialog();
+  });
+
+  it('should fill out dialog to complete & send message', () => {
+    selectSection('ADC');
+    selectRecipient('Test ADC');
+    fillOutMessageField();
+  });
+
+  it('should send the message', () => {
+    sendMessage();
+    progressIndicatorDoesNotExist();
+  });
+});
 // complete and send message
 // fill out form
 // send message
