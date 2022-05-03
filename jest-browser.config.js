c0b6d6a0a6c8c839395b059526e6b30eb44e448e
halprin <halprin@users.noreply.github.com>
2022-05-03 17:06:33 -0600

    Don't mess up the import path for the base level jest.config file

--- jest-browser.config.js
+++ jest-browser.config.js
@@ -1,7 +1,7 @@
 // a global config if running tests singly on CLI
 // this file also contains a set of configuration defaults which can
 // be imported / overridden as a base configuration elsewhere.
-const baseConfig = require('jest.config');
+const baseConfig = require('./jest.config');
 
 module.exports = {
   ...baseConfig,
