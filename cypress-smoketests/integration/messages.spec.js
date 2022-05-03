f2cd35059d23a771f557d2fe7a95a73d6107b878
Rosie Highsmith <ahighsmith@flexion.us>
2022-05-03 11:56:40 -0700

    9492: WIP integration test

--- cypress-smoketests/integration/messages.spec.js
+++ cypress-smoketests/integration/messages.spec.js
@@ -1,5 +1,9 @@
 const {
+  alertSuccessShouldExist,
+  createMessage,
+  enterSubject,
   fillOutMessageField,
+  goToCaseDetail,
   goToDocumentNeedingQC,
   goToMyDocumentQC,
   goToSectionDocumentQC,
@@ -30,46 +34,61 @@ describe('Docket Clerk', () => {
     login(token);
   });
 
-  it('should go to section document QC inbox', () => {
-    goToMyDocumentQC();
-    goToSectionDocumentQC();
+  describe('complete and send message', () => {
+    it('should go to section document QC inbox', () => {
+      goToMyDocumentQC();
+      goToSectionDocumentQC();
+    });
+
+    it('should go to first document needing QC', () => {
+      goToDocumentNeedingQC();
+    });
+
+    it('should open dialog to complete & send message', () => {
+      openCompleteAndSendMessageDialog();
+    });
+
+    it('should fill out dialog to complete & send message', () => {
+      selectSection('ADC');
+      selectRecipient('Test ADC');
+      fillOutMessageField();
+    });
+
+    it('should send the message', () => {
+      sendMessage();
+      progressIndicatorDoesNotExist();
+    });
   });
 
-  it('should go to first document needing QC', () => {
-    goToDocumentNeedingQC();
+  describe('create and send a message', () => {
+    it('should go to case detail and open the dialog to create a new message', () => {
+      goToCaseDetail('103-20');
+      createMessage();
+    });
+
+    it('should fill out the form and send the new message', () => {
+      selectSection('ADC');
+      selectRecipient('Test ADC');
+      enterSubject();
+      fillOutMessageField();
+      sendMessage();
+      alertSuccessShouldExist();
+    });
   });
 
-  it('should open dialog to complete & send message', () => {
-    openCompleteAndSendMessageDialog();
+  describe('reply to a message', () => {
+    // go to message ^^^
+    // reply to it
+    // expect that the message came to me (we think with the bug this would have been broken)
   });
-
-  it('should fill out dialog to complete & send message', () => {
-    selectSection('ADC');
-    selectRecipient('Test ADC');
-    fillOutMessageField();
+  describe('forward a message', () => {
+    // go to message ^^^
+    // forward it
+    // expect that the message came to me (we think with the bug this would have been broken)
   });
-
-  it('should send the message', () => {
-    sendMessage();
-    progressIndicatorDoesNotExist();
+  describe('complete a message', () => {
+    // go to message ^^^
+    // complete it
+    // expect that the message came to me (we think with the bug this would have been broken)
   });
 });
-// complete and send message
-// fill out form
-// send message
-
-// go to 103-20
-// create new message
-// send message
-
-// go to message ^^^
-// reply to it
-// expect that the message came to me (we think with the bug this would have been broken)
-
-// go to message ^^^
-// forward it
-// expect that the message came to me (we think with the bug this would have been broken)
-
-// go to message ^^^
-// complete it
-// expect that the message came to me (we think with the bug this would have been broken)
