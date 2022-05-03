8a198ef3e9383359f08e85621a168dd7c98a8599
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-05-03 11:01:32 -0700

    9492: Scaffold cypress test around bug

---
+++ cypress-smoketests/integration/messages.spec.js
@@ -0,0 +1,22 @@
+// login as docketclerk
+// go to section document qc
+// click document for 103-20
+// complete and send message
+// fill out form
+// send message
+
+// go to 103-20
+// create new message
+// send message
+
+// go to message ^^^
+// reply to it
+// expect that the message came to me (we think with the bug this would have been broken)
+
+// go to message ^^^
+// forward it
+// expect that the message came to me (we think with the bug this would have been broken)
+
+// go to message ^^^
+// complete it
+// expect that the message came to me (we think with the bug this would have been broken)
