4237f6fa84b81463d544b8af2121a6836f06e878
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-05-05 08:19:37 -0600

    Devex 974: Clean up and unify open/closed tests in interactor, refactoring

--- shared/src/business/useCases/getCasesForUserInteractor.test.js
+++ shared/src/business/useCases/getCasesForUserInteractor.test.js
@@ -43,25 +43,24 @@ describe('getCasesForUserInteractor', () => {
     expect(applicationContext.getCurrentUser).toBeCalled();
   });
 
-  it('should make a call to retrieve open cases by user', async () => {
+  it('should make a call to retrieve open and closed cases for the current user', async () => {
     await getCasesForUserInteractor(applicationContext);
 
     expect(
       applicationContext.getPersistenceGateway().getCasesForUser,
     ).toHaveBeenCalledWith({
       applicationContext,
-      statuses: applicationContext.getConstants().OPEN_CASE_STATUSES,
       userId: 'd7d90c05-f6cd-442c-a168-202db587f16f',
     });
   });
 
-  it('should validate the list of found open cases', async () => {
+  it('should validate the list of cases found for the current user', async () => {
     await getCasesForUserInteractor(applicationContext);
 
     expect(UserCase.validateRawCollection).toBeCalled();
   });
 
-  it('should return a list of open cases', async () => {
+  it('should return a list of open and closed cases for the current user', async () => {
     const { openCaseList } = await getCasesForUserInteractor(
       applicationContext,
     );
@@ -71,6 +70,40 @@ describe('getCasesForUserInteractor', () => {
         caseCaption: MOCK_CASE.caseCaption,
         docketNumber: MOCK_CASE.docketNumber,
         docketNumberWithSuffix: MOCK_CASE.docketNumberWithSuffix,
+      },
+    ]);
+  });
+
+  it('should return a list of closed cases sorted by closedDate descending', async () => {
+    const closedToday = applicationContext.getUtilities().createISODateString();
+    const closedYesterday = applicationContext
+      .getUtilities()
+      .calculateISODate({ dateString: closedToday, howMuch: -1 });
+    mockFoundCasesList = [
+      {
+        ...MOCK_CASE,
+        closedDate: closedYesterday,
+        status: CASE_STATUS_TYPES.closed,
+      },
+      {
+        ...MOCK_CASE,
+        closedDate: closedToday,
+        status: CASE_STATUS_TYPES.closed,
+      },
+    ];
+
+    const { closedCaseList } = await getCasesForUserInteractor(
+      applicationContext,
+    );
+
+    expect(closedCaseList).toMatchObject([
+      {
+        closedDate: closedToday,
+        docketNumber: MOCK_CASE.docketNumber,
+      },
+      {
+        closedDate: closedYesterday,
+        docketNumber: MOCK_CASE.docketNumber,
       },
     ]);
   });
@@ -111,83 +144,5 @@ describe('getCasesForUserInteractor', () => {
     expect(openCaseList[0].consolidatedCases[0]).toBe(
       consolidatedCaseThatIsNotTheLeadCase,
     );
-  });
-});
-
-describe('closed case stuff for now', () => {
-  let mockFoundCasesList;
-  const recentClosedDate = applicationContext
-    .getUtilities()
-    .createISODateString();
-  const pastClosedDate = applicationContext
-    .getUtilities()
-    .calculateISODate({ dateString: recentClosedDate, howMuch: -1 });
-
-  beforeEach(() => {
-    mockFoundCasesList = [
-      {
-        ...MOCK_CASE,
-        closedDate: pastClosedDate,
-        status: CASE_STATUS_TYPES.closed,
-      },
-      {
-        ...MOCK_CASE,
-        closedDate: recentClosedDate,
-        status: CASE_STATUS_TYPES.closed,
-      },
-    ];
-
-    applicationContext
-      .getPersistenceGateway()
-      .getUserById.mockReturnValue(
-        MOCK_USERS['d7d90c05-f6cd-442c-a168-202db587f16f'],
-      );
-    applicationContext.getCurrentUser.mockReturnValue(
-      MOCK_USERS['d7d90c05-f6cd-442c-a168-202db587f16f'],
-    );
-    applicationContext
-      .getPersistenceGateway()
-      .getCasesForUser.mockImplementation(() => mockFoundCasesList);
-    UserCase.validateRawCollection.mockImplementation(
-      foundCases => foundCases || [],
-    );
-  });
-
-  it('should make a call to retrieve closed cases by user', async () => {
-    await getCasesForUserInteractor(applicationContext);
-
-    expect(
-      applicationContext.getPersistenceGateway().getCasesForUser,
-    ).toHaveBeenCalledWith({
-      applicationContext,
-      userId: 'd7d90c05-f6cd-442c-a168-202db587f16f',
-    });
-  });
-
-  it('should validate the found closed cases', async () => {
-    await getCasesForUserInteractor(applicationContext);
-
-    expect(UserCase.validateRawCollection).toBeCalled();
-  });
-
-  it('should return a list of closed cases sorted by closedDate descending', async () => {
-    MOCK_CASE.status = 'Closed';
-
-    const { closedCaseList } = await getCasesForUserInteractor(
-      applicationContext,
-    );
-
-    expect(closedCaseList).toMatchObject([
-      {
-        caseCaption: MOCK_CASE.caseCaption,
-        closedDate: recentClosedDate,
-        docketNumber: MOCK_CASE.docketNumber,
-        docketNumberWithSuffix: MOCK_CASE.docketNumberWithSuffix,
-      },
-      {
-        closedDate: pastClosedDate,
-        docketNumber: MOCK_CASE.docketNumber,
-      },
-    ]);
   });
 });
