4b99ee68bd3aa1adf4a7ff09ee0662b92a065e49
Cody Seibert <cseibert113@gmail.com>
2022-05-06 11:12:12 -0400

    devex - adding a test to cover new interactor

---
+++ shared/src/business/useCases/users/getJudgeInSectionInteractor.test.js
@@ -0,0 +1,43 @@
+const {
+  applicationContext,
+} = require('../../test/createTestApplicationContext');
+const {
+  getJudgeInSectionInteractor,
+} = require('./getJudgeInSectionInteractor');
+const { ROLES } = require('../../entities/EntityConstants');
+
+describe('getJudgeInSectionInteractor', () => {
+  beforeEach(() => {
+    applicationContext
+      .getUseCaseHelpers()
+      .getJudgeInSectionHelper.mockResolvedValue({
+        judgeName: 'buch',
+      });
+  });
+
+  it('throws an exception if a petitioner tries to get the judge of a section', async () => {
+    applicationContext.getCurrentUser.mockReturnValue({
+      role: ROLES.petitioner,
+    });
+
+    await expect(() =>
+      getJudgeInSectionInteractor(applicationContext, {
+        section: 'buchsChambers',
+      }),
+    ).rejects.toThrow('Unauthorized');
+  });
+
+  it('returns the correct judge returned from the helper', async () => {
+    applicationContext.getCurrentUser.mockReturnValue({
+      role: ROLES.judge,
+    });
+
+    expect(
+      await getJudgeInSectionInteractor(applicationContext, {
+        section: 'buchsChambers',
+      }),
+    ).toEqual({
+      judgeName: 'buch',
+    });
+  });
+});
