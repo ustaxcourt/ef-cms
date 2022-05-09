90886348534632c8105f518cf392e82ed8059cd6
halprin <halprin@users.noreply.github.com>
2022-05-09 16:39:18 -0600

    9490: Default the sorting for the other boxes and unset the sorting for other queue's and roles

--- web-client/src/presenter/actions/setDefaultTableSortAction.js
+++ web-client/src/presenter/actions/setDefaultTableSortAction.js
@@ -3,12 +3,28 @@ import { state } from 'cerebral';
 /**
  * sets the default sort based on the section picked.
  *
- * @param {object} props the props passed to the sequence
- * @param {object} store the cerebral store object
+ * @param {object} providers the providers object
+ * @param {Function} providers.get the cerebral get function
+ * @param {object} providers.props the props passed to the sequence
+ * @param {object} providers.store the cerebral store object
  */
-export const setDefaultTableSortAction = ({ props, store }) => {
-  if (props.box === 'completed' && props.queue === 'section') {
-    store.set(state.tableSort.sortField, 'completedAt');
+export const setDefaultTableSortAction = ({ get, props, store }) => {
+  // Story 9490 requires the sorting to only apply to the ADC Section boxes.  So these if statements exist to limit the scope for now.  These if statements can be removed in future stories that expand the scope.
+  const userRole = get(state.user.role);
+  if (props.queue !== 'section' || userRole !== 'adc') {
+    store.unset(state.tableSort);
+    return;
+  }
+
+  // different tables require different sorting
+  if (props.box === 'inbox') {
+    store.set(state.tableSort.sortField, 'createdAt');
     store.set(state.tableSort.sortOrder, 'asc');
+  } else if (props.box === 'outbox') {
+    store.set(state.tableSort.sortField, 'createdAt');
+    store.set(state.tableSort.sortOrder, 'desc');
+  } else if (props.box === 'completed') {
+    store.set(state.tableSort.sortField, 'completedAt');
+    store.set(state.tableSort.sortOrder, 'desc');
   }
 };
