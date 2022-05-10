3cb9281876b6ded399ecdf6a18c68be56b758f74
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2022-05-10 13:58:13 -0500

    9490: Add constants for use in presenters [skip ci]

--- web-client/src/views/Messages/sortConstants.js
+++ web-client/src/views/Messages/sortConstants.js
@@ -1,11 +1,13 @@
+const deepFreeze = require('deep-freeze');
+
 const CHRONOLOGICALLY_ASCENDING = 'Oldest to newest';
 const CHRONOLOGICALLY_DESCENDING = 'Newest to oldest';
 const ALPHABETICALLY_ASCENDING = 'In A-Z ascending order';
 const ALPHABETICALLY_DESCENDING = 'In Z-A descending order';
 
-module.exports = {
+module.exports = deepFreeze({
   ALPHABETICALLY_ASCENDING,
   ALPHABETICALLY_DESCENDING,
   CHRONOLOGICALLY_ASCENDING,
   CHRONOLOGICALLY_DESCENDING,
-};
+});
