2c352ba90f506cfe7e45a27b5d323e2abcec7f98
Rick Goshen <rgoshen@flexion.us>
2022-05-10 12:28:37 -0700

    9490: add hasMessages to component

--- web-client/src/ustc-ui/SortableColumnHeaderButton/SortableColumnHeaderButton.jsx
+++ web-client/src/ustc-ui/SortableColumnHeaderButton/SortableColumnHeaderButton.jsx
@@ -8,6 +8,7 @@ export const SortableColumnHeaderButton = ({
   ascText = 'in ascending order',
   defaultSort,
   descText = 'in descending order',
+  hasMessages,
   onClickSequence,
   sortField,
   tableSort,
@@ -26,7 +27,7 @@ export const SortableColumnHeaderButton = ({
     >
       <span
         className={classNames('margin-right-105', {
-          sortActive: tableSort.sortField === sortField,
+          sortActive: tableSort.sortField === sortField && hasMessages,
         })}
       >
         {title}
