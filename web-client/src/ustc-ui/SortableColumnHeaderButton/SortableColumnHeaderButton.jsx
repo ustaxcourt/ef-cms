bef0dcfb9bb68a8348e6073171c9a0fe977e58fc
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2022-05-10 14:37:58 -0500

    9490: Use hasRows to inform onClick of header

--- web-client/src/ustc-ui/SortableColumnHeaderButton/SortableColumnHeaderButton.jsx
+++ web-client/src/ustc-ui/SortableColumnHeaderButton/SortableColumnHeaderButton.jsx
@@ -18,12 +18,14 @@ export const SortableColumnHeaderButton = ({
     <Button
       link
       className={'sortable-header-button margin-right-0'}
-      onClick={() =>
-        onClickSequence({
-          defaultSort,
-          sortField,
-        })
-      }
+      onClick={() => {
+        if (hasRows) {
+          onClickSequence({
+            defaultSort,
+            sortField,
+          });
+        }
+      }}
     >
       <span
         className={classNames('margin-right-105', {
@@ -39,18 +41,6 @@ export const SortableColumnHeaderButton = ({
         sortField,
         tableSort,
       })}
-      {/*{tableSort.sortField !== sortField && defaultSort === 'desc' && (*/}
-      {/*  <FontAwesomeIcon icon="caret-down" title="in descending order" />*/}
-      {/*)}*/}
-      {/*{tableSort.sortField !== sortField && defaultSort === 'asc' && (*/}
-      {/*  <FontAwesomeIcon icon="caret-up" title="in ascending order" />*/}
-      {/*)}*/}
-      {/*{tableSort.sortField === sortField && tableSort.sortOrder === 'asc' && (*/}
-      {/*  <FontAwesomeIcon icon="caret-up" title="in ascending order" />*/}
-      {/*)}*/}
-      {/*{tableSort.sortField === sortField && tableSort.sortOrder === 'desc' && (*/}
-      {/*  <FontAwesomeIcon icon="caret-down" title="in descending order" />*/}
-      {/*)}*/}
     </Button>
   );
 };
