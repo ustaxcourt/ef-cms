9694b1ecd156f699bb30ef1fa1364e6e94f6efd4
Timothy Shumway <tshumway@flexion.us>
2022-05-10 14:32:10 -0600

    9490 - adding testing around section messages [skip ci]

--- web-client/integration-tests/adcClerkViewsSectionMessagesJourney.test.js
+++ web-client/integration-tests/adcClerkViewsSectionMessagesJourney.test.js
@@ -12,6 +12,8 @@ describe('ADC Clerk Views Section Messages Journey', () => {
     cerebralTest.closeSocket();
   });
 
+  const testAdcId = '6805d1ab-18d0-43ec-bafb-654e83405416';
+
   loginAs(cerebralTest, 'petitioner@example.com');
   it('Create case', async () => {
     const caseDetail = await uploadPetition(cerebralTest);
@@ -21,20 +23,36 @@ describe('ADC Clerk Views Section Messages Journey', () => {
 
   const message1Subject = `message 1 ${Date.now()}`;
   const message2Subject = `message 2 ${Date.now()}`;
+  const message3Subject = `message 3 ${Date.now()}`;
 
   // Send some messages to ADC user(s)
   loginAs(cerebralTest, 'petitionsclerk@example.com');
   createNewMessageOnCase(cerebralTest, {
     subject: message1Subject,
     toSection: 'adc',
-    toUserId: 'adc1',
+    toUserId: testAdcId,
   });
 
   loginAs(cerebralTest, 'docketclerk@example.com');
   createNewMessageOnCase(cerebralTest, {
     subject: message2Subject,
     toSection: 'adc',
-    toUserId: 'adc1',
+    toUserId: testAdcId,
+  });
+
+  loginAs(cerebralTest, 'docketclerk@example.com');
+  createNewMessageOnCase(cerebralTest, {
+    subject: message3Subject,
+    toSection: 'adc',
+    toUserId: testAdcId,
+  });
+
+  loginAs(cerebralTest, 'adc@example.com');
+  it('go to section inbox', async () => {
+    await cerebralTest.runSequence('gotoMessagesSequence', {
+      box: 'inbox',
+      queue: 'section',
+    });
   });
 
   // Do sorting and validate:
@@ -42,4 +60,5 @@ describe('ADC Clerk Views Section Messages Journey', () => {
   //    correct icon direction
   //    correct column selection (i.e. underlining)
   //    correct handling of empty table
+  //    Send messages from adc
 });
