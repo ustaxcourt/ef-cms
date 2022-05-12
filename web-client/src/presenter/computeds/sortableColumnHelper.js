ce7b0e7d1a40378ebad8e3bcec01634f635672d4
Cody Seibert <cseibert113@gmail.com>
2022-05-12 15:57:34 -0400

    9490 - refactoring logic for checking if the sortable header is active

--- web-client/src/presenter/computeds/sortableColumnHelper.js
+++ web-client/src/presenter/computeds/sortableColumnHelper.js
@@ -1,32 +1,45 @@
 import { ASCENDING, DESCENDING } from '../presenterConstants';
 
 export const sortableColumnHelper = () => {
-  return ({ ascText, defaultSort, descText, sortField, tableSort }) => {
-    let direction = '';
-    let title = '';
-    if (tableSort.sortField !== sortField && defaultSort === DESCENDING) {
-      direction = DESCENDING;
-      title = descText;
-    } else if (tableSort.sortField !== sortField && defaultSort === ASCENDING) {
-      direction = ASCENDING;
-      title = ascText;
-    } else if (
-      tableSort.sortField === sortField &&
-      tableSort.sortOrder === DESCENDING
-    ) {
-      direction = DESCENDING;
-      title = descText;
-    } else if (
-      tableSort.sortField === sortField &&
-      tableSort.sortOrder === ASCENDING
-    ) {
-      direction = ASCENDING;
-      title = ascText;
-    }
+  return {
+    getSortIndicatorConfiguration: ({
+      ascText,
+      defaultSort,
+      descText,
+      sortField,
+      tableSort,
+    }) => {
+      let direction = '';
+      let title = '';
+      if (tableSort.sortField !== sortField && defaultSort === DESCENDING) {
+        direction = DESCENDING;
+        title = descText;
+      } else if (
+        tableSort.sortField !== sortField &&
+        defaultSort === ASCENDING
+      ) {
+        direction = ASCENDING;
+        title = ascText;
+      } else if (
+        tableSort.sortField === sortField &&
+        tableSort.sortOrder === DESCENDING
+      ) {
+        direction = DESCENDING;
+        title = descText;
+      } else if (
+        tableSort.sortField === sortField &&
+        tableSort.sortOrder === ASCENDING
+      ) {
+        direction = ASCENDING;
+        title = ascText;
+      }
 
-    return {
-      direction,
-      title,
-    };
+      return {
+        direction,
+        title,
+      };
+    },
+    isActive: ({ hasRows, sortField, tableSort }) =>
+      tableSort.sortField === sortField && hasRows,
   };
 };
--- web-client/src/presenter/computeds/sortableColumnHelper.test.js
+++ web-client/src/presenter/computeds/sortableColumnHelper.test.js
@@ -3,96 +3,144 @@ import { runCompute } from 'cerebral/test';
 import { sortableColumnHelper } from './sortableColumnHelper';
 
 describe('sortableColumnHelper', () => {
-  it('direction is set to descending when the last sortField is different from the provided sortField argument and defaultSort is DESCENDING', () => {
-    const getIconFn = runCompute(sortableColumnHelper);
-    const DESC_TEXT = 'down';
-    expect(
-      getIconFn({
-        defaultSort: DESCENDING,
-        descText: DESC_TEXT,
-        sortField: 'createdAt',
-        tableSort: {
+  describe('getSortIndicatorConfiguration', () => {
+    it('direction is set to descending when the last sortField is different from the provided sortField argument and defaultSort is DESCENDING', () => {
+      const { getSortIndicatorConfiguration } =
+        runCompute(sortableColumnHelper);
+      const DESC_TEXT = 'down';
+      expect(
+        getSortIndicatorConfiguration({
+          defaultSort: DESCENDING,
+          descText: DESC_TEXT,
+          sortField: 'createdAt',
+          tableSort: {
+            sortField: 'docketNumber',
+          },
+        }),
+      ).toMatchObject({
+        direction: DESCENDING,
+        title: DESC_TEXT,
+      });
+    });
+
+    it('direction is set to ASCENDING when the last sortField is different from the provided sortField argument and defaultSort is ASCENDING', () => {
+      const { getSortIndicatorConfiguration } =
+        runCompute(sortableColumnHelper);
+      const ASC_TEXT = 'up';
+      expect(
+        getSortIndicatorConfiguration({
+          ascText: ASC_TEXT,
+          defaultSort: ASCENDING,
+          sortField: 'createdAt',
+          tableSort: {
+            sortField: 'docketNumber',
+          },
+        }),
+      ).toMatchObject({
+        direction: ASCENDING,
+        title: ASC_TEXT,
+      });
+    });
+
+    it('direction is set to DESCENDING when the last sortField is the same as the provided sortField argument and sortOrder is DESCENDING', () => {
+      const { getSortIndicatorConfiguration } =
+        runCompute(sortableColumnHelper);
+      const DESC_TEXT = 'down';
+      expect(
+        getSortIndicatorConfiguration({
+          defaultSort: DESCENDING,
+          descText: DESC_TEXT,
+          sortField: 'createdAt',
+          tableSort: {
+            sortField: 'createdAt',
+            sortOrder: DESCENDING,
+          },
+        }),
+      ).toMatchObject({
+        direction: DESCENDING,
+        title: DESC_TEXT,
+      });
+    });
+
+    it('direction is set to ASCENDING when the last sortField is the same as the provided sortField argument and sortOrder is ASCENDING', () => {
+      const { getSortIndicatorConfiguration } =
+        runCompute(sortableColumnHelper);
+      const ASC_TEXT = 'up';
+      expect(
+        getSortIndicatorConfiguration({
+          ascText: ASC_TEXT,
+          defaultSort: ASCENDING,
+          sortField: 'createdAt',
+          tableSort: {
+            sortField: 'createdAt',
+            sortOrder: ASCENDING,
+          },
+        }),
+      ).toMatchObject({
+        direction: ASCENDING,
+        title: ASC_TEXT,
+      });
+    });
+
+    it('sets direction and title to empty strings if no branch logic is executed', () => {
+      const { getSortIndicatorConfiguration } =
+        runCompute(sortableColumnHelper);
+      const ASC_TEXT = 'up';
+      expect(
+        getSortIndicatorConfiguration({
+          ascText: ASC_TEXT,
+          defaultSort: ASCENDING,
+          sortField: 'createdAt',
+          tableSort: {
+            sortField: 'createdAt',
+            sortOrder: 'UNKNOWN',
+          },
+        }),
+      ).toMatchObject({
+        direction: '',
+        title: '',
+      });
+    });
+  });
+
+  describe('isActive', () => {
+    it('returns false when hasRows is false', () => {
+      const { isActive } = runCompute(sortableColumnHelper);
+      expect(
+        isActive({
+          hasRows: false,
+          sortField: 'createdAt',
+          tableSort: {
+            sortField: 'createdAt',
+          },
+        }),
+      ).toBeFalsy();
+    });
+
+    it('returns false when sortField does not match tableSort.sortField', () => {
+      const { isActive } = runCompute(sortableColumnHelper);
+      expect(
+        isActive({
+          hasRows: true,
           sortField: 'docketNumber',
-        },
-      }),
-    ).toMatchObject({
-      direction: DESCENDING,
-      title: DESC_TEXT,
+          tableSort: {
+            sortField: 'createdAt',
+          },
+        }),
+      ).toBeFalsy();
     });
-  });
 
-  it('direction is set to ASCENDING when the last sortField is different from the provided sortField argument and defaultSort is ASCENDING', () => {
-    const getIconFn = runCompute(sortableColumnHelper);
-    const ASC_TEXT = 'up';
-    expect(
-      getIconFn({
-        ascText: ASC_TEXT,
-        defaultSort: ASCENDING,
-        sortField: 'createdAt',
-        tableSort: {
-          sortField: 'docketNumber',
-        },
-      }),
-    ).toMatchObject({
-      direction: ASCENDING,
-      title: ASC_TEXT,
-    });
-  });
-
-  it('direction is set to DESCENDING when the last sortField is the same as the provided sortField argument and sortOrder is DESCENDING', () => {
-    const getIconFn = runCompute(sortableColumnHelper);
-    const DESC_TEXT = 'down';
-    expect(
-      getIconFn({
-        defaultSort: DESCENDING,
-        descText: DESC_TEXT,
-        sortField: 'createdAt',
-        tableSort: {
+    it('returns true when sortFields match and hasRows is true', () => {
+      const { isActive } = runCompute(sortableColumnHelper);
+      expect(
+        isActive({
+          hasRows: true,
           sortField: 'createdAt',
-          sortOrder: DESCENDING,
-        },
-      }),
-    ).toMatchObject({
-      direction: DESCENDING,
-      title: DESC_TEXT,
-    });
-  });
-
-  it('direction is set to ASCENDING when the last sortField is the same as the provided sortField argument and sortOrder is ASCENDING', () => {
-    const getIconFn = runCompute(sortableColumnHelper);
-    const ASC_TEXT = 'up';
-    expect(
-      getIconFn({
-        ascText: ASC_TEXT,
-        defaultSort: ASCENDING,
-        sortField: 'createdAt',
-        tableSort: {
-          sortField: 'createdAt',
-          sortOrder: ASCENDING,
-        },
-      }),
-    ).toMatchObject({
-      direction: ASCENDING,
-      title: ASC_TEXT,
-    });
-  });
-
-  it('sets direction and title to empty strings if no branch logic is executed', () => {
-    const getIconFn = runCompute(sortableColumnHelper);
-    const ASC_TEXT = 'up';
-    expect(
-      getIconFn({
-        ascText: ASC_TEXT,
-        defaultSort: ASCENDING,
-        sortField: 'createdAt',
-        tableSort: {
-          sortField: 'createdAt',
-          sortOrder: 'UNKNOWN',
-        },
-      }),
-    ).toMatchObject({
-      direction: '',
-      title: '',
+          tableSort: {
+            sortField: 'createdAt',
+          },
+        }),
+      ).toBeTruthy();
     });
   });
 });
