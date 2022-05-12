b67b301309657b0143bc0f9dd3aa039a56cf1681
Michael Marcotte <mike.marcotte@gmail.com>
2022-05-12 17:10:39 -0400

    ustc-2142: fix switch colors script

--- web-api/switch-bounce-handler-colors.js
+++ web-api/switch-bounce-handler-colors.js
@@ -7,22 +7,39 @@ const check = (value, message) => {
   }
 };
 
-const { AWS_ACCOUNT_ID, CURRENT_COLOR, DEPLOYING_COLOR, EFCMS_DOMAIN, ENV } =
-  process.env;
+const { AWS_ACCOUNT_ID, CURRENT_COLOR, DEPLOYING_COLOR, ENV } = process.env;
 
 check(CURRENT_COLOR, 'You must have DEPLOYING_COLOR set in your environment');
 check(DEPLOYING_COLOR, 'You must have DEPLOYING_COLOR set in your environment');
-check(EFCMS_DOMAIN, 'You must have EFCMS_DOMAIN set in your environment');
 check(ENV, 'You must have ENV set in your environment');
 check(AWS_ACCOUNT_ID, 'You must have AWS_ACCOUNT_ID set in your environment');
 
 const SNS = new AWS.SNS({ region: 'us-east-1' });
+const TopicArn = `arn:aws:sns:us-east-1:${AWS_ACCOUNT_ID}:bounced_service_emails_${ENV}`;
+
+const getSubscription = async Token => {
+  const CurrentLambda = `arn:aws:lambda:us-east-1:${AWS_ACCOUNT_ID}:function:bounce_handler_${ENV}_${CURRENT_COLOR}`;
+  const { NextToken, Subscriptions } = await SNS.listSubscriptions({
+    NextToken: Token,
+  }).promise();
+
+  const foundSubscription = Subscriptions.find(
+    ({ Endpoint, SubTopicArn, TopicArn: Protocol }) =>
+      SubTopicArn === TopicArn &&
+      Protocol === 'lambda' &&
+      Endpoint === CurrentLambda,
+  );
+
+  if (foundSubscription) return foundSubscription.SubscriptionArn;
+
+  if (NextToken) {
+    return await getSubscription(NextToken);
+  }
+};
 
 const run = async () => {
-  const TopicArn = `arn:aws:sns:us-east-1:${AWS_ACCOUNT_ID}:bounced_service_emails_${ENV}`;
   const Protocol = 'lambda';
   const DeployingLambda = `arn:aws:lambda:us-east-1:${AWS_ACCOUNT_ID}:function:bounce_handler_${ENV}_${DEPLOYING_COLOR}`;
-  const CurrentLambda = `arn:aws:lambda:us-east-1:${AWS_ACCOUNT_ID}:function:bounce_handler_${ENV}_${CURRENT_COLOR}`;
 
   await SNS.subscribe({
     Endpoint: DeployingLambda,
@@ -30,18 +47,10 @@ const run = async () => {
     TopicArn,
   }).promise();
 
-  const { SubscriptionArn } = await SNS.confirmSubscription({
-    Endpoint: CurrentLambda,
-    Protocol,
-    TopicArn,
-  }).promise();
+  const SubscriptionArn = await getSubscription();
 
   if (SubscriptionArn) {
-    await SNS.unsubscribe({
-      Endpoint: CurrentLambda,
-      Protocol,
-      TopicArn,
-    }).promise();
+    await SNS.unsubscribe({ SubscriptionArn }).promise();
   }
 };
 
