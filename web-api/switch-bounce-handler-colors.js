5202df483bd5db122d10356dcd1a64105eeabbb0
Michael Marcotte <mike.marcotte@gmail.com>
2022-05-13 12:59:27 -0400

    ustc-2142: fix switch-colors script for bounce handler

--- web-api/switch-bounce-handler-colors.js
+++ web-api/switch-bounce-handler-colors.js
@@ -8,32 +8,41 @@ const check = (value, message) => {
 };
 
 const { AWS_ACCOUNT_ID, CURRENT_COLOR, DEPLOYING_COLOR, ENV } = process.env;
+const SNS = new AWS.SNS({ region: 'us-east-1' });
+const TopicArn = `arn:aws:sns:us-east-1:${AWS_ACCOUNT_ID}:bounced_service_emails_${ENV}`;
 
 check(CURRENT_COLOR, 'You must have DEPLOYING_COLOR set in your environment');
 check(DEPLOYING_COLOR, 'You must have DEPLOYING_COLOR set in your environment');
 check(ENV, 'You must have ENV set in your environment');
 check(AWS_ACCOUNT_ID, 'You must have AWS_ACCOUNT_ID set in your environment');
 
-const SNS = new AWS.SNS({ region: 'us-east-1' });
-const TopicArn = `arn:aws:sns:us-east-1:${AWS_ACCOUNT_ID}:bounced_service_emails_${ENV}`;
-
-const getSubscription = async Token => {
+/**
+ * getCurrentColorSubscription
+ *
+ * Returns the ARN of the current color's lambda subscription, if one exists. Otherwise it
+ * returns undefined
+ *
+ * @param {string} Token A string to use if we do not find what we are looking for and there are more results
+ * @returns {Promise} which resolves to the ARN of the current color's Lambda subscription, if found
+ */
+const getCurrentColorSubscription = async Token => {
   const CurrentLambda = `arn:aws:lambda:us-east-1:${AWS_ACCOUNT_ID}:function:bounce_handler_${ENV}_${CURRENT_COLOR}`;
   const { NextToken, Subscriptions } = await SNS.listSubscriptions({
     NextToken: Token,
   }).promise();
 
+  // Look for subscriptions that match the current color, and return it.
   const foundSubscription = Subscriptions.find(
-    ({ Endpoint, SubTopicArn, TopicArn: Protocol }) =>
-      SubTopicArn === TopicArn &&
+    ({ Endpoint, Protocol, TopicArn: SubscriptionTopicArn }) =>
+      Endpoint === CurrentLambda &&
       Protocol === 'lambda' &&
-      Endpoint === CurrentLambda,
+      SubscriptionTopicArn === TopicArn,
   );
 
   if (foundSubscription) return foundSubscription.SubscriptionArn;
 
   if (NextToken) {
-    return await getSubscription(NextToken);
+    return getCurrentColorSubscription(NextToken);
   }
 };
 
@@ -47,7 +56,7 @@ const run = async () => {
     TopicArn,
   }).promise();
 
-  const SubscriptionArn = await getSubscription();
+  const SubscriptionArn = await getCurrentColorSubscription();
 
   if (SubscriptionArn) {
     await SNS.unsubscribe({ SubscriptionArn }).promise();
