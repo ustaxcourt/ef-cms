35f289265116fa1b2763680bc3a4544a6d046ffc
Michael Marcotte <mike.marcotte@gmail.com>
2022-05-13 16:52:30 -0400

    ustc-2142: add count for bounce mapping

--- web-api/terraform/api/handle-bounced-service-email.tf
+++ web-api/terraform/api/handle-bounced-service-email.tf
@@ -22,6 +22,7 @@ resource "aws_lambda_function" "zip_handle_bounce" {
 }
 
 resource "aws_lambda_event_source_mapping" "bounced_service_emails_mapping" {
+  count            = var.create_bounce_handler
   event_source_arn = "arn:aws:sns:us-east-1:${var.account_id}:bounced_service_emails_${var.environment}"
-  function_name    = aws_lambda_function.zip_handle_bounce.arn
+  function_name    = aws_lambda_function.zip_handle_bounce[0].arn
 }
