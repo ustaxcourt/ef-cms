72f10391178afba78f445455fe54d6c3043b854c
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-05-16 09:57:01 -0700

    Devex 974: PR feedback

--- web-api/src/cases/getCasesForUserLambda.js
+++ web-api/src/cases/getCasesForUserLambda.js
@@ -1,7 +1,7 @@
 const { genericHandler } = require('../genericHandler');
 
 /**
- * used for fetching all open cases
+ * used for fetching all open and closed cases for a particular user
  *
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
