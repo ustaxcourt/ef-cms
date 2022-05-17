1695ae9014db1f05995e84a72119e49d9a7c9423
Rick Goshen <rgoshen@flexion.us>
2022-05-17 12:08:03 -0700

    3569: moved partitioners list to helper

--- web-client/src/presenter/computeds/consolidatedCaseServiceInitiateHelper.js
+++ web-client/src/presenter/computeds/consolidatedCaseServiceInitiateHelper.js
@@ -1,5 +1,33 @@
 import { state } from 'cerebral';
 
-export const consolidatedCaseServiceInitiateHelper = (get, applicationContext) => {
+export const consolidatedCaseServiceInitiateHelper = (
+  get,
+  applicationContext,
+) => {
+  const formattedCaseDetail = get(state.formattedCaseDetail);
+  return formattedCaseDetail.consolidatedCases.map(consolidatedCase => {
+    console.log('consolidatedCase', consolidatedCase);
+    return {
+      docketNumber: consolidatedCase.docketNumber,
+      petitioners: consolidatedCase.petitioners.reduce((acc, current) => {
+        if (consolidatedCase.petitioners.length === 1) {
+          //one petitioner, so no need for an &
+          return current.name;
+        }
 
+        const concatenatedPetitionersName = acc + current.name;
+
+        if (
+          current ===
+          consolidatedCase.petitioners[consolidatedCase.petitioners.length - 1]
+        ) {
+          //this is the last petitioner, no need for an ending &
+          return concatenatedPetitionersName;
+        }
+
+        return concatenatedPetitionersName + ' & ';
+      }, ''),
+      enabled: true,
+    };
+  });
 };
