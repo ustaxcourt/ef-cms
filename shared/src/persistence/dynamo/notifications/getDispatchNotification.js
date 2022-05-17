07f3a223bfb13e1428dad97e5bc5328c58cdaad4
Michael Marcotte <mike.marcotte@gmail.com>
2022-05-17 15:21:10 -0400

    ustc-2142: switch to query so we can filter it

--- shared/src/persistence/dynamo/notifications/getDispatchNotification.js
+++ shared/src/persistence/dynamo/notifications/getDispatchNotification.js
@@ -1,10 +1,26 @@
-const { get } = require('../../dynamodbClientService');
+const { query } = require('../../dynamodbClientService');
 
-exports.getDispatchNotification = ({ applicationContext, channel }) =>
-  get({
-    Key: {
-      pk: 'dispatch-notification',
-      sk: channel,
+/**
+ * getDispatchNotification
+ *
+ * @param {object} providers
+ * @param {object} providers.applicationContext the application context
+ * @param {string} providers.topic the topic of the dispatch being sent
+ * @returns {array} an array of the results returned by the query
+ */
+exports.getDispatchNotification = async ({ applicationContext, topic }) =>
+  query({
+    ExpressionAttributeNames: {
+      '#pk': 'pk',
+      '#sk': 'sk',
+      '#ttl': 'ttl',
     },
+    ExpressionAttributeValues: {
+      ':currentEpoch': Date.now() / 1000,
+      ':pk': 'dispatch-notification',
+      ':sk': topic,
+    },
+    FilterExpression: '#ttl >= :currentEpoch',
+    KeyConditionExpression: '#pk = :pk AND #sk = :sk',
     applicationContext,
   });
