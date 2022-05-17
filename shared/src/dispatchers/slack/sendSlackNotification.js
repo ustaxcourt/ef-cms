585616daf4eb61d5fd83700ee5a121056590c0d4
Michael Marcotte <mike.marcotte@gmail.com>
2022-05-17 10:35:31 -0400

    ustc-2142: add check to slack dispatcher

--- shared/src/dispatchers/slack/sendSlackNotification.js
+++ shared/src/dispatchers/slack/sendSlackNotification.js
@@ -4,11 +4,31 @@
  * @param {object} applicationContext the application context
  * @param {object} providers the providers object
  * @param {string} providers.text the message to dispatch
+ * @param {string} providers.topic the topic of the message to dispatch (to prevent spamming)
  */
-exports.sendSlackNotification = async ({ applicationContext, message }) => {
-  await applicationContext
-    .getHttpClient()
-    .post(applicationContext.getSlackWebhookUrl(), {
-      text: message,
+exports.sendSlackNotification = async ({ applicationContext, text, topic }) => {
+  const url = applicationContext.getSlackWebhookUrl();
+  if (!url) {
+    return;
+  }
+
+  const hasRecentNotification = await applicationContext
+    .getPersistenceGateway()
+    .getDispatchNotification({
+      applicationContext,
+      topic,
     });
+
+  if (hasRecentNotification) {
+    return;
+  }
+
+  await applicationContext.getHttpClient().post(url, {
+    text,
+  });
+
+  await applicationContext.getPersistenceGateway().saveDispatchNotification({
+    applicationContext,
+    topic,
+  });
 };
