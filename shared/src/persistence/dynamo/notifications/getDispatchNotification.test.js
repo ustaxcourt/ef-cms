ac152a8e1aa865bd71a1f723d7470312a0381aa8
Michael Marcotte <mike.marcotte@gmail.com>
2022-05-17 15:21:10 -0400

    ustc-2142: switch to query so we can filter it

--- shared/src/persistence/dynamo/notifications/getDispatchNotification.test.js
+++ shared/src/persistence/dynamo/notifications/getDispatchNotification.test.js
@@ -6,25 +6,32 @@ const { getDispatchNotification } = require('./getDispatchNotification');
 describe('getDispatchNotification', () => {
   beforeEach(() => {
     applicationContext.getDocumentClient().get.mockReturnValue({
-      promise: () =>
-        Promise.resolve({
-          Item: {
-            pk: 'dispatch-notification',
-            sk: 'test-channel',
-          },
-        }),
+      promise: () => Promise.resolve([]),
     });
   });
 
   it('attempts to retrieve the notification for a dispatched message', async () => {
     const result = await getDispatchNotification({
       applicationContext,
-      channel: 'test-channel',
+      topic: 'test-topic',
     });
 
-    console.log(result);
-
-    expect(applicationContext.getDocumentClient().get).toHaveBeenCalled();
-    expect(result).toBeDefined();
+    expect(applicationContext.getDocumentClient().query).toHaveBeenCalledWith(
+      expect.objectContaining({
+        ExpressionAttributeNames: {
+          '#pk': 'pk',
+          '#sk': 'sk',
+          '#ttl': 'ttl',
+        },
+        ExpressionAttributeValues: {
+          ':currentEpoch': expect.anything(),
+          ':pk': 'dispatch-notification',
+          ':sk': 'test-topic',
+        },
+        FilterExpression: '#ttl >= :currentEpoch',
+        KeyConditionExpression: '#pk = :pk AND #sk = :sk',
+      }),
+    );
+    expect(result).toEqual([]);
   });
 });
