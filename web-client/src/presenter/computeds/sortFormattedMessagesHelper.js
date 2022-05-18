6cd46104f019e1b3d09481f71b0dcb84fa4434ea
Timothy Shumway <tshumway@flexion.us>
2022-05-18 11:42:00 -0600

    1064 - extracted sort logic into helper

---
+++ web-client/src/presenter/computeds/sortFormattedMessagesHelper.js
@@ -0,0 +1,31 @@
+export const sortFormattedMessagesHelper = ({
+  formattedCaseMessages,
+  tableSort = {},
+}) => {
+  formattedCaseMessages.sort((a, b) => {
+    let sortNumber = 0;
+    if (!tableSort) {
+      sortNumber = a.createdAt.localeCompare(b.createdAt);
+    } else if (
+      // 'createdAt' = Recieved Column on Inbox Tab and Sent Column on Sent Tab (Outbox)
+      // 'completedAt' = Completed COlumn on Completed Tab
+
+      ['createdAt', 'completedAt', 'subject'].includes(tableSort.sortField)
+    ) {
+      sortNumber = a[tableSort.sortField].localeCompare(b[tableSort.sortField]);
+    } else if (tableSort.sortField === 'docketNumber') {
+      const aSplit = a.docketNumber.split('-');
+      const bSplit = b.docketNumber.split('-');
+
+      if (aSplit[1] !== bSplit[1]) {
+        // compare years if they aren't the same;
+        // compare as strings, because they *might* have suffix
+        sortNumber = aSplit[1].localeCompare(bSplit[1]);
+      } else {
+        // compare index if years are the same, compare as integers
+        sortNumber = +aSplit[0] - +bSplit[0];
+      }
+    }
+    return sortNumber;
+  });
+};
