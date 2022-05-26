e17947d28fb53a849db9e5824c74d6295e5eda69
Absolutestunna <joshujah007@gmail.com>
2022-05-26 14:51:37 -0400

    9490 - refactoring and clean up based on feedback

--- web-client/integration-tests/journey/getUserMessageCount.js
+++
@@ -1,8 +0,0 @@
-export const getUserMessageCount = async (cerebralTest, box, queue) => {
-  await cerebralTest.runSequence('gotoMessagesSequence', {
-    box,
-    queue,
-  });
-
-  return cerebralTest.getState('messages').length;
-};
