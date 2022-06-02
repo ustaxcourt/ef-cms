e02b87b89c070263aec9fcd2be4b5c0921183f22
Cody Seibert <cseibert113@gmail.com>
2022-06-02 11:31:18 -0400

    9488 - refactoring tests by extracting a mock message object

---
+++ shared/src/test/mockMessages.js
@@ -0,0 +1,25 @@
+exports.MOCK_MESSAGE = {
+  caseStatus: 'Open',
+  createdAt: '2019-01-02T17:29:13.122Z',
+  docketNumber: '103-20',
+  from: 'petitionsclerk',
+  fromSection: 'petitionsclerk',
+  isCompleted: false,
+  message: 'This is a test message',
+  to: 'petitionsclerk',
+  toSection: 'petitionsclerk',
+};
+
+exports.MOCK_COMPLETED_MESSAGE = {
+  caseStatus: 'Open',
+  completedAt: '2019-05-01T17:29:13.122Z',
+  completedBy: 'bob',
+  createdAt: '2019-01-02T17:29:13.122Z',
+  docketNumber: '103-20',
+  from: 'petitionsclerk',
+  fromSection: 'petitionsclerk',
+  isCompleted: true,
+  message: 'This is a test message',
+  to: 'petitionsclerk',
+  toSection: 'petitionsclerk',
+};
