c0cd60b04c4cb9f3c740fc50cc87a404a797b199
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2022-06-03 15:39:20 -0500

    3569: Handle ConditionalCheckFailedException from dynamodb [skip ci]

--- shared/src/persistence/dynamo/documents/updateDocketEntryPendingServiceStatus.js
+++ shared/src/persistence/dynamo/documents/updateDocketEntryPendingServiceStatus.js
@@ -6,20 +6,30 @@ exports.updateDocketEntryPendingServiceStatus = async ({
   docketNumber,
   status,
 }) => {
-  // updated to conditional update since this function should never create a docket entry record if it does not already exist
-  await client.update({
-    ConditionExpression: 'attribute_exists(docketEntryId)',
-    ExpressionAttributeNames: {
-      '#isPendingService': 'isPendingService',
-    },
-    ExpressionAttributeValues: {
-      ':status': status,
-    },
-    Key: {
-      pk: `case|${docketNumber}`,
-      sk: `docket-entry|${docketEntryId}`,
-    },
-    UpdateExpression: 'SET #isPendingService = :status',
-    applicationContext,
-  });
+  try {
+    // updated to conditional update since this function should never create a docket entry record if it does not already exist
+    await client.update({
+      ConditionExpression: 'attribute_exists(docketEntryId)',
+      ExpressionAttributeNames: {
+        '#isPendingService': 'isPendingService',
+      },
+      ExpressionAttributeValues: {
+        ':status': status,
+      },
+      Key: {
+        pk: `case|${docketNumber}`,
+        sk: `docket-entry|${docketEntryId}`,
+      },
+      UpdateExpression: 'SET #isPendingService = :status',
+      applicationContext,
+    });
+  } catch (e) {
+    if (e.code === 'ConditionalCheckFailedException') {
+      applicationContext.logger.info(
+        `Tried to reset docket entry pending state on non-existent docket entry on docket number ${docketNumber}.`,
+      );
+    } else {
+      throw e;
+    }
+  }
 };
