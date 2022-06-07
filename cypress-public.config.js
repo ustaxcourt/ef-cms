7bf81c182c7a5b845e71b0540e7ea1b235d3e730
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-06-07 14:35:42 -0700

    Only run tests for now, update cypress integration public tests to use new Cypress 10 config

--- cypress-public.config.js
+++ cypress-public.config.js
@@ -1,22 +1,30 @@
+const fs = require('fs');
+const path = require('path');
 const {
-  getEmailVerificationToken,
+  setAllowedTerminalIpAddresses,
 } = require('./cypress-integration/support/database');
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  browser: 'edge',
-  chromeWebSecurity: false,
+  browser: 'chrome',
   defaultCommandTimeout: 20000,
   e2e: {
-    baseUrl: 'http://localhost:1234',
+    baseUrl: 'http://localhost:5678',
     setupNodeEvents(on) {
       on('task', {
-        getEmailVerificationToken({ userId }) {
-          return getEmailVerificationToken({ userId });
+        modifyDeployedDateTextFile(deployedDate) {
+          fs.writeFileSync(
+            path.join(__dirname, './web-client/src/deployed-date.txt'),
+            deployedDate,
+          );
+          return null;
+        },
+        setAllowedTerminalIpAddresses(ipAddresses) {
+          return setAllowedTerminalIpAddresses(ipAddresses);
         },
       });
     },
-    specPattern: 'cypress-integration/integration/*.spec.js',
+    specPattern: 'cypress-integration/integration/public/*.spec.js',
     supportFile: 'cypress-integration/support/index.js',
   },
   fixturesFolder: 'cypress-integration/fixtures',
@@ -25,7 +33,6 @@ module.exports = defineConfig({
     toConsole: true,
   },
   requestTimeout: 12000,
-  retries: 3,
   screenshotsFolder: 'cypress-integration/screenshots',
   video: false,
   videosFolder: 'cypress-integration/videos',
