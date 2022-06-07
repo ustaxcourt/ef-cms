670725149e94521c4a222c2856689be56863305d
Rosie Highsmith <ahighsmith@flexion.us>
2022-06-07 15:36:15 -0700

    Finishing up updating cypress configs

---
+++ cypress-smoketests-readonly.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  browser: 'edge',
+  defaultCommandTimeout: 20000,
+  e2e: {
+    specPattern: 'cypress-readonly/integration/*.spec.js',
+    supportFile: 'cypress-readonly/support/index.js',
+  },
+  fixturesFolder: 'cypress-readonly/fixtures',
+  reporter: 'spec',
+  reporterOptions: {
+    toConsole: true,
+  },
+  requestTimeout: 20000,
+  screenshotsFolder: 'cypress-readonly/screenshots',
+  video: true,
+  videoCompression: 10,
+  videoUploadOnPasses: false,
+  videosFolder: 'cypress-readonly/videos',
+  viewportHeight: 900,
+  viewportWidth: 1200,
+});
