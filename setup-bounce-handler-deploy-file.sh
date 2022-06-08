a5205fbf240e8953fe37eb611dbb1341a79ed257
Michael Marcotte <mike.marcotte@gmail.com>
2022-06-08 14:39:36 -0400

    ustc-2142: get the color dynamically

--- setup-bounce-handler-deploy-file.sh
+++ setup-bounce-handler-deploy-file.sh
@@ -14,4 +14,7 @@
 
 BUCKET_NAME="${EFCMS_DOMAIN}.efcms.${ENV}.us-east-1.lambdas"
 
-aws s3 cp "s3://${BUCKET_NAME}/bounce_handler_blue.js.zip" "s3://${BUCKET_NAME}/bounce_handler_green.js.zip"
+DEPLOYING_COLOR=$(./scripts/dynamo/get-deploying-color.sh "${ENV}")
+CURRENT_COLOR=$(./scripts/dynamo/get-current-color.sh "${ENV}")
+
+aws s3 cp "s3://${BUCKET_NAME}/bounce_handler_${DEPLOYING_COLOR}.js.zip" "s3://${BUCKET_NAME}/bounce_handler_${CURRENT_COLOR}.js.zip"
