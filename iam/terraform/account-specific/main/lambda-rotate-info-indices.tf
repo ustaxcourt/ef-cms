6540964851d82fad3a2e11b7bd3fa50e1f869fd8
Michael Marcotte <mike.marcotte@gmail.com>
2022-06-08 16:17:01 -0400

    ustc-1810: add cron for the log rotator

--- iam/terraform/account-specific/main/lambda-rotate-info-indices.tf
+++ iam/terraform/account-specific/main/lambda-rotate-info-indices.tf
@@ -24,3 +24,23 @@ resource "aws_cloudwatch_log_group" "rotate_info_indices" {
   name              = "/aws/lambda/${aws_lambda_function.rotate_info_indices.function_name}"
   retention_in_days = 14
 }
+
+resource "aws_cloudwatch_event_rule" "every_day" {
+  name                = "daily-job"
+  description         = "Fires every day"
+  schedule_expression = "rate(1 day)"
+}
+
+resource "aws_cloudwatch_event_target" "rotate_info_indices_daily" {
+  rule      = "${aws_cloudwatch_event_rule.every_day.name}"
+  target_id = "lambda"
+  arn       = "${aws_lambda_function.rotate_info_indices.arn}"
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_rotate_info_indices_daily" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.rotate_info_indices.function_name}"
+  principal     = "events.amazonaws.com"
+  source_arn    = "${aws_cloudwatch_event_rule.every_day.arn}"
+}
