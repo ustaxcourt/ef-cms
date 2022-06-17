f253f65a197bdd22bd9aff6dbec4f9343e7dda05
Cody Seibert <cseibert113@gmail.com>
2022-06-17 10:14:44 -0400

    9501 - adding coverage over action

--- web-client/src/presenter/actions/CourtIssuedDocketEntry/submitCourtIssuedDocketEntryToConsolidatedGroupAction.js
+++ web-client/src/presenter/actions/CourtIssuedDocketEntry/submitCourtIssuedDocketEntryToConsolidatedGroupAction.js
@@ -1,48 +1,9 @@
+import { filter, flow, map } from 'lodash/fp';
 import { state } from 'cerebral';
-
-export const submitCourtIssuedDocketEntryActionHelper = async ({
-  applicationContext,
-  get,
-  getDocketNumbers,
-  subjectDocketNumber,
-}) => {
-  const docketEntryId = get(state.docketEntryId);
-  const form = get(state.form);
-  const docketNumbers = getDocketNumbers();
-
-  const { COURT_ISSUED_EVENT_CODES_REQUIRING_COVERSHEET } =
-    applicationContext.getConstants();
-
-  const documentMeta = {
-    ...form,
-    docketEntryId,
-    docketNumbers,
-    subjectDocketNumber,
-  };
-
-  await applicationContext
-    .getUseCases()
-    .fileCourtIssuedDocketEntryInteractor(applicationContext, {
-      docketEntryId,
-      documentMeta,
-    });
-
-  if (
-    COURT_ISSUED_EVENT_CODES_REQUIRING_COVERSHEET.includes(
-      documentMeta.eventCode,
-    )
-  ) {
-    await applicationContext
-      .getUseCases()
-      .addCoversheetInteractor(applicationContext, {
-        docketEntryId,
-        docketNumber: subjectDocketNumber,
-      });
-  }
-};
+import { submitCourtIssuedDocketEntryActionHelper } from './submitCourtIssuedDocketEntryActionHelper';
 
 /**
- * creates a docket entry with the given court-issued document
+ * saves the court issued docket entry on all of the checked consolidated cases of the group.
  *
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
@@ -57,10 +18,10 @@ export const submitCourtIssuedDocketEntryToConsolidatedGroupAction = async ({
     applicationContext,
     get,
     getDocketNumbers: () => {
-      const consolidatedCases = get(state.caseDetail.consolidatedCases) || [];
-      return consolidatedCases
-        .filter(consolidatedCase => consolidatedCase.checked)
-        .map(consolidatedCase => consolidatedCase.docketNumber);
+      return flow(
+        filter('checked'),
+        map('docketNumber'),
+      )(get(state.caseDetail.consolidatedCases));
     },
     subjectDocketNumber: get(state.caseDetail.docketNumber),
   });
