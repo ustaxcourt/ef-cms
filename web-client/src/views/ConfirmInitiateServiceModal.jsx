ec1b0fc6ab4ffc184fed691913dd126cb4b69bfb
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2022-06-20 12:53:11 -0500

    3569: Add new verbiage

--- web-client/src/views/ConfirmInitiateServiceModal.jsx
+++ web-client/src/views/ConfirmInitiateServiceModal.jsx
@@ -57,7 +57,8 @@ export const ConfirmInitiateServiceModal = connect(
         title="Are You Ready to Initiate Service?"
       >
         <p className="margin-bottom-1">
-          The following document will be served on all parties:
+          The following document will be served on all parties in selected
+          cases:
         </p>
         <p className="margin-top-0 margin-bottom-2">
           <strong>{documentTitle}</strong>
