619f459a1bfd90ff3ad2b2f8d0c40e3ad7fc3bec
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-06-21 10:40:42 -0700

    Devex 1051: WIP destroy terraform resources associated with ES index to be deleted [skip ci]

--- scripts/elasticsearch/delete-elasticsearch-cluster.sh
+++ scripts/elasticsearch/delete-elasticsearch-cluster.sh
@@ -1,23 +1,110 @@
-#!/bin/bash
+#!/bin/bash -e
+ENV=$1
 
-# Deletes the specified elasticsearch cluster for the environment
-
-# Usage
-#   ./delete-elasticsearch-cluster.sh $SOURCE_ELASTICSEARCH
-
-# Arguments
-#   - $1 - the cluster to delete
-
-( ! command -v jq > /dev/null ) && echo "jq must be installed on your machine." && exit 1
-[ -z "$1" ] && echo "The domain to delete must be provided as the \$1 argument." && exit 1
-
-SOURCE_ELASTICSEARCH=$1
-
-WAS_CLUSTER_DELETED=$(aws es delete-elasticsearch-domain --domain "${SOURCE_ELASTICSEARCH}" --region us-east-1 | jq -r ".DomainStatus.Deleted")
-
-if [[ $WAS_CLUSTER_DELETED == false ]] ; then
-  echo "The cluster ${SOURCE_ELASTICSEARCH} failed to delete."
-  exit 1
+# Getting the environment-specific deployment settings and injecting them into the shell environment
+if [ -z "${SECRETS_LOADED}" ]; then
+  pushd ../../../
+  # shellcheck disable=SC1091
+  . ./scripts/load-environment-from-secrets.sh
+  popd
 fi
 
-echo "The cluster ${SOURCE_ELASTICSEARCH} was either successfully deleted or didn't exist prior to deletion attempt."
+[ -z "${COGNITO_SUFFIX}" ] && echo "You must have COGNITO_SUFFIX set in your environment" && exit 1
+[ -z "${DEPLOYING_COLOR}" ] && echo "You must have DEPLOYING_COLOR set in your environment" && exit 1
+[ -z "${DISABLE_EMAILS}" ] && echo "You must have DISABLE_EMAILS set in your environment" && exit 1
+[ -z "${EFCMS_DOMAIN}" ] && echo "You must have EFCMS_DOMAIN set in your environment" && exit 1
+[ -z "${EMAIL_DMARC_POLICY}" ] && echo "You must have EMAIL_DMARC_POLICY set in your environment" && exit 1
+[ -z "${ENV}" ] && echo "You must have ENV set in your environment" && exit 1
+[ -z "${ES_INSTANCE_TYPE}" ] && echo "You must have ES_INSTANCE_TYPE set in your environment" && exit 1
+[ -z "${ES_VOLUME_SIZE}" ] && echo "You must have ES_VOLUME_SIZE set in your environment" && exit 1
+[ -z "${IRS_SUPERUSER_EMAIL}" ] && echo "You must have IRS_SUPERUSER_EMAIL set in your environment" && exit 1
+[ -z "${MIGRATE_FLAG}" ] && echo "You must have MIGRATE_FLAG set in your environment" && exit 1
+[ -z "${ZONE_NAME}" ] && echo "You must have ZONE_NAME set in your environment" && exit 1
+
+echo "Running terraform with the following environment configs:"
+echo "  - BOUNCED_EMAIL_RECIPIENT=${BOUNCED_EMAIL_RECIPIENT}"
+echo "  - BOUNCE_ALERT_RECIPIENTS=${BOUNCE_ALERT_RECIPIENTS}"
+echo "  - SLACK_WEBHOOK_URL=${SLACK_WEBHOOK_URL}"
+echo "  - CIRCLE_BRANCH=${CIRCLE_BRANCH}"
+echo "  - COGNITO_SUFFIX=${COGNITO_SUFFIX}"
+echo "  - DEPLOYING_COLOR=${DEPLOYING_COLOR}"
+echo "  - DISABLE_EMAILS=${DISABLE_EMAILS}"
+echo "  - EFCMS_DOMAIN=${EFCMS_DOMAIN}"
+echo "  - EMAIL_DMARC_POLICY=${EMAIL_DMARC_POLICY}"
+echo "  - ENV=${ENV}"
+echo "  - ES_INSTANCE_TYPE=${ES_INSTANCE_TYPE}"
+echo "  - ES_VOLUME_SIZE=${ES_VOLUME_SIZE}"
+echo "  - IRS_SUPERUSER_EMAIL=${IRS_SUPERUSER_EMAIL}"
+echo "  - LOWER_ENV_ACCOUNT_ID=${LOWER_ENV_ACCOUNT_ID}"
+echo "  - MIGRATE_FLAG=${MIGRATE_FLAG}"
+echo "  - PROD_ENV_ACCOUNT_ID=${PROD_ENV_ACCOUNT_ID}"
+echo "  - ZONE_NAME=${ZONE_NAME}"
+
+../../../scripts/verify-terraform-version.sh
+
+BUCKET="${ZONE_NAME}.terraform.deploys"
+KEY="documents-${ENV}.tfstate"
+LOCK_TABLE=efcms-terraform-lock
+
+if [[ "${SOURCE_ELASTICSEARCH}" == *'alpha'* ]]; then
+  DOMAIN_TO_DELETE='alpha'
+
+  SHOULD_ES_ALPHA_EXIST=false
+  SHOULD_ES_BETA_EXIST=true
+else
+  DOMAIN_TO_DELETE='beta'
+
+  SHOULD_ES_ALPHA_EXIST=true
+  SHOULD_ES_BETA_EXIST=false
+fi
+
+if [ "${DEPLOYING_COLOR}" == 'blue' ]; then
+  GREEN_TABLE_NAME=$(../../../scripts/dynamo/get-destination-table.sh "${ENV}")
+  BLUE_TABLE_NAME=$(../../../scripts/dynamo/get-source-table.sh "${ENV}")
+  GREEN_ELASTICSEARCH_DOMAIN=$(../../../scripts/elasticsearch/get-destination-elasticsearch.sh "${ENV}")
+  BLUE_ELASTICSEARCH_DOMAIN=$(../../../scripts/elasticsearch/get-source-elasticsearch.sh "${ENV}")
+  COGNITO_TRIGGER_TABLE_NAME=$(../../../scripts/dynamo/get-source-table.sh "${ENV}")
+else
+  BLUE_TABLE_NAME=$(../../../scripts/dynamo/get-destination-table.sh "${ENV}")
+  GREEN_TABLE_NAME=$(../../../scripts/dynamo/get-source-table.sh "${ENV}")
+  BLUE_ELASTICSEARCH_DOMAIN=$(../../../scripts/elasticsearch/get-destination-elasticsearch.sh "${ENV}")
+  GREEN_ELASTICSEARCH_DOMAIN=$(../../../scripts/elasticsearch/get-source-elasticsearch.sh "${ENV}")
+  COGNITO_TRIGGER_TABLE_NAME=$(../../../scripts/dynamo/get-source-table.sh "${ENV}")
+fi
+
+if [[ -z "${DYNAMSOFT_URL_OVERRIDE}" ]]; then
+  SCANNER_RESOURCE_URI="https://dynamsoft-lib.${EFCMS_DOMAIN}/dynamic-web-twain-sdk-14.3.1"
+else
+  SCANNER_RESOURCE_URI="${DYNAMSOFT_URL_OVERRIDE}/dynamic-web-twain-sdk-14.3.1"
+fi
+
+export TF_VAR_blue_elasticsearch_domain=$BLUE_ELASTICSEARCH_DOMAIN
+export TF_VAR_blue_table_name=$BLUE_TABLE_NAME
+export TF_VAR_bounce_alert_recipients=$BOUNCE_ALERT_RECIPIENTS
+export TF_VAR_bounced_email_recipient=$BOUNCED_EMAIL_RECIPIENT
+export TF_VAR_cognito_suffix=$COGNITO_SUFFIX
+export TF_VAR_cognito_table_name=$COGNITO_TRIGGER_TABLE_NAME
+export TF_VAR_deploying_color=$DEPLOYING_COLOR
+export TF_VAR_destination_table=$DESTINATION_TABLE
+export TF_VAR_disable_emails=$DISABLE_EMAILS
+export TF_VAR_dns_domain=$EFCMS_DOMAIN
+export TF_VAR_email_dmarc_policy=$EMAIL_DMARC_POLICY
+export TF_VAR_environment=$ENV
+export TF_VAR_es_instance_count=$ES_INSTANCE_COUNT
+export TF_VAR_es_instance_type=$ES_INSTANCE_TYPE
+export TF_VAR_es_volume_size=$ES_VOLUME_SIZE
+export TF_VAR_green_elasticsearch_domain=$GREEN_ELASTICSEARCH_DOMAIN
+export TF_VAR_green_table_name=$GREEN_TABLE_NAME
+export TF_VAR_irs_superuser_email=$IRS_SUPERUSER_EMAIL
+export TF_VAR_lower_env_account_id=$LOWER_ENV_ACCOUNT_ID
+export TF_VAR_prod_env_account_id=$PROD_ENV_ACCOUNT_ID
+export TF_VAR_scanner_resource_uri=$SCANNER_RESOURCE_URI
+export TF_VAR_should_es_alpha_exist=$SHOULD_ES_ALPHA_EXIST
+export TF_VAR_should_es_beta_exist=$SHOULD_ES_BETA_EXIST
+export TF_VAR_slack_webhook_url=$SLACK_WEBHOOK_URL
+export TF_VAR_zone_name=$ZONE_NAME
+
+terraform destroy -target 'module.ef-cms_apis.module.elasticsearch_alpha[0].module.logs_alarms'
+terraform destroy -target 'module.ef-cms_apis.module.elasticsearch_alpha[0].aws_cloudwatch_log_resource_policy.allow_elasticsearch_to_write_logs'
+terraform destroy -target 'module.ef-cms_apis.module.elasticsearch_alpha[0].module.logs_alarms.data.aws_caller_identity.default'
+terraform destroy -target 'module.ef-cms_apis.module.elasticsearch_alpha[0].aws_cloudwatch_log_resource_policy.allow_elasticsearch_to_write_logs'
