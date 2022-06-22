e6ec07ad2121f7bcc07a2fd95ab94dbae0aff45f
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2022-06-22 13:19:34 -0600

    9512: wip integration test [skip ci]

--- web-client/integration-tests/chambersViewsWorkingCopyTrialSession.test.js
+++ web-client/integration-tests/chambersViewsWorkingCopyTrialSession.test.js
@@ -1,7 +1,25 @@
+import {
+  OBJECTIONS_OPTIONS_MAP,
+  PARTIES_CODES,
+} from '../../shared/src/business/entities/EntityConstants';
 import { chambersViewsTrialSessionWorkingCopy } from './journey/chambersViewsTrialSessionWorkingCopy';
-import { loginAs, setupTest } from './helpers';
+import {
+  contactPrimaryFromState,
+  fakeFile,
+  loginAs,
+  setupTest,
+  uploadPetition,
+} from './helpers';
+import { docketClerkCreatesARemoteTrialSession } from './journey/docketClerkCreatesARemoteTrialSession';
+import { manuallyAddCaseToTrial } from './utils/manuallyAddCaseToTrial';
+import { runCompute } from 'cerebral/test';
+import { trialSessionWorkingCopyHelper as trialSessionWorkingCopyHelperComputed } from '../src/presenter/computeds/trialSessionWorkingCopyHelper';
+import { withAppContextDecorator } from '../src/withAppContext';
 
 const cerebralTest = setupTest();
+const trialSessionWorkingCopyHelper = withAppContextDecorator(
+  trialSessionWorkingCopyHelperComputed,
+);
 
 describe('Chambers dashboard', () => {
   beforeAll(() => {
@@ -15,4 +33,266 @@ describe('Chambers dashboard', () => {
 
   loginAs(cerebralTest, 'colvinsChambers@example.com');
   chambersViewsTrialSessionWorkingCopy(cerebralTest);
+
+  for (let i = 1; i <= 4; i++) {
+    loginAs(cerebralTest, 'petitioner@example.com');
+    it(`Creates case ${i} and adds to trial session`, async () => {
+      const caseDetail = await uploadPetition(cerebralTest);
+      expect(caseDetail.docketNumber).toBeDefined();
+      cerebralTest[`docketNumber${i}`] = caseDetail.docketNumber;
+    });
+  }
+
+  loginAs(cerebralTest, 'docketclerk@example.com');
+  docketClerkCreatesARemoteTrialSession(cerebralTest);
+
+  for (let i = 1; i <= 4; i++) {
+    manuallyAddCaseToTrial(cerebralTest, { index: i });
+  }
+  it('docket clerk adds pretrial memorandum to case 1 filed by petitioner', async () => {
+    await cerebralTest.runSequence('gotoCaseDetailSequence', {
+      docketNumber: cerebralTest.docketNumber1,
+    });
+
+    await cerebralTest.runSequence('gotoAddPaperFilingSequence', {
+      docketNumber: cerebralTest.docketNumber1,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'dateReceivedMonth',
+      value: 4,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'dateReceivedDay',
+      value: 30,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'dateReceivedYear',
+      value: 2001,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'primaryDocumentFile',
+      value: fakeFile,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'primaryDocumentFileSize',
+      value: 100,
+    });
+
+    const contactPrimary = contactPrimaryFromState(cerebralTest);
+
+    await cerebralTest.runSequence(
+      'updateFileDocumentWizardFormValueSequence',
+      {
+        key: `filersMap.${contactPrimary.contactId}`,
+        value: true,
+      },
+    );
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'eventCode',
+      value: 'PMT',
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'objections',
+      value: OBJECTIONS_OPTIONS_MAP.NO,
+    });
+
+    await cerebralTest.runSequence('submitPaperFilingSequence');
+
+    expect(cerebralTest.getState('validationErrors')).toEqual({});
+
+    expect(cerebralTest.getState('alertSuccess').message).toEqual(
+      'Your entry has been added to docket record.',
+    );
+
+    expect(cerebralTest.getState('currentPage')).toEqual('CaseDetailInternal');
+    expect(cerebralTest.getState('form')).toEqual({});
+
+    cerebralTest.docketEntryId = cerebralTest
+      .getState('caseDetail.docketEntries')
+      .find(doc => doc.eventCode === 'PMT').docketEntryId;
+  });
+
+  it('docket clerk adds pretrial memorandum to case 2 filed by respondent', async () => {
+    await cerebralTest.runSequence('gotoCaseDetailSequence', {
+      docketNumber: cerebralTest.docketNumber2,
+    });
+
+    await cerebralTest.runSequence('gotoAddPaperFilingSequence', {
+      docketNumber: cerebralTest.docketNumber2,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'dateReceivedMonth',
+      value: 4,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'dateReceivedDay',
+      value: 30,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'dateReceivedYear',
+      value: 2001,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'primaryDocumentFile',
+      value: fakeFile,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'primaryDocumentFileSize',
+      value: 100,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'partyIrsPractitioner',
+      value: true,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'eventCode',
+      value: 'PMT',
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'objections',
+      value: OBJECTIONS_OPTIONS_MAP.NO,
+    });
+
+    await cerebralTest.runSequence('submitPaperFilingSequence');
+
+    expect(cerebralTest.getState('validationErrors')).toEqual({});
+
+    expect(cerebralTest.getState('alertSuccess').message).toEqual(
+      'Your entry has been added to docket record.',
+    );
+
+    expect(cerebralTest.getState('currentPage')).toEqual('CaseDetailInternal');
+    expect(cerebralTest.getState('form')).toEqual({});
+
+    cerebralTest.docketEntryId = cerebralTest
+      .getState('caseDetail.docketEntries')
+      .find(doc => doc.eventCode === 'PMT').docketEntryId;
+  });
+
+  it('docket clerk adds pretrial memorandum to case 3 filed by petitioner and respondent', async () => {
+    await cerebralTest.runSequence('gotoCaseDetailSequence', {
+      docketNumber: cerebralTest.docketNumber3,
+    });
+
+    await cerebralTest.runSequence('gotoAddPaperFilingSequence', {
+      docketNumber: cerebralTest.docketNumber3,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'dateReceivedMonth',
+      value: 4,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'dateReceivedDay',
+      value: 30,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'dateReceivedYear',
+      value: 2001,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'primaryDocumentFile',
+      value: fakeFile,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'primaryDocumentFileSize',
+      value: 100,
+    });
+
+    const contactPrimary = contactPrimaryFromState(cerebralTest);
+
+    await cerebralTest.runSequence(
+      'updateFileDocumentWizardFormValueSequence',
+      {
+        key: `filersMap.${contactPrimary.contactId}`,
+        value: true,
+      },
+    );
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'partyIrsPractitioner',
+      value: true,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'eventCode',
+      value: 'PMT',
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'objections',
+      value: OBJECTIONS_OPTIONS_MAP.NO,
+    });
+
+    await cerebralTest.runSequence('submitPaperFilingSequence');
+
+    expect(cerebralTest.getState('validationErrors')).toEqual({});
+
+    expect(cerebralTest.getState('alertSuccess').message).toEqual(
+      'Your entry has been added to docket record.',
+    );
+
+    expect(cerebralTest.getState('currentPage')).toEqual('CaseDetailInternal');
+    expect(cerebralTest.getState('form')).toEqual({});
+
+    cerebralTest.docketEntryId = cerebralTest
+      .getState('caseDetail.docketEntries')
+      .find(doc => doc.eventCode === 'PMT').docketEntryId;
+  });
+
+  loginAs(cerebralTest, 'cohensChambers@example.com');
+  it('chambers user verifies PTM column and value for case 1', async () => {
+    await cerebralTest.runSequence('gotoTrialSessionWorkingCopySequence', {
+      trialSessionId: cerebralTest.trialSessionId,
+    });
+
+    expect(cerebralTest.getState('currentPage')).toEqual(
+      'TrialSessionWorkingCopy',
+    );
+
+    let workingCopyHelper = runCompute(trialSessionWorkingCopyHelper, {
+      state: cerebralTest.getState(),
+    });
+
+    const caseWithPtmFiledByPetitioner = workingCopyHelper.formattedCases.find(
+      c => c.docketNumber === cerebralTest.docketNumber1,
+    );
+    const caseWithPtmFiledByRespondent = workingCopyHelper.formattedCases.find(
+      c => c.docketNumber === cerebralTest.docketNumber1,
+    );
+    const caseWithPtmFiledByBoth = workingCopyHelper.formattedCases.find(
+      c => c.docketNumber === cerebralTest.docketNumber1,
+    );
+    const caseWithoutPtm = workingCopyHelper.formattedCases.find(
+      c => c.docketNumber === cerebralTest.docketNumber1,
+    );
+
+    expect(caseWithPtmFiledByPetitioner.filingPartiesCode).toBe(
+      PARTIES_CODES.PETITIONER,
+    );
+    expect(caseWithPtmFiledByRespondent.filingPartiesCode).toBe(
+      PARTIES_CODES.RESPONDENT,
+    );
+    expect(caseWithPtmFiledByBoth.filingPartiesCode).toBe(PARTIES_CODES.BOTH);
+    expect(caseWithoutPtm.filingPartiesCode).toBeUndefined();
+  });
 });
