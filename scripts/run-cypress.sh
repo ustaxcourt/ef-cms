8d54d8826c1dba71515aab89fc184e89d8a8a113
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2022-06-27 10:52:40 -0500

    950: Use CYPRESS_ prepended environment variables

--- scripts/run-cypress.sh
+++ scripts/run-cypress.sh
@@ -51,7 +51,7 @@ while getopts ":chloprs" option; do
          Help
          exit;;
       l) # local
-         export SMOKETESTS_LOCAL=true
+         export CYPRESS_SMOKETESTS_LOCAL=true
          ;;
       o) # open cypress
          OPEN=true
@@ -86,16 +86,18 @@ echo "${CONFIG_FILE}"
 
 if [ -n "${INTEGRATION}" ]; then
   echo "Running integration tests."
-  export TEMP_DOCUMENTS_BUCKET_NAME=noop-temp-documents-local-us-east-1
-  export QUARANTINE_BUCKET_NAME=noop-quarantine-local-us-east-1
-  export DOCUMENTS_BUCKET_NAME=noop-documents-local-us-east-1
-  export S3_ENDPOINT=http://localhost:9000
-  export MASTER_DYNAMODB_ENDPOINT=http://localhost:8000
-  export SLS_DEPLOYMENT_BUCKET=noop
-  export AWS_ACCESS_KEY_ID=S3RVER
-  export AWS_SECRET_ACCESS_KEY=S3RVER
-  export SKIP_CACHE_INVALIDATION=true
-  export CHECK_DEPLOY_DATE_INTERVAL=5000
+  export CYPRESS_TEMP_DOCUMENTS_BUCKET_NAME=noop-temp-documents-local-us-east-1
+  export CYPRESS_QUARANTINE_BUCKET_NAME=noop-quarantine-local-us-east-1
+  export CYPRESS_DOCUMENTS_BUCKET_NAME=noop-documents-local-us-east-1
+  export CYPRESS_S3_ENDPOINT=http://localhost:9000
+  export CYPRESS_MASTER_DYNAMODB_ENDPOINT=http://localhost:8000
+  export CYPRESS_SLS_DEPLOYMENT_BUCKET=noop
+  export CYPRESS_AWS_ACCESS_KEY_ID=S3RVER
+  export CYPRESS_AWS_SECRET_ACCESS_KEY=S3RVER
+  export CYPRESS_SKIP_CACHE_INVALIDATION=true
+  export CYPRESS_CHECK_DEPLOY_DATE_INTERVAL=5000
+elif [ -n "${CYPRESS_SMOKETESTS_LOCAL}" ]; then
+  export CYPRESS_BASE_URL="http://localhost:${PORT}"
 else
   if [ -z "${ENV}" ]; then
     echo "Please export the environment name as a variable named ENV."
@@ -103,15 +105,20 @@ else
   fi
 
   . ./scripts/setup-cypress-variables.sh
-  if [ -n "${SMOKETESTS_LOCAL}" ]; then
-    export CYPRESS_BASE_URL="http://localhost:${PORT}"
-  else
-    export CYPRESS_BASE_URL="https://app-${DEPLOYING_COLOR}.${EFCMS_DOMAIN}"
-  fi
+  export CYPRESS_AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+  export CYPRESS_AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+  export CYPRESS_AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN
+  export CYPRESS_DEFAULT_ACCOUNT_PASS=$DEFAULT_ACCOUNT_PASS
+  export CYPRESS_DEPLOYING_COLOR=$DEPLOYING_COLOR
+  export CYPRESS_DISABLE_EMAILS=$DISABLE_EMAILS
+  export CYPRESS_EFCMS_DOMAIN=$EFCMS_DOMAIN
+  export CYPRESS_USTC_ADMIN_PASS=$USTC_ADMIN_PASS
+  export CYPRESS_BASE_URL="https://app-${DEPLOYING_COLOR}.${EFCMS_DOMAIN}"
 fi
 
 if [ -n "${OPEN}" ]; then
-  cypress open -C "${CONFIG_FILE}" --env "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY,AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN,DEFAULT_ACCOUNT_PASS=$DEFAULT_ACCOUNT_PASS,DEPLOYING_COLOR=$DEPLOYING_COLOR,DISABLE_EMAILS=$DISABLE_EMAILS,EFCMS_DOMAIN=$EFCMS_DOMAIN,ENV=$ENV,USTC_ADMIN_PASS=$USTC_ADMIN_PASS"
+  echo $CYPRESS_ENV
+  cypress open -C "${CONFIG_FILE}" --env ENV="$ENV"
 else
-  cypress run -C "${CONFIG_FILE}" --env "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY,AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN,DEFAULT_ACCOUNT_PASS=$DEFAULT_ACCOUNT_PASS,DEPLOYING_COLOR=$DEPLOYING_COLOR,DISABLE_EMAILS=$DISABLE_EMAILS,EFCMS_DOMAIN=$EFCMS_DOMAIN,ENV=$ENV,USTC_ADMIN_PASS=$USTC_ADMIN_PASS"
+  cypress run -C "${CONFIG_FILE}" --env ENV="$ENV"
 fi
