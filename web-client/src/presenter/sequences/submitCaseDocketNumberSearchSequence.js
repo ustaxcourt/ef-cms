123b8b96ef3244ec600146fe1d7c948e26ab648b
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2022-06-27 15:07:34 -0600

    9523: removing test, updating advanced case search sequence

--- web-client/src/presenter/sequences/submitCaseDocketNumberSearchSequence.js
+++ web-client/src/presenter/sequences/submitCaseDocketNumberSearchSequence.js
@@ -1,12 +1,17 @@
 import { caseExistsAction } from '../actions/caseExistsAction';
 import { clearSearchResultsAction } from '../actions/AdvancedSearch/clearSearchResultsAction';
 import { clearSearchTermAction } from '../actions/clearSearchTermAction';
+import { getCaseAction } from '../actions/getCaseAction';
+import { getCaseAssociationAction } from '../actions/getCaseAssociationAction';
 import { navigateToCaseDetailAction } from '../actions/navigateToCaseDetailAction';
 import { setAlertErrorAction } from '../actions/setAlertErrorAction';
+import { setCaseAction } from '../actions/setCaseAction';
+import { setCurrentPageAction } from '../actions/setCurrentPageAction';
 import { setDocketNumberFromAdvancedSearchAction } from '../actions/AdvancedSearch/setDocketNumberFromAdvancedSearchAction';
 import { setNoMatchesCaseSearchAction } from '../actions/AdvancedSearch/setNoMatchesCaseSearchAction';
 import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
 import { showProgressSequenceDecorator } from '../utilities/showProgressSequenceDecorator';
+import { showSealedToPublicCaseAction } from '../actions/showSealedToPublicCaseAction';
 import { startShowValidationAction } from '../actions/startShowValidationAction';
 import { stopShowValidationAction } from '../actions/stopShowValidationAction';
 import { validateCaseDocketNumberSearchAction } from '../actions/AdvancedSearch/validateCaseDocketNumberSearchAction';
@@ -27,7 +32,19 @@ export const submitCaseDocketNumberSearchSequence = [
       caseExistsAction,
       {
         error: [setNoMatchesCaseSearchAction],
-        success: [navigateToCaseDetailAction],
+        success: [
+          getCaseAction,
+          setCaseAction,
+          getCaseAssociationAction,
+          showSealedToPublicCaseAction,
+          {
+            no: [navigateToCaseDetailAction],
+            yes: [
+              setCurrentPageAction('SealedCaseDetail'),
+              clearSearchTermAction,
+            ],
+          },
+        ],
       },
     ]),
   },
