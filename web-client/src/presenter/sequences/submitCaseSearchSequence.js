f2bb5879d305aa14c7bb655d3ec67d13be0291b1
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2022-06-29 13:29:58 -0600

    9523: more cleanup

--- web-client/src/presenter/sequences/submitCaseSearchSequence.js
+++ web-client/src/presenter/sequences/submitCaseSearchSequence.js
@@ -1,10 +1,7 @@
 import { caseExistsAction } from '../actions/caseExistsAction';
 import { clearSearchTermAction } from '../actions/clearSearchTermAction';
-import { getCaseAction } from '../actions/getCaseAction';
-import { getCaseAssociationAction } from '../actions/getCaseAssociationAction';
 import { navigateToCaseDetailAction } from '../actions/navigateToCaseDetailAction';
 import { navigateToCaseSearchNoMatchesAction } from '../actions/navigateToCaseSearchNoMatchesAction';
-import { setCaseAction } from '../actions/setCaseAction';
 import { setDocketNumberFromSearchAction } from '../actions/setDocketNumberFromSearchAction';
 import { showProgressSequenceDecorator } from '../utilities/showProgressSequenceDecorator';
 
@@ -13,12 +10,6 @@ export const submitCaseSearchSequence = showProgressSequenceDecorator([
   caseExistsAction,
   {
     error: [navigateToCaseSearchNoMatchesAction],
-    success: [
-      getCaseAction,
-      setCaseAction,
-      getCaseAssociationAction,
-      navigateToCaseDetailAction,
-      clearSearchTermAction,
-    ],
+    success: [navigateToCaseDetailAction, clearSearchTermAction],
   },
 ]);
