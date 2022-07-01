131fb199031bf225d26ec8ceff3d774eb746924e
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2022-07-01 17:16:51 -0500

    9479: Create new action and sequence for stamping motions [skip ci]

---
+++ web-client/src/presenter/sequences/saveMotionStampingSequence.js
@@ -0,0 +1,39 @@
+import { clearAlertsAction } from '../actions/clearAlertsAction';
+import { clearFormAction } from '../actions/clearFormAction';
+import { clearPDFSignatureDataAction } from '../actions/clearPDFSignatureDataAction';
+import { completeMotionStampingAction } from '../actions/completeMotionStampingAction';
+import { completeWorkItemForDocumentSigningAction } from '../actions/completeWorkItemForDocumentSigningAction';
+import { followRedirectAction } from '../actions/followRedirectAction';
+import { navigateToDraftDocumentsAction } from '../actions/navigateToDraftDocumentsAction';
+import { setAlertSuccessAction } from '../actions/setAlertSuccessAction';
+import { setCaseDetailPageTabActionGenerator } from '../actions/setCaseDetailPageTabActionGenerator';
+import { setDefaultDraftDocumentIdAction } from '../actions/setDefaultDraftDocumentIdAction';
+import { setDocumentDetailTabAction } from '../actions/setDocumentDetailTabAction';
+import { setRedirectUrlAction } from '../actions/setRedirectUrlAction';
+import { setSaveAlertsForNavigationAction } from '../actions/setSaveAlertsForNavigationAction';
+import { setSuccessFromDocumentTitleAction } from '../actions/setSuccessFromDocumentTitleAction';
+import { showProgressSequenceDecorator } from '../utilities/showProgressSequenceDecorator';
+
+export const saveDocumentSigningSequence = showProgressSequenceDecorator([
+  clearAlertsAction,
+  setSaveAlertsForNavigationAction,
+  setSuccessFromDocumentTitleAction,
+  completeMotionStampingAction,
+  setDefaultDraftDocumentIdAction,
+  setRedirectUrlAction,
+  // TODO: replace completeWorkItemForDocumentSigningAction
+  completeWorkItemForDocumentSigningAction,
+  setDocumentDetailTabAction,
+  // TODO: replace clearPDFSignatureDataAction
+  clearPDFSignatureDataAction,
+  clearFormAction,
+  setAlertSuccessAction,
+  followRedirectAction,
+  {
+    default: [
+      setCaseDetailPageTabActionGenerator('drafts'),
+      navigateToDraftDocumentsAction,
+    ],
+    success: [],
+  },
+]);
