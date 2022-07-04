7f5aca5f925451c01422f80ec71dd108bc0ad0a8
Rosie Highsmith <ahighsmith@flexion.us>
2022-07-04 08:02:59 -0700

    9479: unset denied options action and test [skip ci]

---
+++ web-client/src/presenter/actions/CaseDetail/unsetDeniedOptionsOnStampFormAction.test.js
@@ -0,0 +1,25 @@
+import { applicationContextForClient as applicationContext } from '../../../../../shared/src/business/test/createTestApplicationContext';
+import { presenter } from '../../presenter-mock';
+import { runAction } from 'cerebral/test';
+import { unsetDeniedOptionsOnStampFormAction } from './unsetDeniedOptionsOnStampFormAction';
+
+describe('removeCaseFromTrialAction', () => {
+  presenter.providers.applicationContext = applicationContext;
+
+  it('should unset denied options on the form', async () => {
+    const { state } = await runAction(unsetDeniedOptionsOnStampFormAction, {
+      modules: {
+        presenter,
+      },
+      state: {
+        form: {
+          deniedAsMoot: true,
+          deniedWithoutPrejudice: true,
+        },
+      },
+    });
+
+    expect(state.form.deniedAsMoot).toBeUndefined();
+    expect(state.form.deniedWithoutPrejudice).toBeUndefined();
+  });
+});
