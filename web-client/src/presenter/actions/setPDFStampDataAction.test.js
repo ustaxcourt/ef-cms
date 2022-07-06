c3d75682dd12896d7dfa6238cb47be4820886e6e
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-07-06 10:56:29 -0700

    9479: Adding test coverage

--- web-client/src/presenter/actions/setPDFStampDataAction.test.js
+++ web-client/src/presenter/actions/setPDFStampDataAction.test.js
@@ -2,16 +2,16 @@ import { runAction } from 'cerebral/test';
 import { setPDFStampDataAction } from './setPDFStampDataAction';
 
 describe('setPDFStampDataAction', () => {
-  it('should set state.pdfForSigning.stampData', async () => {
-    const newState = {
+  it('should set state.pdfForSigning.stampData from props', async () => {
+    const mockStampData = {
       scale: 1,
       x: 1,
       y: 1,
     };
 
-    const result = await runAction(setPDFStampDataAction, {
+    const { state } = await runAction(setPDFStampDataAction, {
       props: {
-        stampData: { ...newState },
+        stampData: mockStampData,
       },
       state: {
         pdfForSigning: {
@@ -20,6 +20,42 @@ describe('setPDFStampDataAction', () => {
       },
     });
 
-    expect(result.state.pdfForSigning.stampData).toEqual(newState);
+    expect(state.pdfForSigning.stampData).toEqual(mockStampData);
+  });
+
+  it('should set state.pdfForSigning.stampApplied from props', async () => {
+    const mockStampApplied = true;
+
+    const { state } = await runAction(setPDFStampDataAction, {
+      props: {
+        stampApplied: mockStampApplied,
+      },
+      state: {
+        pdfForSigning: {
+          stampApplied: null,
+        },
+      },
+    });
+
+    expect(state.pdfForSigning.stampApplied).toEqual(mockStampApplied);
+  });
+
+  it('should set state.pdfForSigning.isPdfAlreadyStamped from props', async () => {
+    const mockIsPdfAlreadyStamped = true;
+
+    const { state } = await runAction(setPDFStampDataAction, {
+      props: {
+        isPdfAlreadyStamped: mockIsPdfAlreadyStamped,
+      },
+      state: {
+        pdfForSigning: {
+          stamisPdfAlreadyStampedpData: null,
+        },
+      },
+    });
+
+    expect(state.pdfForSigning.isPdfAlreadyStamped).toEqual(
+      mockIsPdfAlreadyStamped,
+    );
   });
 });
