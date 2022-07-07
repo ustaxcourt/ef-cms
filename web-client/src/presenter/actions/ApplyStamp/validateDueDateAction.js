d486a97e3da19cc3263593a8f886b2f6778a4f52
Rosie Highsmith <ahighsmith@flexion.us>
2022-07-07 15:16:25 -0700

    9479: return to using same form fields for both dueDate datepickers [skip ci]

--- web-client/src/presenter/actions/ApplyStamp/validateDueDateAction.js
+++ web-client/src/presenter/actions/ApplyStamp/validateDueDateAction.js
@@ -6,8 +6,13 @@ import { state } from 'cerebral';
  * @param {object} providers the providers object
  * @param {object} providers.store the cerebral store
  */
-export const validateDueDateAction = ({ get, store }) => {
+export const validateDueDateAction = ({ get, path, store }) => {
   const form = get(state.form);
 
   console.log(JSON.stringify(form, null, 2));
+  if (form['dueDateMonth-stipDecision']) {
+    // validate all the stip decision stuff for due date
+  }
+
+  return path.success();
 };
