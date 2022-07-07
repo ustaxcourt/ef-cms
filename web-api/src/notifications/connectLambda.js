8f93b2842413be8a69553cec2f95032c7551c9ac
TElliott <telliott@flexion.us>
2022-07-07 14:41:55 -0500

    3569: return from genericHandler

--- web-api/src/notifications/connectLambda.js
+++ web-api/src/notifications/connectLambda.js
@@ -1,17 +1,14 @@
 const { genericHandler } = require('../genericHandler');
-const { getConnectionIdFromEvent } = require('../middleware/apiGatewayHelper');
 
 /**
  * save the information about a new websocket connection
  *
  * @param {object} event the AWS event object
  */
-exports.connectLambda = event => {
-  const clientConnectionId = getConnectionIdFromEvent(event);
-
+exports.connectLambda = event =>
   genericHandler(
     event,
-    async ({ applicationContext }) => {
+    async ({ applicationContext, clientConnectionId }) => {
       const endpoint = event.requestContext.domainName;
 
       await applicationContext
@@ -30,4 +27,3 @@ exports.connectLambda = event => {
     },
     { bypassMaintenanceCheck: true },
   );
-};
