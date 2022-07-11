b4240b3cc3abfb861d1df95177f5cb34412cb9c0
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2022-07-11 14:03:57 -0600

    9479: adding more tests, fixing valdation message [skip ci]

--- shared/src/business/entities/Stamp.test.js
+++ shared/src/business/entities/Stamp.test.js
@@ -24,6 +24,40 @@ describe('Stamp entity', () => {
       });
     });
 
+    it('should be invalid when date is undefined but dueDateMessage is defined', () => {
+      const stamp = new Stamp({
+        date: undefined,
+        dueDateMessage: 'something',
+        status: 'Granted',
+      });
+
+      expect(stamp.getFormattedValidationErrors()).toMatchObject({
+        date: 'Enter a valid date',
+      });
+    });
+
+    it('should be valid when date and dueDateMessage are defined', () => {
+      const stamp = new Stamp({
+        date: '2222-12-05T00:00:00.000-05:00',
+        dueDateMessage: 'something',
+        status: 'Granted',
+      });
+
+      expect(stamp.getFormattedValidationErrors()).toBeNull();
+    });
+
+    it('should be valid when it is today in the local timezone', () => {
+      const stamp = new Stamp({
+        date: '2019-12-05T00:00:00.000-05:00',
+        dueDateMessage: 'something',
+        status: 'Granted',
+      });
+
+      expect(stamp.getFormattedValidationErrors()).toMatchObject({
+        date: 'Due date cannot be prior to today. Enter a valid date.',
+      });
+    });
+
     //make a test mocking utc time to be midnight
     //what if it is the next day in utc but still today oin local time, will it fail validation bc acc to utc today is now yesterday
   });
