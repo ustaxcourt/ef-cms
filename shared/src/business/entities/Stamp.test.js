1e6e210304f179e803628a41014ecf8b6a51a3b9
Rosie Highsmith <ahighsmith@flexion.us>
2022-07-11 13:19:32 -0700

    9479: adding last validation test on entity [skip ci]

--- shared/src/business/entities/Stamp.test.js
+++ shared/src/business/entities/Stamp.test.js
@@ -1,3 +1,4 @@
+const { formatNow } = require('../utilities/DateHandler');
 const { Stamp } = require('./Stamp');
 
 const { VALIDATION_ERROR_MESSAGES } = Stamp;
@@ -46,19 +47,16 @@ describe('Stamp entity', () => {
       expect(stamp.getFormattedValidationErrors()).toBeNull();
     });
 
-    it('should be valid when it is today in the local timezone', () => {
+    it('should be valid when date is today', () => {
+      const mockToday = formatNow();
+
       const stamp = new Stamp({
-        date: '2019-12-05T00:00:00.000-05:00',
+        date: mockToday,
         dueDateMessage: 'something',
         status: 'Granted',
       });
 
-      expect(stamp.getFormattedValidationErrors()).toMatchObject({
-        date: 'Due date cannot be prior to today. Enter a valid date.',
-      });
+      expect(stamp.getFormattedValidationErrors()).toBeNull();
     });
-
-    //make a test mocking utc time to be midnight
-    //what if it is the next day in utc but still today oin local time, will it fail validation bc acc to utc today is now yesterday
   });
 });
