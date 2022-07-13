04e058e649e389504be7f49278b223d7ca7fed90
Richard Goshen <rick.goshen@gmail.com>
2022-07-13 09:42:26 -0700

    devex-1071-change EmailHeader from enzyme to react-testing-library

--- shared/src/business/utilities/emailGenerator/components/EmailHeader.jsx
+++ shared/src/business/utilities/emailGenerator/components/EmailHeader.jsx
@@ -4,12 +4,19 @@ export const EmailHeader = ({ date }) => {
   return (
     <>
       <div style={{ textAlign: 'center' }}>
-        <h1 style={{ fontSize: '20px', marginBottom: '0' }}>
+        <h1
+          data-testid="header"
+          style={{ fontSize: '20px', marginBottom: '0' }}
+        >
           United States Tax Court
         </h1>
         <div style={{ fontSize: '16px' }}>
-          <div>Washington, DC 20217</div>
-          {date && <div style={{ marginTop: '15px' }}>{date}</div>}
+          <div data-testid="location">Washington, DC 20217</div>
+          {date && (
+            <div data-testid="date" style={{ marginTop: '15px' }}>
+              {date}
+            </div>
+          )}
         </div>
       </div>
     </>
--- shared/src/business/utilities/emailGenerator/components/EmailHeader.test.js
+++ shared/src/business/utilities/emailGenerator/components/EmailHeader.test.js
@@ -1,18 +1,18 @@
 const React = require('react');
 const { EmailHeader } = require('./EmailHeader.jsx');
-const { shallow } = require('enzyme');
+const { render } = require('@testing-library/react');
 
 describe('EmailHeader', () => {
   it('renders a header with US Tax Court info', () => {
-    let wrapper = shallow(<EmailHeader />);
+    const { getByTestId } = render(<EmailHeader />);
 
-    expect(wrapper.text()).toContain('United States Tax Court');
-    expect(wrapper.text()).toContain('Washington, DC 20217');
+    expect(getByTestId('header').textContent).toBe('United States Tax Court');
+    expect(getByTestId('location').textContent).toBe('Washington, DC 20217');
   });
 
   it('renders the date from props if present', () => {
-    let wrapper = shallow(<EmailHeader date={'May 14, 2020'} />);
+    const { getByTestId } = render(<EmailHeader date={'May 14, 2020'} />);
 
-    expect(wrapper.text()).toContain('May 14, 2020');
+    expect(getByTestId('date').textContent).toBe('May 14, 2020');
   });
 });
