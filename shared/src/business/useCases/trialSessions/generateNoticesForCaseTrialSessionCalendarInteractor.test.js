82b163e1822f5a610dfbfa632b05e608b63088bf
Absolutestunna <joshujah007@gmail.com>
2022-07-15 15:07:58 -0400

    9518 - added test for checking the necessity to combine clinic letters to notice of trial pdf generated doc

--- shared/src/business/useCases/trialSessions/generateNoticesForCaseTrialSessionCalendarInteractor.test.js
+++ shared/src/business/useCases/trialSessions/generateNoticesForCaseTrialSessionCalendarInteractor.test.js
@@ -13,11 +13,26 @@ const { MOCK_CASE } = require('../../../test/mockCase');
 const { MOCK_TRIAL_REGULAR } = require('../../../test/mockTrial');
 const { PDFDocument } = require('pdf-lib');
 
+jest.mock('../../utilities/shouldAppendClinicLetter', () => ({
+  shouldAppendClinicLetter: jest.fn().mockResolvedValue({
+    appendClinicLetter: true,
+  }),
+}));
+
+jest.mock('../../utilities/copyPagesFromPdf', () => ({
+  copyPagesFromPdf: jest.fn(),
+}));
+
 describe('generateNoticesForCaseTrialSessionCalendarInteractor', () => {
+  let docketNumber, trialSession;
   beforeAll(() => {
     const pdfDocumentLoadMock = async () => await PDFDocument.load(testPdfDoc);
     const pdfDocumentcreateMock = async () => await PDFDocument.create();
-
+    docketNumber = '101-20';
+    trialSession = {
+      ...MOCK_TRIAL_REGULAR,
+      proceedingType: TRIAL_SESSION_PROCEEDING_TYPES.inPerson,
+    };
     applicationContext
       .getPersistenceGateway()
       .getDocument.mockResolvedValue(fakeData);
@@ -30,18 +45,9 @@ describe('generateNoticesForCaseTrialSessionCalendarInteractor', () => {
         load: pdfDocumentLoadMock,
       },
     });
-    // applicationContext
-    //   .getUseCaseHelpers()
-    //   .countPagesInDocument.mockReturnValue(3);
   });
 
   it('should decrement the job counter when a worker has processed a pdf file', async () => {
-    const docketNumber = '101-20';
-    const trialSession = {
-      ...MOCK_TRIAL_REGULAR,
-      proceedingType: TRIAL_SESSION_PROCEEDING_TYPES.inPerson,
-    };
-
     await generateNoticesForCaseTrialSessionCalendarInteractor(
       applicationContext,
       {
@@ -57,18 +63,39 @@ describe('generateNoticesForCaseTrialSessionCalendarInteractor', () => {
     // expect(applicationContext.getUtilities().combineTwoPdfs).toHaveBeenCalled();
   });
 
-  // it('should combine the notice of trial issued letter to a clinic letter if a clinic letter is necessary', async () => {
-  //   const docketNumber = '101-20';
+  it('should combine the notice of trial issued letter to a clinic letter if a clinic letter is necessary', async () => {
+    await generateNoticesForCaseTrialSessionCalendarInteractor(
+      applicationContext,
+      {
+        docketNumber,
+        jobId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+        trialSession,
+        userId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+      },
+    );
 
-  //   generateNoticesForCaseTrialSessionCalendarInteractor(applicationContext, {
-  //     docketNumber,
-  //     jobId: '6805d1ab-18d0-43ec-bafb-654e83405416',
-  //     trialSessionId: '6805d1ab-18d0-43ec-bafb-654e83405416',
-  //     userId: '6805d1ab-18d0-43ec-bafb-654e83405416',
-  //   });
-  //   // expect(
-  //   //   applicationContext.getPersistenceGateway().getDocument,
-  //   // ).toHaveBeenCalled();
-  //   // expect(applicationContext.getUtilities().combineTwoPdfs).toHaveBeenCalled();
-  // });
+    expect(
+      applicationContext.getPersistenceGateway().getDocument,
+    ).toHaveBeenCalled();
+
+    expect(applicationContext.getUtilities().combineTwoPdfs).toHaveBeenCalled();
+  });
+
+  it('should NOT combine the notice of trial issued letter to a clinic letter if a clinic letter is necessary', async () => {
+    await generateNoticesForCaseTrialSessionCalendarInteractor(
+      applicationContext,
+      {
+        docketNumber,
+        jobId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+        trialSession,
+        userId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+      },
+    );
+
+    expect(
+      applicationContext.getPersistenceGateway().getDocument,
+    ).toHaveBeenCalled();
+
+    expect(applicationContext.getUtilities().combineTwoPdfs).toHaveBeenCalled();
+  });
 });
