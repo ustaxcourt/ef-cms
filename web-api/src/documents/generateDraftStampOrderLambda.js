158c25eba11f656b06a6040b36158542cdf67ec5
Rosie Highsmith <ahighsmith@flexion.us>
2022-07-18 10:01:21 -0700

    9479: adding scaffolding for generateDraftStampOrder backend [skip ci]

---
+++ web-api/src/documents/generateDraftStampOrderLambda.js
@@ -0,0 +1,17 @@
+const { genericHandler } = require('../genericHandler');
+
+/**
+ * used for generating a draft stamp order
+ *
+ * @param {object} event the AWS event object
+ * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
+ */
+exports.generateDraftStampOrderLambda = event =>
+  genericHandler(event, async ({ applicationContext }) => {
+    return await applicationContext
+      .getUseCases()
+      .generateDraftStampOrderInteractor(
+        applicationContext,
+        JSON.parse(event.body),
+      );
+  });
