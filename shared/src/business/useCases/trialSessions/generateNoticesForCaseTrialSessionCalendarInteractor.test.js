98edf0e55910ad4b4385b6ac565c605bec287a4a
Cody Seibert <cseibert113@gmail.com>
2022-07-18 10:43:37 -0400

    9518 - adding more test coverage

--- shared/src/business/useCases/trialSessions/generateNoticesForCaseTrialSessionCalendarInteractor.test.js
+++ shared/src/business/useCases/trialSessions/generateNoticesForCaseTrialSessionCalendarInteractor.test.js
@@ -6,27 +6,27 @@ const {
   generateNoticesForCaseTrialSessionCalendarInteractor,
 } = require('./generateNoticesForCaseTrialSessionCalendarInteractor');
 const {
+  MOCK_CASE,
+  MOCK_ELIGIBLE_CASE_WITH_PRACTITIONERS,
+} = require('../../../test/mockCase');
+const {
+  SERVICE_INDICATOR_TYPES,
   TRIAL_SESSION_PROCEEDING_TYPES,
 } = require('../../entities/EntityConstants');
+const {
+  shouldAppendClinicLetter,
+} = require('../../utilities/shouldAppendClinicLetter');
 const { fakeData, getFakeFile } = require('../../test/getFakeFile');
-const { MOCK_CASE } = require('../../../test/mockCase');
 const { MOCK_TRIAL_REGULAR } = require('../../../test/mockTrial');
 const { PDFDocument } = require('pdf-lib');
 
-jest.mock('../../utilities/shouldAppendClinicLetter', () => ({
-  shouldAppendClinicLetter: jest.fn().mockResolvedValue({
-    appendClinicLetter: true,
-  }),
-}));
-
-jest.mock('../../utilities/copyPagesFromPdf', () => ({
-  copyPagesFromPdf: jest.fn(),
-}));
+jest.mock('../../utilities/shouldAppendClinicLetter');
 
 describe('generateNoticesForCaseTrialSessionCalendarInteractor', () => {
   let docketNumber, trialSession;
   beforeAll(() => {
     const pdfDocumentLoadMock = async () => await PDFDocument.load(testPdfDoc);
+    shouldAppendClinicLetter.mockResolvedValue({ appendClinicLetter: true });
     const pdfDocumentcreateMock = async () => await PDFDocument.create();
     docketNumber = '101-20';
     trialSession = {
@@ -174,5 +174,143 @@ describe('generateNoticesForCaseTrialSessionCalendarInteractor', () => {
         document: testPdfDoc,
       }),
     );
+  });
+
+  it('should send out notifications emails for the notice docket entry and standing pretrial notice', async () => {
+    applicationContext
+      .getPersistenceGateway()
+      .getCaseByDocketNumber.mockReturnValue({
+        ...MOCK_CASE,
+        procedureType: 'Small',
+      });
+
+    applicationContext
+      .getUseCases()
+      .generateStandingPretrialOrderForSmallCaseInteractor.mockResolvedValue(
+        testPdfDoc,
+      );
+
+    await generateNoticesForCaseTrialSessionCalendarInteractor(
+      applicationContext,
+      {
+        docketNumber,
+        jobId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+        trialSession,
+        userId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+      },
+    );
+
+    expect(
+      applicationContext.getUseCaseHelpers().sendServedPartiesEmails,
+    ).toHaveBeenCalledTimes(2);
+  });
+
+  it('should not append clinic letter when creating notices for a case with a represented petitioner', async () => {
+    applicationContext
+      .getPersistenceGateway()
+      .getCaseByDocketNumber.mockReturnValue({
+        ...MOCK_CASE,
+        petitioners: [
+          {
+            ...MOCK_CASE.petitioners[0],
+            serviceIndicator: SERVICE_INDICATOR_TYPES.SI_PAPER,
+          },
+        ],
+        privatePractitioners: [
+          {
+            ...MOCK_ELIGIBLE_CASE_WITH_PRACTITIONERS.privatePractitioners[0],
+            representing: [MOCK_CASE.petitioners[0].contactId],
+          },
+        ],
+        procedureType: 'Small',
+      });
+
+    await generateNoticesForCaseTrialSessionCalendarInteractor(
+      applicationContext,
+      {
+        docketNumber,
+        jobId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+        trialSession,
+        userId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+      },
+    );
+
+    expect(
+      applicationContext.getDocumentGenerators().addressLabelCoverSheet,
+    ).toHaveBeenCalledTimes(1);
+
+    const pdfBlob =
+      applicationContext.getPersistenceGateway().saveDocumentFromLambda.mock
+        .calls[2][0].document;
+    const pdf = await PDFDocument.load(pdfBlob);
+
+    expect(pdf.getPages().length).toBe(2);
+  });
+
+  it('should append the clinic letter for pro se petitioners', async () => {
+    applicationContext
+      .getPersistenceGateway()
+      .getCaseByDocketNumber.mockReturnValue({
+        ...MOCK_CASE,
+        petitioners: [
+          {
+            ...MOCK_CASE.petitioners[0],
+            serviceIndicator: SERVICE_INDICATOR_TYPES.SI_PAPER,
+          },
+        ],
+        privatePractitioners: [],
+        procedureType: 'Small',
+      });
+
+    await generateNoticesForCaseTrialSessionCalendarInteractor(
+      applicationContext,
+      {
+        docketNumber,
+        jobId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+        trialSession,
+        userId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+      },
+    );
+
+    const pdfBlob =
+      applicationContext.getPersistenceGateway().saveDocumentFromLambda.mock
+        .calls[2][0].document;
+    const pdf = await PDFDocument.load(pdfBlob);
+
+    expect(pdf.getPages().length).toBe(3);
+  });
+
+  it('should not append clinic letter if the clinic letter is not in s3 for the trial location', async () => {
+    shouldAppendClinicLetter.mockResolvedValue({ appendClinicLetter: false });
+    applicationContext
+      .getPersistenceGateway()
+      .getCaseByDocketNumber.mockReturnValue({
+        ...MOCK_CASE,
+        petitioners: [
+          {
+            ...MOCK_CASE.petitioners[0],
+            serviceIndicator: SERVICE_INDICATOR_TYPES.SI_PAPER,
+          },
+        ],
+        privatePractitioners: [],
+        procedureType: 'Small',
+      });
+
+    await generateNoticesForCaseTrialSessionCalendarInteractor(
+      applicationContext,
+      {
+        docketNumber,
+        jobId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+        trialSession,
+        userId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+      },
+    );
+
+    const pdfBlob =
+      applicationContext.getPersistenceGateway().saveDocumentFromLambda.mock
+        .calls[2][0].document;
+    const pdf = await PDFDocument.load(pdfBlob);
+
+    expect(pdf.getPages().length).toBe(3);
   });
 });
