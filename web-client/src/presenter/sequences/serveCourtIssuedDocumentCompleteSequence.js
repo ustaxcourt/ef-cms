17b23479490962f90f1f677f6cb397e6b3af412f
TElliott <telliott@flexion.us>
2022-07-25 15:45:20 -0500

    9513: Get and set case on redirect to ensure latest docket record [skip ci]

--- web-client/src/presenter/sequences/serveCourtIssuedDocumentCompleteSequence.js
+++ web-client/src/presenter/sequences/serveCourtIssuedDocumentCompleteSequence.js
@@ -1,9 +1,11 @@
 import { clearModalAction } from '../actions/clearModalAction';
 import { followRedirectAction } from '../actions/followRedirectAction';
+import { getCaseAction } from '../actions/getCaseAction';
 import { isPrintPreviewPreparedAction } from '../actions/CourtIssuedOrder/isPrintPreviewPreparedAction';
 import { navigateToCaseDetailAction } from '../actions/navigateToCaseDetailAction';
 import { navigateToPrintPaperServiceAction } from '../actions/navigateToPrintPaperServiceAction';
 import { setAlertSuccessAction } from '../actions/setAlertSuccessAction';
+import { setCaseAction } from '../actions/setCaseAction';
 import { setDocumentToDisplayFromDocumentIdAction } from '../actions/setDocumentToDisplayFromDocumentIdAction';
 import { setPdfPreviewUrlAction } from '../actions/CourtIssuedOrder/setPdfPreviewUrlAction';
 import { setSaveAlertsForNavigationAction } from '../actions/setSaveAlertsForNavigationAction';
@@ -18,6 +20,8 @@ export const serveCourtIssuedDocumentCompleteSequence = [
   isPrintPreviewPreparedAction,
   {
     no: [
+      getCaseAction,
+      setCaseAction,
       followRedirectAction,
       {
         default: navigateToCaseDetailAction,
