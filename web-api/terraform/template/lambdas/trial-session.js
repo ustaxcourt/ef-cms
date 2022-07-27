266833e4d843223b8cb1032f1f60f2f935ccbddd
Cody Seibert <cseibert113@gmail.com>
2022-07-27 11:21:22 -0400

    9518 - wrapping the trial session in try catch

--- web-api/terraform/template/lambdas/trial-session.js
+++ web-api/terraform/template/lambdas/trial-session.js
@@ -2,24 +2,32 @@ const createApplicationContext = require('../../../src/applicationContext');
 
 exports.handler = async event => {
   const applicationContext = createApplicationContext({});
-  const { Records } = event;
-  const { body, receiptHandle } = Records[0];
-  const { docketNumber, jobId, trialSession, userId } = JSON.parse(body);
+  try {
+    const { Records } = event;
+    const { body, receiptHandle } = Records[0];
+    const { docketNumber, jobId, trialSession, userId } = JSON.parse(body);
 
-  await applicationContext
-    .getUseCases()
-    .generateNoticesForCaseTrialSessionCalendarInteractor(applicationContext, {
-      docketNumber,
-      jobId,
-      trialSession,
-      userId,
-    });
+    await applicationContext
+      .getUseCases()
+      .generateNoticesForCaseTrialSessionCalendarInteractor(
+        applicationContext,
+        {
+          docketNumber,
+          jobId,
+          trialSession,
+          userId,
+        },
+      );
 
-  const sqs = await applicationContext.getMessagingClient();
-  await sqs
-    .deleteMessage({
-      QueueUrl: `https://sqs.${process.env.REGION}.amazonaws.com/${process.env.AWS_ACCOUNT_ID}/calendar_trial_session_queue_${process.env.STAGE}_${process.env.CURRENT_COLOR}`,
-      ReceiptHandle: receiptHandle,
-    })
-    .promise();
+    const sqs = await applicationContext.getMessagingClient();
+    await sqs
+      .deleteMessage({
+        QueueUrl: `https://sqs.${process.env.REGION}.amazonaws.com/${process.env.AWS_ACCOUNT_ID}/calendar_trial_session_queue_${process.env.STAGE}_${process.env.CURRENT_COLOR}`,
+        ReceiptHandle: receiptHandle,
+      })
+      .promise();
+  } catch (err) {
+    applicationContext.logger.error(err);
+    throw err;
+  }
 };
