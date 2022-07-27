bf7ea4a07552184d84182f30ec4d1feacd2467a5
Cody Seibert <cseibert113@gmail.com>
2022-07-27 10:34:17 -0400

    9518 - logging out the errors

--- web-api/terraform/template/lambdas/send-emails.js
+++ web-api/terraform/template/lambdas/send-emails.js
@@ -5,17 +5,22 @@ const {
 
 exports.handler = async event => {
   const applicationContext = createApplicationContext({});
-  const { Records } = event;
-  const { body, receiptHandle } = Records[0];
-  const params = JSON.parse(body);
+  try {
+    const { Records } = event;
+    const { body, receiptHandle } = Records[0];
+    const params = JSON.parse(body);
 
-  await sendWithRetry({ applicationContext, params });
+    await sendWithRetry({ applicationContext, params });
 
-  const sqs = await applicationContext.getMessagingClient();
-  await sqs
-    .deleteMessage({
-      QueueUrl: `https://sqs.${process.env.REGION}.amazonaws.com/${process.env.AWS_ACCOUNT_ID}/send_emails_queue_${process.env.STAGE}_${process.env.CURRENT_COLOR}.fifo`,
-      ReceiptHandle: receiptHandle,
-    })
-    .promise();
+    const sqs = await applicationContext.getMessagingClient();
+    await sqs
+      .deleteMessage({
+        QueueUrl: `https://sqs.${process.env.REGION}.amazonaws.com/${process.env.AWS_ACCOUNT_ID}/send_emails_queue_${process.env.STAGE}_${process.env.CURRENT_COLOR}.fifo`,
+        ReceiptHandle: receiptHandle,
+      })
+      .promise();
+  } catch (err) {
+    applicationContext.logger.error(err);
+    throw err;
+  }
 };
