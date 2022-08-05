7334e2a1807c032bd89d2ea291f081270b9422c8
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2022-08-05 14:41:56 -0600

    9479: adding pa11y test for apply stamp form

--- web-client/pa11y/pa11y-judge.js
+++ web-client/pa11y/pa11y-judge.js
@@ -45,4 +45,5 @@ module.exports = [
     notes: 'checks a11y of delete procedural note modal',
     url: 'http://localhost:1234/log-in?code=judgeColvin@example.com&path=/case-detail/112-19&info=delete-procedural-note-modal',
   },
+  'http://localhost:1234/log-in?code=judgeColvin@example.com&path=case-detail/105-20/documents/3eb53932-1a44-40d1-bfb8-d9e908b0b32e/apply-stamp',
 ];
