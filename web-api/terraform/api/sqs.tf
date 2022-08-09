c36785098bf551721c7f040e6410543ef5bdadd7
Absolutestunna <joshujah007@gmail.com>
2022-08-09 17:33:01 -0400

    9569 - changed the maxReceiveCount for lambda to have tries to calender job; clean up

--- web-api/terraform/api/sqs.tf
+++ web-api/terraform/api/sqs.tf
@@ -1,6 +1,6 @@
 resource "aws_sqs_queue" "calendar_trial_session_queue" {
-  name                        = "calendar_trial_session_queue_${var.environment}_${var.current_color}"
-  visibility_timeout_seconds  = "30"
+  name                       = "calendar_trial_session_queue_${var.environment}_${var.current_color}"
+  visibility_timeout_seconds = "30"
 
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.calendar_trial_session_dl_queue.arn
@@ -26,6 +26,7 @@ resource "aws_sqs_queue" "send_emails_queue" {
 }
 
 resource "aws_sqs_queue" "send_emails_dl_queue" {
-  name = "send_emails_dl_queue_${var.environment}_${var.current_color}.fifo"
-  fifo_queue                  = true
+  name       = "send_emails_dl_queue_${var.environment}_${var.current_color}.fifo"
+  fifo_queue = true
 }
+
