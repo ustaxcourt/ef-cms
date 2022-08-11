0f76745a6bbf70a850f81a7ad7479640405d18e4
Cody Seibert <cseibert113@gmail.com>
2022-08-11 14:59:11 -0400

    devex - filter out non PMT documents from backend api

--- web-client/integration-tests/chambersViewsWorkingCopyTrialSession.test.js
+++ web-client/integration-tests/chambersViewsWorkingCopyTrialSession.test.js
@@ -73,11 +73,6 @@ describe('Chambers dashboard', () => {
       },
     );
 
-    console.log(
-      'trialSessionFormatted.allCases',
-      trialSessionFormatted.allCases.map(c => c.docketNumber),
-    );
-
     const caseWithPtmFiledByPetitioner = trialSessionFormatted.allCases.find(
       c => c.docketNumber === cerebralTest.docketNumber1,
     );
