e1c828fe3a7340c0ad19e7c83c2ebe2f935afda5
Rosie Highsmith <ahighsmith@flexion.us>
2022-08-12 13:03:24 -0700

    9479: small refactoring to retrigger github actions tests

--- shared/src/business/useCaseHelper/stampDisposition/addDraftStampOrderDocketEntryInteractor.test.js
+++ shared/src/business/useCaseHelper/stampDisposition/addDraftStampOrderDocketEntryInteractor.test.js
@@ -93,27 +93,27 @@ describe('addDraftStampOrderDocketEntryInteractor', () => {
   });
 
   it('should add the stamped document to the latest message if parentMessageId is included', async () => {
+    const mockMessage = {
+      caseStatus: MOCK_CASE.status,
+      caseTitle: 'Test Petitioner',
+      createdAt: '2019-03-01T21:40:46.415Z',
+      docketNumber: MOCK_CASE.docketNumber,
+      docketNumberWithSuffix: MOCK_CASE.docketNumber,
+      from: 'Test Petitionsclerk',
+      fromSection: PETITIONS_SECTION,
+      fromUserId: '4791e892-14ee-4ab1-8468-0c942ec379d2',
+      message: 'hey there',
+      messageId: 'a10d6855-f3ee-4c11-861c-c7f11cba4dff',
+      parentMessageId: mockParentMessageId,
+      subject: 'hello',
+      to: 'Test Petitionsclerk2',
+      toSection: PETITIONS_SECTION,
+      toUserId: '449b916e-3362-4a5d-bf56-b2b94ba29c12',
+    };
+
     applicationContext
       .getPersistenceGateway()
-      .getMessageThreadByParentId.mockReturnValue([
-        {
-          caseStatus: MOCK_CASE.status,
-          caseTitle: 'Test Petitioner',
-          createdAt: '2019-03-01T21:40:46.415Z',
-          docketNumber: MOCK_CASE.docketNumber,
-          docketNumberWithSuffix: MOCK_CASE.docketNumber,
-          from: 'Test Petitionsclerk',
-          fromSection: PETITIONS_SECTION,
-          fromUserId: '4791e892-14ee-4ab1-8468-0c942ec379d2',
-          message: 'hey there',
-          messageId: 'a10d6855-f3ee-4c11-861c-c7f11cba4dff',
-          parentMessageId: mockParentMessageId,
-          subject: 'hello',
-          to: 'Test Petitionsclerk2',
-          toSection: PETITIONS_SECTION,
-          toUserId: '449b916e-3362-4a5d-bf56-b2b94ba29c12',
-        },
-      ]);
+      .getMessageThreadByParentId.mockReturnValue([mockMessage]);
 
     await addDraftStampOrderDocketEntryInteractor(applicationContext, {
       ...args,
