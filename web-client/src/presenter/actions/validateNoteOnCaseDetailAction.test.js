f75ed047bc677d1488e8a863e4e5ab045f3a8d47
Rosie Highsmith <ahighsmith@flexion.us>
2022-08-16 13:41:05 -0700

    9566: WIP validateNoteOnCaseDetailAction test

---
+++ web-client/src/presenter/actions/validateNoteOnCaseDetailAction.test.js
@@ -0,0 +1,62 @@
+import { applicationContextForClient as applicationContext } from '../../../../shared/src/business/test/createTestApplicationContext';
+import { presenter } from '../presenter-mock';
+import { runAction } from 'cerebral/test';
+import { validateNoteOnCaseDetailAction } from './validateNoteOnCaseDetailAction';
+
+describe('validateNoteOnCaseDetailAction', () => {
+  let successStub;
+  let errorStub;
+
+  let mockNote;
+
+  beforeAll(() => {
+    successStub = jest.fn();
+    errorStub = jest.fn();
+
+    mockNote = {
+      notes: 'hello notes',
+    };
+
+    presenter.providers.applicationContext = applicationContext;
+    presenter.providers.path = {
+      error: errorStub,
+      success: successStub,
+    };
+  });
+
+  it('should call the success path when no errors are found', async () => {
+    applicationContext
+      .getUseCases()
+      .validateCaseDetailInteractor.mockReturnValue(null);
+
+    await runAction(validateNoteOnCaseDetailAction, {
+      modules: {
+        presenter,
+      },
+      state: {
+        caseDetail: mockNote,
+        modal: {
+          notes: mockNote,
+        },
+      },
+    });
+
+    expect(successStub.mock.calls.length).toEqual(1);
+  });
+
+  it('should call the error path when any errors are found', async () => {
+    applicationContext
+      .getUseCases()
+      .validateCaseDetailInteractor.mockReturnValue('error');
+    await runAction(validateNoteOnCaseDetailAction, {
+      modules: {
+        presenter,
+      },
+      state: {
+        form: mockNote,
+      },
+    });
+
+    expect(errorStub.mock.calls.length).toEqual(1);
+  });
+});
