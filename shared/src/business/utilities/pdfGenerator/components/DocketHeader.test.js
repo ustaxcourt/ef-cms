8790be96d0c2a9cdc423426d4e9d8abd3b981f77
Rosie Highsmith <ahighsmith@flexion.us>
2022-08-16 08:29:20 -0700

    devex-1071: update DocketHeader test to use RTL

--- shared/src/business/utilities/pdfGenerator/components/DocketHeader.test.js
+++ shared/src/business/utilities/pdfGenerator/components/DocketHeader.test.js
@@ -1,42 +1,45 @@
 const React = require('react');
 const { DocketHeader } = require('./DocketHeader.jsx');
-const { shallow } = require('enzyme');
+const { render, within } = require('@testing-library/react');
 
 describe('DocketHeader', () => {
-  it('renders the case caption from props', () => {
-    let wrapper = shallow(<DocketHeader caseTitle="Test Petitioner" />);
-    expect(wrapper.find('#caption-title').text()).toEqual('Test Petitioner');
+  it('should render the case caption from props', () => {
+    const mockCaseTitle = 'Test Petitioner';
+    const { container } = render(<DocketHeader caseTitle={mockCaseTitle} />);
+    expect(within(container).getByText(mockCaseTitle)).toBeInTheDocument();
   });
 
-  it('renders the case caption extension from props', () => {
-    let wrapper = shallow(
-      <DocketHeader caseCaptionExtension="Petitioner(s)" />,
+  it('should render the case caption extension from props', () => {
+    const mockCaseTitle = 'Petitioner(s)';
+    const { container } = render(<DocketHeader caseTitle="Petitioner(s)" />);
+    expect(within(container).getByText(mockCaseTitle)).toBeInTheDocument();
+  });
+
+  it('should render the case caption postfix', () => {
+    const { container } = render(<DocketHeader />);
+    expect(within(container).getByText('v.')).toBeInTheDocument();
+    expect(
+      within(container).getByText('Commissioner of Internal Revenue'),
+    ).toBeInTheDocument();
+    expect(within(container).getByText('Respondent')).toBeInTheDocument();
+  });
+
+  it('should render the docket number from props', () => {
+    const mockDocketNumber = '123-45S';
+    const { container } = render(
+      <DocketHeader docketNumberWithSuffix={mockDocketNumber} />,
     );
-    expect(wrapper.find('#caption-extension').text()).toEqual('Petitioner(s)');
+    expect(
+      within(container).getByText(mockDocketNumber, { exact: false }),
+    ).toBeInTheDocument();
   });
 
-  it('renders the case caption postfix', () => {
-    const wrapper = shallow(<DocketHeader />);
+  it("should only render document title if it's the only prop passed in", () => {
+    const mockDocumentTitle = 'Test H3';
 
-    expect(wrapper.find('#caption-v').text()).toEqual('v.');
-    expect(wrapper.find('#caption-commissioner').text()).toEqual(
-      'Commissioner of Internal Revenue',
+    const { container } = render(
+      <DocketHeader documentTitle={mockDocumentTitle} />,
     );
-    expect(wrapper.find('#caption-respondent').text()).toEqual('Respondent');
-  });
-
-  it('renders the docket number from props', () => {
-    let wrapper = shallow(<DocketHeader docketNumberWithSuffix="123-45S" />);
-    expect(wrapper.find('#docket-number').text()).toEqual('Docket No. 123-45S');
-  });
-
-  it('only renders an h3 element if h3 prop is provided', () => {
-    let wrapper = shallow(<DocketHeader />);
-    let h3 = wrapper.find('.case-information h3');
-    expect(h3).toEqual({});
-
-    wrapper = shallow(<DocketHeader documentTitle="Test H3" />);
-    h3 = wrapper.find('.case-information h3');
-    expect(h3.text()).toEqual('Test H3');
+    expect(within(container).getByText(mockDocumentTitle)).toBeInTheDocument();
   });
 });
