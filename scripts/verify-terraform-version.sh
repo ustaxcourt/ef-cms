3b91aa54a8db96d49898bac4e2e0f2b6184f1a2b
Absolutestunna <joshujah007@gmail.com>
2022-08-18 17:12:53 -0400

    dep-updates: small change to correct echo statement

--- scripts/verify-terraform-version.sh
+++ scripts/verify-terraform-version.sh
@@ -3,6 +3,6 @@
 tf_version=$(terraform --version)
 
 if [[ ${tf_version} != *"1.2.6"* ]]; then
-  echo "Please set your terraform version to 1.2.6 before deploying."
+  echo "Please set your terraform version to 1.2.7 before deploying."
   exit 1
 fi
