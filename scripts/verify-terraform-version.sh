429ed91c9e941ffe4a74414dace3baeaadc25843
Absolutestunna <joshujah007@gmail.com>
2022-08-19 12:35:27 -0400

    dep-updates: removed ajv from cav, modified messaging in verifying terraform version

--- scripts/verify-terraform-version.sh
+++ scripts/verify-terraform-version.sh
@@ -2,7 +2,7 @@
 
 tf_version=$(terraform --version)
 
-if [[ ${tf_version} != *"1.2.6"* ]]; then
+if [[ ${tf_version} != *"1.2.7"* ]]; then
   echo "Please set your terraform version to 1.2.7 before deploying."
   exit 1
 fi
