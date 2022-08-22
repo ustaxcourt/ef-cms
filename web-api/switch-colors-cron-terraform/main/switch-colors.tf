112064248582e184e1ab29f6219a93d2889c8be3
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2022-08-22 11:10:00 -0500

    1075: Change cron

--- web-api/switch-colors-cron-terraform/main/switch-colors.tf
+++ web-api/switch-colors-cron-terraform/main/switch-colors.tf
@@ -27,7 +27,7 @@ resource "aws_lambda_function" "switch_colors_status_lambda" {
 
 resource "aws_cloudwatch_event_rule" "check_switch_colors_status_cron_rule-sunday" {
   name                = "check_switch_colors_status_cron_${var.environment}"
-  schedule_expression = "cron(0/1 0-3 ? * SUN *)"
+  schedule_expression = "cron(0/1 16 ? * MON-SAT *)"
   is_enabled          = "true"
 }
 
