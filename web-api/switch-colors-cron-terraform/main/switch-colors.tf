9334259bd5e5e41548d66d23b663d2c290d80760
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2022-08-23 09:47:21 -0500

    1075: Disable the rule but use correct time for my test

--- web-api/switch-colors-cron-terraform/main/switch-colors.tf
+++ web-api/switch-colors-cron-terraform/main/switch-colors.tf
@@ -29,8 +29,8 @@ resource "aws_lambda_function" "switch_colors_status_lambda" {
 
 resource "aws_cloudwatch_event_rule" "check_switch_colors_status_cron_rule-sunday" {
   name                = "check_switch_colors_status_cron_${var.environment}"
-  schedule_expression = "cron(0/15 3,4 ? * MON-SAT *)"
-  is_enabled          = "true"
+  schedule_expression = "cron(10/5 15 ? * MON-SAT *)"
+  is_enabled          = "false"
 }
 
 resource "aws_cloudwatch_event_target" "check_switch_colors_status_cron_target" {
