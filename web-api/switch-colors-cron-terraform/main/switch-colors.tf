b5d15ba5be22ccfdcb17a440d67613a771d6f6a1
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2022-08-23 09:00:46 -0500

    1075: Set to trigger at 25 after

--- web-api/switch-colors-cron-terraform/main/switch-colors.tf
+++ web-api/switch-colors-cron-terraform/main/switch-colors.tf
@@ -29,7 +29,7 @@ resource "aws_lambda_function" "switch_colors_status_lambda" {
 
 resource "aws_cloudwatch_event_rule" "check_switch_colors_status_cron_rule-sunday" {
   name                = "check_switch_colors_status_cron_${var.environment}"
-  schedule_expression = "cron(0/1 2 ? * MON-SAT *)"
+  schedule_expression = "cron(25/5 2 ? * MON-SAT *)"
   is_enabled          = "false"
 }
 
