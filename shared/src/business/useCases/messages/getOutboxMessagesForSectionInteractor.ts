7cebf932ddee6181682781253db6f66e6ff496cf
Cody Seibert <cseibert113@gmail.com>
2022-08-25 15:59:08 -0400

    devex - refactor the files back to ts [skip ci]

--- shared/src/business/useCases/messages/getOutboxMessagesForSectionInteractor.ts
+++ shared/src/business/useCases/messages/getOutboxMessagesForSectionInteractor.ts
@@ -1,9 +1,9 @@
-const {
+import {
   isAuthorized,
   ROLE_PERMISSIONS,
-} = require('../../../authorization/authorizationClientService');
-const { Message } = require('../../entities/Message');
-const { UnauthorizedError } = require('../../../errors/errors');
+} from '../../../authorization/authorizationClientService';
+import { Message } from '../../entities/Message';
+import { UnauthorizedError } from '../../../errors/errors';
 
 /**
  * getOutboxMessagesForSectionInteractor
@@ -13,24 +13,22 @@ const { UnauthorizedError } = require('../../../errors/errors');
  * @param {string} providers.section the section to get the outbox messages
  * @returns {object} the messages in the section outbox
  */
-exports.getOutboxMessagesForSectionInteractor = async (
-  applicationContext,
-  { section },
-) => {
-  const authorizedUser = applicationContext.getCurrentUser();
+export const getOutboxMessagesForSectionInteractor: IGetOutboxMessagesForSectionInteractor =
+  async (applicationContext, { section }) => {
+    const authorizedUser = applicationContext.getCurrentUser();
 
-  if (!isAuthorized(authorizedUser, ROLE_PERMISSIONS.VIEW_MESSAGES)) {
-    throw new UnauthorizedError('Unauthorized');
-  }
+    if (!isAuthorized(authorizedUser, ROLE_PERMISSIONS.VIEW_MESSAGES)) {
+      throw new UnauthorizedError('Unauthorized');
+    }
 
-  const messages = await applicationContext
-    .getPersistenceGateway()
-    .getSectionOutboxMessages({
+    const messages = await applicationContext
+      .getPersistenceGateway()
+      .getSectionOutboxMessages({
+        applicationContext,
+        section,
+      });
+
+    return Message.validateRawCollection(messages, {
       applicationContext,
-      section,
     });
-
-  return Message.validateRawCollection(messages, {
-    applicationContext,
-  });
-};
+  };
