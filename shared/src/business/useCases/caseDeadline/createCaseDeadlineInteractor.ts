5595c13845323e21ae1e82c54c886c5d57ab24ef
Rosie Highsmith <ahighsmith@flexion.us>
2022-08-25 14:31:43 -0700

    commit createCaseDeadlineInteractor with refactoring to ts [skip ci]

--- shared/src/business/useCases/caseDeadline/createCaseDeadlineInteractor.ts
+++ shared/src/business/useCases/caseDeadline/createCaseDeadlineInteractor.ts
@@ -14,47 +14,45 @@ const { UnauthorizedError } = require('../../../errors/errors');
  * @param {object} providers.caseDeadline the case deadline data
  * @returns {CaseDeadline} the created case deadline
  */
-exports.createCaseDeadlineInteractor = async (
-  applicationContext,
-  { caseDeadline },
-) => {
-  const user = applicationContext.getCurrentUser();
+export const createCaseDeadlineInteractor: ICreateCaseDeadlineInteractor =
+  async (applicationContext, { caseDeadline }) => {
+    const user = applicationContext.getCurrentUser();
 
-  if (!isAuthorized(user, ROLE_PERMISSIONS.CASE_DEADLINE)) {
-    throw new UnauthorizedError('Unauthorized for create case deadline');
-  }
+    if (!isAuthorized(user, ROLE_PERMISSIONS.CASE_DEADLINE)) {
+      throw new UnauthorizedError('Unauthorized for create case deadline');
+    }
 
-  const caseDetail = await applicationContext
-    .getPersistenceGateway()
-    .getCaseByDocketNumber({
+    const caseDetail = await applicationContext
+      .getPersistenceGateway()
+      .getCaseByDocketNumber({
+        applicationContext,
+        docketNumber: caseDeadline.docketNumber,
+      });
+    let caseEntity = new Case(caseDetail, { applicationContext });
+
+    const newCaseDeadline = new CaseDeadline(
+      { ...caseDeadline, associatedJudge: caseEntity.associatedJudge },
+      {
+        applicationContext,
+      },
+    );
+
+    await applicationContext.getPersistenceGateway().createCaseDeadline({
       applicationContext,
-      docketNumber: caseDeadline.docketNumber,
-    });
-  let caseEntity = new Case(caseDetail, { applicationContext });
-
-  const newCaseDeadline = new CaseDeadline(
-    { ...caseDeadline, associatedJudge: caseEntity.associatedJudge },
-    {
-      applicationContext,
-    },
-  );
-
-  await applicationContext.getPersistenceGateway().createCaseDeadline({
-    applicationContext,
-    caseDeadline: newCaseDeadline.validate().toRawObject(),
-  });
-
-  caseEntity = await applicationContext
-    .getUseCaseHelpers()
-    .updateCaseAutomaticBlock({
-      applicationContext,
-      caseEntity,
+      caseDeadline: newCaseDeadline.validate().toRawObject(),
     });
 
-  await applicationContext.getUseCaseHelpers().updateCaseAndAssociations({
-    applicationContext,
-    caseToUpdate: caseEntity,
-  });
+    caseEntity = await applicationContext
+      .getUseCaseHelpers()
+      .updateCaseAutomaticBlock({
+        applicationContext,
+        caseEntity,
+      });
 
-  return newCaseDeadline;
-};
+    await applicationContext.getUseCaseHelpers().updateCaseAndAssociations({
+      applicationContext,
+      caseToUpdate: caseEntity,
+    });
+
+    return newCaseDeadline;
+  };
