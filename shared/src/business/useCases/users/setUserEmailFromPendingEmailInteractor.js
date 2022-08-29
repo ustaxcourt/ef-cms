42e3f09295ca9c44fc22d4f6adfef169dc8b3d29
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2022-08-29 13:40:51 -0500

    9590: Check for inactivePractitioner role

--- shared/src/business/useCases/users/setUserEmailFromPendingEmailInteractor.js
+++ shared/src/business/useCases/users/setUserEmailFromPendingEmailInteractor.js
@@ -58,7 +58,8 @@ exports.setUserEmailFromPendingEmailInteractor = async (
   let userEntity;
   if (
     user.role === ROLES.privatePractitioner ||
-    user.role === ROLES.irsPractitioner
+    user.role === ROLES.irsPractitioner ||
+    user.role === ROLES.inactivePractitioner
   ) {
     userEntity = new Practitioner({
       ...user,
