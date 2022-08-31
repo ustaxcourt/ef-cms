72853720499e37471d6df28fc7f77a8280be0ed3
Cody Seibert <cseibert113@gmail.com>
2022-08-31 18:18:22 -0400

    devex - only load judge user for users associated with the judge

--- web-client/src/presenter/sequences/chooseWorkQueueSequence.js
+++ web-client/src/presenter/sequences/chooseWorkQueueSequence.js
@@ -1,5 +1,6 @@
 import { chooseWorkQueueAction } from '../actions/chooseWorkQueueAction';
 import { clearWorkQueueAction } from '../actions/clearWorkQueueAction';
+import { getConstants } from '../../getConstants';
 import { getDocumentQCInboxForSectionAction } from '../actions/getDocumentQCInboxForSectionAction';
 import { getDocumentQCInboxForUserAction } from '../actions/getDocumentQCInboxForUserAction';
 import { getDocumentQCServedForSectionAction } from '../actions/getDocumentQCServedForSectionAction';
@@ -7,16 +8,43 @@ import { getDocumentQCServedForUserAction } from '../actions/getDocumentQCServed
 import { getJudgeForCurrentUserAction } from '../actions/getJudgeForCurrentUserAction';
 import { getNotificationsAction } from '../actions/getNotificationsAction';
 import { parallel } from 'cerebral/factories';
+import { runPathForUserRoleAction } from '../actions/runPathForUserRoleAction';
 import { setJudgeUserAction } from '../actions/setJudgeUserAction';
 import { setNotificationsAction } from '../actions/setNotificationsAction';
 import { setWorkItemsAction } from '../actions/setWorkItemsAction';
 import { setWorkItemsCountAction } from '../actions/setWorkItemsCountAction';
 import { showProgressSequenceDecorator } from '../utilities/showProgressSequenceDecorator';
+import { takePathForRoles } from './takePathForRoles';
+const { USER_ROLES } = getConstants();
 
 export const chooseWorkQueueSequence = showProgressSequenceDecorator([
   clearWorkQueueAction,
-  getJudgeForCurrentUserAction,
-  setJudgeUserAction,
+  runPathForUserRoleAction,
+  {
+    ...takePathForRoles(
+      [
+        USER_ROLES.admin,
+        USER_ROLES.admissionsClerk,
+        USER_ROLES.clerkOfCourt,
+        USER_ROLES.docketClerk,
+        USER_ROLES.floater,
+        USER_ROLES.petitionsClerk,
+        USER_ROLES.reportersOffice,
+        USER_ROLES.trialClerk,
+        USER_ROLES.general,
+        USER_ROLES.inactivePractitioner,
+        USER_ROLES.irsPractitioner,
+        USER_ROLES.irsSuperuser,
+        USER_ROLES.petitioner,
+        USER_ROLES.privatePractitioner,
+      ],
+      () => {}, // noop, we don't do anything extra with these roles
+    ),
+    ...takePathForRoles(
+      [USER_ROLES.chambers, USER_ROLES.judge, USER_ROLES.adc],
+      [getJudgeForCurrentUserAction, setJudgeUserAction],
+    ),
+  },
   parallel([
     [getNotificationsAction, setNotificationsAction, setWorkItemsCountAction],
     [
