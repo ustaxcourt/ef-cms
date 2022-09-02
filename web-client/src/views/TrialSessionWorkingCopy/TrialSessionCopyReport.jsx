95008cd5d9f649b97bd59cbd4f09be851b0171d3
Timothy Shumway <tshumway@flexion.us>
2022-09-02 13:15:01 -0600

    9511 - changed href on printable trial session session copy to redirect back to trial session detail page

--- web-client/src/views/TrialSessionWorkingCopy/TrialSessionCopyReport.jsx
+++ web-client/src/views/TrialSessionWorkingCopy/TrialSessionCopyReport.jsx
@@ -2,25 +2,28 @@ import { BigHeader } from '../BigHeader';
 import { Button } from '../../ustc-ui/Button/Button';
 import { PdfPreview } from '../../ustc-ui/PdfPreview/PdfPreview';
 import { connect } from '@cerebral/react';
+import { state } from 'cerebral';
 import React from 'react';
 
 export const TrialSessionCopyReport = connect(
-  {},
-  function TrialSessionPlanningReport() {
+  {
+    formattedTrialSessionDetails: state.formattedTrialSessionDetails,
+  },
+  function TrialSessionPlanningReport({ formattedTrialSessionDetails }) {
     return (
       <>
-        {/* <BigHeader text="Trial Session COPY Report" />
+        <BigHeader text="Trial Session COPY" />
         <div className="grid-container print-docket-record">
           <Button
             link
             className="margin-bottom-3"
-            href="/trial-sessions"
+            href={`/trial-session-working-copy/${formattedTrialSessionDetails.trialSessionId}`}
             icon={['fa', 'arrow-alt-circle-left']}
           >
             Back to Trial Sessions (IN THE COPY REPORT)
-          </Button> */}
-        <PdfPreview />
-        {/* </div> */}
+          </Button>
+          <PdfPreview />
+        </div>
       </>
     );
   },
