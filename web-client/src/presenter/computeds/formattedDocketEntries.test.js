66dcf47e824734ac41aacefbbea9e5638be21162
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-09-02 08:37:39 -0700

    9564: Fixing tests [skip ci]

--- web-client/src/presenter/computeds/formattedDocketEntries.test.js
+++ web-client/src/presenter/computeds/formattedDocketEntries.test.js
@@ -48,6 +48,9 @@ describe('formattedDocketEntries', () => {
     globalUser = user;
     return {
       permissions: getUserPermissions(user),
+      sessionMetadata: {
+        docketRecordFilter: DOCKET_RECORD_FILTER_OPTIONS.allDocuments,
+      },
     };
   };
 
@@ -526,8 +529,8 @@ describe('formattedDocketEntries', () => {
 
       const result = runCompute(formattedDocketEntries, {
         state: {
-          caseDetail,
           ...getBaseState(petitionsClerkUser),
+          caseDetail,
         },
       });
 
@@ -543,7 +546,7 @@ describe('formattedDocketEntries', () => {
       ]);
     });
 
-    it.only('should ONLY show exhibit docket entries when "Exhibits" has been selected as the filter', () => {
+    it('should ONLY show exhibit docket entries when "Exhibits" has been selected as the filter', () => {
       const caseDetail = {
         ...MOCK_CASE,
         docketEntries: [
@@ -564,11 +567,11 @@ describe('formattedDocketEntries', () => {
 
       const result = runCompute(formattedDocketEntries, {
         state: {
+          ...getBaseState(petitionsClerkUser),
           caseDetail,
           sessionMetadata: {
             docketRecordFilter: DOCKET_RECORD_FILTER_OPTIONS.exhibits,
           },
-          ...getBaseState(petitionsClerkUser),
         },
       });
 
@@ -599,8 +602,8 @@ describe('formattedDocketEntries', () => {
 
       const result = runCompute(formattedDocketEntries, {
         state: {
-          caseDetail,
           ...getBaseState(petitionsClerkUser),
+          caseDetail,
         },
       });
 
