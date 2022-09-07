04725b9bbc169c589556403ddac0a2c55f8ee32c
Rosie Highsmith <ahighsmith@flexion.us>
2022-09-07 11:04:00 -0700

    9493: add test for new action, return path depending on event code [skip ci]

--- web-client/src/presenter/actions/CaseConsolidation/shouldSetupConsolidatedCasesAction.js
+++ web-client/src/presenter/actions/CaseConsolidation/shouldSetupConsolidatedCasesAction.js
@@ -1,22 +1,26 @@
 import { state } from 'cerebral';
 
 /**
- * //fix
  * initialize consolidated case state values for ConfirmInitiateServiceModal
  *
  * @param {object} providers the providers object
  * @param {object} providers.get the cerebral get object
  * @param {object} providers.store the cerebral store object
+ * @param {object} providers.path the cerebral path object
+ * @returns {object} the next path based on if the event code can be multi-docketed
  */
 export const shouldSetupConsolidatedCasesAction = ({
   applicationContext,
   get,
+  path,
 }) => {
   const { eventCode } = get(state.form);
   const { SINGLE_DOCKET_RECORD_ONLY_EVENT_CODES } =
     applicationContext.getConstants();
 
-  if (!SINGLE_DOCKET_RECORD_ONLY_EVENT_CODES.includes(eventCode)) {
-    //implement
+  if (SINGLE_DOCKET_RECORD_ONLY_EVENT_CODES.includes(eventCode)) {
+    return path.no();
+  } else {
+    return path.yes();
   }
 };
