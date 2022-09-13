34baa9da50a6b0e58ec13fe68c0305455d07d52e
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2022-09-13 15:26:00 -0600

    9493: renaming and fixing things [skip ci]

--- web-client/src/presenter/actions/DocketEntry/computeDocketEntryIdAction.js
+++ web-client/src/presenter/actions/DocketEntry/computeDocketEntryIdAction.js
@@ -9,7 +9,7 @@ import { state } from 'cerebral';
  * @param {object} providers.props the cerebral props object
  * @param {object} providers.store the cerebral store object
  */
-export const setDocketEntryIdAction = ({
+export const computeDocketEntryIdAction = ({
   applicationContext,
   get,
   props,
