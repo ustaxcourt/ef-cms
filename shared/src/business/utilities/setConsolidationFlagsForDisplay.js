bb777a6df91f3ba3149853aa929f2f7dc7cf2a1a
Cody Seibert <cseibert113@gmail.com>
2022-09-20 16:21:22 -0400

    3572 - changing the sort order for the eligible cases to group consolidated cases

--- shared/src/business/utilities/setConsolidationFlagsForDisplay.js
+++ shared/src/business/utilities/setConsolidationFlagsForDisplay.js
@@ -1,12 +1,22 @@
-exports.setConsolidationFlagsForDisplay = caseItem => {
+exports.setConsolidationFlagsForDisplay = (caseItem, eligibleCases) => {
   caseItem.inConsolidatedGroup = caseItem.leadCase = false;
 
-  if (caseItem.leadDocketNumber) {
-    caseItem.inConsolidatedGroup = true;
-    caseItem.consolidatedIconTooltipText = 'Consolidated case';
-    if (caseItem.leadDocketNumber === caseItem.docketNumber) {
-      caseItem.leadCase = true;
-      caseItem.consolidatedIconTooltipText = 'Lead case';
+  if (
+    !eligibleCases ||
+    (caseItem.leadDocketNumber &&
+      eligibleCases.find(
+        theCase => theCase.docketNumber === caseItem.leadDocketNumber,
+      ))
+  ) {
+    if (caseItem.leadDocketNumber) {
+      caseItem.inConsolidatedGroup = true;
+      caseItem.consolidatedIconTooltipText = 'Consolidated case';
+      if (caseItem.leadDocketNumber === caseItem.docketNumber) {
+        caseItem.leadCase = true;
+        caseItem.consolidatedIconTooltipText = 'Lead case';
+      } else {
+        caseItem.shouldIndent = true;
+      }
     }
   }
 
