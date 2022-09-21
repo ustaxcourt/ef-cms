a8277a45cc654221bb52773f5a97bc4887fa413d
Rosie Highsmith <ahighsmith@flexion.us>
2022-09-21 15:56:40 -0700

    9493: fix test by passing appContext

--- web-client/src/presenter/computeds/utilities/addConsolidatedProperties.test.js
+++ web-client/src/presenter/computeds/utilities/addConsolidatedProperties.test.js
@@ -1,10 +1,14 @@
 import { addConsolidatedProperties } from './addConsolidatedProperties';
+import { applicationContextForClient as applicationContext } from '../../../../../shared/src/business/test/createTestApplicationContext';
 
 describe('addConsolidatedProperties', () => {
   it('should add the Lead case tooltip text and set both inConsolidatedGroup and isLeadCase to true when docket number and leadDocketNumber are the same', () => {
     const result = addConsolidatedProperties({
-      docketNumber: '101-20',
-      leadDocketNumber: '101-20',
+      applicationContext,
+      caseObject: {
+        docketNumber: '101-20',
+        leadDocketNumber: '101-20',
+      },
     });
     expect(result).toMatchObject({
       consolidatedIconTooltipText: 'Lead case',
@@ -15,8 +19,11 @@ describe('addConsolidatedProperties', () => {
 
   it('should add the Consolidated case tooltip text and set inConsolidatedGroup to true and isLeadCase to false when docket number and leadDocketNumber are not the same but in the same consolidated group', () => {
     const result = addConsolidatedProperties({
-      docketNumber: '101-21',
-      leadDocketNumber: '101-20',
+      applicationContext,
+      caseObject: {
+        docketNumber: '101-21',
+        leadDocketNumber: '101-20',
+      },
     });
     expect(result).toMatchObject({
       consolidatedIconTooltipText: 'Consolidated case',
@@ -27,8 +34,11 @@ describe('addConsolidatedProperties', () => {
 
   it('should not add a tool tip and set both inConsolidatedGroup and inLeadCase to false when the docket nuimber is not part of a consolidated group', () => {
     const result = addConsolidatedProperties({
-      docketNumber: '101-21',
-      leadDocketNumber: undefined,
+      applicationContext,
+      caseObject: {
+        docketNumber: '101-21',
+        leadDocketNumber: undefined,
+      },
     });
     expect(result).toMatchObject({
       consolidatedIconTooltipText: null,
