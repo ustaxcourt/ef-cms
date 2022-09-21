c1b1de0662e593abac1570d3c3382fb86134eca7
Cody Seibert <cseibert113@gmail.com>
2022-09-21 12:23:21 -0400

    3572 - adding additional unit tests and fixing indent logic

--- shared/src/business/utilities/setConsolidationFlagsForDisplay.js
+++ shared/src/business/utilities/setConsolidationFlagsForDisplay.js
@@ -1,31 +1,27 @@
-exports.setConsolidationFlagsForDisplay = (caseItem, eligibleCases) => {
+exports.setConsolidationFlagsForDisplay = (caseItem, eligibleCases = []) => {
   caseItem.inConsolidatedGroup = caseItem.leadCase = false;
 
-  if (
-    !eligibleCases ||
-    (caseItem.leadDocketNumber &&
-      eligibleCases.find(
+  if (caseItem.leadDocketNumber) {
+    caseItem.inConsolidatedGroup = true;
+    caseItem.consolidatedIconTooltipText = 'Consolidated case';
+    if (caseItem.leadDocketNumber === caseItem.docketNumber) {
+      caseItem.leadCase = true;
+      caseItem.consolidatedIconTooltipText = 'Lead case';
+    } else {
+      const leadCase = eligibleCases.find(
         theCase => theCase.docketNumber === caseItem.leadDocketNumber,
-      ))
-  ) {
-    if (caseItem.leadDocketNumber) {
-      caseItem.inConsolidatedGroup = true;
-      caseItem.consolidatedIconTooltipText = 'Consolidated case';
-      if (caseItem.leadDocketNumber === caseItem.docketNumber) {
-        caseItem.leadCase = true;
-        caseItem.consolidatedIconTooltipText = 'Lead case';
-      } else {
-        const leadCase = eligibleCases.find(
-          theCase => theCase.docketNumber === caseItem.leadDocketNumber,
-        );
+      );
 
-        if (
-          !leadCase.highPriority &&
-          !leadCase.isManuallyAdded &&
-          !leadCase.isDocketSuffixHighPriority
-        ) {
-          caseItem.shouldIndent = true;
-        }
+      if (
+        !!leadCase &&
+        !leadCase.highPriority &&
+        !leadCase.isManuallyAdded &&
+        !leadCase.isDocketSuffixHighPriority &&
+        !caseItem.highPriority &&
+        !caseItem.isManuallyAdded &&
+        !caseItem.isDocketSuffixHighPriority
+      ) {
+        caseItem.shouldIndent = true;
       }
     }
   }
