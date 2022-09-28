e8bd43e8817572372bb3152a7479028ab6d69e8a
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-09-28 08:35:12 -0700

    Devex 1097: Remove logs

--- shared/src/business/entities/cases/Case.js
+++ shared/src/business/entities/cases/Case.js
@@ -1753,7 +1753,6 @@ const updatePetitioner = function (rawCase, updatedPetitioner) {
     p => p.contactId === updatedPetitioner.contactId,
   );
 
-  console.log(rawCase.petitioners, updatedPetitioner, '@@@@@@@@@@@@@@@@@@@@');
   if (petitionerIndex !== -1) {
     rawCase.petitioners[petitionerIndex] = updatedPetitioner;
   } else {
