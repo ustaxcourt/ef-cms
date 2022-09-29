a4e80494c27a6044d46368000b4ef201308ea3c4
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2022-09-29 11:08:51 -0600

    9493: wip adding test to serve paper filing in consolidated group [skip ci]

---
+++ web-client/integration-tests/journey/docketClerkAddsPaperFiledMultiDocketableDocketEntryAndServes.js
@@ -0,0 +1,92 @@
+import { OBJECTIONS_OPTIONS_MAP } from '../../../shared/src/business/entities/EntityConstants';
+import { applicationContextForClient as applicationContext } from '../../../shared/src/business/test/createTestApplicationContext';
+import { contactPrimaryFromState } from '../helpers';
+
+export const docketClerkAddsPaperFiledMultiDocketableDocketEntryAndServes = (
+  cerebralTest,
+  fakeFile,
+  eventCode,
+) => {
+  //add check for modal with cons cases
+  const { DOCUMENT_RELATIONSHIPS } = applicationContext.getConstants();
+
+  return it('docket clerk adds paper filed docket entry and serves', async () => {
+    await cerebralTest.runSequence('gotoCaseDetailSequence', {
+      docketNumber: cerebralTest.docketNumber,
+    });
+
+    await cerebralTest.runSequence('gotoAddPaperFilingSequence', {
+      docketNumber: cerebralTest.docketNumber,
+    });
+
+    await cerebralTest.runSequence('updateScreenMetadataSequence', {
+      key: DOCUMENT_RELATIONSHIPS.SUPPORTING,
+      value: false,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'dateReceivedMonth',
+      value: 4,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'dateReceivedDay',
+      value: 30,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'dateReceivedYear',
+      value: 2001,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'primaryDocumentFile',
+      value: fakeFile,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'primaryDocumentFileSize',
+      value: 100,
+    });
+
+    const contactPrimary = contactPrimaryFromState(cerebralTest);
+
+    await cerebralTest.runSequence(
+      'updateFileDocumentWizardFormValueSequence',
+      {
+        key: `filersMap.${contactPrimary.contactId}`,
+        value: true,
+      },
+    );
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'eventCode',
+      value: eventCode,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'pending',
+      value: true,
+    });
+
+    await cerebralTest.runSequence('updateDocketEntryFormValueSequence', {
+      key: 'objections',
+      value: OBJECTIONS_OPTIONS_MAP.NO,
+    });
+
+    await cerebralTest.runSequence('submitPaperFilingSequence');
+
+    expect(cerebralTest.getState('validationErrors')).toEqual({});
+
+    expect(cerebralTest.getState('alertSuccess').message).toEqual(
+      'Your entry has been added to the docket record.',
+    );
+
+    expect(cerebralTest.getState('currentPage')).toEqual('CaseDetailInternal');
+    expect(cerebralTest.getState('form')).toEqual({});
+
+    cerebralTest.docketEntryId = cerebralTest
+      .getState('caseDetail.docketEntries')
+      .find(doc => doc.eventCode === eventCode).docketEntryId;
+  });
+};
