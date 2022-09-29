354e08aaa1806e1be2f852a596f4ef53746178eb
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2022-09-29 11:08:51 -0600

    9493: wip adding test to serve paper filing in consolidated group [skip ci]

---
+++ web-client/integration-tests/docketClerkAddsPaperFilingOnLeadCase.test.js
@@ -0,0 +1,67 @@
+import { docketClerkConsolidatesCases } from './journey/docketClerkConsolidatesCases';
+import { docketClerkOpensCaseConsolidateModal } from './journey/docketClerkOpensCaseConsolidateModal';
+import { docketClerkSearchesForCaseToConsolidateWith } from './journey/docketClerkSearchesForCaseToConsolidateWith';
+import { docketClerkUpdatesCaseStatusToReadyForTrial } from './journey/docketClerkUpdatesCaseStatusToReadyForTrial';
+import { docketClerkUploadsACourtIssuedDocument } from './journey/docketClerkUploadsACourtIssuedDocument';
+import {
+  fakeFile,
+  getFormattedDocumentQCSectionInProgress,
+  loginAs,
+  refreshElasticsearchIndex,
+  setupTest,
+  uploadPetition,
+} from './helpers';
+import { formattedCaseDetail } from '../src/presenter/computeds/formattedCaseDetail';
+import { runCompute } from 'cerebral/test';
+import { withAppContextDecorator } from '../src/withAppContext';
+
+const cerebralTest = setupTest();
+const trialLocation = `Boise, Idaho, ${Date.now()}`;
+cerebralTest.consolidatedCasesThatShouldReceiveDocketEntries = [];
+
+const overrides = {
+  preferredTrialCity: trialLocation,
+  trialLocation,
+};
+
+let leadDocketNumber;
+let caseDetail;
+
+describe('docket clerk adds paper filing on lead case', () => {
+  beforeAll(() => {
+    jest.setTimeout(30000);
+    cerebralTest.draftOrders = [];
+  });
+
+  afterAll(() => {
+    cerebralTest.closeSocket();
+  });
+
+  it('login as a petitioner and create the lead case', async () => {
+    caseDetail = await uploadPetition(cerebralTest, overrides);
+    expect(caseDetail.docketNumber).toBeDefined();
+    cerebralTest.docketNumber = cerebralTest.leadDocketNumber =
+      caseDetail.docketNumber;
+    leadDocketNumber = caseDetail.docketNumber;
+  });
+
+  loginAs(cerebralTest, 'docketclerk@example.com');
+  docketClerkUpdatesCaseStatusToReadyForTrial(cerebralTest);
+
+  it('login as a petitioner and create a case to consolidate with', async () => {
+    cerebralTest.docketNumberDifferentPlaceOfTrial = null;
+    caseDetail = await uploadPetition(cerebralTest, overrides);
+    expect(caseDetail.docketNumber).toBeDefined();
+    cerebralTest.docketNumber = caseDetail.docketNumber;
+  });
+
+  loginAs(cerebralTest, 'docketclerk@example.com');
+  docketClerkUpdatesCaseStatusToReadyForTrial(cerebralTest);
+  docketClerkOpensCaseConsolidateModal(cerebralTest);
+  docketClerkSearchesForCaseToConsolidateWith(cerebralTest);
+  docketClerkConsolidatesCases(cerebralTest, 2);
+
+  //add paper filing
+  //save and serve
+  //verify served on non lead case
+});
