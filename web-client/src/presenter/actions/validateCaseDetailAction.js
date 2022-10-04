b7c9711198f187e30b7cdcb618c6633a09c9d31c
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-10-04 10:11:10 -0700

    9633: Add steps in cypress integration test to calculate penalties in order to cover bug

--- web-client/src/presenter/actions/validateCaseDetailAction.js
+++ web-client/src/presenter/actions/validateCaseDetailAction.js
@@ -23,7 +23,6 @@ export const validateCaseDetailAction = ({
   store,
 }) => {
   const { formWithComputedDates } = props;
-  console.log('***formWithComputedDates', formWithComputedDates);
   const { INITIAL_DOCUMENT_TYPES_MAP } = applicationContext.getConstants();
 
   const findDocumentByType = type => {
