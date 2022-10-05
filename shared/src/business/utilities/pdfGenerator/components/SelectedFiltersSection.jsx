f6ee2690cfb3154afaf97c022b9970bf14b02a60
Absolutestunna <joshujah007@gmail.com>
2022-10-05 15:26:32 -0400

    9511 - concluded styling for pdf filters of working trial session copy

--- shared/src/business/utilities/pdfGenerator/components/SelectedFiltersSection.jsx
+++ shared/src/business/utilities/pdfGenerator/components/SelectedFiltersSection.jsx
@@ -9,7 +9,6 @@ export const SelectedFiltersSection = ({ count, selectedFilters }) => {
           <div className="align-right">Total Shown: {count}</div>
         </div>
         <div className="filters-selected-content">
-          {/* <div className="card-content filters-selected-content"> */}
           <div className="filters-row">
             <div className="filter">{selectedFilters[0] || ''}</div>
             <div className="filter">{selectedFilters[2] || ''}</div>
@@ -27,44 +26,5 @@ export const SelectedFiltersSection = ({ count, selectedFilters }) => {
         </div>
       </div>
     </>
-    // <>
-    //   <div className="card margin-top-0" id="notes">
-    //     <div className="card-header filters-selected-header">
-    //       <div>Trial Status Filters Selected</div>
-    //       <div className="align-right">Total Shown: {count}</div>
-    //     </div>
-    //     <div className="card-content filters-selected-content">
-    //       <span className="filters-selected-column">
-    //         <div className="filters">
-    //           <div className="filter">{selectedFilters[0] || ''}</div>
-    //           <div className="filter">{selectedFilters[1] || ''}</div>
-    //         </div>
-    //       </span>
-    //       <span className="filters-selected-column">
-    //         <div className="filters">
-    //           <div className="filter">{selectedFilters[2] || ''}</div>
-    //           <div className="filter">{selectedFilters[3] || ''}</div>
-    //         </div>
-    //       </span>
-    //       <span className="filters-selected-column">
-    //         <div className="filters">
-    //           <div className="filter">{selectedFilters[4] || ''}</div>
-    //           <div className="filter">{selectedFilters[5] || ''}</div>
-    //         </div>
-    //       </span>
-    //       <span className="filters-selected-column">
-    //         <div className="filters">
-    //           <div className="filter">{selectedFilters[6] || ''}</div>
-    //           <div className="filter">{selectedFilters[7] || ''}</div>
-    //         </div>
-    //       </span>
-    //       <span className="filters-selected-column">
-    //         <div className="filters">
-    //           <div className="filter">{selectedFilters[8] || ''}</div>
-    //         </div>
-    //       </span>
-    //     </div>
-    //   </div>
-    // </>
   );
 };
