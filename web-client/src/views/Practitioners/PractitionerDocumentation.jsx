efecb99b4893314cc32d6f0dbef137e6c8121c93
Richard Goshen <rick.goshen@gmail.com>
2022-10-06 12:01:21 -0700

    9587-add basic sortableHeader to first column as a test [skip ci]

--- web-client/src/views/Practitioners/PractitionerDocumentation.jsx
+++ web-client/src/views/Practitioners/PractitionerDocumentation.jsx
@@ -7,19 +7,26 @@ export const PractitionerDocumentation = connect(
   {
     constants: state.constants,
     practitionerDetailHelper: state.practitionerDetailHelper,
-    showSortableHeaders: state.showSortableHeaders,
   },
-  function PractitionerDocumentation({
-    constants,
-    practitionerDetailHelper,
-    showSortableHeaders,
-  }) {
+  function PractitionerDocumentation({ constants, practitionerDetailHelper }) {
     return (
       <>
         <table className="usa-table ustc-table subsection">
           <thead>
             <tr>
-              <th>Date Uploaded</th>
+              <th>
+                <th aria-label="Date Uploaded" className="small" colSpan="2">
+                  <SortableColumnHeaderButton
+                    ascText={constants.CHRONOLOGICALLY_ASCENDING}
+                    defaultSort={constants.DESCENDING}
+                    descText={constants.CHRONOLOGICALLY_DESCENDING}
+                    hasRows={true}
+                    sortField="dateUploaded"
+                    title="Date Uploaded"
+                    onClickSequence={() => console.log('clicked!!!')}
+                  />
+                </th>
+              </th>
               <th>File Name</th>
               <th>Category</th>
               <th>Description</th>
