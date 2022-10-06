26ef2edf1d3a509b70326cc00dfabef24ad920b2
Richard Goshen <rick.goshen@gmail.com>
2022-10-06 13:58:35 -0700

    9587-add file placeholder to documentation tab ui [skip ci]

--- web-client/src/views/Practitioners/PractitionerDocumentation.jsx
+++ web-client/src/views/Practitioners/PractitionerDocumentation.jsx
@@ -1,4 +1,5 @@
-import { SortableColumnHeaderButton } from '../../ustc-ui/SortableColumnHeaderButton/SortableColumnHeaderButton';
+// import { SortableColumnHeaderButton } from '../../ustc-ui/SortableColumnHeaderButton/SortableColumnHeaderButton';
+import { Button } from '../../ustc-ui/Button/Button';
 import { connect } from '@cerebral/react';
 import { state } from 'cerebral';
 import React from 'react';
@@ -8,23 +9,25 @@ export const PractitionerDocumentation = connect(
     constants: state.constants,
     practitionerDetailHelper: state.practitionerDetailHelper,
   },
-  function PractitionerDocumentation({ constants, practitionerDetailHelper }) {
+  function PractitionerDocumentation() {
     return (
       <>
+        <div className="display-flex flex-justify-end">
+          <Button
+            link
+            className="push-right margin-bottom-1"
+            href="*"
+            icon="plus"
+          >
+            Add file
+          </Button>
+        </div>
         <table className="usa-table ustc-table subsection">
           <thead>
             <tr>
               <th>
                 <th aria-label="Date Uploaded" className="small" colSpan="2">
-                  <SortableColumnHeaderButton
-                    ascText={constants.CHRONOLOGICALLY_ASCENDING}
-                    defaultSort={constants.DESCENDING}
-                    descText={constants.CHRONOLOGICALLY_DESCENDING}
-                    hasRows={true}
-                    sortField="dateUploaded"
-                    title="Date Uploaded"
-                    onClickSequence={() => console.log('clicked!!!')}
-                  />
+                  Date Uploaded
                 </th>
               </th>
               <th>File Name</th>
