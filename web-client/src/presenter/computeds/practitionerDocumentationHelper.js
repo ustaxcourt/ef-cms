b8b488b61f102c087f3c2ba3fc512536b75e73f6
Richard Goshen <rick.goshen@gmail.com>
2022-10-07 15:54:34 -0700

    9587 - move showDocumentTab to practitionerDetailHelper from practitionerDocumentationHelper [skip ci]

--- web-client/src/presenter/computeds/practitionerDocumentationHelper.js
+++ web-client/src/presenter/computeds/practitionerDocumentationHelper.js
@@ -4,11 +4,13 @@ export const practitionerDocumentationHelper = get => {
   const documentationCategory = get(
     state.screenMetadata.documentationCategoryDropdown.documentationCategory,
   );
+  const permissions = get(state.permissions);
 
   const isCertificateOfGoodStanding =
     documentationCategory === 'Certificate of Good Standing';
 
   return {
     isCertificateOfGoodStanding,
+    showDocumentationTab: permissions.UPLOAD_PRACTITIONER_DOCUMENT,
   };
 };
