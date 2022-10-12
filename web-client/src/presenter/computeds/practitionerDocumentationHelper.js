5a80970e2dac0039022de5a41d4cb6a8dc402a9e
TElliott <telliott@flexion.us>
2022-10-12 17:15:58 -0500

    9588: add sorting logic to practitioner documentation table

--- web-client/src/presenter/computeds/practitionerDocumentationHelper.js
+++ web-client/src/presenter/computeds/practitionerDocumentationHelper.js
@@ -1,16 +1,25 @@
+import { getConstants } from '../../getConstants';
 import { state } from 'cerebral';
+
+const { DESCENDING } = getConstants();
 
 export const practitionerDocumentationHelper = (get, applicationContext) => {
   const permissions = get(state.permissions);
   const practitionerDocuments = get(state.practitionerDocuments);
+  const tableSort = get(state.tableSort);
 
-  const formattedPractitionerDocuments = practitionerDocuments.map(document => {
+  let formattedPractitionerDocuments = practitionerDocuments.map(document => {
     return {
       ...document,
       formattedUploadDate: applicationContext
         .getUtilities()
         .formatDateString(document.uploadDate, 'MMDDYY'),
     };
+  });
+
+  formattedPractitionerDocuments = sortPractitionerDocuments({
+    formattedPractitionerDocuments,
+    tableSort,
   });
 
   const practitionerDocumentsCount = practitionerDocuments.length;
@@ -20,4 +29,24 @@ export const practitionerDocumentationHelper = (get, applicationContext) => {
     practitionerDocumentsCount,
     showDocumentationTab: permissions.UPLOAD_PRACTITIONER_DOCUMENT,
   };
+};
+
+const sortPractitionerDocuments = ({
+  formattedPractitionerDocuments,
+  tableSort,
+}) => {
+  const sortedPractitionerDocuments = formattedPractitionerDocuments.sort(
+    (a, b) => {
+      const sortA = a[tableSort.sortField] || '';
+      const sortB = b[tableSort.sortField] || '';
+
+      return sortA.localeCompare(sortB);
+    },
+  );
+
+  if (tableSort.sortOrder === DESCENDING) {
+    return sortedPractitionerDocuments.reverse();
+  }
+
+  return sortedPractitionerDocuments;
 };
