cb8e991dc8ca489aa8e135143bd83f69341e11bf
TElliott <telliott@flexion.us>
2022-10-12 17:17:35 -0500

    9588: rename `sortMessagesSequence` to `sortTableSequence` to be reusable [skip ci]

--- web-client/src/presenter/presenter.js
+++ web-client/src/presenter/presenter.js
@@ -346,7 +346,7 @@ import { showMoreResultsSequence } from './sequences/showMoreResultsSequence';
 import { showViewPetitionerCounselModalSequence } from './sequences/showViewPetitionerCounselModalSequence';
 import { signOutSequence } from './sequences/signOutSequence';
 import { skipSigningOrderSequence } from './sequences/skipSigningOrderSequence';
-import { sortMessagesSequence } from './sequences/sortMessagesSequence';
+import { sortTableSequence } from './sequences/sortTableSequence';
 import { startRefreshIntervalSequence } from './sequences/startRefreshIntervalSequence';
 import { startScanSequence } from './sequences/startScanSequence';
 import { state } from './state';
@@ -858,7 +858,7 @@ export const presenter = {
     showViewPetitionerCounselModalSequence,
     signOutSequence,
     skipSigningOrderSequence,
-    sortMessagesSequence,
+    sortTableSequence,
     startRefreshIntervalSequence,
     startScanSequence,
     strikeDocketEntrySequence,
