0ca912d5bf314c003041442c483c48a3bc281fae
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2022-10-12 09:11:05 -0600

    9600: WIP adding failing tests [skip ci]

--- web-client/src/presenter/computeds/caseDetailHelper.showConsolidatedCasesCard.test.js
+++ web-client/src/presenter/computeds/caseDetailHelper.showConsolidatedCasesCard.test.js
@@ -1,4 +1,9 @@
-import { adcUser, petitionerUser } from '../../../../shared/src/test/mockUsers';
+import {
+  adcUser,
+  irsPractitionerUser,
+  petitionerUser,
+  privatePractitionerUser,
+} from '../../../../shared/src/test/mockUsers';
 import { applicationContextForClient as applicationContext } from '../../../../shared/src/business/test/createTestApplicationContext';
 import { caseDetailHelper as caseDetailHelperComputed } from './caseDetailHelper';
 import { getUserPermissions } from '../../../../shared/src/authorization/getUserPermissions';
@@ -22,7 +27,7 @@ const getBaseState = user => {
 };
 
 describe('showConsolidatedCasesCard', () => {
-  it('should be true when the user has VIEW_CONSOLIDATED_CASES_CARD permission and the case is in a consolidated group', () => {
+  it('should be true when the user is a petitioner and the case is in a consolidated group', () => {
     const user = petitionerUser;
 
     const result = runCompute(caseDetailHelper, {
@@ -37,7 +42,37 @@ describe('showConsolidatedCasesCard', () => {
     expect(result.showConsolidatedCasesCard).toEqual(true);
   });
 
-  it('should be false when the user does not have VIEW_CONSOLIDATED_CASES_CARD permission and the case is in a consolidated group', () => {
+  it('should be true when the user is a private practitioner and the case is in a consolidated group', () => {
+    const user = privatePractitionerUser;
+
+    const result = runCompute(caseDetailHelper, {
+      state: {
+        ...getBaseState(user),
+        caseDetail: {
+          docketEntries: [],
+          leadDocketNumber: '101-22F',
+        },
+      },
+    });
+    expect(result.showConsolidatedCasesCard).toEqual(true);
+  });
+
+  it('should be true when the user is an IRS practitioner and the case is in a consolidated group', () => {
+    const user = irsPractitionerUser;
+
+    const result = runCompute(caseDetailHelper, {
+      state: {
+        ...getBaseState(user),
+        caseDetail: {
+          docketEntries: [],
+          leadDocketNumber: '101-22F',
+        },
+      },
+    });
+    expect(result.showConsolidatedCasesCard).toEqual(true);
+  });
+
+  it('should be false when the user is a non practitioner/petitioner user and the case is in a consolidated group', () => {
     const user = adcUser;
 
     const result = runCompute(caseDetailHelper, {
