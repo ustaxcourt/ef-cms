99b4089ab4caef4518684c50edacf0b8e2885f25
Richard Goshen <rick.goshen@gmail.com>
2022-10-13 11:27:15 -0700

    9587 - WIP finish unit tests [skip ci]

--- shared/src/business/useCases/getPractitionerDocumentDownloadUrlInteractor.ts
+++ shared/src/business/useCases/getPractitionerDocumentDownloadUrlInteractor.ts
@@ -8,7 +8,6 @@ import { UnauthorizedError } from '../../errors/errors';
  *
  * @param {object} applicationContext the application context
  * @param {object} providers the providers object
- * @param {string} providers.barNumber the bar number of the practitioner
  * @param {string} providers.practitionerDocumentFileId the key of the document
  * @returns {Array<string>} the filing type options based on user role
  */
