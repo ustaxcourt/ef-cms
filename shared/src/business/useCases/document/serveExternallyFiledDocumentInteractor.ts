0c65fec77bbe1d4f789920ee0015a00cfa87fca1
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2022-10-18 11:31:20 -0600

    9493: making new workitem fr non lead case [skip ci]

--- shared/src/business/useCases/document/serveExternallyFiledDocumentInteractor.ts
+++ shared/src/business/useCases/document/serveExternallyFiledDocumentInteractor.ts
@@ -6,10 +6,11 @@ const {
   ROLE_PERMISSIONS,
 } = require('../../../authorization/authorizationClientService');
 const { addCoverToPdf } = require('../addCoverToPdf');
-const { ALLOWLIST_FEATURE_FLAGS } = require('../../entities/EntityConstants');
+const { ALLOWLIST_FEATURE_FLAGS, DOCKET_SECTION } = require('../../entities/EntityConstants');
 const { Case } = require('../../entities/cases/Case');
 const { createISODateString } = require('../../utilities/DateHandler');
 const { DocketEntry } = require('../../entities/DocketEntry');
+const { WorkItem } = require('../../entities/WorkItem');
 const { NotFoundError, UnauthorizedError } = require('../../../errors/errors');
 const { omit } = require('lodash');
 
@@ -253,6 +254,34 @@ const fileDocumentOnOneCase = async ({
   docketEntryEntity.setAsServed(servedParties.all).validate();
   docketEntryEntity.setAsProcessingStatusAsCompleted();
 
+  const subjectCaseDocketNumber = originalSubjectDocketEntry.docketNumber;
+
+  const isSubjectCase = subjectCaseDocketNumber === caseEntity.docketNumber;
+
+  if (!docketEntryEntity.workItem || !isSubjectCase) {
+    docketEntryEntity.workItem = new WorkItem(
+      {
+        assigneeId: null,
+        assigneeName: null,
+        associatedJudge: caseEntity.associatedJudge,
+        caseStatus: caseEntity.status,
+        caseTitle: Case.getCaseTitle(caseEntity.caseCaption),
+        docketEntry: {
+          ...docketEntryEntity.toRawObject(),
+          createdAt: docketEntryEntity.createdAt,
+        },
+        docketNumber: caseEntity.docketNumber,
+        docketNumberWithSuffix: caseEntity.docketNumberWithSuffix,
+        hideFromPendingMessages: true,
+        inProgress: true,
+        section: DOCKET_SECTION,
+        sentBy: user.name,
+        sentByUserId: user.userId,
+      },
+      { applicationContext },
+    );
+  }
+
   const workItemToUpdate = docketEntryEntity.workItem;
 
   if (workItemToUpdate) {
@@ -312,7 +341,7 @@ const fileDocumentOnOneCase = async ({
     .getUseCaseHelpers()
     .updateCaseAndAssociations({
       applicationContext,
-      caseToUpdate: caseEntity,
+      caseToUpdate: caseEntity.validate(),
     });
 
   return {
