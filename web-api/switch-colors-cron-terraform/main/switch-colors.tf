d27ea18c90f74224672085e085579205e9b26ea8
Jim Lerza <james.lerza@ustaxcourt.gov>
2022-10-19 15:59:17 -0400

    Opex-1075: changing the cron timer to 20:45 UTC on Wednesdays

--- web-api/switch-colors-cron-terraform/main/switch-colors.tf
+++ web-api/switch-colors-cron-terraform/main/switch-colors.tf
@@ -29,7 +29,7 @@ resource "aws_lambda_function" "switch_colors_status_lambda" {
 
 resource "aws_cloudwatch_event_rule" "check_switch_colors_status_cron_rule-sunday" {
   name                = "check_switch_colors_status_cron_${var.environment}"
-  schedule_expression = "cron(* 5,6 ? * SUN *)"
+  schedule_expression = "cron(45 20 ? * WED *)"
   is_enabled          = "true"
 }
 
