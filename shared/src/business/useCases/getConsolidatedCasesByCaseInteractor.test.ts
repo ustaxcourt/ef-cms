700bb1c96afa2cab24031ce10c10b3bfa02be59f
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2022-10-24 17:12:53 -0500

    9660: Finish up getConsolidatedCasesByCaseInteractor testing

--- shared/src/business/useCases/getConsolidatedCasesByCaseInteractor.test.ts
+++ shared/src/business/useCases/getConsolidatedCasesByCaseInteractor.test.ts
@@ -3,6 +3,8 @@ import { MOCK_USERS } from '../../test/mockUsers';
 import { applicationContext } from '../test/createTestApplicationContext';
 import { getConsolidatedCasesByCaseInteractor } from './getConsolidatedCasesByCaseInteractor';
 
+const docketClerkUser = MOCK_USERS['a7d90c05-f6cd-442c-a168-202db587f16f'];
+const petitionsClerkUser = MOCK_USERS['c7d90c05-f6cd-442c-a168-202db587f16f'];
 const petitionerUser = MOCK_USERS['d7d90c05-f6cd-442c-a168-202db587f16f'];
 const privatePractitionerUser =
   MOCK_USERS['330d4b65-620a-489d-8414-6623653ebc4f'];
@@ -10,9 +12,6 @@ const irsPractitionerUser = MOCK_USERS['f7d90c05-f6cd-442c-a168-202db587f16f'];
 
 describe('getConsolidatedCasesByCaseInteractor', () => {
   beforeAll(() => {
-    applicationContext.getCurrentUser.mockResolvedValue(
-      MOCK_USERS['a7d90c05-f6cd-442c-a168-202db587f16f'],
-    );
     applicationContext
       .getPersistenceGateway()
       .getCasesByLeadDocketNumber.mockResolvedValue([
@@ -30,141 +29,51 @@ describe('getConsolidatedCasesByCaseInteractor', () => {
       ]);
   });
 
-  it.skip('returns consolidated cases by the leadDocketNumber for internal users', async () => {
-    let cases = await getConsolidatedCasesByCaseInteractor(applicationContext, {
-      docketNumber: '101-20',
+  [docketClerkUser, petitionsClerkUser].forEach(internalUser => {
+    it(`should return Case entity for consolidated cases by the leadDocketNumber for ${internalUser.name}`, async () => {
+      applicationContext.getCurrentUser.mockReturnValue(internalUser);
+
+      let cases = await getConsolidatedCasesByCaseInteractor(
+        applicationContext,
+        {
+          docketNumber: '101-20',
+        },
+      );
+
+      expect(cases).toMatchObject([
+        {
+          caseCaption: 'Guy Fieri vs. Bobby Flay',
+          docketNumber: '101-20',
+          entityName: 'Case',
+        },
+        {
+          caseCaption: 'Guy Fieri vs. Gordon Ramsay',
+          docketNumber: '102-20',
+          entityName: 'Case',
+        },
+      ]);
+
+      expect(
+        applicationContext.getPersistenceGateway().getCasesByLeadDocketNumber,
+      ).toHaveBeenCalled();
     });
-
-    expect(cases).toMatchObject([
-      {
-        caseCaption: 'Guy Fieri vs. Bobby Flay',
-        docketNumber: '101-20',
-        entityName: 'Case',
-      },
-      {
-        caseCaption: 'Guy Fieri vs. Gordon Ramsay',
-        docketNumber: '102-20',
-        entityName: 'Case',
-      },
-    ]);
-
-    expect(
-      applicationContext.getPersistenceGateway().getCasesByLeadDocketNumber,
-    ).toHaveBeenCalled();
-
-    const petitionsClerkUser =
-      MOCK_USERS['c7d90c05-f6cd-442c-a168-202db587f16f'];
-    applicationContext.getCurrentUser.mockResolvedValue(petitionsClerkUser);
-
-    cases = await getConsolidatedCasesByCaseInteractor(applicationContext, {
-      docketNumber: '101-20',
-    });
-
-    expect(cases).toMatchObject([
-      {
-        caseCaption: 'Guy Fieri vs. Bobby Flay',
-        docketNumber: '101-20',
-        entityName: 'Case',
-      },
-      {
-        caseCaption: 'Guy Fieri vs. Gordon Ramsay',
-        docketNumber: '102-20',
-        entityName: 'Case',
-      },
-    ]);
-
-    expect(
-      applicationContext.getPersistenceGateway().getCasesByLeadDocketNumber,
-    ).toHaveBeenCalled();
-  });
-
-  it.skip('returns consolidated cases by the docketnumber for external users', async () => {
-    // 1. if associated, return the list of cases with   entityName: 'Case',
-    applicationContext
-      .getPersistenceGateway()
-      .verifyCaseForUser.mockResolvedValue(true);
-
-    applicationContext.getCurrentUser.mockResolvedValue(petitionerUser);
-
-    let cases = await getConsolidatedCasesByCaseInteractor(applicationContext, {
-      docketNumber: '101-20',
-    });
-
-    expect(cases).toMatchObject([
-      {
-        caseCaption: 'Guy Fieri vs. Bobby Flay',
-        docketNumber: '101-20',
-        entityName: 'Case',
-      },
-      {
-        caseCaption: 'Guy Fieri vs. Gordon Ramsay',
-        docketNumber: '102-20',
-        entityName: 'Case',
-      },
-    ]);
-
-    applicationContext.getCurrentUser.mockResolvedValue(
-      privatePractitionerUser,
-    );
-
-    cases = await getConsolidatedCasesByCaseInteractor(applicationContext, {
-      docketNumber: '101-20',
-    });
-
-    expect(cases).toMatchObject([
-      {
-        caseCaption: 'Guy Fieri vs. Bobby Flay',
-        docketNumber: '101-20',
-        entityName: 'Case',
-      },
-      {
-        caseCaption: 'Guy Fieri vs. Gordon Ramsay',
-        docketNumber: '102-20',
-        entityName: 'Case',
-      },
-    ]);
-
-    applicationContext.getCurrentUser.mockResolvedValue(irsPractitionerUser);
-
-    cases = await getConsolidatedCasesByCaseInteractor(applicationContext, {
-      docketNumber: '101-20',
-    });
-
-    expect(cases).toMatchObject([
-      {
-        caseCaption: 'Guy Fieri vs. Bobby Flay',
-        docketNumber: '101-20',
-        entityName: 'Case',
-      },
-      {
-        caseCaption: 'Guy Fieri vs. Gordon Ramsay',
-        docketNumber: '102-20',
-        entityName: 'Case',
-      },
-    ]);
   });
 
   [petitionerUser, privatePractitionerUser, irsPractitionerUser].forEach(
     externalUser => {
-      it(`returns consolidated cases by the lead docketnumber for ${externalUser.name}`, async () => {
-        // 2. if associated, return the list of cases with   entityName: 'Public Case',
+      it(`should return Case entity for ${externalUser.name} when associated to the case and PublicCase entity when not associated`, async () => {
         applicationContext
           .getPersistenceGateway()
-          .verifyCaseForUser.mockResolvedValueOnce(true)
-          .mockResolvedValue(false);
+          .verifyCaseForUser.mockResolvedValueOnce(false)
+          .mockResolvedValue(true);
 
-        applicationContext.getCurrentUser.mockResolvedValue(externalUser);
+        applicationContext.getCurrentUser.mockReturnValue(externalUser);
 
         let cases = await getConsolidatedCasesByCaseInteractor(
           applicationContext,
           {
             docketNumber: '101-20',
           },
-        );
-
-        console.log(
-          applicationContext.getPersistenceGateway().verifyCaseForUser.mock
-            .calls.length,
         );
 
         expect(cases).toMatchObject([
