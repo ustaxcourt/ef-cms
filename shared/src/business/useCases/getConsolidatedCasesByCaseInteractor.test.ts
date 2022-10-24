1171e53ffc71fefb635ab755263aeaad9a78c15a
Timothy Shumway <tshumway@flexion.us>
2022-10-24 15:47:59 -0600

    bug-9660 - WIP unit test [skip ci]

--- shared/src/business/useCases/getConsolidatedCasesByCaseInteractor.test.ts
+++ shared/src/business/useCases/getConsolidatedCasesByCaseInteractor.test.ts
@@ -1,13 +1,12 @@
 import { MOCK_CASE } from '../../test/mockCase';
-import {
-  MOCK_EXTERNAL_USERS,
-  MOCK_INTERNAL_USERS,
-  MOCK_USERS,
-} from '../../test/mockUsers';
-import { ROLES } from '../entities/EntityConstants';
-import { User } from '../entities/User';
+import { MOCK_USERS } from '../../test/mockUsers';
 import { applicationContext } from '../test/createTestApplicationContext';
 import { getConsolidatedCasesByCaseInteractor } from './getConsolidatedCasesByCaseInteractor';
+
+const petitionerUser = MOCK_USERS['d7d90c05-f6cd-442c-a168-202db587f16f'];
+const privatePractitionerUser =
+  MOCK_USERS['330d4b65-620a-489d-8414-6623653ebc4f'];
+const irsPractitionerUser = MOCK_USERS['f7d90c05-f6cd-442c-a168-202db587f16f'];
 
 describe('getConsolidatedCasesByCaseInteractor', () => {
   beforeAll(() => {
@@ -26,11 +25,12 @@ describe('getConsolidatedCasesByCaseInteractor', () => {
           ...MOCK_CASE,
           caseCaption: 'Guy Fieri vs. Gordon Ramsay',
           docketNumber: '102-20',
+          leadDocketNumber: '101-20',
         },
       ]);
   });
 
-  it('returns consolidated cases by the leadDocketNumber for internal users', async () => {
+  it.skip('returns consolidated cases by the leadDocketNumber for internal users', async () => {
     let cases = await getConsolidatedCasesByCaseInteractor(applicationContext, {
       docketNumber: '101-20',
     });
@@ -80,34 +80,106 @@ describe('getConsolidatedCasesByCaseInteractor', () => {
 
   it.skip('returns consolidated cases by the docketnumber for external users', async () => {
     // 1. if associated, return the list of cases with   entityName: 'Case',
-    const externalUsers = Object.keys(MOCK_EXTERNAL_USERS);
     applicationContext
       .getPersistenceGateway()
       .verifyCaseForUser.mockResolvedValue(true);
-    externalUsers.forEach(async extUser => {
-      applicationContext.getCurrentUser.mockResolvedValue(extUser);
 
-      const cases = await getConsolidatedCasesByCaseInteractor(
-        applicationContext,
-        {
-          docketNumber: '101-20',
-        },
-      );
+    applicationContext.getCurrentUser.mockResolvedValue(petitionerUser);
 
-      expect(cases).toMatchObject([
-        {
-          caseCaption: 'Guy Fieri vs. Bobby Flay',
-          docketNumber: '101-20',
-          entityName: 'Case',
-        },
-        {
-          caseCaption: 'Guy Fieri vs. Gordon Ramsay',
-          docketNumber: '102-20',
-          entityName: 'Case',
-        },
-      ]);
+    let cases = await getConsolidatedCasesByCaseInteractor(applicationContext, {
+      docketNumber: '101-20',
     });
 
-    // 2. if associated, return the list of cases with   entityName: 'Public Case',
+    expect(cases).toMatchObject([
+      {
+        caseCaption: 'Guy Fieri vs. Bobby Flay',
+        docketNumber: '101-20',
+        entityName: 'Case',
+      },
+      {
+        caseCaption: 'Guy Fieri vs. Gordon Ramsay',
+        docketNumber: '102-20',
+        entityName: 'Case',
+      },
+    ]);
+
+    applicationContext.getCurrentUser.mockResolvedValue(
+      privatePractitionerUser,
+    );
+
+    cases = await getConsolidatedCasesByCaseInteractor(applicationContext, {
+      docketNumber: '101-20',
+    });
+
+    expect(cases).toMatchObject([
+      {
+        caseCaption: 'Guy Fieri vs. Bobby Flay',
+        docketNumber: '101-20',
+        entityName: 'Case',
+      },
+      {
+        caseCaption: 'Guy Fieri vs. Gordon Ramsay',
+        docketNumber: '102-20',
+        entityName: 'Case',
+      },
+    ]);
+
+    applicationContext.getCurrentUser.mockResolvedValue(irsPractitionerUser);
+
+    cases = await getConsolidatedCasesByCaseInteractor(applicationContext, {
+      docketNumber: '101-20',
+    });
+
+    expect(cases).toMatchObject([
+      {
+        caseCaption: 'Guy Fieri vs. Bobby Flay',
+        docketNumber: '101-20',
+        entityName: 'Case',
+      },
+      {
+        caseCaption: 'Guy Fieri vs. Gordon Ramsay',
+        docketNumber: '102-20',
+        entityName: 'Case',
+      },
+    ]);
   });
+
+  [petitionerUser, privatePractitionerUser, irsPractitionerUser].forEach(
+    externalUser => {
+      it(`returns consolidated cases by the lead docketnumber for ${externalUser.name}`, async () => {
+        // 2. if associated, return the list of cases with   entityName: 'Public Case',
+        applicationContext
+          .getPersistenceGateway()
+          .verifyCaseForUser.mockResolvedValueOnce(true)
+          .mockResolvedValue(false);
+
+        applicationContext.getCurrentUser.mockResolvedValue(externalUser);
+
+        let cases = await getConsolidatedCasesByCaseInteractor(
+          applicationContext,
+          {
+            docketNumber: '101-20',
+          },
+        );
+
+        console.log(
+          applicationContext.getPersistenceGateway().verifyCaseForUser.mock
+            .calls.length,
+        );
+
+        expect(cases).toMatchObject([
+          {
+            caseCaption: 'Guy Fieri vs. Bobby Flay',
+            docketNumber: '101-20',
+            entityName: 'PublicCase',
+          },
+          {
+            caseCaption: 'Guy Fieri vs. Gordon Ramsay',
+            docketNumber: '102-20',
+            entityName: 'Case',
+          },
+        ]);
+      });
+    },
+  );
 });
