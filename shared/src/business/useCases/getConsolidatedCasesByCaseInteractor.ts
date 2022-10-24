86a5d01535ffc08c2bf7cef47a7b05269680ffa2
Timothy Shumway <tshumway@flexion.us>
2022-10-24 09:10:09 -0600

    bugfix-9660 - clean up comments [skip ci]

--- shared/src/business/useCases/getConsolidatedCasesByCaseInteractor.ts
+++ shared/src/business/useCases/getConsolidatedCasesByCaseInteractor.ts
@@ -15,7 +15,6 @@ export const getConsolidatedCasesByCaseInteractor = async (
   { docketNumber }: { docketNumber: string },
 ) => {
   const user = applicationContext.getCurrentUser();
-  console.log('user*** ', user);
 
   const consolidatedCases = await applicationContext
     .getPersistenceGateway()
@@ -63,49 +62,3 @@ export const getConsolidatedCasesByCaseInteractor = async (
   }
   return validatedConsolidatedCases;
 };
-
-// we need to know what cases we are a party to
-//    - array of case objects
-// for the cases we aren't associated with, we call getPublicCaseInteractor
-//    - import the public case validator helper function
-
-// const associatedCases = [];
-// const nonAssociatedCases = [];
-
-// for (let index = 0; index < consolidatedCasesForUser.length; index++) {
-//   const conCase = consolidatedCasesForUser[index];
-//   console.log('conCase.docketNumber', conCase.docketNumber);
-
-//   const isAssociated = await applicationContext
-//     .getPersistenceGateway()
-//     .verifyCaseForUser({
-//       applicationContext,
-//       docketNumber: conCase.docketNumber,
-//       userId: user.userId,
-//     });
-//   if (isAssociated) {
-//     associatedCases.push(conCase);
-//   } else {
-//     nonAssociatedCases.push(conCase);
-//   }
-// }
-
-// const nonAssociatedCasesData = nonAssociatedCases.map(conCase =>
-//   formatPublicCase({
-//     applicationContext,
-//     docketNumber: conCase.docketNumber,
-//     rawCaseRecord: conCase,
-//   }),
-// );
-// const associatedCasesData = Case.validateRawCollection(associatedCases, {
-//   applicationContext,
-// });
-
-// console.log('associatedCasesData*****', associatedCasesData);
-
-// return [...nonAssociatedCasesData, ...associatedCasesData];
-
-// console.log('nonAssociatedCases*****', nonAssociatedCases);
-// console.log('associatedCases*****', associatedCases);
-
-// console.log('consolidatedCasesForUser ********', consolidatedCasesForUser);
