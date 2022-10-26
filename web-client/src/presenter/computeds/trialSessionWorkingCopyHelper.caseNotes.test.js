0954789e9f3bf3a903e1b45e267db44ff744c7a9
Rosie Highsmith <ahighsmith@flexion.us>
2022-10-26 08:12:51 -0700

    bug-9648: move reused mock variables outside of each individual test

--- web-client/src/presenter/computeds/trialSessionWorkingCopyHelper.caseNotes.test.js
+++ web-client/src/presenter/computeds/trialSessionWorkingCopyHelper.caseNotes.test.js
@@ -28,12 +28,12 @@ describe('trial session working copy computed', () => {
     trialLocation: 'Hartford, Connecticut',
   };
 
-  it('should set calendarNotes on cases that have them', () => {
-    const mockCaseWithCalendarNotes = '102-19';
-    const mockCaseWithoutCalendarNotes = '5000-17';
-    const mockCaseWithCalendarNotesNotInSelectedTrialSession = '999-99';
-    const mockCalendarNote = 'this is a test note';
+  const mockCaseWithNotes = '102-19';
+  const mockCaseWithoutCalendarNotes = '5000-17';
+  const mockCaseWithNotesNotInSelectedTrialSession = '999-99';
+  const mockNote = 'this is a test note';
 
+  it('should set calendarNotes on cases that have them', () => {
     const { formattedCases } = runCompute(trialSessionWorkingCopyHelper, {
       state: {
         constants: {
@@ -44,17 +44,17 @@ describe('trial session working copy computed', () => {
           ...MOCK_TRIAL_SESSION,
           calendaredCases: [
             MOCK_CASE,
-            { ...MOCK_CASE, docketNumber: mockCaseWithCalendarNotes },
+            { ...MOCK_CASE, docketNumber: mockCaseWithNotes },
             { ...MOCK_CASE, docketNumber: mockCaseWithoutCalendarNotes },
           ],
           caseOrder: [
             {
-              calendarNotes: mockCalendarNote,
-              docketNumber: mockCaseWithCalendarNotes,
+              calendarNotes: mockNote,
+              docketNumber: mockCaseWithNotes,
             },
             {
-              calendarNotes: mockCalendarNote,
-              docketNumber: mockCaseWithCalendarNotesNotInSelectedTrialSession,
+              calendarNotes: mockNote,
+              docketNumber: mockCaseWithNotesNotInSelectedTrialSession,
             },
           ],
         },
@@ -70,9 +70,9 @@ describe('trial session working copy computed', () => {
 
     expect(
       formattedCases.find(
-        ({ docketNumber }) => docketNumber === mockCaseWithCalendarNotes,
+        ({ docketNumber }) => docketNumber === mockCaseWithNotes,
       ).calendarNotes,
-    ).toEqual(mockCalendarNote);
+    ).toEqual(mockNote);
     expect(
       formattedCases.find(
         ({ docketNumber }) => docketNumber === mockCaseWithoutCalendarNotes,
@@ -81,10 +81,7 @@ describe('trial session working copy computed', () => {
   });
 
   it('should set userNotes on cases that are calendared on the trial session when they have them', () => {
-    const mockCaseWithUserNotes = '102-19';
     const mockCaseWithoutUserNotes = '90-07';
-    const mockCaseWithUserNotesNotInSelectedTrialSession = '999-99';
-    const mockUserNote = 'this is a test note';
 
     const { formattedCases } = runCompute(trialSessionWorkingCopyHelper, {
       state: {
@@ -95,12 +92,12 @@ describe('trial session working copy computed', () => {
         trialSession: {
           ...MOCK_TRIAL_SESSION,
           calendaredCases: [
-            { ...MOCK_CASE, docketNumber: mockCaseWithUserNotes },
+            { ...MOCK_CASE, docketNumber: mockCaseWithNotes },
             { ...MOCK_CASE, docketNumber: mockCaseWithoutUserNotes },
           ],
           caseOrder: [
             {
-              docketNumber: mockCaseWithUserNotes,
+              docketNumber: mockCaseWithNotes,
             },
           ],
         },
@@ -110,8 +107,8 @@ describe('trial session working copy computed', () => {
           sort: 'docket',
           sortOrder: 'asc',
           userNotes: {
-            [mockCaseWithUserNotes]: { notes: mockUserNote },
-            [mockCaseWithUserNotesNotInSelectedTrialSession]: { notes: 'blah' },
+            [mockCaseWithNotes]: { notes: mockNote },
+            [mockCaseWithNotesNotInSelectedTrialSession]: { notes: 'blah' },
           },
         },
       },
@@ -119,9 +116,9 @@ describe('trial session working copy computed', () => {
 
     expect(
       formattedCases.find(
-        ({ docketNumber }) => docketNumber === mockCaseWithUserNotes,
+        ({ docketNumber }) => docketNumber === mockCaseWithNotes,
       ).userNotes,
-    ).toEqual(mockUserNote);
+    ).toEqual(mockNote);
     expect(
       formattedCases.find(
         ({ docketNumber }) => docketNumber === mockCaseWithoutUserNotes,
