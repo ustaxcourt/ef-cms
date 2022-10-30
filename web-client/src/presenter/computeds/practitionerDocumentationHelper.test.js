bd5e0bad1852dad81bf0e1c2936c41fe9c04ed44
TElliott <telliott@flexion.us>
2022-10-30 16:18:59 -0500

    9588: add missing unit tests

--- web-client/src/presenter/computeds/practitionerDocumentationHelper.test.js
+++ web-client/src/presenter/computeds/practitionerDocumentationHelper.test.js
@@ -1,10 +1,15 @@
 import { PRACTITIONER_DOCUMENT_TYPES_MAP } from '../../../../shared/src/business/entities/EntityConstants';
 import { applicationContext } from '../../applicationContext';
+import { getConstants } from '../../getConstants';
 import { practitionerDocumentationHelper as practitionerDocumentationHelperComputed } from './practitionerDocumentationHelper';
 import { runCompute } from 'cerebral/test';
 import { withAppContextDecorator } from '../../withAppContext';
 
+const { ASCENDING, DESCENDING } = getConstants();
+
 describe('practitionerDetailHelper', () => {
+  const baseTableSort = { sortField: 'uploadDate', sortOrder: ASCENDING };
+
   const practitionerDocumentationHelper = withAppContextDecorator(
     practitionerDocumentationHelperComputed,
     {
@@ -13,17 +18,6 @@ describe('practitionerDetailHelper', () => {
   );
 
   const mockPractitionerDocuments = [
-    {
-      categoryName: `${PRACTITIONER_DOCUMENT_TYPES_MAP.CERTIFICATE_OF_GOOD_STANDING} - Colorado`,
-      categoryType:
-        PRACTITIONER_DOCUMENT_TYPES_MAP.CERTIFICATE_OF_GOOD_STANDING,
-      description: 'test',
-      entityName: 'Document',
-      fileName: 'test_file.pdf',
-      location: 'Colorado',
-      practitionerDocumentFileId: '18525e5b-c336-479d-a854-1712432ab8ba',
-      uploadDate: '2022-10-10T21:58:40.729Z',
-    },
     {
       categoryName: PRACTITIONER_DOCUMENT_TYPES_MAP.APPLICATION,
       categoryType: PRACTITIONER_DOCUMENT_TYPES_MAP.APPLICATION,
@@ -42,6 +36,17 @@ describe('practitionerDetailHelper', () => {
       practitionerDocumentFileId: 'c58dcf54-6be7-48b2-a129-6ec20e6a0a73',
       uploadDate: '2021-10-10T22:24:30.010Z',
     },
+    {
+      categoryName: `${PRACTITIONER_DOCUMENT_TYPES_MAP.CERTIFICATE_OF_GOOD_STANDING} - Colorado`,
+      categoryType:
+        PRACTITIONER_DOCUMENT_TYPES_MAP.CERTIFICATE_OF_GOOD_STANDING,
+      description: 'test',
+      entityName: 'Document',
+      fileName: 'test_file.pdf',
+      location: 'Colorado',
+      practitionerDocumentFileId: '18525e5b-c336-479d-a854-1712432ab8ba',
+      uploadDate: '2022-10-10T21:58:40.729Z',
+    },
   ];
 
   describe('showDocumentationTab', () => {
@@ -54,6 +59,7 @@ describe('practitionerDetailHelper', () => {
               UPLOAD_PRACTITIONER_DOCUMENT: true,
             },
             practitionerDocuments: [],
+            tableSort: baseTableSort,
             user: { role: 'admissionsclerk' },
           },
         },
@@ -70,6 +76,7 @@ describe('practitionerDetailHelper', () => {
               UPLOAD_PRACTITIONER_DOCUMENT: false,
             },
             practitionerDocuments: [],
+            tableSort: baseTableSort,
             user: { role: 'admissionsclerk' },
           },
         },
@@ -86,12 +93,13 @@ describe('practitionerDetailHelper', () => {
             UPLOAD_PRACTITIONER_DOCUMENT: false,
           },
           practitionerDocuments: [mockPractitionerDocuments[0]],
+          tableSort: baseTableSort,
           user: { role: 'admissionsclerk' },
         },
       });
       expect(
         results.formattedPractitionerDocuments[0].formattedUploadDate,
-      ).toEqual('10/10/22');
+      ).toEqual('10/10/20');
     });
 
     it('should correctly sort documents by uploadDate ascending', () => {
@@ -101,6 +109,7 @@ describe('practitionerDetailHelper', () => {
             UPLOAD_PRACTITIONER_DOCUMENT: false,
           },
           practitionerDocuments: [...mockPractitionerDocuments],
+          tableSort: baseTableSort,
           user: { role: 'admissionsclerk' },
         },
       });
@@ -127,6 +136,7 @@ describe('practitionerDetailHelper', () => {
             UPLOAD_PRACTITIONER_DOCUMENT: false,
           },
           practitionerDocuments: mockPractitionerDocuments,
+          tableSort: baseTableSort,
           user: { role: 'admissionsclerk' },
         },
       });
@@ -143,11 +153,54 @@ describe('practitionerDetailHelper', () => {
             UPLOAD_PRACTITIONER_DOCUMENT: false,
           },
           practitionerDocuments: [],
+          tableSort: baseTableSort,
           user: { role: 'admissionsclerk' },
         },
       });
 
       expect(results.practitionerDocumentsCount).toEqual(0);
+    });
+  });
+
+  describe('sortPractitionerDocuments', () => {
+    it('should sort practitioner documents by uploadDate, descending', () => {
+      const tableSort = { sortField: 'uploadDate', sortOrder: DESCENDING };
+      const results = runCompute(practitionerDocumentationHelper, {
+        state: {
+          permissions: {
+            UPLOAD_PRACTITIONER_DOCUMENT: false,
+          },
+          practitionerDocuments: mockPractitionerDocuments,
+          tableSort,
+          user: { role: 'admissionsclerk' },
+        },
+      });
+
+      expect(results.formattedPractitionerDocuments).toEqual([
+        { ...mockPractitionerDocuments[2], formattedUploadDate: '10/10/22' },
+        { ...mockPractitionerDocuments[1], formattedUploadDate: '10/10/21' },
+        { ...mockPractitionerDocuments[0], formattedUploadDate: '10/10/20' },
+      ]);
+    });
+
+    it('should sort practitioner documents by fileName, ascending', () => {
+      const tableSort = { sortField: 'fileName', sortOrder: ASCENDING };
+      const results = runCompute(practitionerDocumentationHelper, {
+        state: {
+          permissions: {
+            UPLOAD_PRACTITIONER_DOCUMENT: false,
+          },
+          practitionerDocuments: mockPractitionerDocuments,
+          tableSort,
+          user: { role: 'admissionsclerk' },
+        },
+      });
+
+      expect(results.formattedPractitionerDocuments).toEqual([
+        { ...mockPractitionerDocuments[1], formattedUploadDate: '10/10/21' },
+        { ...mockPractitionerDocuments[0], formattedUploadDate: '10/10/20' },
+        { ...mockPractitionerDocuments[2], formattedUploadDate: '10/10/22' },
+      ]);
     });
   });
 });
