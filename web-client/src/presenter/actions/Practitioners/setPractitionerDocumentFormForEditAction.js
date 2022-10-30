f73963cd4ed263ce4fe7ec0764353144fafb48f0
TElliott <telliott@flexion.us>
2022-10-30 16:19:58 -0500

    9588: simplify setting practitioner document info on form

--- web-client/src/presenter/actions/Practitioners/setPractitionerDocumentFormForEditAction.js
+++ web-client/src/presenter/actions/Practitioners/setPractitionerDocumentFormForEditAction.js
@@ -11,14 +11,7 @@ import { state } from 'cerebral';
 export const setPractitionerDocumentFormForEditAction = ({ props, store }) => {
   const { barNumber, practitionerDocument } = props;
 
-  store.set(state.form.categoryType, practitionerDocument.categoryType);
-  store.set(state.form.fileName, practitionerDocument.fileName);
-  store.set(state.form.description, practitionerDocument.description);
-  store.set(state.form.location, practitionerDocument.location);
-  store.set(
-    state.form.practitionerDocumentFileId,
-    practitionerDocument.practitionerDocumentFileId,
-  );
+  store.set(state.form, practitionerDocument);
   store.set(state.form.barNumber, barNumber);
   store.set(state.form.showPractitionerDocumentLink, true);
   store.set(state.form.isEditingDocument, true);
