a6805a2ffeb60fbfba4d189500e3a1d0a77bfc34
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-10-31 11:37:13 -0700

    9681: Add test coverage for service stamp use case helper

--- shared/src/business/useCaseHelper/stampDocumentForService.test.js
+++ shared/src/business/useCaseHelper/stampDocumentForService.test.js
@@ -1,46 +1,53 @@
 const {
-  applicationContext,
-  testPdfDoc,
-} = require('../test/createTestApplicationContext');
-const {
-  MOCK_CASE,
-  MOCK_LEAD_CASE_WITH_PAPER_SERVICE,
-} = require('../../test/mockCase');
-const { Case, getContactPrimary } = require('../entities/cases/Case');
-const { SERVICE_INDICATOR_TYPES } = require('../entities/EntityConstants');
+  ENTERED_AND_SERVED_EVENT_CODES,
+} = require('../entities/courtIssuedDocument/CourtIssuedDocumentConstants');
+const { applicationContext } = require('../test/createTestApplicationContext');
 const { stampDocumentForService } = require('./stampDocumentForService');
 
 describe('stampDocumentForService', () => {
-  let caseEntity;
+  beforeEach(() => {});
 
-  const mockPdfUrl = 'www.example.com';
-  const mockDocketEntryId = 'cf105788-5d34-4451-aa8d-dfd9a851b675';
-
-  beforeEach(() => {
-    caseEntity = new Case(MOCK_CASE, { applicationContext });
-
-    applicationContext.getStorageClient().getObject.mockReturnValue({
-      promise: () => ({
-        Body: testPdfDoc,
-      }),
+  it('should set `Served` as the serviceStampType when the documentType is NOT order and the document eventCode is not one of ENTERED_AND_SERVED_EVENT_CODES', async () => {
+    await stampDocumentForService({
+      applicationContext,
+      documentToStamp: {
+        documentType: 'Motion to Withdraw as Counsel',
+        eventCode: 'M112',
+      },
+      pdfData: {},
     });
 
-    applicationContext
-      .getPersistenceGateway()
-      .getDownloadPolicyUrl.mockReturnValue({ url: mockPdfUrl });
+    expect(
+      applicationContext.getUseCaseHelpers().addServedStampToDocument.mock
+        .calls[0][0].serviceStampText,
+    ).toContain('Served');
+  });
+
+  it('should set serviceStampType from the document when the documentType is Order', async () => {
+    const mockServiceStamp = 'This document is urgent!';
+
+    await stampDocumentForService({
+      applicationContext,
+      documentToStamp: {
+        documentType: 'Order',
+        serviceStamp: mockServiceStamp,
+      },
+      pdfData: {},
+    });
+
+    expect(
+      applicationContext.getUseCaseHelpers().addServedStampToDocument.mock
+        .calls[0][0].serviceStampText,
+    ).toContain(mockServiceStamp);
   });
 
   it('should include `Entered and Served` in the serviceStampType when the eventCode is in ENTERED_AND_SERVED_EVENT_CODES', async () => {
-    await fileAndServeCourtIssuedDocumentInteractor(applicationContext, {
-      clientConnectionId: mockClientConnectionId,
-      docketEntryId: caseRecord.docketEntries[0].docketEntryId,
-      docketNumbers: [caseRecord.docketNumber],
-      form: {
-        ...caseRecord.docketEntries[0],
-        documentType: 'Notice',
-        eventCode: 'ODJ',
+    await stampDocumentForService({
+      applicationContext,
+      documentToStamp: {
+        eventCode: ENTERED_AND_SERVED_EVENT_CODES[0],
       },
-      subjectCaseDocketNumber: caseRecord.docketNumber,
+      pdfData: {},
     });
 
     expect(
