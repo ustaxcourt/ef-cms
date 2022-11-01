8ac89ade446e1841284a52851537215af0ce205b
TElliott <telliott@flexion.us>
2022-11-01 11:01:56 -0500

    9588: fix unit test

--- shared/src/persistence/dynamo/practitioners/editPractitionerDocument.test.ts
+++ shared/src/persistence/dynamo/practitioners/editPractitionerDocument.test.ts
@@ -30,7 +30,6 @@ describe('editPractitionerDocument', () => {
         sk: `document|${practitionerDocumentFileId}`,
         ...practitionerDocument,
       },
-      applicationContext,
     });
   });
 });
