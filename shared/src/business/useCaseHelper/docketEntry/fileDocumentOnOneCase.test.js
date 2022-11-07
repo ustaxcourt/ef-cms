bf368cc4798e385a76dcc280830357283dfb919e
Timothy Shumway <tshumway@flexion.us>
2022-11-07 11:50:19 -0700

    9681: WIP interactor test now green, expand test for fileDocumentOnOneCase

--- shared/src/business/useCaseHelper/docketEntry/fileDocumentOnOneCase.test.js
+++ shared/src/business/useCaseHelper/docketEntry/fileDocumentOnOneCase.test.js
@@ -295,6 +295,30 @@ describe('fileDocumentOnOneCase', () => {
     expect(Case.prototype.updateDocketEntry).toHaveBeenCalled();
   });
 
+  it('should add an index to the docketEntry on the caseEntity', async () => {
+    const result = await fileDocumentOnOneCase({
+      applicationContext,
+      caseEntity: mockCaseEntity,
+      docketEntryEntity: new DocketEntry(
+        {
+          ...mockDocketEntry,
+          index: undefined,
+        },
+        { applicationContext },
+      ),
+      subjectCaseDocketNumber: mockCaseEntity.docketNumber,
+      user: docketClerkUser,
+    });
+
+    expect(Case.prototype.updateDocketEntry).toHaveBeenCalled();
+    expect(
+      result.docketEntries.find(
+        docketEntry =>
+          docketEntry.docketEntryId === mockDocketEntry.docketEntryId,
+      ).index,
+    ).toBeDefined();
+  });
+
   it('should add the docketEntry on the caseEntity when it did NOT already exist on the case', async () => {
     await fileDocumentOnOneCase({
       applicationContext,
