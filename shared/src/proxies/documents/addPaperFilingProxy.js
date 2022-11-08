28ca6436e379acdb5a6237e4cdd62e9f67bcc6eb
Rosie Highsmith <ahighsmith@flexion.us>
2022-11-08 07:22:45 -0700

    9681: combine data and clientConnectionId into one object

--- shared/src/proxies/documents/addPaperFilingProxy.js
+++ shared/src/proxies/documents/addPaperFilingProxy.js
@@ -5,24 +5,16 @@ const { post } = require('../requests');
  *
  * @param {object} applicationContext the application context
  * @param {object} providers the providers object
- * @param {object} providers.clientConnectionId the client connection id
  * @param {object} providers.data the data being forwarded to the API call
  * @returns {Promise<*>} the promise of the api call
  */
-exports.addPaperFilingInteractor = (
-  applicationContext,
-  data,
-  clientConnectionId,
-) => {
+exports.addPaperFilingInteractor = (applicationContext, data) => {
   const { documentMetadata } = data;
   const { docketNumber } = documentMetadata;
 
   return post({
     applicationContext,
-    body: {
-      clientConnectionId,
-      ...data,
-    },
+    body: data,
     endpoint: `/case-documents/${docketNumber}/paper-filing`,
   });
 };
--- shared/src/proxies/serveCourtIssuedDocumentProxy.js
+++ shared/src/proxies/serveCourtIssuedDocumentProxy.js
@@ -5,20 +5,15 @@ const { post } = require('./requests');
  *
  * @param {object} applicationContext the application context
  * @param {object} providers the providers object
- * @param {object} providers.clientConnectionId the client connection id
  * @param {object} providers.data the data being forwarded to the API call
  * @returns {Promise<*>} the promise of the api call
  */
-exports.serveCourtIssuedDocumentInteractor = (
-  applicationContext,
-  data,
-  clientConnectionId,
-) => {
+exports.serveCourtIssuedDocumentInteractor = (applicationContext, data) => {
   const { docketEntryId, subjectCaseDocketNumber } = data;
 
   return post({
     applicationContext,
-    body: { clientConnectionId, ...data },
+    body: data,
     endpoint: `/case-documents/${subjectCaseDocketNumber}/${docketEntryId}/serve-court-issued`,
   });
 };
