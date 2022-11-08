21bf0fffbce0cfbb72e210b1c26035adca2a22c2
Rosie Highsmith <ahighsmith@flexion.us>
2022-11-08 07:22:45 -0700

    9681: combine data and clientConnectionId into one object

--- shared/src/proxies/documents/fileAndServeCourtIssuedDocumentProxy.js
+++ shared/src/proxies/documents/fileAndServeCourtIssuedDocumentProxy.js
@@ -5,22 +5,17 @@ const { post } = require('../requests');
  *
  * @param {object} applicationContext the application context
  * @param {object} providers the providers object
- * @param {object} providers.clientConnectionId the client connection id
  * @param {object} providers.data the data being forwarded to the API call
  * @returns {Promise<*>} the promise of the API call
  */
 exports.fileAndServeCourtIssuedDocumentInteractor = (
   applicationContext,
   data,
-  clientConnectionId,
 ) => {
   const { subjectCaseDocketNumber } = data;
   return post({
     applicationContext,
-    body: {
-      clientConnectionId,
-      ...data,
-    },
+    body: data,
     endpoint: `/async/case-documents/${subjectCaseDocketNumber}/file-and-serve-court-issued-docket-entry`,
   });
 };
