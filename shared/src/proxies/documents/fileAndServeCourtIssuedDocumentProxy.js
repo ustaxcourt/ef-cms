4fa7dd866049c9a686acbdf1ccf638bb1f98a44e
Rosie Highsmith <ahighsmith@flexion.us>
2022-11-07 18:04:12 -0700

    9681: remove expectation that checks for an arg passed in

--- shared/src/proxies/documents/fileAndServeCourtIssuedDocumentProxy.js
+++ shared/src/proxies/documents/fileAndServeCourtIssuedDocumentProxy.js
@@ -4,6 +4,7 @@ const { post } = require('../requests');
  * fileAndServeCourtIssuedDocumentInteractor
  *
  * @param {object} applicationContext the application context
+ * @param {object} providers the providers object
  * @param {object} providers.clientConnectionId the client connection id
  * @param {object} providers.data the data being forwarded to the API call
  * @returns {Promise<*>} the promise of the API call
