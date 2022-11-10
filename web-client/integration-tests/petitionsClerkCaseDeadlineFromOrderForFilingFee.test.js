bfabd5e090d8c9120eb60cec4c4fc14c8ddfd34d
Timothy Shumway <tshumway@flexion.us>
2022-11-10 16:39:17 -0700

    9644: more testing in second flow

--- web-client/integration-tests/petitionsClerkCaseDeadlineFromOrderForFilingFee.test.js
+++ web-client/integration-tests/petitionsClerkCaseDeadlineFromOrderForFilingFee.test.js
@@ -10,7 +10,7 @@ import { petitionsClerkReviewsPaperCaseBeforeServing } from './journey/petitions
 import { runCompute } from 'cerebral/test';
 import { withAppContextDecorator } from '../src/withAppContext';
 
-describe('Petitions Clerk something', () => {
+describe('Autogenerate Deadline when order for filing fee is served', () => {
   const cerebralTest = setupTest();
   cerebralTest.draftOrders = [];
   const caseDetailHelper = withAppContextDecorator(caseDetailHelperComputed);
@@ -27,168 +27,327 @@ describe('Petitions Clerk something', () => {
     cerebralTest.closeSocket();
   });
 
-  loginAs(cerebralTest, 'petitionsclerk@example.com');
-  petitionsClerkCreatesNewCaseFromPaper(cerebralTest, fakeFile, {
-    paymentStatus: PAYMENT_STATUS.UNPAID,
+  describe('1st Flow FIX ME', () => {
+    loginAs(cerebralTest, 'petitionsclerk@example.com');
+    petitionsClerkCreatesNewCaseFromPaper(cerebralTest, fakeFile, {
+      paymentStatus: PAYMENT_STATUS.UNPAID,
+    });
+
+    petitionsClerkReviewsPaperCaseBeforeServing(cerebralTest, {
+      hasIrsNoticeFormatted: 'No',
+      ordersAndNoticesInDraft: [
+        'Order Designating Place of Trial',
+        'Order for Filing Fee',
+      ],
+      ordersAndNoticesNeeded: ['Order for Ratification of Petition'],
+      petitionPaymentStatusFormatted: PAYMENT_STATUS.UNPAID,
+      receivedAtFormatted: '01/01/01',
+      shouldShowIrsNoticeDate: false,
+    });
+
+    it('petitions clerk serves paper petition', async () => {
+      await cerebralTest.runSequence('gotoCaseDetailSequence', {
+        docketNumber: cerebralTest.docketNumber,
+      });
+
+      const petitionDocketEntryId = cerebralTest
+        .getState('caseDetail.docketEntries')
+        .find(d => d.eventCode === 'P').docketEntryId;
+
+      await cerebralTest.runSequence('gotoPetitionQcSequence', {
+        docketNumber: cerebralTest.docketNumber,
+        redirectUrl: `/case-detail/${cerebralTest.docketNumber}/document-view?docketEntryId=${petitionDocketEntryId}`,
+      });
+
+      await cerebralTest.runSequence('updateFormValueSequence', {
+        key: 'hasVerifiedIrsNotice',
+        value: false,
+      });
+
+      expect(cerebralTest.getState('currentPage')).toEqual('PetitionQc');
+
+      await cerebralTest.runSequence('saveSavedCaseForLaterSequence');
+
+      expect(cerebralTest.getState('currentPage')).toEqual(
+        'ReviewSavedPetition',
+      );
+
+      await cerebralTest.runSequence('openConfirmServeToIrsModalSequence');
+
+      await cerebralTest.runSequence('serveCaseToIrsSequence');
+
+      expect(cerebralTest.getState('currentPage')).toEqual(
+        'PrintPaperPetitionReceipt',
+      );
+    });
+
+    loginAs(cerebralTest, 'docketclerk@example.com');
+
+    it('should view the draft order and sign it', async () => {
+      await cerebralTest.runSequence('gotoCaseDetailSequence', {
+        docketNumber: cerebralTest.docketNumber,
+      });
+
+      const docketEntries = cerebralTest.getState('caseDetail.docketEntries');
+      const draftOrderForFilingFeeDocketEntry = docketEntries.find(
+        doc =>
+          doc.eventCode ===
+          SYSTEM_GENERATED_DOCUMENT_TYPES.orderForFilingFee.eventCode,
+      );
+
+      expect(draftOrderForFilingFeeDocketEntry).toBeTruthy();
+
+      cerebralTest.draftDocketEntryId =
+        draftOrderForFilingFeeDocketEntry.docketEntryId;
+
+      await cerebralTest.runSequence('gotoSignOrderSequence', {
+        docketEntryId: cerebralTest.draftDocketEntryId,
+        docketNumber: cerebralTest.docketNumber,
+      });
+
+      await cerebralTest.runSequence('setPDFSignatureDataSequence', {
+        signatureData: {
+          scale: 1,
+          x: 100,
+          y: 100,
+        },
+      });
+      await cerebralTest.runSequence('saveDocumentSigningSequence');
+    });
+
+    it('docket clerk adds a docket entry for order for filing fee and serves it', async () => {
+      await cerebralTest.runSequence('gotoAddCourtIssuedDocketEntrySequence', {
+        docketEntryId: cerebralTest.draftDocketEntryId,
+        docketNumber: cerebralTest.docketNumber,
+      });
+
+      await cerebralTest.runSequence(
+        'updateCourtIssuedDocketEntryFormValueSequence',
+        {
+          key: 'month',
+          value: '2',
+        },
+      );
+
+      await cerebralTest.runSequence(
+        'updateCourtIssuedDocketEntryFormValueSequence',
+        {
+          key: 'day',
+          value: '2',
+        },
+      );
+
+      await cerebralTest.runSequence(
+        'updateCourtIssuedDocketEntryFormValueSequence',
+        {
+          key: 'year',
+          value: '2050',
+        },
+      );
+
+      await cerebralTest.runSequence(
+        'fileAndServeCourtIssuedDocumentFromDocketEntrySequence',
+      );
+
+      expect(cerebralTest.getState('validationErrors')).toEqual({});
+
+      await waitForCondition({
+        booleanExpressionCondition: () =>
+          cerebralTest.getState('currentPage') === 'PrintPaperService',
+      });
+
+      expect(cerebralTest.getState('currentPage')).toEqual('PrintPaperService');
+      expect(cerebralTest.getState('alertSuccess').message).toEqual(
+        'Document served. ',
+      );
+    });
+
+    it('docket clerk verifies there is a new case deadline with date from previous step and correct description', async () => {
+      await cerebralTest.runSequence('gotoCaseDetailSequence', {
+        docketNumber: cerebralTest.docketNumber,
+      });
+
+      const helper = runCompute(caseDetailHelper, {
+        state: cerebralTest.getState(),
+      });
+
+      expect(helper.caseDeadlines[0].deadlineDate).toEqual(
+        '2050-02-02T05:00:00.000Z',
+      );
+      expect(helper.caseDeadlines[0].description).toEqual(
+        FILING_FEE_DEADLINE_DESCRIPTION,
+      );
+    });
+
+    //create a new paper case with filing fee not paid
+    //serve the case
+    //verify OF in drafts
+    //sign OF
+    //add docket entry from OF
+    //serve docket entry
+    //verify there is a new case deadline with date from previous step and correct description
   });
 
-  petitionsClerkReviewsPaperCaseBeforeServing(cerebralTest, {
-    hasIrsNoticeFormatted: 'No',
-    ordersAndNoticesInDraft: [
-      'Order Designating Place of Trial',
-      'Order for Filing Fee',
-    ],
-    ordersAndNoticesNeeded: ['Order for Ratification of Petition'],
-    petitionPaymentStatusFormatted: PAYMENT_STATUS.UNPAID,
-    receivedAtFormatted: '01/01/01',
-    shouldShowIrsNoticeDate: false,
+  describe('2nd Flow FIX ME', () => {
+    loginAs(cerebralTest, 'petitionsclerk@example.com');
+    petitionsClerkCreatesNewCaseFromPaper(cerebralTest, fakeFile, {
+      paymentStatus: PAYMENT_STATUS.UNPAID,
+    });
+
+    petitionsClerkReviewsPaperCaseBeforeServing(cerebralTest, {
+      hasIrsNoticeFormatted: 'No',
+      ordersAndNoticesInDraft: [
+        'Order Designating Place of Trial',
+        'Order for Filing Fee',
+      ],
+      ordersAndNoticesNeeded: ['Order for Ratification of Petition'],
+      petitionPaymentStatusFormatted: PAYMENT_STATUS.UNPAID,
+      receivedAtFormatted: '01/01/01',
+      shouldShowIrsNoticeDate: false,
+    });
+
+    it('petitions clerk serves paper petition', async () => {
+      await cerebralTest.runSequence('gotoCaseDetailSequence', {
+        docketNumber: cerebralTest.docketNumber,
+      });
+
+      const petitionDocketEntryId = cerebralTest
+        .getState('caseDetail.docketEntries')
+        .find(d => d.eventCode === 'P').docketEntryId;
+
+      await cerebralTest.runSequence('gotoPetitionQcSequence', {
+        docketNumber: cerebralTest.docketNumber,
+        redirectUrl: `/case-detail/${cerebralTest.docketNumber}/document-view?docketEntryId=${petitionDocketEntryId}`,
+      });
+
+      await cerebralTest.runSequence('updateFormValueSequence', {
+        key: 'hasVerifiedIrsNotice',
+        value: false,
+      });
+
+      expect(cerebralTest.getState('currentPage')).toEqual('PetitionQc');
+
+      await cerebralTest.runSequence('saveSavedCaseForLaterSequence');
+
+      expect(cerebralTest.getState('currentPage')).toEqual(
+        'ReviewSavedPetition',
+      );
+
+      await cerebralTest.runSequence('openConfirmServeToIrsModalSequence');
+
+      await cerebralTest.runSequence('serveCaseToIrsSequence');
+
+      expect(cerebralTest.getState('currentPage')).toEqual(
+        'PrintPaperPetitionReceipt',
+      );
+    });
+
+    loginAs(cerebralTest, 'docketclerk@example.com');
+
+    it('should view the draft order and sign it', async () => {
+      await cerebralTest.runSequence('gotoCaseDetailSequence', {
+        docketNumber: cerebralTest.docketNumber,
+      });
+
+      const docketEntries = cerebralTest.getState('caseDetail.docketEntries');
+      const draftOrderForFilingFeeDocketEntry = docketEntries.find(
+        doc =>
+          doc.eventCode ===
+          SYSTEM_GENERATED_DOCUMENT_TYPES.orderForFilingFee.eventCode,
+      );
+
+      expect(draftOrderForFilingFeeDocketEntry).toBeTruthy();
+
+      cerebralTest.draftDocketEntryId =
+        draftOrderForFilingFeeDocketEntry.docketEntryId;
+
+      await cerebralTest.runSequence('gotoSignOrderSequence', {
+        docketEntryId: cerebralTest.draftDocketEntryId,
+        docketNumber: cerebralTest.docketNumber,
+      });
+
+      await cerebralTest.runSequence('setPDFSignatureDataSequence', {
+        signatureData: {
+          scale: 1,
+          x: 100,
+          y: 100,
+        },
+      });
+      await cerebralTest.runSequence('saveDocumentSigningSequence');
+    });
+
+    it('docket clerk adds a docket entry for order for filing fee and saves it', async () => {
+      await cerebralTest.runSequence('gotoAddCourtIssuedDocketEntrySequence', {
+        docketEntryId: cerebralTest.draftDocketEntryId,
+        docketNumber: cerebralTest.docketNumber,
+      });
+
+      await cerebralTest.runSequence(
+        'updateCourtIssuedDocketEntryFormValueSequence',
+        {
+          key: 'month',
+          value: '2',
+        },
+      );
+
+      await cerebralTest.runSequence(
+        'updateCourtIssuedDocketEntryFormValueSequence',
+        {
+          key: 'day',
+          value: '2',
+        },
+      );
+
+      await cerebralTest.runSequence(
+        'updateCourtIssuedDocketEntryFormValueSequence',
+        {
+          key: 'year',
+          value: '2050',
+        },
+      );
+
+      await cerebralTest.runSequence('submitCourtIssuedDocketEntrySequence');
+
+      expect(cerebralTest.getState('validationErrors')).toEqual({});
+
+      await waitForCondition({
+        booleanExpressionCondition: () =>
+          cerebralTest.getState('currentPage') === 'CaseDetailInternal',
+      });
+
+      expect(cerebralTest.getState('currentPage')).toEqual(
+        'CaseDetailInternal',
+      );
+      expect(cerebralTest.getState('alertSuccess').message).toEqual(
+        'Your entry has been added to the docket record.',
+      );
+    });
+
+    it('serve the saved order', async () => {});
+
+    it('docket clerk verifies there is a new case deadline with date from previous step and correct description', async () => {
+      await cerebralTest.runSequence('gotoCaseDetailSequence', {
+        docketNumber: cerebralTest.docketNumber,
+      });
+
+      const helper = runCompute(caseDetailHelper, {
+        state: cerebralTest.getState(),
+      });
+
+      expect(helper.caseDeadlines[0].deadlineDate).toEqual(
+        '2050-02-02T05:00:00.000Z',
+      );
+      expect(helper.caseDeadlines[0].description).toEqual(
+        FILING_FEE_DEADLINE_DESCRIPTION,
+      );
+    });
   });
-
-  it('petitions clerk serves paper petition', async () => {
-    await cerebralTest.runSequence('gotoCaseDetailSequence', {
-      docketNumber: cerebralTest.docketNumber,
-    });
-
-    const petitionDocketEntryId = cerebralTest
-      .getState('caseDetail.docketEntries')
-      .find(d => d.eventCode === 'P').docketEntryId;
-
-    await cerebralTest.runSequence('gotoPetitionQcSequence', {
-      docketNumber: cerebralTest.docketNumber,
-      redirectUrl: `/case-detail/${cerebralTest.docketNumber}/document-view?docketEntryId=${petitionDocketEntryId}`,
-    });
-
-    await cerebralTest.runSequence('updateFormValueSequence', {
-      key: 'hasVerifiedIrsNotice',
-      value: false,
-    });
-
-    expect(cerebralTest.getState('currentPage')).toEqual('PetitionQc');
-
-    await cerebralTest.runSequence('saveSavedCaseForLaterSequence');
-
-    expect(cerebralTest.getState('currentPage')).toEqual('ReviewSavedPetition');
-
-    await cerebralTest.runSequence('openConfirmServeToIrsModalSequence');
-
-    await cerebralTest.runSequence('serveCaseToIrsSequence');
-
-    expect(cerebralTest.getState('currentPage')).toEqual(
-      'PrintPaperPetitionReceipt',
-    );
-  });
-
-  loginAs(cerebralTest, 'docketclerk@example.com');
-
-  it('should view the draft order and sign it', async () => {
-    await cerebralTest.runSequence('gotoCaseDetailSequence', {
-      docketNumber: cerebralTest.docketNumber,
-    });
-
-    const docketEntries = cerebralTest.getState('caseDetail.docketEntries');
-    const draftOrderForFilingFeeDocketEntry = docketEntries.find(
-      doc =>
-        doc.eventCode ===
-        SYSTEM_GENERATED_DOCUMENT_TYPES.orderForFilingFee.eventCode,
-    );
-
-    expect(draftOrderForFilingFeeDocketEntry).toBeTruthy();
-
-    cerebralTest.draftDocketEntryId =
-      draftOrderForFilingFeeDocketEntry.docketEntryId;
-
-    await cerebralTest.runSequence('gotoSignOrderSequence', {
-      docketEntryId: cerebralTest.draftDocketEntryId,
-      docketNumber: cerebralTest.docketNumber,
-    });
-
-    await cerebralTest.runSequence('setPDFSignatureDataSequence', {
-      signatureData: {
-        scale: 1,
-        x: 100,
-        y: 100,
-      },
-    });
-    await cerebralTest.runSequence('saveDocumentSigningSequence');
-  });
-
-  it('docket clerk adds a docket entry for order for filing fee and serves it', async () => {
-    await cerebralTest.runSequence('gotoAddCourtIssuedDocketEntrySequence', {
-      docketEntryId: cerebralTest.draftDocketEntryId,
-      docketNumber: cerebralTest.docketNumber,
-    });
-
-    await cerebralTest.runSequence(
-      'updateCourtIssuedDocketEntryFormValueSequence',
-      {
-        key: 'month',
-        value: '2',
-      },
-    );
-
-    await cerebralTest.runSequence(
-      'updateCourtIssuedDocketEntryFormValueSequence',
-      {
-        key: 'day',
-        value: '2',
-      },
-    );
-
-    await cerebralTest.runSequence(
-      'updateCourtIssuedDocketEntryFormValueSequence',
-      {
-        key: 'year',
-        value: '2050',
-      },
-    );
-
-    await cerebralTest.runSequence(
-      'fileAndServeCourtIssuedDocumentFromDocketEntrySequence',
-    );
-
-    expect(cerebralTest.getState('validationErrors')).toEqual({});
-
-    await waitForCondition({
-      booleanExpressionCondition: () =>
-        cerebralTest.getState('currentPage') === 'PrintPaperService',
-    });
-
-    expect(cerebralTest.getState('currentPage')).toEqual('PrintPaperService');
-    expect(cerebralTest.getState('alertSuccess').message).toEqual(
-      'Document served. ',
-    );
-  });
-
-  it('docket clerk verifies there is a new case deadline with date from previous step and correct description', async () => {
-    await cerebralTest.runSequence('gotoCaseDetailSequence', {
-      docketNumber: cerebralTest.docketNumber,
-    });
-
-    const helper = runCompute(caseDetailHelper, {
-      state: cerebralTest.getState(),
-    });
-
-    expect(helper.caseDeadlines[0].deadlineDate).toEqual(
-      '2050-02-02T05:00:00.000Z',
-    );
-    expect(helper.caseDeadlines[0].description).toEqual(
-      FILING_FEE_DEADLINE_DESCRIPTION,
-    );
-  });
-
-  //create a new paper case with filing fee not paid
-  //serve the case
-  //verify OF in drafts
-  //sign OF
-  //add docket entry from OF
-  //serve docket entry
-  //verify there is a new case deadline with date from previous step and correct description
-
-  //create a new paper case with filing fee not paid
-  //serve the case
-  //sign OF
-  //add docket entry from OF
-  //save for later
-  //serve docket entry from document viewer
-  //verify there is a new case deadline with date from previous step and correct description
 });
+
+//create a new paper case with filing fee not paid
+//serve the case
+//sign OF
+//add docket entry from OF
+//save for later
+//serve docket entry from document viewer
+//verify there is a new case deadline with date from previous step and correct description
