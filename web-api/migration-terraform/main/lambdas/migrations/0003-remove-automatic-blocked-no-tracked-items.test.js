2d80b992c90a742654a56d261c3c19e3d567bc93
Rosie Highsmith <ahighsmith@flexion.us>
2022-11-14 12:44:29 -0800

    9628: WIP test [skip ci]

---
+++ web-api/migration-terraform/main/lambdas/migrations/0003-remove-automatic-blocked-no-tracked-items.test.js
@@ -0,0 +1,60 @@
+const {
+  AUTOMATIC_BLOCKED_REASONS,
+} = require('../../../../../shared/src/business/entities/EntityConstants');
+const {
+  migrateItems,
+} = require('./0003-remove-automatic-blocked-no-tracked-items');
+const { MOCK_CASE } = require('../../../../../shared/src/test/mockCase');
+
+describe('migrateItems', () => {
+  let mockCaseItem;
+  let documentClient;
+
+  beforeEach(() => {
+    mockCaseItem = {
+      ...MOCK_CASE,
+      automaticBlocked: true,
+      automaticBlockedDate: '2020-03-01T00:00:00.000Z',
+      automaticBlockedReason: AUTOMATIC_BLOCKED_REASONS.pendingAndDueDate,
+      docketEntries: [],
+      pk: `case|${MOCK_CASE.docketNumber}`,
+      sk: `case|${MOCK_CASE.docketNumber}`,
+      trialDate: '2020-03-01T00:00:00.000Z',
+    };
+  });
+
+  it('should return and not modify records that are NOT case records', async () => {
+    const items = [
+      {
+        pk: `case|${MOCK_CASE.docketNumber}`,
+        sk: 'user|6d74eadc-0181-4ff5-826c-305200e8733d',
+      },
+      {
+        pk: `case|${MOCK_CASE.docketNumber}`,
+        sk: 'docket-entry|83b77e98-4cf6-4fb4-b8c0-f5f90fd68f3c',
+      },
+    ];
+    const results = await migrateItems(items, documentClient);
+
+    expect(results).toEqual([
+      {
+        pk: `case|${MOCK_CASE.docketNumber}`,
+        sk: 'user|6d74eadc-0181-4ff5-826c-305200e8733d',
+      },
+      {
+        pk: `case|${MOCK_CASE.docketNumber}`,
+        sk: 'docket-entry|83b77e98-4cf6-4fb4-b8c0-f5f90fd68f3c',
+      },
+    ]);
+  });
+
+  it('should update automaticBlocked fields when the record is a case entity with a trial date and no pending items or case deadlines', async () => {
+    const items = [mockCaseItem];
+
+    const results = await migrateItems(items, documentClient);
+
+    expect(results[0].automaticBlocked).toBeFalsy();
+    expect(results[0].automaticBlockedDate).toBeUndefined();
+    expect(results[0].automaticBlockedReason).toBeUndefined();
+  });
+});
