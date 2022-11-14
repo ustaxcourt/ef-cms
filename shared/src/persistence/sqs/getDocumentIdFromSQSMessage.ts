c74144dad65485cf1d23d2a90575646ec0e66325
Cody Seibert <cseibert113@gmail.com>
2022-11-14 11:37:58 -0500

    devex-ts - refactor sqs

--- shared/src/persistence/sqs/getDocumentIdFromSQSMessage.ts
+++ shared/src/persistence/sqs/getDocumentIdFromSQSMessage.ts
@@ -1,6 +1,6 @@
-exports.getDocumentIdFromSQSMessage = message => {
+export const getDocumentIdFromSQSMessage = (message: { Body: string }) => {
   const { Body: body } = message;
   const parsedBody = JSON.parse(body);
-  const documentId = parsedBody.Records[0].s3.object.key;
+  const documentId: string = parsedBody.Records[0].s3.object.key;
   return documentId;
 };
