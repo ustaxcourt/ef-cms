7cc93fa03fdddcedac599eba0abda32dff8f0a25
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-11-21 15:34:48 -0800

    9715: Extract utility method into its own file, need to work on test coverage [skip ci]

--- web-client/src/presenter/actions/openPractitionerDocumentDownloadUrlAction.js
+++ web-client/src/presenter/actions/openPractitionerDocumentDownloadUrlAction.js
@@ -1,18 +1,3 @@
-export const openUrlInNewTab = async getUrlCb => {
-  let url;
-
-  const openFileViewerWindow = window.open();
-
-  try {
-    ({ url } = await getUrlCb());
-  } catch (err) {
-    throw new Error(`Unable to get document download url. ${err.message}`);
-  }
-
-  openFileViewerWindow.document.write('Loading your document...');
-  openFileViewerWindow.location.href = url;
-};
-
 /**
  * opens the practitioner document in a new tab
  *
@@ -26,7 +11,7 @@ export const openPractitionerDocumentDownloadUrlAction = async ({
 }) => {
   const { barNumber, practitionerDocumentFileId } = props;
 
-  await openUrlInNewTab(() =>
+  await applicationContext.getUtilities().openUrlInNewTab(() =>
     applicationContext
       .getUseCases()
       .getPractitionerDocumentDownloadUrlInteractor(applicationContext, {
--- web-client/src/presenter/actions/openPractitionerDocumentDownloadUrlAction.test.js
+++ web-client/src/presenter/actions/openPractitionerDocumentDownloadUrlAction.test.js
@@ -1,74 +1,25 @@
 import { applicationContextForClient as applicationContext } from '../../../../shared/src/business/test/createTestApplicationContext';
-import {
-  openPractitionerDocumentDownloadUrlAction,
-  openUrlInNewTab,
-} from './openPractitionerDocumentDownloadUrlAction';
+import { openPractitionerDocumentDownloadUrlAction } from './openPractitionerDocumentDownloadUrlAction';
 import { presenter } from '../presenter-mock';
 import { runAction } from 'cerebral/test';
 
 describe('openPractitionerDocumentDownloadUrlAction', () => {
-  const writeSpy = jest.fn();
+  const mockBarNumber = 'PT1234';
 
-  beforeEach(() => {
-    window.open = jest.fn().mockReturnValue({
-      document: {
-        write: writeSpy,
-      },
-      location: { href: '' },
-    });
+  presenter.providers.applicationContext = applicationContext;
 
-    presenter.providers.applicationContext = applicationContext;
-
-    applicationContext
-      .getUseCases()
-      .getPractitionerDocumentDownloadUrlInteractor.mockResolvedValue({
-        url: 'http://example.com',
-      });
-  });
-
-  it('should open a new tab with the downloaded file', async () => {
+  it('should open a new tab with the downloaded practitioner document', async () => {
     await runAction(openPractitionerDocumentDownloadUrlAction, {
       modules: { presenter },
       props: {
-        barNumber: 'PT1234',
+        barNumber: mockBarNumber,
         fileName: 'file.png',
         practitionerDocumentFileId: 'mockFileId1234',
       },
     });
 
-    expect(window.open().location.href).toEqual('http://example.com');
-  });
-});
-
-describe('openUrlInNewTab', () => {
-  const closeSpy = jest.fn();
-  const writeSpy = jest.fn();
-
-  beforeEach(() => {
-    window.open = jest.fn().mockReturnValue({
-      close: closeSpy,
-      document: {
-        write: writeSpy,
-      },
-      location: { href: '' },
-    });
-  });
-
-  it('should open a new tab before fetching the url to open', async () => {
-    try {
-      await openUrlInNewTab(() => {
-        throw new Error();
-      });
-    } catch (e) {
-      expect(window.open).toHaveBeenCalled();
-    }
-  });
-
-  it('should throw an error if url is invalid', async () => {
-    await expect(
-      openUrlInNewTab(() => {
-        throw new Error();
-      }),
-    ).rejects.toThrow();
+    expect(
+      applicationContext.getUtilities().openUrlInNewTab,
+    ).toHaveBeenCalled();
   });
 });
