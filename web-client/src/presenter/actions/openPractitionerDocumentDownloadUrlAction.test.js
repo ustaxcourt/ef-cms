c0820de686a5c70baec090f047eaf03b3361218d
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-11-21 16:53:19 -0800

    9715: Simplify openUrlInNewTab, pull dawson logic out into action so it only makes browser specific calls

--- web-client/src/presenter/actions/openPractitionerDocumentDownloadUrlAction.test.js
+++ web-client/src/presenter/actions/openPractitionerDocumentDownloadUrlAction.test.js
@@ -5,21 +5,65 @@ import { runAction } from 'cerebral/test';
 
 describe('openPractitionerDocumentDownloadUrlAction', () => {
   const mockBarNumber = 'PT1234';
+  const mockFileId = '5e6b3a82-07d3-44a3-935d-9cad2d97b998';
+  const mockDownloadUrl = 'www.example.com';
 
-  presenter.providers.applicationContext = applicationContext;
+  beforeEach(() => {
+    presenter.providers.applicationContext = applicationContext;
 
-  it('should open a new tab with the downloaded practitioner document', async () => {
+    applicationContext
+      .getUseCases()
+      .getPractitionerDocumentDownloadUrlInteractor.mockReturnValue({
+        url: mockDownloadUrl,
+      });
+  });
+
+  it('should make a call to retrieve the practitioner document download url', async () => {
     await runAction(openPractitionerDocumentDownloadUrlAction, {
       modules: { presenter },
       props: {
         barNumber: mockBarNumber,
-        fileName: 'file.png',
-        practitionerDocumentFileId: 'mockFileId1234',
+        practitionerDocumentFileId: mockFileId,
+      },
+    });
+
+    expect(
+      applicationContext.getUseCases()
+        .getPractitionerDocumentDownloadUrlInteractor.mock.calls[0][1],
+    ).toMatchObject({
+      barNumber: mockBarNumber,
+      practitionerDocumentFileId: mockFileId,
+    });
+  });
+
+  it('should the practitioner document in a new tab', async () => {
+    await runAction(openPractitionerDocumentDownloadUrlAction, {
+      modules: { presenter },
+      props: {
+        barNumber: mockBarNumber,
+        practitionerDocumentFileId: mockFileId,
       },
     });
 
     expect(
       applicationContext.getUtilities().openUrlInNewTab,
-    ).toHaveBeenCalled();
+    ).toHaveBeenCalledWith({ url: mockDownloadUrl });
+  });
+
+  it('should re-throw a formatted error when an error occurs while attempting to open the practitioner document', async () => {
+    const mockError = 'ERROR';
+    applicationContext.getUtilities().openUrlInNewTab.mockImplementation(() => {
+      throw new Error(mockError);
+    });
+
+    await expect(
+      runAction(openPractitionerDocumentDownloadUrlAction, {
+        modules: { presenter },
+        props: {
+          barNumber: mockBarNumber,
+          practitionerDocumentFileId: mockFileId,
+        },
+      }),
+    ).rejects.toThrow(`Unable to open document. ${mockError}`);
   });
 });
--- web-client/src/presenter/utilities/openUrlInNewTab.js
+++ web-client/src/presenter/utilities/openUrlInNewTab.js
@@ -1,14 +1,5 @@
-export const openUrlInNewTab = async getUrlCb => {
-  let url;
-
-  const openFileViewerWindow = window.open();
-
-  try {
-    ({ url } = await getUrlCb());
-  } catch (err) {
-    throw new Error(`Unable to get document download url. ${err.message}`);
-  }
-
-  openFileViewerWindow.document.write('Loading your document...');
-  openFileViewerWindow.location.href = url;
+export const openUrlInNewTab = ({ url }) => {
+  setTimeout(() => {
+    window.open(url, '_blank');
+  });
 };
