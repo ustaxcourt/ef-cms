47a72c234c756fcd50fdf8502404e59c2eb78dbe
Rosie Highsmith <ahighsmith@flexion.us>
2022-11-23 16:07:49 -0800

    intermittent-cypress-test: use navigateTo within test block so on retry it logs in again

--- cypress-integration/integration/docket-clerk-edits-court-issued-document.cy.js
+++ cypress-integration/integration/docket-clerk-edits-court-issued-document.cy.js
@@ -1,11 +1,12 @@
+const {
+  navigateTo: navigateToDashboard,
+} = require('../support/pages/dashboard');
 import { uploadCourtIssuedDocumentAndEditViaDocumentQC } from '../support/pages/document-qc';
 
 describe('Docket clerk edits a court issued document', function () {
-  before(() => {
-    cy.login('docketclerk');
-  });
-
   it('upload a court issued document, click on the in progress link, and navigate to the correct page', () => {
+    navigateToDashboard('docketclerk');
+
     const attempt = cy.state('runnable')._currentRetry;
     uploadCourtIssuedDocumentAndEditViaDocumentQC(attempt);
   });
