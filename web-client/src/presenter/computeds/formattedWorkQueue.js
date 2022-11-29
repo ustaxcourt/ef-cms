2d39a5c15bacb029ec78e4d472d4974371793c40
Cody Seibert <cseibert113@gmail.com>
2022-11-29 12:12:06 -0500

    9617 - removing duplicate test coverage

--- web-client/src/presenter/computeds/formattedWorkQueue.js
+++ web-client/src/presenter/computeds/formattedWorkQueue.js
@@ -311,9 +311,6 @@ export const formattedWorkQueue = (get, applicationContext) => {
   let { assignmentFilterValue } = get(state.screenMetadata);
   const users = get(state.users);
 
-  console.log('workItems', workItems);
-  console.log('workQueueToDisplay', workQueueToDisplay);
-
   if (assignmentFilterValue && assignmentFilterValue.userId !== 'UA') {
     assignmentFilterValue = users.find(
       user => user.userId === assignmentFilterValue.userId,
