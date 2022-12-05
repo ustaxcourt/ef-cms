ab1e32de327b5fcd1812dce62b2d6fa5e6ac3772
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2022-12-05 14:48:11 -0700

    9684: failing test, receiving undefined [skip ci]

--- web-client/integration-tests/messagesTableJourney.test.js
+++ web-client/integration-tests/messagesTableJourney.test.js
@@ -1,12 +1,10 @@
 import { CASE_STATUS_TYPES } from '../../shared/src/business/entities/EntityConstants';
-import { loginAs, refreshElasticsearchIndex, setupTest } from './helpers';
 import { createNewMessageOnCase } from './journey/createNewMessageOnCase';
-import { petitionsClerkServesElectronicCaseToIrs } from './journey/petitionsClerkServesElectronicCaseToIrs';
-import { petitionsClerkSetsATrialSessionsSchedule } from './journey/petitionsClerkSetsATrialSessionsSchedule';
+import { loginAs, setupTest } from './helpers';
 
 describe('messages table journey', () => {
   const cerebralTest = setupTest();
-  // const calendaredCaseDocketNumber = '103-20';
+  const calendaredCaseDocketNumber = '103-20';
 
   beforeAll(() => {
     jest.setTimeout(40000);
@@ -21,40 +19,9 @@ describe('messages table journey', () => {
   });
 
   loginAs(cerebralTest, 'petitionsclerk@example.com');
-  petitionsClerkServesElectronicCaseToIrs(cerebralTest);
-
-  //as docketclerk
-  // docketClerkUpdatesCaseStatusToReadyForTrial
-
-  // /as petitionsclerk
-  //go to trial session detail, mark qc complete
-
-  petitionsClerkSetsATrialSessionsSchedule(cerebralTest);
-
-  it('petitions clerk manually adds a case to a calendared trial session', async () => {
-    await cerebralTest.runSequence('gotoCaseDetailSequence', {
-      docketNumber: cerebralTest.docketNumber,
-    });
-
-    await cerebralTest.runSequence('openAddToTrialModalSequence');
-
-    await cerebralTest.runSequence('updateModalValueSequence', {
-      key: 'showAllLocations',
-      value: true,
-    });
-
-    await cerebralTest.runSequence('updateModalValueSequence', {
-      key: 'trialSessionId',
-      value: cerebralTest.trialSessionId,
-    });
-
-    await cerebralTest.runSequence('addCaseToTrialSessionSequence');
-    await refreshElasticsearchIndex();
-
-    expect(cerebralTest.getState('caseDetail.trialDate')).toBeDefined();
+  createNewMessageOnCase(cerebralTest, {
+    docketNumber: calendaredCaseDocketNumber,
   });
-
-  createNewMessageOnCase(cerebralTest);
 
   it('petitions clerk views case trial information on sent messages view', async () => {
     await cerebralTest.runSequence('gotoMessagesSequence', {
@@ -71,6 +38,7 @@ describe('messages table journey', () => {
     //add checks for trial info
     expect(foundMessage).toMatchObject({
       caseStatus: CASE_STATUS_TYPES.calendared,
+      docketNumber: calendaredCaseDocketNumber,
       trialDate: '',
       trialLocation: '',
     });
