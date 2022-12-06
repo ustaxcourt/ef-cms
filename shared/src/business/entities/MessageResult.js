b2570e67ef8e8659a59fb3e69d4419336ceade7d
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2022-12-06 15:16:45 -0700

    9864: adding validation rules

--- shared/src/business/entities/MessageResult.js
+++ shared/src/business/entities/MessageResult.js
@@ -3,6 +3,11 @@ const {
   joiValidationDecorator,
   validEntityDecorator,
 } = require('./JoiValidationDecorator');
+const {
+  TRIAL_CITY_STRINGS,
+  TRIAL_LOCATION_MATCHER,
+} = require('./EntityConstants');
+const { JoiValidationConstants } = require('./JoiValidationConstants');
 const { messageDecorator } = require('./Message');
 
 /**
@@ -24,9 +29,23 @@ MessageResult.prototype.init = function init(
   this.trialLocation = rawMessage.trialLocation;
 };
 
-MessageResult.VALIDATION_ERROR_MESSAGES = {};
-
-MessageResult.VALIDATION_RULES = {};
+MessageResult.VALIDATION_RULES = {
+  trialDate: joi
+    .alternatives()
+    .optional()
+    .description('When this case goes to trial.'),
+  trialLocation: joi
+    .alternatives()
+    .try(
+      JoiValidationConstants.STRING.valid(...TRIAL_CITY_STRINGS, null),
+      JoiValidationConstants.STRING.pattern(TRIAL_LOCATION_MATCHER),
+      JoiValidationConstants.STRING.valid('Standalone Remote'),
+    )
+    .optional()
+    .description(
+      'Where this case goes to trial. This may be different that the preferred trial location.',
+    ),
+};
 
 joiValidationDecorator(
   MessageResult,
