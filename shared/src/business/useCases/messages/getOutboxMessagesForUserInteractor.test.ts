0821273c1345e8625c88952584eacf52a4ed2b8d
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2022-12-06 14:48:47 -0700

    9684: fixing failing test

--- shared/src/business/useCases/messages/getOutboxMessagesForUserInteractor.test.ts
+++ shared/src/business/useCases/messages/getOutboxMessagesForUserInteractor.test.ts
@@ -1,12 +1,12 @@
-import { omit } from 'lodash';
-import { UnauthorizedError } from '../../../../../shared/src/errors/errors';
 import {
   CASE_STATUS_TYPES,
   PETITIONS_SECTION,
   ROLES,
 } from '../../entities/EntityConstants';
+import { UnauthorizedError } from '../../../../../shared/src/errors/errors';
 import { applicationContext } from '../../test/createTestApplicationContext';
 import { getOutboxMessagesForUserInteractor } from './getOutboxMessagesForUserInteractor';
+import { omit } from 'lodash';
 
 describe('getOutboxMessagesForUserInteractor', () => {
   it('throws unauthorized for a user without MESSAGES permission', async () => {
