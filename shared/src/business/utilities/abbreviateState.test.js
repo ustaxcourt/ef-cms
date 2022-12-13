9abcc22f840b387ba19cb827f004ef5fdcc4389d
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2022-12-13 12:19:54 -0700

    9684: adding additional unit test

--- shared/src/business/utilities/abbreviateState.test.js
+++ shared/src/business/utilities/abbreviateState.test.js
@@ -9,4 +9,13 @@ describe('abbreviateState', () => {
 
     expect(result).toEqual(expectedResult);
   });
+
+  it('should return a string with an abbreviated state when passed a multi word city and unabbreviated state', () => {
+    const locationString = 'Las Vegas, Nevada';
+    const expectedResult = 'Las Vegas, NV';
+
+    const result = abbreviateState(locationString);
+
+    expect(result).toEqual(expectedResult);
+  });
 });
