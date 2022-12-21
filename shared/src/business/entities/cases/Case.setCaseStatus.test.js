964d1189be85d1983554e2c91880067357b45cc5
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-12-21 14:15:23 -0800

    9669: Don't use isClosed method when updating case status, it doesn't read very well. Update tests

--- shared/src/business/entities/cases/Case.setCaseStatus.test.js
+++ shared/src/business/entities/cases/Case.setCaseStatus.test.js
@@ -6,7 +6,7 @@ const { CASE_STATUS_TYPES, CHIEF_JUDGE } = require('../EntityConstants');
 const { MOCK_CASE } = require('../../../test/mockCase');
 
 describe('setCaseStatus', () => {
-  it('should update the case status and set the associated judge to the chief judge if the new status is General Docket - Not At Issue', () => {
+  it('should update the case status and set the associated judge to the chief judge when the new status is "General Docket - Not At Issue"', () => {
     const updatedCase = new Case(
       {
         ...MOCK_CASE,
@@ -23,7 +23,26 @@ describe('setCaseStatus', () => {
     expect(updatedCase.associatedJudge).toEqual(CHIEF_JUDGE);
   });
 
-  it('should update the case status, leave the associated judge unchanged, and call closeCase if the new status is Closed', () => {
+  it('should update the case status and set the associated judge to the chief judge when the new status is "General Docket - Ready for Trial"', () => {
+    const updatedCase = new Case(
+      {
+        ...MOCK_CASE,
+        associatedJudge: 'Judge Buch',
+      },
+      {
+        applicationContext,
+      },
+    );
+
+    updatedCase.setCaseStatus(CASE_STATUS_TYPES.generalDocketReadyForTrial);
+
+    expect(updatedCase.status).toEqual(
+      CASE_STATUS_TYPES.generalDocketReadyForTrial,
+    );
+    expect(updatedCase.associatedJudge).toEqual(CHIEF_JUDGE);
+  });
+
+  it('should update the case status, leave the associated judge unchanged, and call closeCase when the new status is "Closed"', () => {
     const closeCaseSpy = jest.spyOn(Case.prototype, 'closeCase');
 
     const updatedCase = new Case(
@@ -37,6 +56,27 @@ describe('setCaseStatus', () => {
     );
 
     updatedCase.setCaseStatus(CASE_STATUS_TYPES.closed);
+
+    expect(updatedCase.status).toEqual(CASE_STATUS_TYPES.closed);
+    expect(updatedCase.associatedJudge).toEqual('Judge Buch');
+    expect(closeCaseSpy).toHaveBeenCalled();
+    closeCaseSpy.mockRestore();
+  });
+
+  it('should update the case status, leave the associated judge unchanged, and call closeCase when the new status is "Closed - Dismissed"', () => {
+    const closeCaseSpy = jest.spyOn(Case.prototype, 'closeCase');
+
+    const updatedCase = new Case(
+      {
+        ...MOCK_CASE,
+        associatedJudge: 'Judge Buch',
+      },
+      {
+        applicationContext,
+      },
+    );
+
+    updatedCase.setCaseStatus(CASE_STATUS_TYPES.closedDismissed);
 
     expect(updatedCase.status).toEqual(CASE_STATUS_TYPES.closed);
     expect(updatedCase.associatedJudge).toEqual('Judge Buch');
