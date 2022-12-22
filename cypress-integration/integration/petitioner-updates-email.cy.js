205349374d25d9fee0bb4d720195aaf0629189a7
Timothy Shumway <tshumway@flexion.us>
2022-12-22 09:44:52 -0700

    DepUpdates121922: remove use of Cookies API in cypress tests

--- cypress-integration/integration/petitioner-updates-email.cy.js
+++ cypress-integration/integration/petitioner-updates-email.cy.js
@@ -12,15 +12,12 @@ const { navigateTo: loginAs } = require('../support/pages/maintenance');
 
 describe('Petitioner updates and verifies their email', () => {
   before(() => {
+    // cy.session('petitioner9Session', () => {
     loginAs('petitioner9');
     // wait because the UI flips views for some reason when you click on a button too soon when
     // goto sequences are not Fully done (cerebral or riot router bug?)
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(2000);
-  });
-
-  beforeEach(() => {
-    Cypress.Cookies.preserveOnce('refreshToken');
   });
 
   it('petitioner should be able to change their email', () => {
