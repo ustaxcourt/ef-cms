c23f9b491a360a3cff545b9147b795979abf7333
Rosie Highsmith <ahighsmith@flexion.us>
2022-12-28 10:22:00 -0700

    9669: finish migration script test and added to migrationsToRun

--- web-api/migration-terraform/main/lambdas/migrations/0004-remove-closedDate-for-open-cases.test.js
+++ web-api/migration-terraform/main/lambdas/migrations/0004-remove-closedDate-for-open-cases.test.js
@@ -1,20 +1,19 @@
+import { CLOSED_CASE_STATUSES } from '../../../../../shared/src/business/entities/EntityConstants';
 import { migrateItems } from './0004-remove-closedDate-for-open-cases';
 const { MOCK_CASE } = require('../../../../../shared/src/test/mockCase');
 
 describe('migrateItems', () => {
   let mockCaseItem;
-  let documentClient;
 
   beforeEach(() => {
     mockCaseItem = {
       ...MOCK_CASE,
-      hasSealedDocuments: true,
       pk: `case|${MOCK_CASE.docketNumber}`,
       sk: `case|${MOCK_CASE.docketNumber}`,
     };
   });
 
-  it('should return and not modify records that are NOT case records', async () => {
+  it('should return and not modify records that are NOT case records', () => {
     const mockTrialSessionId = '87b86617-f090-4f26-a321-3ee0683cc0f2';
     const mockItems = [
       {
@@ -26,16 +25,30 @@ describe('migrateItems', () => {
         sk: `trial-session|${mockTrialSessionId}`,
       },
     ];
-    const results = await migrateItems(mockItems, documentClient);
+    const results = migrateItems(mockItems);
 
     expect(results).toEqual(mockItems);
   });
 
-  it('should set closedDate to undefined when the record is a case entity with a closedDate and a non-CLOSED_CASE_STATUSES status', async () => {
-    const items = [mockCaseItem];
+  it('should delete closedDate when the record is a case entity with a closedDate and a non-CLOSED_CASE_STATUSES status', () => {
+    const items = [{ ...mockCaseItem, closedDate: '2022-12-28 10:14:04' }];
 
-    const results = await migrateItems(items, documentClient);
+    const results = migrateItems(items);
 
     expect(results[0].closedDate).toBeUndefined();
+  });
+
+  it('should NOT delete closedDate when the record is a case entity with a closedDate and a CLOSED_CASE_STATUSES status', () => {
+    const items = [
+      {
+        ...mockCaseItem,
+        closedDate: '2022-12-28 10:14:04',
+        status: CLOSED_CASE_STATUSES[0],
+      },
+    ];
+
+    const results = migrateItems(items);
+
+    expect(results[0].closedDate).toBeDefined();
   });
 });
