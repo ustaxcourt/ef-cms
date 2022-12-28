4d6eef5a84b87f8147e5a0983bd23a3573628103
Rosie Highsmith <ahighsmith@flexion.us>
2022-12-28 10:22:00 -0700

    9669: finish migration script test and added to migrationsToRun

--- web-api/migration-terraform/main/lambdas/migrations/0004-remove-closedDate-for-open-cases.js
+++ web-api/migration-terraform/main/lambdas/migrations/0004-remove-closedDate-for-open-cases.js
@@ -12,15 +12,11 @@ const isCaseRecord = item => {
 
 const applicationContext = createApplicationContext({});
 
-const migrateItems = async items => {
+const migrateItems = items => {
   const itemsAfter = [];
 
   for (const item of items) {
     if (isCaseRecord(item)) {
-      // if item is a case
-      // if item has a closedDate AND status is NOT CLOSED_CASE_STATUSES
-      // set closedDate to undefined
-      // validate case
       if (item.closedDate && !CLOSED_CASE_STATUSES.includes(item.status)) {
         delete item.closedDate;
 
