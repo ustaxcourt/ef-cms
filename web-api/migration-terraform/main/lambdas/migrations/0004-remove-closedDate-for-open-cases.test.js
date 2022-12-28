a74f464500b8d508b06405324b12662c061691d5
Rosie Highsmith <ahighsmith@flexion.us>
2022-12-28 09:38:02 -0700

    9669: finished test for migration [skip ci]

--- web-api/migration-terraform/main/lambdas/migrations/0004-remove-closedDate-for-open-cases.test.js
+++ web-api/migration-terraform/main/lambdas/migrations/0004-remove-closedDate-for-open-cases.test.js
@@ -1,19 +1,40 @@
 import { migrateItems } from './0004-remove-closedDate-for-open-cases';
+const { MOCK_CASE } = require('../../../../../shared/src/test/mockCase');
 
 describe('migrateItems', () => {
+  let mockCaseItem;
+
+  beforeEach(() => {
+    mockCaseItem = {
+      ...MOCK_CASE,
+      hasSealedDocuments: true,
+      pk: `case|${MOCK_CASE.docketNumber}`,
+      sk: `case|${MOCK_CASE.docketNumber}`,
+    };
+  });
+
   it('should return and not modify records that are NOT case records', async () => {
+    const mockTrialSessionId = '87b86617-f090-4f26-a321-3ee0683cc0f2';
     const mockItems = [
       {
-        pk: 'case|101-10',
+        pk: `case|${MOCK_CASE.docketNumber}`,
         sk: 'user|6d74eadc-0181-4ff5-826c-305200e8733d',
       },
       {
-        pk: 'trial-session|87b86617-f090-4f26-a321-3ee0683cc0f2',
-        sk: 'trial-session|87b86617-f090-4f26-a321-3ee0683cc0f2',
+        pk: `trial-session|${mockTrialSessionId}`,
+        sk: `trial-session|${mockTrialSessionId}`,
       },
     ];
     const results = await migrateItems(mockItems);
 
     expect(results).toEqual(mockItems);
+  });
+
+  it('should set closedDate to undefined when the record is a case entity with a closedDate and a non-CLOSED_CASE_STATUSES status', async () => {
+    const items = [mockCaseItem];
+
+    const results = await migrateItems(items);
+
+    expect(results[0].closedDate).toBeUndefined();
   });
 });
