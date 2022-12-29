8915b24ec10c35fd0585e83443093dc8f7026190
Michael Marcotte <mike.marcotte@gmail.com>
2022-12-29 16:28:10 -0500

    devex-1078: run migration to fix calculated sortable docket number

--- web-api/migration-terraform/main/lambdas/migrationsToRun.js
+++ web-api/migration-terraform/main/lambdas/migrationsToRun.js
@@ -7,6 +7,9 @@ const {
 const {
   migrateItems: migration0003,
 } = require('./migrations/0003-update-trial-session-working-copy-status');
+const {
+  migrateItems: migration0006,
+} = require('./migrations/0006-reformat-sortable-docket-number');
 
 // MODIFY THIS ARRAY TO ADD NEW MIGRATIONS OR REMOVE OLD ONES
 const migrationsToRun = [
@@ -15,6 +18,10 @@ const migrationsToRun = [
   {
     key: '0003-update-trial-session-working-copy-status.js',
     script: migration0003,
+  },
+  {
+    key: '0006-reformat-sortable-docket-number.js',
+    script: migration0006,
   },
 ];
 
