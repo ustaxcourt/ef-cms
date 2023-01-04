e0ef0439dabadd2714879cf506193c5b9aec91ae
Cody Seibert <cseibert113@gmail.com>
2023-01-04 11:18:08 -0500

    9664 - adding unit test to verify set default penalities action

--- web-client/src/presenter/actions/setDefaultPenaltiesAction.test.js
+++ web-client/src/presenter/actions/setDefaultPenaltiesAction.test.js
@@ -15,4 +15,51 @@ describe('setDefaultPenaltiesAction', () => {
 
     expect(penalties.length).toEqual(5);
   });
+
+  it('should keep the initial penalities as is when loading', async () => {
+    const result = await runAction(setDefaultPenaltiesAction, {
+      state: {
+        form: {
+          penalties: [
+            {
+              determinationPenaltyAmount: '1.00',
+              irsPenaltyAmount: '5.00',
+            },
+
+            {
+              determinationPenaltyAmount: '2.00',
+              irsPenaltyAmount: '4.00',
+            },
+
+            {
+              determinationPenaltyAmount: '3.00',
+              irsPenaltyAmount: '4.00',
+            },
+          ],
+        },
+        modal: {
+          penalties: null,
+        },
+      },
+    });
+
+    const { penalties } = result.state.modal;
+
+    expect(penalties).toEqual([
+      {
+        determinationPenaltyAmount: '1.00',
+        irsPenaltyAmount: '5.00',
+      },
+      {
+        determinationPenaltyAmount: '2.00',
+        irsPenaltyAmount: '4.00',
+      },
+      {
+        determinationPenaltyAmount: '3.00',
+        irsPenaltyAmount: '4.00',
+      },
+      {},
+      {},
+    ]);
+  });
 });
