29ffae22e772d56e38314fd3e86d0afc28322d16
Richard Goshen <rick.goshen@gmail.com>
2023-01-06 12:36:00 -0700

    9594-add test for isDirectlyAssociated it set on state

--- web-client/src/presenter/actions/setCaseAssociationAction.test.js
+++ web-client/src/presenter/actions/setCaseAssociationAction.test.js
@@ -2,15 +2,17 @@ import { runAction } from 'cerebral/test';
 import { setCaseAssociationAction } from './setCaseAssociationAction';
 
 describe('setCaseAssociationAction', () => {
-  it('sets state.screenMetadata.isAssociated and state.screenMetadata.pendingAssociation from props', async () => {
+  it('sets state.screenMetadata.isAssociated and state.screenMetadata.pendingAssociation and state.screenMetadata.isDirectlyAssociated from props', async () => {
     const { state } = await runAction(setCaseAssociationAction, {
       props: {
         isAssociated: true,
+        isDirectlyAssociated: true,
         pendingAssociation: true,
       },
       state: {
         screenMetadata: {
           isAssociated: false,
+          isDirectlyAssociated: false,
           pendingAssociation: false,
         },
       },
@@ -18,5 +20,6 @@ describe('setCaseAssociationAction', () => {
 
     expect(state.screenMetadata.isAssociated).toBeTruthy();
     expect(state.screenMetadata.pendingAssociation).toBeTruthy();
+    expect(state.screenMetadata.isDirectlyAssociated).toBeTruthy();
   });
 });
