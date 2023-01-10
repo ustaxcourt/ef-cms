b6e1df8a958798cbcb41f5c93669e69251109572
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2023-01-10 10:46:09 -0700

    9772: adding integration tests WIP [skip ci]

--- web-client/integration-tests/externalUserFilesDocumentForLegacyCase.test.js
+++ web-client/integration-tests/externalUserFilesDocumentForLegacyCase.test.js
@@ -1,7 +1,9 @@
 import { associatedExternalUserViewsCaseDetailForOwnedCase } from './journey/associatedExternalUserViewsCaseDetailForOwnedCase.js';
+import { completeDocumentTypeSectionHelper as completeDocumentTypeSectionHelperComputed } from '../src/presenter/computeds/completeDocumentTypeSectionHelper';
 import { externalUserFilesDocumentForOwnedCase } from './journey/externalUserFilesDocumentForOwnedCase.js';
 import { fakeFile, loginAs, setupTest } from './helpers';
 import { getOtherFilers } from '../../shared/src/business/entities/cases/Case';
+import { withAppContextDecorator } from '../src/withAppContext';
 
 describe('an external user files a document for their legacy case', () => {
   const cerebralTest = setupTest();
@@ -19,29 +21,47 @@ describe('an external user files a document for their legacy case', () => {
   loginAs(cerebralTest, 'petitioner@example.com');
   associatedExternalUserViewsCaseDetailForOwnedCase(cerebralTest);
   externalUserFilesDocumentForOwnedCase(cerebralTest, fakeFile);
-
-  loginAs(cerebralTest, 'privatePractitioner@example.com');
-  associatedExternalUserViewsCaseDetailForOwnedCase(cerebralTest);
-  externalUserFilesDocumentForOwnedCase(cerebralTest, fakeFile);
-
-  loginAs(cerebralTest, 'irsPractitioner@example.com');
-  associatedExternalUserViewsCaseDetailForOwnedCase(cerebralTest);
-  externalUserFilesDocumentForOwnedCase(cerebralTest, fakeFile);
-
-  loginAs(cerebralTest, 'docketclerk@example.com');
-  it('verifies otherFiler parties receive paper service when serviceIndicator is set to paper', async () => {
+  it('they are unable to file a document type of M123', async () => {
     await cerebralTest.runSequence('gotoCaseDetailSequence', {
       docketNumber: cerebralTest.docketNumber,
     });
 
-    const otherFilers = getOtherFilers(cerebralTest.getState('caseDetail'));
-    const docketEntries = cerebralTest.getState('caseDetail.docketEntries');
-    const lastServedDocument = docketEntries.pop();
-
-    const isOtherFilerServed = lastServedDocument.servedParties.find(
-      p => p.name === otherFilers[0].name && p.email === otherFilers[0].email,
+    const completeDocumentTypeSectionHelper = withAppContextDecorator(
+      completeDocumentTypeSectionHelperComputed,
     );
 
-    expect(isOtherFilerServed).toBeTruthy();
+    console.log(completeDocumentTypeSectionHelper, '-------');
+
+    const M123_document =
+      completeDocumentTypeSectionHelper.documentTypesForSelectSorted.find(
+        d => d.eventCode === 'M123',
+      );
+
+    expect(M123_document).toBeUndefined();
   });
+
+  // loginAs(cerebralTest, 'privatePractitioner@example.com');
+  // associatedExternalUserViewsCaseDetailForOwnedCase(cerebralTest);
+  // externalUserFilesDocumentForOwnedCase(cerebralTest, fakeFile);
+
+  // loginAs(cerebralTest, 'irsPractitioner@example.com');
+  // associatedExternalUserViewsCaseDetailForOwnedCase(cerebralTest);
+  // externalUserFilesDocumentForOwnedCase(cerebralTest, fakeFile);
+
+  // loginAs(cerebralTest, 'docketclerk@example.com');
+  // it('verifies otherFiler parties receive paper service when serviceIndicator is set to paper', async () => {
+  //   await cerebralTest.runSequence('gotoCaseDetailSequence', {
+  //     docketNumber: cerebralTest.docketNumber,
+  //   });
+
+  //   const otherFilers = getOtherFilers(cerebralTest.getState('caseDetail'));
+  //   const docketEntries = cerebralTest.getState('caseDetail.docketEntries');
+  //   const lastServedDocument = docketEntries.pop();
+
+  //   const isOtherFilerServed = lastServedDocument.servedParties.find(
+  //     p => p.name === otherFilers[0].name && p.email === otherFilers[0].email,
+  //   );
+
+  //   expect(isOtherFilerServed).toBeTruthy();
+  // });
 });
