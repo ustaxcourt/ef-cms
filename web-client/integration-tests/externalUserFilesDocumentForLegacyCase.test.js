804b0f56da5841c5fbb1e21e5222298d0a8ad106
Rosie Highsmith <ahighsmith@flexion.us>
2023-01-10 10:55:16 -0700

    9772: complete integration test to cover unavailability of M123 event code

--- web-client/integration-tests/externalUserFilesDocumentForLegacyCase.test.js
+++ web-client/integration-tests/externalUserFilesDocumentForLegacyCase.test.js
@@ -1,9 +1,8 @@
 import { associatedExternalUserViewsCaseDetailForOwnedCase } from './journey/associatedExternalUserViewsCaseDetailForOwnedCase.js';
-import { completeDocumentTypeSectionHelper as completeDocumentTypeSectionHelperComputed } from '../src/presenter/computeds/completeDocumentTypeSectionHelper';
 import { externalUserFilesDocumentForOwnedCase } from './journey/externalUserFilesDocumentForOwnedCase.js';
 import { fakeFile, loginAs, setupTest } from './helpers';
 import { getOtherFilers } from '../../shared/src/business/entities/cases/Case';
-import { withAppContextDecorator } from '../src/withAppContext';
+import { userTriesToFileAnUnavailableDocumentType } from './journey/userTriesToFileAnUnavailableDocumentType';
 
 describe('an external user files a document for their legacy case', () => {
   const cerebralTest = setupTest();
@@ -21,47 +20,32 @@ describe('an external user files a document for their legacy case', () => {
   loginAs(cerebralTest, 'petitioner@example.com');
   associatedExternalUserViewsCaseDetailForOwnedCase(cerebralTest);
   externalUserFilesDocumentForOwnedCase(cerebralTest, fakeFile);
-  it('they are unable to file a document type of M123', async () => {
+  userTriesToFileAnUnavailableDocumentType(cerebralTest);
+
+  loginAs(cerebralTest, 'privatePractitioner@example.com');
+  associatedExternalUserViewsCaseDetailForOwnedCase(cerebralTest);
+  externalUserFilesDocumentForOwnedCase(cerebralTest, fakeFile);
+  userTriesToFileAnUnavailableDocumentType(cerebralTest);
+
+  loginAs(cerebralTest, 'irsPractitioner@example.com');
+  associatedExternalUserViewsCaseDetailForOwnedCase(cerebralTest);
+  externalUserFilesDocumentForOwnedCase(cerebralTest, fakeFile);
+  userTriesToFileAnUnavailableDocumentType(cerebralTest);
+
+  loginAs(cerebralTest, 'docketclerk@example.com');
+  it('verifies otherFiler parties receive paper service when serviceIndicator is set to paper', async () => {
     await cerebralTest.runSequence('gotoCaseDetailSequence', {
       docketNumber: cerebralTest.docketNumber,
     });
 
-    const completeDocumentTypeSectionHelper = withAppContextDecorator(
-      completeDocumentTypeSectionHelperComputed,
+    const otherFilers = getOtherFilers(cerebralTest.getState('caseDetail'));
+    const docketEntries = cerebralTest.getState('caseDetail.docketEntries');
+    const lastServedDocument = docketEntries.pop();
+
+    const isOtherFilerServed = lastServedDocument.servedParties.find(
+      p => p.name === otherFilers[0].name && p.email === otherFilers[0].email,
     );
 
-    console.log(completeDocumentTypeSectionHelper, '-------');
-
-    const M123_document =
-      completeDocumentTypeSectionHelper.documentTypesForSelectSorted.find(
-        d => d.eventCode === 'M123',
-      );
-
-    expect(M123_document).toBeUndefined();
+    expect(isOtherFilerServed).toBeTruthy();
   });
-
-  // loginAs(cerebralTest, 'privatePractitioner@example.com');
-  // associatedExternalUserViewsCaseDetailForOwnedCase(cerebralTest);
-  // externalUserFilesDocumentForOwnedCase(cerebralTest, fakeFile);
-
-  // loginAs(cerebralTest, 'irsPractitioner@example.com');
-  // associatedExternalUserViewsCaseDetailForOwnedCase(cerebralTest);
-  // externalUserFilesDocumentForOwnedCase(cerebralTest, fakeFile);
-
-  // loginAs(cerebralTest, 'docketclerk@example.com');
-  // it('verifies otherFiler parties receive paper service when serviceIndicator is set to paper', async () => {
-  //   await cerebralTest.runSequence('gotoCaseDetailSequence', {
-  //     docketNumber: cerebralTest.docketNumber,
-  //   });
-
-  //   const otherFilers = getOtherFilers(cerebralTest.getState('caseDetail'));
-  //   const docketEntries = cerebralTest.getState('caseDetail.docketEntries');
-  //   const lastServedDocument = docketEntries.pop();
-
-  //   const isOtherFilerServed = lastServedDocument.servedParties.find(
-  //     p => p.name === otherFilers[0].name && p.email === otherFilers[0].email,
-  //   );
-
-  //   expect(isOtherFilerServed).toBeTruthy();
-  // });
 });
