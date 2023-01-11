75d99b35a18bc83c2b80ffb541fd3efdf023125a
Michael Marcotte <mike.marcotte@gmail.com>
2023-01-11 16:20:09 -0500

    devex-1088: move getRecordSize into dynamo helper

---
+++ shared/src/persistence/dynamo/helpers/getRecordSize.test.ts
@@ -0,0 +1,17 @@
+const exampleInput = {
+  favouriteNumber: {
+    N: '-1E-130',
+  },
+  foods: {
+    SS: ['pizza', 'burger'],
+  },
+  fullName: {
+    S: 'Zac Charles',
+  },
+  id: {
+    S: 'f0ba8d6c',
+  },
+  isAdmin: {
+    BOOL: 'true',
+  },
+};
