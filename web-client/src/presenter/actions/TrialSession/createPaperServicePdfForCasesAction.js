8dbc83ca6b9e3c49ee16f50d02b19e9863988fac
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2023-01-12 14:58:23 -0700

    1083: created interactor

--- web-client/src/presenter/actions/TrialSession/createPaperServicePdfForCasesAction.js
+++ web-client/src/presenter/actions/TrialSession/createPaperServicePdfForCasesAction.js
@@ -12,45 +12,4 @@ export const createPaperServicePdfForCasesAction = async ({
     .generateTrialSessionPaperServicePdfInteractor(applicationContext, {
       calendaredCasePdfDataArray,
     });
-
-  // const { PDFDocument } = await applicationContext.getPdfLib();
-  // const paperServiceDocumentsPdf = await PDFDocument.create();
-  // const user = applicationContext.getCurrentUser();
-
-  // for (let index = 0; index < calendaredCasePdfDataArray.length; index++) {
-  //   const calendaredCasePdf = await PDFDocument.load(
-  //     calendaredCasePdfDataArray[index],
-  //   );
-  //   await applicationContext.getUtilities().copyPagesAndAppendToTargetPdf({
-  //     copyFrom: calendaredCasePdf,
-  //     copyInto: paperServiceDocumentsPdf,
-  //   });
-  // }
-
-  // const { docketEntryId, hasPaper, url } = await applicationContext
-  //   .getUseCaseHelpers()
-  //   .savePaperServicePdf({
-  //     applicationContext,
-  //     document: paperServiceDocumentsPdf,
-  //   });
-
-  // if (url) {
-  //   applicationContext.logger.info(
-  //     `generated the printable paper service pdf at ${url}`,
-  //     {
-  //       url,
-  //     },
-  //   );
-  // }
-
-  // await applicationContext.getNotificationGateway().sendNotificationToUser({
-  //   applicationContext,
-  //   message: {
-  //     action: 'notice_generation_complete',
-  //     docketEntryId,
-  //     hasPaper,
-  //     pdfUrl: url || null,
-  //   },
-  //   userId: user.userId,
-  // });
 };
