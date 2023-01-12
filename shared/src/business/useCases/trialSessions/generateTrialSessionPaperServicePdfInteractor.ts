af899b8fb38fec143747b4df21871dbf4e24efe9
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2023-01-12 14:58:23 -0700

    1083: created interactor

---
+++ shared/src/business/useCases/trialSessions/generateTrialSessionPaperServicePdfInteractor.ts
@@ -0,0 +1,64 @@
+import {
+  ROLE_PERMISSIONS,
+  isAuthorized,
+} from '../../../authorization/authorizationClientService';
+import { UnauthorizedError } from '../../../errors/errors';
+/**
+ * generateTrialSessionPaperServicePdfInteractor
+ *
+ * @param {object} applicationContext the application context
+ * @param {object} providers the providers object
+ * @param {string} providers.trialSessionId the id for the trial session
+ * @returns {string} trial session calendar pdf url
+ */
+export const generateTrialSessionPaperServicePdfInteractor = async (
+  applicationContext: IApplicationContext,
+  { calendaredCasePdfDataArray }: { calendaredCasePdfDataArray: any[] },
+) => {
+  const user = applicationContext.getCurrentUser();
+
+  if (!isAuthorized(user, ROLE_PERMISSIONS.TRIAL_SESSIONS)) {
+    throw new UnauthorizedError('Unauthorized');
+  }
+
+  const { PDFDocument } = await applicationContext.getPdfLib();
+  const paperServiceDocumentsPdf = await PDFDocument.create();
+
+  for (let index = 0; index < calendaredCasePdfDataArray.length; index++) {
+    const calendaredCasePdf = await PDFDocument.load(
+      calendaredCasePdfDataArray[index],
+    );
+
+    await applicationContext.getUtilities().copyPagesAndAppendToTargetPdf({
+      copyFrom: calendaredCasePdf,
+      copyInto: paperServiceDocumentsPdf,
+    });
+  }
+
+  const { docketEntryId, hasPaper, url } = await applicationContext
+    .getUseCaseHelpers()
+    .savePaperServicePdf({
+      applicationContext,
+      document: paperServiceDocumentsPdf,
+    });
+
+  if (url) {
+    applicationContext.logger.info(
+      `generated the printable paper service pdf at ${url}`,
+      {
+        url,
+      },
+    );
+  }
+
+  await applicationContext.getNotificationGateway().sendNotificationToUser({
+    applicationContext,
+    message: {
+      action: 'notice_generation_complete',
+      docketEntryId,
+      hasPaper,
+      pdfUrl: url || null,
+    },
+    userId: user.userId,
+  });
+};
