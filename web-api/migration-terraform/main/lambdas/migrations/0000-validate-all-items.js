8ee523f5bb0c7b46a2d39aa8f73d7c3b49d6d3b8
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-01-13 09:47:10 -0800

    Devex 1088: Remove logging

--- web-api/migration-terraform/main/lambdas/migrations/0000-validate-all-items.js
+++ web-api/migration-terraform/main/lambdas/migrations/0000-validate-all-items.js
@@ -1,6 +1,4 @@
-const AWS = require('aws-sdk');
 const createApplicationContext = require('../../../../src/applicationContext');
-const { getRecordSize } = require('../utilities/getRecordSize');
 const applicationContext = createApplicationContext({});
 
 const migrateItems = items => {
@@ -8,23 +6,6 @@ const migrateItems = items => {
     const entityConstructor = applicationContext.getEntityByName(
       item.entityName,
     );
-
-    const itemSize = Buffer.byteLength(JSON.stringify(item)) / 1024;
-    applicationContext.logger.info(`JSON Item Size: ${itemSize.toFixed(1)}kb`);
-
-    let recordSize;
-
-    const marshalledItem = AWS.DynamoDB.Converter.marshall(item);
-    try {
-      recordSize = getRecordSize(marshalledItem) / 1000;
-    } catch (e) {
-      applicationContext.logger.info(
-        `DynamoDB Record Size Error (m-s): ${e}, ${JSON.stringify(
-          marshalledItem,
-        )}`,
-      );
-    }
-    applicationContext.logger.info(`DynamoDB Record Size: ${recordSize}kb`);
 
     if (entityConstructor) {
       new entityConstructor(item, {
