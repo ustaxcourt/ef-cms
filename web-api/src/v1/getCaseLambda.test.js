64d673444906bc1af95392751c962e08fd561ca2
Cody Seibert <cseibert113@gmail.com>
2023-01-20 13:46:28 -0500

    9594 - refactoring irs endpoint tests to use a cleaner approach

--- web-api/src/v1/getCaseLambda.test.js
+++ web-api/src/v1/getCaseLambda.test.js
@@ -23,6 +23,32 @@ const REQUEST_EVENT = {
 
 describe('getCaseLambda (which fails if version increase is needed, DO NOT CHANGE TESTS)', () => {
   let CI;
+
+  const mockExpectedResponse = {
+    caseCaption: 'Test Petitioner, Petitioner',
+    caseType: 'Other',
+    contactPrimary: {
+      address1: '123 Main St',
+      city: 'Somewhere',
+      email: 'petitioner@example.com',
+      name: 'Test Petitioner',
+      phone: '1234567',
+      postalCode: '12345',
+      state: 'TN',
+    },
+    docketEntries: [],
+    docketNumber: '101-18',
+    docketNumberSuffix: null,
+    filingType: 'Myself',
+    noticeOfTrialDate: '2020-10-20T01:38:43.489Z',
+    partyType: 'Petitioner',
+    practitioners: [],
+    preferredTrialCity: 'Washington, District of Columbia',
+    respondents: [],
+    sortableDocketNumber: 2018000101,
+    status: 'Calendared',
+  };
+
   // disable logging by mimicking CI for this test
   beforeAll(() => {
     ({ CI } = process.env);
@@ -158,107 +184,34 @@ describe('getCaseLambda (which fails if version increase is needed, DO NOT CHANG
     );
   });
 
-  it('returns the case in v1 format', async () => {
-    // Careful! Changing this test would mean that the v1 format is changing;
-    // this would mean breaking changes for any user of the v1 API
-    const user = MOCK_USERS['b7d90c05-f6cd-442c-a168-202db587f16f'];
-    const applicationContext = createSilentApplicationContext(user);
+  [true, false].forEach(isFeatureFlagOn => {
+    it(`returns the case in v1 format - when feature flag is ${isFeatureFlagOn}`, async () => {
+      // Careful! Changing this test would mean that the v1 format is changing;
+      // this would mean breaking changes for any user of the v1 API
+      const user = MOCK_USERS['b7d90c05-f6cd-442c-a168-202db587f16f'];
+      const applicationContext = createSilentApplicationContext(user);
 
-    applicationContext.getUseCases().getFeatureFlagValueInteractor = jest
-      .fn()
-      .mockResolvedValue(true);
+      applicationContext.getUseCases().getFeatureFlagValueInteractor = jest
+        .fn()
+        .mockResolvedValue(isFeatureFlagOn);
 
-    applicationContext.getDocumentClient = jest.fn().mockReturnValue({
-      query: jest.fn().mockReturnValue({
-        promise: jest.fn().mockReturnValue(
-          Promise.resolve({
-            Items: [mockDynamoCaseRecord],
-          }),
-        ),
-      }),
-    });
+      applicationContext.getDocumentClient = jest.fn().mockReturnValue({
+        query: jest.fn().mockReturnValue({
+          promise: jest.fn().mockReturnValue(
+            Promise.resolve({
+              Items: [mockDynamoCaseRecord],
+            }),
+          ),
+        }),
+      });
 
-    const response = await getCaseLambda(REQUEST_EVENT, {
-      applicationContext,
-    });
+      const response = await getCaseLambda(REQUEST_EVENT, {
+        applicationContext,
+      });
 
-    expect(response.statusCode).toBe('200');
-    expect(response.headers['Content-Type']).toBe('application/json');
-    expect(JSON.parse(response.body)).toMatchObject({
-      caseCaption: 'Test Petitioner, Petitioner',
-      caseType: 'Other',
-      contactPrimary: {
-        address1: '123 Main St',
-        city: 'Somewhere',
-        email: 'petitioner@example.com',
-        name: 'Test Petitioner',
-        phone: '1234567',
-        postalCode: '12345',
-        state: 'TN',
-      },
-      docketEntries: [],
-      docketNumber: '101-18',
-      docketNumberSuffix: null,
-      filingType: 'Myself',
-      noticeOfTrialDate: '2020-10-20T01:38:43.489Z',
-      partyType: 'Petitioner',
-      practitioners: [],
-      preferredTrialCity: 'Washington, District of Columbia',
-      respondents: [],
-      sortableDocketNumber: 2018000101,
-      status: 'Calendared',
-    });
-  });
-
-  it('returns the case in v1 format when feature flag is off', async () => {
-    // Careful! Changing this test would mean that the v1 format is changing;
-    // this would mean breaking changes for any user of the v1 API
-    const user = MOCK_USERS['b7d90c05-f6cd-442c-a168-202db587f16f'];
-    const applicationContext = createSilentApplicationContext(user);
-
-    applicationContext.getUseCases().getFeatureFlagValueInteractor = jest
-      .fn()
-      .mockResolvedValue(false);
-
-    applicationContext.getDocumentClient = jest.fn().mockReturnValue({
-      query: jest.fn().mockReturnValue({
-        promise: jest.fn().mockReturnValue(
-          Promise.resolve({
-            Items: [mockDynamoCaseRecord],
-          }),
-        ),
-      }),
-    });
-
-    const response = await getCaseLambda(REQUEST_EVENT, {
-      applicationContext,
-    });
-
-    expect(response.statusCode).toBe('200');
-    expect(response.headers['Content-Type']).toBe('application/json');
-    expect(JSON.parse(response.body)).toMatchObject({
-      caseCaption: 'Test Petitioner, Petitioner',
-      caseType: 'Other',
-      contactPrimary: {
-        address1: '123 Main St',
-        city: 'Somewhere',
-        email: 'petitioner@example.com',
-        name: 'Test Petitioner',
-        phone: '1234567',
-        postalCode: '12345',
-        state: 'TN',
-      },
-      docketEntries: [],
-      docketNumber: '101-18',
-      docketNumberSuffix: null,
-      filingType: 'Myself',
-      noticeOfTrialDate: '2020-10-20T01:38:43.489Z',
-      partyType: 'Petitioner',
-      practitioners: [],
-      preferredTrialCity: 'Washington, District of Columbia',
-      respondents: [],
-      sortableDocketNumber: 2018000101,
-      status: 'Calendared',
+      expect(response.statusCode).toBe('200');
+      expect(response.headers['Content-Type']).toBe('application/json');
+      expect(JSON.parse(response.body)).toMatchObject(mockExpectedResponse);
     });
   });
 });
