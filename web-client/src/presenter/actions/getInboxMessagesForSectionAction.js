00430048ec787d17564e63cad5ed4668d734eaac
Rosie Highsmith <ahighsmith@flexion.us>
2023-01-23 10:23:56 -0700

    experimenting with adding optional param for section to routes

--- web-client/src/presenter/actions/getInboxMessagesForSectionAction.js
+++ web-client/src/presenter/actions/getInboxMessagesForSectionAction.js
@@ -6,11 +6,18 @@
  */
 export const getInboxMessagesForSectionAction = async ({
   applicationContext,
+  props,
 }) => {
+  console.log('getinboxmsgs section props', props);
+  const { section: selectedSection } = props;
+  // maybe at this point we should get the button that was clicked
+  // either petitions or docket section
+  // off of props?
+  // if the user is CCS role
   const messages = await applicationContext
     .getUseCases()
     .getInboxMessagesForSectionInteractor(applicationContext, {
-      section: applicationContext.getCurrentUser().section,
+      section: selectedSection || applicationContext.getCurrentUser().section,
     });
 
   return { messages };
