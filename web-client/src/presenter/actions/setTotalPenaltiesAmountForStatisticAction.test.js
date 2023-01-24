353e326c3087b661a403eef33f2043161edd0a58
Timothy Shumway <tshumway@flexion.us>
2023-01-23 22:59:20 -0700

    9664 - fix test coverage for changed files, add missing test coverage

--- web-client/src/presenter/actions/setTotalPenaltiesAmountForStatisticAction.test.js
+++ web-client/src/presenter/actions/setTotalPenaltiesAmountForStatisticAction.test.js
@@ -2,41 +2,62 @@ import { runAction } from 'cerebral/test';
 import { setTotalPenaltiesAmountForStatisticAction } from './setTotalPenaltiesAmountForStatisticAction';
 
 describe('setTotalPenaltiesAmountForStatisticAction,', () => {
-  it('sets the totalPenalties value for the given index', async () => {
-    const result = await runAction(setTotalPenaltiesAmountForStatisticAction, {
-      props: {
-        totalPenalties: '$112.99',
-      },
-      state: {
-        form: {
-          statistics: [
-            {
-              irsTotalPenalties: '$1.00',
-            },
-            {
-              irsTotalPenalties: '$2.00',
-            },
-            {
-              irsTotalPenalties: '$3.00',
-            },
-          ],
-        },
-        modal: {
-          statisticIndex: 1,
-        },
-      },
-    });
+  const key = 'irsTotalPenalties';
+  const statisticIndex = 0;
+  const sumOfPenalties = '$112.99';
+  const penalties = [
+    {
+      irsTotalPenalties: '$1.00',
+    },
+    {
+      irsTotalPenalties: '$2.00',
+    },
+    {
+      irsTotalPenalties: '$3.00',
+    },
+  ];
 
-    expect(result.state.form.statistics).toMatchObject([
+  it('sets sumOfPenalties and penalties on the proper state when statisticIndex is defined and a number', async () => {
+    const { state } = await runAction(
+      setTotalPenaltiesAmountForStatisticAction,
       {
-        irsTotalPenalties: '$1.00',
+        props: {
+          penalties,
+          sumOfPenalties,
+        },
+        state: {
+          form: {},
+          modal: {
+            key,
+            statisticIndex,
+          },
+        },
       },
+    );
+
+    expect(state.form.statistics[statisticIndex].penalties).toEqual(penalties);
+    expect(state.form.statistics[statisticIndex][key]).toEqual(sumOfPenalties);
+  });
+
+  it('sets sumOfPenalties and penalties on the proper state when statisticIndex is undefined', async () => {
+    const { state } = await runAction(
+      setTotalPenaltiesAmountForStatisticAction,
       {
-        irsTotalPenalties: '$112.99',
+        props: {
+          penalties,
+          sumOfPenalties,
+        },
+        state: {
+          form: {},
+          modal: {
+            key,
+            statisticIndex: undefined,
+          },
+        },
       },
-      {
-        irsTotalPenalties: '$3.00',
-      },
-    ]);
+    );
+
+    expect(state.form.penalties).toEqual(penalties);
+    expect(state.form[key]).toEqual(sumOfPenalties);
   });
 });
--- web-client/src/presenter/actions/validatePenaltiesAction.test.js
+++ web-client/src/presenter/actions/validatePenaltiesAction.test.js
@@ -6,6 +6,42 @@ import { validatePenaltiesAction } from './validatePenaltiesAction';
 describe('validatePenaltiesAction', () => {
   let successStub;
   let errorStub;
+  const statisticId = applicationContext.getUniqueId();
+  const validIrsPenalty1Mock = {
+    name: 'Penalty 1 (IRS)',
+    penaltyAmount: '1.00',
+    penaltyType:
+      applicationContext.getConstants().PENALTY_TYPES.IRS_PENALTY_AMOUNT,
+    statisticId,
+  };
+  const invalidIrsPenalty1Mock = {
+    name: 'Penalty 1 (IRS)',
+    penaltyAmount: '',
+    penaltyType:
+      applicationContext.getConstants().PENALTY_TYPES.IRS_PENALTY_AMOUNT,
+  };
+  const validIrsPenalty2Mock = {
+    name: 'Penalty 2 (IRS)',
+    penaltyAmount: '2.00',
+    penaltyType:
+      applicationContext.getConstants().PENALTY_TYPES.IRS_PENALTY_AMOUNT,
+    statisticId,
+  };
+
+  const invalidIrsPenalty2Mock = {
+    name: 'Penalty 2 (IRS)',
+    penaltyAmount: '2.00',
+    penaltyType: undefined,
+  };
+
+  const validCourtPenalty1Mock = {
+    name: 'Penalty 1 (USTC)',
+    penaltyAmount: '3.00',
+    penaltyType:
+      applicationContext.getConstants().PENALTY_TYPES
+        .DETERMINATION_PENALTY_AMOUNT,
+    statisticId,
+  };
 
   beforeAll(() => {
     successStub = jest.fn();
@@ -16,10 +52,6 @@ describe('validatePenaltiesAction', () => {
       error: errorStub,
       success: successStub,
     };
-
-    applicationContext
-      .getUseCases()
-      .validatePenaltiesInteractor.mockReturnValue(null);
   });
 
   it('should call the error path when current penalties has a length less than 1', async () => {
@@ -27,7 +59,7 @@ describe('validatePenaltiesAction', () => {
       modules: {
         presenter,
       },
-      props: { allPenalties: [], itemizedPenaltiesOfCurrentType: [] },
+      props: { itemizedPenaltiesOfCurrentType: [], listOfAllPenalties: [] },
       state: {},
     });
 
@@ -42,13 +74,19 @@ describe('validatePenaltiesAction', () => {
     });
   });
 
-  //TODO: do the thing
-  it('should call the success path when current penalties has a length greather than 1', async () => {
+  it('should call the error path when listOfAllPenalties has penalties with validation errors', async () => {
     await runAction(validatePenaltiesAction, {
       modules: {
         presenter,
       },
-      props: { allPenalties: [], itemizedPenaltiesOfCurrentType: [3] },
+      props: {
+        itemizedPenaltiesOfCurrentType: [validCourtPenalty1Mock],
+        listOfAllPenalties: [
+          validCourtPenalty1Mock,
+          invalidIrsPenalty1Mock,
+          invalidIrsPenalty2Mock,
+        ],
+      },
       state: {},
     });
 
@@ -58,8 +96,35 @@ describe('validatePenaltiesAction', () => {
         title: 'Errors were found. Please correct your form and resubmit.',
       },
       error: {
-        penaltyAmount: 'Please enter a penalty.',
+        penaltyAmount: 'Enter penalty amount.',
+        penaltyType: 'Type of penalty is required.',
+        statisticId: 'Statistic ID is required.',
       },
     });
+  });
+
+  it('should call the success path when there are no errors in the errors object', async () => {
+    const props = {
+      itemizedPenaltiesOfCurrentType: [
+        validIrsPenalty1Mock,
+        validIrsPenalty2Mock,
+      ],
+      listOfAllPenalties: [
+        validCourtPenalty1Mock,
+        validIrsPenalty1Mock,
+        validIrsPenalty2Mock,
+      ],
+    };
+
+    await runAction(validatePenaltiesAction, {
+      modules: {
+        presenter,
+      },
+      props,
+      state: {},
+    });
+
+    expect(successStub).toHaveBeenCalled();
+    expect(successStub.mock.calls[0][0]).toEqual(props);
   });
 });
