0516539c9d48a99e8ebae586b85ac99bbac3b547
Absolutestunna <joshujah007@gmail.com>
2023-01-27 12:56:58 -0500

    9616 - create and use new websocket route to communicate the completion of saving a paper filing

--- shared/src/business/useCases/docketEntry/editPaperFilingInteractor.ts
+++ shared/src/business/useCases/docketEntry/editPaperFilingInteractor.ts
@@ -122,14 +122,12 @@ const saveForLaterStrategy = async ({
     applicationContext,
     clientConnectionId,
     message: {
-      action: 'serve_document_complete',
+      action: 'save_document_for_qc',
       alertSuccess: {
         message: 'Entry updated.',
         overwritable: false,
       },
       docketEntryId,
-      generateCoversheet: false,
-      pdfUrl: undefined,
     },
     userId: user.userId,
   });
