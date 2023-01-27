8da9f692d7f916803f2934cc945fb4044a5d71c8
Absolutestunna <joshujah007@gmail.com>
2023-01-27 16:28:29 -0500

    9616 - removed uncessary actions for saveDocumentForQCSequence

--- web-client/src/presenter/sequences/saveDocumentForQCSequence.js
+++ web-client/src/presenter/sequences/saveDocumentForQCSequence.js
@@ -1,12 +1,6 @@
 import { clearModalAction } from '../actions/clearModalAction';
-import { followRedirectAction } from '../actions/followRedirectAction';
-import { getCaseAction } from '../actions/getCaseAction';
-import { getConsolidatedCasesByCaseAction } from '../actions/CaseConsolidation/getConsolidatedCasesByCaseAction';
 import { navigateToCaseDetailAction } from '../actions/navigateToCaseDetailAction';
 import { setAlertSuccessAction } from '../actions/setAlertSuccessAction';
-import { setCaseAction } from '../actions/setCaseAction';
-import { setConsolidatedCasesForCaseAction } from '../actions/CaseConsolidation/setConsolidatedCasesForCaseAction';
-import { setDocumentToDisplayFromDocumentIdAction } from '../actions/setDocumentToDisplayFromDocumentIdAction';
 import { setSaveAlertsForNavigationAction } from '../actions/setSaveAlertsForNavigationAction';
 import { unsetWaitingForResponseAction } from '../actions/unsetWaitingForResponseAction';
 
@@ -15,15 +9,5 @@ export const saveDocumentForQCSequence = [
   unsetWaitingForResponseAction,
   setAlertSuccessAction,
   setSaveAlertsForNavigationAction,
-  followRedirectAction, // check for necessity of followRedirection in context of saving a filing
-  {
-    default: [navigateToCaseDetailAction],
-    success: [
-      getCaseAction,
-      setCaseAction,
-      getConsolidatedCasesByCaseAction,
-      setConsolidatedCasesForCaseAction,
-      setDocumentToDisplayFromDocumentIdAction,
-    ],
-  },
+  navigateToCaseDetailAction,
 ];
