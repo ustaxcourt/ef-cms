7fe57e24168e57939b944dc8d570b6198316196d
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2023-01-27 12:07:20 -0700

    9755: wip removing css section from create message modal dropdown

--- web-client/src/presenter/computeds/workQueueSectionHelper.js
+++ web-client/src/presenter/computeds/workQueueSectionHelper.js
@@ -1,14 +1,22 @@
+import { omit } from 'lodash';
+
 export const workQueueSectionHelper = (get, applicationContext) => {
   const CHAMBERS_SECTIONS_LABELS = applicationContext
     .getPersistenceGateway()
     .getChambersSectionsLabels();
+
+  const { CASE_SERVICES_SUPERVISOR_SECTION, SECTIONS } =
+    applicationContext.getConstants();
+  //move to create message modal helper eventually
+  const toSectionList = omit(SECTIONS, [CASE_SERVICES_SUPERVISOR_SECTION]);
+  console.log(SECTIONS, 'SECTIONS--');
+  console.log(toSectionList, 'toSectionList--');
 
   const sectionDisplay = key => {
     return (
       {
         adc: 'ADC',
         admissions: 'Admissions',
-        caseServicesSupervisor: 'Case Services Supervisor',
         chambers: 'Chambers',
         clerkofcourt: 'Clerk of the Court',
         docket: 'Docket',
@@ -27,10 +35,12 @@ export const workQueueSectionHelper = (get, applicationContext) => {
   const chambersSections = applicationContext
     .getPersistenceGateway()
     .getChambersSections();
+  console.log(toSectionList, '----toSectionList');
 
   return {
     chambersDisplay,
     chambersSections,
     sectionDisplay,
+    toSectionList,
   };
 };
