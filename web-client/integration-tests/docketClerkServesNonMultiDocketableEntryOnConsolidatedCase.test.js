db92268fb10df83d3e3f5bf898073c3715ec457f
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-01-27 14:41:50 -0800

    9616: DOD - refactor integration tests to use new describe for creating consolidated groups

--- web-client/integration-tests/docketClerkServesNonMultiDocketableEntryOnConsolidatedCase.test.js
+++ web-client/integration-tests/docketClerkServesNonMultiDocketableEntryOnConsolidatedCase.test.js
@@ -3,15 +3,11 @@ import {
   SINGLE_DOCKET_RECORD_ONLY_EVENT_CODES,
 } from '../../shared/src/business/entities/EntityConstants';
 import { confirmInitiatePaperFilingServiceModalHelper } from '../src/presenter/computeds/confirmInitiatePaperFilingServiceModalHelper';
+import { createConsolidatedGroup } from './journey/consolidation/createConsolidatedGroup';
 import { docketClerkAddsPaperFiledDocketEntryAndSavesForLater } from './journey/docketClerkAddsPaperFiledDocketEntryAndSavesForLater';
-import { docketClerkConsolidatesCases } from './journey/docketClerkConsolidatesCases';
-import { docketClerkOpensCaseConsolidateModal } from './journey/docketClerkOpensCaseConsolidateModal';
-import { docketClerkSearchesForCaseToConsolidateWith } from './journey/docketClerkSearchesForCaseToConsolidateWith';
-import { docketClerkUpdatesCaseStatusToReadyForTrial } from './journey/docketClerkUpdatesCaseStatusToReadyForTrial';
 import { fakeFile } from '../integration-tests-public/helpers';
-import { loginAs, setupTest, uploadPetition } from './helpers';
-import { petitionsClerkServesElectronicCaseToIrs } from './journey/petitionsClerkServesElectronicCaseToIrs';
 import { runCompute } from 'cerebral/test';
+import { setupTest } from './helpers';
 import { withAppContextDecorator } from '../src/withAppContext';
 
 describe('Docket Clerk serves non multi-docketable entry on consolidated case', () => {
@@ -21,76 +17,58 @@ describe('Docket Clerk serves non multi-docketable entry on consolidated case', 
     cerebralTest.closeSocket();
   });
 
-  it('login as a petitioner and create the lead case', async () => {
-    const caseDetail = await uploadPetition(cerebralTest);
-    expect(caseDetail.docketNumber).toBeDefined();
-    cerebralTest.docketNumber = cerebralTest.leadDocketNumber =
-      caseDetail.docketNumber;
+  describe('Create a consolidated group', () => {
+    createConsolidatedGroup(cerebralTest);
   });
 
-  loginAs(cerebralTest, 'petitionsclerk@example.com');
-  petitionsClerkServesElectronicCaseToIrs(cerebralTest);
+  describe('Create a non-multidocketable docket entry on the lead case and serve', () => {
+    const documentFormValues = {
+      dateReceivedDay: 1,
+      dateReceivedMonth: 1,
+      dateReceivedYear: 2018,
+      eventCode: SINGLE_DOCKET_RECORD_ONLY_EVENT_CODES[0],
+      objections: OBJECTIONS_OPTIONS_MAP.NO,
+      primaryDocumentFile: fakeFile,
+      primaryDocumentFileSize: 100,
+    };
 
-  loginAs(cerebralTest, 'docketclerk@example.com');
-  docketClerkUpdatesCaseStatusToReadyForTrial(cerebralTest);
-  const documentFormValues = {
-    dateReceivedDay: 1,
-    dateReceivedMonth: 1,
-    dateReceivedYear: 2018,
-    eventCode: SINGLE_DOCKET_RECORD_ONLY_EVENT_CODES[0],
-    objections: OBJECTIONS_OPTIONS_MAP.NO,
-    primaryDocumentFile: fakeFile,
-    primaryDocumentFileSize: 100,
-  };
-
-  docketClerkAddsPaperFiledDocketEntryAndSavesForLater({
-    cerebralTest,
-    documentFormValues,
-    expectedDocumentType: 'Agreed Computation for Entry of Decision',
-  });
-
-  it('login as a petitioner and create a case to consolidate with', async () => {
-    cerebralTest.docketNumberDifferentPlaceOfTrial = null;
-    const caseDetail = await uploadPetition(cerebralTest);
-    expect(caseDetail.docketNumber).toBeDefined();
-    cerebralTest.docketNumber = caseDetail.docketNumber;
-  });
-
-  loginAs(cerebralTest, 'petitionsclerk@example.com');
-  petitionsClerkServesElectronicCaseToIrs(cerebralTest);
-
-  loginAs(cerebralTest, 'docketclerk@example.com');
-  docketClerkUpdatesCaseStatusToReadyForTrial(cerebralTest);
-  docketClerkOpensCaseConsolidateModal(cerebralTest);
-  docketClerkSearchesForCaseToConsolidateWith(cerebralTest);
-  docketClerkConsolidatesCases(cerebralTest, 2);
-
-  it('docket clerk serves decision document from document viewer', async () => {
-    await cerebralTest.runSequence('gotoCaseDetailSequence', {
-      docketNumber: cerebralTest.leadDocketNumber,
+    it('setup docket number', () => {
+      cerebralTest.docketNumber = cerebralTest.leadDocketNumber;
     });
 
-    await cerebralTest.runSequence(
-      'openConfirmServePaperFiledDocumentSequence',
-      {
-        docketEntryId: cerebralTest.docketEntryId,
-        redirectUrl: `/case-detail/${cerebralTest.leadDocketNumber}/document-view?docketEntryId=${cerebralTest.docketEntryId}`,
-      },
-    );
+    docketClerkAddsPaperFiledDocketEntryAndSavesForLater({
+      cerebralTest,
+      documentFormValues,
+      expectedDocumentType: 'Agreed Computation for Entry of Decision',
+    });
 
-    expect(cerebralTest.getState('modal.showModal')).toEqual(
-      'ConfirmInitiatePaperFilingServiceModal',
-    );
-  });
+    it('docket clerk serves decision document from document viewer', async () => {
+      await cerebralTest.runSequence('gotoCaseDetailSequence', {
+        docketNumber: cerebralTest.leadDocketNumber,
+      });
 
-  it('docket clerk verifies consolidated case checkboxes do NOT display for case decision service', () => {
-    const modalHelper = runCompute(
-      withAppContextDecorator(confirmInitiatePaperFilingServiceModalHelper),
-      {
-        state: cerebralTest.getState(),
-      },
-    );
+      await cerebralTest.runSequence(
+        'openConfirmServePaperFiledDocumentSequence',
+        {
+          docketEntryId: cerebralTest.docketEntryId,
+          redirectUrl: `/case-detail/${cerebralTest.leadDocketNumber}/document-view?docketEntryId=${cerebralTest.docketEntryId}`,
+        },
+      );
 
-    expect(modalHelper.showConsolidatedCasesForService).toBe(false);
+      expect(cerebralTest.getState('modal.showModal')).toEqual(
+        'ConfirmInitiatePaperFilingServiceModal',
+      );
+    });
+
+    it('docket clerk verifies consolidated case checkboxes do NOT display for case decision service', () => {
+      const modalHelper = runCompute(
+        withAppContextDecorator(confirmInitiatePaperFilingServiceModalHelper),
+        {
+          state: cerebralTest.getState(),
+        },
+      );
+
+      expect(modalHelper.showConsolidatedCasesForService).toBe(false);
+    });
   });
 });
--- web-client/integration-tests/docketClerkVerifiesConsolidatedCases.test.js
+++ web-client/integration-tests/docketClerkVerifiesConsolidatedCases.test.js
@@ -1,16 +1,13 @@
+import { createConsolidatedGroup } from './journey/consolidation/createConsolidatedGroup';
 import { docketClerkAddsCaseToHearing } from './journey/docketClerkAddsCaseToHearing';
 import { docketClerkAddsTrackedDocketEntry } from './journey/docketClerkAddsTrackedDocketEntry';
-import { docketClerkConsolidatesCases } from './journey/docketClerkConsolidatesCases';
 import { docketClerkCreatesATrialSession } from './journey/docketClerkCreatesATrialSession';
-import { docketClerkOpensCaseConsolidateModal } from './journey/docketClerkOpensCaseConsolidateModal';
 import { docketClerkRemovesCaseFromHearing } from './journey/docketClerkRemovesCaseFromHearing';
 import { docketClerkSealsCase } from './journey/docketClerkSealsCase';
-import { docketClerkSearchesForCaseToConsolidateWith } from './journey/docketClerkSearchesForCaseToConsolidateWith';
 import { docketClerkUnsealsCase } from './journey/docketClerkUnsealsCase';
-import { docketClerkUpdatesCaseStatusToReadyForTrial } from './journey/docketClerkUpdatesCaseStatusToReadyForTrial';
 import { docketClerkVerifiesConsolidatedCases } from './journey/docketClerkVerifiesConsolidatedCases';
 import { docketClerkViewsTrialSessionList } from './journey/docketClerkViewsTrialSessionList';
-import { fakeFile, loginAs, setupTest, uploadPetition } from './helpers';
+import { fakeFile, loginAs, setupTest } from './helpers';
 import { manuallyAddCaseToTrial } from './utils/manuallyAddCaseToTrial';
 import { petitionsClerkBlocksCase } from './journey/petitionsClerkBlocksCase';
 import { petitionsClerkPrioritizesCase } from './journey/petitionsClerkPrioritizesCase';
@@ -21,8 +18,10 @@ import { updateACaseCaption } from './journey/updateACaseCaption';
 
 describe('Docket Clerk verifies Consolidated Cases', () => {
   const cerebralTest = setupTest();
-  const trialLocation = `Boise, Idaho, ${Date.now()}`;
 
+  cerebralTest.createdTrialSessions = [];
+
+  const trialLocation = `Boise, Idaho, ${Date.now()}`;
   const overrides = {
     caseCaption: 'Mona Schultz, Petitioner',
     docketNumberSuffix: 'L',
@@ -34,87 +33,69 @@ describe('Docket Clerk verifies Consolidated Cases', () => {
     cerebralTest.closeSocket();
   });
 
-  cerebralTest.createdTrialSessions = [];
-
-  loginAs(cerebralTest, 'petitioner@example.com');
-  it('login as a petitioner and create the lead case', async () => {
-    const caseDetail = await uploadPetition(cerebralTest, overrides);
-    expect(caseDetail.docketNumber).toBeDefined();
-    cerebralTest.docketNumber = cerebralTest.leadDocketNumber =
-      caseDetail.docketNumber;
+  describe('Create a consolidated group', () => {
+    createConsolidatedGroup({
+      caseOverrides: overrides,
+      cerebralTest,
+    });
   });
 
-  loginAs(cerebralTest, 'docketclerk@example.com');
-  docketClerkUpdatesCaseStatusToReadyForTrial(cerebralTest);
+  describe('Verify consolidated group functionality', () => {
+    docketClerkSealsCase(cerebralTest);
+    docketClerkVerifiesConsolidatedCases(cerebralTest);
 
-  loginAs(cerebralTest, 'petitioner@example.com');
-  it('login as a petitioner and create the case to consolidate with', async () => {
-    cerebralTest.docketNumberDifferentPlaceOfTrial = null;
-    const caseDetail = await uploadPetition(cerebralTest, overrides);
-    expect(caseDetail.docketNumber).toBeDefined();
-    cerebralTest.docketNumber = caseDetail.docketNumber;
+    docketClerkUnsealsCase(cerebralTest);
+    docketClerkVerifiesConsolidatedCases(cerebralTest);
+
+    docketClerkCreatesATrialSession(cerebralTest, {
+      sessionType: 'Motion/Hearing',
+      trialLocation,
+    });
+    docketClerkViewsTrialSessionList(cerebralTest);
+    docketClerkAddsCaseToHearing(cerebralTest, 'Low blast radius', 0);
+    docketClerkVerifiesConsolidatedCases(cerebralTest);
+
+    docketClerkRemovesCaseFromHearing(cerebralTest);
+    docketClerkVerifiesConsolidatedCases(cerebralTest);
+
+    manuallyAddCaseToTrial(cerebralTest);
+    docketClerkVerifiesConsolidatedCases(cerebralTest);
+
+    loginAs(cerebralTest, 'petitionsclerk@example.com');
+    petitionsClerkPrioritizesCase(cerebralTest);
+
+    loginAs(cerebralTest, 'docketclerk@example.com');
+    docketClerkVerifiesConsolidatedCases(cerebralTest);
+
+    loginAs(cerebralTest, 'petitionsclerk@example.com');
+    petitionsClerkUnprioritizesCase(cerebralTest);
+
+    loginAs(cerebralTest, 'docketclerk@example.com');
+    docketClerkVerifiesConsolidatedCases(cerebralTest);
+
+    loginAs(cerebralTest, 'petitionsclerk@example.com');
+    petitionsClerkBlocksCase(cerebralTest, trialLocation, overrides);
+
+    loginAs(cerebralTest, 'docketclerk@example.com');
+    docketClerkVerifiesConsolidatedCases(cerebralTest);
+
+    loginAs(cerebralTest, 'petitionsclerk@example.com');
+    petitionsClerkUnblocksCase(cerebralTest, trialLocation);
+
+    loginAs(cerebralTest, 'docketclerk@example.com');
+    docketClerkVerifiesConsolidatedCases(cerebralTest);
+
+    const updatedCaseCaption = 'I am the new case caption!';
+    updateACaseCaption(cerebralTest, updatedCaseCaption);
+    it('docketClerk updates Case Caption', () => {
+      const caseDetail = cerebralTest.getState('caseDetail');
+      expect(caseDetail.caseCaption).toBeDefined();
+      expect(caseDetail.caseCaption).toEqual(updatedCaseCaption);
+    });
+    docketClerkVerifiesConsolidatedCases(cerebralTest);
+
+    docketClerkAddsTrackedDocketEntry(cerebralTest, fakeFile, false);
+    removePendingItemFromCase(cerebralTest, 'Docket Clerk');
+    docketClerkVerifiesConsolidatedCases(cerebralTest);
   });
-
-  loginAs(cerebralTest, 'docketclerk@example.com');
-  docketClerkUpdatesCaseStatusToReadyForTrial(cerebralTest);
-  docketClerkOpensCaseConsolidateModal(cerebralTest);
-  docketClerkSearchesForCaseToConsolidateWith(cerebralTest);
-  docketClerkConsolidatesCases(cerebralTest, 2);
-
-  docketClerkSealsCase(cerebralTest);
-  docketClerkVerifiesConsolidatedCases(cerebralTest);
-
-  docketClerkUnsealsCase(cerebralTest);
-  docketClerkVerifiesConsolidatedCases(cerebralTest);
-
-  docketClerkCreatesATrialSession(cerebralTest, {
-    sessionType: 'Motion/Hearing',
-    trialLocation,
-  });
-  docketClerkViewsTrialSessionList(cerebralTest);
-  docketClerkAddsCaseToHearing(cerebralTest, 'Low blast radius', 0);
-  docketClerkVerifiesConsolidatedCases(cerebralTest);
-
-  docketClerkRemovesCaseFromHearing(cerebralTest);
-  docketClerkVerifiesConsolidatedCases(cerebralTest);
-
-  manuallyAddCaseToTrial(cerebralTest);
-  docketClerkVerifiesConsolidatedCases(cerebralTest);
-
-  loginAs(cerebralTest, 'petitionsclerk@example.com');
-  petitionsClerkPrioritizesCase(cerebralTest);
-
-  loginAs(cerebralTest, 'docketclerk@example.com');
-  docketClerkVerifiesConsolidatedCases(cerebralTest);
-
-  loginAs(cerebralTest, 'petitionsclerk@example.com');
-  petitionsClerkUnprioritizesCase(cerebralTest);
-
-  loginAs(cerebralTest, 'docketclerk@example.com');
-  docketClerkVerifiesConsolidatedCases(cerebralTest);
-
-  loginAs(cerebralTest, 'petitionsclerk@example.com');
-  petitionsClerkBlocksCase(cerebralTest, trialLocation, overrides);
-
-  loginAs(cerebralTest, 'docketclerk@example.com');
-  docketClerkVerifiesConsolidatedCases(cerebralTest);
-
-  loginAs(cerebralTest, 'petitionsclerk@example.com');
-  petitionsClerkUnblocksCase(cerebralTest, trialLocation);
-
-  loginAs(cerebralTest, 'docketclerk@example.com');
-  docketClerkVerifiesConsolidatedCases(cerebralTest);
-
-  const updatedCaseCaption = 'I am the new case caption!';
-  updateACaseCaption(cerebralTest, updatedCaseCaption);
-  it('docketClerk updates Case Caption', () => {
-    const caseDetail = cerebralTest.getState('caseDetail');
-    expect(caseDetail.caseCaption).toBeDefined();
-    expect(caseDetail.caseCaption).toEqual(updatedCaseCaption);
-  });
-  docketClerkVerifiesConsolidatedCases(cerebralTest);
-
-  docketClerkAddsTrackedDocketEntry(cerebralTest, fakeFile, false);
-  removePendingItemFromCase(cerebralTest, 'Docket Clerk');
-  docketClerkVerifiesConsolidatedCases(cerebralTest);
 });
