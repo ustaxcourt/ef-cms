6dd162ee2f585d9556bbb5ff0f6fe559ba0ed6ef
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-01-31 07:29:43 -0800

    9616: Add test for multi docketing a court issued document on a case with paper service, WIP

--- web-client/src/presenter/sequences/openConfirmServeCourtIssuedDocumentSequence.js
+++ web-client/src/presenter/sequences/openConfirmServeCourtIssuedDocumentSequence.js
@@ -1,5 +1,7 @@
 import { clearModalStateAction } from '../actions/clearModalStateAction';
+import { getConsolidatedCasesByCaseAction } from '../actions/CaseConsolidation/getConsolidatedCasesByCaseAction';
 import { isDocketEntryMultiDocketableAction } from '../actions/CaseConsolidation/isDocketEntryMultiDocketableAction';
+import { setConsolidatedCasesForCaseAction } from '../actions/CaseConsolidation/setConsolidatedCasesForCaseAction';
 import { setDocketEntryIdAction } from '../actions/setDocketEntryIdAction';
 import { setMultiDocketingCheckboxesAction } from '../actions/CaseConsolidation/setMultiDocketingCheckboxesAction';
 import { setRedirectUrlAction } from '../actions/setRedirectUrlAction';
@@ -12,7 +14,11 @@ export const openConfirmServeCourtIssuedDocumentSequence = [
   isDocketEntryMultiDocketableAction,
   {
     no: [],
-    yes: [setMultiDocketingCheckboxesAction],
+    yes: [
+      getConsolidatedCasesByCaseAction,
+      setConsolidatedCasesForCaseAction,
+      setMultiDocketingCheckboxesAction,
+    ],
   },
   setShowModalFactoryAction('ConfirmInitiateCourtIssuedFilingServiceModal'),
 ];
