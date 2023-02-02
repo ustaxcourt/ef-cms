d128cf601cc96a3109bf298eebd93d489ca29613
Rosie Highsmith <ahighsmith@flexion.us>
2023-02-02 11:35:29 -0700

    9755: added more tests around getWorkQueueFilters

--- shared/src/business/utilities/getWorkQueueFilters.test.js
+++ shared/src/business/utilities/getWorkQueueFilters.test.js
@@ -1,4 +1,5 @@
 const {
+  CASE_SERVICES_SUPERVISOR_SECTION,
   CASE_STATUS_TYPES,
   DOCKET_SECTION,
   PETITIONS_SECTION,
@@ -338,6 +339,161 @@ describe('getWorkQueueFilters', () => {
           expect.objectContaining({ workItemId: '4' }),
           expect.objectContaining({ workItemId: '8' }),
         ]);
+      });
+    });
+
+    describe('filters for case services supervisor', () => {
+      let user;
+
+      beforeAll(() => {
+        user = {
+          role: ROLES.caseServicesSupervisor,
+          section: CASE_SERVICES_SUPERVISOR_SECTION,
+          userId: '123',
+        };
+      });
+
+      it('returns an object containing a filter map for my work queues and boxes', () => {
+        let myWorkItems = [
+          {
+            // my in progress
+            assigneeId: '123',
+            docketEntry: {
+              isFileAttached: false,
+            },
+            section: CASE_SERVICES_SUPERVISOR_SECTION,
+            workItemId: '1',
+          },
+          {
+            // my in progress
+            assigneeId: '123',
+            caseIsInProgress: true,
+            caseStatus: CASE_STATUS_TYPES.new,
+            docketEntry: {},
+            section: CASE_SERVICES_SUPERVISOR_SECTION,
+            workItemId: '2',
+          },
+          {
+            // my inbox
+            assigneeId: '123',
+            caseIsInProgress: false,
+            docketEntry: {
+              isFileAttached: true,
+            },
+            inProgress: false,
+            section: CASE_SERVICES_SUPERVISOR_SECTION,
+            workItemId: '3',
+          },
+          {
+            // my outbox
+            assigneeId: '123',
+            caseIsInProgress: false,
+            completedAt: '2019-06-17T15:27:55.801Z',
+            completedByUserId: '123',
+            docketEntry: {
+              isFileAttached: true,
+            },
+            inProgress: false,
+            section: CASE_SERVICES_SUPERVISOR_SECTION,
+            workItemId: '4',
+          },
+        ];
+
+        const filters = getWorkQueueFilters({
+          user,
+        });
+
+        const myInProgress = myWorkItems.filter(filters['my']['inProgress']);
+        const myInbox = myWorkItems.filter(filters['my']['inbox']);
+        const myOutbox = myWorkItems.filter(filters['my']['outbox']);
+
+        expect(myInProgress).toMatchObject([
+          expect.objectContaining({ workItemId: '1' }),
+          expect.objectContaining({ workItemId: '2' }),
+        ]);
+        expect(myInbox).toMatchObject([
+          expect.objectContaining({ workItemId: '3' }),
+        ]);
+        expect(myOutbox).toMatchObject([
+          expect.objectContaining({ workItemId: '4' }),
+        ]);
+      });
+
+      [PETITIONS_SECTION, DOCKET_SECTION].forEach(sectionToTest => {
+        it(`returns an object containing a filter map for ${sectionToTest} section work queues and boxes`, () => {
+          let sectionWorkItems = [
+            {
+              // section in progress
+              assigneeId: '234',
+              caseIsInProgress: true,
+              caseStatus: CASE_STATUS_TYPES.new,
+              docketEntry: {
+                isFileAttached: false,
+              },
+              section: `${sectionToTest}`,
+              workItemId: '5',
+            },
+            {
+              // section in progress
+              assigneeId: '234',
+              caseIsInProgress: true,
+              caseStatus: CASE_STATUS_TYPES.new,
+              docketEntry: {},
+              section: `${sectionToTest}`,
+              workItemId: '6',
+            },
+            {
+              // section inbox
+              assigneeId: '234',
+              caseIsInProgress: false,
+              docketEntry: {
+                isFileAttached: true,
+              },
+              inProgress: false,
+              section: `${sectionToTest}`,
+              workItemId: '7',
+            },
+            {
+              // section outbox
+              assigneeId: '234',
+              caseIsInProgress: false,
+              completedAt: '2019-06-17T15:27:55.801Z',
+              completedByUserId: '234',
+              docketEntry: {
+                isFileAttached: true,
+              },
+              inProgress: false,
+              section: `${sectionToTest}`,
+              workItemId: '8',
+            },
+          ];
+
+          const filters = getWorkQueueFilters({
+            section: `${sectionToTest}`,
+            user,
+          });
+
+          const sectionInProgress = sectionWorkItems.filter(
+            filters['section']['inProgress'],
+          );
+          const sectionInbox = sectionWorkItems.filter(
+            filters['section']['inbox'],
+          );
+          const sectionOutbox = sectionWorkItems.filter(
+            filters['section']['outbox'],
+          );
+
+          expect(sectionInProgress).toMatchObject([
+            expect.objectContaining({ workItemId: '5' }),
+            expect.objectContaining({ workItemId: '6' }),
+          ]);
+          expect(sectionInbox).toMatchObject([
+            expect.objectContaining({ workItemId: '7' }),
+          ]);
+          expect(sectionOutbox).toMatchObject([
+            expect.objectContaining({ workItemId: '8' }),
+          ]);
+        });
       });
     });
   });
