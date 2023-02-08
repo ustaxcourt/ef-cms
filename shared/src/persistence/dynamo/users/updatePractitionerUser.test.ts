5af394004cfd517e2aa8719de58447e306bd7a1b
Absolutestunna <joshujah007@gmail.com>
2023-02-08 14:32:59 -0500

    9781 - corrected updatePractitionerUser unit test

--- shared/src/persistence/dynamo/users/updatePractitionerUser.test.ts
+++ shared/src/persistence/dynamo/users/updatePractitionerUser.test.ts
@@ -1,6 +1,10 @@
 import { ROLES } from '../../../business/entities/EntityConstants';
 import { applicationContext } from '../../../business/test/createTestApplicationContext';
 import { updatePractitionerUser } from './updatePractitionerUser';
+import { updateUserRecords } from './updateUserRecords';
+
+jest.mock('./updateUserRecords');
+const updateUserRecordsMock = updateUserRecords as jest.Mock;
 
 describe('updatePractitionerUser', () => {
   const userId = '9b52c605-edba-41d7-b045-d5f992a499d3';
@@ -28,14 +32,6 @@ describe('updatePractitionerUser', () => {
     applicationContext.getCognito().adminUpdateUserAttributes.mockReturnValue({
       promise: () => null,
     });
-
-    applicationContext
-      .getPersistenceGateway()
-      .getUserById.mockReturnValue(updatedUser);
-
-    applicationContext
-      .getPersistenceGateway()
-      .updateUserRecords.mockReturnValue(updatedUser);
   });
 
   it("should log an error when an error occurs while updating the user's cognito attributes", async () => {
@@ -53,6 +49,7 @@ describe('updatePractitionerUser', () => {
   });
 
   it('should return updated practitioner data when the update was successful', async () => {
+    updateUserRecordsMock.mockImplementation(() => updatedUser);
     const results = await updatePractitionerUser({
       applicationContext,
       user: updatedUser as any,
@@ -69,6 +66,8 @@ describe('updatePractitionerUser', () => {
   });
 
   it("should update an existing practitioner user's Cognito attributes using the users email", async () => {
+    updateUserRecordsMock.mockImplementation(() => updatedUser);
+
     await updatePractitionerUser({
       applicationContext,
       user: updatedUser as any,
@@ -87,6 +86,7 @@ describe('updatePractitionerUser', () => {
   it("should update an existing practitioner user's Cognito attributes using the users pending email", async () => {
     updatedUser.email = undefined;
     updatedUser.pendingEmail = pendingEmail;
+    updateUserRecordsMock.mockImplementation(() => updatedUser);
 
     await updatePractitionerUser({
       applicationContext,
