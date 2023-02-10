e4a6ba07f8ee3d6085121372c5fe630228612632
Zachary Rogers <zrogers>
2023-02-10 11:09:27 -0800

    9595: Modify isPetitionerPartOfGroup to check if any user is a party.

--- shared/src/business/entities/cases/Case.isPetitionerPartOfGroup.test.js
+++ shared/src/business/entities/cases/Case.isPetitionerPartOfGroup.test.js
@@ -1,14 +1,15 @@
-const { getPetitionerById, isPetitionerPartOfGroup } = require('./Case');
+const { isPetitionerPartOfGroup } = require('./Case');
 
 describe('isPetitionerPartOfGroup', () => {
-  it('should return true when the userId exists in a consolidated cases parties array', () => {
+  it('should return true when the userId exists in the petitioners', () => {
     const isPartOfCase = isPetitionerPartOfGroup({
       consolidatedCases: [
         {
+          irsPractitioners: [],
           petitioners: [{ contactId: 'abc' }],
+          privatePractitioners: [],
         },
       ],
-      isPartyOfCase: getPetitionerById,
       userId: 'abc',
     });
 
@@ -19,16 +20,49 @@ describe('isPetitionerPartOfGroup', () => {
     const isPartOfCase = isPetitionerPartOfGroup({
       consolidatedCases: [
         {
+          irsPractitioners: [],
           petitioners: [{ contactId: '1' }],
+          privatePractitioners: [],
         },
         {
+          irsPractitioners: [],
           petitioners: [{ contactId: '2' }],
+          privatePractitioners: [],
         },
       ],
-      isPartyOfCase: getPetitionerById,
       userId: 'abc',
     });
 
     expect(isPartOfCase).toBe(false);
+  });
+
+  it('should return true when the userId exists in the privatePractitioners', () => {
+    const isPartOfCase = isPetitionerPartOfGroup({
+      consolidatedCases: [
+        {
+          irsPractitioners: [],
+          petitioners: [{ contactId: 'xyz' }],
+          privatePractitioners: [{ userId: 'abc' }],
+        },
+      ],
+      userId: 'abc',
+    });
+
+    expect(isPartOfCase).toBe(true);
+  });
+
+  it('should return true when the userId exists in the irsPractitioners', () => {
+    const isPartOfCase = isPetitionerPartOfGroup({
+      consolidatedCases: [
+        {
+          irsPractitioners: [],
+          petitioners: [{ contactId: 'xyz' }],
+          privatePractitioners: [{ userId: 'abc' }],
+        },
+      ],
+      userId: 'abc',
+    });
+
+    expect(isPartOfCase).toBe(true);
   });
 });
