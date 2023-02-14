f06fa47e4acf741d7ff6f01e2c281be306cfd056
Jason Fu <jasonfu6699@hotmail.com>
2023-02-13 22:01:05 -0800

    devex-1115 - rename is-migration-complete to migrationWaitHelper; add wait-for-migration lambda

---
+++ web-api/migrationWaitHelper.ts
@@ -0,0 +1,53 @@
+import {
+  CloudWatchClient,
+  GetMetricStatisticsCommand,
+} from '@aws-sdk/client-cloudwatch';
+import { DateTime } from 'luxon';
+import { GetQueueAttributesCommand, SQSClient } from '@aws-sdk/client-sqs';
+
+const cloudwatchClient = new CloudWatchClient({ region: 'us-east-1' });
+const sqsClient = new SQSClient({ region: 'us-east-1' });
+
+export const getSqsQueueCount = async queueUrl => {
+  const command = new GetQueueAttributesCommand({
+    AttributeNames: [
+      'ApproximateNumberOfMessages',
+      'ApproximateNumberOfMessagesNotVisible',
+      'ApproximateNumberOfMessagesDelayed',
+    ],
+    QueueUrl: queueUrl,
+  });
+  let data;
+  try {
+    data = await sqsClient.send(command);
+  } catch (error) {
+    console.log(error);
+  }
+  return data; //data.something.length?
+};
+
+export const getMetricStatistics = async type => {
+  const now = DateTime.now();
+  const start = DateTime.now().minus({ minutes: 15 });
+  const command = new GetMetricStatisticsCommand({
+    Dimensions: [
+      {
+        Name: 'FunctionName',
+        Value: `migration_segments_lambda_${process.env.ENV}`,
+      },
+    ],
+    EndTime: now.toJSDate(),
+    MetricName: type,
+    Namespace: 'AWS/Lambda',
+    Period: 60,
+    StartTime: start.toJSDate(),
+    Statistics: ['sum'],
+  });
+  let data;
+  try {
+    data = await cloudwatchClient.send(command);
+  } catch (error) {
+    console.log(error);
+  }
+  return data;
+};
