71a040c34346f89179c4f375685c7780571791d0
Zachary Rogers <zrogers>
2023-02-17 11:04:15 -0800

    9599: refactored test for clarity

--- shared/src/business/useCases/getCasesForUserInteractor.test.ts
+++ shared/src/business/useCases/getCasesForUserInteractor.test.ts
@@ -5,74 +5,74 @@ import { getCasesForUserInteractor } from './getCasesForUserInteractor';
 
 describe('getCasesForUserInteractor', () => {
   const userId = MOCK_CASE.petitioners[0].contactId;
-  describe('Consolidated cases', () => {
-    let leadCase;
-    let memberCase1;
-    let memberCase2;
-    let consolidatedGroupLeadCase11119: any[];
-    let unconsolidatedCase1;
-    let unconsolidatedCase2;
-    let unconsolidatedCase3;
-    let unconsolidatedClosedCase1;
-    let unconsolidatedClosedCase2;
+  let leadCase;
+  let memberCase1;
+  let memberCase2;
+  let consolidatedGroupLeadCase11119: any[];
+  let unconsolidatedCase1;
+  let unconsolidatedCase2;
+  let unconsolidatedCase3;
+  let unconsolidatedClosedCase1;
+  let unconsolidatedClosedCase2;
 
-    beforeEach(() => {
-      leadCase = {
-        ...MOCK_CASE,
-        createdAt: '2019-12-11T15:25:09.284Z',
-        docketNumber: '111-19',
-        leadDocketNumber: '111-19',
-      };
-      memberCase1 = {
-        ...MOCK_CASE,
-        createdAt: '2019-12-11T15:25:55.006Z',
-        docketNumber: '112-19',
-        leadDocketNumber: '111-19',
-      };
-      memberCase2 = {
-        ...MOCK_CASE,
-        createdAt: '2019-12-11T16:02:31.173Z',
-        docketNumber: '113-19',
-        leadDocketNumber: '111-19',
-      };
-      unconsolidatedCase1 = {
-        ...MOCK_CASE,
-        createdAt: '2020-01-21T16:41:39.474Z',
-        docketNumber: '102-20',
-      };
-      unconsolidatedCase2 = {
-        ...MOCK_CASE,
-        createdAt: '2019-08-16T17:29:10.132Z',
-        docketNumber: '107-19',
-      };
-      unconsolidatedCase3 = {
-        ...MOCK_CASE,
-        createdAt: '2019-03-01T22:53:50.097Z',
-        docketNumber: '103-19',
-      };
-      unconsolidatedClosedCase1 = {
-        ...MOCK_CASE,
-        closedDate: '2019-03-01T22:53:50.097Z',
-        docketNumber: '130-22',
-        status: CASE_STATUS_TYPES.closed,
-      };
-      unconsolidatedClosedCase2 = {
-        ...MOCK_CASE,
-        closedDate: '2018-03-01T22:53:50.097Z',
-        docketNumber: '140-22',
-        status: CASE_STATUS_TYPES.closed,
-      };
+  beforeEach(() => {
+    leadCase = {
+      ...MOCK_CASE,
+      createdAt: '2019-12-11T15:25:09.284Z',
+      docketNumber: '111-19',
+      leadDocketNumber: '111-19',
+    };
+    memberCase1 = {
+      ...MOCK_CASE,
+      createdAt: '2019-12-11T15:25:55.006Z',
+      docketNumber: '112-19',
+      leadDocketNumber: '111-19',
+    };
+    memberCase2 = {
+      ...MOCK_CASE,
+      createdAt: '2019-12-11T16:02:31.173Z',
+      docketNumber: '113-19',
+      leadDocketNumber: '111-19',
+    };
+    unconsolidatedCase1 = {
+      ...MOCK_CASE,
+      createdAt: '2020-01-21T16:41:39.474Z',
+      docketNumber: '102-20',
+    };
+    unconsolidatedCase2 = {
+      ...MOCK_CASE,
+      createdAt: '2019-08-16T17:29:10.132Z',
+      docketNumber: '107-19',
+    };
+    unconsolidatedCase3 = {
+      ...MOCK_CASE,
+      createdAt: '2019-03-01T22:53:50.097Z',
+      docketNumber: '103-19',
+    };
+    unconsolidatedClosedCase1 = {
+      ...MOCK_CASE,
+      closedDate: '2019-03-01T22:53:50.097Z',
+      docketNumber: '130-22',
+      status: CASE_STATUS_TYPES.closed,
+    };
+    unconsolidatedClosedCase2 = {
+      ...MOCK_CASE,
+      closedDate: '2018-03-01T22:53:50.097Z',
+      docketNumber: '140-22',
+      status: CASE_STATUS_TYPES.closed,
+    };
 
-      consolidatedGroupLeadCase11119 = [leadCase, memberCase1, memberCase2];
+    consolidatedGroupLeadCase11119 = [leadCase, memberCase1, memberCase2];
+
+    applicationContext.getCurrentUser.mockResolvedValue({
+      userId,
     });
-
+  });
+  describe('Consolidated cases', () => {
     it('should return the expected associated cases combined with the consolidated group cases for 111-19', async () => {
       memberCase1.petitioners = [];
       leadCase.petitioners = [];
 
-      applicationContext.getCurrentUser.mockResolvedValue({
-        userId,
-      });
       applicationContext
         .getPersistenceGateway()
         .getCasesForUser.mockResolvedValue([
@@ -132,24 +132,9 @@ describe('getCasesForUserInteractor', () => {
     });
 
     it('should return links to both open and closed consolidated cases when there is a single consolidated group and one case is closed, and one case is open', async () => {
-      applicationContext.getCurrentUser.mockResolvedValue({
-        userId,
-      });
       const consolidatedGroup = [
-        {
-          ...MOCK_CASE,
-          createdAt: '2020-01-21T16:41:39.474Z',
-          docketNumber: '102-20',
-          leadDocketNumber: '107-19',
-        },
-        {
-          ...MOCK_CASE,
-          closedDate: '2019-08-16T17:29:10.132Z',
-          createdAt: '2019-08-16T17:29:10.132Z',
-          docketNumber: '107-19',
-          leadDocketNumber: '107-19',
-          status: 'Closed',
-        },
+        { ...leadCase, status: CASE_STATUS_TYPES.closed },
+        memberCase1,
       ];
       applicationContext
         .getPersistenceGateway()
@@ -161,58 +146,31 @@ describe('getCasesForUserInteractor', () => {
 
       const userCases = await getCasesForUserInteractor(applicationContext);
 
-      expect(userCases.openCaseList).toEqual([
+      const expectedCaseList = [
         expect.objectContaining({
           consolidatedCases: expect.arrayContaining([
             expect.objectContaining({
-              docketNumber: '102-20',
+              docketNumber: memberCase1.docketNumber,
               isRequestingUserAssociated: true,
             }),
           ]),
-          docketNumber: '107-19',
+          docketNumber: leadCase.docketNumber,
           isRequestingUserAssociated: true,
-          leadDocketNumber: '107-19',
+          leadDocketNumber: leadCase.leadDocketNumber,
         }),
-      ]);
-      expect(userCases.closedCaseList).toEqual([
-        expect.objectContaining({
-          consolidatedCases: expect.arrayContaining([
-            expect.objectContaining({
-              docketNumber: '102-20',
-              isRequestingUserAssociated: true,
-            }),
-          ]),
-          docketNumber: '107-19',
-          isRequestingUserAssociated: true,
-          leadDocketNumber: '107-19',
-        }),
-      ]);
+      ];
+      expect(userCases.openCaseList).toEqual(expectedCaseList);
+      expect(userCases.closedCaseList).toEqual(expectedCaseList);
     });
 
     it('should return closed consolidated cases when there is a single consolidated group and one case is closed (not directly associated), and one case is open (directly associated)', async () => {
-      applicationContext.getCurrentUser.mockResolvedValue({
-        userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
-      });
       const consolidatedGroup = [
-        {
-          ...MOCK_CASE,
-          createdAt: '2020-01-21T16:41:39.474Z',
-          docketNumber: '102-20',
-          leadDocketNumber: '107-19',
-        },
-        {
-          ...MOCK_CASE,
-          closedDate: '2019-08-16T17:29:10.132Z',
-          createdAt: '2019-08-16T17:29:10.132Z',
-          docketNumber: '107-19',
-          leadDocketNumber: '107-19',
-          petitioners: [],
-          status: 'Closed',
-        },
+        { ...leadCase, petitioners: [], status: CASE_STATUS_TYPES.closed },
+        memberCase1,
       ];
       applicationContext
         .getPersistenceGateway()
-        .getCasesForUser.mockResolvedValue([consolidatedGroup[0]]);
+        .getCasesForUser.mockResolvedValue([memberCase1]);
 
       applicationContext
         .getPersistenceGateway()
@@ -220,58 +178,31 @@ describe('getCasesForUserInteractor', () => {
 
       const userCases = await getCasesForUserInteractor(applicationContext);
 
-      expect(userCases.openCaseList).toEqual([
+      const expectedCaseList = [
         expect.objectContaining({
           consolidatedCases: expect.arrayContaining([
             expect.objectContaining({
-              docketNumber: '102-20',
+              docketNumber: memberCase1.docketNumber,
               isRequestingUserAssociated: true,
             }),
           ]),
-          docketNumber: '107-19',
+          docketNumber: leadCase.docketNumber,
           isRequestingUserAssociated: false,
-          leadDocketNumber: '107-19',
+          leadDocketNumber: leadCase.leadDocketNumber,
         }),
-      ]);
-      expect(userCases.closedCaseList).toEqual([
-        expect.objectContaining({
-          consolidatedCases: expect.arrayContaining([
-            expect.objectContaining({
-              docketNumber: '102-20',
-              isRequestingUserAssociated: true,
-            }),
-          ]),
-          docketNumber: '107-19',
-          isRequestingUserAssociated: false,
-          leadDocketNumber: '107-19',
-        }),
-      ]);
+      ];
+      expect(userCases.openCaseList).toEqual(expectedCaseList);
+      expect(userCases.closedCaseList).toEqual(expectedCaseList);
     });
 
     it('should return open consolidated cases when there is a single consolidated group and one case is closed (directly associated), and one case is open (not directly associated)', async () => {
-      applicationContext.getCurrentUser.mockResolvedValue({
-        userId,
-      });
       const consolidatedGroup = [
-        {
-          ...MOCK_CASE,
-          closedDate: '2019-08-16T17:29:10.132Z',
-          createdAt: '2020-01-21T16:41:39.474Z',
-          docketNumber: '102-20',
-          leadDocketNumber: '107-19',
-          petitioners: [],
-          status: 'Closed',
-        },
-        {
-          ...MOCK_CASE,
-          createdAt: '2019-08-16T17:29:10.132Z',
-          docketNumber: '107-19',
-          leadDocketNumber: '107-19',
-        },
+        leadCase,
+        { ...memberCase1, petitioners: [], status: CASE_STATUS_TYPES.closed },
       ];
       applicationContext
         .getPersistenceGateway()
-        .getCasesForUser.mockResolvedValue([consolidatedGroup[1]]);
+        .getCasesForUser.mockResolvedValue([leadCase]);
 
       applicationContext
         .getPersistenceGateway()
@@ -279,85 +210,55 @@ describe('getCasesForUserInteractor', () => {
 
       const userCases = await getCasesForUserInteractor(applicationContext);
 
-      expect(userCases.openCaseList).toEqual([
+      const expectedCaseList = [
         expect.objectContaining({
           consolidatedCases: expect.arrayContaining([
             expect.objectContaining({
-              docketNumber: '102-20',
+              docketNumber: memberCase1.docketNumber,
               isRequestingUserAssociated: false,
             }),
           ]),
-          docketNumber: '107-19',
+          docketNumber: leadCase.docketNumber,
           isRequestingUserAssociated: true,
-          leadDocketNumber: '107-19',
+          leadDocketNumber: leadCase.leadDocketNumber,
         }),
-      ]);
-      expect(userCases.closedCaseList).toEqual([
-        expect.objectContaining({
-          consolidatedCases: expect.arrayContaining([
-            expect.objectContaining({
-              docketNumber: '102-20',
-              isRequestingUserAssociated: false,
-            }),
-          ]),
-          docketNumber: '107-19',
-          isRequestingUserAssociated: true,
-          leadDocketNumber: '107-19',
-        }),
-      ]);
+      ];
+      expect(userCases.openCaseList).toEqual(expectedCaseList);
+      expect(userCases.closedCaseList).toEqual(expectedCaseList);
     });
 
-    it('should sort closed cases by most recently closed while closed cases contain a consolidated group where the lead case is not closed', async () => {
-      applicationContext.getCurrentUser.mockResolvedValue({
-        userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
-      });
-
+    it('should sort closed cases by most recently closed when closed cases contain a consolidated group where the lead case is not closed (we use the closed date of the next lowest docket number to sort closed cases when the lead case is open)', async () => {
       const closedDate = '2020-08-16T17:29:10.132Z';
       const consolidatedGroup = [
+        leadCase,
         {
-          ...MOCK_CASE,
-          closedDate: 'should not matter',
-          docketNumber: '109-19',
-          leadDocketNumber: '107-19',
-          petitioners: [],
-          status: 'Closed',
-        },
-        {
-          ...MOCK_CASE,
+          ...memberCase1,
           closedDate,
-          docketNumber: '108-19',
-          leadDocketNumber: '107-19',
           petitioners: [],
           status: 'Closed',
         },
         {
-          ...MOCK_CASE,
-          docketNumber: '107-19',
-          leadDocketNumber: '107-19',
+          ...memberCase2,
+          closedDate: 'should not matter',
+          petitioners: [],
+          status: 'Closed',
         },
       ];
 
       const unconsolidatedCases = [
         {
-          ...MOCK_CASE,
+          ...unconsolidatedClosedCase1,
           closedDate: '2008-08-16T17:29:10.132Z',
-          docketNumber: '101-19',
-          status: 'Closed',
         },
         {
-          ...MOCK_CASE,
+          ...unconsolidatedClosedCase2,
           closedDate: '2045-08-16T17:29:10.132Z',
-          docketNumber: '200-19',
-          status: 'Closed',
         },
       ];
 
       applicationContext
         .getPersistenceGateway()
-        .getCasesForUser.mockResolvedValue([
-          consolidatedGroup[2],
-          ...unconsolidatedCases,
-        ]);
+        .getCasesForUser.mockResolvedValue([leadCase, ...unconsolidatedCases]);
 
       applicationContext
         .getPersistenceGateway()
@@ -368,45 +269,42 @@ describe('getCasesForUserInteractor', () => {
       const actualOrder = userCases.closedCaseList.map(
         aCase => aCase.docketNumber,
       );
-      const expectedOrder = ['200-19', '107-19', '101-19'];
+      const expectedOrder = [
+        unconsolidatedClosedCase2.docketNumber,
+        leadCase.docketNumber,
+        unconsolidatedClosedCase1.docketNumber,
+      ];
       expect(actualOrder).toEqual(expectedOrder);
     });
   });
 
   describe('Non Consolidated Cases', () => {
     it('should return the expected associated cases with NO consolidated groups or lead cases', async () => {
-      applicationContext.getCurrentUser.mockResolvedValue({
-        userId: '1',
-      });
       applicationContext
         .getPersistenceGateway()
         .getCasesForUser.mockResolvedValue([
-          {
-            ...MOCK_CASE,
-            createdAt: '2020-01-21T16:41:39.474Z',
-            docketNumber: '102-20',
-          },
-          {
-            ...MOCK_CASE,
-            createdAt: '2019-08-16T17:29:10.132Z',
-            docketNumber: '107-19',
-          },
+          unconsolidatedCase1,
+          unconsolidatedCase2,
+          unconsolidatedClosedCase1,
         ]);
 
       const userCases = await getCasesForUserInteractor(applicationContext);
-      expect(userCases).toMatchObject({
-        closedCaseList: [],
-        openCaseList: [
-          expect.objectContaining({
-            docketNumber: '102-20',
-            isRequestingUserAssociated: true,
-          }),
-          expect.objectContaining({
-            docketNumber: '107-19',
-            isRequestingUserAssociated: true,
-          }),
-        ],
-      });
+      expect(userCases.closedCaseList).toEqual([
+        expect.objectContaining({
+          docketNumber: unconsolidatedClosedCase1.docketNumber,
+          isRequestingUserAssociated: true,
+        }),
+      ]);
+      expect(userCases.openCaseList).toEqual([
+        expect.objectContaining({
+          docketNumber: unconsolidatedCase1.docketNumber,
+          isRequestingUserAssociated: true,
+        }),
+        expect.objectContaining({
+          docketNumber: unconsolidatedCase2.docketNumber,
+          isRequestingUserAssociated: true,
+        }),
+      ]);
     });
   });
 });
