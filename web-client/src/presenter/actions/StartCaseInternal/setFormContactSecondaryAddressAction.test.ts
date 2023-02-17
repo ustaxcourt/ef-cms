34c560ff89457726ba315480691dd1cdd3085aeb
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-02-17 10:26:41 -0800

    9818: Dont override paper petition email and e consent values when use same address is checked

--- web-client/src/presenter/actions/StartCaseInternal/setFormContactSecondaryAddressAction.test.ts
+++ web-client/src/presenter/actions/StartCaseInternal/setFormContactSecondaryAddressAction.test.ts
@@ -2,12 +2,14 @@ import { runAction } from 'cerebral/test';
 import { setFormContactSecondaryAddressAction } from './setFormContactSecondaryAddressAction';
 
 describe('setFormContactSecondaryAddressAction', () => {
-  it('sets form.contactSecondary to the contact prop except for the name', async () => {
+  it('should set form.contactSecondary to the contact props value, excluding name, paper petition email, and e-consent values', async () => {
     const result = await runAction(setFormContactSecondaryAddressAction, {
       props: {
         contact: {
           city: 'Flavortown',
+          hasConsentedToEService: true,
           name: 'Guy Fieri',
+          paperPetitionEmail: 'petitioner@example.com',
         },
       },
       state: {
@@ -15,13 +17,17 @@ describe('setFormContactSecondaryAddressAction', () => {
           contactPrimary: {
             city: 'Flavortown',
             contactId: '123abc',
+            hasConsentedToEService: true,
             name: 'Guy Fieri',
+            paperPetitionEmail: 'petitioner@example.com',
           },
           contactSecondary: {
             city: 'Greenbough',
             contactId: 'abc123',
+            hasConsentedToEService: false,
             inCareOf: 'Jennay',
             name: 'Forrest',
+            paperPetitionEmail: 'notacopy@example.com',
           },
         },
       },
@@ -30,13 +36,17 @@ describe('setFormContactSecondaryAddressAction', () => {
     expect(result.state.form.contactPrimary).toMatchObject({
       city: 'Flavortown',
       contactId: '123abc',
+      hasConsentedToEService: true,
       name: 'Guy Fieri',
+      paperPetitionEmail: 'petitioner@example.com',
     });
     expect(result.state.form.contactSecondary).toMatchObject({
       city: 'Flavortown',
       contactId: 'abc123',
+      hasConsentedToEService: false,
       inCareOf: 'Jennay',
       name: 'Forrest',
+      paperPetitionEmail: 'notacopy@example.com',
     });
   });
 });
