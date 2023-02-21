63f2a6525ffe31d07ea572fe10115628a0ecb8e7
Jason Fu <jfu@flexion.us>
2023-02-21 08:18:44 -0800

    devex-1115 - refactor `migration-status` to be flatter

--- web-api/workflow-terraform/migration-cron/main/lambdas/migration-status.js
+++ web-api/workflow-terraform/migration-cron/main/lambdas/migration-status.js
@@ -17,46 +17,46 @@ const jobName = 'wait-for-migration';
 
 exports.handler = async (input, context) => {
   const results = { migrateFlag: process.env.MIGRATE_FLAG };
-  if (results.migrateFlag === 'true') {
-    results.errorRate = await getSegmentErrorRate();
-    if (results.errorRate === -1) {
-      return context.succeed(results);
-    }
-    if (results.errorRate > 50) {
-      await cancelWorkflow({ apiToken, workflowId });
-      return context.succeed(results);
-    }
-
-    results.dlQueueCount = await getSqsQueueCount(dlQueueUrl);
-    if (results.dlQueueCount === -1) {
-      return context.succeed(results);
-    }
-    if (results.dlQueueCount > 0) {
-      await cancelWorkflow({ apiToken, workflowId });
-      return context.succeed(results);
-    }
-
-    // it's possible the queue has caught up but there are still more segments to process
-    // don't approve the job if this is the first consecutive time the queue is empty
-    results.totalActiveJobs = await getSqsQueueCount(workQueueUrl);
-    if (results.totalActiveJobs === -1) {
-      return context.succeed(results);
-    }
-    if (results.totalActiveJobs > 0) {
-      await putMigrationQueueIsEmptyFlag(false);
-    } else {
-      results.migrationQueueIsEmptyFlag = await getMigrationQueueIsEmptyFlag();
-      if (!results.migrationQueueIsEmptyFlag) {
-        await putMigrationQueueIsEmptyFlag(true);
-      } else {
-        await approvePendingJob({ apiToken, jobName, workflowId });
-      }
-    }
-  } else {
+  if (results.migrateFlag !== 'true') {
     await approvePendingJob({ apiToken, jobName, workflowId });
+    return context.succeed(results);
   }
 
-  return context.succeed(results);
+  results.errorRate = await getSegmentErrorRate();
+  if (results.errorRate === -1) {
+    return context.succeed(results);
+  }
+  if (results.errorRate > 50) {
+    await cancelWorkflow({ apiToken, workflowId });
+    return context.succeed(results);
+  }
+
+  results.dlQueueCount = await getSqsQueueCount(dlQueueUrl);
+  if (results.dlQueueCount === -1) {
+    return context.succeed(results);
+  }
+  if (results.dlQueueCount > 0) {
+    await cancelWorkflow({ apiToken, workflowId });
+    return context.succeed(results);
+  }
+
+  // it's possible the queue has caught up but there are still more segments to process
+  // don't approve the job if this is the first consecutive time the queue is empty
+  results.totalActiveJobs = await getSqsQueueCount(workQueueUrl);
+  if (results.totalActiveJobs === -1) {
+    return context.succeed(results);
+  }
+  if (results.totalActiveJobs > 0) {
+    await putMigrationQueueIsEmptyFlag(false);
+  } else {
+    results.migrationQueueIsEmptyFlag = await getMigrationQueueIsEmptyFlag();
+    if (!results.migrationQueueIsEmptyFlag) {
+      await putMigrationQueueIsEmptyFlag(true);
+    } else {
+      await approvePendingJob({ apiToken, jobName, workflowId });
+    }
+    return context.succeed(results);
+  }
 };
 
 const getSegmentErrorRate = async () => {
@@ -71,24 +71,14 @@ const getSegmentErrorRate = async () => {
   }
 
   let errorTotal = 0;
-  if ('Datapoints' in errorResponse && errorResponse.Datapoints.length > 0) {
-    for (const datapoint of errorResponse.Datapoints) {
-      if ('Sum' in datapoint) {
-        errorTotal += Number(datapoint.Sum);
-      }
-    }
-  }
-
   let invocationTotal = 0;
-  if (
-    'Datapoints' in invocationResponse &&
-    invocationResponse.Datapoints.length > 0
-  ) {
-    for (const datapoint of invocationResponse.Datapoints) {
-      if ('Sum' in datapoint) {
-        invocationTotal += Number(datapoint.Sum);
-      }
-    }
+  try {
+    errorTotal = getTotals(errorResponse);
+    invocationTotal = getTotals(invocationResponse);
+  } catch (error) {
+    console.log('Response from AWS did not match expected structure.');
+    console.error(error);
+    return -1;
   }
 
   if (invocationTotal > 0) {
@@ -101,4 +91,12 @@ const getSegmentErrorRate = async () => {
     return errorRate;
   }
   return 0;
+};
+
+const getTotals = response => {
+  let total = 0;
+  for (const datapoint of response.Datapoints) {
+    total += Number(datapoint.Sum);
+  }
+  return total;
 };
