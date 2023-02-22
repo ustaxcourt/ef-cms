4dea1541d7c53163b5f2249c46fed59cfe56f998
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2023-02-22 12:07:39 -0700

    9818: adding feature flag to hide or show e consent in review petition screen

--- web-client/src/presenter/sequences/gotoReviewSavedPetitionSequence.ts
+++ web-client/src/presenter/sequences/gotoReviewSavedPetitionSequence.ts
@@ -1,9 +1,12 @@
 import { getCaseAction } from '../actions/getCaseAction';
+import { getConstants } from '../../getConstants';
+import { getFeatureFlagFactoryAction } from '../actions/getFeatureFlagFactoryAction';
 import { setCaseAction } from '../actions/setCaseAction';
 import { setCaseOnFormAction } from '../actions/setCaseOnFormAction';
 import { setCaseOnFormUsingStateAction } from '../actions/setCaseOnFormUsingStateAction';
 import { setContactsOnFormAction } from '../actions/setContactsOnFormAction';
 import { setCurrentPageAction } from '../actions/setCurrentPageAction';
+import { setFeatureFlagFactoryAction } from '../actions/setFeatureFlagFactoryAction';
 import { shouldLoadCaseAction } from '../actions/shouldLoadCaseAction';
 import { showProgressSequenceDecorator } from '../utilities/showProgressSequenceDecorator';
 import { startWebSocketConnectionSequenceDecorator } from '../utilities/startWebSocketConnectionSequenceDecorator';
@@ -19,6 +22,14 @@ export const gotoReviewSavedPetitionSequence =
         setCaseOnFormAction,
       ]),
     },
+    getFeatureFlagFactoryAction(
+      getConstants().ALLOWLIST_FEATURE_FLAGS
+        .E_CONSENT_FIELDS_ENABLED_FEATURE_FLAG.key,
+    ),
+    setFeatureFlagFactoryAction(
+      getConstants().ALLOWLIST_FEATURE_FLAGS
+        .E_CONSENT_FIELDS_ENABLED_FEATURE_FLAG.key,
+    ),
     setContactsOnFormAction,
     setCurrentPageAction('ReviewSavedPetition'),
   ]);
