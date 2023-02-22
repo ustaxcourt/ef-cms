27e1a0ff8e45f6752593518789d95a93015adfb8
Michael Marcotte <mike.marcotte@gmail.com>
2023-02-22 16:44:08 -0500

    9845: fix b/g migration

--- web-api/migration-terraform/main/lambdas/migration.js
+++ web-api/migration-terraform/main/lambdas/migration.js
@@ -1,4 +1,6 @@
 const AWS = require('aws-sdk');
+const createApplicationContext = require('../../../src/applicationContext');
+const { createLogger } = require('../../../src/createLogger');
 const { migrateRecords: migrations } = require('./migration-segments');
 
 const dynamodb = new AWS.DynamoDB({
@@ -12,10 +14,13 @@ const docClient = new AWS.DynamoDB.DocumentClient({
   service: dynamodb,
 });
 
-const processItems = async ({ documentClient, items, migrateRecords }) => {
+const processItems = async (
+  applicationContext,
+  { documentClient, items, migrateRecords },
+) => {
   const promises = [];
 
-  items = await migrateRecords({ documentClient, items });
+  items = await migrateRecords(applicationContext, { documentClient, items });
 
   for (const item of items) {
     promises.push(
@@ -46,9 +51,22 @@ const getRemoveEvents = event => {
 
 exports.getFilteredGlobalEvents = getFilteredGlobalEvents;
 exports.processItems = processItems;
-exports.handler = async event => {
+exports.handler = async (event, context) => {
+  const applicationContext = createApplicationContext(
+    {},
+    createLogger({
+      defaultMeta: {
+        environment: {
+          stage: process.env.STAGE || 'local',
+        },
+        requestId: {
+          lambda: context.awsRequestId,
+        },
+      },
+    }),
+  );
   const items = getFilteredGlobalEvents(event);
-  await processItems({
+  await processItems(applicationContext, {
     documentClient: docClient,
     items,
     migrateRecords: migrations,
