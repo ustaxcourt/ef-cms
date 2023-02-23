0393ac57c452518ee01b10d50a7c5684fbc77545
Jim Lerza <james.lerza@ustaxcourt.gov>
2023-02-22 21:28:27 -0500

    devex-1115: environment-specific terraform can use the shared init scripts too

--- iam/terraform/environment-specific/bin/deploy-app.sh
+++ iam/terraform/environment-specific/bin/deploy-app.sh
@@ -1,55 +1,7 @@
 #!/bin/bash -e
 
-ENVIRONMENT=$1
-
-# Getting the environment-specific deployment settings and injecting them into the shell environment
-export ENV=${ENVIRONMENT}
-pushd ../../../../
 # shellcheck disable=SC1091
-. ./scripts/load-environment-from-secrets.sh
-popd
+source ../bin/deploy-init.sh
 
-[ -z "${ENVIRONMENT}" ] && echo "You must have ENVIRONMENT set in your environment" && exit 1
-[ -z "${EFCMS_DOMAIN}" ] && echo "You must have EFCMS_DOMAIN set in your environment" && exit 1
-[ -z "${ZONE_NAME}" ] && echo "You must have ZONE_NAME set in your environment" && exit 1
-
-../../../../scripts/verify-terraform-version.sh
-
-# Each $ENV will have its own terraform deploy bucket (i.e. "exp1.ustc-case-mgmt.flexion.us")
-BUCKET="${ZONE_NAME}.terraform.deploys"
-KEY="permissions-${ENVIRONMENT}.tfstate"
-LOCK_TABLE=efcms-terraform-lock
-REGION=us-east-1
-
-if [[ ${ENVIRONMENT} == "prod" ]]; then
-  if [[ ${EFCMS_DOMAIN} != *"dawson"* ]]; then
-    echo "ENVIRONMENT and EFCMS_DOMAIN do not match. Please check your environment variables and run again."
-    exit 1
-  fi
-elif [[ ${EFCMS_DOMAIN} != "${ENVIRONMENT}"* ]]; then
-  echo "ENVIRONMENT and EFCMS_DOMAIN do not match. Please check your environment variables and run again."
-  exit 1
-fi
-
-rm -rf .terraform
-echo "Initiating provisioning for environment [${ENVIRONMENT}] in AWS region [${REGION}]"
-sh ../bin/create-bucket.sh "${BUCKET}" "${KEY}" "${REGION}"
-
-echo "checking for the dynamodb lock table..."
-aws dynamodb list-tables --output json --region "${REGION}" --query "contains(TableNames, '${LOCK_TABLE}')" | grep 'true'
-result=$?
-if [ ${result} -ne 0 ]; then
-  echo "dynamodb lock does not exist, creating"
-  sh ../bin/create-dynamodb.sh "${LOCK_TABLE}" "${REGION}"
-else
-  echo "dynamodb lock table already exists"
-fi
-
-# exit on any failure
-set -eo pipefail
-
-export TF_VAR_environment=$ENVIRONMENT
-export TF_VAR_dns_domain=$EFCMS_DOMAIN
-
-terraform init -backend=true -backend-config=bucket="${BUCKET}" -backend-config=key="${KEY}" -backend-config=dynamodb_table="${LOCK_TABLE}" -backend-config=region="${REGION}"
+terraform plan
 terraform apply -auto-approve
