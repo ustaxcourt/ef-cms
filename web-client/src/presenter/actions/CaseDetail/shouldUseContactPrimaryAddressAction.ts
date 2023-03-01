bbfc90dc757b9b429912d89648b1a6a149fe1ee1
Rosie Highsmith <ahighsmith@flexion.us>
2023-03-01 08:12:31 -0700

    9797-bugfix: clear contact secondary fields on unchecking use same box on creating a case as petitionsClerk

---
+++ web-client/src/presenter/actions/CaseDetail/shouldUseContactPrimaryAddressAction.ts
@@ -0,0 +1,21 @@
+import { state } from 'cerebral';
+
+/**
+ * returns yes path if useSameAsPrimary is true, no otherwise
+ *
+ * @param {object} providers the providers object
+ * @param {object} providers.get the cerebral get function
+ * @param {object} providers.path the cerebral path which is contains the next paths that can be invoked
+ * @returns {object} continue path for the sequence
+ */
+export const shouldUseContactPrimaryAddressAction = ({ get, path }) => {
+  const useSameAsPrimary = get(state.form.useSameAsPrimary);
+  console.log('get(state.form', get(state.form));
+
+  console.log('useSameAsPrimary', useSameAsPrimary);
+  if (useSameAsPrimary) {
+    return path.yes();
+  } else {
+    return path.no();
+  }
+};
