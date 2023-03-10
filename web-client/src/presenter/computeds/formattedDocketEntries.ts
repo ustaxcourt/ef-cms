d47798c4aeb646a1216a2166376e730eaf9a0dff
Rosie Highsmith <ahighsmith@flexion.us>
2023-03-10 09:16:22 -0700

    9861: create MOTION_EVENT_CODES to be used in the formattedDocketEntries filter

--- web-client/src/presenter/computeds/formattedDocketEntries.ts
+++ web-client/src/presenter/computeds/formattedDocketEntries.ts
@@ -261,6 +261,7 @@ export const formattedDocketEntries = (get, applicationContext) => {
   const {
     DOCKET_RECORD_FILTER_OPTIONS,
     EXHIBIT_EVENT_CODES,
+    MOTION_EVENT_CODES,
     ORDER_EVENT_CODES,
   } = applicationContext.getConstants();
 
@@ -288,6 +289,11 @@ export const formattedDocketEntries = (get, applicationContext) => {
     case DOCKET_RECORD_FILTER_OPTIONS.orders:
       result.formattedDocketEntries = result.formattedDocketEntries.filter(
         entry => ORDER_EVENT_CODES.includes(entry.eventCode) && !entry.isDraft,
+      );
+      break;
+    case DOCKET_RECORD_FILTER_OPTIONS.motions:
+      result.formattedDocketEntries = result.formattedDocketEntries.filter(
+        entry => MOTION_EVENT_CODES.includes(entry.eventCode) && !entry.isDraft,
       );
       break;
   }
