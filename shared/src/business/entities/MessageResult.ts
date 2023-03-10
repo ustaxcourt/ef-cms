94243d449124037abe04941cfc344bc39c53a46d
Cody Seibert <cseibert113@gmail.com>
2023-03-10 14:02:54 -0500

    devex - fixing issue with previous refactor

--- shared/src/business/entities/MessageResult.ts
+++ shared/src/business/entities/MessageResult.ts
@@ -1,14 +1,11 @@
+import { Message } from './Message';
+
 const joi = require('joi');
-const {
-  joiValidationDecorator,
-  validEntityDecorator,
-} = require('./JoiValidationDecorator');
 const {
   TRIAL_CITY_STRINGS,
   TRIAL_LOCATION_MATCHER,
 } = require('./EntityConstants');
 const { JoiValidationConstants } = require('./JoiValidationConstants');
-const { messageDecorator } = require('./Message');
 
 /**
  * constructor
@@ -16,41 +13,40 @@ const { messageDecorator } = require('./Message');
  * @param {object} rawMessage the raw message data
  * @constructor
  */
-function MessageResult() {
-  this.entityName = 'MessageResult';
+export class MessageResult extends Message {
+  public trialDate: string;
+  public trialLocation: string;
+
+  constructor(rawMessage, { applicationContext }) {
+    super(rawMessage, { applicationContext });
+    this.entityName = 'MessageResult';
+    this.trialDate = rawMessage.trialDate;
+    this.trialLocation = rawMessage.trialLocation;
+  }
+
+  static MESSAGE_RESULTS_VALIDATION_RULES = {
+    entityName: JoiValidationConstants.STRING.valid('MessageResult').required(),
+    trialDate: joi
+      .alternatives()
+      .optional()
+      .description('When this case goes to trial.'),
+    trialLocation: joi
+      .alternatives()
+      .try(
+        JoiValidationConstants.STRING.valid(...TRIAL_CITY_STRINGS, null),
+        JoiValidationConstants.STRING.pattern(TRIAL_LOCATION_MATCHER),
+        JoiValidationConstants.STRING.valid('Standalone Remote'),
+      )
+      .optional()
+      .description(
+        'Where this case goes to trial. This may be different that the preferred trial location.',
+      ),
+  };
+
+  getValidationRules() {
+    return {
+      ...super.getValidationRules(),
+      ...MessageResult.MESSAGE_RESULTS_VALIDATION_RULES,
+    } as any;
+  }
 }
-
-MessageResult.prototype.init = function init(
-  rawMessage,
-  { applicationContext },
-) {
-  messageDecorator(this, rawMessage, { applicationContext });
-  this.trialDate = rawMessage.trialDate;
-  this.trialLocation = rawMessage.trialLocation;
-};
-
-MessageResult.VALIDATION_RULES = {
-  trialDate: joi
-    .alternatives()
-    .optional()
-    .description('When this case goes to trial.'),
-  trialLocation: joi
-    .alternatives()
-    .try(
-      JoiValidationConstants.STRING.valid(...TRIAL_CITY_STRINGS, null),
-      JoiValidationConstants.STRING.pattern(TRIAL_LOCATION_MATCHER),
-      JoiValidationConstants.STRING.valid('Standalone Remote'),
-    )
-    .optional()
-    .description(
-      'Where this case goes to trial. This may be different that the preferred trial location.',
-    ),
-};
-
-joiValidationDecorator(
-  MessageResult,
-  joi.object().keys(MessageResult.VALIDATION_RULES),
-  MessageResult.VALIDATION_ERROR_MESSAGES,
-);
-
-exports.MessageResult = validEntityDecorator(MessageResult);
