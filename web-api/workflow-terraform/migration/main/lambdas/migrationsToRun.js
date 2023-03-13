aafec842f54b45fee8856482b572bd2767fe8dd3
Zachary Rogers <zrogers>
2023-03-13 15:41:09 -0700

    9614: add local migration for workitem-assignee

--- web-api/workflow-terraform/migration/main/lambdas/migrationsToRun.js
+++ web-api/workflow-terraform/migration/main/lambdas/migrationsToRun.js
@@ -13,6 +13,9 @@ const {
 const {
   migrateItems: migration0007,
 } = require('./migrations/0007-update-corporate-disclosure-document');
+const {
+  migrateItems: migration0008,
+} = require('./migrations/0008-add-assignee-id-gsi2pk-to-work-item');
 
 // MODIFY THIS ARRAY TO ADD NEW MIGRATIONS OR REMOVE OLD ONES
 const migrationsToRun = [
@@ -35,6 +38,10 @@ const migrationsToRun = [
   {
     key: '0007-update-corporate-disclosure-document.ts',
     script: migration0007,
+  },
+  {
+    key: '0008-add-assignee-id-gsi2pk-to-work-item.ts',
+    script: migration0008,
   },
 ];
 
