4a9c2ffbe67e21e46b39571f24b14891e43e740a
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-03-14 15:20:42 -0700

    9803: Clean up tests, only fetch case after pdf is found

--- shared/src/business/useCaseHelper/coverSheets/removeCoversheet.test.js
+++ shared/src/business/useCaseHelper/coverSheets/removeCoversheet.test.js
@@ -2,59 +2,51 @@ const {
   applicationContext,
   testPdfDoc,
 } = require('../../test/createTestApplicationContext');
-const { PARTY_TYPES } = require('../../entities/EntityConstants');
+const { MOCK_CASE } = require('../../../test/mockCase');
 const { removeCoversheet } = require('./removeCoversheet');
-
 describe('removeCoversheet', () => {
-  const testingCaseData = {
-    contactPrimary: {
-      name: 'Daenerys Stormborn',
-    },
-    createdAt: '2019-04-19T14:45:15.595Z',
-    docketEntries: [
-      {
-        certificateOfService: false,
-        createdAt: '2019-04-19T14:45:15.595Z',
-        docketEntryId: 'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
-        docketNumber: '101-19',
-        documentType: 'Answer',
-        eventCode: 'A',
-        filedBy: 'Test Petitioner',
-        filingDate: '2019-04-19T14:45:15.595Z',
-        isPaper: false,
-        numberOfPages: 2,
-        processingStatus: 'pending',
-        userId: '02323349-87fe-4d29-91fe-8dd6916d2fda',
+  it('should throw an exception when the requested document cannot be found in S3', async () => {
+    applicationContext.getStorageClient().getObject.mockReturnValue({
+      promise: () => {
+        throw new Error('oh no');
       },
-    ],
-    docketNumber: '101-19',
-    partyType: PARTY_TYPES.petitioner,
-  };
-  beforeAll(() => {
-    jest.setTimeout(15000);
+    });
 
+    await expect(removeCoversheet(applicationContext, {})).rejects.toThrow();
+  });
+
+  it('should remove the coversheet from the provided docket entry`s pdf', async () => {
+    const mockDocketEntryId = 'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859';
+    const numberOfPagesBeforeCoversheetRemoval = 2;
+
+    applicationContext
+      .getPersistenceGateway()
+      .getCaseByDocketNumber.mockReturnValue({
+        ...MOCK_CASE,
+        docketEntries: [
+          {
+            docketEntryId: mockDocketEntryId,
+            docketNumber: MOCK_CASE.docketNumber,
+            documentType: 'Answer',
+            eventCode: 'A',
+            filedBy: 'Test Petitioner',
+            numberOfPages: numberOfPagesBeforeCoversheetRemoval,
+            processingStatus: 'pending',
+          },
+        ],
+      });
     applicationContext.getStorageClient().getObject.mockReturnValue({
       promise: () => ({
         Body: testPdfDoc,
       }),
     });
-  });
 
-  it('removes a cover sheet from a pdf document', async () => {
-    applicationContext
-      .getPersistenceGateway()
-      .getCaseByDocketNumber.mockReturnValue(testingCaseData);
-    const params = {
-      docketEntryId: 'a6b81f4d-1e47-423a-8caf-6d2fdc3d3859',
-      docketNumber: '101-19',
-    };
+    const result = await removeCoversheet(applicationContext, {
+      docketEntryId: mockDocketEntryId,
+      docketNumber: MOCK_CASE.docketNumber,
+    });
 
-    const updatedDocketEntry = await removeCoversheet(
-      applicationContext,
-      params,
-    );
-    expect(updatedDocketEntry.numberOfPages).toBe(1);
-
+    expect(result.numberOfPages).toBe(numberOfPagesBeforeCoversheetRemoval - 1);
     expect(
       applicationContext.getDocumentGenerators().coverSheet,
     ).not.toHaveBeenCalled();
@@ -64,22 +56,5 @@ describe('removeCoversheet', () => {
     expect(
       applicationContext.getPersistenceGateway().updateDocketEntry,
     ).toHaveBeenCalled();
-  });
-
-  it('throws an exception if the requested document cannot be found in S3', async () => {
-    applicationContext.getStorageClient().getObject.mockReturnValue({
-      promise: () => {
-        throw new Error('oh no');
-      },
-    });
-
-    const params = {
-      docketEntryId: '55551f4d-1e47-423a-8caf-6d2fdc3d3859',
-      docketNumber: '101-19',
-    };
-
-    await expect(removeCoversheet(applicationContext, params)).rejects.toThrow(
-      'oh no docket entry id is 55551f4d-1e47-423a-8caf-6d2fdc3d3859',
-    );
   });
 });
