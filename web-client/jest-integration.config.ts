9366c9eb9ee5eba38b7f764556c00b8a6754a8e3
Zachary Rogers <zrogers>
2023-03-15 10:11:54 -0700

    devex: Add integration test jest file, remove unused packags json scripts.

---
+++ web-client/jest-integration.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: false,
+  globals: {
+    File() {},
+    FileReader: () => {},
+    atob: x => x,
+  },
+  maxWorkers: 1, // because running integration tests are heavy and can interfere with one another, we are locking this to 1 worker
+  testEnvironment: 'node',
+  // testMatch: [
+  //   '**/web-client/integration-tests/**/?(*.)+(spec|test).[jt]s?(x)',
+  //   '**/web-client/integration-tests-public/**/?(*.)+(spec|test).[jt]s?(x)',
+  // ], // Uncomment testMatch to run all integration tests in integration-tests + integration-tests-public
+  testTimeout: 30000,
+  transform: {
+    '\\.[jt]sx?$': ['babel-jest', { rootMode: 'upward' }],
+  },
+  verbose: false,
+};
+
+// eslint-disable-next-line import/no-default-export
+export default config;
