718348a8c6e5312ce1147c587dea3744557f39e4
Cody Seibert <cseibert113@gmail.com>
2023-03-16 14:03:33 -0400

    devex - penalty

--- shared/src/business/entities/Correspondence.ts
+++ shared/src/business/entities/Correspondence.ts
@@ -1,41 +1,46 @@
-const joi = require('joi');
-const {
-  joiValidationDecorator,
-  validEntityDecorator,
-} = require('./JoiValidationDecorator');
-const { createISODateString } = require('../utilities/DateHandler');
-const { JoiValidationConstants } = require('./JoiValidationConstants');
+import { JoiValidationConstants } from './JoiValidationConstants';
+import { JoiValidationEntity } from './JoiValidationEntity';
+import { createISODateString } from '../utilities/DateHandler';
+import joi from 'joi';
 
-/**
- * @param {object} rawProps the raw document data
- * @constructor
- */
-function Correspondence() {}
-Correspondence.prototype.init = function init(rawProps) {
-  this.entityName = 'Correspondence';
+export class Correspondence extends JoiValidationEntity {
+  public archived: boolean;
+  public correspondenceId: string;
+  public documentTitle: string;
+  public filedBy: string;
+  public userId: string;
+  public filingDate: string;
 
-  this.archived = rawProps.archived;
-  this.correspondenceId = rawProps.correspondenceId;
-  this.documentTitle = rawProps.documentTitle;
-  this.filedBy = rawProps.filedBy;
-  this.userId = rawProps.userId;
-  this.filingDate = rawProps.filingDate || createISODateString();
-};
+  constructor(rawProps) {
+    super('Correspondence');
 
-Correspondence.VALIDATION_RULES = {
-  archived: joi
-    .boolean()
-    .optional()
-    .description('A correspondence document that was archived.'),
-  correspondenceId: JoiValidationConstants.UUID.required(),
-  documentTitle: JoiValidationConstants.STRING.max(500).required(),
-  filedBy: JoiValidationConstants.STRING.max(500).allow('').optional(),
-  filingDate: JoiValidationConstants.ISO_DATE.max('now')
-    .required()
-    .description('Date that this Document was filed.'),
-  userId: JoiValidationConstants.UUID.required(),
-};
+    this.archived = rawProps.archived;
+    this.correspondenceId = rawProps.correspondenceId;
+    this.documentTitle = rawProps.documentTitle;
+    this.filedBy = rawProps.filedBy;
+    this.userId = rawProps.userId;
+    this.filingDate = rawProps.filingDate || createISODateString();
+  }
 
-joiValidationDecorator(Correspondence, Correspondence.VALIDATION_RULES, {});
+  static VALIDATION_RULES = {
+    archived: joi
+      .boolean()
+      .optional()
+      .description('A correspondence document that was archived.'),
+    correspondenceId: JoiValidationConstants.UUID.required(),
+    documentTitle: JoiValidationConstants.STRING.max(500).required(),
+    filedBy: JoiValidationConstants.STRING.max(500).allow('').optional(),
+    filingDate: JoiValidationConstants.ISO_DATE.max('now')
+      .required()
+      .description('Date that this Document was filed.'),
+    userId: JoiValidationConstants.UUID.required(),
+  };
 
-module.exports = { Correspondence: validEntityDecorator(Correspondence) };
+  getValidationRules() {
+    return Correspondence.VALIDATION_RULES;
+  }
+
+  getErrorToMessageMap() {
+    return {};
+  }
+}
