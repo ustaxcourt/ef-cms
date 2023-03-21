bf98d6136b734ae3d443499cd011a231802bb1e0
Jason Fu <jfu@flexion.us>
2023-03-21 14:51:27 -0700

    9723: Added date picker to custom case report

--- web-client/src/views/CustomCaseReport/CustomCaseReport.tsx
+++ web-client/src/views/CustomCaseReport/CustomCaseReport.tsx
@@ -1,8 +1,26 @@
+import { BigHeader } from '../BigHeader';
+import { DateRangePickerComponent } from '../../ustc-ui/DateInput/DateRangePickerComponent';
+import { ErrorNotification } from '../ErrorNotification';
+import { SuccessNotification } from '../SuccessNotification';
 import { connect } from '@cerebral/react';
 import React from 'react';
 
 export const CustomCaseReport = connect({}, function CustomCaseReport() {
-  return <h1>HELLO</h1>;
+  return (
+    <>
+      <BigHeader text="Reports" />
+      <section className="usa-section grid-container">
+        <SuccessNotification />
+        <ErrorNotification />
+        <div className="title">
+          <h1>Custom Case Report</h1>
+        </div>
+        <div className="grid-col-4">
+          <DateRangePickerComponent />
+        </div>
+      </section>
+    </>
+  );
 });
 
 CustomCaseReport.displayName = 'CustomCaseReport';
