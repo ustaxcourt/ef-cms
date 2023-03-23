99753eeb318ae664abad56f9288d5ec78175a927
Timothy Shumway <tshumway@flexion.us>
2023-03-23 14:57:22 -0600

    9796: continue work on migration test WIP

--- web-api/workflow-terraform/migration/main/lambdas/migrations/0008-add-trial-location-field-to-work-item.test.ts
+++ web-api/workflow-terraform/migration/main/lambdas/migrations/0008-add-trial-location-field-to-work-item.test.ts
@@ -4,9 +4,10 @@ import { migrateItems } from './0008-add-trial-location-field-to-work-item';
 // import { MOCK_CASE } from '../../../../../../shared/src/test/mockCase';
 
 let MOCK_CASE_RECORD;
+let documentClientMock;
 
 describe('migrateItems', () => {
-  beforeAll(() => {
+  beforeEach(() => {
     MOCK_CASE_RECORD = {
       docketNumber: '101-18',
       docketNumberWithSuffix: '101-18',
@@ -18,7 +19,17 @@ describe('migrateItems', () => {
       trialLocation: 'Washington, District of Columbia',
       trialSessionId: '7805d1ab-18d0-43ec-bafb-654e83405410',
     };
+
+    documentClientMock = {
+      query: jest.fn().mockReturnValue({
+        promise: () => ({
+          Items: [MOCK_CASE_RECORD],
+          LastEvaluatedKey: '1',
+        }),
+      }),
+    };
   });
+
   it('should return and not modify records that are not Work Items', async () => {
     const mockItems = [
       {
@@ -30,7 +41,7 @@ describe('migrateItems', () => {
         sk: 'case|83b77e98-4cf6-4fb4-b8c0-f5f90fd68f3c',
       },
     ];
-    const results = await migrateItems(mockItems);
+    const results = await migrateItems(mockItems, documentClientMock);
 
     expect(results).toEqual(mockItems);
   });
@@ -42,7 +53,7 @@ describe('migrateItems', () => {
         sk: 'user|6d74eadc-0181-4ff5-826c-305200e8733d',
       },
       {
-        caseStatus: CASE_STATUS_TYPES.calendared,
+        caseStatus: CASE_STATUS_TYPES.generalDocket,
         pk: `case|${MOCK_CASE_RECORD.docketNumber}`,
         sk: 'work-item|6d74eadc-0181-4ff5-826c-123432e8733d',
       },
@@ -56,7 +67,7 @@ describe('migrateItems', () => {
         sk: 'work-item|6d74eadc-0181-4ff5-826c-123432e8733d',
       },
     ];
-    const results = await migrateItems(mockItems);
+    const results = await migrateItems(mockItems, documentClientMock);
 
     expect(results).toEqual(mockItems);
   });
@@ -70,6 +81,17 @@ describe('migrateItems', () => {
       };
     });
 
+    jest.mock(
+      '../../../../../../shared/src/persistence/dynamo/helpers/aggregateCaseItems',
+      () => {
+        return {
+          pk: `case|${MOCK_CASE_RECORD.docketNumber}`,
+          sk: `case|${MOCK_CASE_RECORD.docketNumber}`,
+          trialLocation: `${MOCK_CASE_RECORD.trialLocation}`,
+        };
+      },
+    );
+
     const mockItems = [
       {
         pk: `case|${MOCK_CASE_RECORD.docketNumber}`,
@@ -91,7 +113,7 @@ describe('migrateItems', () => {
       },
     ];
 
-    const results = await migrateItems(mockItems);
+    const results = await migrateItems(mockItems, documentClientMock);
 
     expect(results[1]).toEqual({
       caseStatus: CASE_STATUS_TYPES.calendared,
