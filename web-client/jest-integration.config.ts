c12e32508ef41dce82477c8f9fe696c72e13b389
Zachary Rogers <zrogers>
2023-03-23 13:02:59 -0700

    devex: Set memory limit for integrationt tests so jest runner does not continue to take up memory heap.

--- web-client/jest-integration.config.ts
+++ web-client/jest-integration.config.ts
@@ -19,6 +19,7 @@ const config: Config = {
     '\\.[jt]sx?$': ['babel-jest', { rootMode: 'upward' }],
   },
   verbose: false,
+  workerIdleMemoryLimit: '10%', // After a jest runner uses X% of total system memory, recreate the runner.
 };
 
 // eslint-disable-next-line import/no-default-export
