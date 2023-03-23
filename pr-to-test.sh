0b7561e9d79c7d00121c782f02bd25b477144852
Absolutestunna <joshujah007@gmail.com>
2023-03-23 17:26:30 -0400

    opex-1151: adding script to create int branch

---
+++ pr-to-test.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+COURT_REPO="https://github.com/ustaxcourt/ef-cms.git"
+# FLEXION_REPO="https://github.com/flexion/ef-cms.git"
+
+
+
+# SOURCE_ORG= # some git command to figure out org [flexion|ustaxcourt]
+# SOURCE_BRANCH=$(git branch --show-current)
+
+
+# git checkout test
+# # create new branch "${SOURCE_ORG}:${SOURCE_BRANCH}-to-test-$(date +%s)"
+# # merge $SOURCE_BRANCH into new branch
+# open "https:github.com/ustaxcourt/ef-cms/compare/test...${INTERMEDIATE_BRANCH}"
+
+
+
+
+
+# git remote add $COURT_REPO COURT_REPO
+SOURCE_BRANCH=$(git branch --show-current)
+INTERMEDIATE_BRANCH="${SOURCE_BRANCH}-intermediate-branch"
+git checkout -b "$INTERMEDIATE_BRANCH"
+git pull -n $COURT_REPO staging
+git pull -n $COURT_REPO test
+
+
+# handle merge conflicts (if any)
+if [[ -n $(git status --porcelain | grep '^UU') ]]; then
+    # EXIT
+fi
+
+git push origin $INTERMEDIATE_BRANCH
+
+open "https:github.com/ustaxcourt/ef-cms/compare/test...flexion:${INTERMEDIATE_BRANCH}"
+
+git switch -
+
+
+
