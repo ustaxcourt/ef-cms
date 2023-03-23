2fa0a1d52293efd8e0d03f287f0d70e097eefd75
Timothy Shumway <tshumway@flexion.us>
2023-03-23 11:20:40 -0600

    9796: continue work on migration test

--- web-api/workflow-terraform/migration/main/lambdas/migrations/0008-add-trial-location-field-to-work-item.ts
+++ web-api/workflow-terraform/migration/main/lambdas/migrations/0008-add-trial-location-field-to-work-item.ts
@@ -1,11 +1,10 @@
-// import { aggregateCaseItems } from '../../../../../../shared/src/persistence/dynamo/helpers/aggregateCaseItems';
+import { aggregateCaseItems } from '../../../../../../shared/src/persistence/dynamo/helpers/aggregateCaseItems';
 const createApplicationContext = require('../../../../../src/applicationContext');
 const {
   WorkItem,
 } = require('../../../../../../shared/src/business/entities/WorkItem');
 import { CASE_STATUS_TYPES } from '../../../../../../shared/src/business/entities/EntityConstants';
-
-// const { queryFullCase } = require('../utilities');
+import { queryFullCase } from '../utilities/queryFullCase';
 
 const applicationContext = createApplicationContext({});
 
@@ -13,25 +12,25 @@ const isWorkItem = item => {
   return item.pk.startsWith('case|') && item.sk.startsWith('work-item|');
 };
 
-// export const migrateItems = async (items, documentClient) => {
-export const migrateItems = async items => {
+export const migrateItems = async (items, documentClient) => {
+  // export const migrateItems = async items => {
   const itemsAfter = [];
 
   for (const item of items) {
     if (isWorkItem(item) && item.calendared === CASE_STATUS_TYPES.calendared) {
-      //   const fullCase = await queryFullCase(documentClient, item.docketNumber);
-      //   const caseRecord = aggregateCaseItems(fullCase);
-      const caseDetail = await applicationContext
-        .getUseCases()
-        .getCaseInteractor(applicationContext, {
-          docketNumber: item.docketNumber,
-        });
+      const fullCase = await queryFullCase(documentClient, item.docketNumber);
+      const caseRecord = aggregateCaseItems(fullCase);
+      //   const caseDetail = await applicationContext
+      //     .getUseCases()
+      //     .getCaseInteractor(applicationContext, {
+      //       docketNumber: item.docketNumber,
+      //     });
 
       const theWorkItem = new WorkItem(
         {
           ...item,
-          //   trialLocation: caseRecord.trialLocation,
-          trialLocation: caseDetail.trialLocation,
+          trialLocation: caseRecord.trialLocation,
+          //   trialLocation: caseDetail.trialLocation,
         },
         {
           applicationContext,
