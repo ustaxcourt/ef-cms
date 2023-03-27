f19a44399552fa62a28a81806daf25d928cc04bc
Absolutestunna <joshujah007@gmail.com>
2023-03-27 12:36:28 -0400

    opex-1151 - modifying script to create intermediate

--- pr-to-test.sh
+++ pr-to-test.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 COURT_REPO="https://github.com/ustaxcourt/ef-cms.git"
+FLEXION_REPO="https://github.com/flexion/ef-cms.git"
 SOURCE_BRANCH=$(git branch --show-current)
 INTERMEDIATE_BRANCH="${SOURCE_BRANCH}-intermediate-branch"
 
-git checkout -b "$INTERMEDIATE_BRANCH"
-git pull -n $COURT_REPO test
+git switch -c "${INTERMEDIATE_BRANCH}-$(date +%s)"
+git pull --no-edit $COURT_REPO test
+
+git status
 
 # handle merge conflicts (if any)
 if [[ -n $(git status --porcelain | grep '^UU') ]]; then
-    git reset --hard HEAD
-    exit
+    echo "Merge conflict detected. Please resolve the conflict and try again."
+    exit 1
 fi
 
-git push origin $INTERMEDIATE_BRANCH
+git push -u $FLEXION_REPO $INTERMEDIATE_BRANCH
 
+# TODO: open the browser only if the push operation was successful
 open "https:github.com/ustaxcourt/ef-cms/compare/test...flexion:${INTERMEDIATE_BRANCH}"
-
-git switch -
 
 
 
