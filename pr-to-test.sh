8cbf231dc25894ce5ecae7c07ffe8076689e30e8
Absolutestunna <joshujah007@gmail.com>
2023-03-27 13:20:03 -0400

    opex-1151: corrected intermediate branch name

--- pr-to-test.sh
+++ pr-to-test.sh
@@ -3,14 +3,11 @@
 COURT_REPO="https://github.com/ustaxcourt/ef-cms.git"
 FLEXION_REPO="https://github.com/flexion/ef-cms.git"
 SOURCE_BRANCH=$(git branch --show-current)
-INTERMEDIATE_BRANCH="${SOURCE_BRANCH}-intermediate-branch"
+INTERMEDIATE_BRANCH="${SOURCE_BRANCH}-intermediate-branch-$(date +%s)"
 
-git switch -c "${INTERMEDIATE_BRANCH}-$(date +%s)"
+git switch -c "$INTERMEDIATE_BRANCH"
 git pull --no-edit $COURT_REPO test
 
-git status
-
-# handle merge conflicts (if any)
 if [[ -n $(git status --porcelain | grep '^UU') ]]; then
     echo "Merge conflict detected. Please resolve the conflict and try again."
     exit 1
@@ -18,7 +15,6 @@ fi
 
 git push -u $FLEXION_REPO $INTERMEDIATE_BRANCH
 
-# TODO: open the browser only if the push operation was successful
 open "https:github.com/ustaxcourt/ef-cms/compare/test...flexion:${INTERMEDIATE_BRANCH}"
 
 
