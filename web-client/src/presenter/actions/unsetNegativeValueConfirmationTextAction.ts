f78006a7b9b13cff2215fa36ea649b29bbf7b947
Timothy Shumway <tshumway@flexion.us>
2023-03-28 09:43:59 -0600

    9828: setting and unsetting state based off - sign working

--- web-client/src/presenter/actions/unsetNegativeValueConfirmationTextAction.ts
+++ web-client/src/presenter/actions/unsetNegativeValueConfirmationTextAction.ts
@@ -1,5 +1,6 @@
 import { state } from 'cerebral';
 
 export const unsetNegativeValueConfirmationTextAction = ({ props, store }) => {
+  console.log('props in unSetter****', props);
   store.unset(state.confirmationText[props.key]);
 };
