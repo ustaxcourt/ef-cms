5c94ae2994a3c0867668335a0ad957164b7ec65e
Timothy Shumway <tshumway@flexion.us>
2023-03-29 14:15:56 -0600

    9828: add missing jsdocs

--- web-client/src/presenter/actions/clearConfirmationTextAction.ts
+++ web-client/src/presenter/actions/clearConfirmationTextAction.ts
@@ -1,7 +1,7 @@
 import { state } from 'cerebral';
 
 /**
- * resets the state.confirmationText which is used to display a confirmation message.
+ * unsets the confirmationText object from state.
  *
  * @param {object} providers the providers object
  * @param {object} providers.store the cerebral store object used for clearing the form
