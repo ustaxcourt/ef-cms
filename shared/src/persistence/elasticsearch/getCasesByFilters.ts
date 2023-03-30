61813a2226b824ce3d4358e1695ba711e1580b73
Jason Fu <jfu@flexion.us>
2023-03-30 14:59:02 -0700

    9723: fleshed out persistence class, enabled dropdown values to come from entity constants

--- shared/src/persistence/elasticsearch/getCasesByFilters.ts
+++ shared/src/persistence/elasticsearch/getCasesByFilters.ts
@@ -1,40 +1,67 @@
-export const getCasesByFilters = async ({ applicationContext, params }) => {
-  const params = {
-    caseStatuses: [],
-    caseTypes: [],
-    createEndDate: '',
-    createStartDate: '',
-    paperFilingMethods: [],
+import { CaseStatus, CaseType } from '../../business/entities/EntityConstants';
+
+export const getCasesByFilters = async ({
+  applicationContext,
+  params,
+}: {
+  applicationContext: IApplicationContext;
+  params: {
+    caseStatuses: CaseStatus[];
+    caseTypes: CaseType[];
+    createEndDate: string;
+    createStartDate: string;
+    filingMethod: 'all' | 'electronic' | 'paper';
   };
+}) => {
+  const filters = [];
+
+  const createDateFilter = {
+    range: {
+      'createdAt.S': {
+        gte: params.createStartDate,
+        lt: params.createEndDate,
+      },
+    },
+  };
+  filters.push(createDateFilter);
+
+  if (params.caseStatuses.length) {
+    const caseStatusesFilters = {
+      terms: {
+        'status.S': params.caseStatuses,
+      },
+    };
+    filters.push(caseStatusesFilters);
+  }
+
+  if (params.caseTypes.length) {
+    const caseTypeFilters = {
+      terms: {
+        'caseType.S': params.caseTypes,
+      },
+    };
+    filters.push(caseTypeFilters);
+  }
+
+  if (params.filingMethod !== 'all') {
+    const filingMethodFilter = {
+      match: {
+        'isPaper.BOOL': params.filingMethod === 'paper',
+      },
+    };
+    filters.push(filingMethodFilter);
+  }
+
   const { results } = await search({
     applicationContext,
     searchParameters: {
       body: {
         query: {
           bool: {
-            must: [
-              {
-                range: {
-                  'receivedAt.S': {
-                    gte: computeDate({ day: 1, month: 1, year: 2021 }),
-                    lt: computeDate({ day: 1, month: 11, year: 2021 }),
-                  },
-                },
-              },
-              {
-                match: {
-                  'isPaper.BOOL': true,
-                },
-                // },
-                // {
-                //   match: {
-                //     'caseType.S': 'Whistleblower',
-                //   },
-              },
-            ],
+            must: filters,
           },
         },
-        sort: [{ 'receivedAt.S': 'asc' }],
+        sort: [{ 'createdAt.S': 'asc' }],
       },
       index: 'efcms-case',
       size: 10000,
