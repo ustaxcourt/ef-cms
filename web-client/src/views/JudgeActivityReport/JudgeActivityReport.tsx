05fa12a39fd5c71adefb87e90c81503b4f3c6747
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-03-30 11:18:36 -0700

    9809: WIP, align button with date picker

--- web-client/src/views/JudgeActivityReport/JudgeActivityReport.tsx
+++ web-client/src/views/JudgeActivityReport/JudgeActivityReport.tsx
@@ -1,6 +1,6 @@
 import { BigHeader } from '../BigHeader';
 import { Button } from '../../ustc-ui/Button/Button';
-import { DateRangePickerComponent } from '../../ustc-ui/DateInput/DateRangePickerComponent';
+import { DateInput } from '../../ustc-ui/DateInput/DateInput';
 import { ErrorNotification } from '../ErrorNotification';
 import { SuccessNotification } from '../SuccessNotification';
 import { connect } from '@cerebral/react';
@@ -8,8 +8,10 @@ import { state } from 'cerebral';
 import React from 'react';
 
 export const JudgeActivityReport = connect(
-  {},
-  function JudgeActivityReport({}) {
+  {
+    form: state.form,
+  },
+  function JudgeActivityReport({ form }) {
     return (
       <>
         <BigHeader text="Reports" />
@@ -20,26 +22,53 @@ export const JudgeActivityReport = connect(
             <h1>Activity - </h1>
           </div>
 
-          <div className="grid-row">
-            <div className="blue-container">
-              <DateRangePickerComponent
-                endDateErrorText=""
-                endName="deadlineEnd"
-                endValue=""
-                startDateErrorText=""
-                startName="deadlineStart"
-                startValue=""
-                onChangeEnd={e => {}}
-                onChangeStart={e => {}}
-              />
-              <Button
-                id="update-date-range-deadlines-button"
-                onClick={() => {
-                  // updateDateRangeForDeadlinesSequence();
-                }}
-              >
-                Run Report
-              </Button>
+          <div className="blue-container">
+            <div className="display-flex flex-row flex-align-end">
+              <div className="">
+                <DateInput
+                  errorText=""
+                  label="Start date"
+                  names={{
+                    day: 'startDay',
+                    month: 'startMonth',
+                    year: 'startYear',
+                  }}
+                  values={{
+                    day: form.startDay,
+                    month: form.startMonth,
+                    year: form.startYear,
+                  }}
+                  onBlur={() => {}}
+                  onChange={() => {}}
+                />
+              </div>
+              <div className="display-flex flex-column flex-auto ">
+                <DateInput
+                  errorText=""
+                  label="End date"
+                  names={{
+                    day: 'endDay',
+                    month: 'endMonth',
+                    year: 'endYear',
+                  }}
+                  values={{
+                    day: form.endDay,
+                    month: form.endMonth,
+                    year: form.endYear,
+                  }}
+                  onBlur={() => {}}
+                  onChange={() => {}}
+                />
+              </div>
+              <div className="">
+                <Button
+                  onClick={() => {
+                    // updateDateRangeForDeadlinesSequence();
+                  }}
+                >
+                  Run Report
+                </Button>
+              </div>
             </div>
           </div>
         </section>
