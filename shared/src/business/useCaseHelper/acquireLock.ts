b6b016c0c58a1b56b5a8edfdeef3f589c1ffdf86
Michael Marcotte <mike.marcotte@gmail.com>
2023-04-03 12:33:58 -0400

    9739: add useCaseHelper for acquiring a lock

---
+++ shared/src/business/useCaseHelper/acquireLock.ts
@@ -0,0 +1,25 @@
+import { ServiceUnavailableError } from '../../errors/errors';
+
+export const acquireLock = async ({
+  applicationContext,
+  lockName,
+}: {
+  applicationContext: IApplicationContext;
+  lockName: string;
+}) => {
+  const currentLock = await applicationContext
+    .getPersistenceGateway()
+    .getLock({ applicationContext, lockName });
+
+  if (currentLock) {
+    throw new ServiceUnavailableError(`${lockName} is currently locked`);
+  }
+
+  const lockId = await applicationContext.getUniqueId();
+  return applicationContext.getPersistenceGateway().acquireLock({
+    applicationContext,
+    lockId,
+    lockName,
+    user: applicationContext.getCurrentUser(),
+  });
+};
