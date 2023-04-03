32f3db9f5eaeb57ed40abcd56521436912765a28
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-04-03 10:25:10 -0700

    9809: Add validation for start and end date values for judgea activity report

--- shared/src/business/entities/judgeActivityReport/JudgeActivityReportSearch.test.ts
+++ shared/src/business/entities/judgeActivityReport/JudgeActivityReportSearch.test.ts
@@ -1,19 +1,74 @@
-const { DeadlineSearch } = require('./DeadlineSearch');
+import { JudgeActivityReportSearch } from './JudgeActivityReportSearch';
+import { calculateISODate } from '../../utilities/DateHandler';
 
-describe('DeadlineSearch', () => {
-  it('creates a deadline search and initializes it', () => {
-    const deadlineSearchEntity = new DeadlineSearch();
-
-    expect(deadlineSearchEntity.entityName).toEqual('DeadlineSearch');
-
-    deadlineSearchEntity.init({
+describe('JudgeActivityReportSearch', () => {
+  it('should have validation errors when start date is not provided', () => {
+    const judgeActivityReportSearchEntity = new JudgeActivityReportSearch({
       endDate: '01/01/2000',
-      startDate: '01/01/2000',
+      startDate: undefined,
     });
 
-    expect(deadlineSearchEntity.validate().toRawObject()).toMatchObject({
-      endDate: '01/01/2000',
-      startDate: '01/01/2000',
+    expect(
+      judgeActivityReportSearchEntity.getFormattedValidationErrors(),
+    ).toMatchObject({
+      startDate: 'Enter a start date.',
+    });
+  });
+
+  it('should have validation errors when end date is not provided', () => {
+    const judgeActivityReportSearchEntity = new JudgeActivityReportSearch({
+      endDate: undefined,
+      startDate: '2025-02-01T00:00:00.000Z',
+    });
+
+    expect(
+      judgeActivityReportSearchEntity.getFormattedValidationErrors(),
+    ).toMatchObject({
+      endDate: 'Enter an end date.',
+    });
+  });
+
+  it('should have validation errors when the end date provided is chronologically before the start date', () => {
+    const judgeActivityReportSearchEntity = new JudgeActivityReportSearch({
+      endDate: '2021-02-01T00:00:00.000Z',
+      startDate: '2022-02-01T00:00:00.000Z',
+    });
+
+    expect(
+      judgeActivityReportSearchEntity.getFormattedValidationErrors(),
+    ).toMatchObject({
+      endDate:
+        'End date cannot be prior to Start Date. Enter a valid End date.',
+    });
+  });
+
+  it('should have validation errors when the start date provided is in the future', () => {
+    const mockFutureDate = calculateISODate({ howMuch: 5, units: 'days' });
+
+    const judgeActivityReportSearchEntity = new JudgeActivityReportSearch({
+      endDate: mockFutureDate,
+      startDate: mockFutureDate,
+    });
+
+    expect(
+      judgeActivityReportSearchEntity.getFormattedValidationErrors(),
+    ).toMatchObject({
+      startDate: 'Start date cannot be in the future. Enter a valid date.',
+    });
+  });
+
+  it('should have validation errors when the end date provided is in the future', () => {
+    const mockFutureDate = calculateISODate({ howMuch: 5, units: 'days' });
+
+    const judgeActivityReportSearchEntity = new JudgeActivityReportSearch({
+      endDate: mockFutureDate,
+      startDate: '2020-03-01T00:00:00.000Z',
+    });
+
+    expect(
+      judgeActivityReportSearchEntity.getFormattedValidationErrors(),
+    ).toMatchObject({
+      endDate: 'End date cannot be in the future. Enter a valid date.',
     });
   });
 });
