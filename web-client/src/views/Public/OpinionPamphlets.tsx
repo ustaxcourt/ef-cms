e09b39471e4659890e28fcc66b1ac4101cf84605
Rosie Highsmith <ahighsmith@flexion.us>
2023-04-03 15:27:35 -0700

    9865: fix typo on variable; WIP skeleton add NonMobile and Mobile components to tsx

--- web-client/src/views/Public/OpinionPamphlets.tsx
+++ web-client/src/views/Public/OpinionPamphlets.tsx
@@ -1,5 +1,6 @@
 import { BigHeader } from '../BigHeader';
 import { Button } from '../../ustc-ui/Button/Button';
+import { Mobile, NonMobile } from '../../ustc-ui/Responsive/Responsive';
 import { connect } from '@cerebral/react';
 import { sequences, state } from 'cerebral';
 import React from 'react';
@@ -18,92 +19,183 @@ export const OpinionPamphlets = connect(
       <>
         <BigHeader text="United States Tax Court Reports" />
 
-        <section className="usa-section grid-container opinion-pamphlets">
-          {opinionPamphletsHelper.pamphletPeriods.map(period => {
-            return (
-              <div className="margin-bottom-1" key={period}>
-                <h2>{period}</h2>
-                {opinionPamphletsHelper.yearAndFilingDateMap[period].map(
-                  filingDateKey => {
-                    return (
-                      <div className="grid-row grid-gap" key={filingDateKey}>
-                        <div className="grid-col-4">
-                          <Button
-                            link
-                            aria-label={`View PDF: ${opinionPamphletsHelper.pamphletsGroupedByFilingDate[filingDateKey][0].documentTitle}`}
-                            className="margin-bottom-2 text-left line-height-standard padding-0"
-                            target="_blank"
-                            onClick={() => {
-                              openCaseDocumentDownloadUrlSequence({
-                                docketEntryId:
-                                  opinionPamphletsHelper.getPamhpletToDisplay(
-                                    filingDateKey,
-                                  ).docketEntryId,
-                                docketNumber:
-                                  opinionPamphletsHelper.getPamhpletToDisplay(
-                                    filingDateKey,
-                                  ).docketNumber,
-                                isPublic: true,
-                                useSameTab: false,
-                              });
-                            }}
-                          >
+        <NonMobile>
+          <section className="usa-section grid-container opinion-pamphlets">
+            {opinionPamphletsHelper.pamphletPeriods.map(period => {
+              return (
+                <div className="margin-bottom-1" key={period}>
+                  <h2>{period}</h2>
+                  {opinionPamphletsHelper.yearAndFilingDateMap[period].map(
+                    filingDateKey => {
+                      return (
+                        <div className="grid-row grid-gap" key={filingDateKey}>
+                          <div className="grid-col-4">
+                            <Button
+                              link
+                              aria-label={`View PDF: ${opinionPamphletsHelper.pamphletsGroupedByFilingDate[filingDateKey][0].documentTitle}`}
+                              className="margin-bottom-2 text-left line-height-standard padding-0"
+                              target="_blank"
+                              onClick={() => {
+                                openCaseDocumentDownloadUrlSequence({
+                                  docketEntryId:
+                                    opinionPamphletsHelper.getPamphletToDisplay(
+                                      filingDateKey,
+                                    ).docketEntryId,
+                                  docketNumber:
+                                    opinionPamphletsHelper.getPamphletToDisplay(
+                                      filingDateKey,
+                                    ).docketNumber,
+                                  isPublic: true,
+                                  useSameTab: false,
+                                });
+                              }}
+                            >
+                              {
+                                opinionPamphletsHelper.getPamphletToDisplay(
+                                  filingDateKey,
+                                ).documentTitle
+                              }
+                            </Button>
+                          </div>
+                          <div className="grid-col-6"></div>
+                          <div className="grid-col-2 text-right">
+                            <span className="text-bold">Filed:</span>{' '}
                             {
-                              opinionPamphletsHelper.getPamhpletToDisplay(
+                              opinionPamphletsHelper.getPamphletToDisplay(
                                 filingDateKey,
-                              ).documentTitle
+                              ).formattedFilingDate
                             }
-                          </Button>
+                          </div>
+                          <table
+                            aria-label="docket record"
+                            className="usa-table case-detail ustc-table responsive-table"
+                            id="docket-record-table"
+                            key={filingDateKey}
+                          >
+                            <thead>
+                              <tr>
+                                <th className="width-card-lg">Docket No.</th>
+                                <th>Case</th>
+                                <th className="width-15 text-right">Page</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              {opinionPamphletsHelper.pamphletsGroupedByFilingDate[
+                                filingDateKey
+                              ].map(pamphlet => {
+                                return (
+                                  <tr key={pamphlet.docketEntryId}>
+                                    <td className="width-card-lg">
+                                      {pamphlet.docketNumber}
+                                    </td>
+                                    <td>{pamphlet.caseCaption}</td>
+                                    {/* TODO: update this property with work done in 9967 */}
+                                    <td className="width-15 text-right">
+                                      {pamphlet.pageCount}
+                                    </td>
+                                  </tr>
+                                );
+                              })}
+                            </tbody>
+                          </table>
                         </div>
-                        <div className="grid-col-6"></div>
-                        <div className="grid-col-2 text-right">
-                          <span className="text-bold">Filed:</span>{' '}
-                          {
-                            opinionPamphletsHelper.getPamhpletToDisplay(
-                              filingDateKey,
-                            ).formattedFilingDate
-                          }
+                      );
+                    },
+                  )}
+                </div>
+              );
+            })}
+          </section>
+        </NonMobile>
+
+        <Mobile>
+          <section className="usa-section grid-container opinion-pamphlets">
+            {opinionPamphletsHelper.pamphletPeriods.map(period => {
+              return (
+                <div className="margin-bottom-1" key={period}>
+                  <h2>{period}</h2>
+                  {opinionPamphletsHelper.yearAndFilingDateMap[period].map(
+                    filingDateKey => {
+                      return (
+                        <div className="grid-row grid-gap" key={filingDateKey}>
+                          <div className="grid-col-4">
+                            <Button
+                              link
+                              aria-label={`View PDF: ${opinionPamphletsHelper.pamphletsGroupedByFilingDate[filingDateKey][0].documentTitle}`}
+                              className="margin-bottom-2 text-left line-height-standard padding-0"
+                              target="_blank"
+                              onClick={() => {
+                                openCaseDocumentDownloadUrlSequence({
+                                  docketEntryId:
+                                    opinionPamphletsHelper.getPamphletToDisplay(
+                                      filingDateKey,
+                                    ).docketEntryId,
+                                  docketNumber:
+                                    opinionPamphletsHelper.getPamphletToDisplay(
+                                      filingDateKey,
+                                    ).docketNumber,
+                                  isPublic: true,
+                                  useSameTab: false,
+                                });
+                              }}
+                            >
+                              {
+                                opinionPamphletsHelper.getPamphletToDisplay(
+                                  filingDateKey,
+                                ).documentTitle
+                              }
+                            </Button>
+                          </div>
+                          <div className="grid-col-6"></div>
+                          <div className="grid-col-2 text-right">
+                            <span className="text-bold">Filed:</span>{' '}
+                            {
+                              opinionPamphletsHelper.getPamphletToDisplay(
+                                filingDateKey,
+                              ).formattedFilingDate
+                            }
+                          </div>
+                          <table
+                            aria-label="docket record"
+                            className="usa-table case-detail ustc-table responsive-table"
+                            id="docket-record-table"
+                            key={filingDateKey}
+                          >
+                            <thead>
+                              <tr>
+                                <th className="width-card-lg">Docket No.</th>
+                                <th>Case</th>
+                                <th className="width-15 text-right">Page</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              {opinionPamphletsHelper.pamphletsGroupedByFilingDate[
+                                filingDateKey
+                              ].map(pamphlet => {
+                                return (
+                                  <tr key={pamphlet.docketEntryId}>
+                                    <td className="width-card-lg">
+                                      {pamphlet.docketNumber}
+                                    </td>
+                                    <td>{pamphlet.caseCaption}</td>
+                                    {/* TODO: update this property with work done in 9967 */}
+                                    <td className="width-15 text-right">
+                                      {pamphlet.pageCount}
+                                    </td>
+                                  </tr>
+                                );
+                              })}
+                            </tbody>
+                          </table>
                         </div>
-                        <table
-                          aria-label="docket record"
-                          className="usa-table case-detail ustc-table responsive-table"
-                          id="docket-record-table"
-                          key={filingDateKey}
-                        >
-                          <thead>
-                            <tr>
-                              <th className="width-card-lg">Docket No.</th>
-                              <th>Case</th>
-                              <th className="width-15 text-right">Page</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            {opinionPamphletsHelper.pamphletsGroupedByFilingDate[
-                              filingDateKey
-                            ].map(pamphlet => {
-                              return (
-                                <tr key={pamphlet.docketEntryId}>
-                                  <td className="width-card-lg">
-                                    {pamphlet.docketNumber}
-                                  </td>
-                                  <td>{pamphlet.caseCaption}</td>
-                                  {/* TODO: update this property with work done in 9967 */}
-                                  <td className="width-15 text-right">
-                                    {pamphlet.pageCount}
-                                  </td>
-                                </tr>
-                              );
-                            })}
-                          </tbody>
-                        </table>
-                      </div>
-                    );
-                  },
-                )}
-              </div>
-            );
-          })}
-        </section>
+                      );
+                    },
+                  )}
+                </div>
+              );
+            })}
+          </section>
+        </Mobile>
       </>
     );
   },
