d2d0e9c98b6589c666091387848206b87d85e17f
Timothy Shumway <tshumway@flexion.us>
2023-04-03 15:28:11 -0600

    9598: radio boxes updating value

--- web-client/src/views/FileDocument/ConsolidatedCaseGroupFiling.tsx
+++ web-client/src/views/FileDocument/ConsolidatedCaseGroupFiling.tsx
@@ -1,60 +1,79 @@
 import { FormGroup } from '../../ustc-ui/FormGroup/FormGroup';
+import { connect } from '@cerebral/react';
+import { sequences, state } from 'cerebral';
 import React from 'react';
 
-export const ConsolidatedCaseGroupFiling = () => {
-  return (
-    <>
-      <h2 className="margin-top-4">Which Cases Do You Want to File In?</h2>
-      <div className="blue-container">
-        <FormGroup>
-          <fieldset
-            className="usa-fieldset margin-bottom-0"
-            id="consolidated-case-group-radios"
-          >
-            <legend
-              className="with-hint"
-              htmlFor="consolidated-case-group-radios"
-              id="consolidated-case-group-radios-legend"
+export const ConsolidatedCaseGroupFiling = connect(
+  {
+    form: state.form,
+    updateFormValueSequence: sequences.updateFormValueSequence,
+  },
+  function ConsolidatedCaseGroupFiling({ form, updateFormValueSequence }) {
+    return (
+      <>
+        <h2 className="margin-top-4">Which Cases Do You Want to File In?</h2>
+        <div className="blue-container">
+          <FormGroup>
+            <fieldset
+              className="usa-fieldset margin-bottom-0"
+              id="consolidated-case-group-radios"
             >
-              This case is part of a consolidated group.
-            </legend>
-            <span className="usa-hint">
-              Select the group or this case to file in.
-            </span>
-            <input
-              defaultChecked
-              aria-describedby="consolidated-case-group-radios-legend"
-              className="usa-radio__input"
-              id="consolidated-group-all"
-              name="consolidated-group-all"
-              type="radio"
-              onChange={e => (e.target.checked = true)}
-            />
-            <label
-              className="usa-radio__label"
-              htmlFor="consolidated-group-all"
-            >
-              All in the consolidated group
-            </label>
-            {/* TODO: map over consolidated cases */}
-            <input
-              aria-describedby="consolidated-case-group-radios-legend"
-              className="usa-radio__input"
-              id="consolidated-group-current-case"
-              name="consolidated-group-current-case"
-              type="radio"
-              onChange={e => (e.target.checked = true)}
-            />
-            <label
-              className="usa-radio__label"
-              htmlFor="consolidated-group-current-case"
-            >
-              {/* TODO: get current case information */}
-              Current case
-            </label>
-          </fieldset>
-        </FormGroup>
-      </div>
-    </>
-  );
-};
+              <legend
+                className="with-hint"
+                htmlFor="consolidated-case-group-radios"
+                id="consolidated-case-group-radios-legend"
+              >
+                This case is part of a consolidated group.
+              </legend>
+              <span className="usa-hint">
+                Select the group or this case to file in.
+              </span>
+              <input
+                aria-describedby="consolidated-case-group-radios-legend"
+                checked={form.fileAcrossConsolidatedGroup === true}
+                className="usa-radio__input"
+                id="consolidated-group-all"
+                name="fileAcrossConsolidatedGroup"
+                type="radio"
+                onChange={e => {
+                  updateFormValueSequence({
+                    key: e.target.name,
+                    value: true,
+                  });
+                }}
+              />
+              <label
+                className="usa-radio__label"
+                htmlFor="consolidated-group-all"
+              >
+                All in the consolidated group
+              </label>
+              {/* TODO: map over consolidated cases */}
+              <input
+                aria-describedby="consolidated-case-group-radios-legend"
+                checked={form.fileAcrossConsolidatedGroup === false}
+                className="usa-radio__input"
+                id="consolidated-group-current-case"
+                name="fileAcrossConsolidatedGroup"
+                type="radio"
+                onChange={e => {
+                  updateFormValueSequence({
+                    key: e.target.name,
+                    value: false,
+                  });
+                }}
+              />
+              <label
+                className="usa-radio__label"
+                htmlFor="consolidated-group-current-case"
+              >
+                {/* TODO: get current case information */}
+                Current case
+              </label>
+            </fieldset>
+          </FormGroup>
+        </div>
+      </>
+    );
+  },
+);
