0a49b0b099a289e26af5e0cc639ce90e7e7ee6f7
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2023-04-03 15:06:54 -0600

    9865 sorting working now

--- web-client/src/presenter/computeds/Public/opinionPamphletsHelper.ts
+++ web-client/src/presenter/computeds/Public/opinionPamphletsHelper.ts
@@ -1,19 +1,11 @@
 import { isEmpty, orderBy, uniq } from 'lodash';
 import { state } from 'cerebral';
 
-// --pamphletsGroupedByFilingDate - each array -arrange cases by petr last name alphabetivsly
-//sort years desc
-//sort filing date keys desc
-const sortCasesByPetitionerName = pamphletsGroupedByFilingDate => {
+const sortCasesByCaseCaption = pamphletsGroupedByFilingDate => {
   Object.keys(pamphletsGroupedByFilingDate).forEach(filingDateKey => {
-    pamphletsGroupedByFilingDate[filingDateKey].map(tcrp => {
-      const firstPetitonerName = tcrp.caseCaption.split(',')[0].split(' ');
-      tcrp.sortingName = firstPetitonerName[firstPetitonerName.length - 1];
-    });
-
     pamphletsGroupedByFilingDate[filingDateKey] = orderBy(
       pamphletsGroupedByFilingDate[filingDateKey],
-      'sortingName',
+      'caseCaption',
       'asc',
     );
   });
@@ -46,7 +38,6 @@ const groupPamphletsByFilingDate = ({
 
 export const opinionPamphletsHelper = (get, applicationContext) => {
   const opinionPamphlets: any = get(state.opinionPamphlets);
-  const yearAndFilingDateMap = {};
 
   const pamphletsGroupedByFilingDate = groupPamphletsByFilingDate({
     applicationContext,
@@ -55,15 +46,19 @@ export const opinionPamphletsHelper = (get, applicationContext) => {
 
   const uniqueFilingDates = Object.keys(pamphletsGroupedByFilingDate);
 
-  const pamphletPeriods: string[] = uniq(
+  const pamphletPeriods: any[] = uniq(
     uniqueFilingDates.map(filingDate => {
       const b = applicationContext.getUtilities().deconstructDate(filingDate);
       return b?.year;
     }),
   );
 
-  uniqueFilingDates.forEach(filingDate => {
-    pamphletPeriods.forEach(year => {
+  pamphletPeriods.sort((a, b) => b - a);
+
+  let yearAndFilingDateMap = {};
+
+  pamphletPeriods.forEach(year => {
+    uniqueFilingDates.forEach(filingDate => {
       if (filingDate.split('-')[0] === year) {
         if (isEmpty(yearAndFilingDateMap[year])) {
           yearAndFilingDateMap[year] = [filingDate];
@@ -78,10 +73,11 @@ export const opinionPamphletsHelper = (get, applicationContext) => {
     return pamphletsGroupedByFilingDate[filingDateKey][0];
   };
 
-  sortCasesByPetitionerName(pamphletsGroupedByFilingDate);
+  sortCasesByCaseCaption(pamphletsGroupedByFilingDate);
 
   return {
     getPamhpletToDisplay,
+    pamphletPeriods,
     pamphletsGroupedByFilingDate,
     yearAndFilingDateMap,
   };
