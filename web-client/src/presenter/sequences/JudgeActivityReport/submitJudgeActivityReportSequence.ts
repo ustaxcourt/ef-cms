9cdded41f053edf2826d3115b80c5cc36f85fdf3
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-04-04 12:37:13 -0700

    9809: Clear errors on judge activity report when submitting form and it's valid, add action for retrieving judge activity report

--- web-client/src/presenter/sequences/JudgeActivityReport/submitJudgeActivityReportSequence.ts
+++ web-client/src/presenter/sequences/JudgeActivityReport/submitJudgeActivityReportSequence.ts
@@ -1,22 +1,31 @@
+import { clearAlertsAction } from '../../actions/clearAlertsAction';
+import { clearErrorAlertsAction } from '../../actions/clearErrorAlertsAction';
 import { getComputedFormDateFactoryAction } from '../../actions/getComputedFormDateFactoryAction';
+import { getJudgeActivityReportReportAction } from '../../actions/JudgeActivityReport/getJudgeActivityReportReportAction';
 import { setAlertErrorAction } from '../../actions/setAlertErrorAction';
 import { setValidationAlertErrorsAction } from '../../actions/setValidationAlertErrorsAction';
 import { setValidationErrorsAction } from '../../actions/setValidationErrorsAction';
 import { showProgressSequenceDecorator } from '../../utilities/showProgressSequenceDecorator';
 import { startShowValidationAction } from '../../actions/startShowValidationAction';
+import { stopShowValidationAction } from '../../actions/stopShowValidationAction';
 import { validateJudgeActivityReportSearchAction } from '../../actions/JudgeActivityReport/validateJudgeActivityReportSearchAction';
 
 export const submitJudgeActivityReportSequence = showProgressSequenceDecorator([
+  startShowValidationAction,
   getComputedFormDateFactoryAction('start', false, 'computedStartDate'),
   getComputedFormDateFactoryAction('end', false, 'computedEndDate'),
   validateJudgeActivityReportSearchAction,
   {
     error: [
-      startShowValidationAction,
       setAlertErrorAction,
       setValidationErrorsAction,
       setValidationAlertErrorsAction,
     ],
-    success: [],
+    success: [
+      stopShowValidationAction,
+      clearErrorAlertsAction,
+      clearAlertsAction,
+      getJudgeActivityReportReportAction,
+    ],
   },
 ]);
