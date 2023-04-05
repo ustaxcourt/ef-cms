f37d11d97b260bec8a8260ebb89d404d04cfaa03
Zachary Rogers <zrogers>
2023-04-04 21:22:48 -0700

    9723: WIP added conditions for arrays when setting filters

--- web-client/src/presenter/actions/CaseInventoryReport/setStartOrEndCreatedAtDateAction.test.ts
+++ web-client/src/presenter/actions/CaseInventoryReport/setStartOrEndCreatedAtDateAction.test.ts
@@ -1,3 +1,4 @@
+import { GetCaseInventoryReportRequest } from '../../../../../shared/src/business/useCases/caseInventoryReport/getCustomCaseInventoryReportInteractor';
 import { applicationContextForClient as applicationContext } from '../../../../../shared/src/business/test/createTestApplicationContext';
 import { presenter } from '../../presenter-mock';
 import { runAction } from 'cerebral/test';
@@ -8,24 +9,27 @@ presenter.providers.applicationContext = applicationContext;
 describe('setStartOrEndCreatedAtDateAction', () => {
   const expectedDate = '2019-05-14T04:00:00.000Z';
   const regularDate = '05/14/2019';
-
+  let initialFilterState: GetCaseInventoryReportRequest;
   beforeEach(() => {
-    applicationContext
-      .getUtilities()
-      .createISODateString.mockReturnValue(expectedDate);
+    initialFilterState = {
+      caseStatuses: [],
+      caseTypes: [],
+      createEndDate: '',
+      createStartDate: '',
+      filingMethod: 'all',
+    };
   });
+
   it('should set customCaseInventoryFilters createStartDate in state', async () => {
     const result = await runAction(setStartOrEndCreatedAtDateAction, {
       modules: { presenter },
       props: {
-        key: 'createStartDate',
-        value: regularDate,
+        createStartDate: regularDate,
+      },
+      state: {
+        customCaseInventoryFilters: initialFilterState,
       },
     });
-
-    expect(
-      applicationContext.getUtilities().createISODateString,
-    ).toHaveBeenCalledWith(regularDate, 'MM/dd/yyyy');
 
     expect(result.state.customCaseInventoryFilters.createStartDate).toEqual(
       expectedDate,
@@ -36,17 +40,66 @@ describe('setStartOrEndCreatedAtDateAction', () => {
     const result = await runAction(setStartOrEndCreatedAtDateAction, {
       modules: { presenter },
       props: {
-        key: 'createEndDate',
-        value: regularDate,
+        createEndDate: regularDate,
+      },
+      state: {
+        customCaseInventoryFilters: initialFilterState,
       },
     });
-
-    expect(
-      applicationContext.getUtilities().createISODateString,
-    ).toHaveBeenCalledWith(regularDate, 'MM/dd/yyyy');
 
     expect(result.state.customCaseInventoryFilters.createEndDate).toEqual(
       expectedDate,
     );
+  });
+
+  it('should set filing method in state', async () => {
+    const result = await runAction(setStartOrEndCreatedAtDateAction, {
+      modules: { presenter },
+      props: {
+        filingMethod: 'electronic',
+      },
+      state: {
+        customCaseInventoryFilters: initialFilterState,
+      },
+    });
+
+    expect(result.state.customCaseInventoryFilters.filingMethod).toEqual(
+      'electronic',
+    );
+  });
+
+  it('should add a caseStatus filter to state', async () => {
+    initialFilterState.caseStatuses = ['Assigned - Case'];
+    const result = await runAction(setStartOrEndCreatedAtDateAction, {
+      modules: { presenter },
+      props: {
+        caseStatuses: { action: 'add', caseStatus: 'CAV' },
+      },
+      state: {
+        customCaseInventoryFilters: initialFilterState,
+      },
+    });
+
+    expect(result.state.customCaseInventoryFilters.caseStatuses).toEqual([
+      'Assigned - Case',
+      'CAV',
+    ]);
+  });
+
+  it('should remove a caseStatus filter from state', async () => {
+    initialFilterState.caseStatuses = ['Assigned - Case', 'CAV'];
+    const result = await runAction(setStartOrEndCreatedAtDateAction, {
+      modules: { presenter },
+      props: {
+        caseStatuses: { action: 'remove', caseStatus: 'CAV' },
+      },
+      state: {
+        customCaseInventoryFilters: initialFilterState,
+      },
+    });
+
+    expect(result.state.customCaseInventoryFilters.caseStatuses).toEqual([
+      'Assigned - Case',
+    ]);
   });
 });
