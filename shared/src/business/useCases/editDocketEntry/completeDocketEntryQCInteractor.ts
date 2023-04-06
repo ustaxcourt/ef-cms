11f3afb6c2010081fc90f4174aa50f58401f3ade
Michael Marcotte <mike.marcotte@gmail.com>
2023-04-05 23:39:30 -0400

    9739: make mock locks available for reuse

--- shared/src/business/useCases/editDocketEntry/completeDocketEntryQCInteractor.ts
+++ shared/src/business/useCases/editDocketEntry/completeDocketEntryQCInteractor.ts
@@ -23,7 +23,7 @@ import { generateNoticeOfDocketChangePdf } from '../../useCaseHelper/noticeOfDoc
 import { getCaseCaptionMeta } from '../../utilities/getCaseCaptionMeta';
 import { getDocumentTitleForNoticeOfChange } from '../../utilities/getDocumentTitleForNoticeOfChange';
 import { replaceBracketed } from '../../utilities/replaceBracketed';
-import { withLocking } from '../../../persistence/dynamo/locks/acquireLock';
+import { withLocking } from '../../useCaseHelper/acquireLock';
 
 export const needsNewCoversheet = ({
   applicationContext,
