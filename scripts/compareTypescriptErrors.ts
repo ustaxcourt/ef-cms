985d9b1e46c86ce26d65ba799e2e7815b299484c
Zachary Rogers <zrogers>
2023-04-07 10:55:09 -0700

    Add description text to typescript script

--- scripts/compareTypescriptErrors.ts
+++ scripts/compareTypescriptErrors.ts
@@ -1,16 +1,25 @@
 import { spawnSync } from 'child_process';
 
-// ├── branchToBeCompared
-// │   ├── **Your branch Repository**
-// ├── stagingBranch
-// │   ├── **Entire Staging branch**
-// |   ├── scripts
-// |   |   ├── compareTypescriptErrors.ts
+/*
+ This script is meant to be run in a CI machine and compare the number of type errors your branch has with staging.
+ For this script to work the machine needs both your branch and staging together on the local file system
+ and for "npm i" to be run in both directories before this script can run.
+ This script relies on the folder structure below in order to be able to operate.
+
+ ************Required folder structure for this script **********
+ ├── stagingBranch
+ │   ├── **Entire Staging Branch**
+ ├── branchToBeCompared
+ │   ├── **Your entire branch**
+ |   ├── scripts
+ |   |   ├── compareTypescriptErrors.ts
+*/
 
 function countTypescriptErrors(text: string): number {
   return (text.match(/: error TS/g) || []).length;
 }
 
+console.log('Typechecking your branch...');
 const branchToBeComparedTypescriptOutput = spawnSync(
   'npx',
   ['tsc', '--noEmit'],
@@ -19,23 +28,23 @@ const branchToBeComparedTypescriptOutput = spawnSync(
     maxBuffer: 1024 * 5000,
   },
 );
-
 const branchToBeComparedErrorCount = countTypescriptErrors(
   branchToBeComparedTypescriptOutput.stdout,
 );
+console.log('Typescript errors in your branch: ', branchToBeComparedErrorCount);
 
+console.log('Typechecking staging...');
 const stagingTypescriptOutput = spawnSync('npx', ['tsc', '--noEmit'], {
   cwd: '../stagingBranch',
   encoding: 'utf-8',
   maxBuffer: 1024 * 5000,
 });
-
 const stagingErrorCount = countTypescriptErrors(stagingTypescriptOutput.stdout);
-
-console.log('Typescript errors in your branch: ', branchToBeComparedErrorCount);
 console.log('Typescript errors in staging: ', stagingErrorCount);
 
 if (branchToBeComparedErrorCount > stagingErrorCount) {
+  console.log('Staging errors: ', stagingErrorCount);
+  console.log('Your branch errors: ', branchToBeComparedErrorCount);
   console.log('ERROR: Your branch has more errors than staging. Failing.');
   process.exit(1);
 } else {
