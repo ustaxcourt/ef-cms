3056b89a3d6e8105e20813cc433804700155c7f0
Zachary Rogers <zrogers>
2023-04-07 10:14:16 -0700

    Minimum code to count typescript errors

--- scripts/compareTypescriptErrors.ts
+++ scripts/compareTypescriptErrors.ts
@@ -1,62 +1,9 @@
 import { spawnSync } from 'child_process';
 
-// eslint-disable-next-line jsdoc/require-jsdoc
-// function findTypescriptErrorCount(text: string): number {
-//   const regex = /Found (\d+) errors in \d+ files\./;
-//   const match = text.match(regex);
-
-//   if (match == null) {
-//     throw new Error(
-//       'UNABLE TO DETERMINE HOW MANY TYPESCRIPT ERRORS FROM TEXT.',
-//     );
-//   }
-//   const errorsCount = parseInt(match[1]);
-//   return errorsCount;
-// }
-
-// eslint-disable-next-line jsdoc/require-jsdoc
-
 function countTypescriptErrors(text: string): number {
   return (text.match(/: error TS/g) || []).length;
 }
 
-// "echo 'npx tsc --noEmit' | script whatever.txt"
-
-// *********************************ExecSync ****************************
-// let typescriptErrorOutput: string = 'bleh';
-// try {
-//   console.log('Running command.');
-//   const commandOutput = execSync('echo "npx tsc --noEmit" | script /dev/null', {
-//     encoding: 'utf-8',
-//     maxBuffer: 1024 * 50000,
-//   });
-//   console.log('Command output: ', commandOutput);
-// } catch (error) {
-//   console.log('Inside of catch: ', error.stdout);
-//   typescriptErrorOutput = error.stdout;
-// }
-
-// const errorCount = findTypescriptErrorCount(typescriptErrorOutput);
-// console.log('ERROR COUNT: ', errorCount);
-
-// *********************************Exec ****************************
-// const someProcess = exec(
-//   'npx tsc --noEmit',
-//   {
-//     maxBuffer: 1024 * 5000,
-//     encoding: 'utf-8',
-//   },
-//   (err, stdout, stderr) => {
-//     console.log('err***', err);
-//     console.log('stdout***', stdout);
-//     console.log('stderr***', stderr);
-//   }
-// );
-
-// someProcess.
-
-// *********************************SPAWN SYNC****************************
-// echo 'npx tsc --noEmit' | script /dev/null
 const typescriptProcess = spawnSync('npx', ['tsc', '--noEmit'], {
   encoding: 'utf-8',
   maxBuffer: 1024 * 5000,
@@ -64,12 +11,3 @@ const typescriptProcess = spawnSync('npx', ['tsc', '--noEmit'], {
 
 const errorCount = countTypescriptErrors(typescriptProcess.stdout);
 console.log('Number of errors: ', errorCount);
-
-// // console.log('I am typescript process: ', typescriptProcess.stdout + typescriptProcess.stderr)
-
-// typescriptProcess.stdout.on('data', function (data) {
-//   console.log('stdout data: ' + data);
-// });
-// typescriptProcess.stdout.on('error', function (data) {
-//   console.log('stdout error: ' + data);
-// });
