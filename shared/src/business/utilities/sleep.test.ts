ee16afd6a159d99f4bb804cb4aa7475caead28a7
Michael Marcotte <mike.marcotte@gmail.com>
2023-04-10 16:43:58 -0400

    9739: fix sleep test

--- shared/src/business/utilities/sleep.test.ts
+++ shared/src/business/utilities/sleep.test.ts
@@ -3,10 +3,10 @@ import { sleep } from './sleep';
 describe('sleep', () => {
   it('waits the specified amount of time before proceeding', async () => {
     const start = new Date().getTime();
-    const waitMs = 500;
+    const waitMs = 50;
 
-    await sleep(500);
+    await sleep(waitMs);
     const finish = new Date().getTime();
-    expect(finish).toBeGreaterThan(start + waitMs);
+    expect(finish).toBeGreaterThanOrEqual(start + waitMs);
   });
 });
