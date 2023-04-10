580f1453682207de33992838b4a7737e552d741f
Rosie Highsmith <ahighsmith@flexion.us>
2023-04-10 16:06:24 -0700

    devex: use glob.sync

--- split-tests-glob.ts
+++ split-tests-glob.ts
@@ -1,24 +1,16 @@
+import glob from 'glob';
 import shuffleSeed from 'shuffle-seed';
-let glob = require('glob');
 
 const testType = process.argv[2] || '';
 
 let testFiles;
 if (testType.includes('unit')) {
   // could use for shared? pass the path?
-  testFiles = glob(
-    './web-client/src/**/?(*.)+(spec|test).[jt]s?(x)',
-    function (err, files) {
-      if (err) {
-        console.log('ooooops', err);
-      }
-
-      return files;
-    },
-  );
+  testFiles = glob.sync('./web-client/src/**/?(*.)+(spec|test).[jt]s?(x)');
 }
-const shuffled = shuffleSeed.shuffle(testFiles, process.env.GITHUB_SHA);
 
+const shuffled = shuffleSeed.shuffle(testFiles, process.env.GITHUB_SHA);
+console.log('shuffled!!!', shuffled);
 const total = parseInt(process.env.CI_NODE_TOTAL!, 10);
 const index = parseInt(process.env.CI_NODE_INDEX!, 10);
 
