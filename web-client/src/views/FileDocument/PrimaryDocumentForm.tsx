eed99ec86491276498f88a27f93441dc07247466
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2023-04-11 13:38:38 -0500

    9864: warning banner

--- web-client/src/views/FileDocument/PrimaryDocumentForm.tsx
+++ web-client/src/views/FileDocument/PrimaryDocumentForm.tsx
@@ -3,6 +3,7 @@ import { FormGroup } from '../../ustc-ui/FormGroup/FormGroup';
 import { InclusionsForm } from './InclusionsForm';
 import { ObjectionsForm } from './ObjectionsForm';
 import { StateDrivenFileInput } from './StateDrivenFileInput';
+import { WarningNotificationComponent } from '../../views/WarningNotification';
 import { connect } from '@cerebral/react';
 import { state } from 'cerebral';
 import React from 'react';
@@ -24,6 +25,16 @@ export const PrimaryDocumentForm = connect(
     return (
       <React.Fragment>
         <h2 className="margin-top-4">{form.documentTitle}</h2>
+        {fileDocumentHelper.redactionAcknowledgementEnabled && (
+          <WarningNotificationComponent
+            alertWarning={{
+              message:
+                'Ensure that personal information (such as Social Security Numbers, Taxpayer Identification Numbers, Employer Identification Numbers) has been removed or redacted.',
+            }}
+            dismissable={false}
+            scrollToTop={false}
+          />
+        )}
         <div className="blue-container">
           <FormGroup errorText={validationErrors.primaryDocumentFile}>
             <label
