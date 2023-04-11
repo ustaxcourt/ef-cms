b97151925db768dbf8e7f6e96f1e6f01cf55baae
Absolutestunna <joshujah007@gmail.com>
2023-04-11 15:26:46 -0400

    9723: WIP - added date validation for date range, need to account for future and past dates edge cases

--- web-client/src/presenter/sequences/getCustomCaseInventoryReportSequence.ts
+++ web-client/src/presenter/sequences/getCustomCaseInventoryReportSequence.ts
@@ -1,5 +1,23 @@
+import { clearAlertsAction } from '../actions/clearAlertsAction';
+import { clearErrorAlertsAction } from '../actions/clearErrorAlertsAction';
 import { getCustomCaseInventoryReportAction } from '../actions/CaseInventoryReport/getCustomCaseInventoryReportAction';
+import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
 import { showProgressSequenceDecorator } from '../utilities/showProgressSequenceDecorator';
+import { startShowValidationAction } from '../actions/startShowValidationAction';
+import { stopShowValidationAction } from '../actions/stopShowValidationAction';
+import { validateCustomInventoryFiltersAction } from '../actions/validateCustomInventoryFiltersAction';
 
 export const getCustomCaseInventoryReportSequence =
-  showProgressSequenceDecorator([getCustomCaseInventoryReportAction]);
+  showProgressSequenceDecorator([
+    startShowValidationAction,
+    validateCustomInventoryFiltersAction,
+    {
+      error: [setValidationErrorsAction],
+      success: [
+        stopShowValidationAction,
+        clearErrorAlertsAction,
+        clearAlertsAction,
+        getCustomCaseInventoryReportAction,
+      ],
+    },
+  ]);
