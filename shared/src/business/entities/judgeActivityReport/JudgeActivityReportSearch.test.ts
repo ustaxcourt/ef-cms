7cbdb86d7f333798b8fa572b07054dde5397831c
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2023-04-12 13:25:04 -0600

    9809 more entity tests

--- shared/src/business/entities/judgeActivityReport/JudgeActivityReportSearch.test.ts
+++ shared/src/business/entities/judgeActivityReport/JudgeActivityReportSearch.test.ts
@@ -6,6 +6,30 @@ import {
 import { JudgeActivityReportSearch } from './JudgeActivityReportSearch';
 
 describe('JudgeActivityReportSearch', () => {
+  describe('constructor', () => {
+    it('should convert startDate to an ISO datetime representing the beginning of the day, EDT', () => {
+      const judgeActivityReportSearchEntity = new JudgeActivityReportSearch({
+        endDate: '01/03/2000',
+        startDate: '01/01/2000',
+      });
+
+      expect(judgeActivityReportSearchEntity.startDate).toEqual(
+        '2000-01-01T05:00:00.000Z',
+      );
+    });
+
+    it('should convert endDate to an ISO datetime representing the end of the day, EDT', () => {
+      const judgeActivityReportSearchEntity = new JudgeActivityReportSearch({
+        endDate: '01/03/2000',
+        startDate: '01/01/2000',
+      });
+
+      expect(judgeActivityReportSearchEntity.endDate).toEqual(
+        '2000-01-04T04:59:59.999Z',
+      );
+    });
+  });
+
   describe('validation', () => {
     const mockFutureDate = formatDateString(
       calculateISODate({ howMuch: 5, units: 'days' }),
