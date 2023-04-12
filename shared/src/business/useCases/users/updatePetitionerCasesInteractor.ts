c8a30c665d4c03d82f521329b1dfc53453ac41d9
Michael Marcotte <mike.marcotte@gmail.com>
2023-04-12 12:41:53 -0400

    9739: add retry for acquireLock

--- shared/src/business/useCases/users/updatePetitionerCasesInteractor.ts
+++ shared/src/business/useCases/users/updatePetitionerCasesInteractor.ts
@@ -1,3 +1,38 @@
+import { ServiceUnavailableError } from '../../../errors/errors';
 import { updatePetitionerCases } from './verifyUserPendingEmailInteractor';
 
-export const updatePetitionerCasesInteractor = updatePetitionerCases;
+export const updatePetitionerCasesInteractor = async ({
+  applicationContext,
+  user,
+}) => {
+  const docketNumbers = await applicationContext
+    .getPersistenceGateway()
+    .getDocketNumbersByUser({
+      applicationContext,
+      userId: user.userId,
+    });
+
+  await applicationContext.getUseCaseHelpers().acquireLock({
+    applicationContext,
+    identifier: docketNumbers,
+    onLockError: new ServiceUnavailableError(
+      'One of the docket numbers is being updated',
+    ),
+    prefix: 'case',
+    retries: 10,
+    ttl: 900,
+    waitTime: 5000,
+  });
+
+  await updatePetitionerCases({
+    applicationContext,
+    docketNumbersAssociatedWithUser: docketNumbers,
+    user,
+  });
+
+  await applicationContext.getPersistenceGateway().removeLock({
+    applicationContext,
+    identifier: docketNumbers,
+    prefix: 'case',
+  });
+};
