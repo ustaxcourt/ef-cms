f51d0f9f7d69e96ea02629f17282b0e1a1a09f3d
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-04-13 14:17:26 -0700

    9809: Adding tests

--- shared/src/business/useCases/trialSessions/getTrialSessionsForJudgeActivityReportInteractor.test.ts
+++ shared/src/business/useCases/trialSessions/getTrialSessionsForJudgeActivityReportInteractor.test.ts
@@ -4,34 +4,34 @@ import { applicationContext } from '../../test/createTestApplicationContext';
 import { docketClerkUser, judgeUser } from '../../../test/mockUsers';
 import { getTrialSessionsForJudgeActivityReportInteractor } from './getTrialSessionsForJudgeActivityReportInteractor';
 
-const MOCK_TRIAL_SESSION = {
-  ...MOCK_TRIAL_REGULAR,
-  endDate: '03/31/2020',
-  judge: {
-    name: judgeUser.name,
-    userId: judgeUser.userId,
-  },
-  startDate: '03/30/2020',
-};
-
-const mockTrialSessions = [MOCK_TRIAL_SESSION];
-
-const mockValidRequest = {
-  endDate: '04/01/2020',
-  judgeId: judgeUser.userId,
-  startDate: '01/01/2020',
-};
-
-beforeEach(() => {
-  applicationContext.getCurrentUser.mockReturnValue(judgeUser);
-
-  applicationContext
-    .getPersistenceGateway()
-    .getTrialSessions.mockReturnValue(mockTrialSessions);
-});
-
 describe('getTrialSessionsForJudgeActivityReportInteractor', () => {
-  it('throws error if user is unauthorized', async () => {
+  const mockRegularTrialSession = {
+    ...MOCK_TRIAL_REGULAR,
+    endDate: '03/31/2020',
+    judge: {
+      name: judgeUser.name,
+      userId: judgeUser.userId,
+    },
+    startDate: '03/30/2020',
+  };
+
+  const mockTrialSessions = [mockRegularTrialSession];
+
+  const mockValidRequest = {
+    endDate: '04/01/2020',
+    judgeId: judgeUser.userId,
+    startDate: '01/01/2020',
+  };
+
+  beforeEach(() => {
+    applicationContext.getCurrentUser.mockReturnValue(judgeUser);
+
+    applicationContext
+      .getPersistenceGateway()
+      .getTrialSessions.mockReturnValue(mockTrialSessions);
+  });
+
+  it('should throw an error when user is unauthorized to retrieve the judge activity report', async () => {
     applicationContext.getCurrentUser.mockReturnValue(docketClerkUser);
 
     await expect(
@@ -42,7 +42,7 @@ describe('getTrialSessionsForJudgeActivityReportInteractor', () => {
     ).rejects.toThrow();
   });
 
-  it('should retrieve all trial sessions for filtering', async () => {
+  it('should retrieve all trial sessions from persistence for filtering', async () => {
     await getTrialSessionsForJudgeActivityReportInteractor(
       applicationContext,
       mockValidRequest,
@@ -62,7 +62,7 @@ describe('getTrialSessionsForJudgeActivityReportInteractor', () => {
     expect(result).toEqual({
       Hybrid: 0,
       'Motion/Hearing': 0,
-      Regular: 0,
+      Regular: 1,
       Small: 0,
       Special: 0,
     });
