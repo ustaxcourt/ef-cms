13917a7618849c4d4f4e80af5d691683b23f54de
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2023-04-13 09:57:08 -0600

    9809 adding tests for new action

--- web-client/src/presenter/actions/JudgeActivityReport/setJudgeLastNameOnFormAction.test.ts
+++ web-client/src/presenter/actions/JudgeActivityReport/setJudgeLastNameOnFormAction.test.ts
@@ -1,5 +1,8 @@
-import { CASE_STATUS_TYPES } from '../../../../../shared/src/business/entities/EntityConstants';
 import { applicationContextForClient as applicationContext } from '../../../../../shared/src/business/test/createTestApplicationContext';
+import {
+  chambersUser,
+  judgeUser,
+} from '../../../../../shared/src/test/mockUsers';
 import { presenter } from '../../presenter-mock';
 import { runAction } from 'cerebral/test';
 import { setJudgeLastNameOnFormAction } from './setJudgeLastNameOnFormAction';
@@ -9,6 +12,7 @@ describe('setJudgeLastNameOnFormAction', () => {
 
   it('should set state.form.judgeName to the last name of the current user when they are a judge', async () => {
     applicationContext.getCurrentUser.mockReturnValue(judgeUser);
+
     const { state } = await runAction(setJudgeLastNameOnFormAction, {
       modules: {
         presenter,
@@ -20,12 +24,12 @@ describe('setJudgeLastNameOnFormAction', () => {
       },
     });
 
-    expect(state.judgeActivityReportData.casesClosedByJudge).toBe(
-      mockCasesClosedByJudge,
-    );
+    expect(state.form.judgeName).toBe(judgeUser.name);
   });
 
   it('should set state.form.judgeName to the last name of the judge of the chambers when the current user is a chambers user', async () => {
+    applicationContext.getCurrentUser.mockReturnValue(chambersUser);
+
     const { state } = await runAction(setJudgeLastNameOnFormAction, {
       modules: {
         presenter,
@@ -37,8 +41,6 @@ describe('setJudgeLastNameOnFormAction', () => {
       },
     });
 
-    expect(state.judgeActivityReportData.casesClosedByJudge).toBe(
-      mockCasesClosedByJudge,
-    );
+    expect(state.form.judgeName).toBe('Colvin');
   });
 });
