298d8589680a0c9b5b088f0b23c14c4a645cdffc
Rosie Highsmith <ahighsmith@flexion.us>
2023-04-14 11:31:44 -0700

    9809: lint error fix

--- web-api/src/getUseCases.ts
+++ web-api/src/getUseCases.ts
@@ -654,7 +654,6 @@ const useCases = {
   getCalendaredCasesForTrialSessionInteractor,
   getCaseDeadlinesForCaseInteractor,
   getCaseDeadlinesInteractor,
-  getCasesClosedByJudgeInteractor,
   getCaseExistsInteractor,
   getCaseForPublicDocketSearchInteractor,
   getCaseInteractor,
