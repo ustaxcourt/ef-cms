80a94c7f4c5a416b5b6ee639ad1deadba4a37a2e
Absolutestunna <joshujah007@gmail.com>
2023-04-14 18:10:12 -0400

    9723: corrected test for validateCustomCaseInventorySearchFiltersInteractor

--- shared/src/business/entities/CustomCaseInventorySearch/customCaseInventorySearch.test.ts
+++ shared/src/business/entities/CustomCaseInventorySearch/customCaseInventorySearch.test.ts
@@ -1,77 +1,78 @@
-import { CustomCaseInventorySearch } from './customCaseInventorySearch';
+import { CustomCaseInventorySearch } from './CustomCaseInventorySearch';
 import {
   FORMATS,
   calculateISODate,
   formatDateString,
 } from '../../utilities/DateHandler';
 
-describe('JudgeActivityReportSearch', () => {
+describe('CustomCaseInventorySearch', () => {
   const mockFutureDate = formatDateString(
     calculateISODate({ howMuch: 5, units: 'days' }),
     FORMATS.YYYYMMDD,
   );
 
   it('should have validation errors when start date is not provided', () => {
-    const judgeActivityReportSearchEntity = new CustomCaseInventorySearch({
+    const customCaseInventorySearch = new CustomCaseInventorySearch({
       endDate: '2000/01/01',
       startDate: undefined,
     });
 
     expect(
-      judgeActivityReportSearchEntity.getFormattedValidationErrors(),
+      customCaseInventorySearch.getFormattedValidationErrors(),
     ).toMatchObject({
-      startDate: 'Enter a start date.',
+      startDate: 'Enter a valid start date.',
     });
   });
 
   it('should have validation errors when end date is not provided', () => {
-    const judgeActivityReportSearchEntity = new CustomCaseInventorySearch({
+    const customCaseInventorySearch = new CustomCaseInventorySearch({
       endDate: undefined,
       startDate: '2025-02-01',
     });
 
     expect(
-      judgeActivityReportSearchEntity.getFormattedValidationErrors(),
+      customCaseInventorySearch.getFormattedValidationErrors(),
     ).toMatchObject({
-      endDate: 'Enter an end date.',
+      endDate: 'Enter a valid end date.',
     });
   });
 
   it('should have validation errors when the end date provided is chronologically before the start date', () => {
-    const judgeActivityReportSearchEntity = new CustomCaseInventorySearch({
+    const customCaseInventorySearch = new CustomCaseInventorySearch({
       endDate: '2021-01-01',
       startDate: '2022-02-01',
     });
 
     expect(
-      judgeActivityReportSearchEntity.getFormattedValidationErrors(),
+      customCaseInventorySearch.getFormattedValidationErrors(),
     ).toMatchObject({
       endDate:
-        'End date cannot be prior to Start Date. Enter a valid End date.',
+        'End date cannot be prior to start date. Enter a valid end date.',
     });
   });
 
   it('should have validation errors when the start date provided is in the future', () => {
-    const judgeActivityReportSearchEntity = new CustomCaseInventorySearch({
+    const customCaseInventorySearch = new CustomCaseInventorySearch({
       endDate: mockFutureDate,
       startDate: mockFutureDate,
     });
 
     expect(
-      judgeActivityReportSearchEntity.getFormattedValidationErrors(),
+      customCaseInventorySearch.getFormattedValidationErrors(),
     ).toMatchObject({
       startDate: 'Start date cannot be in the future. Enter a valid date.',
     });
   });
 
-  it('should have validation errors when the end date provided is in the future', () => {
-    const judgeActivityReportSearchEntity = new CustomCaseInventorySearch({
+  // TODO: Decide if we need to restrict end date to future dates
+  it.skip('should have validation errors when the end date provided is in the future', () => {
+    const customCaseInventorySearch = new CustomCaseInventorySearch({
       endDate: mockFutureDate,
       startDate: '2020-03-01',
     });
 
     expect(
-      judgeActivityReportSearchEntity.getFormattedValidationErrors(),
+      customCaseInventorySearch.getFormattedValidationErrors(),
     ).toMatchObject({
       endDate: 'End date cannot be in the future. Enter a valid date.',
     });
