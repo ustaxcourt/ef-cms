24b6ec9fa76e42f76a1c9d6c3b79a46ce8454c5f
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-04-17 15:06:43 -0700

    9809: Add test to cover providing date range filter to retrieving opinion and orders for judge activity report

--- shared/src/business/useCases/judgeActivityReport/getOrdersFiledByJudgeInteractor.test.ts
+++ shared/src/business/useCases/judgeActivityReport/getOrdersFiledByJudgeInteractor.test.ts
@@ -61,6 +61,14 @@ describe('getOrdersFiledByJudgeInteractor', () => {
       mockValidRequest,
     );
 
+    expect(
+      applicationContext.getPersistenceGateway().advancedDocumentSearch.mock
+        .calls[0][0],
+    ).toMatchObject({
+      endDate: '2020-03-22T03:59:59.999Z',
+      judge: mockValidRequest.judgeName,
+      startDate: '2020-02-12T05:00:00.000Z',
+    });
     expect(result).toEqual([
       { count: 2, documentType: 'Order', eventCode: 'O' },
       {
