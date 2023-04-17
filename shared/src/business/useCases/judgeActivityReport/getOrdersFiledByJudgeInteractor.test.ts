d7cb513967e8b41927fa7db5e5484924e7979157
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-04-17 09:33:17 -0700

    9809: Add endpoint for getting orders issued for judge activity report

---
+++ shared/src/business/useCases/judgeActivityReport/getOrdersFiledByJudgeInteractor.test.ts
@@ -0,0 +1,33 @@
+import { applicationContext } from '../../test/createTestApplicationContext';
+import { getOrdersFiledByJudgeInteractor } from './getOrdersFiledByJudgeInteractor';
+import { judgeUser, petitionsClerkUser } from '../../../test/mockUsers';
+
+describe('getOrdersFiledByJudgeInteractor', () => {
+  const mockValidRequest = {
+    endDate: '03/21/2020',
+    judgeName: judgeUser.name,
+    startDate: '02/12/2020',
+  };
+
+  beforeEach(() => {
+    applicationContext.getCurrentUser.mockReturnValue(judgeUser);
+  });
+
+  it('should return an error when the user is not authorized to generate the report', async () => {
+    applicationContext.getCurrentUser.mockReturnValue(petitionsClerkUser);
+
+    await expect(
+      getOrdersFiledByJudgeInteractor(applicationContext, mockValidRequest),
+    ).rejects.toThrow('Unauthorized');
+  });
+
+  it('should return an error when the search parameters are not valid', async () => {
+    await expect(
+      getOrdersFiledByJudgeInteractor(applicationContext, {
+        endDate: 'baddabingbaddaboom',
+        judgeName: judgeUser.name,
+        startDate: 'yabbadabbadoo',
+      }),
+    ).rejects.toThrow();
+  });
+});
