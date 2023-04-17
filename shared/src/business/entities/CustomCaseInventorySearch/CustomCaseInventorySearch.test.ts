568aea83599c2419922fc530c62a61316d3f9bf1
Absolutestunna <joshujah007@gmail.com>
2023-04-17 13:02:15 -0400

    9723 - WIP another file rename

--- shared/src/business/entities/CustomCaseInventorySearch/CustomCaseInventorySearch.test.ts
+++ shared/src/business/entities/CustomCaseInventorySearch/CustomCaseInventorySearch.test.ts
@@ -1,46 +1,44 @@
 import { CustomCaseInventorySearch } from './CustomCaseInventorySearch';
 import {
-  FORMATS,
   calculateISODate,
-  formatDateString,
+  createISODateString,
 } from '../../utilities/DateHandler';
 
 describe('CustomCaseInventorySearch', () => {
-  const mockFutureDate = formatDateString(
-    calculateISODate({ howMuch: 5, units: 'days' }),
-    FORMATS.YYYYMMDD,
-  );
+  const mockFutureDate = calculateISODate({ howMuch: 5, units: 'days' });
+  const mockPastDate = calculateISODate({ howMuch: -5, units: 'days' });
+  const today = createISODateString();
 
   it('should have validation errors when start date is not provided', () => {
     const customCaseInventorySearch = new CustomCaseInventorySearch({
-      endDate: '2000/01/01',
+      endDate: today,
       startDate: undefined,
     });
 
     expect(
       customCaseInventorySearch.getFormattedValidationErrors(),
     ).toMatchObject({
-      startDate: 'Enter a valid start date.',
+      startDate: 'Enter a start date.',
     });
   });
 
   it('should have validation errors when end date is not provided', () => {
     const customCaseInventorySearch = new CustomCaseInventorySearch({
       endDate: undefined,
-      startDate: '2025-02-01',
+      startDate: today,
     });
 
     expect(
       customCaseInventorySearch.getFormattedValidationErrors(),
     ).toMatchObject({
-      endDate: 'Enter a valid end date.',
+      endDate: 'Enter an end date.',
     });
   });
 
   it('should have validation errors when the end date provided is chronologically before the start date', () => {
     const customCaseInventorySearch = new CustomCaseInventorySearch({
-      endDate: '2021-01-01',
-      startDate: '2022-02-01',
+      endDate: mockPastDate,
+      startDate: today,
     });
 
     expect(
@@ -53,7 +51,7 @@ describe('CustomCaseInventorySearch', () => {
 
   it('should have validation errors when the start date provided is in the future', () => {
     const customCaseInventorySearch = new CustomCaseInventorySearch({
-      endDate: mockFutureDate,
+      endDate: undefined,
       startDate: mockFutureDate,
     });
 
