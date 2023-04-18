3b93f872a8c23e3c7843af110cd2d38d5b79b02b
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-04-18 08:18:45 -0700

    9809: WIP adding helper properties to determine which message to show

--- web-client/src/presenter/computeds/JudgeActivityReport/judgeActivityReportHelper.ts
+++ web-client/src/presenter/computeds/JudgeActivityReport/judgeActivityReportHelper.ts
@@ -5,29 +5,38 @@ export const judgeActivityReportHelper = get => {
     state.judgeActivityReportData,
   );
 
-  const closedCasesTotal: number = Object.values(
-    casesClosedByJudge || {},
-  ).reduce((a: number, b: number) => a + b, 0);
+  // if casesClosedByJudge, opinions, orders, trialSessions, haven't submitted form
+  // if casesClosedByJudge, opinions, orders, trialSessions += 0, no results
+  let closedCasesTotal,
+    trialSessionsHeldTotal,
+    opinionsFiledTotal,
+    ordersFiledTotal;
+  const hasFormBeenSubmitted =
+    casesClosedByJudge && opinions && orders && trialSessions;
 
-  const trialSessionsHeldTotal: number = Object.values(
-    trialSessions || {},
-  ).reduce((a: number, b: number) => a + b, 0);
+  if (hasFormBeenSubmitted) {
+    closedCasesTotal = Object.values(casesClosedByJudge).reduce(
+      (a: number, b: number) => a + b,
+      0,
+    );
 
-  const opinionsFiledTotal: number = (opinions || []).reduce(
-    (a: any, b: any) => a + b.count,
-    0,
-  );
+    trialSessionsHeldTotal = Object.values(trialSessions).reduce(
+      (a: number, b: number) => a + b,
+      0,
+    );
 
-  const ordersFiledTotal: number = (orders || []).reduce(
-    (a: any, b: any) => a + b.count,
-    0,
-  );
+    opinionsFiledTotal = opinions.reduce((a: any, b: any) => a + b.count, 0);
+
+    ordersFiledTotal = orders.reduce((a: any, b: any) => a + b.count, 0);
+  } else {
+  }
 
   const resultsCount =
     ordersFiledTotal +
     opinionsFiledTotal +
     trialSessionsHeldTotal +
     closedCasesTotal;
+
   const showResults = resultsCount > 0;
 
   return {
