29b47c8d9e4d08034e675a8786254a432617acb2
Zachary Rogers <zrogers>
2023-04-20 12:39:10 -0700

    convert lambda to es6

--- web-api/src/messages/replyToMessageLambda.ts
+++ web-api/src/messages/replyToMessageLambda.ts
@@ -1,4 +1,4 @@
-const { genericHandler } = require('../genericHandler');
+import { genericHandler } from '../genericHandler';
 
 /**
  * lambda which is used to reply to a message
@@ -6,7 +6,7 @@ const { genericHandler } = require('../genericHandler');
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-exports.replyToMessageLambda = event =>
+export const replyToMessageLambda = event =>
   genericHandler(event, async ({ applicationContext }) => {
     return await applicationContext
       .getUseCases()
