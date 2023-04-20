b69d53bd5b869d4fd1c3a71a16cf714a8a6653e9
Zachary Rogers <zrogers>
2023-04-20 08:46:41 -0700

    9994: Remove case param during migration of work Item

--- web-api/workflow-terraform/migration/main/lambdas/migrations/0009-add-trial-location-field-to-work-item.ts
+++ web-api/workflow-terraform/migration/main/lambdas/migrations/0009-add-trial-location-field-to-work-item.ts
@@ -37,7 +37,6 @@ export const migrateItems = async (items, documentClient) => {
         {
           applicationContext,
         },
-        fullCase,
       ).validateWithLogging(applicationContext);
 
       item.trialLocation = theWorkItem.trialLocation;
