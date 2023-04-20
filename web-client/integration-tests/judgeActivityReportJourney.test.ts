510404f6ec0f302ffde4ac2c27340b475975ac1f
Rosie Highsmith <ahighsmith@flexion.us>
2023-04-20 15:14:57 -0700

    9809: finished integration test

--- web-client/integration-tests/judgeActivityReportJourney.test.ts
+++ web-client/integration-tests/judgeActivityReportJourney.test.ts
@@ -9,10 +9,6 @@ describe('Judge activity report journey', () => {
   afterAll(() => {
     cerebralTest.closeSocket();
   });
-  //go to activity report
-  //submit form with invalid dates, error message verification
-  //add start date and end date
-  //verify resuls
 
   loginAs(cerebralTest, 'judgecolvin@example.com');
   it('should disable the submit button on inital page load when form has not yet been completed', async () => {
@@ -22,11 +18,15 @@ describe('Judge activity report journey', () => {
       judgeActivityReportHelperComputed,
     );
 
-    const { isFormPristine } = runCompute(judgeActivityReportHelper, {
-      state: cerebralTest.getState(),
-    });
+    const { isFormPristine, reportHeader } = runCompute(
+      judgeActivityReportHelper,
+      {
+        state: cerebralTest.getState(),
+      },
+    );
 
     expect(isFormPristine).toBe(true);
+    expect(reportHeader).toContain('Colvin');
   });
 
   it('should display an error message when invalid dates are entered into the form', async () => {
@@ -45,6 +45,58 @@ describe('Judge activity report journey', () => {
     expect(cerebralTest.getState('validationErrors')).toEqual({
       endDate: 'Enter a valid end date.',
       startDate: 'Enter a valid start date.',
+    });
+  });
+
+  it('should submit the form with valid dates and display judge activity report results', async () => {
+    await cerebralTest.runSequence('updateFormValueSequence', {
+      key: 'startDate',
+      value: '01/01/2020',
+    });
+
+    await cerebralTest.runSequence('updateFormValueSequence', {
+      key: 'endDate',
+      value: '04/01/2023',
+    });
+
+    await cerebralTest.runSequence('submitJudgeActivityReportSequence');
+
+    expect(cerebralTest.getState('validationErrors')).toEqual({});
+    expect(cerebralTest.getState('judgeActivityReportData')).toEqual({
+      casesClosedByJudge: {
+        Closed: 0,
+        'Closed - Dismissed': 0,
+      },
+      opinions: [
+        {
+          count: 0,
+          documentType: 'Memorandum Opinion',
+          eventCode: 'MOP',
+        },
+        {
+          count: 0,
+          documentType: 'Order of Service of Transcript (Bench Opinion)',
+          eventCode: 'OST',
+        },
+        {
+          count: 0,
+          documentType: 'Summary Opinion',
+          eventCode: 'SOP',
+        },
+        {
+          count: 1,
+          documentType: 'T.C. Opinion',
+          eventCode: 'TCOP',
+        },
+      ],
+      orders: [],
+      trialSessions: {
+        Hybrid: 1,
+        'Motion/Hearing': 0,
+        Regular: 0.5,
+        Small: 0,
+        Special: 1,
+      },
     });
   });
 });
