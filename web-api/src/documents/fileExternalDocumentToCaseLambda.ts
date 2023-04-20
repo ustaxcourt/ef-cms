bb65e4827fbcf7b445cb5f09d11ae561f9631004
Zachary Rogers <zrogers>
2023-04-20 12:31:00 -0700

    convert more lamdbas to es6

--- web-api/src/documents/fileExternalDocumentToCaseLambda.ts
+++ web-api/src/documents/fileExternalDocumentToCaseLambda.ts
@@ -1,4 +1,4 @@
-const { genericHandler } = require('../genericHandler');
+import { genericHandler } from '../genericHandler';
 
 /**
  * lambda which is used for adding an external document to a case
@@ -6,7 +6,7 @@ const { genericHandler } = require('../genericHandler');
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-exports.fileExternalDocumentToCaseLambda = event =>
+export const fileExternalDocumentToCaseLambda = event =>
   genericHandler(event, async ({ applicationContext }) => {
     return await applicationContext
       .getUseCases()
