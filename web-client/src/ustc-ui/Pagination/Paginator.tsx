2d4ca15cabc5ea1e6d8fcfb8407188ed45e1381f
Zachary Rogers <zrogers>
2023-04-24 10:06:01 -0700

    9723: Use ReactPaginate for paginator component

--- web-client/src/ustc-ui/Pagination/Paginator.tsx
+++ web-client/src/ustc-ui/Pagination/Paginator.tsx
@@ -1,258 +1,38 @@
-import React, { useState } from 'react';
-import classNames from 'classnames';
+import React from 'react';
+import ReactPaginate from 'react-paginate';
 
-const numberOfPaginatorSlots = 7;
-const PageButton = (props: {
-  pageNumber: number;
-  selected: boolean;
-  onClick: (selectedPage: number) => void;
-}) => {
-  return (
-    <>
-      <li className="usa-pagination__item usa-pagination__page-no">
-        <button
-          aria-label="Page 1"
-          className={classNames(
-            'usa-pagination__button',
-            'background-none',
-            props.selected && 'usa-current',
-          )}
-          onClick={() => props.onClick(props.pageNumber)}
-        >
-          {props.pageNumber + 1}
-        </button>
-      </li>
-    </>
-  );
-};
-
-const PreviousPage = (props: { onPreviousClick: Function }) => {
-  return (
-    <>
-      <li className="usa-pagination__item usa-pagination__arrow">
-        <button
-          aria-label="Previous page"
-          className="usa-pagination__link usa-pagination__previous-page background-none border-none"
-          onClick={() => {
-            props.onPreviousClick();
-          }}
-        >
-          <svg aria-hidden="true" className="usa-icon" role="img">
-            <use xlinkHref="/assets/img/sprite.svg#navigate_before"></use>
-          </svg>
-          <span className="usa-pagination__link-text">Previous</span>
-        </button>
-      </li>
-    </>
-  );
-};
-
-const NextPage = (props: { onNextClick: Function }) => {
-  return (
-    <>
-      <li className="usa-pagination__item usa-pagination__arrow">
-        <button
-          aria-label="Next page"
-          className="usa-pagination__link usa-pagination__next-page background-none border-none"
-          onClick={() => {
-            props.onNextClick();
-          }}
-        >
-          <svg aria-hidden="true" className="usa-icon" role="img">
-            <use xlinkHref="/assets/img/sprite.svg#navigate_before"></use>
-          </svg>
-          <span className="usa-pagination__link-text">Next</span>
-        </button>
-      </li>
-    </>
-  );
-};
-
-const PageEllipsis = () => {
-  return (
-    <>
-      <li
-        className="usa-pagination__item usa-pagination__overflow"
-        role="presentation"
-      >
-        <span>…</span>
-      </li>
-    </>
-  );
-};
-
-// eslint-disable-next-line jsdoc/require-jsdoc
-function getSlotComponent({
-  currentPageNumber,
+export const Paginator = ({
   onPageChange,
-  setPageNumber,
-  slotNumber,
-  totalPages,
+  pageCount,
+  pageRangeDisplayed,
 }: {
-  currentPageNumber: number;
-  onPageChange: (selectedPage: number) => any;
-  slotNumber: number;
-  totalPages: number;
-  setPageNumber: (selectedPage: number) => any;
-}) {
-  const isHidingPreviousOptions =
-    currentPageNumber > 3 && totalPages > numberOfPaginatorSlots;
-  const isHidingFutureOptions =
-    totalPages - currentPageNumber > 4 && totalPages > numberOfPaginatorSlots;
-  if (slotNumber === 0) {
-    return (
-      <PageButton
-        pageNumber={0}
-        selected={currentPageNumber === 0}
-        onClick={selectedPage => {
-          setPageNumber(selectedPage);
-          onPageChange(selectedPage);
-        }}
-      />
-    );
-  }
-  if (slotNumber === 1) {
-    if (isHidingPreviousOptions) {
-      return <PageEllipsis />;
-    } else {
-      return (
-        <PageButton
-          pageNumber={1}
-          selected={currentPageNumber === 1}
-          onClick={selectedPage => {
-            setPageNumber(selectedPage);
-            onPageChange(selectedPage);
-          }}
-        />
-      );
-    }
-  }
-  if (slotNumber === 2 || slotNumber === 3 || slotNumber === 4) {
-    if (!isHidingPreviousOptions) {
-      return (
-        <PageButton
-          pageNumber={slotNumber}
-          selected={currentPageNumber === slotNumber}
-          onClick={selectedPage => {
-            setPageNumber(selectedPage);
-            onPageChange(selectedPage);
-          }}
-        />
-      );
-    }
-    if (!isHidingFutureOptions) {
-      return (
-        <PageButton
-          pageNumber={totalPages - numberOfPaginatorSlots + slotNumber}
-          selected={
-            currentPageNumber ===
-            totalPages - numberOfPaginatorSlots + slotNumber
-          }
-          onClick={selectedPage => {
-            setPageNumber(selectedPage);
-            onPageChange(selectedPage);
-          }}
-        />
-      );
-    }
-    return (
-      <PageButton
-        pageNumber={currentPageNumber + slotNumber - 3}
-        selected={currentPageNumber === currentPageNumber + slotNumber - 3}
-        onClick={selectedPage => {
-          setPageNumber(selectedPage);
-          onPageChange(selectedPage);
-        }}
-      />
-    );
-  }
-  if (slotNumber === 5) {
-    if (isHidingFutureOptions) {
-      return <PageEllipsis />;
-    } else {
-      const subtractor = totalPages >= 7 ? 2 : 1;
-      return (
-        <PageButton
-          pageNumber={totalPages - subtractor}
-          selected={currentPageNumber === totalPages - subtractor}
-          onClick={selectedPage => {
-            setPageNumber(selectedPage);
-            onPageChange(selectedPage);
-          }}
-        />
-      );
-    }
-  }
-  if (slotNumber === 6) {
-    return (
-      <PageButton
-        pageNumber={totalPages - 1}
-        selected={currentPageNumber === totalPages - 1}
-        onClick={selectedPage => {
-          setPageNumber(selectedPage);
-          onPageChange(selectedPage);
-        }}
-      />
-    );
-  }
-}
-
-/*
-This component is based off of USWDS implementation of a paginator: https://designsystem.digital.gov/components/pagination/
-The totalPages and selected page work similarly to counting arrays. TotalPages is similar to array.length and selectedPage is 0 based indexing.
-totalPages could be 20 but the maximum value selectedPage could be is 19 and the lowest pages is 0.
-*/
-
-export const Paginator = (props: {
-  totalPages: number;
-  onPageChange: (selectedPage: number) => any;
+  pageCount: number;
+  pageRangeDisplayed: number;
+  onPageChange: (selectedItem: { selected: number }) => void;
 }) => {
-  const [currentPageNumber, setPageNumber] = useState(0);
-  const sevenDisplayedSlots = [];
-  console.log('selected page number: ', currentPageNumber);
-  // 1. Should it render the slot at all?
-  // 2. Should it render a page button or an ellipse?
-  // 3. Should it render The slot number it is or should it add some extras?
-
-  for (let slotNumber = 0; slotNumber < numberOfPaginatorSlots; slotNumber++) {
-    if (slotNumber >= props.totalPages) {
-      continue;
-    }
-    const slotComponent = getSlotComponent({
-      currentPageNumber,
-      onPageChange: props.onPageChange,
-      setPageNumber,
-      slotNumber,
-      totalPages: props.totalPages,
-    });
-    sevenDisplayedSlots.push(slotComponent);
-  }
-
   return (
     <>
       <nav aria-label="Pagination" className="usa-pagination margin-bottom-0">
-        <ul className="usa-pagination__list">
-          {currentPageNumber !== 0 && (
-            <PreviousPage
-              onPreviousClick={() => {
-                setPageNumber(currentPageNumber - 1);
-                props.onPageChange(currentPageNumber - 1);
-              }}
-            />
-          )}
-          {sevenDisplayedSlots}
-          {currentPageNumber < props.totalPages - 1 && (
-            <NextPage
-              onNextClick={() => {
-                setPageNumber(currentPageNumber + 1);
-                props.onPageChange(currentPageNumber + 1);
-              }}
-            />
-          )}
-        </ul>
+        <ReactPaginate
+          activeLinkClassName="usa-current"
+          breakClassName="usa-pagination__item"
+          breakLinkClassName="usa-pagination__button cursor-pointer border-none"
+          className="usa-pagination__list"
+          disabledClassName="hide"
+          nextClassName="usa-pagination__item usa-pagination__arrow"
+          nextLinkClassName="usa-pagination__link usa-pagination__next-page cursor-pointer"
+          pageClassName="usa-pagination__item usa-pagination__page-no"
+          pageCount={pageCount}
+          pageLinkClassName="usa-pagination__button cursor-pointer"
+          pageRangeDisplayed={pageRangeDisplayed}
+          previousClassName="usa-pagination__item usa-pagination__arrow"
+          previousLinkClassName="usa-pagination__link usa-pagination__previous-page cursor-pointer"
+          renderOnZeroPageCount={null}
+          onPageChange={onPageChange}
+        />
       </nav>
     </>
   );
 };
 
-Paginator.displayName = 'Paginator';
+Paginator.displayName = 'PaginationerComponent';
