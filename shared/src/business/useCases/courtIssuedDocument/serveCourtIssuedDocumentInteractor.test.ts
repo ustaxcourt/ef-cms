fdc6ab16634c8cd1215dab24bc265c3cc5e1e9e5
Michael Marcotte <mike.marcotte@gmail.com>
2023-04-24 15:09:41 -0400

    9739: add async lock handling to serveCourtIssuedDocumentInteractor

--- shared/src/business/useCases/courtIssuedDocument/serveCourtIssuedDocumentInteractor.test.ts
+++ shared/src/business/useCases/courtIssuedDocument/serveCourtIssuedDocumentInteractor.test.ts
@@ -3,11 +3,8 @@ import { MOCK_CASE } from '../../../test/mockCase';
 import { MOCK_DOCUMENTS } from '../../../test/mockDocuments';
 import { MOCK_TRIAL_REGULAR } from '../../../test/mockTrial';
 import { applicationContext } from '../../test/createTestApplicationContext';
-import {
-  determineEntitiesToLock,
-  serveCourtIssuedDocumentInteractor,
-} from './serveCourtIssuedDocumentInteractor';
 import { docketClerkUser, judgeUser } from '../../../test/mockUsers';
+import { serveCourtIssuedDocumentInteractor } from './serveCourtIssuedDocumentInteractor';
 import { testPdfDoc } from '../../test/getFakeFile';
 
 describe('serveCourtIssuedDocumentInteractor', () => {
@@ -371,29 +368,5 @@ describe('serveCourtIssuedDocumentInteractor', () => {
       docketNumber: MOCK_CASE.docketNumber,
       status: false,
     });
-  });
-});
-
-describe('determineEntitiesToLock', () => {
-  let mockParams;
-  beforeEach(() => {
-    mockParams = {
-      applicationContext,
-      docketNumbers: [],
-      subjectCaseDocketNumber: MOCK_CASE.docketNumber,
-    };
-  });
-  it('should return an object that includes the prefix case', () => {
-    expect(determineEntitiesToLock(mockParams).prefix).toBe('case');
-  });
-  it('should return an object that includes the subjectCaseDocketNumber in the identifiers', () => {
-    mockParams.subjectCaseDocketNumber = '123-20';
-    expect(determineEntitiesToLock(mockParams).identifier).toContain('123-20');
-  });
-  it('should return an object that includes all of the docketNumbers specified in the identifiers', () => {
-    mockParams.docketNumbers = ['111-20', '222-20', '333-20'];
-    expect(determineEntitiesToLock(mockParams).identifier).toContain('111-20');
-    expect(determineEntitiesToLock(mockParams).identifier).toContain('222-20');
-    expect(determineEntitiesToLock(mockParams).identifier).toContain('333-20');
   });
 });
