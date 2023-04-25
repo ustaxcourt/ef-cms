8bcd5d833be287d4552d087bc40324a2b0affe89
Michael Marcotte <mike.marcotte@gmail.com>
2023-04-25 16:29:27 -0400

    devex-1139: add script to ready an empty cluster for a migration rather than having to delete and start over

---
+++ scripts/ready-cluster-for-migration.ts
@@ -0,0 +1,51 @@
+import { ES } from 'aws-sdk';
+import { elasticsearchIndexes } from '../web-api/elasticsearch/elasticsearch-indexes';
+import { getClient } from '../web-api/elasticsearch/client';
+
+if (!process.argv[2]) {
+  throw new Error('Please provide an Opensearch Domain to check');
+}
+
+(async () => {
+  // check the domain
+  const es: ES = new ES();
+  const DomainName: string = process.argv[2];
+  const [, , ENV, VERSION]: string[] = DomainName.split('-');
+
+  try {
+    await es
+      .describeElasticsearchDomain({
+        DomainName,
+      })
+      .promise();
+  } catch (err: any) {
+    if (err.code === 'ResourceNotFoundException') {
+      console.log('cluster does not exist');
+      return;
+    }
+    throw err;
+  }
+
+  const client = await getClient({ environmentName: ENV, version: VERSION });
+  const res = await client.count({
+    body: {
+      query: {
+        match_all: {},
+      },
+    },
+  });
+
+  // get the count for the domain
+  if (res.body.count > 0) {
+    console.log('cluster is NOT empty, exiting with status code 1');
+    process.exit(1);
+  }
+
+  // if the cluster is empty, just delete the indices as they will be recreated soon
+  // with latest and greatest mappings
+  await Promise.all(
+    elasticsearchIndexes.map(index => {
+      client.indices.delete({ body: {}, index });
+    }),
+  );
+})();
