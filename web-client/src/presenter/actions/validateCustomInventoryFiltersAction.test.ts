61480edf36843f2a1264fcd175a4221e18c05eee
Jason Fu <jfu@flexion.us>
2023-04-25 11:00:01 -0700

    9723: improve tests; delete comments; add formatting before validation

--- web-client/src/presenter/actions/validateCustomInventoryFiltersAction.test.ts
+++ web-client/src/presenter/actions/validateCustomInventoryFiltersAction.test.ts
@@ -1,3 +1,7 @@
+import {
+  CustomCaseInventoryReportState,
+  initialCustomCaseInventoryReportState,
+} from '../customCaseInventoryReportState';
 import { applicationContextForClient as applicationContext } from '../../../../shared/src/business/test/createTestApplicationContext';
 import { presenter } from '../presenter-mock';
 import { runAction } from 'cerebral/test';
@@ -13,45 +17,36 @@ describe('validateCustomInventoryFiltersAction', () => {
     error: mockErrorPath,
     success: mockSuccessPath,
   };
+  let customCaseInventoryState: CustomCaseInventoryReportState;
+  beforeEach(() => {
+    customCaseInventoryState = initialCustomCaseInventoryReportState;
+  });
 
   it('should call the success path when the date range is valid', async () => {
-    applicationContext
-      .getUseCases()
-      .validateCustomCaseInventorySearchFiltersInteractor.mockReturnValue(
-        undefined,
-      );
+    customCaseInventoryState.filters.createStartDate = '01/01/1990';
+    customCaseInventoryState.filters.createEndDate = '01/01/2000';
 
     await runAction(validateCustomInventoryFiltersAction, {
       modules: {
         presenter,
       },
       state: {
-        customCaseInventoryFilters: {
-          createEndDate: '',
-          createStartDate: '',
-        },
+        customCaseInventory: customCaseInventoryState,
       },
     });
 
     expect(mockSuccessPath).toHaveBeenCalled();
   });
 
-  it('should call the error path when the date range is NOT valid', async () => {
-    applicationContext
-      .getUseCases()
-      .validateCustomCaseInventorySearchFiltersInteractor.mockReturnValue({
-        someProperty: 'Some Property is required.',
-      });
-
+  it('should call the error path when the date range is not in the correct format', async () => {
+    customCaseInventoryState.filters.createStartDate = 'blooh blah';
+    customCaseInventoryState.filters.createEndDate = '01/01/2000';
     await runAction(validateCustomInventoryFiltersAction, {
       modules: {
         presenter,
       },
       state: {
-        customCaseInventoryFilters: {
-          createEndDate: 'gobbeltygook',
-          createStartDate: undefined,
-        },
+        customCaseInventory: customCaseInventoryState,
       },
     });
 
