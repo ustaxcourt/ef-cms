b10ef3eff07fc8b718b90e2a7087b306868662d1
Richard Goshen <rick.goshen@gmail.com>
2023-04-26 11:37:34 -0700

    9598: remove empty line in closeFileUploadStatusModalAction

--- web-client/src/presenter/actions/closeFileUploadStatusModalAction.ts
+++ web-client/src/presenter/actions/closeFileUploadStatusModalAction.ts
@@ -9,7 +9,6 @@ export const closeFileUploadStatusModalAction = async ({ store }) => {
   store.set(state.fileUploadProgress.percentComplete, 100);
   store.set(state.fileUploadProgress.timeRemaining, 0);
   store.set(state.fileUploadProgress.isUploading, false);
-
   await new Promise(resolve => {
     setTimeout(resolve, process.env.FILE_UPLOAD_MODAL_TIMEOUT || 3000);
   });
