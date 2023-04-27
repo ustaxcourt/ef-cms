369886583d94fed53e640903e7b206c2367ac43c
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-04-27 13:33:12 -0700

    9809: Fixing tests

--- shared/src/business/useCases/judgeActivityReport/validateJudgeActivityReportSearchInteractor.test.ts
+++ shared/src/business/useCases/judgeActivityReport/validateJudgeActivityReportSearchInteractor.test.ts
@@ -1,30 +1,23 @@
-import { applicationContext } from '../../test/createTestApplicationContext';
 import { judgeUser } from '../../../test/mockUsers';
 import { validateJudgeActivityReportSearchInteractor } from './validateJudgeActivityReportSearchInteractor';
 
 describe('validateJudgeActivityReportSearchInteractor', () => {
   it('should return formatted validation errors when the search criteria are invalid', () => {
-    const errors = validateJudgeActivityReportSearchInteractor(
-      applicationContext,
-      {
-        endDate: undefined,
-        judgeName: judgeUser.name,
-        startDate: undefined,
-      },
-    );
+    const errors = validateJudgeActivityReportSearchInteractor({
+      endDate: 'yabbadabaadoooooo',
+      judgeName: judgeUser.name,
+      startDate: 'NOT_A_DATE',
+    });
 
     expect(Object.keys(errors).length).toBeGreaterThan(0);
   });
 
   it('should return null when the search criteria are valid', () => {
-    const result = validateJudgeActivityReportSearchInteractor(
-      applicationContext,
-      {
-        endDate: '06/07/2022',
-        judgeName: judgeUser.name,
-        startDate: '05/02/2022',
-      },
-    );
+    const result = validateJudgeActivityReportSearchInteractor({
+      endDate: '06/07/2022',
+      judgeName: judgeUser.name,
+      startDate: '05/02/2022',
+    });
 
     expect(result).toEqual(null);
   });
