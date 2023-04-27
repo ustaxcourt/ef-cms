b646ef39b95e2efd34f288d4214de97b218820f2
Richard Goshen <rick.goshen@gmail.com>
2023-04-26 22:01:40 -0700

    9598: WIP irsPractitioner not associated with any case in consolidated group request access to case

--- web-client/integration-tests/journey/externalUserRequestAccessToFileAcrossConsolidatedCasesGroup.ts
+++ web-client/integration-tests/journey/externalUserRequestAccessToFileAcrossConsolidatedCasesGroup.ts
@@ -1,156 +1,171 @@
 import { applicationContextForClient as applicationContext } from '../../../shared/src/business/test/createTestApplicationContext';
+import { caseDetailHeaderHelper as caseDetailHeaderComputed } from '../../src/presenter/computeds/caseDetailHeaderHelper';
 import { contactPrimaryFromState } from '../helpers';
+import { runCompute } from 'cerebral/test';
+import { withAppContextDecorator } from '../../src/withAppContext';
+
+const caseDetailHeaderHelper = withAppContextDecorator(
+  caseDetailHeaderComputed,
+);
 
 export const externalUserRequestAccessToFileAcrossConsolidatedCasesGroup = (
   cerebralTest,
-  fakeFile,
-  overrides = {},
+  { docketNumber, fakeFile, overrides = {} },
 ) => {
-  const { OBJECTIONS_OPTIONS_MAP } = applicationContext.getConstants();
+  // const { OBJECTIONS_OPTIONS_MAP } = applicationContext.getConstants();
 
   return it('external user files a document for owned case', async () => {
     await cerebralTest.runSequence('gotoCaseDetailSequence', {
-      docketNumber: cerebralTest.docketNumber,
+      docketNumber,
     });
 
-    const docketEntriesBefore = cerebralTest.getState(
-      'caseDetail.docketEntries',
-    ).length;
+    // const docketEntriesBefore = cerebralTest.getState(
+    //   'caseDetail.docketEntries',
+    // ).length;
 
-    await cerebralTest.runSequence('gotoFileDocumentSequence', {
-      docketNumber: cerebralTest.docketNumber,
-    });
+    // await cerebralTest.runSequence('gotoFileDocumentSequence', {
+    //   docketNumber: cerebralTest.docketNumber,
+    // });
 
-    const documentToSelect = {
-      category: 'Miscellaneous',
-      documentTitle: 'Civil Penalty Approval Form',
-      documentType: 'Civil Penalty Approval Form',
-      eventCode: 'CIVP',
-      scenario: 'Standard',
-    };
-
-    for (const key of Object.keys(documentToSelect)) {
-      await cerebralTest.runSequence(
-        'updateFileDocumentWizardFormValueSequence',
-        {
-          key,
-          value: documentToSelect[key],
-        },
-      );
-    }
-
-    await cerebralTest.runSequence('validateSelectDocumentTypeSequence');
-
-    expect(cerebralTest.getState('validationErrors')).toEqual({});
-
-    await cerebralTest.runSequence('completeDocumentSelectSequence');
-
-    expect(cerebralTest.getState('form.documentType')).toEqual(
-      'Civil Penalty Approval Form',
-    );
-
-    await cerebralTest.runSequence(
-      'updateFileDocumentWizardFormValueSequence',
+    const { showRequestAccessToCaseButton } = runCompute(
+      caseDetailHeaderHelper,
       {
-        key: 'certificateOfService',
-        value: true,
+        state: cerebralTest.getState(),
       },
     );
 
-    await cerebralTest.runSequence(
-      'updateFileDocumentWizardFormValueSequence',
-      {
-        key: 'hasSupportingDocuments',
-        value: false,
-      },
-    );
+    expect(showRequestAccessToCaseButton).toBe(true);
 
-    await cerebralTest.runSequence(
-      'updateFileDocumentWizardFormValueSequence',
-      {
-        key: 'attachments',
-        value: false,
-      },
-    );
-    await cerebralTest.runSequence(
-      'updateFileDocumentWizardFormValueSequence',
-      {
-        key: 'objections',
-        value: OBJECTIONS_OPTIONS_MAP.NO,
-      },
-    );
+    //   const documentToSelect = {
+    //     category: 'Miscellaneous',
+    //     documentTitle: 'Civil Penalty Approval Form',
+    //     documentType: 'Civil Penalty Approval Form',
+    //     eventCode: 'CIVP',
+    //     scenario: 'Standard',
+    //   };
 
-    await cerebralTest.runSequence(
-      'updateFileDocumentWizardFormValueSequence',
-      {
-        key: 'certificateOfServiceMonth',
-        value: '12',
-      },
-    );
-    await cerebralTest.runSequence(
-      'updateFileDocumentWizardFormValueSequence',
-      {
-        key: 'certificateOfServiceDay',
-        value: '12',
-      },
-    );
-    await cerebralTest.runSequence(
-      'updateFileDocumentWizardFormValueSequence',
-      {
-        key: 'certificateOfServiceYear',
-        value: '2000',
-      },
-    );
+    //   for (const key of Object.keys(documentToSelect)) {
+    //     await cerebralTest.runSequence(
+    //       'updateFileDocumentWizardFormValueSequence',
+    //       {
+    //         key,
+    //         value: documentToSelect[key],
+    //       },
+    //     );
+    //   }
 
-    await cerebralTest.runSequence(
-      'updateFileDocumentWizardFormValueSequence',
-      {
-        key: 'primaryDocumentFile',
-        value: fakeFile,
-      },
-    );
+    //   await cerebralTest.runSequence('validateSelectDocumentTypeSequence');
 
-    const contactPrimary = contactPrimaryFromState(cerebralTest);
+    //   expect(cerebralTest.getState('validationErrors')).toEqual({});
 
-    if (overrides.fileAcrossConsolidatedGroup) {
-      await cerebralTest.runSequence(
-        'updateFileDocumentWizardFormValueSequence',
-        {
-          key: 'partyIrsPractitioner',
-          value: true,
-        },
-      );
-      await cerebralTest.runSequence(
-        'updateFileDocumentWizardFormValueSequence',
-        {
-          key: `filersMap.${contactPrimary.contactId}`,
-          value: false,
-        },
-      );
-    } else {
-      await cerebralTest.runSequence(
-        'updateFileDocumentWizardFormValueSequence',
-        {
-          key: `filersMap.${contactPrimary.contactId}`,
-          value: true,
-        },
-      );
-    }
+    //   await cerebralTest.runSequence('completeDocumentSelectSequence');
 
-    await cerebralTest.runSequence('reviewExternalDocumentInformationSequence');
+    //   expect(cerebralTest.getState('form.documentType')).toEqual(
+    //     'Civil Penalty Approval Form',
+    //   );
 
-    expect(cerebralTest.getState('validationErrors')).toEqual({});
+    //   await cerebralTest.runSequence(
+    //     'updateFileDocumentWizardFormValueSequence',
+    //     {
+    //       key: 'certificateOfService',
+    //       value: true,
+    //     },
+    //   );
 
-    if (overrides.fileAcrossConsolidatedGroup) {
-      const form = cerebralTest.getState('form');
+    //   await cerebralTest.runSequence(
+    //     'updateFileDocumentWizardFormValueSequence',
+    //     {
+    //       key: 'hasSupportingDocuments',
+    //       value: false,
+    //     },
+    //   );
 
-      expect(form.fileAcrossConsolidatedGroup).toEqual(true);
-    }
+    //   await cerebralTest.runSequence(
+    //     'updateFileDocumentWizardFormValueSequence',
+    //     {
+    //       key: 'attachments',
+    //       value: false,
+    //     },
+    //   );
+    //   await cerebralTest.runSequence(
+    //     'updateFileDocumentWizardFormValueSequence',
+    //     {
+    //       key: 'objections',
+    //       value: OBJECTIONS_OPTIONS_MAP.NO,
+    //     },
+    //   );
 
-    await cerebralTest.runSequence('submitExternalDocumentSequence');
+    //   await cerebralTest.runSequence(
+    //     'updateFileDocumentWizardFormValueSequence',
+    //     {
+    //       key: 'certificateOfServiceMonth',
+    //       value: '12',
+    //     },
+    //   );
+    //   await cerebralTest.runSequence(
+    //     'updateFileDocumentWizardFormValueSequence',
+    //     {
+    //       key: 'certificateOfServiceDay',
+    //       value: '12',
+    //     },
+    //   );
+    //   await cerebralTest.runSequence(
+    //     'updateFileDocumentWizardFormValueSequence',
+    //     {
+    //       key: 'certificateOfServiceYear',
+    //       value: '2000',
+    //     },
+    //   );
 
-    expect(cerebralTest.getState('caseDetail.docketEntries').length).toEqual(
-      docketEntriesBefore + 1,
-    );
+    //   await cerebralTest.runSequence(
+    //     'updateFileDocumentWizardFormValueSequence',
+    //     {
+    //       key: 'primaryDocumentFile',
+    //       value: fakeFile,
+    //     },
+    //   );
+
+    //   const contactPrimary = contactPrimaryFromState(cerebralTest);
+
+    //   if (overrides.fileAcrossConsolidatedGroup) {
+    //     await cerebralTest.runSequence(
+    //       'updateFileDocumentWizardFormValueSequence',
+    //       {
+    //         key: 'partyIrsPractitioner',
+    //         value: true,
+    //       },
+    //     );
+    //     await cerebralTest.runSequence(
+    //       'updateFileDocumentWizardFormValueSequence',
+    //       {
+    //         key: `filersMap.${contactPrimary.contactId}`,
+    //         value: false,
+    //       },
+    //     );
+    //   } else {
+    //     await cerebralTest.runSequence(
+    //       'updateFileDocumentWizardFormValueSequence',
+    //       {
+    //         key: `filersMap.${contactPrimary.contactId}`,
+    //         value: true,
+    //       },
+    //     );
+    //   }
+
+    //   await cerebralTest.runSequence('reviewExternalDocumentInformationSequence');
+
+    //   expect(cerebralTest.getState('validationErrors')).toEqual({});
+
+    //   if (overrides.fileAcrossConsolidatedGroup) {
+    //     const form = cerebralTest.getState('form');
+
+    //     expect(form.fileAcrossConsolidatedGroup).toEqual(true);
+    //   }
+
+    //   await cerebralTest.runSequence('submitExternalDocumentSequence');
+
+    //   expect(cerebralTest.getState('caseDetail.docketEntries').length).toEqual(
+    //     docketEntriesBefore + 1,
+    //   );
   });
 };
