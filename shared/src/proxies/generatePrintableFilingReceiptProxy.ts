f06a26d7e2f3bdd8fc41b759fb2ba5c5ee21afd0
Timothy Shumway <tshumway@flexion.us>
2023-05-02 09:34:59 -0600

    3898: clean up jsdocs for DOD

--- shared/src/proxies/generatePrintableFilingReceiptProxy.ts
+++ shared/src/proxies/generatePrintableFilingReceiptProxy.ts
@@ -2,11 +2,11 @@ const { post } = require('./requests');
 
 /**
  * generatePrintableFilingReceiptInteractor (proxy)
- *
  * @param {object} applicationContext the application context
  * @param {object} providers the providers object
  * @param {string} providers.docketNumber the docket number of the case in which the documents were filed
  * @param {object} providers.documentsFiled the documents filed
+ * @param {boolean} providers.fileAcrossConsolidatedGroup flag to determine whether the document should be filed across the consolidated cases group
  * @returns {Promise<*>} the promise of the api call
  */
 exports.generatePrintableFilingReceiptInteractor = (
