6f68392a27919e16cbfff6b2292545cd0a0f13ba
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2023-05-02 16:33:55 -0500

    1006: convert lambda from js to ts

--- shared/src/business/useCases/auth/changePasswordLocalInteractor.ts
+++ shared/src/business/useCases/auth/changePasswordLocalInteractor.ts
@@ -1,12 +1,11 @@
 /**
  * changePasswordLocalInteractor
- *
  * @param {object} applicationContext the application context
  * @param {object} auth an object
  * @param {string} auth.newPassword the new password provided by a local user
  * @param {string} auth.sessionId the cognito (local) session id
  * @param {string} auth.userEmail the email of the user changing passwords
- * @returns {Promise} the promise of both the refresh token and the auth token
+ * @returns {Promise} the promise of a Cognito AuthenticationResult
  */
 export const changePasswordLocalInteractor = async (
   applicationContext: IApplicationContext,
