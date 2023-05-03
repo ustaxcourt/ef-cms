56ff91f263f2adc0ba8cf47e91612c9cf0ec3dee
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2023-05-02 20:05:08 -0500

    1006: rename lambda

--- web-api/src/users/createUserLocalLambda.ts
+++ web-api/src/users/createUserLocalLambda.ts
@@ -5,7 +5,7 @@ import { genericHandler } from '../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-exports.createUserLambdaLocal = event =>
+exports.createUserLocalLambda = event =>
   genericHandler(event, async ({ applicationContext }) => {
     return await applicationContext
       .getUseCases()
