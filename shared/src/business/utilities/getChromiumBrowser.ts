b85cdc1ea54ea0c87583d1360d0ac6f34f5d3c7b
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-05-08 08:30:24 -0700

    Bug fix: Try singleton approach for chromium launcher

--- shared/src/business/utilities/getChromiumBrowser.ts
+++ shared/src/business/utilities/getChromiumBrowser.ts
@@ -2,6 +2,8 @@ import chromium from '@sparticuz/chromium';
 import puppeteer from 'puppeteer';
 import puppeteerCore from 'puppeteer-core';
 
+let browserInstance;
+
 export const getChromiumBrowser = async () => {
   return await puppeteer.launch({
     args: ['--no-sandbox'],
@@ -9,10 +11,16 @@ export const getChromiumBrowser = async () => {
 };
 
 export const getChromiumBrowserAWS = async () => {
-  return await puppeteerCore.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath(),
-    headless: chromium.headless,
-  });
+  if (browserInstance) {
+    return browserInstance;
+  } else {
+    browserInstance = await puppeteerCore.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(),
+      headless: chromium.headless,
+    });
+
+    return browserInstance;
+  }
 };
