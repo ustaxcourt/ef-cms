c42e13fb559305c05370f30e1847fac704d6e5d2
Absolutestunna <joshujah007@gmail.com>
2023-05-10 13:58:20 -0400

    9723: reinstate test playground to test queries of 20k+

---
+++ shared/admin-tools/elasticsearch/test-query.ts
@@ -0,0 +1,85 @@
+// usage: npx ts-node shared/admin-tools/elasticsearch/get-petition-counts.js 2022
+// shared/admin-tools/elasticsearch/zach-jason-opensearch-playground.ts
+import { computeDate } from '../../src/business/utilities/DateHandler';
+import { createApplicationContext } from '../../../web-api/src/applicationContext';
+import { pinkLog } from '../../src/tools/pinkLog';
+import { search } from '../../src/persistence/elasticsearch/searchClient';
+
+const getAllCases = async ({ applicationContext }) => {
+  const source = [
+    'associatedJudge',
+    'caseCaption',
+    'docketNumber',
+    'docketNumberSuffix',
+    'docketNumberWithSuffix',
+    'receivedAt',
+    'status',
+  ];
+  const { results } = await search({
+    applicationContext,
+    searchParameters: {
+      body: {
+        _source: source,
+        query: {
+          bool: {
+            must: [
+              {
+                range: {
+                  'receivedAt.S': {
+                    gte: computeDate({ day: 1, month: 1, year: 2021 }),
+                    lt: computeDate({ day: 1, month: 11, year: 2023 }),
+                  },
+                },
+              },
+              // {
+              //   match: {
+              //     'isPaper.BOOL': false,
+              //   },
+              // },
+              // {
+              //   match: {
+              //     'blocked.BOOL': false,
+              //   },
+              // },
+              // {
+              //   match: {
+              //     'caseType.S': 'Whistleblower',
+              //   },
+              // },
+            ],
+          },
+        },
+        sort: [{ 'receivedAt.S': 'asc' }],
+      },
+      index: 'efcms-case',
+      size: 10000,
+    },
+  });
+  return results;
+};
+
+(async () => {
+  const applicationContext = createApplicationContext({});
+  const start = Date.now();
+  const params = {
+    caseStatuses: [],
+    caseTypes: [],
+    createEndDate: '',
+    createStartDate: '',
+    paperFilingMethods: [],
+  };
+  const petitions = await getAllCases({ applicationContext, params });
+  const end = Date.now();
+  pinkLog(petitions.length);
+  pinkLog('time****', (end - start) / 1000);
+})();
+
+// not query-able:
+// isPaper: boolean
+// createdAt: Date
+// caseType: String
+
+// Questions
+// why is new not in the Figma as an option for Case Status
+
+// Count API
