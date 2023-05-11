918e357d1ab54a8c935175fb66d2bdc1cfc672eb
Cody Seibert <cseibert113@gmail.com>
2023-05-11 16:04:06 -0400

    fixing variable hash bad reference

--- web-api/terraform/template/lambdas/pdf-generation.js
+++ web-api/terraform/template/lambdas/pdf-generation.js
@@ -10,12 +10,5 @@ export const handler = async event => {
     .getUseCaseHelpers()
     .generatePdfFromHtmlHelper(applicationContext, event);
 
-  return {
-    body: Buffer.from(results).toString('base64'),
-    headers: {
-      'Content-Type': 'application/octet-stream',
-    },
-    isBase64Encoded: true,
-    statusCode: 200,
-  };
+  return results.toString('base64');
 };
