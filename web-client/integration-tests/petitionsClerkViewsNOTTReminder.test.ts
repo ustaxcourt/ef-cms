1da7b45c705197fc989fb6c51c486b6c9c9b4683
Rosie Highsmith <ahighsmith@flexion.us>
2023-05-11 12:35:53 -0700

    9969: almost done with integration test

--- web-client/integration-tests/petitionsClerkViewsNOTTReminder.test.ts
+++ web-client/integration-tests/petitionsClerkViewsNOTTReminder.test.ts
@@ -1,13 +1,13 @@
 import { docketClerkCreatesATrialSession } from './journey/docketClerkCreatesATrialSession';
-import { docketClerkViewsNewTrialSession } from './journey/docketClerkViewsNewTrialSession';
 import { docketClerkViewsTrialSessionsTab } from './journey/docketClerkViewsTrialSessionsTab';
 import { formattedTrialSessionDetails } from '../src/presenter/computeds/formattedTrialSessionDetails';
+import { formattedTrialSessions } from '../src/presenter/computeds/formattedTrialSessions';
 import { loginAs, setupTest } from './helpers';
 import { petitionsClerkSetsATrialSessionsSchedule } from './journey/petitionsClerkSetsATrialSessionsSchedule';
 import { petitionsClerkViewsNewTrialSession } from './journey/petitionsClerkViewsNewTrialSession';
-import { petitionsClerkViewsOpenTrialSession } from './journey/petitionsClerkViewsOpenTrialSession';
 import { prepareDateFromString } from '../../shared/src/business/utilities/DateHandler';
 import { runCompute } from 'cerebral/test';
+import { trialSessionDetailsHelper } from '../src/presenter/computeds/trialSessionDetailsHelper';
 import { withAppContextDecorator } from '../src/withAppContext';
 
 describe('petitions clerk views NOTT reminder on calendared trial session within 30-35 day range', () => {
@@ -52,33 +52,117 @@ describe('petitions clerk views NOTT reminder on calendared trial session within
 
   describe('docket clerk views new trial session in trial session list', () => {
     loginAs(cerebralTest, 'docketclerk@example.com');
+    it('should see the NOTT reminder icon on the trial session list', async () => {
+      await cerebralTest.runSequence('gotoTrialSessionsSequence');
+      expect(cerebralTest.getState('currentPage')).toEqual('TrialSessions');
+
+      const trialSessionFormatted = runCompute(
+        withAppContextDecorator(formattedTrialSessions),
+        {
+          state: cerebralTest.getState(),
+        },
+      );
+
+      const filteredSessions =
+        trialSessionFormatted.filteredTrialSessions['Open'];
+
+      let foundSession;
+      filteredSessions.some(trialSession => {
+        trialSession.sessions.some(session => {
+          if (
+            session.trialSessionId === cerebralTest.lastCreatedTrialSessionId
+          ) {
+            foundSession = session;
+            return true;
+          }
+        });
+        if (foundSession) {
+          return true;
+        }
+      });
+
+      expect(foundSession).toBeDefined();
+      expect(foundSession.showAlertForNOTTReminder).toEqual(true);
+      expect(foundSession.alertMessageForNOTT).toEqual(
+        `The 30-day notice is due before ${foundSession.thirtyDaysBeforeTrialFormatted}`,
+      );
+    });
+
     docketClerkViewsTrialSessionsTab(cerebralTest);
 
-    it('should see the NOTT reminder icon', () => {
-      let trialSessionFormatted = runCompute(
+    it('should see the alert banner in the latest trial session but cannot clear it', async () => {
+      await cerebralTest.runSequence('gotoTrialSessionDetailSequence', {
+        trialSessionId: cerebralTest.trialSessionId,
+      });
+
+      const trialSessionDetailsFormatted = runCompute(
         withAppContextDecorator(formattedTrialSessionDetails),
         {
           state: cerebralTest.getState(),
         },
       );
 
-      expect(trialSessionFormatted.showAlertForNOTTReminder).toEqual(true);
+      expect(trialSessionDetailsFormatted.alertMessageForNOTT).toEqual(
+        `30-day trial notices are due before ${trialSessionDetailsFormatted.thirtyDaysBeforeTrialFormatted}. Have notices been served?`,
+      );
+
+      let trialSessionDetailsHelperComputed = runCompute(
+        withAppContextDecorator(trialSessionDetailsHelper),
+        {
+          state: cerebralTest.getState(),
+        },
+      );
+
+      expect(
+        trialSessionDetailsHelperComputed.canDismissThirtyDayAlert,
+      ).toEqual(false);
+    });
+  });
+
+  describe('petitions clerk views new trial session in trial session list', () => {
+    loginAs(cerebralTest, 'petitionsclerk@example.com');
+    it('should go to the created trial session', async () => {
+      await cerebralTest.runSequence('gotoTrialSessionsSequence', {
+        query: {
+          status: 'Open',
+        },
+      });
+
+      expect(cerebralTest.getState('currentPage')).toEqual('TrialSessions');
+      expect(
+        cerebralTest.getState('screenMetadata.trialSessionFilters.status'),
+      ).toEqual('Open');
     });
 
-    it('should see the alert banner in the latest trial session', async () => {
+    it('should see the alert banner in the latest trial session and can clear it', async () => {
       await cerebralTest.runSequence('gotoTrialSessionDetailSequence', {
         trialSessionId: cerebralTest.trialSessionId,
       });
 
-      // expect(trialSessionFormatted.alertMessageForNOTT).toEqual('blah blah');
-    });
+      const trialSessionDetailsFormatted = runCompute(
+        withAppContextDecorator(formattedTrialSessionDetails),
+        {
+          state: cerebralTest.getState(),
+        },
+      );
 
-    // assert that the alert banner is there, but can't clear it
+      expect(trialSessionDetailsFormatted.alertMessageForNOTT).toEqual(
+        `30-day trial notices are due before ${trialSessionDetailsFormatted.thirtyDaysBeforeTrialFormatted}. Have notices been served?`,
+      );
+
+      let trialSessionDetailsHelperComputed = runCompute(
+        withAppContextDecorator(trialSessionDetailsHelper),
+        {
+          state: cerebralTest.getState(),
+        },
+      );
+
+      expect(
+        trialSessionDetailsHelperComputed.canDismissThirtyDayAlert,
+      ).toEqual(true);
+    });
   });
 
-  // login as petitionsClerk
-  // go into the last created trial session
-  // assert that the alert banner is there
   // clear the alert
-  // assert the banner is success
+  // assert the banner is gone
 });
