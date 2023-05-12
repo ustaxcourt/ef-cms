269ae5affe3b7430ea33a633834c0b5ce72eb7b5
Cody Seibert <cseibert113@gmail.com>
2023-05-12 14:06:19 -0400

    finally working

--- shared/src/business/utilities/getChromiumBrowser.ts
+++ shared/src/business/utilities/getChromiumBrowser.ts
@@ -6,17 +6,4 @@ export const getChromiumBrowser = async () => {
   });
 };
 
-export const getChromiumBrowserAWS = async () => {
-  // we need to import these as external dependencies to allow us to reuse the application
-  // context in lambdas that DO NOT have the layer.
-  const { default: chromium } = await import('@sparticuz/chromium');
-  const { default: puppeteerCore } = await import('puppeteer-core');
-
-  chromium.setGraphicsMode = false;
-  return await puppeteerCore.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath(),
-    headless: chromium.headless,
-  });
-};
+export const getChromiumBrowserAWS = async () => {};
