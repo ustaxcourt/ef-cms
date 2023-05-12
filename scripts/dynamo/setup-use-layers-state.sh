72f5e88478c73e3dfea4f93a12999a616ec76602
Cody Seibert <cseibert113@gmail.com>
2023-05-12 10:44:16 -0400

    finally working

---
+++ scripts/dynamo/setup-use-layers-state.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Sets the use layers state in deploy table
+
+# Usage
+#   ENV=dev ./setup-use-layers-state.sh
+
+./check-env-variables.sh \
+  "ENV" \
+  "AWS_SECRET_ACCESS_KEY" \
+  "AWS_ACCESS_KEY_ID"
+
+ITEM=$(cat <<-END
+{
+    "pk": {
+        "S": "deploying-use-layers"
+    },
+    "sk":{
+        "S": "deploying-use-layers"
+    },
+    "current": {
+        "BOOL": false
+    }
+}
+END
+)
+
+echo "HERE"
+aws dynamodb put-item \
+    --region us-east-1 \
+    --table-name "efcms-deploy-${ENV}" \
+    --item "${ITEM}"
+
+
+ITEM=$(cat <<-END
+{
+    "pk": {
+        "S": "current-use-layers"
+    },
+    "sk":{
+        "S": "current-use-layers"
+    },
+    "current": {
+        "BOOL": true
+    }
+}
+END
+)
+
+echo "THERE"
+
+aws dynamodb put-item \
+    --region us-east-1 \
+    --table-name "efcms-deploy-${ENV}" \
+    --item "${ITEM}"
