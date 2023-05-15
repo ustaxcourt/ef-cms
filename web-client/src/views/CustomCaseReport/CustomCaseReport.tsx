9e82bd60fc3c6ac9294b3c1c9b1ef71eae9b6b7c
Jason Fu <jfu@flexion.us>
2023-05-15 10:26:03 -0700

    9723 - conditionally rendering the custom report table body, wrote pa11y test for making sure elements of custom report page being visible.

--- web-client/src/views/CustomCaseReport/CustomCaseReport.tsx
+++ web-client/src/views/CustomCaseReport/CustomCaseReport.tsx
@@ -323,9 +323,9 @@ const ReportTable = ({
   return (
     <>
       <table
-        aria-label="case inventory record"
+        aria-label="custom case inventory record"
         className="usa-table case-detail ustc-table responsive-table"
-        id="docket-record-table"
+        id="custom-case-report-table"
       >
         <thead>
           <tr>
@@ -344,9 +344,9 @@ const ReportTable = ({
             </th>
           </tr>
         </thead>
-        <tbody>
-          {cases &&
-            cases.map(entry => (
+        {cases.length !== 0 && (
+          <tbody id="custom-case-report-table-body">
+            {cases.map(entry => (
               <tr key={`${entry.docketNumber}-${entry.caseCreationEndDate}`}>
                 <td>
                   <CaseLink formattedCase={entry} />
@@ -369,7 +369,8 @@ const ReportTable = ({
                 </td>
               </tr>
             ))}
-        </tbody>
+          </tbody>
+        )}
       </table>
       {hasRunCustomCaseReport && totalCases === 0 && (
         <p>There are no cases for the selected criteria.</p>
