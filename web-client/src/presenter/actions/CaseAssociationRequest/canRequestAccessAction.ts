9e58f3d56c2554cc6fce4c7fbb7c92535a18fa44
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2023-05-23 16:03:19 -0500

    9874: do not allow private practitioners to multi-docket entry of appearance docs

--- web-client/src/presenter/actions/CaseAssociationRequest/canRequestAccessAction.ts
+++ web-client/src/presenter/actions/CaseAssociationRequest/canRequestAccessAction.ts
@@ -4,14 +4,28 @@ import { state } from 'cerebral';
  * invokes the path in the sequences depending on if the user can request access
  * @param {object} providers the providers object
  * @param {Function} providers.get the cerebral get method for getting the state.user
+ * @param {object} providers.applicationContext the application context
  * @param {object} providers.path the cerebral path which is contains the next paths that can be invoked
  * @returns {object} the list of section work items
  */
-export const canRequestAccessAction = ({ get, path, props }: ActionProps) => {
+export const canRequestAccessAction = ({
+  applicationContext,
+  get,
+  path,
+  props,
+}: ActionProps) => {
+  const user = applicationContext.getCurrentUser();
+  const { USER_ROLES } = applicationContext.getConstants();
   const { isDirectlyAssociated } = props;
   const docketNumber = get(state.caseDetail.docketNumber);
+
   if (!isDirectlyAssociated) {
-    const overrideForRequestAccess = true;
+    let overrideForRequestAccess = false;
+
+    if (user.role === USER_ROLES.irsPractitioner) {
+      overrideForRequestAccess = true;
+    }
+
     return path['proceed']({ overrideForRequestAccess });
   } else {
     return path['unauthorized']({ docketNumber });
