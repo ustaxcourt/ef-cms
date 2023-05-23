11598591f48dea4d0e1dd0148f7a2bec48232d2b
Cody Seibert <cseibert113@gmail.com>
2023-05-23 13:22:42 -0400

    10022 - trying to fix more ts errors

--- web-client/src/presenter/presenter-mock.ts
+++ web-client/src/presenter/presenter-mock.ts
@@ -4,7 +4,15 @@ import { baseState } from './state';
 export const presenter = {
   providers: {
     applicationContext: {} as ClientApplicationContext,
+    path: undefined,
   },
   sequences: {},
   state: baseState,
+} as {
+  providers: {
+    applicationContext: ClientApplicationContext;
+    path: any;
+  };
+  sequences: any;
+  state: typeof baseState;
 };
