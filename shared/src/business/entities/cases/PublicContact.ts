deaac62e749664958678370409687f490620ff97
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2023-05-25 13:40:47 -0600

    Revert "9866: fix lint error; update PublicContact to class"
    This reverts commit d183ab5d3b95d6a485cf56b72870eb8e1d16326e.

--- shared/src/business/entities/cases/PublicContact.ts
+++ shared/src/business/entities/cases/PublicContact.ts
@@ -1,53 +1,38 @@
-import { JoiValidationEntity } from '../JoiValidationEntity';
+const joi = require('joi');
+const {
+  joiValidationDecorator,
+  validEntityDecorator,
+} = require('../JoiValidationDecorator');
 const { CONTACT_TYPES } = require('../EntityConstants');
 const { JoiValidationConstants } = require('../JoiValidationConstants');
 
-export class PublicContact extends JoiValidationEntity {
-  public entityName: string;
-  public contactId: string;
-  public contactType: string;
-  public name: string;
-  public state: string;
-
-  constructor(
-    rawContact: any,
-    { applicationContext }: { applicationContext: IApplicationContext },
-  ) {
-    super('PublicContact');
-
-    if (!applicationContext) {
-      throw new TypeError('applicationContext must be defined');
-    }
-
-    this.entityName = 'PublicContact';
-    this.contactId = rawContact.contactId;
-    this.contactType = rawContact.contactType;
-    this.name = rawContact.name;
-    this.state = rawContact.state;
-  }
-
-  static VALIDATION_ERROR_MESSAGES = {
-    contactId: 'Contact ID is required.',
-  };
-
-  static VALIDATION_RULES = {
-    contactId: JoiValidationConstants.UUID.required(),
-    contactType: JoiValidationConstants.STRING.valid(
-      ...Object.values(CONTACT_TYPES),
-    ).optional(),
-    name: JoiValidationConstants.STRING.max(500).optional(),
-    state: JoiValidationConstants.STRING.optional(),
-  };
-
-  getValidationRules() {
-    return PublicContact.VALIDATION_RULES;
-  }
-
-  getErrorToMessageMap() {
-    return PublicContact.VALIDATION_ERROR_MESSAGES;
-  }
+/**
+ * PublicContact
+ * Represents the view of the public case.
+ *
+ * @param {object} rawContact the raw case data
+ * @constructor
+ */
+function PublicContact() {
+  this.entityName = 'PublicContact';
 }
 
-declare global {
-  type RawPublicContact = ExcludeMethods<PublicContact>;
-}
+PublicContact.prototype.init = function init(rawContact) {
+  this.contactId = rawContact.contactId;
+  this.contactType = rawContact.contactType;
+  this.name = rawContact.name;
+  this.state = rawContact.state;
+};
+
+PublicContact.VALIDATION_RULES = joi.object().keys({
+  contactId: JoiValidationConstants.UUID.required(),
+  contactType: JoiValidationConstants.STRING.valid(
+    ...Object.values(CONTACT_TYPES),
+  ).optional(),
+  name: JoiValidationConstants.STRING.max(500).optional(),
+  state: JoiValidationConstants.STRING.optional(),
+});
+
+joiValidationDecorator(PublicContact, PublicContact.VALIDATION_RULES, {});
+
+module.exports = { PublicContact: validEntityDecorator(PublicContact) };
