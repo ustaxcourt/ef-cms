39867e17f9ae9d4e02d3ef80feb69d58924cae99
Absolutestunna <joshujah007@gmail.com>
2023-05-31 13:00:06 -0400

    9998 - add types to cookie formatters

--- web-api/src/auth/deleteAuthCookieLambda.ts
+++ web-api/src/auth/deleteAuthCookieLambda.ts
@@ -3,7 +3,6 @@ import { genericHandler } from '../genericHandler';
 
 /**
  * Clears the refresh token from the cookie
- *
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
