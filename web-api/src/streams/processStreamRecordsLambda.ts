07abe23c274940b380610673f5ff4f6db00d8563
Cody Seibert <cseibert113@gmail.com>
2023-06-02 12:25:56 -0400

    devex-1122 - fist attempt to skip over processed records

--- web-api/src/streams/processStreamRecordsLambda.ts
+++ web-api/src/streams/processStreamRecordsLambda.ts
@@ -1,18 +1,102 @@
 import { createApplicationContext } from '../applicationContext';
+import { getTableName } from '../../../shared/src/persistence/dynamodbClientService';
+import type { DynamoDBRecord, DynamoDBStreamEvent } from 'aws-lambda';
+
+/**
+ *
+ */
+async function putEventHistory(
+  applicationContext: IApplicationContext,
+  eventID: string,
+) {
+  const twentyFourHours = 24 * 60 * 60 * 1000;
+
+  await applicationContext
+    .getDocumentClient({
+      useMasterRegion: true,
+    })
+    .put({
+      Item: {
+        pk: `stream-event-id|${eventID}`,
+        sk: `stream-event-id|${eventID}`,
+        ttl: Date.now() + twentyFourHours,
+      },
+      TableName: getTableName({
+        applicationContext,
+      }),
+    })
+    .promise();
+}
+
+/**
+ *
+ */
+async function hasRecordBeenProcessed(
+  applicationContext: IApplicationContext,
+  record: DynamoDBRecord,
+) {
+  const { eventID } = record;
+  if (!eventID) return false;
+
+  const { Item: eventHistory } = await applicationContext
+    .getDocumentClient({
+      useMasterRegion: true,
+    })
+    .get({
+      Key: {
+        pk: `stream-event-id|${eventID}`,
+        sk: `stream-event-id|${eventID}`,
+      },
+      TableName: getTableName({
+        applicationContext,
+      }),
+    })
+    .promise();
+
+  return !!eventHistory;
+}
 
 /**
  * used for processing stream records from persistence
- *
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const processStreamRecordsLambda = async event => {
+export const processStreamRecordsLambda = async (
+  event: DynamoDBStreamEvent,
+) => {
   const applicationContext = createApplicationContext({});
-  applicationContext.logger.debug('received a stream event of', event);
-  const recordsToProcess = event.Records;
-  return await applicationContext
-    .getUseCases()
-    .processStreamRecordsInteractor(applicationContext, {
-      recordsToProcess,
-    });
+  const recordsToProcess: DynamoDBRecord[] = [];
+
+  const isStreamRecord = record =>
+    record.dynamodb?.Keys?.pk.S?.startsWith('stream-event-id');
+
+  await Promise.all(
+    event.Records.filter(record => !isStreamRecord(record)).map(
+      async record => {
+        const isRecordProcessed = await hasRecordBeenProcessed(
+          applicationContext,
+          record,
+        );
+        if (!isRecordProcessed) {
+          recordsToProcess.push(record);
+        }
+      },
+    ),
+  );
+
+  if (recordsToProcess.length > 0) {
+    await applicationContext
+      .getUseCases()
+      .processStreamRecordsInteractor(applicationContext, {
+        recordsToProcess,
+      });
+
+    await Promise.all(
+      recordsToProcess.map(async record => {
+        const { eventID } = record;
+        if (!eventID) return;
+        await putEventHistory(applicationContext, eventID);
+      }),
+    );
+  }
 };
