b3a180e5879346971e08345f336673abb37569db
Zachary Rogers <zrogers>
2023-06-06 13:30:55 -0700

    10018: Get all case information after deleting a document.

--- web-client/src/presenter/sequences/archiveDraftDocumentSequence.ts
+++ web-client/src/presenter/sequences/archiveDraftDocumentSequence.ts
@@ -1,12 +1,15 @@
 import { archiveDraftDocumentAction } from '../actions/archiveDraftDocumentAction';
 import { clearDraftDocumentViewerAction } from '../actions/clearDraftDocumentViewerAction';
 import { clearModalAction } from '../actions/clearModalAction';
+import { getConsolidatedCasesByCaseAction } from '../actions/CaseConsolidation/getConsolidatedCasesByCaseAction';
 import { getDefaultDraftViewerDocumentToDisplayAction } from '../actions/getDefaultDraftViewerDocumentToDisplayAction';
 import { getMessagesForCaseAction } from '../actions/CaseDetail/getMessagesForCaseAction';
-import { navigateToCaseDetailAction } from '../actions/navigateToCaseDetailAction';
+import { parallel } from 'cerebral';
 import { resetArchiveDraftDocumentAction } from '../actions/resetArchiveDraftDocumentAction';
 import { setAlertSuccessAction } from '../actions/setAlertSuccessAction';
 import { setCaseAction } from '../actions/setCaseAction';
+import { setConsolidatedCasesForCaseAction } from '../actions/CaseConsolidation/setConsolidatedCasesForCaseAction';
+import { setSaveAlertsForNavigationAction } from '../actions/setSaveAlertsForNavigationAction';
 import { setShowModalFactoryAction } from '../actions/setShowModalFactoryAction';
 import { setViewerDraftDocumentToDisplayAction } from '../actions/setViewerDraftDocumentToDisplayAction';
 import { showProgressSequenceDecorator } from '../utilities/showProgressSequenceDecorator';
@@ -18,14 +21,16 @@ export const archiveDraftDocumentSequence = showProgressSequenceDecorator([
   {
     error: [setShowModalFactoryAction('DocketEntryHasAlreadyBeenServedModal')],
     success: [
+      setSaveAlertsForNavigationAction,
       setAlertSuccessAction,
-      // setSaveAlertsForNavigationAction,
       setCaseAction,
+      parallel([
+        [getConsolidatedCasesByCaseAction, setConsolidatedCasesForCaseAction],
+        [getMessagesForCaseAction],
+      ]),
       resetArchiveDraftDocumentAction,
       getDefaultDraftViewerDocumentToDisplayAction,
       setViewerDraftDocumentToDisplayAction,
-      getMessagesForCaseAction,
-      navigateToCaseDetailAction,
     ],
   },
 ]);
