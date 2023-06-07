ee4ab5c884ab48754a04bf9c16d7c329c12bf590
Absolutestunna <joshujah007@gmail.com>
2023-06-07 09:26:35 -0400

    9998 - small refactor for viewJudgeActivityReportResults and judgeViewsJudgeActivityReportPage

--- web-client/integration-tests/journey/viewJudgeActivityReportResults.ts
+++ web-client/integration-tests/journey/viewJudgeActivityReportResults.ts
@@ -15,12 +15,8 @@ const judgeActivityReportHelper = withAppContextDecorator(
 export const viewJudgeActivityReportResults = (
   cerebralTest: any,
   {
-    endDate,
     selectedJudgeName,
-    startDate = '01/01/2020',
   }: {
-    startDate?: string;
-    endDate?: string;
     selectedJudgeName?: string;
   },
 ) => {
@@ -30,6 +26,8 @@ export const viewJudgeActivityReportResults = (
       getConstants().DATE_FORMATS.MMDDYYYY,
     );
 
+    const startDate = '01/01/2020';
+
     await refreshElasticsearchIndex();
     await cerebralTest.runSequence('gotoJudgeActivityReportSequence');
     await cerebralTest.runSequence('setJudgeActivityReportFiltersSequence', {
@@ -37,7 +35,7 @@ export const viewJudgeActivityReportResults = (
     });
 
     await cerebralTest.runSequence('setJudgeActivityReportFiltersSequence', {
-      endDate: endDate || currentDate,
+      endDate: currentDate,
     });
 
     await cerebralTest.runSequence('setJudgeActivityReportFiltersSequence', {
