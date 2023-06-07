5c4c824b80b194bef4bda4b907ee9b9f8e2ea52d
Zachary Rogers <zrogers>
2023-06-07 13:34:58 -0700

    10018: Fixing unit test

--- web-client/src/presenter/actions/clearDraftDocumentViewerAction.test.ts
+++ web-client/src/presenter/actions/clearDraftDocumentViewerAction.test.ts
@@ -3,7 +3,7 @@ import { presenter } from '../presenter-mock';
 import { runAction } from 'cerebral/test';
 
 describe('clearDraftDocumentViewerAction', () => {
-  it('should unset state.draftDocumentViewerDocketEntryId and viewerDraftDocumentToDisplay so the viewer does not display an unexpected document', async () => {
+  it('should unset state.draftDocumentViewerDocketEntryId so the viewer does not display an unexpected document', async () => {
     const { state } = await runAction(clearDraftDocumentViewerAction, {
       modules: {
         presenter,
@@ -11,13 +11,9 @@ describe('clearDraftDocumentViewerAction', () => {
       state: {
         draftDocumentViewerDocketEntryId:
           'be0ce175-874f-4c26-8d20-bf358d26cc55',
-        viewerDraftDocumentToDisplay: {
-          docketEntryId: 'dd6c4a2d-6124-432e-986f-80af716f8f39',
-        },
       },
     });
 
     expect(state.draftDocumentViewerDocketEntryId).toBeUndefined();
-    expect(state.viewerDraftDocumentToDisplay).toBeUndefined();
   });
 });
