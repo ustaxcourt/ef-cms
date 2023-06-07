e86a655bbd5144e4ee8ddbe8b18e0f3abea133a7
Zachary Rogers <zrogers>
2023-06-07 13:08:40 -0700

    10018: remove unset of draftdocument

--- web-client/src/presenter/actions/clearDraftDocumentViewerAction.ts
+++ web-client/src/presenter/actions/clearDraftDocumentViewerAction.ts
@@ -2,5 +2,4 @@ import { state } from 'cerebral';
 
 export const clearDraftDocumentViewerAction = ({ store }: ActionProps) => {
   store.unset(state.draftDocumentViewerDocketEntryId);
-  store.unset(state.viewerDraftDocumentToDisplay);
 };
