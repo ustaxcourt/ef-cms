9736676534772b27589e3e019aa869fba885a472
Richard Goshen <rick.goshen@gmail.com>
2023-06-07 07:32:23 -0700

    9785: remove more jsdocs and add leadDocketNumber to case mapping so deadline will show icons

--- web-client/src/presenter/actions/CaseDeadline/getCaseDeadlinesAction.ts
+++ web-client/src/presenter/actions/CaseDeadline/getCaseDeadlinesAction.ts
@@ -1,12 +1,5 @@
 import { state } from 'cerebral';
 
-/**
- * get case deadlines between start and end date
- * @param {object} providers the providers object
- * @param {object} providers.applicationContext the application context
- * @param {function} providers.get the get function
- * @returns {object} the case deadlines
- */
 export const getCaseDeadlinesAction = async ({
   applicationContext,
   get,
