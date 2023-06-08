6563b7b64d6dba4f6d72a304908b9d7aa2e350ad
Absolutestunna <joshujah007@gmail.com>
2023-06-08 17:51:23 -0400

    9998 - removing more jsdocs

--- shared/src/proxies/reports/getTrialSessionsForJudgeActivityReportProxy.ts
+++ shared/src/proxies/reports/getTrialSessionsForJudgeActivityReportProxy.ts
@@ -1,15 +1,5 @@
 import { post } from '../requests';
 
-/**
- * getTrialSessionsForJudgeActivityReportInteractor
- *
- * @param {object} applicationContext the application context
- * @param {object} providers the providers object
- * @param {object} providers.endDate the report end date
- * @param {object} providers.judgeId the judgeId to query for
- * @param {object} providers.startDate the report start date
- * @returns {Promise<*>} the promise of the api call
- */
 export const getTrialSessionsForJudgeActivityReportInteractor = (
   applicationContext,
   {
