b25939b42561ce68dcb065bbadfef79d2234e7e2
Rosie Highsmith <ahighsmith@flexion.us>
2023-06-08 14:49:45 -0700

    10061: update document title with additional info regardless of addToCoversheet

--- shared/src/business/utilities/getDescriptionDisplay.test.ts
+++ shared/src/business/utilities/getDescriptionDisplay.test.ts
@@ -5,70 +5,56 @@ describe('getDescriptionDisplay', () => {
   const documentType = 'doc type';
   const description = 'doc description';
 
-  let documentMetaData = {
+  let baseDocketEntry = {
     documentTitle,
   };
 
   it('returns descriptionDisplay as documentTitle with no added information', () => {
-    const result = getDescriptionDisplay(documentMetaData);
+    const result = getDescriptionDisplay(baseDocketEntry);
     expect(result).toEqual('doc title');
   });
 
   it('returns descriptionDisplay as documentType with no added information', () => {
-    documentMetaData = {
-      ...documentMetaData,
+    baseDocketEntry = {
+      ...baseDocketEntry,
       documentTitle: undefined,
       documentType,
     };
-    const result = getDescriptionDisplay(documentMetaData);
+    const result = getDescriptionDisplay(baseDocketEntry);
 
     expect(result).toEqual('doc type');
   });
 
   it('returns descriptionDisplay as description with no added information', () => {
-    documentMetaData = {
-      ...documentMetaData,
+    baseDocketEntry = {
+      ...baseDocketEntry,
       description,
       documentTitle: undefined,
       documentType: undefined,
     };
-    const result = getDescriptionDisplay(documentMetaData);
+    const result = getDescriptionDisplay(baseDocketEntry);
     expect(result).toEqual('doc description');
   });
 
-  it('returns descriptionDisplay as document title plus additionalInfo if addToCoversheet is true', () => {
-    documentMetaData = {
-      ...documentMetaData,
-      addToCoversheet: true,
+  it('should return descriptionDisplay as document title plus additionalInfo', () => {
+    baseDocketEntry = {
+      ...baseDocketEntry,
       additionalInfo: 'superfulous text',
       documentTitle,
     };
-    const result = getDescriptionDisplay(documentMetaData);
+    const result = getDescriptionDisplay(baseDocketEntry);
     expect(result).toEqual('doc title superfulous text');
   });
 
-  it('returns descriptionDisplay as document title with no additionalInfo if addToCoversheet is false', () => {
-    documentMetaData = {
-      ...documentMetaData,
-      addToCoversheet: false,
-      additionalInfo: 'superfulous text',
-      documentTitle,
-    };
-    const result = getDescriptionDisplay(documentMetaData);
-
-    expect(result).toEqual('doc title');
-  });
-
   it('returns descriptionDisplay as freeText and documentTitle if eventCode is OCS and freeText is available', () => {
-    documentMetaData = {
-      ...documentMetaData,
+    baseDocketEntry = {
+      ...baseDocketEntry,
       addToCoversheet: false,
-      additionalInfo: 'superfulous text',
       documentTitle,
       eventCode: 'OCS',
       freeText: 'free text',
     };
-    const result = getDescriptionDisplay(documentMetaData);
+    const result = getDescriptionDisplay(baseDocketEntry);
 
     expect(result).toEqual('free text - doc title');
   });
