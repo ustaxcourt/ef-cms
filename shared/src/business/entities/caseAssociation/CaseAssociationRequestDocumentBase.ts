925b20f99407e121472a12d07b4358477dd89b33
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-06-14 15:45:07 -0700

    Devex 1188: Inherit from base class

--- shared/src/business/entities/caseAssociation/CaseAssociationRequestDocumentBase.ts
+++ shared/src/business/entities/caseAssociation/CaseAssociationRequestDocumentBase.ts
@@ -1,6 +1,7 @@
 import {
   ALL_DOCUMENT_TYPES,
   ALL_EVENT_CODES,
+  OBJECTIONS_OPTIONS,
   SCENARIOS,
 } from '../EntityConstants';
 import { CaseAssociationRequestDocument } from './CaseAssociationRequestDocument';
@@ -9,6 +10,7 @@ import { VALIDATION_ERROR_MESSAGES } from '../externalDocument/ExternalDocumentI
 import joi from 'joi';
 
 export class CaseAssociationRequestDocumentBase extends CaseAssociationRequestDocument {
+  public attachments?: boolean;
   public certificateOfService: boolean;
   public certificateOfServiceDate?: string;
   public documentTitle?: string;
@@ -16,34 +18,51 @@ export class CaseAssociationRequestDocumentBase extends CaseAssociationRequestDo
   public documentType: string;
   public eventCode: string;
   public filers?: string[];
+  public hasSupportingDocuments?: boolean;
+  public objections?: string;
   public partyIrsPractitioner?: boolean;
   public partyPrivatePractitioner?: boolean;
   public primaryDocumentFile: any;
   public scenario: string;
+  public supportingDocuments?: any[];
 
   constructor(rawProps) {
     super('CaseAssociationRequestDocumentBase');
 
+    this.attachments = rawProps.attachments;
     this.certificateOfService = rawProps.certificateOfService;
     this.certificateOfServiceDate = rawProps.certificateOfServiceDate;
     this.documentTitle = rawProps.documentTitle;
     this.documentTitleTemplate = rawProps.documentTitleTemplate;
     this.documentType = rawProps.documentType;
     this.eventCode = rawProps.eventCode;
+    this.hasSupportingDocuments = rawProps.hasSupportingDocuments;
+    this.objections = rawProps.objections;
     this.partyPrivatePractitioner = rawProps.partyPrivatePractitioner;
     this.partyIrsPractitioner = rawProps.partyIrsPractitioner;
     this.primaryDocumentFile = rawProps.primaryDocumentFile;
     this.filers = rawProps.filers || [];
     this.scenario = rawProps.scenario;
+    this.supportingDocuments = rawProps.supportingDocuments;
+
+    if (this.supportingDocuments) {
+      this.supportingDocuments = this.supportingDocuments.map(item => {
+        return SupportingDocumentInformationFactory(
+          item,
+          CaseAssociationRequestDocumentBase.VALIDATION_ERROR_MESSAGES,
+        );
+      });
+    }
   }
 
   static VALIDATION_RULES = {
+    attachments: joi.boolean().optional(),
     certificateOfService: joi.boolean().required(),
     certificateOfServiceDate: JoiValidationConstants.ISO_DATE.max('now').when(
       'certificateOfService',
       {
         is: true,
-        otherwise: joi.optional(),
+        otherwise: joi.optional().allow(null),
         then: joi.required(),
       },
     ),
@@ -63,10 +82,15 @@ export class CaseAssociationRequestDocumentBase extends CaseAssociationRequestDo
         otherwise: joi.optional(),
         then: joi.required(),
       }),
+    hasSupportingDocuments: joi.boolean().optional(),
+    objections: JoiValidationConstants.STRING.valid(
+      ...OBJECTIONS_OPTIONS,
+    ).optional(),
     partyIrsPractitioner: joi.boolean().optional(),
     partyPrivatePractitioner: joi.boolean().optional(),
     primaryDocumentFile: joi.object().required(),
     scenario: JoiValidationConstants.STRING.valid(...SCENARIOS).required(),
+    supportingDocuments: joi.array().optional(),
   };
 
   static VALIDATION_ERROR_MESSAGES = {
--- shared/src/business/entities/caseAssociation/CaseAssociationRequestDocumentTypeD.ts
+++ shared/src/business/entities/caseAssociation/CaseAssociationRequestDocumentTypeD.ts
@@ -1,15 +1,7 @@
-import {
-  ALL_DOCUMENT_TYPES,
-  ALL_EVENT_CODES,
-  OBJECTIONS_OPTIONS,
-  SCENARIOS,
-} from '../EntityConstants';
 import { CaseAssociationRequestDocument } from './CaseAssociationRequestDocument';
-import { JoiValidationConstants } from '../JoiValidationConstants';
+import { CaseAssociationRequestDocumentBase } from './CaseAssociationRequestDocumentBase';
 import { SupportingDocumentInformationFactory } from '../externalDocument/SupportingDocumentInformationFactory';
-import { VALIDATION_ERROR_MESSAGES } from '../externalDocument/ExternalDocumentInformationFactory';
 import { replaceBracketed } from '../../utilities/replaceBracketed';
-import joi from 'joi';
 
 export class CaseAssociationRequestDocumentTypeD extends CaseAssociationRequestDocument {
   public attachments?: boolean;
@@ -20,7 +12,7 @@ export class CaseAssociationRequestDocumentTypeD extends CaseAssociationRequestD
   public documentType: string;
   public eventCode: string;
   public filers?: string[];
-  public hasSupportingDocuments: boolean;
+  public hasSupportingDocuments?: boolean;
   public objections: string;
   public partyIrsPractitioner?: boolean;
   public partyPrivatePractitioner?: boolean;
@@ -57,51 +49,10 @@ export class CaseAssociationRequestDocumentTypeD extends CaseAssociationRequestD
     }
   }
 
-  static VALIDATION_RULES = {
-    attachments: joi.boolean().optional(),
-    certificateOfService: joi.boolean().required(),
-    certificateOfServiceDate: JoiValidationConstants.ISO_DATE.max('now').when(
-      'certificateOfService',
-      {
-        is: true,
-        otherwise: joi.optional(),
-        then: joi.required(),
-      },
-    ),
-    documentTitle: JoiValidationConstants.STRING.max(500).optional(),
-    documentTitleTemplate: JoiValidationConstants.STRING.max(500).required(),
-    documentType: JoiValidationConstants.STRING.valid(
-      ...ALL_DOCUMENT_TYPES,
-    ).required(),
-    eventCode: JoiValidationConstants.STRING.valid(
-      ...ALL_EVENT_CODES,
-    ).required(),
-    filers: joi
-      .array()
-      .items(JoiValidationConstants.UUID.required())
-      .when('partyIrsPractitioner', {
-        is: false,
-        otherwise: joi.optional(),
-        then: joi.required(),
-      }),
-    hasSupportingDocuments: joi.boolean().optional(),
-    objections: JoiValidationConstants.STRING.valid(
-      ...OBJECTIONS_OPTIONS,
-    ).optional(),
-    partyIrsPractitioner: joi.boolean().optional(),
-    partyPrivatePractitioner: joi.boolean().optional(),
-    primaryDocumentFile: joi.object().required(),
-    scenario: JoiValidationConstants.STRING.valid(...SCENARIOS).required(),
-    supportingDocuments: joi.array().optional(),
-  };
+  static VALIDATION_RULES = CaseAssociationRequestDocumentBase.VALIDATION_RULES;
 
-  static VALIDATION_ERROR_MESSAGES = {
-    ...VALIDATION_ERROR_MESSAGES,
-    documentTitleTemplate: 'Select a document',
-    eventCode: 'Select a document',
-    filers: 'Select a party',
-    scenario: 'Select a document',
-  };
+  static VALIDATION_ERROR_MESSAGES =
+    CaseAssociationRequestDocumentBase.VALIDATION_ERROR_MESSAGES;
 
   getDocumentTitle = petitioners => {
     let petitionerNames;
