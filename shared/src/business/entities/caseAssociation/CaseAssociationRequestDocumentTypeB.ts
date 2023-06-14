1935e21d01f34c9e0275d5b37c0a35288837dfc6
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-06-14 15:45:07 -0700

    Devex 1188: Inherit from base class

--- shared/src/business/entities/caseAssociation/CaseAssociationRequestDocumentTypeB.ts
+++ shared/src/business/entities/caseAssociation/CaseAssociationRequestDocumentTypeB.ts
@@ -1,15 +1,9 @@
-import {
-  ALL_DOCUMENT_TYPES,
-  ALL_EVENT_CODES,
-  OBJECTIONS_OPTIONS,
-  SCENARIOS,
-} from '../EntityConstants';
 import { CaseAssociationRequestDocument } from './CaseAssociationRequestDocument';
+import { CaseAssociationRequestDocumentBase } from './CaseAssociationRequestDocumentBase';
 import { JoiValidationConstants } from '../JoiValidationConstants';
+import { OBJECTIONS_OPTIONS } from '../EntityConstants';
 import { SupportingDocumentInformationFactory } from '../externalDocument/SupportingDocumentInformationFactory';
-import { VALIDATION_ERROR_MESSAGES } from '../externalDocument/ExternalDocumentInformationFactory';
 import { replaceBracketed } from '../../utilities/replaceBracketed';
-import joi from 'joi';
 
 export class CaseAssociationRequestDocumentTypeB extends CaseAssociationRequestDocument {
   public attachments?: boolean;
@@ -58,50 +52,14 @@ export class CaseAssociationRequestDocumentTypeB extends CaseAssociationRequestD
   }
 
   static VALIDATION_RULES = {
-    attachments: joi.boolean().optional(),
-    certificateOfService: joi.boolean().required(),
-    certificateOfServiceDate: JoiValidationConstants.ISO_DATE.max('now').when(
-      'certificateOfService',
-      {
-        is: true,
-        otherwise: joi.optional(),
-        then: joi.required(),
-      },
-    ),
-    documentTitle: JoiValidationConstants.STRING.max(500).optional(),
-    documentTitleTemplate: JoiValidationConstants.STRING.max(500).required(),
-    documentType: JoiValidationConstants.STRING.valid(
-      ...ALL_DOCUMENT_TYPES,
-    ).required(),
-    eventCode: JoiValidationConstants.STRING.valid(
-      ...ALL_EVENT_CODES,
-    ).required(),
-    filers: joi
-      .array()
-      .items(JoiValidationConstants.UUID.required())
-      .when('partyIrsPractitioner', {
-        is: false,
-        otherwise: joi.optional(),
-        then: joi.required(),
-      }),
-    hasSupportingDocuments: joi.boolean().optional(),
+    ...CaseAssociationRequestDocumentBase.VALIDATION_RULES,
     objections: JoiValidationConstants.STRING.valid(
       ...OBJECTIONS_OPTIONS,
     ).required(),
-    partyIrsPractitioner: joi.boolean().optional(),
-    partyPrivatePractitioner: joi.boolean().optional(),
-    primaryDocumentFile: joi.object().required(),
-    scenario: JoiValidationConstants.STRING.valid(...SCENARIOS).required(),
-    supportingDocuments: joi.array().optional(),
   };
 
-  static VALIDATION_ERROR_MESSAGES = {
-    ...VALIDATION_ERROR_MESSAGES,
-    documentTitleTemplate: 'Select a document',
-    eventCode: 'Select a document',
-    filers: 'Select a party',
-    scenario: 'Select a document',
-  };
+  static VALIDATION_ERROR_MESSAGES =
+    CaseAssociationRequestDocumentBase.VALIDATION_ERROR_MESSAGES;
 
   getDocumentTitle = petitioners => {
     let petitionerNames;
