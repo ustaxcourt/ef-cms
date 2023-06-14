e096972d3fc53d60a08f045a0764a2ebd2582e24
Zachary Rogers <zrogers>
2023-06-14 13:44:25 -0700

    devex: Update jest-scripts config to typescript

--- scripts/jest-scripts.config.ts
+++ scripts/jest-scripts.config.ts
@@ -1,16 +1,21 @@
-const baseConfig = require('../jest.config');
-module.exports = {
-  ...baseConfig,
+import { pathsToModuleNameMapper } from 'ts-jest';
+import tsconfig from '../tsconfig.json';
+import type { Config } from 'jest';
+
+const config: Config = {
+  clearMocks: true,
   collectCoverage: true,
   collectCoverageFrom: [
     '**/*.{js,ts}',
-    '!jest-scripts.config.js',
+    '!jest-scripts.config.ts',
     '!coverage/**',
     '!set-maintenance-mode-locally.js',
     '!data-import/judge/bulkImportJudgeUsers.js',
     '!irs-super-user.js',
     '!compareTypescriptErrors.ts',
   ],
+  coverageDirectory: './coverage',
+  coverageProvider: 'babel',
   coverageThreshold: {
     global: {
       branches: 97,
@@ -19,6 +24,18 @@ module.exports = {
       statements: 99,
     },
   },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
+    prefix: '<rootDir>',
+  }),
   testEnvironment: 'node',
+  testMatch: ['**/scripts/**/?(*.)+(spec|test).[jt]s?(x)'],
+  transform: {
+    '\\.[jt]sx?$': ['babel-jest', { rootMode: 'upward' }],
+  },
+  transformIgnorePatterns: ['/node_modules/(?!uuid)'],
   verbose: false,
 };
+
+// eslint-disable-next-line import/no-default-export
+export default config;
