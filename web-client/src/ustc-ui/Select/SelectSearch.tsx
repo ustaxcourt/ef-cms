0b77c10cba2a05b899b7334238e75a66d8b5cf08
Jason Fu <jfu@flexion.us>
2023-06-16 12:42:01 -0700

    9769: refactor selectSearch to use react hooks

--- web-client/src/ustc-ui/Select/SelectSearch.tsx
+++ web-client/src/ustc-ui/Select/SelectSearch.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable import/named */
+import { connect } from '@cerebral/react';
 import { getSortedOptions } from '../../ustc-ui/Utils/selectSearchHelper';
-import React from 'react';
+import React, { useState } from 'react';
 import ReactSelect, {
   ActionMeta,
   GroupBase,
@@ -8,8 +9,22 @@ import ReactSelect, {
 } from 'react-select';
 import classNames from 'classnames';
 
-export class SelectSearch extends React.Component<
-  {
+export const SelectSearch = connect(
+  {},
+  function SelectSearch({
+    className,
+    disabled,
+    id,
+    isClearable = true,
+    name,
+    onChange,
+    onInputChange,
+    options,
+    placeholder = '- Select -',
+    searchableOptions,
+    value,
+    ...props
+  }: {
     className?: string;
     disabled?: boolean;
     id?: string;
@@ -21,74 +36,40 @@ export class SelectSearch extends React.Component<
     placeholder?: string;
     searchableOptions?: OptionsOrGroups<any, GroupBase<any>> | undefined;
     value?: any;
-  },
-  {
-    selectOptions: OptionsOrGroups<any, GroupBase<any>> | undefined;
-    inputText: string;
-  }
-> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputText: '',
-      selectOptions: props.options,
-    };
-  }
+  }) {
+    const [inputText, setInputText] = useState('');
+    const [selectOptions, setSelectOptions] = useState(options);
 
-  handleOnInputChange(inputText, { action }) {
-    if (action === 'input-change') {
-      // TODO: remove this.setState
-      this.setState({ inputText });
-      if (inputText === '') {
-        this.setState({ selectOptions: this.props.options });
-      } else if (this.props.searchableOptions) {
-        this.setState({ selectOptions: this.props.searchableOptions });
+    function handleOnInputChange(newestInputText, { action }) {
+      if (action === 'input-change') {
+        setInputText(newestInputText);
+        if (newestInputText === '') {
+          setSelectOptions(options);
+        } else if (searchableOptions) {
+          setSelectOptions(searchableOptions);
+        }
+      }
+      if (typeof onInputChange === 'function') {
+        onInputChange(newestInputText, action);
       }
     }
-    if (typeof this.props.onInputChange === 'function') {
-      this.props.onInputChange.apply(this, arguments);
-    }
-  }
 
-  render() {
-    const {
-      className,
-      disabled,
-      id,
-      isClearable = true,
-      name,
-      onChange,
-      placeholder = '- Select -',
-      value,
-    } = this.props;
-
-    let sortedOptions = getSortedOptions(
-      this.state.selectOptions,
-      this.state.inputText,
-    );
-
-    const aria = {
-      'aria-describedby': this.props['aria-describedby'],
-      'aria-disabled': disabled || this.props['aria-disabled'],
-      'aria-label': this.props['aria-label'],
-      'aria-labelledby': this.props['aria-labelledby'],
-    };
+    let sortedOptions = getSortedOptions(selectOptions, inputText);
 
     return (
       <ReactSelect
-        {...aria}
         className={classNames('select-react-element', className)}
         classNamePrefix="select-react-element"
         id={id}
         isClearable={isClearable}
-        isDisabled={aria['aria-disabled']}
+        isDisabled={disabled || props['aria-disabled']}
         name={name}
         options={sortedOptions}
         placeholder={placeholder}
         value={value}
         onChange={onChange}
-        onInputChange={this.handleOnInputChange.bind(this)}
+        onInputChange={handleOnInputChange}
       />
     );
-  }
-}
+  },
+);
