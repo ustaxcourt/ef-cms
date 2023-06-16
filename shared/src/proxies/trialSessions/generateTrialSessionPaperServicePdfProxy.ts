0182d740b83dfc8135fd9202f3eab18d9770923a
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-06-16 09:16:31 -0700

    Devex 1188: Fix some of the lint warnings

--- shared/src/proxies/trialSessions/generateTrialSessionPaperServicePdfProxy.ts
+++ shared/src/proxies/trialSessions/generateTrialSessionPaperServicePdfProxy.ts
@@ -1,12 +1,5 @@
 import { post } from '../requests';
 
-/**
- * generateTrialSessionPaperServicePdfInteractor
- * @param {object} applicationContext the application context
- * @param {object} providers the providers object
- * @param {string} providers.trialNoticePdfsKeys the trial notice PDF keys of calendared cases associated with the trial session
- * @returns {Promise<*>} the promise of the api call
- */
 export const generateTrialSessionPaperServicePdfInteractor = (
   applicationContext,
   { trialNoticePdfsKeys },
