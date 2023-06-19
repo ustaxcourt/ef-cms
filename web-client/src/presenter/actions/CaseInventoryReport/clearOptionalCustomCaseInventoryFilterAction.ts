a6243f9c6205aa7602aa12543484cbfd81137675
Jason Fu <jfu@flexion.us>
2023-06-19 10:00:01 -0700

    9769 - add new filters to work with clear filters button and reset dropdown options onblur

--- web-client/src/presenter/actions/CaseInventoryReport/clearOptionalCustomCaseInventoryFilterAction.ts
+++ web-client/src/presenter/actions/CaseInventoryReport/clearOptionalCustomCaseInventoryFilterAction.ts
@@ -9,4 +9,6 @@ import { state } from '@web-client/presenter/app.cerebral';
 export const clearOptionalCustomCaseInventoryFilterAction = ({ store }) => {
   store.set(state.customCaseInventory.filters.caseStatuses, []);
   store.set(state.customCaseInventory.filters.caseTypes, []);
+  store.set(state.customCaseInventory.filters.judges, []);
+  store.set(state.customCaseInventory.filters.preferredTrialCities, []);
 };
