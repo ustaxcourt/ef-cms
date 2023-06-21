49049c265ecc6433b3d17ada4d94b20a609ed57f
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2023-06-21 15:11:35 -0600

    moving resource setup code to main.tf

--- web-api/terraform/api/sqs.tf
+++ web-api/terraform/api/sqs.tf
@@ -29,22 +29,3 @@ resource "aws_sqs_queue" "send_emails_dl_queue" {
   name       = "send_emails_dl_queue_${var.environment}_${var.current_color}.fifo"
   fifo_queue = true
 }
-
-resource "aws_cloudwatch_metric_alarm" "send_emails_dl_queue_check" {
-  alarm_name          = "${var.environment}_${var.current_color} send emails dl queue check"
-  alarm_description   = "Alarm that triggers when a message is sent to this dl queue: ${resource.send_emails_dl_queue.sqs_queue_name}"
-  namespace           = "AWS/SQS"
-  metric_name         = "NumberOfMessagesSent"
-  comparison_operator = "GreaterThanThreshold"
-  statistic           = "Sum"
-  threshold           = 0
-  evaluation_periods  = 2
-  period              = 120
-
-  dimensions = {
-    "QueueName": resource.send_emails_dl_queue.sqs_queue_name
-  }
-
-  alarm_actions       = [data.aws_sns_topic.system_health_alarms.arn]
-}
-
