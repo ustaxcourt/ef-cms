5857d883df7f84805e2bbe1aae7d61183b2fba06
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2023-06-22 11:05:54 -0600

    10043 moving alarm setup code to main.tf for web api

--- web-client/terraform/main/main.tf
+++ web-client/terraform/main/main.tf
@@ -244,21 +244,3 @@ resource "aws_route53_health_check" "status_health_check" {
   search_string      = "false"                                 # Search for any JSON property returning "false"
   regions            = ["us-east-1", "us-west-1", "us-west-2"] # Minimum of three regions required
 }
-
-resource "aws_cloudwatch_metric_alarm" "send_emails_dl_queue_check" {
-  alarm_name          = "${var.environment}_${var.current_color} send emails dl queue check"
-  alarm_description   = "Alarm that triggers when a message is sent to this dl queue: ${resource.send_emails_dl_queue.sqs_queue_name}"
-  namespace           = "AWS/SQS"
-  metric_name         = "NumberOfMessagesSent"
-  comparison_operator = "GreaterThanThreshold"
-  statistic           = "Sum"
-  threshold           = 0
-  evaluation_periods  = 2
-  period              = 120
-
-  dimensions = {
-    "QueueName": resource.send_emails_dl_queue.sqs_queue_name
-  }
-
-  alarm_actions       = [data.aws_sns_topic.system_health_alarms.arn]
-}
