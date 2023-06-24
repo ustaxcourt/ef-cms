7a2bfe735e623f3c478948e443a010de5b5b8db1
Absolutestunna <joshujah007@gmail.com>
2023-06-24 15:49:45 -0400

    9998 - Opinions: updated persistence for fetching opinions to handle calls for all judges

--- shared/src/proxies/reports/getOpinionsFiledByJudgeProxy.ts
+++ shared/src/proxies/reports/getOpinionsFiledByJudgeProxy.ts
@@ -1,16 +1,6 @@
 import { OrdersAndOpinionTypes } from '../../../../web-client/src/presenter/judgeActivityReportState';
 import { post } from '../requests';
 
-/**
- * getOpinionsFiledByJudgeInteractor
- *
- * @param {object} applicationContext the application context
- * @param {object} providers the providers object
- * @param {object} providers.endDate the report end date
- * @param {object} providers.judgeName the judgeName to query for
- * @param {object} providers.startDate the report start date
- * @returns {Promise<*>} the promise of the api call
- */
 export const getOpinionsFiledByJudgeInteractor = (
   applicationContext,
   {
