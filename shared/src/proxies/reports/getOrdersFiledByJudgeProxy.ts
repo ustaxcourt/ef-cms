c8711759588c5821121ea4ce132984ad09b398de
Absolutestunna <joshujah007@gmail.com>
2023-06-24 16:12:50 -0400

    9998 - Orders: updated persistence for fetching Orders to handle calls for all judges

--- shared/src/proxies/reports/getOrdersFiledByJudgeProxy.ts
+++ shared/src/proxies/reports/getOrdersFiledByJudgeProxy.ts
@@ -1,23 +1,12 @@
-import { OrdersAndOpinionTypes } from '../../../../web-client/src/presenter/judgeActivityReportState';
+import {
+  JudgeActivityReportFilters,
+  OrdersAndOpinionTypes,
+} from '../../../../web-client/src/presenter/judgeActivityReportState';
 import { post } from '../requests';
 
-/**
- * getOrdersFiledByJudgeInteractor
- *
- * @param {object} applicationContext the application context
- * @param {object} providers the providers object
- * @param {object} providers.endDate the report end date
- * @param {object} providers.judgeName the judgeName to query for
- * @param {object} providers.startDate the report start date
- * @returns {Promise<*>} the promise of the api call
- */
 export const getOrdersFiledByJudgeInteractor = (
   applicationContext,
-  {
-    endDate,
-    judgeName,
-    startDate,
-  }: { startDate: string; endDate: string; judgeName: string },
+  { endDate, judgeName, startDate }: JudgeActivityReportFilters,
 ): Promise<OrdersAndOpinionTypes[]> => {
   return post({
     applicationContext,
