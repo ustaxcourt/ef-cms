86dd21bd512c07d0f51434724175587d0e7dfe80
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2023-06-26 10:36:32 -0600

    10043 moving resource creation code to main file, fixing vars

--- web-api/terraform/main/main.tf
+++ web-api/terraform/main/main.tf
@@ -26,6 +26,25 @@ data "aws_sns_topic" "system_health_alarms" {
   name = "system_health_alarms"
 }
 
+resource "aws_cloudwatch_metric_alarm" "send_emails_dl_queue_check" {
+  alarm_name          = "${var.environment}_${var.deploying_color} send emails dl queue check"
+  alarm_description   = "Alarm that triggers when a message is sent to send_emails_dl_queue_${var.environment}_${var.deploying_color}.fifo"
+  namespace           = "AWS/SQS"
+  metric_name         = "NumberOfMessagesSent"
+  comparison_operator = "GreaterThanThreshold"
+  statistic           = "Sum"
+  threshold           = 0
+  evaluation_periods  = 2
+  period              = 120
+
+  dimensions = {
+    QueueName  = "send_emails_dl_queue_${var.environment}_${var.deploying_color}.fifo"
+  }
+
+  alarm_actions       = [data.aws_sns_topic.system_health_alarms.arn]
+}
+
+
 module "ef-cms_apis" {
   source                     = "../template/"
   environment                = var.environment
