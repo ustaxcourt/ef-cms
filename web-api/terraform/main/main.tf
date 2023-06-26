0f2c4307f10baae3dfe6ce8a1ea3f9728cdc0ec4
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2023-06-26 11:29:29 -0600

    10043 standardizing alarm name

--- web-api/terraform/main/main.tf
+++ web-api/terraform/main/main.tf
@@ -27,7 +27,7 @@ data "aws_sns_topic" "system_health_alarms" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "send_emails_dl_queue_check" {
-  alarm_name          = "${var.environment}_${var.deploying_color} send emails dl queue check"
+  alarm_name          = "efcms_${var.environment}_${var.deploying_color}: SendEmails-DLQueueCheck"
   alarm_description   = "Alarm that triggers when a message is sent to send_emails_dl_queue_${var.environment}_${var.deploying_color}.fifo"
   namespace           = "AWS/SQS"
   metric_name         = "NumberOfMessagesSent"
