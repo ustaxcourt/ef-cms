8c1eeff7554b37df221a5dae91c2e0857c04b36a
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-06-27 13:47:11 -0700

    add typing to printPaperService sequence and component

--- web-client/src/views/PrintPaperTrialNotices.tsx
+++ web-client/src/views/PrintPaperTrialNotices.tsx
@@ -5,44 +5,47 @@ import { connect } from '@cerebral/react';
 import { sequences } from '@web-client/presenter/app.cerebral';
 import React from 'react';
 
-export const PrintPaperTrialNotices = connect(
-  {
-    printPaperServiceForTrialCompleteSequence:
-      sequences.printPaperServiceForTrialCompleteSequence,
-  },
-  function PrintPaperTrialNotices({
-    printPaperServiceForTrialCompleteSequence,
-  }) {
-    return (
-      <>
-        <div className="big-blue-header">
-          <div className="grid-container">
-            <div className="margin-bottom-1">
-              <h1>Print Paper Service</h1>
-            </div>
+const props = {
+  printPaperServiceForTrialCompleteSequence:
+    sequences.printPaperServiceForTrialCompleteSequence,
+};
+
+function PrintPaperTrialNoticesComponent({
+  printPaperServiceForTrialCompleteSequence,
+}: typeof props) {
+  return (
+    <>
+      <div className="big-blue-header">
+        <div className="grid-container">
+          <div className="margin-bottom-1">
+            <h1>Print Paper Service</h1>
           </div>
         </div>
-        <section className="usa-section grid-container">
-          <div className="grid-row">
-            <div className="grid-col-8">
-              <WarningNotification />
-            </div>
-            <div className="grid-col-4">
-              <Button
-                className="push-right margin-right-0 margin-top-2"
-                onClick={() => printPaperServiceForTrialCompleteSequence()}
-              >
-                Printing Complete
-              </Button>
-            </div>
+      </div>
+      <section className="usa-section grid-container">
+        <div className="grid-row">
+          <div className="grid-col-8">
+            <WarningNotification />
           </div>
-          <div className="print-trial-session-notices">
-            <PdfPreview />
+          <div className="grid-col-4">
+            <Button
+              className="push-right margin-right-0 margin-top-2"
+              onClick={() => printPaperServiceForTrialCompleteSequence()}
+            >
+              Printing Complete
+            </Button>
           </div>
-        </section>
-      </>
-    );
-  },
+        </div>
+        <div className="print-trial-session-notices">
+          <PdfPreview />
+        </div>
+      </section>
+    </>
+  );
+}
+export const PrintPaperTrialNotices = connect(
+  props,
+  PrintPaperTrialNoticesComponent,
 );
 
 PrintPaperTrialNotices.displayName = 'PrintPaperTrialNotices';
