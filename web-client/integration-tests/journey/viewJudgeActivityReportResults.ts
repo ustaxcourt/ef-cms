7ef1aa7681f84a494060f9f3f965da695152fa12
Absolutestunna <joshujah007@gmail.com>
2023-06-27 23:46:55 -0400

    9998 - added wait after submitting case

--- web-client/integration-tests/journey/viewJudgeActivityReportResults.ts
+++ web-client/integration-tests/journey/viewJudgeActivityReportResults.ts
@@ -4,7 +4,7 @@ import {
 } from '../../../shared/src/business/utilities/DateHandler';
 import { getConstants } from '../../src/getConstants';
 import { judgeActivityReportHelper as judgeActivityReportHelperComputed } from '../../src/presenter/computeds/JudgeActivityReport/judgeActivityReportHelper';
-import { refreshElasticsearchIndex } from '../helpers';
+import { refreshElasticsearchIndex, wait } from '../helpers';
 import { runCompute } from '@web-client/presenter/test.cerebral';
 import { withAppContextDecorator } from '../../src/withAppContext';
 
@@ -36,6 +36,9 @@ export const viewJudgeActivityReportResults = (
     await cerebralTest.runSequence('getCavAndSubmittedCasesForJudgesSequence', {
       selectedPage: 0,
     });
+
+    await wait(5000);
+
     const { progressDescriptionTableTotal } = runCompute(
       judgeActivityReportHelper as any,
       {
