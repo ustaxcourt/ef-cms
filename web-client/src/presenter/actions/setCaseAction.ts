d54e13620d238a09af96f26acd09c56d59103913
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2023-06-29 12:22:21 -0600

    devex 1142 removing log

--- web-client/src/presenter/actions/setCaseAction.ts
+++ web-client/src/presenter/actions/setCaseAction.ts
@@ -6,7 +6,6 @@ import { state } from '@web-client/presenter/app.cerebral';
  * @param {object} providers.props the cerebral props object containing the props.caseDetail
  * @param {object} providers.store the cerebral store used for setting the state.caseDetail
  */
-export const setCaseAction = ({ get, props, store }: ActionProps) => {
+export const setCaseAction = ({ props, store }: ActionProps) => {
   store.set(state.caseDetail, props.caseDetail);
-  console.log(get(state.featureFlags), ')))))');
 };
