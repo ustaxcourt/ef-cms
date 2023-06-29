5ae0a2beec4af21bdc4368599d6aedd721601476
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2023-06-29 13:17:23 -0600

    1142 changing how we key off of featureflags

--- shared/src/business/useCases/trialSessions/generatePrintableTrialSessionCopyReportInteractor.ts
+++ shared/src/business/useCases/trialSessions/generatePrintableTrialSessionCopyReportInteractor.ts
@@ -47,11 +47,12 @@ export const generatePrintableTrialSessionCopyReportInteractor = async (
     throw new UnauthorizedError('Unauthorized');
   }
 
-  const areUpdatedTrialSessionTypesEnabled = await applicationContext
+  const featureFlags = await applicationContext
     .getUseCases()
-    .getAllFeatureFlagsInteractor(applicationContext)[
-    ALLOWLIST_FEATURE_FLAGS.UPDATED_TRIAL_STATUS_TYPES.key
-  ];
+    .getAllFeatureFlagsInteractor(applicationContext);
+
+  const areUpdatedTrialSessionTypesEnabled =
+    featureFlags[ALLOWLIST_FEATURE_FLAGS.UPDATED_TRIAL_STATUS_TYPES.key];
 
   const pdf = await applicationContext
     .getDocumentGenerators()
