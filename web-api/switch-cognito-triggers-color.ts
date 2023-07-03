d94d6cd09eceb4f8b27f1412d413d9f38e23a9ca
Cody Seibert <cseibert113@gmail.com>
2023-07-03 13:16:01 -0400

    fixing ts error

--- web-api/switch-cognito-triggers-color.ts
+++ web-api/switch-cognito-triggers-color.ts
@@ -20,43 +20,44 @@ const cognito = new CognitoIdentityProvider({
 });
 
 const getUserPoolId = async () => {
-  const results = await cognito
-    .listUserPools({
-      MaxResults: 50,
-    })
-    .promise();
+  const results = await cognito.listUserPools({
+    MaxResults: 50,
+  });
+  if (!results) throw new Error('could not get user pool id');
+  if (!results.UserPools) throw new Error('could not get user pool id');
   const userPoolId = results.UserPools.find(
     pool => pool.Name === `efcms-${ENV}`,
-  ).Id;
+  )?.Id;
   return userPoolId;
 };
 
 const run = async () => {
   const userPoolId = await getUserPoolId();
-  const poolSettings = await cognito
-    .describeUserPool({ UserPoolId: userPoolId })
-    .promise();
+  const poolSettings = await cognito.describeUserPool({
+    UserPoolId: userPoolId,
+  });
 
-  await cognito
-    .updateUserPool({
-      AccountRecoverySetting: poolSettings.UserPool.AccountRecoverySetting,
-      AdminCreateUserConfig: omit(
-        poolSettings.UserPool.AdminCreateUserConfig,
-        'UnusedAccountValidityDays',
-      ),
-      AutoVerifiedAttributes: poolSettings.UserPool.AutoVerifiedAttributes,
-      EmailConfiguration: poolSettings.UserPool.EmailConfiguration,
-      LambdaConfig: {
-        PostAuthentication: `arn:aws:lambda:us-east-1:${AWS_ACCOUNT_ID}:function:cognito_post_authentication_lambda_${ENV}_${DEPLOYING_COLOR}`,
-        PostConfirmation: `arn:aws:lambda:us-east-1:${AWS_ACCOUNT_ID}:function:cognito_post_confirmation_lambda_${ENV}_${DEPLOYING_COLOR}`,
-      },
-      Policies: poolSettings.UserPool.Policies,
-      UserPoolId: userPoolId,
-      UserPoolTags: poolSettings.UserPool.UserPoolTags,
-      VerificationMessageTemplate:
-        poolSettings.UserPool.VerificationMessageTemplate,
-    })
-    .promise();
+  if (!poolSettings || !poolSettings.UserPool)
+    throw new Error('could not load the pool settings');
+
+  await cognito.updateUserPool({
+    AccountRecoverySetting: poolSettings.UserPool.AccountRecoverySetting,
+    AdminCreateUserConfig: omit(
+      poolSettings.UserPool.AdminCreateUserConfig,
+      'UnusedAccountValidityDays',
+    ),
+    AutoVerifiedAttributes: poolSettings.UserPool.AutoVerifiedAttributes,
+    EmailConfiguration: poolSettings.UserPool.EmailConfiguration,
+    LambdaConfig: {
+      PostAuthentication: `arn:aws:lambda:us-east-1:${AWS_ACCOUNT_ID}:function:cognito_post_authentication_lambda_${ENV}_${DEPLOYING_COLOR}`,
+      PostConfirmation: `arn:aws:lambda:us-east-1:${AWS_ACCOUNT_ID}:function:cognito_post_confirmation_lambda_${ENV}_${DEPLOYING_COLOR}`,
+    },
+    Policies: poolSettings.UserPool.Policies,
+    UserPoolId: userPoolId,
+    UserPoolTags: poolSettings.UserPool.UserPoolTags,
+    VerificationMessageTemplate:
+      poolSettings.UserPool.VerificationMessageTemplate,
+  });
 };
 
 run();
