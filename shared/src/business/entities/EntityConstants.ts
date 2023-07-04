796c4b2245a8e5efead9e091cf7a0fc980d3ec5e
Jason Fu <jfu@flexion.us>
2023-07-04 09:06:47 -0700

    10070: add backward compatibility to DSC

--- shared/src/business/entities/EntityConstants.ts
+++ shared/src/business/entities/EntityConstants.ts
@@ -26,6 +26,13 @@ export const EXHIBIT_EVENT_CODES = ['EXH', 'PTE', 'HE', 'TE', 'M123', 'STIP'];
 
 export const AMENDMENT_EVENT_CODES = ['AMAT', 'ADMT'];
 
+export const LEGACY_DOCUMENT_TYPES = [
+  {
+    documentType: 'Designation of Counsel to Receive Service',
+    eventCode: 'DSC',
+  },
+];
+
 // city, state, optional unique ID (generated automatically in testing files)
 export const TRIAL_LOCATION_MATCHER = /^[a-zA-Z ]+, [a-zA-Z ]+, [0-9]+$/;
 
@@ -1422,6 +1429,7 @@ export const ALL_EVENT_CODES = flatten([
 ])
   .map(item => item.eventCode)
   .concat(COURT_ISSUED_EVENT_CODES.map(item => item.eventCode))
+  .concat(LEGACY_DOCUMENT_TYPES.map(item => item.eventCode))
   .sort();
 
 export const ALL_DOCUMENT_TYPES_MAP = (() => {
@@ -1445,6 +1453,7 @@ export const ALL_DOCUMENT_TYPES_MAP = (() => {
     ...signedTypes,
     ...systemGeneratedTypes,
     ...minuteEntryTypes,
+    ...LEGACY_DOCUMENT_TYPES,
   ];
   return documentTypes;
 })();
