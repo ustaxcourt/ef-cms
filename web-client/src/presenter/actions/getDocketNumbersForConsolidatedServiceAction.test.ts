ed84c04961370807b24fbb14d53fd9796614f56e
Rosie Highsmith <ahighsmith@flexion.us>
2023-07-11 12:10:11 -0700

    10075: undo changes to getDocketNumbersForConsolidatedServiceAction and associated test; add a little test coverage to that action

--- web-client/src/presenter/actions/getDocketNumbersForConsolidatedServiceAction.test.ts
+++ web-client/src/presenter/actions/getDocketNumbersForConsolidatedServiceAction.test.ts
@@ -1,6 +1,5 @@
 import {
   MOCK_CASE,
-  MOCK_CASE_WITHOUT_PENDING,
   MOCK_CASE_WITH_SECONDARY_OTHERS,
 } from '../../../../shared/src/test/mockCase';
 import { applicationContextForClient as applicationContext } from '../../../../shared/src/business/test/createTestApplicationContext';
@@ -10,8 +9,6 @@ import { runAction } from '@web-client/presenter/test.cerebral';
 
 describe('getDocketNumbersForConsolidatedServiceAction', () => {
   presenter.providers.applicationContext = applicationContext;
-  const { SIMULTANEOUS_DOCUMENT_EVENT_CODES } =
-    applicationContext.getConstants();
 
   it('should return an empty list when the docket entry is multi-docketable and being filed on a non-lead case', async () => {
     const { output } = await runAction(
@@ -121,92 +118,6 @@ describe('getDocketNumbersForConsolidatedServiceAction', () => {
 
     expect(output).toEqual({
       docketNumbers: [MOCK_CASE_WITH_SECONDARY_OTHERS.docketNumber],
-    });
-  });
-
-  it('should return all the consolidated group docket numbers for a multi-docketable docket entry for a simultaneous document type', async () => {
-    const { output } = await runAction(
-      getDocketNumbersForConsolidatedServiceAction,
-      {
-        modules: {
-          presenter,
-        },
-        state: {
-          caseDetail: {
-            consolidatedCases: [
-              {
-                docketNumber: MOCK_CASE.docketNumber,
-                leadDocketNumber: MOCK_CASE.docketNumber,
-              },
-              {
-                docketNumber: MOCK_CASE_WITHOUT_PENDING.docketNumber,
-                leadDocketNumber: MOCK_CASE.docketNumber,
-              },
-              {
-                docketNumber: MOCK_CASE_WITH_SECONDARY_OTHERS.docketNumber,
-                leadDocketNumber: MOCK_CASE.docketNumber,
-              },
-            ],
-            docketNumber: MOCK_CASE.docketNumber,
-            leadDocketNumber: MOCK_CASE.docketNumber,
-          },
-          docketEntryId: MOCK_CASE.docketNumber,
-          form: {},
-          formattedCaseDetail: {
-            docketEntries: [
-              {
-                docketEntryId: MOCK_CASE.docketNumber,
-                eventCode: SIMULTANEOUS_DOCUMENT_EVENT_CODES[0],
-              },
-            ],
-          },
-          modal: {
-            form: {},
-          },
-        },
-      },
-    );
-
-    expect(output).toEqual({
-      docketNumbers: [
-        MOCK_CASE.docketNumber,
-        MOCK_CASE_WITHOUT_PENDING.docketNumber,
-        MOCK_CASE_WITH_SECONDARY_OTHERS.docketNumber,
-      ],
-    });
-  });
-
-  it('should return an empty list when the docket entry is a simultaneous doc type but is not part of a consolidated group', async () => {
-    const { output } = await runAction(
-      getDocketNumbersForConsolidatedServiceAction,
-      {
-        modules: {
-          presenter,
-        },
-        state: {
-          caseDetail: {
-            docketNumber: MOCK_CASE.docketNumber,
-            leadDocketNumber: MOCK_CASE.docketNumber,
-          },
-          docketEntryId: MOCK_CASE.docketNumber,
-          form: {},
-          formattedCaseDetail: {
-            docketEntries: [
-              {
-                docketEntryId: MOCK_CASE.docketNumber,
-                eventCode: SIMULTANEOUS_DOCUMENT_EVENT_CODES[0],
-              },
-            ],
-          },
-          modal: {
-            form: {},
-          },
-        },
-      },
-    );
-
-    expect(output).toEqual({
-      docketNumbers: [],
     });
   });
 
