0ae4cfced69cadce090471ca84a0f85e9fee8c8a
Absolutestunna <joshujah007@gmail.com>
2023-07-11 16:43:29 -0400

    9998 - removing loader around sequence

--- web-client/src/presenter/sequences/JudgeActivityReport/fetchOrdersAndOpinionsForJudgesCompleteSequence.ts
+++ web-client/src/presenter/sequences/JudgeActivityReport/fetchOrdersAndOpinionsForJudgesCompleteSequence.ts
@@ -1,7 +1,5 @@
 import { setJudgeActivityReportOrdersAndOpinionsDataAction } from '../../actions/JudgeActivityReport/setJudgeActivityReportOrdersAndOpinionsDataAction';
-import { showProgressSequenceDecorator } from '@web-client/presenter/utilities/showProgressSequenceDecorator';
 
-export const fetchOrdersAndOpinionsForJudgesCompleteSequence =
-  showProgressSequenceDecorator([
-    setJudgeActivityReportOrdersAndOpinionsDataAction,
-  ]);
+export const fetchOrdersAndOpinionsForJudgesCompleteSequence = [
+  setJudgeActivityReportOrdersAndOpinionsDataAction,
+];
