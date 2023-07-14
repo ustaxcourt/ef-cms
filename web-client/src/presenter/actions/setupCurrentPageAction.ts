51dcc06bf97c47a28bff5071a4b8ef4123862a3d
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2023-07-14 11:57:26 -0500

    delete jsDoc to trigger rerun

--- web-client/src/presenter/actions/setupCurrentPageAction.ts
+++ web-client/src/presenter/actions/setupCurrentPageAction.ts
@@ -1,17 +1,7 @@
 import { state } from '@web-client/presenter/app.cerebral';
 
-/**
- * generates an action for grabbing feature flags if needed and setting the current page
- * @param {string} page the name of the page to set
- * @returns {Promise} async action
- */
 export const setupCurrentPageAction =
   page =>
-  /**
-   * sets the state.currentPage based on the scoped page
-   * @param {object} providers the providers object
-   * @param {object} providers.store the cerebral store used for setting the state.currentPage
-   */
   async ({ applicationContext, get, store }: ActionProps) => {
     if (!get(state.featureFlags)) {
       const featureFlags = await applicationContext
