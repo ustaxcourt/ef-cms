35adf0db41a9973df03c986a6460b353baea0e15
John Cruz <jcruz@flexion.us>
2023-07-14 15:46:38 -0600

    9995 - Create delete issue modal;

---
+++ web-client/src/views/CaseWorksheet/DeletePrimaryIssueModal.tsx
@@ -0,0 +1,28 @@
+import { ConfirmModal } from '../../ustc-ui/Modal/ConfirmModal';
+import { connect } from '@cerebral/react';
+import { state } from '@web-client/presenter/app.cerebral';
+import React from 'react';
+
+export const DeletePrimaryIssueModal = connect(
+  {
+    modal: state.modal,
+    validationErrors: state.validationErrors,
+  },
+  function DeletePrimaryIssueModal() {
+    return (
+      <ConfirmModal
+        noCloseBtn
+        cancelLabel="No, Cancel"
+        confirmLabel="Yes, Delete"
+        preventCancelOnBlur={true}
+        title="Are You Sure You Want to Delete This Note?"
+        onCancelSequence="clearModalSequence"
+        onConfirmSequence="deleteCasePrimaryIssueSequence"
+      >
+        <p>This action cannot be undone.</p>
+      </ConfirmModal>
+    );
+  },
+);
+
+DeletePrimaryIssueModal.displayName = 'DeletePrimaryIssueModal';
