52ab7921e6824782cbcb6da7910d94b99254b4c7
John Cruz <jcruz@flexion.us>
2023-07-17 09:46:41 -0600

    9995 - Add test to setup modal data for AddEditPrimaryIssue modal action

---
+++ web-client/src/presenter/actions/CaseWorksheet/setAddEditPrimaryIssueModalStateAction.test.ts
@@ -0,0 +1,35 @@
+import { applicationContextForClient as applicationContext } from '../../../../../shared/src/business/test/createTestApplicationContext';
+import { presenter } from '../../presenter-mock';
+import { runAction } from '@web-client/presenter/test.cerebral';
+import { setAddEditPrimaryIssueModalStateAction } from './setAddEditPrimaryIssueModalStateAction';
+
+describe('setAddEditPrimaryIssueModalStateAction', () => {
+  presenter.providers.applicationContext = applicationContext;
+  const TEST_DOCKET_NUMBER = '999-99';
+  const TEST_PRIMARY_ISSUE = 'SOME TEST NOTES';
+
+  it('should setup modal state data for AddEditPrimaryIssue Modal', async () => {
+    const { state } = await runAction(
+      setAddEditPrimaryIssueModalStateAction as any,
+      {
+        modules: {
+          presenter,
+        },
+        props: {
+          case: {
+            docketNumber: TEST_DOCKET_NUMBER,
+            petitioners: [
+              { entityName: 'entityName1' },
+              { entityName: 'entityName2' },
+            ],
+            primaryIssue: TEST_PRIMARY_ISSUE,
+          },
+        },
+      },
+    );
+
+    expect(state.modal.docketNumber).toBe(TEST_DOCKET_NUMBER);
+    expect(state.modal.heading).toBe('Docket 999-99: entityName1,entityName2');
+    expect(state.modal.notes).toBe(TEST_PRIMARY_ISSUE);
+  });
+});
