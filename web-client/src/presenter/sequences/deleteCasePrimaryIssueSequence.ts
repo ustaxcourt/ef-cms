84390401cebaf090429c73dac458a7ece6e77894
John Cruz <jcruz@flexion.us>
2023-07-17 09:34:22 -0600

    9995 - Reuse action to fetch docket number from modal

--- web-client/src/presenter/sequences/deleteCasePrimaryIssueSequence.ts
+++ web-client/src/presenter/sequences/deleteCasePrimaryIssueSequence.ts
@@ -1,11 +1,11 @@
 import { clearModalStateAction } from '../actions/clearModalStateAction';
 import { deleteCasePrimaryIssueInDBAction } from '../actions/CaseWorksheet/deleteCasePrimaryIssueInDBAction';
 import { deleteCasePrimaryIssueInStateAction } from '../actions/CaseWorksheet/deleteCasePrimaryIssueInStateAction';
-import { getCaseDocketNumberFromModal } from '../actions/CaseWorksheet/getCaseDocketNumberFromModal';
+import { fetchUpdatedCasePrimaryIssueFromModalStateAction } from '../actions/CaseWorksheet/fetchUpdatedCasePrimaryIssueFromModalStateAction';
 import { showProgressSequenceDecorator } from '../utilities/showProgressSequenceDecorator';
 
 export const deleteCasePrimaryIssueSequence = showProgressSequenceDecorator([
-  getCaseDocketNumberFromModal,
+  fetchUpdatedCasePrimaryIssueFromModalStateAction,
   deleteCasePrimaryIssueInStateAction,
   deleteCasePrimaryIssueInDBAction,
   clearModalStateAction,
