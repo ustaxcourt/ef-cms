5b47101a95e78f7fd1fefa297167bf7e007ef8bf
Rosie Highsmith <ahighsmith@flexion.us>
2023-07-18 17:12:39 -0700

    10075: add more tests

--- web-client/src/presenter/actions/getDocketNumbersForConsolidatedServiceAction.test.ts
+++ web-client/src/presenter/actions/getDocketNumbersForConsolidatedServiceAction.test.ts
@@ -10,6 +10,14 @@ import { runAction } from '@web-client/presenter/test.cerebral';
 describe('getDocketNumbersForConsolidatedServiceAction', () => {
   presenter.providers.applicationContext = applicationContext;
 
+  const mockDocketEntryId = '1234';
+  const baseDocketEntries = [
+    {
+      docketEntryId: mockDocketEntryId,
+      documentTitle: 'cool title',
+    },
+  ];
+
   it('should return an empty list when the docket entry is multi-docketable and being filed on a non-lead case', async () => {
     const { output } = await runAction(
       getDocketNumbersForConsolidatedServiceAction,
@@ -19,9 +27,11 @@ describe('getDocketNumbersForConsolidatedServiceAction', () => {
         },
         state: {
           caseDetail: {
+            docketEntries: baseDocketEntries,
             docketNumber: MOCK_CASE_WITH_SECONDARY_OTHERS.docketNumber,
             leadDocketNumber: MOCK_CASE.docketNumber,
           },
+          docketEntryId: mockDocketEntryId,
           form: {
             eventCode: 'A',
           },
@@ -52,9 +62,92 @@ describe('getDocketNumbersForConsolidatedServiceAction', () => {
         },
         state: {
           caseDetail: {
+            docketEntries: baseDocketEntries,
             docketNumber: MOCK_CASE.docketNumber,
             leadDocketNumber: MOCK_CASE.docketNumber,
           },
+          docketEntryId: mockDocketEntryId,
+          form: {
+            eventCode: 'PSDE',
+          },
+          modal: {
+            form: {
+              consolidatedCasesToMultiDocketOn: [
+                {
+                  checked: true,
+                  docketNumber: MOCK_CASE_WITH_SECONDARY_OTHERS.docketNumber,
+                  leadDocketNumber: MOCK_CASE.docketNumber,
+                },
+              ],
+            },
+          },
+        },
+      },
+    );
+
+    expect(output.docketNumbers).toEqual([]);
+  });
+
+  it('should return an empty list when the docket entry is a simultaneous document', async () => {
+    const { output } = await runAction(
+      getDocketNumbersForConsolidatedServiceAction,
+      {
+        modules: {
+          presenter,
+        },
+        state: {
+          caseDetail: {
+            docketEntries: [
+              {
+                docketEntryId: mockDocketEntryId,
+                documentTitle: 'a title',
+                eventCode: 'SIAB',
+              },
+            ],
+            docketNumber: MOCK_CASE.docketNumber,
+            leadDocketNumber: MOCK_CASE.docketNumber,
+          },
+          docketEntryId: mockDocketEntryId,
+          form: {
+            eventCode: 'PSDE',
+          },
+          modal: {
+            form: {
+              consolidatedCasesToMultiDocketOn: [
+                {
+                  checked: true,
+                  docketNumber: MOCK_CASE_WITH_SECONDARY_OTHERS.docketNumber,
+                  leadDocketNumber: MOCK_CASE.docketNumber,
+                },
+              ],
+            },
+          },
+        },
+      },
+    );
+
+    expect(output.docketNumbers).toEqual([]);
+  });
+
+  it('should return an empty list when the docket entry has "Simultaneous" in the doc title', async () => {
+    const { output } = await runAction(
+      getDocketNumbersForConsolidatedServiceAction,
+      {
+        modules: {
+          presenter,
+        },
+        state: {
+          caseDetail: {
+            docketEntries: [
+              {
+                docketEntryId: mockDocketEntryId,
+                documentTitle: 'Simultaneous!!!',
+              },
+            ],
+            docketNumber: MOCK_CASE.docketNumber,
+            leadDocketNumber: MOCK_CASE.docketNumber,
+          },
+          docketEntryId: mockDocketEntryId,
           form: {
             eventCode: 'PSDE',
           },
@@ -85,9 +178,11 @@ describe('getDocketNumbersForConsolidatedServiceAction', () => {
         },
         state: {
           caseDetail: {
+            docketEntries: baseDocketEntries,
             docketNumber: MOCK_CASE.docketNumber,
             leadDocketNumber: MOCK_CASE.docketNumber,
           },
+          docketEntryId: mockDocketEntryId,
           form: {
             eventCode: 'A',
           },
@@ -130,9 +225,11 @@ describe('getDocketNumbersForConsolidatedServiceAction', () => {
         },
         state: {
           caseDetail: {
+            docketEntries: baseDocketEntries,
             docketNumber: MOCK_CASE_WITH_SECONDARY_OTHERS.docketNumber,
             leadDocketNumber: MOCK_CASE.docketNumber,
           },
+          docketEntryId: mockDocketEntryId,
           form: {
             eventCode: 'A',
           },
