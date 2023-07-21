9d1107737e7e1cece517ed0523e2fd8c7e5d80d2
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2023-07-21 07:08:19 -0500

    10069: add alarm for east/west health checks

--- web-client/terraform/main/main.tf
+++ web-client/terraform/main/main.tf
@@ -212,7 +212,7 @@ resource "aws_route53_health_check" "ui_health_check" {
   regions           = ["us-east-1", "us-west-1", "us-west-2"] # Minimum of three regions required
 }
 
-resource "aws_cloudwatch_metric_alarm" "status_health_check" {
+resource "aws_cloudwatch_metric_alarm" "status_health_check_east" {
   alarm_name          = "${var.dns_domain} health check endpoint"
   namespace           = "AWS/Route53"
   metric_name         = "HealthCheckStatus"
@@ -225,6 +225,26 @@ resource "aws_cloudwatch_metric_alarm" "status_health_check" {
 
   dimensions = {
     HealthCheckId = aws_route53_health_check.status_health_check_east[0].id
+  }
+
+  alarm_actions             = [data.aws_sns_topic.system_health_alarms.arn]
+  insufficient_data_actions = [data.aws_sns_topic.system_health_alarms.arn]
+  ok_actions                = [data.aws_sns_topic.system_health_alarms.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "status_health_check_west" {
+  alarm_name          = "${var.dns_domain} health check endpoint"
+  namespace           = "AWS/Route53"
+  metric_name         = "HealthCheckStatus"
+  comparison_operator = "LessThanThreshold"
+  statistic           = "Minimum"
+  count               = var.enable_health_checks
+  threshold           = "1"
+  evaluation_periods  = "2"
+  period              = "60"
+
+  dimensions = {
+    HealthCheckId = aws_route53_health_check.status_health_check_west[0].id
   }
 
   alarm_actions             = [data.aws_sns_topic.system_health_alarms.arn]
