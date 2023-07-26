609c178cf8fe097cd88d107f5a3af75e544885e7
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2023-07-26 08:20:34 -0500

    10069: unit test fix for now

--- shared/src/persistence/ses/getSesStatus.test.ts
+++ shared/src/persistence/ses/getSesStatus.test.ts
@@ -1,6 +1,14 @@
 import { getSesStatus } from './getSesStatus';
+import NodeCache from 'node-cache';
+
+// const mockNodeCache = new NodeCache();
+jest.mock('node-cache');
+// mockNodeCache.get.mockReturnValue(true);
 
 describe('getSesStatus', () => {
+  beforeEach(() => {
+    NodeCache.get = jest.fn().mockReturnValue(undefined);
+  });
   it('should pass when no rejects have occurred', async () => {
     const applicationContext: any = {
       getEmailClient: () => ({
@@ -21,7 +29,6 @@ describe('getSesStatus', () => {
     const status = await getSesStatus({
       applicationContext,
     });
-
     expect(status).toBeTruthy();
   });
 
