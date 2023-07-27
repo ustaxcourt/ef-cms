4034cf69d8899d044805aa8ad2f310e889f1d1e7
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-07-27 11:59:32 -0700

    10076 - clean up duplicated test

--- web-client/integration-tests/amendedAndRedactedBriefVisibilityJourney.test.ts
+++ web-client/integration-tests/amendedAndRedactedBriefVisibilityJourney.test.ts
@@ -1,8 +1,6 @@
 import { formattedDocketEntries as formattedDocketEntriesComputed } from '../src/presenter/computeds/formattedDocketEntries';
 import { loginAs, setupTest } from './helpers';
-import { publicCaseDetailHelper as publicCaseDetailHelperComputed } from '../src/presenter/computeds/Public/publicCaseDetailHelper';
 import { runCompute } from '@web-client/presenter/test.cerebral';
-import { setupTest as setupPublicTest } from '../integration-tests-public/helpers';
 import { withAppContextDecorator } from '../src/withAppContext';
 
 describe('Amended And Redacted Brief Visibility Journey', () => {
@@ -153,69 +151,4 @@ describe('Amended And Redacted Brief Visibility Journey', () => {
       }
     });
   });
-
-  describe('Public', () => {
-    const cerebralPublicTest = setupPublicTest();
-
-    const publicCaseDetailHelper = withAppContextDecorator(
-      publicCaseDetailHelperComputed,
-    );
-
-    const expectedDocketRecordVisibility = {
-      1: false,
-      2: false,
-      3: false,
-      4: false,
-      5: true,
-      6: true,
-      7: false,
-      8: true,
-      9: false,
-      10: true,
-      11: false,
-      12: true,
-      13: false,
-      14: true,
-      15: false,
-      16: false,
-      17: true,
-      18: true,
-      19: false,
-      20: true,
-      21: true,
-      22: false,
-      23: true,
-      24: true,
-      25: true,
-      26: false,
-      27: false,
-      28: false,
-      29: false,
-      30: false,
-    };
-
-    it('view case detail', async () => {
-      await cerebralPublicTest.runSequence('gotoPublicCaseDetailSequence', {
-        docketNumber: '105-23',
-      });
-
-      const publicCaseDetail = runCompute(publicCaseDetailHelper, {
-        state: cerebralPublicTest.getState(),
-      });
-
-      const { formattedDocketEntriesOnDocketRecord } = publicCaseDetail;
-
-      for (const [docketRecordIndex, isLinked] of Object.entries(
-        expectedDocketRecordVisibility,
-      )) {
-        const found = formattedDocketEntriesOnDocketRecord.find(
-          entry => entry.index === +docketRecordIndex,
-        );
-
-        expect(found?.showLinkToDocument).toBe(isLinked);
-      }
-    });
-  });
-
-  describe.skip('Terminal user', () => {});
 });
