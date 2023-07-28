e65d11dc4c4719c26e913b93962b3c9eee1f3295
Jason Fu <jfu@flexion.us>
2023-07-28 15:42:24 -0700

    9995 - remove console logs

--- web-client/src/presenter/actions/CaseWorksheet/updateSubmittedCavCaseDetailAction.ts
+++ web-client/src/presenter/actions/CaseWorksheet/updateSubmittedCavCaseDetailAction.ts
@@ -1,18 +1,10 @@
 import { state } from '@web-client/presenter/app.cerebral';
 
 export const updateSubmittedCavCaseDetailInStateAction = ({
-  get,
   props,
   store,
 }: ActionProps) => {
   const { finalBriefDueDate, statusOfMatter } = props;
-  console.log('finalBriefDueDate:', finalBriefDueDate);
-
-  const submittedAndCavCasesByJudge: any[] = get(
-    state.judgeActivityReportData.submittedAndCavCasesByJudge,
-  );
-
-  console.log('before:', submittedAndCavCasesByJudge);
 
   if (finalBriefDueDate) {
     store.set(
@@ -29,10 +21,4 @@ export const updateSubmittedCavCaseDetailInStateAction = ({
       statusOfMatter,
     );
   }
-
-  const submittedAndCavCasesByJudge2 = get(
-    state.judgeActivityReportData.submittedAndCavCasesByJudge,
-  );
-
-  console.log('after:', submittedAndCavCasesByJudge2);
 };
