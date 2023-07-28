e73b61de13857bd89a6df97a082f6f02363cb4b9
Jason Fu <jfu@flexion.us>
2023-07-28 13:50:27 -0700

    9995 - set dropdown values in state

--- web-client/src/presenter/sequences/updateSubmittedCavCaseDetailSequence.ts
+++ web-client/src/presenter/sequences/updateSubmittedCavCaseDetailSequence.ts
@@ -1,16 +1,17 @@
-import { clearAlertsAction } from '../actions/clearAlertsAction';
-import { setAlertErrorAction } from '../actions/setAlertErrorAction';
-import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
-import { startShowValidationAction } from '../actions/startShowValidationAction';
-import { updateSubmittedCavCaseDetailAction } from '../actions/updateSubmittedCavCaseDetailAction';
-import { validateSubmittedCavCaseDetailAction } from '../actions/validateSubmittedCavCaseDetailAction';
+// import { clearAlertsAction } from '../actions/clearAlertsAction';
+// import { setAlertErrorAction } from '../actions/setAlertErrorAction';
+// import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
+// import { startShowValidationAction } from '../actions/startShowValidationAction';
+import { updateSubmittedCavCaseDetailInStateAction } from '../actions/CaseWorksheet/updateSubmittedCavCaseDetailAction';
+// import { validateSubmittedCavCaseDetailAction } from '../actions/CaseWorksheet/validateSubmittedCavCaseDetailAction';
 
 export const updateSubmittedCavCaseDetailSequence = [
-  clearAlertsAction,
-  startShowValidationAction,
-  validateSubmittedCavCaseDetailAction,
-  {
-    error: [setAlertErrorAction, setValidationErrorsAction],
-    success: [updateSubmittedCavCaseDetailAction],
-  },
+  //   clearAlertsAction,
+  //   startShowValidationAction,
+  //   validateSubmittedCavCaseDetailAction,
+  //   {
+  //     error: [setAlertErrorAction, setValidationErrorsAction],
+  //     success: [updateSubmittedCavCaseDetailAction],
+  //   },
+  updateSubmittedCavCaseDetailInStateAction,
 ];
