d3bd04ebdb05991d8f914195ed196cfe7572f347
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2023-07-31 18:47:37 -0500

    10116: WIP: importing web-client applicationContext to enable calling proxy from api (gross)

--- shared/src/proxies/health/getHealthCheckAndSetCacheProxy.ts
+++ shared/src/proxies/health/getHealthCheckAndSetCacheProxy.ts
@@ -1,6 +1,7 @@
+import { applicationContext } from '../../../../web-client/src/applicationContext';
 import { post } from '../requests';
 
-export const getHealthCheckAndSetCacheProxy = applicationContext => {
+export const getHealthCheckAndSetCacheProxy = () => {
   return post({
     applicationContext,
     endpoint: '/async/public-api/health-check-cache',
