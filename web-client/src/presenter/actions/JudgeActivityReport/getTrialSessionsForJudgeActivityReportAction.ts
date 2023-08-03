0d5491e42cdffbb4a6cab107e6a35a73b2976ff5
Jason Fu <jfu@flexion.us>
2023-08-03 16:20:31 -0700

    9998 - update import paths again again

--- web-client/src/presenter/actions/JudgeActivityReport/getTrialSessionsForJudgeActivityReportAction.ts
+++ web-client/src/presenter/actions/JudgeActivityReport/getTrialSessionsForJudgeActivityReportAction.ts
@@ -1,4 +1,4 @@
-import { ID_FOR_ALL_JUDGES } from 'shared/src/business/useCases/judgeActivityReport/getTrialSessionsForJudgeActivityReportInteractor';
+import { ID_FOR_ALL_JUDGES } from '../../../../../shared/src/business/useCases/judgeActivityReport/getTrialSessionsForJudgeActivityReportInteractor';
 import { state } from '@web-client/presenter/app.cerebral';
 
 export const getTrialSessionsForJudgeActivityReportAction = async ({
