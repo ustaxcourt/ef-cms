5e2e5cb6121b690c5b377b0b03bcebc328843bc5
Mike Marcotte <mike.marcotte@gmail.com>
2023-08-04 11:01:36 -0400

    use sso login to get aws credentials

--- scripts/env/unset-env.zsh
+++ scripts/env/unset-env.zsh
@@ -2,8 +2,7 @@
 
 # unset everything in .env
 ENV_KEYS=$(awk -F'=' '{print $1}' .env)
-AWS_ENV_KEYS=$(grep "=" ./scripts/env/aws-accounts/example.env | awk -F'[ =]' '{print $2}')
-KEYS="${ENV_KEYS}\n${AWS_ENV_KEYS}"
+KEYS="${ENV_KEYS}"
 
 echo "$KEYS" | while read -r key; do
    unset "$key"
