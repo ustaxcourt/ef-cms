7f5e8954f53f9717e4a3cedc9893f3a16845fe4a
Cody Seibert <cseibert113@gmail.com>
2023-08-04 10:35:04 -0400

    10113 - adding in missing users after test finishes

--- web-client/integration-tests/consolidatedGroupAccessJourney.test.ts
+++ web-client/integration-tests/consolidatedGroupAccessJourney.test.ts
@@ -1,4 +1,5 @@
 import { caseDetailHeaderHelper as caseDetailHeaderComputed } from '../src/presenter/computeds/caseDetailHeaderHelper';
+import { createUsers } from '../../web-api/storage/scripts/createUsers';
 import { formattedDocketEntries as formattedDocketEntriesComputed } from '../src/presenter/computeds/formattedDocketEntries';
 import { loginAs, setupTest } from './helpers';
 import { runCompute } from '@web-client/presenter/test.cerebral';
@@ -26,6 +27,7 @@ describe('User accesses a case that does not belong to them, but is part of a co
   afterAll(async () => {
     cerebralTest.closeSocket();
     await seedFullDataset();
+    await createUsers();
   });
 
   describe('Petitioner', () => {
