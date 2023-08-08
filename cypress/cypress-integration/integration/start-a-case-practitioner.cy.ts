64ba70ae07d7f0d95744250080e719c2b83499b5
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-08-08 09:26:39 -0700

    10113: Don't expect hardcoded open cases for practitioner, makes test very brittle

--- cypress/cypress-integration/integration/start-a-case-practitioner.cy.ts
+++ cypress/cypress-integration/integration/start-a-case-practitioner.cy.ts
@@ -6,21 +6,14 @@ import {
 } from '../support/pages/dashboard-practitioner';
 
 describe('Start a case as a practitioner', () => {
-  it('go to the practitioner dashboard and expect that a case list table is displayed with eight cases', () => {
+  it('go to the practitioner dashboard, file a case, and expect case count to incrememt by one', () => {
     navigateToDashboard('privatepractitioner');
-    getCaseList().should('have.length', 12);
-  });
 
-  it('click the start a case button', () => {
-    getStartCaseButton().click();
-  });
-
-  it('fills in the start a case form', () => {
-    fillInAndSubmitForm();
-  });
-
-  it('expect the case list to be displayed with nine items now', () => {
-    getCaseList().should('exist');
-    getCaseList().should('have.length', 13);
+    getCaseList().then(cases => {
+      getStartCaseButton().click();
+      fillInAndSubmitForm();
+      getCaseList().should('exist');
+      getCaseList().should('have.length', cases.length + 1);
+    });
   });
 });
