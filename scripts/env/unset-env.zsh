e6d42317e3c0fe13a6da69c3f5be878f81b8b2b0
Mike Marcotte <mike.marcotte@gmail.com>
2023-08-08 17:06:35 -0400

    ustc-3755: adjust comments; fix shebang

--- scripts/env/unset-env.zsh
+++ scripts/env/unset-env.zsh
@@ -4,7 +4,7 @@
 ENV_KEYS=$(awk -F'=' '{print $1}' .env)
 KEYS="${ENV_KEYS}"
 
-unset AWS_SESSION_TOKEN AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY
+unset AWS_SESSION_TOKEN AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_PROFILE
 
 echo "$KEYS" | while read -r key; do
    unset "$key"
