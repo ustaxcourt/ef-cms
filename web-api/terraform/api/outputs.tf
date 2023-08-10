a9fb170fee9a04cf6516ee9effedd28ffe9d8eff
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2023-08-10 17:46:52 -0500

    10116: general tf refactoring for aesthetic and functional value

--- web-api/terraform/api/outputs.tf
+++ web-api/terraform/api/outputs.tf
@@ -1,10 +1,10 @@
 output "api_public_custom" {
-  value = aws_api_gateway_domain_name.api_public_custom
+  value     = aws_api_gateway_domain_name.api_public_custom
   sensitive = true
 }
 
 output "api_custom" {
-  value = aws_api_gateway_domain_name.api_custom
+  value     = aws_api_gateway_domain_name.api_custom
   sensitive = true
 }
 
@@ -17,5 +17,5 @@ output "puppeteer_layer_arn" {
 }
 
 output "health_check_id" {
-  value =  length(aws_route53_health_check.status_health_check) > 0 ?  aws_route53_health_check.status_health_check[0].id : null
+  value = length(aws_route53_health_check.failover_health_check) > 0 ? aws_route53_health_check.failover_health_check[0].id : null
 }
