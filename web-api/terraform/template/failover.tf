875a2c6976043ca137e66cc0bf21abeb72822194
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2023-08-15 09:38:15 -0500

    10116: remember to add third parameter to split() function (temporarily)

--- web-api/terraform/template/failover.tf
+++ web-api/terraform/template/failover.tf
@@ -52,7 +52,7 @@ resource "aws_route53_health_check" "failover_health_check_east" {
   // Therefore, we are addding the stage ("/exp1") to the resource path, and omitting ("https://") from the fqdn
   // e.g: https://6oz2qiqb7h.execute-api.us-east-1.amazonaws.com/exp1 --> 6oz2qiqb7h.execute-api.us-east-1.amazonaws.com
   reference_name     = "${var.environment} East Health Check"
-  fqdn               = element(split("/", module.api-east-green.public_api_invoke_url))
+  fqdn               = element(split("/", module.api-east-green.public_api_invoke_url), 2)
   port               = 443
   type               = "HTTPS_STR_MATCH"
   resource_path      = "${var.environment}/public-api/cached-health"
@@ -72,7 +72,7 @@ resource "aws_route53_health_check" "failover_health_check_west" {
   // Therefore, we are addding the stage ("/exp1") to the resource path, and omitting ("https://") from the fqdn
   // e.g: https://6oz2qiqb7h.execute-api.us-east-1.amazonaws.com/exp1 --> 6oz2qiqb7h.execute-api.us-east-1.amazonaws.com
   reference_name     = "${var.environment} West Health Check"
-  fqdn               = element(split("/", module.api-west-green.public_api_invoke_url))
+  fqdn               = element(split("/", module.api-west-green.public_api_invoke_url), 2)
   port               = 443
   type               = "HTTPS_STR_MATCH"
   resource_path      = "${var.environment}/public-api/cached-health"
