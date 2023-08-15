046c69f557ffd593ae99f026797562f976a27be5
Zachary Rogers <zrogers>
2023-08-15 14:18:14 -0700

    10116: remove api. domain as we use color based apis

--- web-api/terraform/template/main-west.tf
+++ web-api/terraform/template/main-west.tf
@@ -291,35 +291,6 @@ data "aws_s3_bucket_object" "cron_green_west_object" {
   provider   = aws.us-west-1
 }
 
-resource "aws_api_gateway_domain_name" "api_custom_main_west" {
-  depends_on               = [aws_acm_certificate.api_gateway_cert_west]
-  regional_certificate_arn = aws_acm_certificate.api_gateway_cert_west.arn
-  domain_name              = "api.${var.dns_domain}"
-  security_policy          = "TLS_1_2"
-  provider                 = aws.us-west-1
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-}
-
-
-resource "aws_route53_record" "api_route53_main_west_regional_record" {
-  name           = aws_api_gateway_domain_name.api_custom_main_west.domain_name
-  type           = "A"
-  zone_id        = data.aws_route53_zone.zone.id
-  set_identifier = "api_main_us_west_1"
-
-  alias {
-    name                   = aws_api_gateway_domain_name.api_custom_main_west.regional_domain_name
-    zone_id                = aws_api_gateway_domain_name.api_custom_main_west.regional_zone_id
-    evaluate_target_health = true
-  }
-
-  latency_routing_policy {
-    region = "us-west-1"
-  }
-}
-
 module "api-west-waf" {
   environment = var.environment
   providers = {
