24c2012ea81f6cffad5c02b337293eddcf28cf2b
Zachary Rogers <zrogers>
2023-08-17 13:14:31 -0700

    Revert "10116: Remove public-api. resources as that endpoint is unused."
    This reverts commit 2250c794aff7894d4a5b54147887f107250e3a6f.

--- web-api/terraform/template/main-east.tf
+++ web-api/terraform/template/main-east.tf
@@ -531,6 +531,16 @@ data "aws_dynamodb_table" "blue_dynamo_table" {
   name = var.blue_table_name
 }
 
+resource "aws_api_gateway_domain_name" "public_api_custom_main_east" {
+  depends_on               = [aws_acm_certificate.api_gateway_cert_east]
+  regional_certificate_arn = aws_acm_certificate.api_gateway_cert_east.arn
+  domain_name              = "public-api.${var.dns_domain}"
+  security_policy          = "TLS_1_2"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
 resource "aws_api_gateway_domain_name" "api_custom_main_east" {
   depends_on               = [aws_acm_certificate.api_gateway_cert_east]
   regional_certificate_arn = aws_acm_certificate.api_gateway_cert_east.arn
@@ -550,6 +560,23 @@ resource "aws_route53_record" "api_route53_main_east_regional_record" {
   alias {
     name                   = aws_api_gateway_domain_name.api_custom_main_east.regional_domain_name
     zone_id                = aws_api_gateway_domain_name.api_custom_main_east.regional_zone_id
+    evaluate_target_health = true
+  }
+
+  latency_routing_policy {
+    region = "us-east-1"
+  }
+}
+
+resource "aws_route53_record" "public_api_route53_main_east_regional_record" {
+  name           = aws_api_gateway_domain_name.public_api_custom_main_east.domain_name
+  type           = "A"
+  zone_id        = data.aws_route53_zone.zone.id
+  set_identifier = "public_api_main_us_east_1"
+
+  alias {
+    name                   = aws_api_gateway_domain_name.public_api_custom_main_east.regional_domain_name
+    zone_id                = aws_api_gateway_domain_name.public_api_custom_main_east.regional_zone_id
     evaluate_target_health = true
   }
 
