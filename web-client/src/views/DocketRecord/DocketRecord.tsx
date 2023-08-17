9709a7af5d659e2b444d668d871d2d3908c4e063
Cody Seibert <cseibert113@gmail.com>
2023-08-17 17:33:26 -0400

    10106 - improve the authenticate docket record & abstract common components

--- web-client/src/views/DocketRecord/DocketRecord.tsx
+++ web-client/src/views/DocketRecord/DocketRecord.tsx
@@ -3,6 +3,7 @@ import { DocketRecordHeader } from './DocketRecordHeader';
 import { DocketRecordOverlay } from './DocketRecordOverlay';
 import { FilingsAndProceedings } from '../DocketRecord/FilingsAndProceedings';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { NonPhone, Phone } from '@web-client/ustc-ui/Responsive/Responsive';
 import { SealDocketEntryModal } from './SealDocketEntryModal';
 import { UnsealDocketEntryModal } from './UnsealDocketEntryModal';
 import { connect } from '@cerebral/react';
@@ -32,131 +33,179 @@ export const DocketRecord = connect(
     return (
       <>
         <DocketRecordHeader />
-        <table
-          aria-label="docket record"
-          className="usa-table case-detail ustc-table responsive-table"
-          id="docket-record-table"
-        >
-          <thead>
-            <tr>
-              <th className="center-column">
-                <span>
-                  <span className="usa-sr-only">Number</span>
-                  <span aria-hidden="true">No.</span>
-                </span>
-              </th>
-              <th>Filed Date</th>
-              <th className="center-column">Event</th>
-              <th aria-hidden="true" className="icon-column" />
-              <th>Filings and Proceedings</th>
-              <th>Pages</th>
-              <th>Filed By</th>
-              <th>Action</th>
-              <th>Served</th>
-              <th className="center-column">Parties</th>
-              {docketRecordHelper.showEditOrSealDocketRecordEntry && (
-                <th>&nbsp;</th>
-              )}
-            </tr>
-          </thead>
-          <tbody>
-            {formattedDocketEntries.formattedDocketEntriesOnDocketRecord.map(
-              (entry, arrayIndex) => {
-                return (
-                  <tr
-                    className={classNames(
-                      entry.isInProgress && 'in-progress',
-                      entry.qcWorkItemsUntouched && 'qc-untouched',
-                    )}
-                    key={entry.docketEntryId}
-                  >
-                    <td className="center-column hide-on-mobile">
-                      {entry.index}
-                    </td>
-                    <td>
-                      <span
+
+        <NonPhone>
+          <div style={{ overflowX: 'scroll', width: '100%' }}>
+            <table
+              aria-label="docket record"
+              className="usa-table ustc-table usa-table--stacked"
+              id="docket-record-table"
+            >
+              <thead>
+                <tr>
+                  <th className="center-column hide-on-mobile">
+                    <span>
+                      <span className="usa-sr-only">Number</span>
+                      <span aria-hidden="true">No.</span>
+                    </span>
+                  </th>
+                  <th>Filed Date</th>
+                  <th className="center-column hide-on-mobile">Event</th>
+                  <th aria-hidden="true" className="icon-column" />
+                  <th>Filings and Proceedings</th>
+                  <th className="hide-on-mobile">Pages</th>
+                  <th className="hide-on-mobile">Filed By</th>
+                  <th className="hide-on-mobile">Action</th>
+                  <th>Served</th>
+                  <th className="center-column hide-on-mobile">Parties</th>
+                  {docketRecordHelper.showEditOrSealDocketRecordEntry && (
+                    <th>&nbsp;</th>
+                  )}
+                </tr>
+              </thead>
+              <tbody>
+                {formattedDocketEntries.formattedDocketEntriesOnDocketRecord.map(
+                  (entry, arrayIndex) => {
+                    return (
+                      <tr
                         className={classNames(
-                          entry.isStricken && 'stricken-docket-record',
-                          'no-wrap',
+                          entry.isInProgress && 'in-progress',
+                          entry.qcWorkItemsUntouched && 'qc-untouched',
                         )}
+                        key={entry.docketEntryId}
                       >
-                        {entry.createdAtFormatted}
-                      </span>
-                    </td>
-                    <td className="center-column hide-on-mobile">
-                      {entry.eventCode}
-                    </td>
-                    <td aria-hidden="true" className="filing-type-icon">
-                      {entry.iconsToDisplay.map(iconInfo => (
-                        <FontAwesomeIcon key={iconInfo.icon} {...iconInfo} />
-                      ))}
-                    </td>
-                    <td>
-                      <FilingsAndProceedings
-                        arrayIndex={arrayIndex}
-                        entry={entry}
-                      />
-                    </td>
-                    <td className="hide-on-mobile number-of-pages">
-                      {entry.numberOfPages}
-                    </td>
-                    <td className="hide-on-mobile">{entry.filedBy}</td>
-                    <td className="hide-on-mobile">{entry.action}</td>
-                    <td className="hide-on-mobile">
-                      {entry.showNotServed && (
-                        <span className="text-semibold not-served">
-                          Not served
-                        </span>
-                      )}
-                      {entry.showServed && (
-                        <span>{entry.servedAtFormatted}</span>
-                      )}
-                    </td>
-                    <td className="center-column hide-on-mobile">
-                      <span className="responsive-label">Parties</span>
-                      {entry.showServed && entry.servedPartiesCode}
-                    </td>
-                    {docketRecordHelper.showEditOrSealDocketRecordEntry && (
-                      <td>
-                        {entry.showEditDocketRecordEntry && (
-                          <Button
-                            link
-                            href={entry.editDocketEntryMetaLink}
-                            icon="edit"
+                        <td className="center-column hide-on-mobile">
+                          {entry.index}
+                        </td>
+                        <td>
+                          <span
+                            className={classNames(
+                              entry.isStricken && 'stricken-docket-record',
+                              'no-wrap',
+                            )}
                           >
-                            Edit
-                          </Button>
+                            {entry.createdAtFormatted}
+                          </span>
+                        </td>
+                        <td className="center-column hide-on-mobile">
+                          {entry.eventCode}
+                        </td>
+                        <td aria-hidden="true" className="filing-type-icon">
+                          {entry.iconsToDisplay.map(iconInfo => (
+                            <FontAwesomeIcon
+                              key={iconInfo.icon}
+                              {...iconInfo}
+                            />
+                          ))}
+                        </td>
+                        <td>
+                          <FilingsAndProceedings
+                            arrayIndex={arrayIndex}
+                            entry={entry}
+                          />
+                        </td>
+                        <td className="hide-on-mobile number-of-pages">
+                          {entry.numberOfPages}
+                        </td>
+                        <td className="hide-on-mobile">{entry.filedBy}</td>
+                        <td className="hide-on-mobile">{entry.action}</td>
+                        <td>
+                          {entry.showNotServed && (
+                            <span className="text-semibold not-served">
+                              Not served
+                            </span>
+                          )}
+                          {entry.showServed && (
+                            <span>{entry.servedAtFormatted}</span>
+                          )}
+                        </td>
+                        <td className="center-column hide-on-mobile">
+                          <span className="responsive-label">Parties</span>
+                          {entry.showServed && entry.servedPartiesCode}
+                        </td>
+                        {docketRecordHelper.showEditOrSealDocketRecordEntry && (
+                          <td>
+                            {entry.showEditDocketRecordEntry && (
+                              <Button
+                                link
+                                href={entry.editDocketEntryMetaLink}
+                                icon="edit"
+                              >
+                                Edit
+                              </Button>
+                            )}
+                            {entry.showSealDocketRecordEntry && (
+                              <Button
+                                link
+                                className={entry.isSealed && 'red-warning'}
+                                data-test={`seal-docket-entry-button-${arrayIndex}`}
+                                icon={entry.sealIcon}
+                                tooltip={entry.sealButtonTooltip}
+                                onClick={() => {
+                                  entry.isSealed
+                                    ? openUnsealDocketEntryModalSequence({
+                                        docketEntryId: entry.docketEntryId,
+                                        showModal: 'UnsealDocketEntryModal',
+                                      })
+                                    : openSealDocketEntryModalSequence({
+                                        docketEntryId: entry.docketEntryId,
+                                        showModal: 'SealDocketEntryModal',
+                                      });
+                                }}
+                              >
+                                {entry.sealButtonText}
+                              </Button>
+                            )}
+                          </td>
                         )}
-                        {entry.showSealDocketRecordEntry && (
-                          <Button
-                            link
-                            className={entry.isSealed && 'red-warning'}
-                            data-test={`seal-docket-entry-button-${arrayIndex}`}
-                            icon={entry.sealIcon}
-                            tooltip={entry.sealButtonTooltip}
-                            onClick={() => {
-                              entry.isSealed
-                                ? openUnsealDocketEntryModalSequence({
-                                    docketEntryId: entry.docketEntryId,
-                                    showModal: 'UnsealDocketEntryModal',
-                                  })
-                                : openSealDocketEntryModalSequence({
-                                    docketEntryId: entry.docketEntryId,
-                                    showModal: 'SealDocketEntryModal',
-                                  });
-                            }}
-                          >
-                            {entry.sealButtonText}
-                          </Button>
+                      </tr>
+                    );
+                  },
+                )}
+              </tbody>
+            </table>
+          </div>
+        </NonPhone>
+
+        <Phone>
+          <table className="usa-table usa-table--stacked-header usa-table--borderless">
+            <thead>
+              <tr>
+                <th scope="col">Filed Date</th>
+                <th scope="col">Filings and Proceedings</th>
+                <th scope="col">Served</th>
+              </tr>
+            </thead>
+            <tbody>
+              {formattedDocketEntries.formattedDocketEntriesOnDocketRecord.map(
+                entry => {
+                  return (
+                    <tr key={entry.index}>
+                      <td data-label="Filings and Proceedings">
+                        <FilingsAndProceedings entry={entry} />
+                      </td>
+                      <td data-label="Filed date">
+                        {entry.createdAtFormatted}
+                      </td>
+                      <td data-label="Served">
+                        {entry.showNotServed && (
+                          <span className="text-secondary text-semibold">
+                            Not served
+                          </span>
+                        )}
+                        {entry.showServed && (
+                          <span>{entry.servedAtFormatted}</span>
                         )}
                       </td>
-                    )}
-                  </tr>
-                );
-              },
-            )}
-          </tbody>
-        </table>
+                      <td data-label="No.">{entry.index}</td>
+                    </tr>
+                  );
+                },
+              )}
+            </tbody>
+          </table>
+        </Phone>
+
         {showModal == 'DocketRecordOverlay' && <DocketRecordOverlay />}
         {showModal == 'SealDocketEntryModal' && <SealDocketEntryModal />}
         {showModal == 'UnsealDocketEntryModal' && <UnsealDocketEntryModal />}
--- web-client/src/views/Public/PublicDocketRecordHeader.tsx
+++ web-client/src/views/Public/PublicDocketRecordHeader.tsx
@@ -1,6 +1,8 @@
-import { BindedSelect } from '../../ustc-ui/BindedSelect/BindedSelect';
 import { Button } from '../../ustc-ui/Button/Button';
-import { DocketRecordSort } from '../DocketRecord/DocketRecordSort';
+import {
+  DocketRecordMobileHeader,
+  NonMobileHeaderControls,
+} from '../DocketRecord/DocketRecordHeader';
 import { Mobile, NonMobile } from '../../ustc-ui/Responsive/Responsive';
 import { OpenPrintableDocketRecordModal } from '../DocketRecord/OpenPrintableDocketRecordModal';
 import { connect } from '@cerebral/react';
@@ -56,114 +58,27 @@ export const PublicDocketRecordHeader = connect(
           <div className="grid-container padding-0 docket-record-header">
             <div className="grid-row grid-gap margin-bottom-2">
               <div className="grid-col-12 display-flex flex-align-center">
-                <label
-                  className="dropdown-label-serif margin-right-3 margin-bottom-0"
-                  htmlFor="docket-record-sort"
-                  id="docket-record-sort-label"
-                >
-                  Sort by
-                </label>
-                <div className="margin-right-3">
-                  <DocketRecordSort
-                    name={`docketRecordSort.${docketNumber}`}
-                    value={sessionMetadata.docketRecordSort[docketNumber]}
-                    onChange={updateSessionMetadataSequence}
-                  />
-                </div>
-                <label
-                  className="dropdown-label-serif margin-right-3 margin-bottom-0"
-                  htmlFor="document-filter-by"
-                  id="docket-record-filter-label"
-                >
-                  Filter by
-                </label>
-                <BindedSelect
-                  aria-describedby="docket-record-filter-label"
-                  aria-label="docket record filter"
-                  bind="sessionMetadata.docketRecordFilter"
-                  className="select-left inline-select"
-                  id="document-filter-by"
-                  name="docketRecordFilter"
-                  style={{ maxWidth: 180 }}
-                >
-                  {Object.entries(PUBLIC_DOCKET_RECORD_FILTER_OPTIONS).map(
-                    ([key, value]) => (
-                      <option key={`filter-${key}`} value={value}>
-                        {value}
-                      </option>
-                    ),
-                  )}
-                </BindedSelect>
+                <NonMobileHeaderControls
+                  docketNumber={docketNumber}
+                  filterOptions={PUBLIC_DOCKET_RECORD_FILTER_OPTIONS}
+                  sessionMetadata={sessionMetadata}
+                  updateSessionMetadataSequence={updateSessionMetadataSequence}
+                />
               </div>
             </div>
           </div>
         </NonMobile>
 
         <Mobile>
-          <div className="grid-container padding-0 docket-record-header">
-            <div className="grid-row">
-              <div className="grid-col-4">
-                <label
-                  className="dropdown-label-serif margin-right-2"
-                  htmlFor="docket-record-sort"
-                  id="docket-record-sort-label"
-                >
-                  Sort by
-                </label>
-              </div>
-              <div className="grid-col-fill">
-                <DocketRecordSort
-                  name={`docketRecordSort.${docketNumber}`}
-                  value={sessionMetadata.docketRecordSort[docketNumber]}
-                  onChange={updateSessionMetadataSequence}
-                />
-              </div>
-            </div>
-
-            <div className="grid-row padding-y-075-rem">
-              <div className="grid-col-4">
-                <label
-                  className="dropdown-label-serif"
-                  htmlFor="document-filter-by"
-                  id="docket-record-filter-label"
-                >
-                  Filter by
-                </label>
-              </div>
-              <div className="grid-col-fill">
-                <BindedSelect
-                  aria-describedby="docket-record-filter-label"
-                  aria-label="docket record filter"
-                  bind="sessionMetadata.docketRecordFilter"
-                  id="document-filter-by"
-                  name="docketRecordFilter"
-                >
-                  {Object.entries(PUBLIC_DOCKET_RECORD_FILTER_OPTIONS).map(
-                    ([key, value]) => (
-                      <option key={`filter-${key}`} value={value}>
-                        {value}
-                      </option>
-                    ),
-                  )}
-                </BindedSelect>
-              </div>
-            </div>
-            <div className="grid-row">
-              <Button
-                link
-                aria-hidden="true"
-                className="margin-top-1 text-left"
-                icon="print"
-                onClick={() => {
-                  gotoPublicPrintableDocketRecordSequence({
-                    docketNumber,
-                  });
-                }}
-              >
-                Printable Docket Record
-              </Button>
-            </div>
-          </div>
+          <DocketRecordMobileHeader
+            docketNumber={docketNumber}
+            filterOptions={PUBLIC_DOCKET_RECORD_FILTER_OPTIONS}
+            gotoPrintableDocketRecordSequence={
+              gotoPublicPrintableDocketRecordSequence
+            }
+            sessionMetadata={sessionMetadata}
+            updateSessionMetadataSequence={updateSessionMetadataSequence}
+          />
         </Mobile>
 
         {showModal === 'OpenPrintableDocketRecordModal' && (
