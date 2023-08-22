ecf86d8b55ae64987e873ff4ea098e0b1bc0068c
Absolutestunna <joshujah007@gmail.com>
2023-08-22 13:57:49 -0400

    9998 - removed unnecessary tests from getCasesByStatusAndByJudgeInteractor; will be tested as an integration test; corrected unit test in getDocketNumbersByStatusAndByJudge

--- web-api/src/persistence/elasticsearch/getDocketNumbersByStatusAndByJudge.test.ts
+++ web-api/src/persistence/elasticsearch/getDocketNumbersByStatusAndByJudge.test.ts
@@ -15,15 +15,13 @@ describe('getDocketNumbersByStatusAndByJudge', () => {
     statuses: CAV_AND_SUBMITTED_CASE_STATUS,
   };
 
-  const responseResults = {
-    results: [
-      { docketNumber: '11315-18' },
-      { docketNumber: { S: '11316-18' } },
-    ],
-  };
+  const responseResults = [
+    { docketNumber: '11315-18' },
+    { docketNumber: { S: '11316-18' } },
+  ];
 
-  it('should make a persistence call to obtain all cases with a status of "Submitted" or "CAV" associated with the given judge', async () => {
-    search.mockReturnValue(responseResults);
+  it('should make a persistence call to obtain all cases with a status of "Submitted" or "CAV" associated with the given judges', async () => {
+    search.mockReturnValue({ results: responseResults });
 
     const docketNumbersSearchResults = await getDocketNumbersByStatusAndByJudge(
       {
@@ -55,5 +53,32 @@ describe('getDocketNumbersByStatusAndByJudge', () => {
     );
 
     expect(docketNumbersSearchResults).toMatchObject(responseResults);
+  });
+
+  it('should make a persistence call to obtain all cases with a status of "Submitted" or "CAV" without judges', async () => {
+    search.mockReturnValue({ results: responseResults });
+
+    await getDocketNumbersByStatusAndByJudge({
+      applicationContext,
+      params: { ...mockValidRequest, judges: undefined },
+    });
+
+    expect(search.mock.calls[0][0].searchParameters.size).toEqual(
+      MAX_ELASTICSEARCH_PAGINATION,
+    );
+
+    expect(
+      search.mock.calls[0][0].searchParameters.body.query.bool.should,
+    ).toMatchObject([]);
+
+    expect(
+      search.mock.calls[0][0].searchParameters.body.query.bool.filter,
+    ).toMatchObject(
+      expect.arrayContaining([
+        {
+          terms: { 'status.S': CAV_AND_SUBMITTED_CASE_STATUS },
+        },
+      ]),
+    );
   });
 });
