2d6d71f73e9914a9217d0f98765b7718b393a832
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2023-08-23 09:13:04 -0500

    9998: remove superfluous cerebraltest modifications and console logs

--- web-client/integration-tests/journey/docketClerkStrikesDocketEntry.ts
+++ web-client/integration-tests/journey/docketClerkStrikesDocketEntry.ts
@@ -4,14 +4,12 @@ export const docketClerkStrikesDocketEntry = (
 ) => {
   return it('docket clerk strikes docket entry', async () => {
     await cerebralTest.runSequence('strikeDocketEntrySequence');
-    console.log('docketRecordIndex FOR STRIKING', docketRecordIndex);
 
     const caseDocuments = cerebralTest.getState('caseDetail.docketEntries');
-    console.log('caseDocuments', caseDocuments);
     const strickenDocument = caseDocuments.find(
       d => d.index === docketRecordIndex,
     );
-    console.log('strickenDocument', strickenDocument);
+
     expect(strickenDocument.isStricken).toEqual(true);
     expect(strickenDocument.strickenBy).toEqual('Test Docketclerk');
   });
