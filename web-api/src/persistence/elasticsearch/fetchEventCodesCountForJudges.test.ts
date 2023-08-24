1addf120eb3e0c78d170b1f5ee3323bcde98fc74
Absolutestunna <joshujah007@gmail.com>
2023-08-24 13:01:14 -0400

    9998 - corrected test for fetchEventCodesCountForJudges

--- web-api/src/persistence/elasticsearch/fetchEventCodesCountForJudges.test.ts
+++ web-api/src/persistence/elasticsearch/fetchEventCodesCountForJudges.test.ts
@@ -1,26 +1,42 @@
-import { ORDER_EVENT_CODES } from '@shared/business/entities/EntityConstants';
+import {
+  OPINION_EVENT_CODES_WITH_BENCH_OPINION,
+  ORDER_EVENT_CODES,
+} from '@shared/business/entities/EntityConstants';
 import { applicationContext } from '../../../../shared/src/business/test/createTestApplicationContext';
 import { fetchEventCodesCountForJudges } from './fetchEventCodesCountForJudges';
 import { judgeUser } from '@shared/test/mockUsers';
-import { mockCountOfFormattedOrdersIssuedByJudge } from '@shared/business/useCases/judgeActivityReport/getCountOfOrdersFiledByJudgesInteractor.test';
 import { search } from './searchClient';
 jest.mock('./searchClient');
 
 describe('fetchEventCodesCountForJudges', () => {
-  const aggsBucket = [
+  const orderAggsBucket = [
     { doc_count: 2, key: 'O' },
-    { doc_count: 1, key: 'OAL' },
     { doc_count: 1, key: 'ODX' },
     { doc_count: 5, key: 'OSC' },
+  ];
+
+  const opinionAggsBucket = [
+    { doc_count: 2, key: 'SOP' },
+    { doc_count: 1, key: 'TCOP' },
+    { doc_count: 5, key: 'MOP' },
   ];
 
   const mockOrdersResults = {
     aggregations: {
       search_field_count: {
-        buckets: aggsBucket,
+        buckets: orderAggsBucket,
       },
     },
-    total: 9,
+    total: 8,
+  };
+
+  const mockOpinionsResults = {
+    aggregations: {
+      search_field_count: {
+        buckets: opinionAggsBucket,
+      },
+    },
+    total: 8,
   };
 
   const documentQueryBody = {
@@ -55,15 +71,24 @@ describe('fetchEventCodesCountForJudges', () => {
     startDate: '2020-02-12T05:00:00.000Z',
   };
 
-  it('returns searches for event codes within a time range using selected judges', async () => {
-    search.mockReturnValue(mockOrdersResults);
+  it("returns the counts of each orders' event codes within a time range using selected judges", async () => {
+    const expectedCount = {
+      O: 2,
+      ODX: 1,
+      OSC: 5,
+    };
 
+    (search as jest.Mock).mockReturnValue(mockOrdersResults);
+
+    params.documentEventCodes = orderEventCodesToSearch;
     const { aggregations, total } = await fetchEventCodesCountForJudges({
       applicationContext,
       params,
     });
 
-    expect(search.mock.calls[0][0].searchParameters.body).toMatchObject({
+    expect(
+      (search as jest.Mock).mock.calls[0][0].searchParameters.body,
+    ).toMatchObject({
       ...documentQueryBody,
       query: {
         bool: {
@@ -101,7 +126,77 @@ describe('fetchEventCodesCountForJudges', () => {
       },
     });
 
-    expect(aggregations).toMatchObject(mockCountOfFormattedOrdersIssuedByJudge);
-    expect(total).toEqual(9);
+    orderEventCodesToSearch.forEach(evCode => {
+      expect(aggregations).toContainEqual({
+        count: expectedCount[evCode] || 0,
+        eventCode: evCode,
+      });
+    });
+    expect(total).toEqual(8);
+  });
+
+  it("returns the count of opinion's event codes within a time range using selected judges", async () => {
+    (search as jest.Mock).mockReturnValue(mockOpinionsResults);
+
+    const expectedCount = {
+      MOP: 5,
+      SOP: 2,
+      TCOP: 1,
+    };
+
+    params.documentEventCodes = OPINION_EVENT_CODES_WITH_BENCH_OPINION;
+
+    const { aggregations, total } = await fetchEventCodesCountForJudges({
+      applicationContext,
+      params,
+    });
+
+    expect(
+      (search as jest.Mock).mock.calls[0][0].searchParameters.body,
+    ).toMatchObject({
+      ...documentQueryBody,
+      query: {
+        bool: {
+          filter: [
+            { term: { 'entityName.S': 'DocketEntry' } },
+            {
+              range: {
+                'filingDate.S': {
+                  gte: `${params.startDate}||/h`,
+                  lte: `${params.endDate}||/h`,
+                },
+              },
+            },
+            {
+              terms: { 'eventCode.S': params.documentEventCodes },
+            },
+          ],
+          minimum_should_match: 1,
+          should: [
+            {
+              match: {
+                ['signedJudgeName.S']: {
+                  operator: 'and',
+                  query: judgeUser.name,
+                },
+              },
+            },
+            {
+              match: {
+                ['judge.S']: judgeUser.name,
+              },
+            },
+          ],
+        },
+      },
+    });
+
+    OPINION_EVENT_CODES_WITH_BENCH_OPINION.forEach(evCode => {
+      expect(aggregations).toContainEqual({
+        count: expectedCount[evCode] || 0,
+        eventCode: evCode,
+      });
+    });
+    expect(total).toEqual(8);
   });
 });
