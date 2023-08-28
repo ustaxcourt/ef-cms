d4af0d6798509154800ecc548f157c5c71298c87
Jim Lerza <james.lerza@ustaxcourt.gov>
2023-08-28 15:16:44 -0400

    devex-1136: removing the process-s3-queue and wait-for-s3-queue lambdas

--- web-api/workflow-terraform/process-s3-queue/main/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "s3_bucket_queue_url" {
-  value = aws_sqs_queue.s3_bucket_queue.id
-}
-
-output "s3_bucket_dl_queue_url" {
-  value = aws_sqs_queue.s3_bucket_dl_queue.id
-}
--- web-api/workflow-terraform/wait-for-s3-queue-cron/bin/enable.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-./check-env-variables.sh "ENV"
-
-aws events enable-rule --name "check_s3_queue_status_cron_${ENV}" --region us-east-1
