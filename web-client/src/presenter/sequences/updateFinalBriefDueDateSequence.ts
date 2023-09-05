0b445ead919ca70bef8a7d70b31c1d47d894c1ef
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-09-05 12:48:36 -0700

    9995: Update sequences to show progress wheel when updating case worksheet in persistence, add back permission that was accidentally removed

--- web-client/src/presenter/sequences/updateFinalBriefDueDateSequence.ts
+++ web-client/src/presenter/sequences/updateFinalBriefDueDateSequence.ts
@@ -1,6 +1,7 @@
 import { clearTableItemValidationErrorAction } from '../actions/CaseWorksheet/clearTableItemValidationErrorAction';
 import { setCaseWorksheetAction } from '../actions/CaseWorksheet/setCaseWorksheetAction';
 import { setTableItemValidationErrorsAction } from '../actions/setTableItemValidationErrorsAction';
+import { showProgressSequenceDecorator } from '@web-client/presenter/utilities/showProgressSequenceDecorator';
 import { updateFinalBriefDueDateAction } from '@web-client/presenter/actions/CaseWorksheet/updateFinalBriefDueDateAction';
 import { validateBriefDueDateAction } from '@web-client/presenter/actions/CaseWorksheet/validateBriefDueDateAction';
 
@@ -8,10 +9,10 @@ export const updateFinalBriefDueDateSequence = [
   validateBriefDueDateAction,
   {
     error: [setTableItemValidationErrorsAction],
-    success: [
+    success: showProgressSequenceDecorator([
       clearTableItemValidationErrorAction,
       updateFinalBriefDueDateAction,
       setCaseWorksheetAction,
-    ],
+    ]),
   },
 ];
