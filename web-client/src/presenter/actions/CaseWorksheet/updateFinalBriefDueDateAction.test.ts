8db54e0abb78b0676322bc26854e314c9e166e9f
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-09-06 10:34:56 -0700

    9995: Update integration test name, WIP update calls to update case worksheet to include judge userId

--- web-client/src/presenter/actions/CaseWorksheet/updateFinalBriefDueDateAction.test.ts
+++ web-client/src/presenter/actions/CaseWorksheet/updateFinalBriefDueDateAction.test.ts
@@ -1,5 +1,6 @@
 import { MOCK_CASE } from '@shared/test/mockCase';
 import { applicationContextForClient as applicationContext } from '../../../../../shared/src/business/test/createTestApplicationContext';
+import { judgeUser } from '@shared/test/mockUsers';
 import { presenter } from '../../presenter-mock';
 import { runAction } from '@web-client/presenter/test.cerebral';
 import { updateFinalBriefDueDateAction } from './updateFinalBriefDueDateAction';
@@ -7,7 +8,7 @@ import { updateFinalBriefDueDateAction } from './updateFinalBriefDueDateAction';
 describe('updateFinalBriefDueDateAction', () => {
   presenter.providers.applicationContext = applicationContext;
 
-  it('should persist the brief due date to the backend', async () => {
+  it('should persist the brief due date to the backend when the current user is a chambers user', async () => {
     const TEST_FINAL_BRIEF_DUE_DATE = '08/28/2023';
     applicationContext
       .getUseCases()
@@ -21,6 +22,9 @@ describe('updateFinalBriefDueDateAction', () => {
         docketNumber: MOCK_CASE.docketNumber,
         finalBriefDueDate: TEST_FINAL_BRIEF_DUE_DATE,
       },
+      state: {
+        judgeUser: { userId: judgeUser.userId },
+      },
     });
 
     expect(
@@ -28,7 +32,35 @@ describe('updateFinalBriefDueDateAction', () => {
         .calls[0][1],
     ).toMatchObject({
       docketNumber: MOCK_CASE.docketNumber,
+      judgeUserId: judgeUser.userId,
       updatedProps: { finalBriefDueDate: TEST_FINAL_BRIEF_DUE_DATE },
+    });
+  });
+
+  it('should persist the brief due date to the backend when the current user is a judge user', async () => {
+    const TEST_FINAL_BRIEF_DUE_DATE = '08/28/2023';
+    applicationContext
+      .getUseCases()
+      .updateCaseWorksheetInteractor.mockReturnValue(null);
+
+    await runAction(updateFinalBriefDueDateAction, {
+      modules: {
+        presenter,
+      },
+      props: {
+        docketNumber: MOCK_CASE.docketNumber,
+        finalBriefDueDate: TEST_FINAL_BRIEF_DUE_DATE,
+      },
+      state: {
+        judgeUser: undefined,
+      },
+    });
+
+    expect(
+      applicationContext.getUseCases().updateCaseWorksheetInteractor.mock
+        .calls[0][1],
+    ).toMatchObject({
+      judgeUserId: judgeUser.userId,
     });
   });
 });
