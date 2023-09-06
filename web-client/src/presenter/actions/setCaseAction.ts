f8bc02249ae112ec59b96d26e2d84661e93d186d
Zachary Rogers <zrogers>
2023-09-06 13:35:01 -0700

    devex-1141: Sort consolidated cases when setting on state.

--- web-client/src/presenter/actions/setCaseAction.ts
+++ web-client/src/presenter/actions/setCaseAction.ts
@@ -1,3 +1,4 @@
+import { compareCasesByDocketNumber } from '@shared/business/utilities/getFormattedTrialSessionDetails';
 import { state } from '@web-client/presenter/app.cerebral';
 
 /**
@@ -6,6 +7,14 @@ import { state } from '@web-client/presenter/app.cerebral';
  * @param {object} providers.props the cerebral props object containing the props.caseDetail
  * @param {object} providers.store the cerebral store used for setting the state.caseDetail
  */
-export const setCaseAction = ({ props, store }: ActionProps) => {
+export const setCaseAction = ({
+  props,
+  store,
+}: ActionProps<{ caseDetail: RawCase }>) => {
+  const unsortedConsolidatedCases = props.caseDetail.consolidatedCases || [];
+  props.caseDetail.consolidatedCases = unsortedConsolidatedCases.sort(
+    compareCasesByDocketNumber,
+  );
+
   store.set(state.caseDetail, props.caseDetail);
 };
