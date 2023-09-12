ef797756436cf3cb82888b0b79de8e371e3e902c
Absolutestunna <joshujah007@gmail.com>
2023-09-11 21:41:27 -0400

    10049 - WIP - creating new proxy, lambda, route, interactor for fetching case document count

--- shared/src/business/useCases/judgeActivityReport/getCountOfCaseDocumentsFiledByJudgesInteractor.ts
+++ shared/src/business/useCases/judgeActivityReport/getCountOfCaseDocumentsFiledByJudgesInteractor.ts
@@ -1,11 +1,25 @@
 import { InvalidRequest, UnauthorizedError } from '@web-api/errors/errors';
 import { JudgeActivityReportSearch } from '../../entities/judgeActivityReport/JudgeActivityReportSearch';
-import { ORDER_EVENT_CODES } from '../../entities/EntityConstants';
+import {
+  OPINION_EVENT_CODES_WITH_BENCH_OPINION,
+  ORDER_EVENT_CODES,
+} from '../../entities/EntityConstants';
 import {
   ROLE_PERMISSIONS,
   isAuthorized,
-} from '../../../authorization/authorizationClientService';
+} from '@shared/authorization/authorizationClientService';
 import { addDocumentTypeToEventCodeAggregation } from './addDocumentTypeToEventCodeAggregation';
+
+export type CaseDocumentsCountType = {
+  count: number;
+  documentType: string | undefined;
+  eventCode: string;
+};
+
+export type CaseDocumentsAggregationReturnType = {
+  aggregations: CaseDocumentsCountType[];
+  total: number | undefined;
+};
 
 // TODO: refactor JudgeActivityReportFilters to be only types for request to BE
 export type JudgeActivityReportFilters = {
@@ -18,31 +32,20 @@ export type JudgeActivityReportFilters = {
   searchType: string;
 };
 
-export type OrdersAndOpinionResultCountTypes = {
-  count: number;
-  documentType: string | undefined;
-  eventCode: string;
-};
-
-export type OrdersReturnType = {
-  aggregations: OrdersAndOpinionResultCountTypes[];
-  total: number | undefined;
-};
-
-export const getCountOfOrdersFiledByJudgesInteractor = async (
-  applicationContext,
+export const getCountOfCaseDocumentsFiledByJudgesInteractor = async (
+  applicationContext: IApplicationContext,
   params: JudgeActivityReportFilters,
-): Promise<OrdersReturnType> => {
+): Promise<CaseDocumentsAggregationReturnType> => {
   const authorizedUser = applicationContext.getCurrentUser();
 
   if (!isAuthorized(authorizedUser, ROLE_PERMISSIONS.JUDGE_ACTIVITY_REPORT)) {
-    throw new UnauthorizedError('Unauthorized');
+    throw new UnauthorizedError('Unauthorized to view Judge Activity Report');
   }
 
   const searchEntity = new JudgeActivityReportSearch(params);
 
   if (!searchEntity.isValid()) {
-    throw new InvalidRequest();
+    throw new InvalidRequest('The Search Params for judges are invalid');
   }
 
   const excludedOrderEventCodes = ['OAJ', 'SPOS', 'SPTO', 'OST'];
@@ -50,12 +53,22 @@ export const getCountOfOrdersFiledByJudgesInteractor = async (
     eventCode => !excludedOrderEventCodes.includes(eventCode),
   );
 
+  let computeDocumentEventCodes; // type
+
+  if (params.searchType === 'order') {
+    computeDocumentEventCodes = orderEventCodesToSearch;
+  }
+
+  if (params.searchType === 'opinion') {
+    computeDocumentEventCodes = OPINION_EVENT_CODES_WITH_BENCH_OPINION;
+  }
+
   const { aggregations, total } = await applicationContext
     .getPersistenceGateway()
     .fetchEventCodesCountForJudges({
       applicationContext,
       params: {
-        documentEventCodes: orderEventCodesToSearch,
+        documentEventCodes: computeDocumentEventCodes,
         endDate: searchEntity.endDate,
         judges: searchEntity.judges,
         startDate: searchEntity.startDate,
