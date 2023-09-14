b0657326034ecf57dacf9c68928150f5d3b34eb3
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-09-14 11:46:15 -0700

    9995: Update final brief due date onChange to also call the validate sequence

--- web-client/src/presenter/sequences/updateDateValueSequence.ts
+++ web-client/src/presenter/sequences/updateDateValueSequence.ts
@@ -1,4 +1,9 @@
 import { formatDateAction } from '@web-client/presenter/actions/formatDateAction';
 import { setFormValueAction } from '../actions/setFormValueAction';
+import { validateCaseWorksheetSequence } from '@web-client/presenter/sequences/validateCaseWorksheetSequence';
 
-export const updateDateValueSequence = [formatDateAction, setFormValueAction];
+export const updateDateValueSequence = [
+  formatDateAction,
+  setFormValueAction,
+  validateCaseWorksheetSequence,
+];
