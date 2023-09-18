412be9bdae47c3363bdfe87698b38972d8b9257d
Nechama Krigsman <nkrigsman@flexion.us>
2023-09-18 11:56:35 -0400

    9995: add date hint

--- web-client/src/views/CaseWorksheet/AddEditCaseWorksheetModal.tsx
+++ web-client/src/views/CaseWorksheet/AddEditCaseWorksheetModal.tsx
@@ -42,6 +42,7 @@ export const AddEditCaseWorksheetModal = connect(
         </h5>
 
         <DateSelector
+          showDateHint
           defaultValue={form.finalBriefDueDate}
           errorText={validationErrors.finalBriefDueDate}
           formGroupClassNames={'width-half'}
