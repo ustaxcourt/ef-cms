f8158b638fd86cf944ca449f7d9511df5721a919
Nechama Krigsman <nkrigsman@flexion.us>
2023-09-18 15:37:36 -0400

    9995: fix tests

--- web-client/src/presenter/actions/TrialSession/computeSubmitTrialSessionDataAction.ts
+++ web-client/src/presenter/actions/TrialSession/computeSubmitTrialSessionDataAction.ts
@@ -4,13 +4,6 @@ import {
 } from './computeTrialSessionFormDataAction';
 import { state } from '@web-client/presenter/app.cerebral';
 
-/**
- * computes the trial session data based on user input for submission
- *
- * @param {object} providers the providers object
- * @param {object} providers.get the cerebral get function
- * @param {object} providers.store the cerebral store function
- */
 export const computeSubmitTrialSessionDataAction = ({
   applicationContext,
   get,
