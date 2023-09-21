273f8d2a5bf06acfabb068ac3da33d908acdafe3
John Cruz <jcruz@flexion.us>
2023-09-20 23:00:08 -0600

    devex-1187: Type and implement the revursive part of the method but leave it commented until we have tests around it ;

--- shared/src/business/entities/joiValidationEntity/JoiValidationEntity.new.getFormattedValidationErrors.ts
+++ shared/src/business/entities/joiValidationEntity/JoiValidationEntity.new.getFormattedValidationErrors.ts
@@ -23,9 +23,7 @@ function isJoiValidationEntity(entity): boolean {
   return !!entity.getFormattedValidationErrors;
 }
 
-export function getFormattedValidationErrors_NEW(
-  entity,
-): Record<string, string> | null {
+export function getFormattedValidationErrors_NEW(entity): TempTyping | null {
   let errors: {} | null = null;
 
   if (isJoiValidationEntity(entity)) {
@@ -35,7 +33,60 @@ export function getFormattedValidationErrors_NEW(
   const updatedErrors =
     removeUnhelpfulErrorMessagesFromContactValidations(errors);
 
-  return updatedErrors;
+  const nestedErrors = appendNestedEntitiesErrors(entity, updatedErrors);
+
+  return nestedErrors;
+}
+
+export type TempTyping = {
+  [key: string]: string | TempTyping | TempTyping[];
+};
+
+// TODO: RECURSIVE LOGIC
+function appendNestedEntitiesErrors(
+  entity: any,
+  errors: { [key: string]: string } | null,
+): TempTyping | null {
+  const entityProperties = Object.keys(entity);
+  const updatedErrors: TempTyping | null = {};
+
+  for (let entityProperty of entityProperties) {
+    // const value = entity[entityProperty];
+
+    if (errors && errors[entityProperty]) {
+      updatedErrors[entityProperty] = errors[entityProperty];
+      continue;
+    }
+
+    // THIS LOGIC RECURSIVELY LOOPS THROUGH ARRAYS AND CHECKS THE ITEMS ARE ENTITIES AND VALIDATES THEM
+    // LEAVE THIS COMMENTED UNTIL WE HAVE TESTS VERIFYING EXPECTED RESULTS
+
+    // else if (Array.isArray(value)) {
+    //   const arrayValidation = value
+    //     .map((v, index) => {
+    //       const e = getFormattedValidationErrors_NEW(v);
+    //       return e ? { ...e, index } : null;
+    //     })
+    //     .filter(v => !!v) as (TempTyping & { index: number })[];
+    //   if (arrayValidation.length)
+    //     updatedErrors[entityProperty] = arrayValidation;
+    //   continue;
+    // }
+
+    // THIS LOGIC WILL RUN ON PROPERTIES THAT ARE OBJECT, IT WILL CHECK IF ITS AN ENTITY AND VALIDATE IT
+    // LEAVE THIS COMMENTED UNTIL WE HAVE TESTS VERIFYING EXPECTED RESULTS
+
+    // else if (
+    //   typeof value === 'object' &&
+    //   value &&
+    //   value.getFormattedValidationErrors
+    // ) {
+    //   const objectErrors = getFormattedValidationErrors_NEW(value);
+    //   if (objectErrors) updatedErrors[entityProperty] = objectErrors;
+    // }
+  }
+
+  return Object.keys(updatedErrors).length ? updatedErrors : null;
 }
 
 function removeUnhelpfulErrorMessagesFromContactValidations(
