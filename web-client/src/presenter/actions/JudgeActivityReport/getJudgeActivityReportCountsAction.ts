84f49b7961776404ec4c150778b3829ee2f1cd29
Mike Marcotte <mike.marcotte@gmail.com>
2023-09-21 16:26:59 -0400

    10049: refactor; continue to refactor endpoint

--- web-client/src/presenter/actions/JudgeActivityReport/getJudgeActivityReportCountsAction.ts
+++ web-client/src/presenter/actions/JudgeActivityReport/getJudgeActivityReportCountsAction.ts
@@ -1,3 +1,7 @@
+import {
+  JUDGE_ACTIVITY_REPORT_ORDER_EVENT_CODES,
+  OPINION_EVENT_CODES_WITH_BENCH_OPINION,
+} from '@shared/business/entities/EntityConstants';
 import { state } from '@web-client/presenter/app.cerebral';
 
 export const getJudgeActivityReportCountsAction = async ({
@@ -6,11 +10,26 @@ export const getJudgeActivityReportCountsAction = async ({
 }: ActionProps) => {
   const { endDate, judges, startDate } = get(state.judgeActivityReport.filters);
 
-  return await applicationContext
-    .getUseCases()
-    .getCountOfCaseDocumentsFiledByJudgesInteractor(applicationContext, {
-      endDate,
-      judges,
-      startDate,
-    });
+  const eventCodes = [
+    JUDGE_ACTIVITY_REPORT_ORDER_EVENT_CODES,
+    OPINION_EVENT_CODES_WITH_BENCH_OPINION,
+  ];
+
+  const [orders, opinions] = await Promise.all(
+    eventCodes.map(documentEventCodes =>
+      applicationContext
+        .getUseCases()
+        .getCountOfCaseDocumentsFiledByJudgesInteractor(applicationContext, {
+          documentEventCodes,
+          endDate,
+          judges,
+          startDate,
+        }),
+    ),
+  );
+
+  return {
+    opinions,
+    orders,
+  };
 };
