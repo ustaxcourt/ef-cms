c26a422b43c3cefc545ee9c0ba6653a0a5842413
Absolutestunna <joshujah007@gmail.com>
2023-09-27 14:20:53 -0400

    9788 - adding types

--- shared/src/business/useCaseHelper/saveFileAndGenerateUrl.ts
+++ shared/src/business/useCaseHelper/saveFileAndGenerateUrl.ts
@@ -1,17 +1,15 @@
-/**
- *
- * Save provided file to temp s3 bucket and return file url
- *
- * @param {object} providers the providers object
- * @param {object} providers.applicationContext the application context
- * @param {string} providers.file the file to save
- * @returns {string} the url to the file
- */
 export const saveFileAndGenerateUrl = async ({
   applicationContext,
   file,
   useTempBucket = false,
-}) => {
+}: {
+  applicationContext: IApplicationContext;
+  file: Buffer;
+  useTempBucket: boolean;
+}): Promise<{
+  fileId: string;
+  url: string;
+}> => {
   const fileId = applicationContext.getUniqueId();
 
   await applicationContext.getPersistenceGateway().saveDocumentFromLambda({
