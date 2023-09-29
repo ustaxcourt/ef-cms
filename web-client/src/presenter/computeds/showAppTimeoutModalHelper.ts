1cadff26257f18ff4c860a5afe00e7ceea57b9c1
Cody Seibert <cseibert113@gmail.com>
2023-09-29 13:26:13 -0400

    bug - fixing tests

--- web-client/src/presenter/computeds/showAppTimeoutModalHelper.ts
+++ web-client/src/presenter/computeds/showAppTimeoutModalHelper.ts
@@ -1,14 +1,11 @@
+import { Get } from 'cerebral';
 import { state } from '@web-client/presenter/app.cerebral';
 
-import { Get } from 'cerebral';
 export const showAppTimeoutModalHelper = (get: Get) => {
   const modalState = get(state.idleLogoutState.state);
   const currentUser = get(state.user);
-  const hasCurrentUser = !!currentUser;
-  const isUploading = get(state.fileUploadProgress.isUploading);
 
   return {
-    beginIdleMonitor: hasCurrentUser && !isUploading,
     currentUser,
     showModal: modalState === 'COUNTDOWN',
   };
