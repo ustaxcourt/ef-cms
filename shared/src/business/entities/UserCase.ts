770dd84dd9c2cee3cf6b209104fddabbafa83dff
John Cruz <jcruz@flexion.us>
2023-09-28 19:51:41 -0600

    devex-1187: Implement new schema for UserCase ;

--- shared/src/business/entities/UserCase.ts
+++ shared/src/business/entities/UserCase.ts
@@ -2,6 +2,7 @@ import { Case } from './cases/Case';
 import { JoiValidationConstants } from './JoiValidationConstants';
 import { JoiValidationEntity } from './JoiValidationEntity';
 import { createISODateString } from '../utilities/DateHandler';
+import { setDefaultErrorMessages } from '@shared/business/entities/utilities/setDefaultErrorMessages';
 
 export class UserCase extends JoiValidationEntity {
   public caseCaption: string;
@@ -34,6 +35,23 @@ export class UserCase extends JoiValidationEntity {
       entityName: JoiValidationConstants.STRING.valid('UserCase').required(),
       leadDocketNumber: Case.VALIDATION_RULES.leadDocketNumber,
       status: Case.VALIDATION_RULES.status,
+    };
+  }
+
+  getValidationRules_NEW() {
+    return {
+      caseCaption: Case.VALIDATION_RULES.caseCaption.messages(
+        setDefaultErrorMessages('Enter a case caption'),
+      ),
+      closedDate: Case.VALIDATION_RULES.closedDate, // NO ERROR MESSAGE FOUND
+      createdAt: Case.VALIDATION_RULES.createdAt, // NO ERROR MESSAGE FOUND
+      docketNumber: Case.VALIDATION_RULES.docketNumber.messages(
+        setDefaultErrorMessages('Docket number is required'),
+      ),
+      docketNumberWithSuffix: Case.VALIDATION_RULES.docketNumberWithSuffix, // NO ERROR MESSAGE FOUND
+      entityName: JoiValidationConstants.STRING.valid('UserCase').required(), // NO ERROR MESSAGE FOUND
+      leadDocketNumber: Case.VALIDATION_RULES.leadDocketNumber, // NO ERROR MESSAGE FOUND
+      status: Case.VALIDATION_RULES.status, // NO ERROR MESSAGE FOUND
     };
   }
 
