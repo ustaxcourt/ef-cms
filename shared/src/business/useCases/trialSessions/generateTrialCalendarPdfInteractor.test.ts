b2130e194ef91b1e6fd273650a9513b69c8a46a8
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-09-28 21:01:54 -0700

    9788: Refactoring interactor to reduce unnecessary formatting calls

--- shared/src/business/useCases/trialSessions/generateTrialCalendarPdfInteractor.test.ts
+++ shared/src/business/useCases/trialSessions/generateTrialCalendarPdfInteractor.test.ts
@@ -1,27 +1,27 @@
 import { MOCK_CASE } from '../../../test/mockCase';
-import { US_STATES } from '../../entities/EntityConstants';
+import { MOCK_TRIAL_INPERSON } from '@shared/test/mockTrial';
+import { TCaseOrder } from '@shared/business/entities/trialSessions/TrialSession';
 import { applicationContext } from '../../test/createTestApplicationContext';
 import {
   formatCases,
   generateTrialCalendarPdfInteractor,
   getPractitionerName,
 } from './generateTrialCalendarPdfInteractor';
+import {
+  irsPractitionerUser,
+  privatePractitionerUser,
+} from '@shared/test/mockUsers';
 
 describe('generateTrialCalendarPdfInteractor', () => {
-  const mockPdfUrl = { url: 'www.example.com' };
-  const mockIrsPractitioner = { barNumber: '123', name: 'IRS Format Me' };
-  const mockPrivatePractitioner = {
-    barNumber: '123',
-    name: 'Private Format Me',
-  };
   // deliberately *not* automatically sorted by docket number for test purposes
-  const mockCases = [
+  const mockCases: (RawCase & TCaseOrder)[] = [
     {
       ...MOCK_CASE,
+      calendarNotes: 'this is a test',
       docketNumber: '102-19',
       docketNumberWithSuffix: '102-19W',
-      irsPractitioners: [mockIrsPractitioner],
-      privatePractitioners: [mockPrivatePractitioner],
+      irsPractitioners: [irsPractitionerUser],
+      privatePractitioners: [privatePractitionerUser],
     },
     {
       ...MOCK_CASE,
@@ -42,7 +42,13 @@ describe('generateTrialCalendarPdfInteractor', () => {
     },
   ];
 
-  beforeAll(() => {
+  const mockPdfUrl = { url: 'www.example.com' };
+
+  beforeEach(() => {
+    applicationContext
+      .getPersistenceGateway()
+      .getTrialSessionById.mockReturnValue(MOCK_TRIAL_INPERSON);
+
     applicationContext
       .getPersistenceGateway()
       .getCalendaredCasesForTrialSession.mockReturnValue(mockCases);
@@ -50,117 +56,74 @@ describe('generateTrialCalendarPdfInteractor', () => {
     applicationContext
       .getPersistenceGateway()
       .getDownloadPolicyUrl.mockReturnValue(mockPdfUrl);
-
-    applicationContext
-      .getPersistenceGateway()
-      .getTrialSessionById.mockReturnValue({
-        address1: '123 Some Street',
-        address2: 'Suite B',
-        caseOrder: [
-          { calendarNotes: 'Calendar notes.', docketNumber: '123-20' },
-          { calendarNotes: undefined, docketNumber: '101-18' },
-        ],
-        city: US_STATES.NY,
-        courtReporter: 'Lois Lane',
-        courthouseName: 'Test Courthouse',
-        irsCalendarAdministrator: 'iCalRS Admin',
-        judge: { name: 'Joseph Dredd' },
-        notes:
-          'The one with the velour shirt is definitely looking at me funny.',
-        sessionType: 'Hybrid',
-        startDate: '2019-12-02T05:00:00.000Z',
-        startTime: '09:00',
-        state: 'NY',
-        term: 'Fall',
-        termYear: '2019',
-        trialClerk: 'Clerky McGee',
-        trialLocation: 'New York City, New York',
-        zip: '10108',
-      });
   });
 
   it('should generate the trial session calendar pdf including open cases', async () => {
-    await expect(
-      generateTrialCalendarPdfInteractor(applicationContext, {
-        trialSessionId: '6805d1ab-18d0-43ec-bafb-654e83405416',
-      }),
-    ).resolves.not.toThrow();
+    await generateTrialCalendarPdfInteractor(applicationContext, {
+      trialSessionId: MOCK_TRIAL_INPERSON.trialSessionId!,
+    });
 
     expect(
-      applicationContext.getPersistenceGateway().getTrialSessionById.mock.calls
-        .length,
-    ).toBe(1);
+      applicationContext.getPersistenceGateway().getTrialSessionById,
+    ).toHaveBeenCalled();
     expect(
       applicationContext.getPersistenceGateway()
-        .getCalendaredCasesForTrialSession.mock.calls.length,
-    ).toBe(1);
-    expect(
-      applicationContext.getDocumentGenerators().trialCalendar.mock.calls
-        .length,
-    ).toBe(1);
+        .getCalendaredCasesForTrialSession,
+    ).toHaveBeenCalled();
     expect(
       applicationContext.getDocumentGenerators().trialCalendar,
-    ).toHaveBeenCalledWith(
-      expect.objectContaining({
-        data: expect.objectContaining({
-          cases: expect.arrayContaining([
-            expect.objectContaining({
-              docketNumber: '101-18',
-            }),
-            expect.objectContaining({
-              caseTitle: 'Test Petitioner',
-              docketNumber: '102-19',
-              petitionerCounsel: ['Private Format Me (123)'],
-              respondentCounsel: ['IRS Format Me (123)'],
-            }),
-            expect.objectContaining({
-              docketNumber: '150-19',
-              isLeadCase: true,
-            }),
-            expect.not.objectContaining({
-              docketNumber: '123-20',
-            }),
-          ]),
-        }),
-      }),
-    );
-  });
-
-  describe('format cases', () => {
-    it('should filter out cases that have been removed from trial', () => {
-      const result = formatCases({
-        applicationContext,
-        calendaredCases: mockCases,
-      });
-      expect(mockCases.find(m => m.removedFromTrial)).toBeTruthy(); // there is a case in the mocks which is removed from trial
-      expect(result.find(m => m.docketNumber === '123-20')).toBeFalsy();
-    });
-
-    it('should sort cases by ascending docket number', () => {
-      const result = formatCases({
-        applicationContext,
-        calendaredCases: mockCases,
-      });
-      expect(result[0].docketNumber).toBe('101-18');
-      expect(result[1].docketNumber).toBe('102-19');
-    });
-
-    it('should set casse title to an empty string when caseCaption is undefined', () => {
-      const result = formatCases({
-        applicationContext,
-        calendaredCases: [{ ...mockCases[0], caseCaption: undefined }],
-      });
-      expect(result[0].caseTitle).toBe('');
-    });
-
-    it('should format practitioner name + barNumber', () => {
-      const result = formatCases({
-        applicationContext,
-        calendaredCases: mockCases,
-      });
-
-      expect(result[1].petitionerCounsel[0]).toBe('Private Format Me (123)');
-      expect(result[1].respondentCounsel[0]).toBe('IRS Format Me (123)');
+    ).toHaveBeenCalledWith({
+      applicationContext: expect.anything(),
+      data: {
+        cases: [
+          {
+            calendarNotes: undefined,
+            caseTitle: 'Test Petitioner',
+            docketNumber: '101-18',
+            docketNumberWithSuffix: '101-18',
+            inConsolidatedGroup: false,
+            isLeadCase: false,
+            petitionerCounsel: [],
+            respondentCounsel: [],
+          },
+          {
+            calendarNotes: 'this is a test',
+            caseTitle: 'Test Petitioner',
+            docketNumber: '102-19',
+            docketNumberWithSuffix: '102-19W',
+            inConsolidatedGroup: false,
+            isLeadCase: false,
+            petitionerCounsel: ['Private Practitioner (BN1234)'],
+            respondentCounsel: ['IRS Practitioner (BN2345)'],
+          },
+          {
+            calendarNotes: undefined,
+            caseTitle: 'Test Petitioner',
+            docketNumber: '150-19',
+            docketNumberWithSuffix: '150-19',
+            inConsolidatedGroup: true,
+            isLeadCase: true,
+            petitionerCounsel: [],
+            respondentCounsel: [],
+          },
+        ],
+        sessionDetail: {
+          address1: MOCK_TRIAL_INPERSON.address1,
+          address2: MOCK_TRIAL_INPERSON.address2,
+          courtReporter: 'Not assigned',
+          courthouseName: MOCK_TRIAL_INPERSON.courthouseName,
+          formattedCityStateZip: 'Scottsburg, IN 47130',
+          irsCalendarAdministrator: 'Not assigned',
+          judge: MOCK_TRIAL_INPERSON.judge!.name,
+          noLocationEntered: false,
+          notes: MOCK_TRIAL_INPERSON.notes,
+          sessionType: MOCK_TRIAL_INPERSON.sessionType,
+          startDate: 'February 28, 3000',
+          startTime: undefined,
+          trialClerk: 'Not assigned',
+          trialLocation: MOCK_TRIAL_INPERSON.trialLocation,
+        },
+      },
     });
   });
 
@@ -168,7 +131,7 @@ describe('generateTrialCalendarPdfInteractor', () => {
     const result = await generateTrialCalendarPdfInteractor(
       applicationContext,
       {
-        trialSessionId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+        trialSessionId: MOCK_TRIAL_INPERSON.trialSessionId!,
       },
     );
 
@@ -176,34 +139,8 @@ describe('generateTrialCalendarPdfInteractor', () => {
   });
 
   it('should set calendarNotes for each case in trialSession.caseOrder when the case has calendarNotes', async () => {
-    applicationContext
-      .getPersistenceGateway()
-      .getTrialSessionById.mockReturnValue({
-        address1: '123 Some Street',
-        address2: 'Suite B',
-        caseOrder: [
-          { calendarNotes: 'this is a test', docketNumber: '102-19' },
-        ],
-        city: US_STATES.NY,
-        courtReporter: 'Lois Lane',
-        courthouseName: 'Test Courthouse',
-        irsCalendarAdministrator: 'iCalRS Admin',
-        judge: { name: 'Joseph Dredd' },
-        notes:
-          'The one with the velour shirt is definitely looking at me funny.',
-        sessionType: 'Hybrid',
-        startDate: '2019-12-02T05:00:00.000Z',
-        startTime: '09:00',
-        state: 'NY',
-        term: 'Fall',
-        termYear: '2019',
-        trialClerk: 'Clerky McGee',
-        trialLocation: 'New York City, New York',
-        zip: '10108',
-      });
-
     await generateTrialCalendarPdfInteractor(applicationContext, {
-      trialSessionId: '6805d1ab-18d0-43ec-bafb-654e83405416',
+      trialSessionId: MOCK_TRIAL_INPERSON.trialSessionId!,
     });
 
     const caseWithCalendarNotes = applicationContext
@@ -214,12 +151,56 @@ describe('generateTrialCalendarPdfInteractor', () => {
     expect(caseWithCalendarNotes.calendarNotes).toBe('this is a test');
   });
 
+  describe('format cases', () => {
+    it('should filter out cases that have been removed from trial', () => {
+      const result = formatCases({
+        applicationContext,
+        calendaredCases: mockCases,
+      });
+
+      expect(mockCases.find(m => m.removedFromTrial)).toBeTruthy(); // there is a case in the mocks which is removed from trial
+      expect(result.find(m => m.docketNumber === '123-20')).toBeFalsy();
+    });
+
+    it('should sort cases by ascending docket number', () => {
+      const result = formatCases({
+        applicationContext,
+        calendaredCases: mockCases,
+      });
+
+      expect(result[0].docketNumber).toBe('101-18');
+      expect(result[1].docketNumber).toBe('102-19');
+    });
+
+    it('should set casse title to an empty string when caseCaption is undefined', () => {
+      const result = formatCases({
+        applicationContext,
+        calendaredCases: [{ ...mockCases[0], caseCaption: undefined }],
+      });
+
+      expect(result[0].caseTitle).toBe('');
+    });
+
+    it('should format practitioner name + barNumber', () => {
+      const result = formatCases({
+        applicationContext,
+        calendaredCases: mockCases,
+      });
+
+      expect(result[1].petitionerCounsel[0]).toBe(
+        'Private Practitioner (BN1234)',
+      );
+      expect(result[1].respondentCounsel[0]).toBe('IRS Practitioner (BN2345)');
+    });
+  });
+
   describe('getPractitionerName', () => {
     it('should return the name and bar number of the practitioner as one string', () => {
       const result = getPractitionerName({
         barNumber: '123',
         name: 'Prac',
       });
+
       expect(result).toEqual('Prac (123)');
     });
 
@@ -228,6 +209,7 @@ describe('generateTrialCalendarPdfInteractor', () => {
         barNumber: undefined,
         name: 'Prac',
       });
+
       expect(result).toEqual('Prac');
     });
   });
