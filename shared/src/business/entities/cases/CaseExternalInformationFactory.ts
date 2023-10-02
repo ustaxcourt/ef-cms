dfe57b93bb300d8b6a919aae83a445bf16d2f43f
John Cruz <jcruz@flexion.us>
2023-10-02 13:59:37 -0600

    devex-1187: Implement new schema for "CaseExternalInformationFactory";

--- shared/src/business/entities/cases/CaseExternalInformationFactory.ts
+++ shared/src/business/entities/cases/CaseExternalInformationFactory.ts
@@ -84,6 +84,23 @@ export class CaseExternalInformationFactory extends JoiValidationEntity {
     return CaseExternalInformationFactory.VALIDATION_RULES;
   }
 
+  static VALIDATION_RULES_NEW = {
+    wizardStep: JoiValidationConstants.STRING.valid(
+      '1',
+      '2',
+      '3',
+      '4',
+    ).required(),
+    ...CaseExternalInformationFactory.wizardStep1_NEW(),
+    ...CaseExternalInformationFactory.wizardStep2_NEW(),
+    ...CaseExternalInformationFactory.wizardStep3_NEW(),
+    ...CaseExternalInformationFactory.wizardStep4_NEW(),
+  };
+
+  getValidationRules_NEW() {
+    return CaseExternalInformationFactory.VALIDATION_RULES_NEW;
+  }
+
   getErrorToMessageMap() {
     return CaseExternalInformationFactory.VALIDATION_ERROR_MESSAGES;
   }
@@ -138,6 +155,42 @@ export class CaseExternalInformationFactory extends JoiValidationEntity {
     return CaseExternalInformationFactory.atWizardStep(4, {
       preferredTrialCity: CaseExternal.VALIDATION_RULES.preferredTrialCity,
       procedureType: CaseExternal.VALIDATION_RULES.procedureType,
+    });
+  }
+
+  static wizardStep1_NEW() {
+    return CaseExternalInformationFactory.atWizardStep(1, {
+      stinFile: CaseExternal.VALIDATION_RULES_NEW.stinFile,
+      stinFileSize: CaseExternal.VALIDATION_RULES_NEW.stinFileSize,
+    });
+  }
+
+  static wizardStep2_NEW() {
+    return CaseExternalInformationFactory.atWizardStep(2, {
+      caseType: CaseExternal.VALIDATION_RULES_NEW.caseType,
+      hasIrsNotice: CaseExternal.VALIDATION_RULES_NEW.hasIrsNotice,
+      petitionFile: CaseExternal.VALIDATION_RULES_NEW.petitionFile,
+      petitionFileSize: CaseExternal.VALIDATION_RULES_NEW.petitionFileSize,
+    });
+  }
+
+  static wizardStep3_NEW() {
+    return CaseExternalInformationFactory.atWizardStep(3, {
+      businessType: CaseExternal.VALIDATION_RULES_NEW.businessType,
+      corporateDisclosureFile:
+        CaseExternal.VALIDATION_RULES_NEW.corporateDisclosureFile,
+      corporateDisclosureFileSize:
+        CaseExternal.VALIDATION_RULES_NEW.corporateDisclosureFileSize,
+      countryType: CaseExternal.VALIDATION_RULES_NEW.countryType,
+      filingType: CaseExternal.VALIDATION_RULES_NEW.filingType,
+      partyType: CaseExternal.VALIDATION_RULES_NEW.partyType,
+    });
+  }
+
+  static wizardStep4_NEW() {
+    return CaseExternalInformationFactory.atWizardStep(4, {
+      preferredTrialCity: CaseExternal.VALIDATION_RULES_NEW.preferredTrialCity,
+      procedureType: CaseExternal.VALIDATION_RULES_NEW.procedureType,
     });
   }
 }
