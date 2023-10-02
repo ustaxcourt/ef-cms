33924ba0a6f7a299e53e510e8bedae8f2bed740f
Cody Seibert <cseibert113@gmail.com>
2023-10-02 12:48:55 -0400

    10039 - removing unused action

--- web-client/src/presenter/actions/resetJudgeActivityReportStateAction.ts
+++ web-client/src/presenter/actions/resetJudgeActivityReportStateAction.ts
@@ -1,6 +1,6 @@
 import { cloneDeep } from 'lodash';
 import { initialJudgeActivityReportState } from '../judgeActivityReportState';
-import { state } from 'cerebral';
+import { state } from '@web-client/presenter/app.cerebral';
 
 export const resetJudgeActivityReportStateAction = ({ store }) => {
   store.set(
