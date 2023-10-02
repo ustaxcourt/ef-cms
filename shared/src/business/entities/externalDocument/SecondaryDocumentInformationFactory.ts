5db059619907fd333b70e39997a3861f92ebaade
John Cruz <jcruz@flexion.us>
2023-10-02 10:31:26 -0600

    devex-1187: Implement new schema for "SecondaryDocumentInformationFactory";

--- shared/src/business/entities/externalDocument/SecondaryDocumentInformationFactory.ts
+++ shared/src/business/entities/externalDocument/SecondaryDocumentInformationFactory.ts
@@ -2,6 +2,7 @@ import { JoiValidationConstants } from '../JoiValidationConstants';
 import { JoiValidationEntity } from '@shared/business/entities/JoiValidationEntity';
 import { includes } from 'lodash';
 import { makeRequiredHelper } from './externalDocumentHelpers';
+import { setDefaultErrorMessages } from '@shared/business/entities/utilities/setDefaultErrorMessages';
 import joi from 'joi';
 
 export class SecondaryDocumentInformationFactory extends JoiValidationEntity {
@@ -36,6 +37,65 @@ export class SecondaryDocumentInformationFactory extends JoiValidationEntity {
       certificateOfService: joi.boolean(),
       certificateOfServiceDate: JoiValidationConstants.ISO_DATE.max('now'),
       objections: JoiValidationConstants.STRING,
+    };
+
+    const makeRequired = itemName => {
+      makeRequiredHelper({
+        itemName,
+        schema,
+        schemaOptionalItems,
+      });
+    };
+
+    if (this.secondaryDocumentFile) {
+      makeRequired('attachments');
+      makeRequired('certificateOfService');
+
+      if (this.certificateOfService === true) {
+        makeRequired('certificateOfServiceDate');
+      }
+
+      if (
+        this.category === 'Motion' ||
+        includes(
+          [
+            'Motion to Withdraw Counsel',
+            'Motion to Withdraw As Counsel',
+            'Application to Take Deposition',
+          ],
+          this.documentType,
+        )
+      ) {
+        makeRequired('objections');
+      }
+    }
+    return schema;
+  }
+
+  getValidationRules_NEW() {
+    let schema = {};
+
+    let schemaOptionalItems = {
+      attachments: joi
+        .boolean()
+        .messages(setDefaultErrorMessages('Enter selection for Attachments.')),
+      certificateOfService: joi
+        .boolean()
+        .messages(
+          setDefaultErrorMessages(
+            'Indicate whether you are including a Certificate of Service',
+          ),
+        ),
+      certificateOfServiceDate: JoiValidationConstants.ISO_DATE.max(
+        'now',
+      ).messages({
+        ...setDefaultErrorMessages('Enter date of service'),
+        'date.max':
+          'Certificate of Service date cannot be in the future. Enter a valid date.',
+      }),
+      objections: JoiValidationConstants.STRING.messages(
+        setDefaultErrorMessages('Enter selection for Objections.'),
+      ),
     };
 
     const makeRequired = itemName => {
