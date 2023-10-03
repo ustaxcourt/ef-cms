dc089db928a118347164d5a7294b75feb7c35a9c
John Cruz <jcruz@flexion.us>
2023-10-03 08:05:34 -0600

    devex-1187: Implement new schema for "DocumentSearch";

--- shared/src/business/entities/documents/DocumentSearch.ts
+++ shared/src/business/entities/documents/DocumentSearch.ts
@@ -6,6 +6,7 @@ import {
   createEndOfDayISO,
   createStartOfDayISO,
 } from '../../utilities/DateHandler';
+import { setDefaultErrorMessages } from '@shared/business/entities/utilities/setDefaultErrorMessages';
 import joi from 'joi';
 
 export class DocumentSearch extends JoiValidationEntity {
@@ -160,6 +161,97 @@ export class DocumentSearch extends JoiValidationEntity {
 
   getValidationRules() {
     return DocumentSearch.VALIDATION_RULES;
+  }
+
+  static VALIDATION_RULES_NEW = joi
+    .object()
+    .keys({
+      caseTitleOrPetitioner: JoiValidationConstants.STRING.allow(
+        '',
+      ).description(
+        'The case title or petitioner name to filter the search results by',
+      ),
+      dateRange: JoiValidationConstants.STRING.allow('').optional(),
+      docketNumber: JoiValidationConstants.STRING.allow('').description(
+        'The docket number to filter the search results by',
+      ),
+      endDate: joi
+        .alternatives()
+        .conditional('startDate', {
+          is: joi.exist().not(null),
+          otherwise: JoiValidationConstants.ISO_DATE.format(
+            DocumentSearch.JOI_VALID_DATE_SEARCH_FORMATS,
+          )
+            .less(joi.ref('tomorrow'))
+            .optional()
+            .description(
+              'The end date search filter is not required if there is no start date',
+            ),
+          then: JoiValidationConstants.ISO_DATE.format(
+            DocumentSearch.JOI_VALID_DATE_SEARCH_FORMATS,
+          )
+            .less(joi.ref('tomorrow'))
+            .min(joi.ref('startDate'))
+            .optional()
+            .description(
+              'The end date search filter must be greater than or equal to the start date, and less than or equal to the current date',
+            ),
+        })
+        .messages({
+          ...setDefaultErrorMessages('Enter a valid end date'),
+          'date.less':
+            'End date cannot be in the future. Enter valid end date.',
+        }),
+      from: joi
+        .number()
+        .integer()
+        .min(0)
+        .required()
+        .description(
+          'The zero-based index representing which page of results we are requesting',
+        ),
+      judge: JoiValidationConstants.STRING.allow('')
+        .optional()
+        .description('The name of the judge to filter the search results by'),
+      keyword: JoiValidationConstants.STRING.optional()
+        .allow('')
+        .description('The keyword to search by'),
+      startDate: joi
+        .alternatives()
+        .conditional('dateRange', {
+          is: DATE_RANGE_SEARCH_OPTIONS.CUSTOM_DATES,
+          otherwise: joi.forbidden(),
+          then: JoiValidationConstants.ISO_DATE.format(
+            DocumentSearch.JOI_VALID_DATE_SEARCH_FORMATS,
+          )
+            .max('now')
+            .required()
+            .description(
+              'The start date to search by, which cannot be greater than the current date, and is required when there is an end date provided',
+            ),
+        })
+        .messages({
+          ...setDefaultErrorMessages('Enter a valid start date'),
+          'date.max':
+            'Start date cannot be in the future. Enter valid start date.',
+        }),
+      tomorrow: joi
+        .optional()
+        .description(
+          'The computed value to validate the endDate against, in order to verify that the endDate is less than or equal to the current date',
+        ),
+      userRole: JoiValidationConstants.STRING.allow('')
+        .optional()
+        .description('The role of the user performing the search'),
+    })
+    .oxor('caseTitleOrPetitioner', 'docketNumber')
+    .messages({
+      'object.oxor':
+        'Enter either a Docket number or a Case name/Petitioner name, not both',
+    });
+
+  getValidationRules_NEW() {
+    return DocumentSearch.VALIDATION_RULES_NEW;
   }
 
   getErrorToMessageMap() {
