fc13d3c405b13108c18f2225df674400f98c2a37
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-10-03 07:28:31 -0700

    9995: Allow irsNoticeDate to be edited and cleared when hasIrsNoticedate is set to false, fix integration test

--- web-client/src/presenter/actions/setupEditPetitionDetailFormAction.ts
+++ web-client/src/presenter/actions/setupEditPetitionDetailFormAction.ts
@@ -1,14 +1,6 @@
-import { FORMATS } from '@shared/business/utilities/DateHandler';
 import { state } from '@web-client/presenter/app.cerebral';
 
-/**
- * sets the form with the necessary date related state that is found on the caseDetail for petition fee payments.
- * @param {object} providers the providers object
- * @param {object} providers.props the cerebral props object containing the props.caseDetail
- * @param {object} providers.store the cerebral store used for setting the state.caseDetail
- */
 export const setupEditPetitionDetailFormAction = ({
-  applicationContext,
   get,
   store,
 }: ActionProps) => {
@@ -17,6 +9,7 @@ export const setupEditPetitionDetailFormAction = ({
   store.set(state.form, {
     caseType: caseDetail.caseType,
     hasVerifiedIrsNotice: caseDetail.hasVerifiedIrsNotice,
+    irsNoticeDate: caseDetail.irsNoticeDate,
     petitionPaymentDate: caseDetail.petitionPaymentDate,
     petitionPaymentMethod: caseDetail.petitionPaymentMethod,
     petitionPaymentStatus: caseDetail.petitionPaymentStatus,
@@ -25,13 +18,4 @@ export const setupEditPetitionDetailFormAction = ({
     procedureType: caseDetail.procedureType,
     statistics: caseDetail.statistics,
   });
-
-  if (caseDetail.irsNoticeDate) {
-    // does this date need formatting?
-    const irsNoticeDate = applicationContext
-      .getUtilities()
-      .formatDateString(caseDetail.irsNoticeDate, FORMATS.YYYYMMDD);
-
-    store.set(state.form.irsNoticeDate, irsNoticeDate);
-  }
 };
