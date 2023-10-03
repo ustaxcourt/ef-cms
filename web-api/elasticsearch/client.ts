49df5d857707c1cd0c6a86f5caa13faa0f4a4d55
Jim Lerza <james.lerza@ustaxcourt.gov>
2023-10-03 15:46:02 -0400

    devex-1027: convert es client to AWS SDK v3

--- web-api/elasticsearch/client.ts
+++ web-api/elasticsearch/client.ts
@@ -1,68 +1,68 @@
 import { AwsSigv4Signer } from '@opensearch-project/opensearch/aws';
 import { Client } from '@opensearch-project/opensearch';
-import { DomainInfoList } from 'aws-sdk/clients/es';
+import {
+  DescribeDomainCommand,
+  ListDomainNamesCommand,
+  OpenSearchClient,
+} from '@aws-sdk/client-opensearch';
+import { defaultProvider } from '@aws-sdk/credential-provider-node';
 import { getVersion } from '../../shared/admin-tools/util';
-import AWS from 'aws-sdk';
-
-const es = new AWS.ES({
-  region: 'us-east-1',
-});
 
 export const getHost = async (
   DomainName: string,
 ): Promise<string | undefined> => {
-  try {
-    const result = await es
-      .describeElasticsearchDomain({
-        DomainName,
-      })
-      .promise();
-    return result.DomainStatus.Endpoint;
-  } catch (err) {
-    console.error(`could not find resource for ${DomainName}`, err);
-  }
-  return;
+  const openSearchClient = new OpenSearchClient({ region: 'us-east-1' });
+  const describeDomainCommand: DescribeDomainCommand =
+    new DescribeDomainCommand({ DomainName });
+  const describeDomainResponse = await openSearchClient.send(
+    describeDomainCommand,
+  );
+  return describeDomainResponse.DomainStatus?.Endpoint || undefined;
 };
 
 export const getClient = async ({
+  elasticsearchEndpoint,
   environmentName,
   version,
 }: {
+  elasticsearchEndpoint?: string;
   environmentName: string;
-  version: string;
+  version?: string;
 }): Promise<Client> => {
-  version = version || (await getVersion());
-  const domainName =
-    version === 'info' ? 'info' : `efcms-search-${environmentName}-${version}`;
-  const host = await getHost(domainName);
+  if (!elasticsearchEndpoint) {
+    version = version || (await getVersion());
+    const domainName =
+      version === 'info'
+        ? 'info'
+        : `efcms-search-${environmentName}-${version}`;
+    const host = await getHost(domainName);
+    elasticsearchEndpoint = `https://${host}:443`;
+  } else {
+    if (environmentName !== 'local') {
+      elasticsearchEndpoint = `https://${elasticsearchEndpoint}:443`;
+    }
+  }
 
   return new Client({
     ...AwsSigv4Signer({
-      getCredentials: () =>
-        new Promise((resolve, reject) => {
-          AWS.config.getCredentials((err, credentials) => {
-            if (err) {
-              reject(err);
-            } else {
-              if (credentials) {
-                resolve(credentials);
-              }
-            }
-          });
-        }),
-
+      getCredentials: () => {
+        const credentialsProvider = defaultProvider();
+        return credentialsProvider();
+      },
       region: 'us-east-1',
     }),
-    node: `https://${host}:443`,
+    node: elasticsearchEndpoint,
   });
 };
 
-export const listDomains = async (): Promise<DomainInfoList | undefined> => {
-  try {
-    const res = await es.listDomainNames().promise();
-    return res.DomainNames;
-  } catch (err) {
-    console.error('unable to list elasticsearch domains', err);
-  }
-  return;
+export const listDomains = async (): Promise<string[] | undefined> => {
+  const openSearchClient = new OpenSearchClient({ region: 'us-east-1' });
+  const listDomainsCommand: ListDomainNamesCommand = new ListDomainNamesCommand(
+    { EngineType: 'OpenSearch' || 'Elasticsearch' },
+  );
+  const listDomainsResponse = await openSearchClient.send(listDomainsCommand);
+  return (
+    listDomainsResponse.DomainNames?.map(domain => domain.DomainName) ||
+    undefined
+  );
 };
--- web-api/elasticsearch/elasticsearch-index-settings.ts
+++ web-api/elasticsearch/elasticsearch-index-settings.ts
@@ -1,56 +1,24 @@
-import { AwsSigv4Signer } from '@opensearch-project/opensearch/aws';
 import { Client } from '@opensearch-project/opensearch';
 import { elasticsearchIndexes } from './elasticsearch-indexes';
 import { elasticsearchMappings } from './elasticsearch-mappings';
 import { esSettingsType } from './elasticsearch-settings';
+import { getClient } from './client';
 import { settings } from './elasticsearch-settings';
-import AWS from 'aws-sdk';
 
 (async () => {
-  AWS.config.region = 'us-east-1';
-  if (AWS.config.httpOptions) {
-    AWS.config.httpOptions.timeout = 300000;
-  }
-
-  const environment: {
-    elasticsearchEndpoint: string;
-    region: string;
-    stage: string;
-  } = {
-    elasticsearchEndpoint: process.argv[2],
-    region: 'us-east-1',
-    stage: process.env.ENV || 'local',
-  };
-
+  const environmentName: string = process.env.ENV || 'local';
+  const elasticsearchEndpoint: string = process.argv[2];
   const overriddenNumberOfReplicasIfNonProd: number = Number(
     process.env.OVERRIDE_ES_NUMBER_OF_REPLICAS,
   );
-  const deployingEnvironment: string = process.env.ENV!;
 
-  const openSearchClient = new Client({
-    ...AwsSigv4Signer({
-      getCredentials: () =>
-        new Promise((resolve, reject) => {
-          AWS.config.getCredentials((err, credentials) => {
-            if (err) {
-              reject(err);
-            } else {
-              if (credentials) {
-                resolve(credentials);
-              }
-            }
-          });
-        }),
-      region: environment.region,
-    }),
-    node:
-      environment.stage === 'local'
-        ? environment.elasticsearchEndpoint
-        : `https://${environment.elasticsearchEndpoint}:443`,
+  const openSearchClient: Client = await getClient({
+    elasticsearchEndpoint,
+    environmentName,
   });
 
   const esSettings: esSettingsType = settings({
-    environment: deployingEnvironment,
+    environment: environmentName,
     overriddenNumberOfReplicasIfNonProd,
   });
 
