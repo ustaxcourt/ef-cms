260a52b49b3c7f13de66f5af076f5c4dd8f6fc7b
John Cruz <jcruz@flexion.us>
2023-10-03 08:45:44 -0600

    devex-1187: Implement new schema for "CalendarNote";

--- shared/src/business/entities/notes/CalendarNote.ts
+++ shared/src/business/entities/notes/CalendarNote.ts
@@ -1,5 +1,6 @@
 import { JoiValidationConstants } from '../JoiValidationConstants';
 import { JoiValidationEntity } from '../JoiValidationEntity';
+import { setDefaultErrorMessages } from '@shared/business/entities/utilities/setDefaultErrorMessages';
 
 export class CalendarNote extends JoiValidationEntity {
   public note?: string;
@@ -20,6 +21,21 @@ export class CalendarNote extends JoiValidationEntity {
 
   getValidationRules() {
     return CalendarNote.VALIDATION_RULES;
+  }
+
+  static VALIDATION_RULES_NEW = {
+    note: JoiValidationConstants.STRING.max(200)
+      .allow('', null)
+      .optional()
+      .messages(
+        setDefaultErrorMessages(
+          'Limit is 200 characters. Enter 200 or fewer characters.',
+        ),
+      ),
+  };
+
+  getValidationRules_NEW() {
+    return CalendarNote.VALIDATION_RULES_NEW;
   }
 
   getErrorToMessageMap() {
