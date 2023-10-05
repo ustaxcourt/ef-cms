6e021fa6e7def320132faae7239fdd21d6d56ef6
Jim Lerza <james.lerza@ustaxcourt.gov>
2023-10-05 17:28:51 -0400

    devex-1027: added a script to create new ES indices which include the md5 hash of their respective mappings in the index name, reindex those indices, delete the originals, and create aliases to the new indices using the original name as the alias

---
+++ scripts/elasticsearch/setup-versioned-indices-and-create-aliases.ts
@@ -0,0 +1,40 @@
+import { Client } from '@opensearch-project/opensearch';
+import { areAllReindexTasksFinished } from '../../shared/admin-tools/elasticsearch/check-reindex-complete';
+import { baseAliases } from '../../web-api/elasticsearch/elasticsearch-aliases';
+import { getClient } from '../../web-api/elasticsearch/client';
+import { reindexIfNecessary } from './reindex.helpers';
+import { requireEnvVars } from '../../shared/admin-tools/util';
+import { setupAliases } from '../../web-api/elasticsearch/elasticsearch-alias-settings.helpers';
+import { setupIndexes } from '../../web-api/elasticsearch/elasticsearch-index-settings.helpers';
+
+requireEnvVars(['ENV', 'ELASTICSEARCH_ENDPOINT']);
+
+(async () => {
+  const elasticsearchEndpoint = process.env.ELASTICSEARCH_ENDPOINT!;
+  const environmentName = process.env.ENV!;
+
+  const client: Client = await getClient({
+    elasticsearchEndpoint,
+    environmentName,
+  });
+
+  // create new indices
+  await setupIndexes({ client, environmentName });
+
+  // reindex new indices
+  await reindexIfNecessary({ client });
+
+  // wait for reindex tasks to complete
+  let reindexFinished: boolean;
+  do {
+    reindexFinished = await areAllReindexTasksFinished({ environmentName });
+    await new Promise(resolve => setTimeout(resolve, 5000));
+  } while (!reindexFinished);
+
+  // delete the old indices
+  const baseAliasNames = baseAliases.map(a => a.alias);
+  await client.indices.delete({ index: baseAliasNames });
+
+  // create new aliases
+  await setupAliases({ client });
+})();
