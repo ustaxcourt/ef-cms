a80e9af157f8f13634cb6d12248dd6e027bfc393
Absolutestunna <joshujah007@gmail.com>
2023-10-06 17:35:25 -0400

    Design debt 1159: Refactor SortableColumnHeader component to NOT hardcode where sort fields are found on state, now it's just a simple react component that is given all it needs to know from the parent component

--- web-client/src/presenter/actions/TrialSessionWorkingCopy/toggleWorkingCopySortAction.ts
+++ web-client/src/presenter/actions/TrialSessionWorkingCopy/toggleWorkingCopySortAction.ts
@@ -1,29 +1,20 @@
 import { state } from '@web-client/presenter/app.cerebral';
 
-/**
- * toggle the sort for the working copy table
- *
- * @param {object} providers the providers object
- * @param {object} providers.props the cerebral props object
- * @param {object} providers.store the cerebral store
- * @param {object} providers.get the cerebral get function
- */
 export const toggleWorkingCopySortAction = ({
   get,
   props,
   store,
-}: ActionProps) => {
-  const { sort } = props;
+}: ActionProps<{ sortField: string }>) => {
+  const { sortField } = props;
   const fromSort = get(state.trialSessionWorkingCopy.sort);
   const fromSortOrder = get(state.trialSessionWorkingCopy.sortOrder);
 
-  const newSort = sort;
   let newSortOrder = 'asc';
 
-  if (sort === fromSort) {
+  if (sortField === fromSort) {
     newSortOrder = (fromSortOrder === 'desc' && 'asc') || 'desc';
   }
 
-  store.set(state.trialSessionWorkingCopy.sort, newSort);
+  store.set(state.trialSessionWorkingCopy.sort, sortField);
   store.set(state.trialSessionWorkingCopy.sortOrder, newSortOrder);
 };
