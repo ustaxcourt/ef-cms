b5d66a59c3d89efd9765e08cc8a46ae0bf2fb4e1
Zachary Rogers <zrogers>
2023-10-06 15:35:51 -0700

    Design debt 1159: sortable column component now determines and outputs the columns new sort order

--- web-client/src/presenter/actions/TrialSessionWorkingCopy/toggleWorkingCopySortAction.ts
+++ web-client/src/presenter/actions/TrialSessionWorkingCopy/toggleWorkingCopySortAction.ts
@@ -1,20 +1,9 @@
 import { state } from '@web-client/presenter/app.cerebral';
 
 export const toggleWorkingCopySortAction = ({
-  get,
   props,
   store,
-}: ActionProps<{ sortField: string }>) => {
-  const { sortField } = props;
-  const fromSort = get(state.trialSessionWorkingCopy.sort);
-  const fromSortOrder = get(state.trialSessionWorkingCopy.sortOrder);
-
-  let newSortOrder = 'asc';
-
-  if (sortField === fromSort) {
-    newSortOrder = (fromSortOrder === 'desc' && 'asc') || 'desc';
-  }
-
-  store.set(state.trialSessionWorkingCopy.sort, sortField);
-  store.set(state.trialSessionWorkingCopy.sortOrder, newSortOrder);
+}: ActionProps<{ sortField: string; sortOrder: 'asc' | 'desc' }>) => {
+  store.set(state.trialSessionWorkingCopy.sort, props.sortField);
+  store.set(state.trialSessionWorkingCopy.sortOrder, props.sortOrder);
 };
