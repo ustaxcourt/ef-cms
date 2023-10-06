8c880ad9d25a984a023bcea02e7338721f036e4d
Absolutestunna <joshujah007@gmail.com>
2023-10-06 18:14:50 -0400

    Design debt 1159: WIP - attempting to use SortableColumnHeaderButton for sortable columns in MessagesIndividualCompleted [skip-ci]

--- web-client/src/presenter/actions/setTableSortConfigurationAction.ts
+++ web-client/src/presenter/actions/setTableSortConfigurationAction.ts
@@ -1,24 +1,21 @@
-import { getConstants } from '../../getConstants';
 import { state } from '@web-client/presenter/app.cerebral';
 
-const { ASCENDING, DESCENDING } = getConstants();
-
-/**
- * toggle the sort for the clicked sortable table button
- * @param {object} providers the providers object
- * @param {object} providers.props the cerebral props object
- * @param {object} providers.store the cerebral store
- * @param {object} providers.get the cerebral get function
- */
 export const setTableSortConfigurationAction = ({
+  applicationContext,
   get,
   props,
   store,
-}: ActionProps) => {
-  const { defaultSort, sortField } = props;
+}: ActionProps<{
+  sortField: string;
+}>) => {
+  const { sortField } = props;
+
+  const { ASCENDING, DESCENDING } = applicationContext.getConstants();
+
   const fromSortOrder = get(state.tableSort.sortOrder);
+
   if (get(state.tableSort.sortField) !== sortField) {
-    store.set(state.tableSort.sortOrder, defaultSort);
+    store.set(state.tableSort.sortOrder, DESCENDING);
   } else {
     const newSortOrder = fromSortOrder === DESCENDING ? ASCENDING : DESCENDING;
     store.set(state.tableSort.sortOrder, newSortOrder);
