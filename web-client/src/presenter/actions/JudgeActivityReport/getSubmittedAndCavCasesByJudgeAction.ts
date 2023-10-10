b7f0ba3dbbcfd10b39561873f57bca636ae5ba59
John Cruz <jcruz@flexion.us>
2023-10-10 14:19:36 -0600

    10159: Fix merge mistakes;

--- web-client/src/presenter/actions/JudgeActivityReport/getSubmittedAndCavCasesByJudgeAction.ts
+++ web-client/src/presenter/actions/JudgeActivityReport/getSubmittedAndCavCasesByJudgeAction.ts
@@ -1,25 +1,17 @@
-import {
-  CAV_AND_SUBMITTED_CASES_PAGE_SIZE,
-  CAV_AND_SUBMITTED_CASE_STATUS,
-} from '../../../../../shared/src/business/entities/EntityConstants';
+import { CAV_AND_SUBMITTED_CASE_STATUS } from '../../../../../shared/src/business/entities/EntityConstants';
 import { state } from '@web-client/presenter/app.cerebral';
 
 export const getSubmittedAndCavCasesByJudgeAction = async ({
   applicationContext,
   get,
-  props,
-}: ActionProps<{
-  selectedPage: number;
-}>) => {
+}: ActionProps) => {
   const { judges } = get(state.judgeActivityReport.filters);
-  const pageNumber = props.selectedPage;
 
   const { cases, totalCount } = await applicationContext
     .getUseCases()
     .getCasesByStatusAndByJudgeInteractor(applicationContext, {
       judges,
-      pageNumber,
-      pageSize: CAV_AND_SUBMITTED_CASES_PAGE_SIZE,
+
       statuses: CAV_AND_SUBMITTED_CASE_STATUS,
     });
 
