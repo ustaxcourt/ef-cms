e25054ce708564ede6e8a6d377b4fff319391e93
Zachary Rogers <zrogers>
2023-10-10 17:27:54 -0700

    10161: Pass along judge user to avoid making an additional network call

--- web-client/src/presenter/actions/setJudgeUserAction.ts
+++ web-client/src/presenter/actions/setJudgeUserAction.ts
@@ -1,12 +1,8 @@
 import { state } from '@web-client/presenter/app.cerebral';
 
 export const setJudgeUserAction = ({
-  applicationContext,
   props,
   store,
-}: ActionProps<{ judgeUser?: RawUser }>) => {
-  const user = applicationContext.getCurrentUser();
-  const judgeUser = props.judgeUser || user;
-
-  store.set(state.judgeUser, judgeUser);
+}: ActionProps<{ judgeUser: RawUser }>) => {
+  store.set(state.judgeUser, props.judgeUser);
 };
