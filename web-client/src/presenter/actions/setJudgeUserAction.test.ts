148bad2bd319e67b87a2c7453822b5d38f654a69
Zachary Rogers <zrogers>
2023-10-10 17:27:54 -0700

    10161: Pass along judge user to avoid making an additional network call

--- web-client/src/presenter/actions/setJudgeUserAction.test.ts
+++ web-client/src/presenter/actions/setJudgeUserAction.test.ts
@@ -5,9 +5,8 @@ import { runAction } from '@web-client/presenter/test.cerebral';
 import { setJudgeUserAction } from './setJudgeUserAction';
 
 describe('setJudgeUserAction', () => {
-  beforeAll(() => {
-    presenter.providers.applicationContext = applicationContext;
-  });
+  presenter.providers.applicationContext = applicationContext;
+
   it('sets state.judgeUser to the passed in props.judgeUser', async () => {
     const { state } = await runAction(setJudgeUserAction, {
       modules: { presenter },
@@ -16,19 +15,5 @@ describe('setJudgeUserAction', () => {
       },
     });
     expect(state.judgeUser).toMatchObject(judgeUser);
-  });
-
-  it('sets judge user on state using the currently logged in user when props.judgeUser is not provided', async () => {
-    applicationContext.getCurrentUser.mockReturnValue(judgeUser);
-
-    const result = await runAction(setJudgeUserAction, {
-      modules: { presenter },
-      props: {},
-      state: {
-        judgeUser: undefined,
-      },
-    });
-
-    expect(result.state.judgeUser).toEqual(judgeUser);
   });
 });
