bb665495a6980c6d4eb70c3a913a64fd4969c50f
Nechama Krigsman <nkrigsman@flexion.us>
2023-10-17 11:03:39 -0400

    devex-1187: update entities to use new validation method

--- shared/src/business/entities/User.ts
+++ shared/src/business/entities/User.ts
@@ -1,7 +1,6 @@
 import {
   CASE_SERVICES_SUPERVISOR_SECTION,
   COUNTRY_TYPES,
-  JUDGE_TITLES,
   JudgeTitle,
   ROLES,
   STATE_NOT_AVAILABLE,
@@ -76,73 +75,6 @@ export class User extends JoiValidationEntity {
     this.section = rawUser.section;
   }
 
-  static USER_CONTACT_VALIDATION_RULES = {
-    address1: JoiValidationConstants.STRING.max(100).required(),
-    address2: JoiValidationConstants.STRING.max(100).optional().allow(null),
-    address3: JoiValidationConstants.STRING.max(100).optional().allow(null),
-    city: JoiValidationConstants.STRING.max(100).required(),
-    country: JoiValidationConstants.STRING.when('countryType', {
-      is: COUNTRY_TYPES.INTERNATIONAL,
-      otherwise: joi.optional().allow(null),
-      then: joi.required(),
-    }),
-    countryType: JoiValidationConstants.STRING.valid(
-      COUNTRY_TYPES.DOMESTIC,
-      COUNTRY_TYPES.INTERNATIONAL,
-    ).required(),
-    phone: JoiValidationConstants.STRING.max(100).required(),
-    postalCode: joi.when('countryType', {
-      is: COUNTRY_TYPES.INTERNATIONAL,
-      otherwise: JoiValidationConstants.US_POSTAL_CODE.required(),
-      then: JoiValidationConstants.STRING.max(100).required(),
-    }),
-    state: JoiValidationConstants.STRING.when('countryType', {
-      is: COUNTRY_TYPES.INTERNATIONAL,
-      otherwise: joi
-        .valid(
-          ...Object.keys(US_STATES),
-          ...Object.keys(US_STATES_OTHER),
-          STATE_NOT_AVAILABLE,
-        )
-        .required(),
-      then: joi.optional().allow(null),
-    }),
-  };
-
-  static BASE_USER_VALIDATION = {
-    judgeFullName: JoiValidationConstants.STRING.max(100).when('role', {
-      is: ROLES.judge,
-      otherwise: joi.optional().allow(null),
-      then: joi.required(),
-    }),
-    judgeTitle: joi.when('role', {
-      is: ROLES.judge,
-      otherwise: joi.optional().allow(null),
-      then: JoiValidationConstants.STRING.valid(...JUDGE_TITLES).required(),
-    }),
-    name: JoiValidationConstants.STRING.max(100).required(),
-    role: JoiValidationConstants.STRING.valid(
-      ...Object.values(ROLES),
-    ).required(),
-  };
-
-  static VALIDATION_ERROR_MESSAGES = {
-    address1: 'Enter mailing address',
-    city: 'Enter city',
-    country: 'Enter a country',
-    countryType: 'Enter country type',
-    name: 'Enter name',
-    phone: 'Enter phone number',
-    postalCode: [
-      {
-        contains: 'match',
-        message: 'Enter ZIP code',
-      },
-      'Enter ZIP code',
-    ],
-    state: 'Enter state',
-  };
-
   isChambersUser(): boolean {
     return !!this.section?.includes('Chambers');
   }
@@ -183,33 +115,7 @@ export class User extends JoiValidationEntity {
     return section === CASE_SERVICES_SUPERVISOR_SECTION;
   }
 
-  getValidationRules() {
-    return {
-      ...User.BASE_USER_VALIDATION,
-      contact: joi.object().keys(User.USER_CONTACT_VALIDATION_RULES).optional(),
-      email: JoiValidationConstants.EMAIL.optional(),
-      entityName: JoiValidationConstants.STRING.valid('User').required(),
-      isSeniorJudge: joi.when('role', {
-        is: ROLES.judge,
-        otherwise: joi.optional().allow(null),
-        then: joi.boolean().required(),
-      }),
-      isUpdatingInformation: joi
-        .boolean()
-        .optional()
-        .description(
-          'Whether the contact information for the user is being updated.',
-        ),
-      pendingEmail: JoiValidationConstants.EMAIL.allow(null).optional(),
-      pendingEmailVerificationToken:
-        JoiValidationConstants.UUID.allow(null).optional(),
-      section: JoiValidationConstants.STRING.optional(),
-      token: JoiValidationConstants.STRING.optional(),
-      userId: JoiValidationConstants.UUID.required(),
-    };
-  }
-
-  static USER_CONTACT_VALIDATION_RULES_NEW = {
+  static USER_CONTACT_VALIDATION_RULES = {
     address1: JoiValidationConstants.STRING.max(100)
       .required()
       .messages(setDefaultErrorMessage('Enter mailing address')),
@@ -252,7 +158,7 @@ export class User extends JoiValidationEntity {
     }).messages(setDefaultErrorMessage('Enter state')),
   };
 
-  static BASE_USER_VALIDATION_NEW = {
+  static BASE_USER_VALIDATION = {
     judgeFullName: JoiValidationConstants.STRING.max(100).when('role', {
       is: ROLES.judge,
       otherwise: joi.optional().allow(null),
@@ -271,13 +177,10 @@ export class User extends JoiValidationEntity {
     ).required(),
   };
 
-  getValidationRules_NEW() {
+  getValidationRules() {
     return {
-      ...User.BASE_USER_VALIDATION_NEW,
-      contact: joi
-        .object()
-        .keys(User.USER_CONTACT_VALIDATION_RULES_NEW)
-        .optional(),
+      ...User.BASE_USER_VALIDATION,
+      contact: joi.object().keys(User.USER_CONTACT_VALIDATION_RULES).optional(),
       email: JoiValidationConstants.EMAIL.optional(),
       entityName: JoiValidationConstants.STRING.valid('User').required(),
       isUpdatingInformation: joi
@@ -294,15 +197,9 @@ export class User extends JoiValidationEntity {
       userId: JoiValidationConstants.UUID.required(),
     };
   }
-
-  getErrorToMessageMap() {
-    return User.VALIDATION_ERROR_MESSAGES;
-  }
 }
 
-export const { USER_CONTACT_VALIDATION_RULES, VALIDATION_ERROR_MESSAGES } =
-  User;
-export const { USER_CONTACT_VALIDATION_RULES_NEW } = User;
+export const { USER_CONTACT_VALIDATION_RULES } = User;
 
 declare global {
   type RawUser = ExcludeMethods<User>;
--- shared/src/business/entities/contacts/Petitioner.ts
+++ shared/src/business/entities/contacts/Petitioner.ts
@@ -1,10 +1,7 @@
 import { CONTACT_TYPES, SERVICE_INDICATOR_TYPES } from '../EntityConstants';
 import { JoiValidationConstants } from '../JoiValidationConstants';
 import { JoiValidationEntity } from '../JoiValidationEntity';
-import {
-  USER_CONTACT_VALIDATION_RULES,
-  USER_CONTACT_VALIDATION_RULES_NEW,
-} from '../User';
+import { USER_CONTACT_VALIDATION_RULES } from '../User';
 import { formatPhoneNumber } from '../../utilities/formatPhoneNumber';
 import { setDefaultErrorMessage } from '@shared/business/entities/utilities/setDefaultErrorMessage';
 import joi from 'joi';
@@ -70,82 +67,6 @@ export class Petitioner extends JoiValidationEntity {
 
   static VALIDATION_RULES = {
     ...USER_CONTACT_VALIDATION_RULES,
-    additionalName: JoiValidationConstants.STRING.max(100).optional(),
-    contactId: JoiValidationConstants.UUID.required().description(
-      'Unique contact ID only used by the system.',
-    ),
-    contactType: JoiValidationConstants.STRING.valid(
-      ...Object.values(CONTACT_TYPES),
-    ).required(),
-    email: JoiValidationConstants.EMAIL.when('hasEAccess', {
-      is: true,
-      otherwise: joi.optional(),
-      then: joi.required(),
-    }),
-    hasConsentedToEService: joi
-      .boolean()
-      .optional()
-      .description(
-        'Flag that indicates the petitioner has consented to receive electronic service on a paper petition',
-      ),
-    hasEAccess: joi
-      .boolean()
-      .optional()
-      .description(
-        'Flag that indicates if the petitioner has credentials to both the legacy and new system.',
-      ),
-    inCareOf: JoiValidationConstants.STRING.max(100).optional(),
-    isAddressSealed: joi.boolean().required(),
-    name: JoiValidationConstants.STRING.max(100).required(),
-    paperPetitionEmail: JoiValidationConstants.EMAIL.optional().description(
-      'Email provided by the petitioner on a paper petition',
-    ),
-    sealedAndUnavailable: joi.boolean().optional(),
-    serviceIndicator: JoiValidationConstants.STRING.valid(
-      ...Object.values(SERVICE_INDICATOR_TYPES),
-    ).required(),
-    title: JoiValidationConstants.STRING.max(100).optional(),
-  };
-
-  static VALIDATION_ERROR_MESSAGES = {
-    additionalName: [
-      {
-        contains: 'must be less than or equal to',
-        message: 'Limit is 100 characters. Enter 100 or fewer characters.',
-      },
-    ],
-    address1: 'Enter mailing address',
-    city: 'Enter city',
-    contactType: 'Select a role type',
-    contactTypeSecondIntervenor:
-      'Only one (1) Intervenor is allowed per case. Please select a different Role.',
-    country: 'Enter a country',
-    countryType: 'Enter country type',
-    name: [
-      {
-        contains: 'must be less than or equal to',
-        message: 'Limit is 100 characters. Enter 100 or fewer characters.',
-      },
-      'Enter name',
-    ],
-    phone: 'Enter phone number',
-    postalCode: [
-      {
-        contains: 'match',
-        message: 'Enter ZIP code',
-      },
-      'Enter ZIP code',
-    ],
-    serviceIndicator: 'Select a service indicator',
-    state: 'Enter state',
-  } as const;
-
-  getValidationRules() {
-    return Petitioner.VALIDATION_RULES;
-  }
-
-  static VALIDATION_RULES_NEW = {
-    ...USER_CONTACT_VALIDATION_RULES_NEW,
     additionalName: JoiValidationConstants.STRING.max(100).optional().messages({
       'string.max': 'Limit is 100 characters. Enter 100 or fewer characters.',
     }),
@@ -194,12 +115,8 @@ export class Petitioner extends JoiValidationEntity {
     title: JoiValidationConstants.STRING.max(100).optional(),
   };
 
-  getValidationRules_NEW() {
-    return Petitioner.VALIDATION_RULES_NEW;
-  }
-
-  getErrorToMessageMap() {
-    return Petitioner.VALIDATION_ERROR_MESSAGES;
+  getValidationRules() {
+    return Petitioner.VALIDATION_RULES;
   }
 }
 
