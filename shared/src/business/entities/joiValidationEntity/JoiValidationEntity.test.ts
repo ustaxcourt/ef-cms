a917c7e8ddea436d759eec545b09f72f39fb4f7c
Nechama Krigsman <nkrigsman@flexion.us>
2023-10-17 16:02:42 -0400

    devex-1187: remove tests from the old method

--- shared/src/business/entities/joiValidationEntity/JoiValidationEntity.test.ts
+++ shared/src/business/entities/joiValidationEntity/JoiValidationEntity.test.ts
@@ -3,278 +3,138 @@ import { TestEntity } from '@shared/business/entities/joiValidationEntity/test/T
 
 describe('Joi Entity', () => {
   describe('getFormattedValidationErrors', () => {
-    describe('LEGACY IMPLEMENTATION', () => {
-      it('should return null if there are no errors in validation', () => {
+    it('should return null if there are no errors in validation', () => {
+      const testEntity = new TestEntity({
+        arrayErrorMessage: 'APPROVED',
+        propUsingReference: 10,
+        singleErrorMessage: 'APPROVED',
+      });
+
+      const errors = testEntity.getFormattedValidationErrors();
+
+      expect(errors).toEqual(null);
+    });
+
+    describe('arrayErrorMessage', () => {
+      it('should return correct error message when "arrayErrorMessage" does not meet min length', () => {
         const testEntity = new TestEntity({
-          arrayErrorMessage: 'APPROVED',
-          propUsingReference: 10,
+          arrayErrorMessage: 'a',
           singleErrorMessage: 'APPROVED',
         });
 
         const errors = testEntity.getFormattedValidationErrors()!;
 
-        expect(errors).toEqual(null);
+        expect(Object.keys(errors).length).toEqual(1);
+        expect(errors.arrayErrorMessage).toEqual(
+          'arrayErrorMessage must be at least 2 characters long.',
+        );
       });
 
-      describe('arrayErrorMessage', () => {
-        it('should return correct error message when "arrayErrorMessage" does not meet min length', () => {
-          const testEntity = new TestEntity({
-            arrayErrorMessage: 'a',
-            singleErrorMessage: 'APPROVED',
-          });
-
-          const errors = testEntity.getFormattedValidationErrors()!;
-
-          expect(Object.keys(errors).length).toEqual(1);
-          expect(errors.arrayErrorMessage).toEqual(
-            'arrayErrorMessage must be at least 2 characters long.',
-          );
-        });
-
-        it('should return correct error message when "arrayErrorMessage" is not defined', () => {
-          const testEntity = new TestEntity({
-            singleErrorMessage: 'APPROVED',
-          });
-
-          const errors = testEntity.getFormattedValidationErrors()!;
-
-          expect(Object.keys(errors).length).toEqual(1);
-          expect(errors.arrayErrorMessage).toEqual(
-            'arrayErrorMessage is required.',
-          );
-        });
-      });
-
-      describe('singleErrorMessage', () => {
-        it('should return default message when "singleErrorMessage" does not meet min length', () => {
-          const testEntity = new TestEntity({
-            arrayErrorMessage: 'APPROVED',
-            singleErrorMessage: 'a',
-          });
-
-          const errors = testEntity.getFormattedValidationErrors()!;
-
-          expect(Object.keys(errors).length).toEqual(1);
-          expect(errors.singleErrorMessage).toEqual(
-            'singleErrorMessage default message.',
-          );
-        });
-
-        it('should return default message when "singleErrorMessage" is not defined', () => {
-          const testEntity = new TestEntity({
-            arrayErrorMessage: 'APPROVED',
-          });
-
-          const errors = testEntity.getFormattedValidationErrors()!;
-
-          expect(Object.keys(errors).length).toEqual(1);
-          expect(errors.singleErrorMessage).toEqual(
-            'singleErrorMessage default message.',
-          );
-        });
-      });
-
-      describe('references', () => {
-        it('should display correct error message when using joi.ref correctly', () => {
-          const testEntity = new TestEntity({
-            arrayErrorMessage: 'APPROVED',
-            propUsingReference: 4,
-            singleErrorMessage: 'APPROVED',
-          });
-
-          const errors = testEntity.getFormattedValidationErrors()!;
-
-          expect(Object.keys(errors).length).toEqual(1);
-          expect(errors.propUsingReference).toEqual(
-            'propUsingReference must be grater than referencedProp.',
-          );
-        });
-      });
-
-      describe('remove unhelpful error messages from contact validations', () => {
-        it('should remove unhelpful error messages that end with "does not match any of the allowed types"', () => {
-          const testCaseEntity = new TestCaseEntity({
-            unhelpfulErrorMessage: 'INVALID',
-          });
-
-          const errors = testCaseEntity.getFormattedValidationErrors()!;
-          expect(errors).toEqual(null);
-        });
-      });
-
-      describe('nested entities', () => {
-        it('should return an array of errors when there is an error in a nested entity list', () => {
-          const testCaseEntity = new TestCaseEntity({
-            caseList: [
-              { contactType: 'VALID_1' },
-              { contactType: 'INVALID' },
-              { contactType: 'VALID_1' },
-            ],
-          });
-
-          const errors = testCaseEntity.getFormattedValidationErrors();
-
-          expect(errors).toEqual({
-            caseList: [
-              {
-                contactType: 'invalid contact type',
-                index: 1,
-              },
-            ],
-            contactType: 'invalid contact type',
-          });
-        });
-
-        it('should return an object of errors when there is an error in a nested entity', () => {
-          const testCaseEntity = new TestCaseEntity({
-            nestedCase: { contactType: 'INVALID_1' },
-          });
-
-          const errors = testCaseEntity.getFormattedValidationErrors();
-
-          expect(errors).toEqual({
-            nestedCase: {
-              contactType: 'invalid contact type',
-            },
-          });
-        });
-      });
-    });
-
-    describe('NEW IMPLEMENTATION', () => {
-      it('should return null if there are no errors in validation', () => {
+      it('should return correct error message when "arrayErrorMessage" is not defined', () => {
         const testEntity = new TestEntity({
-          arrayErrorMessage: 'APPROVED',
-          propUsingReference: 10,
           singleErrorMessage: 'APPROVED',
         });
 
-        const errors = testEntity.getFormattedValidationErrors_NEW();
+        const errors = testEntity.getFormattedValidationErrors()!;
 
+        expect(Object.keys(errors).length).toEqual(1);
+        expect(errors.arrayErrorMessage).toEqual(
+          'arrayErrorMessage is required.',
+        );
+      });
+    });
+
+    describe('singleErrorMessage', () => {
+      it('should return default message when "singleErrorMessage" does not meet min length', () => {
+        const testEntity = new TestEntity({
+          arrayErrorMessage: 'APPROVED',
+          singleErrorMessage: 'a',
+        });
+
+        const errors = testEntity.getFormattedValidationErrors()!;
+
+        expect(Object.keys(errors).length).toEqual(1);
+        expect(errors.singleErrorMessage).toEqual(
+          'singleErrorMessage default message.',
+        );
+      });
+
+      it('should return default message when "singleErrorMessage" is not defined', () => {
+        const testEntity = new TestEntity({
+          arrayErrorMessage: 'APPROVED',
+        });
+
+        const errors = testEntity.getFormattedValidationErrors()!;
+
+        expect(Object.keys(errors).length).toEqual(1);
+        expect(errors.singleErrorMessage).toEqual(
+          'singleErrorMessage default message.',
+        );
+      });
+    });
+
+    describe('references', () => {
+      it('should display correct error message when using joi.ref correctly', () => {
+        const testEntity = new TestEntity({
+          arrayErrorMessage: 'APPROVED',
+          propUsingReference: 4,
+          singleErrorMessage: 'APPROVED',
+        });
+
+        const errors = testEntity.getFormattedValidationErrors()!;
+
+        expect(Object.keys(errors).length).toEqual(1);
+        expect(errors.propUsingReference).toEqual(
+          'propUsingReference must be grater than referencedProp.',
+        );
+      });
+    });
+
+    describe('remove unhelpful error messages from contact validations', () => {
+      it('should remove unhelpful error messages that end with "does not match any of the allowed types"', () => {
+        const testCaseEntity = new TestCaseEntity({
+          unhelpfulErrorMessage: 'INVALID',
+        });
+
+        const errors = testCaseEntity.getFormattedValidationErrors()!;
         expect(errors).toEqual(null);
       });
+    });
 
-      describe('arrayErrorMessage', () => {
-        it('should return correct error message when "arrayErrorMessage" does not meet min length', () => {
-          const testEntity = new TestEntity({
-            arrayErrorMessage: 'a',
-            singleErrorMessage: 'APPROVED',
-          });
-
-          const errors = testEntity.getFormattedValidationErrors_NEW()!;
-
-          expect(Object.keys(errors).length).toEqual(1);
-          expect(errors.arrayErrorMessage).toEqual(
-            'arrayErrorMessage must be at least 2 characters long.',
-          );
+    describe('nested entities', () => {
+      it('should return an array of errors when there is an error in a nested entity list', () => {
+        const testCaseEntity = new TestCaseEntity({
+          caseList: [
+            { contactType: 'VALID_1' },
+            { contactType: 'INVALID' },
+            { contactType: 'VALID_1' },
+          ],
         });
 
-        it('should return correct error message when "arrayErrorMessage" is not defined', () => {
-          const testEntity = new TestEntity({
-            singleErrorMessage: 'APPROVED',
-          });
+        const errors = testCaseEntity.getFormattedValidationErrors();
 
-          const errors = testEntity.getFormattedValidationErrors_NEW()!;
-
-          expect(Object.keys(errors).length).toEqual(1);
-          expect(errors.arrayErrorMessage).toEqual(
-            'arrayErrorMessage is required.',
-          );
-        });
-      });
-
-      describe('singleErrorMessage', () => {
-        it('should return default message when "singleErrorMessage" does not meet min length', () => {
-          const testEntity = new TestEntity({
-            arrayErrorMessage: 'APPROVED',
-            singleErrorMessage: 'a',
-          });
-
-          const errors = testEntity.getFormattedValidationErrors_NEW()!;
-
-          expect(Object.keys(errors).length).toEqual(1);
-          expect(errors.singleErrorMessage).toEqual(
-            'singleErrorMessage default message.',
-          );
-        });
-
-        it('should return default message when "singleErrorMessage" is not defined', () => {
-          const testEntity = new TestEntity({
-            arrayErrorMessage: 'APPROVED',
-          });
-
-          const errors = testEntity.getFormattedValidationErrors_NEW()!;
-
-          expect(Object.keys(errors).length).toEqual(1);
-          expect(errors.singleErrorMessage).toEqual(
-            'singleErrorMessage default message.',
-          );
-        });
-      });
-
-      describe('references', () => {
-        it('should display correct error message when using joi.ref correctly', () => {
-          const testEntity = new TestEntity({
-            arrayErrorMessage: 'APPROVED',
-            propUsingReference: 4,
-            singleErrorMessage: 'APPROVED',
-          });
-
-          const errors = testEntity.getFormattedValidationErrors_NEW()!;
-
-          expect(Object.keys(errors).length).toEqual(1);
-          expect(errors.propUsingReference).toEqual(
-            'propUsingReference must be grater than referencedProp.',
-          );
-        });
-      });
-
-      describe('remove unhelpful error messages from contact validations', () => {
-        it('should remove unhelpful error messages that end with "does not match any of the allowed types"', () => {
-          const testCaseEntity = new TestCaseEntity({
-            unhelpfulErrorMessage: 'INVALID',
-          });
-
-          const errors = testCaseEntity.getFormattedValidationErrors_NEW()!;
-          expect(errors).toEqual(null);
-        });
-      });
-
-      describe('nested entities', () => {
-        it('should return an array of errors when there is an error in a nested entity list', () => {
-          const testCaseEntity = new TestCaseEntity({
-            caseList: [
-              { contactType: 'VALID_1' },
-              { contactType: 'INVALID' },
-              { contactType: 'VALID_1' },
-            ],
-          });
-
-          const errors = testCaseEntity.getFormattedValidationErrors_NEW();
-
-          expect(errors).toEqual({
-            caseList: [
-              {
-                contactType: 'invalid contact type',
-                index: 1,
-              },
-            ],
-            contactType: 'invalid contact type',
-          });
-        });
-
-        it('should return an object of errors when there is an error in a nested entity', () => {
-          const testCaseEntity = new TestCaseEntity({
-            nestedCase: { contactType: 'INVALID_1' },
-          });
-
-          const errors = testCaseEntity.getFormattedValidationErrors_NEW();
-
-          expect(errors).toEqual({
-            nestedCase: {
+        expect(errors).toEqual({
+          caseList: [
+            {
               contactType: 'invalid contact type',
+              index: 1,
             },
-          });
+          ],
+          contactType: 'invalid contact type',
+        });
+      });
+
+      it('should return an object of errors when there is an error in a nested entity', () => {
+        const testCaseEntity = new TestCaseEntity({
+          nestedCase: { contactType: 'INVALID_1' },
+        });
+
+        const errors = testCaseEntity.getFormattedValidationErrors();
+
+        expect(errors).toEqual({
+          nestedCase: {
+            contactType: 'invalid contact type',
+          },
         });
       });
     });
