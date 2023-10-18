f26f91fa902ab25d186cf47cfbd19ce8e56196ab
Cody Seibert <cseibert113@gmail.com>
2023-10-18 10:54:38 -0400

    10011 - display icons

--- web-client/src/views/Public/CreatePetitionerAccount/RequirementsText.tsx
+++ web-client/src/views/Public/CreatePetitionerAccount/RequirementsText.tsx
@@ -1,10 +1,16 @@
+import { Icon } from '@web-client/ustc-ui/Icon/Icon';
 import React from 'react';
 
 export const RequirementsText = ({ label, valid }) => {
   const className = valid ? 'valid-requirement' : 'invalid-requirement';
   return (
-    <>
-      <div className={className}>{label}</div>
-    </>
+    <div className={className}>
+      <Icon
+        className=""
+        icon={valid ? 'check-circle' : 'times-circle'}
+        size="1x"
+      />{' '}
+      {label}
+    </div>
   );
 };
