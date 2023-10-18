84dc882d4cec78fd51f57f7f62252e7a90fb1f3d
John Cruz <jcruz@flexion.us>
2023-10-18 11:45:18 -0600

    10011: Improve typings;

--- web-client/src/presenter/computeds/Public/createAccountHelper.ts
+++ web-client/src/presenter/computeds/Public/createAccountHelper.ts
@@ -1,16 +1,26 @@
-import { CreateAccountForm } from '@shared/business/entities/CreateAccountForm';
+import {
+  CreateAccountForm,
+  CreateAccountFormPasswordValidations,
+} from '@shared/business/entities/CreateAccountForm';
 import { Get } from 'cerebral';
 import { state } from '@web-client/presenter/app-public.cerebral';
 
-export const createAccountHelper = (get: Get) => {
+export type CreateAccuntHelperResults = {
+  confirmPassword: boolean;
+  enableContinueButton: boolean;
+  passwordErrors: CreateAccountFormPasswordValidations | undefined;
+};
+
+export const createAccountHelper = (get: Get): CreateAccuntHelperResults => {
   const form = get(state.form);
-
   const formEntity = new CreateAccountForm(form);
-
   const errors = formEntity.getFormattedValidationErrors();
+  const passwordErrors: CreateAccountFormPasswordValidations | undefined =
+    errors?.password as CreateAccountFormPasswordValidations | undefined;
 
   return {
     confirmPassword: !errors?.confirmPassword,
-    passwordErrors: errors?.password,
+    enableContinueButton: true,
+    passwordErrors,
   };
 };
