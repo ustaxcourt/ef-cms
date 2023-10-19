58fbdabf4dbe6a1561d58510182dcfe29b2500d0
John Cruz <jcruz@flexion.us>
2023-10-19 10:39:50 -0600

    10011: Import icons; dynamically display icon on message alert;

--- web-client/src/views/Public/MessageAlert/MessageAlert.tsx
+++ web-client/src/views/Public/MessageAlert/MessageAlert.tsx
@@ -1,3 +1,4 @@
+import { Icon } from '@web-client/ustc-ui/Icon/Icon';
 import React from 'react';
 
 export const MessageAlert = ({ alertType = 'error', message, title }) => {
@@ -5,12 +6,25 @@ export const MessageAlert = ({ alertType = 'error', message, title }) => {
     error: 'error-message-alert',
     warning: 'warning-message-alert',
   };
+
+  const iconDictionary = {
+    error: ['fas', 'exclamation-circle'],
+    warning: ['fas', 'exclamation-triangle'],
+  };
   const className = alertTypeClassName[alertType] || alertTypeClassName.error;
+  const icon = iconDictionary[alertType] || iconDictionary.error;
 
   return (
     <div className={`padding-3 ${className}`}>
-      <h2>{title}</h2>
-      <p>{message}</p>
+      <div className="grid-row">
+        <div className="grid-col-1">
+          <Icon aria-label="warning icon" className="" icon={icon} size="2x" />
+        </div>
+        <div className="grid-col-11">
+          <h2>{title}</h2>
+          <p>{message}</p>
+        </div>
+      </div>
     </div>
   );
 };
