00f974d99ae0a9e84c03cb64af4bfdf5e5d5e327
Jason Fu <jasonfu6699@hotmail.com>
2023-10-19 14:21:08 -0700

    dx1209: increase test coverage

--- shared/src/business/entities/notes/CalendarNote.test.ts
+++ shared/src/business/entities/notes/CalendarNote.test.ts
@@ -1,11 +1,25 @@
 import { CalendarNote } from './CalendarNote';
+import { getTextByCount } from '@shared/business/utilities/getTextByCount';
 
 describe('CalendarNote', () => {
   describe('validation', () => {
+    it('should have validation rules', () => {
+      const entity = new CalendarNote({ note: '' });
+      expect(entity.getErrorToMessageMap()).toEqual({});
+      expect(entity.getValidationRules()).not.toEqual(null);
+    });
+
+    it('should have not have any error messages for note field longer than 200 characters', () => {
+      const entity = new CalendarNote({ note: getTextByCount(1001) });
+      expect(entity.getErrorToMessageMap()).toEqual({});
+      expect(entity.getFormattedValidationErrors()).toEqual(null);
+    });
+
     it('should be valid when all fields are present', () => {
       const entity = new CalendarNote({
         note: '  some notes   ', // with spaces all around it
       });
+      expect(entity.getErrorToMessageMap()).toEqual({});
       expect(entity.getFormattedValidationErrors()).toEqual(null);
       expect(entity.note).toEqual('some notes');
     });
@@ -14,6 +28,7 @@ describe('CalendarNote', () => {
       const entity = new CalendarNote({
         note: '',
       });
+      expect(entity.getErrorToMessageMap()).toEqual({});
       expect(entity.getFormattedValidationErrors()).toEqual(null);
       expect(entity.note).toEqual('');
     });
