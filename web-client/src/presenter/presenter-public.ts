4d2806db730a07f227034b0cae4713c3c250a26c
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2023-10-20 13:00:45 -0500

    10011: revert removal of navigate sequence

--- web-client/src/presenter/presenter-public.ts
+++ web-client/src/presenter/presenter-public.ts
@@ -25,6 +25,7 @@ import { initialPublicState } from './state-public';
 import { loadMoreTodaysOrdersSequence } from './sequences/loadMoreTodaysOrdersSequence';
 import { navigateBackSequence } from './sequences/navigateBackSequence';
 import { navigateToCognitoSequence } from './sequences/navigateToCognitoSequence';
+import { navigateToCreatePetitionerAccountSequence } from '@web-client/presenter/sequences/navigateToCreatePetitionerAccountSequence';
 import { navigateToPublicSiteSequence } from './sequences/Public/navigateToPublicSiteSequence';
 import { notFoundErrorSequence } from './sequences/notFoundErrorSequence';
 import { openAppMaintenanceModalSequence } from './sequences/openAppMaintenanceModalSequence';
@@ -92,6 +93,7 @@ export const presenterSequences = {
   loadMoreTodaysOrdersSequence,
   navigateBackSequence,
   navigateToCognitoSequence,
+  navigateToCreatePetitionerAccountSequence,
   navigateToPublicSiteSequence,
   notFoundErrorSequence,
   openAppMaintenanceModalSequence,
