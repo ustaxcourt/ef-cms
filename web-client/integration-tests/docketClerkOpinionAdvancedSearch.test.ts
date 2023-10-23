e6885c9d2ab93735da16f7fd56250e51df309266
Nechama Krigsman <nkrigsman@flexion.us>
2023-10-23 16:12:22 -0400

    devex-1187: update tests that were using VALIDATION_ERROR_MESSAGES

--- web-client/integration-tests/docketClerkOpinionAdvancedSearch.test.ts
+++ web-client/integration-tests/docketClerkOpinionAdvancedSearch.test.ts
@@ -3,7 +3,6 @@ import {
   ADVANCED_SEARCH_TABS,
   DATE_RANGE_SEARCH_OPTIONS,
 } from '../../shared/src/business/entities/EntityConstants';
-import { DocumentSearch } from '../../shared/src/business/entities/documents/DocumentSearch';
 import {
   loginAs,
   refreshElasticsearchIndex,
@@ -20,7 +19,7 @@ describe('Docket clerk opinion advanced search', () => {
 
   loginAs(cerebralTest, 'docketclerk@example.com');
 
-  it('go to advanced opinion search tab', async () => {
+  it('should allow users to search for opinions without any search criteria', async () => {
     await refreshElasticsearchIndex();
 
     await cerebralTest.runSequence('gotoAdvancedSearchSequence');
@@ -34,9 +33,7 @@ describe('Docket clerk opinion advanced search', () => {
 
     await cerebralTest.runSequence('submitOpinionAdvancedSearchSequence');
 
-    expect(cerebralTest.getState('validationErrors')).toEqual({
-      keyword: DocumentSearch.VALIDATION_ERROR_MESSAGES.keyword,
-    });
+    expect(cerebralTest.getState('validationErrors')).toEqual({});
   });
 
   it('should clear search fields when "Clear Search" is clicked', async () => {
