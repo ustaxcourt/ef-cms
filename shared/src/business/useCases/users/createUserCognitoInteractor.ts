19dfa0c71ab2deb22eb2c7d4cb83bb6ca2d58007
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2023-10-23 17:09:55 -0500

    10011: remove custom role from new petitioner user sign up

--- shared/src/business/useCases/users/createUserCognitoInteractor.ts
+++ shared/src/business/useCases/users/createUserCognitoInteractor.ts
@@ -1,12 +1,3 @@
-import { ROLES } from '../../entities/EntityConstants';
-
-/**
- * createUserCognitoInteractor
- * @param {object} applicationContext the application context
- * @param {object} providers the providers object
- * @param {object} providers.user the user data
- * @returns {Promise} the promise of the createUser call
- */
 export const createUserCognitoInteractor = async (
   applicationContext: IApplicationContext,
   { user }: { user: { password: string; name: string; email: string } },
@@ -22,10 +13,6 @@ export const createUserCognitoInteractor = async (
       {
         Name: 'email_verified',
         Value: 'True',
-      },
-      {
-        Name: 'custom:role',
-        Value: ROLES.petitioner,
       },
       {
         Name: 'name',
