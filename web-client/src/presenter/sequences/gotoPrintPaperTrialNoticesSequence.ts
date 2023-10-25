2d579ace28105d2eeeb70910aea4959a29104a52
Zachary Rogers <zrogers>
2023-10-25 12:30:29 -0700

    10060 - WIP add temp code

--- web-client/src/presenter/sequences/gotoPrintPaperTrialNoticesSequence.ts
+++ web-client/src/presenter/sequences/gotoPrintPaperTrialNoticesSequence.ts
@@ -1,6 +1,16 @@
+import { sequence } from 'cerebral';
 import { setupCurrentPageAction } from '../actions/setupCurrentPageAction';
 
-export const gotoPrintPaperTrialNoticesSequence = [
+export const gotoPrintPaperTrialNoticesSequence = sequence<{
+  fileId: string;
+  trialSessionId: string;
+}>([
   // TODO: Fetch trial session data
+  // setupPrintPaperTrialNoticesData
+  async (props: ActionProps) => {
+    const pdfPreviewUrl = await applicationContext
+      .getUseCases()
+      .getPdfDownloadUrlInteractor();
+  },
   setupCurrentPageAction('PrintPaperTrialNotices'),
-];
+]);
