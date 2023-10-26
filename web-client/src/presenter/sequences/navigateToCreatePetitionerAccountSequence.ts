3e960dfee8184dee424ee20066443111f0d2fca3
John Cruz <jcruz@flexion.us>
2023-10-26 11:34:56 -0600

    10011: move action to other sequence;

--- web-client/src/presenter/sequences/navigateToCreatePetitionerAccountSequence.ts
+++ web-client/src/presenter/sequences/navigateToCreatePetitionerAccountSequence.ts
@@ -1,7 +1,5 @@
-import { clearFormAction } from '@web-client/presenter/actions/clearFormAction';
 import { navigateToCreatePetitionerAccountAction } from '@web-client/presenter/actions/navigateToCreatePetitionerAccountAction';
 
 export const navigateToCreatePetitionerAccountSequence = [
-  clearFormAction,
   navigateToCreatePetitionerAccountAction,
 ];
