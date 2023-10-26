f903b3f891cb6e4b7c36f515361bd952b6f96dcd
John Cruz <jcruz@flexion.us>
2023-10-26 11:13:28 -0600

    10011: add tests for CreateAccuntHelperResults;

---
+++ web-client/src/presenter/computeds/Public/createAccountHelper.test.ts
@@ -0,0 +1,127 @@
+import { createAccountHelper } from '@web-client/presenter/computeds/Public/createAccountHelper';
+import { runCompute } from '@web-client/presenter/test.cerebral';
+
+describe('createAccountHelper', () => {
+  describe('passwordErrors', () => {
+    it('should return object with all validations set to true when "password" passes validations', () => {
+      const result = runCompute(createAccountHelper, {
+        state: {
+          form: {
+            confirmPassword: 'aA1!aaaa',
+          },
+        },
+      });
+
+      expect(result).toMatchObject({
+        passwordErrors: {
+          hasNoLeadingOrTrailingSpace: true,
+          hasOneLowercase: true,
+          hasOneNumber: true,
+          hasOneUppercase: true,
+          hasSpecialCharacterOrSpace: true,
+          isProperLength: true,
+        },
+      });
+    });
+  });
+
+  describe('confirmPassword', () => {
+    it('should return "true" if "confirmPassword" passes validation', () => {
+      const result = runCompute(createAccountHelper, {
+        state: {
+          form: {
+            confirmPassword: 'aA1!aaaa',
+            password: 'aA1!aaaa',
+          },
+        },
+      });
+
+      expect(result).toMatchObject({
+        confirmPassword: true,
+      });
+    });
+
+    it('should return "false" if "confirmPassword" fails validation', () => {
+      const result = runCompute(createAccountHelper, {
+        state: {
+          form: {
+            confirmPassword: 'aA1!aaaa',
+            password: 'aA1!aaaa_MORE',
+          },
+        },
+      });
+
+      expect(result).toMatchObject({
+        confirmPassword: false,
+      });
+    });
+  });
+
+  describe('email', () => {
+    it('should return the error message when "email" fails validation', () => {
+      const result = runCompute(createAccountHelper, {
+        state: {
+          form: {
+            email: 'NOT A REAL EMAIL',
+          },
+        },
+      });
+
+      expect(result).toMatchObject({
+        email: 'Enter a valid email address',
+      });
+    });
+  });
+
+  describe('name', () => {
+    it('should return the error message when "name" fails validation', () => {
+      const result = runCompute(createAccountHelper, {
+        state: {
+          form: {
+            name: 'a'.repeat(101),
+          },
+        },
+      });
+
+      expect(result).toMatchObject({
+        name: 'Enter a name with less than 100 characters',
+      });
+    });
+  });
+
+  describe('formIsValid', () => {
+    it('should return "true" when the form is all valid', () => {
+      const result = runCompute(createAccountHelper, {
+        state: {
+          form: {
+            confirmPassword: 'aA1!aaaa',
+            email: 'example@example.com',
+            name: 'John Cena',
+            password: 'aA1!aaaa',
+          },
+        },
+      });
+
+      expect(result).toMatchObject({
+        formIsValid: true,
+      });
+    });
+
+    it('should return "false" when the something failed the form validation', () => {
+      const result = runCompute(createAccountHelper, {
+        state: {
+          form: {
+            confirmPassword: 'aA1!aaaa_NOT_MATCHING',
+            email: 'example@example.com',
+            name: 'John Cena',
+            password: 'aA1!aaaa',
+          },
+        },
+      });
+
+      expect(result).toMatchObject({
+        formIsValid: false,
+      });
+    });
+  });
+});
