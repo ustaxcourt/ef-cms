239a0fb7727bf724a95a6bfa3f3492987a24c931
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2023-10-27 10:52:58 -0500

    10011: minor accessibility updates, add account creation page pa11y test

--- web-client/src/presenter/actions/navigateToVerificationSentAction.ts
+++ web-client/src/presenter/actions/navigateToVerificationSentAction.ts
@@ -1,5 +1,6 @@
 export const navigateToVerificationSentAction = async ({
   router,
 }: ActionProps) => {
+  //  TODO: ask UX to confirm, but probably navigate to login if email not defined in state
   await router.route('/create-account/verification-sent');
 };
