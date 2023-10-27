0251416673c0418356bf0ffef2644b4950054253
John Cruz <jcruz@flexion.us>
2023-10-27 12:41:15 -0600

    10011: Redirect to login page instead of home;

--- web-client/src/presenter/sequences/goToVerificationSentSequence.ts
+++ web-client/src/presenter/sequences/goToVerificationSentSequence.ts
@@ -1,11 +1,11 @@
 import { checkIfCognitoEmailInState } from '@web-client/presenter/actions/checkIfCognitoEmailInState';
-import { gotoPublicSearchSequence } from '@web-client/presenter/sequences/Public/gotoPublicSearchSequence';
+import { navigateToCognitoAction } from '@web-client/presenter/actions/navigateToCognitoAction';
 import { setupCurrentPageAction } from '@web-client/presenter/actions/setupCurrentPageAction';
 
 export const goToVerificationSentSequence = [
   checkIfCognitoEmailInState,
   {
-    doesNotExist: [gotoPublicSearchSequence],
+    doesNotExist: [navigateToCognitoAction],
     exists: [setupCurrentPageAction('VerificationSent')],
   },
 ];
