7074ff4c143e9de8f3f962400472331aaa3d603c
John Cruz <jcruz@flexion.us>
2023-10-27 12:48:21 -0600

    10011: Completed todo comment;

--- web-client/src/presenter/actions/navigateToVerificationSentAction.ts
+++ web-client/src/presenter/actions/navigateToVerificationSentAction.ts
@@ -1,6 +1,5 @@
 export const navigateToVerificationSentAction = async ({
   router,
 }: ActionProps) => {
-  //  TODO: ask UX to confirm, but probably navigate to login if email not defined in state
   await router.route('/create-account/verification-sent');
 };
