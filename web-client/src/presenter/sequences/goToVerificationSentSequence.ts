d1875ae12830c3afd7dcc4dd0c6daa4dfd29bb30
John Cruz <jcruz@flexion.us>
2023-10-27 11:24:54 -0600

    10011: Check state if email exists to navigate to Verification Sent or navigate to Puiblic Search if it does not;

--- web-client/src/presenter/sequences/goToVerificationSentSequence.ts
+++ web-client/src/presenter/sequences/goToVerificationSentSequence.ts
@@ -1,5 +1,11 @@
+import { checkIfCognitoEmailInState } from '@web-client/presenter/actions/checkIfCognitoEmailInState';
+import { gotoPublicSearchSequence } from '@web-client/presenter/sequences/Public/gotoPublicSearchSequence';
 import { setupCurrentPageAction } from '@web-client/presenter/actions/setupCurrentPageAction';
 
 export const goToVerificationSentSequence = [
-  setupCurrentPageAction('VerificationSent'),
+  checkIfCognitoEmailInState,
+  {
+    doesNotExist: [gotoPublicSearchSequence],
+    exists: [setupCurrentPageAction('VerificationSent')],
+  },
 ];
