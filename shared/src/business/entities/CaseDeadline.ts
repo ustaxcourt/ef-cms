f3cac74434229c09a4fd7ffe7cf1bd07e61dc9ad
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-10-27 14:20:23 -0700

    Devex 1187: Follow pattern for how validation rules are defined on entities

--- shared/src/business/entities/CaseDeadline.ts
+++ shared/src/business/entities/CaseDeadline.ts
@@ -34,46 +34,45 @@ export class CaseDeadline extends JoiValidationEntity_New {
       Case.getSortableDocketNumber(this.docketNumber);
   }
 
+  static VALIDATION_RULES = {
+    associatedJudge: JoiValidationConstants.STRING.max(50)
+      .required()
+      .description('Judge assigned to the case containing this Case Deadline.')
+      .messages({ '*': 'Associated judge is required' }),
+    caseDeadlineId: JoiValidationConstants.UUID.required().description(
+      'Unique Case Deadline ID only used by the system.',
+    ),
+    createdAt: JoiValidationConstants.ISO_DATE.required().description(
+      'When the Case Deadline was added to the system.',
+    ),
+    deadlineDate: JoiValidationConstants.ISO_DATE.required()
+      .description('When the Case Deadline expires.')
+      .messages({ '*': 'Enter a valid deadline date' }),
+    description: JoiValidationConstants.STRING.max(120)
+      .min(1)
+      .required()
+      .description('User provided description of the Case Deadline.')
+      .messages({
+        '*': 'Enter a description of this deadline',
+        'string.max':
+          'The description is too long. Please enter a valid description.',
+      }),
+    docketNumber: JoiValidationConstants.DOCKET_NUMBER.required()
+      .description('Docket number of the case containing the Case Deadline.')
+      .messages({ '*': 'You must have a docket number.' }),
+    entityName: JoiValidationConstants.STRING.valid('CaseDeadline').required(),
+    leadDocketNumber: JoiValidationConstants.DOCKET_NUMBER.optional(),
+    sortableDocketNumber: joi
+      .number()
+      .required()
+      .description(
+        'A sortable representation of the docket number of the case containing the Case Deadline.',
+      )
+      .messages({ '*': 'Sortable docket number is required' }),
+  };
+
   getValidationRules() {
-    return {
-      associatedJudge: JoiValidationConstants.STRING.max(50)
-        .required()
-        .description(
-          'Judge assigned to the case containing this Case Deadline.',
-        )
-        .messages({ '*': 'Associated judge is required' }),
-      caseDeadlineId: JoiValidationConstants.UUID.required().description(
-        'Unique Case Deadline ID only used by the system.',
-      ),
-      createdAt: JoiValidationConstants.ISO_DATE.required().description(
-        'When the Case Deadline was added to the system.',
-      ),
-      deadlineDate: JoiValidationConstants.ISO_DATE.required()
-        .description('When the Case Deadline expires.')
-        .messages({ '*': 'Enter a valid deadline date' }),
-      description: JoiValidationConstants.STRING.max(120)
-        .min(1)
-        .required()
-        .description('User provided description of the Case Deadline.')
-        .messages({
-          '*': 'Enter a description of this deadline',
-          'string.max':
-            'The description is too long. Please enter a valid description.',
-        }),
-      docketNumber: JoiValidationConstants.DOCKET_NUMBER.required()
-        .description('Docket number of the case containing the Case Deadline.')
-        .messages({ '*': 'You must have a docket number.' }),
-      entityName:
-        JoiValidationConstants.STRING.valid('CaseDeadline').required(),
-      leadDocketNumber: JoiValidationConstants.DOCKET_NUMBER.optional(),
-      sortableDocketNumber: joi
-        .number()
-        .required()
-        .description(
-          'A sortable representation of the docket number of the case containing the Case Deadline.',
-        )
-        .messages({ '*': 'Sortable docket number is required' }),
-    };
+    return CaseDeadline.VALIDATION_RULES;
   }
 }
 
