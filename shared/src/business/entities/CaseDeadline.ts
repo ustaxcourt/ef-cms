124f1866cd2997380d1024da95b3b16665bd9d14
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-10-27 12:44:44 -0700

    Devex 1187: Remove method that defaults error message for now until a pattern emerges, using wildcard selector for now

--- shared/src/business/entities/CaseDeadline.ts
+++ shared/src/business/entities/CaseDeadline.ts
@@ -1,9 +1,6 @@
 import { Case } from './cases/Case';
 import { JoiValidationConstants } from './JoiValidationConstants';
-import {
-  JoiValidationEntity_New,
-  setDefaultErrorMessage,
-} from '@shared/business/entities/JoiValidationEntity_New';
+import { JoiValidationEntity_New } from '@shared/business/entities/JoiValidationEntity_New';
 import { createISODateString } from '../utilities/DateHandler';
 import joi from 'joi';
 
@@ -38,13 +35,13 @@ export class CaseDeadline extends JoiValidationEntity_New {
   }
 
   getValidationRules() {
-    return joi.object({
+    return {
       associatedJudge: JoiValidationConstants.STRING.max(50)
         .required()
         .description(
           'Judge assigned to the case containing this Case Deadline.',
         )
-        .messages(setDefaultErrorMessage('Associated judge is required')),
+        .messages({ '*': 'Associated judge is required' }),
       caseDeadlineId: JoiValidationConstants.UUID.required().description(
         'Unique Case Deadline ID only used by the system.',
       ),
@@ -53,19 +50,19 @@ export class CaseDeadline extends JoiValidationEntity_New {
       ),
       deadlineDate: JoiValidationConstants.ISO_DATE.required()
         .description('When the Case Deadline expires.')
-        .messages(setDefaultErrorMessage('Enter a valid deadline date')),
+        .messages({ '*': 'Enter a valid deadline date' }),
       description: JoiValidationConstants.STRING.max(120)
         .min(1)
         .required()
         .description('User provided description of the Case Deadline.')
         .messages({
-          ...setDefaultErrorMessage('Enter a description of this deadline'),
+          '*': 'Enter a description of this deadline',
           'string.max':
             'The description is too long. Please enter a valid description.',
         }),
       docketNumber: JoiValidationConstants.DOCKET_NUMBER.required()
         .description('Docket number of the case containing the Case Deadline.')
-        .messages(setDefaultErrorMessage('You must have a docket number.')),
+        .messages({ '*': 'You must have a docket number.' }),
       entityName:
         JoiValidationConstants.STRING.valid('CaseDeadline').required(),
       leadDocketNumber: JoiValidationConstants.DOCKET_NUMBER.optional(),
@@ -75,8 +72,8 @@ export class CaseDeadline extends JoiValidationEntity_New {
         .description(
           'A sortable representation of the docket number of the case containing the Case Deadline.',
         )
-        .messages(setDefaultErrorMessage('Sortable docket number is required')),
-    });
+        .messages({ '*': 'Sortable docket number is required' }),
+    };
   }
 }
 
