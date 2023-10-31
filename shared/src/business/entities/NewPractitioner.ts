56424516607d79fc852e6a8ee89d8240579f37bc
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-10-31 15:47:10 -0700

    Devex 1187: Fixing tests, mark optional properties for NewPractitioner that are not defined as optional for base class

--- shared/src/business/entities/NewPractitioner.ts
+++ shared/src/business/entities/NewPractitioner.ts
@@ -1,45 +1,48 @@
+import { ExcludeMethods } from 'types/TEntity';
 import { JoiValidationConstants } from './JoiValidationConstants';
 import { Practitioner } from './Practitioner';
 import joi from 'joi';
 
 export class NewPractitioner extends Practitioner {
+  public barNumber?: string;
+  public userId?: string;
+  public name?: string;
+
   constructor(rawUser, options?) {
     super(rawUser, options);
-    this.entityName = 'Practitioner';
+
+    this.entityName = Practitioner.ENTITY_NAME;
   }
 
-  static VALIDATION_ERROR_MESSAGES = {
-    ...Practitioner.VALIDATION_ERROR_MESSAGES,
-    confirmEmail: [
-      {
-        contains: 'must be [ref:email]',
-        message: 'Email addresses do not match',
-      },
-      { contains: 'is required', message: 'Enter a valid email address' },
-      { contains: 'must be a valid', message: 'Enter a valid email address' },
-    ],
-    email: 'Enter email address',
-    firstName: 'Enter first name',
-    lastName: 'Enter last name',
+  static VALIDATION_RULES = {
+    ...super.VALIDATION_RULES,
+    barNumber: JoiValidationConstants.STRING.optional().allow(null),
+    confirmEmail: JoiValidationConstants.EMAIL.when('email', {
+      is: joi.exist().not(null),
+      otherwise: joi.optional().allow(null),
+      then: joi.valid(joi.ref('email')).required(),
+    }).messages({
+      'any.only': 'Email addresses do not match',
+      'any.required': 'Enter a valid email address',
+      'string.email': 'Enter a valid email address',
+    }),
+    email: JoiValidationConstants.EMAIL.required().messages({
+      '*': 'Enter email address',
+    }),
+    firstName: super.VALIDATION_RULES.firstName.messages({
+      '*': 'Enter first name',
+    }),
+    lastName: super.VALIDATION_RULES.lastName.messages({
+      '*': 'Enter last name',
+    }),
+    role: JoiValidationConstants.STRING.optional().allow(null),
+    updatedEmail: JoiValidationConstants.STRING.optional().allow(null),
+    userId: JoiValidationConstants.STRING.optional().allow(null),
   };
 
   getValidationRules() {
-    return {
-      ...super.getValidationRules(),
-      barNumber: JoiValidationConstants.STRING.optional().allow(null),
-      confirmEmail: JoiValidationConstants.EMAIL.when('email', {
-        is: joi.exist().not(null),
-        otherwise: joi.optional().allow(null),
-        then: joi.valid(joi.ref('email')).required(),
-      }),
-      email: JoiValidationConstants.EMAIL.required(),
-      role: JoiValidationConstants.STRING.optional().allow(null),
-      updatedEmail: JoiValidationConstants.STRING.optional().allow(null),
-      userId: JoiValidationConstants.STRING.optional().allow(null),
-    };
-  }
-
-  getErrorToMessageMap() {
-    return NewPractitioner.VALIDATION_ERROR_MESSAGES;
+    return NewPractitioner.VALIDATION_RULES;
   }
 }
+
+export type RawNewPractitioner = ExcludeMethods<NewPractitioner>;
