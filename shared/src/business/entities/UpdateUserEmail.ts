f3aa057fe3ea8b8dc186e98463519ff39d5b189c
Nechama Krigsman <nkrigsman@flexion.us>
2023-10-31 14:51:26 -0400

    Devex 1187: Convert UpdateUserEmail entity to use joi validation error messages format

--- shared/src/business/entities/UpdateUserEmail.ts
+++ shared/src/business/entities/UpdateUserEmail.ts
@@ -1,5 +1,5 @@
 import { JoiValidationConstants } from './JoiValidationConstants';
-import { JoiValidationEntity } from './JoiValidationEntity';
+import { JoiValidationEntity_New } from './joiValidationEntity/JoiValidationEntity_New';
 import joi from 'joi';
 
 /**
@@ -10,7 +10,7 @@ import joi from 'joi';
  * @param {object} providers.rawUpdateUserEmail the raw UpdateUserEmail data
  * @constructor
  */
-export class UpdateUserEmail extends JoiValidationEntity {
+export class UpdateUserEmail extends JoiValidationEntity_New {
   public email: string;
   public confirmEmail: string;
 
@@ -20,28 +20,20 @@ export class UpdateUserEmail extends JoiValidationEntity {
     this.confirmEmail = rawUpdateUserEmail.confirmEmail;
   }
 
-  static VALIDATION_ERROR_MESSAGES = {
-    confirmEmail: [
-      {
-        contains: 'must be [ref:email]',
-        message: 'Email addresses do not match',
-      },
-      { contains: 'is required', message: 'Enter a valid email address' },
-      { contains: 'must be a valid', message: 'Enter a valid email address' },
-    ],
-    email: 'Enter a valid email address',
+  static VALIDATION_RULES = {
+    confirmEmail: JoiValidationConstants.EMAIL.valid(joi.ref('email'))
+      .required()
+      .messages({
+        'any.only': 'Email addresses do not match',
+        'any.required': 'Enter a valid email address',
+        'string.email': 'Enter a valid email address',
+      }),
+    email: JoiValidationConstants.EMAIL.required().messages({
+      '*': 'Enter a valid email address',
+    }),
   };
 
   getValidationRules() {
-    return {
-      confirmEmail: JoiValidationConstants.EMAIL.valid(
-        joi.ref('email'),
-      ).required(),
-      email: JoiValidationConstants.EMAIL.required(),
-    };
-  }
-
-  getErrorToMessageMap() {
-    return UpdateUserEmail.VALIDATION_ERROR_MESSAGES;
+    return UpdateUserEmail.VALIDATION_RULES;
   }
 }
