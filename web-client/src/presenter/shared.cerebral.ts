25807d461d263b72bd0e21d75c4ab4e4440e30bc
Zachary Rogers <zrogers>
2023-11-02 07:57:51 -0700

    devex: remove unused compute function

--- web-client/src/presenter/shared.cerebral.ts
+++ web-client/src/presenter/shared.cerebral.ts
@@ -1,16 +1,8 @@
 import { connect as cerebralConnect } from '@cerebral/react';
-import type { ClientApplicationContext } from '@web-client/applicationContext';
 import type { FunctionComponent } from 'react';
-import type { Get } from 'cerebral';
 
 type FakeConnectType = <PassedProps, Deps>(
   depsMap: Deps,
   component: FunctionComponent<Deps & PassedProps>,
 ) => FunctionComponent<PassedProps>;
 export const connect = cerebralConnect as unknown as FakeConnectType;
-
-export function computed<Helper>(
-  helper: (get: Get, applicationContext: ClientApplicationContext) => Helper,
-): Helper {
-  return helper as unknown as Helper;
-}
