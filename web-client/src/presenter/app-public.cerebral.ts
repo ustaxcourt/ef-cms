ffd71adea4da0b01679d001c3032932db31c9b0f
Zachary Rogers <zrogers>
2023-11-01 21:57:29 -0700

    devex: update public sequences to point to public state

--- web-client/src/presenter/app-public.cerebral.ts
+++ web-client/src/presenter/app-public.cerebral.ts
@@ -1,10 +1,10 @@
-import { PublicClientState } from './state-public';
 import {
   sequences as cerebralSequences,
   state as cerebralState,
 } from 'cerebral';
-import type { Sequences } from '@web-client/presenter/presenter-public';
+import type { PublicClientState } from './state-public';
+import type { PublicSequences } from '@web-client/presenter/presenter-public';
 
 export const state = cerebralState as unknown as PublicClientState;
 
-export const sequences = cerebralSequences as unknown as Sequences;
+export const sequences = cerebralSequences as unknown as PublicSequences;
