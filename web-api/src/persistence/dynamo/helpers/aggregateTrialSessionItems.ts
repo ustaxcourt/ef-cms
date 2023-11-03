7aad8452830ee0c13e4105b0ddc239cca6a835fd
Zachary Rogers <zrogers>
2023-11-03 08:26:59 -0700

    10060: Filter out pdf links greater than 3 days

--- web-api/src/persistence/dynamo/helpers/aggregateTrialSessionItems.ts
+++ web-api/src/persistence/dynamo/helpers/aggregateTrialSessionItems.ts
@@ -1,23 +1,28 @@
 import { RawTrialSession } from '@shared/business/entities/trialSessions/TrialSession';
 import {
+  TDynamoRecord,
   TrialSessionPaperPdfRecord,
   TrialSessionRecord,
 } from '@web-api/persistence/dynamo/dynamoTypes';
 
 export const aggregateTrialSessionItems = (
-  items,
+  items: TDynamoRecord[],
 ): RawTrialSession | undefined => {
   let paperServicePdfs: { title: string; fileId: string }[] = [];
 
   let trialSession: RawTrialSession | undefined;
-  items.forEach(item => {
-    if (isTrialSessionItem(item)) {
-      trialSession = item as unknown as RawTrialSession;
-    }
-    if (isPaperServicePdfItem(item)) {
-      paperServicePdfs.push(item);
-    }
-  });
+  items
+    .filter(record => {
+      return record.ttl ? record.ttl >= Math.floor(Date.now() / 1000) : true;
+    })
+    .forEach(item => {
+      if (isTrialSessionItem(item)) {
+        trialSession = item as unknown as RawTrialSession;
+      }
+      if (isPaperServicePdfItem(item)) {
+        paperServicePdfs.push(item);
+      }
+    });
 
   if (trialSession) {
     trialSession.paperServicePdfs = paperServicePdfs;
