a28fcc3c76936d952d16e4fde7b76cd43b52ef96
Cody Seibert <cseibert113@gmail.com>
2023-11-07 17:32:04 -0500

    refactoring an integration test in place of a cypress test due to how flaky it was

---
+++ cypress/cypress-integration/integration/respondent-modifies-contact-info.cy.ts
@@ -0,0 +1,42 @@
+import { petitionerCreatesACase } from '../support/setup/petitioner-creates-a-case';
+import { petitionsclerkAddsRespondentToCase } from '../support/setup/petitionsclerk-adds-respondent-to-case';
+import { petitionsclerkServePetition } from '../support/setup/petitionsclerk-serves-petition';
+import { respondentModifiesContactInfo } from '../support/setup/respondent-modifies-contact-info';
+
+const BAR_NUMBER = 'WN7777';
+const USER = 'irspractitioner2';
+
+describe('a repondent modifies their address', () => {
+  it('should generate a notice of change address for all cases associated with the respondent', function () {
+    petitionerCreatesACase();
+    petitionsclerkServePetition();
+    petitionsclerkAddsRespondentToCase(BAR_NUMBER);
+    respondentModifiesContactInfo(USER);
+
+    cy.get('@docketNumber').then(docketNumber => {
+      cy.login(USER, `case-detail/${docketNumber}`);
+    });
+
+    cy.get('button').contains('Notice of Change of Address').should('exist');
+    cy.get('button').contains('Case Information').click();
+    cy.get('button').contains('Parties').click();
+    cy.get('button').contains('Respondent Counsel').click();
+    cy.get('@contactAddress').then(newAddress => {
+      cy.get('span').contains(`${newAddress}`).should('exist');
+    });
+  });
+
+  it('should not generate a notice of change address for any cases with unserved petitions', function () {
+    petitionerCreatesACase();
+    petitionsclerkAddsRespondentToCase(BAR_NUMBER);
+    respondentModifiesContactInfo(USER);
+
+    cy.get('@docketNumber').then(docketNumber => {
+      cy.login(USER, `case-detail/${docketNumber}`);
+    });
+
+    cy.get('button')
+      .contains('Notice of Change of Address for Test IRS Practitioner')
+      .should('not.exist');
+  });
+});
