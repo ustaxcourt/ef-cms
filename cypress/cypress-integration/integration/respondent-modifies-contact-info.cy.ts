82d6740f37ae659bc75d2ca15a42f5a637d76e5c
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-11-08 13:43:26 -0800

    10188: wait for pdf to upload and service to complete before moving on to next step

--- cypress/cypress-integration/integration/respondent-modifies-contact-info.cy.ts
+++ cypress/cypress-integration/integration/respondent-modifies-contact-info.cy.ts
@@ -1,6 +1,6 @@
 import { petitionerCreatesACase } from '../support/setup/petitioner-creates-a-case';
+import { petitionsClerkServesPetition } from '../support/setup/petitionsclerk-serves-petition';
 import { petitionsclerkAddsRespondentToCase } from '../support/setup/petitionsclerk-adds-respondent-to-case';
-import { petitionsclerkServePetition } from '../support/setup/petitionsclerk-serves-petition';
 import { respondentModifiesContactInfo } from '../support/setup/respondent-modifies-contact-info';
 
 const BAR_NUMBER = 'WN7777';
@@ -9,7 +9,7 @@ const USER = 'irspractitioner2';
 describe('a repondent modifies their address', () => {
   it('should generate a notice of change address for all cases associated with the respondent', function () {
     petitionerCreatesACase();
-    petitionsclerkServePetition();
+    petitionsClerkServesPetition();
     petitionsclerkAddsRespondentToCase(BAR_NUMBER);
     respondentModifiesContactInfo(USER);
 
--- cypress/cypress-integration/integration/stamp-order.cy.ts
+++ cypress/cypress-integration/integration/stamp-order.cy.ts
@@ -1,47 +1,62 @@
+import { petitionerCreatesACase } from '../support/setup/petitioner-creates-a-case';
+import { petitionsClerkServesPetition } from '../support/setup/petitionsclerk-serves-petition';
+
 describe('Judge`s chambers stamps an order', () => {
   it('should create an order, serve it, and apply a stamp to it', () => {
-    cy.login('docketclerk1', 'case-detail/103-20');
+    petitionerCreatesACase();
+    petitionsClerkServesPetition();
 
-    cy.get('[data-test="create-dropdown"]').click();
-    cy.get('[data-test="menu-button-add-paper-filing"').click();
+    cy.get('@docketNumber').then(docketNumber => {
+      cy.login('docketclerk1', `case-detail/${docketNumber}`);
 
-    cy.get('input#date-received-picker').type('11/01/2023');
-    cy.get('#document-type .select-react-element__input-container input').type(
-      'Motion for Continuance',
-    );
-    cy.get('#react-select-2-option-0').click({ force: true });
-    cy.get('[data-test="filed-by-option"]').contains('Petitioner').click();
+      cy.get('[data-test="create-dropdown"]').click();
+      cy.get('[data-test="menu-button-add-paper-filing"').click();
 
-    cy.get('#upload-mode-upload').click();
-    cy.get('input#primaryDocumentFile-file').attachFile(
-      '../fixtures/w3-dummy.pdf',
-    );
+      cy.get('input#date-received-picker').type('11/01/2023');
+      cy.get(
+        '#document-type .select-react-element__input-container input',
+      ).type('Motion for Continuance');
+      cy.get('#react-select-2-option-0').click({ force: true });
+      cy.get('[data-test="filed-by-option"]').contains('Petitioner').click();
 
-    cy.get('[data-test="save-and-serve"]').click();
+      cy.get('#upload-mode-upload').click();
+      cy.get('input#primaryDocumentFile-file').attachFile(
+        '../fixtures/w3-dummy.pdf',
+      );
 
-    cy.get('[data-test="confirm"]').click();
+      cy.get('[data-cy="remove-pdf"]');
 
-    cy.login('colvinschambers', 'case-detail/103-20');
+      cy.get('[data-test="save-and-serve"]').click();
 
-    cy.get('[data-test="document-viewer-link-M006"]').last().click();
+      cy.get('[data-test="confirm"]').click();
 
-    cy.get('[data-test="apply-stamp"]').click();
+      cy.get('.usa-alert').should(
+        'contain',
+        'Print and mail to complete paper service.',
+      );
 
-    cy.get('[data-test="status-report-or-stip-decision-due-date"]').click();
-    cy.get('input#due-date-input-statusReportDueDate-picker').type(
-      '11/02/2023',
-    );
-    cy.get('input#due-date-input-statusReportDueDate-picker').should(
-      'have.value',
-      '11/02/2023',
-    );
+      cy.login('colvinschambers', `case-detail/${docketNumber}`);
 
-    cy.get('[data-test="clear-optional-fields"]').click();
+      cy.get('[data-test="document-viewer-link-M006"]').last().click();
 
-    cy.get('[data-test="status-report-or-stip-decision-due-date"]').click();
-    cy.get('input#due-date-input-statusReportDueDate-picker').should(
-      'have.value',
-      '',
-    );
+      cy.get('[data-test="apply-stamp"]').click();
+
+      cy.get('[data-test="status-report-or-stip-decision-due-date"]').click();
+      cy.get('input#due-date-input-statusReportDueDate-picker').type(
+        '11/02/2023',
+      );
+      cy.get('input#due-date-input-statusReportDueDate-picker').should(
+        'have.value',
+        '11/02/2023',
+      );
+
+      cy.get('[data-test="clear-optional-fields"]').click();
+
+      cy.get('[data-test="status-report-or-stip-decision-due-date"]').click();
+      cy.get('input#due-date-input-statusReportDueDate-picker').should(
+        'have.value',
+        '',
+      );
+    });
   });
 });
