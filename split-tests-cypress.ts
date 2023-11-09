a1f61092d43310a8a56706799593593519f52013
Cody Seibert <cseibert113@gmail.com>
2023-11-09 15:03:20 -0500

    provide full test file paths

--- split-tests-cypress.ts
+++ split-tests-cypress.ts
@@ -11,6 +11,8 @@ const shuffled = shuffleSeed.shuffle(files, process.env.GITHUB_SHA);
 const total = parseInt(process.env.CI_NODE_TOTAL!, 10);
 const index = parseInt(process.env.CI_NODE_INDEX!, 10);
 
-const tests = shuffled.filter((num, i) => i % total === index);
+const tests = shuffled
+  .filter((num, i) => i % total === index)
+  .map(file => `./cypress/cypress-integration/integration/${file}`);
 
 console.log(tests.join(','));
