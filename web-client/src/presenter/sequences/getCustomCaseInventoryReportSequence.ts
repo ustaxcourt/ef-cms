eb14b537a03fdb684f6d74ddf873c49bf3bf1cd7
Zachary Rogers <zrogers>
2023-11-10 07:59:03 -0800

    10172 - add type for sequence

--- web-client/src/presenter/sequences/getCustomCaseInventoryReportSequence.ts
+++ web-client/src/presenter/sequences/getCustomCaseInventoryReportSequence.ts
@@ -3,22 +3,24 @@ import { clearErrorAlertsAction } from '../actions/clearErrorAlertsAction';
 import { getCustomCaseInventoryReportAction } from '../actions/CaseInventoryReport/getCustomCaseInventoryReportAction';
 import { setAlertErrorAction } from '../actions/setAlertErrorAction';
 import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
-import { showProgressSequenceDecorator } from '../utilities/showProgressSequenceDecorator';
+import { setWaitingForResponseAction } from '../actions/setWaitingForResponseAction';
 import { startShowValidationAction } from '../actions/startShowValidationAction';
 import { stopShowValidationAction } from '../actions/stopShowValidationAction';
+import { unsetWaitingForResponseAction } from '../actions/unsetWaitingForResponseAction';
 import { validateCustomInventoryFiltersAction } from '../actions/validateCustomInventoryFiltersAction';
 
-export const getCustomCaseInventoryReportSequence =
-  showProgressSequenceDecorator([
-    startShowValidationAction,
-    validateCustomInventoryFiltersAction,
-    {
-      error: [setAlertErrorAction, setValidationErrorsAction],
-      success: [
-        stopShowValidationAction,
-        clearErrorAlertsAction,
-        clearAlertsAction,
-        getCustomCaseInventoryReportAction,
-      ],
-    },
-  ]);
+export const getCustomCaseInventoryReportSequence = [
+  setWaitingForResponseAction,
+  startShowValidationAction,
+  validateCustomInventoryFiltersAction,
+  {
+    error: [setAlertErrorAction, setValidationErrorsAction],
+    success: [
+      stopShowValidationAction,
+      clearErrorAlertsAction,
+      clearAlertsAction,
+      getCustomCaseInventoryReportAction,
+    ],
+  },
+  unsetWaitingForResponseAction,
+] as unknown as (props: { selectedPage: number }) => void;
