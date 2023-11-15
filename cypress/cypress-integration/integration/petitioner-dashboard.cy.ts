d35acd901c48fae07863e093f2cec09668e1b912
Nechama Krigsman <nkrigsman@flexion.us>
2023-11-15 13:46:27 -0500

    10117: create a case instead of hardcoding case

--- cypress/cypress-integration/integration/petitioner-dashboard.cy.ts
+++ cypress/cypress-integration/integration/petitioner-dashboard.cy.ts
@@ -1,18 +1,15 @@
 import { navigateTo as loginAs } from '../support/pages/maintenance';
-// import { petitionerCreatesACase } from '../support/setup/petitioner-creates-case';
+import { petitionerCreatesACase } from '../support/setup/petitioner-creates-a-case';
 
 describe('Petitioner views dashboard', () => {
   it('should display filing fee column', () => {
     loginAs('petitioner');
     cy.getByTestId('filing-fee');
-    cy.getByTestId('102-22')
-      .find('[data-testid="petition-payment-status"]')
-      .should('have.text', 'Not paid');
-    //   petitionerCreatesACase().then(docketNumber => {
-    //     cy.getByTestId('filing-fee');
-    //     cy.getByTestId(docketNumber)
-    //       .getByTestId('petition-payment-status')
-    //       .should('have.text', 'Not paid');
-    //   });
+    petitionerCreatesACase().then(docketNumber => {
+      cy.getByTestId('filing-fee');
+      cy.getByTestId(docketNumber)
+        .find('[data-testid="petition-payment-status"]')
+        .should('have.text', 'Not paid');
+    });
   });
 });
