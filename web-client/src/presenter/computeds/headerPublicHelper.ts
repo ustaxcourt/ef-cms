b592f1f76610d613d0727eed31e3cc8b0e257c82
Jason Fu <jasonfu6699@hotmail.com>
2023-11-17 10:27:57 -0800

    10011: address UX and testing feedback

--- web-client/src/presenter/computeds/headerPublicHelper.ts
+++ web-client/src/presenter/computeds/headerPublicHelper.ts
@@ -1,11 +1,13 @@
 import { Get } from 'cerebral';
 import { state } from '@web-client/presenter/app.cerebral';
 
-export const headerPublicHelper = (get: Get): { inSignUpProcess: boolean } => {
+export const headerPublicHelper = (
+  get: Get,
+): { onCreationPage: boolean; onVerificationSentPage: boolean } => {
   const currentPage = get(state.currentPage) || '';
-  const inSignUpProcess =
-    currentPage.startsWith('CreatePetitionerAccount') ||
-    currentPage.startsWith('VerificationSent');
 
-  return { inSignUpProcess };
+  const onCreationPage = currentPage.startsWith('CreatePetitionerAccount');
+  const onVerificationSentPage = currentPage.startsWith('VerificationSent');
+
+  return { onCreationPage, onVerificationSentPage };
 };
