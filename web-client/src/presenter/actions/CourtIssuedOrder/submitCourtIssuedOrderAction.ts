23f469f9f75376cecc5f6b5b06b8f0a6c1051d72
Absolutestunna <joshujah007@gmail.com>
2023-11-20 11:14:56 -0500

    1137 - saving when consolidated cases have been selected. continuation of newly created compute for selecting consolidated cases

--- web-client/src/presenter/actions/CourtIssuedOrder/submitCourtIssuedOrderAction.ts
+++ web-client/src/presenter/actions/CourtIssuedOrder/submitCourtIssuedOrderAction.ts
@@ -1,37 +1,38 @@
 import { omit } from 'lodash';
 import { state } from '@web-client/presenter/app.cerebral';
 
-/**
- * sets the court issued order onto the case
- * @param {object} providers the providers object
- * @param {object} providers.applicationContext the application context
- * @param {object} providers.props the cerebral props object
- * @returns {Promise} async action
- */
 export const submitCourtIssuedOrderAction = async ({
   applicationContext,
   get,
   props,
-}: ActionProps) => {
+}: ActionProps<{
+  primaryDocumentFileId: string;
+}>) => {
   let caseDetail;
   const { docketNumber } = get(state.caseDetail);
   const { primaryDocumentFileId: docketEntryId } = props;
   const formData = get(state.form);
   const { docketEntryIdToEdit } = formData;
+  const consolidatedCasesToMultiDocketOn = get(
+    state.modal.form.consolidatedCasesToMultiDocketOn,
+  );
+
+  const addedDocketNumbers = applicationContext
+    .getUtilities()
+    .getSelectedConsolidatedCasesToMultiDocketOn(
+      consolidatedCasesToMultiDocketOn,
+    );
 
   let documentMetadata = omit(formData, [
     'primaryDocumentFile',
     'docketEntryIdToEdit',
   ]);
 
-  documentMetadata = {
-    ...documentMetadata,
-    docketNumber,
-  };
+  documentMetadata.docketNumber = docketNumber;
 
   documentMetadata.draftOrderState = {
     ...documentMetadata,
-    addedDocketNumbers: get(state.addedDocketNumbers),
+    addedDocketNumbers,
   };
 
   await applicationContext
