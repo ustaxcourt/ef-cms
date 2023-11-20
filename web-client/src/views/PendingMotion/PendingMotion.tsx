4c01071c56c39a39e46b22ce65750a1a5c228e0e
John Cruz <jcruz@flexion.us>
2023-11-20 15:17:56 -0700

    10176: Added message on top of the pendingmotion component;

--- web-client/src/views/PendingMotion/PendingMotion.tsx
+++ web-client/src/views/PendingMotion/PendingMotion.tsx
@@ -1,8 +1,31 @@
+import { Button } from '@web-client/ustc-ui/Button/Button';
 import { connect } from '@web-client/presenter/shared.cerebral';
+import { sequences } from '@web-client/presenter/app.cerebral';
 import React from 'react';
 
-export const PendingMotion = connect({}, function PendingMotion({}) {
-  return <>john testing</>;
-});
+export const PendingMotion = connect(
+  {
+    navigateToPathSequence: sequences.navigateToPathSequence,
+  },
+  function PendingMotion({ navigateToPathSequence }) {
+    return (
+      <>
+        <span>
+          Showing motions pending for more than 180 days. To view all, run the{' '}
+          <Button
+            link
+            onClick={() =>
+              navigateToPathSequence({
+                path: '/reports/pending-report',
+              })
+            }
+          >
+            Pending Report.
+          </Button>
+        </span>
+      </>
+    );
+  },
+);
 
 PendingMotion.displayName = 'PendingMotion';
