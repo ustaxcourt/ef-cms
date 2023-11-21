0fe1a1ba12b43c49525b667b80ff7f5a4828932a
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-11-21 11:26:55 -0800

    Backmerge from staging: Update cypress helper method name after refactor

--- cypress/cypress-integration/integration/private-practitioner-dashboard.cy.ts
+++ cypress/cypress-integration/integration/private-practitioner-dashboard.cy.ts
@@ -1,12 +1,12 @@
 import { navigateTo as loginAs } from '../support/pages/maintenance';
-import { petitionerCreatesACase } from '../support/setup/petitioner-creates-a-case';
+import { petitionerCreatesEletronicCase } from '../../helpers/petitioner-creates-electronic-case';
 
 describe('Private practitioner views dashboard', () => {
   it('should display filing fee column', () => {
     loginAs('privatepractitioner');
     cy.get('[data-testid="case-list-table"]');
     cy.get('[data-testid="filing-fee"]');
-    petitionerCreatesACase().then(docketNumber => {
+    petitionerCreatesEletronicCase().then(docketNumber => {
       cy.get('[data-testid="filing-fee"]');
       cy.get(`[data-testid="${docketNumber}"]`)
         .find('[data-testid="petition-payment-status"]')
