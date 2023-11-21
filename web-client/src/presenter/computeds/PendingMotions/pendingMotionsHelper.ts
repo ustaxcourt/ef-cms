c8ffc75b2eeadf549e086bdf93784a2b4c4d3b08
John Cruz <jcruz@flexion.us>
2023-11-21 15:48:20 -0700

    10176: Created helper;

---
+++ web-client/src/presenter/computeds/PendingMotions/pendingMotionsHelper.ts
@@ -0,0 +1,29 @@
+import { DocketEntryWithWorksheet } from '@shared/business/useCases/pendingMotion/getPendingMotionDocketEntriesForCurrentJudgeInteractor';
+import { Get } from 'cerebral';
+import { isLeadCase } from '@shared/business/entities/cases/Case';
+import { state } from '@web-client/presenter/app.cerebral';
+
+export type PendingMotionsHelperResults = {
+  formattedPendingMotions: (DocketEntryWithWorksheet & {
+    consolidatedIconTooltipText: string;
+    inConsolidatedGroup: boolean;
+    isLeadCase: boolean;
+  })[];
+};
+
+export const pendingMotionsHelper = (get: Get): PendingMotionsHelperResults => {
+  const { docketEntries } = get(state.pendingMotions);
+
+  const formattedPendingMotions = docketEntries.map(entry => {
+    return {
+      ...entry,
+      consolidatedIconTooltipText: isLeadCase(entry) ? 'Lead case' : '',
+      inConsolidatedGroup: !!entry.leadDocketNumber,
+      isLeadCase: isLeadCase(entry),
+    };
+  });
+
+  return {
+    formattedPendingMotions,
+  };
+};
