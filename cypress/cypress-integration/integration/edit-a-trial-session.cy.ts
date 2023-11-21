bd6f9ff49d9c2fef57b4e21bddf05c9ab31906c4
Cody Seibert <cseibert113@gmail.com>
2023-11-21 16:56:21 -0500

    removing changes which force the failure

--- cypress/cypress-integration/integration/edit-a-trial-session.cy.ts
+++ cypress/cypress-integration/integration/edit-a-trial-session.cy.ts
@@ -7,8 +7,8 @@ import { getCancelModalTitle } from '../support/pages/form-cancel-modal-dialog';
 describe('Edit a trial session', () => {
   it('should display a modal to confirm discarding changes when cancel is clicked', () => {
     navigateToTrialSessionDetail(
-      'asdfasdfsadf',
-      '208a959f-9526-dd-b262-e58c476a4604',
+      'petitionsclerk',
+      '208a959f-9526-4db5-b262-e58c476a4604',
     );
 
     getCancelButton().click();
