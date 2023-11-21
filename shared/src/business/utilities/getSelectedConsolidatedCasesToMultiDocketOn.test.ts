8e00a654d8637851e6ca2b809db6a84dbda2c582
Absolutestunna <joshujah007@gmail.com>
2023-11-21 15:01:35 -0500

    1137 - clean up getSelectedConsolidatedCasesToMultiDocketOn utilitiy function

---
+++ shared/src/business/utilities/getSelectedConsolidatedCasesToMultiDocketOn.test.ts
@@ -0,0 +1,35 @@
+import {
+  ConsolidatedCasesWithCheckboxInfoType,
+  getSelectedConsolidatedCasesToMultiDocketOn,
+} from './getSelectedConsolidatedCasesToMultiDocketOn';
+
+describe('getSelectedConsolidatedCasesToMultiDocketOn', () => {
+  let consolidatedCases: ConsolidatedCasesWithCheckboxInfoType[];
+  beforeEach(() => {
+    consolidatedCases = [];
+  });
+  it('returns an empty array if there are no consolidated cases passed', () => {
+    const result =
+      getSelectedConsolidatedCasesToMultiDocketOn(consolidatedCases);
+
+    expect(result).toEqual([]);
+  });
+
+  it('returns a list of cases that were previously selected', () => {
+    consolidatedCases = [
+      {
+        checked: true,
+        docketNumberWithSuffix: '101-20L',
+      },
+      {
+        checked: false,
+        docketNumberWithSuffix: '103-20S',
+      },
+    ];
+
+    const result =
+      getSelectedConsolidatedCasesToMultiDocketOn(consolidatedCases);
+
+    expect(result).toEqual(['101-20L']);
+  });
+});
