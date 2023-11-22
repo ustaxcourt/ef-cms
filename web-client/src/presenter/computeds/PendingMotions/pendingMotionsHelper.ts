29370f45156b3f4371cd797afd9ff7f4b8ac3490
John Cruz <jcruz@flexion.us>
2023-11-21 17:05:28 -0700

    10176: Sort by daysSinceCreated;

--- web-client/src/presenter/computeds/PendingMotions/pendingMotionsHelper.ts
+++ web-client/src/presenter/computeds/PendingMotions/pendingMotionsHelper.ts
@@ -14,14 +14,20 @@ export type PendingMotionsHelperResults = {
 export const pendingMotionsHelper = (get: Get): PendingMotionsHelperResults => {
   const { docketEntries } = get(state.pendingMotions);
 
-  const formattedPendingMotions = docketEntries.map(entry => {
-    return {
-      ...entry,
-      consolidatedIconTooltipText: isLeadCase(entry) ? 'Lead case' : '',
-      inConsolidatedGroup: !!entry.leadDocketNumber,
-      isLeadCase: isLeadCase(entry),
-    };
-  });
+  const formattedPendingMotions = docketEntries
+    .map(entry => {
+      return {
+        ...entry,
+        consolidatedIconTooltipText: isLeadCase(entry) ? 'Lead case' : '',
+        inConsolidatedGroup: !!entry.leadDocketNumber,
+        isLeadCase: isLeadCase(entry),
+      };
+    })
+    .sort((aCase, bCase) => {
+      if (aCase.daysSinceCreated < bCase.daysSinceCreated) return -1;
+      if (aCase.daysSinceCreated > bCase.daysSinceCreated) return 1;
+      return 0;
+    });
 
   return {
     formattedPendingMotions,
