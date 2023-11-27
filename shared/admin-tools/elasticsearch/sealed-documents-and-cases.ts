780c7932302f83252fa39094e707fe8a6efba565
Zachary Rogers <zrogers>
2023-11-27 15:02:22 -0800

    devex - Update dynamoClient to V3

--- shared/admin-tools/elasticsearch/sealed-documents-and-cases.ts
+++ shared/admin-tools/elasticsearch/sealed-documents-and-cases.ts
@@ -1,40 +1,36 @@
 // usage: npx ts-node --transpile-only shared/admin-tools/elasticsearch/sealed-documents-and-cases.ts > ~/Desktop/sealed-cases.csv
 
-import { createApplicationContext } from '../../../web-api/src/applicationContext';
+import {
+  ServerApplicationContext,
+  createApplicationContext,
+} from '../../../web-api/src/applicationContext';
 import { searchAll } from '../../../web-api/src/persistence/elasticsearch/searchClient';
-import { unmarshall } from '@aws-sdk/util-dynamodb';
 
 const loadCaseFromInitialBlackstoneMigrationDb = async ({
   applicationContext,
   docketNumber,
 }: {
-  applicationContext: any;
+  applicationContext: ServerApplicationContext;
   docketNumber: string;
 }) => {
   const result = await applicationContext
-    .getDynamoClient()
-    .getItem({
+    .getDocumentClient()
+    .get({
       Key: {
-        pk: {
-          S: `case|${docketNumber}`,
-        },
-        sk: {
-          S: `case|${docketNumber}`,
-        },
+        pk: `case|${docketNumber}`,
+        sk: `case|${docketNumber}`,
       },
       TableName: 'efcms-prod-first',
     })
     .promise();
 
-  return result && 'Item' in result && result.Item
-    ? unmarshall(result.Item)
-    : {};
+  return result || {};
 };
 
 const getSealedCases = async ({
   applicationContext,
 }: {
-  applicationContext: any;
+  applicationContext: ServerApplicationContext;
 }) => {
   const searchParameters = {
     body: {
@@ -110,7 +106,7 @@ const getSealedDocketEntries = async ({
   applicationContext,
   docketNumbers,
 }: {
-  applicationContext: any;
+  applicationContext: ServerApplicationContext;
   docketNumbers: string[];
 }) => {
   const searchParameters = {
@@ -184,7 +180,7 @@ const getOrdersSinceDawson = async ({
   applicationContext,
   docketNumbers,
 }: {
-  applicationContext: any;
+  applicationContext: ServerApplicationContext;
   docketNumbers: string[];
 }) => {
   const searchParameters = {
