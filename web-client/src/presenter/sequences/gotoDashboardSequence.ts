b7e53f46670f8c5cec13815756a564f2797c6817
John Cruz <jcruz@flexion.us>
2023-11-27 21:13:55 -0700

    10176: Updated Chambers Dashboard;

--- web-client/src/presenter/sequences/gotoDashboardSequence.ts
+++ web-client/src/presenter/sequences/gotoDashboardSequence.ts
@@ -80,6 +80,10 @@ const goToDashboard = [
                       getSubmittedAndCavCasesForCurrentJudgeAction,
                       setSubmittedAndCavCasesForJudgeAction,
                     ],
+                    [
+                      getPendingMotionDocketEntriesForCurrentJudgeAction,
+                      setPendingMotionDocketEntriesForCurrentJudgeAction,
+                    ],
                   ]),
                   setupCurrentPageAction('DashboardChambers'),
                 ],
