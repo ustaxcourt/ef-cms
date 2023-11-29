b0d02fb03f17d13802227b3c4428ea6d63a77d01
John Cruz <jcruz@flexion.us>
2023-11-29 13:49:59 -0700

    10197: Fix padding;

--- web-client/src/views/WorkQueue/RecentMessages.tsx
+++ web-client/src/views/WorkQueue/RecentMessages.tsx
@@ -5,7 +5,12 @@ import React from 'react';
 export const RecentMessages = () => {
   return (
     <div className="margin-top-6">
-      <Button link className="float-right" href="/messages/my/inbox">
+      <Button
+        link
+        className="float-right"
+        href="/messages/my/inbox"
+        overrideMargin="margin-0"
+      >
         View All Messages
       </Button>
 
