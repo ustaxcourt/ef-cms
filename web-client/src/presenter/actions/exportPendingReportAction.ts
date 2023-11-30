5686a004055731ba70445dc137e8f093b77471bb
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2023-11-30 11:37:34 -0600

    10194: manually create and download csv

--- web-client/src/presenter/actions/exportPendingReportAction.ts
+++ web-client/src/presenter/actions/exportPendingReportAction.ts
@@ -1,3 +1,59 @@
-export const exportPendingReportAction = ({ props }: ActionProps) => {
-  console.log('export export export', props.export);
+import { FORMATS } from '@shared/business/utilities/DateHandler';
+import { pick } from 'lodash';
+import { state } from '@web-client/presenter/app.cerebral';
+
+export const exportPendingReportAction = ({
+  applicationContext,
+  get,
+}: ActionProps) => {
+  //create fileName
+  const today = applicationContext.getUtilities().formatNow(FORMATS.MMDDYYYY);
+  const fileName = getFileName(get(state.pendingReports.selectedJudge), today);
+  const formattedPendingItems = get(state.formattedPendingItemsHelper.items);
+  const headers = [
+    'Docket No.',
+    'Date Filed',
+    'Case Title',
+    'Filings and Proceedings',
+    'Case Status',
+    'Judge',
+  ];
+
+  const data = formattedPendingItems.map(item => {
+    return pick(item, [
+      'docketNumberWithSuffix',
+      'formattedFiledDate',
+      'caseTitle',
+      'formattedName',
+      'formattedStatus',
+      'associatedJudgeFormatted',
+    ]);
+  });
+  const csv = getCsv(data, headers);
+
+  const blob = new Blob([csv], { type: 'text/csv' });
+  const url = window.URL.createObjectURL(blob);
+  const a = window.document.createElement('a');
+  a.setAttribute('href', url);
+  a.setAttribute('download', fileName);
+  a.click();
+};
+
+const getFileName = (judgeName, date) => {
+  return 'Pending Report - ' + judgeName + ' ' + date;
+};
+
+const getCsv = (data, headers) => {
+  const lines = headers;
+  for (let item of data) {
+    const line = Object.values(item).map(x => escapeCsvValue(x));
+    lines.push(line.join(';'));
+  }
+  return lines.join('\n');
+};
+
+const escapeCsvValue = value => {
+  if (!value || value.indexOf(';') === -1) return value;
+
+  return '"' + value.replace('"', '""') + '"';
 };
