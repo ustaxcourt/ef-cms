2c6f88f079223c5367dd0b60f4406aaa49f28775
Jason Fu <jasonfu6699@hotmail.com>
2023-11-30 10:51:25 -0800

    10194: fix a typo, add new actions

--- web-client/src/presenter/sequences/exportPendingReportSequence.ts
+++ web-client/src/presenter/sequences/exportPendingReportSequence.ts
@@ -1,5 +1,7 @@
 import { chooseExportPendingReportMethodAction } from '@web-client/presenter/actions/chooseExportPendingReportMethodAction';
-import { exportPendingReportAction } from '@web-client/presenter/actions/exportPendingReportAction copy';
+import { exportPendingReportAction } from '@web-client/presenter/actions/exportPendingReportAction';
+import { exportWithCSVSAction } from '@web-client/presenter/actions/exportWithCSVSAction';
+import { exportWithE2CSVAction } from '@web-client/presenter/actions/exportWithE2CSVAction';
 
 export const exportPendingReportSequence = [
   chooseExportPendingReportMethodAction,
