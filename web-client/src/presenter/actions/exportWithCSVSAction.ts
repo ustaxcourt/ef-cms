7f3261c53b6e5422c7a66ff0ddd26026058419f3
Jason Fu <jasonfu6699@hotmail.com>
2023-11-30 11:39:09 -0800

    10194: use libraries in different paths

--- web-client/src/presenter/actions/exportWithCSVSAction.ts
+++ web-client/src/presenter/actions/exportWithCSVSAction.ts
@@ -1,12 +1,11 @@
 import { FORMATS } from '@shared/business/utilities/DateHandler';
-import { pick } from 'lodash';
 import { state } from '@web-client/presenter/app.cerebral';
+import { stringify } from 'csv-stringify/browser/esm/sync';
 
 export const exportWithCSVSAction = ({
   applicationContext,
   get,
 }: ActionProps) => {
-  //remove
   const start = Date.now();
 
   const today = applicationContext
@@ -14,26 +13,18 @@ export const exportWithCSVSAction = ({
     .formatNow(FORMATS.MMDDYYYY_UNDERSCORED);
   const fileName = getFileName(get(state.pendingReports.selectedJudge), today);
   const formattedPendingItems = get(state.formattedPendingItemsHelper.items);
-  const headers = [
-    'Docket No.',
-    'Date Filed',
-    'Case Title',
-    'Filings and Proceedings',
-    'Case Status',
-    'Judge',
-  ];
-
-  const data = formattedPendingItems.map(item => {
-    return pick(item, [
-      'docketNumberWithSuffix',
-      'formattedFiledDate',
-      'caseTitle',
-      'formattedName',
-      'formattedStatus',
-      'associatedJudgeFormatted',
-    ]);
+  const csv = stringify(formattedPendingItems, {
+    bom: true,
+    columns: [
+      { header: 'Docket No.', key: 'docketNumberWithSuffix' },
+      { header: 'Date Filed', key: 'formattedFiledDate' },
+      { header: 'Case Title', key: 'caseTitle' },
+      { header: 'Filings and Proceedings', key: 'formattedName' },
+      { header: 'Case Status', key: 'formattedStatus' },
+      { header: 'Judge', key: 'associatedJudgeFormatted' },
+    ],
+    header: true,
   });
-  const csv = getCsv(data, headers);
 
   const blob = new Blob([csv], { type: 'text/csv' });
   const url = window.URL.createObjectURL(blob);
@@ -47,19 +38,4 @@ export const exportWithCSVSAction = ({
 
 const getFileName = (judgeName, date) => {
   return 'Pending Report - ' + judgeName + ' ' + date;
-};
-
-const getCsv = (data, headers) => {
-  const lines = [headers.join(';')];
-  for (let item of data) {
-    const line = Object.values(item).map(x => escapeCsvValue(x));
-    lines.push(line.join(';'));
-  }
-  return lines.join('\n');
-};
-
-const escapeCsvValue = value => {
-  if (!value || value.indexOf(';') === -1) return value;
-
-  return '"' + value.replace('"', '""') + '"';
 };
