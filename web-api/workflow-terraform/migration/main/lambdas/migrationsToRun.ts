1701d69584bf92e2e63ec979695a358006227788
Zachary Rogers <zrogers>
2023-12-01 10:14:21 -0800

    10117 - removed unused test file

--- web-api/workflow-terraform/migration/main/lambdas/migrationsToRun.ts
+++ web-api/workflow-terraform/migration/main/lambdas/migrationsToRun.ts
@@ -1,5 +1,1 @@
-import { migrateItems as cleanupUserCaseMappings } from './migrations/cleanup-usercase-mappings';
-
-export const migrationsToRun = [
-  { key: 'cleanup-usercase-mappings', script: cleanupUserCaseMappings },
-];
+export const migrationsToRun = [];
