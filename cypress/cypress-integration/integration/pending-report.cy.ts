6fc74231128b50e55d333705081b03446eccda28
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2023-12-05 15:57:42 -0600

    10194: update cypress test

--- cypress/cypress-integration/integration/pending-report.cy.ts
+++ cypress/cypress-integration/integration/pending-report.cy.ts
@@ -6,17 +6,17 @@ import { loginAsDocketClerk } from '../../helpers/auth/login-as-helpers';
 import path from 'path';
 
 describe('Pending report', () => {
+  const today = formatNow(FORMATS.MMDDYYYY_UNDERSCORED);
+  const fileName = `Pending Report - Chief Judge ${today}.csv`;
+  const downloadsFolder = Cypress.config('downloadsFolder');
   describe('View pending report', () => {
     it('should view pending report', () => {
       loginAsDocketClerk();
-      cy.get('#reports-btn').click();
-      cy.get('#pending-report').click();
+      cy.get('[data-testid="dropdown-select-report"]').click();
+      cy.get('[data-testid="select-pending-report"]').click();
       cy.get('[data-testid="dropdown-select-judge"]').select('Chief Judge');
-      cy.intercept('GET', 'url').as('download');
       cy.get('[data-testid="export-pending-report"]').click();
-      const today = formatNow(FORMATS.MMDDYYYY_UNDERSCORED);
-      const fileName = `Pending Report - Chief Judge ${today}.csv`;
-      const downloadsFolder = Cypress.config('downloadsFolder');
+
       cy.readFile(path.join(downloadsFolder, fileName));
     });
   });
