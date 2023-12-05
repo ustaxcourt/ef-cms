b6314d82612981fd4cee1e06b56fbf450f3133f3
Zachary Rogers <zrogers>
2023-12-05 14:51:20 -0800

    10007: WIP style login form

--- web-client/src/views/Public/Login/Login.tsx
+++ web-client/src/views/Public/Login/Login.tsx
@@ -1,113 +1,63 @@
 import { Button } from '@web-client/ustc-ui/Button/Button';
 import { connect } from '@cerebral/react';
-import { state } from '@web-client/presenter/app-public.cerebral';
 import React from 'react';
 
-export const Login = connect(
-  {
-    alertError: state.alertError,
-  },
-  ({ alertError }) => {
-    return (
-      <>
-        <div className="grid-container grid-gap-lg padding-x-4">
-          {alertError && (
-            <div
-              className="grid-row margin-bottom-2"
-              style={{ width: 'fit-content' }}
-            >
-              {/* <MessageAlert
-                alertType={alertError.alertType}
-                message={alertError.message}
-                title={alertError.title}
-              ></MessageAlert> */}
+export const Login = connect({}, () => {
+  return (
+    <>
+      <div className="grid-container">
+        <div className="grid-row bg-white padding-x-5 padding-y-3">
+          <div className="grid-col">
+            <h1 className="margin-bottom-1">Log in to DAWSON</h1>
+            <span>Email address and password are case sensitive.</span>
+            <form className="margin-top-4">
+              <label className="usa-label" htmlFor="email">
+                Email address
+              </label>
+              <input
+                required
+                autoCapitalize="off"
+                autoCorrect="off"
+                className="usa-input"
+                id="email"
+                name="email"
+                type="text"
+              />
+              <label className="usa-label" htmlFor="password">
+                Password
+              </label>
+              <input
+                required
+                className="usa-input"
+                id="password"
+                name="password"
+              />
+              <button
+                className="usa-show-password"
+                data-hide-text="Hide password"
+                data-show-text="Show password"
+                type="button"
+              >
+                Show password
+              </button>
+              <Button className="usa-button margin-top-3">Log in</Button>
+            </form>
+            <div>
+              <Button className="margin-top-1" link={true} type="button">
+                Forgot password?
+              </Button>
             </div>
-          )}
-          <div className="grid-row bg-white padding-x-5 padding-y-4">
-            <div className="grid-col">
-              <h1 className="margin-bottom-1">Log in to DAWSON</h1>
-              <span>Email address and password are case sensitive.</span>
-
-              <form>
-                <label className="usa-label" htmlFor="email">
-                  Email address
-                </label>
-                <input
-                  required
-                  autoCapitalize="off"
-                  autoCorrect="off"
-                  className="usa-input"
-                  id="email"
-                  name="email"
-                  type="text"
-                  onBlur={() => {
-                    // setInFocusEmail(false);
-                  }}
-                  onChange={e => {
-                    // updateFormValueSequence({
-                    //   key: 'email',
-                    //   value: e.target.value,
-                    // });
-                  }}
-                  // onFocus={() => setInFocusEmail(true)}
-                />
-                <label className="usa-label" htmlFor="password">
-                  Password
-                </label>
-                <input
-                  required
-                  className="usa-input"
-                  id="password"
-                  name="password"
-                  // type={showPassword ? 'text' : 'password'}
-                  onChange={e => {
-                    // updateFormValueSequence({
-                    //   key: 'password',
-                    //   value: e.target.value,
-                    // });
-                  }}
-                />
-                <button
-                  aria-controls="password password-create-account-confirm"
-                  className="usa-show-password"
-                  data-hide-text="Hide password"
-                  data-show-text="Show password"
-                  type="button"
-                >
-                  Show password
-                </button>
-                <Button className="usa-button margin-top-2" id="submit-button">
-                  Log in
-                </Button>
-              </form>
-
-              <div>
-                <Button
-                  aria-controls="password password-create-account-confirm"
-                  className=""
-                  link={true}
-                  type="button"
-                >
-                  Forgot password?
-                </Button>
-              </div>
-
-              <div>
-                <span>Don't have an account?</span>{' '}
-                <Button
-                  aria-controls="password password-create-account-confirm"
-                  link={true}
-                  type="button"
-                >
-                  Create your account now.
-                </Button>
-              </div>
+            <div>
+              <span>Don&apos;t have an account?</span>{' '}
+              <Button className="padding-top-0" link={true} type="button">
+                Create your account now.
+              </Button>
             </div>
           </div>
         </div>
-      </>
-    );
-  },
-);
+      </div>
+    </>
+  );
+});
 
 Login.displayName = 'Login';
