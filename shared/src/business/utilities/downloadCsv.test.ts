a22938fbbf2bf78170c4ad40bd54c8892bcea1a5
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2023-12-05 15:32:28 -0600

    10194: finish unit test for downloadCsv

--- shared/src/business/utilities/downloadCsv.test.ts
+++ shared/src/business/utilities/downloadCsv.test.ts
@@ -1,8 +1,9 @@
 import { downloadCsv } from '@shared/business/utilities/downloadCsv';
 
 const oldWindowUrl = window.URL;
-// const oldDocument = window.document;
 
+const csvString = 'abc123';
+const fileName = 'fileName';
 const mockUrl = 'www.test.com';
 
 let createObjectURLSpy;
@@ -12,30 +13,28 @@ let createElementSpy;
 describe('downloadCsv', () => {
   beforeAll(() => {
     createObjectURLSpy = jest.fn().mockReturnValue(mockUrl);
+
     anchorElement = window.document.createElement('a');
     createElementSpy = jest.fn().mockReturnValue(anchorElement);
+
     window.URL.createObjectURL = createObjectURLSpy;
     window.document.createElement = createElementSpy;
   });
 
   afterAll(() => {
     window.URL = oldWindowUrl;
-    // window.document = oldDocument;
   });
 
   it('creates csv and download link using provided csvString and fileName', () => {
-    const csvString = 'abc123';
     const expectedCsvBlob = new Blob([csvString], { type: 'text/csv' });
-    const fileName = 'fileName';
+    const expectedAnchorElement = `<a id="download-csv" href="${mockUrl}" download="${fileName}"></a>`;
 
     downloadCsv({ csvString, fileName });
 
     expect(createObjectURLSpy).toHaveBeenCalledWith(expectedCsvBlob);
     expect(createElementSpy).toHaveBeenCalledWith('a');
-    const expectedAnchorElement = `<a download="fileName" href="${mockUrl}" id="download-csv"/>`;
-    // const expectedAnchorElement =
-    //   '<a download="fileName" href="\'www.test.com\'" id="download-csv"/>';
-    expect(anchorElement).toContain(expectedAnchorElement);
+
+    expect(anchorElement.outerHTML).toEqual(expectedAnchorElement);
   });
 
   it('clicks on download link', () => {
@@ -44,9 +43,6 @@ describe('downloadCsv', () => {
       setAttribute: jest.fn(),
     };
     createElementSpy.mockReturnValueOnce(mockAnchorElement);
-    const csvString = 'abc123';
-
-    const fileName = 'fileName';
 
     downloadCsv({ csvString, fileName });
 
