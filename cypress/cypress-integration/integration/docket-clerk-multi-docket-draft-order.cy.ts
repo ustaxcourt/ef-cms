5bd86af0c44b9c5d2a8e0fa9b87ae4e3de23806a
Absolutestunna <joshujah007@gmail.com>
2023-12-05 18:06:45 -0500

    1137 - correct cypress test for draft orders flow

--- cypress/cypress-integration/integration/docket-clerk-multi-docket-draft-order.cy.ts
+++ cypress/cypress-integration/integration/docket-clerk-multi-docket-draft-order.cy.ts
@@ -2,10 +2,15 @@ import { loginAsDocketClerk } from '../../helpers/auth/login-as-helpers';
 
 describe('Docket clerk creates a draft order with selected docket numbers', function () {
   const leadCase = '111-19';
+  const orderTitle = 'Order first title';
+  const orderEventCode = 'O';
+  const editedOrderTitle = 'Order edited title';
+
   let draftsCount: number;
 
   it('should create an order with selected cases', () => {
     loginAsDocketClerk();
+
     cy.get('[data-testid="docket-number-search-input"]').clear();
     cy.get('[data-testid="docket-number-search-input"]').type(
       `${leadCase}{enter}`,
@@ -14,32 +19,36 @@ describe('Docket clerk creates a draft order with selected docket numbers', func
     cy.get('[data-testid="icon-tab-unread-messages-count"]')
       .invoke('text')
       .then(text => {
-        draftsCount = Number(text);
+        // default count to 0 if there are no drafts
+        draftsCount = !text ? 0 : Number(text);
       });
 
     cy.get('[data-testid="search-docket-number"]').click();
     cy.get('[data-testid="case-detail-menu-button"] > .svg-inline--fa').click();
     cy.get('[data-testid="menu-button-create-order"]').click();
-    cy.get('[data-testid="event-code-select"]').select('O');
+    cy.get('[data-testid="event-code-select"]').select(orderEventCode);
     cy.get('[data-testid="create-order-document-title"]').clear();
-    cy.get('[data-testid="create-order-document-title"]').type(
-      'Order first title',
-    );
-    cy.get('[data-testid="modal-button-confirm"]').click();
+    cy.get('[data-testid="create-order-document-title"]').type(orderTitle);
+    cy.get('[data-testid="modal-button-confirm"]')
+      .invoke('click')
+      .then(() => {
+        cy.visit(
+          `case-detail/${leadCase}/create-order?documentTitle=${orderTitle}&documentType=Order&eventCode=${orderEventCode}`,
+        );
+      });
+
     cy.get('[data-testid="create-order-page-title"]').should(
-      'have.text',
-      'Create Order first title Edit Title',
+      'contain',
+      `Create ${orderTitle}`,
     );
     cy.get('[data-testid="create-order-page-title"] > .margin-left-1').click();
     cy.get('[data-testid="edit-order-document-title"]').click();
     cy.get('[data-testid="edit-order-document-title"]').clear();
-    cy.get('[data-testid="edit-order-document-title"]').type(
-      'Order edited title',
-    );
+    cy.get('[data-testid="edit-order-document-title"]').type(editedOrderTitle);
     cy.get('[data-testid="modal-button-confirm"]').click();
     cy.get('[data-testid="create-order-page-title"]').should(
-      'have.text',
-      'Create Order edited title Edit Title',
+      'contain',
+      `Create ${editedOrderTitle}`,
     );
     cy.get('[data-testid="add-docket-number-btn"]').should(
       'have.text',
@@ -61,6 +70,7 @@ describe('Docket clerk creates a draft order with selected docket numbers', func
     );
     cy.get('.ql-editor').click();
     cy.get('[data-testid="save-order-button"]').click();
+
     cy.get('[data-testid="sign-pdf-canvas"]').click();
     cy.get('[data-testid="save-signature-button"]').click();
     cy.get('.usa-alert__text').should(
