08604599303f47e4b32f2438b1d511b1e341df7a
Absolutestunna <joshujah007@gmail.com>
2023-12-05 14:51:32 -0500

    1137 - remove action to reset title and icon after editing order title.

--- web-client/src/presenter/sequences/openEditOrderTitleModalSequence.ts
+++ web-client/src/presenter/sequences/openEditOrderTitleModalSequence.ts
@@ -1,9 +1,7 @@
 import { setEditOrderTitleDataOnModalAction } from '../actions/CourtIssuedOrder/setEditOrderTitleDataOnModalAction';
-import { setSelectedConsolidatedCasesToMultiDocketOnAction } from '@web-client/presenter/actions/setSelectedConsolidatedCasesToMultiDocketOnAction';
 import { setShowModalFactoryAction } from '../actions/setShowModalFactoryAction';
 
 export const openEditOrderTitleModalSequence = [
-  setSelectedConsolidatedCasesToMultiDocketOnAction(false),
   setEditOrderTitleDataOnModalAction,
   setShowModalFactoryAction('EditOrderTitleModal'),
 ];
