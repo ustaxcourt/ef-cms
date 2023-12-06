ed073db6381946cb4ba1fc20926b97eb0ca339d8
John Cruz <jcruz@flexion.us>
2023-12-06 09:57:47 -0700

    10176: Add authorization check in interactor;

--- shared/src/business/useCases/pendingMotion/deleteDocketEntryWorksheetInteractor.ts
+++ shared/src/business/useCases/pendingMotion/deleteDocketEntryWorksheetInteractor.ts
@@ -1,12 +1,22 @@
+import {
+  ROLE_PERMISSIONS,
+  isAuthorized,
+} from '@shared/authorization/authorizationClientService';
+import { UnauthorizedError } from '@web-api/errors/errors';
+
 export const deleteDocketEntryWorksheetInteractor = async (
   applicationContext: IApplicationContext,
   docketEntryId: string,
 ): Promise<void> => {
-  // TODO: Determine who can delete Docket Entry Worksheet
-  // const authorizedUser = applicationContext.getCurrentUser();
-  // if (!isAuthorized(authorizedUser, ROLE_PERMISSIONS.JUDGE_ACTIVITY_REPORT)) {
-  // throw new UnauthorizedError('Unauthorized');
-  // }
+  const authorizedUser = applicationContext.getCurrentUser();
+  if (
+    !isAuthorized(
+      authorizedUser,
+      ROLE_PERMISSIONS.DELETE_DOCKET_ENTRY_WORKSHEET,
+    )
+  ) {
+    throw new UnauthorizedError('Unauthorized');
+  }
 
   await applicationContext
     .getPersistenceGateway()
