d47ddf15e568e4a03f132c65a12b74b2271c3e48
Zachary Rogers <zrogers>
2023-12-06 10:23:32 -0800

    devex-1236: Add simple logs for getMaintenance mode to debug 504s if they continue to occur.

--- shared/src/business/useCases/getMaintenanceModeInteractor.ts
+++ shared/src/business/useCases/getMaintenanceModeInteractor.ts
@@ -7,8 +7,13 @@
 export const getMaintenanceModeInteractor = async (
   applicationContext: IApplicationContext,
 ) => {
+  const start = Date.now();
+  console.log('Start getMaintenanceModeInteractor');
   const result = await applicationContext
     .getPersistenceGateway()
     .getMaintenanceMode({ applicationContext });
+  console.log(
+    `End getMaintenanceModeInteractor. Time: ${Date.now() - start}ms`,
+  );
   return !!(result && result.current);
 };
