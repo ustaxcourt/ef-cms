5937ac8c4cd543290dc1b02713822f312a88883b
John Cruz <jcruz@flexion.us>
2023-12-06 11:22:14 -0700

    10176: Delete comments;

--- web-api/src/persistence/dynamo/pendingMotion/deleteDocketEntryWorksheetRecord.ts
+++ web-api/src/persistence/dynamo/pendingMotion/deleteDocketEntryWorksheetRecord.ts
@@ -1,13 +1,5 @@
 import { remove } from '../../dynamodbClientService';
 
-/**
- * deleteCaseTrialSortMappingRecords
- *
- * @param {object} providers the providers object
- * @param {object} providers.applicationContext the application context
- * @param {string} providers.docketNumber the docket number of the case to delete the mapping records for
- * @returns {Promise} the return from the persistence delete calls
- */
 export const deleteDocketEntryWorksheetRecord = async ({
   applicationContext,
   docketEntryId,
