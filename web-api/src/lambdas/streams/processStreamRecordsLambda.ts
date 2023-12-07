d9ee4b491399816c8da20523026261d1d4e07454
Jim Lerza <james.lerza@ustaxcourt.gov>
2023-12-07 12:47:17 -0500

    devex-1231: began deprecating stream-event-id, added logging to evaluate solution

--- web-api/src/lambdas/streams/processStreamRecordsLambda.ts
+++ web-api/src/lambdas/streams/processStreamRecordsLambda.ts
@@ -1,86 +1,43 @@
-import {
-  batchGet,
-  getTableName,
-} from '../../persistence/dynamodbClientService';
 import { createApplicationContext } from '../../applicationContext';
 import type { DynamoDBRecord, DynamoDBStreamEvent } from 'aws-lambda';
 
-/**
- *
- */
-async function putEventHistory(
-  applicationContext: IApplicationContext,
-  eventID: string,
-) {
-  const twentyFourHours = 24 * 60 * 60;
-
-  await applicationContext
-    .getDocumentClient({
-      useMasterRegion: true,
-    })
-    .put({
-      Item: {
-        pk: `stream-event-id|${eventID}`,
-        sk: `stream-event-id|${eventID}`,
-        ttl: Math.floor(Date.now() / 1000) + twentyFourHours,
-      },
-      TableName: getTableName({
-        applicationContext,
-      }),
-    })
-    .promise();
-}
-
-/**
- *
- */
-async function getEventHistories(
-  applicationContext: IApplicationContext,
-  records: DynamoDBRecord[],
-) {
-  return (await batchGet({
-    applicationContext,
-    keys: records
-      .map(record => record.eventID)
-      .map(eventID => ({
-        pk: `stream-event-id|${eventID}`,
-        sk: `stream-event-id|${eventID}`,
-      })),
-  })) as { pk: string; sk: string }[];
-}
-
-/**
- * used for processing stream records from persistence
- * @param {object} event the AWS event object
- * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
- */
 export const processStreamRecordsLambda = async (
   event: DynamoDBStreamEvent,
 ) => {
   const applicationContext = createApplicationContext({});
-  const recordsToProcess: DynamoDBRecord[] = [];
+  const deployedTimestamp: number = 1701790000; // Number(process.env.DEPLOYED_TIMESTAMP!);
 
-  const isStreamRecord = record =>
-    record.dynamodb?.Keys?.pk.S?.startsWith('stream-event-id');
-
-  const recordsToCheck = event.Records.filter(
-    record => !isStreamRecord(record),
-  );
-
-  const allEventHistories = await getEventHistories(
-    applicationContext,
-    recordsToCheck,
-  );
-
-  await Promise.all(
-    recordsToCheck.map(record => {
-      const isRecordProcessed = !!allEventHistories.find(
-        history => history.pk === `stream-event-id|${record.eventID}`,
+  const shouldProcessRecord = (record: DynamoDBRecord): boolean => {
+    if (
+      'dynamodb' in record &&
+      record.dynamodb &&
+      'ApproximateCreationDateTime' in record.dynamodb &&
+      typeof record.dynamodb.ApproximateCreationDateTime !== 'undefined'
+    ) {
+      const approximateCreationDateTime: any = record.dynamodb
+        .ApproximateCreationDateTime as any;
+      const streamCreationTime: number =
+        approximateCreationDateTime instanceof Date
+          ? Math.floor(approximateCreationDateTime.getTime() / 1000)
+          : Number(record.dynamodb.ApproximateCreationDateTime);
+      applicationContext.logger.info(
+        `stream event was created at ${record.dynamodb.ApproximateCreationDateTime}`,
+        {
+          ApproximateCreationDateTime:
+            record.dynamodb.ApproximateCreationDateTime,
+          ApproximateCreationSeconds: streamCreationTime,
+          IsDate: approximateCreationDateTime instanceof Date,
+          IsSeconds: streamCreationTime < 1800000000,
+          StreamRecord: record.dynamodb,
+        },
       );
-      if (!isRecordProcessed) {
-        recordsToProcess.push(record);
-      }
-    }),
+      return streamCreationTime >= deployedTimestamp;
+    }
+    return true;
+  };
+
+  const recordsToProcess: DynamoDBRecord[] = event.Records.filter(record =>
+    shouldProcessRecord(record),
   );
 
   if (recordsToProcess.length > 0) {
@@ -89,13 +46,5 @@ export const processStreamRecordsLambda = async (
       .processStreamRecordsInteractor(applicationContext, {
         recordsToProcess,
       });
-
-    await Promise.all(
-      recordsToProcess.map(async record => {
-        const { eventID } = record;
-        if (!eventID) return;
-        await putEventHistory(applicationContext, eventID);
-      }),
-    );
   }
 };
