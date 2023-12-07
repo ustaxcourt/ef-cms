2fdd0d1dd30edb534ef8aa0bc9a670ed9905062a
Zachary Rogers <zrogers>
2023-12-06 22:01:09 -0800

    devex: Convert lambdas to typescript

--- webpack.config.lambda.api.js
+++ webpack.config.lambda.api.js
@@ -3,28 +3,28 @@ const config = require('./webpack.config.lambda');
 module.exports = {
   ...config,
   entry: {
-    api: './web-api/terraform/template/lambdas/api.js',
-    'api-public': './web-api/terraform/template/lambdas/api-public.js',
+    api: './web-api/terraform/template/lambdas/api.ts',
+    'api-public': './web-api/terraform/template/lambdas/api-public.ts',
     'cognito-authorizer':
-      './web-api/terraform/template/lambdas/cognito-authorizer.js',
+      './web-api/terraform/template/lambdas/cognito-authorizer.ts',
     'cognito-triggers':
-      './web-api/terraform/template/lambdas/cognito-triggers.js',
-    cron: './web-api/terraform/template/lambdas/cron.js',
+      './web-api/terraform/template/lambdas/cognito-triggers.ts',
+    cron: './web-api/terraform/template/lambdas/cron.ts',
     'handle-bounced-service-email':
-      './web-api/terraform/template/lambdas/handle-bounced-service-email.js',
+      './web-api/terraform/template/lambdas/handle-bounced-service-email.ts',
     'maintenance-notify':
-      './web-api/terraform/template/lambdas/maintenance-notify.js',
-    'pdf-generation': './web-api/terraform/template/lambdas/pdf-generation.js',
+      './web-api/terraform/template/lambdas/maintenance-notify.ts',
+    'pdf-generation': './web-api/terraform/template/lambdas/pdf-generation.ts',
     'public-api-authorizer':
-      './web-api/terraform/template/lambdas/public-api-authorizer.js',
+      './web-api/terraform/template/lambdas/public-api-authorizer.ts',
     'seal-in-lower-environment':
-      './web-api/terraform/template/lambdas/seal-in-lower-environment.js',
-    'send-emails': './web-api/terraform/template/lambdas/send-emails.js',
-    streams: './web-api/terraform/template/lambdas/streams.js',
-    'trial-session': './web-api/terraform/template/lambdas/trial-session.js',
+      './web-api/terraform/template/lambdas/seal-in-lower-environment.ts',
+    'send-emails': './web-api/terraform/template/lambdas/send-emails.ts',
+    streams: './web-api/terraform/template/lambdas/streams.ts',
+    'trial-session': './web-api/terraform/template/lambdas/trial-session.ts',
     'websocket-authorizer':
-      './web-api/terraform/template/lambdas/websocket-authorizer.js',
-    websockets: './web-api/terraform/template/lambdas/websockets.js',
+      './web-api/terraform/template/lambdas/websocket-authorizer.ts',
+    websockets: './web-api/terraform/template/lambdas/websockets.ts',
   },
   output: {
     clean: true,
