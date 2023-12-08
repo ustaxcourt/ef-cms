3b02a0c7375416324e186cd7ff513356c8aa49f7
Mike Marcotte <mike.marcotte@gmail.com>
2023-12-08 17:15:24 -0500

    10011: wip - use admin-create-user

--- shared/src/business/useCases/users/signUpUserInteractor.ts
+++ shared/src/business/useCases/users/signUpUserInteractor.ts
@@ -1,4 +1,10 @@
 import {
+  AdminCreateUserCommandInput,
+  AdminSetUserPasswordCommandInput,
+  ListUsersCommandInput,
+  ResendConfirmationCodeCommandInput,
+} from '@aws-sdk/client-cognito-identity-provider';
+import {
   type AdminCreateUserResponse,
   UsersListType,
 } from 'aws-sdk/clients/cognitoidentityserviceprovider';
@@ -36,9 +42,19 @@ export const signUpUserInteractor = async (
 
   const newUser = new NewPetitionerUser(user).validate().toRawObject();
 
-  const params = {
-    ClientId: process.env.COGNITO_CLIENT_ID,
-    Password: newUser.password,
+  const result = await signupUser(applicationContext, newUser);
+  // if error, return now instead of attempting the rest...
+  await setPassword(applicationContext, newUser);
+  await sendConfirmationEmail(applicationContext, newUser);
+  return result;
+};
+
+const signupUser = async (
+  applicationContext: IApplicationContext,
+  newUser: NewPetitionerUser,
+) => {
+  const params: AdminCreateUserCommandInput = {
+    MessageAction: 'SUPPRESS',
     UserAttributes: [
       {
         Name: 'email',
@@ -48,18 +64,53 @@ export const signUpUserInteractor = async (
         Name: 'name',
         Value: newUser.name,
       },
+      {
+        Name: 'email_verified',
+        Value: 'false',
+      },
     ],
+    UserPoolId: process.env.USER_POOL_ID,
     Username: newUser.email,
   };
 
-  return await applicationContext.getCognito().signUp(params).promise();
+  return await applicationContext
+    .getCognito()
+    .adminCreateUser(params)
+    .promise();
+};
+
+const setPassword = async (
+  applicationContext: IApplicationContext,
+  newUser: NewPetitionerUser,
+) => {
+  const params: AdminSetUserPasswordCommandInput = {
+    Password: newUser.password,
+    Permanent: true,
+    UserPoolId: process.env.USER_POOLID,
+    Username: newUser.email,
+  };
+  await applicationContext.getCognito().adminSetUserPassword(params).promise();
+};
+
+const sendConfirmationEmail = async (
+  applicationContext: IApplicationContext,
+  newUser: NewPetitionerUser,
+) => {
+  const params: ResendConfirmationCodeCommandInput = {
+    ClientId: process.env.COGNITO_CLIENT_ID,
+    Username: newUser.email,
+  };
+  await applicationContext
+    .getCognito()
+    .resendConfirmationCode(params)
+    .promise();
 };
 
 const checkUserAlreadyExists = async (
   applicationContext: IApplicationContext,
   email: string,
 ): Promise<UsersListType> => {
-  const filters = {
+  const filters: ListUsersCommandInput = {
     AttributesToGet: ['email'],
     Filter: `email = "${email}"`,
     UserPoolId: process.env.USER_POOL_ID,
