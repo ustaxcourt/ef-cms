d1e2accce1a55fbb34e285889a5aba798b55a1ab
Kaitlyn Swann <kswann@flexion.us>
2023-12-11 16:59:08 -0700

    10007 - WIP set expiresAt

--- web-client/src/presenter/actions/setTokenAction.ts
+++ web-client/src/presenter/actions/setTokenAction.ts
@@ -1,3 +1,4 @@
+import { getUTCDate } from '@shared/business/utilities/DateHandler';
 import { state } from '@web-client/presenter/app.cerebral';
 
 export const setTokenAction = ({
@@ -5,16 +6,14 @@ export const setTokenAction = ({
   props,
   store,
 }: ActionProps<{ token: string; refreshToken: string }>): void => {
-  const expiresAt = applicationContext.getUtilities().calculateISODate({
+  const expiresAtIso = applicationContext.getUtilities().calculateISODate({
     dateString: applicationContext.getUtilities().createISODateString(),
     howMuch: 29,
     units: 'days',
   });
 
-  const path = '/';
-  const httpOnly = false; // TODO should be true if not local
-  // window.document.cookie = `refreshToken=${props.refreshToken}; expires=${expires}; path=${path}; secure=true; httpOnly=${httpOnly}`;
-  window.document.cookie = `refreshToken=${props.refreshToken}; path=${path}; expires=${expires};`;
+  const expiresAt = getUTCDate(expiresAtIso);
+  window.document.cookie = `refreshToken=${props.refreshToken}; path=/; expires=${expiresAt};`;
 
   store.set(state.token, props.token);
   store.set(state.refreshToken, props.refreshToken || null);
