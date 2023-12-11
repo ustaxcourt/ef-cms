cbfb13aca3fdf21124548c06901def1f48bf7b16
John Cruz <jcruz@flexion.us>
2023-12-11 10:16:38 -0700

    10185: Exclude those Irs Pract that are not Employed by IRS;

--- web-client/src/presenter/computeds/TrialSession/getAllIrsPractitionersForSelectHelper.ts
+++ web-client/src/presenter/computeds/TrialSession/getAllIrsPractitionersForSelectHelper.ts
@@ -20,6 +20,7 @@ export const getAllIrsPractitionersForSelectHelper = (
   const irsPractitioners = get(state.irsPractitioners);
   const irsPractitionersContactInfo = irsPractitioners
     .filter(irsPrac => irsPrac['admissionsStatus'] === 'Active')
+    .filter(irsPrac => irsPrac['employer'] === 'IRS')
     .map(irsPrac => getFormattedIrsPractitionerInfo(irsPrac));
 
   return {
