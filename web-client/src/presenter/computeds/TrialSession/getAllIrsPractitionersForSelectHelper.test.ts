967c7e553d4201b674e49779bb83af73df92b588
John Cruz <jcruz@flexion.us>
2023-12-11 10:16:38 -0700

    10185: Exclude those Irs Pract that are not Employed by IRS;

--- web-client/src/presenter/computeds/TrialSession/getAllIrsPractitionersForSelectHelper.test.ts
+++ web-client/src/presenter/computeds/TrialSession/getAllIrsPractitionersForSelectHelper.test.ts
@@ -28,6 +28,7 @@ describe('getAllIrsPractitionersForSelectHelper', () => {
         state: 'TEST_STATE',
       },
       email: 'TEST_EMAIL',
+      employer: 'IRS',
       name: 'TEST_NAME',
     });
     const result = runCompute(getAllIrsPractitionersForSelectHelper, {
@@ -63,6 +64,7 @@ describe('getAllIrsPractitionersForSelectHelper', () => {
         state: 'TEST_STATE',
       },
       email: 'TEST_EMAIL',
+      employer: 'IRS',
       name: 'TEST_NAME',
     });
 
@@ -77,6 +79,7 @@ describe('getAllIrsPractitionersForSelectHelper', () => {
         state: 'ACTIVE_TEST_STATE',
       },
       email: 'ACTIVE_TEST_EMAIL',
+      employer: 'IRS',
       name: 'ACTIVE_TEST_NAME',
     });
 
@@ -97,6 +100,59 @@ describe('getAllIrsPractitionersForSelectHelper', () => {
           phone: 'ACTIVE_TEST_PHONE',
           value:
             'ACTIVE_TEST_NAME; ACTIVE_TEST_ADDRESS_1, ACTIVE_TEST_ADDRESS_2; ACTIVE_TEST_CITY, ACTIVE_TEST_STATE ACTIVE_TEST_POSTAL_CODE',
+        },
+      ],
+    });
+  });
+
+  it('should return the formatted irs calendar admin contact info excluding non "IRS" employedusers', () => {
+    TEST_IRS_PRACTITIONERS.push({
+      admissionsStatus: 'Active',
+      contact: {
+        address1: 'TEST_ADDRESS_1',
+        address2: 'TEST_ADDRESS_2',
+        city: 'TEST_CITY',
+        phone: 'TEST_PHONE',
+        postalCode: 'TEST_POSTAL_CODE',
+        state: 'TEST_STATE',
+      },
+      email: 'TEST_EMAIL',
+      employer: 'DOJ',
+      name: 'TEST_NAME',
+    });
+
+    TEST_IRS_PRACTITIONERS.push({
+      admissionsStatus: 'Active',
+      contact: {
+        address1: 'IRS_TEST_ADDRESS_1',
+        address2: 'IRS_TEST_ADDRESS_2',
+        city: 'IRS_TEST_CITY',
+        phone: 'IRS_TEST_PHONE',
+        postalCode: 'IRS_TEST_POSTAL_CODE',
+        state: 'IRS_TEST_STATE',
+      },
+      email: 'IRS_TEST_EMAIL',
+      employer: 'IRS',
+      name: 'IRS_TEST_NAME',
+    });
+
+    const result = runCompute(getAllIrsPractitionersForSelectHelper, {
+      state: {
+        irsPractitioners: TEST_IRS_PRACTITIONERS,
+      },
+    });
+
+    expect(result.irsPractitionersContactInfo.length).toEqual(1);
+    expect(result).toEqual({
+      irsPractitionersContactInfo: [
+        {
+          email: 'IRS_TEST_EMAIL',
+          label:
+            'IRS_TEST_NAME; IRS_TEST_ADDRESS_1, IRS_TEST_ADDRESS_2; IRS_TEST_CITY, IRS_TEST_STATE IRS_TEST_POSTAL_CODE',
+          name: 'IRS_TEST_NAME',
+          phone: 'IRS_TEST_PHONE',
+          value:
+            'IRS_TEST_NAME; IRS_TEST_ADDRESS_1, IRS_TEST_ADDRESS_2; IRS_TEST_CITY, IRS_TEST_STATE IRS_TEST_POSTAL_CODE',
         },
       ],
     });
