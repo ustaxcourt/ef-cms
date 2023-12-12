d030f86efb82833b3444fb095628367589c6601f
John Cruz <jcruz@flexion.us>
2023-12-12 08:41:01 -0700

    10185: Setup the admin info in state correctly using legacy and new properties; added test;

--- web-client/src/presenter/actions/TrialSession/setTrialSessionDetailsOnFormAction.test.ts
+++ web-client/src/presenter/actions/TrialSession/setTrialSessionDetailsOnFormAction.test.ts
@@ -28,6 +28,9 @@ describe('setTrialSessionDetailsOnFormAction', () => {
 
     expect(result.state.form).toEqual({
       estimatedEndDate: '2019-05-01T21:40:46.415Z',
+      irsCalendarAdministratorInfo: {
+        name: undefined,
+      },
       judge: { userId: '456' },
       judgeId: '456',
       sessionType: 'Regular',
@@ -59,12 +62,74 @@ describe('setTrialSessionDetailsOnFormAction', () => {
     expect(result.state.form).toEqual({
       alternateTrialClerkName: 'Iron Man',
       estimatedEndDate: '2019-05-01T21:40:46.415Z',
+      irsCalendarAdministratorInfo: {
+        name: undefined,
+      },
       judge: { userId: '456' },
       judgeId: '456',
       sessionType: 'Regular',
       startDate: '2019-03-01T21:40:46.415Z',
       trialClerkId: 'Other',
       trialSessionId: '123',
+    });
+  });
+
+  it('should set the "irsCalendarAdministratorInfo" property correctly if "irsCalendarAdministratorInfo" doesn\'t exist already', async () => {
+    const result = await runAction(setTrialSessionDetailsOnFormAction, {
+      modules: {
+        presenter,
+      },
+      props: {
+        trialSession: {
+          alternateTrialClerkName: 'Iron Man',
+          estimatedEndDate: '2019-05-01T21:40:46.415Z',
+          irsCalendarAdministrator: 'SOME_TEST_IRS_CALENDAR_ADMINISTRATOR',
+          judge: { userId: '456' },
+          sessionType: 'Regular',
+          startDate: '2019-03-01T21:40:46.415Z',
+          trialSessionId: '123',
+        },
+      },
+      state: { form: {} },
+    });
+
+    expect(result.state.form).toMatchObject({
+      irsCalendarAdministratorInfo: {
+        name: 'SOME_TEST_IRS_CALENDAR_ADMINISTRATOR',
+      },
+    });
+  });
+
+  it('should set the "irsCalendarAdministratorInfo" property correctly if "irsCalendarAdministratorInfo" exist in trial session', async () => {
+    const result = await runAction(setTrialSessionDetailsOnFormAction, {
+      modules: {
+        presenter,
+      },
+      props: {
+        trialSession: {
+          alternateTrialClerkName: 'Iron Man',
+          estimatedEndDate: '2019-05-01T21:40:46.415Z',
+          irsCalendarAdministrator: 'SOME_TEST_IRS_CALENDAR_ADMINISTRATOR',
+          irsCalendarAdministratorInfo: {
+            email: 'TEST_EMAIL',
+            name: 'TEST_NAME',
+            phone: 'TEST_PHONE',
+          },
+          judge: { userId: '456' },
+          sessionType: 'Regular',
+          startDate: '2019-03-01T21:40:46.415Z',
+          trialSessionId: '123',
+        },
+      },
+      state: { form: {} },
+    });
+
+    expect(result.state.form).toMatchObject({
+      irsCalendarAdministratorInfo: {
+        email: 'TEST_EMAIL',
+        name: 'TEST_NAME',
+        phone: 'TEST_PHONE',
+      },
     });
   });
 });
