7991aacdbaf176ceb68e136d40ca986f86e533ab
Mike Marcotte <mike.marcotte@gmail.com>
2023-12-13 22:06:32 -0500

    debug: add logging

--- web-api/src/persistence/messages/sendUpdatePetitionerCasesMessage.ts
+++ web-api/src/persistence/messages/sendUpdatePetitionerCasesMessage.ts
@@ -4,7 +4,15 @@ export const sendUpdatePetitionerCasesMessage = async ({
 }) => {
   const sqs = await applicationContext.getMessagingClient();
   const address = `https://sqs.us-east-1.amazonaws.com/${process.env.AWS_ACCOUNT_ID}/update_petitioner_cases_queue_${process.env.STAGE}_${process.env.CURRENT_COLOR}`;
-  return sqs
+  const result = await sqs
     .sendMessage({ MessageBody: JSON.stringify(user), QueueUrl: address })
     .promise();
+
+  applicationContext.logger.info('sendUpdatePetitionerCasesMessage', {
+    address,
+    result,
+    user,
+  });
+
+  return result;
 };
