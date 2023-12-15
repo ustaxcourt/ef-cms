f12a0f66486f9be07fe261ea91bcd5b387761e26
Mike Marcotte <mike.marcotte@gmail.com>
2023-12-15 10:12:41 -0500

    possible fix: async functions needed to be async and awaited

--- web-api/src/persistence/messages/sendUpdatePetitionerCasesMessage.ts
+++ web-api/src/persistence/messages/sendUpdatePetitionerCasesMessage.ts
@@ -4,7 +4,18 @@ export const sendUpdatePetitionerCasesMessage = async ({
 }) => {
   const sqs = await applicationContext.getMessagingClient();
   const address = `https://sqs.us-east-1.amazonaws.com/${process.env.AWS_ACCOUNT_ID}/update_petitioner_cases_queue_${process.env.STAGE}_${process.env.CURRENT_COLOR}`;
-  return sqs
+
+  applicationContext.logger.info('sendUpdatePetitionerCasesMessage in', {
+    address,
+    user,
+  });
+  const result = await sqs
     .sendMessage({ MessageBody: JSON.stringify(user), QueueUrl: address })
     .promise();
+
+  applicationContext.logger.info('sendUpdatePetitionerCasesMessage out', {
+    result,
+  });
+
+  return result;
 };
