02dcdfb8c23a5f42414721458dbf217ef25ec6cd
Mike Marcotte <mike.marcotte@gmail.com>
2023-12-15 11:25:20 -0500

    bugfix: improve debugging

--- web-api/src/persistence/messages/sendUpdatePetitionerCasesMessage.ts
+++ web-api/src/persistence/messages/sendUpdatePetitionerCasesMessage.ts
@@ -4,17 +4,14 @@ export const sendUpdatePetitionerCasesMessage = async ({
 }) => {
   const sqs = await applicationContext.getMessagingClient();
   const address = `https://sqs.us-east-1.amazonaws.com/${process.env.AWS_ACCOUNT_ID}/update_petitioner_cases_queue_${process.env.STAGE}_${process.env.CURRENT_COLOR}`;
-
-  applicationContext.logger.info('sendUpdatePetitionerCasesMessage in', {
-    address,
-    user,
-  });
   const result = await sqs
     .sendMessage({ MessageBody: JSON.stringify(user), QueueUrl: address })
     .promise();
 
   applicationContext.logger.info('sendUpdatePetitionerCasesMessage out', {
+    address,
     result,
+    user,
   });
 
   return result;
