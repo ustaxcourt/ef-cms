4331d94689fc9a6070ea9795c9540929c382738a
Jason Fu <jasonfu6699@hotmail.com>
2023-12-20 15:47:00 -0800

    dx1219 - move buttons for add correspondence page

--- web-client/src/views/Correspondence/AddCorrespondenceDocument.tsx
+++ web-client/src/views/Correspondence/AddCorrespondenceDocument.tsx
@@ -78,6 +78,28 @@ export const AddCorrespondenceDocument = connect(
                       }}
                     />
                   </FormGroup>
+                  <div className="grid-row grid-gap margin-top-4">
+                    <div className="grid-col-8">
+                      <Button
+                        id="upload-correspondence"
+                        onClick={() => {
+                          uploadCorrespondenceDocumentSequence({
+                            tab: 'correspondence',
+                          });
+                        }}
+                      >
+                        Finish
+                      </Button>
+                      <Button
+                        link
+                        onClick={() => {
+                          formCancelToggleCancelSequence();
+                        }}
+                      >
+                        Cancel
+                      </Button>
+                    </div>
+                  </div>
                 </div>
               </div>
 
@@ -89,29 +111,6 @@ export const AddCorrespondenceDocument = connect(
                     validateUploadCorrespondenceDocumentSequence
                   }
                 />
-              </div>
-            </div>
-
-            <div className="grid-row grid-gap margin-top-4">
-              <div className="grid-col-8">
-                <Button
-                  id="upload-correspondence"
-                  onClick={() => {
-                    uploadCorrespondenceDocumentSequence({
-                      tab: 'correspondence',
-                    });
-                  }}
-                >
-                  Finish
-                </Button>
-                <Button
-                  link
-                  onClick={() => {
-                    formCancelToggleCancelSequence();
-                  }}
-                >
-                  Cancel
-                </Button>
               </div>
             </div>
           </div>
