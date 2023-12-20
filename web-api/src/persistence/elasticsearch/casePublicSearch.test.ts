18cbb0f7db80b9423c781870ee580c18585a50ad
Absolutestunna <joshujah007@gmail.com>
2023-12-20 16:55:28 -0500

    10151 - dod - clean up fetching of public cases by updating the query params in casePublicSearch to not include cases that are sealed.

--- web-api/src/persistence/elasticsearch/casePublicSearch.test.ts
+++ web-api/src/persistence/elasticsearch/casePublicSearch.test.ts
@@ -1,19 +1,100 @@
+import { CasePublicSearchRequestType } from '@shared/business/useCases/public/casePublicSearchInteractor';
 import { applicationContext } from '../../../../shared/src/business/test/createTestApplicationContext';
 import { casePublicSearch } from './casePublicSearch';
 jest.mock('./searchClient');
+import { MAX_SEARCH_RESULTS } from '@shared/business/entities/EntityConstants';
 import { search } from './searchClient';
 
 describe('casePublicSearch', () => {
-  it('returns results from an exact-matches query', async () => {
-    search.mockReturnValue({ results: ['some', 'matches'], total: 0 });
+  const searchTerms: CasePublicSearchRequestType = {
+    petitionerName: 'test person',
+  };
 
-    const results = await casePublicSearch({
+  search.mockReturnValue({ results: ['some', 'matches'], total: 0 });
+
+  const mustClause = [
+    {
+      bool: {
+        should: [
+          {
+            simple_query_string: {
+              boost: 20,
+              default_operator: 'and',
+              fields: ['petitioners.L.M.name.S^4', 'caseCaption.S^0.2'],
+              flags: 'AND|PHRASE|PREFIX',
+              query: '"test person"',
+            },
+          },
+          {
+            simple_query_string: {
+              boost: 0.5,
+              default_operator: 'and',
+              fields: ['petitioners.L.M.name.S^4', 'caseCaption.S^0.2'],
+              flags: 'AND|PHRASE|PREFIX',
+              query: 'test person',
+            },
+          },
+        ],
+      },
+    },
+    {
+      match: {
+        'entityName.S': 'Case',
+      },
+    },
+  ];
+
+  const mustNotClause = [
+    {
+      bool: {
+        should: [
+          {
+            simple_query_string: {
+              boost: 20,
+              default_operator: 'and',
+              fields: ['petitioners.L.M.name.S^4', 'caseCaption.S^0.2'],
+              flags: 'AND|PHRASE|PREFIX',
+              query: '"test person"',
+            },
+          },
+          {
+            simple_query_string: {
+              boost: 0.5,
+              default_operator: 'and',
+              fields: ['petitioners.L.M.name.S^4', 'caseCaption.S^0.2'],
+              flags: 'AND|PHRASE|PREFIX',
+              query: 'test person',
+            },
+          },
+        ],
+      },
+    },
+    {
+      match: {
+        'entityName.S': 'Case',
+      },
+    },
+  ];
+
+  it('returns results from an exact-matches query', async () => {
+    await casePublicSearch({
       applicationContext,
-      searchTerms: 'search for this',
+      searchTerms,
     });
+
+    const expectedQuery = {
+      bool: {
+        must: mustClause,
+        must_not: mustNotClause,
+      },
+    };
     expect(search).toHaveBeenCalledTimes(1);
-    const { searchParameters } = search.mock.calls[0][0];
-    expect(searchParameters.body.query.bool.must_not).toBeDefined();
-    expect(results).toMatchObject(['some', 'matches']);
+    expect(search.mock.calls[0][0].searchParameters.body.size).toEqual(
+      MAX_SEARCH_RESULTS,
+    );
+
+    expect(search.mock.calls[0][0].searchParameters.body.query).toEqual(
+      expectedQuery,
+    );
   });
 });
