86a4f5aedcd86dd0dec9577e2cb1ef3df30b593a
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-12-20 12:01:33 -0800

    check for maintenance mode when redirecting to login

--- web-client/src/presenter/sequences/Login/gotoLoginSequence.ts
+++ web-client/src/presenter/sequences/Login/gotoLoginSequence.ts
@@ -1,5 +1,15 @@
+import { getMaintenanceModeAction } from '@web-client/presenter/actions/Maintenance/getMaintenanceModeAction';
+import { isMaintenanceModeEngagedAction } from '@web-client/presenter/actions/Maintenance/isMaintenanceModeEngagedAction';
+import { navigateToMaintenanceAction } from '@web-client/presenter/actions/navigateToMaintenanceAction';
+import { setMaintenanceModeAction } from '@web-client/presenter/actions/setMaintenanceModeAction';
 import { setupCurrentPageAction } from '../../actions/setupCurrentPageAction';
 
 export const gotoLoginSequence = [
-  setupCurrentPageAction('Login'),
+  getMaintenanceModeAction,
+  setMaintenanceModeAction,
+  isMaintenanceModeEngagedAction,
+  {
+    maintenanceModeOff: [setupCurrentPageAction('Login')],
+    maintenanceModeOn: [navigateToMaintenanceAction],
+  },
 ] as unknown as () => void;
