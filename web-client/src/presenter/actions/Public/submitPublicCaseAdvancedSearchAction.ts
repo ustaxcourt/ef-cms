cfe9c0bcdfc013a92cef13c225bbd7969f8ba8c9
Absolutestunna <joshujah007@gmail.com>
2023-12-21 14:04:43 -0500

    10151 - corrected data transfer of search results for public user.

--- web-client/src/presenter/actions/Public/submitPublicCaseAdvancedSearchAction.ts
+++ web-client/src/presenter/actions/Public/submitPublicCaseAdvancedSearchAction.ts
@@ -1,25 +1,21 @@
+import { CasePublicSearchResultsType } from '@web-api/persistence/elasticsearch/casePublicSearch';
 import { ClientPublicApplicationContext } from '@web-client/applicationContextPublic';
 import { prepareFormDataForCaseSearchApi } from '@web-client/presenter/actions/AdvancedSearch/submitCaseAdvancedSearchAction';
 import { state } from '@web-client/presenter/app-public.cerebral';
 
-/**
- * submit public case advanced search form
- * @param {object} providers the providers object
- * @param {object} providers.applicationContext the application context
- * @param {Function} providers.get the cerebral get function
- * @returns {Promise} async action
- */
 export const submitPublicCaseAdvancedSearchAction = async ({
   applicationContext,
   get,
-}: ActionProps<{}, ClientPublicApplicationContext>) => {
+}: ActionProps<{}, ClientPublicApplicationContext>): Promise<{
+  searchResults: CasePublicSearchResultsType;
+}> => {
   const form = get(state.advancedSearchForm.caseSearchByName);
 
-  const searchResults = await applicationContext
+  const { results } = await applicationContext
     .getUseCases()
     .casePublicSearchInteractor(applicationContext, {
       searchParams: prepareFormDataForCaseSearchApi(form),
     });
 
-  return { searchResults };
+  return { searchResults: results };
 };
