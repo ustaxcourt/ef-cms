45452a5c95924eda4ac87f97861ec57c3184688e
Absolutestunna <joshujah007@gmail.com>
2023-12-22 12:06:15 -0500

    10205 - reduce wait time in selectTypeaheadInput per feedback

--- cypress/helpers/select-typeahead-input.ts
+++ cypress/helpers/select-typeahead-input.ts
@@ -1,6 +1,6 @@
 export function selectTypeaheadInput(testId: string, value: string) {
   // eslint-disable-next-line cypress/no-unnecessary-waiting
-  cy.wait(1000); // TODO: we shouldn't need this wait, but the app fails to type the document type without it
+  cy.wait(100); // app fails without a wait
   cy.get(`[data-testid="${testId}"] .select-react-element__input`).type(
     `${value}{enter}`,
   );
