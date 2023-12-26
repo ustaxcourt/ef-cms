d66cb844505f6ecc8f219f5c27ca24e0b6b31e27
Nechama Krigsman <nkrigsman@flexion.us>
2023-12-25 20:44:09 -0500

    10214: load CAV report data on initial page load

--- web-client/src/presenter/sequences/JudgeActivityReport/gotoJudgeActivityReportSequence.ts
+++ web-client/src/presenter/sequences/JudgeActivityReport/gotoJudgeActivityReportSequence.ts
@@ -1,11 +1,13 @@
 import { clearErrorAlertsAction } from '../../actions/clearErrorAlertsAction';
 import { clearScreenMetadataAction } from '../../actions/clearScreenMetadataAction';
 import { closeMobileMenuAction } from '../../actions/closeMobileMenuAction';
+import { getSubmittedAndCavCasesByJudgeAction } from '@web-client/presenter/actions/JudgeActivityReport/getSubmittedAndCavCasesByJudgeAction';
 import { getUsersInSectionAction } from '../../actions/getUsersInSectionAction';
 import { isLoggedInAction } from '../../actions/isLoggedInAction';
 import { redirectToCognitoAction } from '../../actions/redirectToCognitoAction';
 import { resetJudgeActivityReportStateAction } from '../../actions/resetJudgeActivityReportStateAction';
 import { setAllAndCurrentJudgesAction } from '../../actions/setAllAndCurrentJudgesAction';
+import { setCavAndSubmittedCasesAction } from '@web-client/presenter/actions/JudgeActivityReport/setCavAndSubmittedCasesAction';
 import { setJudgeLastNameOnJudgeActivityReportAction } from '../../actions/JudgeActivityReport/setJudgeLastNameOnJudgeActivityReportAction';
 import { setupCurrentPageAction } from '../../actions/setupCurrentPageAction';
 import { startWebSocketConnectionSequenceDecorator } from '../../utilities/startWebSocketConnectionSequenceDecorator';
@@ -22,6 +24,8 @@ const gotoJudgeActivityReport = [
   getUsersInSectionAction({ section: 'judge' }),
   setAllAndCurrentJudgesAction,
   setupCurrentPageAction('JudgeActivityReport'),
+  getSubmittedAndCavCasesByJudgeAction,
+  setCavAndSubmittedCasesAction,
 ];
 
 export const gotoJudgeActivityReportSequence = [
