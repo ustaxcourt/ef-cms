45ef5899bbac0669a07e44adfd88ba077be933b0
Mike Marcotte <mike.marcotte@gmail.com>
2023-12-28 13:31:06 -0500

    10176: supply elasticsearch_domain to migration lambdas

--- web-api/workflow-terraform/migration/bin/deploy-init.sh
+++ web-api/workflow-terraform/migration/bin/deploy-init.sh
@@ -21,11 +21,15 @@ export ENVIRONMENT="${ENVIRONMENT}"
 
 STREAM_ARN=$(aws dynamodbstreams list-streams --region us-east-1 --query "Streams[?TableName=='${SOURCE_TABLE}'].StreamArn | [0]" --output text)
 
+DESTINATION_TABLE_VERSION=$(aws dynamodb get-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --key '{"pk":{"S":"destination-table-version"},"sk":{"S":"destination-table-version"}}' | jq -r ".Item.current.S")
+ELASTICSEARCH_ENDPOINT=$(aws es describe-elasticsearch-domain --domain-name efcms-search-test-"${DESTINATION_TABLE_VERSION}" --output json | jq -r .DomainStatus.Endpoint)
+
 export TF_VAR_destination_table=$DESTINATION_TABLE
 export TF_VAR_dns_domain=$EFCMS_DOMAIN
 export TF_VAR_documents_bucket_name=$DOCUMENTS_BUCKET_NAME
 export TF_VAR_environment=$ENVIRONMENT
 export TF_VAR_source_table=$SOURCE_TABLE
 export TF_VAR_stream_arn=$STREAM_ARN
+export TF_VAR_elasticsearch_domain=$ELASTICSEARCH_ENDPOINT
 
 ../../../../shared/terraform/bin/init.sh migration --build-lambda
