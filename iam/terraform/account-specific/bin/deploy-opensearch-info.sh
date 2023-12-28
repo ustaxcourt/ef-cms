e4cf81923423f9044a02d16b921d4e49f9690991
Zachary Rogers <zrogers>
2023-12-28 07:17:46 -0800

    dep-updates: update docs. Add temp opensearch deploy

---
+++ iam/terraform/account-specific/bin/deploy-opensearch-info.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+# shellcheck disable=SC1091 # this file does exist, we promise
+source ../bin/deploy-init.sh
+
+# this hasnt been tested yet
+terraform state rm aws_elasticsearch_domain.efcms-logs
+terraform import aws_opensearch_domain.efcms-logs info
+terraform plan
