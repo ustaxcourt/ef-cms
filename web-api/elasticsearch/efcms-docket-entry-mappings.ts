2a36a5f343386e841fb25d8d020bee137ba25163
Mike Marcotte <mike.marcotte@gmail.com>
2023-12-29 14:19:50 -0500

    update mappings for items to reindex to help get story over the line

--- web-api/elasticsearch/efcms-docket-entry-mappings.ts
+++ web-api/elasticsearch/efcms-docket-entry-mappings.ts
@@ -3,6 +3,11 @@ import { createHash } from 'crypto';
 export const efcmsDocketEntryMappings = {
   properties: {
     'associatedJudge.S': {
+      fields: {
+        raw: {
+          type: 'keyword',
+        },
+      },
       type: 'text',
     },
     case_relations: {
