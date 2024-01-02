d212d65556b3ba4d12c7f2755c1b18b11c81d769
Zachary Rogers <zrogers>
2024-01-01 20:21:35 -0800

    10007: Add error handling for logging in

--- web-client/src/presenter/actions/Login/submitLoginAction.ts
+++ web-client/src/presenter/actions/Login/submitLoginAction.ts
@@ -3,6 +3,7 @@ import { state } from '@web-client/presenter/app.cerebral';
 export const submitLoginAction = async ({
   applicationContext,
   get,
+  path,
 }: ActionProps): Promise<{
   accessToken: string;
   idToken: string;
@@ -10,13 +11,27 @@ export const submitLoginAction = async ({
 }> => {
   const { email, password } = get(state.form);
 
-  const { accessToken, idToken, refreshToken } = await applicationContext
-    .getUseCases()
-    .loginInteractor(applicationContext, { email, password });
+  try {
+    const { accessToken, idToken, refreshToken } = await applicationContext
+      .getUseCases()
+      .loginInteractor(applicationContext, { email, password });
 
-  return {
-    accessToken,
-    idToken,
-    refreshToken,
-  };
+    return path.success({ accessToken, idToken, refreshToken });
+  } catch (err: any) {
+    if (err.responseCode === 401) {
+      return path.error({
+        alertError: {
+          message: 'The email address or password you entered is invalid.',
+          title: 'Please correct the following errors:',
+        },
+      });
+    }
+
+    return path.error({
+      alertError: {
+        title:
+          'There was an unexpected error when logging in. Please try again.',
+      },
+    });
+  }
 };
--- web-client/src/presenter/sequences/Login/submitLoginSequence.ts
+++ web-client/src/presenter/sequences/Login/submitLoginSequence.ts
@@ -2,6 +2,7 @@ import { clearFormAction } from '@web-client/presenter/actions/clearFormAction';
 import { decodeTokenAction } from '@web-client/presenter/actions/decodeTokenAction';
 import { getUserAction } from '@web-client/presenter/actions/getUserAction';
 import { navigateToPathAction } from '@web-client/presenter/actions/navigateToPathAction';
+import { setAlertErrorAction } from '@web-client/presenter/actions/setAlertErrorAction';
 import { setTokenAction } from '@web-client/presenter/actions/Login/setTokenAction';
 import { setUserAction } from '@web-client/presenter/actions/setUserAction';
 import { setUserPermissionsAction } from '@web-client/presenter/actions/setUserPermissionsAction';
@@ -9,11 +10,16 @@ import { submitLoginAction } from '@web-client/presenter/actions/Login/submitLog
 
 export const submitLoginSequence = [
   submitLoginAction,
-  clearFormAction,
-  decodeTokenAction,
-  setTokenAction,
-  getUserAction,
-  setUserAction,
-  setUserPermissionsAction,
-  navigateToPathAction,
+  {
+    error: [setAlertErrorAction],
+    success: [
+      clearFormAction,
+      decodeTokenAction,
+      setTokenAction,
+      getUserAction,
+      setUserAction,
+      setUserPermissionsAction,
+      navigateToPathAction,
+    ],
+  },
 ] as unknown as () => void;
