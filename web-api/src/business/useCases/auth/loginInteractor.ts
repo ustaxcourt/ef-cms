9f5974425318775305987197c046d7c5876423ca
Zachary Rogers <zrogers>
2024-01-01 20:21:35 -0800

    10007: Add error handling for logging in

--- web-api/src/business/useCases/auth/loginInteractor.ts
+++ web-api/src/business/useCases/auth/loginInteractor.ts
@@ -1,4 +1,5 @@
 import { ServerApplicationContext } from '@web-api/applicationContext';
+import { UnknownUserError } from '@web-api/errors/errors';
 
 export const loginInteractor = async (
   applicationContext: ServerApplicationContext,
@@ -6,20 +7,28 @@ export const loginInteractor = async (
 ): Promise<{ idToken: string; accessToken: string; refreshToken: string }> => {
   const cognito = applicationContext.getCognito();
 
-  const result = await cognito
-    .initiateAuth({
-      AuthFlow: 'USER_PASSWORD_AUTH',
-      AuthParameters: {
-        PASSWORD: password,
-        USERNAME: email,
-      },
-      ClientId: applicationContext.environment.cognitoClientId,
-    })
-    .promise(); // TODO 10007: Update cognito to v3
+  try {
+    const result = await cognito
+      .initiateAuth({
+        AuthFlow: 'USER_PASSWORD_AUTH',
+        AuthParameters: {
+          PASSWORD: password,
+          USERNAME: email,
+        },
+        ClientId: applicationContext.environment.cognitoClientId,
+      })
+      .promise(); // TODO 10007: Update cognito to v3
 
-  return {
-    accessToken: result.AuthenticationResult!.AccessToken!,
-    idToken: result.AuthenticationResult!.IdToken!,
-    refreshToken: result.AuthenticationResult!.RefreshToken!,
-  };
+    return {
+      accessToken: result.AuthenticationResult!.AccessToken!,
+      idToken: result.AuthenticationResult!.IdToken!,
+      refreshToken: result.AuthenticationResult!.RefreshToken!,
+    };
+  } catch (err: any) {
+    // AWS Cognito InvalidPasswordException
+    if (err.code === 'InvalidPasswordException') {
+      throw new UnknownUserError('Invalid Username or Password');
+    }
+    throw err;
+  }
 };
