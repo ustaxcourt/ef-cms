4bf1112e708d7d847bf6ccb61b46f7a927906ac7
Cody Seibert <cseibert113@gmail.com>
2024-01-02 17:15:42 -0500

    verify the text is updated before submitting the modal

--- cypress/cypress-integration/integration/judge-activity-report.cy.ts
+++ cypress/cypress-integration/integration/judge-activity-report.cy.ts
@@ -114,7 +114,12 @@ describe('Verify the activity report', () => {
 
         cy.get('[data-testid="case-detail-menu-button"]').click();
         cy.get('[data-testid="menu-button-create-order"]').click();
-        cy.get('[data-testid="event-code-select"]').select('OAD');
+        cy.get('[data-testid="event-code-select"]').select(
+          'Order and Decision',
+        );
+        cy.get('[data-testid="event-code-select"]')
+          .find('option:selected')
+          .should('have.text', 'Order and Decision');
         cy.get('[data-testid="modal-button-confirm"]').click();
         cy.get('.ql-editor').click();
         cy.get('[data-testid="save-order-button"]').click();
@@ -154,7 +159,12 @@ describe('Verify the activity report', () => {
 
         cy.get('[data-testid="case-detail-menu-button"]').click();
         cy.get('[data-testid="menu-button-create-order"]').click();
-        cy.get('[data-testid="event-code-select"]').select('OAD');
+        cy.get('[data-testid="event-code-select"]').select(
+          'Order and Decision',
+        );
+        cy.get('[data-testid="event-code-select"]')
+          .find('option:selected')
+          .should('have.text', 'Order and Decision');
         cy.get('[data-testid="modal-button-confirm"]').click();
         cy.get('.ql-editor').click();
         cy.get('[data-testid="save-order-button"]').click();
