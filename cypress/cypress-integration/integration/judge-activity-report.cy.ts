a7e445dc644476b3b5067492588a600401bf7dbb
Cody Seibert <cseibert113@gmail.com>
2024-01-03 11:15:09 -0500

    10214 - fixing issue with orders opening new tabs which causes cypress to hang

--- cypress/cypress-integration/integration/judge-activity-report.cy.ts
+++ cypress/cypress-integration/integration/judge-activity-report.cy.ts
@@ -114,12 +114,7 @@ describe('Verify the activity report', () => {
 
         cy.get('[data-testid="case-detail-menu-button"]').click();
         cy.get('[data-testid="menu-button-create-order"]').click();
-        cy.get('[data-testid="event-code-select"]').select(
-          'Order and Decision',
-        );
-        cy.get('[data-testid="event-code-select"]')
-          .find('option:selected')
-          .should('have.text', 'Order and Decision');
+        cy.get('[data-testid="event-code-select"]').select('OAD');
         cy.get('[data-testid="modal-button-confirm"]').click();
         cy.get('.ql-editor').click();
         cy.get('[data-testid="save-order-button"]').click();
@@ -159,13 +154,9 @@ describe('Verify the activity report', () => {
 
         cy.get('[data-testid="case-detail-menu-button"]').click();
         cy.get('[data-testid="menu-button-create-order"]').click();
-        cy.get('[data-testid="event-code-select"]').select(
-          'Order and Decision',
-        );
-        cy.get('[data-testid="event-code-select"]')
-          .find('option:selected')
-          .should('have.text', 'Order and Decision');
+        cy.get('[data-testid="event-code-select"]').select('OAD');
         cy.get('[data-testid="modal-button-confirm"]').click();
+
         cy.get('.ql-editor').click();
         cy.get('[data-testid="save-order-button"]').click();
         cy.get('[data-testid="sign-pdf-canvas"]').click();
