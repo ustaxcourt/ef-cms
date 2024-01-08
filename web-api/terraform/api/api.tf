33c606d01d130aaf0b1e04150b7a68b8b89baafb
Zachary Rogers <zrogers>
2024-01-08 13:26:30 -0800

    10007: update resource

--- web-api/terraform/api/api.tf
+++ web-api/terraform/api/api.tf
@@ -150,7 +150,7 @@ resource "aws_api_gateway_resource" "api_maintenance_mode_resource" {
 
 resource "aws_api_gateway_method" "api_maintenance_mode_method_any" {
   rest_api_id   = aws_api_gateway_rest_api.gateway_for_api.id
-  resource_id   = aws_api_gateway_resource.api_auth_resource.id
+  resource_id   = aws_api_gateway_resource.api_maintenance_mode_resource.id
   http_method   = "ANY"
   authorization = "NONE"
 }
@@ -160,7 +160,7 @@ resource "aws_api_gateway_integration" "api_integration_maintenance_mode" {
     aws_api_gateway_integration.api_integration_options
   ]
   rest_api_id = aws_api_gateway_rest_api.gateway_for_api.id
-  resource_id = aws_api_gateway_method.api_maintenance_mode_resource.resource_id
+  resource_id = aws_api_gateway_resource.api_maintenance_mode_resource.id
   http_method = aws_api_gateway_method.api_maintenance_mode_method_any.http_method
 
   integration_http_method = "POST"
