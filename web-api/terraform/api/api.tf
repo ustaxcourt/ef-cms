d002212f939c847c710fd59ff7baa7ef3dfa64f1
Zachary Rogers <zrogers>
2024-01-08 13:15:53 -0800

    10007: Remove attempt to passthrough authorizer for certain routes, deploy infrastructure for maintenance-mode so it does not go through authorizer

--- web-api/terraform/api/api.tf
+++ web-api/terraform/api/api.tf
@@ -141,6 +141,34 @@ resource "aws_api_gateway_method" "api_method_head" {
   authorizer_id = aws_api_gateway_authorizer.custom_authorizer.id
 }
 
+# 
+resource "aws_api_gateway_resource" "api_maintenance_mode_resource" {
+  rest_api_id = aws_api_gateway_rest_api.gateway_for_api.id
+  parent_id   = aws_api_gateway_rest_api.gateway_for_api.root_resource_id
+  path_part   = "maintenance-mode"
+}
+
+resource "aws_api_gateway_method" "api_maintenance_mode_method_any" {
+  rest_api_id   = aws_api_gateway_rest_api.gateway_for_api.id
+  resource_id   = aws_api_gateway_resource.api_auth_resource.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "api_integration_maintenance_mode" {
+  depends_on = [
+    aws_api_gateway_integration.api_integration_options
+  ]
+  rest_api_id = aws_api_gateway_rest_api.gateway_for_api.id
+  resource_id = aws_api_gateway_method.api_maintenance_mode_resource.resource_id
+  http_method = aws_api_gateway_method.api_maintenance_mode_method_any.http_method
+
+  integration_http_method = "ANY"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.api_lambda.invoke_arn
+}
+# 
+
 resource "aws_api_gateway_authorizer" "custom_authorizer" {
   name                   = "custom_authorizer_${var.environment}_${var.current_color}"
   rest_api_id            = aws_api_gateway_rest_api.gateway_for_api.id
