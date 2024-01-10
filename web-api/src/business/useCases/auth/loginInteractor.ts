2302cc3b2e31b7ed7c883bf4dac3145dfa37ca91
Kaitlyn Swann <kswann@flexion.us>
2024-01-10 12:00:20 -0700

    10007: Create new utility function for sending an email to a user

--- web-api/src/business/useCases/auth/loginInteractor.ts
+++ web-api/src/business/useCases/auth/loginInteractor.ts
@@ -1,6 +1,6 @@
 import { ServerApplicationContext } from '@web-api/applicationContext';
 import { UnknownUserError } from '@web-api/errors/errors';
-import { sendAccountCreationConfirmation } from '@web-api/business/useCases/auth/signUpUserInteractor';
+import qs from 'qs';
 
 export const loginInteractor = async (
   applicationContext: ServerApplicationContext,
@@ -31,45 +31,7 @@ export const loginInteractor = async (
     }
 
     if (err.name === 'UserNotConfirmedException') {
-      const cognito = applicationContext.getCognito();
-      const users = await cognito.listUsers({
-        AttributesToGet: ['sub'],
-        Filter: `email = "${email}"`,
-        UserPoolId: process.env.USER_POOL_ID,
-      });
-
-      console.log(
-        '*** user',
-        users.Users?.[0],
-        JSON.stringify(users.Users?.[0].Attributes?.[0].Value),
-      );
-
-      const userId = users.Users?.[0].Attributes?.[0].Value!;
-
-      const accountConfirmationRecord = await applicationContext
-        .getPersistenceGateway()
-        .getAccountConfirmationCode(applicationContext, { userId });
-
-      console.log('*** accountConfirmationRecord', accountConfirmationRecord);
-
-      let newConfirmationCode = accountConfirmationRecord.confirmationCode;
-
-      // if doesn't exist regenerate new confirmation code
-      if (!newConfirmationCode) {
-        console.log('*** generate new confirmation code');
-        const { confirmationCode } = await applicationContext
-          .getPersistenceGateway()
-          .generateAccountConfirmationCode(applicationContext, { userId });
-        newConfirmationCode = confirmationCode;
-      }
-
-      // Assume exists and resend confirmation email
-      console.log('*** sending confirmation cod email');
-      await sendAccountCreationConfirmation(applicationContext, {
-        confirmationCode: newConfirmationCode!,
-        email,
-        userId,
-      });
+      await resendAccountConfirmation(applicationContext, email);
 
       throw new Error('User is unconfirmed');
     }
@@ -77,3 +39,77 @@ export const loginInteractor = async (
     throw err;
   }
 };
+
+async function resendAccountConfirmation(
+  applicationContext: ServerApplicationContext,
+  email: string,
+): Promise<string> {
+  const cognito = applicationContext.getCognito();
+
+  const users = await cognito.listUsers({
+    AttributesToGet: ['sub'],
+    Filter: `email = "${email}"`,
+    UserPoolId: process.env.USER_POOL_ID,
+  });
+  console.log(
+    '*** user',
+    users.Users?.[0],
+    JSON.stringify(users.Users?.[0].Attributes?.[0].Value),
+  );
+
+  const userId = users.Users?.[0].Attributes?.[0].Value!;
+
+  const accountConfirmationRecord = await applicationContext
+    .getPersistenceGateway()
+    .getAccountConfirmationCode(applicationContext, { userId });
+
+  console.log('*** accountConfirmationRecord', accountConfirmationRecord);
+
+  let newConfirmationCode = accountConfirmationRecord.confirmationCode;
+
+  // if doesn't exist regenerate new confirmation code
+  if (!newConfirmationCode) {
+    console.log('*** generate new confirmation code');
+    const { confirmationCode } = await applicationContext
+      .getPersistenceGateway()
+      .generateAccountConfirmationCode(applicationContext, { userId });
+    newConfirmationCode = confirmationCode;
+  }
+
+  // Assume exists and resend confirmation email
+  console.log('*** sending confirmation cod email');
+
+  const queryString = qs.stringify(
+    { confirmationCode: newConfirmationCode, userId },
+    { encode: false },
+  );
+
+  const verificationLink = `https://app.${process.env.EFCMS_DOMAIN}/confirm-signup?${queryString}`;
+
+  const emailBody =
+    'Welcome to DAWSON! Your account with DAWSON has been created. Use the' +
+    ' button below to verify your email address.' +
+    `<button style="font-family: Source Sans Pro Web,Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;
+        font-size: 1.06rem;
+        line-height: .9;
+        color: #fff;
+        background-color: #005ea2;
+        border: 0;
+        border-radius: 0.25rem;
+        cursor: pointer;
+        display: inline-block;
+        margin-right: 0.5rem;
+        padding: .75rem 2.25rem;
+        text-align: center;
+        text-decoration: none;"><a href="${verificationLink}">Verify Email</a></button>` +
+    '<br><br><br>If you did not create an account with DAWSON, please contact support at ' +
+    '<a href="mailto:dawson.support@ustaxcourt.gov">dawson.support@ustaxcourt.gov</a>.';
+
+  return await applicationContext
+    .getMessageGateway()
+    .sendEmailToUser(applicationContext, {
+      body: emailBody,
+      subject: 'U.S. Tax Court DAWSON Account Verification',
+      to: email,
+    });
+}
