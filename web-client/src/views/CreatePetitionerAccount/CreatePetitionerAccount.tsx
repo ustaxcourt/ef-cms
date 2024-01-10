1531592f292df885d64fd949746b996dfad2296c
Zachary Rogers <zrogers>
2024-01-10 13:40:42 -0800

    10007: re-add navigate to create account sequence to allow navigation from login page; fix styling on verification sent page

--- web-client/src/views/CreatePetitionerAccount/CreatePetitionerAccount.tsx
+++ web-client/src/views/CreatePetitionerAccount/CreatePetitionerAccount.tsx
@@ -12,24 +12,22 @@ export const CreatePetitionerAccount = connect(
   ({ alertError }) => {
     return (
       <>
-        <div className="padding-y-5 padding-x-5 display-flex flex-justify-center">
-          <div className="grid-container grid-gap-lg padding-x-4">
-            {alertError && (
-              <div
-                className="grid-row margin-bottom-2"
-                style={{ width: 'fit-content' }}
-              >
-                <MessageAlert
-                  alertType={alertError.alertType}
-                  message={alertError.message}
-                  title={alertError.title}
-                ></MessageAlert>
-              </div>
-            )}
-            <div className="grid-row bg-white padding-x-5 padding-y-4 create-petitioner-account">
-              <CreatePetitionerAccountForm />
-              <CreatePetitionerAccountInfo />
+        <div className="grid-container grid-gap-lg padding-y-5 display-flex flex-justify-center">
+          {alertError && (
+            <div
+              className="grid-row margin-bottom-2"
+              style={{ width: 'fit-content' }}
+            >
+              <MessageAlert
+                alertType={alertError.alertType}
+                message={alertError.message}
+                title={alertError.title}
+              ></MessageAlert>
             </div>
+          )}
+          <div className="grid-row bg-white padding-x-5 padding-y-4 create-petitioner-account">
+            <CreatePetitionerAccountForm />
+            <CreatePetitionerAccountInfo />
           </div>
         </div>
       </>
