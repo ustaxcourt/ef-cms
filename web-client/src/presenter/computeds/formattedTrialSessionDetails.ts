46be3b31e6d8ca76e567d1bc7cbc03b5f0ce894c
Absolutestunna <joshujah007@gmail.com>
2024-01-11 15:57:12 -0500

    10202 - correct test for formattedTrialSessionDetails

--- web-client/src/presenter/computeds/formattedTrialSessionDetails.ts
+++ web-client/src/presenter/computeds/formattedTrialSessionDetails.ts
@@ -43,7 +43,7 @@ export const formattedTrialSessionDetails = (
     .getUtilities()
     .getFormattedTrialSessionDetails({
       applicationContext,
-      trialSession: get(state.trialSession),
+      trialSession,
     });
 
   const {
