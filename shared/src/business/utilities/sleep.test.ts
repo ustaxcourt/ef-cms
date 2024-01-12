fa882f475aeb161718fd012b587307561e896b2b
Zachary Rogers <zrogers>
2024-01-11 20:27:24 -0800

    devex: Fix all floating promises. Delete unused scripts files.

--- shared/src/business/utilities/sleep.test.ts
+++ shared/src/business/utilities/sleep.test.ts
@@ -5,7 +5,7 @@ import { sleep } from './sleep';
 describe('sleep', () => {
   it('waits the specified amount of time before proceeding', async () => {
     const waitMs = 50;
-    Promise.resolve().then(() => jest.advanceTimersByTime(100));
+    await Promise.resolve().then(() => jest.advanceTimersByTime(100));
     await sleep(waitMs);
     expect(setTimeout).toHaveBeenCalledTimes(1);
     expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 50);
