6cb17134ffc5ccf4b6d4a9a49f6c7268b43a9aed
Zachary Rogers <zrogers>
2024-01-11 21:10:07 -0800

    devex: fix sleep teest to allow floating promise

--- shared/src/business/utilities/sleep.test.ts
+++ shared/src/business/utilities/sleep.test.ts
@@ -5,7 +5,8 @@ import { sleep } from './sleep';
 describe('sleep', () => {
   it('waits the specified amount of time before proceeding', async () => {
     const waitMs = 50;
-    await Promise.resolve().then(() => jest.advanceTimersByTime(100));
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    Promise.resolve().then(() => jest.advanceTimersByTime(100));
     await sleep(waitMs);
     expect(setTimeout).toHaveBeenCalledTimes(1);
     expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 50);
