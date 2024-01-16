a21be52dc9a6faaf9f6e468ff49b2ed075c253a9
Zachary Rogers <zrogers>
2024-01-16 10:19:18 -0800

    10007: Remove check of maintenance mode in sequence. Rely on init app

--- web-client/src/presenter/sequences/Login/gotoLoginSequence.ts
+++ web-client/src/presenter/sequences/Login/gotoLoginSequence.ts
@@ -1,15 +1,5 @@
-import { getMaintenanceModeAction } from '@web-client/presenter/actions/Maintenance/getMaintenanceModeAction';
-import { isMaintenanceModeEngagedAction } from '@web-client/presenter/actions/Maintenance/isMaintenanceModeEngagedAction';
-import { navigateToMaintenanceAction } from '@web-client/presenter/actions/navigateToMaintenanceAction';
-import { setMaintenanceModeAction } from '@web-client/presenter/actions/setMaintenanceModeAction';
 import { setupCurrentPageAction } from '../../actions/setupCurrentPageAction';
 
 export const gotoLoginSequence = [
-  getMaintenanceModeAction,
-  setMaintenanceModeAction,
-  isMaintenanceModeEngagedAction,
-  {
-    maintenanceModeOff: [setupCurrentPageAction('Login')],
-    maintenanceModeOn: [navigateToMaintenanceAction],
-  },
+  setupCurrentPageAction('Login'),
 ] as unknown as () => void;
