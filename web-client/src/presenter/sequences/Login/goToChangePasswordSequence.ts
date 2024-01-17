7c431b3ba0b0b2d7625cceccec5c00b62e83d2bc
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-01-16 22:04:30 -0600

    10007: fix validation on change password page

--- web-client/src/presenter/sequences/Login/goToChangePasswordSequence.ts
+++ web-client/src/presenter/sequences/Login/goToChangePasswordSequence.ts
@@ -1,7 +1,9 @@
+import { clearFormAction } from '@web-client/presenter/actions/clearFormAction';
 import { setupChangePasswordFormAction } from '@web-client/presenter/actions/setupChangePasswordFormAction';
 import { setupCurrentPageAction } from '../../actions/setupCurrentPageAction';
 
 export const goToChangePasswordSequence = [
+  clearFormAction,
   setupChangePasswordFormAction,
   setupCurrentPageAction('ChangePassword'),
 ] as unknown as () => void;
