0f81080826528cd1afa648e420d315762076d232
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-01-18 16:47:13 -0600

    10007: fix validateNewPetitionerAction, fixing pa11y

--- web-client/src/presenter/actions/CreateAccount/validateNewPetitionerAction.ts
+++ web-client/src/presenter/actions/CreateAccount/validateNewPetitionerAction.ts
@@ -1,10 +1,11 @@
 import { NewPetitionerUser } from '@shared/business/entities/NewPetitionerUser';
+import { state } from '@web-client/presenter/app.cerebral';
 
 export const validateNewPetitionerAction = ({ get, path }) => {
   const petitionerUserForm = get(state.form);
 
   const petitionerUser = new NewPetitionerUser(petitionerUserForm);
-
+  console.log('1111111');
   if (!petitionerUser.isValid()) {
     return path.invalid();
   }
