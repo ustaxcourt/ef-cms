55a63402cb77d02ea3145b4b1f3cb49b37200d3e
Absolutestunna <joshujah007@gmail.com>
2024-01-18 13:12:58 -0500

    10218 - fix property name in createCaseFromPaperInteractor proxy

--- shared/src/proxies/createCaseFromPaperProxy.ts
+++ shared/src/proxies/createCaseFromPaperProxy.ts
@@ -1,22 +1,10 @@
 import { post } from './requests';
 
-/**
- * createCaseProxy
- *
- * @param {object} providers the providers object
- * @param {object} providers.applicationContext the application context
- * @param {string} providers.corporateDisclosureFileId the id of the corporate disclosure file
- * @param {string} providers.petitionFileId the id of the petition file
- * @param {string} providers.petitionMetadata the petition metadata
- * @param {string} providers.requestForPlaceOfTrialFileId the id of the request for place of trial file
- * @param {string} providers.stinFileId the id of the stin file
- * @returns {Promise<*>} the promise of the api call
- */
 export const createCaseFromPaperInteractor = (
   applicationContext,
   {
     applicationForWaiverOfFilingFeeFileId,
-    atpFileId,
+    attachmentToPetitionFileId,
     corporateDisclosureFileId,
     petitionFileId,
     petitionMetadata,
@@ -24,7 +12,7 @@ export const createCaseFromPaperInteractor = (
     stinFileId,
   }: {
     applicationForWaiverOfFilingFeeFileId: string;
-    atpFileId: string;
+    attachmentToPetitionFileId: string;
     corporateDisclosureFileId: string;
     petitionFileId: string;
     petitionMetadata: any; // TODO: add types
@@ -36,7 +24,7 @@ export const createCaseFromPaperInteractor = (
     applicationContext,
     body: {
       applicationForWaiverOfFilingFeeFileId,
-      atpFileId,
+      attachmentToPetitionFileId,
       corporateDisclosureFileId,
       petitionFileId,
       petitionMetadata,
