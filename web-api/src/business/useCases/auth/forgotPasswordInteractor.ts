12ffa3aba48829426fcc7e3ca91c6679d54fe0af
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-01-19 14:37:25 -0600

    10007: type forgotPasswordInteractor

--- web-api/src/business/useCases/auth/forgotPasswordInteractor.ts
+++ web-api/src/business/useCases/auth/forgotPasswordInteractor.ts
@@ -1,9 +1,19 @@
-export const forgotPasswordInteractor = async (): Promise<{
+import { ServerApplicationContext } from '@web-api/applicationContext';
+
+export const forgotPasswordInteractor = async (
+  applicationContext: ServerApplicationContext,
+  {
+    email,
+  }: {
+    email: string;
+  },
+): Promise<{
   bad?: string;
   good?: string;
 }> => {
-  console.log('this is good');
-  const result = { good: 'this is good?' };
+  console.log('this is good', email);
+  await applicationContext.getCognito();
+  const result = { good: `this is good? ${email}` };
   return result;
   // return { bad: 'this is bad' };
 };
