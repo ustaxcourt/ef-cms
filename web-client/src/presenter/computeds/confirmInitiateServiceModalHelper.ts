8f9d599f3abecf028a911bc07455d6fbc64770b1
Mike Marcotte <mike.marcotte@gmail.com>
2024-01-22 15:58:10 -0500

    10216: address some more typescript errors

--- web-client/src/presenter/computeds/confirmInitiateServiceModalHelper.ts
+++ web-client/src/presenter/computeds/confirmInitiateServiceModalHelper.ts
@@ -58,7 +58,11 @@ export const confirmInitiateServiceModalHelper = (
   if (showConsolidatedCasesForService) {
     const { consolidatedCasesToMultiDocketOn } = get(state.modal.form);
 
-    const paperServiceParties = [];
+    const paperServiceParties: {
+      contactId: string;
+      userId: string;
+      name: string;
+    }[] = [];
 
     consolidatedCasesToMultiDocketOn.forEach(aCase => {
       if (aCase.checked) {
@@ -92,7 +96,7 @@ export const confirmInitiateServiceModalHelper = (
     parties = getPaperServiceParties(applicationContext, formattedCaseDetail);
   }
 
-  const contactsNeedingPaperService = [];
+  const contactsNeedingPaperService: { name: string }[] = [];
 
   const roleToDisplay = party => {
     if (party.role === USER_ROLES.privatePractitioner) {
