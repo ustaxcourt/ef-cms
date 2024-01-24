54c45cecf1980c0478e695e8eb62642fb536dec0
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-01-23 21:22:09 -0600

    10007: clearAuthStateAction and test

--- web-client/src/presenter/actions/Login/clearAuthStateAction.ts
+++ web-client/src/presenter/actions/Login/clearAuthStateAction.ts
@@ -2,17 +2,20 @@ import { state } from '@web-client/presenter/app.cerebral';
 
 export const clearAuthStateAction = ({ store }: ActionProps) => {
   store.set(state.authentication, {
-    forgotPassword: {
+    authentication: {
       code: '',
-      email: '',
-      userId: '',
+      forgotPassword: {
+        code: '',
+        email: '',
+        userId: '',
+      },
+      form: {
+        confirmPassword: '',
+        email: '',
+        password: '',
+      },
+      tempPassword: '',
+      userEmail: '',
     },
-    form: {
-      confirmPassword: '',
-      email: '',
-      password: '',
-    },
-    tempPassword: '',
-    userEmail: '',
   });
 };
