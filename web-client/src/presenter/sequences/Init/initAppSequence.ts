08e5db5c8cc2bc5a03ab4e1e9f166d0e442f868c
Zachary Rogers <zrogers>
2024-01-25 14:03:41 -0800

    10007: remove deleted tsx from public app, show spinner on init app

--- web-client/src/presenter/sequences/Init/initAppSequence.ts
+++ web-client/src/presenter/sequences/Init/initAppSequence.ts
@@ -7,26 +7,29 @@ import { setMaintenanceModeAction } from '@web-client/presenter/actions/setMaint
 import { setTokenAction } from '@web-client/presenter/actions/Login/setTokenAction';
 import { setUserAction } from '@web-client/presenter/actions/setUserAction';
 import { setUserPermissionsAction } from '@web-client/presenter/actions/setUserPermissionsAction';
+import { showProgressSequenceDecorator } from '@web-client/presenter/utilities/showProgressSequenceDecorator';
 import { startRefreshIntervalSequence } from '@web-client/presenter/sequences/startRefreshIntervalSequence';
 
 export const initAppSequence = [
-  getMaintenanceModeAction,
-  setMaintenanceModeAction,
-  isMaintenanceModeEngagedAction,
-  {
-    maintenanceModeOff: [
-      startRefreshIntervalSequence,
-      refreshTokenAction,
-      {
-        userIsLoggedIn: [
-          setTokenAction,
-          getUserAction,
-          setUserAction,
-          setUserPermissionsAction,
-        ],
-        userIsNotLoggedIn: [],
-      },
-    ],
-    maintenanceModeOn: [navigateToMaintenanceAction],
-  },
+  showProgressSequenceDecorator([
+    getMaintenanceModeAction,
+    setMaintenanceModeAction,
+    isMaintenanceModeEngagedAction,
+    {
+      maintenanceModeOff: [
+        startRefreshIntervalSequence,
+        refreshTokenAction,
+        {
+          userIsLoggedIn: [
+            setTokenAction,
+            getUserAction,
+            setUserAction,
+            setUserPermissionsAction,
+          ],
+          userIsNotLoggedIn: [],
+        },
+      ],
+      maintenanceModeOn: [navigateToMaintenanceAction],
+    },
+  ]),
 ] as unknown as () => void;
