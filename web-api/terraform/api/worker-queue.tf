c0ba82b5f21841b252428d4ae7c79c3a9c0609a4
Zachary Rogers <zrogers>
2024-01-26 16:14:35 -0800

    10007: **Infrastructure changes**  Finished creating new resources for worker queue

--- web-api/terraform/api/worker-queue.tf
+++ web-api/terraform/api/worker-queue.tf
@@ -5,14 +5,41 @@ resource "aws_lambda_function" "worker_lambda" {
   timeout          = "900"
   memory_size      = "3008"
   runtime          = var.node_version
-  depends_on       = [var.worker_object] # 10007 come back
+  depends_on       = [var.worker_object]
   s3_bucket        = var.lambda_bucket_id
-  s3_key           = "worker_${var.current_color}.js.zip" # 10007 come back
-  source_code_hash = var.worker_object_hash #10007 come back
+  s3_key           = "worker_${var.current_color}.js.zip"
+  source_code_hash = var.worker_object_hash
 
   layers = var.use_layers ? [aws_lambda_layer_version.puppeteer_layer.arn] : null
 
   environment {
     variables = var.lambda_environment
   }
+}
+
+resource "aws_sqs_queue" "worker_queue" {
+  name = "worker_queue_${var.environment}_${var.current_color}"
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.worker_dl_queue.arn
+    maxReceiveCount     = 1
+  })
+}
+
+resource "aws_lambda_event_source_mapping" "worker_event_mapping" {
+  event_source_arn = aws_sqs_queue.worker_queue.arn
+  function_name    = aws_lambda_function.worker_lambda.arn
+  batch_size       = 1
+}
+
+resource "aws_sqs_queue" "worker_dl_queue" {
+  name = "worker_dl_queue_${var.environment}_${var.current_color}"
+}
+
+resource "aws_sqs_queue_redrive_allow_policy" "worker_queue_redrive_allow_policy" {
+  queue_url = aws_sqs_queue.worker_dl_queue.id
+
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = [aws_sqs_queue.worker_queue.arn]
+  })
 }
