b80358e100bde58849d30caaae0f86853a35b292
Kaitlyn Swann <kswann@flexion.us>
2024-01-29 09:30:31 -0700

    10007: Make cognito triggers return event at a minimum or else it is an invalid cognito trigger.

--- web-api/terraform/template/lambdas/cognito-triggers.ts
+++ web-api/terraform/template/lambdas/cognito-triggers.ts
@@ -1,7 +1,9 @@
-export const handler = () => {
+export const handler = event => {
   // NOOP - not deleting this until after 10007 has been deployed so that the active color during a deployment does not lose cognito trigger capabilities.
+  return event;
 };
 
-export const updatePetitionerCasesLambda = () => {
+export const updatePetitionerCasesLambda = event => {
   // NOOP - not deleting this until after 10007 has been deployed so that the active color during a deployment does not lose cognito trigger capabilities.
+  return event;
 };
