cd5fd5d7d11847ea06aff3940811d421b74a9e79
Mike Marcotte <mike.marcotte@gmail.com>
2024-01-29 20:31:59 -0500

    10216: fix tests and account for terminal user

--- shared/src/business/useCases/getDownloadPolicyUrlInteractor.ts
+++ shared/src/business/useCases/getDownloadPolicyUrlInteractor.ts
@@ -1,16 +1,12 @@
-import {
-  ALLOWLIST_FEATURE_FLAGS,
-  INITIAL_DOCUMENT_TYPES,
-  ROLES,
-} from '../entities/EntityConstants';
-import { Case, isSealedCase, isUserPartOfGroup } from '../entities/cases/Case';
+import { ALLOWLIST_FEATURE_FLAGS } from '../entities/EntityConstants';
+import { Case } from '../entities/cases/Case';
 import { DocketEntry } from '../entities/DocketEntry';
 import { NotFoundError, UnauthorizedError } from '@web-api/errors/errors';
 import {
   ROLE_PERMISSIONS,
   isAuthorized,
 } from '../../authorization/authorizationClientService';
-import { User } from '../entities/User';
+import { User } from '@shared/business/entities/User';
 
 export const getDownloadPolicyUrlInteractor = async (
   applicationContext: IApplicationContext,
@@ -21,9 +17,6 @@ export const getDownloadPolicyUrlInteractor = async (
   if (!isAuthorized(user, ROLE_PERMISSIONS.VIEW_DOCUMENTS)) {
     throw new UnauthorizedError('Unauthorized');
   }
-
-  const { isInternalUser, isIrsSuperuser, isPetitionsClerk } =
-    getUserRoles(user);
 
   const caseData = await applicationContext
     .getPersistenceGateway()
@@ -37,82 +30,49 @@ export const getDownloadPolicyUrlInteractor = async (
   }
 
   const caseEntity = new Case(caseData, { applicationContext });
-  const petitionDocketEntry = caseEntity.getPetitionDocketEntry();
   const docketEntryEntity = caseEntity.getDocketEntryById({
     docketEntryId: key,
   });
 
-  if (isInternalUser) {
-    handleInternalUser({
-      docketEntryEntity,
-      isPetitionsClerk,
-      petitionDocketEntry,
-    });
-  } else if (isIrsSuperuser) {
-    handleIrsSuperUser({ docketEntryEntity, key, petitionDocketEntry });
+  if (key.includes('.pdf')) {
+    if (
+      caseEntity.getCaseConfirmationGeneratedPdfFileName() !== key ||
+      !caseEntity.userHasAccessToCase(user)
+    ) {
+      throw new UnauthorizedError('Unauthorized');
+    }
+  } else if (caseEntity.getCorrespondenceById({ correspondenceId: key })) {
+    if (!User.isInternalUser(user.role)) {
+      throw new UnauthorizedError(UNAUTHORIZED_DOCUMENT_MESSAGE);
+    }
   } else {
-    let userHasAccessToCase;
-    if (caseEntity.leadDocketNumber) {
-      const { consolidatedCases } = caseData;
-
-      userHasAccessToCase = isUserPartOfGroup({
-        consolidatedCases,
-        userId: user.userId,
-      });
-    } else {
-      userHasAccessToCase = await applicationContext
-        .getPersistenceGateway()
-        .verifyCaseForUser({
-          applicationContext,
-          docketNumber: caseEntity.docketNumber,
-          userId: user.userId,
-        });
+    if (!docketEntryEntity) {
+      throw new NotFoundError(`Docket entry ${key} was not found.`);
+    }
+    if (!docketEntryEntity.isFileAttached) {
+      throw new NotFoundError(
+        `Docket entry ${key} does not have an attached file.`,
+      );
     }
 
-    if (key.includes('.pdf')) {
-      if (
-        caseEntity.getCaseConfirmationGeneratedPdfFileName() !== key ||
-        !userHasAccessToCase
-      ) {
-        throw new UnauthorizedError('Unauthorized');
-      }
-    } else {
-      if (!docketEntryEntity) {
-        throw new NotFoundError(`Docket entry ${key} was not found.`);
-      }
-      if (!docketEntryEntity.isFileAttached) {
-        throw new NotFoundError(
-          `Docket entry ${key} does not have an attached file.`,
-        );
-      }
+    const featureFlags = await applicationContext
+      .getUseCases()
+      .getAllFeatureFlagsInteractor(applicationContext);
 
-      const featureFlags = await applicationContext
-        .getUseCases()
-        .getAllFeatureFlagsInteractor(applicationContext);
+    const documentVisibilityChangeDate =
+      featureFlags[
+        ALLOWLIST_FEATURE_FLAGS.DOCUMENT_VISIBILITY_POLICY_CHANGE_DATE.key
+      ];
 
-      const documentVisibilityChangeDate =
-        featureFlags[
-          ALLOWLIST_FEATURE_FLAGS.DOCUMENT_VISIBILITY_POLICY_CHANGE_DATE.key
-        ];
-
-      const isPublic = DocketEntry.isPublic(docketEntryEntity, {
-        caseIsSealed: isSealedCase(caseEntity),
-        rootDocument: DocketEntry.fetchRootDocument(
-          docketEntryEntity,
-          caseData.docketEntries,
-        ),
+    if (
+      !DocketEntry.isDownloadable(docketEntryEntity, {
+        isTerminalUser: false,
+        rawCase: caseData,
+        user,
         visibilityChangeDate: documentVisibilityChangeDate,
-      });
-
-      if (
-        !DocketEntry.isDownloadable(docketEntryEntity, {
-          isCourtUser: false,
-          isPublic,
-          userHasAccessToCase,
-        })
-      ) {
-        throw new UnauthorizedError(UNAUTHORIZED_DOCUMENT_MESSAGE);
-      }
+      })
+    ) {
+      throw new UnauthorizedError(UNAUTHORIZED_DOCUMENT_MESSAGE);
     }
   }
 
@@ -122,53 +82,5 @@ export const getDownloadPolicyUrlInteractor = async (
   });
 };
 
-const UNAUTHORIZED_DOCUMENT_MESSAGE =
+export const UNAUTHORIZED_DOCUMENT_MESSAGE =
   'Unauthorized to view document at this time.';
-
-const handleInternalUser = ({
-  docketEntryEntity,
-  isPetitionsClerk,
-  petitionDocketEntry,
-}) => {
-  const selectedIsStin =
-    docketEntryEntity &&
-    docketEntryEntity.documentType === INITIAL_DOCUMENT_TYPES.stin.documentType;
-
-  const petitionIsServed =
-    petitionDocketEntry && DocketEntry.isServed(petitionDocketEntry);
-
-  if (selectedIsStin && (!isPetitionsClerk || petitionIsServed)) {
-    throw new UnauthorizedError(
-      'Unauthorized to view case documents at this time.',
-    );
-  }
-};
-
-const handleIrsSuperUser = ({
-  docketEntryEntity,
-  key,
-  petitionDocketEntry,
-}) => {
-  if (petitionDocketEntry && !DocketEntry.isServed(petitionDocketEntry)) {
-    throw new UnauthorizedError(
-      'Unauthorized to view case documents until the petition has been served.',
-    );
-  }
-
-  if (!docketEntryEntity) {
-    throw new NotFoundError(`Docket entry ${key} was not found.`);
-  }
-  if (!docketEntryEntity.isFileAttached) {
-    throw new NotFoundError(
-      `Docket entry ${key} does not have an attached file.`,
-    );
-  }
-};
-
-const getUserRoles = user => {
-  return {
-    isInternalUser: User.isInternalUser(user.role),
-    isIrsSuperuser: user.role === ROLES.irsSuperuser,
-    isPetitionsClerk: user.role === ROLES.petitionsClerk,
-  };
-};
