78d36f94a31c7f7916d99b6d4fa2c43eb58deb2f
Rachel Schneiderman <rschneiderman379@gmail.com>
2024-01-30 13:51:36 -0800

    10007: Exclude getCognito from test coverage, add more coverage

--- web-api/workflow-terraform/migration/main/lambdas/migrations/0000-validate-all-items.test.ts
+++ web-api/workflow-terraform/migration/main/lambdas/migrations/0000-validate-all-items.test.ts
@@ -1,4 +1,3 @@
-import { CASE_STATUS_TYPES } from '@shared/business/entities/EntityConstants';
 import { RawUserCase } from '@shared/business/entities/UserCase';
 import { migrateItems } from './0000-validate-all-items';
 
@@ -7,7 +6,6 @@ describe('0000-validate-all-items', () => {
 
   const entities = [
     {
-      contactPrimary: {},
       entityName: 'Case',
     },
     {
@@ -56,16 +54,16 @@ describe('0000-validate-all-items', () => {
     });
   });
 
+  it('should NOT validate an entity that is not recognized', () => {
+    expect(() =>
+      migrateItems([{ entityName: 'DOES_NOT_EXIST' }]),
+    ).not.toThrow();
+  });
+
   it('should not throw an error when the entity is valid', () => {
     const validUserCase: RawUserCase = {
-      caseCaption: 'Guy Fieri, Petitioner',
-      closedDate: '2019-05-01T21:40:48.000Z',
-      createdAt: '2019-03-01T21:40:46.415Z',
       docketNumber: '104-21',
-      docketNumberWithSuffix: '104-21W',
       entityName: 'UserCase',
-      leadDocketNumber: '101-21',
-      status: CASE_STATUS_TYPES.closed,
     };
 
     const result = migrateItems([validUserCase]);
