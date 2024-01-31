2b05f9e5a7f18b8265b04912c53541414230b304
Jim Lerza <james.lerza@ustaxcourt.gov>
2024-01-31 15:43:20 -0500

    devex-1195: this no-floating-promises check is coming for basically all of our ts scripts...oh joy

--- web-api/terraform/bin/edit-lambda-environment.ts
+++ web-api/terraform/bin/edit-lambda-environment.ts
@@ -7,7 +7,8 @@ const FunctionName = process.env.LAMBDA!;
 const Environment = { Variables: JSON.parse(process.env.LAMBDA_ENV!) };
 const region = process.env.LAMBDA_REGION || '';
 
-await (async () => {
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+(async () => {
   await setEnvironmentVariables({
     Environment,
     FunctionName,
