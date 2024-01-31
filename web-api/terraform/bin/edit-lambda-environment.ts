bbe4c5da4119e7be05f7d032a52f35967676082e
Jim Lerza <james.lerza@ustaxcourt.gov>
2024-01-31 11:53:47 -0500

    devex-1195: if we're not gonna check if an env variable exists, we gotta support it not existing.

--- web-api/terraform/bin/edit-lambda-environment.ts
+++ web-api/terraform/bin/edit-lambda-environment.ts
@@ -5,9 +5,9 @@ requireEnvVars(['LAMBDA', 'LAMBDA_ENV']);
 
 const FunctionName = process.env.LAMBDA!;
 const Environment = { Variables: JSON.parse(process.env.LAMBDA_ENV!) };
-const region = process.env.LAMBDA_REGION!;
+const region = process.env.LAMBDA_REGION || '';
 
-(async () => {
+await (async () => {
   await setEnvironmentVariables({
     Environment,
     FunctionName,
