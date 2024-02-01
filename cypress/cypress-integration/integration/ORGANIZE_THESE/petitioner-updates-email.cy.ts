db332b9c02c8c8173a19983dc0ee9631a30a3162
Zachary Rogers <zrogers>
2024-02-01 12:23:21 -0800

    10007: Redo post-authentication so that it works locally and after migrations.

--- cypress/cypress-integration/integration/ORGANIZE_THESE/petitioner-updates-email.cy.ts
+++ cypress/cypress-integration/integration/ORGANIZE_THESE/petitioner-updates-email.cy.ts
@@ -6,7 +6,6 @@ import {
   goToMyAccount,
 } from '../../support/pages/my-account';
 import { createAPetitioner } from '../../../helpers/create-a-petitioner';
-import { createAndServePaperPetition } from '../../../helpers/create-and-serve-paper-petition';
 import { faker } from '@faker-js/faker';
 import { navigateTo as loginAs } from '../../support/pages/maintenance';
 import { petitionerCreatesElectronicCase } from '../../../helpers/petitioner-creates-electronic-case';
@@ -15,9 +14,6 @@ import { v4 } from 'uuid';
 import { verifyPetitionerAccount } from '../../../helpers/verify-petitioner-account';
 
 describe('Given a petitioner with a DAWSON account', () => {
-  after(() => {
-    cy.task('deleteAllCypressTestAccounts');
-  });
   describe('When they log in and change their email', () => {
     describe('And they do not verify their new email', () => {
       describe('And attempt to log in', () => {
@@ -109,57 +105,6 @@ describe('Given a petitioner with a DAWSON account', () => {
               `petitioner9+test${randomSuffix}@example.com`,
             );
           });
-        });
-      });
-
-      //Replacement for user-post-auth. Should be moved
-      it('a noce should be generated after granting e-access to a practitioner', () => {
-        createAndServePaperPetition().then(({ docketNumber }) => {
-          const practitionerUserName = `cypress_test_account+${v4()}`;
-          const practitionerEmail = `${practitionerUserName}@example.com`;
-          cy.login('admissionsclerk1');
-          cy.get('[data-testid="messages-banner"]');
-          cy.get('[data-testid="docket-number-search-input"]').type(
-            docketNumber,
-          );
-          cy.get('[data-testid="search-docket-number"]').click();
-          cy.get('[data-testid="tab-case-information"]').click();
-          cy.get('[data-testid="tab-parties"]').click();
-          cy.get('[data-testid="edit-petitioner-counsel"]').click();
-          cy.get('[data-testid="internal-edit-petitioner-email-input"]').type(
-            practitionerEmail,
-          );
-          cy.get(
-            '[data-testid="internal-confirm-petitioner-email-input"]',
-          ).type(practitionerEmail);
-          cy.get(
-            '[data-testid="submit-edit-petitioner-information-button"]',
-          ).click();
-          cy.get('[data-testid="modal-button-confirm"]').click();
-          cy.get('[data-testid="success-alert"]').contains('Changes saved');
-
-          cy.visit('/login');
-          cy.get('[data-testid="email-input"]').type(practitionerEmail);
-          cy.get('[data-testid="password-input"]').type('Testing1234$', {
-            log: false,
-          });
-          cy.get('[data-testid="login-button"]').click();
-          cy.get('[data-testid="new-password-input"]').type('Testing1234$');
-          cy.get('[data-testid="confirm-new-password-input"]').type(
-            'Testing1234$',
-          );
-          cy.get('[data-testid="change-password-button"]').click();
-          cy.get('[data-testid="my-cases-link"]');
-          cy.task('waitForNoce', { docketNumber }).then(isNOCECreated => {
-            expect(isNOCECreated).to.equal(
-              true,
-              'NOCE was not generated on a case that a practitioner was granted e-access for.',
-            );
-          });
-          cy.get(`[data-testid="${docketNumber}"]`)
-            .contains(docketNumber)
-            .click();
-          cy.get('tbody:contains(NOCE)').should('exist');
         });
       });
     });
