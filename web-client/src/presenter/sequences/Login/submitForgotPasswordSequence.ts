80d38f3f0a4c40b6cff4a7db2bae27e46ab7391f
Zachary Rogers <zrogers>
2024-02-01 15:41:54 -0800

    10007: Remove createForgotPasswordSequence

--- web-client/src/presenter/sequences/Login/submitForgotPasswordSequence.ts
+++ web-client/src/presenter/sequences/Login/submitForgotPasswordSequence.ts
@@ -1,5 +1,4 @@
 import { clearAuthStateAction } from '@web-client/presenter/actions/Login/clearAuthStateAction';
-import { createForgotPasswordLinkAction } from '@web-client/presenter/actions/Login/createForgotPasswordLinkAction';
 import { forgotPasswordAction } from '@web-client/presenter/actions/Login/forgotPasswordAction';
 import { navigateToLoginAction } from '@web-client/presenter/actions/Login/navigateToLoginAction';
 import { setAlertSuccessAction } from '@web-client/presenter/actions/setAlertSuccessAction';
@@ -11,11 +10,7 @@ export const submitForgotPasswordSequence = [
   showProgressSequenceDecorator([
     forgotPasswordAction,
     {
-      success: [
-        createForgotPasswordLinkAction,
-        setAlertSuccessAction,
-        clearAuthStateAction,
-      ],
+      success: [setAlertSuccessAction, clearAuthStateAction],
       unconfirmedAccount: [
         setAlertWarningAction,
         setSaveAlertsForNavigationAction,
