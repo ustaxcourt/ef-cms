ae44013821d4f0681d5992949719fc312518e78e
Jim Lerza <james.lerza@ustaxcourt.gov>
2024-02-02 13:34:38 -0500

    devex-1195: Enable the destination stream after deploying and enabling the reindex-cron lambda, not before. In the enable-deploying-dynamo-stream-trigger script, return immediately once we detect that the stream is enabled.

--- web-api/enable-deploying-dynamo-stream-trigger.sh
+++ web-api/enable-deploying-dynamo-stream-trigger.sh
@@ -8,7 +8,16 @@
 UUID=$(aws lambda list-event-source-mappings --function-name "arn:aws:lambda:us-east-1:${AWS_ACCOUNT_ID}:function:streams_${ENV}_${DEPLOYING_COLOR}" --region us-east-1 | jq -r ".EventSourceMappings[0].UUID")
 aws lambda update-event-source-mapping --uuid "${UUID}" --region us-east-1 --enabled
 
-sleep 90
+BEGIN=$(date -u "+%s")
+ELAPSED='0'
+STATE=$(aws lambda get-event-source-mapping --uuid "${UUID}" --region us-east-1 --query "State" --output text)
 
-STATE=$(aws lambda get-event-source-mapping --uuid "${UUID}" --region us-east-1 --query "State")
+while [[ "$STATE" != "Enabled" ]] || [[ "$ELAPSED" -gt 120 ]]; do
+  sleep 2
+  STATE=$(aws lambda get-event-source-mapping --uuid "${UUID}" --region us-east-1 --query "State" --output text)
+  ELAPSED=$(($(date -u "+%s") - "$BEGIN"))
+done
+
 echo "${ENV}'s ${DEPLOYING_COLOR} dynamo stream is now: ${STATE}"
+
+[[ "$STATE" != "Enabled" ]] && exit 1
