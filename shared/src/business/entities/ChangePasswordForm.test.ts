034be033fb9e1363fb2c4471e2d6ea40998ab2a2
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-02-02 14:07:13 -0600

    10007: misc tests

--- shared/src/business/entities/ChangePasswordForm.test.ts
+++ shared/src/business/entities/ChangePasswordForm.test.ts
@@ -1,11 +1,11 @@
 import { ChangePasswordForm } from '@shared/business/entities/ChangePasswordForm';
 
 describe('ChangePasswordForm', () => {
-  const userEmail = 'updateMyPassword@example.com';
+  const email = 'updateMyPassword@example.com';
   it('should be valid when all required fields are present and password meets requirements', () => {
     const validChangePasswordForm = {
+      email,
       password: 'TestPassword!1',
-      userEmail,
     };
     validChangePasswordForm['confirmPassword'] =
       validChangePasswordForm.password;
@@ -18,8 +18,8 @@ describe('ChangePasswordForm', () => {
   it('should not be valid when password does not match confirmPassword', () => {
     const inValidChangePasswordForm = {
       confirmPassword: 'Testing1$',
+      email,
       password: 'Testing2$',
-      userEmail,
     };
     const changePasswordForm = new ChangePasswordForm(
       inValidChangePasswordForm,
@@ -35,7 +35,7 @@ describe('ChangePasswordForm', () => {
     it('should not be valid when password is not present', () => {
       const inValidChangePasswordForm = {
         confirmPassword: 'TestPassword!1',
-        userEmail,
+        email,
       };
       const changePasswordForm = new ChangePasswordForm(
         inValidChangePasswordForm,
@@ -49,8 +49,8 @@ describe('ChangePasswordForm', () => {
 
     it('should not be valid when confirmPassword is not present', () => {
       const inValidChangePasswordForm = {
+        email,
         password: 'TestPassword!1',
-        userEmail,
       };
       const changePasswordForm = new ChangePasswordForm(
         inValidChangePasswordForm,
@@ -62,7 +62,7 @@ describe('ChangePasswordForm', () => {
       });
     });
 
-    it('should not be valid when userEmail is not present', () => {
+    it('should not be valid when email is not present', () => {
       const inValidChangePasswordForm = {
         confirmPassword: 'TestPassword!1',
         password: 'TestPassword!1',
@@ -73,7 +73,7 @@ describe('ChangePasswordForm', () => {
 
       expect(changePasswordForm.isValid()).toBe(false);
       expect(changePasswordForm.getFormattedValidationErrors()).toMatchObject({
-        userEmail: 'Enter a valid email address',
+        email: 'Enter a valid email address',
       });
     });
   });
@@ -81,8 +81,8 @@ describe('ChangePasswordForm', () => {
   describe('Password Requirements', () => {
     it('should not be valid when password does not contain number', () => {
       const inValidChangePasswordForm = {
+        email,
         password: 'testPassword',
-        userEmail,
       };
       inValidChangePasswordForm['confirmPassword'] =
         inValidChangePasswordForm.password;
@@ -98,8 +98,8 @@ describe('ChangePasswordForm', () => {
 
     it('should not be valid when password does not contain space or special character', () => {
       const inValidChangePasswordForm = {
+        email,
         password: 'testpassword1',
-        userEmail,
       };
       inValidChangePasswordForm['confirmPassword'] =
         inValidChangePasswordForm.password;
@@ -115,8 +115,8 @@ describe('ChangePasswordForm', () => {
 
     it('should not be valid when password does not contain an uppercase character', () => {
       const inValidChangePasswordForm = {
+        email,
         password: 'testpassword!1',
-        userEmail,
       };
       inValidChangePasswordForm['confirmPassword'] =
         inValidChangePasswordForm.password;
@@ -132,8 +132,8 @@ describe('ChangePasswordForm', () => {
 
     it('should not be valid when password does not contain an lowercase character', () => {
       const inValidChangePasswordForm = {
+        email,
         password: 'TESTPASSWORD!1',
-        userEmail,
       };
       inValidChangePasswordForm['confirmPassword'] =
         inValidChangePasswordForm.password;
@@ -149,8 +149,8 @@ describe('ChangePasswordForm', () => {
 
     it('should not be valid when password does contains trailing space', () => {
       const inValidChangePasswordForm = {
+        email,
         password: ' TestPassword!1',
-        userEmail,
       };
       inValidChangePasswordForm['confirmPassword'] =
         inValidChangePasswordForm.password;
@@ -166,8 +166,8 @@ describe('ChangePasswordForm', () => {
 
     it('should not be valid when password does not meet min length of 8', () => {
       const inValidChangePasswordForm = {
+        email,
         password: 'TPwd!1',
-        userEmail,
       };
       inValidChangePasswordForm['confirmPassword'] =
         inValidChangePasswordForm.password;
@@ -183,9 +183,9 @@ describe('ChangePasswordForm', () => {
 
     it('should not be valid when password does not meet max length of 99', () => {
       const inValidChangePasswordForm = {
+        email,
         password:
           'G?=Q2hiJHtTb/]n.EjFqqqaWBWJT29B(R8L{b]t#Kw+trZR=Y}EiVe68eRK[z&Yq/*P)L:y7Afr$V_e(7*fM?)b@0,[?Gb&?_@/(',
-        userEmail,
       };
       inValidChangePasswordForm['confirmPassword'] =
         inValidChangePasswordForm.password;
---
+++ web-api/src/business/useCases/auth/forgotPasswordInteractor.test.ts
@@ -0,0 +1,214 @@
+import { ROLES } from '@shared/business/entities/EntityConstants';
+import { UnauthorizedError } from '@web-api/errors/errors';
+import { UserStatusType } from '@aws-sdk/client-cognito-identity-provider';
+import { applicationContext } from '../../../../../shared/src/business/test/createTestApplicationContext';
+import { forgotPasswordInteractor } from '@web-api/business/useCases/auth/forgotPasswordInteractor';
+
+describe('forgotPasswordInteractor', () => {
+  const OLD_ENV = process.env;
+
+  const email = 'example@example.com';
+  const name = 'Antoninus Sara';
+  const userId = 'c3f56e3d-0e6e-44bb-98f1-7c4a91dca1b9';
+  const userStatus = UserStatusType.CONFIRMED;
+  const mockConfirmationCode = '09d0322d-12da-47c8-8d8b-cc76f97022c2';
+  const mockUser = {
+    accountStatus: userStatus,
+    email,
+    name,
+    role: ROLES.petitioner,
+    userId,
+  };
+  const expectedForgotPasswordCode = 'b0f56806-a9ab-4152-ac3f-2da231499368';
+
+  beforeAll(() => {
+    applicationContext
+      .getUserGateway()
+      .getUserByEmail.mockResolvedValue(mockUser);
+
+    applicationContext
+      .getUseCaseHelpers()
+      .createUserConfirmation.mockResolvedValue({
+        confirmationCode: mockConfirmationCode,
+      });
+
+    applicationContext
+      .getPersistenceGateway()
+      .generateForgotPasswordCode.mockResolvedValue({
+        code: expectedForgotPasswordCode,
+      });
+  });
+
+  beforeEach(() => {
+    process.env.DEFAULT_ACCOUNT_PASS = 'password';
+    process.env.STAGE = 'local';
+  });
+
+  afterEach(() => {
+    applicationContext.environment.stage = 'local';
+    process.env = OLD_ENV;
+  });
+
+  it('should return early when user account does not exist', async () => {
+    applicationContext
+      .getUserGateway()
+      .getUserByEmail.mockResolvedValueOnce(undefined);
+
+    const result = await forgotPasswordInteractor(applicationContext, {
+      email,
+    });
+
+    expect(
+      applicationContext.getUserGateway().getUserByEmail.mock.calls[0][1],
+    ).toEqual({ email });
+
+    expect(result).toEqual({
+      email,
+    });
+  });
+
+  it('should throw an UnauthorizedError and call createUserConfirmation when user account is unconfirmed', async () => {
+    applicationContext.getUserGateway().getUserByEmail.mockResolvedValueOnce({
+      ...mockUser,
+      accountStatus: UserStatusType.UNCONFIRMED,
+    });
+
+    await expect(
+      forgotPasswordInteractor(applicationContext, {
+        email,
+      }),
+    ).rejects.toThrow(new UnauthorizedError('User is unconfirmed'));
+
+    expect(
+      applicationContext.getUserGateway().getUserByEmail.mock.calls[0][1],
+    ).toEqual({ email });
+
+    expect(
+      applicationContext.getUseCaseHelpers().createUserConfirmation.mock
+        .calls[0][1],
+    ).toEqual({ email, userId: mockUser.userId });
+  });
+
+  it('should throw an UnauthorizedError and call adminCreateUser (without TemporaryPassword on prod) when user account is in FORCE_CHANGE_PASSWORD status', async () => {
+    process.env.STAGE = 'prod';
+
+    applicationContext.getUserGateway().getUserByEmail.mockResolvedValueOnce({
+      ...mockUser,
+      accountStatus: UserStatusType.FORCE_CHANGE_PASSWORD,
+    });
+
+    await expect(
+      forgotPasswordInteractor(applicationContext, {
+        email,
+      }),
+    ).rejects.toThrow(new UnauthorizedError('User is unconfirmed'));
+
+    expect(
+      applicationContext.getCognito().adminCreateUser.mock.calls[0][0]
+        .TemporaryPassword,
+    ).toBeUndefined();
+  });
+
+  it('should throw an UnauthorizedError and call adminCreateUser (with TemporaryPassword on non-prod environments) when user account is in FORCE_CHANGE_PASSWORD status', async () => {
+    applicationContext.getUserGateway().getUserByEmail.mockResolvedValueOnce({
+      ...mockUser,
+      accountStatus: UserStatusType.FORCE_CHANGE_PASSWORD,
+    });
+
+    await expect(
+      forgotPasswordInteractor(applicationContext, {
+        email,
+      }),
+    ).rejects.toThrow(new UnauthorizedError('User is unconfirmed'));
+
+    expect(
+      applicationContext.getUserGateway().getUserByEmail.mock.calls[0][1],
+    ).toEqual({ email });
+    expect(
+      applicationContext.getUseCaseHelpers().createUserConfirmation,
+    ).not.toHaveBeenCalled();
+
+    expect(
+      applicationContext.getCognito().adminCreateUser.mock.calls[0][0],
+    ).toEqual({
+      DesiredDeliveryMediums: ['EMAIL'],
+      MessageAction: 'RESEND',
+      TemporaryPassword: process.env.DEFAULT_ACCOUNT_PASS,
+      UserPoolId: applicationContext.environment.userPoolId,
+      Username: email,
+    });
+  });
+
+  it('should generate a forgot password code and send it to the user when user account has a valid status', async () => {
+    const expectedQueryString = `code=${expectedForgotPasswordCode}&email=${email}`;
+    const expectedVerificationLink = `https://app.${process.env.EFCMS_DOMAIN}/reset-password?${expectedQueryString}`;
+
+    const result = await forgotPasswordInteractor(applicationContext, {
+      email,
+    });
+
+    expect(
+      applicationContext.getUserGateway().getUserByEmail.mock.calls[0][1],
+    ).toEqual({ email });
+    expect(
+      applicationContext.getUseCaseHelpers().createUserConfirmation,
+    ).not.toHaveBeenCalled();
+    expect(
+      applicationContext.getCognito().adminCreateUser,
+    ).not.toHaveBeenCalled();
+    expect(
+      applicationContext.getPersistenceGateway().generateForgotPasswordCode.mock
+        .calls[0][1],
+    ).toEqual({ userId: mockUser.userId });
+    expect(
+      applicationContext.getMessageGateway().sendEmailToUser.mock.calls[0][1]
+        .subject,
+    ).toEqual('U.S. Tax Court DAWSON Account Verification');
+    expect(
+      applicationContext.getMessageGateway().sendEmailToUser.mock.calls[0][1]
+        .to,
+    ).toEqual(email);
+    expect(
+      applicationContext.getMessageGateway().sendEmailToUser.mock.calls[0][1]
+        .body,
+    ).toEqual(`<div>
+    <h3>Welcome to DAWSON!</h3>
+    <span>
+      ${email} requested a password reset. Use the button below to reset your password. <span style="font-weight: bold;">This will expire in 24 hours</span>.
+    </span>
+    <div style="margin-top: 20px;">
+      <a href="${expectedVerificationLink}" style="background-color: #005ea2; color: white; line-height: 0.9; border-radius: 0.25rem; text-decoration: none; font-size: 1.06rem; padding: .6rem 2.25rem; font-family: Source Sans Pro Web,Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;">Reset Password</a>
+    </div>
+    <div style="margin-top: 20px;">
+      <span>Or you can use this URL: </span>
+      <a href="${expectedVerificationLink}">${expectedVerificationLink}</a>
+    </div>
+    <div style="margin-top: 20px;">
+    <span>If you did not request to reset your password, contact <a href="mailto:dawson.support@ustaxcourt.gov">dawson.support@ustaxcourt.gov</a>.</span>
+    </div>
+
+    <hr style="border-top:1px solid #000000;">
+    <div style="margin-top: 20px;">
+      <span>This is an automated email. We are unable to respond to any messages to this email address.</span>
+    </div>
+  </div>`);
+
+    expect(result).toEqual({
+      code: expectedForgotPasswordCode,
+      email,
+      userId: mockUser.userId,
+    });
+  });
+
+  it('should omit code and userId from response when not on local', async () => {
+    applicationContext.environment.stage = 'notLocal';
+
+    const result = await forgotPasswordInteractor(applicationContext, {
+      email,
+    });
+
+    expect(result).toEqual({
+      email,
+    });
+  });
+});
