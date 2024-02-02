414482d5f8c796da976ba91a66f8b943ab427161
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-02-02 11:59:15 -0600

    10007: add and update client unit tests

---
+++ web-client/src/presenter/actions/Login/validateChangePasswordFormAction.test.ts
@@ -0,0 +1,103 @@
+import { applicationContextForClient as applicationContext } from '@web-client/test/createClientTestApplicationContext';
+import { presenter } from '../../presenter-mock';
+import { runAction } from '@web-client/presenter/test.cerebral';
+import { validateChangePasswordFormAction } from '@web-client/presenter/actions/Login/validateChangePasswordFormAction';
+
+describe('validateChangePasswordFormAction', () => {
+  const mockSuccessPath = jest.fn();
+  const mockErrorPath = jest.fn();
+
+  const TEST_EMAIL = 'example@example.com';
+  const TEST_PASSWORD = 'aA1!aaaa';
+  const TEST_CONFIRM_PASSWORD = TEST_PASSWORD;
+
+  beforeEach(() => {
+    presenter.providers.applicationContext = applicationContext;
+
+    presenter.providers.path = {
+      error: mockErrorPath,
+      success: mockSuccessPath,
+    };
+  });
+
+  it('should call the success path when the authentication form passes ChangePasswordForm validation', async () => {
+    await runAction(validateChangePasswordFormAction, {
+      modules: {
+        presenter,
+      },
+      state: {
+        authentication: {
+          form: {
+            confirmPassword: TEST_CONFIRM_PASSWORD,
+            email: TEST_EMAIL,
+            password: TEST_PASSWORD,
+          },
+        },
+      },
+    });
+
+    expect(mockSuccessPath.mock.calls.length).toEqual(1);
+    expect(mockErrorPath).not.toHaveBeenCalled();
+  });
+
+  it('should call the error path when the authentication form has an invalid email', async () => {
+    await runAction(validateChangePasswordFormAction, {
+      modules: {
+        presenter,
+      },
+      state: {
+        authentication: {
+          form: {
+            confirmPassword: TEST_CONFIRM_PASSWORD,
+            email: 'invalid@example',
+            password: TEST_PASSWORD,
+          },
+        },
+      },
+    });
+
+    expect(mockErrorPath.mock.calls.length).toEqual(1);
+    expect(mockSuccessPath).not.toHaveBeenCalled();
+  });
+
+  it('should call the error path when the authentication form has incongruent password and confirmPassword fields', async () => {
+    await runAction(validateChangePasswordFormAction, {
+      modules: {
+        presenter,
+      },
+      state: {
+        authentication: {
+          form: {
+            confirmPassword: TEST_CONFIRM_PASSWORD,
+            email: TEST_EMAIL,
+            password: 'not_test_password',
+          },
+        },
+      },
+    });
+
+    expect(mockErrorPath.mock.calls.length).toEqual(1);
+    expect(mockSuccessPath).not.toHaveBeenCalled();
+  });
+
+  it('should call the error path when the authentication form has an invalid password', async () => {
+    const mockInvalidPassword = 'abc';
+    await runAction(validateChangePasswordFormAction, {
+      modules: {
+        presenter,
+      },
+      state: {
+        authentication: {
+          form: {
+            confirmPassword: mockInvalidPassword,
+            email: TEST_EMAIL,
+            password: mockInvalidPassword,
+          },
+        },
+      },
+    });
+
+    expect(mockErrorPath.mock.calls.length).toEqual(1);
+    expect(mockSuccessPath).not.toHaveBeenCalled();
+  });
+});
