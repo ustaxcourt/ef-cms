27612dfa231ea1b9746a40f8614b18f464d685cf
Jim Lerza <james.lerza@ustaxcourt.gov>
2024-02-02 14:37:52 -0500

    devex-1195: fix calculation of elapsed seconds

--- web-api/enable-deploying-dynamo-stream-trigger.sh
+++ web-api/enable-deploying-dynamo-stream-trigger.sh
@@ -6,16 +6,16 @@
 [ -z "${DEPLOYING_COLOR}" ] && echo "You must have DEPLOYING_COLOR set in your environment" && exit 1
 
 UUID=$(aws lambda list-event-source-mappings --function-name "arn:aws:lambda:us-east-1:${AWS_ACCOUNT_ID}:function:streams_${ENV}_${DEPLOYING_COLOR}" --region us-east-1 | jq -r ".EventSourceMappings[0].UUID")
-aws lambda update-event-source-mapping --uuid "${UUID}" --region us-east-1 --enabled
+aws lambda update-event-source-mapping --uuid "$UUID" --region us-east-1 --enabled
 
 BEGIN=$(date -u "+%s")
 ELAPSED=0
-STATE=$(aws lambda get-event-source-mapping --uuid "${UUID}" --region us-east-1 --query "State" --output text)
+STATE=$(aws lambda get-event-source-mapping --uuid "$UUID" --region us-east-1 --query "State" --output text)
 
 while [ "$STATE" != "Enabled" ]; do
   sleep 2
-  STATE=$(aws lambda get-event-source-mapping --uuid "${UUID}" --region us-east-1 --query "State" --output text)
-  ELAPSED=$(($(date -u "+%s") - "$BEGIN"))
+  STATE=$(aws lambda get-event-source-mapping --uuid "$UUID" --region us-east-1 --query "State" --output text)
+  ELAPSED=$(($(date -u "+%s") - BEGIN))
   [ "$ELAPSED" -gt 90 ] && break
 done
 
