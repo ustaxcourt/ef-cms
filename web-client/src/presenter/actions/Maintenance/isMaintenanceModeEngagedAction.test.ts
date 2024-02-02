d8ac0bd68ffe60fddb22888942e521369b375363
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-02-02 09:30:53 -0600

    10007: client unit tests

---
+++ web-client/src/presenter/actions/Maintenance/isMaintenanceModeEngagedAction.test.ts
@@ -0,0 +1,39 @@
+import { isMaintenanceModeEngagedAction } from '@web-client/presenter/actions/Maintenance/isMaintenanceModeEngagedAction';
+import { runAction } from '@web-client/presenter/test.cerebral';
+
+describe('isMaintenanceModeEngagedAction', () => {
+  let maintenanceModeOnStub;
+  let maintenanceModeOffStub;
+
+  beforeEach(() => {
+    maintenanceModeOnStub = jest.fn();
+    maintenanceModeOffStub = jest.fn();
+
+    presenter.providers.path = {
+      maintenanceModeOff: maintenanceModeOffStub,
+      maintenanceModeOn: maintenanceModeOnStub,
+    };
+  });
+
+  it('should call path.maintenanceModeOn when state.maintenanceMode is truthy', async () => {
+    await runAction(isMaintenanceModeEngagedAction, {
+      modules: {
+        presenter,
+      },
+      state: { maintenanceMode: true },
+    });
+
+    expect(maintenanceModeOnStub).toHaveBeenCalled();
+  });
+
+  it('should call path.maintenanceModeOff when state.maintenanceMode is falsy', async () => {
+    await runAction(isMaintenanceModeEngagedAction, {
+      modules: {
+        presenter,
+      },
+      state: {},
+    });
+
+    expect(maintenanceModeOffStub).toHaveBeenCalled();
+  });
+});
