718ee89b7db055462fa8ccf9e73166681c498d6d
Nechama Krigsman <nkrigsman@flexion.us>
2024-02-02 11:09:58 -0500

    10217: Remove files that exceed the size limit from the file input

--- web-client/src/views/FileDocument/FileInput.tsx
+++ web-client/src/views/FileDocument/FileInput.tsx
@@ -6,6 +6,44 @@ import { state } from '@web-client/presenter/app.cerebral';
 import React, { useEffect, useRef } from 'react';
 import fileInput from '../../../../node_modules/@uswds/uswds/packages/usa-file-input/src';
 
+const removeNode = node => {
+  if (node && node.parentNode) {
+    node.parentNode.removeChild(node);
+  }
+};
+
+const removeFilesExceedingLimit = (dropTarget, e, filesExceedingSizeLimit) => {
+  const currentPreviewHeading = dropTarget.querySelector(
+    '.usa-file-input__preview-heading',
+  );
+  removeNode(currentPreviewHeading);
+
+  const filePreviews = dropTarget.querySelectorAll('.usa-file-input__preview');
+  const updatedFiles = [];
+  filePreviews.forEach((file, index) => {
+    const fileName = file.textContent.trim();
+    if (filesExceedingSizeLimit.includes(fileName)) {
+      removeNode(file);
+    } else {
+      updatedFiles.push(e.target.files[index]);
+    }
+  });
+
+  const dataTransfer = new DataTransfer();
+  updatedFiles.forEach(file => {
+    dataTransfer.items.add(file);
+  });
+
+  e.target.files = dataTransfer.files;
+
+  const instructions = dropTarget.querySelector(
+    '.usa-file-input__instructions',
+  );
+  if (filesExceedingSizeLimit.length === filePreviews.length && instructions) {
+    instructions.removeAttribute('hidden');
+  }
+};
+
 function DragDropInput({
   existingFiles,
   fileInputName,
@@ -58,29 +96,29 @@ function handleFileSelectionAndValidation(
   updateFormValueSequence,
 ) {
   const { name: inputName } = e.target;
+  const { files } = e.target;
 
-  const filesExceedingSizeLimit = Array.from(e.target.files!)
+  const filesExceedingSizeLimit = Array.from(files)
     .map(capturedFile => {
-      if (
-        capturedFile.size >=
-        maxFileSize * 1024 * 1024
-        // capturedFile.size >= 5
-      ) {
+      if (capturedFile.size >= maxFileSize * 1024 * 1024) {
         return capturedFile.name;
       }
     })
     .filter(file => !!file);
+
   if (filesExceedingSizeLimit.length) {
-    alert(
-      `Your file(s) size is too big. The maximum file size is ${maxFileSize}MB.
-        ${filesExceedingSizeLimit.join(', ')}
-        `,
-    );
-    e.target.value = '';
-    return false;
+    setTimeout(() => {
+      const dropTarget = window.document.querySelector(
+        '.usa-file-input__target',
+      ) as HTMLInputElement;
+
+      removeFilesExceedingLimit(dropTarget, e, filesExceedingSizeLimit);
+    }, 1);
   }
 
-  const clonedFilePromises = Array.from(e.target.files!).map(capturedFile => {
+  if (!files.length) return false;
+
+  const clonedFilePromises = Array.from(files).map(capturedFile => {
     return cloneFile(capturedFile).catch(() => null);
   });
 
