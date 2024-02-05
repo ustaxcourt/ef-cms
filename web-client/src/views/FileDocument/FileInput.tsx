519f9eefeeab2b086596ea5e4bd3f5a2f8923fe3
Nechama Krigsman <nkrigsman@flexion.us>
2024-02-05 13:53:59 -0500

    10217: WIP attempt to get file input working

--- web-client/src/views/FileDocument/FileInput.tsx
+++ web-client/src/views/FileDocument/FileInput.tsx
@@ -6,15 +6,80 @@ import { state } from '@web-client/presenter/app.cerebral';
 import React, { useEffect, useRef } from 'react';
 import fileInput from '../../../../node_modules/@uswds/uswds/packages/usa-file-input/src';
 
-const forceLoadFiles = (fileInputElement, files) => {
-  const loadFiles = new Event('change');
-  Object.defineProperty(loadFiles, 'target', {
-    value: {
-      files,
-    },
-    writable: false,
+const removeNode = node => {
+  if (node && node.parentNode) {
+    node.parentNode.removeChild(node);
+  }
+};
+
+const updatePreviewHeading = (dropTarget, numFilesSelected) => {
+  const previewHeading = dropTarget.querySelector(
+    '.usa-file-input__preview-heading',
+  );
+  if (numFilesSelected === 0) {
+    removeNode(previewHeading);
+    return;
+  }
+  const textNode = previewHeading.firstChild;
+  textNode.textContent = `${numFilesSelected} file${
+    numFilesSelected !== 1 ? 's' : ''
+  } selected`;
+};
+
+const updateFormValues = (files, updateFormValueSequence, inputName) => {
+  const clonedFilePromises = Array.from(files).map(capturedFile => {
+    return cloneFile(capturedFile).catch(() => null);
   });
-  fileInputElement.dispatchEvent(loadFiles);
+
+  Promise.all(clonedFilePromises)
+    .then(clonedFiles => {
+      const validatedFiles = clonedFiles.filter(file => file !== null);
+      updateFormValueSequence({
+        key: inputName,
+        value: validatedFiles,
+      });
+    })
+    .catch(() => {
+      /* no-op */
+    });
+};
+
+const removeFilesExceedingLimit = (dropTarget, e, filesExceedingSizeLimit) => {
+  const numFilesSelected =
+    e.target.files.length - filesExceedingSizeLimit.length;
+  updatePreviewHeading(dropTarget, numFilesSelected);
+
+  const filePreviews = dropTarget.querySelectorAll('.usa-file-input__preview');
+  const updatedFiles = [];
+
+  Array.from(e.target.files).forEach(file => {
+    const fileName = file.name;
+    if (!filesExceedingSizeLimit.includes(fileName)) {
+      updatedFiles.push(file);
+    }
+  });
+
+  // Remove file previews that exceed the size limit
+  filePreviews.forEach(file => {
+    const fileName = file.textContent.trim();
+    if (filesExceedingSizeLimit.includes(fileName)) {
+      removeNode(file);
+    }
+  });
+
+  const dataTransfer = new DataTransfer();
+  updatedFiles.forEach(file => {
+    dataTransfer.items.add(file);
+  });
+
+  e.target.files = dataTransfer.files;
+
+  const instructions = dropTarget.querySelector(
+    '.usa-file-input__instructions',
+  );
+  if (filesExceedingSizeLimit.length === filePreviews.length && instructions) {
+    instructions.removeAttribute('hidden');
+  }
 };
 
 function DragDropInput({
@@ -31,9 +96,20 @@ function DragDropInput({
   }, [inputRef]);
 
   useEffect(() => {
-    const fileInputElement = window.document.getElementById('file-input');
+    const fileInputElement = window.document.getElementById(
+      'file-input',
+    ) as HTMLInputElement;
     if (fileInputElement && existingFiles) {
-      forceLoadFiles(fileInputElement, existingFiles);
+      const loadFilesFromFormEvent = new Event('change');
+
+      Object.defineProperty(loadFilesFromFormEvent, 'target', {
+        value: {
+          files: existingFiles,
+        },
+        writable: false,
+      });
+
+      fileInputElement.dispatchEvent(loadFilesFromFormEvent);
     }
   }, []);
 
@@ -52,87 +128,51 @@ function DragDropInput({
   );
 }
 
-const setFormValues = (updateFormValueSequence, inputName, files) => {
-  const clonedFilePromises = Array.from(files).map(capturedFile => {
-    return cloneFile(capturedFile).catch(() => null);
-  });
-
-  Promise.all(clonedFilePromises)
-    .then(clonedFiles => {
-      const validatedFiles = clonedFiles.filter(file => file !== null);
-      updateFormValueSequence({
-        key: inputName,
-        value: validatedFiles,
-      });
-    })
-    .catch(() => {
-      /* no-op */
-    });
-};
-
 function handleFileSelectionAndValidation(
   e,
   maxFileSize,
   updateFormValueSequence,
 ) {
-  const { files, name: inputName } = e.target;
-  const fileList = Array.from(files);
+  const { name: inputName } = e.target;
+  const { files } = e.target;
 
   const filesExceedingSizeLimit = Array.from(files)
     .map(capturedFile => {
-      if (capturedFile.size >= maxFileSize * 1024 * 1024) {
+      // if (capturedFile.size >= maxFileSize * 1024 * 1024) {
+      if (capturedFile.size >= 40 * 1024 * 1024) {
         return capturedFile.name;
       }
     })
     .filter(file => !!file);
 
   if (filesExceedingSizeLimit.length) {
-    const fileInputElement = window.document.getElementById(
-      'file-input',
-    ) as HTMLInputElement;
-
     setTimeout(() => {
-      const validFiles = fileList.filter(
-        file => !filesExceedingSizeLimit.includes(file.name),
-      );
-      forceLoadFiles(fileInputElement, validFiles);
-
-      const dataTransfer = new DataTransfer();
-      validFiles.forEach(file => {
-        dataTransfer.items.add(file);
-      });
-
-      e.target.files = dataTransfer.files;
-
-      if (e.target.files.length) {
-        setFormValues(updateFormValueSequence, inputName, e.target.files);
-      }
-
+      const dropTarget = window.document.querySelector(
+        '.usa-file-input__target',
+      ) as HTMLInputElement;
+      removeFilesExceedingLimit(dropTarget, e, filesExceedingSizeLimit);
+      updateFormValues(e.target.files, updateFormValueSequence, inputName);
       alert(
         `Your file(s) size is too big. The maximum file size is ${maxFileSize}MB.
           ${filesExceedingSizeLimit.join(', \n')}
           `,
       );
     }, 1);
-
     return false;
   }
 
   if (!files.length) return false;
 
-  setFormValues(updateFormValueSequence, inputName, files);
-
+  updateFormValues(files, updateFormValueSequence, inputName);
   // run validationSequence
 }
 
 export const FileInput = connect(
   {
-    clearModalSequence: sequences['clearModalSequence'],
     constants: state.constants,
     form: state.form,
     name: props.name,
     updateFormValueSequence: sequences[props.updateFormValueSequence],
-
     // validationSequence: sequences[props.validationSequence],
   },
   function FileInput({
@@ -144,7 +184,6 @@ export const FileInput = connect(
     ...remainingProps
     // validationSequence,
   }) {
-    console.log('existing files', form);
     return (
       <React.Fragment>
         <DragDropInput
@@ -164,3 +203,5 @@ export const FileInput = connect(
     );
   },
 );
+
+FileInput.displayName = 'FileInput';
