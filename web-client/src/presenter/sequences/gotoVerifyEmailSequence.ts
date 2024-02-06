cb6bef7334c7b525888c59c5a11ec2ad10c31c63
Kaitlyn Swann <kswann@flexion.us>
2024-02-06 14:58:03 -0700

    10007: Refactor verify userPending email to not be async as all of its work happens on the worker queue.

--- web-client/src/presenter/sequences/gotoVerifyEmailSequence.ts
+++ web-client/src/presenter/sequences/gotoVerifyEmailSequence.ts
@@ -1,14 +1,15 @@
 import { clearUserAction } from '../actions/clearUserAction';
 import { navigateToLoginAction } from '@web-client/presenter/actions/Login/navigateToLoginAction';
+import { setAlertErrorAction } from '@web-client/presenter/actions/setAlertErrorAction';
 import { setAlertSuccessAction } from '@web-client/presenter/actions/setAlertSuccessAction';
-import { startWebSocketConnectionSequenceDecorator } from '../utilities/startWebSocketConnectionSequenceDecorator';
 import { verifyUserPendingEmailAction } from '../actions/verifyUserPendingEmailAction';
 
 export const gotoVerifyEmailSequence = [
-  startWebSocketConnectionSequenceDecorator([
-    verifyUserPendingEmailAction,
-    setAlertSuccessAction,
-    clearUserAction,
-    navigateToLoginAction,
-  ]),
+  verifyUserPendingEmailAction,
+  {
+    error: [setAlertErrorAction],
+    success: [setAlertSuccessAction],
+  },
+  clearUserAction,
+  navigateToLoginAction,
 ] as unknown as (props: { token: string }) => void;
