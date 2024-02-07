75ec9723f796fad0b6646401fd5d9fa2e615ec7d
Rachel Schneiderman <rschneiderman379@gmail.com>
2024-02-06 22:54:56 -0800

    10007: Fixing client unit tests and adding test coverage and types

--- web-client/src/presenter/actions/verifyUserPendingEmailAction.test.ts
+++ web-client/src/presenter/actions/verifyUserPendingEmailAction.test.ts
@@ -4,34 +4,60 @@ import { runAction } from '@web-client/presenter/test.cerebral';
 import { verifyUserPendingEmailAction } from './verifyUserPendingEmailAction';
 
 describe('verifyUserPendingEmailAction', () => {
+  const errorMock = jest.fn();
+  const successMock = jest.fn();
+
+  const mockToken = 'abc';
+
   beforeAll(() => {
     presenter.providers.applicationContext = applicationContext;
+    presenter.providers.path = {
+      error: errorMock,
+      success: successMock,
+    };
   });
 
-  it('should make a call to verifyUserPendingEmailInteractor and return an alertSuccess', async () => {
-    applicationContext
-      .getUseCases()
-      .verifyUserPendingEmailInteractor.mockReturnValue();
-    const result = await runAction(verifyUserPendingEmailAction, {
+  it('should return a success message when the user`s pending email is successfully verified', async () => {
+    await runAction(verifyUserPendingEmailAction, {
       modules: {
         presenter,
       },
       props: {
-        token: 'abc',
+        token: mockToken,
       },
-      state: { form: { contact: {} } },
     });
 
     expect(
-      applicationContext.getUseCases().verifyUserPendingEmailInteractor.mock
-        .calls[0][1].token,
-    ).toEqual('abc');
-
-    expect(result.output).toEqual({
+      applicationContext.getUseCases().verifyUserPendingEmailInteractor,
+    ).toHaveBeenCalledWith(expect.anything(), { token: mockToken });
+    expect(successMock).toHaveBeenCalledWith({
       alertSuccess: {
         message:
           'Your email address is verified. You can now sign in to DAWSON.',
         title: 'Email address verified',
+      },
+    });
+  });
+
+  it('should return an error message when an error occurred while verifying the user`s pending email', async () => {
+    applicationContext
+      .getUseCases()
+      .verifyUserPendingEmailInteractor.mockRejectedValue(new Error());
+
+    await runAction(verifyUserPendingEmailAction, {
+      modules: {
+        presenter,
+      },
+      props: {
+        token: mockToken,
+      },
+    });
+
+    expect(errorMock).toHaveBeenCalledWith({
+      alertError: {
+        message:
+          'An unexpected error occurred. Could not verify e-mail address. Please contact DAWSON support if this continues.',
+        title: 'Unable to verify e-mail address',
       },
     });
   });
