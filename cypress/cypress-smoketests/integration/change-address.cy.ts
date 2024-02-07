61ecb3f3dfe0d09d39bcb859c1498d48f760d70c
Rachel Schneiderman <rschneiderman379@gmail.com>
2024-02-07 09:09:05 -0800

    10007: Fixing change of address cypress test, rename testid for clarity

--- cypress/cypress-smoketests/integration/change-address.cy.ts
+++ cypress/cypress-smoketests/integration/change-address.cy.ts
@@ -1,8 +1,5 @@
 import { faker } from '@faker-js/faker';
-import {
-  loginAsAdmissionsClerk,
-  loginAsPrivatePractitioner,
-} from '../../helpers/auth/login-as-helpers';
+import { logout } from '../../helpers/auth/logout';
 import { petitionsclerkServePetition } from '../../helpers/petitionsclerk-serves-petition';
 import { practitionerCreatesEletronicCase } from '../../helpers/practitioner-creates-electronic-case';
 import { searchByDocketNumberInHeader } from '../../helpers/search-by-docket-number-in-header';
@@ -10,9 +7,14 @@ import { searchByDocketNumberInHeader } from '../../helpers/search-by-docket-num
 describe('change of address', () => {
   it('changing the address of a private practitioner should generate NCA and update their cases', () => {
     const newAddress = faker.location.streetAddress();
-    loginAsPrivatePractitioner('privatePractitioner2');
+    cy.login('privatePractitioner2');
     practitionerCreatesEletronicCase().then(docketNumber => {
+      logout();
+
+      cy.login('petitionsclerk1');
       petitionsclerkServePetition(docketNumber);
+      logout();
+
       cy.login('privatePractitioner2');
       cy.get('[data-testid="case-list-table"]').should('exist');
       cy.get('[data-testid="account-menu-button"]').click();
@@ -25,28 +27,23 @@ describe('change of address', () => {
       cy.get('[data-testid="docket-search-field"]').clear();
       cy.get('[data-testid="docket-search-field"]').type(docketNumber);
       cy.get('[data-testid="search-by-docket-number"]').click();
-      cy.get('[data-testid="docket-record-table"] td')
-        .contains('NCA')
-        .should('exist');
-      cy.get('[data-testid="docket-record-table"] td')
-        .contains('NOTR')
-        .should('exist');
+      cy.get('[data-testid="docket-record-table"] td').contains('NCA');
+      cy.get('[data-testid="docket-record-table"] td').contains('NOTR');
       cy.get('[data-testid="tab-case-information"]').click();
       cy.get('[data-testid="tab-parties"]').click();
       cy.get(
         '[data-testid="petitioner-card-John"] [data-testid="view-counsel-info"]',
       ).click();
-      cy.get('[data-testid="address1-line"]')
-        .contains(newAddress)
-        .should('exist');
+      cy.get('[data-testid="address1-line"]').contains(newAddress);
+      cy.get('[data-testid="modal-button-confirm"]').click();
+      logout();
 
-      loginAsAdmissionsClerk('admissionsclerk1');
+      cy.login('admissionsclerk1');
       searchByDocketNumberInHeader(docketNumber);
-
       cy.get('[data-testid="tab-case-information"] > .button-text').click();
       cy.get('[data-testid="tab-parties"] > .button-text').click();
       cy.get(
-        '[data-testid="petitioner-card-John"] [data-testid="edit-petitioner-counsel"]',
+        '[data-testid="petitioner-card-John"] [data-testid="edit-private-practitioner-counsel"]',
       ).click();
       cy.get('[data-testid="remove-petitioner-btn"]').click();
       cy.get('[data-testid="modal-button-confirm"]').click();
