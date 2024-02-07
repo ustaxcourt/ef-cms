2fcdef8cf5ac6bd5e0f72db00ccf404b55b5b421
Rachel Schneiderman <rschneiderman379@gmail.com>
2024-02-06 22:40:25 -0800

    10007: Add api test coverage

--- web-api/src/business/useCases/auth/confirmSignUpInteractor.test.ts
+++ web-api/src/business/useCases/auth/confirmSignUpInteractor.test.ts
@@ -2,52 +2,64 @@ import { applicationContext } from '../../../../../shared/src/business/test/crea
 import { confirmSignUpInteractor } from './confirmSignUpInteractor';
 
 describe('confirmSignUpInteractor', () => {
-  const confirmationCode = '123456';
-  const userId = 'abc';
-  const userEmail = 'example@example.com';
+  const mockConfirmationCode = 'b4a9ccf0-ad83-4aa5-9241-90bc88f29c5d';
+  const mockUserId = '8a36f1bf-aa46-4495-b33f-4bb34319fa87';
+  const mockEmail = 'example@example.com';
 
-  beforeAll(() => {
-    applicationContext.getCognito().confirmSignUp.mockResolvedValue({});
-  });
-
-  it('should call cognito confirmSignUp with correctly formatted parameters', async () => {
+  it('should throw an error when the confirmation code has expired', async () => {
     applicationContext
       .getPersistenceGateway()
-      .getAccountConfirmationCode.mockResolvedValue(confirmationCode);
+      .getAccountConfirmationCode(undefined);
+
+    await expect(
+      confirmSignUpInteractor(applicationContext, {
+        confirmationCode: mockConfirmationCode,
+        email: mockEmail,
+        userId: mockUserId,
+      }),
+    ).rejects.toThrow('Confirmation code expired');
+  });
+
+  it('should throw an error when the user is not found in persistence by the provided email', async () => {
+    applicationContext
+      .getPersistenceGateway()
+      .getAccountConfirmationCode.mockResolvedValue(mockConfirmationCode);
+    applicationContext
+      .getUserGateway()
+      .getUserByEmail.mockResolvedValue(undefined);
+
+    await expect(
+      confirmSignUpInteractor(applicationContext, {
+        confirmationCode: mockConfirmationCode,
+        email: mockEmail,
+        userId: mockUserId,
+      }),
+    ).rejects.toThrow(`User not found with email: ${mockEmail}`);
+  });
+
+  it('should make several calls to persistence to confirm the petitioner account', async () => {
+    applicationContext
+      .getPersistenceGateway()
+      .getAccountConfirmationCode.mockResolvedValue(mockConfirmationCode);
     applicationContext.getCognito().adminConfirmSignUp.mockResolvedValue({});
     applicationContext
       .getUserGateway()
-      .getUserByEmail.mockResolvedValue({ email: userEmail });
+      .getUserByEmail.mockResolvedValue({ email: mockEmail });
 
     await confirmSignUpInteractor(applicationContext, {
-      confirmationCode,
-      userEmail,
-      userId,
+      confirmationCode: mockConfirmationCode,
+      email: mockEmail,
+      userId: mockUserId,
     });
 
     expect(
       applicationContext.getCognito().adminConfirmSignUp,
     ).toHaveBeenCalled();
-
     expect(
       applicationContext.getCognito().adminUpdateUserAttributes,
     ).toHaveBeenCalled();
-  });
-
-  it('should return an error when confirmSignUp fails', async () => {
-    applicationContext
-      .getCognito()
-      .adminConfirmSignUp.mockRejectedValue(new Error());
-
-    await expect(
-      confirmSignUpInteractor(applicationContext, {
-        confirmationCode,
-        userEmail,
-        userId,
-      }),
-    ).rejects.toThrow();
     expect(
-      applicationContext.getCognito().adminConfirmSignUp,
+      applicationContext.getUseCases().createPetitionerAccountInteractor,
     ).toHaveBeenCalled();
   });
 });
