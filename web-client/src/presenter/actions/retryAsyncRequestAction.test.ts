8d9c2103eddc6a4cb5a3efd4ab654ca0702f38ee
John Cruz <jcruz@flexion.us>
2024-02-07 12:23:22 -0700

    10247: Added more tests, increased test coverage;

--- web-client/src/presenter/actions/retryAsyncRequestAction.test.ts
+++ web-client/src/presenter/actions/retryAsyncRequestAction.test.ts
@@ -29,10 +29,41 @@ describe('retryAsyncRequestAction', () => {
   it('should call the the interactor for the requestToRetry with the originalRequest that it received', async () => {
     mockCall.props.requestToRetry = 'file_and_serve_court_issued_document';
 
-    await runAction(retryAsyncRequestAction, mockCall);
+    await runAction(retryAsyncRequestAction as any, mockCall);
     expect(
       applicationContext.getUseCases().fileAndServeCourtIssuedDocumentInteractor
         .mock.calls[0][1],
     ).toMatchObject(mockOriginalRequest);
+  });
+
+  [
+    ['add_paper_filing', 'addPaperFilingInteractor'],
+    ['edit_paper_filing', 'editPaperFilingInteractor'],
+    [
+      'file_and_serve_court_issued_document',
+      'fileAndServeCourtIssuedDocumentInteractor',
+    ],
+    [
+      'set_notices_for_calendared_trial_session',
+      'setNoticesForCalendaredTrialSessionInteractor',
+    ],
+    ['update_practitioner_user', 'updatePractitionerUserInteractor'],
+    ['update_trial_session', 'updateTrialSessionInteractor'],
+    [
+      'update_user_contact_information',
+      'updateUserContactInformationInteractor',
+    ],
+    ['verify_user_pending_email', 'verifyUserPendingEmailInteractor'],
+  ].forEach(([key, method]) => {
+    it(`should call "${method}" when the key is "${key}"`, async () => {
+      mockCall.props.requestToRetry = key;
+      applicationContext.getUseCases()[method] = jest.fn();
+
+      await runAction(retryAsyncRequestAction as any, mockCall);
+
+      expect(
+        applicationContext.getUseCases()[method].mock.calls[0][1],
+      ).toMatchObject(mockOriginalRequest);
+    });
   });
 });
