b3b084df01f9aed9cc668bddcf7aef0371cef64e
Jim deVos <james.devos@ustaxcourt.gov>
2024-02-08 15:10:00 -0700

    10257: incomplete work on error dialog

---
+++ web-client/src/presenter/sequences/serveToIrsErrorSequence.ts
@@ -0,0 +1,11 @@
+import { clearModalStateAction } from '@web-client/presenter/actions/clearModalStateAction';
+import { setShowModalAction } from '../actions/setShowModalAction';
+import { unsetWaitingForResponseAction } from '@web-client/presenter/actions/unsetWaitingForResponseAction';
+
+export const serveToIrsErrorSequence = [
+  unsetWaitingForResponseAction,
+  clearModalStateAction,
+  setShowModalAction,
+  //   setAlertErrorAction,
+  // error alert? modal? ask ux
+];
