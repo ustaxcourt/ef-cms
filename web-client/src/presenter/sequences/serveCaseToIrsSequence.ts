e655e2863235f83c8a9e64681a161d173b3cd73b
Jason Fu <jasonfu6699@hotmail.com>
2024-02-09 10:10:48 -0800

    10257 - WIP fix integration tests and show error modal

--- web-client/src/presenter/sequences/serveCaseToIrsSequence.ts
+++ web-client/src/presenter/sequences/serveCaseToIrsSequence.ts
@@ -1,8 +1,18 @@
 import { clearPdfPreviewUrlAction } from '../actions/CourtIssuedOrder/clearPdfPreviewUrlAction';
 import { serveCaseToIrsAction } from '../actions/StartCaseInternal/serveCaseToIrsAction';
-import { showProgressSequenceDecorator } from '../utilities/showProgressSequenceDecorator';
+import { setShowModalAction } from '../actions/setShowModalAction';
+import { setWaitingForResponseAction } from '../actions/setWaitingForResponseAction';
+import { setWaitingTextAction } from '../actions/setWaitingTextAction';
 
 export const serveCaseToIrsSequence = [
   clearPdfPreviewUrlAction,
-  showProgressSequenceDecorator([serveCaseToIrsAction]),
+  setWaitingForResponseAction,
+  setWaitingTextAction(
+    'Please stay on this page while we process your request.',
+  ),
+  serveCaseToIrsAction,
+  {
+    error: [setShowModalAction],
+    success: [],
+  },
 ];
