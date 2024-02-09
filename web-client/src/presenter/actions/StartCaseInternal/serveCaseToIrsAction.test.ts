c4c5513fb9c8bc037b4a30c42ada300223380faa
Mike Marcotte <mike.marcotte@gmail.com>
2024-02-09 16:39:01 -0500

    10257: fix unit test

--- web-client/src/presenter/actions/StartCaseInternal/serveCaseToIrsAction.test.ts
+++ web-client/src/presenter/actions/StartCaseInternal/serveCaseToIrsAction.test.ts
@@ -4,26 +4,14 @@ import { runAction } from '@web-client/presenter/test.cerebral';
 import { serveCaseToIrsAction } from './serveCaseToIrsAction';
 
 describe('serveCaseToIrsAction', () => {
-  let pathPaperStub;
-  let pathElectronicStub;
-  let createObjectURLStub;
+  const pathErrorStub = jest.fn();
+  const pathSuccessStub = jest.fn();
 
   beforeAll(() => {
-    global.window ??= Object.create(global);
-    global.Blob = () => {};
-
-    pathPaperStub = jest.fn();
-    pathElectronicStub = jest.fn();
-    createObjectURLStub = jest.fn().mockReturnValue('123456-abcdef');
-
-    presenter.providers.router = {
-      createObjectURL: createObjectURLStub,
-    };
-
     presenter.providers.applicationContext = applicationContext;
     presenter.providers.path = {
-      electronic: pathElectronicStub,
-      paper: pathPaperStub,
+      error: pathErrorStub,
+      success: pathSuccessStub,
     };
   });
 
@@ -42,28 +30,6 @@ describe('serveCaseToIrsAction', () => {
     expect(
       applicationContext.getUseCases().serveCaseToIrsInteractor,
     ).toHaveBeenCalled();
-    expect(pathElectronicStub).toHaveBeenCalled();
-  });
-
-  it('serves a paper case and return the paper path', async () => {
-    applicationContext
-      .getUseCases()
-      .serveCaseToIrsInteractor.mockReturnValue(['pdf-bytes']);
-
-    await runAction(serveCaseToIrsAction, {
-      modules: {
-        presenter,
-      },
-      state: {
-        trialSession: {
-          trialSessionId: 'abc-123',
-        },
-      },
-    });
-
-    expect(
-      applicationContext.getUseCases().serveCaseToIrsInteractor,
-    ).toHaveBeenCalled();
-    expect(pathPaperStub).toHaveBeenCalled();
+    expect(pathSuccessStub).toHaveBeenCalled();
   });
 });
