fc15ee242a6402b7d2aebb74bc0e4dadb56f073a
Jason Fu <jasonfu6699@hotmail.com>
2024-02-09 11:59:37 -0800

    10257 - fix cypress test; remove waiting text when serving case to IRS

--- cypress/helpers/search-by-docket-number-in-header.ts
+++ cypress/helpers/search-by-docket-number-in-header.ts
@@ -1,5 +1,5 @@
 export function searchByDocketNumberInHeader(docketNumber: string) {
-  cy.get('[data-testid="docket-number-search-input"]').clear();
+  cy.get('[data-testid="docket-number-search-input"]').scrollIntoView().clear();
   cy.get('[data-testid="docket-number-search-input"]').type(docketNumber);
   cy.get('[data-testid="search-docket-number"]').click();
   cy.get('[data-testid="case-detail-menu-button"]').should('exist');
