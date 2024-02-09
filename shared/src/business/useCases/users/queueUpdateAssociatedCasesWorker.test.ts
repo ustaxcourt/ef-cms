e01026f3159fed8bdf32389439628cdb8477dca2
Kaitlyn Swann <kswann@flexion.us>
2024-02-09 09:30:38 -0700

    10007: Update test for queueUpdateAssociatedCasesWorker to include current user object

--- shared/src/business/useCases/users/queueUpdateAssociatedCasesWorker.test.ts
+++ shared/src/business/useCases/users/queueUpdateAssociatedCasesWorker.test.ts
@@ -4,14 +4,14 @@ import { applicationContext } from '../../test/createTestApplicationContext';
 import { queueUpdateAssociatedCasesWorker } from './queueUpdateAssociatedCasesWorker';
 
 describe('queueUpdateAssociatedCasesWorker', () => {
-  beforeEach(() => {
+  beforeEach(() => {});
+
+  it('should lookup the docket numbers for the current user', async () => {
     applicationContext
       .getPersistenceGateway()
       .getDocketNumbersByUser.mockReturnValue(['111-20', '222-20', '333-20']);
     applicationContext.getCurrentUser.mockReturnValue(MOCK_PRACTITIONER);
-  });
 
-  it('should lookup the docket numbers for the current user', async () => {
     await queueUpdateAssociatedCasesWorker(applicationContext, {
       user: MOCK_PRACTITIONER,
     });
@@ -25,6 +25,10 @@ describe('queueUpdateAssociatedCasesWorker', () => {
   });
 
   it('should return an object that includes all of the docketNumbers associated with the practitioner', async () => {
+    applicationContext
+      .getPersistenceGateway()
+      .getDocketNumbersByUser.mockReturnValue(['111-20', '222-20', '333-20']);
+    applicationContext.getCurrentUser.mockReturnValue(MOCK_PRACTITIONER);
     applicationContext.getWorkerGateway().initialize.mockReturnValue({});
 
     await queueUpdateAssociatedCasesWorker(applicationContext, {
@@ -37,6 +41,7 @@ describe('queueUpdateAssociatedCasesWorker', () => {
       message: {
         payload: { docketNumber: '111-20', user: MOCK_PRACTITIONER },
         type: MESSAGE_TYPES.UPDATE_ASSOCIATED_CASE,
+        user: MOCK_PRACTITIONER,
       },
     });
     expect(
@@ -45,6 +50,7 @@ describe('queueUpdateAssociatedCasesWorker', () => {
       message: {
         payload: { docketNumber: '222-20', user: MOCK_PRACTITIONER },
         type: MESSAGE_TYPES.UPDATE_ASSOCIATED_CASE,
+        user: MOCK_PRACTITIONER,
       },
     });
     expect(
@@ -53,11 +59,16 @@ describe('queueUpdateAssociatedCasesWorker', () => {
       message: {
         payload: { docketNumber: '333-20', user: MOCK_PRACTITIONER },
         type: MESSAGE_TYPES.UPDATE_ASSOCIATED_CASE,
+        user: MOCK_PRACTITIONER,
       },
     });
   });
 
   it('should attempt to send a message to update the petitioner cases via the worker gateway', async () => {
+    applicationContext
+      .getPersistenceGateway()
+      .getDocketNumbersByUser.mockReturnValue(['111-20', '222-20', '333-20']);
+    applicationContext.getCurrentUser.mockReturnValue(petitionerUser);
     applicationContext.getWorkerGateway().initialize.mockReturnValue({});
 
     await queueUpdateAssociatedCasesWorker(applicationContext, {
@@ -70,6 +81,7 @@ describe('queueUpdateAssociatedCasesWorker', () => {
       message: {
         payload: { docketNumber: '111-20', user: petitionerUser },
         type: MESSAGE_TYPES.UPDATE_ASSOCIATED_CASE,
+        user: petitionerUser,
       },
     });
     expect(
@@ -78,6 +90,7 @@ describe('queueUpdateAssociatedCasesWorker', () => {
       message: {
         payload: { docketNumber: '222-20', user: petitionerUser },
         type: MESSAGE_TYPES.UPDATE_ASSOCIATED_CASE,
+        user: petitionerUser,
       },
     });
     expect(
@@ -86,6 +99,7 @@ describe('queueUpdateAssociatedCasesWorker', () => {
       message: {
         payload: { docketNumber: '333-20', user: petitionerUser },
         type: MESSAGE_TYPES.UPDATE_ASSOCIATED_CASE,
+        user: petitionerUser,
       },
     });
   });
