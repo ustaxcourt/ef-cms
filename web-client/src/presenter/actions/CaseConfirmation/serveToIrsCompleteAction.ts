adbc0ecce0e2b95c9cc6ebb558cba47a871986dd
Mike Marcotte <mike.marcotte@gmail.com>
2024-02-12 15:20:03 -0500

    10257: clean up action

--- web-client/src/presenter/actions/CaseConfirmation/serveToIrsCompleteAction.ts
+++ web-client/src/presenter/actions/CaseConfirmation/serveToIrsCompleteAction.ts
@@ -1,14 +1,3 @@
-// import { state } from '@web-client/presenter/app.cerebral';
-
-/**
- * calls proxy endpoint to serve a case
- * @param {object} providers the providers object
- * @param {object} providers.applicationContext the application context
- * @param {Function} providers.get the cerebral get helper function
- * @param {object} providers.path the cerebral path which contains the next path in the sequence (path of success or error)
- * @param {object} providers.props the cerebral props object
- * @returns {object} the next path based upon if there was any paper service or all electronic service
- */
 export const serveToIrsCompleteAction = ({ path, props }: ActionProps) => {
   const { pdfUrl } = props;
 
