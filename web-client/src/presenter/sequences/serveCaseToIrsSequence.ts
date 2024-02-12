fd88e6289d6b6a42095c423a31139688f37422e1
Mike Marcotte <mike.marcotte@gmail.com>
2024-02-12 12:13:17 -0500

    10257: more changes cherry picked

--- web-client/src/presenter/sequences/serveCaseToIrsSequence.ts
+++ web-client/src/presenter/sequences/serveCaseToIrsSequence.ts
@@ -2,14 +2,10 @@ import { clearPdfPreviewUrlAction } from '../actions/CourtIssuedOrder/clearPdfPr
 import { serveCaseToIrsAction } from '../actions/StartCaseInternal/serveCaseToIrsAction';
 import { setShowModalAction } from '../actions/setShowModalAction';
 import { setWaitingForResponseAction } from '../actions/setWaitingForResponseAction';
-import { setWaitingTextAction } from '../actions/setWaitingTextAction';
 
 export const serveCaseToIrsSequence = [
   clearPdfPreviewUrlAction,
   setWaitingForResponseAction,
-  setWaitingTextAction(
-    'Please stay on this page while we process your request.',
-  ),
   serveCaseToIrsAction,
   {
     error: [setShowModalAction],
