1c9cf0f6d27d1461c915b90ba17809806c5d7c3c
Zachary Rogers <zrogers>
2024-02-13 10:19:49 -0800

    10007: use local storage to force application to open orders in same tab.

--- cypress/helpers/create-order-and-decision.ts
+++ cypress/helpers/create-order-and-decision.ts
@@ -1,7 +1,8 @@
-export function createOrderAndDecision(docketNumber: string) {
-  cy.goToRoute(
-    `/case-detail/${docketNumber}/create-order?documentTitle=Order%20and%20Decision&documentType=Order%20and%20Decision&eventCode=OAD`,
-  );
+export function createOrderAndDecision() {
+  cy.get('[data-testid="case-detail-menu-button"]').click();
+  cy.get('[data-testid="menu-button-create-order"]').click();
+  cy.get('[data-testid="event-code-select"]').select('OAD');
+  cy.get('[data-testid="modal-button-confirm"]').click();
   cy.get('.ql-editor').click();
   cy.get('[data-testid="save-order-button"]').click();
   cy.get('[data-testid="sign-pdf-canvas"]').click();
