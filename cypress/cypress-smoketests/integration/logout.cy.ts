48817e7ba62f608f503354ca77722e0f0084e959
Zachary Rogers <zrogers>
2024-02-16 14:56:04 -0800

    10007: refactor tests to single describe. GHERKIN syntax above tests

--- cypress/cypress-smoketests/integration/logout.cy.ts
+++ cypress/cypress-smoketests/integration/logout.cy.ts
@@ -1,59 +1,53 @@
-import { cypressEnv } from '../../helpers/env/cypressEnvironment';
+import { loginAsDocketClerk } from '../../helpers/auth/login-as-helpers';
 
-describe('Given a user is logged in to DAWSON on a desktop', () => {
+describe('logout', () => {
   beforeEach(() => {
     Cypress.session.clearCurrentSessionData();
   });
 
-  describe('When the user clicks logout', () => {
-    it('Then they should be taken to the login page', () => {
-      const email = 'docketclerk1@example.com';
-      const password = cypressEnv.defaultAccountPass;
-      cy.visit('/login');
-      cy.get('[data-testid="email-input"]').type(email);
-      cy.get('[data-testid="password-input"]').type(password, {
-        log: false,
-      });
-      cy.get('[data-testid="login-button"]').click();
-      cy.get('[data-testid="messages-banner"]');
-      cy.get('[data-testid="account-menu-button"]').click();
-      cy.get('[data-testid="logout-button-desktop"]').click();
-      cy.get('[data-testid="login-header"]');
-      cy.reload();
-      cy.get('[data-testid="login-header"]');
-      cy.url().should('contain', '/login');
-    });
-
-    describe('And they try to access a case directly as a URL', () => {
-      it('Then they should be taken to the login page', () => {
-        cy.visit('/trial-sessions');
-        cy.get('[data-testid="login-header"]');
-      });
-    });
+  /*
+    Given a user is logged in to DAWSON on a desktop
+    When the user clicks logout
+    Then they should be taken to the login page
+    */
+  it('should route the user to the /login page after they logout', () => {
+    loginAsDocketClerk();
+    cy.get('[data-testid="account-menu-button"]').click();
+    cy.get('[data-testid="logout-button-desktop"]').click();
+    cy.get('[data-testid="login-header"]');
+    cy.reload();
+    cy.get('[data-testid="login-header"]');
+    cy.url().should('contain', '/login');
   });
-});
 
-describe('Given a user is logged in to DAWSON on a mobile device', () => {
-  beforeEach(() => {
+  /*
+    Given a user is logged in to DAWSON on a desktop
+    When the user clicks logout
+    And they try to access a case directly as a URL
+    Then they should be taken to the login page
+    */
+  it('should not allow a user to directly access a protected route after they logout', () => {
+    loginAsDocketClerk();
+    cy.get('[data-testid="account-menu-button"]').click();
+    cy.get('[data-testid="logout-button-desktop"]').click();
+    cy.get('[data-testid="login-header"]');
+    cy.visit('/trial-sessions');
+    cy.get('[data-testid="login-header"]');
+  });
+
+  /*
+  Given a user is logged in to DAWSON on a mobile device
+  When the user clicks logout
+  Then they should be taken to the login page
+  */
+  it('should route the user to the /login page after they logout on a mobile device', () => {
     cy.viewport('iphone-6');
-  });
-  describe('When the user clicks logout', () => {
-    it('Then they should be taken to the login page', () => {
-      const email = 'docketclerk1@example.com';
-      const password = cypressEnv.defaultAccountPass;
-      cy.visit('/login');
-      cy.get('[data-testid="email-input"]').type(email);
-      cy.get('[data-testid="password-input"]').type(password, {
-        log: false,
-      });
-      cy.get('[data-testid="login-button"]').click();
-      cy.get('[data-testid="messages-banner"]');
-      cy.get('[data-testid="account-menu-button-mobile"]').click();
-      cy.get('[data-testid="logout-button-mobile"]').click();
-      cy.get('[data-testid="login-header"]');
-      cy.reload();
-      cy.get('[data-testid="login-header"]');
-      cy.url().should('contain', '/login');
-    });
+    loginAsDocketClerk();
+    cy.get('[data-testid="account-menu-button-mobile"]').click();
+    cy.get('[data-testid="logout-button-mobile"]').click();
+    cy.get('[data-testid="login-header"]');
+    cy.reload();
+    cy.get('[data-testid="login-header"]');
+    cy.url().should('contain', '/login');
   });
 });
