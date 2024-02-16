b233a33c6bb58dfff0b8c43c9b7a730eefd15b7f
Jim Lerza <james.lerza@ustaxcourt.gov>
2024-02-16 12:59:44 -0500

    devex-1218: when moving migration records to the source table, only copy migration records that are still relevant (but still delete all of them from the deploy table)

--- scripts/run-once-scripts/move-migration-records-to-main-table.ts
+++ scripts/run-once-scripts/move-migration-records-to-main-table.ts
@@ -1,8 +1,9 @@
 // usage: npx ts-node --transpile-only scripts/run-once-scripts/move-migration-records-to-main-table.ts
 
+import { AttributeValue, DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
 import { BatchWriteCommand, DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
-import { DynamoDBClient, ScanCommand } from '@aws-sdk/client-dynamodb';
 import { chunk } from 'lodash';
+import { getMigrationFiles } from '../../web-api/workflow-terraform/migration/bin/migrationFilesHelper';
 import { requireEnvVars } from '../../shared/admin-tools/util';
 import { unmarshall } from '@aws-sdk/util-dynamodb';
 
@@ -11,24 +12,38 @@ requireEnvVars(['ENV', 'SOURCE_TABLE']);
 const environment = process.env.ENV!;
 const mainTable = process.env.SOURCE_TABLE!;
 const deployTable = `efcms-deploy-${environment}`;
+const migrationFiles = getMigrationFiles();
+
+const dynamodb = new DynamoDBClient({ region: 'us-east-1' });
+const documentClient = DynamoDBDocument.from(dynamodb);
+
+const scanFull = async ({ TableName }: { TableName: string }) => {
+  let items: Record<string, AttributeValue>[] = [];
+  let hasMoreResults = true;
+  let lastKey: Record<string, AttributeValue> | undefined = undefined;
+
+  while (hasMoreResults) {
+    hasMoreResults = false;
+    const nextScanCommand = new ScanCommand({ ExclusiveStartKey: lastKey, TableName });
+    const results = await documentClient.send(nextScanCommand);
+    hasMoreResults = !!results.LastEvaluatedKey;
+    lastKey = results.LastEvaluatedKey;
+    items = [...items, ...results.Items];
+  }
+
+  return items;
+};
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
-  const dynamodb = new DynamoDBClient({ region: 'us-east-1' });
-  const documentClient = DynamoDBDocument.from(dynamodb);
+  const items = await scanFull({ TableName: deployTable });
 
-  const scanCommand = new ScanCommand({
-    TableName: deployTable,
-  });
-
-  const { Items } = await documentClient.send(scanCommand);
-
-  if (!Items || Items.length === 0) {
+  if (!items || items.length === 0) {
     console.error(`${deployTable} is empty. Exiting.`);
     process.exit(1);
   }
 
-  const migrationRecords = Items.filter(item =>
+  const migrationRecords = items.filter(item =>
     item.pk.S?.includes('migration|'),
   );
 
@@ -39,34 +54,46 @@ const deployTable = `efcms-deploy-${environment}`;
 
   const mrChunks = chunk(migrationRecords, 25);
   const commands: BatchWriteCommand[] = [];
+  let moved = 0;
+  let deleted = 0;
   for (const mrChunk of mrChunks) {
-    const putRequests = mrChunk.map(record => ({
+    const relevantMigrationRecords = mrChunk.filter(record => {
+      const filename = record.sk.S?.split('|')[1] || '';
+      return migrationFiles.includes(filename);
+    });
+    moved += relevantMigrationRecords.length;
+    const putRequests = relevantMigrationRecords.map(record => ({
       PutRequest: {
         Item: { ...unmarshall(record), pk: 'migration' },
       },
     }));
-
-    commands.push(new BatchWriteCommand({
-      RequestItems: {
-        [mainTable]: putRequests,
-      },
-    }));
+    if (putRequests.length > 0) {
+      commands.push(new BatchWriteCommand({
+        RequestItems: {
+          [mainTable]: putRequests,
+        },
+      }));
+    }
 
     const deleteRequests = mrChunk.map(record => ({
       DeleteRequest: {
         Key: { pk: record.pk.S, sk: record.sk.S },
       },
     }));
-    commands.push(new BatchWriteCommand({
-      RequestItems: {
-        [deployTable]: deleteRequests,
-      },
-    }));
+    deleted += mrChunk.length;
+    if (deleteRequests.length > 0) {
+      commands.push(new BatchWriteCommand({
+        RequestItems: {
+          [deployTable]: deleteRequests,
+        },
+      }));
+    }
   }
 
   await Promise.all(commands.map(command => documentClient.send(command)));
 
   console.log(
-    `Migrated ${migrationRecords.length} records to ${mainTable} and deleted them from ${deployTable}.`,
+    `Migrated ${moved} record${moved > 1 ? 's' : ''} to ${mainTable} and ` +
+      `deleted ${deleted} record${deleted > 1 ? 's' : ''} from ${deployTable}.`,
   );
 })();
--- web-api/workflow-terraform/migration/bin/is-migration-needed.ts
+++ web-api/workflow-terraform/migration/bin/is-migration-needed.ts
@@ -1,8 +1,7 @@
 import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
-import { requireEnvVars } from '../shared/admin-tools/util';
-import fs from 'fs';
-import path from 'path';
+import { getMigrationFiles } from './migrationFilesHelper';
+import { requireEnvVars } from '../../../../shared/admin-tools/util';
 
 requireEnvVars(['SOURCE_TABLE']);
 
@@ -10,16 +9,6 @@ const { SOURCE_TABLE } = process.env;
 
 const dynamodb = new DynamoDBClient({ region: 'us-east-1' });
 const docClient = DynamoDBDocument.from(dynamodb);
-
-const getFilesInDirectory = dir => {
-  const files = fs.readdirSync(dir);
-  return files.filter(
-    file =>
-      !file.endsWith('.test.js') &&
-      !file.endsWith('.test.ts') &&
-      !file.startsWith('0000'),
-  );
-};
 
 const hasMigrationRan = async key => {
   const getItemCommand = new GetItemCommand({
@@ -35,12 +24,7 @@ const hasMigrationRan = async key => {
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
-  const migrationFiles = getFilesInDirectory(
-    path.join(
-      __dirname,
-      './workflow-terraform/migration/main/lambdas/migrations',
-    ),
-  );
+  const migrationFiles = getMigrationFiles();
   for (let migrationFile of migrationFiles) {
     const hasRan = await hasMigrationRan(migrationFile);
     if (!hasRan) {
