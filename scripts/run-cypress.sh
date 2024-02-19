652435ecd44863e7c8e466679e12afe253b75f8b
Zachary Rogers <zrogers>
2024-02-19 08:48:34 -0800

    10007: remove unused variable

--- scripts/run-cypress.sh
+++ scripts/run-cypress.sh
@@ -88,7 +88,6 @@ fi
 CONFIG_FILE="cypress${SMOKETESTS}${READONLY}${PUBLIC}.config.ts"
 echo "${CONFIG_FILE}"
 
-export CYPRESS_STAGE=$STAGE
 if [ -n "${INTEGRATION}" ]; then
   echo "Running integration tests."
   export CYPRESS_TEMP_DOCUMENTS_BUCKET_NAME=noop-temp-documents-local-us-east-1
