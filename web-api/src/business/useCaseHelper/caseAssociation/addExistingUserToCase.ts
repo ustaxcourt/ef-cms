ece2fdadd1df4f71a8f59dac3e47a316ff067dea
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-02-20 15:33:02 -0600

    10007: bug investigation

--- web-api/src/business/useCaseHelper/caseAssociation/addExistingUserToCase.ts
+++ web-api/src/business/useCaseHelper/caseAssociation/addExistingUserToCase.ts
@@ -37,6 +37,8 @@ export const addExistingUserToCase = async ({
     throw new Error(`no user found with the provided email of ${email}`);
   }
 
+  // 10007 bug here
+
   const userToAdd = await applicationContext
     .getPersistenceGateway()
     .getUserById({
