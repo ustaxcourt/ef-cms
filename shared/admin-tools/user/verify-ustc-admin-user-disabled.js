b4b9ab2a728554ba10b98d9382d48f828a3cbb9e
Jim Lerza <james.lerza@ustaxcourt.gov>
2024-02-22 14:16:10 -0500

    devex-1197: es6 import syntax

--- shared/admin-tools/user/verify-ustc-admin-user-disabled.js
+++ shared/admin-tools/user/verify-ustc-admin-user-disabled.js
@@ -1,5 +1,6 @@
-const { verifyAdminUserDisabled } = require('./admin');
+import { verifyAdminUserDisabled } from './admin';
 
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
   console.log('== Verifying USTC admin user is disabled');
   await verifyAdminUserDisabled({ attempt: 0 });
