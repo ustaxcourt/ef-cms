34490a6155e4fb2f1f5d856d01276d9e4f95be94
Jim Lerza <james.lerza@ustaxcourt.gov>
2024-02-26 14:47:27 -0500

    devex-1197: convert find-cases-linked-to-practitioners script to typescript

---
+++ scripts/find-cases-linked-to-practitioners.ts
@@ -0,0 +1,118 @@
+// usage: npx ts-node --transpile-only scripts/run-once-scripts/find-petitioners-missing-cases.ts
+
+import { createApplicationContext } from '@web-api/applicationContext';
+import { searchAll } from '@web-api/persistence/elasticsearch/searchClient';
+import type { RawPractitioner } from '@shared/business/entities/Practitioner';
+
+const getOpenCases = async ({
+  applicationContext,
+}: {
+  applicationContext: IApplicationContext;
+}): Promise<RawCase[]> => {
+  const { results } = await searchAll({
+    applicationContext,
+    searchParameters: {
+      body: {
+        _source: ['docketNumber', 'petitioners'],
+        query: {
+          bool: {
+            must_not: [
+              {
+                terms: { 'status.S': ['Closed', 'Closed - Dismissed'] },
+              },
+            ],
+          },
+        },
+      },
+      index: 'efcms-case',
+    },
+  });
+
+  return results as unknown as RawCase[];
+};
+
+// const isPractitionerInCognito = async email => {
+//   try {
+//     const user = await cognito
+//       .adminGetUser({
+//         UserPoolId: cognitoPoolId,
+//         Username: email,
+//       })
+//       .promise();
+//     if (
+//       user.UserAttributes.find(attribute => attribute.Name === 'custom:role')
+//         .Value === 'privatePractitioner'
+//     ) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   } catch (err) {
+//     return false;
+//   }
+// };
+
+const getPrivatePractitionersOnCase = async ({
+  applicationContext,
+  docketNumber,
+}: {
+  applicationContext: IApplicationContext;
+  docketNumber: string;
+}): Promise<RawPractitioner[]> => {
+  const result = await applicationContext.getDocumentClient().query({
+    ExpressionAttributeNames: {
+      '#pk': 'pk',
+      '#sk': 'sk',
+    },
+    ExpressionAttributeValues: {
+      ':pk': `case|${docketNumber}`,
+      ':prefix': 'privatePractitioner',
+    },
+    KeyConditionExpression: '#pk = :pk and begins_with(#sk, :prefix)',
+    TableName: applicationContext.environment.dynamoDbTableName,
+  });
+  if ('Items' in result && result.Items) {
+    return result.Items as unknown as RawPractitioner[];
+  }
+  return [];
+};
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+(async () => {
+  const applicationContext = createApplicationContext({});
+  const allOpenCases = await getOpenCases({ applicationContext });
+
+  console.log(`found ${allOpenCases.length} open cases`);
+  let i = 1;
+
+  for (let openCase of allOpenCases) {
+    console.log(`case ${i++} / ${allOpenCases.length}`);
+    const primaryPetitioner = openCase.petitioners.find(
+      p => p.contactType === 'primary',
+    );
+    if (
+      primaryPetitioner &&
+      'email' in primaryPetitioner &&
+      primaryPetitioner.email
+    ) {
+      const { email } = primaryPetitioner;
+      const practitioners = await getPrivatePractitionersOnCase({
+        applicationContext,
+        docketNumber: openCase.docketNumber,
+      });
+      if (practitioners.find(practitioner => practitioner.email === email)) {
+        console.log(
+          `found a practitioner on case ${openCase.docketNumber} that matches contactPrimary.email of ${email}`,
+        );
+      }
+      // const isEmailActuallyAPractitionerEmail = await isPractitionerInCognito(
+      //   email,
+      // );
+      // if (isEmailActuallyAPractitionerEmail) {
+      //   console.log(
+      //     `case of ${openCase.docketNumber} is bad: ${email} should be removed from contactPrimary`,
+      //   );
+      // }
+    }
+  }
+})();
