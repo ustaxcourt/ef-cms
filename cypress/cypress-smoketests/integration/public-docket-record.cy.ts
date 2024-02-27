588cb9ade02d22ac8444e159a465b8687f72c111
Rachel Schneiderman <rschneiderman379@gmail.com>
2024-02-27 09:47:51 -0800

    10270: more work on public docket record test

--- cypress/cypress-smoketests/integration/public-docket-record.cy.ts
+++ cypress/cypress-smoketests/integration/public-docket-record.cy.ts
@@ -1,9 +1,17 @@
 import { createAndServePaperPetition } from '../../helpers/create-and-serve-paper-petition';
 
+const getPublicSiteUrl = () => {
+  return `https://${process.env.EFCMS_DOMAIN}` || 'http://localhost:5678';
+};
+
 describe('Public Docket Record', () => {
   it('should allow the user to generate and download a PDF of the docket record', () => {
     createAndServePaperPetition().then(({ docketNumber }) => {
-      cy.visit('/');
+      cy.get('[data-testid="account-menu-button"]').click();
+      // cy.get('[data-testid="logout-button-desktop"]').click();
+      cy.get('[id="log-out"]').last().click();
+
+      cy.visit(getPublicSiteUrl());
       cy.get('input#docket-number').type(docketNumber);
       cy.get('button#docket-search-button').click();
       cy.get('[data-testid="header-public-case-detail"]').contains(
