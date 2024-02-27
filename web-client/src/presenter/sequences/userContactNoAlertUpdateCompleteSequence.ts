c7d07010ebba30949db9b7629da6dca54eb7deb0
Zachary Rogers <zrogers>
2024-02-27 09:35:57 -0800

    10007: remove socket sequence that is unused.

--- web-client/src/presenter/sequences/userContactNoAlertUpdateCompleteSequence.ts
+++
@@ -1,7 +0,0 @@
-import { setUserAction } from '@web-client/presenter/actions/setUserAction';
-import { unsetUserContactEditProgressAction } from '../actions/unsetUserContactEditProgressAction';
-
-export const userContactNoAlertUpdateCompleteSequence = [
-  setUserAction,
-  unsetUserContactEditProgressAction,
-];
