8febfab4eac2292a7c2aaa632af013a54a78b401
Rachel Schneiderman <rschneiderman379@gmail.com>
2024-02-27 10:49:45 -0800

    10270: Remove function we aren't using anymore

--- cypress/helpers/auth/logout.ts
+++
@@ -1,5 +0,0 @@
-export function logout() {
-  cy.get('[data-testid="account-menu-button"]').click();
-  cy.get('[data-testid="logout-button-desktop"]').click();
-  cy.get('[data-testid="login-header"]');
-}
