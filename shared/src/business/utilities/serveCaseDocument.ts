bc89bcd8fb94aad25f7b5ed05e924fe0127d2a4a
Nechama Krigsman <nkrigsman@flexion.us>
2024-02-28 10:11:41 -0500

    10217: remove file array logic for atp service

--- shared/src/business/utilities/serveCaseDocument.ts
+++ shared/src/business/utilities/serveCaseDocument.ts
@@ -1,5 +1,4 @@
-import { Case } from '../entities/cases/Case';
-import { DocketEntry } from '@shared/business/entities/DocketEntry';
+import { Case } from '@shared/business/entities/cases/Case';
 import { INITIAL_DOCUMENT_TYPES, ROLES } from '../entities/EntityConstants';
 
 export const serveCaseDocument = async ({
@@ -12,52 +11,11 @@ export const serveCaseDocument = async ({
   initialDocumentTypeKey: string;
 }) => {
   const initialDocumentType = INITIAL_DOCUMENT_TYPES[initialDocumentTypeKey];
-  if (
-    initialDocumentType.eventCode ===
-    INITIAL_DOCUMENT_TYPES.attachmentToPetition.eventCode
-  ) {
-    const initialDocketEntries = caseEntity.docketEntries.filter(
-      doc => doc.documentType === initialDocumentType.documentType,
-    );
 
-    const documentServiceCalls = initialDocketEntries.map(
-      initialDocketEntry => {
-        return serveDocuments({
-          applicationContext,
-          caseEntity,
-          initialDocketEntry,
-        });
-      },
-    );
-    try {
-      await Promise.all(documentServiceCalls);
-    } catch (e) {
-      applicationContext.logger.error(
-        'Error sending service case documents to IRS',
-        e,
-      );
-    }
-  } else {
-    const initialDocketEntry = caseEntity.docketEntries.find(
-      doc => doc.documentType === initialDocumentType.documentType,
-    );
-    await serveDocuments({
-      applicationContext,
-      caseEntity,
-      initialDocketEntry,
-    });
-  }
-};
+  const initialDocketEntry = caseEntity.docketEntries.find(
+    doc => doc.documentType === initialDocumentType.documentType,
+  );
 
-const serveDocuments = ({
-  applicationContext,
-  caseEntity,
-  initialDocketEntry,
-}: {
-  applicationContext: IApplicationContext;
-  caseEntity: Case;
-  initialDocketEntry: DocketEntry;
-}) => {
   if (initialDocketEntry && !initialDocketEntry.isMinuteEntry) {
     initialDocketEntry.setAsServed([
       {
@@ -71,7 +29,7 @@ const serveDocuments = ({
       initialDocketEntry.documentType ===
       INITIAL_DOCUMENT_TYPES.petition.documentType
     ) {
-      return applicationContext
+      await applicationContext
         .getUseCaseHelpers()
         .sendIrsSuperuserPetitionEmail({
           applicationContext,
@@ -79,7 +37,7 @@ const serveDocuments = ({
           docketEntryId: initialDocketEntry.docketEntryId,
         });
     } else {
-      return applicationContext.getUseCaseHelpers().sendServedPartiesEmails({
+      await applicationContext.getUseCaseHelpers().sendServedPartiesEmails({
         applicationContext,
         caseEntity,
         docketEntryId: initialDocketEntry.docketEntryId,
