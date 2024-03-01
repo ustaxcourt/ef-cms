2fda86b1c958ddc803dd30b790fe85a1eb93bd1e
Rachel Schneiderman <rschneiderman379@gmail.com>
2024-03-01 13:42:24 -0800

    10007: Create user gateway method for refreshing id token

---
+++ web-api/src/gateways/user/renewIdToken.test.ts
@@ -0,0 +1,39 @@
+import { InitiateAuthCommandOutput } from '@aws-sdk/client-cognito-identity-provider';
+import { applicationContext } from '../../../../shared/src/business/test/createTestApplicationContext';
+import { renewIdToken } from '@web-api/gateways/user/renewIdToken';
+
+describe('renewIdToken', () => {
+  it('should throw an error when initiateAuth does not return an IdToken', async () => {
+    const mockOutput: InitiateAuthCommandOutput = {
+      $metadata: {},
+    };
+    applicationContext
+      .getCognito()
+      .initiateAuth.mockResolvedValueOnce(mockOutput);
+
+    await expect(
+      renewIdToken(applicationContext, {
+        refreshToken: 'some_token',
+      }),
+    ).rejects.toThrow('Id token not present on initiateAuth response');
+  });
+
+  it('should use the provided refresh token to request and return a new id token', async () => {
+    const mockIdToken = 'some_id_token';
+    const mockOutput: InitiateAuthCommandOutput = {
+      $metadata: {},
+      AuthenticationResult: {
+        IdToken: mockIdToken,
+      },
+    };
+    applicationContext
+      .getCognito()
+      .initiateAuth.mockResolvedValueOnce(mockOutput);
+
+    const result = await renewIdToken(applicationContext, {
+      refreshToken: 'some_token',
+    });
+
+    expect(result).toEqual(mockIdToken);
+  });
+});
--- web-api/src/getUserGateway.ts
+++ web-api/src/getUserGateway.ts
@@ -1,5 +1,7 @@
 import { getUserByEmail } from '@web-api/gateways/user/getUserByEmail';
+import { renewIdToken } from '@web-api/gateways/user/renewIdToken';
 
 export const getUserGateway = () => ({
   getUserByEmail,
+  renewIdToken,
 });
