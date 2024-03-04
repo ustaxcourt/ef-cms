7eac07f96aad11da0194cf6232f43defa0d7673a
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-03-04 16:32:13 -0600

    10270: create ruleset in account-specific, set it as active in main api deploy

--- web-api/terraform/template/ses.tf
+++ web-api/terraform/template/ses.tf
@@ -180,6 +180,10 @@ resource "aws_ses_identity_notification_topic" "bounced_service_emails" {
   include_original_headers = true
 }
 
+resource "aws_ses_active_receipt_rule_set" "main" {
+  rule_set_name = var.active_ses_ruleset
+}
+
 resource "aws_ses_receipt_rule" "email_forwarding_rule" {
   name          = "email_forwarding_rule"
   rule_set_name = var.active_ses_ruleset
