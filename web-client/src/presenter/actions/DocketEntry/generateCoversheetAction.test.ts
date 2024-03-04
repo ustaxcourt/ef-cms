4211fd7c5022bc905df3313db4961abacc7ab20b
John Cruz <jcruz@flexion.us>
2024-03-04 09:05:00 -0700

    10260: WIP start mocking in test;

--- web-client/src/presenter/actions/DocketEntry/generateCoversheetAction.test.ts
+++ web-client/src/presenter/actions/DocketEntry/generateCoversheetAction.test.ts
@@ -4,11 +4,32 @@ import { presenter } from '../../presenter-mock';
 import { runAction } from '@web-client/presenter/test.cerebral';
 
 describe('generateCoversheetAction', () => {
+  const TEST_URL = 'TEST_URL';
+  let HTTP_CLIENT_MOCK;
+  let GET_MOCK;
+  let COVERSHEET_RESULTS;
+  let PDF_LIB_MOCK;
+
   beforeAll(() => {
+    applicationContext.getUseCases().getDocumentDownloadUrlInteractor = jest
+      .fn()
+      .mockReturnValue({ url: TEST_URL });
+
+    GET_MOCK = jest.fn();
+    HTTP_CLIENT_MOCK = {
+      get: GET_MOCK,
+    };
+
+    applicationContext.getHttpClient = () => HTTP_CLIENT_MOCK;
+    applicationContext.getUseCases().getCoversheetInteractor = () =>
+      COVERSHEET_RESULTS;
+
+    PDF_LIB_MOCK = {};
+    applicationContext.getPdfLib = () => PDF_LIB_MOCK;
     presenter.providers.applicationContext = applicationContext;
   });
 
-  it('should call addCoversheetInteractor', async () => {
+  it('should download the file and append cover sheet on the pdf', async () => {
     const mockDocketEntryId = '456';
     const mockDocketNumber = '123-45';
     await runAction(generateCoversheetAction, {
