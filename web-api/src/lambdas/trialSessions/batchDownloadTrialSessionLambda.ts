92bab7d3489e31a443d51f270d19ceb026e38065
Zachary Rogers <zrogers>
2024-03-05 14:00:15 -0800

    Add async router and gateway for completing async websocket requests.

--- web-api/src/lambdas/trialSessions/batchDownloadTrialSessionLambda.ts
+++ web-api/src/lambdas/trialSessions/batchDownloadTrialSessionLambda.ts
@@ -1,47 +1,15 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
-import { InvokeCommand, LambdaClient } from '@aws-sdk/client-lambda';
-import { createApplicationContext } from '@web-api/applicationContext';
 import { genericHandler } from '../../genericHandler';
+import { getAsyncGateway } from '@web-api/asyncGateway';
 
-/**
- * batch download trial session
- *
- * @param {object} event the AWS event object
- * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
- */
 export const batchDownloadTrialSessionLambda = (event: APIGatewayProxyEvent) =>
   genericHandler(event, async ({ applicationContext }) => {
     const { trialSessionId } = event.pathParameters || event.path;
-
-    // const { region } = applicationContext.environment;
-    console.log('******* ' + trialSessionId);
-    const client = new LambdaClient({
-      region: 'us-east-1',
-    });
     const user = applicationContext.getCurrentUser();
-    const command = new InvokeCommand({
-      FunctionName:
-        'zach-ef-cms-api-Site-batchDownloadFunctionAE7C1BEB-OA4iWlDSKM47',
-      InvocationType: 'Event',
-      Payload: JSON.stringify({ payload: { trialSessionId }, user }),
+
+    await getAsyncGateway().runAsync(applicationContext, {
+      payload: { trialSessionId },
+      type: 'BATCH_DOWNLOAD_TRIAL_SESSION',
+      user,
     });
-    console.log('******* sending command to lambda');
-    await client.send(command);
-    console.log('******* done sending command');
   });
-
-export const batchDownloadTrialSessionHandler = async (event: {
-  user: any;
-  payload: {
-    trialSessionId: string;
-  };
-}) => {
-  console.log(event);
-  const applicationContext = createApplicationContext(event.user);
-
-  await applicationContext
-    .getUseCases()
-    .batchDownloadTrialSessionInteractor(applicationContext, event.payload);
-
-  return null;
-};
