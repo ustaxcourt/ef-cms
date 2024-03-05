9ac60835b95ac7d72956de064a3918578a05427c
Absolutestunna <joshujah007@gmail.com>
2024-03-05 13:11:32 -0500

    10068 - WIP adding UI elements and some basic interactions

--- web-client/src/presenter/computeds/docketRecordHelper.ts
+++ web-client/src/presenter/computeds/docketRecordHelper.ts
@@ -1,12 +1,11 @@
+import { Get } from 'cerebral';
 import { state } from '@web-client/presenter/app.cerebral';
 
-import { Get } from 'cerebral';
 export const docketRecordHelper = (get: Get): any => {
   const permissions = get(state.permissions);
   const showPrintableDocketRecord = get(
     state.caseDetail.canAllowPrintableDocketRecord,
   );
-
   const docketRecordSort = get(state.sessionMetadata.docketRecordSort);
   const docketNumber = get(state.caseDetail.docketNumber);
 
@@ -18,7 +17,12 @@ export const docketRecordHelper = (get: Get): any => {
 
   const sortLabelTextMobile = sortLabelsMobile[sortOrder];
 
+  const isDownloadButtonLinkDisabled = false;
+  const showDownloadLink = showPrintableDocketRecord; // question: shoud showPrintableDocketRecord be the rule for displaying downloads as well?
+
   return {
+    isDownloadButtonLinkDisabled,
+    showDownloadLink,
     showEditOrSealDocketRecordEntry:
       permissions.EDIT_DOCKET_ENTRY || permissions.SEAL_DOCKET_ENTRY,
     showPrintableDocketRecord,
