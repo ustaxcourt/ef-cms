da5b39d26f12b8368802d46c1ac6efedb00af596
John Cruz <jcruz@flexion.us>
2024-03-05 15:09:33 -0700

    10249: Add test around getAllCustomCaseReportDataAction;

---
+++ web-client/src/presenter/actions/CaseInventoryReport/getAllCustomCaseReportDataAction.test.ts
@@ -0,0 +1,97 @@
+import { applicationContextForClient as applicationContext } from '@web-client/test/createClientTestApplicationContext';
+import { getAllCustomCaseReportDataAction } from '@web-client/presenter/actions/CaseInventoryReport/getAllCustomCaseReportDataAction';
+import { presenter } from '../../presenter-mock';
+import { runAction } from '@web-client/presenter/test.cerebral';
+
+describe('getAllCustomCaseReportDataAction', () => {
+  let counter = 1;
+
+  beforeEach(() => {
+    counter = 1;
+    applicationContext.getUseCases().getCustomCaseReportInteractor = jest
+      .fn()
+      .mockImplementation(() => {
+        const results = {
+          foundCases: [
+            {
+              highPriority: counter !== 1,
+              receivedAt: counter,
+              testProp: 'John Cruz' + counter,
+            },
+          ],
+          lastCaseId: { pk: counter.toString(), receivedAt: counter },
+        };
+
+        counter = counter + 1;
+
+        return results;
+      });
+
+    presenter.providers.applicationContext = applicationContext;
+  });
+
+  it('should test', async () => {
+    const result = await runAction(getAllCustomCaseReportDataAction, {
+      modules: {
+        presenter,
+      },
+      state: {
+        customCaseReport: {
+          filters: {
+            endDate: '12/30/2024',
+            judges: ['John The Best Judger to Judge'],
+            startDate: '12/30/1960',
+            testFilterOne: 'TEST_FILTERS_1',
+            testFilterTwo: 'TEST_FILTERS_2',
+          },
+          totalCases: 9001,
+        },
+        judges: [
+          {
+            name: 'John The Best Judger to Judge',
+            userId: 1,
+          },
+        ],
+      },
+    });
+
+    const getCustomCaseReportInteractorCalls =
+      applicationContext.getUseCases().getCustomCaseReportInteractor.mock.calls;
+
+    expect(getCustomCaseReportInteractorCalls.length).toEqual(2);
+    expect(getCustomCaseReportInteractorCalls[0][1]).toEqual({
+      endDate: '2024-12-31T04:59:59.999Z',
+      judges: [1],
+      pageSize: 9000,
+      searchAfter: { pk: '', receivedAt: 0 },
+      startDate: '1960-12-30T05:00:00.000Z',
+      testFilterOne: 'TEST_FILTERS_1',
+      testFilterTwo: 'TEST_FILTERS_2',
+    });
+
+    expect(getCustomCaseReportInteractorCalls[1][1]).toEqual({
+      endDate: '2024-12-31T04:59:59.999Z',
+      judges: [1],
+      pageSize: 9000,
+      searchAfter: { pk: '1', receivedAt: 1 },
+      startDate: '1960-12-30T05:00:00.000Z',
+      testFilterOne: 'TEST_FILTERS_1',
+      testFilterTwo: 'TEST_FILTERS_2',
+    });
+
+    expect(result.output.formattedCases).toEqual([
+      {
+        calendaringHighPriority: '',
+        highPriority: false,
+        receivedAt: 'Invalid DateTime',
+        testProp: 'John Cruz1',
+      },
+      {
+        calendaringHighPriority: 'yes',
+        highPriority: true,
+        receivedAt: 'Invalid DateTime',
+        testProp: 'John Cruz2',
+      },
+    ]);
+  });
+});
