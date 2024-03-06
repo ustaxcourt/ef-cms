42af150af49d5685256edd65934b5a6aea4a4c73
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-03-06 16:02:47 -0600

    10270: remove arg from script usage

--- scripts/ses/get-ses-ruleset.sh
+++ scripts/ses/get-ses-ruleset.sh
@@ -3,7 +3,7 @@
 # Returns active ruleset for an AWS SES account
 
 # Usage
-#   ./get-ses-ruleset.sh dev
+#   ./get-ses-ruleset.sh
 
 ( ! command -v jq > /dev/null ) && echo "jq must be installed on your machine." && exit 1
 
