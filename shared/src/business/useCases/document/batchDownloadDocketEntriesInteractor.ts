6b43d501715f49577e29249c3a4a61eb47be909b
Absolutestunna <joshujah007@gmail.com>
2024-03-06 18:31:44 -0500

    10068 - WIP add sudo code for how to zip files [skip ci]

--- shared/src/business/useCases/document/batchDownloadDocketEntriesInteractor.ts
+++ shared/src/business/useCases/document/batchDownloadDocketEntriesInteractor.ts
@@ -21,7 +21,45 @@ export const batchDownloadDocketEntriesInteractor = async (
 ) => {
   const user = applicationContext.getCurrentUser();
   console.log('documentIds in BE', documentIds);
-  //   if (!isAuthorized(user, ROLE_PERMISSIONS.TRIAL_SESSIONS)) {
-  //     throw new UnauthorizedError('Unauthorized');
-  //   }
+  if (!isAuthorized(user, ROLE_PERMISSIONS.GET_CASE)) {
+    throw new UnauthorizedError('Unauthorized');
+  }
+
+  try {
+    const documentPdfsFromS3 = await Promise.all(
+      documentIds.map(docId => {
+        console.log('docId', docId);
+        return applicationContext.getPersistenceGateway().getDocument({
+          applicationContext,
+          key: docId,
+          useTempBucket: false,
+        });
+      }),
+    );
+
+    console.log('documentPdfsFromS3', documentPdfsFromS3);
+  } catch (e) {
+    console.error('ERROR FETCHING IDS', e);
+  }
+
+  // fetch all the documents in s3 (applicationContext.getPersistenceGateway().getDocument)
+  // process these docs
+  //    generae file names for each document
+  //    generae pdf titles for each document
+  //    set s3ids (docket entry id) for batching
+  // send websocket message that processing has started
+  //    send batch name
+  //    send case number
+  //    send number of files batches
+  // send request to zip files (applicationContext.getPersistenceGateway().zipDocuments)
+  //     applicationContext,
+  //     fileNames,
+  //     onEntry,
+  //     onError,
+  //     onProgress,
+  //     onUploadStart,
+  //     s3Ids,
+  //     zipName,
+  // create new pdf url for zipped files
+  // send final websocket notification to FE
 };
