43a183f81b9e4cc5ac45a8ec60f630fca1fbbefd
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-03-06 11:41:17 -0600

    10270: clean up

--- cypress/cypress-smoketests/integration/verify-email-receipt.cy.ts
+++ cypress/cypress-smoketests/integration/verify-email-receipt.cy.ts
@@ -9,28 +9,28 @@ import { navigateTo } from '../../cypress-integration/support/pages/maintenance'
 
 if (!Cypress.env('SMOKETESTS_LOCAL') && !Cypress.env('MIGRATE')) {
   describe('Verify verification email', () => {
-    const BUCKET_NAME = Cypress.env('SMOKETEST_BUCKET');
-    const EFCMS_DOMAIN = Cypress.env('EFCMS_DOMAIN');
-    const UNIQUE_TIMESTAMP = Date.now();
-    const TEST_EMAIL = `smoketest+${UNIQUE_TIMESTAMP}@${EFCMS_DOMAIN}`;
+    const bucketName = Cypress.env('SMOKETEST_BUCKET');
+    const emailDomain = Cypress.env('EFCMS_DOMAIN');
+    const uniqueTimestamp = Date.now();
+    const testEmailAddress = `smoketest+${uniqueTimestamp}@${emailDomain}`;
 
     before(() => {
-      cy.task('deleteAllItemsInEmailBucket', BUCKET_NAME);
+      cy.task('deleteAllItemsInEmailBucket', bucketName);
     });
 
     after(() => {
-      cy.task('deleteAllItemsInEmailBucket', BUCKET_NAME);
+      cy.task('deleteAllItemsInEmailBucket', bucketName);
     });
 
     it('should update petitioner email and confirm that a verification email is received by the updated email address', () => {
       navigateTo('petitioner9');
       goToMyAccount();
       clickChangeEmail();
-      changeEmailTo(TEST_EMAIL);
+      changeEmailTo(testEmailAddress);
       clickConfirmModal();
       confirmEmailPendingAlert();
 
-      readItemsAndRetry({ BUCKET_NAME, TEST_EMAIL });
+      readS3InboxAndRetry({ bucketName, testEmailAddress });
     });
   });
 } else {
@@ -41,27 +41,27 @@ if (!Cypress.env('SMOKETESTS_LOCAL') && !Cypress.env('MIGRATE')) {
   });
 }
 
-const readItemsAndRetry = ({
-  BUCKET_NAME,
-  TEST_EMAIL,
+const readS3InboxAndRetry = ({
+  bucketName,
+  testEmailAddress,
 }: {
-  BUCKET_NAME: string;
-  TEST_EMAIL: string;
+  bucketName: string;
+  testEmailAddress: string;
 }) => {
   const MAX_RETRIES = 3;
   let retryCount = 0;
-  cy.task<any[]>('readAllItemsInBucket', BUCKET_NAME).then(items => {
+  cy.task<any[]>('readAllItemsInBucket', bucketName).then(items => {
     if (items.length === 0 && retryCount < MAX_RETRIES) {
       retryCount++;
       // eslint-disable-next-line cypress/no-unnecessary-waiting
       cy.wait(2000);
-      readItemsAndRetry({ BUCKET_NAME, TEST_EMAIL });
+      readS3InboxAndRetry({ bucketName, testEmailAddress });
     } else {
       expect(items).to.have.length(1);
       expect(items[0].content).to.contain(
         'The email on your account has been changed. Once verified, this email will be your log in and where you will receive service.',
       );
-      expect(items[0].content).to.contain(TEST_EMAIL);
+      expect(items[0].content).to.contain(testEmailAddress);
     }
   });
 };
