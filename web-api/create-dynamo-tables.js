0aec513b94971c36b2c85db5d61a43054f4133cc
Jim Lerza <james.lerza@ustaxcourt.gov>
2024-03-07 13:38:39 -0500

    devex-1258: convert create-dynamo-tables to es6 style import/export and aws sdk v3

--- web-api/create-dynamo-tables.js
+++ web-api/create-dynamo-tables.js
@@ -1,6 +1,10 @@
-const AWS = require('aws-sdk');
+import {
+  CreateTableCommand,
+  DeleteTableCommand,
+  DynamoDBClient,
+} from '@aws-sdk/client-dynamodb';
 
-const dynamo = new AWS.DynamoDB({
+const dynamodb = new DynamoDBClient({
   credentials: {
     accessKeyId: 'S3RVER',
     secretAccessKey: 'S3RVER',
@@ -9,13 +13,10 @@ const dynamo = new AWS.DynamoDB({
   region: 'us-east-1',
 });
 
-const deleteTable = async tableName => {
+const deleteTable = async ({ TableName }) => {
+  const deleteTableCommand = new DeleteTableCommand({ TableName });
   try {
-    return await dynamo
-      .deleteTable({
-        TableName: tableName,
-      })
-      .promise();
+    return await dynamodb.send(deleteTableCommand);
   } catch (error) {
     // ResourceNotFoundException
     console.log('no table to delete');
@@ -23,99 +24,99 @@ const deleteTable = async tableName => {
   }
 };
 
-const createEFCMSTable = async tableName => {
-  console.log(`Creating EFCMS Table: ${tableName}`);
-  return dynamo
-    .createTable({
-      AttributeDefinitions: [
-        {
-          AttributeName: 'pk',
-          AttributeType: 'S',
-        },
-        {
-          AttributeName: 'sk',
-          AttributeType: 'S',
-        },
-        {
-          AttributeName: 'gsi1pk',
-          AttributeType: 'S',
-        },
-        {
-          AttributeName: 'gsi2pk',
-          AttributeType: 'S',
-        },
-      ],
-      GlobalSecondaryIndexes: [
-        {
-          IndexName: 'gsi1',
-          KeySchema: [
-            {
-              AttributeName: 'gsi1pk',
-              KeyType: 'HASH',
-            },
-            {
-              AttributeName: 'pk',
-              KeyType: 'RANGE',
-            },
-          ],
-          Projection: {
-            ProjectionType: 'ALL',
-          },
-          ProvisionedThroughput: {
-            ReadCapacityUnits: 1,
-            WriteCapacityUnits: 1,
-          },
-        },
-        {
-          IndexName: 'gsi2',
-          KeySchema: [
-            {
-              AttributeName: 'gsi2pk',
-              KeyType: 'HASH',
-            },
-            {
-              AttributeName: 'sk',
-              KeyType: 'RANGE',
-            },
-          ],
-          Projection: {
-            ProjectionType: 'ALL',
-          },
-          ProvisionedThroughput: {
-            ReadCapacityUnits: 1,
-            WriteCapacityUnits: 1,
-          },
-        },
-      ],
-      KeySchema: [
-        {
-          AttributeName: 'pk',
-          KeyType: 'HASH',
-        },
-        {
-          AttributeName: 'sk',
-          KeyType: 'RANGE',
-        },
-      ],
-      ProvisionedThroughput: {
-        ReadCapacityUnits: 1,
-        WriteCapacityUnits: 1,
+const createTable = ({ TableName }) => {
+  console.log('Creating EFCMS Table:', TableName);
+  const createTableCommand = new CreateTableCommand({
+    AttributeDefinitions: [
+      {
+        AttributeName: 'pk',
+        AttributeType: 'S',
       },
-      StreamSpecification: {
-        StreamEnabled: true,
-        StreamViewType: 'NEW_AND_OLD_IMAGES',
+      {
+        AttributeName: 'sk',
+        AttributeType: 'S',
       },
-      TableName: tableName,
-    })
-    .promise();
+      {
+        AttributeName: 'gsi1pk',
+        AttributeType: 'S',
+      },
+      {
+        AttributeName: 'gsi2pk',
+        AttributeType: 'S',
+      },
+    ],
+    GlobalSecondaryIndexes: [
+      {
+        IndexName: 'gsi1',
+        KeySchema: [
+          {
+            AttributeName: 'gsi1pk',
+            KeyType: 'HASH',
+          },
+          {
+            AttributeName: 'pk',
+            KeyType: 'RANGE',
+          },
+        ],
+        Projection: {
+          ProjectionType: 'ALL',
+        },
+        ProvisionedThroughput: {
+          ReadCapacityUnits: 1,
+          WriteCapacityUnits: 1,
+        },
+      },
+      {
+        IndexName: 'gsi2',
+        KeySchema: [
+          {
+            AttributeName: 'gsi2pk',
+            KeyType: 'HASH',
+          },
+          {
+            AttributeName: 'sk',
+            KeyType: 'RANGE',
+          },
+        ],
+        Projection: {
+          ProjectionType: 'ALL',
+        },
+        ProvisionedThroughput: {
+          ReadCapacityUnits: 1,
+          WriteCapacityUnits: 1,
+        },
+      },
+    ],
+    KeySchema: [
+      {
+        AttributeName: 'pk',
+        KeyType: 'HASH',
+      },
+      {
+        AttributeName: 'sk',
+        KeyType: 'RANGE',
+      },
+    ],
+    ProvisionedThroughput: {
+      ReadCapacityUnits: 1,
+      WriteCapacityUnits: 1,
+    },
+    StreamSpecification: {
+      StreamEnabled: true,
+      StreamViewType: 'NEW_AND_OLD_IMAGES',
+    },
+    TableName,
+  });
+  return dynamodb.send(createTableCommand);
 };
 
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
   try {
-    await deleteTable('efcms-local');
-    await deleteTable('efcms-local-1');
-    await createEFCMSTable('efcms-local');
-    await createEFCMSTable('efcms-local-1');
+    await deleteTable({ TableName: 'efcms-local' });
+    await deleteTable({ TableName: 'efcms-local-1' });
+    await createTable({ TableName: 'efcms-local' });
+    await createTable({ TableName: 'efcms-local-1' });
   } catch (err) {
     console.error(err);
   }
