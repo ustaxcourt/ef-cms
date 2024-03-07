0c78179f80ff234b3cf9452482a3b34392567339
Jim Lerza <james.lerza@ustaxcourt.gov>
2024-03-07 16:00:02 -0500

    devex-1258: typed getUserById

--- web-api/workflow-terraform/migration/main/lambdas/utilities/getUserById.test.ts
+++ web-api/workflow-terraform/migration/main/lambdas/utilities/getUserById.test.ts
@@ -1,17 +1,22 @@
-import { docketClerk1User } from '../../../../../../shared/src/test/mockUsers';
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
+import { docketClerk1User } from '@shared/test/mockUsers';
 import { getUserById } from './getUserById';
+
+const mockUser = {
+  ...docketClerk1User,
+  pk: `user|${docketClerk1User.userId}`,
+  sk: `user|${docketClerk1User.userId}`,
+};
 
 describe('getUserById', () => {
   let documentClient;
 
   beforeEach(() => {
     documentClient = {
-      get: jest.fn().mockReturnValueOnce({
-        promise: () => ({
-          Item: docketClerk1User,
-        }),
+      get: jest.fn().mockResolvedValueOnce({
+        Item: mockUser,
       }),
-    };
+    } as unknown as DynamoDBDocument;
   });
 
   it('should make a call to persistence to retrieve a user by userId', async () => {
@@ -30,6 +35,6 @@ describe('getUserById', () => {
   it('should return the user record when found in persistence', async () => {
     const result = await getUserById(documentClient, docketClerk1User.userId);
 
-    expect(result).toEqual({ Item: docketClerk1User });
+    expect(result).toEqual(mockUser);
   });
 });
