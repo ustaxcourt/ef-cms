d16aa3d2da356b638f2e9425be2fa7eae36663ee
Absolutestunna <joshujah007@gmail.com>
2024-03-07 18:30:24 -0500

    10068 - clean up in batchDownloadDocketEntriesInteractor

--- shared/src/business/useCases/document/batchDownloadDocketEntriesInteractor.ts
+++ shared/src/business/useCases/document/batchDownloadDocketEntriesInteractor.ts
@@ -1,5 +1,5 @@
-import { Case, isClosed } from '../../entities/cases/Case';
-import { NotFoundError } from '../../../../../web-api/src/errors/errors';
+import { Case } from '../../entities/cases/Case';
+// import { NotFoundError } from '../../../../../web-api/src/errors/errors';
 import {
   ROLE_PERMISSIONS,
   isAuthorized,
@@ -33,42 +33,24 @@ export const batchDownloadDocketEntriesInteractor = async (
     throw new UnauthorizedError('Unauthorized');
   }
 
-  let extraFiles: any = [];
-  let extraFileNames: string[] = [];
   let s3Ids: string[] = [];
   let fileNames: string[] = [];
 
   const zipName = 'case-documents.zip';
   const { caseCaption, clientConnectionId, docketEntries, docketNumber } =
     requestParams;
-  // things to bring into the BE
-  // 1. Case - caseCaption, docketNumber (caseTitle)
-  // 2. Docket Entry
-  //    - isOnDocketRecord, isFileAttached, docketEntryId (documentId), filingDate, index, documentTitle
-  //    - use `getDocketEntryOnCase` to get docket entry and get the appropriate meta data
 
   try {
     await Promise.all(
       docketEntries.map(docEntry => {
         const docId = docEntry.docketEntryId;
-        console.log('docId', docEntry.docketEntryId);
         const filename = generateValidDocketEntryFilename(docEntry); // move to utility function
         const caseTitle = Case.getCaseTitle(caseCaption);
         const caseFolder = `${docketNumber}, ${caseTitle}`;
-
         const pdfTitle = `${caseFolder}/${filename}`;
-        const doc = applicationContext.getPersistenceGateway().getDocument({
-          applicationContext,
-          key: docId,
-          useTempBucket: false,
-        });
-
-        console.log('doc', doc);
 
         s3Ids.push(docId);
         fileNames.push(pdfTitle);
-        extraFiles.push(doc);
-        extraFileNames.push(`${docId}/0_Docket Record.pdf`);
       }),
     );
   } catch (e) {
@@ -160,31 +142,4 @@ export const batchDownloadDocketEntriesInteractor = async (
     },
     userId: user.userId,
   });
-  // }
-  // catch (error) {
-  //   applicationContext.logger.error('Error zipping documents', {
-  //     error,
-  //   });
-  // }
-
-  // fetch all the documents in s3 (applicationContext.getPersistenceGateway().getDocument)
-  // process some data about these docs
-  //    generae file names for each document
-  //    generae pdf titles for each document
-  //    set s3ids (docket entry id) for batching
-  // send websocket message that processing has started
-  //    send batch name
-  //    send case number
-  //    send number of files batches
-  // send request to zip files (applicationContext.getPersistenceGateway().zipDocuments)
-  //     applicationContext,
-  //     fileNames,
-  //     onEntry,
-  //     onError,
-  //     onProgress,
-  //     onUploadStart,
-  //     s3Ids,
-  //     zipName,
-  // create new pdf url for zipped files
-  // send final websocket notification to FE
 };
