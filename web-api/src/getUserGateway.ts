10eabb5860c138077314ad8a01e3b62fadf3968b
Rachel Schneiderman <rschneiderman379@gmail.com>
2024-03-08 15:03:43 -0800

    10007: Extract forgot password cognito call to user gateway

--- web-api/src/getUserGateway.ts
+++ web-api/src/getUserGateway.ts
@@ -1,5 +1,6 @@
 import { changePassword } from '@web-api/gateways/user/changePassword';
 import { disableUser } from '@web-api/gateways/user/disableUser';
+import { forgotPassword } from '@web-api/gateways/user/forgotPassword';
 import { getUserByEmail } from '@web-api/gateways/user/getUserByEmail';
 import { initiateAuth } from '@web-api/gateways/user/initiateAuth';
 import { renewIdToken } from '@web-api/gateways/user/renewIdToken';
@@ -7,6 +8,7 @@ import { renewIdToken } from '@web-api/gateways/user/renewIdToken';
 export const getUserGateway = () => ({
   changePassword,
   disableUser,
+  forgotPassword,
   getUserByEmail,
   initiateAuth,
   renewIdToken,
