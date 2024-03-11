f19a2e1e0c861f2d102835e831ebcedd03aff831
Rachel Schneiderman <rschneiderman379@gmail.com>
2024-03-11 08:09:09 -0700

    10007: Extract adminCreateUser to createUser function in user gateway

--- web-api/src/getUserGateway.ts
+++ web-api/src/getUserGateway.ts
@@ -1,4 +1,5 @@
 import { changePassword } from '@web-api/gateways/user/changePassword';
+import { createUser } from '@web-api/gateways/user/createUser';
 import { disableUser } from '@web-api/gateways/user/disableUser';
 import { forgotPassword } from '@web-api/gateways/user/forgotPassword';
 import { getUserByEmail } from '@web-api/gateways/user/getUserByEmail';
@@ -8,6 +9,7 @@ import { updateUser } from '@web-api/gateways/user/updateUser';
 
 export const getUserGateway = () => ({
   changePassword,
+  createUser,
   disableUser,
   forgotPassword,
   getUserByEmail,
