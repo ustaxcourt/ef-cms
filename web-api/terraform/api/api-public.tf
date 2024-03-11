4c0e844fbd0bca7bd18bba57ad6750a7944b79ac
Cody Seibert <cseibert113@gmail.com>
2024-03-11 13:35:37 -0400

    refactoring all the lambdas to use the new module

--- web-api/terraform/api/api-public.tf
+++ web-api/terraform/api/api-public.tf
@@ -9,11 +9,22 @@ module "api_public_lambda" {
   memory_size    = "3008"
 }
 
+module "public_api_authorizer_lambda" {
+  source         = "./lambda"
+  handler        = "./web-api/terraform/template/lambdas/public-api-authorizer.ts"
+  handler_method = "handler"
+  lambda_name    = "public_api_authorizer_lambda_${var.environment}_${var.current_color}"
+  role           =  "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/public_api_authorizer_role_${var.environment}"
+  environment    = var.lambda_environment
+  timeout        = "29"
+  memory_size    = "3008"
+}
+
 
 resource "aws_api_gateway_authorizer" "public_authorizer" {
   name                             = "public_authorizer_${var.environment}_${var.current_color}"
   rest_api_id                      = aws_api_gateway_rest_api.gateway_for_api_public.id
-  authorizer_uri                   = var.public_authorizer_uri
+  authorizer_uri                   = module.public_api_authorizer_lambda.invoke_arn
   type                             = "REQUEST"
   identity_source                  = "context.identity.sourceIp"
   authorizer_result_ttl_in_seconds = 300
--- web-api/terraform/api/lambda/outputs.tf
+++ web-api/terraform/api/lambda/outputs.tf
@@ -6,3 +6,11 @@ output "invoke_arn" {
 output "function_name" {
   value = aws_lambda_function.lambda_function.function_name
 }
+
+output "arn" {
+  value = aws_lambda_function.lambda_function.arn
+}
+
+output "function_name" {
+  value = aws_lambda_function.lambda_function.function_name
+}
