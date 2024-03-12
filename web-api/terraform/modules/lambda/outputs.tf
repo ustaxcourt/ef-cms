2a47ea93e6af98dc8957f0ae5085f59fd2dab695
Zachary Rogers <zrogers>
2024-03-12 07:34:16 -0700

    Update file deletion

--- web-api/terraform/modules/lambda/outputs.tf
+++ web-api/terraform/modules/lambda/outputs.tf
@@ -10,7 +10,3 @@ output "function_name" {
 output "arn" {
   value = aws_lambda_function.lambda_function.arn
 }
-
-output "function_name" {
-  value = aws_lambda_function.lambda_function.function_name
-}
