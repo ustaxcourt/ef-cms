16209b4a1bfb4ed86ea18445c17f8430daabaf6a
Zachary Rogers <zrogers>
2024-03-14 13:01:17 -0700

    10007: reduce concurrency

--- web-client/pa11y/pa11y-ci.base-config.js
+++ web-client/pa11y/pa11y-ci.base-config.js
@@ -16,7 +16,7 @@ const defaults = {
   chromeLaunchConfig: {
     args: ['--no-sandbox'],
   },
-  concurrency: 8,
+  concurrency: 6,
   debug: false,
   'include-notices': true,
   'include-warnings': true,
