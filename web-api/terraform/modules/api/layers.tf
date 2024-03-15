7bf0360f5cfd922bf7b6f1771502ad4f0dde2b47
Zachary Rogers <zrogers>
2024-03-15 09:54:31 -0700

    devex-1256: Refactor layers to an aws_s3_object instead of always uploading a new object.

--- web-api/terraform/modules/api/layers.tf
+++ web-api/terraform/modules/api/layers.tf
@@ -1,25 +1,15 @@
-resource "null_resource" "puppeteer_layer" {
-  depends_on = [var.lambda_bucket_id]
-  provisioner "local-exec" {
-    command = "aws s3 cp ../../../runtimes/puppeteer/puppeteer_lambda_layer.zip s3://${var.lambda_bucket_id}/${var.current_color}_puppeteer_lambda_layer.zip"
-  }
-
-  triggers = {
-    always_run = timestamp()
-  }
-}
-
-data "aws_s3_object" "puppeteer_object" {
-  depends_on = [null_resource.puppeteer_layer]
-  bucket     = var.lambda_bucket_id
-  key        = "${var.current_color}_puppeteer_lambda_layer.zip"
+resource "aws_s3_object" "puppeteer_object" {
+  bucket = var.lambda_bucket_id
+  key    = "${var.current_color}_puppeteer_lambda_layer.zip"
+  source = "../../../runtimes/puppeteer/puppeteer_lambda_layer.zip"
+  source_hash = filemd5("../../../runtimes/puppeteer/puppeteer_lambda_layer.zip")
 }
 
 resource "aws_lambda_layer_version" "puppeteer_layer" {
   s3_bucket           = var.lambda_bucket_id
   s3_key              = "${var.current_color}_puppeteer_lambda_layer.zip"
   layer_name          = "puppeteer-${var.environment}-${var.current_color}"
-  source_code_hash    = data.aws_s3_object.puppeteer_object.etag
+  source_code_hash    = aws_s3_object.puppeteer_object.source_hash
   compatible_runtimes = [var.node_version]
 }
 
