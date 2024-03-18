620cd2a7e72cbe9338735433048d7b79e0d6067b
Nechama Krigsman <nkrigsman@flexion.us>
2024-03-18 10:42:26 -0400

    10260: add tests around resolveAsyncSyncRequestAction;

---
+++ web-client/src/presenter/actions/resolveAsyncSyncRequestAction.test.ts
@@ -0,0 +1,38 @@
+import { applicationContextForClient as applicationContext } from '@web-client/test/createClientTestApplicationContext';
+import { presenter } from '../presenter-mock';
+import { resolveAsyncSyncRequestAction } from '@web-client/presenter/actions/resolveAsyncSyncRequestAction';
+import { runAction } from 'cerebral/test';
+
+describe('resolveAsyncSyncRequestAction', () => {
+  const asyncSyncCompleterDict = {
+    asyncSyncId1: jest.fn(),
+    asyncSyncId2: jest.fn(),
+  };
+
+  const mockGetAsyncSyncCompleter = jest.fn(id => asyncSyncCompleterDict[id]);
+
+  presenter.providers.applicationContext = {
+    ...applicationContext,
+    getAsynSyncUtil: jest.fn().mockReturnValue({
+      getAsyncSyncCompleter: mockGetAsyncSyncCompleter,
+    }),
+  };
+
+  it('should call callback with response if callback exists', async () => {
+    const asyncSyncId = 'asyncSyncId1';
+    const response = 'testResponse';
+
+    await runAction(resolveAsyncSyncRequestAction as any, {
+      modules: {
+        presenter,
+      },
+      props: {
+        asyncSyncId,
+        response,
+      },
+    });
+
+    expect(mockGetAsyncSyncCompleter).toHaveBeenCalledWith(asyncSyncId);
+    expect(asyncSyncCompleterDict[asyncSyncId]).toHaveBeenCalledWith(response);
+  });
+});
