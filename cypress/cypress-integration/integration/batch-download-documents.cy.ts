a18ffe94034798a9ccc10926ecfe293735e5a059
Absolutestunna <joshujah007@gmail.com>
2024-03-19 18:17:56 -0400

    10068 - adding more cypress tests for batch downloading documents
    Co-authored-by: Jason Fu <jasonfu6699@hotmail.com>

--- cypress/cypress-integration/integration/batch-download-documents.cy.ts
+++ cypress/cypress-integration/integration/batch-download-documents.cy.ts
@@ -1,8 +1,15 @@
 import { createAndServePaperPetition } from '../../helpers/create-and-serve-paper-petition';
+import { externalUserSearchesDocketNumber } from '../../helpers/external-user-searches-docket-number';
+import {
+  loginAsDocketClerk,
+  loginAsPetitioner,
+} from '../../helpers/auth/login-as-helpers';
+import { petitionerCreatesElectronicCase } from '../../helpers/petitioner-creates-electronic-case';
+import { petitionsClerkQcsAndServesElectronicCase } from '../../helpers/petitions-clerk-qcs-and-serves-electronic-case';
 import { searchByDocketNumberInHeader } from '../../helpers/search-by-docket-number-in-header';
 
+// todo: type correctly
 const confirmDownloadOfDocuments = (documentsCreated: {}[]) => {
-  // type correctly
   cy.get('[data-testid="download-docket-records-button"]').should(
     'be.disabled',
   );
@@ -16,43 +23,125 @@ const confirmDownloadOfDocuments = (documentsCreated: {}[]) => {
   );
 };
 
+const includePrintableDocketRecord = () => {
+  cy.get(
+    '[data-testid="include-printable-docket-record-checkbox-checkbox-label"]',
+  ).click();
+};
+
 describe('Batch Download Documents', () => {
   // get from cypress/cypress-integration/integration/custom-case-report-csv-export.cy.ts from 10249 work
   // beforeEach(() => {
   //   const downloadPath = Cypress.config('downloadsFolder');
-  //   cy.task('deleteAllFilesInFolder', downloadPath);
+  // cy.task('deleteAllFilesInFolder', downloadPath);
   // });
 
-  // all cases (not stricken, not sealed)
-  it.skip('should download all eligible documents in a case', () => {
+  it('should download all eligible documents that are not stricken with the printable docket record, with all files in a flat directory', () => {
     createAndServePaperPetition().then(({ docketNumber, documentsCreated }) => {
       searchByDocketNumberInHeader(docketNumber);
       confirmDownloadOfDocuments(documentsCreated);
-      cy.get('#modal-button-confirm').click();
+      includePrintableDocketRecord();
+      cy.get('[data-testid="modal-button-confirm"]').click();
 
-      // how do we confirm download?
+      //TODO: verify download
     });
   });
 
-  // all cases (not stricken, not sealed) with printable docket record
-  it('should download all eligible documents in a case', () => {
+  it('should download all eligible documents with some stricken and some sealed, labeling files and adding sealed docs to sealed directory', () => {
     createAndServePaperPetition().then(({ docketNumber, documentsCreated }) => {
+      loginAsDocketClerk();
       searchByDocketNumberInHeader(docketNumber);
+
+      // strike documents
+      cy.get('[data-testid="edit-ATP"]').click();
+      cy.get('[data-testid="tab-action"]').click();
+      cy.get('[data-testid="strike-entry"]').click();
+      cy.get('[data-testid="modal-button-confirm"]').click();
+
+      cy.get('[data-testid="edit-APW"]').click();
+      cy.get('[data-testid="tab-action"]').click();
+      cy.get('[data-testid="strike-entry"]').click();
+      cy.get('[data-testid="modal-button-confirm"]').click();
+
+      // seal documents
+      cy.get('[data-testid="seal-docket-entry-button-3"]').click();
+      cy.get('[data-testid="modal-button-confirm"]').click();
+
+      cy.get('[data-testid="seal-docket-entry-button-5"]').click();
+      cy.get('[data-testid="modal-button-confirm"]').click();
+
       confirmDownloadOfDocuments(documentsCreated);
+      cy.get('[data-testid="modal-button-confirm"]').click();
+
+      // TODO: verify download
+    });
+  });
+
+  it('should download selected documents, labeling stricken documents and putting everything except the printable docket record into the sealed directory when then the case is sealed', () => {
+    createAndServePaperPetition().then(({ docketNumber, documentsCreated }) => {
+      loginAsDocketClerk();
+      searchByDocketNumberInHeader(docketNumber);
+
+      // strike documents
+      cy.get('[data-testid="edit-ATP"]').click();
+      cy.get('[data-testid="tab-action"]').click();
+      cy.get('[data-testid="strike-entry"]').click();
+      cy.get('[data-testid="modal-button-confirm"]').click();
+
+      cy.get('[data-testid="edit-APW"]').click();
+      cy.get('[data-testid="tab-action"]').click();
+      cy.get('[data-testid="strike-entry"]').click();
+      cy.get('[data-testid="modal-button-confirm"]').click();
+
+      // seal case
+      cy.get('[data-testid="tab-case-information"]').click();
+      cy.get('[data-testid="seal-case-button"]').click();
+      cy.get('[data-testid="modal-button-confirm"]').click();
+      cy.get('[data-testid="tab-docket-record"] > .button-text').click();
+
+      confirmDownloadOfDocuments(documentsCreated);
+      includePrintableDocketRecord();
+
+      // TODO: verify download
+    });
+  });
+
+  it('should download selected documents filtered by file type', () => {
+    createAndServePaperPetition().then(({ docketNumber, documentsCreated }) => {
+      loginAsDocketClerk();
+      searchByDocketNumberInHeader(docketNumber);
+      cy.get('[data-testid="tab-drafts"] > .button-text').click();
+      cy.get('[data-testid="docket-entry-description-1"]').click();
+      cy.get('#apply-signature').click();
+      cy.get('[data-testid="sign-pdf-canvas"]').click();
+      cy.get('[data-testid="save-signature-button"]').click();
+      cy.get('[data-testid="add-court-issued-docket-entry-button"]').click();
+      cy.get('.usa-date-picker__button').click();
       cy.get(
-        '[data-testid="include-printable-docket-record-checkbox-checkbox-label"]',
+        ':nth-child(4) > :nth-child(3) > .usa-date-picker__calendar__date',
       ).click();
-      cy.get('#modal-button-confirm').click();
-      // how do we confirm download?
+      cy.get('[data-testid="serve-to-parties-btn"]').click();
+      cy.get('[data-testid="modal-button-confirm"]').click();
+      cy.get('[data-testid="print-paper-service-done-button"]').click();
+      cy.get('#document-filter-by').select('Orders');
+      confirmDownloadOfDocuments(documentsCreated);
+
+      // TODO: verify download
     });
   });
-  // all cases (no stricken, sealed)
-  // all cases (stricken and sealed)
-  // all cases ((stricken and sealed) with printable docket record
 
-  // selected sealed documents
-  // selected stricken documents
-  // selected sealed and stricken documents
-
-  // electronic case with rqt (will not have a document, worth testing?)
+  it('should not show download controls for external users', () => {
+    loginAsPetitioner();
+    petitionerCreatesElectronicCase().then(docketNumber => {
+      petitionsClerkQcsAndServesElectronicCase(docketNumber);
+      loginAsPetitioner();
+      externalUserSearchesDocketNumber(docketNumber);
+      cy.get('[data-testid="download-docket-records-button"]').should(
+        'not.be.visible',
+      );
+      cy.get('[data-testid="all-selectable-docket-entries-checkbox"]').should(
+        'not.be.visible',
+      );
+    });
+  });
 });
