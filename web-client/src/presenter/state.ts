30d8b6f8acfd1d8696b82764dd32d9ff58e92c15
Nechama Krigsman <nkrigsman@flexion.us>
2024-03-19 22:46:54 -0400

    10249: make property optional

--- web-client/src/presenter/state.ts
+++ web-client/src/presenter/state.ts
@@ -575,7 +575,7 @@ export const baseState = {
     zipInProgress?: boolean;
     totalFiles?: number;
     fileCount?: number;
-    title: string;
+    title?: string;
   },
   caseDeadlineReport: {} as {
     caseDeadlines: (RawCaseDeadline & {
