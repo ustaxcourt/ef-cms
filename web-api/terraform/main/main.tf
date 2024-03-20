9615a8064b54ca4faf892a9c16cf943978a55e71
Rachel Schneiderman <rschneiderman379@gmail.com>
2024-03-20 08:27:30 -0700

    Dependency updates: Fix typo in aws provider version

--- web-api/terraform/main/main.tf
+++ web-api/terraform/main/main.tf
@@ -17,7 +17,7 @@ terraform {
   }
 
   required_providers {
-    aws = "5.40.1"
+    aws = "5.41.0"
   }
 }
 
--- web-api/workflow-terraform/switch-colors-cron/main/main.tf
+++ web-api/workflow-terraform/switch-colors-cron/main/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   required_providers {
-    aws = "5.40.1"
+    aws = "5.41.0"
   }
 }
 
