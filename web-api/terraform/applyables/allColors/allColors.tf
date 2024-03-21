048ff0e6c2a2808b6a19e07e320c39b147635389
Cody Seibert <cseibert113@gmail.com>
2024-03-21 16:43:43 -0400

    devex-1236: create two files for deploying allColors and color

--- web-api/terraform/applyables/allColors/allColors.tf
+++ web-api/terraform/applyables/allColors/allColors.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
 }
 
 provider "aws" {
@@ -26,26 +26,10 @@ data "aws_sns_topic" "system_health_alarms" {
   name = "system_health_alarms"
 }
 
-data "aws_sns_topic" "system_health_alarms_west" {
-  // account-level resource
-  name     = "system_health_alarms"
-  provider = aws.us-west-1
-}
-
 module "ef-cms_apis" {
   source                     = "../../modules/everything-else-deprecated"
   alert_sns_topic_arn        = data.aws_sns_topic.system_health_alarms.arn
-  alert_sns_topic_west_arn   = data.aws_sns_topic.system_health_alarms_west.arn
-  blue_elasticsearch_domain  = var.blue_elasticsearch_domain
-  blue_table_name            = var.blue_table_name
-  bounce_alert_recipients    = var.bounce_alert_recipients
-  bounced_email_recipient    = var.bounced_email_recipient
   cognito_suffix             = var.cognito_suffix
-  cognito_table_name         = var.cognito_table_name
-  default_account_pass       = var.default_account_pass
-  deployment_timestamp       = var.deployment_timestamp
-  destination_table          = var.destination_table
-  disable_emails             = var.disable_emails
   dns_domain                 = var.dns_domain
   email_dmarc_policy         = var.email_dmarc_policy
   enable_health_checks       = var.enable_health_checks
@@ -53,14 +37,9 @@ module "ef-cms_apis" {
   es_instance_count          = var.es_instance_count
   es_instance_type           = var.es_instance_type
   es_volume_size             = var.es_volume_size
-  green_elasticsearch_domain = var.green_elasticsearch_domain
-  green_table_name           = var.green_table_name
-  irs_superuser_email        = var.irs_superuser_email
   lower_env_account_id       = var.lower_env_account_id
   prod_env_account_id        = var.prod_env_account_id
-  scanner_resource_uri       = var.scanner_resource_uri
   should_es_alpha_exist      = var.should_es_alpha_exist
   should_es_beta_exist       = var.should_es_beta_exist
-  slack_webhook_url          = var.slack_webhook_url
   zone_name                  = var.zone_name
 }
--- web-api/terraform/applyables/allColors/variables.tf
+++ web-api/terraform/applyables/allColors/variables.tf
@@ -1,7 +1,3 @@
-variable "aws_region" {
-  default = "us-east-1"
-}
-
 variable "environment" {
   type = string
 }
@@ -32,57 +28,8 @@ variable "es_instance_type" {
   default = "t2.small.search"
 }
 
-variable "irs_superuser_email" {
-  type = string
-}
-
-variable "blue_table_name" {
-  type = string
-}
-
-variable "green_table_name" {
-  type = string
-}
-
-variable "blue_elasticsearch_domain" {
-  type = string
-}
-
-variable "green_elasticsearch_domain" {
-  type = string
-}
-
-variable "destination_table" {
-  type = string
-}
-
-variable "disable_emails" {
-  type    = bool
-  default = false
-}
-
 variable "es_volume_size" {
   type = number
-}
-
-variable "bounced_email_recipient" {
-  type = string
-}
-
-variable "bounce_alert_recipients" {
-  type = string
-}
-
-variable "slack_webhook_url" {
-  type = string
-}
-
-variable "scanner_resource_uri" {
-  type = string
-}
-
-variable "cognito_table_name" {
-  type = string
 }
 
 variable "prod_env_account_id" {
@@ -101,15 +48,7 @@ variable "should_es_beta_exist" {
   type = bool
 }
 
-variable "default_account_pass" {
-  type = string
-}
-
 variable "enable_health_checks" {
   // e.g. "1" or "0"
   type = string
-}
-
-variable "deployment_timestamp" {
-  type = number
 }
