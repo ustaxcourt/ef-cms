d479637a8a65938ba1b202ae28576b486e3c4af0
Cody Seibert <cseibert113@gmail.com>
2024-03-21 12:50:30 -0400

    devex-1236: Update lambdas to typescript

--- web-client/terraform/common/header-security-lambda.tf
+++ web-client/terraform/common/header-security-lambda.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 
 module "header_security_lambda" {
   source         = "../../../web-api/terraform/modules/lambda"
-  handler_file   = "./web-client/terraform/common/cloudfront-edge/header-security-lambda.js"
+  handler_file   = "./web-client/terraform/common/cloudfront-edge/header-security-lambda.ts"
   handler_method = "handler"
   lambda_name    = "header_security_lambda_${var.environment}"
   role           = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/header_security_lambda_role_${var.environment}"
--- web-client/terraform/common/strip-basepath-lambda.tf
+++ web-client/terraform/common/strip-basepath-lambda.tf
@@ -1,6 +1,6 @@
 module "strip_basepath_lambda" {
   source         = "../../../web-api/terraform/modules/lambda"
-  handler_file   = "./web-client/terraform/common/cloudfront-edge/strip-basepath-lambda.js"
+  handler_file   = "./web-client/terraform/common/cloudfront-edge/strip-basepath-lambda.ts"
   handler_method = "handler"
   lambda_name    = "strip_basepath_lambda_${var.environment}"
   role           = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/strip_basepath_lambda_role_${var.environment}"
