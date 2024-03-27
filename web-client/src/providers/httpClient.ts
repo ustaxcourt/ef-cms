536a8fa392b2da3aa86065fef0a262ca3bc92743
Rachel Schneiderman <rschneiderman379@gmail.com>
2024-03-27 08:30:25 -0700

    Dependency updates: Upgrade axios, try explicitly setting keep alive to false to see if it helps with socket timeouts

--- web-client/src/providers/httpClient.ts
+++ web-client/src/providers/httpClient.ts
@@ -1,3 +1,4 @@
+import { Agent } from 'http';
 import axios, { AxiosInstance } from 'axios';
 
 let axiosClient: AxiosInstance;
@@ -8,7 +9,11 @@ export const getHttpClient = (): AxiosInstance => {
   When axios throws an error, the stack trace does not show who called axios. This helps accurately display a stack trace when axios throws an error.
   */
   const stackError = new Error(); // Look at the stack trace for more information on the error.
-  axiosClient = axiosClient || axios.create();
+  axiosClient =
+    axiosClient ||
+    axios.create({
+      httpAgent: new Agent({ keepAlive: false }),
+    });
 
   axiosClient.interceptors.response.use(undefined, error => {
     error.stack = stackError.stack;
