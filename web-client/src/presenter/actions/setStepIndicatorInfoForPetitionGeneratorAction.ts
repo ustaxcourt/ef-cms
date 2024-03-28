9b4c9970a548940cf93848d1b039643bdd351733
John Cruz <jcruz@flexion.us>
2024-03-28 12:04:54 -0600

    5512: set step indicator state;

---
+++ web-client/src/presenter/actions/setStepIndicatorInfoForPetitionGeneratorAction.ts
@@ -0,0 +1,20 @@
+import { state } from '@web-client/presenter/app.cerebral';
+
+export const setStepIndicatorInfoForPetitionGeneratorAction = ({
+  store,
+}: ActionProps) => {
+  const stepTitles = [
+    'Petition',
+    'Petitioner Information',
+    'IRS Notice',
+    'Case Procedure & Trial Location',
+    'Statement of Taxpayer Identification',
+    'Review & Submit Case',
+    'Pay Filing Fee',
+  ];
+
+  store.set(state.stepIndicatorInfo, {
+    currentStep: 0,
+    steps: stepTitles,
+  });
+};
