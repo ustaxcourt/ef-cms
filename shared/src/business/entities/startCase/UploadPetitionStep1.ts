566b243220dc498d740c56abd481c592204ec1e9
Nechama Krigsman <nkrigsman@flexion.us>
2024-03-29 14:13:51 -0400

    5512: Updated entity's validation to consider radio selection;

--- shared/src/business/entities/startCase/UploadPetitionStep1.ts
+++ shared/src/business/entities/startCase/UploadPetitionStep1.ts
@@ -1,25 +1,38 @@
+import { JoiValidationConstants } from '@shared/business/entities/JoiValidationConstants';
 import { JoiValidationEntity } from '@shared/business/entities/JoiValidationEntity';
 import { MAX_FILE_SIZE_BYTES, MAX_FILE_SIZE_MB } from '../EntityConstants';
 import joi from 'joi';
 
 export class UploadPetitionStep1 extends JoiValidationEntity {
+  public petitionFileSource: 'upload' | 'generated';
   public petitionFile: File;
   public petitionFileSize: number;
   public acknowledgeChecked: boolean;
 
+  //TODO: Handle generated petition inputs
+
   constructor(rawProps) {
     super('UploadPetitionStep1');
+    this.petitionFileSource = rawProps.petitionFileSource;
     this.petitionFile = rawProps.petitionFile;
     this.petitionFileSize = rawProps.petitionFileSize;
     this.acknowledgeChecked = rawProps.acknowledgeChecked;
   }
 
   static VALIDATION_RULES = {
-    acknowledgeChecked: joi.boolean().required().truthy(),
-    petitionFile: joi
-      .object()
-      .required()
-      .messages({ '*': 'Upload the Petition PDF.' }),
+    acknowledgeChecked: joi.boolean().when('petitionFileSource', {
+      is: JoiValidationConstants.STRING.valid('upload'),
+      otherwise: joi.optional(),
+      then: joi.boolean().required().truthy(),
+    }),
+    petitionFile: joi.object().when('petitionFileSource', {
+      is: JoiValidationConstants.STRING.valid('upload'),
+      otherwise: joi.optional(),
+      then: joi
+        .object()
+        .required()
+        .messages({ '*': 'Upload the Petition PDF.' }),
+    }),
     petitionFileSize: joi
       .number()
       .integer()
@@ -34,6 +47,7 @@ export class UploadPetitionStep1 extends JoiValidationEntity {
         '*': 'Your Petition file size is empty',
         'number.max': `Your Petition file size is too big. The maximum file size is ${MAX_FILE_SIZE_MB}MB.`,
       }),
+    petitionFileSource: joi.string().required().valid('upload', 'generated'),
   };
 
   getValidationRules() {
