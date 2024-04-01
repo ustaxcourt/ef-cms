60f5d700883b3d824d2d323dfa140543a87f883a
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-04-01 15:36:26 -0500

    5512: set up state for petition generation

--- web-client/src/presenter/sequences/gotoUpdatedPetitionFlowSequence.ts
+++ web-client/src/presenter/sequences/gotoUpdatedPetitionFlowSequence.ts
@@ -1,9 +1,11 @@
 import { setStepIndicatorInfoForPetitionGeneratorAction } from '@web-client/presenter/actions/setStepIndicatorInfoForPetitionGeneratorAction';
 import { setupCurrentPageAction } from '../actions/setupCurrentPageAction';
+import { setupPetitionStateAction } from '@web-client/presenter/actions/setupPetitionStateAction';
 import { startWebSocketConnectionSequenceDecorator } from '../utilities/startWebSocketConnectionSequenceDecorator';
 
 export const gotoUpdatedPetitionFlowSequence =
   startWebSocketConnectionSequenceDecorator([
     setStepIndicatorInfoForPetitionGeneratorAction,
+    setupPetitionStateAction,
     setupCurrentPageAction('UpdatedFilePetition'),
   ]);
