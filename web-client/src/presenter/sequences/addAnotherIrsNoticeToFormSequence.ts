60ac33d2f585489f429064c7de6fac60ee870dff
John Cruz <jcruz@flexion.us>
2024-04-09 10:57:37 -0600

    5512: WIP support multiple IRS Notice uploads;

---
+++ web-client/src/presenter/sequences/addAnotherIrsNoticeToFormSequence.ts
@@ -0,0 +1,5 @@
+import { addAnotherIrsNoticeToFormAction } from '@web-client/presenter/actions/addAnotherIrsNoticeToFormAction';
+
+export const addAnotherIrsNoticeToFormSequence = [
+  addAnotherIrsNoticeToFormAction,
+];
