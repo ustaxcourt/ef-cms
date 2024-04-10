af45638151b01ceb9959d313d6da6a1979b3d078
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-04-10 08:23:34 -0500

    5512: add index to form update sequence for arrays

--- web-client/src/presenter/actions/setFormValueAction.ts
+++ web-client/src/presenter/actions/setFormValueAction.ts
@@ -1,14 +1,12 @@
 import { state } from '@web-client/presenter/app.cerebral';
 
-/**
- * sets the state.form to the props.value passed in
- * @param {object} providers the providers object
- * @param {object} providers.store the cerebral store
- * @param {object} providers.props the cerebral props object
- */
 export const setFormValueAction = ({ props, store }: ActionProps) => {
   if (props.value !== '' && props.value !== null) {
-    store.set(state.form[props.key], props.value);
+    if (props.index !== null) {
+      store.set(state.form[props.key][props.index], props.value);
+    } else {
+      store.set(state.form[props.key], props.value);
+    }
   } else {
     store.unset(state.form[props.key]);
   }
