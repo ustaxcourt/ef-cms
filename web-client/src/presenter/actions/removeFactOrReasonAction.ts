ceb04e9e4d0b67b3e531488b11995d048ac1f075
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-04-10 14:30:17 -0500

    5512: remove fact/reason cerebral

---
+++ web-client/src/presenter/actions/removeFactOrReasonAction.ts
@@ -0,0 +1,11 @@
+import { state } from '@web-client/presenter/app.cerebral';
+
+export const removeFactOrReasonAction = ({
+  get,
+  props,
+  store,
+}: ActionProps) => {
+  // const newIndex = get(state.form[props.key]).length;
+  // store.set(state.form[props.key][newIndex], '');
+  console.log('remove');
+};
