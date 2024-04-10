771769c4ff18109d0c2155156a8bdf63bfe0b4a3
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-04-10 08:52:38 -0500

    5512: fix non-index form updates

--- web-client/src/presenter/actions/setFormValueAction.ts
+++ web-client/src/presenter/actions/setFormValueAction.ts
@@ -2,7 +2,7 @@ import { state } from '@web-client/presenter/app.cerebral';
 
 export const setFormValueAction = ({ props, store }: ActionProps) => {
   if (props.value !== '' && props.value !== null) {
-    if (props.index !== null) {
+    if (props.index || props.index === 0) {
       store.set(state.form[props.key][props.index], props.value);
     } else {
       store.set(state.form[props.key], props.value);
