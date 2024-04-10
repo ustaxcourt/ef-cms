92a82068131a569bb0d899bbe28e2073f861783d
John Cruz <jcruz@flexion.us>
2024-04-10 09:14:12 -0600

    5512: WIP Format the date in action; update typing;

--- web-client/src/presenter/actions/updateIrsNoticeUploadedFileAction.ts
+++ web-client/src/presenter/actions/updateIrsNoticeUploadedFileAction.ts
@@ -1,13 +1,45 @@
+import { ClientApplicationContext } from '@web-client/applicationContext';
+import { FORMATS } from '@shared/business/utilities/DateHandler';
 import { state } from '@web-client/presenter/app.cerebral';
 
 export const updateIrsNoticeIndexPropertyAction = ({
+  applicationContext,
   props,
   store,
 }: ActionProps) => {
-  const { key, property, value } = props;
+  const { key, property, toFormat, value } = props;
+  const updatedValue = toFormat
+    ? formatValue(applicationContext, value, toFormat)
+    : value;
   if (props.value !== '' && props.value !== null) {
-    store.set(state.irsNoticeUploadFormInfo[+key][property], value);
+    store.set(state.irsNoticeUploadFormInfo[+key][property], updatedValue);
   } else {
     store.unset(state.irsNoticeUploadFormInfo[+key][property]);
   }
 };
+
+function formatValue(
+  applicationContext: ClientApplicationContext,
+  value: string,
+  toFormat: keyof typeof FORMATS,
+): string {
+  if (!value) return value;
+
+  try {
+    const inputFormat = applicationContext
+      .getUtilities()
+      .getDateFormat(value, [FORMATS.MDYYYY, FORMATS.MMDDYYYY]);
+
+    const luxonDate = applicationContext
+      .getUtilities()
+      .prepareDateFromString(value, inputFormat) as unknown as string;
+
+    const formattedDate = applicationContext
+      .getUtilities()
+      .formatDateString(luxonDate, toFormat);
+
+    return formattedDate;
+  } catch {
+    return value;
+  }
+}
