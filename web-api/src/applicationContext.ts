7d3542e48bde1c23ca3e680e42338c3ff82f9a3a
Nechama Krigsman <nkrigsman@flexion.us>
2024-04-12 12:54:04 -0400

    10260: Move persistence file to web-api folder;

--- web-api/src/applicationContext.ts
+++ web-api/src/applicationContext.ts
@@ -55,7 +55,7 @@ import { getUtilities } from './getUtilities';
 import { isAuthorized } from '../../shared/src/authorization/authorizationClientService';
 import { isCurrentColorActive } from './persistence/dynamo/helpers/isCurrentColorActive';
 import { retrySendNotificationToConnections } from '../../shared/src/notifications/retrySendNotificationToConnections';
-import { saveRequestResponse } from '@shared/notifications/saveRequestResponse';
+import { saveRequestResponse } from '@web-api/persistence/dynamo/polling/saveRequestResponse';
 import { sendBulkTemplatedEmail } from './dispatchers/ses/sendBulkTemplatedEmail';
 import { sendEmailEventToQueue } from './persistence/messages/sendEmailEventToQueue';
 import { sendEmailToUser } from '@web-api/persistence/messages/sendEmailToUser';
