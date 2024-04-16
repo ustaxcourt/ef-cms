1f6d56ebed201fb068fbb5bf51417d811ad3bf09
John Cruz <jcruz@flexion.us>
2024-04-16 08:52:17 -0600

    5512: Created initial componenet for step 4;

---
+++ web-client/src/views/StartCaseUpdated/UpdatedFilePetitionStep4.tsx
@@ -0,0 +1,9 @@
+import { connect } from '@web-client/presenter/shared.cerebral';
+import React from 'react';
+
+export const UpdatedFilePetitionStep4 = connect(
+  {},
+  function UpdatedFilePetitionStep4() {
+    return <>John Is Testing</>;
+  },
+);
