8a1af8544a17bd3e7fb08d7d2d05907ec1d12828
Zachary Rogers <zrogers>
2024-04-17 15:36:41 -0700

    10345: Move migration related shell scripts out of terraform-workflow

--- web-api/terraform/bin/deploy-migration.sh
+++ web-api/terraform/bin/deploy-migration.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+# shellcheck disable=SC1091
 ENVIRONMENT=$1
 
 [ -z "${ENVIRONMENT}" ] && echo "You must pass in ENVIRONMENT as command line argument 1" && exit 1
@@ -8,8 +9,6 @@ ENVIRONMENT=$1
 [ -z "${DESTINATION_TABLE}" ] && echo "You must set DESTINATION_TABLE as an environment variable" && exit 1
 [ -z "${EFCMS_DOMAIN}" ] && echo "You must set EFCMS_DOMAIN as an environment variable" && exit 1
 [ -z "${DOCUMENTS_BUCKET_NAME}" ] && DOCUMENTS_BUCKET_NAME="${EFCMS_DOMAIN}-documents-${ENVIRONMENT}-${REGION}"
-
-export ENVIRONMENT="${ENVIRONMENT}"
 
 STREAM_ARN=$(aws dynamodbstreams list-streams --region us-east-1 --query "Streams[?TableName=='${SOURCE_TABLE}'].StreamArn | [0]" --output text)
 SOURCE_TABLE_VERSION=$(aws dynamodb get-item --region us-east-1 --table-name "efcms-deploy-${ENVIRONMENT}" --key '{"pk":{"S":"source-table-version"},"sk":{"S":"source-table-version"}}' | jq -r ".Item.current.S")
@@ -31,4 +30,12 @@ export TF_VAR_source_table=$SOURCE_TABLE
 export TF_VAR_stream_arn=$STREAM_ARN
 export TF_VAR_elasticsearch_domain=$ELASTICSEARCH_ENDPOINT
 
-../../../../shared/terraform/bin/init.sh migration
+../../../../scripts/verify-terraform-version.sh
+
+terraform init -upgrade -backend=true \
+ -backend-config=bucket="${ZONE_NAME}.terraform.deploys" \
+ -backend-config=key="migration-${ENVIRONMENT}.tfstate" \
+ -backend-config=dynamodb_table="efcms-terraform-lock" \
+ -backend-config=region="us-east-1"
+terraform plan
+terraform apply -auto-approve
