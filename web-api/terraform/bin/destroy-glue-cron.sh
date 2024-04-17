c5660cb6f21dbc564f3d1f4031f776e6937d506e
Zachary Rogers <zrogers>
2024-04-17 14:53:57 -0700

    10345: Moved destroy glue cron to applyables directory

--- web-api/terraform/bin/destroy-glue-cron.sh
+++ web-api/terraform/bin/destroy-glue-cron.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+# shellcheck disable=SC1091
 ENVIRONMENT=$1
 
 [ -z "${ENVIRONMENT}" ] && echo "You must pass in ENVIRONMENT as command line argument 1" && exit 1
@@ -8,9 +9,15 @@ echo "Running terraform with the following environment configs:"
 echo "  - ENVIRONMENT=${ENVIRONMENT}"
 
 export ENVIRONMENT="${ENVIRONMENT}"
-
 export TF_VAR_circle_machine_user_token=$CIRCLE_MACHINE_USER_TOKEN
 export TF_VAR_circle_workflow_id=$CIRCLE_WORKFLOW_ID
 export TF_VAR_environment=$ENVIRONMENT
 
-../../../../shared/terraform/bin/init.sh glue-cron
+../../../../scripts/verify-terraform-version.sh
+
+terraform init -upgrade -backend=true \
+ -backend-config=bucket="${ZONE_NAME}.terraform.deploys" \
+ -backend-config=key="glue-cron-${ENVIRONMENT}.tfstate" \
+ -backend-config=dynamodb_table="efcms-terraform-lock" \
+ -backend-config=region="us-east-1"
+terraform destroy -auto-approve
