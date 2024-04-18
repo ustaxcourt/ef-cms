a01bcd06af812b51f7273b0f8689f65ad783f213
Zachary Rogers <zrogers>
2024-04-18 15:46:22 -0700

    10345: Update pathing

--- scripts/reindex/disable-reindex-cron.sh
+++ scripts/reindex/disable-reindex-cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-../../check-env-variables.sh "ENV"
+./check-env-variables.sh "ENV"
 
 aws events disable-rule --name "check_reindex_status_cron_${ENV}" --region us-east-1
--- scripts/wait-for-workflow/enable-wait-for-workflow-cron.sh
+++ scripts/wait-for-workflow/enable-wait-for-workflow-cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-../../check-env-variables.sh "ENV"
+./check-env-variables.sh "ENV"
 
 aws events enable-rule --name "wait_for_workflow_cron_${ENV}" --region us-east-1
