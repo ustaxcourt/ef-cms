480acf9e3fb30120cac1f0b1ba6262a17a52f074
Zachary Rogers <zrogers>
2024-04-19 14:31:38 -0700

    10345: WIP Move creation of iam role for reindex-status infra into reindex-status module

--- web-api/terraform/modules/reindex-cron/reindex-status.tf
+++ web-api/terraform/modules/reindex-cron/reindex-status.tf
@@ -1,11 +1,65 @@
 data "aws_caller_identity" "current" {}
 
+resource "aws_iam_role" "reindex_status_role" {
+  name = "reindex_status_role_${var.environment}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "reindex_status_policy" {
+  name = "reindex_status_policy_${var.environment}"
+  role = aws_iam_role.reindex_status_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogStreams"
+      ],
+      "Resource": [
+        "arn:aws:logs:*:*:*"
+      ]
+    },
+    {
+      "Sid": "Other",
+      "Effect": "Allow",
+      "Action": [
+        "es:*"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+
 module "reindex_status_lambda" {
   source         = "../lambda"
   handler_file   = "./web-api/src/lambdas/reindex/reindex-status.ts"
   handler_method = "handler"
   lambda_name    = "reindex_status_lambda_${var.environment}"
-  role           = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/reindex_status_role_${var.environment}"
+  role           = aws_iam_role.reindex_status_role.arn
   timeout        = "900"
   environment = {
     ACCOUNT_ID                = data.aws_caller_identity.current.account_id
