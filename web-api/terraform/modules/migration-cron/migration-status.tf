8462a0d77694a8af509e66ec781ae82e9992242e
Zachary Rogers <zrogers>
2024-04-19 14:07:33 -0700

    10345: WIP Move creation of iam role for migration infra into migration related modules.

--- web-api/terraform/modules/migration-cron/migration-status.tf
+++ web-api/terraform/modules/migration-cron/migration-status.tf
@@ -1,11 +1,94 @@
 data "aws_caller_identity" "current" {}
 
+resource "aws_iam_role" "migration_status_role" {
+  name = "migration_status_role_${var.environment}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "migration_status_policy" {
+  name = "migration_status_policy_${var.environment}"
+  role = aws_iam_role.migration_status_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogStreams"
+      ],
+      "Resource": [
+        "arn:aws:logs:*:*:*"
+      ]
+    },
+    {
+      "Sid": "Other",
+      "Effect": "Allow",
+      "Action": [
+        "es:*"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Sid": "CloudWatch",
+      "Effect": "Allow",
+      "Action": [
+        "cloudwatch:GetMetricStatistics"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Sid": "SQS",
+      "Effect": "Allow",
+      "Action": [
+        "sqs:GetQueueAttributes"
+      ],
+      "Resource": [
+        "arn:aws:sqs:us-east-1:${data.aws_caller_identity.current.account_id}:migration_segments_queue_${var.environment}",
+        "arn:aws:sqs:us-east-1:${data.aws_caller_identity.current.account_id}:migration_segments_dl_queue_${var.environment}"
+      ]
+    },
+    {
+      "Sid": "DynamoDB",
+      "Effect": "Allow",
+      "Action": [
+        "dynamodb:GetItem",
+        "dynamodb:PutItem"
+      ],
+      "Resource": [
+        "arn:aws:dynamodb:us-east-1:${data.aws_caller_identity.current.account_id}:table/efcms-deploy-${var.environment}"
+      ]
+    }
+  ]
+}
+EOF
+}
+
 module "migration_status_lambda" {
   source         = "../lambda"
   handler_file   = "./web-api/src/lambdas/migration/migration-status.ts"
   handler_method = "handler"
   lambda_name    = "migration_status_lambda_${var.environment}"
-  role           = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/migration_status_role_${var.environment}"
+  role           = aws_iam_role.migration_status_role.arn
   timeout        = "900"
   environment = {
     STAGE                     = var.environment
