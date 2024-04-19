f520207be04a1c846acf585502912f453a687f49
Zachary Rogers <zrogers>
2024-04-19 13:15:07 -0700

    10345: WIP Move creation of iam role for glue-job into glue-job module

--- web-api/terraform/modules/api/iam.tf
+++ web-api/terraform/modules/api/iam.tf
@@ -36,6 +36,28 @@ resource "aws_iam_role_policy" "api_gateway_invocation_policy" {
 EOF
 }
 
+resource "aws_iam_role" "authorizer_lambda" {
+  name = "authorizer_lambda_role_${var.environment}_${var.current_color}_${var.region}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": [
+          "lambda.amazonaws.com",
+          "apigateway.amazonaws.com"
+        ]
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
 
 resource "aws_iam_role_policy" "authorizer_invocation_policy" {
   name = "cognito_authorizer_policy_${var.environment}_${var.current_color}_${var.region}"
@@ -58,29 +80,6 @@ resource "aws_iam_role_policy" "authorizer_invocation_policy" {
         "logs:PutLogEvents"
       ],
       "Resource": "arn:aws:logs:*:*:*"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role" "authorizer_lambda" {
-  name = "authorizer_lambda_role_${var.environment}_${var.current_color}_${var.region}"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": [
-          "lambda.amazonaws.com",
-          "apigateway.amazonaws.com"
-        ]
-      },
-      "Effect": "Allow",
-      "Sid": ""
     }
   ]
 }
--- web-api/terraform/modules/glue-cron/glue-job-status.tf
+++ web-api/terraform/modules/glue-cron/glue-job-status.tf
@@ -1,11 +1,66 @@
 data "aws_caller_identity" "current" {}
 
+resource "aws_iam_role" "glue_job_status_role" {
+  name = "glue_job_status_role_${var.environment}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "glue_job_status_policy" {
+  name = "glue_job_status_policy_${var.environment}"
+  role = aws_iam_role.glue_job_status_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogStreams"
+      ],
+      "Resource": [
+        "arn:aws:logs:*:*:*"
+      ]
+    },
+    {
+      "Action": [
+        "glue:GetJobRuns"
+      ],
+      "Resource": [
+        "arn:aws:glue:us-east-1:${data.aws_caller_identity.current.account_id}:job/*"
+      ],
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+
 module "glue_job_status_lambda" {
   source         = "../lambda"
   handler_file   = "./web-api/src/lambdas/glue-cron/glue-job-status.ts"
   handler_method = "handler"
   lambda_name    = "glue_job_status_lambda_${var.environment}"
-  role           = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/glue_job_status_role_${var.environment}"
+  role           = aws_iam_role.glue_job_status_role.arn
   environment = {
     STAGE                     = var.environment
     NODE_ENV                  = "production"
