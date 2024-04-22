326198013e7d85dd95da094179455efed5011b9d
Jim Lerza <james.lerza@ustaxcourt.gov>
2024-04-22 17:01:09 -0400

    devex-10334: convert instances of API Gateway v2 to API Gateway v3

--- web-api/importHelpers.ts
+++ web-api/importHelpers.ts
@@ -1,6 +1,9 @@
+import {
+  APIGatewayClient,
+  GetRestApisCommand,
+} from '@aws-sdk/client-api-gateway';
 import { CognitoIdentityProvider } from '@aws-sdk/client-cognito-identity-provider';
 import { getUserToken } from './storage/scripts/loadTest/loadTestHelpers';
-import AWS from 'aws-sdk';
 import fs from 'fs';
 import jwt from 'jsonwebtoken';
 import localUsers from './storage/fixtures/seed/users.json';
@@ -10,8 +13,8 @@ export const getToken = async () => {
     const adminUser = localUsers.find(user => user.role === 'admin');
     const user = {
       ...adminUser,
-      'custom:role': adminUser.role,
-      sub: adminUser.userId,
+      'custom:role': adminUser!.role,
+      sub: adminUser!.userId,
     };
 
     return Promise.resolve(jwt.sign(user, 'secret'));
@@ -23,30 +26,28 @@ export const getToken = async () => {
 
   return await getUserToken({
     cognito,
-    env: process.env.ENV,
-    password: process.env.USTC_ADMIN_PASS,
-    username: process.env.USTC_ADMIN_USER,
+    env: process.env.ENV!,
+    password: process.env.USTC_ADMIN_PASS!,
+    username: process.env.USTC_ADMIN_USER!,
   });
 };
 
 export const getServices = async () => {
-  const apigateway = new AWS.APIGateway({
+  const apigateway = new APIGatewayClient({
     region: process.env.REGION,
   });
-  const { items: apis } = await apigateway
-    .getRestApis({ limit: 200 })
-    .promise();
+  const getRestApisCommand = new GetRestApisCommand({ limit: 200 });
+  const { items: apis } = await apigateway.send(getRestApisCommand);
 
-  return apis
+  return (apis ?? [])
     .filter(api =>
-      api.name.includes(
+      api.name?.includes(
         `gateway_api_${process.env.ENV}_${process.env.DEPLOYING_COLOR}`,
       ),
     )
     .reduce((obj, api) => {
-      obj[
-        api.name.replace(`_${process.env.ENV}`, '')
-      ] = `https://${api.id}.execute-api.${process.env.REGION}.amazonaws.com/${process.env.ENV}`;
+      obj[api.name?.replace(`_${process.env.ENV}`, '')!] =
+        `https://${api.id}.execute-api.${process.env.REGION}.amazonaws.com/${process.env.ENV}`;
       return obj;
     }, {});
 };
--- web-api/smoke-tests.ts
+++ web-api/smoke-tests.ts
@@ -1,11 +1,11 @@
 import { CognitoIdentityProvider } from '@aws-sdk/client-cognito-identity-provider';
-import AWS from 'aws-sdk';
+import { getServices } from './importHelpers';
 import assert from 'assert';
 import axios from 'axios';
 
 const ENV = process.argv[2];
 const REGION = process.argv[3];
-const DEPLOYING_COLOR = process.argv[4];
+// const DEPLOYING_COLOR = process.argv[4];
 const DEFAULT_ACCOUNT_PASS = process.argv[5];
 
 if (!ENV || !REGION || !DEFAULT_ACCOUNT_PASS) {
@@ -19,63 +19,73 @@ const cognito = new CognitoIdentityProvider({
   region: 'us-east-1',
 });
 
-const getClientId = async userPoolId => {
-  const results = await cognito
-    .listUserPoolClients({
-      MaxResults: 60,
-      UserPoolId: userPoolId,
-    })
-    .promise();
-  const clientId = results.UserPoolClients[0].ClientId;
+const getClientId = async (userPoolId: string): Promise<string> => {
+  const results = await cognito.listUserPoolClients({
+    MaxResults: 60,
+    UserPoolId: userPoolId,
+  });
+  let clientId = '';
+  if (
+    results &&
+    'UserPoolClients' in results &&
+    results.UserPoolClients &&
+    results.UserPoolClients[0] &&
+    'ClientId' in results.UserPoolClients[0] &&
+    results.UserPoolClients[0].ClientId
+  ) {
+    clientId = results.UserPoolClients[0].ClientId;
+  }
   return clientId;
 };
 
-const getUserPoolId = async () => {
-  const results = await cognito
-    .listUserPools({
-      MaxResults: 50,
-    })
-    .promise();
-  const userPoolId = results.UserPools.find(
-    pool => pool.Name === `efcms-${ENV}`,
-  ).Id;
+const getUserPoolId = async (): Promise<string> => {
+  const results = await cognito.listUserPools({
+    MaxResults: 50,
+  });
+  let userPoolId = '';
+  if (results && 'UserPools' in results && results.UserPools) {
+    const userPool = results.UserPools.find(
+      pool => pool.Name === `efcms-${ENV}`,
+    );
+    if (userPool && 'Id' in userPool && userPool.Id) {
+      userPoolId = userPool.Id;
+    }
+  }
   return userPoolId;
 };
 
-const getUserToken = async (username, password) => {
+const getUserToken = async (
+  username: string,
+  password: string,
+): Promise<string> => {
   const userPoolId = await getUserPoolId();
   const clientId = await getClientId(userPoolId);
 
-  const response = await cognito
-    .adminInitiateAuth({
-      AuthFlow: 'ADMIN_NO_SRP_AUTH',
-      AuthParameters: {
-        PASSWORD: password,
-        USERNAME: username,
-      },
-      ClientId: clientId,
-      UserPoolId: userPoolId,
-    })
-    .promise();
-  return response.AuthenticationResult.IdToken;
+  const response = await cognito.adminInitiateAuth({
+    AuthFlow: 'ADMIN_NO_SRP_AUTH',
+    AuthParameters: {
+      PASSWORD: password,
+      USERNAME: username,
+    },
+    ClientId: clientId,
+    UserPoolId: userPoolId,
+  });
+  let idToken = '';
+  if (
+    response &&
+    'AuthenticationResult' in response &&
+    response.AuthenticationResult &&
+    'IdToken' in response.AuthenticationResult &&
+    response.AuthenticationResult.IdToken
+  ) {
+    idToken = response.AuthenticationResult.IdToken;
+  }
+  return idToken;
 };
 
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
-  let apigateway = new AWS.APIGateway({
-    region: REGION,
-  });
-  const { items: apis } = await apigateway
-    .getRestApis({ limit: 200 })
-    .promise();
-
-  const services = apis
-    .filter(api => api.name.includes(`gateway_api_${ENV}_${DEPLOYING_COLOR}`))
-    .reduce((obj, api) => {
-      obj[
-        api.name.replace(`_${ENV}_${DEPLOYING_COLOR}`, '')
-      ] = `https://${api.id}.execute-api.${REGION}.amazonaws.com/${ENV}`;
-      return obj;
-    }, {});
+  const services = getServices();
 
   const token = await getUserToken(
     'petitionsclerk1@example.com',
