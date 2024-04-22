12c74196f2acc44fda75a82d81557120f8d72376
Jim Lerza <james.lerza@ustaxcourt.gov>
2024-04-22 17:01:09 -0400

    devex-10334: convert instances of API Gateway v2 to API Gateway v3

--- web-client/switch-api-colors.ts
+++ web-client/switch-api-colors.ts
@@ -1,54 +1,52 @@
-const AWS = require('aws-sdk');
+import {
+  APIGatewayClient,
+  CreateBasePathMappingCommand,
+  DeleteBasePathMappingCommand,
+  GetRestApisCommand,
+} from '@aws-sdk/client-api-gateway';
+import { requireEnvVars } from '../shared/admin-tools/util';
 
-const check = (value, message) => {
-  if (!value) {
-    console.log(message);
-    process.exit(1);
-  }
-};
+requireEnvVars(['DEPLOYING_COLOR', 'EFCMS_DOMAIN', 'ENV']);
 
 const { DEPLOYING_COLOR, EFCMS_DOMAIN, ENV } = process.env;
-
-check(DEPLOYING_COLOR, 'You must have DEPLOYING_COLOR set in your environment');
-check(EFCMS_DOMAIN, 'You must have EFCMS_DOMAIN set in your environment');
-check(ENV, 'You must have ENV set in your environment');
-
 const REGIONS = ['us-east-1', 'us-west-1'];
 
-const run = async () => {
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+(async () => {
   for (const region of REGIONS) {
-    const apigateway = new AWS.APIGateway({
-      region,
-    });
+    const apigateway = new APIGatewayClient({ region });
+    const getRestApisCommand = new GetRestApisCommand({ limit: 500 });
+    const { items } = await apigateway.send(getRestApisCommand);
 
-    const { items } = await apigateway.getRestApis({ limit: 500 }).promise();
-
-    const apiGatewayRecord = items.find(
+    const apiGatewayRecord = items?.find(
       record => record.name === `gateway_api_${ENV}_${DEPLOYING_COLOR}`,
     );
 
-    apigateway.deleteBasePathMapping(
-      {
-        basePath: '(none)',
-        domainName: `api.${EFCMS_DOMAIN}`,
-      },
-      () => {
-        console.log('Successfully deleted base path mapping'); // successful response
-        apigateway.createBasePathMapping(
-          {
+    if (apiGatewayRecord && 'id' in apiGatewayRecord && apiGatewayRecord.id) {
+      try {
+        const deleteBasePathMappingResult = await apigateway.send(
+          new DeleteBasePathMappingCommand({
+            basePath: '(none)',
             domainName: `api.${EFCMS_DOMAIN}`,
-            restApiId: apiGatewayRecord.id,
-            stage: ENV,
-          },
-          (err, data) => {
-            if (err) console.log(err, err.stack);
-            // an error occurred
-            else console.log(data); // successful response
-          },
+          }),
         );
-      },
-    );
+        if (deleteBasePathMappingResult) {
+          const createBasePathMappingResult = await apigateway.send(
+            new CreateBasePathMappingCommand({
+              domainName: `api.${EFCMS_DOMAIN}`,
+              restApiId: apiGatewayRecord.id,
+              stage: ENV,
+            }),
+          );
+          if (createBasePathMappingResult) {
+            console.log(createBasePathMappingResult);
+          }
+        }
+      } catch (err: any) {
+        if (err) {
+          console.log(err, err.stack);
+        }
+      }
+    }
   }
-};
-
-run();
+})();
