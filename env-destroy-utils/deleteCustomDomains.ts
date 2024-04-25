93572c4a71a7e3aad393eb62a13bc840e6253813
Jim Lerza <james.lerza@ustaxcourt.gov>
2024-04-25 11:14:16 -0400

    devex-10334: convert some env-destroy-utils (the ones that touch API Gateway) to typescript

--- env-destroy-utils/deleteCustomDomains.ts
+++ env-destroy-utils/deleteCustomDomains.ts
@@ -1,22 +1,31 @@
-import { DeleteDomainNameCommand } from '@aws-sdk/client-api-gateway';
-import { getApiGateway } from './getApiGateway';
+import {
+  ApiGatewayV2Client,
+  DeleteDomainNameCommand,
+  DomainName,
+} from '@aws-sdk/client-apigatewayv2';
+import { getApiGateway, type tApiGatewayEnvironment } from './getApiGateway';
 import { getCustomDomains } from './getCustomDomains';
 import { sleepForMilliseconds } from './sleep';
 
-export const deleteCustomDomains = async ({ environment }) => {
-  const apiGateway = getApiGateway({ environment });
+export const deleteCustomDomains = async ({
+  environment,
+}: {
+  environment: tApiGatewayEnvironment;
+}): Promise<void> => {
+  const apiGateway: ApiGatewayV2Client = getApiGateway({ environment });
 
-  const customDomains = await getCustomDomains({
+  const customDomains: DomainName[] = await getCustomDomains({
     apiGateway,
     environment,
   });
 
   for (const domain of customDomains) {
     console.log('Delete Custom Domain:', domain.DomainName);
-    const deleteDomainNameCommand = new DeleteDomainNameCommand({
-      DomainName: domain.DomainName,
-    });
-    await apiGateway.send(deleteDomainNameCommand);
+    await apiGateway.send(
+      new DeleteDomainNameCommand({
+        DomainName: domain.DomainName,
+      }),
+    );
     await sleepForMilliseconds(100);
   }
 };
--- env-destroy-utils/getApiGateway.ts
+++ env-destroy-utils/getApiGateway.ts
@@ -1,9 +1,19 @@
 import { ApiGatewayV2Client } from '@aws-sdk/client-apigatewayv2';
 
-export const getApiGateway = ({ environment }) => {
+export type tApiGatewayEnvironment = {
+  apiVersion?: string;
+  name?: string;
+  region?: string;
+};
+
+export const getApiGateway = ({
+  environment,
+}: {
+  environment: tApiGatewayEnvironment;
+}): ApiGatewayV2Client => {
   return new ApiGatewayV2Client({
-    apiVersion: 'latest',
-    maxRetries: 20,
+    apiVersion: environment.apiVersion || 'latest',
+    maxAttempts: 20,
     region: environment.region,
   });
 };
