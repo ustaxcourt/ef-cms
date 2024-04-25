8c78103e3f35311c2f4f4af5128df356f2f19c1f
Jim Lerza <james.lerza@ustaxcourt.gov>
2024-04-25 11:14:16 -0400

    devex-10334: convert some env-destroy-utils (the ones that touch API Gateway) to typescript

--- env-destroy-utils/getCustomDomains.ts
+++ env-destroy-utils/getCustomDomains.ts
@@ -1,11 +1,24 @@
-import { filter } from 'lodash';
+import {
+  ApiGatewayV2Client,
+  DomainName,
+  GetDomainNamesCommand,
+} from '@aws-sdk/client-apigatewayv2';
+import { tApiGatewayEnvironment } from './getApiGateway';
 
-export const getCustomDomains = async ({ apiGateway, environment }) => {
-  const { Items } = await apiGateway.getDomainNames();
-  return filter(Items, customDomain => {
-    return (
-      customDomain.DomainName.includes(`-${environment.name}`) ||
-      customDomain.DomainName.includes(`.${environment.name}`)
-    );
-  });
+export const getCustomDomains = async ({
+  apiGateway,
+  environment,
+}: {
+  apiGateway: ApiGatewayV2Client;
+  environment: tApiGatewayEnvironment;
+}): Promise<DomainName[]> => {
+  const { Items } = await apiGateway.send(new GetDomainNamesCommand({}));
+  return (
+    Items?.filter(customDomain => {
+      return (
+        customDomain.DomainName?.includes(`-${environment.name}`) ||
+        customDomain.DomainName?.includes(`.${environment.name}`)
+      );
+    }) || []
+  );
 };
