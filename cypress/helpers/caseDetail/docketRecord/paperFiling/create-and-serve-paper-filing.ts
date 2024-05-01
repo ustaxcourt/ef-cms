8f9e2980f1f18485cfb76d71712330507898ec77
Rachel Schneiderman <rschneiderman379@gmail.com>
2024-05-01 09:19:06 -0700

    Remove comment

--- cypress/helpers/caseDetail/docketRecord/paperFiling/create-and-serve-paper-filing.ts
+++ cypress/helpers/caseDetail/docketRecord/paperFiling/create-and-serve-paper-filing.ts
@@ -14,7 +14,6 @@ export function createAndServePaperFiling(
   cy.get('[data-testid="objections-No"]').click();
   cy.get('[data-testid="upload-pdf-button"]').click();
 
-  //TODO: this is a broken path
   cy.get('input#primaryDocumentFile-file').attachFile('../fixtures/sample.pdf');
 
   cy.get('[data-testid="save-and-serve"]').click();
