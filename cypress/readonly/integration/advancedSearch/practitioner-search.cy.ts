fa6fb0952250e3ac1bbf2cfe1f24557d69c3ba95
Jason Fu <jasonfu6699@hotmail.com>
2024-05-02 16:15:12 -0700

    10281: improve cypress tests somewhat

--- cypress/readonly/integration/advancedSearch/practitioner-search.cy.ts
+++ cypress/readonly/integration/advancedSearch/practitioner-search.cy.ts
@@ -1,15 +1,14 @@
 import { isValidRequest } from '../../support/helpers';
-import { loginAsTestAdmissionsClerk } from '../../../helpers/authentication/login-as-helpers';
+import { loginAsDocketClerk1 } from '../../../helpers/authentication/login-as-helpers';
 
 const EFCMS_DOMAIN = Cypress.env('EFCMS_DOMAIN');
 const DEPLOYING_COLOR = Cypress.env('DEPLOYING_COLOR');
 
 describe('Practitioner Search', () => {
   it('should do a practitioner search by name and bar number', () => {
-    loginAsTestAdmissionsClerk();
+    loginAsDocketClerk1();
     cy.get('.advanced').contains('Advanced').click();
-
-    cy.get('button#tab-practitioner').click();
+    cy.get('[data-testid="tab-practitioner"]').click();
 
     cy.intercept({
       hostname: `api-${DEPLOYING_COLOR}.${EFCMS_DOMAIN}`,
@@ -18,17 +17,19 @@ describe('Practitioner Search', () => {
     }).as('getPractitionerByName');
 
     cy.get('input#practitioner-name').type('test');
-
     cy.get('button#practitioner-search-by-name-button').click();
-
     cy.wait('@getPractitionerByName').then(isValidRequest);
-    cy.get('[data-testid="practitioner-row-PT1111"]').should('exist');
-    cy.get('[data-testid="practitioner-search-result-count"]').should(
-      'have.text',
-      '20',
-    );
+    cy.get('[data-testid="practitioner-results-table"]')
+      .find('tr')
+      .then(row => {
+        return row.length > 0;
+      });
+  });
 
-    cy.get('button#tab-practitioner').click();
+  it('should do a practitioner search by bar number', () => {
+    loginAsDocketClerk1();
+    cy.get('.advanced').contains('Advanced').click();
+    cy.get('[data-testid="tab-practitioner"]').click();
 
     cy.intercept({
       hostname: `api-${DEPLOYING_COLOR}.${EFCMS_DOMAIN}`,
@@ -36,10 +37,8 @@ describe('Practitioner Search', () => {
       url: '/practitioners/test',
     }).as('getPractitionerByBarNumber');
 
-    cy.get('input#bar-number').type('test');
-
+    cy.get('input#bar-number').type('PT1111');
     cy.get('button#practitioner-search-by-bar-number-button').click();
-
     cy.wait('@getPractitionerByBarNumber').then(isValidRequest);
   });
 });
