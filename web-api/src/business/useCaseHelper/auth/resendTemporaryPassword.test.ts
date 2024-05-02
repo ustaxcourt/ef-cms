8a1f27e9563ee3b96d05b85054bcb678dbb7484e
Zachary Rogers <zrogers>
2024-05-02 14:33:19 -0700

    10208: Make resendTempPassword call cognito directly so as not to overload createUser so much.

--- web-api/src/business/useCaseHelper/auth/resendTemporaryPassword.test.ts
+++ web-api/src/business/useCaseHelper/auth/resendTemporaryPassword.test.ts
@@ -1,27 +1,22 @@
+import { DeliveryMediumType } from '@aws-sdk/client-cognito-identity-provider';
 import { applicationContext } from '@shared/business/test/createTestApplicationContext';
 import { resendTemporaryPassword } from '@web-api/business/useCaseHelper/auth/resendTemporaryPassword';
 
 describe('resendTemporaryPassword', () => {
   it('should make a call to persistence to resend an invitation email to the provided email address', async () => {
-    const mockEmail = 'petitioner@example.com';
-    const mockUserId = '9b58ab0d-b74d-4f28-9b8d-57e8f29833b9';
-    applicationContext.getUserGateway().getUserByEmail.mockResolvedValue({
-      userId: mockUserId,
-    });
+    const mockEmail = 'PetitioneR@example.com';
 
     await resendTemporaryPassword(applicationContext, {
       email: mockEmail,
     });
 
-    expect(applicationContext.getUserGateway().createUser).toHaveBeenCalledWith(
-      applicationContext,
-      {
-        attributesToUpdate: {
-          userId: mockUserId,
-        },
-        email: mockEmail,
-        resendInvitationEmail: true,
-      },
-    );
+    expect(
+      applicationContext.getCognito().adminCreateUser,
+    ).toHaveBeenCalledWith({
+      DesiredDeliveryMediums: [DeliveryMediumType.EMAIL],
+      MessageAction: 'RESEND',
+      UserPoolId: applicationContext.environment.userPoolId,
+      Username: 'petitioner@example.com',
+    });
   });
 });
