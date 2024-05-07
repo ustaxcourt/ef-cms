4e7ec10eb7b4d00194a12a641fef27ad80df4a0d
Rachel Schneiderman <rschneiderman379@gmail.com>
2024-05-07 12:52:08 -0700

    10345: Move ci/cd permissions to new applyables format

--- web-api/terraform/applyables/account-specific/moved.tf
+++ web-api/terraform/applyables/account-specific/moved.tf
@@ -15,9 +15,39 @@ moved {
 # api-gateway-cloudwatch.tf
 moved {
   from = aws_iam_role.cloudwatch
-  to   = module.api_gateway_global_logging_permissions.aws_iam_role.cloudwatch
+  to   = module.api-gateway-global-logging-permissions.aws_iam_role.cloudwatch
 }
 moved {
   from = aws_iam_role_policy.cloudwatch
-  to   = module.api_gateway_global_logging_permissions.aws_iam_role_policy.cloudwatch
+  to   = module.api-gateway-global-logging-permissions.aws_iam_role_policy.cloudwatch
+}
+
+# circle-ci.tf
+moved {
+  from = aws_iam_user.circle_ci
+  to   = module.ci-cd-permissions.aws_iam_user.ci_cd
+}
+moved {
+  from = aws_iam_user_policy_attachment.circle_ci_policy_attachment
+  to   = module.ci-cd-permissions.aws_iam_user_policy_attachment.ci_cd_policy_attachment
+}
+moved {
+  from = aws_iam_policy.circle_ci_policy
+  to   = module.ci-cd-permissions.aws_iam_policy.ci_cd_policy
+}
+moved {
+  from = aws_iam_user_policy_attachment.circle_ci_route53_policy_attachment
+  to   = module.ci-cd-permissions.aws_iam_user_policy_attachment.ci_cd_route53_policy_attachment
+}
+moved {
+  from = aws_iam_policy.circle_ci_route53_policy
+  to   = module.ci-cd-permissions.aws_iam_policy.ci_cd_route53_policy
+}
+moved {
+  from = aws_iam_user_policy_attachment.circle_ci_iam_policy_attachment
+  to   = module.ci-cd-permissions.aws_iam_user_policy_attachment.ci_cd_iam_policy_attachment
+}
+moved {
+  from = aws_iam_policy.circle_ci_iam_policy
+  to   = module.ci-cd-permissions.aws_iam_policy.ci_cd_iam_policy
 }
