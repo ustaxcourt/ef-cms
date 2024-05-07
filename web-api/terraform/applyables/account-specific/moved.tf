ff2ba0055431af2cf64ae23c6dfe2373097ee49d
Zachary Rogers <zrogers>
2024-05-07 10:30:05 -0700

    10345: WIP creating moved terraform file for refactoring account-specific without destroying. system-health alarms and cloudwatch permissions

--- web-api/terraform/applyables/account-specific/moved.tf
+++ web-api/terraform/applyables/account-specific/moved.tf
@@ -1,5 +1,7 @@
-# This file exists to allow us to refactor account-specific to modules
+# This file exists to allow us to refactor account-specific to modules.
+# This file can be deleted once account-specific deploy has been run on every account.
 
+# Health alarms
 moved {
   from = aws_sns_topic.system_health_alarms
   to   = module.health-alarms-east.aws_sns_topic.system_health_alarms
@@ -8,4 +10,14 @@ moved {
 moved {
   from = aws_sns_topic.system_health_alarms_west
   to   = module.health-alarms-west.aws_sns_topic.system_health_alarms
+}
+
+# api-gateway-cloudwatch.tf
+moved {
+  from = aws_iam_role.cloudwatch
+  to   = module.api_gateway_global_logging_permissions.aws_iam_role.cloudwatch
+}
+moved {
+  from = aws_iam_role_policy.cloudwatch
+  to   = module.api_gateway_global_logging_permissions.aws_iam_role_policy.cloudwatch
 }
