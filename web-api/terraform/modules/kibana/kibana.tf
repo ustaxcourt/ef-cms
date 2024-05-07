2dca89b7a8bbcca79b9bc239d1a55d3bda8c58f4
Rachel Schneiderman <rschneiderman379@gmail.com>
2024-05-07 14:38:47 -0700

    10345: Move more opensearch/kibana resources to new applyables format

--- web-api/terraform/modules/kibana/kibana.tf
+++ web-api/terraform/modules/kibana/kibana.tf
@@ -216,3 +216,54 @@ module "logs_alarms" {
   create_sns_topic             = false
   sns_topic                    = var.sns_alarm_arn
 }
+
+resource "aws_iam_role" "lambda_elasticsearch_execution_role" {
+  name = "lambda_elasticsearch_execution_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "lambda_elasticsearch_execution_policy" {
+  name = "lambda_elasticsearch_execution_policy"
+  role = aws_iam_role.lambda_elasticsearch_execution_role.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": [
+        "arn:aws:logs:*:*:*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "es:*"
+      ],
+      "Resource": [
+        "${aws_opensearch_domain.efcms-logs.arn}/*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
