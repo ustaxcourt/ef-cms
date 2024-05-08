49d8b5a292b7db53925acedc4ec657ad7ee012cd
John Cruz <jcruz@flexion.us>
2024-05-08 11:21:51 -0700

    5512: Add live validation for step 1;

--- web-client/src/presenter/sequences/petitionGenerationLiveValidationSequence.ts
+++ web-client/src/presenter/sequences/petitionGenerationLiveValidationSequence.ts
@@ -1,8 +1,10 @@
 import { determineStepPathAction } from '@web-client/presenter/actions/determineStepPathAction';
+import { getStep1DataAction } from '@web-client/presenter/actions/getStep1DataAction';
 import { getStep3DataAction } from '@web-client/presenter/actions/getStep3DataAction';
 import { getStep4DataAction } from '@web-client/presenter/actions/getStep4DataAction';
 import { setSingleValidationErrorAction } from '@web-client/presenter/actions/getSingleValidationMessageAction';
 import { startShowValidationAction } from '@web-client/presenter/actions/startShowValidationAction';
+import { validateUploadPetitionStep1Action } from '@web-client/presenter/actions/validateUploadPetitionStep1Action';
 import { validateUploadPetitionStep3Action } from '@web-client/presenter/actions/validateUploadPetitionStep3Action';
 import { validateUploadPetitionStep4Action } from '@web-client/presenter/actions/validateUploadPetitionStep4Action';
 
@@ -15,6 +17,11 @@ export const petitionGenerationLiveValidationSequence = [
   startShowValidationAction,
   determineStepPathAction,
   {
+    step1: [
+      getStep1DataAction,
+      validateUploadPetitionStep1Action,
+      handleValidationErrorsRoute,
+    ],
     step3: [
       getStep3DataAction,
       validateUploadPetitionStep3Action,
