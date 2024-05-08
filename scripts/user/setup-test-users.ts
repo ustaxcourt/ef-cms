b7c181df132043665260525910895afaf7dd10ce
Zachary Rogers <zrogers>
2024-05-07 17:01:56 -0700

    devex: Make creating users run serially instead of in parallel so it is not throttled.

--- scripts/user/setup-test-users.ts
+++ scripts/user/setup-test-users.ts
@@ -74,10 +74,10 @@ const setupCourtUsers = async (
     [5, 'chambers', 'kerrigansChambers'],
   ];
 
-  const promises = userSet.map(user =>
-    createManyAccounts(applicationContext, user),
-  );
-  await Promise.all(promises);
+  for (let index = 0; index < userSet.length; index++) {
+    const user = userSet[index];
+    await createManyAccounts(applicationContext, user); // Not doing parallel because of cognito throttling
+  }
 };
 
 const setupPetitioners = async (
@@ -93,74 +93,68 @@ const setupPetitioners = async (
 const setupPractitioners = async (
   applicationContext: ServerApplicationContext,
 ) => {
-  const practitioners = {
-    irsPractitioner: [
-      'RT6789',
-      'RT0987',
-      'RT7777',
-      'RT8888',
-      'RT9999',
-      'RT6666',
-      'RT0000',
-      'RT1111',
-      'RT2222',
-      'RT3333',
-    ],
-    privatePractitioner: [
-      'PT1234',
-      'PT5432',
-      'PT1111',
-      'PT2222',
-      'PT3333',
-      'PT4444',
-      'PT5555',
-      'PT6666',
-      'PT7777',
-      'PT8888',
-    ],
-  };
+  const allPractitioners = [
+    { barNumber: 'RT6789', role: 'irsPractitioner' },
+    { barNumber: 'RT0987', role: 'irsPractitioner' },
+    { barNumber: 'RT7777', role: 'irsPractitioner' },
+    { barNumber: 'RT8888', role: 'irsPractitioner' },
+    { barNumber: 'RT9999', role: 'irsPractitioner' },
+    { barNumber: 'RT6666', role: 'irsPractitioner' },
+    { barNumber: 'RT0000', role: 'irsPractitioner' },
+    { barNumber: 'RT1111', role: 'irsPractitioner' },
+    { barNumber: 'RT2222', role: 'irsPractitioner' },
+    { barNumber: 'RT3333', role: 'irsPractitioner' },
+    { barNumber: 'PT1234', role: 'privatePractitioner' },
+    { barNumber: 'PT5432', role: 'privatePractitioner' },
+    { barNumber: 'PT1111', role: 'privatePractitioner' },
+    { barNumber: 'PT2222', role: 'privatePractitioner' },
+    { barNumber: 'PT3333', role: 'privatePractitioner' },
+    { barNumber: 'PT4444', role: 'privatePractitioner' },
+    { barNumber: 'PT5555', role: 'privatePractitioner' },
+    { barNumber: 'PT6666', role: 'privatePractitioner' },
+    { barNumber: 'PT7777', role: 'privatePractitioner' },
+    { barNumber: 'PT8888', role: 'privatePractitioner' },
+  ];
 
-  for (let role in practitioners) {
-    const promises = practitioners[role].map((barNumber, i) => {
-      const practiceType = role === 'privatePractitioner' ? 'Private' : 'IRS';
-      const email = `${role}${i + 1}@example.com`;
-      const user = {
-        admissionsDate: '2019-03-01',
-        admissionsStatus: 'Active',
-        barNumber,
-        birthYear: '1950',
-        contact: {
-          address1: '234 Main St',
-          address2: 'Apartment 4',
-          address3: 'Under the stairs',
-          city: 'Chicago',
-          countryType: 'domestic',
-          phone: '+1 (555) 555-5555',
-          postalCode: '61234',
-          state: 'IL',
-        },
-        email,
-        firmName: 'Some Firm',
-        firstName: `${role} ${i + 1}`,
-        lastName: 'Test',
-        name: `Test ${role}${i + 1}`,
-        originalBarState: 'WA',
-        password: DEFAULT_ACCOUNT_PASS,
-        practiceType,
-        practitionerType: 'Attorney',
-        role,
-        section: role,
-        suffix: '',
-      };
+  for (let i = 0; i < allPractitioners.length; i++) {
+    const { barNumber, role } = allPractitioners[i];
 
-      return createOrUpdateUser(applicationContext, {
-        password: DEFAULT_ACCOUNT_PASS!,
-        setPasswordAsPermanent: true,
-        user,
-      });
+    const practiceType = role === 'privatePractitioner' ? 'Private' : 'IRS';
+    const email = `${role}${i + 1}@example.com`;
+    const user = {
+      admissionsDate: '2019-03-01',
+      admissionsStatus: 'Active',
+      barNumber,
+      birthYear: '1950',
+      contact: {
+        address1: '234 Main St',
+        address2: 'Apartment 4',
+        address3: 'Under the stairs',
+        city: 'Chicago',
+        countryType: 'domestic',
+        phone: '+1 (555) 555-5555',
+        postalCode: '61234',
+        state: 'IL',
+      },
+      email,
+      firmName: 'Some Firm',
+      firstName: `${role} ${i + 1}`,
+      lastName: 'Test',
+      name: `Test ${role}${i + 1}`,
+      originalBarState: 'WA',
+      password: DEFAULT_ACCOUNT_PASS,
+      practiceType,
+      practitionerType: 'Attorney',
+      role,
+      section: role,
+      suffix: '',
+    };
+
+    await createOrUpdateUser(applicationContext, {
+      password: DEFAULT_ACCOUNT_PASS!,
+      setPasswordAsPermanent: true,
+      user,
     });
-
-    await Promise.all(promises);
   }
 };
 
