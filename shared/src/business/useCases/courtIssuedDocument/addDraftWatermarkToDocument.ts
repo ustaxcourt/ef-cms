2dbf8b18dafaf92affb24517bf1417ba04b9625d
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-05-12 16:28:13 -0500

    5512: add DRAFT watermark to petition pdf

---
+++ shared/src/business/useCases/courtIssuedDocument/addDraftWatermarkToDocument.ts
@@ -0,0 +1,45 @@
+const WATERMARK_TEXT = 'DRAFT';
+
+export const addDraftWatermarkToDocument = async ({
+  applicationContext,
+  pdfData,
+}) => {
+  const { degrees, rgb } = await applicationContext.getPdfLib();
+
+  const { pdfDoc, textFont } = await applicationContext
+    .getUtilities()
+    .setupPdfDocument({
+      applicationContext,
+      pdfData,
+    });
+
+  const pages = pdfDoc.getPages();
+  pages.forEach(page => {
+    const { width } = page.getSize();
+
+    let fontSize = 200;
+    let textWidth = textFont.widthOfTextAtSize(WATERMARK_TEXT, fontSize);
+
+    while (textWidth > width * 0.8) {
+      fontSize -= 1;
+      textWidth = textFont.widthOfTextAtSize(WATERMARK_TEXT, fontSize);
+    }
+
+    const centerX = width / 2 - textWidth / 2;
+    const centerY = page.getSize().height / 1.85;
+
+    page.drawText(WATERMARK_TEXT, {
+      color: rgb(0, 0, 0),
+      opacity: 0.15,
+      rotate: degrees(-15),
+      size: fontSize,
+      textFont,
+      x: centerX,
+      y: centerY,
+    });
+  });
+
+  return await pdfDoc.save({
+    useObjectStreams: false,
+  });
+};
