a586b2d77b28f25b244e9c4d9a5137b1bff44fc1
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-05-12 16:22:42 -0500

    5512: get petition information from props instead of state.form

--- web-client/src/presenter/sequences/updatedFilePetitionCompleteStep5Sequence.ts
+++ web-client/src/presenter/sequences/updatedFilePetitionCompleteStep5Sequence.ts
@@ -1,5 +1,9 @@
 import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { generatePetitionPdfAction } from '@web-client/presenter/actions/generatePetitionPdfAction';
+import { getStep1DataAction } from '@web-client/presenter/actions/getStep1DataAction';
+import { getStep2DataAction } from '@web-client/presenter/actions/getStep2DataAction';
+import { getStep3DataAction } from '@web-client/presenter/actions/getStep3DataAction';
+import { getStep4DataAction } from '@web-client/presenter/actions/getStep4DataAction';
 import { getStep5DataAction } from '@web-client/presenter/actions/getStep5DataAction';
 import { incrementCurrentStepIndicatorAction } from '@web-client/presenter/actions/incrementCurrentStepIndicatorAction';
 import { setPdfPreviewUrlAction } from '@web-client/presenter/actions/CourtIssuedOrder/setPdfPreviewUrlAction';
@@ -19,6 +23,10 @@ export const updatedFilePetitionCompleteStep5Sequence = [
     success: showProgressSequenceDecorator([
       clearAlertsAction,
       stopShowValidationAction,
+      getStep1DataAction,
+      getStep2DataAction,
+      getStep3DataAction,
+      getStep4DataAction,
       generatePetitionPdfAction,
       setPdfPreviewUrlAction,
       incrementCurrentStepIndicatorAction,
