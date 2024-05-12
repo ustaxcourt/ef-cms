ae67a525f8c1715ff8409954052e75506c698c63
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-05-12 14:13:39 -0500

    5512: fix validation for facts and reasons

--- web-client/src/presenter/actions/validateUploadPetitionStep1Action.ts
+++ web-client/src/presenter/actions/validateUploadPetitionStep1Action.ts
@@ -1,12 +1,26 @@
 import { UploadPetitionStep1 } from '@shared/business/entities/startCase/UploadPetitionStep1';
+import { state } from '@web-client/presenter/app.cerebral';
 
 export const validateUploadPetitionStep1Action = ({
   path,
   props,
+  store,
 }: ActionProps<{ step1Data: any }>) => {
   const { step1Data } = props;
 
-  const errors = new UploadPetitionStep1(
+  // move this logic to entity?
+  step1Data.petitionReasons = (arr => (arr.length > 0 ? arr : ['']))(
+    step1Data.petitionReasons.filter(r => r.length > 1),
+  );
+
+  step1Data.petitionFacts = (arr => (arr.length > 0 ? arr : ['']))(
+    step1Data.petitionFacts.filter(r => r.length > 1),
+  );
+
+  store.set(state.form.petitionReasons, step1Data.petitionReasons);
+  store.set(state.form.petitionFacts, step1Data.petitionFacts);
+
+  let errors = new UploadPetitionStep1(
     step1Data,
   ).getFormattedValidationErrors();
 
