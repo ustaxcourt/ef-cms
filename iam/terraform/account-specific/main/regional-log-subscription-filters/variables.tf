6118924bee9ec8abced82dcc9549908ccc389f1f
Zachary Rogers <zrogers>
2024-05-13 15:06:39 -0700

    10345: Remove account specific resource files that are no longer needed as they have been moved to applyables

--- iam/terraform/account-specific/main/regional-log-subscription-filters/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "logs_to_elasticsearch_lambda_arn" {
-  type = string
-}
-
-variable "log_group_environments" {
-  type = list(string)
-}
--- iam/terraform/account-specific/main/route-53.tf
+++
@@ -1,3 +0,0 @@
-# resource "aws_route53_zone" "primary" {
-#   name = "${var.zone_name}"
-# }
