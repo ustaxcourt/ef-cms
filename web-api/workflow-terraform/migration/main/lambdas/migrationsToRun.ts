3c69802f02d214fd02ed97fc2467cb52399e9e3b
Rachel Schneiderman <rschneiderman379@gmail.com>
2024-05-13 10:57:57 -0700

    Devex: Remove deleted script from migrations to run

--- web-api/workflow-terraform/migration/main/lambdas/migrationsToRun.ts
+++ web-api/workflow-terraform/migration/main/lambdas/migrationsToRun.ts
@@ -1,5 +1,1 @@
-import { migrateItems as migrate0002 } from './migrations/0002-change-employer-to-practice-type';
-
-export const migrationsToRun = [
-  { key: '0002-change-employer-to-practice-type.ts', script: migrate0002 },
-];
+export const migrationsToRun = [];
