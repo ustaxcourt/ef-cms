61f22f2f8fbdbe3f98fb890e41ee7e8ff7352202
Andy Kuny <akuny@flexion.us>
2024-05-14 14:25:21 -0400

    Add specific validation message for payment method that exceeds 50 character limit

--- shared/src/business/entities/cases/Case.ts
+++ shared/src/business/entities/cases/Case.ts
@@ -577,7 +577,10 @@ export class Case extends JoiValidationEntity {
         then: joi.required(),
       })
       .description('How the petitioner paid the case fee.')
-      .messages({ '*': 'Enter payment method' }),
+      .messages({
+        '*': 'Enter payment method',
+        'string.max': 'Payment method must contain fewer than 50 characters',
+      }),
     petitionPaymentStatus: JoiValidationConstants.STRING.valid(
       ...Object.values(PAYMENT_STATUS),
     )
