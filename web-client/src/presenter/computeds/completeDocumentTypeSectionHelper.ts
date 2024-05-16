26854b1808ffb7545d11e8a6f3d8bfb8c0ba9cd4
Nate Elliott <nelliott@flexion.us>
2024-05-16 09:01:11 -0500

    10350: Only allow EA for first IRS filing

--- web-client/src/presenter/computeds/completeDocumentTypeSectionHelper.ts
+++ web-client/src/presenter/computeds/completeDocumentTypeSectionHelper.ts
@@ -39,6 +39,16 @@ export const completeDocumentTypeSectionHelper = (
       currentUser.role === USER_ROLES.irsPractitioner &&
       documentType.eventCode === 'EA'
     ) {
+      const isCaseSealed = applicationContext
+        .getUtilities()
+        .isSealedCase(caseDetail);
+
+      const caseHasRespondent = !!(
+        !!caseDetail.hasIrsPractitioner || caseDetail.irsPractitioners?.length
+      );
+
+      let showFileFirstDocumentButton = !caseHasRespondent && !isCaseSealed;
+      if (!showFileFirstDocumentButton) return false;
       documentType.documentTitle += ' for Respondent';
     } else if (documentType.eventCode === 'EA') return false;
 
