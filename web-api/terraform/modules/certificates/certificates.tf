e0b10951f333d63064f3d233b3f675088449a05f
Zachary Rogers <zrogers>
2024-05-20 15:46:55 -0700

    10345: Move certificates module into new modules pattern and have allColors deploy them

--- web-api/terraform/modules/certificates/certificates.tf
+++ web-api/terraform/modules/certificates/certificates.tf
@@ -1,3 +1,8 @@
+data "aws_route53_zone" "zone" {
+  name         = var.hosted_zone_name
+  private_zone = "false"
+}
+
 resource "aws_acm_certificate" "this" {
   domain_name       = var.domain_name
   validation_method = "DNS"
