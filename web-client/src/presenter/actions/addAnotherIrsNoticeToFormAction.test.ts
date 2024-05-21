d4e3b65132975210d045be858b925df7a7243c04
John Cruz <jcruz@flexion.us>
2024-05-21 11:06:04 -0600

    5512: Create unit test for "addAnotherIrsNoticeToFormAction";

---
+++ web-client/src/presenter/actions/addAnotherIrsNoticeToFormAction.test.ts
@@ -0,0 +1,43 @@
+import { FORMATS } from '@shared/business/utilities/DateHandler';
+import { addAnotherIrsNoticeToFormAction } from '@web-client/presenter/actions/addAnotherIrsNoticeToFormAction';
+import { applicationContextForClient as applicationContext } from '@web-client/test/createClientTestApplicationContext';
+import { presenter } from '../presenter-mock';
+import { runAction } from '@web-client/presenter/test.cerebral';
+
+describe('addAnotherIrsNoticeToFormAction', () => {
+  const TEST_GUID = 'TEST_GUID';
+  const TEST_NOW_DATE = 'TEST_NOW_DATE';
+
+  beforeEach(() => {
+    applicationContext.getUniqueId.mockImplementation(() => TEST_GUID);
+    applicationContext
+      .getUtilities()
+      .formatNow.mockImplementation(() => TEST_NOW_DATE);
+
+    presenter.providers.applicationContext = applicationContext;
+  });
+
+  it('should add a new IRS form into state with correct initial data', async () => {
+    const result = await runAction(addAnotherIrsNoticeToFormAction, {
+      modules: { presenter },
+      state: {
+        irsNoticeUploadFormInfo: [],
+      },
+    });
+
+    const { irsNoticeUploadFormInfo } = result.state;
+    expect(irsNoticeUploadFormInfo.length).toEqual(1);
+
+    const formatNowCalls =
+      applicationContext.getUtilities().formatNow.mock.calls;
+
+    expect(formatNowCalls.length).toEqual(1);
+    expect(formatNowCalls[0][0]).toEqual(FORMATS.YYYYMMDD);
+
+    const irsForm = irsNoticeUploadFormInfo[0];
+    expect(irsForm).toEqual({
+      key: TEST_GUID,
+      todayDate: TEST_NOW_DATE,
+    });
+  });
+});
