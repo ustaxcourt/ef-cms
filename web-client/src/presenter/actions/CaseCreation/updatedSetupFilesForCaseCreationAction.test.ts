8425e47a748ea15d33e687cc02b0e309f0d25171
John Cruz <jcruz@flexion.us>
2024-05-22 08:59:14 -0600

    5512: Create unit test for "updatedSetupFilesForCaseCreationAction";

---
+++ web-client/src/presenter/actions/CaseCreation/updatedSetupFilesForCaseCreationAction.test.ts
@@ -0,0 +1,64 @@
+import { runAction } from '@web-client/presenter/test.cerebral';
+import { updatedSetupFilesForCaseCreationAction } from '@web-client/presenter/actions/CaseCreation/updatedSetupFilesForCaseCreationAction';
+
+describe('updatedSetupFilesForCaseCreationAction', () => {
+  it('should return all files from petitionFormatted in state', async () => {
+    const result = await runAction(updatedSetupFilesForCaseCreationAction, {
+      state: {
+        petitionFormatted: {
+          applicationForWaiverOfFilingFeeFile:
+            'TEST_applicationForWaiverOfFilingFeeFile',
+          attachmentToPetitionFile: 'TEST_attachmentToPetitionFile',
+          corporateDisclosureFile: 'TEST_corporateDisclosureFile',
+          petitionFile: 'TEST_petitionFile',
+          primaryDocumentFile: 'TEST_primaryDocumentFile',
+          requestForPlaceOfTrialFile: 'TEST_requestForPlaceOfTrialFile',
+          stinFile: 'TEST_stinFile',
+        },
+      },
+    });
+
+    expect(result.output).toEqual({
+      files: {
+        applicationForWaiverOfFilingFee:
+          'TEST_applicationForWaiverOfFilingFeeFile',
+        attachmentToPetition: 'TEST_attachmentToPetitionFile',
+        corporateDisclosure: 'TEST_corporateDisclosureFile',
+        petition: 'TEST_petitionFile',
+        primary: 'TEST_primaryDocumentFile',
+        requestForPlaceOfTrial: 'TEST_requestForPlaceOfTrialFile',
+        stin: 'TEST_stinFile',
+      },
+    });
+  });
+
+  it('should return all files from petitionFormatted in state and use default when petitionFile is falsey', async () => {
+    const result = await runAction(updatedSetupFilesForCaseCreationAction, {
+      state: {
+        petitionFormatted: {
+          applicationForWaiverOfFilingFeeFile:
+            'TEST_applicationForWaiverOfFilingFeeFile',
+          attachmentToPetitionFile: 'TEST_attachmentToPetitionFile',
+          corporateDisclosureFile: 'TEST_corporateDisclosureFile',
+          petitionFile: '',
+          primaryDocumentFile: 'TEST_primaryDocumentFile',
+          requestForPlaceOfTrialFile: 'TEST_requestForPlaceOfTrialFile',
+          stinFile: 'TEST_stinFile',
+        },
+      },
+    });
+
+    expect(result.output).toEqual({
+      files: {
+        applicationForWaiverOfFilingFee:
+          'TEST_applicationForWaiverOfFilingFeeFile',
+        attachmentToPetition: 'TEST_attachmentToPetitionFile',
+        corporateDisclosure: 'TEST_corporateDisclosureFile',
+        petition: undefined,
+        primary: 'TEST_primaryDocumentFile',
+        requestForPlaceOfTrial: 'TEST_requestForPlaceOfTrialFile',
+        stin: 'TEST_stinFile',
+      },
+    });
+  });
+});
