ef89d99d390e297abcdc3eb3be8124603f455685
Zachary Rogers <zrogers>
2024-05-23 13:04:20 -0700

    10345: Add more descriptive logs for script. Update basePath lambda to not use callbacks

--- web-api/terraform/applyables/blue/move-client-resources-blue.sh
+++ web-api/terraform/applyables/blue/move-client-resources-blue.sh
@@ -17,23 +17,23 @@ set -eo pipefail
 
 rm -rf .terraform/ .terraform.lock.hcl
 terraform init -upgrade -backend=true -backend-config=bucket="${ZONE_NAME}.terraform.deploys" -backend-config=key="documents-${ENV}-blue.tfstate" -backend-config=dynamodb_table="efcms-terraform-lock" -backend-config=region="us-east-1"
-echo 111111111
+echo init blue complete
 terraform state pull > "./documents-${ENV}-blue.tfstate" # A
-echo 2222222222
+echo blue state pull complete
 
 cd "../../../../web-client/terraform/main"
 rm -rf .terraform/ .terraform.lock.hcl
 terraform init -upgrade -backend=true -backend-config=bucket="${ZONE_NAME}.terraform.deploys" -backend-config=key="ui-${ENV}.tfstate" -backend-config=dynamodb_table="efcms-terraform-lock" -backend-config=region="us-east-1"
-echo 33333333
+echo ui init complete
 terraform state pull > "./ui-${ENV}.tfstate" # B
-echo 4444444
+echo ui state pull complete
 
 cd "../../../web-api/terraform/applyables/blue"
 
 terraform state mv -state="../../../../web-client/terraform/main/ui-${ENV}.tfstate" -state-out="documents-${ENV}-blue.tfstate" module.environment.module.ui-blue module.ui-blue
 
-echo 5555555
+echo state mv complete
 
 terraform state push "./documents-${ENV}-blue.tfstate"
-echo 6666666
+echo state push complete
 
--- web-api/terraform/applyables/green/move-client-resources-green.sh
+++ web-api/terraform/applyables/green/move-client-resources-green.sh
@@ -17,23 +17,23 @@ set -eo pipefail
 
 rm -rf .terraform/ .terraform.lock.hcl
 terraform init -upgrade -backend=true -backend-config=bucket="${ZONE_NAME}.terraform.deploys" -backend-config=key="documents-${ENV}-green.tfstate" -backend-config=dynamodb_table="efcms-terraform-lock" -backend-config=region="us-east-1"
-echo 111111111
+echo init blue complete
 terraform state pull > "./documents-${ENV}-green.tfstate" # A
-echo 2222222222
+echo blue state pull complete
 
 cd "../../../../web-client/terraform/main"
 rm -rf .terraform/ .terraform.lock.hcl
 terraform init -upgrade -backend=true -backend-config=bucket="${ZONE_NAME}.terraform.deploys" -backend-config=key="ui-${ENV}.tfstate" -backend-config=dynamodb_table="efcms-terraform-lock" -backend-config=region="us-east-1"
-echo 33333333
+echo ui init complete
 terraform state pull > "./ui-${ENV}.tfstate" # B
-echo 4444444
+echo ui state pull complete
 
 cd "../../../web-api/terraform/applyables/green"
 
 terraform state mv -state="../../../../web-client/terraform/main/ui-${ENV}.tfstate" -state-out="documents-${ENV}-green.tfstate" module.environment.module.ui-green module.ui-green
 
-echo 5555555
+echo state mv complete
 
 terraform state push "./documents-${ENV}-green.tfstate"
-echo 6666666
+echo state push complete
 
