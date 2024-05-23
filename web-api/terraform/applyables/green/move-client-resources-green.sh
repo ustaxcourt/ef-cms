b34895333ca2b70a60f49979bfb64f852476db0a
Zachary Rogers <zrogers>
2024-05-23 10:59:23 -0700

    10345: Auto clean .terraform directory for easier steps

--- web-api/terraform/applyables/green/move-client-resources-green.sh
+++ web-api/terraform/applyables/green/move-client-resources-green.sh
@@ -15,12 +15,14 @@ fi
 # exit on any failure
 set -eo pipefail
 
+rm -rf .terraform/ .terraform.lock.hcl
 terraform init -upgrade -backend=true -backend-config=bucket="${ZONE_NAME}.terraform.deploys" -backend-config=key="documents-${ENV}-green.tfstate" -backend-config=dynamodb_table="efcms-terraform-lock" -backend-config=region="us-east-1"
 echo 111111111
 terraform state pull > "./documents-${ENV}-green.tfstate" # A
 echo 2222222222
 
 cd "../../../../web-client/terraform/main"
+rm -rf .terraform/ .terraform.lock.hcl
 terraform init -upgrade -backend=true -backend-config=bucket="${ZONE_NAME}.terraform.deploys" -backend-config=key="ui-${ENV}.tfstate" -backend-config=dynamodb_table="efcms-terraform-lock" -backend-config=region="us-east-1"
 echo 33333333
 terraform state pull > "./ui-${ENV}.tfstate" # B
