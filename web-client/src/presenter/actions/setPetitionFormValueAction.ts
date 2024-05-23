5502698d5fb9dfda2ba5a9de91ad3a091417c034
John Cruz <jcruz@flexion.us>
2024-05-23 11:26:44 -0600

    5512: Create unit test for "setPetitionFormValueAction";

--- web-client/src/presenter/actions/setPetitionFormValueAction.ts
+++ web-client/src/presenter/actions/setPetitionFormValueAction.ts
@@ -1,13 +1,17 @@
 import { state } from '@web-client/presenter/app.cerebral';
 
-export const setPetitionFormValueAction = ({ props, store }: ActionProps) => {
-  if (props.value !== null) {
-    if (props.index || props.index === 0) {
-      store.set(state.form[props.key][props.index], props.value);
+export const setPetitionFormValueAction = ({
+  props,
+  store,
+}: ActionProps<{ index: number; key: string; value: any }>) => {
+  const { index, key, value } = props;
+  if (value !== null) {
+    if (index || index === 0) {
+      store.set(state.form[key][index], value);
     } else {
-      store.set(state.form[props.key], props.value);
+      store.set(state.form[key], value);
     }
   } else {
-    store.unset(state.form[props.key]);
+    store.unset(state.form[key]);
   }
 };
