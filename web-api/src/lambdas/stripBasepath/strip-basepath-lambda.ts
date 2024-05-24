e58149e5ae55bc2a8677400cf4427d19a6c57a5a
Zachary Rogers <zrogers>
2024-05-24 12:39:17 -0700

    10345: Make AWS lambda handlers async to try to resolve return issues

--- web-api/src/lambdas/stripBasepath/strip-basepath-lambda.ts
+++ web-api/src/lambdas/stripBasepath/strip-basepath-lambda.ts
@@ -1,8 +1,10 @@
+// AWS lambda handlers must be async to work properly
+/* eslint-disable require-await */
 import { CloudFrontRequest, CloudFrontRequestEvent } from 'aws-lambda';
 
-export const handler = (
+export const handler = async (
   awsEvent: CloudFrontRequestEvent,
-): CloudFrontRequest => {
+): Promise<CloudFrontRequest> => {
   const { request } = awsEvent.Records[0].cf;
   request.uri = `/${[...request.uri.split('/').slice(2)].join('/')}`;
   return request;
