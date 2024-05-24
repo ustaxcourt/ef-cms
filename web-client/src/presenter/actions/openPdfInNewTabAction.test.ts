179126566e7b37617fad89df7f6655f28f9b5aac
Andy Kuny <akuny@flexion.us>
2024-05-24 15:56:07 -0400

    Add foundational test for openPdfInNewTabAction.ts

---
+++ web-client/src/presenter/actions/openPdfInNewTabAction.test.ts
@@ -0,0 +1,48 @@
+import { applicationContextForClient as applicationContext } from '@web-client/test/createClientTestApplicationContext';
+import { openPdfInNewTabAction } from './openPdfInNewTabAction';
+import { presenter } from '../presenter-mock';
+import { runAction } from '@web-client/presenter/test.cerebral';
+import { testPdfDoc } from '../../../../shared/src/business/test/getFakeFile';
+
+describe('openPdfInNewTabAction', () => {
+  const fakeFile = testPdfDoc;
+  const fakeUrl = new URL('http://www.fakeurl.com/test').toString();
+
+  const mockReadArrayBuffer = jest.fn().mockImplementation(async function () {
+    this.result = fakeFile;
+    await this.onload();
+  });
+
+  function MockFileReader() {
+    this.onload = null;
+    this.onerror = null;
+    this.readAsArrayBuffer = mockReadArrayBuffer;
+  }
+
+  beforeAll(() => {
+    applicationContext.getFileReaderInstance.mockReturnValue(
+      new MockFileReader(),
+    );
+    presenter.providers.applicationContext = applicationContext;
+    presenter.providers.router = {
+      createObjectURL: jest.fn().mockImplementation(() => {
+        return fakeUrl;
+      }),
+    };
+    window.open = jest.fn();
+  });
+
+  it('opens a valid PDF in a new tab', async () => {
+    await runAction(openPdfInNewTabAction, {
+      modules: {
+        presenter,
+      },
+      props: {
+        file: fakeFile,
+      },
+    });
+
+    expect(mockReadArrayBuffer).toHaveBeenCalled();
+    expect(window.open).toHaveBeenCalledWith(fakeUrl.toString(), '_blank');
+  });
+});
