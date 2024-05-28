6ce43332cd5b86c85a2bfe465166da6051cfd5fc
Rachel Schneiderman <rschneiderman379@gmail.com>
2024-05-28 13:27:06 -0700

    Devex 10382: Look for correct selector for the page under test

--- cypress/accessibility/integration/authentication/create-petitioner-account.cy.ts
+++ cypress/accessibility/integration/authentication/create-petitioner-account.cy.ts
@@ -8,7 +8,7 @@ describe('Login Page Accessibility', () => {
   it('should be free of a11y issues', () => {
     cy.visit('/create-account/petitioner');
 
-    cy.get('[data-testid="email-input"]');
+    cy.get('[data-testid="create-petitioner-account-container"]');
 
     cy.injectAxe();
 
