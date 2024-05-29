ceb5430d390ea4d653bfbc20bf43de6da0ed6496
Rachel Schneiderman <rschneiderman379@gmail.com>
2024-05-29 15:20:45 -0700

    Devex 10382: Convert admissions clerk pa11y test to cypress axe

--- cypress/local-only/tests/accessibility/editPetitionerInformationInternal/admissions-clerk.cy.ts
+++ cypress/local-only/tests/accessibility/editPetitionerInformationInternal/admissions-clerk.cy.ts
@@ -25,4 +25,66 @@ describe('Edit Petitioner Information - Admissions Clerk Accessibility', () => {
       terminalLog,
     );
   });
+
+  describe('Matching Email Found Modal', () => {
+    it('should be free of a11y issues', () => {
+      loginAsAdmissionsClerk();
+
+      cy.visit(
+        '/case-detail/124-20/edit-petitioner-information/d2fadb14-b0bb-4019-b6b1-cb51cb1cb92f',
+      );
+      cy.get('[data-testid="edit-petitioner-contact-type-select"]');
+      cy.get('[data-testid="internal-edit-petitioner-email-input"]').type(
+        'petitioner1@example.com',
+      );
+      cy.get('[data-testid="internal-confirm-petitioner-email-input"]').type(
+        'petitioner1@example.com',
+      );
+      cy.get(
+        '[data-testid="submit-edit-petitioner-information-button"]',
+      ).click();
+      cy.get('[data-testid="matching-email-found-modal"]');
+
+      cy.injectAxe();
+
+      cy.checkA11y(
+        undefined,
+        {
+          includedImpacts: impactLevel,
+        },
+        terminalLog,
+      );
+    });
+  });
+
+  describe('No Matching Email Found Modal', () => {
+    it('should be free of a11y issues', () => {
+      loginAsAdmissionsClerk();
+
+      cy.visit(
+        '/case-detail/124-20/edit-petitioner-information/d2fadb14-b0bb-4019-b6b1-cb51cb1cb92f',
+      );
+      cy.get('[data-testid="edit-petitioner-contact-type-select"]');
+      cy.get('[data-testid="internal-edit-petitioner-email-input"]').type(
+        'available@example.com',
+      );
+      cy.get('[data-testid="internal-confirm-petitioner-email-input"]').type(
+        'available@example.com',
+      );
+      cy.get(
+        '[data-testid="submit-edit-petitioner-information-button"]',
+      ).click();
+      cy.get('[data-testid="no-matching-email-found-modal"]');
+
+      cy.injectAxe();
+
+      cy.checkA11y(
+        undefined,
+        {
+          includedImpacts: impactLevel,
+        },
+        terminalLog,
+      );
+    });
+  });
 });
