521f50775919d3c0e2520d6d39d80597a8189b9a
Zachary Rogers <zrogers>
2024-05-29 13:37:06 -0700

    devex: move consolidatedCase to web-api

--- web-api/src/business/useCases/caseConsolidation/addConsolidatedCaseInteractor.test.ts
+++ web-api/src/business/useCases/caseConsolidation/addConsolidatedCaseInteractor.test.ts
@@ -1,9 +1,9 @@
-import { MOCK_CASE } from '../../../test/mockCase';
-import { MOCK_LOCK } from '../../../test/mockLock';
-import { ROLES } from '../../entities/EntityConstants';
+import { MOCK_CASE } from '../../../../../shared/src/test/mockCase';
+import { MOCK_LOCK } from '../../../../../shared/src/test/mockLock';
+import { ROLES } from '../../../../../shared/src/business/entities/EntityConstants';
 import { ServiceUnavailableError } from '@web-api/errors/errors';
 import { addConsolidatedCaseInteractor } from './addConsolidatedCaseInteractor';
-import { applicationContext } from '../../test/createTestApplicationContext';
+import { applicationContext } from '../../../../../shared/src/business/test/createTestApplicationContext';
 
 let mockCases;
 let mockLock;
