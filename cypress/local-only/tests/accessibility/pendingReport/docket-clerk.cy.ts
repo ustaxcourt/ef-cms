858db06c96954345e862b5e07074cc82f9ec8bb3
Rachel Schneiderman <rschneiderman379@gmail.com>
2024-05-30 12:02:14 -0700

    Devex 10382: WIP - Convert another docket clerk pa11y test to cypress axe

---
+++ cypress/local-only/tests/accessibility/pendingReport/docket-clerk.cy.ts
@@ -0,0 +1,26 @@
+import { impactLevel } from '../../../../helpers/accessibility-impact';
+import { loginAsDocketClerk } from '../../../../helpers/authentication/login-as-helpers';
+import { terminalLog } from '../../../../helpers/cypressTasks/logs';
+
+describe('Pending Report - Docket Clerk Accessibility', () => {
+  beforeEach(() => {
+    Cypress.session.clearCurrentSessionData();
+  });
+
+  it('should be free of a11y issues', () => {
+    loginAsDocketClerk();
+
+    cy.visit('/reports/pending-report');
+    cy.get('[data-testid="pending-report-container"]');
+
+    cy.injectAxe();
+
+    cy.checkA11y(
+      undefined,
+      {
+        includedImpacts: impactLevel,
+      },
+      terminalLog,
+    );
+  });
+});
