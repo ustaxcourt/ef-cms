1f0c6c57f823ef05cfbcb3247c951da9db0ff68a
Rachel Schneiderman <rschneiderman379@gmail.com>
2024-06-03 16:23:15 -0700

    Devex 10382: Fix tests and try running without disabling color contrast rule, organize accessibility tests more

--- cypress/local-only/tests/accessibility/petitionQc/petitions-clerk.cy.ts
+++ cypress/local-only/tests/accessibility/petitionQc/petitions-clerk.cy.ts
@@ -7,81 +7,71 @@ describe('Petition QC - Petitions Clerk Accessibility', () => {
     Cypress.session.clearCurrentSessionData();
   });
 
-  it('should be free of a11y issues for confirm replace petition modal', () => {
-    loginAsPetitionsClerk();
-    cy.visit('/case-detail/121-20/petition-qc');
-    cy.contains('Petition').should('exist');
-    cy.get('.remove-pdf-button').click();
-    cy.get('.confirm-replace-petition-modal').should('exist');
+  describe('Parties tab', () => {
+    it('should be free of a11y issues', () => {
+      loginAsPetitionsClerk();
 
-    cy.injectAxe();
-    cy.checkA11y(
-      undefined,
-      {
-        includedImpacts: impactLevel,
-        rules: {
-          'nested-interactive': { enabled: false }, // TODO LINK
+      cy.visit('/case-detail/104-19/petition-qc?tab=partyInfo');
+      cy.contains('Petition').should('exist');
+
+      cy.injectAxe();
+
+      cy.checkA11y(
+        undefined,
+        {
+          includedImpacts: impactLevel,
+          rules: {
+            'nested-interactive': { enabled: false }, // TODO LINK
+          },
         },
-      },
-      terminalLog,
-    );
+        terminalLog,
+      );
+    });
   });
 
-  it('should be free of a11y issues on the parties tab', () => {
-    loginAsPetitionsClerk();
-    cy.visit('/case-detail/104-19/petition-qc?tab=partyInfo');
-    cy.contains('Petition').should('exist');
+  describe('Case info tab', () => {
+    it('should be free of a11y issues', () => {
+      loginAsPetitionsClerk();
 
-    cy.injectAxe();
-    cy.checkA11y(
-      undefined,
-      {
-        includedImpacts: impactLevel,
-        rules: {
-          'nested-interactive': { enabled: false }, // TODO LINK
+      cy.visit('/case-detail/104-19/petition-qc?tab=caseInfo');
+      cy.contains('Petition').should('exist');
+
+      cy.injectAxe();
+
+      cy.checkA11y(
+        undefined,
+        {
+          includedImpacts: impactLevel,
+          rules: {
+            'nested-interactive': { enabled: false }, // TODO LINK
+          },
         },
-      },
-      terminalLog,
-    );
+        terminalLog,
+      );
+    });
   });
 
-  // TODO aria list element
-  it('should be free of a11y issues on the case info tab', () => {
-    loginAsPetitionsClerk();
-    cy.visit('/case-detail/104-19/petition-qc?tab=caseInfo');
-    cy.contains('Petition').should('exist');
+  describe('IRS notice tab', () => {
+    it('should be free of a11y issues', () => {
+      loginAsPetitionsClerk();
 
-    cy.injectAxe();
-    cy.checkA11y(
-      undefined,
-      {
-        includedImpacts: impactLevel,
-        rules: {
-          'color-contrast': { enabled: false }, // Ignore contrast as it's good enough for now
-          'nested-interactive': { enabled: false }, // TODO LINK
+      cy.visit('/case-detail/102-19/petition-qc?tab=irsNotice');
+      cy.contains('Petition').should('exist');
+      cy.get('#has-irs-verified-notice-yes').click();
+      cy.get('#date-of-notice-picker').should('exist');
+
+      cy.injectAxe();
+
+      cy.checkA11y(
+        undefined,
+        {
+          includedImpacts: impactLevel,
+          rules: {
+            'nested-interactive': { enabled: false }, // TODO LINK
+          },
         },
-      },
-      terminalLog,
-    );
-  });
-
-  it('should be free of a11y issues on the irs notice tab', () => {
-    loginAsPetitionsClerk();
-    cy.visit('/case-detail/102-19/petition-qc?tab=irsNotice');
-    cy.contains('Petition').should('exist');
-    cy.get('#has-irs-verified-notice-yes').click();
-    cy.get('#date-of-notice-picker').should('exist');
-
-    cy.injectAxe();
-    cy.checkA11y(
-      undefined,
-      {
-        includedImpacts: impactLevel,
-        rules: {
-          'nested-interactive': { enabled: false }, // TODO LINK
-        },
-      },
-      terminalLog,
-    );
+        terminalLog,
+      );
+    });
   });
 });
