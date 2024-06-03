111bbc32a43f141b56fc08811883b2f267d6f687
Kaitlyn Swann <kswann@flexion.us>
2024-06-03 11:07:23 -0600

    10382: wip public accessibility tests

---
+++ cypress/local-only/tests/accessibility/public/todaysOrders/no-user.cy.ts
@@ -0,0 +1,26 @@
+import { impactLevel } from '../../../../../helpers/accessibility-impact';
+import { terminalLog } from '../../../../../helpers/cypressTasks/logs';
+
+describe('Todays Orders - Accessibility', () => {
+  beforeEach(() => {
+    Cypress.session.clearCurrentSessionData();
+  });
+
+  it('should be free of a11y issues', () => {
+    cy.visit('/todays-orders');
+    cy.get('.todays-orders').should('exist');
+
+    cy.injectAxe();
+
+    cy.checkA11y(
+      undefined,
+      {
+        includedImpacts: impactLevel,
+        rules: {
+          'color-contrast': { enabled: false }, // Ignore contrast as it's good enough for now
+        },
+      },
+      terminalLog,
+    );
+  });
+});
