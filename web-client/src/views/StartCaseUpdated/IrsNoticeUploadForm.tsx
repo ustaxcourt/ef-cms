7b809e76b3b4aef5244d7562a01d1ceb2795ba37
John Cruz <jcruz@flexion.us>
2024-06-04 08:49:58 -0600

    5512: add data testid for helper functions;

--- web-client/src/views/StartCaseUpdated/IrsNoticeUploadForm.tsx
+++ web-client/src/views/StartCaseUpdated/IrsNoticeUploadForm.tsx
@@ -173,7 +173,7 @@ export const IrsNoticeUploadForm = connect(
               <DateSelector
                 defaultValue={noticeIssuedDate}
                 errorText={validationError.noticeIssuedDate}
-                id="notice-issued-date"
+                id={`notice-issued-date-${index}`}
                 label="Date IRS issued the notice"
                 maxDate={todayDate}
                 onBlur={() => {
@@ -216,6 +216,7 @@ export const IrsNoticeUploadForm = connect(
                 <input
                   autoCapitalize="none"
                   className="usa-input"
+                  data-testid={`irs-notice-tax-year-${index}`}
                   id="noticeIssuesTaxYear"
                   name="taxYear"
                   type="text"
@@ -250,7 +251,7 @@ export const IrsNoticeUploadForm = connect(
                 <DateSelector
                   defaultValue={noticeIssuedDate}
                   errorText={validationError.noticeIssuedDate}
-                  id="notice-issued-date"
+                  id={`notice-issued-date-${index}`}
                   label="Date IRS issued the notice"
                   maxDate={todayDate}
                   onBlur={() => {
