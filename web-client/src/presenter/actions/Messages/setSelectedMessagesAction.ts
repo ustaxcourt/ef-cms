8bc42f2b8d15f5bc31473833c8f95b9777a3af0f
Zachary Rogers <zrogers>
2024-06-04 15:16:33 -0700

    10372: Use a map to keep track of what is selected.

--- web-client/src/presenter/actions/Messages/setSelectedMessagesAction.ts
+++ web-client/src/presenter/actions/Messages/setSelectedMessagesAction.ts
@@ -4,29 +4,17 @@ export const setSelectedMessagesAction = ({
   get,
   props,
   store,
-}: ActionProps<{
-  messageId?: string;
-  selectAll?: boolean;
-}>) => {
-  if ('selectAll' in props) {
-    const allMessageIds: string[] = get(state.messages).map(
-      message => message.messageId,
-    );
-    const selectedMessageIds = props.selectAll ? allMessageIds : [];
-    store.set(state.messagesPage.selectedMessages, selectedMessageIds);
-    return;
+}: ActionProps<{ messages: { messageId: string; selected: boolean }[] }>) => {
+  let selectedMap = get(state.messagesPage.selectedMessages);
+  if (props.messages.length === 0) {
+    selectedMap = new Map<string, boolean>();
   }
-
-  if (props.messageId) {
-    const selectedMessages = get(state.messagesPage.selectedMessages);
-    const messageIndex = selectedMessages.findIndex(
-      message => message === props.messageId,
-    );
-    if (messageIndex !== -1) {
-      selectedMessages.splice(messageIndex, 1);
+  props.messages.forEach(message => {
+    if (message.selected) {
+      selectedMap.set(message.messageId, message.selected);
     } else {
-      selectedMessages.push(props.messageId);
+      selectedMap.delete(message.messageId);
     }
-    store.set(state.messagesPage.selectedMessages, selectedMessages);
-  }
+  });
+  store.set(state.messagesPage.selectedMessages, selectedMap);
 };
