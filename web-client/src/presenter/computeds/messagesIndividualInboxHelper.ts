02cc71d699ad81e4da1d6935185ab2cccf9b5878
Jason Fu <jasonfu6699@hotmail.com>
2024-06-04 09:43:59 -0700

    10372 - enable / disable complete button in the right states

--- web-client/src/presenter/computeds/messagesIndividualInboxHelper.ts
+++ web-client/src/presenter/computeds/messagesIndividualInboxHelper.ts
@@ -3,13 +3,22 @@ import { state } from '@web-client/presenter/app.cerebral';
 
 export const messagesIndividualInboxHelper = (
   get: Get,
-): { allMessagesSelected: boolean; someMessagesSelected: boolean } => {
+): {
+  allMessagesSelected: boolean;
+  someMessagesSelected: boolean;
+  isCompletionButtonEnabled: boolean;
+} => {
   const messagesInboxCount = get(state.messagesInboxCount);
   const messagesSelectedCount = get(state.messagesPage.selectedMessages).length;
 
+  const allMessagesSelected = messagesInboxCount === messagesSelectedCount;
+  const someMessagesSelected =
+    messagesSelectedCount > 0 && messagesSelectedCount < messagesInboxCount;
+  const isCompletionButtonEnabled = allMessagesSelected || someMessagesSelected;
+
   return {
-    allMessagesSelected: messagesInboxCount === messagesSelectedCount,
-    someMessagesSelected:
-      messagesSelectedCount > 0 && messagesSelectedCount < messagesInboxCount,
+    allMessagesSelected,
+    isCompletionButtonEnabled,
+    someMessagesSelected,
   };
 };
