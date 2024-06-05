82ca1435e48561ca5b9dc89ee85bdac61480a982
Kaitlyn Swann <kswann@flexion.us>
2024-06-05 10:56:37 -0600

    Devex 10382: Split accessibility and integration tests so that integration tests that change data do not cause accessibility tests to fail on CI

--- split-tests-cypress.ts
+++ split-tests-cypress.ts
@@ -1,10 +1,25 @@
 import fs from 'fs';
 import shuffleSeed from 'shuffle-seed';
 
+// # Usage
+// #   npx ts-node split-tests-cypress.ts integration
+// #   npx ts-node split-tests-cypress.ts accessibility
+
+// # Arguments
+// #   - $1 - the folder of tests to include when looking for tests to split across action runners
+
+const args = process.argv.slice(2);
+const testFolderToInclude = args[0];
+
 const specDir = './cypress/local-only/tests';
 const files = fs
   .readdirSync(specDir, { recursive: true })
-  .filter(f => (f as string).endsWith('cy.ts') && !f.includes('public/'));
+  .filter(
+    f =>
+      (f as string).endsWith('cy.ts') &&
+      !f.includes('public/') &&
+      f.includes(`${testFolderToInclude}/`),
+  );
 
 const shuffled = shuffleSeed.shuffle(files, process.env.GITHUB_SHA);
 
