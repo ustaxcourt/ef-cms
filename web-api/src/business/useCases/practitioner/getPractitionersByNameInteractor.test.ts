28bf52c1332cda3475f9f46cff980dd30e49dc12
John Cruz <jcruz@flexion.us>
2024-06-05 14:50:18 -0600

    10258: Add unit tests around "getPractitionersByNameInteractor";

--- web-api/src/business/useCases/practitioner/getPractitionersByNameInteractor.test.ts
+++ web-api/src/business/useCases/practitioner/getPractitionersByNameInteractor.test.ts
@@ -86,4 +86,76 @@ describe('getPractitionersByNameInteractor', () => {
       });
     });
   });
+
+  describe('Public User', () => {
+    beforeEach(() => {
+      applicationContext.getCurrentUser.mockReturnValue(undefined);
+
+      applicationContext
+        .getPersistenceGateway()
+        .getPractitionersByName.mockReturnValue({
+          lastKey: [1.23],
+          results: [
+            {
+              barNumber: 'PT1234',
+              contact: { flavor: 'bbq', state: 'WI' },
+              name: 'Test Practitioner1',
+              originalBarState: 'originalBarState_WI',
+              role: ROLES.irsPractitioner,
+              userId: '8190d648-e643-4964-988e-141e4e0db861',
+            },
+            {
+              barNumber: 'PT5432',
+              contact: { favoriteColor: 'chartreuse', state: 'WI' },
+              name: 'Test Practitioner2',
+              originalBarState: 'originalBarState_WI',
+              role: ROLES.privatePractitioner,
+              userId: '12d5bb3a-e867-4066-bda5-2f178a76191f',
+            },
+          ],
+          total: 2,
+        });
+    });
+
+    it('should not throw an Unauthorized error if its a Public user', async () => {
+      const results = await getPractitionersByNameInteractor(
+        applicationContext,
+        {
+          name: 'Test Practitioner',
+          searchAfter: undefined as unknown as string,
+        },
+      );
+
+      expect(results).toBeDefined();
+    });
+
+    it('should return the correct practitioner data when a Public user requests it', async () => {
+      const results = await getPractitionersByNameInteractor(
+        applicationContext,
+        {
+          name: 'Test Practitioner',
+          searchAfter: undefined as unknown as string,
+        },
+      );
+
+      expect(results).toMatchObject({
+        searchResults: {
+          lastKey: [1.23],
+          practitioners: [
+            {
+              barNumber: 'PT1234',
+              contact: { state: 'originalBarState_WI' },
+              name: 'Test Practitioner1',
+            },
+            {
+              barNumber: 'PT5432',
+              contact: { state: 'originalBarState_WI' },
+              name: 'Test Practitioner2',
+            },
+          ],
+          total: 2,
+        },
+      });
+    });
+  });
 });
