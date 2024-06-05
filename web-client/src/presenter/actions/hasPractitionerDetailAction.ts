10fb38bfb7fad2d7f0392b1033c1904fcf1e35ba
John Cruz <jcruz@flexion.us>
2024-06-05 15:11:20 -0600

    10258: Expanded and added unit tests around "hasPractitionerDetailAction";

--- web-client/src/presenter/actions/hasPractitionerDetailAction.ts
+++ web-client/src/presenter/actions/hasPractitionerDetailAction.ts
@@ -1,12 +1,5 @@
 import { isEmpty } from 'lodash';
 
-/**
- * returns the next path based on whether props.practitionerDetail is set
- * @param {object} providers the providers object
- * @param {object} providers.props the cerebral props object containing the props.practitionerDetail
- * @param {object} providers.path the cerebral store used for setting the state.practitionerDetail
- * @returns {Function} the next path in the sequence
- */
 export const hasPractitionerDetailAction = ({ path, props }: ActionProps) => {
   const { isPublicUser, practitionerDetail } = props;
 
