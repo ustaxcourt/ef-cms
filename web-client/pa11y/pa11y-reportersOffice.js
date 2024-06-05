daef6317132a4bff60ada454159631ff68241e83
Kaitlyn Swann <kswann@flexion.us>
2024-06-05 11:40:02 -0600

    Devex 10382: Remove pa11y tests and pa11y-ci package, update documentation that references pa11y

--- web-client/pa11y/pa11y-reportersOffice.js
+++
@@ -1,8 +0,0 @@
-const { loginAs } = require('./helpers');
-
-module.exports = [
-  {
-    actions: [...loginAs({ username: 'reportersoffice@example.com' })], // DONE
-    url: 'http://localhost:1234/',
-  },
-];
