df3145f0db164ffb32a8ab12468c1e53299c1842
Kaitlyn Swann <kswann@flexion.us>
2024-06-06 15:21:25 -0600

    10382: update todo with link to bug

--- cypress/local-only/tests/accessibility/editUploadCourtIssuedDocument/docket-clerk.cy.ts
+++ cypress/local-only/tests/accessibility/editUploadCourtIssuedDocument/docket-clerk.cy.ts
@@ -4,13 +4,6 @@ describe('Edit Upload Court Issued Document - Docket Clerk Accessibility', () =>
   });
 
   it('should be free of a11y issues', () => {
-    // TODO
-    // {
-    //   actions: [
-    //     ...loginAs({ username: 'docketclerk@example.com' }),
-    //     'navigate to http://localhost:1234/case-detail/104-20/edit-upload-court-issued/b1aa4aa2-c214-424c-8870-d0049c5744d8',
-    //   ],
-    //   url: 'http://localhost:1234/',
-    // },
+    // TODO https://github.com/flexion/ef-cms/issues/10399
   });
 });
