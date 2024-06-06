3b5c59581548855c2c2f3d7f7fca1ef5db6155ff
Nechama Krigsman <nkrigsman@flexion.us>
2024-06-06 11:01:14 -0400

    5512: fix uploaded petition preview url; remove link icon;

--- web-client/src/presenter/actions/clearPetitionFormAction.ts
+++ web-client/src/presenter/actions/clearPetitionFormAction.ts
@@ -4,6 +4,7 @@ export const clearPetitionFormAction = ({ store }: ActionProps) => {
   store.set(state.form.petitionFacts, ['']);
   store.unset(state.form.petitionFile);
   store.unset(state.form.petitionFileSize);
+  store.unset(state.form.petitionFileUrl);
   store.set(state.form.petitionReasons, ['']);
   store.unset(state.form.petitionRedactionAcknowledgement);
   store.unset(state.form.petitionType);
