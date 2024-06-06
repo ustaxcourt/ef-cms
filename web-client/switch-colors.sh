0e1186245ad10bf4dc67d2a57de11fe9c05dc413
Nate Elliott <nelliott@flexion.us>
2024-06-06 12:30:19 -0500

    10368: Use already-existing script for setting lambda env and disable traffic on newly-passive color

--- web-client/switch-colors.sh
+++ web-client/switch-colors.sh
@@ -29,10 +29,10 @@ fi
 UUID=$(aws lambda list-event-source-mappings --function-name "arn:aws:lambda:us-east-1:${AWS_ACCOUNT_ID}:function:streams_${ENV}_${DEPLOYING_COLOR}" --region us-east-1 | jq -r ".EventSourceMappings[0].UUID")
 aws lambda update-event-source-mapping --uuid "${UUID}" --region us-east-1 --enabled
 
-# For the following lambdas, set PREVENT_ALL_TRAFFIC=true in env
-npx ts-node --transpile-only ./scripts/deployment/enable-new-traffic.ts "api_async_${ENV}_${DEPLOYING_COLOR}"
-npx ts-node --transpile-only ./scripts/deployment/enable-new-traffic.ts "api_${ENV}_${DEPLOYING_COLOR}"
-npx ts-node --transpile-only ./scripts/deployment/enable-new-traffic.ts "websockets_connect_${ENV}_${DEPLOYING_COLOR}"
+# Enable traffic on the deploying color
+./web-api/terraform/bin/edit-lambda-environment.sh -l "api_async_${ENV}_${DEPLOYING_COLOR}" -k DISABLE_ALL_TRAFFIC -v false
+./web-api/terraform/bin/edit-lambda-environment.sh -l "api_${ENV}_${DEPLOYING_COLOR}" -k DISABLE_ALL_TRAFFIC -v false
+./web-api/terraform/bin/edit-lambda-environment.sh -l "websockets_connect_${ENV}_${DEPLOYING_COLOR}" -k DISABLE_ALL_TRAFFIC -v false
 
 npx ts-node --transpile-only ./web-client/switch-public-ui-colors.ts
 npx ts-node --transpile-only ./web-client/switch-ui-colors.ts
@@ -40,6 +40,10 @@ npx ts-node --transpile-only ./web-client/switch-api-colors.ts
 npx ts-node --transpile-only ./web-client/switch-public-api-colors.ts
 npx ts-node --transpile-only ./web-api/switch-bounce-handler-colors.ts
 npx ts-node --transpile-only ./web-api/switch-health-check-domain.ts
+
+./web-api/terraform/bin/edit-lambda-environment.sh -l "api_async_${ENV}_${CURRENT_COLOR}" -k DISABLE_ALL_TRAFFIC -v true
+./web-api/terraform/bin/edit-lambda-environment.sh -l "api_${ENV}_${CURRENT_COLOR}" -k DISABLE_ALL_TRAFFIC -v true
+./web-api/terraform/bin/edit-lambda-environment.sh -l "websockets_connect_${ENV}_${CURRENT_COLOR}" -k DISABLE_ALL_TRAFFIC -v true
 
 aws dynamodb put-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --item '{"pk":{"S":"current-color"},"sk":{"S":"current-color"},"current":{"S":"'"${DEPLOYING_COLOR}"'"}}'
 
