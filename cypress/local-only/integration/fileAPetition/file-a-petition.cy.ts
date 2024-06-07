3609650b12878b5a4b181da5e16e0b43610b3370
Nechama Krigsman <nkrigsman@flexion.us>
2024-06-07 13:50:08 -0400

    5512: remove tests around cancel modal from before filing a petition screen since the modal was removed;

--- cypress/local-only/integration/fileAPetition/file-a-petition.cy.ts
+++ cypress/local-only/integration/fileAPetition/file-a-petition.cy.ts
@@ -56,16 +56,6 @@ describe('File a Petition', () => {
     it('should navigate to dashboard when cancel is clicked', () => {
       cy.get('[data-testid="cancel-before-starting-case"]').click();
 
-      cy.get('button.modal-button-confirm').click();
-
-      cy.url().should('not.include', 'before-filing-a-petition');
-    });
-
-    it('should navigate to dashboard when close is clicked', () => {
-      cy.get('[data-testid="cancel-before-starting-case"]').click();
-
-      cy.get('button.text-no-underline').click();
-
       cy.url().should('not.include', 'before-filing-a-petition');
     });
   });
