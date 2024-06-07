b7b741aec49c66f7d9a042ceeb37031dd3e35c1d
Nate Elliott <nelliott@flexion.us>
2024-06-07 13:13:02 -0500

    10368: Make new check in clearModalAction null safe

--- web-client/src/presenter/actions/clearModalAction.ts
+++ web-client/src/presenter/actions/clearModalAction.ts
@@ -3,7 +3,8 @@ import { state } from '@web-client/presenter/app.cerebral';
 
 export const clearModalAction = ({ props, store }: ActionProps) => {
   if (
-    props.error.originalError.response.headers.get(X_FORCE_REFRESH) !== 'true'
+    props.error?.originalError?.response?.headers?.get(X_FORCE_REFRESH) !==
+    'true'
   ) {
     store.unset(state.modal.showModal);
   }
