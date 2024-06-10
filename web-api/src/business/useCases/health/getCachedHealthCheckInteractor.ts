9cb79b42fc82075ecae1e0632c7f1b165f893483
Zachary Rogers <zrogers>
2024-06-10 14:25:35 -0700

    devex: Use ServerApplicationContext

--- web-api/src/business/useCases/health/getCachedHealthCheckInteractor.ts
+++ web-api/src/business/useCases/health/getCachedHealthCheckInteractor.ts
@@ -1,7 +1,8 @@
+import { ServerApplicationContext } from '@web-api/applicationContext';
 import { StoredApplicationHealth } from '@web-api/persistence/dynamo/deployTable/setStoredApplicationHealth';
 
 export const getCachedHealthCheckInteractor = async (
-  applicationContext: IApplicationContext,
+  applicationContext: ServerApplicationContext,
 ): Promise<StoredApplicationHealth> => {
   const appHealth = await applicationContext
     .getPersistenceGateway()
