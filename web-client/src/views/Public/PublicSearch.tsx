7ef1f3834bc40bd7d971360a0a1becbed65bf09f
John Cruz <jcruz@flexion.us>
2024-06-11 09:58:04 -0600

    10258: Fix typos; make bullets wrap;

--- web-client/src/views/Public/PublicSearch.tsx
+++ web-client/src/views/Public/PublicSearch.tsx
@@ -91,18 +91,18 @@ export const PublicSearch = connect(
     function PractitionerRender() {
       return (
         <>
-          <>
+          <div>
             <p className="margin-top-0">
-              The information provided in the search results below is maintained
-              by the Admissions Clerk of the U.S. Tax Court. Practitioners may:
+              The information in the search results below is maintained by the
+              Admissions Clerk of the U.S.T.C. Practitioners may:
             </p>
-            <ul>
+            <ul className="grid-col-6">
               <li>
                 Update their contact information by logging into DAWSON and
                 updating their practitioner accounts.
               </li>{' '}
               <li>
-                Change thier practitioner type, practice type, or admission
+                Change their practitioner type, practice type, or admission
                 status by contacting the Admissions Clerk at{' '}
                 <a href="mailto:admissions@ustaxcourt.gov">
                   admissions@ustaxcourt.gov
@@ -110,7 +110,7 @@ export const PublicSearch = connect(
                 .
               </li>
             </ul>
-          </>
+          </div>
           <PractitionerSearchForm isPublicUser={true}></PractitionerSearchForm>
 
           <PractitionerSearchResults
