d662988e9b4805b5dfbc49de150fe3deb8ce3d3c
Rachel Schneiderman <rschneiderman379@gmail.com>
2024-06-14 11:49:31 -0700

    10345: bring back lambda_role permissions

--- web-api/terraform/modules/ci-cd/ci-cd-permissions.tf
+++ web-api/terraform/modules/ci-cd/ci-cd-permissions.tf
@@ -357,7 +357,8 @@ resource "aws_iam_policy" "ci_cd_iam_policy" {
         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/switch_colors_cron_lambda_role_*",
         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/wait_for_workflow_lambda_role_*",
         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/strip_basepath_role_*",
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/header_security_role_*"
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/header_security_role_*",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/lambda_role_*"
       ]
     }
   ]
