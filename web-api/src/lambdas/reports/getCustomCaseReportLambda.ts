287548520d8a1080a41adc9566889812ce61b76f
Zachary Rogers <zrogers>
2024-06-17 12:08:26 -0700

    devex: pass the through all of the handlers rather than using global applicationContext

--- web-api/src/lambdas/reports/getCustomCaseReportLambda.ts
+++ web-api/src/lambdas/reports/getCustomCaseReportLambda.ts
@@ -1,11 +1,13 @@
+import { UnknownAuthUser } from '@web-api/middleware/apiGatewayHelper';
 import { genericHandler } from '../../genericHandler';
 
-export const getCustomCaseReportLambda = event =>
+export const getCustomCaseReportLambda = (event, user: UnknownAuthUser) =>
   genericHandler(event, async ({ applicationContext }) => {
     return await applicationContext
       .getUseCases()
       .getCustomCaseReportInteractor(
         applicationContext,
         event.queryStringParameters,
+        user,
       );
   });
