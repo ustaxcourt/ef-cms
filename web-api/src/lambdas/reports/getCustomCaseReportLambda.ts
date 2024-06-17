16158387a5e1622e61563e354096d2245a8e47d9
Zachary Rogers <zrogers>
2024-06-17 13:06:05 -0700

    devex: Create schema to help validate an authenticated user.

--- web-api/src/lambdas/reports/getCustomCaseReportLambda.ts
+++ web-api/src/lambdas/reports/getCustomCaseReportLambda.ts
@@ -1,13 +1,16 @@
-import { UnknownAuthUser } from '@web-api/middleware/apiGatewayHelper';
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
+import { getCustomCaseReportInteractor } from '@web-api/business/useCases/caseInventoryReport/getCustomCaseReportInteractor';
 
 export const getCustomCaseReportLambda = (event, user: UnknownAuthUser) =>
-  genericHandler(event, async ({ applicationContext }) => {
-    return await applicationContext
-      .getUseCases()
-      .getCustomCaseReportInteractor(
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      return await getCustomCaseReportInteractor(
         applicationContext,
         event.queryStringParameters,
         user,
       );
-  });
+    },
+    user,
+  );
