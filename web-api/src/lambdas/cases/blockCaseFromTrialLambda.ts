ebacf65a83e6823f7e26a53a17adfd9cf6418053
Zachary Rogers <zrogers>
2024-06-18 22:11:14 -0700

    devex: Update interactors to accept authorizedUser as an argument

--- web-api/src/lambdas/cases/blockCaseFromTrialLambda.ts
+++ web-api/src/lambdas/cases/blockCaseFromTrialLambda.ts
@@ -1,3 +1,5 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
+import { blockCaseFromTrialInteractor } from '@shared/business/useCases/blockCaseFromTrialInteractor';
 import { genericHandler } from '../../genericHandler';
 
 /**
@@ -6,16 +8,25 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const blockCaseFromTrialLambda = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    const lambdaArguments = {
-      ...event.pathParameters,
-      ...JSON.parse(event.body),
-    };
+export const blockCaseFromTrialLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      const lambdaArguments = {
+        ...event.pathParameters,
+        ...JSON.parse(event.body),
+      };
 
-    return await applicationContext
-      .getUseCases()
-      .blockCaseFromTrialInteractor(applicationContext, {
-        ...lambdaArguments,
-      });
-  });
+      return await blockCaseFromTrialInteractor(
+        applicationContext,
+        {
+          ...lambdaArguments,
+        },
+        authorizedUser,
+      );
+    },
+    authorizedUser,
+  );
