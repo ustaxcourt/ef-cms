f319fceb23ca22370dac9af8c636889fbadd75b8
Zachary Rogers <zrogers>
2024-06-19 14:25:16 -0700

    10336: WIP, refactor how we zipDocuments

--- web-api/src/persistence/s3/zipDocuments.ts
+++ web-api/src/persistence/s3/zipDocuments.ts
@@ -1,70 +1,68 @@
+import { PassThrough } from 'stream';
 import { ServerApplicationContext } from '@web-api/applicationContext';
-import { zipS3Files } from './zipS3Files';
+import { Upload } from '@aws-sdk/lib-storage';
 import archiver from 'archiver';
-import s3FilesLib from 's3-files';
-import stream from 'stream';
 
-/**
- * zipDocuments
- *
- * @param {object} providers the providers object
- * @param {object} providers.applicationContext the application context
- * @param {Array} providers.fileNames the names of the files to zip
- * @param {Array} providers.s3Ids the s3 ids of the files to zip
- * @param {string} providers.zipName the name of the generated zip file
- * @returns {Promise} the created zip
- */
-export const zipDocuments = async ({
-  applicationContext,
-  extraFileNames,
-  extraFiles,
-  fileNames,
-  onEntry,
-  onError,
-  onProgress,
-  onUploadStart,
-  s3Ids,
-  zipName,
-}: {
-  applicationContext: ServerApplicationContext;
-  extraFileNames: string[];
-  extraFiles: any[];
-  fileNames: string[];
-  onEntry: (entryData: any) => void;
-  onError: (error: any) => void;
-  onProgress: (data: any) => void;
-  onUploadStart: () => void;
-  s3Ids: string[];
-  zipName: string;
-}) => {
-  const s3Client = applicationContext.getStorageClient();
+export async function zipDocuments(
+  applicationContext: ServerApplicationContext,
+  {
+    documents,
+    outputZipName,
+    useTempBucket,
+  }: {
+    documents: { documentId: string; filePathInZip: string }[];
+    outputZipName: string;
+    useTempBucket: boolean;
+  },
+): Promise<void> {
+  // download all s3 files
+  const files = await Promise.all(
+    documents.map(async document => {
+      const pdf = await applicationContext.getPersistenceGateway().getDocument({
+        applicationContext,
+        key: document.documentId,
+        useTempBucket,
+      });
+      return { filePathInZip: document.filePathInZip, pdf };
+    }),
+  );
 
-  onUploadStart?.();
+  const passThrough = new PassThrough();
 
-  const passThrough = new stream.PassThrough();
-
-  await applicationContext.getPersistenceGateway().uploadDocument({
-    applicationContext,
-    pdfData: passThrough,
-    pdfName: zipName,
-    useTempBucket: true,
+  const upload = new Upload({
+    client: applicationContext.getStorageClient(),
+    params: {
+      Body: passThrough,
+      Bucket: applicationContext.environment.tempDocumentsBucketName,
+      Key: outputZipName,
+    },
   });
 
-  passThrough.on('error', () => {
-    throw new Error('');
+  upload.on('httpUploadProgress', progress => {
+    console.log('s3 upload progress: ', progress);
   });
 
-  zipS3Files({
-    additionalFileNames: extraFileNames,
-    additionalFiles: extraFiles,
-    archiver,
-    bucket: applicationContext.environment.documentsBucketName,
-    onEntry,
-    onError,
-    onProgress,
-    s3Client,
-    s3FilesLib,
-    s3Keys: s3Ids,
-    s3KeysFileNames: fileNames,
-  }).pipe(passThrough);
-};
+  // combine downloaded files into a zip document
+  const zip = archiver('zip');
+  zip.on('close', () => {
+    console.log('archiving is close');
+  });
+  zip.on('finish', () => {
+    console.log('archiving is finish');
+  });
+  zip.on('error', err => {
+    console.log('archiving ERROR!!!!');
+    throw err;
+  });
+
+  files.map(file =>
+    zip.append(Buffer.from(file.pdf), { name: file.filePathInZip }),
+  );
+  zip.pipe(passThrough);
+  await zip.finalize();
+
+  await upload.done();
+}
+
+// Pdfs are not being named correctly
+// Progress indicator functionality
