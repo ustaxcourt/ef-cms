4835a9f9f16aaf6758858be24e9fe156bb97f669
Zachary Rogers <zrogers>
2024-06-18 22:20:15 -0700

    devex: Update interactors to accept authorizedUser as an argument

--- web-api/src/lambdas/users/getAllUsersByRoleLambda.ts
+++ web-api/src/lambdas/users/getAllUsersByRoleLambda.ts
@@ -1,16 +1,21 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
+import { getAllUsersByRoleInteractor } from '@shared/business/useCases/getAllUsersByRoleInteractor';
 
-export const getAllUsersByRoleLambda = event =>
+export const getAllUsersByRoleLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
   genericHandler(
     event,
     async ({ applicationContext }) => {
-      return await applicationContext
-        .getUseCases()
-        .getAllUsersByRoleInteractor(
-          applicationContext,
-          event.queryStringParameters,
-        );
+      return await getAllUsersByRoleInteractor(
+        applicationContext,
+        event.queryStringParameters,
+        authorizedUser,
+      );
     },
+    authorizedUser,
     {
       bypassMaintenanceCheck: true,
     },
