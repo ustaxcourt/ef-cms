c7024f9c2cc8bc02a3ec8bacd8450c67cf785588
Nechama Krigsman <nkrigsman@flexion.us>
2024-06-20 00:58:59 -0400

    5512: update test id; put back assertion;

--- cypress/helpers/fileAPetition/petitioner-creates-electronic-case-updated.ts
+++ cypress/helpers/fileAPetition/petitioner-creates-electronic-case-updated.ts
@@ -34,6 +34,7 @@ export function petitionerCreatesElectronicCaseUpdated(
   uploadFile('stin-file');
   cy.get('[data-testid="step-5-next-button"]').click();
 
+  cy.get('[data-testid="atp-preview-button"]').should('exist');
   cy.get('[data-testid="stin-preview-button"]').should('exist');
 
   cy.get('[data-testid="step-6-next-button"]').click();
