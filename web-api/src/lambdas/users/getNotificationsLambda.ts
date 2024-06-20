b5e901aedf7b5c18e633dfb7d8717aed1f1bc6f2
Zachary Rogers <zrogers>
2024-06-19 22:14:39 -0700

    devex: Update interactors to accept authorizedUser as an argument

--- web-api/src/lambdas/users/getNotificationsLambda.ts
+++ web-api/src/lambdas/users/getNotificationsLambda.ts
@@ -1,16 +1,27 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
+import { getNotificationsInteractor } from '@shared/business/useCases/getNotificationsInteractor';
 
 /**
- * creates a new document and attaches it to a case.  It also creates a work item on the docket section.
+ * creates a new document and attaches it to a case. It also creates a work item on the docket section.
  *
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const getNotificationsLambda = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    return await applicationContext
-      .getUseCases()
-      .getNotificationsInteractor(applicationContext, {
-        ...event.queryStringParameters,
-      });
-  });
+export const getNotificationsLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      return await getNotificationsInteractor(
+        applicationContext,
+        {
+          ...event.queryStringParameters,
+        },
+        authorizedUser,
+      );
+    },
+    authorizedUser,
+  );
