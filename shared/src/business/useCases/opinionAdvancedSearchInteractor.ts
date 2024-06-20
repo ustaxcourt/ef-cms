4e298fd3cda71ff7665b599741276e6cf58b6fb4
Zachary Rogers <zrogers>
2024-06-19 22:31:24 -0700

    devex: Update interactors to accept authorizedUser as an argument

--- shared/src/business/useCases/opinionAdvancedSearchInteractor.ts
+++ shared/src/business/useCases/opinionAdvancedSearchInteractor.ts
@@ -7,6 +7,7 @@ import {
   isAuthorized,
 } from '../../authorization/authorizationClientService';
 import { UnauthorizedError } from '@web-api/errors/errors';
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { omit } from 'lodash';
 
 export const opinionAdvancedSearchInteractor = async (
@@ -30,9 +31,8 @@ export const opinionAdvancedSearchInteractor = async (
     opinionTypes: string[];
     startDate: string;
   },
+  authorizedUser: UnknownAuthUser,
 ) => {
-  const authorizedUser = applicationContext.getCurrentUser();
-
   if (!isAuthorized(authorizedUser, ROLE_PERMISSIONS.ADVANCED_SEARCH)) {
     throw new UnauthorizedError('Unauthorized');
   }
