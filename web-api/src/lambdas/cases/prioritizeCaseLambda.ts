c405ea9edd7ad0f101b2041fef7b8a4180a979f5
Zachary Rogers <zrogers>
2024-06-19 22:40:15 -0700

    devex: Update interactors to accept authorizedUser as an argument

--- web-api/src/lambdas/cases/prioritizeCaseLambda.ts
+++ web-api/src/lambdas/cases/prioritizeCaseLambda.ts
@@ -1,4 +1,6 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
+import { prioritizeCaseInteractor } from '@shared/business/useCases/prioritizeCaseInteractor';
 
 /**
  * used for prioritizing a case
@@ -6,12 +8,18 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const prioritizeCaseLambda = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    return await applicationContext
-      .getUseCases()
-      .prioritizeCaseInteractor(applicationContext, {
-        ...event.pathParameters,
-        ...JSON.parse(event.body),
-      });
-  });
+export const prioritizeCaseLambda = (event, authorizedUser: UnknownAuthUser) =>
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      return await prioritizeCaseInteractor(
+        applicationContext,
+        {
+          ...event.pathParameters,
+          ...JSON.parse(event.body),
+        },
+        authorizedUser,
+      );
+    },
+    authorizedUser,
+  );
