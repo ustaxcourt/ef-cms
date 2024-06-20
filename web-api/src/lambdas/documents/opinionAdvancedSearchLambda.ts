026a1ccf5dd7be67c911da60bd0236a07fdc41af
Zachary Rogers <zrogers>
2024-06-19 22:31:24 -0700

    devex: Update interactors to accept authorizedUser as an argument

--- web-api/src/lambdas/documents/opinionAdvancedSearchLambda.ts
+++ web-api/src/lambdas/documents/opinionAdvancedSearchLambda.ts
@@ -1,4 +1,6 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
+import { opinionAdvancedSearchInteractor } from '@shared/business/useCases/opinionAdvancedSearchInteractor';
 
 /**
  * used for fetching opinions matching the provided search string
@@ -6,15 +8,24 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const opinionAdvancedSearchLambda = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    const opinionTypes =
-      event.queryStringParameters.opinionTypes?.split(',') || [];
+export const opinionAdvancedSearchLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      const opinionTypes =
+        event.queryStringParameters.opinionTypes?.split(',') || [];
 
-    return await applicationContext
-      .getUseCases()
-      .opinionAdvancedSearchInteractor(applicationContext, {
-        ...event.queryStringParameters,
-        opinionTypes,
-      });
-  });
+      return await opinionAdvancedSearchInteractor(
+        applicationContext,
+        {
+          ...event.queryStringParameters,
+          opinionTypes,
+        },
+        authorizedUser,
+      );
+    },
+    authorizedUser,
+  );
