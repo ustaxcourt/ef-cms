a0afcd86d650a33cc0c2b51c9e4eef6ac9a9a857
Zachary Rogers <zrogers>
2024-06-20 12:47:16 -0700

    10336: WIP trying fflate to upload files to s3

--- web-api/src/persistence/s3/zipDocuments.ts
+++ web-api/src/persistence/s3/zipDocuments.ts
@@ -1,7 +1,18 @@
-import { PassThrough } from 'stream';
+import {
+  AsyncGzip,
+  AsyncUnzipInflate,
+  AsyncZipDeflate,
+  Unzip,
+  Zip,
+  gzip,
+  strFromU8,
+  unzip,
+  zip,
+  zlib,
+} from 'fflate';
+import { PassThrough, Writable } from 'stream';
 import { ServerApplicationContext } from '@web-api/applicationContext';
 import { Upload } from '@aws-sdk/lib-storage';
-import archiver from 'archiver';
 
 export async function zipDocuments(
   applicationContext: ServerApplicationContext,
@@ -15,7 +26,7 @@ export async function zipDocuments(
     useTempBucket: boolean;
   },
 ): Promise<void> {
-  const passThrough = new PassThrough();
+  const passThrough = new PassThrough({ highWaterMark: 1024 * 1024 * 1 });
 
   const upload = new Upload({
     client: applicationContext.getStorageClient(),
@@ -29,29 +40,45 @@ export async function zipDocuments(
     console.log('s3 upload progress: ', progress);
   });
 
-  const zip = archiver('zip');
-  zip.on('error', err => {
-    console.log(
-      `Error while creating zip file: ${outputZipName}. Zipping files:${documents.map(doc => ` ${doc.documentId}`)}`,
-    );
-    throw err;
+  const writable = new Writable({
+    write(chunk, encoding, callback) {
+      console.log('Received chunk:');
+      callback();
+    },
   });
-  zip.pipe(passThrough);
+  passThrough.pipe(writable);
 
-  await Promise.all(
-    documents.map(async document => {
-      const pdf = await applicationContext.getPersistenceGateway().getDocument({
-        applicationContext,
-        key: document.documentId,
-        useTempBucket,
+  // Start the upload process
+  const uploadPromise = upload.done();
+
+  let count = 1;
+  for (let document of documents) {
+    const response = await applicationContext.getStorageClient().getObject({
+      Bucket: useTempBucket
+        ? applicationContext.environment.tempDocumentsBucketName
+        : applicationContext.environment.documentsBucketName,
+      Key: document.documentId,
+    });
+    const bigArray = await response.Body?.transformToByteArray()!;
+
+    await new Promise((resolve, reject) => {
+      zlib(bigArray, { consume: true, level: 6 }, (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          passThrough.write(data, () => {
+            resolve(true);
+          });
+        }
       });
+    });
 
-      zip.append(Buffer.from(pdf), { name: document.filePathInZip });
-    }),
-  );
+    count = count + 1;
+    console.log('CompletedDocuments: ', count);
+  }
+  passThrough.end();
 
-  await zip.finalize();
-  await upload.done();
+  await uploadPromise;
 }
 
 // Pdfs are not being named correctly
