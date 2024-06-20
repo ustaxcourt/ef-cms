fc28c26b6a8f7d461e3b1f60729e06bbfd9179e5
John Cruz <jcruz@flexion.us>
2024-06-20 10:12:02 -0600

    5512: Move cypress test files to correct directory;

--- cypress/local-only/tests/integration/fileAPetitionUpdated/file-a-petition-step-2.cy.ts
+++ cypress/local-only/tests/integration/fileAPetitionUpdated/file-a-petition-step-2.cy.ts
@@ -1,5 +1,5 @@
 import { fillPetitionerInformation } from './petition-helper';
-import { loginAsPetitioner } from '../../../helpers/authentication/login-as-helpers';
+import { loginAsPetitioner } from '../../../../helpers/authentication/login-as-helpers';
 
 describe('File a petition', () => {
   beforeEach(() => {
