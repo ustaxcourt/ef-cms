c1744690b33460a6015f296420fb6449294c84d8
Zachary Rogers <zrogers>
2024-06-25 15:07:27 -0700

    Devex: Begin pattern for fixing interactor unit tests where applicationContext.getCurrentUser has been replaced by user argument to interactor

---
+++ shared/src/test/mockAuthUsers.ts
@@ -0,0 +1,16 @@
+import { AuthUser } from '@shared/business/entities/authUser/AuthUser';
+import { ROLES } from '@shared/business/entities/EntityConstants';
+
+export const mockPetitionerUser: AuthUser = {
+  email: 'mockPetitioner@example.com',
+  name: 'Tax Payer',
+  role: ROLES.petitioner,
+  userId: 'e4988d2d-deb0-4b65-a97f-5abfadb0970a',
+};
+
+export const mockJudgeUser: AuthUser = {
+  email: 'mockJudge@example.com',
+  name: 'Judge Judy',
+  role: ROLES.judge,
+  userId: '3a13531b-0fc5-4a4c-8188-c72f50b1889d',
+};
