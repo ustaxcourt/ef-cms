fc339904eeea30b2d01b79313d0bb66057ce1378
Kaitlyn Swann <kswann@flexion.us>
2024-06-26 15:50:14 -0600

    10102: Begin Adding in Text for PDF Generation

--- web-client/src/presenter/actions/prepareStatusReportOrderResponseAction.ts
+++ web-client/src/presenter/actions/prepareStatusReportOrderResponseAction.ts
@@ -1,14 +1,39 @@
-import { state } from 'cerebral';
+import { state } from '@web-client/presenter/app.cerebral';
 
 export const prepareStatusReportOrderResponseAction = ({
   store,
 }: ActionProps) => {
+  // TODO, setup actual text here..
+  const hasOrderType = !!state.form.orderType;
+  const hasStrickenFromTrialSessions = !!state.form.strikenFromTrialSessions;
+  const hasJurisdiction = !!state.form.jurisdiction;
+  const hasAdditionalOrderText = !!state.form.additionalOrderText;
+
+  const filedLine =
+    'On [FILED DATE OF SR], a status report was filed in this case (Index no. [INDEX_NUMBER_OF_STATUS_REPORT]). For cause, it is';
+
+  const orderTypeLine =
+    hasOrderType && state.form.orderType === 'statusReport'
+      ? 'ORDERED that the parties shall file a further status report by [DATE SELECTED].'
+      : `ORDERED that the parties shall file a status report or proposed stipulated decision by
+  [DATE SELECTED].`;
+
+  const strickenLine =
+    hasStrickenFromTrialSessions &&
+    'ORDERED that this case is stricken from the trial session. It is further';
+
+  const jurisdictionLine =
+    hasJurisdiction && state.form.jurisdiction === 'retained'
+      ? 'ORDERED that this case is restored to the general docket'
+      : 'ORDERED that jurisdiction is retained by the undersigned.';
+
+  const additionalTextLine =
+    hasAdditionalOrderText && `ORDERED that ${state.form.additionalOrderText}.`;
+
+  const richText = `${filedLine}\n\n${hasOrderType && orderTypeLine}\n\n${hasStrickenFromTrialSessions && strickenLine}\n\n${hasJurisdiction && jurisdictionLine}\n\n${hasAdditionalOrderText && additionalTextLine}\n\n`;
+
   // TODO, maybe add documentType=Order ?
   store.set(state.form.documentTitle, 'Order');
   store.set(state.form.eventCode, 'O');
-  // TODO, setup actual text here..
-  store.set(
-    state.form.richText,
-    'On [FILED DATE OF SR], a status report was filed in this case (Index no. [INDEX_NUMBER_OF_STATUS_REPORT]). For cause, it is',
-  );
+  store.set(state.form.richText, richText);
 };
