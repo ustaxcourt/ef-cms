4a410fb546c7a8008ad5e66a6595848c1538a61c
Zachary Rogers <zrogers>
2024-06-27 12:58:33 -0700

    10379: Update test name

--- cypress/local-only/tests/integration/myAccount/bug-switching-accounts-state-cache.cy.ts
+++ cypress/local-only/tests/integration/myAccount/bug-switching-accounts-state-cache.cy.ts
@@ -1,11 +1,11 @@
 import { getCypressEnv } from '../../../../helpers/env/cypressEnvironment';
 import { navigateTo as loginAs } from '../../../support/pages/maintenance';
 
-describe('BUG: State caching when switching accounts', () => {
+describe('BUG: State not recomputing when switching accounts', () => {
   const PETITIONER_ACCOUNT = 'petitioner1';
   const PRIVATE_PRACTITIONER_ACCOUNT = 'privatePractitioner1';
 
-  it('should test', () => {
+  it('should show the my contact information card when logging in as a petitioner, then logging in as a practitioner', () => {
     loginAs(PETITIONER_ACCOUNT);
     cy.get('[data-testid="account-menu-button"]').click();
     cy.get('[data-testid="my-account-link"]').click();
