6d1c1e06335436db5073ffba5ebd23579a326568
Nechama Krigsman <nkrigsman@flexion.us>
2024-07-01 13:18:46 -0400

    5512: only remove redaction acknowledgement state if there are no IRS notice files;

--- web-client/src/presenter/actions/clearIrsNoticeRedactionAcknowledgementAction.ts
+++ web-client/src/presenter/actions/clearIrsNoticeRedactionAcknowledgementAction.ts
@@ -1,7 +1,12 @@
 import { state } from '@web-client/presenter/app.cerebral';
 
 export const clearIrsNoticeRedactionAcknowledgementAction = ({
+  get,
   store,
 }: ActionProps) => {
-  store.unset(state.form.irsNoticesRedactionAcknowledgement);
+  const notices = get(state.irsNoticeUploadFormInfo);
+  const noticesWithFiles = notices?.filter(notice => !!notice.file).length;
+  if (!noticesWithFiles) {
+    store.unset(state.form.irsNoticesRedactionAcknowledgement);
+  }
 };
