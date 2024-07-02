936c308fb5c6e7905bb563a4ef690cd1e7b9a460
Zachary Rogers <zrogers>
2024-07-02 13:15:24 -0700

    devex: refactor to authUser

--- web-api/src/lambdas/trialSessions/getGeneratePrintableTrialSessionCopyReportLambda.ts
+++ web-api/src/lambdas/trialSessions/getGeneratePrintableTrialSessionCopyReportLambda.ts
@@ -1,3 +1,5 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
+import { generatePrintableTrialSessionCopyReportInteractor } from '@web-api/business/useCases/trialSessions/generatePrintableTrialSessionCopyReportInteractor';
 import { genericHandler } from '../../genericHandler';
 
 /**
@@ -6,18 +8,27 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const getGeneratePrintableTrialSessionCopyReportLambda = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    const body = JSON.parse(event.body);
-    return await applicationContext
-      .getUseCases()
-      .generatePrintableTrialSessionCopyReportInteractor(applicationContext, {
-        filters: body.filters,
-        formattedCases: body.formattedCases,
-        formattedTrialSession: body.formattedTrialSession,
-        sessionNotes: body.sessionNotes,
-        showCaseNotes: body.showCaseNotes,
-        sort: body.sort,
-        userHeading: body.userHeading,
-      });
-  });
+export const getGeneratePrintableTrialSessionCopyReportLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      const body = JSON.parse(event.body);
+      return await generatePrintableTrialSessionCopyReportInteractor(
+        applicationContext,
+        {
+          filters: body.filters,
+          formattedCases: body.formattedCases,
+          formattedTrialSession: body.formattedTrialSession,
+          sessionNotes: body.sessionNotes,
+          showCaseNotes: body.showCaseNotes,
+          sort: body.sort,
+          userHeading: body.userHeading,
+        },
+        authorizedUser,
+      );
+    },
+    authorizedUser,
+  );
