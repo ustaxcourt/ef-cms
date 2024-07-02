6fec88664160721f06ac90135141aaf916d171c0
Zachary Rogers <zrogers>
2024-07-02 11:25:55 -0700

    devex: refactor to authUser

--- web-api/src/lambdas/messages/getOutboxMessagesForUserLambda.ts
+++ web-api/src/lambdas/messages/getOutboxMessagesForUserLambda.ts
@@ -1,4 +1,6 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
+import { getOutboxMessagesForUserInteractor } from '@web-api/business/useCases/messages/getOutboxMessagesForUserInteractor';
 
 /**
  * gets the outbox messages for the user
@@ -6,11 +8,20 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const getOutboxMessagesForUserLambda = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    return await applicationContext
-      .getUseCases()
-      .getOutboxMessagesForUserInteractor(applicationContext, {
-        userId: event.pathParameters.userId,
-      });
-  });
+export const getOutboxMessagesForUserLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      return await getOutboxMessagesForUserInteractor(
+        applicationContext,
+        {
+          userId: event.pathParameters.userId,
+        },
+        authorizedUser,
+      );
+    },
+    authorizedUser,
+  );
