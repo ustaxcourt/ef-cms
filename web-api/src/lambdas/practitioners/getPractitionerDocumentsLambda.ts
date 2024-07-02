9fe17a59a62718b090f9b4ad3a80cb2ce8b19faf
Zachary Rogers <zrogers>
2024-07-02 11:20:38 -0700

    devex: refactor to authUser

--- web-api/src/lambdas/practitioners/getPractitionerDocumentsLambda.ts
+++ web-api/src/lambdas/practitioners/getPractitionerDocumentsLambda.ts
@@ -1,4 +1,6 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
+import { getPractitionerDocumentsInteractor } from '@web-api/business/useCases/practitioner/getPractitionerDocumentsInteractor';
 
 /**
  * creates a practitioner document for a practitioner
@@ -6,11 +8,20 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const getPractitionerDocumentsLambda = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    return await applicationContext
-      .getUseCases()
-      .getPractitionerDocumentsInteractor(applicationContext, {
-        barNumber: event.pathParameters.barNumber,
-      });
-  });
+export const getPractitionerDocumentsLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      return await getPractitionerDocumentsInteractor(
+        applicationContext,
+        {
+          barNumber: event.pathParameters.barNumber,
+        },
+        authorizedUser,
+      );
+    },
+    authorizedUser,
+  );
