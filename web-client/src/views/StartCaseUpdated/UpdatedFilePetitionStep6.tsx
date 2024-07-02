5e2452d5f04450e17d6e04e9ff623ecda69d2a84
Nechama Krigsman <nkrigsman@flexion.us>
2024-07-02 15:48:06 -0400

    5512: span 5 equal columns case information;

--- web-client/src/views/StartCaseUpdated/UpdatedFilePetitionStep6.tsx
+++ web-client/src/views/StartCaseUpdated/UpdatedFilePetitionStep6.tsx
@@ -386,14 +386,14 @@ function CaseInformation({ petitionFormatted }) {
     <div className="border-top-1px padding-top-2 padding-bottom-2 height-full margin-bottom-0">
       <div className="content-wrapper">
         <CardHeader step={4} title="Case Procedure and Trial Location" />
-        <div className="grid-row grid-gap">
-          <div className="tablet:grid-col-2 line-height-2">
+        <div className="petition-review-case-information-section">
+          <div className="line-height-2">
             <div className="margin-bottom-1 semi-bold">Case procedure</div>
             <div className="margin-bottom-1">
               {petitionFormatted.procedureType}
             </div>
           </div>
-          <div className="tablet:grid-col-2 line-height-2">
+          <div className="line-height-2">
             <div className="margin-bottom-1 semi-bold">
               Requested trial location
             </div>
