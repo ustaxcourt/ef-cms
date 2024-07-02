6abdda8521dd8cbfcbcbc0705fa0514393f96080
Zachary Rogers <zrogers>
2024-07-02 11:00:26 -0700

    devex: refactor to authUser

--- web-api/src/lambdas/reports/coldCaseReportLambda.ts
+++ web-api/src/lambdas/reports/coldCaseReportLambda.ts
@@ -1,8 +1,12 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
+import { coldCaseReportInteractor } from '@web-api/business/useCases/reports/coldCaseReportInteractor';
 import { genericHandler } from '../../genericHandler';
 
-export const coldCaseReportLambda = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    return await applicationContext
-      .getUseCases()
-      .coldCaseReportInteractor(applicationContext);
-  });
+export const coldCaseReportLambda = (event, authorizedUser: UnknownAuthUser) =>
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      return await coldCaseReportInteractor(applicationContext, authorizedUser);
+    },
+    authorizedUser,
+  );
