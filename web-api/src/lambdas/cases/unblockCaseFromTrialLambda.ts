40e10058931f019ae545e50cf0a81037a06d0d7d
Zachary Rogers <zrogers>
2024-07-04 14:20:17 -0700

    10417: Remove getCurrentUser from shared

--- web-api/src/lambdas/cases/unblockCaseFromTrialLambda.ts
+++ web-api/src/lambdas/cases/unblockCaseFromTrialLambda.ts
@@ -1,4 +1,6 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
+import { unblockCaseFromTrialInteractor } from '@shared/business/useCases/unblockCaseFromTrialInteractor';
 
 /**
  * used for unblocking a case
@@ -6,11 +8,20 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const unblockCaseFromTrialLambda = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    return await applicationContext
-      .getUseCases()
-      .unblockCaseFromTrialInteractor(applicationContext, {
-        ...event.pathParameters,
-      });
-  });
+export const unblockCaseFromTrialLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      return await unblockCaseFromTrialInteractor(
+        applicationContext,
+        {
+          ...event.pathParameters,
+        },
+        authorizedUser,
+      );
+    },
+    authorizedUser,
+  );
