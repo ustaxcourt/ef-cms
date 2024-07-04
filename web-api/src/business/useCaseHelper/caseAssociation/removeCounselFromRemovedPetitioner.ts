f1c65807a113326f27631d6ae77ef79adc8fc81e
Zachary Rogers <zrogers>
2024-07-04 13:54:22 -0700

    10417: Remove getCurrentUser from shared

--- web-api/src/business/useCaseHelper/caseAssociation/removeCounselFromRemovedPetitioner.ts
+++ web-api/src/business/useCaseHelper/caseAssociation/removeCounselFromRemovedPetitioner.ts
@@ -1,8 +1,11 @@
+import { Case } from '@shared/business/entities/cases/Case';
 import {
   ROLE_PERMISSIONS,
   isAuthorized,
 } from '../../../../../shared/src/authorization/authorizationClientService';
+import { ServerApplicationContext } from '@web-api/applicationContext';
 import { UnauthorizedError } from '@web-api/errors/errors';
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 /**
  * removeCounselFromRemovedPetitioner
  *
@@ -14,11 +17,15 @@ import { UnauthorizedError } from '@web-api/errors/errors';
  */
 export const removeCounselFromRemovedPetitioner = async ({
   applicationContext,
+  authorizedUser,
   caseEntity,
   petitionerContactId,
+}: {
+  applicationContext: ServerApplicationContext;
+  caseEntity: Case;
+  petitionerContactId: string;
+  authorizedUser: UnknownAuthUser;
 }) => {
-  const authorizedUser = applicationContext.getCurrentUser();
-
   if (
     !isAuthorized(authorizedUser, ROLE_PERMISSIONS.QC_PETITION) &&
     !isAuthorized(authorizedUser, ROLE_PERMISSIONS.REMOVE_PETITIONER)
--- web-api/src/lambdas/cases/removePetitionerAndUpdateCaptionLambda.ts
+++ web-api/src/lambdas/cases/removePetitionerAndUpdateCaptionLambda.ts
@@ -1,4 +1,6 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
+import { removePetitionerAndUpdateCaptionInteractor } from '@shared/business/useCases/removePetitionerAndUpdateCaptionInteractor';
 
 /**
  * lambda which is used for removing a petitioner from a case
@@ -6,12 +8,21 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const removePetitionerAndUpdateCaptionLambda = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    return await applicationContext
-      .getUseCases()
-      .removePetitionerAndUpdateCaptionInteractor(applicationContext, {
-        ...event.pathParameters,
-        ...JSON.parse(event.body),
-      });
-  });
+export const removePetitionerAndUpdateCaptionLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      return await removePetitionerAndUpdateCaptionInteractor(
+        applicationContext,
+        {
+          ...event.pathParameters,
+          ...JSON.parse(event.body),
+        },
+        authorizedUser,
+      );
+    },
+    authorizedUser,
+  );
