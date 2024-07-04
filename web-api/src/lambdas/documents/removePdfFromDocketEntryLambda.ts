42e378ed643668eb67fb76b346d898e4bbf7def5
Zachary Rogers <zrogers>
2024-07-04 12:54:40 -0700

    10417: Remove getCurrentUser from shared

--- web-api/src/lambdas/documents/removePdfFromDocketEntryLambda.ts
+++ web-api/src/lambdas/documents/removePdfFromDocketEntryLambda.ts
@@ -1,4 +1,6 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
+import { removePdfFromDocketEntryInteractor } from '@shared/business/useCases/removePdfFromDocketEntryInteractor';
 
 /**
  * used for removing a pdf from a docket entry
@@ -6,12 +8,18 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const removePdfFromDocketEntryLambda = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    return await applicationContext
-      .getUseCases()
-      .removePdfFromDocketEntryInteractor(
+export const removePdfFromDocketEntryLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      return await removePdfFromDocketEntryInteractor(
         applicationContext,
         event.pathParameters,
+        authorizedUser,
       );
-  });
+    },
+    authorizedUser,
+  );
