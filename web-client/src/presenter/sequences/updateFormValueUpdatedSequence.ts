a6fcdb584052d6a2cc498cb602d4f0b92d5e22ff
Nechama Krigsman <nkrigsman@flexion.us>
2024-07-08 10:38:53 -0400

    5512: add types for sequences;

--- web-client/src/presenter/sequences/updateFormValueUpdatedSequence.ts
+++ web-client/src/presenter/sequences/updateFormValueUpdatedSequence.ts
@@ -4,4 +4,4 @@ import { setFormValueAction } from '../actions/setFormValueAction';
 export const updateFormValueUpdatedSequence = [
   clearValidationMessageAction,
   setFormValueAction,
-];
+] as unknown as (params: { key: string; value: any; index?: number }) => void;
