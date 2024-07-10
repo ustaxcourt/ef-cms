61b990be402aa350e7ae644d49e1b801d32f9dd0
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-07-10 07:59:24 -0500

    10372: update standard message completion

--- web-client/src/presenter/actions/Messages/setCompleteMessageAlertAction.ts
+++ web-client/src/presenter/actions/Messages/setCompleteMessageAlertAction.ts
@@ -1,7 +1,16 @@
 import { createISODateString } from '@shared/business/utilities/DateHandler';
+import { formatDateIfToday } from '@web-client/presenter/computeds/formattedWorkQueue';
 import { state } from '@web-client/presenter/app.cerebral';
 
-export const setCompleteMessageAlertAction = ({ get, store }: ActionProps) => {
-  store.set(state.messagesPage.messagesCompletedBy, get(state.user));
-  store.set(state.messagesPage.messagesCompletedAt, createISODateString());
+export const setCompleteMessageAlertAction = ({
+  applicationContext,
+  get,
+  store,
+}: ActionProps) => {
+  store.set(state.messagesPage.messagesCompletedBy, get(state.user).name);
+  store.set(
+    state.messagesPage.messagesCompletedAt,
+    formatDateIfToday(createISODateString(), applicationContext),
+  );
+  store.set(state.messagesPage.completionSuccess, true);
 };
--- web-client/src/presenter/actions/removeCompletedMessagesFromDisplayAction.ts
+++ web-client/src/presenter/actions/removeCompletedMessagesFromDisplayAction.ts
@@ -2,20 +2,15 @@ import { state } from '@web-client/presenter/app.cerebral';
 
 export const removeCompletedMessagesFromDisplayAction = ({
   get,
+  props,
   store,
 }: ActionProps) => {
-  const selectedMap = get(state.messagesPage.selectedMessages);
-  const completedMessages = Array.from(selectedMap.keys());
-  const formattedMessages = get(state.formattedMessages);
-  console.log('formatted', formattedMessages);
-  // either remove only the message from display, or remove everything about the message from the whole object
-  // I opt for #1
-  const messagesInInbox = formattedMessages.messages;
+  const { completedMessageIds } = props;
+  const messagesInInbox = get(state.formattedMessages.messages);
+
   const remainingInboxItems = messagesInInbox.filter(
-    message => !completedMessages.includes(message.messageId),
+    message => !completedMessageIds.includes(message.messageId),
   );
 
-  console.log('remaining', remainingInboxItems);
-  store.set(state.formattedMessages.messages, remainingInboxItems);
-  store.set(state.formattedMessages.hasMessages, !!remainingInboxItems.length);
+  store.set(state.messages, remainingInboxItems);
 };
