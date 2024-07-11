c46c0c069031337a0530f2b42095a38e5b2e4252
Nate Elliott <nelliott@flexion.us>
2024-07-11 15:48:57 -0500

    10417 remove getCurrentUser from getOutboxMessagesForUserAction

--- web-client/src/presenter/actions/getOutboxMessagesForUserAction.ts
+++ web-client/src/presenter/actions/getOutboxMessagesForUserAction.ts
@@ -1,3 +1,5 @@
+import { state } from '@web-client/presenter/app.cerebral';
+
 /**
  * fetches the outbox messages for the user
  * @param {object} applicationContext object that contains all the context specific methods
@@ -5,11 +7,13 @@
  */
 export const getOutboxMessagesForUserAction = async ({
   applicationContext,
+  get,
 }: ActionProps) => {
+  const user = get(state.user);
   const messages = await applicationContext
     .getUseCases()
     .getOutboxMessagesForUserInteractor(applicationContext, {
-      userId: applicationContext.getCurrentUser().userId,
+      userId: user.userId,
     });
 
   return { messages };
