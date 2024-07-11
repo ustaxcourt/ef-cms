a7f051e372a44f36cff71f6ee89b45e71b3f9c6d
Zachary Rogers <zrogers>
2024-07-11 08:40:41 -0700

    10417: migrate away from applicationContext.getCurrentUser

--- web-api/src/lambdas/cases/privatePractitionerCaseAssociationLambda.ts
+++ web-api/src/lambdas/cases/privatePractitionerCaseAssociationLambda.ts
@@ -1,4 +1,6 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
+import { submitCaseAssociationRequestInteractor } from '@web-api/business/useCases/caseAssociationRequest/submitCaseAssociationRequestInteractor';
 
 /**
  * lambda which is used for associating a user to a case
@@ -6,12 +8,21 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const privatePractitionerCaseAssociationLambda = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    return await applicationContext
-      .getUseCases()
-      .submitCaseAssociationRequestInteractor(applicationContext, {
-        ...event.pathParameters,
-        ...JSON.parse(event.body),
-      });
-  });
+export const privatePractitionerCaseAssociationLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      return await submitCaseAssociationRequestInteractor(
+        applicationContext,
+        {
+          ...event.pathParameters,
+          ...JSON.parse(event.body),
+        },
+        authorizedUser,
+      );
+    },
+    authorizedUser,
+  );
