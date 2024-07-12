3dd756326956121355d88af05c8fcb9e9b6b324b
Nate Elliott <nelliott@flexion.us>
2024-07-12 13:17:13 -0500

    10417 remove getCurrentUser from dashboardExternalHelper

--- web-client/src/presenter/computeds/dashboardExternalHelper.test.ts
+++ web-client/src/presenter/computeds/dashboardExternalHelper.test.ts
@@ -1,7 +1,11 @@
-import { ROLES } from '../../../../shared/src/business/entities/EntityConstants';
 import { applicationContext } from '../../applicationContext';
 import { dashboardExternalHelper as dashboardExternalHelperComputed } from './dashboardExternalHelper';
-import { docketClerk1User } from '@shared/test/mockUsers';
+import {
+  docketClerk1User,
+  irsPractitionerUser,
+  petitionerUser,
+  privatePractitionerUser,
+} from '@shared/test/mockUsers';
 import { runCompute } from '@web-client/presenter/test.cerebral';
 import { withAppContextDecorator } from '../../withAppContext';
 
@@ -12,40 +16,33 @@ describe('dashboardExternalHelper', () => {
   );
 
   it('should show "what to expect" but not case list when there are no open or closed cases', () => {
-    applicationContext.getCurrentUser = () => ({
-      role: ROLES.petitioner,
-    });
     const result = runCompute(dashboardExternalHelper, {
       state: {
         closedCases: [],
         openCases: [],
+        user: petitionerUser,
       },
     });
     expect(result.showWhatToExpect).toEqual(true);
   });
 
   it('should show case list but not "what to expect" when there is an open or closed case case', () => {
-    applicationContext.getCurrentUser = () => ({
-      role: ROLES.petitioner,
-    });
     const result = runCompute(dashboardExternalHelper, {
       state: {
         closedCases: [{ something: true }],
         openCases: [{ something: true }],
+        user: petitionerUser,
       },
     });
     expect(result.showWhatToExpect).toEqual(false);
   });
 
   it('should keep the showFileACase flag as false when the user role is petitioner', () => {
-    applicationContext.getCurrentUser = () => ({
-      role: ROLES.petitioner,
-    });
-
     const result = runCompute(dashboardExternalHelper, {
       state: {
         closedCases: [{ something: true }],
         openCases: [{ something: true }],
+        user: petitionerUser,
       },
     });
 
@@ -53,14 +50,11 @@ describe('dashboardExternalHelper', () => {
   });
 
   it('should set the showFileACase flag as true when the user role is a private practitioner', () => {
-    applicationContext.getCurrentUser = () => ({
-      role: ROLES.privatePractitioner,
-    });
-
     const result = runCompute(dashboardExternalHelper, {
       state: {
         closedCases: [{ something: true }],
         openCases: [{ something: true }],
+        user: privatePractitionerUser,
       },
     });
 
@@ -68,14 +62,11 @@ describe('dashboardExternalHelper', () => {
   });
 
   it('should keep the showStartButton flag as false when the user role is irs practitioner', () => {
-    applicationContext.getCurrentUser = () => ({
-      role: ROLES.irsPractitioner,
-    });
-
     const result = runCompute(dashboardExternalHelper, {
       state: {
         closedCases: [{ something: true }],
         openCases: [{ something: true }],
+        user: irsPractitionerUser,
       },
     });
 
@@ -83,17 +74,14 @@ describe('dashboardExternalHelper', () => {
   });
 
   it('should set the showStartButton flag as true when the user role is a private practitioner or petitioner', () => {
-    const userRoles = ['petitioner', 'privatePractitioner'];
+    const userRoles = [petitionerUser, privatePractitionerUser];
 
-    userRoles.forEach(userRole => {
-      applicationContext.getCurrentUser = () => ({
-        role: userRole,
-      });
-
+    userRoles.forEach(user => {
       const result = runCompute(dashboardExternalHelper, {
         state: {
           closedCases: [{ something: true }],
           openCases: [{ something: true }],
+          user,
         },
       });
 
@@ -102,17 +90,14 @@ describe('dashboardExternalHelper', () => {
   });
 
   it('should set the showFilingFee to true when the user is a private practitioner or petitioner', () => {
-    const userRoles = ['petitioner', 'privatePractitioner'];
+    const userRoles = [petitionerUser, privatePractitionerUser];
 
-    userRoles.forEach(userRole => {
-      applicationContext.getCurrentUser = () => ({
-        role: userRole,
-      });
-
+    userRoles.forEach(user => {
       const result = runCompute(dashboardExternalHelper, {
         state: {
           closedCases: [{ something: true }],
           openCases: [{ something: true }],
+          user,
         },
       });
 
@@ -121,12 +106,11 @@ describe('dashboardExternalHelper', () => {
   });
 
   it('should set the showFilingFee to false when the user is NOT a private practitioner or petitioner', () => {
-    applicationContext.getCurrentUser = () => docketClerk1User;
-
     const result = runCompute(dashboardExternalHelper, {
       state: {
         closedCases: [{ something: true }],
         openCases: [{ something: true }],
+        user: docketClerk1User,
       },
     });
 
