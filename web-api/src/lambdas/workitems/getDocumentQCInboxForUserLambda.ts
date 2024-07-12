e7b96c675a0774e8f16d4771ddb8efcdbe61b729
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-07-12 08:08:02 -0500

    10417: remove getCurrentUser from getDocumentQCInboxForUserInteractor

--- web-api/src/lambdas/workitems/getDocumentQCInboxForUserLambda.ts
+++ web-api/src/lambdas/workitems/getDocumentQCInboxForUserLambda.ts
@@ -1,4 +1,6 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
+import { getDocumentQCInboxForUserInteractor } from '@web-api/business/useCases/workItems/getDocumentQCInboxForUserInteractor';
 
 /**
  * returns the users inbox
@@ -6,13 +8,22 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const getDocumentQCInboxForUserLambda = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    const { userId } = event.pathParameters || {};
+export const getDocumentQCInboxForUserLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      const { userId } = event.pathParameters || {};
 
-    return await applicationContext
-      .getUseCases()
-      .getDocumentQCInboxForUserInteractor(applicationContext, {
-        userId,
-      });
-  });
+      return await getDocumentQCInboxForUserInteractor(
+        applicationContext,
+        {
+          userId,
+        },
+        authorizedUser,
+      );
+    },
+    authorizedUser,
+  );
