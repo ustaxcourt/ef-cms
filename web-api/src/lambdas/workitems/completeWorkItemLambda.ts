d66190414d7983e4172ee6016b54fa962a5781a9
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-07-12 08:26:40 -0500

    10417: remove getCurrentUser from completeWorkItemInteractor

--- web-api/src/lambdas/workitems/completeWorkItemLambda.ts
+++ web-api/src/lambdas/workitems/completeWorkItemLambda.ts
@@ -1,3 +1,5 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
+import { completeWorkItemInteractor } from '@web-api/business/useCases/workItems/completeWorkItemInteractor';
 import { genericHandler } from '../../genericHandler';
 
 /**
@@ -6,12 +8,21 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const completeWorkItemLambda = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    return await applicationContext
-      .getUseCases()
-      .completeWorkItemInteractor(applicationContext, {
-        completedMessage: JSON.parse(event.body).completedMessage,
-        workItemId: event.pathParameters.workItemId,
-      });
-  });
+export const completeWorkItemLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      return await completeWorkItemInteractor(
+        applicationContext,
+        {
+          completedMessage: JSON.parse(event.body).completedMessage,
+          workItemId: event.pathParameters.workItemId,
+        },
+        authorizedUser,
+      );
+    },
+    authorizedUser,
+  );
