acc34c2ee9ee5846072747877b4602c8c9de4cfb
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-07-12 08:39:12 -0500

    10417: remove getCurrentUser from assignWorkItemsInteractor

--- web-api/src/lambdas/workitems/assignWorkItemsLambda.ts
+++ web-api/src/lambdas/workitems/assignWorkItemsLambda.ts
@@ -1,3 +1,5 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
+import { assignWorkItemsInteractor } from '@web-api/business/useCases/workItems/assignWorkItemsInteractor';
 import { genericHandler } from '../../genericHandler';
 
 /**
@@ -6,11 +8,17 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const assignWorkItemsLambda = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    return await applicationContext
-      .getUseCases()
-      .assignWorkItemsInteractor(applicationContext, {
-        ...JSON.parse(event.body),
-      });
-  });
+export const assignWorkItemsLambda = (event, authorizedUser: UnknownAuthUser) =>
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      return await assignWorkItemsInteractor(
+        applicationContext,
+        {
+          ...JSON.parse(event.body),
+        },
+        authorizedUser,
+      );
+    },
+    authorizedUser,
+  );
