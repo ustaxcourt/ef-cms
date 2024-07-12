a189804e51fab42b44825b6ea1b0f2de581e9597
Zachary Rogers <zrogers>
2024-07-12 10:16:34 -0700

    10417: Update single interactor to use authUser

--- web-api/src/lambdas/reports/getCustomCaseReportLambda.ts
+++ web-api/src/lambdas/reports/getCustomCaseReportLambda.ts
@@ -1,11 +1,15 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
+import { getCustomCaseReportInteractor } from '@web-api/business/useCases/caseInventoryReport/getCustomCaseReportInteractor';
 
-export const getCustomCaseReportLambda = event =>
+export const getCustomCaseReportLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
   genericHandler(event, async ({ applicationContext }) => {
-    return await applicationContext
-      .getUseCases()
-      .getCustomCaseReportInteractor(
-        applicationContext,
-        event.queryStringParameters,
-      );
+    return await getCustomCaseReportInteractor(
+      applicationContext,
+      event.queryStringParameters,
+      authorizedUser,
+    );
   });
