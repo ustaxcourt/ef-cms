c17ecd946e6a34e8eec3c80db82882fe4c8066cc
Jim deVos <james.devos@ustaxcourt.gov>
2024-07-15 11:20:52 -0700

    10417: remove appcontext.getCurrentUser from createCaseDeadlineInteractor

--- web-api/src/lambdas/caseDeadline/createCaseDeadlineLambda.ts
+++ web-api/src/lambdas/caseDeadline/createCaseDeadlineLambda.ts
@@ -1,3 +1,4 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
 
 /**
@@ -6,11 +7,22 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const createCaseDeadlineLambda = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    return await applicationContext
-      .getUseCases()
-      .createCaseDeadlineInteractor(applicationContext, {
-        ...JSON.parse(event.body),
-      });
-  });
+export const createCaseDeadlineLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+): Promise<any | undefined> =>
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      return await applicationContext
+        .getUseCases()
+        .createCaseDeadlineInteractor(
+          applicationContext,
+          {
+            ...JSON.parse(event.body),
+          },
+          authorizedUser,
+        );
+    },
+    authorizedUser,
+  );
