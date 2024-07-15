b8ec39993a90ad963ef8845388acd5960e24d2c6
Nechama Krigsman <nkrigsman@flexion.us>
2024-07-15 16:03:29 -0400

    5512: For secondary contact info save email user inputted as paperPetitionEmail because we need the secondary contact to confirm access first; Updated entities to support new property for secondary contact info;

--- web-client/src/presenter/actions/validateUploadPetitionStep1Action.ts
+++ web-client/src/presenter/actions/validateUploadPetitionStep1Action.ts
@@ -25,7 +25,7 @@ export const validateUploadPetitionStep1Action = ({
     'postalCode',
     'placeOfLegalResidence',
     'phone',
-    'email',
+    'paperPetitionEmail',
   ];
 
   return path.error({
