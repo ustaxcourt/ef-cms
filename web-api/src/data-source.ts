abc60de9839fc4bf2c548e1a314f579af918514d
Kaitlyn Swann <kswann@flexion.us>
2024-07-16 08:21:58 -0600

    10391: move data-source

---
+++ web-api/src/data-source.ts
@@ -0,0 +1,20 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { Message } from './persistence/repository/Message';
+
+export const AppDataSource = new DataSource({
+  database: 'postgres',
+  entities: [Message],
+  host: 'localhost',
+  logging: false,
+  migrations: ['migrations/*.ts'],
+  password: 'example',
+  port: 5432,
+  subscribers: [],
+  synchronize: true,
+  type: 'postgres',
+  username: 'postgres',
+});
+
+const dataSource = AppDataSource.initialize().then(() => AppDataSource);
+export const getDataSource = () => dataSource;
