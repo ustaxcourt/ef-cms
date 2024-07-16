0c01d45b34a65808dcd01f381179bff0ac8544de
Jim deVos <james.devos@ustaxcourt.gov>
2024-07-16 07:40:11 -0700

    10417: remove appcontext.getCurrentUser from getCaseInventoryReportInteractor

--- web-api/src/lambdas/reports/getCaseInventoryReportLambda.ts
+++ web-api/src/lambdas/reports/getCaseInventoryReportLambda.ts
@@ -1,3 +1,4 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
 
 /**
@@ -6,11 +7,22 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const getCaseInventoryReportLambda = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    return await applicationContext
-      .getUseCases()
-      .getCaseInventoryReportInteractor(applicationContext, {
-        ...event.queryStringParameters,
-      });
-  });
+export const getCaseInventoryReportLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      return await applicationContext
+        .getUseCases()
+        .getCaseInventoryReportInteractor(
+          applicationContext,
+          {
+            ...event.queryStringParameters,
+          },
+          authorizedUser,
+        );
+    },
+    authorizedUser,
+  );
