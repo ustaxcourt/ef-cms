0c60b1089a2097bb4386d24bb39234f02b2341c7
Jim deVos <james.devos@ustaxcourt.gov>
2024-07-16 14:10:15 -0700

    10417: remove appcontext.getCurrentUser from getUserCaseNoteInteractor

--- web-api/src/lambdas/caseNote/getUserCaseNoteLambda.ts
+++ web-api/src/lambdas/caseNote/getUserCaseNoteLambda.ts
@@ -1,3 +1,4 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
 
 /**
@@ -6,11 +7,17 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const getUserCaseNoteLambda = event =>
-  genericHandler(event, async ({ applicationContext }) => {
-    return await applicationContext
-      .getUseCases()
-      .getUserCaseNoteInteractor(applicationContext, {
-        ...event.pathParameters,
-      });
-  });
+export const getUserCaseNoteLambda = (event, authorizedUser: UnknownAuthUser) =>
+  genericHandler(
+    event,
+    async ({ applicationContext }) => {
+      return await applicationContext.getUseCases().getUserCaseNoteInteractor(
+        applicationContext,
+        {
+          ...event.pathParameters,
+        },
+        authorizedUser,
+      );
+    },
+    authorizedUser,
+  );
