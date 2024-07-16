0e8a2735a84b93eecf5ca4995acbb24c8519b320
Nate Elliott <nelliott@flexion.us>
2024-07-16 10:52:20 -0500

    10417: update tests for v2 getCaseLambda

--- web-api/src/persistence/dynamo/helpers/aggregateCaseItems.ts
+++ web-api/src/persistence/dynamo/helpers/aggregateCaseItems.ts
@@ -60,6 +60,8 @@ export const aggregateCaseItems = (caseAndCaseItems): RawCase => {
   let irsPractitioners = [];
   let privatePractitioners = [];
 
+  // console.log(caseAndCaseItems);
+
   caseAndCaseItems.forEach(item => {
     if (isDocketEntryItem(item)) {
       // Docket Entries
