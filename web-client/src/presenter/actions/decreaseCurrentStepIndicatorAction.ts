d66ce42cd1b3023319d758a83b94fdb8b8623736
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-07-17 14:49:02 -0500

    5512: remove renamed action

--- web-client/src/presenter/actions/decreaseCurrentStepIndicatorAction.ts
+++
@@ -1,9 +0,0 @@
-import { state } from '@web-client/presenter/app.cerebral';
-
-export const decreaseCurrentStepIndicatorAction = ({
-  get,
-  store,
-}: ActionProps) => {
-  const currentStep = get(state.stepIndicatorInfo.currentStep);
-  store.set(state.stepIndicatorInfo.currentStep, currentStep - 1);
-};
