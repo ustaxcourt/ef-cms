ac46bba16000a585cfac7d18260c088c386445b8
Nate Elliott <nelliott@flexion.us>
2024-07-17 16:03:28 -0500

    10417 Remove getCurrentUser from trialSessionsSummaryHelper

--- web-client/src/presenter/computeds/trialSessionsSummaryHelper.test.ts
+++ web-client/src/presenter/computeds/trialSessionsSummaryHelper.test.ts
@@ -1,12 +1,8 @@
 import { ROLES } from '../../../../shared/src/business/entities/EntityConstants';
+import { mockChambersUser, mockJudgeUser } from '@shared/test/mockAuthUsers';
 import { runCompute } from '@web-client/presenter/test.cerebral';
 import { trialSessionsSummaryHelper as trialSessionsSummaryHelperComputed } from './trialSessionsSummaryHelper';
 import { withAppContextDecorator } from '../../withAppContext';
-
-let currentUser;
-
-const judgeId = '733777c2-cb31-44c6-afce-f7bc1a3f613b';
-const chambersId = 'a5ac202c-4e96-46f2-8968-2749ea845143';
 
 const trialSessionsSummaryHelper = withAppContextDecorator(
   trialSessionsSummaryHelperComputed,
@@ -14,41 +10,31 @@ const trialSessionsSummaryHelper = withAppContextDecorator(
     getConstants: () => ({
       USER_ROLES: ROLES,
     }),
-    getCurrentUser: () => currentUser,
   },
 );
 
 describe('trialSessionsSummaryHelper', () => {
-  beforeEach(() => {
-    currentUser = {
-      role: ROLES.judge,
-      userId: judgeId,
-    };
-  });
-
   it('should return the judeUserId as the logged in user when that user is a judge', () => {
     const result = runCompute(trialSessionsSummaryHelper, {
-      state: {},
+      state: {
+        user: mockJudgeUser,
+      },
     });
 
-    expect(result.judgeUserId).toEqual(judgeId);
+    expect(result.judgeUserId).toEqual(mockJudgeUser.userId);
   });
 
   it('should return the judeUserId as the chambers judge associated with the logged in user', () => {
-    currentUser = {
-      role: ROLES.chambers,
-      userId: chambersId,
-    };
-
     const result = runCompute(trialSessionsSummaryHelper, {
       state: {
         judgeUser: {
           role: ROLES.judge,
-          userId: judgeId,
+          userId: mockJudgeUser.userId,
         },
+        user: mockChambersUser,
       },
     });
 
-    expect(result.judgeUserId).toEqual(judgeId);
+    expect(result.judgeUserId).toEqual(mockJudgeUser.userId);
   });
 });
