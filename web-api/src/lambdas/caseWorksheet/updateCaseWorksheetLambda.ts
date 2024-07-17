aee9cf026525e631cbda5bf97c0ac353df41d435
Jim deVos <james.devos@ustaxcourt.gov>
2024-07-17 15:23:49 -0700

    10417: remove appcontext.getCurrentUser from updateCaseWorksheetInteractor

--- web-api/src/lambdas/caseWorksheet/updateCaseWorksheetLambda.ts
+++ web-api/src/lambdas/caseWorksheet/updateCaseWorksheetLambda.ts
@@ -1,11 +1,16 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
 
-export const updateCaseWorksheetLambda = event =>
+export const updateCaseWorksheetLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
   genericHandler(event, async ({ applicationContext }) => {
     return await applicationContext
       .getUseCases()
       .updateCaseWorksheetInteractor(
         applicationContext,
         JSON.parse(event.body),
+        authorizedUser,
       );
   });
