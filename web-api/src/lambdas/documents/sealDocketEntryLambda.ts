2858840fcd8e5a643fbc80a97e98b049b6f6cb3f
Jim deVos <james.devos@ustaxcourt.gov>
2024-07-18 15:15:50 -0700

    10417: remove appcontext.getCurrentUser from sealDocketEntryInteractor

--- web-api/src/lambdas/documents/sealDocketEntryLambda.ts
+++ web-api/src/lambdas/documents/sealDocketEntryLambda.ts
@@ -1,12 +1,14 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
 
 /**
  * used for sealing docket entries
  *
  * @param {object} event the AWS event object
+ * @param {UnknownAuthUser} authorizedUser current user associated with the request
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const sealDocketEntryLambda = event =>
+export const sealDocketEntryLambda = (event, authorizedUser: UnknownAuthUser) =>
   genericHandler(event, async ({ applicationContext }) => {
     const {
       pathParameters: { docketEntryId, docketNumber },
@@ -14,11 +16,13 @@ export const sealDocketEntryLambda = event =>
 
     const { docketEntrySealedTo } = JSON.parse(event.body);
 
-    return await applicationContext
-      .getUseCases()
-      .sealDocketEntryInteractor(applicationContext, {
+    return await applicationContext.getUseCases().sealDocketEntryInteractor(
+      applicationContext,
+      {
         docketEntryId,
         docketEntrySealedTo,
         docketNumber,
-      });
+      },
+      authorizedUser,
+    );
   });
