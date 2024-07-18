50ad430e09ec58b21243a7c5ff0f00fc18c7218d
Jim deVos <james.devos@ustaxcourt.gov>
2024-07-18 13:58:07 -0700

    10417: comment cleanup

--- web-api/src/lambdas/documents/addPaperFilingLambda.ts
+++ web-api/src/lambdas/documents/addPaperFilingLambda.ts
@@ -1,12 +1,6 @@
 import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
 
-/**
- * lambda used for adding a paper filing to a case
- *
- * @param {object} event the AWS event object
- * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
- */
 export const addPaperFilingLambda = (event, authorizedUser: UnknownAuthUser) =>
   genericHandler(event, async ({ applicationContext }) => {
     return await applicationContext.getUseCases().addPaperFilingInteractor(
