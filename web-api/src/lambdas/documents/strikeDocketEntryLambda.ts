d51454b8c7c68c19aee2156ca8838d95905a8f3f
Jim deVos <james.devos@ustaxcourt.gov>
2024-07-18 16:45:05 -0700

    10417: remove appcontext.getCurrentUser from strikeDocketEntryInteractor

--- web-api/src/lambdas/documents/strikeDocketEntryLambda.ts
+++ web-api/src/lambdas/documents/strikeDocketEntryLambda.ts
@@ -1,21 +1,28 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
 
 /**
  * used for striking docket records
  *
  * @param {object} event the AWS event object
+ * @param {UnknownAuthUser} authorizedUser current user associated with the request
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const strikeDocketEntryLambda = event =>
+export const strikeDocketEntryLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
   genericHandler(event, async ({ applicationContext }) => {
     const {
       pathParameters: { docketEntryId, docketNumber },
     } = event;
 
-    return await applicationContext
-      .getUseCases()
-      .strikeDocketEntryInteractor(applicationContext, {
+    return await applicationContext.getUseCases().strikeDocketEntryInteractor(
+      applicationContext,
+      {
         docketEntryId,
         docketNumber,
-      });
+      },
+      authorizedUser,
+    );
   });
