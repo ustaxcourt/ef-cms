a20744b40f392ce1f3cc6c41eb916b4e220b5245
Jim deVos <james.devos@ustaxcourt.gov>
2024-07-18 17:03:12 -0700

    10417: remove appcontext.getCurrentUser from updateCourtIssuedDocketEntryInteractor

--- web-api/src/lambdas/documents/updateCourtIssuedDocketEntryLambda.ts
+++ web-api/src/lambdas/documents/updateCourtIssuedDocketEntryLambda.ts
@@ -1,17 +1,23 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
 
 /**
  * lambda which is used for updating a court issued docket entry
  *
  * @param {object} event the AWS event object
+ * @param {UnknownAuthUser} authorizedUser user associated with the current request
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const updateCourtIssuedDocketEntryLambda = event =>
+export const updateCourtIssuedDocketEntryLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
   genericHandler(event, async ({ applicationContext }) => {
     return await applicationContext
       .getUseCases()
       .updateCourtIssuedDocketEntryInteractor(
         applicationContext,
         JSON.parse(event.body),
+        authorizedUser,
       );
   });
