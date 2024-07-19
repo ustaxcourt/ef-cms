2cd073001d094c494c2fc02b2545144e31e66231
Javis Sullivan <jsullivan@flexion.us>
2024-07-19 19:31:10 -0400

    10417 rm getCurrentUser from dismissNOTReminderForTrial
    code and tests

--- web-api/src/lambdas/trialSessions/dismissNOTTReminderForTrialLambda.ts
+++ web-api/src/lambdas/trialSessions/dismissNOTTReminderForTrialLambda.ts
@@ -1,3 +1,4 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
 
 /**
@@ -5,11 +6,18 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const dismissNOTTReminderForTrialLambda = event =>
+export const dismissNOTTReminderForTrialLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
   genericHandler(event, async ({ applicationContext }) => {
     return await applicationContext
       .getUseCases()
-      .dismissNOTTReminderForTrialInteractor(applicationContext, {
-        ...JSON.parse(event.body),
-      });
+      .dismissNOTTReminderForTrialInteractor(
+        applicationContext,
+        authorizedUser,
+        {
+          ...JSON.parse(event.body),
+        },
+      );
   });
