157e1c5fa47c0c9f05e7ad73eb34df14ec300b28
Nate Elliott <nelliott@flexion.us>
2024-07-19 12:42:10 -0500

    10417 remove getCurrentUser from getPrivatePractitionersBySearchKeyInteractor

--- web-api/src/lambdas/users/getPrivatePractitionersBySearchKeyLambda.ts
+++ web-api/src/lambdas/users/getPrivatePractitionersBySearchKeyLambda.ts
@@ -1,3 +1,4 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
 
 /**
@@ -6,13 +7,20 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const getPrivatePractitionersBySearchKeyLambda = event =>
+export const getPrivatePractitionersBySearchKeyLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
   genericHandler(event, async ({ applicationContext }) => {
     const { searchKey } = event.queryStringParameters;
 
     return await applicationContext
       .getUseCases()
-      .getPrivatePractitionersBySearchKeyInteractor(applicationContext, {
-        searchKey,
-      });
+      .getPrivatePractitionersBySearchKeyInteractor(
+        applicationContext,
+        {
+          searchKey,
+        },
+        authorizedUser,
+      );
   });
--- web-api/src/lambdas/users/getUserPendingEmailLambda.ts
+++ web-api/src/lambdas/users/getUserPendingEmailLambda.ts
@@ -1,3 +1,4 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
 
 /**
@@ -6,7 +7,10 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const getUserPendingEmailLambda = (event, authorizedUser) =>
+export const getUserPendingEmailLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
   genericHandler(event, async ({ applicationContext }) => {
     return await applicationContext.getUseCases().getUserPendingEmailInteractor(
       applicationContext,
