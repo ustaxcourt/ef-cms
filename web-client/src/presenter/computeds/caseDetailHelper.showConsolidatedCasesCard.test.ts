bc02097c3833805d3b9242ed50e6ab8474f3e886
Nate Elliott <nelliott@flexion.us>
2024-07-19 07:57:16 -0500

    10417 fix caseDetailHelper tests

--- web-client/src/presenter/computeds/caseDetailHelper.showConsolidatedCasesCard.test.ts
+++ web-client/src/presenter/computeds/caseDetailHelper.showConsolidatedCasesCard.test.ts
@@ -2,20 +2,18 @@ import {
   adcUser,
   irsPractitionerUser,
   petitionerUser,
-  privatePractitionerUser,
 } from '../../../../shared/src/test/mockUsers';
 import { applicationContextForClient as applicationContext } from '@web-client/test/createClientTestApplicationContext';
 import { caseDetailHelper as caseDetailHelperComputed } from './caseDetailHelper';
 import { getUserPermissions } from '../../../../shared/src/authorization/getUserPermissions';
+import { mockPrivatePractitionerUser } from '@shared/test/mockAuthUsers';
 import { runCompute } from '@web-client/presenter/test.cerebral';
 import { withAppContextDecorator } from '../../withAppContext';
 
-const caseDetailHelper = withAppContextDecorator(caseDetailHelperComputed, {
-  ...applicationContext,
-  getCurrentUser: () => {
-    return globalUser;
-  },
-});
+const caseDetailHelper = withAppContextDecorator(
+  caseDetailHelperComputed,
+  applicationContext,
+);
 
 let globalUser;
 
@@ -23,6 +21,7 @@ const getBaseState = user => {
   globalUser = user;
   return {
     permissions: getUserPermissions(user),
+    user: globalUser,
   };
 };
 
@@ -43,7 +42,7 @@ describe('showConsolidatedCasesCard', () => {
   });
 
   it('should be true when the user is a private practitioner and has the VIEW_CONSOLIDATED_CASES_CARD permission and the case is in a consolidated group', () => {
-    const user = privatePractitionerUser;
+    const user = mockPrivatePractitionerUser;
 
     const result = runCompute(caseDetailHelper, {
       state: {
