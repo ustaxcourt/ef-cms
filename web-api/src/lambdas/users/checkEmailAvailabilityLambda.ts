816b2bf0c63ab215b7148ee1346161cf64afa2da
Nate Elliott <nelliott@flexion.us>
2024-07-19 12:53:10 -0500

    10417 remove getCurrentUser from checkEmailAvailabilityInteractor

--- web-api/src/lambdas/users/checkEmailAvailabilityLambda.ts
+++ web-api/src/lambdas/users/checkEmailAvailabilityLambda.ts
@@ -1,3 +1,4 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
 
 /**
@@ -6,13 +7,20 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const checkEmailAvailabilityLambda = event =>
+export const checkEmailAvailabilityLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
   genericHandler(event, async ({ applicationContext }) => {
     const { email } = event.queryStringParameters;
 
     return await applicationContext
       .getUseCases()
-      .checkEmailAvailabilityInteractor(applicationContext, {
-        email,
-      });
+      .checkEmailAvailabilityInteractor(
+        applicationContext,
+        {
+          email,
+        },
+        authorizedUser,
+      );
   });
