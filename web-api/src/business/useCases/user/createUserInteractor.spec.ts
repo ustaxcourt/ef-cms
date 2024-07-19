a0d8c3d322cb76d4177faf5751f8047a20edafab
Nate Elliott <nelliott@flexion.us>
2024-07-19 12:50:04 -0500

    10417 remove getCurrentUser from createUserInteractor

--- web-api/src/business/useCases/user/createUserInteractor.spec.ts
+++ web-api/src/business/useCases/user/createUserInteractor.spec.ts
@@ -1,8 +1,8 @@
-import { ROLES } from '@shared/business/entities/EntityConstants';
 import { applicationContext } from '@shared/business/test/createTestApplicationContext';
 import { createOrUpdateUser } from '../../../../../shared/admin-tools/user/admin';
 import { createUserInteractor } from '@web-api/business/useCases/user/createUserInteractor';
 import { docketClerk1User } from '@shared/test/mockUsers';
+import { mockAdminUser, mockPetitionerUser } from '@shared/test/mockAuthUsers';
 
 jest.mock('../../../../../shared/admin-tools/user/admin', () => ({
   createOrUpdateUser: jest.fn(),
@@ -10,27 +10,26 @@ jest.mock('../../../../../shared/admin-tools/user/admin', () => ({
 
 describe('createUserInteractor', () => {
   it('should throw an error if a user with create permissions tries to create a user', async () => {
-    applicationContext.getCurrentUser.mockReturnValue({
-      role: ROLES.legacyJudge,
-      userId: '47cc9719-a392-4cb0-b744-ffb243eff3a3',
-    });
     await expect(
-      createUserInteractor(applicationContext, {
-        user: { ...docketClerk1User, password: 'junkPass' },
-      }),
+      createUserInteractor(
+        applicationContext,
+        {
+          user: { ...docketClerk1User, password: 'junkPass' },
+        },
+        mockPetitionerUser,
+      ),
     ).rejects.toThrow('Unauthorized');
     expect(createOrUpdateUser).not.toHaveBeenCalled();
   });
 
   it('should create a new user when an admin tries to create a user', async () => {
-    applicationContext.getCurrentUser.mockReturnValue({
-      role: ROLES.admin,
-      userId: '3aa4b9c3-a66a-486a-bd01-fb370f52f45a',
-    });
-
-    await createUserInteractor(applicationContext, {
-      user: { ...docketClerk1User, password: 'junkPass' },
-    });
+    await createUserInteractor(
+      applicationContext,
+      {
+        user: { ...docketClerk1User, password: 'junkPass' },
+      },
+      mockAdminUser,
+    );
 
     expect(createOrUpdateUser).toHaveBeenCalledWith(expect.anything(), {
       password: 'junkPass',
