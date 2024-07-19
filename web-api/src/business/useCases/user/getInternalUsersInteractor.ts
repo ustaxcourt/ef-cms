a2dfa455be6711995c9c0461d9d7f571bcd87f26
Nate Elliott <nelliott@flexion.us>
2024-07-19 12:45:43 -0500

    10417 remove getCurrentUser from getInternalUsersInteractor

--- web-api/src/business/useCases/user/getInternalUsersInteractor.ts
+++ web-api/src/business/useCases/user/getInternalUsersInteractor.ts
@@ -4,6 +4,7 @@ import {
 } from '../../../../../shared/src/authorization/authorizationClientService';
 import { ServerApplicationContext } from '@web-api/applicationContext';
 import { UnauthorizedError } from '@web-api/errors/errors';
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { User } from '../../../../../shared/src/business/entities/User';
 
 /**
@@ -14,13 +15,9 @@ import { User } from '../../../../../shared/src/business/entities/User';
  */
 export const getInternalUsersInteractor = async (
   applicationContext: ServerApplicationContext,
+  authorizedUser: UnknownAuthUser,
 ) => {
-  if (
-    !isAuthorized(
-      applicationContext.getCurrentUser(),
-      ROLE_PERMISSIONS.WORKITEM,
-    )
-  ) {
+  if (!isAuthorized(authorizedUser, ROLE_PERMISSIONS.WORKITEM)) {
     throw new UnauthorizedError('Unauthorized');
   }
 
