ce299fa0c6af6365c70122bc2e42b53d199a17d4
Jim deVos <james.devos@ustaxcourt.gov>
2024-07-22 13:49:46 -0700

    10417: remove appcontext.getCurrentUser from getMessageThreadInteractor

--- web-api/src/lambdas/messages/getMessageThreadLambda.ts
+++ web-api/src/lambdas/messages/getMessageThreadLambda.ts
@@ -1,16 +1,16 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
 
-/**
- * lambda which is used for retrieving messages by the parent message id
- *
- * @param {object} event the AWS event object
- * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
- */
-export const getMessageThreadLambda = event =>
+export const getMessageThreadLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
   genericHandler(event, async ({ applicationContext }) => {
-    return await applicationContext
-      .getUseCases()
-      .getMessageThreadInteractor(applicationContext, {
+    return await applicationContext.getUseCases().getMessageThreadInteractor(
+      applicationContext,
+      {
         parentMessageId: event.pathParameters.parentMessageId,
-      });
+      },
+      authorizedUser,
+    );
   });
