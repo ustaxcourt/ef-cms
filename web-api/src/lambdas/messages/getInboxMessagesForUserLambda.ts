f854c0aa8d3ed08e8365bea135e61fba0dcaf6d4
Jim deVos <james.devos@ustaxcourt.gov>
2024-07-22 13:45:55 -0700

    10417: remove appcontext.getCurrentUser from getInboxMessagesForUserInteractor

--- web-api/src/lambdas/messages/getInboxMessagesForUserLambda.ts
+++ web-api/src/lambdas/messages/getInboxMessagesForUserLambda.ts
@@ -1,16 +1,18 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
 
-/**
- * gets the inbox messages for the user
- *
- * @param {object} event the AWS event object
- * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
- */
-export const getInboxMessagesForUserLambda = event =>
+export const getInboxMessagesForUserLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
   genericHandler(event, async ({ applicationContext }) => {
     return await applicationContext
       .getUseCases()
-      .getInboxMessagesForUserInteractor(applicationContext, {
-        userId: event.pathParameters.userId,
-      });
+      .getInboxMessagesForUserInteractor(
+        applicationContext,
+        {
+          userId: event.pathParameters.userId,
+        },
+        authorizedUser,
+      );
   });
