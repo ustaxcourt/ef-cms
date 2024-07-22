d6729c4646ac2c7418418a2a2b14fb9cc2c70c5f
Jim deVos <james.devos@ustaxcourt.gov>
2024-07-22 12:53:58 -0700

    10417: remove appcontext.getCurrentUser from associatePrivatePractitionerWithCaseInteractor

--- web-api/src/lambdas/manualAssociation/associatePrivatePractitionerWithCaseLambda.ts
+++ web-api/src/lambdas/manualAssociation/associatePrivatePractitionerWithCaseLambda.ts
@@ -1,16 +1,18 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
 
-/**
- * associate practitioner with case
- *
- * @param {object} event the AWS event object
- * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
- */
-export const associatePrivatePractitionerWithCaseLambda = event =>
+export const associatePrivatePractitionerWithCaseLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
   genericHandler(event, async ({ applicationContext }) => {
     return await applicationContext
       .getUseCases()
-      .associatePrivatePractitionerWithCaseInteractor(applicationContext, {
-        ...JSON.parse(event.body),
-      });
+      .associatePrivatePractitionerWithCaseInteractor(
+        applicationContext,
+        {
+          ...JSON.parse(event.body),
+        },
+        authorizedUser,
+      );
   });
