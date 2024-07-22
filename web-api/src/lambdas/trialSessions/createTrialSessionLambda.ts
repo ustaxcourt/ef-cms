5deb4a68ce0e15a537806b8afe75cfa50bee182b
Nate Elliott <nelliott@flexion.us>
2024-07-22 09:50:11 -0500

    10417 remove getCurrentUser from createTrialSessionInteractor

--- web-api/src/lambdas/trialSessions/createTrialSessionLambda.ts
+++ web-api/src/lambdas/trialSessions/createTrialSessionLambda.ts
@@ -1,3 +1,4 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
 
 /**
@@ -6,11 +7,16 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const createTrialSessionLambda = event =>
+export const createTrialSessionLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
   genericHandler(event, async ({ applicationContext }) => {
-    return await applicationContext
-      .getUseCases()
-      .createTrialSessionInteractor(applicationContext, {
+    return await applicationContext.getUseCases().createTrialSessionInteractor(
+      applicationContext,
+      {
         trialSession: JSON.parse(event.body),
-      });
+      },
+      authorizedUser,
+    );
   });
