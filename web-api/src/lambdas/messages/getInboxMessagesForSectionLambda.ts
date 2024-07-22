fda2e10c7d7162ffb517cc6e54296212765dcc94
Jim deVos <james.devos@ustaxcourt.gov>
2024-07-22 13:36:18 -0700

    10417: remove appcontext.getCurrentUser from getInboxMessagesForSectionInteractor

--- web-api/src/lambdas/messages/getInboxMessagesForSectionLambda.ts
+++ web-api/src/lambdas/messages/getInboxMessagesForSectionLambda.ts
@@ -1,16 +1,18 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
 
-/**
- * gets the inbox messages for the section
- *
- * @param {object} event the AWS event object
- * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
- */
-export const getInboxMessagesForSectionLambda = event =>
+export const getInboxMessagesForSectionLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
   genericHandler(event, async ({ applicationContext }) => {
     return await applicationContext
       .getUseCases()
-      .getInboxMessagesForSectionInteractor(applicationContext, {
-        section: event.pathParameters.section,
-      });
+      .getInboxMessagesForSectionInteractor(
+        applicationContext,
+        {
+          section: event.pathParameters.section,
+        },
+        authorizedUser,
+      );
   });
