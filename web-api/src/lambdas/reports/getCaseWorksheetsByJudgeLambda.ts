03de2507d4d401a53d7e76f39d8d48dd2d23a798
Jim deVos <james.devos@ustaxcourt.gov>
2024-07-22 12:31:41 -0700

    10417: remove appcontext.getCurrentUser from getCaseWorksheetsByJudgeInteractor

--- web-api/src/lambdas/reports/getCaseWorksheetsByJudgeLambda.ts
+++ web-api/src/lambdas/reports/getCaseWorksheetsByJudgeLambda.ts
@@ -1,15 +1,20 @@
-import { APIGatewayProxyEvent } from 'aws-lambda';
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
 
-export const getCaseWorksheetsByJudgeLambda = (event: APIGatewayProxyEvent) =>
+export const getCaseWorksheetsByJudgeLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
   genericHandler(
     event,
+
     async ({ applicationContext }) => {
       return await applicationContext
         .getUseCases()
         .getCaseWorksheetsByJudgeInteractor(
           applicationContext,
           event.queryStringParameters,
+          authorizedUser,
         );
     },
     { logResults: false },
