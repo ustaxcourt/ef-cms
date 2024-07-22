74770caea31a98bb9a4908de002ef52e69b17ec7
Jim deVos <james.devos@ustaxcourt.gov>
2024-07-22 13:24:30 -0700

    10417: remove appcontext.getCurrentUser from getCompletedMessagesForUserInteractor

--- web-api/src/business/useCases/messages/getCompletedMessagesForUserInteractor.ts
+++ web-api/src/business/useCases/messages/getCompletedMessagesForUserInteractor.ts
@@ -5,21 +5,13 @@ import {
 } from '../../../../../shared/src/authorization/authorizationClientService';
 import { ServerApplicationContext } from '@web-api/applicationContext';
 import { UnauthorizedError } from '@web-api/errors/errors';
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 
-/**
- * getCompletedMessagesForUserInteractor
- *
- * @param {object} applicationContext the application context
- * @param {object} providers the providers object
- * @param {string} providers.userId the user to get the inbox messages
- * @returns {object} the messages in the user inbox
- */
 export const getCompletedMessagesForUserInteractor = async (
   applicationContext: ServerApplicationContext,
   { userId }: { userId: string },
+  authorizedUser: UnknownAuthUser,
 ) => {
-  const authorizedUser = applicationContext.getCurrentUser();
-
   if (!isAuthorized(authorizedUser, ROLE_PERMISSIONS.VIEW_MESSAGES)) {
     throw new UnauthorizedError('Unauthorized');
   }
--- web-api/src/lambdas/messages/getCompletedMessagesForUserLambda.ts
+++ web-api/src/lambdas/messages/getCompletedMessagesForUserLambda.ts
@@ -1,16 +1,18 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
 
-/**
- * gets the completed messages for the user
- *
- * @param {object} event the AWS event object
- * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
- */
-export const getCompletedMessagesForUserLambda = event =>
+export const getCompletedMessagesForUserLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
   genericHandler(event, async ({ applicationContext }) => {
     return await applicationContext
       .getUseCases()
-      .getCompletedMessagesForUserInteractor(applicationContext, {
-        userId: event.pathParameters.userId,
-      });
+      .getCompletedMessagesForUserInteractor(
+        applicationContext,
+        {
+          userId: event.pathParameters.userId,
+        },
+        authorizedUser,
+      );
   });
