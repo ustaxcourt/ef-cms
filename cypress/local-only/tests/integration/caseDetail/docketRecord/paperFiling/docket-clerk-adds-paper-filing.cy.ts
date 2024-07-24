b401ac4fddbfedfb7e34fcc44344553ac562764b
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-07-24 13:59:28 -0500

    5512: remove remaining instances of toggleFeatureFlag

--- cypress/local-only/tests/integration/caseDetail/docketRecord/paperFiling/docket-clerk-adds-paper-filing.cy.ts
+++ cypress/local-only/tests/integration/caseDetail/docketRecord/paperFiling/docket-clerk-adds-paper-filing.cy.ts
@@ -12,22 +12,6 @@ import { petitionsClerkServesPetition } from '../../../../../../helpers/document
  * Then they should see a validation error message
  */
 describe('Docket clerk adding a paper filing', () => {
-  before(() => {
-    cy.task('toggleFeatureFlag', {
-      flag: 'updated-petition-flow',
-      flagValue: false,
-    });
-
-    cy.reload(true);
-  });
-
-  after(() => {
-    cy.task('toggleFeatureFlag', {
-      flag: 'updated-petition-flow',
-      flagValue: true,
-    });
-  });
-
   it('should be alerted when the paper filing is not valid when they attempt to serve', () => {
     loginAsPetitioner();
     petitionerCreatesElectronicCase().then(docketNumber => {
