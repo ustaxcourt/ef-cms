8d06e129ade162ff08de1476c74e2a5ddaac95e6
Nate Elliott <nelliott@flexion.us>
2024-07-25 15:22:43 -0500

    10417 add authorizedUser to forwardMessageLambda

--- web-api/src/lambdas/messages/forwardMessageLambda.ts
+++ web-api/src/lambdas/messages/forwardMessageLambda.ts
@@ -1,3 +1,4 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { forwardMessageInteractor } from '@web-api/business/useCases/messages/forwardMessageInteractor';
 import { genericHandler } from '../../genericHandler';
 
@@ -7,10 +8,14 @@ import { genericHandler } from '../../genericHandler';
  * @param {object} event the AWS event object
  * @returns {Promise<*|undefined>} the api gateway response object containing the statusCode, body, and headers
  */
-export const forwardMessageLambda = event =>
+export const forwardMessageLambda = (event, authorizedUser: UnknownAuthUser) =>
   genericHandler(event, async ({ applicationContext }) => {
-    return await forwardMessageInteractor(applicationContext, {
-      parentMessageId: event.pathParameters.parentMessageId,
-      ...JSON.parse(event.body),
-    });
+    return await forwardMessageInteractor(
+      applicationContext,
+      {
+        parentMessageId: event.pathParameters.parentMessageId,
+        ...JSON.parse(event.body),
+      },
+      authorizedUser,
+    );
   });
