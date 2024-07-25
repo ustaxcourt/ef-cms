99d0ff3b89f1b09e89115d6ba28efba249ba230b
Nechama Krigsman <nkrigsman@flexion.us>
2024-07-25 13:11:49 -0400

    5512: Extracted React components from Step 1 to own files;

---
+++ web-client/src/views/StartCaseUpdated/BusinessInfo.tsx
@@ -0,0 +1,85 @@
+import { BUSINESS_TYPES } from '@shared/business/entities/EntityConstants';
+import { ContactPrimaryUpdated } from '@web-client/views/StartCase/ContactPrimaryUpdated';
+import { CorporateDisclosureUpload } from './CorporateDisclosureUpload';
+import { FormGroup } from '@web-client/ustc-ui/FormGroup/FormGroup';
+import React from 'react';
+
+export function BusinessInfo({
+  businessFieldNames,
+  form,
+  petitionGenerationLiveValidationSequence,
+  registerRef,
+  updateFilingTypeSequence,
+  updateFormValueCountryTypeSequence,
+  updateFormValueUpdatedSequence,
+  validationErrors,
+}) {
+  const selectedBusinessType = form.businessType;
+  return (
+    <div className="ustc-secondary-question">
+      <FormGroup
+        errorMessageId="business-type-error-message"
+        errorText={validationErrors.businessType}
+      >
+        <fieldset className="usa-fieldset" id="business-type-radios">
+          <legend id="business-type-legend">
+            What type of business are you filing for?
+          </legend>
+          {[
+            BUSINESS_TYPES.corporation,
+            BUSINESS_TYPES.partnershipAsTaxMattersPartner,
+            BUSINESS_TYPES.partnershipOtherThanTaxMatters,
+            BUSINESS_TYPES.partnershipBBA,
+          ].map((businessType, idx) => (
+            <div className="usa-radio max-width-fit-content" key={businessType}>
+              <input
+                aria-describedby="business-type-legend"
+                checked={selectedBusinessType === businessType}
+                className="usa-radio__input"
+                id={`businessType-${businessType}`}
+                name="businessType"
+                type="radio"
+                value={businessType}
+                onChange={e => {
+                  updateFilingTypeSequence({
+                    key: e.target.name,
+                    value: e.target.value,
+                  });
+                }}
+              />
+              <label
+                className="usa-radio__label business-type-radio-option"
+                data-testid={`business-type-${idx}`}
+                htmlFor={`businessType-${businessType}`}
+                id={`is-business-type-${idx}`}
+              >
+                {businessType}
+              </label>
+            </div>
+          ))}
+        </fieldset>
+      </FormGroup>
+
+      {selectedBusinessType && (
+        <div>
+          <ContactPrimaryUpdated
+            addressInfo={form.contactPrimary}
+            handleBlur={petitionGenerationLiveValidationSequence}
+            handleChange={updateFormValueUpdatedSequence}
+            handleChangeCountryType={updateFormValueCountryTypeSequence}
+            nameLabel={businessFieldNames.primary}
+            placeOfLegalResidenceTitle="Place of business"
+            registerRef={registerRef}
+            secondaryLabel={businessFieldNames.secondary}
+            showInCareOf={businessFieldNames.showInCareOf}
+            showInCareOfOptional={businessFieldNames.showInCareOfOptional}
+          />
+          <CorporateDisclosureUpload
+            hasCorporateDisclosureFile={form.corporateDisclosureFile}
+            validationErrors={validationErrors}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ web-client/src/views/StartCaseUpdated/SecondaryMinorIncompetentOptions.tsx
@@ -0,0 +1,61 @@
+import { FormGroup } from '@web-client/ustc-ui/FormGroup/FormGroup';
+import { OTHER_TYPES } from '@shared/business/entities/EntityConstants';
+import React from 'react';
+
+export function SecondaryMinorIncompetentOptions({
+  selectedMinorIncompetentType,
+  updateFilingTypeSequence,
+  validationErrors,
+}) {
+  return (
+    <div className="ustc-secondary-question">
+      <FormGroup
+        errorMessageId="minor-incompetent-type-error-message"
+        errorText={validationErrors.minorIncompetentType}
+      >
+        <fieldset className="usa-fieldset usa-sans" id="estate-type-radios">
+          <legend id="estate-type-legend">
+            What is your role in filing for this minor or legally incompetent
+            person?
+          </legend>
+          {[
+            OTHER_TYPES.conservator,
+            OTHER_TYPES.guardian,
+            OTHER_TYPES.custodian,
+            OTHER_TYPES.nextFriendForMinor,
+            OTHER_TYPES.nextFriendForIncompetentPerson,
+          ].map((minorIncompetentType, idx) => (
+            <div
+              className="usa-radio max-width-fit-content"
+              key={minorIncompetentType}
+            >
+              <input
+                aria-describedby="minorIncompetent-type-legend"
+                checked={selectedMinorIncompetentType === minorIncompetentType}
+                className="usa-radio__input"
+                id={`minorIncompetentType-${minorIncompetentType}`}
+                name="minorIncompetentType"
+                type="radio"
+                value={minorIncompetentType}
+                onChange={e => {
+                  updateFilingTypeSequence({
+                    key: e.target.name,
+                    value: e.target.value,
+                  });
+                }}
+              />
+              <label
+                className="usa-radio__label"
+                data-testid={`minor-incompetent-type-radio-option-${idx}`}
+                htmlFor={`minorIncompetentType-${minorIncompetentType}`}
+                id={`is-minorIncompetent-type-${idx}`}
+              >
+                {minorIncompetentType}
+              </label>
+            </div>
+          ))}
+        </fieldset>
+      </FormGroup>
+    </div>
+  );
+}
