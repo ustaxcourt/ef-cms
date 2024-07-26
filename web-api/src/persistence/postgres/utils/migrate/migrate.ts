4392e6eb26b753d1c3b01ba7695fd1098a85833e
Kaitlyn Swann <kswann@flexion.us>
2024-07-26 06:51:52 -0600

    10391: add migration support

---
+++ web-api/src/persistence/postgres/utils/migrate/migrate.ts
@@ -0,0 +1,35 @@
+import * as path from 'path';
+import { FileMigrationProvider, Migrator } from 'kysely';
+import { db } from '../../../../database';
+import { promises as fs } from 'fs';
+
+async function migrateToLatest() {
+  const migrator = new Migrator({
+    db,
+    provider: new FileMigrationProvider({
+      fs,
+      migrationFolder: path.join(__dirname, 'migrations'),
+      path,
+    }),
+  });
+
+  const { error, results } = await migrator.migrateToLatest();
+
+  results?.forEach(it => {
+    if (it.status === 'Success') {
+      console.log(`migration "${it.migrationName}" was executed successfully`);
+    } else if (it.status === 'Error') {
+      console.error(`failed to execute migration "${it.migrationName}"`);
+    }
+  });
+
+  if (error) {
+    console.error('failed to migrate');
+    console.error(error);
+    process.exit(1);
+  }
+
+  await db.destroy();
+}
+
+migrateToLatest().catch;
