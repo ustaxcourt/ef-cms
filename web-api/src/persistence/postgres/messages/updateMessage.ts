b9e46f5feb9c6ff1c75423a28228f996ff5755a2
Cody Seibert <cseibert113@gmail.com>
2024-07-26 16:09:08 -0400

    10391 - fixing restart issue with streams constantly restarting

--- web-api/src/persistence/postgres/messages/updateMessage.ts
+++ web-api/src/persistence/postgres/messages/updateMessage.ts
@@ -1,29 +1,12 @@
 import { RawMessage } from '@shared/business/entities/Message';
 import { db } from '@web-api/database';
-import { getMessageById } from '@web-api/persistence/postgres/messages/getMessageById';
-import { omit } from 'lodash';
+import { toKyselyUpdateMessage } from './mapper';
 
 export const updateMessage = async ({ message }: { message: RawMessage }) => {
-  const existingMessage = await getMessageById({
-    messageId: message.messageId,
-  });
-
-  if (existingMessage) {
-    const updatedMessage = {
-      ...existingMessage,
-      ...message,
-    };
-
-    return await db
-      .updateTable('message')
-      .set({
-        ...omit(updatedMessage, 'entityName'),
-        attachments: JSON.stringify(updatedMessage.attachments),
-      })
-      .where('messageId', '=', message.messageId)
-      .returningAll()
-      .executeTakeFirst();
-  } else {
-    throw new Error(`Message with id ${message.messageId} not found`);
-  }
+  return await db
+    .updateTable('message')
+    .set(toKyselyUpdateMessage(message))
+    .where('messageId', '=', message.messageId)
+    .returningAll()
+    .executeTakeFirst();
 };
