c5b9da803d3e5c30f97a4293933d682c348feac2
Kaitlyn Swann <kswann@flexion.us>
2024-07-26 09:05:40 -0600

    test

--- web-api/terraform/applyables/green/green.tf
+++ web-api/terraform/applyables/green/green.tf
@@ -78,7 +78,8 @@ resource "terraform_data" "locals" {
     STAGE                              = var.environment
     USER_POOL_ID                       = data.terraform_remote_state.remote.outputs.aws_cognito_user_pool_id
     USER_POOL_IRS_ID                   = data.terraform_remote_state.remote.outputs.aws_cognito_user_pool_irs_id
-    POSTGRES_USERNAME                  = var.postgres_user
+    POSTGRES_HOST                      = data.terraform_remote_state.remote.outputs.rds_host_name
+    POSTGRES_USER                      = var.postgres_user
     POSTGRES_PASSWORD                  = var.postgres_password
   }
 }
