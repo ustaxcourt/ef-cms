27e69133ebd4372d8309bd3819287d45ea1b0cdb
Christopher Bisom <cbisom@flexion.us>
2024-07-26 15:16:25 -0400

    10349-bug: undo logging, try removing render check in app.tsx to see what happens

--- cypress.config.ts
+++ cypress.config.ts
@@ -75,9 +75,6 @@ export default defineConfig({
           });
         },
       });
-      require('cypress-terminal-report/src/installLogsPrinter')(on, {
-        printLogsToConsole: 'always',
-      });
       // Setup for puppeteer, which supports multi-tab tests
       // Define your function in onMessage, and call it like cy.puppeteer('yourFunctionName', arg1, arg2 ...)
       setup({
