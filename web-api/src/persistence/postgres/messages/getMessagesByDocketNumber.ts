b3d65bb512e0f9dcf1a5fd0c9f2615de93630caf
Kaitlyn Swann <kswann@flexion.us>
2024-07-30 10:53:43 -0600

    10391: add jest mocks

--- web-api/src/persistence/postgres/messages/getMessagesByDocketNumber.ts
+++ web-api/src/persistence/postgres/messages/getMessagesByDocketNumber.ts
@@ -2,14 +2,6 @@ import { MessageResult } from '@shared/business/entities/MessageResult';
 import { db } from '@web-api/database';
 import { transformNullToUndefined } from '@web-api/persistence/postgres/utils/transformNullToUndefined';
 
-/**
- * getMessagesByDocketNumber
- *
- * @param {object} providers the providers object
- * @param {object} providers.applicationContext the application context
- * @param {string} providers.docketNumber the docket number of the case
- * @returns {object} the messages
- */
 export const getMessagesByDocketNumber = async ({
   applicationContext,
   docketNumber,
