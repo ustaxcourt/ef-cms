54cb332d0f021d8438154f45121ed42f374f26ca
Nechama Krigsman <nkrigsman@flexion.us>
2024-07-31 01:00:05 -0400

    5512: accessibility test step 7 and welcome page;

--- cypress/local-only/tests/accessibility/petitionGeneration/petition-generation-before-filing-a-case.cy.ts
+++ cypress/local-only/tests/accessibility/petitionGeneration/petition-generation-before-filing-a-case.cy.ts
@@ -1,8 +1,13 @@
 import { checkA11y } from '../../../support/generalCommands/checkA11y';
 import { loginAsPetitioner } from '../../../../helpers/authentication/login-as-helpers';
 
-describe('Petition generation - before filing a petition page', () => {
-  it('should be free of a11y issues', () => {
+describe('Petition generation - intro pages', () => {
+  it('welcome to dawson: should be free of a11y issues', () => {
+    loginAsPetitioner('petitioner7');
+    checkA11y();
+  });
+
+  it('before filing a case: should be free of a11y issues', () => {
     loginAsPetitioner();
     cy.visit('/before-filing-a-petition');
     checkA11y();
