837472959f6f8a5c8f1716f9152f4e203054f404
Christopher Bisom <cbisom@flexion.us>
2024-08-02 01:30:03 -0400

    10323-story: get tabs to render too

--- web-client/src/views/CreateOrder/TextEditor.tsx
+++ web-client/src/views/CreateOrder/TextEditor.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 import 'react-quill/dist/quill.snow.css';
+import { DomUtils, parseDocument } from 'htmlparser2';
 import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html';
+import { render as renderHtml } from 'dom-serializer';
 import React, { Suspense, useEffect, useRef } from 'react';
 import reactQuill from 'react-quill';
 
@@ -19,6 +21,32 @@ const ReactQuill = React.lazy(async () => {
   return { default: reactQuill };
 });
 
+// Quill does two things to render indentations:
+// 1) Adds padding-left so that the richText (and therefore the pdf) has indentations.
+// 2) Adds custom ql-indent-{level} classes to display the indentation in the Quill editor itself.
+// As part of the richText, 1 is automatically preserved across save and reload.
+// As part of the client-side Quill text rendering, 2 is *not* preserved across save and reload,
+// so even though the indentations are "there" in the richText, the user won't see them in the editor.
+// We call addQuillIndentationClasses on loading the text into the editor in order to correct that.
+const addQuillIndentationClasses = (html: string) => {
+  const dom = parseDocument(html);
+  // Traverse the DOM and add the appropriate ql-indent-{level} class corresponding to any elements with padding-left
+  DomUtils.findAll(elem => {
+    if (elem.name === 'p' && elem.attribs && elem.attribs.style) {
+      const { style } = elem.attribs;
+      const paddingLeftMatch = style.match(/padding-left:([0-9]+)em/);
+      if (paddingLeftMatch) {
+        const paddingLeftValue = parseInt(paddingLeftMatch[1]);
+        const indentLevel = paddingLeftValue / 3;
+        elem.attribs.class = `ql-indent-${indentLevel}`;
+      }
+    }
+    return true;
+  }, dom.children);
+
+  return renderHtml(dom);
+};
+
 export const TextEditor = ({
   defaultValue,
   editorDelta,
@@ -26,6 +54,7 @@ export const TextEditor = ({
   updateScreenMetadataSequence,
 }) => {
   const quillEscapeRef = useRef(null);
+  defaultValue = addQuillIndentationClasses(defaultValue);
 
   const onKeyboard = event => {
     const pressedESC = event.keyCode === 27;
@@ -84,9 +113,8 @@ export const TextEditor = ({
             const converter = new QuillDeltaToHtmlConverter(fullDelta.ops, {
               // We add custom CSS styles to maintain tabbing across save and reload.
               // (Note that preserveWhitespace does not work: https://github.com/ustaxcourt/ef-cms/pull/1408.)
-              // TODO: preserve toolbar-generated indentation across save and reload.
               customCssStyles: () => {
-                return 'white-space: pre-wrap !important;';
+                return 'white-space: pre-wrap !important';
               },
               inlineStyles: {
                 size: inlineStylesFontSizes,
