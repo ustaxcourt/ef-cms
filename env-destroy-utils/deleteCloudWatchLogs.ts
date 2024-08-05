ba56d5370cea5bbe2a8e9ea8fccb6036548b1236
Jim Lerza <james.lerza@ustaxcourt.gov>
2024-08-05 12:26:21 -0400

    devex: convert env-destroy-utils to use CloudWatchLogsClient from the AWS v3 SDK

--- env-destroy-utils/deleteCloudWatchLogs.ts
+++ env-destroy-utils/deleteCloudWatchLogs.ts
@@ -1,30 +1,41 @@
-const { getCloudWatch } = require('./getCloudWatch');
+import {
+  DeleteLogGroupCommand,
+  DescribeLogGroupsCommand,
+} from '@aws-sdk/client-cloudwatch-logs';
+import { getCloudWatch } from './getCloudWatch';
 
-exports.deleteCloudWatchLogs = async ({ environment }) => {
+export const deleteCloudWatchLogs = async ({
+  environment,
+}: {
+  environment: { name: string; region: string };
+}): Promise<void> => {
   const cloudWatchLogs = getCloudWatch({ environment });
 
-  let logGroups = await cloudWatchLogs.describeLogGroups().promise();
-  let envToDestroyLogGroups = logGroups.logGroups.filter(group =>
-    group.logGroupName.includes(environment.name),
-  );
+  let logGroups = await cloudWatchLogs.send(new DescribeLogGroupsCommand({}));
+  let envToDestroyLogGroups =
+    logGroups.logGroups?.filter(group =>
+      group.logGroupName?.includes(environment.name),
+    ) || [];
 
   do {
-    logGroups = await cloudWatchLogs
-      .describeLogGroups({ nextToken: logGroups.nextToken })
-      .promise();
+    logGroups = await cloudWatchLogs.send(
+      new DescribeLogGroupsCommand({ nextToken: logGroups.nextToken }),
+    );
 
+    const moreEnvToDestroyLogGroups =
+      logGroups.logGroups?.filter(group =>
+        group.logGroupName?.includes(environment.name),
+      ) || [];
     envToDestroyLogGroups = envToDestroyLogGroups.concat(
-      logGroups.logGroups.filter(group =>
-        group.logGroupName.includes(environment.name),
-      ),
+      moreEnvToDestroyLogGroups,
     );
   } while (logGroups.nextToken);
 
   for (const group of envToDestroyLogGroups) {
     console.log('Deleting CloudWatch log group: ', group.logGroupName);
 
-    await cloudWatchLogs
-      .deleteLogGroup({ logGroupName: group.logGroupName })
-      .promise();
+    await cloudWatchLogs.send(
+      new DeleteLogGroupCommand({ logGroupName: group.logGroupName }),
+    );
   }
 };
