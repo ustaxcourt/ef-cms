eff97a4daba30dda7cc5ef1d822162fb9be9255f
John Cruz <jcruz@flexion.us>
2024-08-06 13:18:58 -0600

    10432: Construct object with info we want to log;

--- web-client/src/presenter/actions/performanceMeasurementEndAction.ts
+++ web-client/src/presenter/actions/performanceMeasurementEndAction.ts
@@ -1,16 +1,38 @@
+import { ALLOWLIST_FEATURE_FLAGS } from '@shared/business/entities/EntityConstants';
+import { state } from '@web-client/presenter/app.cerebral';
+
 export const performanceMeasurementEndAction = ({
+  get,
   props,
 }: ActionProps<{
   sequenceName?: string;
   performanceMeasurementStart?: number;
+  actionPerformanceArray?: { actionName: string; duration: number }[];
 }>) => {
-  const { performanceMeasurementStart, sequenceName } = props;
-  if (!sequenceName || !performanceMeasurementStart) return;
+  const { email } = get(state.user) || {};
+  const { actionPerformanceArray, performanceMeasurementStart, sequenceName } =
+    props;
+
+  if (!sequenceName || !performanceMeasurementStart || !email) return;
+
   const performanceMeasurementEnd = Date.now();
   const durationInSeconds =
     (performanceMeasurementEnd - performanceMeasurementStart) / 1000;
 
-  const MINIMUM_TIME_LIMIT_IN_SECONDS = 5;
-  if (durationInSeconds > MINIMUM_TIME_LIMIT_IN_SECONDS)
-    console.log(`${sequenceName}: ${durationInSeconds} seconds`);
+  const featureFlags = get(state.featureFlags);
+  const MINIMUM_TIME_LIMIT_IN_SECONDS =
+    +featureFlags[
+      ALLOWLIST_FEATURE_FLAGS.SEQUENCE_PERFORMANCE_MINIMUM_TIME.key
+    ] || 5;
+
+  if (durationInSeconds > MINIMUM_TIME_LIMIT_IN_SECONDS) {
+    const RESULTS = {
+      actionPerformanceArray,
+      duration: durationInSeconds,
+      email,
+      sequenceName,
+    };
+
+    console.log('RESULTS', RESULTS);
+  }
 };
