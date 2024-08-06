0979326c1aa89b72f7d6ec1d6e434324253547d9
Nechama Krigsman <nkrigsman@flexion.us>
2024-08-06 13:00:33 -0400

    10339: display counsel information for private practitioner on petition review screen;

--- web-client/src/views/CaseDetail/AddressDisplay.tsx
+++ web-client/src/views/CaseDetail/AddressDisplay.tsx
@@ -1,4 +1,5 @@
 import { Button } from '../../ustc-ui/Button/Button';
+import { COUNTRY_TYPES } from '@shared/business/entities/EntityConstants';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { connect } from '@web-client/presenter/shared.cerebral';
 import { sequences } from '@web-client/presenter/app.cerebral';
@@ -7,7 +8,6 @@ import React from 'react';
 import classNames from 'classnames';
 
 const addessDisplayDeps = {
-  constants: state.constants,
   formattedCaseDetail: state.formattedCaseDetail,
   openSealAddressModalSequence: sequences.openSealAddressModalSequence,
 };
@@ -26,7 +26,6 @@ export const AddressDisplay = connect<
   addessDisplayDeps,
   function AddressDisplay({
     boldName,
-    constants,
     contact,
     nameOverride,
     noMargin,
@@ -34,70 +33,6 @@ export const AddressDisplay = connect<
     showEmail,
     showSealAddressLink,
   }) {
-    const contactDetails = () => (
-      <p
-        className={classNames(
-          'no-margin',
-          contact.isAddressSealed && 'sealed-address',
-        )}
-        data-testid="contact-address-information"
-      >
-        <span className="address-line" data-testid="address1-line">
-          {contact.address1}
-        </span>
-        <span className="address-line">{contact.address2}</span>
-        <span className="address-line">{contact.address3}</span>
-        <span className="address-line">
-          {contact.city && `${contact.city}, `}
-          {contact.state} {contact.postalCode}
-        </span>
-        {contact.countryType === constants.COUNTRY_TYPES.INTERNATIONAL && (
-          <span className="address-line" data-testid="contact-country-line">
-            {contact.country}
-          </span>
-        )}
-        {contact.phone && (
-          <span
-            className={classNames(
-              noMargin ? 'no-margin' : 'margin-top-1',
-              'address-line',
-            )}
-            data-testid="contact-info-phone-number"
-          >
-            {contact.phone}
-          </span>
-        )}
-        {contact.email && showEmail && (
-          <span className="address-line">
-            <span data-testid="contact-info-email">{contact.email}</span>
-            {contact.showEAccessFlag && (
-              <FontAwesomeIcon
-                aria-label="has e-access"
-                className="margin-left-05 fa-icon-blue"
-                icon="flag"
-                size="1x"
-              />
-            )}
-          </span>
-        )}
-        {showSealAddressLink && !contact.isAddressSealed && (
-          <span className="sealed-address">
-            <Button
-              link
-              className="red-warning"
-              icon="lock"
-              iconColor="red"
-              onClick={() =>
-                openSealAddressModalSequence({ contactToSeal: contact })
-              }
-            >
-              Seal Address
-            </Button>
-          </span>
-        )}
-      </p>
-    );
-
     return (
       <div className={classNames(contact.isAddressSealed && 'margin-left-205')}>
         <div className="margin-top-0 position-relative">
@@ -146,7 +81,17 @@ export const AddressDisplay = connect<
               ),
           )}
         </div>
-        <div>{!contact.sealedAndUnavailable && contactDetails()}</div>
+        <div>
+          {!contact.sealedAndUnavailable && (
+            <ContactDetails
+              contact={contact}
+              noMargin={noMargin}
+              openSealAddressModalSequence={openSealAddressModalSequence}
+              showEmail={showEmail}
+              showSealAddressLink={showSealAddressLink}
+            />
+          )}
+        </div>
         {contact.sealedAndUnavailable && (
           <div className="sealed-address">Address sealed</div>
         )}
@@ -155,4 +100,75 @@ export const AddressDisplay = connect<
   },
 );
 
+function ContactDetails({
+  contact,
+  noMargin,
+  openSealAddressModalSequence,
+  showEmail,
+  showSealAddressLink,
+}) {
+  return (
+    <p
+      className={classNames(
+        'no-margin',
+        contact.isAddressSealed && 'sealed-address',
+      )}
+      data-testid="contact-address-information"
+    >
+      <span className="address-line" data-testid="address1-line">
+        {contact.address1}
+      </span>
+      <span className="address-line">{contact.address2}</span>
+      <span className="address-line">{contact.address3}</span>
+      <span className="address-line">
+        {contact.city && `${contact.city}, `}
+        {contact.state} {contact.postalCode}
+      </span>
+      {contact.countryType === COUNTRY_TYPES.INTERNATIONAL && (
+        <span className="address-line" data-testid="contact-country-line">
+          {contact.country}
+        </span>
+      )}
+      {contact.phone && (
+        <span
+          className={classNames(
+            noMargin ? 'no-margin' : 'margin-top-1',
+            'address-line',
+          )}
+          data-testid="contact-info-phone-number"
+        >
+          {contact.phone}
+        </span>
+      )}
+      {contact.email && showEmail && (
+        <span className="address-line">
+          <span data-testid="contact-info-email">{contact.email}</span>
+          {contact.showEAccessFlag && (
+            <FontAwesomeIcon
+              aria-label="has e-access"
+              className="margin-left-05 fa-icon-blue"
+              icon="flag"
+              size="1x"
+            />
+          )}
+        </span>
+      )}
+      {showSealAddressLink && !contact.isAddressSealed && (
+        <span className="sealed-address">
+          <Button
+            link
+            className="red-warning"
+            icon="lock"
+            iconColor="red"
+            onClick={() =>
+              openSealAddressModalSequence({ contactToSeal: contact })
+            }
+          >
+            Seal Address
+          </Button>
+        </span>
+      )}
+    </p>
+  );
+}
 AddressDisplay.displayName = 'AddressDisplay';
--- web-client/src/views/StartCaseUpdated/CardHeader.tsx
+++ web-client/src/views/StartCaseUpdated/CardHeader.tsx
@@ -5,35 +5,45 @@ import { sequences } from '@web-client/presenter/app.cerebral';
 import React from 'react';
 
 const props = cerebralProps as unknown as {
-  step: number;
+  showEditButton?: boolean;
+  step?: number;
   title: string;
 };
 
 export const CardHeader = connect(
   {
+    showEditButton: props.showEditButton,
     step: props.step,
     title: props.title,
     updateStepIndicatorSequence: sequences.updateStepIndicatorSequence,
   },
-  function CardHeader({ step, title, updateStepIndicatorSequence }) {
+  function CardHeader({
+    showEditButton = true,
+    step,
+    title,
+    updateStepIndicatorSequence,
+  }) {
     return (
       <h3 className="create-petition-review-step-title">
         <div>
-          {step}. {title}
+          {step && <span>{step}. </span>}
+          {title}
         </div>
-        <div>
-          <Button
-            link
-            className="margin-left-2 padding-0"
-            data-testid={`edit-petition-section-button-${step}`}
-            icon="edit"
-            onClick={() => {
-              updateStepIndicatorSequence({ step });
-            }}
-          >
-            Edit
-          </Button>
-        </div>
+        {showEditButton && (
+          <div>
+            <Button
+              link
+              className="margin-left-2 padding-0"
+              data-testid={`edit-petition-section-button-${step}`}
+              icon="edit"
+              onClick={() => {
+                updateStepIndicatorSequence({ step });
+              }}
+            >
+              Edit
+            </Button>
+          </div>
+        )}
       </h3>
     );
   },
