f94024cf27e3e1d04fce5e969e760a18dbe90733
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-08-07 13:49:57 -0500

    10441-story: Rename run once script;

--- scripts/run-once-scripts/promote-special-trial-judge.ts
+++ scripts/run-once-scripts/promote-special-trial-judge.ts
@@ -6,9 +6,7 @@ import {
   requireEnvVars,
 } from '../../shared/admin-tools/util';
 import { User } from '../../shared/src/business/entities/User';
-
 requireEnvVars(['ENV']);
-
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
   // 'stjudge.landy@ustaxcourt.gov'; // OLD
@@ -31,16 +29,18 @@ requireEnvVars(['ENV']);
       email: oldEmail,
     });
 
-  const dynamoLandyUserInfo = await applicationContext
+  // simpler than updating only judgeTitle and email fields in dynamo
+  const dynamoUser = await applicationContext
     .getPersistenceGateway()
     .getUserById({
       applicationContext,
       userId,
     });
 
-  dynamoLandyUserInfo.email = newEmail;
-  dynamoLandyUserInfo.judgeTitle = 'Judge';
-  const rawUser = new User(dynamoLandyUserInfo).validate().toRawObject();
+  dynamoUser.email = newEmail;
+  dynamoUser.judgeTitle = 'Judge';
+
+  const rawUser = new User(dynamoUser).validate().toRawObject();
 
   await applicationContext.getUserGateway().updateUser(applicationContext, {
     attributesToUpdate: {
