7da2c7438549246d2eb8e7181978bb521d812bb9
John Cruz <jcruz@flexion.us>
2024-08-08 13:05:07 -0600

    10432: Pass user to interactor;

--- web-api/src/lambdas/system/logUserPerformanceDataLambda.ts
+++ web-api/src/lambdas/system/logUserPerformanceDataLambda.ts
@@ -1,10 +1,18 @@
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '@web-api/genericHandler';
 
-export const logUserPerformanceDataLambda = event =>
+export const logUserPerformanceDataLambda = (
+  event,
+  authorizedUser: UnknownAuthUser,
+) =>
   genericHandler(event, async ({ applicationContext }) => {
     const { performanceData } = JSON.parse(event.body);
 
     return await applicationContext
       .getUseCases()
-      .logUserPerformanceDataInteractor(applicationContext, performanceData);
+      .logUserPerformanceDataInteractor(
+        applicationContext,
+        performanceData,
+        authorizedUser,
+      );
   });
