40a8b8437cf791a36810efe33f7dc6aef7700174
Nechama Krigsman <nkrigsman@flexion.us>
2024-08-08 16:10:59 -0400

    10339: add FF check for practitioner cypress test;

--- cypress/local-only/tests/integration/caseDetail/docketRecord/electronicFiling/logged-in-user-can-see-un-served-petition-document.cy.ts
+++ cypress/local-only/tests/integration/caseDetail/docketRecord/electronicFiling/logged-in-user-can-see-un-served-petition-document.cy.ts
@@ -3,10 +3,7 @@ import {
   loginAsPetitioner,
   loginAsPrivatePractitioner,
 } from '../../../../../../helpers/authentication/login-as-helpers';
-import {
-  petitionerCreatesElectronicCaseForBusiness,
-  privatePractitionerCreatesElectronicCaseForBusiness,
-} from '../../../../../../helpers/fileAPetition/petitioner-creates-electronic-case';
+import { petitionerCreatesElectronicCaseForBusiness } from '../../../../../../helpers/fileAPetition/petitioner-creates-electronic-case';
 
 describe('Logged In User Can See Un-Served Petition Document', () => {
   it('should display a preview link to ["P", "ATP", "DISC"] documents for the Petitioner that created the case', () => {
@@ -47,7 +44,7 @@ describe('Logged In User Can See Un-Served Petition Document', () => {
 
   it('should display a preview link to ["P", "ATP", "DISC"] documents for the Private Practitioner that created the case', () => {
     loginAsPrivatePractitioner();
-    privatePractitionerCreatesElectronicCaseForBusiness().as('DOCKET_NUMBER');
+    petitionerCreatesElectronicCaseForBusiness().as('DOCKET_NUMBER');
 
     cy.login('privatePractitioner1');
     cy.get<string>('@DOCKET_NUMBER').then(docketNumber => {
