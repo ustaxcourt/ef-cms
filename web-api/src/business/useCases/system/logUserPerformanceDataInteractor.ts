9fc4074a0e28f701fe512242c27e2b773b589e4e
John Cruz <jcruz@flexion.us>
2024-08-09 10:25:01 -0600

    10432: Check user is authorized to log performance data;

--- web-api/src/business/useCases/system/logUserPerformanceDataInteractor.ts
+++ web-api/src/business/useCases/system/logUserPerformanceDataInteractor.ts
@@ -1,4 +1,6 @@
 import { ServerApplicationContext } from '@web-api/applicationContext';
+import { UnauthorizedError } from '@web-api/errors/errors';
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 
 export const logUserPerformanceDataInteractor = async (
   applicationContext: ServerApplicationContext,
@@ -8,7 +10,12 @@ export const logUserPerformanceDataInteractor = async (
     actionPerformanceArray: { actionName: string; duration: number }[];
     email: string;
   },
+  authorizedUser: UnknownAuthUser,
 ): Promise<void> => {
+  if (!authorizedUser || !authorizedUser.userId) {
+    throw new UnauthorizedError('Unauthorized to log performance data');
+  }
+
   await applicationContext
     .getPersistenceGateway()
     .saveSystemPerformanceData({ applicationContext, performanceData });
