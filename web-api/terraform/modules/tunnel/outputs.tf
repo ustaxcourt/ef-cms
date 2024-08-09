3abf6c8c61a66f379a79eaed522f022e8b752d52
Cody Seibert <cseibert113@gmail.com>
2024-08-09 16:02:45 -0400

    adding module security group to rds

--- web-api/terraform/modules/tunnel/outputs.tf
+++ web-api/terraform/modules/tunnel/outputs.tf
@@ -8,3 +8,7 @@ output "instance_private_ip" {
   value = aws_instance.tunnel_instance.private_ip
   description = "Private IP address of the EC2 instance"
 }
+
+output "tunnel_security_group_id" {
+  value = aws_security_group.tunnel_sg.id
+}
