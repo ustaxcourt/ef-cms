0354e3eb7f8850ef7ae1e6db32093bfa54e4661b
Christopher Bisom <cbisom@flexion.us>
2024-08-09 12:56:48 -0400

    10349-bug: rename LOGOUT_BROADCAST_MESSAGES to BROADCAST_MESSAGE; add some new actions for coordinating "DAWSON has been updated" messages across tabs; prevent idle sign out from signing out if DAWSON has been updated; begin adding tests

--- web-client/src/presenter/actions/setLogoutTypeAction.test.ts
+++ web-client/src/presenter/actions/setLogoutTypeAction.test.ts
@@ -1,14 +1,14 @@
-import { LOGOUT_BROADCAST_MESSAGES } from '@shared/business/entities/EntityConstants';
+import { BROADCAST_MESSAGES } from '@shared/business/entities/EntityConstants';
 import { runAction } from '@web-client/presenter/test.cerebral';
 import { setLogoutTypeAction } from './setLogoutTypeAction';
 
 describe('setLogoutTypeAction', () => {
   it('sets state.logoutType when called', async () => {
     const { state } = await runAction(
-      setLogoutTypeAction(LOGOUT_BROADCAST_MESSAGES.userLogout),
+      setLogoutTypeAction(BROADCAST_MESSAGES.userLogout),
       {},
     );
 
-    expect(state.logoutType).toEqual(LOGOUT_BROADCAST_MESSAGES.userLogout);
+    expect(state.logoutType).toEqual(BROADCAST_MESSAGES.userLogout);
   });
 });
