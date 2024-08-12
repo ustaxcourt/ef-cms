61324e398d628426a9a78224c1e78461ba3b35b6
Christopher Bisom <cbisom@flexion.us>
2024-08-12 11:02:00 -0400

    10102-story: get rid of isEmpty check in clearStatusReportOrderFormAction and update test (it causes a bug and is no longer needed since we have since switched to separate create/edit routes)

--- web-client/src/presenter/actions/StatusReportOrder/clearStatusReportOrderFormAction.ts
+++ web-client/src/presenter/actions/StatusReportOrder/clearStatusReportOrderFormAction.ts
@@ -1,22 +1,15 @@
 import { STATUS_REPORT_ORDER_OPTIONS } from '@shared/business/entities/EntityConstants';
-import { isEmpty } from 'lodash';
 import { state } from '@web-client/presenter/app.cerebral';
 
-export const clearStatusReportOrderFormAction = ({
-  get,
-  store,
-}: ActionProps) => {
-  const documentToEdit = get(state.documentToEdit);
-  if (isEmpty(documentToEdit)) {
-    store.unset(state.form.orderType);
-    store.unset(state.form.dueDate);
-    store.unset(state.form.strickenFromTrialSessions);
-    store.unset(state.form.jurisdiction);
-    store.set(state.form.additionalOrderText, '');
-    store.set(state.form.docketEntryDescription, 'Order');
-    store.set(
-      state.form.issueOrder,
-      STATUS_REPORT_ORDER_OPTIONS.issueOrderOptions.allCasesInGroup,
-    );
-  }
+export const clearStatusReportOrderFormAction = ({ store }: ActionProps) => {
+  store.unset(state.form.orderType);
+  store.unset(state.form.dueDate);
+  store.unset(state.form.strickenFromTrialSessions);
+  store.unset(state.form.jurisdiction);
+  store.set(state.form.additionalOrderText, '');
+  store.set(state.form.docketEntryDescription, 'Order');
+  store.set(
+    state.form.issueOrder,
+    STATUS_REPORT_ORDER_OPTIONS.issueOrderOptions.allCasesInGroup,
+  );
 };
