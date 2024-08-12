cfb2268176f820ea360013266e227dfdac7176b7
Christopher Bisom <cbisom@flexion.us>
2024-08-12 11:02:00 -0400

    10102-story: get rid of isEmpty check in clearStatusReportOrderFormAction and update test (it causes a bug and is no longer needed since we have since switched to separate create/edit routes)

--- web-client/src/presenter/actions/StatusReportOrder/clearStatusReportOrderFormAction.test.ts
+++ web-client/src/presenter/actions/StatusReportOrder/clearStatusReportOrderFormAction.test.ts
@@ -3,7 +3,7 @@ import { clearStatusReportOrderFormAction } from './clearStatusReportOrderFormAc
 import { runAction } from '@web-client/presenter/test.cerebral';
 
 describe('clearStatusReportOrderFormAction,', () => {
-  it("should clear form state if there's no document to edit", async () => {
+  it('should clear form state', async () => {
     const result = await runAction(clearStatusReportOrderFormAction, {
       state: {
         form: {
@@ -25,35 +25,5 @@ describe('clearStatusReportOrderFormAction,', () => {
       docketEntryDescription: 'Order',
       issueOrder: STATUS_REPORT_ORDER_OPTIONS.issueOrderOptions.allCasesInGroup,
     });
-  });
-
-  it("should not clear form state if there's a document to edit", async () => {
-    const result = await runAction(clearStatusReportOrderFormAction, {
-      state: {
-        documentToEdit: 'stuff',
-        form: {
-          additionalOrderText: 'Test',
-          docketEntryDescription: 'Order Test',
-          dueDate: '07/04/2024',
-          issueOrder:
-            STATUS_REPORT_ORDER_OPTIONS.issueOrderOptions.allCasesInGroup,
-          jurisdiction:
-            STATUS_REPORT_ORDER_OPTIONS.jurisdictionOptions.retained,
-          orderType: STATUS_REPORT_ORDER_OPTIONS.orderTypeOptions.statusReport,
-          strickenFromTrialSessions: 'true',
-        },
-      },
-    });
-
-    expect(result.state.form).toEqual({
-      additionalOrderText: 'Test',
-      docketEntryDescription: 'Order Test',
-      dueDate: '07/04/2024',
-      issueOrder: STATUS_REPORT_ORDER_OPTIONS.issueOrderOptions.allCasesInGroup,
-      jurisdiction: STATUS_REPORT_ORDER_OPTIONS.jurisdictionOptions.retained,
-      orderType: STATUS_REPORT_ORDER_OPTIONS.orderTypeOptions.statusReport,
-      strickenFromTrialSessions: 'true',
-    });
-    expect(result.state.documentToEdit).toEqual('stuff');
   });
 });
