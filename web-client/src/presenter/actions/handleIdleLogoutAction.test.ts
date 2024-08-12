05581a6567e0284a44d59f670c74ba155ad829fd
Christopher Bisom <cbisom@flexion.us>
2024-08-12 12:18:20 -0400

    10349-bug: fix handleIdleLogoutAction.test.ts

--- web-client/src/presenter/actions/handleIdleLogoutAction.test.ts
+++ web-client/src/presenter/actions/handleIdleLogoutAction.test.ts
@@ -16,7 +16,7 @@ describe('handleIdleLogoutAction', () => {
     };
   });
 
-  it('should stay in the INITIAL state if the user is not logged in', async () => {
+  it('should do nothing when the user is not logged in', async () => {
     const result = await runAction(handleIdleLogoutAction, {
       modules: {
         presenter,
@@ -34,7 +34,7 @@ describe('handleIdleLogoutAction', () => {
           state: IDLE_LOGOUT_STATES.INITIAL,
         },
         lastIdleAction: 0,
-        user: undefined,
+        token: undefined,
       },
     });
 
@@ -42,9 +42,40 @@ describe('handleIdleLogoutAction', () => {
       logoutAt: undefined,
       state: IDLE_LOGOUT_STATES.INITIAL,
     });
+    expect(presenter.providers.path.continue).toHaveBeenCalled();
   });
 
-  it('should stay in the INITIAL state if the user is uploading', async () => {
+  it('should do nothing when the client needs to be refreshed', async () => {
+    const result = await runAction(handleIdleLogoutAction, {
+      modules: {
+        presenter,
+      },
+      state: {
+        clientNeedsToRefresh: true,
+        constants: {
+          SESSION_MODAL_TIMEOUT: 5000,
+          SESSION_TIMEOUT: 10000,
+        },
+        fileUploadProgress: {
+          isUploading: false,
+        },
+        idleLogoutState: {
+          logoutAt: undefined,
+          state: IDLE_LOGOUT_STATES.INITIAL,
+        },
+        lastIdleAction: 0,
+        token: '92c17761-d382-4231-b497-bc8c9e3ffea1',
+      },
+    });
+
+    expect(result.state.idleLogoutState).toEqual({
+      logoutAt: undefined,
+      state: IDLE_LOGOUT_STATES.INITIAL,
+    });
+    expect(presenter.providers.path.continue).toHaveBeenCalled();
+  });
+
+  it('should stay in the INITIAL state when the user is uploading', async () => {
     const result = await runAction(handleIdleLogoutAction, {
       modules: {
         presenter,
@@ -62,7 +93,7 @@ describe('handleIdleLogoutAction', () => {
           state: IDLE_LOGOUT_STATES.INITIAL,
         },
         lastIdleAction: 0,
-        user: {},
+        token: '92c17761-d382-4231-b497-bc8c9e3ffea1',
       },
     });
 
@@ -70,9 +101,10 @@ describe('handleIdleLogoutAction', () => {
       logoutAt: undefined,
       state: IDLE_LOGOUT_STATES.INITIAL,
     });
+    expect(presenter.providers.path.continue).toHaveBeenCalled();
   });
 
-  it('should stay move into the MONITORING state if the user is logged in and currently in initial state', async () => {
+  it('should move into the MONITORING state when the user is logged in and currently in initial state', async () => {
     const result = await runAction(handleIdleLogoutAction, {
       modules: {
         presenter,
@@ -90,7 +122,7 @@ describe('handleIdleLogoutAction', () => {
           state: IDLE_LOGOUT_STATES.INITIAL,
         },
         lastIdleAction: 0,
-        user: {},
+        token: '9dfbf86d-d4e7-41da-a85a-1b57910a5eaa',
       },
     });
 
@@ -98,9 +130,10 @@ describe('handleIdleLogoutAction', () => {
       logoutAt: expect.any(Number),
       state: IDLE_LOGOUT_STATES.MONITORING,
     });
+    expect(presenter.providers.path.continue).toHaveBeenCalled();
   });
 
-  it('should move into the COUNTDOWN if the current time is past the session timeout limits', async () => {
+  it('should move into the COUNTDOWN when the current time is past the session timeout limits', async () => {
     jest.spyOn(Date, 'now').mockReturnValue(11000);
     const result = await runAction(handleIdleLogoutAction, {
       modules: {
@@ -119,7 +152,7 @@ describe('handleIdleLogoutAction', () => {
           state: IDLE_LOGOUT_STATES.MONITORING,
         },
         lastIdleAction: 0,
-        user: {},
+        token: '0e4d3b74-89bc-44a0-a9b9-59f5eece40a5',
       },
     });
 
@@ -127,9 +160,10 @@ describe('handleIdleLogoutAction', () => {
       logoutAt: expect.any(Number),
       state: IDLE_LOGOUT_STATES.COUNTDOWN,
     });
+    expect(presenter.providers.path.continue).toHaveBeenCalled();
   });
 
-  it('should logout if in COUNTDOWN and the total time has elapsed the total elapsed time', async () => {
+  it('should logout when in COUNTDOWN and the total time has elapsed the total elapsed time', async () => {
     jest.spyOn(Date, 'now').mockReturnValue(16000);
     const result = await runAction(handleIdleLogoutAction, {
       modules: {
@@ -148,7 +182,7 @@ describe('handleIdleLogoutAction', () => {
           state: IDLE_LOGOUT_STATES.COUNTDOWN,
         },
         lastIdleAction: 0,
-        user: {},
+        token: 'd53d2132-860e-41a0-9f42-f73c7285721b',
       },
     });
 
