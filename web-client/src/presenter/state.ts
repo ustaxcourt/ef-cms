182ec19419e2616a471dd9ff97cf2a80cac986b8
Christopher Bisom <cbisom@flexion.us>
2024-08-15 09:07:09 -0400

    10102-story: update filingDate to be string

--- web-client/src/presenter/state.ts
+++ web-client/src/presenter/state.ts
@@ -872,6 +872,6 @@ export type CreateCaseIrsForm = {
 export type ViewerDocument = {
   docketEntryId: string;
   documentTitle?: string; // Should this be required?
-  filingDate?: Date;
+  filingDate?: string;
   index?: number;
 };
