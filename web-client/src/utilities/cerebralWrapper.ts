17bb3f43f442061da17ea8eada0ed629691ad8d8
Javis Sullivan <jsullivan@flexion.us>
2024-08-15 16:49:38 -0400

    add state to wrapper export

--- web-client/src/utilities/cerebralWrapper.ts
+++ web-client/src/utilities/cerebralWrapper.ts
@@ -1,6 +1,6 @@
-import { Get, parallel, props, sequences } from 'cerebral';
+import { Get, parallel, props, sequences, state } from 'cerebral';
 import App from 'cerebral';
 
 // eslint-disable-next-line import/no-default-export
 export default App;
-export { parallel, props, sequences, Get };
+export { parallel, props, sequences, state, Get };
