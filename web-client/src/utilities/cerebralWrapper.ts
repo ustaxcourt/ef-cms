3a354221612dec30535bfff9dc0a8250530051e1
Javis Sullivan <jsullivan@flexion.us>
2024-08-14 21:31:26 -0400

    use humble object wrapper for core cerebral imports

---
+++ web-client/src/utilities/cerebralWrapper.ts
@@ -0,0 +1,6 @@
+import { Get, parallel, props, sequences } from 'cerebral';
+import App from 'cerebral';
+
+// eslint-disable-next-line import/no-default-export
+export default App;
+export { parallel, props, sequences, Get };
