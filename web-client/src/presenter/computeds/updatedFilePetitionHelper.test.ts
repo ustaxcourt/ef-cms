2320d9e7be4fba035fc83c6b9849042488ce6ddd
Nechama Krigsman <nkrigsman@flexion.us>
2024-08-16 11:25:18 -0400

    10339: fix type error;

--- web-client/src/presenter/computeds/updatedFilePetitionHelper.test.ts
+++ web-client/src/presenter/computeds/updatedFilePetitionHelper.test.ts
@@ -99,7 +99,6 @@ describe('updatedFilePetitionHelper', () => {
       ]);
     });
     it('should return filing options for practitioner', () => {
-      user = privatePractitionerUser;
       const result = runCompute(updatedFilePetitionHelper, {
         state: { form: {}, user: privatePractitionerUser },
       });
