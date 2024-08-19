90b37d15c53604285bb322a4ea16a8aef360a5ec
John Cruz <jcruz@flexion.us>
2024-08-19 10:28:39 -0600

    10432: Check that the user is logged in;

--- web-client/src/presenter/actions/performanceMeasurementEndAction.ts
+++ web-client/src/presenter/actions/performanceMeasurementEndAction.ts
@@ -1,17 +1,25 @@
 import { DateTime } from 'luxon';
+import { state } from '@web-client/presenter/app.cerebral';
 
 export const performanceMeasurementEndAction = ({
   applicationContext,
+  get,
   props,
 }: ActionProps<{
   sequenceName?: string;
   performanceMeasurementStart?: number;
   actionPerformanceArray?: { actionName: string; duration: number }[];
 }>) => {
+  const userToken = get(state.token);
   const { actionPerformanceArray, performanceMeasurementStart, sequenceName } =
     props;
 
-  if (!sequenceName || !performanceMeasurementStart || !actionPerformanceArray)
+  if (
+    !sequenceName ||
+    !performanceMeasurementStart ||
+    !actionPerformanceArray ||
+    !userToken
+  )
     return;
 
   const performanceMeasurementEnd = DateTime.now().toMillis();
