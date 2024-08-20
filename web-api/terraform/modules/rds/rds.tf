3739f30a85e53590cfddd886ece76010c9e78fd5
Cody Seibert <cseibert113@gmail.com>
2024-08-20 16:55:55 -0400

    10391 - working on the terraform

--- web-api/terraform/modules/rds/rds.tf
+++ web-api/terraform/modules/rds/rds.tf
@@ -1,28 +1,18 @@
-# resource "aws_db_instance" "postgres" {
-#   allocated_storage    = 20
-#   engine               = "postgres"
-#   engine_version       = "16.3"
-#   instance_class       = var.instance_size
-#   db_name              = "${var.environment}_dawson"
-#   username             = var.postgres_user
-#   password             = var.postgres_password
-#   parameter_group_name = aws_db_parameter_group.postgres.name
-#   vpc_security_group_ids = [aws_security_group.postgres.id]
-#   db_subnet_group_name   = var.subnet_group_name
-#   skip_final_snapshot = true
-#   publicly_accessible = false
-#   apply_immediately = true
-# }
+resource "aws_rds_global_cluster" "global_cluster" {
+  global_cluster_identifier = "${var.environment}-global-dawson-cluster"
+  engine                    = "aurora-postgresql"
+}
 
 resource "aws_rds_cluster" "postgres" {
   cluster_identifier = "${var.environment}-dawson-cluster"
   engine             = "aurora-postgresql"
   engine_mode        = "provisioned"
-  engine_version     = "15.4"
+  engine_version     = var.engine_version
   database_name      = "${var.environment}_dawson"
   master_username    = var.postgres_user
   master_password    = var.postgres_password
   storage_encrypted  = true
+  global_cluster_identifier = aws_rds_global_cluster.global_cluster.id
 
   serverlessv2_scaling_configuration {
     max_capacity = 1.0
@@ -37,34 +27,20 @@ resource "aws_rds_cluster_instance" "cluster_instance" {
   engine_version     = aws_rds_cluster.postgres.engine_version
 }
 
-# resource "aws_db_parameter_group" "postgres" {
-#   name   = "postgres"
-#   family = "postgres16"
+resource "aws_rds_cluster" "west_replica" {
+  provider               = aws.us-west-1
+  cluster_identifier     = "${var.environment}-dawson-replica"
+  engine                 = "aurora-postgresql"
+  engine_mode            = "provisioned"
+  engine_version         = var.engine_version
+  storage_encrypted      = true
+  global_cluster_identifier = aws_rds_global_cluster.global_cluster.id
+}
 
-#   parameter {
-#     name  = "log_connections"
-#     value = "1"
-#   }
-# }
-
-# resource "aws_security_group" "postgres" {
-#   vpc_id = var.vpc_id
-
-#   ingress {
-#     from_port   = 5432
-#     to_port     = 5432
-#     protocol    = "tcp"
-#     security_groups = var.security_group_ids
-#   }
-
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   tags = {
-#     Name = "db-security-group"
-#   }
-# }
+resource "aws_rds_cluster_instance" "west_replica_instance" {
+  provider               = aws.us-west-1
+  cluster_identifier     = aws_rds_cluster.west_replica.id
+  instance_class         = "db.serverless"
+  engine                 = aws_rds_cluster.west_replica.engine
+  engine_version         = aws_rds_cluster.west_replica.engine_version
+}
