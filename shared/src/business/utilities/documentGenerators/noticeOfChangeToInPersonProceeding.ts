a45d031324ec2b7b949ccf510036592222b497d8
Zachary Rogers <zrogers>
2024-08-20 10:30:56 -0700

    10417: Fix type errors. Remove unused parameter from sendNotificationOfSealing

--- shared/src/business/utilities/documentGenerators/noticeOfChangeToInPersonProceeding.ts
+++ shared/src/business/utilities/documentGenerators/noticeOfChangeToInPersonProceeding.ts
@@ -1,5 +1,5 @@
 import { DateServedFooter } from '@shared/business/utilities/pdfGenerator/components/DateServedFooter';
-import { FormattedTrialInfo } from '@web-api/business/useCases/trialSessions/generateNoticeOfChangeOfTrialJudgeInteractor';
+import { FormattedTrialInfoType } from '@web-api/business/useCases/trialSessions/generateNoticeOfTrialIssuedInteractor';
 import { NoticeOfChangeToInPersonProceeding } from '@shared/business/utilities/pdfGenerator/documentTemplates/NoticeOfChangeToInPersonProceeding';
 import { ServerApplicationContext } from '@web-api/applicationContext';
 import { generateHTMLTemplateForPDF } from '../generateHTMLTemplateForPDF/generateHTMLTemplateForPDF';
@@ -17,9 +17,9 @@ export const noticeOfChangeToInPersonProceeding = async ({
     docketNumberWithSuffix: string;
     nameOfClerk: string;
     titleOfClerk: string;
-    trialInfo: FormattedTrialInfo;
+    trialInfo: FormattedTrialInfoType;
   };
-}): Promise<Buffer> => {
+}): Promise<Uint8Array> => {
   const noticeOfChangeToInPersonProceedingTemplate = ReactDOM.renderToString(
     React.createElement(NoticeOfChangeToInPersonProceeding, data),
   );
