57accafce26d2af80fe766bfed482e9b00ab7c89
John Cruz <jcruz@flexion.us>
2024-08-21 14:26:49 -0600

    10437: Return automatic blocked reasons in helper;

--- web-client/src/presenter/computeds/selectCriteriaHelper.ts
+++ web-client/src/presenter/computeds/selectCriteriaHelper.ts
@@ -1,13 +1,23 @@
 import {
+  AUTOMATIC_BLOCKED_REASONS,
+  AutomaticBlockedReasons,
   CASE_STATUS_TYPES,
   CaseStatus,
 } from '@shared/business/entities/EntityConstants';
 
 type SelectCriteriaHelperResults = {
+  automaticBlockedReasons: { key: string; value: AutomaticBlockedReasons }[];
   caseStatuses: { key: string; value: CaseStatus }[];
 };
 
 export const selectCriteriaHelperInternal = (): SelectCriteriaHelperResults => {
+  const automaticBlockedReasons = Object.entries(AUTOMATIC_BLOCKED_REASONS).map(
+    ([key, value]) => ({
+      key,
+      value,
+    }),
+  );
+
   const caseStatuses: { key: string; value: CaseStatus }[] = Object.entries(
     CASE_STATUS_TYPES,
   ).map(([key, value]) => ({
@@ -16,6 +26,7 @@ export const selectCriteriaHelperInternal = (): SelectCriteriaHelperResults => {
   }));
 
   return {
+    automaticBlockedReasons,
     caseStatuses,
   };
 };
