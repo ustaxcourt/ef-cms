e2bea485859d48d889bee095a883a2e31debf265
John Cruz <jcruz@flexion.us>
2024-08-22 08:43:47 -0600

    10437: Add "Manual Block" for reason filter;

--- web-client/src/presenter/computeds/selectCriteriaHelper.ts
+++ web-client/src/presenter/computeds/selectCriteriaHelper.ts
@@ -6,17 +6,26 @@ import {
 } from '@shared/business/entities/EntityConstants';
 
 type SelectCriteriaHelperResults = {
-  automaticBlockedReasons: { key: string; value: AutomaticBlockedReasons }[];
+  automaticBlockedReasons: {
+    key: string;
+    value: AutomaticBlockedReasons | 'Manual Block';
+  }[];
   caseStatuses: { key: string; value: CaseStatus }[];
 };
 
 export const selectCriteriaHelperInternal = (): SelectCriteriaHelperResults => {
-  const automaticBlockedReasons = Object.entries(AUTOMATIC_BLOCKED_REASONS).map(
-    ([key, value]) => ({
-      key,
-      value,
-    }),
-  );
+  const automaticBlockedReasons: {
+    key: string;
+    value: AutomaticBlockedReasons | 'Manual Block';
+  }[] = Object.entries(AUTOMATIC_BLOCKED_REASONS).map(([key, value]) => ({
+    key,
+    value,
+  }));
+
+  automaticBlockedReasons.push({
+    key: 'manualBlock',
+    value: 'Manual Block',
+  });
 
   const caseStatuses: { key: string; value: CaseStatus }[] = Object.entries(
     CASE_STATUS_TYPES,
@@ -25,9 +34,15 @@ export const selectCriteriaHelperInternal = (): SelectCriteriaHelperResults => {
     value,
   }));
 
+  const sortByLabel = (a, b) => {
+    if (a.value < b.value) return -1;
+    if (a.value > b.value) return 1;
+    return 0;
+  };
+
   return {
-    automaticBlockedReasons,
-    caseStatuses,
+    automaticBlockedReasons: automaticBlockedReasons.sort(sortByLabel),
+    caseStatuses: caseStatuses.sort(sortByLabel),
   };
 };
 
