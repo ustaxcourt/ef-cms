6785e5a6c201eac84f1d8e13847632a1990f8b40
Nechama Krigsman <nkrigsman@flexion.us>
2024-08-22 14:50:39 -0400

    10339: add pdf snapshot test for generated petition;

--- web-client/src/presenter/state.ts
+++ web-client/src/presenter/state.ts
@@ -866,7 +866,7 @@ export type CreateCaseIrsForm = {
   size?: number;
   caseType?: string;
   noticeIssuedDate?: string;
-  taxYear?: number;
+  taxYear?: string;
   irsNoticeFileUrl?: string;
   cityAndStateIssuingOffice?: string;
 };
