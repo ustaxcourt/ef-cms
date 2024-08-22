034cbae772027c05bfe6dd2d2c7d41126f775590
Zachary Rogers <zrogers>
2024-08-22 10:46:48 -0700

    10437: Add reset of filters when trial location changes

--- web-client/src/presenter/sequences/getBlockedCasesByTrialLocationSequence.ts
+++ web-client/src/presenter/sequences/getBlockedCasesByTrialLocationSequence.ts
@@ -1,10 +1,12 @@
 import { getBlockedCasesByTrialLocationAction } from '../actions/CaseDetail/getBlockedCasesByTrialLocationAction';
+import { resetBlockedCasesFiltersAction } from '@web-client/presenter/actions/Reports/BlockedCaseReport/resetBlockedCasesFiltersAction';
 import { setBlockedCasesAction } from '../actions/CaseDetail/setBlockedCasesAction';
 import { setFormValueAction } from '../actions/setFormValueAction';
 import { showProgressSequenceDecorator } from '../utilities/showProgressSequenceDecorator';
 
 export const getBlockedCasesByTrialLocationSequence =
   showProgressSequenceDecorator([
+    resetBlockedCasesFiltersAction,
     setFormValueAction,
     getBlockedCasesByTrialLocationAction,
     setBlockedCasesAction,
