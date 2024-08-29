0c89d2aa600ef58353d88ed7db8f018d58338ff4
Kaitlyn Swann <kswann@flexion.us>
2024-08-29 07:17:12 -0600

    10391: update docs + work on fire drill

--- web-api/terraform/applyables/allColors/allColors.tf
+++ web-api/terraform/applyables/allColors/allColors.tf
@@ -202,9 +202,26 @@ module "rds" {
   environment       = var.environment
   postgres_user     = var.postgres_user
   postgres_password = var.postgres_password
-  
+  postgres_snapshot = ""
+  delete_protection = true
+
   providers = {
     aws           = aws.us-east-1
     aws.us-west-1 = aws.us-west-1
   }
 }
+
+# Used to add additional clusters while keeping the current cluster active
+# module "rds-cluster" {
+#   source            = "../../modules/rds"
+#   environment       = var.environment
+#   postgres_user     = var.postgres_user
+#   postgres_password = var.postgres_password
+#   postgres_postfix  = "-1"
+#   postgres_snapshot = "snapshot_identifier"
+
+#   providers = {
+#     aws           = aws.us-east-1
+#     aws.us-west-1 = aws.us-west-1
+#   }
+# }
--- web-api/terraform/modules/rds/rds.tf
+++ web-api/terraform/modules/rds/rds.tf
@@ -1,7 +1,7 @@
 # Define a KMS key in the primary region (e.g., us-east-1)
 resource "aws_kms_key" "primary" {
-  description = "Primary KMS key for Aurora RDS encryption"
-  key_usage   = "ENCRYPT_DECRYPT"
+  description         = "Primary KMS key for Aurora RDS encryption"
+  key_usage           = "ENCRYPT_DECRYPT"
   enable_key_rotation = true
 
   tags = {
@@ -11,9 +11,9 @@ resource "aws_kms_key" "primary" {
 
 # Define a KMS key in the replica region (e.g., us-west-1)
 resource "aws_kms_key" "replica" {
-  provider = aws.us-west-1
-  description = "Replica KMS key for Aurora RDS encryption"
-  key_usage   = "ENCRYPT_DECRYPT"
+  provider            = aws.us-west-1
+  description         = "Replica KMS key for Aurora RDS encryption"
+  key_usage           = "ENCRYPT_DECRYPT"
   enable_key_rotation = true
 
   tags = {
@@ -22,44 +22,45 @@ resource "aws_kms_key" "replica" {
 }
 
 resource "aws_rds_global_cluster" "global_cluster" {
-  global_cluster_identifier = "${var.environment}-dawson-global"
+  global_cluster_identifier = "${var.environment}-dawson-global${var.postgres_postfix}"
   engine                    = "aurora-postgresql"
-  storage_encrypted = true
-  deletion_protection = true
+  storage_encrypted         = true
+  deletion_protection       = var.delete_protection
   lifecycle {
     prevent_destroy = true
   }
 }
 
 resource "aws_rds_cluster" "postgres" {
-  cluster_identifier         = "${var.environment}-dawson-cluster"
-  engine                     = "aurora-postgresql"
-  engine_mode                = "provisioned"
-  engine_version             = var.engine_version
-  deletion_protection = true
-  database_name              = "${var.environment}_dawson"
-  master_username            = var.postgres_user
-  master_password            = var.postgres_password
-  storage_encrypted          = true
-  global_cluster_identifier  = aws_rds_global_cluster.global_cluster.id
+  cluster_identifier                  = "${var.environment}-dawson-cluster"
+  engine                              = "aurora-postgresql"
+  engine_mode                         = "provisioned"
+  engine_version                      = var.engine_version
+  deletion_protection                 = var.delete_protection
+  database_name                       = "${var.environment}_dawson"
+  master_username                     = var.postgres_user
+  master_password                     = var.postgres_password
+  storage_encrypted                   = true
+  global_cluster_identifier           = aws_rds_global_cluster.global_cluster.id
   iam_database_authentication_enabled = true
-  kms_key_id                 = aws_kms_key.primary.arn
+  kms_key_id                          = aws_kms_key.primary.arn
+  #snapshot_identifier                 = var.postgres_snapshot
 
   serverlessv2_scaling_configuration {
     max_capacity = 1.0
     min_capacity = 0.5
   }
-  
+
   lifecycle {
     prevent_destroy = true
   }
 }
 
 resource "aws_rds_cluster_instance" "cluster_instance" {
-  cluster_identifier = aws_rds_cluster.postgres.id
-  instance_class     = "db.serverless"
-  engine             = aws_rds_cluster.postgres.engine
-  engine_version     = aws_rds_cluster.postgres.engine_version
+  cluster_identifier  = aws_rds_cluster.postgres.id
+  instance_class      = "db.serverless"
+  engine              = aws_rds_cluster.postgres.engine
+  engine_version      = aws_rds_cluster.postgres.engine_version
   publicly_accessible = true
 
   lifecycle {
@@ -68,18 +69,18 @@ resource "aws_rds_cluster_instance" "cluster_instance" {
 }
 
 resource "aws_rds_cluster" "west_replica" {
-  provider                   = aws.us-west-1
-  cluster_identifier         = "${var.environment}-dawson-replica"
-  engine                     = "aurora-postgresql"
-  engine_mode                = "provisioned"
-  engine_version             = var.engine_version
-  deletion_protection = true
-  storage_encrypted          = true
-  global_cluster_identifier  = aws_rds_global_cluster.global_cluster.id
+  provider                            = aws.us-west-1
+  cluster_identifier                  = "${var.environment}-dawson-replica"
+  engine                              = "aurora-postgresql"
+  engine_mode                         = "provisioned"
+  engine_version                      = var.engine_version
+  deletion_protection                 = var.delete_protection
+  storage_encrypted                   = true
+  global_cluster_identifier           = aws_rds_global_cluster.global_cluster.id
   iam_database_authentication_enabled = true
-  kms_key_id                 = aws_kms_key.replica.arn
+  kms_key_id                          = aws_kms_key.replica.arn
 
-  depends_on = [ aws_rds_cluster.postgres ]
+  depends_on = [aws_rds_cluster.postgres]
 
   serverlessv2_scaling_configuration {
     max_capacity = 1.0
@@ -92,11 +93,11 @@ resource "aws_rds_cluster" "west_replica" {
 }
 
 resource "aws_rds_cluster_instance" "west_replica_instance" {
-  provider               = aws.us-west-1
-  cluster_identifier     = aws_rds_cluster.west_replica.id
-  instance_class         = "db.serverless"
-  engine                 = aws_rds_cluster.west_replica.engine
-  engine_version         = aws_rds_cluster.west_replica.engine_version
+  provider            = aws.us-west-1
+  cluster_identifier  = aws_rds_cluster.west_replica.id
+  instance_class      = "db.serverless"
+  engine              = aws_rds_cluster.west_replica.engine
+  engine_version      = aws_rds_cluster.west_replica.engine_version
   publicly_accessible = true
 
   lifecycle {
