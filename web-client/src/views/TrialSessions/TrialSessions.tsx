c1ed4e35ec17de97d9b2e368e31c6e41eb46ae5d
Zachary Rogers <zrogers>
2024-08-30 10:57:41 -0700

    10409: Add new styling for trial sessions. Extract filters to its own component separate from the table

--- web-client/src/views/TrialSessions/TrialSessions.tsx
+++ web-client/src/views/TrialSessions/TrialSessions.tsx
@@ -1,8 +1,14 @@
 import { BigHeader } from '../BigHeader';
+import { BindedSelect } from '@web-client/ustc-ui/BindedSelect/BindedSelect';
 import { Button } from '../../ustc-ui/Button/Button';
 import { ErrorNotification } from '../ErrorNotification';
+import {
+  SESSION_TYPES,
+  TRIAL_SESSION_PROCEEDING_TYPES,
+} from '@shared/business/entities/EntityConstants';
 import { SuccessNotification } from '../SuccessNotification';
 import { Tab, Tabs } from '../../ustc-ui/Tabs/Tabs';
+import { TrialCityOptions } from '@web-client/views/TrialCityOptions';
 import { TrialSessionsTable } from './TrialSessionsTable';
 import { connect } from '@web-client/presenter/shared.cerebral';
 import { sequences } from '@web-client/presenter/app.cerebral';
@@ -14,12 +20,12 @@ export const TrialSessions = connect(
     defaultTab: state.screenMetadata.trialSessionFilters.status,
     openTrialSessionPlanningModalSequence:
       sequences.openTrialSessionPlanningModalSequence,
-    showNewTrialSession: state.trialSessionsHelper.showNewTrialSession,
+    trialSessionHelper: state.trialSessionsHelper,
   },
   function TrialSessions({
     defaultTab,
     openTrialSessionPlanningModalSequence,
-    showNewTrialSession,
+    trialSessionHelper,
   }) {
     return (
       <>
@@ -30,7 +36,7 @@ export const TrialSessions = connect(
 
           <Tabs
             bind="currentViewMetadata.trialSessions.tab"
-            defaultActiveTab={defaultTab || 'open'}
+            defaultActiveTab={defaultTab || 'calendared'}
             headingLevel="2"
             id="trial-sessions-tabs"
           >
@@ -44,48 +50,24 @@ export const TrialSessions = connect(
                 Trial Session Planning Report
               </Button>
             </div>
-            {showNewTrialSession && (
-              <Button
-                className="tab-right-button"
-                data-testid="add-trial-session-button"
-                href="/add-a-trial-session"
-                icon="plus-circle"
-              >
-                Add Trial Session
-              </Button>
-            )}
-            {showNewTrialSession && (
+            {trialSessionHelper.showNewTrialSession && (
               <Tab
                 data-testid="new-trial-sessions-tab"
                 id="new-trial-sessions-tab"
                 tabName="new"
                 title="New"
               >
+                <TrialSessionFilters />
                 <TrialSessionsTable filter="New" />
               </Tab>
             )}
             <Tab
-              data-testid="open-trial-sessions-tab"
-              id="open-trial-sessions-tab"
-              tabName="open"
-              title="Open"
+              data-testid="calendared-trial-sessions-tab"
+              id="calendared-trial-sessions-tab"
+              tabName="calendared"
+              title="Calendared"
             >
-              <TrialSessionsTable filter="Open" />
-            </Tab>
-            <Tab
-              data-testid="closed-trial-sessions-tab"
-              id="closed-trial-sessions-tab"
-              tabName="closed"
-              title="Closed"
-            >
-              <TrialSessionsTable filter="Closed" />
-            </Tab>
-            <Tab
-              data-testid="all-trial-sessions-tab"
-              id="all-trial-sessions-tab"
-              tabName="all"
-              title="All"
-            >
+              <TrialSessionFilters />
               <TrialSessionsTable filter="All" />
             </Tab>
           </Tabs>
@@ -94,5 +76,149 @@ export const TrialSessions = connect(
     );
   },
 );
+
+const TrialSessionFilters = connect(
+  { trialSessionsHelper: state.trialSessionsHelper },
+  function TrialSessionFilters({ trialSessionsHelper }) {
+    return (
+      <>
+        <div className="margin-bottom-4">
+          <div className="usa-radio usa-radio__inline">
+            <legend>Session Status</legend>
+            <input
+              aria-describedby="petition-filing-method-radios"
+              checked={false}
+              className="usa-radio__input"
+              id="petitionFilingMethod-electronic"
+              name="filingMethod"
+              type="radio"
+              onChange={() => {}}
+            />
+            <label
+              className="usa-radio__label"
+              htmlFor="petitionFilingMethod-electronic"
+            >
+              All
+            </label>
+            <input
+              aria-describedby="petition-filing-method-radios"
+              checked={false}
+              className="usa-radio__input"
+              id="petitionFilingMethod-electronic"
+              name="filingMethod"
+              type="radio"
+              onChange={() => {}}
+            />
+            <label
+              className="usa-radio__label"
+              htmlFor="petitionFilingMethod-electronic"
+            >
+              Open
+            </label>
+            <input
+              aria-describedby="petition-filing-method-radios"
+              checked={false}
+              className="usa-radio__input"
+              id="petitionFilingMethod-electronic"
+              name="filingMethod"
+              type="radio"
+              onChange={() => {}}
+            />
+            <label
+              className="usa-radio__label"
+              htmlFor="petitionFilingMethod-electronic"
+            >
+              Closed
+            </label>
+          </div>
+        </div>
+        <div className="margin-bottom-5 grid-row flex-justify">
+          <div>
+            <label
+              className="dropdown-label-serif margin-right-3"
+              htmlFor="inline-select"
+              id="trial-sessions-filter-label"
+            >
+              Filter by
+            </label>
+            <BindedSelect
+              aria-label="location"
+              bind="screenMetadata.trialSessionFilters.trialLocation"
+              className="select-left width-180 inline-select"
+              id="locationFilter"
+              name="trialLocation"
+            >
+              <option value="">-Location-</option>
+              <TrialCityOptions procedureType="AllPlusStandalone" />
+            </BindedSelect>
+            <BindedSelect
+              aria-label="proceeding"
+              bind="screenMetadata.trialSessionFilters.proceedingType"
+              className="select-left width-180 inline-select margin-left-1pt5rem"
+              id="proceedingFilter"
+              name="proceedingType"
+            >
+              <option value="">-Proceeding Type-</option>
+              {Object.values(TRIAL_SESSION_PROCEEDING_TYPES).map(
+                proceedingType => (
+                  <option key={proceedingType} value={proceedingType}>
+                    {proceedingType}
+                  </option>
+                ),
+              )}
+            </BindedSelect>
+            <BindedSelect
+              aria-label="session"
+              bind="screenMetadata.trialSessionFilters.sessionType"
+              className="select-left width-180 inline-select margin-left-1pt5rem"
+              id="sessionFilter"
+              name="sessionType"
+            >
+              <option value="">-Session Type-</option>
+              {Object.values(SESSION_TYPES).map(sessionType => (
+                <option key={sessionType} value={sessionType}>
+                  {sessionType}
+                </option>
+              ))}
+            </BindedSelect>
+            <BindedSelect
+              aria-label="judge"
+              bind="screenMetadata.trialSessionFilters.judge.userId"
+              className="select-left width-180 inline-select margin-left-1pt5rem"
+              id="judgeFilter"
+              name="judge"
+            >
+              <option value="">-Judge-</option>
+              {trialSessionsHelper.trialSessionJudges.map(judge => (
+                <option key={judge.name} value={judge.userId}>
+                  {judge.name}
+                </option>
+              ))}
+
+              {trialSessionsHelper.showUnassignedJudgeFilter && (
+                <option
+                  key={trialSessionsHelper.trialSessionJudges.length}
+                  value="unassigned"
+                >
+                  Unassigned
+                </option>
+              )}
+            </BindedSelect>
+          </div>
+          {trialSessionsHelper.showNewTrialSession && (
+            <Button
+              data-testid="add-trial-session-button"
+              href="/add-a-trial-session"
+              icon="plus-circle"
+            >
+              Add Trial Session
+            </Button>
+          )}
+        </div>
+      </>
+    );
+  },
+);
+// });
 
 TrialSessions.displayName = 'TrialSessions';
