217f957fd2b4286cba28f485adf206ec19c5a6e2
Christopher Bisom <cbisom@flexion.us>
2024-09-05 18:13:58 -0400

    10001-design-debt: more test fixes, again

--- cypress/local-only/tests/integration/fileAPetitionUpdated/file-a-petition-step-1-business--partnership-representative-under-BBA.cy.ts
+++ cypress/local-only/tests/integration/fileAPetitionUpdated/file-a-petition-step-1-business--partnership-representative-under-BBA.cy.ts
@@ -132,7 +132,8 @@ describe('File a petition: Step 1 - Petitioner Information', () => {
               attachFile({
                 filePath: uploadFile,
                 selector: `[data-testid="${input}"]`,
-                selectorToAwaitOnSuccess: '[data-testid="remove-pdf"]',
+                selectorToAwaitOnSuccess:
+                  '[data-testid^="upload-file-success"]',
               });
             }
           });
--- cypress/local-only/tests/integration/fileAPetitionUpdated/file-a-petition-step-1-business--partnership-tax-matters-partner.cy.ts
+++ cypress/local-only/tests/integration/fileAPetitionUpdated/file-a-petition-step-1-business--partnership-tax-matters-partner.cy.ts
@@ -132,7 +132,8 @@ describe('File a petition: Step 1 - Petitioner Information', () => {
               attachFile({
                 filePath: uploadFile,
                 selector: `[data-testid="${input}"]`,
-                selectorToAwaitOnSuccess: '[data-testid="remove-pdf"]',
+                selectorToAwaitOnSuccess:
+                  '[data-testid^="upload-file-success"]',
               });
             }
           });
@@ -305,7 +306,8 @@ describe('File a petition: Step 1 - Petitioner Information', () => {
               attachFile({
                 filePath: uploadFile,
                 selector: `[data-testid="${input}"]`,
-                selectorToAwaitOnSuccess: '[data-testid="remove-pdf"]',
+                selectorToAwaitOnSuccess:
+                  '[data-testid^="upload-file-success"]',
               });
             }
           });
