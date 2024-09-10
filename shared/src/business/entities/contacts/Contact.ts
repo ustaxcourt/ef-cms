ac16f474d85ad80d49572a8fb51baecc1822a3d3
Andy Kuny <akuny@flexion.us>
2024-09-10 10:12:28 -0400

    10466: update Contact entity test case

--- shared/src/business/entities/contacts/Contact.ts
+++ shared/src/business/entities/contacts/Contact.ts
@@ -87,6 +87,10 @@ export class Contact extends JoiValidationEntity {
       is: true,
       otherwise: joi.optional(),
       then: joi.required(),
+    }).messages({
+      'any.required': 'Enter a valid email address',
+      'string.email': 'Enter email address in format: yourname@example.com',
+      'string.empty': 'Enter a valid email address',
     }),
     hasConsentedToEService: joi
       .boolean()
