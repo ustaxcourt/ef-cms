ff6cbd0f2d0fae616b00cb854dbf3feade4b7847
Jim Lerza <james.lerza@ustaxcourt.gov>
2024-09-10 13:21:16 -0400

    (opex irs superuser tests): if the docket clerk sends a message when QCing a document, return the message id as a chainable

--- cypress/helpers/documentQC/docket-clerk-qcs-document.ts
+++ cypress/helpers/documentQC/docket-clerk-qcs-document.ts
@@ -2,7 +2,7 @@ export const docketClerkQCsDocument = (
   docketNumber: string,
   docketEntryId: string,
   sendMessage?: { body: string; recipient: string; section: string },
-) => {
+): Cypress.Chainable<string | undefined> | undefined => {
   cy.login(
     'docketclerk1',
     `/case-detail/${docketNumber}/documents/${docketEntryId}/edit?fromPage=qc-section-inbox`,
@@ -14,6 +14,16 @@ export const docketClerkQCsDocument = (
     cy.get('#message').type(sendMessage.body);
     cy.get('[data-testid="modal-confirm"]').click();
     cy.get('[data-testid="success-alert"]').should('exist');
+    cy.get('[data-testid="header-messages-link"]').click();
+    cy.get('[data-testid="messages-outbox-tab"]').click();
+    return cy
+      .get('[data-testid="message-individual-outbox-table"]')
+      .find('tbody > tr')
+      .first()
+      .invoke('attr', 'data-testid')
+      .then(messageId => {
+        return cy.wrap(messageId);
+      });
   } else {
     cy.get('#save-and-finish').click();
     cy.get('[data-testid="success-alert"]').should('exist');
