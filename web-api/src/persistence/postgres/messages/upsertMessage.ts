f87088078007256db45eaa9a3ee0860bd457245b
Kaitlyn Swann <kswann@flexion.us>
2024-09-12 09:50:31 -0600

    10391: split out upsert message and messages

--- web-api/src/persistence/postgres/messages/upsertMessage.ts
+++ web-api/src/persistence/postgres/messages/upsertMessage.ts
@@ -1,10 +1,6 @@
 import { RawMessage } from '@shared/business/entities/Message';
 import { getDbWriter } from '@web-api/database';
-import {
-  toKyselyNewMessage,
-  toKyselyNewMessages,
-  toKyselyUpdateMessage,
-} from './mapper';
+import { toKyselyNewMessage, toKyselyUpdateMessage } from './mapper';
 
 export const upsertMessage = async (message: RawMessage) => {
   await getDbWriter(writer =>
@@ -13,25 +9,6 @@ export const upsertMessage = async (message: RawMessage) => {
       .values(toKyselyNewMessage(message))
       .onConflict(oc =>
         oc.column('messageId').doUpdateSet(toKyselyUpdateMessage(message)),
-      )
-      .execute(),
-  );
-};
-
-export const upsertMessages = async (messages: RawMessage[]) => {
-  if (messages.length === 0) return;
-
-  await getDbWriter(writer =>
-    writer
-      .insertInto('message')
-      .values(toKyselyNewMessages(messages))
-      .onConflict(oc =>
-        oc.column('messageId').doUpdateSet(c => {
-          const keys = Object.keys(messages[0]!) as any[];
-          return toKyselyUpdateMessage(
-            Object.fromEntries(keys.map(key => [key, c.ref(key)])),
-          );
-        }),
       )
       .execute(),
   );
