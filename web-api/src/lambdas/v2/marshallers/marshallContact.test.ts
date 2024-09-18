cd58d19673ff2302a9b3cc0124865429c1c188e1
Mike Marcotte <mike.marcotte@gmail.com>
2024-09-18 16:18:39 -0400

    #5377: include country

--- web-api/src/lambdas/v2/marshallers/marshallContact.test.ts
+++ web-api/src/lambdas/v2/marshallers/marshallContact.test.ts
@@ -5,6 +5,11 @@ const MOCK_CONTACT = Object.assign({}, getContactPrimary(MOCK_CASE), {
   serviceIndicator: 'Electronic',
 });
 
+const MOCK_CONTACT_INTERNATIONAL = Object.assign({}, MOCK_CONTACT, {
+  country: 'Brazil',
+  serviceIndicator: 'Electronic',
+});
+
 describe('marshallContact', () => {
   it('returns a contact object with the expected properties', () => {
     expect(Object.keys(marshallContact(MOCK_CONTACT)).sort()).toEqual([
@@ -12,6 +17,23 @@ describe('marshallContact', () => {
       'address2',
       'address3',
       'city',
+      'country',
+      'email',
+      'name',
+      'phone',
+      'postalCode',
+      'serviceIndicator',
+      'state',
+    ]);
+
+    expect(
+      Object.keys(marshallContact(MOCK_CONTACT_INTERNATIONAL)).sort(),
+    ).toEqual([
+      'address1',
+      'address2',
+      'address3',
+      'city',
+      'country',
       'email',
       'name',
       'phone',
@@ -37,6 +59,7 @@ describe('marshallContact', () => {
     expect(mock.postalCode).toBeDefined();
     expect(mock.serviceIndicator).toBeDefined();
     expect(mock.state).toBeDefined();
+    expect(mock.country).toBeUndefined(); // undefined
 
     const marshalled = marshallContact(mock);
 
@@ -44,6 +67,40 @@ describe('marshallContact', () => {
     expect(marshalled.address2).toEqual(mock.address2);
     expect(marshalled.address3).toEqual(mock.address3);
     expect(marshalled.city).toEqual(mock.city);
+    expect(marshalled.email).toEqual(mock.email);
+    expect(marshalled.name).toEqual(mock.name);
+    expect(marshalled.phone).toEqual(mock.phone);
+    expect(marshalled.postalCode).toEqual(mock.postalCode);
+    expect(marshalled.serviceIndicator).toEqual(mock.serviceIndicator);
+    expect(marshalled.state).toEqual(mock.state);
+    expect(marshalled.country).toEqual(mock.country);
+  });
+
+  it('marshalls from the current contact format for users with a country', () => {
+    const mock = Object.assign({}, MOCK_CONTACT_INTERNATIONAL, {
+      address2: 'address2',
+      address3: 'address3',
+    });
+
+    expect(mock.address1).toBeDefined();
+    expect(mock.address2).toBeDefined();
+    expect(mock.address3).toBeDefined();
+    expect(mock.city).toBeDefined();
+    expect(mock.country).toBeDefined();
+    expect(mock.email).toBeDefined();
+    expect(mock.name).toBeDefined();
+    expect(mock.phone).toBeDefined();
+    expect(mock.postalCode).toBeDefined();
+    expect(mock.serviceIndicator).toBeDefined();
+    expect(mock.state).toBeDefined();
+
+    const marshalled = marshallContact(mock);
+
+    expect(marshalled.address1).toEqual(mock.address1);
+    expect(marshalled.address2).toEqual(mock.address2);
+    expect(marshalled.address3).toEqual(mock.address3);
+    expect(marshalled.city).toEqual(mock.city);
+    expect(marshalled.country).toEqual(mock.country);
     expect(marshalled.email).toEqual(mock.email);
     expect(marshalled.name).toEqual(mock.name);
     expect(marshalled.phone).toEqual(mock.phone);
