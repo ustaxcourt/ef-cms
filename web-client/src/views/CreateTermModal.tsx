84baed2b9a97aec974a20a445caddd93fe61c510
Nate Elliott <nelliott@flexion.us>
2024-09-23 15:20:10 -0500

    10275 Remove 'term' select input from create term modal

--- web-client/src/views/CreateTermModal.tsx
+++ web-client/src/views/CreateTermModal.tsx
@@ -5,7 +5,6 @@ import { connect } from '@web-client/presenter/shared.cerebral';
 import { sequences } from '@web-client/presenter/app.cerebral';
 import { state } from '@web-client/presenter/app.cerebral';
 import React, { useState } from 'react';
-import classNames from 'classnames';
 
 export const CreateTermModal = connect(
   {
@@ -34,95 +33,59 @@ export const CreateTermModal = connect(
         title="Create Term"
         onModalMount={() => setIsModalMounted(true)}
       >
-        <div className="margin-bottom-4">
-          <FormGroup errorText={validationErrors.termName}>
-            <fieldset className="usa-fieldset margin-bottom-0">
-              <legend className="display-block" id="trial-term">
-                Term name
-              </legend>
-              <input
-                className="usa-input"
-                data-testid="term-name-field"
-                id="term-name-field"
-                name="termName"
-                type="text"
-                value={modal.termName}
-                onChange={e => {
-                  updateModalValueSequence({
-                    key: e.target.name,
-                    value: e.target.value,
-                  });
-                }}
-              />
-            </fieldset>
-          </FormGroup>
+        <FormGroup errorText={validationErrors.termName}>
+          <fieldset className="usa-fieldset margin-bottom-0">
+            <legend className="display-block" id="trial-term">
+              Term name
+            </legend>
+            <input
+              className="usa-input"
+              data-testid="term-name-field"
+              id="term-name-field"
+              name="termName"
+              type="text"
+              value={modal.termName}
+              onChange={e => {
+                updateModalValueSequence({
+                  key: e.target.name,
+                  value: e.target.value,
+                });
+              }}
+            />
+          </fieldset>
+        </FormGroup>
 
-          <FormGroup errorText={validationErrors.term}>
-            <fieldset className="usa-fieldset margin-bottom-0">
-              <legend className="display-block" id="trial-term">
-                Trial term
-              </legend>
-              <select
-                aria-label="trial report term"
-                className={classNames(
-                  'usa-select',
-                  validationErrors.term && 'usa-select--error',
-                )}
-                name="term"
-                onChange={e => {
-                  updateModalValueSequence({
-                    key: e.target.name,
-                    value: e.target.value,
-                  });
-                }}
-              >
-                <option value="">- Select -</option>
-                {/* 10275: the three options below should be the "next" three terms */}
-                <option key="winter" value="winter">
-                  Winter
-                </option>
-                <option key="spring" value="spring">
-                  Spring
-                </option>
-                <option key="fall" value="fall">
-                  Fall
-                </option>
-              </select>
-            </fieldset>
-          </FormGroup>
-
-          <FormGroup>
-            <fieldset className="usa-fieldset margin-bottom-0">
-              <DateRangePickerComponent
-                endDateErrorText={validationErrors.termEndDate}
-                endLabel="Term end date"
-                endName="termEndDate"
-                endPickerCls={'grid-col-6'}
-                endValue={modal.termEndDate}
-                formGroupCls="margin-bottom-0"
-                parentModalHasMounted={isModalMounted}
-                rangePickerCls={'grid-row '}
-                startDateErrorText={validationErrors.termStartDate}
-                startLabel="Term start date"
-                startName="termStartDate"
-                startPickerCls={'grid-col-6'}
-                startValue={modal.termStartDate}
-                onChangeEnd={e => {
-                  updateModalValueSequence({
-                    key: 'termEndDate',
-                    value: e.target.value,
-                  });
-                }}
-                onChangeStart={e => {
-                  updateModalValueSequence({
-                    key: 'termStartDate',
-                    value: e.target.value,
-                  });
-                }}
-              />
-            </fieldset>
-          </FormGroup>
-        </div>
+        <FormGroup>
+          <fieldset className="usa-fieldset margin-bottom-0">
+            <DateRangePickerComponent
+              endDateErrorText={validationErrors.termEndDate}
+              endLabel="Term end date"
+              endName="termEndDate"
+              endPickerCls={'grid-col-6'}
+              endValue={modal.termEndDate}
+              formGroupCls="margin-bottom-0"
+              parentModalHasMounted={isModalMounted}
+              rangePickerCls={'grid-row '}
+              startDateErrorText={validationErrors.termStartDate}
+              startLabel="Term start date"
+              startName="termStartDate"
+              startPickerCls={'grid-col-6'}
+              startValue={modal.termStartDate}
+              onChangeEnd={e => {
+                updateModalValueSequence({
+                  key: 'termEndDate',
+                  value: e.target.value,
+                });
+              }}
+              onChangeStart={e => {
+                updateModalValueSequence({
+                  key: 'termStartDate',
+                  value: e.target.value,
+                });
+              }}
+            />
+          </fieldset>
+        </FormGroup>
       </ModalDialog>
     );
   },
