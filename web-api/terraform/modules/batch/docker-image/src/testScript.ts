4f8591290815e61e055af487e3ff9459b3d96979
John Cruz <jcruz@flexion.us>
2024-09-23 08:35:43 -0600

    10363: Extract region to cconstant;

--- web-api/terraform/modules/batch/docker-image/src/testScript.ts
+++ web-api/terraform/modules/batch/docker-image/src/testScript.ts
@@ -3,12 +3,13 @@ import { NodeHttpHandler } from '@aws-sdk/node-http-handler';
 import { S3 } from '@aws-sdk/client-s3';
 
 const { EFCMS_DOMAIN, files, stage } = process.env;
+const AWS_REGION = 'us-east-1';
 
 const s3Client = new S3({
-  endpoint: 'https://s3.us-east-1.amazonaws.com',
+  endpoint: `https://s3.${AWS_REGION}.amazonaws.com`,
   forcePathStyle: true,
   maxAttempts: 3,
-  region: 'us-east-1',
+  region: AWS_REGION,
   requestHandler: new NodeHttpHandler({
     connectionTimeout: 3000,
     httpsAgent: new Agent({ keepAlive: true, maxSockets: 75 }),
@@ -19,7 +20,7 @@ const s3Client = new S3({
 async function app() {
   const fileNames = JSON.parse(files!);
   const data = await s3Client.getObject({
-    Bucket: `${EFCMS_DOMAIN}-documents-${stage}-us-east-1`,
+    Bucket: `${EFCMS_DOMAIN}-documents-${stage}-${AWS_REGION}`,
     Key: fileNames[0],
   });
   console.log('data', data);
