e7e5db633aaaeae4973d1f4311e8f5dfdc1a7b46
Zachary Rogers <zrogers>
2024-09-25 16:19:35 -0700

    10409: WIP creating test for trial session filters

--- cypress/helpers/trialSession/create-trial-session.ts
+++ cypress/helpers/trialSession/create-trial-session.ts
@@ -1,20 +1,48 @@
-export function createTrialSession(): Cypress.Chainable<{
+import {
+  SESSION_TYPES,
+  TRIAL_SESSION_PROCEEDING_TYPES,
+  TrialSessionProceedingType,
+  TrialSessionTypes,
+} from '../../../shared/src/business/entities/EntityConstants';
+
+export function createTrialSession(
+  {
+    endDate = '02/02/2100',
+    judge = 'Carluzzo',
+    proceedingType = TRIAL_SESSION_PROCEEDING_TYPES.inPerson,
+    sessionType = SESSION_TYPES.hybrid,
+    startDate = '02/02/2099',
+    trialLocation = 'Anchorage, Alaska',
+  }: Partial<{
+    trialLocation: string;
+    endDate: string;
+    startDate: string;
+    sessionType: TrialSessionTypes;
+    proceedingType: TrialSessionProceedingType;
+    judge: string;
+  }> = {
+    endDate: '02/02/2100',
+    judge: 'Carluzzo',
+    proceedingType: TRIAL_SESSION_PROCEEDING_TYPES.inPerson,
+    sessionType: SESSION_TYPES.hybrid,
+    startDate: '02/02/2099',
+    trialLocation: 'Anchorage, Alaska',
+  },
+): Cypress.Chainable<{
   trialSessionId: string;
 }> {
   cy.get('[data-testid="inbox-tab-content"]').should('exist');
   cy.get('[data-testid="trial-session-link"]').click();
   cy.get('[data-testid="add-trial-session-button"]').click();
   cy.get('#start-date-picker').clear();
-  cy.get('#start-date-picker').type('02/02/2099');
+  cy.get('#start-date-picker').type(startDate);
   cy.get('#estimated-end-date-picker').clear();
-  cy.get('#estimated-end-date-picker').type('02/02/2100');
-  cy.get('[data-testid="session-type-Hybrid"]').click();
+  cy.get('#estimated-end-date-picker').type(endDate);
+  cy.get(`[data-testid="session-type-${sessionType}"]`).click();
   cy.get('[data-testid="trial-session-number-of-cases-allowed"]').clear();
   cy.get('[data-testid="trial-session-number-of-cases-allowed"]').type('10');
-  cy.get('[data-testid="inPerson-proceeding-label"]').click();
-  cy.get('[data-testid="trial-session-trial-location"]').select(
-    'Anchorage, Alaska',
-  );
+  cy.get(`[data-testid="${proceedingType}-proceeding-label"]`).click();
+  cy.get('[data-testid="trial-session-trial-location"]').select(trialLocation);
   cy.get('[data-testid="courthouse-name"]').clear();
   cy.get('[data-testid="courthouse-name"]').type('a courthouse');
   cy.get('[data-testid="city"]').clear();
@@ -22,7 +50,7 @@ export function createTrialSession(): Cypress.Chainable<{
   cy.get('[data-testid="state"]').select('TN');
   cy.get('[data-testid="postal-code"]').clear();
   cy.get('[data-testid="postal-code"]').type('33333');
-  cy.get('[data-testid="trial-session-judge"]').select('Carluzzo');
+  cy.get('[data-testid="trial-session-judge"]').select(judge);
   cy.get('[data-testid="trial-session-trial-clerk"]').select(
     'Test trialclerk1',
   );
