95ee9d2b5104cf9de266c99a4324ccc51e66b738
Zachary Rogers <zrogers>
2024-09-25 15:05:43 -0700

    10409: Updating creation of a trial session to be more generic

---
+++ cypress/local-only/tests/integration/trialSession/trial-sessions-filtering.cy.ts
@@ -0,0 +1,7 @@
+import { createTrialSession } from '../../../../helpers/trialSession/create-trial-session';
+
+describe('trial sesions filtering', () => {
+  it('should filter trial sessions', () => {
+    createTrialSession();
+  });
+});
