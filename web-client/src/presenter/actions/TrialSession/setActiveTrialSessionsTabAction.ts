ea9ed972ab3925d506bcc138c9aa1c1c60098704
Zachary Rogers <zrogers>
2024-09-25 13:58:44 -0700

    10409: Route to the new tab after creating a trial session

--- web-client/src/presenter/actions/TrialSession/setActiveTrialSessionsTabAction.ts
+++
@@ -1,8 +0,0 @@
-import { TrialSessionScope } from '@shared/business/entities/EntityConstants';
-import { state } from '@web-client/presenter/app.cerebral';
-
-export const setActiveTrialSessionsTabAction = ({
-  store,
-}: ActionProps<{ sessionScope: TrialSessionScope }>) => {
-  store.set(state.trialSessionsPage.filters.currentTab, 'new');
-};
