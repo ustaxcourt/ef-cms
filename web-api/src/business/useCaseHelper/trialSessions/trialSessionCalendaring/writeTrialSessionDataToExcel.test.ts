cf8474445ead54b9b4e0b023c3a3ea752343695f
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-09-25 14:22:38 -0500

    10275: use correct colors for session type in xlsx, tweak styling

--- web-api/src/business/useCaseHelper/trialSessions/trialSessionCalendaring/writeTrialSessionDataToExcel.test.ts
+++ web-api/src/business/useCaseHelper/trialSessions/trialSessionCalendaring/writeTrialSessionDataToExcel.test.ts
@@ -9,9 +9,10 @@ const weeks = ['09/01', '09/08', '09/15', '09/45', '09/89', '09/37'];
 describe('writeTrialSessionDataToExcel', () => {
   for (const city of cities) {
     for (const week of weeks) {
+      const randomType = Math.floor(Math.random() * 3);
       mockScheduledTrialSessions.push({
         city,
-        sessionType: SESSION_TYPES.regular,
+        sessionType: SESSION_TYPES[Object.keys(SESSION_TYPES)[randomType]],
         weekOf: week,
       });
     }
