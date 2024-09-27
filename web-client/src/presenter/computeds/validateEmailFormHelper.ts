b0a6b74e3c678a1b65a8105e53bf7b60f9f7809d
Andy Kuny <akuny@flexion.us>
2024-09-27 10:02:58 -0400

    10466: explicitly set return type of new computed to any

--- web-client/src/presenter/computeds/validateEmailFormHelper.ts
+++ web-client/src/presenter/computeds/validateEmailFormHelper.ts
@@ -2,7 +2,7 @@ import { EmailForm } from '@shared/business/entities/EmailForm';
 import { Get } from 'cerebral';
 import { state } from '@web-client/presenter/app.cerebral';
 
-export const validateEmailFormHelper = (get: Get) => {
+export const validateEmailFormHelper = (get: Get): any => {
   const form = get(state.form);
   const formEntity = new EmailForm(form);
   const errors = formEntity.getFormattedValidationErrors();
