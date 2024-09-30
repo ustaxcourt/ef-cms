3c5a8518f9b038a2e4d6c1bfb2e2f172ba9361b3
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-09-30 13:46:48 -0500

    10275: update generateSuggestedTrialSessionCalendarInteractor and test to accept MMDDYYYY format

--- web-api/src/business/useCases/trialSessions/generateSuggestedTrialSessionCalendarInteractor.test.ts
+++ web-api/src/business/useCases/trialSessions/generateSuggestedTrialSessionCalendarInteractor.test.ts
@@ -35,8 +35,10 @@ describe('generateSuggestedTrialSessionCalendar', () => {
   });
 
   it('should generate a trial term when valid date range is provided and sufficient data is present in the system', async () => {
-    const mockStartDate = '2019-08-22T04:00:00.000Z';
-    const mockEndDate = '2019-09-22T04:00:00.000Z';
+    // const mockStartDate = '2019-08-22T04:00:00.000Z';
+    // const mockEndDate = '2019-09-22T04:00:00.000Z';
+    const mockStartDate = '08/22/2019';
+    const mockEndDate = '09/22/2019';
 
     const { bufferArray, message } =
       await generateSuggestedTrialSessionCalendarInteractor(
