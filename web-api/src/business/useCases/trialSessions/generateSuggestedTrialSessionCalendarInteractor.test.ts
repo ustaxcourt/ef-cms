ba686277a4865c8ee71cc2e73d9b2d03fe8290aa
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-09-30 13:36:29 -0500

    10275: bare minimum  generateSuggested...Interactor test

--- web-api/src/business/useCases/trialSessions/generateSuggestedTrialSessionCalendarInteractor.test.ts
+++ web-api/src/business/useCases/trialSessions/generateSuggestedTrialSessionCalendarInteractor.test.ts
@@ -1,56 +1,51 @@
-import { SESSION_TYPES } from '../../../../../shared/src/business/entities/EntityConstants';
+import { MOCK_CASE_READY_FOR_TRIAL_SESSION_SCHEDULING } from '@shared/test/mockCase';
+import {
+  PROCEDURE_TYPES_MAP,
+  TRIAL_CITY_STRINGS,
+} from '../../../../../shared/src/business/entities/EntityConstants';
 import { applicationContext } from '../../../../../shared/src/business/test/createTestApplicationContext';
 import { generateSuggestedTrialSessionCalendarInteractor } from '@web-api/business/useCases/trialSessions/generateSuggestedTrialSessionCalendarInteractor';
 import { mockPetitionsClerkUser } from '@shared/test/mockAuthUsers';
 
 describe('generateSuggestedTrialSessionCalendar', () => {
-  // beforeAll(() => {
-  //   applicationContext
-  //     .getPersistenceGateway()
-  //     .getReadyForTrialCases.mockImplementation(() => mockCasesReadyForTrial);
+  // REGULAR_CASE_MINIMUM_QUANTITY + 1
+  const totalNumberOfMockCases = 40 + 1;
 
-  // beforeEach(() => {
-  //   applicationContext
-  //     .getPersistenceGateway()
-  //     .getLock.mockReturnValue(undefined);
-  // });
+  const mockRegularCityString =
+    TRIAL_CITY_STRINGS[TRIAL_CITY_STRINGS.length - 1];
 
-  it('should generate a trial term when valid date range is provided', async () => {
+  const mockCases: RawCase[] = [];
+  for (let i = 0; i < totalNumberOfMockCases; ++i) {
+    mockCases.push({
+      ...MOCK_CASE_READY_FOR_TRIAL_SESSION_SCHEDULING,
+      docketNumber: `10${i}-24`,
+      preferredTrialCity: mockRegularCityString,
+      procedureType: PROCEDURE_TYPES_MAP.regular,
+    });
+  }
+
+  beforeAll(() => {
+    applicationContext
+      .getPersistenceGateway()
+      .getReadyForTrialCases.mockResolvedValue(mockCases);
+
+    applicationContext
+      .getPersistenceGateway()
+      .getTrialSessions.mockResolvedValue([]);
+  });
+
+  it('should generate a trial term when valid date range is provided and sufficient data is present in the system', async () => {
     const mockStartDate = '2019-08-22T04:00:00.000Z';
     const mockEndDate = '2019-09-22T04:00:00.000Z';
 
-    const result = await generateSuggestedTrialSessionCalendarInteractor(
-      applicationContext,
-      { termEndDate: mockEndDate, termStartDate: mockStartDate },
-      mockPetitionsClerkUser,
-    );
+    const { bufferArray, message } =
+      await generateSuggestedTrialSessionCalendarInteractor(
+        applicationContext,
+        { termEndDate: mockEndDate, termStartDate: mockStartDate },
+        mockPetitionsClerkUser,
+      );
 
-    expect(result).toEqual([
-      {
-        city: 'City A',
-        sessionType: SESSION_TYPES.regular,
-        weekOf: '01/01/01',
-      },
-      {
-        city: 'City B',
-        sessionType: SESSION_TYPES.small,
-        weekOf: '01/07/01',
-      },
-    ]);
+    expect(message).toEqual('Trial session calendar generated');
+    expect(bufferArray).toBeDefined();
   });
-
-  // it('should throw an error when...', async () => {
-  //   mockCasesReadyForTrial = [];
-  //   applicationContext
-  //     .getPersistenceGateway()
-  //     .getCaseByDocketNumber.mockReturnValue(MOCK_CASE);
-
-  //   await expect(
-  //     checkForReadyForTrialCasesInteractor(applicationContext),
-  //   ).resolves.not.toThrow();
-
-  //   expect(
-  //     applicationContext.getPersistenceGateway().getReadyForTrialCases,
-  //   ).toHaveBeenCalled();
-  // });
 });
