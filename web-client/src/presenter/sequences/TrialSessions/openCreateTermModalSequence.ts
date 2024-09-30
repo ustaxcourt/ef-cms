796463d2b2b89ac643b4aa1cd3296d2973ddb441
Andy Kuny <akuny@flexion.us>
2024-09-30 14:04:41 -0400

    10275: remove unnecessary action in openCreateTermModalSequence

--- web-client/src/presenter/sequences/TrialSessions/openCreateTermModalSequence.ts
+++ web-client/src/presenter/sequences/TrialSessions/openCreateTermModalSequence.ts
@@ -1,9 +1,7 @@
 import { clearModalStateAction } from '@web-client/presenter/actions/clearModalStateAction';
 import { setShowModalFactoryAction } from '@web-client/presenter/actions/setShowModalFactoryAction';
-import { setupTrialYearsAction } from '@web-client/presenter/actions/setupTrialYearsAction';
 
 export const openCreateTermModalSequence = [
   clearModalStateAction,
-  setupTrialYearsAction,
   setShowModalFactoryAction('CreateTermModal'),
 ];
