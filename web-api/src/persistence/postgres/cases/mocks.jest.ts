919113ea4d4bb3a3eedd3f1fd01181351979c6c9
Kaitlyn Swann <kswann@flexion.us>
2024-10-01 07:44:24 -0600

    10391: update upsert case mock

--- web-api/src/persistence/postgres/cases/mocks.jest.ts
+++ web-api/src/persistence/postgres/cases/mocks.jest.ts
@@ -1,5 +1,5 @@
 import { mockFactory } from '@shared/test/mockFactory';
 
-jest.mock('@web-api/persistence/postgres/cases/upsertCase', () =>
-  mockFactory('upsertCase'),
+jest.mock('@web-api/persistence/postgres/cases/upsertCases', () =>
+  mockFactory('upsertCases'),
 );
