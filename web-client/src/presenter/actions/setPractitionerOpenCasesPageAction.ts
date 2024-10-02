16c876465f947456077b0587bee9741172347bc2
Christopher Bisom <cbisom@flexion.us>
2024-10-02 14:40:04 -0400

    10261-story: fix pagination

--- web-client/src/presenter/actions/setPractitionerOpenCasesPageAction.ts
+++ web-client/src/presenter/actions/setPractitionerOpenCasesPageAction.ts
@@ -1,11 +1,19 @@
 import { state } from '@web-client/presenter/app.cerebral';
 
 export const setPractitionerOpenCasesPageAction = ({
+  get,
   props,
   store,
 }: ActionProps<{
   pageNumber: number;
 }>) => {
   const { pageNumber } = props;
-  store.set(state.practitionerDetail.openCaseInfo?.currentPage, pageNumber);
+  const openCaseInfo = get(state.practitionerDetail.openCaseInfo);
+
+  if (!openCaseInfo) {
+    throw new Error(
+      'Attempting to set state.practitionerDetail.openCaseInfo.currentPage without first setting up state.practitionerDetail.openCaseInfo',
+    );
+  }
+  store.set(state.practitionerDetail.openCaseInfo!.currentPage, pageNumber);
 };
