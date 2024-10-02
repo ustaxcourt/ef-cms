5a0c2560ab9e30e1613b9be77bf699bb76672676
John Cruz <jcruz@flexion.us>
2024-10-02 08:08:34 -0600

    10363: Remove life cycle policy;

--- web-api/terraform/modules/ecr-for-env/batch-ecs-repo.tf
+++ web-api/terraform/modules/ecr-for-env/batch-ecs-repo.tf
@@ -2,27 +2,27 @@ resource "aws_ecr_repository" "docket_entry_zipper_repo" {
   name = "docket-entry-zipp`r-${var.environment}-${var.color}-${var.region}"
 }
 
-# TODO: This is not applying as we do not tag images with 'SNAPSHOT-' prefix anymore. 
-resource "aws_ecr_lifecycle_policy" "repo_policy" {
-  repository = aws_ecr_repository.docket_entry_zipper_repo.name
+# # TODO: This is not applying as we do not tag images with 'SNAPSHOT-' prefix anymore. 
+# resource "aws_ecr_lifecycle_policy" "repo_policy" {
+#   repository = aws_ecr_repository.docket_entry_zipper_repo.name
 
-  policy = <<EOF
-{
-    "rules": [
-        {
-            "rulePriority": 1,
-            "description": "Keep last 30 images",
-            "selection": {
-                "tagStatus": "tagged",
-                "tagPrefixList": ["SNAPSHOT-"],
-                "countType": "imageCountMoreThan",
-                "countNumber": 10
-            },
-            "action": {
-                "type": "expire"
-            }
-        }
-    ]
-}
-EOF
-}
+#   policy = <<EOF
+# {
+#     "rules": [
+#         {
+#             "rulePriority": 1,
+#             "description": "Keep last 30 images",
+#             "selection": {
+#                 "tagStatus": "tagged",
+#                 "tagPrefixList": ["SNAPSHOT-"],
+#                 "countType": "imageCountMoreThan",
+#                 "countNumber": 10
+#             },
+#             "action": {
+#                 "type": "expire"
+#             }
+#         }
+#     ]
+# }
+# EOF
+# }
