984ecd4ffec4e9fabf0e5938c79514f320af81d5
Nate Elliott <nelliott@flexion.us>
2024-10-03 08:04:35 -0500

    10275 More tests for special session filter

--- web-api/src/business/useCases/trialSessions/generateSuggestedTrialSessionCalendarInteractor.getSpecialSessionsInTerm.test.ts
+++ web-api/src/business/useCases/trialSessions/generateSuggestedTrialSessionCalendarInteractor.getSpecialSessionsInTerm.test.ts
@@ -1,15 +1,11 @@
 import { MOCK_TRIAL_REGULAR } from '@shared/test/mockTrial';
 import {
+  SESSION_STATUS_TYPES,
   SESSION_TYPES,
   TRIAL_CITY_STRINGS,
 } from '@shared/business/entities/EntityConstants';
 import { getSpecialSessionsInTerm } from '@web-api/business/useCases/trialSessions/generateSuggestedTrialSessionCalendarInteractor';
 import { getUniqueId } from '@shared/sharedAppContext';
-
-// is special
-// isCalendared
-// not closed
-// in term
 
 const mockEligibleSpecialSessions = [
   {
@@ -49,6 +45,79 @@ describe('generateSuggestedTrialSessionCalendar.getSpecialSessionsInTerm', () =>
         isCalendared: true,
         sessionType: SESSION_TYPES.special,
         startDate: '2019-08-29T04:00:00.000Z',
+        trialLocation: TRIAL_CITY_STRINGS[0],
+        trialSessionId: getUniqueId(),
+      },
+    ];
+
+    const actualSpecialSessions = getSpecialSessionsInTerm({
+      sessions: mockSessions,
+      termEndDate: mockEndDate,
+      termStartDate: mockStartDate,
+    });
+
+    expect(actualSpecialSessions).toEqual(mockEligibleSpecialSessions);
+  });
+
+  it('should filter out non-special sessions within the term date range', () => {
+    const mockStartDate = '2019-09-01T04:00:00.000Z';
+    const mockEndDate = '2019-09-30T04:00:00.000Z';
+    const mockSessions = [
+      ...mockEligibleSpecialSessions,
+      {
+        ...MOCK_TRIAL_REGULAR,
+        isCalendared: true,
+        sessionType: SESSION_TYPES.regular,
+        startDate: '2019-09-10T04:00:00.000Z',
+        trialLocation: TRIAL_CITY_STRINGS[0],
+        trialSessionId: getUniqueId(),
+      },
+    ];
+
+    const actualSpecialSessions = getSpecialSessionsInTerm({
+      sessions: mockSessions,
+      termEndDate: mockEndDate,
+      termStartDate: mockStartDate,
+    });
+
+    expect(actualSpecialSessions).toEqual(mockEligibleSpecialSessions);
+  });
+
+  it('should filter out non-calendared special sessions within the term date range', () => {
+    const mockStartDate = '2019-09-01T04:00:00.000Z';
+    const mockEndDate = '2019-09-30T04:00:00.000Z';
+    const mockSessions = [
+      ...mockEligibleSpecialSessions,
+      {
+        ...MOCK_TRIAL_REGULAR,
+        isCalendared: false,
+        sessionType: SESSION_TYPES.special,
+        startDate: '2019-09-10T04:00:00.000Z',
+        trialLocation: TRIAL_CITY_STRINGS[0],
+        trialSessionId: getUniqueId(),
+      },
+    ];
+
+    const actualSpecialSessions = getSpecialSessionsInTerm({
+      sessions: mockSessions,
+      termEndDate: mockEndDate,
+      termStartDate: mockStartDate,
+    });
+
+    expect(actualSpecialSessions).toEqual(mockEligibleSpecialSessions);
+  });
+
+  it('should filter out closed special sessions within the term date range', () => {
+    const mockStartDate = '2019-09-01T04:00:00.000Z';
+    const mockEndDate = '2019-09-30T04:00:00.000Z';
+    const mockSessions = [
+      ...mockEligibleSpecialSessions,
+      {
+        ...MOCK_TRIAL_REGULAR,
+        isCalendared: true,
+        sessionStatus: SESSION_STATUS_TYPES.closed,
+        sessionType: SESSION_TYPES.special,
+        startDate: '2019-09-10T04:00:00.000Z',
         trialLocation: TRIAL_CITY_STRINGS[0],
         trialSessionId: getUniqueId(),
       },
