4a76a2d48a5cd9c8fa161627376dcfee2d81b0b3
Zachary Rogers <zrogers>
2024-10-03 12:01:01 -0700

    10487-story: fix trial sessions with start dates after the search end date from appearing in judge activity report

--- web-api/src/business/useCases/judgeActivityReport/getTrialSessionsForJudgeActivityReportInteractor.ts
+++ web-api/src/business/useCases/judgeActivityReport/getTrialSessionsForJudgeActivityReportInteractor.ts
@@ -1,3 +1,7 @@
+import {
+  FORMATS,
+  formatDateString,
+} from '@shared/business/utilities/DateHandler';
 import { InvalidRequest, UnauthorizedError } from '@web-api/errors/errors';
 import { JudgeActivityReportSearch } from '../../../../../shared/src/business/entities/judgeActivityReport/JudgeActivityReportSearch';
 import { JudgeActivityStatisticsRequest } from '@web-api/business/useCases/judgeActivityReport/getCountOfCaseDocumentsFiledByJudgesInteractor';
@@ -60,10 +64,25 @@ export const getTrialSessionsForJudgeActivityReportInteractor = async (
     return searchEntity.judges.includes(session.judge.name);
   });
 
+  const formattedSearchStartDate = formatDateString(
+    searchEntity.startDate,
+    FORMATS.ISO,
+  );
+  const formattedSearchEndDate = formatDateString(
+    searchEntity.endDate,
+    FORMATS.ISO,
+  );
   const judgeSessionsInDateRange = trialSessionsForSelectedJudges.filter(
-    session =>
-      session.startDate <= searchEntity.endDate &&
-      session.startDate >= searchEntity.startDate,
+    session => {
+      const formattedSessionStartDate = formatDateString(
+        session.startDate,
+        FORMATS.ISO,
+      );
+      return (
+        formattedSessionStartDate <= formattedSearchEndDate &&
+        formattedSessionStartDate >= formattedSearchStartDate
+      );
+    },
   );
 
   const smallNonSwingSessions = judgeSessionsInDateRange.filter(
