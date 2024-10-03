41f0b32a8b3c926e968e7527c27972b93e527332
Andy Kuny <akuny@flexion.us>
2024-10-03 15:30:35 -0400

    10275: use new mock data in interactor test case

--- web-api/src/business/useCases/trialSessions/generateSuggestedTrialSessionCalendarInteractor.test.ts
+++ web-api/src/business/useCases/trialSessions/generateSuggestedTrialSessionCalendarInteractor.test.ts
@@ -1,29 +1,9 @@
-import { MOCK_CASE_READY_FOR_TRIAL_SESSION_SCHEDULING } from '@shared/test/mockCase';
-import {
-  PROCEDURE_TYPES_MAP,
-  TRIAL_CITY_STRINGS,
-} from '../../../../../shared/src/business/entities/EntityConstants';
 import { applicationContext } from '../../../../../shared/src/business/test/createTestApplicationContext';
 import { generateSuggestedTrialSessionCalendarInteractor } from '@web-api/business/useCases/trialSessions/generateSuggestedTrialSessionCalendarInteractor';
 import { mockPetitionsClerkUser } from '@shared/test/mockAuthUsers';
+import mockCases from '@shared/test/mockCasesReadyForTrial.json';
 
 describe('generateSuggestedTrialSessionCalendar', () => {
-  // REGULAR_CASE_MINIMUM_QUANTITY + 1
-  const totalNumberOfMockCases = 40 + 1;
-
-  const mockRegularCityString =
-    TRIAL_CITY_STRINGS[TRIAL_CITY_STRINGS.length - 1];
-
-  const mockCases: RawCase[] = [];
-  for (let i = 0; i < totalNumberOfMockCases; ++i) {
-    mockCases.push({
-      ...MOCK_CASE_READY_FOR_TRIAL_SESSION_SCHEDULING,
-      docketNumber: `10${i}-24`,
-      preferredTrialCity: mockRegularCityString,
-      procedureType: PROCEDURE_TYPES_MAP.regular,
-    });
-  }
-
   beforeAll(() => {
     applicationContext
       .getPersistenceGateway()
