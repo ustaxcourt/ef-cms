43586581dceae7b53f865b7fac7831ad0417e283
John Cruz <jcruz@flexion.us>
2024-10-03 13:41:30 -0600

    10363: Rename terraform resource;

--- web-api/terraform/modules/ecr-for-env/batch-ecs-repo.tf
+++ web-api/terraform/modules/ecr-for-env/batch-ecs-repo.tf
@@ -2,7 +2,7 @@ resource "aws_ecr_repository" "docket_entry_zipper_repo" {
   name = "docket-entry-zipper-${var.environment}-${var.color}-${var.region}"
 }
 
-resource "aws_ecr_lifecycle_policy" "example" {
+resource "aws_ecr_lifecycle_policy" "docket_entry_zipper_lifecycle_policy" {
   repository = aws_ecr_repository.docket_entry_zipper_repo.name
 
   policy = <<EOF
