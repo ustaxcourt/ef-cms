a8ab583e758dc78af327b8f332dbdd416bc4d530
Christopher Bisom <cbisom@flexion.us>
2024-10-04 21:13:13 -0400

    10074-bug: went through and found a few more discrepancies because why not

--- web-client/src/presenter/sequences/CaseAssociation/validateEditPetitionerCounselSequence.ts
+++ web-client/src/presenter/sequences/CaseAssociation/validateEditPetitionerCounselSequence.ts
@@ -1,5 +1,6 @@
 import { clearAlertsAction } from '../../actions/clearAlertsAction';
 import { setRepresentingFromRepresentingMapActionFactory } from '../../actions/setRepresentingFromRepresentingMapActionFactory';
+import { setValidationAlertErrorsAction } from '@web-client/presenter/actions/setValidationAlertErrorsAction';
 import { setValidationErrorsAction } from '../../actions/setValidationErrorsAction';
 import { shouldValidateAction } from '../../actions/shouldValidateAction';
 import { validateEditPetitionerCounselAction } from '../../actions/CaseAssociation/validateEditPetitionerCounselAction';
@@ -12,7 +13,7 @@ export const validateEditPetitionerCounselSequence = [
       setRepresentingFromRepresentingMapActionFactory('form'),
       validateEditPetitionerCounselAction,
       {
-        error: [setValidationErrorsAction],
+        error: [setValidationErrorsAction, setValidationAlertErrorsAction],
         success: [clearAlertsAction],
       },
     ],
