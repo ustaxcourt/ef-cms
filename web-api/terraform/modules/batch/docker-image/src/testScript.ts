7e0ed5672230a7d2aec147ef0d4e5108b672bbfd
John Cruz <jcruz@flexion.us>
2024-10-08 14:01:03 -0600

    10363: revert import statements; Ignore ts check in file;

--- web-api/terraform/modules/batch/docker-image/src/testScript.ts
+++ web-api/terraform/modules/batch/docker-image/src/testScript.ts
@@ -1,6 +1,4 @@
-import * as archiver from 'archiver';
-import * as fs from 'fs';
-import * as path from 'path';
+//@ts-nocheck
 import { Agent } from 'https';
 import {
   ApiGatewayManagementApiClient,
@@ -10,6 +8,9 @@ import { GetObjectCommand, PutObjectCommand, S3 } from '@aws-sdk/client-s3';
 import { NodeHttpHandler } from '@smithy/node-http-handler';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { writeFile } from 'fs/promises';
+import archiver from 'archiver';
+import fs from 'fs';
+import path from 'path';
 
 //TODO: Meaningful logs for debugging
 
