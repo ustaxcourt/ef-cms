865b37a938c59bcaba55fee710767f724319894f
Christopher Bisom <cbisom@flexion.us>
2024-10-10 15:25:24 -0400

    10313-bug: Initial styling for verify email banner

---
+++ web-client/src/ustc-ui/Icon/WarningIcon.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import USWDSWarningIcon from '../../../../node_modules/@uswds/uswds/dist/img/usa-icons/warning.svg';
+
+export function WarningIcon() {
+  return <img alt="Warning" src={USWDSWarningIcon}></img>;
+}
