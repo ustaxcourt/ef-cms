821ffa979f909c7382c5a7a61365acd6eb1357b6
Javis Sullivan <jsullivan@flexion.us>
2024-10-16 11:38:00 -0400

    10408: wip

--- web-client/src/presenter/computeds/trialSessionsHelper.ts
+++ web-client/src/presenter/computeds/trialSessionsHelper.ts
@@ -234,6 +234,7 @@ export const formatTrialSessions = ({
   trialSessions: TrialSessionInfoDTO[];
   judgeAssociatedToUser?: RawUser;
 }): (TrialSessionRow | TrialSessionWeek)[] => {
+  const specialTrialSessionNotes = null;
   const trialSessionRows: TrialSessionRow[] = trialSessions.map(
     trialSession => {
       const showAlertForNOTTReminder =
@@ -268,6 +269,10 @@ export const formatTrialSessions = ({
       */
       const userIsAssignedToSession = isJudgeUserAssigned;
 
+      if (trialSession.sessionType === 'Special') {
+        console.log('Special session in trialSessionsHelper');
+      }
+
       return {
         alertMessageForNOTT,
         formattedEstimatedEndDate,
@@ -278,6 +283,7 @@ export const formatTrialSessions = ({
         sessionStatus: trialSession.sessionStatus,
         sessionType: trialSession.sessionType,
         showAlertForNOTTReminder,
+        specialTrialSessionNotes,
         startDate: trialSession.startDate,
         swingSession: !!trialSession.swingSession,
         trialLocation: trialSession.trialLocation || '',
@@ -336,6 +342,7 @@ export type TrialSessionRow = {
   proceedingType: string;
   startDate: string; // ISO format
   sessionType: string;
+  specialTrialSessionNotes: string | null;
   judge: { name: string; userId: string };
   formattedNoticeIssuedDate: string;
   sessionStatus: string;
