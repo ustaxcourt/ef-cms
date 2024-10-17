d200c5a2d684fa8e22286fde3ce6d0c8b85b19ec
Zachary Rogers <zrogers>
2024-10-17 14:59:00 -0700

    10391: WIP create script for backup

--- restoreDbFromTarget.ts
+++ restoreDbFromTarget.ts
@@ -1,46 +1,90 @@
-import { SSMClient } from '@aws-sdk/client-ssm';
+import { Signer } from '@aws-sdk/rds-signer';
 import { execSync } from 'child_process';
 
 async function main() {
-  // const ssmClient = new SSMClient({ credentials: {}, region: 'us-east-1' });
   const sourceDbname = process.env.SOURCE_DB_NAME;
   const sourceHost = process.env.SOURCE_HOST;
-  const sourcePassword = process.env.SOURCE_PASSWORD;
   const sourcePort = process.env.SOURCE_PORT;
   const sourceUsername = process.env.SOURCE_USERNAME;
-  // const targetDbname = process.env.TARGET_DB_NAME;
-  // const targetHost = process.env.TARGET_HOST;
-  // const targetPassword = process.env.TARGET_PASSWORD;
-  // const targetPort = process.env.TARGET_PORT;
-  // const targetUsername = process.env.TARGET_USERNAME;
+  const targetDbname = process.env.TARGET_DB_NAME;
+  const targetHost = process.env.TARGET_HOST;
+  const targetUsername = process.env.TARGET_USERNAME;
+  const targetPort = process.env.TARGET_PORT;
+  const targetAccessKeyId = process.env.TARGET_AWS_ACCESS_KEY_ID;
+  const targetSecretAccessKey = process.env.TARGET_AWS_SECRET_ACCESS_KEY;
+  const targetAccountId = process.env.TARGET_AWS_ACCOUNT_ID;
+  const targetSessionToken = process.env.TARGET_AWS_SESSION_TOKEN;
 
   if (
     !sourceDbname ||
     !sourceHost ||
-    !sourcePassword ||
     !sourcePort ||
-    !sourceUsername
-    // !targetDbname ||
-    // !targetHost ||
-    // !targetPassword ||
-    // !targetPort ||
-    // !targetUsername
+    !sourceUsername ||
+    !targetDbname ||
+    !targetHost ||
+    !targetUsername ||
+    !targetPort ||
+    !targetAccessKeyId ||
+    !targetSecretAccessKey ||
+    !targetAccountId
   ) {
     throw new Error('Missing environment variables');
   }
 
-  const pgDumpOutput = execSync(
-    `PGPASSWORD="${sourcePassword}" pg_dump --host=${sourceHost} --username=${sourceUsername} --port=${sourcePort} --dbname=${sourceDbname} --file=db_dump.dump --format=c --verbose`,
-    { encoding: 'utf-8' },
-  );
+  const sourceSigner = new Signer({
+    hostname: sourceHost,
+    port: 5432,
+    region: 'us-east-1',
+    username: sourceUsername,
+  });
+
+  const sourcePassword = await sourceSigner.getAuthToken();
+  const dumpCommand = [
+    `PGPASSWORD="${sourcePassword}"`,
+    'pg_dump',
+    '--no-privileges',
+    '--no-owner',
+    `--host=${sourceHost}`,
+    `--username=${sourceUsername}`,
+    `--port=${sourcePort}`,
+    `--dbname=${sourceDbname}`,
+    '--file=ts_dump.dump',
+    '--format=c',
+    '--verbose',
+  ].join(' ');
+
+  const pgDumpOutput = execSync(dumpCommand, { encoding: 'utf-8' });
   console.log(pgDumpOutput);
+
+  const targetSigner = new Signer({
+    credentials: {
+      accessKeyId: targetAccessKeyId,
+      accountId: targetAccountId,
+      secretAccessKey: targetSecretAccessKey,
+      sessionToken: targetSessionToken,
+    },
+    hostname: targetHost,
+    port: parseInt(targetPort),
+    region: 'us-east-1',
+    username: targetUsername,
+  });
+  const targetPassword = await targetSigner.getAuthToken();
+
+  const restoreCommand = [
+    `PGPASSWORD="${targetPassword}"`,
+    'pg_restore',
+    `--host ${targetHost}`,
+    `--username ${targetUsername}`,
+    `--dbname ${targetDbname}`,
+    `--port=${targetPort}`,
+    '--format=c',
+    '--verbose',
+    '--clean',
+    '--no-privileges',
+    '--no-owner',
+    'ts_dump.dump',
+  ].join(' ');
+  const restoreOutput = execSync(restoreCommand, { encoding: 'utf-8' });
+  console.log(restoreOutput);
 }
 void main();
-
-type DatabaseInfo = {
-  password: string;
-  host: string;
-  username: string;
-  port: string;
-  dbname: string;
-};
