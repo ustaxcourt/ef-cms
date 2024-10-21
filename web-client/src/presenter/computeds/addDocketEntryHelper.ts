fb4d89f9ad7ebf71ded9a3863dd5801c280fba2d
Christopher Bisom <cbisom@flexion.us>
2024-10-21 09:21:39 -0400

    10480-story: fix import

--- web-client/src/presenter/computeds/addDocketEntryHelper.ts
+++ web-client/src/presenter/computeds/addDocketEntryHelper.ts
@@ -1,6 +1,6 @@
 import { ClientApplicationContext } from '@web-client/applicationContext';
 import { Get } from 'cerebral';
-import { INTERNAL_DOCUMENTS_REQUIRING_OBJECTION } from '@shared/business/entities/EntityConstants';
+import { INTERNAL_DOCUMENT_TYPES_REQUIRING_OBJECTION } from '@shared/business/entities/EntityConstants';
 import { find, orderBy } from 'lodash';
 import { getOptionsForCategory } from './selectDocumentTypeHelper';
 import { state } from '@web-client/presenter/app.cerebral';
@@ -111,9 +111,9 @@ export const addDocketEntryHelper = (
   const showFilingPartiesForm = !isSystemGeneratedNoticeOfChangeOfAddress;
 
   const showObjection =
-    INTERNAL_DOCUMENTS_REQUIRING_OBJECTION.has(form.documentType) ||
+    INTERNAL_DOCUMENT_TYPES_REQUIRING_OBJECTION.has(form.documentType) ||
     (AMENDMENT_EVENT_CODES.includes(form.eventCode) &&
-      INTERNAL_DOCUMENTS_REQUIRING_OBJECTION.has(
+      INTERNAL_DOCUMENT_TYPES_REQUIRING_OBJECTION.has(
         form.previousDocument?.documentType,
       ));
 
--- web-client/src/presenter/computeds/editDocketEntryMetaHelper.ts
+++ web-client/src/presenter/computeds/editDocketEntryMetaHelper.ts
@@ -4,7 +4,7 @@ import { state } from '@web-client/presenter/app.cerebral';
 
 import { ClientApplicationContext } from '@web-client/applicationContext';
 import { Get } from 'cerebral';
-import { INTERNAL_DOCUMENTS_REQUIRING_OBJECTION } from '@shared/business/entities/EntityConstants';
+import { INTERNAL_DOCUMENT_TYPES_REQUIRING_OBJECTION } from '@shared/business/entities/EntityConstants';
 export const editDocketEntryMetaHelper = (
   get: Get,
   applicationContext: ClientApplicationContext,
@@ -42,9 +42,9 @@ export const editDocketEntryMetaHelper = (
     .formatDateString(strickenAt, 'MMDDYYYY');
 
   const showObjection =
-    INTERNAL_DOCUMENTS_REQUIRING_OBJECTION.has(form.documentType) ||
+    INTERNAL_DOCUMENT_TYPES_REQUIRING_OBJECTION.has(form.documentType) ||
     (AMENDMENT_EVENT_CODES.includes(form.eventCode) &&
-      INTERNAL_DOCUMENTS_REQUIRING_OBJECTION.has(
+      INTERNAL_DOCUMENT_TYPES_REQUIRING_OBJECTION.has(
         form.previousDocument?.documentType,
       ));
 
