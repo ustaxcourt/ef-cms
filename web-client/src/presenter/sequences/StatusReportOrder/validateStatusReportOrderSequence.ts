3e7e52079bc38596b9f250f8af59a0a4c78073bb
Christopher Bisom <cbisom@flexion.us>
2024-10-21 14:36:01 -0400

    10074-bug: remove unnecessary decorator from validateStatusReportOrderSequence

--- web-client/src/presenter/sequences/StatusReportOrder/validateStatusReportOrderSequence.ts
+++ web-client/src/presenter/sequences/StatusReportOrder/validateStatusReportOrderSequence.ts
@@ -1,16 +1,13 @@
 import { clearAlertsAction } from '@web-client/presenter/actions/clearAlertsAction';
 import { setValidationAlertErrorsAction } from '@web-client/presenter/actions/setValidationAlertErrorsAction';
 import { setValidationErrorsAction } from '../../actions/setValidationErrorsAction';
-import { showProgressSequenceDecorator } from '../../utilities/showProgressSequenceDecorator';
 import { validateStatusReportOrderFormAction } from '../../actions/StatusReportOrder/validateStatusReportOrderFormAction';
 
 export const validateStatusReportOrderSequence = [
-  showProgressSequenceDecorator([
-    clearAlertsAction,
-    validateStatusReportOrderFormAction,
-    {
-      error: [setValidationErrorsAction, setValidationAlertErrorsAction],
-      success: [clearAlertsAction],
-    },
-  ]),
+  clearAlertsAction,
+  validateStatusReportOrderFormAction,
+  {
+    error: [setValidationErrorsAction, setValidationAlertErrorsAction],
+    success: [clearAlertsAction],
+  },
 ] as unknown as () => void;
