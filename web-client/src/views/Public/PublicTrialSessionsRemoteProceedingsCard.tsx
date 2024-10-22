716941e4e5207db5ccccaa63ed8459dfac90aed0
John Cruz <jcruz@flexion.us>
2024-10-22 14:51:28 -0600

    10460: Text align left for Card;

--- web-client/src/views/Public/PublicTrialSessionsRemoteProceedingsCard.tsx
+++ web-client/src/views/Public/PublicTrialSessionsRemoteProceedingsCard.tsx
@@ -11,15 +11,18 @@ export const PublicTrialSessionsRemoteProceedingsCard = connect(
           <div className="card-header padding-left-2 padding-top-2 border-bottom-1px border-base-lighter">
             <h2>Remote Proceedings</h2>
           </div>
-          <div className="card-content">
+          <div className="card-content ">
             <div className="padding-left-2 margin-bottom-2">
               <Button
-                className="padding-bottom-0"
+                className="padding-bottom-0 text-left"
                 link="https://www.ustaxcourt.gov/remote_proceedings.html"
               >
                 Public Access to Remote Proceedings
               </Button>
-              <Button link="https://www.ustaxcourt.gov/zoomgov.html">
+              <Button
+                className="text-left"
+                link="https://www.ustaxcourt.gov/zoomgov.html"
+              >
                 Zoomgov Proceedings Resources
               </Button>
             </div>
