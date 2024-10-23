debb8ee5793194e60834666d1fdac4105035e2b9
John Cruz <jcruz@flexion.us>
2024-10-23 11:49:52 -0600

    10460: Added more properties to the helper;

--- web-client/src/presenter/computeds/Public/publicTrialSessionHelper.test.ts
+++ web-client/src/presenter/computeds/Public/publicTrialSessionHelper.test.ts
@@ -21,4 +21,94 @@ describe('publicTrialSessionsHelper', () => {
 
     expect(fetchedDateString).toBe('10/22/24 12:00 AM Eastern');
   });
+
+  it('should return the "sessiontTypeOptions" value correctly', () => {
+    const { sessionTypeOptions } = runCompute(publicTrialSessionsHelper, {
+      state: {
+        FetchedTrialSessions: TEST_TIME,
+      },
+    });
+
+    expect(sessionTypeOptions).toEqual([
+      {
+        label: 'Regular',
+        value: 'Regular',
+      },
+      {
+        label: 'Small',
+        value: 'Small',
+      },
+      {
+        label: 'Hybrid',
+        value: 'Hybrid',
+      },
+      {
+        label: 'Hybrid-S',
+        value: 'Hybrid-S',
+      },
+      {
+        label: 'Special',
+        value: 'Special',
+      },
+      {
+        label: 'Motion/Hearing',
+        value: 'Motion/Hearing',
+      },
+    ]);
+  });
+
+  it('should return the "trialCitiesByState" value correctly', () => {
+    const { trialCitiesByState } = runCompute(publicTrialSessionsHelper, {
+      state: {
+        FetchedTrialSessions: TEST_TIME,
+      },
+    });
+
+    expect(trialCitiesByState).toBeDefined();
+  });
+
+  it('should return the "trialSessionJudgeOptions" value correctly', () => {
+    const { trialSessionJudgeOptions } = runCompute(publicTrialSessionsHelper, {
+      state: {
+        FetchedTrialSessions: TEST_TIME,
+        judges: [
+          { name: 'TEST_JUDGE_1', userId: '1' },
+          { name: 'TEST_JUDGE_2', userId: '2' },
+          { name: 'TEST_JUDGE_3', userId: '3' },
+          { name: 'TEST_JUDGE_4', userId: '4' },
+        ],
+      },
+    });
+
+    expect(trialSessionJudgeOptions).toEqual([
+      {
+        label: 'TEST_JUDGE_1',
+        value: {
+          name: 'TEST_JUDGE_1',
+          userId: '1',
+        },
+      },
+      {
+        label: 'TEST_JUDGE_2',
+        value: {
+          name: 'TEST_JUDGE_2',
+          userId: '2',
+        },
+      },
+      {
+        label: 'TEST_JUDGE_3',
+        value: {
+          name: 'TEST_JUDGE_3',
+          userId: '3',
+        },
+      },
+      {
+        label: 'TEST_JUDGE_4',
+        value: {
+          name: 'TEST_JUDGE_4',
+          userId: '4',
+        },
+      },
+    ]);
+  });
 });
--- web-client/src/presenter/computeds/Public/publicTrialSessionsHelper.ts
+++ web-client/src/presenter/computeds/Public/publicTrialSessionsHelper.ts
@@ -1,19 +1,55 @@
 import { Get } from 'cerebral';
+import { SESSION_TYPES } from '@shared/business/entities/EntityConstants';
+import { getTrialCitiesGroupedByState } from '@shared/business/utilities/trialSession/trialCitiesGroupedByState';
 import { state } from '@web-client/presenter/app-public.cerebral';
 
 export type PublicTrialSessionsHelperResults = {
   fetchedDateString: string;
+  sessionTypeOptions: {
+    label: string;
+    value: string;
+  }[];
+  trialCitiesByState: {
+    label: string;
+    options: { label: string; value: string }[];
+  }[];
+  trialSessionJudgeOptions: {
+    label: string;
+    value: {
+      name: string;
+      userId: string;
+    };
+  }[];
 };
 
 export const publicTrialSessionsHelper = (
   get: Get,
 ): PublicTrialSessionsHelperResults => {
   const fetchedTrialSessions = get(state['FetchedTrialSessions']);
+  const trialSessionJudges = get(state.judges) || [];
+
   const fetchedDateString = fetchedTrialSessions.toFormat(
     "MM/dd/yy hh:mm a 'Eastern'",
   );
 
+  const sessionTypeOptions = Object.values(SESSION_TYPES).map(sessionType => ({
+    label: sessionType,
+    value: sessionType,
+  }));
+
+  const trialCitiesByState = getTrialCitiesGroupedByState();
+
+  const trialSessionJudgeOptions = trialSessionJudges.map(
+    trialSessionJudge => ({
+      label: trialSessionJudge.name,
+      value: { name: trialSessionJudge.name, userId: trialSessionJudge.userId },
+    }),
+  );
+
   return {
     fetchedDateString,
+    sessionTypeOptions,
+    trialCitiesByState,
+    trialSessionJudgeOptions,
   };
 };
