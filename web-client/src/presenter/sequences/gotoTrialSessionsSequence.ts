b56eaf087b1d734f25b521364631cae64f95c962
Javis Sullivan <jsullivan@flexion.us>
2024-10-24 18:10:48 -0400

    10408: add special session copy notes to trialsessions page

--- web-client/src/presenter/sequences/gotoTrialSessionsSequence.ts
+++ web-client/src/presenter/sequences/gotoTrialSessionsSequence.ts
@@ -1,6 +1,7 @@
 import { TrialSessionsFilters } from '@web-client/presenter/state/trialSessionsPageState';
 import { clearErrorAlertsAction } from '../actions/clearErrorAlertsAction';
 import { closeMobileMenuAction } from '../actions/closeMobileMenuAction';
+import { getBulkSpecialTrialSessionCopyNotesAction } from '../actions/TrialSession/getBulkSpecialTrialSessionCopyNotesAction';
 import { getJudgeForCurrentUserAction } from '../actions/getJudgeForCurrentUserAction';
 import { getNotificationsAction } from '../actions/getNotificationsAction';
 import { getTrialSessionsAction } from '../actions/TrialSession/getTrialSessionsAction';
@@ -8,6 +9,7 @@ import { getUsersInSectionAction } from '../actions/getUsersInSectionAction';
 import { parallel } from 'cerebral/factories';
 import { resetTrialSessionsFiltersAction } from '@web-client/presenter/actions/TrialSession/resetTrialSessionsFiltersAction';
 import { setAllAndCurrentJudgesAction } from '../actions/setAllAndCurrentJudgesAction';
+import { setBulkSpecialTrialSessionCopyNotesAction } from '../actions/TrialSession/setBulkSpecialTrialSessionCopyNotesAction';
 import { setJudgeUserAction } from '../actions/setJudgeUserAction';
 import { setNotificationsAction } from '../actions/setNotificationsAction';
 import { setTrialSessionsFiltersAction } from '@web-client/presenter/actions/TrialSession/setTrialSessionsFiltersAction';
@@ -31,5 +33,9 @@ export const gotoTrialSessionsSequence =
         setAllAndCurrentJudgesAction,
       ],
     ]),
+    [
+      getBulkSpecialTrialSessionCopyNotesAction,
+      setBulkSpecialTrialSessionCopyNotesAction,
+    ],
     setupCurrentPageAction('TrialSessions'),
   ]) as unknown as (props: ActionProps<Partial<TrialSessionsFilters>>) => void;
