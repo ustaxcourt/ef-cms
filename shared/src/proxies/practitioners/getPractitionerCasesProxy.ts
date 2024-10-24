147c650cc12e92943a37dffcadebc54acb8d2961
Christopher Bisom <cbisom@flexion.us>
2024-10-24 16:02:15 -0400

    10261-story: UserContact to type instead of interface, and type getPractitionerCasesInteractor

--- shared/src/proxies/practitioners/getPractitionerCasesProxy.ts
+++ shared/src/proxies/practitioners/getPractitionerCasesProxy.ts
@@ -1,9 +1,13 @@
+import { PractitionerCaseDetail } from '@web-client/presenter/state';
 import { get } from '../requests';
 
 export const getPractitionerCasesInteractor = (
   applicationContext,
   { userId },
-) => {
+): Promise<{
+  openCases: PractitionerCaseDetail[];
+  closedCases: PractitionerCaseDetail[];
+}> => {
   return get({
     applicationContext,
     endpoint: `/practitioners/${userId}/case-list`,
