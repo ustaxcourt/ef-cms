932afcb33cae410a4dcfa1eacace416352b468d8
John Cruz <jcruz@flexion.us>
2024-10-25 10:59:05 -0600

    10460: Separate into NonMobile and Mobile;

--- web-client/src/views/Public/TrialsSessions/PublicTrialSessions.tsx
+++ web-client/src/views/Public/TrialsSessions/PublicTrialSessions.tsx
@@ -1,6 +1,8 @@
 import { BigHeader } from '@web-client/views/BigHeader';
 import { Button } from '@web-client/ustc-ui/Button/Button';
+import { Mobile, NonMobile } from '@web-client/ustc-ui/Responsive/Responsive';
 import { PublicTrialSessionsFilters } from '@web-client/views/Public/TrialsSessions/PublicTrialSessionsFilters';
+import { PublicTrialSessionsHelperResults } from '@web-client/presenter/computeds/Public/publicTrialSessionsHelper';
 import { PublicTrialSessionsRemoteProceedingsCard } from '@web-client/views/Public/TrialsSessions/PublicTrialSessionsRemoteProceedingsCard';
 import { PublicTrialSessionsTable } from '@web-client/views/Public/TrialsSessions/PublicTrialSessionsTable';
 import { connect } from '@web-client/presenter/shared.cerebral';
@@ -8,6 +10,24 @@ import { sequences, state } from '@web-client/presenter/app-public.cerebral';
 import React from 'react';
 
 const ROOT = 'publicTrialSessionData';
+
+type TrialsSessionsUiParams = {
+  publicTrialSessionsHelper: PublicTrialSessionsHelperResults;
+  resetPublicTrialSessionDataSequence: () => void;
+  publicTrialSessionData: {
+    judges?: {
+      [key: string]: string;
+    };
+    locations?: {
+      [key: string]: string;
+    };
+    sessionTypes?: {
+      [key: string]: string;
+    };
+    pageNumber?: number;
+    proceedingType?: string;
+  };
+};
 
 export const PublicTrialSessions = connect(
   {
@@ -24,37 +44,64 @@ export const PublicTrialSessions = connect(
     return (
       <>
         <BigHeader text="Scheduled Trial Sessions" />
-        <section className="usa-section grid-container">
-          <div className="grid-row">
-            <div className="tablet:grid-col-8 grid-col-12 padding-top-2">
-              <PublicTrialSessionsFilters
-                ROOT={ROOT}
-              ></PublicTrialSessionsFilters>
-            </div>
-            <div className="tablet:grid-col-4 grid-col-12 padding-top-1">
-              <PublicTrialSessionsRemoteProceedingsCard></PublicTrialSessionsRemoteProceedingsCard>
-            </div>
-          </div>
-          <div className="grid-row">
-            <Button
-              link
-              disabled={!publicTrialSessionsHelper.filtersHaveBeenModified}
-              onClick={() => resetPublicTrialSessionDataSequence()}
-            >
-              Reset Filters
-            </Button>
-          </div>
-          <div className="grid-row padding-top-1">
-            <PublicTrialSessionsTable
-              ROOT={ROOT}
-              pageNumber={publicTrialSessionData.pageNumber || 0}
-              totalPages={publicTrialSessionsHelper.totalPages}
-            ></PublicTrialSessionsTable>
-          </div>
-        </section>
+
+        {NonMobilePublicTrialsSessions({
+          publicTrialSessionData,
+          publicTrialSessionsHelper,
+          resetPublicTrialSessionDataSequence,
+        })}
+        {MobilePublicTrialsSessions()}
       </>
     );
   },
 );
+
+function NonMobilePublicTrialsSessions({
+  publicTrialSessionData,
+  publicTrialSessionsHelper,
+  resetPublicTrialSessionDataSequence,
+}: TrialsSessionsUiParams) {
+  return (
+    <NonMobile>
+      <section className="usa-section grid-container">
+        <div className="grid-row">
+          <div className="tablet:grid-col-8 grid-col-12 padding-top-2">
+            <PublicTrialSessionsFilters
+              ROOT={ROOT}
+            ></PublicTrialSessionsFilters>
+          </div>
+          <div className="tablet:grid-col-4 grid-col-12 padding-top-1">
+            <PublicTrialSessionsRemoteProceedingsCard></PublicTrialSessionsRemoteProceedingsCard>
+          </div>
+        </div>
+        <div className="grid-row">
+          <Button
+            link
+            disabled={!publicTrialSessionsHelper.filtersHaveBeenModified}
+            onClick={() => resetPublicTrialSessionDataSequence()}
+          >
+            Reset Filters
+          </Button>
+        </div>
+        <div className="grid-row padding-top-1">
+          <PublicTrialSessionsTable
+            ROOT={ROOT}
+            pageNumber={publicTrialSessionData.pageNumber || 0}
+            totalPages={publicTrialSessionsHelper.totalPages}
+          ></PublicTrialSessionsTable>
+        </div>
+      </section>
+    </NonMobile>
+  );
+}
+function MobilePublicTrialsSessions() {
+  return (
+    <Mobile>
+      <section className="usa-section grid-container">
+        John is testing mobile
+      </section>
+    </Mobile>
+  );
+}
 
 PublicTrialSessions.displayName = 'PublicTrialSessions';
