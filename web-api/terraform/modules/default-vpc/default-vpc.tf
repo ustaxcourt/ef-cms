16ba9f829cfa72636a27b9d655af82c72ec4274d
Kaitlyn Swann <kswann@flexion.us>
2024-10-29 14:14:09 -0600

    10391: add default vpc and security group settings

---
+++ web-api/terraform/modules/default-vpc/default-vpc.tf
@@ -0,0 +1,28 @@
+resource "aws_default_vpc" "default" {
+}
+
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 5432
+    to_port          = 5432
+    protocol         = "tcp"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
