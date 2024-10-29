45d9c1c41479218b76f5cc9b77faf7e1d9bb775a
John Cruz <jcruz@flexion.us>
2024-10-29 11:23:39 -0600

    10460: Fix typing in unit test;

--- web-client/src/presenter/actions/resetPublicTrialSessionDataAction.test.ts
+++ web-client/src/presenter/actions/resetPublicTrialSessionDataAction.test.ts
@@ -1,13 +1,17 @@
+import { PublicClientState } from '@web-client/presenter/state-public';
 import { resetPublicTrialSessionDataAction } from '@web-client/presenter/actions/resetPublicTrialSessionDataAction';
 import { runAction } from '@web-client/presenter/test.cerebral';
 
 describe('resetPublicTrialSessionDataAction', () => {
   it('should reset trial session data', async () => {
-    const result = await runAction(resetPublicTrialSessionDataAction, {
-      state: {
-        publicTrialSessionData: { id: 123 },
+    const result = await runAction<void, PublicClientState>(
+      resetPublicTrialSessionDataAction,
+      {
+        state: {
+          publicTrialSessionData: { id: 123 },
+        },
       },
-    });
+    );
 
     expect(result.state.publicTrialSessionData).toEqual({});
   });
