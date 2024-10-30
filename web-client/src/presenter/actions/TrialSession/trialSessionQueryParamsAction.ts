7df36acb1f98fa3cc722a7b120122f3321bf91b8
Nechama Krigsman <nkrigsman@flexion.us>
2024-10-30 13:03:35 -0400

    10535: add tests around trialSessionQueryParamsAction;

--- web-client/src/presenter/actions/TrialSession/trialSessionQueryParamsAction.ts
+++ web-client/src/presenter/actions/TrialSession/trialSessionQueryParamsAction.ts
@@ -1,19 +1,20 @@
 import { state } from '@web-client/presenter/app.cerebral';
 
-export const transformTrialSessionQueryParamsAction = ({
+export const trialSessionQueryParamsAction = ({
   get,
   props,
+  store,
 }: ActionProps) => {
   if (props.judgeId) {
     const judges = get(state.legacyAndCurrentJudges);
     const currentJudge = judges.find(judge => judge.userId === props.judgeId);
     if (currentJudge) {
-      return {
-        judges: {
-          action: 'add',
-          judge: { name: currentJudge.name, userId: currentJudge.userId },
+      store.set(state.trialSessionsPage.filters.judges, {
+        [currentJudge.userId]: {
+          name: currentJudge.name,
+          userId: currentJudge.userId,
         },
-      };
+      });
     }
   }
 };
