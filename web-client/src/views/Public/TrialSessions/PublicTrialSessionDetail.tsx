2703eef8fba6e407f1a46c2b34f010928399f7ac
Christopher Bisom <cbisom@flexion.us>
2024-10-30 18:12:58 -0400

    10461-story: refactor PublicTrialSessionDetail to avoid unnecessary divs and classes

--- web-client/src/views/Public/TrialSessions/PublicTrialSessionDetail.tsx
+++ web-client/src/views/Public/TrialSessions/PublicTrialSessionDetail.tsx
@@ -47,66 +47,44 @@ export const PublicTrialSessionInformation = connect(
   }) {
     return (
       <>
-        <div className="grid-container padding-x-0">
-          <div className="grid-row">
-            <div className="grid-col-auto">
-              <h1>Session Information</h1>
-            </div>
-          </div>
-        </div>
-        <div className="margin-bottom-4">
-          <div className="grid-container padding-x-0">
-            <div className="grid-row grid-gap margin-bottom-2">
-              <div className="grid-col-6">
-                <div className="card trial-session-card">
-                  <div className="content-wrapper">
-                    <h3 className="underlined">Details</h3>
-                    <div className="display-flex padding-x-0 padding-top-1 padding-bottom-1">
-                      <div>
-                        <p className="label">Courthouse location</p>
-                        <div className="padding-05"></div>
-                        <div>
-                          <p>
-                            <span>{trialSession.courthouseName}</span>
-                            <span className="address-line">
-                              {trialSession.address1}
-                            </span>
-                            <span className="address-line">
-                              {trialSession.address2}
-                            </span>
-                            <span className="address-line">
-                              {
-                                publicTrialSessionDetailHelper
-                                  .formattedTrialSession.formattedCityStateZip
-                              }
-                            </span>
-                          </p>
-                        </div>
-                      </div>
-                      {trialSession.isSwingSession && (
-                        <div>
-                          <p className="label">Swing session</p>
-                          <div className="padding-05"></div>
-                          <span className="display-flex gap-1 flex-align-center">
-                            <FontAwesomeIcon
-                              className="fa-icon-blue"
-                              icon="link"
-                              size="sm"
-                              title="swing session"
-                            />
-                            <a
-                              href={`/trial-session-detail/${trialSession.swingSessionId}`}
-                            >
-                              {trialSession.swingSessionLocation}
-                            </a>
-                          </span>
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                </div>
+        <h1>Session Information</h1>
+        <div className="card padding-205 maxw-mobile-lg">
+          <h3 className="underlined">Details</h3>
+          <div className="display-flex flex-wrap gap-3">
+            <div>
+              <span className="label">Courthouse location</span>
+              <div className="padding-05"></div>
+              <div>
+                <span>{trialSession.courthouseName}</span>
+                <span className="address-line">{trialSession.address1}</span>
+                <span className="address-line">{trialSession.address2}</span>
+                <span className="address-line">
+                  {
+                    publicTrialSessionDetailHelper.formattedTrialSession
+                      .formattedCityStateZip
+                  }
+                </span>
               </div>
             </div>
+            {trialSession.isSwingSession && (
+              <div>
+                <span className="label">Swing session</span>
+                <div className="padding-05"></div>
+                <span className="display-flex gap-1 flex-align-center">
+                  <FontAwesomeIcon
+                    className="fa-icon-blue"
+                    icon="link"
+                    size="sm"
+                    title="swing session"
+                  />
+                  <a
+                    href={`/trial-session-detail/${trialSession.swingSessionId}`}
+                  >
+                    {trialSession.swingSessionLocation}
+                  </a>
+                </span>
+              </div>
+            )}
           </div>
         </div>
       </>
