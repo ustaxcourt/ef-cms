8a12158a912d0a85ce2b89b93de8ffc683e22d18
Christopher Bisom <cbisom@flexion.us>
2024-10-30 14:08:15 -0400

    10461-story: get swing session information for a trial; fix typing of existing gotoTrialSessionDetailSequence

--- web-api/src/business/useCases/trialSessions/getPublicTrialSessionDetailsInteractor.ts
+++ web-api/src/business/useCases/trialSessions/getPublicTrialSessionDetailsInteractor.ts
@@ -5,15 +5,23 @@ import { TrialSession } from '../../../../../shared/src/business/entities/trialS
 
 type PublicTrialSessionDetails = {
   openCases: any[];
+  isRemote: boolean;
   isSwingSession: boolean;
   swingSessionId?: string;
-  swingSessionLocation: string;
-  isRemote: boolean;
+  swingSessionLocation?: string; // The location of the related swing session, if there is one
+  trialLocation?: string; // The location of this particular session
+  startDate: string;
+  estimatedEndDate?: string;
+  term: string;
+  sessionStatus: string;
 };
 
 export const getPublicTrialSessionDetailsInteractor = async (
   applicationContext: ServerApplicationContext,
-  { trialSessionId }: { trialSessionId: string },
+  {
+    addSwingSessionDetails = true,
+    trialSessionId,
+  }: { trialSessionId: string; addSwingSessionDetails: boolean },
 ): Promise<PublicTrialSessionDetails> => {
   console.log('getPublicTrialSessionDetailsInteractor', trialSessionId);
   const trialSessionDetails = await applicationContext
@@ -38,21 +46,38 @@ export const getPublicTrialSessionDetailsInteractor = async (
       docketNumbers: fullTrialSessionEntity.caseOrder.map(c => c.docketNumber),
     });
 
-  console.log('LOOK HERE!');
-  console.log(allCases);
-
   const publicTrialSessionData: PublicTrialSessionDetails = {
-    // TODO, see getTrialSessionDetailsAction, but why is this not in the API itself?
+    estimatedEndDate: fullTrialSessionEntity.estimatedEndDate,
     isRemote:
       fullTrialSessionEntity.sessionScope ===
       TRIAL_SESSION_SCOPE_TYPES.standaloneRemote,
-
     isSwingSession: !!fullTrialSessionEntity.swingSession,
-
     openCases: allCases.filter(c => !c.removedFromTrial),
+    sessionStatus: fullTrialSessionEntity.sessionStatus,
+    startDate: fullTrialSessionEntity.startDate,
     swingSessionId: fullTrialSessionEntity.swingSessionId,
     swingSessionLocation: '',
+    term: fullTrialSessionEntity.term,
+    trialLocation: fullTrialSessionEntity.trialLocation,
   };
+
+  // Get the details associated with this session's related swing session, if applicable
+  if (
+    fullTrialSessionEntity.swingSession &&
+    fullTrialSessionEntity.swingSessionId &&
+    addSwingSessionDetails
+  ) {
+    const relatedSwingSession = await getPublicTrialSessionDetailsInteractor(
+      applicationContext,
+      {
+        addSwingSessionDetails: false, // Avoid infinite recursion
+        trialSessionId: fullTrialSessionEntity.swingSessionId,
+      },
+    );
+    // Set swing session details here
+    publicTrialSessionData.swingSessionLocation =
+      relatedSwingSession.trialLocation;
+  }
 
   return publicTrialSessionData;
 };
