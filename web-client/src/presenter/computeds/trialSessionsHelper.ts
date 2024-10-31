ea8a8242deb32b55fff13fd3c4eafbc547603669
Zachary Rogers <zrogers>
2024-10-31 08:33:09 -0700

    10535: Add Standalone remote to beginning of list

--- web-client/src/presenter/computeds/trialSessionsHelper.ts
+++ web-client/src/presenter/computeds/trialSessionsHelper.ts
@@ -97,7 +97,7 @@ export const trialSessionsHelper = (
   }
 
   const states = getTrialCitiesGroupedByState();
-  states.push({
+  states.unshift({
     label: TRIAL_SESSION_SCOPE_TYPES.standaloneRemote,
     options: [
       {
