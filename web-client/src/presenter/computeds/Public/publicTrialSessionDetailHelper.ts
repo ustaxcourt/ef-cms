a2dc4e65fd90b994a19b2965474a3433a9caec03
Christopher Bisom <cbisom@flexion.us>
2024-10-31 10:58:45 -0400

    10461-story: WIP Getting the nonmobile table working; still need petitioner and respondent counsel information to show

--- web-client/src/presenter/computeds/Public/publicTrialSessionDetailHelper.ts
+++ web-client/src/presenter/computeds/Public/publicTrialSessionDetailHelper.ts
@@ -1,7 +1,44 @@
+import {
+  Case,
+  isInConsolidatedGroup,
+  isLeadCase,
+} from '@shared/business/entities/cases/Case';
 import { ClientPublicApplicationContext } from '@web-client/applicationContextPublic';
 import { Get } from 'cerebral';
 import { compact, some } from 'lodash';
 import { state } from '@web-client/presenter/app-public.cerebral';
+
+// 10461 TODO: can we extend getFormattedCaseDetail
+const formatPublicCase = (publicCase: any) => {
+  /*
+    consolidatedIconToolTipText X
+    inConsolidatedGroup X
+    isLeadCase X
+    shouldIndent?!!
+    docketNumber X
+    respondent.name
+    privatePractitioners .name
+    irsPractitioners .name
+    caseTitle X
+  */
+  publicCase.isLeadCase = isLeadCase(publicCase);
+  publicCase.inConsolidatedGroup = isInConsolidatedGroup(publicCase);
+
+  // 10461 TODO: copied almost verbatim from getFormattedCaseDetail, should be extracted into something more reusable
+  let consolidatedIconTooltipText;
+
+  if (publicCase.inConsolidatedGroup) {
+    if (publicCase.isLeadCase) {
+      consolidatedIconTooltipText = 'Lead case';
+    } else {
+      consolidatedIconTooltipText = 'Consolidated case';
+    }
+  }
+
+  publicCase.consolidatedIconTooltipText = consolidatedIconTooltipText;
+  publicCase.caseTitle = Case.getCaseTitle(publicCase.caseCaption);
+  return publicCase;
+};
 
 export const publicTrialSessionDetailHelper = (
   get: Get,
@@ -16,6 +53,7 @@ export const publicTrialSessionDetailHelper = (
     sessionStatus: string;
     formattedCityStateZip: string;
     hasCourthouseInformation: boolean;
+    formattedCases: any[];
   };
 } => {
   const trialSession = get(state.trialSessionDetailsPage.trialSession);
@@ -48,13 +86,22 @@ export const publicTrialSessionDetailHelper = (
     formattedCityStateZip,
   ]);
 
+  // 10461 TODO: Are they already sorted?
+  const formattedCases = Case.sortByDocketNumber(
+    trialSession.openCases.map(c => formatPublicCase(c)),
+  );
+
+  console.log('formattedCases', formattedCases);
+
   const formattedTrialSession = {
+    formattedCases,
     formattedCityStateZip,
     formattedEstimatedEndDate,
     formattedStartDate,
     formattedStartDateFull,
     formattedTerm,
     hasCourthouseInformation,
+
     sessionStatus: trialSession.sessionStatus,
     // TODO 10461: remove as needed
     trialLocation: trialSession.trialLocation,
