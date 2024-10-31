58ac8faeacb173e607f7d241061690020b93b23e
Javis Sullivan <jsullivan@flexion.us>
2024-10-31 16:07:37 -0400

    10408: types and organization

--- web-api/src/business/useCases/trialSessions/getBulkTrialSessionCopyNotesInteractor.ts
+++ web-api/src/business/useCases/trialSessions/getBulkTrialSessionCopyNotesInteractor.ts
@@ -3,7 +3,10 @@ import {
   isAuthorized,
 } from '../../../../../shared/src/authorization/authorizationClientService';
 import { ServerApplicationContext } from '@web-api/applicationContext';
-import { SpecialTrialSession } from '@shared/business/entities/trialSessions/SpeciailTrialSessions';
+import {
+  SpecialTrialSession,
+  TrialSessionWorkingCopyNotes,
+} from '@shared/business/entities/trialSessions/SpeciailTrialSessions';
 import { UnauthorizedError } from '@web-api/errors/errors';
 import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 
@@ -11,7 +14,7 @@ export const getBulkTrialSessionCopyNotesInteractor = async (
   applicationContext: ServerApplicationContext,
   { specialTrialSessions }: { specialTrialSessions: SpecialTrialSession[] },
   authorizedUser: UnknownAuthUser,
-) => {
+): Promise<Array<TrialSessionWorkingCopyNotes>> => {
   if (!isAuthorized(authorizedUser, ROLE_PERMISSIONS.TRIAL_SESSIONS)) {
     throw new UnauthorizedError('Unauthorized');
   }
