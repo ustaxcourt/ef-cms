07e02cd07bc2536d4e00d7c47bd65dc81e47aeee
Jim Lerza <james.lerza@ustaxcourt.gov>
2024-10-31 13:29:19 -0400

    10391: explicitly cd to the efcms root dir from within the get-token-for-tableplus.sh script

--- scripts/postgres/get-token-for-tableplus.sh
+++ scripts/postgres/get-token-for-tableplus.sh
@@ -11,10 +11,15 @@ done
 [[ -z "$env" ]] && [[ -n "$ENV" ]] && env="$ENV"
 [[ -z "$env" ]] && exit 1
 
+ROOT_DIR="$(readlink -f "$(dirname "$(readlink -f "$0")")/../..")"
+pushd "$ROOT_DIR"
+
 # shellcheck disable=SC1091
 source "./scripts/env/set-env.zsh" --quiet "$env"
 # shellcheck disable=SC1091
 source "./scripts/postgres/generate-token.sh" --quiet "$RW"
+
+popd
 
 [[ -z "$DB_TOKEN" ]] && exit 1
 
