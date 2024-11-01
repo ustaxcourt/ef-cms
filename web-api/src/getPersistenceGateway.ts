afc81bc5e9469ffe188ea8bcb388a5e051063411
Javis Sullivan <jsullivan@flexion.us>
2024-10-31 23:01:17 -0400

    10408: add some tests

--- web-api/src/getPersistenceGateway.ts
+++ web-api/src/getPersistenceGateway.ts
@@ -284,7 +284,7 @@ const gatewayMethods = {
   getAllUsersByRole,
   getAllWebSocketConnections,
   getBlockedCases,
-  getBulkTrialSessionWorkingCopies,
+  getBulkTrialSessionWorkingCopyNotes: getBulkTrialSessionWorkingCopies,
   getCalendaredCasesForTrialSession,
   getCaseByDocketNumber,
   getCaseDeadlinesByDateRange,
