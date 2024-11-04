b65a88a89a97262db1de1139d2e233361f803d85
Javis Sullivan <jsullivan@flexion.us>
2024-11-04 12:18:30 -0500

    10408: fix type errors

--- web-api/src/lambdas/trialSessions/getBulkTrialSessionCopyNotesLambda.ts
+++ web-api/src/lambdas/trialSessions/getBulkTrialSessionCopyNotesLambda.ts
@@ -1,10 +1,10 @@
-import { AuthUser } from '@shared/business/entities/authUser/AuthUser';
+import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { genericHandler } from '../../genericHandler';
 import { getBulkTrialSessionCopyNotesInteractor } from '@web-api/business/useCases/trialSessions/getBulkTrialSessionCopyNotesInteractor';
 
 export const getBulkTrialSessionCopyNotesLambda = (
   event,
-  authorizedUser: AuthUser,
+  authorizedUser: UnknownAuthUser,
 ) =>
   genericHandler(event, async ({ applicationContext }) => {
     const { specialTrialSessions } = JSON.parse(event.body || '{}');
