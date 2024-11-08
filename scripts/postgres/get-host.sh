d24e5180cd8669a3b11ca46ad0afbdfba407ad6e
Jim Lerza <james.lerza@ustaxcourt.gov>
2024-11-08 09:26:33 -0500

    opex: refactored get-read-write-endpoint to use the same parameters as generate-token and behave the same way. updated old references. added the DB env vars to env-for-circle.

--- scripts/postgres/get-host.sh
+++ scripts/postgres/get-host.sh
@@ -1,23 +1,53 @@
-#!/bin/bash
+#!/bin/bash -e
 
-# Returns the read-write endpoint for a given environment's RDS cluster
+# Returns the host endpoint for a given environment's RDS cluster
 
 # Parameters
-#   --yes    Presumes prior confirmation when checking environment variables
+#   --rw          Returns the writeable endpoint
+#   --yes         Presumes prior confirmation when checking environment variables
+#   --quiet       Exports the value to environment variable $DB_HOST and produces no output
+#   --succinct    Outputs only the host endpoint (not compatible with --quiet)
 
-# Usage
-#   ENV=dev ./scripts/postgres/get-read-write-endpoint.sh --yes
+# Usage examples
+#   ./scripts/postgres/get-host.sh
+#   ./scripts/postgres/get-host.sh --rw --succinct
 
 ( ! command -v jq > /dev/null ) && echo "jq must be installed on your machine." && exit 1
 
-# shellcheck disable=SC1091
-source "./scripts/helpers/prior-confirmation.sh"
+{
+  [[ -n "$ZSH_VERSION" && "$ZSH_EVAL_CONTEXT" =~ :file$ ]] ||
+  [[ -n "$BASH_VERSION" ]] && (return 0 2>/dev/null);
+} && sourced=1 || sourced=0
+[[ "$sourced" -eq 0 ]] && exit="exit" || exit="return"
+
+rw=0
+[[ -n "$CI" ]] && succinct=1 || succinct=0
+sshhh=0
+confirmed=0
+for param in "$@"; do
+  { [[ "$param" == "--rw" ]] || [[ "$param" == "-w" ]]; } && rw=1
+  { [[ "$param" == "--succinct" ]] || [[ "$param" == "-h" ]]; } && succinct=1
+  { [[ "$param" == "--quiet" ]] || [[ "$param" == "-q" ]]; } && sshhh=1
+  { [[ "$param" == "--yes" ]] || [[ "$param" == "-y" ]]; } && confirmed=1
+done
+
+[[ "$sourced" -eq 0 ]] && [[ "$sshhh" -eq 1 ]] && $exit 1
 
 CHECK_PARAMS=("ENV" "AWS_ACCESS_KEY_ID" "AWS_SECRET_ACCESS_KEY")
 
-confirmed=$(has_prior_confirmation "$@")
-{ [[ "$confirmed" -eq 1 ]] || [[ -n "$CI" ]]; } && CHECK_PARAMS+=("--quiet")
+{ [[ "$sshhh" -eq 1 ]] || [[ "$succinct" -eq 1 ]]; } && CHECK_PARAMS+=("--quiet")
+[[ "$sshhh" -eq 0 ]] && [[ "$succinct" -eq 0 ]] && [[ "$confirmed" -eq 1 ]] && CHECK_PARAMS+=("--yes")
 
 ./check-env-variables.sh "${CHECK_PARAMS[@]}"
 
-aws rds describe-db-clusters --db-cluster-identifier "${ENV}-dawson-cluster" | jq -r ".DBClusters[0].Endpoint"
+[[ "$rw" -eq 1 ]] && ep="Endpoint" || ep="ReaderEndpoint"
+
+ENDPOINT=$(aws rds describe-db-clusters --db-cluster-identifier "${ENV}-dawson-cluster" | jq -r ".DBClusters[0].${ep}")
+
+if [[ -z "$ENDPOINT" ]]; then
+    [[ "$sshhh" -eq 0 ]] && echo -e "\nError: Unable to determine ${ep}."
+    $exit 1
+fi
+
+# shellcheck disable=SC2015
+[[ "$sshhh" -eq 0 ]] && echo "$ENDPOINT" || export DB_HOST="$ENDPOINT"
