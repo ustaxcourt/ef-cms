6bfea1aac1c99a3159c5417ee71b03cf39ceecf2
Nechama Krigsman <nkrigsman@flexion.us>
2024-11-13 12:56:30 -0500

    10525: Default sorting by filed date;

--- web-client/src/presenter/actions/setDefaultDocketEntriesTableSortAction.ts
+++ web-client/src/presenter/actions/setDefaultDocketEntriesTableSortAction.ts
@@ -4,6 +4,9 @@ import { state } from '@web-client/presenter/app.cerebral';
 export const setDefaultDocketEntriesTableSortAction = ({
   store,
 }: ActionProps) => {
-  store.set(state[KEYS.DOCKET_RECORD_TABLE_SORT].sortField, 'index');
+  store.set(
+    state[KEYS.DOCKET_RECORD_TABLE_SORT].sortField,
+    'sortingFilingDate',
+  );
   store.set(state[KEYS.DOCKET_RECORD_TABLE_SORT].sortOrder, 'asc');
 };
