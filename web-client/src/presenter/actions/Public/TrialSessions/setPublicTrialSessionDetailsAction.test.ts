5c876532040851400157aecce071d764a73c68fe
Christopher Bisom <cbisom@flexion.us>
2024-11-13 16:23:38 -0500

    10461-story: fix type errors

--- web-client/src/presenter/actions/Public/TrialSessions/setPublicTrialSessionDetailsAction.test.ts
+++ web-client/src/presenter/actions/Public/TrialSessions/setPublicTrialSessionDetailsAction.test.ts
@@ -9,7 +9,6 @@ describe('setPublicTrialSessionDetailsAction', () => {
       address1: '123 Main St',
       calendaredCases: [],
       city: 'San Francisco',
-      estimatedEndDate: '2020-11-29T05:00:00.000Z',
       postalCode: '94535',
       startDate: '2020-11-27T05:00:00.000Z',
       state: 'CA',
