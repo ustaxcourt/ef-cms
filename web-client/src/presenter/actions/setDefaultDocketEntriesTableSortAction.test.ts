9c1ca275e1c3132f29cc3d2565231ac0a4deaa34
John Cruz <jcruz@flexion.us>
2024-11-13 09:15:51 -0700

    10525: Change name of action;

--- web-client/src/presenter/actions/setDefaultDocketEntriesTableSortAction.test.ts
+++ web-client/src/presenter/actions/setDefaultDocketEntriesTableSortAction.test.ts
@@ -1,9 +1,9 @@
-import { clearDocketRecordTableSortAction } from '@web-client/presenter/actions/clearDocketRecordTableSortAction';
 import { runAction } from '@web-client/presenter/test.cerebral';
+import { setDefaultDocketEntriesTableSortAction } from '@web-client/presenter/actions/setDefaultDocketEntriesTableSortAction';
 
 describe('clearDocketRecordTableSortAction', () => {
   it('should clear docket record table sort', async () => {
-    const { state } = await runAction(clearDocketRecordTableSortAction, {
+    const { state } = await runAction(setDefaultDocketEntriesTableSortAction, {
       state: {
         DOCKET_RECORD_TABLE_SORT: {
           sortField: 'TEST_sortField',
---
+++ web-client/src/presenter/actions/setDefaultDocketEntriesTableSortAction.ts
@@ -0,0 +1,9 @@
+import { KEYS } from '@shared/business/entities/EntityConstants';
+import { state } from '@web-client/presenter/app.cerebral';
+
+export const setDefaultDocketEntriesTableSortAction = ({
+  store,
+}: ActionProps) => {
+  store.set(state[KEYS.DOCKET_RECORD_TABLE_SORT].sortField, 'index');
+  store.set(state[KEYS.DOCKET_RECORD_TABLE_SORT].sortOrder, 'asc');
+};
