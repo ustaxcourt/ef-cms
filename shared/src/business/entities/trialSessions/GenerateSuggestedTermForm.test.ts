9973cf6846795578ff1eb53e617c620352533447
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-11-13 12:14:38 -0600

    10275: update GenerateSuggestedTermForm validation rules

--- shared/src/business/entities/trialSessions/GenerateSuggestedTermForm.test.ts
+++ shared/src/business/entities/trialSessions/GenerateSuggestedTermForm.test.ts
@@ -1,4 +1,5 @@
 import { GenerateSuggestedTermForm } from './GenerateSuggestedTermForm';
+import { yesterdayFormatted } from '@shared/business/entities/courtIssuedDocument/CourtIssuedDocumentConstants';
 
 describe('GenerateSuggestTermForm', () => {
   describe('validation', () => {
@@ -20,6 +21,33 @@ describe('GenerateSuggestTermForm', () => {
         termEndDate: 'Enter date in format MM/DD/YYYY.',
         termName: 'Enter a term name',
         termStartDate: 'Enter date in format MM/DD/YYYY.',
+      });
+    });
+
+    it('should fail validation when end date is prior to start date', () => {
+      const formEntity = new GenerateSuggestedTermForm({
+        termEndDate: '01/01/2050',
+        termName: 'Test Term',
+        termStartDate: '03/31/2050',
+      });
+
+      expect(formEntity.isValid()).toBeFalsy();
+      expect(formEntity.getFormattedValidationErrors()).toEqual({
+        termEndDate:
+          'End date cannot be prior to start date. Enter a valid end date.',
+      });
+    });
+
+    it('should fail validation when start date is in the past', () => {
+      const formEntity = new GenerateSuggestedTermForm({
+        termEndDate: '01/01/2050',
+        termName: 'Test Term',
+        termStartDate: yesterdayFormatted,
+      });
+
+      expect(formEntity.isValid()).toBeFalsy();
+      expect(formEntity.getFormattedValidationErrors()).toEqual({
+        termStartDate: 'Start date cannot be in the past. Enter a valid date.',
       });
     });
   });
