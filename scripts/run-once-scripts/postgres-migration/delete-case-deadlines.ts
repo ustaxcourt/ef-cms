e387197b0bbc8633dea99496b121b924bc7bded0
Christopher Bisom <cbisom@flexion.us>
2024-11-13 11:46:11 -0500

    10504-dxox: add deletition scripts, add validation for judge id to CaseWorksheet.ts

---
+++ scripts/run-once-scripts/postgres-migration/delete-case-deadlines.ts
@@ -0,0 +1,62 @@
+/**
+ * HOW TO RUN
+ *
+ * TABLE_NAME=testing npx ts-node --transpileOnly scripts/postgres/delete-case-deadlines.ts
+ */
+
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { requireEnvVars } from '../../../shared/admin-tools/util';
+import { getDbReader } from '../../../web-api/src/database';
+import { isEmpty } from 'lodash';
+import { batchDeleteDynamoItems } from './batch-delete-dynamo-items';
+
+const caseDeadlinePageSize = 10000;
+const dynamoDbClient = new DynamoDBClient({ region: 'us-east-1' });
+const dynamoDbDocClient = DynamoDBDocumentClient.from(dynamoDbClient);
+
+requireEnvVars(['TABLE_NAME']);
+
+const tableNameInput = process.env.TABLE_NAME!;
+
+const getCaseDeadlinesToDelete = async (offset: number) => {
+  const caseDeadlines = await getDbReader(reader =>
+    reader
+      .selectFrom('dwCaseDeadline')
+      .select(['docketNumber', 'caseDeadlineId'])
+      .orderBy('caseDeadlineId')
+      .limit(caseDeadlinePageSize)
+      .offset(offset)
+      .execute(),
+  );
+  return caseDeadlines;
+};
+
+let totalItemsDeleted = 0;
+
+async function main() {
+  let offset = 0;
+  let caseDeadlinesToDelete = await getCaseDeadlinesToDelete(offset);
+
+  while (!isEmpty(caseDeadlinesToDelete)) {
+    const dynamoItemsToDelete = caseDeadlinesToDelete.map(cd => ({
+      DeleteRequest: {
+        Key: {
+          pk: `case|${cd.docketNumber}`,
+          sk: `case-deadline|${cd.caseDeadlineId}`,
+        },
+      },
+    }));
+    totalItemsDeleted += await batchDeleteDynamoItems(
+      dynamoItemsToDelete,
+      dynamoDbDocClient,
+      tableNameInput,
+    );
+    console.log(`Total case deadlines deleted: ${totalItemsDeleted}`);
+    offset += caseDeadlinePageSize;
+    caseDeadlinesToDelete = await getCaseDeadlinesToDelete(offset);
+  }
+  console.log('Done deleting case deadlines from Dynamo');
+}
+
+main().catch(console.error);
