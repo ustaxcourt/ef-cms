6e1098e982333f53d5d8a8e228f5bca6ca7f2e58
Christopher Bisom <cbisom@flexion.us>
2024-12-03 14:18:22 -0500

    10504-dxox: update delete script

--- scripts/run-once-scripts/postgres-migration/delete-case-correspondences.ts
+++ scripts/run-once-scripts/postgres-migration/delete-case-correspondences.ts
@@ -1,23 +1,21 @@
 /**
  * HOW TO RUN
- *
  * TABLE_NAME=testing npx ts-node --transpileOnly scripts/run-once-scripts/postgres-migration/delete-case-correspondences.ts
  */
 
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { requireEnvVars } from '../../../shared/admin-tools/util';
 import { getDbReader } from '../../../web-api/src/database';
 import { isEmpty } from 'lodash';
 import { batchDeleteDynamoItems } from './batch-delete-dynamo-items';
+import { environment } from '../../../web-api/src/environment';
 
 const caseCorrespondencePageSize = 10000;
 const dynamoDbClient = new DynamoDBClient({ region: 'us-east-1' });
 const dynamoDbDocClient = DynamoDBDocumentClient.from(dynamoDbClient);
 
-requireEnvVars(['TABLE_NAME']);
-
-const tableNameInput = process.env.TABLE_NAME!;
+// We set the environment as 'production' (= "a deployed environment") to get the RDS connection to work properly
+environment.nodeEnv = 'production';
 
 const getCaseCorrespondencesToDelete = async (offset: number) => {
   const caseCorrespondences = await getDbReader(reader =>
@@ -51,7 +49,7 @@ async function main() {
     totalItemsDeleted += await batchDeleteDynamoItems(
       dynamoItemsToDelete,
       dynamoDbDocClient,
-      tableNameInput,
+      environment.dynamoDbTableName,
     );
     console.log(
       `Total case correspondences deleted so far: ${totalItemsDeleted}`,
