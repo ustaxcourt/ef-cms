c2bb36a00e7fbf2ee8094271e9f06d5bd217b13a
Nechama Krigsman <nkrigsman@flexion.us>
2024-12-03 15:22:21 -0500

    10553: display error message when user attempts to verify email when not logged in;

--- web-client/src/presenter/sequences/gotoVerifyEmailSequence.ts
+++ web-client/src/presenter/sequences/gotoVerifyEmailSequence.ts
@@ -1,13 +1,18 @@
 import { clearUserAction } from '../actions/clearUserAction';
 import { navigateToLoginAction } from '@web-client/presenter/actions/Login/navigateToLoginAction';
+import { refreshTokenAction } from '@web-client/presenter/actions/Login/refreshTokenAction';
 import { setAlertInfoAction } from '@web-client/presenter/actions/setAlertInfoAction';
+import { setNotLoggedInVerificationAction } from '@web-client/presenter/actions/setNotLoggedInVerificationAction';
 import { startWebSocketConnectionSequenceDecorator } from '@web-client/presenter/utilities/startWebSocketConnectionSequenceDecorator';
 import { verifyUserPendingEmailAction } from '../actions/verifyUserPendingEmailAction';
 
 export const gotoVerifyEmailSequence =
   startWebSocketConnectionSequenceDecorator([
-    verifyUserPendingEmailAction,
-    setAlertInfoAction,
+    refreshTokenAction,
+    {
+      userIsLoggedIn: [verifyUserPendingEmailAction, setAlertInfoAction],
+      userIsNotLoggedIn: [setNotLoggedInVerificationAction],
+    },
     clearUserAction,
     navigateToLoginAction,
   ]) as unknown as (props: { token: string }) => void;
