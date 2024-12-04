8a6b68a92b497936ed2ed0447694740279523306
Christopher Bisom <cbisom@flexion.us>
2024-12-04 08:16:23 -0500

    10502-dxox: update updateCase, and sync mocks.jest.ts with current persistence methods

--- web-api/src/persistence/postgres/cases/mocks.jest.ts
+++ web-api/src/persistence/postgres/cases/mocks.jest.ts
@@ -1,21 +1,40 @@
 import { mockFactory } from '@shared/test/mockFactory';
 
+jest.mock('@web-api/persistence/postgres/cases/createCase', () =>
+  mockFactory('createCase'),
+);
+
+jest.mock('@web-api/persistence/postgres/cases/generateDocketNumber', () =>
+  mockFactory('generateDocketNumber'),
+);
+
 jest.mock('@web-api/persistence/postgres/cases/getCaseByDocketNumber', () =>
   mockFactory('getCaseByDocketNumber'),
 );
 
-jest.mock('@web-api/persistence/postgres/cases/upsertCases', () =>
-  mockFactory('upsertCases'),
+jest.mock(
+  '@web-api/persistence/postgres/cases/getCaseMetadataByDocketNumber',
+  () => mockFactory('getCaseMetadataByDocketNumber'),
 );
 
-jest.mock('@web-api/persistence/postgres/cases/createCase', () =>
-  mockFactory('createCase'),
+// jest.mock(
+//   '@web-api/persistence/postgres/cases/getCasesByLeadDocketNumber',
+//   () => mockFactory('getCasesByLeadDocketNumber'),
+// );
+
+jest.mock(
+  '@web-api/persistence/postgres/cases/getCasesMetadataByDocketNumbers',
+  () => mockFactory('getCasesMetadataByDocketNumbers'),
+);
+
+jest.mock('@web-api/persistence/postgres/cases/getConsolidatedCasesCount', () =>
+  mockFactory('getConsolidatedCasesCount'),
 );
 
 jest.mock('@web-api/persistence/postgres/cases/updateCase', () =>
   mockFactory('updateCase'),
 );
 
-jest.mock('@web-api/persistence/postgres/cases/generateDocketNumber', () =>
-  mockFactory('generateDocketNumber'),
+jest.mock('@web-api/persistence/postgres/cases/upsertCases', () =>
+  mockFactory('upsertCases'),
 );
--- web-api/src/persistence/postgres/cases/updateCase.ts
+++ web-api/src/persistence/postgres/cases/updateCase.ts
@@ -1,8 +1,6 @@
 import { Case } from '@shared/business/entities/cases/Case';
-import { calculateDate } from '@shared/business/utilities/DateHandler';
-import { fieldsToOmitBeforePersisting } from '@web-api/persistence/postgres/cases/mapper';
 import { getDbWriter } from '@web-api/database';
-import { omit } from 'lodash';
+import { toNewKyselyCase } from '@web-api/persistence/postgres/cases/mapper';
 import { transformNullToUndefined } from '@web-api/persistence/postgres/utils/transformNullToUndefined';
 
 export const updateCase = async ({
@@ -13,41 +11,7 @@ export const updateCase = async ({
   const updatedCase = await getDbWriter(writer =>
     writer
       .updateTable('dwCase')
-      .set({
-        ...omit(caseToUpdate, fieldsToOmitBeforePersisting),
-        automaticBlockedDate: caseToUpdate.automaticBlockedDate
-          ? calculateDate({ dateString: caseToUpdate.automaticBlockedDate })
-          : undefined,
-        blockedDate: caseToUpdate.blockedDate
-          ? calculateDate({ dateString: caseToUpdate.blockedDate })
-          : undefined,
-        caption: caseToUpdate.caseCaption,
-        closedDate: caseToUpdate.closedDate
-          ? calculateDate({ dateString: caseToUpdate.closedDate })
-          : undefined,
-        createdAt: calculateDate({ dateString: caseToUpdate.createdAt }),
-        irsNoticeDate: caseToUpdate.irsNoticeDate
-          ? calculateDate({ dateString: caseToUpdate.irsNoticeDate })
-          : undefined,
-        noticeOfTrialDate: caseToUpdate.noticeOfTrialDate
-          ? calculateDate({ dateString: caseToUpdate.noticeOfTrialDate })
-          : undefined,
-        petitionPaymentDate: caseToUpdate.petitionPaymentDate
-          ? calculateDate({ dateString: caseToUpdate.petitionPaymentDate })
-          : undefined,
-        petitionPaymentWaivedDate: caseToUpdate.petitionPaymentWaivedDate
-          ? calculateDate({
-              dateString: caseToUpdate.petitionPaymentWaivedDate,
-            })
-          : undefined,
-        receivedAt: calculateDate({ dateString: caseToUpdate.receivedAt }),
-        sealedDate: caseToUpdate.sealedDate
-          ? calculateDate({ dateString: caseToUpdate.sealedDate })
-          : undefined,
-        trialDate: caseToUpdate.trialDate
-          ? calculateDate({ dateString: caseToUpdate.trialDate })
-          : undefined,
-      })
+      .set(toNewKyselyCase(caseToUpdate))
       .where('docketNumber', '=', caseToUpdate.docketNumber)
       .returningAll()
       .executeTakeFirst(),
