0db4382aa5b6235757f091ffc4247aff24900782
Nechama Krigsman <nkrigsman@flexion.us>
2024-12-04 15:11:29 -0500

    10553: Delete unused action (removed since we no longer use websockets);

--- web-client/src/presenter/actions/setNotLoggedInVerificationAction.tsx
+++
@@ -1,8 +0,0 @@
-import { genericAlertError } from '@web-client/presenter/actions/verifyUserPendingEmailAction';
-import { state } from '@web-client/presenter/app.cerebral';
-
-export const setNotLoggedInVerificationAction = ({
-  store,
-}: ActionProps<{ alertInfo: any }>) => {
-  store.set(state.alertWarning, genericAlertError);
-};
