5578d45a6ed0fb7760948ebd388bb4016bf3c7d0
Nechama Krigsman <nkrigsman@flexion.us>
2024-12-04 12:09:08 -0500

    10553: Display initial message for users verifying pending email; Display new error message for when the request times out;

--- web-client/src/presenter/sequences/gotoVerifyEmailSequence.ts
+++ web-client/src/presenter/sequences/gotoVerifyEmailSequence.ts
@@ -1,16 +1,19 @@
 import { clearUserAction } from '../actions/clearUserAction';
 import { navigateToLoginAction } from '@web-client/presenter/actions/Login/navigateToLoginAction';
 import { setAlertErrorAction } from '@web-client/presenter/actions/setAlertErrorAction';
+import { setAlertInfoAction } from '@web-client/presenter/actions/setAlertInfoAction';
 import { setAlertSuccessAction } from '@web-client/presenter/actions/setAlertSuccessAction';
+import { setInitialVerifyAlertMessageAction } from '@web-client/presenter/actions/setInitialVerifyAlertMessageAction';
 import { verifyUserPendingEmailAction } from '../actions/verifyUserPendingEmailAction';
 
 export const gotoVerifyEmailSequence = [
-  //set initial message here
+  setInitialVerifyAlertMessageAction,
+  setAlertInfoAction,
+  navigateToLoginAction,
   verifyUserPendingEmailAction,
   {
     error: [setAlertErrorAction],
     success: [setAlertSuccessAction],
   },
   clearUserAction,
-  navigateToLoginAction,
 ] as unknown as (props: { token: string }) => void;
