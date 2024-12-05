3fd0143b13881ee5e4f0284fa2c2c7cce264d0f6
Jim Lerza <james.lerza@ustaxcourt.gov>
2024-12-05 15:44:41 -0500

    devex: refactored event-codes-by-year and count-event-codes-by-year to use the parseArguments helper

--- scripts/reports/count-event-codes-by-year.ts
+++ scripts/reports/count-event-codes-by-year.ts
@@ -1,34 +1,32 @@
 #!/usr/bin/env npx ts-node --transpile-only
 
-// usage examples:
-//   scripts/reports/count-event-codes-by-year.ts NOA -f -y 2024
-//   scripts/reports/count-event-codes-by-year.ts m01,m02,FEEW -y 2000-2020
-//   scripts/reports/count-event-codes-by-year.ts M071,M074 --years 2021,2022,2024
-
 import { DateTime } from 'luxon';
+import { type ScriptConfig, parseArguments } from '../helpers/parseArguments';
 import {
   ServerApplicationContext,
   createApplicationContext,
 } from '@web-api/applicationContext';
 import { count } from '@web-api/persistence/elasticsearch/searchClient';
-import { parseArgs } from 'node:util';
-import { parseIntsArg } from './reportUtils';
 import { requireEnvVars } from '../../shared/admin-tools/util';
 import { validateDateAndCreateISO } from '@shared/business/utilities/DateHandler';
 
 requireEnvVars(['ENV', 'REGION']);
 
-const config = {
-  allowPositionals: true,
-  options: {
+const scriptConfig: ScriptConfig = {
+  description:
+    'count-event-codes-by-year - Count instances of documents with the ' +
+    'given event code(s) filed within the given duration.',
+  parameters: {
+    eventCodes: {
+      commaDelimited: true,
+      position: 0,
+      required: true,
+      transform: 'toUpperCase',
+      type: 'string',
+    },
     fiscal: {
       default: false,
       short: 'f',
-      type: 'boolean',
-    },
-    help: {
-      default: false,
-      short: 'h',
       type: 'boolean',
     },
     stricken: {
@@ -36,75 +34,14 @@ const config = {
       short: 's',
       type: 'boolean',
     },
-    verbose: {
-      default: false,
-      short: 'v',
-      type: 'boolean',
-    },
     years: {
-      default: `${DateTime.now().toObject().year}`,
+      default: [`${DateTime.now().toObject().year}`],
+      multiple: true,
       short: 'y',
+      transform: 'number',
       type: 'string',
     },
   },
-  strict: true,
-} as const;
-
-const usage = (warning?: string): void => {
-  if (warning) {
-    console.log(warning);
-  }
-  console.log(`Usage: ${process.argv[1]} M071,m074 [-f -y 2000-2022]`);
-  console.log('Options:', JSON.stringify(config, null, 2));
-};
-
-const parseArguments = (): {
-  eventCodes: string[];
-  fiscal: boolean;
-  stricken: boolean;
-  years: number[];
-} => {
-  let positionals: string[];
-  let values: {
-    [k: string]: any;
-    fiscal: boolean;
-    help: boolean;
-    stricken: boolean;
-    verbose: boolean;
-    years: string;
-  };
-  try {
-    ({ positionals, values } = parseArgs(config));
-  } catch (ex) {
-    usage(`Error: ${ex}`);
-    process.exit(1);
-  }
-  if (values.verbose) {
-    usage('Verbose output enabled');
-    console.log('positionals:', positionals);
-    console.log('values:', values);
-  }
-  if (values.help) {
-    if (!values.verbose) {
-      usage();
-    }
-    process.exit(0);
-  }
-  if (!positionals || positionals.length === 0) {
-    const errorMessage = 'invalid input: expected event codes';
-    if (values.verbose) {
-      console.log(errorMessage);
-    } else {
-      usage(errorMessage);
-    }
-    process.exit(1);
-  }
-  return {
-    eventCodes: positionals[0].split(',').map(s => s.toUpperCase()),
-    fiscal: values.fiscal,
-    stricken: values.stricken,
-    years: parseIntsArg(values.years),
-  };
 };
 
 const getCountDocketEntriesByEventCodesAndYears = async ({
@@ -198,7 +135,14 @@ const getCountDocketEntriesByEventCodesAndYears = async ({
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
-  const { eventCodes, fiscal, stricken, years } = parseArguments();
+  const { eventCodes, fiscal, stricken, years } = parseArguments(
+    scriptConfig,
+  ) as {
+    eventCodes: string[];
+    fiscal: boolean;
+    stricken: boolean;
+    years: number[];
+  };
   const ret = await getCountDocketEntriesByEventCodesAndYears({
     applicationContext: createApplicationContext({}),
     eventCodes,
