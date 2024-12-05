2d3dd29ada96547c70fd3758d41ec388339357b9
Christopher Bisom <cbisom@flexion.us>
2024-12-05 11:09:53 -0500

    10502-dxox: WIP: getting statistics and case status updates to work; next up, petitioner on case

---
+++ web-api/src/persistence/postgres/cases/upsertCaseStatusUpdates.ts
@@ -0,0 +1,34 @@
+import { CaseStatusChange } from '@shared/business/entities/cases/Case';
+import { calculateDate } from '@shared/business/utilities/DateHandler';
+import { getDbWriter } from '@web-api/database';
+
+export const upsertCaseStatusUpdates = async ({
+  docketNumber,
+  statusUpdates,
+}: {
+  docketNumber: string;
+  statusUpdates: CaseStatusChange[];
+}): Promise<void> => {
+  await getDbWriter(writer =>
+    writer
+      .insertInto('dwCaseStatusUpdate')
+      .values(
+        statusUpdates.map(s => ({
+          changedBy: s.changedBy,
+          date: calculateDate({ dateString: s.date }),
+          docketNumber,
+          updatedCaseStatus: s.updatedCaseStatus,
+        })),
+      )
+      .onConflict(oc =>
+        oc.columns(['docketNumber', 'date']).doUpdateSet(s => {
+          return {
+            changedBy: s.ref('excluded.changedBy'),
+            date: s.ref('excluded.date'),
+            updatedCaseStatus: s.ref('excluded.updatedCaseStatus'),
+          };
+        }),
+      )
+      .execute(),
+  );
+};
