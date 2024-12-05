84d952483d4a6f598286b2a4d8c58d1cc34433bf
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2024-12-05 09:20:21 -0600

    10436: return all eligible cases from interactor

--- shared/src/business/useCases/getEligibleForTrialCasesByCityInteractor.ts
+++ shared/src/business/useCases/getEligibleForTrialCasesByCityInteractor.ts
@@ -1,26 +1,23 @@
-// import { CaseFactory } from '@shared/business/entities/cases/CaseFactory';
-// import { ServerApplicationContext } from '@web-api/applicationContext';
-// import { UnauthorizedError } from '@web-api/errors/errors';
-// import {
-//   UnknownAuthUser,
-//   isAuthUser,
-// } from '@shared/business/entities/authUser/AuthUser';
-// import { getEligibleForTrialCasesByCity } from '@web-api/persistence/postgres/cases/getEligibleTrialCasesForCity';
+import { ServerApplicationContext } from '@web-api/applicationContext';
+import { UnauthorizedError } from '@web-api/errors/errors';
+import {
+  UnknownAuthUser,
+  isAuthUser,
+} from '@shared/business/entities/authUser/AuthUser';
+import { getEligibleForTrialCasesByCity } from '@web-api/persistence/postgres/cases/getEligibleTrialCasesForCity';
 
-// export const getEligibleForTrialCasesByCityInteractor = async (
-//   applicationContext: ServerApplicationContext,
-//   { trialCity }: { trialCity: string },
-//   authorizedUser: UnknownAuthUser,
-// ) => {
-//   if (!isAuthUser(authorizedUser)) {
-//     throw new UnauthorizedError(
-//       `Invalid User attempting to view eligible cases for : ${trialCity}`,
-//     );
-//   }
+export const getEligibleForTrialCasesByCityInteractor = async (
+  applicationContext: ServerApplicationContext,
+  { trialCity }: { trialCity: string },
+  authorizedUser: UnknownAuthUser,
+): Promise<RawCase[] | undefined> => {
+  if (!isAuthUser(authorizedUser)) {
+    throw new UnauthorizedError(
+      `Invalid User attempting to view eligible cases for: ${trialCity}`,
+    );
+  }
 
-//   const eligibleCases = await getEligibleForTrialCasesByCity({
-//     trialCity,
-//   });
-
-//   return CaseFactory.getCase({ rawCase: caseRecord, user: authorizedUser });
-// };
+  return await getEligibleForTrialCasesByCity({
+    trialCity,
+  });
+};
