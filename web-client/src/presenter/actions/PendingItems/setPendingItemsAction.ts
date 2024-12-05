369c8fff8af7ca57df8d0ea4e231a28ffc1c06f3
Andy Kuny <akuny@flexion.us>
2024-12-05 09:51:07 -0500

    10293: format items for pending report when setting to state instead of in computed

--- web-client/src/presenter/actions/PendingItems/setPendingItemsAction.ts
+++ web-client/src/presenter/actions/PendingItems/setPendingItemsAction.ts
@@ -2,11 +2,25 @@ import { PendingItem } from '@web-api/business/useCases/pendingItems/fetchPendin
 import { state } from '@web-client/presenter/app.cerebral';
 
 export const setPendingItemsAction = ({
+  applicationContext,
+
   props,
   store,
 }: ActionProps<{ pendingItems: PendingItem[] }>) => {
   const { pendingItems } = props;
-  store.set(state.pendingReports.pendingItems, pendingItems);
-  store.set(state.pendingReports.hasPendingItemsResults, !!pendingItems.length);
-  store.set(state.pendingReports.pendingItemsTotal, pendingItems.length);
+  const formattedPendingItems = pendingItems.map(item =>
+    applicationContext
+      .getUtilities()
+      .formatPendingItem(item, { applicationContext }),
+  );
+
+  store.set(state.pendingReports.pendingItems, formattedPendingItems);
+  store.set(
+    state.pendingReports.hasPendingItemsResults,
+    !!formattedPendingItems.length,
+  );
+  store.set(
+    state.pendingReports.pendingItemsTotal,
+    formattedPendingItems.length,
+  );
 };
