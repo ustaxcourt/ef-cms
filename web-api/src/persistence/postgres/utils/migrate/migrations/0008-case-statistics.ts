d9d3601bbd99a89ef614db746d51e056a999437a
Christopher Bisom <cbisom@flexion.us>
2024-12-05 16:09:40 -0500

    10502-dxox: WIP: fix a few bugs and start work on Statistic penalties

--- web-api/src/persistence/postgres/utils/migrate/migrations/0008-case-statistics.ts
+++ web-api/src/persistence/postgres/utils/migrate/migrations/0008-case-statistics.ts
@@ -16,8 +16,24 @@ export async function up(db: Kysely<any>): Promise<void> {
       ['docketNumber'],
     )
     .execute();
+
+  await db.schema
+    .createTable('dwStatisticPenalty')
+    .addColumn('statisticId', 'varchar')
+    .addColumn('name', 'varchar')
+    .addColumn('penaltyAmount', 'decimal')
+    .addColumn('penaltyId', 'varchar', col => col.primaryKey())
+    .addColumn('penaltyType', 'varchar')
+    .addForeignKeyConstraint(
+      'statistic_penalty_to_case_statistic_fk',
+      ['statisticId'],
+      'dwCaseStatistic',
+      ['statisticId'],
+    )
+    .execute();
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable('dwStatisticPenalty').execute();
   await db.schema.dropTable('dwCaseStatistic').execute();
 }
--- web-api/src/persistence/postgres/utils/seed/fixtures/cases/caseStatistics.ts
+++ web-api/src/persistence/postgres/utils/seed/fixtures/cases/caseStatistics.ts
@@ -5,26 +5,32 @@ import { SEEDED_DOCKET_NUMBERS_310_399 } from '@web-api/persistence/postgres/uti
 export const caseStatistics: CaseStatisticKysely[] = [
   // 105-20
   {
+    determinationTotalPenalties: undefined,
     docketNumber: SEEDED_DOCKET_NUMBERS_105_109['105-20'],
     irsDeficiencyAmount: 5678,
     irsTotalPenalties: 1234,
+    lastDateOfPeriod: undefined,
     statisticId: 'cb557361-50ee-4440-aaff-0a9f1bfa30ed',
     year: 2018,
     yearOrPeriod: 'Year',
   },
   {
+    determinationTotalPenalties: undefined,
     docketNumber: SEEDED_DOCKET_NUMBERS_105_109['105-20'],
     irsDeficiencyAmount: 55,
     irsTotalPenalties: 99,
+    lastDateOfPeriod: undefined,
     statisticId: 'ab557361-50ee-4440-aaff-0a9f1bfa30ed',
     year: 2019,
     yearOrPeriod: 'Year',
   },
   // 320-21
   {
+    determinationTotalPenalties: undefined,
     docketNumber: SEEDED_DOCKET_NUMBERS_310_399['320-21'],
-    irsDeficiencyAmount: 1500.0, // "1500.00"
-    irsTotalPenalties: 150.0, // "150.00"
+    irsDeficiencyAmount: 1500.0,
+    irsTotalPenalties: 150.0,
+    lastDateOfPeriod: undefined,
     statisticId: '38128813-53b7-4c53-acc2-33eb31ebc0ef',
     year: 2019,
     yearOrPeriod: 'Year',
