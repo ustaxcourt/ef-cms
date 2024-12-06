7a2338f15adf42930320aee55d2749b8caca1eee
Jim Lerza <james.lerza@ustaxcourt.gov>
2024-12-05 23:23:15 -0500

    devex: update script template to include environment variable examples

--- scripts/template.ts
+++ scripts/template.ts
@@ -1,18 +1,22 @@
 #!/usr/bin/env npx ts-node --transpile-only
 
-import { type ScriptConfig, parseArguments } from './helpers/parseArguments';
+import {
+  type ScriptConfig,
+  parseArgumentsAndEnvironmentVariables,
+} from './helpers/parseArgumentsAndEnvironmentVariables';
 import {
   type ServerApplicationContext,
   createApplicationContext,
 } from '@web-api/applicationContext';
-import { requireEnvVars } from '../shared/admin-tools/util';
-
-requireEnvVars(['ENV', 'REGION']);
 
 // Example:
-//   scripts/template.ts m073,m074 --fiscal -y 2018 --year 2020,2022-2024
+//   scripts/template.ts m073,m074 --fiscal -y 2018 --years 2020,2022-2024
 const scriptConfig: ScriptConfig = {
   description: 'TypeScript Shell Script Template',
+  environment: {
+    env: 'ENV',
+    region: 'REGION',
+  },
   parameters: {
     eventCodes: {
       commaDelimited: true,
@@ -37,23 +41,26 @@ const scriptConfig: ScriptConfig = {
 };
 // Example:
 //   {
+//      env: 'env',
 //      eventCodes: [ 'M073', 'M074' ],
 //      fiscal: true,
 //      verbose: false,
 //      years: [ 2018, 2020, 2022, 2023, 2024 ]
 //    }
-const { eventCodes, fiscal, verbose, years } = parseArguments(scriptConfig) as {
-  eventCodes: string[];
-  fiscal: boolean;
-  verbose: boolean;
-  years: number[];
-};
+const { env, eventCodes, fiscal, verbose, years } =
+  parseArgumentsAndEnvironmentVariables(scriptConfig) as {
+    env: string;
+    eventCodes: string[];
+    fiscal: boolean;
+    verbose: boolean;
+    years: number[];
+  };
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
   const applicationContext: ServerApplicationContext = createApplicationContext(
     {},
   );
-  console.log({ eventCodes, fiscal, verbose, years });
+  console.log({ env, eventCodes, fiscal, verbose, years });
   console.log(applicationContext.environment.stage);
 })();
