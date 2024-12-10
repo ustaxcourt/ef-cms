16c71577a2a54f58bf0291290ea09f32c4c4e059
Tejha Bollu <tbollu@flexion.us>
2024-12-10 16:42:30 -0500

    10436: Added back to trial session planning report markup

--- web-client/src/views/TrialSessions/TrialLocation.tsx
+++ web-client/src/views/TrialSessions/TrialLocation.tsx
@@ -3,21 +3,39 @@ import { Tab, Tabs } from '../../ustc-ui/Tabs/Tabs';
 import { TrialLocationBlockedTable } from '@web-client/views/TrialSessions/TrialLocationBlockedTable';
 import { TrialLocationTable } from '@web-client/views/TrialSessions/TrialLocationTable';
 import { connect } from '@web-client/presenter/shared.cerebral';
-import { state } from '@web-client/presenter/app.cerebral';
+import { sequences, state } from '@web-client/presenter/app.cerebral';
 import React from 'react';
+import { Button } from '@web-client/ustc-ui/Button/Button';
 
 export const TrialLocation = connect(
   {
     currentTab: state.trialLocationPage.currentTab,
+    openTrialSessionPlanningModalSequence:
+      sequences.openTrialSessionPlanningModalSequence,
     trialLocationHelper: state.trialLocationHelper,
   },
-  function TrialLocation({ currentTab, trialLocationHelper }) {
+  function TrialLocation({
+    currentTab,
+    openTrialSessionPlanningModalSequence,
+    trialLocationHelper,
+  }) {
     const { blockedCases, formattedEligibleCases, location } =
       trialLocationHelper;
     return (
       <>
         <BigHeader text={location} />
         <section className="usa-section grid-container">
+          <div>
+            <Button
+              link
+              noMargin
+              className="margin-right-0"
+              icon="print"
+              onClick={() => openTrialSessionPlanningModalSequence()}
+            >
+              Back to Trial Session Planning Report
+            </Button>
+          </div>
           <Tabs
             defaultActiveTab={'eligibleCases'}
             headingLevel="2"
