98b35e8e6def9cb35485ee5000344ea5eaeb8907
Andy Kuny <akuny@flexion.us>
2024-12-10 08:35:22 -0500

    10293: modify naming and organization of pending report-related computeds

--- web-client/src/presenter/computeds/pendingReportListHelper.ts
+++ web-client/src/presenter/computeds/pendingReportListHelper.ts
@@ -1,4 +1,6 @@
+import { CHIEF_JUDGE } from '@shared/business/entities/EntityConstants';
 import { Get } from 'cerebral';
+import { formatJudgeName } from '@shared/business/utilities/getFormattedJudgeName';
 import { state } from '@web-client/presenter/app.cerebral';
 
 export const pendingReportListHelper = (
@@ -6,6 +8,7 @@ export const pendingReportListHelper = (
 ): {
   showNoPendingItems: boolean;
   showSelectJudgeText: boolean;
+  judges: string[];
 } => {
   const searchResultsCount = get(state.pendingReports.pendingItemsTotal);
   const hasPendingItemsResults = get(
@@ -17,7 +20,13 @@ export const pendingReportListHelper = (
   const showNoPendingItems =
     searchResultsCount === 0 && !hasPendingItemsResults && !!judge;
 
+  const judges = get(state.judges)
+    .map(i => formatJudgeName(i.name))
+    .concat(CHIEF_JUDGE)
+    .sort();
+
   return {
+    judges,
     showNoPendingItems,
     showSelectJudgeText,
   };
