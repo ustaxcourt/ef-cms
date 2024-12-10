b9fb6ba9de39fb291ada49de0542b3e08a094745
Andy Kuny <akuny@flexion.us>
2024-12-10 09:39:46 -0500

    10293: update PendingItemFormatted type

--- shared/src/business/utilities/formatPendingItem.ts
+++ shared/src/business/utilities/formatPendingItem.ts
@@ -15,7 +15,6 @@ export type PendingItemFormatted = {
   isLeadCase: boolean;
   docketNumberWithSuffix: string;
   receivedAt: string;
-  renderKey: string;
 };
 
 export const formatPendingItem = (
