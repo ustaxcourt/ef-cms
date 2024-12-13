fa32194b62164cf848b5c194358d0c76e67ff954
John Cruz <jcruz@flexion.us>
2024-12-13 11:41:06 -0700

    10434: Fix tests for "runTrialSessionPlanningReportAction"; Delete comments;

--- web-client/src/presenter/actions/TrialSession/runTrialSessionPlanningReportAction.ts
+++ web-client/src/presenter/actions/TrialSession/runTrialSessionPlanningReportAction.ts
@@ -1,9 +1,3 @@
-/**
- * get the pdf file and pdf blob url from the passed in htmlString
- * @param {object} providers the providers object
- * @param {Function} providers.get the cerebral get function
- * @returns {object} the pdfUrl
- */
 export const runTrialSessionPlanningReportAction = async ({
   applicationContext,
   props,
