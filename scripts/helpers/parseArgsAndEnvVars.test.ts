2273fd6743fd5aab81a06e5eb45b3d178b35454c
Jim Lerza <jim@lerza.com>
2024-12-13 21:15:07 -0500

    devex: ok, parseArgumentsAndEnvironmentVariables was just too long of a name

--- scripts/helpers/parseArgsAndEnvVars.test.ts
+++ scripts/helpers/parseArgsAndEnvVars.test.ts
@@ -1,10 +1,10 @@
 import {
   type ScriptConfig,
-  parseArgumentsAndEnvironmentVariables,
+  parseArgsAndEnvVars,
   parseIntRange,
   parseInts,
   parseIntsArg,
-} from './parseArgumentsAndEnvironmentVariables';
+} from './parseArgsAndEnvVars';
 import { cloneDeep } from 'lodash';
 
 const mockScriptConfig: ScriptConfig = {
@@ -113,7 +113,7 @@ describe('parseArgumentsAndEnvironmentVariables', () => {
     it('prints help output and exits before validating parameters', () => {
       process.argv = ['ts-node', 'some-script.ts', '-h'];
       try {
-        parseArgumentsAndEnvironmentVariables(mockScriptConfig);
+        parseArgsAndEnvVars(mockScriptConfig);
       } catch (err: any) {
         expect(err.toString()).toEqual('Error: caught process.exit');
       }
@@ -123,7 +123,7 @@ describe('parseArgumentsAndEnvironmentVariables', () => {
     it('generates a usage example from provided configuration', () => {
       process.argv = ['ts-node', 'some-script.ts', '-h'];
       try {
-        parseArgumentsAndEnvironmentVariables(mockScriptConfig);
+        parseArgsAndEnvVars(mockScriptConfig);
       } catch (err: any) {
         expect(err.toString()).toEqual('Error: caught process.exit');
       }
@@ -137,7 +137,7 @@ describe('parseArgumentsAndEnvironmentVariables', () => {
     it('prints verbose output after validating parameters and does not exit', () => {
       process.argv.push('-v');
       const { eventCode, verbose } =
-        parseArgumentsAndEnvironmentVariables(mockScriptConfig);
+        parseArgsAndEnvVars(mockScriptConfig);
       expect(eventCode).toEqual(mockEventCode); // parameters are parsed
       expect(verbose).toBeTruthy();
       expect(mockConsoleLog).toHaveBeenNthCalledWith(
@@ -152,7 +152,7 @@ describe('parseArgumentsAndEnvironmentVariables', () => {
     it('does not allow a boolean parameter to be defaulted to true', () => {
       const itsScriptConfig = cloneDeep(mockScriptConfig);
       itsScriptConfig.parameters!.fiscal.required = true;
-      const { fiscal } = parseArgumentsAndEnvironmentVariables(itsScriptConfig);
+      const { fiscal } = parseArgsAndEnvVars(itsScriptConfig);
       expect(fiscal).toBeFalsy();
       expect(mockExit).not.toHaveBeenCalled();
     });
@@ -170,7 +170,7 @@ describe('parseArgumentsAndEnvironmentVariables', () => {
         type: 'string',
       };
       try {
-        parseArgumentsAndEnvironmentVariables(itsScriptConfig);
+        parseArgsAndEnvVars(itsScriptConfig);
       } catch (err: any) {
         expect(err.toString()).toEqual('Error: caught process.exit');
       }
@@ -198,7 +198,7 @@ describe('parseArgumentsAndEnvironmentVariables', () => {
         type: 'string',
       };
       try {
-        parseArgumentsAndEnvironmentVariables(itsScriptConfig);
+        parseArgsAndEnvVars(itsScriptConfig);
       } catch (err: any) {
         expect(err.toString()).toEqual('Error: caught process.exit');
       }
@@ -227,7 +227,7 @@ describe('parseArgumentsAndEnvironmentVariables', () => {
         type: 'string',
       };
       try {
-        parseArgumentsAndEnvironmentVariables(itsScriptConfig);
+        parseArgsAndEnvVars(itsScriptConfig);
       } catch (err: any) {
         expect(err.toString()).toEqual('Error: caught process.exit');
       }
@@ -245,7 +245,7 @@ describe('parseArgumentsAndEnvironmentVariables', () => {
         type: 'string',
       };
       try {
-        parseArgumentsAndEnvironmentVariables(itsScriptConfig);
+        parseArgsAndEnvVars(itsScriptConfig);
       } catch (err: any) {
         expect(err.toString()).toEqual('Error: caught process.exit');
       }
@@ -263,7 +263,7 @@ describe('parseArgumentsAndEnvironmentVariables', () => {
         type: 'string',
       };
       try {
-        parseArgumentsAndEnvironmentVariables(itsScriptConfig);
+        parseArgsAndEnvVars(itsScriptConfig);
       } catch (err: any) {
         expect(err.toString()).toEqual('Error: caught process.exit');
       }
@@ -281,13 +281,13 @@ describe('parseArgumentsAndEnvironmentVariables', () => {
         itsScriptConfig.parameters!.year.default = '2024';
         itsScriptConfig.parameters!.year.multiple = false;
         process.argv.push(...['-y', '2018']);
-        const { year } = parseArgumentsAndEnvironmentVariables(itsScriptConfig);
+        const { year } = parseArgsAndEnvVars(itsScriptConfig);
         expect(year).toEqual(2018);
       });
       it('transforms an array of strings into an array of numbers', () => {
         process.argv.push(...['-y', '2020', '-y', '2024']);
         const { year } =
-          parseArgumentsAndEnvironmentVariables(mockScriptConfig);
+          parseArgsAndEnvVars(mockScriptConfig);
         expect(year).toEqual([2020, 2024]);
       });
       it(
@@ -296,7 +296,7 @@ describe('parseArgumentsAndEnvironmentVariables', () => {
         () => {
           process.argv.push(...['-y', '8,12,3-5,1,7-9']);
           const { year } =
-            parseArgumentsAndEnvironmentVariables(mockScriptConfig);
+            parseArgsAndEnvVars(mockScriptConfig);
           expect(year).toEqual([1, 3, 4, 5, 7, 8, 9, 12]);
         },
       );
@@ -307,7 +307,7 @@ describe('parseArgumentsAndEnvironmentVariables', () => {
         itsScriptConfig.parameters!.eventCode.transform = 'toLowerCase';
         process.argv = ['ts-node', 'some-script.ts', 'FEEW'];
         const { eventCode } =
-          parseArgumentsAndEnvironmentVariables(itsScriptConfig);
+          parseArgsAndEnvVars(itsScriptConfig);
         expect(eventCode).toEqual('feew');
       });
     });
@@ -316,7 +316,7 @@ describe('parseArgumentsAndEnvironmentVariables', () => {
         const itsScriptConfig = cloneDeep(mockScriptConfig);
         itsScriptConfig.parameters!.eventCode.transform = 'toUpperCase';
         const { eventCode } =
-          parseArgumentsAndEnvironmentVariables(itsScriptConfig);
+          parseArgsAndEnvVars(itsScriptConfig);
         expect(eventCode).toEqual('NOA');
       });
       it('transforms a comma-delimited string into an array of upper case strings', () => {
@@ -325,7 +325,7 @@ describe('parseArgumentsAndEnvironmentVariables', () => {
         itsScriptConfig.parameters!.eventCode.transform = 'toUpperCase';
         process.argv = ['ts-node', 'some-script.ts', 'm071,m074'];
         const { eventCode } =
-          parseArgumentsAndEnvironmentVariables(itsScriptConfig);
+          parseArgsAndEnvVars(itsScriptConfig);
         expect(eventCode).toEqual(['M071', 'M074']);
       });
     });
@@ -337,7 +337,7 @@ describe('parseArgumentsAndEnvironmentVariables', () => {
         itsScriptConfig.parameters!.eventCode.commaDelimited = true;
         process.argv = ['ts-node', 'some-script.ts', 'M071,M074,FEEW'];
         const { eventCode } =
-          parseArgumentsAndEnvironmentVariables(itsScriptConfig);
+          parseArgsAndEnvVars(itsScriptConfig);
         expect(eventCode).toEqual(['M071', 'M074', 'FEEW']);
       });
     });
@@ -351,7 +351,7 @@ describe('parseArgumentsAndEnvironmentVariables', () => {
         };
         process.argv = ['ts-node', 'some-script.ts', '--event-code', 'NOA'];
         const { eventCode } =
-          parseArgumentsAndEnvironmentVariables(itsScriptConfig);
+          parseArgsAndEnvVars(itsScriptConfig);
         expect(eventCode).toEqual('NOA');
       });
     });
@@ -379,7 +379,7 @@ describe('parseArgumentsAndEnvironmentVariables', () => {
             'M071,M074',
           ];
           const { eventCode } =
-            parseArgumentsAndEnvironmentVariables(itsScriptConfig);
+            parseArgsAndEnvVars(itsScriptConfig);
           expect(eventCode).toEqual(['M01', 'M02', 'M042', 'M071', 'M074']);
         },
       );
@@ -389,7 +389,7 @@ describe('parseArgumentsAndEnvironmentVariables', () => {
         () => {
           process.argv.push(...['-y', '2018,2020', '-y', '2022-2024']);
           const { year } =
-            parseArgumentsAndEnvironmentVariables(mockScriptConfig);
+            parseArgsAndEnvVars(mockScriptConfig);
           expect(year).toEqual([2018, 2020, 2022, 2023, 2024]);
         },
       );
@@ -401,7 +401,7 @@ describe('parseArgumentsAndEnvironmentVariables', () => {
       itsScriptConfig.environment!.missing = 'MISSINGVAR';
       itsScriptConfig.environment!.notSet = 'NOTAREALVAR';
       try {
-        parseArgumentsAndEnvironmentVariables(itsScriptConfig);
+        parseArgsAndEnvVars(itsScriptConfig);
       } catch (err: any) {
         expect(err.toString()).toEqual('Error: caught process.exit');
       }
@@ -412,7 +412,7 @@ describe('parseArgumentsAndEnvironmentVariables', () => {
       expect(mockExit).toHaveBeenCalledWith(1);
     });
     it('returns the defined environment variables', () => {
-      const { env } = parseArgumentsAndEnvironmentVariables(mockScriptConfig);
+      const { env } = parseArgsAndEnvVars(mockScriptConfig);
       expect(env).toEqual('jest');
     });
   });
