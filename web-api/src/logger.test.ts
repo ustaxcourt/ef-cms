5c671a4b06adbb2912d390c7d81e4a6d45a70bc6
Christopher Bisom <cbisom@flexion.us>
2024-12-16 13:42:07 -0500

    fix failing test

--- web-api/src/logger.test.ts
+++ web-api/src/logger.test.ts
@@ -61,11 +61,12 @@ describe('logger', () => {
     const instance = req.locals.logger;
 
     instance.info = jest.fn();
+    instance.addContext = jest.fn();
 
     res.end();
 
-    expect(instance.info).toHaveBeenCalledWith(
-      expect.any(String),
+    expect(instance.info).toHaveBeenCalledWith(expect.any(String));
+    expect(instance.addContext).toHaveBeenCalledWith(
       expect.objectContaining({
         response: expect.objectContaining({
           statusCode: 200,
