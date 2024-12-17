8b6e4de482f7338f576f6441aba555a277251558
John Cruz <jcruz@flexion.us>
2024-12-17 14:30:45 -0700

    10434: Fix typo;

--- web-client/src/views/TrialSessions/TrialSessionPlanningReportView.tsx
+++ web-client/src/views/TrialSessions/TrialSessionPlanningReportView.tsx
@@ -209,7 +209,7 @@ function CitiesNotCalendaredInPastTwoTerms({
       data-testid="cities-not-calendared-in-past-two-terms-table"
     >
       <div className="border-1px border-gray-30">
-        <div className="bg-yellow padding-top-2 padding-bottom-2 padding-left-3 border-bottom-1px border-gray-30 cities-notcalendared-header">
+        <div className="bg-yellow padding-top-2 padding-bottom-2 padding-left-3 border-bottom-1px border-gray-30 cities-not-calendared-header">
           <FontAwesomeIcon
             className="fa-icon-blue margin-right-2"
             icon="info-circle"
