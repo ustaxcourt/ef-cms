46beb5326c3b8e7081452b1cfc0e5de99356645e
John Cruz <jcruz@flexion.us>
2024-12-17 09:38:02 -0700

    10434: Add more validation to navigation to planning report;

--- web-client/src/presenter/actions/validateTrialSessionPlanningAction.ts
+++ web-client/src/presenter/actions/validateTrialSessionPlanningAction.ts
@@ -1,17 +1,37 @@
+import { state } from '@web-client/presenter/app.cerebral';
+
 export const validateTrialSessionPlanningAction = ({
+  get,
   path,
   props,
 }: ActionProps<{ term: string; year: number }>) => {
   const { term, year } = props;
+  const validYears = get(state.modal.trialYears).map(validYear =>
+    Number(validYear),
+  );
 
-  if (!term || !year) {
+  const termError: string | undefined = getTermError(term);
+  const yearError: string | undefined = getYearError(Number(year), validYears);
+
+  if (termError || yearError) {
     return path.error({
       errors: {
-        term: !term ? 'Select a term' : undefined,
-        year: !year ? 'Select a year' : undefined,
+        term: termError,
+        year: yearError,
       },
     });
   }
 
   return path.success();
 };
+
+function getTermError(term: string): string | undefined {
+  if (!term) return 'Select a term';
+  if (!['winter', 'fall', 'spring'].includes(term.toLocaleLowerCase()))
+    return 'Select a valid term';
+}
+
+function getYearError(year: number, validYears: number[]): string | undefined {
+  if (!year) return 'Select a year';
+  if (!validYears.includes(year)) return 'Select a valid year';
+}
--- web-client/src/presenter/sequences/gotoTrialSessionPlanningReportViewSequence.ts
+++ web-client/src/presenter/sequences/gotoTrialSessionPlanningReportViewSequence.ts
@@ -1,22 +1,26 @@
 import { clearModalStateAction } from '@web-client/presenter/actions/clearModalStateAction';
 import { getTrialSessionPlanningReportAction } from '@web-client/presenter/actions/TrialSession/getTrialSessionPlanningReportAction';
+import { gotoTrialSessionsSequence } from '@web-client/presenter/sequences/gotoTrialSessionsSequence';
 import { setTrialSessionPlanningReportAction } from '@web-client/presenter/actions/TrialSession/setTrialSessionPlanningReportAction';
 import { setupCurrentPageAction } from '../actions/setupCurrentPageAction';
 import { setupTrialSessionPlanningReportViewDataAction } from '@web-client/presenter/actions/setupTrialSessionPlanningReportViewDataAction';
+import { setupTrialYearsAction } from '@web-client/presenter/actions/setupTrialYearsAction';
 import { startWebSocketConnectionSequenceDecorator } from '../utilities/startWebSocketConnectionSequenceDecorator';
+import { validateTrialSessionPlanningAction } from '@web-client/presenter/actions/validateTrialSessionPlanningAction';
 
 export const gotoTrialSessionPlanningReportViewSequence =
   startWebSocketConnectionSequenceDecorator([
     setupCurrentPageAction('Interstitial'),
-    setupTrialSessionPlanningReportViewDataAction,
-    getTrialSessionPlanningReportAction,
-    setTrialSessionPlanningReportAction,
-    clearModalStateAction,
-    setupCurrentPageAction('TrialSessionPlanningReportView'),
-  ]) as unknown as ({
-    trialTerm,
-    trialYear,
-  }: {
-    trialTerm: string;
-    trialYear: number;
-  }) => void;
+    setupTrialYearsAction,
+    validateTrialSessionPlanningAction,
+    {
+      error: [gotoTrialSessionsSequence],
+      success: [
+        setupTrialSessionPlanningReportViewDataAction,
+        getTrialSessionPlanningReportAction,
+        setTrialSessionPlanningReportAction,
+        clearModalStateAction,
+        setupCurrentPageAction('TrialSessionPlanningReportView'),
+      ],
+    },
+  ]) as unknown as ({ term, year }: { term: string; year: number }) => void;
