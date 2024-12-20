2804bd6d425d0d0e0b11b7895d22f39f8556cfe2
Javis Sullivan <jsullivan@flexion.us>
2024-12-20 12:53:42 -0500

    10472: address final feedback

--- web-client/src/presenter/actions/PDFPreviewTab/loadPdfForTabAction.ts
+++ web-client/src/presenter/actions/PDFPreviewTab/loadPdfForTabAction.ts
@@ -1,3 +1,4 @@
+import { openUrlInNewTab } from '../../utilities/openUrlInNewTab';
 export const loadPdfForTabAction = ({
   applicationContext,
   props,
@@ -23,7 +24,7 @@ export const loadPdfForTabAction = ({
           // @ts-ignore
           new Blob([binaryFile], { type: 'application/pdf' }),
         );
-        window.open(pdfDataUri, '_blank');
+        openUrlInNewTab({ url: pdfDataUri });
         resolve();
       } catch (err) {
         reject(err);
