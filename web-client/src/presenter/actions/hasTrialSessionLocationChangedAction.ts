8cddc3bb107a3a3eca9114dbd89523c6ac853653
John Cruz <jcruz@flexion.us>
2025-01-07 09:43:23 -0700

    10522: Create new action to check if there is a change in Location information when it is an in person Trial Session; Add unit tests; Updated type to include proceeding type;

---
+++ web-client/src/presenter/actions/hasTrialSessionLocationChangedAction.ts
@@ -0,0 +1,46 @@
+import {
+  TRIAL_SESSION_ADDRESS_PROPERTIES,
+  TrialSessionLocationInfo,
+} from '@shared/business/entities/trialSessions/TrialSession';
+import { TRIAL_SESSION_PROCEEDING_TYPES } from '@shared/business/entities/EntityConstants';
+import { isEqual } from 'lodash';
+import { state } from '@web-client/presenter/app.cerebral';
+
+export const hasTrialSessionLocationChangedAction = ({
+  get,
+  path,
+}: ActionProps) => {
+  const updatedTrialSessionLocation = get(state.form);
+  const currentTrialSessionLocation = get(state.formattedTrialSessionDetails);
+
+  if (
+    currentTrialSessionLocation.proceedingType !==
+      TRIAL_SESSION_PROCEEDING_TYPES.inPerson ||
+    updatedTrialSessionLocation.proceedingType !==
+      TRIAL_SESSION_PROCEEDING_TYPES.inPerson
+  )
+    return path.unchanged();
+
+  const currentLocation = TRIAL_SESSION_ADDRESS_PROPERTIES.reduce(
+    (acc, prop) => {
+      acc[prop] = currentTrialSessionLocation[prop];
+      return acc;
+    },
+    {} as TrialSessionLocationInfo,
+  );
+
+  const updatedLocation = TRIAL_SESSION_ADDRESS_PROPERTIES.reduce(
+    (acc, prop) => {
+      acc[prop] = updatedTrialSessionLocation[prop];
+      return acc;
+    },
+    {} as TrialSessionLocationInfo,
+  );
+
+  const hasNotChanged = isEqual(currentLocation, updatedLocation);
+  if (hasNotChanged) return path.unchanged();
+  return path.updated({
+    currentTrialSessionLocation,
+    updatedTrialSessionLocation,
+  });
+};
