39d38fed3bb64022d77efa3fe637e1e6e71344d8
Zachary Rogers <zrogers>
2025-01-10 13:00:07 -0800

    dep-updates: WIP fix new opensearch errors

--- web-api/src/persistence/elasticsearch/searchClient.test.constants.ts
+++ web-api/src/persistence/elasticsearch/searchClient.test.constants.ts
@@ -1,3 +1,4 @@
+import { Search_Request } from '@opensearch-project/opensearch/api';
 import { efcmsCaseIndex } from '../../../elasticsearch/efcms-case-mappings';
 import { efcmsDocketEntryIndex } from '../../../elasticsearch/efcms-docket-entry-mappings';
 import { efcmsWorkItemIndex } from '../../../elasticsearch/efcms-work-item-mappings';
@@ -422,7 +423,7 @@ export const mockPractitionerRoleAggregationResult = {
   },
 };
 
-export const openCasesReceivedOnJulyFourthSearchParameters = {
+export const openCasesReceivedOnJulyFourthSearchParameters: Search_Request = {
   body: {
     _source: ['docketNumber.S', 'receivedAt.S'],
     query: {
