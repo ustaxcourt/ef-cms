a65a61ad00c6857c0a3f28605c2af1ccec466445
John Cruz <jcruz@flexion.us>
2025-01-10 07:40:11 -0700

    10522: Updated PDF approval test; Create new approval;

--- shared/src/business/utilities/documentGenerators/noticeOfChangeOfTrialLocation.test.ts
+++ shared/src/business/utilities/documentGenerators/noticeOfChangeOfTrialLocation.test.ts
@@ -4,9 +4,21 @@ import { generateAndVerifyPdfDiff } from './generateAndVerifyPdfDiff';
 import { noticeOfChangeOfTrialLocation } from '@shared/business/utilities/documentGenerators/noticeOfChangeOfTrialLocation';
 
 describe('noticeOfChangeOfTrialLocation', () => {
-  const TRIAL_SESSION = {
-    trialSessionId: 'abc123',
-  } as TrialSessionLocationChangePDFInfo;
+  const TRIAL_SESSION: TrialSessionLocationChangePDFInfo = {
+    address1: 'test_address1',
+    address2: 'test_address2',
+    city: 'test_city',
+    courthouseName: 'test_courthouseName',
+    judge: {
+      name: 'test_judge_name',
+      userId: 'test_judge_userId',
+    },
+    postalCode: 'test_postalCode',
+    startDate: '2019-12-02T05:00:00.000Z',
+    state: 'test_state',
+    trialLocation: 'test_trialLocation',
+    trialSessionId: 'test_trialSessionId',
+  };
 
   generateAndVerifyPdfDiff({
     fileName: 'Notice_Of_Change_Of_Trial_Location.pdf',
@@ -18,7 +30,10 @@ describe('noticeOfChangeOfTrialLocation', () => {
           caseCaptionExtension: 'Petitioner(s)',
           caseTitle: 'Jerad Mayer',
           docketNumberWithSuffix: '123-45S',
-          previousTrialSession: TRIAL_SESSION,
+          previousTrialSession: {
+            ...TRIAL_SESSION,
+            trialLocation: 'old_test_trialLocation',
+          },
           updatedTrialSession: TRIAL_SESSION,
         },
       });
