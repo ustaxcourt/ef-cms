12f7161c5eb6b41249b387140270119ffb21c961
Zachary Rogers <zrogers>
2025-01-19 19:40:09 -0800

    10578: Update tests for internalTypes. add deprecated to legacy filter

--- web-client/src/presenter/computeds/internalTypesHelper.ts
+++ web-client/src/presenter/computeds/internalTypesHelper.ts
@@ -1,9 +1,43 @@
-import { ClientApplicationContext } from '@web-client/applicationContext';
 import { Get } from 'cerebral';
 import { orderBy } from 'lodash';
 import { state } from '@web-client/presenter/app.cerebral';
-import { INTERNAL_DOCUMENTS_ARRAY } from '@shared/business/entities/EntityConstants';
+import {
+  INTERNAL_DOCUMENTS_ARRAY,
+  LODGED_EVENT_CODE,
+} from '@shared/business/entities/EntityConstants';
 import { InternalFilingEvent } from '@shared/business/entities/docketEntry/internalFilingEvents';
+
+export const internalTypesHelper = (
+  get: Get,
+): {
+  internalDocumentTypesForSelectSorted: (InternalFilingEvent & {
+    label: string;
+    value: string;
+  })[];
+  internalDocumentTypesForSelectWithLegacySorted: (InternalFilingEvent & {
+    label: string;
+    value: string;
+  })[];
+} => {
+  const searchText = get(state.screenMetadata.searchText) || '';
+
+  const internalDocumentTypesForSelect = getDocumentTypesForSelect(
+    INTERNAL_DOCUMENTS_ARRAY,
+  );
+
+  const internalDocumentTypesForSelectWithLegacySorted =
+    internalDocumentTypesForSelect
+      .sort(getSortFunction(searchText))
+      .filter(d => d.eventCode !== LODGED_EVENT_CODE);
+
+  const internalDocumentTypesForSelectSorted =
+    internalDocumentTypesForSelectWithLegacySorted.filter(d => !d?.deprecated);
+
+  return {
+    internalDocumentTypesForSelectSorted,
+    internalDocumentTypesForSelectWithLegacySorted,
+  };
+};
 
 export const getDocumentTypesForSelect = <T>(
   documents: ({ documentType: string; eventCode: string } & T)[],
@@ -41,40 +75,5 @@ export const getSortFunction = searchText => {
     // continue comparing labels without regard to search string
     const [x, y] = [first.label, second.label];
     return x.localeCompare(y, { sensitivity: 'base' }); // a == A
-  };
-};
-
-export const internalTypesHelper = (
-  get: Get,
-  applicationContext: ClientApplicationContext,
-): {
-  internalDocumentTypesForSelectSorted: (InternalFilingEvent & {
-    label: string;
-    value: string;
-  })[];
-  internalDocumentTypesForSelectWithLegacySorted: (InternalFilingEvent & {
-    label: string;
-    value: string;
-  })[];
-} => {
-  const { LODGED_EVENT_CODE } = applicationContext.getConstants();
-  const searchText = get(state.screenMetadata.searchText) || '';
-
-  const internalDocumentTypesForSelect = getDocumentTypesForSelect(
-    INTERNAL_DOCUMENTS_ARRAY,
-  );
-
-  const internalDocumentTypesForSelectWithLegacySorted =
-    internalDocumentTypesForSelect
-      .sort(getSortFunction(searchText))
-      .filter(d => d.eventCode !== LODGED_EVENT_CODE)
-      .filter(d => !d?.deprecated);
-
-  const internalDocumentTypesForSelectSorted =
-    internalDocumentTypesForSelectWithLegacySorted;
-
-  return {
-    internalDocumentTypesForSelectSorted,
-    internalDocumentTypesForSelectWithLegacySorted,
   };
 };
