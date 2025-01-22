64579edac90ac405583845c9b76313f6cb7dbe89
Christopher Bisom <cbisom@flexion.us>
2025-01-22 10:49:32 -0500

    10502-dxox: add upsertCasePetitionersData and use it in updateCase

---
+++ web-api/src/persistence/postgres/cases/parties/upsertCasePetitionersData.ts
@@ -0,0 +1,74 @@
+import { Petitioner } from '@shared/business/entities/contacts/Petitioner';
+import { getDbWriter } from '@web-api/database';
+
+export const upsertCasePetitionersData = async ({
+  docketNumber,
+  petitionersData,
+}: {
+  docketNumber: string;
+  petitionersData: Petitioner[];
+}) => {
+  await getDbWriter(writer =>
+    writer
+      .insertInto('dwPetitionerOnCase')
+      .values(
+        petitionersData.map(p => ({
+          additionalName: p.additionalName,
+          address1: p.address1,
+          address2: p.address2,
+          address3: p.address3,
+          city: p.city,
+          contactId: p.contactId!,
+          contactType: p.contactType,
+          country: p.country,
+          countryType: p.countryType,
+          docketNumber,
+          email: p.email,
+          hasConsentedToEService: p.hasConsentedToEService,
+          hasEAccess: p.hasEAccess,
+          inCareOf: p.inCareOf,
+          isAddressSealed: p.isAddressSealed,
+          name: p.name,
+          paperPetitionEmail: p.paperPetitionEmail,
+          phone: p.phone,
+          placeOfLegalResidence: p.placeOfLegalResidence,
+          postalCode: p.postalCode,
+          sealedAndUnavailable: p.sealedAndUnavailable,
+          secondaryName: p.secondaryName,
+          serviceIndicator: p.serviceIndicator,
+          state: p.state,
+          title: p.title,
+        })),
+      )
+      .onConflict(oc =>
+        oc.columns(['docketNumber', 'contactId']).doUpdateSet(p => {
+          return {
+            additionalName: p.ref('excluded.additionalName'),
+            address1: p.ref('excluded.address1'),
+            address2: p.ref('excluded.address2'),
+            address3: p.ref('excluded.address3'),
+            city: p.ref('excluded.city'),
+            contactType: p.ref('excluded.contactType'),
+            country: p.ref('excluded.country'),
+            countryType: p.ref('excluded.countryType'),
+            email: p.ref('excluded.email'),
+            hasConsentedToEService: p.ref('excluded.hasConsentedToEService'),
+            hasEAccess: p.ref('excluded.hasEAccess'),
+            inCareOf: p.ref('excluded.inCareOf'),
+            isAddressSealed: p.ref('excluded.isAddressSealed'),
+            name: p.ref('excluded.name'),
+            paperPetitionEmail: p.ref('excluded.paperPetitionEmail'),
+            phone: p.ref('excluded.phone'),
+            placeOfLegalResidence: p.ref('excluded.placeOfLegalResidence'),
+            postalCode: p.ref('excluded.postalCode'),
+            sealedAndUnavailable: p.ref('excluded.sealedAndUnavailable'),
+            secondaryName: p.ref('excluded.secondaryName'),
+            serviceIndicator: p.ref('excluded.serviceIndicator'),
+            state: p.ref('excluded.state'),
+            title: p.ref('excluded.title'),
+          };
+        }),
+      )
+      .execute(),
+  );
+};
--- web-api/src/persistence/postgres/cases/updateCase.ts
+++ web-api/src/persistence/postgres/cases/updateCase.ts
@@ -2,6 +2,8 @@ import { CaseStatusChange } from '@shared/business/entities/cases/Case';
 import { convertRawCaseToDbRow } from '@web-api/persistence/postgres/cases/mapper';
 import { getDbWriter } from '@web-api/database';
 import { upsertCaseStatusUpdates } from '@web-api/persistence/postgres/cases/upsertCaseStatusUpdates';
+import { upsertCasePetitionersData } from '@web-api/persistence/postgres/cases/parties/upsertCasePetitionersData';
+import { Petitioner } from '@shared/business/entities/contacts/Petitioner';
 
 export const updateCase = async ({
   caseToUpdate,
@@ -17,9 +19,17 @@ export const updateCase = async ({
       .executeTakeFirst(),
   );
 
+  // Because we used to have nested objects in our case records, we upserted everything.
+  // Now, with separate tables, we need to update these separate tables as well.
+  // In the future, we should try to avoid upserting everything.
   await upsertCaseStatusUpdates({
     docketNumber: caseToUpdate.docketNumber,
     statusUpdates: caseToUpdate.caseStatusHistory as CaseStatusChange[],
+  });
+
+  await upsertCasePetitionersData({
+    docketNumber: caseToUpdate.docketNumber,
+    petitionersData: caseToUpdate.petitioners.map(p => new Petitioner(p)),
   });
 
   if (!updatedCase) {
