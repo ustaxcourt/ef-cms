fbaf2661fad720a67a8da10552b8492e8333950b
Zachary Rogers <zrogers>
2025-01-22 08:04:03 -0800

    devex: Fix test mocking

--- web-api/src/lambdas/v1/getCaseLambda.test.ts
+++ web-api/src/lambdas/v1/getCaseLambda.test.ts
@@ -1,6 +1,3 @@
-import '@web-api/persistence/postgres/caseCorrespondences/mocks.jest';
-import '@web-api/persistence/postgres/cases/mocks.jest';
-import '@web-api/persistence/postgres/workitems/mocks.jest';
 jest.mock('@web-api/persistence/dynamo/deployTable/getMaintenanceMode');
 jest.mock('@web-api/persistence/dynamo/cases/getCaseByDocketNumber');
 import { MOCK_CASE_WITH_TRIAL_SESSION } from '../../../../shared/src/test/mockCase';
