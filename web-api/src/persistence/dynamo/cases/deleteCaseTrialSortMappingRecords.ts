cc0eaafdb036af036e8e086f65507b8183b4dab2
Zachary Rogers <zrogers>
2025-01-22 15:45:24 -0800

    10440: WIP Remove cases from eligibible case list when a case is blocked in consolidated group

--- web-api/src/persistence/dynamo/cases/deleteCaseTrialSortMappingRecords.ts
+++ web-api/src/persistence/dynamo/cases/deleteCaseTrialSortMappingRecords.ts
@@ -1,4 +1,6 @@
-import { batchDelete, query } from '../../dynamodbClientService';
+import { getCaseByDocketNumber } from '@web-api/persistence/dynamo/cases/getCaseByDocketNumber';
+import { batchWrite, query } from '../../dynamodbClientService';
+import { DeleteRequest } from '@web-api/persistence/dynamo/dynamoTypes';
 
 /**
  * deleteCaseTrialSortMappingRecords
@@ -11,24 +13,44 @@ import { batchDelete, query } from '../../dynamodbClientService';
 export const deleteCaseTrialSortMappingRecords = async ({
   applicationContext,
   docketNumber,
+  deleteConsolidatedCases = false,
 }: {
   applicationContext: IApplicationContext;
   docketNumber: string;
+  deleteConsolidatedCases?: boolean;
 }) => {
-  const records = await query({
-    ExpressionAttributeNames: {
-      '#gsi1pk': 'gsi1pk',
-    },
-    ExpressionAttributeValues: {
-      ':gsi1pk': `eligible-for-trial-case-catalog|${docketNumber}`,
-    },
-    IndexName: 'gsi1',
-    KeyConditionExpression: '#gsi1pk = :gsi1pk',
-    applicationContext,
-  });
+  const docketNumbersToDelete: string[] = [docketNumber];
+  const recordsToDelete: DeleteRequest[] = [];
+  if (deleteConsolidatedCases) {
+    const theCase = await getCaseByDocketNumber({
+      applicationContext,
+      docketNumber,
+      includeConsolidatedCases: true,
+    });
+    theCase.consolidatedCases.forEach(c =>
+      docketNumbersToDelete.push(c.docketNumber),
+    );
+  }
+  await Promise.all(
+    docketNumbersToDelete.map(async dn => {
+      const records = await query({
+        ExpressionAttributeNames: {
+          '#gsi1pk': 'gsi1pk',
+        },
+        ExpressionAttributeValues: {
+          ':gsi1pk': `eligible-for-trial-case-catalog|${dn}`,
+        },
+        IndexName: 'gsi1',
+        KeyConditionExpression: '#gsi1pk = :gsi1pk',
+        applicationContext,
+      });
 
-  await batchDelete({
-    applicationContext,
-    items: records,
-  });
+      records.forEach(r => {
+        recordsToDelete.push({
+          DeleteRequest: { Key: { pk: r.pk, sk: r.sk } },
+        });
+      });
+    }),
+  );
+  await batchWrite(recordsToDelete, applicationContext);
 };
