68c762446e5a112e63b3af8059cf5745bf0bc589
Kaitlyn Swann <kswann@flexion.us>
2025-01-23 09:54:03 -0700

    10504: fix linting errors

--- web-api/src/persistence/postgres/utils/seed/fixtures/correspodence.ts
+++ web-api/src/persistence/postgres/utils/seed/fixtures/correspodence.ts
@@ -1,5 +1,4 @@
-/* eslint-disable @miovision/disallow-date/no-new-date */
-
+import { calculateDate } from '@shared/business/utilities/DateHandler';
 import { NewCaseCorrespondenceKysely } from '@web-api/database-types';
 
 export const correspondence: NewCaseCorrespondenceKysely[] = [
@@ -8,7 +7,7 @@ export const correspondence: NewCaseCorrespondenceKysely[] = [
     docketNumber: '103-19',
     documentTitle: 'Internal Memo',
     filedBy: 'Test Petitionsclerk',
-    filingDate: new Date('2019-08-14T20:35:52.915Z'),
+    filingDate: calculateDate({ dateString: '2019-08-14T20:35:52.915Z' }),
     userId: '3805d1ab-18d0-43ec-bafb-654e83405416',
   },
 ];
