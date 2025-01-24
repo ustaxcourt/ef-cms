096784cfd2ec07b115a1433c751b516c8ca418c4
John Cruz <jcruz@flexion.us>
2025-01-24 13:12:58 -0700

    10522: Create new Endpoint to get count of all cases assoicaited to the Trial Session; Use new Endpoint in action to determine if we need to display location change modal; Add unit tests to all new code;

--- web-client/src/applicationContext.ts
+++ web-client/src/applicationContext.ts
@@ -359,6 +359,7 @@ import { verifyPendingCaseForUserInteractor } from '../../shared/src/proxies/ver
 import { verifyUserPendingEmailInteractor } from '../../shared/src/proxies/users/verifyUserPendingEmailProxy';
 import ImageBlobReduce from 'image-blob-reduce';
 import deepFreeze from 'deep-freeze';
+import { getTrialSessionOpenCasesCountInteractor } from '@shared/proxies/trialSessions/getTrialSessionOpenCasesCountProxy';
 
 const reduce = ImageBlobReduce({
   pica: ImageBlobReduce.pica({ features: ['js'] }),
@@ -527,6 +528,7 @@ const allUseCases = {
   getPractitionersByNameInteractor,
   getPrivatePractitionersBySearchKeyInteractor,
   getTrialSessionDetailsInteractor,
+  getTrialSessionOpenCasesCountInteractor,
   getTrialSessionPlanningReportDataInteractor,
   getTrialSessionWorkingCopyInteractor,
   getTrialSessionsForJudgeActivityReportInteractor,
