4a954f62393fb9b894c5dd53a60229d160d25fd6
Zachary Rogers <zrogers>
2025-01-30 15:40:04 -0800

    10440: WIP convert integration test to cypress test

--- cypress/local-only/tests/integration/blockedCasesReport/blocked-cases-report.cy.ts
+++ cypress/local-only/tests/integration/blockedCasesReport/blocked-cases-report.cy.ts
@@ -7,9 +7,13 @@ import {
   formatNow,
 } from '../../../../../shared/src/business/utilities/DateHandler';
 import { goToCase } from '../../../../helpers/caseDetail/go-to-case';
-import { loginAsColvin } from '../../../../helpers/authentication/login-as-helpers';
+import {
+  loginAsColvin,
+  loginAsPetitionsClerk1,
+} from '../../../../helpers/authentication/login-as-helpers';
 import { retry } from '../../../../helpers/retry';
 import { createAndServeConsolidatedGroup } from 'cypress/helpers/fileAPetition/create-consolidated-case-group';
+import { createTrialSession } from 'cypress/helpers/trialSession/create-trial-session';
 
 describe('Blocked Cases Report', () => {
   beforeEach(() => {
@@ -56,7 +60,9 @@ describe('Blocked Cases Report', () => {
       retry(checkIfOpensearchHasIndexedBlockedCase);
 
       cy.get('[data-testid="blocked-cases-count"]').should('exist');
-      cy.get(`[data-testid="blocked-case-${leadDocketNumber}-row"]`).contains('Grouped with blocked case')
+      cy.get(`[data-testid="blocked-case-${leadDocketNumber}-row"]`).contains(
+        'Grouped with blocked case',
+      );
 
       //download csv
       cy.get('[data-testid="export-blocked-case-report"]').click();
@@ -66,6 +72,92 @@ describe('Blocked Cases Report', () => {
         fileContent => {
           expect(fileContent).to.include(leadDocketNumber);
           expect(fileContent).to.include(memberDocketNumber);
+        },
+      );
+    });
+  });
+
+  it.only('should show a consolidated group in the eligible cases list, then when blocked, show in the blocked cases report', () => {
+    const trialLocation = 'Portland, Maine';
+    const [_trialCity, _trialState] = trialLocation.split(', ');
+    const procedureType = PROCEDURE_TYPES_MAP.small;
+    const caseStatus = CASE_STATUS_TYPES.generalDocketReadyForTrial;
+    createAndServeConsolidatedGroup({
+      procedureType,
+      trialLocation,
+      caseStatus,
+    }).then(({ leadDocketNumber, memberDocketNumber }) => {
+      loginAsPetitionsClerk1();
+      createTrialSession({ trialLocation, sessionType: procedureType }).then(
+        ({ trialSessionId }) => {
+          //Shows as eligible
+          loginAsPetitionsClerk1();
+          cy.get('[data-testid="trial-session-link"]').click();
+          cy.visit(`/trial-session-detail/${trialSessionId}`);
+          cy.get(`label[for="qc-complete-${leadDocketNumber}"]`);
+          cy.get(`label[for="qc-complete-${memberDocketNumber}"]`);
+
+          //block case
+          loginAsColvin();
+          goToCase(memberDocketNumber);
+          cy.get('[data-testid="tab-case-information"]').click();
+          cy.get('[data-testid="add-manual-block-button"]').click();
+          cy.get('[data-testid="blocked-from-trial-reason-textarea"]').type(
+            'This case cannot go to trial.',
+          );
+          cy.get('[data-testid="modal-button-confirm"]').click();
+          cy.get('[data-testid="success-alert"]').contains(
+            'Case blocked from being set for trial.',
+          );
+
+          //Does not show in eligible case list
+          loginAsPetitionsClerk1();
+          cy.get('[data-testid="trial-session-link"]').click();
+          cy.visit(`/trial-session-detail/${trialSessionId}`);
+          cy.get(`label[for="qc-complete-${leadDocketNumber}"]`).should(
+            'not.exist',
+          );
+          cy.get(`label[for="qc-complete-${memberDocketNumber}"]`).should(
+            'not.exist',
+          );
+
+          //Shows in blocked cases report
+          cy.get('[data-testid="dropdown-select-report"]').click();
+          cy.get('[data-testid="blocked-cases-report"]').click();
+          cy.get('[data-testid="trial-location-filter"]').select(trialLocation);
+          cy.get('[data-testid="procedure-type-filter"]').select(procedureType);
+          cy.get('[data-testid="case-status-filter"]').select(caseStatus);
+          cy.get('[data-testid="blocked-reason-filter"]').select(
+            'Manual Block',
+          );
+          cy.get(`[data-testid="blocked-case-${leadDocketNumber}-row"]`);
+          cy.get(`[data-testid="blocked-case-${memberDocketNumber}-row"]`);
+
+          //Remove block
+          goToCase(memberDocketNumber);
+          cy.get('[data-testid="tab-case-information"]').click();
+          cy.get('[data-testid=remove-block-button').click();
+          cy.get('[data-testid=modal-button-confirm').click();
+          cy.get('[data-testid="success-alert"]').contains(
+            'Block removed. Case is eligible for next available trial session.',
+          );
+
+          //Shows as eligible
+          cy.get('[data-testid="trial-session-link"]').click();
+          cy.visit(`/trial-session-detail/${trialSessionId}`);
+          cy.get(`label[for="qc-complete-${leadDocketNumber}"]`);
+          cy.get(`label[for="qc-complete-${memberDocketNumber}"]`);
+
+          //Does not shows in blocked cases report
+          cy.get('[data-testid="dropdown-select-report"]').click();
+          cy.get('[data-testid="blocked-cases-report"]').click();
+          cy.get('[data-testid="trial-location-filter"]').select(trialLocation);
+          cy.get(`[data-testid="blocked-case-${leadDocketNumber}-row"]`).should(
+            'not.exist',
+          );
+          cy.get(
+            `[data-testid="blocked-case-${memberDocketNumber}-row"]`,
+          ).should('not.exist');
         },
       );
     });
