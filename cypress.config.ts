fce72fd33f4d7541342210691d8fc9cf6d727463
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2025-02-06 13:07:40 -0600

    combine new and old approaches to testing idle logout

--- cypress.config.ts
+++ cypress.config.ts
@@ -121,17 +121,24 @@ export default defineConfig({
             browser: any,
             close: boolean = true,
           ) {
-            // We must retry this call as the pages are sometimes not fully loaded.
-            await retry(
-              async () => {
-                // Note that browser.pages is *not* sorted in any particular order.
-                const pages: Page[] = await browser.pages();
-                for (const page of pages) {
-                  if (page.url().includes('.cy.ts')) {
-                    continue;
-                  }
+            const pages: Page[] = await browser.pages();
 
+            // We must retry this call as the pages are sometimes not fully loaded.
+            // Note that browser.pages is *not* sorted in any particular order.
+            for (const page of pages) {
+              if (page.url().includes('.cy.ts')) {
+                continue;
+              }
+
+              await retry(
+                async () => {
                   await page.bringToFront();
+
+                  // Make sure selector exists
+                  await page.waitForSelector(
+                    '[data-testid="idle-logout-login-button"]',
+                    { timeout: 30000 },
+                  );
 
                   if (!page.url().includes('/idle-logout')) {
                     throw new Error('Page is not on idle logout screen!');
@@ -140,10 +147,10 @@ export default defineConfig({
                   if (close) {
                     await page.close();
                   }
-                }
-              },
-              { delayBetweenTries: 1000, timeout: 30000 },
-            );
+                },
+                { delayBetweenTries: 1000, timeout: 30000 },
+              );
+            }
 
             return true;
           },
