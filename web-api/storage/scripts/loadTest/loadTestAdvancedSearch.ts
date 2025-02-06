648a63c4f72eec7d3ccac9521a0610a6c9ad6507
Jim Lerza <james.lerza@ustaxcourt.gov>
2025-02-06 09:23:33 -0500

    opex: fire off concurrent searches with pqueue

--- web-api/storage/scripts/loadTest/loadTestAdvancedSearch.ts
+++ web-api/storage/scripts/loadTest/loadTestAdvancedSearch.ts
@@ -5,6 +5,8 @@ import {
   parseArgsAndEnvVars,
 } from '../../../../scripts/helpers/parseArgsAndEnvVars';
 import axios from 'axios';
+import { existsSync, readFileSync } from 'fs';
+import PQueue from 'p-queue';
 
 const scriptConfig: ScriptConfig = {
   description:
@@ -13,15 +15,32 @@ const scriptConfig: ScriptConfig = {
     deployingColor: 'DEPLOYING_COLOR',
     efcmsDomain: 'EFCMS_DOMAIN',
   },
+  parameters: {
+    searchesFile: {
+      position: 0,
+      required: true,
+      type: 'string',
+    },
+  },
 };
-const { deployingColor, efcmsDomain } = parseArgsAndEnvVars(scriptConfig) as {
+const { deployingColor, efcmsDomain, searchesFile } = parseArgsAndEnvVars(
+  scriptConfig,
+) as {
   [k: string]: string;
 };
-const apiUrl = `https://public-api-${deployingColor}.${efcmsDomain}/public-api`;
+const apiUrl = `https://public-api-${deployingColor}.${efcmsDomain}`;
+const concurrency = 100;
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
-  for (let i = 0; i < 100; i++) {
-    await axios.get(`${apiUrl}/search`);
+  if (!existsSync(searchesFile)) {
+    console.error('No searches file found.');
+    process.exit(1);
   }
+  const searches: string[] = readFileSync(searchesFile, 'utf8').split('\n');
+  const queue = new PQueue({ concurrency });
+  const funcs = searches.map(
+    (search: string) => async () => await axios.get(`${apiUrl}${search}`),
+  );
+  await queue.addAll(funcs);
 })();
