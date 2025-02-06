f2c0c5c75d4f24c9e75d5075abf61a30101686c0
Nate Elliott <nelliott@flexion.us>
2025-02-06 08:11:16 -0600

    10419: add loading indicator to PDF generation

--- web-client/src/presenter/sequences/TrialSessionMinutes/downloadMinuteSheetFormPdfSequence.ts
+++ web-client/src/presenter/sequences/TrialSessionMinutes/downloadMinuteSheetFormPdfSequence.ts
@@ -1,9 +1,12 @@
 import { DownloadPdfHandler } from '@web-client/presenter/state/TrialSessionMinutesForm/trialSessionMinutesFormHandlers';
 import { downloadMinuteSheetFormPdfAction } from '@web-client/presenter/actions/TrialSessionMinutes/downloadMinuteSheetFormPdfAction';
 import { trialSessionMinutesAutosaveAction } from '@web-client/presenter/actions/TrialSessionMinutes/trialSessionMinutesAutosaveAction';
+import { showProgressSequenceDecorator } from '@web-client/presenter/utilities/showProgressSequenceDecorator';
 
-export const downloadMinuteSheetFormPdfSequence = [
-  () => ({ forceAutosave: true }),
-  trialSessionMinutesAutosaveAction,
-  downloadMinuteSheetFormPdfAction,
-] as unknown as DownloadPdfHandler;
+export const downloadMinuteSheetFormPdfSequence = showProgressSequenceDecorator(
+  [
+    () => ({ forceAutosave: true }),
+    trialSessionMinutesAutosaveAction,
+    downloadMinuteSheetFormPdfAction,
+  ],
+) as unknown as DownloadPdfHandler;
