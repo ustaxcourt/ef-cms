9961fbf7d5bbcd1000368c7c76f905149c92c238
John Cruz <jcruz@flexion.us>
2025-02-14 08:04:54 -0700

    10373: Refactor action, use application context for sleep; Updated tests;

--- web-client/src/presenter/actions/closeFileUploadStatusModalAction.test.ts
+++ web-client/src/presenter/actions/closeFileUploadStatusModalAction.test.ts
@@ -1,8 +1,14 @@
+import { applicationContextForClient as applicationContext } from '@web-client/test/createClientTestApplicationContext';
 import { closeFileUploadStatusModalAction } from './closeFileUploadStatusModalAction';
 import { presenter } from '../presenter-mock';
 import { runAction } from '@web-client/presenter/test.cerebral';
 
 describe('closeFileUploadStatusModalAction', () => {
+  beforeEach(() => {
+    applicationContext.getUtilities().sleep = jest.fn();
+    presenter.providers.applicationContext = applicationContext;
+  });
+
   beforeAll(() => {
     jest.spyOn(global, 'setTimeout');
   });
@@ -21,7 +27,11 @@ describe('closeFileUploadStatusModalAction', () => {
     });
 
     // inspecting arguments of 4th call because it seems `store.set` also invokes setTimeout
-    expect(global.setTimeout.mock.calls[3][1]).toBe('77');
+    const sleepCalls = (applicationContext.getUtilities().sleep as jest.Mock)
+      .mock.calls;
+
+    expect(sleepCalls.length).toBe(1);
+    expect(sleepCalls[0][0]).toBe(77);
     expect(result.state).toMatchObject({
       fileUploadProgress: {
         isUploading: false,
@@ -44,6 +54,7 @@ describe('closeFileUploadStatusModalAction', () => {
     });
     expect(result.state.modal.showModal).toEqual('TEST_MODAL');
   });
+
   it('uses a default timeout value if not provided', async () => {
     delete process.env.FILE_UPLOAD_MODAL_TIMEOUT;
     await runAction(closeFileUploadStatusModalAction, {
@@ -53,6 +64,10 @@ describe('closeFileUploadStatusModalAction', () => {
     });
 
     // inspecting arguments of 4th call because it seems `store.set` also invokes setTimeout
-    expect(global.setTimeout.mock.calls[3][1]).toEqual(3000);
+
+    const sleepCalls = (applicationContext.getUtilities().sleep as jest.Mock)
+      .mock.calls;
+    expect(sleepCalls.length).toBe(1);
+    expect(sleepCalls[0][0]).toBe(3000);
   });
 });
