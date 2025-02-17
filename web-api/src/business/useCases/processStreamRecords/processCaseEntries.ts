244a0f196563d7fcfca557a6f67eae380b0cd010
Christopher Bisom <cbisom@flexion.us>
2025-02-17 15:29:45 -0500

    10502-dxox: fix upserting case data in processCaseEntries

--- web-api/src/business/useCases/processStreamRecords/processCaseEntries.ts
+++ web-api/src/business/useCases/processStreamRecords/processCaseEntries.ts
@@ -1,44 +1,29 @@
-import { NotFoundError } from '@web-api/errors/errors';
-import { getCaseMetadataWithCounsel } from '@web-api/persistence/postgres/cases/getCaseMetadataWithCounsel';
 import { upsertCases } from '@web-api/persistence/postgres/cases/upsertCases';
-import type { ServerApplicationContext } from '@web-api/applicationContext';
 import { upsertCaseStatistics } from '@web-api/persistence/postgres/cases/statistics/upsertCaseStatistics';
 import { upsertPetitionersOnCase } from '@web-api/persistence/postgres/cases/parties/upsertPetitionersOnCase';
 import { upsertCaseStatusUpdates } from '@web-api/persistence/postgres/cases/upsertCaseStatusUpdates';
 import { Statistic } from '@shared/business/entities/Statistic';
 import { Petitioner } from '@shared/business/entities/contacts/Petitioner';
+import { unmarshall } from '@aws-sdk/util-dynamodb';
 
 export const processCaseEntries = async ({
-  applicationContext,
   caseEntityRecords,
 }: {
-  applicationContext: ServerApplicationContext;
   caseEntityRecords: any[];
 }) => {
   if (!caseEntityRecords.length) return;
 
-  const casesToUpsert: Record<string, RawCase> = {};
+  const casesToUpsert: Record<string, any> = {};
 
   for (const caseRecord of caseEntityRecords) {
-    const caseNewImage = caseRecord.dynamodb.NewImage;
-
-    const caseMetadataWithCounsel = await getCaseMetadataWithCounsel({
-      applicationContext,
-      docketNumber: caseNewImage.docketNumber.S,
-    });
-
-    if (!caseMetadataWithCounsel) {
-      throw new NotFoundError(`Case ${caseNewImage.docketNumber.S} not found`);
-    }
+    const caseNewImage = unmarshall(caseRecord.dynamodb.NewImage);
 
     // Only upsert the most recent update of any duplicate case record since otherwise Postgres will throw an error.
-    casesToUpsert[caseMetadataWithCounsel.docketNumber] =
-      caseMetadataWithCounsel;
+    casesToUpsert[caseNewImage.docketNumber] = caseNewImage;
   }
 
-  const postgresUpserts: Promise<void>[] = [
-    upsertCases(Object.values(casesToUpsert)),
-  ];
+  await upsertCases(Object.values(casesToUpsert));
+  const postgresUpserts: Promise<void>[] = [];
   for (const caseRecord of Object.values(casesToUpsert)) {
     postgresUpserts.push(
       upsertPetitionersOnCase({
