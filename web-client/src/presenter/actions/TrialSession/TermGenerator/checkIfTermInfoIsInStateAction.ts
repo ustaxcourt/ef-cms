9bc76c81a14562efbeba82d59d235b8f25d206e4
John Cruz <jcruz@flexion.us>
2025-02-19 13:36:06 -0700

    10561: Remove console logs;

--- web-client/src/presenter/actions/TrialSession/TermGenerator/checkIfTermInfoIsInStateAction.ts
+++ web-client/src/presenter/actions/TrialSession/TermGenerator/checkIfTermInfoIsInStateAction.ts
@@ -1,9 +1,7 @@
 import { state } from '@web-client/presenter/app.cerebral';
 
 export const checkIfTermInfoIsInStateAction = ({ get, path }: ActionProps) => {
-  console.log('aaaaaaaaaaaaa');
   const termGeneratorInformation = get(state.termGeneratorInformation);
   if (!termGeneratorInformation) return path.doesNotExist();
-  console.log('bbbbbbbbbbbbbbbbbb');
   return path.exist();
 };
