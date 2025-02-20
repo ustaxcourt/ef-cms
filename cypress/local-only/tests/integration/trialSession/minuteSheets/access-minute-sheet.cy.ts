56a7c7f6263ddac24843cad7a2bdaa911a4e906f
Nate Elliott <nelliott@flexion.us>
2025-02-20 15:32:36 -0600

    10419: update cypress test for new changes

--- cypress/local-only/tests/integration/trialSession/minuteSheets/access-minute-sheet.cy.ts
+++ cypress/local-only/tests/integration/trialSession/minuteSheets/access-minute-sheet.cy.ts
@@ -88,6 +88,8 @@ describe('Access a minute sheet, fill out sections of the form, navigate away an
 
       it('Can fill out court reporter input and check remote session', function () {
         cy.get('#remoteSession').check({ force: true });
+        cy.get('#courtReporter').should('have.value', 'Not assigned');
+        cy.get('#courtReporter').clear();
         cy.get('#courtReporter').type('Test Court Reporter');
 
         cy.get('#courtReporter').should('have.value', 'Test Court Reporter');
