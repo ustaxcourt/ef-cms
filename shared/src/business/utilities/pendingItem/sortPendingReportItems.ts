7e2813884890537b5afc39ef67894dbcdae27d7b
John Cruz <jcruz@flexion.us>
2025-02-24 09:14:57 -0700

    10570: Update sortable method to handle Upper/Lowercase equally; Added unit test to cover new behavior;

--- shared/src/business/utilities/pendingItem/sortPendingReportItems.ts
+++ shared/src/business/utilities/pendingItem/sortPendingReportItems.ts
@@ -10,10 +10,26 @@ export function sortPendingReportItems(
     return sortBy(pendingItems, ['receivedAt']);
   }
 
-  const sortedPendingItems = sortBy(pendingItems, [
-    pendingItemSortField,
+  const KEY = `sortable__${pendingItemSortField}`;
+  const SORTABLE_PENDING_ITEMS = pendingItems.map(pi => {
+    const isPropertyString = typeof pi[pendingItemSortField] === 'string';
+    return {
+      ...pi,
+      [KEY]: isPropertyString
+        ? pi[pendingItemSortField].toLocaleLowerCase()
+        : pi[pendingItemSortField],
+    };
+  });
+
+  const SORTED_PENDING_ITEMS = sortBy(SORTABLE_PENDING_ITEMS, [
+    KEY,
     'receivedAt',
   ]);
+
+  const sortedPendingItems = SORTED_PENDING_ITEMS.map(pi => ({
+    ...pi,
+    [KEY]: undefined,
+  }));
 
   if (pendingItemSortOrder === 'desc') return sortedPendingItems.reverse();
   return sortedPendingItems;
