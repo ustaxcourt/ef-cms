15f34cc33bada76e422ec096acf1450f4edfc0b8
John Cruz <jcruz@flexion.us>
2025-02-24 11:01:30 -0700

    10561: Clear modal data in state when navigating to Term Builder page;

--- web-client/src/presenter/sequences/TrialSessions/navigateToTermBuilderPageSequence.ts
+++ web-client/src/presenter/sequences/TrialSessions/navigateToTermBuilderPageSequence.ts
@@ -1,7 +1,9 @@
+import { clearModalAction } from '@web-client/presenter/actions/clearModalAction';
 import { navigateToTermBuilderPageAction } from '@web-client/presenter/actions/TrialSession/TermBuilder/navigateToTermBuilderPageAction';
 import { saveTermBuilderInfoAction } from '@web-client/presenter/actions/TrialSession/TermBuilder/saveTermBuilderInfoAction';
 
 export const navigateToTermBuilderPageSequence = [
+  clearModalAction,
   saveTermBuilderInfoAction,
   navigateToTermBuilderPageAction,
 ] as unknown as Function;
