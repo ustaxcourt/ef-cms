7a7e2bc41cc6b9b44a5c023823f59742791a0c9e
Zachary Rogers <zrogers>
2025-03-04 14:46:53 -0800

    devex: Fix petitions clerk login

--- cypress/local-only/tests/integration/documentQC/assign-a-work-item-to-self.cy.ts
+++ cypress/local-only/tests/integration/documentQC/assign-a-work-item-to-self.cy.ts
@@ -1,18 +1,23 @@
 import { externalUserCreatesElectronicCase } from '../../../../helpers/fileAPetition/petitioner-creates-electronic-case';
-import { loginAsPetitioner } from '../../../../helpers/authentication/login-as-helpers';
+import {
+  loginAsPetitioner,
+  loginAsPetitionsClerk,
+} from '../../../../helpers/authentication/login-as-helpers';
 
 describe('Work item assignment', () => {
   it('petitionsClerk assigns a work item to themselves, docketClerk should NOT see that work item as unassigned', () => {
     loginAsPetitioner();
     externalUserCreatesElectronicCase().then(docketNumber => {
-      cy.login('petitionsclerk', '/document-qc/section/inbox');
+      loginAsPetitionsClerk();
+      cy.visit('/document-qc/section/inbox');
       cy.get(`[data-testid="work-item-${docketNumber}"]`)
         .find('[data-testid="checkbox-assign-work-item"]')
         .click();
       cy.get('[data-testid="dropdown-select-assignee"]').select(
         'Test Petitionsclerk',
       );
-      cy.login('petitionsclerk', '/document-qc/section/inbox');
+      loginAsPetitionsClerk();
+      cy.visit('/document-qc/section/inbox')
       cy.get(
         `[data-testid="work-item-${docketNumber}"] [data-testid="table-column-work-item-assigned-to"]:contains("Test Petitionsclerk")`,
       ).should('have.length.above', 0);
