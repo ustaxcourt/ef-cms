5cd82263da6ca8019bb6813f32331b2d532ac8dc
Zachary Rogers <zrogers>
2025-03-04 14:58:59 -0800

    devex: Update all hard coded cy.logins to use helper functions

--- cypress/local-only/tests/integration/documentQC/assign-a-work-item-to-self.cy.ts
+++ cypress/local-only/tests/integration/documentQC/assign-a-work-item-to-self.cy.ts
@@ -1,5 +1,6 @@
 import { externalUserCreatesElectronicCase } from '../../../../helpers/fileAPetition/petitioner-creates-electronic-case';
 import {
+  loginAsDocketClerk,
   loginAsPetitioner,
   loginAsPetitionsClerk,
 } from '../../../../helpers/authentication/login-as-helpers';
@@ -17,11 +18,12 @@ describe('Work item assignment', () => {
         'Test Petitionsclerk',
       );
       loginAsPetitionsClerk();
-      cy.visit('/document-qc/section/inbox')
+      cy.visit('/document-qc/section/inbox');
       cy.get(
         `[data-testid="work-item-${docketNumber}"] [data-testid="table-column-work-item-assigned-to"]:contains("Test Petitionsclerk")`,
       ).should('have.length.above', 0);
-      cy.login('docketclerk', '/document-qc/section/inbox');
+      loginAsDocketClerk();
+      cy.visit('/document-qc/section/inbox');
       cy.get('[data-testid="checkbox-select-all-workitems"]').click();
       cy.get('[data-testid="select-work-item"]:checked').then(elm => {
         cy.get('[data-testid="select-work-item"]').should(
