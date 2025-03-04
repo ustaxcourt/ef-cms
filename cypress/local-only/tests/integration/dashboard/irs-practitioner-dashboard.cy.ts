e0b76cceaedc101089a13dab06a5467dd0d4eee9
Zachary Rogers <zrogers>
2025-03-04 15:24:03 -0800

    devex: remove cy.login

--- cypress/local-only/tests/integration/dashboard/irs-practitioner-dashboard.cy.ts
+++ cypress/local-only/tests/integration/dashboard/irs-practitioner-dashboard.cy.ts
@@ -1,8 +1,8 @@
-import { navigateTo as loginAs } from '../../../support/pages/maintenance';
+import { loginAsIrsPractitioner } from 'cypress/helpers/authentication/login-as-helpers';
 
 describe('IRS practitioner views dashboard', () => {
   it('should NOT have a column for filing fee in the case list table', () => {
-    loginAs('irspractitioner');
+    loginAsIrsPractitioner();
     cy.get('[data-testid="case-list-table"]');
     cy.get('[data-testid="filing-fee"]').should('not.exist');
     cy.get('[data-testid="petition-payment-status"]').should('not.exist');
@@ -11,7 +11,7 @@ describe('IRS practitioner views dashboard', () => {
   it('should NOT display the "Create a Case" button', () => {
     cy.viewport('iphone-5');
 
-    loginAs('irspractitioner');
+    loginAsIrsPractitioner();
     cy.get('[data-testid="additional-case-select"]').should('exist');
     cy.get('[data-testid="file-a-petition"]').should('not.exist');
   });
