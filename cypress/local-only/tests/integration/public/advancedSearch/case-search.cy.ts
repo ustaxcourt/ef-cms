f369181dea919747cdfea94ab545c40d23e3223b
Zachary Rogers <zrogers>
2025-03-04 10:20:52 -0800

    devex: Move case, order, opinion search to its own file

--- cypress/local-only/tests/integration/public/advancedSearch/case-search.cy.ts
+++ cypress/local-only/tests/integration/public/advancedSearch/case-search.cy.ts
@@ -1,25 +1,14 @@
+import { faker } from '@faker-js/faker';
+import { loginAsDocketClerk1 } from 'cypress/helpers/authentication/login-as-helpers';
+import { goToCase } from 'cypress/helpers/caseDetail/go-to-case';
 import {
   createAndServePaperPetition,
   createAndServePaperPetitionMyselfAndSpouse,
-} from '../../../../helpers/fileAPetition/create-and-serve-paper-petition';
-import {
-  docketRecordTable,
-  enterDocumentDocketNumber,
-  enterDocumentKeywordForAdvancedSearch,
-  firstSearchResultJudgeField,
-  navigateTo as navigateToDashboard,
-  noSearchResultsContainer,
-  searchForCaseByDocketNumber,
-  searchForDocuments,
-  searchForOrderByJudge,
-  searchResultsTable,
-  unselectOpinionTypesExceptBench,
-} from '../../../support/pages/public/advanced-search';
-import { faker } from '@faker-js/faker';
-import { goToCase } from '../../../../helpers/caseDetail/go-to-case';
-import { loginAsDocketClerk1 } from '../../../../helpers/authentication/login-as-helpers';
+} from 'cypress/helpers/fileAPetition/create-and-serve-paper-petition';
+import { navigateToDashboard } from 'cypress/local-only/support/pages/maintenance';
+import { searchForCaseByDocketNumber } from 'cypress/local-only/support/pages/public/advanced-search';
 
-describe('Advanced search', () => {
+describe('Case Search', () => {
   describe('Case Search By Name', () => {
     it('should show order search results by [petitioner name, secondary contact name, case caption] when searching', () => {
       const nameToSearchFor = `${faker.person.firstName()} ${faker.person.lastName()}`;
@@ -72,59 +61,13 @@ describe('Advanced search', () => {
     it('should display "No Matches Found" when case search yields no results', () => {
       navigateToDashboard();
       searchForCaseByDocketNumber('999-99');
-      expect(noSearchResultsContainer()).to.exist;
+      cy.get('div#no-search-results');
     });
 
     it('should route to case detail when a case search match is found', () => {
       navigateToDashboard();
       searchForCaseByDocketNumber('103-20');
-      expect(docketRecordTable()).to.exist;
-    });
-  });
-
-  describe('opinion', () => {
-    it('should display results when a keyword and docketNumberWithSuffix is provided', () => {
-      navigateToDashboard();
-      cy.get('[data-testid="opinion-search-tab"]').click();
-      enterDocumentKeywordForAdvancedSearch('opinion');
-      enterDocumentDocketNumber('124-20L');
-      searchForDocuments();
-      expect(searchResultsTable()).to.exist;
-    });
-
-    it('should display results with a judge name', () => {
-      navigateToDashboard();
-      cy.get('[data-testid="opinion-search-tab"]').click();
-      enterDocumentDocketNumber('107-19');
-
-      unselectOpinionTypesExceptBench();
-      searchForDocuments();
-
-      expect(searchResultsTable()).to.exist;
-      expect(firstSearchResultJudgeField()).to.exist;
-    });
-  });
-
-  describe('order', () => {
-    it('should be able to search for an order by legacy judge', () => {
-      const judgeNameColumnIndex = 5;
-      const wantedLegacyJudge = 'Fieri';
-
-      navigateToDashboard();
-      cy.get('[data-testid="order-search-tab"]').click();
-      searchForOrderByJudge(wantedLegacyJudge);
-      searchForDocuments();
-
-      expect(searchResultsTable()).to.exist;
-
-      //assert that every judge in the search result list is the wanted legacy judge
-      cy.get('tr.search-result').each(element => {
-        cy.wrap(element).within(() => {
-          cy.get('td')
-            .eq(judgeNameColumnIndex)
-            .should('have.text', wantedLegacyJudge);
-        });
-      });
+      cy.get('table#docket-record-table');
     });
   });
 });
