3c7701e61ffd0e1580252450e1d488bd5bc57cc2
Zachary Rogers <zrogers>
2025-03-04 15:57:13 -0800

    devex: fix login issue

--- cypress/local-only/tests/integration/dashboard/irs-practitioner-dashboard.cy.ts
+++ cypress/local-only/tests/integration/dashboard/irs-practitioner-dashboard.cy.ts
@@ -1,4 +1,7 @@
-import { loginAsIrsPractitioner } from 'cypress/helpers/authentication/login-as-helpers';
+import {
+  login,
+  loginAsIrsPractitioner,
+} from 'cypress/helpers/authentication/login-as-helpers';
 
 describe('IRS practitioner views dashboard', () => {
   it('should NOT have a column for filing fee in the case list table', () => {
@@ -11,7 +14,7 @@ describe('IRS practitioner views dashboard', () => {
   it('should NOT display the "Create a Case" button', () => {
     cy.viewport('iphone-5');
 
-    loginAsIrsPractitioner();
+    login({ email: 'irsPractitioner@example.com' });
     cy.get('[data-testid="additional-case-select"]').should('exist');
     cy.get('[data-testid="file-a-petition"]').should('not.exist');
   });
