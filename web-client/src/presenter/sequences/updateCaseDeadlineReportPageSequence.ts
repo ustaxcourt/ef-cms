816fbe15fda9fa9372c3a6d4014583d1ed48e3e7
Andy Kuny <akuny@flexion.us>
2025-03-12 15:56:37 -0400

    10608: add sequence to handle page changes for case deadline report

---
+++ web-client/src/presenter/sequences/updateCaseDeadlineReportPageSequence.ts
@@ -0,0 +1,11 @@
+import { clearCaseDeadlinesAction } from '../actions/CaseDeadline/clearCaseDeadlinesAction';
+import { getCaseDeadlinesAction } from '../actions/CaseDeadline/getCaseDeadlinesAction';
+import { setCaseDeadlinesAction } from '../actions/CaseDeadline/setCaseDeadlinesAction';
+import { showProgressSequenceDecorator } from '../utilities/showProgressSequenceDecorator';
+
+export const updateCaseDeadlineReportPageSequence =
+  showProgressSequenceDecorator([
+    clearCaseDeadlinesAction,
+    getCaseDeadlinesAction,
+    setCaseDeadlinesAction,
+  ]) as unknown as (props: { code?: string }) => void;
