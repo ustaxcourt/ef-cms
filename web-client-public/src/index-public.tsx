f49f7c4ec2cd39c29c49ec31045ffb4172d0ef11
Zachary Rogers <zrogers>
2025-03-12 15:07:30 -0700

    devex: Rename files

--- web-client-public/src/index-public.tsx
+++ web-client-public/src/index-public.tsx
@@ -10,7 +10,7 @@ import { rootRoute } from 'web-client-public/src/routes/PublicRoot';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { publicDefaultLayoutRoute } from 'web-client-public/src/routes/_default-layout/_defaultLayoutComponent';
 import { publicTrialSessionsRoute } from 'web-client-public/src/routes/trial-sessions/PublicTrialSessions';
-import { publicTrialSessionDetailsRoute } from 'web-client-public/src/routes/trial-session-detail/PublicaTrialSessionDetails';
+import { publicTrialSessionDetailsRoute } from '@web-client-public/routes/trial-session-detail/PublicTrialSessionDetails';
 import { healthCheckRoute } from 'web-client-public/src/routes/health/HealthCheck';
 import { maintenanceRoute } from 'web-client-public/src/routes/maintenance/Maintenance';
 
