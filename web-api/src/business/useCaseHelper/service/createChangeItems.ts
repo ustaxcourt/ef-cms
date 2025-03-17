3ffab814aea445feb284c86adb5f298a39c512a3
zachrog <ztrogers1@gmail.com>
2025-03-17 10:07:51 -0700

    10103: Remove unused fields

--- web-api/src/business/useCaseHelper/service/createChangeItems.ts
+++ web-api/src/business/useCaseHelper/service/createChangeItems.ts
@@ -1,5 +1,4 @@
 import { AuthUser } from '@shared/business/entities/authUser/AuthUser';
-import { Case } from '../../../../../shared/src/business/entities/cases/Case';
 import {
   DOCKET_SECTION,
   DOCUMENT_PROCESSING_STATUS_OPTIONS,
@@ -144,21 +143,14 @@ const createWorkItemForChange = async ({
     {
       assigneeId: null,
       assigneeName: null,
-      associatedJudge: caseEntity.associatedJudge,
-      associatedJudgeId: caseEntity.associatedJudgeId,
-      caseStatus: caseEntity.status,
-      caseTitle: Case.getCaseTitle(caseEntity.caseCaption),
       docketEntry: {
         ...changeOfAddressDocketEntry.toRawObject(),
         createdAt: changeOfAddressDocketEntry.createdAt,
       },
       docketNumber: caseEntity.docketNumber,
-      docketNumberWithSuffix: caseEntity.docketNumberWithSuffix,
       section: DOCKET_SECTION,
       sentBy: user.name,
       sentByUserId: user.userId,
-      trialDate: caseEntity.trialDate,
-      trialLocation: caseEntity.trialLocation,
     }
   );
 
--- web-api/src/business/useCases/createCaseFromPaperInteractor.ts
+++ web-api/src/business/useCases/createCaseFromPaperInteractor.ts
@@ -37,23 +37,15 @@ const addPetitionDocketEntryWithWorkItemToCase = ({
     {
       assigneeId: user.userId,
       assigneeName: user.name,
-      associatedJudge: caseToAdd.associatedJudge,
-      associatedJudgeId: caseToAdd.associatedJudgeId,
-      caseIsInProgress: true,
-      caseStatus: caseToAdd.status,
-      caseTitle: Case.getCaseTitle(Case.getCaseCaption(caseToAdd)),
       docketEntry: {
         ...docketEntryEntity.toRawObject(),
         createdAt: docketEntryEntity.createdAt,
       },
       docketNumber: caseToAdd.docketNumber,
-      docketNumberWithSuffix: caseToAdd.docketNumberWithSuffix,
       section: user.section,
       sentBy: user.name,
       sentBySection: user.section,
       sentByUserId: user.userId,
-      trialDate: caseToAdd.trialDate,
-      trialLocation: caseToAdd.trialLocation,
     }
   );
 
