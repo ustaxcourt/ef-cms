b609d38d70950335d1385d81fd4f3f80cd00adfa
Christopher Bisom <cbisom@flexion.us>
2025-03-19 13:07:38 -0400

    10502-dxox: make cold case report more efficient

--- web-api/src/persistence/postgres/cases/reports/getColdCases.ts
+++ web-api/src/persistence/postgres/cases/reports/getColdCases.ts
@@ -11,9 +11,9 @@ import {
   formatNow,
 } from '@shared/business/utilities/DateHandler';
 import { getDbReader } from '@web-api/database';
-import { isEmpty } from 'lodash';
 import { sql } from 'kysely';
 
+// Get all general-docket cases for which the most recent filing is before coldCaseLookBackDate and which has no pending documents.
 export async function getColdCases() {
   const coldCaseLookBackDate = calculateDate({
     dateString: formatNow(),
@@ -21,64 +21,66 @@ export async function getColdCases() {
     units: 'days',
   });
 
-  // Get all of the docket numbers that fit the requirements
-  const validDocketNumbers = await getDbReader(reader =>
-    reader
-      .selectFrom('dwDocketEntry as d')
-      .select('d.docketNumber')
-      .groupBy('d.docketNumber')
-      // We want cases such that the most recent docket entry is before our cold case constant and such that no docket entries are pending
-      .having(eb => eb.fn.max('d.filingDate'), '<=', coldCaseLookBackDate)
-      .having(
-        eb => eb.fn.sum(sql`CASE WHEN d.pending THEN 1 ELSE 0 END`),
-        '=',
-        0,
-      )
-      .execute(),
-  );
-
-  if (isEmpty(validDocketNumbers)) {
-    return [];
-  }
-
   const rawResults = await getDbReader(reader =>
+    // First CTE: We will filter the cases to the correct status to avoid scanning all cases in the join
     reader
-      .selectFrom('dwCase as c')
-      // Limit to not-at-issue (general docket) cases
-      .where('c.status', '=', CASE_STATUS_TYPES.generalDocket)
-      .where(
-        'c.docketNumber',
-        'in',
-        validDocketNumbers.map(d => d.docketNumber),
+      .with('filteredCases', db =>
+        db
+          .selectFrom('dwCase')
+          .select([
+            'caseType',
+            'createdAt',
+            'docketNumber',
+            'docketNumberSuffix',
+            'leadDocketNumber',
+            'preferredTrialCity',
+          ])
+          .where('status', '=', CASE_STATUS_TYPES.generalDocket),
       )
+      // Second CTE: We will aggregate the docket entries, filtering out those with pending documents and whose most recent filing date is after coldCaseLookBackDate
+      .with('aggregatedEntries', db =>
+        db
+          .selectFrom('dwDocketEntry as d')
+          .innerJoin('filteredCases as fc', 'd.docketNumber', 'fc.docketNumber')
+          .select([
+            'd.docketNumber',
+            ({ fn }) => fn.max('d.filingDate').as('mostRecentFilingDate'),
+          ])
+          .groupBy('d.docketNumber')
+          .having(
+            ({ fn }) => fn.max('d.filingDate'),
+            '<=',
+            coldCaseLookBackDate,
+          )
+          .having(
+            ({ fn }) => fn.max(sql`CASE WHEN pending THEN 1 ELSE 0 END`),
+            '=',
+            0,
+          ),
+      )
+      // Final query: join the CTEs with docketEntry and grab what we need
+      .selectFrom('filteredCases as fc')
+      .innerJoin(
+        'aggregatedEntries as ae',
+        'fc.docketNumber',
+        'ae.docketNumber',
+      )
+      .innerJoin('dwDocketEntry as d', join =>
+        join
+          .onRef('d.docketNumber', '=', 'ae.docketNumber')
+          .onRef('d.filingDate', '=', 'ae.mostRecentFilingDate'),
+      )
+      .selectAll()
       .select([
-        'c.caseType',
-        'c.createdAt',
-        'c.docketNumber',
-        'c.docketNumberSuffix',
-        'c.leadDocketNumber',
-        'c.preferredTrialCity',
+        'fc.caseType',
+        'fc.createdAt',
+        'fc.docketNumber',
+        'fc.docketNumberSuffix',
+        'fc.leadDocketNumber',
+        'fc.preferredTrialCity',
+        'ae.mostRecentFilingDate',
+        'd.eventCode as mostRecentEventCode',
       ])
-      // Sub-select #1: most recent filingDate
-      .select(eb =>
-        eb
-          .selectFrom('dwDocketEntry as recent')
-          .select('recent.filingDate')
-          .whereRef('recent.docketNumber', '=', 'c.docketNumber')
-          .orderBy('recent.filingDate', 'desc')
-          .limit(1)
-          .as('mostRecentFilingDate'),
-      )
-      // Sub-select #2: eventCode from that same row
-      .select(eb =>
-        eb
-          .selectFrom('dwDocketEntry as recent')
-          .select('recent.eventCode')
-          .whereRef('recent.docketNumber', '=', 'c.docketNumber')
-          .orderBy('recent.filingDate', 'desc')
-          .limit(1)
-          .as('mostRecentEventCode'),
-      )
       .execute(),
   );
 
