66b9f8620921153add39ab605ab2fa5cbd78edf9
zachrog <ztrogers1@gmail.com>
2025-03-20 15:37:56 -0700

    dep-updates: WIP first working example of upgrading pdfjs-dist

--- web-api/terraform/modules/lambda/esbuildLambda.mjs
+++ web-api/terraform/modules/lambda/esbuildLambda.mjs
@@ -34,7 +34,7 @@ await esbuild.build({
         {
           from: [
             getPathFromRoot(
-              'node_modules/pdfjs-dist/legacy/build/pdf.worker.js',
+              'node_modules/pdfjs-dist/build/pdf.worker.mjs',
             ),
           ],
           keepStructure: true,
