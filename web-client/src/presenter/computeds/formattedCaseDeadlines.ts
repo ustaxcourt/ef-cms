92331eca98cff4e760c3279e40afa663c4804d2f
John Cruz <jcruz@flexion.us>
2025-03-20 08:48:26 -0600

    10576: Refactor helper and improve typing;

--- web-client/src/presenter/computeds/formattedCaseDeadlines.ts
+++ web-client/src/presenter/computeds/formattedCaseDeadlines.ts
@@ -1,41 +1,52 @@
 import { cloneDeep } from 'lodash';
 import { state } from '@web-client/presenter/app.cerebral';
+import { ClientApplicationContext } from '@web-client/applicationContext';
+import { Get } from 'cerebral';
+import { RawCaseDeadline } from '@shared/business/entities/CaseDeadline';
 
-const formatCaseDeadline = (applicationContext, caseDeadline) => {
-  const result = cloneDeep(caseDeadline);
-  result.deadlineDateFormatted = applicationContext
+type FormattedCaseDeadlinesType = RawCaseDeadline & {
+  deadlineDateFormatted: string;
+  overdue?: boolean;
+};
+
+const formatCaseDeadline = (
+  applicationContext: ClientApplicationContext,
+  _caseDeadline: RawCaseDeadline,
+): FormattedCaseDeadlinesType => {
+  const caseDeadline = cloneDeep(_caseDeadline);
+
+  const deadlineDateFormatted = applicationContext
     .getUtilities()
-    .formatDateString(result.deadlineDate, 'MMDDYY');
+    .formatDateString(caseDeadline.deadlineDate, 'MMDDYY');
 
   // use the app context utility function so the time zones match when comparing dates
   const deadlineDate = applicationContext
     .getUtilities()
-    .prepareDateFromString(result.deadlineDate);
+    .prepareDateFromString(caseDeadline.deadlineDate);
 
   const today = applicationContext.getUtilities().prepareDateFromString();
 
   const deadLineIsBeforeToday =
     deadlineDate.startOf('day') < today.startOf('day');
-  if (deadLineIsBeforeToday) {
-    result.overdue = true;
-  }
+
+  const result: FormattedCaseDeadlinesType = {
+    ...caseDeadline,
+    deadlineDateFormatted,
+    overdue: deadLineIsBeforeToday || undefined,
+  };
 
   return result;
 };
 
-import { ClientApplicationContext } from '@web-client/applicationContext';
-import { Get } from 'cerebral';
 export const formattedCaseDeadlines = (
   get: Get,
   applicationContext: ClientApplicationContext,
-): any => {
-  const caseDeadlines = get(state.caseDeadlines);
+): FormattedCaseDeadlinesType[] => {
+  const caseDeadlines = get(state.caseDeadlines) || [];
 
-  const caseDeadlinesFormatted = (caseDeadlines || [])
+  return caseDeadlines
     .map(d => formatCaseDeadline(applicationContext, d))
     .sort((a, b) =>
       String.prototype.localeCompare.call(a.deadlineDate, b.deadlineDate),
     );
-
-  return caseDeadlinesFormatted;
 };
