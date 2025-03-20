521ea26979549f025b4681aa2c21d7dcd4811251
Nechama Krigsman <nkrigsman@flexion.us>
2025-03-20 13:08:20 -0400

    10474: add additional search filters;

--- shared/src/proxies/public/getPublicPractitionersByNameProxy.ts
+++ shared/src/proxies/public/getPublicPractitionersByNameProxy.ts
@@ -5,12 +5,18 @@ import { get } from '../requests';
 export const getPublicPractitionersByNameInteractor = (
   applicationContext: ClientApplicationContext,
   {
+    admissionStatus,
     name,
-    practitionerType,
+    originalBarState,
     searchAfter,
+    practiceType,
+    practitionerType,
   }: {
+    admissionStatus?: string[];
     name: string;
-    practitionerType;
+    originalBarState?: string[];
+    practiceType?: string[];
+    practitionerType?: string;
     searchAfter: (string | number)[];
   },
 ): Promise<PractitionersByName> => {
@@ -18,9 +24,12 @@ export const getPublicPractitionersByNameInteractor = (
     applicationContext,
     endpoint: '/public-api/practitioners',
     params: {
+      admissionStatus,
       name,
-      practitionerType,
+      originalBarState,
       searchAfter,
+      practiceType,
+      practitionerType,
     },
   });
 };
--- web-client/src/presenter/actions/AdvancedSearch/updateAdvancedSearchFormAction.ts
+++ web-client/src/presenter/actions/AdvancedSearch/updateAdvancedSearchFormAction.ts
@@ -5,12 +5,29 @@ export const updateAdvancedSearchFormAction =
   ({
     props,
     store,
+    get,
   }: ActionProps<{
     formType: string;
     key: string;
     value: string;
+    isArray?: boolean;
   }>) => {
     const formType = formName || props.formType;
+
+    if (props.isArray) {
+      const currentValue =
+        get(state.advancedSearchForm[formType][props.key]) || [];
+      const updatedValue = currentValue.includes(props.value)
+        ? currentValue.filter((value: string) => value !== props.value)
+        : [...currentValue, props.value];
+      if (updatedValue.length > 0) {
+        store.set(state.advancedSearchForm[formType][props.key], updatedValue);
+      } else {
+        store.unset(state.advancedSearchForm[formType][props.key]); // Unset if empty
+      }
+      return;
+    }
+
     if (props.value) {
       store.set(state.advancedSearchForm[formType][props.key], props.value);
     } else {
