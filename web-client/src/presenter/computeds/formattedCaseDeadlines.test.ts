d87711cd8f659c5c92628640825cc3ca738e2a61
John Cruz <jcruz@flexion.us>
2025-03-21 10:15:51 -0600

    10576: Extend unit test coverage;

--- web-client/src/presenter/computeds/formattedCaseDeadlines.test.ts
+++ web-client/src/presenter/computeds/formattedCaseDeadlines.test.ts
@@ -74,5 +74,46 @@ describe('formattedCaseDeadlines', () => {
     expect(result.length).toEqual(0);
   });
 
-	it('')
+  it('should not display the edit and delete link when the case deadline is associated to a child case', () => {
+    const caseDeadlines = [
+      {
+        caseDeadlineId: '1',
+        consolidatedCaseDeadlineId: undefined,
+        deadlineDate: '2019-06-30T04:00:00.000Z',
+      },
+      {
+        caseDeadlineId: '2',
+        consolidatedCaseDeadlineId: '1',
+        deadlineDate: '2019-01-30T05:00:00.000Z',
+      },
+      {
+        caseDeadlineId: '3',
+        consolidatedCaseDeadlineId: undefined,
+        deadlineDate: '2025-07-30T04:00:00.000Z',
+      },
+    ];
+
+    const result = runCompute(formattedCaseDeadlines, {
+      state: {
+        caseDeadlines,
+        caseDetail: { leadDocketNumber: '101-25' },
+      },
+    });
+
+    expect(result.length).toEqual(3);
+    expect(result).toMatchObject([
+      {
+        caseDeadlineId: '2',
+        displayEditAndDeleteLink: false,
+      },
+      {
+        caseDeadlineId: '1',
+        displayEditAndDeleteLink: true,
+      },
+      {
+        caseDeadlineId: '3',
+        displayEditAndDeleteLink: true,
+      },
+    ]);
+  });
 });
