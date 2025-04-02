f070d5aa2a35bc921b5bb2b2543394ce6ab3562d
Christopher Bisom <cbisom@flexion.us>
2025-04-02 14:56:56 -0400

    10502-dxox: update mapper to fix blockedReason and automaticBlockedReason not clearing; update schema and migration file to remove unused column judgeUserId

--- web-api/src/persistence/postgres/cases/mapper.ts
+++ web-api/src/persistence/postgres/cases/mapper.ts
@@ -19,12 +19,12 @@ export const toKyselyNewCase = (rawCase: RawCase) => {
     automaticBlockedDate: rawCase.automaticBlockedDate
       ? calculateDate({ dateString: rawCase.automaticBlockedDate })
       : null,
-    automaticBlockedReason: rawCase.automaticBlockedReason,
+    automaticBlockedReason: rawCase.automaticBlockedReason || null,
     blocked: rawCase.blocked,
     blockedDate: rawCase.blockedDate
       ? calculateDate({ dateString: rawCase.blockedDate })
       : null,
-    blockedReason: rawCase.blockedReason,
+    blockedReason: rawCase.blockedReason || null,
     caption: rawCase.caseCaption,
     caseNote: rawCase.caseNote,
     caseType: rawCase.caseType,
@@ -49,7 +49,6 @@ export const toKyselyNewCase = (rawCase: RawCase) => {
       : null,
     isPaper: rawCase.isPaper,
     isSealed: rawCase.isSealed,
-    judgeUserId: rawCase.judgeUserId,
     leadDocketNumber: rawCase.leadDocketNumber || null,
     litigationCosts: rawCase.litigationCosts,
     mailingDate: rawCase.mailingDate,
