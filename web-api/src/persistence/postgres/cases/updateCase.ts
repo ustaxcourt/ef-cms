d62cc452c82bb647c3ebc592e2b9313ae7422d0a
Christopher Bisom <cbisom@flexion.us>
2025-04-02 14:35:13 -0400

    10502-dxox: fix bug with statistics not updating when they are deleted, and preemptively address a similar potential issue for petitioners

--- web-api/src/persistence/postgres/cases/updateCase.ts
+++ web-api/src/persistence/postgres/cases/updateCase.ts
@@ -5,6 +5,8 @@ import { upsertPetitionersOnCase } from '@web-api/persistence/postgres/cases/par
 import { upsertCaseStatistics } from '@web-api/persistence/postgres/cases/statistics/upsertCaseStatistics';
 import { pgUpdateTable } from '@web-api/persistence/postgres/utils/operation/pgUpdateTable';
 import { isEmpty } from 'lodash';
+import { clearCaseStatistics } from '@web-api/persistence/postgres/cases/statistics/clearCaseStatistics';
+import { clearPetitionersOnCase } from '@web-api/persistence/postgres/cases/parties/clearPetitionersOnCase';
 
 export const updateCase = async ({
   caseToUpdate,
@@ -20,26 +22,44 @@ export const updateCase = async ({
   // Because we used to have nested objects in our case records, we upserted everything.
   // Now, with separate tables, we need to update these separate tables as well.
   // In the future, we should try to avoid upserting everything.
-  await upsertCaseStatusUpdates({
-    docketNumber: caseToUpdate.docketNumber,
-    statusUpdates: caseToUpdate.caseStatusHistory as CaseStatusChange[],
-  });
-
-  await upsertPetitionersOnCase({
-    docketNumber: caseToUpdate.docketNumber,
-    petitionerCase: caseToUpdate,
-  });
-
-  if (caseToUpdate.statistics) {
-    await upsertCaseStatistics({
+  await Promise.all([
+    upsertCaseStatusUpdates({
       docketNumber: caseToUpdate.docketNumber,
-      statistics: caseToUpdate.statistics,
-    });
-  }
+      statusUpdates: caseToUpdate.caseStatusHistory as CaseStatusChange[],
+    }),
+    clearAndUpdatePetitioners({ caseToUpdate }),
+    clearAndUpsertStatistics({ caseToUpdate }),
+  ]);
 
   if (isEmpty(updatedCase)) {
     throw new Error('could not update the case');
   }
 
   return caseToUpdate;
+};
+
+const clearAndUpdatePetitioners = async ({
+  caseToUpdate,
+}: {
+  caseToUpdate: RawCase;
+}) => {
+  await clearPetitionersOnCase({ docketNumber: caseToUpdate.docketNumber });
+  await upsertPetitionersOnCase({
+    docketNumber: caseToUpdate.docketNumber,
+    petitionerCase: caseToUpdate,
+  });
+};
+
+const clearAndUpsertStatistics = async ({
+  caseToUpdate,
+}: {
+  caseToUpdate: RawCase;
+}) => {
+  await clearCaseStatistics({ docketNumber: caseToUpdate.docketNumber });
+  if (caseToUpdate.statistics) {
+    await upsertCaseStatistics({
+      docketNumber: caseToUpdate.docketNumber,
+      statistics: caseToUpdate.statistics,
+    });
+  }
 };
