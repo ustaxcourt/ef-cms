603035f9beeae4672efbe29e6db257481cff6f96
Christopher Bisom <cbisom@flexion.us>
2025-04-03 12:37:34 -0400

    10502-dxox: get statistics as part of getCaseMetadataByDocketNumber

--- web-api/src/persistence/postgres/cases/getCaseMetadataByDocketNumber.ts
+++ web-api/src/persistence/postgres/cases/getCaseMetadataByDocketNumber.ts
@@ -2,6 +2,7 @@ import { fromKyselyCase } from '@web-api/persistence/postgres/cases/mapper';
 import { getDbReader } from '@web-api/database';
 import { sql } from 'kysely';
 import { Case } from '@shared/business/entities/cases/Case';
+import { getCaseStatistics } from '@web-api/persistence/postgres/cases/statistics/getCaseStatistics';
 
 export const getCaseMetadataByDocketNumber = async ({
   docketNumber,
@@ -14,27 +15,35 @@ export const getCaseMetadataByDocketNumber = async ({
     >
   | undefined
 > => {
-  const dbCase = await getDbReader(reader =>
-    reader
-      .selectFrom('dwCase as c')
-      .leftJoin('dwPetitionerOnCase as p', 'c.docketNumber', 'p.docketNumber')
-      .selectAll('c')
-      .select(
-        sql`jsonb_agg(to_jsonb(p) ORDER BY p.order_on_case)`.as('petitioners'),
-      )
-      .where('c.docketNumber', '=', docketNumber)
-      .groupBy('c.docketNumber')
-      .executeTakeFirst(),
-  );
+  const [dbCaseMetadata, statistics] = await Promise.all([
+    getDbReader(reader =>
+      reader
+        .selectFrom('dwCase as c')
+        .leftJoin('dwPetitionerOnCase as p', 'c.docketNumber', 'p.docketNumber')
+        .selectAll('c')
+        .select(
+          sql`jsonb_agg(to_jsonb(p) ORDER BY p.order_on_case)`.as(
+            'petitioners',
+          ),
+        )
+        .where('c.docketNumber', '=', docketNumber)
+        .groupBy('c.docketNumber')
+        .executeTakeFirst(),
+    ),
+    getCaseStatistics({ docketNumber }),
+  ]);
 
   // Note that json_agg will get [null] if there are no petitioners on the case, so filter out nulls
-  return dbCase
+  return dbCaseMetadata
     ? fromKyselyCase({
-        ...dbCase,
-        petitioners: (dbCase.petitioners as TPetitioner[]).filter(p => p), // This is a hack because our typing for Petitioners is yucky
+        ...dbCaseMetadata,
+        statistics,
+        petitioners: (dbCaseMetadata.petitioners as TPetitioner[]).filter(
+          p => p,
+        ), // This is a hack because our typing for Petitioners is yucky
         docketNumberWithSuffix: Case.getDocketNumberWithSuffix({
-          docketNumber: dbCase.docketNumber,
-          docketNumberSuffix: dbCase.docketNumberSuffix,
+          docketNumber: dbCaseMetadata.docketNumber,
+          docketNumberSuffix: dbCaseMetadata.docketNumberSuffix,
         }),
       })
     : undefined;
