7e97c4ed8b0de21837456c4a9661f09a70a17681
Christopher Bisom <cbisom@flexion.us>
2025-04-03 17:09:28 -0400

    10502-dxox: fix stupid mistake with processPractitionerMappingEntries

--- web-api/src/business/useCases/processStreamRecords/processPractitionerMappingEntries.ts
+++ web-api/src/business/useCases/processStreamRecords/processPractitionerMappingEntries.ts
@@ -7,9 +7,8 @@ import type {
 import type { ServerApplicationContext } from '@web-api/applicationContext';
 import { getCaseMetadataWithCounsel } from '@web-api/persistence/postgres/cases/getCaseMetadataWithCounsel';
 import { getLogger } from '@web-api/utilities/logger/getLogger';
-import { get, query } from '@web-api/persistence/dynamodbClientService';
-import { aggregateCaseItems } from '@web-api/persistence/dynamo/helpers/aggregateCaseItems';
 import { transformNullToUndefined } from '@web-api/persistence/postgres/utils/transformNullToUndefined';
+import { getCaseDataFromDynamo } from '@web-api/business/useCases/processStreamRecords/getCaseDataFromDynamo';
 
 export const processPractitionerMappingEntries = async ({
   applicationContext,
@@ -46,7 +45,7 @@ export const processPractitionerMappingEntries = async ({
           `Failed to find case ${practitionerMappingData.pk.S} in postgres in processPractitionerMappingEntries: ${e}.
           If this occurred in a test or as part of re-indexing during a blue-green migration, it is safe to ignore.`,
         );
-        caseRecord = await exports.getCaseDataFromDynamo({
+        caseRecord = await getCaseDataFromDynamo({
           applicationContext,
           docketNumber,
         });
@@ -164,52 +163,4 @@ const getCaseDataFromPostgres = async ({
     { removeUndefinedValues: true },
   );
   return marshalledCase;
-};
-
-// TODO: Delete after 10502 is finished
-export const getCaseDataFromDynamo = async ({
-  applicationContext,
-  docketNumber,
-}: {
-  applicationContext: ServerApplicationContext;
-  docketNumber: string;
-}) => {
-  const caseItems = [
-    await get({
-      Key: {
-        pk: `case|${docketNumber}`,
-        sk: `case|${docketNumber}`,
-      },
-      applicationContext,
-    }),
-
-    ...(await query({
-      ExpressionAttributeNames: {
-        '#pk': 'pk',
-        '#sk': 'sk',
-      },
-      ExpressionAttributeValues: {
-        ':pk': `case|${docketNumber}`,
-        ':prefix': 'privatePractitioner',
-      },
-      KeyConditionExpression: '#pk = :pk and begins_with(#sk, :prefix)',
-      applicationContext,
-    })),
-
-    ...(await query({
-      ExpressionAttributeNames: {
-        '#pk': 'pk',
-        '#sk': 'sk',
-      },
-      ExpressionAttributeValues: {
-        ':pk': `case|${docketNumber}`,
-        ':prefix': 'irsPractitioner',
-      },
-      KeyConditionExpression: '#pk = :pk and begins_with(#sk, :prefix)',
-      applicationContext,
-    })),
-  ];
-
-  const unmarshalledCase = aggregateCaseItems(caseItems);
-  return marshall(unmarshalledCase);
 };
