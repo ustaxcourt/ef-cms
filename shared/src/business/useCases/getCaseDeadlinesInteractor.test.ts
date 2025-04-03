7a7fe88d8acad192ed6147fd4624d3e2568e3100
Christopher Bisom <cbisom@flexion.us>
2025-04-03 12:43:46 -0400

    10502-dxox: fix type error and resulting broken test in getCaseDeadlinesInteractor.test.ts

--- shared/src/business/useCases/getCaseDeadlinesInteractor.test.ts
+++ shared/src/business/useCases/getCaseDeadlinesInteractor.test.ts
@@ -186,7 +186,6 @@ describe('getCaseDeadlinesInteractor', () => {
 
   it('passes null for judgeId to getCaseDeadlinesByDateRange persistence call when chief judge is requested', async () => {
     await getCaseDeadlinesInteractor(
-      applicationContext,
       {
         endDate: END_DATE,
         from: 0,
