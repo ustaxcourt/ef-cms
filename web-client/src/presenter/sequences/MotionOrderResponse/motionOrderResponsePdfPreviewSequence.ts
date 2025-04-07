9549bb8597a01eb1d74367f46ee25f5f9eff6f1f
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2025-04-07 14:52:30 -0500

    10586: remove unused action

--- web-client/src/presenter/sequences/MotionOrderResponse/motionOrderResponsePdfPreviewSequence.ts
+++ web-client/src/presenter/sequences/MotionOrderResponse/motionOrderResponsePdfPreviewSequence.ts
@@ -14,7 +14,6 @@ export const motionOrderResponsePdfPreviewSequence =
     createOrderAction,
     clearPdfPreviewUrlAction,
     getPdfUrlAction,
-    // getMotionOrderResponsePdfUrlAction,
     getPdfFromUrlAction,
     setPdfFileAction,
     setPdfPreviewUrlAction,
