5ae6a6a4de61bb27b890d2d0e9f0d71f53fc63eb
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2025-04-07 15:03:56 -0500

    10586: consolidate isEditStatusReportOrderAction and isEditMotionOrderResponseAction

--- web-client/src/presenter/actions/MotionOrderResponse/isEditMotionOrderResponseAction.ts
+++
@@ -1,6 +0,0 @@
-export const isEditMotionOrderResponseAction = ({ path, props }: ActionProps) => {
-    if (props.isEditing) {
-      return path.edit({ docketEntryIdToEdit: props.docketEntryId });
-    }
-    return path.create();
-  };
