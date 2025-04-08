cd86bb0c39a13782bcd956f238c60169a323c394
Pavan <pyenduri@flexion.us>
2025-04-08 10:54:28 -0500

    10506: WIP create lambda that will delete all expired records in the Database;

---
+++ web-api/src/lambdas/rdsExpiredRecordsCleanup/rdsExpiredRecordsCleanupLambda.ts
@@ -0,0 +1,29 @@
+import { getDbReader } from '@web-api/database';
+import { pgDeleteFrom } from '@web-api/persistence/postgres/utils/operation/pgDeleteFrom';
+import type { Handler } from 'aws-lambda';
+
+const EXPIRATION_COLUMN = 'expirationDate';
+
+export const handler: Handler = async (_event, context) => {
+  await getDbReader(async reader => {
+    const ALL_TABLES = await reader.introspection.getTables();
+    const TABLES_WITH_EXPIRATIONS = ALL_TABLES.filter(tableMetaData => {
+      return tableMetaData.columns.some(
+        columnMetaData => columnMetaData.name === EXPIRATION_COLUMN,
+      );
+    });
+
+    await Promise.all(
+      TABLES_WITH_EXPIRATIONS.map(tableMetaData => async () => {
+        const { name: tableName } = tableMetaData;
+        await pgDeleteFrom({
+          // fix the type
+          table: tableName as any,
+          where: cb =>
+            cb.where(EXPIRATION_COLUMN, '<', Math.floor(Date.now() / 1000)),
+        });
+      }),
+    );
+  });
+  context.succeed('Completed Cleanup!');
+};
