6d4d457a9d81edb19869991131e95020d6a346db
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2025-04-08 12:23:06 -0500

    10586: remove unused motionOrderResponseFormHelper variables

--- web-client/src/presenter/computeds/motionOrderResponseFormHelper.ts
+++ web-client/src/presenter/computeds/motionOrderResponseFormHelper.ts
@@ -8,39 +8,14 @@ export const motionOrderResponseFormHelper = (
   applicationContext: ClientApplicationContext,
 ): any => {
   const { DATE_FORMATS } = applicationContext.getConstants();
-  const form = get(state.form);
   const { status } = get(state.caseDetail);
   const showStrikeCheckBox = status === CASE_STATUS_TYPES.calendared;
-
-  const { customText } = form;
-
-  const CUSTOM_ORDER_MAX_LENGTH = 60;
-  const customOrderTextCharacterCount = customText?.length
-    ? CUSTOM_ORDER_MAX_LENGTH - customText?.length
-    : CUSTOM_ORDER_MAX_LENGTH;
-
-  // TODO 10586: fix this
-  const canSaveOrderRespones =
-    !!form.disposition && get(state.pdfForSigning.stampApplied);
 
   const minDate = applicationContext
     .getUtilities()
     .formatNow(DATE_FORMATS.YYYYMMDD);
 
-  const validationErrors = get(state.validationErrors);
-  const dateErrorClass = !validationErrors.date
-    ? 'order-response-form-group'
-    : 'order-response-form-group-error';
-
-  const dispositionErrorClass = !validationErrors.disposition
-    ? 'order-response-form-group'
-    : 'order-response-form-group-error';
-
   return {
-    canSaveOrderRespones,
-    customOrderTextCharacterCount,
-    dateErrorClass,
-    dispositionErrorClass,
     minDate,
     showStrikeCheckBox,
   };
