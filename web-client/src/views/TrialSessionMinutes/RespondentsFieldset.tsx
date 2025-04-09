e55dcdfa186c2bf4e2d374bc1c35bde41f73cd5f
Andy Kuny <akuny@flexion.us>
2025-04-09 12:09:57 -0400

    10419: Use typed value when "blurring" off respondent input without clicking "Use..." option

--- web-client/src/views/TrialSessionMinutes/RespondentsFieldset.tsx
+++ web-client/src/views/TrialSessionMinutes/RespondentsFieldset.tsx
@@ -67,6 +67,20 @@ export const RespondentsFieldset = ({
                       value: inputValue?.value || '',
                     })
                   }
+                  onBlur={e => {
+                    if (e.target.value) {
+                      onChangeHandler({
+                        name: 'respondents',
+                        rowInfo: {
+                          key: row.renderKey,
+                          nestedName: 'name',
+                        },
+                        section:
+                          MINUTE_SHEET_FORM_SECTION_MAP.respondentsSection,
+                        value: e.target.value,
+                      });
+                    }
+                  }}
                 />
               </FormGroup>
             </div>
