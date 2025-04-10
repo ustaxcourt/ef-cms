bc7a2897b763842a877528816eb5225e9f0d0fa9
zachrog <ztrogers1@gmail.com>
2025-04-10 10:37:10 -0700

    10494: Update script to not use array of arrays

--- scripts/run-once-scripts/postgres-migration/migrate-all-docket-entries.ts
+++ scripts/run-once-scripts/postgres-migration/migrate-all-docket-entries.ts
@@ -4,7 +4,6 @@ import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
 import { DynamoDBClient, ScanCommandInput } from '@aws-sdk/client-dynamodb';
 import { environment } from '@web-api/environment';
 import { TDynamoRecord } from '@web-api/persistence/dynamo/dynamoTypes';
-import { unmarshall } from '@aws-sdk/util-dynamodb';
 import { upsertDocketEntries } from '@web-api/persistence/postgres/docketEntries/upsertDocketEntries';
 
 const dynamoDbClient = new DynamoDBClient({ region: 'us-east-1' });
@@ -13,10 +12,6 @@ const documentClient = DynamoDBDocument.from(dynamoDbClient, {
 });
 const totalSegments = 10;
 let itemsScanned = 0;
-const docketEntries: RawDocketEntry[][] = Array.from({
-  length: totalSegments,
-}).map(_ => []);
-const BATCH_SIZE = 100;
 
 async function main() {
   await Promise.all(
@@ -30,7 +25,11 @@ async function main() {
   );
 }
 
-async function scanDynamoAndBatchUpsertDocketEntriesToPostgres(params: ScanCommandInput) {
+async function scanDynamoAndBatchUpsertDocketEntriesToPostgres(
+  params: ScanCommandInput,
+) {
+  let docketEntries: RawDocketEntry[] = [];
+  const BATCH_SIZE = 100;
   let lastEvaluatedKey: typeof params.ExclusiveStartKey | undefined = undefined;
 
   do {
@@ -43,23 +42,20 @@ async function scanDynamoAndBatchUpsertDocketEntriesToPostgres(params: ScanComma
 
     for (const record of items as TDynamoRecord[]) {
       if (record.sk.startsWith('docket-entry|')) {
-        const unmarshalledRecord = unmarshall(
-          record.dynamodb.NewImage,
-        ) as RawDocketEntry;
-        docketEntries[params.Segment!].push(unmarshalledRecord);
-        if (docketEntries[params.Segment!].length >= BATCH_SIZE) {
-          console.log(
-            `Migrating ${BATCH_SIZE} DocketEntry records for scan segment ${params.Segment!}`,
-          );
-          await upsertDocketEntries(docketEntries[params.Segment!]);
-          docketEntries[params.Segment!].length = 0;
-          console.log(
-            `Successfully migrated ${BATCH_SIZE} DocketEntry records for scan segment ${params.Segment!}`,
-          );
-        }
+        docketEntries.push(record as unknown as RawDocketEntry);
       }
     }
 
+    if (docketEntries.length >= BATCH_SIZE) {
+      console.log(
+        `Migrating ${BATCH_SIZE} DocketEntry records for scan segment ${params.Segment!}`,
+      );
+      await upsertDocketEntries(docketEntries);
+      docketEntries = [];
+      console.log(
+        `Successfully migrated ${BATCH_SIZE} DocketEntry records for scan segment ${params.Segment!}`,
+      );
+    }
     itemsScanned += items.length;
     console.log('itemsScanned:', itemsScanned);
 
