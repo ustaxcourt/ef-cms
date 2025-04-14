001fd614f1f1f3a3c0552c5cb10e4c074c06121e
zachrog <ztrogers1@gmail.com>
2025-04-14 08:15:19 -0700

    10494: docs

--- scripts/run-once-scripts/readBigFile.ts
+++ scripts/run-once-scripts/readBigFile.ts
@@ -1,16 +1,24 @@
 import { createReadStream } from 'fs';
 import readline from 'readline';
 
+const uniqueKeysMap: Map<string, number> = new Map();
+
 const rl = readline.createInterface({
-  input: createReadStream('allTestDocketEntries.txt'),
+  input: createReadStream(
+    '/Users/zacharyrogers/Documents/allTestDocketEntries.txt',
+  ),
   crlfDelay: Infinity,
 });
 
 rl.on('line', line => {
   const obj = JSON.parse(line);
-  console.log('Got item:', obj);
+  Object.keys(obj).forEach((key) => {
+    const currentCount = uniqueKeysMap.get(key) || 0;
+    uniqueKeysMap.set(key, currentCount + 1);
+  });
 });
 
 rl.on('close', () => {
   console.log('Done!');
+  console.log(uniqueKeysMap)
 });
