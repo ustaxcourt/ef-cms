95ba4448d767bc696aaaf1539a303e30f98bb619
Christopher Bisom <cbisom@flexion.us>
2025-04-15 16:01:53 -0400

    10502-dxox: update mocks to include case status updates

--- web-api/src/persistence/postgres/cases/mocks.jest.ts
+++ web-api/src/persistence/postgres/cases/mocks.jest.ts
@@ -156,3 +156,13 @@ jest.mock(
   '@web-api/persistence/postgres/cases/statistics/clearCaseStatistics',
   () => mockFactory('clearCaseStatistics'),
 );
+
+// Case status updates
+
+jest.mock('@web-api/persistence/postgres/cases/getCaseStatusHistory', () =>
+  mockFactory('getCaseStatusHistory'),
+);
+
+jest.mock('@web-api/persistence/postgres/cases/upsertCaseStatusUpdates', () =>
+  mockFactory('upsertCaseStatusUpdates'),
+);
