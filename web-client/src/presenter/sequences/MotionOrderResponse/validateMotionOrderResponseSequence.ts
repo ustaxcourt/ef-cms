cc19dbfa0d517e47bc4fd456c1955ec54d181577
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2025-04-17 16:15:55 -0500

    10586: feedback; validation, verbiage

--- web-client/src/presenter/sequences/MotionOrderResponse/validateMotionOrderResponseSequence.ts
+++ web-client/src/presenter/sequences/MotionOrderResponse/validateMotionOrderResponseSequence.ts
@@ -2,12 +2,19 @@ import { clearAlertsAction } from '@web-client/presenter/actions/clearAlertsActi
 import { setValidationAlertErrorsAction } from '@web-client/presenter/actions/setValidationAlertErrorsAction';
 import { setValidationErrorsAction } from '../../actions/setValidationErrorsAction';
 import { validateMotionOrderResponseFormAction } from '../../actions/MotionOrderResponse/validateMotionOrderResponseFormAction';
+import { shouldValidateAction } from '@web-client/presenter/actions/shouldValidateAction';
 
 export const validateMotionOrderResponseSequence = [
-  clearAlertsAction,
-  validateMotionOrderResponseFormAction,
+  shouldValidateAction,
   {
-    error: [setValidationErrorsAction, setValidationAlertErrorsAction],
-    success: [clearAlertsAction],
+    ignore: [],
+    validate: [
+      clearAlertsAction,
+      validateMotionOrderResponseFormAction,
+      {
+        error: [setValidationErrorsAction, setValidationAlertErrorsAction],
+        success: [clearAlertsAction],
+      },
+    ],
   },
 ] as unknown as () => void;
