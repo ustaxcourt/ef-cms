1e47ce1ef85cb2c868e8c5683068fab5036dcc28
Nate Elliott <nelliott@flexion.us>
2025-04-21 13:35:29 -0500

    10612: return updated petitioner instead of relying on read-after-write in frontend (via reload)

--- web-client/src/presenter/sequences/removePetitionerEmailSequence.ts
+++ web-client/src/presenter/sequences/removePetitionerEmailSequence.ts
@@ -2,30 +2,14 @@ import { removePetitionerEmailAction } from '@web-client/presenter/actions/CaseA
 import { clearModalAction } from '../actions/clearModalAction';
 import { setAlertSuccessAction } from '../actions/setAlertSuccessAction';
 import { showProgressSequenceDecorator } from '../utilities/showProgressSequenceDecorator';
-import { navigateToCaseDetailCaseInformationActionFactory } from '@web-client/presenter/actions/navigateToCaseDetailCaseInformationActionFactory';
-import { setSaveAlertsForNavigationAction } from '@web-client/presenter/actions/setSaveAlertsForNavigationAction';
 import { clearModalStateAction } from '@web-client/presenter/actions/clearModalStateAction';
 import { setAlertErrorAction } from '@web-client/presenter/actions/setAlertErrorAction';
-import { state } from '@web-client/presenter/app.cerebral';
 
 export const removePetitionerEmailSequence = showProgressSequenceDecorator([
   clearModalAction,
   removePetitionerEmailAction,
   {
     error: [setAlertErrorAction, clearModalAction, clearModalStateAction],
-    success: [
-      setSaveAlertsForNavigationAction,
-      setAlertSuccessAction,
-      clearModalAction,
-      clearModalStateAction,
-      // This is a workaround to force the router (in navigateToCaseDetailCaseInformationActionFactory)
-      // to re-run the router handler in the event that the user is removing the email
-      // immediately after having edited the petitioner details via the edit form.
-      ({ get, router }) => {
-        const { docketNumber } = get(state.caseDetail);
-        router.route(`/case-detail/${docketNumber}`);
-      },
-      navigateToCaseDetailCaseInformationActionFactory('parties'),
-    ],
+    success: [setAlertSuccessAction, clearModalAction, clearModalStateAction],
   },
 ]) as unknown as () => void;
