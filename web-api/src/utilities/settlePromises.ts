3a50369b6e0df00f0048a5ee3f43d902be6dea46
zachrog <ztrogers1@gmail.com>
2025-04-24 15:56:48 -0700

    10502-dxox: Fix typing on new helper function

--- web-api/src/utilities/settlePromises.ts
+++ web-api/src/utilities/settlePromises.ts
@@ -1,14 +1,27 @@
 import { getLogger } from '@web-api/utilities/logger/getLogger';
 
-export async function settlePromises<T>(
-  promises: Promise<T>[],
-): Promise<Awaited<T>[]> {
+export async function settlePromises<T extends readonly unknown[]>(
+  promises: readonly [...T],
+): Promise<{ [K in keyof T]: Awaited<T[K]> }> {
   const results = await Promise.allSettled(promises);
-  const successes = results.filter(result => result.status === 'fulfilled');
-  const failures = results.filter(result => result.status === 'rejected');
-  if (failures.length) {
-    failures.forEach(failure => getLogger().error(failure.reason));
-    throw new Error(`${failures.length} promises rejected in settlePromises`);
+
+  const errors: unknown[] = [];
+
+  results.forEach(result => {
+    if (result.status === 'rejected') {
+      getLogger().error(result.reason);
+      errors.push(result.reason);
+    }
+  });
+
+  if (errors.length > 0) {
+    throw new Error(`${errors.length} promises rejected in settlePromises`);
   }
-  return successes;
+
+  return results.map(
+    result => (result as PromiseFulfilledResult<unknown>).value,
+  ) as {
+    [K in keyof T]: Awaited<T[K]>;
+  };
 }
+
