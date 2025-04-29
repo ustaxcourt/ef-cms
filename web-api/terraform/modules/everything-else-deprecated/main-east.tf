bb9094764ef6b8b0652037244ac0e8394f19c82b
Kaitlyn Swann <kswann@flexion.us>
2025-04-29 16:10:27 -0600

    10502: add missing weighted routing to dns

--- web-api/terraform/modules/everything-else-deprecated/main-east.tf
+++ web-api/terraform/modules/everything-else-deprecated/main-east.tf
@@ -92,8 +92,8 @@ resource "aws_route53_record" "api_route53_main_east_regional_record" {
     evaluate_target_health = true
   }
 
-  latency_routing_policy {
-    region = "us-east-1"
+  weighted_routing_policy {
+    weight = var.route_53_regional_weight
   }
 }
 
--- web-api/terraform/modules/everything-else-deprecated/main-west.tf
+++ web-api/terraform/modules/everything-else-deprecated/main-west.tf
@@ -116,8 +116,8 @@ resource "aws_route53_record" "public_api_route53_main_west_regional_record" {
     evaluate_target_health = true
   }
 
-  latency_routing_policy {
-    region = "us-west-1"
+  weighted_routing_policy {
+    weight = var.route_53_regional_weight
   }
 }
 
