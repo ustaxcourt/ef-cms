b8335745f41b4a476edc7a3e223c1bcdd019ae8a
Pavan <pyenduri@flexion.us>
2025-05-01 11:38:01 -0500

    10507: Create a run once script to migrate the current values in deploy dynamo table and migrate to SSM Parameter store;

---
+++ scripts/run-once-scripts/migrate-configuration-dynamo-to-ssm.ts
@@ -0,0 +1,47 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { PutParameterCommand, SSMClient } from '@aws-sdk/client-ssm';
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
+
+const configurations = [
+  'migration-queue-empty',
+  'destination-table-version',
+  'maintenance-mode',
+  'current-color',
+];
+
+const { STAGE } = process.env;
+
+async function script() {
+  const DYNAMO_CLIENT = new DynamoDBClient({
+    region: 'us-east-1',
+  });
+
+  const DOCUMENT_CLIENT = DynamoDBDocument.from(DYNAMO_CLIENT, {
+    marshallOptions: { removeUndefinedValues: true },
+  });
+
+  const SSM_CLIENT = new SSMClient({ region: 'us-east-1' });
+
+  for (const index in configurations) {
+    const config = configurations[index];
+    const configRecord = await DOCUMENT_CLIENT.get({
+      Key: { pk: config, sk: config },
+      TableName: `efcms-deploy-${STAGE}`,
+    });
+
+    const CURRENT_VALUE = configRecord?.Item?.current;
+    if (!CURRENT_VALUE && typeof CURRENT_VALUE !== 'boolean') {
+      console.log('Could not find value for ->', config);
+      continue;
+    }
+    const SSM_COMMAND = new PutParameterCommand({
+      Name: `/DAWSON/${STAGE}/${config}`,
+      Value: CURRENT_VALUE.toString(),
+      Type: 'String',
+      Overwrite: true,
+    });
+    await SSM_CLIENT.send(SSM_COMMAND);
+  }
+}
+
+void script();
