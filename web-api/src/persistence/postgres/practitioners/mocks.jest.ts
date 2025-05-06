bc1362e64c3e8c972796132ffab13168548a68af
Andy Kuny <akuny@flexion.us>
2025-05-06 13:29:48 -0400

    10495: update practitioner mocks

--- web-api/src/persistence/postgres/practitioners/mocks.jest.ts
+++ web-api/src/persistence/postgres/practitioners/mocks.jest.ts
@@ -1,6 +1,66 @@
 import { mockFactory } from '@shared/test/mockFactory';
 
 jest.mock(
+  '@web-api/persistence/postgres/practitioners/barNumberGenerator',
+  () => mockFactory('barNumberGenerator'),
+);
+
+jest.mock(
+  '@web-api/persistence/postgres/practitioners/createNewPractitionerUser',
+  () => mockFactory('createNewPractitionerUser'),
+);
+
+jest.mock(
+  '@web-api/persistence/postgres/practitioners/createOrUpdatePractitionerUser',
+  () => mockFactory('createOrUpdatePractitionerUser'),
+);
+
+jest.mock(
+  '@web-api/persistence/postgres/practitioners/deletePractitionerRecord',
+  () => mockFactory('deletePractitionerRecord'),
+);
+
+jest.mock(
+  '@web-api/persistence/postgres/practitioners/getIrsPractitionerOnCase',
+  () => mockFactory('getIrsPractitionerOnCase'),
+);
+
+jest.mock(
+  '@web-api/persistence/postgres/practitioners/getPractitionersByDocketNumber',
+  () => mockFactory('getPractitionersByDocketNumber'),
+);
+
+jest.mock(
+  '@web-api/persistence/postgres/practitioners/getPractitionerByBarNumber',
+  () => mockFactory('getPractitionerByBarNumber'),
+);
+
+jest.mock(
+  '@web-api/persistence/postgres/practitioners/getPractitionerById',
+  () => mockFactory('getPractitionerById'),
+);
+
+jest.mock(
+  '@web-api/persistence/postgres/practitioners/getPractitionerBySearchKey',
+  () => mockFactory('getPractitionerBySearchKey'),
+);
+
+jest.mock(
+  '@web-api/persistence/postgres/practitioners/getPrivatePractitionersOnCase',
+  () => mockFactory('getPrivatePractitionersOnCase'),
+);
+
+jest.mock(
+  '@web-api/persistence/postgres/practitioners/updatePractitioner',
+  () => mockFactory('updatePractitioner'),
+);
+
+jest.mock(
+  '@web-api/persistence/postgres/practitioners/updatePractitionerUser',
+  () => mockFactory('updatePractitionerUser'),
+);
+
+jest.mock(
   '@web-api/persistence/postgres/practitioners/upsertPractitionerRecord',
   () => mockFactory('upsertPractitionerRecord'),
 );
