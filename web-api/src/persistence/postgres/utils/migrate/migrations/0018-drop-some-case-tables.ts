22064d1864b7540f19c8739d00f595d3f2ccf3ca
zachrog <ztrogers1@gmail.com>
2025-05-06 13:04:31 -0700

    10502: Add tables in the down

--- web-api/src/persistence/postgres/utils/migrate/migrations/0018-drop-some-case-tables.ts
+++ web-api/src/persistence/postgres/utils/migrate/migrations/0018-drop-some-case-tables.ts
@@ -1,3 +1,4 @@
+import { formatNow } from '@shared/business/utilities/DateHandler';
 import { Kysely } from 'kysely';
 
 export async function up(db: Kysely<any>): Promise<void> {
@@ -6,6 +7,64 @@ export async function up(db: Kysely<any>): Promise<void> {
   await db.schema.dropTable('dwStatisticPenalty').execute();
 }
 
-export function down(_: Kysely<any>): void {
-  console.error('Cannot re-add discarded tables');
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable('dwPetitionerOnCase')
+    .addColumn('additionalName', 'varchar')
+    .addColumn('contactType', 'varchar', col => col.notNull())
+    .addColumn('docketNumber', 'varchar', col => col.notNull())
+    .addColumn('hasConsentedToElectronicService', 'boolean')
+    .addColumn('hasElectronicAccess', 'boolean')
+    .addColumn('inCareOf', 'varchar')
+    .addColumn('isAddressSealed', 'boolean', col => col.defaultTo(false))
+    .addColumn('paperPetitionEmail', 'varchar')
+    .addColumn('placeOfLegalResidence', 'varchar')
+    .addColumn('sealedAndUnavailable', 'boolean')
+    .addColumn('secondaryName', 'varchar')
+    .addColumn('serviceIndicator', 'varchar')
+    .addColumn('title', 'varchar')
+    .addColumn('orderOnCase', 'smallint') // 0 for first petitioner on a case, 1 for second, etc.
+    // Contact information
+    .addColumn('address1', 'varchar', col => col.notNull())
+    .addColumn('address2', 'varchar')
+    .addColumn('address3', 'varchar')
+    .addColumn('city', 'varchar', col => col.notNull())
+    .addColumn('contactId', 'varchar')
+    .addColumn('country', 'varchar')
+    .addColumn('countryType', 'varchar', col => col.notNull())
+    .addColumn('email', 'varchar')
+    .addColumn('name', 'varchar', col => col.notNull())
+    .addColumn('phone', 'varchar')
+    .addColumn('postalCode', 'varchar', col => col.notNull())
+    .addColumn('state', 'varchar')
+    .addPrimaryKeyConstraint('pkUserCaseMapping', ['docketNumber', 'contactId'])
+    .execute();
+
+  await db.schema
+    .createTable('dwCaseStatistic')
+    .addColumn('docketNumber', 'varchar')
+    .addColumn('irsDeficiencyAmount', 'decimal')
+    .addColumn('irsTotalPenalties', 'decimal')
+    .addColumn('statisticId', 'varchar', col => col.primaryKey())
+    .addColumn('year', 'smallint')
+    .addColumn('yearOrPeriod', 'varchar')
+    .addColumn('determinationTotalPenalties', 'decimal')
+    .addColumn('determinationDeficiencyAmount', 'decimal')
+    .addColumn('lastDateOfPeriod', 'timestamptz')
+    .addColumn('updatedAt', 'timestamp', col =>
+      col.notNull().defaultTo(formatNow()),
+    )
+    .execute();
+
+  await db.schema
+    .createTable('dwStatisticPenalty')
+    .addColumn('statisticId', 'varchar')
+    .addColumn('name', 'varchar')
+    .addColumn('penaltyAmount', 'decimal')
+    .addColumn('penaltyId', 'varchar', col => col.primaryKey())
+    .addColumn('penaltyType', 'varchar')
+    .addColumn('updatedAt', 'timestamp', col =>
+      col.notNull().defaultTo(formatNow()),
+    )
+    .execute();
 }
