fcf2c5bf0957f0ef3f40126e127975c5062975ce
John Cruz <jcruz@flexion.us>
2025-05-08 14:21:19 -0600

    10507: TEST do nothing in mocks;

--- web-api/src/persistence/postgres/featureFlag/mocks.jest.ts
+++ web-api/src/persistence/postgres/featureFlag/mocks.jest.ts
@@ -1,13 +1,13 @@
-jest.mock('@web-api/persistence/postgres/featureFlag/getFeatureFlagValue');
-import { ALLOWLIST_FEATURE_FLAGS } from '@shared/business/entities/EntityConstants';
-import { getFeatureFlagValues as getFeatureFlagValuesMock } from '@web-api/persistence/postgres/featureFlag/getFeatureFlagValue';
+// jest.mock('@web-api/persistence/postgres/featureFlag/getFeatureFlagValue');
+// import { ALLOWLIST_FEATURE_FLAGS } from '@shared/business/entities/EntityConstants';
+// import { getFeatureFlagValues as getFeatureFlagValuesMock } from '@web-api/persistence/postgres/featureFlag/getFeatureFlagValue';
 
-const getFeatureFlagValues = getFeatureFlagValuesMock as jest.Mock;
-getFeatureFlagValues.mockResolvedValue([
-  {
-    name: ALLOWLIST_FEATURE_FLAGS.ENTITY_LOCKING_FEATURE_FLAG.key,
-    value: {
-      current: true,
-    },
-  },
-]);
+// const getFeatureFlagValues = getFeatureFlagValuesMock as jest.Mock;
+// getFeatureFlagValues.mockResolvedValue([
+//   {
+//     name: ALLOWLIST_FEATURE_FLAGS.ENTITY_LOCKING_FEATURE_FLAG.key,
+//     value: {
+//       current: true,
+//     },
+//   },
+// ]);
