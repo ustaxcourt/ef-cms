69b015788641824ac068d1210135a06dfe55ebe3
John Cruz <jcruz@flexion.us>
2025-05-12 09:58:46 -0600

    10507: Migrated "AWS_BATCH_ZIPPER_MINIMUM_COUNT" to postgres;

---
+++ web-api/src/business/useCases/featureFlag/getAllFeatureFlagsFromPostgresInteractor.ts
@@ -0,0 +1,40 @@
+import { ServerApplicationContext } from '@web-api/applicationContext';
+import {
+  ALLOWLIST_FEATURE_FLAGS_POSTGRES,
+  PostgresFeatureFlagKeys,
+} from '@shared/business/entities/EntityConstants';
+import { isEmpty } from 'lodash';
+import { getFeatureFlagValues } from '@web-api/persistence/postgres/featureFlag/getFeatureFlagValues';
+
+type AllFeatureFlags = Partial<{ [K in PostgresFeatureFlagKeys]: any }>;
+const allFeatureFlags: AllFeatureFlags = {};
+
+export const getAllFeatureFlagsFromPostgresInteractor = async (
+  applicationContext: ServerApplicationContext,
+  hardReload: boolean = false,
+): Promise<AllFeatureFlags> => {
+  const allowlistFeatures = Object.values(ALLOWLIST_FEATURE_FLAGS_POSTGRES).map(
+    flag => flag.key,
+  );
+
+  // we use ENV so that we emulate a fresh lambda invocation each time.
+  // caching the feature flags locally causes integration tests to fail.
+  if (
+    hardReload ||
+    isEmpty(allFeatureFlags) ||
+    applicationContext.environment.stage === 'local'
+  ) {
+    const ALL_FEATURE_FLAGS = await getFeatureFlagValues(allowlistFeatures);
+    ALL_FEATURE_FLAGS.forEach(({ name, value }) => {
+      const { current } = value;
+      allFeatureFlags[name] = current;
+    });
+
+    allowlistFeatures.forEach(featureFlag => {
+      if (allFeatureFlags[featureFlag] === undefined)
+        allFeatureFlags[featureFlag] = false;
+    });
+  }
+
+  return allFeatureFlags;
+};
--- web-api/src/getUseCases.ts
+++ web-api/src/getUseCases.ts
@@ -212,6 +212,7 @@ import { updateUserPendingEmailInteractor } from './business/useCases/user/updat
 import { validatePdfInteractor } from './business/useCases/pdf/validatePdfInteractor';
 import { verifyPendingCaseForUserInteractor } from './business/useCases/caseAssociationRequest/verifyPendingCaseForUserInteractor';
 import { verifyUserPendingEmailInteractor } from './business/useCases/user/verifyUserPendingEmailInteractor';
+import { getAllFeatureFlagsFromPostgresInteractor } from '@web-api/business/useCases/featureFlag/getAllFeatureFlagsFromPostgresInteractor';
 
 const useCases = {
   addCaseToTrialSessionInteractor,
@@ -289,6 +290,7 @@ const useCases = {
   generateTrialCalendarPdfInteractor,
   generateTrialSessionPaperServicePdfInteractor,
   getAllFeatureFlagsInteractor,
+  getAllFeatureFlagsFromPostgresInteractor,
   getAllUsersByRoleInteractor,
   getCachedHealthCheckInteractor,
   getCalendaredCasesForTrialSessionInteractor,
