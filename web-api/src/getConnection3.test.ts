8337f6000cee299a461655674935d77fb0321712
Christopher Bisom <cbisom@flexion.us>
2025-05-13 15:38:04 -0400

    10502-dxox: fix issues with resetPoolPassword not working properly with Promise.all and add related tests

--- web-api/src/getConnection3.test.ts
+++ web-api/src/getConnection3.test.ts
@@ -11,10 +11,13 @@ jest.mock('@aws-sdk/rds-signer', () => {
 });
 import { getConnection } from '@web-api/getConnection';
 import { environment } from '@web-api/environment';
+import { sleep } from '@shared/tools/helpers';
 
 describe('getConnection', () => {
   environment.nodeEnv = 'production';
   it('should wait for pool password when multiple promises are attempting to reset', async () => {
+    // first two calls to Date.now() correspond to the FIRST getConnection call (Date.now is called twice)
+    // thereafter, our token should be expired (set to 15 minutes later which is greater than the 13 minute timer in getConnection)
     mockGetAuthToken.mockResolvedValue('12346789');
     const dateNowStub = jest
       .fn()
@@ -23,13 +26,37 @@ describe('getConnection', () => {
       .mockReturnValue(1747161078724 + 1000 * 60 * 15);
     global.Date.now = dateNowStub;
     await getConnection({ cb: () => {} });
+    let hasResetPassword = false;
 
-    await Promise.all([
-      getConnection({ cb: () => {} }),
-      getConnection({ cb: () => {} }),
-      getConnection({ cb: () => {} }),
+    // We want all calls to getConnection to wait until after the call to getToken has resolved. hasResetPassword is a proxy for this.
+    mockGetAuthToken.mockImplementation(async () => {
+      await sleep(20);
+      hasResetPassword = true;
+      return Promise.resolve(resolve => {
+        return resolve('123456789');
+      });
+    });
+
+    const result = await Promise.all([
+      getConnection({
+        cb: () => {
+          return hasResetPassword;
+        },
+      }),
+      getConnection({
+        cb: () => {
+          return hasResetPassword;
+        },
+      }),
+      getConnection({
+        cb: () => {
+          return hasResetPassword;
+        },
+      }),
     ]);
 
+    // ensures that password has been reset before simultaneous connections execute callbacks (only one token reset)
+    expect(result).toEqual([true, true, true]);
     expect(mockGetAuthToken).toHaveBeenCalledTimes(2);
   });
 });
