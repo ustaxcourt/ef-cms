6f0cfbebf19083cc5fed6e5358a3d91d94c58687
John Cruz <jcruz@flexion.us>
2025-05-15 13:49:18 -0600

    10507: Remove mock;

--- web-api/src/business/useCases/blockCaseFromTrialInteractor.test.ts
+++ web-api/src/business/useCases/blockCaseFromTrialInteractor.test.ts
@@ -2,7 +2,6 @@ import '@web-api/persistence/postgres/cases/mocks.jest';
 import { MOCK_CASE } from '@shared/test/mockCase';
 import { MOCK_LOCK } from '@shared/test/mockLock';
 import '@web-api/persistence/postgres/featureFlag/mocks.jest';
-jest.mock('@web-api/persistence/dynamo/cases/getCaseByDocketNumber');
 jest.mock(
   '@web-api/business/useCaseHelper/caseAssociation/updateCaseAndAssociations',
 );
