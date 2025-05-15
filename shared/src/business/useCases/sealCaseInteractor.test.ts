8f3b089ab8015fbfeecab58ebd40b4ae9d3d7c53
John Cruz <jcruz@flexion.us>
2025-05-15 13:31:59 -0600

    10507: Add feature flag mock to unit tests;

--- shared/src/business/useCases/sealCaseInteractor.test.ts
+++ shared/src/business/useCases/sealCaseInteractor.test.ts
@@ -1,3 +1,4 @@
+import '@web-api/persistence/postgres/featureFlag/mocks.jest';
 import '@web-api/persistence/postgres/cases/mocks.jest';
 import '@web-api/persistence/postgres/workitems/mocks.jest';
 import { MOCK_CASE } from '../../test/mockCase';
