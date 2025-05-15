4aa8d520632f31e549d8638b3ee65f30db99b70a
John Cruz <jcruz@flexion.us>
2025-05-15 11:36:38 -0600

    10507: Add feature flag mock to API unit tests;

--- web-api/src/business/useCases/caseAssociation/deleteCounselFromCaseInteractor.test.ts
+++ web-api/src/business/useCases/caseAssociation/deleteCounselFromCaseInteractor.test.ts
@@ -1,5 +1,6 @@
 import '@web-api/persistence/postgres/cases/mocks.jest';
 import '@web-api/persistence/postgres/workitems/mocks.jest';
+import '@web-api/persistence/postgres/featureFlag/mocks.jest';
 import {
   CONTACT_TYPES,
   ROLES,
--- web-api/src/business/useCases/practitioner/updatePractitionerUserInteractor.locking.test.ts
+++ web-api/src/business/useCases/practitioner/updatePractitionerUserInteractor.locking.test.ts
@@ -1,3 +1,4 @@
+import '@web-api/persistence/postgres/featureFlag/mocks.jest';
 import { MOCK_CASE } from '../../../../../shared/src/test/mockCase';
 import { MOCK_LOCK } from '../../../../../shared/src/test/mockLock';
 import {
