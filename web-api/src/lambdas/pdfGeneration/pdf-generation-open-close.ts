133cdb93001d84e58d5c6f0b1f36fb8671b177ea
zachrog <ztrogers1@gmail.com>
2025-05-16 14:01:28 -0700

    10658 - Remove test lambdas

--- web-api/src/lambdas/pdfGeneration/pdf-generation-open-close.ts
+++
@@ -1,8 +0,0 @@
-import { getChromiumBrowserAWS } from '@shared/business/utilities/getChromiumBrowser';
-
-export const openAndCloseAlot = async () => {
-  for (let index = 0; index < 30; index++) {
-    const browser = await getChromiumBrowserAWS();
-    await Promise.race([browser.close(), browser.close(), browser.close()]);
-  }
-};
--- web-api/terraform/modules/api/pdf-generation.tf
+++ web-api/terraform/modules/api/pdf-generation.tf
@@ -16,7 +16,7 @@ module "pdf_generation_lambda" {
 
 resource "aws_cloudwatch_metric_alarm" "lambda_error_alarm" {
   alarm_name          = "pdf_generator_${var.environment}_${var.current_color}_errors"
-  alarm_description   = "Alarm for Lambda function errors in PDF generation"
+  alarm_description   = "Alarm if any pdfs fail to generate. View logs in pdf_generator_${var.environment}_${var.current_color}."
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
   metric_name         = "Errors"
@@ -31,18 +31,4 @@ resource "aws_cloudwatch_metric_alarm" "lambda_error_alarm" {
   }
 
   alarm_actions = [var.alert_sns_topic_arn]
-}
-
-module "pdf_load_test_investigation_lambda" {
-  source         = "../lambda"
-  handler_file   = "./web-api/src/lambdas/pdfGeneration/pdf-generation-open-close.ts"
-  handler_method = "openAndCloseAlot"
-  lambda_name    = "open_close_a_lot_${var.environment}_${var.current_color}"
-  role           = var.lambda_role_arn
-  environment    = var.lambda_environment
-  timeout        = "900"
-  memory_size    = "6508"
-  layers = [
-    aws_lambda_layer_version.puppeteer_layer.arn
-  ]
 }
