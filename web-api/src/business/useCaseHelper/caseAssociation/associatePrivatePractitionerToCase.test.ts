3a7507999ddf1e9d8880e8b2ad303669290934d7
zachrog <ztrogers1@gmail.com>
2025-05-20 10:37:17 -0700

    10494: Add getDocketEntries mock

--- web-api/src/business/useCaseHelper/caseAssociation/associatePrivatePractitionerToCase.test.ts
+++ web-api/src/business/useCaseHelper/caseAssociation/associatePrivatePractitionerToCase.test.ts
@@ -1,5 +1,6 @@
 import '@web-api/persistence/postgres/cases/mocks.jest';
 import '@web-api/persistence/postgres/workitems/mocks.jest';
+import '@web-api/persistence/postgres/docketEntries/mocks.jest';
 import { getLogger } from '@web-api/utilities/logger/getLogger';
 import {
   CASE_TYPES_MAP,
