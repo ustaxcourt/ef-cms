5fe9bf0c10b427518ca10cb182a603fbb397fbaa
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2025-05-22 15:39:10 -0500

    10436: remember that manuallyAddedToTrial is never undefined

--- web-api/src/persistence/postgres/cases/mapper.ts
+++ web-api/src/persistence/postgres/cases/mapper.ts
@@ -53,7 +53,7 @@ export const toKyselyNewCase = (rawCase: RawCase) => {
     leadDocketNumber: rawCase.leadDocketNumber ?? null,
     litigationCosts: rawCase.litigationCosts,
     mailingDate: rawCase.mailingDate,
-    manuallyAddedToTrial: rawCase.manuallyAddedToTrial ?? undefined,
+    manuallyAddedToTrial: rawCase.manuallyAddedToTrial ?? false,
     noticeOfAttachments: rawCase.noticeOfAttachments,
     noticeOfTrialDate: rawCase.noticeOfTrialDate
       ? calculateDate({ dateString: rawCase.noticeOfTrialDate })
