4a5f33ed940111ec0f19a15e101f73d1319dee3f
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2025-05-22 11:40:21 -0500

    10436: add index for manuallyAddedToTrial on dwCase

--- web-api/src/persistence/postgres/utils/migrate/migrations/2025-05-21T19_33_25Z-add-manually-added-tag.ts
+++ web-api/src/persistence/postgres/utils/migrate/migrations/2025-05-21T19_33_25Z-add-manually-added-tag.ts
@@ -3,11 +3,13 @@ import { Kysely } from 'kysely';
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .alterTable('dwCase')
-    .addColumn(
-      'manuallyAddedToTrial',
-      'boolean',
-      col => col.defaultTo(false), // ideally undefined
-    )
+    .addColumn('manuallyAddedToTrial', 'boolean', col => col.defaultTo(false))
+    .execute();
+
+  await db.schema
+    .createIndex('idxManuallyAddedToTrial')
+    .on('dwCase')
+    .column('manuallyAddedToTrial')
     .execute();
 }
 
@@ -16,4 +18,6 @@ export async function down(db: Kysely<any>): Promise<void> {
     .alterTable('dwCase')
     .dropColumn('manuallyAddedToTrial')
     .execute();
+
+  await db.schema.dropIndex('idxManuallyAddedToTrial').execute();
 }
