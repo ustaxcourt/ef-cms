07a98e2495a11a2d4dae013aa98d740ba5102353
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2025-05-27 12:04:03 -0500

    10505: use older acquireLock interface once

--- shared/src/business/useCases/prioritizeCaseInteractor.ts
+++ shared/src/business/useCases/prioritizeCaseInteractor.ts
@@ -7,10 +7,7 @@ import { ServerApplicationContext } from '@web-api/applicationContext';
 import { UnauthorizedError } from '@web-api/errors/errors';
 import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { getCaseByDocketNumber } from '@web-api/persistence/postgres/cases/getCaseByDocketNumber';
-import {
-  hashLockId,
-  mutexLockWrapper,
-} from '@web-api/persistence/postgres/utils/mutex';
+import { withLocking } from '@web-api/persistence/postgres/utils/mutex';
 
 /**
  * used for setting a case as high priority
@@ -66,27 +63,27 @@ export const prioritizeCase = async (
   return new Case(updatedCase, { authorizedUser }).validate().toRawObject();
 };
 
-export const prioritizeCaseInteractor = async (
-  applicationContext: ServerApplicationContext,
-  { docketNumber, reason }: { docketNumber: string; reason: string },
-  authorizedUser: UnknownAuthUser,
-) => {
-  const lockId = hashLockId(`case|${docketNumber}`);
+// export const prioritizeCaseInteractor = async (
+//   applicationContext: ServerApplicationContext,
+//   { docketNumber, reason }: { docketNumber: string; reason: string },
+//   authorizedUser: UnknownAuthUser,
+// ) => {
+//   const lockId = hashLockId(`case|${docketNumber}`);
 
-  return mutexLockWrapper({
-    lockId,
-    callback: () =>
-      prioritizeCase(
-        applicationContext,
-        { docketNumber, reason },
-        authorizedUser,
-      ),
-  });
-};
+//   return mutexLockWrapper({
+//     lockId,
+//     callback: () =>
+//       prioritizeCase(
+//         applicationContext,
+//         { docketNumber, reason },
+//         authorizedUser,
+//       ),
+//   });
+// };
 
-// export const prioritizeCaseInteractor = withLocking(
-//   prioritizeCase,
-//   (_applicationContext, { docketNumber }) => ({
-//     identifiers: [`case|${docketNumber}`],
-//   }),
-// );
+export const prioritizeCaseInteractor = withLocking(
+  prioritizeCase,
+  (_applicationContext, { docketNumber }) => ({
+    identifiers: [`case|${docketNumber}`],
+  }),
+);
