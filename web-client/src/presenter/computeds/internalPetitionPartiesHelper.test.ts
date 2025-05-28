46cea9d0c70a349e991ce2a613973d47e8d03469
Pavan <pyenduri@flexion.us>
2025-05-28 11:55:46 -0500

    10633 remove redundant conditions

--- web-client/src/presenter/computeds/internalPetitionPartiesHelper.test.ts
+++ web-client/src/presenter/computeds/internalPetitionPartiesHelper.test.ts
@@ -497,41 +497,6 @@ describe('internalPetitionPartiesHelper', () => {
   });
 
   describe('showSecondaryContactEmailFieldAndConsentBox', () => {
-    it('should display secondary contact email field when petition is filed by a petitioner', () => {
-      const result = runCompute(internalPetitionPartiesHelper, {
-        state: {
-          featureFlags: {
-            [ALLOWLIST_FEATURE_FLAGS.E_CONSENT_FIELDS_ENABLED_FEATURE_FLAG.key]:
-              true,
-          },
-          form: {
-            filingType: FILING_TYPES_DICT.MYSELF_AND_SPOUSE,
-            isPaper: false,
-          },
-          user: petitionsClerkUser,
-        },
-      });
-
-      expect(result.showSecondaryContactEmailFieldAndConsentBox).toEqual(true);
-    });
-
-    it('should display secondary contact email field when petition is filed by a private practitioner', () => {
-      const result = runCompute(internalPetitionPartiesHelper, {
-        state: {
-          featureFlags: {
-            [ALLOWLIST_FEATURE_FLAGS.E_CONSENT_FIELDS_ENABLED_FEATURE_FLAG.key]:
-              true,
-          },
-          form: {
-            filingType: FILING_TYPES_DICT.PETITIONER_SPOUSE,
-            isPaper: false,
-          },
-          user: petitionsClerkUser,
-        },
-      });
-
-      expect(result.showSecondaryContactEmailFieldAndConsentBox).toEqual(true);
-    });
 
     it('should display secondary contact email field when petition is filed by a petitioner updating party type to petitioner and spouse', () => {
       const result = runCompute(internalPetitionPartiesHelper, {
