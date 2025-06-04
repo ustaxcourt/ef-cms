d2ff8b4bdba0f2203c5bc859a98a70773f3058fd
Pavan <pyenduri@flexion.us>
2025-06-04 13:31:02 -0500

    10505: use postgres instance of get and save request results

--- web-api/src/getPersistenceGateway.ts
+++ web-api/src/getPersistenceGateway.ts
@@ -61,7 +61,6 @@ import { getPractitionerDocumentByFileId } from './persistence/dynamo/practition
 import { getPractitionerDocuments } from './persistence/dynamo/practitioners/getPractitionerDocuments';
 import { getPractitionersByName } from './persistence/elasticsearch/getPractitionersByName';
 import { getReconciliationReport } from './persistence/elasticsearch/getReconciliationReport';
-import { getRequestResults } from '@web-api/persistence/dynamo/polling/getRequestResults';
 import { getSesStatus } from './persistence/ses/getSesStatus';
 import { getColdCases } from './persistence/elasticsearch/getColdCases';
 import { getStoredApplicationHealth } from '@web-api/persistence/dynamo/deployTable/getStoredApplicationHealth';
@@ -116,6 +115,7 @@ import { verifyCaseForUser } from './persistence/dynamo/cases/verifyCaseForUser'
 import { verifyPendingCaseForUser } from './persistence/dynamo/cases/verifyPendingCaseForUser';
 import { zipDocuments } from './persistence/s3/zipDocuments';
 import { getEligibleCasesForTrialSession } from '@web-api/persistence/postgres/cases/getEligibleCasesForTrialSession';
+import { getRequestResults } from '@web-api/persistence/postgres/polling/getRequestResults';
 
 const isValidatedDecorator = <T>(persistenceGatewayMethods: T): T => {
   /**
