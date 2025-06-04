b524894f783005bbe95ebdf90be5f93930ab6232
Andy Kuny <akuny@flexion.us>
2025-06-04 14:23:19 -0400

    10495: getAllUsersByRole must return practitioner fields

--- web-api/src/persistence/postgres/users/getAllUsersByRole.ts
+++ web-api/src/persistence/postgres/users/getAllUsersByRole.ts
@@ -1,19 +1,26 @@
 import { RawUser } from '@shared/business/entities/User';
 import { getDbReader } from '@web-api/database';
-import { userEntity } from '@web-api/persistence/postgres/users/mapper';
+import { rawUserWithPractitionerEntity } from '@web-api/persistence/postgres/users/mapper';
+import { PRACTITIONER_ONLY_FIELDS } from '../practitioners/mapper';
 
 export const getAllUsersByRole = async ({
   roles,
 }: {
   roles: string[];
 }): Promise<RawUser[]> => {
+  const practitionerOnlyFields = PRACTITIONER_ONLY_FIELDS.map(
+    field => `p.${field}` as const,
+  );
+
   const users = await getDbReader(reader =>
     reader
       .selectFrom('dwUser as u')
       .where('u.role', 'in', roles)
+      .leftJoin('dwPractitioner as p', 'u.userId', 'p.userId')
       .selectAll('u')
+      .select(practitionerOnlyFields)
       .execute(),
   );
 
-  return users.map(user => userEntity(user).toRawObject());
+  return users.map(user => rawUserWithPractitionerEntity(user));
 };
