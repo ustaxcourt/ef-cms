09102b539a82650d4191fcf4ef921216f3d1e5d0
Cody Seibert <cseibert113@gmail.com>
2025-06-06 13:58:05 -0400

    fixing the dynamsoft intercept

--- cypress/deployed-and-local/support/index.ts
+++ cypress/deployed-and-local/support/index.ts
@@ -4,3 +4,16 @@ before(() => {
   // Skip subsequent tests in spec when one fails.
   (cy.state('runnable').ctx as Mocha.Context).currentTest.parent.bail(true);
 });
+
+beforeEach(() => {
+  // This is used to intercept the dynamsoft javascript to prevent it from showing a modal
+  // asking the user to download the dynamsoft software which causes the tests to break.
+  // Due to race conditions, it's easier to just prevent dynamsoft from ever loading in smoketests
+  // than it is to figure out how to always assume the modal will pop up (which sometimes it doesn't show)
+  cy.intercept('GET', 'https://**/dynamsoft.webtwain.viewer.js**', {
+    body: `window.Dynamsoft = {DWT: {
+            CreateDWTObject() {}
+          }}`,
+    statusCode: 200,
+  });
+});
