cd6f255ce4d3987f3d79eb6c5e6ac95d8607e01b
Pavan <pyenduri@flexion.us>
2025-06-10 10:37:12 -0500

    10505 use postgres instead of dynamo

--- web-api/src/notifications/notificationClient/getNotificationGateway.ts
+++ web-api/src/notifications/notificationClient/getNotificationGateway.ts
@@ -1,7 +1,7 @@
 import { retrySendNotificationToConnections } from '@web-api/notifications/retrySendNotificationToConnections';
 import { sendNotificationToConnection } from '@web-api/notifications/sendNotificationToConnection';
 import { sendNotificationToUser } from '@web-api/notifications/sendNotificationToUser';
-import { saveRequestResponse } from '@web-api/persistence/dynamo/polling/saveRequestResponse';
+import { saveRequestResponse } from '@web-api/persistence/postgres/polling/saveRequestResponse';
 
 export const getNotificationGateway = () => ({
   retrySendNotificationToConnections,
