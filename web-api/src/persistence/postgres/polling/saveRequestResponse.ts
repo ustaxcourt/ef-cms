52a0e69d157d515c6d9d79fd216177ac65c5b372
Javis Sullivan <jsullivan@flexion.us>
2025-06-11 15:34:42 -0400

    10505: cleanup

--- web-api/src/persistence/postgres/polling/saveRequestResponse.ts
+++ web-api/src/persistence/postgres/polling/saveRequestResponse.ts
@@ -1,8 +1,6 @@
 import { formatNow, FORMATS } from '@shared/business/utilities/DateHandler';
 import { pgInsertInto } from '../utils/operation/pgInsertInto';
 
-// TODO: move this to a shared location
-
 /**
  * Saves a request response chunk to postgres
  */
@@ -19,10 +17,8 @@ export const saveRequestResponse = async ({
   index: number;
   totalNumberOfChunks: number;
 }) => {
-  // Calculate expiration time (16 minutes from now, matching dynamo TTL)
   const nowUnix = Number(formatNow(FORMATS.UNIX_TIMESTAMP_SECONDS));
   const ttl = nowUnix + 16 * 60;
-  // Save this chunk
   await pgInsertInto({
     table: 'dwResponseChunk',
     values: [
@@ -35,6 +31,5 @@ export const saveRequestResponse = async ({
         ttl,
       },
     ],
-    onConflictColumns: ['userId', 'requestId', 'index'],
   });
 };
