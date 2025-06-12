ae1572d62b3e76834bb221c57f5028d554f0ac1f
Javis Sullivan <jsullivan@flexion.us>
2025-06-12 14:00:04 -0400

    10505: fix ttl calculations

--- web-api/src/persistence/postgres/polling/mapper.ts
+++ web-api/src/persistence/postgres/polling/mapper.ts
@@ -3,24 +3,27 @@ import { NewRequestKysely, NewResponseChunkKysely } from './schema';
 
 const SIXTEEN_MINUTES = 16 * 60;
 
+const getTtl = (ttl?: number): number => {
+  return (
+    ttl ?? Number(formatNow(FORMATS.UNIX_TIMESTAMP_SECONDS)) + SIXTEEN_MINUTES
+  );
+};
+
 export const toKyselyNewRequest = (rawRequest: {
   requestId: string;
   userId: string;
   totalChunks?: number;
   ttl?: number;
 }): NewRequestKysely => {
-  const ttl = Number(
-    formatNow(FORMATS.UNIX_TIMESTAMP_SECONDS) + SIXTEEN_MINUTES,
-  );
+  const ttl = getTtl(rawRequest.ttl);
 
   return {
     requestId: rawRequest.requestId,
     userId: rawRequest.userId,
-    totalChunks: rawRequest.totalChunks || 0,
+    totalChunks: rawRequest.totalChunks ?? 0,
     ttl,
   };
 };
-
 
 export const toKyselyNewResponseChunk = (rawChunk: {
   requestId: string;
@@ -30,9 +33,7 @@ export const toKyselyNewResponseChunk = (rawChunk: {
   totalNumberOfChunks: number;
   ttl?: number;
 }): NewResponseChunkKysely => {
-  const ttl = Number(
-    formatNow(FORMATS.UNIX_TIMESTAMP_SECONDS) + SIXTEEN_MINUTES,
-  );
+  const ttl = getTtl(rawChunk.ttl);
 
   return {
     requestId: rawChunk.requestId,
