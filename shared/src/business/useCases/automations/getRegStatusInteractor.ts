5f69daacacb7011d273e10dd25977d5f9a80c64e
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2025-06-12 16:30:54 -0500

    10638: working version of regStatus interactor

--- shared/src/business/useCases/automations/getRegStatusInteractor.ts
+++ shared/src/business/useCases/automations/getRegStatusInteractor.ts
@@ -4,8 +4,15 @@ import {
   UnknownAuthUser,
   isAuthUser,
 } from '@shared/business/entities/authUser/AuthUser';
-import { getUserByEmail } from '@web-api/gateways/user/getUserByEmail';
 import { ROLES } from '@shared/business/entities/EntityConstants';
+import {
+  ListUsersCommand,
+  UserType,
+} from '@aws-sdk/client-cognito-identity-provider';
+import { getCognito } from '@web-api/persistence/cognito/getCognito';
+import { getDocketNumbersByUser } from '@web-api/persistence/postgres/users/cases/getCasesForUser';
+import { settlePromises } from '@web-api/utilities/settlePromises';
+import { getBarNumberByPractitionerId } from '@web-api/persistence/postgres/users/getBarNumberByPractitionerId';
 
 export const getRegStatusInteractor = async (
   applicationContext: ServerApplicationContext,
@@ -16,13 +23,90 @@ export const getRegStatusInteractor = async (
     throw new UnauthorizedError(`Invalid User`);
   }
 
-  const user = await getUserByEmail(applicationContext, { email: userEmail });
+  const matchedUsers = await getUsersWithSimilarEmails({
+    applicationContext,
+    userEmail,
+  });
 
-  // get cases for user
+  const fullUsers = await settlePromises(
+    matchedUsers.map(async user => {
+      const docketNumbers = await getDocketNumbersByUser({
+        userId: user.userId,
+      });
 
-  if (user?.role === ROLES.irsPractitioner || ROLES.privatePractitioner) {
-    // get bar number for practitioner user
-  }
+      let barNumber: string | undefined;
 
-  return user;
+      if (
+        user.role === ROLES.privatePractitioner ||
+        user.role === ROLES.irsPractitioner
+      ) {
+        barNumber = await getBarNumberByPractitionerId({ userId: user.userId });
+      }
+
+      return {
+        ...user,
+        docketNumbers,
+        ...(barNumber && { barNumber }),
+      };
+    }),
+  );
+
+  return fullUsers;
+};
+
+// leaving the functions below in this file to make clear that they are only to be used by Zendesk automations
+
+function gatherUserInfo(user: UserType): UserInfo {
+  const attributes = user.Attributes ?? [];
+
+  const getAttr = (name: string) =>
+    attributes.find(attr => attr.Name === name)?.Value;
+
+  return {
+    email: getAttr('email')!,
+    role: getAttr('custom:role') ?? 'petitioner',
+    status: user.UserStatus!,
+    userId: getAttr('custom:userId') ?? user.Username!,
+    enabled: user.Enabled ?? true,
+  };
+}
+
+async function getUsersWithSimilarEmails({
+  applicationContext,
+  userEmail,
+}: {
+  applicationContext: ServerApplicationContext;
+  userEmail: string;
+}): Promise<UserInfo[]> {
+  const client = await getCognito();
+
+  const normalizedEmail = userEmail.toLowerCase();
+  const [username, domain] = normalizedEmail.split('@');
+
+  const listCommand = new ListUsersCommand({
+    UserPoolId: applicationContext.environment.userPoolId,
+    AttributesToGet: ['email', 'custom:role', 'custom:userId'],
+    Filter: `email ^= "${username}"`,
+    Limit: 60,
+  });
+
+  const { Users = [] } = await client.send(listCommand);
+
+  const matchedUsers = Users.reduce<UserInfo[]>((acc, user) => {
+    const info = gatherUserInfo(user);
+    if (info.email.endsWith(`@${domain}`)) {
+      acc.push(info);
+    }
+    return acc;
+  }, []);
+
+  return matchedUsers;
+}
+
+type UserInfo = {
+  email: string;
+  role: string;
+  status: string;
+  userId: string;
+  enabled: boolean;
 };
