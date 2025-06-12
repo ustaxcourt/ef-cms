ee1eff8d62f6b51119fdcd36bb51c02d9300d98e
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2025-06-12 09:57:47 -0500

    10505: pluralize tryGetLock

--- web-api/src/persistence/postgres/utils/operation/tryGetLocks.ts
+++ web-api/src/persistence/postgres/utils/operation/tryGetLocks.ts
@@ -3,6 +3,7 @@ import { hashLockId } from '@web-api/persistence/postgres/utils/mutex';
 import { CompiledQuery, Kysely } from 'kysely';
 
 // Danger: never use this outside of acquireLock. This should only be used with a specific scoped db connection which is immediately destroyed after being used
+
 export const tryGetLocks = async ({
   db,
   identifiers,
@@ -10,14 +11,18 @@ export const tryGetLocks = async ({
   db: Kysely<Database>;
   identifiers: string[];
 }): Promise<{ identifier: string; successfullyLocked: boolean }[]> => {
-  const hashedIdentifiers = identifiers.map(id => hashLockId(id));
+  const hashedIdentifiers = identifiers.map(hashLockId);
+
   const idMap = new Map(hashedIdentifiers.map((h, i) => [h, identifiers[i]]));
 
-  const sql = hashedIdentifiers
-    .map(val => `SELECT ${val} as id, pg_try_advisory_lock(${val}) as locked`)
-    .join(' UNION ALL ');
+  const sql = `
+    SELECT id, pg_try_advisory_lock(id) AS locked
+    FROM UNNEST($1::int[]) AS t(id)
+  `;
 
-  const result = await db.executeQuery(CompiledQuery.raw(sql));
+  const result = await db.executeQuery(
+    CompiledQuery.raw(sql, [hashedIdentifiers]),
+  );
 
   return result.rows.map((row: any) => ({
     identifier: idMap.get(row.id)!,
