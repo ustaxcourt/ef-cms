1b1f77bb575273b117a91ddc2308a8cce85b7932
zachrog <ztrogers1@gmail.com>
2025-06-13 10:15:22 -0700

    10495: remove old seed data for an irsPractitioner and remove a pinkLog

--- web-api/src/persistence/postgres/practitioners/upsertPractitionerRecords.ts
+++ web-api/src/persistence/postgres/practitioners/upsertPractitionerRecords.ts
@@ -1,13 +1,11 @@
 import { getUniqueId } from '@shared/sharedAppContext';
 import { pgInsertInto } from '../utils/operation/pgInsertInto';
 import { pickPractitionerFields } from './mapper';
-import { pinkLog } from '@shared/tools/pinkLog';
 
 export const upsertPractitionerRecords = async practitioners => {
   await pgInsertInto({
     table: 'dwPractitioner',
-    values: practitioners.map((practitioner, i) => {
-      pinkLog(`Practitioner #${i}`, practitioner);
+    values: practitioners.map(practitioner => {
       return {
         ...pickPractitionerFields(practitioner),
         practitionerId: getUniqueId(),
