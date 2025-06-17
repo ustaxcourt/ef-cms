a609a2dccf501407951c078016a2efaa8de09e9b
Alex Dziarnowski <alexdziarn@gmail.com>
2025-06-17 17:17:54 -0400

    single quotes

--- web-client/integration-tests/journey/petitionerFilesDocumentForCase.ts
+++ web-client/integration-tests/journey/petitionerFilesDocumentForCase.ts
@@ -113,7 +113,7 @@ export const petitionerFilesDocumentForCase = (cerebralTest, fakeFile) => {
 
     expect(cerebralTest.getState('validationErrors')).toEqual({
       secondaryDocument: {
-        category: "Select a Category.",
+        category: 'Select a Category.',
         documentType: 'Select a document type',
       },
     });
