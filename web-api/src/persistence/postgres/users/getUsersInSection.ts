d2a4551b8cc9d1873a544cfbd9c7c065c782a811
zachrog <ztrogers1@gmail.com>
2025-06-20 14:46:49 -0700

    10495: Added back privatepractitioner4 to seed data. In getUsersBysection look by section rather than by role.

--- web-api/src/persistence/postgres/users/getUsersInSection.ts
+++ web-api/src/persistence/postgres/users/getUsersInSection.ts
@@ -1,14 +1,18 @@
 import { User } from '@shared/business/entities/User';
 import { getDbReader } from '@web-api/database';
 import { userEntity } from '@web-api/persistence/postgres/users/mapper';
-import { ROLES } from '@shared/business/entities/EntityConstants';
+import {
+  CASE_SERVICES_SUPERVISOR_SECTION,
+  DOCKET_SECTION,
+  ROLES,
+} from '@shared/business/entities/EntityConstants';
 
 export const getUsersInSection = async ({
   section,
 }: {
   section: string;
 }): Promise<User[]> => {
-  return await getDbReader(async reader => {
+  const users = await getDbReader(async reader => {
     let query = reader.selectFrom('dwUser as u');
 
     if (section === ROLES.judge) {
@@ -18,19 +22,19 @@ export const getUsersInSection = async ({
           eb('u.role', '=', ROLES.legacyJudge),
         ]),
       );
-    } else if (section === ROLES.docketClerk) {
+    } else if (section === DOCKET_SECTION) {
       query = query.where(eb =>
         eb.or([
-          eb('u.section', '=', ROLES.docketClerk),
-          eb('u.section', '=', ROLES.caseServicesSupervisor),
+          eb('u.section', '=', DOCKET_SECTION),
+          eb('u.section', '=', CASE_SERVICES_SUPERVISOR_SECTION),
         ]),
       );
     } else {
       query = query.where('u.section', '=', section);
     }
 
-    const users = await query.selectAll('u').execute();
-
-    return users.map(user => userEntity(user));
+    return query.selectAll('u').execute();
   });
+
+  return users.map(user => userEntity(user));
 };
