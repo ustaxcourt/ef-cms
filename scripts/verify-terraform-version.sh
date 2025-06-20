8353139642903fd34980be55a78702f244b410d2
Anthony <Anthony.Loera.ctr@ustaxcourt.gov>
2025-06-20 10:13:34 -0700

    updated some more packages

--- scripts/verify-terraform-version.sh
+++ scripts/verify-terraform-version.sh
@@ -3,6 +3,6 @@
 tf_version=$(terraform --version)
 
 if [[ ${tf_version} != *"1.12.2"* ]]; then
-  echo "Please set your terraform version to 1.12.1 before deploying."
+  echo "Please set your terraform version to 1.12.2 before deploying."
   exit 1
 fi
