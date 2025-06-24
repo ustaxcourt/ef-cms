d01ad512a2cb1b6ee8fa131f0d9e439587d45820
Jim Lerza <james.lerza@ustaxcourt.gov>
2025-06-24 14:40:24 -0400

    opex: fixed the key for the 'logs volume size' secret

--- scripts/secrets/create-account-secrets.ts
+++ scripts/secrets/create-account-secrets.ts
@@ -106,7 +106,7 @@ const {
     DAWSON_DEV_TRUSTED_ROLE_ARNS: dawsonDevTrustedRoleArns,
     ES_LOGS_INSTANCE_COUNT: opensearchLogsInstanceCount,
     ES_LOGS_INSTANCE_TYPE: opensearchLogsInstanceType,
-    ES_LOGS_VOLUME_SIZE: opensearchLogsVolumeSize,
+    ES_LOGS_EBS_VOLUME_SIZE_GB: opensearchLogsVolumeSize,
     LOG_GROUP_ENVIRONMENTS: [env],
     LOG_SNAPSHOT_BUCKET_NAME: `${repoSlug}-${env}-log-snapshots`,
     NUM_DAYS_TO_KEEP_LOGS: logExpirationDays,
