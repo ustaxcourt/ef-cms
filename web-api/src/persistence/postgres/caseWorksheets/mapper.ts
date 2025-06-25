dbb4c2088a8054485086e2de8eb92e4b47c3fad8
Christopher Bisom <cbisom@flexion.us>
2025-06-25 13:54:35 -0400

    devex-better-postgres-types: better typing for CaseWorksheetKysely and related

--- web-api/src/persistence/postgres/caseWorksheets/mapper.ts
+++ web-api/src/persistence/postgres/caseWorksheets/mapper.ts
@@ -1,7 +1,23 @@
-import { CaseWorksheet } from '@shared/business/entities/caseWorksheet/CaseWorksheet';
+import {
+  CaseWorksheet,
+  RawCaseWorksheet,
+} from '@shared/business/entities/caseWorksheet/CaseWorksheet';
+import { calculateDate } from '@shared/business/utilities/DateHandler';
 import { transformNullToUndefined } from '@web-api/persistence/postgres/utils/transformNullToUndefined';
 
-export function caseWorksheetEntity(caseWorksheet) {
+export function toKyselyNewCaseWorksheet(caseWorksheet: RawCaseWorksheet) {
+  return {
+    docketNumber: caseWorksheet.docketNumber,
+    finalBriefDueDate: caseWorksheet.finalBriefDueDate
+      ? calculateDate({ dateString: caseWorksheet.finalBriefDueDate })
+      : null,
+    judgeUserId: caseWorksheet.judgeUserId,
+    primaryIssue: caseWorksheet.primaryIssue,
+    statusOfMatter: caseWorksheet.statusOfMatter,
+  };
+}
+
+export function fromKyselyCaseWorksheet(caseWorksheet) {
   return new CaseWorksheet(
     transformNullToUndefined({
       ...caseWorksheet,
