489c6db9da5c9290b84caf7a6bc61b660b1982eb
zachrog <ztrogers1@gmail.com>
2025-06-25 09:42:54 -0700

    10495- script

--- scripts/run-once-scripts/readBigFile.ts
+++ scripts/run-once-scripts/readBigFile.ts
@@ -1,5 +1,7 @@
+/* eslint-disable complexity */
 import { createReadStream } from 'fs';
 import readline from 'readline';
+import prodUsers from 'prodUsers.json';
 
 let scanCount = 0;
 const rl = readline.createInterface({
@@ -9,14 +11,43 @@ const rl = readline.createInterface({
   crlfDelay: Infinity,
 });
 
+// const data = {} as any;
+
 rl.on('line', line => {
   const obj = JSON.parse(line);
+
+  // if (obj.sk.startsWith('user|') && obj.pk.startsWith('user|')) {
+  //   data[obj.userId] = obj;
+  // }
+
   if (
-    obj.pk.startsWith('user|') &&
-    obj.sk.startsWith('user|') &&
-    typeof obj.birthYear == 'string'
+    (obj.sk.startsWith('irsPractitioner|') && obj.pk.startsWith('case|')) ||
+    (obj.sk.startsWith('privatePractitioner|') && obj.pk.startsWith('case|')) ||
+    (obj.sk.startsWith('inactivePractitioner|') && obj.pk.startsWith('case|'))
   ) {
-    console.log('obj', obj);
+    const userUserEntry = prodUsers[obj.userId];
+    // delete userUserEntry.pk;
+    // delete userUserEntry.sk;
+
+    // delete obj.pk;
+    // delete obj.sk;
+
+    // if (!isEqual(userUserEntry.contact, obj.contact)) {
+    //   console.log('userUserEntry!: ', userUserEntry.contact);
+    //   console.log('caseUserEntry!: ', obj.contact);
+    // }
+    if (
+      userUserEntry?.contact?.city !== obj?.contact?.city &&
+      !userUserEntry?.email?.includes('irsPractitioner') &&
+      !userUserEntry?.firmName
+        ?.toLowerCase()
+        .includes('united states tax court') &&
+      userUserEntry?.role != 'irsPractitioner'
+    ) {
+      // console.log('userUserEntry.contact', userUserEntry);
+      console.log('userUserEntry!: ', userUserEntry);
+      console.log('caseUserEntry!: ', obj);
+    }
   }
 
   if (scanCount % 100000 === 0) {
@@ -27,5 +58,6 @@ rl.on('line', line => {
 });
 
 rl.on('close', () => {
+  // writeFileSync('prodUsers.json', JSON.stringify(data, null, 2));
   console.log('Done!');
 });
