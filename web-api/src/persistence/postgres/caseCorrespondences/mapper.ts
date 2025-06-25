5f3355195bc28c5184da7ff0cf1c162482e6660e
Christopher Bisom <cbisom@flexion.us>
2025-06-25 13:39:42 -0400

    devex-better-postgres-types: better typing for CaseCorrespondenceKysely and related

--- web-api/src/persistence/postgres/caseCorrespondences/mapper.ts
+++ web-api/src/persistence/postgres/caseCorrespondences/mapper.ts
@@ -1,7 +1,26 @@
-import { Correspondence } from '@shared/business/entities/Correspondence';
+import {
+  Correspondence,
+  RawCorrespondence,
+} from '@shared/business/entities/Correspondence';
+import { calculateDate } from '@shared/business/utilities/DateHandler';
+import { CaseCorrespondenceKysely } from '@web-api/persistence/postgres/caseCorrespondences/schema';
 import { transformNullToUndefined } from '@web-api/persistence/postgres/utils/transformNullToUndefined';
 
-export function caseCorrespondenceEntity(caseCorrespondence) {
+export function toKyselyCaseCorrespondence(correspondence: RawCorrespondence) {
+  return {
+    archived: correspondence.archived,
+    correspondenceId: correspondence.correspondenceId,
+    docketNumber: correspondence.docketNumber!,
+    documentTitle: correspondence.documentTitle,
+    filedBy: correspondence.filedBy,
+    filingDate: calculateDate({ dateString: correspondence.filingDate }),
+    userId: correspondence.userId,
+  };
+}
+
+export function fromKyselyCaseCorrespondence(
+  caseCorrespondence: CaseCorrespondenceKysely,
+) {
   return new Correspondence(
     transformNullToUndefined({
       ...caseCorrespondence,
--- web-api/src/persistence/postgres/caseCorrespondences/upsertCaseCorrespondences.ts
+++ web-api/src/persistence/postgres/caseCorrespondences/upsertCaseCorrespondences.ts
@@ -1,21 +1,13 @@
 import { RawCorrespondence } from '@shared/business/entities/Correspondence';
-import { calculateDate } from '@shared/business/utilities/DateHandler';
+import { toKyselyCaseCorrespondence } from '@web-api/persistence/postgres/caseCorrespondences/mapper';
 import { pgInsertInto } from '@web-api/persistence/postgres/utils/operation/pgInsertInto';
 
 export const upsertCaseCorrespondences = async (
   correspondences: RawCorrespondence[],
 ) => {
-  const correspondencesToUpsert = correspondences.map(correspondence => {
-    return {
-      archived: correspondence.archived,
-      correspondenceId: correspondence.correspondenceId,
-      docketNumber: correspondence.docketNumber!,
-      documentTitle: correspondence.documentTitle,
-      filedBy: correspondence.filedBy,
-      filingDate: calculateDate({ dateString: correspondence.filingDate }),
-      userId: correspondence.userId,
-    };
-  });
+  const correspondencesToUpsert = correspondences.map(correspondence =>
+    toKyselyCaseCorrespondence(correspondence),
+  );
 
   await pgInsertInto({
     table: 'dwCaseCorrespondence',
