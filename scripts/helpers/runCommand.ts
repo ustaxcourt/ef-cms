73e5be60537e828c6adf2b66231f0dca85ff99da
Jim Lerza <james.lerza@ustaxcourt.gov>
2025-06-25 09:44:40 -0400

    opex: runCommand can return stderror too

--- scripts/helpers/runCommand.ts
+++ scripts/helpers/runCommand.ts
@@ -6,26 +6,34 @@ export const runCommand = async (
   params?: string[],
 ): Promise<string> => {
   return new Promise((resolve, reject) => {
-    let output: string;
+    let stdout: string;
+    let stderr: string;
     const result = spawn(cmd, params, {
       env: { ...process.env },
       stdio: 'pipe',
     });
     result.stdout.on('data', data => {
-      if (!output) {
-        output = data.toString('utf-8');
+      if (!stdout) {
+        stdout = data.toString('utf-8');
       } else {
-        output += data.toString('utf-8');
+        stdout += data.toString('utf-8');
       }
     });
     result.stderr.on('data', data => {
       console.error(trim(data.toString('utf-8')));
+      if (!stderr) {
+        stderr = data.toString('utf-8');
+      } else {
+        stderr += data.toString('utf-8');
+      }
     });
     result.on('close', code => {
       if (code) {
-        reject(new Error(`Unable to run ${cmd} ${params?.join(' ') ?? ''}`));
+        const command = trim(`${cmd} ${params?.join(' ') ?? ''}`);
+        const message = `Error: \`${command}\` exited with code ${code}\n${trim(stderr)}`;
+        reject(new Error(message));
       } else {
-        resolve(trim(output));
+        resolve(trim(stdout));
       }
     });
   });
