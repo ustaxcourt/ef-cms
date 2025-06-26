77b23f2e92548c447888ef6ac8200d97c6e23a51
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2025-06-26 13:15:40 -0500

    10495: remove references to updatePractitioner and consolidate with updateUser

--- web-api/src/persistence/postgres/users/updateUser.ts
+++ web-api/src/persistence/postgres/users/updateUser.ts
@@ -1,22 +1,36 @@
-import { RawUser, User } from '@shared/business/entities/User';
+import { RawUser } from '@shared/business/entities/User';
 import { pgUpdateTable } from '../utils/operation/pgUpdateTable';
-import { toKyselyUpdateUser, userEntity } from './mapper';
+import { toKyselyUpdateUser } from './mapper';
 import { isEmpty } from 'lodash';
+import { RawPractitioner } from '@shared/business/entities/Practitioner';
+import { toKyselyUpdatePractitioner } from '@web-api/persistence/postgres/practitioners/mapper';
+import {
+  PRACTITIONER_ROLES,
+  ROLES,
+} from '@shared/business/entities/EntityConstants';
 
 export const updateUser = async ({
   userToUpdate,
 }: {
-  userToUpdate: RawUser;
-}): Promise<User> => {
+  userToUpdate: RawUser | RawPractitioner;
+}): Promise<void> => {
   const updatedUser = await pgUpdateTable({
     table: 'dwUser',
     values: toKyselyUpdateUser(userToUpdate),
     where: cb => cb.where('userId', '=', userToUpdate.userId),
   });
-
   if (isEmpty(updatedUser)) {
-    throw new Error('could not update the user');
+    throw new Error('could not update the user record');
   }
 
-  return userEntity(updatedUser) as User;
+  if (PRACTITIONER_ROLES.includes(ROLES[userToUpdate.role])) {
+    const updatedPractitioner = await pgUpdateTable({
+      table: 'dwPractitioner',
+      values: toKyselyUpdatePractitioner(userToUpdate as RawPractitioner),
+      where: cb => cb.where('userId', '=', userToUpdate.userId),
+    });
+    if (isEmpty(updatedPractitioner)) {
+      throw new Error('could not update the practitioner record');
+    }
+  }
 };
