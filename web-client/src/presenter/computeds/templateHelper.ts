3b5953ee5abdc5abbca4e6f8bb4047e1c7cdbc3a
Jim Lerza <james.lerza@ustaxcourt.gov>
2025-06-30 16:26:29 -0400

    throwaway: log env vars in templateHeader for debugging

--- web-client/src/presenter/computeds/templateHelper.ts
+++ web-client/src/presenter/computeds/templateHelper.ts
@@ -2,15 +2,20 @@ import { state } from '@web-client/presenter/app.cerebral';
 
 import { ClientApplicationContext } from '@web-client/applicationContext';
 import { Get } from 'cerebral';
+
 export const templateHelper = (
   get: Get,
   applicationContext: ClientApplicationContext,
 ): any => {
   const initialBetaBarState = get(state.header.showBetaBar);
-  const isProduction = applicationContext.getEnvironment().stage === 'prod';
+  const isProduction = applicationContext.getEnvironment().stage === 'dev';
+
+  console.log('Environment:', applicationContext.getEnvironment());
 
   const showBetaBar = !isProduction && initialBetaBarState;
   const showDeployedDate = !isProduction;
+
+  console.log('Showing BetaBar:', { showBetaBar });
 
   return {
     showBetaBar,
