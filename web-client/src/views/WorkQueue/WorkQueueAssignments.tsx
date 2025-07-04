a6989dec2f0271cfc3f3a26d11fbf5e081baabba
John Cruz <jcruz@flexion.us>
2025-07-04 08:43:27 -0600

    10469: Refacotr - extracted markup to a function;

--- web-client/src/views/WorkQueue/WorkQueueAssignments.tsx
+++ web-client/src/views/WorkQueue/WorkQueueAssignments.tsx
@@ -33,118 +33,58 @@ export const WorkQueueAssignments = connect<
     users,
     workQueueHelper,
   }) {
+    const { currentBoxView, showSendToBar } = workQueueHelper;
     return (
       <React.Fragment>
         {workQueueHelper.showDocketClerkFilter ? (
-          <>
-            <div className="action-section grid-row inline-block margin-bottom-1">
-              <label
-                className="dropdown-label-serif padding-top-05"
-                htmlFor="inline-select"
-                id="trial-sessions-filter-label"
-              >
-                Filter by
-              </label>
-              <BindedSelect
-                aria-label="assignment"
-                bind="screenMetadata.assignmentFilterValue.userId"
-                className="select-left inline-select margin-left-1pt5rem"
-                data-testid="dropdown-filter-assignee"
-                id="assignmentFilter"
-                name="assignment"
-                onChange={() => {
-                  clearSelectedWorkItemsSequence();
-                }}
-              >
-                <option value="">-Assignment-</option>
-                <option value="UA">Unassigned</option>
-                {users.map(user => (
-                  <option key={user.name} value={user.userId}>
-                    {user.name}
-                  </option>
-                ))}
-              </BindedSelect>
-              {workQueueHelper.currentBoxView !== 'outbox' && (
-                <select
-                  aria-label="select an assignee"
-                  className="usa-select select-left inline-select margin-left-1pt5rem"
-                  data-testid="dropdown-select-assignee"
-                  disabled={!workQueueHelper.showSendToBar}
-                  id="options"
-                  name="options"
-                  onChange={evt => {
-                    selectAssigneeSequence({
-                      assigneeId: evt.target.value,
-                      assigneeName:
-                        evt.target.options[evt.target.selectedIndex].text,
-                    });
-                    assignSelectedWorkItemsSequence();
-                    //reset input manually
-                    evt.target.value = '';
-                  }}
-                >
-                  <option key="assignTo" value="">
-                    Assign to...
-                  </option>
-                  {users.map(user => (
-                    <option key={user.userId} value={user.userId}>
-                      {user.name}
-                    </option>
-                  ))}
-                </select>
-              )}
-              {workQueueHelper.showSendToBar && (
-                <span
-                  className="assign-work-item-count-docket"
-                  data-testid="assign-work-item-count-docket"
-                >
-                  <Icon aria-label="selected work items count" icon="check" />
-                  {selectedWorkItemsLength}
-                </span>
-              )}
-            </div>
-            <div className="push-right margin-top-4">
-              <b className='text-semibold'>Count:</b> {formattedWorkQueue.length}
-            </div>
-          </>
+          DocketClerkFilterMarkup({
+            clearSelectedWorkItemsSequence,
+            users,
+            currentBoxView,
+            selectAssigneeSequence,
+            showSendToBar,
+            assignSelectedWorkItemsSequence,
+            selectedWorkItemsLength,
+            formattedWorkQueueLength: formattedWorkQueue.length,
+          })
         ) : (
           <>
             {workQueueHelper.showSendToBar && (
               <>
-              <div className="action-section grid-row inline-block margin-bottom-1">
-                <span className="assign-work-item-count">
-                  <Icon aria-label="selected work items count" icon="check" />
-                  {selectedWorkItemsLength}
-                </span>
-                <select
-                  aria-label="select an assignee"
-                  className="usa-select"
-                  data-testid="dropdown-select-assignee"
-                  id="options"
-                  name="options"
-                  onChange={evt => {
-                    selectAssigneeSequence({
-                      assigneeId: evt.target.value,
-                      assigneeName:
-                        evt.target.options[evt.target.selectedIndex].text,
-                    });
-                    assignSelectedWorkItemsSequence();
-                  }}
-                >
-                  <option value="">Assign to...</option>
-                  {users.map(user => (
-                    <option key={user.userId} value={user.userId}>
-                      {user.name}
-                    </option>
-                  ))}
-                </select>
-              </div>
+                <div className="action-section grid-row inline-block margin-bottom-1">
+                  <span className="assign-work-item-count">
+                    <Icon aria-label="selected work items count" icon="check" />
+                    {selectedWorkItemsLength}
+                  </span>
+                  <select
+                    aria-label="select an assignee"
+                    className="usa-select"
+                    data-testid="dropdown-select-assignee"
+                    id="options"
+                    name="options"
+                    onChange={evt => {
+                      selectAssigneeSequence({
+                        assigneeId: evt.target.value,
+                        assigneeName:
+                          evt.target.options[evt.target.selectedIndex].text,
+                      });
+                      assignSelectedWorkItemsSequence();
+                    }}
+                  >
+                    <option value="">Assign to...</option>
+                    {users.map(user => (
+                      <option key={user.userId} value={user.userId}>
+                        {user.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
                 <div className="push-right margin-top-4">
-                  <b className='text-semibold'>Count:</b> {formattedWorkQueue.length}
+                  <b className="text-semibold">Count:</b>{' '}
+                  {formattedWorkQueue.length}
                 </div>
               </>
             )}
-
           </>
         )}
       </React.Fragment>
@@ -152,4 +92,97 @@ export const WorkQueueAssignments = connect<
   },
 );
 
-WorkQueueAssignments.displayName = 'WorkQueueAssignments';
+type DocketClerkFilterMarkupParams = {
+  clearSelectedWorkItemsSequence: Function;
+  users: RawUser[];
+  currentBoxView: string;
+  selectAssigneeSequence: Function;
+  showSendToBar: boolean;
+  assignSelectedWorkItemsSequence: Function;
+  selectedWorkItemsLength: number;
+  formattedWorkQueueLength: number;
+};
+
+function DocketClerkFilterMarkup({
+  clearSelectedWorkItemsSequence,
+  users,
+  currentBoxView,
+  selectAssigneeSequence,
+  showSendToBar,
+  assignSelectedWorkItemsSequence,
+  selectedWorkItemsLength,
+  formattedWorkQueueLength,
+}: DocketClerkFilterMarkupParams) {
+  return (
+    <>
+      <div className="action-section grid-row inline-block margin-bottom-1">
+        <label
+          className="dropdown-label-serif padding-top-05"
+          htmlFor="inline-select"
+          id="trial-sessions-filter-label"
+        >
+          Filter by
+        </label>
+        <BindedSelect
+          aria-label="assignment"
+          bind="screenMetadata.assignmentFilterValue.userId"
+          className="select-left inline-select margin-left-1pt5rem"
+          data-testid="dropdown-filter-assignee"
+          id="assignmentFilter"
+          name="assignment"
+          onChange={() => {
+            clearSelectedWorkItemsSequence();
+          }}
+        >
+          <option value="">-Assignment-</option>
+          <option value="UA">Unassigned</option>
+          {users.map(user => (
+            <option key={user.name} value={user.userId}>
+              {user.name}
+            </option>
+          ))}
+        </BindedSelect>
+        {currentBoxView !== 'outbox' && (
+          <select
+            aria-label="select an assignee"
+            className="usa-select select-left inline-select margin-left-1pt5rem"
+            data-testid="dropdown-select-assignee"
+            disabled={!showSendToBar}
+            id="options"
+            name="options"
+            onChange={evt => {
+              selectAssigneeSequence({
+                assigneeId: evt.target.value,
+                assigneeName: evt.target.options[evt.target.selectedIndex].text,
+              });
+              assignSelectedWorkItemsSequence();
+              //reset input manually
+              evt.target.value = '';
+            }}
+          >
+            <option key="assignTo" value="">
+              Assign to...
+            </option>
+            {users.map(user => (
+              <option key={user.userId} value={user.userId}>
+                {user.name}
+              </option>
+            ))}
+          </select>
+        )}
+        {showSendToBar && (
+          <span
+            className="assign-work-item-count-docket"
+            data-testid="assign-work-item-count-docket"
+          >
+            <Icon aria-label="selected work items count" icon="check" />
+            {selectedWorkItemsLength}
+          </span>
+        )}
+      </div>
+      <div className="push-right margin-top-4">
+        <b className="text-semibold">Count:</b> {formattedWorkQueueLength}
+      </div>
+    </>
+  );
+}
