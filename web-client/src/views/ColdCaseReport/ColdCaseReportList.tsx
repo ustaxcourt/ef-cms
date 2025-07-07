2c7dc6d5c1c3318ecacb699bf2a64d29748a7f37
Christie Laferriere <christie.laferriere.ctr@ustaxcourt.gov>
2025-07-07 12:06:55 -0400

    reduce bold on count value and adjust distance between count and table to 24px

--- web-client/src/views/ColdCaseReport/ColdCaseReportList.tsx
+++ web-client/src/views/ColdCaseReport/ColdCaseReportList.tsx
@@ -72,7 +72,7 @@ export function ColdCaseReportList({ entries }: { entries: ColdCaseEntry[] }) {
         </div>
       )}
 
-      <div className="margin-bottom-2">
+      <div className="margin-bottom-105">
         <div className="text-right">
           <Button
             link
@@ -86,8 +86,9 @@ export function ColdCaseReportList({ entries }: { entries: ColdCaseEntry[] }) {
             Export
           </Button>
           <span className="text-semibold" data-testid="display-data-count">
-            Count: {entries.length}
+            Count:{' '}
           </span>
+          {entries.length}
         </div>
       </div>
 
