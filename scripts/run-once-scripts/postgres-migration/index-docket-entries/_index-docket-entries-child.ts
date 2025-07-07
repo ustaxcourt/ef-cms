92508be9f3d6aa2049e99d71e2e097b1676b4ff7
Christopher Bisom <cbisom@flexion.us>
2025-07-07 17:04:36 -0400

    devex: WIP scripts

---
+++ scripts/run-once-scripts/postgres-migration/index-docket-entries/_index-docket-entries-child.ts
@@ -0,0 +1,91 @@
+#!/usr/bin/env -S npx ts-node --transpile-only
+
+import {
+  parseArgsAndEnvVars,
+  type ScriptConfig,
+} from '../../../helpers/parseArgsAndEnvVars';
+import { getDbReader } from '@web-api/database';
+import { isEmpty } from 'lodash';
+import {
+  OPENSEARCH_SYNC_ACTIONS,
+  OpenSearchSyncMessageType,
+} from '@web-api/lambdas/openSearch/openSearchSyncHandler';
+import { indexOpenSearchDocketEntries } from 'web-api/elasticsearch/docketEntries/indexOpenSearchDocketEntries';
+import { calculateDate } from '@shared/business/utilities/DateHandler';
+
+const scriptConfig: ScriptConfig = {
+  description: 'add-cases-to-opensearch - Reupsert cases',
+  environment: {
+    env: 'ENV',
+    sourceTable: 'SOURCE_TABLE',
+  },
+  parameters: {
+    startDate: {
+      position: 0,
+      required: true,
+      type: 'string',
+    },
+    endDate: {
+      position: 1,
+      required: true,
+      type: 'string',
+    },
+  },
+  requireActiveAwsSession: true,
+};
+const { startDate, endDate } = parseArgsAndEnvVars(scriptConfig) as {
+  startDate: string;
+  endDate: string;
+};
+
+const PAGE_SIZE = 2000;
+
+const getDocketEntriesToIndex = async (startDate: Date) => {
+  console.log('startDate', startDate);
+  return await getDbReader(reader =>
+    reader
+      .selectFrom('dwDocketEntry')
+      .select(['docketEntryId', 'docketNumber', 'createdAt'])
+      .orderBy('createdAt')
+      .orderBy('docketNumber')
+      .orderBy('docketEntryId')
+      .where('createdAt', '>', startDate)
+      .where('createdAt', '<=', calculateDate({ dateString: endDate }))
+      .limit(PAGE_SIZE)
+      .execute(),
+  );
+};
+
+let totalItems = 0;
+
+async function main() {
+  console.log('HERE WE ARE!', startDate, endDate);
+  let currentStartDate = calculateDate({ dateString: startDate });
+  let docketEntriesToIndex = await getDocketEntriesToIndex(currentStartDate);
+
+  while (!isEmpty(docketEntriesToIndex)) {
+    const message = {
+      payload: docketEntriesToIndex.map(data => ({
+        docketEntryId: data.docketEntryId,
+        docketNumber: data.docketNumber,
+      })),
+      type: 'dwDocketEntry' as OpenSearchSyncMessageType,
+      timestamp: Date.now().toString(),
+      action: OPENSEARCH_SYNC_ACTIONS.UPSERT,
+    };
+    await indexOpenSearchDocketEntries({ message });
+    totalItems += docketEntriesToIndex.length;
+    console.log(
+      `Total docket entries indexed for date range ${startDate} to ${endDate} so far: ${totalItems}`,
+    );
+    const lastSeenDate =
+      docketEntriesToIndex[docketEntriesToIndex.length - 1].createdAt;
+    currentStartDate = lastSeenDate;
+    docketEntriesToIndex = await getDocketEntriesToIndex(currentStartDate);
+  }
+  console.log(
+    `Done indexing docket entries for for date range ${startDate} to ${endDate}`,
+  );
+}
+
+main().catch(console.error);
---
+++ scripts/run-once-scripts/postgres-migration/index-docket-entries/index-docket-entries.ts
@@ -0,0 +1,151 @@
+#!/usr/bin/env -S npx ts-node --transpile-only
+
+import { calculateDate } from '@shared/business/utilities/DateHandler';
+import {
+  parseArgsAndEnvVars,
+  type ScriptConfig,
+} from '../../../helpers/parseArgsAndEnvVars';
+import { getDbReader } from '@web-api/database';
+import { spawn } from 'child_process';
+import { CompiledQuery } from 'kysely';
+
+const scriptConfig: ScriptConfig = {
+  description: 'add-cases-to-opensearch - Reupsert cases',
+  environment: {
+    env: 'ENV',
+    sourceTable: 'SOURCE_TABLE',
+  },
+  parameters: {
+    num_processes: {
+      position: 0,
+      required: true,
+      transform: 'toLowerCase',
+      type: 'string',
+    },
+  },
+  requireActiveAwsSession: true,
+};
+const { num_processes } = parseArgsAndEnvVars(scriptConfig) as {
+  num_processes: string;
+};
+
+// ts-node script.ts --num-processes 5 [--offset X --partitionSize Y]
+
+// We will count all docket entries. Call this n.
+// Unfortunately, createdAt lies. To handle this, we will set N = n + 1000
+// We will partition these into baskets of count / N (ceiling/floor), sorted by createdAt
+// It doesn't matter if more come in during this:
+//   these should already be indexed
+//   we have the buffer of 1000 in case they affect the offset such that we would miss existing data at the end of our sort
+// In other words, if we only index n and there are now N + X, those X will already be indexed
+// Done!
+
+// sort the table once
+// at each offset (calculated by num processes), get the createdAt
+// pass those createdAt dates into the child processes, which will function as the where query bounds
+
+async function getDocketEntriesCount() {
+  const countQuery = await getDbReader(reader =>
+    reader
+      .selectFrom('dwDocketEntry')
+      .select(reader.fn.countAll().as('count'))
+      .executeTakeFirst(),
+  );
+  return Number(countQuery?.count) + 1000; // TODO: add comment explaining this
+}
+
+// get earliest date
+// get latest date
+// divide
+
+async function getChunks(offsets: number[]) {
+  return (
+    await getDbReader(reader =>
+      reader.executeQuery(
+        CompiledQuery.raw(
+          `SELECT
+        created_at
+      FROM (
+        SELECT
+          created_at,
+          ROW_NUMBER() OVER (
+            ORDER BY 
+            created_at,
+            docket_number,
+            docket_entry_id             
+          ) AS rn
+        FROM dw_docket_entry
+      ) AS numbered_entries
+      WHERE rn IN (${offsets});`,
+        ),
+      ),
+    )
+  ).rows as unknown as { createdAt: Date }[];
+}
+
+async function getDateChunks(offsets: number[]) {
+  const rows = await getChunks(offsets);
+  console.log('rows', rows);
+
+  const updatedRows: Date[] = [
+    calculateDate({ dateString: '0000-01-01' }),
+    ...rows.map(r => r.createdAt),
+    calculateDate({ dateString: '5000-01-01' }),
+  ];
+  const chunkedDates: Date[][] = [];
+  for (let i = 1; i <= updatedRows.length - 1; i++) {
+    chunkedDates.push([updatedRows[i - 1], updatedRows[i]]);
+  }
+  return chunkedDates;
+}
+
+async function main() {
+  // If not offset, kick off the tasks
+  const count = await getDocketEntriesCount();
+  console.log('Number of docket entries', count);
+  const numProcesses = Number(num_processes);
+  console.log('Number of processes', numProcesses);
+  const partitionSize = Math.ceil(count / numProcesses);
+  const offsets: number[] = [];
+  for (let i = 0; i <= count; i += partitionSize) {
+    offsets.push(i);
+  }
+  console.log('offsets', offsets);
+  const dateChunks = await getDateChunks(offsets);
+  console.log('dateChunks', dateChunks);
+
+  for (const dateChunk of dateChunks) {
+    console.log('DOING THE THING');
+    const child = spawn(
+      'npx',
+      [
+        'ts-node',
+        '--transpile-only',
+        './scripts/run-once-scripts/postgres-migration/index-docket-entries/_index-docket-entries-child.ts',
+        dateChunk[0].toISOString(),
+        dateChunk[1].toISOString(),
+      ],
+      {
+        // stdio: 'pipe',
+        env: {
+          ...process.env,
+          NODE_ENV: 'production',
+        },
+      },
+    );
+
+    child.stdout.on('data', data => {
+      console.log(`[stdout] ${data.toString()}`);
+    });
+
+    child.stderr.on('data', data => {
+      console.error(`[stderr] ${data.toString()}`);
+    });
+
+    child.on('close', code => {
+      console.log(`Child process exited with code ${code}`);
+    });
+  }
+}
+
+main().catch(console.error);
