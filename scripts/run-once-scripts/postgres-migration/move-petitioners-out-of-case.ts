c65efc62948b49e59220d72e8d04ba79339334c9
zachrog <ztrogers1@gmail.com>
2025-07-08 08:48:18 -0700

    10495: Manually insert petitioners onto dwUser as it is going to have duplicates and we need to handle it.

--- scripts/run-once-scripts/postgres-migration/move-petitioners-out-of-case.ts
+++ scripts/run-once-scripts/postgres-migration/move-petitioners-out-of-case.ts
@@ -4,11 +4,14 @@ import {
   parseArgsAndEnvVars,
   type ScriptConfig,
 } from '../../helpers/parseArgsAndEnvVars';
-import { getDbReader } from '@web-api/database';
+import { getDbReader, getDbWriter } from '@web-api/database';
 import { isEmpty } from 'lodash';
-import { upsertUsers } from '@web-api/persistence/postgres/users/upsertUsers';
 import { RawUser } from '@shared/business/entities/User';
 import { ROLES } from '@shared/business/entities/EntityConstants';
+import { RawIrsPractitioner } from '@shared/business/entities/IrsPractitioner';
+import { RawPractitioner } from '@shared/business/entities/Practitioner';
+import { toKyselyNewUser } from '@web-api/persistence/postgres/users/mapper';
+import { OPENSEARCH_SYNC_ACTIONS } from '@web-api/lambdas/openSearch/openSearchSyncHandler';
 import { associateUsersWithCases } from '@web-api/persistence/postgres/cases/userOnCase/associateUsersWithCases';
 
 const scriptConfig: ScriptConfig = {
@@ -22,7 +25,7 @@ const scriptConfig: ScriptConfig = {
 };
 parseArgsAndEnvVars(scriptConfig);
 
-const pageSize = 2000; // Arbitrary, but seemed reasonably performant
+const pageSize = 500;
 
 const getCasesToMovePetitioners = async (offset: number) => {
   return await getDbReader(reader =>
@@ -36,6 +39,27 @@ const getCasesToMovePetitioners = async (offset: number) => {
   );
 };
 
+const upsertUsers = async (
+  users: (RawUser | RawPractitioner | RawIrsPractitioner)[],
+): Promise<void> => {
+  const dbUsers = users.map(user => toKyselyNewUser(user));
+
+  // Bypassing upsertUsers + pgInsertInto as there will be duplicate information in petitioners.
+  // When we upsert there will be duplicate petitioners with the same contactId in the same insert statement.
+  // This onConflict doNothing() will pick the first value if there are duplicate userIds
+  await getDbWriter({
+    action: OPENSEARCH_SYNC_ACTIONS.UPSERT,
+    cb: db =>
+      db
+        .insertInto('dwUser')
+        .values(dbUsers)
+        .onConflict(oc => oc.columns(['userId']).doNothing())
+        .returningAll()
+        .execute(),
+    table: 'dwUser',
+  });
+};
+
 let totalItems = 0;
 
 async function main() {
@@ -43,18 +67,21 @@ async function main() {
   let casesToIndex = await getCasesToMovePetitioners(offset);
 
   while (!isEmpty(casesToIndex)) {
-    for (let index = 0; index < casesToIndex.length; index++) {
-      const aCase = casesToIndex[index];
-      const petitionerThings: (RawUser & { docketNumber: string })[] =
-        aCase.petitioners.map(petitioner => ({
+    const petitionersToUpsert: (RawUser & { docketNumber: string })[] = [];
+
+    casesToIndex.forEach(aCase => {
+      aCase.petitioners?.forEach(petitioner => {
+        const petitionerToUpsert = {
           ...petitioner,
           userId: petitioner.contactId,
           role: ROLES.petitioner,
           docketNumber: aCase.docketNumber,
-        }));
-      await upsertUsers(petitionerThings);
-      await associateUsersWithCases(petitionerThings);
-    }
+        };
+        petitionersToUpsert.push(petitionerToUpsert);
+      });
+    });
+    await upsertUsers(petitionersToUpsert);
+    await associateUsersWithCases(petitionersToUpsert);
 
     totalItems += casesToIndex.length;
     console.log(`Total cases index so far: ${totalItems}`);
