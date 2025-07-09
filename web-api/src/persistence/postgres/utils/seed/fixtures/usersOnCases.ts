398c9531c3c17c0966f59acee0c12dae5e04a6a8
zachrog <ztrogers1@gmail.com>
2025-07-09 11:14:11 -0700

    10495 - add petitioners to usersOnCases for seed data

--- web-api/src/persistence/postgres/utils/seed/fixtures/usersOnCases.ts
+++ web-api/src/persistence/postgres/utils/seed/fixtures/usersOnCases.ts
@@ -122,4 +122,459 @@ export const usersOnCases: NewUserOnCaseKysely[] = [
     representing: JSON.stringify(['7805d1ab-18d0-43ec-bafb-654e83405416']),
     serviceIndicator: 'None',
   },
+  {
+    serviceIndicator: 'Paper',
+    userId: '9909b20a-c495-4438-a78c-8cf51afbbed3',
+    docketNumber: '101-11',
+  },
+  {
+    serviceIndicator: 'Paper',
+    userId: '3b124785-74b3-4f90-bcd8-12f35f6e4b2a',
+    docketNumber: '101-16',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '101-19',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '101-20',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '706bd02d-d672-4987-884d-6f32b013d225',
+    docketNumber: '101-20',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7b07cb79-f093-414a-a5c5-100abc3c389c',
+    docketNumber: '101-20',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '572962a0-f88a-4d43-a4be-0f9fb80dea11',
+    docketNumber: '101-20',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '6637ac05-0b98-4c2a-97f3-66109338a0f8',
+    docketNumber: '101-20',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '3593fc83-837b-40b2-a90d-7ca42f9bdfbd',
+    docketNumber: '101-20',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '227d3ce6-4052-49ab-b47a-452d15c2c95e',
+    docketNumber: '101-20',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '98c15355-7a7e-498a-9006-875d34eee803',
+    docketNumber: '101-20',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '101-21',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '101-22',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '101-23',
+  },
+  {
+    serviceIndicator: 'Paper',
+    userId: 'b3e1941f-230a-47bb-80ec-6b561c1765cd',
+    docketNumber: '101-24',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '102-19',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: 'f7272c25-99e1-4448-960e-e2a6b86e7d17',
+    docketNumber: '102-19',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '102-20',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '102-22',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '5da54af1-1969-4011-8275-a949084b7928',
+    docketNumber: '102-67',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '23d9342c-577d-4cc4-a44d-aae2dca9a436',
+    docketNumber: '103-19',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '103-20',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '103-22',
+  },
+  {
+    serviceIndicator: 'Paper',
+    userId: '93dc623e-6ba3-487b-a336-c2fe18db8188',
+    docketNumber: '103-67',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '103-67',
+  },
+  {
+    serviceIndicator: 'Paper',
+    userId: 'f7272c25-99e1-4448-960e-e2a6b86e7d17',
+    docketNumber: '104-17',
+  },
+  {
+    serviceIndicator: 'Paper',
+    userId: 'f7272c25-99e1-4448-960e-e2a6b86e7d17',
+    docketNumber: '104-18',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7f86ad38-2ccb-4010-bc6c-0a1304b80609',
+    docketNumber: '104-19',
+  },
+  {
+    serviceIndicator: 'Paper',
+    userId: 'f7272c25-99e1-4448-960e-e2a6b86e7d17',
+    docketNumber: '104-20',
+  },
+  {
+    serviceIndicator: 'Paper',
+    userId: '40eb0890-aab3-4925-8769-e056b30c502d',
+    docketNumber: '104-23',
+  },
+  {
+    serviceIndicator: 'Paper',
+    userId: '2fbbbe4c-c675-4f16-a8ce-228ae572d2d9',
+    docketNumber: '104-67',
+  },
+  {
+    serviceIndicator: 'Paper',
+    userId: '0e7e0136-7bed-4008-b7cc-d41b4c2d5b90',
+    docketNumber: '104-67',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '28cae029-bae2-4eef-ac54-878fbbab65e3',
+    docketNumber: '105-19',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '105-20',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405418',
+    docketNumber: '105-23',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '29425430-3b75-456d-9e25-6f906b8f11d1',
+    docketNumber: '105-23',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: 'bc936c44-ff22-478b-a9e9-971f250d599c',
+    docketNumber: '105-67',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '106-19',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405417',
+    docketNumber: '106-23',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '107-19',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '89d7d182-46da-4b96-b29b-260d15249c25',
+    docketNumber: '107-19',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '277ca24a-7be3-49e9-80d3-529a19a1a841',
+    docketNumber: '107-19',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '108-19',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '89d7d182-46da-4b96-b29b-260d15249c25',
+    docketNumber: '108-19',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '277ca24a-7be3-49e9-80d3-529a19a1a841',
+    docketNumber: '108-19',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '9a45147f-7859-4dcd-8a00-fecb72dee4b1',
+    docketNumber: '108-19',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '8b1cc7b5-ecee-4d5d-b065-f39911a16e4f',
+    docketNumber: '108-19',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: 'f6a1e0df-f893-4b2b-ab47-ac1c874da34d',
+    docketNumber: '108-19',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: 'b9a124ae-c9f7-4a52-9a51-e537081c08e1',
+    docketNumber: '109-19',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '6ddeb6c3-9666-4f6b-94fb-dbc625a0ffb6',
+    docketNumber: '110-19',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '111-19',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '112-19',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '113-19',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '117-20',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '120-20',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '1566ea80-1d95-42bc-b006-7fc55c698dea',
+    docketNumber: '121-20',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '122-20',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '123-20',
+  },
+  {
+    serviceIndicator: 'Paper',
+    userId: 'd2fadb14-b0bb-4019-b6b1-cb51cb1cb92f',
+    docketNumber: '124-20',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '125-20',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '8d6d7ebb-6863-41c8-acc4-b014935efafb',
+    docketNumber: '127-20',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '128-20',
+  },
+  {
+    serviceIndicator: 'Paper',
+    userId: '05b98fff-e4ca-4114-b8ec-7218effc982e',
+    docketNumber: '129-20',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '1338-20',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '150-12',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '151-12',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '152-12',
+  },
+  {
+    serviceIndicator: 'None',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '301-21',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '302-21',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '303-21',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '304-21',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '305-21',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '306-21',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '307-21',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '308-21',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '309-21',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '310-21',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '311-21',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '312-21',
+  },
+  {
+    serviceIndicator: 'Paper',
+    userId: '6206bb45-fa32-4391-8ec3-e48b0359dcf2',
+    docketNumber: '313-21',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '314-21',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '315-21',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '316-21',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '317-21',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '318-21',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '319-21',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '320-21',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '69312-87',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405417',
+    docketNumber: '69312-87',
+  },
+  {
+    serviceIndicator: 'Electronic',
+    userId: '7805d1ab-18d0-43ec-bafb-654e83405416',
+    docketNumber: '999-15',
+  },
+  {
+    serviceIndicator: 'Paper',
+    userId: '89d7d182-46da-4b96-b29b-260d15249c25',
+    docketNumber: '999-15',
+  },
 ];
