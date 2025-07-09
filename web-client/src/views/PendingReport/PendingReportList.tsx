29a693beaf16ba7283e2aa4ecc50459c4d8b86bb
Christie Laferriere <christie.laferriere.ctr@ustaxcourt.gov>
2025-07-09 13:55:09 -0400

    add count to bottom align with paginator when paginator renders and bottom align with filters when only filters render in Pending Reports table

--- web-client/src/views/PendingReport/PendingReportList.tsx
+++ web-client/src/views/PendingReport/PendingReportList.tsx
@@ -34,7 +34,6 @@ type PendingReportListParams = {
 };
 
 export function PendingReportList({
-  hasPendingItemsResults,
   pendingItems,
   pendingItemsTotal,
   pendingReportListHelper,
@@ -49,8 +48,8 @@ export function PendingReportList({
 
   return (
     <>
-      <div className="grid-row margin-bottom-2">
-        <div className="tablet:grid-col-8">
+      <div className="grid-row margin-bottom-3">
+        <div className="tablet:grid-col-11">
           <div className="grid-row grid-gap">
             <label
               className="dropdown-label-serif margin-right-3"
@@ -83,26 +82,31 @@ export function PendingReportList({
             </BindedSelect>
           </div>
         </div>
-      </div>
-
-      <div ref={paginatorTop}>
-        {totalPages > 1 && (
-          <Paginator
-            currentPageIndex={activePage}
-            totalPages={totalPages}
-            onPageChange={pageChange => {
-              setActivePage(pageChange);
-              focusPaginatorTop(paginatorTop);
-            }}
-          />
+        {totalPages <= 1 && (
+          <div className="text-right margin-top-3 grid-col-fill">
+            <span className="text-semibold">Count: </span>
+            {pendingItemsTotal}
+          </div>
         )}
       </div>
 
-      {hasPendingItemsResults && (
-        <div className="text-right margin-bottom-2">
-          <span className="text-semibold" data-testid="display-data-count">
-            Count: {pendingItemsTotal}
-          </span>
+      {totalPages > 1 && (
+        <div ref={paginatorTop} className="grid-row margin-bottom-3">
+          <div className="grid-col-2"></div>
+          <div className="grid-col-8">
+            <Paginator
+              currentPageIndex={activePage}
+              totalPages={totalPages}
+              onPageChange={pageChange => {
+                setActivePage(pageChange);
+                focusPaginatorTop(paginatorTop);
+              }}
+            />
+          </div>
+          <div className="grid-col-fill text-right margin-top-3 padding-0">
+            <span className="text-semibold">Count: </span>
+            {pendingItemsTotal}
+          </div>
         </div>
       )}
 
