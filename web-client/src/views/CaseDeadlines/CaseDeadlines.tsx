7022176109d94eba8628b3c4fd5a7b85322de63e
Alex Dziarnowski <alexdziarn@gmail.com>
2025-07-15 17:05:59 -0400

    10603: fix merge conflict

--- web-client/src/views/CaseDeadlines/CaseDeadlines.tsx
+++ web-client/src/views/CaseDeadlines/CaseDeadlines.tsx
@@ -12,7 +12,6 @@ import { focusPaginatorTop } from '@web-client/presenter/utilities/focusPaginato
 import { sequences } from '@web-client/presenter/app.cerebral';
 import { state } from '@web-client/presenter/app.cerebral';
 import React, { useRef, useState } from 'react';
-import { updateScreenMetadataSequence } from '@web-client/presenter/sequences/updateScreenMetadataSequence';
 
 export const CaseDeadlines = connect(
   {
