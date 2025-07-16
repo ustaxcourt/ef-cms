319db8a90adf3790aabede2ba868ecbb82cb8b55
zachrog <ztrogers1@gmail.com>
2025-07-16 09:35:58 -0700

    10495 - remove appcontext from getUserInteractor

--- web-api/src/persistence/postgres/users/getUserByIdOnceAllUpdatesComplete.ts
+++ web-api/src/persistence/postgres/users/getUserByIdOnceAllUpdatesComplete.ts
@@ -1,18 +1,18 @@
-import { RawIrsPractitioner } from '@shared/business/entities/IrsPractitioner';
-import { RawPractitioner } from '@shared/business/entities/Practitioner';
-import { RawPrivatePractitioner } from '@shared/business/entities/PrivatePractitioner';
-import { RawUser } from '@shared/business/entities/User';
 import { sleep } from '@shared/tools/helpers';
+import { NotFoundError } from '@web-api/errors/errors';
 import { getUserById } from '@web-api/persistence/postgres/users/getUserById';
+import { DbUser } from '@web-api/persistence/postgres/users/mapper';
 
 export const getUserByIdOnceAllUpdatesComplete = async ({
   userId,
 }: {
   userId: string;
-}): Promise<
-  RawPrivatePractitioner | RawIrsPractitioner | RawPractitioner | RawUser
-> => {
+}): Promise<DbUser> => {
   const user = await getUserById({ userId });
+
+  if (!user) {
+    throw new NotFoundError(`User not found with user id ${userId}`);
+  }
 
   if (!user.isUpdatingInformation) return user;
 
