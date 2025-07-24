6d697af115911f37daca4cbacbdef1ec3da230ab
John Cruz <jcruz@flexion.us>
2025-07-24 15:46:34 -0600

    10576: Fix unit testsl

--- web-api/src/business/useCases/caseDeadline/createCaseDeadlineInteractor.consolidateCases.test.ts
+++ web-api/src/business/useCases/caseDeadline/createCaseDeadlineInteractor.consolidateCases.test.ts
@@ -1,11 +1,16 @@
 import '@web-api/persistence/postgres/caseDeadlines/mocks.jest';
 import '@web-api/persistence/postgres/cases/mocks.jest';
 import '@web-api/persistence/postgres/workitems/mocks.jest';
+
 jest.mock('@web-api/persistence/postgres/caseDeadlines/upsertCaseDeadlines');
 jest.mock('@web-api/persistence/postgres/cases/getCaseByDocketNumber');
 jest.mock(
   '@web-api/business/useCaseHelper/automaticBlock/updateCaseAutomaticBlock',
 );
+jest.mock(
+  '@web-api/business/useCaseHelper/caseAssociation/updateCaseAndAssociations',
+);
+
 import {
   CaseDeadline,
   RawCaseDeadline,
@@ -20,6 +25,7 @@ import { createCaseDeadline } from '@web-api/business/useCases/caseDeadline/crea
 import { MOCK_CASE } from '@shared/test/mockCase';
 import { getCaseByDocketNumber as getCaseByDocketNumberMock } from '@web-api/persistence/postgres/cases/getCaseByDocketNumber';
 import { updateCaseAutomaticBlock as updateCaseAutomaticBlockMock } from '@web-api/business/useCaseHelper/automaticBlock/updateCaseAutomaticBlock';
+import { updateCaseAndAssociations as updateCaseAndAssociationsMock } from '@web-api/business/useCaseHelper/caseAssociation/updateCaseAndAssociations';
 
 describe('createCaseDeadlineInteractor - Consolidated Cases', () => {
   const LEAD_DOCKET_NUMBER = '101-25';
@@ -27,12 +33,14 @@ describe('createCaseDeadlineInteractor - Consolidated Cases', () => {
   const CASE_DEADLINE_ID = getUniqueId();
   const getCaseByDocketNumber = jest.mocked(getCaseByDocketNumberMock);
   const updateCaseAutomaticBlock = jest.mocked(updateCaseAutomaticBlockMock);
+  const updateCaseAndAssociations = jest.mocked(updateCaseAndAssociationsMock);
 
   beforeEach(() => {
     updateCaseAutomaticBlock.mockImplementation(
       // eslint-disable-next-line @typescript-eslint/require-await
       async params => params.caseEntity,
     );
+    updateCaseAndAssociations.mockResolvedValue(MOCK_CASE);
   });
 
   it('should create a case deadline for all the cases in the consolidated group', async () => {
@@ -74,6 +82,7 @@ describe('createCaseDeadlineInteractor - Consolidated Cases', () => {
 
     const upsertCaseDeadlinesCalls = (upsertCaseDeadlines as jest.Mock).mock
       .calls;
+    console.log('upsertCaseDeadlinesCalls', upsertCaseDeadlinesCalls);
     expect(upsertCaseDeadlinesCalls.length).toEqual(2);
     expect(upsertCaseDeadlinesCalls[0][0]).toMatchObject([
       {
