8cd8fa2de07729f6d53ad178efafda2cb80b9ce9
Anthony <Anthony.Loera.ctr@ustaxcourt.gov>
2025-07-27 14:56:12 -0700

    10589: code clean-up

--- web-client/src/views/EditDocketEntry/EditDocketEntryMetaFormDocument.tsx
+++ web-client/src/views/EditDocketEntry/EditDocketEntryMetaFormDocument.tsx
@@ -10,10 +10,7 @@ import { reactSelectValue } from '@web-client/ustc-ui/Utils/documentTypeSelectHe
 import { sequences } from '@web-client/presenter/app.cerebral';
 import { state } from '@web-client/presenter/app.cerebral';
 import React from 'react';
-import {
-  SYSTEM_AND_INTERNAL_DOCUMENT_TYPES,
-  SYSTEM_ROLE,
-} from '@shared/business/entities/EntityConstants';
+import { SYSTEM_AND_INTERNAL_DOCUMENT_TYPES } from '@shared/business/entities/EntityConstants';
 
 export const EditDocketEntryMetaFormDocument = connect(
   {
@@ -103,7 +100,6 @@ export const EditDocketEntryMetaFormDocument = connect(
               documentTypes: SYSTEM_AND_INTERNAL_DOCUMENT_TYPES,
               selectedEventCode: form.eventCode,
             })}
-            isDisabled={form.filedBy === SYSTEM_ROLE}
             onChange={inputValue => {
               const value = inputValue?.value || '';
               updateDocketEntryMetaDocumentFormValueSequence({
