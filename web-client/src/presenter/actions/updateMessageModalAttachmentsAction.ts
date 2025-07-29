5bd84cd2903be5dae5aa15fc2132381a6466eaf6
Russ Frith <rgf314159@gmail.com>
2025-07-29 08:32:03 -0800

    10687-story : resolved topics C426, C427, C192 from Excel test matrix found in story specs

--- web-client/src/presenter/actions/updateMessageModalAttachmentsAction.ts
+++ web-client/src/presenter/actions/updateMessageModalAttachmentsAction.ts
@@ -6,9 +6,10 @@ export const updateMessageModalAttachmentsAction = ({
   props,
   store,
 }: ActionProps) => {
-  const { attachments, draftAttachments } = get(state.modal.form);
+  const { draftAttachments } = get(state.modal.form);
   const caseDetail = get(state.caseDetail);
   const documentId = props.documentId || get(state.docketEntryId);
+  const showModal = get(state.modal.showModal);
 
   if (documentId) {
     const document = applicationContext
@@ -22,21 +23,33 @@ export const updateMessageModalAttachmentsAction = ({
       .getUtilities()
       .getDescriptionDisplay(document);
 
-    if (attachments.length + draftAttachments.length === 0) {
-      // This is the first attachment, so we should update the subject
-      store.set(state.modal.form.subject, documentTitle.slice(0, 250));
-    }
     if (props.action === 'add') {
       draftAttachments.push({
         documentId,
         documentTitle,
         index: document.index,
       });
+      if (showModal === 'CreateMessageModal') {
+        store.set(state.modal.form.subject, documentTitle.slice(0, 250));
+      }
     } else if (props.action === 'remove') {
       const foundIndex = draftAttachments.findIndex(
         attachment => attachment.documentId == props.documentId,
       );
       draftAttachments.splice(foundIndex, 1);
+
+      if (showModal === 'CreateMessageModal') {
+        if (draftAttachments.length === 0) {
+          store.unset(state.modal.form.subject);
+        } else {
+          const latestAttachment =
+            draftAttachments[draftAttachments.length - 1];
+          store.set(
+            state.modal.form.subject,
+            latestAttachment.documentTitle.slice(0, 250),
+          );
+        }
+      }
     }
 
     store.set(state.modal.form.draftAttachments, draftAttachments);
