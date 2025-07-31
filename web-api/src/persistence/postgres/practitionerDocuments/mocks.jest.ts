104c1ee793aec0dda977a3a5d0bb184893d3ab3a
Pavan <pyenduri@flexion.us>
2025-07-31 14:48:51 -0500

    10637 script to migrate practitionerDocument entities from dynamo to postgres

---
+++ web-api/src/persistence/postgres/practitionerDocuments/mocks.jest.ts
@@ -0,0 +1,21 @@
+import { mockFactory } from '@shared/test/mockFactory';
+
+jest.mock(
+  '@web-api/persistence/postgres/practitionerDocuments/createOrEditPractitionerDocument.ts',
+  () => mockFactory('createOrEditPractitionerDocument'),
+);
+
+jest.mock(
+  '@web-api/persistence/postgres/practitionerDocuments/getPractitionerDocumentByField.ts',
+  () => mockFactory('getPractitionerDocumentByField'),
+);
+
+jest.mock(
+  '@web-api/persistence/postgres/practitionerDocuments/getPractitionerDocuments.ts',
+  () => mockFactory('getPractitionerDocuments'),
+);
+
+jest.mock(
+  '@web-api/persistence/postgres/practitionerDocuments/deletePractitionerDocument.ts',
+  () => mockFactory('deletePractitionerDocument'),
+);
