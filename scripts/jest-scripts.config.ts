ea4aa9e1ca22348e9a41220542d7cbab479734b1
zachrog <ztrogers1@gmail.com>
2025-08-12 13:31:20 -0700

    10495 - ignore small scripts

--- scripts/jest-scripts.config.ts
+++ scripts/jest-scripts.config.ts
@@ -46,6 +46,8 @@ const config: Config = {
     '!secrets/**',
     '!set-maintenance-mode-locally.ts',
     '!template.ts',
+    '!judge/get-judge-name.ts',
+    '!judge/set-judge-title.ts',
     '!upload-practitioner-application-packages.ts',
     '!user/**',
   ],
