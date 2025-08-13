75ce17a8ee7a2b8d6b37fd59dccc8372bf7d4d6b
Russ Frith <rgf314159@gmail.com>
2025-08-13 09:44:59 -0800

    8727-order-search-improvements: filtered search results to exclude sealed cases

--- shared/src/business/useCases/orderAdvancedSearchInteractor.ts
+++ shared/src/business/useCases/orderAdvancedSearchInteractor.ts
@@ -1,5 +1,8 @@
 import { FORMATS, formatNow } from '@shared/business/utilities/DateHandler';
-import { ORDER_EVENT_CODES } from '@shared/business/entities/EntityConstants';
+import {
+  MAX_SEARCH_RESULTS,
+  ORDER_EVENT_CODES,
+} from '@shared/business/entities/EntityConstants';
 import {
   ROLE_PERMISSIONS,
   isAuthorized,
@@ -10,6 +13,8 @@ import { User } from '@shared/business/entities/User';
 import { omit } from 'lodash';
 import { ServerApplicationContext } from '@web-api/applicationContext';
 import { DocumentSearch } from '@shared/business/entities/documents/DocumentSearch';
+import { InternalDocumentSearchResult } from '../entities/documents/InternalDocumentSearchResult';
+import { filterCaseSearchResultsNotAccessibleToUser } from '../utilities/caseFilter';
 
 export const orderAdvancedSearchInteractor = async (
   applicationContext: ServerApplicationContext,
@@ -69,5 +74,10 @@ export const orderAdvancedSearchInteractor = async (
     userRole: authorizedUser.role,
   });
 
-  return results;
+  const filteredResults = filterCaseSearchResultsNotAccessibleToUser(
+    results,
+    authorizedUser,
+  ).slice(0, MAX_SEARCH_RESULTS);
+
+  return InternalDocumentSearchResult.validateRawCollection(filteredResults);
 };
