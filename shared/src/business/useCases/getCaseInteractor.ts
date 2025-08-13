b29def699da1c9f32a4e826549d4a8bc5f49d719
Abel <Abel.Ratanaphan.ctr@ustaxcourt.gov>
2025-08-13 13:56:53 -0400

    8727-order-search-improvements: remove code to add orders locally

--- shared/src/business/useCases/getCaseInteractor.ts
+++ shared/src/business/useCases/getCaseInteractor.ts
@@ -6,8 +6,6 @@ import {
   isAuthUser,
 } from '@shared/business/entities/authUser/AuthUser';
 import { getCaseByDocketNumber } from '@web-api/persistence/postgres/cases/getCaseByDocketNumber';
-import { upsertDocketEntries } from '@web-api/persistence/postgres/docketEntries/upsertDocketEntries';
-import { v4 } from 'uuid';
 
 export const getCaseInteractor = async (
   { docketNumber }: { docketNumber: string },
@@ -29,24 +27,6 @@ export const getCaseInteractor = async (
     const error = new NotFoundError(`Case ${docketNumber} was not found.`);
     error.skipLogging = true;
     throw error;
-  }
-
-  // ** BELOW IS CODE TO ADD ORDERS LOCALLY -------- NEEDS TO BE REMOVED BEFORE STAGING **
-
-  if (
-    caseRecord.docketNumber === '104-17' &&
-    caseRecord.docketEntries.length < 10000
-  ) {
-    // use multiple cases to add to
-    const rawOrder = caseRecord.docketEntries.find(
-      entry => entry.docketEntryId === '1f1aa3f7-e2e3-43e6-885d-4ce341588c76',
-    );
-    const orders = Array.from({ length: 500 }, (_, i) => ({
-      ...rawOrder,
-      docketEntryId: v4(),
-      index: caseRecord.docketEntries.length + i,
-    }));
-    await upsertDocketEntries(orders);
   }
 
   return CaseFactory.getCase({ rawCase: caseRecord, user: authorizedUser });
