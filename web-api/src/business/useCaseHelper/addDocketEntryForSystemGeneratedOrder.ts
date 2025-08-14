d5f5542f8f25e34198cd060ee543b228ccac6f5b
Anthony <Anthony.Loera.ctr@ustaxcourt.gov>
2025-08-14 09:35:02 -0700

    10685: removed code from untouched file

--- web-api/src/business/useCaseHelper/addDocketEntryForSystemGeneratedOrder.ts
+++ web-api/src/business/useCaseHelper/addDocketEntryForSystemGeneratedOrder.ts
@@ -71,7 +71,6 @@ export const addDocketEntryForSystemGeneratedOrder = async ({
       caseCaptionExtension,
       caseTitle,
       docketNumberWithSuffix,
-      addedDocketNumbers: [],
       nameOfClerk,
       orderContent: systemGeneratedDocument.content,
       orderTitle: systemGeneratedDocument.documentTitle.toUpperCase(),
