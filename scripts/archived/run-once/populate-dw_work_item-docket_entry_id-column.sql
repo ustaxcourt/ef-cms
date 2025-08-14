5dfb47503af753415e2da3a26383555ebfaaca24
Jim Lerza <james.lerza@ustaxcourt.gov>
2025-08-14 08:57:46 -0400

    opex-populate-docket-entry-id-column-in-work-items-table: added a usage comment to populate-dw_work_item-docket_entry_id-column

--- scripts/archived/run-once/populate-dw_work_item-docket_entry_id-column.sql
+++ scripts/archived/run-once/populate-dw_work_item-docket_entry_id-column.sql
@@ -1,3 +1,16 @@
+-- This script populates dw_work_item.docket_entry_id from dw_work_item_prod.docket_entry.docket_entry_id
+
+-- Usage:
+--   . scripts/env/set-env.zsh ustc-test
+--   export PGPASSWORD=$(scripts/postgres/generate-token.sh -t -w)
+--   psql \
+--     --host=$DB_HOST \
+--     --username=$DB_USER \
+--     --dbname=$DB_NAME \
+--     --port=5432 \
+--     --file=scripts/archived/run-once/populate-dw_work_item-docket_entry_id-column.sql \
+--     --echo-errors
+
 WITH batch AS (
     SELECT wi.work_item_id, (wip.docket_entry->>'docketEntryId') AS new_id
     FROM dw_work_item wi JOIN dw_work_item_prod wip USING (work_item_id)
