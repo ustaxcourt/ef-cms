5932056eedd44350e0453b5204457308c09152e1
Anthony <Anthony.Loera.ctr@ustaxcourt.gov>
2025-08-14 08:24:09 -0700

    10685: Fixed permissions and removed drafts, and STIN from recent fillings page

--- web-api/src/business/useCaseHelper/addDocketEntryForSystemGeneratedOrder.ts
+++ web-api/src/business/useCaseHelper/addDocketEntryForSystemGeneratedOrder.ts
@@ -66,6 +66,7 @@ export const addDocketEntryForSystemGeneratedOrder = async ({
       caseCaptionExtension,
       caseTitle,
       docketNumberWithSuffix,
+      addedDocketNumbers: [],
       nameOfClerk,
       orderContent: systemGeneratedDocument.content,
       orderTitle: systemGeneratedDocument.documentTitle.toUpperCase(),
