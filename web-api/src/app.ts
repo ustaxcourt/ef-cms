31c171e78c478503a722477f6f78c332c1522190
TomElliottFlexion <telliott@flexion.us>
2025-08-18 10:59:55 -0500

    10638: add endpoint for use with update-email-no-access zendesk automation

--- web-api/src/app.ts
+++ web-api/src/app.ts
@@ -208,7 +208,8 @@ import express from 'express';
 import { getTrialSessionOpenCasesCountLambda } from '@web-api/lambdas/trialSessions/getTrialSessionOpenCasesCountLambda';
 import { getConsolidatedCaseDeadlinesLambda } from '@web-api/lambdas/caseDeadline/getConsolidatedCaseDeadlinesLambda';
 import { removePetitionerEmailLambda } from '@web-api/lambdas/cases/removePetitionerEmailLambda';
-import { deactiveUserLambda } from '@web-api/lambdas/automations/deactivateUserLambda';
+import { deactivateUserLambda } from '@web-api/lambdas/automations/deactivateUserLambda';
+import { removeUserPendingEmailLambda } from '@web-api/lambdas/automations/removeUserPendingEmailLambda';
 
 export const app = express();
 
@@ -1095,7 +1096,11 @@ app.delete(
  */
 app.post('/users', lambdaWrapper(createUserLambda)); // NOTE: These are meant for admins and zendesk automations. Not meant for regular application use.
 app.get('/users/user-summary', lambdaWrapper(regStatusLambda));
-app.post('/users/deactivate', lambdaWrapper(deactiveUserLambda));
+app.post('/users/deactivate', lambdaWrapper(deactivateUserLambda));
+app.post(
+  '/users/remove-pending-email',
+  lambdaWrapper(removeUserPendingEmailLambda),
+);
 
 /**
  * work-items
