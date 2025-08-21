62426bccbbe2ad79810aac5716a6a7d659b4dcd8
Anthony <Anthony.Loera.ctr@ustaxcourt.gov>
2025-08-21 11:59:12 -0700

    10748: Updated toRawObject command to not override validation properties

--- shared/src/business/entities/Practitioner.ts
+++ shared/src/business/entities/Practitioner.ts
@@ -199,12 +199,14 @@ export class Practitioner extends User {
     return Practitioner.VALIDATION_RULES as any;
   }
 
-  toRawObject() {
+  toRawObject(removeValidationProperties: boolean = true) {
     const result = super.toRawObject() as any;
 
     // We don't want to persist these values as they are only used for validation
-    result.confirmEmail = undefined;
-    result.updatedEmail = undefined;
+    if (removeValidationProperties) {
+      result.confirmEmail = undefined;
+      result.updatedEmail = undefined;
+    }
 
     return result;
   }
