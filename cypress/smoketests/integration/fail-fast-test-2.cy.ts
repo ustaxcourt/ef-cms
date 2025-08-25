779171bddb3c84da882cbc5623538a5199a46ec7
Anthony <Anthony.Loera.ctr@ustaxcourt.gov>
2025-08-25 09:43:01 -0700

    10478: Reverted fail-fast tests, added new fail fast tests, edited run-cypress.sh file to handle fail fast method

---
+++ cypress/smoketests/integration/fail-fast-test-2.cy.ts
@@ -0,0 +1,9 @@
+describe('Fail Fast Test 2', () => {
+  it('should pass this test', () => {
+    // This test should pass
+  });
+
+  it('should also pass', () => {
+    // This test should also pass
+  });
+});
---
+++ cypress/smoketests/integration/fail-fast-test.cy.ts
@@ -0,0 +1,13 @@
+describe('Fail Fast Test', () => {
+  it('should pass this test', () => {
+    // This test should pass
+  });
+
+  it('should fail this test', () => {
+    throw new Error('This test is designed to fail for fail-fast testing');
+  });
+
+  it('should not run this test if fail-fast works', () => {
+    // This test should not run if fail-fast works
+  });
+});
