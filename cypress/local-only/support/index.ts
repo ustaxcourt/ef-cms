617a12178b5cef1dbf0b8b5803196d6eba2eb782
Anthony <Anthony.Loera.ctr@ustaxcourt.gov>
2025-08-25 10:42:03 -0700

    10478: reverted puroseful failing test, reverted unneeded changes

--- cypress/local-only/support/index.ts
+++ cypress/local-only/support/index.ts
@@ -5,7 +5,7 @@ import { mockDynamsoftLibrary } from 'cypress/helpers/authentication/dynamsoft';
 
 before(() => {
   // Skip subsequent tests in spec when one fails.
-  (cy.state('runnable').ctx as Mocha.Context).currentTest?.parent?.bail(true);
+  (cy.state('runnable').ctx as Mocha.Context).currentTest.parent.bail(true);
 });
 
 beforeEach(() => {
--- scripts/run-cypress.sh
+++ scripts/run-cypress.sh
@@ -149,12 +149,10 @@ fi
 if [ -n "${OPEN}" ]; then
   ./node_modules/.bin/cypress open --browser "${BROWSER}" -C "${CONFIG_FILE}"
 else
-  CYPRESS_ARGS="--browser ${BROWSER} -C ${CONFIG_FILE}"
-  [ -n "${RUN_SPECIFIC_TEST}" ] && CYPRESS_ARGS="${CYPRESS_ARGS} --spec ${RUN_SPECIFIC_TEST// /,}"
-  [ -n "${FAIL_FAST}" ] && [ -n "${SMOKETESTS}" ] && CYPRESS_ARGS="${CYPRESS_ARGS} --reporter junit --reporter-options mochaFile=cypress/results/results-[hash].xml"
-  
-  ./node_modules/.bin/cypress run "${CYPRESS_ARGS}" || {
-    [ -n "${FAIL_FAST}" ] && [ -n "${SMOKETESTS}" ] && echo "Tests failed in fail-fast mode, but artifacts will be preserved"
-    exit 1
-  }
+  if [ -n "${RUN_SPECIFIC_TEST}" ]; then
+    RUN_SPECIFIC_TEST="${RUN_SPECIFIC_TEST// /,}"
+    ./node_modules/.bin/cypress run --browser "${BROWSER}" -C "${CONFIG_FILE}" --spec "${RUN_SPECIFIC_TEST}"
+  else
+    ./node_modules/.bin/cypress run --browser "${BROWSER}" -C "${CONFIG_FILE}"
+  fi
 fi
