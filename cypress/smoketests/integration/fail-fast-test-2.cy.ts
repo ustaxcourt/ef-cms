57c00ac8ffe511179f74077efaf7cdacef1eab38
Anthony <Anthony.Loera.ctr@ustaxcourt.gov>
2025-08-25 11:14:13 -0700

    10478: Added failing test in smoketest

--- cypress/smoketests/integration/fail-fast-test-2.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('Fail Fast Test 2', () => {
-  it('should pass this test', () => {
-    // This test should pass
-  });
-
-  it('should also pass', () => {
-    // This test should also pass
-  });
-});
