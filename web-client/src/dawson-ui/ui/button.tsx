d79e2002f918e734e47f15ea6acc63e255ba9a94
Jake Roberts <jdr81394@gmail.com>
2025-08-26 14:34:52 -0400

    primary, secondary, destructive. All button styles on desktop completed

--- web-client/src/dawson-ui/ui/button.tsx
+++ web-client/src/dawson-ui/ui/button.tsx
@@ -16,27 +16,37 @@ const buttonVariants = cva(
         primary: cn(
           'tw:bg-primary tw:text-primary-foreground tw:shadow-xs tw:outline-none', // standard
           'tw:hover:bg-primary-darker', // hover
-          'tw:focus-visible:ring-offset-4', // focus-visible
+          'tw:focus-visible:ring-offset-[4px] tw:focus-visible:ring-[4px]', // focus-visible
+          'tw:active:bg-primary-active',  // active
+          'tw:invalid:bg-primary-invalid' // inactive / invalid
         ),
         destructive: cn(
           'tw:h-9 tw:bg-destructive tw:text-white tw:shadow-xs  tw:outline-none tw:border-none', // standard
           'tw:hover:bg-destructive-darker', // hover
           'tw:focus-visible:ring-offset-4', // focus-visible
+          'tw:active:bg-destructive-active', // active
+          'tw:invalid:bg-destructive-invalid', // invalid
         ),
         secondary: cn(
           'tw:h-9 tw:shadow-none tw:bg-background tw:outline-primary tw:outline-2 tw:text-primary', // standard
           'tw:hover:text-primary-darker tw:hover:outline-primary-darker', // hover
-          'tw:focus-visible:ring-offset-4', // focus-visible
+          'tw:focus-visible:ring-offset-[6px] tw:focus-visible:ring-[4px]', // focus-visible
+          'tw:active:text-secondary-active tw:active:outline-secondary-active', // active
+          'tw:invalid:text-secondary-invalid tw:invalid:outline-secondary-invalid' // inactive / invalid
         ),
         destructiveTertiary: cn(
           'tw:bg-transparent tw:text-destructive tw:font-[400] tw:underline tw:underline-offset-4 tw:outline-none ', // standard,
           'tw:hover:underline tw:hover:text-destructive-darker', // hover
-          'tw:has-[>svg]:p-0, ', // focus-visible
+          'tw:has-[>svg]:p-0 tw:focus-visible:ring-offset-[3px]', // focus-visible
+          'tw:active:text-tertiary-active', // active
+          'tw:invalid:text-tertiary-invalid', // invalid
         ),
         primaryTertiary: cn(
           'tw:bg-transparent tw:text-primary tw:font-[400] tw:underline tw:underline-offset-4  tw:outline-none ', // standard
           'tw:hover:underline tw:hover:text-primary-darker', // hover,
           'tw:has-[>svg]:px-[4px] tw:has-[>svg]:py-[2px] h-[24px]', // focus-visible
+          'tw:active:text-primary-active' , // active
+          'tw:invalid:text-tertiary-invalid', // active
         ),
       },
       size: {
