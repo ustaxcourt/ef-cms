496877b15a5cb1661cfa73d61f33bc698f65176b
John Cruz <jcruz@flexion.us>
2025-08-26 07:25:25 -0600

    10576: Check if potentially no case deadline ids are available before calling method;

--- web-api/src/business/useCases/caseConsolidation/removeConsolidatedCasesInteractor.ts
+++ web-api/src/business/useCases/caseConsolidation/removeConsolidatedCasesInteractor.ts
@@ -158,6 +158,9 @@ async function updateConsolidatedCaseDeadlineReferenceId(
   const results = await Promise.allSettled(
     LEAD_DEADLINES.map(async leadCaseDeadline => {
       const { caseDeadlineId: oldLeadCaseDeadlineId } = leadCaseDeadline;
+
+      if (!oldLeadCaseDeadlineId) return;
+
       const CHILD_DEADLINES =
         await getCaseDeadlinesByConsolidatedCaseDeadlineIds([
           oldLeadCaseDeadlineId,
