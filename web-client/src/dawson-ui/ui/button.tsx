18fe2f1602c49d3c7f51e6f09ff1381a6f9369a2
Jake Roberts <jdr81394@gmail.com>
2025-09-04 17:57:56 -0400

    Completed the following: when the button wraps, the height needs to remain the same (40px) Desktop View - Check correct font size (18px) AND mobile must stay at 16px In the middle of: - Icon width needs to be 16px (the path element in the svg element must have width of 16px)

--- web-client/src/dawson-ui/ui/button.tsx
+++ web-client/src/dawson-ui/ui/button.tsx
@@ -8,7 +8,7 @@ import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 const buttonVariants = cva(
   cn(
-    'tw:cursor-pointer tw:inline-flex tw:items-center tw:justify-center tw:gap-2 tw:whitespace-nowrap tw:rounded-md tw:text-base tw:font-bold tw:transition-all tw:disabled:pointer-events-none tw:disabled:opacity-50 tw:[&_svg]:pointer-events-none tw:[&_svg:not([class*=size-])]:size-4 tw:shrink-0 tw:[&_svg]:shrink-0 tw:border-none',
+    'tw:text-[16px] tw:sm:text-[18px] tw:cursor-pointer tw:inline-flex tw:items-center tw:justify-center tw:gap-2 tw:whitespace-nowrap tw:rounded-md tw:text-base tw:transition-all tw:disabled:pointer-events-none tw:disabled:opacity-50 tw:[&_svg]:pointer-events-none tw:[&_svg:not([class*=size-])]:size-4 tw:shrink-0 tw:[&_svg]:shrink-0 tw:border-none',
     'tw:focus-visible:ring-[3px] tw:focus-visible:ring-ring  focus-visible:ring-4 tw:focus-visible:border-ring', // focus-visible
     'tw:aria-invalid:ring-destructive/20 tw:aria-invalid:border-destructive', // aria
     'tw:w-full tw:xs:w-auto', // small & greater
@@ -17,35 +17,35 @@ const buttonVariants = cva(
     variants: {
       variant: {
         primary: cn(
-          'tw:bg-primary tw:text-primary-foreground tw:shadow-xs tw:outline-none', // standard
+          'tw:h-[40px] tw:bg-primary tw:text-primary-foreground tw:shadow-xs tw:outline-none tw:font-bold', // standard
           'tw:hover:bg-primary-darker', // hover
           'tw:focus-visible:ring-offset-[4px] tw:focus-visible:ring-[4px]', // focus-visible
           'tw:active:bg-primary-active', // active
           'tw:invalid:bg-primary-invalid', // inactive / invalid
         ),
         destructive: cn(
-          'tw:h-9 tw:bg-destructive tw:text-white tw:shadow-xs  tw:outline-none tw:border-none', // standard
+          'tw:h-[40px] tw:bg-destructive tw:text-white tw:shadow-xs  tw:outline-none tw:border-none tw:font-bold', // standard
           'tw:hover:bg-destructive-darker', // hover
           'tw:focus-visible:ring-offset-4', // focus-visible
           'tw:active:bg-destructive-active', // active
           'tw:invalid:bg-destructive-invalid', // invalid
         ),
         secondary: cn(
-          'tw:h-9 tw:shadow-none tw:bg-background tw:outline-primary tw:outline-2 tw:text-primary', // standard
+          'tw:h-[40px] tw:shadow-none tw:bg-background tw:outline-primary tw:outline-2 tw:text-primary tw:font-bold', // standard
           'tw:hover:text-primary-darker tw:hover:outline-primary-darker', // hover
           'tw:focus-visible:ring-offset-[6px] tw:focus-visible:ring-[4px]', // focus-visible
           'tw:active:text-secondary-active tw:active:outline-secondary-active', // active
           'tw:invalid:text-secondary-invalid tw:invalid:outline-secondary-invalid', // inactive / invalid
         ),
         destructiveTertiary: cn(
-          'tw:bg-transparent tw:text-destructive tw:font-[400] tw:underline tw:underline-offset-4 tw:outline-none ', // standard,
+          'tw:bg-transparent tw:text-destructive tw:font-[400] tw:underline tw:underline-offset-4 tw:outline-none tw:font-normal ', // standard,
           'tw:hover:underline tw:hover:text-destructive-darker', // hover
           'tw:has-[>svg]:p-0 tw:focus-visible:ring-offset-[3px]', // focus-visible
           'tw:active:text-tertiary-active', // active
           'tw:invalid:text-tertiary-invalid', // invalid
         ),
         primaryTertiary: cn(
-          'tw:bg-transparent tw:text-primary tw:font-[400] tw:underline tw:underline-offset-4  tw:outline-none ', // standard
+          'tw:bg-transparent tw:text-primary tw:font-[400] tw:underline tw:underline-offset-4  tw:outline-none tw:font-normal', // standard
           'tw:hover:underline tw:hover:text-primary-darker', // hover,
           'tw:has-[>svg]:px-[4px] tw:has-[>svg]:py-[2px] h-[24px]', // focus-visible
           'tw:active:text-primary-active', // active
@@ -66,7 +66,8 @@ const buttonVariants = cva(
 export function Button({
   className,
   variant,
-  size,
+  iconHeight = 16,
+  iconWidth = 16,
   asChild = false,
   icon,
   children,
@@ -75,16 +76,19 @@ export function Button({
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean;
     icon?: IconProp;
+    iconHeight?: number;
+    iconWidth?: number;
   }) {
   const Comp = asChild ? Slot : 'button';
 
   return (
     <Comp
       data-slot="button"
-      className={cn(buttonVariants({ variant, size, className }))}
+      className={cn(buttonVariants({ variant, className }))
+      }
       {...props}
     >
-      {icon ? <FontAwesomeIcon className="mr-1" icon={icon} size="1x" /> : ''}
+      {icon ? <FontAwesomeIcon className={`mr-1 tw:!h-[${iconHeight}px] tw:!w-[${iconWidth}px]`} icon={icon} /> : ''}
       {children}
     </Comp>
   );
