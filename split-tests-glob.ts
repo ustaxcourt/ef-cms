2f67fb301a4cd9074eb4417a451bf812b6b00cfb
Russ Frith <rgf314159@gmail.com>
2025-09-04 13:57:07 -0800

    dependencies: update file list delimiter and updated jest rev lvls

--- split-tests-glob.ts
+++ split-tests-glob.ts
@@ -13,5 +13,5 @@ if (testType.includes('unit')) {
 const shuffled = shuffleSeed.shuffle(testFiles, process.env.GITHUB_SHA);
 const total = parseInt(process.env.CI_NODE_TOTAL!, 10);
 const index = parseInt(process.env.CI_NODE_INDEX!, 10);
-const tests = shuffled.filter((num, i) => i % total === index);
-console.log(tests.join(' '));
+const tests = shuffled.filter((_num, i) => i % total === index);
+console.log(tests.join('|'));
