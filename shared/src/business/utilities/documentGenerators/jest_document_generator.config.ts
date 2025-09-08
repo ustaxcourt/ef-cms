87730ea02b2ddd8f1e28781ffa97f4e28fdae00b
TomElliottFlexion <telliott@flexion.us>
2025-09-08 12:20:19 -0500

    dependencies: update jest config to read tsconfig json

--- shared/src/business/utilities/documentGenerators/jest_document_generator.config.ts
+++ shared/src/business/utilities/documentGenerators/jest_document_generator.config.ts
@@ -1,7 +1,14 @@
 /** @jest-config-loader ts-node */
 import { pathsToModuleNameMapper } from 'ts-jest';
-import tsconfig from '../../../../../tsconfig.json';
 import type { Config } from 'jest';
+import fs from 'node:fs';
+import path from 'node:path';
+
+const tsconfigPath = path.resolve(
+  process.cwd(),
+  '../../../../../tsconfig.json',
+);
+const tsconfig = JSON.parse(fs.readFileSync(tsconfigPath, 'utf8'));
 
 const config: Config = {
   clearMocks: true,
