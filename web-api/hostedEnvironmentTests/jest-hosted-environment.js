481054ea7492f50f7f89d8c1e920cd2b19aba280
Alex Dziarnowski <alexdziarn@gmail.com>
2025-09-10 15:23:26 -0400

    load tsconfig by reading file and parsing it into json

--- web-api/hostedEnvironmentTests/jest-hosted-environment.js
+++ web-api/hostedEnvironmentTests/jest-hosted-environment.js
@@ -1,6 +1,11 @@
 import { pathsToModuleNameMapper } from 'ts-jest';
-import tsconfig from '../../tsconfig.json';
+import fs from 'fs';
+import path from 'path';
 
+const tsconfigPath = path.join(process.cwd(), 'tsconfig.json');
+const tsconfig = JSON.parse(fs.readFileSync(tsconfigPath, 'utf8'));
+
+/** @type {import('jest').Config} */
 const config = {
   clearMocks: true,
   collectCoverage: false,
