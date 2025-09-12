a431587ef220c265e7d0a066121c3b11f031578f
Alex Dziarnowski <alexdziarn@gmail.com>
2025-09-12 14:02:09 -0400

    8937: fix lint errors, correctly colors column headers

--- web-client/src/presenter/computeds/termBuilderHelper.ts
+++ web-client/src/presenter/computeds/termBuilderHelper.ts
@@ -2,19 +2,25 @@ import { Get } from 'cerebral';
 import { state } from '@web-client/presenter/app.cerebral';
 import { STATE_KEYS } from '@shared/business/entities/EntityConstants';
 import {
+  createISODateString,
   formatDateString,
   FORMATS,
 } from '@shared/business/utilities/DateHandler';
-import { getHolidaysInDateRange } from '@shared/business/utilities/getHolidaysInDateRange';
 import { Holiday } from '@18f/us-federal-holidays';
+import { getHolidaysInDateRange } from '@shared/business/utilities/getHolidaysInDateRange';
 
 export const termBuilderHelper = (get: Get): any => {
-  const { termStartDate, termEndDate } =
-    get(state[STATE_KEYS.TERM_BUILDER_INFORMATION]) ?? {};
+  const termState = get(state[STATE_KEYS.TERM_BUILDER_INFORMATION]);
+
+  if (!termState) {
+    throw Error('Could not get term state');
+  }
+
+  const { termStartDate, termEndDate } = termState;
 
   const holidaysInDateRange = getHolidaysInDateRange(
-    termStartDate,
-    termEndDate,
+    createISODateString(termStartDate, FORMATS.MMDDYYYY),
+    createISODateString(termEndDate, FORMATS.MMDDYYYY),
   );
   const formattedHolidaysInDateRange = holidaysInDateRange.map(
     (holiday: Holiday) => {
