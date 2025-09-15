2142ec265445e1befe2518b251f6be46acf846d1
Kaitlyn Swann <kswann@flexion.us>
2025-09-15 12:29:16 -0600

    Dawson-Library: Remove Titles from Alerts

--- web-client/src/views/Public/DawsonLibrary/Alerts.tsx
+++ web-client/src/views/Public/DawsonLibrary/Alerts.tsx
@@ -53,7 +53,6 @@ export function Alerts() {
   return (
     <div className="tw:my-4">
       <h2>Alerts</h2>
-      Info Alert
       {alert.info ? (
         <AlertInfo {...alertInfoProps}></AlertInfo>
       ) : (
@@ -146,7 +145,6 @@ export function Alerts() {
           }
           className="tw:mb-4"
           alertWarning={{
-            title: 'Warning Status',
             message: 'You’ll need to change your password by April 25, 2020.',
           }}
         />
@@ -161,7 +159,6 @@ export function Alerts() {
       {staticAlerts.error ? (
         <AlertError
           alertError={{
-            ...alertError,
             message: 'Sorry, a password needs more than four characters.',
           }}
           alertHelper={alertHelper}
@@ -181,7 +178,6 @@ export function Alerts() {
       {staticAlerts.success ? (
         <AlertSuccess
           alertSuccess={{
-            title: 'Success Status',
             message: 'This is a succinct, helpful message',
           }}
           dismissAlertSequence={() =>
