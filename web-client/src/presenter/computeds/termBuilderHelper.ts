942efbc400ad1ec94ee53455a89e8e30d6077cd4
Alex Dziarnowski <alexdziarn@gmail.com>
2025-09-15 14:08:00 -0400

    8937: termbuilderhelper test

--- web-client/src/presenter/computeds/termBuilderHelper.ts
+++ web-client/src/presenter/computeds/termBuilderHelper.ts
@@ -30,5 +30,5 @@ export const termBuilderHelper = (get: Get): any => {
       };
     },
   );
-  return { holidaysInDateRange, formattedHolidaysInDateRange };
+  return { formattedHolidaysInDateRange };
 };
