8886be3127d576f32f922577cb3c2be97dab86ef
Cody Seibert <cseibert113@gmail.com>
2025-09-15 12:45:43 -0400

    10737: allow turning on maintence mode from lambda env

--- web-api/src/persistence/postgres/featureFlag/getMaintenanceMode.ts
+++ web-api/src/persistence/postgres/featureFlag/getMaintenanceMode.ts
@@ -3,15 +3,20 @@ import { getFeatureFlagValues } from '@web-api/persistence/postgres/featureFlag/
 export function getMaintenanceMode(): Promise<
   { current: boolean } | undefined
 > {
-  return getFeatureFlagValues(['maintenance-mode'])
-    .then(POSTGRES_RECORDS => {
-      if (!POSTGRES_RECORDS) return { current: false };
-      if (!POSTGRES_RECORDS.length) return { current: false };
-      const MAINTENANCE_RECORD = POSTGRES_RECORDS[0];
-      return MAINTENANCE_RECORD.value;
-    })
-    .catch(() => {
-      // if we can't connect to postgres, we assume maintence mode on due to critical issues
-      return { current: true };
-    });
+  if (process.env.MAINTENCE_MODE === 'true') {
+    return Promise.resolve({ current: true });
+  } else {
+    return getFeatureFlagValues(['maintenance-mode'])
+      .then(POSTGRES_RECORDS => {
+        if (!POSTGRES_RECORDS) return { current: false };
+        if (!POSTGRES_RECORDS.length) return { current: false };
+        const MAINTENANCE_RECORD = POSTGRES_RECORDS[0];
+        return MAINTENANCE_RECORD.value;
+      })
+      .catch(() => {
+        // if we can't connect to postgres, we assume maintence mode on due to critical issues
+        return { current: true };
+      });
+  }
+
 }
