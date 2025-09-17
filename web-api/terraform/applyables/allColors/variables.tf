b6e001fd73fb87a4eaeb78f6af80f7feb487181e
Jake Roberts <jdr81394@gmail.com>
2025-09-17 16:05:47 -0400

    9035: Made open search versions dynamic for terraform and kibana

--- web-api/terraform/applyables/allColors/variables.tf
+++ web-api/terraform/applyables/allColors/variables.tf
@@ -87,3 +87,7 @@ variable "restoring_aws_account_id" {
 variable "rum_sample_rate" {
   type = number
 }
+
+variable "es_engine_version" {
+  type = string
+}
--- web-api/terraform/bin/deploy-account-specific.sh
+++ web-api/terraform/bin/deploy-account-specific.sh
@@ -63,6 +63,7 @@ fi
 export TF_VAR_dawson_dev_trusted_role_arns="${DAWSON_DEV_TRUSTED_ROLE_ARNS}"
 export TF_VAR_log_snapshot_bucket_name="${LOG_SNAPSHOT_BUCKET_NAME}"
 export TF_VAR_lower_env_restore_roles="[\"arn:aws:iam::${LOWER_ENV_ACCOUNT_IDS//,/:role/restore_role_*\",\"arn:aws:iam::}:role/restore_role_*\"]"
+export TF_VAR_es_logs_engine_version="$ES_LOGS_ENGINE_VERSION"
 
 npm run build:assets
 
