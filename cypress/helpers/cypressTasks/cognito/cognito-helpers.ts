04d8286114db934868d19c1cfd2d2f10fb00b5f3
Cody Seibert <cseibert113@gmail.com>
2025-09-18 13:33:56 -0400

    refactor import to fix crypto import issue

--- cypress/helpers/cypressTasks/cognito/cognito-helpers.ts
+++ cypress/helpers/cypressTasks/cognito/cognito-helpers.ts
@@ -7,8 +7,6 @@ import { getCognito } from './getCognitoCypress';
 import { getCypressEnv } from '../../env/cypressEnvironment';
 import { deleteAllUserRecords } from '../postgres/postgres-helpers';
 
-export const DEFAULT_FORGOT_PASSWORD_CODE = '385030';
-
 export const confirmUser = async ({ email }: { email: string }) => {
   const userPoolId = await getUserPoolId();
   const clientId = await getClientId(userPoolId);
