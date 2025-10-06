6c6d4f63477f73b3ce2e4c0858b7150989270cd6
Favour Ori <favour.ori.ctr@ustaxcourt.gov>
2025-10-06 11:51:28 -0500

    files setup

---
+++ web-client/src/views/Public/DawsonLibrary/Inputs.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+//import { Input } from '@web-client/dawson-ui/ui/input';
+
+export function Inputs() {
+  return (
+    <div className="tw:my-4">
+      <h2>Text Input </h2>
+
+    
+
+    </div>
+  );
+}
