678ab16b10e7ad348ffc722481e2a0eb0e2f1305
Favour Ori <favour.ori.ctr@ustaxcourt.gov>
2025-10-07 14:42:32 -0500

    updated textfield width for desktop & mobile

--- web-client/src/views/Public/DawsonLibrary/Inputs.tsx
+++ web-client/src/views/Public/DawsonLibrary/Inputs.tsx
@@ -14,6 +14,7 @@ export function Inputs() {
             label="Field Label"
             helpText="Help text"
             placeholder="Enter text here"
+            className="tw:w-[380px]"
           />
         </div>
 
@@ -60,6 +61,7 @@ export function Inputs() {
             label="Field Label"
             helpText="Help text"
             placeholder="Enter text here"
+            className="tw:w-[380px]"
           />
         </div>
 
