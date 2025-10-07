3548fe6f95b8c3f6eae24123fac3c163b0a9d931
Alex Dziarnowski <alexdziarn@gmail.com>
2025-10-07 16:57:01 -0400

    dawson-library: remove dismiss button from alert in login screen

--- web-client/src/dawson-ui/ui/Alert/AlertError.tsx
+++ web-client/src/dawson-ui/ui/Alert/AlertError.tsx
@@ -26,6 +26,7 @@ type AlertErrorProps = {
   alertHelper?: AlertHelper;
   className?: string;
   closeButtonOnClick?: () => void;
+  isDismissible?: boolean;
 };
 
 export function AlertError({
@@ -33,6 +34,7 @@ export function AlertError({
   alertHelper,
   className,
   closeButtonOnClick,
+  isDismissible,
 }: AlertErrorProps) {
   const notificationRef = useRef<HTMLDivElement | null>(null);
 
@@ -47,6 +49,7 @@ export function AlertError({
     closeButtonOnClick,
     title: alertError?.title,
     variant: 'error',
+    isDismissible,
   };
 
   return (
