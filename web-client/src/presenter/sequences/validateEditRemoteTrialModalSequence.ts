c11e6495b693fc1446123959bb3042c89409961f
Paulo Da Rocha Filho <paulo.filho.ctr@gmail.com>
2025-10-09 11:33:44 -0500

    9013: add validation to edit remote status modal

--- web-client/src/presenter/sequences/validateEditRemoteTrialModalSequence.ts
+++ web-client/src/presenter/sequences/validateEditRemoteTrialModalSequence.ts
@@ -2,10 +2,8 @@ import { clearAlertsAction } from '../actions/clearAlertsAction';
 import { setValidationErrorsAction } from '../actions/setValidationErrorsAction';
 import { shouldValidateAction } from '../actions/shouldValidateAction';
 import { validateRemoteTrialPermissionAction } from '../actions/CaseDetail/validateRemoteTrialPermissionAction';
-import { startShowValidationAction } from '@web-client/presenter/actions/startShowValidationAction';
 
 export const validateEditRemoteTrialModalSequence = [
-  startShowValidationAction,
   shouldValidateAction,
   {
     ignore: [],
