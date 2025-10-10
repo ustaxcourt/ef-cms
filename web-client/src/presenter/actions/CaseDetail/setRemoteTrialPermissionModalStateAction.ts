3b201a537fcdd677f5325deac0cc70fe785dc093
Paulo Da Rocha Filho <paulo.filho.ctr@gmail.com>
2025-10-10 11:10:27 -0500

    8576: edit remote status modal persistence wired

--- web-client/src/presenter/actions/CaseDetail/setRemoteTrialPermissionModalStateAction.ts
+++ web-client/src/presenter/actions/CaseDetail/setRemoteTrialPermissionModalStateAction.ts
@@ -3,13 +3,15 @@ import { state } from '@web-client/presenter/app.cerebral';
 /**
  * Sets the modal state for editing remote trial permission from props
  * @param {object} providers the providers object
- * @param {object} providers.props the cerebral props
+ * @param {object} providers.get the cerebral get function
  * @param {object} providers.store the cerebral store
  */
-export const setRemoteTrialPermissionModalStateAction = ({ props, store }) => {
+export const setRemoteTrialPermissionModalStateAction = ({ get, store }) => {
   // Only set the date - the granted status is derived from whether there's a date
+  const caseDetail = get(state.caseDetail);
+
   store.set(
     state.modal.remoteTrialGrantedDate,
-    props.remoteTrialGrantedDate || '',
+    caseDetail.remoteTrialGrantedDate || '',
   );
 };
