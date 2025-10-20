835d712d769dbbb575585be973661f9bb35b17b8
Jim Lerza <james.lerza@ustaxcourt.gov>
2025-10-20 17:02:47 -0400

    opex: updated cases-closed-in-year report to include the number of days each case was open

--- scripts/reports/cases-closed-in-year.ts
+++ scripts/reports/cases-closed-in-year.ts
@@ -4,21 +4,21 @@ import { CASE_STATUS_TYPES } from '@shared/business/entities/EntityConstants';
 import { DateTime } from 'luxon';
 import {
   type ScriptConfig,
+  getTimeframeForYear,
   parseArgsAndEnvVars,
 } from '../helpers/parseArgsAndEnvVars';
+import { calculateDifferenceInDays } from '@shared/business/utilities/DateHandler';
 import { fromKyselyCase } from '@web-api/persistence/postgres/cases/mapper';
 import { generateCsv } from '../helpers/generate-csv';
 import { getDbReader } from '@web-api/database';
 import { pick } from 'lodash';
 import { sql } from 'kysely';
-import { validateDateAndCreateISO } from '@shared/business/utilities/DateHandler';
 
 const scriptConfig: ScriptConfig = {
   description:
     'cases-closed-in-year - Generates a spreadsheet of cases closed at any ' +
     'point in the given year, even if they were later reopened.',
   environment: {
-    elasticsearchEndpoint: 'ELASTICSEARCH_ENDPOINT',
     env: 'ENV',
   },
   parameters: {
@@ -39,36 +39,55 @@ const { fiscal, year } = parseArgsAndEnvVars(scriptConfig) as {
   fiscal: boolean;
   year: string;
 };
+const { begin, end } = getTimeframeForYear({ fiscal, year });
 
 const OUTPUT_DIR = `${process.env.HOME}/Documents`;
 const CLOSED_STATUSES: string[] = [
   CASE_STATUS_TYPES.closed,
   CASE_STATUS_TYPES.closedDismissed,
 ];
-const BEGIN = validateDateAndCreateISO({
-  day: '1',
-  month: fiscal ? '10' : '1',
-  year: fiscal ? `${Number(year) - 1}` : year,
-})!;
-const END = validateDateAndCreateISO({
-  day: '1',
-  month: fiscal ? '10' : '1',
-  year: fiscal ? year : `${Number(year) + 1}`,
-})!;
 
-const getAllCasesClosedInYear = async (): Promise<RawCase[]> => {
-  const allResults = (await getDbReader(reader =>
+const getClosedDateFromCaseStatusHistory = async (): Promise<RawCase[]> => {
+  return (await getDbReader(reader =>
     reader
       .selectFrom('dwCase')
       .crossJoin(
         sql`LATERAL jsonb_array_elements(case_status_history)`.as('csh'),
       )
       .select(sql`dw_case.*`.as('dwCase'))
-      .where(sql`csh->>'updatedCaseStatus'`, 'in', CLOSED_STATUSES)
-      .where(sql`(csh->>'date')::date`, '>=', BEGIN)
-      .where(sql`(csh->>'date')::date`, '<', END)
+      .where(eb =>
+        eb.or([
+          eb.and([
+            eb('closedDate', '>=', DateTime.fromISO(begin).toJSDate()),
+            eb('closedDate', '<', DateTime.fromISO(end).toJSDate()),
+          ]),
+          eb.and([
+            eb(sql`csh->>'updatedCaseStatus'`, 'in', CLOSED_STATUSES),
+            eb(sql`(csh->>'date')::date`, '>=', begin),
+            eb(sql`(csh->>'date')::date`, '<', end),
+          ]),
+        ]),
+      )
       .execute(),
   )) as unknown as RawCase[];
+};
+
+const getClosedDateFromCaseRecord = async (): Promise<RawCase[]> => {
+  return (await getDbReader(reader =>
+    reader
+      .selectFrom('dwCase as c')
+      .selectAll('c')
+      .where('c.closedDate', '>=', DateTime.fromISO(begin).toJSDate())
+      .where('c.closedDate', '<', DateTime.fromISO(end).toJSDate())
+      .execute(),
+  )) as unknown as RawCase[];
+};
+
+const getAllCasesClosedInYear = async (): Promise<RawCase[]> => {
+  const allResults =
+    Number(year) < 2024
+      ? await getClosedDateFromCaseRecord()
+      : await getClosedDateFromCaseStatusHistory();
   const results = [
     ...new Map(allResults.map(c => [c.docketNumber, c])).values(),
   ].map(fromKyselyCase);
@@ -78,6 +97,34 @@ const getAllCasesClosedInYear = async (): Promise<RawCase[]> => {
       `that was generated in ${fiscal ? 'fiscal' : 'calendar'} year ${year}`,
   );
   return results;
+};
+
+const petitionServiceDates = new Map<string, string>();
+
+const getPetitionServiceDates = async ({
+  docketNumbers,
+}: {
+  docketNumbers: string[];
+}): Promise<void> => {
+  const results = (await getDbReader(reader =>
+    reader
+      .selectFrom('dwDocketEntry as de')
+      .leftJoin('dwCase as c', 'de.docketNumber', 'c.docketNumber')
+      .select(['de.docketNumber', 'de.servedAt'])
+      .where('de.eventCode', '=', 'P')
+      .where('de.docketNumber', 'in', docketNumbers)
+      .orderBy('de.receivedAt', 'asc')
+      .execute(),
+  )) as { docketNumber: string; servedAt: Date | null }[];
+  for (const r of results) {
+    if (Object.prototype.toString.call(r.servedAt) === '[object Date]') {
+      const petitionServiceDateISO =
+        DateTime.fromJSDate(r.servedAt!).toISO() || '';
+      if (petitionServiceDateISO.length > 0) {
+        petitionServiceDates.set(r.docketNumber, petitionServiceDateISO);
+      }
+    }
+  }
 };
 
 const outputCsv = ({
@@ -91,32 +138,54 @@ const outputCsv = ({
     { header: 'Case Title', key: 'caption' },
     { header: 'Judge', key: 'judge' },
     { header: 'Case Status', key: 'status' },
+    { header: 'Petition Service Date', key: 'petitionServedDateHumanized' },
     { header: 'Closed Date', key: 'closedDateHumanized' },
+    { header: 'Number of Days Open', key: 'daysOpen' },
   ];
+  const totals: { [k: string]: number } = { cases: 0, daysOpen: 0 };
   const rows = casesClosedInYear.map(c => {
     const judge =
       c.associatedJudge
         ?.replace('Chief Special Trial ', '')
         .replace('Special Trial ', '')
         .replace('Judge ', '') || '';
-    const closedDateHumanized =
+    const closedDateISO =
       (c.caseStatusHistory || [])
         .reverse()
         .find(
           csh =>
             CLOSED_STATUSES.includes(csh.updatedCaseStatus) &&
-            csh.date >= BEGIN &&
-            csh.date < END,
-        )
-        ?.date.split('T')[0] || '';
+            csh.date >= begin &&
+            csh.date < end,
+        )?.date ||
+      c.closedDate ||
+      '';
+    const closedDateHumanized = closedDateISO.split('T')[0] || '';
     const caption = c.caseCaption.replace(/\r\n|\r|\n/g, ' ').trim();
+    const petitionServedDateISO = petitionServiceDates.get(c.docketNumber)!;
+    const petitionServedDateHumanized =
+      petitionServedDateISO?.split('T')[0] || '';
+    const daysOpen = calculateDifferenceInDays(
+      closedDateISO,
+      petitionServedDateISO,
+    );
+    if (daysOpen > 0) {
+      totals.cases += 1;
+      totals.daysOpen += daysOpen;
+    }
     return {
       ...pick(c, ['docketNumber', 'status']),
       caption,
       closedDateHumanized,
+      daysOpen,
       judge,
+      petitionServedDateHumanized,
     };
   });
+  const averageAge = Math.round(totals.daysOpen / totals.cases);
+  console.log(
+    `Cases closed in ${fiscal ? 'fiscal' : 'calendar'} year ${year} were open for an average of ${averageAge} days`,
+  );
   generateCsv({ columns, filename, rows });
   console.log(`Generated ${filename}`);
 };
@@ -124,5 +193,8 @@ const outputCsv = ({
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
   const casesClosedInYear = await getAllCasesClosedInYear();
+  await getPetitionServiceDates({
+    docketNumbers: casesClosedInYear.map(c => c.docketNumber),
+  });
   outputCsv({ casesClosedInYear });
 })();
