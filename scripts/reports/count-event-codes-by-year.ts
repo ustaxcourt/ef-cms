3ac21a52744648aa368712045cd8139a3592b23a
Jim Lerza <james.lerza@ustaxcourt.gov>
2025-10-23 11:24:23 -0400

    opex: converted count-event-codes-by-year to use postgres instead of opensearch

--- scripts/reports/count-event-codes-by-year.ts
+++ scripts/reports/count-event-codes-by-year.ts
@@ -3,21 +3,16 @@
 import { DateTime } from 'luxon';
 import {
   type ScriptConfig,
+  getJsTimeframeForYear,
   parseArgsAndEnvVars,
 } from '../helpers/parseArgsAndEnvVars';
-import {
-  type ServerApplicationContext,
-  createApplicationContext,
-} from '@web-api/applicationContext';
-import { count } from '@web-api/persistence/elasticsearch/searchClient';
-import { validateDateAndCreateISO } from '@shared/business/utilities/DateHandler';
+import { getDbReader } from '@web-api/database';
 
 const scriptConfig: ScriptConfig = {
   description:
     'count-event-codes-by-year - Count instances of documents with the ' +
     'given event code(s) filed within the given duration.',
   environment: {
-    elasticsearchEndpoint: 'ELASTICSEARCH_ENDPOINT',
     env: 'ENV',
   },
   parameters: {
@@ -48,112 +43,74 @@ const scriptConfig: ScriptConfig = {
   },
   requireActiveAwsSession: true,
 };
+const { eventCodes, fiscal, stricken, years } = parseArgsAndEnvVars(
+  scriptConfig,
+) as {
+  eventCodes: string[];
+  fiscal: boolean;
+  stricken: boolean;
+  years: number[];
+};
 
 const getCountDocketEntriesByEventCodesAndYears = async ({
-  applicationContext,
   eventCodes,
   fiscal,
   onlyNonStricken,
   years,
 }: {
-  applicationContext: ServerApplicationContext;
   eventCodes: string[];
   fiscal: boolean;
   onlyNonStricken: boolean;
   years?: number[];
 }): Promise<number> => {
-  const must: {}[] = [
-    {
-      bool: {
-        should: eventCodes.map(eventCode => ({
-          term: {
-            'eventCode.S': eventCode,
-          },
-        })),
-      },
-    },
-  ];
-  if (onlyNonStricken) {
-    must.push({
-      term: {
-        'isStricken.BOOL': false,
-      },
-    });
-  }
-  if (years && years.length) {
-    if (years.length === 1) {
-      must.push({
-        range: {
-          'receivedAt.S': {
-            gte: validateDateAndCreateISO({
-              day: '1',
-              month: fiscal ? '10' : '1',
-              year: fiscal ? `${years[0] - 1}` : `${years[0]}`,
-            }),
-            lt: validateDateAndCreateISO({
-              day: '1',
-              month: fiscal ? '10' : '1',
-              year: fiscal ? `${years[0]}` : `${years[0] + 1}`,
-            }),
-          },
-        },
-      });
-    } else {
-      must.push({
-        bool: {
-          should: years.map(year => ({
-            range: {
-              'receivedAt.S': {
-                gte: validateDateAndCreateISO({
-                  day: '1',
-                  month: fiscal ? '10' : '1',
-                  year: fiscal ? `${year - 1}` : `${year}`,
-                }),
-                lt: validateDateAndCreateISO({
-                  day: '1',
-                  month: fiscal ? '10' : '1',
-                  year: fiscal ? `${year}` : `${year + 1}`,
-                }),
-              },
-            },
-          })),
-        },
-      });
+  const results: { count: number } = await getDbReader(async reader => {
+    let query = reader
+      .selectFrom('dwDocketEntry')
+      .where('eventCode', 'in', eventCodes);
+    if (onlyNonStricken) {
+      query = query.where('isStricken', '!=', true);
     }
-  }
-  const searchParameters = {
-    body: {
-      query: {
-        bool: {
-          must,
-        },
-      },
-    },
-    index: 'efcms-docket-entry',
-  };
-
-  return await count({
-    applicationContext,
-    searchParameters,
+    if (years && years.length) {
+      if (years.length === 1) {
+        const { begin, end } = getJsTimeframeForYear({
+          fiscal,
+          year: `${years[0]}`,
+        });
+        query = query
+          .where('receivedAt', '>=', begin)
+          .where('receivedAt', '<', end);
+      } else {
+        query = query.where(qb =>
+          qb.or(
+            years.map(year => {
+              const { begin, end } = getJsTimeframeForYear({
+                fiscal,
+                year: `${year}`,
+              });
+              return qb.and([
+                qb('receivedAt', '>=', begin),
+                qb('receivedAt', '<', end),
+              ]);
+            }),
+          ),
+        );
+      }
+    }
+    query = query.select(reader.fn.countAll().as('count'));
+    return (await query.executeTakeFirst()) as { count: number };
   });
+  return results?.count || 0;
 };
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
-  const { eventCodes, fiscal, stricken, years } = parseArgsAndEnvVars(
-    scriptConfig,
-  ) as {
-    eventCodes: string[];
-    fiscal: boolean;
-    stricken: boolean;
-    years: number[];
-  };
   const ret = await getCountDocketEntriesByEventCodesAndYears({
-    applicationContext: createApplicationContext({}),
     eventCodes,
     fiscal,
     onlyNonStricken: !stricken,
     years,
   });
-  console.log(ret);
+  console.log(
+    `Found ${ret} ${eventCodes.join(',')} documents filed in ${fiscal ? 'fy' : ''} ${years.join(',')}.`,
+  );
 })();
