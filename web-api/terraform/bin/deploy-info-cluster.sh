79c6f98ed7e400742dca7a0f5743df13cba6e376
Paulo Da Rocha Filho <paulo.filho.ctr@gmail.com>
2025-11-03 13:57:21 -0600

    9284: add deploy info cluster function

--- web-api/terraform/bin/deploy-info-cluster.sh
+++ web-api/terraform/bin/deploy-info-cluster.sh
@@ -30,5 +30,60 @@ deploy_primary() {
   local domain="$2"
   log_info "Switching to primary environment: ${env}"
 
-  . ./scripts/env/set-env.zsh "${env}" || log_error "Failed to switch environments: ${env}"; exit 1
+  . ./scripts/env/set-env.zsh "${env}" || {
+    log_error "Failed to switch environment: ${env}"
+    exit 1
+    }
+
+  log_info "Writing secrets to primary environment: ${env}"
+  npx ts-node scripts/secrets/create-account-secrets.ts \
+  --env "${env}" \
+  --domain "${domain}" \
+  --update || {
+     log_error "Failed to write secrets for environment: ${env}"
+     exit 1
+   }
+
+  log_info "Deploying primary environment: ${env}"
+  pushd web-api/terraform/applyables/account-specific/account-specific >/dev/null || {
+   log_error "Failed to change directory for Terraform applyables: ${env}"
+   exit 1
+  }
+
+   ../..//bin/deploy-account-specific.sh || {
+    log_error "Failed to deploy primary environment: ${env}"
+    popd >/dev/null
+    exit 1
+  }
+
+  log_info "Capturing info cluster endpoint and arn from Terraform"
+
+  local endpoint
+  local arn
+
+  endpoint=$(terraform output -raw es_info_cluster_shared_cluster_endpoint 2>/dev/null) || {
+    log_error "Failed to get info cluster shared endpoint from Terraform: ${env}"
+    popd >/dev/null
+    exit 1
+  }
+
+  arn=$(terraform output -raw es_info_cluster_shared_cluster_arn 2>/dev/null) || {
+    log_error "Failed to get info cluster shared arn from Terraform: ${env}"
+    popd >/dev/null
+    exit 1
+  }
+
+  popd >/dev/null
+  if [ -z "${endpoint}" ] || [ -z "${arn}" ]; then
+      log_error "Terraform outputs are empty. Endpoint: '${endpoint}' Arn: '${arn}'"
+      exit 1
+  fi
+  log_success "Primary deployment complete."
+  log_info "Cluster endpoint: ${endpoint}"
+  log_info "Cluster arn: ${arn}"
+
+  PRIMARY_ENDPOINT="${endpoint}"
+  PRIMARY_ARN="${arn}"
 }
+
+deploy_primary "${PRIMARY_ENV}" "${BASE_DOMAIN}"
