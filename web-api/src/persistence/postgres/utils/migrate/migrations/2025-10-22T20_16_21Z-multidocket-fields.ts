3aeb22fac558575460002d276780e787493f16b9
Abel <Abel.Ratanaphan.ctr@ustaxcourt.gov>
2025-11-05 13:32:09 -0500

    8477: update seed data
    Co-authored-by: Christie Laferriere <christie.laferriere.ctr@ustaxcourt.gov>

--- web-api/src/persistence/postgres/utils/migrate/migrations/2025-10-22T20_16_21Z-multidocket-fields.ts
+++ web-api/src/persistence/postgres/utils/migrate/migrations/2025-10-22T20_16_21Z-multidocket-fields.ts
@@ -3,7 +3,9 @@ import { Kysely } from 'kysely';
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .alterTable('dwDocketEntry')
-    .addColumn('multiDocketedOn', 'jsonb', col => col.defaultTo([]).notNull())
+    .addColumn('multiDocketedOn', 'jsonb', col =>
+      col.defaultTo(JSON.stringify([])).notNull(),
+    )
     .execute();
 
   await db.schema
