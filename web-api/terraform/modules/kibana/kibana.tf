70579b601cf98018eadf03b798a9fc36a140904f
Paulo Da Rocha Filho <paulo.filho.ctr@gmail.com>
2025-11-06 14:30:09 -0600

    9284: add cross account variables

--- web-api/terraform/modules/kibana/kibana.tf
+++ web-api/terraform/modules/kibana/kibana.tf
@@ -113,7 +113,7 @@ resource "aws_elasticsearch_domain_policy" "kibana_access" {
     }, {
       "Effect":"Allow",
       "Principal": {
-        "AWS": ["${aws_iam_role.lambda_elasticsearch_execution_role.arn}"]
+        "AWS": ["${jsonencode(local.all_lambda_arn)}"]
       },
       "Action": "es:ESHttp*",
       "Resource":"${aws_opensearch_domain.efcms-logs[0].arn}/*"
@@ -219,6 +219,15 @@ locals {
   instance_size_in_mb = var.es_info_cluster_create ? aws_opensearch_domain.efcms-logs.ebs_options[0].volume_size * 1000 : 0
   info_cluster_arn = var.es_info_cluster_create ? aws_opensearch_domain.efcms-logs[0].arn : var.es_info_cluster_shared_cluster_arn
   info_cluster_endpoint = var.es_info_cluster_create ? aws_opensearch_domain.efcms-logs[0].endpoint : var.es_info_cluster_shared_cluster_endpoint
+  info_cluster_shared_cluster_consumers = [
+    for consumer_ids in var.es_info_cluster_shared_cluster_account_ids:
+    "arn:aws:iam::${consumer_ids}:role.lambda_elasticsearch_execution_role"
+  ]
+  all_lambda_arn = concat([
+    aws_iam_role.lambda_elasticsearch_execution_role.arn
+  ],
+    local.info_cluster_shared_cluster_consumers
+  )
 }
 
 module "logs_alarms" {
