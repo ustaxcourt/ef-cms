52c7589e637309796bc4d04913b38de909d3a60a
Anthony <Anthony.Loera.ctr@ustaxcourt.gov>
2025-11-12 10:29:46 -0800

    8783: updated file to handle null in type

--- web-client/src/presenter/actions/completeDocumentSigningAction.ts
+++ web-client/src/presenter/actions/completeDocumentSigningAction.ts
@@ -1,12 +1,5 @@
 import { state } from '@web-client/presenter/app.cerebral';
 
-type PdfjsObj = { getData: () => Promise<unknown> };
-type PdfjsSource =
-  | PdfjsObj
-  | (() => PdfjsObj | null | undefined)
-  | null
-  | undefined;
-
 /**
  * generates an action for completing document signing
  * @param {object} providers the providers object
@@ -30,21 +23,15 @@ export const completeDocumentSigningAction = async ({
       state.pdfForSigning,
     );
     const { scale, x, y } = signatureData;
-    const pdfjsSource: PdfjsSource =
-      (
-        globalThis as typeof globalThis & {
-          pdfjsObj?: PdfjsSource;
-        }
-      ).pdfjsObj ?? get(state.pdfForSigning.pdfjsObj);
+    const windowWithPdfjs = window as Window & {
+      pdfjsObj?: { getData: () => Promise<unknown> };
+    };
+    const pdfjsObj: { getData: () => Promise<unknown> } | null =
+      windowWithPdfjs.pdfjsObj || get(state.pdfForSigning.pdfjsObj);
 
-    const pdfjsObjCandidate =
-      typeof pdfjsSource === 'function' ? pdfjsSource() : pdfjsSource;
-
-    if (!pdfjsObjCandidate) {
+    if (!pdfjsObj) {
       throw new Error('pdfjsObj is required to complete document signing.');
     }
-
-    const pdfjsObj = pdfjsObjCandidate;
 
     // generate signed document to bytes
     const signedPdfBytes = await applicationContext
