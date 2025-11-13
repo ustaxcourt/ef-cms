d45586edd21125924022e1a7b98a8330cde34d74
Paulo Da Rocha Filho <paulo.filho.ctr@gmail.com>
2025-11-13 13:21:15 -0600

    9284: update circleCI permissions

--- web-api/terraform/modules/edge-lambda-permissions/edge-lambda-permissions.tf
+++ web-api/terraform/modules/edge-lambda-permissions/edge-lambda-permissions.tf
@@ -1,43 +1,7 @@
 resource "aws_iam_service_linked_role" "lambda_replication_role" {
   aws_service_name = "replicator.lambda.amazonaws.com"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": [
-          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/dawson_dev",
-          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/CircleCI",
-        ]
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
 }
 
 resource "aws_iam_service_linked_role" "lambda_cloudfront_logger_role" {
   aws_service_name = "logger.cloudfront.amazonaws.com"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": [
-          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/dawson_dev",
-          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/CircleCI",
-        ]
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
 }
