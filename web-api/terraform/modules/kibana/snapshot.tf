3345447a4381fdc00b6d8d594d2956bbda024c5e
Paulo Da Rocha Filho <paulo.filho.ctr@gmail.com>
2025-11-14 13:31:38 -0600

    9284: clean up

--- web-api/terraform/modules/kibana/snapshot.tf
+++ web-api/terraform/modules/kibana/snapshot.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 provider "opensearch" {
-  url = "https://${aws_opensearch_domain.efcms-logs[0].endpoint}"
+  url = var.es_info_cluster_create ? "https://${aws_opensearch_domain.efcms-logs[0].endpoint}" : "https://${jsondecode(data.aws_secretsmanager_secret_version.info_cluster[0].secret_string)["endpoint"]}"
 }
 
 resource "opensearch_snapshot_repository" "archived-logs" {
