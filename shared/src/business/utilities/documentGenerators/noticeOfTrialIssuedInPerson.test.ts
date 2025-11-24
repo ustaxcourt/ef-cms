f131fd27e43d1c06c58a371b55cced9344afb5c2
Devin Ganey <dganey@gunnison.biz>
2025-11-24 14:34:15 -0500

    8782: Fixed type error in noticeOfTrialIssuedInPerson test

--- shared/src/business/utilities/documentGenerators/noticeOfTrialIssuedInPerson.test.ts
+++ shared/src/business/utilities/documentGenerators/noticeOfTrialIssuedInPerson.test.ts
@@ -1,3 +1,9 @@
+import {
+  SESSION_STATUS_TYPES,
+  SESSION_TYPES,
+  TRIAL_SESSION_PROCEEDING_TYPES,
+  TRIAL_SESSION_SCOPE_TYPES,
+} from '@shared/business/entities/EntityConstants';
 import { applicationContext } from '../../test/createTestApplicationContext';
 import { generateAndVerifyPdfDiff } from './generateAndVerifyPdfDiff';
 import { noticeOfTrialIssuedInPerson } from './noticeOfTrialIssuedInPerson';
@@ -26,6 +32,18 @@ describe('noticeOfTrialIssuedInPerson', () => {
             postalCode: 'Boise, Idaho',
             state: '33333',
             trialLocation: 'Birmingham, Alabama',
+            caseOrder: [],
+            isCalendared: true,
+            proceedingType: TRIAL_SESSION_PROCEEDING_TYPES.inPerson,
+            sessionScope: TRIAL_SESSION_SCOPE_TYPES.locationBased,
+            sessionStatus: SESSION_STATUS_TYPES.new,
+            sessionType: SESSION_TYPES.regular,
+            startDate: '2001-01-01T00:00:00Z',
+            term: 'Winter',
+            termYear: '2001',
+            trialSessionId: '111-111-112',
+            paperServicePdfs: [],
+            hasNottBeenServed: false
           },
         },
       }),
