3144f8301e05012cb3eaa12e8f53e725d35f40b9
Alex Dziarnowski <alexdziarn@gmail.com>
2025-12-01 17:20:32 -0500

    8994: all auto generated pdf files are routed one place

---
+++ web-client/src/presenter/actions/FileDocument/autoGenerateFilingPdfAction.ts
@@ -0,0 +1,50 @@
+import { getCaseCaptionMeta } from '@shared/business/utilities/getCaseCaptionMeta';
+import { state } from '@web-client/presenter/app.cerebral';
+
+export const autoGenerateFilingPdfAction = async ({
+  applicationContext,
+  get,
+}: ActionProps) => {
+  const { GENERATION_TYPES } = applicationContext.getConstants();
+
+  const { petitioners } = get(state.caseDetail);
+
+  const { generationType, eventCode } = get(state.form);
+
+  if (generationType === GENERATION_TYPES.AUTO) {
+    const caseDetail = get(state.caseDetail);
+    const { caseCaptionExtension, caseTitle } = getCaseCaptionMeta(caseDetail);
+
+    const { docketNumberWithSuffix } = caseDetail;
+
+    const { filers } = get(state.form);
+
+    let response;
+    switch (eventCode) {
+      case 'EA':
+        response = await applicationContext
+          .getUseCases()
+          .generateEntryOfAppearancePdfInteractor(applicationContext, {
+            caseCaptionExtension,
+            caseTitle,
+            docketNumberWithSuffix,
+            filers,
+            petitioners,
+          });
+        break;
+      case 'NOTW':
+        response = await applicationContext
+          .getUseCases()
+          .generateNoticeOfWithdrawalPdfInteractor(applicationContext, {
+            caseCaptionExtension,
+            caseTitle,
+            docketNumberWithSuffix,
+            filers,
+            petitioners,
+          });
+        break;
+    }
+
+    return { pdfUrl: response.url };
+  }
+};
