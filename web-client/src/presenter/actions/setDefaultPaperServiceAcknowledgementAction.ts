a13c8bf41a9738a723b36a2972feb9d080b16afa
Alex Dziarnowski <alexdziarn@gmail.com>
2025-12-02 16:54:51 -0500

    8994: sets the paperServiceAcknowledgement field if there are paper petitioners

--- web-client/src/presenter/actions/setDefaultPaperServiceAcknowledgementAction.ts
+++ web-client/src/presenter/actions/setDefaultPaperServiceAcknowledgementAction.ts
@@ -1,7 +1,21 @@
 import { state } from '../app.cerebral';
+import { getPartiesToWithrawFrom } from './validateNoticeOfWithdrawalAction';
 
 export const setDefaultPaperServiceAcknowledgementAction = ({
+  get,
   store,
 }: ActionProps) => {
-  store.set(state.form.paperServiceAcknowledgement, false);
+  const caseDetail = get(state.caseDetail);
+  const partiesToWithrawFrom = getPartiesToWithrawFrom(caseDetail);
+  const hasPartyWithPaperService = partiesToWithrawFrom.some(partyContactId => {
+    const petitioner = caseDetail.petitioners.find(
+      petitioner => petitioner.contactId === partyContactId,
+    );
+    return petitioner?.serviceIndicator === 'Paper';
+  });
+  if (hasPartyWithPaperService) {
+    store.set(state.form.paperServiceAcknowledgement, false);
+  } else {
+    store.unset(state.form.paperServiceAcknowledgement);
+  }
 };
