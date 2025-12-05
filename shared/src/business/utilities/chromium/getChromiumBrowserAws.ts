92ac0c68a13e2f86dec271a32f637a46c6f45166
Devin Ganey <dganey@gunnison.biz>
2025-12-05 09:17:23 -0500

    Removed prefixes from puppeteer imports. Added logging for starting browser

--- shared/src/business/utilities/chromium/getChromiumBrowserAws.ts
+++ shared/src/business/utilities/chromium/getChromiumBrowserAws.ts
@@ -17,6 +17,7 @@ export const getChromiumBrowserAWS = async (): Promise<Browser> => {
           args: chromium.args,
           headless: 'shell',
         }),
+        dumpio: true,
         executablePath: await chromium.executablePath(),
         headless: 'shell',
         env: {
