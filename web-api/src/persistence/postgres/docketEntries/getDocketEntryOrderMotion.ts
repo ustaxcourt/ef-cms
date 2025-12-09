c5a6efe8864b3b1d539334d463a3550604523b1d
Cody Seibert <cseibert113@gmail.com>
2025-12-09 14:02:01 -0500

    10705 - fixing two other places using wrong import

--- web-api/src/persistence/postgres/docketEntries/getDocketEntryOrderMotion.ts
+++ web-api/src/persistence/postgres/docketEntries/getDocketEntryOrderMotion.ts
@@ -1,4 +1,4 @@
-import { getDbReader } from '@web-api/database';
+import { getDbReader } from '@web-api/persistence/postgres/database';
 import { DocketEntryOrderMotionKysely } from '@web-api/persistence/postgres/docketEntries/schema';
 
 export const getDocketEntryOrderMotion = async (
