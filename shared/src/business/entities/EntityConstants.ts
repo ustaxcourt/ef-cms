2c6f69051b125ad06d68f97c70120475844f6d63
Alex Dziarnowski <alexdziarn@gmail.com>
2025-12-10 10:07:03 -0500

    8994: remove and simplify filedby field

--- shared/src/business/entities/EntityConstants.ts
+++ shared/src/business/entities/EntityConstants.ts
@@ -991,13 +991,6 @@ export const PRACTITIONER_ASSOCIATION_DOCUMENT_TYPES_MAP = [
     allowImmediateAssociation: false,
     filedByPractitioner: true,
   },
-  {
-    documentType: 'Notice of Withdrawal',
-    documentTitle: 'Notice of Withdrawal',
-    eventCode: 'NOTW',
-    allowImmediateAssociation: true,
-    filedByPractitioner: true,
-  },
 ];
 
 export const PRACTITIONER_ASSOCIATION_DOCUMENT_TYPES =
