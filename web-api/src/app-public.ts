609e9f4d3b05cf0dc6c3bf3dff8bd39174018c13
Andrew Kuny <akuny@flexion.us>
2025-12-12 12:09:10 -0500

    8655: move verify-email PUT endpoint to public api

--- web-api/src/app-public.ts
+++ web-api/src/app-public.ts
@@ -91,6 +91,7 @@ import { orderPublicSearchLambda } from './lambdas/public-api/orderPublicSearchL
 import { todaysOpinionsLambda } from './lambdas/public-api/todaysOpinionsLambda';
 import { todaysOrdersLambda } from './lambdas/public-api/todaysOrdersLambda';
 import { getDbReader } from '@web-api/database';
+import { verifyUserPendingEmailLambda } from './lambdas/users/verifyUserPendingEmailLambda';
 
 /** Case */
 {
@@ -177,4 +178,8 @@ app.get('/public-api/judges', lambdaWrapper(getPublicJudgesLambda));
  */
 {
   app.get('/system/feature-flag', lambdaWrapper(getAllFeatureFlagsLambda));
+}
+
+{
+  app.put('/public-api/verify-email', lambdaWrapper(verifyUserPendingEmailLambda));
 }
