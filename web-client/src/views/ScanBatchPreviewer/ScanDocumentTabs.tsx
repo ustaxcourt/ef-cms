d2cc0346eed74f728bbfd5b75bedc61d671ec34e
Paulo Da Rocha Filho <paulo.filho.ctr@gmail.com>
2025-12-17 13:02:52 -0600

    8821: add cypress tests to file selection components

--- web-client/src/views/ScanBatchPreviewer/ScanDocumentTabs.tsx
+++ web-client/src/views/ScanBatchPreviewer/ScanDocumentTabs.tsx
@@ -41,7 +41,7 @@ export const ScanDocumentTabs = ({
               const fileUploaded = isFileUploaded(documentTab.eventCode);
               return (
                 <Tab
-                  data-testid={`tabButton-${documentTab.documentType}`}
+                  data-testid={documentTab.tabTitle}
                   icon={
                     fileUploaded ? (
                       <>
