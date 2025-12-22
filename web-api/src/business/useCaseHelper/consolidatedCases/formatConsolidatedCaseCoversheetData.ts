c5b2e96d6ef4d1200773cdc7d46c170a6db3b7f1
Abel <Abel.Ratanaphan.ctr@ustaxcourt.gov>
2025-12-22 15:37:40 -0500

    8477: revert back to using leadCase caption for consolidated coversheets
    Co-authored-by: Christie Laferriere <christie.laferriere.ctr@ustaxcourt.gov>

--- web-api/src/business/useCaseHelper/consolidatedCases/formatConsolidatedCaseCoversheetData.ts
+++ web-api/src/business/useCaseHelper/consolidatedCases/formatConsolidatedCaseCoversheetData.ts
@@ -1,16 +1,7 @@
 import { Case } from '@shared/business/entities/cases/Case';
 import { formatCaseTitle } from '@web-api/business/useCases/generateCoverSheetData';
-import { getCaseByDocketNumber } from '@web-api/persistence/postgres/cases/getCaseByDocketNumber';
 import { getConsolidatedCases } from '@web-api/persistence/postgres/cases/getConsolidatedCases';
 
-/**
- * Formats consolidated cases coversheet data
- * @param {object} providers.applicationContext the applicationContext
- * @param {object} providers.caseEntity the case entity
- * @param {object} providers.coverSheetData the coversheet data
- * @param {object} providers.docketEntryEntity the docketEntryEntity
- * @returns {object} coversheet data with consolidated cases
- */
 export const formatConsolidatedCaseCoversheetData = async ({
   applicationContext,
   caseEntity,
@@ -28,10 +19,7 @@ export const formatConsolidatedCaseCoversheetData = async ({
   let caseCaptionExtension;
   const consolidatedCasesFiltered = consolidatedCases
     ?.map(consolidatedCase => {
-      if (
-        consolidatedCase.docketNumber ===
-        docketEntryEntity.multiDocketedOriginalDocketNumber
-      ) {
+      if (consolidatedCase.docketNumber === caseEntity.leadDocketNumber) {
         ({ caseCaptionExtension, caseTitle } = formatCaseTitle({
           applicationContext,
           caseEntity: consolidatedCase,
@@ -49,18 +37,6 @@ export const formatConsolidatedCaseCoversheetData = async ({
       };
     })
     .filter(consolidatedCase => consolidatedCase.documentNumber !== undefined);
-
-  if (!caseTitle) {
-    const originalFilingCase = await getCaseByDocketNumber({
-      docketNumber: docketEntryEntity.multiDocketedOriginalDocketNumber,
-      includeConsolidatedCases: false,
-    });
-    ({ caseCaptionExtension, caseTitle } = formatCaseTitle({
-      applicationContext,
-      caseEntity: originalFilingCase,
-      useInitialData,
-    }));
-  }
 
   if (consolidatedCasesFiltered.length > 1) {
     coverSheetData.consolidatedCases = consolidatedCasesFiltered;
