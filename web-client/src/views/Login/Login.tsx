757ba71e12df47af28f219140afa77186cc2183d
Kwabena Ampofo <amp.kwabena@gmail.com>
2025-12-31 15:27:26 -0500

    9367-dawson-ui: Update various login page styles

--- web-client/src/views/Login/Login.tsx
+++ web-client/src/views/Login/Login.tsx
@@ -1,5 +1,4 @@
 import { Button as DawsonUiButton } from '@web-client/dawson-ui/ui/button';
-import { Button } from '@web-client/ustc-ui/Button/Button';
 import { InfoNotificationComponent } from '@web-client/views/InfoNotification';
 import { SuccessNotification } from '@web-client/views/SuccessNotification';
 import { WarningNotification } from '@web-client/views/WarningNotification';
@@ -8,7 +7,6 @@ import { sequences, state } from '@web-client/presenter/app.cerebral';
 import React from 'react';
 import { AlertError } from '@web-client/dawson-ui/ui/Alert/AlertError';
 import { TextField } from '@web-client/dawson-ui/ui/input';
-import { Mobile, NonMobile } from '@web-client/ustc-ui/Responsive/Responsive';
 
 export const Login = connect(
   {
@@ -38,146 +36,129 @@ export const Login = connect(
     alertHelper,
     dismissAlertSequence,
   }) => {
-    const LoginDiv = (
+    return (
       <>
-        <div className="grid-row flex-justify-center">
-          <div className="tw:w-md">
-            <SuccessNotification isDismissible={false} />
-            <WarningNotification isDismissible={false} />
-            {alertInfo && (
-              <InfoNotificationComponent
-                alertInfo={alertInfo}
-                dismissible={false}
-              ></InfoNotificationComponent>
-            )}
+        <section className="grid-container tw:px-0 tw:pb-16 tw:pt-0">
+          <div className="grid-row flex-justify-center">
+            <div className="tw:w-full tw:xs:w-135">
+              <SuccessNotification isDismissible={false} />
+              <WarningNotification isDismissible={false} />
+              {alertInfo && (
+                <InfoNotificationComponent
+                  alertInfo={alertInfo}
+                  dismissible={false}
+                />
+              )}
 
-            <div className="tw:mb-4">
-              <AlertError
-                alertError={alertError}
-                alertHelper={alertHelper}
-                closeButtonOnClick={() => dismissAlertSequence()}
-                isDismissible={false}
-              />
-            </div>
+              <div className="tw:mb-4">
+                <AlertError
+                  alertError={alertError}
+                  alertHelper={alertHelper}
+                  closeButtonOnClick={() => dismissAlertSequence()}
+                  isDismissible={false}
+                />
+              </div>
 
-            <div className="grid-container bg-white tw:py-6 border border-base-lighter login">
-              <div className="display-flex flex-column">
-                <div className="flex-align-center">
-                  <h1
-                    className="tw:mb-2 inherit-body-font-family"
-                    data-testid="login-header"
-                  >
-                    Log in to DAWSON
-                  </h1>
-                  <form
-                    className="usa-form tw:mt-8 max-width-unset"
-                    onSubmit={e => {
-                      e.preventDefault();
-                      submitLoginSequence();
-                    }}
-                  >
-                    <label className="usa-label tw:mb-1.75" htmlFor="email">
-                      Email address
-                    </label>
-                    <TextField
-                      required
-                      autoCapitalize="off"
-                      autoCorrect="off"
-                      className="usa-input"
-                      data-testid="email-input"
-                      id="email"
-                      name="email"
-                      type="email"
-                      onChange={e => {
-                        updateAuthenticationFormValueSequence({
-                          email: e.target.value,
-                        });
-                      }}
-                    />
-                    <label
-                      className="usa-label tw:mb-1.75 tw:mt-4"
-                      htmlFor="password"
+              <div className="grid-container bg-white tw:p-12 border border-base-lighter login tw:rounded-2xl tw:max-[33.75rem]:rounded-none">
+                <div className="display-flex flex-column">
+                  <div className="flex-align-center">
+                    <h1
+                      className="tw:mb-0 tw:font-bold tw:font-['Noto_Serif_JP'] inherit-body-font-family tw:xs:text-[2rem] tw:text-[1.5rem]"
+                      data-testid="login-header"
                     >
-                      Password
-                    </label>
-                    <TextField
-                      required
-                      className="usa-input tw:mt-0"
-                      data-testid="password-input"
-                      id="password"
-                      name="password"
-                      type={showPassword ? 'text' : 'password'}
-                      onChange={e => {
-                        updateAuthenticationFormValueSequence({
-                          password: e.target.value,
-                        });
+                      Log in to DAWSON
+                    </h1>
+                    <form
+                      className="usa-form tw:mt-8 max-width-unset"
+                      onSubmit={e => {
+                        e.preventDefault();
+                        submitLoginSequence();
                       }}
-                    />
-                    <button
-                      className="usa-show-password"
-                      data-hide-text="Hide password"
-                      data-show-text="Show password"
-                      type="button"
-                      onClick={() =>
-                        toggleShowPasswordSequence({
-                          passwordType: 'showPassword',
-                        })
-                      }
                     >
-                      {showPassword ? 'Hide password' : 'Show password'}
-                    </button>
-                    <div className="tw:mt-6 tw:mb-4">
+                      <TextField
+                        required
+                        autoCapitalize="off"
+                        autoCorrect="off"
+                        className="usa-input tw:mb-4"
+                        data-testid="email-input"
+                        id="email"
+                        name="email"
+                        type="email"
+                        label="Email address"
+                        onChange={e => {
+                          updateAuthenticationFormValueSequence({
+                            email: e.target.value,
+                          });
+                        }}
+                      />
+                      <TextField
+                        required
+                        className="usa-input tw:mt-0"
+                        data-testid="password-input"
+                        id="password"
+                        name="password"
+                        type={showPassword ? 'text' : 'password'}
+                        label="Password"
+                        onChange={e => {
+                          updateAuthenticationFormValueSequence({
+                            password: e.target.value,
+                          });
+                        }}
+                      />
+                      <div className="tw:mt-2 tw:text-right">
+                        <DawsonUiButton
+                          variant="primaryTertiary"
+                          type="button"
+                          data-hide-text="Hide password"
+                          data-show-text="Show password"
+                          className="tw:text-base"
+                          onClick={() =>
+                            toggleShowPasswordSequence({
+                              passwordType: 'showPassword',
+                            })
+                          }
+                        >
+                          {showPassword ? 'Hide password' : 'Show password'}
+                        </DawsonUiButton>
+                      </div>
+                      <div className="tw:mt-3 tw:mb-4">
+                        <DawsonUiButton
+                          aria-label="Login"
+                          data-testid="login-button"
+                          variant="primary"
+                        >
+                          Log in
+                        </DawsonUiButton>
+                      </div>
+                    </form>
+                    <div className="tw:my-4">
                       <DawsonUiButton
-                        aria-label="Login"
-                        data-testid="login-button"
-                        variant="primary"
+                        aria-label="Forgot password"
+                        variant="primaryTertiary"
+                        data-testid="forgot-password-button"
+                        onClick={() => navigateToForgotPasswordSequence()}
                       >
-                        Log in
+                        Forgot password?
                       </DawsonUiButton>
                     </div>
-                  </form>
-                  <div className="tw:my-4">
+                    <div>Don&apos;t have an account? </div>
                     <DawsonUiButton
-                      aria-label="Forgot password"
+                      className="tw:block tw:pt-0 tw:pl-0 tw:mt-2"
                       variant="primaryTertiary"
-                      data-testid="forgot-password-button"
-                      onClick={() => navigateToForgotPasswordSequence()}
+                      type="button"
+                      onClick={e => {
+                        e.preventDefault();
+                        navigateToCreatePetitionerAccountSequence();
+                      }}
                     >
-                      Forgot password?
+                      Create your account now.
                     </DawsonUiButton>
                   </div>
-                  Don&apos;t have an account?{' '}
-                  <Button
-                    link={true}
-                    className="tw:block tw:pt-0 tw:pl-0"
-                    type="button"
-                    onClick={e => {
-                      e.preventDefault();
-                      navigateToCreatePetitionerAccountSequence();
-                    }}
-                    variant="primary"
-                  >
-                    Create your account now.
-                  </Button>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </>
-    );
-    return (
-      <>
-        <NonMobile>
-          <section className="grid-container tw:px-4 tw:pb-16 tw:pt-0">
-            {LoginDiv}
-          </section>
-        </NonMobile>
-        <Mobile>
-          <section className="grid-container tw:px-0 tw:pb-16 tw:pt-0">
-            {LoginDiv}
-          </section>
-        </Mobile>
+        </section>
       </>
     );
   },
