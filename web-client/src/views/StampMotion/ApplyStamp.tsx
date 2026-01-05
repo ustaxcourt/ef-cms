7171dc24869a9e426f2bf6de348ad6721a00f72a
Paulo Da Rocha Filho <paulo.filho.ctr@gmail.com>
2026-01-05 13:09:56 -0600

    9472: fix typescript errors

--- web-client/src/views/StampMotion/ApplyStamp.tsx
+++ web-client/src/views/StampMotion/ApplyStamp.tsx
@@ -10,6 +10,10 @@ import React, { useEffect, useRef } from 'react';
 import { RenderParameters } from 'pdfjs-dist/types/src/display/api';
 import { PageViewport } from 'pdfjs-dist/types/src/display/display_utils';
 
+type RenderParametersWithCanvas = RenderParameters & {
+  canvas?: HTMLCanvasElement;
+};
+
 export const ApplyStamp = connect(
   {
     applyStampFormChangeSequence: sequences.applyStampFormChangeSequence,
@@ -66,7 +70,7 @@ export const ApplyStamp = connect(
               canvas.height = viewport.height;
               canvas.width = viewport.width;
 
-              const renderContext: RenderParameters = {
+              const renderContext: RenderParametersWithCanvas = {
                 canvas,
                 canvasContext,
                 viewport,
