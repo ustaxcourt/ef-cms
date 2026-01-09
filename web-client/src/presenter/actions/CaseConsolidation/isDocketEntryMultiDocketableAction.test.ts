edff425947398fc7a97ffc009cc7e2b081f1a9f4
Abel <Abel.Ratanaphan.ctr@ustaxcourt.gov>
2026-01-09 11:46:10 -0500

    8477: fix isDocketEntryMultiDocketableAction test
    Co-authored-by: Christie Laferriere <christie.laferriere.ctr@ustaxcourt.gov>

--- web-client/src/presenter/actions/CaseConsolidation/isDocketEntryMultiDocketableAction.test.ts
+++ web-client/src/presenter/actions/CaseConsolidation/isDocketEntryMultiDocketableAction.test.ts
@@ -1,20 +1,23 @@
+jest.mock('@web-client/presenter/computeds/confirmInitiateServiceModalHelper');
 import { applicationContextForClient as applicationContext } from '@web-client/test/createClientTestApplicationContext';
 import { isDocketEntryMultiDocketableAction } from './isDocketEntryMultiDocketableAction';
 import { presenter } from '../../presenter-mock';
 import { runAction } from '@web-client/presenter/test.cerebral';
+import { MOCK_LEAD_CASE_WITH_PAPER_SERVICE } from '@shared/test/mockCase';
+import { shouldAllowMultiDocketing as shouldAllowMultiDocketingMock } from '@web-client/presenter/computeds/confirmInitiateServiceModalHelper';
+import { MOCK_ANSWER } from '@shared/test/mockDocketEntry';
 
 describe('isDocketEntryMultiDocketableAction', () => {
-  const mockDocketEntryId = '123333333';
   let pathYesStub;
   let pathNoStub;
-  const {
-    ENTERED_AND_SERVED_EVENT_CODES,
-    SINGLE_DOCKET_RECORD_ONLY_EVENT_CODES,
-  } = applicationContext.getConstants();
+
+  const shouldAllowMultiDocketing = jest.mocked(shouldAllowMultiDocketingMock);
 
   beforeAll(() => {
     pathYesStub = jest.fn();
     pathNoStub = jest.fn();
+
+    shouldAllowMultiDocketing.mockReturnValue(true);
 
     presenter.providers.applicationContext = applicationContext;
 
@@ -24,69 +27,70 @@ describe('isDocketEntryMultiDocketableAction', () => {
     };
   });
 
-  SINGLE_DOCKET_RECORD_ONLY_EVENT_CODES.forEach(eventCode => {
-    it(`should return the no path when the eventCode ${eventCode} is one of SINGLE_DOCKET_RECORD_ONLY_EVENT_CODES`, async () => {
-      await runAction(isDocketEntryMultiDocketableAction, {
-        modules: {
-          presenter,
-        },
-        state: {
-          form: {
-            eventCode: 'M083',
-          },
-        },
-      });
-
-      expect(pathNoStub).toHaveBeenCalled();
-    });
-  });
-
-  ENTERED_AND_SERVED_EVENT_CODES.forEach(eventCode => {
-    it(`should return the no path when the eventCode ${eventCode} is one of ENTERED_AND_SERVED_EVENT_CODES`, async () => {
-      await runAction(isDocketEntryMultiDocketableAction, {
-        modules: {
-          presenter,
-        },
-        state: {
-          form: {
-            eventCode,
-          },
-        },
-      });
-
-      expect(pathNoStub).toHaveBeenCalled();
-    });
-  });
-
-  it('should return the yes path when the form eventCode is not one of SINGLE_DOCKET_RECORD_ONLY_EVENT_CODES and ENTERED_AND_SERVED_EVENT_CODES', async () => {
+  it('should return the yes path if shouldAllowMultiDocketing is true', async () => {
     await runAction(isDocketEntryMultiDocketableAction, {
       modules: {
         presenter,
       },
       state: {
-        form: {
-          eventCode: 'A',
-        },
-      },
-    });
-
-    expect(pathYesStub).toHaveBeenCalled();
-  });
-
-  it('should return the yes path when the docket entry eventCode is not one of SINGLE_DOCKET_RECORD_ONLY_EVENT_CODES and ENTERED_AND_SERVED_EVENT_CODES', async () => {
-    await runAction(isDocketEntryMultiDocketableAction, {
-      modules: {
-        presenter,
-      },
-      state: {
-        caseDetail: {
-          docketEntries: [{ docketEntryId: mockDocketEntryId, eventCode: 'A' }],
-        },
-        docketEntryId: mockDocketEntryId,
+        caseDetail: MOCK_LEAD_CASE_WITH_PAPER_SERVICE,
         form: {},
       },
     });
 
     expect(pathYesStub).toHaveBeenCalled();
+  });
+
+  it('should return the no path if shouldAllowMultiDocketing is false', async () => {
+    shouldAllowMultiDocketing.mockReturnValueOnce(false);
+    await runAction(isDocketEntryMultiDocketableAction, {
+      modules: {
+        presenter,
+      },
+      state: {
+        caseDetail: MOCK_LEAD_CASE_WITH_PAPER_SERVICE,
+        form: {},
+      },
+    });
+
+    expect(pathNoStub).toHaveBeenCalled();
+  });
+
+  it('should use docketEntry from state.form if form is populated', async () => {
+    await runAction(isDocketEntryMultiDocketableAction, {
+      modules: {
+        presenter,
+      },
+      state: {
+        caseDetail: MOCK_LEAD_CASE_WITH_PAPER_SERVICE,
+        form: MOCK_ANSWER,
+      },
+    });
+
+    expect(shouldAllowMultiDocketing).toHaveBeenCalledWith(
+      expect.objectContaining({
+        docketEntry: MOCK_ANSWER,
+      }),
+    );
+  });
+
+  it('should use docketEntry from state.caseDetail if form is not populated', async () => {
+    await runAction(isDocketEntryMultiDocketableAction, {
+      modules: {
+        presenter,
+      },
+      state: {
+        docketEntryId:
+          MOCK_LEAD_CASE_WITH_PAPER_SERVICE.docketEntries[0].docketEntryId,
+        caseDetail: MOCK_LEAD_CASE_WITH_PAPER_SERVICE,
+        form: {},
+      },
+    });
+
+    expect(shouldAllowMultiDocketing).toHaveBeenCalledWith(
+      expect.objectContaining({
+        docketEntry: MOCK_LEAD_CASE_WITH_PAPER_SERVICE.docketEntries[0],
+      }),
+    );
   });
 });
