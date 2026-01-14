e8ced0fbf6f642e7512cc30358339afbefe82784
Cody Seibert <cseibert113@gmail.com>
2026-01-14 16:32:53 -0500

    9305 - fix code for cypress testing

--- web-client/src/presenter/actions/TrialSession/navigateToMinuteSheetForUnscheduledCaseAction.ts
+++ web-client/src/presenter/actions/TrialSession/navigateToMinuteSheetForUnscheduledCaseAction.ts
@@ -5,9 +5,11 @@ import { state } from '@web-client/presenter/app.cerebral';
  * Uses lead case docket number if the case is consolidated
  * @param {object} providers the providers object
  * @param {object} providers.get the cerebral get function
+ * @param {object} providers.router the router object for navigation
  */
-export const navigateToMinuteSheetForUnscheduledCaseAction = ({
+export const navigateToMinuteSheetForUnscheduledCaseAction = async ({
   get,
+  router,
 }: ActionProps) => {
   const caseInfo = get(state.modal.caseInfo);
   const trialSessionId = get(state.modal.trialSessionId);
@@ -15,8 +17,11 @@ export const navigateToMinuteSheetForUnscheduledCaseAction = ({
   // Use lead case docket number if consolidated, otherwise use the case's docket number
   const docketNumber = caseInfo.leadDocketNumber || caseInfo.docketNumber;
 
-  window.open(
-    `/trial-session-detail/${trialSessionId}/case/${docketNumber}/minutes?isUnscheduledCase=true`,
-    '_blank',
-  );
+  const url = `/trial-session-detail/${trialSessionId}/case/${docketNumber}/minutes?isUnscheduledCase=true`;
+
+  if (window.localStorage?.getItem('__cypressMinuteSheetInSameTab')) {
+    await router.route(url);
+  } else {
+    window.open(url, '_blank');
+  }
 };
