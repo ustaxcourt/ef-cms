7d656f1a96c85cabee1ca1fbe48890d8c9ebe403
Andrew Kuny <akuny@flexion.us>
2026-01-15 10:53:19 -0500

    9266: rework feature flag script to take argument, add step to run feature flag script to circle config deploy job

--- scripts/postgres/featureFlags/setup-restricted-event-codes.ts
+++ scripts/postgres/featureFlags/setup-restricted-event-codes.ts
@@ -1,9 +1,30 @@
 #!/usr/bin/env -S npx ts-node --transpile-only
 
 import { pgInsertInto } from '@web-api/persistence/postgres/utils/operation/pgInsertInto';
+import {
+  parseArgsAndEnvVars,
+  ScriptConfig,
+} from 'scripts/helpers/parseArgsAndEnvVars';
+
+const scriptConfig: ScriptConfig = {
+  description:
+    'setup-restricted-event-codes - Sets the list of restricted event codes in Feature Flag table',
+  parameters: {
+    eventCodes: {
+      default: '', // Pass in a comma-separated string of event codes when calling the script
+      position: 0,
+      required: false,
+      type: 'string',
+    },
+  },
+};
+
+const { eventCodes } = parseArgsAndEnvVars(scriptConfig) as {
+  eventCodes: string;
+};
 
 async function setupRestrictedEventCodes() {
-  const VALUE = 'M116';
+  const VALUE = eventCodes;
 
   await pgInsertInto({
     table: 'dwFeatureFlag',
