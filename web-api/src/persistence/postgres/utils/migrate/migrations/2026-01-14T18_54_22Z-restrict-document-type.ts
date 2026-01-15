35a9ba4be8203fbe54ce763978c0a5d300e914ff
Andrew Kuny <akuny@flexion.us>
2026-01-15 15:46:31 -0500

    9266: update migration

--- web-api/src/persistence/postgres/utils/migrate/migrations/2026-01-14T18_54_22Z-restrict-document-type.ts
+++ web-api/src/persistence/postgres/utils/migrate/migrations/2026-01-14T18_54_22Z-restrict-document-type.ts
@@ -1,65 +1,60 @@
 import { Kysely, sql } from 'kysely';
 
 export async function up(db: Kysely<any>): Promise<void> {
-  await db.transaction().execute(async trx => {
-    // Update previousDocument.documentType where it matches
-    await trx
-      .updateTable('dwDocketEntry')
-      .set({
-        previousDocument: sql`jsonb_set(
-          "previousDocument",
+  await db
+    .updateTable('dwDocketEntry')
+    .set({
+      previousDocument: sql`jsonb_set(
+          previous_document,
           '{documentType}',
           '"Motion to Withdraw Counsel by Party"'
         )`,
-      })
-      .where(
-        sql`"previousDocument"->>'documentType'`,
-        '=',
-        'Motion to Withdraw Counsel (filed by petitioner)',
-      )
-      .execute();
+    })
+    .where(
+      sql`previous_document->>'documentType'`,
+      '=',
+      'Motion to Withdraw Counsel (filed by petitioner)',
+    )
+    .execute();
 
-    // Update documentType column where it matches
-    await trx
-      .updateTable('dwDocketEntry')
-      .set({
-        documentType: 'Motion to Withdraw Counsel by Party',
-      })
-      .where(
-        'documentType',
-        '=',
-        'Motion to Withdraw Counsel (filed by petitioner)',
-      )
-      .execute();
-  });
+  // Update documentType column where it matches
+  await db
+    .updateTable('dwDocketEntry')
+    .set({
+      documentType: 'Motion to Withdraw Counsel by Party',
+    })
+    .where(
+      'documentType',
+      '=',
+      'Motion to Withdraw Counsel (filed by petitioner)',
+    )
+    .execute();
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-  await db.transaction().execute(async trx => {
-    // Update previousDocument.documentType where it matches
-    await trx
-      .updateTable('dwDocketEntry')
-      .set({
-        previousDocument: sql`jsonb_set(
-          "previousDocument",
+  // Update previousDocument.documentType where it matches
+  await db
+    .updateTable('dwDocketEntry')
+    .set({
+      previousDocument: sql`jsonb_set(
+          previous_document,
           '{documentType}',
           '"Motion to Withdraw Counsel (filed by petitioner)"'
         )`,
-      })
-      .where(
-        sql`"previousDocument"->>'documentType'`,
-        '=',
-        'Motion to Withdraw Counsel by Party',
-      )
-      .execute();
+    })
+    .where(
+      sql`previous_document->>'documentType'`,
+      '=',
+      'Motion to Withdraw Counsel by Party',
+    )
+    .execute();
 
-    // Update documentType column where it matches
-    await trx
-      .updateTable('dwDocketEntry')
-      .set({
-        documentType: 'Motion to Withdraw Counsel (filed by petitioner)',
-      })
-      .where('documentType', '=', 'Motion to Withdraw Counsel by Party')
-      .execute();
-  });
+  // Update documentType column where it matches
+  await db
+    .updateTable('dwDocketEntry')
+    .set({
+      documentType: 'Motion to Withdraw Counsel (filed by petitioner)',
+    })
+    .where('documentType', '=', 'Motion to Withdraw Counsel by Party')
+    .execute();
 }
