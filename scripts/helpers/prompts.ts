99f27ea8c55e4f5908e04832c055367c9b8463a4
Jim Lerza <james.lerza@ustaxcourt.gov>
2026-01-23 21:28:14 -0500

    opex-deficiencies-report: added a 'choose' helper that will prompt a CLI user to select a string from an array of strings

--- scripts/helpers/prompts.ts
+++ scripts/helpers/prompts.ts
@@ -1,4 +1,5 @@
 import * as readline from 'node:readline/promises';
+import inquirer from 'inquirer';
 
 export const ask = async (query: string): Promise<string> => {
   const rl = readline.createInterface({
@@ -8,4 +9,20 @@ export const ask = async (query: string): Promise<string> => {
   const answer = await rl.question(query);
   rl.close();
   return answer;
+};
+
+export const choose = async (
+  query: string,
+  choices: string[],
+): Promise<string> => {
+  const { choice } = await inquirer.prompt([
+    {
+      choices,
+      message: query,
+      name: 'choice',
+      type: 'list',
+    },
+  ]);
+
+  return choice;
 };
