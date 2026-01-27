712852dd4e498802384be1623bf31cbab7f119ac
Kwabena Ampofo <amp.kwabena@gmail.com>
2026-01-27 16:09:16 -0500

    8784: Fix type mismatch error for file getCaseExistsProxy.ts

--- shared/src/proxies/requests.ts
+++ shared/src/proxies/requests.ts
@@ -19,7 +19,15 @@ export const setCurrentUserToken = (newToken: string) => {
  * @param {object} providers.params the params to send to the endpoint
  * @returns {Promise<*>} the response data
  */
-export const head = async ({ applicationContext, endpoint, params }) => {
+export const head = async ({
+  applicationContext,
+  endpoint,
+  params,
+}: {
+  applicationContext: ClientApplicationContext;
+  endpoint: string;
+  params?: Record<string, any>;
+}) => {
   return await applicationContext
     .getHttpClient()
     .head(`${applicationContext.getBaseUrl()}${endpoint}`, {
