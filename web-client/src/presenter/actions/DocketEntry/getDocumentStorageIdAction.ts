a7261442c6eb0c22ef8a73431b34258bd9bdc3fd
Abel <Abel.Ratanaphan.ctr@ustaxcourt.gov>
2026-01-29 18:18:04 -0500

    8477: fix getDocumentStorageIdAction pdf loading
    Co-authored-by: Christie Laferriere <christie.laferriere.ctr@ustaxcourt.gov>

--- web-client/src/presenter/actions/DocketEntry/getDocumentStorageIdAction.ts
+++ web-client/src/presenter/actions/DocketEntry/getDocumentStorageIdAction.ts
@@ -1,20 +1,12 @@
 import { state } from '@web-client/presenter/app.cerebral';
+import { getDocumentStorageId } from '@shared/business/utilities/getDocumentStorageId';
 
 export const getDocumentStorageIdAction = ({ get, props }: ActionProps) => {
   const docketEntryId = props.docketEntryId || props.file?.docketEntryId;
 
-  if (!docketEntryId) {
-    return {};
-  }
+  const caseDetail = get(state.caseDetail);
 
-  const { docketEntries } = get(state.caseDetail);
-  const docketEntry = docketEntries.find(de => {
-    return de.docketEntryId === docketEntryId;
-  });
+  const documentStorageId = getDocumentStorageId({ caseDetail, docketEntryId });
 
-  if (!docketEntry) {
-    return {};
-  }
-
-  return { documentStorageId: docketEntry.documentStorageId };
+  return { documentStorageId };
 };
