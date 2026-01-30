42c1560d6aca1fc395a1a8d3fc40f90070609a89
Alex Dziarnowski <alexdziarn@gmail.com>
2026-01-30 12:23:08 -0500

    9593: remove unused field

--- shared/src/business/entities/cases/PublicCase.ts
+++ shared/src/business/entities/cases/PublicCase.ts
@@ -33,8 +33,6 @@ export class PublicCase extends JoiValidationEntity {
   public privatePractitioners?: RawPublicContact[] | PrivatePractitioner[];
   public consolidatedCases?: ConsolidatedCaseSummary[];
 
-  private _score?: string;
-
   constructor(
     rawCase: any,
     {
