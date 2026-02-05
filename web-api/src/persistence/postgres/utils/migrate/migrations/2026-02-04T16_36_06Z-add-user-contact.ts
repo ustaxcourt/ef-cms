036c07239d0a4ee11eb7f4ab6cc938865ffcefc5
nechamakrigsman <nechamafire@gmail.com>
2026-02-05 16:07:55 -0500

    9521: Store lat/lng per user per case in dwUserContact table, remove geocodes from dwUser;

--- web-api/src/persistence/postgres/utils/migrate/migrations/2026-02-04T16_36_06Z-add-user-contact.ts
+++ web-api/src/persistence/postgres/utils/migrate/migrations/2026-02-04T16_36_06Z-add-user-contact.ts
@@ -3,19 +3,17 @@ import { Kysely } from 'kysely';
 export const up = async (db: Kysely<any>): Promise<void> => {
   await db.schema
     .createTable('dwUserContact')
-    .addColumn('userId', 'varchar', col => col.primaryKey())
-    .addColumn('docketNumber', 'varchar')
-    .addColumn('address1', 'varchar', col => col.notNull())
-    .addColumn('address2', 'varchar')
-    .addColumn('address3', 'varchar')
-    .addColumn('city', 'varchar', col => col.notNull())
-    .addColumn('country', 'varchar')
-    .addColumn('countryType', 'varchar', col => col.notNull())
+    .addColumn('userId', 'varchar', col => col.notNull())
+    .addColumn('docketNumber', 'varchar', col => col.notNull())
     .addColumn('lat', 'decimal')
     .addColumn('lng', 'decimal')
-    .addColumn('phone', 'varchar', col => col.notNull())
-    .addColumn('postalCode', 'varchar', col => col.notNull())
-    .addColumn('state', 'varchar')
+    .addPrimaryKeyConstraint('pkUserContact', ['userId', 'docketNumber'])
+    .addForeignKeyConstraint(
+      'dwUserContactUserFK',
+      ['userId'],
+      'dwUser',
+      ['userId'],
+    )
     .addForeignKeyConstraint(
       'dwUserContactDocketFK',
       ['docketNumber'],
