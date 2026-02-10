264f1b1ddae87d263b8190e89dcf32e49b43abf1
Jim Lerza <james.lerza@ustaxcourt.gov>
2026-02-10 15:24:05 -0500

    devex-docker-revival: one too many servor pkills

--- init-local.sh
+++ init-local.sh
@@ -115,7 +115,6 @@ setup_opensearch() {
 setup_s3() {
   echo "Cleaning up old s3rver instance"
   pkill -f s3rver || true
-  pkill -f servor || true
   # Give the OS a moment to release ports
   sleep 2
 
