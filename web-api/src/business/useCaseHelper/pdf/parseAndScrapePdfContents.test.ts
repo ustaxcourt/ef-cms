1cab4984aceb57fe68c3ec71dc93aed58887da9b
devinganey613 <dganey@gunnison.biz>
2026-02-10 13:05:52 -0500

    8790: Fixed api test

--- web-api/src/business/useCaseHelper/pdf/parseAndScrapePdfContents.test.ts
+++ web-api/src/business/useCaseHelper/pdf/parseAndScrapePdfContents.test.ts
@@ -12,7 +12,7 @@ describe('parseAndScrapePdfContents', () => {
 
     expect(
       applicationContext.getUtilities().scrapePdfContents.mock.calls[0][0]
-        .pdfBuffer instanceof Uint8Array,
+        .pdfBuffer instanceof ArrayBuffer,
     ).toEqual(true);
   });
 
