0aa71969f57e0f00699b45dba82bc9b6f0f1236a
Abel <Abel.Ratanaphan.ctr@ustaxcourt.gov>
2026-02-17 14:03:52 -0500

    8477: fix and update unit tests
    Co-authored-by: Christie Laferriere <christie.laferriere.ctr@ustaxcourt.gov>

--- shared/src/business/utilities/getFormattedCaseDetail.sortUndefined.test.ts
+++ shared/src/business/utilities/getFormattedCaseDetail.sortUndefined.test.ts
@@ -2,29 +2,13 @@ import { sortUndefined } from './getFormattedCaseDetail';
 
 describe('sortUndefined', () => {
   it('should return -1 if first arg has createdAtFormatted and the second does not', () => {
-    const result = sortUndefined(
-      {
-        createdAtFormatted: '2019-07-08',
-        index: 2,
-      },
-      {
-        index: 1,
-      },
-    );
+    const result = sortUndefined({ createdAtFormatted: '2019-07-08' }, {});
 
     expect(result).toEqual(-1);
   });
 
   it('should return 1 if second arg has createdAtFormatted and the first does not', () => {
-    const result = sortUndefined(
-      {
-        index: 2,
-      },
-      {
-        createdAtFormatted: '2019-07-08',
-        index: 1,
-      },
-    );
+    const result = sortUndefined({}, { createdAtFormatted: '2019-07-08' });
 
     expect(result).toEqual(1);
   });
