0cb08951bae5c07c293b6534cedcaa99f34c1cf2
devinganey613 <dganey@gunnison.biz>
2026-02-19 09:53:02 -0500

    8784: Updated data test id for messages to use docket number for more consistent selection by cypress tests

--- cypress/local-only/tests/integration/caseDetail/docketRecord/courtIssuedFiling/clerk-of-court-stamp-order.cy.ts
+++ cypress/local-only/tests/integration/caseDetail/docketRecord/courtIssuedFiling/clerk-of-court-stamp-order.cy.ts
@@ -114,9 +114,9 @@ describe('Judge`s chambers stamps an order', () => {
           loginAsClerkOfCourt();
           cy.visit('/messages/my/inbox');
           cy.get(
-            '.message-subject > .message-document-title > [data-testid="messages-individual-inbox-subject-cell"]',
+            `.message-subject > .message-document-title > [data-testid="messages-individual-inbox-subject-cell-${docketNumber}"]`,
           )
-            .last()
+            .first()
             .click();
           cy.get('[data-testid="message-detail-container"]').should('exist');
           cy.get('[data-testid="apply-stamp"]').click();
