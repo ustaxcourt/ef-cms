273648050eaaac849fea55aa8c9d3b7a789525ae
nechamakrigsman <nechamafire@gmail.com>
2026-03-05 17:00:41 -0500

    9745 - refactor saveSignedDocumentInteractor to not return full case;

--- shared/src/business/useCases/saveSignedDocumentInteractor.test.ts
+++ shared/src/business/useCases/saveSignedDocumentInteractor.test.ts
@@ -18,10 +18,12 @@ import { mockDocketClerkUser } from '@shared/test/mockAuthUsers';
 import { saveSignedDocumentInteractor } from './saveSignedDocumentInteractor';
 import { upsertMessages as upsertMessagesMock } from '@web-api/persistence/postgres/messages/upsertMessages';
 import { getCaseByDocketNumber as getCaseByDocketNumberMock } from '@web-api/persistence/postgres/cases/getCaseByDocketNumber';
+import { updateCaseAndAssociations as updateCaseAndAssociationsMock } from '@web-api/business/useCaseHelper/caseAssociation/updateCaseAndAssociations';
 
 describe('saveSignedDocumentInteractor', () => {
   let mockCase;
   const getCaseByDocketNumber = getCaseByDocketNumberMock as jest.Mock;
+  const updateCaseAndAssociations = jest.mocked(updateCaseAndAssociationsMock);
 
   const mockSigningName = 'Roslindis Angelino';
   const mockDocumentIdBeforeSignature = 'abc81f4d-1e47-423a-8caf-6d2fdc3d3857';
@@ -123,7 +125,7 @@ describe('saveSignedDocumentInteractor', () => {
   });
 
   it('should add the signed Stipulated Decision to the case given a Proposed Stipulated Decision', async () => {
-    const { caseEntity } = await saveSignedDocumentInteractor(
+    await saveSignedDocumentInteractor(
       applicationContext,
       {
         docketNumber: mockCase.docketNumber,
@@ -134,15 +136,18 @@ describe('saveSignedDocumentInteractor', () => {
       mockDocketClerkUser,
     );
 
-    expect(caseEntity.docketEntries.length).toEqual(MOCK_DOCUMENTS.length + 1);
-    const signedDocument = caseEntity.docketEntries.find(
+    expect(updateCaseAndAssociations).toHaveBeenCalled();
+    const { caseToUpdate } = updateCaseAndAssociations.mock.calls[0][0];
+
+    expect(caseToUpdate.docketEntries.length).toEqual(MOCK_DOCUMENTS.length + 1);
+    const signedDocument = caseToUpdate.docketEntries.find(
       e =>
         e.documentType ===
         SIGNED_DOCUMENT_TYPES.signedStipulatedDecision.documentType,
     );
-    expect(signedDocument?.docketNumber).toEqual(caseEntity.docketNumber);
+    expect(signedDocument?.docketNumber).toEqual(caseToUpdate.docketNumber);
 
-    const signedDocketEntry = caseEntity.docketEntries.find(
+    const signedDocketEntry = caseToUpdate.docketEntries.find(
       doc =>
         doc.documentType === 'Stipulated Decision' &&
         doc.docketEntryId === mockSignedDocketEntryId,
@@ -156,7 +161,7 @@ describe('saveSignedDocumentInteractor', () => {
   });
 
   it("should set the document's processing status to complete", async () => {
-    const { caseEntity } = await saveSignedDocumentInteractor(
+    await saveSignedDocumentInteractor(
       applicationContext,
       {
         docketNumber: mockCase.docketNumber,
@@ -167,7 +172,8 @@ describe('saveSignedDocumentInteractor', () => {
       mockDocketClerkUser,
     );
 
-    const signedDocument = caseEntity.docketEntries.find(
+    const { caseToUpdate } = updateCaseAndAssociations.mock.calls[0][0];
+    const signedDocument = caseToUpdate.docketEntries.find(
       doc => doc.docketEntryId === mockOriginalDocketEntryId,
     );
     expect(signedDocument?.processingStatus).toBe(
@@ -176,7 +182,7 @@ describe('saveSignedDocumentInteractor', () => {
   });
 
   it('should set the documentIdBeforeSignature', async () => {
-    const { caseEntity } = await saveSignedDocumentInteractor(
+    await saveSignedDocumentInteractor(
       applicationContext,
       {
         docketNumber: mockCase.docketNumber,
@@ -187,7 +193,8 @@ describe('saveSignedDocumentInteractor', () => {
       mockDocketClerkUser,
     );
 
-    const signedDocument = caseEntity.docketEntries.find(
+    const { caseToUpdate } = updateCaseAndAssociations.mock.calls[0][0];
+    const signedDocument = caseToUpdate.docketEntries.find(
       doc => doc.docketEntryId === mockOriginalDocketEntryId,
     );
     expect(signedDocument?.documentIdBeforeSignature).toBe(
