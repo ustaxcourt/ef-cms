669439351ca242879a998ea1db8d29e4e1fdaea9
nechamakrigsman <nechamafire@gmail.com>
2026-03-05 13:55:20 -0500

    9745 - refactor add/update/delete statistic to not return case - dont need to refresh since these statistics sequences all use navigateToCaseDetailCaseInformationActionFactory which navigates away from the current page;

--- web-api/src/business/useCases/caseStatistics/updateOtherStatisticsInteractor.ts
+++ web-api/src/business/useCases/caseStatistics/updateOtherStatisticsInteractor.ts
@@ -9,7 +9,6 @@ import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { getCaseByDocketNumber } from '@web-api/persistence/postgres/cases/getCaseByDocketNumber';
 import { updateCaseAndAssociations } from '@web-api/business/useCaseHelper/caseAssociation/updateCaseAndAssociations';
 import { withLocking } from '@web-api/persistence/postgres/utils/mutex';
-import { CaseDTO } from '@shared/business/dto/cases/CaseDTO';
 
 /**
  * updateOtherStatistics
@@ -19,7 +18,6 @@ import { CaseDTO } from '@shared/business/dto/cases/CaseDTO';
  * @param {string} providers.docketNumber the docket number of the case to update statistics
  * @param {number} providers.damages damages statistic to add to the case
  * @param {number} providers.litigationCosts litigation costs statistic to add to the case
- * @returns {object} the updated case
  */
 export const updateOtherStatistics = async (
   _applicationContext: ServerApplicationContext,
@@ -29,7 +27,7 @@ export const updateOtherStatistics = async (
     litigationCosts,
   }: { damages: number; docketNumber: string; litigationCosts: number },
   authorizedUser: UnknownAuthUser,
-): Promise<CaseDTO> => {
+): Promise<void> => {
   if (!isAuthorized(authorizedUser, ROLE_PERMISSIONS.ADD_EDIT_STATISTICS)) {
     throw new UnauthorizedError('Unauthorized for editing statistics');
   }
@@ -43,14 +41,10 @@ export const updateOtherStatistics = async (
     { authorizedUser },
   );
 
-  const updatedCase = await updateCaseAndAssociations({
+  await updateCaseAndAssociations({
     authorizedUser,
     caseToUpdate: newCase,
   });
-
-  return new CaseDTO(
-    new Case(updatedCase, { authorizedUser }).validate().toRawObject(),
-  );
 };
 
 export const updateOtherStatisticsInteractor = withLocking(
