f0cbcbad2581e4347eefadb50dbb5ba3bcbb0999
Paulo Da Rocha Filho <paulo.filho.ctr@gmail.com>
2026-03-18 15:03:18 -0500

    8465: update should generate notice of change of trial date computation

--- shared/src/business/utilities/trialSession/shouldGenerateNoticeOfChangeTrialStartDate.ts
+++ shared/src/business/utilities/trialSession/shouldGenerateNoticeOfChangeTrialStartDate.ts
@@ -1,4 +1,8 @@
 import { RawTrialSession } from '@shared/business/entities/trialSessions/TrialSession';
+import {
+  formatDateString,
+  FORMATS,
+} from '@shared/business/utilities/DateHandler';
 
 export function shouldGenerateNoticeOfChangeTrialStartDate(
   currentTrialSession: RawTrialSession,
@@ -6,6 +10,14 @@ export function shouldGenerateNoticeOfChangeTrialStartDate(
 ): boolean {
   if (!currentTrialSession.isCalendared || !updatedTrialSession.isCalendared)
     return false;
-
-  return currentTrialSession.startDate !== updatedTrialSession.startDate;
+  return (
+    formatDateString(
+      currentTrialSession.startDate,
+      FORMATS.MONTH_DAY_YEAR_WITH_DAY_OF_WEEK,
+    ) !==
+    formatDateString(
+      updatedTrialSession.startDate,
+      FORMATS.MONTH_DAY_YEAR_WITH_DAY_OF_WEEK,
+    )
+  );
 }
