36e52df1573aee47521873b4676c8f4d199dcca4
Jim Lerza <james.lerza@ustaxcourt.gov>
2026-03-20 14:25:18 -0400

    8791: fix lost formatting

--- web-client/src/presenter/computeds/formattedCaseDetail.ts
+++ web-client/src/presenter/computeds/formattedCaseDetail.ts
@@ -117,10 +117,9 @@ export const formattedCaseDetail = (
       isCurrentUser: petitioner.contactId === user.userId,
     }));
 
-  const consolidatedCases =
-    formattedCase.consolidatedCases.map(c =>
-      formatCase(applicationContext, c, user),
-    ) || [];
+  const consolidatedCases = formattedCase.consolidatedCases.map(c =>
+    formatCase(applicationContext, c, user),
+  );
 
   const allTrialSessions = get(state.trialSessions);
 
