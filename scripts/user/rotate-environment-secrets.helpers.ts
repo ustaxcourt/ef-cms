a3dff537c7db32eab2dee70fe4a0f8bb00969045
Jim Lerza <james.lerza@ustaxcourt.gov>
2026-03-23 21:51:55 -0400

    9841: added an invokePasswordUpdateLambdaInVaultAccount helper to invoke the password update lambda in the vault account and added a call to it at the end of rotateSecrets

--- scripts/user/rotate-environment-secrets.helpers.ts
+++ scripts/user/rotate-environment-secrets.helpers.ts
@@ -1,41 +1,22 @@
-#!/usr/bin/env -S npx ts-node --transpile-only
-
+import { AssumeRoleCommand, STSClient } from '@aws-sdk/client-sts';
 import { CognitoIdentityProvider } from '@aws-sdk/client-cognito-identity-provider';
+import { InvokeCommand, LambdaClient } from '@aws-sdk/client-lambda';
+import { PutParameterCommand, SSMClient } from '@aws-sdk/client-ssm';
 import {
   GetSecretValueCommand,
   PutSecretValueCommand,
   SecretsManagerClient,
 } from '@aws-sdk/client-secrets-manager';
-import {
-  type ScriptConfig,
-  parseArgsAndEnvVars,
-} from '../helpers/parseArgsAndEnvVars';
 import { makeNewPassword } from './make-new-password';
 
-const scriptConfig: ScriptConfig = {
-  description:
-    'rotate-environment-secrets - Rotates secrets in a deployed environment.',
-  environment: {
-    UserPoolId: 'COGNITO_USER_POOL',
-    ci: 'CI',
-    env: 'ENV',
-  },
-  requireActiveAwsSession: true,
-};
-const { ci, env, UserPoolId } = parseArgsAndEnvVars(scriptConfig) as {
-  ci: string;
-  env: string;
-  UserPoolId: string;
-};
-
-const secretsClient = new SecretsManagerClient({ region: 'us-east-1' });
-const cognitoClient = new CognitoIdentityProvider({
-  region: 'us-east-1',
-});
-
-const isDevelopmentEnvironment = !['prod', 'test'].includes(env);
-
-const loadSecrets = async (secretsName: string): Promise<any> => {
+export const loadSecrets = async ({
+  region,
+  secretsName,
+}: {
+  region: string;
+  secretsName: string;
+}): Promise<any> => {
+  const secretsClient = new SecretsManagerClient({ region });
   const getSecretValueCommand = new GetSecretValueCommand({
     SecretId: secretsName,
   });
@@ -48,10 +29,28 @@ const loadSecrets = async (secretsName: string): Promise<any> => {
   return secrets;
 };
 
-const rotateSecrets = async (environmentName: string): Promise<void> => {
-  console.log(`Rotating secrets for Environment: ${environmentName}\n`);
+export const rotateSecrets = async ({
+  ci,
+  env,
+  region,
+  UserPoolId,
+}: {
+  ci: string;
+  env: string;
+  region: string;
+  UserPoolId: string;
+}): Promise<void> => {
+  const secretsClient = new SecretsManagerClient({ region });
+  const cognitoClient = new CognitoIdentityProvider({ region });
 
-  const secrets = await loadSecrets(`${environmentName}_deploy`);
+  console.log(`Rotating secrets for Environment: ${env}\n`);
+
+  const isDevelopmentEnvironment = !['prod', 'test'].includes(env);
+
+  const secrets = await loadSecrets({
+    region,
+    secretsName: `${env}_deploy`,
+  });
 
   const DEFAULT_ACCOUNT_PASS = isDevelopmentEnvironment
     ? 'Testing1234$'
@@ -101,14 +100,17 @@ const rotateSecrets = async (environmentName: string): Promise<void> => {
 
   let zendeskSecrets;
   try {
-    zendeskSecrets = await loadSecrets(`${environmentName}/ZendeskDawson`);
+    zendeskSecrets = await loadSecrets({
+      region,
+      secretsName: `${env}/ZendeskDawson`,
+    });
   } catch (e) {
     console.log('No Zendesk secrets found');
   }
 
   if (zendeskSecrets) {
     const putZendeskSecretValueCommand = new PutSecretValueCommand({
-      SecretId: `${environmentName}/ZendeskDawson`,
+      SecretId: `${env}/ZendeskDawson`,
       SecretString: JSON.stringify({
         ...zendeskSecrets,
         USTC_ZENDESK_PASS,
@@ -119,12 +121,95 @@ const rotateSecrets = async (environmentName: string): Promise<void> => {
 
     console.log('✅ Zendesk secrets updated');
   }
+
+  if (secrets.VAULT_ACCOUNT_ID?.length) {
+    await invokePasswordUpdateLambdaInVaultAccount({
+      env,
+      newPassword: DEFAULT_ACCOUNT_PASS,
+      region,
+      vaultAccountId: secrets.VAULT_ACCOUNT_ID,
+    });
+  }
 };
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-(async () => {
-  await rotateSecrets(env);
-  console.log(
-    '🏁 All done. Be sure to run setup-test-users.ts or wait for the next deploy',
+export const invokePasswordUpdateLambdaInVaultAccount = async ({
+  env,
+  newPassword,
+  region,
+  vaultAccountId,
+}: {
+  env: string;
+  newPassword: string;
+  region: string;
+  vaultAccountId: string;
+}): Promise<any> => {
+  const parameters = getLambdaParameters(env);
+
+  if (!parameters) {
+    throw new Error(
+      `Could not get lambda parameters for ${env} environment: unsupported environment`,
+    );
+  }
+
+  const roleArn = `arn:aws:iam::${vaultAccountId}:role/vaultwarden-password-rotator`;
+  const stsClient = new STSClient({ region });
+  const assumeRoleCommand = new AssumeRoleCommand({
+    RoleArn: roleArn,
+    RoleSessionName: 'RotationSession',
+  });
+  const { Credentials } = await stsClient.send(assumeRoleCommand);
+
+  if (
+    !Credentials?.AccessKeyId ||
+    !Credentials?.SecretAccessKey ||
+    !Credentials?.SessionToken
+  ) {
+    throw new Error(
+      'Could not get credentials for vaultwarden-password-rotator role in the vault account',
+    );
+  }
+
+  const credentials = {
+    accessKeyId: Credentials.AccessKeyId,
+    secretAccessKey: Credentials.SecretAccessKey,
+    sessionToken: Credentials.SessionToken,
+  };
+
+  const ssmClient = new SSMClient({ credentials, region });
+  const putParameterCommand = new PutParameterCommand({
+    Name: '/vaultwarden/dawson/test',
+    Overwrite: true,
+    Type: 'SecureString',
+    Value: newPassword,
+  });
+  await ssmClient.send(putParameterCommand);
+
+  const lambdaClient = new LambdaClient({ credentials, region });
+  const invokeCommand = new InvokeCommand({
+    FunctionName: `arn:aws:lambda:${vaultAccountId}:function:vaultwarden-rotate-passwords`,
+    Payload: Buffer.from(JSON.stringify(parameters)),
+  });
+  const { Payload } = await lambdaClient.send(invokeCommand);
+
+  if (!Payload) {
+    throw new Error('Lambda invocation failed to return a payload');
+  }
+
+  return JSON.parse(Buffer.from(Payload).toString());
+};
+
+const getLambdaParameters = (
+  env: string,
+): { [key: string]: string } | undefined => {
+  if (env === 'test') {
+    return {
+      collection_name: 'DAWSON Passwords',
+      item_prefix: 'DAWSON (test)',
+      parameter_name: '/vaultwarden/dawson/test',
+      script_path: 'scripts/dawson/rotate-test-passwords.sh',
+    };
+  }
+  console.error(
+    'Did you set a VAULT_ACCOUNT_ID in an environment other than test?',
   );
-})();
+};
