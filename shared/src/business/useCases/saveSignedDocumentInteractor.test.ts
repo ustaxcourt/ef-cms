05650d6e677d6e49626bb926a17024042350ea34
Cody Seibert <cseibert113@gmail.com>
2026-03-24 17:20:49 -0400

    9745 - ignore scripts from ts checks

--- shared/src/business/useCases/saveSignedDocumentInteractor.test.ts
+++ shared/src/business/useCases/saveSignedDocumentInteractor.test.ts
@@ -178,7 +178,7 @@ describe('saveSignedDocumentInteractor', () => {
     expect(updateCaseAndAssociations).toHaveBeenCalled();
     const { caseToUpdate } = updateCaseAndAssociations.mock.calls[0][0];
 
-    expect(caseToUpdate.docketEntries.length).toEqual(MOCK_DOCUMENTS.length + 1);
+    expect(caseToUpdate.docketEntries.length).toEqual(MOCK_CASE.docketEntries.length + 1);
     const signedDocument = caseToUpdate.docketEntries.find(
       e =>
         e.documentType ===
