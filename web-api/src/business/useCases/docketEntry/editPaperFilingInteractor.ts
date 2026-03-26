f2496988c39e8ae32e9ee4413f40fbe0e2ba88cb
Christie Laferriere <christie.laferriere.ctr@ustaxcourt.gov>
2026-03-26 10:55:36 -0400

    8477: fix type error in editPaperFilingInteractor for the fileAndServeDocumentOnOneCase function
    Co-authored-by: Abel Ratanaphan <Abel.Ratanaphan.ctr@ustaxcourt.gov>

--- web-api/src/business/useCases/docketEntry/editPaperFilingInteractor.ts
+++ web-api/src/business/useCases/docketEntry/editPaperFilingInteractor.ts
@@ -312,7 +312,6 @@ const serveDocketEntry = async ({
           docketEntryEntity: new DocketEntry(cloneDeep(updatedDocketEntry), {
             authorizedUser,
           }),
-          subjectCaseDocketNumber: subjectCaseEntity.docketNumber,
           user,
         }),
       ),
