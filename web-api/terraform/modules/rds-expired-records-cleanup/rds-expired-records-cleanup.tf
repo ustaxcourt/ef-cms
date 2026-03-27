21089295c37e986d7b3b9e9471e9f2df620ce6d7
Andrew Kuny <akuny@flexion.us>
2026-03-27 09:28:26 -0400

    9787: scope logging permissions for rds expired records cleanup lambda

--- web-api/terraform/modules/rds-expired-records-cleanup/rds-expired-records-cleanup.tf
+++ web-api/terraform/modules/rds-expired-records-cleanup/rds-expired-records-cleanup.tf
@@ -38,7 +38,8 @@ resource "aws_iam_role_policy" "rds_expired_records_cleanup_lambda_policy" {
         "logs:DescribeLogStreams"
       ],
       "Resource": [
-        "arn:aws:logs:*:*:*"
+        "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/rds_expired_records_cleanup_lambda_${var.environment}_blue:*",
+        "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/rds_expired_records_cleanup_lambda_${var.environment}_green:*"
       ]
     },
 		{
