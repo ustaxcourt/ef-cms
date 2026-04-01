28c332d38c020718aa3bded380d0da0edce52d39
Zachary Brown <zacharybrown204@yahoo.com>
2026-04-01 12:22:25 -0400

    8847: updating tests for sending emails

--- web-api/src/dispatchers/ses/sendBulkTemplatedEmail.test.ts
+++ web-api/src/dispatchers/ses/sendBulkTemplatedEmail.test.ts
@@ -134,7 +134,7 @@ describe('sendBulkTemplatedEmail', () => {
         Destinations: [
           {
             Destination: {
-              ToAddresses: ['test.email@example.com'],
+              ToAddresses: ['test.email@example.net'],
             },
             ReplacementTemplateData: JSON.stringify({
               name: 'Roslindis Angelino',
@@ -144,7 +144,7 @@ describe('sendBulkTemplatedEmail', () => {
           },
           {
             Destination: {
-              ToAddresses: ['test.email2@example.com'],
+              ToAddresses: ['test.email2@example.net'],
             },
             ReplacementTemplateData: JSON.stringify({
               name: 'Roslindis Angelino',
@@ -154,7 +154,7 @@ describe('sendBulkTemplatedEmail', () => {
           },
           {
             Destination: {
-              ToAddresses: ['test.email3@example.com'],
+              ToAddresses: ['test.email3@example.net'],
             },
             ReplacementTemplateData: JSON.stringify({
               name: 'Roslindis Angelino',
@@ -205,17 +205,17 @@ describe('sendBulkTemplatedEmail', () => {
           Destinations: [
             {
               Destination: {
-                ToAddresses: ['test.email@example.com'],
+                ToAddresses: ['test.email@example.net'],
               },
             },
             {
               Destination: {
-                ToAddresses: ['test.email2@example.com'],
+                ToAddresses: ['test.email2@example.net'],
               },
             },
             {
               Destination: {
-                ToAddresses: ['test.email3@example.com'],
+                ToAddresses: ['test.email3@example.net'],
               },
             },
           ],
@@ -225,10 +225,36 @@ describe('sendBulkTemplatedEmail', () => {
         retryCount: 0,
       }),
     ).rejects.toEqual(
-      'Could not complete service to test.email@example.com,test.email2@example.com,test.email3@example.com',
+      'Could not complete service to test.email@example.net,test.email2@example.net,test.email3@example.net',
     );
     expect(applicationContext.getEmailClient().send).toHaveBeenCalledTimes(
       MAX_SES_RETRIES + 1,
     );
   }, 30000);
+
+  it('should not skip sending emails to specific domains', async () => {
+    await sendWithRetry({
+      applicationContext,
+      params: {
+        DefaultTemplateData: undefined,
+        Destinations: [
+          {
+            Destination: {
+              ToAddresses: ['test.email@ustc.gov'],
+            },
+            ReplacementTemplateData: JSON.stringify({
+              name: 'Roslindis Angelino',
+              welcomeMessage: 'Welcome to Flavortown',
+              whoAmI: 'The Sauce Boss',
+            }),
+          },
+        ],
+        Source: 'jest@example.com',
+        Template: 'case_served',
+      },
+      retryCount: 0,
+    });
+
+    expect(applicationContext.getEmailClient().send).not.toHaveBeenCalled();
+  });
 });
