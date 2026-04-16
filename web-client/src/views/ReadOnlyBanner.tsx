bfb9690683d7f4a16218457c2e88d62ecc1d78ad
Jim Lerza <james.lerza@ustaxcourt.gov>
2026-04-16 09:50:53 -0400

    9764: read-only mode - move read-only-banner css to custom.scss

--- web-client/src/views/ReadOnlyBanner.tsx
+++ web-client/src/views/ReadOnlyBanner.tsx
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { connect } from '@web-client/presenter/shared.cerebral';
 import { state } from '@web-client/presenter/app.cerebral';
 import React from 'react';
@@ -10,24 +11,16 @@ export const ReadOnlyBanner = connect(
     if (!readOnlyMode) return null;
 
     return (
-      <div
-        className="read-only-banner"
-        style={{
-          width: '100%',
-          backgroundColor: '#8b1303',
-          color: '#ffffff',
-          fontWeight: 'bold',
-          padding: '10px',
-          textAlign: 'center',
-          boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
-          zIndex: 9999,
-          position: 'relative',
-        }}
-      >
-        <span>
-          ⚠️ We are performing maintenance. During this time, you cannot submit
+      <div className="read-only-banner">
+        <div className="grid-container text-bold">
+          <FontAwesomeIcon
+            className="margin-right-1"
+            icon="warning"
+            size="1x"
+          />
+          We are performing maintenance. During this time, you cannot submit
           filings or edit information. Normal operations will resume shortly.
-        </span>
+        </div>
       </div>
     );
   },
