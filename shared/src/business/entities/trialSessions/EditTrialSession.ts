ed22e713a77d868386dd478c704fa3ed17633dc2
Alex Dziarnowski <alexdziarn@gmail.com>
2026-04-22 15:14:27 -0400

    9458: update shared unit tests

--- shared/src/business/entities/trialSessions/EditTrialSession.ts
+++ shared/src/business/entities/trialSessions/EditTrialSession.ts
@@ -1,5 +1,6 @@
 import { JoiValidationConstants } from '../JoiValidationConstants';
 import { TrialSession } from './TrialSession';
+import joi from 'joi';
 
 export class EditTrialSession extends TrialSession {
   constructor(rawSession: RawEditTrialSession) {
@@ -9,9 +10,15 @@ export class EditTrialSession extends TrialSession {
   getValidationRules() {
     return {
       ...TrialSession.validationRules.COMMON,
-      estimatedEndDate: JoiValidationConstants.ISO_DATE.optional().messages({
-        '*': 'Enter a valid estimated end date',
-      }),
+      estimatedEndDate: JoiValidationConstants.ISO_DATE.min(
+        joi.ref('startDate'),
+      )
+        .optional()
+        .allow(null)
+        .messages({
+          '*': 'Enter a valid estimated end date',
+          'date.min': 'Estimated end date must be after start date',
+        }),
     };
   }
 }
