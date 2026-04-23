ce47a144f659fa707d4423bd21e2187b90b3016d
devinganey613 <dganey@gunnison.biz>
2026-04-23 10:45:00 -0400

    9809: Fixed contract down migration

--- web-api/src/persistence/postgres/utils/migrate/migrations/2026-04-20T18_31_46Z-rename-change-of-address-table.contract.ts
+++ web-api/src/persistence/postgres/utils/migrate/migrations/2026-04-20T18_31_46Z-rename-change-of-address-table.contract.ts
@@ -1,4 +1,4 @@
-import { Kysely } from "kysely";
+import { Kysely } from 'kysely';
 
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema.dropTable('dwChangeOfAddressNew').execute();
@@ -6,9 +6,22 @@ export async function up(db: Kysely<any>): Promise<void> {
 
 export async function down(db: Kysely<any>): Promise<void> {
   await db.schema
+    .alterTable('dwChangeOfAddress')
+    .renameTo('dwChangeOfAddressNew')
+    .execute();
+
+  await db.schema
+    .alterTable('dwChangeOfAddressNew')
+    .renameConstraint('dwChangeOfAddressPkey', 'dwChangeOfAddressNewPkey')
+    .execute();
+
+  await db.schema
     .createTable('dwChangeOfAddress')
     .addColumn('jobId', 'varchar')
     .addColumn('docketNumber', 'varchar')
-    .addPrimaryKeyConstraint('dwChangeOfAddressNewPkey', ['jobId', 'docketNumber'])
+    .addPrimaryKeyConstraint('dwChangeOfAddressPkey', [
+      'jobId',
+      'docketNumber',
+    ])
     .execute();
 }
