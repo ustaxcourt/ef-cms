1e4be5ce473160e74f7e35d4b4e3cd384bf5219c
nechamakrigsman <nechamafire@gmail.com>
2026-04-24 14:10:58 -0400

    9009: Only send parties emails when transactions are successful when inside a transaction;

--- web-api/src/business/useCaseHelper/service/createChangeItems.ts
+++ web-api/src/business/useCaseHelper/service/createChangeItems.ts
@@ -16,6 +16,9 @@ import {
   formattedNewEmailForChangeOfAddress,
   formattedOldEmailForChangeOfAddress,
 } from '@shared/business/utilities/calculateEmail';
+import { inTransaction, onTransactionCommit } from '@web-api/persistence/postgres/utils/transactions';
+
+
 
 /**
  * This function isolates task of generating the Docket Entry
@@ -237,12 +240,21 @@ export const generateAndServeDocketEntry = async ({
       user,
     });
   }
-  await applicationContext.getUseCaseHelpers().sendServedPartiesEmails({
-    applicationContext,
-    caseEntity,
-    docketEntryId: changeOfAddressDocketEntry.docketEntryId,
-    servedParties,
-  });
+
+  async function sendServedPartiesEmails() {
+    return applicationContext.getUseCaseHelpers().sendServedPartiesEmails({
+      applicationContext,
+      caseEntity,
+      docketEntryId: changeOfAddressDocketEntry.docketEntryId,
+      servedParties,
+    });
+  }
+
+  if (inTransaction()) {
+    onTransactionCommit(sendServedPartiesEmails);
+  } else {
+    await sendServedPartiesEmails();
+  }
 
   return { caseEntity, url };
 };
