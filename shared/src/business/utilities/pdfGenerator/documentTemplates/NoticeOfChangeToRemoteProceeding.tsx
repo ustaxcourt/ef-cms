71dd5a28689d5097cde2ae50c3bafac78c95ad08
Abel <Abel.Ratanaphan.ctr@ustaxcourt.gov>
2026-04-27 16:22:30 -0400

    9863: fix apostrophe
    Co-authored-by: Christie Laferriere <christie.laferriere.ctr@ustaxcourt.gov>

--- shared/src/business/utilities/pdfGenerator/documentTemplates/NoticeOfChangeToRemoteProceeding.tsx
+++ shared/src/business/utilities/pdfGenerator/documentTemplates/NoticeOfChangeToRemoteProceeding.tsx
@@ -84,8 +84,8 @@ export const NoticeOfChangeToRemoteProceeding = ({
           <a href="https://www.zoomgov.com" rel="noreferrer" target="_blank">
             www.zoomgov.com
           </a>{' '}
-          and click `Join`. Enter the Meeting ID and Passcode above when
-          prompted.
+          and click &apos;Join&apos;. Enter the Meeting ID and Passcode above
+          when prompted.
         </p>
 
         <p>
