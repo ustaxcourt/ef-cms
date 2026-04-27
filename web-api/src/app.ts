e181f0c2a8a88c97a034520fbb82af91a2f9875b
Andrew Kuny <akuny@flexion.us>
2026-04-27 09:07:51 -0400

    8655: temporarily add endpoint to app.ts

--- web-api/src/app.ts
+++ web-api/src/app.ts
@@ -1095,6 +1095,10 @@ app.delete(
   );
   app.put('/users/pending-email', lambdaWrapper(updateUserPendingEmailLambda));
   app.put('/users/verify-email', lambdaWrapper(verifyUserPendingEmailLambda));
+  app.put(
+    '/public-api/verify-email',
+    lambdaWrapper(verifyUserPendingEmailLambda),
+  );
   app.get(
     '/users/email-availability',
     lambdaWrapper(checkEmailAvailabilityLambda),
