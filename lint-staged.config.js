757117d806f4a7f24c22cf1232dd2bf43a608c3a
TomElliottFlexion <telliott@flexion.us>
2026-04-28 09:25:52 -0500

    experiment: align lint-staged validation with CI lint validation

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  '*.{css,scss}': ['stylelint'],
-  '*.{js,jsx,ts,tsx}': ['eslint'],
-  '*.{sh}': ['lint-staged-shellcheck'],
+  '{shared,web-client}/**/*.{css,scss}': ['stylelint'],
+  '{scripts,shared,web-api,web-client}/**/*.{js,jsx,ts,tsx}': ['eslint'],
+  '{scripts,shared,web-api,web-client}/**/*.sh': ['lint-staged-shellcheck'],
   'swagger.json,v1.yaml,v2.yaml': ['swagger-cli validate'],
 };
