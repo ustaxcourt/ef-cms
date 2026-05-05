d2130127fdd02dde0530babcfdcaecaaa2a330c8
Christie Laferriere <christie.laferriere.ctr@ustaxcourt.gov>
2026-05-05 14:08:28 -0400

    9922: fix cases disappearing after dismissing 30 day notice modal in trial session
    Co-authored-by: Abel Ratanaphan <Abel.Ratanaphan.ctr@ustaxcourt.gov>

--- web-client/src/presenter/sequences/dismissThirtyDayTrialAlertSequence.ts
+++ web-client/src/presenter/sequences/dismissThirtyDayTrialAlertSequence.ts
@@ -1,7 +1,14 @@
 import { clearModalAction } from '../actions/clearModalAction';
 import { dismissThirtyDayAlertFromTrialSessionAction } from '../actions/TrialSession/dismissThirtyDayAlertFromTrialSessionAction';
+import { getCalendaredCasesForTrialSessionAction } from '../actions/TrialSession/getCalendaredCasesForTrialSessionAction';
+import { getEligibleCasesForTrialSessionAction } from '../actions/TrialSession/getEligibleCasesForTrialSessionAction';
 import { getTrialSessionDetailsAction } from '../actions/TrialSession/getTrialSessionDetailsAction';
+import { isTrialSessionCalendaredAction } from '../actions/TrialSession/isTrialSessionCalendaredAction';
+import { mergeCaseOrderIntoCalendaredCasesAction } from '../actions/TrialSession/mergeCaseOrderIntoCalendaredCasesAction';
+import { mergeCaseOrderIntoEligibleCasesAction } from '../actions/TrialSession/mergeCaseOrderIntoEligibleCasesAction';
 import { setAlertErrorAction } from '../actions/setAlertErrorAction';
+import { setCalendaredCasesOnTrialSessionAction } from '../actions/TrialSession/setCalendaredCasesOnTrialSessionAction';
+import { setEligibleCasesOnTrialSessionAction } from '../actions/TrialSession/setEligibleCasesOnTrialSessionAction';
 import { setTrialSessionDetailsAction } from '../actions/TrialSession/setTrialSessionDetailsAction';
 
 export const dismissThirtyDayTrialAlertSequence = [
@@ -9,6 +16,22 @@ export const dismissThirtyDayTrialAlertSequence = [
   dismissThirtyDayAlertFromTrialSessionAction,
   {
     error: [setAlertErrorAction],
-    success: [getTrialSessionDetailsAction, setTrialSessionDetailsAction],
+    success: [
+      getTrialSessionDetailsAction,
+      setTrialSessionDetailsAction,
+      isTrialSessionCalendaredAction,
+      {
+        no: [
+          getEligibleCasesForTrialSessionAction,
+          setEligibleCasesOnTrialSessionAction,
+          mergeCaseOrderIntoEligibleCasesAction,
+        ],
+        yes: [
+          getCalendaredCasesForTrialSessionAction,
+          setCalendaredCasesOnTrialSessionAction,
+          mergeCaseOrderIntoCalendaredCasesAction,
+        ],
+      },
+    ],
   },
 ];
