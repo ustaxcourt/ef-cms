c614c9bca09c4bfc2fe55350395dd5c07e4e8a8a
Christie Laferriere <christie.laferriere.ctr@ustaxcourt.gov>
2026-05-06 11:47:55 -0400

    9922: remove isCalendared check and update test
    Co-authored-by: Abel Ratanaphan <Abel.Ratanaphan.ctr@ustaxcourt.gov>

--- web-client/src/presenter/sequences/dismissThirtyDayTrialAlertSequence.ts
+++ web-client/src/presenter/sequences/dismissThirtyDayTrialAlertSequence.ts
@@ -1,14 +1,10 @@
 import { clearModalAction } from '../actions/clearModalAction';
 import { dismissThirtyDayAlertFromTrialSessionAction } from '../actions/TrialSession/dismissThirtyDayAlertFromTrialSessionAction';
 import { getCalendaredCasesForTrialSessionAction } from '../actions/TrialSession/getCalendaredCasesForTrialSessionAction';
-import { getEligibleCasesForTrialSessionAction } from '../actions/TrialSession/getEligibleCasesForTrialSessionAction';
 import { getTrialSessionDetailsAction } from '../actions/TrialSession/getTrialSessionDetailsAction';
-import { isTrialSessionCalendaredAction } from '../actions/TrialSession/isTrialSessionCalendaredAction';
 import { mergeCaseOrderIntoCalendaredCasesAction } from '../actions/TrialSession/mergeCaseOrderIntoCalendaredCasesAction';
-import { mergeCaseOrderIntoEligibleCasesAction } from '../actions/TrialSession/mergeCaseOrderIntoEligibleCasesAction';
 import { setAlertErrorAction } from '../actions/setAlertErrorAction';
 import { setCalendaredCasesOnTrialSessionAction } from '../actions/TrialSession/setCalendaredCasesOnTrialSessionAction';
-import { setEligibleCasesOnTrialSessionAction } from '../actions/TrialSession/setEligibleCasesOnTrialSessionAction';
 import { setTrialSessionDetailsAction } from '../actions/TrialSession/setTrialSessionDetailsAction';
 
 export const dismissThirtyDayTrialAlertSequence = [
@@ -19,19 +15,9 @@ export const dismissThirtyDayTrialAlertSequence = [
     success: [
       getTrialSessionDetailsAction,
       setTrialSessionDetailsAction,
-      isTrialSessionCalendaredAction,
-      {
-        no: [
-          getEligibleCasesForTrialSessionAction,
-          setEligibleCasesOnTrialSessionAction,
-          mergeCaseOrderIntoEligibleCasesAction,
-        ],
-        yes: [
-          getCalendaredCasesForTrialSessionAction,
-          setCalendaredCasesOnTrialSessionAction,
-          mergeCaseOrderIntoCalendaredCasesAction,
-        ],
-      },
+      getCalendaredCasesForTrialSessionAction,
+      setCalendaredCasesOnTrialSessionAction,
+      mergeCaseOrderIntoCalendaredCasesAction,
     ],
   },
 ];
