8ce838b8c3a9772536614d11fcde87f00d4f4b8a
Jake Roberts <jdr81394@gmail.com>
2026-05-06 10:15:41 -0400

    prevent tabs from display on test

--- web-client/src/views/Dashboards/DashboardClerkOfCourt.tsx
+++ web-client/src/views/Dashboards/DashboardClerkOfCourt.tsx
@@ -23,6 +23,9 @@ export const DashboardClerkOfCourt = connect(
     user: state.user,
   },
   function DashboardClerkOfCourt({ clerkOfCourtDashboard, user }) {
+    const shouldDisplay =
+      process.env.USTC_ENV !== 'prod' && process.env.USTC_ENV !== 'test';
+
     const {
       caseTypeBreakdownDatasets,
       caseTypeBreakdownLabels,
@@ -54,75 +57,81 @@ export const DashboardClerkOfCourt = connect(
                 <Tab tabName="recentMessages" title="Recent Messages">
                   <RecentMessagesCotC />
                 </Tab>
-                <Tab tabName="pieChart" title="Pie Chart">
-                  <div className="tw:mt-6 tw:mx-4">
-                    <h2 className="tw:xs:text-2xl tw:text-lg">
-                      Total sessions scheduled: {totalSessionsScheduled}
-                    </h2>
-                    <div className="tw:flex tw:flex-wrap tw:gap-12 tw:mt-4">
-                      <PieGraph
-                        title="Procedure Type"
-                        data={procedureTypePieData}
+                {shouldDisplay && (
+                  <Tab tabName="pieChart" title="Pie Chart">
+                    <div className="tw:mt-6 tw:mx-4">
+                      <h2 className="tw:xs:text-2xl tw:text-lg">
+                        Total sessions scheduled: {totalSessionsScheduled}
+                      </h2>
+                      <div className="tw:flex tw:flex-wrap tw:gap-12 tw:mt-4">
+                        <PieGraph
+                          title="Procedure Type"
+                          data={procedureTypePieData}
+                        />
+                        <PieGraph
+                          title="Session Type"
+                          data={sessionTypePieData}
+                        />
+                      </div>
+                    </div>
+                  </Tab>
+                )}
+                {shouldDisplay && (
+                  <Tab tabName="barGraph" title="Bar Graph">
+                    <div className="tw:mt-6">
+                      <SingleBarGraph
+                        showLabels={false}
+                        title="Created Special Sessions by Location"
+                        width={CHART_WIDTH}
+                        data={specialSessionsByLocation}
                       />
-                      <PieGraph
-                        title="Session Type"
-                        data={sessionTypePieData}
+                      <div className="tw:mt-12" />
+                      <MultiBarGraph
+                        showLabels={false}
+                        stacked
+                        title="Total Petitions by Month"
+                        width={CHART_WIDTH}
+                        xLabelRotation={45}
+                        datasets={petitionsByMonthDatasets}
+                        labels={petitionsByMonthLabels}
+                      />
+                      <div className="tw:mt-12" />
+                      <MultiBarGraph
+                        showLabels={false}
+                        title="Closed/Closed - Dismissed &amp; Changed to On Appeal"
+                        width={CHART_WIDTH}
+                        xLabelRotation={45}
+                        datasets={closedCasesDatasets}
+                        labels={closedCasesLabels}
                       />
                     </div>
-                  </div>
-                </Tab>
-                <Tab tabName="barGraph" title="Bar Graph">
-                  <div className="tw:mt-6">
-                    <SingleBarGraph
-                      showLabels={false}
-                      title="Created Special Sessions by Location"
-                      width={CHART_WIDTH}
-                      data={specialSessionsByLocation}
-                    />
-                    <div className="tw:mt-12" />
-                    <MultiBarGraph
-                      showLabels={false}
-                      stacked
-                      title="Total Petitions by Month"
-                      width={CHART_WIDTH}
-                      xLabelRotation={45}
-                      datasets={petitionsByMonthDatasets}
-                      labels={petitionsByMonthLabels}
-                    />
-                    <div className="tw:mt-12" />
-                    <MultiBarGraph
-                      showLabels={false}
-                      title="Closed/Closed - Dismissed &amp; Changed to On Appeal"
-                      width={CHART_WIDTH}
-                      xLabelRotation={45}
-                      datasets={closedCasesDatasets}
-                      labels={closedCasesLabels}
-                    />
-                  </div>
-                </Tab>
-                <Tab tabName="lineGraph" title="Line Graph">
-                  <div className="tw:mt-6">
-                    <LineGraph
-                      smooth
-                      title="Cases Filed Over Time"
-                      width={CHART_WIDTH}
-                      xAxisLabel="Month"
-                      xLabelRotation={45}
-                      yAxisLabel="Number of Cases"
-                      datasets={casesFiledDatasets}
-                      labels={casesFiledLabels}
-                    />
-                    <div className="tw:mt-[6.25rem]" />
-                    <LineGraph
-                      title="Case Type Breakdown by Quarter"
-                      width={CHART_WIDTH}
-                      xAxisLabel="Quarter"
-                      yAxisLabel="Number of Cases"
-                      datasets={caseTypeBreakdownDatasets}
-                      labels={caseTypeBreakdownLabels}
-                    />
-                  </div>
-                </Tab>
+                  </Tab>
+                )}
+                {shouldDisplay && (
+                  <Tab tabName="lineGraph" title="Line Graph">
+                    <div className="tw:mt-6">
+                      <LineGraph
+                        smooth
+                        title="Cases Filed Over Time"
+                        width={CHART_WIDTH}
+                        xAxisLabel="Month"
+                        xLabelRotation={45}
+                        yAxisLabel="Number of Cases"
+                        datasets={casesFiledDatasets}
+                        labels={casesFiledLabels}
+                      />
+                      <div className="tw:mt-[6.25rem]" />
+                      <LineGraph
+                        title="Case Type Breakdown by Quarter"
+                        width={CHART_WIDTH}
+                        xAxisLabel="Quarter"
+                        yAxisLabel="Number of Cases"
+                        datasets={caseTypeBreakdownDatasets}
+                        labels={caseTypeBreakdownLabels}
+                      />
+                    </div>
+                  </Tab>
+                )}
               </Tabs>
             </NonMobile>
             <Mobile>
@@ -135,9 +144,13 @@ export const DashboardClerkOfCourt = connect(
                   onChange={e => setMobileSection(e.target.value)}
                 >
                   <option value="recentMessages">Recent Messages</option>
-                  <option value="pieChart">Pie Chart</option>
-                  <option value="barGraph">Bar Graph</option>
-                  <option value="lineGraph">Line Graph</option>
+                  {shouldDisplay && (
+                    <>
+                      <option value="pieChart">Pie Chart</option>
+                      <option value="barGraph">Bar Graph</option>
+                      <option value="lineGraph">Line Graph</option>
+                    </>
+                  )}
                 </select>
               </div>
               {mobileSection === 'recentMessages' && (
