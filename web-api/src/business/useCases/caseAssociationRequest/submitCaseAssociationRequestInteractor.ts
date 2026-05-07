4a883d518c70b508732d9b9cd9ffddf36a552f3f
Paulo Da Rocha Filho <paulo.filho.ctr@gmail.com>
2026-05-07 11:30:49 -0500

    9697: fix test client integration failures in submit case association

--- web-api/src/business/useCases/caseAssociationRequest/submitCaseAssociationRequestInteractor.ts
+++ web-api/src/business/useCases/caseAssociationRequest/submitCaseAssociationRequestInteractor.ts
@@ -1,27 +1,22 @@
-import { ROLES } from '../../../../../shared/src/business/entities/EntityConstants';
+import { ROLES } from '@shared/business/entities/EntityConstants';
 import {
   ROLE_PERMISSIONS,
   isAuthorized,
-} from '../../../../../shared/src/authorization/authorizationClientService';
+} from '@shared/authorization/authorizationClientService';
 import { ServerApplicationContext } from '@web-api/applicationContext';
 import { NotFoundError, UnauthorizedError } from '@web-api/errors/errors';
 import { UnknownAuthUser } from '@shared/business/entities/authUser/AuthUser';
 import { getUserById } from '@web-api/persistence/postgres/users/getUserById';
 import { withLocking } from '@web-api/persistence/postgres/utils/mutex';
 import { RawPractitioner } from '@shared/business/entities/Practitioner';
-import { CaseFactory } from '@web-api/business/entities/cases/CaseFactory';
-import { CaseDTO } from '@shared/business/dto/cases/CaseDTO';
-import { PublicCaseDTO } from '@shared/business/dto/cases/PublicCaseDTO';
-import { RestrictedCaseDTO } from '@shared/business/dto/cases/RestrictedCaseDTO';
 
 /**
  * submitCaseAssociationRequestInteractor
  * @param {object} applicationContext the application context
  * @param {object} providers the providers object
- * @param {array}  providers.consolidatedCasesDocketNumbers a list of the docketNumbers on which to file the case association document
  * @param {string} providers.docketNumber the docket number of the case
  * @param {string} providers.filers the parties represented by the practitioner
- * @returns {Promise<CaseDTO | PublicCaseDTO | RestrictedCaseDTO>} the promise of the case association request
+ * @returns {Promise<void>} the promise of the case association request
  */
 const submitCaseAssociationRequest = async (
   applicationContext: ServerApplicationContext,
@@ -33,7 +28,7 @@ const submitCaseAssociationRequest = async (
     filers: string[];
   },
   authorizedUser: UnknownAuthUser,
-): Promise<CaseDTO | PublicCaseDTO | RestrictedCaseDTO | undefined> => {
+): Promise<void> => {
   if (
     !isAuthorized(authorizedUser, ROLE_PERMISSIONS.ASSOCIATE_SELF_WITH_CASE)
   ) {
@@ -51,7 +46,7 @@ const submitCaseAssociationRequest = async (
   const isIrsPractitioner = authorizedUser.role === ROLES.irsPractitioner;
 
   if (isPrivatePractitioner && filers) {
-    const theCase = await applicationContext
+    await applicationContext
       .getUseCaseHelpers()
       .associatePrivatePractitionerToCase({
         authorizedUser,
@@ -60,25 +55,17 @@ const submitCaseAssociationRequest = async (
         user: user as RawPractitioner,
       });
 
-    return CaseFactory.getCaseDTO({
-      rawCase: theCase,
-      user: authorizedUser,
-    });
+    return;
   }
 
   if (isIrsPractitioner) {
-    const theCase = await applicationContext
+    await applicationContext
       .getUseCaseHelpers()
       .associateIrsPractitionerToCase({
         authorizedUser,
         docketNumber,
         user,
       });
-
-    return CaseFactory.getCaseDTO({
-      rawCase: theCase,
-      user: authorizedUser,
-    });
   }
 };
 
