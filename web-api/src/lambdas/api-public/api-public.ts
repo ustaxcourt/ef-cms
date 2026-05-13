300859614f5874559179108b534badec5e2227ec
Paulo Da Rocha Filho <paulo.filho.ctr@gmail.com>
2026-05-13 13:28:22 -0500

    9697: type api handlers for serverless-express v5.0.0

--- web-api/src/lambdas/api-public/api-public.ts
+++ web-api/src/lambdas/api-public/api-public.ts
@@ -1,6 +1,15 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { app } from '../../app-public';
 import awsServerlessExpress from '@codegenie/serverless-express';
 
-export const handler = (event, context) => {
-  return awsServerlessExpress({ app })(event, context);
+export const handler = async (
+  event: APIGatewayProxyEvent,
+  context: any,
+): Promise<APIGatewayProxyResult> => {
+  return (
+    awsServerlessExpress({ app }) as unknown as (
+      event: APIGatewayProxyEvent,
+      context: any,
+    ) => Promise<APIGatewayProxyResult>
+  )(event, context);
 };
