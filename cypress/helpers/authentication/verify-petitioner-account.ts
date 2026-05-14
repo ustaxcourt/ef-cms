dde57c24315dd4c71dfa10c20471846792714c93
TomElliottFlexion <telliott@flexion.us>
2026-05-14 11:57:34 -0500

    devex: fix old lint rule violations in cypress tests

--- cypress/helpers/authentication/verify-petitioner-account.ts
+++ cypress/helpers/authentication/verify-petitioner-account.ts
@@ -2,8 +2,8 @@ import qs from 'qs';
 
 export function verifyPetitionerAccount({ email }: { email: string }) {
   cy.task('getNewAccountVerificationCode', { email }).then((userInfo: any) => {
-    expect(userInfo).to.have.property('userId').that.is.not.undefined;
-    expect(userInfo).to.have.property('confirmationCode').that.is.not.undefined;
+    expect(userInfo).to.have.property('userId');
+    expect(userInfo).to.have.property('confirmationCode');
     const { confirmationCode, userId } = userInfo;
     const queryString = qs.stringify(
       { confirmationCode, email, userId },
