52ccaf7bfc76ca5adc2ff7d0f508344fe873bac9
Abel <Abel.Ratanaphan.ctr@ustaxcourt.gov>
2026-05-14 17:19:41 -0400

    8477: borrow 9745 code to optimize inbox notification query
    Co-authored-by: Christie Laferriere

--- web-api/src/business/useCases/getNotificationsInteractor.ts
+++ web-api/src/business/useCases/getNotificationsInteractor.ts
@@ -4,13 +4,13 @@ import {
   UnknownAuthUser,
   isAuthUser,
 } from '@shared/business/entities/authUser/AuthUser';
-import { getDocumentQCInboxForSection } from '@web-api/persistence/postgres/workitems/getDocumentQCInboxForSection';
-import { getDocumentQCInboxForUser } from '@web-api/persistence/postgres/workitems/getDocumentQCInboxForUser';
 import { getSectionInboxMessages } from '@web-api/persistence/postgres/messages/getSectionInboxMessages';
 import { getUserInboxMessages } from '@web-api/persistence/postgres/messages/getUserInboxMessages';
-import { getWorkQueueFilters } from '@shared/business/utilities/getWorkQueueFilters';
-import { RawWorkItemWithCaseAndDocketEntryInfo } from '@web-api/persistence/postgres/workitems/schema';
 import { getQCInboxParameters } from '@web-api/business/utilities/getQCInboxParameters';
+import {
+  getDocumentQCInboxCountsForUser,
+  getDocumentQCInboxCountsForSection,
+} from '@web-api/persistence/postgres/workitems/getDocumentQCInboxCounts';
 
 export const getNotificationsInteractor = async (
   applicationContext: ServerApplicationContext,
@@ -48,111 +48,38 @@ export const getNotificationsInteractor = async (
     selectedSection,
   });
 
-  const filters = getWorkQueueFilters({
-    section: qcInboxParameters.section,
-    user: { ...authorizedUser, section: qcInboxParameters.section },
-  });
-
-  applicationContext.logger.info(
-    'getNotificationsInteractor about to start queries',
-    {
-      sectionToDisplay: qcInboxParameters.section,
-    },
-  );
-
-  const [
-    userInbox,
-    sectionInbox,
-    documentQCIndividualInbox,
-    documentQCSectionInbox,
-  ] = await Promise.all([
-    getUserInboxMessages({
-      applicationContext,
-      userId: authorizedUser.userId,
-    }),
-    getSectionInboxMessages({
-      applicationContext,
-      section: selectedSection || section,
-    }),
-    getDocumentQCInboxForUser({
-      userId: authorizedUser.userId,
-    }),
-    getDocumentQCInboxForSection(qcInboxParameters),
-  ]);
-
-  applicationContext.logger.info(
-    'getNotificationsInteractor queries complete',
-    {
-      documentQCIndividualInbox: documentQCIndividualInbox.length,
-      documentQCSectionInbox: documentQCSectionInbox?.length,
-      sectionInbox: sectionInbox.length,
-      userInbox: userInbox.length,
-    },
-  );
-
-  const qcIndividualInProgressCount = countUniqueWorkItems(
-    documentQCIndividualInbox.filter(filters['my']['inProgress']),
-  );
-
-  const qcIndividualInboxCount = countUniqueWorkItems(
-    documentQCIndividualInbox.filter(filters['my']['inbox']),
-  );
-
-  const qcSectionInProgressCount = countUniqueWorkItems(
-    (documentQCSectionInbox ?? []).filter(filters['section']['inProgress']),
-  );
-
-  const qcSectionInboxCount = countUniqueWorkItems(
-    (documentQCSectionInbox ?? []).filter(filters['section']['inbox']),
-  );
+  const [userInbox, sectionInbox, userQCCounts, sectionQCCounts] =
+    await Promise.all([
+      getUserInboxMessages({
+        applicationContext,
+        userId: authorizedUser.userId,
+      }),
+      getSectionInboxMessages({
+        applicationContext,
+        section: selectedSection || section,
+      }),
+      getDocumentQCInboxCountsForUser({
+        userId: authorizedUser.userId,
+        role: authorizedUser.role,
+        section: qcInboxParameters.section,
+      }),
+      getDocumentQCInboxCountsForSection({
+        ...qcInboxParameters,
+        role: authorizedUser.role,
+      }),
+    ]);
 
   const unreadMessageCount = userInbox.filter(
     message => !message.isRead,
   ).length;
 
-  applicationContext.logger.info('getNotificationsInteractor done filtering', {
-    qcIndividualInProgressCount,
-    qcIndividualInboxCount,
-    qcSectionInProgressCount,
-    qcSectionInboxCount,
-    unreadMessageCount,
-  });
-
   return {
-    qcIndividualInProgressCount,
-    qcIndividualInboxCount,
-    qcSectionInProgressCount,
-    qcSectionInboxCount,
+    qcIndividualInProgressCount: userQCCounts.inProgressCount,
+    qcIndividualInboxCount: userQCCounts.inboxCount,
+    qcSectionInProgressCount: sectionQCCounts.inProgressCount,
+    qcSectionInboxCount: sectionQCCounts.inboxCount,
     unreadMessageCount,
     userInboxCount: userInbox.length,
     userSectionCount: sectionInbox.length,
   };
-};
-
-const countUniqueWorkItems = (
-  workItems: RawWorkItemWithCaseAndDocketEntryInfo[],
-): number => {
-  let count = 0;
-  const consolidatedGroups = new Map<
-    string,
-    RawWorkItemWithCaseAndDocketEntryInfo[]
-  >();
-
-  for (const workItem of workItems) {
-    const multiDocketedOnLength =
-      workItem.docketEntry.multiDocketedOn?.length ?? 0;
-
-    if (multiDocketedOnLength < 2) {
-      count += 1;
-    } else {
-      const key = workItem.docketEntryId;
-      const group = consolidatedGroups.get(key) ?? [];
-      group.push(workItem);
-      consolidatedGroups.set(key, group);
-    }
-  }
-
-  count += consolidatedGroups.size;
-
-  return count;
 };
