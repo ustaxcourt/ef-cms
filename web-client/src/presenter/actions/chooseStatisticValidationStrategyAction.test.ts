c30e546d56cae4c93a1d9cbfba67dcb7278f294e
Abel <Abel.Ratanaphan.ctr@ustaxcourt.gov>
2026-05-15 11:12:38 -0400

    9771: add caseDetail path to chooseStatisticValidationStrategyAction test
    Co-authored-by: Christie Laferriere

--- web-client/src/presenter/actions/chooseStatisticValidationStrategyAction.test.ts
+++ web-client/src/presenter/actions/chooseStatisticValidationStrategyAction.test.ts
@@ -4,11 +4,13 @@ import { runAction } from '@web-client/presenter/test.cerebral';
 
 describe('chooseStatisticValidationStrategyAction', () => {
   const addEditStatisticMock = jest.fn();
+  const caseDetailMock = jest.fn();
   const startCaseMock = jest.fn();
 
   beforeAll(() => {
     presenter.providers.path = {
       addEditStatistic: addEditStatisticMock,
+      caseDetail: caseDetailMock,
       startCase: startCaseMock,
     };
   });
@@ -17,35 +19,41 @@ describe('chooseStatisticValidationStrategyAction', () => {
     await runAction(chooseStatisticValidationStrategyAction, {
       modules: { presenter },
       state: {
+        form: { isPaper: false },
         modal: { statisticIndex: undefined },
       },
     });
 
     expect(addEditStatisticMock).toHaveBeenCalled();
     expect(startCaseMock).not.toHaveBeenCalled();
+    expect(caseDetailMock).not.toHaveBeenCalled();
   });
 
-  it('should call path.startCase when statisticIndex is defined', async () => {
+  it('should call path.startCase when statisticIndex is 0 and isPaper is true', async () => {
     await runAction(chooseStatisticValidationStrategyAction, {
       modules: { presenter },
       state: {
-        modal: { statisticIndex: 1 },
-      },
-    });
-
-    expect(addEditStatisticMock).not.toHaveBeenCalled();
-    expect(startCaseMock).toHaveBeenCalled();
-  });
-
-  it('should call path.startCase when statisticIndex is 0', async () => {
-    await runAction(chooseStatisticValidationStrategyAction, {
-      modules: { presenter },
-      state: {
+        form: { isPaper: true },
         modal: { statisticIndex: 0 },
       },
     });
 
-    expect(addEditStatisticMock).not.toHaveBeenCalled();
     expect(startCaseMock).toHaveBeenCalled();
+    expect(caseDetailMock).not.toHaveBeenCalled();
+    expect(addEditStatisticMock).not.toHaveBeenCalled();
+  });
+
+  it('should call path.caseDetail when statisticIndex is 0 and isPaper is false', async () => {
+    await runAction(chooseStatisticValidationStrategyAction, {
+      modules: { presenter },
+      state: {
+        form: { isPaper: false },
+        modal: { statisticIndex: 0 },
+      },
+    });
+
+    expect(caseDetailMock).toHaveBeenCalled();
+    expect(startCaseMock).not.toHaveBeenCalled();
+    expect(addEditStatisticMock).not.toHaveBeenCalled();
   });
 });
