bb3e23696f92f6a41eb3f99aaac9f096b7ccbba1
Abel <Abel.Ratanaphan.ctr@ustaxcourt.gov>
2026-05-15 12:32:56 -0400

    9971: remove missed import and type Function
    Co-authored-by: Christie Laferriere <christie.laferriere.ctr@ustaxcourt.gov>

--- web-client/src/views/StartCaseInternal/StatisticsForm.tsx
+++ web-client/src/views/StartCaseInternal/StatisticsForm.tsx
@@ -2,14 +2,13 @@ import { Button } from '../../ustc-ui/Button/Button';
 import { DateSelector } from '@web-client/ustc-ui/DateInput/DateSelector';
 import { DollarsInput } from '../../ustc-ui/DollarsInput/DollarsInput';
 import { FormGroup } from '../../ustc-ui/FormGroup/FormGroup';
-import { ValidateFormData } from '@web-client/views/IRSNotice';
 import { connect } from '@web-client/presenter/shared.cerebral';
 import { sequences } from '@web-client/presenter/app.cerebral';
 import { state } from '@web-client/presenter/app.cerebral';
 import React from 'react';
 
 type StatisticsFormType = {
-  validateFormData: ValidateFormData;
+  validateFormData: Function;
 };
 
 const statisticsFormDependencies = {
