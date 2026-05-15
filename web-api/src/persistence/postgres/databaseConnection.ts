5586f16ea742bd44e55ce4bf945a01f276dc68bc
devinganey613 <dganey@gunnison.biz>
2026-05-15 12:43:22 -0400

    9009: Fixed test. Updated import.

--- web-api/src/persistence/postgres/databaseConnection.ts
+++ web-api/src/persistence/postgres/databaseConnection.ts
@@ -2,7 +2,7 @@ import { CamelCasePlugin, Kysely, PostgresDialect, Transaction } from 'kysely';
 import { Database } from '@web-api/persistence/postgres/database-schema';
 import { Pool, PoolConfig } from 'pg';
 import { Signer } from '@aws-sdk/rds-signer';
-import { environment } from '../../environment';
+import { environment } from '@web-api/environment';
 import fs from 'fs';
 import { AsyncLocalStorage } from 'async_hooks';
 import { getDawsonLogger } from '@web-api/utilities/logger/getDawsonLogger';
