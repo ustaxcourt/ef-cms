5d2dd6a6730982fde87965de2258a424dba42f5b
Alex Dziarnowski <alexdziarn@gmail.com>
2026-05-18 12:20:41 -0400

    9776: waits for cloudfront invalidation to occur

--- web-client/deploy-public.sh
+++ web-client/deploy-public.sh
@@ -21,9 +21,13 @@ aws s3 sync dist-public "s3://failover-${DEPLOYING_COLOR}.${EFCMS_DOMAIN}" --del
 aws s3 cp "s3://failover-${DEPLOYING_COLOR}.${EFCMS_DOMAIN}/index.html" "s3://failover-${DEPLOYING_COLOR}.${EFCMS_DOMAIN}/index.html" --metadata-directive REPLACE --content-type text/html --cache-control max-age=0
 
 # invalidate cloudfront cache for this color (public site)
-DISTRIBUTION_ID=$(aws cloudfront list-distributions --query "DistributionList.Items[?Aliases.Items && contains(Aliases.Items, '${DEPLOYING_COLOR}.${EFCMS_DOMAIN}')].Id | [0]" --output text)
-if [ -z "${DISTRIBUTION_ID}" ] || [ "${DISTRIBUTION_ID}" = "None" ]; then
-  echo "Could not find CloudFront distribution for ${DEPLOYING_COLOR}.${EFCMS_DOMAIN}"
-  exit 1
+PUBLIC_DISTRIBUTION_ID=$(aws cloudfront list-distributions --query "DistributionList.Items[?Aliases.Items && contains(Aliases.Items, '${DEPLOYING_COLOR}.${EFCMS_DOMAIN}')].Id | [0]" --output text)
+if [ -n "${PUBLIC_DISTRIBUTION_ID}" ] && [ "${PUBLIC_DISTRIBUTION_ID}" != "None" ]; then
+  echo "Creating CloudFront invalidation for distribution ${PUBLIC_DISTRIBUTION_ID}..."
+  INVALIDATION_ID=$(aws cloudfront create-invalidation --distribution-id "${PUBLIC_DISTRIBUTION_ID}" --paths "/*" --query "Invalidation.Id" --output text)
+  echo "Invalidation ${INVALIDATION_ID} created. Waiting for completion..."
+  aws cloudfront wait invalidation-completed --distribution-id "${PUBLIC_DISTRIBUTION_ID}" --id "${INVALIDATION_ID}"
+  echo "Invalidation ${INVALIDATION_ID} completed."
+else
+  echo "No CloudFront distribution found for ${DEPLOYING_COLOR}.${EFCMS_DOMAIN}, skipping invalidation."
 fi
-aws cloudfront create-invalidation --distribution-id "${DISTRIBUTION_ID}" --paths "/*"
--- web-client/deploy-ui.sh
+++ web-client/deploy-ui.sh
@@ -18,9 +18,13 @@ aws s3 sync dist "s3://app-failover-${DEPLOYING_COLOR}.${EFCMS_DOMAIN}" --delete
 aws s3 cp "s3://app-failover-${DEPLOYING_COLOR}.${EFCMS_DOMAIN}/index.html" "s3://app-failover-${DEPLOYING_COLOR}.${EFCMS_DOMAIN}/index.html" --metadata-directive REPLACE --content-type text/html --cache-control max-age=0
 
 # invalidate cloudfront cache for this color
-DISTRIBUTION_ID=$(aws cloudfront list-distributions --query "DistributionList.Items[?Aliases.Items && contains(Aliases.Items, 'app-${DEPLOYING_COLOR}.${EFCMS_DOMAIN}')].Id | [0]" --output text)
-if [ -z "${DISTRIBUTION_ID}" ] || [ "${DISTRIBUTION_ID}" = "None" ]; then
-  echo "Could not find CloudFront distribution for app-${DEPLOYING_COLOR}.${EFCMS_DOMAIN}"
-  exit 1
+PRIVATE_DISTRIBUTION_ID=$(aws cloudfront list-distributions --query "DistributionList.Items[?Aliases.Items && contains(Aliases.Items, 'app-${DEPLOYING_COLOR}.${EFCMS_DOMAIN}')].Id | [0]" --output text)
+if [ -n "${PRIVATE_DISTRIBUTION_ID}" ] && [ "${PRIVATE_DISTRIBUTION_ID}" != "None" ]; then
+  echo "Creating CloudFront invalidation for distribution ${PRIVATE_DISTRIBUTION_ID}..."
+  INVALIDATION_ID=$(aws cloudfront create-invalidation --distribution-id "${PRIVATE_DISTRIBUTION_ID}" --paths "/*" --query "Invalidation.Id" --output text)
+  echo "Invalidation ${INVALIDATION_ID} created. Waiting for completion..."
+  aws cloudfront wait invalidation-completed --distribution-id "${PRIVATE_DISTRIBUTION_ID}" --id "${INVALIDATION_ID}"
+  echo "Invalidation ${INVALIDATION_ID} completed."
+else
+  echo "No CloudFront distribution found for app-${DEPLOYING_COLOR}.${EFCMS_DOMAIN}, skipping invalidation."
 fi
-aws cloudfront create-invalidation --distribution-id "${DISTRIBUTION_ID}" --paths "/*"
