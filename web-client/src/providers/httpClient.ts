55906dd64a1df1f8da01984d2ca547dddc0b6b67
Kwabena Ampofo <amp.kwabena@gmail.com>
2026-05-21 11:32:29 -0400

    9776: Test fix by breaking test on purpose for testing

--- web-client/src/providers/httpClient.ts
+++ web-client/src/providers/httpClient.ts
@@ -38,7 +38,6 @@ const setStoredDeploymentTimestamp = (
 
 export const getHttpClient = (
   forceRefreshCallback: () => void,
-  apiUrl: string,
 ): AxiosInstance => {
   axiosClient = axiosClient || axios.create();
 
@@ -50,9 +49,9 @@ export const getHttpClient = (
         (config as any)._stackError = new Error();
 
         const deploymentTimestamp = getStoredDeploymentTimestamp();
-        const isExternalRequest = config.url && !config.url.startsWith(apiUrl);
 
-        if (deploymentTimestamp && !isExternalRequest) {
+        // TODO: TEMP - remove this to re-introduce the CORS bug for test validation
+        if (deploymentTimestamp) {
           config.headers.set(X_DEPLOYMENT_TIMESTAMP, deploymentTimestamp);
         }
 
