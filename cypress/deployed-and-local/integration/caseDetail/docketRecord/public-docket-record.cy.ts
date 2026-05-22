a1619295ff7ec2513c8dec06a4b6954551bfecaf
Alex Dziarnowski <alexdziarn@gmail.com>
2026-05-22 11:47:56 -0400

    9776: fix cross-origin issue in cypress tests

--- cypress/deployed-and-local/integration/caseDetail/docketRecord/public-docket-record.cy.ts
+++ cypress/deployed-and-local/integration/caseDetail/docketRecord/public-docket-record.cy.ts
@@ -7,14 +7,21 @@ describe('Public Docket Record', () => {
       cy.get('[data-testid="account-menu-button"]').click();
       cy.get('[data-testid="logout-button-desktop"]').click();
 
-      cy.visit(getCypressEnv().publicSiteUrl);
-      cy.get('input#docket-number').type(docketNumber);
-      cy.get('button#docket-search-button').click();
-      cy.get('[data-testid="header-public-case-detail"]').contains(
-        `Docket Number: ${docketNumber}`,
+      const publicSiteUrl = getCypressEnv().publicSiteUrl;
+      cy.origin(
+        publicSiteUrl,
+        { args: { docketNumber, publicSiteUrl } },
+        ({ docketNumber: dn, publicSiteUrl: url }) => {
+          cy.visit(url);
+          cy.get('input#docket-number').type(dn);
+          cy.get('button#docket-search-button').click();
+          cy.get('[data-testid="header-public-case-detail"]').contains(
+            `Docket Number: ${dn}`,
+          );
+          cy.get('[data-testid="print-public-docket-record-button"]').click();
+          cy.get('[data-testid="modal-header"]');
+        },
       );
-      cy.get('[data-testid="print-public-docket-record-button"]').click();
-      cy.get('[data-testid="modal-header"]');
     });
   });
 });
