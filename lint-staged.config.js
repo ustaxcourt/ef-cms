51dbd8598dfe8a7681478f17686a4cf1d2591295
Abel <Abel.Ratanaphan.ctr@ustaxcourt.gov>
2026-05-28 10:26:43 -0400

    9031: use --check instead of --write in lint-staged config
    Co-authored-by: Christie Laferriere <christie.laferriere.ctr@ustaxcourt.gov>

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   '{shared,web-client}/**/*.{css,scss}': ['stylelint'],
   '{,scripts/**/,shared/**/,web-api/**/,web-client/**/,cypress/**/}*.{js,jsx,ts,tsx}':
-    ['prettier --write', 'eslint'],
+    ['prettier --check', 'eslint'],
   '{,scripts/**/,shared/**/,web-api/**/,web-client/**/}*.sh': [
     'lint-staged-shellcheck',
   ],
