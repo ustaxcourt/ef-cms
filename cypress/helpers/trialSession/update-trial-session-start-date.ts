f444dbbaf141d6ccd8ea7d3879ccae68ab6a0ef2
Paulo Da Rocha Filho <paulo.filho.ctr@gmail.com>
2026-06-09 09:03:53 -0500

    9607: tighten cypress tests for paper service and grant deny

--- cypress/helpers/trialSession/update-trial-session-start-date.ts
+++ cypress/helpers/trialSession/update-trial-session-start-date.ts
@@ -11,14 +11,21 @@ export const updateTrialSessionStartDate = (
   cy.get('[data-testid="start-date-picker"]').eq(1).clear();
   cy.get('[data-testid="start-date-picker"]').eq(1).type(newStartDate);
   cy.get('[data-testid="submit-edit-trial-session"]').click();
-  cy.get('body').then($body => {
-    if ($body.find('[data-testid="modal-button-confirm"]').length > 0) {
+
+  cy.get(
+    '[data-testid="current-start-date-info"], [data-testid="success-alert"]',
+    { timeout: 60000 },
+  ).then($el => {
+    if ($el.attr('data-testid') === 'current-start-date-info') {
+      cy.get('[data-testid="updated-start-date-info"]').should(
+        'contain.text',
+        newStartDate,
+      );
       cy.get('[data-testid="modal-button-confirm"]').click();
     }
   });
-  cy.get('[data-testid="success-alert"]').should('exist');
-  cy.get('[data-testid="success-alert"]').should(
-    'contain.text',
-    'Trial session updated',
-  );
+
+  cy.get('[data-testid="success-alert"]', { timeout: 120000 })
+    .should('exist')
+    .and('contain.text', 'Trial session updated');
 };
