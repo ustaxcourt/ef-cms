48f48d63d2b40985a1ac751ab42e6a5be72d8e67
devinganey613 <dganey@gunnison.biz>
2026-06-17 12:41:40 -0400

    9561: Fixed accessibility issues in piechart, removed tab index and updated contrast to 'no data' text. Added cypress tests for petitions data tab of cotc dashboard.

--- cypress/local-only/tests/accessibility/dashboard/clerk-of-court.cy.ts
+++ cypress/local-only/tests/accessibility/dashboard/clerk-of-court.cy.ts
@@ -12,9 +12,58 @@ describe('Dashboard - Clerk of Court Accessibility', () => {
     checkA11y();
   });
 
+  describe('Petitions tab', () => {
+    it('should be free of a11y issues when viewing petitions data', () => {
+      // mock the call with minimal petitions data so we can guarentee the pie chart renders
+      cy.intercept('GET', '**/clerk-dashboard-stats', {
+        statusCode: 200,
+        body: {
+          calendarYearPetitionStats: {
+            petitionFullPaperMonths: [],
+            petitionFullElectronicMonths: [],
+            petitionsByRepresentation: [
+              {
+                total: 50,
+                isRepresenting: false,
+              },
+              {
+                total: 50,
+                isRepresenting: true,
+              },
+            ],
+            petitionsByServiceType: [
+              {
+                total: 66,
+                isPaper: false,
+              },
+              {
+                total: 34,
+                isPaper: true,
+              },
+            ],
+          },
+          fiscalYearPetitionStats: {
+            petitionFullPaperMonths: [],
+            petitionFullElectronicMonths: [],
+            petitionsByRepresentation: [],
+            petitionsByServiceType: [],
+          },
+          year: '2026',
+        },
+      }).as('mockStats');
+      loginAsClerkOfCourt();
+      cy.get('#tabButton-petitions').click();
+
+      cy.get('[data-testid="petitions-data-div"]').should('exist');
+
+      checkA11y();
+    });
+  });
+
   describe('Recent Messages tab', () => {
     it('should be free of a11y issues when viewing recent messages', () => {
       loginAsClerkOfCourt();
+      cy.get('#tabButton-recentMessages').click();
 
       cy.get('[data-testid="recent-messages-table"]').should('exist');
 
@@ -25,6 +74,7 @@ describe('Dashboard - Clerk of Court Accessibility', () => {
   describe('Keyboard navigation', () => {
     it('should have focusable interactive elements', () => {
       loginAsClerkOfCourt();
+      cy.get('#tabButton-recentMessages').click();
 
       cy.get('a:contains("View All Messages")')
         .should('be.visible')
