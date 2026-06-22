874e54245b0af8001145353cce9e1cb97e19306f
Jake Roberts <jdr81394@gmail.com>
2026-06-22 16:16:24 -0400

    9276: REMOVE - this is to get more of an error message

--- web-client/src/presenter/actions/createTestApiErrorAction.ts
+++ web-client/src/presenter/actions/createTestApiErrorAction.ts
@@ -1,17 +1,13 @@
-import { state } from '@web-client/presenter/app.cerebral';
-
 /**
- * intentionally throws an error to test RUM http telemetry.
+ * intentionally hits a nonexistent API endpoint to test RUM http telemetry.
  * @param {object} providers the providers object
  * @param {object} providers.applicationContext the application context
- * @param {Function} providers.get the cerebral get function
  * @returns {Promise<*>}
  */
 export const createTestApiErrorAction = ({
   applicationContext,
-  get,
 }: ActionProps) => {
-  const url = get(state.currentPage);
+  const url = `${applicationContext.getBaseUrl()}/api/does-not-exist`;
   return applicationContext
     .getUseCases()
     .createTestApiErrorInteractor(applicationContext, {
