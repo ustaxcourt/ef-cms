ec50d4669e273e37191d2fc2a9b131621d77bbac
TomElliottFlexion <telliott@flexion.us>
2026-07-06 18:40:46 -0500

    10144: remove serviceIndicator, contactId from PublicUser

--- shared/src/business/entities/cases/PublicContact.ts
+++ shared/src/business/entities/cases/PublicContact.ts
@@ -3,7 +3,6 @@ import {
   CONTACT_TYPES,
   PRACTICE_TYPE_OPTIONS,
   PRACTITIONER_TYPE_OPTIONS,
-  SERVICE_INDICATOR_TYPES,
 } from '../EntityConstants';
 import { JoiValidationConstants } from '../JoiValidationConstants';
 import { JoiValidationEntity } from '../JoiValidationEntity';
@@ -12,13 +11,11 @@ export class PublicContact extends JoiValidationEntity {
   public admissionsDate?: string;
   public admissionsStatus?: string;
   public barNumber?: string;
-  public contactId: string;
   public contactType?: string;
   public name?: string;
   public originalBarState?: string;
   public practiceType?: string;
   public practitionerType?: string;
-  public serviceIndicator: string;
   public state?: string;
 
   constructor(rawProps) {
@@ -27,13 +24,11 @@ export class PublicContact extends JoiValidationEntity {
     this.admissionsDate = rawProps.admissionsDate;
     this.admissionsStatus = rawProps.admissionsStatus;
     this.barNumber = rawProps.barNumber;
-    this.contactId = rawProps.contactId || rawProps.userId;
     this.contactType = rawProps.contactType;
     this.name = rawProps.name;
     this.originalBarState = rawProps.originalBarState;
     this.practiceType = rawProps.practiceType;
     this.practitionerType = rawProps.practitionerType;
-    this.serviceIndicator = rawProps.serviceIndicator;
     this.state = rawProps.state;
   }
 
@@ -43,7 +38,6 @@ export class PublicContact extends JoiValidationEntity {
       ...ADMISSIONS_STATUS_OPTIONS,
     ).optional(),
     barNumber: JoiValidationConstants.STRING.max(100).optional(),
-    contactId: JoiValidationConstants.UUID.required(),
     contactType: JoiValidationConstants.STRING.valid(
       ...Object.values(CONTACT_TYPES),
     ).optional(),
@@ -54,9 +48,6 @@ export class PublicContact extends JoiValidationEntity {
     ).optional(),
     practitionerType: JoiValidationConstants.STRING.valid(
       ...PRACTITIONER_TYPE_OPTIONS,
-    ).optional(),
-    serviceIndicator: JoiValidationConstants.STRING.valid(
-      ...Object.values(SERVICE_INDICATOR_TYPES),
     ).optional(),
     state: JoiValidationConstants.STRING.optional(),
   };
