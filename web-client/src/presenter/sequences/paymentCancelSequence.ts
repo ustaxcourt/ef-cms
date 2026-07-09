f11deaadffea3142ef1fa627fd71c0db6e3110d4
Zachary Brown <zacharybrown204@yahoo.com>
2026-07-09 12:17:48 -0400

    6551 - Adding a guard so users cant follow cancel route if not related to case, or if case has been paid.

--- web-client/src/presenter/sequences/paymentCancelSequence.ts
+++ web-client/src/presenter/sequences/paymentCancelSequence.ts
@@ -3,16 +3,26 @@ import { getCaseAction } from '@web-client/presenter/actions/getCaseAction';
 import { setStepIndicatorAction } from '@web-client/presenter/actions/setStepIndicatorAction';
 import { setupCurrentPageAction } from '@web-client/presenter/actions/setupCurrentPageAction';
 import { setStepIndicatorInfoForPetitionGeneratorAction } from '@web-client/presenter/actions/setStepIndicatorInfoForPetitionGeneratorAction';
+import { getCaseAssociationAction } from '@web-client/presenter/actions/getCaseAssociationAction';
+import { redirectToDashboardAction } from '@web-client/presenter/actions/redirectToDashboardAction';
+import { checkCaseAssociationAndPaymentStatusAction } from '@web-client/presenter/actions/FilingFee/checkCaseAssociationAndPaymentStatusAction';
 
 //Does this need a guard once payment has been made??
 //Do we need to ensure that this is your case?
 export const paymentCancelSequence = [
-  setStepIndicatorInfoForPetitionGeneratorAction,
-  () => {
-    return { step: 7 };
-  },
-  setStepIndicatorAction,
-  getCaseAction, // Add error handling here
+  getCaseAction,
   setCaseAction,
-  setupCurrentPageAction('FilePetition'),
+  getCaseAssociationAction,
+  checkCaseAssociationAndPaymentStatusAction,
+  {
+    success: [
+      setStepIndicatorInfoForPetitionGeneratorAction,
+      () => {
+        return { step: 7 };
+      },
+      setStepIndicatorAction,
+      setupCurrentPageAction('FilePetition'),
+    ],
+    error: [redirectToDashboardAction],
+  },
 ];
