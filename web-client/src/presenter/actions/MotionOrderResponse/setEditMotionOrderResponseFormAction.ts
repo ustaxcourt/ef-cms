7ac7270cff71b6c2bd26dd93e678cfc3f64d3dff
Paulo Da Rocha Filho <paulo.filho.ctr@gmail.com>
2026-07-13 11:41:48 -0500

    10227: fix save draft edit hydration

--- web-client/src/presenter/actions/MotionOrderResponse/setEditMotionOrderResponseFormAction.ts
+++ web-client/src/presenter/actions/MotionOrderResponse/setEditMotionOrderResponseFormAction.ts
@@ -28,16 +28,21 @@ export const setEditMotionOrderResponseFormAction = ({
     ? `/messages/${caseDetail.docketNumber}/message-detail/${props.parentMessageId}/${docketEntryIdToEdit}/motion-order-response-edit`
     : `/case-detail/${caseDetail.docketNumber}/documents/${docketEntryIdToEdit}/motion-order-response-edit`;
 
-  const { ...draftFields } = draftOrderState;
-
   const rawAdditionalOrderTextArray =
     draftOrderState.additionalOrderTextArray ?? [];
 
   store.set(state.form, {
-    ...draftFields,
+    ...draftOrderState,
     additionalOrderTextArray: additionalOrderTextArrayWithRequiredFirstField(
       normalizeAdditionalOrderTextArray(rawAdditionalOrderTextArray),
     ),
+    docketEntryIdToEdit,
+    motionOrderResponse:
+      draftOrderState.motionOrderResponse ?? !!draftOrderState.dueDate,
+    strickenFromTrialSession:
+      draftOrderState.strickenFromTrialSession ??
+      draftOrderState.showStrickenFromTrialSession ??
+      false,
   });
   store.set(
     state.docketEntryId,
