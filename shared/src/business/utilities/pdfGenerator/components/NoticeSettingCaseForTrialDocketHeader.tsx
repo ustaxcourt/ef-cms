1693a79aa0be1e4769b745352e08d9e7f721f108
Paulo Da Rocha Filho <paulo.filho.ctr@gmail.com>
2026-07-14 14:36:40 -0500

    10076: update in person pdf spacing

--- shared/src/business/utilities/pdfGenerator/components/NoticeSettingCaseForTrialDocketHeader.tsx
+++ shared/src/business/utilities/pdfGenerator/components/NoticeSettingCaseForTrialDocketHeader.tsx
@@ -13,14 +13,14 @@ export const NoticeSettingCaseForTrialDocketHeader = ({
     <div className="order-docket-header">
       <div id="caption">
         <div id="caption-title">{caseTitle.toUpperCase()},</div>
-        <div id="caption-extension">{caseCaptionExtension.toUpperCase()}</div>
+        <div id="caption-extension">{caseCaptionExtension}</div>
         <div id="caption-v">v.</div>
         <div id="caption-commissioner">
           COMMISSIONER OF INTERNAL
           <br />
           REVENUE,
         </div>
-        <div id="caption-respondent">RESPONDENT</div>
+        <div id="caption-respondent">Respondent</div>
       </div>
       <div id="docket-number">Docket No. {docketNumberWithSuffix}</div>
       <div className="clear"></div>
