2b8225eacc9e31fbd72ab427f97ba110fa69b5dd
Paulo Da Rocha Filho <paulo.filho.ctr@gmail.com>
2026-07-20 13:52:56 -0500

    9607: fix duplicate keys in submitCourtIssuedOrderAction

--- web-client/src/presenter/actions/CourtIssuedOrder/submitCourtIssuedOrderAction.ts
+++ web-client/src/presenter/actions/CourtIssuedOrder/submitCourtIssuedOrderAction.ts
@@ -34,13 +34,11 @@ export const submitCourtIssuedOrderAction = async ({
   const documentMetadata = Object.fromEntries(
     Object.entries({
       additionalOrderTextArray: formData.additionalOrderTextArray,
-      additionalOrderTextArray: formData.additionalOrderTextArray,
       deniedAsMoot: formData.deniedAsMoot,
       deniedWithoutPrejudice: formData.deniedWithoutPrejudice,
       disposition: formData.disposition,
       dueDateMessage: formData.dueDateMessage,
       filingParty: formData.filingParty,
-      strickenFromTrialSession: formData.strickenFromTrialSession,
       affectedDocketEntries: formData.affectedDocketEntries,
       attachments: formData.attachments,
       date: formData.date,
@@ -85,14 +83,6 @@ export const submitCourtIssuedOrderAction = async ({
       trialLocation: formData.trialLocation,
     }).filter(([, value]) => value !== undefined),
   );
-
-  if (Array.isArray(documentMetadata.additionalOrderTextArray)) {
-    const forPersistence = normalizeAdditionalOrderTextArray(
-      documentMetadata.additionalOrderTextArray,
-    );
-    documentMetadata.additionalOrderTextArray =
-      forPersistence.length > 0 ? forPersistence : [];
-  }
 
   if (Array.isArray(documentMetadata.additionalOrderTextArray)) {
     const forPersistence = normalizeAdditionalOrderTextArray(
