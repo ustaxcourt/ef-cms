6e27e66e354f0f7f6468e4abd2da2c24218c9b36
Abel <Abel.Ratanaphan.ctr@ustaxcourt.gov>
2026-07-27 16:24:10 -0400

    10170: simplify trial cities feature flag setup
    Co-authored-by: Christie Laferriere <christie.laferriere.ctr@ustaxcourt.gov>

--- scripts/postgres/featureFlags/setup-new-trial-cities.ts
+++ scripts/postgres/featureFlags/setup-new-trial-cities.ts
@@ -1,32 +1,16 @@
 #!/usr/bin/env -S npx ts-node --transpile-only
 
 import { pgInsertInto } from '@web-api/persistence/postgres/utils/operation/pgInsertInto';
-import {
-  parseArgsAndEnvVars,
-  ScriptConfig,
-} from 'scripts/helpers/parseArgsAndEnvVars';
 
-const scriptConfig: ScriptConfig = {
-  description:
-    'setup-new-trial-cities - Sets whether the new trial cities are enabled',
-  parameters: {
-    enabled: {
-      default: false,
-      type: 'boolean',
-    },
-  },
-};
+async function setupNewTrialCities() {
+  const VALUE = false;
 
-const parsedArguments = parseArgsAndEnvVars(scriptConfig);
-const enabled = parsedArguments.enabled === true;
-
-async function setupNewTrialCities(): Promise<void> {
   await pgInsertInto({
     table: 'dwFeatureFlag',
     values: [
       {
         name: 'new-trial-cities',
-        value: { current: enabled },
+        value: { current: VALUE },
       },
     ],
     onConflictColumns: ['name'],
