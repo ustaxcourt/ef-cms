54b57432c52a25c3cfdcaf6c37fbb0a6006b5eff
Alex Dziarnowski <alexdziarn@gmail.com>
2026-07-30 11:25:19 -0400

    10242: add lock icon to service email

--- web-client/src/views/CaseDetail/PetitionersAndCounsel.tsx
+++ web-client/src/views/CaseDetail/PetitionersAndCounsel.tsx
@@ -76,6 +76,19 @@ export const PetitionersAndCounsel = connect(
                         })}
                         data-testid="petitioner-email"
                       >
+                        {petitioner.isAddressSealed && (
+                          <span
+                            aria-label="sealed paper petition email"
+                            className="sealed-contact-icon"
+                            role="img"
+                          >
+                            <FontAwesomeIcon
+                              className="margin-right-05"
+                              icon={['fas', 'lock']}
+                              size="sm"
+                            />
+                          </span>
+                        )}
                         <span>{petitioner.formattedEmail}</span>
                         {petitioner.showEAccessFlag && (
                           <FontAwesomeIcon
