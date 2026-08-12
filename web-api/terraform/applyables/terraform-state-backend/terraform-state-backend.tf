9c5f13e1bc6cff29334191e5489ed1ac2271ae82
nechamakrigsman <nechamafire@gmail.com>
2026-08-12 13:34:54 -0400

    10094: rename duplicate terrafrom resource;

--- web-api/terraform/applyables/terraform-state-backend/terraform-state-backend.tf
+++ web-api/terraform/applyables/terraform-state-backend/terraform-state-backend.tf
@@ -15,8 +15,8 @@ resource "aws_s3_bucket" "terraform_state_bucket" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_public_access_block" "terraform_state_bucket" {
-  bucket                  = aws_s3_bucket.terraform_state_bucket.id
+resource "aws_s3_bucket_public_access_block" "terraform_state_bucket_block" {
+  bucket                  = aws_s3_bucket.terraform_state_bucket_block.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
