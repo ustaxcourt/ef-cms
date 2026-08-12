01651b463d13cf5f30b5126eb7c56255e61a6ab3
nechamakrigsman <nechamafire@gmail.com>
2026-08-12 14:14:39 -0400

    10267: correct bucket reference in S3 public access block resource;

--- web-api/terraform/applyables/terraform-state-backend/terraform-state-backend.tf
+++ web-api/terraform/applyables/terraform-state-backend/terraform-state-backend.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket" "terraform_state_bucket" {
 }
 
 resource "aws_s3_bucket_public_access_block" "terraform_state_bucket_block" {
-  bucket                  = aws_s3_bucket.terraform_state_bucket_block.id
+  bucket                  = aws_s3_bucket.terraform_state_bucket.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
