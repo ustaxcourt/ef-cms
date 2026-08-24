d8304ad87baf59f169d171fb9ce863a32efb1ac1
Paulo Da Rocha Filho <paulo.filho.ctr@gmail.com>
2026-08-24 16:12:49 -0500

    dep-updates: revert Jest ESM config for @joi/date 3

--- scripts/jest-scripts.config.ts
+++ scripts/jest-scripts.config.ts
@@ -5,7 +5,6 @@ import { loadTsConfigPaths } from '../utils/load-tsconfig-paths.mjs';
 const tsConfigPaths = loadTsConfigPaths('tsconfig.json');
 
 const transformIgnoreModules = [
-  '@joi/date',
   'dom-serializer',
   'domelementtype',
   'domhandler',
@@ -84,7 +83,7 @@ const config: Config = {
       statements: 99,
     },
   },
-  moduleFileExtensions: ['js', 'jsx', 'mjs', 'ts', 'tsx'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   moduleNameMapper: {
     ...pathsToModuleNameMapper(tsConfigPaths, {
       prefix: '<rootDir>/../',
@@ -95,7 +94,7 @@ const config: Config = {
   testEnvironment: 'node',
   testMatch: ['<rootDir>/**/?(*.)+(spec|test).[jt]s?(x)'],
   transform: {
-    '\\.m?[jt]sx?$': ['babel-jest', { rootMode: 'upward' }],
+    '\\.[jt]sx?$': ['babel-jest', { rootMode: 'upward' }],
   },
   transformIgnorePatterns: [
     `/node_modules/(?!(${transformIgnoreModules.join('|')})/)`,
