4a452726d6dfd17d8cf6d9a1d9c52fb50b9e8ac1
devinganey613 <dganey@gunnison.biz>
2026-09-04 14:24:09 -0400

    9460: Added indexing to policies and roles

--- web-api/terraform/modules/everything-else-deprecated/cognito.tf
+++ web-api/terraform/modules/everything-else-deprecated/cognito.tf
@@ -250,7 +250,7 @@ module "cognito_pre_signup_lambda" {
   handler_file   = "./web-api/src/lambdas/cognitoPreSignup/cognitoPreSignupLambda.ts"
   handler_method = "cognitoPreSignupLambdaHandler"
   lambda_name    = "cognito_pre_signup_lambda_${var.environment}"
-  role           = aws_iam_role.pre_signup_lambda.arn
+  role           = aws_iam_role.pre_signup_lambda[0].arn
   environment = {
     IDP_NAME = var.idp_name
   }
@@ -264,7 +264,7 @@ module "cognito_inbound_federation_lambda" {
   handler_file   = "./web-api/src/lambdas/cognitoInboundFederation/cognitoInboundFederationLambda.ts"
   handler_method = "cognitoInboundFederationLambdaHandler"
   lambda_name    = "cognito_inbound_federation_lambda_${var.environment}"
-  role           = aws_iam_role.inbound_federation_lambda.arn
+  role           = aws_iam_role.inbound_federation_lambda[0].arn
   environment = {
     IDP_NAME = var.idp_name
   }
@@ -341,7 +341,7 @@ EOF
 resource "aws_iam_role_policy" "presignup_policy" {
   count = var.idp_name != "" ? 1 : 0
   name  = "pre_signup_policy_${var.environment}"
-  role  = aws_iam_role.pre_signup_lambda.id
+  role  = aws_iam_role.pre_signup_lambda[0].id
 
   policy = <<EOF
 {
@@ -374,7 +374,7 @@ EOF
 resource "aws_iam_role_policy" "inbound_federation_policy" {
   count = var.idp_name != "" ? 1 : 0
   name  = "inbound_federation_policy_${var.environment}"
-  role  = aws_iam_role.inbound_federation_lambda.id
+  role  = aws_iam_role.inbound_federation_lambda[0].id
 
   policy = <<EOF
 {
