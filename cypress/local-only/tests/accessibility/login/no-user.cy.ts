1ea9c28b5d571945ba352c78c43a209c9f150210
Zachary Brown <zacharybrown204@yahoo.com>
2026-09-14 14:53:03 -0400

    9460 - Accessability test

--- cypress/local-only/tests/accessibility/login/no-user.cy.ts
+++ cypress/local-only/tests/accessibility/login/no-user.cy.ts
@@ -1,6 +1,31 @@
+import { getCypressEnv } from '../../../../helpers/env/cypressEnvironment';
 import { checkA11y } from '../../../support/generalCommands/checkA11y';
 
 describe('Login - Accessibility', () => {
+  before(function () {
+    cy.task('getRawFeatureFlagValue', {
+      flag: 'allow-idp-login',
+    }).as('ORIGINAL_FEATURE_FLAG_VALUE');
+
+    cy.task('toggleFeatureFlag', {
+      flag: 'allow-idp-login',
+      flagValue: true,
+    });
+  });
+
+  after(function () {
+    if (getCypressEnv().isLocal) {
+      return;
+    }
+
+    cy.get('@ORIGINAL_FEATURE_FLAG_VALUE').then(ORIGINAL_FEATURE_FLAG_VALUE => {
+      cy.task('toggleFeatureFlag', {
+        flag: 'allow-idp-login',
+        flagValue: ORIGINAL_FEATURE_FLAG_VALUE,
+      });
+    });
+  });
+
   beforeEach(() => {
     Cypress.session.clearCurrentSessionData();
   });
